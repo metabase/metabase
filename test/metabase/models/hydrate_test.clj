73eec2b5785ddf6318ce9d858469df778d80649e
Cam Saul <cam@geotip.com>
2015-02-06 16:19:08 -0800

    better dox for hydration

--- test/metabase/models/hydrate_test.clj
+++ test/metabase/models/hydrate_test.clj
@@ -5,13 +5,15 @@
 (def obj1 {:a 1
            :b (fn [] 100)})
 
+;; make sure we can do basic hydration
 (expect {:a 1 :b 100}
         (hydrate obj1 :b))
 
+;; specifying "nested" hydration with no "nested" keys should still work
 (expect {:a 1 :b 100}
         (hydrate obj1 [:b]))
 
-
+;; check that returning an array works correctly
 (def obj2 {:c (fn [] [1 2 3])})
 (expect {:c [1 2 3]}
         (hydrate obj2 :c))
@@ -20,9 +22,11 @@
 (defn fn-that-returns-1 [] 1)
 (def obj3 {:d (fn [] {:e fn-that-returns-1})})
 
+;; check that nested keys aren't hydrated if we don't ask for it
 (expect {:d {:e fn-that-returns-1}}
         (hydrate obj3 :d))
 
+;; check that nested keys can be hydrated if we DO ask for it
 (expect {:d {:e 1}}
         (hydrate obj3 [:d :e]))
 
@@ -31,6 +35,7 @@
                      {:g (fn [] 2)}
                      {:g (fn [] 3)}])})
 
+;; check that nested hydration also works if one step returns multiple results
 (expect {:f [{:g 1}
              {:g 2}
              {:g 3}]}
