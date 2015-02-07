a985a2570345755ef3bfa4b4aa51f26d38a14f56
Cam Saul <cam@geotip.com>
2015-02-06 17:19:48 -0800

    implement `default-fields`

--- src/metabase/models/org_perm.clj
+++ src/metabase/models/org_perm.clj
@@ -3,3 +3,8 @@
 
 (defentity OrgPerm
   (table :core_userorgperm))
+
+(defmethod post-select OrgPerm [_ {:keys [organization_id user_id] :as result}]
+  (assoc result
+         :organization (sel-fn :one Org :id organization_id)
+         :user (sel-fn :one (resolve 'metabase.models.user/User) :id user_id)))  ; have to use `resolve` here to avoid circular dependencies
--- src/metabase/models/user.clj
+++ src/metabase/models/user.clj
@@ -5,3 +5,17 @@
 (defentity User
   (table :core_user)
   (has-many OrgPerm {:fk :user_id}))
+
+(defmethod default-fields User [_]
+  [:id
+   :date_joined
+   :first_name
+   :is_active
+   :is_staff
+   :is_superuser
+   :last_login
+   :last_name]) ; don't return :password!
+
+(defmethod post-select User [_ {:keys [id] :as result}]
+  (-> result
+      (assoc :org_perms (sel-fn :many OrgPerm :user_id id))))
