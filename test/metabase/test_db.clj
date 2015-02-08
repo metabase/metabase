fc504536c23f9d8d44b8500e8f1eb0e157d9b77d
Allen Gilliland <agilliland@gmail.com>
2015-02-08 14:25:30 -0800

    fixing unit testing.

--- test/metabase/test_db.clj
+++ test/metabase/test_db.clj
@@ -22,11 +22,11 @@
   (some-> (jdbc/query testdb-jdbc ["select count(*) as cnt from del_test"] :row-fn :cnt) first))
 
 ; insert a value and make sure its there
-(jdbc/insert! testdb-jdbc :del_test {:id 1})
+(expect [nil] (jdbc/insert! testdb-jdbc :del_test {:id 1}))
 (expect 1 (count-rows))
 
 ; call del function and validate result was true
-(expect true (del DelTest :id 1))
+(expect nil (del DelTest :id 1))
 
 ; make sure value was removed
 (expect 0 (count-rows))
