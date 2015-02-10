dd18315bba3cc3cd27c6a76bf69508fc91ccd1ed
Cam Saul <cam@geotip.com>
2015-02-09 16:55:44 -0800

    api/meta/db/:id endpoint

--- src/metabase/api/meta_db.clj
+++ src/metabase/api/meta_db.clj
@@ -3,11 +3,16 @@
   (:require [compojure.core :refer [GET]]
             [metabase.api.common :refer :all]
             [metabase.db :refer :all]
-            (metabase.models [database :refer [Database]]
+            (metabase.models [hydrate :refer [hydrate]]
+                             [database :refer [Database]]
                              [table :refer [Table]])))
 
 (defendpoint GET "/" [org]
   (sel :many Database :organization_id org))
+
+(defendpoint GET "/:id" [id]
+  (or-404-> (sel :one Database :id id)
+    (hydrate :organization)))
 
 (defendpoint GET "/:id/tables" [id]
   (sel :many Table :db_id id))
