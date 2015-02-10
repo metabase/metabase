b0f393363afbda71ac15bcb562efc693fbd28261
Cam Saul <cam@geotip.com>
2015-02-09 19:47:48 -0800

    api/dashboard/:id endpoint

--- src/metabase/api/dash.clj
+++ src/metabase/api/dash.clj
@@ -10,4 +10,9 @@
   (-> (sel :many Dashboard :organization_id org)
       (hydrate :creator :organization)))
 
+(defendpoint GET "/:id" [id]
+  (let-or-404 [db (-> (sel :one Dashboard :id id)
+                      (hydrate :creator :organization [:ordered_cards [:card :creator]]))]
+    {:dashboard db})) ; why is this returned with this {:dashboard} wrapper?
+
 (define-routes)
--- src/metabase/models/dashboard.clj
+++ src/metabase/models/dashboard.clj
@@ -1,15 +1,17 @@
 (ns metabase.models.dashboard
   (:require [korma.core :refer :all]
             [metabase.db :refer :all]
-            (metabase.models [org :refer [Org]]
+            (metabase.models [dashboard-card :refer [DashboardCard]]
+                             [org :refer [Org]]
                              [user :refer [User]])))
 
 (defentity Dashboard
   (table :report_dashboard))
 
-(defmethod post-select Dashboard [_ {:keys [creator_id organization_id] :as dash}]
+(defmethod post-select Dashboard [_ {:keys [id creator_id organization_id] :as dash}]
   (assoc dash
          :creator (sel-fn :one User :id creator_id)
-         :organization (sel-fn :one Org :id organization_id)))
+         :organization (sel-fn :one Org :id organization_id)
+         :ordered_cards (sel-fn :many DashboardCard :dashboard_id id)))
 
 ; TODO - ordered_cards
