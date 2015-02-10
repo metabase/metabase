7790246228ac85ece2b66b198b284a8190f9dff2
Cam Saul <cam@geotip.com>
2015-02-09 19:14:54 -0800

    api/card/:id/favorite endpoint

--- src/metabase/api/card.clj
+++ src/metabase/api/card.clj
@@ -2,8 +2,9 @@
   (:require [compojure.core :refer [GET]]
             [metabase.api.common :refer :all]
             [metabase.db :refer :all]
-            [metabase.models.card :refer :all]
-            [metabase.models.hydrate :refer [hydrate]]))
+            (metabase.models [hydrate :refer [hydrate]]
+                             [card :refer :all]
+                             [card-favorite :refer :all])))
 
 (defendpoint GET "/:id" [id]
   (or-404-> (sel :one Card :id id)))
@@ -11,5 +12,9 @@
 (defendpoint GET "/" [org f] ; TODO - need to do something with the `f` param
   (-> (sel :many Card :organization_id org)
       (hydrate :creator)))
+
+(defendpoint GET "/:id/favorite" [id]
+  {:favorite (boolean (some->> *current-user-id*
+                               (sel :one CardFavorite :card_id id :owner_id)))})
 
 (define-routes)
