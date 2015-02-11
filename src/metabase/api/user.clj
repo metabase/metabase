ac2ec832e4ee3c59a7804ef83220edeb8c624004
Allen Gilliland <agilliland@gmail.com>
2015-02-10 18:12:36 -0800

    convert routes to new 'defendpoint' macro and tidy up implementations.  also added implementation for password update endpoint.

--- src/metabase/api/user.clj
+++ src/metabase/api/user.clj
@@ -6,55 +6,49 @@
             [metabase.models.user :refer [User]]))
 
 
-(def user-list
-  (GET "/" []
-       ;; TODO - permissions check
-       ;; TODO - map to serializer.
-       {:status 200
-        :body (sel :many User :is_active true)}))
+(defendpoint GET "/" []
+  ;; TODO - permissions check
+  ;; TODO - limit the keys available in the response objects
+  (sel :many User :is_active true))
 
-(def user-get
-  (GET "/:user-id" [user-id]
-       ;; TODO - permissions check
-       ;; TODO - map to serializer.  strip fields (is_active, is_staff).  calculate fields (common_name)
-       (let-or-404 [user (sel :one User :id user-id)]
-                   {:status 200
-                    ;; TODO - we need a reusable way to do something like this for serializing output for responses
-                    :body (select-keys user [:id :email :first_name :last_name :last_login :is_superuser])})))
+
+(defendpoint GET "/:user-id" [user-id]
+  ;; TODO - permissions check
+  ;; TODO - map to serializer.  strip fields (is_active, is_staff).  calculate fields (common_name)
+  (let-or-404 [user (sel :one User :id user-id)]
+    ;; TODO - we need a reusable way to do something like this for serializing output for responses
+    (select-keys user [:id :email :first_name :last_name :last_login :is_superuser])))
+
 
 (defendpoint GET "/current" []
   (or-404-> (*current-user*)
     (hydrate [:org_perms :organization])))
 
-(def user-update
-  (PUT "/:user-id" [user-id :as {body :body}]
-       ;; TODO - permissions check
-       ;; TODO - validations (email address must be unique)
-       (let-or-404 [user (sel :one User :id user-id)]
-                   ;; TODO - how can we pass a useful error message in the 500 response on error?
-                   (do
-                     ;; TODO - is there a more idiomatic way to do this?  update row, then select it for output
-                     (upd User user-id
-                          ;; TODO - find a way to make this cleaner.  we don't want to modify the value if it doesn't exist
-                          :email (get body :email (:email user))
-                          :first_name (get body :first_name (:first_name user))
-                          :last_name (get body :last_name (:last_name user)))
-                     (let [updated-user (sel :one User :id user-id)]
-                       {:status 200
-                        :body (select-keys updated-user [:id :email :first_name :last_name :last_login :is_superuser])})))))
 
-(def user-update-password
-  (PUT "/:user-id/password" [user-id :as {body :body}]
-       ;; TODO - implementation
-       {:status 200
-        :body {}}))
+(defendpoint PUT "/:user-id" [user-id :as {body :body}]
+  ;; TODO - permissions check
+  ;; TODO - validations (email address must be unique)
+  (let-or-404 [user (sel :one User :id user-id)]
+    ;; TODO - how can we pass a useful error message in the 500 response on error?
+    (do
+     ;; TODO - is there a more idiomatic way to do this?  update row, then select it for output
+     (upd User user-id
+          ;; TODO - find a way to make this cleaner.  we don't want to modify the value if it doesn't exist
+          :email (get body :email (:email user))
+          :first_name (get body :first_name (:first_name user))
+          :last_name (get body :last_name (:last_name user)))
+     (let [updated-user (sel :one User :id user-id)]
+       (select-keys updated-user [:id :email :first_name :last_name :last_login :is_superuser])))))
 
 
-(define-routes
-  ;; TODO - this feels bleh.  is it better to put the actual route data here
-  ;;        and just have the endpoints be plain functions?
-  ;;        best way to automate building this list?
-  user-list
-  user-get
-  user-update
-  user-update-password)
+(defendpoint PUT "/:user-id/password" [user-id :as {body :body}]
+  ;; TODO - password validation
+  ;; TODO - match old password against current one
+  (let-or-404 [user (sel :one User :id user-id)]
+    (upd User user-id
+         :password (get body :password))
+    (let [updated-user (sel :one User :id user-id)]
+      (select-keys updated-user [:id :email :first_name :last_name :last_login :is_superuser]))))
+
+
+(define-routes)
