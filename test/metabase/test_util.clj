0516527d547cdbb50b72ee495ff9c07894b8e7e4
Cam Saul <cam@geotip.com>
2015-02-10 18:01:31 -0800

    `simple-batched-hydrate` and remove util function that duplicates `medley.core/mapply`

--- test/metabase/test_util.clj
+++ test/metabase/test_util.clj
@@ -6,11 +6,3 @@
 
 (expect {:a 100 :b 200}
   (select-non-nil-keys {:a 100 :b 200 :c nil :d 300} :a :b :c))
-
-;;; tests for apply-kwargs
-
-(expect {:a 1 :c 3 :d 4}
-  (apply-kwargs assoc {:a 1} {:c 3 :d 4})) ; should work like (assoc {:a 1} :c 3 :d 4)
-
-(expect {:a 1 :b 2 :c 3 :d 4}
-    (apply-kwargs assoc {:a 1} :b 2 {:c 3 :d 4})) ; should work like (assoc {:a 1} :b 2 :c 3 :d 4)
