6e63a6644c71568362467b15055c2075b2bb2e69
Cam Saul <cam@geotip.com>
2015-02-11 12:13:09 -0800

    remove test functions

--- src/metabase/api/card.clj
+++ src/metabase/api/card.clj
@@ -11,10 +11,6 @@
   (-> (sel :many Card :organization_id org (order :name :ASC))
       (hydrate :creator)))
 
-(defendpoint GET "/pass/" []
-  (check-403 true)
-  {:status "OK"})
-
 (defendpoint GET "/:id" [id]
   (->404 (sel :one Card :id id)
          (hydrate :can_read :can_write)))
@@ -23,10 +19,6 @@
   (let-404 [card (sel :one Card :id id)]
     (check-403 @(:can_write card))
     (del Card :id id)))
-
-(defendpoint GET "/fail" []
-  (check-403 false)
-  {:status "OK"})
 
 (defendpoint GET "/:id/favorite" [id]
   {:favorite (boolean (some->> *current-user-id*
