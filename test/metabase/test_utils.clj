3a5f6566062f8b5cbf8b112c172b5e7466d463db
Allen Gilliland <agilliland@gmail.com>
2015-02-13 10:38:40 -0800

    gotta check if file exists before trying to delete.

--- test/metabase/test_utils.clj
+++ test/metabase/test_utils.clj
@@ -10,8 +10,10 @@
   {:expectations-options :before-run}
   []
   ;; db-file is prefixed with "file:", so we strip that off
-  (io/delete-file (str (subs db-file 5) ".h2.db"))
-  (io/delete-file (str (subs db-file 5) ".trace.db"))
+  (let [h2-file (str (subs db-file 5) ".h2.db")
+        trace-file ((str (subs db-file 5) ".trace.db"))]
+    (when (.exists (io/file h2-file)) (io/delete-file h2-file))
+    (when (.exists (io/file trace-file)) (io/delete-file trace-file)))
   ; TODO - lets just completely delete the db before each test to ensure we start fresh
   (log/info "tearing down database and resetting to empty schema")
   (migrate :down)
