2b56b16b77ec3caabbbea7b77f02d35ff8df3251
Cam Saul <cam@geotip.com>
2015-02-17 13:50:53 -0800

    driver.structured -> driver.postgres

--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -1,9 +1,9 @@
 (ns metabase.driver
   (:require [clojure.core.match :refer [match]]
             (metabase.driver native
-                             structured)))
+                             postgres)))
 
 (defn process-and-run [{:keys [type] :as query}]
   (case (keyword type)
     :native (metabase.driver.native/process-and-run query)
-    :query (metabase.driver.structured/process-and-run query)))
+    :query (metabase.driver.postgres/process-and-run query)))
