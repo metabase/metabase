9b4d61363b34d9e975997c6c06a91c5b12588799
Cam Saul <cam@geotip.com>
2015-02-16 19:20:54 -0800

    create a CardFavorite + delete it

--- src/metabase/models/card_favorite.clj
+++ src/metabase/models/card_favorite.clj
@@ -1,5 +1,12 @@
 (ns metabase.models.card-favorite
-  (:use korma.core))
+  (:require [korma.core :refer :all]
+            [metabase.db :refer :all]
+            [metabase.util :refer [new-sql-date]]))
 
 (defentity CardFavorite
   (table :report_cardfavorite))
+
+(defmethod pre-insert CardFavorite [_ card-favorite]
+  (let [defaults {:created_at (new-sql-date)
+                  :updated_at (new-sql-date)}]
+    (merge defaults card-favorite)))
