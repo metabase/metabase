ccfa31aba83ca93edb0fe1f5f7222f5842025726
Cam Saul <cam@geotip.com>
2015-02-17 19:10:00 -0800

    perms check

--- src/metabase/api/datasource.clj
+++ src/metabase/api/datasource.clj
@@ -4,9 +4,12 @@
             [metabase.api.common :refer :all]
             [metabase.db :refer :all]
             (metabase.models [hydrate :as h]
-                             [datasource :refer [DataSource]])))
+                             [datasource :refer [DataSource]]
+                             [org :refer [Org]])))
 
 (defendpoint GET "/source" [org]
+  (let-404 [{:keys [can_read]} (sel :one Org :id org)]
+    (check-403 @can_read))
   (-> (sel :many DataSource :organization_id org)
       (h/hydrate :database)))
 
