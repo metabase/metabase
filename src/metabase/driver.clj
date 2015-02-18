2995a8b511af3066907f1059c9b36af293f6357e
Cam Saul <cam@geotip.com>
2015-02-18 13:54:03 -0800

    GET /api/meta/form_input endpoint

--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -3,6 +3,9 @@
             (metabase.driver native
                              postgres)))
 
+(def available-drivers
+  [["postgres" "PostgreSQL"]])
+
 (defn process-and-run [{:keys [type] :as query}]
   (case (keyword type)
     :native (metabase.driver.native/process-and-run query)
