2592cec9f0ec63d1017b6ec7f8357852e9f82a5a
Allen Gilliland <agilliland@gmail.com>
2015-02-18 11:38:56 -0800

    make a reusable 'build-response' function like we had in the django app.

--- src/metabase/api/qs.clj
+++ src/metabase/api/qs.clj
@@ -8,7 +8,7 @@
               [query-execution :refer [all-fields]])))
 
 
-(declare execute-query)
+(declare execute-query build-response)
 
 
 (defendpoint POST "/" [:as {body :body}]
@@ -28,16 +28,10 @@
 ;; TODO - not using defendpoint due to string params causing number format exceptions
 (def query-result
   (GET "/:uuid" [uuid]
-    (let-404 [{:keys [result_data] :as query-execution} (eval `(sel :one ~all-fields :uuid ~uuid))]
-      ;; NOTE - this builds the final response dictionary by piecing a few things together
-      (-> (select-keys query-execution [:id :uuid :status])                                          ;; start with just the things we are sure to need
-        (assoc :columns [] :data [])                                                                 ;; default columns/data
-        (cond->
-          (= "failed" (:status query-execution)) (assoc :error (:error query-execution)
-                                                        :sql_error (:error query-execution))         ;; TODO - sql error formatting FN
-          (= "completed" (:status query-execution)) (assoc :columns (get result_data :columns [])    ;; add real column & row data
-                                                           :data (get result_data :rows [])))
-        (->> (assoc {:status 200} :body))))))
+    (let-404 [query-execution (eval `(sel :one ~all-fields :uuid ~uuid))]
+      (->>
+        (build-response query-execution)
+        (assoc {:status 200} :body)))))
 
 
 (defendpoint GET "/:query-uuid/csv" [query-uuid]
@@ -55,4 +49,20 @@
 (defn execute-query
   "Run a single `native` type query against a database, optionally run asynchronously."
   [query]
-  query)
+  query
+  ;; TODO - run the query on the database
+  ;; TODO - build-response
+  )
+
+
+(defn build-response
+  "Build a query response from a QueryExecution record."
+  [{:keys [result_data] :as query-execution}]
+  (->
+    (select-keys query-execution [:id :uuid :status])                                            ;; start with just the things we are sure to need
+    (assoc :columns [] :data [])                                                                 ;; default columns/data
+    (cond->
+      (= "failed" (:status query-execution)) (assoc :error (:error query-execution)              ;; add errors if we failed
+                                                    :sql_error (:error query-execution))         ;; TODO - sql error formatting FN
+      (= "completed" (:status query-execution)) (assoc :columns (get result_data :columns [])    ;; add real column & row data
+                                                       :data (get result_data :rows [])))))
