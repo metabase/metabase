9d98ece126be550be5db666a8c50dc1240f72c8e
Cam Saul <cam@geotip.com>
2015-02-17 18:22:48 -0800

    fix refresh on admin pages

--- src/metabase/routes.clj
+++ src/metabase/routes.clj
@@ -13,8 +13,8 @@
 (let [index (serve-file "frontend_client/index.html")
       admin (serve-file "frontend_client/admin_index.html")]
   (defroutes routes
-    (GET "/" [] index)                                  ; ^/$         -> index.html
-    (GET "/:org-slug/admin/" [] admin)                  ; ^/org/admin -> admin_index.html
-    (context "/api" [] api/routes)                      ; ^/api/      -> API routes
-    (route/files "/app/" {:root "frontend_client/app"}) ; ^/app/      -> static files under frontend_client/app
+    (GET "/" [] index)                                  ; ^/$           -> index.html
+    (context "/api" [] api/routes)                      ; ^/api/        -> API routes
+    (route/files "/app/" {:root "frontend_client/app"}) ; ^/app/        -> static files under frontend_client/app
+    (GET "/:org-slug/admin/*" [] admin)                 ; ^/org/admin/* -> admin_index.html
     (GET "*" [] index)))                                ; Anything else (e.g. /user/edit_current) should serve up index.html; Angular app will handle the rest
