e9af36a1a32b8a0c9bcf2cb48526f2347733feab
Allen Gilliland <agilliland@gmail.com>
2015-02-20 12:26:21 -0800

    clean up a few things in session create/delete (login/logout).

--- src/metabase/api/session.clj
+++ src/metabase/api/session.clj
@@ -6,26 +6,23 @@
                              [session :refer [Session]])))
 
 
-(defendpoint POST "/" [:as {body :body}]
-  (if-not (and (contains? body :email) (contains? body :password))
-    {:status 400 :body "You must supply email & password credentials to login"}
-    (api-let [400 "Invalid username/password combination"] [user (sel :one User :email (:email body))]
-      ;; TODO - password validation via encryption
-      ; (check-400 (= (:password body) (:password user)))
-      (let [session-id (str (java.util.UUID/randomUUID))]
-        (ins Session
-          :id session-id
-          :user_id (:id user)
-          :created_at (new java.util.Date))
-        ;; TODO - figure out how to apply session to client.  cookies??
-        {:id session-id}))))
+(defendpoint POST "/" [:as {{:keys [email password] :as body} :body}]
+  (check (and email password) [400 "You must supply email & password credentials to login"])
+  (let-400 [user (sel :one [User :id :password] :email email)]
+    ;; TODO - password validation via encryption
+    (check (= password (:password user)) [400 "password mismatch"])
+    (let [session-id (str (java.util.UUID/randomUUID))]
+      (ins Session
+        :id session-id
+        :user_id (:id user))
+      ;; TODO - figure out how to apply session to client.  cookies??
+      {:id session-id})))
 
 
-(defendpoint DELETE "/" [:as {body :body}]
-  (if-not (contains? body :session_id)
-    {:status 400 :body "You must supply a session_id"}
-    (api-let [400 "Invalid session"] [session (sel :one Session :id (:session_id body))]
-      (del Session :id (:session_id body)))))
+(defendpoint DELETE "/" [:as {{:keys [session_id] :as body} :body}]
+  (check-400 session_id)
+  (let-400 [session (sel :one Session :id session_id)]
+    (del Session :id session_id)))
 ;; TODO - do we need to remove any cookies??
 
 
