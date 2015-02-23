3d52cc0ac77fb6030c833bc5ced4a3f0d4e4edb1
Sameer Al-Sakran <sameer@expa.com>
2015-02-23 15:20:58 -0800

    use require-params

--- src/metabase/api/permission_violation.clj
+++ src/metabase/api/permission_violation.clj
@@ -5,15 +5,12 @@
             [metabase.api.common :refer :all]
             [metabase.db :refer :all]
             [metabase.models.hydrate :refer :all]
-            (metabase.models [annotation :refer [PermissionViolation]]
+            (metabase.models [permission_violation :refer [PermissionViolation]]
               [database :refer [Database databases-for-org]]
               [org :refer [Org]]
               [common :as common])
             [metabase.util :as util]))
 
 (defendpoint POST "/" [:as {body :body}]
-  (check-400 (util/contains-many? body :user :url))
-  (check-500 (->> (-> body
-                    (select-keys [:user :url])
-                    (clojure.set/rename-keys {:user :user_id} ))
-               (mapply ins PermissionViolation))))
+  (require-params user url)
+  (check-500 (ins PermissionViolation :user_id user :url url)))
