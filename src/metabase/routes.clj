cd4ea249e49eebd0a7a3d8639016a20dc1e6497e
Cam Saul <cam@geotip.com>
2015-02-26 12:06:59 -0800

    return 404 for files starting with ^/app/ that don't exist (instead of returning index.html)

--- src/metabase/routes.clj
+++ src/metabase/routes.clj
@@ -13,8 +13,11 @@
 (let [index (serve-file "frontend_client/index.html")
       admin (serve-file "frontend_client/admin_index.html")]
   (defroutes routes
-    (GET "/" [] index)                                  ; ^/$           -> index.html
-    (context "/api" [] api/routes)                      ; ^/api/        -> API routes
-    (route/files "/app/" {:root "frontend_client/app"}) ; ^/app/        -> static files under frontend_client/app
-    (GET "/:org-slug/admin/*" [] admin)                 ; ^/org/admin/* -> admin_index.html
-    (GET "*" [] index)))                                ; Anything else (e.g. /user/edit_current) should serve up index.html; Angular app will handle the rest
+    (GET "/" [] index)                                ; ^/$           -> index.html
+    (context "/api" [] api/routes)                    ; ^/api/        -> API routes
+    (context "/app" []
+      (route/files "/" {:root "frontend_client/app"}) ; ^/app/        -> static files under frontend_client/app
+      (GET "/*" [] {:status 404                       ;                  return 404 for anything else starting with ^/app/ that doesn't exist
+                    :body "Not found."}))
+    (GET "/:org-slug/admin/*" [] admin)               ; ^/org/admin/* -> admin_index.html
+    (GET "*" [] index)))                              ; Anything else (e.g. /user/edit_current) should serve up index.html; Angular app will handle the rest
