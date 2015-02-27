42fe86e69a656d82e41525d467e5b569238da048
Cam Saul <cam@geotip.com>
2015-02-26 19:35:39 -0800

    revert accidental logging checkin

--- test/metabase/test_utils.clj
+++ test/metabase/test_utils.clj
@@ -11,7 +11,6 @@
   "setup database schema"
   {:expectations-options :before-run}
   []
-  (println "\n--------------------> WARNING: SETUP-DB IS BEING CALLED <--------------------\n")
   (let [filename (-> (re-find #"file:(\w+\.db).*" db-file) second)] ; db-file is prefixed with "file:", so we strip that off
     (map (fn [file-extension]                                        ; delete the database files, e.g. `metabase.db.h2.db`, `metabase.db.trace.db`, etc.
            (let [file (str filename file-extension)]
