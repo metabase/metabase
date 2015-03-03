e302e2fcdeaf227e30cca1650049c62fac731aa7
Allen Gilliland <agilliland@gmail.com>
2015-03-03 08:58:00 -0800

    put in an app global config entry for :max-session-age

--- src/metabase/config.clj
+++ src/metabase/config.clj
@@ -2,4 +2,5 @@
 
 (def app-defaults
   "Global application defaults"
-  {:database-file "metabase.db"})
+  {:database-file "metabase.db"                             ; name of the local h2 database file
+   :max-session-age (* 60 24 14)})                           ; session length in minutes
