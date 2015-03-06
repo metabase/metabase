5264809446c81c71fc0386068e533ad78987e054
Cam Saul <cam@geotip.com>
2015-03-05 17:14:30 -0800

    cleaner impl

--- src/metabase/async.clj
+++ src/metabase/async.clj
@@ -1,12 +1,12 @@
 (ns metabase.async)
 
+(def great-future (future nil))
+
 (defn cancel-all-futures []
-  (->> (all-ns)                     ; seq of all loaded namespaces
-       (mapcat ns-interns)          ; seq of *every* [symbol var]
-       vals                         ; seq of *every* var
-       (keep (fn [v]
-               (let [v (var-get v)] ; get value of var
-                 (when (future? v)  ; filter out ones that aren't futures
-                   v))))
-       (map future-cancel)          ; cancel all futures
-       dorun))                      ; force lazy eval without retaining head
+  (->> (all-ns)             ; seq of all loaded namespaces
+       (mapcat ns-interns)  ; seq of *every* [symbol var]
+       vals                 ; seq of *every* var
+       (map var-get)        ; get values of each var
+       (filter future?)
+       (map future-cancel)  ; cancel all futures
+       dorun))              ; force lazy eval without retaining head
