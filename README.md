d9f123c3724a5e06f4d96c21482e8d3044a45946
Cam Saul <cam@geotip.com>
2015-03-06 15:47:22 -0800

    update dependenices w/ lein ancient

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
+[![Circle CI](https://circleci.com/gh/metabase/metabase-init.svg?style=svg&circle-token=3ccf0aa841028af027f2ac9e8df17ce603e90ef9)](https://circleci.com/gh/metabase/metabase-init)
+
 ## Install Prerequisites
 
 1. Oracle JDK 8 (http://www.oracle.com/technetwork/java/javase/downloads/index.html)
 2. Node.js for npm (http://nodejs.org/)
 3. Leiningen (http://leiningen.org/)
 
-## Build Status
-
-[![Circle CI](https://circleci.com/gh/metabase/metabase-init.svg?style=svg&circle-token=3ccf0aa841028af027f2ac9e8df17ce603e90ef9)](https://circleci.com/gh/metabase/metabase-init)
 
 ## Build
 
@@ -59,6 +58,15 @@ Open a REPL in Emacs or with `lein repl` and enter the following:
 	(bootstrap)
 
 You'll be walked through the steps to get started.
+
+
+## Checking for Out-of-Date Dependencies
+
+    lein ancient
+
+Will give you a list of out-of-date plugins and dependencies. (Once's this repo is made public, this Clojars badge will work and show the status as well:)
+
+[![Dependencies Status](http://jarkeeper.com/metabase/metabase-init/status.png)](http://jarkeeper.com/metabase/metabase-init)
 
 
 ## License
--- project.clj
+++ project.clj
@@ -3,53 +3,51 @@
 (defproject metabase "metabase-0.1.0-SNAPSHOT"
   :description "Metabase Community Edition"
   :url "http://metabase.com/"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.4.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "LATEST"]          ; facilities for async programming + communication (using 'LATEST' because this is an alpha library)
                  [org.clojure/core.match "0.3.0-alpha4"]    ; optimized pattern matching library for Clojure
                  [org.clojure/math.numeric-tower "0.0.4"]   ; math functions like `ceil`
-                 [org.clojure/core.memoize "0.5.6"]         ; needed by core.match; has useful FIFO, LRU, etc. caching mechanisms
+                 [org.clojure/core.memoize "0.5.7"]         ; needed by core.match; has useful FIFO, LRU, etc. caching mechanisms
                  [org.clojure/data.csv "0.1.2"]             ; CSV parsing / generation
-                 [org.clojure/data.json "0.2.5"]            ; JSON parsing / generation
+                 [org.clojure/data.json "0.2.6"]            ; JSON parsing / generation
                  [org.clojure/java.jdbc "0.3.6"]            ; basic jdbc access from clojure
                  [org.clojure/tools.logging "0.3.1"]        ; logging framework
-                 [org.clojure/tools.macro "0.1.2"]          ; tools for writing macros
+                 [org.clojure/tools.macro "0.1.5"]          ; tools for writing macros
                  [org.clojure/tools.trace "0.7.8"]          ; "tracing macros/fns to help you see what your code is doing"
                  [clj-http "1.0.1"]                         ; HTTP Client
-                 [clj-time "0.5.1"]                         ; library for dealing with date/time
+                 [clj-time "0.9.0"]                         ; library for dealing with date/time
                  [com.cemerick/friend "0.2.1"]              ; auth library
-                 [com.h2database/h2 "1.3.170"]              ; embedded SQL database
+                 [com.h2database/h2 "1.4.186"]              ; embedded SQL database
                  [com.mattbertolini/liquibase-slf4j "1.2.1"]
                  [compojure "1.3.2"]                        ; HTTP Routing library built on Ring
-                 [environ "0.5.0"]                          ; easy environment management
+                 [environ "1.0.0"]                          ; easy environment management
                  [korma "0.4.0"]                            ; SQL lib
                  [log4j/log4j "1.2.17"
                   :exclusions [javax.mail/mail
                                javax.jms/jms
                                com.sun.jdmk/jmxtools
                                com.sun.jmx/jmxri]]
-                 [marginalia "0.7.1"]                       ; for documentation
+                 [marginalia "0.8.0"]                       ; for documentation
                  [medley "0.5.5"]                           ; lightweight lib of useful functions
                  [org.liquibase/liquibase-core "3.3.2"]     ; migration management (Java lib)
-                 [org.slf4j/slf4j-log4j12 "1.7.1"]
-                 [org.yaml/snakeyaml "1.14"]                ; YAML parser (required by liquibase)
-                 [postgresql "9.1-901.jdbc4"]               ; Postgres driver
+                 [org.slf4j/slf4j-log4j12 "1.7.10"]
+                 [org.yaml/snakeyaml "1.15"]                ; YAML parser (required by liquibase)
+                 [postgresql "9.3-1102.jdbc41"]             ; Postgres driver
                  [ring/ring-jetty-adapter "1.3.2"]          ; Ring adapter using Jetty webserver (used to run a Ring server for unit tests)
                  [ring/ring-json "0.3.1"]                   ; Ring middleware for reading/writing JSON automatically
-                 [swiss-arrows "1.0.0"]                     ; 'Magic wand' macro -<>, etc.
-                 ]
+                 [swiss-arrows "1.0.0"]]                    ; 'Magic wand' macro -<>, etc.
   :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]            ; Interactive development w/ cider NREPL in Emacs
+            [lein-ancient "0.6.4"]                          ; Check project for outdated dependencies + plugins w/ 'lein ancient'
             [lein-environ "0.5.0"]                          ; easy access to environment variables
             [lein-expectations "0.0.7"]                     ; run unit tests with 'lein expectations'
             [lein-marginalia "LATEST"]                      ; generate documentation with 'lein marg'
-            [lein-ring "0.8.10"]                            ; start the HTTP server with 'lein ring server'
-            ]
+            [lein-ring "0.8.10"]]                           ; start the HTTP server with 'lein ring server'
   :java-source-paths ["src/java"]
   :main ^:skip-aot metabase.core
   :target-path "target/%s"
   :ring {:handler metabase.core/app}
-  :profiles {:dev {:dependencies [[expectations "2.0.12"]   ; unit tests
-                                  [ring/ring-mock "0.2.0"]
-                                  ]
+  :profiles {:dev {:dependencies [[expectations "2.0.16"]   ; unit tests
+                                  [ring/ring-mock "0.2.0"]]
                    :jvm-opts ["-Dlogfile.path=target/log"]}
              :uberjar {:aot :all}})
