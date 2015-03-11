93da556a7c4839649f2d47851482bf4b5bc13f1f
Cam Saul <cam@geotip.com>
2015-03-10 22:05:59 -0700

    actually, no point in putting the 'settings' table in the DB

--- src/metabase/models/setting.clj
+++ src/metabase/models/setting.clj
@@ -2,55 +2,58 @@
   (:refer-clojure :exclude [get set])
   (:require [clojure.core.match :refer [match]]
             [korma.core :refer :all :exclude [delete]]
-            [metabase.db :refer [sel del post-select]]))
+            [metabase.db :refer [sel del]]))
 
 (declare Setting
-         SettingValue
          cached-setting-values
-         setup)
+         restore-cache-if-needed)
 
 ;; # PUBLIC
 
 ;; ## ACCESSORS
 
 (defn get
-  "Fetch setting with key K for Org.
-   Cached lookup time is ~70 s, compared to ~1100 s for DB lookup."
+  "Fetch value of `Setting` for `Org`.
+   Cached lookup time is ~60µs, compared to ~1800µs for DB lookup."
   [org-id k]
   {:pre [(integer? org-id)
          (keyword? k)]}
-  (setup)
+  (restore-cache-if-needed)
   (or (@cached-setting-values [k org-id])
-      (when-let [v (sel :one :field [SettingValue :value] :setting_key (name k) :organization_id org-id)]
+      (when-let [v (sel :one :field [Setting :value] :key (name k) :organization_id org-id)]
         (swap! cached-setting-values assoc [k org-id] v)
         v)))
 
-(defn set [org-id k v]
+(defn set
+  "Set the value of `Setting` for `Org`.
+
+    (set org-id :mandrill-api-key \"xyz123\")"
+  [org-id k v]
   {:pre [(integer? org-id)
          (keyword? k)
          (string? v)]}
-  (if (get org-id k) (update SettingValue
+  (if (get org-id k) (update Setting
                              (set-fields {:value v})
-                             (where {:setting_key (name k)
+                             (where {:key (name k)
                                      :organization_id org-id}))
-      (insert SettingValue
-              (values {:setting_key (name k)
+      (insert Setting
+              (values {:key (name k)
                        :organization_id org-id
                        :value v})))
-  (setup)
+  (restore-cache-if-needed)
   (swap! cached-setting-values assoc [k org-id] v)
   v)
 
-(defn delete [org-id k]
+(defn delete
+  "Delete a `Setting` value for `Org`."
+  [org-id k]
   {:pre [(integer? org-id)
          (keyword? k)]}
-  (setup)
+  (restore-cache-if-needed)
   (swap! cached-setting-values dissoc [k org-id])
-  (del SettingValue :setting_key (name k) :organization_id org-id))
+  (del Setting :key (name k) :organization_id org-id))
 
 ;; ## DEFSETTING
-
-(def defsetting-dirty? (atom false))
 
 (defmacro defsetting
   "Defines a new `Setting` that will be added to the DB at some point in the future.
@@ -69,15 +72,16 @@
           (get org-id# ~setting-key))
          ([org-id# value#]
           (set org-id# ~setting-key value#)))
-       (alter-meta! #'~nm assoc :is-setting? true)
-       (reset! metabase.models.setting/defsetting-dirty? true))))
+       (alter-meta! #'~nm assoc :is-setting? true))))
 
 
 ;; ## ALL SETTINGS (ETC)
 
-(defn all [org-id]
+(defn all
+  "Return all `Settings` for `Org`."
+  [org-id]
   {:pre [(integer? org-id)]}
-  (setup)
+  (restore-cache-if-needed)
   (->> @cached-setting-values
        (map (fn [[[k o-id] v]]
               (when (= org-id o-id)
@@ -85,9 +89,23 @@
        (filter identity)
        (reduce merge {})))
 
-(defn all-with-descriptions [org-id]
+(defn settings-list
+  "Return a list of all Settings (as created with `defsetting`)."
+  []
+  (->> (all-ns)
+       (mapcat ns-interns)
+       vals
+       (map meta)
+       (filter :is-setting?)
+       (map (fn [{k :name desc :doc}]
+              {:key (keyword k)
+               :description desc}))))
+
+(defn all-with-descriptions
+  "Return a combined list of all `Settings` and values for `Org`, if they exist."
+  [org-id]
   (let [settings-for-org (all org-id)]
-    (->> (sel :many Setting)
+    (->> (settings-list)
          (map (fn [{k :key :as setting}]
                 (assoc setting
                        :value (k settings-for-org)))))))
@@ -95,41 +113,10 @@
 
 ;; # IMPLEMENTATION
 
-(declare create-settings-if-needed
-         restore-cache-if-needed)
-
-(defn- setup []
-  (create-settings-if-needed)
-  (restore-cache-if-needed))
-
-;; ## DEFSETTING
-
-(defn- create-settings-if-needed []
-  (when @defsetting-dirty?
-    (println "RECREATING SETTINGS...")
-    (reset! defsetting-dirty? false)
-    (let [existing-settings (->> (sel :many :field [Setting :key])
-                                 (map name)
-                                 clojure.core/set)]
-      (->> (all-ns)
-           (mapcat ns-interns)
-           vals
-           (map meta)
-           (filter :is-setting?)
-           (map (fn [{k :name desc :doc}]
-                  {:key (name k)
-                   :description desc}))
-           (filter #((complement contains?) existing-settings (:key %)))
-           (map (fn [setting]
-                  (insert Setting (values setting))))))))
-
-
-;; ## ACCESSORS
-
 (defn- restore-cache-if-needed []
   (when-not @cached-setting-values
-    (reset! cached-setting-values (->> (sel :many SettingValue)
-                                       (map (fn [{k :setting_key org :organization_id v :value}]
+    (reset! cached-setting-values (->> (sel :many Setting)
+                                       (map (fn [{k :key org :organization_id v :value}]
                                               {[(keyword k) org] v}))
                                        (reduce merge {})))))
 
@@ -138,10 +125,3 @@
 
 (defentity ^:private Setting
   (table :setting))
-
-(defmethod post-select Setting [_ {k :key :as setting}]
-  (assoc setting
-         :key (keyword k)))
-
-(defentity ^:private SettingValue
-  (table :setting_value))
