691fe046db89d5fb5ad8fcdafcc79ecafcd6a66d
Allen Gilliland <agilliland@gmail.com>
2015-03-13 10:10:36 -0700

    add in new superadmin pages.

---
+++ resources/frontend_client/app/superadmin/index/index.services.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var SettingsAdminServices = angular.module('superadmin.index.services', ['ngResource']);
+
+SettingsAdminServices.factory('SettingsAdminServices', ['$resource', function($resource) {
+    return $resource('/api/setting', {}, {
+        list: {
+            url: '/api/setting',
+            method: 'GET',
+            isArray: true
+        },
+
+        // POST endpoint handles create + update in this case
+        put: {
+            url: '/api/setting/:key',
+            method: 'PUT'
+        },
+
+        delete: {
+            url: '/api/setting/:key',
+            method: 'DELETE'
+        }
+    });
+}]);
