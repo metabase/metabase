4c57a0b4ed7f4ca98ea49d4aff062e961eac67e4
Allen Gilliland <agilliland@gmail.com>
2015-03-13 12:02:21 -0700

    add /superadmin/organization/ section which provides a crude way to manage organizations for superusers.

---
+++ resources/frontend_client/app/superadmin/organization/organization.controllers.js
@@ -0,0 +1,83 @@
+'use strict';
+/*global _*/
+
+var OrganizationControllers = angular.module('superadmin.organization.controllers', ['corvus.services']);
+
+OrganizationControllers.controller('OrganizationListController', ['$scope', 'Organization',
+    function($scope, Organization) {
+        $scope.organizations = [];
+
+        $scope.saveSetting = function(setting) {
+            SettingsAdminServices.put({
+                key: setting.key
+            }, setting, function() {
+                setting.originalValue = setting.value;
+            }, function(error) {
+                console.log("Error saving setting: ", error);
+            });
+        };
+
+        $scope.deleteSetting = function(setting) {
+            SettingsAdminServices.delete({
+                key: setting.key
+            }, function() {
+                setting.value = null;
+                setting.originalValue = null;
+            }, function(error) {
+                console.log("Error deleting setting: ", error);
+            });
+        };
+
+        // initialize on load
+        Organization.list(function (orgs) {
+            $scope.organizations = orgs;
+        }, function (error) {
+            console.log("Error getting organizations: ", error);
+        });
+    }
+]);
+
+OrganizationControllers.controller('OrganizationDetailController', ['$scope', '$routeParams', '$location', 'Organization',
+    function($scope, $routeParams, $location, Organization) {
+        $scope.organization = undefined;
+
+        // initialize on load
+        if($routeParams.orgId) {
+            // editing an existing organization
+            Organization.get({
+                'orgId': $routeParams.orgId
+            },
+            function (org) {
+                $scope.organization = org;
+            }, function (error) {
+                console.log("Error getting organization: ", error);
+                // TODO - should be a 404 response
+            });
+
+            // provide a relevant save() function
+            $scope.save = function(organization) {
+                Organization.update(organization,
+                function (org) {
+                    $scope.organization = org;
+                }, function (error) {
+                    console.log(error);
+                });
+            };
+        } else {
+            // assume we are creating a new org
+            $scope.organization = {};
+
+            // provide a relevant save() function
+            $scope.save = function(organization) {
+                // TODO - some simple validation checks
+
+                Organization.create(organization,
+                function (org) {
+                    $location.path('/superadmin/organization/'+org.id);
+                }, function (error) {
+                    console.log(error);
+                });
+            };
+        }
+    }
+]);
