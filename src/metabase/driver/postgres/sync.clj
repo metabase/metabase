e035e4541a4797ac7dc9ada0bee3394b7875c1f5
Cam Saul <cam@geotip.com>
2015-03-16 17:56:35 -0700

    define additional Postgres types in driver

--- src/metabase/driver/postgres/sync.clj
+++ src/metabase/driver/postgres/sync.clj
@@ -6,23 +6,63 @@
 (def column->base-type
   "Map of Postgres column types -> Field base types.
    Add more mappings here as you come across them."
-  {:bool :BooleanField
-   :bytea :UnknownField
-   :date :DateField
-   :float8 :FloatField
-   :geometry :UnknownField
-   :inet :TextField             ; This was `GenericIPAddressField` in some places in the Django code but not others ...
-   :int2 :IntegerField
-   :int4 :IntegerField
-   :int8 :BigIntegerField
-   :json :TextField
-   :numeric :DecimalField
-   :serial :IntegerField
-   :text :TextField
-   :time :TimeField
-   :timestamp :DateTimeField
-   :timestamptz :DateTimeField
-   :varchar :TextField})
+  {:bigint        :BigIntegerField
+   :bigserial     :BigIntegerField
+   :bit           :UnknownField
+   :bool          :BooleanField
+   :boolean       :BooleanField
+   :box           :UnknownField
+   :bpchar        :CharField        ; "blank-padded char" is the internal name of "character"
+   :bytea         :UnknownField     ; byte array
+   :cidr          :TextField        ; IPv4/IPv6 network address
+   :circle        :UnknownField
+   :date          :DateField
+   :decimal       :DecimalField
+   :float4        :FloatField
+   :float8        :FloatField
+   :geometry      :UnknownField
+   :inet          :TextField        ; This was `GenericIPAddressField` in some places in the Django code but not others ...
+   :int           :IntegerField
+   :int2          :IntegerField
+   :int4          :IntegerField
+   :int8          :BigIntegerField
+   :interval      :UnknownField     ; time span
+   :json          :TextField
+   :jsonb         :TextField
+   :line          :UnknownField
+   :lseg          :UnknownField
+   :macaddr       :TextField
+   :money         :DecimalField
+   :numeric       :DecimalField
+   :path          :UnknownField
+   :pg_lsn        :IntegerField     ; PG Log Sequence #
+   :point         :UnknownField
+   :real          :FloatField
+   :serial        :IntegerField
+   :serial2       :IntegerField
+   :serial4       :IntegerField
+   :serial8       :BigIntegerField
+   :smallint      :IntegerField
+   :smallserial   :IntegerField
+   :text          :TextField
+   :time          :TimeField
+   :timetz        :TimeField
+   :timestamp     :DateTimeField
+   :timestamptz   :DateTimeField
+   :tsquery       :UnknownField
+   :tsvector      :UnknownField
+   :txid_snapshot :UnknownField
+   :uuid          :TextField
+   :varbit        :UnknownField
+   :varchar       :TextField
+   :xml           :TextField
+   (keyword "bit varying")                :UnknownField
+   (keyword "character varying")          :TextField
+   (keyword "double precision")           :FloatField
+   (keyword "time with time zone")        :TimeField
+   (keyword "time without time zone")     :TimeField
+   (keyword "timestamp with timezone")    :DateTimeField
+   (keyword "timestamp without timezone") :DateTimeField})
 
 (defmethod sync-tables :postgres [database]
   (binding [generic/*column->base-type* column->base-type]
