17722a3440490507f5ad005fb89addd9599521bc
Cam Saul <cam@geotip.com>
2015-03-16 18:08:47 -0700

    map addition H2 database data types

--- src/metabase/driver/h2/sync.clj
+++ src/metabase/driver/h2/sync.clj
@@ -5,12 +5,68 @@
 
 (def column->base-type
   "Map of H2 Column types -> Field base types. (Add more mappings here as needed)"
-  {:BIGINT :BigIntegerField
-   :DATE :DateField
-   :DOUBLE :FloatField
-   :INTEGER :IntegerField
-   :TIMESTAMP :DateTimeField
-   :VARCHAR :TextField})
+  {:ARRAY                 :UnknownField
+   :BIGINT                :BigIntegerField
+   :BINARY                :UnknownField
+   :BIT                   :BooleanField
+   :BLOB                  :UnknownField
+   :BOOL                  :BooleanField
+   :BOOLEAN               :BooleanField
+   :BYTEA                 :UnknownField
+   :CHAR                  :CharField
+   :CHARACTER             :CharField
+   :CLOB                  :TextField
+   :DATE                  :DateField
+   :DATETIME              :DateTimeField
+   :DEC                   :DecimalField
+   :DECIMAL               :DecimalField
+   :DOUBLE                :FloatField
+   :FLOAT                 :FloatField
+   :FLOAT4                :FloatField
+   :FLOAT8                :FloatField
+   :GEOMETRY              :UnknownField
+   :IDENTITY              :IntegerField
+   :IMAGE                 :UnknownField
+   :INT                   :IntegerField
+   :INT2                  :IntegerField
+   :INT4                  :IntegerField
+   :INT8                  :BigIntegerField
+   :INTEGER               :IntegerField
+   :LONGBLOB              :UnknownField
+   :LONGTEXT              :TextField
+   :LONGVARBINARY         :UnknownField
+   :LONGVARCHAR           :TextField
+   :MEDIUMBLOB            :UnknownField
+   :MEDIUMINT             :IntegerField
+   :MEDIUMTEXT            :TextField
+   :NCHAR                 :CharField
+   :NCLOB                 :TextField
+   :NTEXT                 :TextField
+   :NUMBER                :DecimalField
+   :NUMERIC               :DecimalField
+   :NVARCHAR              :TextField
+   :NVARCHAR2             :TextField
+   :OID                   :UnknownField
+   :OTHER                 :UnknownField
+   :RAW                   :UnknownField
+   :REAL                  :FloatField
+   :SIGNED                :IntegerField
+   :SMALLDATETIME         :DateTimeField
+   :SMALLINT              :IntegerField
+   :TEXT                  :TextField
+   :TIME                  :TimeField
+   :TIMESTAMP             :DateTimeField
+   :TINYBLOB              :UnknownField
+   :TINYINT               :IntegerField
+   :TINYTEXT              :TextField
+   :UUID                  :TextField
+   :VARBINARY             :UnknownField
+   :VARCHAR               :TextField
+   :VARCHAR2              :TextField
+   :VARCHAR_CASESENSITIVE :TextField
+   :VARCHAR_IGNORECASE    :TextField
+   :YEAR                  :IntegerField
+   (keyword "DOUBLE PRECISION") :FloatField})
 
 (defmethod sync-tables :h2 [database]
   (binding [generic/*column->base-type* column->base-type]
