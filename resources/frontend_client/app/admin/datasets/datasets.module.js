3836ceab4272165202ceb1814b80c8d606005b4b
Allen Gilliland <agilliland@gmail.com>
2015-03-20 14:43:18 -0700

    remove routes, templates, controllers for listing dependents of a table.  that stuff is to remain back in the django app.

--- resources/frontend_client/app/admin/datasets/datasets.module.js
+++ resources/frontend_client/app/admin/datasets/datasets.module.js
@@ -15,10 +15,6 @@ AdminDatasets.config(['$routeProvider', function ($routeProvider) {
         templateUrl: '/app/admin/datasets/partials/dataset_edit.html',
         controller: 'AdminDatasetEdit'
     });
-    $routeProvider.when('/:orgSlug/admin/datasets/:tableId/dependents', {
-        templateUrl: '/app/admin/datasets/partials/table_dependents.html',
-        controller: 'AdminTableDependents'
-    });
     $routeProvider.when('/:orgSlug/admin/datasets/field/:fieldId', {
         templateUrl: '/app/admin/datasets/partials/field_detail.html',
         controller: 'AdminFieldDetail'
