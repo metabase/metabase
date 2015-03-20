523f9940233aba5b8651f63258b7519f85b481ea
Cam Saul <cam@getluckybird.com>
2015-03-19 18:44:40 -0700

    fix annotation creation. FANCY COLOR LOGGING FOR API CALLS

--- src/metabase/middleware/log_api_call.clj
+++ src/metabase/middleware/log_api_call.clj
@@ -1,10 +1,17 @@
 (ns metabase.middleware.log-api-call
   "Middleware to log API calls. Primarily for debugging purposes."
-  (:require [clojure.pprint :refer [pprint]]
-            [clojure.tools.logging :as log]))
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.pprint :refer [pprint]]
+            [clojure.tools.logging :as log]
+            [colorize.core :as color]))
 
 (declare api-call?
-         log-request)
+         log-request
+         log-response)
+
+(def ^:private only-display-output-on-error
+  "Set this to `false` to see out API responses."
+  true)
 
 (defn log-api-call
   "Middleware to log `:request` and/or `:response` by passing corresponding OPTIONS."
@@ -18,14 +25,13 @@
                 (when log-request?
                   (log-request request))
                 (let [start-time (System/nanoTime)
-                      response (handler request)]
-                  (when log-request?
-                    (let [elapsed-time (-> (- (System/nanoTime) start-time)
-                                           double
-                                           (/ 1000000.0))]
-                      (log/debug (str "Elapsed time: " elapsed-time " msecs"))))
+                      response (handler request)
+                      elapsed-time (-> (- (System/nanoTime) start-time)
+                                       double
+                                       (/ 1000000.0)
+                                       math/round)]
                   (when log-response?
-                    (log/debug (with-out-str (pprint response))))
+                    (log-response request response elapsed-time))
                   response))))))
 
 (defn- api-call?
@@ -34,6 +40,15 @@
   (and (>= (count uri) 4)
        (= (.substring uri 0 4) "/api")))
 
-(defn- log-request [{:keys [uri request-method params]}]
-  (log/debug (.toUpperCase (name request-method)) uri) (when-not (empty? params)
-                                                         (with-out-str (pprint params))))
+(defn- log-request [{:keys [uri request-method body]}]
+  (log/debug (color/blue (format "%s %s " (.toUpperCase (name request-method)) uri)
+                         (when-let [body-output (with-out-str (pprint body))]
+                           (str "\n" body-output)))))
+
+(defn- log-response [{:keys [uri request-method]} {:keys [status body]} elapsed-time]
+  (let [error? (>= status 400)
+        color-fn (if error? color/red color/green)]
+    (log/debug (color-fn (format "%s %s %d (%d ms)" (.toUpperCase (name request-method)) uri status elapsed-time)
+                         (when (and error? only-display-output-on-error)
+                           (when-let [body-output (with-out-str (pprint body))]
+                             (str "\n" body-output)))))))
