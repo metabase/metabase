76e31b12f91291f5c3bb7f7951157963644a7802
Allen Gilliland <agilliland@gmail.com>
2015-03-27 16:41:59 -0700

    add email message function for `send-new-user-email` which formats and sends the user welcome email.

--- src/metabase/email/messages.clj
+++ src/metabase/email/messages.clj
@@ -2,10 +2,31 @@
   "Convenience functions for sending templated email messages.  Each function here should represent a single email.
    NOTE: we want to keep this about email formatting, so don't put heavy logic here RE: building data for emails."
   (:require [hiccup.core :refer [html]]
-            [metabase.email :as email]))
+            [metabase.email :as email]
+            [metabase.util :as u]))
 
 
 ;;; ### Public Interface
+
+(defn send-new-user-email
+  "Format and Send an welcome email for newly created users."
+  [first_name email password-reset-url]
+  {:pre [(string? first_name)
+         (string? email)
+         (u/is-email? email)
+         (string? password-reset-url)]}
+  (let [message-body (html [:html
+                            [:body
+                             [:p (format "Welcome to Metabase %s!" first_name)]
+                             [:p "Your account is setup and ready to go, you just need to set a password so you can login.  Follow the link below to reset your account password."]
+                             [:p [:a {:href password-reset-url} password-reset-url]]]])]
+    (email/send-message
+      "Your new Metabase account is all set up"
+      {email email}
+      :html message-body)
+    ;; return the message body we sent
+    message-body))
+
 
 (defn send-email-report
   "Format and Send an `EmailReport` email."
