bcecc828693148eeebd387cca9f89f367b4df9d9
Allen Gilliland <agilliland@gmail.com>
2015-03-27 16:21:42 -0700

    implement GET /api/meta/dataset/csv/

--- src/metabase/api/meta/dataset.clj
+++ src/metabase/api/meta/dataset.clj
@@ -1,12 +1,30 @@
 (ns metabase.api.meta.dataset
   "/api/meta/dataset endpoints."
-  (:require [compojure.core :refer [POST]]
+  (:require [clojure.data.csv :as csv]
+            [compojure.core :refer [GET POST]]
             [metabase.api.common :refer :all]
-            [metabase.driver :as driver]))
+            [metabase.driver :as driver]
+            [metabase.util :as u]))
 
 (defendpoint POST "/" [:as {:keys [body]}]
   (let [{:keys [status] :as response} (driver/dataset-query body {:executed_by *current-user-id*})]
     {:status (if (= status :completed) 200 400)
      :body response}))
+
+(defendpoint GET "/csv/"
+  "Execute an MQL query and download the results as a CSV file."
+  [query]
+  {query [Required String->Dict]}
+  (let [{{:keys [columns rows]} :data :keys [status] :as response} (driver/dataset-query query {:executed_by *current-user-id*})]
+    (if (= status :completed)
+      ;; successful query, send CSV file
+      {:status 200
+       :body (with-out-str
+               (csv/write-csv *out* (into [columns] rows)))
+       :headers {"Content-Type" "text/csv"
+                 "Content-Disposition" (str "attachment; filename=\"query_result_" (u/now-iso8601) ".csv\"")}}
+      ;; failed query, send error message
+      {:status 500
+       :body response})))
 
 (define-routes)
