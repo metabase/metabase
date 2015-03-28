8f2d9351cc9962747c00e32aa1dfc8fb31561b25
Allen Gilliland <agilliland@gmail.com>
2015-03-27 20:35:03 -0700

    avoid really long strings.

--- test/metabase/email/messages_test.clj
+++ test/metabase/email/messages_test.clj
@@ -4,5 +4,8 @@
 
 
 (expect
-  "<html><body><p>Welcome to Metabase test!</p><p>Your account is setup and ready to go, you just need to set a password so you can login.  Follow the link below to reset your account password.</p><p><a href=\"http://localhost/some/url\">http://localhost/some/url</a></p></body></html>"
+  (str "<html><body><p>Welcome to Metabase test!</p>"
+    "<p>Your account is setup and ready to go, you just need to set a password so you can login.  "
+    "Follow the link below to reset your account password.</p>"
+    "<p><a href=\"http://localhost/some/url\">http://localhost/some/url</a></p></body></html>")
   (send-new-user-email "test" "test@test.com" "http://localhost/some/url"))
