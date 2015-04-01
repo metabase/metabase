8369cbff746b7b2ecb251906f4bb7c28a7653bba
Cam Saul <cam@geotip.com>
2015-03-31 19:23:44 -0700

    test fix

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -1,5 +1,5 @@
 (ns metabase.models.card
-  (:require [clojure.data.json :as json]
+  (:require [cheshire.core :as cheshire]
             [korma.core :refer :all]
             [metabase.api.common :refer [*current-user-id* org-perms-case]]
             [metabase.db :refer :all]
@@ -31,20 +31,21 @@
   (let [defaults {:created_at (util/new-sql-timestamp)
                   :updated_at (util/new-sql-timestamp)}]
     (-> (merge defaults card)
-        (assoc :dataset_query (json/write-str dataset_query)
-               :visualization_settings (json/write-str visualization_settings)))))
+        (assoc :dataset_query          (cheshire/generate-string dataset_query)
+               :display                (name display)
+               :visualization_settings (cheshire/generate-string visualization_settings)))))
 
 (defmethod pre-update Card [_ {:keys [dataset_query visualization_settings display] :as card}]
-  (when display
-    (assert (contains? display-types (keyword display))))
-  (assoc card
-         :updated_at (util/new-sql-timestamp)
-         :dataset_query (json/write-str dataset_query)
-         :visualization_settings (json/write-str visualization_settings)))
+  (cond-> (assoc card
+                 :updated_at             (util/new-sql-timestamp)
+                 :dataset_query          (cheshire/generate-string dataset_query)
+                 :visualization_settings (cheshire/generate-string visualization_settings))
+    display (do (assert (contains? display-types (keyword display)))
+                (assoc :display (name display)))))
 
 (defmethod post-select Card [_ {:keys [organization_id creator_id] :as card}]
   (-> card
       (realize-json :dataset_query :visualization_settings)
-      (assoc :creator (delay (sel :one User :id creator_id))
+      (assoc :creator      (delay (sel :one User :id creator_id))
              :organization (delay (sel :one Org :id organization_id)))
       assoc-permissions-sets))
