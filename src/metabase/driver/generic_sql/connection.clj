647586e5d04755889c0895aa218511d27598980c
Cam Saul <cam@getluckybird.com>
2015-04-09 13:15:19 -0700

    test fix, for real

--- src/metabase/driver/generic_sql/connection.clj
+++ src/metabase/driver/generic_sql/connection.clj
@@ -8,8 +8,10 @@
 
     (can-connect? (sel :one Database ....)) -> true"
   [database]
-  (try (= 1 (let [[result] (k/exec-raw (driver/connection database) "SELECT 1 AS one" :results)]
-              (:one result)))
+  (try (= 1 (-> (k/exec-raw (driver/connection database) "SELECT 1" :results)
+                first
+                vals
+                first))
        (catch Throwable e
          (log/error "Failed to connect to database:" (.getMessage e))
          false)))
