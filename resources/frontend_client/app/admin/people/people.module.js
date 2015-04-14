6ce88a517eee6ef6870632981eefd23a045e5f4b
Allen Gilliland <agilliland@gmail.com>
2015-04-14 13:40:02 -0700

    updated People admin section.

--- resources/frontend_client/app/admin/people/people.module.js
+++ resources/frontend_client/app/admin/people/people.module.js
@@ -6,19 +6,8 @@ var Organization = angular.module('corvusadmin.people', [
 ]);
 
 Organization.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/:orgSlug/admin/'});
     $routeProvider.when('/:orgSlug/admin/people/', {
         templateUrl: '/app/admin/people/partials/people.html',
         controller: 'PeopleList'
     });
-    $routeProvider.when('/:orgSlug/admin/people/:userId/modify', {
-        templateUrl: '/app/admin/people/partials/user_edit.html',
-        controller: 'PeopleEdit'
-    });
-
-    $routeProvider.when('/:orgSlug/admin/people/:userId/change_password', {
-        templateUrl: '/app/admin/people/partials/user_password_edit.html',
-        controller: 'PeopleChangePassword'
-    });
-
 }]);
