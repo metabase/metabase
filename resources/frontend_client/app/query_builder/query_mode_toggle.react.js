04a049a328be7d074c763f9d6325045cb0729519
Kyle Doherty <kyle.l.doherty@gmail.com>
2015-04-28 12:32:03 -0700

    add sql icon

--- resources/frontend_client/app/query_builder/query_mode_toggle.react.js
+++ resources/frontend_client/app/query_builder/query_mode_toggle.react.js
@@ -9,6 +9,7 @@ var QueryModeToggle = React.createClass({
     },
     render: function () {
         // only render if the card is NEW && unmodified
+        // TODO: @allen i'm not sure this is working
         if (this.props.card.id !== undefined ||
             this.props.card.isDirty() ||
             !this.props.card.dataset_query) {
@@ -16,7 +17,8 @@ var QueryModeToggle = React.createClass({
         }
 
         var newType;
-        // shorthand for the current type
+
+        // set up a horthand for the current type
         var type = this.props.card.dataset_query.type;
 
         // determine the type to switch to based on the type
@@ -25,13 +27,17 @@ var QueryModeToggle = React.createClass({
         // set css classes based on the current type
         var buttonClasses = cx({
             'Button-toggle': true,
-            'Mode--native': type === 'native',
-            'Mode--query': type === 'query'
+            'Button--toggled': type === 'native',
         });
 
         return (
             <div className={buttonClasses} onClick={this.props.setQueryModeFn.bind(this, newType)}>
-                <span className="Button-toggleIndicator"></span>
+                <span className="Button-toggleIndicator">
+                    <svg className="Sql-icon" width="14px" height="14px" viewBox="0 0 16 16" fill="currentcolor">
+                        <path d="M-0.237037037,9.10836763 L-0.237037037,11.8518519 L6.16296296,5.92592593 L-0.237037037,8.22888605e-15 L-0.237037037,2.74348422 L3.2,5.92592593 L-0.237037037,9.10836763 Z M4.14814815,13.44 L16,13.44 L16,16 L4.14814815,16 L4.14814815,13.44 Z">
+                        </path>
+                    </svg>
+                </span>
             </div>
         );
     }
