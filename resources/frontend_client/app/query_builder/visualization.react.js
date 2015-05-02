7b184f21d2446fa2dfa5a417e0cfc90fef21f97b
Allen Gilliland <agilliland@gmail.com>
2015-05-02 14:06:36 -0700

    make our Visualization more "pure" by implementing shouldComponentUpdate() and avoiding cases where the chart was re-rendering all the time even though the actual visualized data hadn't changed.

--- resources/frontend_client/app/query_builder/visualization.react.js
+++ resources/frontend_client/app/query_builder/visualization.react.js
@@ -17,11 +17,26 @@ var QueryVisualization = React.createClass({
         };
     },
 
+    shouldComponentUpdate: function(nextProps, nextState) {
+        // visualizations only change when the result itself changes OR the card display is different
+        // NOTE: we are purposely doing an identity comparison here with props.result and NOT a value comparison
+        if (this.props.result == nextProps.result &&
+                this.display === nextProps.card.display) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+
     componentDidMount: function () {
+        // TODO: this is a workaround because our objects are mutable
+        this.display = this.props.card.display;
         this.renderChartIfNeeded();
     },
 
     componentDidUpdate: function () {
+        // TODO: this is a workaround because our objects are mutable
+        this.display = this.props.card.display;
         this.renderChartIfNeeded();
     },
 
