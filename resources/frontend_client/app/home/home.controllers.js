230e9343069effff915a7dc453e6209958353c3a
Kyle Doherty <kyle.l.doherty@gmail.com>
2015-06-06 12:58:15 -0700

    home

--- resources/frontend_client/app/home/home.controllers.js
+++ resources/frontend_client/app/home/home.controllers.js
@@ -5,5 +5,12 @@ HomeControllers.controller('Home', ['$scope', '$location',  function($scope, $lo
 }]);
 
 HomeControllers.controller('HomeGreeting', ['$scope', '$location',  function($scope, $location) {
-    console.log("FUCK YEAH WE'RE GREETING")
+    var greetingPrefixes = [
+        'Hey there',
+        'How\'s it going',
+    ];
+    function buildGreeting () {
+    }
+    $scope.greeting = 'Hey there Kyle';
+    console.log($scope);
 }]);
--- resources/frontend_client/app/home/home.module.js
+++ resources/frontend_client/app/home/home.module.js
@@ -1,7 +1,15 @@
 var Home = angular.module('corvus.home', [
-    'ngRoute',
-    'ngCookies',
-    'corvus.filters',
-    'corvus.directives',
-    'corvus.services',
+    'corvus.home.controllers',
 ]);
+
+Home.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/app/home/home.html',
+        controller: 'Home',
+        resolve: {
+            appState: function(AppState) {
+                return AppState.init();
+            }
+        }
+    });
+}]);
