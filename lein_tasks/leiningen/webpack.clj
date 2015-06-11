b9eb3ff76776278b67c24884b05e30a4303d5518
Cam Saul <cam@geotip.com>
2015-06-10 19:08:58 -0700

    Disable webpack minification on CircleCI :unamused:

--- lein_tasks/leiningen/webpack.clj
+++ lein_tasks/leiningen/webpack.clj
@@ -1,11 +1,12 @@
 (ns leiningen.webpack
-  (:use clojure.java.shell))
+  (:require [clojure.java.shell :refer :all]))
 
-
+;; Set the CI_DISABLE_WEBPACK_MINIFICATION environment variable to skip minification which takes ~6+ minutes on CircleCI
 (defn webpack [projects & args]
   ;; TODO - some better validations such as checking that we have webpack available
   (println "Running `webpack -p` to assemble and minify frontend assets")
-  (let [result (sh (str (:root projects) "/node_modules/webpack/bin/webpack.js") "-p")]
+  (let [result (sh (str (:root projects) "/node_modules/webpack/bin/webpack.js") (when-not (System/getenv "CI_DISABLE_WEBPACK_MINIFICATION")
+                                                                                   "-p"))]
     (if (= 0 (:exit result))
       (println (:out result))
       (println (:err result)))))
