6f3ea17f9b52a9f928ea8aeec524d623b75daea8
Cam Saul <cam@geotip.com>
2015-06-10 19:21:44 -0700

    test fix

--- lein_tasks/leiningen/webpack.clj
+++ lein_tasks/leiningen/webpack.clj
@@ -5,8 +5,8 @@
 (defn webpack [projects & args]
   ;; TODO - some better validations such as checking that we have webpack available
   (println "Running `webpack -p` to assemble and minify frontend assets")
-  (let [result (sh (str (:root projects) "/node_modules/webpack/bin/webpack.js") (when-not (System/getenv "CI_DISABLE_WEBPACK_MINIFICATION")
-                                                                                   "-p"))]
+  (let [result (sh (str (:root projects) "/node_modules/webpack/bin/webpack.js") (if (System/getenv "CI_DISABLE_WEBPACK_MINIFICATION") ""
+                                                                                     "-p"))]
     (if (= 0 (:exit result))
       (println (:out result))
       (println (:err result)))))
