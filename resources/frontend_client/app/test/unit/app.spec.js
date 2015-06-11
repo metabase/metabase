d60ed991ddc588767ed77e66c20bb2d3aa4b5153
Tom Robinson <tlrobinson@gmail.com>
2015-06-11 00:31:23 -0700

    Karma CircleCI test, should pass

--- resources/frontend_client/app/test/unit/app.spec.js
+++ resources/frontend_client/app/test/unit/app.spec.js
@@ -6,7 +6,7 @@ import 'angular-mocks';
 describe('Hello Service', function () {
   describe('.hello', function () {
     it('1 should equal 1', function () {
-      expect(1).toEqual(2);
+      expect(1).toEqual(1);
     });
   });
 });
