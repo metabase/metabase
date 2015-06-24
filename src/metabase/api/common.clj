8a4eab9a89689a3c4d5c744cada4722759621e3f
Cam Saul <cam@geotip.com>
2015-06-24 13:48:17 -0700

    lint fix 2000

--- src/metabase/api/common.clj
+++ src/metabase/api/common.clj
@@ -451,7 +451,7 @@
      ;; a separate multimethod or protocol so other models besides DB can write optimized
      ;; implementations. Currently, we always fetch an *entire* object to do read checking,
      ;; which is wasteful.
-     (= (name entity) "Database") `nil
+     (= (name entity) "Database") `(comment "@(:can-read database) is always true.") ; put some non-constant value here so Eastwood doesn't complain about unused return values
      :else                        `(read-check (sel :one ~entity :id ~id)))))
 
 (defmacro write-check
