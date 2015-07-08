ccac24fab3571e67b40ea016a7687af9ddbd4c2c
Tom Robinson <tlrobinson@gmail.com>
2015-07-07 18:07:54 -0700

    Use the correct form of nested hydrate

--- src/metabase/api/meta/table.clj
+++ src/metabase/api/meta/table.clj
@@ -62,7 +62,7 @@
   {include_sensitive_fields String->Boolean}
   (->404 (sel :one Table :id id)
          read-check
-         (hydrate :db [:fields [:target] [:human_readable_name]] :field_values :human_readable_name)
+         (hydrate :db [:fields :target :human_readable_name] :field_values :human_readable_name)
          (update-in [:fields] (if include_sensitive_fields
                                 ;; If someone passes include_sensitive_fields return hydrated :fields as-is
                                 identity
