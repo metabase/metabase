645735d82698bdf3d963a058523ac7651ba6a9cf
Cam Saül <cammsaul@gmail.com>
2015-07-14 03:17:48 -0700

    core.logic annotate working nicely

--- .dir-locals.el
+++ .dir-locals.el
@@ -36,6 +36,7 @@
                               (macrolet 1)
                               (org-perms-case 1)
                               (pdoseq 1)
+                              (project 1)
                               (qp-expect-with-datasets 1)
                               (query-with-temp-db 1)
                               (resolve-private-fns 1)
