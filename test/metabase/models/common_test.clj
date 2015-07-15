545ec1a3d57f4ef838feccd24b04ce1c0f74f19e
Allen Gilliland <agilliland@gmail.com>
2015-07-15 14:24:53 -0700

    fixes #735 null pointer issue.

---
+++ test/metabase/models/common_test.clj
@@ -0,0 +1,17 @@
+(ns metabase.models.common-test
+  (:require [expectations :refer :all]
+            [metabase.db :refer :all]
+            [metabase.models.common :refer [name->human-readable-name]]
+            [metabase.test.data :refer :all]))
+
+
+;; testing on `(name->human-readable-name)`
+(expect nil (name->human-readable-name nil))
+(expect nil (name->human-readable-name ""))
+(expect "" (name->human-readable-name "_"))
+(expect "" (name->human-readable-name "-"))
+(expect "Agent Invite Migration" (name->human-readable-name "_agent_invite_migration"))
+(expect "Agent Invite Migration" (name->human-readable-name "-agent-invite-migration"))
+(expect "Foobar" (name->human-readable-name "fooBar"))
+(expect "Foo Bar" (name->human-readable-name "foo-bar"))
+(expect "Foo Bar" (name->human-readable-name "foo_bar"))
