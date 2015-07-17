b9b11e043dd9e654b884612675cc7ee682b28756
Cam Saül <cammsaul@gmail.com>
2015-07-16 19:57:13 -0700

    you just needed to change the regex :unamused:

--- src/metabase/models/common.clj
+++ src/metabase/models/common.clj
@@ -24,18 +24,16 @@
    {:id perms-read :name "Read Only"},
    {:id perms-readwrite :name "Read & Write"}])
 
-
 (defn name->human-readable-name
   "Convert a string NAME of some object like a `Table` or `Field` to one more friendly to humans.
 
     (name->human-readable-name \"admin_users\") -> \"Admin Users\""
   [^String n]
   (when (seq n)
-    (when-let [n (->> (some-> n
-                        (s/replace #"(?:-|_id)$" "")                                       ; strip off any trailing _id or -id suffix
-                        (s/split #"_|-"))
-                   (filterv not-empty)) ]                                                 ; explode string on underscores and hyphens
-      (->> (for [[first-letter & rest-letters] n]                                     ; for each part of the string,
+    (when-let [n (some-> n
+                         (s/replace #"^(.+)(?:[-_]id)$" "$1")                         ; strip off any trailing _id or -id suffix
+                         (s/split #"_|-"))]                                           ; explode string on underscores and hyphens
+      (->> (for [[first-letter & rest-letters] (filter (complement s/blank?) n)]      ; for each part of the string,
              (apply str (s/upper-case first-letter) (map s/lower-case rest-letters))) ; upcase the first char and downcase the rest
-        (interpose " ")                                                               ; add a space between each part
-        (apply str)))))                                                               ; convert back to a single string
+           (interpose " ")                                                            ; add a space between each part
+           (apply str)))))                                                            ; convert back to a single string
