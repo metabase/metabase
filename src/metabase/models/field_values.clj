7ffca1a145499bbc0bd27d31c859d427fc367599
Cam Saul <cam@geotip.com>
2015-07-21 12:07:53 -0700

    test fix

--- src/metabase/models/field_values.clj
+++ src/metabase/models/field_values.clj
@@ -39,13 +39,13 @@
 (def ^:private field-distinct-values
   (u/runtime-resolved-fn 'metabase.db.metadata-queries 'field-distinct-values))
 
-(defn create-field-values
+(defn- create-field-values
   "Create `FieldValues` for a `Field`."
   {:arglists '([field] [field human-readable-values])}
   [{field-id :id, field-name :name, :as field} & [human-readable-values]]
   {:pre [(integer? field-id)
          (:table field)]} ; need to pass a full `Field` object with delays beause the `metadata/` functions need those
-  (log/debug (format "Creating FieldValues for Field %s..." (or field-name field-id))) ; use field name if available
+  (log/info (u/format-color 'red "Creating FieldValues for Field %s..." (or field-name field-id))) ; use field name if available
   (ins FieldValues
     :field_id              field-id
     :values                (field-distinct-values field)
