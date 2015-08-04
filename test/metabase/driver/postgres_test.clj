ab44e7915176d9a8d1032073b02fa8d308b18863
Cam Saul <cam@geotip.com>
2015-08-04 11:57:46 -0700

    slightly shorter name for database def

--- test/metabase/driver/postgres_test.clj
+++ test/metabase/driver/postgres_test.clj
@@ -51,7 +51,7 @@
                                                  :dbname "bird_sightings"
                                                  :user   "camsaul"}))
 ;;; # UUID Support
-(def-database-definition ^:private postgres-with-uuid
+(def-database-definition ^:private with-uuid
   ["users"
    [{:field-name "user_id", :base-type :UUIDField}]
    [[#uuid "4f01dcfd-13f7-430c-8e6f-e505c0851027"]
@@ -69,14 +69,14 @@
                    [3 #uuid "da1d6ecc-e775-4008-b366-c38e7a2e8433"]
                    [4 #uuid "7a5ce4a2-0958-46e7-9685-1a4eaa3bd08a"]
                    [5 #uuid "84ed434e-80b4-41cf-9c88-e334427104ae"]]}
-  (-> (Q run against metabase.driver.postgres-test/postgres-with-uuid using postgres
+  (-> (Q run against metabase.driver.postgres-test/with-uuid using postgres
          return :data
          aggregate rows of users)
       (update :cols (partial mapv #(dissoc % :id :table_id)))))
 
 ;; Check that we can filter by a UUID Field
 (expect [[2 #uuid "4652b2e7-d940-4d55-a971-7e484566663e"]]
-  (Q run against metabase.driver.postgres-test/postgres-with-uuid using postgres
+  (Q run against metabase.driver.postgres-test/with-uuid using postgres
      return :data :rows
      aggregate rows of users
      filter = user_id "4652b2e7-d940-4d55-a971-7e484566663e"))
