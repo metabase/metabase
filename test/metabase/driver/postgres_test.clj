0489150a451b7acb94ec4ba2bd5946a672ec233c
Cam Saul <cam@geotip.com>
2015-08-04 11:52:51 -0700

    make sure to fully qualify the dataset names

--- test/metabase/driver/postgres_test.clj
+++ test/metabase/driver/postgres_test.clj
@@ -69,14 +69,14 @@
                    [3 #uuid "da1d6ecc-e775-4008-b366-c38e7a2e8433"]
                    [4 #uuid "7a5ce4a2-0958-46e7-9685-1a4eaa3bd08a"]
                    [5 #uuid "84ed434e-80b4-41cf-9c88-e334427104ae"]]}
-  (-> (Q run against postgres-with-uuid using postgres
+  (-> (Q run against metabase.driver.postgres-test/postgres-with-uuid using postgres
          return :data
          aggregate rows of users)
       (update :cols (partial mapv #(dissoc % :id :table_id)))))
 
 ;; Check that we can filter by a UUID Field
 (expect [[2 #uuid "4652b2e7-d940-4d55-a971-7e484566663e"]]
-  (Q run against postgres-with-uuid using postgres
+  (Q run against metabase.driver.postgres-test/postgres-with-uuid using postgres
      return :data :rows
      aggregate rows of users
      filter = user_id "4652b2e7-d940-4d55-a971-7e484566663e"))
