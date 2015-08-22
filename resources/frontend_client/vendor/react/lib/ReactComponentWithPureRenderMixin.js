b5576176ab7ade089aaed4133940fba5292b8a23
Tom Robinson <tlrobinson@gmail.com>
2015-08-21 17:30:22 -0700

    Improved webpack configuration, always build our own React but provide the correct flags to strip debugging info

--- resources/frontend_client/vendor/react/lib/ReactComponentWithPureRenderMixin.js
+++
@@ -1,6 +0,0 @@
-// stub for dependencies that require('react/lib/ReactComponentWithPureRenderMixin')
-// e.x. react-grid-layout
-
-import React from "react/addons";
-
-export default React.addons.ReactComponentWithPureRenderMixin;
