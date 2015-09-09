42e7f9037175d4d9552c2111dbabe11de106f93d
Allen Gilliland <agilliland@gmail.com>
2015-09-08 18:04:59 -0700

    try to get the smiley face working, but something not right :(

--- resources/frontend_client/app/home/components/Homepage.react.js
+++ resources/frontend_client/app/home/components/Homepage.react.js
@@ -53,7 +53,24 @@ export default class Homepage extends Component {
                     <div style={this.styles.main}>
                         <div style={this.styles.mainWrapper}>
                             <header style={this.styles.headerGreeting} className="pb4">
-                                <span className="float-left"><Icon className="mr3" name={'star'} height={36} width={36}></Icon></span>
+                                <span className="float-left">
+                                    <svg width="48px" height="48px" viewBox="0 0 48 48">
+                                        <defs>
+                                            <filter x="-50%" y="-50%" width="200%" height="200%" filterUnits="objectBoundingBox" id="filter-1">
+                                                <feOffset dx="0" dy="1" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+                                                <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                                                <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.0982591712 0" in="shadowBlurOuter1" type="matrix" result="shadowMatrixOuter1"></feColorMatrix>
+                                                <feMerge>
+                                                    <feMergeNode in="shadowMatrixOuter1"></feMergeNode>
+                                                    <feMergeNode in="SourceGraphic"></feMergeNode>
+                                                </feMerge>
+                                            </filter>
+                                        </defs>
+                                        <g filter="url(#filter-1)" fill="#FFFFFF">
+                                            <path d="M24,43 C35.045695,43 44,34.045695 44,23 C44,11.954305 35.045695,3 24,3 C12.954305,3 4,11.954305 4,23 C4,34.045695 12.954305,43 24,43 Z M18.173913,21.0544037 C19.1344083,21.0544037 19.9130435,19.3819413 19.9130435,17.3188544 C19.9130435,15.2557674 19.1344083,13.583305 18.173913,13.583305 C17.2134178,13.583305 16.4347826,15.2557674 16.4347826,17.3188544 C16.4347826,19.3819413 17.2134178,21.0544037 18.173913,21.0544037 Z M28.9565217,21.0544037 C29.917017,21.0544037 30.6956522,19.3819413 30.6956522,17.3188544 C30.6956522,15.2557674 29.917017,13.583305 28.9565217,13.583305 C27.9960265,13.583305 27.2173913,15.2557674 27.2173913,17.3188544 C27.2173913,19.3819413 27.9960265,21.0544037 28.9565217,21.0544037 Z M24,34.5616363 C28.4700217,34.5616363 32.1182868,30.5056662 32.1182868,26.0356445 C26.4909049,26.0356446 23.3555312,26.0356445 15.9608154,26.0356445 C15.9608154,30.5056662 19.5299783,34.5616363 24,34.5616363 Z"></path>
+                                        </g>
+                                    </svg>
+                                </span>
                                 <span>{(user) ? this.state.greeting + ' ' + user.first_name : this.state.greeting}</span>
                             </header>
                             <div className="">
