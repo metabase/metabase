091e7771f460a1b7a7b2c3116ed918a180105475
Allen Gilliland <agilliland@gmail.com>
2015-09-09 10:51:48 -0700

    strip out old activity unit tests which are not relevant any more.

--- test/metabase/models/activity_test.clj
+++ test/metabase/models/activity_test.clj
@@ -1,29 +1,6 @@
 (ns metabase.models.activity-test
   (:require [expectations :refer :all]
             [korma.core :refer [table]]
-            [medley.core :as m]
-            [metabase.db :as db]
-            (metabase.models [activity :refer :all]
-                             [card :refer [Card]]
-                             [interface :refer [defentity]])
-            [metabase.test.data.users :refer :all]
-            [metabase.util :as u]))
+            [metabase.events.activity-feed :refer :all]
+            [metabase.test.data.users :refer :all]))
 
-
-;;; # REVISIONS + PUSH-REVISION
-
-;; Test valid-activity-topic?
-(expect [false
-         false
-         false
-         true
-         true]
-        [(valid-activity-topic? nil)
-         (valid-activity-topic? {})
-         (valid-activity-topic? [])
-         (valid-activity-topic? "user-create")
-         (valid-activity-topic? :user-create)])
-
-;; Test topic->model
-(expect []
-        [(topic->model :abc)])
