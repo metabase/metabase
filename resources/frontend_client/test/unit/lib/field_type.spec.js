8288ad1221e6931b96915ba97f2d3a92ce37f603
Kyle Doherty <kyle.l.doherty@gmail.com>
2015-09-18 14:47:42 -0700

    fixes

--- resources/frontend_client/test/unit/lib/field_type.spec.js
+++ resources/frontend_client/test/unit/lib/field_type.spec.js
@@ -14,43 +14,43 @@ const baseField = {
 
 describe('parseBaseType', () => {
     it('should know a date', () => {
-        expect(parseBaseType('DateField').toEqual('time'))
-        expect(parseBaseType('DateTimeField').toEqual('time'))
-        expect(parseBaseType('TimeField').toEqual('time'))
+        expect(parseBaseType('DateField')).toEqual('time')
+        expect(parseBaseType('DateTimeField')).toEqual('time')
+        expect(parseBaseType('TimeField')).toEqual('time')
     })
     it('should know a number', () => {
-        expect(parseBaseType('BigIntegerField').toEqual('number'))
-        expect(parseBaseType('IntegerField').toEqual('number'))
-        expect(parseBaseType('FloatField').toEqual('number'))
-        expect(parseBaseType('DecimalField').toEqual('number'))
+        expect(parseBaseType('BigIntegerField')).toEqual('number')
+        expect(parseBaseType('IntegerField')).toEqual('number')
+        expect(parseBaseType('FloatField')).toEqual('number')
+        expect(parseBaseType('DecimalField').toEqual('number')
     })
     it('should know a string', () => {
-        expect(parseBaseType('CharField').toEqual('string'))
-        expect(parseBaseType('TextField').toEqual('string'))
+        expect(parseBaseType('CharField')).toEqual('string')
+        expect(parseBaseType('TextField')).toEqual('string')
     })
     it('should know a bool', () => {
-        expect(parseBaseType('BooleanField').toEqual('bool'))
+        expect(parseBaseType('BooleanField')).toEqual('bool')
     })
     it('should know what it doesn\'t know', () => {
-        expect(parseBaseType('DERP DERP DERP').toEqual('SODA'))
+        expect(parseBaseType('DERP DERP DERP')).toEqual('SODA')
     })
 })
 
 describe('parseSpecialType', () => {
     it('should know a date', () => {
-        expect(parseSpecialType('timestamp_seconds').toEqual('time'))
-        expect(parseSpecialType('timestamp_millisecons').toEqual('time'))
+        expect(parseSpecialType('timestamp_seconds')).toEqual('time')
+        expect(parseSpecialType('timestamp_millisecons')).toEqual('time')
     })
     it('should know a location', () => {
-        expect(parseSpecialType('city').toEqual('location'))
-        expect(parseSpecialType('country').toEqual('location'))
-        expect(parseSpecialType('latitude').toEqual('location'))
-        expect(parseSpecialType('longitude').toEqual('location'))
+        expect(parseSpecialType('city')).toEqual('location')
+        expect(parseSpecialType('country')).toEqual('location')
+        expect(parseSpecialType('latitude')).toEqual('location')
+        expect(parseSpecialType('longitude')).toEqual('location')
     })
 })
 
 describe('getUmbrellaType', () => {
     it('should parse a special type if both types are present', () => {
-        expect(getUmbrellaType(specialField).toEqual('location'))
+        expect(getUmbrellaType(specialField)).toEqual('location')
     })
 })
