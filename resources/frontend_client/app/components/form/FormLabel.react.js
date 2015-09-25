a41f10c18cf363427a9dbcb11e676c1e69b446a5
Allen Gilliland <agilliland@gmail.com>
2015-09-25 14:18:18 -0700

    remove `help` param from FormLabel, we'll handle that elsewhere, and allow explicit setting of the error message.

--- resources/frontend_client/app/components/form/FormLabel.react.js
+++ resources/frontend_client/app/components/form/FormLabel.react.js
@@ -6,16 +6,21 @@ import React, { Component, PropTypes } from "react";
 export default class FormLabel extends Component {
 
     render() {
-        let { fieldName, formError, help, title } = this.props;
-        const message = (formError && formError.data.errors && fieldName in formError.data.errors) ? formError.data.errors[fieldName] : undefined;
+        let { fieldName, formError, message, title } = this.props;
+
+        if (!message) {
+            message = (formError && formError.data.errors && fieldName in formError.data.errors) ? formError.data.errors[fieldName] : undefined;
+        }
 
         return (
-            <label className="Form-label Form-offset">{title}: {help !== undefined ? help : null} { message !== undefined ? <span>{message}</span> : null }</label>
+            <label className="Form-label Form-offset">{title}: { message !== undefined ? <span>{message}</span> : null }</label>
         );
     }
 }
 
 FormLabel.propTypes = {
     fieldName: PropTypes.string.isRequired,
+    formError: PropTypes.object,
+    message: PropTypes.string,
     title: PropTypes.string.isRequired
 }
