da2d0f39731b03b0aaa3586f1131dc7c5c27c428
Cam Saul <cam@geotip.com>
2015-09-28 13:51:34 -0700

    Sample Dataset sync improvements. Options for `defsetting` :yum:

--- src/metabase/core.clj
+++ src/metabase/core.clj
@@ -97,7 +97,7 @@
   []
   (log/info "Metabase Initializing ... ")
   ;; First of all, lets register a shutdown hook that will tidy things up for us on app exit
-  (.addShutdownHook (Runtime/getRuntime) (Thread. destroy))
+  (.addShutdownHook (Runtime/getRuntime) (Thread. ^Runnable destroy))
   (log/debug "Using Config:\n" (with-out-str (clojure.pprint/pprint config/config-all)))
 
   ;; Bootstrap the event system
@@ -152,8 +152,15 @@
 (def ^:private ^:const sample-dataset-name "Sample Dataset")
 (def ^:private ^:const sample-dataset-filename "sample-dataset.db.mv.db")
 
+(defsetting sample-dataset-id
+  "The string-serialized integer ID of the `Database` entry for the Sample Dataset. If this is `nil`, the Sample Dataset
+   hasn't been loaded yet, and we should do so; otherwise we've already loaded it, and should not do so again. Keep in
+   mind the user may delete the Sample Dataset's DB, so this ID is not guaranteed to correspond to an existent object."
+  nil
+  :internal true) ; don't expose in the UI
+
 (defn- add-sample-dataset! []
-  (when-not (db/exists? Database :name sample-dataset-name)
+  (when-not (sample-dataset-id)
     (try
       (log/info "Loading sample dataset...")
       (let [resource (-> (Thread/currentThread) ; hunt down the sample dataset DB file inside the current JAR
@@ -162,13 +169,15 @@
         (if-not resource
           (log/error (format "Can't load sample dataset: the DB file '%s' can't be found by the ClassLoader." sample-dataset-filename))
           (let [h2-file (-> (.getPath resource)
-                            (s/replace #"^file:" "zip:")         ; to connect to an H2 DB inside a JAR just replace file: with zip:
-                            (s/replace #"\.mv\.db$" "")          ; strip the .mv.db suffix from the path
-                            (str ";USER=GUEST;PASSWORD=guest"))] ; specify the GUEST user account created for the DB
-            (driver/sync-database! (db/ins Database
-                                     :name    sample-dataset-name
-                                     :details {:db h2-file}
-                                     :engine  :h2)))))
+                            (s/replace #"^file:" "zip:")        ; to connect to an H2 DB inside a JAR just replace file: with zip:
+                            (s/replace #"\.mv\.db$" "")         ; strip the .mv.db suffix from the path
+                            (str ";USER=GUEST;PASSWORD=guest")) ; specify the GUEST user account created for the DB
+                db      (db/ins Database
+                          :name    sample-dataset-name
+                          :details {:db h2-file}
+                          :engine  :h2)]
+            (driver/sync-database! db)
+            (sample-dataset-id (str (:id db))))))
       (catch Throwable e
         (log/error (format "Failed to load sample dataset: %s" (.getMessage e)))))))
 
