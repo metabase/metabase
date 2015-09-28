fbddeab86349d0c7de4c1f1587fa6eb13de8b661
Cam Saul <cam@geotip.com>
2015-09-28 11:10:58 -0700

    code formatting tweak

--- resources/frontend_client/app/auth/auth.controllers.js
+++ resources/frontend_client/app/auth/auth.controllers.js
@@ -95,34 +95,35 @@ AuthControllers.controller('PasswordReset', ['$scope', '$routeParams', '$locatio
     }, function(result) {
         if (!result.valid) {
             $location.path('/auth/password_reset_token_expired');
-        } else {
-            $scope.resetSuccess = false;
-            $scope.newUserJoining = ($location.hash() === 'new');
+            return;
+        }
 
-            $scope.resetPassword = function(password) {
-                $scope.$broadcast("form:reset");
+        $scope.resetSuccess = false;
+        $scope.newUserJoining = ($location.hash() === 'new');
 
-                if (password != $scope.password2) {
-                    $scope.$broadcast("form:api-error", {'data': {'errors': {'password2': "Passwords do not match"}}});
-                    return;
+        $scope.resetPassword = function(password) {
+            $scope.$broadcast("form:reset");
+
+            if (password != $scope.password2) {
+                $scope.$broadcast("form:api-error", {'data': {'errors': {'password2': "Passwords do not match"}}});
+                return;
+            }
+
+            Session.reset_password({
+                'token': $routeParams.token,
+                'password': password
+            }, function (result) {
+                $scope.resetSuccess = true;
+
+                // we should have a valid session that we can use immediately now!
+                if (result.session_id) {
+                    AuthUtil.setSession(result.session_id);
                 }
 
-                Session.reset_password({
-                    'token': $routeParams.token,
-                    'password': password
-                }, function (result) {
-                    $scope.resetSuccess = true;
-
-                    // we should have a valid session that we can use immediately now!
-                    if (result.session_id) {
-                        AuthUtil.setSession(result.session_id);
-                    }
-
-                }, function (error) {
-                    $scope.$broadcast("form:api-error", error);
-                });
-            };
-        }
+            }, function (error) {
+                $scope.$broadcast("form:api-error", error);
+            });
+        };
     }, function(error) {
         $scope.$broadcast('form:api-error', error);
     });
