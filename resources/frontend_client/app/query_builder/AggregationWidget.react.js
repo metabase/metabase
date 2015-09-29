7202edc5c62e21ebc1d23464bb4b699da4d39baf
Tom Robinson <tlrobinson@gmail.com>
2015-09-29 02:03:15 -0700

    Remove chevrondowns from aggregation widgets, convert to ES6

--- resources/frontend_client/app/query_builder/AggregationWidget.react.js
+++ resources/frontend_client/app/query_builder/AggregationWidget.react.js
@@ -1,26 +1,28 @@
-'use strict';
+"use strict";
 
-import _ from "underscore";
+import React, { Component, PropTypes } from "react";
 
 import SelectionModule from './SelectionModule.react';
 import FieldWidget from './FieldWidget.react';
-import Icon from "metabase/components/Icon.react";
 
 import Query from "metabase/lib/query";
 
-export default React.createClass({
-    displayName: 'AggregationWidget',
-    propTypes: {
-        aggregation: React.PropTypes.array.isRequired,
-        tableMetadata: React.PropTypes.object.isRequired,
-        updateAggregation: React.PropTypes.func.isRequired
-    },
+import _ from "underscore";
 
-    componentWillMount: function() {
+export default class AggregationWidget extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+        _.bindAll(this, "setAggregation", "setAggregationTarget");
+    }
+
+    componentWillMount() {
         this.componentWillReceiveProps(this.props);
-    },
+    }
 
-    componentWillReceiveProps: function(newProps) {
+    componentWillReceiveProps(newProps) {
         // build a list of aggregations that are valid, taking into account specifically if we have valid fields available
         var aggregationFieldOptions = [];
         var availableAggregations = [];
@@ -43,9 +45,9 @@ export default React.createClass({
             availableAggregations: availableAggregations,
             aggregationFieldOptions: aggregationFieldOptions
         });
-    },
+    }
 
-    setAggregation: function(aggregation) {
+    setAggregation(aggregation) {
         var queryAggregation = [aggregation];
 
         // check to see if this aggregation type requires another choice
@@ -59,16 +61,16 @@ export default React.createClass({
         });
 
         this.props.updateAggregation(queryAggregation);
-    },
+    }
 
-    setAggregationTarget: function(target) {
+    setAggregationTarget(target) {
         var queryAggregation = this.props.aggregation;
         queryAggregation[1] = target;
 
         this.props.updateAggregation(queryAggregation);
-    },
+    }
 
-    render: function() {
+    render() {
         if (this.props.aggregation.length === 0) {
             // we can't do anything without a valid aggregation
             return;
@@ -93,7 +95,6 @@ export default React.createClass({
                         setField={this.setAggregationTarget}
                         isInitiallyOpen={aggregationTargetListOpen}
                     />
-                    <Icon name="chevrondown" width="8px" height="8px" />
                 </div>
             );
         }
@@ -116,4 +117,10 @@ export default React.createClass({
             </div>
         );
     }
-});
+}
+
+AggregationWidget.propTypes = {
+    aggregation: PropTypes.array.isRequired,
+    tableMetadata: PropTypes.object.isRequired,
+    updateAggregation: PropTypes.func.isRequired
+};
