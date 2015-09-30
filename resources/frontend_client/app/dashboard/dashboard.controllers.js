3c2be31e137c9ff06c32a18b21222dd0c9574172
Allen Gilliland <agilliland@gmail.com>
2015-09-30 16:24:48 -0700

    clearing out some old angular code that we don't use any more.

--- resources/frontend_client/app/dashboard/dashboard.controllers.js
+++ resources/frontend_client/app/dashboard/dashboard.controllers.js
@@ -41,32 +41,3 @@ DashboardControllers.controller('Dashboard', ['$scope', '$rootScope', '$routePar
     $scope.store = finalCreateStore(reducer, { selectedDashboard: $routeParams.dashId });
     // $scope.monitor = LogMonitor;
 }]);
-
-DashboardControllers.controller('DashList', ['$scope', '$location', 'Dashboard', function($scope, $location, Dashboard) {
-    $scope.dashboards = [];
-
-    var refreshListing = function() {
-        Dashboard.list({
-            'filterMode': 'all'
-        }, function (dashes) {
-            $scope.dashboards = dashes;
-        }, function (error) {
-            console.log('error getting dahsboards list', error);
-        });
-    };
-
-    $scope.$on("dashboard:create", function(event, dashboardId) {
-        refreshListing();
-    });
-
-    $scope.$on("dashboard:delete", function(event, dashboardId) {
-        refreshListing();
-    });
-
-    $scope.$on("dashboard:update", function(event, dashboardId) {
-        refreshListing();
-    });
-
-    // always initialize with a fresh listing
-    refreshListing();
-}]);
