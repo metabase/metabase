883b19f33bb72c6d15395794ed89d5ceb5e1ee61
Tom Robinson <tlrobinson@gmail.com>
2015-10-23 11:35:59 -0700

    Fix native query errors

--- frontend/src/query_builder/QueryVisualization.jsx
+++ frontend/src/query_builder/QueryVisualization.jsx
@@ -208,7 +208,7 @@ export default class QueryVisualization extends Component {
                                     <path d="M4 8 L8 4 L16 12 L24 4 L28 8 L20 16 L28 24 L24 28 L16 20 L8 28 L4 24 L12 16 z "></path>
                                 </svg>
                             </div>
-                            <span className="QueryError-message">{message}</span>
+                            <span className="QueryError-message">{error}</span>
                         </div>
                     );
                 } else {
