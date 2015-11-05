8f33ba6809aa25db10d65218b8ad971410af3696
Tom Robinson <tlrobinson@gmail.com>
2015-11-05 12:30:59 -0800

    Move duplicated redux functions to a lib

--- frontend/src/card/actions.js
+++ frontend/src/card/actions.js
@@ -1,33 +1,6 @@
 import { createAction } from "redux-actions";
+import { AngularResourceProxy, createThunkAction } from "metabase/lib/redux";
 import moment from "moment";
-
-
-// HACK: just use our Angular resources for now
-function AngularResourceProxy(serviceName, methods) {
-    methods.forEach((methodName) => {
-        this[methodName] = function(...args) {
-            let service = angular.element(document.querySelector("body")).injector().get(serviceName);
-            return service[methodName](...args).$promise;
-        }
-    });
-}
-
-// similar to createAction but accepts a (redux-thunk style) thunk and dispatches based on whether
-// the promise returned from the thunk resolves or rejects, similar to redux-promise
-function createThunkAction(actionType, actionThunkCreator) {
-    return function(...actionArgs) {
-        var thunk = actionThunkCreator(...actionArgs);
-        return async function(dispatch, getState) {
-            try {
-                let payload = await thunk(dispatch, getState);
-                dispatch({ type: actionType, payload });
-            } catch (error) {
-                dispatch({ type: actionType, payload: error, error: true });
-                throw error;
-            }
-        }
-    }
-}
 
 // resource wrappers
 const CardApi = new AngularResourceProxy("Card", ["list"]);
--- frontend/src/setup/actions.js
+++ frontend/src/setup/actions.js
@@ -1,37 +1,11 @@
 //import _ from "underscore";
 import { createAction } from "redux-actions";
+import { AngularResourceProxy, createThunkAction } from "metabase/lib/redux";
 
 import MetabaseAnalytics from "metabase/lib/analytics";
 import MetabaseCookies from "metabase/lib/cookies";
 import MetabaseSettings from "metabase/lib/settings";
 
-
-// HACK: just use our Angular resources for now
-function AngularResourceProxy(serviceName, methods) {
-    methods.forEach((methodName) => {
-        this[methodName] = function(...args) {
-            let service = angular.element(document.querySelector("body")).injector().get(serviceName);
-            return service[methodName](...args).$promise;
-        }
-    });
-}
-
-// similar to createAction but accepts a (redux-thunk style) thunk and dispatches based on whether
-// the promise returned from the thunk resolves or rejects, similar to redux-promise
-function createThunkAction(actionType, actionThunkCreator) {
-    return function(...actionArgs) {
-        var thunk = actionThunkCreator(...actionArgs);
-        return async function(dispatch, getState) {
-            try {
-                let payload = await thunk(dispatch, getState);
-                dispatch({ type: actionType, payload });
-            } catch (error) {
-                dispatch({ type: actionType, payload: error, error: true });
-                throw error;
-            }
-        }
-    }
-}
 
 // // resource wrappers
 const SetupApi = new AngularResourceProxy("Setup", ["create", "validate_db"]);
