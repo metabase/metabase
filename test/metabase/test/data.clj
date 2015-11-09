0b252624da7c61875c26ac6431f437b675fdf29d
Allen Gilliland <agilliland@gmail.com>
2015-11-08 20:58:10 -0800

    add some basic macros for `expect-with-test-db` and `expect-with-test-data` which allow us to more easily define unit tests which do some prep work for setting up data the test needs to run.

--- test/metabase/test/data.clj
+++ test/metabase/test/data.clj
@@ -4,6 +4,7 @@
                      [walk :as walk])
             [clojure.tools.logging :as log]
             [colorize.core :as color]
+            [expectations :as expectations]
             [medley.core :as m]
             (metabase [db :refer :all]
                       [driver :as driver])
@@ -212,3 +213,53 @@
   `(-with-temp-db ~database-definition
      (fn [~db-binding]
        ~@body)))
+
+
+(defmacro expect-with-test-db
+  "Identical to `expect` but wraps the entire evaluation in `with-temp-db` so that both expected and actual statements
+   have access to the test data required for the test.
+
+     (expect-with-test-db [_ test-data]
+       (id :venues :id)
+       (sel :one :field [ForeignKey :destination_id] :origin_id (id :checkins :venue_id)))"
+  [test-db-binding expected actual]
+  (let [fn-name (gensym)]
+    `(def ~(vary-meta fn-name assoc :expectation true)
+       (fn []
+         (with-temp-db ~test-db-binding
+           (expectations/doexpect ~expected ~actual))))))
+
+
+;;; ## ---------------------------------------- Test-Data Interface ----------------------------------------
+
+
+(def base-test-data
+  {:init! (fn [_])
+   :restart! (fn [_])
+   :destroy! (fn [_])})
+
+(defn with-test-data [i-test-data f]
+  (try
+    (binding [*sel-disable-logging* true]
+      (when (:init! i-test-data)
+        ((:init! i-test-data) i-test-data))
+      (binding [*sel-disable-logging* false]
+        (f)))
+    (finally
+      (binding [*sel-disable-logging* true]
+        (when (:destroy! i-test-data)
+          ((:destroy! i-test-data) i-test-data))))))
+
+(defmacro expect-with-test-data
+  "Identical to `expect` but wraps the entire evaluation in `with-temp-db` so that both expected and actual statements
+   have access to the test data required for the test.
+
+     (expect-with-test-db [_ test-data]
+       (id :venues :id)
+       (sel :one :field [ForeignKey :destination_id] :origin_id (id :checkins :venue_id)))"
+  [i-test-data expected actual]
+  (let [fn-name (gensym)]
+    `(def ~(vary-meta fn-name assoc :expectation true)
+       (fn []
+         (with-test-data ~i-test-data
+           (fn [] (expectations/doexpect ~expected ~actual)))))))
