6ce2bec50874f047b967d43430e0d6e1fcc8399a
Tom Robinson <tlrobinson@gmail.com>
2015-11-11 11:32:42 -0800

    Switch spinner to CSS because SVG animations are deprecated and Chrome complains

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -4,19 +4,16 @@ export default class LoadingSpinner extends Component {
     static defaultProps = {
         width: '32px',
         height: '32px',
+        borderWidth: '4px',
         fill: 'currentcolor',
-        spinnerClass: 'Loading-indicator',
+        spinnerClassName: 'LoadingSpinner'
     };
 
     render() {
-        var props = this.props;
-        var animate = '<animateTransform attributeName="transform" type="rotate" from="0 16 16" to="360 16 16" dur="0.8s" repeatCount="indefinite" />';
+        var { width, height, borderWidth, className, spinnerClassName } = this.props;
         return (
-            <div className={props.spinnerClass}>
-                <svg viewBox="0 0 32 32" {...props}>
-                  <path opacity=".25" d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"/>
-                  <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z" dangerouslySetInnerHTML={{__html: animate}}></path>
-                </svg>
+            <div className={className}>
+                <div className={spinnerClassName} style={{ width, height, borderWidth }}></div>
             </div>
         );
     }
