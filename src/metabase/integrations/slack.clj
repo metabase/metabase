8e6c1b6f6abd39d62235a62af7f1b80da69621f6
Allen Gilliland <agilliland@gmail.com>
2015-11-10 17:58:28 -0800

    tightening things up a little bit and moving all of our Slack api handling code into a metabase.integrations.slack namespace so it's centralized.  also added a new function for `channels-list` which pulls the list of channels available in Slack.

---
+++ src/metabase/integrations/slack.clj
@@ -0,0 +1,74 @@
+(ns metabase.integrations.slack
+  (:require [cheshire.core :as cheshire]
+            [clj-http.lite.client :as client]
+            [clj-http.client :as http]
+            [clojure.tools.logging :as log]
+            [metabase.models.setting :refer [defsetting]]))
+
+
+;; Define a setting which captures our Slack api token
+(defsetting slack-token "Slack API bearer token obtained from https://api.slack.com/web#authentication")
+
+(def ^:const slack-api-baseurl "https://slack.com/api")
+
+
+(defn slack-api-get
+  "Generic function which calls a given method on the Slack api via HTTP GET."
+  ([token method]
+    (slack-api-get token method {}))
+  ([token method params]
+    {:pre [(string? method)
+           (map? params)]}
+   (when token
+     (client/get (str slack-api-baseurl "/" method) {:query-params   (merge params {:token token})
+                                                     :conn-timeout   1000
+                                                     :socket-timeout 1000}))))
+
+(defn slack-api-post
+  "Generic function which calls a given method on the Slack api via HTTP POST."
+  ([token method]
+   (slack-api-get token method {}))
+  ([token method params]
+   {:pre [(string? method)
+          (map? params)]}
+   (when token
+     (client/post (str slack-api-baseurl "/" method) {:form-params   (merge params {:token token})
+                                                      :conn-timeout   1000
+                                                      :socket-timeout 1000}))))
+
+(defn- ^:private handle-api-response
+  "Simple helper that checks that response is a HTTP 200 and deserializes the `:body` if so, otherwise logs an error"
+  [response]
+  (if (= 200 (:status response))
+    (cheshire/parse-string (:body response))
+    (log/warn "Error in Slack api response:" (with-out-str (clojure.pprint/pprint response)))))
+
+(defn channels-list
+  "Calls Slack api `channels.list` function and returns the list of available channels."
+  []
+  (-> (slack-api-get (slack-token) "channels.list" {:exclude_archived 1})
+      (handle-api-response)))
+
+(defn files-upload
+  "Calls Slack api `files.upload` function and returns the url of the uploaded file."
+  [file]
+  (let [response (http/post (str slack-api-baseurl "/files.upload") {:multipart [["token" (slack-token)]
+                                                                                 ["file" file]]
+                                                                     :as :json})]
+    (if (= 200 (:status response))
+      (:body response)
+      (log/warn "Error uploading file to Slack:" (with-out-str (clojure.pprint/pprint response))))))
+
+(defn chat-post-message
+  "Calls Slack api `chat.postMessage` function and posts a message to a given channel."
+  ([channel text]
+    (chat-post-message channel text []))
+  ([channel text attachments]
+   {:pre [(string? channel)
+          (string? text)]}
+    ;; TODO: it would be nice to have an emoji or icon image to use here
+   (-> (slack-api-post (slack-token) "chat.postMessage" {:channel     channel
+                                                         :username    "MetaBot"
+                                                         :text        text
+                                                         :attachments attachments})
+       (handle-api-response))))
