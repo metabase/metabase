6900c443be92152cf614d80a66177e39f5a86de1
Allen Gilliland <agilliland@gmail.com>
2015-11-13 08:20:50 -0800

    add a new api path for /api/email/* which has endpoints specific to dealing with email settings and testing.

---
+++ src/metabase/api/email.clj
@@ -0,0 +1,42 @@
+(ns metabase.api.email
+  "/api/email endpoints"
+  (:require [compojure.core :refer [GET PUT DELETE POST]]
+            [metabase.api.common :refer :all]
+            [metabase.email :as email]
+            [metabase.models.setting :as setting]
+            [clojure.set :as set]))
+
+(defonce ^:private ^:const email-settings
+  {:email-smtp-host     :host
+   :email-smtp-username :user
+   :email-smtp-password :pass
+   :email-smtp-port     :port
+   :email-smtp-security :security
+   :email-from-address  :from})
+
+(defendpoint PUT "/"
+  "Update multiple `Settings` values.  You must be a superuser to do this."
+  [:as {settings :body}]
+  {settings [Required Dict]}
+  (check-superuser)
+  (setting/set-all (select-keys settings (keys email-settings))))
+
+(defendpoint POST "/test"
+  "Send a test email using the supplied settings. You must be a superuser to do this."
+  [:as {settings :body}]
+  {settings [Required Dict]}
+  (check-superuser)
+  (let [smtp-details (-> (select-keys settings (keys email-settings))
+                         (set/rename-keys email-settings))
+        response     (email/send-message
+                       :details      smtp-details
+                       :subject      "Metabase Test Email"
+                       :recipients   [(:email @*current-user*)]
+                       :message-type :text
+                       :message      "The Metabase emails are working, hooray!")]
+    (if (= (:error response) :SUCCESS)
+      response
+      {:status 500
+       :body   response})))
+
+(define-routes)
