3b41a112a5892a069a89082b4101d7756aba713a
Allen Gilliland <agilliland@gmail.com>
2015-11-23 15:52:28 -0800

    fixing indentation issue.

--- src/metabase/task/sync_databases.clj
+++ src/metabase/task/sync_databases.clj
@@ -19,7 +19,7 @@
 (jobs/defjob SyncDatabases
   [ctx]
   (dorun
-   (for [database (db/sel :many Database :is_sample false)] ; skip Sample Dataset DB
+    (for [database (db/sel :many Database :is_sample false)] ; skip Sample Dataset DB
       (try
         ;; NOTE: this happens synchronously for now to avoid excessive load if there are lots of databases
         (driver/sync-database! database)
