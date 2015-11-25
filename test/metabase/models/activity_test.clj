ae8145003a53ea3a91be7fd07e1ffb762387d468
Allen Gilliland <agilliland@gmail.com>
2015-11-24 16:53:42 -0800

    remove empty unit test.

--- test/metabase/models/activity_test.clj
+++
@@ -1,5 +0,0 @@
-(ns metabase.models.activity-test
-  (:require [expectations :refer :all]
-            [korma.core :refer [table]]
-            [metabase.events.activity-feed :refer :all]
-            [metabase.test.data.users :refer :all]))
