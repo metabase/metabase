98a23724e1b6641c22d6ee432b8162cec7204da5
Allen Gilliland <agilliland@gmail.com>
2015-12-01 15:38:43 -0800

    tidy up and consolidate all of our aws-eb scripts.  remove unneeded release/dockerhub scripts.

--- bin/aws-eb-docker/release-eb-version.sh
+++ bin/aws-eb-docker/release-eb-version.sh
@@ -2,7 +2,6 @@
 
 BASEDIR=$(dirname $0)
 CURRENTDIR=$PWD
-PROJECT_ROOT=$(cd ${BASEDIR}/../../..; pwd)
 
 DOCKERHUB_REPO=metabase
 S3_BUCKET=downloads.metabase.com
@@ -27,7 +26,7 @@ S3_FILE=s3://${S3_BUCKET}/${MB_TAG}/metabase-aws-eb.zip
 S3_LAUNCH_FILE=s3://${S3_BUCKET}/${MB_TAG}/${EB_LAUNCH_FILE}
 
 # make the release file
-${PROJECT_ROOT}/bin/aws-eb-docker/build-eb-version.sh ${MB_TAG} ${DOCKERHUB_REPO}
+${BASEDIR}/build-eb-version.sh ${MB_TAG} ${DOCKERHUB_REPO}
 
 # dynamically insert our MB version into the EB launch file
 sed "s/@@MB_TAG@@/${MB_TAG}/" < ${BASEDIR}/${EB_LAUNCH_FILE}.template > ${BASEDIR}/${EB_LAUNCH_FILE}
