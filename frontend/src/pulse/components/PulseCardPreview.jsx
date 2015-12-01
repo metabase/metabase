bff1fdfb9b70ca91c07d6284912fedec59f07e8a
Tom Robinson <tlrobinson@gmail.com>
2015-12-01 15:52:58 -0800

    Don't let large pulse cards overflow the pulse card preivew. Just scroll for now

--- frontend/src/pulse/components/PulseCardPreview.jsx
+++ frontend/src/pulse/components/PulseCardPreview.jsx
@@ -43,7 +43,7 @@ export default class PulseCardPreview extends Component {
                 <a className="text-grey-2 absolute" style={{ top: "15px", right: "15px" }} onClick={this.props.onRemove}>
                     <Icon name="close" width={16} height={16} />
                 </a>
-                <div className="bordered rounded flex-full" style={{ display: !cardPreview && "none" }} dangerouslySetInnerHTML={{__html: cardPreview && cardPreview.pulse_card_html}} />
+                <div className="bordered rounded flex-full scroll-x" style={{ display: !cardPreview && "none" }} dangerouslySetInnerHTML={{__html: cardPreview && cardPreview.pulse_card_html}} />
                 { !cardPreview &&
                     <div className="flex-full flex align-center layout-centered pt1">
                         <LoadingSpinner className="inline-block" />
