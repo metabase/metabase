09a8d9444523dda32839c4c48cb1786f724d4bd8
Allen Gilliland <agilliland@gmail.com>
2015-12-01 09:37:39 -0800

    update our aws-eb release scripts to reuse our more generic scripts for building an aws-eb-docker deployment file and add in a little code which publishes the EB launch file.

--- bin/release/aws-eb/release-eb-version.sh
+++ bin/release/aws-eb/release-eb-version.sh
@@ -2,65 +2,46 @@
 
 BASEDIR=$(dirname $0)
 CURRENTDIR=$PWD
+PROJECT_ROOT=$(cd ${BASEDIR}/../../..; pwd)
 
+DOCKERHUB_REPO=metabase
 S3_BUCKET=downloads.metabase.com
 
 
 # parse any cmd line arguments
-while [[ $# > 0 ]]
-do
-key="$1"
+MB_TAG=$1
+if [ -z $MB_TAG ]; then
+    echo "usage: $0 <release-name> [--publish]"
+    exit 1
+fi
 
-case $key in
-    --publish)
-    PUBLISH=YES
-    ;;
-    *)
-        # unknown option
-    ;;
-esac
-
-shift # past argument or value
-done
-
-
-if [ "$PUBLISH" == "YES" ] && [ -z "$AWS_DEFAULT_PROFILE" ]; then
+if [ -z "$AWS_DEFAULT_PROFILE" ]; then
     echo "In order to publish an Elastic Beanstalk version to S3 you must set the AWS_DEFAULT_PROFILE environment variable"
     exit 1
 fi
 
-# we need to know the tag of the current repo, and if we can't get a tag then bail
-MB_TAG=`${BASEDIR}/../current_tag.sh`
-if [ $? -eq 1 ]; then
-    echo "Failed to get current tag from git.  Make sure you are on a tagged commit!"
-    exit 1
-fi
+# TODO: improve this (hard coding)
+EB_FILE=/tmp/${MB_TAG}.zip
+EB_LAUNCH_FILE=launch-aws-eb.html
+S3_FILE=s3://${S3_BUCKET}/${MB_TAG}/metabase-aws-eb.zip
+S3_LAUNCH_FILE=s3://${S3_BUCKET}/${MB_TAG}/${EB_LAUNCH_FILE}
 
-EB_FILE=metabase-aws-eb.zip
-S3_FILE=s3://${S3_BUCKET}/${MB_TAG}/${EB_FILE}
+# make the release file
+${PROJECT_ROOT}/bin/aws-eb-docker/build-eb-version.sh ${MB_TAG} ${DOCKERHUB_REPO}
 
-
-echo "Building Elastic Beanstalk app version from Metabase ${MB_TAG}"
-
-# dynamically insert our MB version into the EB config file
-sed "s/@@MB_TAG@@/${MB_TAG}/" < ${BASEDIR}/Dockerrun.aws.json.template > ${BASEDIR}/Dockerrun.aws.json
-
-# create our EB zip file
-cd $BASEDIR; zip -r ${EB_FILE} .ebextensions Dockerrun.aws.json; cd $CURRENTDIR
-
-# clean up the temporary Dockerrun.aws.json file we created
-rm ${BASEDIR}/Dockerrun.aws.json
+# dynamically insert our MB version into the EB launch file
+sed "s/@@MB_TAG@@/${MB_TAG}/" < ${BASEDIR}/${EB_LAUNCH_FILE}.template > ${BASEDIR}/${EB_LAUNCH_FILE}
 
 
-if [ "$PUBLISH" == "YES" ]; then
-    echo "Publishing EB file to S3 at ${S3_FILE}"
+echo "Publishing EB files to S3 at ${S3_FILE} and ${S3_LAUNCH_FILE}"
 
-    # s3 put
-    aws s3 cp ${BASEDIR}/${EB_FILE} ${S3_FILE}
+# s3 put
+aws s3 cp ${EB_FILE} ${S3_FILE}
+aws s3 cp ${BASEDIR}/${EB_LAUNCH_FILE} ${S3_LAUNCH_FILE}
 
-    # TODO: quick check to see that we succeeded
+# TODO: quick check to see that we succeeded
 
-fi
-
+# clean up the temporary EB launch file we created
+rm ${BASEDIR}/${EB_LAUNCH_FILE}
 
 echo "Done"
