76e40f4eb600bdd78f794c042edbe8fbfc4f0001
Tom Robinson <tlrobinson@gmail.com>
2016-01-19 17:09:55 -0800

    Add metabase-bootstrap.js to testing environment to emulate settings inlined in our index.html

---
+++ frontend/test/metabase-bootstrap.js
@@ -0,0 +1,14 @@
+window.MetabaseBootstrap = {
+  "timezones": [
+    "GMT",
+    "UTC",
+    "US\/Alaska",
+    "US\/Arizona",
+    "US\/Central",
+    "US\/Eastern",
+    "US\/Hawaii",
+    "US\/Mountain",
+    "US\/Pacific",
+    "America\/Costa_Rica"
+  ]
+};
