5bd601fd5632916e6885d3f99c7beeff3a124814
Kyle Doherty <kyle.l.doherty@gmail.com>
2016-02-03 12:01:56 -0800

    fix settings header

--- frontend/src/admin/settings/components/SettingsHeader.jsx
+++ frontend/src/admin/settings/components/SettingsHeader.jsx
@@ -5,11 +5,11 @@ import SaveStatus from "metabase/components/SaveStatus.jsx";
 export default class SettingsHeader extends Component {
     render() {
         return (
-            <div className="MetadataEditor-header flex align-center relative">
-                <div className="MetadataEditor-headerSection h2 text-grey-4">
+            <div className="MetadataEditor-header clearfix relative">
+                <div className="MetadataEditor-headerSection float-left h2 text-grey-4">
                     Settings
                 </div>
-                <div className="MetadataEditor-headerSection absolute right top bottom flex layout-centered">
+                <div className="MetadataEditor-headerSection absolute right float-right top bottom flex layout-centered">
                     <SaveStatus ref="status" />
                 </div>
             </div>
