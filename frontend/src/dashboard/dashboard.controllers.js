444fda567a74d856850cfebac3b5a80daece8f8b
Allen Gilliland <agilliland@gmail.com>
2016-02-09 20:52:50 -0800

    add the ability to load up a dashboard with the `?add=<card_id>` query string to start in edit mode and add the specified card to the dashboard.

--- frontend/src/dashboard/dashboard.controllers.js
+++ frontend/src/dashboard/dashboard.controllers.js
@@ -11,6 +11,7 @@ var DashboardControllers = angular.module('metabase.dashboard.controllers', []);
 DashboardControllers.controller('Dashboard', ['$scope', '$rootScope', '$routeParams', '$location', function($scope, $rootScope, $routeParams, $location) {
     $scope.Component = DashboardApp;
     $scope.props = {
+        addCardOnLoad: $routeParams.add,
         onChangeLocation: function(url) {
             $scope.$apply(() => $location.url(url));
         },
@@ -20,4 +21,9 @@ DashboardControllers.controller('Dashboard', ['$scope', '$rootScope', '$routePar
     };
     $scope.store = createStore(reducer, { selectedDashboard: $routeParams.dashId });
     // $scope.monitor = LogMonitor;
+
+    // this simply clears the query param so the url is tidy and the user doesn't accidentally reload and get the edit page again
+    if ($routeParams.add) {
+        $location.search("add", null);
+    }
 }]);
