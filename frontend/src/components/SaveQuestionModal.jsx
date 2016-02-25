94d09662c84f0e51e5977b2962c10df5cb22d6e3
Kyle Doherty <kyle.l.doherty@gmail.com>
2016-02-25 10:31:22 -0800

    fix issue with saving modal calling method on null value

--- frontend/src/components/SaveQuestionModal.jsx
+++ frontend/src/components/SaveQuestionModal.jsx
@@ -80,7 +80,8 @@ export default class SaveQuestionModal extends Component {
         let card = this.props.card;
 
         card.name = details.name.trim();
-        card.description = details.description.trim();
+        // since description is optional, it can be null, so check for a description before trimming it
+        details.description ? card.description = details.description.trim() : details.description;
         card.public_perms = 2; // public read/write
 
         if (details.saveType === "create") {
