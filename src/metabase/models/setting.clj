8cf15e5f79ee5d3643ed6c4345f2bd8bc302d761
Cam Saül <cammsaul@gmail.com>
2016-02-26 18:47:14 -0800

    Include default value in Settings dox

--- src/metabase/models/setting.clj
+++ src/metabase/models/setting.clj
@@ -142,9 +142,8 @@
        (format "You can also set its value with the env var `MB_%s`.\n"
                (s/upper-case (s/replace (name symb) #"-" "_")))
        "Clear its value by calling\n\n"
-       (format "    (%s nil)" symb)
-       (when default-value
-         (format "\n\nIts default value is `%s`." default-value))))
+       (format "    (%s nil)\n\n" symb)
+       (format "Its default value is `%s`." default-value)))
 
 (defmacro defsetting
   "Defines a new `Setting` that will be added to the DB at some point in the future.
