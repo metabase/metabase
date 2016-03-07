604e738717d2b07049c0215fe8687e1b0beac594
Kyle Doherty <kyle.l.doherty@gmail.com>
2016-03-07 13:38:01 -0800

    add proptypes and default props to icon component

--- frontend/src/components/Icon.jsx
+++ frontend/src/components/Icon.jsx
@@ -1,10 +1,26 @@
 import React, { Component, PropTypes } from "react";
-
 import RetinaImage from "react-retina-image";
 
 import { loadIcon } from 'metabase/icon_paths';
 
 export default class Icon extends Component {
+    static propTypes = {
+      name: PropTypes.string.isRequired,
+      width: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number,
+      ]),
+      height: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number,
+      ]),
+    }
+
+    static defaultProps = {
+      width: 16,
+      height: 16
+    }
+
     render() {
         var icon = loadIcon(this.props.name);
         if (icon.img) {
