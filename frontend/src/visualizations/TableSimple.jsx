583bd3b71dbda70c6ef6c0ad5749ef723c7fcf6d
Tom Robinson <tlrobinson@gmail.com>
2016-03-08 01:15:43 -0800

    Scalable Table

--- frontend/src/visualizations/TableSimple.jsx
+++ frontend/src/visualizations/TableSimple.jsx
@@ -1,8 +1,26 @@
 import React, { Component, PropTypes } from "react";
+import styles from "./Table.css";
+
+import Ellipsified from "metabase/components/Ellipsified.jsx";
 
 import { formatValue } from "metabase/lib/formatting";
+import { getFriendlyName } from "metabase/visualizations/lib/utils";
+
+import cx from "classnames";
+import _ from "underscore";
+
+const ROWS_PER_GRID_CELL = 1.2;
 
 export default class TableSimple extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            page: 0,
+            sortColumn: null,
+            sortDescending: false
+        }
+    }
 
     static propTypes = {
         data: PropTypes.object.isRequired
@@ -12,52 +30,70 @@ export default class TableSimple extends Component {
         className: ""
     };
 
-    /* returns custom title for column set by user, or native label if
-     * user did not specify a title
-     *
-     * @param index of the column in the card's dataset, as returned by dataset.data.columns
-     */
-     getDatasetColumnTitleByIndex(index) {
-        var { card, data } = this.props;
-        if (typeof card.visualization_settings !== "undefined" &&
-            typeof card.visualization_settings.columns !== "undefined" &&
-            typeof card.visualization_settings.columns.dataset_column_titles[index] !== "undefined") {
-            return card.visualization_settings.columns.dataset_column_titles[index];
+    setSort(colIndex) {
+        if (this.state.sortColumn === colIndex) {
+            this.setState({ sortDescending: !this.state.sortDescending });
         } else {
-            if (typeof data.cols !== "undefined") {
-                return data.cols[index].name;
-            } else if (typeof data.columns !== "undefined") {
-                return data.columns[index];
-            }
+            this.setState({ sortColumn: colIndex });
         }
     }
 
     render() {
-        var { rows, cols } = this.props.data;
+        const { gridSize, series } = this.props;
+        const { page, sortColumn, sortDescending } = this.state;
+
+        let pageSize = gridSize ? Math.round((gridSize.height - 1) * ROWS_PER_GRID_CELL) : 10;
+        let start = pageSize * page;
+        let end = pageSize * (page + 1) - 1;
+
+        let { rows, cols } = series[0].data;
+        if (sortColumn != null) {
+            rows = _.sortBy(rows, (row) => row[sortColumn]);
+            if (sortDescending) {
+                rows.reverse();
+            }
+        }
+
         return (
-            <div className={"Card--table scroll-x scroll-y " + this.props.className}>
-                <table className="Table border-top">
-                    <thead>
-                        <tr>
-                            {cols.map((column, colIndex) =>
-                                <th key={colIndex}>
-                                    { this.getDatasetColumnTitleByIndex(colIndex) }
-                                </th>
+            <div className={cx(this.props.className, "relative flex flex-column")}>
+                <div className="flex-full relative border-bottom">
+                    <div className="absolute top bottom left right scroll-x scroll-show scroll-show--horizontal" style={{ overflowY: "hidden" }}>
+                        <table className={cx(styles.Table, styles.TableSimple)} style={{height: "100%"}}>
+                            <thead>
+                                <tr>
+                                    {cols.map((col, colIndex) =>
+                                        <th key={colIndex} className={cx("px1 pb1 border-bottom text-brand-hover", { "text-brand": sortColumn === colIndex })} onClick={() => this.setSort(colIndex)}>
+                                            <div className="relative">
+                                                { sortColumn === colIndex &&
+                                                    <span style={{ position: "absolute", right: "100%", marginRight: 3 }}>{sortDescending ? "▾" : "▴"}</span>
+                                                }
+                                                <Ellipsified>{getFriendlyName(col)}</Ellipsified>
+                                            </div>
+                                        </th>
+                                    )}
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {rows.slice(start, end + 1).map((row, rowIndex) =>
+                                <tr key={rowIndex}>
+                                    {row.map((cell, colIndex) =>
+                                        <td key={colIndex} style={{ whiteSpace: "nowrap" }} className="px1 border-bottom">
+                                            { cell == null ? "-" : formatValue(cell, cols[colIndex], { jsx: true }) }
+                                        </td>
+                                    )}
+                                </tr>
                             )}
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {rows.slice(0, 100).map((row, rowIndex) =>
-                        <tr key={rowIndex}>
-                            {row.map((cell, colIndex) =>
-                                <td key={colIndex}>
-                                    { formatValue(cell, cols[colIndex], { jsx: true }) }
-                                </td>
-                            )}
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                { pageSize < rows.length ?
+                    <div className="p1 flex flex-no-shrink flex-align-right">
+                        <span className="text-bold">Rows {start + 1}-{end + 1} of {rows.length}</span>
+                        <span className={cx("text-brand-hover px1", { disabled: page === 0 })} onClick={() => this.setState({ page: page - 1 })}>◀</span>
+                        <span className={cx("text-brand-hover pr1", { disabled: page >= rows.length / pageSize })} onClick={() => this.setState({ page: page + 1 })}>▶</span>
+                    </div>
+                : null }
             </div>
         );
     }
