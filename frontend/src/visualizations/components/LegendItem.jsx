04322c0fcb084005c1940161fe37b83b977123ba
Tom Robinson <tlrobinson@gmail.com>
2016-03-07 23:25:44 -0800

    Make legend hide items if not enough room

--- frontend/src/visualizations/components/LegendItem.jsx
+++ frontend/src/visualizations/components/LegendItem.jsx
@@ -7,16 +7,16 @@ import Ellipsified from "metabase/components/Ellipsified.jsx";
 
 import cx from "classnames";
 
-const LegendItem = ({ title, href, color, showDot = true, showTitle = true, isMuted = false, onMouseEnter, onMouseLeave }) =>
+const LegendItem = ({ title, href, color, showDot = true, showTitle = true, isMuted = false, showTooltip = true, onMouseEnter, onMouseLeave, className }) =>
     <a
         href={href}
-        className={cx(styles.LegendItem, "no-decoration h3 text-bold flex align-center", { mr1: showTitle, muted: isMuted })}
+        className={cx(styles.LegendItem, className, "no-decoration h3 text-bold flex align-center", { mr1: showTitle, muted: isMuted })}
         style={{ overflowX: "hidden", flex: "0 1 auto" }}
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
     >
         { showDot &&
-            <Tooltip tooltip={title}>
+            <Tooltip tooltip={title} isEnabled={showTooltip}>
                 <div
                     className={cx("flex-no-shrink", "inline-block circular")}
                     style={{width: 13, height: 13, margin: 4, marginRight: 8, backgroundColor: color }}
@@ -24,7 +24,7 @@ const LegendItem = ({ title, href, color, showDot = true, showTitle = true, isMu
             </Tooltip>
         }
         { showTitle &&
-            <Ellipsified>{title}</Ellipsified>
+            <Ellipsified showTooltip={showTooltip}>{title}</Ellipsified>
         }
     </a>
 
