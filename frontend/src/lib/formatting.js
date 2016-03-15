29add0731d7952480566601e3ab53ae8a2fa53d0
Tom Robinson <tlrobinson@gmail.com>
2016-03-14 17:20:34 -0700

    Fix SQL timestamp parsing and retain timezone

--- frontend/src/lib/formatting.js
+++ frontend/src/lib/formatting.js
@@ -86,8 +86,8 @@ export function formatValue(value, column, options = {}) {
         return null
     } else if (column && column.unit != null) {
         return formatTimeWithUnit(value, column.unit, options);
-    } else if (moment.isDate(value) || moment(value, ["YYYY-MM-DD'T'HH:mm:ss.SSSZ"], true).isValid()) {
-        return moment(value).format("LLLL");
+    } else if (moment.isDate(value) || moment.isMoment(value)  || moment(value, ["YYYY-MM-DD'T'HH:mm:ss.SSSZ"]).isValid()) {
+        return moment.parseZone(value).format("LLLL");
     } else if (typeof value === "string") {
         return value;
     } else if (typeof value === "number") {
