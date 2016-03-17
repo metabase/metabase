19192e58750aa1aead728322035c6df5b809f4a5
Tom Robinson <tlrobinson@gmail.com>
2016-03-16 17:18:58 -0700

    Swap sun/moon in night mode icon

--- frontend/src/components/icons/NightModeIcon.jsx
+++ frontend/src/components/icons/NightModeIcon.jsx
@@ -3,6 +3,6 @@ import React from "react";
 import Icon from "metabase/components/Icon.jsx";
 
 const NightModeIcon = (props) =>
-    <Icon name={props.isNightMode ? "moon" : "sun"} {...props} />
+    <Icon name={props.isNightMode ? "sun" : "moon"} {...props} />
 
 export default NightModeIcon;
