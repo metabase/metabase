5a11d48a23cfdc3f06ca682b120440cfea44b54c
Tom Robinson <tlrobinson@gmail.com>
2016-03-17 12:44:38 -0700

    Fix 'Cannot read property 'height' of undefined'

--- frontend/src/lib/dashboard_grid.js
+++ frontend/src/lib/dashboard_grid.js
@@ -3,7 +3,7 @@ export const GRID_WIDTH = 18;
 export const GRID_ASPECT_RATIO = 4 / 3;
 export const GRID_MARGIN = 6;
 
-export const DEFAULT_SIZE = { width: 4, height: 4 };
+export const DEFAULT_CARD_SIZE = { width: 4, height: 4 };
 
 // returns the first available position from left to right, top to bottom,
 // based on the existing cards,  item size, and grid width
