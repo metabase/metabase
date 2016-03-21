b41667b798ea74603473127b261f63ab0b56397e
Cam Saül <cammsaul@gmail.com>
2016-03-21 14:40:22 -0700

    Basic autocomplete

--- frontend/src/query_builder/ExpressionWidget.jsx
+++ frontend/src/query_builder/ExpressionWidget.jsx
@@ -3,18 +3,31 @@ import React, { Component, PropTypes } from 'react';
 import _ from 'underscore';
 
 import Icon from "metabase/components/Icon.jsx";
+import Popover from "metabase/components/Popover.jsx";
 
 
 const VALID_OPERATORS = new Set(['+', '-', '*', '/']);
 
+const OPERATOR_SUGGESTIONS = [
+    {name: '+'},
+    {name: '-'},
+    {name: '*'},
+    {name: '/'}
+];
+
 const ERROR_MESSAGE_EMPTY_NAME       = 'what should this expression be called?';
 const ERROR_MESSAGE_EMPTY_EXPRESSION = 'enter an expression';
+
+const KEYCODE_ENTER = 13;
+const KEYCODE_UP    = 38;
+const KEYCODE_DOWN  = 40;
 
 
 export default class ExpressionWidget extends Component {
     constructor(props, context) {
         super(props, context);
-        _.bindAll(this, 'parseToken', 'parseExpression', 'tokenizeExpressionString', 'updateName', 'updateExpression', 'removeExpression');
+        _.bindAll(this, 'parseToken', 'parseExpression', 'tokenizeExpressionString', 'updateName', 'updateExpression', 'removeExpression', 'updateSuggestions', 'onExpressionInputKeyDown',
+                        'onExpressionInputBlur', 'onExpressionInputFocus');
     }
 
     static propTypes = {
@@ -31,7 +44,6 @@ export default class ExpressionWidget extends Component {
     }
 
     componentWillReceiveProps(newProps) {
-        console.log('newProps:', newProps);
         let parsedExpression = newProps.expression[0],
             expressionString = newProps.expression[1];
         this.setState({
@@ -39,18 +51,78 @@ export default class ExpressionWidget extends Component {
             parsedExpression:       parsedExpression,
             expressionString:       expressionString,
             nameErrorMessage:       newProps.name.length    ? null : ERROR_MESSAGE_EMPTY_NAME,
-            expressionErrorMessage: expressionString.length ? null : ERROR_MESSAGE_EMPTY_EXPRESSION
+            expressionErrorMessage: expressionString.length ? null : ERROR_MESSAGE_EMPTY_EXPRESSION,
+            suggestions:            [],
+            highlightedSuggestion:  0,
+            suggestionsTitle:       'FIELDS'
         });
     }
 
     componentWillUnmount() {
-        console.log('componentWillUnmount() state ->', this.state);
         if (this.state.nameErrorMessage || this.state.expressionErrorMessage) {
             this.props.removeExpression(this.state.name);
         } else {
             this.props.updateName(this.props.name, this.state.name);
             this.props.updateExpression(this.state.name, [this.state.parsedExpression, this.state.expressionString]);
         }
+    }
+
+    onExpressionInputKeyDown(event) {
+        if (!this.state.suggestions.length) return;
+
+        if (event.keyCode === KEYCODE_ENTER) {
+            console.log('expression was -> ', this.state.expressionString);
+
+            let suggestedField = this.state.suggestions[this.state.highlightedSuggestion];
+
+            // strip off the last partial token
+            let expression = this.state.expressionString.replace(new RegExp('\\s*[^\\s]+$'), '');
+
+            // tack on the new token
+            expression = expression.length ? (expression + ' ' + suggestedField.name) : suggestedField.name;
+
+            console.log('expression becomes -> ', expression);
+
+            this.updateExpression(expression + ' '); // add a blank space after end of token
+
+        } else if (event.keyCode === KEYCODE_UP) {
+            this.setState({
+                highlightedSuggestion: this.state.highlightedSuggestion === 0 ? (this.state.suggestions.length - 1) : (this.state.highlightedSuggestion - 1)
+            });
+        } else if (event.keyCode === KEYCODE_DOWN) {
+            this.setState({
+                highlightedSuggestion: this.state.highlightedSuggestion === (this.state.suggestions.length - 1) ? 0 : (this.state.highlightedSuggestion + 1)
+            });
+        }
+    }
+
+    onExpressionInputBlur() {
+        this.setState({
+            suggestions: [],
+            highlightedSuggestion: 0
+        });
+    }
+
+    onExpressionInputFocus() {
+        this.updateExpression(this.state.expressionString); // trigger update of state related to expression so autocomplete will pop up, etc.
+    }
+
+    /// update suggestions with ones for fieldName
+    updateSuggestions(fieldName) {
+        let suggestions = _.filter(this.props.tableMetadata.fields, function(field) {
+            return field.name.indexOf(fieldName) > -1 && field.name !== fieldName; // don't return exact matches
+        });
+        suggestions = _.sortBy(suggestions, 'name');
+
+        var highlightedSuggestion = this.state.highlightedSuggestion;
+        if (highlightedSuggestion >= suggestions.length) highlightedSuggestion = suggestions.length - 1;
+        if (highlightedSuggestion < 0)                   highlightedSuggestion = 0;
+
+        this.setState({
+            suggestions:           suggestions,
+            highlightedSuggestion: highlightedSuggestion,
+            suggestionsTitle:      'FIELDS'
+        });
     }
 
     parseToken(token) {
@@ -62,6 +134,8 @@ export default class ExpressionWidget extends Component {
         if (!isNaN(numericValue)) return numericValue;
 
         // if not, it is a field name
+        this.updateSuggestions(token);
+
         let fields = this.props.tableMetadata.fields;
         let field = _.findWhere(fields, {name: token});
         if (!field) throw 'no field named "' + token + '"';
@@ -74,13 +148,28 @@ export default class ExpressionWidget extends Component {
 
         let [lhs, operator, rhs] = tokens;
 
-        if (!lhs) throw 'expression is empty';
+        if (!lhs) {
+            this.updateSuggestions(''); // show suggestions for all fields
+            throw 'expression is empty';
+        }
+
         lhs = this.parseToken(lhs);
 
-        if (!operator) throw 'missing operator';
+        if (!operator) {
+            this.setState({
+                suggestions:           OPERATOR_SUGGESTIONS,
+                highlightedSuggestion: 0,
+                suggestionsTitle:      'OPERATORS'
+            });
+            throw 'missing operator';
+        }
+
         if (!VALID_OPERATORS.has(operator)) throw 'invalid operator: ' + operator;
 
-        if (!rhs) throw 'add something to the right of ' + operator;
+        if (!rhs) {
+            this.updateSuggestions(''); // show suggestions for all fields
+            throw 'add something to the right of ' + operator;
+        }
         // if we have more than one remaining arg recur to parse a nested expression
         rhs = tokens.length > 3 ? this.parseExpression(tokens.slice(2)) : this.parseToken(rhs);
 
@@ -94,7 +183,6 @@ export default class ExpressionWidget extends Component {
 
         for (; i < expressionString.length; i++) {
             let c = expressionString.charAt(i);
-            console.log('c:', c, ', i:', i, ', level:', level, ', tokens:', tokens, ', currentToken:', currentToken,);
 
             if (c === ' ' || c === '\n') {
                 if (currentToken) {
@@ -126,8 +214,6 @@ export default class ExpressionWidget extends Component {
     }
 
     updateName(newName) {
-        console.log('updateName:', newName);
-
         this.setState({
             nameErrorMessage: newName.length ? null : ERROR_MESSAGE_EMPTY_NAME,
             name: newName
@@ -135,18 +221,13 @@ export default class ExpressionWidget extends Component {
     }
 
     updateExpression(expressionString) {
-        console.log('updateExpression:', expressionString);
-
         var errorMessage = null,
             parsedExpression = null;
 
         try {
             let tokens = this.tokenizeExpressionString(expressionString);
             parsedExpression = this.parseExpression(tokens);
-            console.log('tokens -> ', tokens);
-            console.log('parsedExpression:', parsedExpression);
         } catch (e) {
-            console.log('error:', e);
             errorMessage = e;
         }
 
@@ -167,6 +248,28 @@ export default class ExpressionWidget extends Component {
     render() {
         let errorMessage = this.state.nameErrorMessage || this.state.expressionErrorMessage;
 
+        console.log('suggestions:', this.state.suggestions, 'highlightedSuggestion:', this.state.highlightedSuggestion);
+        let autocomplete = this.state.suggestions.length ? (
+            <Popover className="p1"
+                     tetherOptions={{
+                         attachment: 'top center',
+                         targetAttachment: 'bottom center',
+                         targetOffset: '-20 45'
+                     }}
+            >
+                <h6 className="text-grey-1">
+                    {this.state.suggestionsTitle}
+                </h6>
+                <ul className="my1">
+                    {this.state.suggestions.map((suggestion, i) => (
+                         <li className={i == this.state.highlightedSuggestion ? 'text-bold text-brand' : null}>
+                             {suggestion.name}
+                         </li>
+                     ))}
+                </ul>
+            </Popover>
+        ) : null;
+
         return (
             <div className="align-center">
                 <input type="text"
@@ -178,10 +281,14 @@ export default class ExpressionWidget extends Component {
                        onChange={(event) => this.updateExpression(event.target.value)}
                        value={this.state.expressionString}
                        placeholder="expression"
+                       onKeyDown={this.onExpressionInputKeyDown}
+                       onBlur={this.onExpressionInputBlur}
+                       onFocus={this.onExpressionInputFocus}
                 />
                 <a onClick={() => this.removeExpression()}>
                     <Icon name='close' width="12px" height="12px" />
                 </a>
+                {autocomplete}
                 <p className={errorMessage ? 'text-warning' : 'text-green'}>
                     {errorMessage || '✓ expression is valid'}
                 </p>
