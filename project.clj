9a7061f3ca0f7f41ba3ca711d6c1c38c6663eb00
Philipp Bogensberger <philipp@crate.io>
2016-03-29 15:27:22 +0200

    added crate dependencies

--- project.clj
+++ project.clj
@@ -68,7 +68,10 @@
                  [ring/ring-jetty-adapter "1.4.0"]                    ; Ring adapter using Jetty webserver (used to run a Ring server for unit tests)
                  [ring/ring-json "0.4.0"]                             ; Ring middleware for reading/writing JSON automatically
                  [stencil "0.5.0"]                                    ; Mustache templates for Clojure
-                 [swiss-arrows "1.0.0"]]                              ; 'Magic wand' macro -<>, etc.
+                 [swiss-arrows "1.0.0"]                               ; 'Magic wand' macro -<>, etc.
+                 [io.crate/crate-jdbc "1.11.0"]
+                 [io.crate/crate-client "0.54.7"]]
+  :repositories [["bintray" "https://dl.bintray.com/crate/crate"]]
   :plugins [[lein-environ "1.0.2"]                                    ; easy access to environment variables
             [lein-ring "0.9.7"                                        ; start the HTTP server with 'lein ring server'
              :exclusions [org.clojure/clojure]]]                      ; TODO - should this be a dev dependency ?
