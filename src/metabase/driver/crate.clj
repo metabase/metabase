3eb8a8e9c3d630ca9a0c625fef484a887090c415
Michael Beer <michael.beer@crate.io>
2016-03-29 17:24:19 +0200

    added verification for Crate connection setup

--- src/metabase/driver/crate.clj
+++ src/metabase/driver/crate.clj
@@ -1,7 +1,7 @@
 (ns metabase.driver.crate
   (:require [metabase.driver :as driver]
             [metabase.driver.generic-sql :as sql]
-            [clojure.string :as s])
+            (korma [core :as k]))
   (:import (clojure.lang Named)))
 
 (defrecord CrateDriver []
@@ -21,8 +21,15 @@
 (defn- connection-details->spec [_ details]
   (-> details crate-spec))
 
+(defn- can-connect [driver details]
+  (let [connection (connection-details->spec driver details)]
+    (= 1 (-> (k/exec-raw connection "select 1 from sys.cluster" :results)
+             first
+             vals
+             first))))
+
 (def CrateISQLDriverMixin
-  "Implementations of `ISQLDriver` methods for `PostgresDriver`."
+  "Implementations of `ISQLDriver` methods for `CrateDriver`."
   (merge (sql/ISQLDriverDefaultsMixin)
          {:connection-details->spec  connection-details->spec}))
 
@@ -36,7 +43,7 @@
                                         :display-name "Port"
                                         :type         :integer
                                         :default      4300}])
-          })
+          :can-connect?  can-connect})
   sql/ISQLDriver CrateISQLDriverMixin)
 
 (driver/register-driver! :crate (CrateDriver.))
