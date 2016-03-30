cd680623a099a8c9ff0ebd749854ca10a69632fc
Michael Beer <michael.beer@crate.io>
2016-03-30 16:23:40 +0200

    applied custom filter

--- src/metabase/driver/crate.clj
+++ src/metabase/driver/crate.clj
@@ -1,8 +1,43 @@
 (ns metabase.driver.crate
   (:require [metabase.driver :as driver]
             [metabase.driver.generic-sql :as sql]
-            (korma [core :as k]))
+            [metabase.driver.generic-sql.query-processor :as qp]
+            (korma [core :as k])
+            [korma.sql.engine :as kengine]
+            [korma.sql.fns :as kfns])
   (:import (clojure.lang Named)))
+
+;; # Adapt generic SQL to Crate SQL
+
+(defn- filter-subclause->predicate
+  "Given a filter SUBCLAUSE, return a Korma filter predicate form for use in korma `where`."
+  [{:keys [filter-type field value], :as filter}]
+  {:pre [(map? filter) field]}
+  (let [field (qp/formatted field)]
+    {field (case          filter-type
+             :between     ['between [(qp/formatted (:min-val filter)) (qp/formatted (:max-val filter))]]
+             :starts-with ['like (qp/formatted (update value :value (fn [s] (str    s \%)))) ]
+             :contains    ['like (qp/formatted (update value :value (fn [s] (str \% s \%))))]
+             :ends-with   ['like (qp/formatted (update value :value (fn [s] (str \% s))))]
+             :>           ['>    (qp/formatted value)]
+             :<           ['<    (qp/formatted value)]
+             :>=          ['>=   (qp/formatted value)]
+             :<=          ['<=   (qp/formatted value)]
+             :=           ['=    (qp/formatted value)]
+             :!=          ['not= (qp/formatted value)])}))
+
+(defn- filter-clause->predicate [{:keys [compound-type subclause subclauses], :as clause}]
+  (case compound-type
+    :and (apply kfns/pred-and (map filter-clause->predicate subclauses))
+    :or  (apply kfns/pred-or (map filter-clause->predicate subclauses))
+    :not (kfns/pred-not (kengine/pred-map (filter-subclause->predicate subclause)))
+    nil  (filter-subclause->predicate clause)))
+
+(defn- apply-filter
+  "Apply custom generic SQL filter"
+  [_ korma-form {clause :filter}]
+  (k/where korma-form (filter-clause->predicate clause)))
+
 
 (defn- column->base-type
   "Map of Postgres column types -> Field base types.
@@ -50,7 +85,8 @@
   (merge (sql/ISQLDriverDefaultsMixin)
          {:connection-details->spec  connection-details->spec
           :column->base-type         column->base-type
-          :string-length-fn          (constantly :CHAR_LENGTH)}))
+          :string-length-fn          (constantly :CHAR_LENGTH)
+          :apply-filter              apply-filter}))
 
 (extend CrateDriver
   driver/IDriver
