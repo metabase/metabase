5f9a1af31348cfb3fa3a084660b572bc0ec0eeeb
Allen Gilliland <agilliland@gmail.com>
2016-04-03 11:25:50 -0700

    add fk_target_column_id to raw_column table for better tracking of fks and remove previous :fks attribute on raw_table.  move our sync related raw_* functions to a new namespace.

--- src/metabase/models/raw_column.clj
+++ src/metabase/models/raw_column.clj
@@ -1,7 +1,5 @@
 (ns metabase.models.raw-column
-  (:require [clojure.tools.logging :as log]
-            [metabase.db :as db]
-            [metabase.models.interface :as i]
+  (:require [metabase.models.interface :as i]
             [metabase.util :as u]))
 
 
@@ -20,44 +18,3 @@
                     :types              (constantly {:base_type :keyword, :details :json})
                     :timestamped?       (constantly true)
                     :pre-insert         pre-insert}))
-
-
-;;; ## ---------------------------------------- PERSISTENCE FUNCTIONS ----------------------------------------
-
-
-(defn save-all-table-columns
-  "Save *all* `RawColumns` for a given RAW-TABLE."
-  [{:keys [id]} columns]
-  {:pre [(integer? id)
-         (coll? columns)
-         (every? map? columns)]}
-  (let [existing-columns (into {} (for [{:keys [name] :as column} (db/sel :many :fields [RawColumn :id :name] :raw_table_id id)]
-                                    {name column}))]
-
-    ;; deactivate any columns which were removed
-    (doseq [[column-name {column-id :id}] (sort-by :name existing-columns)]
-      (when-not (some #(= column-name (:name %)) columns)
-        (log/debug (u/format-color 'cyan "Marked column %s as inactive." column-name))
-        (db/upd RawColumn column-id :active false)))
-
-    ;; insert or update the remaining columns
-    (doseq [{column-name :name, :keys [base-type pk? special-type details]} (sort-by :name columns)]
-      (let [details (merge (or details {})
-                           (when special-type {:special-type special-type}))
-            is_pk   (true? pk?)]
-        (if-let [{column-id :id} (get existing-columns column-name)]
-          ;; column already exists, update it
-          (db/upd RawColumn column-id
-            :name      column-name
-            :base_type base-type
-            :is_pk     is_pk
-            :details   details
-            :active    true)
-          ;; must be a new column, insert it
-          (db/ins RawColumn
-            :raw_table_id  id
-            :name          column-name
-            :base_type     base-type
-            :is_pk         is_pk
-            :details       details
-            :active        true))))))
--- src/metabase/models/raw_table.clj
+++ src/metabase/models/raw_table.clj
@@ -2,7 +2,7 @@
   (:require [korma.core :as k]
             [metabase.db :as db]
             [metabase.models.interface :as i]
-            [metabase.models.raw-column :refer [RawColumn], :as raw-column]
+            [metabase.models.raw-column :refer [RawColumn]]
             [metabase.util :as u]))
 
 
@@ -17,7 +17,7 @@
 
 (u/strict-extend (class RawTable)
   i/IEntity (merge i/IEntityDefaults
-                   {:types              (constantly {:details :json, :fks :json})
+                   {:types              (constantly {:details :json})
                     :timestamped?       (constantly true)
                     :pre-insert         pre-insert
                     :pre-cascade-delete pre-cascade-delete}))
@@ -31,45 +31,12 @@
   [{:keys [id]}]
   (db/sel :many RawColumn :raw_table_id id, (k/order :name :ASC)))
 
+(defn active-tables
+  "Return the active `RawColumns` belonging to RAW-TABLE."
+  [database-id]
+  (db/sel :many RawTable :database_id database-id, :active true, (k/order :schema :ASC), (k/order :name :ASC)))
 
-(defn create-raw-table
-  "Create a new `RawTable`, includes saving all specified `:columns`."
-  [database-id {table-name :name, table-schema :schema, :keys [details columns fks]}]
-  {:pre [(integer? database-id)
-         (string? table-name)]}
-  (let [table (db/ins RawTable
-                :database_id  database-id
-                :schema       table-schema
-                :name         table-name
-                :details      (or details {})
-                :fks          fks
-                :active       true)]
-    ;; save columns
-    (raw-column/save-all-table-columns table columns)))
-
-(defn update-raw-table
-  "Update an existing `RawTable`, includes saving all specified `:columns`."
-  [{table-id :id, :as table} {table-name :name, table-schema :schema, :keys [details columns fks]}]
-  (db/upd RawTable table-id
-    :schema   table-schema
-    :name     table-name
-    :details  (or details {})
-    :fks      fks
-    :active   true)
-  ;; save columns
-  (raw-column/save-all-table-columns table columns))
-
-(defn disable-raw-tables
-  "Disable a list of `RawTable` ids, including all `RawColumns` associated with those tables."
-  [table-ids]
-  {:pre [(coll? table-ids)
-         (every? integer? table-ids)]}
-  (let [table-ids (filter identity table-ids)]
-    ;; disable the tables
-    (k/update RawTable
-      (k/where {:id [in table-ids]})
-      (k/set-fields {:active false}))
-    ;; whenever a table is disabled we need to disable all of its fields too
-    (k/update RawColumn
-      (k/where {:raw_table_id [in table-ids]})
-      (k/set-fields {:active false}))))
+(defn active-columns
+  "Return the active `RawColumns` belonging to RAW-TABLE."
+  [{:keys [id]}]
+  (db/sel :many RawColumn :raw_table_id id, :active true, (k/order :name :ASC)))
