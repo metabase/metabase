5a62a88f3ed83a4b2eaa707ec71d9e438464b805
Allen Gilliland <agilliland@gmail.com>
2016-04-20 15:04:08 -0700

    adding some more frontend unit tests.

--- frontend/test/unit/lib/expressions.spec.js
+++ frontend/test/unit/lib/expressions.spec.js
@@ -1,16 +1,14 @@
 /*eslint-env jasmine */
 
+import _ from "underscore";
 import { parseExpressionString, tokenAtPosition, tokensToExpression } from "metabase/lib/expressions";
 
 const mockFields = [
     {id: 1, display_name: "A"},
-    {id: 2, display_name: "B"}
+    {id: 2, display_name: "B"},
+    {id: 3, display_name: "C"},
+    {id: 10, display_name: "Toucan Sam"}
 ];
-
-const parsedMockFields = {
-    "A": { value: 'A', start: 0, end: 1, suggestions: [  ], parsedValue: [ 'field-id', 1 ] },
-    "B": { value: 'B', start: 4, end: 5, suggestions: [  ], parsedValue: [ 'field-id', 2 ] }
-}
 
 const mathOperators = new Set(['+', '-', '*', '/']);
 
@@ -21,6 +19,20 @@ const parsedMathOperators = {
     "/": { value: '/', start: 2, end: 3, parsedValue: '/' }
 }
 
+function stripStartEnd(list) {
+    return list.map(i => {
+        delete i.start;
+        delete i.end;
+
+        if (_.isArray(i.value)) {
+            i.value = stripStartEnd(i.value);
+        }
+
+        return i;
+    });
+}
+
+
 describe("parseExpressionString", () => {
     it("should return empty array for null or empty string", () => {
         expect(parseExpressionString()).toEqual([]);
@@ -28,17 +40,40 @@ describe("parseExpressionString", () => {
         expect(parseExpressionString("")).toEqual([]);
     });
 
+    // simplest possible expression
     it("can parse single operator math", () => {
-        expect(parseExpressionString("A - B", mockFields, mathOperators)).toEqual([parsedMockFields["A"], parsedMathOperators["-"], parsedMockFields["B"]]);
-        expect(parseExpressionString("A + B", mockFields, mathOperators)).toEqual([parsedMockFields["A"], parsedMathOperators["+"], parsedMockFields["B"]]);
-        expect(parseExpressionString("A * B", mockFields, mathOperators)).toEqual([parsedMockFields["A"], parsedMathOperators["*"], parsedMockFields["B"]]);
-        expect(parseExpressionString("A / B", mockFields, mathOperators)).toEqual([parsedMockFields["A"], parsedMathOperators["/"], parsedMockFields["B"]]);
+        expect(stripStartEnd(parseExpressionString("A - B", mockFields, mathOperators)))
+        .toEqual([
+            { value: 'A', suggestions: [mockFields[0], mockFields[3]], parsedValue: [ 'field-id', 1 ] },
+            { value: '-', parsedValue: '-' },
+            { value: 'B', suggestions: [], parsedValue: [ 'field-id', 2 ] }
+        ]);
     });
 
-    // field names with spaces
+    // quoted field name w/ a space in it
+    it("can parse a field with quotes and spaces", () => {
+        expect(stripStartEnd(parseExpressionString("\"Toucan Sam\" + B", mockFields, mathOperators)))
+        .toEqual([
+            { value: 'Toucan Sam', suggestions: [], parsedValue: [ 'field-id', 10 ] },
+            { value: '+', parsedValue: '+' },
+            { value: 'B', suggestions: [], parsedValue: [ 'field-id', 2 ] }
+        ]);
+    });
+
+    // parentheses / nested parens
+    it("can parse expressions with parentheses", () => {
+        expect(stripStartEnd(parseExpressionString("\"Toucan Sam\" + (A * (B / C))", mockFields, mathOperators)))
+        .toEqual([
+            { value: 'Toucan Sam', suggestions: [], parsedValue: [ 'field-id', 10 ] },
+            { value: '+', parsedValue: '+' },
+            { value: [{ value: 'A', suggestions: [mockFields[0], mockFields[3]], parsedValue: [ 'field-id', 1 ] },
+                      { value: '+', parsedValue: '*' },
+                      { value: [{ value: 'B', suggestions: [], parsedValue: [ 'field-id', 2 ] },
+                                { value: '+', parsedValue: '/' },
+                                { value: 'C', suggestions: [mockFields[2], mockFields[3]], parsedValue: [ 'field-id', 3 ] }], isParent: true}], isParent: true }
+        ]);
+    });
+
     // fks
-    // nested fields
     // multiple tables with the same field name resolution
-    // parentheses
-    // nested parens
 });
