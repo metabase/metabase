6aa0b0d75528f15b31f8f71f1cab2b02e6a66d8d
Tom Robinson <tlrobinson@gmail.com>
2016-04-22 11:33:57 -0700

    Disable redux logging unless in 'debug' mode

---
+++ frontend/src/lib/debug.js
@@ -0,0 +1,11 @@
+let debug;
+if (typeof window === "object" && (
+    (window.location && window.location.hash === "#debug") ||
+    (window.localStorage && window.localStorage.getItem("debug"))
+)) {
+    debug = true;
+} else {
+    debug = false;
+}
+
+export const DEBUG = debug;
