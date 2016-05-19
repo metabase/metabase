6b077de9e01311c293e75cf6031b2748e85c9e05
Tom Robinson <tlrobinson@gmail.com>
2016-05-19 16:33:06 -0700

    Add some unit tests

--- frontend/src/metabase/meta/Query.js
+++ frontend/src/metabase/meta/Query.js
@@ -6,26 +6,6 @@ export function hasValidAggregation(query: StructuredQueryObject): bool {
     return false; // FIXME
 }
 
-export function getFilters(query: StructuredQueryObject): Array<FilterClause> {
-    if (query.filters == null || query.filters.length === 0) {
-        return [];
-    } else if (query.filters[0] === "and") {
-        return query.filters.slice(1);
-    } else {
-        return [query.filters];
-    }
-}
-
-export function setFilters(query: StructuredQueryObject, filters: Array<FilterClause>): StructuredQueryObject {
-    if (filters.length === 0) {
-        return { ...query, filters: undefined };
-    } else if (filters.length === 1) {
-        return { ...query, filters: filters[0] };
-    } else {
-        return { ...query, filters: ["and"].concat(filters) };
-    }
-}
-
 export function addFilter(query: StructuredQueryObject, newFilter: FilterClause): StructuredQueryObject {
     const filters = getFilters(query);
     return setFilters(query, [...filters.slice(), newFilter]); // TODO: figure out why flow complains without .slice()
@@ -39,4 +19,26 @@ export function updateFilter(query: StructuredQueryObject, index: number, update
 export function removeFilter(query: StructuredQueryObject, index: number): StructuredQueryObject {
     const filters = getFilters(query);
     return setFilters(query, [...filters.slice(0, index), ...filters.slice(index + 1)]);
+}
+
+function getFilters(query: StructuredQueryObject): Array<FilterClause> {
+    if (query.filters == null || query.filters.length === 0) {
+        return [];
+    } else if (query.filters[0] === "and") {
+        return query.filters.slice(1);
+    } else {
+        return [query.filters];
+    }
+}
+
+function setFilters(query: StructuredQueryObject, filters: Array<FilterClause>): StructuredQueryObject {
+    if (filters.length === 0) {
+        query = { ...query };
+        delete query.filters;
+        return query;
+    } else if (filters.length === 1) {
+        return { ...query, filters: filters[0] };
+    } else {
+        return { ...query, filters: ["and"].concat(filters) };
+    }
 }
