9ae86bcd9c0192c225d9b3473a3b8bf5a9b91c58
Tom Robinson <tlrobinson@gmail.com>
2016-05-18 17:55:32 -0700

    Flow types for query

---
+++ frontend/src/metabase/meta/QueryTypes.js
@@ -0,0 +1,115 @@
+/* @flow */
+
+export type DatabaseId = number;
+export type TableId = number;
+export type FieldId = number;
+export type SegmentId = number;
+export type MetricId = number;
+
+export type StringLiteralType = string;
+export type NumericLiteralType = number;
+export type DatetimeLiteralType = string;
+
+export type ValueType = null | boolean | StringLiteralType | NumericLiteralType | DatetimeLiteralType;
+export type OrderableValueType = NumericLiteralType | DatetimeLiteralType;
+
+export type RelativeDatetimePeriodType = "current" | "last" | "next" | number;
+export type RelativeDatetimeUnitType = "minute" | "hour" | "day" | "week" | "month" | "quarter" | "year";
+export type DatetimeUnitType = "default" | "minute" | "minute-of-hour" | "hour" | "hour-of-day" | "day" | "day-of-week" | "day-of-month" | "day-of-year" | "week" | "week-of-year" | "month" | "month-of-year" | "quarter" | "quarter-of-year" | "year";
+
+export type DatasetQueryType =
+    { database: DatabaseId, type: "query",  query: StructuredQueryType } |
+    { database: DatabaseId, type: "native", query: NativeQueryType };
+
+export type NativeQueryType = {
+    query: string
+};
+
+export type StructuredQueryType = {
+    source_table: TableId,
+    aggregation?: AggregationClauseType,
+    breakout?:    BreakoutClauseType,
+    filter?:      FilterClauseType,
+    order_by?:    OrderByClauseType,
+    limit?:       number,
+    expressions?: { [key: string]: ExpressionType }
+};
+
+export type AggregationClauseType =
+    ["rows"] | // deprecated
+    ["avg", ConcreteFieldType] |
+    ["count"] |
+    ["count", ConcreteFieldType] |
+    ["cum_sum", ConcreteFieldType] |
+    ["distinct", ConcreteFieldType] |
+    ["stddev", ConcreteFieldType] |
+    ["sum", ConcreteFieldType] |
+    ["min", ConcreteFieldType] |
+    ["max", ConcreteFieldType] |
+    ["metric", MetricId];
+
+export type BreakoutClauseType = Array<ConcreteFieldType>;
+export type FilterClauseType =
+    ["and",                 FilterClauseType] |
+    ["or",                  FilterClauseType] |
+    ["not",                 FilterClauseType] |
+    ["=",                   ConcreteFieldType, ValueType] |
+    ["!=",                  ConcreteFieldType, ValueType] |
+    ["<",                   ConcreteFieldType, OrderableValueType] |
+    [">",                   ConcreteFieldType, OrderableValueType] |
+    ["<=",                  ConcreteFieldType, OrderableValueType] |
+    [">=",                  ConcreteFieldType, OrderableValueType] |
+    ["is-null",             ConcreteFieldType] |
+    ["not-null",            ConcreteFieldType] |
+    ["between",             ConcreteFieldType, OrderableValueType, OrderableValueType] |
+    ["inside",              ConcreteFieldType, ConcreteFieldType, NumericLiteralType, NumericLiteralType, NumericLiteralType, NumericLiteralType] |
+    ["starts-with",         ConcreteFieldType, StringLiteralType] |
+    ["contains",            ConcreteFieldType, StringLiteralType] |
+    ["does-not-contain",    ConcreteFieldType, StringLiteralType] |
+    ["ends-with",           ConcreteFieldType, StringLiteralType] |
+    ["time-interval",       ConcreteFieldType, RelativeDatetimePeriodType, RelativeDatetimeUnitType] |
+    ["segment",             SegmentId];
+
+export type OrderByClauseType = Array<OrderByType>;
+export type OrderByType = ["asc" | "desc", FieldType];
+
+export type FieldType =
+    ConcreteFieldType |
+    ExpressionReferenceType |
+    AggregateFieldType;
+
+export type ConcreteFieldType =
+    LocalFieldReferenceType |
+    ForeignFieldReferenceType |
+    DatetimeFieldType;
+
+export type LocalFieldReferenceType =
+    ["field-id", FieldId] |
+    FieldId; // deprecated
+
+export type ForeignFieldReferenceType =
+    ["fk->", FieldId, FieldId];
+
+export type DatetimeFieldType =
+    ["datetime-field", LocalFieldReferenceType | ForeignFieldReferenceType, DatetimeUnitType] |
+    ["datetime-field", LocalFieldReferenceType | ForeignFieldReferenceType, "as", DatetimeUnitType]; // deprecated
+
+export type ExpressionReferenceType = ["expression", string];
+
+export type AggregateFieldType = ["aggregation", number];
+
+export type ExpressionBinaryOperatorType = "+" | "-" | "*" | "/";
+export type ExpressionArgumentType = ConcreteFieldType | NumericLiteralType;
+
+export type ExpressionType =
+    [ExpressionBinaryOperatorType, ExpressionArgumentType, ExpressionArgumentType];
+
+export const DatasetQuery = {
+    isStructured(dataset_query : DatasetQueryType) : boolean {
+        return dataset_query && dataset_query.type === "query";
+    },
+
+    isNative(dataset_query : DatasetQueryType) : boolean {
+        return dataset_query && dataset_query.type === "native";
+    },
+};
--- frontend/src/metabase/meta/demo.js
+++ frontend/src/metabase/meta/demo.js
@@ -1,32 +1,52 @@
-/* @flow weak */
+/* @flow */
 
 import Metadata from "./Metadata";
 import Database from "./Database";
 
-async function getDatabases() {
-    let response = await fetch("/api/database?include_tables=true", { credentials: 'same-origin' });
-    return await response.json();
-}
+// async function getDatabases() {
+//     let response = await fetch("/api/database?include_tables=true", { credentials: 'same-origin' });
+//     return await response.json();
+// }
+//
+// async function getTable(table) {
+//     let response = await fetch("/api/table/" + table.id + "/query_metadata", { credentials: 'same-origin' });
+//     return await response.json();
+// }
+//
+// async function loadDatabaseTables(database) {
+//     database.tables = await Promise.all(database.tables.map(getTable));
+// }
+//
+// async function loadMetadata() {
+//     let databases = await getDatabases();
+//     await Promise.all(databases.map(loadDatabaseTables));
+//     return databases;
+// }
+//
+// loadMetadata().then((databases) => {
+//     window.m = new Metadata(databases);
+//     window.d = new Database(databases[0]);
+//     console.log(window.m.databases());
+//     console.log(window.m.databases()[1].tables()[0].field(1835).target().table().database().tables()[0].fields()[0].isNumeric());
+//     console.log(window.d.tables());
+// }).then(null, (err) => console.error(err))
 
-async function getTable(table) {
-    let response = await fetch("/api/table/" + table.id + "/query_metadata", { credentials: 'same-origin' });
-    return await response.json();
-}
 
-async function loadDatabaseTables(database) {
-    database.tables = await Promise.all(database.tables.map(getTable));
-}
+import type { DatasetQueryType } from "./QueryTypes";
 
-async function loadMetadata() {
-    let databases = await getDatabases();
-    await Promise.all(databases.map(loadDatabaseTables));
-    return databases;
-}
+export const DatasetQuery = {
+    isStructured(dataset_query : DatasetQueryType) : boolean {
+        return dataset_query && dataset_query.type === "query";
+    },
 
-loadMetadata().then((databases) => {
-    window.m = new Metadata(databases);
-    window.d = new Database(databases[0]);
-    console.log(window.m.databases());
-    console.log(window.m.databases()[1].tables()[0].field(1835).target().table().database().tables()[0].fields()[0].isNumeric());
-    console.log(window.d.tables());
-}).then(null, (err) => console.error(err))
+    isNative(dataset_query : DatasetQueryType) : boolean {
+        return dataset_query && dataset_query.type === "native";
+    },
+};
+
+const dataset_query : DatasetQueryType = {"database":10,"type":"query","query":{"source_table":89,"aggregation":["metric",6],"breakout":[],"filter":["and",["segment",7]]}};
+
+console.log(DatasetQuery.isStructured(dataset_query));
+console.log(DatasetQuery.isNative(dataset_query));
+
+// console.log(DatasetQuery.isNative({})); // FAILS TYPECHECKING!
