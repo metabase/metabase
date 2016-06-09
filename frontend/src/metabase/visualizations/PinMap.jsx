6048e323695e891aa4bc8b1676896d71090fb734
Shahin <ishahinism@gmail.com>
2016-06-09 10:59:32 +0430

    Cleanup

--- frontend/src/metabase/visualizations/PinMap.jsx
+++ frontend/src/metabase/visualizations/PinMap.jsx
@@ -56,37 +56,7 @@ export default class PinMap extends Component {
         this.setState({ zoom });
     }
 
-    getTileUrl(settings, coord, zoom) {
-        let query = this.props.series[0].card.dataset_query;
-
-        let latitude_dataset_col_index = settings.map.latitude_dataset_col_index;
-        let longitude_dataset_col_index = settings.map.longitude_dataset_col_index;
-        let latitude_source_table_field_id = settings.map.latitude_source_table_field_id;
-        let longitude_source_table_field_id = settings.map.longitude_source_table_field_id;
-
-        if (latitude_dataset_col_index == null || longitude_dataset_col_index == null) {
-            return;
-        }
-
-        if (latitude_source_table_field_id == null || longitude_source_table_field_id == null) {
-            throw ("Map ERROR: latitude and longitude column indices must be specified");
-        }
-        if (latitude_dataset_col_index == null || longitude_dataset_col_index == null) {
-            throw ("Map ERROR: unable to find specified latitude / longitude columns in source table");
-        }
-
-        return '/api/tiles/' + zoom + '/' + coord.x + '/' + coord.y + '/' +
-            latitude_source_table_field_id + '/' + longitude_source_table_field_id + '/' +
-            latitude_dataset_col_index + '/' + longitude_dataset_col_index + '/' +
-            '?query=' + encodeURIComponent(JSON.stringify(query))
-    }
-
     componentDidMount() {
-        if (typeof google === undefined) {
-            setTimeout(() => this.componentDidMount(), 500);
-            return;
-        }
-
         try {
             let element = ReactDOM.findDOMNode(this.refs.map);
 
@@ -97,38 +67,6 @@ export default class PinMap extends Component {
             settings = getSettingsForVisualization(settings, "pin_map");
             settings = setLatitudeAndLongitude(settings, data.cols);
 
-          /* let mapOptions = {
-           *     zoom: settings.map.zoom,
-           *     center: new google.maps.LatLng(settings.map.center_latitude, settings.map.center_longitude),
-           *     mapTypeId: google.maps.MapTypeId.MAP,
-           *     scrollwheel: false
-           * };
-
-           * let map = this.map = new google.maps.Map(element, mapOptions);*/
-
-          /* if (data.rows.length < 2000) {
-           *     let tooltip = new google.maps.InfoWindow();
-           *     let latColIndex = settings.map.latitude_dataset_col_index;
-           *     let lonColIndex = settings.map.longitude_dataset_col_index;
-           *     for (let row of data.rows) {
-           *         let marker = new google.maps.Marker({
-           *             position: new google.maps.LatLng(row[latColIndex], row[lonColIndex]),
-           *             map: map,
-           *             icon: "/app/img/pin.png"
-           *         });
-           *         marker.addListener("click", () => {
-           *             let tooltipElement = document.createElement("div");
-           *             ReactDOM.render(<ObjectDetailTooltip row={row} cols={data.cols} />, tooltipElement);
-           *             tooltip.setContent(tooltipElement);
-           *             tooltip.open(map, marker);
-           *         });
-           *     }
-           * } else {
-           *     map.overlayMapTypes.push(new google.maps.ImageMapType({
-           *         getTileUrl: this.getTileUrl.bind(this, settings),
-           *         tileSize: new google.maps.Size(256, 256)
-           *     }));
-           * }*/
             L.Icon.Default.imagePath = '/app/img';
             let map = L.map(element).setView([settings.map.center_latitude, settings.map.center_longitude], settings.map.zoom);
 
@@ -156,23 +94,9 @@ export default class PinMap extends Component {
               this.onMapZoomChange(map.getZoom());
             })
 
-          /* map.addListener("center_changed", () => {
-           *     let center = map.getCenter();
-           *     this.onMapCenterChange(center.lat(), center.lng());
-           * });
-
-           * map.addListener("zoom_changed", () => {
-           *     this.onMapZoomChange(map.getZoom());
-           * });*/
         } catch (err) {
             console.error(err);
             this.props.onRenderError(err.message || err);
-        }
-    }
-
-    componentDidUpdate() {
-        if (typeof google !== "undefined") {
-            google.maps.event.trigger(this.map, "resize");
         }
     }
 
