615c4d0df8e0c69ecb9e62f887d361c0c2bf32f5
Tom Robinson <tlrobinson@gmail.com>
2016-06-10 16:26:41 -0700

    Close SingleDate widget when clickign a date

--- frontend/src/metabase/dashboard/components/parameters/widgets/DateSingleWidget.jsx
+++ frontend/src/metabase/dashboard/components/parameters/widgets/DateSingleWidget.jsx
@@ -3,13 +3,13 @@ import React, { Component, PropTypes } from "react";
 import Calendar from "metabase/components/Calendar.jsx";
 import moment from "moment";
 
-const DateSingleWidget = ({ value, setValue }) =>
+const DateSingleWidget = ({ value, setValue, onClose }) =>
     <div className="p1">
         <Calendar
             initial={value ? moment(value) : null}
             selected={value ? moment(value) : null}
             selectedEnd={value ? moment(value) : null}
-            onChange={setValue}
+            onChange={(value) => { setValue(value); onClose() }}
         />
     </div>
 
