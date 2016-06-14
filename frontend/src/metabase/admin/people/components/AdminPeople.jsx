a6883d97e417aebc54945ed4424011c9a547b6fd
Allen Gilliland <agilliland@gmail.com>
2016-06-14 12:05:03 -0700

    go for a slightly lighter touch and still allow the user to modify their own details from the People admin page, except for removing themselves.

--- frontend/src/metabase/admin/people/components/AdminPeople.jsx
+++ frontend/src/metabase/admin/people/components/AdminPeople.jsx
@@ -382,9 +382,7 @@ export default class AdminPeople extends Component {
                                     </td>
                                     <td>{ user.last_login ? user.last_login.fromNow() : "Never" }</td>
                                     <td className="text-right">
-                                        { this.props.user.id !== user.id &&
-                                            <UserActionsSelect user={user} dispatch={this.props.dispatch} />
-                                        }
+                                        <UserActionsSelect user={user} dispatch={this.props.dispatch} isActiveUser={this.props.user.id !== user.id} />
                                     </td>
                                 </tr>
                                 )}
