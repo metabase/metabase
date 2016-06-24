b2595e51863007f66f5849fa1dd29030c86f45f0
Kyle Doherty <kyle.l.doherty@gmail.com>
2016-06-24 13:16:01 -0700

    clean up login app

--- frontend/src/metabase/auth/containers/LoginApp.jsx
+++ frontend/src/metabase/auth/containers/LoginApp.jsx
@@ -80,46 +80,42 @@ export default class LoginApp extends Component {
         const { loginError } = this.props;
 
         return (
-            <div>
-                <div className="full-height bg-white flex flex-column flex-full md-layout-centered">
-                    <div className="wrapper">
-                        <div className="Login-wrapper Grid Grid--full md-Grid--1of2">
-                            <div className="Grid-cell flex layout-centered text-brand">
-                                <LogoIcon className="Logo my4 sm-my0" width={66} height={85} />
+            <div className="full-height full bg-white flex flex-column flex-full md-layout-centered">
+                <div className="Login-wrapper wrapper Grid Grid--full md-Grid--1of2">
+                    <div className="Grid-cell flex layout-centered text-brand">
+                        <LogoIcon className="Logo my4 sm-my0" width={66} height={85} />
+                    </div>
+                    <div className="Login-content Grid-cell">
+                        <form className="Form-new bg-white bordered rounded shadowed" name="form" onSubmit={(e) => this.formSubmitted(e)} noValidate>
+                            <h3 className="Login-header Form-offset mb3">Sign in to Metabase</h3>
+
+                            <FormMessage formError={loginError && loginError.data.message ? loginError : null} ></FormMessage>
+
+                            <FormField key="email" fieldName="email" formError={loginError}>
+                                <FormLabel title={"Email address"}  fieldName={"email"} formError={loginError} />
+                                <input className="Form-input Form-offset full py1" name="email" placeholder="youlooknicetoday@email.com" type="text" onChange={(e) => this.onChange("email", e.target.value)} autoFocus />
+                                <span className="Form-charm"></span>
+                            </FormField>
+
+                            <FormField key="password" fieldName="password" formError={loginError}>
+                                <FormLabel title={"Password"}  fieldName={"password"} formError={loginError} />
+                                <input className="Form-input Form-offset full py1" name="password" placeholder="Shh..." type="password" onChange={(e) => this.onChange("password", e.target.value)} />
+                                <span className="Form-charm"></span>
+                            </FormField>
+
+                            <div className="Form-field">
+                                <ul className="Form-offset">
+                                    <input name="remember" type="checkbox" ng-init="remember_me = true" checked /> <label className="inline-block">Remember Me:</label>
+                                </ul>
                             </div>
-                            <div className="Login-content Grid-cell">
-                                <form className="Form-new bg-white bordered rounded shadowed" name="form" onSubmit={(e) => this.formSubmitted(e)} noValidate>
-                                    <h3 className="Login-header Form-offset mb3">Sign in to Metabase</h3>
 
-                                    <FormMessage formError={loginError && loginError.data.message ? loginError : null} ></FormMessage>
-
-                                    <FormField key="email" fieldName="email" formError={loginError}>
-                                        <FormLabel title={"Email address"}  fieldName={"email"} formError={loginError} />
-                                        <input className="Form-input Form-offset full py1" name="email" placeholder="youlooknicetoday@email.com" type="text" onChange={(e) => this.onChange("email", e.target.value)} autoFocus />
-                                        <span className="Form-charm"></span>
-                                    </FormField>
-
-                                    <FormField key="password" fieldName="password" formError={loginError}>
-                                        <FormLabel title={"Password"}  fieldName={"password"} formError={loginError} />
-                                        <input className="Form-input Form-offset full py1" name="password" placeholder="Shh..." type="password" onChange={(e) => this.onChange("password", e.target.value)} />
-                                        <span className="Form-charm"></span>
-                                    </FormField>
-
-                                    <div className="Form-field">
-                                        <ul className="Form-offset">
-                                            <input name="remember" type="checkbox" ng-init="remember_me = true" checked /> <label className="inline-block">Remember Me:</label>
-                                        </ul>
-                                    </div>
-
-                                    <div className="Form-actions p2 Grid Grid--full md-Grid--1of2">
-                                        <button className={cx("Button Grid-cell", {'Button--primary': this.state.valid})} disabled={!this.state.valid}>
-                                            Sign in
-                                        </button>
-                                        <a className="Grid-cell py2 sm-py0 text-grey-3 md-text-right text-centered flex-full link" href="/auth/forgot_password" onClick={(e) => { window.OSX ? window.OSX.resetPassword() : null }}>I seem to have forgotten my password</a>
-                                    </div>
-                                </form>
+                            <div className="Form-actions p2 Grid Grid--full md-Grid--1of2">
+                                <button className={cx("Button Grid-cell", {'Button--primary': this.state.valid})} disabled={!this.state.valid}>
+                                    Sign in
+                                </button>
+                                <a className="Grid-cell py2 sm-py0 text-grey-3 md-text-right text-centered flex-full link" href="/auth/forgot_password" onClick={(e) => { window.OSX ? window.OSX.resetPassword() : null }}>I seem to have forgotten my password</a>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
                 <AuthScene />
