c35870c008e51f5db58241ee545c2be485c65cbb
Cam Saül <cammsaul@gmail.com>
2016-07-05 19:07:44 -0700

    More test cleanup :100:

--- test/metabase/events/last_login_test.clj
+++ test/metabase/events/last_login_test.clj
@@ -1,27 +1,15 @@
 (ns metabase.events.last-login-test
   (:require [expectations :refer :all]
-            [metabase.db :as db]
-            [metabase.events.last-login :refer :all]
-            (metabase.models [user :refer [User]])
-            [metabase.test.data :refer :all]
-            [metabase.test.util :refer [expect-eval-actual-first random-name]]
-            [metabase.test-setup :refer :all]))
-
-
-(defn- create-test-user []
-  (let [rand-name (random-name)]
-    (db/insert! User
-      :email      (str rand-name "@metabase.com")
-      :first_name rand-name
-      :last_name  rand-name
-      :password   rand-name)))
+            [metabase.events.last-login :refer [process-last-login-event]]
+            [metabase.models.user :refer [User]]
+            [metabase.test.util :as tu]))
 
 
 ;; `:user-login` event
-(expect-let [{user-id :id last-login :last_login} (create-test-user)]
+(expect
   {:orig-last-login nil
    :upd-last-login  false}
-  (do
+  (tu/with-temp User [{user-id :id, last-login :last_login}]
     (process-last-login-event {:topic :user-login
                                :item  {:user_id    user-id
                                        :session_id "doesntmatter"}})
