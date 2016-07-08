d4ce2ecce19e7e2c7b40ee696fbe0f6c82040c99
Lewis Liu <develop@lewisl.net>
2016-07-08 14:25:22 -0700

    Quick fix for CI error

--- frontend/src/metabase/reference/reference.js
+++ frontend/src/metabase/reference/reference.js
@@ -2,6 +2,9 @@ import { createAction, createThunkAction } from 'metabase/lib/redux';
 
 import i from 'icepick';
 
+const initialState = {};
+// Placeholder reducer for gettings started guide state
+// State for everything else is stored in metabase/redux/metadata
 export default (state = initialState, { type, payload, error }) => {
     switch (type) {
         default:
