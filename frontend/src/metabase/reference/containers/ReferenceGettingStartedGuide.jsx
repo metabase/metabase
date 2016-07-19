8ccfa584052fd7f2c67d460718c47e78cafcb08c
Lewis Liu <develop@lewisl.net>
2016-07-19 15:35:37 -0700

    Updated getting started guide placeholder view

--- frontend/src/metabase/reference/containers/ReferenceGettingStartedGuide.jsx
+++ frontend/src/metabase/reference/containers/ReferenceGettingStartedGuide.jsx
@@ -1,11 +1,17 @@
 /* eslint "react/prop-types": "warn" */
 import React, { Component, PropTypes } from "react";
 
+import S from "metabase/reference/Reference.css";
+
 import { pure } from "recompose";
 
 const ReferenceGettingStartedGuide = () =>
-    <div>
-        WIP
+    <div className={S.guideEmpty}>
+        <div className={S.guideEmptyBody}>
+            <img className="mb4" src={`/app/img/lightbulb.png`} height="200px" alt="Lightbulb" srcSet={`/app/img/lightbulb@2x.png 2x`} />
+            <h1 className="text-bold text-dark">Understanding our data</h1>
+            <div className={S.guideEmptyMessage}>This guide lets you explore all the metrics, segments, and raw data that we currently have in Metabase. Select a section on the left to learn more about our data.</div>
+        </div>
     </div>
 
 export default pure(ReferenceGettingStartedGuide);
