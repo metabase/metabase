96e5c9a24a7dfc50f2591b5cb6bb8cc6ad981147
Lewis Liu <develop@lewisl.net>
2016-08-02 22:09:19 +0000

    Added separate empty state component for getting started guide

--- frontend/src/metabase/reference/containers/ReferenceGettingStartedGuide.jsx
+++ frontend/src/metabase/reference/containers/ReferenceGettingStartedGuide.jsx
@@ -1,17 +1,45 @@
 /* eslint "react/prop-types": "warn" */
 import React, { Component, PropTypes } from "react";
+import { connect } from 'react-redux';
 
 import S from "metabase/reference/Reference.css";
 
-import { pure } from "recompose";
+import GuideEmptyState from "metabase/reference/components/GuideEmptyState.jsx"
 
-const ReferenceGettingStartedGuide = () =>
-    <div className={S.guideEmpty}>
-        <div className={S.guideEmptyBody}>
-            <img className="mb4" src={`/app/img/lightbulb.png`} height="200px" alt="Lightbulb" srcSet={`/app/img/lightbulb@2x.png 2x`} />
-            <h1 className="text-bold text-dark">Understanding our data</h1>
-            <div className={S.guideEmptyMessage}>This guide lets you explore all the metrics, segments, and raw data that we currently have in Metabase. Select a section on the left to learn more about our data.</div>
-        </div>
-    </div>
+import * as actions from 'metabase/reference/reference';
 
-export default pure(ReferenceGettingStartedGuide);
+import {
+    getUser,
+    getIsEditing
+} from '../selectors';
+
+const mapStateToProps = (state, props) => ({
+    user: getUser(state),
+    isEditing: getIsEditing(state)
+});
+
+const mapDispatchToProps = {
+    ...actions
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class ReferenceGettingStartedGuide extends Component {
+    static propTypes = {
+        isEditing: PropTypes.bool
+    };
+
+    render() {
+        const {
+            user,
+            isEditing,
+            startEditing
+        } = this.props;
+
+        return (
+            <GuideEmptyState 
+                isSuperuser={user && user.is_superuser}
+                startEditing={startEditing} 
+            />
+        );
+    }
+}
