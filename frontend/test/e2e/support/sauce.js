f1593f15ffd14b14cfa574c727f6731b8db85909
Tom Robinson <tlrobinson@gmail.com>
2016-09-27 02:22:30 -0700

    tweaks

--- frontend/test/e2e/support/sauce.js
+++ frontend/test/e2e/support/sauce.js
@@ -3,13 +3,15 @@ import sauceConnectLauncher from "sauce-connect-launcher";
 export const USE_SAUCE = process.env["USE_SAUCE"];
 const SAUCE_USERNAME = process.env["SAUCE_USERNAME"];
 const SAUCE_ACCESS_KEY = process.env["SAUCE_ACCESS_KEY"];
+const CIRCLE_BUILD_NUM = process.env["CIRCLE_BUILD_NUM"];
 
 export const sauceCapabilities = {
-    'browserName': 'chrome',
-    'platform': 'Mac',
-    'version': '48.0',
-    'username': SAUCE_USERNAME,
-    'accessKey': SAUCE_ACCESS_KEY
+    browserName: 'chrome',
+    platform: 'Mac',
+    version: '48.0',
+    username: SAUCE_USERNAME,
+    accessKey: SAUCE_ACCESS_KEY,
+    build: CIRCLE_BUILD_NUM
 };
 
 export const sauceServer = `http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@localhost:4445/wd/hub`;
