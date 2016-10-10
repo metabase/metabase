3a134dafd23668abc61c6bfec2d93eb09be3e413
Kyle Doherty <kdoh@users.noreply.github.com>
2016-10-10 16:20:15 -0700

    permission tweaks (#3458)
    * change 'raw queries' to 'SQL queries'
    
    * standardize confirm content
    
    * admin edit header
    
    * autosize columns
    
    * Update react-virtualized to version 8 to prevent conflict with .Grid selectors
    
    * shrinkwrap
    
    * cleaner subtitle hiding
    
    * remove unused reference

--- frontend/src/metabase/admin/permissions/components/FixedHeaderGrid.jsx
+++ frontend/src/metabase/admin/permissions/components/FixedHeaderGrid.jsx
@@ -2,7 +2,7 @@
 
 import React, { Component, PropTypes } from "react";
 
-import { Grid, AutoSizer, ScrollSync } from 'react-virtualized'
+import { Grid, ScrollSync } from 'react-virtualized'
 import 'react-virtualized/styles.css';
 import S from "./FixedHeaderGrid.css";
 
@@ -13,93 +13,94 @@ const SCROLLBAR_SIZE = 50;
 
 const FixedHeaderGrid = ({
     className,
-
-    rowsCount,
-    columnsCount,
+    rowCount,
+    columnCount,
     renderCell,
     columnWidth,
     rowHeight,
-
     renderColumnHeader,
     columnHeaderHeight,
-
     rowHeaderWidth,
     renderRowHeader,
-
     renderCorner,
-
+    width,
+    height,
     paddingBottom = 25
 }) =>
     <div className={cx(className, S.fixedHeaderGrid, "relative")}>
-        <AutoSizer>
-        {({ height, width }) =>
-            <ScrollSync>
-                {({ clientHeight, clientWidth, onScroll, scrollHeight, scrollLeft, scrollTop, scrollWidth }) =>
-                    <div>
-                        {/* CORNER */}
-                        <div style={{ position: "absolute", top: 0, left: 0, width: rowHeaderWidth, height: columnHeaderHeight, overflow: "hidden" }}>
-                            {renderCorner()}
-                        </div>
-                        {/* COLUMN HEADERS */}
-                        <div style={{ position: "absolute", top: 0, left: rowHeaderWidth, height: columnHeaderHeight, overflow: "hidden" }}>
-                            <Grid
-                                width={width - rowHeaderWidth}
-                                height={columnHeaderHeight + SCROLLBAR_SIZE}
-                                renderCell={(props) =>
-                                    // HACK: offsets the additional height needed to hide the scrollbars
-                                    <div style={{ height: columnHeaderHeight, position: "relative" }}>{renderColumnHeader(props)}</div>
-                                }
-                                columnsCount={columnsCount}
-                                rowsCount={1}
-                                columnWidth={columnWidth}
-                                rowHeight={columnHeaderHeight + SCROLLBAR_SIZE}
-                                onScroll={({ scrollLeft }) => onScroll({ scrollLeft })}
-                                scrollLeft={scrollLeft}
-                            />
-                        </div>
-                        {/* ROW HEADERS */}
-                        <div style={{ position: "absolute", top: columnHeaderHeight, left: 0, width: rowHeaderWidth, overflow: "hidden" }}>
-                            <Grid
-                                width={rowHeaderWidth + SCROLLBAR_SIZE}
-                                height={height - columnHeaderHeight}
-                                renderCell={(props) =>
-                                    // HACK: pad the bottom with a phantom cell
-                                    props.rowIndex >= rowsCount ? <div /> :
-                                    // HACK: offsets the additional width needed to hide the scrollbars
-                                    <div style={{ width: rowHeaderWidth, position: "relative" }}>{renderRowHeader(props)}</div>
-                                }
-                                columnsCount={1}
-                                rowsCount={rowsCount + 1}
-                                columnWidth={rowHeaderWidth + SCROLLBAR_SIZE}
-                                rowHeight={(index) => index >= rowsCount ? paddingBottom : rowHeight}
-                                onScroll={({ scrollTop }) => onScroll({ scrollTop })}
-                                scrollTop={scrollTop}
-                            />
-                        </div>
-                        {/* CELLS */}
-                        <div style={{ position: "absolute", top: columnHeaderHeight, left: rowHeaderWidth, overflow: "hidden" }}>
-                            <Grid
-                                width={width - rowHeaderWidth}
-                                height={height - columnHeaderHeight}
-                                renderCell={(props) =>
-                                    // HACK: pad the bottom with a phantom cell
-                                    props.rowIndex >= rowsCount ? <div /> :
-                                    renderCell(props)
-                                }
-                                columnsCount={columnsCount}
-                                rowsCount={rowsCount + 1}
-                                columnWidth={columnWidth}
-                                rowHeight={(index) => index >= rowsCount ? paddingBottom : rowHeight}
-                                onScroll={({ scrollTop, scrollLeft }) => onScroll({ scrollTop, scrollLeft })}
-                                scrollTop={scrollTop}
-                                scrollLeft={scrollLeft}
-                            />
-                        </div>
+        <ScrollSync>
+            {({ clientHeight, clientWidth, onScroll, scrollHeight, scrollLeft, scrollTop, scrollWidth }) =>
+                <div>
+                    {/* CORNER */}
+                    <div style={{ position: "absolute", top: 0, left: 0, width: rowHeaderWidth, height: columnHeaderHeight, overflow: "hidden" }}>
+                        {renderCorner()}
                     </div>
-                }
-            </ScrollSync>
-        }
-        </AutoSizer>
+                    {/* COLUMN HEADERS */}
+                    <div style={{ position: "absolute", top: 0, left: rowHeaderWidth, height: columnHeaderHeight, overflow: "hidden" }}>
+                        <Grid
+                            width={width - rowHeaderWidth}
+                            height={columnHeaderHeight + SCROLLBAR_SIZE}
+                            cellRenderer={({ key, style, columnIndex, rowIndex }) =>
+                                <div key={key} style={style}>
+                                    {/* HACK: offsets the additional height needed to hide the scrollbars */}
+                                    <div style={{ height: columnHeaderHeight, position: "relative" }}>
+                                        {renderColumnHeader({ columnIndex })}
+                                    </div>
+                                </div>
+                            }
+                            columnCount={columnCount}
+                            rowCount={1}
+                            columnWidth={columnWidth}
+                            rowHeight={columnHeaderHeight + SCROLLBAR_SIZE}
+                            onScroll={({ scrollLeft }) => onScroll({ scrollLeft })}
+                            scrollLeft={scrollLeft}
+                        />
+                    </div>
+                    {/* ROW HEADERS */}
+                    <div style={{ position: "absolute", top: columnHeaderHeight, left: 0, width: rowHeaderWidth, overflow: "hidden" }}>
+                        <Grid
+                            width={rowHeaderWidth + SCROLLBAR_SIZE}
+                            height={height - columnHeaderHeight}
+                            cellRenderer={({ key, style, columnIndex, rowIndex }) =>
+                                <div key={key} style={style}>
+                                    {/* HACK: offsets the additional width needed to hide the scrollbars */}
+                                    <div style={{ width: rowHeaderWidth, position: "relative" }}>
+                                        {/*  HACK: pad the bottom with a phantom cell */}
+                                        {rowIndex >= rowCount ? null : renderRowHeader({ rowIndex })}
+                                    </div>
+                                </div>
+                            }
+                            columnCount={1}
+                            rowCount={rowCount + 1}
+                            columnWidth={rowHeaderWidth + SCROLLBAR_SIZE}
+                            rowHeight={(index) => index >= rowCount ? paddingBottom : rowHeight}
+                            onScroll={({ scrollTop }) => onScroll({ scrollTop })}
+                            scrollTop={scrollTop}
+                        />
+                    </div>
+                    {/* CELLS */}
+                    <div style={{ position: "absolute", top: columnHeaderHeight, left: rowHeaderWidth, overflow: "hidden" }}>
+                        <Grid
+                            width={width - rowHeaderWidth}
+                            height={height - columnHeaderHeight}
+                            cellRenderer={({ key, style, columnIndex, rowIndex }) =>
+                                <div key={key} style={style}>
+                                    {/*  HACK: pad the bottom with a phantom cell */}
+                                    {rowIndex >= rowCount ? null : renderCell({ columnIndex, rowIndex })}
+                                </div>
+                            }
+                            columnCount={columnCount}
+                            rowCount={rowCount + 1}
+                            columnWidth={columnWidth}
+                            rowHeight={(index) => index >= rowCount ? paddingBottom : rowHeight}
+                            onScroll={({ scrollTop, scrollLeft }) => onScroll({ scrollTop, scrollLeft })}
+                            scrollTop={scrollTop}
+                            scrollLeft={scrollLeft}
+                        />
+                    </div>
+                </div>
+            }
+        </ScrollSync>
     </div>
 
 export default FixedHeaderGrid;
