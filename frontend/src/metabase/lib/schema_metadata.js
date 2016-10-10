6aefcaf9709392a29d0e6dc94873a451176ce277
Tom Robinson <tlrobinson@gmail.com>
2016-10-10 13:14:04 -0700

    Fix isNumber

--- frontend/src/metabase/lib/schema_metadata.js
+++ frontend/src/metabase/lib/schema_metadata.js
@@ -111,7 +111,7 @@ export const isMetric    = (col) => (col && col.source !== "breakout") && isSumm
 export const isNumericBaseType = (field) => isa(field && field.base_type, TYPE.Number);
 
 // ZipCode, ID, etc derive from Number but should not be formatted as numbers
-export const isNumber = (field) => (field.special_type == null || field.special_type === TYPE.Number);
+export const isNumber = (field) => field && isNumericBaseType(field) && (field.special_type == null || field.special_type === TYPE.Number);
 
 export const isCoordinate = (field) => isa(field && field.special_type, TYPE.Coordinate);
 export const isLatitude = (field) => isa(field && field.special_type, TYPE.Latitude);
--- frontend/test/unit/lib/formatting.spec.js
+++ frontend/test/unit/lib/formatting.spec.js
@@ -24,15 +24,18 @@ describe('formatting', () => {
     });
 
     describe("formatValue", () => {
+        it("should format numbers with null column", () => {
+            expect(formatValue(12345)).toEqual("12345");
+        });
         it("should format numbers with commas", () => {
-            expect(formatValue(12345, { column: { special_type: TYPE.Number }})).toEqual("12,345");
+            expect(formatValue(12345, { column: { base_type: TYPE.Number, special_type: TYPE.Number }})).toEqual("12,345");
         });
         it("should format zip codes without commas", () => {
-            expect(formatValue(12345, { column: { special_type: TYPE.ZipCode }})).toEqual("12345");
+            expect(formatValue(12345, { column: { base_type: TYPE.Number, special_type: TYPE.ZipCode }})).toEqual("12345");
         });
         it("should format latitude and longitude columns correctly", () => {
-            expect(formatValue(37.7749, { column: { special_type: TYPE.Latitude }})).toEqual("37.77490000");
-            expect(formatValue(-122.4194, { column: { special_type: TYPE.Longitude }})).toEqual("-122.41940000");
+            expect(formatValue(37.7749, { column: { base_type: TYPE.Number, special_type: TYPE.Latitude }})).toEqual("37.77490000");
+            expect(formatValue(-122.4194, { column: { base_type: TYPE.Number, special_type: TYPE.Longitude }})).toEqual("-122.41940000");
         });
     });
 });
