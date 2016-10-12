031d8aad5078faa548fb63537e5bff0e44f04c8a
Tom Robinson <tlrobinson@gmail.com>
2016-10-12 15:59:25 -0700

    Remove log

--- frontend/src/metabase/query_builder/NativeQueryEditor.jsx
+++ frontend/src/metabase/query_builder/NativeQueryEditor.jsx
@@ -102,7 +102,6 @@ export default class NativeQueryEditor extends Component {
         if (modeInfo) {
             editor.setReadOnly(modeInfo.database.native_permissions !== "write");
             if (editor.getSession().$modeId !== modeInfo.mode) {
-                console.log('Setting ACE Editor mode to:', modeInfo.mode);
                 editor.getSession().setMode(modeInfo.mode);
                 // monkey patch the mode to add our bracket/paren/braces-matching behavior
                 if (!editor.getSession().$mode.$behaviour) {
