2fcc835fe27a00316f911b75412d933ec7891c26
Tom Robinson <tlrobinson@gmail.com>
2016-10-14 08:39:06 -0700

    slack-token marked as required on front and backend, validation interpreting 'false' as empty. Resolves #3518

--- src/metabase/api/slack.clj
+++ src/metabase/api/slack.clj
@@ -9,16 +9,18 @@
 (defendpoint PUT "/settings"
   "Update Slack related settings. You must be a superuser to do this."
   [:as {{slack-token :slack-token, metabot-enabled :metabot-enabled, :as slack-settings} :body}]
-  {slack-token     [Required NonEmptyString]
-   metabot-enabled [Required]}
+  {metabot-enabled [Required]}
   (check-superuser)
-  (try
-    ;; just check that channels.list doesn't throw an exception (a.k.a. that the token works)
-    (when-not config/is-test?
-      (slack/GET :channels.list, :exclude_archived 1, :token slack-token))
-    (setting/set-many! slack-settings)
-    {:ok true}
-    (catch clojure.lang.ExceptionInfo info
-      {:status 400, :body (ex-data info)})))
+  (if (not slack-token)
+    (setting/set-many! { :slack-token nil, :metabot-enabled false })
+    (try
+      ;; just check that channels.list doesn't throw an exception (a.k.a. that the token works)
+      (when-not config/is-test?
+        (slack/GET :channels.list, :exclude_archived 1, :token slack-token))
+      (setting/set-many! slack-settings)
+      {:ok true}
+      (catch clojure.lang.ExceptionInfo info
+        {:status 400, :body (ex-data info)})))
+  )
 
 (define-routes)
