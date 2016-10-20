180e6d84f44a52130db40ed152e0d2e0a3b4d96d
Felipe Gasparini <felipegasparini@gmail.com>
2016-10-19 22:42:08 -0500

    Job to evict card cache entries

--- src/metabase/models/card_cache.clj
+++ src/metabase/models/card_cache.clj
@@ -1,5 +1,7 @@
 (ns metabase.models.card-cache
   (:require
+    (honeysql [core :as hsql]
+              [helpers :as h])
     (metabase [db :as db])
     [metabase.models.interface :as i]
     [metabase.util :as u]
@@ -9,12 +11,12 @@
 (i/defentity CardCache :report_card_cache)
 
 (u/strict-extend (class CardCache)
-  i/IEntity
-  (merge i/IEntityDefaults
-         {:timestamped?       (constantly true)
-          :types              (constantly {:result :json})
-          :can-read?          (constantly true)
-          :can-write?         (constantly true)}))
+   i/IEntity
+   (merge i/IEntityDefaults
+          {:timestamped? (constantly true)
+           :types        (constantly {:result :json})
+           :can-read?    (constantly true)
+           :can-write?   (constantly true)}))
 
 
 (defn- calc-age
@@ -38,5 +40,33 @@
   [card-id query-hash result]
   (let [id (db/select-field :id CardCache :card_id card-id :query_hash query-hash)]
     (if (some? id)
-      (db/update! CardCache (first id)  {:result result})
+      (db/update! CardCache (first id) {:result result})
       (db/insert! CardCache {:card_id card-id :query_hash query-hash :result result}))))
+
+
+(defn- select-expired-cache-query
+  "Builds the honeysql query to select the card cache entries that should be evicted"
+  []
+  (-> (h/select :cc.id)
+      (h/from [:report_card_cache :cc])
+      (h/join [:report_card :c] [:= :c.id :cc.card_id])
+      (h/where [:or
+                [:= :c.cache_result false]
+                [:= :c.archived true]
+                [:> :%now (hsql/call :dateadd (hsql/raw "'second'") :c.cache_max_age :cc.updated_at)]])))
+
+(defn- delete-expired-cache-query
+  "Builds the honeysql query to delete the card cache entries that should be evicted"
+  []
+  (-> (h/delete-from :report_card_cache)
+      (h/where [:in :id (select-expired-cache-query)])))
+
+(defn evict!
+  "Delete all card cache entries that are expired or no longer needed"
+  []
+  (log/info "Going to evict cache entries...")
+  (let [result (db/execute! (delete-expired-cache-query))
+        rows-affected (first result)]
+    (if (zero? rows-affected)
+      (log/info "No cache entries evicted")
+      (log/info (format "Evicted %d cache entries" rows-affected)))))
