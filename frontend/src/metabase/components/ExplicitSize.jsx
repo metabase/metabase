fafac0c2a7b7ab8952e96c5a2fd0b7b982644024
Tom Robinson <tlrobinson@gmail.com>
2016-10-31 14:38:17 -0700

    Update ExplicitSize to watch for window and print media query changes, use to ensure Dashboard is resized when printing

--- frontend/src/metabase/components/ExplicitSize.jsx
+++ frontend/src/metabase/components/ExplicitSize.jsx
@@ -12,11 +12,26 @@ export default ComposedComponent => class extends Component {
         };
     }
 
+    componentWillMount() {
+        window.addEventListener("resize", this._updateSize);
+        this._mql = window.matchMedia("print");
+        this._mql.addListener(this._updateSize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this._updateSize);
+        this._mql.removeListener(this._updateSize);
+    }
+
     componentDidMount() {
-        this.componentDidUpdate();
+        this._updateSize();
     }
 
     componentDidUpdate() {
+        this._updateSize();
+    }
+
+    _updateSize = () => {
         const { width, height } = ReactDOM.findDOMNode(this).getBoundingClientRect();
         if (this.state.width !== width || this.state.height !== height) {
             this.setState({ width, height });
