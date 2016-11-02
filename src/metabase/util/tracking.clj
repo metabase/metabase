e3839f7827de890777d7fb4bb1077e9174cabd2b
Sameer Al-Sakran <sameer@expa.com>
2016-11-02 15:57:16 -0700

    oops. 2/2 [ci skip]

--- src/metabase/util/tracking.clj
+++ src/metabase/util/tracking.clj
@@ -1,16 +1,14 @@
 (ns metabase.util.tracking
   (:require [metabase.models.setting :as setting]
-            [clj-http.client :as client]
-            [ring.util.codec/form-encode :as form-encode]))
+            [clj-http.client :as client]))
 
 (def ^:private ^:const ^String google-analytics-collect-url "http://www.google-analytics.com/collect")
 ;; This is also in public_settings.clj. Should I access it from there?
 (def ^:private ^:const ^String metabase-google-analytics-id "UA-60817802-1")
 
-(defn- generate-anonymous-id
+(def ^:private ^Integer anonymous-id
   "Generate an anonymous id. Don't worry too much about hash collisions or localhost cases, etc.
    The goal is to be able to get a rough sense for how many different hosts are throwing a specific error/event." 
-  []
   (hash (str (java.net.InetAddress/getLocalHost)))
   )
 
@@ -23,13 +21,13 @@
   ([event-category event-action event-label]
    (track-event! event-category event-action event-label ""))
   ([event-category event-action event-label event-value]
-  	(when (setting/get :anon-tracking-enabled)
+    (when (setting/get :anon-tracking-enabled)
       (let [form-params {:v 1
-    		             :tid metabase-google-analytics-id 
-             		     :cid (generate-anonymous-id)
-                		 :t "event"
-                		 :ec event-category
-                		 :ea event-action
-                	     :el event-label
-                		 :ev event-value } ]
-  		(client/post google-analytics-collect-url {:form-params form-params})))))
+                         :tid metabase-google-analytics-id 
+                         :cid anonymous-id
+                         :t "event"
+                         :ec event-category
+                         :ea event-action
+                         :el event-label
+                         :ev event-value } ]
+        (client/post google-analytics-collect-url {:form-params form-params})))))
