0a425d0b5a4d4db7cfd2c05430d3bdc357567bbc
Tom Robinson <tlrobinson@gmail.com>
2016-11-07 17:36:49 -0800

    Prototype of Google Analytics field value fetching

--- src/metabase/driver/googleanalytics.clj
+++ src/metabase/driver/googleanalytics.clj
@@ -25,7 +25,7 @@
            com.google.api.client.json.JsonFactory
            com.google.api.client.json.jackson2.JacksonFactory
            (com.google.api.services.analytics Analytics Analytics$Builder Analytics$Data$Ga$Get AnalyticsScopes)
-           (com.google.api.services.analytics.model Account Accounts Columns Column Profile Profiles Webproperty Webproperties)))
+           (com.google.api.services.analytics.model Account Accounts Columns Column GaData Profile Profiles Webproperty Webproperties)))
 
 (set! *warn-on-reflection* true) ; NOCOMMIT
 
@@ -228,6 +228,54 @@
   [query]
   (google/execute (mbql-query->request query)))
 
+;;; analyze
+
+(def ^:private ^:const low-cardinality-threshold
+  "Fields with less than this many distinct values should automatically be given a special type of `:type/Category`."
+  40)
+
+(def ^:private ^:const field-values-entry-max-length
+  "The maximum character length for a stored `FieldValues` entry."
+  100)
+
+(defn field-distinct-values
+  "Return the distinct values of FIELD.
+   This is used to create a `FieldValues` object for `:type/Category` Fields."
+  [{field-id :id :as field} max-results]
+  (try
+    (let [table            (field/table field)
+          database         (table/database table)
+          query            {:database database
+                            :native {:query {:ids                 (str "ga:" (:name table))
+                                             :start-date          "2005-01-01"
+                                             :end-date            "today"
+                                             :metrics             "ga:users"
+                                             :dimensions          (:name field)
+                                             :include-empty-rows  true
+                                             :max-results         (int (inc low-cardinality-threshold))}}}
+          ^GaData response (google/execute-no-auto-retry (mbql-query->request query))
+          values           (for [row (.getRows response)]
+                             (first row))]
+      (log/info (str "FIELD VALUES FOR " (:name field) " = " (vec values)))
+      values)
+  (catch Throwable e
+    nil)))
+
+(defn test:cardinality-and-extract-field-values
+  "Extract field-values for FIELD.  If number of values exceeds `low-cardinality-threshold` then we return an empty set of values."
+  [field field-stats]
+  ;; TODO: we need some way of marking a field as not allowing field-values so that we can skip this work if it's not appropriate
+  ;;       for example, :type/Category fields with more than MAX values don't need to be rescanned all the time
+  (let [non-nil-values  (filter identity (field-distinct-values field (inc low-cardinality-threshold)))
+        ;; only return the list if we didn't exceed our MAX values and if the the total character count of our values is reasable (#2332)
+        distinct-values (when-not (or (< low-cardinality-threshold (count non-nil-values))
+                                      ;; very simple check to see if total length of field-values exceeds (total values * max per value)
+                                      (< (* low-cardinality-threshold
+                                            field-values-entry-max-length) (reduce + (map (comp count str) non-nil-values))))
+                          non-nil-values)]
+    (cond-> (assoc field-stats :values distinct-values)
+      (and (nil? (:special_type field))
+           (pos? (count distinct-values))) (assoc :special-type :type/Category))))
 
 ;;; ------------------------------------------------------------ Driver ------------------------------------------------------------
 
@@ -238,7 +286,10 @@
 (u/strict-extend GoogleAnalyticsDriver
   driver/IDriver
   (merge driver/IDriverDefaultsMixin
-         {:can-connect?             (u/drop-first-arg can-connect?)
+         {:analyze-table              (fn [driver table new-field-ids]
+                                        {:fields (for [{:keys [id] :as field} (table/fields table)]
+                                                   (test:cardinality-and-extract-field-values field {:id id}))})
+          :can-connect?             (u/drop-first-arg can-connect?)
           :describe-database        (u/drop-first-arg describe-database)
           :describe-table           (u/drop-first-arg describe-table)
           :details-fields           (constantly [{:name         "account-id"
