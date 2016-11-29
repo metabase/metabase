e3e4e61b76e1fb79d433f182552e30e8aeb739e5
Tom Robinson <tlrobinson@gmail.com>
2016-11-29 14:50:07 -0800

    Enable table canvas hack, fix horizontal scroll issue, upgrade react-virtualized

--- frontend/src/metabase/visualizations/TableInteractive.jsx
+++ frontend/src/metabase/visualizations/TableInteractive.jsx
@@ -318,18 +318,19 @@ export default class TableInteractive extends Component {
             <ScrollSync>
             {({ clientHeight, clientWidth, onScroll, scrollHeight, scrollLeft, scrollTop, scrollWidth }) =>
                 <div className={cx(className, 'MB-DataTable relative', { 'MB-DataTable--pivot': this.props.isPivoted, 'MB-DataTable--ready': this.state.contentWidths })}>
-                    { window.localStorage.tableCanvasHack && <canvas className="spread" style={{ pointerEvents: "none", zIndex: 999 }} width={width} height={height} /> }
+                    <canvas className="spread" style={{ pointerEvents: "none", zIndex: 999 }} width={width} height={height} />
                     <Grid
                         ref={(ref) => this.header = ref}
                         style={{ top: 0, left: 0, right: 0, height: HEADER_HEIGHT, position: "absolute", overflow: "hidden" }}
                         className="MB-DataTable-header scroll-hide-all"
                         width={width || 0}
                         height={HEADER_HEIGHT}
-                        columnCount={cols.length}
-                        columnWidth={this.getColumnWidth}
                         rowCount={1}
                         rowHeight={HEADER_HEIGHT}
-                        cellRenderer={this.tableHeaderRenderer}
+                        // HACK: there might be a better way to do this, but add a phantom padding cell at the end to ensure scroll stays synced if main content scrollbars are visible
+                        columnCount={cols.length + 1}
+                        columnWidth={(props) => props.index < cols.length ? this.getColumnWidth(props) : 50}
+                        cellRenderer={(props) => props.columnIndex < cols.length ? this.tableHeaderRenderer(props) : null}
                         onScroll={({ scrollLeft }) => onScroll({ scrollLeft })}
                         scrollLeft={scrollLeft}
                         tabIndex={null}
