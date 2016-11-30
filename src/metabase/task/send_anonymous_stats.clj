cf0161aa72e2db7471b25629a41fce8957a91fb6
Sameer Al-Sakran <sameer@expa.com>
2016-11-29 17:35:09 -0800

    re-order deps

--- src/metabase/task/send_anonymous_stats.clj
+++ src/metabase/task/send_anonymous_stats.clj
@@ -1,9 +1,9 @@
 (ns metabase.task.send-anonymous-stats
   "Contains a Metabase task which periodically sends anonymous usage information to the Metabase team."
   (:require [clojure.tools.logging :as log]
-            (clojurewerkz.quartzite [jobs :as jobs]
-                                    [triggers :as triggers])
+            [clojurewerkz.quartzite.jobs :as jobs]
             [clojurewerkz.quartzite.schedule.cron :as cron]
+            [clojurewerkz.quartzite.triggers :as triggers]
             (metabase [config :as config]
                       [public-settings :as public-settings]
                       [task :as task])
