f59e1b873ed099b6fc7e06d6acac5db9f166bd94
Tom Robinson <tlrobinson@gmail.com>
2016-12-02 09:51:38 -0800

    Add minute time grouping, week filtering, and tweak date operator/oeriod select chevron

--- frontend/src/metabase/query_builder/components/filters/pickers/RelativeDatePicker.jsx
+++ frontend/src/metabase/query_builder/components/filters/pickers/RelativeDatePicker.jsx
@@ -1,9 +1,18 @@
 import React, { Component, PropTypes } from "react";
-import { pluralize, titleCase } from "humanize-plus";
+import { pluralize, titleCase, capitalize } from "humanize-plus";
 import cx from "classnames";
 
 import Icon from "metabase/components/Icon";
 import NumericInput from "./NumericInput.jsx";
+
+const PERIODS = [
+    "minute",
+    "hour",
+    "day",
+    "week",
+    "month",
+    "year"
+];
 
 export default class RelativeDatePicker extends Component {
     constructor () {
@@ -57,7 +66,7 @@ export const UnitPicker = ({ open, value, onChange, togglePicker, intervals, for
        >
            <h3>{pluralize(formatter(intervals) || 1, titleCase(value))}</h3>
            <Icon
-               name='chevrondown'
+               name={open ? 'chevronup' : 'chevrondown'}
                width="12"
                height="12"
                className="ml1"
@@ -70,17 +79,17 @@ export const UnitPicker = ({ open, value, onChange, togglePicker, intervals, for
                 overflow: 'hidden'
             }}
         >
-           { ['Minute', 'Hour', 'Day', 'Month', 'Year',].map((unit, index) =>
+           { PERIODS.map((unit, index) =>
                <li
                    className={cx(
                        'List-item cursor-pointer p1',
                        { 'List-item--selected': unit === value }
                    )}
                    key={index}
-                   onClick={ () => onChange(unit.toLowerCase()) }
+                   onClick={ () => onChange(unit) }
                >
                    <h4 className="List-item-title">
-                       {pluralize(formatter(intervals) || 1, unit)}
+                       {capitalize(pluralize(formatter(intervals) || 1, unit))}
                    </h4>
                </li>
              )
