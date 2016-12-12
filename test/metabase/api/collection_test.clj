98f9cf7611a2568bfe4f8bae41437971f2b899f5
Cam Saül <cammsaul@gmail.com>
2016-12-12 12:54:16 -0800

    Additional tests for /api/collection endpoints :100:

--- test/metabase/api/collection_test.clj
+++ test/metabase/api/collection_test.clj
@@ -1,8 +1,11 @@
 (ns metabase.api.collection-test
   "Tests for /api/collection endpoints."
   (:require [expectations :refer :all]
+            [metabase.db :as db]
             (metabase.models [card :refer [Card]]
-                             [collection :refer [Collection]])
+                             [collection :refer [Collection]]
+                             [permissions :as perms]
+                             [permissions-group :as group])
             [metabase.test.data.users :refer [user->client]]
             [metabase.test.util :as tu]
             [metabase.util :as u]))
@@ -12,9 +15,35 @@
   [(into {} collection)]
   ((user->client :crowberto) :get 200 "collection"))
 
-;; TODO - check that we don't see collections if we don't have permissions for them
+;; check that we don't see collections if we don't have permissions for them
+(tu/expect-with-temp [Collection [collection-1 {:name "Collection 1"}]
+                      Collection [collection-2 {:name "Collection 2"}]]
+  ["Collection 1"]
+  (do
+    (perms/grant-collection-read-permissions! (group/all-users) collection-1)
+    (map :name ((user->client :rasta) :get 200 "collection"))))
 
-;; TODO - check that we don't see collections if they're archived
+;; check that we don't see collections if they're archived
+(tu/expect-with-temp [Collection [collection-1 {:name "Archived Collection", :archived true}]
+                      Collection [collection-2 {:name "Regular Collection"}]]
+  ["Regular Collection"]
+  (do
+    (perms/grant-collection-read-permissions! (group/all-users) collection-1)
+    (perms/grant-collection-read-permissions! (group/all-users) collection-2)
+    (map :name ((user->client :rasta) :get 200 "collection"))))
+
+;; check that we can see collection details (GET /api/collection/:id)
+(expect
+  "Coin Collection"
+  (tu/with-temp Collection [collection {:name "Coin Collection"}]
+    (perms/grant-collection-read-permissions! (group/all-users) collection)
+    (:name ((user->client :rasta) :get 200 (str "collection/" (u/get-id collection))))))
+
+;; check that collections detail properly checks permissions
+(expect
+  "You don't have permissions to do that."
+  (tu/with-temp Collection [collection]
+    ((user->client :rasta) :get 403 (str "collection/" (u/get-id collection)))))
 
 ;; check that cards are returned with the collections detail endpoint
 (tu/expect-with-temp [Collection [collection]
@@ -22,13 +51,31 @@
   (tu/obj->json->obj (assoc collection :cards [card]))
   (tu/obj->json->obj ((user->client :crowberto) :get 200 (str "collection/" (u/get-id collection)))))
 
-;; TODO - check that collections detail properly checks permissions
+;; check that collections detail doesn't return archived collections
+(expect
+  "Not found."
+  (tu/with-temp Collection [collection {:archived true}]
+    (perms/grant-collection-read-permissions! (group/all-users) collection)
+    ((user->client :rasta) :get 404 (str "collection/" (u/get-id collection)))))
 
-;; TODO - check that collections detail doesn't return archived collections
+;; test that we can create a new collection (POST /api/collection)
+(expect
+  {:name        "Stamp Collection"
+   :slug        "stamp_collection"
+   :description nil
+   :color       "#123456"
+   :archived    false}
+  (dissoc (u/prog1 ((user->client :crowberto) :post 200 "collection"
+                    {:name "Stamp Collection", :color "#123456"})
+            ;; make sure we clean up after ourselves
+            (db/cascade-delete! Collection :id (u/get-id <>)))
+          :id))
 
-;; TODO - test that we can create a new collection (POST /api/collection)
-
-;; TODO - test that non-admins aren't allowed to create a collection
+;; test that non-admins aren't allowed to create a collection
+(expect
+  "You don't have permissions to do that."
+  ((user->client :rasta) :post 403 "collection"
+   {:name "Stamp Collection", :color "#123456"}))
 
 ;; test that we can update a collection (PUT /api/collection/:id)
 (tu/expect-with-temp [Collection [collection]]
