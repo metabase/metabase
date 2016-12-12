d43133e2ca6d78756b1ecb70f43ae6ae51b2c950
Cam Saül <cammsaul@gmail.com>
2016-12-12 14:11:56 -0800

    Make sure BigQuery driver declares :basic-aggregations feature [ci drivers]

--- src/metabase/driver/bigquery.clj
+++ src/metabase/driver/bigquery.clj
@@ -419,9 +419,9 @@
           ;; people can manually specifiy "foreign key" relationships in admin and everything should work correctly.
           ;; Since we can't infer any "FK" relationships during sync our normal FK tests are not appropriate for BigQuery, so they're disabled for the time being.
           ;; TODO - either write BigQuery-speciifc tests for FK functionality or add additional code to manually set up these FK relationships for FK tables
-          :features              (constantly (when-not config/is-test?
-                                               ;; during unit tests don't treat bigquery as having FK support
-                                               #{:foreign-keys}))
+          :features              (constantly (set/union #{:basic-aggregations} (when-not config/is-test?
+                                                                                 ;; during unit tests don't treat bigquery as having FK support
+                                                                                 #{:foreign-keys})))
           :field-values-lazy-seq (u/drop-first-arg field-values-lazy-seq)
           :mbql->native          (u/drop-first-arg mbql->native)}))
 
