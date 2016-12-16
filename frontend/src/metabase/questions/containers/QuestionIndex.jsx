f5a72d75ab9657f8c7fca0548476dc287b3b7ec0
Kyle Doherty <kyle.l.doherty@gmail.com>
2016-12-15 17:18:52 -0800

    expand arrow

--- frontend/src/metabase/questions/containers/QuestionIndex.jsx
+++ frontend/src/metabase/questions/containers/QuestionIndex.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router";
+import { Motion, spring, presets } from "react-motion";
 
 // import ActionGroup from "metabase/components/ActionGroup";
 import Icon from "metabase/components/Icon";
@@ -68,14 +69,24 @@ export default class QuestionIndex extends Component {
                 <div className="mb2">
                     <CollectionButtons collections={collections} />
                 </div>
-                <div className="flex align-center mt4">
-                    {/* TODO: proper arrow icon */}
-                    <Icon
-                        className="p2"
-                        name={questionsExpanded ? "chevrondown" : "chevronright"}
-                        onClick={() => this.setState({ questionsExpanded: !questionsExpanded })}
-                    />
-                    <h2>Everything Else</h2>
+                <div
+                    className="inline-block mt4 mb2 cursor-pointer text-brand-hover"
+                    onClick={() => this.setState({ questionsExpanded: !questionsExpanded })}
+                >
+                    <div className="flex align-center">
+                        <Motion defaultStyle={{ deg: 0 }} style={{ deg: questionsExpanded ? spring(0, presets.gentle) : spring(270, presets.gentle) }}>
+                            { motionStyle =>
+                                <Icon
+                                    className="ml1 mr1"
+                                    name="expandarrow"
+                                    style={{
+                                        transform: `rotate(${motionStyle.deg}deg)`
+                                    }}
+                                />
+                            }
+                        </Motion>
+                        <h2>Everything Else</h2>
+                    </div>`
                 </div>
                 { questionsExpanded &&
                     <div className="">
