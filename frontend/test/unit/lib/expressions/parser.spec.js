9ea995efca6f9fce1c1659f9b8473e06991b210b
Tom Robinson <tlrobinson@gmail.com>
2016-12-20 16:33:10 -0800

    Negative number literal tests

--- frontend/test/unit/lib/expressions/parser.spec.js
+++ frontend/test/unit/lib/expressions/parser.spec.js
@@ -52,6 +52,10 @@ describe("lib/expressions/parser", () => {
             expect(compile("1 + 2 + 3 * 4 * 5", expressionOpts)).toEqual(["+", 1, 2, ["*", 3, 4, 5]]);
         });
 
+        it("can handle negative number literals", () => {
+            expect(compile("1 + -1", expressionOpts)).toEqual(["+", 1, -1]);
+        });
+
         // quoted field name w/ a space in it
         it("can parse a field with quotes and spaces", () => {
             expect(compile("\"Toucan Sam\" + B", expressionOpts)).toEqual(["+", ['field-id', 10], ['field-id', 2]]);
@@ -73,6 +77,10 @@ describe("lib/expressions/parser", () => {
 
         it("can parse aggregation with argument", () => {
             expect(compile("Sum(A)", aggregationOpts)).toEqual(["sum", ["field-id", 1]]);
+        });
+
+        it("can handle negative number literals in aggregations", () => {
+            expect(compile("-1 * Count", aggregationOpts)).toEqual(["*", -1, ["count"]]);
         });
 
         it("can parse complex aggregation", () => {
