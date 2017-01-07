351926b48b6834831f70dcfe6980737b993d1748
Cam Saül <cammsaul@gmail.com>
2017-01-06 18:03:47 -0800

    Compare set of migration files with set of ran migrations :100:

--- src/metabase/db.clj
+++ src/metabase/db.clj
@@ -106,44 +106,53 @@
 ;;; |                                                        MIGRATE!                                                        |
 ;;; +------------------------------------------------------------------------------------------------------------------------+
 
-(defn- migrations-files-count:jar
-  "Get the number of migration files inside the uberjar. (`io/resource` doesn't work here; this approach adapted from [this StackOverflow answer](http://stackoverflow.com/a/20073154/1198455).)"
-  ^Integer []
+(defn- filename-without-path-or-prefix
+  "Strip the path and/or prefix from a migration FILENAME if it has them."
+  [filename]
+  (s/replace filename #"^(?:migrations/)?([\w\d]+)(?:\.(?:json|yaml))?$" "$1"))
+
+(defn- migration-files:jar
+  "Return the set of migration filenames (without path or prefix) inside the uberjar.
+   (`io/resource` doesn't work here; this approach adapted from [this StackOverflow answer](http://stackoverflow.com/a/20073154/1198455).)"
+  []
   ;; get path to the jar -- see this SO answer http://stackoverflow.com/a/320595/1198455
   (let [^String                   jar-path (s/replace (-> ComboPooledDataSource .getProtectionDomain .getCodeSource .getLocation .getPath) #"%20" " ")
         ^JarFile$JarEntryIterator entries  (.entries (JarFile. jar-path))
-        ^Atom                     n        (atom 0)]
+        ^Atom                     files    (atom #{})]
     (while (.hasMoreElements entries)
       (let [^JarFile$JarFileEntry entry      (.nextElement entries)
             ^String               entry-name (.getName entry)]
         (when (and (.startsWith entry-name "migrations/")
                    (not= entry-name "migrations/"))       ; skip the directory itself
-          (swap! n inc))))
-    @n))
+          (swap! files conj (filename-without-path-or-prefix entry-name)))))
+    @files))
 
-(defn- migrations-files-count
-  "Return the number of files in the `resources/migrations` directory."
-  ^Integer []
+(defn- migration-files
+  "Return the set of migration filenames (without path or prefix) in the `resources/migrations` directory or from the JAR."
+  []
   ;; unfortunately io/as-file doesn't seem to work for directories inside a JAR. Try it for local dev but fall back to hacky Java interop method if that fails
-  (try (count (.list (io/as-file (io/resource "migrations"))))
+  (try (set (map filename-without-path-or-prefix (.list (io/as-file (io/resource "migrations")))))
        (catch Throwable _
-         (migrations-files-count:jar))))
+         (migration-files:jar))))
 
 (declare quote-fn)
 
-(defn- migrations-entries-count
-  "Return the number of rows in the Liquibase `databasechangelog` table."
-  ^Integer []
-  ;; an Exception will get thrown if there is no databasechangelog table yet
-  (or (u/ignore-exceptions
-        (:count (first (jdbc/query (jdbc-details) [(format "SELECT COUNT(*) AS count FROM %s;" ((quote-fn) "databasechangelog"))]))))
-      0))
+(defn- migration-entries
+  "Return a set of migration files (without path or prefix) that have already been ran.
+   This is fetched from the `databasechangelog` table.
 
-(defn- ^Boolean has-unran-migration-files?
-  "`true` if there are more files in the migrations directory than there are entries in the `databasechangelog` table."
+     (migration-entires) -> #{\"001_initial_schema\", \"002_add_session_table\", ...}"
   []
-  (> (migrations-files-count)
-     (migrations-entries-count)))
+  ;; an Exception will get thrown if there is no databasechangelog table yet; just return nil in that case because nil will never equal any set
+  (u/ignore-exceptions
+    (set (for [{filename :filename} (jdbc/query (jdbc-details) [(format "SELECT %s AS filename FROM %s;" ((quote-fn) "filename") ((quote-fn) "databasechangelog"))])]
+           (filename-without-path-or-prefix filename)))))
+
+(defn- has-unran-migration-files?
+  "`true` if the set of migration files is the same as the set of migrations that have already been ran."
+  ^Boolean []
+  (not= (migration-files)
+        (migration-entries)))
 
 (defn migrate!
   "Migrate the database (this can also be ran via command line like `java -jar metabase.jar migrate up` or `lein run migrate up`):
