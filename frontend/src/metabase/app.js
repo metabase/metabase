35a84fe96b4c9f826d285c85696e82b6cefa1f77
Tom Robinson <tlrobinson@gmail.com>
2017-01-24 11:55:35 -0800

    Fix segment/metric editor. Add comments explaining whitelisted 403 urls [ci all]

--- frontend/src/metabase/app.js
+++ frontend/src/metabase/app.js
@@ -18,6 +18,18 @@ import { setErrorPage } from "metabase/redux/app";
 import { Router, browserHistory } from "react-router";
 import { push, syncHistoryWithStore } from 'react-router-redux'
 
+// we shouldn't redirect these URLs because we want to handle them differently
+const WHITELIST_FORBIDDEN_URLS = [
+    // on dashboards, we show permission errors for individual cards we don't have access to
+    /api\/card\/\d+\/query$/,
+    // metadata endpoints should not cause redirects
+    // we should gracefully handle cases where we don't have access to metadata
+    /api\/database\/\d+\/metadata$/,
+    /api\/database\/\d+\/fields/,
+    /api\/table\/\d+\/query_metadata$/,
+    /api\/table\/\d+\/fks$/
+];
+
 function init() {
     const store = getStore(browserHistory);
     const routes = getRoutes(store);
@@ -50,14 +62,6 @@ function init() {
         store.dispatch(push("/auth/login"));
     });
 
-    // we shouldn't redirect these URLs because we want to handle them differently
-    let WHITELIST_FORBIDDEN_URLS = [
-        /api\/card\/\d+\/query$/,
-        /api\/database\/\d+\/metadata$/,
-        /api\/database\/\d+\/fields/,
-        /api\/table\/\d+\/query_metadata$/,
-        /api\/table\/\d+\/fks$/
-    ];
     // received a 403 response
     api.on("403", (url) => {
         if (url) {
