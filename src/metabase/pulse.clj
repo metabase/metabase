e688e9639529c3cb2cf1acfb99b6a08b22510f18
Andres Gomez Urquiza <andres@fractal.ai>
2017-01-31 21:47:27 -0600

    :row-count -> :row_count and small fn simplification

--- src/metabase/pulse.clj
+++ src/metabase/pulse.clj
@@ -66,13 +66,14 @@
 (defn is-card-empty?
   "Check if the card is empty"
   [card]
-  (or (zero? (-> card :result :row-count))
-      (= [[nil]]
-         (-> card :result :data :rows))
-      (and (= [["count"]]
-              (-> card :card :dataset_query :query :aggregation))
-           (= [[0]]
-              (-> card :result :data :rows)))))
+  (let [result (:result card)]
+    (or (zero? (-> result :row_count))
+        (= [[nil]]
+           (-> result :data :rows))
+        (and (= [["count"]]
+                (-> card :card :dataset_query :query :aggregation))
+             (= [[0]]
+                (-> result :data :rows))))))
 
 (defn are-all-cards-empty?
   "Do none of the cards have any results?"
