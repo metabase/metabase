1f022e970e5b166cc81a7f6b1bc2d72e0fb01d42
Kyle Doherty <kdoh@users.noreply.github.com>
2017-03-14 17:39:07 -0600

    clean up pulse empty state modals (#4422)
    * clean up pulse modals
    
    * clean up

--- frontend/src/metabase/components/ModalContent.jsx
+++ frontend/src/metabase/components/ModalContent.jsx
@@ -58,7 +58,7 @@ const FORM_WIDTH = 500 + 32 * 2; // includes padding
 
 export const ModalHeader = ({ children }, { fullPageModal, formModal }) =>
     <div className={cx("ModalHeader flex-no-shrink px4 py4 full")}>
-        <h2 className={cx("text-bold", { "text-centered": fullPageModal })}>{children}</h2>
+        <h2 className={cx("text-bold", { "text-centered": fullPageModal }, { "mr4": !fullPageModal})}>{children}</h2>
     </div>
 
 ModalHeader.contextTypes = MODAL_CHILD_CONTEXT_TYPES;
