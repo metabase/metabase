d60d30ccac37b0aadb5032e0d23541b94b9a8a1b
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-04-03 10:44:27 -0700

    pivot selection

--- frontend/src/metabase/new_question/containers/PivotSelection.jsx
+++ frontend/src/metabase/new_question/containers/PivotSelection.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
 import { connect } from "react-redux";
 
+import Icon from "metabase/components/Icon"
+import cx from 'classnames'
+
 import {
     isDate
 } from "metabase/lib/schema_metadata"
@@ -8,33 +11,59 @@ import {
 import {
     selectAndAdvance,
     setPivotBreakouts,
+    setTip
 } from '../actions'
 
 import {
     getBreakoutsForFlow,
+    getCurrentStepTip
 } from '../selectors'
 
 const mapStateToProps = state => ({
-    fields: getBreakoutsForFlow(state)
+    fields: getBreakoutsForFlow(state),
+    tip: getCurrentStepTip(state)
 })
 
 const mapDispatchToProps = ({
     selectAndAdvance,
     setPivotBreakouts,
+    setTip
 })
+
+const SelectedBreakouts = ({ breakouts, remove }) =>
+    <ol>
+        { breakouts.map((breakout, index) =>
+            <li key={breakout.id} className="inline-block mr1">
+                <div
+                    className="bg-brand p1 text-white rounded flex align-center"
+                >
+                    {breakout.display_name}
+                    <Icon className="text-white" name="close" onClick={() => remove(index) } />
+                </div>
+            </li>
+        )}
+    </ol>
+
 
 @connect(mapStateToProps, mapDispatchToProps)
 class PivotSelection extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             breakouts: []
         }
+        this.tip = this.props.tip
     }
 
     selectBreakout = (breakout) => {
-        console.log('breakout', breakout)
+        if(this.state.breakouts.length === 2) {
+            return false
+        }
         return this.setState({ breakouts: this.state.breakouts.concat([breakout]) })
+    }
+
+    removeBreakout = (index) => {
+        return this.setState({ breakouts: this.state.breakouts.splice(index, 1) })
     }
 
     completeStep = () => {
@@ -54,20 +83,41 @@ class PivotSelection extends Component {
     }
     render () {
         const { fields } = this.props
+        const { breakouts } = this.state
         return (
             <div>
                 <div className="flex align-center">
                     Pivot by:
-                    { this.state.breakouts.map(breakout => <div>{breakout.display_name}</div>) }
-                    <button className="ml-auto Button Button--primary" onClick={() => this.completeStep()}>Next</button>
+                    <SelectedBreakouts
+                        breakouts={breakouts}
+                        remove={this.removeBreakout}
+                    />
+                    { breakouts.length === 2 && (
+                        <button
+                            className="ml-auto Button Button--primary"
+                            onClick={() => this.completeStep()}
+                        >
+                            Next
+                        </button>
+                    )}
                 </div>
 
                 <div className="border-top">
-                { fields.map(field =>
-                    <div onClick={() => this.selectBreakout(field) }>
-                        {field.display_name}
-                    </div>
-                )}
+                    { fields.map(field =>
+                        <div
+                            key={field.id}
+                            className={cx(
+                                "h4 py1",
+                                { 'link cursor-pointer': breakouts.length < 2 },
+                                { 'disabled': breakouts.length === 2 }
+                            )}
+                            onMouseEnter={() => setTip({ title: field.display_name, text: field.description}) }
+                            onMouseLeave={() => setTip(this.tip)}
+                            onClick={() => this.selectBreakout(field) }
+                        >
+                            {field.display_name}
+                        </div>
+                    )}
                 </div>
             </div>
         )
