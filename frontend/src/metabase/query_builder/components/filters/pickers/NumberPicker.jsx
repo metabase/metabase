0644816bfc64a7caed630623a4bd34fd076c3a87
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-04-04 10:55:20 -0700

    handle 0

--- frontend/src/metabase/query_builder/components/filters/pickers/NumberPicker.jsx
+++ frontend/src/metabase/query_builder/components/filters/pickers/NumberPicker.jsx
@@ -23,7 +23,12 @@ export default class NumberPicker extends Component<*, Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            stringValues: props.values.map(v => String(v || "")),
+            stringValues: props.values.map(v => {
+                if(v === 0) {
+                    return String(v)
+                }
+                return String(v || "")}
+            ),
             validations: this._validate(props.values)
         }
     }
