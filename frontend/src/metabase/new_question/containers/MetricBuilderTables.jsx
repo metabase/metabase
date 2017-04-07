3a9c0879fced5c1b0ab0a5a5a138fb4ac3cd5215
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-04-07 12:00:25 -0700

    table listing styles

--- frontend/src/metabase/new_question/containers/MetricBuilderTables.jsx
+++ frontend/src/metabase/new_question/containers/MetricBuilderTables.jsx
@@ -17,24 +17,45 @@ const mapDispatchToProps = {
 
 @connect(mapStateToProps, mapDispatchToProps)
 class MetricBuilderTables extends Component {
+    state = {
+        search: ''
+    }
     render() {
         const { title, tables, selectAndAdvance, setTable } = this.props;
         return (
             <div>
                 <h3>{title}</h3>
-                <ol>
-                    {tables.map(({ id, name }) => (
-                        <li key={id}>
-                            <a
-                                className="link"
-                                onClick={() =>
-                                    selectAndAdvance(() => setTable(id))}
-                            >
-                                {name}
-                            </a>
-                        </li>
-                    ))}
-                </ol>
+                <div className="bg-white bordered rounded mt2">
+                    <div className="border-bottom">
+                        <input
+                            className="borderless p2 h3 full"
+                            placeholder="Search for a table"
+                            type="text"
+                            onChange={(ev) => this.setState({ search: ev.target.value })}
+                            autoFocus
+                        />
+                    </div>
+                    <ol>
+                        {tables
+                                .filter(table =>
+                                    table.name.toLowerCase().includes(this.state.search.toLowerCase())
+                                )
+                                .map(({ id, display_name }) => (
+                                <li
+                                    key={id}
+                                    className="border-bottom p2"
+                                >
+                                    <a
+                                        className="link"
+                                        onClick={() =>
+                                            selectAndAdvance(() => setTable(id))}
+                                    >
+                                        <h3>{display_name}</h3>
+                                    </a>
+                                </li>
+                            ))}
+                    </ol>
+                </div>
             </div>
         );
     }
