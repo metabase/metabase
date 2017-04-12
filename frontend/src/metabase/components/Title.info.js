95782ee27fd0b5790142340bff69031b9e9a9f60
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-04-12 16:59:09 -0700

    move title component

---
+++ frontend/src/metabase/components/Title.info.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Title from 'metabase/components/Title'
+
+export const component = Title
+export const description = `
+Big text. This should be used when a main heading is needed
+`
+export const examples = {
+    "": <Title>I am big text</Title>
+}
