4a43b1057dff24db27642fd0c844ad85de7cc235
Arthur Ulfeldt <arthur@ulfeldt.com>
2017-04-25 10:42:06 -0700

    ssh-tunnels for vertica

--- src/metabase/driver/vertica.clj
+++ src/metabase/driver/vertica.clj
@@ -7,7 +7,8 @@
             [metabase.driver :as driver]
             [metabase.driver.generic-sql :as sql]
             [metabase.util :as u]
-            [metabase.util.honeysql-extensions :as hx]))
+            [metabase.util.honeysql-extensions :as hx]
+            [metabase.util.ssh :as ssh]))
 
 (def ^:private ^:const column->base-type
   "Map of Vertica column types -> Field base types.
@@ -100,28 +101,29 @@
 
 (u/strict-extend VerticaDriver
   driver/IDriver
-  (merge (sql/IDriverSQLDefaultsMixin)
-         {:date-interval     (u/drop-first-arg date-interval)
-          :describe-database describe-database
-          :details-fields    (constantly [{:name         "host"
-                                           :display-name "Host"
-                                           :default      "localhost"}
-                                          {:name         "port"
-                                           :display-name "Port"
-                                           :type         :integer
-                                           :default      5433}
-                                          {:name         "dbname"
-                                           :display-name "Database name"
-                                           :placeholder  "birds_of_the_word"
-                                           :required     true}
-                                          {:name         "user"
-                                           :display-name "Database username"
-                                           :placeholder  "What username do you use to login to the database?"
-                                           :required     true}
-                                          {:name         "password"
-                                           :display-name "Database password"
-                                           :type         :password
-                                           :placeholder  "*******"}])})
+  (ssh/with-tunnel-config
+    (merge (sql/IDriverSQLDefaultsMixin)
+           {:date-interval     (u/drop-first-arg date-interval)
+            :describe-database describe-database
+            :details-fields    (constantly [{:name         "host"
+                                             :display-name "Host"
+                                             :default      "localhost"}
+                                            {:name         "port"
+                                             :display-name "Port"
+                                             :type         :integer
+                                             :default      5433}
+                                            {:name         "dbname"
+                                             :display-name "Database name"
+                                             :placeholder  "birds_of_the_word"
+                                             :required     true}
+                                            {:name         "user"
+                                             :display-name "Database username"
+                                             :placeholder  "What username do you use to login to the database?"
+                                             :required     true}
+                                            {:name         "password"
+                                             :display-name "Database password"
+                                             :type         :password
+                                             :placeholder  "*******"}])}))
   sql/ISQLDriver
   (merge (sql/ISQLDriverDefaultsMixin)
          {:column->base-type         (u/drop-first-arg column->base-type)
