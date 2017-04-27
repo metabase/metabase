56ed48614face863109300802e54d78987b94712
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-04-26 17:17:52 -0700

    refactor and add tests

--- frontend/src/metabase/components/HeaderBar.jsx
+++ frontend/src/metabase/components/HeaderBar.jsx
@@ -1,53 +1,40 @@
+/* @flow */
 import React, { Component } from "react";
+import cx from "classnames";
 
 import Input from "metabase/components/Input.jsx";
 import TitleAndDescription from "metabase/components/TitleAndDescription.jsx";
 
-import cx from "classnames";
-
-export default class Header extends Component {
+export default class HeaderBar extends Component {
 
     static defaultProps = {
         buttons: null,
-        className: "py1 lg-py2 xl-py3 wrapper",
-        breadcrumb: null
     };
 
     render() {
-        const { isEditing, name, description, breadcrumb, buttons, className, badge } = this.props;
-
-        let titleAndDescription;
-        if (isEditing) {
-            titleAndDescription = (
-                <div className="Header-title flex flex-column flex-full bordered rounded my1">
-                    <Input className="AdminInput text-bold border-bottom rounded-top h3" type="text" value={name} onChange={(e) => this.props.setItemAttributeFn("name", e.target.value)} />
-                    <Input className="AdminInput rounded-bottom h4" type="text" value={description} onChange={(e) => this.props.setItemAttributeFn("description", e.target.value)} placeholder="No description yet" />
-                </div>
-            );
-        } else {
-            if (name && description) {
-                titleAndDescription = (
-                    <TitleAndDescription
-                        title={name}
-                        description={description}
-                    />
-                );
-            } else {
-                titleAndDescription = (
-                    <div className="flex align-baseline">
-                        <h1 className="Header-title-name my1">{name}</h1> {breadcrumb}
-                    </div>
-                );
-            }
-        }
+        const { isEditing, name, description, buttons, badge } = this.props;
 
         return (
-            <div className={cx("QueryBuilder-section pt2 sm-pt2 flex align-center", className)}>
-                <div className={cx("px2 sm-px0 pt2 relative flex-full")}>
-                    { badge &&
-                        <div className="absolute top left ml2 sm-ml2">{badge}</div>
+            <div className="wrapper sm-py3 flex align-center">
+                <div className="flex-full">
+                    { isEditing
+                            ? (
+                                <div className="Header-title flex flex-column flex-full bordered rounded my1">
+                                    <Input className="AdminInput text-bold border-bottom rounded-top h3" type="text" value={name} onChange={(e) => this.props.setItemAttributeFn("name", e.target.value)} />
+                                    <Input className="AdminInput rounded-bottom h4" type="text" value={description} onChange={(e) => this.props.setItemAttributeFn("description", e.target.value)} placeholder="No description yet" />
+                                </div>
+
+                            )
+                            : (
+                                <div className="sm-ml1">
+                                    { badge && <span className="mb2">{badge}</span>}
+                                    <TitleAndDescription
+                                        title={name}
+                                        description={description}
+                                    />
+                                </div>
+                            )
                     }
-                    {titleAndDescription}
                 </div>
 
                 <div className="flex-align-right hide sm-show">
