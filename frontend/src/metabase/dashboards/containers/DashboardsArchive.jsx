a62ab78a549baff77945c63eda497f58e795744c
Atte Keinänen <atte.keinanen@gmail.com>
2017-04-27 11:06:31 -0700

    Add empty state ui to dashboards archive

--- frontend/src/metabase/dashboards/containers/DashboardsArchive.jsx
+++ frontend/src/metabase/dashboards/containers/DashboardsArchive.jsx
@@ -58,10 +58,6 @@ export class Dashboards extends Component {
             .value()
     }
 
-    updateSection = (section: ListFilterWidgetItem) => {
-        this.setState({section});
-    }
-
     render() {
         let {searchText} = this.state;
 
@@ -70,47 +66,61 @@ export class Dashboards extends Component {
         const filteredDashboards = isLoading ? [] : this.getFilteredDashboards();
         const noSearchResults = searchText !== "" && filteredDashboards.length === 0;
 
+        const headerWithBackContainer =
+            <div className="flex align-center pt4 pb1">
+                <HeaderWithBack name="Archive"/>
+            </div>
+
         return (
             <LoadingAndErrorWrapper
                 loading={isLoading}
                 className={cx("relative mx4", {"flex-full ": noDashboardsArchived})}
             >
-
-                <div>
-                    <div className="flex align-center pt4 pb1">
-                        <HeaderWithBack name="Archive"/>
-                    </div>
-                    <div className="flex align-center pb1">
-                        <SearchHeader
-                            searchText={searchText}
-                            setSearchText={(text) => this.setState({searchText: text})}
-                        />
-                    </div>
-                    { noSearchResults ?
-                        <div className="flex justify-center">
+                { noDashboardsArchived ?
+                    <div>
+                        {headerWithBackContainer}
+                        <div className="full flex justify-center" style={{marginTop: "75px"}}>
                             <EmptyState
-                                message={
-                                    <div className="mt4">
-                                        <h3 className="text-grey-5">No results found</h3>
-                                        <p className="text-grey-4">Try adjusting your filter to find what you’re
-                                            looking for.</p>
-                                    </div>
-                                }
-                                image="/app/img/empty_dashboard"
-                                imageClassName="mln2"
-                                smallDescription
+                                message={"No dashboards have been archived yet"}
+                                icon="viewArchive"
                             />
                         </div>
-                        : <div>
-                            { filteredDashboards.map((dashboard) =>
-                                <ArchivedItem key={dashboard.id} name={dashboard.name} type="dashboard" icon="dashboard"
-                                              isAdmin={true} onUnarchive={async () => {
-                                    await this.props.setArchived(dashboard.id, false);
-                                }}/>
-                            )}
+                    </div>
+                    : <div>
+                        {headerWithBackContainer}
+                        <div className="flex align-center pb1">
+                            <SearchHeader
+                                searchText={searchText}
+                                setSearchText={(text) => this.setState({searchText: text})}
+                            />
                         </div>
-                    }
-                </div>
+                        { noSearchResults ?
+                            <div className="flex justify-center">
+                                <EmptyState
+                                    message={
+                                        <div className="mt4">
+                                            <h3 className="text-grey-5">No results found</h3>
+                                            <p className="text-grey-4">Try adjusting your filter to find what you’re
+                                                looking for.</p>
+                                        </div>
+                                    }
+                                    image="/app/img/empty_dashboard"
+                                    imageClassName="mln2"
+                                    smallDescription
+                                />
+                            </div>
+                            : <div>
+                                { filteredDashboards.map((dashboard) =>
+                                    <ArchivedItem key={dashboard.id} name={dashboard.name} type="dashboard"
+                                                  icon="dashboard"
+                                                  isAdmin={true} onUnarchive={async () => {
+                                        await this.props.setArchived(dashboard.id, false);
+                                    }}/>
+                                )}
+                            </div>
+                        }
+                    </div>
+                }
             </LoadingAndErrorWrapper>
         );
     }
