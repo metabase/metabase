74a08a1c6b2e4a1d865dac69ad2210e1c3e0fb2d
Atte Keinänen <atte.keinanen@gmail.com>
2017-05-08 10:38:42 -0700

    Fix loading wrapper transparent background

--- frontend/src/metabase/dashboards/containers/Dashboards.jsx
+++ frontend/src/metabase/dashboards/containers/Dashboards.jsx
@@ -147,6 +147,7 @@ export class Dashboards extends Component {
                 style={{ backgroundColor: "#f9fbfc" }}
                 loading={isLoading}
                 className={cx("relative px4 full-height", {"flex flex-full flex-column": noDashboardsCreated})}
+                noBackground
             >
                 { modalOpen ? this.renderCreateDashboardModal() : null }
                 <div className="flex align-center pt4 pb1">
