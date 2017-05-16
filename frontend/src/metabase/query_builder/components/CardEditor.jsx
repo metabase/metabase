2b43da5e713e9f38b884d4f3160e47a22e12c590
Atte Keinänen <atte.keinanen@gmail.com>
2017-05-16 13:03:20 -0700

    New CardBuilder container for saved questions & a refactored header

---
+++ frontend/src/metabase/query_builder/components/CardEditor.jsx
@@ -0,0 +1,359 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+
+import AggregationWidget from './AggregationWidget.jsx';
+import BreakoutWidget from './BreakoutWidget.jsx';
+import DataSelector from './DataSelector.jsx';
+import ExtendedOptions from "./ExtendedOptions.jsx";
+import FilterList from './filters/FilterList.jsx';
+import FilterPopover from './filters/FilterPopover.jsx';
+import Icon from "metabase/components/Icon.jsx";
+import IconBorder from 'metabase/components/IconBorder.jsx';
+import PopoverWithTrigger from "metabase/components/PopoverWithTrigger.jsx";
+
+import Query from "metabase/lib/query";
+
+import cx from "classnames";
+import _ from "underscore";
+
+
+export default class CardEditor extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            expanded: true
+        };
+    }
+
+    static propTypes = {
+        databases: PropTypes.array,
+        datasetQuery: PropTypes.object.isRequired,
+        tableMetadata: PropTypes.object, // can't be required, sometimes null
+        isShowingDataReference: PropTypes.bool.isRequired,
+        setDatasetQuery: PropTypes.func.isRequired,
+        setDatabaseFn: PropTypes.func,
+        setSourceTableFn: PropTypes.func,
+        features: PropTypes.object,
+        supportMultipleAggregations: PropTypes.bool
+    };
+
+    static defaultProps = {
+        features: {
+            data: true,
+            filter: true,
+            aggregation: true,
+            breakout: true,
+            sort: true,
+            limit: true
+        },
+        supportMultipleAggregations: true
+    };
+
+    renderAdd(text, onClick, targetRefName) {
+        let className = "AddButton text-grey-2 text-bold flex align-center text-grey-4-hover cursor-pointer no-decoration transition-color";
+        if (onClick) {
+            return (
+                <a className={className} onClick={onClick}>
+                    { text && <span className="mr1">{text}</span> }
+                    {this.renderAddIcon(targetRefName)}
+                </a>
+            );
+        } else {
+            return (
+                <span className={className}>
+                    { text && <span className="mr1">{text}</span> }
+                    {this.renderAddIcon(targetRefName)}
+                </span>
+            );
+        }
+    }
+
+    renderAddIcon(targetRefName) {
+        return (
+            <IconBorder borderRadius="3px" ref={targetRefName}>
+                <Icon name="add" size={14} />
+            </IconBorder>
+        )
+    }
+
+    renderFilters() {
+        if (!this.props.features.filter) return;
+
+        let enabled;
+        let filterList;
+        let addFilterButton;
+
+        if (this.props.tableMetadata) {
+            enabled = true;
+
+            let filters = Query.getFilters(this.props.datasetQuery.query);
+            if (filters && filters.length > 0) {
+                filterList = (
+                    <FilterList
+                        filters={filters}
+                        tableMetadata={this.props.tableMetadata}
+                        removeFilter={this.props.removeQueryFilter}
+                        updateFilter={this.props.updateQueryFilter}
+                    />
+                );
+            }
+
+            if (Query.canAddFilter(this.props.datasetQuery.query)) {
+                addFilterButton = this.renderAdd((filterList ? null : "Add filters to narrow your answer"), null, "addFilterTarget");
+            }
+        } else {
+            enabled = false;
+            addFilterButton = this.renderAdd("Add filters to narrow your answer", null, "addFilterTarget");
+        }
+
+        return (
+            <div className={cx("Query-section", { disabled: !enabled })}>
+                <div className="Query-filters">
+                    {filterList}
+                </div>
+                <div className="mx2">
+                    <PopoverWithTrigger
+                        id="FilterPopover"
+                        ref="filterPopover"
+                        triggerElement={addFilterButton}
+                        triggerClasses="flex align-center"
+                        getTarget={() => this.refs.addFilterTarget}
+                        horizontalAttachments={["left"]}
+                    >
+                        <FilterPopover
+                            isNew={true}
+                            tableMetadata={this.props.tableMetadata || {}}
+                            customFields={Query.getExpressions(this.props.datasetQuery.query)}
+                            onCommitFilter={this.props.addQueryFilter}
+                            onClose={() => this.refs.filterPopover.close()}
+                        />
+                    </PopoverWithTrigger>
+                </div>
+            </div>
+        );
+    }
+
+    renderAggregation() {
+        const { datasetQuery: { query }, tableMetadata, supportMultipleAggregations } = this.props;
+
+        if (!this.props.features.aggregation) {
+            return;
+        }
+
+        // aggregation clause.  must have table details available
+        if (tableMetadata) {
+            let isBareRows = Query.isBareRows(query);
+            let aggregations = Query.getAggregations(query);
+
+            if (aggregations.length === 0) {
+                // add implicit rows aggregation
+                aggregations.push(["rows"]);
+            }
+
+            const canRemoveAggregation = aggregations.length > 1;
+
+            if (supportMultipleAggregations && !isBareRows) {
+                // Placeholder aggregation for showing the add button
+                aggregations.push([]);
+            }
+
+            let aggregationList = [];
+            for (const [index, aggregation] of aggregations.entries()) {
+                aggregationList.push(
+                    <AggregationWidget
+                        key={"agg"+index}
+                        aggregation={aggregation}
+                        tableMetadata={tableMetadata}
+                        customFields={Query.getExpressions(this.props.datasetQuery.query)}
+                        updateAggregation={(aggregation) => this.props.updateQueryAggregation(index, aggregation)}
+                        removeAggregation={canRemoveAggregation ? this.props.removeQueryAggregation.bind(null, index) : null}
+                        addButton={this.renderAdd(null)}
+                    />
+                );
+                if (aggregations[index + 1] != null && aggregations[index + 1].length > 0) {
+                    aggregationList.push(
+                        <span key={"and"+index} className="text-bold">and</span>
+                    );
+                }
+            }
+            return aggregationList
+        } else {
+            // TODO: move this into AggregationWidget?
+            return (
+                <div className="Query-section Query-section-aggregation disabled">
+                    <a className="QueryOption p1 flex align-center">Raw data</a>
+                </div>
+            );
+        }
+    }
+
+    renderBreakouts() {
+        const { datasetQuery: { query }, tableMetadata, features } = this.props;
+
+        if (!features.breakout) {
+            return;
+        }
+
+        const enabled = tableMetadata && tableMetadata.breakout_options.fields.length > 0;
+        const breakoutList = [];
+
+        if (enabled) {
+            const breakouts = Query.getBreakouts(query);
+
+            const usedFields = {};
+            for (const breakout of breakouts) {
+                usedFields[Query.getFieldTargetId(breakout)] = true;
+            }
+
+            const remainingFieldOptions = Query.getFieldOptions(tableMetadata.fields, true, tableMetadata.breakout_options.validFieldsFilter, usedFields);
+            if (remainingFieldOptions.count > 0 && (breakouts.length === 0 || breakouts[breakouts.length - 1] != null)) {
+                breakouts.push(null);
+            }
+
+            for (let i = 0; i < breakouts.length; i++) {
+                const breakout = breakouts[i];
+
+                if (breakout == null) {
+                    breakoutList.push(<span key="nullBreakout" className="ml1" />);
+                }
+
+                breakoutList.push(
+                    <BreakoutWidget
+                        key={"breakout"+i}
+                        className="View-section-breakout SelectionModule p1"
+                        fieldOptions={Query.getFieldOptions(tableMetadata.fields, true, tableMetadata.breakout_options.validFieldsFilter, _.omit(usedFields, breakout))}
+                        customFieldOptions={Query.getExpressions(query)}
+                        tableMetadata={tableMetadata}
+                        field={breakout}
+                        setField={(field) => this.props.updateQueryBreakout(i, field)}
+                        addButton={this.renderAdd(i === 0 ? "Add a grouping" : null)}
+                    />
+                );
+
+                if (breakouts[i + 1] != null) {
+                    breakoutList.push(
+                        <span key={"and"+i} className="text-bold">and</span>
+                    );
+                }
+            }
+        }
+
+        return (
+            <div className={cx("Query-section Query-section-breakout", { disabled: !enabled })}>
+                {breakoutList}
+            </div>
+        );
+    }
+
+    renderDataSection() {
+        return (
+            <div className={"GuiBuilder-section GuiBuilder-data flex align-center arrow-right"}>
+                <span className="GuiBuilder-section-label Query-label">Data</span>
+                { this.props.features.data ?
+                    <DataSelector
+                        ref="dataSection"
+                        includeTables={true}
+                        datasetQuery={this.props.datasetQuery}
+                        databases={this.props.databases}
+                        tables={this.props.tables}
+                        setDatabaseFn={this.props.setDatabaseFn}
+                        setSourceTableFn={this.props.setSourceTableFn}
+                        isInitiallyOpen={(!this.props.datasetQuery.database || !this.props.datasetQuery.query.source_table) && !this.props.isShowingTutorial}
+                    />
+                    :
+                    <span className="flex align-center px2 py2 text-bold text-grey">
+                        {this.props.tableMetadata && this.props.tableMetadata.display_name}
+                    </span>
+                }
+            </div>
+        );
+    }
+
+    renderFilterSection() {
+        if (!this.props.features.filter) {
+            return;
+        }
+
+        return (
+            <div className="GuiBuilder-section GuiBuilder-filtered-by flex align-center" ref="filterSection">
+                <span className="GuiBuilder-section-label Query-label">Filtered by</span>
+                {this.renderFilters()}
+            </div>
+        );
+    }
+
+    renderViewSection() {
+        const { features } = this.props;
+        if (!features.aggregation && !features.breakout) {
+            return;
+        }
+
+        return (
+            <div className="GuiBuilder-section GuiBuilder-view flex align-center px1 pr2" ref="viewSection">
+                <span className="GuiBuilder-section-label Query-label">View</span>
+                {this.renderAggregation()}
+            </div>
+        );
+    }
+
+    renderGroupedBySection() {
+        const { features } = this.props;
+        if (!features.aggregation && !features.breakout) {
+            return;
+        }
+
+        return (
+            <div className="GuiBuilder-section GuiBuilder-groupedBy flex align-center px1" ref="viewSection">
+                <span className="GuiBuilder-section-label Query-label">Grouped By</span>
+                {this.renderBreakouts()}
+            </div>
+        );
+    }
+
+    componentDidUpdate() {
+        const guiBuilder = ReactDOM.findDOMNode(this.refs.guiBuilder);
+        if (!guiBuilder) {
+            return;
+        }
+
+        // HACK: magic number "5" accounts for the borders between the sections?
+        let contentWidth = ["data", "filter", "view", "groupedBy","sortLimit"].reduce((acc, ref) => {
+            let node = ReactDOM.findDOMNode(this.refs[`${ref}Section`]);
+            return acc + (node ? node.offsetWidth : 0);
+        }, 0) + 5;
+        let guiBuilderWidth = guiBuilder.offsetWidth;
+
+        let expanded = (contentWidth < guiBuilderWidth);
+        if (this.state.expanded !== expanded) {
+            this.setState({ expanded });
+        }
+    }
+
+    render() {
+        const { datasetQuery, databases } = this.props;
+        const readOnly = datasetQuery.database != null && !_.findWhere(databases, { id: datasetQuery.database });
+        if (readOnly) {
+            return <div className="border-bottom border-med" />
+        }
+
+        return (
+            <div className={cx("GuiBuilder rounded shadowed", { "GuiBuilder--expand": this.state.expanded, disabled: readOnly })} ref="guiBuilder">
+                <div className="GuiBuilder-row flex">
+                    {this.renderDataSection()}
+                    {this.renderFilterSection()}
+                </div>
+                <div className="GuiBuilder-row flex flex-full">
+                    {this.renderViewSection()}
+                    {this.renderGroupedBySection()}
+                    <div className="flex-full"></div>
+                    {this.props.children}
+                    <ExtendedOptions
+                        {...this.props}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
---
+++ frontend/src/metabase/query_builder/containers/CardBuilder.jsx
@@ -0,0 +1,244 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { connect } from "react-redux";
+
+import cx from "classnames";
+import _ from "underscore";
+
+import { loadTableAndForeignKeys } from "metabase/lib/table";
+import { isPK, isFK } from "metabase/lib/types";
+
+import QueryBuilderTutorial from "metabase/tutorial/QueryBuilderTutorial.jsx";
+
+import QueryHeader from "../components/QueryHeader.jsx";
+import GuiQueryEditor from "../components/GuiQueryEditor.jsx";
+import NativeQueryEditor from "../components/NativeQueryEditor.jsx";
+import QueryVisualization from "../components/QueryVisualization.jsx";
+import DataReference from "../components/dataref/DataReference.jsx";
+import TagEditorSidebar from "../components/template_tags/TagEditorSidebar.jsx";
+import SavedQuestionIntroModal from "../components/SavedQuestionIntroModal.jsx";
+import ActionsWidget from "../components/ActionsWidget.jsx";
+
+import title from "metabase/hoc/Title";
+
+import {
+    getCard,
+    getOriginalCard,
+    getLastRunCard,
+    getQueryResult,
+    getParameterValues,
+    getIsDirty,
+    getIsNew,
+    getIsObjectDetail,
+    getTables,
+    getTableMetadata,
+    getTableForeignKeys,
+    getTableForeignKeyReferences,
+    getUiControls,
+    getParameters,
+    getDatabaseFields,
+    getSampleDatasetId,
+    getNativeDatabases,
+    getIsRunnable,
+    getIsResultDirty,
+    getMode,
+} from "../selectors";
+
+import { getMetadata, getDatabasesList } from "metabase/selectors/metadata";
+
+import { getUserIsAdmin } from "metabase/selectors/user";
+
+import * as actions from "../actions";
+import { push } from "react-router-redux";
+
+import { MetabaseApi } from "metabase/services";
+import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
+import CardHeader from "metabase/query_builder/components/CardHeader";
+import CardEditor from "metabase/query_builder/components/CardEditor";
+
+function cellIsClickable(queryResult, rowIndex, columnIndex) {
+    if (!queryResult) return false;
+
+    // lookup the coldef and cell value of the cell we are curious about
+    var coldef = queryResult.data.cols[columnIndex];
+
+    if (!coldef || !coldef.special_type) return false;
+
+    return (coldef.table_id != null && (isPK(coldef.special_type) || (isFK(coldef.special_type) && coldef.target)));
+}
+
+function autocompleteResults(card, prefix) {
+    let databaseId = card && card.dataset_query && card.dataset_query.database;
+    let apiCall = MetabaseApi.db_autocomplete_suggestions({
+        dbId: databaseId,
+        prefix: prefix
+    });
+    return apiCall;
+}
+
+const mapStateToProps = (state, props) => {
+    return {
+        isAdmin:                   getUserIsAdmin(state, props),
+        fromUrl:                   props.location.query.from,
+
+        mode:                      getMode(state),
+
+        card:                      getCard(state),
+        originalCard:              getOriginalCard(state),
+        lastRunCard:               getLastRunCard(state),
+
+        parameterValues:           getParameterValues(state),
+
+        databases:                 getDatabasesList(state),
+        nativeDatabases:           getNativeDatabases(state),
+        tables:                    getTables(state),
+        tableMetadata:             getTableMetadata(state),
+        metadata:                  getMetadata(state),
+
+        tableForeignKeys:          getTableForeignKeys(state),
+        tableForeignKeyReferences: getTableForeignKeyReferences(state),
+
+        result:                    getQueryResult(state),
+
+        isDirty:                   getIsDirty(state),
+        isNew:                     getIsNew(state),
+        isObjectDetail:            getIsObjectDetail(state),
+
+        uiControls:                getUiControls(state),
+        parameters:                getParameters(state),
+        databaseFields:            getDatabaseFields(state),
+        sampleDatasetId:           getSampleDatasetId(state),
+
+        isShowingDataReference:    state.qb.uiControls.isShowingDataReference,
+        isShowingTutorial:         state.qb.uiControls.isShowingTutorial,
+        isEditing:                 state.qb.uiControls.isEditing,
+        isRunning:                 state.qb.uiControls.isRunning,
+        isRunnable:                getIsRunnable(state),
+        isResultDirty:             getIsResultDirty(state),
+
+        loadTableAndForeignKeysFn: loadTableAndForeignKeys,
+        autocompleteResultsFn:     (prefix) => autocompleteResults(state.qb.card, prefix),
+        cellIsClickableFn:         (rowIndex, columnIndex) => cellIsClickable(state.qb.queryResult, rowIndex, columnIndex)
+    }
+}
+
+const getURL = (location) =>
+location.pathname + location.search + location.hash;
+
+
+const mapDispatchToProps = {
+    ...actions,
+    onChangeLocation: push
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+@title(({ card }) => (card && card.name) || "Question")
+export default class CardBuilder extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        // TODO: React tells us that forceUpdate() is not the best thing to use, so ideally we can find a different way to trigger this
+        this.forceUpdateDebounced = _.debounce(this.forceUpdate.bind(this), 400);
+
+        this.state = {
+            legacy: true
+        }
+    }
+
+    componentWillMount() {
+        this.props.initializeQB(this.props.location, this.props.params);
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.uiControls.isShowingDataReference !== this.props.uiControls.isShowingDataReference ||
+            nextProps.uiControls.isShowingTemplateTagsEditor !== this.props.uiControls.isShowingTemplateTagsEditor) {
+            // when the data reference is toggled we need to trigger a rerender after a short delay in order to
+            // ensure that some components are updated after the animation completes (e.g. card visualization)
+            window.setTimeout(this.forceUpdateDebounced, 300);
+        }
+
+        // if (nextProps.location.action === "POP" && getURL(nextProps.location) !== getURL(this.props.location)) {
+        //     this.props.popState(nextProps.location);
+        // } else if (this.props.location.hash !== "#?tutorial" && nextProps.location.hash === "#?tutorial") {
+        //     this.props.initializeQB(nextProps.location, nextProps.params);
+        // } else if (getURL(nextProps.location) === "/question" && getURL(this.props.location) !== "/question") {
+        //     this.props.initializeQB(nextProps.location, nextProps.params);
+        // }
+    }
+
+    componentDidUpdate() {
+        let viz = ReactDOM.findDOMNode(this.refs.viz);
+        if (viz) {
+            viz.style.opacity = 1.0;
+        }
+    }
+
+    componentWillUnmount() {
+        // cancel the query if one is running
+        this.props.cancelQuery();
+
+        window.removeEventListener("resize", this.handleResize);
+    }
+
+    // When the window is resized we need to re-render, mainly so that our visualization pane updates
+    // Debounce the function to improve resizing performance.
+    handleResize = (e) => {
+        this.forceUpdateDebounced();
+        let viz = ReactDOM.findDOMNode(this.refs.viz);
+        if (viz) {
+            viz.style.opacity = 0.2;
+        }
+    };
+
+    render() {
+        const { card, isDirty, databases, uiControls, mode } = this.props;
+        const showDrawer = uiControls.isShowingDataReference || uiControls.isShowingTemplateTagsEditor;
+        const ModeFooter = mode && mode.ModeFooter;
+
+        const isInitializing = !card || !databases;
+
+        return (
+            <LoadingAndErrorWrapper loading={isInitializing} noBackground={true} showSpinner={false}>
+                { () =>
+                    <div className="flex-full flex relative">
+                        <div className={cx("QueryBuilder flex flex-column bg-white spread", {"QueryBuilder--showSideDrawer": showDrawer})}>
+                            <div id="react_qb_header">
+                                <CardHeader {...this.props}/>
+                            </div>
+
+                            <div id="react_qb_editor" className="z2 hide sm-show">
+                                <CardEditor
+                                    {...this.props}
+                                    datasetQuery={card && card.dataset_query}
+                                />
+                            </div>
+
+                            <div ref="viz" id="react_qb_viz" className="flex z1" style={{ "transition": "opacity 0.25s ease-in-out" }}>
+                                <QueryVisualization {...this.props} className="full wrapper mb2 z1" />
+                            </div>
+
+                            { ModeFooter &&
+                            <ModeFooter {...this.props} className="flex-no-shrink" />
+                            }
+                        </div>
+
+                        <div className={cx("SideDrawer hide sm-show", { "SideDrawer--show": showDrawer })}>
+                            { uiControls.isShowingDataReference &&
+                            <DataReference {...this.props} onClose={() => this.props.toggleDataReference()} />
+                            }
+
+                            { uiControls.isShowingTemplateTagsEditor &&
+                            <TagEditorSidebar {...this.props} onClose={() => this.props.toggleTemplateTagsEditor()} />
+                            }
+                        </div>
+                    </div>
+                }
+            </LoadingAndErrorWrapper>
+        )
+    }
+}
