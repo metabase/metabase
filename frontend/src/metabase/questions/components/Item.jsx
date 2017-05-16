4cf4defe5a12be508b1f442100651bc251d34cc4
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-05-15 18:09:31 -0700

    tweak color classes

--- frontend/src/metabase/questions/components/Item.jsx
+++ frontend/src/metabase/questions/components/Item.jsx
@@ -133,8 +133,8 @@ const ItemBody = pure(({ entity, id, name, description, labels, favorite, collec
                 <Tooltip tooltip={favorite ? "Unfavorite" : "Favorite"}>
                     <Icon
                         className={cx(
-                            "flex cursor-pointer text-brand-hover transition-color",
-                            {"hover-child text-light-blue": !favorite},
+                            "flex cursor-pointer",
+                            {"hover-child text-light-blue text-brand-hover": !favorite},
                             {"visible text-gold": favorite}
                         )}
                         name={favorite ? "star" : "staroutline"}
