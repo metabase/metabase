b4247e9b08f2c479759f29a82731a2c21e0a885e
Joel Wilsson <joel.wilsson@gmail.com>
2017-05-18 01:13:48 +0200

    Re-enable :foreign-keys for Drill and Spark SQL, break tests

--- src/metabase/driver/sparksql.clj
+++ src/metabase/driver/sparksql.clj
@@ -125,7 +125,7 @@
                          :execute-query execute-query
                          :features (constantly #{:basic-aggregations
                                                  :standard-deviation-aggregations
-                                                 ;:foreign-keys
+                                                 :foreign-keys
                                                  :expressions
                                                  :expression-aggregations
                                                  :native-parameters})})
