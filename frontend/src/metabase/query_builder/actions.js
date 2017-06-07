1581fc24ffb1c667373d636ade4d097822a41bcd
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-06-07 16:40:42 -0700

    don't initialize with a database

--- frontend/src/metabase/query_builder/actions.js
+++ frontend/src/metabase/query_builder/actions.js
@@ -243,7 +243,7 @@ export const initializeQB = createThunkAction(INITIALIZE_QB, (location, params) 
             // we are starting a new/empty card
             const databaseId = (options.db) ? parseInt(options.db) : (databasesList && databasesList.length > 0 && databasesList[0].id);
 
-            card = startNewCard("query", databaseId);
+            card = startNewCard("query");
 
             // initialize parts of the query based on optional parameters supplied
             if (options.table != undefined && card.dataset_query.query) {
