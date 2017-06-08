08740c1b254b26651bd42194c98c4e90b9421b21
Cam Saül <cammsaul@gmail.com>
2017-06-07 17:06:42 -0700

    Add some tests :wrench:

--- test/metabase/driver_test.clj
+++ test/metabase/driver_test.clj
@@ -16,3 +16,23 @@
 
 (expect true  (driver/driver-supports? (TestDriver.) :a))
 (expect false (driver/driver-supports? (TestDriver.) :b))
+
+;; values->base-type
+(expect
+  :type/Text
+  (driver/values->base-type ["A" "B" "C"]))
+
+;; should ignore nils
+(expect
+  :type/Text
+  (driver/values->base-type [nil nil "C"]))
+
+;; should pick base-type of most common class
+(expect
+  :type/Text
+  (driver/values->base-type ["A" 100 "C"]))
+
+;; should fall back to :type/* if no better type is found
+(expect
+  :type/*
+  (driver/values->base-type [(Object.)]))
