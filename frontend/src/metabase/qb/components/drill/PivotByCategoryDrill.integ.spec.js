1ecc6ada2d7a1f9334c9b3a50c015d1583d4a5a0
Atte Keinänen <atte.keinanen@gmail.com>
2017-06-15 15:49:27 -0700

    Move the integrated test boilerplate to __support__/integrated_tests

--- frontend/src/metabase/qb/components/drill/PivotByCategoryDrill.integ.spec.js
+++ frontend/src/metabase/qb/components/drill/PivotByCategoryDrill.integ.spec.js
@@ -1,47 +1,12 @@
 /* @flow weak */
 
-import { BackendResource } from "../../../../../test/e2e/support/backend.js";
-import api from "metabase/lib/api";
-import { SessionApi } from "metabase/services";
-import { METABASE_SESSION_COOKIE } from "metabase/lib/cookies";
 import { DATABASE_ID, ORDERS_TABLE_ID, metadata } from "metabase/__support__/sample_dataset_fixture";
 import Question from "metabase-lib/lib/Question";
-
-var loginSession = null;
-api._makeRequest = async (method, url, headers, body, data, options) => {
-    const headersWithSessionCookie = {
-        ...headers,
-        ...(loginSession ? {"Cookie": `${METABASE_SESSION_COOKIE}=${loginSession.id}`} : {})
-    }
-
-    const fetchOptions = {
-        credentials: "include",
-        method,
-        headers: new Headers(headersWithSessionCookie),
-        ...(body ? {body} : {})
-    };
-
-    const result = await fetch(api.basename + url, fetchOptions);
-    if (result.status >= 200 && result.status <= 299) {
-        return result.json();
-    } else {
-        throw { status: result.status, data: result.json() }
-    }
-}
-async function login() {
-    loginSession = await SessionApi.create({ email: "bob@metabase.com", password: "12341234"});
-}
-
-let server = BackendResource.get({});
-api.basename = server.host;
-
-// TODO: How to have the high timeout interval only for integration tests?
-// or even better, just for the setup/teardown of server process?
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
+import { login, startServer, stopServer } from "metabase/__support__/integrated_tests";
 
 describe("PivotByCategoryDrill", () => {
     beforeAll(async () => {
-        await BackendResource.start(server);
+        await startServer();
         await login();
     })
 
@@ -59,7 +24,6 @@ describe("PivotByCategoryDrill", () => {
     })
 
     afterAll(async () => {
-        // For making test running more FUN
-        // await BackendResource.stop(server);
+        await stopServer();
     })
 });
