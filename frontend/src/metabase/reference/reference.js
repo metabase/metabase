33a037d20db1dffcd1b05b6e4f413fb8f791cae8
Sameer Al-Sakran <sameer@expa.com>
2017-06-16 17:53:09 -0700

    re-organize constants

--- frontend/src/metabase/reference/reference.js
+++ frontend/src/metabase/reference/reference.js
@@ -12,6 +12,18 @@ import MetabaseAnalytics from 'metabase/lib/analytics';
 import { GettingStartedApi } from "metabase/services";
 
 const FETCH_GUIDE = "metabase/reference/FETCH_GUIDE";
+const SET_ERROR = "metabase/reference/SET_ERROR";
+const CLEAR_ERROR = "metabase/reference/CLEAR_ERROR";
+const START_LOADING = "metabase/reference/START_LOADING";
+const END_LOADING = "metabase/reference/END_LOADING";
+const START_EDITING = "metabase/reference/START_EDITING";
+const END_EDITING = "metabase/reference/END_EDITING";
+const EXPAND_FORMULA = "metabase/reference/EXPAND_FORMULA";
+const COLLAPSE_FORMULA = "metabase/reference/COLLAPSE_FORMULA";
+const SHOW_DASHBOARD_MODAL = "metabase/reference/SHOW_DASHBOARD_MODAL";
+const HIDE_DASHBOARD_MODAL = "metabase/reference/HIDE_DASHBOARD_MODAL";
+
+
 export const fetchGuide = createThunkAction(FETCH_GUIDE, (reload = false) => {
     return async (dispatch, getState) => {
         const requestStatePath = ["reference", 'guide'];
@@ -31,39 +43,29 @@ export const fetchGuide = createThunkAction(FETCH_GUIDE, (reload = false) => {
     };
 });
 
-const SET_ERROR = "metabase/reference/SET_ERROR";
 export const setError = createAction(SET_ERROR);
 
-const CLEAR_ERROR = "metabase/reference/CLEAR_ERROR";
 export const clearError = createAction(CLEAR_ERROR);
 
-const START_LOADING = "metabase/reference/START_LOADING";
 export const startLoading = createAction(START_LOADING);
 
-const END_LOADING = "metabase/reference/END_LOADING";
 export const endLoading = createAction(END_LOADING);
 
-const START_EDITING = "metabase/reference/START_EDITING";
 export const startEditing = createAction(START_EDITING, () => {
     MetabaseAnalytics.trackEvent('Data Reference', 'Started Editing');
 });
 
-const END_EDITING = "metabase/reference/END_EDITING";
 export const endEditing = createAction(END_EDITING, () => {
     MetabaseAnalytics.trackEvent('Data Reference', 'Ended Editing');
 });
 
-const EXPAND_FORMULA = "metabase/reference/EXPAND_FORMULA";
 export const expandFormula = createAction(EXPAND_FORMULA);
 
-const COLLAPSE_FORMULA = "metabase/reference/COLLAPSE_FORMULA";
 export const collapseFormula = createAction(COLLAPSE_FORMULA);
 
 //TODO: consider making an app-wide modal state reducer and related actions
-const SHOW_DASHBOARD_MODAL = "metabase/reference/SHOW_DASHBOARD_MODAL";
 export const showDashboardModal = createAction(SHOW_DASHBOARD_MODAL);
 
-const HIDE_DASHBOARD_MODAL = "metabase/reference/HIDE_DASHBOARD_MODAL";
 export const hideDashboardModal = createAction(HIDE_DASHBOARD_MODAL);
 
 
