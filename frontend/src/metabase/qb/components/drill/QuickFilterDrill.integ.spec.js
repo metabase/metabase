f4ddeb6ecfdbbdcbdf469b42adfef325d544baf7
Atte Keinänen <atte.keinanen@gmail.com>
2017-06-21 12:57:38 -0700

    Add stubs for mode actions and drills

---
+++ frontend/src/metabase/qb/components/drill/QuickFilterDrill.integ.spec.js
@@ -0,0 +1,10 @@
+describe("QuickFilterDrill", () => {
+    it("should return correct operators for various inputs", () => {
+        // This could also be in a separate unit test file
+        pending();
+    })
+    it("should produce correct query results for various inputs", () => {
+        // Would be nice to run an integration test here to make sure that the resulting MBQL is valid and runnable
+        pending();
+    })
+})
