194d17f2fc47b58367b502f8eecbb2a38570eaa5
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-07-05 13:18:26 -0700

    remove toggle

--- frontend/src/metabase/admin/settings/components/SettingsAuthenticationForm.jsx
+++ frontend/src/metabase/admin/settings/components/SettingsAuthenticationForm.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-import Button from 'metabase/components/Button'
-import Toggle from 'metabase/components/Toggle'
-
 class SettingsAuthenticationForm extends Component {
     render () {
         return (
@@ -12,12 +9,7 @@ class SettingsAuthenticationForm extends Component {
                     <div className="bordered rounded shadowed bg-white p4">
                         <h2>Sign in with Google</h2>
                         <p>Allows users with existing Metabase accounts to login with a Google account that matches their email address in addition to their Metabase username and password.</p>
-                        <div className="flex align-center mt3">
-                            <Toggle />
-                            <div className="ml-auto">
-                                <Link className="Button" to="/admin/settings/authentication/google">Configure</Link>
-                            </div>
-                        </div>
+                        <Link className="Button" to="/admin/settings/authentication/google">Configure</Link>
                     </div>
                 </li>
 
@@ -25,12 +17,7 @@ class SettingsAuthenticationForm extends Component {
                     <div className="bordered rounded shadowed bg-white p4">
                         <h2>LDAP</h2>
                         <p>Allows users within your LDAP directory to log in to Metabase with their LDAP credentials, and allows automatic mapping of LDAP groups to Metabase groups.</p>
-                        <div className="flex align-center mt3">
-                            <Toggle />
-                            <div className="ml-auto">
-                                <Link className="Button" to="/admin/settings/authentication/ldap">Configure</Link>
-                            </div>
-                        </div>
+                        <Link className="Button" to="/admin/settings/authentication/ldap">Configure</Link>
                     </div>
                 </li>
             </ul>
