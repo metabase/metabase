594f600e9fdbacc433a303380cad196e76456c70
Atte Keinänen <atte.keinanen@gmail.com>
2017-07-05 15:55:59 -0700

    See if the tests pass after increasing waitForActions timeout

--- frontend/src/metabase/__support__/integrated_tests.js
+++ frontend/src/metabase/__support__/integrated_tests.js
@@ -177,7 +177,7 @@ const testStoreEnhancer = (createStore, history) => {
              *
              * Convenient in tests for waiting specific actions to be executed after mounting a React container.
              */
-            waitForActions: (actionTypes, {timeout = 2000} = {}) => {
+            waitForActions: (actionTypes, {timeout = 10000} = {}) => {
                 actionTypes = Array.isArray(actionTypes) ? actionTypes : [actionTypes]
 
                 const allActionsAreTriggered = () => _.every(actionTypes, actionType =>
