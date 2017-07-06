403ff49aa39ad69a7176f2b85f115766691ac514
Atte Keinänen <atte.keinanen@gmail.com>
2017-07-06 11:16:45 -0700

    Integrated test infrastructure + simple QB test case [ci e2e]

--- frontend/src/metabase/query_builder/components/QueryVisualization.jsx
+++ frontend/src/metabase/query_builder/components/QueryVisualization.jsx
@@ -222,7 +222,7 @@ export default class QueryVisualization extends Component {
     }
 }
 
-const VisualizationEmptyState = ({showTutorialLink}) =>
+export const VisualizationEmptyState = ({showTutorialLink}) =>
     <div className="flex full layout-centered text-grey-1 flex-column">
         <h1>If you give me some data I can show you something cool. Run a Query!</h1>
         { showTutorialLink && <Link to={Urls.question(null, "?tutorial")} className="link cursor-pointer my2">How do I use this thing?</Link> }
