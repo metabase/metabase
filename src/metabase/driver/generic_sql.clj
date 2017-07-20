b5ab1c59692c4ed099f3ec96a35195812a7bcfaf
Cam Saül <cammsaul@gmail.com>
2017-07-19 17:11:35 -0700

    Lint fix :shirt: [ci drivers]

--- src/metabase/driver/generic_sql.clj
+++ src/metabase/driver/generic_sql.clj
@@ -189,7 +189,7 @@
    Optionally specify SEPERATOR-STYLE, which defaults to `:url` (e.g. `?a=1&b=2`). You may instead set it to `:semicolon`,
    which will separate different options with semicolons instead (e.g. `;a=1;b=2`). (While most drivers require the former
    style, some require the latter.)"
-  {:arglists '([connection-spec] [connection-spec details])}
+  {:arglists '([connection-spec] [connection-spec details & {:keys [seperator-style]}])}
   ;; single arity provided for cases when `connection-spec` is built by applying simple transformations to `details`
   ([connection-spec]
    (handle-additional-options connection-spec connection-spec))
