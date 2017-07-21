164f67a5faa0d0a6fff04261619d1efcaf4ec86d
Atte Keinänen <atte.keinanen@gmail.com>
2017-07-21 15:20:54 -0700

    Fix Lint and Flow errors, loosen summarize field criteria

--- frontend/src/metabase/qb/components/drill/SummarizeColumnByTimeDrill.js
+++ frontend/src/metabase/qb/components/drill/SummarizeColumnByTimeDrill.js
@@ -4,7 +4,11 @@ import React from "react";
 
 import StructuredQuery from "metabase-lib/lib/queries/StructuredQuery";
 import { getFieldRefFromColumn } from "metabase/qb/lib/actions";
-import { isNumeric, isDate, getAggregator, isCompatibleAggregatorForField } from "metabase/lib/schema_metadata";
+import {
+    isDate,
+    getAggregator,
+    isCompatibleAggregatorForField
+} from "metabase/lib/schema_metadata";
 import { capitalize } from "metabase/lib/formatting";
 
 import type {
@@ -20,11 +24,7 @@ export default ({ question, clicked }: ClickActionProps): ClickAction[] => {
 
     const dateField = query.table().fields.filter(isDate)[0];
     if (
-        !dateField ||
-        !clicked ||
-        !clicked.column ||
-        clicked.value !== undefined ||
-        !isNumeric(clicked.column)
+        !dateField || !clicked || !clicked.column || clicked.value !== undefined
     ) {
         return [];
     }
@@ -32,7 +32,8 @@ export default ({ question, clicked }: ClickActionProps): ClickAction[] => {
 
     return ["sum", "count"]
         .map(getAggregator)
-        .filter((aggregator) => isCompatibleAggregatorForField(aggregator, column))
+        .filter(aggregator =>
+            isCompatibleAggregatorForField(aggregator, column))
         .map(aggregator => ({
             name: "summarize-by-time",
             section: "sum",
