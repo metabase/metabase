8290a9b087ca329987d83daeef3a33bbc6795403
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-08-02 13:53:57 -0700

    flip SimpleStat order

--- frontend/src/metabase/xray/SimpleStat.jsx
+++ frontend/src/metabase/xray/SimpleStat.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import Tooltip from 'metabase/components/Tooltip'
 import Icon from 'metabase/components/Icon'
 
+import { formatValue } from "metabase/lib/formatting";
+
 const SimpleStat = ({ stat, showDescription }) =>
     <div>
-        { /* call toString to ensure that values like true / false show up */ }
-        <h1>{stat.value.toString()}</h1>
         <div className="flex align-center">
-            <h3 className="mr1">{stat.label}</h3>
+            <h3 className="mr1 text-grey-4">{stat.label}</h3>
             { showDescription && (
                 <Tooltip tooltip={stat.description}>
                     <Icon name='infooutlined' />
                 </Tooltip>
             )}
         </div>
+        { /* call toString to ensure that values like true / false show up */ }
+        <h1 className="my1">
+            {formatValue(stat.value.toString(), {})}
+        </h1>
     </div>
 
 export default SimpleStat
