b6f398c40d6d9a1332b7fd4b30d3f7344ba2c2cf
Sameer Al-Sakran <sameer@expa.com>
2017-08-03 17:34:54 -0700

    include redux store size as well

--- frontend/src/metabase/admin/TroubleshootingApp.jsx
+++ frontend/src/metabase/admin/TroubleshootingApp.jsx
@@ -5,8 +5,44 @@ import { getTroubleshootingInfo } from 'metabase/admin/admin'
 
 import LoadingAndErrorWrapper from 'metabase/components/LoadingAndErrorWrapper'
 
+
+function roughSizeOfObject( object ) {
+
+    var objectList = [];
+    var stack = [ object ];
+    var bytes = 0;
+
+    while ( stack.length ) {
+        var value = stack.pop();
+
+        if ( typeof value === 'boolean' ) {
+            bytes += 4;
+        }
+        else if ( typeof value === 'string' ) {
+            bytes += value.length * 2;
+        }
+        else if ( typeof value === 'number' ) {
+            bytes += 8;
+        }
+        else if
+        (
+            typeof value === 'object'
+            && objectList.indexOf( value ) === -1
+        )
+        {
+            objectList.push( value );
+
+            for( var i in value ) {
+                stack.push( value[ i ] );
+            }
+        }
+    }
+    return bytes;
+}
+
 const mapStateToProps = state => ({
-    troubleShootingInfo: state.admin.troubleshooting
+    troubleShootingInfo: state.admin.troubleshooting,
+    reduxStoreSize: roughSizeOfObject(state)
 })
 
 const mapDispatchToProps = {
@@ -39,6 +75,9 @@ class TroubleshootingApp extends Component {
                             </li>)
                         }
                         </ul>
+
+                        <h2>Client Info</h2>
+                        <p> Metadata Cache Size (bytes) : {this.props.reduxStoreSize} </p>
                     </div>
                 }
             </LoadingAndErrorWrapper>
