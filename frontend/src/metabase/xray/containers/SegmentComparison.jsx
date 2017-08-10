ec5b6bedfe66ce6ce0f2cbd9377a0600fe0498dc
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-08-10 10:35:10 -0700

    segment/segment

--- frontend/src/metabase/xray/containers/SegmentComparison.jsx
+++ frontend/src/metabase/xray/containers/SegmentComparison.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { fetchSegmentComparison } from 'metabase/xray/xray'
 import { getComparison } from 'metabase/xray/selectors'
 
+import LoadingAndErrorWrapper from 'metabase/components/LoadingAndErrorWrapper'
 import XRayComparison from 'metabase/xray/components/XRayComparison'
 
 const mapStateToProps = state => ({
@@ -17,11 +18,50 @@ const mapDispatchToProps = {
 class SegmentComparison extends Component {
     componentWillMount () {
         const { segmentId1, segmentId2 } = this.props.params
-        this.props.fetchSegmentTableComparison(segmentId1, segmentId2)
+        this.props.fetchSegmentComparison(segmentId1, segmentId2)
     }
 
     render () {
-        return <XRayComparison comparison={this.props.comparison} />
+        const { comparison } = this.props
+        return (
+            <LoadingAndErrorWrapper
+                loading={!comparison}
+                noBackground
+            >
+                { () =>
+
+                    <XRayComparison
+                        fields={
+                            Object.values(comparison.constituents[0].constituents).map(c =>
+                                c.field
+                            )
+                        }
+                        comparisonFields={[
+                            'distance',
+                            'threshold',
+                            'entropy',
+                            'count',
+                            'histogram',
+                            'uniqueness',
+                            'nil%',
+                        ]}
+                        comparison={comparison.comparison}
+                        itemA={{
+                            name: comparison.constituents[0].features.segment.name,
+                            constituents: comparison.constituents[0].constituents,
+                            itemType: 'segment',
+                            id: comparison.constituents[0].features.segment.id,
+                        }}
+                        itemB={{
+                            name: comparison.constituents[1].features.segment.name,
+                            constituents: comparison.constituents[1].constituents,
+                            itemType: 'segment',
+                            id: comparison.constituents[0].features.segment.id,
+                        }}
+                    />
+                }
+            </LoadingAndErrorWrapper>
+        )
     }
 }
 
