d59a8b93115c29ef524eb01c8f0ad40455723ecf
Cam Saul <cammsaul@gmail.com>
2017-08-22 11:46:05 -0700

    Better Getting Started Guide API endpoint

--- src/metabase/api/getting_started.clj
+++ src/metabase/api/getting_started.clj
@@ -1,11 +1,19 @@
 (ns metabase.api.getting-started
+  "/api/getting_started endpoints."
   (:require [compojure.core :refer [GET]]
-            [medley.core :as m]
             [metabase.api.common :as api]
             [metabase.models
+             [dashboard :refer [Dashboard]]
              [interface :as mi]
-             [setting :refer [defsetting]]]
-            [toucan.db :as db]))
+             [metric :refer [Metric]]
+             [metric-important-field :refer [MetricImportantField]]
+             [segment :refer [Segment]]
+             [setting :refer [defsetting]]
+             [table :refer [Table]]]
+            [metabase.util :as u]
+            [toucan
+             [db :as db]
+             [hydrate :refer [hydrate]]]))
 
 (defsetting getting-started-things-to-know
   "'Some things to know' text field for the Getting Started guide.")
@@ -17,25 +25,51 @@
   "Email of somebody users can contact for help in the Getting Started guide.")
 
 
+(defn- important-metrics
+  "Metrics that should be shown in the Getting Started Guide, that are allowed to be viewed by the current user.
+   Includes hydrated important fields for each Metric. These are recorded via the intermediate MetricImportantField
+   model, which is basically a many-to-many table."
+  []
+  (let [metrics (filter mi/can-read? (db/select Metric, :show_in_getting_started true, {:order-by [:%lower.name]}))
+        metric-id->important-fields (when (seq metrics)
+                                      (as-> (db/select [MetricImportantField :field_id :metric_id]
+                                              :metric_id [:in (map u/get-id metrics)]) <>
+                                        (hydrate <> :field)
+                                        (group-by :metric_id <>)))]
+    (for [metric metrics]
+      ;; add the important fields to each metric; unwrap them so we just return the Fields instead of entire
+      ;; MetricImportantField objects
+      (assoc metric :fields (for [important-field (metric-id->important-fields (u/get-id metric))]
+                              (:field important-field))))))
+
+(defn- important-tables
+  "Tables that should be shown in the Getting Started Guide, that are allowed to be viewed by the current user."
+  []
+  (filter mi/can-read? (db/select Table, :show_in_getting_started true, {:order-by [:%lower.name]})))
+
+(defn- important-segments
+  "Segments that should be shown in the Getting Started Guide, that are allowed to be viewed by the current user."
+  []
+  (filter mi/can-read? (db/select Segment, :show_in_getting_started true, {:order-by [:%lower.name]})))
+
+(defn- most-important-dashboard
+  "The 'most important Dashboard' of the application, if its allowed to be viewed by the current user."
+  []
+  (when-let [dashboard (Dashboard :show_in_getting_started true)]
+    (when (mi/can-read? dashboard)
+      dashboard)))
+
+
 (api/defendpoint GET "/"
   "Fetch basic info for the Getting Started guide."
   []
-  (let [metric-ids  (map :id (filter mi/can-read? (db/select ['Metric :table_id :id]     :show_in_getting_started true, {:order-by [:%lower.name]})))
-        table-ids   (map :id (filter mi/can-read? (db/select ['Table :db_id :schema :id] :show_in_getting_started true, {:order-by [:%lower.name]})))
-        segment-ids (map :id (filter mi/can-read? (db/select ['Segment :table_id :id]    :show_in_getting_started true, {:order-by [:%lower.name]})))]
-    {:things_to_know           (getting-started-things-to-know)
-     :contact                  {:name  (getting-started-contact-name)
-                                :email (getting-started-contact-email)}
-     :most_important_dashboard (when-let [dashboard (db/select-one ['Dashboard :id] :show_in_getting_started true)]
-                                 (when (mi/can-read? dashboard)
-                                   (:id dashboard)))
-     :important_metrics        metric-ids
-     :important_tables         table-ids
-     :important_segments       segment-ids
-     ;; A map of metric_id -> sequence of important field_ids
-     :metric_important_fields  (m/map-vals (partial map :field_id)
-                                           (group-by :metric_id (when (seq metric-ids)
-                                                                  (db/select ['MetricImportantField :field_id :metric_id]
-                                                                    :metric_id [:in metric-ids]))))}))
+  {:things_to_know           (getting-started-things-to-know)
+   :contact                  {:name  (getting-started-contact-name)
+                              :email (getting-started-contact-email)}
+   :most_important_dashboard (most-important-dashboard)
+   :important_metrics        (important-metrics)
+   :important_tables         (important-tables)
+   :important_segments       (important-segments)})
+
 
 (api/define-routes)
