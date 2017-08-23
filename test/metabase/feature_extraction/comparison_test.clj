ebf0777027988954321f9f6553c76d0e0fffd699
Simon Belak <simon.belak@gmail.com>
2017-08-23 22:05:42 +0200

    Update for updated distance measure

--- test/metabase/feature_extraction/comparison_test.clj
+++ test/metabase/feature_extraction/comparison_test.clj
@@ -47,7 +47,7 @@
                                              {:x 0.9 :y 0.1}))))
 
 (expect
-  (approximately 0.78 0.1)
+  (approximately 0.39 0.1)
   (chi-squared-distance [0.1 0.2 0.7] [0.5 0.4 0.1]))
 (expect
   0
