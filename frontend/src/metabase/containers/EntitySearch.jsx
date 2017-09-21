27463895e37ceba1fe8650aa8f80983816dbdc53
Maz Ameli <maz@metabase.com>
2017-09-21 14:59:18 -0700

    style tweaks

--- frontend/src/metabase/containers/EntitySearch.jsx
+++ frontend/src/metabase/containers/EntitySearch.jsx
@@ -459,7 +459,7 @@ class SearchResultsList extends Component {
         const isInEnd = end + 1 >= entityCount
 
         return (
-            <li className="py1 px3 flex justify-end align-center">
+            <li className="py2 px3 flex justify-end align-center">
                 <span className="text-bold">{ currentEntitiesText }</span>&nbsp;of&nbsp;<span
                 className="text-bold">{entityCount}</span>
                 <span
@@ -543,13 +543,13 @@ export class SearchResultListItem extends Component {
                 className={cx("no-decoration flex align-center py2 px3 cursor-pointer bg-slate-extra-light-hover border-bottom", { "bg-grey-0": highlight })}
                 to={getUrlForEntity(entity)}
                 >
-                    { entity.isChild && <Icon className="mr2" size={14} style={{color: "#DCE1E4"}} name="segment"/> }
-                    <h4 className={cx("flex-full mr1", { "text-brand-darken": !entity.isChild }, { "text-brand text-normal": entity.isChild })}>
+                    { entity.isChild && <Icon className="mr2" size={12} style={{color: "#DCE1E4"}} name="segment"/> }
+                    <h4 className={cx("flex-full mr1 text-brand-saturated", { "text-normal": entity.isChild })}>
                         {entity.name}
                     </h4>
 
                     { entity.descriptionJsx &&
-                        <div className="text-grey-4 text-capitalize" style={{maxWidth: "450px"}}>
+                        <div className="text-grey-3 text-capitalize" style={{maxWidth: "450px"}}>
                             <Ellipsified>{entity.descriptionJsx}</Ellipsified>
                         </div>
                     }
