8bfaaf382c115e0773c25a16984c24822acc3ffa
Cam Saul <cammsaul@gmail.com>
2017-10-06 16:13:01 -0700

    Rework pw reset to stop main task. Remove H2 AUTO_SERVER mode and 60s timeouts

--- OSX/Metabase/Backend/ResetPasswordTask.m
+++ OSX/Metabase/Backend/ResetPasswordTask.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 Metabase. All rights reserved.
 //
 
+#import "AppDelegate.h"
 #import "ResetPasswordTask.h"
 
 @interface ResetPasswordTask ()
@@ -19,28 +20,29 @@
 
 - (void)resetPasswordForEmailAddress:(NSString *)emailAddress success:(void (^)(NSString *))successBlock error:(void (^)(NSString *))errorBlock {
 	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        // first, we need to stop the main Metabase task so we can access the DB
+        NSLog(@"Stopping Metabase task in order to reset password...");
+        [[AppDelegate instance] stopMetabaseTask];
+        
 		self.task = [[NSTask alloc] init];
 				
-		NSString *dbPath = [DBPath() stringByAppendingString:@";IFEXISTS=TRUE;AUTO_SERVER=TRUE"];
-		self.task.environment = @{@"MB_DB_FILE": dbPath, @"HOME": @"/Users/camsaul"};
-		
 		// time travelers from the future: this is hardcoded since I'm the only one who works on this. I give you permission to fix it - Cam
 		#define DEBUG_RUN_LEIN_TASK 0
 		
 		#if DEBUG_RUN_LEIN_TASK
-			self.task.environment			= @{@"MB_DB_FILE": dbPath};
+			self.task.environment			= @{@"MB_DB_FILE": DBPath()};
 			self.task.currentDirectoryPath	= @"/Users/cam/metabase";
 			self.task.launchPath			= @"/usr/local/bin/lein";
 			self.task.arguments				= @[@"run", @"reset-password", emailAddress];
-			NSLog(@"Launching ResetPasswordTask\nMB_DB_FILE='%@' lein run reset-password %@", dbPath, emailAddress);
+			NSLog(@"Launching ResetPasswordTask\nMB_DB_FILE='%@' lein run reset-password %@", DBPath(), emailAddress);
 		#else
-			self.task.environment	= @{@"MB_DB_FILE": dbPath};
+			self.task.environment	= @{@"MB_DB_FILE": DBPath()};
 			self.task.launchPath	= JREPath();
             self.task.arguments		= @[@"-Djava.awt.headless=true", // this prevents the extra java icon from popping up in the dock when running
                                         @"-Xverify:none",            // disable bytecode verification for faster launch speed, not really needed here since JAR is packaged as part of signed .app
                                         @"-jar", UberjarPath(),
                                         @"reset-password", emailAddress];
-			NSLog(@"Launching ResetPasswordTask\nMB_DB_FILE='%@' %@ -jar %@ reset-password %@", dbPath, JREPath(), UberjarPath(), emailAddress);
+			NSLog(@"Launching ResetPasswordTask\nMB_DB_FILE='%@' %@ -jar %@ reset-password %@", DBPath(), JREPath(), UberjarPath(), emailAddress);
 		#endif
 		
 		__weak ResetPasswordTask *weakSelf = self;
@@ -54,6 +56,10 @@
 				} else {
 					errorBlock(weakSelf.output.length ? weakSelf.output : @"An unknown error has occured.");
 				}
+                
+                // now restart the main Metabase task
+                NSLog(@"Reset password complete, restarting Metabase task...");
+                [[AppDelegate instance] startMetabaseTask];
 			});
 		};
 		
