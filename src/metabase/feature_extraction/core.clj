16e15dae8b1ed84c4d4f66f8e8d28a93af2e752c
Simon Belak <simon.belak@gmail.com>
2017-10-09 16:20:24 +0200

    Add comparables

--- src/metabase/feature_extraction/core.clj
+++ src/metabase/feature_extraction/core.clj
@@ -18,7 +18,10 @@
              [table :refer [Table]]]
             [metabase.query-processor :as qp]
             [metabase.util :as u]
-            [redux.core :as redux]))
+            [redux.core :as redux]
+            [toucan
+             [db :as db]
+             [hydrate :refer [hydrate]]]))
 
 (defn- field->features
   "Transduce given column with corresponding feature extractor."
@@ -39,8 +42,78 @@
                                   #(nth % i))])))
              rows))
 
+(defmulti comparables
+  ^{:doc "Given a model, return a list of models it can be compared to."
+    :arglists '([model])}
+  type)
+
+(defn- field-fingerprint
+  [table]
+  (->> (hydrate table :fields)
+       :fields
+       (map (juxt :name :basic_type :special_type))))
+
+(defn- full-listing?
+  [{{:keys [query type]} :dataset_query}]
+  (and (= type "query")
+       (not-any? #{:breakout :aggregation} (keys query))))
+
+(defmethod comparables (type Table)
+  [{:keys [db_id id] :as table}]
+  (let [shards (->> (db/select Table
+                      :db_id db_id
+                      :id    [:not= id])
+                    (filter (comp #{(field-fingerprint table)}
+                                  field-fingerprint)))]
+    (concat
+     (for [{:keys [id]} shards]
+       {:id    id
+        :model :table})
+     (for [{:keys [id]} (db/select Segment
+                          :table_id [:in (conj (map :id shards) id)])]
+       {:id    id
+        :model :segment})
+     (for [{:keys [id] :as card} (db/select Card
+                                   :table_id [:in (conj (map :id shards) id)])
+           :when (full-listing? card)]
+       {:id    id
+        :model :card}))))
+
+(defmethod comparables (type Segment)
+  [{:keys [table_id id]}]
+  (remove #{{:id id, :model :segment}} (conj (comparables (Table table_id))
+                                             {:id    table_id
+                                              :model :table})))
+
+(def ^:private ^{:arglists '([card])} breakout-fingerprint
+  (comp :breakout :query :dataset_query))
+
+(defmethod comparables (type Card)
+  [{:keys [table_id id] :as card}]
+  (->> (concat
+        (for [{:keys [id]} (->> (db/select-ids Card :table_id table_id)
+                                (filter (comp #{(breakout-fingerprint card)}
+                                              breakout-fingerprint)))]
+          {:id    id
+           :model :card})
+        (when (full-listing? card)
+          (conj (comparables (Table table_id))
+                {:id    table_id
+                 :model :table})))
+       (remove #{{:id id, :model :card}})))
+
+(defmethod comparables (type Field)
+  [{:keys [id base_type special_type table_id]}]
+  (for [field-id (db/select-ids  Field
+                   :table_id     table_id
+                   :base_type    base_type
+                   :special_type special_type
+                   :id           [:not= id])]
+    {:model :field
+     :id    field-id}))
+
 (defmulti
-  ^{:doc "Given a model, fetch corresponding dataset and compute its features.
+  ^{:doc "Given a model, fetch the corresponding dataset and compute its features.
 
           Takes a map of options as first argument. Recognized options:
           * `:max-cost`   a map with keys `:computation` and `:query` which
@@ -68,10 +141,11 @@
 (defmethod extract-features (type Field)
   [opts field]
   (let [{:keys [field row]} (values/field-values field (extract-query-opts opts))]
-    {:features (->> row
-                    (field->features opts field)
-                    (merge {:table (Table (:table_id field))}))
-     :sample?  (sampled? opts row)}))
+    {:features    (->> row
+                       (field->features opts field)
+                       (merge {:table (Table (:table_id field))}))
+     :sample?     (sampled? opts row)
+     :comparables (comparables field)}))
 
 (defmethod extract-features (type Table)
   [opts table]
@@ -80,7 +154,8 @@
                                             {:source-table (:id table)}))]
     {:constituents (dataset->features opts dataset)
      :features     {:table table}
-     :sample?      (sampled? opts dataset)}))
+     :sample?      (sampled? opts dataset)
+     :comparables  (comparables table)}))
 
 (defn index-of
   "Return index of the first element in `coll` for which `pred` reutrns true."
@@ -106,16 +181,18 @@
                                          (or (first aggregation)
                                              (second breakout))]]
     {:constituents (dataset->features opts dataset)
-     :features     (merge (field->features (->> card
-                                                :dataset_query
-                                                :query
-                                                (assoc opts :query))
-                                           fields
-                                           (ensure-aligment fields cols rows))
+     :features     (merge (when (every? some? fields)
+                            (field->features (->> card
+                                                  :dataset_query
+                                                  :query
+                                                  (assoc opts :query))
+                                             fields
+                                             (ensure-aligment fields cols rows)))
                           {:card  card
                            :table (Table (:table_id card))})
      :dataset      dataset
-     :sample?      (sampled? opts dataset)}))
+     :sample?      (sampled? opts dataset)
+     :comparables  (comparables card)}))
 
 (defmethod extract-features (type Segment)
   [opts segment]
@@ -125,7 +202,8 @@
     {:constituents (dataset->features opts dataset)
      :features     {:table   (Table (:table_id segment))
                     :segment segment}
-     :sample?      (sampled? opts dataset)}))
+     :sample?      (sampled? opts dataset)
+     :comparables  (comparables segment)}))
 
 (defn- trim-decimals
   [decimal-places features]
