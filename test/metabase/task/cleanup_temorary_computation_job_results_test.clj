979c38bed29690290c0dfcf87228c7122d9d3fd6
Simon Belak <simon.belak@gmail.com>
2017-10-18 20:16:57 +0200

    Add test

---
+++ test/metabase/task/cleanup_temorary_computation_job_results_test.clj
@@ -0,0 +1,27 @@
+(ns metabase.task.cleanup-temorary-computation-job-results-test
+  (:require [clj-time
+             [coerce :as t.coerce]
+             [core :as t]]
+            [expectations :refer :all]
+            [metabase.models
+             [computation-job :refer [ComputationJob]]
+             [computation-job-result :refer [ComputationJobResult]]]
+            [metabase.task.cleanup-temporary-computation-job-results :as task]
+            [toucan.db :as db]
+            [toucan.util.test :as tt]))
+
+(tt/expect-with-temp
+ [ComputationJob       [{job-id :id} {:type   :simple-job
+                                      :status :done}]
+  ComputationJobResult [{result-id :id} {:job_id     job-id
+                                         :permanence :temporary
+                                         :payload    1}]]
+  [1 0]
+  (let [c1 (count (ComputationJobResult))]
+    (db/update! ComputationJobResult result-id
+                :created_at (->> #'task/temporary-result-lifetime
+                                 var-get
+                                 (t/minus (t/now) (t/days 1))
+                                 t.coerce/to-sql-time))
+    (#'task/cleanup-temporary-results!)
+    [c1 (count (ComputationJobResult))]))
