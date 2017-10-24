099b697547e8dc8c9a5762d00bb460ce52389f39
Kyle Doherty <kdoh@users.noreply.github.com>
2017-10-23 18:05:43 -0700

    fix component info file regex (#6244)

--- frontend/src/metabase/internal/lib/components-webpack.js
+++ frontend/src/metabase/internal/lib/components-webpack.js
@@ -2,7 +2,7 @@
 const req = require.context(
     "metabase/components",
     true,
-    /^(.*\.info\.(js$))[^.]*$/igm
+    /^(.*\.info\.(js$))[^.]*$/im
 );
 
 export default req.keys().map(key => req(key));
