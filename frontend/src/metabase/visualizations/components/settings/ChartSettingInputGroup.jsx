b9157514d79dfb45cb98a4d744bb0e586f0af392
Cam Saul <cammsaul@gmail.com>
2017-10-24 17:54:49 -0700

    value -> values

--- frontend/src/metabase/visualizations/components/settings/ChartSettingInputGroup.jsx
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingInputGroup.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import Input from "metabase/components/Input.jsx";
 
 // value is an array of strings. This component provides one input box per string
-export default function ChartSettingInputGroup({ value, onChange }) {
-    const inputs = value.map((str, i) => (
+export default function ChartSettingInputGroup({ value: values, onChange }) {
+    const inputs = values.map((str, i) => (
         <Input
             key={i}
             className="input block full"
@@ -12,10 +12,10 @@ export default function ChartSettingInputGroup({ value, onChange }) {
             onBlurChange={(e) => {
                 const newStr = e.target.value.trim();
                 if (!newStr || !newStr.length) return;
-
-                const newValue = value.slice(); // clone the original `value` array
-                newValue[i] = newStr;
-                onChange(newValue);
+                // clone the original values array. It's read-only so we can't just replace the one value we want
+                const newValues = values.slice();
+                newValues[i] = newStr;
+                onChange(newValues);
             }}
         />
     ));
