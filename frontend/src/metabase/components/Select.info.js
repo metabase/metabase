dbb3d987d348cb16aa861bbe0610050e9d015665
Kyle Doherty <kyle.l.doherty@gmail.com>
2017-10-25 15:20:16 -0700

    clean up example

--- frontend/src/metabase/components/Select.info.js
+++ frontend/src/metabase/components/Select.info.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import insightful from 'insightful'
 
 import Select, { Option } from 'metabase/components/Select'
 
 export const component = Select
 
-// Create a big list of garbage so that we know our virtualization is working
-// we also need to have them be objects, so return an object with a name property
-const fixture = insightful(200000).map(i => ({ name: i }))
-
-export const showCode = false
+const fixture = [
+    { name: 'Blue' },
+    { name: 'Green' },
+    { name: 'Red' },
+    { name: 'Yellow' },
+]
 
 export const description = `
     A component used to make a selection
@@ -23,9 +23,7 @@ export const examples = {
     ),
     'With search': (
         <Select
-            isInitiallyOpen
             searchProp='name'
-            searchCaseInsensitive
             onChange={() => alert('Selected')}
         >
             { fixture.map(f => <Option name={f.name}>{f.name}</Option>)}
