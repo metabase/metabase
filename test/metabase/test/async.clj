2b6388f8b316be2e23aae691c550350496f26c9d
Simon Belak <simon.belak@gmail.com>
2017-12-05 17:54:15 +0100

    Fix potential race condition where the job gets cleaned up too soon.

--- test/metabase/test/async.clj
+++ test/metabase/test/async.clj
@@ -7,11 +7,12 @@
 (defn result!
   "Blocking version of async/result."
   [job-id]
-  (-> #'async/running-jobs
-      deref          ; var
-      deref          ; atom
-      (get job-id)
-      deref)         ; future
+  (some-> #'async/running-jobs   ; It might be that the job has finished and
+                                 ; cleand up already, hence some->
+          deref                  ; var
+          deref                  ; atom
+          (get job-id)
+          deref)                 ; future
   (async/result (ComputationJob job-id)))
 
 (def ^:dynamic *max-while-runtime*
