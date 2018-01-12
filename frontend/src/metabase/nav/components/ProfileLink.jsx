4c372463d9c69bd44960e5a0690d6f6b86e9441b
Sameer Al-Sakran <salsakran@users.noreply.github.com>
2018-01-12 13:00:42 -0800

    Remove dupe 2

--- frontend/src/metabase/nav/components/ProfileLink.jsx
+++ frontend/src/metabase/nav/components/ProfileLink.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { Link } from "react-router";
-import { t } from 'c-3po'
 
 import OnClickOutsideWrapper from 'metabase/components/OnClickOutsideWrapper';
 import { t } from 'c-3po';
