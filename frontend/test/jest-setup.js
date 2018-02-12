184105d30fecb33a4eb9ee1987e2fde2845cbc78
Tom Robinson <tlrobinson@gmail.com>
2018-02-12 17:57:33 +0700

    fix merge error

--- frontend/test/jest-setup.js
+++ frontend/test/jest-setup.js
@@ -1,6 +1,6 @@
 import 'raf/polyfill';
 
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
