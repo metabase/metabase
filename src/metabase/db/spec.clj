fac369ec35a49f617a7bd520fda5753ce3e96374
Mobile PC <tahri.e@gmail.com>
2018-02-20 12:45:06 -0500

    Updated spec.clj to fix merge conflict
    src/metabase/db/spec.clj updated from master

--- src/metabase/db/spec.clj
+++ src/metabase/db/spec.clj
@@ -1,5 +1,7 @@
 (ns metabase.db.spec
-  "Functions for creating JDBC DB specs for a given engine.")
+  "Functions for creating JDBC DB specs for a given engine.
+   Only databases that are supported as application DBs should have functions in this namespace;
+   otherwise, similar functions are only needed by drivers, and belong in those namespaces.")
 
 (defn h2
   "Create a database specification for a h2 database. Opts should include a key
@@ -21,7 +23,7 @@
     :as opts}]
   (merge {:classname "org.postgresql.Driver" ; must be in classpath
           :subprotocol "postgresql"
-          :subname (str "//" host ":" port "/" db)}
+          :subname (str "//" host ":" port "/" db "?OpenSourceSubProtocolOverride=true")}
          (dissoc opts :host :port :db)))
 
 (defn mysql
@@ -37,20 +39,6 @@
           :delimiters "`"}
          (dissoc opts :host :port :db)))
 
-
-;; TODO - These other ones can acutally be moved directly into their respective drivers themselves since they're not supported as backing DBs
-
-(defn mssql
-  "Create a database specification for a mssql database. Opts should include keys
-  for :db, :user, and :password. You can also optionally set host and port."
-  [{:keys [user password db host port]
-    :or {user "dbuser", password "dbpassword", db "", host "localhost", port 1433}
-    :as opts}]
-  (merge {:classname "com.microsoft.sqlserver.jdbc.SQLServerDriver" ; must be in classpath
-          :subprotocol "sqlserver"
-          :subname (str "//" host ":" port ";database=" db ";user=" user ";password=" password)}
-         (dissoc opts :host :port :db)))
-
 (defn sybase
   "Create a database specification for a sybase database. Opts should include keys
   for :db, :user, and :password. You can also optionally set host and port."
@@ -61,24 +49,3 @@
           :subprotocol "sybase"
           :subname (str "//" host ":" port ";database=" db ";user=" user ";password=" password)}
          (dissoc opts :host :port :db)))
-
-(defn sqlite3
-  "Create a database specification for a SQLite3 database. Opts should include a
-  key for :db which is the path to the database file."
-  [{:keys [db]
-    :or {db "sqlite.db"}
-    :as opts}]
-  (merge {:classname "org.sqlite.JDBC" ; must be in classpath
-          :subprotocol "sqlite"
-          :subname db}
-         (dissoc opts :db)))
-
-(defn oracle
-  "Create a database specification for an Oracle database. Opts should include keys
-  for :user and :password. You can also optionally set host and port."
-  [{:keys [host port]
-    :or {host "localhost", port 1521}
-    :as opts}]
-  (merge {:subprotocol "oracle:thin"
-          :subname     (str "@" host ":" port)}
-         (dissoc opts :host :port)))
