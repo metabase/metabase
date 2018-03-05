d3d8faefee87df61ee304c64216d30f5680658ad
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-03-05 14:55:43 -0800

    init

---
+++ frontend/src/metabase/setup/components/ExploreStep.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { t } from "c-3po";
+
+const ExploreStep = () => (
+  <div>
+    <h3>{t`Explore your data`}</h3>
+  </div>
+);
+
+export default ExploreStep;
--- frontend/src/metabase/setup/components/Setup.jsx
+++ frontend/src/metabase/setup/components/Setup.jsx
@@ -13,12 +13,14 @@ import UserStep from "./UserStep.jsx";
 import DatabaseConnectionStep from "./DatabaseConnectionStep.jsx";
 import PreferencesStep from "./PreferencesStep.jsx";
 import DatabaseSchedulingStep from "metabase/setup/components/DatabaseSchedulingStep";
+import ExploreStep from "metabase/setup/components/ExploreStep.jsx";
 
 const WELCOME_STEP_NUMBER = 0;
 const USER_STEP_NUMBER = 1;
 const DATABASE_CONNECTION_STEP_NUMBER = 2;
 const DATABASE_SCHEDULING_STEP_NUMBER = 3;
 const PREFERENCES_STEP_NUMBER = 4;
+const EXPLORE_STEP_NUMBER = 5;
 
 export default class Setup extends Component {
   static propTypes = {
@@ -134,6 +136,8 @@ export default class Setup extends Component {
                 {...this.props}
                 stepNumber={PREFERENCES_STEP_NUMBER}
               />
+
+              <ExploreStep stepNumber={EXPLORE_STEP_NUMBER} />
 
               {setupComplete ? (
                 <section className="SetupStep rounded SetupStep--active flex flex-column layout-centered p4">
