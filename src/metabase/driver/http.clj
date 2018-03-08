c7b232bcf1fa7e66644b8c2a4c8828b68251d3da
Tom Robinson <tlrobinson@gmail.com>
2018-03-08 00:48:06 -0600

    Add basic breakout/aggregation support

--- src/metabase/driver/http.clj
+++ src/metabase/driver/http.clj
@@ -12,6 +12,7 @@
   clojure.lang.Named
   (getName [_] "HTTP"))
 
+(declare compile-expression compile-function)
 
 (defn- database->definitions
   [database]
@@ -26,8 +27,45 @@
   (first (filter #(= (:name %) name) (database->table-defs database))))
 
 (defn json-path
-  [body query]
+  [query body]
   (JsonPath/read body query (into-array Predicate [])))
+
+(defn compile-function
+  [[operator & arguments]]
+  (case (keyword operator)
+    :count count
+    :sum   #(reduce + (map (compile-expression (first arguments)) %))
+    :float #(Float/parseFloat ((compile-expression (first arguments)) %))
+           (throw (Exception. (str "Unknown operator: " operator)))))
+
+(defn compile-expression
+  [expr]
+  (cond
+    (string? expr)  (partial json-path expr)
+    (number? expr)  (constantly expr)
+    (vector? expr)  (compile-function expr)
+    :else           (throw (Exception. (str "Unknown expression: " expr)))))
+
+(defn aggregate
+  [rows metrics breakouts]
+  (let [breakouts-fns (map compile-expression breakouts)
+        breakout-fn   (fn [row] (for [breakout breakouts-fns] (breakout row)))
+        metrics-fns   (map compile-expression metrics)]
+    (for [[breakout-key breakout-rows] (group-by breakout-fn rows)]
+      (concat breakout-key (for [metrics-fn metrics-fns]
+                             (metrics-fn breakout-rows))))))
+
+(defn extract-fields
+  [rows fields]
+  (let [fields-fns (map compile-expression fields)]
+    (for [row rows]
+      (for [field-fn fields-fns]
+        (field-fn row)))))
+
+(defn field-names
+  [fields]
+  (for [field fields]
+    (keyword (json/generate-string field))))
 
 (defn execute-query
   [query]
@@ -39,13 +77,18 @@
                                        :headers (:headers query)
                                        :accept  :json
                                        :as      :json})
-        items-path    (or (:path (:result query)) "$")
-        items         (json-path (walk/stringify-keys (:body result)) items-path)
-        fields-paths  (or (:fields (:result query)) (keys (first items)))]
-    {:columns (for [fields-path fields-paths] (keyword fields-path))
-     :rows    (for [item items]
-                (for [fields-path fields-paths]
-                  (json-path item fields-path)))}))
+        rows-path     (or (:path (:result query)) "$")
+        rows          (json-path rows-path (walk/stringify-keys (:body result)))
+        fields        (or (:fields (:result query)) (keys (first rows)))
+        aggregations  (or (:aggregation (:result query)) [])
+        breakouts     (or (:breakout (:result query)) [])
+        raw           (and (= (count breakouts) 0) (= (count aggregations) 0))]
+    {:columns (if raw
+                (field-names fields)
+                (field-names (concat breakouts aggregations)))
+     :rows    (if raw
+                (extract-fields rows fields)
+                (aggregate rows aggregations breakouts))}))
 
 (defn- mbql->native
   [query]
