93051aca227e61c383582423c6149164d9b230af
Tom Robinson <tlrobinson@gmail.com>
2018-03-20 11:37:23 -0700

    Minor cleanup, re-enable settings for x/y axis labels

--- frontend/src/metabase/visualizations/lib/settings/graph.js
+++ frontend/src/metabase/visualizations/lib/settings/graph.js
@@ -315,23 +315,28 @@ export const GRAPH_AXIS_SETTINGS = {
   "graph.x_axis.axis_enabled": {
     section: "Axes",
     title: t`Show x-axis line and marks`,
-    widget: "toggle",
-    // widget: "select",
-    // props: {
-    //   options: [
-    //     { name: t`Disabled`, value: false },
-    //     { name: t`Enabled`, value: true },
-    //     { name: t`Compact`, value: "compact" },
-    //     { name: t`45°`, value: "rotate-45" },
-    //     { name: t`90°`, value: "rotate-90" },
-    //   ],
-    // },
+    widget: "select",
+    props: {
+      options: [
+        { name: t`Hide`, value: false },
+        { name: t`Show`, value: true },
+        { name: t`Compact`, value: "compact" },
+        { name: t`Rotate 45°`, value: "rotate-45" },
+        { name: t`Rotate 90°`, value: "rotate-90" },
+      ],
+    },
     default: true,
   },
   "graph.y_axis.axis_enabled": {
     section: "Axes",
     title: t`Show y-axis line and marks`,
-    widget: "toggle",
+    widget: "select",
+    props: {
+      options: [
+        { name: t`Hide`, value: false },
+        { name: t`Show`, value: true },
+      ],
+    },
     default: true,
   },
   "graph.y_axis.auto_range": {
