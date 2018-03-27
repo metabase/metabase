c59ded3218117c3650d3633b8c04153db5cb6740
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-03-26 22:18:22 -0700

    add copy color

--- frontend/src/metabase/internal/components/ColorsApp.jsx
+++ frontend/src/metabase/internal/components/ColorsApp.jsx
@@ -2,23 +2,54 @@
 
 import React from "react";
 import { Box, Flex, Subhead } from "rebass";
+import { connect } from "react-redux";
 import cx from "classnames";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 import { normal, saturated, harmony } from "metabase/lib/colors";
 
+import { addUndo, createUndo } from "metabase/redux/undo";
+
 const SWATCH_SIZE = 200;
-const ColorSwatch = ({ color, name }) => (
-  <Box
-    w={SWATCH_SIZE}
-    style={{ backgroundColor: color, height: SWATCH_SIZE, borderRadius: 12 }}
-    p={3}
-    mr={2}
-    mb={2}
-  >
-    {name}
-    <h2>{color}</h2>
-  </Box>
-);
+
+const mapDispatchToProps = {
+  addUndo,
+  createUndo,
+};
+
+@connect(() => ({}), mapDispatchToProps)
+class ColorSwatch extends React.Component {
+  _onCopy(colorValue) {
+    const { addUndo, createUndo } = this.props;
+    addUndo(
+      createUndo({
+        type: "copy-color",
+        message: <div>Copied {colorValue} to clipboard</div>,
+      }),
+    );
+  }
+  render() {
+    const { color, name } = this.props;
+    return (
+      <CopyToClipboard text={color} onCopy={() => this._onCopy(color)}>
+        <Box
+          w={SWATCH_SIZE}
+          style={{
+            backgroundColor: color,
+            height: SWATCH_SIZE,
+            borderRadius: 12,
+          }}
+          p={3}
+          mr={2}
+          mb={2}
+        >
+          {name}
+          <h2>{color}</h2>
+        </Box>
+      </CopyToClipboard>
+    );
+  }
+}
 
 // eslint-disable-next-line import/no-commonjs
 let colorStyles = require("!style-loader!css-loader?modules!postcss-loader!metabase/css/core/colors.css");
