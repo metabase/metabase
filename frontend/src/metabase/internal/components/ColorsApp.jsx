8fe1618282e916e4e5ccab8ee5986e69c0979f9f
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-03-27 10:32:49 -0700

    copy to clipboard w/ notification

--- frontend/src/metabase/internal/components/ColorsApp.jsx
+++ frontend/src/metabase/internal/components/ColorsApp.jsx
@@ -1,25 +1,51 @@
 /* @flow */
-
 import React from "react";
 import cx from "classnames";
+import { connect } from "react-redux";
+import CopyToClipboard from "react-copy-to-clipboard";
 
+import { addUndo, createUndo } from "metabase/redux/undo";
 import { normal, saturated, harmony } from "metabase/lib/colors";
 
 const SWATCH_SIZE = 200;
-const ColorSwatch = ({ color, name }) => (
-  <div
-    style={{
-      backgroundColor: color,
-      height: SWATCH_SIZE,
-      borderRadius: 12,
-      width: SWATCH_SIZE,
-    }}
-    className="p3 mr2 mb2"
-  >
-    {name}
-    <h2>{color}</h2>
-  </div>
-);
+
+const mapDispatchToProps = {
+  addUndo,
+  createUndo,
+};
+
+@connect(() => ({}), mapDispatchToProps)
+class ColorSwatch extends React.Component {
+  _onCopy(colorValue) {
+    const { addUndo, createUndo } = this.props;
+    addUndo(
+      createUndo({
+        type: "copy-color",
+        message: <div>Copied {colorValue} to clipboard</div>,
+      }),
+    );
+  }
+  render() {
+    const { color, name } = this.props;
+    return (
+      <CopyToClipboard value={color} onCopy={() => this._onCopy(color)}>
+        <div
+          style={{
+            backgroundColor: color,
+            height: SWATCH_SIZE,
+            borderRadius: 12,
+            width: SWATCH_SIZE,
+            color: "white",
+          }}
+          className="p3 mr2 mb2"
+        >
+          {name}
+          <h2>{color}</h2>
+        </div>
+      </CopyToClipboard>
+    );
+  }
+}
 
 // eslint-disable-next-line import/no-commonjs
 let colorStyles = require("!style-loader!css-loader?modules!postcss-loader!metabase/css/core/colors.css");
@@ -30,7 +56,7 @@ const ColorsApp = () => (
       <h2 className="my3">Normal</h2>
       <div className="flex flex-wrap">
         {Object.entries(normal).map(([name, value]) => (
-          <ColorSwatch color={value} name={name} />
+          <ColorSwatch color={value} name={name} key={`noraml-${name}`} />
         ))}
       </div>
     </div>
@@ -38,7 +64,7 @@ const ColorsApp = () => (
       <h2 className="my3">Saturated</h2>
       <div className="flex flex-wrap">
         {Object.entries(saturated).map(([name, value]) => (
-          <ColorSwatch color={value} name={name} />
+          <ColorSwatch color={value} name={name} key={`saturated-${name}`} />
         ))}
       </div>
     </div>
@@ -46,7 +72,7 @@ const ColorsApp = () => (
       <h2 className="my3">Chart colors</h2>
       <div className="flex flex-wrap">
         {harmony.map((color, index) => (
-          <ColorSwatch color={color} name={`Series ${index + 1}`} />
+          <ColorSwatch color={color} name={`Series ${index + 1}`} key={index} />
         ))}
       </div>
     </div>
@@ -55,6 +81,7 @@ const ColorsApp = () => (
       {Object.entries(colorStyles).map(([name, className]) => (
         <div
           className={cx(className, "rounded px1")}
+          key={className}
           style={{
             paddingTop: "0.25em",
             paddingBottom: "0.25em",
