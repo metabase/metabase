378cd2edb344e21770b7981ba84f4ba3df45161c
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-03-27 15:49:22 -0700

    clean up landing nav

--- frontend/src/metabase/components/LandingNav.jsx
+++ frontend/src/metabase/components/LandingNav.jsx
@@ -5,25 +5,34 @@ import { Link } from "react-router";
 import Icon from "metabase/components/Icon";
 import { normal } from "metabase/lib/colors";
 
+const ICON_SIZE = 42
+
 const LandingNav = props => {
   return (
     <Box>
       <Flex>
         <Box flex={1} bg={normal.green} color="white">
-          <Icon name="insight" />
-          <Link to="metrics">Metrics</Link>
-        </Box>
-        <Box flex={1} bg={normal.indigo} color="white">
-          <Icon name="segment" />
-          <Link to="segments">Segments</Link>
-        </Box>
-        <Box flex={1} bg={normal.grey1}>
-          <Flex>
-            <Link to="questions">Questions</Link>
+          <Flex align='center' justify='center' style={{ height: 120 }} direction='column'>
+            <Icon name="insight" size={ICON_SIZE} />
+            <Link to="metrics">Metrics</Link>
           </Flex>
         </Box>
-        <Box flex={1} bg={normal.blue}>
-          <Link to="metrics">Metrics</Link>
+        <Box flex={1} bg={normal.indigo} color="white">
+          <Flex align='center' justify='center' style={{ height: 120 }} direction='column'>
+            <Icon name="segment" size={ICON_SIZE} />
+            <Link to="segments">Segments</Link>
+          </Flex>
+        </Box>
+        <Box flex={1} bg={normal.blue} color="white">
+          <Flex align='center' justify='center' style={{ height: 120 }} direction='column'>
+            <Icon name="dashboard" size={ICON_SIZE} />
+            <Link to="dashboards">Dashboards</Link>
+          </Flex>
+        </Box>
+        <Box flex={1} bg={normal.grey1}>
+          <Flex align='center' justify='center' style={{ height: 120 }} direction='column'>
+            <Link to="questions">Questions</Link>
+          </Flex>
         </Box>
       </Flex>
     </Box>
