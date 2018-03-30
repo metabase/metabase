8b7b0530031684d7847d19e68783c221cd7a1f13
Tom Robinson <tlrobinson@gmail.com>
2018-03-30 14:52:17 -0700

    Cleanup LineAreaBarRenderer-bar.unit.spec.js

--- frontend/test/visualizations/components/LineAreaBarRenderer-bar.unit.spec.js
+++ frontend/test/visualizations/components/LineAreaBarRenderer-bar.unit.spec.js
@@ -15,14 +15,19 @@ const DEFAULT_SETTINGS = {
   "graph.colors": ["#00FF00", "#FF0000"],
 };
 
+const WIDTH = 1200;
+const HEIGHT = 800;
+
 describe("LineAreaBarRenderer-bar", () => {
   let element;
   const qsa = selector => [...element.querySelectorAll(selector)];
 
   beforeEach(function() {
+    document.body.style.width = `${WIDTH}px`;
+    document.body.style.height = `${WIDTH}px`;
     document.body.insertAdjacentHTML(
       "afterbegin",
-      '<div id="fixture" style="height: 800px; width: 1200px;">',
+      `<div id="fixture" style="height: ${HEIGHT}px; width: ${WIDTH}px;">`,
     );
     element = document.getElementById("fixture");
   });
@@ -31,111 +36,78 @@ describe("LineAreaBarRenderer-bar", () => {
     document.body.removeChild(document.getElementById("fixture"));
   });
 
-  ["area", "bar"].forEach(chartType =>
-    ["stacked", "normalized"].forEach(stack_type =>
-      it(
-        "should render a " +
-          (stack_type || "") +
-          " " +
-          chartType +
-          " chart with 2 series",
-        function() {
-          return new Promise((resolve, reject) => {
-            let hoverCount = 0;
-            lineAreaBarRenderer(element, {
-              chartType: chartType,
-              series: [
-                {
-                  card: {},
-                  data: {
-                    cols: [
-                      StringColumn({
-                        display_name: "Category",
-                        source: "breakout",
-                      }),
-                      NumberColumn({
-                        display_name: "Sum",
-                        source: "aggregation",
-                      }),
-                    ],
-                    rows: [["A", 1]],
-                  },
+  describe("with 2 series", () => {
+    ["area", "bar"].forEach(chartType =>
+      ["stacked", "normalized"].forEach(stackType =>
+        it(`should render a ${stackType} ${chartType} chart`, () => {
+          const onHoverChange = jest.fn();
+          lineAreaBarRenderer(element, {
+            chartType: chartType,
+            series: [
+              {
+                card: {},
+                data: {
+                  cols: [
+                    StringColumn({
+                      display_name: "Category",
+                      source: "breakout",
+                    }),
+                    NumberColumn({
+                      display_name: "Sum",
+                      source: "aggregation",
+                    }),
+                  ],
+                  rows: [["A", 1]],
                 },
-                {
-                  card: {},
-                  data: {
-                    cols: [
-                      StringColumn({
-                        display_name: "Category",
-                        source: "breakout",
-                      }),
-                      NumberColumn({
-                        display_name: "Count",
-                        source: "aggregation",
-                      }),
-                    ],
-                    rows: [["A", 2]],
-                  },
+              },
+              {
+                card: {},
+                data: {
+                  cols: [
+                    StringColumn({
+                      display_name: "Category",
+                      source: "breakout",
+                    }),
+                    NumberColumn({
+                      display_name: "Count",
+                      source: "aggregation",
+                    }),
+                  ],
+                  rows: [["A", 2]],
                 },
-              ],
-              settings: {
-                ...DEFAULT_SETTINGS,
-                "stackable.stack_type": stack_type,
               },
-              onHoverChange: hover => {
-                try {
-                  const data =
-                    hover.data &&
-                    hover.data.map(({ key, value }) => ({ key, value }));
-                  let standardDisplay;
-                  let normalizedDisplay;
-
-                  hoverCount++;
-                  if (hoverCount === 1) {
-                    standardDisplay = [
-                      { key: "Category", value: "A" },
-                      { key: "Sum", value: 1 },
-                    ];
-
-                    normalizedDisplay = [
-                      { key: "Category", value: "A" },
-                      { key: "% Sum", value: "33%" },
-                    ];
-
-                    expect(data).toEqual(
-                      stack_type === "normalized"
-                        ? normalizedDisplay
-                        : standardDisplay,
-                    );
-                    dispatchUIEvent(qsa(".bar, .dot")[1], "mousemove");
-                  } else if (hoverCount === 2) {
-                    standardDisplay = [
-                      { key: "Category", value: "A" },
-                      { key: "Count", value: 2 },
-                    ];
-
-                    normalizedDisplay = [
-                      { key: "Category", value: "A" },
-                      { key: "% Count", value: "67%" },
-                    ];
-
-                    expect(data).toEqual(
-                      stack_type === "normalized"
-                        ? normalizedDisplay
-                        : standardDisplay,
-                    );
-
-                    resolve();
-                  }
-                } catch (e) {
-                  reject(e);
-                }
-              },
-            });
-            dispatchUIEvent(qsa(".bar, .dot")[0], "mousemove");
+            ],
+            settings: {
+              ...DEFAULT_SETTINGS,
+              "stackable.stack_type": stackType,
+            },
+            onHoverChange,
           });
-        },
+
+          expect(onHoverChange).toHaveBeenCalledTimes(0);
+          dispatchUIEvent(qsa(".bar, .dot")[0], "mousemove");
+          expect(onHoverChange).toHaveBeenCalledTimes(1);
+          dispatchUIEvent(qsa(".bar, .dot")[1], "mousemove");
+          expect(onHoverChange).toHaveBeenCalledTimes(2);
+
+          const NORMALIZED_DATA = [
+            [{ key: "Category", value: "A" }, { key: "% Sum", value: "33%" }],
+            [{ key: "Category", value: "A" }, { key: "% Count", value: "67%" }],
+          ];
+          const STACKED_DATA = [
+            [{ key: "Category", value: "A" }, { key: "Sum", value: 1 }],
+            [{ key: "Category", value: "A" }, { key: "Count", value: 2 }],
+          ];
+
+          expect(
+            onHoverChange.mock.calls.map(call =>
+              call[0].data.map(({ key, value }) => ({ key, value })),
+            ),
+          ).toEqual(
+            stackType === "normalized" ? NORMALIZED_DATA : STACKED_DATA,
+          );
+        }),
       ),
-    ),
-  );
+    );
+  });
 });
