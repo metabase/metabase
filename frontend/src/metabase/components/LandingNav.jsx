3210f393c60e8150ff4cbb523b3cd7ab3d582aaa
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-04-04 12:49:11 -0700

    nav tweaks

--- frontend/src/metabase/components/LandingNav.jsx
+++ frontend/src/metabase/components/LandingNav.jsx
@@ -16,6 +16,7 @@ const linkStyle = {
   padding: '1em',
   borderRadius: 6,
   display: 'block',
+  color: '#93A1AB'
 }
 
 const ICON_SIZE = 18;
@@ -23,7 +24,7 @@ const ICON_SIZE = 18;
 const LandingNav = props => {
   const { collectionSlug } = props
   return (
-    <Box className="absolute left" px={2}>
+    <Box className="absolute left top" px={2}>
       <Box>
         <Link to="/" style={linkStyle} activeStyle={activeStyle}>
           <Icon name="reference" size={ICON_SIZE} />
@@ -45,8 +46,13 @@ const LandingNav = props => {
         </Link>
       </Box>
       <Box>
+        <Link to="pulse" style={linkStyle} activeStyle={activeStyle}>
+          <Icon name="pulse" size={ICON_SIZE} />
+        </Link>
+      </Box>
+      <Box>
         <Link to={ collectionSlug ? Urls.collection({ slug: collectionSlug }) : "questions"} activeStyle={activeStyle} style={linkStyle}>
-          <Icon name="add" size={ICON_SIZE} />
+          <Icon name="beaker" size={ICON_SIZE} />
         </Link>
       </Box>
     </Box>
