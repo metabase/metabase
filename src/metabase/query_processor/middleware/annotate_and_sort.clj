9d216d3ae898fb23d7bc3c1ac69c7cde83ed76be
Ryan Senior <ryan@metabase.com>
2018-04-04 17:47:21 -0500

    Include timezone on datetime columns of native queries [ci drivers]

--- src/metabase/query_processor/middleware/annotate_and_sort.clj
+++ src/metabase/query_processor/middleware/annotate_and_sort.clj
@@ -19,15 +19,18 @@
 (defn- infer-column-types
   "Infer the types of columns by looking at the first value for each in the results, and add the relevant information in `:cols`.
    This is used for native queries, which don't have the type information from the original `Field` objects used in the query, which is added to the results by `annotate`."
-  [{:keys [columns rows], :as results}]
+  [query {:keys [columns rows], :as results}]
   (assoc results
     :columns (mapv name columns)
     :cols    (vec (for [i    (range (count columns))
-                        :let [col (nth columns i)]]
-                    {:name         (name col)
-                     :display_name (humanization/name->human-readable-name (name col))
-                     :base_type    (driver/values->base-type (for [row rows]
-                                                               (nth row i)))}))))
+                        :let [col (nth columns i)
+                              base-type (driver/values->base-type (for [row rows]
+                                                                    (nth row i)))]]
+                    (merge {:name         (name col)
+                            :display_name (humanization/name->human-readable-name (name col))
+                            :base_type    base-type}
+                           (when (isa? base-type :type/DateTime)
+                             {:timezone (qputil/query-timezone query)}))))))
 
 
 ;;; +------------------------------------------------------------------------------------------------------------------------+
@@ -41,6 +44,6 @@
     (let [results (qp query)]
       (-> (if-not (or (qputil/mbql-query? query)
                       (:annotate? results))
-            (infer-column-types results)
+            (infer-column-types query results)
             (annotate/annotate-and-sort query results))
           (dissoc :annotate?)))))
--- test/metabase/query_processor/middleware/annotate_and_sort_test.clj
+++ test/metabase/query_processor/middleware/annotate_and_sort_test.clj
@@ -1,13 +1,24 @@
 (ns metabase.query-processor.middleware.annotate-and-sort-test
   (:require [expectations :refer :all]
-            [metabase.query-processor.middleware.annotate-and-sort :as annotate-and-sort]))
+            [metabase.query-processor.middleware.annotate-and-sort :as annotate-and-sort])
+  (:import java.util.Date))
 
 ;; make sure that `infer-column-types` can still infer types even if the initial value(s) are `nil` (#4256)
 (expect
   [{:name "a", :display_name "A", :base_type :type/Integer}
    {:name "b", :display_name "B", :base_type :type/Integer}]
-  (:cols (#'annotate-and-sort/infer-column-types {:columns [:a :b], :rows [[1 nil]
-                                                                           [2 nil]
-                                                                           [3 nil]
-                                                                           [4   5]
-                                                                           [6   7]]})))
+  (:cols (#'annotate-and-sort/infer-column-types {} {:columns [:a :b], :rows [[1 nil]
+                                                                              [2 nil]
+                                                                              [3 nil]
+                                                                              [4   5]
+                                                                              [6   7]]})))
+
+(expect
+  [{:name "a", :display_name "A", :base_type :type/Integer}
+   {:name "b", :display_name "B", :base_type :type/Integer}
+   {:name "c", :display_name "C", :base_type :type/DateTime, :timezone "UTC"}]
+  (:cols (#'annotate-and-sort/infer-column-types {} {:columns [:a :b :c], :rows [[1 nil nil]
+                                                                                 [2 nil nil]
+                                                                                 [3 nil nil]
+                                                                                 [4   5 (Date.)]
+                                                                                 [6   7 (Date.)]]})))
