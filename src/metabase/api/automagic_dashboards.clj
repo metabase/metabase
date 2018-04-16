a4b24d701ac92b5c6eca52b89748a8e8dca14ea4
Simon Belak <simonbelak@Simons-MacBook-Pro.local>
2018-04-17 00:11:06 +0200

    fix cell deep linking. Correct titles for indepth

--- src/metabase/api/automagic_dashboards.clj
+++ src/metabase/api/automagic_dashboards.clj
@@ -106,8 +106,8 @@
   (-> id
       Card
       api/check-404
-      (magic/automagic-analysis {:show        (keyword show)
-                                 :refinements (decode-base64-json cell-query)})))
+      (magic/automagic-analysis {:show       (keyword show)
+                                 :cell-query (decode-base64-json cell-query)})))
 
 (api/defendpoint GET "/question/:id/cell/:cell-query/:prefix/:rule"
   "Return an automagic dashboard analyzing cell in question  with id `id` defined by
@@ -120,9 +120,9 @@
   (-> id
       Card
       api/check-404
-      (magic/automagic-analysis {:show        (keyword show)
-                                 :rule        (load-rule "table" prefix rule)
-                                 :refinements (decode-base64-json cell-query)})))
+      (magic/automagic-analysis {:show       (keyword show)
+                                 :rule       (load-rule "table" prefix rule)
+                                 :cell-query (decode-base64-json cell-query)})))
 
 (api/defendpoint GET "/metric/:id"
   "Return an automagic dashboard analyzing metric with id `id`."
@@ -162,8 +162,8 @@
         cell-query (decode-base64-json cell-query)]
     (-> query
         query/adhoc-query
-        (magic/automagic-analysis {:show        (keyword show)
-                                   :refinements cell-query}))))
+        (magic/automagic-analysis {:show       (keyword show)
+                                   :cell-query cell-query}))))
 
 (def ^:private valid-comparison-pair?
   #{["segment" "segment"]
