73488c41ce2604353b80befba0f589299a921cb2
Sameer Al-Sakran <sameer@expa.com>
2018-04-24 12:07:38 -0700

    enable question xrays

--- frontend/src/metabase/qb/components/actions/index.js
+++ frontend/src/metabase/qb/components/actions/index.js
@@ -2,10 +2,10 @@
 
 import UnderlyingDataAction from "./UnderlyingDataAction";
 import UnderlyingRecordsAction from "./UnderlyingRecordsAction";
-// import GenerateDashboardAction from "./GenerateDashboardAction";
+import GenerateDashboardAction from "./GenerateDashboardAction";
 
 export const DEFAULT_ACTIONS = [
   UnderlyingDataAction,
   UnderlyingRecordsAction,
-  // GenerateDashboardAction,
+  GenerateDashboardAction,
 ];
