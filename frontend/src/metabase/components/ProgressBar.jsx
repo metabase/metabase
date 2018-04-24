92f5f3b22ad3f1db770dbab63381ad87c1a491e3
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-04-24 11:17:03 -0700

    usage examples

--- frontend/src/metabase/components/ProgressBar.jsx
+++ frontend/src/metabase/components/ProgressBar.jsx
@@ -1,51 +1,51 @@
 /* @flow */
 import React, { Component } from "react";
-import cxs from 'cxs'
+import cxs from "cxs";
 
 type Props = {
   percentage: Number,
   animated: Boolean,
-}
+};
 
 export default class ProgressBar extends Component {
-
-  props: Props
+  props: Props;
 
   static defaultProps = {
     animated: false,
   };
 
   render() {
-    const { percentage, animated } = this.props
+    const { percentage, animated } = this.props;
 
     const wrapperStyles = cxs({
-      position: 'relative',
-      border: '1px solid #6f7a8b',
+      position: "relative",
+      border: "1px solid #6f7a8b",
       height: 10,
       borderRadius: 99,
-    })
+    });
 
     const progressStyles = cxs({
-      overflow: 'hidden',
-      backgroundColor: '#6f7a8b',
-      position: 'relative',
-      height: '100%',
+      overflow: "hidden",
+      backgroundColor: "#6f7a8b",
+      position: "relative",
+      height: "100%",
       top: 0,
       left: 0,
-      borderRadius: 'inherit',
+      borderRadius: "inherit",
       borderTopLeftRadius: 0,
       borderBottomLeftRadius: 0,
       width: `${percentage * 100}%`,
-      ':before': {
-        display: 'block',
-        position: 'absolute',
+      ":before": {
+        display: "block",
+        position: "absolute",
         content: '""', // need to wrap this in quotes so it actually outputs as valid CSS
+        left: 0,
         width: 50,
-        height: '100',
-        backgroundColor: 'red',
-        animation: animated ? 'move-ltr 1.5s linear infinite' : 'none',
-      }
-    })
+        height: "100",
+        backgroundColor: "red",
+        animation: animated ? "move-ltr 1.5s linear infinite" : "none",
+      },
+    });
 
     return (
       <div className={wrapperStyles}>
