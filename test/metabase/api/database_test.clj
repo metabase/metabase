45968d63b9c09bae1dec8a5ad74880457a3a4746
Ryan Senior <ryan@metabase.com>
2018-04-27 14:55:32 -0500

    Trigger analyze whenever the user manually syncs a database
    Fixes #7374

--- test/metabase/api/database_test.clj
+++ test/metabase/api/database_test.clj
@@ -12,6 +12,7 @@
              [field-values :refer [FieldValues]]
              [table :refer [Table]]]
             [metabase.sync
+             [analyze :as analyze]
              [field-values :as field-values]
              [sync-metadata :as sync-metadata]]
             [metabase.test
@@ -545,15 +546,27 @@
     (-> ((user->client :crowberto) :get 200 (format "database/%d" (u/get-id db)))
         (select-keys [:cache_field_values_schedule :metadata_sync_schedule :schedules]))))
 
+;; Five minutes
+(def ^:private long-timeout (* 5 60 1000))
+
+(defn- deliver-when-db [promise-to-deliver expected-db]
+  (fn [db]
+    (when (= (u/get-id db) (u/get-id expected-db))
+      (deliver promise-to-deliver true))))
+
 ;; Can we trigger a metadata sync for a DB?
 (expect
-  (let [sync-called? (atom false)]
+  [true true]
+  (let [sync-called?    (promise)
+        analyze-called? (promise)]
     (tt/with-temp Database [db {:engine "h2", :details (:details (data/db))}]
-      (with-redefs [sync-metadata/sync-db-metadata! (fn [synced-db]
-                                                      (when (= (u/get-id synced-db) (u/get-id db))
-                                                        (reset! sync-called? true)))]
+      (with-redefs [sync-metadata/sync-db-metadata! (deliver-when-db sync-called? db)
+                    analyze/analyze-db!             (deliver-when-db analyze-called? db)]
         ((user->client :crowberto) :post 200 (format "database/%d/sync_schema" (u/get-id db)))
-        @sync-called?))))
+        ;; Block waiting for the promises from sync and analyze to be delivered. Should be delivered instantly,
+        ;; however if something went wrong, don't hang forever, eventually timeout and fail
+        [(deref sync-called? long-timeout :sync-never-called)
+         (deref analyze-called? long-timeout :analyze-never-called)]))))
 
 ;; (Non-admins should not be allowed to trigger sync)
 (expect
