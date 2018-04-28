cb797d013e1998ff50d2601c79b635d436aa12ab
Sameer Al-Sakran <sameer@expa.com>
2018-04-27 17:23:29 -0700

    fix drill-through to automatic dashboards

--- frontend/src/metabase/qb/components/actions/GenerateDashboardAction.jsx
+++ frontend/src/metabase/qb/components/actions/GenerateDashboardAction.jsx
@@ -11,6 +11,12 @@ export default ({ question, settings }: ClickActionProps): ClickAction[] => {
   // currently time series xrays require the maximum fidelity
   console.log(JSON.stringify(question.query().datasetQuery()));
   let dashboard_url = "adhoc";
+  const query = question.query();
+
+  // aggregations
+  if (query.aggregations().length) {
+    return [];
+  }
   if (question.card().id) {
     dashboard_url = `/auto/dashboard/question/${question.card().id}`;
   } else {
--- frontend/src/metabase/qb/components/drill/AutomaticDashboardDrill.jsx
+++ frontend/src/metabase/qb/components/drill/AutomaticDashboardDrill.jsx
@@ -15,11 +15,6 @@ export default ({ question, clicked }: ClickActionProps): ClickAction[] => {
     return [];
   }
 
-  // aggregations
-  if (query.aggregations().length) {
-    return [];
-  }
-
   // questions with a breakout
   const dimensions = (clicked && clicked.dimensions) || [];
   if (!clicked || dimensions.length === 0) {
