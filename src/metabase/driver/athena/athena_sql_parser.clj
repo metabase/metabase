ff5b7abee5923fdc7702c0826b7d7b0cee939f8c
Thiago Lopes <thiago.slopes@b2wdigital.com>
2018-05-09 17:14:06 -0300

    Fix Athena sync database

--- src/metabase/driver/athena/athena_sql_parser.clj
+++ src/metabase/driver/athena/athena_sql_parser.clj
@@ -22,8 +22,11 @@
   (set (map (fn [[k v]]
               (let [root {:name (name k)
                           :base-type (cond (map? v) :type/Dictionary
-                                                          (sequential? v) :type/Array
-                                                          :else (column->base-type v))}]
+                                           (sequential? v) :type/Array
+                                           :else (column->base-type v))
+                          :database-type (cond (map? v) "map"
+                                               (sequential? v) "array"
+                                               :else  v)}]
                 (cond
                   (map? v) (assoc root :nested-fields (create-nested-fields v))
                   :else root)))
@@ -41,12 +44,13 @@
         schema (remove-root-field root-field-name (hsp/hive-schema->map (:type field-info)))]
     {:name root-field-name
      :base-type :type/Dictionary
+     :database-type "struct"
      :nested-fields (create-nested-fields schema)}))
 
 (defn- parse-array-type-field [field-info]
   (let [root-field-name (:name field-info)
         schema (hsp/hive-schema->map (:type field-info))]
-    {:name root-field-name :base-type :type/Array}))
+    {:name root-field-name :base-type :type/Array :database-type "array"}))
 
 (defn- is-struct-type-field? [field-info]
   (clojure.string/starts-with? (:type field-info) "struct"))
@@ -59,4 +63,4 @@
     ; :TODO Should we also validate maps?
     (is-struct-type-field? field-info) (parse-struct-type-field field-info)
     (is-array-type-field? field-info) (parse-array-type-field field-info)
-    :else {:name (:name field-info) :base-type (column->base-type (:type field-info))}))
+    :else {:name (:name field-info) :base-type (column->base-type (:type field-info)) :database-type (:type field-info)}))
--- test/metabase/driver/athena/athena_sql_parser_test.clj
+++ test/metabase/driver/athena/athena_sql_parser_test.clj
@@ -5,55 +5,65 @@
 
 (expect
   {:name "sk_id"
-   :base-type :type/Text}
+   :base-type :type/Text
+   :database-type "string"}
   (parser/parse-schema {:name "sk_id" :type "string"}))
 
 (expect
   {:name "sk_date"
-   :base-type :type/Date}
+   :base-type :type/Date
+   :database-type "date"}
   (parser/parse-schema {:name "sk_date" :type "date"}))
 
 (expect
   {:name "sk_hour"
-   :base-type :type/Integer}
+   :base-type :type/Integer
+   :database-type "int"}
   (parser/parse-schema {:name "sk_hour" :type "int"}))
 
 (expect
   {:name "warnings"
-   :base-type :type/Array}
+   :base-type :type/Array
+   :database-type "array"}
   (parser/parse-schema {:name "warnings" :type "array<string>"}))
 
 (expect
   {:name "scheduleslots"
    :base-type :type/Dictionary
-   :nested-fields #{{:name "availabledays" :base-type :type/Array}
-                    {:name "availablepartsofday" :base-type :type/Array}}}
+   :database-type "struct"
+   :nested-fields #{{:name "availabledays" :base-type :type/Array :database-type "array"}
+                    {:name "availablepartsofday" :base-type :type/Array :database-type "array"}}}
   (parser/parse-schema {:name "scheduleslots" :type "struct<availabledays:array<string>,availablepartsofday:array<string>>"}))
 
 (expect
   {:name "price"
-   :base-type :type/Float}
+   :base-type :type/Float
+   :database-type "double"}
   (parser/parse-schema {:name "price" :type "double"}))
 
 (expect
   {:name "sk_references"
    :base-type :type/Dictionary
-   :nested-fields #{{:name "cepid" :base-type :type/Text}
-                    {:name "customerid" :base-type :type/Text}
-                    {:name "id" :base-type :type/Integer}}}
+   :database-type "struct"
+   :nested-fields #{{:name "cepid" :base-type :type/Text :database-type "string"}
+                    {:name "customerid" :base-type :type/Text :database-type "string"}
+                    {:name "id" :base-type :type/Integer :database-type "int"}}}
   (parser/parse-schema {:name "sk_references" :type "struct<cepid:string,customerid:string,id:int>"}))
 
 (expect
   {:name "options"
    :base-type :type/Dictionary
+   :database-type "struct"
    :nested-fields #{{:name "conventional"
                      :base-type :type/Dictionary
-                     :nested-fields #{{:name "price" :base-type :type/Float}
+                     :database-type "map"
+                     :nested-fields #{{:name "price" :base-type :type/Float :database-type "double"}
                                       {:name "eta"
                                        :base-type :type/Dictionary
-                                       :nested-fields #{{:name "date" :base-type :type/Text}
-                                                        {:name "businessdays" :base-type :type/Integer}
-                                                        {:name "lowerboundbusinessdays" :base-type :type/Integer}}}}}}}
+                                       :database-type "map"
+                                       :nested-fields #{{:name "date" :base-type :type/Text :database-type "string"}
+                                                        {:name "businessdays" :base-type :type/Integer :database-type "int"}
+                                                        {:name "lowerboundbusinessdays" :base-type :type/Integer :database-type "int"}}}}}}}
   (parser/parse-schema {:name "options" :type (str "struct<options:struct<conventional:struct<"
                                                    "price:double,eta:struct<"
                                                    "date:string,businessdays:int,lowerboundbusinessdays:int>>>>")}))
