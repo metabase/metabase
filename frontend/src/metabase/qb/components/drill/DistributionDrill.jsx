6223a94e0473e492d0b5b240000c333c9a3c0731
Tom Robinson <tlrobinson@gmail.com>
2018-05-23 11:59:39 -0700

    Distribution drill: exclude PK, Description, Comment, SerializedJSON

--- frontend/src/metabase/qb/components/drill/DistributionDrill.jsx
+++ frontend/src/metabase/qb/components/drill/DistributionDrill.jsx
@@ -1,12 +1,20 @@
 /* @flow */
 
 import { t } from "c-3po";
-import { isPK } from "metabase/lib/schema_metadata";
+import { TYPE, isa } from "metabase/lib/types";
+import _ from "underscore";
 
 import type {
   ClickAction,
   ClickActionProps,
 } from "metabase/meta/types/Visualization";
+
+const BLACKLIST_TYPES = [
+  TYPE.PK,
+  TYPE.SerializedJSON,
+  TYPE.Description,
+  TYPE.Comment,
+];
 
 export default ({ question, clicked }: ClickActionProps): ClickAction[] => {
   if (
@@ -14,7 +22,7 @@ export default ({ question, clicked }: ClickActionProps): ClickAction[] => {
     !clicked.column ||
     clicked.value !== undefined ||
     clicked.column.source !== "fields" ||
-    isPK(clicked.column)
+    _.any(BLACKLIST_TYPES, t => isa(clicked.column.special_type, t))
   ) {
     return [];
   }
