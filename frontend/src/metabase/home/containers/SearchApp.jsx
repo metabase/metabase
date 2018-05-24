58d33180dd479c24fdde2d94022c3d43b2f8c379
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-05-24 12:19:39 -0700

    init search empty state

--- frontend/src/metabase/home/containers/SearchApp.jsx
+++ frontend/src/metabase/home/containers/SearchApp.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { jt } from "c-3po";
+import { t, jt } from "c-3po";
 import _ from "underscore";
 import Link from "metabase/components/Link";
 
-import { Box, Flex, Subhead } from "rebass";
+import { Box, Flex, Subhead, Text } from "rebass";
 
 import { withBackground } from "metabase/hoc/Background";
 import EntityListLoader from "metabase/entities/containers/EntityListLoader";
@@ -26,32 +26,48 @@ export default class SearchApp extends React.Component {
             entityQuery={this.props.location.query}
             wrapped
           >
-            {({ list }) => (
-              <Box>
-                {_.chain(list)
-                  .groupBy("type")
-                  .pairs()
-                  .value()
-                  .map(([section, items]) => (
-                    <Box mt={2} mb={3}>
-                      <div className="text-uppercase text-grey-4 text-small text-bold my1">
-                        {section}
-                      </div>
-                      <Card>
-                        {items.map(item => (
-                          <Link to={item.getUrl()}>
-                            <EntityItem
-                              name={item.getName()}
-                              iconName={item.getIcon()}
-                              iconColor={item.getColor()}
-                            />
-                          </Link>
-                        ))}
-                      </Card>
+            {({ list }) => {
+              if (list.length === 0) {
+                return (
+                  <Flex align="center" justify="center" my={4} py={4}>
+                    <Box>
+                      <img src="../app/assets/img/no_results.svg" />
                     </Box>
-                  ))}
-              </Box>
-            )}
+                    <Box mt={4}>
+                      <Subhead>{t`It's quiet around here...`}</Subhead>
+                      <Text
+                      >{t`Metabase couldn't find any results for this.`}</Text>
+                    </Box>
+                  </Flex>
+                );
+              }
+              return (
+                <Box>
+                  {_.chain(list)
+                    .groupBy("type")
+                    .pairs()
+                    .value()
+                    .map(([section, items]) => (
+                      <Box mt={2} mb={3}>
+                        <div className="text-uppercase text-grey-4 text-small text-bold my1">
+                          {section}
+                        </div>
+                        <Card>
+                          {items.map(item => (
+                            <Link to={item.getUrl()}>
+                              <EntityItem
+                                name={item.getName()}
+                                iconName={item.getIcon()}
+                                iconColor={item.getColor()}
+                              />
+                            </Link>
+                          ))}
+                        </Card>
+                      </Box>
+                    ))}
+                </Box>
+              );
+            }}
           </EntityListLoader>
         </Box>
       </Box>
