d2aa71ea01cec53222d6f0d3a7671ae8860f5175
Kyle Doherty <kdoh@users.noreply.github.com>
2018-05-31 11:43:07 -0700

    Improve Toast workflow (#7755)
    * initial version of withToast
    
    * add dark card, use for undo item
    
    * use triggerToast on AutomaticDashboardApp
    
    * betterify
    
    * fix flow

--- frontend/src/metabase/components/Card.jsx
+++ frontend/src/metabase/components/Card.jsx
@@ -3,14 +3,15 @@ import { space } from "styled-system";
 import { normal } from "metabase/lib/colors";
 
 const Card = styled.div`
-  ${space} background-color: white;
-  border: 1px solid #f5f6f7;
+  ${space} background-color: ${props => (props.dark ? "#2e353b" : "white")};
+  border: 1px solid ${props => (props.dark ? "transparent" : "#f5f6f7")};
+  ${props => props.dark && `color: white`};
   border-radius: 6px;
-  box-shadow: 0 1px 3px ${normal.grey1};
+  box-shadow: 0 1px 3px ${props => (props.dark ? "#65686b" : normal.grey1)};
   ${props =>
     props.hoverable &&
     `&:hover {
-    box-shadow: 0 2px 3px #DCE1E4;
+    box-shadow: 0 2px 3px ${props.dark ? "#2e35b" : "#DCE1E4"};
   }`};
 `;
 
