c53e1837bfca6aee5f3bf6dff1892ee9ef5c2e55
Tom Robinson <tlrobinson@gmail.com>
2018-06-05 23:11:53 -0700

    Revert some integration test changes

--- frontend/test/__support__/integrated_tests.js
+++ frontend/test/__support__/integrated_tests.js
@@ -98,15 +98,11 @@ export function useSharedNormalLogin() {
     id: process.env.TEST_FIXTURE_SHARED_NORMAL_LOGIN_SESSION_ID,
   };
 }
-export const forBothAdminsAndNormalUsers = tests => {
-  describe("for admins", () => {
-    beforeEach(useSharedAdminLogin);
-    tests();
-  });
-  describe("for normal users", () => {
-    beforeEach(useSharedNormalLogin);
-    tests();
-  });
+export const forBothAdminsAndNormalUsers = async tests => {
+  useSharedAdminLogin();
+  await tests();
+  useSharedNormalLogin();
+  await tests();
 };
 
 export function logout() {
@@ -581,24 +577,17 @@ export const eventually = async (assertion, timeout = 5000, period = 250) => {
 // })
 // afterAll(cleanup);
 //
-export const cleanup = async () => {
+export const cleanup = () => {
   useSharedAdminLogin();
-  try {
-    await Promise.all(
-      cleanup.actions.splice(0, cleanup.actions.length).map(action => action()),
-    );
-  } catch (e) {
-    console.warn("CLEANUP FAILED", e);
-    throw e;
-  }
+  Promise.all(
+    cleanup.actions.splice(0, cleanup.actions.length).map(action => action()),
+  );
 };
 cleanup.actions = [];
 cleanup.fn = action => cleanup.actions.push(action);
 cleanup.metric = metric => cleanup.fn(() => deleteMetric(metric));
 cleanup.segment = segment => cleanup.fn(() => deleteSegment(segment));
 cleanup.question = question => cleanup.fn(() => deleteQuestion(question));
-cleanup.collection = collection =>
-  cleanup.fn(() => deleteCollection(collection));
 
 export const deleteQuestion = question =>
   CardApi.delete({ cardId: getId(question) });
@@ -606,8 +595,6 @@ export const deleteSegment = segment =>
   SegmentApi.delete({ segmentId: getId(segment), revision_message: "Please" });
 export const deleteMetric = metric =>
   MetricApi.delete({ metricId: getId(metric), revision_message: "Please" });
-export const deleteCollection = collection =>
-  CollectionsApi.update({ id: getId(collection), archived: true });
 
 const getId = o =>
   typeof o === "object" && o != null
--- frontend/test/query_builder/new_question.integ.spec.js
+++ frontend/test/query_builder/new_question.integ.spec.js
@@ -86,8 +86,8 @@ describe("new question flow", async () => {
       expect(store.getPath()).toBe("/question/new");
     });
 
-    forBothAdminsAndNormalUsers(() => {
-      it("renders all options for both admins and normal users if metrics & segments exist", async () => {
+    it("renders all options for both admins and normal users if metrics & segments exist", async () => {
+      await forBothAdminsAndNormalUsers(async () => {
         const store = await createTestStore();
 
         store.pushPath(Urls.newQuestion());
@@ -181,8 +181,8 @@ describe("new question flow", async () => {
       );
     });
 
-    forBothAdminsAndNormalUsers(() => {
-      it("shows an empty state if there are no databases", async () => {
+    it("shows an empty state if there are no databases", async () => {
+      await forBothAdminsAndNormalUsers(async () => {
         await withApiMocks([[Databases.api, "list", () => []]], async () => {
           const store = await createTestStore();
 
