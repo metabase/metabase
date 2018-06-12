4858b739657351ae87d852c6c93a595004bf10ce
Tom Robinson <tlrobinson@gmail.com>
2018-06-12 15:32:50 -0700

    Port EntityMenu to use Popover

--- frontend/src/metabase/components/EntityMenu.jsx
+++ frontend/src/metabase/components/EntityMenu.jsx
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import { Motion, spring } from "react-motion";
 import cx from "classnames";
 
-import OnClickOutsideWrapper from "metabase/components/OnClickOutsideWrapper";
-
 import Card from "metabase/components/Card";
 import EntityMenuTrigger from "metabase/components/EntityMenuTrigger";
 import EntityMenuItem from "metabase/components/EntityMenuItem";
+import Popover from "metabase/components/Popover";
 
 type EntityMenuOption = {
   icon: string,
@@ -55,12 +54,19 @@ class EntityMenu extends Component {
           onClick={this.toggleMenu}
           open={open}
         />
-        {open && (
-          /* Note: @kdoh 10/12/17
-                     * React Motion has a flow type problem with children see
-                     * https://github.com/chenglou/react-motion/issues/375
-                     * TODO This can be removed if we upgrade to flow 0.53 and react-motion >= 0.5.1
-                     */
+        <Popover
+          isOpen={open}
+          onClose={this.toggleMenu}
+          hasArrow={false}
+          hasBackground={false}
+          horizontalAttachments={["right"]}
+          targetOffsetY={0}
+        >
+          {/* Note: @kdoh 10/12/17
+            * React Motion has a flow type problem with children see
+            * https://github.com/chenglou/react-motion/issues/375
+            * TODO This can be removed if we upgrade to flow 0.53 and react-motion >= 0.5.1
+            */}
           <Motion
             defaultStyle={{
               opacity: 0,
@@ -72,63 +78,59 @@ class EntityMenu extends Component {
             }}
           >
             {({ opacity, translateY }) => (
-              <OnClickOutsideWrapper handleDismissal={this.toggleMenu}>
-                <div
-                  className="absolute right"
-                  style={{
-                    top: 35,
-                    opacity: opacity,
-                    transform: `translateY(${translateY}px)`,
-                  }}
-                >
-                  <Card>
-                    {menuItemContent || (
-                      <ol className="py1" style={{ minWidth: 210 }}>
-                        {items.map(item => {
-                          if (item.content) {
-                            return (
-                              <li key={item.title}>
-                                <EntityMenuItem
-                                  icon={item.icon}
-                                  title={item.title}
-                                  action={() =>
-                                    this.replaceMenuWithItemContent(
-                                      item.content(
-                                        this.toggleMenu,
-                                        this.setFreezeMenu,
-                                      ),
-                                    )
-                                  }
-                                />
-                              </li>
-                            );
-                          } else {
-                            return (
-                              <li key={item.title}>
-                                <EntityMenuItem
-                                  icon={item.icon}
-                                  title={item.title}
-                                  action={
-                                    item.action &&
-                                    (() => {
-                                      item.action();
-                                      this.toggleMenu();
-                                    })
-                                  }
-                                  link={item.link}
-                                />
-                              </li>
-                            );
-                          }
-                        })}
-                      </ol>
-                    )}
-                  </Card>
-                </div>
-              </OnClickOutsideWrapper>
+              <div
+                style={{
+                  opacity: opacity,
+                  transform: `translateY(${translateY}px)`,
+                }}
+              >
+                <Card>
+                  {menuItemContent || (
+                    <ol className="py1" style={{ minWidth: 210 }}>
+                      {items.map(item => {
+                        if (item.content) {
+                          return (
+                            <li key={item.title}>
+                              <EntityMenuItem
+                                icon={item.icon}
+                                title={item.title}
+                                action={() =>
+                                  this.replaceMenuWithItemContent(
+                                    item.content(
+                                      this.toggleMenu,
+                                      this.setFreezeMenu,
+                                    ),
+                                  )
+                                }
+                              />
+                            </li>
+                          );
+                        } else {
+                          return (
+                            <li key={item.title}>
+                              <EntityMenuItem
+                                icon={item.icon}
+                                title={item.title}
+                                action={
+                                  item.action &&
+                                  (() => {
+                                    item.action();
+                                    this.toggleMenu();
+                                  })
+                                }
+                                link={item.link}
+                              />
+                            </li>
+                          );
+                        }
+                      })}
+                    </ol>
+                  )}
+                </Card>
+              </div>
             )}
           </Motion>
-        )}
+        </Popover>
       </div>
     );
   }
