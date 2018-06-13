127f4706bdf61b6374d159654c84dbe6ab16f7e2
Tom Robinson <tlrobinson@gmail.com>
2018-06-13 13:22:41 -0700

    Add  to /api/collection/:id items

--- src/metabase/api/collection.clj
+++ src/metabase/api/collection.clj
@@ -53,14 +53,14 @@
 
 (defmethod fetch-collection-children :card
   [_ collection {:keys [archived?]}]
-  (-> (db/select [Card :id :name :description :collection_position]
+  (-> (db/select [Card :id :name :description :collection_position :archived]
         :collection_id (:id collection)
         :archived      archived?)
       (hydrate :favorite)))
 
 (defmethod fetch-collection-children :dashboard
   [_ collection {:keys [archived?]}]
-  (db/select [Dashboard :id :name :description :collection_position]
+  (db/select [Dashboard :id :name :description :collection_position :archived]
     :collection_id (:id collection)
     :archived      archived?))
 
