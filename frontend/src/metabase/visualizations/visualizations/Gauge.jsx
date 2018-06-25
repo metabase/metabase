6fda7d203b3683e9e84cc2886acb3f5a02c0ad46
Tom Robinson <tlrobinson@gmail.com>
2018-06-25 15:37:13 -0700

    Basic gauge visualization

---
+++ frontend/src/metabase/visualizations/visualizations/Gauge.jsx
@@ -0,0 +1,95 @@
+/* @flow */
+
+import React, { Component } from "react";
+import { t } from "c-3po";
+import d3 from "d3";
+
+import { normal } from "metabase/lib/colors";
+
+import ChartSettingGaugeSegments from "metabase/visualizations/components/settings/ChartSettingGaugeSegments";
+
+import type { VisualizationProps } from "metabase/meta/types/Visualization";
+
+const OUTER_RADIUS = 50; // within 100px canvas
+const INNER_RADIUS_RATIO = 4 / 5;
+
+export default class Scalar extends Component {
+  props: VisualizationProps;
+
+  static uiName = t`Gauge`;
+  static identifier = "gauge";
+  static iconName = "number";
+
+  static minSize = { width: 3, height: 3 };
+
+  static isSensible(cols, rows) {
+    return rows.length === 1 && cols.length === 1;
+  }
+
+  static checkRenderable([{ data: { cols, rows } }]) {
+    // scalar can always be rendered, nothing needed here
+  }
+
+  static settings = {
+    "gauge.segments": {
+      section: "Display",
+      title: t`Segments`,
+      default: [
+        { value: 0, color: normal.green },
+        { value: 33, color: normal.yellow },
+        { value: 66, color: normal.red },
+        { value: 100, color: normal.gray },
+      ],
+      widget: ChartSettingGaugeSegments,
+    },
+  };
+
+  render() {
+    const { series: [{ data: { rows } }], settings, className } = this.props;
+
+    const segments = settings["gauge.segments"];
+
+    const arc = d3.svg
+      .arc()
+      .outerRadius(OUTER_RADIUS)
+      .innerRadius(OUTER_RADIUS * INNER_RADIUS_RATIO);
+
+    const angle = d3.scale
+      .linear()
+      .domain([segments[0].value, segments[segments.length - 1].value])
+      .range([-Math.PI / 2, Math.PI / 2]);
+
+    const value = rows[0][0];
+
+    const dialPosition = distance =>
+      Math.cos(angle(value) - Math.PI / 2) * distance +
+      " " +
+      Math.sin(angle(value) - Math.PI / 2) * distance;
+
+    return (
+      <div className={className}>
+        <svg viewBox="0 0 100 100">
+          <g transform={`translate(50,50)`}>
+            {segments.slice(0, -1).map((segment, index) => (
+              <path
+                d={arc({
+                  startAngle: angle(segments[index].value),
+                  endAngle: angle(segments[index + 1].value),
+                })}
+                fill={segment.color}
+              />
+            ))}
+            <path
+              d={`M${dialPosition(
+                OUTER_RADIUS * INNER_RADIUS_RATIO,
+              )} L${dialPosition(OUTER_RADIUS)} `}
+              strokeWidth={2}
+              strokeLinecap="round"
+              stroke={normal.grey2}
+            />
+          </g>
+        </svg>
+      </div>
+    );
+  }
+}
