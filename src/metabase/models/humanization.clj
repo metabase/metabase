9c99ea0ed4da27fd30e61652eb226050d25f8d6d
Doug Vargas <dvarg321@gmail.com>
2018-06-26 11:24:47 -0700

    Proper spacing in table name humanization string
    Adds spaces after periods, so
    "Metabase can attempt to transform your table and field names into more sensible, human-readable versions, e.g. "somehorriblename" becomes "Some Horrible Name".This doesn’t work all that well if the names are in a language other than English, however.Do you want us to take a guess?"
    becomes
    "Metabase can attempt to transform your table and field names into more sensible, human-readable versions, e.g. "somehorriblename" becomes "Some Horrible Name". This doesn’t work all that well if the names are in a language other than English, however. Do you want us to take a guess?"

--- src/metabase/models/humanization.clj
+++ src/metabase/models/humanization.clj
@@ -114,7 +114,7 @@
 
 (defsetting ^{:added "0.28.0"} humanization-strategy
   (str (tru "Metabase can attempt to transform your table and field names into more sensible, human-readable versions, e.g. \"somehorriblename\" becomes \"Some Horrible Name\".")
-       (tru "This doesn’t work all that well if the names are in a language other than English, however.")
-       (tru "Do you want us to take a guess?"))
+       (tru " This doesn’t work all that well if the names are in a language other than English, however.")
+       (tru " Do you want us to take a guess?"))
   :default "advanced"
   :setter  set-humanization-strategy!)
