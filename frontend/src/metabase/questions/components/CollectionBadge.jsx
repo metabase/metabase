99c54fef3ea106739fd53467110a8bfe809b9e3d
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-07-12 14:23:40 -0700

    inline

--- frontend/src/metabase/questions/components/CollectionBadge.jsx
+++ frontend/src/metabase/questions/components/CollectionBadge.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Flex } from "grid-styled";
 import { Link } from "react-router";
 
 import Icon from "metabase/components/Icon";
@@ -17,14 +18,13 @@ class CollectionBadge extends React.Component {
   render() {
     const { object } = this.props;
     return (
-      <Link
-        to={Urls.collection(object.id)}
-        className={cx("flex align-center link")}
-      >
-        <Icon name={object.getIcon()} mr={1} />
-        <h5 className="text-uppercase" style={{ fontWeight: 900 }}>
-          {object.name}
-        </h5>
+      <Link to={Urls.collection(object.id)} className={cx("inline-block link")}>
+        <Flex align="center">
+          <Icon name={object.getIcon()} mr={1} />
+          <h5 className="text-uppercase" style={{ fontWeight: 900 }}>
+            {object.name}
+          </h5>
+        </Flex>
       </Link>
     );
   }
