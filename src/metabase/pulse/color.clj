df2645002e5e249a04da31e86542f3ebd9c78dfd
Ryan Senior <ryan@metabase.com>
2018-07-16 15:29:11 -0500

    Convert visualization settings and columns to JS data
    Passing in the clojure data structures directly to the Nashorn code
    causes Java objects to be used that don't have all of the functions
    that regular Javascript objects do, such as `map`. This commit
    dumps that data into a JS array/map before invoking the Nashorn code.

--- src/metabase/pulse/color.clj
+++ src/metabase/pulse/color.clj
@@ -2,12 +2,12 @@
   "Namespaces that uses the Nashorn javascript engine to invoke some shared javascript code that we use to determine
   the background color of pulse table cells"
   (:require [clojure.walk :as walk]
-            [cheshire.core :as json]
+            [metabase.query-processor.util :as qputil]
             [puppetlabs.i18n.core :refer [trs]]
             [schema.core :as s])
   (:import java.io.InputStream
-           [javax.script Invocable ScriptEngineManager]
-           jdk.nashorn.api.scripting.JSObject))
+           [javax.script Invocable ScriptEngine ScriptEngineManager]
+           [jdk.nashorn.api.scripting JSObject ScriptObjectMirror]))
 
 (defn- make-js-engine-with-script [^String script]
   (let [engine-mgr (ScriptEngineManager.)
@@ -38,16 +38,45 @@
    :rows [[s/Any]]
    s/Any s/Any})
 
+(defn- map->js-map [m]
+  (let [^ScriptEngine engine       @js-engine
+        ^ScriptObjectMirror js-map (.eval engine "var map = {}; map")]
+    (doseq [[k v] m]
+      (.put js-map (name k) v))
+    js-map))
+
+(defn- coll->js-array [coll]
+  (let [^ScriptEngine engine         @js-engine
+        ^ScriptObjectMirror js-array (.eval engine "var array = []; array")]
+    (doseq [[idx item] (partition-all 2 (interleave (range) coll))]
+      (.put js-array (str idx) item))
+    js-array))
+
+(defn- convert-to-js-data
+  "When passing in function arguments from Clojure, it doesn't create proper JS maps and arrays but rather just uses
+  the Java objects. Not all JS functions work on these Java objects so this function walks the Clojure data structure
+  and dumps the data into a JS data structure. This ensures we get a real map/array that supports all of the regular
+  JS functions"
+  [x]
+  (->> x
+       (qputil/postwalk-pred map? map->js-map)
+       ;; This is for converting vectors/lists to a JS data type. This is handled separately from the map conversion
+       ;; (in a separate pass) because clojure.walk will also pass key value pairs in as a vector. Converting maps
+       ;; first, then converting vectors/lists prevents that issue
+       (qputil/postwalk-pred coll? coll->js-array)))
+
 (s/defn make-color-selector
   "Returns a curried javascript function (object) that can be used with `get-background-color` for delegating to JS
   code to pick out the correct color for a given cell in a pulse table. The logic for picking a color is somewhat
   complex, but defined in a set of rules in `viz-settings`. There are some colors that are picked based on a
   particular cell value, others affect the row, so it's necessary to call this once for the resultset and then
   `get-background-color` on each cell."
-  [query-results :- QueryResults, viz-settings]
+  [{:keys [cols rows]} :- QueryResults, viz-settings]
   (let [^Invocable engine @js-engine
-        ;; Keyword strings don't serialize correctly when being passed to the JS engine
-        js-fn-args (object-array [(json/generate-string query-results) (json/generate-string viz-settings)])]
+        ;; Ideally we'd convert everything to JS data before invoking the function below, but converting rows would be
+        ;; expensive. The JS code is written to deal with `rows` in it's native Nashorn format but since `cols` and
+        ;; `viz-settings` are small, convert those so that they're easier to work with once in JS code
+        js-fn-args (object-array [rows (convert-to-js-data cols) (convert-to-js-data viz-settings)])]
     (.invokeFunction engine "makeCellBackgroundGetter" js-fn-args)))
 
 (defn get-background-color
--- test/metabase/pulse/color_test.clj
+++ test/metabase/pulse/color_test.clj
@@ -6,7 +6,8 @@
 (def ^:private green "#00ff00")
 
 (def ^:private ^String test-script
-  "function makeCellBackgroundGetter(data, settings) {
+  "function makeCellBackgroundGetter(rows, cols, settings) {
+     cols.map(function (a) { return a; });
      return function(value, rowIndex, columnName) {
         if(rowIndex % 2 == 0){
           return settings[\"even\"]
@@ -19,14 +20,14 @@
 (defmacro ^:private with-test-js-engine
   "Setup a javascript engine with a stubbed script useful making sure `get-background-color` works independently from
   the real color picking script"
-  [& body]
-  `(with-redefs [color/js-engine (delay (#'color/make-js-engine-with-script test-script))]
+  [script & body]
+  `(with-redefs [color/js-engine (delay (#'color/make-js-engine-with-script ~script))]
      ~@body))
 
 ;; The test script above should return red on even rows, green on odd rows
 (expect
   [red green red green]
-  (with-test-js-engine
+  (with-test-js-engine test-script
     (let [color-selector (make-color-selector {:cols [{:name "test"}]
                                                :rows [[1] [2] [3] [4]]}
                                               {"even" red, "odd" green})]
@@ -37,9 +38,30 @@
 ;; when passed to a nashorn function
 (expect
   [red green red green]
-  (with-test-js-engine
+  (with-test-js-engine test-script
     (let [color-selector (make-color-selector {:cols [{:name "test"}]
                                                :rows [[1] [2] [3] [4]]}
                                               {:even red, :odd  green})]
       (for [row-index (range 0 4)]
         (get-background-color color-selector "any value" "any column" row-index)))))
+
+(def ^:private ^String test-conversion-script
+  "function addTestNumbers(data) {
+     var total = 0;
+     data.map(function (m){
+                total = total + m[\"test\"];
+              });
+     return total;
+   }")
+
+;; Test the conversion of clojure data to JS data. This just adds up all of the `"test"` values to ensure we can map
+;; over the arrays and access keys in the map like we would any other JS array/map
+;;
+;; Passing in a vector without convert it will cause this test to fail as the Java objects that get passed in don't
+;; support `map`
+(expect
+  10.0
+  (with-test-js-engine test-conversion-script
+    (.invokeFunction (deref (var-get #'color/js-engine))
+           "addTestNumbers"
+           (object-array [(#'color/convert-to-js-data (vec (repeat 10 {"test" 1, "not-test" 2})))]))))
