436bea31f8796a0b2bb34224fd8ea70791b68c9a
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-07-31 15:40:26 -0700

    refactor EmptyState and apply to search / collection landing

--- frontend/src/metabase/components/EmptyState.jsx
+++ frontend/src/metabase/components/EmptyState.jsx
@@ -1,83 +1,60 @@
 /* @flow */
 import React from "react";
-import { Link } from "react-router";
-import cx from "classnames";
+import { Box, Flex } from "grid-styled";
+
+import Button from "metabase/components/Button";
+import Link from "metabase/components/Link";
+import Text from "metabase/components/Text";
+
 /*
  * EmptyState is a component that can
  *  1) introduce a new section of Metabase to a user and encourage the user to take an action
  *  2) indicate an empty result after a user-triggered search/query
  */
 
-import Icon from "metabase/components/Icon.jsx";
-
 type EmptyStateProps = {
-  message: string | React$Element<any>,
+  message: React$Element<any>,
   title?: string,
-  icon?: string,
-  image?: string,
-  imageHeight?: string, // for reducing ui flickering when the image is loading
-  imageClassName?: string,
   action?: string,
   link?: string,
+  illustrationElement: React$Element<any>,
   onActionClick?: () => void,
-  smallDescription?: boolean,
 };
 
 const EmptyState = ({
   title,
   message,
-  icon,
-  image,
-  imageHeight,
-  imageClassName,
   action,
   link,
+  illustrationElement,
   onActionClick,
-  smallDescription = false,
+  ...rest
 }: EmptyStateProps) => (
-  <div
-    className="text-centered text-brand-light my2"
-    style={smallDescription ? {} : { width: "350px" }}
-  >
-    {title && <h2 className="text-brand mb4">{title}</h2>}
-    {icon && <Icon name={icon} size={40} />}
-    {image && (
-      <img
-        src={`${image}.png`}
-        width="300px"
-        height={imageHeight}
-        alt={message}
-        srcSet={`${image}@2x.png 2x`}
-        className={imageClassName}
-      />
-    )}
-    <div className="flex justify-center">
-      <h2
-        className={cx("text-light text-normal mt2 mb4", {
-          "text-paragraph": smallDescription,
-        })}
-        style={{ lineHeight: "1.5em" }}
-      >
-        {message}
-      </h2>
-    </div>
-    {action &&
-      link && (
-        <Link
-          to={link}
-          className="Button Button--primary mt4"
-          target={link.startsWith("http") ? "_blank" : ""}
-        >
-          {action}
-        </Link>
-      )}
-    {action &&
-      onActionClick && (
-        <a onClick={onActionClick} className="Button Button--primary mt4">
-          {action}
-        </a>
-      )}
-  </div>
+  <Box {...rest}>
+    <Flex justify="center" flexDirection="column" align="center">
+      <Box>{illustrationElement && illustrationElement}</Box>
+      {title && <h2>{title}</h2>}
+      {message && <Text color="medium">{message}</Text>}
+    </Flex>
+    <Box mt={2}>
+      {action &&
+        link && (
+          <Link
+            to={link}
+            mt={4}
+            target={link.startsWith("http") ? "_blank" : ""}
+          >
+            <Button primary>{action}</Button>
+          </Link>
+        )}
+      {action &&
+        onActionClick && (
+          <Button onClick={onActionClick} primary mt={4}>
+            {action}
+          </Button>
+        )}
+    </Box>
+  </Box>
 );
 
 export default EmptyState;
