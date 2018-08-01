92044850a0bd38532b858c8539bdc45f14cec95f
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-07-31 18:09:42 -0700

    clean up component

--- frontend/src/metabase/components/EmptyState.jsx
+++ frontend/src/metabase/components/EmptyState.jsx
@@ -6,12 +6,6 @@ import Button from "metabase/components/Button";
 import Link from "metabase/components/Link";
 import Text from "metabase/components/Text";
 
-/*
- * EmptyState is a component that can
- *  1) introduce a new section of Metabase to a user and encourage the user to take an action
- *  2) indicate an empty result after a user-triggered search/query
- */
-
 type EmptyStateProps = {
   message: React$Element<any>,
   title?: string,
@@ -32,7 +26,7 @@ const EmptyState = ({
 }: EmptyStateProps) => (
   <Box {...rest}>
     <Flex justify="center" flexDirection="column" align="center">
-      <Box>{illustrationElement && illustrationElement}</Box>
+      {illustrationElement && <Box mb={[2, 3]}>{illustrationElement}</Box>}
       {title && <h2>{title}</h2>}
       {message && <Text color="medium">{message}</Text>}
     </Flex>
