eb834259b705e6de5d8d43631dc130f16b12139d
Maz Ameli <maz@metabase.com>
2018-08-06 17:26:17 -0700

    move toggle to right

--- frontend/src/metabase/admin/permissions/containers/TogglePropagateAction.jsx
+++ frontend/src/metabase/admin/permissions/containers/TogglePropagateAction.jsx
@@ -22,8 +22,8 @@ const TogglePropagateAction = connect(mapStateToProps, mapDispatchToProps)(
       className="flex align-center bg-medium px2 py1 cursor-pointer"
       onClick={() => setPropagatePermissions(!propagate)}
     >
+      <span className="mr2 text-small">{t`Also change sub-collections`}</span>
       <Toggle small value={propagate} />
-      <span className="ml1 text-small">{t`Also change sub-collections`}</span>
     </div>
   ),
 );
