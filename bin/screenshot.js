7d2adfaadcaf0d086944d68e8dd951ba51ac5d36
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-08-10 12:18:18 -0700

    trial gif support [ci skip]

--- bin/screenshot.js
+++ bin/screenshot.js
@@ -1,5 +1,12 @@
+#!/usr/bin/env node
+
 import puppeteer from "puppeteer";
 import creds from "./creds";
+
+import tempdir from "tempdir";
+import fs from "fs";
+import pngFileStream from "png-file-stream";
+import GIFEncoder from "gifencoder";
 
 const CAPTURES = [
   {
@@ -17,6 +24,7 @@ const CAPTURES = [
       page.waitForSelector(".LineAreaBarChart"),
       page.click(".AddButton"),
     ],
+    gif: true,
   },
 ];
 
@@ -31,7 +39,60 @@ const BASE_URL = "http://localhost:3000";
 (async () => {
   const browser = await puppeteer.launch();
 
-  async function capturePage({ url, imagePath, setup }) {
+  // Basically a rip of webgif. Thanks to https://github.com/anishkny/webgif for
+  // the inspiration to add gif support
+  async function captureGif({ url, imagePath, setup, gif, duration = 10 }) {
+    const page = await browser.newPage();
+
+    // create a directory to store our sequential pngs until we create the gif
+    const workingDir = await tempdir();
+
+    page.setViewport({
+      width: 1024,
+      height: 768,
+    });
+
+    if (!setup) {
+      console.log(
+        "In order to capture a gif something needs to be happening. Define `setup` steps",
+      );
+      return false;
+    }
+
+    console.log(`GIF Capture for ${url}`);
+
+    // create an array to store our screenshot promises
+    const screenshotPromises = [];
+
+    try {
+      await page.goto(`${BASE_URL}${url}`, {
+        waitUntil: ["load"],
+        timeout: 12000,
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+
+    // loop once for each "frame" of the gif
+    for (let i = 0; i < duration; i++) {
+      let filename = `${workingDir}/T${new Date().getTime()}.png`;
+      process.stdout.write(".");
+      await setup(page)[i];
+      screenshotPromises.push(page.screenshot({ path: filename }));
+      await delay(1000);
+    }
+
+    await delay(1000);
+    await Promise.all(screenshotPromises);
+    console.log(`\nEncoding GIF: ${imagePath}`);
+    const encoder = new GIFEncoder(1024, 768);
+    await pngFileStream(`${workingDir}/T*png`)
+      .pipe(encoder.createWriteStream({ repeat: 0, delay: 200, quality: 20 }))
+      .pipe(fs.createWriteStream(`${imagePath}.gif`));
+    await page.close();
+  }
+
+  async function capturePage({ url, imagePath, setup, gif }) {
     const page = await browser.newPage();
 
     page.setViewport(PAGE_CONFIG);
@@ -90,9 +151,13 @@ const BASE_URL = "http://localhost:3000";
   await login();
 
   for (let capture of CAPTURES) {
-    await capturePage(capture);
+    capture.gif ? await captureGif(capture) : await capturePage(capture);
   }
 
   console.log("Tasks complete, closing browser");
   browser.close();
 })();
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
