39d9f4d337a446a1d74d7f4a414b48606f8deab1
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-08-15 16:29:35 -0700

    tweak download widget

--- frontend/src/metabase/components/DownloadButton.jsx
+++ frontend/src/metabase/components/DownloadButton.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Box, Flex } from "grid-styled";
 
+import { Absolute } from "metabase/components/Position";
+
+import colors from "metabase/lib/colors";
 import { extractQueryParams } from "metabase/lib/urls";
 
-import Button from "metabase/components/Button.jsx";
+import Icon from "metabase/components/Icon";
+import Text from "metabase/components/Text";
+
+function iconForType(type) {
+  return "document";
+}
+
+function colorForType(type) {
+  switch (type) {
+    case "csv":
+      return colors["accent7"];
+    case "xlsx":
+      return colors["accent1"];
+    case "json":
+      return colors["bg-dark"];
+    default:
+      return colors["brand"];
+  }
+}
 
 const DownloadButton = ({
-  className,
-  style,
   children,
   method,
   url,
@@ -15,22 +35,32 @@ const DownloadButton = ({
   extensions,
   ...props
 }) => (
-  <form className={className} style={style} method={method} action={url}>
-    {params && extractQueryParams(params).map(getInput)}
-    <Button
-      onClick={e => {
-        if (window.OSX) {
-          // prevent form from being submitted normally
-          e.preventDefault();
-          // download using the API provided by the OS X app
-          window.OSX.download(method, url, params, extensions);
-        }
-      }}
-      {...props}
-    >
-      {children}
-    </Button>
-  </form>
+  <Box>
+    <form method={method} action={url}>
+      {params && extractQueryParams(params).map(getInput)}
+      <Flex
+        className="text-brand-hover cursor-pointer"
+        align="center"
+        onClick={e => {
+          if (window.OSX) {
+            // prevent form from being submitted normally
+            e.preventDefault();
+            // download using the API provided by the OS X app
+            window.OSX.download(method, url, params, extensions);
+          }
+        }}
+        {...props}
+      >
+        <Icon
+          name={iconForType(children)}
+          size={32}
+          mr={1}
+          color={colorForType(children)}
+        />
+        <Text>.{children}</Text>
+      </Flex>
+    </form>
+  </Box>
 );
 
 const getInput = ([name, value]) => (
@@ -38,17 +68,13 @@ const getInput = ([name, value]) => (
 );
 
 DownloadButton.propTypes = {
-  className: PropTypes.string,
-  style: PropTypes.object,
   url: PropTypes.string.isRequired,
   method: PropTypes.string,
   params: PropTypes.object,
-  icon: PropTypes.string,
   extensions: PropTypes.array,
 };
 
 DownloadButton.defaultProps = {
-  icon: "downarrow",
   method: "POST",
   params: {},
   extensions: [],
