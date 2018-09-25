95a2f96c692b492b09dac0b69c345ec1a407f1d1
Cam Saul <cammsaul@gmail.com>
2018-09-24 18:13:38 -0700

    QP refactor part 6 - rework add-dimension-projections [ci drivers]

--- src/metabase/models/dimension.clj
+++ src/metabase/models/dimension.clj
@@ -1,4 +1,7 @@
 (ns metabase.models.dimension
+  "Dimensions are used to define remappings for Fields handled automatically when those Fields are encountered by the
+  Query Processor. For a more detailed explanation, refer to the documentation in
+  `metabase.query-processor.middleware.add-dimension-projections`."
   (:require [toucan.models :as models]
             [metabase.util :as u]))
 
