8f31988c2eeafb7bcbc062a8f7e0d2049868b4bc
Simon Belak <simon@metabase.com>
2018-09-28 20:03:04 +0200

    Add query cancelation

--- src/metabase/api/metaml.clj
+++ src/metabase/api/metaml.clj
@@ -10,7 +10,7 @@
             [metabase.models.query.permissions :as query-perms]
             [metabase.util :as u]))
 
-(api/defendpoint GET "/fill-nils/card/:card-id/:field-id"
+(api/defendpoint GET "/fill-nils/card/:card-id/field/:field-id"
   "Fill nils in field `FIELD-ID` in results of card `CARD-ID`.
    Card has to return raw data or some subset thereof."
   [card-id field-id]
@@ -19,7 +19,9 @@
     (assert (-> card :database_id Database :engine (= :bigquery)))
     (assert (-> card :dataset_query :query :aggregation nil?))
     (assert (-> card :dataset_query :query :breakout nil?))
-    (metaml/fill-nils (:dataset_query card) field)))
+    (api/cancellable-json-response
+     (fn []
+       (metaml/fill-nils (:dataset_query card) field)))))
 
 (api/defendpoint GET "/fill-nils/field/:id"
   "Fill nils in field `ID`."
@@ -27,9 +29,11 @@
   (let [field (api/read-check Field id)
         table (-> field :table_id Table)]
     (assert (-> table :db_id Database :engine (= :bigquery)))
-    (metaml/fill-nils {:database (:db_id table)
-                       :type     :query
-                       :query    {:source-table (u/get-id table)}}
-                      field)))
+    (api/cancellable-json-response
+     (fn []
+       (metaml/fill-nils {:database (:db_id table)
+                          :type     :query
+                          :query    {:source-table (u/get-id table)}}
+                         field)))))
 
 (api/define-routes)
