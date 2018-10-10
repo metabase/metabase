f76f568bc9fd716c7c392ad89fdb74e380c1253c
Cam Saul <cammsaul@gmail.com>
2018-10-10 08:55:31 -0700

    QP refactor part 8c [ci drivers]

---
+++ test/metabase/query_processor/middleware/catch_exceptions_test.clj
@@ -0,0 +1,17 @@
+(ns metabase.query-processor.middleware.catch-exceptions-test
+  (:require [expectations :refer [expect]]
+            [metabase.query-processor.middleware.catch-exceptions :as catch-exceptions]))
+
+(expect
+  {}
+  ((catch-exceptions/catch-exceptions identity) {}))
+
+(expect
+  {:status         :failed
+   :class          java.lang.Exception
+   :error          "Something went wrong"
+   :stacktrace     true
+   :expanded-query nil
+   :query          {}}
+  (-> ((catch-exceptions/catch-exceptions (fn [_] (throw (Exception. "Something went wrong")))) {})
+      (update :stacktrace boolean)))
