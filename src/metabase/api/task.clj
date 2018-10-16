eb500a26adde8dc2999c89905725f29453d6663b
Kyle Doherty <kyle.l.doherty@gmail.com>
2018-10-16 11:46:17 -0700

    clean up task api based on feedback

--- src/metabase/api/task.clj
+++ src/metabase/api/task.clj
@@ -2,25 +2,21 @@
   "/api/task endpoints"
   (:require
     [compojure.core :refer [GET]]
-    [metabase
-     [util :as u]]
-    [metabase.api
-     [common :as api]]
-    [metabase.models
-     [task-history :as tasks :refer [TaskHistory]]]
-    [toucan
-     [db :as db]]))
+    [metabase.util :as u]
+    [metabase.api.common :as api]
+    [metabase.models.task-history :as tasks :refer [TaskHistory]]
+    [toucan.db :as db]))
 
 
 (api/defendpoint GET "/"
   "Fetch a list of recent tasks stored as Task History"
   []
-  (as-> (db/select TaskHistory) tasks))
+  (api/check-superuser)
+  (db/select TaskHistory))
 
 (api/defendpoint GET "/:id"
   "Get `TaskHistory` entry with ID."
   [id]
-  (u/prog1 (-> (TaskHistory id)
-               api/read-check)))
+  (api/read-check TaskHistory id))
 
 (api/define-routes)
