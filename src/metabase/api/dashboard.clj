9654a2c41c96a6c7c3419bffb8c408e32450ce13
MarkRx <MarkRx@users.noreply.github.com>
2018-11-09 16:08:48 -0500

    Clojure PR changes; Unit test improvements

--- src/metabase/api/dashboard.clj
+++ src/metabase/api/dashboard.clj
@@ -188,16 +188,16 @@
   (update dashboard :ordered_cards add-query-average-duration-to-dashcards))
 
 
-(defn get-dashboard
+(defn- get-dashboard
   "Get `Dashboard` with ID."
   [id]
-  (u/prog1 (-> (Dashboard id)
+  (-> (Dashboard id)
                api/check-404
                (hydrate [:ordered_cards :card :series] :can_write)
                api/read-check
                api/check-not-archived
                hide-unreadable-cards
-               add-query-average-durations)))
+               add-query-average-durations))
 
 
 (api/defendpoint POST "/:from-dashboard-id/copy"
@@ -209,24 +209,24 @@
    collection_position (s/maybe su/IntGreaterThanZero)}
   ;; if we're trying to save the new dashboard in a Collection make sure we have permissions to do that
   (collection/check-write-perms-for-collection collection_id)
-  (let [existing-dashboard (get-dashboard from-dashboard-id)]
-    (let [dashboard-data {:name                (or name (existing-dashboard :name))
-                          :description         (or description (existing-dashboard :description))
-                          :parameters          (or (existing-dashboard :parameters) [])
-                          :creator_id          api/*current-user-id*
-                          :collection_id       collection_id
-                          :collection_position collection_position}
-
-          dashboard      (db/transaction
-                           ;; Adding a new dashboard at `collection_position` could cause other dashboards in this collection to change
-                           ;; position, check that and fix up if needed
-                           (api/maybe-reconcile-collection-position! dashboard-data)
-                           ;; Ok, now save the Dashboard
-                           (u/prog1 (db/insert! Dashboard dashboard-data)
-                                    ;; Get cards from existing dashboard and associate to copied dashboard
-                                    (doseq [card (existing-dashboard :ordered_cards)]
-                                      (u/prog1 (api/check-500 (dashboard/add-dashcard! <> (card :card_id) card))))))]
-      (events/publish-event! :dashboard-create dashboard))))
+  (let [
+      existing-dashboard (get-dashboard from-dashboard-id)
+      dashboard-data {:name                  (or name (:name existing-dashboard))
+                        :description         (or description (:description existing-dashboard))
+                        :parameters          (or (:parameters existing-dashboard) [])
+                        :creator_id          api/*current-user-id*
+                        :collection_id       collection_id
+                        :collection_position collection_position}
+      dashboard      (db/transaction
+                          ;; Adding a new dashboard at `collection_position` could cause other dashboards in this collection to change
+                          ;; position, check that and fix up if needed
+                          (api/maybe-reconcile-collection-position! dashboard-data)
+                          ;; Ok, now save the Dashboard
+                          (u/prog1 (db/insert! Dashboard dashboard-data)
+                                  ;; Get cards from existing dashboard and associate to copied dashboard
+                                  (doseq [card (existing-dashboard :ordered_cards)]
+                                    (api/check-500 (dashboard/add-dashcard! <> (card :card_id) card)))))]
+    (events/publish-event! :dashboard-create dashboard)))
 
 
 ;;; --------------------------------------------- Fetching/Updating/Etc. ---------------------------------------------
