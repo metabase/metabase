cd2f3f79d025a3ee6c695e682a75fdfd6ac801a9
yunusdogan <yunusdgntr@gmail.com>
2018-12-10 09:42:28 +0300

    language changed

--- frontend/src/metabase/app-public.js
+++ frontend/src/metabase/app-public.js
@@ -3,4 +3,7 @@ import { init } from "./app";
 import { getRoutes } from "./routes-public.jsx";
 import reducers from "./reducers-public";
 
+import moment from "moment";
+moment.locale("tr");
+
 init(reducers, getRoutes, () => {});
