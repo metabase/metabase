a0adcc98f6560c2b567f51b24a61af6d4601fc39
Tom Robinson <tlrobinson@gmail.com>
2019-01-22 11:44:59 -0800

    Misc cleanup / flow fixes

--- frontend/src/metabase/query_builder/components/BreakoutName.jsx
+++ frontend/src/metabase/query_builder/components/BreakoutName.jsx
@@ -1,16 +1,18 @@
+/* @flow */
+
 import React from "react";
 
-import type { Breakout as BreakoutT } from "metabase/meta/types/Query";
-import Metadata from "metabase-lib/lib/metadata/Metadata";
+import type { Breakout } from "metabase/meta/types/Query";
+import StructuredQuery from "metabase-lib/lib/queries/StructuredQuery";
 
 import FieldName from "./FieldName.jsx";
 
 type Props = {
-  breakout: BreakoutT,
-  query: any,
+  breakout: Breakout,
+  query: StructuredQuery,
 };
 
-const BreakoutName = ({ breakout, query, ...props }) => (
+const BreakoutName = ({ breakout, query, ...props }: Props) => (
   <FieldName field={breakout} query={query} {...props} />
 );
 
