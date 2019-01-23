fa1f957ffa9143ffb7c20f8afb158473af2b4092
Kyle Doherty <kdoh@users.noreply.github.com>
2019-01-23 13:22:01 -0800

    require recipient for test email in pulses (#8291)
    * require recipient for test email
    
    * only apply recipient requirement to channel test button

--- frontend/src/metabase/pulse/components/PulseEditChannels.jsx
+++ frontend/src/metabase/pulse/components/PulseEditChannels.jsx
@@ -18,8 +18,6 @@ import MetabaseAnalytics from "metabase/lib/analytics";
 
 import { channelIsValid, createChannel } from "metabase/lib/pulse";
 
-import cx from "classnames";
-
 export const CHANNEL_ICONS = {
   email: "mail",
   slack: "slack",
@@ -231,7 +229,12 @@ export default class PulseEditChannels extends Component {
           <div className="pt2">
             <ActionButton
               actionFn={this.onTestPulseChannel.bind(this, channel)}
-              className={cx("Button", { disabled: !isValid })}
+              disabled={
+                !isValid ||
+                /* require at least one email recipient to allow email pulse testing */
+                (channelSpec.type === "email" &&
+                  channel.recipients.length === 0)
+              }
               normalText={
                 channelSpec.type === "email"
                   ? t`Send email now`
