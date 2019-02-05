5324041233352031d1ead61eff140a71faad5078
Cam Saul <cammsaul@gmail.com>
2019-02-04 19:38:11 -0800

    Tasks cleanup & admin troubleshooting page

---
+++ frontend/src/metabase/admin/tasks/containers/JobTriggersModal.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { t } from "c-3po";
+import { connect } from "react-redux";
+import { goBack } from "react-router-redux";
+import _ from "underscore";
+
+import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper.jsx";
+import ModalContent from "metabase/components/ModalContent";
+import { Box } from "grid-styled";
+
+import { fetchJobInfo } from "../jobInfo";
+
+const renderTriggersTable = triggers => {
+  return (
+    <table className="ContentTable mt2">
+      <thead>
+        <tr>
+          <th>{t`Key`}</th>
+          <th>{t`Description`}</th>
+          <th>{t`State`}</th>
+          <th>{t`Priority`}</th>
+          <th>{t`Last Fired`}</th>
+          <th>{t`Next Fire Time`}</th>
+          <th>{t`Start Time`}</th>
+          <th>{t`End Time`}</th>
+          <th>{t`Final Fire Time`}</th>
+          <th>{t`May Fire Again?`}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {triggers &&
+          triggers.map(trigger => (
+            <tr key={trigger.key}>
+              <td className="text-bold">{trigger.key}</td>
+              <td>{trigger.description}</td>
+              <td>{trigger.state}</td>
+              <td>{trigger.priority}</td>
+              <td>{trigger["previous-fire-time"]}</td>
+              <td>{trigger["next-fire-time"]}</td>
+              <td>{trigger["start-time"]}</td>
+              <td>{trigger["end-time"]}</td>
+              <td>{trigger["final-fire-time"]}</td>
+              <td>{trigger["may-fire-again?"] ? t`Yes` : t`No`}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
+
+@connect(null, { fetchJobInfo, goBack })
+export default class JobTriggersModal extends React.Component {
+  async componentDidMount() {
+    try {
+      const { jobKey } = this.props.params;
+      const jobs = jobKey && (await this.props.fetchJobInfo()).payload.jobs;
+      const job = jobs && _.findWhere(jobs, { key: jobKey });
+      const triggers = (job && job.triggers) || [];
+
+      this.setState({ triggers, error: null });
+    } catch (error) {
+      this.setState({ error });
+    }
+  }
+
+  render() {
+    const { jobKey, goBack } = this.props.params;
+    const { triggers, error } = this.state || {};
+
+    return (
+      <ModalContent title={t`Triggers for ${jobKey}`} onClose={goBack}>
+        <LoadingAndErrorWrapper loading={!triggers} error={error}>
+          <Box p={3}>{renderTriggersTable(triggers)}</Box>
+        </LoadingAndErrorWrapper>
+      </ModalContent>
+    );
+  }
+}
--- src/metabase/task/upgrade_checks.clj
+++ src/metabase/task/upgrade_checks.clj
@@ -13,12 +13,6 @@
              [task :as task]]
             [metabase.util.i18n :refer [trs]]))
 
-(def ^:private ^:const job-key     "metabase.task.upgrade-checks.job")
-(def ^:private ^:const trigger-key "metabase.task.upgrade-checks.trigger")
-
-(defonce ^:private job     (atom nil))
-(defonce ^:private trigger (atom nil))
-
 (defn- get-version-info []
   (let [version-info-url      (config/config-str :mb-version-info-url)
         {:keys [status body]} (http/get version-info-url {:content-type "application/json"})]
@@ -27,8 +21,7 @@
     (json/parse-string body keyword)))
 
 ;; simple job which looks up all databases and runs a sync on them
-(jobs/defjob CheckForNewVersions
-  [ctx]
+(jobs/defjob CheckForNewVersions [_]
   (when (public-settings/check-for-updates)
     (log/debug (trs "Checking for new Metabase version info."))
     (try
@@ -36,21 +29,19 @@
       (when-let [version-info (get-version-info)]
         (public-settings/version-info version-info))
       (catch Throwable e
-        (log/error e (trs "Error fetching version info: "))))))
+        (log/error e (trs "Error fetching version info"))))))
 
-(defn task-init
-  "Job initialization"
-  []
-  ;; build our job
-  (reset! job (jobs/build
-               (jobs/of-type CheckForNewVersions)
-               (jobs/with-identity (jobs/key job-key))))
-  ;; build our trigger
-  (reset! trigger (triggers/build
-                   (triggers/with-identity (triggers/key trigger-key))
-                   (triggers/start-now)
-                   (triggers/with-schedule
-                     ;; run twice a day
-                     (cron/cron-schedule "0 15 6,18 * * ? *"))))
-  ;; submit ourselves to the scheduler
-  (task/schedule-task! @job @trigger))
+(def ^:private job-key     "metabase.task.upgrade-checks.job")
+(def ^:private trigger-key "metabase.task.upgrade-checks.trigger")
+
+(defmethod task/init! ::CheckForNewVersions [_]
+  (let [job     (jobs/build
+                 (jobs/of-type CheckForNewVersions)
+                 (jobs/with-identity (jobs/key job-key)))
+        trigger (triggers/build
+                 (triggers/with-identity (triggers/key trigger-key))
+                 (triggers/start-now)
+                 (triggers/with-schedule
+                   ;; run twice a day
+                   (cron/cron-schedule "0 15 6,18 * * ? *")))]
+    (task/schedule-task! job trigger)))
