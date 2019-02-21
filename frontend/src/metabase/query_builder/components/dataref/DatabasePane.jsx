707f88e34db07b615cd2f6ad024eadeec6acca2e
Maz Ameli <maz@metabase.com>
2019-02-21 15:52:41 -0800

    change layout for list of all databases

--- frontend/src/metabase/query_builder/components/dataref/DatabasePane.jsx
+++ frontend/src/metabase/query_builder/components/dataref/DatabasePane.jsx
@@ -1,9 +1,47 @@
+/* eslint "react/prop-types": "warn" */
 import React from "react";
+import PropTypes from "prop-types";
+import { t } from "c-3po";
+import { isQueryable } from "metabase/lib/table";
+import Icon from "metabase/components/Icon.jsx";
 
-import MainPane from "./MainPane";
+import cx from "classnames";
 
-const DatabasePane = ({ database, ...props }) => (
-  <MainPane {...props} databases={[database]} />
+const DatabasePane = ({ database, show, ...props }) => (
+  <div>
+    <div className="ml1 my2 flex align-center justify-between border-bottom pb1">
+      <div className="flex align-center">
+        <Icon name="database" className="text-medium pr1" size={14} />
+        <h3>{database.name}</h3>
+      </div>
+      <div className="flex align-center">
+        <Icon name="table2" className="text-light pr1" size={12} />
+        <span className="text-medium">{database.tables.length}</span>
+      </div>
+    </div>
+
+    <ul>
+      {database.tables.filter(isQueryable).map((table, index) => (
+        <li
+          key={table.id}
+          className={cx("", {
+            "": index !== database.tables.length - 1,
+          })}
+        >
+          <a
+            className="flex-full flex p1 text-bold text-brand no-decoration bg-medium-hover"
+            onClick={() => show("table", table)}
+          >
+            {table.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
+
+DatabasePane.propTypes = {
+  show: PropTypes.func.isRequired,
+};
 
 export default DatabasePane;
--- frontend/src/metabase/query_builder/components/dataref/MainPane.jsx
+++ frontend/src/metabase/query_builder/components/dataref/MainPane.jsx
@@ -2,50 +2,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { t } from "c-3po";
-import { isQueryable } from "metabase/lib/table";
 import Icon from "metabase/components/Icon.jsx";
-
-import cx from "classnames";
 
 const MainPane = ({ databases, show }) => (
   <div>
-    <div className="ml1 mt2">
+    <div className="ml1 mt2 mb3">
       <h2>{t`Data Reference`}</h2>
-      <p>{t`Information about your tables and columns`}.</p>
+      <p className="text-spaced">
+        {t`Browse the contents of your databases, tables, and columns. Pick a database to get started`}.
+      </p>
     </div>
     <ul>
       {databases &&
         databases
           .filter(db => db.tables && db.tables.length > 0)
           .map(database => (
-            <li className="pt1" key={database.id}>
-              <div className="ml1 my2 flex align-center justify-between border-bottom pb1">
-                <div className="flex align-center">
-                  <Icon name="database" className="text-medium pr1" size={14} />
-                  <h3>{database.name}</h3>
-                </div>
-                <div className="flex align-center">
-                  <Icon name="table2" className="text-light pr1" size={12} />
-                  <span className="text-medium">{database.tables.length}</span>
-                </div>
-              </div>
-              <ul>
-                {database.tables.filter(isQueryable).map((table, index) => (
-                  <li
-                    key={table.id}
-                    className={cx("", {
-                      "": index !== database.tables.length - 1,
-                    })}
-                  >
-                    <a
-                      className="flex-full flex p1 text-bold text-brand no-decoration bg-medium-hover"
-                      onClick={() => show("table", table)}
-                    >
-                      {table.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+            <li className="mb2" key={database.id}>
+              <a
+                onClick={() => show("database", database)}
+                className="p1 flex align-center no-decoration bg-medium-hover"
+              >
+                <Icon name="database" className="pr1 text-medium" size={14} />
+                <h3>{database.name}</h3>
+              </a>
             </li>
           ))}
     </ul>
