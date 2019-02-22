3875b782a7d254f078bd40dfb406a03710497bf2
Simon Belak <simon@metabase.com>
2019-02-22 21:51:34 +0100

    Test interaction with segments

--- test/metabase/driver/sql/query_processor_test.clj
+++ test/metabase/driver/sql/query_processor_test.clj
@@ -1,8 +1,10 @@
 (ns metabase.driver.sql.query-processor-test
   (:require [expectations :refer [expect]]
             [metabase.driver.sql.query-processor :as sql.qp]
+            [metabase.models.segment :refer [Segment]]
             [metabase.query-processor :as qp]
-            [metabase.test.data :as data]))
+            [metabase.test.data :as data]
+            [toucan.util.test :as tt]))
 
 ;; make sure our logic for deciding which order to process keys in the query works as expected
 (expect
@@ -14,23 +16,33 @@
                                               :fields       4
                                               :breakout     2}))
 
+
 (defn- test-query
   [query]
   (:data (qp/process-query {:database (data/id)
-                            :type :query
-                            :query query})))
+                            :type     :query
+                            :query    query})))
 
 (expect
   0.94
   (-> (test-query {:source-table (data/id :venues)
-                   :aggregation [[:share [:< [:field-id (data/id :venues :price)] 4]]]})
+                   :aggregation  [[:share [:< [:field-id (data/id :venues :price)] 4]]]})
       :rows
       ffirst
       double))
 (expect
   nil
   (-> (test-query {:source-table (data/id :venues)
-                   :aggregation [[:share [:< [:field-id (data/id :venues :price)] 4]]]
-                   :filter [:> [:field-id (data/id :venues :price)] Long/MAX_VALUE]})
+                   :aggregation  [[:share [:< [:field-id (data/id :venues :price)] 4]]]
+                   :filter       [:> [:field-id (data/id :venues :price)] Long/MAX_VALUE]})
       :rows
       ffirst))
+(tt/expect-with-temp [Segment [{segment-id :id} {:table_id   (data/id :venues)
+                                                 :definition {:source-table (data/id :venues)
+                                                              :filter       [:< [:field-id (data/id :venues :price)] 4]}}]]
+   0.94
+   (-> (test-query {:source-table (data/id :venues)
+                    :aggregation  [[:share [:segment segment-id]]] })
+       :rows
+       ffirst
+       double))
