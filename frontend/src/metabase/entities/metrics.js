b4bea90cfec09d4b0a918f3aa5273baf96aaeaa2
Tom Robinson <tlrobinson@gmail.com>
2019-02-25 17:49:50 -0800

    Add setArchived for segment/metric now that it's been implemented in the backend

--- frontend/src/metabase/entities/metrics.js
+++ frontend/src/metabase/entities/metrics.js
@@ -3,10 +3,23 @@ import { createEntity } from "metabase/lib/entities";
 import { MetricSchema } from "metabase/schema";
 import colors from "metabase/lib/colors";
 
-export default createEntity({
+const Metrics = createEntity({
   name: "metrics",
   path: "/api/metric",
   schema: MetricSchema,
+
+  objectActions: {
+    setArchived: ({ id }, archived, opts) =>
+      Metrics.actions.update(
+        { id },
+        {
+          archived,
+          // NOTE: this is still required by the endpoint even though we don't really use it
+          revision_message: archived ? "(Archive)" : "(Unarchive)",
+        },
+        opts,
+      ),
+  },
 
   objectSelectors: {
     getName: segment => segment && segment.name,
@@ -15,3 +28,5 @@ export default createEntity({
     getIcon: question => "sum",
   },
 });
+
+export default Metrics;
--- frontend/src/metabase/entities/segments.js
+++ frontend/src/metabase/entities/segments.js
@@ -1,14 +1,28 @@
 /* @flow */
 
-import { createEntity } from "metabase/lib/entities";
+import { createEntity, undo } from "metabase/lib/entities";
 
 import { SegmentSchema } from "metabase/schema";
 import colors from "metabase/lib/colors";
 
-export default createEntity({
+const Segments = createEntity({
   name: "segments",
   path: "/api/segment",
   schema: SegmentSchema,
+
+
+    objectActions: {
+      setArchived: ({ id }, archived, opts) =>
+        Segments.actions.update(
+          { id },
+          {
+            archived,
+            // NOTE: this is still required by the endpoint even though we don't really use it
+            revision_message: archived ? "(Archive)" : "(Unarchive)"
+          },
+          opts
+        ),
+    },
 
   objectSelectors: {
     getName: segment => segment && segment.name,
@@ -17,3 +31,5 @@ export default createEntity({
     getIcon: question => "segment",
   },
 });
+
+export default Segments
