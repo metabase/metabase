cfc083988cbef56263540796915fb7b361508f49
Kyle Doherty <kyle.l.doherty@gmail.com>
2019-03-25 13:22:06 -0700

    larger sidebar

--- frontend/src/metabase/query_builder/components/view/ViewSidebar.jsx
+++ frontend/src/metabase/query_builder/components/view/ViewSidebar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import cx from "classnames";
 
-const ViewSideBar = ({ left, right, width = 300, children }) => (
+const ViewSideBar = ({ left, right, width = 420, children }) => (
   <div
-    className={cx("bg-light relative", { "border-right": left, "border-left": right })}
+    className={cx("bg-light relative", {
+      "border-right": left,
+      "border-left": right,
+    })}
     style={{ width }}
   >
     <div className="spread scroll-y">{children}</div>
