15747fad54405d60d5da2c5a5f97f29fa5146c5d
Cam Saul <cammsaul@gmail.com>
2019-04-08 17:23:41 -0700

    Fix too much logging on query cancelation :wrench:

--- src/metabase/driver/sql_jdbc/execute.clj
+++ src/metabase/driver/sql_jdbc/execute.clj
@@ -181,7 +181,7 @@
           ;; we can give up on the query running in the future
           @query-future)
         (catch InterruptedException e
-          (log/warn e (tru "Client closed connection, cancelling query"))
+          (log/warn (tru "Client closed connection, canceling query"))
           ;; This is what does the real work of canceling the query. We aren't checking the result of
           ;; `query-future` but this will cause an exception to be thrown, saying the query has been cancelled.
           (.cancel stmt)
