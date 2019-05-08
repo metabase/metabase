9280cc85258668b4b10a716d55b2000694da9094
Tom Robinson <tlrobinson@gmail.com>
2019-05-08 13:59:25 -0700

    stray prettier bits

--- frontend/src/metabase/dashboard/components/DashboardHistoryModal.jsx
+++ frontend/src/metabase/dashboard/components/DashboardHistoryModal.jsx
@@ -8,7 +8,10 @@ import { connect } from "react-redux";
 import { fetchDashboard } from "metabase/dashboard/dashboard";
 
 @withRouter
-@connect(null, { fetchDashboard })
+@connect(
+  null,
+  { fetchDashboard },
+)
 export default class DashboardHistoryModal extends React.Component {
   render() {
     const { fetchDashboard, onClose, location, params } = this.props;
