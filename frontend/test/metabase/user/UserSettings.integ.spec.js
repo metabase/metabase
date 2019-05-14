fd6e33fcf09bdf66db30529b988679679845b45a
Kyle Doherty <kyle.l.doherty@gmail.com>
2019-05-14 10:44:06 -0700

    init UserSettings integ test

---
+++ frontend/test/metabase/user/UserSettings.integ.spec.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {
+  mountWithStore,
+  fillAndSubmitForm,
+  getFormValues,
+} from "__support__/integration_tests";
+
+import UserSettings from "metabase/user/components/UserSettings";
+
+const MOCK_USER = {
+  first_name: "Testy",
+  last_name: "McTestFace",
+  email: "test@metabase.com",
+};
+
+describe("UserSettings", () => {
+  it("should show user info", async () => {
+    const { wrapper, store } = mountWithStore(
+      <UserSettings user={MOCK_USER} />,
+    );
+
+    console.log(wrapper.debug());
+
+    console.log(store);
+    expect(await getFormValues(wrapper)).toEqual(MOCK_USER);
+  });
+});
