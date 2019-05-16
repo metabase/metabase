cb374b4fe71350be768f632601023e68f2e17579
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-05-16 19:03:04 -0400

    refactor revision history in a questionable way

--- frontend/src/metabase/admin/datamodel/containers/RevisionHistoryApp.jsx
+++ frontend/src/metabase/admin/datamodel/containers/RevisionHistoryApp.jsx
@@ -2,33 +2,87 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import RevisionHistory from "../components/revisions/RevisionHistory.jsx";
+import Metrics from "metabase/entities/metrics";
+import Segments from "metabase/entities/segments";
 
-import { revisionHistorySelectors } from "../selectors";
-import * as actions from "../datamodel";
+import { getRevisions, getCurrentUser } from "../selectors";
+import { fetchRevisions } from "../datamodel";
 
-const mapStateToProps = (state, props) => {
-  return {
-    ...revisionHistorySelectors(state, props),
-    entity: props.params.entity,
-    id: props.params.id,
-  };
-};
+const mapStateToProps = (state, props) => ({
+  entityName: props.params.entity,
+  id: props.params.id,
+  user: getCurrentUser(state),
+  revisions: getRevisions(state),
+});
 
-const mapDispatchToProps = {
-  ...actions,
-};
+const mapDispatchToProps = { fetchRevisions };
 
 @connect(
   mapStateToProps,
   mapDispatchToProps,
 )
 export default class RevisionHistoryApp extends Component {
-  componentWillMount() {
-    let { entity, id } = this.props;
-
-    this.props.fetchRevisions({ entity, id });
-  }
   render() {
-    return <RevisionHistory {...this.props} objectType={this.props.entity} />;
+    return this.props.entityName == "metric" ? (
+      <MetricRevisionHistory {...this.props} />
+    ) : (
+      <SegmentRevisionHistory {...this.props} />
+    );
+  }
+}
+
+@Metrics.load({ id: (state, { id }) => id })
+class MetricRevisionHistory extends Component {
+  componentWillMount() {
+    const { metric } = this.props;
+    if (metric) {
+      this.props.fetchRevisions({ entity: "metric", id: metric.id });
+    }
+  }
+
+  componentDidUpdate({ metric: { id: prevId } }) {
+    const { id } = this.props.metric;
+    if (id !== prevId) {
+      this.props.fetchRevisions({ entity: "metric", id });
+    }
+  }
+
+  render() {
+    const { metric, ...props } = this.props;
+    return (
+      <RevisionHistory
+        {...props}
+        object={metric}
+        objectType={this.props.entity}
+      />
+    );
+  }
+}
+
+@Segments.load({ id: (state, { id }) => id })
+class SegmentRevisionHistory extends Component {
+  componentWillMount() {
+    const { segment } = this.props;
+    if (segment) {
+      this.props.fetchRevisions({ entity: "segment", id: segment.id });
+    }
+  }
+
+  componentDidUpdate({ segment: { id: prevId } }) {
+    const { id } = this.props.segment;
+    if (id !== prevId) {
+      this.props.fetchRevisions({ entity: "segment", id });
+    }
+  }
+
+  render() {
+    const { segment, ...props } = this.props;
+    return (
+      <RevisionHistory
+        {...props}
+        object={segment}
+        objectType={this.props.entityName}
+      />
+    );
   }
 }
