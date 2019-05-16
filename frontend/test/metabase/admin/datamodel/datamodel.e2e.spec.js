50bb93a1a9b2eec15c0ed90ec7327fb689db6f8a
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-05-16 11:43:43 -0400

    update datamodel e2e tests

--- frontend/test/metabase/admin/datamodel/datamodel.e2e.spec.js
+++ frontend/test/metabase/admin/datamodel/datamodel.e2e.spec.js
@@ -7,14 +7,7 @@ import {
 } from "__support__/e2e_tests";
 import { click, clickButton, setInputValue } from "__support__/enzyme_utils";
 import { mount } from "enzyme";
-import {
-  FETCH_IDFIELDS,
-  INITIALIZE_METADATA,
-  SELECT_TABLE,
-  UPDATE_FIELD,
-  UPDATE_PREVIEW_SUMMARY,
-  UPDATE_TABLE,
-} from "metabase/admin/datamodel/datamodel";
+import { UPDATE_PREVIEW_SUMMARY } from "metabase/admin/datamodel/datamodel";
 import { FETCH_TABLE_METADATA } from "metabase/redux/metadata";
 
 import { Link } from "react-router";
@@ -28,7 +21,15 @@ import SegmentItem from "metabase/admin/datamodel/components/database/SegmentIte
 import MetricsList from "metabase/admin/datamodel/components/database/MetricsList";
 import MetricItem from "metabase/admin/datamodel/components/database/MetricItem";
 import { MetabaseApi } from "metabase/services";
-import { metrics as Metrics, segments as Segments } from "metabase/entities";
+import {
+  metrics as Metrics,
+  segments as Segments,
+  databases as Databases,
+  tables as Tables,
+  fields as Fields,
+} from "metabase/entities";
+import { FETCH_DATABASE_IDFIELDS } from "metabase/entities/databases";
+import { push } from "react-router-redux";
 
 describe("admin/datamodel", () => {
   beforeAll(async () => useSharedAdminLogin());
@@ -40,27 +41,27 @@ describe("admin/datamodel", () => {
       store.pushPath("/admin/datamodel/database");
       const app = mount(store.getAppContainer());
 
-      await store.waitForActions([INITIALIZE_METADATA, FETCH_IDFIELDS]);
+      await store.waitForActions([Databases.actionTypes.FETCH_LIST]);
 
       // Open "Orders" table section
       const adminListItems = app.find(".AdminList-item");
       click(adminListItems.at(0));
-      await store.waitForActions([SELECT_TABLE]);
+      await store.waitForActions([push("").type]);
 
       // Toggle its visibility to "Hidden"
       click(app.find("#VisibilityTypes > span").at(1));
-      await store.waitForActions([UPDATE_TABLE]);
+      await store.waitForActions([Tables.actionTypes.UPDATE]);
 
       // Toggle "Why hide" to "Irrelevant/Cruft"
       click(app.find("#VisibilitySubTypes > span").at(2));
-      await store.waitForActions([UPDATE_TABLE]);
+      await store.waitForActions([Tables.actionTypes.UPDATE]);
 
       // Unhide
       click(app.find("#VisibilityTypes > span").at(0));
 
       // Open "People" table section
       click(adminListItems.at(1));
-      await store.waitForActions([SELECT_TABLE]);
+      await store.waitForActions([push("").type]);
 
       // hide fields from people table
       // Set Address field to "Only in Detail Views"
@@ -73,7 +74,7 @@ describe("admin/datamodel", () => {
         .at(1);
       expect(onlyInDetailViewsRow.text()).toMatch(/Only in Detail Views/);
       click(onlyInDetailViewsRow);
-      await store.waitForActions([UPDATE_FIELD]);
+      await store.waitForActions([Fields.actionTypes.UPDATE]);
 
       // Set Birth Date field to "Do Not Include"
       click(columnsListItems.at(1).find(".TableEditor-field-visibility"));
@@ -85,7 +86,7 @@ describe("admin/datamodel", () => {
       expect(doNotIncludeRow.text()).toMatch(/Do Not Include/);
       click(doNotIncludeRow);
 
-      await store.waitForActions([UPDATE_FIELD]);
+      await store.waitForActions([Fields.actionTypes.UPDATE]);
 
       // modify special type for address field
       click(columnsListItems.first().find(".TableEditor-field-special-type"));
@@ -95,7 +96,7 @@ describe("admin/datamodel", () => {
         .at(1);
       expect(entityNameTypeRow.text()).toMatch(/Entity Name/);
       click(entityNameTypeRow);
-      await store.waitForActions([UPDATE_FIELD]);
+      await store.waitForActions([Fields.actionTypes.UPDATE]);
 
       // TODO Atte Keinänen 8/9/17: Currently this test doesn't validate that the updates actually are reflected in QB
     });
@@ -107,7 +108,10 @@ describe("admin/datamodel", () => {
       store.pushPath("/admin/datamodel/database/1/table/2");
       const app = mount(store.getAppContainer());
 
-      await store.waitForActions([INITIALIZE_METADATA, FETCH_IDFIELDS]);
+      await store.waitForActions([
+        Databases.actionTypes.FETCH_LIST,
+        FETCH_DATABASE_IDFIELDS,
+      ]);
 
       // Click the new segment button and check that we get properly redirected
       click(app.find(SegmentsList).find(Link));
@@ -144,8 +148,8 @@ describe("admin/datamodel", () => {
       click(app.find('button[children="Save changes"]'));
 
       await store.waitForActions([
-        Segments.actions.create,
-        INITIALIZE_METADATA,
+        Segments.actionTypes.CREATE,
+        Databases.actionTypes.FETCH_LIST,
       ]);
       expect(store.getPath()).toBe("/admin/datamodel/database/1/table/2");
 
@@ -166,7 +170,10 @@ describe("admin/datamodel", () => {
       store.pushPath("/admin/datamodel/database/1/table/2");
       const app = mount(store.getAppContainer());
 
-      await store.waitForActions([INITIALIZE_METADATA, FETCH_IDFIELDS]);
+      await store.waitForActions([
+        Databases.actionTypes.FETCH_LIST,
+        FETCH_DATABASE_IDFIELDS,
+      ]);
 
       // Click the new metric button and check that we get properly redirected
       click(app.find(MetricsList).find(Link));
@@ -190,7 +197,10 @@ describe("admin/datamodel", () => {
       // Save the metric
       click(app.find('button[children="Save changes"]'));
 
-      await store.waitForActions([Metrics.actions.create, INITIALIZE_METADATA]);
+      await store.waitForActions([
+        Metrics.actionTypes.CREATE,
+        Databases.actionTypes.FETCH_LIST,
+      ]);
       expect(store.getPath()).toBe("/admin/datamodel/database/1/table/2");
 
       // Validate that the segment got actually added
