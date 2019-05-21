9a87deaec5838dbc16924eb67ae835f4519ff021
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-05-21 12:07:49 -0400

    populate current user in store for test

--- frontend/test/metabase/admin/people/containers/GroupDetailApp.integ.spec.js
+++ frontend/test/metabase/admin/people/containers/GroupDetailApp.integ.spec.js
@@ -1,6 +1,7 @@
 import React from "react";
 import mock from "xhr-mock";
 import { mountWithStore } from "__support__/integration_tests";
+import { refreshCurrentUser } from "metabase/redux/user";
 
 import GroupDetailApp from "metabase/admin/people/containers/GroupDetailApp";
 
@@ -18,18 +19,20 @@ describe("GroupDetailApp", () => {
   it("should load the user", async () => {
     expect.assertions(1);
 
+    mock.get("/api/user/current", (req, res) => res.json(MOCK_USER));
     mock.get("/api/user", (req, res) => res.json([MOCK_USER]));
     mock.get("/api/permissions/group/42", (req, res) => {
       return res.json({
         id: 42,
         name: "Administrators",
-        members: [MOCK_USER],
+        members: [{ ...MOCK_USER, user_id: MOCK_USER.id }],
       });
     });
 
-    const { wrapper } = mountWithStore(
+    const { wrapper, store } = mountWithStore(
       <GroupDetailApp params={{ groupId: 42 }} />,
     );
+    store.dispatch(refreshCurrentUser());
 
     expect((await wrapper.async.find("tr td")).map(td => td.text())).toEqual([
       "Testy McTestFace",
