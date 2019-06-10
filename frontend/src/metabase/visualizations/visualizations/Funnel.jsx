3165deeb121bde01521294bd808178a08b6f29c4
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-06-10 12:36:56 -0400

    only show funnel legend header if there are action buttons but no title

--- frontend/src/metabase/visualizations/visualizations/Funnel.jsx
+++ frontend/src/metabase/visualizations/visualizations/Funnel.jsx
@@ -40,7 +40,7 @@ export default class Funnel extends Component {
     height: 4,
   };
 
-  static isSensible(cols, rows) {
+  static isSensible({ cols, rows }) {
     return cols.length === 2;
   }
 
@@ -166,12 +166,15 @@ export default class Funnel extends Component {
               actionButtons={actionButtons}
             />
           )}
-          <LegendHeader
-            className="flex-no-shrink"
-            series={series._raw || series}
-            actionButtons={!hasTitle && actionButtons}
-            onChangeCardAndRun={onChangeCardAndRun}
-          />
+          {!hasTitle &&
+          actionButtons && ( // always show action buttons if we have them
+              <LegendHeader
+                className="flex-no-shrink"
+                series={series._raw || series}
+                actionButtons={actionButtons}
+                onChangeCardAndRun={onChangeCardAndRun}
+              />
+            )}
           <FunnelNormal {...this.props} className="flex-full" />
         </div>
       );
