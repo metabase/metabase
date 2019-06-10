b045c2537bc1cf7808f3642e903f7891a4183b10
Cam Saul <cammsaul@gmail.com>
2019-06-10 14:16:59 -0700

    Add :include-all-drivers to :ring profile; fix middleware when deps not present

--- lein-plugins/include-drivers/project.clj
+++ lein-plugins/include-drivers/project.clj
@@ -1,4 +1,4 @@
-(defproject metabase/lein-include-drivers "1.0.5"
+(defproject metabase/lein-include-drivers "1.0.6"
   :min-lein-version "2.5.0"
   :eval-in-leiningen true
   :deploy-repositories [["clojars" {:sign-releases false}]])
