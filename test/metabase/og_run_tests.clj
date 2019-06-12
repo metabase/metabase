d7bfbb04c65c440a440463cfdc3a482cc59b5c6f
Octavian Geagla <ogeagla@gmail.com>
2019-06-12 08:38:11 -0600

    [test] app and test runners wip

---
+++ test/metabase/og_run_tests.clj
@@ -0,0 +1,46 @@
+(ns metabase.og-run-tests
+  (:require
+    [metabase.query-processor-test.breakout-test]
+    [clojure.test :refer :all]
+    [clojure.tools.namespace.find :as ns-find]
+    [clojure.java.classpath :as classpath]
+    [clojure.string :as str]
+    [metabase.test-setup :as metabase.test-setup]
+    [expectations :as expectations]))
+
+
+(defn run-driver-tests []
+  (System/setProperty "DRIVERS" "h2,mongo")
+
+  (doseq [ns-symb (ns-find/find-namespaces (classpath/system-classpath))
+          :when (and (str/starts-with? ns-symb "metabase.")
+                     (find-ns ns-symb))
+          [_ varr] (ns-interns ns-symb)
+          :let [{:keys [expectations-options]} (meta varr)]
+          :when (= expectations-options :before-run)]
+    (varr))
+  (metabase.test-setup/call-with-test-scaffolding
+    (fn [] (expectations/run-tests '[metabase.query-processor-test.breakout-test]))))
+
+
+
+(defn load-server []
+  (require 'metabase.core
+           'metabase.server
+           'metabase.db
+           'metabase.handler
+           'metabase.plugins)
+  (metabase.server/start-web-server! #'metabase.handler/app)
+  (metabase.db/setup-db!)
+  (metabase.plugins/load-plugins!)
+  (metabase.core.initialization-status/set-complete!))
+
+(comment
+
+  ;; Run tests with driver
+  (run-driver-tests)
+
+
+  ;; Load app server
+  (load-server)
+  )
