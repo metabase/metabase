metabase/metabase

commit d0b7a3d6b5812f67fb5325248619089c8d9f32d7

Fix issue #10186 : Properly map hive data types in spark driver (#10207)

modules/drivers/sparksql/src/metabase/driver/hive_like.clj
@@ -22,25 +22,26 @@
 (driver/register! :hive-like, :parent :sql-jdbc, :abstract? true)
 
 (defmethod sql-jdbc.sync/database-type->base-type :hive-like [_ database-type]
-  ({ ;; Numeric types
-    :tinyint                     :type/Integer
-    :smallint                    :type/Integer
-    :int                         :type/Integer
-    :integer                     :type/Integer
-    :bigint                      :type/BigInteger
-    :float                       :type/Float
-    :double                      :type/Float
-    (keyword "double precision") :type/Float
-    :decimal                     :type/Decimal
-    ;; Date/Time types
-    :timestamp                   :type/DateTime
-    :date                        :type/Date
-    :interval                    :type/*
-    :string                      :type/Text
-    :varchar                     :type/Text
-    :char                        :type/Text
-    :boolean                     :type/Boolean
-    :binary                      :type/*} database-type))
+  (condp re-matches (name database-type)
+    #"boolean"               :type/Boolean
+    #"tinyint"               :type/Integer
+    #"smallint"              :type/Integer
+    #"int"                   :type/Integer
+    #"bigint"                :type/BigInteger
+    #"float"                 :type/Float
+    #"double"                :type/Float
+    #"double precision"      :type/Double
+    #"decimal.*"             :type/Decimal
+    #"char.*"                :type/Text
+    #"varchar.*"             :type/Text
+    #"binary*"               :type/*
+    #"date"                  :type/Date
+    #"time"                  :type/Time
+    #"timestamp"             :type/DateTime
+    #"interval"              :type/*
+    #"array.*"               :type/Array
+    #"map"                   :type/Dictionary
+    #".*"                    :type/*))
 
 (defmethod sql.qp/current-datetime-fn :hive-like [_] :%now)
 

commit dc0ab391c440ffdcfa16879f1c9451710bfe3925

Fix regression against syncing SparkSQL string type caused by #10207 [ci sparksql]

modules/drivers/sparksql/src/metabase/driver/hive_like.clj
@@ -23,25 +23,26 @@
 
 (defmethod sql-jdbc.sync/database-type->base-type :hive-like [_ database-type]
   (condp re-matches (name database-type)
-    #"boolean"               :type/Boolean
-    #"tinyint"               :type/Integer
-    #"smallint"              :type/Integer
-    #"int"                   :type/Integer
-    #"bigint"                :type/BigInteger
-    #"float"                 :type/Float
-    #"double"                :type/Float
-    #"double precision"      :type/Double
-    #"decimal.*"             :type/Decimal
-    #"char.*"                :type/Text
-    #"varchar.*"             :type/Text
-    #"binary*"               :type/*
-    #"date"                  :type/Date
-    #"time"                  :type/Time
-    #"timestamp"             :type/DateTime
-    #"interval"              :type/*
-    #"array.*"               :type/Array
-    #"map"                   :type/Dictionary
-    #".*"                    :type/*))
+    #"boolean"          :type/Boolean
+    #"tinyint"          :type/Integer
+    #"smallint"         :type/Integer
+    #"int"              :type/Integer
+    #"bigint"           :type/BigInteger
+    #"float"            :type/Float
+    #"double"           :type/Float
+    #"double precision" :type/Double
+    #"decimal.*"        :type/Decimal
+    #"char.*"           :type/Text
+    #"varchar.*"        :type/Text
+    #"string.*"         :type/Text
+    #"binary*"          :type/*
+    #"date"             :type/Date
+    #"time"             :type/Time
+    #"timestamp"        :type/DateTime
+    #"interval"         :type/*
+    #"array.*"          :type/Array
+    #"map"              :type/Dictionary
+    #".*"               :type/*))
 
 (defmethod sql.qp/current-datetime-fn :hive-like [_] :%now)
 

modules/drivers/sparksql/test/metabase/driver/hive_like_test.clj
@@ -1,0 +1,10 @@
+(ns metabase.driver.hive-like-test
+  (:require [expectations :refer [expect]]
+            [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]))
+
+;; make sure the various types we use for running tests are actually mapped to the correct DB type
+(expect :type/Text     (sql-jdbc.sync/database-type->base-type :hive-like :string))
+(expect :type/Integer  (sql-jdbc.sync/database-type->base-type :hive-like :int))
+(expect :type/Date     (sql-jdbc.sync/database-type->base-type :hive-like :date))
+(expect :type/DateTime (sql-jdbc.sync/database-type->base-type :hive-like :timestamp))
+(expect :type/Float    (sql-jdbc.sync/database-type->base-type :hive-like :double))

modules/drivers/sparksql/test/metabase/driver/sparksql_test.clj
@@ -1,5 +1,5 @@
 (ns metabase.driver.sparksql-test
-  (:require [expectations :refer :all]
+  (:require [expectations :refer [expect]]
             [metabase.driver.sql.query-processor :as sql.qp]))
 
 ;; Make sure our custom implementation of `apply-page` works the way we'd expect