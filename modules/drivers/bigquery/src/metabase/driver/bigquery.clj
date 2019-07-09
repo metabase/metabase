996e975c68c8919525cdc454a6cf38f957a6faac
Simon Belak <simon@metabase.com>
2019-07-09 15:10:38 +0100

    bq is apparently picky about parens [ci all]

--- modules/drivers/bigquery/src/metabase/driver/bigquery.clj
+++ modules/drivers/bigquery/src/metabase/driver/bigquery.clj
@@ -408,7 +408,7 @@
 
 (defmethod driver/date-add :bigquery
   [driver dt amount unit]
-  (hsql/call :date_add (hx/->timestamp dt) (hsql/raw (format "(INTERVAL %d %s)" (int amount) (name unit)))))
+  (hsql/call :date_add (hx/->timestamp dt) (hsql/raw (format "INTERVAL %d %s" (int amount) (name unit)))))
 
 (defmethod driver/mbql->native :bigquery
   [driver
