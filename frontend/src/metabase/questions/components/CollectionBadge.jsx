650f982ac5d8194811ad78d46de983c9fac4045c
Tom Robinson <tlrobinson@gmail.com>
2019-07-18 12:45:52 -0700

    Fix CollectionBadge style

--- frontend/src/metabase/questions/components/CollectionBadge.jsx
+++ frontend/src/metabase/questions/components/CollectionBadge.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Flex } from "grid-styled";
-import { Link } from "react-router";
 
+import Link from "metabase/components/Link";
 import Icon from "metabase/components/Icon";
 
-import * as Urls from "metabase/lib/urls";
-import colors, { lighten } from "metabase/lib/colors";
+import { lighten } from "metabase/lib/colors";
 
 import Collection from "metabase/entities/collections";
 
@@ -21,17 +20,17 @@ class CollectionBadge extends React.Component {
   render() {
     const {
       analyticsContext,
-      object,
+      collection,
       className,
       hasBackground,
       style = {},
     } = this.props;
-    if (!object) {
+    if (!collection) {
       return null;
     }
     const backgroundStyle = hasBackground
       ? {
-          backgroundColor: lighten(colors["brand"], 0.55),
+          backgroundColor: lighten("brand"),
           paddingTop: "0.3em",
           paddingBottom: "0.3em",
           borderRadius: "0.4em",
@@ -40,10 +39,12 @@ class CollectionBadge extends React.Component {
 
     return (
       <Link
-        to={Urls.collection(object.id)}
-        className={cx(className, "block cursor-pointer", {
-          px0: backgroundStyle,
-        })}
+        to={collection.getUrl()}
+        className={cx(
+          className,
+          "block cursor-pointer h4 text-bold text-medium text-brand-hover",
+          { px1: hasBackground },
+        )}
         style={{
           ...backgroundStyle,
           ...style,
@@ -51,10 +52,8 @@ class CollectionBadge extends React.Component {
         data-metabase-event={`${analyticsContext};Collection Badge Click`}
       >
         <Flex align="center">
-          <Icon name={object.getIcon()} mr={1} />
-          <h5 className="text-uppercase text-wrap" style={{ fontWeight: 900 }}>
-            {object.name}
-          </h5>
+          <Icon name={collection.getIcon()} mr={1} size={13} />
+          <span className="text-wrap">{collection.getName()}</span>
         </Flex>
       </Link>
     );
