8431df0b2e7f27c87d3fe6ef80fc040121a57d54
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2019-08-10 01:24:19 +0200

    Add versions of `trs` and `tru` that wrap results in `str`
    The pre-existing `trs` and `tru` were renamed to `lazy-trs` and `lazy-tru`
    respectively, and new `trs` and `tru` funcs were added, wrapping the result
    of its lazy counterpart in `str`.
    
    This way the normal use case of `trs` and `tru` is improved by no longer
    requiring that it be wrapped in `str`.
    
    Cases where the translated result is passed to `str` anyway, the lazy
    version can be used.

--- src/metabase/util/i18n.clj
+++ src/metabase/util/i18n.clj
@@ -62,7 +62,7 @@
   "Schema for user and system localized string instances"
   (s/cond-pre UserLocalizedString SystemLocalizedString))
 
-(defmacro tru
+(defmacro lazy-tru
   "Similar to `puppetlabs.i18n.core/tru` but creates a `UserLocalizedString` instance so that conversion to the
   correct locale can be delayed until it is needed. The user locale comes from the browser, so conversion to the
   localized string needs to be 'late bound' and only occur when the user's locale is in scope. Calling `str` on the
@@ -70,13 +70,23 @@
   [msg & args]
   `(UserLocalizedString. ~(namespace-munge *ns*) ~msg ~(vec args)))
 
-(defmacro trs
+(defmacro lazy-trs
   "Similar to `puppetlabs.i18n.core/trs` but creates a `SystemLocalizedString` instance so that conversion to the
   correct locale can be delayed until it is needed. This is needed as the system locale from the JVM can be
   overridden/changed by a setting. Calling `str` on the results of this invocation will lookup the translated version
   of the string."
   [msg & args]
   `(SystemLocalizedString. ~(namespace-munge *ns*) ~msg ~(vec args)))
+
+(defmacro tru
+  "Applies `str` to `lazy-tru`'s expansion."
+  [msg & args]
+  `(str (lazy-tru ~msg ~@(vec args))))
+
+(defmacro trs
+  "Applies `str` to `lazy-trs`'s expansion."
+  [msg & args]
+  `(str (lazy-trs ~msg ~@(vec args))))
 
 (def ^:private localized-string-checker
   "Compiled checker for `LocalizedString`s which is more efficient when used repeatedly like in `localized-string?`
