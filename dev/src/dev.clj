c2071aa4a594352dd547a8fbafee8038af0d551b
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-08-20 10:50:14 -0400

    make repl development a little quicker

---
+++ dev/src/dev.clj
@@ -0,0 +1,28 @@
+(ns dev
+  (:require [metabase.core] 
+            [metabase.server] 
+            [metabase.db] 
+            [metabase.handler] 
+            [metabase.plugins]))
+
+(defn start!
+  []
+  (metabase.server/start-web-server! #'metabase.handler/app)
+  (metabase.db/setup-db!)
+  (metabase.plugins/load-plugins!)
+  (metabase.core.initialization-status/set-complete!))
+
+(defn stop!
+  []
+  (metabase.server/stop-web-server!))
+
+(defn restart!
+  []
+  (stop!)
+  (start!))
+
+(defn run-tests
+  [& ns-names]
+  (doseq [ns-name ns-names]
+    (require ns-name :reload))
+  (expectations/run-tests ns-names))
