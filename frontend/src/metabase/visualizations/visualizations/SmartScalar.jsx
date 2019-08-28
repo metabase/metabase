a51b8e5da83d1f1a78a28b151c6f74b79813416a
Simon Belak <simon@metabase.com>
2019-08-28 12:17:11 +0100

    Insights: include unit in response

--- frontend/src/metabase/visualizations/visualizations/SmartScalar.jsx
+++ frontend/src/metabase/visualizations/visualizations/SmartScalar.jsx
@@ -94,17 +94,14 @@ export default class Smart extends React.Component {
     const column = cols[metricIndex];
     const dimensionColumn = cols[dimensionIndex];
 
-    const granularity =
-      dimensionColumn && dimensionColumn.unit
-        ? formatBucketing(dimensionColumn.unit).toLowerCase()
-        : null;
-
     const insights =
       rawSeries && rawSeries[0].data && rawSeries[0].data.insights;
     const insight = _.findWhere(insights, { col: column.name });
     if (!insight) {
       return null;
     }
+
+    const granularity = insight["unit"];
 
     const lastChange = insight["last-change"];
     const previousValue = insight["previous-value"];
