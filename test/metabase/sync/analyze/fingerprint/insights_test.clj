9b0722d982f357509f008317bf67937360b7a41a
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-08-28 16:04:44 -0400

    years are timestamps now

--- test/metabase/sync/analyze/fingerprint/insights_test.clj
+++ test/metabase/sync/analyze/fingerprint/insights_test.clj
@@ -38,7 +38,8 @@
   (transduce identity
              (insights [{:base_type :type/DateTime :unit :year}
                         {:base_type :type/Integer}])
-             [["2014" 100] ["2015" 200]]))
+             [["2014-01-01T00:00:00Z" 100]
+              ["2015-01-01T00:00:00Z" 200]]))
 
 (defn- inst->day
   [inst]
