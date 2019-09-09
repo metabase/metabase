dde6d5cd24eb49af73c6cd84a6dd416e5a712908
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-09-09 16:34:40 -0400

    able to copy details

--- frontend/src/metabase/admin/tasks/containers/BugReportApp.jsx
+++ frontend/src/metabase/admin/tasks/containers/BugReportApp.jsx
@@ -4,6 +4,16 @@ import AdminHeader from "metabase/components/AdminHeader";
 import { t } from "ttag";
 import { UtilApi } from "metabase/services";
 
+function navigatorInfo() {
+  return {
+    appVersion: navigator.appVersion,
+    language: navigator.language,
+    platform: navigator.platform,
+    userAgent: navigator.userAgent,
+    vendor: navigator.vendor
+  };
+}
+
 export default class BugReportApp extends Component {
   constructor() {
     super();
@@ -11,6 +21,12 @@ export default class BugReportApp extends Component {
       details: {},
     };
   }
+
+  copyToClipboard = (e) => {
+    this.textArea.select();
+    document.execCommand('copy');
+    this.setState({ copySuccess: 'Copied!' });
+  };
   
   async fetchDetails() {
     const details = await UtilApi.bug_report_details();
@@ -23,11 +39,33 @@ export default class BugReportApp extends Component {
   
   render() {
     const { details } = this.state;
-    console.log("STATE", this.state);
     return (
       <Box p={3}>
-        { String(details) }
-        <AdminHeader title={t`Report a Bug`} />
+        <AdminHeader title={t`Bug Report Details`} />
+        <h3>System Info</h3>
+        <pre>{ JSON.stringify(details, null, 2) }</pre>
+
+        <h3>Browser Info</h3>
+        <pre>{ JSON.stringify(navigatorInfo(), null, 2) }</pre>
+
+        <div>{
+            /* Logical shortcut for only displaying the 
+               button if the copy command exists */
+            document.queryCommandSupported('copy') &&
+              <div>
+                <button onClick={ this.copyToClipboard }>Copy</button> 
+                { this.state.copySuccess }
+              </div>
+           }
+           
+           <form>
+             <textarea
+               ref={(textarea) => this.textArea = textarea}
+               defaultValue={ JSON.stringify(details, null, 2) +
+                              "\n\n" +
+                              JSON.stringify(navigatorInfo(), null, 2) }/>
+           </form>
+        </div>
       </Box>
     );
   }
