823154599b9361fd515e5fe75e4381a5872d5932
Kyle Doherty <kyle.l.doherty@gmail.com>
2019-09-09 16:24:03 -0700

    basic info styling and CopyButton use

--- frontend/src/metabase/admin/tasks/containers/BugReportApp.jsx
+++ frontend/src/metabase/admin/tasks/containers/BugReportApp.jsx
@@ -4,15 +4,30 @@ import AdminHeader from "metabase/components/AdminHeader";
 import { t } from "ttag";
 import { UtilApi } from "metabase/services";
 
+import CopyButton from "metabase/components/CopyButton";
+
 function navigatorInfo() {
   return {
     appVersion: navigator.appVersion,
     language: navigator.language,
     platform: navigator.platform,
     userAgent: navigator.userAgent,
-    vendor: navigator.vendor
+    vendor: navigator.vendor,
   };
 }
+
+const InfoBlock = ({ children }) => (
+  <Box p={2} className="bordered rounded bg-light relative">
+    <Box m={2} className="absolute top right text-brand-hover cursor-pointer">
+      <CopyButton value={children} />
+    </Box>
+    <pre
+      style={{ fontFamily: "Lucida Console, Monaco, monospace", fontSize: 14 }}
+    >
+      {children}
+    </pre>
+  </Box>
+);
 
 export default class BugReportApp extends Component {
   constructor() {
@@ -22,12 +37,12 @@ export default class BugReportApp extends Component {
     };
   }
 
-  copyToClipboard = (e) => {
+  copyToClipboard = e => {
     this.textArea.select();
-    document.execCommand('copy');
-    this.setState({ copySuccess: 'Copied!' });
+    document.execCommand("copy");
+    this.setState({ copySuccess: "Copied!" });
   };
-  
+
   async fetchDetails() {
     const details = await UtilApi.bug_report_details();
     this.setState({ details: details });
@@ -36,36 +51,19 @@ export default class BugReportApp extends Component {
   componentWillMount() {
     this.fetchDetails();
   }
-  
+
   render() {
     const { details } = this.state;
     return (
       <Box p={3}>
         <AdminHeader title={t`Bug Report Details`} />
-        <h3>System Info</h3>
-        <pre>{ JSON.stringify(details, null, 2) }</pre>
+        <Box my={2}>
+          <h3 className="mb1">System Info</h3>
+          <InfoBlock>{JSON.stringify(details, null, 2)}</InfoBlock>
+        </Box>
 
-        <h3>Browser Info</h3>
-        <pre>{ JSON.stringify(navigatorInfo(), null, 2) }</pre>
-
-        <div>{
-            /* Logical shortcut for only displaying the 
-               button if the copy command exists */
-            document.queryCommandSupported('copy') &&
-              <div>
-                <button onClick={ this.copyToClipboard }>Copy</button> 
-                { this.state.copySuccess }
-              </div>
-           }
-           
-           <form>
-             <textarea
-               ref={(textarea) => this.textArea = textarea}
-               defaultValue={ JSON.stringify(details, null, 2) +
-                              "\n\n" +
-                              JSON.stringify(navigatorInfo(), null, 2) }/>
-           </form>
-        </div>
+        <h3 className="mb1">Browser Info</h3>
+        <InfoBlock>{JSON.stringify(navigatorInfo(), null, 2)}</InfoBlock>
       </Box>
     );
   }
