6dbfc7a1c13060fb851f683ed7d40fb9e2100273
Cam Saul <cammsaul@gmail.com>
2019-09-12 20:36:49 -0700

    Test fixes [ci drivers]

--- test/metabase/test/data/env/impl.clj
+++ test/metabase/test/data/env/impl.clj
@@ -1,7 +1,6 @@
 (ns metabase.test.data.env.impl
   (:require [clojure.string :as str]
             [environ.core :refer [env]]
-            [metabase.test.initialize :as initialize]
             [metabase.util :as u]))
 
 (defn- get-drivers-from-env []
@@ -17,8 +16,4 @@
 (defn get-test-drivers
   "Return a set of drivers to test against from the env var `DRIVERS`."
   []
-  (if-let [drivers (get-drivers-from-env)]
-    (do
-      (initialize/initialize-if-needed! :plugins)
-      drivers)
-    #{:h2}))
+  (or (get-drivers-from-env) #{:h2}))
