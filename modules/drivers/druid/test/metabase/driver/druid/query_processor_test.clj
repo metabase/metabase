60d2905454c638363b70c1bad977271293d7b6dd
Simon Belak <simon@metabase.com>
2019-09-22 13:04:09 +0100

    Druid: set correct type to complex aggregators in post aggregation (#10836)
    Druid: set correct type to complex aggregators in post aggregation

--- modules/drivers/druid/test/metabase/driver/druid/query_processor_test.clj
+++ modules/drivers/druid/test/metabase/driver/druid/query_processor_test.clj
@@ -117,3 +117,27 @@
    {:aggregation [[:aggregation-options [:distinct $checkins.venue_name] {:name "__count_0"}]]
     :breakout    [$venue_category_name]
     :order-by    [[:desc [:aggregation 0]] [:asc $checkins.venue_category_name]]}))
+
+;; `distinct` when used in post aggregations should have type `:finalizingFieldAccess`
+(datasets/expect-with-driver :druid
+  {:projections [:__distinct_0 :expression]
+   :query       {:queryType        :timeseries
+                 :granularity      :all
+                 :dataSource       "checkins"
+                 :context          {:timeout 60000, :queryId "<Query ID>"}
+                 :intervals        ["1900-01-01/2100-01-01"]
+                 :aggregations     [{:type       :cardinality
+                                     :name       "__distinct_0"
+                                     :fieldNames ["venue_name"]
+                                     :byRow      true
+                                     :round      true}]
+                 :postAggregations [{:type :arithmetic,
+                                     :name "expression",
+                                     :fn :+,
+                                     :fields
+                                     [{:type :constant, :name "1", :value 1}
+                                      {:type :finalizingFieldAccess, :fieldName "__distinct_0"}]}]}
+   :query-type  ::druid.qp/total
+   :mbql?       true}
+  (query->native
+   {:aggregation [[:+ 1 [:aggregation-options [:distinct $checkins.venue_name] {:name "__distinct_0"}]]]}))
