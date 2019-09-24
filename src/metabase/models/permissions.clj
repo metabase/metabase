feedb6df1dd7520fd7498eb8ca24d87fa9672ef2
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-09-24 17:05:07 -0400

    revert baroque changes

--- src/metabase/models/permissions.clj
+++ src/metabase/models/permissions.clj
@@ -58,8 +58,7 @@
    #"^/collection/(\d+)/$"                                ; readwrite permissions for a collection
    #"^/collection/(\d+)/read/$"                           ; read permissions for a collection
    #"^/collection/root/$"                                 ; readwrite permissions for the 'Root' Collection (things with `nil` collection_id)
-   #"^/collection/root/read/$"                            ; read permissions for the 'Root' Collection
-   ])
+   #"^/collection/root/read/$"])                          ; read permissions for the 'Root' Collection
 
 (defn valid-object-path?
   "Does `object-path` follow a known, allowed format to an *object*? (The root path, \"/\", is not considered an object;
--- src/metabase/models/user.clj
+++ src/metabase/models/user.clj
@@ -277,11 +277,13 @@
 (defn permissions-set
   "Return a set of all permissions object paths that USER-OR-ID has been granted access to. (2 DB Calls)"
   [user-or-id]
-  (when-let [user-id (u/get-id user-or-id)]
-    (-> #{(str "/member/" user-or-id "/")}
-        (into (map perms/collection-readwrite-path (collection/user->personal-collection-and-descendant-ids user-or-id)))
-        (into (map :object (db/query {:select [:p.object]
-                                      :from   [[:permissions_group_membership :pgm]]
-                                      :join   [[:permissions_group :pg] [:= :pgm.group_id :pg.id]
-                                               [:permissions :p]        [:= :p.group_id :pg.id]]
-                                      :where  [:= :pgm.user_id user-id]}))))))
+  (set (when-let [user-id (u/get-id user-or-id)]
+         (concat
+          ;; Current User always gets readwrite perms for their Personal Collection and for its descendants! (1 DB Call)
+          (map perms/collection-readwrite-path (collection/user->personal-collection-and-descendant-ids user-or-id))
+          ;; include the other Perms entries for any Group this User is in (1 DB Call)
+          (map :object (db/query {:select [:p.object]
+                                  :from   [[:permissions_group_membership :pgm]]
+                                  :join   [[:permissions_group :pg] [:= :pgm.group_id :pg.id]
+                                           [:permissions :p]        [:= :p.group_id :pg.id]]
+                                  :where  [:= :pgm.user_id user-id]}))))))
