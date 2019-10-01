40f88ef246f54fd3f79ff3eceef80e7b5864b005
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-10-01 12:48:23 -0400

    group reference sidebar tables by schema

--- frontend/src/metabase/query_builder/components/dataref/DatabasePane.jsx
+++ frontend/src/metabase/query_builder/components/dataref/DatabasePane.jsx
@@ -4,33 +4,49 @@ import PropTypes from "prop-types";
 import { isQueryable } from "metabase/lib/table";
 import Icon from "metabase/components/Icon";
 
-const DatabasePane = ({ database, show, ...props }) => (
-  <div>
-    <div className="ml1 my2 flex align-center justify-between border-bottom pb1">
-      <div className="flex align-center">
-        <Icon name="database" className="text-medium pr1" size={14} />
-        <h3 className="text-wrap">{database.name}</h3>
-      </div>
-      <div className="flex align-center">
-        <Icon name="table2" className="text-light pr1" size={12} />
-        <span className="text-medium">{database.tables.length}</span>
-      </div>
-    </div>
+function groupBySchema(tables) {
+  const bySchema = {};
+  for (const table of tables) {
+    bySchema[table.schema] = [...(bySchema[table.schema] || []), table];
+  }
+  return bySchema;
+}
 
-    <ul>
-      {database.tables.filter(isQueryable).map((table, index) => (
-        <li key={table.id}>
-          <a
-            className="flex-full flex p1 text-bold text-brand text-wrap no-decoration bg-medium-hover"
-            onClick={() => show("table", table)}
-          >
-            {table.name}
-          </a>
-        </li>
+const DatabasePane = ({ database, show, ...props }) => {
+  const tablesBySchema = groupBySchema(database.tables.filter(isQueryable));
+  return (
+    <div>
+      <div className="ml1 my2 flex align-center justify-between border-bottom pb1">
+        <div className="flex align-center">
+          <Icon name="database" className="text-medium pr1" size={14} />
+          <h3 className="text-wrap">{database.name}</h3>
+        </div>
+        <div className="flex align-center">
+          <Icon name="table2" className="text-light pr1" size={12} />
+          <span className="text-medium">{database.tables.length}</span>
+        </div>
+      </div>
+
+      {Object.entries(tablesBySchema).map(([schemaName, tables]) => (
+        <div>
+          {Object.keys(tablesBySchema).length > 1 && <span>{schemaName}</span>}
+          <ul>
+            {tables.map(table => (
+              <li key={table.id}>
+                <a
+                  className="flex-full flex p1 text-bold text-brand text-wrap no-decoration bg-medium-hover"
+                  onClick={() => show("table", table)}
+                >
+                  {table.display_name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       ))}
-    </ul>
-  </div>
-);
+    </div>
+  );
+};
 
 DatabasePane.propTypes = {
   show: PropTypes.func.isRequired,
