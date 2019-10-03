a32e6a4eb3cc74a6234a580c1e8a8db90d3f5583
Paul Rosenzweig <paulrosenzweig@users.noreply.github.com>
2019-10-03 13:36:40 -0400

    show am/pm if locale uses 12-hour clock (#11049)

--- frontend/src/metabase/lib/i18n.js
+++ frontend/src/metabase/lib/i18n.js
@@ -25,6 +25,13 @@ export function setLocalization(translationsObject) {
   moment.locale(locale);
 }
 
+// Format a fixed timestamp in local time to see if the current locale defaults
+// to using a 24 hour clock.
+export function isLocale24Hour() {
+  const formattedTime = moment("2000-01-01T13:00:00").format("LT");
+  return /^13:/.test(formattedTime);
+}
+
 // we delete msgid property since it's redundant, but have to add it back in to
 // make ttag happy
 function addMsgIds(translationsObject) {
