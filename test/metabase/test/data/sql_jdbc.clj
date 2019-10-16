c2781b1f3c8922343ce75c8f94fa54d40c9f5509
Cam Saul <github@camsaul.com>
2019-10-16 14:31:56 -0700

    Test fixes [ci drivers]

--- test/metabase/test/data/sql_jdbc.clj
+++ test/metabase/test/data/sql_jdbc.clj
@@ -4,6 +4,7 @@
             [metabase.test.data
              [interface :as tx]
              [sql :as sql.tx]]
+            [metabase.test.initialize :as initialize]
             [metabase.test.data.sql-jdbc.load-data :as load-data]))
 
 (driver/register! :sql-jdbc/test-extensions, :abstract? true)
@@ -11,6 +12,7 @@
 (sql.tx/add-test-extensions! :sql-jdbc/test-extensions)
 
 (defn add-test-extensions! [driver]
+  (initialize/initialize-if-needed! :plugins)
   (driver/add-parent! driver :sql-jdbc/test-extensions)
   (println "Added SQL JDBC test extensions for" driver "➕"))
 
