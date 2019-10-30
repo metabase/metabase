1e079d4cbd64997cbe94f8fb8478be6c68ec441d
Mohammed Amarnah <m.amarnah@gmail.com>
2019-10-30 03:57:19 +0200

    fix front-end test

--- frontend/test/metabase/reference/databases.e2e.spec.js
+++ frontend/test/metabase/reference/databases.e2e.spec.js
@@ -102,7 +102,8 @@ describe("The Reference Section", () => {
     // database update
     it("should update the sample database", async () => {
       // create a new db by cloning #1
-      const d1 = await MetabaseApi.db_get({ dbId: 1 });
+      var d1 = await MetabaseApi.db_get({ dbId: 1 });
+      d1.name = "cloned-db";
       const { id } = await MetabaseApi.db_create(d1);
 
       // go to that db's reference page
