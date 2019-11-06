95460c361e2d33f060eeb4057e2572aecf60f9b3
Maz Ameli <maz@metabase.com>
2019-11-06 13:56:09 -0800

    update locales.clj

--- resources/locales.clj
+++ resources/locales.clj
@@ -1,5 +1,5 @@
 {
-  :locales  #{"en" "uk" "fr" "nl" "tr" "it" "es" "nb" "ca" "de" "ja" "vi" "pl" "pt" "ru" }
+  :locales  #{"en" "zh" "uk" "fr" "nl" "tr" "sk" "it" "es" "nb" "ca" "de" "fa" "ja" "vi" "pl" "pt" "ru" }
   :packages ["metabase"]
   :bundle   "metabase.Messages"
 }
