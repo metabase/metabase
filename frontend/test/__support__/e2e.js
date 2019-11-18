97a48a00c5a9f6800dd9d229ec92042b858789b2
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-11-18 11:44:16 -0500

    allow waiting on concurrent requests in e2e

--- frontend/test/__support__/e2e.js
+++ frontend/test/__support__/e2e.js
@@ -52,7 +52,7 @@ let hasFinishedCreatingStore = false;
 let loginSession = null; // Stores the current login session
 let previousLoginSession = null;
 let simulateOfflineMode = false;
-let apiRequestCompletedCallback = null;
+const apiRequestCompletedCallbacks = [];
 let skippedApiRequests = [];
 
 // load files that are loaded at the top if app.js
@@ -460,16 +460,22 @@ export const waitForRequestToComplete = (
             ) || "No requests"}`,
         ),
       );
+      removeCallback();
     }, timeout);
-
-    apiRequestCompletedCallback = (requestMethod, requestUrl) => {
+    const cb = (requestMethod, requestUrl) => {
       if (requestMethod === method && urlRegex.test(requestUrl)) {
         clearTimeout(completionTimeoutId);
+        removeCallback();
         resolve();
       } else {
         skippedApiRequests.push(`${requestMethod} ${requestUrl}`);
       }
     };
+    const removeCallback = () => {
+      const index = apiRequestCompletedCallbacks.findIndex(f => f === cb);
+      apiRequestCompletedCallbacks.splice(index, 1);
+    };
+    apiRequestCompletedCallbacks.push(cb);
   });
 };
 
@@ -664,8 +670,9 @@ api._makeRequest = async (method, url, headers, requestBody, data, options) => {
       resultBody = JSON.parse(resultBody);
     } catch (e) {}
 
-    apiRequestCompletedCallback &&
-      setTimeout(() => apiRequestCompletedCallback(method, url), 0);
+    apiRequestCompletedCallbacks.forEach(f =>
+      setTimeout(() => f(method, url), 0),
+    );
 
     events.emit("request", { method, url });
 
