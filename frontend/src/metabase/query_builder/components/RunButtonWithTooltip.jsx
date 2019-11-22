913bda88060795e057492b10ac47056bc637ac4f
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-11-22 16:10:52 -0500

    fix issue with null result

--- frontend/src/metabase/query_builder/components/RunButtonWithTooltip.jsx
+++ frontend/src/metabase/query_builder/components/RunButtonWithTooltip.jsx
@@ -8,15 +8,16 @@ import RunButton from "./RunButton";
 
 const REFRESH_TOOLTIP_THRESHOLD = 30 * 1000; // 30 seconds
 
-const defaultGetTooltip = ({
-  isDirty,
-  result: { cached, average_execution_time } = {},
-}) =>
-  !isDirty && cached && average_execution_time > REFRESH_TOOLTIP_THRESHOLD
+const defaultGetTooltip = ({ isDirty, result }) => {
+  const { cached, average_execution_time } = result || {};
+  return !isDirty &&
+    cached &&
+    average_execution_time > REFRESH_TOOLTIP_THRESHOLD
     ? t`This question will take approximately ${duration(
         average_execution_time,
       )} to refresh`
     : null;
+};
 
 export default function RunButtonWithTooltip({
   getTooltip = defaultGetTooltip,
