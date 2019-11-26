8ba21a713489d4df4ff1e8a3b1c7a8ec334fb4c6
Timur Gibadullin <tim.gibadullin@gmail.com>
2019-11-26 21:19:40 +0300

    Fix Maximum call stack size exceeded bug on dashboard

--- frontend/src/metabase/visualizations/components/LegendVertical.jsx
+++ frontend/src/metabase/visualizations/components/LegendVertical.jsx
@@ -25,8 +25,9 @@ export default class LegendVertical extends Component {
     // legend items will overflow the widget area
     const size = ReactDOM.findDOMNode(this).getBoundingClientRect();
 
-    // only check the height. width may flucatuate depending on the browser causing an infinite loop
-    if (this.state.size && size.height !== this.state.size.height) {
+    // check the height, width may flucatuate depending on the browser causing an infinite loop
+    // check overflowCount, because after setting overflowCount the height changes and it causing an infinite loop too
+    if (this.state.size && size.height !== this.state.size.height && prevState.overflowCount === this.state.overflowCount) {
       this.setState({ overflowCount: 0, size });
     } else if (this.state.overflowCount === 0) {
       let overflowCount = 0;
