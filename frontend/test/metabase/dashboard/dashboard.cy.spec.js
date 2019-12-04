b89b5fec0b6fb23bab7fb897f29f507732fa566a
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-12-04 16:50:30 -0500

    port ove one dashboard test

--- frontend/test/metabase/dashboard/dashboard.cy.spec.js
+++ frontend/test/metabase/dashboard/dashboard.cy.spec.js
@@ -3,7 +3,14 @@ import { signInAsAdmin } from "__support__/cypress";
 describe("dashboard", () => {
   Cypress.on("uncaught:exception", (err, runnable) => false);
   beforeEach(signInAsAdmin);
-  const JS_CODE = `// you will need to install via 'npm install jsonwebtoken' or in your package.json
+
+  it("should have the correct embed snippet", () => {
+    cy.visit("/dashboard/1");
+    cy.get(".Icon-share").click();
+    cy.contains(/Embed this .* in an application/).click();
+    cy.contains("Code").click();
+
+    const JS_CODE = `// you will need to install via 'npm install jsonwebtoken' or in your package.json
 
 var jwt = require("jsonwebtoken");
 
@@ -18,29 +25,56 @@ var payload = {
 var token = jwt.sign(payload, METABASE_SECRET_KEY);
 
 var iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#bordered=true&titled=true";`
-    .split("\n")
-    .join("");
+      .split("\n")
+      .join("");
 
-  const IFRAME_CODE = `<iframe
+    const IFRAME_CODE = `<iframe
     src="{{iframeUrl}}"
     frameborder="0"
     width="800"
     height="600"
     allowtransparency
 ></iframe>`
-    .split("\n")
-    .join("");
+      .split("\n")
+      .join("");
 
-  it("should have the correct embed snippet", () => {
-    cy.visit("/dashboard/1");
-    cy.get(".Icon-share").click();
-    cy.contains(/Embed this .* in an application/).click();
-    cy.contains("Code").click();
     cy.get(".ace_content")
       .first()
       .should("have.text", JS_CODE);
     cy.get(".ace_content")
       .last()
       .should("have.text", IFRAME_CODE);
+  });
+
+  it("should update the name and description", () => {
+    cy.visit("/dashboard/1");
+    // click pencil icon to edit
+    cy.get(".Icon-pencil").click();
+    // update title
+    cy.get(".Header-title input")
+      .first()
+      .type("{selectall}Orders per year");
+    // update desc
+    cy.get(".Header-title input")
+      .last()
+      .type("{selectall}How many orders were placed in each year?");
+    cy.contains("Save").click();
+
+    // refresh page and check that title/desc were updated
+    cy.visit("/dashboard/1");
+    cy.contains("Orders per year")
+      .next()
+      .trigger("mouseenter");
+    cy.contains("How many orders were placed in each year?");
+
+    // reset title/desc
+    cy.get(".Icon-pencil").click();
+    cy.get(".Header-title input")
+      .first()
+      .type("{selectall}Orders over time");
+    cy.get(".Header-title input")
+      .last()
+      .clear();
+    cy.contains("Save").click();
   });
 });
