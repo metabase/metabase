20f2a5df18e30077cd55bf3f8b6ac5bad60c9421
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2019-12-18 18:34:34 -0500

    rewrite test to use normal db fixture

--- frontend/test/metabase/visualizations/components/LineAreaBarChart.tz.cy.spec.js
+++ frontend/test/metabase/visualizations/components/LineAreaBarChart.tz.cy.spec.js
@@ -1,3 +1,4 @@
+import path from "path";
 import { signInAsAdmin } from "__support__/cypress";
 
 const expectedValuesByReportingTZ = {
@@ -12,7 +13,8 @@ describe("LineAreaBarChart", () => {
   beforeEach(signInAsAdmin);
 
   it(`should display correctly with server tz ${serverTZ} and client tz ${clientTZ}`, () => {
-    cy.visit("/question/1");
+    addTimestampDatabase();
+    setupQuestion();
 
     // Check bar count and x-axis labels
     cy.get(".bar").should("have.length", 2);
@@ -38,3 +40,50 @@ describe("LineAreaBarChart", () => {
     tooltipContains(secondValue);
   });
 });
+
+function addTimestampDatabase() {
+  cy.visit("/admin/databases/create");
+  cy.get("select").select("H2");
+
+  cy.get('input[name="name"]').type("Timezone Data");
+  const dbPath = path.resolve(
+    Cypress.config("fileServerFolder"),
+    "frontend/test/__runner__/timezone-data.db",
+  );
+  cy.get("input[name='db']").type(`file:${dbPath}`);
+  cy.contains("Save").click();
+  cy.contains("Explore this data");
+}
+
+function setupQuestion() {
+  cy.visit("/question/new");
+  cy.contains("Simple question").click();
+  cy.contains("Timezone Data").click();
+  cy.contains("Times Tamps").click();
+  cy.contains("March 9, 2019");
+  cy.contains("Summarize").click();
+  cy.contains("Group by")
+    .parent()
+    .find(".Icon-calendar")
+    .click();
+  cy.contains("Filter").click();
+  cy.contains("Filter by")
+    .parent()
+    .next()
+    .find(".Icon-calendar")
+    .click();
+
+  cy.contains("Previous")
+    .parent()
+    .click();
+  cy.contains("Before").click();
+
+  cy.get(`input[type="text"]`).type("{selectAll}03/11/2019");
+  cy.contains("Add filter").click();
+
+  cy.contains("Visualization").click();
+  cy.get(".Icon-bar").click();
+  // wait for the chart to finish resizing, so we don't grab the bars as
+  // they're being added/removed
+  cy.wait(2000);
+}
