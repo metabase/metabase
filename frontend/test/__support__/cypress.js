354b862590e86ec6b1506efde3041bb6b1cc666e
Tom Robinson <tlrobinson@gmail.com>
2019-12-18 16:50:34 -0800

    Initial e2e test snapshot/restore prototype

--- frontend/test/__support__/cypress.js
+++ frontend/test/__support__/cypress.js
@@ -10,4 +10,13 @@ export function signInAsNormalUser() {
 
 export const plainDbHost = Cypress.env("PLAIN_DB_HOST");
 
+export function snapshot(name = "snapshot.sql") {
+  console.log("snapshot", name);
+  cy.request("POST", `/api/util/snapshot/${name}`);
+}
+export function restore(name = "snapshot.sql") {
+  console.log("restore", name);
+  cy.request("POST", `/api/util/restore/${name}`);
+}
+
 Cypress.on("uncaught:exception", (err, runnable) => false);
