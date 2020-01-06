214072cbd4867f9cb851734bdc513acf52c30366
Kyle Doherty <kyle.l.doherty@gmail.com>
2020-01-06 15:31:39 -0800

    combine strings for query caching ttl ratio copy

--- src/metabase/public_settings.clj
+++ src/metabase/public_settings.clj
@@ -147,8 +147,7 @@
   :default 60)
 
 (defsetting query-caching-ttl-ratio
-  (str (deferred-tru "To determine how long each saved question''s cached result should stick around, we take the query''s average execution time and multiply that by whatever you input here. ")
-       (deferred-tru "So if a query takes on average 2 minutes to run, and you input 10 for your multiplier, its cache entry will persist for 20 minutes."))
+  (deferred-tru "To determine how long each saved question''s cached result should stick around, we take the query''s average execution time and multiply that by whatever you input here. So if a query takes on average 2 minutes to run, and you input 10 for your multiplier, its cache entry will persist for 20 minutes.")
   :type    :integer
   :default 10)
 
