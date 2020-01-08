373cfc6de65f12e538157958baf24da65104064c
Tom Robinson <tlrobinson@gmail.com>
2020-01-08 13:47:45 -0800

    E2E test snapshot/restore (#11564)
    E2E (for Cypress) snapshot/restore

--- frontend/test/__support__/cypress.js
+++ frontend/test/__support__/cypress.js
@@ -1,13 +1,26 @@
+export const ADMIN_CREDS = {
+  username: "bob@metabase.com",
+  password: "12341234",
+};
+
+export const NORMAL_USER_CREDS = {
+  username: "robert@metabase.com",
+  password: "12341234",
+};
+
 export function signInAsAdmin() {
-  const sessionId = Cypress.env("TEST_FIXTURE_SHARED_ADMIN_LOGIN_SESSION_ID");
-  cy.setCookie("metabase.SESSION", sessionId);
+  cy.request("POST", "/api/session", ADMIN_CREDS);
 }
 
 export function signInAsNormalUser() {
-  const sessionId = Cypress.env("TEST_FIXTURE_SHARED_NORMAL_LOGIN_SESSION_ID");
-  cy.setCookie("metabase.SESSION", sessionId);
+  cy.request("POST", "/api/session", NORMAL_USER_CREDS);
 }
 
-export const plainDbHost = Cypress.env("PLAIN_DB_HOST");
+export function snapshot(name) {
+  cy.request("POST", `/api/testing/snapshot/${name}`);
+}
+export function restore(name = "default") {
+  cy.request("POST", `/api/testing/restore/${name}`);
+}
 
 Cypress.on("uncaught:exception", (err, runnable) => false);
--- frontend/test/metabase/query_builder/simple-question.cy.spec.js
+++ frontend/test/metabase/query_builder/simple-question.cy.spec.js
@@ -1,6 +1,7 @@
-import { signInAsAdmin } from "__support__/cypress";
+import { signInAsAdmin, restore } from "__support__/cypress";
 
 describe("new question", () => {
+  before(restore);
   beforeEach(signInAsAdmin);
 
   it("should count all orders", () => {
