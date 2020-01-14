92ca35d22ac87aa0f1719793afa85f555d70d937
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-01-13 20:26:08 -0500

    import regexvalidator

--- src/metabase/util.clj
+++ src/metabase/util.clj
@@ -25,7 +25,8 @@
            java.util.concurrent.TimeoutException
            java.util.Locale
            javax.xml.bind.DatatypeConverter
-           org.apache.commons.validator.routines.UrlValidator))
+           org.apache.commons.validator.routines.UrlValidator
+           org.apache.commons.validator.routines.RegexValidator))
 
 ;; This is the very first log message that will get printed.
 ;;
