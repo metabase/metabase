c4a8bc9f597b01629cb7fc1d6930bbf91f59b6d2
Cam Saul <github@camsaul.com>
2020-01-21 14:11:44 -0800

    Only run tests against MySQL [ci mysql]

--- test/metabase/db/fix_mysql_utf8_test.clj
+++ test/metabase/db/fix_mysql_utf8_test.clj
@@ -34,39 +34,40 @@
     (mt/dbdef->connection-details :mysql :db {:database-name "utf8_test"})))
 
 (deftest utf8-test
-  (doseq [{:keys [character-set collation]} [{:character-set "utf8",   :collation "utf8_general_ci"}
-                                             {:character-set "latin1", :collation "latin1_swedish_ci"}]]
-    (testing (format "character set = %s" character-set)
-      (create-utf8-test-db! character-set collation)
-      ;; make sure the Table is created without Unicode options set
-      (jdbc/with-db-connection [conn (remove-unicode-connection-options (utf8-test-db-spec))]
-        (jdbc/execute! conn (str (format "CREATE TABLE people (id INTEGER, name TEXT CHARACTER SET %s COLLATE %s NOT NULL)"
-                                         character-set collation)
-                                 (format " ENGINE InnoDB CHARACTER SET %s COLLATE %s;" character-set collation)))
-        (testing "Verify character set and collation were set correctly"
-          (is (= {:character-set character-set, :collation collation}
-                 (#'fix-mysql-utf8/db-character-set-and-collation conn "utf8_test")
-                 (#'fix-mysql-utf8/table-character-set-and-collation conn "utf8_test" "people")
-                 (column-character-set-and-collation conn "utf8_test" "people" "name")))))
-      ;; use spec with normal unicode options from this point on
-      (jdbc/with-db-connection [conn (utf8-test-db-spec)]
-        (testing "This is a latin1/utf8 DB/table, so we shouldn't be able to insert Unicode stuff in it"
-          (is (thrown-with-msg?
-               Exception
-               #"Incorrect string value:"
-               (jdbc/execute! conn ["INSERT INTO people(name) VALUES (?);" test-unicode-str]))))
-        (fix-mysql-utf8/convert-to-utf8mb4! conn "utf8_test")
-        (testing "Verify character set and collation were set correctly"
-          (is (= {:character-set "utf8mb4", :collation "utf8mb4_unicode_ci"}
-                 (#'fix-mysql-utf8/db-character-set-and-collation conn "utf8_test")
-                 (#'fix-mysql-utf8/table-character-set-and-collation conn "utf8_test" "people")
-                 (column-character-set-and-collation conn "utf8_test" "people" "name"))))
-        (testing "Verify that we can now set UTF-8 values"
-          (is true
-              (jdbc/execute! conn ["INSERT INTO people(name) VALUES (?);" test-unicode-str])))
-        (testing "Verify that UTF-8 values come back correctly"
-          (is (= {:id nil, :name test-unicode-str}
-                 (first (jdbc/query conn "SELECT * FROM people;"))))))
-      (testing "if the Table is already `utf8mb4`, `convert-to-utf8mb4` should no-op"
-        (is (= nil
-               (#'fix-mysql-utf8/convert-to-utf8mb4-statements (utf8-test-db-spec) "utf8_test")))))))
+  (mt/test-driver :mysql
+    (doseq [{:keys [character-set collation]} [{:character-set "utf8", :collation "utf8_general_ci"}
+                                               {:character-set "latin1", :collation "latin1_swedish_ci"}]]
+      (testing (format "character set = %s" character-set)
+        (create-utf8-test-db! character-set collation)
+        ;; make sure the Table is created without Unicode options set
+        (jdbc/with-db-connection [conn (remove-unicode-connection-options (utf8-test-db-spec))]
+          (jdbc/execute! conn (str (format "CREATE TABLE people (id INTEGER, name TEXT CHARACTER SET %s COLLATE %s NOT NULL)"
+                                           character-set collation)
+                                   (format " ENGINE InnoDB CHARACTER SET %s COLLATE %s;" character-set collation)))
+          (testing "Verify character set and collation were set correctly"
+            (is (= {:character-set character-set, :collation collation}
+                   (#'fix-mysql-utf8/db-character-set-and-collation conn "utf8_test")
+                   (#'fix-mysql-utf8/table-character-set-and-collation conn "utf8_test" "people")
+                   (column-character-set-and-collation conn "utf8_test" "people" "name")))))
+        ;; use spec with normal unicode options from this point on
+        (jdbc/with-db-connection [conn (utf8-test-db-spec)]
+          (testing "This is a latin1/utf8 DB/table, so we shouldn't be able to insert Unicode stuff in it"
+            (is (thrown-with-msg?
+                 Exception
+                 #"Incorrect string value:"
+                 (jdbc/execute! conn ["INSERT INTO people(name) VALUES (?);" test-unicode-str]))))
+          (fix-mysql-utf8/convert-to-utf8mb4! conn "utf8_test")
+          (testing "Verify character set and collation were set correctly"
+            (is (= {:character-set "utf8mb4", :collation "utf8mb4_unicode_ci"}
+                   (#'fix-mysql-utf8/db-character-set-and-collation conn "utf8_test")
+                   (#'fix-mysql-utf8/table-character-set-and-collation conn "utf8_test" "people")
+                   (column-character-set-and-collation conn "utf8_test" "people" "name"))))
+          (testing "Verify that we can now set UTF-8 values"
+            (is true
+                (jdbc/execute! conn ["INSERT INTO people(name) VALUES (?);" test-unicode-str])))
+          (testing "Verify that UTF-8 values come back correctly"
+            (is (= {:id nil, :name test-unicode-str}
+                   (first (jdbc/query conn "SELECT * FROM people;"))))))
+        (testing "if the Table is already `utf8mb4`, `convert-to-utf8mb4` should no-op"
+          (is (= nil
+                 (#'fix-mysql-utf8/convert-to-utf8mb4-statements (utf8-test-db-spec) "utf8_test"))))))))
