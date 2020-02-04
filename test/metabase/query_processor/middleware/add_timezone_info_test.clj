0cc6f4bf96bd13697608620ec3e6034c40aa6624
Cam Saul <github@camsaul.com>
2020-02-04 15:29:29 -0800

    Fix add-timezone-info-test

--- test/metabase/query_processor/middleware/add_timezone_info_test.clj
+++ test/metabase/query_processor/middleware/add_timezone_info_test.clj
@@ -1,9 +1,9 @@
-(ns metabase.query-processor.middleware.add-settings-test
+(ns metabase.query-processor.middleware.add-timezone-info-test
   (:require [clojure.test :refer :all]
             [metabase
              [driver :as driver]
              [test :as mt]]
-            [metabase.query-processor.middleware.add-settings :as add-settings]))
+            [metabase.query-processor.middleware.add-timezone-info :as add-timezone-info]))
 
 (driver/register! ::timezone-driver, :abstract? true)
 
@@ -26,32 +26,5 @@
         (driver/with-driver driver
           (mt/with-database-timezone-id nil
             (is (= expected
-                   (let [query        {:query? true}
-                         results      {:results? true}
-                         add-settings (add-settings/add-settings (constantly results))]
-                     (:data (add-settings query)))))))))))
-
-(defn- env [_]
-  "SOME_VALUE")
-
-(defprotocol Config
-  (config-1 [_])
-  (config-2 [_])
-  (config-3 [_]))
-
-(defn env-config []
-  (reify Config
-    (config-1 [_] (env :config-1))
-    (config-2 [_] (env :config-2))
-    (config-3 [_] (env :config-3))))
-
-(defn- pretty-print-config-3 [config]
-  (format "config 3 is '%s'" (config-3 config)))
-
-(deftest pretty-print-config-3-test
-  (is (= "config 3 is 'SOME_VALUE'"
-         (pretty-print-config-3
-          (reify Config
-            (config-1 [_] "SOME_VALUE")
-            (config-2 [_] "SOME_VALUE")
-            (config-3 [_] "SOME_VALUE"))))))
+                   (-> (mt/test-qp-middleware add-timezone-info/add-timezone-info nil nil nil)
+                       :metadata)))))))))
