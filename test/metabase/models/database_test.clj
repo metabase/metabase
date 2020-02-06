3bd8627433acdc00e1a21e132074b0c1cb240492
Mohammed Amarnah <m.amarnah@gmail.com>
2020-02-06 17:59:21 +0200

    Add database test for unique name/engine migration

---
+++ test/metabase/models/database_test.clj
@@ -0,0 +1,33 @@
+(ns metabase.models.database-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :refer [join lower-case split]]
+            [metabase
+             [models :refer [Database]]]
+            [toucan.db :as db]))
+
+(defn- create-database!
+  [db-name engine]
+  (db/insert! Database
+        :name db-name
+        :engine (name engine)
+        :details {:db (join "-" (mapv lower-case (split db-name #"(?=[A-Z])")))}))
+
+(deftest disallow-duplicate-databases-with-same-name-and-engine
+  (testing "Migration #107\n"
+    (testing "Shouldn't be able to create two Databases with the same name and engine..."
+
+      (testing "mysql..."
+        (let [db (create-database! "TestDatabase" :mysql)]
+          (is (thrown? Exception (create-database! "TestDatabase" :mysql)))))
+
+      (testing "mariadb..."
+        (let [db (create-database! "TestDatabase" :mariadb)]
+          (is (thrown? Exception (create-database! "TestDatabase" :mariadb)))))
+
+      (testing "h2..."
+        (let [db (create-database! "TestDatabase" :h2)]
+          (is (thrown? Exception (create-database! "TestDatabase" :h2)))))
+
+      (testing "postgresql..."
+        (let [db (create-database! "TestDatabase" :postgresql)]
+          (is (thrown? Exception (create-database! "TestDatabase" :postgresql))))))))
