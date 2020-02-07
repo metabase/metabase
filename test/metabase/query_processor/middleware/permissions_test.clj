d9abd6cadbb77f768687c402d65dbef362429f28
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2020-02-08 01:28:07 +0200

    Convert tests to clojure.test

--- test/metabase/query_processor/middleware/permissions_test.clj
+++ test/metabase/query_processor/middleware/permissions_test.clj
@@ -1,6 +1,6 @@
 (ns metabase.query-processor.middleware.permissions-test
   "Tests for the middleware that checks whether the current user has permissions to run a given query."
-  (:require [expectations :refer :all]
+  (:require [clojure.test :refer :all]
             [metabase
              [query-processor :as qp]
              [util :as u]]
@@ -32,111 +32,105 @@
   [query]
   (do-with-rasta (fn [] (check-perms query))))
 
+(deftest native-query-perms-test
+  (testing "Make sure the NATIVE query fails to run if current user doesn't have perms"
+    (is (thrown?  Exception
+                 (check-perms-for-rasta
+                  {:database 1000
+                   :type     :native
+                   :native   {:query "SELECT * FROM VENUES"}}))))
 
-;;; ------------------------------------------------- Native Queries -------------------------------------------------
+  (testing "...but it should work if user has perms"
+    (tt/with-temp Database [db]
+      ;; query should be returned by middleware unchanged
+      (is (= {:database (u/get-id db)
+              :type     :native
+              :native   {:query "SELECT * FROM VENUES"}}
+             (check-perms-for-rasta
+              {:database (u/get-id db)
+               :type     :native
+               :native   {:query "SELECT * FROM VENUES"}}))))))
 
-;; Make sure the NATIVE query fails to run if current user doesn't have perms
-(expect
-  Exception
-  (check-perms-for-rasta
-    {:database 1000
-     :type     :native
-     :native   {:query "SELECT * FROM VENUES"}}))
+(deftest mbql-query-perms-test
+  (testing "Make sure the MBQL query fails to run if current user doesn't have perms"
+    (is (thrown? Exception
+                 (tt/with-temp* [Database [db]
+                                 Table    [table {:db_id (u/get-id db)}]]
+                   ;; All users get perms for all new DBs by default
+                   (perms/revoke-permissions! (perms-group/all-users) (u/get-id db))
+                   (check-perms-for-rasta
+                    {:database (u/get-id db)
+                     :type     :query
+                     :query    {:source-table {:name "Toucans", :id (u/get-id table)}}})))))
 
-;; ...but it should work if user has perms
-(tt/expect-with-temp [Database [db]]
-  ;; query should be returned by middleware unchanged
-  {:database (u/get-id db)
-   :type     :native
-   :native   {:query "SELECT * FROM VENUES"}}
-  (check-perms-for-rasta
-    {:database (u/get-id db)
-     :type     :native
-     :native   {:query "SELECT * FROM VENUES"}}))
+  (testing "...but it should work if user has perms [MBQL]"
+    (tt/with-temp* [Database [db]
+                    Table    [table {:db_id (u/get-id db)}]]
+      ;; query should be returned by middleware unchanged
+      (= {:database (u/get-id db)
+          :type     :query
+          :query    {:source-table (u/get-id table)}}
+         (check-perms-for-rasta
+          {:database (u/get-id db)
+           :type     :query
+           :query    {:source-table (u/get-id table)}})))))
 
+(deftest nested-native-query-test
+  (testing "Make sure nested native query fails to run if current user doesn't have perms"
+    (is (thrown? Exception
+                 (check-perms-for-rasta
+                  {:database 1000
+                   :type     :query
+                   :query   {:source-query {:native "SELECT * FROM VENUES"}}}))))
 
-;;; -------------------------------------------------- MBQL Queries --------------------------------------------------
+  (testing "...but it should work if user has perms [nested native queries]"
+    (tt/with-temp Database [db]
+      ;; query should be returned by middleware unchanged
+      (= {:database (u/get-id db)
+          :type     :query
+          :query    {:source-query {:native "SELECT * FROM VENUES"}}}
+         (check-perms-for-rasta
+          {:database (u/get-id db)
+           :type     :query
+           :query   {:source-query {:native "SELECT * FROM VENUES"}}})))))
 
-(expect
-  Exception
-  (tt/with-temp* [Database [db]
-                  Table    [table {:db_id (u/get-id db)}]]
-    (perms/revoke-permissions! (perms-group/all-users) (u/get-id db)) ; All users get perms for all new DBs by default
-    (check-perms-for-rasta
-      {:database (u/get-id db)
-       :type     :query
-       :query    {:source-table {:name "Toucans", :id (u/get-id table)}}})))
+(deftest nested-mbql-query-test
+  (testing "Make sure nested MBQL query fails to run if current user doesn't have perms"
+    (is (thrown? Exception
+                 (tt/with-temp* [Database [db]
+                                 Table    [table {:db_id (u/get-id db)}]]
+                   ;; All users get perms for all new DBs by default
+                   (perms/revoke-permissions! (perms-group/all-users) (u/get-id db))
+                   (check-perms-for-rasta
+                    {:database (u/get-id db)
+                     :type     :query
+                     :query    {:source-query {:source-table {:name "Toucans", :id (u/get-id table)}}}})))))
 
-(tt/expect-with-temp [Database [db]
-                      Table    [table {:db_id (u/get-id db)}]]
-  ;; query should be returned by middleware unchanged
-  {:database (u/get-id db)
-   :type     :query
-   :query    {:source-table (u/get-id table)}}
-  (check-perms-for-rasta
-    {:database (u/get-id db)
-     :type     :query
-     :query    {:source-table (u/get-id table)}}))
+  (testing "...but it should work if user has perms [nested MBQL queries]"
+    (tt/with-temp* [Database [db]
+                    Table    [table {:db_id (u/get-id db)}]]
+      (= {:database (u/get-id db)
+          :type     :query
+          :query    {:source-query {:source-table (u/get-id table)}}}
+         (check-perms-for-rasta
+          {:database (u/get-id db)
+           :type     :query
+           :query    {:source-query {:source-table (u/get-id table)}}}))))
 
-
-;;; --------------------------------------------- Nested Native Queries ----------------------------------------------
-
-(expect
-  Exception
-  (check-perms-for-rasta
-    {:database 1000
-     :type     :query
-     :query   {:source-query {:native "SELECT * FROM VENUES"}}}))
-
-;; ...but it should work if user has perms
-(tt/expect-with-temp [Database [db]]
-  {:database (u/get-id db)
-   :type     :query
-   :query    {:source-query {:native "SELECT * FROM VENUES"}}}
-  (check-perms-for-rasta
-    {:database (u/get-id db)
-     :type     :query
-     :query   {:source-query {:native "SELECT * FROM VENUES"}}}))
-
-
-;;; ---------------------------------------------- Nested MBQL Queries -----------------------------------------------
-
-;; For nested queries MBQL make sure perms are checked
-(expect
-  Exception
-  (tt/with-temp* [Database [db]
-                  Table    [table {:db_id (u/get-id db)}]]
-    (perms/revoke-permissions! (perms-group/all-users) (u/get-id db)) ; All users get perms for all new DBs by default
-    (check-perms-for-rasta
-      {:database (u/get-id db)
-       :type     :query
-       :query    {:source-query {:source-table {:name "Toucans", :id (u/get-id table)}}}})))
-
-(tt/expect-with-temp [Database [db]
-                      Table    [table {:db_id (u/get-id db)}]]
-  {:database (u/get-id db)
-   :type     :query
-   :query    {:source-query {:source-table (u/get-id table)}}}
-  (check-perms-for-rasta
-    {:database (u/get-id db)
-     :type     :query
-     :query    {:source-query {:source-table (u/get-id table)}}}))
-
-
-;; Make sure it works end-to-end: make sure bound `*current-user-id*` and `*current-user-permissions-set*` are used to
-;; permissions check queries
-(tu/expect-schema
-  {:status   (s/eq :failed)
-   :class    (s/eq clojure.lang.ExceptionInfo)
-   :error    (s/eq "You do not have permissions to run this query.")
-   :ex-data  (s/eq {:required-permissions #{(perms/table-query-path (data/id) "PUBLIC" (data/id :venues))}
-                    :actual-permissions   #{}
-                    :permissions-error?   true})
-   s/Keyword s/Any}
-  (binding [api/*current-user-id*              (users/user->id :rasta)
-            api/*current-user-permissions-set* (delay #{})]
-   (qp/process-query
-     {:database (data/id)
-      :type     :query
-      :query    {:source-table (data/id :venues)
-                 :limit        1}})))
+  ;; Make sure it works end-to-end:
+  (testing "make sure `*current-user-id*` and `*current-user-permissions-set*` are used to check query permissions"
+    (is (schema=
+         {:status   (s/eq :failed)
+          :class    (s/eq clojure.lang.ExceptionInfo)
+          :error    (s/eq "You do not have permissions to run this query.")
+          :ex-data  (s/eq {:required-permissions #{(perms/table-query-path (data/id) "PUBLIC" (data/id :venues))}
+                           :actual-permissions   #{}
+                           :permissions-error?   true})
+          s/Keyword s/Any}
+         (binding [api/*current-user-id*              (users/user->id :rasta)
+                   api/*current-user-permissions-set* (delay #{})]
+           (qp/process-query
+            {:database (data/id)
+             :type     :query
+             :query    {:source-table (data/id :venues)
+                        :limit        1}}))))))
