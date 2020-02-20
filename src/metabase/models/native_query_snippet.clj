9d5e1044c64e8966f08769fe9df3504f4fbddc9d
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2020-02-20 22:37:25 +0200

    Add migration and model for native query snippet table

---
+++ src/metabase/models/native_query_snippet.clj
@@ -0,0 +1,35 @@
+(ns metabase.models.native-query-snippet
+  (:require [metabase.models
+             [interface :as i]
+             [permissions :as perms]]
+            [metabase.util :as u]
+            [metabase.util.i18n :refer [deferred-tru]]
+            [toucan.models :as models]))
+
+;;; ----------------------------------------------- Entity & Lifecycle -----------------------------------------------
+
+(models/defmodel NativeQuerySnippet :native_query_snippet)
+
+(defn- pre-delete
+  [{:keys [id]}]
+  (throw (ex-info (deferred-tru "Can''t delete snippets; mark as archived instead")
+                  {:native-query-snippet-id id})))
+
+(defn- perms-objects-set
+  "Permissions to read or write a native query snippet are the same as those of its parent Database."
+  [snippet _]
+  #{(perms/object-path (u/get-id (:database-id snippet)))})
+
+(u/strict-extend (class NativeQuerySnippet)
+  models/IModel
+  (merge
+   models/IModelDefaults
+   {:properties (constantly {:timestamped? true})
+    :pre-delete pre-delete})
+
+  i/IObjectPermissions
+  (merge
+   i/IObjectPermissionsDefaults
+   {:can-read?         (partial i/current-user-has-full-permissions? :read)
+    :can-write?        (partial i/current-user-has-full-permissions? :write)
+    :perms-objects-set perms-objects-set}))
