e5d8439a6629be461d2fb756e86587f679505d3f
Cam Saul <1455846+camsaul@users.noreply.github.com>
2020-02-19 16:44:25 -0800

    Simplify streaming QP/API response glue namespaces (#11943)

--- src/metabase/query_processor/streaming.clj
+++ src/metabase/query_processor/streaming.clj
@@ -1,9 +1,6 @@
 (ns metabase.query-processor.streaming
-  (:require [cheshire.core :as json]
-            [clojure.core.async :as a]
-            [metabase.async
-             [streaming-response :as streaming-response]
-             [util :as async.u]]
+  (:require [clojure.core.async :as a]
+            [metabase.async.streaming-response :as streaming-response]
             [metabase.query-processor.context :as context]
             [metabase.query-processor.streaming
              [csv :as streaming.csv]
@@ -11,9 +8,8 @@
              [json :as streaming.json]
              [xlsx :as streaming.xlsx]]
             [metabase.util :as u])
-  (:import [java.io BufferedWriter OutputStream OutputStreamWriter]
-           java.nio.charset.StandardCharsets
-           org.eclipse.jetty.io.EofException))
+  (:import clojure.core.async.impl.channels.ManyToManyChannel
+           java.io.OutputStream))
 
 ;; these are loaded for side-effects so their impls of `i/results-writer` will be available
 ;; TODO - consider whether we should lazy-load these!
@@ -46,13 +42,6 @@
       (.close os))
     (context/resultf final-metadata context)))
 
-(defn- write-qp-failure-and-close! [^OutputStream os result]
-  (with-open [writer (BufferedWriter. (OutputStreamWriter. os StandardCharsets/UTF_8))]
-    (try
-      (json/generate-stream result writer)
-      (catch EofException _)))
-  (.close os))
-
 (defn streaming-context
   "Context to pass to the QP to streaming results as `export-format` to an output stream. Can be used independently of
   the normal `streaming-response` macro, which is geared toward Ring responses.
@@ -64,41 +53,23 @@
     {:rff      (streaming-rff results-writer)
      :reducedf (streaming-reducedf results-writer os)}))
 
-;; TODO -- consider whether it makes sense to begin writing keepalive chars right away or if maybe we should wait to
-;; call `respond` in async endpoints for 30-60 seconds that way we're not wasting a Ring thread right away
-(defn do-streaming-response
+(defn streaming-response*
   "Impl for `streaming-response`."
-  ^metabase.async.streaming_response.StreamingResponse [export-format run-query-fn]
+  [export-format f]
   (streaming-response/streaming-response (i/stream-options export-format) [os canceled-chan]
-    (let [out-chan (try
-                     (run-query-fn (streaming-context export-format os))
-                     (catch Throwable e
-                       e))]
-      (if (async.u/promise-chan? out-chan)
-        (a/go
-          (let [[val port] (a/alts! [out-chan canceled-chan] :priority true)]
-            (cond
-              ;; if result is an Exception or a QP failure response write that out (async) and close up
-              (and (= port out-chan)
-                   (instance? Throwable val))
-              (a/thread (streaming-response/write-error-and-close! os val))
-
-              (and (= port out-chan)
-                   (map? val)
-                   (= (:status val) :failed))
-              (a/thread (write-qp-failure-and-close! os val))
-
-              ;; otherwise if the `cancled-chan` go a message we can tell the QP to cancel the running query by
-              ;; closing `out-chan`
-              (and (= port canceled-chan)
-                   (nil? val))
-              (a/close! out-chan))))
-        ;; if we got something besides a channel, such as a Throwable, write it as JSON to the `out-chan` and close
-        (a/thread
-          (if (instance? Throwable out-chan)
-            (streaming-response/write-error-and-close! os out-chan)
-            (write-qp-failure-and-close! os out-chan))))
-      nil)))
+    (let [result (try
+                   (f (streaming-context export-format os))
+                   (catch Throwable e
+                     e))
+          result (if (instance? ManyToManyChannel result)
+                   (let [[val port] (a/alts!! [result canceled-chan])]
+                     (when (= port canceled-chan)
+                       (a/close! result))
+                     val)
+                   result)]
+      (when (or (instance? Throwable result)
+                (= (:status result) :failed))
+        (streaming-response/write-error! os result)))))
 
 (defmacro streaming-response
   "Return results of processing a query as a streaming response. This response implements the appropriate Ring/Compojure
@@ -109,11 +80,13 @@
 
     (api/defendpoint GET \"/whatever\" []
       (qp.streaming/streaming-response [context :json]
-        (qp/process-query-and-save-with-max-results-constraints! (assoc my-query :async? true) context)))"
+        (qp/process-query-and-save-with-max-results-constraints! (assoc query :async true) context)))
+
+  Handles either async or sync QP results, but you should prefer returning sync results so we can handle query
+  cancelations properly."
   {:style/indent 1}
-  [[context-binding export-format :as bindings] & body]
-  {:pre [(vector? bindings) (= (count bindings) 2)]}
-  `(do-streaming-response ~export-format (fn [~context-binding] ~@body)))
+  [[context-binding export-format] & body]
+  `(streaming-response* ~export-format (fn [~context-binding] ~@body)))
 
 (defn export-formats
   "Set of valid streaming response formats. Currently, `:json`, `:csv`, `:xlsx`, and `:api` (normal JSON API results
