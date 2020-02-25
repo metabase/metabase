26d5be7899b9f3fb23685e89a7f20ef8d2ec157d
Cam Saul <github@camsaul.com>
2020-02-25 13:15:11 -0800

    Cleanup metabase.api.field-test [ci druid]

--- test/metabase/timeseries_query_processor_test/util.clj
+++ test/metabase/timeseries_query_processor_test/util.clj
@@ -1,6 +1,7 @@
 (ns metabase.timeseries-query-processor-test.util
   "Utility functions and macros for testing timeseries database drivers, such as Druid."
   (:require [metabase.test.data :as data]
+            [metabase.test :as mt]
             [metabase.test.data
              [dataset-definitions :as defs]
              [datasets :as datasets]
@@ -16,12 +17,20 @@
 (defn do-with-flattened-dbdef
   "Execute `f` with a flattened version of the test data DB as the current DB def."
   [f]
-  (data/dataset flattened-db-def (f)))
+  (mt/dataset flattened-db-def (f)))
 
 (defmacro with-flattened-dbdef
   "Execute `body` using the flattened test data DB definition."
   [& body]
   `(do-with-flattened-dbdef (fn [] ~@body)))
+
+(defn do-test-timeseries-drivers [thunk]
+  (mt/test-drivers (timeseries-drivers)
+    (with-flattened-dbdef
+      (thunk))))
+
+(defmacro test-timeseries-drivers {:style/indent 0} [& body]
+  `(do-test-timeseries-drivers (fn [] ~@body)))
 
 (defmacro ^:deprecated expect-with-timeseries-dbs
   "DEPRECATED: Prefer
