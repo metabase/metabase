e9e8d766e5965b988b935dab5ab670e69f7002f5
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2020-02-26 00:10:30 +0200

    Test update API endpoint for native query snippets

--- test/metabase/api/native_query_snippet_test.clj
+++ test/metabase/api/native_query_snippet_test.clj
@@ -15,6 +15,11 @@
 (def ^:private test-snippet-fields [:content :creator_id :database_id :description :name])
 (def ^:private rasta (fetch-user :rasta))
 
+(defn- snippet-url
+  [& [arg & _]]
+  (str "native-query-snippet"
+       (when arg (str "/" arg))))
+
 ;; GET /api/native-query-snippet
 (deftest list-snippets-api-test
   (tt/with-temp* [Database           [db]
@@ -29,7 +34,7 @@
                                                  :description "Test snippet 2"
                                                  :name        "snippet_2"}]]
     (testing "list returns all snippets"
-      (let [snippets-from-api (->> ((user->client :crowberto) :get 200 "native-query-snippet")
+      (let [snippets-from-api (->> ((user->client :crowberto) :get 200 (snippet-url))
                                    (map #(select-keys % test-snippet-fields))
                                    set)]
         (is (contains? snippets-from-api (select-keys snippet-1 test-snippet-fields)))
@@ -37,7 +42,7 @@
 
     (testing "list fails for user without read permission"
       (perms/revoke-permissions! (group/all-users) db)
-      (is (empty? ((user->client :rasta) :get 200 "native-query-snippet"))))))
+      (is (empty? ((user->client :rasta) :get 200 (snippet-url)))))))
 
 ;; GET /api/native-query-snippet/:id
 (deftest read-snippet-api-test
@@ -48,7 +53,7 @@
                                                :description "SQL comment snippet"
                                                :name        "comment"}]]
     (testing "read returns all snippet fields"
-      (let [snippet-from-api ((user->client :crowberto) :get 200 (str "native-query-snippet/" (:id snippet)))]
+      (let [snippet-from-api ((user->client :crowberto) :get 200 (snippet-url (:id snippet)))]
         (is (= (select-keys snippet test-snippet-fields)
                (select-keys snippet-from-api test-snippet-fields)))))
 
@@ -63,18 +68,18 @@
   (tt/with-temp* [Database [db]]
     (testing "new snippet field validation"
       (is (= {:errors {:content "value must be a string."}}
-             ((user->client :rasta) :post 400 "native-query-snippet" {})))
+             ((user->client :rasta) :post 400 (snippet-url) {})))
       (is (= {:errors {:database_id "value must be an integer greater than zero."}}
-             ((user->client :rasta) :post 400 "native-query-snippet" {:content "NULL"})))
+             ((user->client :rasta) :post 400 (snippet-url) {:content "NULL"})))
       (let [response ((user->client :rasta)
-                      :post 400 "native-query-snippet"
+                      :post 400 (snippet-url)
                       {:content "NULL", :database_id (:id db)})]
         (is (str/starts-with? (get-in response [:errors :name])
                               "Value does not match schema: "))))
 
     (testing "successful create returns new snippet's data"
       (let [snippet-input    {:name "test-snippet", :description "Just null", :content "NULL", :database_id (:id db)}
-            snippet-from-api ((user->client :crowberto) :post 200 "native-query-snippet" snippet-input)]
+            snippet-from-api ((user->client :crowberto) :post 200 (snippet-url) snippet-input)]
         (is (pos? (:id snippet-from-api)))
 
         (is (= (:database_id snippet-input)
@@ -102,14 +107,27 @@
       (perms/revoke-permissions! (group/all-users) db)
       (is (= "You don't have permissions to do that."
              ((user->client :rasta)
-              :post 403 "native-query-snippet"
+              :post 403 (snippet-url)
               {:name "test-snippet", :content "NULL", :database_id (:id db)}))))))
 
 ;; PUT /api/native-query-snippet/:id
 (deftest update-snippet-api-test
-  (testing "update stores updated snippet"
-    (is false))
+  (tt/with-temp* [Database           [db]
+                  NativeQuerySnippet [snippet {:content     "-- SQL comment here"
+                                               :creator_id  (:id rasta)
+                                               :database_id (:id db)
+                                               :description "SQL comment snippet"
+                                               :name        "comment"}]]
+    (testing "update stores updated snippet"
+      (let [updated-desc    "Updated descripted."
+            updated-snippet ((user->client :crowberto)
+                             :put 200 (snippet-url (:id snippet))
+                             {:description updated-desc})]
+        (is (= updated-desc (:description updated-snippet)))))
 
-  (testing "update fails for non-admin user"
-    ;; TODO implement this
-    (is false)))
+    (testing "update fails for non-admin user"
+      (perms/revoke-permissions! (group/all-users) db)
+      (is (= "You don't have permissions to do that."
+             ((user->client :rasta)
+              :put 403 (snippet-url (:id snippet))
+              {:description "This description shouldn't get updated due to permissions error."}))))))
