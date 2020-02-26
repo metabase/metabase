c04f818ae51b8d0c6e0341a7929ccca4108bbba2
Mohammed Amarnah <m.amarnah@gmail.com>
2020-02-26 13:29:25 +0200

    tweak tests to be enabled only on registered drivers

--- test/metabase/models/database_test.clj
+++ test/metabase/models/database_test.clj
@@ -3,31 +3,32 @@
              [string :refer [join lower-case split]]
              [test :refer :all]]
             [metabase.models :refer [Database]]
+            [metabase
+             [driver :as driver]
+             [util :as u]]
             [toucan.db :as db]))
 
+(driver/register! ::test-driver
+                  :parent :sql-jdbc
+                  :abstract? true)
+
 (defn- create-database!
-  [db-name engine]
+  [db-name]
   (db/insert! Database
-        :name db-name
-        :engine (name engine)
-        :details {:db (join "-" (mapv lower-case (split db-name #"(?=[A-Z])")))}))
+              :name db-name
+              :engine (u/qualified-name ::test-driver)
+              :details {:db (join "-" (mapv lower-case (split db-name #"(?=[A-Z])")))}))
+
+(defn- delete-database!
+  [db-name]
+  (db/delete! Database :name db-name))
 
 (deftest disallow-duplicate-databases-with-same-name-and-engine
-  (testing "Migration #107\n"
+  (testing "Migration #162\n"
     (testing "Shouldn't be able to create two Databases with the same name and engine..."
-
-      (testing "mysql..."
-        (let [db (create-database! "TestDatabase" :mysql)]
-          (is (thrown? Exception (create-database! "TestDatabase" :mysql)))))
-
-      (testing "mariadb..."
-        (let [db (create-database! "TestDatabase" :mariadb)]
-          (is (thrown? Exception (create-database! "TestDatabase" :mariadb)))))
-
-      (testing "h2..."
-        (let [db (create-database! "TestDatabase" :h2)]
-          (is (thrown? Exception (create-database! "TestDatabase" :h2)))))
-
-      (testing "postgresql..."
-        (let [db (create-database! "TestDatabase" :postgresql)]
-          (is (thrown? Exception (create-database! "TestDatabase" :postgresql))))))))
+      (try
+        (do
+          (create-database! "TestDatabase")
+          (is (thrown? Exception (create-database! "TestDatabase"))))
+        (finally
+          (delete-database! "TestDatabase"))))))
