7ef36e570687de2f71c282e8e94fecf716a2070c
Maz Ameli <maz@metabase.com>
2020-03-02 17:25:39 -0800

    mock up the file

---
+++ frontend/src/metabase/lib/expressions/helper_text_strings.js
@@ -0,0 +1,19 @@
+import { t } from "ttag";
+
+// avg
+{
+  name: average,
+  structure: "avg(" + t`column` + ")",
+  description: t`Returns the average of the values in the column.`,
+  example: "avg( [" + t`Quantity` + "] )",
+  arg1: t`column`,
+  arg1Desc: t`The column whose values to average.`,
+},
+{
+  name: between,
+  structure: "between(" + t`column` + "," + t`start`+ "," + t`end` + ")",
+  description: t`Checks a date or number column's values to see if they're within the specified range.`,
+  example: "between( [" + t`Rating` + "], 3.75, 5 )",
+  arg1: t`column`,
+  arg1Desc: t`The column whose values to average.`,
+},
