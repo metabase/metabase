3a7d3412b1929a391a141d82380ce48ac4efb4ae
Walter Leibbrandt <23798+walterl@users.noreply.github.com>
2020-03-04 23:55:22 +0200

    Rename param k→ks for clarity

--- src/metabase/util.clj
+++ src/metabase/util.clj
@@ -607,9 +607,9 @@
 (defn update-in-when
   "Like `clojure.core/update-in` but does not create new keys if they do not exist. Useful when you don't want to create
   cruft."
-  [m k f & args]
-  (if (not= ::not-found (get-in m k ::not-found))
-    (apply update-in m k f args)
+  [m ks f & args]
+  (if (not= ::not-found (get-in m ks ::not-found))
+    (apply update-in m ks f args)
     m))
 
 (defn index-of
