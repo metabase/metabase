9c2ec99dec34f032b46f4868f3e54524c3e5f9c3
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-03-05 11:56:36 -0500

    update query field ids

--- src/metabase/cmd/change_card_source.clj
+++ src/metabase/cmd/change_card_source.clj
@@ -1,5 +1,6 @@
 (ns metabase.cmd.change-card-source
-  (:require [toucan.db :as tdb]
+  (:require [clojure.walk :as walk]
+            [toucan.db :as tdb]
             [metabase.models.card :as card]
             [metabase.models.field :as field]))
 
@@ -19,13 +20,21 @@
 (defn remap-card
   "Updates table_id, database_id, and field_ids in card"
   [mapping card-id]
-  (let [original-card (tdb/select-one card/Card :id card-id)]
+  (let [original-card (tdb/select-one card/Card :id card-id)
+        db-id         (:new (:database mapping))
+        table-id      (:new (:table mapping))]
     (-> original-card
-        (assoc :table_id    (:table-id mapping)
-               :database_id (:database-id mapping)))))
+        (assoc :table_id    table-id
+               :database_id db-id)
+        (assoc-in [:dataset_query :database] db-id)
+        (assoc-in [:dataset_query :query :source-table] table-id)
+        (update-in [:dataset_query :query]
+                   (fn [query]
+                     (walk/postwalk (fn [x]
+                                      (if (and (vector? x)
+                                               (= :field-id (first x)))
+                                        (update x 1 (fn [old-field-id]
+                                                      (get-in mapping [:fields old-field-id])))
+                                        x))
+                                    query))))))
 
-{:table {:old 1572
-         :new 1586}
- :database {:old 1429
-            :new 1430}
- :fields {"ts" "created_at"}}
