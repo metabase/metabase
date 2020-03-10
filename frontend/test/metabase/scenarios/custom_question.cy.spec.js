5b837aad5c0c93d7cf7d8379ba96caa77fc1c0c4
Paul Rosenzweig <paulrosenzweig@users.noreply.github.com>
2020-03-10 17:05:52 -0400

    Fix data selector for joins (#12066)

--- frontend/test/metabase/scenarios/custom_question.cy.spec.js
+++ frontend/test/metabase/scenarios/custom_question.cy.spec.js
@@ -27,4 +27,39 @@ describe("custom question", () => {
     cy.contains("2372"); // user's id in the table
     cy.contains("Showing 1 row"); // ensure only one user was returned
   });
+
+  it("should allow joins", () => {
+    // start a custom question with orders
+    cy.visit("/question/new");
+    cy.contains("Custom question").click();
+    cy.contains("Sample Dataset").click();
+    cy.contains("Orders").click();
+
+    // join to Reviews on orders.product_id = reviews.product_id
+    cy.get(".Icon-join_left_outer").click();
+    popover()
+      .contains("Reviews")
+      .click();
+    popover()
+      .contains("Product ID")
+      .click();
+    popover()
+      .contains("Product ID")
+      .click();
+
+    // get the average rating across all rows (not a useful metric)
+    cy.contains("Pick the metric you want to see").click();
+    popover()
+      .contains("Average of")
+      .click();
+    popover()
+      .find(".Icon-join_left_outer")
+      .click();
+    popover()
+      .contains("Rating")
+      .click();
+    cy.contains("Visualize").click();
+    cy.contains("Orders + Reviews");
+    cy.contains("3");
+  });
 });
