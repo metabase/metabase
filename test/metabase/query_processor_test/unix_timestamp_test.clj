843acf491a82af5920e00c9085cf2e418be514f1
Cam Saul <github@camsaul.com>
2020-03-12 21:06:06 -0700

    Test that date/range Field filters work with UNIX timestamps for all DBs that support filters [ci drivers]

--- test/metabase/query_processor_test/unix_timestamp_test.clj
+++ test/metabase/query_processor_test/unix_timestamp_test.clj
@@ -3,29 +3,27 @@
   (:require [clojure.test :refer :all]
             [metabase
              [driver :as driver]
-             [query-processor-test :as qp.test :refer :all]]
-            [metabase.test
-             [data :as data]
-             [util :as tu]]
-            [metabase.test.data.datasets :as datasets]))
+             [query-processor :as qp]
+             [query-processor-test :as qp.test]
+             [test :as mt]]))
 
 (deftest filter-test
-  (datasets/test-drivers (qp.test/normal-drivers)
+  (mt/test-drivers (mt/normal-drivers)
     (is (= 10
            ;; There's a race condition with this test. If we happen to grab a
            ;; connection that is in a session with the timezone set to pacific,
            ;; we'll get 9 results even when the above if statement is true. It
            ;; seems to be pretty rare, but explicitly specifying UTC will make
            ;; the issue go away
-           (tu/with-temporary-setting-values [report-timezone "UTC"]
-             (count (rows (data/dataset sad-toucan-incidents
-                            (data/run-mbql-query incidents
+           (mt/with-temporary-setting-values [report-timezone "UTC"]
+             (count (rows (mt/dataset sad-toucan-incidents
+                            (mt/run-mbql-query incidents
                               {:filter   [:= [:datetime-field $timestamp :day] "2015-06-02"]
                                :order-by [[:asc $timestamp]]}))))))
         "There were 10 'sad toucan incidents' on 2015-06-02 in UTC")))
 
 (deftest results-test
-  (datasets/test-drivers (qp.test/normal-drivers)
+  (mt/test-drivers (mt/normal-drivers)
     (is (= (cond
              (= :sqlite driver/*driver*)
              [["2015-06-01"  6]
@@ -74,10 +72,30 @@
               ["2015-06-08T00:00:00Z" 9]
               ["2015-06-09T00:00:00Z" 7]
               ["2015-06-10T00:00:00Z" 9]])
-           (tu/with-temporary-setting-values [report-timezone "America/Los_Angeles"]
-             (->> (data/dataset sad-toucan-incidents
-                    (data/run-mbql-query incidents
+           (mt/with-temporary-setting-values [report-timezone "America/Los_Angeles"]
+             (->> (mt/dataset sad-toucan-incidents
+                    (mt/run-mbql-query incidents
                       {:aggregation [[:count]]
                        :breakout    [$timestamp]
                        :limit       10}))
                   rows (format-rows-by [identity int])))))))
+
+(deftest substitute-native-parameters-test
+  (mt/test-drivers (mt/normal-drivers-with-feature :native-parameters)
+    (testing "Make sure `:date/range` SQL field filters work correctly with UNIX timestamps (#11934)"
+      (mt/dataset tupac-sightings
+        (let [{native-query :query} (mt/dataset tupac-sightings (qp/query->native (mt/mbql-query sightings {:aggregation [[:count]]})))
+              query                 (mt/query sightings
+                                      {:type   :native
+                                       :native {:query         (str native-query " WHERE {{timestamp}}")
+                                                :template-tags {"timestamp" {:name         "timestamp"
+                                                                             :display-name "Sighting Timestamp"
+                                                                             :type         :dimension
+                                                                             :dimension    $timestamp
+                                                                             :widget-type  :date/range}}
+                                                :parameters    [{:type   :date/range
+                                                                 :target [:dimension [:template-tag "timestamp"]]
+                                                                 :value  "2014-02-01~2015-02-29"}]}})]
+          (is (= [[41]]
+                 (mt/formatted-rows [int]
+                   (qp/process-query query)))))))))
