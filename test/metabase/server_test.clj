6b1698483d91a9ffec83dbd46ca32525dd0f564c
Juhana <juahan.h@gmail.com>
2020-03-18 18:50:17 +0100

    Made changes to the exposed variable names and added the variables to the failed test.

--- test/metabase/server_test.clj
+++ test/metabase/server_test.clj
@@ -6,18 +6,20 @@
 
 ;; Make sure our Jetty config functions work as expected/we don't accidentally break things (#9333)
 (expect
-  {:keystore       "10"
-   :max-queued     10
-   :port           10
-   :min-threads    10
-   :host           "10"
-   :daemon?        false
-   :ssl?           true
-   :trust-password "10"
-   :key-password   "10"
-   :truststore     "10"
-   :max-threads    10
-   :max-idle-time  10
-   :ssl-port       10}
+  {:keystore              "10"
+   :max-queued            10
+   :port                  10
+   :min-threads           10
+   :host                  "10"
+   :daemon?               false
+   :ssl?                  true
+   :trust-password        "10"
+   :key-password          "10"
+   :truststore            "10"
+   :max-threads           10
+   :max-idle-time         10
+   :request-header-size   10
+   :response-header-size  10
+   :ssl-port              10}
   (with-redefs [config/config-str (constantly "10")]
     (#'server/jetty-config)))
