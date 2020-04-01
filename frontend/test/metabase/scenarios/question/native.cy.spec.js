1a1265c09019932a8738b7212054f69129d36b8b
Paul Rosenzweig <paulrosenzweig@users.noreply.github.com>
2020-04-01 19:52:27 -0400

    Fix query param date bug (#12239)
    * Add Cypress test for #12228 (#12235)
    
    * fix logic to check if we'll use fieldValueWidget while parsing query params
    
    Co-authored-by: Damon P. Cortesi <d.lifehacker@gmail.com>

--- frontend/test/metabase/scenarios/question/native.cy.spec.js
+++ frontend/test/metabase/scenarios/question/native.cy.spec.js
@@ -1,4 +1,9 @@
-import { signInAsNormalUser, restore, popover } from "__support__/cypress";
+import {
+  signInAsNormalUser,
+  restore,
+  popover,
+  modal,
+} from "__support__/cypress";
 
 describe("scenarios > question > native", () => {
   before(restore);
@@ -149,5 +154,40 @@ describe("scenarios > question > native", () => {
     // run query again and see new result
     cy.get(".NativeQueryEditor .Icon-play").click();
     cy.contains("18,760");
+  });
+
+  it("can load a question with a date filter (from issue metabase#12228)", () => {
+    cy.request("POST", "/api/card", {
+      name: "Test Question",
+      dataset_query: {
+        type: "native",
+        native: {
+          query: "select count(*) from orders where {{created_at}}",
+          "template-tags": {
+            created_at: {
+              id: "6b8b10ef-0104-1047-1e1b-2492d5954322",
+              name: "created_at",
+              "display-name": "Created at",
+              type: "dimension",
+              dimension: ["field-id", 15],
+              "widget-type": "date/month-year",
+            },
+          },
+        },
+        database: 1,
+      },
+      display: "scalar",
+      description: null,
+      visualization_settings: {},
+      collection_id: null,
+      result_metadata: null,
+      metadata_checksum: null,
+    }).then(response => {
+      cy.visit(`/question/${response.body.id}?created_at=2020-01`);
+      modal()
+        .contains("Okay")
+        .click();
+      cy.contains("580");
+    });
   });
 });
