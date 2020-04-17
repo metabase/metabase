212267e7db6e169610db98ece5a7d91b01cc0d82
Simon Belak <simon@metabase.com>
2020-04-17 22:02:55 +0200

    Make linter happy

--- src/metabase/driver/sql/query_processor.clj
+++ src/metabase/driver/sql/query_processor.clj
@@ -224,8 +224,8 @@
   (some-> *name-store* deref (get field-id)))
 
 (defn- store-alias
-  [field-id alias]
-  (some-> *name-store* (swap! assoc field-id alias)))
+  [field-id field-alias]
+  (some-> *name-store* (swap! assoc field-id field-alias)))
 
 (defmethod ->honeysql [:sql nil]    [_ _]    nil)
 (defmethod ->honeysql [:sql Object] [_ this] this)
