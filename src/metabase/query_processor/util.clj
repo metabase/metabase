8eac45a67f26daaccf9a60b312978e5397316cdd
Robert Roland <rob@metabase.com>
2020-04-27 15:57:57 -0700

    Allow a driver to customize the query remarks
    This allows driver-specific metadata to be added to each query. If the
    driver can apply the default metadata, it should call
    default-query->remark

--- src/metabase/query_processor/util.clj
+++ src/metabase/query_processor/util.clj
@@ -5,6 +5,7 @@
              [hash :as hash]]
             [cheshire.core :as json]
             [clojure.string :as str]
+            [metabase.driver :as driver]
             [metabase.util.schema :as su]
             [schema.core :as s]))
 
@@ -23,11 +24,8 @@
        (not page)
        (nil? aggregations)))
 
-(defn query->remark
-  "Generate an approparite REMARK to be prepended to a query to give DBAs additional information about the query being
-  executed. See documentation for `mbql->native` and [issue #2386](https://github.com/metabase/metabase/issues/2386)
-  for more information."
-  ^String [{{:keys [executed-by query-hash], :as info} :info, query-type :type}]
+(defn default-query->remark
+  [{{:keys [executed-by query-hash], :as info} :info, query-type :type}]
   (str "Metabase" (when executed-by
                     (assert (instance? (Class/forName "[B") query-hash))
                     (format ":: userID: %s queryType: %s queryHash: %s"
@@ -36,6 +34,18 @@
                               :query  "MBQL"
                               :native "native")
                             (codecs/bytes->hex query-hash)))))
+
+(defmulti query->remark
+  "Generate an appropriate REMARK to be prepended to a query to give DBAs additional information about the query being
+  executed. See documentation for `mbql->native` and [issue #2386](https://github.com/metabase/metabase/issues/2386)
+  for more information."
+  {:arglists '(^String [driver data])}
+  driver/dispatch-on-initialized-driver
+  :hierarchy #'driver/hierarchy)
+
+(defmethod query->remark :default
+  [_ params]
+  (default-query->remark params))
 
 
 ;;; ------------------------------------------------- Normalization --------------------------------------------------
