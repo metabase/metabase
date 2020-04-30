fdce7b7d9aaa05b1676e9a9a957013fae53c69a1
flamber <1447303+flamber@users.noreply.github.com>
2020-04-30 16:24:31 +0200

    Paul: Remove comment

--- frontend/src/metabase/visualizations/components/LeafletChoropleth.jsx
+++ frontend/src/metabase/visualizations/components/LeafletChoropleth.jsx
@@ -42,10 +42,6 @@ const LeafletChoropleth = ({
         keyboard: false,
       });
 
-      // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      //     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-      // }).addTo(map);
-
       const style = feature => ({
         fillColor: getColor(feature),
         weight: 1,
