dd0938601c27ebfb2928cf8f52bf4d5eb780793f
Cam Saul <1455846+camsaul@users.noreply.github.com>
2020-05-11 22:35:06 -0700

    Allow users to run ad-hoc query starting with Card if they have perms for it (#12505)
    * Allow users to run ad-hoc query starting with Card if they have perms for it
    
    * Convert metabase.query-processor.middleware.fetch-source-query-test to modern style
    
    * When resolving source query from card_id, use Card ID for perms
    
    * Add test to make sure we don't overwrite existing card-id
    
    * Add missing require
    
    * Test fix :wrench:
    
    * Sort namespaces
    
    * Tweak codecov.yaml

--- src/metabase/query_processor.clj
+++ src/metabase/query_processor.clj
@@ -1,12 +1,10 @@
 (ns metabase.query-processor
-  "Preprocessor that does simple transformations to all incoming queries, simplifing the driver-specific
-  implementations."
+  "The main entrypoint to running queries."
   (:require [clojure.tools.logging :as log]
             [metabase
              [config :as config]
              [driver :as driver]]
             [metabase.driver.util :as driver.u]
-            [metabase.mbql.schema :as mbql.s]
             [metabase.query-processor
              [context :as context]
              [error-type :as error-type]
@@ -219,18 +217,15 @@
          query
          args))
 
-(s/defn ^:private add-info [query info :- mbql.s/Info]
-  (update query :info merge info))
-
 (s/defn process-query-and-save-execution!
   "Process and run a 'userland' MBQL query (e.g. one ran as the result of an API call, scheduled Pulse, MetaBot query,
   etc.). Returns results in a format appropriate for consumption by FE client. Saves QueryExecution row in application
   DB."
   ([query info]
-   (process-userland-query (add-info query info)))
+   (process-userland-query (assoc query :info info)))
 
   ([query info context]
-   (process-userland-query (add-info query info) context)))
+   (process-userland-query (assoc query :info info) context)))
 
 (defn- add-default-constraints [query]
   (assoc-in query [:middleware :add-default-userland-constraints?] true))
