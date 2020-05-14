fb58e1a7f5ce14053cfe2ac847c687b94bbf242d
Cam Saul <github@camsaul.com>
2020-05-13 21:36:21 -0700

    Fix output formatting

--- test/metabase/junit.clj
+++ test/metabase/junit.clj
@@ -5,32 +5,43 @@
              [test :as t]]
             [pjstadig.print :as p]))
 
-(defn format-result
-  [{:keys [type expected actual diffs message] :as event}]
+(defn- result-output [{:keys [type expected actual diffs message] :as event}]
+  (with-out-str
+    (println "\nFAIL in" (t/testing-vars-str event))
+    (when (seq t/*testing-contexts*)
+      (println (t/testing-contexts-str)))
+    (when message (println message))
+    (p/with-pretty-writer (fn []
+                            (let [print-expected (fn [actual]
+                                                   (p/rprint "expected: ")
+                                                   (pp/pprint expected *out*)
+                                                   (p/rprint "  actual: ")
+                                                   (pp/pprint actual *out*)
+                                                   (p/clear))]
+                              (if (seq diffs)
+                                (doseq [[actual [a b]] diffs]
+                                  (print-expected actual)
+                                  (p/rprint "    diff:")
+                                  (if a
+                                    (do (p/rprint " - ")
+                                        (pp/pprint a *out*)
+                                        (p/rprint "          + "))
+                                    (p/rprint " + "))
+                                  (when b
+                                    (pp/pprint b *out*))
+                                  (p/clear))
+                                (print-expected actual)))))))
+
+(defmulti format-result
+  {:arglists '([event])}
+  :type)
+
+(defmethod format-result :default
+  [event]
+  ((requiring-resolve 'test-report-junit-xml.core/format-result) event))
+
+(defmethod format-result :fail
+  [event]
   {:tag     :failure
    :attrs   {:message (t/testing-vars-str event)}
-   :content (with-out-str
-              (println "\nFAIL in" (t/testing-vars-str event))
-              (when (seq t/*testing-contexts*)
-                (println (t/testing-contexts-str)))
-              (when message (println message))
-              (p/with-pretty-writer (fn []
-                                      (let [print-expected (fn [actual]
-                                                             (p/rprint "expected: ")
-                                                             (pp/pprint expected *out*)
-                                                             (p/rprint "  actual: ")
-                                                             (pp/pprint actual *out*)
-                                                             (p/clear))]
-                                        (if (seq diffs)
-                                          (doseq [[actual [a b]] diffs]
-                                            (print-expected actual)
-                                            (p/rprint "    diff:")
-                                            (if a
-                                              (do (p/rprint " - ")
-                                                  (pp/pprint a *out*)
-                                                  (p/rprint "          + "))
-                                              (p/rprint " + "))
-                                            (when b
-                                              (pp/pprint b *out*))
-                                            (p/clear))
-                                          (print-expected actual))))))})
+   :content (result-output event)})
