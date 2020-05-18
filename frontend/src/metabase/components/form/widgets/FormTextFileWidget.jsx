adbd3a1d7f5c1edcd7bc6b5b615607b22ff358dd
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2020-05-18 15:58:20 -0400

    fixes and code cleanup

--- frontend/src/metabase/components/form/widgets/FormTextFileWidget.jsx
+++ frontend/src/metabase/components/form/widgets/FormTextFileWidget.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-// import { formDomOnlyProps } from "metabase/lib/redux";
+import { formDomOnlyProps } from "metabase/lib/redux";
 
-const FormTextFileWidget = ({ field }) => (
-  <input
-    type="file"
-    className="Form-input"
-    aria-labelledby={`${field.name}-label`}
-    onChange={async e => field.onChange(await e.target.files[0].text())}
-    // {...formDomOnlyProps(field)}
-  />
-);
+const FormTextFileWidget = ({ field }) => {
+  const otherProps = formDomOnlyProps(field);
+  delete otherProps.value; // setting value on a file input throws an error
+  return (
+    <input
+      type="file"
+      className="Form-input"
+      aria-labelledby={`${field.name}-label`}
+      {...otherProps}
+      onChange={wrapHandler(field.onChange)}
+      onBlur={wrapHandler(field.onBlur)}
+    />
+  );
+};
+
+function wrapHandler(h) {
+  return async ({ target: { files } }) =>
+    h(files.length === 0 ? "" : await files[0].text());
+}
 
 export default FormTextFileWidget;
