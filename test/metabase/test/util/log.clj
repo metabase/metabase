ff09f933d78c340482b85cc4e67eee29fd81edcf
Cam Saul <github@camsaul.com>
2020-05-19 20:25:59 -0700

    Less noisy tests!

--- test/metabase/test/util/log.clj
+++ test/metabase/test/util/log.clj
@@ -11,28 +11,33 @@
     (into {} (for [^Logger logger loggers]
                [logger (.getLevel logger)]))))
 
+(def ^:private ^:dynamic *suppressed* false)
+
 (defn do-with-suppressed-output
   "Impl for `suppress-output` macro; don't use this directly."
   [f]
-  ;; yes, swapping out *out*/*err*, swapping out System.out/System.err, and setting all the log levels to OFF is
-  ;; really necessary to suppress everyting (!)
-  (let [orig-out (System/out)
-        orig-err (System/err)]
-    (with-open [null-stream (PrintStream. (NullOutputStream.))
-                null-writer (NullWriter.)]
-      (try
-        (System/setOut null-stream)
-        (System/setErr null-stream)
-        (doseq [[^Logger logger] logger->original-level]
-          (.setLevel logger Level/OFF))
-        (binding [*out* null-writer
-                  *err* null-writer]
-          (f))
-        (finally
-          (System/setOut orig-out)
-          (System/setErr orig-err)
-          (doseq [[^Logger logger, ^Level old-level] logger->original-level]
-            (.setLevel logger old-level)))))))
+  (if *suppressed*
+    (f)
+    (binding [*suppressed* true]
+      ;; yes, swapping out *out*/*err*, swapping out System.out/System.err, and setting all the log levels to OFF is
+      ;; really necessary to suppress everyting (!)
+      (let [orig-out (System/out)
+            orig-err (System/err)]
+        (with-open [null-stream (PrintStream. (NullOutputStream.))
+                    null-writer (NullWriter.)]
+          (try
+            (System/setOut null-stream)
+            (System/setErr null-stream)
+            (doseq [[^Logger logger] logger->original-level]
+              (.setLevel logger Level/OFF))
+            (binding [*out* null-writer
+                      *err* null-writer]
+              (f))
+            (finally
+              (System/setOut orig-out)
+              (System/setErr orig-err)
+              (doseq [[^Logger logger, ^Level old-level] logger->original-level]
+                (.setLevel logger old-level)))))))))
 
 (defmacro suppress-output
   "Execute `body` with all logging/`*out*`/`*err*` messages suppressed. Useful for avoiding cluttering up test output
