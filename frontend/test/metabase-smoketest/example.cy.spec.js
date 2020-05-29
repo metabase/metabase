826dae7973cdd8dc7f7233aaf6068ac0e710258c
Damon P. Cortesi <d.lifehacker@gmail.com>
2020-05-29 09:49:41 -0700

    yarn so pretty

--- frontend/test/metabase-smoketest/example.cy.spec.js
+++ frontend/test/metabase-smoketest/example.cy.spec.js
@@ -1,14 +1,14 @@
 import { restore } from "__support__/cypress";
 
 describe("scenarios > happypath > core usage", () => {
-    // Much of this setup is already done in setup.cy.spec.js
-    // So we egregiously borrow from there, but assume the person did everything right.
-    describe("admin setup", () => {
-      before(() => restore("blank"));
-  
-      it("signs up as a brand new Metabase person!", () => {
-        cy.visit("/");
-        cy.url().should("be", "/setup");
-      })
-    })
-})
+  // Much of this setup is already done in setup.cy.spec.js
+  // So we egregiously borrow from there, but assume the person did everything right.
+  describe("admin setup", () => {
+    before(() => restore("blank"));
+
+    it("signs up as a brand new Metabase person!", () => {
+      cy.visit("/");
+      cy.url().should("be", "/setup");
+    });
+  });
+});
