b257b705d74b78c80593065bf66cb2ac7d2c6194
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-06-03 15:20:21 -0700

    user > creates custom q

--- frontend/test/metabase-smoketest/user.cy.spec.js
+++ frontend/test/metabase-smoketest/user.cy.spec.js
@@ -7,11 +7,40 @@ describe("smoketest > new_user", () => {
     it("should be able to do header actions", () => {
 
         // =================
+        // should be able to ask a custom question
+        // =================
+        cy.visit("/");
+
+        cy.findByText("Ask a question").click();
+        cy.contains("Simple question");
+
+        cy.findByText("Custom question").click();
+        cy.findByText("Sample Dataset").click();
+        cy.findByText("Products").click();
+        cy.findByText("Add filters to narrow your answer").click();
+        cy.findByText("Vendor").click();
+        cy.findByText("Is").click();
+        cy.findByText("Not empty").click();
+        cy.findByText("Add filter").click();
+        cy.findByText("Pick the metric you want to see").click();
+        cy.findByText("Average of ...").click();
+        cy.findByText("Rating").click();
+        cy.findByText("Pick a column to group by").click();
+        cy.findByText("Title").click();
+
+        cy.contains("Average of Rating");
+
+        cy.findByText("Visualize").click();
+        
+        cy.contains("Vendor is not empty");
+        cy.contains("Visualization");
+        
+        // =================
         // should ensuring that header actions are appropriate for different data types
         // =================
-
-
-
+        
+        cy.contains("Filter").click();
+        
         // =================
         // should filter via both the sidebar and the header
         // =================
