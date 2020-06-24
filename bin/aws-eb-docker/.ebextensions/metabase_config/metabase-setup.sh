fc0f4cdeac806aada04f8a9303893aab96b02b5d
Tim McEwan <tim@mcewan.it>
2020-06-25 09:48:23 +1000

    Add timeouts for both configurations
    We don't use `NGINX_FORCE_SSL` as we're in a VPC.

--- bin/aws-eb-docker/.ebextensions/metabase_config/metabase-setup.sh
+++ bin/aws-eb-docker/.ebextensions/metabase_config/metabase-setup.sh
@@ -127,6 +127,10 @@ server {
         proxy_set_header    Host                $host;
         proxy_set_header    X-Real-IP            $remote_addr;
         proxy_set_header    X-Forwarded-For        $proxy_add_x_forwarded_for;
+        proxy_connect_timeout 600;
+        proxy_send_timeout 600;
+        proxy_read_timeout 600;
+        send_timeout 600;
     }
 }
 EOF
