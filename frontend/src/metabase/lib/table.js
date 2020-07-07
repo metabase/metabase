83324a9e3bb61817732db98a783716f48724b361
Tom Robinson <tlrobinson@gmail.com>
2020-07-07 15:54:57 -0700

    Remove loadTableAndForeignKeys

--- frontend/src/metabase/lib/table.js
+++ frontend/src/metabase/lib/table.js
@@ -1,29 +1,5 @@
 import { addValidOperatorsToFields } from "metabase/lib/schema_metadata";
 
-import _ from "underscore";
-
-import { MetabaseApi } from "metabase/services";
-
-export async function loadTableAndForeignKeys(tableId) {
-  const [table, foreignKeys] = await Promise.all([
-    MetabaseApi.table_query_metadata({ tableId }),
-    MetabaseApi.table_fks({ tableId }),
-  ]);
-
-  await augmentTable(table);
-
-  return {
-    table,
-    foreignKeys,
-  };
-}
-
-export async function augmentTable(table) {
-  table = populateQueryOptions(table);
-  table = await loadForeignKeyTables(table);
-  return table;
-}
-
 export function augmentDatabase(database) {
   database.tables_lookup = createLookupByProperty(database.tables, "id");
   for (const table of database.tables) {
@@ -38,37 +14,6 @@ export function augmentDatabase(database) {
     }
   }
   return database;
-}
-
-async function loadForeignKeyTables(table) {
-  // Load joinable tables
-  await Promise.all(
-    table.fields
-      .filter(f => f.target != null)
-      .map(async field => {
-        const targetTable = await MetabaseApi.table_query_metadata({
-          tableId: field.target.table_id,
-        });
-        field.target.table = populateQueryOptions(targetTable);
-      }),
-  );
-  return table;
-}
-
-function populateQueryOptions(table) {
-  table = addValidOperatorsToFields(table);
-
-  table.fields_lookup = {};
-
-  _.each(table.fields, function(field) {
-    table.fields_lookup[field.id] = field;
-    field.filter_operators_lookup = {};
-    _.each(field.filter_operators, function(operator) {
-      field.filter_operators_lookup[operator.name] = operator;
-    });
-  });
-
-  return table;
 }
 
 function addFkTargets(field, tables) {
