c0b23794335d976c29459a937fd8539076769197
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-07-14 14:30:08 -0700

    Text box should work

---
+++ frontend/test/metabase/scenarios/dashboard/text-box.cy.spec.js
@@ -0,0 +1,43 @@
+import { signInAsAdmin, restore, popover, modal } from "__support__/cypress";
+
+describe("scenarios > dashboard > text-box", () => {
+  before(restore);
+  beforeEach(signInAsAdmin);
+
+  it("should load text box card", () => {
+    // Create dashboard
+    cy.server();
+    cy.request("POST", "/api/dashboard", {
+      name: "Test Dashboard",
+    });
+    cy.visit(`/dashboard/2`);
+
+    cy.findByText("Test Dashboard");
+    cy.findByText("This dashboard is looking empty.");
+
+    // Add text box to dash
+    cy.get(".Icon-pencil").click();
+    cy.get(".Icon-string").click();
+    cy.findByPlaceholderText("Write here, and use Markdown if you'd like").type(
+      "Dashboard testing text",
+    );
+    cy.findByText("Save").click();
+
+    cy.findByText("Saving…");
+    cy.findByText("Saving…").should("not.exist");
+
+    // Reload page
+    cy.reload();
+
+    // Page should still load
+    cy.findByText("Ask a question");
+    cy.findByText("Loading...").should("not.exist");
+    cy.findByText("Cannot read property 'type' of undefined").should(
+      "not.exist",
+    );
+    cy.findByText("Test Dashboard");
+
+    // Text box should still load
+    cy.findByText("Dashboard testing text");
+  });
+});
