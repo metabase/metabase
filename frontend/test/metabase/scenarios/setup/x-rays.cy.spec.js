152f079b64dd361df05c9fbeacf4a3c32e74fea7
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-07-16 13:28:36 -0700

    x-ray load test

--- frontend/test/metabase/scenarios/setup/x-rays.cy.spec.js
+++ frontend/test/metabase/scenarios/setup/x-rays.cy.spec.js
@@ -1,15 +1,20 @@
 import { restore, signInAsAdmin } from "../../../__support__/cypress";
 
 describe("scenarios > x-rays", () => {
-    before(restore);
-    beforeEach(signInAsAdmin);
-  
-    it("should exist when person first signs in", () => {
-        cy.visit("/");
-        cy.pause();
-    });
+  before(restore);
+  beforeEach(signInAsAdmin);
 
-    it("should exist after text-box is added", () => {
+  it("should exist on homepage when person first signs in", () => {
+    cy.visit("/");
+    cy.contains("A look at your People table");
+    cy.contains("A look at your Orders table");
+    cy.contains("A look at your Products table");
+    cy.contains("A look at your Reviews table");
+  });
 
-    });
+  it.skip("should be populated (Issue #12917)", () => {
+    cy.visit("/");
+    cy.findByText("People table").click();
+    cy.findByText("Something's gone wrong").should("not.exist");
+  });
 });
