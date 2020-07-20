99ce28224f32d0826bd0da1bec11b4b9654fa334
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-07-20 14:25:44 -0700

    repro of issue

--- frontend/test/metabase/scenarios/admin/settings/spinner.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/spinner.cy.spec.js
@@ -1,8 +1,20 @@
+import { restore, signInAsAdmin } from "__support__/cypress";
+
 describe("scenarios > admin > spinner", () => {
-    it("should not spin forever when a save fails", () => {
+  before(restore);
+  beforeEach(signInAsAdmin);
 
+  describe("API request", () => {
+    it("should return correct DB", () => {
+      cy.visit("/admin/databases/1");
+      cy.findByText("Sample Dataset");
+      cy.findByText("Add Database").should("not.exist");
     });
-    it("should not spin forever if API request returns an error", () => {
 
+    it.skip("should not spin forever if it returns an error (Issue #11037)", () => {
+      cy.visit("/admin/databases/999");
+      cy.findAllByText("Databases").should("have.length", 2);
+      cy.findByText("Loading...").should("not.exist");
     });
+  });
 });
