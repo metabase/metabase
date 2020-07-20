8f143f5dc38447a5fb2bc560fc8adbf1dba5801d
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-07-20 13:47:24 -0700

    outline for repro

---
+++ frontend/test/metabase/scenarios/admin/settings/spinner.cy.spec.js
@@ -0,0 +1,8 @@
+describe("scenarios > admin > spinner", () => {
+    it("should not spin forever when a save fails", () => {
+
+    });
+    it("should not spin forever if API request returns an error", () => {
+
+    });
+});
