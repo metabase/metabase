253677a9f1e219d0e460621d22a3d512ff5885a7
Jessica DeWitt <58329466+Opalevanescence@users.noreply.github.com>
2020-07-22 16:19:42 -0700

    Deleted change of table permissions

--- frontend/test/metabase/scenarios/auth/search.cy.spec.js
+++ frontend/test/metabase/scenarios/auth/search.cy.spec.js
@@ -24,19 +24,6 @@ describe("scenarios > auth > search", () => {
     });
 
     it("should not work for user without permissions", () => {
-      // Take away table permissions
-      signInAsAdmin();
-      cy.visit("/admin/permissions/collections");
-      cy.get(".Icon-check")
-        .last()
-        .click();
-      cy.findByText("Revoke access").click();
-      cy.findByText("Save Changes").click();
-
-      cy.findByText("Are you sure you want to do this?");
-      cy.findByText("Yes").click();
-
-      // Check
       signIn("nodata");
       cy.visit("/");
       cy.findByPlaceholderText("Search…").type("product{enter}");
