98d2cc0211cea6154834129e91c1b9b2c55fa625
Jessica DeWitt <58329466+Opalevanescence@users.noreply.github.com>
2020-07-29 16:45:58 -0700

    Update segments.cy.spec.js

--- frontend/test/metabase/scenarios/admin/datamodel/segments.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/datamodel/segments.cy.spec.js
@@ -1,4 +1,4 @@
-import { restore, signInAsAdmin, popover, modal } from "__support__/cypress";
+import { restore, signInAsAdmin, popover, modal, sidebar } from "__support__/cypress";
 // Ported from `segments.e2e.spec.js`
 
 describe("scenarios > admin > datamodel > segments", () => {
@@ -105,7 +105,9 @@ describe("scenarios > admin > datamodel > segments", () => {
       cy.visit("/reference/segments/1/questions");
       cy.get(".full .Button").click();
       cy.findByText("Filter").click();
-      cy.findByText("Product ID").click();
+      sidebar().within(() => {
+        cy.findByText("Product ID").click();
+      });
       cy.findByPlaceholderText("Enter an ID").type("14");
       cy.findByText("Add filter").click();
       cy.findByText("Product ID is 14");
