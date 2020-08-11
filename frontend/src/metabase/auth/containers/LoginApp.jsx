0a0d383049830b52f4e1375360e1e00b029f4314
Kyle Doherty <kyle.l.doherty@gmail.com>
2020-08-11 11:16:36 -0400

    add new illustration and update /login layout

--- frontend/src/metabase/auth/containers/LoginApp.jsx
+++ frontend/src/metabase/auth/containers/LoginApp.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Link } from "react-router";
 import { connect } from "react-redux";
 
+import { Flex } from "grid-styled"
+import Card from "metabase/components/Card"
+
 import { t } from "ttag";
 import _ from "underscore";
 import AuthScene from "../components/AuthScene";
@@ -18,41 +21,36 @@ export default class LoginApp extends Component {
   renderPanel(provider) {
     return <provider.Panel {...this.props} />;
   }
-  renderButton(provider) {
-    return (
-      <Link
-        key={provider.name}
-        to={provider.Panel ? `/auth/login/${provider.name}` : null}
-        className="mt2 block"
-      >
-        <provider.Button {...this.props} />
-      </Link>
-    );
-  }
   render() {
     const { providers, params } = this.props;
     const selected = _.findWhere(providers, { name: params.provider });
     const visibleProviders = selected ? [selected] : providers;
 
     return (
-      <div className="flex flex-column flex-full md-layout-centered">
-        <div className="Login-wrapper wrapper Grid Grid--full md-Grid--1of2 relative z2">
-          <div className="Grid-cell flex layout-centered text-brand">
-            <LogoIcon className="Logo my4 sm-my0" height={65} />
-          </div>
-          <div className="Login-content Grid-cell p4 bg-white bordered rounded shadowed">
-            <h2 className="Login-header mb2">{t`Sign in to Metabase`}</h2>
+      <Flex flexDirection="column" flex={1} justifyContent='center' alignItems="center" className="overflow-hidden relative">
+        <Flex mt={-4} flexDirection="column">
+          <LogoIcon height={65} />
+          <Card p={3} mt={3} className="relative z2" w={420}>
+            <h2 className="text-centered mb3">{t`Sign in to Metabase`}</h2>
             {visibleProviders.length === 1 && visibleProviders[0].Panel ? (
               this.renderPanel(visibleProviders[0])
             ) : (
               <div className="pt2 relative">
-                {visibleProviders.map(provider => this.renderButton(provider))}
+                {visibleProviders.map(provider =>
+                  <Link
+                    key={provider.name}
+                    to={provider.Panel ? `/auth/login/${provider.name}` : null}
+                    className="mt2 block"
+                  >
+                    <provider.Button {...this.props} />
+                  </Link>
+                )}
               </div>
             )}
-          </div>
-        </div>
+          </Card>
+        </Flex>
         <AuthScene />
-      </div>
+      </Flex>
     );
   }
 }
