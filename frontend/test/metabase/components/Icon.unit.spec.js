5b4313d0aec8424b1cf706a4b48acaf42ec99a23
Nemanja Glumac <git@glumac.anonaddy.me>
2020-09-11 04:32:30 +0200

    Expand `Icon` unit tests

--- frontend/test/metabase/components/Icon.unit.spec.js
+++ frontend/test/metabase/components/Icon.unit.spec.js
@@ -1,4 +1,9 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+
 import { ICON_PATHS, loadIcon, parseViewBox } from "metabase/icon_paths";
+import Icon from "metabase/components/Icon";
 
 // find the first icon with a non standard viewBox
 const NON_STANDARD_VIEWBOX_ICON = Object.keys(ICON_PATHS).filter(key => {
@@ -25,6 +30,35 @@ describe("Icon", () => {
 
       expect(width).toEqual(parsedWidth / 2);
       expect(height).toEqual(parsedHeight / 2);
+    });
+  });
+
+  describe("component", () => {
+    it("should render correct icon for the valid `name`", () => {
+      // used "google" to cover the use of `dangerouslySetInnerHTML`
+      render(<Icon name="google" />);
+      const icon = screen.getByRole("img", { name: /google icon/i });
+
+      expect(icon).toHaveClass("Icon-google");
+    });
+
+    it("should render `unknown` icon given non-existing `name`", () => {
+      render(<Icon name="404-icon-not-found" />);
+      const icon = screen.getByRole("img", { name: /unknown icon/i });
+
+      expect(icon).toHaveClass("Icon-unknown");
+    });
+
+    it("should render `unknown` icon when `name` is not provided", () => {
+      render(<Icon />);
+      const icon = screen.getByRole("img", { name: /unknown icon/i });
+
+      expect(icon).toHaveClass("Icon-unknown");
+    });
+
+    it("should render an image if `img` attribute is present in `ICON_PATHS`", () => {
+      render(<Icon name="slack" />);
+      expect(screen.getByRole("img")).toHaveAttribute("src");
     });
   });
 });
