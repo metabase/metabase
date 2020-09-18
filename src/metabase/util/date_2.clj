aa7ec96644ae7678a947b88f0ac8f9b425c8d736
Simon Belak <simon@metabase.com>
2020-09-18 17:51:20 +0200

    Punt on adjust for now

--- src/metabase/util/date_2.clj
+++ src/metabase/util/date_2.clj
@@ -144,7 +144,7 @@
    (t/as t (case unit
              :minute-of-hour   :minute-of-hour
              :hour-of-day      :hour-of-day
-             :day-of-week      (+ (.dayOfWeek (week-fields :sunday-start)) (driver.common/start-of-week-offset))
+             :day-of-week      (.dayOfWeek (week-fields :sunday-start))
              :iso-day-of-week  (.dayOfWeek (week-fields :iso))
              :day-of-month     :day-of-month
              :day-of-year      :day-of-year
