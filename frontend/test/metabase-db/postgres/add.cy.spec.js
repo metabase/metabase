48f3b041ba9c003923d5a078e80df4e82f8ad8bc
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2020-09-25 02:00:00 +0200

    Add repro for #13263 (#13303)

--- frontend/test/metabase-db/postgres/add.cy.spec.js
+++ frontend/test/metabase-db/postgres/add.cy.spec.js
@@ -53,4 +53,44 @@ describe("postgres > admin > add", () => {
       .contains("I'm good thanks")
       .click();
   });
+
+  it.skip("should show row details when clicked on its entity key (metabase#13263)", () => {
+    cy.route({
+      method: "POST",
+      url: "/api/database",
+    }).as("createDatabase");
+
+    addPostgresDatabase();
+
+    cy.wait("@createDatabase");
+
+    cy.url().should("match", /\/admin\/databases\?created=\d+$/);
+    cy.contains("Your database has been added!");
+    modal()
+      .contains("I'm good thanks")
+      .click();
+
+    // Repro starts here
+    cy.visit("/");
+    cy.findByText("Ask a question").click();
+    cy.findByText("Simple question").click();
+    cy.findByText("QA Postgres12").click();
+    cy.findByText("Orders").click();
+
+    // We're clicking on ID: 1 (the first order) => do not change!
+    // It is tightly coupled to the assertion ("37.65"), which is "Subtotal" value for that order.
+    cy.get(".Table-ID")
+      .eq(0)
+      .click();
+
+    // Wait until "doing science" spinner disappears (DOM is ready for assertions)
+    // TODO: if this proves to be reliable, extract it as a helper function for waiting on DOM to render
+    cy.get(".LoadingSpinner").should("not.exist");
+
+    // Assertions
+    cy.log("**Fails in v0.36.6**");
+    // This could be omitted because real test is searching for "37.65" on the page
+    cy.findByText("There was a problem with your question").should("not.exist");
+    cy.contains("37.65");
+  });
 });
