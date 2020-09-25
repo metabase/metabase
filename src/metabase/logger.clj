0a7db81fc8b89c2b33de1eb802e2a6a5ac38adeb
Robert Roland <rob@metabase.com>
2020-09-24 17:09:44 -0700

    fixing reflection warnings

--- src/metabase/logger.clj
+++ src/metabase/logger.clj
@@ -4,9 +4,10 @@
              [coerce :as coerce]
              [format :as time]]
             [metabase.config :refer [local-process-uuid]])
-  (:import [org.apache.logging.log4j.core Appender LogEvent]
-           [org.apache.logging.log4j.core.config AppenderRef LoggerConfig]
-           org.apache.logging.log4j.LogManager))
+  (:import org.apache.commons.lang3.exception.ExceptionUtils
+           [org.apache.logging.log4j Level LogManager]
+           [org.apache.logging.log4j.core Appender Filter LogEvent LoggerContext]
+           [org.apache.logging.log4j.core.config AppenderRef LoggerConfig Property]))
 
 (def ^:private ^:const max-log-entries 2500)
 
@@ -19,11 +20,12 @@
 
 (defn- event->log-data [^LogEvent event]
   {:timestamp    (time/unparse (time/formatter :date-time)
-                               (coerce/from-long (.getTimeStamp event)))
+                               (coerce/from-long (.getTimeMillis event)))
    :level        (.getLevel event)
    :fqns         (.getLoggerName event)
    :msg          (.getMessage event)
-   :exception    (.getThrowableStrRep event)
+   :exception    (when-let [throwable (.getThrown event)]
+                   (ExceptionUtils/getStackFrames throwable))
    :process_uuid local-process-uuid})
 
 (defn- metabase-appender ^Appender []
@@ -41,18 +43,21 @@
   (when-not @has-added-appender?
     (reset! has-added-appender? true)
     ;; this is painful. see: https://logging.apache.org/log4j/2.x/manual/customconfig.html#AddingToCurrent
-    (let [ctx          (LogManager/getContext false)
-          config       (.getConfiguration ctx)
-          appender     (metabase-appender)
-          appender-ref (AppenderRef/createAppenderRef "metabase" nil nil)
-          logger-cfg   (LoggerConfig/createLogger true
-                                                  nil
-                                                  "metabase"
-                                                  "true"
-                                                  (into-array AppenderRef [appender-ref])
-                                                  nil
-                                                  config
-                                                  nil)]
+    (let [^LoggerContext ctx       (LogManager/getContext false)
+          config                   (.getConfiguration ctx)
+          appender                 (metabase-appender)
+          appender-ref             (AppenderRef/createAppenderRef "metabase" nil nil)
+          ^Filter filter           nil ; here because of a reflection warning
+          ^LoggerConfig logger-cfg (LoggerConfig/createLogger true
+                                                              Level/ALL
+                                                              "metabase"
+                                                              "true"
+                                                              ^"[Lorg.apache.logging.log4j.core.config.AppenderRef;"
+                                                              (into-array AppenderRef [appender-ref])
+                                                              ^"[Lorg.apache.logging.log4j.core.config.Property;"
+                                                              (into-array Property nil)
+                                                              config
+                                                              filter)]
       (.addAppender config appender)
       (.addAppender logger-cfg appender nil nil)
       (.updateLoggers ctx))))
