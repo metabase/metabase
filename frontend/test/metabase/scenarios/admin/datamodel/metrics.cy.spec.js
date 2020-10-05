4d95d0f00ad388eba4973991b50b32dee1986de2
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2020-10-05 18:08:18 +0200

    Add repro for #13069 (#13363)
    Tests are in stored in two separate files:
    - frontend/test/metabase/scenarios/admin/datamodel/segments.cy.spec.js and
    - frontend/test/metabase/scenarios/admin/datamodel/metrics.cy.spec.js

--- frontend/test/metabase/scenarios/admin/datamodel/metrics.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/datamodel/metrics.cy.spec.js
@@ -15,6 +15,21 @@ describe("scenarios > admin > datamodel > metrics", () => {
       );
     });
 
+    it.skip("should have 'Custom expression' in a filter list (metabase#13069)", () => {
+      cy.visit("/admin/datamodel/metrics");
+      cy.findByText("New metric").click();
+      cy.findByText("Select a table").click();
+      popover().within(() => {
+        cy.findByText("Orders").click();
+      });
+      cy.findByText("Add filters to narrow your answer").click();
+
+      cy.log("**Fails in v0.36.0 and v0.36.3. It exists in v0.35.4**");
+      popover().within(() => {
+        cy.findByText("Custom Expression");
+      });
+    });
+
     it("should show how to create metrics", () => {
       cy.visit("/reference/metrics");
       cy.findByText(
