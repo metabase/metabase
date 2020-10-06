67bd25dd03666fe4906bda4bea326b4116ec1ddc
Robert Roland <rob@metabase.com>
2020-10-06 10:02:20 -0700

    Dashboard subscriptions work, squashed
    This test namespace differs in EE and OSS
    
    Without other changes from EE, this won't work
    
    so, for now, I've restored the pure OSS version of this test
    
    redo wording on existing subscription cards
    
    hacks? to address some timing issues where the render loop executes
    before properties are available (loaded from the server)
    
    links to configure pulse types
    
    Display a link to configure email or slack when they aren't already
    
    Fix the am/pm picker
    
    set up a new schedule tooltip
    
    The sidebar can appear and disappear from the Sharing popover
    
    run prettier on everything
    
    email recipients now does the "name [and n other]" thing
    
    Dashboard subscriptions work, squashed
    
    redo wording on existing subscription cards
    
    hacks? to address some timing issues where the render loop executes
    before properties are available (loaded from the server)
    
    styling for email not configured state
    
    add styling for slack unconfigured state
    
    disable cursor-pointer if email or slack isn't configured
    
    Implement cancel button for the sidebar (#13741)
    
    Dashboard subscription UX enhancements:
    
    * Implement cancel button for the sidebar
    
    * Don't return duplicate dashboard subscriptions
    
    * Move sidebar cancellation into Redux
    
    * Add close icon
    
    * Fix popover behavior
    
    Dashboard Subscriptions: Fix build (#13859)
    
    * Frontend lint fixes
    
    * Fix failing email tests
    
    * Revert some of the snapshot changes
    
    * regenerate snapshots
    
    * done fixing FE tests?
    
    * Make `dashboard_card_id` optional in CardRef schema
    
    * Fix test setup in pulse model test
    
    * Fix collection API test
    
    * Order pulses by name
    
    * Cypress fixes/skips
    
    Co-authored-by: Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
    
    Subscribe to dashboards send test email (#13886)
    
    * Frontend lint fixes
    
    * Fix failing email tests
    
    * Revert some of the snapshot changes
    
    * regenerate snapshots
    
    * done fixing FE tests?
    
    * Make `dashboard_card_id` optional in CardRef schema
    
    * Fix test setup in pulse model test
    
    * Fix collection API test
    
    * Order pulses by name
    
    * Cypress fixes/skips
    
    * uggh cypress
    
    * Add button to send test email for dashboard subscriptions
    
    * Disable test email with no recipients
    
    * Refactor SendTestEmail
    
    Co-authored-by: Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
    
    Fix failing collection tests (#13891)
    
    * Move dashboard embedding button (#13928)

--- frontend/src/metabase/dashboard/components/DashboardHeader.jsx
+++ frontend/src/metabase/dashboard/components/DashboardHeader.jsx
@@ -10,15 +10,12 @@ import Header from "metabase/components/Header";
 import Icon from "metabase/components/Icon";
 import ModalWithTrigger from "metabase/components/ModalWithTrigger";
 import Tooltip from "metabase/components/Tooltip";
-import DashboardEmbedWidget from "../containers/DashboardEmbedWidget";
 
 import { getDashboardActions } from "./DashboardActions";
 
 import ParametersPopover from "./ParametersPopover";
 import Popover from "metabase/components/Popover";
 import PopoverWithTrigger from "metabase/components/PopoverWithTrigger";
-
-import MetabaseSettings from "metabase/lib/settings";
 
 import cx from "classnames";
 
@@ -71,6 +68,9 @@ type Props = {
   onFullscreenChange: boolean => void,
 
   onChangeLocation: string => void,
+
+  onSharingClick: void => void,
+  onEmbeddingClick: void => void,
 };
 
 type State = {
@@ -104,6 +104,9 @@ export default class DashboardHeader extends Component {
     onRefreshPeriodChange: PropTypes.func.isRequired,
     onNightModeChange: PropTypes.func.isRequired,
     onFullscreenChange: PropTypes.func.isRequired,
+
+    onSharingClick: PropTypes.func.isRequired,
+    onEmbeddingClick: PropTypes.func.isRequred,
   };
 
   handleEdit(dashboard: DashboardWithCards) {
@@ -181,13 +184,9 @@ export default class DashboardHeader extends Component {
       isEditing,
       isFullscreen,
       isEditable,
-      isAdmin,
       location,
     } = this.props;
     const canEdit = dashboard.can_write && isEditable && !!dashboard;
-
-    const isPublicLinksEnabled = MetabaseSettings.get("enable-public-sharing");
-    const isEmbeddingEnabled = MetabaseSettings.get("enable-embedding");
 
     const buttons = [];
     const extraButtons = [];
@@ -341,18 +340,7 @@ export default class DashboardHeader extends Component {
       );
     }
 
-    buttons.push(...getDashboardActions(this.props));
-
-    if (
-      !isEditing &&
-      !isFullscreen &&
-      ((isPublicLinksEnabled && (isAdmin || dashboard.public_uuid)) ||
-        (isEmbeddingEnabled && isAdmin))
-    ) {
-      buttons.push(
-        <DashboardEmbedWidget key="dashboard-embed" dashboard={dashboard} />,
-      );
-    }
+    buttons.push(...getDashboardActions(this, this.props));
 
     if (extraButtons.length > 0 && !isEditing) {
       buttons.push(
