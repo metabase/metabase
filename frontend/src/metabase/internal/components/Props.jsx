7f7bf8f72e973a21c6d59c3602417095584fbaf3
Kyle Doherty <kyle.l.doherty@gmail.com>
2020-10-07 14:43:31 -0400

    [styleguide] - display props as table

--- frontend/src/metabase/internal/components/Props.jsx
+++ frontend/src/metabase/internal/components/Props.jsx
@@ -3,21 +3,28 @@ import React from "react";
 const Props = ({ of }) => {
   const component = of;
   return (
-    <div>
-      <div className="">
+    <table className="Table">
+      <thead>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Default</th>
+        <th>Description</th>
+      </thead>
+      <tbody>
         {Object.keys(component.propTypes).map(prop => (
-          <div>
-            {prop}{" "}
-            {component.defaultProps &&
-            component.defaultProps[prop] !== undefined
-              ? "(default: " +
-                JSON.stringify(component.defaultProps[prop]) +
-                ")"
-              : ""}
-          </div>
+          <tr>
+            <td>{prop}</td>
+            <td>TYPE GOES HERE</td>
+            <td>
+              {component.defaultProps &&
+              component.defaultProps[prop] !== undefined
+                ? JSON.stringify(component.defaultProps[prop])
+                : ""}
+            </td>
+          </tr>
         ))}
-      </div>
-    </div>
+      </tbody>
+    </table>
   );
 };
 
