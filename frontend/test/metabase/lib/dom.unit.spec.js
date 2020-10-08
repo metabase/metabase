82550390a7562f7ba197684a26461b0e97c465d1
Nemanja <glumac@protonmail.com>
2020-10-08 16:43:54 +0200

    Remove unsupported tests

--- frontend/test/metabase/lib/dom.unit.spec.js
+++ frontend/test/metabase/lib/dom.unit.spec.js
@@ -18,26 +18,4 @@ describe("getSelectionPosition/setSelectionPosition", () => {
     const position = getSelectionPosition(input);
     expect(position).toEqual([3, 6]);
   });
-
-  // NOTE (based on original Tom Robinson's note from 1/7/2019):
-  // this worked in karma but jsdom doesn't have the required APIs on div/contenteditable
-  xit("should get/set selection on contenteditable correctly", () => {
-    const contenteditable = document.createElement("div");
-    container.appendChild(contenteditable);
-    contenteditable.textContent = "<div>hello world</div>";
-    setSelectionPosition(contenteditable, [3, 6]);
-    const position = getSelectionPosition(contenteditable);
-    expect(position).toEqual([3, 6]);
-  });
-
-  xit("should not mutate the actual selection", () => {
-    const contenteditable = document.createElement("div");
-    container.appendChild(contenteditable);
-    contenteditable.textContent = "<div>hello world</div>";
-    setSelectionPosition(contenteditable, [3, 6]);
-    const position = getSelectionPosition(contenteditable);
-    expect(position).toEqual([3, 6]);
-    const position2 = getSelectionPosition(contenteditable);
-    expect(position2).toEqual([3, 6]);
-  });
 });
