ab03beca3ea263ce63f69d7e538749bdd9ef5a26
Damon P. Cortesi <d.lifehacker@gmail.com>
2020-10-14 14:58:19 -0700

    Switch to SheetJS for xlsx/csv testing and upgrade Cypress because of https://github.com/cypress-io/cypress/pull/7382

--- frontend/test/metabase/scenarios/excel/downloads.cy.spec.js
+++ frontend/test/metabase/scenarios/excel/downloads.cy.spec.js
@@ -1,29 +1,12 @@
 import { restore, signInAsAdmin, openOrdersTable } from "__support__/cypress";
 
 const path = require("path");
+const xlsx = require("xlsx");
 
 describe("file download", () => {
-  // metabase/downloads
-  const downloadsFolder = "./downloads";
-
   before(restore);
   beforeEach(() => {
     signInAsAdmin();
-    cy.task("clearDownloads");
-
-    // The next command allow downloads in Electron, Chrome, and Edge
-    // without any users popups or file save dialogs.
-    if (Cypress.browser.name !== "firefox") {
-      // since this call returns a promise, must tell Cypress to wait
-      // for it to be resolved
-      cy.wrap(
-        Cypress.automation("remote:debugger:protocol", {
-          command: "Page.setDownloadBehavior",
-          params: { behavior: "allow", downloadPath: downloadsFolder },
-        }),
-        { log: false },
-      );
-    }
   });
 
   it("downloads Excel file", () => {
@@ -36,63 +19,45 @@ describe("file download", () => {
     cy.findByText("Orders, Count").click();
     cy.contains("18,760");
     cy.get(".Icon-download").click();
-    cy.get(".Icon-xlsx").click();
-    // The test doesn't go past this point - it hangs on "form submit" and times out after 1 minute
-    // with the message: "Your page did not fire its 'load' event within '60000ms'".
+    cy.get(".Icon-xlsx")
+      .parent()
+      .parent()
+      .get('input[name="query"]')
+      .invoke("val")
+      .then(xlsx_query_params => {
+        cy.request({
+          url: "/api/dataset/xlsx",
+          method: "POST",
+          form: true,
+          body: { query: xlsx_query_params },
+          encoding: "binary",
+        }).then(resp => {
+          var workbook = xlsx.read(resp.body, { type: "binary" });
 
-    cy.log("**confirm downloaded file**");
+          expect(workbook.SheetNames[0]).to.eq("Query result");
+          expect(workbook.Sheets["Query result"]["A1"].v).to.eq("Count");
+          expect(workbook.Sheets["Query result"]["A2"].v).to.eq(18760);
+        });
+      });
 
-    // NOTE: We need to figure out a way to read the file name, because it is auto-generated:
-    // query_result_YYYYY_MM_DD+TIMESTAMP
-
-    cy.log("**Test will break at this point - the file doesn't exist YET**");
-    const downloadedFilename = path.join(
-      downloadsFolder,
-      "FIGURE_THIS_OUT.xlsx",
-    );
-
-    // ensure the file has been saved before trying to parse it
-    cy.readFile(downloadedFilename, "binary", { timeout: 15000 }).should(
-      buffer => {
-        // by having length assertion we ensure the file has text
-        // since we don't know when the browser finishes writing it to disk
-
-        // Tip: use expect() form to avoid dumping binary contents
-        // of the buffer into the Command Log
-        expect(buffer.length).to.be.gt(100);
-      },
-    );
-
-    cy.log("**the file exists**");
-
-    // the first utility library we use to parse Excel files
-    // only works in Node, thus we can read and parse
-    // the downloaded file using cy.task
-    cy.task("readExcelFile", downloadedFilename);
-    // returns an array of lines read from Excel file
-
-    // NOTE: This is copied just as an example from Cypress repo
-    //       We obviously need to make our own assertions here once we figure the previous steps out.
-
-    // .should("have.length", 4)
-    // .then(list => {
-    //   expect(list[0], "header line").to.deep.equal([
-    //     "First name",
-    //     "Last name",
-    //     "Occupation",
-    //     "Age",
-    //     "City",
-    //     "State",
-    //   ]);
-
-    //   expect(list[1], "first person").to.deep.equal([
-    //     "Joe",
-    //     "Smith",
-    //     "student",
-    //     20,
-    //     "Boston",
-    //     "MA",
-    //   ]);
-    // });
+    cy.get(".Icon-csv")
+      .parent()
+      .parent()
+      .get('input[name="query"]')
+      .invoke("val")
+      .then(csv_query_params => {
+        cy.request({
+          url: "/api/dataset/csv",
+          method: "POST",
+          form: true,
+          body: { query: csv_query_params },
+          encoding: "binary",
+        }).then(resp => {
+          var workbook = xlsx.read(resp.body, { type: "binary" });
+          expect(workbook.SheetNames[0]).to.eq("Sheet1");
+          expect(workbook.Sheets["Sheet1"]["A1"].v).to.eq("Count");
+          expect(workbook.Sheets["Sheet1"]["A2"].v).to.eq(18760);
+        });
+      });
   });
 });
