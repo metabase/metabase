cfb86cb4a995027f366373f7b8d3d781bf6a286b
Kyle Doherty <kyle.l.doherty@gmail.com>
2020-10-18 20:57:01 -0400

    restore old legacy app routes

--- frontend/src/metabase/internal/routes.js
+++ frontend/src/metabase/internal/routes.js
@@ -10,6 +10,20 @@ import {
   Unauthorized,
 } from "metabase/containers/ErrorPages";
 
+// Import legacy apps - TODO - move this to a different style of documentation
+// $FlowFixMe: doesn't know about require.context
+const req = require.context(
+  "metabase/internal/components",
+  true,
+  /(\w+)App.jsx$/,
+);
+
+const APPS = {};
+for (const key of req.keys()) {
+  const name = key.match(/(\w+)App.jsx$/)[1];
+  APPS[name] = req(key).default;
+}
+
 /* Pages - In order they appear in nav */
 import WelcomePage from "metabase/internal/pages/WelcomePage";
 import TypePage from "metabase/internal/pages/TypePage";
@@ -31,6 +45,14 @@ export default (
     <Route path="colors" component={ColorsPage} />
     <Route path="components/:componentName" component={ComponentsPage} />
     <Route path="modals" component={ModalsPage} />
+    {/* Legacy App pages - not really style guide related, but keep for now */}
+    {Object.entries(APPS).map(
+      ([name, Component]) =>
+        Component &&
+        (Component.routes || (
+          <Route path={name.toLowerCase()} component={Component} />
+        )),
+    )}
     <Route path="errors">
       <Route path="404" component={NotFound} />
       <Route path="archived" component={Archived} />
