e3075eb5e74faa2cef782c4d148b4435da30c5b3
dan sutton <dan@dpsutton.com>
2020-11-04 17:47:27 -0600

    Fingerprints reduce over table-rows-sample instead of realizing all
    tests are failing because we test fingerprinters a lot by supplying
    fake results and then reducing over those results. Now we are reducing
    over them in the query processor and need to get a better testing
    strategy.
    
    I think i can stub out the query-processor to take the `:rff` and just
    transduce over some fake supplied results but need to verify

--- src/metabase/db/metadata_queries.clj
+++ src/metabase/db/metadata_queries.clj
@@ -93,32 +93,46 @@
 
 (def TableRowsSampleOptions
   "Schema for `table-rows-sample` options"
-  (s/maybe {(s/optional-key :truncation-size) s/Int}))
+  (s/maybe {(s/optional-key :truncation-size) s/Int
+            (s/optional-key :rff)             s/Any}))
 
-(s/defn table-rows-sample :- (s/maybe si/TableSample)
-  "Run a basic MBQL query to fetch a sample of rows belonging to a Table. Can pass in optional `:truncation-size` for
-  text fields which will be honored only if the driver supports expressions."
-  {:style/indent 1}
-  ([table :- si/TableInstance, fields :- [si/FieldInstance]]
-   (table-rows-sample table fields nil))
-  ([table :- si/TableInstance, fields :- [si/FieldInstance]
-    {:keys [truncation-size] :as _opts} :- TableRowsSampleOptions]
+(defn table-rows-sample-query
+  ([table fields {:keys [truncation-size] :as _opts}]
    (let [driver             (-> table table/database driver.u/database->driver)
          text-fields        (filter (comp #{:type/Text} :base_type) fields)
          field->expressions (when (and truncation-size (driver/supports? driver :expressions))
                               (into {} (for [field text-fields]
                                          [field [(str (gensym "substring"))
-                                                 [:substring [:field-id (u/get-id field)] 1 truncation-size]]])))
-         results            ((resolve 'metabase.query-processor/process-query)
-                             {:database   (:db_id table)
-                              :type       :query
-                              :query      {:source-table (u/get-id table)
-                                           :expressions  (into {} (vals field->expressions))
-                                           :fields       (vec (for [field fields]
-                                                                (if-let [[expression-name _] (get field->expressions field)]
-                                                                  [:expression expression-name]
-                                                                  [:field-id (u/get-id field)])))
-                                           :limit        max-sample-rows}
-                              :middleware {:format-rows?           false
-                                           :skip-results-metadata? true}})]
-     (get-in results [:data :rows]))))
+                                                 [:substring [:field-id (u/get-id field)] 1 truncation-size]]])))]
+     {:database   (:db_id table)
+      :type       :query
+      :query      {:source-table (u/get-id table)
+                   :expressions  (into {} (vals field->expressions))
+                   :fields       (vec (for [field fields]
+                                        (if-let [[expression-name _] (get field->expressions field)]
+                                          [:expression expression-name]
+                                          [:field-id (u/get-id field)])))
+                   :limit        max-sample-rows}
+      :middleware {:format-rows?           false
+                   :skip-results-metadata? true}})))
+
+(s/defn table-rows-sample
+  "Run a basic MBQL query to fetch a sample of rows belonging to a Table.
+
+  Options: a map of
+  `:truncation-size`: [optional] size to truncate text fields if the driver supports expressions.
+  `:rff`: [optional] a reducing function to reduce over the result set in the the query-processor rather than
+  realizing the whole collection"
+  {:style/indent 1}
+  ([table :- si/TableInstance, fields :- [si/FieldInstance]]
+   (table-rows-sample table fields nil))
+  ([table :- si/TableInstance, fields :- [si/FieldInstance]
+    {:keys [rff] :as opts} :- TableRowsSampleOptions]
+   (let [query   (table-rows-sample-query table fields opts)
+         qp      (resolve 'metabase.query-processor/process-query)
+         results (if rff
+                   (qp query {:rff rff})
+                   (qp query))]
+     (if rff
+       results
+       (get-in results [:data :rows])))))
