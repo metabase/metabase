5e132effd15773c9f4925ec695d1f235a0e3361f
Cam Saul <github@camsaul.com>
2020-11-06 14:25:11 -0800

    /bin/bash instead of /usr/bin/env bash

--- .circleci/save-files-with-changes.sh
+++ .circleci/save-files-with-changes.sh
@@ -1,8 +1,8 @@
-#! /usr/bin/env bash
+#! /bin/bash
 
 set -euo pipefail
 
-parent_commit_info=`git log --oneline --first-parent --decorate=short --simplify-by-decoration --branches -n 10 | grep 'origin/' | head -n 1`
+parent_commit_info=`git log --oneline --first-parent --decorate=short --simplify-by-decoration --branches | grep 'origin/' | head -n 1`
 echo "parent commit info: $parent_commit_info";
 
 parent_branch=`echo "$parent_commit_info" | sed -r 's/^\w+\s\(origin\/([^,)]+).+$/\1/'`;
