694b3249f56c6d2c51477755b323290f57bf3b7d
Kyle Doherty <kyle.l.doherty@gmail.com>
2020-11-12 10:51:42 -0500

    rm old prototype [ci skip]

--- frontend/src/metabase/routes.jsx
+++ frontend/src/metabase/routes.jsx
@@ -44,8 +44,6 @@ import ArchiveCollectionModal from "metabase/components/ArchiveCollectionModal";
 import CollectionPermissionsModal from "metabase/admin/permissions/containers/CollectionPermissionsModal";
 import UserCollectionList from "metabase/containers/UserCollectionList";
 
-import NewCollection from "metabase/containers/NewCollection";
-
 import PulseEditApp from "metabase/pulse/containers/PulseEditApp";
 import SetupApp from "metabase/setup/containers/SetupApp";
 import PostSetupApp from "metabase/setup/containers/PostSetupApp";
@@ -201,10 +199,6 @@ export const getRoutes = store => (
 
         <Route path="collection/users" component={IsAdmin}>
           <IndexRoute component={UserCollectionList} />
-        </Route>
-
-        <Route path="collection/v2">
-          <IndexRoute component={NewCollection} />
         </Route>
 
         <Route path="collection/:collectionId" component={CollectionLanding}>
