4405785cd41400b024d44ccd7c8dcb9bf5a5ca6c
dpsutton <dan@dpsutton.com>
2020-11-18 14:39:06 -0600

    Revert "Add `updated` special type for timestamps (#13818)" (#13836)
    This reverts commit dc4d4de2c4629b5cf0c9d28e9750c6a910fb0e6a.

--- src/metabase/types.clj
+++ src/metabase/types.clj
@@ -134,12 +134,6 @@
 (derive :type/DeletionDate :type/Date)
 (derive :type/DeletionDate :type/DeletionTimestamp)
 
-(derive :type/UpdatedTimestamp :type/DateTime)
-(derive :type/UpdatedTime :type/Date)
-(derive :type/UpdatedTime :type/UpdatedTimestamp)
-(derive :type/UpdatedDate :type/Date)
-(derive :type/UpdatedDate :type/UpdatedTimestamp)
-
 (derive :type/Birthdate :type/Date)
 
 
