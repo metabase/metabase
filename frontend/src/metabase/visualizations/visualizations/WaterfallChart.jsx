159b917d7596ffac4ad7649ba3393042bb23d3a5
Ariya Hidayat <ariya@metabase.com>
2020-12-01 16:03:47 -0800

    Waterfall settings: color widgets for increases, decreases, total

--- frontend/src/metabase/visualizations/visualizations/WaterfallChart.jsx
+++ frontend/src/metabase/visualizations/visualizations/WaterfallChart.jsx
@@ -5,10 +5,11 @@ import { assocIn } from "icepick";
 
 import {
   GRAPH_DATA_SETTINGS,
-  GRAPH_COLORS_SETTINGS,
   GRAPH_AXIS_SETTINGS,
   GRAPH_DISPLAY_VALUES_SETTINGS,
 } from "../lib/settings/graph";
+
+import { color } from "metabase/lib/colors";
 
 export default class WaterfallChart extends LineAreaBarChart {
   static uiName = t`Waterfall`;
@@ -17,8 +18,25 @@ export default class WaterfallChart extends LineAreaBarChart {
   static noun = t`waterfall chart`;
 
   static settings = {
-    ...GRAPH_COLORS_SETTINGS,
     ...GRAPH_AXIS_SETTINGS,
+    "waterfall.increase_color": {
+      section: t`Display`,
+      title: t`Increase color`,
+      widget: "color",
+      default: color("accent1"),
+    },
+    "waterfall.decrease_color": {
+      section: t`Display`,
+      title: t`Decrease color`,
+      widget: "color",
+      default: color("accent3"),
+    },
+    "waterfall.total_color": {
+      section: t`Display`,
+      title: t`Total color`,
+      widget: "color",
+      default: color("accent7"),
+    },
     "waterfall.show_total": {
       section: t`Display`,
       title: t`Show total`,
