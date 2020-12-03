b731f5c18b90486fcb8947639db64fb3be88910f
Ariya Hidayat <ariya@metabase.com>
2020-12-03 06:14:30 -0800

    Waterfall settings: color settings for increases, decreases, total (#13931)
    * Waterfall settings: color widgets for increases, decreases, total
    
    * Compactify the color settings for waterfall
    
    * Waterfall colors: apply the increase & decrease colors
    
    * Move waterfall coloring to setChartColor()
    
    * Separate waterfall's total into its own (in the virtual stacked bars)
    
    This makes it easy to color the total bar, which has a value only for
    the last one in the series anyway
    
    * Waterfall total color should be conditional on the total toggle

--- frontend/src/metabase/visualizations/visualizations/WaterfallChart.jsx
+++ frontend/src/metabase/visualizations/visualizations/WaterfallChart.jsx
@@ -5,10 +5,11 @@ import { assocIn } from "icepick";
 
 import {
   GRAPH_DATA_SETTINGS,
-  GRAPH_COLORS_SETTINGS,
   GRAPH_AXIS_SETTINGS,
   GRAPH_DISPLAY_VALUES_SETTINGS,
 } from "../lib/settings/graph";
+
+import { color } from "metabase/lib/colors";
 
 export default class WaterfallChart extends LineAreaBarChart {
   static uiName = t`Waterfall`;
@@ -17,13 +18,33 @@ export default class WaterfallChart extends LineAreaBarChart {
   static noun = t`waterfall chart`;
 
   static settings = {
-    ...GRAPH_COLORS_SETTINGS,
     ...GRAPH_AXIS_SETTINGS,
+    "waterfall.increase_color": {
+      section: t`Display`,
+      props: { title: t`Increase color` },
+      widget: "color",
+      default: color("accent1"),
+    },
+    "waterfall.decrease_color": {
+      section: t`Display`,
+      props: { title: t`Decrease color` },
+      widget: "color",
+      default: color("accent3"),
+    },
     "waterfall.show_total": {
       section: t`Display`,
       title: t`Show total`,
       widget: "toggle",
       default: true,
+    },
+    "waterfall.total_color": {
+      section: t`Display`,
+      props: { title: t`Total color` },
+      widget: "color",
+      default: color("accent7"),
+      getHidden: (series, vizSettings) =>
+        vizSettings["waterfall.show_total"] !== true,
+      readDependencies: ["waterfall.show_total"],
     },
     ...GRAPH_DISPLAY_VALUES_SETTINGS,
     ...GRAPH_DATA_SETTINGS,
