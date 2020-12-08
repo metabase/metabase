77614d5c076e544cf8fad815922b8810f1e2d528
Cam Saul <github@camsaul.com>
2020-12-08 12:53:56 -0800

    Test fixes :wrench:

--- test/metabase/query_processor_test/explicit_joins_test.clj
+++ test/metabase/query_processor_test/explicit_joins_test.clj
@@ -483,7 +483,7 @@
   ;; currently work. We should use the closest equivalent types (e.g. `DATETIME` or `TIMESTAMP` so we can still load
   ;; the dataset and run tests using this dataset such as these, which doesn't even use the TIME type.
   (mt/test-drivers (set/difference (mt/normal-drivers-with-feature :nested-queries :left-join)
-                                   #{:oracle :presto :redshift :sparksql :snowflake})
+                                   metabase.query-processor-test.timezones-test/broken-drivers)
     (testing "Date filter should behave the same for joined columns"
       (mt/dataset attempted-murders
         (is (= [["2019-11-01T07:23:18.331Z" "2019-11-01T07:23:18.331Z"]]
