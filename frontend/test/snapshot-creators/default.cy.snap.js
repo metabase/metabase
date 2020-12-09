0b69d09ba8f2b35083382f52b58c37b5e1d6de7a
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2020-12-09 02:43:34 +0100

    Extract sample dataset metadata in Cypress (#13994)
    * Extract `SAMPLE_DATASET` metadata
    
    * Test that metadata extraction is working properly in CI
    
    
    Note: `frontend/test/__support__/cypress_sample_dataset.json` file will be overwritten every single time Cypress runs. It is added to `.gitignore` for that reason.

--- frontend/test/snapshot-creators/default.cy.snap.js
+++ frontend/test/snapshot-creators/default.cy.snap.js
@@ -16,6 +16,10 @@ describe("snapshots", () => {
       addUsersAndGroups();
       withSampleDataset(SAMPLE_DATASET => {
         createQuestionAndDashboard(SAMPLE_DATASET);
+        cy.writeFile(
+          "frontend/test/__support__/cypress_sample_dataset.json",
+          SAMPLE_DATASET,
+        );
       });
       snapshot("default");
       restore("blank");
