30e5948d358f103070588b4b7b112e26e8f48415
dpsutton <dan@dpsutton.com>
2020-12-14 17:29:38 -0600

    Unix fingerprinters (#14068)
    * Incorporate magnitude when fingerprinting UNIX timestamps
    
    * cleanup of transducers and reducing functions for fingerprinters
    
    * Handle fractions in microseconds -> milliseconds
    
    * Use quotient over (long (/ ...))

--- src/metabase/sync/analyze/fingerprint/fingerprinters.clj
+++ src/metabase/sync/analyze/fingerprint/fingerprinters.clj
@@ -186,16 +186,29 @@
 (extend-protocol ITemporalCoerceable
   nil      (->temporal [_]    nil)
   String   (->temporal [this] (->temporal (u.date/parse this)))
-  Long     (->temporal [this] (->temporal (t/instant this)))
+  Long     (->temporal [this] (->temporal (t/instant this))) ;; expects millis
   Integer  (->temporal [this] (->temporal (t/instant this)))
   ChronoLocalDateTime (->temporal [this] (.toInstant this (ZoneOffset/UTC)))
   ChronoZonedDateTime (->temporal [this] (.toInstant this))
   Temporal (->temporal [this] this))
 
-(deffingerprinter :type/DateTime
+(def date-time-rf
+  "Reducing function for date times, coercing to temporal using `ITemporalCoerceable` and accumulating earliest and
+  latest values."
   ((map ->temporal)
    (robust-fuse {:earliest earliest
                  :latest   latest})))
+
+(deffingerprinter :type/DateTime
+  date-time-rf)
+
+(deffingerprinter [:type/DateTime :type/UNIXTimestampSeconds]
+  ((map (fn [x] (when x (* x 1000))))
+   date-time-rf))
+
+(deffingerprinter [:type/DateTime :type/UNIXTimestampMicroseconds]
+  ((map (fn [x] (when x (quot x 1000))))
+   date-time-rf))
 
 (defn- histogram
   "Transducer that summarizes numerical data with a histogram."
