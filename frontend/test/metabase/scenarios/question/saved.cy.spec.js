52672f3e020716e45a540029a0bc4e287c614c75
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2020-12-15 17:48:07 +0100

    #13817 Repro: "Save question" modal wrong state applied to "Save" button (#14082) [ci skip]

--- frontend/test/metabase/scenarios/question/saved.cy.spec.js
+++ frontend/test/metabase/scenarios/question/saved.cy.spec.js
@@ -3,11 +3,60 @@ import {
   signInAsNormalUser,
   popover,
   modal,
+  openOrdersTable,
 } from "__support__/cypress";
 
 describe("scenarios > question > saved", () => {
-  before(restore);
-  beforeEach(signInAsNormalUser);
+  beforeEach(() => {
+    restore();
+    signInAsNormalUser();
+  });
+
+  it.skip("should should correctly display 'Save' modal (metabase#13817)", () => {
+    openOrdersTable({ mode: "notebook" });
+    cy.findByText("Summarize").click();
+    cy.findByText("Count of rows").click();
+    cy.findByText("Pick a column to group by").click();
+    cy.findByText("Total").click();
+    // Save the question
+    cy.findByText("Save").click();
+    modal().within(() => {
+      cy.findByText("Save").click();
+    });
+    cy.findByText("Not now").click();
+    cy.findByText("Save").should("not.exist");
+
+    // Add a filter in order to be able to save question again
+    cy.findByText("Filter").click();
+    cy.findByText(/^Total$/).click();
+    cy.findByText("Equal to").click();
+    cy.findByText("Greater than").click();
+    cy.findByPlaceholderText("Enter a number").type("60");
+    cy.findByText("Add filter").click();
+
+    // Save question - opens "Save question" modal
+    cy.findByText("Save").click();
+
+    modal().within(() => {
+      cy.findByText("Save question");
+      cy.findByRole("button", { name: /save/i }).as("saveButton");
+      cy.get("@saveButton").should("not.be.disabled");
+
+      cy.log(
+        "**When there is no question name, it shouldn't be possible to save**",
+      );
+      cy.findByText("Save as new question").click();
+      cy.findByLabelText("Name").should("be.empty");
+      cy.findByLabelText("Description").should("be.empty");
+      cy.get("@saveButton").should("be.disabled");
+
+      cy.log(
+        "**It should `always` be possible to overwrite the original question**",
+      );
+      cy.findByText(/^Replace original question,/).click();
+      cy.get("@saveButton").should("not.be.disabled");
+    });
+  });
 
   it("view and filter saved question", () => {
     cy.visit("/question/1");
