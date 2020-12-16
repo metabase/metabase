8b868f7b921c22f0049eec8ec0fb5be0d353050d
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2020-12-17 00:42:02 +0100

    Convert `QuestionAndResultLoader.unit.spec.js`

--- frontend/test/metabase/containers/QuestionAndResultLoader.unit.spec.js
+++ frontend/test/metabase/containers/QuestionAndResultLoader.unit.spec.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
 
 import QuestionAndResultLoader from "metabase/containers/QuestionAndResultLoader";
 
 describe("QuestionAndResultLoader", () => {
   it("should load a question and a result", () => {
-    shallow(<QuestionAndResultLoader>{() => <div />}</QuestionAndResultLoader>);
+    render(<QuestionAndResultLoader>{() => <div />}</QuestionAndResultLoader>);
   });
 });
