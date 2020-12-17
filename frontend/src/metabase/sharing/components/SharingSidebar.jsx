02fc48721dc77462473945771e8012b918afe8d6
Maz Ameli <maz@metabase.com>
2020-12-17 10:32:25 -0800

    Dash subscription polish batch 3 (#14105)
    * change pulse sent to email sent
    
    * remove transition from Card.jsx
    
    * tweak lineheight for email and slack boxes
    
    * emailAttachmentPicker styling
    
    * add caveat about charts looking different
    
    * recipient token styling
    
    * prettier
    
    * remove anchor link to appease ci
    
    * update snapshots
    
    * dogs living with cats and urls don't need file extensions anymore it seems

--- frontend/src/metabase/sharing/components/SharingSidebar.jsx
+++ frontend/src/metabase/sharing/components/SharingSidebar.jsx
@@ -10,6 +10,7 @@ import EmailAttachmentPicker from "metabase/sharing/components/EmailAttachmentPi
 import Icon from "metabase/components/Icon";
 import Label from "metabase/components/type/Label";
 import Subhead from "metabase/components/type/Subhead";
+import Text from "metabase/components/type/Text";
 import Link from "metabase/components/Link";
 import ModalWithTrigger from "metabase/components/ModalWithTrigger";
 import RecipientPicker from "metabase/pulse/components/RecipientPicker";
@@ -28,6 +29,7 @@ import { push, goBack } from "react-router-redux";
 import { connect } from "react-redux";
 
 import { cleanPulse, createChannel } from "metabase/lib/pulse";
+import MetabaseSettings from "metabase/lib/settings";
 
 import {
   getPulseId,
@@ -497,6 +499,20 @@ class SharingSidebar extends React.Component {
     const { editingMode } = this.state;
     const { pulse, formInput, pulseList, onCancel } = this.props;
 
+    const caveatMessage = (
+      <Text className="mx4 my2 p2 bg-light text-dark rounded">{jt`${(
+        <span className="text-bold">Note:</span>
+      )} charts in your subscription won't look the same as in your dashboard. ${(
+        <a
+          className="link"
+          target="_blank"
+          href={MetabaseSettings.docsUrl("users-guide/10-pulses")}
+        >
+          Learn more
+        </a>
+      )}.`}</Text>
+    );
+
     // protect from empty values that will mess this up
     if (formInput === null || pulse === null || pulseList === null) {
       return <Sidebar />;
@@ -585,8 +601,8 @@ class SharingSidebar extends React.Component {
                 }
               }}
             >
-              <div className="p3">
-                <div className="flex align-center mb1">
+              <div className="px3 pt3 pb2">
+                <div className="flex align-center">
                   <Icon
                     name="mail"
                     className={cx(
@@ -602,7 +618,8 @@ class SharingSidebar extends React.Component {
                     className={cx({ "text-light": !emailSpec.configured })}
                   >{t`Email it`}</h3>
                 </div>
-                <div
+                <Text
+                  lineHeight={1.5}
                   className={cx("text-medium", {
                     "hover-child hover--inherit": emailSpec.configured,
                   })}
@@ -615,7 +632,7 @@ class SharingSidebar extends React.Component {
                     )} first.`}
                   {emailSpec.configured &&
                     t`You can send this dashboard regularly to users or email addresses.`}
-                </div>
+                </Text>
               </div>
             </Card>
             <Card
@@ -631,7 +648,7 @@ class SharingSidebar extends React.Component {
                 }
               }}
             >
-              <div className="p3">
+              <div className="px3 pt3 pb2">
                 <div className="flex align-center mb1">
                   <Icon
                     name={slackSpec.configured ? "slack_colorized" : "slack"}
@@ -645,7 +662,8 @@ class SharingSidebar extends React.Component {
                     className={cx({ "text-light": !slackSpec.configured })}
                   >{t`Send it to Slack`}</h3>
                 </div>
-                <div
+                <Text
+                  lineHeight={1.5}
                   className={cx("text-medium", {
                     "hover-child hover--inherit": slackSpec.configured,
                   })}
@@ -658,7 +676,7 @@ class SharingSidebar extends React.Component {
                     )}.`}
                   {slackSpec.configured &&
                     t`Pick a channel and a schedule, and Metabase will do the rest.`}
-                </div>
+                </Text>
               </div>
             </Card>
           </div>
@@ -691,10 +709,11 @@ class SharingSidebar extends React.Component {
           onCancel={onCancel}
           className="text-dark"
         >
-          <div className="pt4 flex align-center px4">
+          <div className="pt4 px4 flex align-center">
             <Icon name="mail" className="mr1" size={21} />
             <Heading>{t`Email this dashboard`}</Heading>
           </div>
+          {caveatMessage}
           <div className="my2 px4">
             <div>
               <div className="text-bold mb1">
@@ -794,6 +813,7 @@ class SharingSidebar extends React.Component {
             <Icon name="slack" className="mr1" size={21} />
             <Heading>{t`Send this dashboard to Slack`}</Heading>
           </div>
+          {caveatMessage}
           <div className="pb2 px4">
             {channelSpec.fields &&
               this.renderFields(channel, index, channelSpec)}
