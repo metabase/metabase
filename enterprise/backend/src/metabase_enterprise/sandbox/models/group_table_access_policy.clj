57e23debb0b28354bbabb7b207af89a2e7f44f1b
Cam Saul <github@camsaul.com>
2020-12-17 16:04:22 -0800

    Constraint sandboxing queries (rebased on infer-source-metadata-when-saving-card)

--- enterprise/backend/src/metabase_enterprise/sandbox/models/group_table_access_policy.clj
+++ enterprise/backend/src/metabase_enterprise/sandbox/models/group_table_access_policy.clj
@@ -2,12 +2,25 @@
   "Model definition for Group Table Access Policy, aka GTAP. A GTAP is useed to control access to a certain Table for a
   certain PermissionsGroup. Whenever a member of that group attempts to query the Table in question, a Saved Question
   specified by the GTAP is instead used as the source of the query."
-  (:require [clojure.walk :as walk]
+  (:require [clojure.tools.logging :as log]
+            [clojure.walk :as walk]
             [medley.core :as m]
             [metabase.mbql.normalize :as normalize]
-            [metabase.models.interface :as i]
+            [metabase.middleware.session :as session]
+            [metabase.models
+             [card :as card :refer [Card]]
+             [interface :as i]
+             [table :as table]]
+            [metabase.plugins.classloader :as classloader]
+            [metabase.query-processor.error-type :as qp.error-type]
             [metabase.util :as u]
-            [toucan.models :as models]))
+            [metabase.util
+             [i18n :refer [tru]]
+             [schema :as su]]
+            [schema.core :as s]
+            [toucan
+             [db :as db]
+             [models :as models]]))
 
 (models/defmodel GroupTableAccessPolicy :group_table_access_policy)
 
@@ -24,11 +37,92 @@
   :in  (comp i/json-in normalize-attribute-remapping-targets)
   :out (comp normalize-attribute-remapping-targets i/json-out-without-keywordization))
 
+(s/defn check-columns-match-table
+  "Make sure the result metadata data columns for the Card associated with a GTAP match up with the columns in the Table
+  that's getting GTAPped. It's ok to remove columns, but you cannot add new columns. The base types of the Card
+  columns can derive from the respective base types of the columns in the Table itself, but you cannot return an
+  entirely different type."
+  ([{card-id :card_id, table-id :table_id}]
+   ;; not all GTAPs have Cards
+   (when card-id
+     ;; not all Cards have saved result metadata
+     (when-let [result-metadata (db/select-one-field :result_metadata Card :id card-id)]
+       (check-columns-match-table table-id result-metadata))))
+
+  ([table-id :- su/IntGreaterThanZero result-metadata-columns]
+   ;; prevent circular refs
+   (classloader/require 'metabase.query-processor)
+   (let [table-cols (into {} (for [col (session/with-current-user nil
+                                         ((resolve 'metabase.query-processor/query->expected-cols)
+                                          {:database (table/table-id->database-id table-id)
+                                           :type     :query
+                                           :query    {:source-table table-id}}))]
+                               [(:name col) col]))]
+     (doseq [col  result-metadata-columns
+             :let [table-col-base-type (get-in table-cols [(:name col) :base_type])]]
+       ;; These errors might get triggered by API endpoints or by the QP (this code is used in the
+       ;; `row-level-restrictions` middleware). So include `:type` and `:status-code` information in the ExceptionInfo
+       ;; data so it can be passed along if applicable.
+       (when-not table-col-base-type
+         (throw (ex-info (tru "Sandbox Cards can''t return columns that aren't present in the Table they are sandboxing.")
+                         {:type        qp.error-type/bad-configuration
+                          :status-code 400
+                          :new-column  col
+                          :expected    (mapv :name table-cols)
+                          :actual      (mapv :name result-metadata-columns)})))
+       (when-not (isa? (keyword (:base_type col)) table-col-base-type)
+         (throw (ex-info (tru "Sandbox Cards can''t return columns that have different types than the Table they are sandboxing.")
+                         {:type        qp.error-type/bad-configuration
+                          :status-code 400
+                          :new-col     col
+                          :expected    table-col-base-type
+                          :actual      (:base_type col)})))))))
+
+;; TODO -- should we only check these constraints if EE features are enabled??
+(defn update-card-check-gtaps
+  "If a Card is updated, and its result metadata changes, check that these changes do not violate the constraints placed
+  on GTAPs (the Card cannot add fields or change types vs. the original Table)."
+  [{new-result-metadata :result_metadata, card-id :id}]
+  (when new-result-metadata
+    (when-let [gtaps-using-this-card (not-empty (db/select [GroupTableAccessPolicy :id :table_id] :card_id card-id))]
+      (let [original-result-metadata (db/select-one-field :result_metadata Card :id card-id)]
+        (when-not (= original-result-metadata new-result-metadata)
+          (doseq [{gtap-id :id, table-id :table_id} gtaps-using-this-card]
+            (try
+              (check-columns-match-table table-id new-result-metadata)
+              (catch clojure.lang.ExceptionInfo e
+                (throw (ex-info (str (tru "Cannot update Card: Card is used for Sandboxing, and updates would violate sandbox rules.")
+                                     " "
+                                     (.getMessage e))
+                                (ex-data e)
+                                e))))))))))
+
+(log/trace "Installing additional EE pre-update checks for Card")
+(reset! card/pre-update-check-sandbox-constraints update-card-check-gtaps)
+
+(defn- pre-insert [gtap]
+  (u/prog1 gtap
+    (check-columns-match-table gtap)))
+
+(defn- pre-update [{:keys [id], :as updates}]
+  ;; TODO -- consider whether it even makes sense to let
+  (u/prog1 updates
+    (let [original (GroupTableAccessPolicy id)
+          updated  (merge original updates)]
+      (when-not (= (:table_id original) (:table_id updated))
+        (throw (ex-info (tru "You cannot change the Table ID of a GTAP once it has been created.")
+                        {:id          id
+                         :status-code 400})))
+      (when (:card_id updates)
+        (check-columns-match-table updated)))))
+
 (u/strict-extend (class GroupTableAccessPolicy)
   models/IModel
   (merge
    models/IModelDefaults
-   {:types (constantly {:attribute_remappings ::attribute-remappings})})
+   {:types      (constantly {:attribute_remappings ::attribute-remappings})
+    :pre-insert pre-insert
+    :pre-update pre-update})
 
   ;; only admins can work with GTAPs
   i/IObjectPermissions
--- enterprise/backend/test/metabase_enterprise/sandbox/models/group_table_access_policy_test.clj
+++ enterprise/backend/test/metabase_enterprise/sandbox/models/group_table_access_policy_test.clj
@@ -1,29 +1,128 @@
 (ns metabase-enterprise.sandbox.models.group-table-access-policy-test
-  (:require [expectations :refer [expect]]
+  (:require [clojure.test :refer :all]
+            [metabase
+             [models :refer [Card]]
+             [query-processor :as qp]
+             [test :as mt]
+             [util :as u]]
             [metabase-enterprise.sandbox.models.group-table-access-policy :refer [GroupTableAccessPolicy]]
             [metabase.models.permissions-group :as group]
-            [metabase.test.data :as data]
-            [metabase.util :as u]
-            [toucan.db :as db]
-            [toucan.util.test :as tt]))
+            [toucan.db :as db]))
 
-;; make sure attribute-remappings come back from the DB normalized the way we'd expect
-(expect
-  {"venue_id" {:type   :category
-               :target [:variable [:field-id (data/id :venues :id)]]
-               :value  5}}
-  (tt/with-temp GroupTableAccessPolicy [gtap {:table_id             (data/id :venues)
-                                              :group_id             (u/get-id (group/all-users))
-                                              :attribute_remappings {"venue_id" {:type   "category"
-                                                                                 :target ["variable" ["field-id" (data/id :venues :id)]]
-                                                                                 :value  5}}}]
-    (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap))))
+(deftest normalize-attribute-remappings-test
+  (testing "make sure attribute-remappings come back from the DB normalized the way we'd expect"
+    (mt/with-temp GroupTableAccessPolicy [gtap {:table_id             (mt/id :venues)
+                                                :group_id             (u/get-id (group/all-users))
+                                                :attribute_remappings {"venue_id"
+                                                                       {:type   "category"
+                                                                        :target ["variable" ["field-id" (mt/id :venues :id)]]
+                                                                        :value  5}}}]
+      (is (= {"venue_id" {:type   :category
+                          :target [:variable [:field-id (mt/id :venues :id)]]
+                          :value  5}}
+             (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap)))))
 
-;; apparently sometimes they are saved with just the target, but not type or value? Make sure these get normalized
-;; correctly.
-(expect
-  {"user" [:variable [:field-id (data/id :venues :id)]]}
-  (tt/with-temp GroupTableAccessPolicy [gtap {:table_id             (data/id :venues)
-                                              :group_id             (u/get-id (group/all-users))
-                                              :attribute_remappings {"user" ["variable" ["field-id" (data/id :venues :id)]]}}]
-    (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap))))
+    (testing (str "apparently sometimes they are saved with just the target, but not type or value? Make sure these "
+                  "get normalized correctly.")
+      (mt/with-temp GroupTableAccessPolicy [gtap {:table_id             (mt/id :venues)
+                                                  :group_id             (u/get-id (group/all-users))
+                                                  :attribute_remappings {"user" ["variable" ["field-id" (mt/id :venues :id)]]}}]
+        (is (= {"user" [:variable [:field-id (mt/id :venues :id)]]}
+               (db/select-one-field :attribute_remappings GroupTableAccessPolicy :id (u/get-id gtap))))))))
+
+(deftest disallow-changing-table-id-test
+  (testing "You can't change the table_id of a GTAP after it has been created."
+    (mt/with-temp GroupTableAccessPolicy [gtap {:table_id (mt/id :venues)
+                                                :group_id (u/get-id (group/all-users))}]
+      (is (thrown-with-msg?
+           clojure.lang.ExceptionInfo
+           #"You cannot change the Table ID of a GTAP once it has been created"
+           (db/update! GroupTableAccessPolicy (:id gtap) :table_id (mt/id :checkins)))))))
+
+(deftest disallow-queries-that-add-columns-test
+  (testing "Don't allow saving a Sandboxing query that contains columns not in the Table it replaces (#13715)"
+    (doseq [[msg f] {"Create a new GTAP"
+                     (fn [query]
+                       (mt/with-temp* [Card                   [card {:dataset_query   query
+                                                                     :result_metadata (qp/query->expected-cols query)}]
+                                       GroupTableAccessPolicy [gtap {:table_id (mt/id :venues)
+                                                                     :group_id (u/get-id (group/all-users))
+                                                                     :card_id  (:id card)}]]
+                         :ok))
+
+                     "Update an existing GTAP"
+                     (fn [query]
+                       (mt/with-temp* [Card                   [card {:dataset_query   query
+                                                                     :result_metadata (qp/query->expected-cols query)}]
+                                       GroupTableAccessPolicy [gtap {:table_id (mt/id :venues)
+                                                                     :group_id (u/get-id (group/all-users))}]]
+                         (db/update! GroupTableAccessPolicy (:id gtap) :card_id (:id card))
+                         :ok))
+
+                     "Update query for Card associated with an existing GTAP"
+                     (fn [query]
+                       (mt/with-temp* [Card                   [card {:dataset_query   (mt/mbql-query :venues)
+                                                                     :result_metadata (qp/query->expected-cols (mt/mbql-query :venues))}]
+                                       GroupTableAccessPolicy [gtap {:table_id (mt/id :venues)
+                                                                     :group_id (u/get-id (group/all-users))
+                                                                     :card_id  (:id card)}]]
+                         (db/update! Card (:id card) :dataset_query query)
+                         :ok))}]
+      (testing (str "\n" msg "\n")
+        (testing "sanity check"
+          (is (= :ok
+                 (f (mt/mbql-query venues)))))
+        (testing "adding new columns = not ok"
+          (is (thrown-with-msg?
+               clojure.lang.ExceptionInfo
+               #"Sandbox Cards can't return columns that arent present in the Table they are sandboxing"
+               (f (mt/mbql-query checkins)))))
+        (testing "removing columns = ok"
+          (is (= :ok
+                 (f (mt/mbql-query venues {:fields [$id $name]})))))
+        (testing "changing order of columns = ok"
+          (is (= :ok
+                 (f (mt/mbql-query venues
+                      {:fields (for [id (shuffle (map :id (qp/query->expected-cols (mt/mbql-query venues))))]
+                                 [:field-id id])})))))))))
+
+(deftest disallow-queries-that-change-types-test
+  (testing "Don't allow saving a Sandboxing query that changes the type of a column vs. the type in the Table it replaces (#13715)"
+    (doseq [[msg f] {"Create a new GTAP"
+                     (fn [metadata]
+                       (mt/with-temp* [Card                   [card {:dataset_query   (mt/mbql-query :venues)
+                                                                     :result_metadata metadata}]
+                                       GroupTableAccessPolicy [gtap {:table_id (mt/id :venues)
+                                                                     :group_id (u/get-id (group/all-users))
+                                                                     :card_id  (:id card)}]]
+                         :ok))
+
+                     "Update an existing GTAP"
+                     (fn [metadata]
+                       (mt/with-temp* [Card                   [card {:dataset_query   (mt/mbql-query :venues)
+                                                                     :result_metadata metadata}]
+                                       GroupTableAccessPolicy [gtap {:table_id (mt/id :venues)
+                                                                     :group_id (u/get-id (group/all-users))}]]
+                         (db/update! GroupTableAccessPolicy (:id gtap) :card_id (:id card))
+                         :ok))
+
+                     "Update query for Card associated with an existing GTAP"
+                     (fn [metadata]
+                       (mt/with-temp* [Card                   [card {:dataset_query   (mt/mbql-query :venues)
+                                                                     :result_metadata (qp/query->expected-cols (mt/mbql-query :venues))}]
+                                       GroupTableAccessPolicy [gtap {:table_id (mt/id :venues)
+                                                                     :group_id (u/get-id (group/all-users))
+                                                                     :card_id  (:id card)}]]
+                         (db/update! Card (:id card) :result_metadata metadata)
+                         :ok))}]
+      (testing (str "\n" msg "\n")
+        (is (thrown-with-msg?
+             clojure.lang.ExceptionInfo
+             #"Sandbox Cards can't return columns that have different types than the Table they are sandboxing"
+             (f (-> (vec (qp/query->expected-cols (mt/mbql-query venues)))
+                    (assoc-in [0 :base_type] :type/Text)))))
+        (testing "type changes to a descendant type = ok"
+          (is (= :ok
+                 (f
+                  (-> (vec (qp/query->expected-cols (mt/mbql-query venues)))
+                      (assoc-in [0 :base_type] :type/BigInteger))))))))))
