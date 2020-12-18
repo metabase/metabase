539756bd42af0bd24b329ef7ecaa5625e73e90ed
Cam Saul <github@camsaul.com>
2020-12-17 16:53:03 -0800

    Rework test

--- test/metabase/pulse/render_test.clj
+++ test/metabase/pulse/render_test.clj
@@ -1,13 +1,12 @@
 (ns metabase.pulse.render-test
-  (:require [expectations :refer [expect]]
+  (:require [clojure.test :refer :all]
             [metabase
              [pulse :as pulse]
-             [query-processor :as qp]]
+             [query-processor :as qp]
+             [test :as mt]]
             [metabase.mbql.util :as mbql.u]
             [metabase.models.card :refer [Card]]
-            [metabase.pulse.render :as render]
-            [metabase.test.data :as data]
-            [toucan.util.test :as tt]))
+            [metabase.pulse.render :as render]))
 
 ;; Let's make sure rendering Pulses actually works
 
@@ -19,13 +18,13 @@
    (render/render-pulse-card-for-display (pulse/defaulted-timezone card) card results)))
 
 (defn- render-results [query]
-  (tt/with-temp Card [card {:dataset_query query}]
+  (mt/with-temp Card [card {:dataset_query query}]
     (render-pulse-card card)))
 
-;; if the pulse rendered correctly it will have this one row that says "November 2015" (not sure why)
-(expect
-  (mbql.u/match-one (render-results
-                     (data/mbql-query checkins
-                       {:aggregation  [[:count]]
-                        :breakout     [!month.date]}))
-    [:td _ "November 2015"]))
+(deftest render-test
+  (testing "if the pulse rendered correctly it will have this one row that says \"November 2015\" (not sure why)"
+    (is (some? (mbql.u/match-one (render-results
+                                  (mt/mbql-query checkins
+                                    {:aggregation [[:count]]
+                                     :breakout    [!month.date]}))
+                 [:td _ "November 2015"])))))
