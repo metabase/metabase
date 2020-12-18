0f67a7c19f0ce7c9c7aa7fbe198baa1271d55661
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2020-12-18 02:58:34 +0100

    Remove new user onboarding modal and related component

--- frontend/src/metabase/home/containers/HomepageApp.jsx
+++ frontend/src/metabase/home/containers/HomepageApp.jsx
@@ -5,13 +5,11 @@ import { t } from "ttag";
 import { push } from "react-router-redux";
 
 import Greeting from "metabase/lib/greeting";
-import Modal from "metabase/components/Modal";
 import Card from "metabase/components/Card";
 import Subhead from "metabase/components/type/Subhead";
 
 import Activity from "../components/Activity";
 import RecentViews from "../components/RecentViews";
-import NewUserOnboardingModal from "../components/NewUserOnboardingModal";
 import NextStep from "../components/NextStep";
 
 import * as homepageActions from "../actions";
@@ -23,7 +21,6 @@ const mapStateToProps = (state, props) => ({
   activity: getActivity(state),
   recentViews: getRecentViews(state),
   user: getUser(state),
-  showOnboarding: "new" in props.location.query,
 });
 
 const mapDispatchToProps = {
@@ -38,7 +35,6 @@ const mapDispatchToProps = {
 export default class HomepageApp extends Component {
   static propTypes = {
     onChangeLocation: PropTypes.func.isRequired,
-    showOnboarding: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
     // TODO - these should be used by their call sites rather than passed
     activity: PropTypes.array,
@@ -52,27 +48,12 @@ export default class HomepageApp extends Component {
     super(props);
     this.state = {
       greeting: Greeting.sayHello(props.user && props.user.first_name),
-      onboarding: props.showOnboarding,
     };
   }
 
-  completeOnboarding() {
-    this.setState({ onboarding: false });
-  }
-
   render() {
-    const { user } = this.props;
-
     return (
       <Box mx={4}>
-        {this.state.onboarding ? (
-          <Modal>
-            <NewUserOnboardingModal
-              user={user}
-              onClose={() => this.completeOnboarding()}
-            />
-          </Modal>
-        ) : null}
         <Box py={3}>
           <Subhead>{t`Activity`}</Subhead>
         </Box>
