320c0885a1e5f5a4abb8b39e728d550811d29460
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2020-12-18 04:47:02 +0100

    Convert `DateRangeWidget.unit.spec.js`

--- frontend/test/metabase/parameters/components/widgets/DateRangeWidget.unit.spec.js
+++ frontend/test/metabase/parameters/components/widgets/DateRangeWidget.unit.spec.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { mount } from "enzyme";
+import { fireEvent, render, screen } from "@testing-library/react";
 import DateRangeWidget from "metabase/parameters/components/widgets/DateRangeWidget";
 
 describe("DateRangeWidget", () => {
   it("should allow selections spanning years", () => {
     const setValue = jest.fn();
-    const picker = mount(
+    render(
       <DateRangeWidget value={"2018-12-01~2018-12-01"} setValue={setValue} />,
     );
-    picker
-      .find(".Calendar-day.Calendar-day--this-month")
-      .first()
-      .simulate("click");
-    picker.find(".Icon-chevronright").simulate("click");
-    picker
-      .find(".Calendar-day.Calendar-day--this-month")
-      .first()
-      .simulate("click");
+    const NEXT = screen.getByRole("img", { name: /chevronright icon/i });
+    // There can be day 1 in the next month as well. We want to target this month's day 1 only.
+    const FIRST_DAY = screen.getAllByText("1")[0];
+
+    FIRST_DAY.click();
+    fireEvent.click(NEXT);
+    FIRST_DAY.click();
+
     expect(setValue).toHaveBeenCalledWith("2018-12-01~2019-01-01");
   });
 });
