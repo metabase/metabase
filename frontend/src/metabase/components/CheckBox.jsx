6e0b21b613a3e4422253aaba97f3d7eb06a3222a
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2020-12-22 14:32:42 +0100

    Add aria roles to our custom checkbox
    https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/checkbox_role

--- frontend/src/metabase/components/CheckBox.jsx
+++ frontend/src/metabase/components/CheckBox.jsx
@@ -67,6 +67,8 @@ export default class CheckBox extends Component {
         onClick={e => {
           this.onClick(e);
         }}
+        role="checkbox"
+        aria-checked={checked}
       >
         {(checked || indeterminate) && !noIcon && (
           <Icon
