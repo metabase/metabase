035963803798e883103fd58a49b4ffe961f45c61
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2020-12-24 20:48:09 +0100

    Add todo note to `DashCard.unit.spec.js`
    The test was already skipped.

--- frontend/test/metabase/dashboard/DashCard.unit.spec.js
+++ frontend/test/metabase/dashboard/DashCard.unit.spec.js
@@ -4,6 +4,7 @@ jest.unmock("metabase/components/ExplicitSize");
 
 import React from "react";
 import renderer from "react-test-renderer";
+// TODO: re-write using React Testing Library (Enzyme is removed!)
 import { render } from "enzyme";
 import { assocIn } from "icepick";
 
