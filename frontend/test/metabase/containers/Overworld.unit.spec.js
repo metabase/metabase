cc42476eef4a5b2c4d4c6b7d518b013a8431f9f7
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-01-05 21:28:25 +0100

    Use more descriptive const names

--- frontend/test/metabase/containers/Overworld.unit.spec.js
+++ frontend/test/metabase/containers/Overworld.unit.spec.js
@@ -7,8 +7,8 @@ import {
 import "@testing-library/jest-dom/extend-expect";
 import { fireEvent, render, screen } from "@testing-library/react";
 
-const STRING_1 = "Your team's most important dashboards go here";
-const STRING_2 =
+const PIN_MESSAGE_DESCRIPTION = "Your team's most important dashboards go here";
+const PIN_MESSAGE_HINT =
   "Pin dashboards in Our analytics to have them appear in this space for everyone";
 
 describe("AdminPinMessage", () => {
@@ -38,9 +38,9 @@ describe("AdminPinMessage", () => {
 });
 
 function assertPresence() {
-  screen.getByText(STRING_1);
+  screen.getByText(PIN_MESSAGE_DESCRIPTION);
   // Note: the following text is broken between elements. Thus, we use `findBy` instead of `getBy`
-  screen.findByText(STRING_2);
+  screen.findByText(PIN_MESSAGE_HINT);
 
   screen.getByRole("img", { name: /dashboard/i });
   screen.getByRole("img", { name: /close/i });
@@ -50,8 +50,8 @@ function assertAbsence() {
   const DASHBOARD_ICON = screen.queryByRole("img", { name: /dashboard/i });
   const CLOSE_ICON = screen.queryByRole("img", { name: /close/i });
 
-  expect(screen.queryByText(STRING_1)).not.toBeInTheDocument();
-  expect(screen.queryByText(STRING_2)).not.toBeInTheDocument();
+  expect(screen.queryByText(PIN_MESSAGE_DESCRIPTION)).not.toBeInTheDocument();
+  expect(screen.queryByText(PIN_MESSAGE_HINT)).not.toBeInTheDocument();
   expect(CLOSE_ICON).not.toBeInTheDocument();
   expect(DASHBOARD_ICON).not.toBeInTheDocument();
 }
