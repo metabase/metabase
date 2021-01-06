a7a983b5e7eb5636264b1071c1f7fd42a41f6361
Ariya Hidayat <ariya@metabase.com>
2021-01-06 14:17:21 -0800

    Revamp the grammar of custom expression (#14235)
    * Revamp the grammar of custom expression
    
    Handle logical operations (AND, OR, NOT) by having them as part of the
    grammar productions/rules, instead of only as special cases of
    filters/aggregations.
    
    This finally allows any arithmetic operations inside a filter. In fact,
    this lets the correct mix-up between any logical and arithmetic
    operations (including function calls), also by leveraging the type
    checker to determine the correct type constraints for the terminals (Booleans,
    expressions, dimensions, metrics, segments, etc).
    
    * Be more explicit about skipped (known) regression

--- frontend/src/metabase/lib/expressions/typechecker.js
+++ frontend/src/metabase/lib/expressions/typechecker.js
@@ -26,6 +26,34 @@ export function typeCheck(cst, rootType) {
       return result;
     }
 
+    logicalOrExpression(ctx) {
+      const type = ctx.operands.length > 1 ? "boolean" : this.typeStack[0];
+      this.typeStack.unshift(type);
+      const result = super.logicalOrExpression(ctx);
+      this.typeStack.shift();
+      return result;
+    }
+    logicalAndExpression(ctx) {
+      const type = ctx.operands.length > 1 ? "boolean" : this.typeStack[0];
+      this.typeStack.unshift(type);
+      const result = super.logicalAndExpression(ctx);
+      this.typeStack.shift();
+      return result;
+    }
+    logicalNotExpression(ctx) {
+      this.typeStack.unshift("boolean");
+      const result = super.logicalNotExpression(ctx);
+      this.typeStack.shift();
+      return result;
+    }
+    relationalExpression(ctx) {
+      const type = ctx.operands.length > 1 ? "expression" : this.typeStack[0];
+      this.typeStack.unshift(type);
+      const result = super.relationalExpression(ctx);
+      this.typeStack.shift();
+      return result;
+    }
+
     // TODO check for matching argument signature
     functionExpression(ctx) {
       const args = ctx.arguments || [];
@@ -50,27 +78,6 @@ export function typeCheck(cst, rootType) {
         }
       }
       return super.dimensionExpression(ctx);
-    }
-
-    booleanExpression(ctx) {
-      const type = ctx.operands.length > 1 ? rootType : this.typeStack[0];
-      this.typeStack.unshift(type);
-      const result = super.booleanExpression(ctx);
-      this.typeStack.shift();
-      return result;
-    }
-    comparisonExpression(ctx) {
-      this.typeStack.unshift("expression");
-      const result = super.comparisonExpression(ctx);
-      this.typeStack.shift();
-      return result;
-    }
-    booleanUnaryExpression(ctx) {
-      const type = ctx.operands.length > 1 ? rootType : this.typeStack[0];
-      this.typeStack.unshift(type);
-      const result = super.booleanUnaryExpression(ctx);
-      this.typeStack.shift();
-      return result;
     }
   }
   new TypeChecker().visit(cst);
