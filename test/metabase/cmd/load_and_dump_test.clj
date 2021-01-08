aa22371b909dd365d3c3c28bdb5d9a344659d91f
Raimon Grau <raimonster@gmail.com>
2021-01-08 13:21:33 +0000

    Fix test on h2 db
    When testing with h2 driver as the destination db, special case the
    jdbc spec.
    
    H2 being in memory makes that the usual jdbc connection that would be
    created would be ephemeral, so when doing the dump, and comparing
    again against the first, the db would be gone. Adding a CLOSE_DELAY=10
    makes it stick for a little while so we can do the comparison.
    
    not calling `tx/create-db!` in that case because in h2, db is always
    created already.

--- test/metabase/cmd/load_and_dump_test.clj
+++ test/metabase/cmd/load_and_dump_test.clj
@@ -5,15 +5,15 @@
             [metabase.cmd.copy.h2 :as h2]
             [metabase.cmd.dump-to-h2 :as dump-to-h2]
             [metabase.cmd.load-from-h2 :as load-from-h2]
-            [metabase.cmd.migrate :as migrate]
             [metabase.db.connection :as mdb.connection]
+            [metabase.db.setup :as mdb.setup]
+            [metabase.db.spec :as db.spec]
             [metabase.driver :as driver]
-            [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
-            [metabase.models.user :refer [User]]
+            [metabase.models.setting :as setting]
             [metabase.test :as mt]
             [metabase.test.data.interface :as tx]
-            [metabase.test.util :as tu]
-            [toucan.db :as db]))
+            [toucan.db :as db]
+            [toucan.util.test :as tt]))
 
 (defn- abs-path
   [path]
@@ -22,15 +22,22 @@
 (deftest load-and-dump-test
   (testing "loading data to h2 and porting to DB and migrating back to H2"
     (let [h2-fixture-db-file (abs-path "frontend/test/__runner__/test_db_fixture.db")
-          h2-file (abs-path "frontend/test/__runner__/test_db_fixture2.db")
-          db-name "dump-test"]
+          h2-file            (abs-path "/tmp/out.db")
+          db-name            "dump-test"]
       (mt/test-drivers #{:postgres :mysql :h2}
         (h2/delete-existing-h2-database-files! h2-file)
-        (binding [mdb.connection/*db-type* driver/*driver*
-                  mdb.connection/*jdbc-spec* (sql-jdbc.conn/connection-details->spec driver/*driver*
-                                               (tx/dbdef->connection-details driver/*driver*
-                                                                             :db {:database-name db-name}))]
-          (tx/create-db! driver/*driver* {:database-name db-name})
+        (binding [mdb.connection/*db-type*   driver/*driver*
+                  mdb.connection/*jdbc-spec* (if (= driver/*driver* :h2)
+                                               {:subprotocol "h2"
+                                                :subname     (format "mem:%s;DB_CLOSE_DELAY=10" (mt/random-name))
+                                                :classname   "org.h2.Driver"}
+                                               (let [details (tx/dbdef->connection-details driver/*driver*
+                                                                                           :db {:database-name db-name})]
+                                                 ((case driver/*driver*
+                                                    :postgres db.spec/postgres
+                                                    :mysql    db.spec/mysql) details)))]
+          (when-not (= driver/*driver* :h2)
+            (tx/create-db! driver/*driver* {:database-name db-name}))
           (load-from-h2/load-from-h2! h2-fixture-db-file)
           (dump-to-h2/dump-to-h2! h2-file)
           (is (not (compare-h2-dbs/different-contents?
