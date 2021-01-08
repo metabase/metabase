a683fd1fb5f79b51c19913ed0cf912981df8f43b
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-01-08 21:48:52 +0100

    Add repro for #14294

--- frontend/test/metabase/scenarios/admin/settings/localization.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/localization.cy.spec.js
@@ -81,6 +81,55 @@ describe("scenarios > admin > permissions", () => {
     cy.get(".axis.x").contains(/monday/i);
     cy.get(".axis.x").contains(/tuesday/i);
   });
+
+  // HANDLE WITH CARE!
+  // This test is extremely tricky and fragile because it needs to test for the "past X weeks" to check if week starts on Sunday or Monday.
+  // As the time goes by we're risking that past X weeks don't yield any result when applied to the sample dataset.
+  // For that reason I've chosen the past 220 weeks (mid-October 2016). This should give us 3+ years to run this test without updates.
+  // If there is a week in sample dataset which doesn't have Sunday or Monday, that can potentially break this test.
+  // Going randomly through at least 10-15 different numbers, each week had both Sunday and Monday so we should be ok.
+
+  // TODO:
+  //  - Keep an eye on this test in CI and update the week range as needed.
+  it.skip("should respect start of the week in SQL questions with filters (metabase#14294)", () => {
+    cy.request("POST", "/api/card", {
+      name: "14294",
+      dataset_query: {
+        database: 1,
+        native: {
+          "template-tags": {
+            date_range: {
+              id: "93961154-c3d5-7c93-7b59-f4e494fda499",
+              name: "date_range",
+              "display-name": "Date range",
+              type: "dimension",
+              dimension: ["field-id", ORDERS.CREATED_AT],
+              "widget-type": "date/all-options",
+              default: "past220weeks",
+              required: true,
+            },
+          },
+          query:
+            "select ID, CREATED_AT, dayname(CREATED_AT) as CREATED_AT_DAY\nfrom ORDERS \n[[where {{date_range}}]]\norder by CREATED_AT",
+        },
+        type: "native",
+      },
+      display: "table",
+      visualization_settings: {},
+    }).then(({ body: { id: QUESTION_ID } }) => {
+      cy.visit(`/question/${QUESTION_ID}`);
+      cy.get(".TableInteractive-header")
+        .next()
+        .as("resultTable");
+
+      cy.get("@resultTable").within(() => {
+        // The third cell in the first row (CREATED_AT_DAY)
+        cy.get(".cellData")
+          .eq(2)
+          .should("not.contain", "Sunday");
+      });
+    });
+  });
 });
 
 function setFirstWeekDayTo(day) {
