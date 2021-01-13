e535af7a391471b0aa07e761e4d95bf463d25425
Robert Roland <rob@metabase.com>
2021-01-13 11:31:14 -0800

    removing function

--- src/metabase/api/pivot.clj
+++
@@ -1,9 +0,0 @@
-(ns metabase.api.pivot
-  (:require [metabase.util.i18n :refer [tru]]))
-
-(defn check-query-type
-  "Check that a query type is of a specific type, for example pivot tables require MBQL queries"
-  [expected-type query]
-  (let [found-type (:type query)]
-    (when (not= expected-type found-type)
-      (throw (ex-info (tru "Queries must be of type ''{0}'', found ''{1}''" expected-type found-type) {:type found-type})))))
