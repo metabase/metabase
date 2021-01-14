b2b344a50cbf2edb307b89e2ef309a427055de75
Tim Macdonald <tsmacdonald@gmail.com>
2021-01-14 10:17:45 -0600

    Revert frontend changes from 8fd4f2af6fdcaa02e88ec2163280ddb1a981a086 (#14392)
    These snuck in by mistake in #14300

--- frontend/test/metabase/scenarios/dashboard/subscriptions.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard/subscriptions.cy.spec.js
@@ -1,51 +1,108 @@
-import { restore, signInAsAdmin, USERS } from "__support__/cypress";
+import {
+  restore,
+  signInAsAdmin,
+  setupDummySMTP,
+  USERS,
+} from "__support__/cypress";
 const { admin } = USERS;
 
-describe("scenarios > dashboard", () => {
+describe("scenarios > dashboard > subscriptions", () => {
   beforeEach(() => {
     restore();
     signInAsAdmin();
-    // Dummy SMTP setup
-    cy.request("PUT", "/api/setting", {
-      "email-smtp-host": "smtp.foo.test",
-      "email-smtp-port": "587",
-      "email-smtp-security": "none",
-      "email-smtp-username": "nevermind",
-      "email-smtp-password": "it-is-secret-NOT",
-      "email-from-address": "nonexisting@metabase.test",
+  });
+
+  it("should not allow creation if there are no dashboard cards", () => {
+    cy.log("**Create fresh new dashboard**");
+    cy.request("POST", "/api/dashboard", {
+      name: "Empty Dashboard",
+    }).then(({ body: { id: DASHBOARD_ID } }) => {
+      cy.visit(`/dashboard/${DASHBOARD_ID}`);
+    });
+    // It would be great if we can use either aria-attributes or better class naming to suggest when icons are disabled
+    cy.get(".Icon-share")
+      .closest("a")
+      .should("have.class", "cursor-default");
+  });
+
+  describe("with no channels set up", () => {
+    it("should instruct user to connect email or slack", () => {
+      openDashboardSubscriptions();
+      // Look for the messaging about configuring slack and email
+      cy.findByRole("link", { name: /set up email/i });
+      cy.findByRole("link", { name: /configure Slack/i });
     });
   });
 
-  it.skip("should persist attachments for dashboard subscriptions (metabase#14117)", () => {
-    // Orders in a dashboard
-    cy.visit("/dashboard/1");
-    cy.get(".Icon-share").click();
-    cy.findByText("Dashboard subscriptions").click();
-    cy.findByText("Email it").click();
-    cy.findByPlaceholderText("Enter user names or email addresses")
-      .click()
-      .type(`${admin.first_name} ${admin.last_name}{enter}`);
-    // This is extremely fragile
-    // TODO: update test once changes from `https://github.com/metabase/metabase/pull/14121` are merged into `master`
-    cy.findByText("Attach results")
-      .parent()
-      .parent()
-      .next()
-      .find("a") // Toggle
-      .click();
-    cy.findByText("Questions to attach").click();
-    cy.contains("Done")
-      .closest(".Button")
-      .should("not.be.disabled")
-      .click();
-    cy.findByText("Subscriptions");
-    cy.findByText("Emailed daily at 8:00 AM").click();
-    cy.findByText("Delete this subscription").scrollIntoView();
-    cy.findByText("Questions to attach");
-    cy.findAllByRole("listitem")
-      .contains("Orders") // yields the whole <li> element
-      .within(() => {
-        cy.findByRole("checkbox").should("have.attr", "aria-checked", "true");
+  describe("with email and slack set up", () => {
+    beforeEach(() => {
+      setupDummySMTP();
+    });
+
+    describe("with no existing subscriptions", () => {
+      it("should allow creation of a new email subscription", () => {
+        createEmailSubscription();
+        cy.findByText("Emailed daily at 8:00 AM");
       });
+    });
+
+    describe("with existing subscriptions", () => {
+      beforeEach(createEmailSubscription);
+      it("should show existing dashboard subscriptions", () => {
+        openDashboardSubscriptions();
+        cy.findByText("Emailed daily at 8:00 AM");
+      });
+    });
+
+    it("should persist attachments for dashboard subscriptions (metabase#14117)", () => {
+      assignRecipient();
+      // This is extremely fragile
+      // TODO: update test once changes from `https://github.com/metabase/metabase/pull/14121` are merged into `master`
+      cy.findByText("Attach results")
+        .parent()
+        .parent()
+        .next()
+        .find("a") // Toggle
+        .click();
+      cy.findByText("Questions to attach").click();
+      clickButton("Done");
+      cy.findByText("Subscriptions");
+      cy.findByText("Emailed daily at 8:00 AM").click();
+      cy.findByText("Delete this subscription").scrollIntoView();
+      cy.findByText("Questions to attach");
+      cy.findAllByRole("listitem")
+        .contains("Orders") // yields the whole <li> element
+        .within(() => {
+          cy.findByRole("checkbox").should("have.attr", "aria-checked", "true");
+        });
+    });
   });
 });
+
+// Helper functions
+function openDashboardSubscriptions(dashboard_id = 1) {
+  // Orders in a dashboard
+  cy.visit(`/dashboard/${dashboard_id}`);
+  cy.get(".Icon-share").click();
+  cy.findByText("Dashboard subscriptions").click();
+}
+
+function assignRecipient(user = admin) {
+  openDashboardSubscriptions();
+  cy.findByText("Email it").click();
+  cy.findByPlaceholderText("Enter user names or email addresses")
+    .click()
+    .type(`${user.first_name} ${user.last_name}{enter}`);
+}
+
+function clickButton(button_name) {
+  cy.contains(button_name)
+    .closest(".Button")
+    .should("not.be.disabled")
+    .click();
+}
+
+function createEmailSubscription() {
+  assignRecipient();
+  clickButton("Done");
+}
