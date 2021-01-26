df195b76f918036d96676896d17fcf407d46a567
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-01-26 01:11:59 +0100

    Expand test to include hosted instances

--- frontend/test/metabase/nav/ProfileLink.unit.spec.js
+++ frontend/test/metabase/nav/ProfileLink.unit.spec.js
@@ -4,57 +4,66 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import MetabaseSettings from "metabase/lib/settings";
 import ProfileLink from "metabase/nav/components/ProfileLink";
 
-const OPTIONS = [
+const REGULAR_ITEMS = [
   "Account settings",
   "Activity",
   "Help",
   "About Metabase",
   "Sign out",
 ];
-const ADMIN_OPTIONS = [...OPTIONS, "Admin"];
+const ADMIN_ITEMS = [...REGULAR_ITEMS, "Admin"];
+const HOSTED_ITEMS = [...ADMIN_ITEMS, "Manage hosting"];
 
 describe("ProfileLink", () => {
   describe("options", () => {
-    beforeEach(() => {
-      jest.spyOn(MetabaseSettings, "isHosted");
-      MetabaseSettings.isHosted = jest.fn(() => false);
-    });
-
-    afterEach(() => {
-      MetabaseSettings.isHosted.mockRestore();
-    });
-    describe("normal user", () => {
-      it("should show the proper set of items", () => {
-        const normalUser = { is_superuser: false };
-        render(<ProfileLink user={normalUser} context={""} />);
-
-        const SETTINGS = screen.getByRole("img", { name: /gear/i });
-        fireEvent.click(SETTINGS);
-
-        OPTIONS.forEach(title => {
-          screen.getByText(title);
+    ["regular", "hosted"].forEach(testCase => {
+      describe(`${testCase} instance`, () => {
+        beforeEach(() => {
+          jest.spyOn(MetabaseSettings, "isHosted");
+          MetabaseSettings.isHosted = jest.fn(() => false);
         });
 
-        expect(screen.getAllByRole("listitem").length).toEqual(OPTIONS.length);
-      });
-    });
-
-    describe("admin", () => {
-      it("should show the proper set of items", () => {
-        const admin = { is_superuser: true };
-        render(<ProfileLink user={admin} context={""} />);
-
-        const SETTINGS = screen.getByRole("img", { name: /gear/i });
-        fireEvent.click(SETTINGS);
-
-        ADMIN_OPTIONS.forEach(title => {
-          screen.getByText(title);
+        afterEach(() => {
+          MetabaseSettings.isHosted.mockRestore();
         });
 
-        expect(screen.getAllByRole("listitem").length).toEqual(
-          ADMIN_OPTIONS.length,
-        );
+        if (testCase === "hosted") {
+          beforeEach(() => {
+            MetabaseSettings.isHosted = jest.fn(() => true);
+          });
+        }
+
+        describe("normal user", () => {
+          it("should show the proper set of items", () => {
+            const normalUser = { is_superuser: false };
+            render(<ProfileLink user={normalUser} context={""} />);
+
+            assertOn(REGULAR_ITEMS);
+          });
+        });
+
+        describe("admin", () => {
+          it("should show the proper set of items", () => {
+            const admin = { is_superuser: true };
+            render(<ProfileLink user={admin} context={""} />);
+
+            testCase === "hosted"
+              ? assertOn(HOSTED_ITEMS)
+              : assertOn(ADMIN_ITEMS);
+          });
+        });
       });
     });
   });
 });
+
+function assertOn(items) {
+  const SETTINGS = screen.getByRole("img", { name: /gear/i });
+  fireEvent.click(SETTINGS);
+
+  items.forEach(title => {
+    screen.getByText(title);
+  });
+
+  expect(screen.getAllByRole("listitem").length).toEqual(items.length);
+}
