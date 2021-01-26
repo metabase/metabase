c8a9403f1c6b1ea4bb3a446c0307c1221d9407fb
Maz Ameli <maz@metabase.com>
2021-01-26 15:58:01 -0800

    use the right bg color for ProfileLink's hover state

--- frontend/src/metabase/nav/components/ProfileLink.jsx
+++ frontend/src/metabase/nav/components/ProfileLink.jsx
@@ -94,6 +94,7 @@ export default class ProfileLink extends Component {
 
   render() {
     const { modalOpen } = this.state;
+    const adminContext = this.props.context === "admin";
     const { tag, date, ...versionExtra } = MetabaseSettings.get("version");
     // don't show trademark if application name is whitelabeled
     const showTrademark = t`Metabase` === "Metabase";
@@ -105,7 +106,9 @@ export default class ProfileLink extends Component {
           triggerIcon="gear"
           triggerProps={{
             hover: {
-              backgroundColor: darken(color("brand")),
+              backgroundColor: adminContext
+                ? darken(color("accent7"))
+                : darken(color("brand")),
               color: "white",
             },
           }}
