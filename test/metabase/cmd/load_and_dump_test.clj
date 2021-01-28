d2e706eca7718018e32e778efcc3ea96824d8b25
Raimon Grau <raimonster@gmail.com>
2021-01-28 13:15:53 +0000

    disable settings cache to fix flakiness (#14541)

--- test/metabase/cmd/load_and_dump_test.clj
+++ test/metabase/cmd/load_and_dump_test.clj
@@ -6,14 +6,12 @@
             [metabase.cmd.dump-to-h2 :as dump-to-h2]
             [metabase.cmd.load-from-h2 :as load-from-h2]
             [metabase.db.connection :as mdb.connection]
-            [metabase.db.setup :as mdb.setup]
             [metabase.db.spec :as db.spec]
             [metabase.driver :as driver]
             [metabase.models.setting :as setting]
+            [metabase.util.i18n.impl :as i18n.impl]
             [metabase.test :as mt]
-            [metabase.test.data.interface :as tx]
-            [toucan.db :as db]
-            [toucan.util.test :as tt]))
+            [metabase.test.data.interface :as tx]))
 
 (defn- abs-path
   [path]
@@ -24,9 +22,10 @@
     (let [h2-fixture-db-file (abs-path "frontend/test/__runner__/test_db_fixture.db")
           h2-file            (abs-path "/tmp/out.db")
           db-name            "dump-test"]
-      (mt/test-drivers #{:postgres :mysql :h2}
+      (mt/test-drivers #{:mysql :postgres :h2}
         (h2/delete-existing-h2-database-files! h2-file)
-        (binding [mdb.connection/*db-type*   driver/*driver*
+        (binding [setting/*disable-cache* true
+                  mdb.connection/*db-type*   driver/*driver*
                   mdb.connection/*jdbc-spec* (if (= driver/*driver* :h2)
                                                {:subprotocol "h2"
                                                 :subname     (format "mem:%s;DB_CLOSE_DELAY=10" (mt/random-name))
@@ -36,10 +35,11 @@
                                                  ((case driver/*driver*
                                                     :postgres db.spec/postgres
                                                     :mysql    db.spec/mysql) details)))]
-          (when-not (= driver/*driver* :h2)
-            (tx/create-db! driver/*driver* {:database-name db-name}))
-          (load-from-h2/load-from-h2! h2-fixture-db-file)
-          (dump-to-h2/dump-to-h2! h2-file)
-          (is (not (compare-h2-dbs/different-contents?
-                    h2-file
-                    h2-fixture-db-file))))))))
+          (with-redefs [i18n.impl/site-locale-from-setting-fn (atom (constantly false))]
+            (when-not (= driver/*driver* :h2)
+             (tx/create-db! driver/*driver* {:database-name db-name}))
+           (load-from-h2/load-from-h2! h2-fixture-db-file)
+           (dump-to-h2/dump-to-h2! h2-file)
+           (is (not (compare-h2-dbs/different-contents?
+                     h2-file
+                     h2-fixture-db-file)))))))))
