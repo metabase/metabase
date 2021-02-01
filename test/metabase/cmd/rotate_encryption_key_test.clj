5ee47ab0a679359b067f61dbbb2e90611b0c7a51
Raimon Grau <raimonster@gmail.com>
2021-02-01 11:44:15 +0000

    make sure we start from clean encryption key

--- test/metabase/cmd/rotate_encryption_key_test.clj
+++ test/metabase/cmd/rotate_encryption_key_test.clj
@@ -33,52 +33,53 @@
                              ["select value from setting where setting.key=?;" key]))))
 
 (deftest rotate-keys!-test
-  (let [h2-fixture-db-file (abs-path "frontend/test/__runner__/test_db_fixture.db")
-        db-name (str "test_" (mt/random-name))
-        [k1 k2 k3] ["89ulvIGoiYw6mNELuOoEZphQafnF/zYe+3vT+v70D1A="
-                    "yHa/6VEQuIItMyd5CNcgV9nXvzZcX6bWmiY0oOh6pLU="
-                    "BCQbKNVu6N8TQ2BwyTC0U0oCBqsvFVr2uhEM/tRgJUM="]]
-    (mt/test-drivers #{:mysql :postgres :h2}
-      (binding [mdb.connection/*db-type*   driver/*driver*
-                mdb.connection/*jdbc-spec* (persistent-jdbcspec driver/*driver* db-name)
-                db/*db-connection* (persistent-jdbcspec driver/*driver* db-name)
-                db/*quoting-style* driver/*driver*]
-        (when-not (= driver/*driver* :h2)
-          (tx/create-db! driver/*driver* {:database-name db-name}))
-        (load-from-h2/load-from-h2! h2-fixture-db-file)
-        (db/insert! Setting {:key "setting0", :value "val0"})
-        (eu/with-secret-key k1
-          (db/insert! Setting {:key "setting1", :value "val1"})
-          (db/update! Database 1 {:details "{\"db\":\"/tmp/test.db\"}"}))
-
-        (testing "rotating with the same key is a noop"
+  (eu/with-secret-key nil
+    (let [h2-fixture-db-file (abs-path "frontend/test/__runner__/test_db_fixture.db")
+          db-name (str "test_" (mt/random-name))
+          [k1 k2 k3] ["89ulvIGoiYw6mNELuOoEZphQafnF/zYe+3vT+v70D1A="
+                      "yHa/6VEQuIItMyd5CNcgV9nXvzZcX6bWmiY0oOh6pLU="
+                      "BCQbKNVu6N8TQ2BwyTC0U0oCBqsvFVr2uhEM/tRgJUM="]]
+      (mt/test-drivers #{:mysql :postgres :h2}
+        (binding [mdb.connection/*db-type*   driver/*driver*
+                  mdb.connection/*jdbc-spec* (persistent-jdbcspec driver/*driver* db-name)
+                  db/*db-connection* (persistent-jdbcspec driver/*driver* db-name)
+                  db/*quoting-style* driver/*driver*]
+          (when-not (= driver/*driver* :h2)
+            (tx/create-db! driver/*driver* {:database-name db-name}))
+          (load-from-h2/load-from-h2! h2-fixture-db-file)
+          (db/insert! Setting {:key "setting0", :value "val0"})
           (eu/with-secret-key k1
-            (is (rotate-keys! k1))
-            ;; plain->newkey
-            (is (not (= "val0" (raw-value "setting0"))))
-            (is (= "val0" (:value (first (db/select Setting :key [:= "setting0"])))))
-            ;; oldkey->newkey
-            (is (not (= "val1" (raw-value "setting1"))))
-            (is (= "val1" (:value (first (db/select Setting :key [:= "setting1"])))))))
+            (db/insert! Setting {:key "setting1", :value "val1"})
+            (db/update! Database 1 {:details "{\"db\":\"/tmp/test.db\"}"}))
 
-        (testing "rotating with a new key is recoverable"
-          (eu/with-secret-key k1 (is (rotate-keys! k2)))
-          (eu/with-secret-key k2 (is (= "val0" (:value (first (db/select Setting :key [:= "setting0"]))))))
-          (eu/with-secret-key k1
-            (is (not (= "val0" (:value (first (db/select Setting :key [:= "setting0"]))))))))
+          (testing "rotating with the same key is a noop"
+            (eu/with-secret-key k1
+              (is (rotate-keys! k1))
+              ;; plain->newkey
+              (is (not (= "val0" (raw-value "setting0"))))
+              (is (= "val0" (:value (first (db/select Setting :key [:= "setting0"])))))
+              ;; oldkey->newkey
+              (is (not (= "val1" (raw-value "setting1"))))
+              (is (= "val1" (:value (first (db/select Setting :key [:= "setting1"])))))))
 
-        (testing "full rollback when a field looks encrypted with a different key than the current one"
-          (eu/with-secret-key k3
-            (db/insert! Setting {:key "setting3", :value "val3"}))
-          (eu/with-secret-key k2
-            (db/insert! Setting {:key "setting2", :value "val2"})
-            (is (not (rotate-keys! k3))))
-          (eu/with-secret-key k3
-            (is (not (= "val2" (:value (first (db/select Setting :key [:= "setting2"]))))))
-            (is (= "val3" (:value (first (db/select Setting :key [:= "setting3"])))))))
+          (testing "rotating with a new key is recoverable"
+            (eu/with-secret-key k1 (is (rotate-keys! k2)))
+            (eu/with-secret-key k2 (is (= "val0" (:value (first (db/select Setting :key [:= "setting0"]))))))
+            (eu/with-secret-key k1
+              (is (not (= "val0" (:value (first (db/select Setting :key [:= "setting0"]))))))))
 
-        (testing "rotate-keys! to nil decrypts the encrypted keys"
-          (db/delete! Setting :key "setting3")
-          (eu/with-secret-key k2
-            (is (rotate-keys! nil)))
-          (is (= "val0" (raw-value "setting0"))))))))
+          (testing "full rollback when a field looks encrypted with a different key than the current one"
+            (eu/with-secret-key k3
+              (db/insert! Setting {:key "setting3", :value "val3"}))
+            (eu/with-secret-key k2
+              (db/insert! Setting {:key "setting2", :value "val2"})
+              (is (not (rotate-keys! k3))))
+            (eu/with-secret-key k3
+              (is (not (= "val2" (:value (first (db/select Setting :key [:= "setting2"]))))))
+              (is (= "val3" (:value (first (db/select Setting :key [:= "setting3"])))))))
+
+          (testing "rotate-keys! to nil decrypts the encrypted keys"
+            (db/delete! Setting :key "setting3")
+            (eu/with-secret-key k2
+              (is (rotate-keys! nil)))
+            (is (= "val0" (raw-value "setting0")))))))))
