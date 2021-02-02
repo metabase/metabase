35e79c5d3312f926f4208ea856bcdf5f6dafb4d8
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-02-02 10:07:08 -0800

    Add a specific test for trimmed fields

--- frontend/test/metabase/scenarios/admin/databases/add.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/databases/add.cy.spec.js
@@ -40,13 +40,35 @@ describe("scenarios > admin > databases > add", () => {
     ).should("have.attr", "aria-checked", "true");
 
     typeField("Name", "Test db name");
-    typeField("Host", "localhost  \n  ");
+    typeField("Host", "localhost");
     typeField("Database name", "test_postgres_db");
     typeField("Username", "uberadmin");
 
     cy.findByText("Save")
       .should("not.be.disabled")
       .click();
+
+    cy.wait("@createDatabase");
+
+    cy.url().should("match", /\/admin\/databases\?created=42$/);
+  });
+
+  it("should trim fields needed to connect to the database", () => {
+    cy.route({
+      method: "POST",
+      url: "/api/database",
+      response: { id: 42 },
+      delay: 1000,
+    }).as("createDatabase");
+
+    cy.visit("/admin/databases/create");
+
+    typeField("Name", "Test db name");
+    typeField("Host", "localhost  \n  ");
+    typeField("Database name", " test_postgres_db");
+    typeField("Username", "   uberadmin   ");
+
+    cy.findByText("Save").click();
 
     cy.wait("@createDatabase");
 
