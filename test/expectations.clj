2873de87d736141d21be4445ef0ff144fe7766a9
Cam Saul <github@camsaul.com>
2021-02-01 19:55:19 -0800

    Update expectations counts

--- test/expectations.clj
+++ test/expectations.clj
@@ -137,8 +137,8 @@
       (t/testing "Don't write any new tests using expect!"
         (let [ee? (u/ignore-exceptions (require 'metabase-enterprise.core) true)]
           ;; TODO - update the numbers for EE
-          (t/is (<= total-expect-forms (if ee? 871 842)))
-          (t/is (<= total-namespaces-using-expect (if ee? 84 80))))))))
+          (t/is (<= total-expect-forms (if ee? 683 656)))
+          (t/is (<= total-namespaces-using-expect (if ee? 68 65))))))))
 
 (defmacro ^:deprecated expect
   "Simple macro that simulates converts an Expectations-style `expect` form into a `clojure.test` `deftest` form."
