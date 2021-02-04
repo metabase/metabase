3b44499bcc997a34f9e2eb055342703ccca9f665
dan sutton <dan@dpsutton.com>
2021-02-04 12:04:18 -0600

    Ensure auth-ed in api tests

--- test/metabase/api/notify_test.clj
+++ test/metabase/api/notify_test.clj
@@ -1,8 +1,10 @@
 (ns metabase.api.notify-test
-  (:require [clj-http.client :as client]
+  (:require [cheshire.core :as json]
+            [clj-http.client :as client]
             [clojure.test :refer :all]
             [metabase.http-client :as http]
-            [metabase.models.database :as database :refer [Database]]
+            [metabase.models.database :as database]
+            [metabase.server.middleware.auth :as auth]
             [metabase.server.middleware.util :as middleware.u]
             [metabase.test :as mt]
             [metabase.test.fixtures :as fixtures]
@@ -23,7 +25,7 @@
               :body   "Not found."}
              (try (client/post (http/build-url (format "notify/db/%d" Integer/MAX_VALUE) {})
                                {:accept  :json
-                                :headers {"X-METABASE-APIKEY" "test-api-key"
+                                :headers {"X-METABASE-APIKEY" (auth/api-key)
                                           "Content-Type"      "application/json"}})
                   (catch clojure.lang.ExceptionInfo e
                     (select-keys (:object (ex-data e)) [:status :body]))))))))
@@ -31,22 +33,24 @@
 (deftest post-db-id-test
   (mt/test-drivers (mt/normal-drivers)
     (let [table-name (->> (mt/db) database/tables first :name)
-          post (fn [quick]
-                 (try
-                   (mt/user-http-request :rasta :post (format "notify/db/%d" (u/the-id (mt/db)))
-                                         {:quick quick, :table_name table-name})
-                   (catch clojure.lang.ExceptionInfo e
-                     (select-keys (:object (ex-data e)) [:status :body]))))]
+          post       (fn [quick]
+                       (mt/user-http-request :crowberto
+                                             :post
+                                             (format "notify/db/%d" (u/the-id (mt/db)))
+                                             {:request-options
+                                              {:headers {"X-METABASE-APIKEY" (auth/api-key)
+                                                         "Content-Type"      "application/json"}}}
+                                             {:quick quick, :table_name table-name}))]
       (testing "sync just table when table is provided"
         (let [long-sync-called? (atom false), short-sync-called? (atom false)]
-          (with-redefs [metabase.sync/sync-table! (fn [_table] (reset! long-sync-called? true))
+          (with-redefs [metabase.sync/sync-table!                        (fn [_table] (reset! long-sync-called? true))
                         metabase.sync.sync-metadata/sync-table-metadata! (fn [_table] (reset! short-sync-called? true))]
             (is (= {:success true} (post false)))
             (is @long-sync-called?)
             (is (not @short-sync-called?)))))
       (testing "only a quick sync when quick parameter is provided"
         (let [long-sync-called? (atom false), short-sync-called? (atom false)]
-          (with-redefs [metabase.sync/sync-table! (fn [_table] (reset! long-sync-called? true))
+          (with-redefs [metabase.sync/sync-table!                        (fn [_table] (reset! long-sync-called? true))
                         metabase.sync.sync-metadata/sync-table-metadata! (fn [_table] (reset! short-sync-called? true))]
             (is (= {:success true} (post true)))
             (is (not @long-sync-called?))
