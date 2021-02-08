32eebb4cbca0ca17972bf2cc50244b92e6194149
Cam Saul <github@camsaul.com>
2021-02-08 10:26:27 -0800

    Quick sync

--- src/metabase/sync.clj
+++ src/metabase/sync.clj
@@ -30,16 +30,23 @@
   note that this function is *not* what is called by the scheduled tasks; those call different steps independently.
   This function is called when a Database is first added."
   {:style/indent 1}
-  [database :- i/DatabaseInstance]
-  (sync-util/sync-operation :sync database (format "Sync %s" (sync-util/name-for-logging database))
-    (mapv (fn [[f step-name]] (assoc (f database) :name step-name))
-          [
-           ;; First make sure Tables, Fields, and FK information is up-to-date
-           [sync-metadata/sync-db-metadata! "metadata"]
-           ;; Next, run the 'analysis' step where we do things like scan values of fields and update special types accordingly
-           [analyze/analyze-db! "analyze"]
-           ;; Finally, update cached FieldValues
-           [field-values/update-field-values! "field-values"]])))
+  ([database]
+   (sync-database! database nil))
+
+  ([database :- i/DatabaseInstance {:keys [quick?], :or {quick? false}}]
+   (sync-util/sync-operation :sync database (format "Sync %s" (sync-util/name-for-logging database))
+     (mapv (fn [[f step-name]] (assoc (f database) :name step-name))
+           (filter
+            some?
+            [;; First make sure Tables, Fields, and FK information is up-to-date
+             [sync-metadata/sync-db-metadata! "metadata"]
+             ;; Next, run the 'analysis' step where we do things like scan values of fields and update special types
+             ;; accordingly
+             (when-not quick?
+               [analyze/analyze-db! "analyze"])
+             ;; Finally, update cached FieldValues
+             (when-not quick?
+               [field-values/update-field-values! "field-values"])])))))
 
 (s/defn sync-table!
   "Perform all the different sync operations synchronously for a given `table`."
