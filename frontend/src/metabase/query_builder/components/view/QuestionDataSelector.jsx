332598178f3a8005fd302d013b3f8fed96806119
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2021-02-16 17:38:27 -0500

    test fit swapping components
    this gets a new popover in place when you click Saved Questions (only in simple mode right now).
    we keep some basic UI state in the QuestionDataSelector and either show the list of databases or swap over
    to our stubbbed our placeholder for saved questions.

--- frontend/src/metabase/query_builder/components/view/QuestionDataSelector.jsx
+++ frontend/src/metabase/query_builder/components/view/QuestionDataSelector.jsx
@@ -2,22 +2,48 @@ import React from "react";
 
 import { DatabaseSchemaAndTableDataSelector } from "metabase/query_builder/components/DataSelector";
 
-export default function QuestionDataSelector({ query, triggerElement }) {
-  return (
-    <DatabaseSchemaAndTableDataSelector
-      // Set this to false for now so we use our own trigger and component instead
-      databaseQuery={{ saved: false }}
-      selectedDatabaseId={query.databaseId()}
-      selectedTableId={query.tableId()}
-      setSourceTableFn={tableId =>
-        query
-          .setTableId(tableId)
-          .setDefaultQuery()
-          .update(null, { run: true })
-      }
-      triggerElement={triggerElement}
-      isOpen
-      onSwitchToSavedQuestions={() => alert("clicked saved q")}
-    />
-  );
+import PopoverWithTrigger from "metabase/components/PopoverWithTrigger";
+import Icon from "metabase/components/Icon";
+
+export default class QuestionDataSelector extends React.Component {
+  state = {
+    isShowingSavedQuestions: false,
+  };
+  render() {
+    const { query, triggerElement } = this.props;
+    return this.state.isShowingSavedQuestions ? (
+      <PopoverWithTrigger triggerElement={triggerElement} isOpen>
+        <div style={{ width: 400 }}>
+          <div>
+            <span
+              onClick={() => this.setState({ isShowingSavedQuestions: false })}
+              className="text-brand-hover flex align-center"
+            >
+              <Icon name="chevronleft" />
+              Back
+            </span>
+          </div>
+          Saved questions go here
+        </div>
+      </PopoverWithTrigger>
+    ) : (
+      <DatabaseSchemaAndTableDataSelector
+        // Set this to false for now so we use our own trigger and component instead
+        databaseQuery={{ saved: false }}
+        selectedDatabaseId={query.databaseId()}
+        selectedTableId={query.tableId()}
+        setSourceTableFn={tableId =>
+          query
+            .setTableId(tableId)
+            .setDefaultQuery()
+            .update(null, { run: true })
+        }
+        triggerElement={triggerElement}
+        isOpen
+        onSwitchToSavedQuestions={() =>
+          this.setState({ isShowingSavedQuestions: true })
+        }
+      />
+    );
+  }
 }
