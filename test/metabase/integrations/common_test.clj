391c4eb9eb876429c197289c6b430aadfdcfe2a4
Robert Roland <rob@metabase.com>
2021-03-01 15:54:02 -0800

    Adding the ability to sync admin groups in EE (#14993)
    * Adding the ability to sync admin groups in EE
    
    Adds a LDAP setting for EE to enable to LDAP Administrator group syncs.
    
    Adds the setting to admin UI to enable syncing of the admin group.
    
    Removes two unused settings from the OSS side.
    
    metabase/metabase-enterprise#445
    
    * Apply suggestions from code review
    
    Co-authored-by: Dalton <daltojohnso@users.noreply.github.com>
    
    Co-authored-by: Dalton <daltojohnso@users.noreply.github.com>

--- test/metabase/integrations/common_test.clj
+++ test/metabase/integrations/common_test.clj
@@ -1,13 +1,17 @@
 (ns metabase.integrations.common-test
-  (:require [expectations :refer [expect]]
+  (:require [clojure.test :refer :all]
             [metabase.integrations.common :as integrations.common]
             [metabase.models.permissions-group :as group :refer [PermissionsGroup]]
             [metabase.models.permissions-group-membership :refer [PermissionsGroupMembership]]
             [metabase.models.user :refer [User]]
+            [metabase.test :as mt]
+            [metabase.test.fixtures :as fixtures]
             [metabase.test.util.log :as tu.log]
             [metabase.util :as u]
             [toucan.db :as db]
             [toucan.util.test :as tt]))
+
+(use-fixtures :once (fixtures/initialize :db))
 
 (defn- group-memberships
   "Return set of names of PermissionsGroups `user` currently belongs to."
@@ -45,68 +49,82 @@
     (let [[user-binding groups-or-ids-to-put-user-in] bindings]
       `(do-with-user-in-groups (fn [~user-binding] ~@body) ~groups-or-ids-to-put-user-in))))
 
-;; does syncing group memberships leave existing memberships in place if nothing has changed?
-(expect
-  #{"All Users" ":metabase.integrations.common-test/group"}
-  (with-user-in-groups [group {:name (str ::group)}
-                        user  [group]]
-    (integrations.common/sync-group-memberships! user #{group})
-    (group-memberships user)))
+(deftest sync-groups-test
+  (testing "does syncing group memberships leave existing memberships in place if nothing has changed?"
+    (with-user-in-groups [group {:name (str ::group)}
+                          user  [group]]
+      (integrations.common/sync-group-memberships! user #{group} false)
+      (is (= #{"All Users" ":metabase.integrations.common-test/group"}
+             (group-memberships user)))))
 
-;; the actual `PermissionsGroupMembership` object should not have been replaced
-(expect
-  (with-user-in-groups [group {:name (str ::group)}
-                        user  [group]]
-    (let [membership-id          #(db/select-one-id PermissionsGroupMembership
-                                    :group_id (u/get-id group)
-                                    :user_id  (u/get-id user))
-          original-membership-id (membership-id)]
-      (integrations.common/sync-group-memberships! user #{group})
-      (= original-membership-id
-         (membership-id)))))
+  (testing "the actual `PermissionsGroupMembership` object should not have been replaced"
+    (with-user-in-groups [group {:name (str ::group)}
+                          user  [group]]
+      (let [membership-id          #(db/select-one-id PermissionsGroupMembership
+                                                      :group_id (u/get-id group)
+                                                      :user_id  (u/get-id user))
+            original-membership-id (membership-id)]
+        (integrations.common/sync-group-memberships! user #{group} false)
+        (is (= original-membership-id
+               (membership-id))))))
 
-;; does syncing group memberships add users to new groups correctly?
-(expect
-  #{":metabase.integrations.common-test/group-1"
-    ":metabase.integrations.common-test/group-2"
-    "All Users"}
-  (with-user-in-groups [group-1 {:name (str ::group-1)}
-                        group-2 {:name (str ::group-2)}
-                        user    [group-1]]
-    (integrations.common/sync-group-memberships! user #{group-1 group-2})
-    (group-memberships user)))
+  (testing "does syncing group memberships add users to new groups correctly?"
+    (with-user-in-groups [group-1 {:name (str ::group-1)}
+                          group-2 {:name (str ::group-2)}
+                          user    [group-1]]
+      (integrations.common/sync-group-memberships! user #{group-1 group-2} false)
+      (is (= #{":metabase.integrations.common-test/group-1"
+               ":metabase.integrations.common-test/group-2"
+               "All Users"}
+             (group-memberships user)))))
 
-;; does syncing group memberships remove users from old groups correctly?
-(expect
-  #{"All Users"}
-  (with-user-in-groups [group-1 {:name (str ::group-1)}
-                        group-2 {:name (str ::group-2)}
-                        user    [group-1]]
-    (integrations.common/sync-group-memberships! user #{})
-    (group-memberships user)))
+  (testing "does syncing group memberships remove users from old groups correctly?"
+    (with-user-in-groups [group-1 {:name (str ::group-1)}
+                          group-2 {:name (str ::group-2)}
+                          user    [group-1]]
+      (integrations.common/sync-group-memberships! user #{} false)
+      (is (= #{"All Users"}
+             (group-memberships user)))))
 
-;; does adding & removing at the same time work correctly?
-(expect
-  #{":metabase.integrations.common-test/group-2" "All Users"}
-  (with-user-in-groups [group-1 {:name (str ::group-1)}
-                        group-2 {:name (str ::group-2)}
-                        user    [group-1]]
-    (integrations.common/sync-group-memberships! user #{group-2})
-    (group-memberships user)))
+  (testing "does adding & removing at the same time work correctly?"
+    (with-user-in-groups [group-1 {:name (str ::group-1)}
+                          group-2 {:name (str ::group-2)}
+                          user    [group-1]]
+      (integrations.common/sync-group-memberships! user #{group-2} false)
+      (is (= #{":metabase.integrations.common-test/group-2" "All Users"}
+             (group-memberships user)))))
 
-;; if we attempt to add a user to a group that doesn't exist, does the group sync complete for the other groups?
+  (testing "if we attempt to add a user to a group that doesn't exist, does the group sync complete for the other groups?"
+    (with-user-in-groups [group {:name (str ::group)}
+                          user    [group]]
+      (tu.log/suppress-output
+       (integrations.common/sync-group-memberships! user [Integer/MAX_VALUE group] false))
+      (is (= #{"All Users" ":metabase.integrations.common-test/group"}
+             (group-memberships user)))))
 
-(expect
-  #{"All Users" ":metabase.integrations.common-test/group"}
-  (with-user-in-groups [group {:name (str ::group)}
-                        user    [group]]
-    (tu.log/suppress-output
-      (integrations.common/sync-group-memberships! user [Integer/MAX_VALUE group]))
-    (group-memberships user)))
+  (mt/with-test-user :crowberto
+    (testing "with admin group sync enabled"
+      (testing "are admins synced?"
+        (with-user-in-groups [user]
+          (integrations.common/sync-group-memberships! user [(group/admin)] true)
+          (is (= #{"Administrators" "All Users"}
+                 (group-memberships user)))))
 
-;; are special groups like admin & all users ignored?
-(expect
-  #{"All Users"}
-  (with-user-in-groups [user]
-    (integrations.common/sync-group-memberships! user [(group/admin)])
-    (group-memberships user)))
+      (testing "are administrators removed appropriately?"
+        (with-user-in-groups [user [(group/admin)]]
+          (integrations.common/sync-group-memberships! user [] true)
+          (is (= #{"All Users"}
+                 (group-memberships user)))))))
+
+  (testing "with admin group sync disabled"
+    (testing "are admins synced?"
+      (with-user-in-groups [user]
+        (integrations.common/sync-group-memberships! user [(group/admin)] false)
+        (is (= #{"All Users"}
+               (group-memberships user)))))
+
+    (testing "are administrators removed appropriately?"
+      (with-user-in-groups [user [(group/admin)]]
+        (integrations.common/sync-group-memberships! user [] false)
+        (is (= #{"Administrators" "All Users"}
+               (group-memberships user)))))))
