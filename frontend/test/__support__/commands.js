855c35a61e20ff7b672dbb4ad0e0dcc77a8ce7f9
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-03-03 23:57:12 +0100

    Account for the possibility of passing multiple user attributes

--- frontend/test/__support__/commands.js
+++ frontend/test/__support__/commands.js
@@ -143,10 +143,12 @@ Cypress.Commands.add(
   } = {}) => {
     // Extract the name of the table, as well as `schema` and `db_id` that we'll need later on for `cy.updatePermissionsSchemas()`
     cy.request("GET", "/api/table").then(({ body: tables }) => {
-      const { name, schema, db_id } = tables.find(table => table.id === table_id);
-      const [attr] = Object.keys(attribute_remappings);
+      const { name, schema, db_id } = tables.find(
+        table => table.id === table_id,
+      );
+      const attr = Object.keys(attribute_remappings).join(", "); // Account for the possiblity of passing multiple user attributes
 
-      cy.log(`Sandbox "${name}" table on "${attr}" attribute`);
+      cy.log(`Sandbox "${name}" table on "${attr}"`);
       cy.request("POST", "/api/mt/gtap", {
         attribute_remappings,
         card_id,
