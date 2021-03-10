218cb425482c5bd2e626c4eedcdc24ed68d06d51
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-03-10 22:23:05 +0100

    Fix `react/no-unescaped-entities` error (#15111)
    * Fix `react/no-unescaped-entities` error
    
    * Handle untransalted string

--- frontend/src/metabase/admin/settings/components/SettingsSlackForm.jsx
+++ frontend/src/metabase/admin/settings/components/SettingsSlackForm.jsx
@@ -268,7 +268,7 @@ export default class SettingsSlackForm extends Component {
           </div>
           <div className="py2">
             {jt`Once you're there, give it a name and click ${(
-              <strong>"Add bot integration"</strong>
+              <strong>&quot;{t`Add bot integration`}&quot;</strong>
             )}. Then copy and paste the Bot API Token into the field below. Once you are done, create a "metabase_files" channel in Slack. Metabase needs this to upload graphs.`}
           </div>
         </div>
