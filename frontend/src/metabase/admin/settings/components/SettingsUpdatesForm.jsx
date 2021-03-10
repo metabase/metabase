8153b88287838662297290e239c664da3ed47316
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-03-10 22:19:55 +0100

    Fix `react/jsx-no-target-blank` error (#15110)
    * Fix `react/jsx-no-target-blank` error
    
    * Handle untranslated string
    
    * Add `dataMetabaseEvent` prop to `ExternalLink`
    
    * Update missed `ExternalLink` component in `frontend/src/metabase/query_builder/components/template_tags/TagEditorHelp.jsx` with `dataMetabaseEvent`

--- frontend/src/metabase/admin/settings/components/SettingsUpdatesForm.jsx
+++ frontend/src/metabase/admin/settings/components/SettingsUpdatesForm.jsx
@@ -9,6 +9,7 @@ import SettingsSetting from "./SettingsSetting";
 import HostingInfoLink from "metabase/admin/settings/components/widgets/HostingInfoLink";
 import Icon from "metabase/components/Icon";
 import Text from "metabase/components/type/Text";
+import ExternalLink from "metabase/components/ExternalLink";
 
 export default class SettingsUpdatesForm extends Component {
   static propTypes = {
@@ -41,8 +42,8 @@ export default class SettingsUpdatesForm extends Component {
               {jt`Metabase ${formatVersion(latestVersion)} is available.`}{" "}
               {jt`You're running ${formatVersion(currentVersion)}`}
             </span>
-            <a
-              data-metabase-event={
+            <ExternalLink
+              dataMetabaseEvent={
                 "Updates Settings; Update link clicked; " + latestVersion
               }
               className="Button Button--white Button--medium borderless"
@@ -50,7 +51,7 @@ export default class SettingsUpdatesForm extends Component {
               target="_blank"
             >
               {t`Update`}
-            </a>
+            </ExternalLink>
           </div>
 
           <div
