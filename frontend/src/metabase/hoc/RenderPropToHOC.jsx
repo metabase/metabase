e47641b68d8efd61d83570e88fae428368da9a4e
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-03-11 17:34:00 +0100

    Fix `react/no-children-prop` error (#15122)

--- frontend/src/metabase/hoc/RenderPropToHOC.jsx
+++ frontend/src/metabase/hoc/RenderPropToHOC.jsx
@@ -3,11 +3,8 @@ import React from "react";
 export default function renderPropToHoc(RenderPropComponent) {
   // eslint-disable-next-line react/display-name
   return ComposedComponent => props => (
-    <RenderPropComponent
-      {...props}
-      children={childrenProps => (
-        <ComposedComponent {...props} {...childrenProps} />
-      )}
-    />
+    <RenderPropComponent {...props}>
+      {childrenProps => <ComposedComponent {...props} {...childrenProps} />}
+    </RenderPropComponent>
   );
 }
