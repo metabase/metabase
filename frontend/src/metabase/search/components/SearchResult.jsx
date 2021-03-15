ff071e8953b0f4e91f9245b5762f24fc18adec63
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2021-03-15 10:30:16 -0400

    restructure and simplify search result component
    - simplify jsx structure
    - show display_name for metrics and segments
    - remove collection parent info for now
    - fix show description for collections if they exist

--- frontend/src/metabase/search/components/SearchResult.jsx
+++ frontend/src/metabase/search/components/SearchResult.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Flex } from "grid-styled";
 import styled from "styled-components";
-import { jt } from "ttag";
+import { t, jt } from "ttag";
 
 import * as Urls from "metabase/lib/urls";
 import { color, lighten } from "metabase/lib/colors";
@@ -94,61 +94,6 @@ function ItemIcon({ item, type }) {
   );
 }
 
-export default function SearchResult(props) {
-  const { result } = props;
-  switch (result.model) {
-    case "card":
-      return <QuestionResult question={result} options={props} />;
-    case "collection":
-      return <CollectionResult collection={result} options={props} />;
-    case "dashboard":
-      return <DashboardResult dashboard={result} options={props} />;
-    case "table":
-      return <TableResult table={result} options={props} />;
-    case "segment":
-      return <SegmentResult segment={result} options={props} />;
-    case "metric":
-      return <MetricResult metric={result} options={props} />;
-    default:
-      return <DefaultResult result={result} options={props} />;
-  }
-}
-
-function TableResult({ table, options }) {
-  return (
-    <ResultLink to={table.getUrl()} compact={options.compact}>
-      <Flex align="start">
-        <ItemIcon item={table} type="table" />
-        <Box>
-          <Title>{table.name}</Title>
-          <Text>
-            Table in &nbsp;
-            <span>
-              <Link to={Urls.browseDatabase({ id: table.database_id })}>
-                <Database.Name id={table.database_id} />&nbsp;
-              </Link>
-              {table.table_schema && (
-                <span>
-                  <Icon name="chevronright" mx="4px" size={10} />
-                  {/* we have to do some {} manipulation here to make this look like the table object that browseSchema was written for originally */}
-                  <Link
-                    to={Urls.browseSchema({
-                      db: { id: table.database_id },
-                      schema_name: table.table_schema,
-                    })}
-                  >
-                    {table.table_schema}
-                  </Link>
-                </span>
-              )}
-            </span>
-          </Text>
-        </Box>
-      </Flex>
-    </ResultLink>
-  );
-}
-
 const CollectionLink = styled(Link)`
   text-decoration: dashed;
   &:hover {
@@ -173,35 +118,6 @@ function Score({ scores }) {
     <pre className="hide search-score">{JSON.stringify(scores, null, 2)}</pre>
   );
 }
-
-// Generally these should be at the bottom of the list I'd hope
-function CollectionResult({ collection, options }) {
-  return (
-    <ResultLink to={Urls.collection(collection.id)} compact={options.compact}>
-      <Flex align="start">
-        <ItemIcon item={collection} type="collection" />
-        <Box>
-          <Title>{collection.name}</Title>
-          <Text>{jt`Collection in ${formatCollection(
-            collection.getCollection(),
-          )}`}</Text>
-        </Box>
-        <Score scores={collection.scores} />
-      </Flex>
-    </ResultLink>
-  );
-}
-
-function contextText(context) {
-  return context.map(function({ is_match, text }) {
-    if (is_match) {
-      return <strong style={{ color: color("brand") }}> {text}</strong>;
-    } else {
-      return <span> {text}</span>;
-    }
-  });
-}
-
 const Context = styled("p")`
   line-height: 1.4em;
   color: ${color("text-medium")};
@@ -229,111 +145,83 @@ const Description = styled(Text)`
   border-left: 2px solid ${lighten("brand", 0.45)};
 `;
 
-function DashboardResult({ dashboard, options }) {
-  return (
-    <ResultLink to={dashboard.getUrl()} compact={options.compact}>
-      <Flex align="start">
-        <ItemIcon item={dashboard} type="dashboard" />
-        <Box>
-          <Title>{dashboard.name}</Title>
-          <Text>{jt`Dashboard in ${formatCollection(
-            dashboard.getCollection(),
-          )}`}</Text>
-          {dashboard.description && (
-            <Description>{dashboard.description}</Description>
-          )}
-          <Score scores={dashboard.scores} />
-        </Box>
-      </Flex>
-      {formatContext(dashboard.context, options.compact)}
-    </ResultLink>
-  );
+function contextText(context) {
+  return context.map(function ({ is_match, text }) {
+    if (is_match) {
+      return <strong style={{ color: color("brand") }}> {text}</strong>;
+    } else {
+      return <span> {text}</span>;
+    }
+  });
 }
 
-function QuestionResult({ question, options }) {
+export default function SearchResult({ result, compact }) {
+  let info;
+  const collection = result.getCollection();
+  switch (result.model) {
+    case "card":
+      info = jt`Saved question in ${formatCollection(collection)}`;
+      break;
+    case "collection":
+      info = t`Collection`;
+      break;
+    case "table":
+      info = (
+        <span>
+          {jt`Table in ${(
+            <span>
+              <Link to={Urls.browseDatabase({ id: result.database_id })}>
+                <Database.Name id={result.database_id} />{" "}
+              </Link>
+              {result.table_schema && (
+                <span>
+                  <Icon name="chevronright" mx="4px" size={10} />
+                  {/* we have to do some {} manipulation here to make this look like the table object that browseSchema was written for originally */}
+                  <Link
+                    to={Urls.browseSchema({
+                      db: { id: result.database_id },
+                      schema_name: result.table_schema,
+                    })}
+                  >
+                    {result.table_schema}
+                  </Link>
+                </span>
+              )}
+            </span>
+          )}`}
+        </span>
+      );
+      break;
+    case "segment":
+    case "metric":
+      info = jt`${result.model === "segment" ? "Segment of" : "Metric for"
+        } of ${(
+          <Link to={Urls.tableRowsQuery(result.database_id, result.table_id)}>
+            <Table.Loader id={result.table_id}>
+              {({ table }) => <span>{table.display_name}</span>}
+            </Table.Loader>
+          </Link>
+        )}`;
+      break;
+    default:
+      info = jt`${result.model.charAt(0).toUpperCase() +
+        result.model.slice(1)} in ${formatCollection(collection)}`;
+      break;
+  }
   return (
-    <ResultLink to={question.getUrl()} compact={options.compact}>
+    <ResultLink to={result.getUrl()} compact={compact}>
       <Flex align="start">
-        <ItemIcon item={question} type="question" />
-        <Box>
-          <Title>{question.name}</Title>
-          <Text>{jt`Saved question in ${formatCollection(
-            question.getCollection(),
-          )}`}</Text>
-          {question.description && (
-            <Description>{question.description}</Description>
-          )}
-          <Score scores={question.scores} />
-        </Box>
-      </Flex>
-      {formatContext(question.context, options.compact)}
-    </ResultLink>
-  );
-}
-
-function SegmentResult({ segment, options }) {
-  return (
-    <ResultLink to={segment.getUrl()} compact={options.compact}>
-      <Flex align="start">
-        <ItemIcon item={segment} />
-        <Box>
-          <Title>{segment.name}</Title>
-          <Text>{jt`Segment of ${(
-            <Link
-              to={Urls.tableRowsQuery(segment.database_id, segment.table_id)}
-            >
-              <Table.Name id={segment.table_id} />
-            </Link>
-          )}`}</Text>
-          {segment.description && (
-            <Description>{segment.description}</Description>
-          )}
-          <Score scores={segment.scores} />
-        </Box>
-      </Flex>
-    </ResultLink>
-  );
-}
-
-function MetricResult({ metric, options }) {
-  return (
-    <ResultLink to={metric.getUrl()} compact={options.compact}>
-      <Flex align="start">
-        <ItemIcon item={metric} />
-        <Box>
-          <Title>{metric.name}</Title>
-          <Text>{jt`Metric for ${(
-            <Link to={Urls.tableRowsQuery(metric.database_id, metric.table_id)}>
-              <Table.Name id={metric.table_id} />
-            </Link>
-          )}`}</Text>
-          {metric.description && (
-            <Description>{metric.description}</Description>
-          )}
-          <Score scores={metric.scores} />
-        </Box>
-      </Flex>
-    </ResultLink>
-  );
-}
-
-function DefaultResult({ result, options }) {
-  return (
-    <ResultLink to={result.getUrl()} compact={options.compact}>
-      <Flex align="start">
-        <ItemIcon item={result} />
+        <ItemIcon item={result} type={result.model} />
         <Box>
           <Title>{result.name}</Title>
-          <Text>{jt`${result.model} in ${formatCollection(
-            result.getCollection(),
-          )}`}</Text>
+          <Text>{info}</Text>
           {result.description && (
             <Description>{result.description}</Description>
           )}
           <Score scores={result.scores} />
         </Box>
       </Flex>
-      {formatContext(result.context, options.compact)}
+      {formatContext(result.context, compact)}
     </ResultLink>
   );
 }
