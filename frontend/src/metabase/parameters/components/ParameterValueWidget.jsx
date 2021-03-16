6051ee8dc84a1a2d0b48d9af8d4525dffb9954ce
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-03-16 10:11:31 -0700

    rmv unused value

--- frontend/src/metabase/parameters/components/ParameterValueWidget.jsx
+++ frontend/src/metabase/parameters/components/ParameterValueWidget.jsx
@@ -146,7 +146,6 @@ export default class ParameterValueWidget extends Component {
       metadata,
       parameter,
       value,
-      values,
       setValue,
       isEditing,
       placeholder,
