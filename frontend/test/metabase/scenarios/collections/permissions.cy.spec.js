1e1b73e216f54a1a3fe313a7448f6725da7615ec
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-03-17 23:22:04 +0100

    Add repro for #6884

---
+++ frontend/test/metabase/scenarios/collections/permissions.cy.spec.js
@@ -0,0 +1,56 @@
+import { onlyOn } from "@cypress/skip-test";
+import { restore } from "__support__/cypress";
+
+const PERMISSIONS = {
+  curate: ["admin", "normal", "nodata"],
+  view: ["nodata"],
+  no: ["nocollection", "none"],
+};
+
+describe("collection permissions", () => {
+  beforeEach(() => {
+    restore();
+    cy.server();
+  });
+
+  describe("revision history", () => {
+    beforeEach(() => {
+      cy.route("POST", "/api/revision/revert").as("revert");
+    });
+
+    Object.entries(PERMISSIONS).forEach(([permission, userGroup]) => {
+      context(`${permission} access`, () => {
+        // This function `onlyOn` will not generate tests for any other condition.
+        // It helps to make both our tests and Cypress runner sidebar clean
+        onlyOn(permission === "curate", () => {
+          userGroup.forEach(user => {
+            beforeEach(() => {
+              cy.signIn(user);
+            });
+
+            it.skip(`${user} user should be able to revert the dashboard (metabase#6884)`, () => {
+              cy.visit("/dashboard/1");
+              cy.icon("ellipsis").click();
+              cy.findByText("Revision history").click();
+              clickRevert("First revision.");
+              cy.wait("@revert").then(xhr => {
+                expect(xhr.status).to.eq(200);
+                expect(xhr.cause).not.to.exist;
+              });
+              cy.findAllByText(/Revert/).should("not.exist");
+              // We reverted the dashboard to the state prior to adding any cards to it
+              cy.findByText("This dashboard is looking empty.");
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+function clickRevert(event_name) {
+  cy.findByText(event_name)
+    .closest("tr")
+    .findByText(/Revert/i)
+    .click();
+}
