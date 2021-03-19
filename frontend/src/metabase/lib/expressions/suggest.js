708c39b4eb75b400e0d9f7d021d9275223cf5de3
howon lee <hlee.howon@gmail.com>
2021-03-19 13:01:49 -0700

    complicated prefix trim to solve 15247

--- frontend/src/metabase/lib/expressions/suggest.js
+++ frontend/src/metabase/lib/expressions/suggest.js
@@ -388,7 +388,7 @@ function operatorSuggestion(clause) {
     type: "operators",
     name: name,
     text: " " + name + " ",
-    prefixTrim: /\s*$/,
+    prefixTrim: new RegExp("\\s*" + name.replace(/(.{1})/g, "$1?") + "$", "i"),
     postfixTrim: new RegExp("/^s*" + escape(name) + "?s*/"),
   };
 }
