095fec3727577fa1a76a62887b532a13ab076c7e
howon lee <hlee.howon@gmail.com>
2021-03-29 13:52:01 -0700

    Finish linting

--- frontend/src/metabase/components/TextEditor.jsx
+++ frontend/src/metabase/components/TextEditor.jsx
@@ -1,5 +1,3 @@
-/*global ace*/
-
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
--- frontend/src/metabase/query_builder/components/expressions/ExpressionEditorTextfield.jsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionEditorTextfield.jsx
@@ -1,5 +1,3 @@
-/*global ace*/
-
 import React from "react";
 import PropTypes from "prop-types";
 
