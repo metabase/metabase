9ced9bd7df63c18403be677ec327fb194152e3a0
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2021-03-31 23:53:28 +0300

    disable submit button on pristine user settings form (#15385)
    * disable submit button on pristine user settings form
    
    * replace using dirty state with pristine state inside admin panel forms

--- frontend/src/metabase/containers/Form.jsx
+++ frontend/src/metabase/containers/Form.jsx
@@ -65,6 +65,7 @@ type FormObject = {
   initial: () => FormValues,
   normalize: (values: FormValues) => FormValues,
   validate: (values: FormValues, props: FormProps) => FormErrors,
+  disablePristineSubmit?: boolean,
 };
 
 type FormProps = {
@@ -76,7 +77,10 @@ type Props = {
   initialValues?: ?FormValues,
   formName?: string,
   onSubmit: (values: FormValues) => Promise<any>,
-  formComponent?: React.Component,
+  onSubmitSuccess: (action: any) => Promise<any>,
+  formComponent?: React$Component<any, any, any>,
+  dispatch: Function,
+  values: FormValues,
 };
 
 type State = {
@@ -239,7 +243,6 @@ export default class Form extends React.Component {
       Object.keys(prevState.inlineFields),
     );
     if (newFields.length > 0) {
-      // $FlowFixMe: dispatch provided by connect
       this.props.dispatch(
         initialize(this.props.formName, this._getInitialValues(), newFields),
       );
@@ -316,8 +319,14 @@ export default class Form extends React.Component {
     }
   };
 
+  _handleSubmitSuccess = async (action: any) => {
+    await this.props.onSubmitSuccess(action);
+    this.props.dispatch(
+      initialize(this.props.formName, this.props.values, this._getFieldNames()),
+    );
+  };
+
   _handleChangeField = (fieldName: FormFieldName, value: FormValue) => {
-    // $FlowFixMe: dispatch provided by @connect
     return this.props.dispatch(change(this.props.formName, fieldName, value));
   };
 
@@ -338,6 +347,7 @@ export default class Form extends React.Component {
         initialValues={initialValues}
         validate={this._validate}
         onSubmit={this._onSubmit}
+        onSubmitSuccess={this._handleSubmitSuccess}
         onChangeField={this._handleChangeField}
         // HACK: _state is a mutable object so we can pass by reference into the ReduxFormComponent
         submitState={this._state}
