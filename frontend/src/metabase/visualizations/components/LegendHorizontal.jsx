13765ae4e76357b95281b790e91383506e44c9e4
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-04-01 01:25:00 +0200

    Handle errors in `LegendHorizontal.jsx`
    Note: dynamic refs

--- frontend/src/metabase/visualizations/components/LegendHorizontal.jsx
+++ frontend/src/metabase/visualizations/components/LegendHorizontal.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import styles from "./Legend.css";
 
 import LegendItem from "./LegendItem";
@@ -7,17 +6,21 @@ import LegendItem from "./LegendItem";
 import cx from "classnames";
 
 export default class LegendHorizontal extends Component {
+  constructor(props) {
+    super(props);
+
+    props.titles.map((title, index) => {
+      this["legendItem" + index] = React.createRef();
+    });
+  }
   render() {
     const { className, titles, colors, hovered, onHoverChange } = this.props;
     return (
-      <ol
-        ref="container"
-        className={cx(className, styles.Legend, styles.horizontal)}
-      >
+      <ol className={cx(className, styles.Legend, styles.horizontal)}>
         {titles.map((title, index) => (
           <li key={index}>
             <LegendItem
-              ref={"legendItem" + index}
+              ref={this["legendItem" + index]}
               title={title}
               color={colors[index % colors.length]}
               isMuted={
@@ -28,9 +31,7 @@ export default class LegendHorizontal extends Component {
                 onHoverChange &&
                 onHoverChange({
                   index,
-                  element: ReactDOM.findDOMNode(
-                    this.refs["legendItem" + index],
-                  ),
+                  element: this["legendItem" + index].current,
                 })
               }
               onMouseLeave={() => onHoverChange && onHoverChange(null)}
