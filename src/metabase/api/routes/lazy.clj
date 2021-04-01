b081df2b289f59f0a5835b65e6db88b99aa3426e
Cam Saul <github@camsaul.com>
2021-03-31 20:14:21 -0700

    Lazy loading API routes extreme performance edition

--- src/metabase/api/routes/lazy.clj
+++ src/metabase/api/routes/lazy.clj
@@ -1,5 +1,8 @@
 (ns metabase.api.routes.lazy
-  "Utils for creating lazy-loading Compojure routes, for fast Metabase startup times."
+  "Utils for creating lazy-loading Compojure routes, for fast Metabase startup times.
+
+  Some of the code here relies on a little bit of knowledge about Compojure/Clout internals -- look at the way normal
+  Compojure `routes` and `context` work if the things we're doing here aren't clear."
   (:require [clojure.tools.logging :as log]
             [compojure.core :as compojure]
             [metabase.plugins.classloader :as classloader]))
@@ -25,29 +28,73 @@
      (log/debugf "Lazy-loading API routes in %s" ns-symb)
      ;; classloader/require is thread-safe
      (classloader/require ns-symb)
-     (ns-resolve ns-symb 'routes))))
+     (var-get (ns-resolve ns-symb 'routes)))))
 
-(defmacro context
+(defn context
   "Basically the same as normal Compojure `context`, but avoids reevaluating the `handler` form on every request, which
   is important for lazy-loading stuff. One difference: doesn't take an args vector, which we don't make a lot of use
   of in Metabase.
 
     (lazy/context \"/mt\" my-handler)"
   [route handler]
-  `(let [handler# ~handler]
-     (compojure/context ~route [] handler#)))
+  (compojure/make-context
+   (#'compojure/context-route route)
+   (constantly handler)))
 
-(defmacro form-context
-  "Impl for `routes` macro; don't use directly."
+(defn- route+handler
+  "Convert a form passed to `routes*` to a pair of `[route handler]`.
+
+    (route+handler 'metabase.api '(+auth card)) ;-> [\"/card/\" (+auth (ns-handler 'metabase.api.card))]"
   [root-namespace form]
   (letfn [(unwrap [form]
             (if (symbol? form)
-              [(str "/" form) `(ns-handler '~(symbol (str root-namespace \. form)))]
+              [(str "/" form) (ns-handler (symbol (str root-namespace \. form)))]
               (let [[middleware nested-form] form
                     [route handler]           (unwrap nested-form)]
                 [route (list middleware handler)])))]
-    (let [[route handler] (unwrap form)]
-      `(context ~route ~handler))))
+    (unwrap form)))
+
+(defn- path-info
+  "Get the current path for the request. This is the same thing Compojure/Clout do under the hood. `:path-info` is the
+  part of the request path we haven't handled yet; e.g. for `GET /api/card` the handler in `metabase.api.routes` would
+  see a `:path-info` of `/card`."
+  [request]
+  (or (:path-info request)
+      (:uri request)))
+
+(defn- split-context
+  "Split a request URI like `/card/1` into two parts, the 'context' (`/card`) and everything else (`/1`). The context is
+  used for routing to send this request to the appropriate handler. The other stuff is passed along to the next
+  handler so it can do whatever is appropriate with it.
+
+  Splitting a string with `loop` is significantly faster than using a regex -- I profiled a bunch of different things
+  and this is the fastest method I came up with."
+  [^String s]
+  (let [len (count s)]
+    (loop [i 1]
+      (when (< i len)
+        (if (= (.charAt s i) \/)
+          [(.substring s 0 i)
+           (.substring s i (count s))]
+          (recur (inc i)))))))
+
+(defn routes*
+  "Impl for `routes` macro; don't call directly."
+  [root-namespace forms]
+  ;; build a map of route context -> handler e.g.
+  ;; {"/card" <card-handler>
+  ;;  "/dashboard" <dashboard-handler>}
+  (let [handler-map (into {} (for [form forms]
+                               (route+handler root-namespace form)))]
+    (fn [request respond raise]
+      ;; look for a matching handler based on the context.
+      (let [[context more] (split-context (path-info request))]
+        (if-let [handler (get handler-map context)]
+          ;; If we find one, update `:path-info` with everything after `context` and forward the request to it -- this
+          ;; is the same thing Compojure does under the hood
+          (handler (assoc request :path-info more) respond raise)
+          ;; otherwise return `nil`; Compojure will try the next handler
+          (respond nil))))))
 
 (defmacro routes
   "Similar to normal Compojure `routes` in combination with `context`, but lazy loads routes upon first matching
@@ -71,5 +118,4 @@
   understand what it does. <3 Cam"
   {:style/indent 1}
   [root-namespace & forms]
-  `(compojure/routes ~@(for [form forms]
-                         `(form-context ~root-namespace ~form))))
+  `(routes* '~root-namespace '~forms))
