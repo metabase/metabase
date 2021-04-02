1a46cc6bd7142e6818b4ef9b7fb989ab8fd73795
Cam Saul <1455846+camsaul@users.noreply.github.com>
2021-04-01 20:05:09 -0700

    Improve card permissions errors (#15424)
    * Explicitly specify %throwable in Log4j logging pattern so the ex-data gets printed
    
    This tweaks means Exceptions get rendered for logging with .toString instead .getMessage.
    
    See https://github.com/clojure/tools.logging/tree/31073fe1fbb0533f3c775fa667de70809f24b127#log4j2
    and https://logging.apache.org/log4j/2.x/manual/layouts.html#Patterns
    
    * Remove ad-hoc query perms check on save logic from metabase.models.card.
    
    We already do this in metabase.api.card, so in practice this is redundant code.
    
    * Better permissions exceptions when missing ad-hoc perms for creating/updating a Card
    
    * Fix #11719
    
    * Return more info with most API exception responses
    
    * Add tests for #11719 and #14931
    
    * Fix namespace lint errors
    
    * Fix error message grammar (thanks @jeff303)

--- test/metabase/api/user_test.clj
+++ test/metabase/api/user_test.clj
@@ -12,6 +12,7 @@
             [metabase.test.fixtures :as fixtures]
             [metabase.util :as u]
             [metabase.util.i18n :as i18n]
+            [schema.core :as s]
             [toucan.db :as db]
             [toucan.hydrate :as hydrate :refer [hydrate]]))
 
@@ -665,8 +666,9 @@
                (mt/user-http-request :crowberto :put 404 (format "user/%s/reactivate" Integer/MAX_VALUE)))))
 
       (testing " Attempting to reactivate an already active user should fail"
-        (is (= {:message "Not able to reactivate an active user"}
-               (mt/user-http-request :crowberto :put 400 (format "user/%s/reactivate" (mt/user->id :rasta)))))))
+        (is (schema= {:message  (s/eq "Not able to reactivate an active user")
+                      s/Keyword s/Any}
+                     (mt/user-http-request :crowberto :put 400 (format "user/%s/reactivate" (mt/user->id :rasta)))))))
 
     (testing (str "test that when disabling Google auth if a user gets disabled and re-enabled they are no longer "
                   "Google Auth (#3323)")
