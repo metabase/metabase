57a3e2dd892224889643750581dda8bb2625a9b2
Tim Macdonald <tsmacdonald@gmail.com>
2021-04-05 16:47:25 -0500

    Merge pulse/dashboard parameters correctly for dashboard subs

--- enterprise/backend/src/metabase_enterprise/pulse.clj
+++ enterprise/backend/src/metabase_enterprise/pulse.clj
@@ -3,11 +3,19 @@
             [metabase.pulse.interface :as i])
   (:import metabase.pulse.interface.SubscriptionParameters))
 
-(def ^:private parameters-impl
+(def parameters-impl
+  "Blend parameters from dashboard subscription and the dashboard itself"
   (reify
     i/SubscriptionParameters
     (the-parameters [_ pulse dashboard]
-      (some seq (map :parameters [pulse dashboard])))))
+      (let [pulse-params           (:parameters pulse)
+            dashboard-params       (:parameters dashboard)
+            pulse-params-by-id     (group-by :id pulse-params)
+            dashboard-params-by-id (group-by :id dashboard-params)
+            ids                    (distinct (map :id (concat pulse-params dashboard-params)))]
+        (for [id ids]
+          (merge (first (get dashboard-params-by-id id))
+                 (first (get pulse-params-by-id id))))))))
 
 (def ee-strategy-parameters-impl
   "Enterprise way of getting dashboard filter parameters"
