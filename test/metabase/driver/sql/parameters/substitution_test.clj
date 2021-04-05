10e5c5a243c482f48bd86a8b7027f14596956859
dan sutton <dan@dpsutton.com>
2021-04-05 15:52:39 -0500

    (#15460) Correct field names in native field filters
    ```sql
    select p.created_at, products.category
    from products
    left join products p on p.id=products.id
    where {{category}}
    ```
    
    field filter is a string/= on category, a column in this twice due to
    the self join.
    
    Just mimic how the rest of this emits a column name

--- test/metabase/driver/sql/parameters/substitution_test.clj
+++ test/metabase/driver/sql/parameters/substitution_test.clj
@@ -2,10 +2,22 @@
   "Most of the code in `metabase.driver.sql.parameters.substitution` is actually tested by
   `metabase.driver.sql.parameters.substitute-test`."
   (:require [clojure.test :refer :all]
-            [metabase.driver.sql.parameters.substitution :as substitution]))
+            [metabase.driver.sql.parameters.substitution :as substitution]
+            [metabase.models :refer [Field]]
+            [metabase.test :as mt]))
 
 (deftest honeysql->replacement-snippet-info-test
   (testing "make sure we handle quotes inside names correctly!"
     (is (= {:replacement-snippet     "\"test-data\".\"PUBLIC\".\"checkins\".\"date\""
             :prepared-statement-args nil}
            (#'substitution/honeysql->replacement-snippet-info :h2 :test-data.PUBLIC.checkins.date)))))
+
+(deftest field-filter->replacement-snippet-info-test
+  (testing "Ensure native snippet expansion uses proper names for fields (#15460)"
+    (is (= {:replacement-snippet "\"PUBLIC\".\"VENUES\".\"NAME\" = ?"
+            :prepared-statement-args ["Doohickey"]}
+           (#'substitution/field-filter->replacement-snippet-info
+            :h2
+            {:field (Field (mt/id :venues :name))
+             :value {:type  :string/=
+                     :value ["Doohickey"]}})))))
