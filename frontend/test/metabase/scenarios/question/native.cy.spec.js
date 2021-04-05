5392dc4af6d3a0c72123dac24e40cdc226119037
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-04-05 21:35:07 +0200

    Add repro for #15444

--- frontend/test/metabase/scenarios/question/native.cy.spec.js
+++ frontend/test/metabase/scenarios/question/native.cy.spec.js
@@ -568,4 +568,42 @@ describe("scenarios > question > native", () => {
       cy.findAllByText("Gizmo").should("not.exist");
     });
   });
+
+  it.skip("should run with the default field filter set (metabase#15444)", () => {
+    cy.intercept("POST", "/api/dataset").as("dataset");
+
+    cy.visit("/");
+    cy.icon("sql").click();
+    cy.get(".ace_content").type("select * from products where {{category}}", {
+      parseSpecialCharSequences: false,
+    });
+    // Change filter type from "Text" to Field Filter
+    cy.get(".AdminSelect")
+      .contains("Text")
+      .click();
+    popover()
+      .findByText("Field Filter")
+      .click();
+    popover()
+      .findByText("Products")
+      .click();
+    popover()
+      .findByText("Category")
+      .click();
+    cy.findByText("Required?").scrollIntoView();
+    // Add the default value
+    cy.findByText("Enter a default value...").click();
+    popover()
+      .findByText("Doohickey")
+      .click();
+    cy.findByRole("button", { name: "Add filter" }).click();
+    cy.get(".NativeQueryEditor .Icon-play").click();
+    cy.wait("@dataset").then(xhr => {
+      expect(xhr.response.body.error).not.to.exist;
+    });
+    cy.get(".Visualization").within(() => {
+      cy.findAllByText("Doohickey");
+      cy.findAllByText("Gizmo").should("not.exist");
+    });
+  });
 });
