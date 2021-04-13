80ea7990c132e75e2971cec7580ad9d99393076c
howon lee <hlee.howon@gmail.com>
2021-04-13 13:09:11 -0700

    did not actually get rid of it, lol

--- frontend/src/metabase/query_builder/components/DimensionList.jsx
+++ frontend/src/metabase/query_builder/components/DimensionList.jsx
@@ -127,7 +127,6 @@ export default class DimensionList extends Component {
               multiSelect,
             )}
             tetherOptions={multiSelect ? null : SUBMENU_TETHER_OPTIONS}
-            sizeToFit
           >
             {({ onClose }) => (
               <DimensionPicker
