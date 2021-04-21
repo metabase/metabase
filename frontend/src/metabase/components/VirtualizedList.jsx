254e28f0ead6fbdf1c05b7c82b45432a150011d8
Anton Kulyk <kuliks.anton@gmail.com>
2021-04-21 16:45:42 +0300

    Optimize VirtualizedList

--- frontend/src/metabase/components/VirtualizedList.jsx
+++ frontend/src/metabase/components/VirtualizedList.jsx
@@ -1,39 +1,41 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-
 import { List, WindowScroller, AutoSizer } from "react-virtualized";
+import "react-virtualized/styles.css";
 
-const VirtualizedList = ({
-  items,
-  rowHeight,
-  renderItem,
-  useAutoSizerHeight = false,
-}) => (
-  <AutoSizer>
-    {({ height: autosizerHeight, width }) => (
-      <WindowScroller>
-        {({ height, isScrolling, scrollTop }) => (
-          <List
-            autoHeight
-            width={width}
-            height={Math.min(
-              useAutoSizerHeight ? autosizerHeight : height,
-              rowHeight * items.length,
-            )}
-            isScrolling={isScrolling}
-            rowCount={items.length}
-            rowHeight={rowHeight}
-            rowRenderer={({ index, key, style }) => (
-              <div key={key} style={style}>
-                {renderItem({ item: items[index], index })}
-              </div>
-            )}
-            scrollTop={scrollTop}
-          />
-        )}
-      </WindowScroller>
-    )}
-  </AutoSizer>
-);
+function VirtualizedList({ items, rowHeight, renderItem }) {
+  const rowRenderer = React.useCallback(
+    ({ index, key, style }) => (
+      <div key={key} style={style}>
+        {renderItem({ item: items[index], index })}
+      </div>
+    ),
+    [items, renderItem],
+  );
+
+  const renderScrollComponent = React.useCallback(
+    ({ width }) => {
+      return (
+        <WindowScroller>
+          {({ height, isScrolling, scrollTop }) => (
+            <List
+              autoHeight
+              width={width}
+              height={Math.min(height, rowHeight * items.length)}
+              isScrolling={isScrolling}
+              rowCount={items.length}
+              rowHeight={rowHeight}
+              rowRenderer={rowRenderer}
+              scrollTop={scrollTop}
+            />
+          )}
+        </WindowScroller>
+      );
+    },
+    [items, rowHeight, rowRenderer],
+  );
+
+  return <AutoSizer>{renderScrollComponent}</AutoSizer>;
+}
 
 export default VirtualizedList;
