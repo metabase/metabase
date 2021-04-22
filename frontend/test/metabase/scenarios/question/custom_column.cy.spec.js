8de741206959a86500d6aaf1298fbf9ef1484dac
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-04-22 19:49:54 +0200

    #15741 Repro: Custom expressions don't recognize floating point numbers with 0 omitted (#15753)

--- frontend/test/metabase/scenarios/question/custom_column.cy.spec.js
+++ frontend/test/metabase/scenarios/question/custom_column.cy.spec.js
@@ -481,4 +481,13 @@ describe("scenarios > question > custom columns", () => {
     });
     cy.get("[contenteditable='true']").contains("Sum([MyCC [2021]])");
   });
+
+  it.skip("should handle floating point numbers with '0' omitted (metabase#15741)", () => {
+    openOrdersTable({ mode: "notebook" });
+    cy.findByText("Custom column").click();
+    cy.get("[contenteditable='true']").type(".5 * [Discount]");
+    cy.findByPlaceholderText("Something nice and descriptive").type("Foo");
+    cy.findByText("Unknown Field: .5").should("not.exist");
+    cy.findByRole("button", { name: "Done" }).should("not.be.disabled");
+  });
 });
