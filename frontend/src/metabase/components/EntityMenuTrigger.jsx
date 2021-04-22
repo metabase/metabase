992712a139abd38b40783b2a2ecfc844722f381f
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-04-21 18:06:19 -0700

    support button EntityMenuTrigger + a few additional optional item props

--- frontend/src/metabase/components/EntityMenuTrigger.jsx
+++ frontend/src/metabase/components/EntityMenuTrigger.jsx
@@ -2,10 +2,22 @@
 import React from "react";
 
 import Icon, { IconWrapper } from "metabase/components/Icon";
+import Button from "metabase/components/Button";
 import Tooltip from "metabase/components/Tooltip";
 
-const EntityMenuTrigger = ({ icon, onClick, open, tooltip, triggerProps }) => {
-  const trigger = (
+const EntityMenuTrigger = ({
+  children,
+  icon,
+  onClick,
+  open,
+  tooltip,
+  triggerProps,
+}) => {
+  const trigger = children ? (
+    <Button icon={icon} onClick={onClick} {...triggerProps}>
+      {children}
+    </Button>
+  ) : (
     <IconWrapper onClick={onClick} {...triggerProps}>
       <Icon size={18} name={icon} m={1} />
     </IconWrapper>
