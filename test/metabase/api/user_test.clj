f7505cc15ee8dc45e2c6611c31f1240caef9d6de
howon lee <hlee.howon@gmail.com>
2021-04-23 10:14:19 -0700

    juice the coverity lol

--- test/metabase/api/user_test.clj
+++ test/metabase/api/user_test.clj
@@ -155,7 +155,8 @@
 (deftest user-list-limit-test
   (testing "GET /api/user?limit=1&offset=1"
     (testing "Limit and offset pagination have to have both limit and offset"
-      (is (= "When including a limit, an offset must also be included." (mt/user-http-request :crowberto :get 400 "user" :limit "1"))))
+      (is (= "When including a limit, an offset must also be included." (mt/user-http-request :crowberto :get 400 "user" :limit "1")))
+      (is (= "When including an offset, a limit must also be included." (mt/user-http-request :crowberto :get 400 "user" :offset "1"))))
     (testing "Limit and offset pagination works for user list"
       (is (= [{:id          (mt/user->id :lucky)
                :email       "lucky@metabase.com"
