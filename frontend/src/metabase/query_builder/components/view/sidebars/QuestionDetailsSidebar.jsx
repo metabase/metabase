b0bd419207a31382c5d954fc8c8780021294c8dd
Dalton <daltojohnso@users.noreply.github.com>
2021-04-27 08:56:36 -0700

    first pass at button to trigger the creation of moderation issues (#15789)
    * support button EntityMenuTrigger + a few additional optional item props
    
    * first pass at  ModerationIssueActionMenu
    
    import the ee moderation stuff
    
    rmv unnecessary cx usage
    
    localize
    
    play with keeping more biz logic in lib/moderation
    
    name/status --> type
    
    make the moderation ee plugin feel a little more ergonomic
    
    move rest of moderation logic into ee directory
    
    * rmv lib/moderation and move all logic to ee folder

--- frontend/src/metabase/query_builder/components/view/sidebars/QuestionDetailsSidebar.jsx
+++ frontend/src/metabase/query_builder/components/view/sidebars/QuestionDetailsSidebar.jsx
@@ -3,15 +3,34 @@ import PropTypes from "prop-types";
 
 import SidebarContent from "metabase/query_builder/components/SidebarContent";
 import QuestionActionButtons from "metabase/questions/components/QuestionActionButtons";
+import { PLUGIN_MODERATION_COMPONENTS } from "metabase/plugins";
+
+const {
+  active: isPluginActive,
+  ModerationIssueActionMenu,
+} = PLUGIN_MODERATION_COMPONENTS;
 
 function QuestionDetailsSidebar({ question, onOpenModal }) {
   const canWrite = question && question.canWrite();
 
   return (
     <SidebarContent className="full-height px1">
-      <div>
-        <QuestionActionButtons canWrite={canWrite} onOpenModal={onOpenModal} />
-      </div>
+      {isPluginActive ? (
+        <div>
+          <QuestionActionButtons
+            canWrite={canWrite}
+            onOpenModal={onOpenModal}
+          />
+          <ModerationIssueActionMenu onAction={() => {}} />
+        </div>
+      ) : (
+        <div>
+          <QuestionActionButtons
+            canWrite={canWrite}
+            onOpenModal={onOpenModal}
+          />
+        </div>
+      )}
     </SidebarContent>
   );
 }
