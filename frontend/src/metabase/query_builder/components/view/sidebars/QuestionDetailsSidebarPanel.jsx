1f95531a8c991418c8656cfda4c36cf2d766561f
Dalton <daltojohnso@users.noreply.github.com>
2021-04-28 14:25:44 -0700

    add description to details sidebar + edit action icon (#15840)

--- frontend/src/metabase/query_builder/components/view/sidebars/QuestionDetailsSidebarPanel.jsx
+++ frontend/src/metabase/query_builder/components/view/sidebars/QuestionDetailsSidebarPanel.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import SidebarContent from "metabase/query_builder/components/SidebarContent";
 import QuestionActionButtons from "metabase/questions/components/QuestionActionButtons";
+import ClampedText from "metabase/components/ClampedText";
 import { PLUGIN_MODERATION_COMPONENTS } from "metabase/plugins";
 import { SIDEBAR_VIEWS } from "./constants";
 
 const { ModerationIssueActionMenu } = PLUGIN_MODERATION_COMPONENTS;
 
 function QuestionDetailsSidebarPanel({ setView, question, onOpenModal }) {
-  const canWrite = question && question.canWrite();
+  const canWrite = question.canWrite();
+  const description = question.description();
 
   return (
     <SidebarContent className="full-height px1">
       <div>
         <QuestionActionButtons canWrite={canWrite} onOpenModal={onOpenModal} />
+        <ClampedText className="pb2 px2" text={description} visibleLines={8} />
         <ModerationIssueActionMenu
           onAction={issueType => {
             setView({
