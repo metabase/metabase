6337d3f183094662c12754cb1d8b09e4a29074b6
Tim Macdonald <tim@metabase.com>
2021-05-03 14:53:25 -0500

    Let ModerationReviews be created with a status (#15884)

--- src/metabase/api/moderation_review.clj
+++ src/metabase/api/moderation_review.clj
@@ -8,14 +8,16 @@
 
 (api/defendpoint POST "/"
   "Create a new `ModerationReview`."
-  [:as {{:keys [text moderated_item_id moderated_item_type]} :body}]
-  {text                (s/maybe s/Str)
-   moderated_item_id   su/IntGreaterThanZero
-   moderated_item_type moderation/moderated-item-types}
-  (let [review-data {:text                text
-                     :moderated_item_id   moderated_item_id
-                     :moderated_item_type moderated_item_type
-                     :moderator_id        api/*current-user-id*}]
+  [:as {{:keys [text moderated_item_id moderated_item_type status]} :body}]
+  {text                    (s/maybe s/Str)
+   moderated_item_id       su/IntGreaterThanZero
+   moderated_item_type     moderation/moderated-item-types
+   (s/optional-key status) moderation-review/statuses}
+  (let [review-data (merge {:text                text
+                            :moderated_item_id   moderated_item_id
+                            :moderated_item_type moderated_item_type
+                            :moderator_id        api/*current-user-id*}
+                           (when status {:status status}))]
     ;;TODO permissions
     (api/check-500
      (moderation-review/create-review! review-data))))
