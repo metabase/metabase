71f71b2cbb6081c29311bec5fabf3e4404bcd445
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-05-04 11:16:45 -0700

    add open issues panel with mock data

--- enterprise/frontend/src/metabase-enterprise/moderation/components/ModerationIssueActionMenu.jsx
+++ enterprise/frontend/src/metabase-enterprise/moderation/components/ModerationIssueActionMenu.jsx
@@ -3,26 +3,40 @@ import PropTypes from "prop-types";
 
 import EntityMenu from "metabase/components/EntityMenu";
 import { MODERATION_TEXT } from "metabase-enterprise/moderation/constants";
-import { getModerationActionsList } from "metabase-enterprise/moderation";
+import {
+  getModerationIssueTypes,
+  getModerationRequestActionTypes,
+  getColor,
+  getModerationStatusIcon,
+} from "metabase-enterprise/moderation";
 
-function ModerationIssueActionMenu({ className, onAction }) {
-  const moderationActionsList = getModerationActionsList();
+function ModerationIssueActionMenu({
+  className,
+  triggerClassName,
+  onAction,
+  issue,
+}) {
+  const types = issue
+    ? getModerationRequestActionTypes()
+    : getModerationIssueTypes();
 
   return (
     <EntityMenu
       triggerChildren={MODERATION_TEXT.moderator.action}
       triggerProps={{
         iconRight: "chevrondown",
-        round: true,
-        className: "text-brand border-brand",
+        className: triggerClassName,
       }}
       className={className}
-      items={moderationActionsList.map(({ type, icon, color }) => {
+      items={types.map(type => {
+        const color = getColor(type);
+        const icon = getModerationStatusIcon(type);
         return {
           icon,
           iconSize: 18,
           className: `text-${color}`,
           action: () => onAction(type),
+          // todo  -- fn to change title when issue exists
           title: MODERATION_TEXT.moderator[type].action,
         };
       })}
@@ -32,7 +46,9 @@ function ModerationIssueActionMenu({ className, onAction }) {
 
 ModerationIssueActionMenu.propTypes = {
   className: PropTypes.string,
+  triggerClassName: PropTypes.string,
   onAction: PropTypes.func,
+  issue: PropTypes.object,
 };
 
 export default ModerationIssueActionMenu;
