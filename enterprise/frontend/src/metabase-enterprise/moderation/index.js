2bedb96cca9a9cc7820781735f1b4f363f6a1ff4
Dalton <daltojohnso@users.noreply.github.com>
2021-05-07 09:23:28 -0700

    add open moderation issues panel with mock data (#15918)
    * add open issues panel with mock data
    
    * rmv comment

--- enterprise/frontend/src/metabase-enterprise/moderation/index.js
+++ enterprise/frontend/src/metabase-enterprise/moderation/index.js
@@ -6,10 +6,14 @@ import {
 import { ACTIONS } from "metabase-enterprise/moderation/constants";
 import ModerationIssueActionMenu from "metabase-enterprise/moderation/components/ModerationIssueActionMenu";
 import CreateModerationIssuePanel from "metabase-enterprise/moderation/components/CreateModerationIssuePanel";
+import { OpenModerationIssuesButton } from "metabase-enterprise/moderation/components/OpenModerationIssuesButton";
+import { OpenModerationIssuesPanel } from "metabase-enterprise/moderation/components/OpenModerationIssuesPanel";
 
 Object.assign(PLUGIN_MODERATION_COMPONENTS, {
   ModerationIssueActionMenu,
   CreateModerationIssuePanel,
+  OpenModerationIssuesButton,
+  OpenModerationIssuesPanel,
 });
 
 Object.assign(PLUGIN_MODERATION_SERVICE, {
@@ -17,8 +21,12 @@ Object.assign(PLUGIN_MODERATION_SERVICE, {
   getColorForReview,
 });
 
-export function getModerationActionsList() {
-  return [ACTIONS.verified, ACTIONS.misleading, ACTIONS.confusing];
+export function getModerationIssueTypes() {
+  return ["verified", "misleading", "confusing"];
+}
+
+export function getModerationRequestActionTypes() {
+  return [...getModerationIssueTypes(), "dismiss"];
 }
 
 export function getStatusIconForReview(review) {
@@ -35,4 +43,34 @@ export function getModerationStatusIcon(type) {
 
 export function getColor(type) {
   return getIn(ACTIONS, [type, "color"]);
+}
+
+export function getOpenIssues() {
+  return [
+    {
+      id: 1,
+      type: "verified",
+      title: "User 2",
+      text: "1\n2\n3\n4\n5\n6",
+      timestamp: Date.now(),
+      comments: [],
+    },
+    {
+      id: 2,
+      type: "misleading",
+      title: "User 2",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet sagittis dui. Morbi in odio laoreet, finibus erat vitae, sagittis dui. Ut at mauris eget ligula volutpat pellentesque. Integer non faucibus urna. Maecenas faucibus ornare gravida. Aliquam orci tortor, ullamcorper et vehicula accumsan, malesuada in ipsum. Nullam auctor, justo et mattis fringilla, enim ipsum aliquet nunc, quis posuere odio erat in nulla. Suspendisse elementum, est et rutrum volutpat, purus mi placerat odio, sit amet blandit nulla diam at lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ultricies placerat mollis.",
+      timestamp: Date.now(),
+      comments: [
+        {
+          id: 1,
+          title: "Moderator",
+          isModerator: true,
+          text: "Nulla nec est eu est condimentum facilisis sit amet et ex.",
+          timestamp: Date.now(),
+        },
+      ],
+    },
+  ];
 }
