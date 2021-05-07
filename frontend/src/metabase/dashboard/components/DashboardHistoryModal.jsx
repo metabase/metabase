48708477d3e0022d4032f4e5e4564a1bfc00bc6b
Anton Kulyk <kuliks.anton@gmail.com>
2021-05-07 14:53:57 +0300

    Fix dashboard history

--- frontend/src/metabase/dashboard/components/DashboardHistoryModal.jsx
+++ frontend/src/metabase/dashboard/components/DashboardHistoryModal.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { withRouter } from "react-router";
 
 import HistoryModal from "metabase/containers/HistoryModal";
-import { withRouter } from "react-router";
+import * as Urls from "metabase/lib/urls";
 import Dashboards from "metabase/entities/dashboards";
 
 @withRouter
 @Dashboards.load({
-  id: (state, props) => parseInt(props.params.dashboardId),
+  id: (state, props) => Urls.extractEntityId(props.params.slug),
   wrapped: false,
 })
 export default class DashboardHistoryModal extends React.Component {
