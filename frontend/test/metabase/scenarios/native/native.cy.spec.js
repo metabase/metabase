27f5495dce63c4ac32d7df0423e0f1674f02801b
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-05-10 20:04:49 +0200

    #15981 Repro: Feature flag causes problems with Text and Number filters in Native query (#15985)
    Co-authored-by: flamber <1447303+flamber@users.noreply.github.com>

--- frontend/test/metabase/scenarios/native/native.cy.spec.js
+++ frontend/test/metabase/scenarios/native/native.cy.spec.js
@@ -648,9 +648,11 @@ describe("scenarios > question > native", () => {
   it.skip("should be able to add new columns after hiding some (metabase#15393)", () => {
     cy.visit("/");
     cy.icon("sql").click();
-    cy.get(".ace_content").type("select 1 as visible, 2 as hidden");
-    cy.get(".NativeQueryEditor .Icon-play")
+    cy.get(".ace_content")
       .as("editor")
+      .type("select 1 as visible, 2 as hidden");
+    cy.get(".NativeQueryEditor .Icon-play")
+      .as("runQuery")
       .click();
     cy.findByText("Settings").click();
     cy.findByTestId("sidebar-left")
@@ -659,9 +661,67 @@ describe("scenarios > question > native", () => {
       .siblings(".Icon-close")
       .click();
     cy.get("@editor").type("{movetoend}, 3 as added");
-    cy.get(".NativeQueryEditor .Icon-play")
-      .as("editor")
-      .click();
+    cy.get("@runQuery").click();
     cy.get("@sidebar").contains(/added/i);
+  });
+
+  ["off", "on"].forEach(testCase => {
+    const isFeatureFlagTurnedOn = testCase === "off" ? false : true;
+
+    describe.skip("Feature flag causes problems with Text and Number filters in Native query (metabase#15981)", () => {
+      beforeEach(() => {
+        mockSessionProperty(
+          "field-filter-operators-enabled?",
+          isFeatureFlagTurnedOn,
+        );
+
+        cy.visit("/");
+        cy.icon("sql").click();
+        cy.get(".ace_content").as("editor");
+
+        cy.intercept("POST", "/api/dataset").as("dataset");
+      });
+
+      it(`text filter should work with the feature flag turned ${testCase}`, () => {
+        cy.get("@editor").type(
+          "select * from PRODUCTS where CATEGORY = {{text_filter}}",
+          {
+            parseSpecialCharSequences: false,
+          },
+        );
+        cy.get("fieldset").type("Gizmo");
+        cy.get(".NativeQueryEditor .Icon-play").click();
+        cy.wait("@dataset");
+        cy.findByText("Rustic Paper Wallet");
+        cy.icon("contract").click();
+        cy.findByText("Showing 51 rows");
+        cy.get(".RunButton").should("not.exist");
+      });
+
+      it(`number filter should work with the feature flag turned ${testCase}`, () => {
+        cy.get("@editor").type(
+          "select * from ORDERS where QUANTITY = {{number_filter}}",
+          {
+            parseSpecialCharSequences: false,
+          },
+        );
+        cy.findByTestId("sidebar-right")
+          .as("sidebar")
+          .within(() => {
+            cy.get(".AdminSelect")
+              .contains("Text")
+              .click();
+          });
+        popover()
+          .contains("Number")
+          .click();
+        cy.get("fieldset").type("20");
+        cy.get(".NativeQueryEditor .Icon-play").click();
+        cy.wait("@dataset").then(xhr => {
+          expect(xhr.response.body.error).not.to.exist;
+        });
+        cy.get(".Visualization").contains("23.54");
+      });
+    });
   });
 });
