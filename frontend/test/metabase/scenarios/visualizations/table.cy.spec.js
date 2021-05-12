22436168166c023ef54a2264920add76c568fbca
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2021-05-12 23:41:27 +0300

    format links on the table visualization (#15964)
    * fix question level link display settings
    
    * add cypress spec for the question-level link settings on the table visualization
    
    * Address review comments: add a spec for clicn behavior with view_as = link, fix admin column settings
    
    * revert a straightforward fix for hiding irrelevant settings
    
    * fixup! Address review comments: add a spec for clicn behavior with view_as = link, fix admin column settings
    
    * prettier
    
    * handle link_text null value

---
+++ frontend/test/metabase/scenarios/visualizations/table.cy.spec.js
@@ -0,0 +1,53 @@
+import { restore, visitQuestionAdhoc, popover } from "__support__/e2e/cypress";
+import { SAMPLE_DATASET } from "__support__/e2e/cypress_sample_dataset";
+
+const { PEOPLE_ID } = SAMPLE_DATASET;
+
+const testQuery = {
+  database: 1,
+  query: {
+    "source-table": PEOPLE_ID,
+  },
+  type: "query",
+};
+
+describe("scenarios > visualizations > table", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("should allow to display any column as link with extrapolated url and text", () => {
+    visitQuestionAdhoc({
+      dataset_query: testQuery,
+      display: "table",
+    });
+
+    cy.findByText("City").click();
+
+    popover().within(() => {
+      cy.icon("gear").click();
+    });
+
+    cy.findByText("Off").click();
+
+    popover().within(() => {
+      cy.findByText("Link").click();
+    });
+
+    cy.findByTestId("link_text").type("{{CITY}} {{ID}} fixed text", {
+      parseSpecialCharSequences: false,
+    });
+    cy.findByTestId("link_url").type("http://metabase.com/people/{{ID}}", {
+      parseSpecialCharSequences: false,
+    });
+
+    cy.findByText("Done").click();
+
+    cy.findByText("Wood River 1 fixed text").should(
+      "have.attr",
+      "href",
+      "http://metabase.com/people/1",
+    );
+  });
+});
