b3afc53a2c161b29fef10655d3657f430816b760
Dalton <daltojohnso@users.noreply.github.com>
2021-05-13 14:03:13 -0700

    add timeline with mock data to sidebar (#15944)
    * add timeline with mock data to sidebar
    
    * make the sidebar scrollbar show and not mess up panel dimensions
    
    * correctly hide sidebar when CLOSED_NATIVE_EDITOR_SIDEBARS action triggers
    
    * hide border next to final event in timeline
    
    * make each timeline item expand to full width
    
    * show revision history in sidebar timeline

--- frontend/src/metabase/query_builder/components/view/ViewSidebar.jsx
+++ frontend/src/metabase/query_builder/components/view/ViewSidebar.jsx
@@ -18,14 +18,14 @@ const ViewSideBar = ({ left, right, width = 355, isOpen, children }) => (
   >
     {motionStyle => (
       <div
-        className={cx("bg-white relative overflow-x-hidden", {
+        className={cx("scroll-y bg-white relative overflow-x-hidden", {
           "border-right": left,
           "border-left": right,
         })}
         style={motionStyle}
       >
         <div
-          className="absolute top bottom scroll-y"
+          className="absolute top bottom"
           style={{
             width: width,
             right: left ? 0 : undefined,
