eb5278a6af90a9dd07fbffc02bd63cbd57de2be2
Noah Moss <noahbmoss@gmail.com>
2021-05-13 12:01:11 -0700

    run prettier

--- frontend/src/metabase/user/containers/UserSettingsApp.jsx
+++ frontend/src/metabase/user/containers/UserSettingsApp.jsx
@@ -5,7 +5,12 @@ import { connect } from "react-redux";
 import UserSettings from "../components/UserSettings";
 import { selectors } from "../selectors";
 
-import { setTab, updatePassword, updateUser, validatePassword } from "../actions";
+import {
+  setTab,
+  updatePassword,
+  updateUser,
+  validatePassword,
+} from "../actions";
 
 const mapStateToProps = (state, props) => {
   return {
