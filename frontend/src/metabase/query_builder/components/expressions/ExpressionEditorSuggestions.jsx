0cfa24eacc447791f1960311301aba88fa2bc2bc
Gustavo Saiani <gustavo@poe.ma>
2021-05-14 18:39:28 -0300

    Extract styled components from suggestions file

--- frontend/src/metabase/query_builder/components/expressions/ExpressionEditorSuggestions.jsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionEditorSuggestions.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 
 import { t } from "ttag";
 import cx from "classnames";
 
 import Popover from "metabase/components/Popover";
+import {
+  UlStyled,
+  LiStyled,
+  LiStyledHighlighted,
+  LiTitle,
+} from "./ExpressionEditorSuggestions.styled";
 
 import { isObscured } from "metabase/lib/dom";
 
@@ -16,26 +21,6 @@ const SUGGESTION_SECTION_NAMES = {
   metrics: t`Metrics`,
   other: t`Other`,
 };
-
-const UlStyled = styled.ul.attrs({ className: "pb1" })`
-  min-width: 150px;
-  overflow-y: auto;
-`;
-
-const liTitleClassName = "mx2 h6 text-uppercase text-bold text-medium py1 pt2";
-const LiTitle = styled.li.attrs({ className: liTitleClassName });
-
-const liStyledClassName =
-  "px2 cursor-pointer text-white-hover bg-brand-hover hover-parent hover--inherit";
-const LiStyled = styled.li.attrs({ className: liStyledClassName })`
-  padding-top: 5px;
-  padding-bottom: 5px;
-`;
-
-const liStyledHighlightedClassName = "text-white bg-brand";
-const LiStyledHighlighted = styled(LiStyled).attrs({
-  className: liStyledHighlightedClassName,
-});
 
 export default class ExpressionEditorSuggestions extends React.Component {
   static propTypes = {
---
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionEditorSuggestions.styled.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const UlStyled = styled.ul.attrs({ className: "pb1" })`
+  min-width: 150px;
+  overflow-y: auto;
+`;
+
+const liTitleClassName = "mx2 h6 text-uppercase text-bold text-medium py1 pt2";
+export const LiTitle = styled.li.attrs({ className: liTitleClassName });
+
+const liStyledClassName =
+  "px2 cursor-pointer text-white-hover bg-brand-hover hover-parent hover--inherit";
+export const LiStyled = styled.li.attrs({ className: liStyledClassName })`
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+const liStyledHighlightedClassName = "text-white bg-brand";
+export const LiStyledHighlighted = styled(LiStyled).attrs({
+  className: liStyledHighlightedClassName,
+});
