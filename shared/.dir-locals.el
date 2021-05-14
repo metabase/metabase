90890745b5a8a4966ec804833034b661709f31c2
Cam Saul <github@camsaul.com>
2021-05-14 15:09:52 -0700

    clojurescript-mode is derived from clojure-mode, combine the dir locals

--- shared/.dir-locals.el
+++ shared/.dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((cider-default-cljs-repl . shadow-select)
-         (cider-shadow-default-options . "node-repl")))
- (clojure-mode . ((cider-preferred-build-tool . shadow-cljs))))
+((clojure-mode . ((cider-default-cljs-repl . shadow-select)
+                  (cider-shadow-default-options . "node-repl")
+                  (cider-preferred-build-tool . shadow-cljs))))
