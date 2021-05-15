73f41282bfedf5d3ff662d2454c5dea142adb886
Dennis Schridde <devurandom@gmx.net>
2021-05-15 12:08:05 +0200

    Extend migration linter to check dbms preconditions

--- bin/lint-migrations-file/src/change_set/common.clj
+++ bin/lint-migrations-file/src/change_set/common.clj
@@ -14,5 +14,15 @@
 
 (s/def ::author string?)
 
+(s/def ::preConditions
+  (s/coll-of ::pre-condition))
+
+(s/def ::pre-condition
+  (s/keys :opt-un [::dbms]))
+
+(s/def ::dbms
+  (s/keys :req-un [::type]))
+
 (s/def ::change-set
-  (s/keys :req-un [::id ::author]))
+  (s/keys :req-un [::id ::author]
+          :opt-un [::preConditions]))
