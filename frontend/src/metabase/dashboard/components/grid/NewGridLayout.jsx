e63066162e7e9b3d68c01a0bae303b8023ca55bc
Anton Kulyk <kuliks.anton@gmail.com>
2021-05-17 12:01:05 +0300

    Add basic NewGridLayout implementation

---
+++ frontend/src/metabase/dashboard/components/grid/NewGridLayout.jsx
@@ -0,0 +1,93 @@
+/* eslint-disable react/prop-types */
+import React, { useCallback, useMemo } from "react";
+import ReactGridLayout, { WidthProvider } from "react-grid-layout";
+import _ from "underscore";
+
+import { color } from "metabase/lib/colors";
+
+export { WidthProvider };
+
+function NewGridLayout({
+  items,
+  itemRenderer,
+  layout,
+  cols,
+  width: gridWidth,
+  margin,
+  rowHeight,
+  isEditing,
+  className,
+  ...props
+}) {
+  const cellSize = useMemo(
+    () => ({
+      width: (gridWidth + margin) / cols,
+      height: rowHeight,
+    }),
+    [cols, gridWidth, margin, rowHeight],
+  );
+
+  const background = useMemo(() => {
+    const XMLNS = "http://www.w3.org/2000/svg";
+    const fullWidth = cellSize.width * cols;
+    const cellStrokeColor = color("border");
+
+    const rectangles = _(cols).times(i => {
+      const x = Math.round(margin / 2 + i * cellSize.width);
+      const y = margin / 2;
+      const w = Math.round(cellSize.width - margin);
+      const h = cellSize.height - margin;
+      return `<rect stroke='${cellStrokeColor}' stroke-width='1' fill='none' x='${x}' y='${y}' width='${w}' height='${h}'/>`;
+    });
+
+    const svg = [
+      `<svg xmlns='${XMLNS}' width='${fullWidth}' height='${cellSize.height}'>`,
+      ...rectangles,
+      `</svg>`,
+    ].join("");
+
+    return `url("data:image/svg+xml;utf8,${encodeURIComponent(svg)}")`;
+  }, [cellSize, margin, cols]);
+
+  const renderItem = useCallback(
+    item => {
+      const itemLayout = layout.find(l => String(l.i) === String(item.id));
+      const gridItemWidth = cellSize.width * itemLayout.w - margin;
+      return itemRenderer({
+        item,
+        gridItemWidth,
+      });
+    },
+    [layout, cellSize, margin, itemRenderer],
+  );
+
+  return (
+    <div
+      className={className}
+      style={{
+        position: "relative",
+        width: gridWidth,
+        background: isEditing ? background : "",
+
+        // subtract half of a margin to ensure it lines up with the edges
+        marginLeft: -margin / 2,
+        marginRight: -margin / 2,
+      }}
+    >
+      <ReactGridLayout
+        cols={cols}
+        layout={layout}
+        width={gridWidth}
+        margin={[margin, margin]}
+        rowHeight={rowHeight}
+        isDraggable={isEditing}
+        isResizable={isEditing}
+        {...props}
+      >
+        {items.map(renderItem)}
+      </ReactGridLayout>
+    </div>
+  );
+}
+
+export default NewGridLayout;
