88251f619225c9580dd31ae24a64c51e15cbc213
Gustavo Saiani <gustavo@poe.ma>
2021-05-18 16:42:39 -0300

    Refactor and propType Notebook.jsx

--- frontend/src/metabase/query_builder/components/notebook/Notebook.jsx
+++ frontend/src/metabase/query_builder/components/notebook/Notebook.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/prop-types */
 import React from "react";
+import PropTypes from "prop-types";
 
 import { t } from "ttag";
 import cx from "classnames";
@@ -9,6 +9,17 @@ import Button from "metabase/components/Button";
 
 import NotebookSteps from "./NotebookSteps";
 
+const getCleanQuestion = (question) => {
+  const cleanQuery = question.query().clean();
+
+  let cleanQuestion = question.setQuery(cleanQuery);
+  if (cleanQuestion.display() === "table") {
+    cleanQuestion = cleanQuestion.setDefaultDisplay();
+  }
+
+  return cleanQuestion;
+}
+
 export default function Notebook({ className, ...props }) {
   const {
     question,
@@ -17,6 +28,20 @@ export default function Notebook({ className, ...props }) {
     runQuestionQuery,
     setQueryBuilderMode,
   } = props;
+
+  const handleClick = async () => {
+    const cleanQuestion = getCleanQuestion(question);
+
+    await cleanQuestion.update();
+
+    // switch mode before running otherwise URL update may cause it to switch back to notebook mode
+    await setQueryBuilderMode("view");
+
+    if (isResultDirty) {
+      await runQuestionQuery();
+    }
+  }
+
   return (
     <Box className={cx(className, "relative mb4")} px={[2, 3]}>
       <NotebookSteps {...props} />
@@ -25,22 +50,20 @@ export default function Notebook({ className, ...props }) {
           medium
           primary
           style={{ minWidth: 220 }}
-          onClick={async () => {
-            let cleanQuestion = question.setQuery(question.query().clean());
-            if (cleanQuestion.display() === "table") {
-              cleanQuestion = cleanQuestion.setDefaultDisplay();
-            }
-            await cleanQuestion.update();
-            // switch mode before running otherwise URL update may cause it to switch back to notebook mode
-            await setQueryBuilderMode("view");
-            if (isResultDirty) {
-              await runQuestionQuery();
-            }
-          }}
+          onClick={handleClick}
         >
           {t`Visualize`}
         </Button>
       )}
     </Box>
   );
+}
+
+Notebook.propTypes = {
+  className: PropTypes.string,
+  question: PropTypes.object,
+  isRunnable: PropTypes.bool.isRequired,
+  isResultDirty: PropTypes.bool.isRequired,
+  runQuestionQuery: PropTypes.func.isRequired,
+  setQueryBuilderMode: PropTypes.func.isRequired,
 }
