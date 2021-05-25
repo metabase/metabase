c8033d22a1868994650dafbd7ce26f6b44161b95
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-05-25 13:21:45 +0200

    #13313 Repro: It shouldn't be possible to save invalid (non-numeric) LDAP port (#16204)
    * Add repro for #13313
    
    * Add comment related to #16255

--- frontend/test/metabase/scenarios/admin/settings/sso.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/sso.cy.spec.js
@@ -14,6 +14,38 @@ describe("scenarios > admin > settings > SSO", () => {
     cy.findByDisplayValue("123").type("456");
     saveSettings();
   });
+
+  describe("LDAP", () => {
+    /**
+     * IMPORTANT:
+     * These repros currently rely on the broken behavior. It is possible to save settings without authentication turned on.
+     * See: https://github.com/metabase/metabase/issues/16225
+     * It is possible related repros will have to be rewritten/adjusted once #16255 gets fixed!
+     */
+
+    beforeEach(() => {
+      cy.visit("/admin/settings/authentication/ldap");
+    });
+
+    // Space after "123 " is crucial for #13313
+    const INVALID_PORTS = ["21.3", "asd", "123 "];
+
+    INVALID_PORTS.forEach(port => {
+      it("shouldn't be possible to save non-numeric port (#13313)", () => {
+        // The real endpoint is `/api/ldap/settings` but I had to use this ambiguous one for this repro because of #16173
+        // Although the endpoint was fixed, we want to always be able to test these issues separately and independently of each other.
+        cy.intercept("PUT", "/api/**").as("update");
+
+        cy.findByPlaceholderText("ldap.yourdomain.org").type("foobar");
+        cy.findByPlaceholderText("389").type(port);
+        cy.button("Save changes").click();
+        cy.wait("@update").then(interception => {
+          expect(interception.response.statusCode).to.eq(500);
+          expect(interception.response.body.cause).to.include(port);
+        });
+      });
+    });
+  });
 });
 
 function saveSettings() {
