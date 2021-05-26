9655f93572ce35c7a793da2dd0662f57d862631c
Tim Macdonald <tim@metabase.com>
2021-05-26 09:28:25 -0500

    Close moderation requests when a review is verified (#16219)
    * Close moderation requests when a review is verified
    
    * Use real pre-update hooks instead of handrolling them
    
    * Also close requests when a question is misleading or confusing

--- src/metabase/models/moderation_review.clj
+++ src/metabase/models/moderation_review.clj
@@ -12,18 +12,58 @@
   "Schema enum of the acceptable values for the `status` column"
   (s/enum "verified" "misleading" "confusing" "not_misleading" "pending"))
 
+(def ReviewChanges
+  "Schema for a ModerationReview that's being updated (so most keys are optional)"
+  {(s/optional-key :id)                  su/IntGreaterThanZero
+   (s/optional-key :moderated_item_id)   su/IntGreaterThanZero
+   (s/optional-key :moderated_item_type) moderation/moderated-item-types
+   (s/optional-key :status)              statuses
+   (s/optional-key :text)                (s/maybe s/Str)
+   s/Any                                 s/Any})
+
 (models/defmodel ModerationReview :moderation_review)
+
+(s/defn ^:private newly-reviewed?
+  [old-status :- (s/maybe statuses)
+   new-status :- (s/maybe statuses)]
+  (and
+   (not= old-status new-status)
+   (#{"misleading" "confusing" "verified"} new-status)))
+
+(defn- resolve-requests!
+  "Close all open requests for verification connected to the same question/dashboard"
+  [{:keys [moderated_item_id moderated_item_type]}]
+  (db/update-where! 'ModerationRequest {:moderated_item_id   moderated_item_id
+                                        :moderated_item_type (name moderated_item_type)
+                                        :type                "verification_request"
+                                        :status              "open"}
+    :status "resolved"))
+
+(defn- pre-insert-or-update
+  [maybe-old-review new-review]
+  (u/prog1 new-review
+    (when (newly-reviewed? (:status maybe-old-review) (:status new-review))
+      (resolve-requests! (merge maybe-old-review new-review)))))
+
+(defn- pre-insert
+  [new-review]
+  (pre-insert-or-update nil new-review))
+
+(defn- pre-update
+  [new-review]
+  (pre-insert-or-update (ModerationReview (u/the-id new-review)) new-review))
 
 (u/strict-extend (class ModerationReview)
   models/IModel
   (merge models/IModelDefaults
          {:properties (constantly {:timestamped? true})
-          :types      (constantly {:moderated_item_type :keyword})})
+          :types      (constantly {:moderated_item_type :keyword})
+          :pre-insert pre-insert
+          :pre-update pre-update})
 
   ;; Todo: this is wrong, but what should it be?
   i/IObjectPermissions
   perms/IObjectPermissionsForParentCollection)
-
 
 (s/defn create-review!
   "Create a new ModerationReview"
@@ -37,11 +77,7 @@
 
 (s/defn update-review!
   "Update the given keys for an existing ModerationReview with the given `:id`"
-  [review :-
-   {:id                                   su/IntGreaterThanZero
-    (s/optional-key :moderated_item_id)   su/IntGreaterThanZero
-    (s/optional-key :moderated_item_type) moderation/moderated-item-types
-    (s/optional-key :status)              statuses
-    (s/optional-key :text)                (s/maybe s/Str)}]
-  (when (db/update! ModerationReview (u/the-id review) (dissoc review :id))
-    (ModerationReview (u/the-id review))))
+  [review :- ReviewChanges]
+  (let [id (u/the-id review)]
+    (and (db/update! ModerationReview id review)
+         (ModerationReview id))))
