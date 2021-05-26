6ec25c71c1b6a1a03aab5a074c3301ea8222bb4b
Anton Kulyk <kuliks.anton@gmail.com>
2021-05-26 15:47:02 +0300

    Display menu items at pinned items table

--- frontend/src/metabase/collections/components/PinnedItemsTable.jsx
+++ frontend/src/metabase/collections/components/PinnedItemsTable.jsx
@@ -10,10 +10,23 @@ import Link from "metabase/components/Link";
 
 import { ANALYTICS_CONTEXT } from "metabase/collections/constants";
 
-function PinnedItem({ item, onToggleSelected, getIsSelected }) {
+function PinnedItem({
+  item,
+  collection = {},
+  onCopy,
+  onMove,
+  onToggleSelected,
+  getIsSelected,
+}) {
   const lastEditInfo = item["last-edit-info"];
   const lastEditedBy = `${lastEditInfo.first_name} ${lastEditInfo.last_name}`;
   const lastEditedAt = moment(lastEditInfo.timestamp).format("MMMM DD, YYYY");
+
+  const handlePin = useCallback(() => item.setPinned(false), [item]);
+  const handleMove = useCallback(() => onMove([item]), [item, onMove]);
+  const handleCopy = useCallback(() => onCopy([item]), [item, onCopy]);
+  const handleArchive = useCallback(() => item.setArchived(true), [item]);
+
   return (
     <tr>
       <td>
@@ -47,30 +60,55 @@ function PinnedItem({ item, onToggleSelected, getIsSelected }) {
       <td>
         <p className="text-dark text-bold">{lastEditedAt}</p>
       </td>
+      <td>
+        <EntityItem.Menu
+          item={item}
+          onPin={collection.can_write ? handlePin : null}
+          onMove={
+            collection.can_write && item.setCollection ? handleMove : null
+          }
+          onCopy={item.copy ? handleCopy : null}
+          onArchive={
+            collection.can_write && item.setArchived ? handleArchive : null
+          }
+          ANALYTICS_CONTEXT={ANALYTICS_CONTEXT}
+        />
+      </td>
     </tr>
   );
 }
 
-function PinnedItemsTable({ items, onToggleSelected, getIsSelected }) {
+function PinnedItemsTable({
+  items,
+  collection,
+  onCopy,
+  onMove,
+  onToggleSelected,
+  getIsSelected,
+}) {
   const renderItem = useCallback(
     item => (
       <PinnedItem
         key={`${item.model}-${item.id}`}
         item={item}
+        collection={collection}
+        onCopy={onCopy}
+        onMove={onMove}
         onToggleSelected={onToggleSelected}
         getIsSelected={getIsSelected}
       />
     ),
-    [onToggleSelected, getIsSelected],
+    [collection, onCopy, onMove, onToggleSelected, getIsSelected],
   );
 
   return (
     <table className="ContentTable">
       <colgroup>
         <col span="1" style={{ width: "5%" }} />
-        <col span="1" style={{ width: "60%" }} />
+        <col span="1" style={{ width: "50%" }} />
         <col span="1" style={{ width: "15%" }} />
         <col span="1" style={{ width: "20%" }} />
+        <col span="1" style={{ width: "10%" }} />
       </colgroup>
       <thead>
         <tr>
@@ -78,6 +116,7 @@ function PinnedItemsTable({ items, onToggleSelected, getIsSelected }) {
           <th>{t`Name`}</th>
           <th>{t`Last edited by`}</th>
           <th>{t`Last edited at`}</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>{items.map(renderItem)}</tbody>
