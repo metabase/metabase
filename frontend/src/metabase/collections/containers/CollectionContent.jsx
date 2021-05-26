01546f2312db95d1a31017d24cd0bd038726c5f1
Anton Kulyk <kuliks.anton@gmail.com>
2021-05-26 14:58:18 +0300

    Make CollectionContent wider

--- frontend/src/metabase/collections/containers/CollectionContent.jsx
+++ frontend/src/metabase/collections/containers/CollectionContent.jsx
@@ -141,7 +141,7 @@ function CollectionContent({
 
         return (
           <Box pt={2}>
-            <Box w={"80%"} ml="auto" mr="auto">
+            <Box w="90%" ml="auto" mr="auto">
               <Header
                 isRoot={isRoot}
                 isAdmin={isAdmin}
