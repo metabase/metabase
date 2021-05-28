da0f800d95ce692ec8836fba6c35c40f7f232111
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2021-05-28 16:07:50 -0400

    extract styles

--- frontend/src/metabase/nav/components/SearchResults.jsx
+++ frontend/src/metabase/nav/components/SearchResults.jsx
@@ -3,41 +3,42 @@ import PropTypes from "prop-types";
 import { t } from "ttag";
 
 import Icon from "metabase/components/Icon";
-import Card from "metabase/components/Card";
 import SearchResult from "metabase/search/components/SearchResult";
 import Search from "metabase/entities/search";
 
+import {
+  SearchTypeaheadCard,
+  SearchResultList,
+  NoSearchResults,
+} from "./SearchResults.styled";
+
 function SearchResults({ searchText }) {
   return (
-    <Card className="overflow-y-auto" style={{ maxHeight: 400 }} py={1}>
+    <SearchTypeaheadCard>
       <Search.ListLoader
         query={{ q: searchText.trim() }}
         wrapped
         reload
         debounced
       >
-        {({ list }) => {
-          return (
-            <ol>
-              {list.length === 0 ? (
-                <li className="flex flex-column align-center justify-center p4 text-medium text-centered">
-                  <div className="my3">
-                    <Icon name="search" mb={1} size={24} />
-                    <h3 className="text-light">{t`Didn't find anything`}</h3>
-                  </div>
+        {({ list: results }) => (
+          <SearchResultList>
+            {results.length === 0 ? (
+              <NoSearchResults>
+                <Icon name="search" mb={1} size={24} />
+                <h3 className="text-light">{t`Didn't find anything`}</h3>
+              </NoSearchResults>
+            ) : (
+              results.map(result => (
+                <li key={`${result.model}:${result.id}`}>
+                  <SearchResult result={result} compact={true} />
                 </li>
-              ) : (
-                list.map(l => (
-                  <li key={`${l.model}:${l.id}`}>
-                    <SearchResult result={l} compact={true} />
-                  </li>
-                ))
-              )}
-            </ol>
-          );
-        }}
+              ))
+            )}
+          </SearchResultList>
+        )}
       </Search.ListLoader>
-    </Card>
+    </SearchTypeaheadCard>
   );
 }
 
