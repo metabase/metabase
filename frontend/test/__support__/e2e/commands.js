47f102fe4ec53d31c551651664b98efb22f0852d
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-05-31 13:00:29 +0200

    Extract database related custom commands

--- frontend/test/__support__/e2e/commands.js
+++ frontend/test/__support__/e2e/commands.js
@@ -10,29 +10,9 @@ import "./commands/user/authentication";
 import "./commands/permissions/updatePermissions";
 import "./commands/permissions/sandboxTable";
 
+import "./commands/database/addH2SampleDataset";
+
 import "./commands/helpers/isVisibleInPopover";
 import "./commands/helpers/isInViewport";
 
 import "./commands/overwrites/log";
-
-/**
- * DATABASES
- */
-
-Cypress.Commands.add(
-  "addH2SampleDataset",
-  ({ name, auto_run_queries = false, is_full_sync = false } = {}) => {
-    cy.log(`Add another H2 sample dataset DB called "${name}"`);
-    cy.request("POST", "/api/database", {
-      engine: "h2",
-      name,
-      details: {
-        db:
-          "zip:./target/uberjar/metabase.jar!/sample-dataset.db;USER=GUEST;PASSWORD=guest",
-      },
-      auto_run_queries,
-      is_full_sync,
-      schedules: {},
-    });
-  },
-);
