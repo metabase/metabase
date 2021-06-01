9a4b9a7d173a2b1bcc50c954b022f0fd0a339c22
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-06-01 12:21:20 +0200

    Refactor repro for #13150 using the new custom command

--- frontend/test/metabase/scenarios/dashboard/dashboard.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard/dashboard.cy.spec.js
@@ -436,75 +436,80 @@ describe("scenarios > dashboard", () => {
   });
 
   it.skip("should not send additional card queries for all filters (metabase#13150)", () => {
-    cy.createQuestion({
-      name: "13150 (Products)",
+    const questionDetails = {
       query: { "source-table": PRODUCTS_ID },
-    }).then(({ body: { id: QUESTION_ID } }) => {
-      cy.createDashboard("13150D").then(({ body: { id: DASHBOARD_ID } }) => {
-        cy.log("Add 3 filters to the dashboard");
+    };
 
-        cy.request("PUT", `/api/dashboard/${DASHBOARD_ID}`, {
-          parameters: [
-            { name: "Title", slug: "title", id: "9f20a0d5", type: "category" },
+    const filter1 = {
+      name: "Title",
+      slug: "title",
+      id: "9f20a0d5",
+      type: "category",
+    };
+
+    const filter2 = {
+      name: "Category",
+      slug: "category",
+      id: "719fe1c2",
+      type: "category",
+    };
+
+    const filter3 = {
+      name: "Vendor",
+      slug: "vendor",
+      id: "a73b7c9",
+      type: "category",
+    };
+
+    cy.createQuestionAndDashboard({ questionDetails }).then(
+      ({ body: { id, card_id, dashboard_id } }) => {
+        //Add filters to the dashboard
+        cy.request("PUT", `/api/dashboard/${dashboard_id}`, {
+          parameters: [filter1, filter2, filter3],
+        });
+
+        //Connect all filters to the card
+        cy.request("PUT", `/api/dashboard/${dashboard_id}/cards`, {
+          cards: [
             {
-              name: "Category",
-              slug: "category",
-              id: "719fe1c2",
-              type: "category",
+              id,
+              card_id,
+              row: 0,
+              col: 0,
+              sizeX: 14,
+              sizeY: 12,
+              parameter_mappings: [
+                {
+                  parameter_id: filter1.id,
+                  card_id,
+                  target: ["dimension", ["field", PRODUCTS.TITLE, null]],
+                },
+                {
+                  parameter_id: filter2.id,
+                  card_id,
+                  target: ["dimension", ["field", PRODUCTS.CATEGORY, null]],
+                },
+                {
+                  parameter_id: filter3.id,
+                  card_id,
+                  target: ["dimension", ["field", PRODUCTS.VENDOR, null]],
+                },
+              ],
+              visualization_settings: {},
             },
-            { name: "Vendor", slug: "vendor", id: "a73b7c9", type: "category" },
           ],
         });
+        cy.server();
+        cy.route("POST", `/api/card/${card_id}/query`).as("cardQuery");
 
-        cy.log("Add previously created qeustion to the dashboard");
+        cy.visit(
+          `/dashboard/${dashboard_id}?title=Awesome Concrete Shoes&category=Widget&vendor=McClure-Lockman`,
+        );
 
-        cy.request("POST", `/api/dashboard/${DASHBOARD_ID}/cards`, {
-          cardId: QUESTION_ID,
-        }).then(({ body: { id: DASH_CARD_ID } }) => {
-          cy.log("Connect all filters to the card");
-
-          cy.request("PUT", `/api/dashboard/${DASHBOARD_ID}/cards`, {
-            cards: [
-              {
-                id: DASH_CARD_ID,
-                card_id: QUESTION_ID,
-                row: 0,
-                col: 0,
-                sizeX: 14,
-                sizeY: 12,
-                parameter_mappings: [
-                  {
-                    parameter_id: "9f20a0d5",
-                    card_id: QUESTION_ID,
-                    target: ["dimension", ["field", PRODUCTS.TITLE, null]],
-                  },
-                  {
-                    parameter_id: "719fe1c2",
-                    card_id: QUESTION_ID,
-                    target: ["dimension", ["field", PRODUCTS.CATEGORY, null]],
-                  },
-                  {
-                    parameter_id: "a73b7c9",
-                    card_id: QUESTION_ID,
-                    target: ["dimension", ["field", PRODUCTS.VENDOR, null]],
-                  },
-                ],
-                visualization_settings: {},
-              },
-            ],
-          });
-          cy.server();
-          cy.route("POST", `/api/card/${QUESTION_ID}/query`).as("cardQuery");
-
-          cy.visit(
-            `/dashboard/${DASHBOARD_ID}?title=Awesome Concrete Shoes&category=Widget&vendor=McClure-Lockman`,
-          );
-
-          cy.wait("@cardQuery.all");
-          expectedRouteCalls({ route_alias: "cardQuery", calls: 1 });
-        });
-      });
-    });
+        cy.wait("@cardQuery.all");
+        expectedRouteCalls({ route_alias: "cardQuery", calls: 1 });
+      },
+    );
   });
 
   it("should show sub-day resolutions in relative date filter (metabase#6660)", () => {
