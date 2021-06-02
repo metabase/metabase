2454001ac8f259bb761f55b28fe9de31bacf30eb
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-06-02 12:41:36 +0200

    Remove unused import

--- frontend/test/metabase/dashboard/DashCard.unit.spec.js
+++ frontend/test/metabase/dashboard/DashCard.unit.spec.js
@@ -3,7 +3,6 @@
 jest.unmock("metabase/components/ExplicitSize");
 
 import React from "react";
-import renderer from "react-test-renderer";
 import { render } from "@testing-library/react";
 import { assocIn } from "icepick";
 
