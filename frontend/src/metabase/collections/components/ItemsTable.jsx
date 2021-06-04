e343549b480a394d0f7957a0d765cc13c182e9f0
Anton Kulyk <kuliks.anton@gmail.com>
2021-06-04 18:40:01 +0300

    Add empty ItemsTable drop area

--- frontend/src/metabase/collections/components/ItemsTable.jsx
+++ frontend/src/metabase/collections/components/ItemsTable.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { t } from "ttag";
+import { Flex } from "grid-styled";
+
+import { color } from "metabase/lib/colors";
+
+import PinDropTarget from "metabase/containers/dnd/PinDropTarget";
 
 import { ANALYTICS_CONTEXT } from "metabase/collections/constants";
 
 import BaseItemsTable from "./BaseItemsTable";
+
+function ItemsEmptyState() {
+  return (
+    <PinDropTarget pinIndex={null} hideUntilDrag margin={10}>
+      {({ hovered }) => (
+        <Flex
+          align="center"
+          justify="center"
+          py={2}
+          m={2}
+          color={hovered ? color("brand") : color("text-medium")}
+        >
+          {t`Drag here to un-pin`}
+        </Flex>
+      )}
+    </PinDropTarget>
+  );
+}
 
 Item.propTypes = {
   item: PropTypes.object.isRequired,
@@ -23,7 +47,17 @@ function Item({ item, ...props }) {
   );
 }
 
+ItemsTable.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
 function ItemsTable(props) {
+  const { items } = props;
+
+  if (items.length === 0) {
+    return <ItemsEmptyState />;
+  }
+
   return <BaseItemsTable {...props} renderItem={Item} />;
 }
 
