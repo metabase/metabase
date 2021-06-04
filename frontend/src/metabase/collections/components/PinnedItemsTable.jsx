f27928b5ac12b9d501cf644edbc0a5849d4f35fa
Anton Kulyk <kuliks.anton@gmail.com>
2021-06-04 13:34:57 +0300

    Highlight pinned items

--- frontend/src/metabase/collections/components/PinnedItemsTable.jsx
+++ frontend/src/metabase/collections/components/PinnedItemsTable.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import BaseItemsTable from "./BaseItemsTable";
 
 function PinnedItemsTable(props) {
-  return <BaseItemsTable {...props} />;
+  return <BaseItemsTable {...props} isPinned />;
 }
 
 export default PinnedItemsTable;
