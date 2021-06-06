568fabfc4172a9a6fa095f174461673322980b8f
Anton Kulyk <kuliks.anton@gmail.com>
2021-06-06 23:25:20 +0300

    Show "edited by you" if user is the last editor

--- frontend/src/metabase/components/LastEditInfoLabel.js
+++ frontend/src/metabase/components/LastEditInfoLabel.js
@@ -1,15 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import styled from "styled-components";
 import { t } from "ttag";
 import moment from "moment";
 
 import { color } from "metabase/lib/colors";
 
+import { getUser } from "metabase/selectors/user";
+
 const Label = styled.span`
   font-weight: bold;
   color: ${color("text-medium")};
 `;
+
+function mapStateToProps(state) {
+  return {
+    user: getUser(state),
+  };
+}
 
 LastEditInfoLabel.propTypes = {
   item: PropTypes.shape({
@@ -21,14 +30,26 @@ LastEditInfoLabel.propTypes = {
       timestamp: PropTypes.string.isRequired,
     }).isRequired,
   }),
+  user: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
 };
 
-function LastEditInfoLabel({ item, ...props }) {
-  const { first_name, last_name, timestamp } = item["last-edit-info"];
+function formatEditorName(firstName, lastName) {
+  const lastNameFirstLetter = lastName.charAt(0);
+  return `${firstName} ${lastNameFirstLetter}.`;
+}
+
+function LastEditInfoLabel({ item, user, ...props }) {
+  const { first_name, last_name, id: editorId, timestamp } = item[
+    "last-edit-info"
+  ];
   const time = moment(timestamp).fromNow();
-  const lastNameFirstLetter = last_name.charAt(0);
-  const editor = `${first_name} ${lastNameFirstLetter}.`;
+
+  const editor =
+    editorId === user.id ? "you" : formatEditorName(first_name, last_name);
+
   return <Label {...props}>{t`Edited ${time} by ${editor}`}</Label>;
 }
 
-export default LastEditInfoLabel;
+export default connect(mapStateToProps)(LastEditInfoLabel);
