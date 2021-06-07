b1405e83e635f51ce785e022706f3d11d346675d
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-06-07 21:14:43 +0200

    Update repro for #12492

--- frontend/test/metabase/scenarios/question/filter.cy.spec.js
+++ frontend/test/metabase/scenarios/question/filter.cy.spec.js
@@ -571,8 +571,7 @@ describe("scenarios > question > filter", () => {
   });
 
   it("should display original custom expression filter with dates on subsequent click (metabase#12492)", () => {
-    cy.server();
-    cy.route("POST", "/api/dataset").as("dataset");
+    cy.intercept("POST", "/api/dataset").as("dataset");
 
     visitQuestionAdhoc({
       dataset_query: {
@@ -595,11 +594,9 @@ describe("scenarios > question > filter", () => {
     });
 
     cy.wait("@dataset");
-    cy.findByText(/^Created At is after/i)
-      .should("not.contain", "Unknown")
-      .click();
+    cy.findByText(/Created At > Product? → Created At/i).click();
     cy.get("[contenteditable='true']").contains(
-      /\[Created At\] > \[Products? -> Created At\]/,
+      /\[Created At\] > \[Products? → Created At\]/,
     );
   });
 
