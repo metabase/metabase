72d6ec6aca517d56e972c7eccdb16cc7cceb303b
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-06-07 13:23:32 +0200

    Add repro for #16378

--- frontend/test/metabase/scenarios/admin/settings/sso.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/sso.cy.spec.js
@@ -1,4 +1,4 @@
-import { restore } from "__support__/e2e/cypress";
+import { restore, describeWithToken } from "__support__/e2e/cypress";
 
 describe("scenarios > admin > settings > SSO", () => {
   beforeEach(() => {
@@ -72,6 +72,36 @@ describe("scenarios > admin > settings > SSO", () => {
 
       cy.findByText('For input string: "baz"'); // The error message
       cy.findByDisplayValue("foobar");
+    });
+  });
+
+  describeWithToken("JWT", () => {
+    beforeEach(() => {
+      cy.visit("/admin/settings/authentication/jwt");
+    });
+
+    it.skip("should save JWT without an error (metabase#16378)", () => {
+      cy.intercept("PUT", "/api/**").as("update");
+
+      cy.findByText("JWT Authentication")
+        .closest("li")
+        .within(() => {
+          cy.findByText("Disabled")
+            .siblings("a")
+            .click();
+        });
+      cy.findByText("Enabled");
+
+      cy.findByPlaceholderText("https://jwt.yourdomain.org")
+        .type("localhost")
+        .blur();
+      cy.button("Save changes").click();
+
+      cy.wait("@update");
+      cy.button("Changes saved!");
+
+      cy.reload();
+      cy.findByText("Enabled");
     });
   });
 });
