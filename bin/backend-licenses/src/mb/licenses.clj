7a290c978b5799f0170f3ff39e0f3102ed4028b2
dan sutton <dan@dpsutton.com>
2021-06-08 18:49:51 -0500

    Lots of changes
    - bump clojure.data.xml to prevent illegal access
    - had to change navigators because of this
    - switch to full text of license
    - algo is now:
      1. check for a license file in jar
      2. check for an override pointing to a license file locally in
         resources
      3. Check pom file for license information and just use that
    
    can be invoked as:
    ```bash
    % clj -X mb.licenses/process :classpath \"$(cd ../.. && lein classpath)\" :backfill "\"overrides.edn\"" :output-filename "\"backend-licenses.txt\""
    License information written to  backend-licenses.txt
    /Users/dan/.m2/repository/net/jcip/jcip-annotations/1.0/jcip-annotations-1.0.jar  : No license information found.
    org.clojars.brenton:google-diff-match-patch  : No license information found.
    
    % wc backend-licenses.txt
       33638  299118 2092948 backend-licenses.txt
    ```

--- bin/backend-licenses/src/mb/licenses.clj
+++ bin/backend-licenses/src/mb/licenses.clj
@@ -1,6 +1,7 @@
 (ns mb.licenses
   "Functionality to take a classpath and generate a file containing all libraries and their respective licenses."
   (:require [clojure.data.xml :as xml]
+            [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.string :as str])
   (:import (java.util.jar JarFile JarFile$JarFileEntry)))
@@ -25,25 +26,26 @@
     (if (.exists adjacent-pom)
       (xml/parse (io/input-stream adjacent-pom))
       (when-let [jar-pom (first (into [] pom-filter (jar-files jar-filename)))]
-        (xml/parse (.getInputStream (JarFile. jar-filename) jar-pom))))))
+        (xml/parse (.getInputStream (JarFile. ^String jar-filename) jar-pom))))))
 
 (defn- get-entry [^JarFile jar ^String name]
   (.getEntry jar name))
 
-(def ^:private tag-content (juxt :tag (comp first :content)))
+(def tag-name (comp keyword (fnil name "") :tag))
+(def ^:private tag-content (juxt tag-name (comp first :content)))
 
 (defn- pom->coordinates [pom-xml]
   (let [coords (->> pom-xml
                     :content
-                    (filter #(#{:groupId :artifactId :version} (:tag %)))
+                    (filter #(#{:groupId :artifactId :version} (tag-name %)))
                     (map tag-content)
                     (into {}))
         parent (->> pom-xml
                     :content
-                    (filter #(#{:parent} (:tag %)))
+                    (filter #(#{:parent} (tag-name %)))
                     first
                     :content
-                    (map tag-content)
+                    (keep tag-content)
                     (into {}))]
     {:group (or (:groupId coords) (:groupId parent))
      :artifact (:artifactId coords)
@@ -52,11 +54,14 @@
 (defn pom->licenses [pom-xml]
   (let [licenses (some->> pom-xml
                           :content
-                          (filter #(#{:licenses} (:tag %)))
+                          (filter #(#{:licenses} (tag-name %)))
+                          (remove string?)
                           first
                           :content
+                          (remove string?)
                           first
                           :content
+                          (remove string?)
                           (map tag-content)
                           (into {}))]
     licenses))
@@ -66,32 +71,66 @@
    "META-INF/LICENSE.txt" "license/LICENSE"])
 
 (defn license-from-jar
-  [jar]
-  (if-let [license-entry (some (partial get-entry jar) license-file-names)]
-    (with-open [rdr (io/reader (.getInputStream jar license-entry))]
-      (let [[license version] (->> rdr line-seq (remove str/blank?) (map str/trim) (take 2))]
-        {:name (str license ": " version)}))))
+  [^JarFile jar]
+  (when-let [license-entry (some (partial get-entry jar) license-file-names)]
+    (with-open [is (.getInputStream jar license-entry)]
+      (slurp is))))
 
 (defn license-from-backfill
   [{:keys [group artifact]} backfill]
-  (some #(get-in backfill %)
-        [[group artifact]
-         [:override/group group]]))
+  (if-let [license (some #(get-in backfill %)
+                         [[group artifact]
+                          [:override/group group]])]
+    (if (string? license)
+      license
+      (slurp (io/resource (:resource license))))))
 
-(defn discern-license-and-coords [jar-filename backfill]
+(defn discern-license-and-coords [^String jar-filename backfill]
   (try
     (let [pom-xml (jar->pom jar-filename)
           coords (pom->coordinates pom-xml)
-          license (or (pom->licenses pom-xml)
-                      (license-from-jar (JarFile. jar-filename))
-                      (license-from-backfill coords backfill))]
+          license (or (license-from-jar (JarFile. jar-filename))
+                      (license-from-backfill coords backfill)
+                      (when-let [{:keys [name url]} (pom->licenses pom-xml)]
+                        (str name ": " url)))]
       [jar-filename (cond-> {:coords coords :license license}
-                      (not (and license coords)) (assoc :error "Error determining license or coords"))])
+                      (not (and license coords))
+                      (assoc :error "Error determining license or coords"))])
     (catch Exception e
       [jar-filename {:error e}])))
 
+(defn write-license [success-os [jar {:keys [coords license]}]]
+  (let [{:keys [group artifact]} coords]
+    (binding [*out* success-os]
+      (println "The following software may be included in this product: "
+               group ": " artifact
+               ". This software contains the following license and notice below:")
+      (println "\n")
+      (println license)
+      (println "\n\n----------\n"))))
+
+(defn report-missing [error-os [jar {:keys [coords]}]]
+  (let [{:keys [group artifact]} coords
+        dep-name (or (when artifact
+                       (str (when group (str group ":")) artifact))
+                     jar)]
+    (binding [*out* error-os]
+      (println dep-name " : No license information found."))))
+
 (defn process
   [{:keys [classpath backfill output-filename]}]
-  (let [entries (->> (str/split classpath (re-pattern classpath-separator))
-                     (filter jar-file?))]
-    (map #(discern-license-and-coords % backfill) entries)))
+  (let [backfill (if (string? backfill)
+                   (edn/read-string (slurp backfill))
+                   (or backfill {}))
+        entries (->> (str/split classpath (re-pattern classpath-separator))
+                     (filter jar-file?))
+        info    (map #(discern-license-and-coords % backfill) entries)
+
+        {with-license true without-license false}
+        (group-by (comp (complement #(contains? % :error)) second) info)]
+    (when (seq with-license)
+      (with-open [os (io/writer output-filename)]
+        (run! #(write-license os %) with-license))
+      (println "License information written to " output-filename))
+    (when (seq without-license)
+      (run! #(report-missing *err* %) without-license))))
