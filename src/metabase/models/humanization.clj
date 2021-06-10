bfafbb33e3690326a5d611616a57f336bc84812d
Howon Lee <hlee.howon@gmail.com>
2021-06-10 12:36:28 -0700

    Capitalize only first letter, don't mess with existing caps for unfriendly field name (#16539)
    If you put in fooBarID, previous behavior on `simple` was to change to Foobarid. New behavior is to keep the caps on non-initial letters, turning it into FooBarID. So camelCaps turns into PascalCaps. ALLCAPS is Titlecapsed, tho.

--- src/metabase/models/humanization.clj
+++ src/metabase/models/humanization.clj
@@ -39,7 +39,10 @@
 (defn- capitalize-word [word]
   (if (contains? acronyms (str/lower-case word))
     (str/upper-case word)
-    (str/capitalize word)))
+    ;; We are assuming that ALL_UPPER_CASE means we should be Title Casing
+    (if (= word (str/upper-case word))
+      (str/capitalize word)
+      (str (str/capitalize (subs word 0 1)) (subs word 1)))))
 
 ;; simple replaces hyphens and underscores with spaces and capitalizes
 (defmethod name->human-readable-name :simple
