85282851ded5dfa1a7e81ea07a86bb3353b5b5fa
Anton Kulyk <kuliks.anton@gmail.com>
2021-06-10 20:41:47 +0300

    Test dashboards & questions display last edit time

--- frontend/test/metabase/scenarios/collections/items-metadata.cy.spec.js
+++ frontend/test/metabase/scenarios/collections/items-metadata.cy.spec.js
@@ -1,9 +1,66 @@
+import moment from "moment";
 import { restore } from "__support__/e2e/cypress";
 import { USERS } from "__support__/e2e/cypress_data";
 
 describe("scenarios > collection items metadata", () => {
   beforeEach(() => {
     restore();
+  });
+
+  describe("last edit date", () => {
+    beforeEach(() => {
+      cy.signInAsAdmin();
+    });
+
+    const IN_15_MIN = moment().add(15, "minutes");
+    const IN_HOUR = moment().add(1, "hours");
+    const IN_4_HOURS = moment().add(4, "hours");
+    const TOMORROW = moment().add(1, "days");
+    const IN_THREE_DAYS = moment().add(3, "days");
+    const NEXT_WEEK = moment().add(1, "week");
+    const NEXT_MONTH = moment().add(1, "month");
+    const IN_4_MONTHS = moment().add(4, "month");
+    const NEXT_YEAR = moment().add(1, "year");
+
+    const testCases = [
+      { date: IN_15_MIN, expected: /Edited 15 minutes ago/i },
+      { date: IN_HOUR, expected: /Edited an hour ago/i },
+      { date: IN_4_HOURS, expected: /Edited 4 hours ago/i },
+      { date: TOMORROW, expected: /Edited a day ago/i },
+      { date: IN_THREE_DAYS, expected: /Edited 3 days ago/i },
+      { date: NEXT_WEEK, expected: /Edited 7 days ago/i },
+      { date: NEXT_MONTH, expected: /Edited a month ago/i },
+      { date: IN_4_MONTHS, expected: /Edited 4 months ago/i },
+      { date: NEXT_YEAR, expected: /Edited a year ago/i },
+    ];
+
+    it("should display last edit moment for dashboards", () => {
+      cy.visit("/dashboard/1");
+      changeDashboard();
+      cy.findByText(/Edited a few seconds ago/i);
+
+      testCases.forEach(testCase => {
+        const { date, expected } = testCase;
+        cy.clock(date.valueOf());
+        cy.reload();
+        cy.findByText(expected);
+        cy.clock().invoke("restore");
+      });
+    });
+
+    it("should display last edit moment for questions", () => {
+      cy.visit("/question/1");
+      changeQuestion();
+      cy.findByText(/Edited a few seconds ago/i);
+
+      testCases.forEach(testCase => {
+        const { date, expected } = testCase;
+        cy.clock(date.valueOf());
+        cy.reload();
+        cy.findByText(expected);
+        cy.clock().invoke("restore");
+      });
+    });
   });
 
   describe("last editor", () => {
@@ -52,21 +109,25 @@ describe("scenarios > collection items metadata", () => {
 });
 
 function changeDashboard() {
+  cy.intercept("PUT", "/api/dashboard/**").as("updateDashboard");
   cy.icon("ellipsis").click();
   cy.findByText("Change title and description").click();
   cy.findByLabelText("Description")
     .click()
     .type("This dashboard is just beautiful");
   cy.button("Update").click();
+  cy.wait("@updateDashboard");
 }
 
 function changeQuestion() {
+  cy.intercept("PUT", "/api/card/**").as("updateQuestion");
   cy.icon("pencil").click();
   cy.findByText("Edit this question").click();
   cy.findByLabelText("Description")
     .click()
     .type("Very insightful");
   cy.button("Save").click();
+  cy.wait("@updateQuestion");
 }
 
 function getTableRowFor(name) {
