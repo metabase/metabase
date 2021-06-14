5275cf25b9a48b3772948be3ec65b77aad5ead44
howon lee <hlee.howon@gmail.com>
2021-06-14 14:09:46 -0700

    reset that sucker

--- src/metabase/driver/sql/query_processor.clj
+++ src/metabase/driver/sql/query_processor.clj
@@ -310,17 +310,13 @@
       (prefix-field-alias driver join-alias field-alias)
       field-alias)))
 
-(defn- field->clause
-  [{field-name :name, table-id :table_id, database-type :database_type, :as field}]
-  (if *table-alias*
-    [*table-alias* (unambiguous-field-alias driver [:field (:id field) nil])]
-    (let [{schema :schema, table-name :name} (qp.store/table table-id)]
-      [schema table-name field-name])))
-
 (defmethod ->honeysql [:sql (class Field)]
-  [driver field]
+  [driver {field-name :name, table-id :table_id, database-type :database_type, :as field}]
   ;; `indentifer` will automatically unnest nested calls to `identifier`
-  (as-> (field->clause field) expr
+  (as-> (if *table-alias*
+          [*table-alias* (unambiguous-field-alias driver [:field (:id field) nil])]
+          (let [{schema :schema, table-name :name} (qp.store/table table-id)]
+            [schema table-name field-name])) expr
     (apply hx/identifier :field expr)
     (->honeysql driver expr)
     (cast-field-if-needed driver field expr)
@@ -386,7 +382,7 @@
 (defmethod ->honeysql [:sql :field]
   [driver [_ field-id-or-name options :as field-clause]]
   (binding [*field-options* options]
-    (if (and (:join-alias options) (not (contains? options :function-field)))
+    (if (:join-alias options)
       (compile-field-with-join-aliases driver field-clause)
       (let [honeysql-form (cond
                             ;; selects from an inner select should not
@@ -408,13 +404,6 @@
   (if field
     (hsql/call :count (->honeysql driver field))
     :%count.*))
-
-;; ":function-field true"
-
-(defn- ->function-field [field-or-clause]
-  (let [clause (if (class dealio) (field->clause field-or-clause) field-or-clause)]
-    ;;; splice this shit in here
-  ))
 
 (defmethod ->honeysql [:sql :avg]        [driver [_ field]]   (hsql/call :avg             (->honeysql driver field)))
 (defmethod ->honeysql [:sql :median]     [driver [_ field]]   (hsql/call :median          (->honeysql driver field)))
