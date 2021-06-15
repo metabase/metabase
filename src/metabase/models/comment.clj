104ff8fdb5e86e275fc208fd3c9693aa1721ac6a
Tim Macdonald <tim@metabase.com>
2021-06-15 10:44:29 -0400

    BUCM Notifications (#16543)
    * Basic notification endpoint
    
    * Create notifications for comments
    
    * Create notifications for moderation reviews
    
    * Add test

--- src/metabase/models/comment.clj
+++ src/metabase/models/comment.clj
@@ -1,5 +1,6 @@
 (ns metabase.models.comment
   (:require [metabase.models.interface :as i]
+            [metabase.models.notification :as notification]
             [metabase.models.permissions :as perms]
             [metabase.util :as u]
             [metabase.util.schema :as su]
@@ -11,13 +12,43 @@
   "Schema enum of the acceptable values for the `commented_item_type` column"
   (s/enum "moderation_request" "moderation_review"))
 
+(def commented-item->model
+  "Maps DB name of the commented item type to the model symbol (used for db/select and such)"
+  {"moderation_request" 'ModerationRequest
+   :moderation_request  'ModerationRequest
+   "moderation_review"  'ModerationReview
+   :moderation_review   'ModerationReview})
+
+(defn- commented-item
+  [{:keys [commented_item_id commented_item_type]}]
+  (db/select-one (commented-item->model commented_item_type) :id commented_item_id))
+
+(defn- author-for
+  [{:keys [moderator_id requester_id]}]
+  (or moderator_id requester_id))
+
+(defn- create-notifications!
+  [{:keys [commented_item_id commented_item_type author_id id] :as comment}]
+  (u/prog1 comment
+    (let [user-ids (disj (set (conj (db/select-field :author_id 'Comment
+                                      :commented_item_type (name commented_item_type)
+                                      :commented_item_id commented_item_id)
+                                    (author-for (commented-item comment))))
+                         author_id)]
+      (notification/create-notifications!
+       (map (fn [user-id] {:notifier_id id
+                           :notifier_type "comment"
+                           :user_id user-id})
+            user-ids)))))
+
 (models/defmodel Comment :comment)
 
 (u/strict-extend (class Comment)
   models/IModel
   (merge models/IModelDefaults
          {:properties (constantly {:timestamped? true})
-          :types      (constantly {:commented_item_type :keyword})})
+          :types      (constantly {:commented_item_type :keyword})
+          :post-insert create-notifications!})
 
   ;; Todo: this is wrong, but what should it be?
   i/IObjectPermissions
---
+++ src/metabase/models/notification.clj
@@ -0,0 +1,55 @@
+(ns metabase.models.notification
+  (:require [metabase.util :as u]
+            [metabase.util.schema :as su]
+            [schema.core :as s]
+            [toucan.db :as db]
+            [toucan.models :as models]))
+
+(def notifier-types
+  "Schema enum of the acceptable values for the `notifier_type` column"
+  (s/enum "comment" "moderation_review"))
+
+(def notifier->model
+  "Maps DB name of notifier to the model symbol (used for db/select and such)"
+  {"comment"           'Comment
+   "moderation_review" 'ModerationReview})
+
+(models/defmodel Notification :notification)
+(u/strict-extend (class Notification)
+  models/IModel
+  (merge models/IModelDefaults
+         {:properties (constantly {:timestamped? true})}))
+
+(defn- in-clause
+  [ids]
+  (if (seq ids)
+    [:in :id (distinct ids)]
+    [:= 0 1]))
+
+(defn hydrate-notifiers
+  "The comments / moderation reviews associated with these notifications"
+  {:batched-hydrate :notifier}
+  [notifications]
+  (when (seq notifications)
+    (let [notifiers (->> notifications
+                         (group-by :notifier_type)
+                         (mapcat (fn [[type notifications]]
+                                   (map #(assoc % :type type)
+                                        (db/select (notifier->model type)
+                                          {:where
+                                           (in-clause (map :notifier_id notifications))}))))
+                         (group-by (juxt :id :type)))]
+      (for [{:keys [notifier_id notifier_type] :as notification} notifications]
+        (assoc notification :notifier (first (get notifiers [notifier_id notifier_type])))))))
+
+(defn for-user
+  "Seq of notifications for the given user"
+  [user-id]
+  (db/select Notification :user_id user-id, :read false, {:order-by [[:created_at :desc]]}))
+
+(s/defn create-notifications!
+  [row-maps :-
+   [{:notifier_id   su/IntGreaterThanZero
+     :notifier_type notifier-types
+     :user_id       su/IntGreaterThanZero}]]
+  (db/insert-many! Notification row-maps))
