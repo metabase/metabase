c1a46d6ee19f7743f4a993bbb58f6c24699b1cf9
alxnddr <alxnddr@gmail.com>
2021-06-16 22:55:21 +0300

    fix trend visualization is not visible if the first column is not date

--- frontend/src/metabase/visualizations/visualizations/SmartScalar.jsx
+++ frontend/src/metabase/visualizations/visualizations/SmartScalar.jsx
@@ -4,6 +4,7 @@ import { Box, Flex } from "grid-styled";
 import { t, jt } from "ttag";
 import _ from "underscore";
 
+import { isDate } from "metabase/lib/schema_metadata";
 import { formatNumber, formatValue } from "metabase/lib/formatting";
 import { color } from "metabase/lib/colors";
 
@@ -93,8 +94,8 @@ export default class Smart extends React.Component {
       rawSeries,
     } = this.props;
 
-    const metricIndex = 1;
-    const dimensionIndex = 0;
+    const metricIndex = _.findIndex(cols, col => !isDate(col));
+    const dimensionIndex = _.findIndex(cols, isDate);
 
     const lastRow = rows[rows.length - 1];
     const value = lastRow && lastRow[metricIndex];
