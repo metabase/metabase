4d529587c5acb5bf1c96708dda486f34de577577
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-06-17 17:45:31 +0200

    Update longitude spec

--- frontend/test/metabase/scenarios/binning/correctness/longitude.cy.spec.js
+++ frontend/test/metabase/scenarios/binning/correctness/longitude.cy.spec.js
@@ -1,86 +1,62 @@
 import { restore, popover, openPeopleTable } from "__support__/e2e/cypress";
 
+const LONGITUDE_OPTIONS = {
+  "Auto bin": {
+    selected: "Auto binned",
+    representativeValues: ["170° W", "100° W", "60° W"],
+  },
+  "Bin every 0.1 degrees": {
+    selected: "0.1°",
+    representativeValues: null,
+  },
+  "Bin every 1 degree": {
+    selected: "1°",
+    representativeValues: ["167° W", "166° W", "60° W"],
+  },
+  "Bin every 10 degrees": {
+    selected: "10°",
+    representativeValues: ["170° W", "100° W", "60° W"],
+  },
+  "Bin every 20 degrees": {
+    selected: "20°",
+    representativeValues: ["180° W", "160° W", "100° W", "80° W", "60° W"],
+  },
+};
 describe("scenarios > binning > correctness > longitude", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
     openPeopleTable();
     cy.findByText("Summarize").click();
+    openPopoverFromSelectedBinningOption("Longitude", "Auto bin");
   });
 
-  it("Auto bin", () => {
-    openPopoverFromSelectedBinningOption("Longitude", "Auto bin");
-    popover().within(() => {
-      cy.findByText("Auto bin").click();
-    });
-    getTitle("Count by Longitude: Auto binned");
-    cy.get(".bar");
-    cy.get(".y-axis-label")
-      .invoke("text")
-      .should("eq", "Count");
-    cy.get(".x-axis-label")
-      .invoke("text")
-      .should("eq", "Longitude");
+  Object.entries(LONGITUDE_OPTIONS).forEach(
+    ([bucketSize, { selected, representativeValues }]) => {
+      it(`should return correct values for ${bucketSize}`, () => {
+        popover().within(() => {
+          cy.findByText(bucketSize).click();
+        });
 
-    cy.get(".axis.x").within(() => {
-      cy.findByText("170° W");
-    });
-  });
+        getTitle(`Count by Longitude: ${selected}`);
+        cy.get(".bar");
 
-  it("0.1 degrees", () => {
-    openPopoverFromSelectedBinningOption("Longitude", "Auto bin");
-    popover().within(() => {
-      cy.findByText("Bin every 0.1 degrees").click();
-    });
-    getTitle("Count by Longitude: 0.1°");
-    cy.get(".bar");
+        assertOnXYAxisLabels();
 
-    cy.get(".axis.x").should("not.exist");
-  });
+        if (representativeValues) {
+          cy.get(".axis.x").within(() => {
+            representativeValues.forEach(value => {
+              cy.findByText(value);
+            });
+          });
+        } else {
+          cy.get(".axis.x").should("not.exist");
+        }
+      });
+    },
+  );
 
-  it("1 degree", () => {
-    openPopoverFromSelectedBinningOption("Longitude", "Auto bin");
-    popover().within(() => {
-      cy.findByText("Bin every 1 degree").click();
-    });
-    getTitle("Count by Longitude: 1°");
-    cy.get(".bar");
-
-    cy.get(".axis.x").should("not.exist");
-  });
-
-  it("10 degrees", () => {
-    openPopoverFromSelectedBinningOption("Longitude", "Auto bin");
-    popover().within(() => {
-      cy.findByText("Bin every 10 degrees").click();
-    });
-    getTitle("Count by Longitude: 10°");
-    cy.get(".bar");
-
-    cy.get(".axis.x").within(() => {
-      cy.findByText("170° W");
-      cy.findByText("160° W");
-      cy.findByText("60° W");
-    });
-  });
-
-  it("20 degrees", () => {
-    openPopoverFromSelectedBinningOption("Longitude", "Auto bin");
-    popover().within(() => {
-      cy.findByText("Bin every 20 degrees").click();
-    });
-    getTitle("Count by Longitude: 20°");
-    cy.get(".bar");
-
-    cy.get(".axis.x").within(() => {
-      cy.findByText("180° W");
-      cy.findByText("160° W");
-      cy.findByText("60° W");
-    });
-  });
-
-  it("Don't bin", () => {
-    openPopoverFromSelectedBinningOption("Longitude", "Auto bin");
+  it.only("Don't bin", () => {
     popover().within(() => {
       cy.findByText("Don't bin").click();
     });
@@ -107,4 +83,13 @@ function openPopoverFromSelectedBinningOption(column, binning) {
 
 function getTitle(title) {
   cy.findByText(title);
+}
+
+function assertOnXYAxisLabels() {
+  cy.get(".y-axis-label")
+    .invoke("text")
+    .should("eq", "Count");
+  cy.get(".x-axis-label")
+    .invoke("text")
+    .should("eq", "Longitude");
 }
