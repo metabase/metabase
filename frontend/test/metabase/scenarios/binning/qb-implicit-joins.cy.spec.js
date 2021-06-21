2691375633a30a01cb56b4fb190baec7a6f3b33c
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-06-21 12:56:01 +0200

    Add tests for custom question mode

--- frontend/test/metabase/scenarios/binning/qb-implicit-joins.cy.spec.js
+++ frontend/test/metabase/scenarios/binning/qb-implicit-joins.cy.spec.js
@@ -63,6 +63,69 @@ describe("scenarios > binning > from a saved QB question using implicit joins", 
       });
     });
   });
+
+  context("via custom question", () => {
+    beforeEach(() => {
+      cy.visit("/question/1/notebook");
+      cy.findByText("Summarize").click();
+      cy.findByText("Count of rows").click();
+      cy.findByText("Pick a column to group by").click();
+      // Click "Order" accordion to collapse it and expose the other tables
+      cy.findByText("Order").click();
+    });
+
+    it("should work for time series", () => {
+      cy.findByText("User").click();
+      cy.findByPlaceholderText("Find...").type("birth");
+      openPopoverFromDefaultBucketSize("Birth Date", "by month");
+
+      chooseBucketAndAssert({
+        bucketSize: "Year",
+        title: "Count by User → Birth Date: Year",
+        mode: "notebook",
+        values: ["1958", "313"],
+      });
+
+      // Make sure time series chooseBucketAndAssertter works as well
+      cy.get(".AdminSelect-content")
+        .contains("Year")
+        .click();
+      cy.findByText("Month").click();
+
+      cy.get(".cellData")
+        .should("contain", "April, 1958")
+        .and("contain", "37");
+    });
+
+    it("should work for number", () => {
+      cy.findByText("Product").click();
+
+      openPopoverFromDefaultBucketSize("Price", "Auto bin");
+
+      chooseBucketAndAssert({
+        bucketSize: "50 bins",
+        title: "Count by Product → Price: 50 bins",
+        mode: "notebook",
+        values: ["14  –  16", "96"],
+      });
+    });
+
+    it("should work for longitude", () => {
+      cy.findByText("User").click();
+      cy.findByPlaceholderText("Find...").type("longitude");
+
+      openPopoverFromDefaultBucketSize("Longitude", "Auto bin");
+
+      chooseBucketAndAssert({
+        // Test is currently incorrect in that it displays wrong binning options (please see: https://github.com/metabase/metabase/issues/16674)
+        // Once #16674 gets fixed, update the following line to say: `bucketSize: "Bin every 20 degrees"`
+        bucketSize: "20°",
+        title: "Count by User → Longitude: 20°",
+        mode: "notebook",
+        values: ["180° W  –  160° W", "75"],
+      });
+    });
+  });
 });
 
 function openPopoverFromDefaultBucketSize(column, bucket) {
