3fe828b02f08c47924f426ba5c7631ea11374fac
Cam Saul <github@camsaul.com>
2021-06-23 16:08:50 -0700

    Fix Oracle/Vertica comparison against empty string

--- modules/drivers/oracle/test/metabase/test/data/oracle.clj
+++ modules/drivers/oracle/test/metabase/test/data/oracle.clj
@@ -148,7 +148,8 @@
 (defn- execute! [format-string & args]
   (let [sql (apply format format-string args)]
     (println (u/format-color 'blue "[oracle] %s" sql))
-    (jdbc/execute! (dbspec) sql))
+    (jdbc/execute! (dbspec) sql)
+    (println (u/format-color 'green "    [ok]")))
   (println (u/format-color 'blue "[ok]")))
 
 (defn- clean-session-schemas! []
