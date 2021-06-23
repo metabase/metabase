2e3425d4424516edda45648774dcfb4eeab375bf
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2021-06-23 10:43:42 -0300

    fix typos

--- src/metabase/sync/analyze/query_results.clj
+++ src/metabase/sync/analyze/query_results.clj
@@ -78,7 +78,7 @@
 
 (defn insights-rf
   "A reducing function that calculates what is ultimately returned as `[:data :results_metadata]` in userland QP
-  results. `metadata` is the usual QP results metadata e.g. as recieved by an `rff`."
+  results. `metadata` is the usual QP results metadata e.g. as received by an `rff`."
   {:arglists '([metadata])}
   [{:keys [cols]}]
   (let [cols (for [col cols]
