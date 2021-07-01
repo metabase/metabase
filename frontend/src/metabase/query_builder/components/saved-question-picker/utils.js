b614091dbcd4bd9f62d5e51a24c817d886151f20
alxnddr <alxnddr@gmail.com>
2021-07-01 21:21:10 +0300

    fix incorrectly hidden collections in the saved question picker

--- frontend/src/metabase/query_builder/components/saved-question-picker/utils.js
+++ frontend/src/metabase/query_builder/components/saved-question-picker/utils.js
@@ -5,12 +5,21 @@ export function buildCollectionTree(collections, allowedSchemas) {
   }
 
   return collections
-    .filter(collection => allowedSchemas.has(collection.name))
-    .map(collection => ({
-      id: collection.id,
-      name: collection.name,
-      schemaName: collection.name,
-      icon: "folder",
-      children: buildCollectionTree(collection.children, allowedSchemas),
-    }));
+    .map(collection => {
+      const children = buildCollectionTree(collection.children, allowedSchemas);
+
+      const shouldInclude =
+        allowedSchemas.has(collection.name) || children.length > 0;
+
+      return shouldInclude
+        ? {
+            id: collection.id,
+            name: collection.name,
+            schemaName: collection.name,
+            icon: "folder",
+            children: buildCollectionTree(collection.children, allowedSchemas),
+          }
+        : null;
+    })
+    .filter(Boolean);
 }
