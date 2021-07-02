ad7a1a3143219a6c0913252bdea25f597358e986
alxnddr <alxnddr@gmail.com>
2021-07-02 18:57:00 +0300

    hide other personal collections folder when no other users

--- frontend/src/metabase/query_builder/components/saved-question-picker/SavedQuestionPicker.jsx
+++ frontend/src/metabase/query_builder/components/saved-question-picker/SavedQuestionPicker.jsx
@@ -5,11 +5,18 @@ import PropTypes from "prop-types";
 import { t } from "ttag";
 import { connect } from "react-redux";
 
-import Collection, { ROOT_COLLECTION } from "metabase/entities/collections";
 import Icon from "metabase/components/Icon";
 import { Tree } from "metabase/components/tree";
-
 import { SAVED_QUESTIONS_VIRTUAL_DB_ID } from "metabase/lib/constants";
+import Collection, {
+  ROOT_COLLECTION,
+  PERSONAL_COLLECTIONS,
+} from "metabase/entities/collections";
+import {
+  isPersonalCollection,
+  nonPersonalOrArchivedCollection,
+  currentUserPersonalCollections,
+} from "metabase/collections/utils";
 import Schemas from "metabase/entities/schemas";
 
 import SavedQuestionList from "./SavedQuestionList";
@@ -25,14 +32,19 @@ const propTypes = {
   onBack: PropTypes.func.isRequired,
   collections: PropTypes.array.isRequired,
   schemas: PropTypes.array.isRequired,
+  currentUser: PropTypes.object.isRequired,
   databaseId: PropTypes.string,
   tableId: PropTypes.string,
 };
 
 const OUR_ANALYTICS_COLLECTION = {
+  ...ROOT_COLLECTION,
   schemaName: "Everything else",
   icon: "folder",
-  ...ROOT_COLLECTION,
+};
+
+const ALL_PERSONAL_COLLECTIONS_ROOT = {
+  ...PERSONAL_COLLECTIONS,
 };
 
 function SavedQuestionPicker({
@@ -40,6 +52,7 @@ function SavedQuestionPicker({
   onSelect,
   collections,
   schemas,
+  currentUser,
   databaseId,
   tableId,
 }) {
@@ -47,21 +60,46 @@ function SavedQuestionPicker({
     OUR_ANALYTICS_COLLECTION,
   );
 
-  const handleSelect = useCallback(id => {
-    setSelectedCollection(id);
+  const handleSelect = useCallback(collection => {
+    setSelectedCollection(collection);
   }, []);
 
   const collectionTree = useMemo(() => {
-    return schemas.length > 0
-      ? [
-          OUR_ANALYTICS_COLLECTION,
-          ...buildCollectionTree(
-            collections,
-            new Set(schemas.map(schema => schema.name)),
-          ),
-        ]
-      : [OUR_ANALYTICS_COLLECTION];
-  }, [collections, schemas]);
+    const preparedCollections = [];
+    const userPersonalCollections = currentUserPersonalCollections(
+      collections,
+      currentUser.id,
+    );
+    const nonPersonalOrArchivedCollections = collections.filter(
+      nonPersonalOrArchivedCollection,
+    );
+
+    preparedCollections.push(...nonPersonalOrArchivedCollections);
+    preparedCollections.push(...userPersonalCollections);
+
+    if (currentUser.is_superuser) {
+      const otherPersonalCollections = collections.filter(
+        collection =>
+          isPersonalCollection(collection) &&
+          collection.personal_owner_id !== currentUser.id,
+      );
+
+      if (otherPersonalCollections.length > 0) {
+        preparedCollections.push({
+          ...ALL_PERSONAL_COLLECTIONS_ROOT,
+          children: otherPersonalCollections,
+        });
+      }
+    }
+
+    return [
+      OUR_ANALYTICS_COLLECTION,
+      ...buildCollectionTree(
+        [OUR_ANALYTICS_COLLECTION, ...preparedCollections],
+        new Set(schemas.map(schema => schema.name)),
+      ),
+    ];
+  }, [collections, schemas, currentUser]);
 
   return (
     <SavedQuestionPickerRoot>
@@ -79,9 +117,9 @@ function SavedQuestionPicker({
         </Box>
       </CollectionsContainer>
       <SavedQuestionList
+        collection={selectedCollection}
         selectedId={tableId}
         databaseId={databaseId}
-        schemaName={selectedCollection.schemaName}
         onSelect={onSelect}
       />
     </SavedQuestionPickerRoot>
--- frontend/src/metabase/query_builder/components/saved-question-picker/utils.js
+++ frontend/src/metabase/query_builder/components/saved-question-picker/utils.js
@@ -1,22 +1,33 @@
+import { isPersonalCollection } from "metabase/collections/utils";
+import { PERSONAL_COLLECTIONS } from "metabase/entities/collections";
+
+const getCollectionIcon = collection => {
+  if (collection.id === PERSONAL_COLLECTIONS.id) {
+    return "group";
+  }
+
+  return isPersonalCollection(collection) ? "person" : "folder";
+};
+
 // FIXME: Collections must be filtered on the back end
 export function buildCollectionTree(collections, allowedSchemas) {
-  if (collections == null) {
+  if (collections == null || allowedSchemas.size === 0) {
     return [];
   }
 
   return collections
     .map(collection => {
       const children = buildCollectionTree(collection.children, allowedSchemas);
-      const isPersonal = !!collection.personal_owner_id;
       const shouldInclude =
-        allowedSchemas.has(collection.name) || children.length > 0;
+        allowedSchemas.has(collection.originalName || collection.name) ||
+        children.length > 0;
 
       return shouldInclude
         ? {
             id: collection.id,
             name: collection.name,
-            schemaName: collection.name,
-            icon: isPersonal ? "person" : "folder",
+            schemaName: collection.originalName || collection.name,
+            icon: getCollectionIcon(collection),
             children,
           }
         : null;
