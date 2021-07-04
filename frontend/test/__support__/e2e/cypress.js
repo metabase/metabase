fd243996ee22252c6d5f3b58053b5a3c643d2845
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-07-04 15:15:50 +0200

    Annotate the function using JSDoc

--- frontend/test/__support__/e2e/cypress.js
+++ frontend/test/__support__/e2e/cypress.js
@@ -41,6 +41,25 @@ export function browse() {
   return cy.get(".Nav .Icon-table_spaced");
 }
 
+/**
+ * Get the `fieldset` HTML element that we use as a filter widget container.
+ *
+ * @returns HTMLFieldSetElement
+ *
+ * @example
+ * // Simple SQL filter widget (works for "Text" and "Number" SQL variable types)
+ * filterWidget().type("123");
+ *
+ * @example
+ * // Filter widget that opens some other type of a filter picker (search, dropdown, input)
+ * filterWidget()
+ *  .contains("Search")
+ *  .click();
+ *
+ * @todo Add the ability to choose between multiple widgets using their index.
+ * @todo Add the ability to alias the chosen filter widget.
+ * @todo Extract into a separate helper file.
+ */
 export function filterWidget() {
   return cy.get("fieldset");
 }
