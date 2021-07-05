d9d3ec2f6af1f1fb63b14975a653e67fee6a7567
Anton Kulyk <kuliks.anton@gmail.com>
2021-07-05 13:03:45 +0300

    Add basic segmented control component

---
+++ frontend/src/metabase/components/SegmentedControl.info.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { SegmentedControl } from "metabase/components/SegmentedControl";
+
+export const component = SegmentedControl;
+export const category = "input";
+
+export const description = `
+Radio-like segmented control input
+`;
+
+const SIMPLE_OPTIONS = [
+  { name: "Gadget", value: 0 },
+  { name: "Gizmo", value: 1 },
+];
+
+const OPTIONS_WITH_ICONS = [
+  { name: "Gadget", value: 0, icon: "lightbulb" },
+  { name: "Gizmo", value: 1, icon: "folder" },
+  { name: "Doohickey", value: 2, icon: "insight" },
+];
+
+function SegmentedControlDemo(props) {
+  const [value, setValue] = useState(0);
+  return (
+    <SegmentedControl
+      {...props}
+      value={value}
+      onChange={val => setValue(val)}
+    />
+  );
+}
+
+export const examples = {
+  default: <SegmentedControlDemo options={SIMPLE_OPTIONS} />,
+  icons: <SegmentedControlDemo options={OPTIONS_WITH_ICONS} />,
+};
