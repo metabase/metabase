840ed121524426d0d6ef02138a64dd01f739a553
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-07-06 15:09:36 +0200

    Extract repro for #12581 into a separate file

--- frontend/test/metabase/scenarios/native/native.cy.spec.js
+++ frontend/test/metabase/scenarios/native/native.cy.spec.js
@@ -252,56 +252,6 @@ describe("scenarios > question > native", () => {
     });
   });
 
-  it("should correctly display a revision state after a restore (metabase#12581)", () => {
-    const ORIGINAL_QUERY = "SELECT * FROM ORDERS WHERE {{filter}} LIMIT 2";
-
-    // Start with the original version of the question made with API
-    cy.createNativeQuestion({
-      name: "12581",
-      native: {
-        query: ORIGINAL_QUERY,
-        "template-tags": {
-          filter: {
-            id: "a3b95feb-b6d2-33b6-660b-bb656f59b1d7",
-            name: "filter",
-            "display-name": "Filter",
-            type: "dimension",
-            dimension: ["field", ORDERS.CREATED_AT, null],
-            "widget-type": "date/month-year",
-            default: null,
-          },
-        },
-      },
-    }).then(({ body: { id: QUESTION_ID } }) => {
-      cy.visit(`/question/${QUESTION_ID}`);
-    });
-    cy.findByText(/Open Editor/i).click();
-    cy.findByText(/Open Editor/i).should("not.exist");
-    // Both delay and a repeated sequence of `{selectall}{backspace}` are there to prevent typing flakes
-    // Without them at least 1 in 10 test runs locally didn't fully clear the field or type correctly
-    cy.get(".ace_content")
-      .click()
-      .type("{selectall}{backspace}", { delay: 50 });
-    cy.get(".ace_content")
-      .click()
-      .type("{selectall}{backspace}SELECT * FROM ORDERS");
-    cy.findByText("Save").click();
-    modal().within(() => {
-      cy.findByText("Save").click();
-    });
-
-    cy.reload();
-    cy.icon("pencil").click();
-    cy.findByText(/View revision history/i).click();
-    cy.findByText(/Revert/i).click(); // Revert to the first revision
-    cy.findByText(/Open Editor/i).click();
-
-    cy.log("Reported failing on v0.35.3");
-    cy.get(".ace_content").contains(ORIGINAL_QUERY);
-    // Filter dropdown field
-    filterWidget().contains("Filter");
-  });
-
   it("should reorder template tags by drag and drop (metabase#9357)", () => {
     openNativeEditor().type(
       "{{firstparameter}} {{nextparameter}} {{lastparameter}}",
