bf638f4427d9d798dadf1be749e30fe1d683ed21
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-07-08 00:04:23 +0200

    Register `cypress-grep` plugin

--- frontend/test/__support__/e2e/cypress-plugins.js
+++ frontend/test/__support__/e2e/cypress-plugins.js
@@ -32,6 +32,7 @@ const webpackPluginOptions = {
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  require("cypress-grep/src/plugin")(config);
 
   /********************************************************************
    **                          WEBPACK                               **
