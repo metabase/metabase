c7b966be966acbaa808a1f37ffb57e37eea62971
Gustavo Saiani <gustavo@poe.ma>
2021-07-07 09:24:03 -0300

    Group all tests into one

--- frontend/test/metabase/scenarios/collections/collections-sidebar.cy.spec.js
+++ frontend/test/metabase/scenarios/collections/collections-sidebar.cy.spec.js
@@ -1,4 +1,4 @@
-import { restore } from "__support__/e2e/cypress";
+import { restore, sidebar } from "__support__/e2e/cypress";
 
 describe("collections sidebar (metabase#15006)", () => {
   beforeEach(() => {
@@ -8,34 +8,23 @@ describe("collections sidebar (metabase#15006)", () => {
     cy.visit("/collection/root");
   });
 
-  describe("in desktop form factors", () => {
-    it("should not be able to toggle collections sidebar visibility", () => {
-      cy.icon("close").should("not.be.visible");
-      cy.icon("burger").should("not.be.visible");
-    });
-  });
+  it("should be able to toggle collections sidebar when switched to mobile screen size", () => {
+    cy.icon("close").should("not.be.visible");
+    cy.icon("burger").should("not.be.visible");
 
-  describe("in mobile form factors", () => {
-    beforeEach(() => {
-      cy.viewport(480, 800);
+    // resize window to mobile form factor
+    cy.viewport(480, 800);
+
+    sidebar().should("not.be.visible");
+
+    cy.icon("burger").click();
+    cy.icon("burger").should("not.be.visible");
+
+    sidebar().within(() => {
+      cy.findByText("First collection");
+      cy.icon("close").click();
     });
 
-    it("should not display sidebar on page load, and enable opening and closing it via clicks", () => {
-      cy.findByText("First collection").should("not.be.visible");
-      openSidebarFromBurgerIcon();
-      closeSidebarFromCloseIcon();
-    });
+    cy.icon("burger");
   });
 });
-
-function openSidebarFromBurgerIcon() {
-  cy.icon("burger").click();
-
-  cy.findByText("First collection").should("be.visible");
-  cy.icon("burger").should("not.be.visible");
-}
-
-function closeSidebarFromCloseIcon() {
-  cy.icon("close").click();
-  cy.icon("burger").should("be.visible");
-}
