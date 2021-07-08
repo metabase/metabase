bf8efcd32b42faf704c80d9ec9fc592b94e37be2
Bruno Bergher <me@brunobergher.comexport>
2021-07-08 13:41:54 -0700

    Remove email quotations which could be deemed controversial. Address part of #11155

--- src/metabase/util/quotation.clj
+++ src/metabase/util/quotation.clj
@@ -6,16 +6,12 @@
     :author "Andrew McAfee"}
    {:quote "Data really powers everything that we do."
     :author "Jeff Weiner"}
-   {:quote "I keep saying that the sexy job in the next 10 years will be statisticians, and I'm not kidding."
-    :author "Hal Varian"}
    {:quote "Data is the new oil!"
     :author "Clive Humby"}
    {:quote "If we have data, let's look at data.  If all we have are opinions, let's go with mine."
     :author "Jim Barksdale"}
    {:quote "Data that is loved tends to survive."
     :author "Kurt Bollacker"}
-   {:quote "Torture the data, and it will confess to anything."
-    :author "Ronald Coase"}
    {:quote "The price of light is less than the cost of darkness."
     :author "Arthur C. Nielsen"}
    {:quote "Information is the oil of the 21st century, and analytics is the combustion engine."
@@ -24,7 +20,7 @@
     :author "Aldous Huxley"}
    {:quote "It's easy to lie with statistics.  It's hard to tell the truth without statistics."
     :author "Andrejs Dunkels"}
-   {:quote "What gets measured gets managed"
+   {:quote "What gets measured gets managed."
     :author "Peter Drucker"}
    {:quote "Anything that is measured and watched improves."
     :author "Bob Parsons"}])
