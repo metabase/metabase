41f7d8a72d46275ad1f8acc7927da85023efa013
Ariya Hidayat <ariya@metabase.com>
2021-07-11 13:17:40 -0700

    When running <Logs> unit tests, mock XMLHttpRequest
    Since Jest runs with JSDom and JSDom doesn't really implement XHR,
    provide a mock implementation to avoid a thrown exception.

--- frontend/test/metabase/admin/tasks/containers/Logs.unit.spec.js
+++ frontend/test/metabase/admin/tasks/containers/Logs.unit.spec.js
@@ -6,6 +6,16 @@ import { UtilApi } from "metabase/services";
 
 describe("Logs", () => {
   describe("log fetching", () => {
+    beforeEach(() => {
+      const xhrMockClass = () => ({
+        open: jest.fn(),
+        send: jest.fn(),
+        setRequestHeader: jest.fn(),
+      });
+
+      window.XMLHttpRequest = jest.fn().mockImplementation(xhrMockClass);
+    });
+
     it("should call UtilApi.logs after 1 second", () => {
       jest.useFakeTimers();
       render(<Logs />);
