bc51ecb2c61f84e471b33b0aee8ba3802a917e04
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-07-13 12:36:30 +0200

    Set `HAS_ENTERPRISE_TOKEN` using Cypress Configuration and Plugins API

--- frontend/test/__support__/e2e/cypress-plugins.js
+++ frontend/test/__support__/e2e/cypress-plugins.js
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// This file can be used to load plugins and register events.
 //
 // You can change the location of this file or turn off loading
 // the plugins file with the 'pluginsFile' configuration option.
@@ -7,6 +7,15 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
+
+/**
+ * This env var provides the token to the backend.
+ * If it is not present, we skip some tests that depend on a valid token.
+ *
+ * @type {boolean}
+ */
+const hasEnterpriseToken =
+  process.env["ENTERPRISE_TOKEN"] && process.env["MB_EDITION"] === "ee";
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
@@ -39,4 +48,12 @@ module.exports = (on, config) => {
       return launchOptions;
     }
   });
+
+  /********************************************************************
+   **                          CONFIG                                **
+   ********************************************************************/
+
+  config.env.HAS_ENTERPRISE_TOKEN = hasEnterpriseToken;
+
+  return config;
 };
