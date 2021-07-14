bce3ec3c2572fe87b33b3042a8e961d44ae3177f
Jeff Evans <jeff.evans@metabase.com>
2021-07-14 17:16:29 -0500

    Add support for driver deprecation
    Frontend changes
    
    Calculate supersededBy and supersedes maps from the "superseded-by" property for each engine
    
    Change the options for the engine field to use a function to dynamically show the legacy driver if allowed by rules
    
    Add new DriverWarning component to show these warnings based on supersede status

---
+++ frontend/src/metabase/components/DriverWarning/DriverWarning.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { Flex } from "grid-styled";
+
+export const CardContent = styled(Flex)``;
+
+export const DriverWarningContainer = styled(Flex)`
+  background-color: #f9fbfb;
+  border-radius: 10px;
+  width: 300px;
+  margin-bottom: 8px;
+`;
+
+export const IconContainer = styled(Flex)`
+  width: 32px;
+  height: 32px;
+  background-color: "transparent";
+`;
