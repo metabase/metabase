83338b17e79ca4860462debdf90d9cab2e1829b8
dan sutton <dan@dpsutton.com>
2021-07-15 14:16:59 -0500

    Add ModerationReview model to models.clj and copy infra

--- src/metabase/models.clj
+++ src/metabase/models.clj
@@ -15,6 +15,7 @@
             [metabase.models.field-values :as field-values]
             [metabase.models.login-history :as login-history]
             [metabase.models.metric :as metric]
+            [metabase.models.moderation-review :as moderation-review]
             [metabase.models.metric-important-field :as metric-important-field]
             [metabase.models.native-query-snippet :as native-query-snippet]
             [metabase.models.permissions :as permissions]
@@ -54,6 +55,7 @@
          field-values/keep-me
          login-history/keep-me
          metric/keep-me
+         moderation-review/keep-me
          metric-important-field/keep-me
          native-query-snippet/keep-me
          permissions/keep-me
@@ -92,6 +94,7 @@
  [field-values FieldValues]
  [login-history LoginHistory]
  [metric Metric]
+ [moderation-review ModerationReview]
  [metric-important-field MetricImportantField]
  [native-query-snippet NativeQuerySnippet]
  [permissions Permissions]
