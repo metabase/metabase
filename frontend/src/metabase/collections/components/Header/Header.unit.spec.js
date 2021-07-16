0b2fffcddaddf62f454425d81f96ac685fbb6726
Gustavo Saiani <gustavo@poe.ma>
2021-07-16 18:01:54 -0300

    Test permissions link

--- frontend/src/metabase/collections/components/Header/Header.unit.spec.js
+++ frontend/src/metabase/collections/components/Header/Header.unit.spec.js
@@ -31,3 +31,52 @@ describe("description tooltip", () => {
     screen.getByLabelText(ariaLabel);
   });
 });
+
+describe("permissions link", () => {
+  const ariaLabel = "lock icon";
+
+  describe("should not be displayed", () => {
+    it("when isAdmin is not passed", () => {
+      render(<Header collection={collection} />);
+
+      expect(screen.queryByLabelText(ariaLabel)).not.toBeInTheDocument();
+    });
+
+    it("when collection includes personal_owner_id", () => {
+      render(
+        <Header
+          isAdmin={true}
+          collection={{ ...collection, personal_owner_id: 1 }}
+        />,
+      );
+
+      expect(screen.queryByLabelText(ariaLabel)).not.toBeInTheDocument();
+    });
+
+    it("when isPersonalCollectionChild is truthy", () => {
+      render(
+        <Header
+          isAdmin={true}
+          collection={collection}
+          isPersonalCollectionChild={true}
+        />,
+      );
+
+      expect(screen.queryByLabelText(ariaLabel)).not.toBeInTheDocument();
+    });
+  });
+
+  describe("should be displayed", () => {
+    it("when user is admin, collection does not include personal_owner_id and isPersonalCollectionChild is falsey", () => {
+      render(
+        <Header
+          collection={collection}
+          isAdmin={true}
+          isPersonalCollectionChild={false}
+        />,
+      );
+
+      screen.getByLabelText(ariaLabel);
+    });
+  });
+});
