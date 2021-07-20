7ac53c50e9f7c30474c9ec243605f34c9a5fd6ab
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-07-20 18:41:48 +0300

    Fix cypress tests

--- frontend/test/metabase/scenarios/admin/settings/sso/ldap.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/sso/ldap.cy.spec.js
@@ -7,15 +7,34 @@ describe("scenarios > admin > settings > SSO > LDAP", () => {
     cy.visit("/admin/settings/authentication/ldap");
   });
 
+  it("should be possible to save correct settings", () => {
+    cy.intercept("PUT", "/api/**").as("update");
+
+    findToggleByLabel("LDAP authentication").click();
+    findInputByLabel("LDAP host").type("example.com");
+    findInputByLabel("LDAP port").type("390");
+    findInputByLabel("User search base").type("example");
+
+    findButtonByText("Save changes").click();
+    cy.wait("@update");
+    cy.findByText(/An error occurred while attempting to connect/);
+  });
+
   it("shouldn't be possible to save non-numeric port (#13313)", () => {
     findToggleByLabel("LDAP authentication").click();
-
     findInputByLabel("LDAP host").type("example.com");
     findInputByLabel("LDAP port").type("21.3");
     findInputByLabel("User search base").type("example");
 
-    cy.findByText("That's not a valid port number").should("exist");
-    cy.findByRole("button", { name: "Save changes" }).should("be.disabled");
+    cy.findByText("That's not a valid port number");
+    findButtonByText("Save changes").should("be.disabled");
+  });
+
+  it("shouldn't be possible to change settings when disabled", () => {
+    findInputByLabel("LDAP host").should("be.disabled");
+    findInputByLabel("LDAP port").should("be.disabled");
+    findInputByLabel("User search base").should("be.disabled");
+    findButtonByText("Save changes").should("be.disabled");
   });
 });
 
@@ -33,4 +52,8 @@ function findToggleByLabel(text) {
 
 function findInputByLabel(text) {
   return findByLabel(text, "input");
+}
+
+function findButtonByText(text) {
+  return cy.findByRole("button", { name: text });
 }
