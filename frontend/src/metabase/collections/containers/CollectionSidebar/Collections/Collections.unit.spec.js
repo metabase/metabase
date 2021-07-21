33362aae934c58f0b96d41e3499d4483705faeb1
Gustavo Saiani <gustavo@poe.ma>
2021-07-21 13:45:36 -0300

    Unit test Collections component

--- frontend/src/metabase/collections/containers/CollectionSidebar/Collections/Collections.unit.spec.js
+++ frontend/src/metabase/collections/containers/CollectionSidebar/Collections/Collections.unit.spec.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { DragDropContextProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+
 import Collections from "./Collections";
 
-it("sanity check", () => {
+const name = "A collection name";
+const list = [{ name }];
+
+it("displays entries", () => {
   render(
-    <Collections
-      collectionId={1}
-      currentUserId={1}
-      list={{}}
-      onClose={() => {}}
-      onOpen={() => {}}
-      openCollections={[]}
-    />,
+    <DragDropContextProvider backend={HTML5Backend}>
+      <Collections
+        collectionId={1}
+        currentUserId={1}
+        list={list}
+        onClose={() => {}}
+        onOpen={() => {}}
+        openCollections={[]}
+      />
+    </DragDropContextProvider>,
   );
 
-  screen.debug();
+  screen.getByText(name);
 });
