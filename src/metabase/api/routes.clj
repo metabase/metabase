947591f84ab5281b7b2fbdc3dac82e930620f5ed
dan sutton <dan@dpsutton.com>
2021-07-22 12:07:23 -0500

    Remove api route for render

--- src/metabase/api/routes.clj
+++ src/metabase/api/routes.clj
@@ -39,7 +39,6 @@
             [metabase.api.util :as util]
             [metabase.config :as config]
             [metabase.plugins.classloader :as classloader]
-            [metabase.pulse.render.poc :as render.poc]
             [metabase.server.middleware.auth :as middleware.auth]
             [metabase.server.middleware.exceptions :as middleware.exceptions]
             [metabase.util :as u]
@@ -108,8 +107,4 @@
   (context "/transform"            [] (+auth transform/routes))
   (context "/user"                 [] (+auth user/routes))
   (context "/util"                 [] util/routes)
-  (GET "/poc" [] (fn [_ respond _]
-                   (respond {:status  200
-                             :headers {"Content-Type" "image/png"}
-                             :body    (render.poc/png)})))
   (route/not-found (constantly {:status 404, :body (deferred-tru "API endpoint does not exist.")})))
