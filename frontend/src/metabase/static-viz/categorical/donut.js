f604e367a0e3ec8f2770713f44a55dbc631c4d7f
dan sutton <dan@dpsutton.com>
2021-07-23 14:26:10 -0500

    Revert "add ordinal legend to donuts (#17177)"
    This reverts commit 1eb81d2ebf52a3fab19aa34ef76ab79d492d5da0.

--- frontend/src/metabase/static-viz/categorical/donut.js
+++ frontend/src/metabase/static-viz/categorical/donut.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Pie } from "@visx/shape";
 import { scaleOrdinal } from "@visx/scale";
 import { Group } from "@visx/group";
-import { LegendOrdinal } from "@visx/legend";
 
 export default function Donut({ data, accessors }, layout) {
   const scale = scaleOrdinal({
@@ -60,12 +59,6 @@ export default function Donut({ data, accessors }, layout) {
           }}
         </Pie>
       </Group>
-      <LegendOrdinal
-        scale={scale}
-        direction={"row"}
-        itemMargin={8}
-        shape={"circle"}
-      />
     </svg>
   );
 }
