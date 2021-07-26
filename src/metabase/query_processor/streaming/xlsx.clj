e5a559e2af36127335c915db3124e13db2434f52
Noah Moss <noahbmoss@gmail.com>
2021-07-26 16:42:59 -0700

    initial work

--- src/metabase/query_processor/streaming/xlsx.clj
+++ src/metabase/query_processor/streaming/xlsx.clj
@@ -5,11 +5,12 @@
             [metabase.query-processor.error-type :as qp.error-type]
             [metabase.query-processor.streaming.common :as common]
             [metabase.query-processor.streaming.interface :as i]
+            [metabase.shared.models.visualization-settings :as mb.viz]
             [metabase.util.date-2 :as u.date]
             [metabase.util.i18n :refer [tru]])
   (:import java.io.OutputStream
            [java.time LocalDate LocalDateTime LocalTime OffsetDateTime OffsetTime ZonedDateTime]
-           [org.apache.poi.ss.usermodel BuiltinFormats Cell CellType DateUtil Workbook]
+           [org.apache.poi.ss.usermodel BuiltinFormats Cell CellType DateUtil Sheet Workbook]
            org.apache.poi.xssf.streaming.SXSSFWorkbook))
 
 (defmethod i/stream-options :xlsx
@@ -55,28 +56,48 @@
                            (doto (.createCellStyle workbook)
                              (.setDataFormat (cell-format style-name))))])))
 
+(defn- format-string
+  [format-settings]
+  (when (= (::mb.viz/number-style format-settings) "percent") "0%"))
+
+(defn- column-style-delays
+  "Creates a map of column name or ids -> delay. This is bound to `*cell-styles*` by `streaming-results-writer`.
+  Dereffing the delay will create the style and add it to the workbook if needed."
+  [^Workbook workbook column-settings]
+  (into {} (for [[field settings] column-settings]
+             ;; TODO reduce the number of data formats created?
+             (let [data-format (. workbook createDataFormat)
+                   id-or-name (or (::mb.viz/field-id field) (::mb.viz/column-name field))
+                   fmt-str (format-string settings)]
+               (when fmt-str
+                 {id-or-name (delay
+                              (doto (.createCellStyle workbook)
+                                (.setDataFormat (. data-format getFormat fmt-str))))})))))
+
 (defn- cell-style
   "Get the cell style associated with `style-name` by dereffing the delay in `*cell-styles*`."
   ^org.apache.poi.ss.usermodel.CellStyle [style-name]
   (some-> style-name *cell-styles* deref))
 
+(defmulti set-cell! (fn [^Cell _cell value column] (type value)))
+
 ;; Temporal values in Excel are just NUMERIC cells that are stored in a floating-point format and have some cell
 ;; styles applied that dictate how to format them
 
-(defmethod spreadsheet/set-cell! LocalDate
-  [^Cell cell ^LocalDate t]
+(defmethod set-cell! LocalDate
+  [^Cell cell ^LocalDate t _]
   (.setCellValue cell t)
   (.setCellType cell CellType/NUMERIC)
   (.setCellStyle cell (cell-style :date)))
 
-(defmethod spreadsheet/set-cell! LocalDateTime
-  [^Cell cell ^LocalDateTime t]
+(defmethod set-cell! LocalDateTime
+  [^Cell cell ^LocalDateTime t _]
   (.setCellValue cell t)
   (.setCellType cell CellType/NUMERIC)
   (.setCellStyle cell (cell-style :datetime)))
 
-(defmethod spreadsheet/set-cell! LocalTime
-  [^Cell cell t]
+(defmethod set-cell! LocalTime
+  [^Cell cell t _]
   ;; there's no `.setCellValue` for a `LocalTime` -- but all the built-in impls for `LocalDate` and `LocalDateTime` do
   ;; anyway is convert the date(time) to an Excel datetime floating-point number and then set that.
   ;;
@@ -88,23 +109,38 @@
   (.setCellType cell CellType/NUMERIC)
   (.setCellStyle cell (cell-style :time)))
 
-(defmethod spreadsheet/set-cell! OffsetTime
-  [cell t]
-  (spreadsheet/set-cell! cell (t/local-time (common/in-result-time-zone t))))
+(defmethod set-cell! OffsetTime
+  [cell t _]
+  (set-cell! cell (t/local-time (common/in-result-time-zone t))))
 
-(defmethod spreadsheet/set-cell! OffsetDateTime
-  [cell t]
-  (spreadsheet/set-cell! cell (t/local-date-time (common/in-result-time-zone t))))
+(defmethod set-cell! OffsetDateTime
+  [cell t _]
+  (set-cell! cell (t/local-date-time (common/in-result-time-zone t))))
 
-(defmethod spreadsheet/set-cell! ZonedDateTime
-  [cell t]
-  (spreadsheet/set-cell! cell (t/offset-date-time t)))
+(defmethod set-cell! ZonedDateTime
+  [cell t _]
+  (set-cell! cell (t/offset-date-time t)))
+
+(defmethod set-cell! String [^Cell cell value _]
+  (if (= (.getCellType cell) CellType/FORMULA) (.setCellType cell CellType/STRING))
+  (.setCellValue cell ^String value))
+
+(defmethod set-cell! Number [^Cell cell value column]
+  (let [name-or-id (or (:id column) (:name column))]
+    (def my-name-or-id name-or-id)
+    (if (= (.getCellType cell) CellType/FORMULA) (.setCellType cell CellType/NUMERIC))
+    (.setCellValue cell (double value))
+    (.setCellStyle cell (cell-style name-or-id))))
+
+(defmethod set-cell! Boolean [^Cell cell value _]
+  (if (= (.getCellType cell) CellType/FORMULA) (.setCellType cell CellType/BOOLEAN))
+  (.setCellValue cell ^Boolean value))
 
 ;; add a generic implementation for the method that writes values to XLSX cells that just piggybacks off the
 ;; implementations we've already defined for encoding things as JSON. These implementations live in
 ;; `metabase.server.middleware`.
-(defmethod spreadsheet/set-cell! Object
-  [^Cell cell value]
+(defmethod set-cell! Object
+  [^Cell cell value _]
   (when (= (.getCellType cell) CellType/FORMULA)
     (.setCellType cell CellType/STRING))
   ;; stick the object in a JSON map and encode it, which will force conversion to a string. Then unparse that JSON and
@@ -114,22 +150,40 @@
                                (json/parse-string keyword)
                                :v))))
 
+(defmethod set-cell! nil [^Cell cell _value _col]
+  (let [^String null nil]
+    (if (= (.getCellType cell) CellType/FORMULA) (.setCellType cell CellType/BLANK))
+    (.setCellValue cell null)))
+
+(defn add-row! [^Sheet sheet values cols]
+  (let [row-num (if (= 0 (.getPhysicalNumberOfRows sheet))
+                  0
+                  (inc (.getLastRowNum sheet)))
+        row (.createRow sheet row-num)]
+    ;; TODO rewrite
+    (doseq [[column-index value] (map-indexed #(list %1 %2) values)]
+      (def my-value value)
+      (set-cell! (.createCell row column-index) value (nth cols column-index)))
+    row))
+
 (defmethod i/streaming-results-writer :xlsx
   [_ ^OutputStream os]
   (let [workbook    (SXSSFWorkbook.)
         cell-styles (cell-style-delays workbook)
         sheet       (spreadsheet/add-sheet! workbook (tru "Query result"))]
     (reify i/StreamingResultsWriter
-      (begin! [_ {{:keys [ordered-cols]} :data} _]
+      (begin! [_ {{:keys [ordered-cols]} :data} viz-settings]
         (spreadsheet/add-row! sheet (map (some-fn :display_name :name) ordered-cols)))
 
-      (write-row! [_ row _ _ {:keys [output-order]}]
-        (binding [*cell-styles* cell-styles]
-          (let [ordered-row (if output-order
-                              (let [row-v (into [] row)]
-                                (for [i output-order] (row-v i)))
-                              row)]
-            (spreadsheet/add-row! sheet ordered-row))))
+      (write-row! [_ row _ ordered-cols {:keys [output-order] :as viz-settings}]
+        (let [ordered-row (if output-order
+                            (let [row-v (into [] row)]
+                              (for [i output-order] (row-v i)))
+                            row)
+              col-settings (::mb.viz/column-settings viz-settings)
+              col-styles (column-style-delays workbook col-settings)]
+          (binding [*cell-styles* (merge cell-styles col-styles)]
+            (add-row! sheet ordered-row ordered-cols))))
 
       (finish! [_ _]
         (spreadsheet/save-workbook-into-stream! os workbook)
