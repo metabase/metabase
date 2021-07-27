5e103ddc44d8067cd909b0034eb2b57481c6636d
Cam Saul <github@camsaul.com>
2021-07-27 15:59:14 -0700

    Don't need dependency on metabase-core to AOT the Spark SQL namespaces

--- modules/drivers/sparksql/build.clj
+++ modules/drivers/sparksql/build.clj
@@ -1,10 +1,16 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.pprint :as pprint]
+            [clojure.tools.build.api :as b]))
 
 (defn aot [_]
-  (b/compile-clj
-   {:src-dirs   ["src"]
-    :class-dir  "target/classes"
-    :basis      (b/create-basis {:aliases #{:compilation-basis}})
-    :ns-compile '[metabase.driver.FixedHiveConnection
-                  metabase.driver.FixedHiveDriver]}))
+  (try
+    (b/compile-clj
+     {:src-dirs   ["src"]
+      :class-dir  "target/classes"
+      :basis      (b/create-basis nil)
+      :ns-compile '[metabase.driver.FixedHiveConnection
+                    metabase.driver.FixedHiveDriver]})
+    (catch Throwable e
+      (println "Error AOT compiling Spark SQL namespaces:" (ex-message e))
+      (pprint/pprint (Throwable->map e))
+      (throw e))))
