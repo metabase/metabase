7ef0cb21c9237a1a21147bf82f16fd9a075f8144
Anton Kulyk <kuliks.anton@gmail.com>
2021-07-27 13:18:51 +0300

    Display official collection badge on the homepage

--- frontend/src/metabase/components/CollectionItem.jsx
+++ frontend/src/metabase/components/CollectionItem.jsx
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import Card from "metabase/components/Card";
 import Ellipsified from "metabase/components/Ellipsified";
 
+import { getCollectionIcon } from "metabase/entities/collections";
+
 import {
   ItemLink,
   IconContainer,
@@ -13,17 +15,17 @@ import {
 
 const propTypes = {
   collection: PropTypes.object.isRequired,
-  iconName: PropTypes.string,
   event: PropTypes.string,
 };
 
-const CollectionItem = ({ collection, event, iconName }) => {
+const CollectionItem = ({ collection, event }) => {
+  const icon = getCollectionIcon(collection);
   return (
     <ItemLink to={collection.getUrl()} data-metabase-event={event}>
       <Card hoverable>
         <CardContent>
           <IconContainer>
-            <CollectionIcon name={iconName} />
+            <CollectionIcon name={icon.name} />
           </IconContainer>
           <h4 className="overflow-hidden">
             <Ellipsified>{collection.name}</Ellipsified>
@@ -35,9 +37,5 @@ const CollectionItem = ({ collection, event, iconName }) => {
 };
 
 CollectionItem.propTypes = propTypes;
-
-CollectionItem.defaultProps = {
-  iconName: "folder",
-};
 
 export default CollectionItem;
