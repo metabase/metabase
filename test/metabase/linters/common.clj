40e3ab0165cb5311342cd03a61adb1693b29f697
Cam Saul <github@camsaul.com>
2021-07-28 13:30:46 -0700

    Remove linter shim namespaces (deps.edn branch specific)

--- test/metabase/linters/common.clj
+++
@@ -1,9 +0,0 @@
-(ns metabase.linters.common
-  (:require [clojure.java.classpath :as classpath]
-            [clojure.string :as str]))
-
-(defn source-paths []
-  (filter (fn [^java.io.File file]
-            (and (.isDirectory ^java.io.File file)
-                 (not (str/ends-with? (.getName file) "resources"))))
-          (classpath/system-classpath)))
