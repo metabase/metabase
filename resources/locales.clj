b0f2fc84ee40a277fd0b0315ce1f9868f33cc4f8
Cam Saul <github@camsaul.com>
2021-07-28 10:56:36 -0700

    Revert formatting changes to locales.clj

--- resources/locales.clj
+++ resources/locales.clj
@@ -1,27 +1,5 @@
 {:locales
- #{"nl"
-   "en"
-   "zh"
-   "sr"
-   "tr"
-   "it"
-   "fa"
-   "vi"
-   "zh-TW"
-   "uk"
-   "pl"
-   "ca"
-   "sv"
-   "zh-HK"
-   "fr"
-   "pt-BR"
-   "de"
-   "nb"
-   "ru"
-   "sk"
-   "es"
-   "ja"
-   "cs"
-   "bg"},
+ #{"nl" "en" "zh" "tr" "it" "fa" "vi" "zh-TW" "uk" "pl" "ca" "sv"
+   "zh-HK" "fr" "pt-BR" "de" "nb" "ru" "sk" "es" "ja" "cs" "bg"},
  :packages ["metabase"],
  :bundle "metabase.Messages"}
