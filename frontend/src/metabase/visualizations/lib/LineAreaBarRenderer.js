18e6a3bd2133c357f58a4b5cb5c0ffc84aeabf37
alxnddr <alxnddr@gmail.com>
2021-07-29 00:57:57 +0300

    percy demo

--- frontend/src/metabase/visualizations/lib/LineAreaBarRenderer.js
+++ frontend/src/metabase/visualizations/lib/LineAreaBarRenderer.js
@@ -453,7 +453,7 @@ function doScatterChartStuff(chart, datas, index, { yExtent, yExtents }) {
         );
     } else {
       chart.radiusValueAccessor(d => 1);
-      chart.MIN_RADIUS = 3;
+      chart.MIN_RADIUS = 30;
     }
     chart.minRadiusWithLabel(Infinity);
   }
