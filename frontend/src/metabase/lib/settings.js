fd0c526a93a29d813a6d611cf7709ef0b6468bf5
Noah Moss <noahbmoss@gmail.com>
2021-07-29 11:57:13 -0700

    expose is-hosted? setting

--- frontend/src/metabase/lib/settings.js
+++ frontend/src/metabase/lib/settings.js
@@ -86,11 +86,8 @@ class Settings {
     return this.get("email-configured?");
   }
 
-  // Right now, all Metabase Cloud hosted instances run on *.metabaseapp.com
-  // We plan on changing this to look at an envvar in the future instead.
   isHosted() {
-    // matches <custom>.metabaseapp.com and <custom>.metabaseapp.com/
-    return /.+\.metabaseapp.com\/?$/i.test(this.get("site-url"));
+    return this.get("is-hosted?");
   }
 
   googleAuthEnabled() {
@@ -111,10 +108,6 @@ class Settings {
 
   searchTypeaheadEnabled() {
     return this.get("search-typeahead-enabled");
-  }
-
-  ssoEnabled() {
-    return this.get("google-auth-client-id") != null;
   }
 
   trackingEnabled() {
--- src/metabase/public_settings/metastore.clj
+++ src/metabase/public_settings/metastore.clj
@@ -184,6 +184,13 @@
   :setter     :none
   :getter     (fn [] (and config/ee-available? (boolean ((token-features) "sso")))))
 
+(defsetting is-hosted?
+  "Is the Metabase instance running in the cloud?"
+  :type       :boolean
+  :visibility :public
+  :setter     :none
+  :getter     (fn [] (boolean ((token-features) "hosting"))))
+
 ;; `enhancements` are not currently a specific "feature" that EE tokens can have or not have. Instead, it's a
 ;; catch-all term for various bits of EE functionality that we assume all EE licenses include. (This may change in the
 ;; future.)
