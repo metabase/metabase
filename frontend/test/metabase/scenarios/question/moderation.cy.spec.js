f9b283531eac6643b2ca5711a17d3a08e840b295
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-07-30 13:00:26 -0700

    fix moderation cy tests

--- frontend/test/metabase/scenarios/question/moderation.cy.spec.js
+++ frontend/test/metabase/scenarios/question/moderation.cy.spec.js
@@ -1,7 +1,4 @@
 import { describeWithToken, restore } from "__support__/e2e/cypress";
-import { SAMPLE_DATASET } from "__support__/e2e/cypress_sample_dataset";
-
-const { PRODUCTS_ID } = SAMPLE_DATASET;
 
 describeWithToken("scenarios > saved question moderation", () => {
   describe("as an admin", () => {
@@ -29,9 +26,10 @@ describeWithToken("scenarios > saved question moderation", () => {
       cy.findByTestId("moderation-verify-action").click();
       cy.findByTestId("moderation-remove-review-action").click();
 
-      cy.findByText("You verified this").should("not.be.visible");
+      cy.findByText("You verified this").should("not.exist");
       cy.findByTestId("saved-question-header-button").click();
-      cy.icon("verified").should("not.be.visible");
+
+      cy.icon("verified").should("not.exist");
     });
   });
 
@@ -39,28 +37,34 @@ describeWithToken("scenarios > saved question moderation", () => {
     beforeEach(() => {
       restore();
       cy.signInAsNormalUser();
+
+      cy.intercept("GET", "/api/card/1", req => {
+        req.reply(res => {
+          res.body.moderation_reviews = [
+            {
+              status: "verified",
+              most_recent: true,
+              moderator_id: 999,
+              id: 1,
+              moderated_item_type: "card",
+              moderated_item_id: 4,
+              updated_at: "2021-07-23T09:56:46.276-07:00",
+              created_at: "2021-07-23T09:56:46.276-07:00",
+            },
+          ];
+        });
+      }).as("cardGet");
     });
 
     it("should be able to see that a question has been verified", () => {
-      cy.createQuestion({
-        name: "Verified Question",
-        query: { "source-table": PRODUCTS_ID },
-        moderation_reviews: [
-          {
-            state: "verified",
-            most_recent: true,
-            moderator_id: 999,
-            created_at: Date.now(),
-          },
-        ],
-      }).then(({ body: { id: QUESTION_ID } }) => {
-        cy.visit(`/question/${QUESTION_ID}`);
+      cy.visit(`/question/1`);
 
-        cy.icon("verified").should("be.visible");
+      cy.wait("@cardGet");
 
-        cy.findByTestId("saved-question-header-button").click();
-        cy.findByText("A moderator verified this").should("be.visible");
-      });
+      cy.icon("verified").should("exist");
+
+      cy.findByTestId("saved-question-header-button").click();
+      cy.findByText("A moderator verified this").should("exist");
     });
   });
 });
