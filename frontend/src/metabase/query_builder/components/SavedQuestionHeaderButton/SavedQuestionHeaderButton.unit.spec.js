b02dadb3b37eb54e7b4ad3d49f05171dba0f4c55
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-08-03 16:01:24 +0200

    Remove unnecessary imports from unit tests

--- frontend/src/metabase/query_builder/components/SavedQuestionHeaderButton/SavedQuestionHeaderButton.unit.spec.js
+++ frontend/src/metabase/query_builder/components/SavedQuestionHeaderButton/SavedQuestionHeaderButton.unit.spec.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 
 import SavedQuestionHeaderButton from "./SavedQuestionHeaderButton";
--- frontend/test/metabase/query_builder/components/ClampedDescription.unit.spec.js
+++ frontend/test/metabase/query_builder/components/ClampedDescription.unit.spec.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 
 import { ClampedDescription } from "metabase/query_builder/components/ClampedDescription";
