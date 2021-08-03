5384cf838cdffbef80156b3bf437ab5e5ecc038c
Howon Lee <hlee.howon@gmail.com>
2021-08-03 11:21:09 -0700

    cardinality

--- frontend/src/metabase/nav/components/SearchBar.jsx
+++ frontend/src/metabase/nav/components/SearchBar.jsx
@@ -52,7 +52,7 @@ const SearchInput = styled.input`
 `;
 
 const ALLOWED_SEARCH_FOCUS_ELEMENTS = new Set(["BODY", "A"]);
-const SEARCH_LIMIT = 1000;
+const SEARCH_LIMIT = 100;
 
 export default class SearchBar extends React.Component {
   state = {
