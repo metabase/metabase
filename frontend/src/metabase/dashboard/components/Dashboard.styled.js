ae54d883ea903847a053a898249c9c75af60fba4
Gustavo Saiani <gustavo@poe.ma>
2021-08-04 10:30:13 -0300

    Remove classNames from filter container

--- frontend/src/metabase/dashboard/components/Dashboard.styled.js
+++ frontend/src/metabase/dashboard/components/Dashboard.styled.js
@@ -1,16 +1,35 @@
 import styled from "styled-components";
+import {
+  breakpointMinSmall,
+  breakpointMinMedium,
+} from "metabase/styled-components/theme/media-queries";
 
 import { color } from "metabase/lib/colors";
+import { space } from "metabase/styled-components/theme/space";
 
-export const ParametersWidgetContainer = styled.div.attrs({
-  className: "wrapper flex flex-column align-start mt2",
-})`
+const Wrapper = styled.div`
+  margin: 10px auto 0;
+  width: 100%;
+
+  ${breakpointMinSmall} {
+    padding-left: 2em;
+    padding-right: 2em;
+  }
+
+  ${breakpointMinMedium} {
+    padding-left: 3em;
+    padding-right: 3em;
+  }
+`;
+
+export const ParametersWidgetContainer = styled(Wrapper)`
+  align-items: flex-start;
   background-color: ${color("bg-light")};
-  margin: 0 auto;
-  padding-top: 10px !important;
-  padding-bottom: 5px !important;
+  display: flex;
+  flex-direction: column;
+  padding-top: ${space(2)};
+  padding-bottom: ${space(1)};
   position: sticky;
   top: 0;
-  width: 100%;
   z-index: 3;
 `;
