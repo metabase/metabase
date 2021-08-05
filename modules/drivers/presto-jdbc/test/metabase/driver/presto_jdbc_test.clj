0ba568ac377db294d6104d860fc70117b9f310a3
Jeff Evans <jeff.evans@metabase.com>
2021-08-05 11:15:16 -0500

    Fix namespace again

--- modules/drivers/presto-jdbc/test/metabase/driver/presto_jdbc_test.clj
+++ modules/drivers/presto-jdbc/test/metabase/driver/presto_jdbc_test.clj
@@ -10,7 +10,6 @@
             [metabase.driver.presto-jdbc :as presto-jdbc]
             [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
             [metabase.driver.sql.query-processor :as sql.qp]
-            [metabase.driver.util :as driver.u]
             [metabase.models.database :refer [Database]]
             [metabase.models.field :refer [Field]]
             [metabase.models.table :as table :refer [Table]]
