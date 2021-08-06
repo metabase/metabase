bbdf3768cb05a9a14ea387fffd72f69e05c748b7
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-08-06 02:09:24 +0200

    Update frontend/test/metabase/scenarios/onboarding/about.cy.spec.js
    Co-authored-by: flamber <1447303+flamber@users.noreply.github.com>

--- frontend/test/metabase/scenarios/onboarding/about.cy.spec.js
+++ frontend/test/metabase/scenarios/onboarding/about.cy.spec.js
@@ -11,7 +11,7 @@ describe("scenarios > about Metabase", () => {
   });
 
   it.skip("should display correct Metabase version (metabase#15656)", () => {
-    cy.findByText(/You're on version v[01]\.\d+\.\d+(-SNAPSHOT)?/i);
+    cy.findByText(/You're on version v[01](\.\d+){2,3}(-[\w\d]+)?/i);
     cy.findByText(/Built on \d{4}-\d{2}-\d{2}/);
 
     cy.findByText("Branch: ?").should("not.exist");
