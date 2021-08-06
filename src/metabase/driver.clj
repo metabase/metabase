bfb04ca0aa7a0c969501fedbef72baf3682ae52a
Howon Lee <hlee.howon@gmail.com>
2021-08-06 14:58:00 -0700

    Another comment whacked

--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -425,6 +425,14 @@
 
   Database is guaranteed to be a Database instance.
 
+  Most drivers can always return true or always return false for a given feature
+  (e.g., :left-join is not supported by any version of Mongo DB).
+
+  In some cases, a feature may only be supported by certain versions of the database engine.
+  In this case, after implementing `:version` in `describe-database` for the driver,
+  you can check in `(get-in db [:details :version])` and determine
+  whether a feature is supported for this particular database.
+
     (database-supports? :mongo :set-timezone mongo-db) ; -> true"
   {:arglists '([driver feature database])}
   (fn [driver feature database]
