9cb18a0770f53332c8dfe4fa830441d40655bff2
Cam Saul <github@camsaul.com>
2021-08-06 15:02:55 -0700

    Unskip repro for #16170

--- frontend/test/metabase-db/mongo/line.cy.spec.js
+++ frontend/test/metabase-db/mongo/line.cy.spec.js
@@ -2,17 +2,14 @@ import { restore, addMongoDatabase, popover } from "__support__/e2e/cypress";
 
 const MONGO_DB_NAME = "QA Mongo4";
 
-// Skipping the whole describe block because it contains only one skipped test so far!
-// We don't want to run the whole beforeEeach block in CI only to skip the test afterwards.
-// IMPORTANT: when #16170 gets fixed, unskip both describe block and the test itself!
-describe.skip("mongodb > visualization > line chart", () => {
+describe("mongodb > visualization > line chart", () => {
   before(() => {
     restore();
     cy.signInAsAdmin();
     addMongoDatabase(MONGO_DB_NAME);
   });
 
-  it.skip("should correctly replace only the missing values with zero (metabase#16170)", () => {
+  it("should correctly replace only the missing values with zero (metabase#16170)", () => {
     cy.visit("/question/new");
     cy.findByText("Simple question").click();
     cy.findByText(MONGO_DB_NAME).click();
