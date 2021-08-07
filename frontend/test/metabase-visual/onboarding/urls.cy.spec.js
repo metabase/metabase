4dfadbbc8bfeaace8c05f570eaf8d00fff5fa110
Ariya Hidayat <ariya@metabase.com>
2021-08-06 18:21:23 -0700

    Add a few checks before taking Percy snapshot

--- frontend/test/metabase-visual/onboarding/urls.cy.spec.js
+++ frontend/test/metabase-visual/onboarding/urls.cy.spec.js
@@ -7,22 +7,50 @@ describe("visual tests > onboarding > URLs", () => {
   });
 
   it("home", () => {
+    cy.intercept("GET", `/api/automagic-dashboards`).as("automagic-dashboards");
     cy.visit("/");
+
+    cy.wait("@automagic-dashboards");
+
+    cy.findByText("Reviews table");
+    cy.findByText("First collection");
+    cy.findByText("Sample Dataset");
+
     cy.percySnapshot();
   });
 
   it("root collection", () => {
+    cy.intercept("GET", `api/collection/root/items`).as("collection-items");
     cy.visit("/collection/root");
+
+    // Twice, one for pinned items and another for dashboard
+    cy.wait("@collection-items");
+    cy.wait("@collection-items");
+
+    cy.findByText("First collection");
+    cy.findByText("Your personal collection");
+
     cy.percySnapshot();
   });
 
   it("browse", () => {
+    cy.intercept("GET", `api/database`).as("database");
     cy.visit("/browse/");
+
+    cy.wait("@database");
+    cy.findByText("Sample Dataset");
+
     cy.percySnapshot();
   });
 
   it("browse/1 (Sample Dataset)", () => {
+    cy.intercept("GET", `api/database/1/schemas`).as("schemas");
     cy.visit("/browse/1");
+
+    cy.wait("@schemas");
+    cy.findByText("Sample Dataset");
+    cy.findByText("Reviews");
+
     cy.percySnapshot();
   });
 });
