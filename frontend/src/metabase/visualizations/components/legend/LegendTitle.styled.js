70e9c40f15fc0bf2fb37e7dcdaec1284aafb35e0
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-09 13:31:38 +0300

    Add caption

---
+++ frontend/src/metabase/visualizations/components/legend/LegendTitle.styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import colors from "metabase/lib/colors";
+import Icon from "metabase/components/Icon";
+import { space } from "metabase/styled-components/theme";
+
+export const LegendTitleRoot = styled.div`
+  display: flex;
+  align-items: center;
+  min-width: 0;
+`;
+
+export const LegendTitleIcon = styled(Icon)`
+  padding-right: ${space(0)};
+`;
+
+export const LegendTitleLabel = styled.div`
+  color: ${colors["text-dark"]};
+  font-weight: bold;
+  cursor: ${({ onClick }) => (onClick ? "pointer" : "")};
+
+  &:hover {
+    color: ${colors["brand"]};
+  }
+`;
+
+export const LegendTitleInfo = styled(Icon).attrs({
+  name: "info",
+})`
+  color: ${colors["text-medium"]};
+  margin-left: ${space(1)};
+`;
+
+export const LegendTitleButtonGroup = styled.span`
+  flex: 0 0 auto;
+  position: relative;
+  margin-left: ${space(1)};
+`;
