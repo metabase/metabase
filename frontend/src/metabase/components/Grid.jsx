b7148c5cde172dd3d730eb6c870f5ff4de55e2e0
Ariya Hidayat <ariya@metabase.com>
2021-08-09 11:27:33 -0700

    Grid's width is an array (responsive sizes)

--- frontend/src/metabase/components/Grid.jsx
+++ frontend/src/metabase/components/Grid.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Box, Flex } from "grid-styled";
 
 export const GridItem = ({ children, w, px, py, ...props }) => (
-  <Box px={px} py={py} {...props} style={{ width: w }}>
+  <Box px={px} py={py} {...props} width={w}>
     {children}
   </Box>
 );
