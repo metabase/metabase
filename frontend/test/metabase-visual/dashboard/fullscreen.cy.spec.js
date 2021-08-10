bc8a5dccb39b765abfa7be0918646ceb5fbb8723
Gustavo Saiani <gustavo@poe.ma>
2021-08-10 15:17:47 -0300

    Simplify fullscreen test

--- frontend/test/metabase-visual/dashboard/fullscreen.cy.spec.js
+++ frontend/test/metabase-visual/dashboard/fullscreen.cy.spec.js
@@ -1,70 +1,67 @@
 import { restore } from "__support__/e2e/cypress";
+import { SAMPLE_DATASET } from "__support__/e2e/cypress_sample_dataset";
 
-import {
-  addFiltersToDashboard,
-  saveCard,
-} from "../../metabase/scenarios/dashboard/helpers/e2e-filter-helpers";
+const { PRODUCTS, PRODUCTS_ID } = SAMPLE_DATASET;
 
-const questionParams = {
-  name: "14473",
-  native: { query: "SELECT COUNT(*) FROM PRODUCTS", "template-tags": {} },
+const questionDetails = {
+  name: "15119",
+  query: { "source-table": PRODUCTS_ID },
 };
 
-describe("visual tests > dashboard", () => {
+const filter = {
+  name: "Category",
+  slug: "category",
+  id: "ad1c877e",
+  type: "category",
+};
+
+describe("visual tests > dashboard > fullscreen", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+
+    cy.createQuestionAndDashboard({ questionDetails }).then(
+      ({ body: { id, card_id, dashboard_id } }) => {
+        cy.addFilterToDashboard({ filter, dashboard_id });
+
+        // Connect filter to the card
+        cy.request("PUT", `/api/dashboard/${dashboard_id}/cards`, {
+          cards: [
+            {
+              id,
+              card_id,
+              row: 0,
+              col: 0,
+              sizeX: 12,
+              sizeY: 9,
+              visualization_settings: {},
+              parameter_mappings: [
+                {
+                  parameter_id: "ad1c877e",
+                  card_id,
+                  target: ["dimension", ["field-id", PRODUCTS.CATEGORY]],
+                },
+              ],
+            },
+          ],
+        });
+
+        cy.visit(`/dashboard/${dashboard_id}`);
+      },
+    );
   });
 
-  describe("shows dashboard in fullscreen mode", () => {
-    it("renders in day mode", () => {
-      cy.createNativeQuestion(questionParams).then(
-        ({ body: { id: questionId } }) => {
-          cy.createDashboard("14473D").then(({ body: { id: dashboardId } }) => {
-            cy.log("Add 4 filters to the dashboard");
+  it("renders in day mode and night mode", () => {
+    cy.icon("expand").click();
 
-            addFiltersToDashboard(dashboardId);
+    cy.icon("moon");
 
-            saveCard(dashboardId, questionId);
+    cy.percySnapshot("Shows dashboard in fullscreen day mode" + Date.now());
 
-            cy.visit(`/dashboard/${dashboardId}`);
+    cy.icon("moon").click();
 
-            cy.icon("expand").click();
+    cy.icon("sun");
 
-            cy.icon("moon");
-
-            cy.percySnapshot(
-              "Shows dashboard in fullscreen day mode" + Date.now(),
-            );
-          });
-        },
-      );
-    });
-
-    it("renders in night mode", () => {
-      cy.createNativeQuestion(questionParams).then(
-        ({ body: { id: questionId } }) => {
-          cy.createDashboard("14473D").then(({ body: { id: dashboardId } }) => {
-            cy.log("Add 4 filters to the dashboard");
-
-            addFiltersToDashboard(dashboardId);
-
-            saveCard(dashboardId, questionId);
-
-            cy.visit(`/dashboard/${dashboardId}`);
-
-            cy.icon("expand").click();
-
-            cy.icon("moon").click();
-
-            cy.icon("sun");
-
-            cy.percySnapshot(
-              "Shows dashboard in fullscreen night mode" + Date.now(),
-            );
-          });
-        },
-      );
-    });
+    cy.percySnapshot("Shows dashboard in fullscreen night mode" + Date.now());
   });
 });
