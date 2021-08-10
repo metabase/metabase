f530ee2d13d479f8bfd5f366b6d34a3e0cd7d4be
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-10 22:29:28 +0300

    Add layout with spacing

--- frontend/src/metabase/visualizations/components/legend/LegendLayout.jsx
+++ frontend/src/metabase/visualizations/components/legend/LegendLayout.jsx
@@ -1,49 +1,82 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { iconPropTypes } from "metabase/components/Icon";
+import Legend from "./Legend";
+import LegendCaption from "./LegendCaption";
 import {
   CaptionContainer,
   ChartContainer,
   LegendContainer,
   LegendLayoutRoot,
+  MainContainer,
 } from "./LegendLayout.styled";
-import Legend from "./Legend";
-import LegendCaption from "./LegendCaption";
-import PropTypes from "prop-types";
-import { iconPropTypes } from "metabase/components/Icon";
 
 const propTypes = {
   className: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
+  labels: PropTypes.array.isRequired,
+  colors: PropTypes.array.isRequired,
+  hovered: PropTypes.object,
+  hasTitle: PropTypes.bool,
   icon: PropTypes.shape(iconPropTypes),
   actionButtons: PropTypes.node,
   children: PropTypes.node,
   onSelectTitle: PropTypes.func,
+  onHoverChange: PropTypes.func,
+  onAddSeries: PropTypes.func,
+  onSelectSeries: PropTypes.func,
+  onRemoveSeries: PropTypes.func,
 };
 
 const LegendLayout = ({
   className,
   title,
   description,
+  labels,
+  colors,
+  hovered,
+  hasTitle,
+  hasLegend,
   icon,
   actionButtons,
   children,
   onSelectTitle,
+  onHoverChange,
+  onAddSeries,
+  onSelectSeries,
+  onRemoveSeries,
 }) => {
   return (
     <LegendLayoutRoot className={className}>
-      <CaptionContainer>
-        <LegendCaption
-          title={title}
-          description={description}
-          icon={icon}
-          actionButtons={actionButtons}
-          onSelectTitle={onSelectTitle}
-        />
-      </CaptionContainer>
-      <LegendContainer>
-        <Legend />
-      </LegendContainer>
-      <ChartContainer>{children}</ChartContainer>
+      {hasTitle && (
+        <CaptionContainer>
+          <LegendCaption
+            title={title}
+            description={description}
+            icon={icon}
+            actionButtons={actionButtons}
+            onSelectTitle={onSelectTitle}
+          />
+        </CaptionContainer>
+      )}
+      <MainContainer>
+        {hasLegend && (
+          <LegendContainer>
+            <Legend
+              labels={labels}
+              colors={colors}
+              hovered={hovered}
+              actionButtons={!hasTitle && actionButtons}
+              onHoverChange={onHoverChange}
+              onAddSeries={onAddSeries}
+              onSelectSeries={onSelectSeries}
+              onRemoveSeries={onRemoveSeries}
+            />
+          </LegendContainer>
+        )}
+        <ChartContainer>{children}</ChartContainer>
+      </MainContainer>
     </LegendLayoutRoot>
   );
 };
--- frontend/src/metabase/visualizations/components/legend/LegendLayout.styled.jsx
+++ frontend/src/metabase/visualizations/components/legend/LegendLayout.styled.jsx
@@ -3,15 +3,23 @@ import styled from "styled-components";
 export const LegendLayoutRoot = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 0.5rem 1rem;
 `;
 
 export const CaptionContainer = styled.div`
   flex: 0 0 auto;
-  margin: 0 0.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex: 1 0 auto;
+  flex-direction: column;
 `;
 
 export const LegendContainer = styled.div`
   flex: 0 0 auto;
+  margin-bottom: 0.5rem;
 `;
 
 export const ChartContainer = styled.div`
