36d641f61bc72a1bb9a524bbc6cac656538be3db
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-08-14 00:03:08 +0200

    Refactor and dry up `question` custom Cypress commands

--- frontend/test/__support__/e2e/commands/api/question.js
+++ frontend/test/__support__/e2e/commands/api/question.js
@@ -1,51 +1,70 @@
-Cypress.Commands.add(
-  "createQuestion",
-  ({
-    name = "card",
-    query = {},
-    display = "table",
-    database = 1,
-    visualization_settings = {},
-    collection_id = null,
-    collection_position = null,
-  } = {}) => {
-    cy.log(`Create a question: ${name}`);
-    cy.request("POST", "/api/card", {
-      name,
-      dataset_query: {
-        type: "query",
-        query,
-        database,
-      },
-      display,
-      visualization_settings,
-      collection_id,
-      collection_position,
-    });
-  },
-);
+Cypress.Commands.add("createQuestion", (questionDetails, customOptions) => {
+  const { name, query } = questionDetails;
+
+  throwIfNotPresent(query);
+
+  logAction("Create a QB question", name);
+
+  question("query", questionDetails, customOptions);
+});
 
 Cypress.Commands.add(
   "createNativeQuestion",
-  ({
-    name = "native",
-    native = {},
-    display = "table",
-    database = 1,
-    visualization_settings = {},
-    collection_id = null,
-  } = {}) => {
-    cy.log(`Create a native question: ${name}`);
-    cy.request("POST", "/api/card", {
-      name,
-      collection_id,
-      dataset_query: {
-        type: "native",
-        native,
-        database,
-      },
-      display,
-      visualization_settings,
-    });
+  (questionDetails, customOptions) => {
+    const { name, native } = questionDetails;
+
+    throwIfNotPresent(native);
+
+    logAction("Create a native question", name);
+
+    question("native", questionDetails, customOptions);
   },
 );
+
+function question(
+  type,
+  {
+    name = "test question",
+    native,
+    query,
+    database = 1,
+    display = "table",
+    visualization_settings = {},
+    collection_id,
+    collection_position,
+  } = {},
+  { loadMetadata = false, visitQuestion = false } = {},
+) {
+  cy.request("POST", "/api/card", {
+    name,
+    dataset_query: {
+      type,
+      [type]: type === "native" ? native : query,
+      database,
+    },
+    display,
+    visualization_settings,
+    collection_id,
+    collection_position,
+  }).then(({ body }) => {
+    if (loadMetadata || visitQuestion) {
+      cy.intercept("POST", `/api/card/${body.id}/query`).as("cardQuery");
+      cy.visit(`/question/${body.id}`);
+
+      // Wait for `result_metadata` to load
+      cy.wait("@cardQuery");
+    }
+  });
+}
+
+function throwIfNotPresent(param) {
+  if (!param) {
+    throw new Error(`Wrong key! Expected "query" or "native".`);
+  }
+}
+
+function logAction(string, name) {
+  const message = name ? `${string}: ${name}` : string;
+
+  cy.log(message);
+}
