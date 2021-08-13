01078ee678208ccbafba448e1640fad2e41ab394
Anton Kulyk <kuliks.anton@gmail.com>
2021-08-13 22:03:27 +0300

    Use tooltips for CollectionAuthorityLevelIcon

--- enterprise/frontend/src/metabase-enterprise/collections/components/CollectionAuthorityLevelIcon.jsx
+++ enterprise/frontend/src/metabase-enterprise/collections/components/CollectionAuthorityLevelIcon.jsx
@@ -7,12 +7,17 @@ import { color } from "metabase/lib/colors";
 import { AUTHORITY_LEVELS, REGULAR_COLLECTION } from "../constants";
 
 const propTypes = {
+  tooltip: PropTypes.string,
   collection: PropTypes.shape({
     authority_level: PropTypes.oneOf(["official"]),
   }),
 };
 
-export function CollectionAuthorityLevelIcon({ collection, ...iconProps }) {
+export function CollectionAuthorityLevelIcon({
+  collection,
+  tooltip = "default",
+  ...iconProps
+}) {
   const level = AUTHORITY_LEVELS[collection.authority_level];
   if (!level || level.type === REGULAR_COLLECTION.type) {
     return null;
@@ -21,6 +26,7 @@ export function CollectionAuthorityLevelIcon({ collection, ...iconProps }) {
     <Icon
       {...iconProps}
       name={level.icon}
+      tooltip={level.tooltips?.[tooltip] || tooltip}
       style={{ color: color(level.color) }}
       data-testid={`${level.type}-collection-marker`}
     />
