fb79485a527bb381b8c16bfb8a62b2ec76af0e4b
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-16 14:44:01 +0300

    Fix button

--- frontend/src/metabase/visualizations/components/legend/LegendLayout.styled.jsx
+++ frontend/src/metabase/visualizations/components/legend/LegendLayout.styled.jsx
@@ -14,6 +14,7 @@ export const MainContainer = styled.div`
 `;
 
 export const LegendContainer = styled.div`
+  display: ${({ isVertical }) => (isVertical ? "block" : "flex")};
   max-width: ${({ isVertical }) => (isVertical ? "25%" : "")};
   max-width: ${({ isVertical }) => (isVertical ? "min(25%, 20rem)" : "")};
   margin-right: ${({ isVertical }) => (isVertical ? "0.5rem" : "")};
