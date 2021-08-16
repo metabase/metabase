743da4d039436bbf63e80d4c5e62f60d20f7f160
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-16 17:15:11 +0300

    Add account header

--- frontend/src/metabase/account/layout/AccountHeader.jsx
+++ frontend/src/metabase/account/layout/AccountHeader.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 import { t } from "ttag";
+import Radio from "metabase/components/Radio";
+import { PLUGIN_SHOW_CHANGE_PASSWORD_CONDITIONS } from "metabase/plugins";
 import {
   AccountHeaderRoot,
   HeaderAvatar,
@@ -9,18 +11,42 @@ import {
 } from "./AccountHeader.styled";
 
 const propTypes = {
+  tab: PropTypes.string,
   user: PropTypes.object,
+  onTabChange: PropTypes.func,
 };
 
-const AccountHeader = ({ user }) => {
+const AccountHeader = ({ tab, user, onTabChange }) => {
+  const hasPasswordChange = useMemo(
+    () => PLUGIN_SHOW_CHANGE_PASSWORD_CONDITIONS.every(f => f(user)),
+    [user],
+  );
+
+  const tabs = useMemo(
+    () => [
+      { name: t`Profile`, value: "details" },
+      ...(hasPasswordChange ? [{ name: t`Password`, value: "password" }] : []),
+      { name: t`Login History`, value: "loginHistory" },
+    ],
+    [hasPasswordChange],
+  );
+
   return (
     <AccountHeaderRoot>
       <HeaderSection>
         <HeaderAvatar user={user} />
         <HeaderTitle>{t`Account settings`}</HeaderTitle>
       </HeaderSection>
+      <Radio
+        value={tab}
+        variant="underlined"
+        options={tabs}
+        onChange={onTabChange}
+      />
     </AccountHeaderRoot>
   );
 };
 
 AccountHeader.propTypes = propTypes;
+
+export default AccountHeader;
