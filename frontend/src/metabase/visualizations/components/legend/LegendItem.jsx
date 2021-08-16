56b0802c7c0da1de0730d14f6f5c0c964799ef6e
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-16 14:41:25 +0300

    Add LegendLayout to LineChart  (#17467)

---
+++ frontend/src/metabase/visualizations/components/legend/LegendItem.jsx
@@ -0,0 +1,80 @@
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+import {
+  LegendItemDot,
+  LegendItemLabel,
+  LegendItemRemoveIcon,
+  LegendItemRoot,
+  LegendItemTitle,
+} from "./LegendItem.styled";
+import Ellipsified from "metabase/components/Ellipsified";
+import Tooltip from "metabase/components/Tooltip";
+
+const propTypes = {
+  label: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+  isMuted: PropTypes.bool,
+  isNarrow: PropTypes.bool,
+  isVertical: PropTypes.bool,
+  onHoverChange: PropTypes.func,
+  onSelectSeries: PropTypes.func,
+  onRemoveSeries: PropTypes.func,
+};
+
+const LegendItem = ({
+  label,
+  index,
+  color,
+  isMuted,
+  isNarrow,
+  isVertical,
+  onHoverChange,
+  onSelectSeries,
+  onRemoveSeries,
+}) => {
+  const handleItemClick = event => {
+    onSelectSeries && onSelectSeries(event, index);
+  };
+
+  const handleItemMouseEnter = event => {
+    onHoverChange && onHoverChange({ index, element: event.currentTarget });
+  };
+
+  const handleItemMouseLeave = () => {
+    onHoverChange && onHoverChange();
+  };
+
+  const handleRemoveClick = event => {
+    onRemoveSeries && onRemoveSeries(event, index);
+  };
+
+  return (
+    <LegendItemRoot
+      isNarrow={isNarrow}
+      isVertical={isVertical}
+      data-testid="legend-item"
+    >
+      <Tooltip tooltip={label}>
+        <LegendItemLabel
+          isMuted={isMuted}
+          onClick={onSelectSeries && handleItemClick}
+          onMouseEnter={onHoverChange && handleItemMouseEnter}
+          onMouseLeave={onHoverChange && handleItemMouseLeave}
+        >
+          <LegendItemDot color={color} />
+          {!isNarrow && (
+            <LegendItemTitle>
+              <Ellipsified>{label}</Ellipsified>
+            </LegendItemTitle>
+          )}
+        </LegendItemLabel>
+      </Tooltip>
+      {onRemoveSeries && <LegendItemRemoveIcon onClick={handleRemoveClick} />}
+    </LegendItemRoot>
+  );
+};
+
+LegendItem.propTypes = propTypes;
+
+export default memo(LegendItem);
