5c2237b3f3e794628ae68990dd47c14cf6496fd0
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-18 19:56:18 +0300

    Add user id

--- frontend/src/metabase/account/notifications/containers/NotificationListApp.jsx
+++ frontend/src/metabase/account/notifications/containers/NotificationListApp.jsx
@@ -1,11 +1,11 @@
 import { connect } from "react-redux";
+import { push } from "react-router-redux";
 import _ from "underscore";
 import Alerts from "metabase/entities/alerts";
 import Pulses from "metabase/entities/pulses";
 import { getUser, getUserId } from "metabase/selectors/user";
 import { getNotifications } from "../selectors";
 import NotificationList from "../components/NotificationList";
-import { push } from "react-router-redux";
 
 const mapStateToProps = (state, props) => ({
   user: getUser(state),
