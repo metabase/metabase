da24ff717a5495d897b0ceef8af112e791008d54
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-18 17:03:47 +0300

    Add HelpNotificationForm unit tests

---
+++ frontend/src/metabase/account/notifications/components/HelpNotificationForm.unit.spec.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import HelpNotificationForm from "./HelpNotificationForm";
+
+describe("HelpNotificationForm", () => {
+  const onClose = jest.fn();
+
+  it("should render with admin email", () => {
+    render(
+      <HelpNotificationForm
+        adminEmail={"admin@example.com"}
+        onClose={onClose}
+      />,
+    );
+
+    const link = screen.getByRole("link");
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveProperty("href", "mailto:admin@example.com");
+  });
+
+  it("should render without admin email", () => {
+    render(<HelpNotificationForm onClose={onClose} />);
+
+    const message = screen.getByText("administrator", { exact: false });
+    expect(message).toBeInTheDocument();
+  });
+
+  it("should close on button click", () => {
+    render(<HelpNotificationForm onClose={onClose} />);
+
+    screen.getByText("Got it").click();
+    expect(onClose).toHaveBeenCalled();
+  });
+});
