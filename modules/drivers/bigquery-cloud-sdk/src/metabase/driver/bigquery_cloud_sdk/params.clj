f4b03a7aeeae24be4426d849fcb207da724ea971
Jeff Evans <jeff.evans@metabase.com>
2021-08-19 16:41:58 -0500

    PR feedback
    Remove dead code in params.clj and switch to threaded forms
    
    Remove spurious project.clj that found its way into the google driver

--- modules/drivers/bigquery-cloud-sdk/src/metabase/driver/bigquery_cloud_sdk/params.clj
+++ modules/drivers/bigquery-cloud-sdk/src/metabase/driver/bigquery_cloud_sdk/params.clj
@@ -4,19 +4,6 @@
             [metabase.util.date-2 :as u.date])
   (:import [com.google.cloud.bigquery QueryJobConfiguration$Builder QueryParameterValue StandardSQLTypeName]))
 
-#_(defn- param-type-old ^QueryParameterType [^String type-name]
-    (doto (QueryParameterType.)
-      (.setType type-name)))
-
-#_(defn- param-value-old ^com.google.api.services.bigquery.model.QueryParameterValue [v]
-    (doto (com.google.api.services.bigquery.model.QueryParameterValue.)
-      (.setValue (str v))))
-
-#_(defn- param-old [type-name v]
-    (doto (QueryParameter.)
-      (.setParameterType (param-type-old type-name))
-      (.setParameterValue (param-value-old v))))
-
 (defn- param ^QueryParameterValue [type-name v]
   (.build (doto (QueryParameterValue/newBuilder)
             (.setType (StandardSQLTypeName/valueOf type-name))
@@ -55,22 +42,15 @@
 (defmethod ->QueryParameterValue java.time.LocalDate      [t] (param "DATE" (u.date/format t)))
 (defmethod ->QueryParameterValue java.time.LocalDateTime  [t] (param "DATETIME" (u.date/format t)))
 (defmethod ->QueryParameterValue java.time.LocalTime      [t] (param "TIME" (u.date/format t)))
-(defmethod ->QueryParameterValue java.time.OffsetTime     [t] (param "TIME" (u.date/format
-                                                                             (t/local-time
-                                                                              (t/with-offset-same-instant
-                                                                                t
-                                                                                (t/zone-offset 0)))))) ;;TODO thread
+(defmethod ->QueryParameterValue java.time.OffsetTime     [t] (param "TIME" (->> (t/zone-offset 0)
+                                                                                 (t/with-offset-same-instant t)
+                                                                                 t/local-time
+                                                                                 u.date/format)))
 (defmethod ->QueryParameterValue java.time.OffsetDateTime [t] (param "TIMESTAMP" (u.date/format t)))
-(defmethod ->QueryParameterValue java.time.ZonedDateTime  [t] (param "TIMESTAMP" (u.date/format
-                                                                                  (t/offset-date-time
-                                                                                   (t/with-zone-same-instant
-                                                                                     t
-                                                                                     (t/zone-id "UTC")))))) ;;TODO thread
-
-#_(defn- query-parameter-old ^QueryParameter [value]
-    (let [param (->QueryParameter value)]
-      (log/tracef "Set parameter ^%s %s -> %s" (some-> value class .getCanonicalName) (pr-str value) (pr-str param))
-      param))
+(defmethod ->QueryParameterValue java.time.ZonedDateTime  [t] (param "TIMESTAMP" (->> (t/zone-id "UTC")
+                                                                                      (t/with-zone-same-instant t)
+                                                                                      t/offset-date-time
+                                                                                      u.date/format)))
 
 (defn- query-parameter ^QueryParameterValue [value]
   (let [param (->QueryParameterValue value)]
@@ -84,13 +64,3 @@
   (doseq [p parameters]
     (.addPositionalParameter query (query-parameter p)))
   query)
-
-#_(defn set-parameters-old!
-    "Set the `parameters` (i.e., values for `?` positional placeholders in the SQL) for a `query` request. Equivalent to
-  JDBC `.setObject()` and the like."
-    ^QueryRequest [^QueryRequest query parameters]
-    (if (seq parameters)
-      (doto query
-        (.setParameterMode "POSITIONAL")
-        (.setQueryParameters (apply list (map query-parameter parameters))))
-      query))
