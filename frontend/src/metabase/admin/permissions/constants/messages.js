697649540e14ecaf6434a29b15051732761cc9f0
alxnddr <alxnddr@gmail.com>
2021-08-19 10:29:32 +0300

    fix permissions disabled tooltips copy

--- frontend/src/metabase/admin/permissions/constants/messages.js
+++ frontend/src/metabase/admin/permissions/constants/messages.js
@@ -1,4 +1,4 @@
 import { t } from "ttag";
 
-export const UNABLE_TO_CHANGE_ADMIN_PERMISSIONS = t`Cannot change the data access for Administrators`;
-export const DATA_ACCESS_IS_REQUIRED = t`Data access must be allowed`;
+export const UNABLE_TO_CHANGE_ADMIN_PERMISSIONS = t`Administrators always have the highest level of access to everything in Metabase.`;
+export const DATA_ACCESS_IS_REQUIRED = t`Native query editor access requires full data access.`;
