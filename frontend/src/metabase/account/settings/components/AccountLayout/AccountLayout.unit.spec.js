c21c7696c400dcf3b991cc0baf8064c598a779ef
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-08-19 15:57:59 +0300

    Add plugin tests

--- frontend/src/metabase/account/settings/components/AccountLayout/AccountLayout.unit.spec.js
+++ frontend/src/metabase/account/settings/components/AccountLayout/AccountLayout.unit.spec.js
@@ -2,16 +2,18 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import AccountLayout from "./AccountLayout";
 
-const REGULAR_USER = {
+const getUser = () => ({
   id: 1,
   first_name: "John",
   last_name: "Doe",
   email: "john@metabase.test",
-};
+});
 
 describe("AccountLayout", () => {
   it("should render header and content", () => {
-    render(<AccountLayout user={REGULAR_USER}>Content</AccountLayout>);
+    const user = getUser();
+
+    render(<AccountLayout user={user}>Content</AccountLayout>);
 
     screen.getByText("Profile");
     screen.getByText("Content");
