4300e11257bcf38ce8b5b46bf7fcedb01cc410a4
Anton Kulyk <kuliks.anton@gmail.com>
2021-08-20 22:47:00 +0300

    Refactor Join's unit tests

--- frontend/test/metabase-lib/lib/queries/structured/Join.unit.spec.js
+++ frontend/test/metabase-lib/lib/queries/structured/Join.unit.spec.js
@@ -1,89 +1,105 @@
 import { ORDERS, PRODUCTS, REVIEWS } from "__support__/sample_dataset_fixture";
 import Join from "metabase-lib/lib/queries/structured/Join";
 
+function getOrdersJoinQuery({
+  alias = "Products",
+  condition,
+  sourceTable = PRODUCTS.id,
+} = {}) {
+  return ORDERS.query().join({
+    alias,
+    condition,
+    "source-table": sourceTable,
+  });
+}
+
+function getJoin({ query = getOrdersJoinQuery() } = {}) {
+  return query.joins()[0];
+}
+
+const ORDERS_PRODUCT_ID_FIELD_REF = ["field", ORDERS.PRODUCT_ID.id, null];
+
+const PRODUCTS_ID_FIELD_REF = [
+  "field",
+  PRODUCTS.ID.id,
+  { "join-alias": "Products" },
+];
+
+const ORDERS_PRODUCT_JOIN_CONDITION = [
+  "=",
+  ORDERS_PRODUCT_ID_FIELD_REF,
+  PRODUCTS_ID_FIELD_REF,
+];
+
+const REVIEWS_PRODUCT_ID_FIELD_REF = [
+  "field",
+  REVIEWS.PRODUCT_ID.id,
+  { "join-alias": "Reviews - Products" },
+];
+
+const ORDERS_REVIEWS_JOIN_CONDITION = [
+  "=",
+  ORDERS_PRODUCT_ID_FIELD_REF,
+  REVIEWS_PRODUCT_ID_FIELD_REF,
+];
+
 describe("Join", () => {
   describe("setJoinSourceTableId", () => {
     it("should pick an alias based on the source table name by default", () => {
-      const q = ORDERS.query();
-      const j = new Join({}, 0, q).setJoinSourceTableId(PRODUCTS.id);
-      expect(j.alias).toEqual("Products");
+      const query = ORDERS.query();
+      const join = new Join({}, 0, query).setJoinSourceTableId(PRODUCTS.id);
+      expect(join.alias).toEqual("Products");
     });
 
     it("should deduplicate aliases", () => {
-      const q = ORDERS.query().join({
-        alias: "Products",
-        "source-table": PRODUCTS.id,
-      });
-      const j = new Join({}, 1, q).setJoinSourceTableId(PRODUCTS.id);
-      expect(j.alias).toEqual("Products_2");
+      const join = new Join({}, 1, getOrdersJoinQuery()).setJoinSourceTableId(
+        PRODUCTS.id,
+      );
+      expect(join.alias).toEqual("Products_2");
     });
   });
 
   describe("setDefaultCondition", () => {
     it("should set default condition to be fk relationship", () => {
-      const q = ORDERS.query().join({
-        alias: "x",
+      let join = getJoin();
+
+      join = join.setDefaultCondition();
+
+      expect(join).toEqual({
+        alias: "Products",
+        condition: ORDERS_PRODUCT_JOIN_CONDITION,
         "source-table": PRODUCTS.id,
-      });
-      const j = q.joins()[0].setDefaultCondition();
-      expect(j).toEqual({
-        alias: "x",
-        condition: [
-          "=",
-          ["field", 3, null],
-          ["field", 24, { "join-alias": "x" }],
-        ],
-        "source-table": 3,
       });
     });
   });
 
   describe("setDefaultAlias", () => {
     it("should set default alias to be table + field name and update join condition", () => {
-      const q = ORDERS.query().join({
-        alias: "x",
-        condition: [
-          "=",
-          ["field", ORDERS.PRODUCT_ID.id, null],
-          ["field", REVIEWS.PRODUCT_ID.id, { "join-alias": "x" }],
-        ],
-        "source-table": REVIEWS.id,
+      let join = getJoin({
+        query: getOrdersJoinQuery({
+          alias: "x",
+          condition: ORDERS_REVIEWS_JOIN_CONDITION,
+          sourceTable: REVIEWS.id,
+        }),
       });
-      const j = q
-        .joins()[0]
-        .setDefaultCondition()
-        .setDefaultAlias();
-      expect(j).toEqual({
+
+      join = join.setDefaultCondition().setDefaultAlias();
+
+      expect(join).toEqual({
         alias: "Reviews - Product",
-        condition: [
-          "=",
-          ["field", ORDERS.PRODUCT_ID.id, null],
-          [
-            "field",
-            REVIEWS.PRODUCT_ID.id,
-            { "join-alias": "Reviews - Product" },
-          ],
-        ],
+        condition: ORDERS_REVIEWS_JOIN_CONDITION,
         "source-table": REVIEWS.id,
       });
     });
 
     it("should set default alias to be table name only if it is similar to field name", () => {
-      const q = ORDERS.query().join({
-        alias: "x",
-        "source-table": PRODUCTS.id,
-      });
-      const j = q
-        .joins()[0]
-        .setDefaultCondition()
-        .setDefaultAlias();
-      expect(j).toEqual({
+      let join = getJoin({ query: getOrdersJoinQuery({ alias: "x" }) });
+
+      join = join.setDefaultCondition().setDefaultAlias();
+
+      expect(join).toEqual({
         alias: "Products",
-        condition: [
-          "=",
-          ["field", ORDERS.PRODUCT_ID.id, null],
-          ["field", PRODUCTS.ID.id, { "join-alias": "Products" }],
-        ],
+        condition: ORDERS_PRODUCT_JOIN_CONDITION,
         "source-table": PRODUCTS.id,
       });
     });
