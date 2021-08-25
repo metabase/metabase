50b28113355059dfafe19921ca79ece6ef0bb450
Cam Saul <github@camsaul.com>
2021-08-25 16:53:02 -0700

    Test init code: read driver parents from driver deps.edn

--- test/metabase/test/initialize/plugins.clj
+++ test/metabase/test/initialize/plugins.clj
@@ -1,6 +1,6 @@
 (ns metabase.test.initialize.plugins
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
+            [clojure.tools.reader.edn :as edn]
             [metabase.plugins :as plugins]
             [metabase.plugins.initialize :as plugins.init]
             [metabase.test.data.env.impl :as tx.env.impl]
@@ -26,10 +26,14 @@
                                   nm))
                        (yaml/parse-string (slurp manifest))))))))
 
-(defn- driver-parents [driver]
-  (let [parents-file (io/file (format "modules/drivers/%s/parents" (name driver)))]
-    (when (.exists parents-file)
-      (str/split-lines (slurp parents-file)))))
+(defn- driver-parents
+  "Return the set of parents for `driver`. Based on the value of `:metabase.build-driver/parents` in its `deps.edn`
+  file."
+  [driver]
+  (let [driver-deps-edn-file (io/file (format "modules/drivers/%s/deps.edn" (name driver)))]
+    (when (.exists driver-deps-edn-file)
+      (let [edn (edn/read-string (slurp driver-deps-edn-file))]
+        (:metabase.build-driver/parents edn)))))
 
 (defn- load-plugin-manifests!
   "When running tests driver plugins aren't loaded the normal way -- instead, to keep things sane, we simply merge their
