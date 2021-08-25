79ea2d32d3e78dafd2a6aa381540e3d613a72ec4
Anton Kulyk <kuliks.anton@gmail.com>
2021-08-25 15:34:20 +0300

    Update notebook editor's join step UI (#17523)
    * Move JoinStep's NotebookCell down to JoinClause
    
    * Break JoinClause into two NotebookCells
    
    * Update labels
    
    * Use `space` helper function
    
    * Simplify conditional statement
    
    * Fix not translated string

--- frontend/src/metabase/query_builder/components/notebook/steps/JoinStep.styled.jsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/JoinStep.styled.jsx
@@ -3,10 +3,15 @@ import { color } from "metabase/lib/colors";
 import { space } from "metabase/styled-components/theme";
 import Icon from "metabase/components/Icon";
 
+export const JoinStepRoot = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 export const JoinClausesContainer = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1 0 auto;
+  flex: 1;
 `;
 
 export const JoinClauseContainer = styled.div`
@@ -16,7 +21,7 @@ export const JoinClauseContainer = styled.div`
 export const JoinClauseRoot = styled.div`
   display: flex;
   align-items: center;
-  flex: 1 1 auto;
+  margin-bottom: ${props => (props.isLast ? 0 : "2px")};
 `;
 
 export const JoinStrategyIcon = styled(Icon).attrs({ size: 32 })`
@@ -56,25 +61,18 @@ export const JoinTypeIcon = styled(Icon).attrs({ size: 24 })`
   color: ${props => (props.isSelected ? color("text-white") : color("brand"))};
 `;
 
-export const JoinedTableControlRoot = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-export const JoinWhereConditionLabel = styled.span.attrs({ children: "where" })`
-  color: ${color("text-medium")};
+export const JoinWhereConditionLabel = styled.span.attrs({ children: "on" })`
+  color: ${color("brand")};
   font-weight: bold;
-  margin-top: 6px;
-  margin-left: 10px;
-  margin-right: 14px;
+  margin: 0 ${space(2)};
 `;
 
 export const JoinOnConditionLabel = styled.span.attrs({ children: "=" })`
+  font-size: 20;
   font-weight: bold;
   color: ${color("text-medium")};
   margin-left: 2px;
   margin-right: 6px;
-  margin-top: 6px;
 `;
 
 export const RemoveJoinIcon = styled(Icon).attrs({ name: "close", size: 18 })`
