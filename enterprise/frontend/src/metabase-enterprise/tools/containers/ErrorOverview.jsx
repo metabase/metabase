86eadbb2b40fa88655846b7dfd6d16cc254e988f
Howon Lee <hlee.howon@gmail.com>
2021-08-25 23:54:12 -0700

    overview route works

---
+++ enterprise/frontend/src/metabase-enterprise/tools/containers/ErrorOverview.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { t } from "ttag";
+
+export default class ErrorOverview extends Component {
+  render() {
+    return (
+      <div>blah blah blah</div>
+    );
+  }
+}
--- enterprise/frontend/src/metabase-enterprise/tools/routes.jsx
+++ enterprise/frontend/src/metabase-enterprise/tools/routes.jsx
@@ -4,9 +4,12 @@ import { Route } from "metabase/hoc/Title";
 import { IndexRedirect } from "react-router";
 import { t } from "ttag";
 import ToolsApp from "./containers/ToolsApp";
+import ErrorOverview from "./containers/ErrorOverview";
 
 const getRoutes = (store: any) => (
   <Route path="tools" title={t`Tools`} component={ToolsApp}>
+    <IndexRedirect to="errors" />
+    <Route path="errors" title={t`Erroring Questions`} component={ErrorOverview} />
   </Route>
 );
 
