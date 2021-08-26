d55f753612c4d055a01a5c7d806dd1f8f1a8c6ac
Gustavo Saiani <gustavo@poe.ma>
2021-08-26 15:56:28 -0300

    Fully cover component with unit tests

--- frontend/src/metabase/admin/databases/components/DatabaseEditApp/Sidebar/Sidebar.unit.spec.js
+++ frontend/src/metabase/admin/databases/components/DatabaseEditApp/Sidebar/Sidebar.unit.spec.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Sidebar from "./Sidebar";
 
-it("sanity check", () => {
-  render(<Sidebar database={{}} />);
-
-  screen.getByText("Actions");
-  screen.getByText("Discard saved field values");
-  screen.getByText("Remove this database");
-});
-
-it("triggers syncing database schema", () => {
+it("syncs database schema", () => {
   const databaseId = 1;
   const database = { id: databaseId };
   const syncDatabaseSchema = jest.fn();
@@ -25,4 +18,82 @@ it("triggers syncing database schema", () => {
   fireEvent.click(syncButton);
 
   expect(syncDatabaseSchema).toHaveBeenCalledWith(databaseId);
+});
+
+it("rescans database field values", () => {
+  const databaseId = 1;
+  const database = { id: databaseId };
+  const rescanDatabaseFields = jest.fn();
+
+  render(
+    <Sidebar database={database} rescanDatabaseFields={rescanDatabaseFields} />,
+  );
+
+  const rescanButton = screen.getByText("Re-scan field values now");
+
+  fireEvent.click(rescanButton);
+
+  expect(rescanDatabaseFields).toHaveBeenCalledWith(databaseId);
+});
+
+it("discards saved field values", () => {
+  const databaseId = 1;
+  const database = { id: databaseId };
+  const discardSavedFieldValues = jest.fn();
+
+  render(
+    <Sidebar
+      database={database}
+      discardSavedFieldValues={discardSavedFieldValues}
+    />,
+  );
+
+  const discardButton = screen.getByText("Discard saved field values");
+
+  fireEvent.click(discardButton);
+
+  expect(screen.getAllByText("Discard saved field values").length).toBe(2);
+
+  const cancelButton = screen.getByText("Cancel");
+
+  fireEvent.click(cancelButton);
+
+  fireEvent.click(discardButton);
+
+  const yesButton = screen.getByText("Yes");
+
+  fireEvent.click(yesButton);
+
+  expect(discardSavedFieldValues).toHaveBeenCalledWith(databaseId);
+});
+
+it("removes database", () => {
+  const databaseId = 1;
+  const name = "DB Name";
+  const database = { id: databaseId, name };
+  const deleteDatabase = jest.fn();
+
+  render(<Sidebar database={database} deleteDatabase={deleteDatabase} />);
+
+  const removeDBButton = screen.getByText("Remove this database");
+
+  fireEvent.click(removeDBButton);
+
+  screen.getByText(`Delete the ${name} database?`);
+
+  const cancelButton = screen.getByText("Cancel");
+
+  fireEvent.click(cancelButton);
+
+  fireEvent.click(removeDBButton);
+
+  const input = screen.getByRole("textbox");
+
+  userEvent.type(input, name);
+
+  const deleteButton = screen.getByText("Delete");
+
+  fireEvent.click(deleteButton);
+
+  expect(deleteDatabase).toHaveBeenCalled();
 });
