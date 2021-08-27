21a3b7bd8de7c495f7d143f4b84e99e77539a3c8
dan sutton <dan@dpsutton.com>
2021-08-27 17:37:12 -0500

    Ensure that data_readers functions are included in tests
    `clj -X:dev:test :only metabase.pulse.render.js-svg-test` uses the
    `#t` data reader, which passes the value to
    `metabase.util.date-2/parse` but this function hasn't been evaluated
    yet and it errors
    
    ```shell
    % clj -X:dev:test :only metabase.pulse.render.js-svg-test
    Warning: environ value /Users/dan/.sdkman/candidates/java/current for key :java-home has been overwritten with /Users/dan/.sdkman/candidates/java/11.0.11.hs-adpt
    Running tests in metabase.pulse.render.js-svg-test
    Syntax error reading source at (metabase/pulse/render/js_svg_test.clj:71:27).
    Attempting to call unbound fn: #'metabase.util.date-2/parse
    ```

--- test/metabase/test_runner/effects.clj
+++ test/metabase/test_runner/effects.clj
@@ -9,7 +9,14 @@
   This would not have had the random namespace that requires these helpers and the run fails.
   "
   (:require [clojure.test :as t]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [metabase.util.date-2 :as date-2]
+            [metabase.util.i18n.impl :as i18n.impl]))
+
+(comment
+  ;; these are necessary so data_readers.clj functions can function
+  date-2/keep-me
+  i18n.impl/keep-me)
 
 (defmethod t/assert-expr 're= [msg [_ pattern actual]]
   `(let [pattern#  ~pattern
