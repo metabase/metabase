02297f478d633956e17f98ca0ff24897232355ba
Anton Kulyk <kuliks.anton@gmail.com>
2021-09-01 11:31:36 +0300

    Enable join clean unit tests back

--- frontend/test/metabase-lib/lib/queries/StructuredQuery-clean.unit.spec.js
+++ frontend/test/metabase-lib/lib/queries/StructuredQuery-clean.unit.spec.js
@@ -43,14 +43,14 @@ describe("StructuredQuery", () => {
         expect(q.clean().query()).toEqual({ "source-table": 12345 });
       });
 
-      xit("should remove join referencing invalid source field", () => {
+      it("should remove join referencing invalid source field", () => {
         const q = ORDERS.query().join(
           getJoin({ condition: ["=", null, PRODUCT_ID_FIELD_REF] }),
         );
         expect(q.clean().query()).toEqual({ "source-table": ORDERS.id });
       });
 
-      xit("should remove join referencing invalid join field", () => {
+      it("should remove join referencing invalid join field", () => {
         const q = ORDERS.query().join(
           getJoin({ condition: ["=", ORDERS_PRODUCT_ID_FIELD_REF, null] }),
         );
