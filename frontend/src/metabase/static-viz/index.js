0da71ff011fc8369a5bb6f0aaa3b0e8b76e3f38d
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-02 00:55:54 +0300

    Add formatting funcs

--- frontend/src/metabase/static-viz/index.js
+++ frontend/src/metabase/static-viz/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOMServer from "react-dom/server";
 import StaticChart from "./containers/StaticChart";
 
-export function RenderChart(type, data) {
+export function RenderChart(type, options) {
   return ReactDOMServer.renderToStaticMarkup(
-    <StaticChart type={type} data={data} />,
+    <StaticChart type={type} options={options} />,
   );
 }
