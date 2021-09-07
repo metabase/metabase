6f15da154b05ee7464339b14a57fc5615b061cbc
Ariya Hidayat <ariya@metabase.com>
2021-09-07 11:29:25 -0700

    Split into commands (currently returning JSON)

--- frontend/parse-deps.js
+++ frontend/parse-deps.js
@@ -17,71 +17,115 @@ const ALIAS = {
   "metabase-types": "frontend/src/metabase-types",
 };
 
-const files = glob.sync(PATTERN);
+function files() {
+  return glob.sync(PATTERN);
+}
 
-const deps = files.map(fileName => {
-  const contents = fs.readFileSync(fileName, "utf-8");
-  const options = {
-    allowImportExportEverywhere: true,
-    allowReturnOutsideFunction: true,
-    decoratorsBeforeExport: true,
-    sourceType: "unambiguous",
-    plugins: ["jsx", "flow", "decorators-legacy", "exportDefaultFrom"],
-  };
-  const importList = [];
-  try {
-    const ast = parser.parse(contents, options);
-    traverse(ast, {
-      enter(path) {
-        if (path.node.type === "ImportDeclaration") {
-          importList.push(path.node.source.value);
-        }
-        if (path.node.type === "CallExpression") {
-          const callee = path.node.callee;
-          const args = path.node.arguments;
-          if (callee.type === "Identifier" && callee.name === "require") {
-            if (args.length === 1 && args[0].type === "StringLiteral") {
-              importList.push(args[0].value);
+function dependencies() {
+  const deps = files().map(fileName => {
+    const contents = fs.readFileSync(fileName, "utf-8");
+    const options = {
+      allowImportExportEverywhere: true,
+      allowReturnOutsideFunction: true,
+      decoratorsBeforeExport: true,
+      sourceType: "unambiguous",
+      plugins: ["jsx", "flow", "decorators-legacy", "exportDefaultFrom"],
+    };
+    const importList = [];
+    try {
+      const ast = parser.parse(contents, options);
+      traverse(ast, {
+        enter(path) {
+          if (path.node.type === "ImportDeclaration") {
+            importList.push(path.node.source.value);
+          }
+          if (path.node.type === "CallExpression") {
+            const callee = path.node.callee;
+            const args = path.node.arguments;
+            if (callee.type === "Identifier" && callee.name === "require") {
+              if (args.length === 1 && args[0].type === "StringLiteral") {
+                importList.push(args[0].value);
+              }
             }
           }
-        }
-      },
-    });
-  } catch (e) {
-    console.error(fileName, e.toString());
-    process.exit(-1);
-  }
-  const base = path.dirname(fileName) + path.sep;
-  const absoluteImportList = importList
-    .map(name => {
-      const absName = name[0] === "." ? path.normalize(base + name) : name;
-      const parts = absName.split(path.sep);
-      const realPath = ALIAS[parts[0]];
-      parts[0] = realPath ? realPath : parts[0];
-      const realName = parts.join(path.sep);
-      return realName;
-    })
-    .filter(name => name.indexOf("frontend") === 0);
-
-  const source = path.format({
-    ...path.parse(fileName),
-    ext: null,
-    base: null,
-  });
-  const dependencies = absoluteImportList.sort();
-
-  return { source, dependencies };
-});
-
-let dependents = {};
-deps.forEach(dep => {
-  const { source, dependencies } = dep;
-  dependencies.forEach(d => {
-    if (!dependents[d]) {
-      dependents[d] = [];
+        },
+      });
+    } catch (e) {
+      console.error(fileName, e.toString());
+      process.exit(-1);
+      n;
     }
-    dependents[d].push(source);
-  });
-});
+    const base = path.dirname(fileName) + path.sep;
+    const absoluteImportList = importList
+      .map(name => {
+        const absName = name[0] === "." ? path.normalize(base + name) : name;
+        const parts = absName.split(path.sep);
+        const realPath = ALIAS[parts[0]];
+        parts[0] = realPath ? realPath : parts[0];
+        const realName = parts.join(path.sep);
+        return realName;
+      })
+      .filter(name => name.indexOf("frontend") === 0);
 
-console.log(JSON.stringify(dependents, null, 2));
+    const source = path.format({
+      ...path.parse(fileName),
+      ext: null,
+      base: null,
+    });
+    const dependencies = absoluteImportList.sort();
+
+    return { source, dependencies };
+  });
+  return deps;
+}
+
+function dependents() {
+  let dependents = {};
+  dependencies().forEach(dep => {
+    const { source, dependencies } = dep;
+    dependencies.forEach(d => {
+      if (!dependents[d]) {
+        dependents[d] = [];
+      }
+      dependents[d].push(source);
+    });
+  });
+  return dependents;
+}
+
+const USAGE = `
+parse-deps cmd
+
+cmd must be one of:
+
+        files     Display list of source files
+ dependencies     Show the dependencies of each source file
+   dependents     Show the dependents of each source file
+`;
+
+function main(args) {
+  const cmd = args[0];
+  if (cmd) {
+    switch (cmd.toLowerCase()) {
+      case "files":
+        console.log(files().join("\n"));
+        break;
+      case "dependencies":
+        console.log(JSON.stringify(dependencies(), null, 2));
+        break;
+      case "dependents":
+        console.log(JSON.stringify(dependents(), null, 2));
+        break;
+      default:
+        console.log(USAGE);
+        break;
+    }
+  } else {
+    console.log(USAGE);
+  }
+}
+
+let args = process.argv;
+args.shift();
+args.shift();
+main(args);
