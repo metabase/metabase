7bc96b81b8d2703b85bc0e2d0abb031d6a1bbb50
Anton Kulyk <kuliks.anton@gmail.com>
2021-09-07 22:06:42 +0300

    Use simple for for EditQuestionInfoModal

--- frontend/src/metabase/query_builder/components/view/EditQuestionInfoModal.jsx
+++ frontend/src/metabase/query_builder/components/view/EditQuestionInfoModal.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useCallback } from "react";
 import { t } from "ttag";
 import _ from "underscore";
+import { connect } from "react-redux";
+
+import Form from "metabase/containers/Form";
+import ModalContent from "metabase/components/ModalContent";
+import CollapseSection from "metabase/components/CollapseSection";
 
 import Questions from "metabase/entities/questions";
-import CollapseSection from "metabase/components/CollapseSection";
 
 const COLLAPSED_FIELDS = ["cache_ttl"];
 
@@ -15,48 +19,63 @@ function getInitialCacheTTL(question) {
   return question.card().cache_ttl || question.database().cache_ttl || 0;
 }
 
-const EditQuestionInfoModal = ({ question, onClose, onSave }) => (
-  <Questions.ModalForm
-    title={t`Edit question`}
-    form={Questions.forms.edit}
-    question={{
-      ...question.card(),
-      cache_ttl: getInitialCacheTTL(question),
-    }}
-    onClose={onClose}
-    onSaved={async card => {
+const mapDispatchToProps = {
+  updateQuestion: Questions.actions.update,
+};
+
+function EditQuestionInfoModal({ question, updateQuestion, onClose, onSave }) {
+  const onSubmit = useCallback(
+    async card => {
+      await updateQuestion({ id: card.id }, card);
       await onSave({ ...question.card(), ...card });
       onClose();
-    }}
-  >
-    {({ Form, FormField, FormFooter, formFields, onClose }) => {
-      const [visibleFields, collapsedFields] = _.partition(
-        formFields,
-        field => !COLLAPSED_FIELDS.includes(field.name),
-      );
-      return (
-        <Form>
-          {visibleFields.map(field => (
-            <FormField key={field.name} name={field.name} />
-          ))}
-          {collapsedFields.length > 0 && (
-            <CollapseSection
-              header={t`More options`}
-              iconVariant="up-down"
-              iconPosition="right"
-              headerClass="text-bold text-medium"
-              bodyClass="pt1"
-            >
-              {collapsedFields.map(field => (
+    },
+    [question, updateQuestion, onSave, onClose],
+  );
+
+  return (
+    <ModalContent title={t`Edit question`} onClose={onClose}>
+      <Form
+        initialValues={{
+          ...question.card(),
+          cache_ttl: getInitialCacheTTL(question),
+        }}
+        form={Questions.forms.edit}
+        onSubmit={onSubmit}
+      >
+        {({ Form, FormField, FormFooter, formFields }) => {
+          const [visibleFields, collapsedFields] = _.partition(
+            formFields,
+            field => !COLLAPSED_FIELDS.includes(field.name),
+          );
+          return (
+            <Form>
+              {visibleFields.map(field => (
                 <FormField key={field.name} name={field.name} />
               ))}
-            </CollapseSection>
-          )}
-          <FormFooter submitTitle={t`Save`} onCancel={onClose} />
-        </Form>
-      );
-    }}
-  </Questions.ModalForm>
-);
+              {collapsedFields.length > 0 && (
+                <CollapseSection
+                  header={t`More options`}
+                  iconVariant="up-down"
+                  iconPosition="right"
+                  headerClass="text-bold text-medium"
+                  bodyClass="pt1"
+                >
+                  {collapsedFields.map(field => (
+                    <FormField key={field.name} name={field.name} />
+                  ))}
+                </CollapseSection>
+              )}
+              <FormFooter submitTitle={t`Save`} onCancel={onClose} />
+            </Form>
+          );
+        }}
+      </Form>
+    </ModalContent>
+  );
+}
 
-export default EditQuestionInfoModal;
+export default connect(
+  null,
+  mapDispatchToProps,
+)(EditQuestionInfoModal);
