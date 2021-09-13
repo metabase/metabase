0e634f6b361cdfd4a30909bdafdd11a6ef70d4fa
Anton Kulyk <kuliks.anton@gmail.com>
2021-09-13 15:08:28 +0300

    Fix getRevisionEventsForTimeline work with updated helper

--- frontend/src/metabase/lib/revisions/revisions.js
+++ frontend/src/metabase/lib/revisions/revisions.js
@@ -1,5 +1,5 @@
 import { t } from "ttag";
-import { isValidRevision, getRevisionMessage } from "./utils";
+import { isValidRevision, getChangedFields, getRevisionMessage } from "./utils";
 
 export const REVISION_EVENT_ICON = "pencil";
 
@@ -20,15 +20,27 @@ export function getRevisionEventsForTimeline(revisions = [], canWrite) {
       const isRevertable = canWrite && index !== 0;
       const username = getRevisionUsername(revision);
       const message = getRevisionMessage(revision);
-      if (!message) {
-        return;
-      }
-      const { title, description } = message;
+
+      // If > 1 item's fields are changed in a single revision,
+      // the changes are batched into a single string like:
+      // "added a description, moved cards around and archived this"
+      // Batched messages can be long, so if the revision's diff contains > 1 field,
+      // we want to show the changelog in a description and set a title to just "User edited this"
+      // If only one field is changed, we just show everything in the title
+      // like "John added a description"
+      const areMultipleFieldsChanged = getChangedFields(revision).length > 1;
+      const title = areMultipleFieldsChanged
+        ? username + " " + t`edited this`
+        : `${username} ${message}`;
+      const description = areMultipleFieldsChanged
+        ? capitalize(message)
+        : undefined;
+
       return {
         timestamp: getRevisionEpochTimestamp(revision),
         icon: REVISION_EVENT_ICON,
-        title: `${username} ${title}`,
-        description: description && capitalize(description),
+        title,
+        description,
         isRevertable,
         revision,
       };
