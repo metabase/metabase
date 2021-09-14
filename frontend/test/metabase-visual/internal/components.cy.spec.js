05e4c84397726310851f9b3b8312daf84d477587
Ariya Hidayat <ariya@metabase.com>
2021-09-14 10:44:39 -0700

    TEST: swap for easier debugging

--- frontend/test/metabase-visual/internal/components.cy.spec.js
+++ frontend/test/metabase-visual/internal/components.cy.spec.js
@@ -6,18 +6,18 @@ describe("visual tests > internal > components", () => {
     cy.signInAsNormalUser();
   });
 
-  it("ClampedText", () => {
-    cy.visit("/_internal/components/clampedtext");
-
-    cy.wait(2000).findByText("No 'See more' button when all text visible:");
-
-    cy.percySnapshot();
-  });
-
   it("UserAvatar", () => {
     cy.visit("/_internal");
 
     cy.findByText("UserAvatar").click();
+
+    cy.percySnapshot();
+  });
+
+  it("ClampedText", () => {
+    cy.visit("/_internal/components/clampedtext");
+
+    cy.wait(2000).findByText("No 'See more' button when all text visible:");
 
     cy.percySnapshot();
   });
