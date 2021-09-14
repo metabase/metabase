d79882ca4922907e40ffb0f497400169332e19b1
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-14 16:58:07 +0300

    Decouple width/height calc

--- frontend/src/metabase/static-viz/lib/text.unit.spec.js
+++ frontend/src/metabase/static-viz/lib/text.unit.spec.js
@@ -1,7 +1,21 @@
-import { measureText } from "./text";
+import { measureText, truncateText } from "./text";
 
 describe("measureText", () => {
   it("should measure text assuming 6px char width", () => {
     expect(measureText("abc")).toBe(18);
+  });
+});
+
+describe("truncateText", () => {
+  it("should not truncate text with ellipses if there is no overflow", () => {
+    expect(truncateText("John Doe", 48)).toBe("John Doe");
+  });
+
+  it("should truncate text with ellipses if there is overflow", () => {
+    expect(truncateText("John Doe", 47)).toBe("John D…");
+  });
+
+  it("should use ellipses in case there is no space for text at all", () => {
+    expect(truncateText("John Doe", 0)).toBe("…");
   });
 });
