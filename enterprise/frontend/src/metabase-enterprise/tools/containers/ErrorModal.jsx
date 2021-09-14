8514e965b9445fef57a6db3fc8a3a92569bf6f52
Howon Lee <hlee.howon@gmail.com>
2021-09-13 17:57:22 -0700

    add in the modal

--- enterprise/frontend/src/metabase-enterprise/tools/containers/ErrorModal.jsx
+++ enterprise/frontend/src/metabase-enterprise/tools/containers/ErrorModal.jsx
@@ -1,1 +1,20 @@
-some shit here
+import React, { useState } from "react";
+
+import { t } from "ttag";
+
+import Modal from "metabase/components/Modal";
+
+export default class ErrorModal extends React.Component {
+  render() {
+    const {
+      onClose,
+      onSaved,
+    } = this.props;
+
+    return (
+      <Modal onClose={onClose}>
+      <div>blah blah blah maybe this one will work lol</div>
+      </Modal>
+    );
+  g
+}
