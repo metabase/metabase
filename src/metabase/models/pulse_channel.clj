3a2658ce72c13f0c001ddbdffc39e295958183ea
Cam Saul <github@camsaul.com>
2021-09-15 10:26:00 -0700

    Test fix :wrench:

--- src/metabase/models/pulse_channel.clj
+++ src/metabase/models/pulse_channel.clj
@@ -133,20 +133,21 @@
   (classloader/require 'metabase.models.pulse)
   ((resolve 'metabase.models.pulse/will-delete-channel) pulse-channel))
 
-(def ^:private ^{:arglists '([pulse-channel])} validate-email-domains
+;; we want to load this at the top level so the Setting the namespace defines gets loaded
+(def ^:private ^{:arglists '([email-addresses])} validate-email-domains*
+  (or (u/ignore-exceptions
+        (classloader/require 'metabase-enterprise.advanced-config.models.pulse-channel)
+        (resolve 'metabase-enterprise.advanced-config.models.pulse-channel/validate-email-domains))
+      (constantly nil)))
+
+(defn- validate-email-domains
   "For channels that are being sent to raw email addresses: check that the domains in the emails are allowed by
   the [[metabase-enterprise.advanced-config.models.pulse-channel/subscription-allowed-domains]] Setting, if set. This
   will no-op if `subscription-allowed-domains` is unset or if we do not have a premium token with the
   `:advanced-config` feature."
-  (let [validate* (delay
-                    (u/ignore-exceptions
-                      (classloader/require 'metabase-enterprise.advanced-config.models.pulse-channel)
-                      (resolve 'metabase-enterprise.advanced-config.models.pulse-channel/validate-email-domains)))]
-    (fn [{{:keys [emails]} :details, :as pulse-channel}]
-      (u/prog1 pulse-channel
-        (when (seq emails)
-          (when-let [validate @validate*]
-            (validate emails)))))))
+  [{{:keys [emails]} :details, :as pulse-channel}]
+  (u/prog1 pulse-channel
+    (validate-email-domains* emails)))
 
 (u/strict-extend (class PulseChannel)
   models/IModel
