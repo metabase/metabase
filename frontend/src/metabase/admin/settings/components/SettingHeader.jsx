1fadeef5648f6b7b86994b959dcda606c2cb57c5
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-15 14:52:54 +0300

    Revert header changes

--- frontend/src/metabase/admin/settings/components/SettingHeader.jsx
+++ frontend/src/metabase/admin/settings/components/SettingHeader.jsx
@@ -1,36 +1,21 @@
-import React, { Fragment } from "react";
-import PropTypes from "prop-types";
+/* eslint-disable react/prop-types */
+import React from "react";
 
-const propTypes = {
-  setting: PropTypes.object,
-};
-
-const SettingHeader = ({ setting }) => {
-  const description = setting.description ?? "";
-
-  return (
-    <div>
-      <div className="text-medium text-bold text-uppercase">
-        {setting.display_name}
-      </div>
-      <div className="text-medium text-measure my1">
-        {setting.warningMessage && (
-          <Fragment>
-            <strong>{setting.warningMessage}</strong>{" "}
-          </Fragment>
-        )}
-        {description.split("<br>").map((text, index) => (
-          <Fragment key={index}>
-            {index > 0 && <br />}
-            <span>{text}</span>
-          </Fragment>
-        ))}
-        {setting.note && <div>{setting.note}</div>}
-      </div>
+const SettingHeader = ({ setting }) => (
+  <div>
+    <div className="text-medium text-bold text-uppercase">
+      {setting.display_name}
     </div>
-  );
-};
-
-SettingHeader.propTypes = propTypes;
+    <div className="text-medium text-measure my1">
+      {setting.warningMessage && (
+        <React.Fragment>
+          <strong>{setting.warningMessage}</strong>{" "}
+        </React.Fragment>
+      )}
+      {setting.description}
+      {setting.note && <div>{setting.note}</div>}
+    </div>
+  </div>
+);
 
 export default SettingHeader;
