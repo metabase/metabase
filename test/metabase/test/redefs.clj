f3c139c5a38e4bd0b58fd1168d99b6c500375582
Cam Saul <github@camsaul.com>
2021-09-14 20:07:23 -0700

    Improve the error handling for with-temp a little more.

--- test/metabase/test/redefs.clj
+++ test/metabase/test/redefs.clj
@@ -3,13 +3,12 @@
   needed when running)."
   (:require [metabase.plugins.classloader :as classloader]
             [metabase.test-runner.parallel :as test-runner.parallel]
+            [toucan.db :as db]
             [toucan.util.test :as tt]))
 
-;; wrap `do-with-temp` so it initializes the DB before doing the other stuff it usually does
+;; replace [[toucan.util.test/do-with-temp]] so it initializes the DB before doing the other stuff it usually does
 
-(defonce orig-do-with-temp tt/do-with-temp)
-
-(defn do-with-temp [& args]
+(defn do-with-temp [model attributes f]
   (classloader/require 'metabase.test.initialize)
   ((resolve 'metabase.test.initialize/initialize-if-needed!) :db)
   ;; so with-temp-defaults are loaded
@@ -19,19 +18,19 @@
   ;; TODO -- there's not really a reason that we can't use with-temp in parallel tests -- it depends on the test -- so
   ;; once we're a little more comfortable with the current parallel stuff we should remove this restriction.
   (test-runner.parallel/assert-test-is-not-parallel "with-temp")
-  ;; catch any Exceptions thrown by the original call and rethrow them with some extra context to make them a little
-  ;; easier to debug.
-  (try
-    (apply orig-do-with-temp args)
-    (catch Throwable e
-      ;; only wrap the Exception with additional context if it's not already wrapped. We don't need a bunch of nested
-      ;; `do-with-temp` calls that didn't fail wrapping the Exception from the one that did and adding a bunch of
-      ;; noise
-      (if (::with-temp-error (ex-data e))
-        (throw e)
-        (throw (ex-info (str "with-temp error: " (ex-message e))
-                        {:args args, ::with-temp-error true}
-                        e))))))
+  ;; catch any Exceptions thrown when creating the object and rethrow them with some extra context to make them a
+  ;; little easier to debug.
+  (let [temp-object (try
+                      (db/insert! model (merge (tt/with-temp-defaults model)
+                                               attributes))
+                      (catch Throwable e
+                        (throw (ex-info (str "with-temp error: " (ex-message e))
+                                        {:model (name model), :attributes attributes}
+                                        e))))]
+    (try
+      (f temp-object)
+      (finally
+        (db/delete! model :id (:id temp-object))))))
 
 (alter-var-root #'tt/do-with-temp (constantly do-with-temp))
 
