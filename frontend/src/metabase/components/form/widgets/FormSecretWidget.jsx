f89506293b968884ce4edc9bb8a93b9c11a751e4
Gustavo Saiani <gustavo@poe.ma>
2021-09-15 13:21:19 -0300

    Style select and text input

--- frontend/src/metabase/components/form/widgets/FormSecretWidget.jsx
+++ frontend/src/metabase/components/form/widgets/FormSecretWidget.jsx
@@ -1,7 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import styled from "styled-components";
+import { t } from "ttag";
+
+import Select, { Option } from "metabase/components/Select";
 
 import { formDomOnlyProps } from "metabase/lib/redux";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledSelect = styled(Select)`
+  width: 30%;
+`;
+
+const Input = styled.input`
+  width: calc(70% - 10px);
+`;
 
 const FormSecretWidget = ({
   type = "text",
@@ -9,16 +26,40 @@ const FormSecretWidget = ({
   field,
   readOnly,
   autoFocus,
-}) => (
-  <input
-    className="Form-input full"
-    type={type}
-    placeholder={placeholder}
-    aria-labelledby={`${field.name}-label`}
-    readOnly={readOnly}
-    autoFocus={autoFocus}
-    {...formDomOnlyProps(field)}
-  />
-);
+}) => {
+  // const handleSelectChange = () => {
+  //   console.log("🚀", "handleSelectChange");
+  // };
+
+  return (
+    <Container>
+      <StyledSelect
+        defaultValue={"local"}
+        placeholder={placeholder}
+        onChange={e => {
+          console.log("🚀", "heyheyheyhey");
+        }}
+        style={{ width: 100 }}
+      >
+        <Option key={`secret-select-option1`} value={"local"}>
+          {t`Local file`}
+        </Option>
+        <Option key={`secret-select-option2`} value={"uploaded"}>
+          {t`Uploaded file`}
+        </Option>
+      </StyledSelect>
+
+      <Input
+        className="Form-input"
+        type={type}
+        placeholder={placeholder}
+        aria-labelledby={`${field.name}-label`}
+        readOnly={readOnly}
+        autoFocus={autoFocus}
+        {...formDomOnlyProps(field)}
+      />
+    </Container>
+  );
+};
 
 export default FormSecretWidget;
