051ac7214277dedb6498309a8854ef603aeef8f9
Howon Lee <hlee.howon@gmail.com>
2021-09-15 23:00:32 -0700

    i think runnable

--- frontend/test/metabase/query_builder/components/VisualizationError.unit.spec.js
+++ frontend/test/metabase/query_builder/components/VisualizationError.unit.spec.js
@@ -4,12 +4,15 @@ import {
 } from "metabase/query_builder/components/DataSelector";
 
 describe("VisualizationError", () => {
-  const remarkedQuery = "";
-  const remarkedRedshiftQuery = "";
-  const unremarkedQuery = "";
+  const remarkedQuery = "-- Metabase:: userID: 1 queryType: native queryHash: 9863b8284f269ce8763ad59b04cec26407a1dd74eebeb16cffdf1ef3e23b325a\nfwefwef";
+  const remarkedRedshiftQuery = "/* anything before the remarks in multiline is redshift */-- Metabase:: userID: 1 queryType: native queryHash: 9863b8284f269ce8763ad59b04cec26407a1dd74eebeb16cffdf1ef3e23b325a\nfwefwef";
+  const unremarkedQuery = "fwefwef";
 
-  const errorPgSql = "";
-  const errorLineNumbers = "";
+  const unadjustedError = `boopy boop boopy boop fake error message Position: 107;`;
+  const adjustedError = `boopy boop boopy boop fake error message Position: 107;`;
+  const redshiftAdjustedError = `boopy boop boopy boop fake error message Position: 107;`;
+
+  const errorLineNumbers = "boopy boop boopy boop fake error message Line: 2";
 
   const errorH2Unstripped = `
   Syntax error in SQL statement " FWEFWEF[*] "; expected "FROM, {"; SQL statement: -- Metabase:: userID: 1 queryType: native queryHash: 9863b8284f269ce8763ad59b04cec26407a1dd74eebeb16cffdf1ef3e23b325a\nfwefwef [42001-197]`;
@@ -17,20 +20,16 @@ describe("VisualizationError", () => {
   Syntax error in SQL statement " FWEFWEF[*] "; expected "FROM, {"; SQL statement: fwefwef [42001-197]`;
 
   it("error adjusted pg", () => {
-    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+    expect(adjustPositions(unadjustedError, remarkedQuery)).toEqual(adjustedError);
   });
 
   it("error adjusted redshift", () => {
-    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+    expect(adjustPositions(unadjustedError, remarkedRedshiftQuery)).toEqual(redshiftAdjustedError);
   });
 
-  it("error adjusted line numbers", () => {
-    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+  it("error adjusted line numbers should be a noop", () => {
+    expect(adjustPositions(errorLineNumbers, remarkedQuery)).toEqual(errorLineNumbers);
   });
-
-  it("should just not adjust if there's nothing to adjust (pg)", () => {});
-
-  it("should just not adjust if there's nothing to adjust (line numbers)", () => {});
 
   it("should strip remarks from query with stripRemarks", () => {
     expect(stripRemarks(errorH2Unstripped)).toEqual(errorH2Stripped);
