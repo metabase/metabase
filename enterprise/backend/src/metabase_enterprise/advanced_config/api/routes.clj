c682455a00ff1d9a26ec5492aa189aadf48a2b85
Cam Saul <github@camsaul.com>
2021-09-15 19:30:53 -0700

    Remove unused var

--- enterprise/backend/src/metabase_enterprise/advanced_config/api/routes.clj
+++ enterprise/backend/src/metabase_enterprise/advanced_config/api/routes.clj
@@ -3,11 +3,7 @@
   `/api/ee/advanced-config/`."
   (:require [compojure.core :as compojure]
             [metabase-enterprise.advanced-config.api.user :as user]
-            [metabase-enterprise.api.routes.common :as ee.api.common]
             [metabase.api.routes.common :refer [+auth]]))
-
-(defn- +require-advanced-config [handler]
-  (ee.api.common/+require-premium-feature :advanced-config handler))
 
 (compojure/defroutes ^{:doc "Ring routes for mt API endpoints."} routes
   (compojure/context "/user" [] (+auth user/routes)))
