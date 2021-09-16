195c548e3938e58733280cc053f8c28557c69ad3
dan sutton <dan@dpsutton.com>
2021-09-16 18:31:33 -0500

    Formatting
    return a format string so it can be used over multiple rows. The rules
    for formatting are quite annoying because there are defaults that are
    assumed rather than present. IE, if you set a column as currency it
    doesn't set the number style sometimes and defaults to USD without that
    being in the settings. In the future would be far preferable to always
    create a fully fleshed map of defaults rather than inferring them in
    random places.

--- src/metabase/pulse/render/common.clj
+++ src/metabase/pulse/render/common.clj
@@ -1,10 +1,14 @@
 (ns metabase.pulse.render.common
   (:require [clojure.pprint :refer [cl-format]]
             [hiccup.util :as hutil]
+            [metabase.public-settings :as public-settings]
+            [metabase.shared.models.visualization-settings :as mb.viz]
+            [metabase.shared.util.currency :as currency]
             [metabase.util.ui-logic :as ui-logic]
             [potemkin.types :as p.types]
             [schema.core :as s])
-  (:import java.net.URL))
+  (:import java.net.URL
+           (java.text DecimalFormat DecimalFormatSymbols)))
 
 ;; Fool Eastwood into thinking this namespace is used
 (comment hutil/keep-me)
@@ -22,11 +26,53 @@
   Object
   (toString [_] num-str))
 
+(defn number-formatter
+  ([col-viz]
+   (number-formatter col-viz (mb.viz/db->norm-column-settings-entries
+                              (:type/Number (public-settings/custom-formatting)))))
+  ([col-viz site-settings]
+   (let [{::mb.viz/keys [number-separators decimals scale number-style
+                         prefix suffix currency-style currency]} (merge site-settings col-viz)
+
+         currency?          (boolean (or (= number-style "currency")
+                                         (and (nil? number-style)
+                                              (or
+                                               currency-style
+                                               currency))))
+         [decimal grouping] (or number-separators ".,")
+         symbols            (doto (DecimalFormatSymbols.)
+                              (cond-> decimal (.setDecimalSeparator decimal))
+                              (cond-> grouping (.setGroupingSeparator grouping)))
+         base               (if (= number-style "scientific") "0" "#,###")
+         fmt-str            (cond-> (cond
+                                      decimals  (apply str base "." (repeat decimals "0"))
+                                      currency? (str base ".00")
+                                      :else     (str base ".##"))
+                              (= number-style "scientific") (str "E0")
+                              (= number-style "percent")    (str "%"))
+         fmtr               (DecimalFormat. fmt-str symbols)]
+     (fn [value]
+       (when value
+         (NumericWrapper.
+          (str (when prefix prefix)
+               (when (and currency? (or (nil? currency-style)
+                                        (= currency-style "symbol")))
+                 (get-in currency/currency [(keyword (or currency "USD")) :symbol]))
+               (when (and currency? (= currency-style "code"))
+                 (str (get-in currency/currency [(keyword (or currency "USD")) :code]) \space))
+               (.format fmtr (* value (or scale 1)))
+               (when (and currency? (= currency-style "name"))
+                 (str \space (get-in currency/currency [(keyword (or currency "USD")) :name_plural])))
+               (when suffix suffix))))))))
+
 (s/defn format-number :- NumericWrapper
   "Format a number `n` and return it as a NumericWrapper; this type is used to do special formatting in other
   `pulse.render` namespaces."
-  [n :- s/Num]
-  (NumericWrapper. (cl-format nil (if (integer? n) "~:d" "~,2f") n)))
+  ([n :- s/Num]
+   (NumericWrapper. (cl-format nil (if (integer? n) "~:d" "~,2f") n)))
+  ([value col-viz]
+   (let [fmttr (number-formatter col-viz)]
+     (fmttr value))))
 
 (defn graphing-column-row-fns
   "Return a pair of `[get-x-axis get-y-axis]` functions that can be used to get the x-axis and y-axis values in a row,
