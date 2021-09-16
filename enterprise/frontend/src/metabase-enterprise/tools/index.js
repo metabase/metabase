cc8aa349c9c5983a31832bc921c3726a40c7046d
Howon Lee <hlee.howon@gmail.com>
2021-09-15 21:26:01 -0700

    external imports first

--- enterprise/frontend/src/metabase-enterprise/tools/index.js
+++ enterprise/frontend/src/metabase-enterprise/tools/index.js
@@ -1,7 +1,7 @@
-import { PLUGIN_ADMIN_NAV_ITEMS, PLUGIN_ADMIN_ROUTES } from "metabase/plugins";
-
-import { hasPremiumFeature } from "metabase-enterprise/settings";
 import { t } from "ttag";
+
+import { PLUGIN_ADMIN_NAV_ITEMS, PLUGIN_ADMIN_ROUTES } from "metabase/plugins";
+import { hasPremiumFeature } from "metabase-enterprise/settings";
 import getToolsRoutes from "./routes";
 
 if (hasPremiumFeature("audit_app")) {
