4ce28e4e6e704c58dbcd26b09d697b2309437cb7
Pawit Pornkitprasan <pawit@metabase.com>
2021-09-16 09:48:34 +0700

    prettier

--- frontend/src/metabase/dashboard/components/DashboardHistoryModal.jsx
+++ frontend/src/metabase/dashboard/components/DashboardHistoryModal.jsx
@@ -5,7 +5,10 @@ import { withRouter } from "react-router";
 
 import HistoryModal from "metabase/containers/HistoryModal";
 import * as Urls from "metabase/lib/urls";
-import { fetchDashboard, fetchDashboardCardData } from "metabase/dashboard/actions";
+import {
+  fetchDashboard,
+  fetchDashboardCardData,
+} from "metabase/dashboard/actions";
 import Dashboards from "metabase/entities/dashboards";
 
 @withRouter
