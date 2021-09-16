f01a924d1a9696a4be0b26161b2b19863d18f8ff
Howon Lee <hlee.howon@gmail.com>
2021-09-15 22:50:00 -0700

    stripping unit test works

--- frontend/test/metabase/query_builder/components/VisualizationError.unit.spec.js
+++ frontend/test/metabase/query_builder/components/VisualizationError.unit.spec.js
@@ -12,9 +12,9 @@ describe("VisualizationError", () => {
   const errorLineNumbers = "";
 
   const errorH2Unstripped = `
-  Syntax error in SQL statement " FWEFWEF[*] "; expected "FROM, {"; SQL statement: -- Metabase:: userID: 1 queryType: native queryHash: 9863b8284f269ce8763ad59b04cec26407a1dd74eebeb16cffdf1ef3e23b325a\nfwefwef [42001-197]`
+  Syntax error in SQL statement " FWEFWEF[*] "; expected "FROM, {"; SQL statement: -- Metabase:: userID: 1 queryType: native queryHash: 9863b8284f269ce8763ad59b04cec26407a1dd74eebeb16cffdf1ef3e23b325a\nfwefwef [42001-197]`;
   const errorH2Stripped = `
-  Syntax error in SQL statement " FWEFWEF[*] "; expected "FROM, {"; SQL statement: fwefwef [42001-197]`
+  Syntax error in SQL statement " FWEFWEF[*] "; expected "FROM, {"; SQL statement: fwefwef [42001-197]`;
 
   it("error adjusted pg", () => {
     expect(stripRemarks(remarkedQuery)).toEqual("some shit");
@@ -28,11 +28,9 @@ describe("VisualizationError", () => {
     expect(stripRemarks(remarkedQuery)).toEqual("some shit");
   });
 
-  it("should just not adjust if there's nothing to adjust (pg)", () => {
-  })
+  it("should just not adjust if there's nothing to adjust (pg)", () => {});
 
-  it("should just not adjust if there's nothing to adjust (line numbers)", () => {
-  })
+  it("should just not adjust if there's nothing to adjust (line numbers)", () => {});
 
   it("should strip remarks from query with stripRemarks", () => {
     expect(stripRemarks(errorH2Unstripped)).toEqual(errorH2Stripped);
