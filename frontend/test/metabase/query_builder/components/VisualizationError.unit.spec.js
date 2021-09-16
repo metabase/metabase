330faa954e97d7dd791eeabef11811abdc86a37f
Howon Lee <hlee.howon@gmail.com>
2021-09-15 22:05:22 -0700

    sketch which tests to write

--- frontend/test/metabase/query_builder/components/VisualizationError.unit.spec.js
+++ frontend/test/metabase/query_builder/components/VisualizationError.unit.spec.js
@@ -1,7 +1,41 @@
-import { adjustPositions, stripRemarks } from "metabase/query_builder/components/DataSelector";
+import {
+  adjustPositions,
+  stripRemarks,
+} from "metabase/query_builder/components/DataSelector";
 
 describe("VisualizationError", () => {
   const remarkedQuery = "";
+  const remarkedRedshiftQuery = "";
+  const unremarkedQuery = "";
+
+  const errorPgSql = "";
+  const errorMySql = "";
+  const errorMsSql = "";
+  const errorH2 = "";
+
+  it("error adjusted pg", () => {
+    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+  });
+
+  it("error adjusted redshift", () => {
+    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+  });
+
+  it("error adjusted mysql", () => {
+    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+  });
+
+  it("error adjusted mssql", () => {
+    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+  });
+
+  it("error adjusted h2", () => {
+    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+  });
+
+  it("unremarked errors unchanged", () => {
+    expect(stripRemarks(remarkedQuery)).toEqual("some shit");
+  });
 
   it("should strip remarks from query with stripRemarks", () => {
     expect(stripRemarks(remarkedQuery)).toEqual("some shit");
