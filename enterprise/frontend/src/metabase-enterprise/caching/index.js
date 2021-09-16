6f3fd21f4f8fa53bfe0d053d5bb3c47ed272f502
Anton Kulyk <kuliks.anton@gmail.com>
2021-09-16 15:26:37 +0300

    Feature flag for caching UI

--- enterprise/frontend/src/metabase-enterprise/caching/index.js
+++ enterprise/frontend/src/metabase-enterprise/caching/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { t, jt } from "ttag";
+import { hasPremiumFeature } from "metabase-enterprise/settings";
 import { PLUGIN_CACHING, PLUGIN_FORM_WIDGETS } from "metabase/plugins";
 import Link from "metabase/components/Link";
 import { CacheTTLField } from "./components/CacheTTLField";
@@ -10,14 +11,6 @@ import {
   validateCacheTTL,
   normalizeCacheTTL,
 } from "./utils";
-
-PLUGIN_CACHING.getQuestionsImplicitCacheTTL = getQuestionsImplicitCacheTTL;
-
-PLUGIN_CACHING.cacheTTLFormField = {
-  name: "cache_ttl",
-  validate: validateCacheTTL,
-  normalize: normalizeCacheTTL,
-};
 
 function getDatabaseCacheTTLFieldDescription() {
   return (
@@ -33,16 +26,26 @@ function getDatabaseCacheTTLFieldDescription() {
   );
 }
 
-PLUGIN_CACHING.databaseCacheTTLFormField = {
-  name: "cache_ttl",
-  type: "databaseCacheTTL",
-  title: t`Default result cache duration`,
-  description: getDatabaseCacheTTLFieldDescription(),
-  descriptionPosition: "bottom",
-  validate: validateCacheTTL,
-  normalize: normalizeCacheTTL,
-};
+if (hasPremiumFeature("advanced_config")) {
+  PLUGIN_CACHING.cacheTTLFormField = {
+    name: "cache_ttl",
+    validate: validateCacheTTL,
+    normalize: normalizeCacheTTL,
+  };
 
-PLUGIN_FORM_WIDGETS.dashboardCacheTTL = CacheTTLField;
-PLUGIN_FORM_WIDGETS.databaseCacheTTL = DatabaseCacheTTLField;
-PLUGIN_FORM_WIDGETS.questionCacheTTL = QuestionCacheTTLField;
+  PLUGIN_CACHING.databaseCacheTTLFormField = {
+    name: "cache_ttl",
+    type: "databaseCacheTTL",
+    title: t`Default result cache duration`,
+    description: getDatabaseCacheTTLFieldDescription(),
+    descriptionPosition: "bottom",
+    validate: validateCacheTTL,
+    normalize: normalizeCacheTTL,
+  };
+
+  PLUGIN_FORM_WIDGETS.dashboardCacheTTL = CacheTTLField;
+  PLUGIN_FORM_WIDGETS.databaseCacheTTL = DatabaseCacheTTLField;
+  PLUGIN_FORM_WIDGETS.questionCacheTTL = QuestionCacheTTLField;
+
+  PLUGIN_CACHING.getQuestionsImplicitCacheTTL = getQuestionsImplicitCacheTTL;
+}
