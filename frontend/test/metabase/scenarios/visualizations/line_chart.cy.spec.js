25cd2bab4fc19b56e70b2904c709a2e1add7d2f0
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-17 15:42:20 +0300

    Show error messages when rendering card fails (#17929)

--- frontend/test/metabase/scenarios/visualizations/line_chart.cy.spec.js
+++ frontend/test/metabase/scenarios/visualizations/line_chart.cy.spec.js
@@ -68,6 +68,32 @@ describe("scenarios > visualizations > line chart", () => {
     cy.get(".value-labels").contains("30%");
   });
 
+  it("should display an error message when there are more series than the chart supports", () => {
+    visitQuestionAdhoc({
+      display: "line",
+      dataset_query: {
+        database: 1,
+        type: "query",
+        query: {
+          "source-table": PRODUCTS_ID,
+          aggregation: [["count"]],
+          breakout: [
+            ["field", PRODUCTS.CREATED_AT, { "temporal-unit": "year" }],
+            ["field", PRODUCTS.TITLE, null],
+          ],
+        },
+      },
+      visualization_settings: {
+        "graph.dimensions": ["CREATED_AT", "TITLE"],
+        "graph.metrics": ["count"],
+      },
+    });
+
+    cy.findByText(
+      "This chart type doesn't support more than 100 series of data.",
+    );
+  });
+
   it("should correctly display tooltip values when X-axis is numeric and style is 'Ordinal' (metabase#15998)", () => {
     visitQuestionAdhoc({
       dataset_query: {
