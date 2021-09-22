a225aa7a4fbe9cc07b359f003457fd9cb46a74ac
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-22 14:39:07 +0300

    Add empty stack

--- frontend/src/metabase/visualizations/lib/graph/stack.js
+++ frontend/src/metabase/visualizations/lib/graph/stack.js
@@ -9,6 +9,37 @@ export default function() {
   let y = stackY;
 
   function stack(data, index) {
+    if (!data.length) {
+      return data;
+    }
+
+    // Convert series to canonical two-dimensional representation.
+    let series = data.map(function(d, i) {
+      return values.call(stack, d, i);
+    });
+
+    // Convert each series to canonical [[x,y]] representation.
+    let points = series.map(function(d) {
+      return d.map(function(v, i) {
+        return [x.call(stack, v, i), y.call(stack, v, i)];
+      });
+    });
+
+    // Compute the order of series, and permute them.
+    const orders = order.call(stack, points, index);
+    series = d3.permute(series, orders);
+    points = d3.permute(points, orders);
+
+    // Compute the baseline…
+    const offsets = offset.call(stack, points, index);
+
+    // And propagate it to other series.
+    for (let j = 0; j < series[0].length; ++j) {
+      for (let i = 0; i < data.length; ++i) {
+        out.call(stack, series[i][j], offsets[i][j], points[i][j][1]);
+      }
+    }
+
     return data;
   }
 
