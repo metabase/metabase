faf007aaa4c61eea65eef034549130b6f943ea9c
Cam Saul <1455846+camsaul@users.noreply.github.com>
2021-09-21 17:55:38 -0700

    DELETE /api/ee/audit-app/user/:id/subscriptions archive Pulses created by user (#17981)

--- enterprise/backend/src/metabase_enterprise/audit_app/api/user.clj
+++ enterprise/backend/src/metabase_enterprise/audit_app/api/user.clj
@@ -3,15 +3,20 @@
   (:require [compojure.core :refer [DELETE]]
             [metabase.api.common :as api]
             [metabase.api.user :as api.user]
+            [metabase.models.pulse :refer [Pulse]]
             [metabase.models.pulse-channel-recipient :refer [PulseChannelRecipient]]
             [toucan.db :as db]))
 
 (api/defendpoint DELETE "/:id/subscriptions"
-  "Delete all Alert and DashboardSubscription subscriptions for a User. Only allowed for admins or for the current
-  user."
+  "Delete all Alert and DashboardSubscription subscriptions for a User (i.e., so they will no longer receive them).
+  Archive all Alerts and DashboardSubscriptions created by the User. Only allowed for admins or for the current user."
   [id]
   (api.user/check-self-or-superuser id)
+  ;; delete all `PulseChannelRecipient` rows for this User, which means they will no longer receive any
+  ;; Alerts/DashboardSubscriptions
   (db/delete! PulseChannelRecipient :user_id id)
+  ;; archive anything they created.
+  (db/update-where! Pulse {:creator_id id, :archived false} :archived true)
   api/generic-204-no-content)
 
 (api/define-routes)
