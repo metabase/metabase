10cf29f8da9375dbe54d99c3ee1c07a611dd2262
Anton Kulyk <kuliks.anton@gmail.com>
2021-09-24 13:09:26 +0300

    Enable the repro test

--- frontend/test/metabase/scenarios/admin/audit/ad-hoc.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/audit/ad-hoc.cy.spec.js
@@ -24,7 +24,7 @@ describeWithToken("audit > ad-hoc", () => {
       cy.signInAsAdmin();
     });
 
-    it.skip("should appear in audit log (metabase#16845 metabase-enterprise#486)", () => {
+    it("should appear in audit log (metabase#16845 metabase-enterprise#486)", () => {
       cy.visit("/admin/audit/members/log");
 
       cy.findByText("Native")
