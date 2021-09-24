1b8903eb9f5c93f4b8478fb14940bf0f2676b551
Anton Kulyk <kuliks.anton@gmail.com>
2021-09-24 19:55:11 +0300

    Always set sortable dragged item z-index to 1000

--- frontend/src/metabase/components/sortable/sortable.jsx
+++ frontend/src/metabase/components/sortable/sortable.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
 import styled from "styled-components";
 import {
   SortableContainer as OriginalSortableContainer,
   SortableElement,
   SortableHandle,
 } from "react-sortable-hoc";
+import "./sortable.css";
 
 const SortableContainerWrapper = styled.div`
   user-select: none;
@@ -13,7 +16,7 @@ const SortableContainerWrapper = styled.div`
 // Workaround for https://github.com/clauderic/react-sortable-hoc/issues/253
 // See issue demo here: https://github.com/metabase/metabase/issues/12870
 export function SortableContainer(WrappedComponent, config) {
-  return OriginalSortableContainer(
+  const SortableList = OriginalSortableContainer(
     props => (
       <SortableContainerWrapper>
         <WrappedComponent {...props} />
@@ -21,6 +24,27 @@ export function SortableContainer(WrappedComponent, config) {
     ),
     config,
   );
+
+  // Makes sure the sortable-hoc always receives "react-sortable-hoc-helper" class
+  // The class just applies a big z-index to the dragged item created by sortable-hoc
+  // to prevent https://github.com/clauderic/react-sortable-hoc#item-disappearing-when-sorting--css-issues
+  function SortableListWithDraggedItemVisibilityFix({ helperClass, ...props }) {
+    return (
+      <SortableList
+        {...props}
+        helperClass={cx("react-sortable-hoc-helper", helperClass)}
+      />
+    );
+  }
+
+  SortableListWithDraggedItemVisibilityFix.displayName =
+    WrappedComponent.displayName;
+
+  SortableListWithDraggedItemVisibilityFix.propTypes = {
+    helperClass: PropTypes.string,
+  };
+
+  return SortableListWithDraggedItemVisibilityFix;
 }
 
 export { SortableElement, SortableHandle };
