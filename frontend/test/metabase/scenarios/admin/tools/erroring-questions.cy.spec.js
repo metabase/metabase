bea0d77bebca06cd13a62f65f2da4399bff00b7c
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-09-25 00:39:14 +0200

    Add test section with that contains broken questions

--- frontend/test/metabase/scenarios/admin/tools/erroring-questions.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/tools/erroring-questions.cy.spec.js
@@ -2,10 +2,30 @@ import { restore, describeWithToken } from "__support__/e2e/cypress";
 
 const TOOLS_ERRORS_URL = "/admin/tools/errors";
 
+// The filter is required but doesn't have a default value set
+const brokenQuestionDetails = {
+  name: "Broken SQL",
+  native: {
+    "template-tags": {
+      filter: {
+        id: "ce8f111c-24c4-6823-b34f-f704404572f1",
+        name: "filter",
+        "display-name": "Filter",
+        type: "text",
+        required: true,
+      },
+    },
+    query: "select {{filter}}",
+  },
+  display: "scalar",
+};
+
 describeWithToken("admin > tools > erroring questions ", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+
+    cy.intercept("POST", "/api/dataset").as("dataset");
   });
 
   describe("without broken questions", () => {
@@ -39,6 +59,54 @@ describeWithToken("admin > tools > erroring questions ", () => {
 
       // When the issue gets fixed, it's safe to merge these assertions with the main test above
       cy.button("Rerun Selected").should("be.disabled");
+    });
+  });
+
+  describe("with the existing broken questions", () => {
+    beforeEach(() => {
+      cy.createNativeQuestion(brokenQuestionDetails, {
+        loadMetadata: true,
+      });
+
+      cy.visit("/admin/tools/errors");
+    });
+
+    it.skip('should disable "Rerun Selected" button (metabase#18048)', () => {
+      cy.visit(TOOLS_ERRORS_URL);
+
+      // When the issue gets fixed, merge it with the main test below
+      cy.button("Rerun Selected").should("be.disabled");
+    });
+
+    it("should render correctly", () => {
+      cy.wait("@dataset");
+
+      cy.findByText(brokenQuestionDetails.name)
+        .closest("tr")
+        .within(() => {
+          cy.findByRole("checkbox")
+            .click()
+            .should("be.checked");
+        });
+
+      cy.button("Rerun Selected")
+        .should("not.be.disabled")
+        .click();
+
+      cy.wait("@dataset");
+
+      // The question is still there because we didn't fix it
+      cy.findByText(brokenQuestionDetails.name);
+
+      cy.findByPlaceholderText("Error name").should("not.be.disabled");
+      cy.findByPlaceholderText("DB name").should("not.be.disabled");
+      cy.findByPlaceholderText("Collection name")
+        .should("not.be.disabled")
+        .type("foo");
+
+      cy.wait("@dataset");
+
+      cy.findByText("No results");
     });
   });
 });
