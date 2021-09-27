2d5db675679f0c64b388f703862d8aaeb2c2d5d1
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-09-27 21:52:38 +0200

    Add repro for #18021

---
+++ frontend/test/metabase/scenarios/moderation/reproductions/18021-verified-badge-not-visible-in-recents.cy.spec.js
@@ -0,0 +1,30 @@
+import { restore, describeWithToken } from "__support__/e2e/cypress";
+
+describeWithToken.skip("issue 18021", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+
+    cy.request("POST", "/api/moderation-review", {
+      status: "verified",
+      moderated_item_id: 1,
+      moderated_item_type: "card",
+    });
+
+    cy.visit("/question/1");
+
+    cy.findByTestId("saved-question-header-button").find(".Icon-verified");
+  });
+
+  it("should show verified badge in the 'Recently viewed' list (metabase#18021)", () => {
+    cy.findByPlaceholderText("Search…").click();
+
+    cy.findByText("Recently viewed")
+      .parent()
+      .within(() => {
+        cy.findByText("Orders")
+          .closest("a")
+          .find(".Icon-verified");
+      });
+  });
+});
