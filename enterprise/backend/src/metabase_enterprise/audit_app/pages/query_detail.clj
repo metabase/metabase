896e7bf829bf959ff71c7aa26203ee8a208d0f86
Howon Lee <hlee.howon@gmail.com>
2021-09-27 11:33:45 -0700

    more recondite linter comment

--- enterprise/backend/src/metabase_enterprise/audit_app/pages/query_detail.clj
+++ enterprise/backend/src/metabase_enterprise/audit_app/pages/query_detail.clj
@@ -28,35 +28,34 @@
               [:user_name       {:display_name "Created By",         :base_type :type/Text    :remapped_from :user_id}]
               [:updated_at      {:display_name "Updated At",         :base_type :type/DateTime}]]
    :results (common/reducible-query
-              (->
-                {:with      [cards/query-runs
-                             cards/latest-qe
-                             cards/dashboards-ids]
-                 :select    [[:card.id :card_id]
-                             [:card.name :card_name]
-                             [:latest_qe.error :error_str]
-                             :collection_id
-                             [:coll.name :collection_name]
-                             :card.database_id
-                             [:db.name :database_name]
-                             [:t.schema :schema_name]
-                             :card.table_id
-                             [:t.name :table_name]
-                             [:latest_qe.started_at :last_run_at]
-                             [:query_runs.count :total_runs]
-                             [:dash_card.name_str :dash_name_str]
-                             [:card.creator_id :user_id]
-                             [(common/user-full-name :u) :user_name]
-                             [:card.updated_at :updated_at]]
-                 :from      [[:report_card :card]]
-                 :left-join [[:collection :coll]                [:= :card.collection_id :coll.id]
-                             [:metabase_database :db]           [:= :card.database_id :db.id]
-                             [:metabase_table :t]               [:= :card.table_id :t.id]
-                             [:core_user :u]                    [:= :card.creator_id :u.id]
-                             :latest_qe                         [:= :card.id :latest_qe.card_id]
-                             :query_runs                        [:= :card.id :query_runs.card_id]
-                             :dash_card                         [:= :card.id :dash_card.card_id]]
-                 :where     [:= :card.id card-id] }))})
+              {:with      [cards/query-runs
+                           cards/latest-qe
+                           cards/dashboards-ids]
+               :select    [[:card.id :card_id]
+                           [:card.name :card_name]
+                           [:latest_qe.error :error_str]
+                           :collection_id
+                           [:coll.name :collection_name]
+                           :card.database_id
+                           [:db.name :database_name]
+                           [:t.schema :schema_name]
+                           :card.table_id
+                           [:t.name :table_name]
+                           [:latest_qe.started_at :last_run_at]
+                           [:query_runs.count :total_runs]
+                           [:dash_card.name_str :dash_name_str]
+                           [:card.creator_id :user_id]
+                           [(common/user-full-name :u) :user_name]
+                           [:card.updated_at :updated_at]]
+               :from      [[:report_card :card]]
+               :left-join [[:collection :coll]                [:= :card.collection_id :coll.id]
+                           [:metabase_database :db]           [:= :card.database_id :db.id]
+                           [:metabase_table :t]               [:= :card.table_id :t.id]
+                           [:core_user :u]                    [:= :card.creator_id :u.id]
+                           :latest_qe                         [:= :card.id :latest_qe.card_id]
+                           :query_runs                        [:= :card.id :query_runs.card_id]
+                           :dash_card                         [:= :card.id :dash_card.card_id]]
+               :where     [:= :card.id card-id] })})
 
 ;; Details about a specific query (currently just average execution time).
 (s/defmethod audit.i/internal-query ::details
