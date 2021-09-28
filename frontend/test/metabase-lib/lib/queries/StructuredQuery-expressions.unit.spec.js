5b99c537a6ed7ec8ad686743f1c633d6462c1ab2
Anton Kulyk <kuliks.anton@gmail.com>
2021-09-28 18:17:19 +0300

    Clean up query expressions unit tests

--- frontend/test/metabase-lib/lib/queries/StructuredQuery-expressions.unit.spec.js
+++ frontend/test/metabase-lib/lib/queries/StructuredQuery-expressions.unit.spec.js
@@ -1,17 +1,16 @@
 import { ORDERS } from "__support__/sample_dataset_fixture";
 
+const TEST_EXPRESSION = ["*", ["field", ORDERS.TOTAL.id, null], 2];
+
 describe("StructuredQuery", () => {
   describe("hasExpressions", () => {
     it("should return false for queries without expressions", () => {
       const q = ORDERS.query();
       expect(q.hasExpressions()).toBe(false);
     });
+
     it("should return true for queries with expressions", () => {
-      const q = ORDERS.query().addExpression("double_total", [
-        "*",
-        ["field", ORDERS.TOTAL.id, null],
-        2,
-      ]);
+      const q = ORDERS.query().addExpression("double_total", TEST_EXPRESSION);
       expect(q.hasExpressions()).toBe(true);
     });
   });
