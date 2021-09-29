8ce94ad365d6fdc9146b5c8a9b0f9917cb5effce
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-09-29 18:06:46 +0300

    Fix cutting of data points

--- frontend/src/metabase/visualizations/components/legend/LegendLayout.styled.jsx
+++ frontend/src/metabase/visualizations/components/legend/LegendLayout.styled.jsx
@@ -4,7 +4,8 @@ export const LegendLayoutRoot = styled.div`
   display: flex;
   flex: 1 1 auto;
   flex-direction: ${({ isVertical }) => (isVertical ? "row" : "column")};
-  overflow: hidden;
+  min-width: 0;
+  min-height: 0;
 `;
 
 export const MainContainer = styled.div`
