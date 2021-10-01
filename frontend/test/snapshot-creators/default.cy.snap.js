30f7d6289f07475485006193279f219597c39a3a
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-01 13:49:28 +0200

    Fix eslint error

--- frontend/test/snapshot-creators/default.cy.snap.js
+++ frontend/test/snapshot-creators/default.cy.snap.js
@@ -1,10 +1,5 @@
 import _ from "underscore";
-import {
-  snapshot,
-  restore,
-  withSampleDataset,
-  addPostgresDatabase,
-} from "__support__/e2e/cypress";
+import { snapshot, restore, withSampleDataset } from "__support__/e2e/cypress";
 import { USERS, USER_GROUPS } from "__support__/e2e/cypress_data";
 
 const {
