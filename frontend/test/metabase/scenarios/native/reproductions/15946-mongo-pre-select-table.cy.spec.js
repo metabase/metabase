1af26be34461141138abbf8f220e5f7105de5e9e
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-01 15:09:14 +0200

    Relocate mongo `native` spec

--- frontend/test/metabase/scenarios/native/reproductions/15946-mongo-pre-select-table.cy.spec.js
+++ frontend/test/metabase/scenarios/native/reproductions/15946-mongo-pre-select-table.cy.spec.js
@@ -2,25 +2,25 @@ import { restore, modal } from "__support__/e2e/cypress";
 
 const MONGO_DB_NAME = "QA Mongo4";
 
-// Skipping the whole describe block because it contains only one skipped test so far!
-// We don't want to run the whole beforeEeach block in CI only to skip the test afterwards.
-// IMPORTANT: when #15946 gets fixed, unskip both describe block and the test itself!
-describe.skip("mongodb > native query", () => {
+describe.skip("issue 15946", () => {
   before(() => {
     restore("mongo-4");
     cy.signInAsAdmin();
-  });
 
-  it.skip("converting a question to the native query should pre-select a table (metabase#15946)", () => {
     cy.visit("/question/new");
     cy.findByText("Custom question").click();
     cy.findByText(MONGO_DB_NAME).click();
     cy.findByText("Orders").click();
+  });
+
+  it("converting a question to the native query should pre-select a table (metabase#15946)", () => {
     cy.get(".QueryBuilder .Icon-sql").click();
+
     modal()
       .findByText("Convert this question to a native query")
       .click();
     modal().should("not.exist");
+
     cy.get(".GuiBuilder-data").contains(MONGO_DB_NAME);
     cy.get(".GuiBuilder-data").contains("Orders");
   });
