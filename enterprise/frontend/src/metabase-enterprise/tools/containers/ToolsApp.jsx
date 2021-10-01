89370a3e5bd1561e195957e043bc4ba857c93e94
Howon Lee <hlee.howon@gmail.com>
2021-10-01 06:42:11 -0700

    title

--- enterprise/frontend/src/metabase-enterprise/tools/containers/ToolsApp.jsx
+++ enterprise/frontend/src/metabase-enterprise/tools/containers/ToolsApp.jsx
@@ -10,6 +10,6 @@ export default class ToolsApp extends Component {
 
   render() {
     const { children } = this.props;
-    return <AdminLayout>{children}</AdminLayout>;
+    return <AdminLayout><h2>Questions that errored when last run</h2>{children}</AdminLayout>;
   }
 }
