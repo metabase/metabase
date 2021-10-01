ca74bb6729aef3c5b2fa1179c37bebcd8ec3731b
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-01 14:47:38 +0200

    Create `mongo-4` snapshot

--- frontend/test/snapshot-creators/qa-db.cy.snap.js
+++ frontend/test/snapshot-creators/qa-db.cy.snap.js
@@ -2,6 +2,7 @@ import {
   restore,
   snapshot,
   addPostgresDatabase,
+  addMongoDatabase,
 } from "__support__/e2e/cypress";
 
 describe("qa databases snapshots", () => {
@@ -13,6 +14,11 @@ describe("qa databases snapshots", () => {
   it("creates snapshots for supported qa databases", () => {
     addPostgresDatabase();
     snapshot("postgres-12");
+
+    restore("default");
+
+    addMongoDatabase();
+    snapshot("mongo-4");
 
     restore("blank");
   });
