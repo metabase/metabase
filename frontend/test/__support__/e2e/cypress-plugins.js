8bb35ecdcfb34e89db18681e28114bd135f27fca
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-01 13:26:20 +0200

    Ignore QA DB snapshots if `QA_DB_ENABLED` env var is not present

--- frontend/test/__support__/e2e/cypress-plugins.js
+++ frontend/test/__support__/e2e/cypress-plugins.js
@@ -67,8 +67,9 @@ module.exports = (on, config) => {
    ********************************************************************/
 
   if (!isQaDatabase) {
-    config.ignoreTestFiles = "**/metabase-db/**";
+    config.ignoreTestFiles = ["**/metabase-db/**", "qa-db.cy.snap.js"];
   }
+
   config.env.HAS_ENTERPRISE_TOKEN = hasEnterpriseToken;
 
   return config;
