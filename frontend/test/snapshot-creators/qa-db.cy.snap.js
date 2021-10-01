metabase/metabase

commit a56b640dfc8bbd4ae3d6047e0a9594f1c709a2b1

Cypress E2E postgres snapshot (#18106)
* Create `postgres-12` snapshot

* Use `postgres-12` snapshot in tests

* Relocate postgres `add` spec

* Relocate postgres `sandboxes` spec

* Relocate postgres `native` spec

This actually didn't have anything to do with the native questions.
We simply used native editor because it was easier to reproduce the issue.

* Relocate postgres `permissions` spec

* Relocate postgres `query` spec

* Relocate postgres `custom-column` spec

* Remove postgres from `metabase-db`

.circleci/config.yml
@@ -1291,15 +1291,6 @@ workflows:
           <<: *Matrix
 
       - fe-tests-cypress:
-          name: e2e-tests-postgres-12-<< matrix.edition >>
-          requires:
-            - build-uberjar-<< matrix.edition >>
-          cypress-group: "postgres"
-          source-folder: postgres
-          qa-db: true
-          <<: *Matrix
-
-      - fe-tests-cypress:
           name: e2e-tests-mysql-8-<< matrix.edition >>
           requires:
             - build-uberjar-<< matrix.edition >>

frontend/test/snapshot-creators/default.cy.snap.js
@@ -1,5 +1,10 @@
 import _ from "underscore";
-import { snapshot, restore, withSampleDataset } from "__support__/e2e/cypress";
+import {
+  snapshot,
+  restore,
+  withSampleDataset,
+  addPostgresDatabase,
+} from "__support__/e2e/cypress";
 import { USERS, USER_GROUPS } from "__support__/e2e/cypress_data";
 
 const {
@@ -26,7 +31,12 @@ describe("snapshots", () => {
           SAMPLE_DATASET,
         );
       });
+
       snapshot("default");
+
+      addPostgresDatabase();
+      snapshot("postgres-12");
+
       restore("blank");
     });
   });

frontend/test/__runner__/cypress-runner-run-tests.js
@@ -12,7 +12,7 @@ const args = arg(
 const folder = args["--folder"];
 const isFolder = !!folder;
 
-const supportedDatabases = ["mongo", "mysql", "postgres"];
+const supportedDatabases = ["mongo", "mysql"];
 const isQaDatabase = supportedDatabases.includes(folder);
 
 const isOpenMode = args["--open"];

frontend/test/metabase-db/postgres/custom-column.cy.spec.js
@@ -1,98 +1,0 @@
-import { restore, addPostgresDatabase, popover } from "__support__/e2e/cypress";
-
-const PG_DB_NAME = "QA Postgres12";
-
-describe("postgres > question > custom columns", () => {
-  beforeEach(() => {
-    restore();
-    cy.signInAsAdmin();
-    addPostgresDatabase(PG_DB_NAME);
-  });
-
-  it("should allow using strings in filter based on a custom column (metabase#13751)", () => {
-    const CC_NAME = "C-States";
-
-    cy.visit("/question/new");
-    cy.findByText("Custom question").click();
-    cy.findByText(PG_DB_NAME).click();
-    cy.findByText("People").click();
-
-    cy.log("Create custom column using `regexextract()`");
-
-    cy.icon("add_data").click();
-    popover().within(() => {
-      cy.get("[contenteditable='true']").type(
-        'regexextract([State], "^C[A-Z]")',
-      );
-      cy.findByPlaceholderText("Something nice and descriptive").type(CC_NAME);
-      cy.get(".Button")
-        .contains("Done")
-        .should("not.be.disabled")
-        .click();
-    });
-
-    cy.log("Add filter based on custom column");
-
-    cy.findByText("Add filters to narrow your answer").click();
-    popover().within(() => {
-      cy.findByText(CC_NAME).click();
-      cy.get(".AdminSelect").click();
-      cy.log(
-        "**It fails here already because it doesn't find any condition for strings. Only numbers.**",
-      );
-      cy.findByText("Is");
-      cy.get("input").type("CO");
-      cy.get(".Button")
-        .contains("Add filter")
-        .should("not.be.disabled")
-        .click();
-    });
-
-    cy.button("Visualize").click();
-    cy.findByText("Arnold Adams");
-  });
-
-  it.skip("should not remove regex escape characters (metabase#14517)", () => {
-    // Ironically, both Prettier and Cypress remove escape characters from our code as well
-    // We're testing for the literal sting `(?<=\/\/)[^\/]*`, but we need to escape the escape characters to make it work
-    const ESCAPED_REGEX = "(?<=\\/\\/)[^\\/]*";
-
-    cy.visit("/question/new");
-    cy.findByText("Custom question").click();
-    cy.findByText(PG_DB_NAME).click();
-    cy.findByText("People").click();
-
-    cy.log("Create custom column using `regexextract()`");
-
-    cy.icon("add_data").click();
-    popover().within(() => {
-      cy.get("[contenteditable='true']")
-        .type(`regexextract([State], "${ESCAPED_REGEX}")`)
-        .blur();
-
-      // It removes escaped characters already on blur
-      cy.log("Reported failing on v0.36.4");
-      cy.contains(ESCAPED_REGEX);
-    });
-  });
-
-  it("`Percentile` custom expression function should accept two parameters (metabase#15714)", () => {
-    cy.visit("/question/new");
-    cy.findByText("Custom question").click();
-    cy.findByText(PG_DB_NAME).click();
-    cy.findByText("Orders").click();
-    cy.icon("add_data").click();
-    cy.get("[contenteditable='true']")
-      .click()
-      .type("Percentile([Subtotal], 0.1)");
-    cy.findByPlaceholderText("Something nice and descriptive")
-      .as("description")
-      .click();
-    cy.findByText("Function Percentile expects 1 argument").should("not.exist");
-    cy.get("@description").type("A");
-    cy.button("Done")
-      .should("not.be.disabled")
-      .click();
-    // Todo: Add positive assertions once this is fixed
-  });
-});

frontend/test/metabase-db/postgres/native.cy.spec.js
@@ -1,25 +1,0 @@
-import { restore, addPostgresDatabase, modal } from "__support__/e2e/cypress";
-
-const PG_DB_NAME = "QA Postgres12";
-
-// Skipping the whole describe block because it contains only one skipped test so far!
-// We don't want to run the whole beforeEeach block in CI only to skip the test afterwards.
-// IMPORTANT: when #14957 gets fixed, unskip both describe block and the test itself!
-describe.skip("postgres > question > native", () => {
-  beforeEach(() => {
-    restore();
-    cy.signInAsAdmin();
-    addPostgresDatabase(PG_DB_NAME);
-  });
-
-  it.skip("should save a question before query has been executed (metabase#14957)", () => {
-    cy.visit("/question/new");
-    cy.findByText("Native query").click();
-    cy.findByText(PG_DB_NAME).click();
-    cy.get(".ace_content").type("select pg_sleep(60)");
-    cy.findByText("Save").click();
-    cy.findByLabelText("Name").type("14957");
-    cy.button("Save").click();
-    modal().should("not.exist");
-  });
-});

frontend/test/metabase-db/postgres/permissions.cy.spec.js
@@ -1,61 +1,0 @@
-import {
-  restore,
-  addPostgresDatabase,
-  withDatabase,
-} from "__support__/e2e/cypress";
-import { USER_GROUPS } from "__support__/e2e/cypress_data";
-
-const { ALL_USERS_GROUP } = USER_GROUPS;
-const PG_DB_NAME = "QA Postgres12";
-const PG_DB_ID = 2;
-
-describe("postgres > permissions", () => {
-  beforeEach(() => {
-    restore();
-    cy.signInAsAdmin();
-    addPostgresDatabase(PG_DB_NAME);
-    cy.server();
-  });
-
-  // NOTE: This issue wasn't specifically related to PostgreSQL. We simply needed to add another DB to reproduce it.
-  ["QB", "Native"].forEach(test => {
-    it.skip(`${test.toUpperCase()} version:\n should be able to select question (from "Saved Questions") which belongs to the database user doesn't have data-permissions for (metabase#13347)`, () => {
-      cy.route("POST", "/api/dataset").as("dataset");
-
-      cy.updatePermissionsGraph({
-        [ALL_USERS_GROUP]: {
-          1: { schemas: "all", native: "write" },
-          [PG_DB_ID]: { schemas: "none", native: "none" },
-        },
-      });
-
-      cy.updateCollectionGraph({
-        [ALL_USERS_GROUP]: { root: "read" },
-      });
-
-      withDatabase(
-        PG_DB_ID,
-        ({ ORDERS_ID }) =>
-          cy.createQuestion({
-            name: "Q1",
-            query: { "source-table": ORDERS_ID },
-            database: PG_DB_ID,
-          }),
-
-        cy.createNativeQuestion({
-          name: "Q2",
-          native: { query: "SELECT * FROM ORDERS" },
-          database: PG_DB_ID,
-        }),
-      );
-
-      cy.signIn("none");
-      cy.visit("/question/new");
-      cy.findByText("Simple question").click();
-      cy.findByText("Saved Questions").click();
-      test === "QB" ? cy.findByText("Q1").click() : cy.findByText("Q2").click();
-      cy.wait("@dataset", { timeout: 5000 });
-      cy.contains("37.65");
-    });
-  });
-});

frontend/test/metabase-db/postgres/query.cy.spec.js
@@ -1,72 +1,0 @@
-import {
-  restore,
-  addPostgresDatabase,
-  withDatabase,
-  visitQuestionAdhoc,
-} from "__support__/e2e/cypress";
-
-const PG_DB_NAME = "QA Postgres12";
-const PG_DB_ID = 2;
-
-describe("postgres > user > query", () => {
-  beforeEach(() => {
-    restore();
-    cy.signInAsAdmin();
-    addPostgresDatabase(PG_DB_NAME);
-  });
-
-  it("should show row details when clicked on its entity key (metabase#13263)", () => {
-    cy.visit("/question/new");
-    cy.findByText("Simple question").click();
-    cy.findByText(PG_DB_NAME).click();
-    cy.findByText("Orders").click();
-
-    // We're clicking on ID: 1 (the first order) => do not change!
-    // It is tightly coupled to the assertion ("37.65"), which is "Subtotal" value for that order.
-    cy.get(".Table-ID")
-      .eq(0)
-      .click();
-
-    // Wait until "doing science" spinner disappears (DOM is ready for assertions)
-    // TODO: if this proves to be reliable, extract it as a helper function for waiting on DOM to render
-    cy.get(".LoadingSpinner").should("not.exist");
-
-    // Assertions
-    cy.log("Fails in v0.36.6");
-    // This could be omitted because real test is searching for "37.65" on the page
-    cy.findByText("There was a problem with your question").should("not.exist");
-    cy.contains("37.65");
-  });
-
-  it("should display pivot tables (metabase#14148)", () => {
-    cy.server();
-    cy.route("POST", "/api/dataset/pivot").as("pivotDataset");
-
-    withDatabase(PG_DB_ID, ({ PEOPLE, PEOPLE_ID }) =>
-      visitQuestionAdhoc({
-        display: "pivot",
-        dataset_query: {
-          type: "query",
-          database: PG_DB_ID,
-          query: {
-            "source-table": PEOPLE_ID,
-            aggregation: [["count"]],
-            breakout: [
-              ["field", PEOPLE.SOURCE, null],
-              ["field", PEOPLE.CREATED_AT, { "temporal-unit": "year" }],
-            ],
-          },
-        },
-      }),
-    );
-
-    cy.log(
-      "Reported failing on v0.38.0-rc1 querying Postgres, Redshift and BigQuery. It works on MySQL and H2.",
-    );
-    cy.wait("@pivotDataset").then(xhr => {
-      expect(xhr.response.body.cause || "").not.to.contain("ERROR");
-    });
-    cy.findByText(/Grand totals/i);
-    cy.findByText("2,500");
-  });
-});

frontend/test/metabase/scenarios/admin/databases/add-external.cy.spec.js
@@ -1,15 +1,14 @@
 import { restore, modal, typeAndBlurUsingLabel } from "__support__/e2e/cypress";
 
-describe("postgres > admin > add", () => {
+describe("admin > database > add > postgres", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
-    cy.server();
+
+    cy.intercept("POST", "/api/database").as("createDatabase");
   });
 
   it("should add a database and redirect to listing", () => {
-    cy.route("POST", "/api/database").as("createDatabase");
-
     cy.visit("/admin/databases/create");
     cy.contains("Database type")
       .closest(".Form-field")

commit 170a2b849f264c2a9ef0f38af9a8ff48c0ddf4a1

Separate qa db snapshots (#18179)
- a56b640 broke Percy workflow on master and introduced a major inconvenience for developers who wish to run Cypress locally. In both cases, it's required to have all supported QA DB docker images running at all times. Otherwise, the Cypress snapshot creation phase breaks.

This PR:
- uses the existing ENV var to conditionally skip qa db snapshots
- this doesn't affect CircleCI workflow - it merely makes life easier for developers running Cypress locally and for a Percy workflow that runs on GitHub Actions (where we still don't have QA docker images available)

.circleci/config.yml
@@ -1263,6 +1263,7 @@ workflows:
             - build-uberjar-<< matrix.edition >>
           cypress-group: "<< matrix.folder >>-<< matrix.edition >>"
           source-folder: << matrix.folder >>
+          qa-db: true
           before-steps:
             - wait-for-port:
                 port: 3306

frontend/test/__support__/e2e/cypress-plugins.js
@@ -67,8 +67,9 @@ module.exports = (on, config) => {
    ********************************************************************/
 
   if (!isQaDatabase) {
-    config.ignoreTestFiles = "**/metabase-db/**";
+    config.ignoreTestFiles = ["**/metabase-db/**", "qa-db.cy.snap.js"];
   }
+
   config.env.HAS_ENTERPRISE_TOKEN = hasEnterpriseToken;
 
   return config;

frontend/test/snapshot-creators/default.cy.snap.js
@@ -1,10 +1,5 @@
 import _ from "underscore";
-import {
-  snapshot,
-  restore,
-  withSampleDataset,
-  addPostgresDatabase,
-} from "__support__/e2e/cypress";
+import { snapshot, restore, withSampleDataset } from "__support__/e2e/cypress";
 import { USERS, USER_GROUPS } from "__support__/e2e/cypress_data";
 
 const {
@@ -33,9 +28,6 @@ describe("snapshots", () => {
       });
 
       snapshot("default");
-
-      addPostgresDatabase();
-      snapshot("postgres-12");
 
       restore("blank");
     });

frontend/test/snapshot-creators/qa-db.cy.snap.js
@@ -1,0 +1,19 @@
+import {
+  restore,
+  snapshot,
+  addPostgresDatabase,
+} from "__support__/e2e/cypress";
+
+describe("qa databases snapshots", () => {
+  beforeEach(() => {
+    restore("default");
+    cy.signInAsAdmin();
+  });
+
+  it("creates snapshots for supported qa databases", () => {
+    addPostgresDatabase();
+    snapshot("postgres-12");
+
+    restore("blank");
+  });
+});