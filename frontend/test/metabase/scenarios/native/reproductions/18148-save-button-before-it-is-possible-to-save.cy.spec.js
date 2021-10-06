ea87c9303b50ff1ba4b48c36de721d6097b08f4c
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-06 14:19:50 +0200

    #18148 Repro: "Save" button shouldn't appear before one selects the source for a native question (#18259)

---
+++ frontend/test/metabase/scenarios/native/reproductions/18148-save-button-before-it-is-possible-to-save.cy.spec.js
@@ -0,0 +1,32 @@
+import { restore } from "__support__/e2e/cypress";
+
+const dbName = "Sample2";
+
+describe("issue 18148", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+
+    cy.addH2SampleDataset({
+      name: dbName,
+    });
+
+    cy.visit("/");
+    cy.icon("sql").click();
+  });
+
+  it("should not offer to save the question before it is actually possible to save it (metabase#18148)", () => {
+    cy.findByText("Select a database");
+    cy.findByText("Save").should("have.class", "disabled");
+
+    cy.findByText(dbName).click();
+
+    cy.get(".ace_content")
+      .should("be.visible")
+      .type("select foo");
+
+    cy.findByText("Save").click();
+
+    cy.get(".Modal").should("exist");
+  });
+});
