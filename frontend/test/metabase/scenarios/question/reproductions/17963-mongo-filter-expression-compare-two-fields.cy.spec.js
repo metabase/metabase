83852362017c538d2fc9a0e81dafee84cf9580fd
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-06 21:47:11 +0200

    Add repro for #17963

---
+++ frontend/test/metabase/scenarios/question/reproductions/17963-mongo-filter-expression-compare-two-fields.cy.spec.js
@@ -0,0 +1,47 @@
+import { restore, popover } from "__support__/e2e/cypress";
+
+describe.skip("issue 17963", () => {
+  beforeEach(() => {
+    restore("mongo-4");
+    cy.signInAsAdmin();
+
+    cy.visit("/question/new");
+    cy.findByText("Custom question").click();
+    cy.findByText("QA Mongo4").click();
+    cy.findByText("Orders").click();
+  });
+
+  it("should be able to compare two fields using filter expression (metabase#17963)", () => {
+    cy.findByText("Add filters to narrow your answer").click();
+
+    popover()
+      .contains("Custom Expression")
+      .click();
+
+    typeAndSelect([
+      { string: "dis", field: "Discount" },
+      { string: "> qu", field: "Quantity" },
+    ]);
+
+    cy.button("Done").click();
+
+    cy.findByText("Discount > Quantity");
+
+    cy.findByText("Pick the metric you want to see").click();
+    cy.findByText("Count of rows").click();
+
+    cy.button("Visualize").click();
+
+    cy.get(".ScalarValue").contains("1,337");
+  });
+});
+
+function typeAndSelect(arr) {
+  arr.forEach(({ string, field }) => {
+    cy.get("[contenteditable=true]").type(string);
+
+    popover()
+      .contains(field)
+      .click();
+  });
+}
