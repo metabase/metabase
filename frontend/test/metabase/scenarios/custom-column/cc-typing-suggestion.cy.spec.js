f3e2955ba92467f5eed10e8b24393b098fbca2c1
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-07 23:46:54 +0200

    Extract custom column typing-suggestion spec

---
+++ frontend/test/metabase/scenarios/custom-column/cc-typing-suggestion.cy.spec.js
@@ -0,0 +1,47 @@
+import { restore, openProductsTable } from "__support__/e2e/cypress";
+
+describe("scenarios > question > custom column > typing suggestion", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+
+    openProductsTable({ mode: "notebook" });
+    cy.findByText("Custom column").click();
+  });
+
+  it("should not suggest arithmetic operators", () => {
+    cy.get("[contenteditable='true']").type("[Price] ");
+    cy.contains("/").should("not.exist");
+  });
+
+  it("should correctly accept the chosen field suggestion", () => {
+    cy.get("[contenteditable='true']").type(
+      "[Rating]{leftarrow}{leftarrow}{leftarrow}",
+    );
+
+    // accept the only suggested item, i.e. "[Rating]"
+    cy.get("[contenteditable='true']").type("{enter}");
+
+    // if the replacement is correct -> "[Rating]"
+    // if the replacement is wrong -> "[Rating] ng"
+    cy.get("[contenteditable='true']")
+      .contains("[Rating] ng")
+      .should("not.exist");
+  });
+
+  it("should correctly accept the chosen function suggestion", () => {
+    cy.get("[contenteditable='true']").type("LTRIM([Title])");
+
+    // Place the cursor between "is" and "empty"
+    cy.get("[contenteditable='true']").type(
+      Array(13)
+        .fill("{leftarrow}")
+        .join(""),
+    );
+
+    // accept the first suggested function, i.e. "length"
+    cy.get("[contenteditable='true']").type("{enter}");
+
+    cy.get("[contenteditable='true']").contains("length([Title])");
+  });
+});
--- frontend/test/metabase/scenarios/question/notebook.cy.spec.js
+++ frontend/test/metabase/scenarios/question/notebook.cy.spec.js
@@ -930,50 +930,6 @@ describe("scenarios > question > notebook", () => {
     });
   });
 
-  describe("typing suggestion", () => {
-    it("should not suggest arithmetic operators", () => {
-      openProductsTable({ mode: "notebook" });
-      cy.findByText("Custom column").click();
-      cy.get("[contenteditable='true']").type("[Price] ");
-      cy.contains("/").should("not.exist");
-    });
-
-    it("should correctly accept the chosen field suggestion", () => {
-      openProductsTable({ mode: "notebook" });
-      cy.findByText("Custom column").click();
-      cy.get("[contenteditable='true']").type(
-        "[Rating]{leftarrow}{leftarrow}{leftarrow}",
-      );
-
-      // accept the only suggested item, i.e. "[Rating]"
-      cy.get("[contenteditable='true']").type("{enter}");
-
-      // if the replacement is correct -> "[Rating]"
-      // if the replacement is wrong -> "[Rating] ng"
-      cy.get("[contenteditable='true']")
-        .contains("[Rating] ng")
-        .should("not.exist");
-    });
-
-    it("should correctly accept the chosen function suggestion", () => {
-      openProductsTable({ mode: "notebook" });
-      cy.findByText("Custom column").click();
-      cy.get("[contenteditable='true']").type("LTRIM([Title])");
-
-      // Place the cursor between "is" and "empty"
-      cy.get("[contenteditable='true']").type(
-        Array(13)
-          .fill("{leftarrow}")
-          .join(""),
-      );
-
-      // accept the first suggested function, i.e. "length"
-      cy.get("[contenteditable='true']").type("{enter}");
-
-      cy.get("[contenteditable='true']").contains("length([Title])");
-    });
-  });
-
   it("should correctly insert function suggestion with the opening parenthesis", () => {
     openProductsTable({ mode: "notebook" });
     cy.findByText("Custom column").click();
