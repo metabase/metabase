eb20022b80f560e8a30377a7a80c5e83523fc3a6
Alexander Kiselev <alexander.kiselev@metabase.com>
2021-10-06 20:17:40 -0700

    Kitchen sink ran away without the IconBorder TS update

--- frontend/src/metabase/components/IconBorder.tsx
+++ frontend/src/metabase/components/IconBorder.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import PropTypes, { InferProps } from "prop-types";
 import cx from "classnames";
 
 /*
@@ -13,49 +13,39 @@ import cx from "classnames";
    </IconBorder>
  */
 
-export default class IconBorder extends Component {
-  static propTypes = {
-    borderWidth: PropTypes.string,
-    borderStyle: PropTypes.string,
-    borderColor: PropTypes.string,
-    borderRadius: PropTypes.string,
-    style: PropTypes.object,
-    children: PropTypes.any.isRequired,
+const propTypes = {
+  className: PropTypes.string,
+  borderWidth: PropTypes.string,
+  borderStyle: PropTypes.string,
+  borderColor: PropTypes.string,
+  borderRadius: PropTypes.string,
+  style: PropTypes.object,
+  children: PropTypes.any.isRequired,
+};
+
+const IconBorder: React.FC<InferProps<typeof propTypes>> = ({
+  borderWidth = "1px",
+  borderStyle = "solid",
+  borderColor = "currentcolor",
+  borderRadius = "99px",
+  className,
+  style = {},
+  children,
+}) => {
+  const size = parseInt(children.props.size || children.props.width, 10) * 2;
+  const styles = {
+    width: size,
+    height: size,
+    borderWidth: borderWidth,
+    borderStyle: borderStyle,
+    borderColor: borderColor,
+    borderRadius: borderRadius,
+    ...style,
   };
 
-  static defaultProps = {
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: "currentcolor",
-    borderRadius: "99px",
-    style: {},
-  };
-
-  render() {
-    const {
-      borderWidth,
-      borderStyle,
-      borderColor,
-      borderRadius,
-      className,
-      style,
-      children,
-    } = this.props;
-    const size = parseInt(children.props.size || children.props.width, 10) * 2;
-    const styles = {
-      width: size,
-      height: size,
-      borderWidth: borderWidth,
-      borderStyle: borderStyle,
-      borderColor: borderColor,
-      borderRadius: borderRadius,
-      ...style,
-    };
-
-    return (
-      <div className={cx("flex layout-centered", className)} style={styles}>
-        {children}
-      </div>
-    );
-  }
+  return (
+    <div className={cx("flex layout-centered", className)} style={styles}>
+      {children}
+    </div>
+  );
 }
