0d5e18077005ea93833ee975077a9f78069228f9
Gustavo Saiani <gustavo@poe.ma>
2021-10-08 09:24:54 -0300

    Back away from treating in the widget

--- frontend/src/metabase/components/form/widgets/FormTextFileWidget.jsx
+++ frontend/src/metabase/components/form/widgets/FormTextFileWidget.jsx
@@ -3,16 +3,18 @@ import React from "react";
 import cx from "classnames";
 import { formDomOnlyProps } from "metabase/lib/redux";
 
-const FormTextFileWidget = ({ field, treatBeforePosting }) => {
-  // setting value on a file input throws an error
-  const { value, ...otherProps } = formDomOnlyProps(field); // eslint-disable-line no-unused-vars
+// This is a special-case widget
+// setting value on a file input widget throws an error
+
+const FormTextFileWidget = ({ field }) => {
+  const { value, ...otherProps } = formDomOnlyProps(field);
   return (
     <input
       type="file"
       className={cx({ "Form-file-input--has-value": value }, "Form-file-input")}
       aria-labelledby={`${field.name}-label`}
       {...otherProps}
-      onChange={wrapHandler(field.onChange, treatBeforePosting)}
+      onChange={wrapHandler(field.onChange)}
       onBlur={wrapHandler(field.onBlur)}
     />
   );
@@ -25,21 +27,10 @@ function wrapHandler(h, treatBeforePosting) {
     }
     const fr = new FileReader();
     fr.onload = () => {
-      const text = treatOnLoadValue(fr.result, treatBeforePosting);
-      console.log("🚀", { text });
-      return h(text);
+      return h(fr.result);
     };
     fr.readAsText(files[0]);
   };
-}
-
-function treatOnLoadValue(value, treatment) {
-  console.log("🚀", { value, treatment });
-  if (treatment === "base64") {
-    return btoa(value);
-  }
-
-  return value;
 }
 
 export default FormTextFileWidget;
