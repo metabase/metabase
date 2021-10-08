d657ebb663bccf4b3c0cefd82020dc7ac8181c6c
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-08 16:55:26 +0200

    Extract "custom column" into a separate e2e and CI group (#18336)
    * Extract cc reproductions from `question` group
    
    * Extract custom-column spec
    
    * Extract custom column help-text spec
    
    * Extract custom column typing-suggestion spec
    
    * Extract custom column error-feedback spec
    
    * Expand typing-suggestions spec
    
    * Consolidate repros
    
    * Extract custom column data-type spec
    
    * Remove references to `cy.server` from custom-column spec
    
    * Extract custom column expression-editor spec
    
    * Remove duplicate tests
    
    * Extract and unskip repro for #13289
    
    * Register custom column helpers
    
    * Use CC helper in tests
    
    * Move related tests to help-text spec
    
    * Enable "custom-column" e2e group in CI
    
    * Delete a duplicate repro for #13289
    
    * Remove obsolete test
    
    * Expand error-feedback spec
    
    * Extract and expand repro for #14843
    
    * Fix linter errors

---
+++ frontend/test/metabase/scenarios/custom-column/cc-data-type.cy.spec.js
@@ -0,0 +1,98 @@
+import {
+  restore,
+  openTable,
+  popover,
+  enterCustomColumnDetails,
+} from "__support__/e2e/cypress";
+
+import { SAMPLE_DATASET } from "__support__/e2e/cypress_sample_dataset";
+
+const { ORDERS_ID, PEOPLE_ID, PRODUCTS_ID } = SAMPLE_DATASET;
+
+describe("scenarios > question > custom column > data type", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("should understand string functions (metabase#13217)", () => {
+    openCustomColumnInTable(PRODUCTS_ID);
+
+    enterCustomColumnDetails({
+      formula: "concat([Category], [Title])",
+      name: "CategoryTitle",
+    });
+
+    cy.button("Done").click();
+
+    cy.findByText("Filter").click();
+    popover()
+      .findByText("CategoryTitle")
+      .click();
+
+    cy.findByPlaceholderText("Enter a number").should("not.exist");
+    cy.findByPlaceholderText("Enter some text");
+  });
+
+  it("should relay the type of a date field", () => {
+    openCustomColumnInTable(PEOPLE_ID);
+
+    enterCustomColumnDetails({ formula: "[Birth Date]", name: "DoB" });
+    cy.button("Done").click();
+
+    cy.findByText("Filter").click();
+    popover()
+      .findByText("DoB")
+      .click();
+
+    cy.findByPlaceholderText("Enter a number").should("not.exist");
+
+    cy.findByText("Previous");
+    cy.findByText("Days");
+  });
+
+  it("should handle CASE (metabase#13122)", () => {
+    openCustomColumnInTable(ORDERS_ID);
+
+    enterCustomColumnDetails({
+      formula: "case([Discount] > 0, [Created At], [Product → Created At])",
+      name: "MiscDate",
+    });
+    cy.button("Done").click();
+
+    cy.findByText("Filter").click();
+    popover()
+      .findByText("MiscDate")
+      .click();
+
+    cy.findByPlaceholderText("Enter a number").should("not.exist");
+
+    cy.findByText("Previous");
+    cy.findByText("Days");
+  });
+
+  it("should handle COALESCE", () => {
+    openCustomColumnInTable(ORDERS_ID);
+
+    enterCustomColumnDetails({
+      formula: "COALESCE([Product → Created At], [Created At])",
+      name: "MiscDate",
+    });
+    cy.button("Done").click();
+
+    cy.findByText("Filter").click();
+    popover()
+      .findByText("MiscDate")
+      .click();
+
+    cy.findByPlaceholderText("Enter a number").should("not.exist");
+
+    cy.findByText("Previous");
+    cy.findByText("Days");
+  });
+});
+
+function openCustomColumnInTable(table) {
+  openTable({ table, mode: "notebook" });
+  cy.findByText("Custom column").click();
+}
