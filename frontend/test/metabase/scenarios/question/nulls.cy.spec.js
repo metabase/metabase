880baa0a9539b7886acf159f1bc6700a9d2b4552
Gustavo Saiani <gustavo@poe.ma>
2021-10-15 16:41:29 -0300

    Update question nulls tests

--- frontend/test/metabase/scenarios/question/nulls.cy.spec.js
+++ frontend/test/metabase/scenarios/question/nulls.cy.spec.js
@@ -190,8 +190,12 @@ describe("scenarios > question > null", () => {
       openOrdersTable();
       cy.wait("@dataset");
       cy.contains("Summarize").click();
+
       // remove pre-selected "Count"
-      cy.icon("close").click();
+      cy.findByTestId("sidebar-right").within(() => {
+        cy.icon("close").click();
+      });
+
       // dropdown immediately opens with the new set of metrics to choose from
       popover().within(() => {
         cy.findByText("Cumulative sum of ...").click();
