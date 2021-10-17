6c790ae4fc73b44d5d8efd4dffd2599ddd79804d
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-18 00:40:33 +0200

    Apply new `visualize` helper to a several different specs as a PoC

--- frontend/test/metabase/scenarios/custom-column/custom-column.cy.spec.js
+++ frontend/test/metabase/scenarios/custom-column/custom-column.cy.spec.js
@@ -1,6 +1,7 @@
 import {
   restore,
   popover,
+  visualize,
   openOrdersTable,
   visitQuestionAdhoc,
   enterCustomColumnDetails,
@@ -25,8 +26,7 @@ describe("scenarios > question > custom column", () => {
     enterCustomColumnDetails({ formula: "1 + 1", name: "Math" });
     cy.button("Done").click();
 
-    cy.button("Visualize").click();
-    cy.wait("@dataset");
+    visualize();
 
     cy.findByText("There was a problem with your question").should("not.exist");
     cy.get(".Visualization").contains("Math");
@@ -51,8 +51,7 @@ describe("scenarios > question > custom column", () => {
       enterCustomColumnDetails({ formula, name });
       cy.button("Done").click();
 
-      cy.button("Visualize").click();
-      cy.wait("@dataset");
+      visualize();
 
       cy.get(".Visualization").contains(name);
     });
@@ -92,8 +91,7 @@ describe("scenarios > question > custom column", () => {
     });
     cy.button("Done").click();
 
-    cy.button("Visualize").click();
-    cy.wait("@dataset");
+    visualize();
 
     cy.findByText("There was a problem with your question").should("not.exist");
     // This is a pre-save state of the question but the column name should appear
@@ -330,9 +328,10 @@ describe("scenarios > question > custom column", () => {
       .should("not.exist");
     cy.button("Visualize").click();
 
-    cy.wait("@dataset").then(xhr => {
-      expect(xhr.response.body.error).to.not.exist;
+    visualize(response => {
+      expect(response.body.error).to.not.exist;
     });
+
     cy.contains("37.65");
   });
 
@@ -417,10 +416,11 @@ describe("scenarios > question > custom column", () => {
       name: "No discount",
     });
     cy.button("Done").click();
-    cy.button("Visualize").click();
-    cy.wait("@dataset").then(xhr => {
-      expect(xhr.response.body.error).to.not.exist;
+
+    visualize(response => {
+      expect(response.body.error).to.not.exist;
     });
+
     cy.contains("37.65");
     cy.findByText("No discount");
   });
@@ -446,10 +446,11 @@ describe("scenarios > question > custom column", () => {
     cy.findByText("Days").click();
     cy.findByText("Years").click();
     cy.button("Add filter").click();
-    cy.button("Visualize").click();
-    cy.wait("@dataset").then(interception => {
-      expect(interception.response.body.error).not.to.exist;
+
+    visualize(response => {
+      expect(response.body.error).to.not.exist;
     });
+
     cy.findByText("MiscDate");
   });
 });
