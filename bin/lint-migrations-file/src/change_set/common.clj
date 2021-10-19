21489adb243763b097a0242f104635194152eb16
dpsutton <dan@dpsutton.com>
2021-10-19 23:12:35 +0200

    Also validate the `type` field of the `dbms` precondition

--- bin/lint-migrations-file/src/change_set/common.clj
+++ bin/lint-migrations-file/src/change_set/common.clj
@@ -1,5 +1,6 @@
 (ns change-set.common
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.string :as str]))
 
 (s/def ::id
   (s/or
@@ -22,6 +23,14 @@
 
 (s/def ::dbms
   (s/keys :req-un [::type]))
+
+(s/def ::type (s/and string? ::valid-dbs))
+
+(s/def ::valid-dbs
+  (fn [s]
+    (let [dbs (into #{} (map str/trim) (str/split s #","))]
+      (and (seq dbs)
+           (every? #{"h2" "mysql" "mariadb" "postgresql"} dbs)))))
 
 (s/def ::change-set
   (s/keys :req-un [::id ::author]
