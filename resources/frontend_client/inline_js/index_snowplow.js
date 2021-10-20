52d7914fe3a2593da95a55282f7c8bfa80e0aff1
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-10-20 18:51:10 +0300

    Refactor analytics

--- resources/frontend_client/inline_js/index_snowplow.js
+++ resources/frontend_client/inline_js/index_snowplow.js
@@ -1,18 +1,4 @@
-(function(p, l, o, w, i, n, g) {
-  if (!p[i]) {
-    p.GlobalSnowplowNamespace = p.GlobalSnowplowNamespace || [];
-    p.GlobalSnowplowNamespace.push(i);
-    p[i] = function() {
-      (p[i].q = p[i].q || []).push(arguments);
-    };
-    p[i].q = p[i].q || [];
-    n = l.createElement(o);
-    g = l.getElementsByTagName(o)[0];
-    n.async = 1;
-    n.src = w;
-    g.parentNode.insertBefore(n, g);
-  }
-})(window, document, "script", "/app/dist/sp.lite.js", "snowplow");
+(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[]; p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments) };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1; n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","/app/dist/sp.lite.js","snowplow"));
 
 window.snowplow("newTracker", "sp", "https://sp.metabase.com", {
   appId: "metabase",
