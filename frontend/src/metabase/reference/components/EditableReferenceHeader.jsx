ace1023454bb0f2a11842bc89367418ba759901c
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-10-20 16:01:03 +0300

    Remove data-metabase-event

--- frontend/src/metabase/reference/components/EditableReferenceHeader.jsx
+++ frontend/src/metabase/reference/components/EditableReferenceHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router";
 import cx from "classnames";
@@ -10,10 +10,10 @@ import Icon from "metabase/components/Icon";
 import InputBlurChange from "metabase/components/InputBlurChange";
 import Ellipsified from "metabase/components/Ellipsified";
 import Button from "metabase/components/Button";
+import MetabaseAnalytics from "metabase/lib/analytics";
 
 const EditableReferenceHeader = ({
   entity = {},
-  table,
   type,
   headerIcon,
   headerLink,
@@ -25,74 +25,80 @@ const EditableReferenceHeader = ({
   startEditing,
   displayNameFormField,
   nameFormField,
-}) => (
-  <div className="wrapper">
-    <div className={cx("relative", L.header)}>
-      <div className="flex align-center mr1">
-        {headerIcon && (
-          <Icon className="text-light" name={headerIcon} size={21} />
+}) => {
+  const handleLinkClick = useCallback(() => {
+    MetabaseAnalytics.trackEvent("Data Reference", "Entity -> QB click", type);
+  }, [type]);
+
+  return (
+    <div className="wrapper">
+      <div className={cx("relative", L.header)}>
+        <div className="flex align-center mr1">
+          {headerIcon && (
+            <Icon className="text-light" name={headerIcon} size={21} />
+          )}
+        </div>
+        {type === "table" && !hasSingleSchema && !isEditing && (
+          <div className={S.headerSchema}>{entity.schema}</div>
         )}
-      </div>
-      {type === "table" && !hasSingleSchema && !isEditing && (
-        <div className={S.headerSchema}>{entity.schema}</div>
-      )}
-      <div
-        className={S.headerBody}
-        style={
-          isEditing && name === "Details" ? { alignItems: "flex-start" } : {}
-        }
-      >
-        {isEditing && name === "Details" ? (
-          <InputBlurChange
-            className={S.headerTextInput}
-            type="text"
-            placeholder={entity.name}
-            onChange={
-              hasDisplayName
-                ? displayNameFormField.onChange
-                : nameFormField.onChange
-            }
-            defaultValue={hasDisplayName ? entity.display_name : entity.name}
-          />
-        ) : (
-          [
-            <Ellipsified
-              key="1"
-              className={!headerLink && "flex-full"}
-              tooltipMaxWidth="100%"
-            >
-              {name === "Details"
-                ? hasDisplayName
-                  ? entity.display_name || entity.name
-                  : entity.name
-                : name}
-            </Ellipsified>,
-            headerLink && (
-              <Button
-                primary
-                className="flex flex-align-right mr2"
-                style={{ fontSize: 14 }}
-                data-metabase-event={`Data Reference;Entity -> QB click;${type}`}
+        <div
+          className={S.headerBody}
+          style={
+            isEditing && name === "Details" ? { alignItems: "flex-start" } : {}
+          }
+        >
+          {isEditing && name === "Details" ? (
+            <InputBlurChange
+              className={S.headerTextInput}
+              type="text"
+              placeholder={entity.name}
+              onChange={
+                hasDisplayName
+                  ? displayNameFormField.onChange
+                  : nameFormField.onChange
+              }
+              defaultValue={hasDisplayName ? entity.display_name : entity.name}
+            />
+          ) : (
+            [
+              <Ellipsified
+                key="1"
+                className={!headerLink && "flex-full"}
+                tooltipMaxWidth="100%"
               >
-                <Link to={headerLink}>{t`See this ${type}`}</Link>
-              </Button>
-            ),
-          ]
-        )}
-        {user && user.is_superuser && !isEditing && (
-          <Button
-            secondary
-            icon="pencil"
-            style={{ fontSize: 14 }}
-            onClick={startEditing}
-          >
-            {t`Edit`}
-          </Button>
-        )}
+                {name === "Details"
+                  ? hasDisplayName
+                    ? entity.display_name || entity.name
+                    : entity.name
+                  : name}
+              </Ellipsified>,
+              headerLink && (
+                <Button
+                  primary
+                  className="flex flex-align-right mr2"
+                  style={{ fontSize: 14 }}
+                  onClick={handleLinkClick}
+                >
+                  <Link to={headerLink}>{t`See this ${type}`}</Link>
+                </Button>
+              ),
+            ]
+          )}
+          {user && user.is_superuser && !isEditing && (
+            <Button
+              secondary
+              icon="pencil"
+              style={{ fontSize: 14 }}
+              onClick={startEditing}
+            >
+              {t`Edit`}
+            </Button>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 EditableReferenceHeader.propTypes = {
   entity: PropTypes.object,
   table: PropTypes.object,
