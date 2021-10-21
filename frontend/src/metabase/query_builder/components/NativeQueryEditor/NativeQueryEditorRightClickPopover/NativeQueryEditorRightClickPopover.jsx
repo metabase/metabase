e9894ac9d8001c7d7317b20c7f6b71553b59205a
Gustavo Saiani <gustavo@poe.ma>
2021-10-21 20:47:24 -0300

    Extract Icon

--- frontend/src/metabase/query_builder/components/NativeQueryEditor/NativeQueryEditorRightClickPopover/NativeQueryEditorRightClickPopover.jsx
+++ frontend/src/metabase/query_builder/components/NativeQueryEditor/NativeQueryEditorRightClickPopover/NativeQueryEditorRightClickPopover.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { t } from "ttag";
 
-import { Anchor, Container } from "./NativeQueryEditorRightClickPopover.styled";
-import Icon from "metabase/components/Icon";
+import {
+  Anchor,
+  Container,
+  IconStyled as Icon,
+} from "./NativeQueryEditorRightClickPopover.styled";
 import Popover from "metabase/components/Popover";
 
 const propTypes = {
@@ -22,11 +25,11 @@ const NativeQueryEditorRightClickPopover = ({
   <Popover isOpen={isOpen} target={target}>
     <Container>
       <Anchor onClick={runQuery}>
-        <Icon name={"play"} size={16} className="mr1" />
+        <Icon name={"play"} size={16} />
         <h4>{t`Run selection`}</h4>
       </Anchor>
       <Anchor onClick={openSnippetModalWithSelectedText}>
-        <Icon name={"snippet"} size={16} className="mr1" />
+        <Icon name={"snippet"} size={16} />
         <h4>{t`Save as snippet`}</h4>
       </Anchor>
     </Container>
