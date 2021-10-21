2a8beeab82dd1136fae9e37305289ddbf0bbd2bf
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-21 19:47:25 +0200

    Update cell index

--- frontend/test/metabase/scenarios/admin/audit/questions-audit.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/audit/questions-audit.cy.spec.js
@@ -72,7 +72,7 @@ describeWithToken("audit > auditing > questions", () => {
     });
 
     it("should display total runtime correctly (metabase#18317)", () => {
-      const runtimeIndex = 4;
+      const runtimeIndex = 7;
 
       cy.visit("/admin/audit/questions/all");
 
