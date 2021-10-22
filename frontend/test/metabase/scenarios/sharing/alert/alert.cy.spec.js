115375c833f87f23dbffa50d522378a58eb163a7
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-10-22 19:29:52 +0200

    Refactor and enable tests in the `alert` spec

--- frontend/test/metabase/scenarios/sharing/alert/alert.cy.spec.js
+++ frontend/test/metabase/scenarios/sharing/alert/alert.cy.spec.js
@@ -1,15 +1,21 @@
-import { restore, setupLocalHostEmail } from "__support__/e2e/cypress";
+import {
+  restore,
+  setupSMTP,
+  mockSlackConfigured,
+} from "__support__/e2e/cypress";
+
+const channels = { slack: mockSlackConfigured, email: setupSMTP };
 
 describe("scenarios > alert", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
   describe("with nothing set", () => {
-    beforeEach(() => {
-      restore();
-      cy.signInAsAdmin();
-    });
-
     it("should prompt you to add email/slack credentials", () => {
       cy.visit("/question/1");
       cy.icon("bell").click();
+
       cy.findByText(
         "To send alerts, you'll need to set up email or Slack integration.",
       );
@@ -17,40 +23,42 @@ describe("scenarios > alert", () => {
 
     it("should say to non-admins that admin must add email credentials", () => {
       cy.signInAsNormalUser();
+
       cy.visit("/question/1");
       cy.icon("bell").click();
+
       cy.findByText(
         "To send alerts, an admin needs to set up email integration.",
       );
     });
   });
 
-  // [quarantine]: cannot run tests that rely on email setup in CI (yet)
-  describe.skip("educational screen", () => {
-    before(() => {
-      // NOTE: Must run `python -m smtpd -n -c DebuggingServer localhost:1025` before these tests
-      cy.signInAsAdmin();
-      cy.visit("/admin/settings/email");
-      setupLocalHostEmail();
-      cy.server();
-    });
+  Object.entries(channels).forEach(([channel, setup]) => {
+    describe(`with ${channel} set up`, () => {
+      beforeEach(setup);
 
-    it("should show for the first alert, but not the second", () => {
-      // Create first alert
-      cy.visit("/question/1");
-      cy.icon("bell").click();
+      it("educational screen should show for the first alert, but not for the second", () => {
+        // Open the first alert screen and create an alert
+        cy.visit("/question/1");
+        cy.icon("bell").click();
 
-      cy.findByText("The wide world of alerts");
-      cy.contains("When a raw data question returns any results");
+        cy.findByText("The wide world of alerts");
+        cy.findByText("There are a few different kinds of alerts you can get");
 
-      cy.findByText("Set up an alert").click();
-      cy.findByText("Done").click();
+        cy.contains("When a raw data question returns any results");
+        cy.contains("When a line or bar crosses a goal line");
+        cy.contains("When a progress bar reaches its goal");
 
-      // Create second alert
-      cy.visit("/question/1");
-      cy.icon("bell").click();
+        cy.findByText("Set up an alert").click();
+        cy.findByText("Done").click();
 
-      cy.findByText("The wide world of alerts").should("not.exist");
+        // Open the second alert screen
+        cy.visit("/question/2");
+        cy.icon("bell").click();
+
+        cy.findByText("Let's set up your alert");
+        cy.findByText("The wide world of alerts").should("not.exist");
+      });
     });
   });
 });
