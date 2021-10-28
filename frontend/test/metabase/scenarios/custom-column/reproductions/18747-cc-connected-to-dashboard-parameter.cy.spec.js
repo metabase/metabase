a91f366bd763f718472d4f2955151c10c36bc97c
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-10-28 16:40:49 -0700

    add a repro

---
+++ frontend/test/metabase/scenarios/custom-column/reproductions/18747-cc-connected-to-dashboard-parameter.cy.spec.js
@@ -0,0 +1,86 @@
+import { restore, popover } from "__support__/e2e/cypress";
+
+import { SAMPLE_DATASET } from "__support__/e2e/cypress_sample_dataset";
+
+const { ORDERS, ORDERS_ID } = SAMPLE_DATASET;
+
+const questionDetails = {
+  name: "18747",
+  query: {
+    "source-table": ORDERS_ID,
+    expressions: {
+      ["Quantity_2"]: ["field", ORDERS.QUANTITY, null],
+    },
+  },
+};
+
+describe("issue 18747", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+
+    cy.createQuestionAndDashboard({ questionDetails }).then(
+      ({ body: { id, card_id, dashboard_id } }) => {
+        cy.request("PUT", `/api/dashboard/${dashboard_id}`, {
+          cards: [
+            {
+              id,
+              card_id,
+              row: 0,
+              col: 0,
+              sizeX: 12,
+              sizeY: 8,
+            },
+          ],
+        }).then(() => {
+          cy.visit(`/dashboard/${dashboard_id}`);
+        });
+      },
+    );
+  });
+
+  it("should correctly filter the table with a number parameter mapped to the custom column Quantity_2", () => {
+    addNumberParameterToDashboard();
+    mapParameterToCustomColumn();
+
+    cy.contains("Save").click();
+    // wait for saving to finish
+    cy.contains("You're editing this dashboard.").should("not.exist");
+
+    addValueToParameterFilter();
+
+    cy.get(".CardVisualization tbody > tr").should("have.length", 1);
+
+    // check that the parameter value is parsed correctly on page load
+    cy.reload();
+    cy.get(".LoadingSpinner").should("not.exist");
+
+    cy.get(".CardVisualization tbody > tr").should("have.length", 1);
+  });
+});
+
+function addNumberParameterToDashboard() {
+  cy.icon("pencil").click();
+  cy.icon("filter").click();
+  cy.contains("Number").click();
+  cy.findByText("Equal to").click();
+}
+
+function mapParameterToCustomColumn() {
+  cy.get(".DashCard")
+    .contains("Select…")
+    .click();
+  popover()
+    .contains("Quantity_2")
+    .click({ force: true });
+}
+
+function addValueToParameterFilter() {
+  cy.contains("Equal to").click();
+  popover()
+    .find("input")
+    .type("14");
+  popover()
+    .contains("Add filter")
+    .click();
+}
