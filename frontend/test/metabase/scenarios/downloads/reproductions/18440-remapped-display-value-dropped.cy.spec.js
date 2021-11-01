10e4dec3d506702d97017191e67132482e597b87
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2021-11-01 17:56:06 +0100

    Abstract e2e downloads helpers (#18746) (#18780)

--- frontend/test/metabase/scenarios/downloads/reproductions/18440-remapped-display-value-dropped.cy.spec.js
+++ frontend/test/metabase/scenarios/downloads/reproductions/18440-remapped-display-value-dropped.cy.spec.js
@@ -1,26 +1,28 @@
-import { restore, visitQuestionAdhoc } from "__support__/e2e/cypress";
+import {
+  restore,
+  visitQuestionAdhoc,
+  downloadAndAssert,
+} from "__support__/e2e/cypress";
 import { SAMPLE_DATASET } from "__support__/e2e/cypress_sample_dataset";
 
-const xlsx = require("xlsx");
-
 const { ORDERS, ORDERS_ID, PRODUCTS } = SAMPLE_DATASET;
+
+const query = { "source-table": ORDERS_ID, limit: 5 };
 
 const questionDetails = {
   dataset_query: {
     type: "query",
-    query: { "source-table": ORDERS_ID, limit: 5 },
+    query,
     database: 1,
   },
 };
 
-const testCases = [
-  { type: "csv", sheetName: "Sheet1" },
-  { type: "xlsx", sheetName: "Query result" },
-];
+const testCases = ["csv", "xlsx"];
 
 describe("issue 18440", () => {
   beforeEach(() => {
     cy.intercept("POST", "/api/card").as("saveQuestion");
+    cy.intercept("POST", "/api/dataset").as("dataset");
 
     restore();
     cy.signInAsAdmin();
@@ -31,76 +33,36 @@ describe("issue 18440", () => {
       type: "external",
       human_readable_field_id: PRODUCTS.TITLE,
     });
-
-    visitQuestionAdhoc(questionDetails);
   });
 
-  it("export should include a column with remapped values (metabase#18440)", () => {
-    cy.findByText("Product ID");
-    cy.findByText("Awesome Concrete Shoes");
+  testCases.forEach(fileType => {
+    it(`export should include a column with remapped values for ${fileType} (metabase#18440-1)`, () => {
+      visitQuestionAdhoc(questionDetails);
+      cy.wait("@dataset");
 
-    cy.icon("download").click();
+      cy.findByText("Product ID");
+      cy.findByText("Awesome Concrete Shoes");
 
-    cy.wrap(testCases).each(({ type, sheetName }) => {
-      cy.log(`downloading a ${type} file for an unsaved question`);
-
-      const downloadClassName = `.Icon-${type}`;
-      const endpoint = `/api/dataset/${type}`;
-
-      cy.get(downloadClassName)
-        .parent()
-        .parent()
-        .get('input[name="query"]')
-        .invoke("val")
-        .then(download_query_params => {
-          cy.request({
-            url: endpoint,
-            method: "POST",
-            form: true,
-            body: { query: download_query_params },
-            encoding: "binary",
-          }).then(resp => {
-            const workbook = xlsx.read(resp.body, {
-              type: "binary",
-              raw: true,
-            });
-
-            expect(workbook.Sheets[sheetName]["C1"].v).to.eq("Product ID");
-            expect(workbook.Sheets[sheetName]["C2"].v).to.eq(
-              "Awesome Concrete Shoes",
-            );
-          });
-        });
+      downloadAndAssert({ fileType }, assertion);
     });
 
-    // Save the question using UI
-    cy.findByText("Save").click();
-    cy.get(".Modal")
-      .button("Save")
-      .click();
+    it(`export should include a column with remapped values for ${fileType} for a saved question (metabase#18440-2)`, () => {
+      cy.createQuestion({ query }).then(({ body: { id } }) => {
+        cy.intercept("POST", `/api/card/${id}/query`).as("cardQuery");
 
-    cy.wait("@saveQuestion").then(({ response: { body: { id } } }) => {
-      cy.wrap(testCases).each(({ type, sheetName }) => {
-        cy.log(`downloading a ${type} file for a saved question`);
+        cy.visit(`/question/${id}`);
+        cy.wait("@cardQuery");
 
-        const endpoint = `/api/card/${id}/query/${type}`;
+        cy.findByText("Product ID");
+        cy.findByText("Awesome Concrete Shoes");
 
-        cy.request({
-          url: endpoint,
-          method: "POST",
-          encoding: "binary",
-        }).then(resp => {
-          const workbook = xlsx.read(resp.body, {
-            type: "binary",
-            raw: true,
-          });
-
-          expect(workbook.Sheets[sheetName]["C1"].v).to.eq("Product ID");
-          expect(workbook.Sheets[sheetName]["C2"].v).to.eq(
-            "Awesome Concrete Shoes",
-          );
-        });
+        downloadAndAssert({ fileType, questionId: id }, assertion);
       });
     });
   });
 });
+
+function assertion(sheet) {
+  expect(sheet["C1"].v).to.eq("Product ID");
+  expect(sheet["C2"].v).to.eq("Awesome Concrete Shoes");
+}
