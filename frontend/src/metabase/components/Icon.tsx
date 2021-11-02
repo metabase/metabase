817ec7ce9ebeffd81fcdaf9e5b3f3b71b1804603
Anton Kulyk <kuliks.anton@gmail.com>
2021-11-02 12:31:23 +0200

    Fix typo

--- frontend/src/metabase/components/Icon.tsx
+++ frontend/src/metabase/components/Icon.tsx
@@ -28,7 +28,7 @@ export const IconWrapper = styled.div<IconWrapperProps>`
   cursor: pointer;
   color: ${props => (props.open ? c("brand") : "inherit")};
   // special cases for certain icons
-  // Icon-share has a taller viewvbox than most so to optically center
+  // Icon-share has a taller viewbox than most so to optically center
   // the icon we need to translate it upwards
   "> .icon.icon-share": {
     transform: translateY(-2px);
