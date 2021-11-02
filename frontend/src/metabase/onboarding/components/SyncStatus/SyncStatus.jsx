f68626ffd4c5f2f756bac0636929f3ae5b9767b6
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-02 16:55:56 +0300

    Add simple card

--- frontend/src/metabase/onboarding/components/SyncStatus/SyncStatus.jsx
+++ frontend/src/metabase/onboarding/components/SyncStatus/SyncStatus.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { t } from "ttag";
+import Icon from "metabase/components/Icon";
 import {
   DatabaseCard,
   DatabaseContent,
+  DatabaseDescription,
   DatabaseIcon,
   DatabaseTitle,
   SyncStatusRoot,
 } from "./SyncStatus.styled";
-import Icon from "metabase/components/Icon";
 
 const propTypes = {
   databases: PropTypes.array,
@@ -25,6 +27,9 @@ const SyncStatus = ({ databases }) => {
             <DatabaseTitle>
               {database.display_name || database.name}
             </DatabaseTitle>
+            <DatabaseDescription>
+              {getDatabaseDescription(database)}
+            </DatabaseDescription>
           </DatabaseContent>
         </DatabaseCard>
       ))}
@@ -33,5 +38,12 @@ const SyncStatus = ({ databases }) => {
 };
 
 SyncStatus.propTypes = propTypes;
+
+const getDatabaseDescription = ({ tables }) => {
+  const doneCount = tables.filter(t => t.initial_sync).length;
+  const totalCount = tables.length;
+
+  return t`${doneCount} of ${totalCount} done`;
+};
 
 export default SyncStatus;
--- frontend/src/metabase/onboarding/components/SyncStatus/SyncStatus.styled.jsx
+++ frontend/src/metabase/onboarding/components/SyncStatus/SyncStatus.styled.jsx
@@ -7,6 +7,7 @@ export const SyncStatusRoot = styled.div`
   bottom: 1.5rem;
   border-radius: 6px;
   background-color: ${color("white")};
+  box-shadow: 0 1px 12px ${color("shadow")};
 `;
 
 export const DatabaseCard = styled.div`
@@ -20,11 +21,6 @@ export const DatabaseContent = styled.div`
   margin: 0 0.75rem;
 `;
 
-export const DatabaseTitle = styled.div`
-  color: ${color("text-dark")};
-  font-weight: 700;
-`;
-
 export const DatabaseIcon = styled.div`
   display: flex;
   align-items: center;
@@ -34,4 +30,19 @@ export const DatabaseIcon = styled.div`
   border-radius: 1rem;
   color: ${color("brand")};
   background-color: ${color("brand-light")};
+`;
+
+export const DatabaseTitle = styled.div`
+  color: ${color("text-dark")};
+  font-size: 0.875rem;
+  font-weight: bold;
+  line-height: 1rem;
+`;
+
+export const DatabaseDescription = styled.div`
+  color: ${color("bg-dark")};
+  font-size: 0.6875rem;
+  font-weight: bold;
+  line-height: 0.8125rem;
+  margin-top: 0.25rem;
 `;
