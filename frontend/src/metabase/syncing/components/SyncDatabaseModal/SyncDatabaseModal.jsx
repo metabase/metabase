26ee475634df6a6e52c90c2f7394ce53c06ca557
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-03 22:23:49 +0300

    Add special case for sample dataset exploration

--- frontend/src/metabase/syncing/components/SyncDatabaseModal/SyncDatabaseModal.jsx
+++ frontend/src/metabase/syncing/components/SyncDatabaseModal/SyncDatabaseModal.jsx
@@ -6,26 +6,43 @@ import Link from "metabase/components/Link";
 import ModalContent from "metabase/components/ModalContent";
 
 const propTypes = {
+  sampleDatabase: PropTypes.object,
   xraysEnabled: PropTypes.bool,
   onClose: PropTypes.func,
 };
 
-const SyncDatabaseModal = ({ xraysEnabled, onClose }) => {
+const SyncDatabaseModal = ({ sampleDatabase, xraysEnabled, onClose }) => {
   return (
     <ModalContent
       title={t`Great, we're taking a look at your database!`}
       footer={
-        <Link to={xraysEnabled ? "/explore/1" : "/"}>
-          <Button primary>{t`Explore sample data`}</Button>
-        </Link>
+        sampleDatabase ? (
+          <Link to={xraysEnabled ? `/explore/${sampleDatabase.id}` : "/"}>
+            <Button primary>{t`Explore sample data`}</Button>
+          </Link>
+        ) : (
+          <Link to="/">
+            <Button primary>{t`Explore your Metabase`}</Button>
+          </Link>
+        )
       }
       onClose={onClose}
     >
       <div>
-        {t`You’ll be able to use individual tables as they finish syncing. `}
-        {jt`You can also explore our ${(
-          <strong>{t`Sample Dataset`}</strong>
-        )} in the meantime if you want to get a head start.`}
+        <span>
+          {t`You’ll be able to use individual tables as they finish syncing. `}
+        </span>
+        {sampleDatabase ? (
+          <span>
+            {jt`You can also explore our ${(
+              <strong>{sampleDatabase.name}</strong>
+            )} in the meantime if you want to get a head start.`}
+          </span>
+        ) : (
+          <span>
+            {t`Have a look around your Metabase in the meantime if you want to get a head start.`}
+          </span>
+        )}
       </div>
     </ModalContent>
   );
--- frontend/src/metabase/syncing/containers/SyncDatabaseModal/SyncDatabaseModal.jsx
+++ frontend/src/metabase/syncing/containers/SyncDatabaseModal/SyncDatabaseModal.jsx
@@ -1,9 +1,15 @@
 import { connect } from "react-redux";
+import _ from "underscore";
 import Settings from "metabase/lib/settings";
 import SyncDatabaseModal from "../../components/SyncDatabaseModal";
+import Databases from "metabase/entities/databases";
 
-const mapStateToProps = () => ({
+const mapStateToProps = (state, { databases }) => ({
+  sampleDatabase: databases.find(d => d.is_sample),
   xraysEnabled: Settings.get("enable-xrays"),
 });
 
-export default connect(mapStateToProps)(SyncDatabaseModal);
+export default _.compose(
+  Databases.loadList(),
+  connect(mapStateToProps),
+)(SyncDatabaseModal);
