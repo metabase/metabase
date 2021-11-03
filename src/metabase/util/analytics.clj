95b1471193f7e4791dd6692d75b90e0b6a35ed0e
Noah Moss <noahbmoss@gmail.com>
2021-11-03 18:36:47 -0400

    add subject and context

--- src/metabase/util/analytics.clj
+++ src/metabase/util/analytics.clj
@@ -1,6 +1,10 @@
 (ns metabase.util.analytics
   "Functions for sending Snowplow analytics events"
-  (:import [com.snowplowanalytics.snowplow.tracker Tracker Tracker$TrackerBuilder]
+  (:require [metabase.models.setting :refer [defsetting]]
+            [metabase.util.i18n :as i18n :refer [available-locales-with-names deferred-tru trs tru]]
+            [metabase.public-settings :as public-settings]
+            [metabase.api.common :as api])
+  (:import [com.snowplowanalytics.snowplow.tracker Subject$SubjectBuilder Tracker Tracker$TrackerBuilder]
            [com.snowplowanalytics.snowplow.tracker.emitter BatchEmitter Emitter]
            com.snowplowanalytics.snowplow.tracker.http.ApacheHttpClientAdapter
            org.apache.http.impl.client.HttpClients
@@ -8,33 +12,61 @@
            com.snowplowanalytics.snowplow.tracker.payload.SelfDescribingJson
            com.snowplowanalytics.snowplow.tracker.events.Unstructured))
 
+(defsetting snowplow-url
+  (deferred-tru "The URL of the Snowplow collector to send analytics events to")
+  :default "http://localhost:9095";"https://sp.metabase.com"
+  :visibility :public)
+
 (def ^:private ^Emitter emitter
   "An instance of a Snowplow emitter"
-  (let [manager (new PoolingHttpClientConnectionManager)
-        client  (-> (. HttpClients custom)
-                    (.setConnectionManager manager)
-                    (.build))
-        adapter (-> (. ApacheHttpClientAdapter builder)
-                    (.url "http://localhost:9095")
-                    (.httpClient client)
-                    (.build))]
-    (-> (. BatchEmitter builder)
-        (.httpClientAdapter adapter)
-        (.bufferSize 1) ;; TODO bump buffer size back to 5?
-        (.build))))
+  (delay
+   (let [manager (new PoolingHttpClientConnectionManager)
+         client  (-> (. HttpClients custom)
+                     (.setConnectionManager manager)
+                     (.build))
+         adapter (-> (. ApacheHttpClientAdapter builder)
+                     (.url (snowplow-url))
+                     (.httpClient client)
+                     (.build))]
+     (-> (. BatchEmitter builder)
+         (.httpClientAdapter adapter)
+         (.bufferSize 1) ;; TODO bump buffer size back to 5?
+         (.build)))))
 
 (def ^:private ^Tracker tracker
   "An instance of a Snowplow tracker"
-  (-> (Tracker$TrackerBuilder. emitter "sp" "metabase")
-    (.build)))
+  (delay
+   (-> (Tracker$TrackerBuilder. @emitter "sp" "metabase")
+       (.build))))
+
+(defn- subject
+  "Create a Subject object for a given user ID, to be included in analytics events"
+  [user-id]
+  (-> (Subject$SubjectBuilder.)
+      (.userId (str user-id))
+      (.build)))
 
 (defn- payload
-  []
-  (let [event-data (doto (new java.util.HashMap)
-                     (.put "event" "tracking_permission_enabled")
-                     (.put "source" "setup"))]
-    (new SelfDescribingJson "iglu:com.metabase/settings/jsonschema/1-0-0" event-data)))
+  [event-data]
+  ; (let [event-data {"event" "tracking_permission_enabled",
+  ;                   "source" "setup"}]
+  (new SelfDescribingJson
+       "iglu:com.metabase/settings/jsonschema/1-0-0"
+       ;; Make sure keys are strings
+       (into {} (for [[k v] event-data] [(name k) v]))))
 
-(comment (.track tracker (-> (. Unstructured builder)
-                             (.eventData (payload))
-                             (.build))))
+(defn- context
+  "Common context included in every analytics event"
+  []
+  (new SelfDescribingJson
+       "iglu:com.metabase/instance/jsonschema/1-0-0"
+       {"id"             (public-settings/analytics-uuid),
+        "version"        {"tag" (:tag (public-settings/version))},
+        "token-features" (into {} (for [[token enabled?] (public-settings/token-features)]
+                                    [(name token) enabled?]))}))
+
+(comment (.track @tracker (-> (. Unstructured builder)
+                              (.subject (subject 1))
+                              (.eventData (payload))
+                              (.customContext [(context)])
+                              (.build))))
