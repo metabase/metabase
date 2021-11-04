b1a625758124f33e92ee060e07b80e894c128c17
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-04 21:15:44 +0300

    Add unit tests

--- frontend/src/metabase/syncing/components/SyncModal/SyncModal.unit.spec.js
+++ frontend/src/metabase/syncing/components/SyncModal/SyncModal.unit.spec.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { SyncModal } from "./SyncModal";
 
 describe("SyncModal", () => {
   const database = { id: 1, name: "Sample Dataset" };
 
   it("should render with sample dataset and xrays enabled", () => {
-    render(<SyncModal sampleDatabase={database}  />)
-  })
+    render(<SyncModal sampleDatabase={database} xraysEnabled />);
+
+    expect(screen.getByText("Explore sample data")).toBeInTheDocument();
+  });
+
+  it("should render with no sample dataset but with xrays enabled", () => {
+    render(<SyncModal xraysEnabled />);
+
+    expect(screen.getByText("Explore your Metabase")).toBeInTheDocument();
+  });
 });
