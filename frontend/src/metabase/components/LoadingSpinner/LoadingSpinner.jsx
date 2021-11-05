14177c7b37ef25fc159edfa65ebd2b6628fa8e28
Ariya Hidayat <ariya@metabase.com>
2021-11-05 06:49:31 -0700

    Static hourglass indicator when reduced motion is preferred (#18847)
    The rotating spinner is replaced by a static hourglass icon.

--- frontend/src/metabase/components/LoadingSpinner/LoadingSpinner.jsx
+++ frontend/src/metabase/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+import { isReducedMotionPreferred } from "metabase/lib/dom";
+
+import Icon from "metabase/components/Icon";
 import { SpinnerIcon, SpinnerRoot } from "./LoadingSpinner.styled";
 
 const propTypes = {
@@ -10,7 +14,11 @@ const propTypes = {
 
 const LoadingSpinner = ({ className, size = 32, borderWidth = 4 }) => (
   <SpinnerRoot className={className} data-testid="loading-spinner">
-    <SpinnerIcon iconSize={size} borderWidth={borderWidth} />
+    {isReducedMotionPreferred() ? (
+      <Icon name="hourglass" size="24" />
+    ) : (
+      <SpinnerIcon iconSize={size} borderWidth={borderWidth} />
+    )}
   </SpinnerRoot>
 );
 
