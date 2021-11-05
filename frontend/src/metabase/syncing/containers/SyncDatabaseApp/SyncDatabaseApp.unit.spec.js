752f9e9a726e4422b474458488d3ea673275391b
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-05 17:19:17 +0300

    Add unit tests

---
+++ frontend/src/metabase/syncing/containers/SyncDatabaseApp/SyncDatabaseApp.unit.spec.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { SyncDatabaseApp } from "./SyncDatabaseApp";
+
+const SyncModalSwitch = () => <div>SyncModalSwitch</div>;
+const SyncSnackbarSwitch = () => <div>SyncSnackbarSwitch</div>;
+
+jest.mock("../../components/SyncModalSwitch", () => SyncModalSwitch);
+jest.mock("../../components/SyncSnackbarSwitch", () => SyncSnackbarSwitch);
+
+describe("SyncDatabaseApp", () => {
+  it("should display syncing indicators for an dmin user", () => {
+    render(<SyncDatabaseApp isAdmin={true} />);
+
+    expect(screen.queryByText("SyncModalSwitch")).toBeInTheDocument();
+    expect(screen.queryByText("SyncSnackbarSwitch")).toBeInTheDocument();
+  });
+
+  it("should not display syncing indicators for a non-admin user", () => {
+    render(<SyncDatabaseApp isAdmin={false} />);
+
+    expect(screen.queryByText("SyncModalSwitch")).not.toBeInTheDocument();
+    expect(screen.queryByText("SyncSnackbarSwitch")).not.toBeInTheDocument();
+  });
+});
