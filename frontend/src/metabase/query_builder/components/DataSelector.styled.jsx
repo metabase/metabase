61d8b9824e1464fa37dbaf070834fe8c45c99a7c
Anton Kulyk <kuliks.anton@gmail.com>
2021-11-05 14:39:48 +0200

    Switch to custom SelectList for data bucket step

--- frontend/src/metabase/query_builder/components/DataSelector.styled.jsx
+++ frontend/src/metabase/query_builder/components/DataSelector.styled.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { t } from "ttag";
 import styled from "styled-components";
 import Icon from "metabase/components/Icon";
+import { SelectList } from "metabase/components/select-list";
 import { color } from "metabase/lib/colors";
 import { space } from "metabase/styled-components/theme";
 
@@ -43,5 +44,69 @@ export function RawDataBackButton() {
       <Icon name="chevronleft" size={16} />
       <BackButtonLabel>{t`Raw Data`}</BackButtonLabel>
     </BackButtonContainer>
+  );
+}
+
+export const DataBucketList = styled(SelectList)`
+  width: 300px;
+  padding: ${space(0)} ${space(1)} 12px ${space(1)};
+`;
+
+const DataBucketListItemIcon = styled(Icon)`
+  color: ${color("text-dark")};
+`;
+
+const DataBucketTitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const DataBucketListItemTitle = styled.span`
+  color: ${color("text-dark")};
+  font-weight: 700;
+  font-size: 14px;
+  margin-left: ${space(1)};
+`;
+
+const DataBucketListItemDescriptionContainer = styled.div`
+  margin-top: ${space(0)};
+`;
+
+const DataBucketListItemDescription = styled.span`
+  color: ${color("text-light")};
+  font-weight: 700;
+  font-size: 12px;
+`;
+
+const DataBucketListItemContainer = styled(SelectList.BaseItem)`
+  &:hover {
+    ${DataBucketListItemIcon},
+    ${DataBucketListItemTitle},
+    ${DataBucketListItemDescription} {
+      color: ${color("text-white")};
+    }
+  }
+`;
+
+DataBucketListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export function DataBucketListItem(props) {
+  const { name, icon, description } = props;
+  return (
+    <DataBucketListItemContainer {...props}>
+      <DataBucketTitleContainer>
+        <DataBucketListItemIcon name={icon} size={18} />
+        <DataBucketListItemTitle>{name}</DataBucketListItemTitle>
+      </DataBucketTitleContainer>
+      <DataBucketListItemDescriptionContainer>
+        <DataBucketListItemDescription>
+          {description}
+        </DataBucketListItemDescription>
+      </DataBucketListItemDescriptionContainer>
+    </DataBucketListItemContainer>
   );
 }
