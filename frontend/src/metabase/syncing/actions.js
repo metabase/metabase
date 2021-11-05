30680b5c32ca979fb08ed663efc9aa3daf1940cb
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-05 17:57:28 +0300

    Fix tests

--- frontend/src/metabase/syncing/actions.js
+++ frontend/src/metabase/syncing/actions.js
@@ -1,4 +1,5 @@
 import { createThunkAction } from "metabase/lib/redux";
+import { SettingsApi } from "metabase/services";
 import Settings from "metabase/lib/settings";
 
 export const DISABLE_SYNCING_MODAL = "metabase/syncing/DISABLE_SYNCING_MODAL";
@@ -6,8 +7,13 @@ export const DISABLE_SYNCING_MODAL = "metabase/syncing/DISABLE_SYNCING_MODAL";
 export const disableSyncingModal = createThunkAction(
   DISABLE_SYNCING_MODAL,
   () => {
-    return () => {
+    return async () => {
       Settings.set("enable-database-syncing-modal", false);
+
+      await SettingsApi.put({
+        key: "enable-database-syncing-modal",
+        value: false,
+      });
     };
   },
 );
