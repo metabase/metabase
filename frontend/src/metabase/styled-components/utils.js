2b6f7e7807a2c280f719b0ea6afb917f9fd34feb
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-11-06 00:06:25 -0700

    convert styled-components util into typescript

--- frontend/src/metabase/styled-components/utils.js
+++
@@ -1,8 +0,0 @@
-import React, { forwardRef } from "react";
-
-// this function should be removed after we update to styled-components v4
-export function forwardRefToInnerRef(Component) {
-  return forwardRef(function StyledComponentWithInnerRef(props, ref) {
-    return <Component {...props} innerRef={ref} />;
-  });
-}
