450ca5eb3bff6b0d16d0675a35ad2fdeb77d11e6
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-11-05 22:55:09 -0700

    use innerRef util with StoreLink target

--- frontend/src/metabase/nav/components/StoreLink/StoreLink.styled.jsx
+++ frontend/src/metabase/nav/components/StoreLink/StoreLink.styled.jsx
@@ -1,17 +1,16 @@
-import React, { forwardRef } from "react";
 import styled from "styled-components";
+
 import { color, darken } from "metabase/lib/colors";
 import { space } from "metabase/styled-components/theme";
 import Icon, { IconWrapper } from "metabase/components/Icon";
 import ExternalLink from "metabase/components/ExternalLink";
+import { forwardRefToInnerRef } from "metabase/styled-components/utils";
 
-export const StoreIconRoot = forwardRef(function StoreIconRoot(props, ref) {
-  return <StoreIconRootComponent {...props} innerRef={ref} />;
-});
-
-const StoreIconRootComponent = styled(ExternalLink)`
-  margin-right: ${space(1)};
-`;
+export const StoreIconRoot = forwardRefToInnerRef(
+  styled(ExternalLink)`
+    margin-right: ${space(1)};
+  `,
+);
 
 export const StoreIconWrapper = styled(IconWrapper)`
   color: ${color("white")};
