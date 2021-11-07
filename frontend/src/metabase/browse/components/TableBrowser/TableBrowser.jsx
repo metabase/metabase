f5b8eacf1ad3c62b72c7b4a6376fe1f7b457519d
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-07 20:15:46 +0300

    Add component

--- frontend/src/metabase/browse/components/TableBrowser/TableBrowser.jsx
+++ frontend/src/metabase/browse/components/TableBrowser/TableBrowser.jsx
@@ -2,9 +2,97 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { t } from "ttag";
 import { color } from "metabase/lib/colors";
+import * as Urls from "metabase/lib/urls";
+import { SAVED_QUESTIONS_VIRTUAL_DB_ID } from "metabase/lib/saved-questions";
+import Database from "metabase/entities/databases";
+import Card from "metabase/components/Card";
+import EntityItem from "metabase/components/EntityItem";
 import Icon from "metabase/components/Icon";
 import Link from "metabase/components/Link";
-import { ANALYTICS_CONTEXT } from "../../constants";
+import { Grid, GridItem } from "metabase/components/Grid";
+import { ANALYTICS_CONTEXT, ITEM_WIDTHS } from "../../constants";
+import BrowseHeader from "../BrowseHeader";
+import { LinkIcon } from "./TableBrowser.styled";
+
+const propTypes = {
+  tables: PropTypes.array.isRequired,
+  databaseId: PropTypes.number,
+  schemaName: PropTypes.string,
+  xraysEnabled: PropTypes.bool,
+  showSchemaInHeader: PropTypes.bool,
+  getTableUrl: PropTypes.func,
+};
+
+const TableBrowser = ({
+  tables,
+  databaseId,
+  schemaName,
+  xraysEnabled,
+  showSchemaInHeader = true,
+  getTableUrl,
+}) => {
+  return (
+    <div>
+      <BrowseHeader
+        crumbs={[
+          { title: t`Our data`, to: "/browse" },
+          getDatabaseCrumbs(databaseId),
+          showSchemaInHeader && { title: schemaName },
+        ]}
+      />
+      <Grid>
+        {tables.map(table => (
+          <GridItem key={table.id} width={ITEM_WIDTHS}>
+            <Card hoverable={table.initial_sync}>
+              <Link
+                to={getTableUrl && table.initial_sync ? getTableUrl(table) : ""}
+                data-metabase-event={`${ANALYTICS_CONTEXT};Table Click`}
+              >
+                <TableBrowserItem
+                  table={table}
+                  databaseId={databaseId}
+                  xraysEnabled={xraysEnabled}
+                />
+              </Link>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+TableBrowser.propTypes = propTypes;
+
+const itemPropTypes = {
+  table: PropTypes.object.isRequired,
+  databaseId: PropTypes.number,
+  xraysEnabled: PropTypes.bool,
+};
+
+const TableBrowserItem = ({ table, databaseId, xraysEnabled }) => {
+  return (
+    <EntityItem
+      item={table}
+      name={table.display_name || table.name}
+      iconName="table"
+      iconColor={color("accent2")}
+      loading={!table.initial_sync}
+      disabled={!table.initial_sync}
+      buttons={
+        table.initial_sync && (
+          <TableBrowserItemButtons
+            tableId={table.id}
+            databaseId={databaseId}
+            xraysEnabled={xraysEnabled}
+          />
+        )
+      }
+    />
+  );
+};
+
+TableBrowserItem.propTypes = itemPropTypes;
 
 const itemButtonsPropTypes = {
   tableId: PropTypes.number,
@@ -16,34 +104,45 @@ const TableBrowserItemButtons = ({ tableId, databaseId, xraysEnabled }) => {
   return (
     <Fragment>
       {xraysEnabled && (
-        <Link
+        <LinkIcon
           to={`/auto/dashboard/table/${tableId}`}
           data-metabase-event={`${ANALYTICS_CONTEXT};Table Item;X-ray Click`}
-          className="link--icon ml1"
         >
           <Icon
             name="bolt"
+            size={20}
             tooltip={t`X-ray this table`}
             color={color("warning")}
-            size={20}
-            className="hover-child"
           />
-        </Link>
+        </LinkIcon>
       )}
-      <Link
+      <LinkIcon
         to={`/reference/databases/${databaseId}/tables/${tableId}`}
         data-metabase-event={`${ANALYTICS_CONTEXT};Table Item;Reference Click`}
-        className="link--icon ml1"
       >
         <Icon
           name="reference"
           tooltip={t`Learn about this table`}
           color={color("text-medium")}
-          className="hover-child"
         />
-      </Link>
+      </LinkIcon>
     </Fragment>
   );
 };
 
 TableBrowserItemButtons.propTypes = itemButtonsPropTypes;
+
+const getDatabaseCrumbs = dbId => {
+  if (dbId === SAVED_QUESTIONS_VIRTUAL_DB_ID) {
+    return {
+      title: t`Saved Questions`,
+      to: Urls.browseDatabase({ id: SAVED_QUESTIONS_VIRTUAL_DB_ID }),
+    };
+  } else {
+    return {
+      title: <Database.Link id={dbId} />,
+    };
+  }
+};
+
+export default TableBrowser;
---
+++ frontend/src/metabase/browse/components/TableBrowser/TableBrowser.styled.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { space } from "metabase/styled-components/theme";
+import Link from "metabase/components/Link";
+
+export const LinkIcon = styled(Link)`
+  line-height: initial;
+  margin-left: ${space(1)};
+`;
