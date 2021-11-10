4ff8259fb7452aee513a42832756d7241b627732
Anton Kulyk <kuliks.anton@gmail.com>
2021-11-10 14:07:50 +0200

    Fix HeaderBreadcrumbs font-size in "head" mode

--- frontend/src/metabase/query_builder/components/view/HeaderBreadcrumbs/HeaderBreadcrumbs.styled.jsx
+++ frontend/src/metabase/query_builder/components/view/HeaderBreadcrumbs/HeaderBreadcrumbs.styled.jsx
@@ -3,23 +3,25 @@ import styled, { css } from "styled-components";
 import Badge from "metabase/components/Badge";
 import { color } from "metabase/lib/colors";
 
-export const Container = styled.span`
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-
-  ${props =>
-    props.variant === "head" &&
-    css`
-      font-size: 1.25rem;
-    `}
-`;
-
 export const HeaderBadge = styled(Badge)`
   .Icon {
     width: 1em;
     height: 1em;
     margin-right: 0.5em;
+  }
+`;
+
+export const Container = styled.span`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  ${HeaderBadge} {
+    ${props =>
+      props.variant === "head" &&
+      css`
+        font-size: 1.25rem;
+      `}
   }
 `;
 
