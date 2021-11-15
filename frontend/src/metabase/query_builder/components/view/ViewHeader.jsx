114bc9d607d41a10b8d616036cd43b3eabce6264
Anton Kulyk <kuliks.anton@gmail.com>
2021-11-15 20:54:30 +0200

    Fix flickering when going from dataset to ad-hoc

--- frontend/src/metabase/query_builder/components/view/ViewHeader.jsx
+++ frontend/src/metabase/query_builder/components/view/ViewHeader.jsx
@@ -263,6 +263,7 @@ function AhHocQuestionLeftSide(props) {
           ) : (
             <QuestionDescription
               question={question}
+              originalQuestion={originalQuestion}
               isObjectDetail={isObjectDetail}
             />
           )}
