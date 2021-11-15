bce75e5e59aa7766c92a78dc88303fe65b5d65e4
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2021-11-15 16:02:24 +0400

    specs

--- frontend/src/metabase/static-viz/lib/waterfall.unit.spec.js
+++ frontend/src/metabase/static-viz/lib/waterfall.unit.spec.js
@@ -6,9 +6,35 @@ const accessors = {
 };
 
 describe("calculateWaterfallEntries", () => {
+  it("calculates waterfall entries without total", () => {
+    const data = [["1", 100], ["2", -50], ["3", 10]];
+    const entries = calculateWaterfallEntries(data, accessors, false);
+
+    expect(entries).toStrictEqual([
+      {
+        start: 0,
+        end: 100,
+        x: "1",
+        y: 100,
+      },
+      {
+        start: 100,
+        end: 50,
+        x: "2",
+        y: -50,
+      },
+      {
+        start: 50,
+        end: 60,
+        x: "3",
+        y: 10,
+      },
+    ]);
+  });
+
   it("calculates waterfall entries with positive total", () => {
     const data = [["1", 100], ["2", -50], ["3", 10]];
-    const entries = calculateWaterfallEntries(data, accessors);
+    const entries = calculateWaterfallEntries(data, accessors, true);
 
     expect(entries).toStrictEqual([
       {
@@ -41,7 +67,7 @@ describe("calculateWaterfallEntries", () => {
 
   it("calculates waterfall entries with negative total", () => {
     const data = [["1", 100], ["2", -200], ["3", 50]];
-    const entries = calculateWaterfallEntries(data, accessors);
+    const entries = calculateWaterfallEntries(data, accessors, true);
 
     expect(entries).toStrictEqual([
       {
