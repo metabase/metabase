003fe9cf04754c7917bce670e60213b10e2fe7a9
Maz Ameli <maz@metabase.com>
2021-11-17 14:51:55 -0800

    maz add icons and change tooltip text

--- frontend/src/metabase/collections/components/NewCollectionItemMenu.jsx
+++ frontend/src/metabase/collections/components/NewCollectionItemMenu.jsx
@@ -15,23 +15,26 @@ const propTypes = {
 function NewCollectionItemMenu({ collection }) {
   const items = [
     {
+      icon: "insight",
       title: t`Question`,
       link: Urls.newQuestion({ mode: "notebook", collectionId: collection.id }),
       event: `${ANALYTICS_CONTEXT};New Item Menu;Question Click`,
     },
     {
+      icon: "dashboard",
       title: t`Dashboard`,
       link: Urls.newDashboard(collection.id),
       event: `${ANALYTICS_CONTEXT};New Item Menu;Dashboard Click`,
     },
     {
+      icon: "folder",
       title: t`Collection`,
       link: Urls.newCollection(collection.id),
       event: `${ANALYTICS_CONTEXT};New Item Menu;Collection Click`,
     },
   ];
 
-  return <EntityMenu items={items} triggerIcon="add" tooltip={t`Create`} />;
+  return <EntityMenu items={items} triggerIcon="add" tooltip={t`New…`} />;
 }
 
 NewCollectionItemMenu.propTypes = propTypes;
