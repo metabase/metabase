a32f764a89cf79fd6daf3621271fd1b802fa9a50
Noah Moss <noahbmoss@gmail.com>
2021-11-18 13:37:01 -0500

    actually commit changes

--- frontend/test/metabase/scenarios/admin/settings/sso/google.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/sso/google.cy.spec.js
@@ -8,22 +8,28 @@ describe("scenarios > admin > settings > SSO > Google", () => {
   });
 
   it("Google sign-in client ID should save on subsequent tries (metabase#15974)", () => {
-    cy.findByLabelText("Client ID").type("fake-client-id.apps.googleusercontent.com");
+    cy.findByLabelText("Client ID").type(
+      "fake-client-id.apps.googleusercontent.com",
+    );
     saveSettings();
 
     // This string lingers for far too long in the UI, so we have to wait for it to disappear before we assert on that same button again.
     // Otherwise, the test fails. That's why we added a custom timeout of 6s.
     cy.findByText("Success", { timeout: 6000 }).should("not.exist");
 
-    cy.findByDisplayValue("123").type("fake-client-id2.apps.googleusercontent.com");
+    cy.findByDisplayValue("123").type(
+      "fake-client-id2.apps.googleusercontent.com",
+    );
     saveSettings();
   });
 
-  it("Google sign-in client ID form should show an error message if it does not end with the correct suffix (metabase#15975)")
-    cy.findByLabelText("Client ID").type("fake-client-id");
-    saveSettings();
+  it(
+    "Google sign-in client ID form should show an error message if it does not end with the correct suffix (metabase#15975)",
+  );
+  cy.findByLabelText("Client ID").type("fake-client-id");
+  saveSettings();
 
-    cy.findByText("Invalid Google Sign-In Client ID");
+  cy.findByText("Invalid Google Sign-In Client ID");
 });
 
 function saveSettings() {
