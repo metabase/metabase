268a2ce8881390ff13aa19ebd4ae85266fa6017e
Noah Moss <noahbmoss@gmail.com>
2021-11-18 14:29:53 -0500

    fix cypress syntax

--- frontend/test/metabase/scenarios/admin/settings/sso/google.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/sso/google.cy.spec.js
@@ -25,11 +25,13 @@ describe("scenarios > admin > settings > SSO > Google", () => {
 
   it(
     "Google sign-in client ID form should show an error message if it does not end with the correct suffix (metabase#15975)",
-  );
-  cy.findByLabelText("Client ID").type("fake-client-id");
-  saveSettings();
+  ),
+    () => {
+      cy.findByLabelText("Client ID").type("fake-client-id");
+      saveSettings();
 
-  cy.findByText("Invalid Google Sign-In Client ID");
+      cy.findByText("Invalid Google Sign-In Client ID");
+    };
 });
 
 function saveSettings() {
