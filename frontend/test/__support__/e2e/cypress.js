8efdc6498c75b7265a9b1405edcda43ececd9323
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-23 22:06:14 +0300

    Add snowplow CI (#19040)

--- frontend/test/__support__/e2e/cypress.js
+++ frontend/test/__support__/e2e/cypress.js
@@ -25,6 +25,7 @@ export * from "./helpers/e2e-data-model-helpers";
 export * from "./helpers/e2e-misc-helpers";
 export * from "./helpers/e2e-email-helpers";
 export * from "./helpers/e2e-slack-helpers";
+export * from "./helpers/e2e-snowplow-helpers";
 export * from "./helpers/e2e-custom-column-helpers";
 export * from "./helpers/e2e-dimension-list-helpers";
 export * from "./helpers/e2e-downloads-helpers";
