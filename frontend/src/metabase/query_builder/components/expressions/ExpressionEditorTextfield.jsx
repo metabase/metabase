5a7ce338111d5d2782f553dc2ceb053bf61f446a
Gustavo Saiani <gustavo@poe.ma>
2021-11-23 16:36:31 -0300

    Refresh isFocused state on blur

--- frontend/src/metabase/query_builder/components/expressions/ExpressionEditorTextfield.jsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionEditorTextfield.jsx
@@ -280,8 +280,16 @@ export default class ExpressionEditorTextfield extends React.Component {
     this.setState({ isFocused: true });
   };
 
-  handleEditorBlur = () => {
+  onInputBlur = e => {
     this.setState({ isFocused: false });
+
+    // Switching to another window also triggers the blur event.
+    // When our window gets focus again, the input will automatically
+    // get focus, so ignore the blue event to avoid showing an
+    // error message when the user is not actually done.
+    if (e.target === document.activeElement) {
+      return;
+    }
 
     this.clearSuggestions();
 
