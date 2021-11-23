6d5a60494a7889f045efb14e2c52b33e70afc5d6
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-23 20:25:40 +0300

    Add snowplow docs

--- frontend/test/__support__/e2e/helpers/e2e-snowplow-helpers.js
+++ frontend/test/__support__/e2e/helpers/e2e-snowplow-helpers.js
@@ -1,35 +1,50 @@
-const hasSnowplowMicro = Cypress.env("HAS_SNOWPLOW_MICRO");
-const snowplowMicroUrl = Cypress.env("SNOWPLOW_MICRO_URL");
+const HAS_SNOWPLOW = Cypress.env("HAS_SNOWPLOW_MICRO");
+const SNOWPLOW_URL = Cypress.env("SNOWPLOW_MICRO_URL");
+const SNOWPLOW_INTERVAL = 100;
+const SNOWPLOW_RETRIES = 10;
 
-export const describeWithSnowplow = hasSnowplowMicro ? describe : describe.skip;
+export const describeWithSnowplow = HAS_SNOWPLOW ? describe : describe.skip;
 
 export const resetSnowplow = () => {
-  cy.request({
-    url: `${snowplowMicroUrl}/micro/reset`,
-    json: true,
-  });
+  sendSnowplowRequest("micro/reset");
 };
 
 export const blockSnowplow = () => {
-  cy.intercept("POST", `${snowplowMicroUrl}/*/tp2`, req => {
-    req.destroy();
-  });
+  blockSnowplowRequest("*/tp2");
 };
 
 export const expectGoodSnowplowEvents = count => {
-  cy.request({
-    url: `${snowplowMicroUrl}/micro/good`,
-    json: true,
-  })
+  retrySnowplowRequest("micro/good", ({ body }) => body.length >= count)
     .its("body")
     .should("have.length", count);
 };
 
 export const expectNoBadSnowplowEvents = () => {
-  cy.request({
-    url: `${snowplowMicroUrl}/micro/bad`,
-    json: true,
-  })
+  sendSnowplowRequest("micro/bad")
     .its("body")
-    .should("be.empty");
+    .should("have.length", 0);
+};
+
+const sendSnowplowRequest = url => {
+  return cy.request({
+    url: `${SNOWPLOW_URL}/${url}`,
+    json: true,
+  });
+};
+
+const retrySnowplowRequest = (url, condition, retries = SNOWPLOW_RETRIES) => {
+  return sendSnowplowRequest(url).then(response => {
+    if (condition(response)) {
+      return cy.wrap(response);
+    } else if (retries > 0) {
+      cy.wait(SNOWPLOW_INTERVAL);
+      return retrySnowplowRequest(url, condition, retries - 1);
+    } else {
+      throw new Error(response);
+    }
+  });
+};
+
+const blockSnowplowRequest = url => {
+  return cy.intercept("POST", `${SNOWPLOW_URL}/${url}`, req => req.destroy());
 };
