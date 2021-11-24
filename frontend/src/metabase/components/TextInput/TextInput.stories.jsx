077b9fc523ca3310946858d6fb52ba8b40dbe593
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-11-24 14:20:32 +0300

    Add stories

---
+++ frontend/src/metabase/components/TextInput/TextInput.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TextInput from "./TextInput";
+
+export default {
+  title: "Components/TextInput",
+  component: TextInput,
+};
+
+export const Default = () => <TextInput />;
