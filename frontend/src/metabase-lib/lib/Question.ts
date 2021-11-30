25ad0270e72ae8c230284ac500946c06f49f1573
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-11-29 22:11:01 -0800

    fix title drill navigating to new question

--- frontend/src/metabase-lib/lib/Question.ts
+++ frontend/src/metabase-lib/lib/Question.ts
@@ -1221,7 +1221,7 @@ export default class Question {
 
   getUrlWithParameters() {
     const question = this.query().isEditable()
-      ? this.convertParametersToFilters().markDirty()
+      ? this.convertParametersToFilters()
       : this.markDirty();
 
     let parameterValues;
@@ -1233,10 +1233,12 @@ export default class Question {
     }
 
     delete question._parameterValues;
-    delete this._parameterValues;
+
+    const originalQuestion = this.setParameters([]);
+    delete originalQuestion._parameterValues;
 
     return question.getUrl({
-      originalQuestion: this,
+      originalQuestion,
       query: parameterValues,
       includeDisplayIsLocked: true,
     });
