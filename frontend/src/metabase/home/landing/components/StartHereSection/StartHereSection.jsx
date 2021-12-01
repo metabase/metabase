3873c4c5729bdb370d5bef2763f81b512b4d0dcc
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-01 16:48:04 +0300

    Add start here section

--- frontend/src/metabase/home/landing/components/StartHereSection/StartHereSection.jsx
+++ frontend/src/metabase/home/landing/components/StartHereSection/StartHereSection.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { t } from "ttag";
+import { t, jt } from "ttag";
+import Settings from "metabase/lib/settings";
+import * as Urls from "metabase/lib/urls";
+import Link from "metabase/components/Link";
 import Section, { SectionHeader, SectionTitle } from "../LandingSection";
+import {
+  BannerContent,
+  BannerDescription,
+  BannerIcon,
+  BannerIconContainer,
+  BannerRoot,
+  BannerTitle,
+} from "./StartHereSection.styled";
+import ExternalLink from "metabase/components/ExternalLink";
 
 const propTypes = {
   isAdmin: PropTypes.bool,
@@ -14,10 +26,36 @@ const StartHereSection = () => {
       <SectionHeader>
         <SectionTitle>{t`Start here`}</SectionTitle>
       </SectionHeader>
+      <DatabaseBanner />
     </Section>
   );
 };
 
 StartHereSection.propTypes = propTypes;
+
+const DatabaseBanner = () => {
+  const userLink = Urls.newUser();
+  const docsLink = Settings.docsUrl("getting-started");
+
+  return (
+    <BannerRoot>
+      <BannerIconContainer>
+        <BannerIcon name="database" />
+      </BannerIconContainer>
+      <BannerContent>
+        <BannerTitle>{t`Connect your data to get the most out of Metabase`}</BannerTitle>
+        <BannerDescription>
+          {jt`If you need help, you can ${(
+            <Link to={userLink}>{t`invite a teammate`}</Link>
+          )} or ${(
+            <ExternalLink href={docsLink}>
+              {t`check out our set up guides`}
+            </ExternalLink>
+          )}.`}
+        </BannerDescription>
+      </BannerContent>
+    </BannerRoot>
+  );
+};
 
 export default StartHereSection;
---
+++ frontend/src/metabase/home/landing/components/StartHereSection/StartHereSection.styled.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { color } from "metabase/lib/colors";
+import Icon from "metabase/components/Icon";
+
+export const BannerRoot = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1.5rem;
+  border: 1px solid ${color("border")};
+  border-radius: 0.25rem;
+  background-color: ${color("white")};
+`;
+
+export const BannerIconContainer = styled.div`
+  display: flex;
+  flex: 0 0 auto;
+  justify-content: center;
+  align-items: center;
+  width: 2.5rem;
+  height: 2.5rem;
+  border: 1px solid ${color("border")};
+  border-radius: 50%;
+`;
+
+export const BannerIcon = styled(Icon)`
+  display: block;
+  color: ${color("brand")};
+  width: 1rem;
+  height: 1rem;
+`;
+
+export const BannerContent = styled.div`
+  margin-left: 1rem;
+`;
+
+export const BannerTitle = styled.div`
+  color: ${color("text-dark")};
+  font-weight: 700;
+`;
+
+export const BannerDescription = styled.div`
+  color: ${color("text-medium")};
+  margin-top: 0.5rem;
+`;
