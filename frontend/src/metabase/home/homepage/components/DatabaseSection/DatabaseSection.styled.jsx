ad256dcc8f9056811115c5cc4b250927f87d1999
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-03 17:26:48 +0300

    Add hover state for button

--- frontend/src/metabase/home/homepage/components/DatabaseSection/DatabaseSection.styled.jsx
+++ frontend/src/metabase/home/homepage/components/DatabaseSection/DatabaseSection.styled.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Link } from "react-router";
 import { color } from "metabase/lib/colors";
 import Icon from "metabase/components/Icon";
