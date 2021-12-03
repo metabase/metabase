2ae79f901259f6c35ddcbb3a1b94f9d12c407541
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-12-02 19:20:57 -0800

    add cards unit test

--- frontend/src/metabase/parameters/utils/cards.unit.spec.js
+++ frontend/src/metabase/parameters/utils/cards.unit.spec.js
@@ -1,5 +1,8 @@
 import MetabaseSettings from "metabase/lib/settings";
-import { getTemplateTagParameters } from "./cards";
+import {
+  getTemplateTagParameters,
+  remapParameterValuesToTemplateTags,
+} from "./cards";
 
 MetabaseSettings.get = jest.fn();
 
@@ -167,6 +170,57 @@ describe("parameters/utils/cards", () => {
           },
         ];
         expect(getTemplateTagParameters(tags)).toEqual(parameters);
+      });
+    });
+  });
+
+  describe("remapParameterValuesToTemplateTags", () => {
+    it("should convert a dashboard parameterValues map into a map of template tag values", () => {
+      const parameterValues = {
+        "dashboard-parameter-1": "aaa",
+        "dashboard-parameter-2": "bbb",
+        "dashboard-parameter-3": "ccc",
+      };
+
+      const dashboardParameters = [
+        {
+          id: "dashboard-parameter-1",
+          target: ["variable", ["template-tag", "template-tag-1"]],
+        },
+        {
+          id: "dashboard-parameter-2",
+          target: ["dimension", ["template-tag", "template-tag-2"]],
+        },
+        {
+          id: "dashboard-parameter-3",
+          target: ["dimension", ["field", 1, null]],
+        },
+        {
+          id: "dashboard-parameter-4",
+        },
+      ];
+
+      const templateTags = [
+        {
+          name: "template-tag-1",
+        },
+        {
+          name: "template-tag-2",
+        },
+        {
+          name: "template-tag-3",
+        },
+      ];
+
+      expect(
+        remapParameterValuesToTemplateTags(
+          templateTags,
+          dashboardParameters,
+          parameterValues,
+        ),
+      ).toEqual({
+        "template-tag-1": "aaa",
+        "template-tag-2": "bbb",
       });
     });
   });
