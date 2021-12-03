7fc4fc7c3778a2c9eb460ffbc78fffb867dc0ec8
Anton Kulyk <kuliks.anton@gmail.com>
2021-12-03 12:04:50 +0200

    Hide collections without datasets in dataset picker (#19153)
    * Add tests for buildCollectionTree
    
    * Rename test objects for better readability
    
    * Allow filtering collection tree items by models
    
    * Hide collections without datasets in dataset picker
    
    * Keep parent nodes if they don't have target models

--- frontend/src/metabase/query_builder/components/saved-question-picker/utils.js
+++ frontend/src/metabase/query_builder/components/saved-question-picker/utils.js
@@ -1,16 +1,33 @@
+import _ from "underscore";
 import { getCollectionIcon } from "metabase/entities/collections";
 
-export function buildCollectionTree(collections) {
+function hasIntersection(list1, list2) {
+  return _.intersection(list1, list2).length > 0;
+}
+
+export function buildCollectionTree(collections, { targetModels } = {}) {
   if (collections == null) {
     return [];
   }
-  return collections.map(collection => ({
-    id: collection.id,
-    name: collection.name,
-    schemaName: collection.originalName || collection.name,
-    icon: getCollectionIcon(collection),
-    children: buildCollectionTree(collection.children),
-  }));
+
+  const shouldFilterCollections = Array.isArray(targetModels);
+
+  return collections.flatMap(collection => {
+    const hasTargetModels =
+      !shouldFilterCollections ||
+      hasIntersection(targetModels, collection.below) ||
+      hasIntersection(targetModels, collection.here);
+
+    return hasTargetModels
+      ? {
+          id: collection.id,
+          name: collection.name,
+          schemaName: collection.originalName || collection.name,
+          icon: getCollectionIcon(collection),
+          children: buildCollectionTree(collection.children, { targetModels }),
+        }
+      : [];
+  });
 }
 
 export const findCollectionByName = (collections, name) => {
