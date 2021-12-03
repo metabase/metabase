e9979fd8c779ce2b6bae43713076d563fe96a130
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-12-03 11:26:56 -0800

    clean up .info example

--- frontend/src/metabase/components/Popover/TippyPopover.info.js
+++ frontend/src/metabase/components/Popover/TippyPopover.info.js
@@ -1,31 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+
+import { forwardRefToInnerRef } from "metabase/styled-components/utils";
 
 import TippyPopover from "./TippyPopover";
 
 export const component = TippyPopover;
 export const description = "Wrapper around react-popper";
 
-const style = {
-  border: "1px solid black",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
+const Base = styled.div`
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const content = (
-  <div
-    style={{
-      ...style,
-      border: "none",
-      height: 200,
-      width: 200,
-    }}
-  >
-    popover body
-  </div>
-);
+const PopoverBody = forwardRefToInnerRef(styled(Base)`
+  border: none;
+  height: 200px;
+  width: 200px;
+`);
 
-function Content() {
+const LazyPopoverBody = forwardRefToInnerRef(styled(Base)`
+  border: none;
+  height: 200px;
+  width: 200px;
+  transition: opacity 1s;
+  opacity: ${props => props.opacity};
+`);
+
+const PopoverTarget = forwardRefToInnerRef(styled(Base)`
+  height: 100px;
+  width: 100px;
+`);
+
+const content = <PopoverBody>popover body</PopoverBody>;
+const target = <PopoverTarget>popover target</PopoverTarget>;
+
+function LazyContentExample() {
   const [opacity, setOpacity] = React.useState(0.1);
   const timeout = React.useRef();
 
@@ -39,46 +51,37 @@ function Content() {
     };
   }, [opacity]);
 
-  return (
-    <div
-      style={{
-        ...style,
-        border: "none",
-        height: 200,
-        width: 200,
-        transition: "opacity 1s",
-        opacity,
-      }}
-    >
-      popover content
-    </div>
-  );
+  return <LazyPopoverBody opacity={opacity}>popover content</LazyPopoverBody>;
 }
 
 export const examples = {
   "vertical placement": (
     <TippyPopover placement="top-start" content={content}>
-      <div style={{ ...style, width: 100, height: 100 }}>popover target</div>
+      {target}
     </TippyPopover>
   ),
   "horizontal placement": (
     <TippyPopover placement="left-end" content={content}>
-      <div style={{ ...style, width: 100, height: 100 }}>popover target</div>
+      {target}
     </TippyPopover>
   ),
   "lazy content rendering": (
     <React.Fragment>
-      <TippyPopover placement="left-end" content={<Content />}>
-        <div style={{ ...style, width: 100, height: 100 }}>lazy target</div>
+      <TippyPopover placement="left-end" content={<LazyContentExample />}>
+        <PopoverTarget>lazy target</PopoverTarget>
       </TippyPopover>
-      <TippyPopover lazy={false} placement="left-end" content={<Content />}>
-        <div style={{ ...style, width: 100, height: 100 }}>not lazy target</div>
+      <TippyPopover
+        lazy={false}
+        placement="left-end"
+        content={<LazyContentExample />}
+      >
+        <PopoverTarget>not lazy target</PopoverTarget>
       </TippyPopover>
     </React.Fragment>
   ),
   interactive: (
     <TippyPopover interactive placement="left-end" content={content}>
-      <div style={{ ...style, width: 100, height: 100 }}>popover target</div>
+      {target}
     </TippyPopover>
   ),
 };
