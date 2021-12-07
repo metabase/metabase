6233874ea1a91a39d5ff0867a8c5efedbaf57625
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-07 15:14:03 +0300

    Remove flow from EE (#19236)

--- enterprise/frontend/src/metabase-enterprise/audit_app/pages/AuditSchemas.jsx
+++ enterprise/frontend/src/metabase-enterprise/audit_app/pages/AuditSchemas.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 import AuditContent from "../components/AuditContent";
@@ -6,11 +7,7 @@ import AuditTableWithSearch from "../containers/AuditTableWithSearch";
 
 import * as SchemasCards from "../lib/cards/schemas";
 
-type Props = {
-  params: { [key: string]: string },
-};
-
-const AuditSchemas = (props: Props) => (
+const AuditSchemas = props => (
   <AuditContent {...props} title="Schemas" tabs={AuditSchemas.tabs} />
 );
 
