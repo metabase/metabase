ed71a65d4af7322a44e9243339fa77179160b752
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-12-08 12:34:39 -0800

    Disallow native queries with a tag widget-type of "none"
    Template tags with a widget-type that is undefined, null, or "none" now
    cause the query's isRunnable method to return false. Existing questions
    that have this defect won't be runnable until they are resaved with a
    set widget-type.

--- frontend/src/metabase/query_builder/components/template_tags/TagEditorParam.jsx
+++ frontend/src/metabase/query_builder/components/template_tags/TagEditorParam.jsx
@@ -17,7 +17,10 @@ import { getMetadata } from "metabase/selectors/metadata";
 import { SchemaTableAndFieldDataSelector } from "metabase/query_builder/components/DataSelector";
 import MetabaseSettings from "metabase/lib/settings";
 
-@connect(state => ({ metadata: getMetadata(state) }), { fetchField })
+@connect(
+  state => ({ metadata: getMetadata(state) }),
+  { fetchField },
+)
 export default class TagEditorParam extends Component {
   UNSAFE_componentWillMount() {
     const { tag, fetchField } = this.props;
@@ -114,6 +117,8 @@ export default class TagEditorParam extends Component {
     const hasSelectedDimensionField =
       isDimension && Array.isArray(tag.dimension);
     const hasWidgetOptions = widgetOptions && widgetOptions.length > 0;
+    const hasNoWidgetType =
+      tag["widget-type"] === "none" || !tag["widget-type"];
 
     return (
       <div className="px3 pt3 mb1 border-top">
@@ -142,7 +147,7 @@ export default class TagEditorParam extends Component {
             <h4 className="text-medium pb1">
               {t`Field to map to`}
               {tag.dimension == null && (
-                <span className="text-error mx1">(required)</span>
+                <span className="text-error mx1">{t`(required)`}</span>
               )}
             </h4>
 
@@ -167,7 +172,12 @@ export default class TagEditorParam extends Component {
 
         {hasSelectedDimensionField && (
           <div className="pb4">
-            <h4 className="text-medium pb1">{t`Filter widget type`}</h4>
+            <h4 className="text-medium pb1">
+              {t`Filter widget type`}
+              {hasNoWidgetType && (
+                <span className="text-error mx1">{t`(required)`}</span>
+              )}
+            </h4>
             <Select
               className="block"
               // avoid `undefined` value because it makes the component "uncontrollable"
@@ -181,13 +191,14 @@ export default class TagEditorParam extends Component {
               isInitiallyOpen={!tag["widget-type"] && hasWidgetOptions}
               placeholder={t`Select…`}
             >
-              {[{ name: "None", type: "none" }]
-                .concat(widgetOptions)
-                .map(widgetOption => (
-                  <Option key={widgetOption.type} value={widgetOption.type}>
-                    {widgetOption.name}
-                  </Option>
-                ))}
+              {(hasWidgetOptions
+                ? widgetOptions
+                : [{ name: t`None`, type: "none" }]
+              ).map(widgetOption => (
+                <Option key={widgetOption.type} value={widgetOption.type}>
+                  {widgetOption.name}
+                </Option>
+              ))}
             </Select>
             {!hasWidgetOptions && (
               <p>
