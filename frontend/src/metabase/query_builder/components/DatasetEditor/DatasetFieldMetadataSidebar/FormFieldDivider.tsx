594b61b2faf560a14e8df52cb1e523aa751bab52
Anton Kulyk <kuliks.anton@gmail.com>
2021-12-08 18:23:49 +0200

    Fix FormFieldDivider margin

--- frontend/src/metabase/query_builder/components/DatasetEditor/DatasetFieldMetadataSidebar/FormFieldDivider.tsx
+++ frontend/src/metabase/query_builder/components/DatasetEditor/DatasetFieldMetadataSidebar/FormFieldDivider.tsx
@@ -5,7 +5,7 @@ const FormFieldDivider = styled.div`
   height: 1px;
   width: 100%;
   background-color: ${color("bg-medium")};
-  margin-bottom: 21px;
+  margin-bottom: 1.5em;
 `;
 
 export default FormFieldDivider;
