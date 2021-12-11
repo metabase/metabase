fc3a2db723bfd1c32d4f3ccb53cf3509884caea5
Ariya Hidayat <ariya@metabase.com>
2021-12-10 19:06:55 -0800

    Custom expression: switch to a recursive-descent parser
    The fault-tolerant/recovery mode of the Chevrotain-based parser isn't
    needed anymore since the suggestion was made to be lexical-based in
    PR #15688. Therefore, there is no more reason to carry the Chevrotain
    baggage. The grammar construct for Chevrotain was converted into a
    simple recursive-descen parser. This represents a reduction from over
    1,100 LoC to a leaner parser of only 270 LoC.
    
    Also, we remove the dependency to Chevrotain, reducing the front-end
    bundle by approx 18 KB.

--- frontend/test/metabase/scenarios/custom-column/cc-expression-editor.cy.spec.js
+++ frontend/test/metabase/scenarios/custom-column/cc-expression-editor.cy.spec.js
@@ -49,7 +49,7 @@ describe("scenarios > question > custom column > expression editor", () => {
       .type("{movetoend}{backspace}", { force: true })
       .blur();
 
-    cy.findByText("Expected expression");
+    cy.findByText("Unexpected end of input");
     cy.button("Done").should("be.disabled");
   });
 
