d7de4c764f5032583e7016b882b935a40ea2e39d
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2021-12-11 19:44:20 +0300

    upgrade react-redux

--- frontend/src/metabase/admin/settings/components/SettingsBatchForm.jsx
+++ frontend/src/metabase/admin/settings/components/SettingsBatchForm.jsx
@@ -38,7 +38,7 @@ const SAVE_SETTINGS_BUTTONS_STATES = {
       updateSettings || (settings => dispatch(defaultUpdateSettings(settings))),
   }),
   null,
-  { withRef: true }, // HACK: needed so consuming components can call methods on the component :-/
+  { forwardRef: true }, // HACK: needed so consuming components can call methods on the component :-/
 )
 export default class SettingsBatchForm extends Component {
   constructor(props, context) {
