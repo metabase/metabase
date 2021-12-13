7a69db3d903fbf521cc171b7149bb42328ab14e9
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-13 21:52:27 +0300

    Add user step

--- frontend/src/metabase/setup/containers/UserStep/UserStep.tsx
+++ frontend/src/metabase/setup/containers/UserStep/UserStep.tsx
@@ -1,15 +1,18 @@
 import { connect } from "react-redux";
 import UserStep from "../../components/UserStep";
-import { setStep } from "../../actions";
+import { setStep, setUser } from "../../actions";
 import { LANGUAGE_STEP, USER_STEP } from "../../constants";
-import { isStepActive, isStepCompleted } from "../../selectors";
+import { getUser, isStepActive, isStepCompleted } from "../../selectors";
+import { UserInfo } from "../../types";
 
 const mapStateToProps = (state: any) => ({
+  user: getUser(state),
   isActive: isStepActive(state, USER_STEP),
   isCompleted: isStepCompleted(state, USER_STEP),
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
+  onChangeUser: (user: UserInfo) => dispatch(setUser(user)),
   onSelectThisStep: () => dispatch(setStep(LANGUAGE_STEP)),
   onSelectNextStep: () => dispatch(setStep(USER_STEP)),
 });
