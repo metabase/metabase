7a3ab05b14a186f72b18619af90bc7ca170f2c35
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-13 16:11:07 +0300

    Add tests

---
+++ frontend/src/metabase/setup/components/LanguageStep/LanguageStep.styled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { color } from "metabase/lib/colors";
+
+export const StepDescription = styled.div`
+  color: ${color("text-dark")};
+  margin: 0.875rem 0;
+`;
--- frontend/src/metabase/setup/components/LanguageStep/LanguageStep.tsx
+++ frontend/src/metabase/setup/components/LanguageStep/LanguageStep.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { t } from "ttag";
+import Button from "metabase/components/Button";
 import SetupStep from "../SetupStep";
+import { StepDescription } from "./LanguageStep.styled";
 
 const LanguageStep = () => {
-  return <SetupStep title={t`What's your preferred language?`} label={t`1`} />;
+  return (
+    <SetupStep title={t`What's your preferred language?`} label={t`1`}>
+      <StepDescription>{t`This language will be used throughout Metabase and will be the default for new users.`}</StepDescription>
+      <Button primary>{t`Next`}</Button>
+    </SetupStep>
+  );
 };
 
 export default LanguageStep;
