41fff372478afeda9dccefa1785e715bc6ca1220
Anton Kulyk <kuliks.anton@gmail.com>
2021-12-16 18:36:37 +0200

    Forward SelectButton ref

--- frontend/src/metabase/components/SelectButton.tsx
+++ frontend/src/metabase/components/SelectButton.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from "react";
+import React, { forwardRef, HTMLAttributes } from "react";
 import cx from "classnames";
 import Icon from "metabase/components/Icon";
 
@@ -7,25 +7,26 @@ type Props = HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
 };
 
-const SelectButton = ({
-  className,
-  children,
-  hasValue = true,
-  ...props
-}: Props) => (
-  <div
-    {...props}
-    className={cx(className, "AdminSelect flex align-center", {
-      "text-medium": !hasValue,
-    })}
-  >
-    <span className="AdminSelect-content mr1">{children}</span>
-    <Icon
-      className="AdminSelect-chevron flex-align-right"
-      name="chevrondown"
-      size={12}
-    />
-  </div>
-);
+const SelectButton = forwardRef<HTMLDivElement, Props>(function SelectButton(
+  { className, children, hasValue, ...props }: Props,
+  ref,
+) {
+  return (
+    <div
+      {...props}
+      className={cx(className, "AdminSelect flex align-center", {
+        "text-medium": !hasValue,
+      })}
+      ref={ref}
+    >
+      <span className="AdminSelect-content mr1">{children}</span>
+      <Icon
+        className="AdminSelect-chevron flex-align-right"
+        name="chevrondown"
+        size={12}
+      />
+    </div>
+  );
+});
 
 export default SelectButton;
