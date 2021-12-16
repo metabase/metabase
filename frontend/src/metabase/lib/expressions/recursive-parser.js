95040b79fca8ff1ddfc2c08a6994e0418afacaf9
Ariya Hidayat <ariya@metabase.com>
2021-12-16 11:37:10 -0800

    Annotate relevant parse* functions with the corresponding BNF grammar

--- frontend/src/metabase/lib/expressions/recursive-parser.js
+++ frontend/src/metabase/lib/expressions/recursive-parser.js
@@ -14,10 +14,10 @@ const COMPARISON_OPS = [
 function recursiveParse(source) {
   const { tokens } = tokenize(source);
 
-  const peek = () => tokens[0];
-
+  // Get the next token and remove it from the token list
   const next = () => tokens.shift();
 
+  // Throw an error if the next token isn't the expected operator
   const expectOp = nextOp => {
     const token = next();
     if (!token) {
@@ -30,11 +30,13 @@ function recursiveParse(source) {
     }
   };
 
+  // Return true if the next token is one of the specified operators
   const matchOps = ops =>
     tokens.length > 0 &&
     tokens[0].type === TOKEN.Operator &&
     ops.includes(tokens[0].op);
 
+  // Group ::= "(" Expression ")"
   const parseGroup = () => {
     expectOp(OP.OpenParenthesis);
     const expr = parseExpression();
@@ -46,6 +48,7 @@ function recursiveParse(source) {
     return expr;
   };
 
+  // Parameters ::= "(" * Expression ")"
   const parseParameters = () => {
     expectOp(OP.OpenParenthesis);
     const params = [];
@@ -69,6 +72,10 @@ function recursiveParse(source) {
     return ["dimension", ref];
   };
 
+  // Primary ::= Literal |
+  //             Identifier |
+  //             FunctionName Parameters |
+  //             Group
   const parsePrimary = () => {
     if (matchOps([OP.OpenParenthesis])) {
       return parseGroup();
@@ -83,8 +90,8 @@ function recursiveParse(source) {
     }
     const text = source.substring(start, end);
     if (type === TOKEN.Identifier) {
-      const after = peek();
-      if (after && after.op === OP.OpenParenthesis) {
+      const peek = tokens[0];
+      if (peek && peek.op === OP.OpenParenthesis) {
         const fn = getMBQLName(text.trim().toLowerCase());
         const params = parseParameters();
         return [fn ? fn : text, ...params];
@@ -96,6 +103,9 @@ function recursiveParse(source) {
     return type === TOKEN.String ? shrink(text) : parseFloat(text);
   };
 
+  // Unary ::= Primary |
+  //           "+" Unary |
+  //           "-" Unary
   const parseUnary = () => {
     if (matchOps([OP.Plus, OP.Minus])) {
       const { op } = next();
@@ -105,6 +115,9 @@ function recursiveParse(source) {
     return parsePrimary();
   };
 
+  // Multiplicative ::= Unary |
+  //                    Multiplicative "*"" Unary |
+  //                    Multiplicative "/" Unary
   const parseMultiplicative = () => {
     let expr = parseUnary();
     while (matchOps([OP.Star, OP.Slash])) {
@@ -117,6 +130,10 @@ function recursiveParse(source) {
     }
     return expr;
   };
+
+  // Additive ::= Multiplicative |
+  //              Additive "+" Multiplicative
+  //              Additive "-" Multiplicative
 
   const parseAdditive = () => {
     let expr = parseMultiplicative();
@@ -131,6 +148,8 @@ function recursiveParse(source) {
     return expr;
   };
 
+  // Comparison ::= Additive |
+  //                Comparison COMPARISON_OPS Additive
   const parseComparison = () => {
     let expr = parseAdditive();
     if (matchOps(COMPARISON_OPS)) {
@@ -140,6 +159,8 @@ function recursiveParse(source) {
     return expr;
   };
 
+  // BooleanUnary ::= Comparison |
+  //                 "NOT" BooleanUnary
   const parseBooleanUnary = () => {
     if (matchOps([OP.Not])) {
       const { op } = next();
@@ -148,6 +169,8 @@ function recursiveParse(source) {
     return parseComparison();
   };
 
+  // BooleanAnd ::= BooleanUnary |
+  //                BooleanAnd "AND" BooleanUnary
   const parseBooleanAnd = () => {
     let expr = parseBooleanUnary();
     while (matchOps([OP.And])) {
@@ -161,6 +184,8 @@ function recursiveParse(source) {
     return expr;
   };
 
+  // BooleanOr ::= BooleanAnd |
+  //               BooleanOr "OR" BooleanAnd
   const parseBooleanOr = () => {
     let expr = parseBooleanAnd();
     while (matchOps([OP.Or])) {
@@ -174,9 +199,8 @@ function recursiveParse(source) {
     return expr;
   };
 
-  const booleanExpression = () => parseBooleanOr();
-
-  const parseExpression = () => booleanExpression();
+  // Expression ::= BooleanOr
+  const parseExpression = () => parseBooleanOr();
 
   return parseExpression();
 }
