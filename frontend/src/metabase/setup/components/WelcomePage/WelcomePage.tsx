035db4401d888b38b7f1f368437e3cf537b3d4c2
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-16 22:30:13 +0300

    Add containers

--- frontend/src/metabase/setup/components/WelcomePage/WelcomePage.tsx
+++ frontend/src/metabase/setup/components/WelcomePage/WelcomePage.tsx
@@ -26,7 +26,8 @@ const WelcomePage = ({ onStepShow, onStepSubmit }: Props) => {
         <LogoIcon height={118} />
         <PageTitle>{t`Welcome to Metabase`}</PageTitle>
         <PageBody>
-          {t`Looks like everything is working. Now let’s get to know you, connect to your data, and start finding you some answers!`}
+          {t`Looks like everything is working.`}{" "}
+          {`Now let’s get to know you, connect to your data, and start finding you some answers!`}
         </PageBody>
         <PageButton
           primary
