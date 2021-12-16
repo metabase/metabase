56f6a20c70434ab4ae49a8045495075b3e6a8b4c
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-16 20:36:17 +0300

    Add containers

--- frontend/src/metabase/setup/analytics.ts
+++ frontend/src/metabase/setup/analytics.ts
@@ -1,4 +1,4 @@
-import { trackSchemaEvent } from "metabase/lib/analytics";
+import { trackSchemaEvent, trackStructEvent } from "metabase/lib/analytics";
 import { STEPS } from "./constants";
 
 export const trackStepSeen = (step: number) => {
@@ -26,11 +26,39 @@ export const trackAddDataLaterClicked = (engine?: string) => {
   });
 };
 
-export const trackTrackingChanged = (isAllowed: boolean) => {
+export const trackTrackingChanged = (isTrackingAllowed: boolean) => {
   trackSchemaEvent("settings", "1-0-1", {
-    event: isAllowed
+    event: isTrackingAllowed
       ? "tracking_permission_enabled"
       : "tracking_permission_disabled",
     source: "setup",
   });
+};
+
+export const trackWelcomeStepCompleted = () => {
+  trackStructEvent("Setup", "Welcome");
+};
+
+export const trackUserStepCompleted = () => {
+  trackStructEvent("Setup", "User Details Step");
+};
+
+export const trackDatabaseStepCompleted = (engine?: string) => {
+  trackStructEvent("Setup", "Database Step", engine);
+};
+
+export const trackPreferencesStepCompleted = (isTrackingAllowed: boolean) => {
+  trackStructEvent("Setup", "Preferences Step", isTrackingAllowed);
+};
+
+export const trackSetupCompleted = () => {
+  trackStructEvent("Setup", "Complete");
+};
+
+export const trackPasswordValidationError = () => {
+  trackStructEvent("Setup", "Error", "password validation");
+};
+
+export const trackDatabaseValidationError = (engine: string) => {
+  trackStructEvent("Setup", "Error", "database validation: " + engine);
 };
