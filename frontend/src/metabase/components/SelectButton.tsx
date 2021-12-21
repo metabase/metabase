a0c9d5021980f0f0666c21ed1c7bc053458de65f
Anton Kulyk <kuliks.anton@gmail.com>
2021-12-21 12:30:45 +0200

    Add tab-key navigation for metadata editor (#19379)
    * Forward form field ref
    
    * Focus "display_name" when selecting a new field
    
    * Hide query editor in metadata mode
    
    This will remove it from the "tab-flow"
    
    * Pass tabIndex prop to form text inputs
    
    * Add basic tab flow for metadata editor
    
    * Allow passing more props to SelectButton
    
    * Allow passing props to AccordionList's search input
    
    * Use numbers for tab indexes
    
    * Fix SelectButton's props type
    
    * Add special type picker to tab flow
    
    * Fix initial field focusing
    
    * Add "tab" icon
    
    * Add basic TabHintToast component
    
    * Show tab hint toast in metadata editor
    
    * Export wrapped CustomFormField
    
    * Forward SelectButton ref
    
    * Add react-virtualized scrollToColumn prop to Table
    
    * Don't open SemanticTypePicker on focus
    
    * Open SemanticTypePicker on "Enter" key press
    
    * Improve tabbing experience
    
    * automatically scroll the InteractiveTable while tabbing through columns
    * focus the first column on hitting "Enter" on the last column
    
    * Remove failed experiment
    
    It was my last hope to combine both props, but no lucj
    
    * Animate sidebar when focused field changes
    
    * Minor rename
    
    * Fix mapped field picker component
    
    * Pass visibility callbacks to trigger elements
    
    * Handle focus for mapped field picker
    
    * Pass searchPlaceholder to Select
    
    * Pass triggerable's onClose prop to Select
    
    * Add onClose prop to DataSelector
    
    * Use Select from special type picker
    
    * Handle focus for MappedFieldPicker
    
    * Add :focus style for essential select inputs
    
    * Fix jump from last to first column on tab key

--- frontend/src/metabase/components/SelectButton.tsx
+++ frontend/src/metabase/components/SelectButton.tsx
@@ -1,36 +1,32 @@
-import React from "react";
+import React, { forwardRef, HTMLAttributes } from "react";
 import cx from "classnames";
 import Icon from "metabase/components/Icon";
 
-type Props = {
-  className?: string;
-  style?: React.CSSProperties;
+type Props = HTMLAttributes<HTMLDivElement> & {
   hasValue?: boolean;
   children: React.ReactNode;
-  onClick?: () => void;
 };
 
-const SelectButton = ({
-  className,
-  style,
-  children,
-  hasValue = true,
-  onClick,
-}: Props) => (
-  <div
-    onClick={onClick}
-    style={style}
-    className={cx(className, "AdminSelect flex align-center", {
-      "text-medium": !hasValue,
-    })}
-  >
-    <span className="AdminSelect-content mr1">{children}</span>
-    <Icon
-      className="AdminSelect-chevron flex-align-right"
-      name="chevrondown"
-      size={12}
-    />
-  </div>
-);
+const SelectButton = forwardRef<HTMLDivElement, Props>(function SelectButton(
+  { className, children, hasValue, ...props }: Props,
+  ref,
+) {
+  return (
+    <div
+      {...props}
+      className={cx(className, "AdminSelect flex align-center", {
+        "text-medium": !hasValue,
+      })}
+      ref={ref}
+    >
+      <span className="AdminSelect-content mr1">{children}</span>
+      <Icon
+        className="AdminSelect-chevron flex-align-right"
+        name="chevrondown"
+        size={12}
+      />
+    </div>
+  );
+});
 
 export default SelectButton;
