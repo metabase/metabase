d1425f059f6f76b98faf6f32d78bbde9ff9220c4
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-12-22 16:16:57 -0800

    use TableInfo in TablePane

--- frontend/src/metabase/query_builder/components/dataref/TablePane.jsx
+++ frontend/src/metabase/query_builder/components/dataref/TablePane.jsx
@@ -1,19 +1,11 @@
-/* eslint "react/prop-types": "warn" */
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { t } from "ttag";
-import cx from "classnames";
 
-// components
 import Icon from "metabase/components/Icon";
 import Expandable from "metabase/components/Expandable";
-
-// lib
-import { foreignKeyCountsByOriginTable } from "metabase/lib/schema_metadata";
-import { inflect } from "metabase/lib/formatting";
-
-// entities
+import { TableInfo } from "./TablePane.styled";
 import Table from "metabase/entities/tables";
 
 const mapStateToProps = (state, ownProps) => ({
@@ -29,7 +21,6 @@ const mapDispatchToProps = {
 @connect(mapStateToProps, mapDispatchToProps)
 export default class TablePane extends React.Component {
   state = {
-    pane: "fields",
     error: null,
   };
 
@@ -57,87 +48,10 @@ export default class TablePane extends React.Component {
     }
   }
 
-  showPane = name => {
-    this.setState({ pane: name });
-  };
-
   render() {
     const { table } = this.props;
-    const { pane, error } = this.state;
+    const { error } = this.state;
     if (table) {
-      const fks = table.fks || [];
-      const panes = {
-        fields: table.fields.length,
-        // "metrics": table.metrics.length,
-        // "segments": table.segments.length,
-        connections: fks.length,
-      };
-      const tabs = Object.entries(panes).map(([name, count]) => (
-        <a
-          key={name}
-          className={cx("Button Button--small", {
-            "Button--active": name === pane,
-          })}
-          onClick={this.showPane.bind(null, name)}
-        >
-          <span className="DataReference-paneCount">{count}</span>
-          <span>{inflect(name, count)}</span>
-        </a>
-      ));
-
-      const descriptionClasses = cx({ "text-medium": !table.description });
-      const description = (
-        <p className={"text-spaced " + descriptionClasses}>
-          {table.description || t`No description set.`}
-        </p>
-      );
-      let content;
-      if (pane === "connections") {
-        const fkCountsByTable = foreignKeyCountsByOriginTable(fks);
-        content = (
-          <ul>
-            {fks
-              .sort((a, b) =>
-                a.origin.table.display_name.localeCompare(
-                  b.origin.table.display_name,
-                ),
-              )
-              .map(fk => (
-                <li key={fk.id}>
-                  <a
-                    onClick={() => this.props.show("field", fk.origin)}
-                    className="flex-full flex p1 text-bold text-brand text-wrap no-decoration bg-medium-hover"
-                  >
-                    {fk.origin.table.display_name}
-                    {fkCountsByTable[fk.origin.table.id] > 1 ? (
-                      <span className="text-medium text-light h5">
-                        {" "}
-                        via {fk.origin.display_name}
-                      </span>
-                    ) : null}
-                  </a>
-                </li>
-              ))}
-          </ul>
-        );
-      } else if (pane) {
-        const itemType = pane.replace(/s$/, "");
-        content = (
-          <ul>
-            {table[pane].map((item, index) => (
-              <li key={item.id}>
-                <a
-                  onClick={() => this.props.show(itemType, item)}
-                  className="flex-full flex p1 text-bold text-brand text-wrap no-decoration bg-medium-hover"
-                >
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        );
-      }
-
       return (
         <div>
           <div className="ml1">
@@ -145,12 +59,24 @@ export default class TablePane extends React.Component {
               <Icon name="table2" className="text-medium pr1" size={16} />
               <h3 className="text-wrap">{table.name}</h3>
             </div>
-            {description}
+            <TableInfo tableId={table.id} />
             <div className="my2 Button-group Button-group--brand text-uppercase">
-              {tabs}
+              {
+                <ul>
+                  {table.fields.map((item, index) => (
+                    <li key={item.id}>
+                      <a
+                        onClick={() => this.props.show("field", item)}
+                        className="flex-full flex p1 text-bold text-brand text-wrap no-decoration bg-medium-hover"
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              }
             </div>
           </div>
-          {content}
         </div>
       );
     } else {
