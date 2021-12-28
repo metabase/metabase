fbf55f112427f214fbb1e009e3c64991f34ef29a
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-28 18:00:11 +0300

    Add button

--- frontend/src/metabase/auth/components/AuthButton/AuthButton.tsx
+++ frontend/src/metabase/auth/components/AuthButton/AuthButton.tsx
@@ -4,12 +4,13 @@ import { TextRoot, TextLink } from "./AuthButton.styled";
 export interface AuthButtonProps {
   icon?: string;
   text?: string;
+  href?: string;
 }
 
-const AuthButton = ({ text }: AuthButtonProps): JSX.Element => {
+const AuthButton = ({ text, href = "" }: AuthButtonProps): JSX.Element => {
   return (
     <TextRoot>
-      <TextLink to="">{text}</TextLink>
+      <TextLink to={href}>{text}</TextLink>
     </TextRoot>
   );
 };
--- frontend/src/metabase/auth/components/EmailButton/EmailButton.tsx
+++ frontend/src/metabase/auth/components/EmailButton/EmailButton.tsx
@@ -3,7 +3,9 @@ import { t } from "ttag";
 import AuthButton from "../AuthButton";
 
 const EmailButton = () => {
-  return <AuthButton text={t`Sign in with email`} />;
+  return (
+    <AuthButton text={t`Sign in with email`} href="/auth/login/password" />
+  );
 };
 
 export default EmailButton;
