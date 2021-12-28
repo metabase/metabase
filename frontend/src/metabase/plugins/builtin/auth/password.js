7364ee31ae6262b22db1c07b54b9d5b6133741b8
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-28 20:51:15 +0300

    Add password panel

--- frontend/src/metabase/plugins/builtin/auth/password.js
+++ frontend/src/metabase/plugins/builtin/auth/password.js
@@ -1,11 +1,11 @@
 import { PLUGIN_AUTH_PROVIDERS } from "metabase/plugins";
+import PasswordButton from "metabase/auth/components/PasswordButton";
+import PasswordPanel from "metabase/auth/containers/PasswordPanel";
 
-PLUGIN_AUTH_PROVIDERS.push(providers => {
-  const PASSWORD_PROVIDER = {
-    name: "password",
-    Button: require("metabase/auth/components/PasswordButton").default,
-    Panel: require("metabase/auth/containers/PasswordPanel").default,
-  };
+const PASSWORD_PROVIDER = {
+  name: "password",
+  Button: PasswordButton,
+  Panel: PasswordPanel,
+};
 
-  return [...providers, PASSWORD_PROVIDER];
-});
+PLUGIN_AUTH_PROVIDERS.push(providers => [...providers, PASSWORD_PROVIDER]);
