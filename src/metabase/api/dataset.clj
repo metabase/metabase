14dcb0f9dfaa57a76ad1d6c5d1faaa9909d85d81
dan sutton <dan@dpsutton.com>
2021-12-28 18:17:47 -0600

    A few important changes:
    - ensure that we don't throw away metadata and return an empty channel
    in the card api if the query hasn't changed
    - ensure api/dataset uses `:metadata/dataset-metadata`
    - hydrate fields on api/table/card__id/query_metadata so that it has
    target and fk information
    - preserve id so this information can be recovered†
    
    †: When a user enters an id we preserve it, but it also gets confusing
    if that's the actual field's id or the user-identified id. This is
    important when we drill down (and probably lots of other places too if
    they start assuming an id means they know exactly what is going on).
    
    A situation based on the orders table:
    
    Suppose you make a native question
    
    ```sql
    SELECT "PUBLIC"."ORDERS"."ID" AS "ID",
      "PUBLIC"."ORDERS"."USER_ID" AS "user_id_bro",  -- note this is renamed
      "PUBLIC"."ORDERS"."PRODUCT_ID" AS "PRODUCT_ID",
      "PUBLIC"."ORDERS"."SUBTOTAL" AS "SUBTOTAL",
      "PUBLIC"."ORDERS"."TAX" AS "TAX",
      "PUBLIC"."ORDERS"."TOTAL" AS "TOTAL",
      "PUBLIC"."ORDERS"."DISCOUNT" AS "DISCOUNT",
      "PUBLIC"."ORDERS"."CREATED_AT" AS "CREATED_AT",
    "PUBLIC"."ORDERS"."QUANTITY" AS "QUANTITY"
    FROM "PUBLIC"."ORDERS"
    LIMIT 15
    ```
    
    In my db, the "USER_ID" column is field 3 and points at field 22. Our db
    knows that field 3 is called "USER_ID". But in the source query that
    column is actually called "user_id_bro". And lots of places assume that
    if there's a field's `:id` it knows what the name is. This is obviously
    incorrect here. It is a very important distinction that this column "is
    basically field 3 USER_ID" and this is literally the field "USER_ID".
    
    So if you do a drilldown based on this information, the FE will issue a
    query to "select field 3 from card__127" which expands to roughly
    "select USER_ID from (select ... USER_ID as user_id_bro ...)" and this
    query obviously will fail.

--- src/metabase/api/dataset.clj
+++ src/metabase/api/dataset.clj
@@ -55,10 +55,14 @@
       (events/publish-event! :table-read (assoc (Table table-id) :actor_id api/*current-user-id*))))
   ;; add sensible constraints for results limits on our query
   (let [source-card-id (query->source-card-id query)
-        info           {:executed-by api/*current-user-id*
-                        :context     context
-                        :card-id     source-card-id
-                        :nested?     (boolean source-card-id)}]
+        source-card    (when source-card-id
+                            (Card source-card-id))
+        info           (cond-> {:executed-by api/*current-user-id*
+                                :context     context
+                                :card-id     source-card-id
+                                :nested?     (boolean source-card-id)}
+                         (:dataset source-card)
+                         (assoc :metadata/dataset-metadata (:result_metadata source-card)))]
     (binding [qp.perms/*card-id* source-card-id]
       (qp.streaming/streaming-response [context export-format]
         (qp-runner query info context)))))
