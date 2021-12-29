9559212fa2fdf0c1806ff9226be7352d344ea2e1
Alexander Polyankin <alexander.polyankin@metabase.com>
2021-12-29 18:32:21 +0300

    Fix url redirect

--- frontend/src/metabase/auth/components/PasswordButton/PasswordButton.tsx
+++ frontend/src/metabase/auth/components/PasswordButton/PasswordButton.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { t } from "ttag";
 import AuthButton from "../AuthButton";
 
-const PasswordButton = () => {
-  return (
-    <AuthButton link="/auth/login/password">{t`Sign in with email`}</AuthButton>
-  );
+interface PasswordButtonProps {
+  redirectUrl?: string;
+}
+
+const PasswordButton = ({ redirectUrl }: PasswordButtonProps) => {
+  const link = redirectUrl
+    ? `/auth/login/password?redirect=${encodeURIComponent(redirectUrl)}`
+    : `/auth/login/password`;
+
+  return <AuthButton link={link}>{t`Sign in with email`}</AuthButton>;
 };
 
 export default PasswordButton;
