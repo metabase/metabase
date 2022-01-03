20c2552b01fe878d6558f7606c1c347823ee5560
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-03 14:36:11 +0300

    Fix setup for mobile

---
+++ frontend/src/metabase/components/Toggle/Toggle.styled.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { color } from "metabase/lib/colors";
+
+export interface ToggleRootProps {
+  isSmall?: boolean;
+  isSelected?: boolean;
+  currentColor?: string;
+}
+
+const getLeft = ({ isSmall, isSelected }: ToggleRootProps): string => {
+  if (!isSelected) {
+    return "1px";
+  } else if (!isSmall) {
+    return "25px";
+  } else {
+    return "12px";
+  }
+};
+
+export const ToggleRoot = styled.a<ToggleRootProps>`
+  position: relative;
+  display: inline-block;
+  color: ${props => props.currentColor ?? color("brand")};
+  box-sizing: border-box;
+  width: ${props => (props.isSmall ? "28px" : "48px")};
+  height: ${props => (props.isSmall ? "17px" : "24px")};
+  border-radius: 99px;
+  border: 1px solid ${color("border")};
+  background-color: ${color("bg-medium")};
+  transition: all 0.3s;
+
+  &:after {
+    content: "";
+    width: ${props => (props.isSmall ? "13px" : "20px")};
+    height: ${props => (props.isSmall ? "13px" : "20px")};
+    border-radius: 99px;
+    position: absolute;
+    top: 1px;
+    left: ${getLeft};
+    background-color: ${props =>
+      props.isSelected ? "currentColor" : color("white")};
+    transition: all 0.3s;
+    box-shadow: 2px 2px 6px ${color("shadow")};
+  }
+`;
