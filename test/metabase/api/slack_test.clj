8cfea976a6f0b00623c1f83942f814827926ce64
Noah Moss <noahbmoss@gmail.com>
2022-01-04 16:25:59 -0500

    add slack api test

---
+++ test/metabase/api/slack_test.clj
@@ -0,0 +1,20 @@
+(ns metabase.api.slack-test
+  (:require [clojure.test :refer :all]
+            [metabase.integrations.slack :as slack]
+            [metabase.test :as mt]))
+
+(deftest update-slack-settings-test
+  (testing "PUT /api/slack/settings"
+    (testing "A 400 error is returned when setting an invalid Slack app token"
+      (mt/user-http-request :crowberto :put 400 "slack/settings"
+                            {:slack-app-token "fake-token"}))
+
+    (testing "An admin can set a valid Slack app token to the slack-app-token setting, and any value in the
+             `slack-token` setting is cleared"
+      (with-redefs [slack/valid-token? (constantly true)]
+        (mt/with-temporary-setting-values [slack-app-token nil
+                                           slack-token     "fake-token"]
+          (mt/user-http-request :crowberto :put 200 "slack/settings"
+                                {:slack-app-token "fake-token"})
+          (is (= "fake-token" (slack/slack-app-token)))
+          (is (= nil (slack/slack-token))))))))
