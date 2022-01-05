66f48df091a91a85f3532d6b08b4a82dd796c498
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-05 19:35:04 +0300

    Fix postcss issues

---
+++ frontend/src/metabase/admin/settings/slack/components/SlackAppSettings/SlackAppSettings.tsx
@@ -0,0 +1,79 @@
+import React, { ReactNode, useState } from "react";
+import { jt, t } from "ttag";
+import {
+  SectionBody,
+  SectionButton,
+  SectionButtonIcon,
+  SectionButtonText,
+  SectionCode,
+  SectionHeader,
+  SectionLink,
+  SectionMessage,
+  SectionRoot,
+  SectionTitle,
+  SectionToggle,
+} from "./SlackAppSettings.styled";
+
+interface SettingsSectionProps {
+  title: string;
+  children?: ReactNode;
+}
+
+const SettingsSection = ({
+  title,
+  children,
+}: SettingsSectionProps): JSX.Element => {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  return (
+    <SectionRoot>
+      <SectionHeader onClick={() => setIsExpanded(!isExpanded)}>
+        <SectionTitle>{title}</SectionTitle>
+        <SectionToggle
+          round
+          icon={isExpanded ? "chevronup" : "chevrondown"}
+          aria-label={t`Setup section`}
+          aria-expanded={isExpanded}
+        />
+      </SectionHeader>
+      {isExpanded && <SectionBody>{children}</SectionBody>}
+    </SectionRoot>
+  );
+};
+
+const CreateAppSection = (): JSX.Element => {
+  return (
+    <SettingsSection title={t`1. Create your Slack App`}>
+      <SectionMessage>
+        {t`To create your Metabase integration on Slack you’ll need to set up some things.`}{" "}
+        {jt`First, go to ${(
+          <SectionLink href="https://api.slack.com/apps">{t`Slack Apps`}</SectionLink>
+        )}, hit “${(<strong>{t`Create New App`}</strong>)}” and pick “${(
+          <strong>{t`From an app manifest`}</strong>
+        )}”.`}
+      </SectionMessage>
+      <SectionButton
+        className="Button Button--primary"
+        href="https://api.slack.com/apps"
+      >
+        <SectionButtonText>{`Open Slack Apps`}</SectionButtonText>
+        <SectionButtonIcon name="external" />
+      </SectionButton>
+    </SettingsSection>
+  );
+};
+
+const CopyManifestSection = (): JSX.Element => {
+  return (
+    <SettingsSection title={t`2. Copy the Metabase manifest`}>
+      <SectionMessage>
+        {jt`Copy our ${(
+          <strong>{t`Slack Manifest`}</strong>
+        )} below and paste it in to create the app. In the following screen, click “${(
+          <strong>{t`Install to workspace`}</strong>
+        )}” and authorize it.`}
+      </SectionMessage>
+      <SectionCode />
+    </SettingsSection>
+  );
+};
