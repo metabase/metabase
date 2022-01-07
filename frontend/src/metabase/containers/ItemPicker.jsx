01762f79b10cfdb30a14f88bd2fb429f4d5f4cd9
Chris Wu <chris@faros.ai>
2022-01-06 19:25:05 -0800

    Formatting

--- frontend/src/metabase/containers/ItemPicker.jsx
+++ frontend/src/metabase/containers/ItemPicker.jsx
@@ -60,10 +60,12 @@ export default class ItemPicker extends React.Component {
   getCrumbs(collection, collectionsById) {
     if (collection && collection.path) {
       return [
-        ...collection.path.filter(id => collectionsById[id]).map(id => [
-          collectionsById[id].name,
-          () => this.setState({ parentId: id }),
-        ]),
+        ...collection.path
+          .filter(id => collectionsById[id])
+          .map(id => [
+            collectionsById[id].name,
+            () => this.setState({ parentId: id }),
+          ]),
         [collection.name],
       ];
     } else {
