2d07f70a8e03a2f515869076108c1b8d3a14ae1f
Jeff Evans <jeff303@users.noreply.github.com>
2022-01-07 17:32:43 -0600

    Allow override of WEEK_START in Snowflake driver (#19375)
    * Allow override of WEEK_START in Snowflake driver
    
    Incorporate the following sources of WEEK_START, in decreasing order of precedence:
    1) the connection string, a.k.a. additional-options (ex: WEEK_START=N)
    2) the Metabase "start-of-week" setting (i.e. day of a week)
    3) the default value (7: Sunday)
    
    Adding new fn to the driver.common namespace to parse a connection string param out of additional-settings (plus a test for that)
    
    Adding Snowflake driver test to ensure that all cases above are covered as expected, by invoking the DAYOFWEEK function on a set of fixed dates
    
    Update documentation for start-of-week setting to clarify that it now influences this behavior
    
    Swap out parse-additional-options-value for additional-options->map instead (more generic)
    
    Update test accordingly

--- modules/drivers/snowflake/test/metabase/test/data/snowflake.clj
+++ modules/drivers/snowflake/test/metabase/test/data/snowflake.clj
@@ -39,14 +39,15 @@
 (defmethod tx/dbdef->connection-details :snowflake
   [_ context {:keys [database-name]}]
   (merge
-   {:account   (tx/db-test-env-var-or-throw :snowflake :account)
-    :user      (tx/db-test-env-var-or-throw :snowflake :user)
-    :password  (tx/db-test-env-var-or-throw :snowflake :password)
+   {:account             (tx/db-test-env-var-or-throw :snowflake :account)
+    :user                (tx/db-test-env-var-or-throw :snowflake :user)
+    :password            (tx/db-test-env-var-or-throw :snowflake :password)
+    :additional-options  (tx/db-test-env-var :snowflake :additional-options)
     ;; this lowercasing this value is part of testing the fix for
     ;; https://github.com/metabase/metabase/issues/9511
-    :warehouse (u/lower-case-en (tx/db-test-env-var-or-throw :snowflake :warehouse))
+    :warehouse           (u/lower-case-en (tx/db-test-env-var-or-throw :snowflake :warehouse))
     ;; SESSION parameters
-    :timezone "UTC"}
+    :timezone            "UTC"}
    ;; Snowflake JDBC driver ignores this, but we do use it in the `query-db-name` function in
    ;; `metabase.driver.snowflake`
    (when (= context :db)
