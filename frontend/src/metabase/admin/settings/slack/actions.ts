0aaa496fc5118b6345c4a5b5308cc345387366bf
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-10 15:09:05 +0300

    Add help card

--- frontend/src/metabase/admin/settings/slack/actions.ts
+++ frontend/src/metabase/admin/settings/slack/actions.ts
@@ -3,6 +3,11 @@ import { createThunkAction } from "metabase/lib/redux";
 import { reloadSettings } from "metabase/admin/settings/settings";
 import { SlackSettings } from "metabase-types/api/slack";
 
+export const LOAD_MANIFEST = "metabase/admin/settings/slack/LOAD_MANIFEST";
+export const loadManifest = createThunkAction(LOAD_MANIFEST, () => async () => {
+  return await SlackApi.getManifest();
+});
+
 export const UPDATE_SETTINGS = "metabase/admin/settings/slack/UPDATE_SETTINGS";
 export const updateSettings = createThunkAction(
   UPDATE_SETTINGS,
