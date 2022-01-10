a1faf3b0cc886d09768d9c70aa763b293d1a0d33
Jeff Evans <jeff.evans@metabase.com>
2022-01-10 16:40:01 -0600

    Fix linters again
    Fix add Cypress test

--- frontend/test/metabase/scenarios/admin/databases/add.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/databases/add.cy.spec.js
@@ -229,7 +229,7 @@ describe("scenarios > admin > databases > add", () => {
       // enter text
       typeField("Display name", "bq db");
       // typeField("Dataset ID", "some-dataset");
-      selectFieldOption("Datasets", "Only these...")
+      selectFieldOption("Datasets", "Only these...");
       cy.findByPlaceholderText("E.x. public,auth*").type("some-dataset");
 
       // create blob to act as selected file
