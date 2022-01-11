dcb675f642a264755f3fb6514aa43248ec148057
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-11 16:17:41 +0300

    Fix the test

--- frontend/src/metabase-types/store/mocks/state.ts
+++ frontend/src/metabase-types/store/mocks/state.ts
@@ -1,7 +1,11 @@
 import { State } from "metabase-types/store";
-import { createMockSettingsState } from "metabase-types/store/mocks";
+import {
+  createMockAdminState,
+  createMockSettingsState,
+} from "metabase-types/store/mocks";
 
 export const createMockState = (opts?: Partial<State>): State => ({
+  admin: createMockAdminState(),
   settings: createMockSettingsState(),
   ...opts,
 });
