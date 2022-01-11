81d2d240106bf54b81edaa39c7f7133a0f6dcc53
dpsutton <dan@dpsutton.com>
2022-01-11 12:20:03 -0600

    Exclude archived collections from tree view (#19609)
    * Exclude archived collections from tree view
    
    This is a bit more nuanced that it might first appear. The bug report is
    the permissions editor for collections is showing child collections
    which are archived. But the api sends back ALL collections, regardless
    of archival state. So the FE knows to omit top-level archived
    collections but not archived child collections.
    
    So we thought about just having the FE remove all archived ones, but
    best to remove it from the response. One worry is that we're not sure if
    any callers want archived collections in the tree view or not. So right
    now they are removed with a `?exclude-archived=true` query param, but if
    we can demonstrate that no callers of the tree api ever want archived
    items we can make this the behavior, rather than an optional behavior.
    
    To verify that the tree api is sending back all collections, you can
    check
    
    ```clj
    collection=> (collection/collections->tree
                   {}
                   [{:archived false
                     :name "foo"
                     :id 1
                     :location "/"}
                    {:archived true
                     :name "foo"
                     :id 2
                     :location "/1/"}
                    {:archived true
                     :name "its archived"
                     :location "/"
                     :id 3}])
    ;; we have always sent back archived collections, both top level and children
    ({:archived false,
      :name "foo",
      :id 1,
      :location "/",
      :children ({:archived true,
                  :name "foo",
                  :id 2,
                  :location "/1/",
                  :children ()})}
     {:archived true,
      :name "its archived",
      :location "/",
      :id 3,
      :children ()})
    ```
    
    * hide archived collections from the collections permissions page (#19616)
    
    * fix incorrect issue number
    
    Co-authored-by: Alexander Lesnenko <alxnddr@users.noreply.github.com>
    Co-authored-by: Aleksandr Lesnenko <alxnddr@gmail.com>

---
+++ frontend/test/metabase/scenarios/permissions/reproductions/19603-do-not-show-archived-collections.cy.spec.js
@@ -0,0 +1,32 @@
+import { restore } from "__support__/e2e/cypress";
+
+const UNARCHIVED_PARENT_NAME = "Unarchived parent";
+const ARCHIVED_NAME = "Archived child";
+
+describe("issue 19603", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+
+    cy.createCollection({
+      name: UNARCHIVED_PARENT_NAME,
+    }).then(response => {
+      const { id: collectionId } = response.body;
+      cy.createCollection({
+        name: ARCHIVED_NAME,
+        parent_id: collectionId,
+        archived: true,
+      }).then(response => {
+        const { id: archivedCollectionId } = response.body;
+        cy.archiveCollection(archivedCollectionId);
+      });
+    });
+  });
+
+  it("should not show archived collections on the collections permissions page (metabase#19603)", () => {
+    cy.visit("admin/permissions/collections");
+
+    cy.findByText(UNARCHIVED_PARENT_NAME).click();
+    cy.findByText(ARCHIVED_NAME).should("not.exist");
+  });
+});
