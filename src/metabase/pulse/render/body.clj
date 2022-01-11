0c190ec62f2bf93b6c4a5967aa5f9bbcf123c5b9
Howon Lee <hlee.howon@gmail.com>
2022-01-10 19:45:57 -0800

    Waterfall bignum display fix (#19025) (#19563)
    This actually has nothing to do with waterfalls. Really, it's that static viz is brutally incompatible with bignum types, bigint and bigdec. However, nothing will fit in the screen anyways if you actually need a bignum or bigint type so the solution was to just coerce and be OK with exploding if it actually needed bignum range.

--- src/metabase/pulse/render/body.clj
+++ src/metabase/pulse/render/body.clj
@@ -404,7 +404,7 @@
   [_ render-type _timezone-id :- (s/maybe s/Str) card _ {:keys [rows] :as data}]
   (let [[x-axis-rowfn y-axis-rowfn] (common/graphing-column-row-fns card data)
         rows                        (map (juxt (comp str x-axis-rowfn) y-axis-rowfn)
-                                         (common/non-nil-rows x-axis-rowfn y-axis-rowfn rows))
+                                         (common/row-preprocess x-axis-rowfn y-axis-rowfn rows))
         slice-threshold             (or (get-in card [:visualization_settings :pie.slice_threshold])
                                         2.5)
         {:keys [rows percentages]}  (donut-info slice-threshold rows)
@@ -489,7 +489,7 @@
         row-seqs      (for [[row-seq rowfnpair] (map vector row-seqs rowfns)]
                         (let [[x-rowfn y-rowfn] rowfnpair]
                           (map (juxt x-rowfn y-rowfn)
-                               (common/non-nil-rows x-rowfn y-rowfn row-seq))))
+                               (common/row-preprocess x-rowfn y-rowfn row-seq))))
         col-seqs      (map :cols multi-data)
         first-rowfns  (first rowfns)
         [x-col y-col] ((juxt (first first-rowfns) (second first-rowfns)) (first col-seqs))
@@ -742,10 +742,8 @@
          y-axis-rowfn] (common/graphing-column-row-fns card data)
         [x-col y-col]  ((juxt x-axis-rowfn y-axis-rowfn) cols)
         rows           (map (juxt x-axis-rowfn y-axis-rowfn)
-                            (common/non-nil-rows x-axis-rowfn y-axis-rowfn rows))
+                            (common/row-preprocess x-axis-rowfn y-axis-rowfn rows))
         last-rows      (reverse (take-last 2 rows))
-        values         (for [row last-rows]
-                         (some-> row y-axis-rowfn common/format-number))
         labels         (x-and-y-axis-label-info x-col y-col viz-settings)
         render-fn      (if (isa? (-> cols x-axis-rowfn :effective_type) :type/Temporal)
                          js-svg/timelineseries-waterfall
@@ -767,7 +765,7 @@
 (s/defmethod render :funnel :- common/RenderedPulseCard
   [_ render-type timezone-id card _ {:keys [rows cols viz-settings] :as data}]
   ;; x-axis-rowfn is always first, y-axis-rowfn is always second
-  (let [rows          (common/non-nil-rows first second rows)
+  (let [rows          (common/row-preprocess first second rows)
         [x-col y-col] cols
         settings      (->js-viz x-col y-col viz-settings)
         settings      (assoc settings
