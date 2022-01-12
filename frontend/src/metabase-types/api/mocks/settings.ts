46b5b3b27bc0db572fd79a3a9689e7c0c76e0303
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-12 12:11:54 +0300

    Fix naming

--- frontend/src/metabase-types/api/mocks/settings.ts
+++ frontend/src/metabase-types/api/mocks/settings.ts
@@ -1,5 +1,33 @@
-import { Settings } from "metabase-types/api";
+import { Engine, Settings, Version } from "metabase-types/api";
+
+export const createMockEngine = (opts?: Partial<Engine>): Engine => ({
+  "display-name": "PostgreSQL",
+  "superseded-by": undefined,
+  ...opts,
+});
+
+export const createMockEngines = (): Record<string, Engine> => ({
+  postgres: createMockEngine(),
+});
+
+export const createMockVersion = (opts?: Partial<Version>): Version => ({
+  tag: "v1",
+  ...opts,
+});
 
 export const createMockSettings = (opts?: Partial<Settings>): Settings => ({
+  "enable-public-sharing": false,
+  "enable-xrays": false,
+  engines: createMockEngines(),
+  "deprecation-notice-version": undefined,
+  "show-database-syncing-modal": false,
+  "show-homepage-data": false,
+  "show-homepage-xrays": false,
+  "show-homepage-pin-message": false,
+  "slack-token": undefined,
+  "slack-token-valid?": true,
+  "slack-app-token": undefined,
+  "slack-files-channel": undefined,
+  version: createMockVersion(),
   ...opts,
 });
--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -1,18 +1,24 @@
+export interface Engine {
+  "display-name": string;
+  "superseded-by": string | undefined;
+}
+
 export interface Version {
   tag: string;
 }
 
 export interface Settings {
-  "enable-public-sharing"?: boolean;
-  "enable-xrays"?: boolean;
-  "deprecation-notice-version"?: string;
-  "show-database-syncing-modal"?: boolean;
-  "show-homepage-data"?: boolean;
-  "show-homepage-xrays"?: boolean;
-  "show-homepage-pin-message"?: boolean;
-  "slack-token"?: string;
-  "slack-token-valid?"?: boolean;
-  "slack-app-token"?: string;
-  "slack-files-channel"?: string;
-  version?: Version;
+  "enable-public-sharing": boolean;
+  "enable-xrays": boolean;
+  engines: Record<string, Engine>;
+  "deprecation-notice-version": string | undefined;
+  "show-database-syncing-modal": boolean;
+  "show-homepage-data": boolean;
+  "show-homepage-xrays": boolean;
+  "show-homepage-pin-message": boolean;
+  "slack-token": string | undefined;
+  "slack-token-valid?": boolean;
+  "slack-app-token": string | undefined;
+  "slack-files-channel": string | undefined;
+  version: Version;
 }
