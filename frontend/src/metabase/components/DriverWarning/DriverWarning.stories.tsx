3991ea91c6494144fe99ae9fb0ae902411dcdbd2
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-12 14:40:42 +0300

    Add driver warning

---
+++ frontend/src/metabase/components/DriverWarning/DriverWarning.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ComponentStory } from "@storybook/react";
+import { createMockDatabase, createMockEngine } from "metabase-types/api/mocks";
+import DriverWarning from "./DriverWarning";
+
+export default {
+  title: "Components/DriverWarning",
+  component: DriverWarning,
+  argTypes: { onChange: { action: "onChange" } },
+};
+
+const Template: ComponentStory<typeof DriverWarning> = args => {
+  return <DriverWarning {...args} />;
+};
+Template.args = {
+  engines: {
+    presto: createMockEngine({
+      "display-name": "Presto (Deprecated)",
+      "superseded-by": "presto-jdbc",
+    }),
+    "presto-jdbc": createMockEngine({
+      "display-name": "Presto",
+    }),
+  },
+};
+
+export const New = Template.bind({});
+New.args = {
+  engine: "presto-jdbc",
+  ...Template.args,
+};
+
+export const Deprecated = Template.bind({});
+Deprecated.args = {
+  engine: "presto",
+  ...Template.args,
+};
