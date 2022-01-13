c53c1afe8c7f4fe2b5abccef2b3c3ce144502e5c
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-13 15:55:35 +0300

    Position card in setup

--- frontend/src/metabase/setup/components/DatabaseHelp/DatabaseHelp.styled.tsx
+++ frontend/src/metabase/setup/components/DatabaseHelp/DatabaseHelp.styled.tsx
@@ -6,15 +6,17 @@ interface DatabaseHelpRootProps {
 }
 
 export const DatabaseHelpRoot = styled.div<DatabaseHelpRootProps>`
-  transform: ${props => `translateY(${props.isVisible ? "0" : "200%"})`};
-  transition: transform 0.4s;
+  display: ${props => (props.isVisible ? "block" : "none")};
   margin-bottom: 1.75rem;
 
   ${breakpointMinLarge} {
+    display: block;
     position: fixed;
     right: 2em;
     bottom: 2em;
     max-width: 20%;
     margin-bottom: 0;
+    transform: ${props => `translateY(${props.isVisible ? "0" : "200%"})`};
+    transition: transform 0.4s;
   }
 `;
