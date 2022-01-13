21c66ebb2b67d4273b360cbcf0a48e69cc3cd8c5
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-13 16:21:39 +0300

    Delete old component

---
+++ frontend/src/metabase/admin/databases/containers/DatabaseEditApp.styled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import DatabaseHelpCard from "metabase/components/DatabaseHelpCard";
+
+export const HelpCard = styled(DatabaseHelpCard)`
+  margin-left: 6.5rem;
+`;
