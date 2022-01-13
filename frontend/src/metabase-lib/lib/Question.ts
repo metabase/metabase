8250d1790e90f6f4fcf59c1f513d82f2b9350ebe
Anton Kulyk <kuliks.anton@gmail.com>
2022-01-13 16:06:32 +0200

    Fix summarize sidebar behavior

--- frontend/src/metabase-lib/lib/Question.ts
+++ frontend/src/metabase-lib/lib/Question.ts
@@ -1005,14 +1005,28 @@ export default class Question {
   /**
    * Returns true if the questions are equivalent (including id, card, and parameters)
    */
-  isEqual(other) {
+  isEqual(other, { compareResultsMetadata = true } = {}) {
     if (!other) {
       return false;
-    } else if (this.id() !== other.id()) {
+    }
+    if (this.id() !== other.id()) {
       return false;
-    } else if (!_.isEqual(this.card(), other.card())) {
+    }
+
+    const card = this.card();
+    const otherCard = other.card();
+    const areCardsEqual = compareResultsMetadata
+      ? _.isEqual(card, otherCard)
+      : _.isEqual(
+          _.omit(card, "result_metadata"),
+          _.omit(otherCard, "result_metadata"),
+        );
+
+    if (!areCardsEqual) {
       return false;
-    } else if (!_.isEqual(this.parameters(), other.parameters())) {
+    }
+
+    if (!_.isEqual(this.parameters(), other.parameters())) {
       return false;
     }
 
