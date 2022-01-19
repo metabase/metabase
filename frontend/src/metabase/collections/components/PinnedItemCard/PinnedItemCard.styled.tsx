38efe6870cc0964588ea31923bac402adb848295
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-01-18 18:09:35 -0800

    Add PinnedItemCard component

---
+++ frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.styled.tsx
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+
+import { color } from "metabase/lib/colors";
+import { forwardRefToInnerRef } from "metabase/styled-components/utils";
+import Icon from "metabase/components/Icon";
+import Link from "metabase/components/Link";
+import Card from "metabase/components/Card";
+import EntityItem from "metabase/components/EntityItem";
+
+export const ItemCard = styled(Card)``;
+
+export const ItemLink = styled(Link)`
+  display: block;
+
+  &:focus {
+    ${ItemCard} {
+      outline: 1px solid ${color("brand")};
+    }
+  }
+`;
+
+export const ItemIcon = styled(Icon)`
+  color: ${color("brand")};
+  height: 1.7rem;
+  width: 1.7rem;
+`;
+
+export const HoverMenu = styled(EntityItem.Menu)`
+  visibility: hidden;
+  color: ${color("text-medium")};
+
+  &:hover {
+    color: ${color("text-dark")};
+  }
+
+  &.EntityMenu--open {
+    visibility: visible;
+    color: ${color("text-dark")};
+  }
+`;
+
+export const Title = styled.div`
+  font-weight: bold;
+  font-size: 1.15rem;
+  line-height: 1.7rem;
+  color: ${color("text-dark")};
+  transition: color 0.2s ease;
+`;
+
+export const Description = forwardRefToInnerRef(styled.div`
+  color: ${color("text-medium")};
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`);
+
+export const Body = styled.div`
+  padding: 1.7rem;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+
+  &:hover {
+    ${Title} {
+      color: ${color("brand")};
+    }
+
+    ${HoverMenu} {
+      visibility: visible;
+    }
+  }
+`;
+
+export const Header = styled.div`
+  padding-bottom: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
---
+++ frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.tsx
@@ -0,0 +1,119 @@
+import React, { useState, useCallback } from "react";
+
+import Tooltip from "metabase/components/Tooltip";
+import { ANALYTICS_CONTEXT } from "metabase/collections/constants";
+
+import {
+  ItemLink,
+  ItemCard,
+  Header,
+  Body,
+  ItemIcon,
+  Title,
+  Description,
+  HoverMenu,
+} from "./PinnedItemCard.styled";
+
+type Item = {
+  name: string;
+  description: string | null;
+  collection_position?: number | null;
+  id: number;
+  getIcon: () => { name: string };
+  getUrl: () => string;
+  setArchived: (isArchived: boolean) => void;
+  copy?: boolean;
+  setCollection?: boolean;
+};
+
+type Collection = {
+  can_write: boolean;
+};
+
+type Props = {
+  className?: string;
+  item: Item;
+  collection: Collection;
+  onToggleSelected: (item: Item) => void;
+  onCopy: (items: Item[]) => void;
+  onMove: (items: Item[]) => void;
+};
+
+function PinnedItemCard({
+  className,
+  item,
+  collection,
+  onToggleSelected,
+  onCopy,
+  onMove,
+}: Props) {
+  const [showDescriptionTooltip, setShowDescriptionTooltip] = useState(false);
+  const icon = item.getIcon().name;
+  const { description, name } = item;
+
+  const maybeEnableDescriptionTooltip = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    const target = event.target as HTMLDivElement;
+    // check if the description is wider than the card
+    if (target && target.scrollWidth > target.clientWidth) {
+      setShowDescriptionTooltip(true);
+    }
+  };
+
+  const handlePin = useCallback(() => {
+    onToggleSelected(item);
+  }, [item, onToggleSelected]);
+
+  const handleCopy = useCallback(() => {
+    onCopy([item]);
+  }, [item, onCopy]);
+
+  const handleMove = useCallback(() => {
+    onMove([item]);
+  }, [item, onMove]);
+
+  const handleArchive = useCallback(() => {
+    item.setArchived(true);
+  }, [item]);
+
+  return (
+    <ItemLink to={item.getUrl()}>
+      <ItemCard className={className}>
+        <Body>
+          <Header>
+            <ItemIcon name={icon} />
+            <HoverMenu
+              item={item}
+              onPin={collection.can_write ? handlePin : null}
+              onMove={
+                collection.can_write && item.setCollection ? handleMove : null
+              }
+              onCopy={item.copy ? handleCopy : null}
+              onArchive={
+                collection.can_write && item.setArchived ? handleArchive : null
+              }
+              analyticsContext={ANALYTICS_CONTEXT}
+              className={undefined}
+            />
+          </Header>
+          <Title>{name}</Title>
+          <Tooltip
+            tooltip={description}
+            placement="bottom"
+            maxWidth={450}
+            isEnabled={showDescriptionTooltip}
+          >
+            {description && (
+              <Description onMouseEnter={maybeEnableDescriptionTooltip}>
+                {description}
+              </Description>
+            )}
+          </Tooltip>
+        </Body>
+      </ItemCard>
+    </ItemLink>
+  );
+}
+
+export default PinnedItemCard;
