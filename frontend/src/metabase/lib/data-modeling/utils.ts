0525e11b5d2b598cfe5b795d4a4930a461ceeb7d
Anton Kulyk <kuliks.anton@gmail.com>
2022-01-19 20:03:01 +0200

    Add utility to check if DB supports nested queries

--- frontend/src/metabase/lib/data-modeling/utils.ts
+++ frontend/src/metabase/lib/data-modeling/utils.ts
@@ -1,15 +1,29 @@
 import Question from "metabase-lib/lib/Question";
 import NativeQuery from "metabase-lib/lib/queries/NativeQuery";
+import Database from "metabase-lib/lib/metadata/Database";
 import { TemplateTag } from "metabase-types/types/Query";
 
 export function isSupportedTemplateTagForModel(tag: TemplateTag) {
   return tag.type === "card";
 }
 
+export function checkDatabaseSupportsModels(database: Database) {
+  return database.hasFeature("nested-queries");
+}
+
 export function checkCanBeModel(question: Question) {
+  const query = question.query();
+  const db = query.database();
+
+  if (!db || !checkDatabaseSupportsModels(db)) {
+    return false;
+  }
+
   if (!question.isNative()) {
     return true;
   }
-  const query = (question.query() as unknown) as NativeQuery;
-  return query.templateTags().every(isSupportedTemplateTagForModel);
+
+  return (query as NativeQuery)
+    .templateTags()
+    .every(isSupportedTemplateTagForModel);
 }
--- frontend/src/metabase/lib/data-modeling/utils.unit.spec.ts
+++ frontend/src/metabase/lib/data-modeling/utils.unit.spec.ts
@@ -1,27 +1,34 @@
 import Question from "metabase-lib/lib/Question";
+import Database from "metabase-lib/lib/metadata/Database";
 import {
   TemplateTag,
   TemplateTagType,
   TemplateTags,
 } from "metabase-types/types/Query";
-import { ORDERS } from "__support__/sample_database_fixture";
+import { createMockDatabase } from "metabase-types/api/mocks/database";
+import { ORDERS, metadata } from "__support__/sample_database_fixture";
 import { checkCanBeModel } from "./utils";
 
 function getNativeQuestion(tags: TemplateTags = {}) {
-  return new Question({
-    id: 1,
-    display: "table",
-    can_write: true,
-    public_uuid: "",
-    dataset_query: {
-      type: "native",
-      native: {
-        query: "select * from orders",
-        "template-tags": tags,
+  const question = new Question(
+    {
+      id: 1,
+      display: "table",
+      can_write: true,
+      public_uuid: "",
+      dataset_query: {
+        type: "native",
+        database: 1,
+        native: {
+          query: "select * from orders",
+          "template-tags": tags,
+        },
       },
+      visualization_settings: {},
     },
-    visualization_settings: {},
-  });
+    metadata,
+  );
+  return question;
 }
 
 function getTemplateTag(tag: Partial<TemplateTag> = {}): TemplateTag {
@@ -35,6 +42,11 @@ function getTemplateTag(tag: Partial<TemplateTag> = {}): TemplateTag {
 }
 
 describe("data model utils", () => {
+  const DB_WITHOUT_NESTED_QUERIES_SUPPORT = new Database({
+    ...createMockDatabase(),
+    features: [],
+  });
+
   describe("checkCanBeModel", () => {
     const UNSUPPORTED_TEMPLATE_TAG_TYPES: TemplateTagType[] = [
       "text",
@@ -43,38 +55,54 @@ describe("data model utils", () => {
       "dimension",
     ];
 
-    it("returns true for structured queries", () => {
-      const question = ORDERS.question();
-      expect(checkCanBeModel(question)).toBe(true);
-    });
-
-    it("returns true for native queries without variables", () => {
-      const question = getNativeQuestion();
-      expect(checkCanBeModel(question)).toBe(true);
-    });
-
-    it("returns true for native queries with 'card' variables", () => {
-      const question = getNativeQuestion({
-        "#5": getTemplateTag({ type: "card" }),
+    describe("structured queries", () => {
+      it("returns true for regular questions", () => {
+        const question = ORDERS.question();
+        expect(checkCanBeModel(question)).toBe(true);
       });
-      expect(checkCanBeModel(question)).toBe(true);
-    });
 
-    UNSUPPORTED_TEMPLATE_TAG_TYPES.forEach(tagType => {
-      it(`returns false false for native queries with '${tagType}' variables`, () => {
-        const question = getNativeQuestion({
-          foo: getTemplateTag({ type: tagType }),
-        });
+      it("returns false if database does not support nested queries", () => {
+        const question = ORDERS.question();
+        question.query().database = () => DB_WITHOUT_NESTED_QUERIES_SUPPORT;
         expect(checkCanBeModel(question)).toBe(false);
       });
     });
 
-    it("returns false for native queries if it uses at least one unsupported variable type", () => {
-      const question = getNativeQuestion({
-        "#5": getTemplateTag({ type: "card" }),
-        foo: getTemplateTag({ type: "dimension" }),
+    describe("native queries", () => {
+      it("returns true if no variables used", () => {
+        const question = getNativeQuestion();
+        expect(checkCanBeModel(question)).toBe(true);
       });
-      expect(checkCanBeModel(question)).toBe(false);
+
+      it("returns false if database does not support nested queries", () => {
+        const question = getNativeQuestion();
+        question.query().database = () => DB_WITHOUT_NESTED_QUERIES_SUPPORT;
+
+        expect(checkCanBeModel(question)).toBe(false);
+      });
+      it("returns true when 'card' variables are used", () => {
+        const question = getNativeQuestion({
+          "#5": getTemplateTag({ type: "card" }),
+        });
+        expect(checkCanBeModel(question)).toBe(true);
+      });
+
+      UNSUPPORTED_TEMPLATE_TAG_TYPES.forEach(tagType => {
+        it(`returns false when '${tagType}' variables are used`, () => {
+          const question = getNativeQuestion({
+            foo: getTemplateTag({ type: tagType }),
+          });
+          expect(checkCanBeModel(question)).toBe(false);
+        });
+      });
+
+      it("returns false if at least one unsupported variable type is used", () => {
+        const question = getNativeQuestion({
+          "#5": getTemplateTag({ type: "card" }),
+          foo: getTemplateTag({ type: "dimension" }),
+        });
+        expect(checkCanBeModel(question)).toBe(false);
+      });
     });
   });
 });
