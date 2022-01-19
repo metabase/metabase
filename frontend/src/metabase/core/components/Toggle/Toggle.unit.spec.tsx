122bf2f4e82d3728a844b69881dfd815c25d42ca
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-19 15:44:52 +0300

    Add focus ring

--- frontend/src/metabase/core/components/Toggle/Toggle.unit.spec.tsx
+++ frontend/src/metabase/core/components/Toggle/Toggle.unit.spec.tsx
@@ -1,27 +1,53 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Toggle from "./Toggle";
 
 describe("Toggle", () => {
+  it("should receive focus on tab", () => {
+    const onChange = jest.fn();
+
+    render(<Toggle value={false} onChange={onChange} />);
+
+    const checkbox = screen.getByRole("checkbox");
+    userEvent.tab();
+
+    expect(checkbox).toHaveFocus();
+  });
+
+  it("should toggle on enter", () => {
+    const onChange = jest.fn();
+
+    render(<Toggle value={false} onChange={onChange} />);
+
+    const checkbox = screen.getByRole("checkbox");
+    userEvent.tab();
+    userEvent.type(checkbox, "{enter}");
+
+    expect(onChange).toHaveBeenCalledWith(true);
+  });
+
+  it("should toggle on space", () => {
+    const onChange = jest.fn();
+
+    render(<Toggle value={false} onChange={onChange} />);
+
+    const checkbox = screen.getByRole("checkbox");
+    userEvent.tab();
+    userEvent.type(checkbox, "{space}");
+
+    expect(onChange).toHaveBeenCalledWith(true);
+  });
+
   it("should toggle on click", () => {
     const onChange = jest.fn();
 
     render(<Toggle value={false} onChange={onChange} />);
 
-    const toggle = screen.getByRole("checkbox");
-    userEvent.click(toggle);
+    const checkbox = screen.getByRole("checkbox");
+    userEvent.click(checkbox);
 
-    expect(toggle).toHaveFocus();
+    expect(checkbox).toHaveFocus();
     expect(onChange).toHaveBeenCalledWith(true);
-  });
-
-  it("should receive focus on tab", () => {
-    render(<Toggle value={false} />);
-
-    const toggle = screen.getByRole("checkbox");
-    userEvent.tab();
-
-    expect(toggle).toHaveFocus();
   });
 });
