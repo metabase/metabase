4cc863df639488b7260ae4465e7036e30fd262c9
Cam Saul <github@camsaul.com>
2022-01-20 11:04:18 -0800

    Update test for new error message

--- src/metabase/models/field.clj
+++ src/metabase/models/field.clj
@@ -66,8 +66,8 @@
       (when-not (some
                  (partial isa? k)
                  ancestor-types)
-        (let [message (tru "Invalid value for Field column {0}: {1} is not an descendant of any of these types: {2}"
-                           (pr-str k) (pr-str column-name) (pr-str ancestor-types))]
+        (let [message (tru "Invalid value for Field column {0}: {1} is not a descendant of any of these types: {2}"
+                           (pr-str column-name) (pr-str k) (pr-str ancestor-types))]
           (throw (ex-info message
                           {:status-code       400
                            :errors            {column-name message}
