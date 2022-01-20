546ff3040b0ff53190266201650de6f3c86011d2
Anton Kulyk <kuliks.anton@gmail.com>
2022-01-20 20:51:27 +0200

    Fix model metadata strength calculation (#19803)
    * Use `display_name` for metadata completeness calc
    
    * Fix unit tests

--- frontend/src/metabase/lib/data-modeling/metadata.unit.spec.js
+++ frontend/src/metabase/lib/data-modeling/metadata.unit.spec.js
@@ -7,8 +7,8 @@ describe("getDatasetMetadataCompletenessPercentage", () => {
 
   it("returns 0 for completely missing metadata", () => {
     const percent = getDatasetMetadataCompletenessPercentage([
-      { name: "Created_At" },
-      { name: "Products → Category" },
+      { display_name: "Created_At" },
+      { display_name: "Products → Category" },
     ]);
     expect(percent).toBe(0);
   });
@@ -16,12 +16,12 @@ describe("getDatasetMetadataCompletenessPercentage", () => {
   it("returns 1 for complete metadata", () => {
     const percent = getDatasetMetadataCompletenessPercentage([
       {
-        name: "Created At",
+        display_name: "Created At",
         description: "Date created",
         semantic_type: "DateTime",
       },
       {
-        name: "Product Category",
+        display_name: "Product Category",
         description: "The name is pretty self-explaining",
         semantic_type: "String",
       },
@@ -31,9 +31,9 @@ describe("getDatasetMetadataCompletenessPercentage", () => {
 
   it("returns 0.5 for half-complete metadata", () => {
     const percent = getDatasetMetadataCompletenessPercentage([
-      { name: "Created_At" },
+      { display_name: "Created_At" },
       {
-        name: "Product Category",
+        display_name: "Product Category",
         description: "The name is pretty self-explaining",
         semantic_type: "String",
       },
@@ -43,9 +43,9 @@ describe("getDatasetMetadataCompletenessPercentage", () => {
 
   it("returns percent value for partially complete metadata", () => {
     const percent = getDatasetMetadataCompletenessPercentage([
-      { name: "Created_At" },
+      { display_name: "Created_At" },
       {
-        name: "Product Category",
+        display_name: "Product Category",
         semantic_type: "String",
       },
     ]);
