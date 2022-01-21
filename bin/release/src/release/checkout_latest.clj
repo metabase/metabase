26422d4424d593e3d62d9a60f7c06e47dea5b89c
Ariya Hidayat <ariya@metabase.com>
2022-01-21 07:55:34 -0800

    Release script: no need for `git pull`

--- bin/release/src/release/checkout_latest.clj
+++
@@ -1,7 +0,0 @@
-(ns release.checkout-latest
-  (:require [metabuild-common.core :as u]
-            [release.common :as c]))
-
-(defn checkout-latest! []
-  (u/step (format "Checkout latest code for %s" (c/branch))
-    (u/sh {:dir u/project-root-directory} "git" "pull")))
