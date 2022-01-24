65b9630f255539f3f6e2a40f2e745782bd80a079
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-01-24 21:11:35 +0000

    fix license and billing page

--- frontend/src/metabase/admin/settings/components/SettingsLicense/SettingsLicense.tsx
+++ frontend/src/metabase/admin/settings/components/SettingsLicense/SettingsLicense.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import MetabaseSettings from "metabase/lib/settings";
-import { StarterContent } from "./content/StarterContent";
 import { UnlicensedContent } from "./content/UnlicensedContent";
 import { PLUGIN_LICENSE_PAGE } from "metabase/plugins";
 import { SettingsLicenseContainer } from "./SettingsLicense.styled";
@@ -13,17 +12,6 @@ const SettingsLicense = () => {
     return (
       <SettingsLicenseContainer>
         <UnlicensedContent />
-      </SettingsLicenseContainer>
-    );
-  }
-
-  const isStarter =
-    MetabaseSettings.isHosted() && !MetabaseSettings.isEnterprise();
-
-  if (isStarter) {
-    return (
-      <SettingsLicenseContainer>
-        <StarterContent />
       </SettingsLicenseContainer>
     );
   }
