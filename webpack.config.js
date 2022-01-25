704f3c7abb649d21e9dc03e0d5c5cac2da105e94
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-01-25 23:43:58 +0000

    allow disabling eslint

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,9 @@ const BUILD_PATH = __dirname + "/resources/frontend_client";
 const WEBPACK_BUNDLE = process.env.WEBPACK_BUNDLE || "development";
 const devMode = WEBPACK_BUNDLE !== "production";
 const useFilesystemCache = process.env.FS_CACHE === "true";
+const shouldUseEslint =
+  process.env.WEBPACK_BUNDLE !== "production" &&
+  process.env.DISABLE_ESLINT !== "true";
 
 // Babel:
 const BABEL_CONFIG = {
@@ -68,18 +71,22 @@ const config = (module.exports = {
         exclude: /node_modules|cljs/,
         use: [{ loader: "babel-loader", options: BABEL_CONFIG }],
       },
-      {
-        test: /\.(tsx?|jsx?)$/,
-        exclude: /node_modules|cljs|\.spec\.js/,
-        use: [
-          {
-            loader: "eslint-loader",
-            options: {
-              rulePaths: [__dirname + "/frontend/lint/eslint-rules"],
+      ...(shouldUseEslint
+        ? [
+            {
+              test: /\.(tsx?|jsx?)$/,
+              exclude: /node_modules|cljs|\.spec\.js/,
+              use: [
+                {
+                  loader: "eslint-loader",
+                  options: {
+                    rulePaths: [__dirname + "/frontend/lint/eslint-rules"],
+                  },
+                },
+              ],
             },
-          },
-        ],
-      },
+          ]
+        : []),
       {
         test: /\.(eot|woff2?|ttf|svg|png)$/,
         type: "asset/resource",
@@ -290,13 +297,6 @@ if (WEBPACK_BUNDLE !== "production") {
     }),
   );
 } else {
-  // Don't bother with ESLint for CI/production (we catch linting errors with another CI run)
-  config.module.rules = config.module.rules.filter(rule => {
-    return Array.isArray(rule.use)
-      ? rule.use[0].loader != "eslint-loader"
-      : true;
-  });
-
   config.plugins.push(
     new TerserPlugin({ parallel: true, test: /\.(tsx?|jsx?)($|\?)/i }),
   );
