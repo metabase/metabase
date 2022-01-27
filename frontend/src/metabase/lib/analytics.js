ed9665a6cfa54cc97e2962b8cacabdc9b14ed582
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-01-27 18:06:07 +0300

    Fix original question check for analytics

--- frontend/src/metabase/lib/analytics.js
+++ frontend/src/metabase/lib/analytics.js
@@ -31,7 +31,7 @@ export const trackPageView = url => {
 };
 
 export const trackStructEvent = (category, action, label, value) => {
-  if (!category || !label || Settings.trackingEnabled()) {
+  if (!category || !label || !Settings.trackingEnabled()) {
     return;
   }
 
@@ -41,7 +41,7 @@ export const trackStructEvent = (category, action, label, value) => {
 };
 
 export const trackSchemaEvent = (schema, version, data) => {
-  if (!schema || Settings.trackingEnabled()) {
+  if (!schema || !Settings.trackingEnabled()) {
     return;
   }
 
