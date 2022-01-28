4d96ca05e707338ddf2915b4c9ae786e6510021c
Anton Kulyk <kuliks.anton@gmail.com>
2022-01-28 16:05:08 +0200

    Update QuickFilterDrill tests

--- frontend/test/metabase/modes/components/drill/QuickFilterDrill.unit.spec.js
+++ frontend/test/metabase/modes/components/drill/QuickFilterDrill.unit.spec.js
@@ -1,45 +1,74 @@
-/* eslint-disable */
-
 import QuickFilterDrill from "metabase/modes/components/drill/QuickFilterDrill";
-
 import { ORDERS } from "__support__/sample_database_fixture";
+
+function setup({ question = ORDERS.question(), clicked } = {}) {
+  return QuickFilterDrill({ question, clicked });
+}
+
+const NUMBER_AND_DATE_FILTERS = ["<", ">", "=", "!="];
 
 describe("QuickFilterDrill", () => {
   it("should not be valid for top level actions", () => {
-    expect(QuickFilterDrill({ question: ORDERS.question() })).toHaveLength(0);
+    const actions = setup({ question: ORDERS.question() });
+    expect(actions).toHaveLength(0);
   });
-  it("should be valid for click on numeric cell", () => {
-    const actions = QuickFilterDrill({
-      question: ORDERS.question(),
+
+  describe("numeric cells", () => {
+    const CELL_VALUE = 42;
+    const actions = setup({
       clicked: {
         column: ORDERS.TOTAL.column(),
-        value: 42,
+        value: CELL_VALUE,
       },
     });
-    expect(actions.length).toEqual(4);
-    let newCard = actions[0].question().card();
-    expect(newCard.dataset_query.query).toEqual({
-      "source-table": ORDERS.id,
-      filter: ["<", ["field", ORDERS.TOTAL.id, null], 42],
+
+    it("should return correct filters", () => {
+      const filters = NUMBER_AND_DATE_FILTERS.map(operator => ({
+        name: operator,
+      }));
+      expect(actions).toMatchObject(filters);
     });
-    expect(newCard.display).toEqual("table");
+
+    actions.forEach((action, i) => {
+      const operator = NUMBER_AND_DATE_FILTERS[i];
+      it(`should correctly apply "${operator}" filter`, () => {
+        const card = action.question().card();
+        expect(card.dataset_query.query).toEqual({
+          "source-table": ORDERS.id,
+          filter: [operator, ["field", ORDERS.TOTAL.id, null], CELL_VALUE],
+        });
+        expect(card.display).toBe("table");
+      });
+    });
   });
-  it('should be valid for click on joined "field" numeric cell ', () => {
-    const actions = QuickFilterDrill({
-      question: ORDERS.question(),
+
+  describe("joined numeric field cell", () => {
+    const CELL_VALUE = 42;
+    const joinedFieldRef = ["field", ORDERS.TOTAL.id, { "join-alias": "foo" }];
+    const actions = setup({
       clicked: {
-        column: ORDERS.TOTAL.column({
-          field_ref: ["field", ORDERS.TOTAL.id, { "join-alias": "foo" }],
-        }),
-        value: 42,
+        column: ORDERS.TOTAL.column({ field_ref: joinedFieldRef }),
+        value: CELL_VALUE,
       },
     });
-    expect(actions.length).toEqual(4);
-    let newCard = actions[0].question().card();
-    expect(newCard.dataset_query.query).toEqual({
-      "source-table": ORDERS.id,
-      filter: ["<", ["field", ORDERS.TOTAL.id, { "join-alias": "foo" }], 42],
+
+    it("should return correct filters", () => {
+      const filters = NUMBER_AND_DATE_FILTERS.map(operator => ({
+        name: operator,
+      }));
+      expect(actions).toMatchObject(filters);
     });
-    expect(newCard.display).toEqual("table");
+
+    actions.forEach((action, i) => {
+      const operator = NUMBER_AND_DATE_FILTERS[i];
+      it(`should correctly apply "${operator}" filter`, () => {
+        const card = action.question().card();
+        expect(card.dataset_query.query).toEqual({
+          "source-table": ORDERS.id,
+          filter: [operator, joinedFieldRef, CELL_VALUE],
+        });
+        expect(card.display).toBe("table");
+      });
+    });
   });
 });
