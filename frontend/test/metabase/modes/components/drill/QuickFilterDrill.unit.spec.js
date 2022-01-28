5e8011ebaed57d1d3984f857e3d17dc2bce172f7
Anton Kulyk <kuliks.anton@gmail.com>
2022-01-28 20:05:32 +0200

    Extend QuickFilterDrill unit tests

--- frontend/test/metabase/modes/components/drill/QuickFilterDrill.unit.spec.js
+++ frontend/test/metabase/modes/components/drill/QuickFilterDrill.unit.spec.js
@@ -1,36 +1,75 @@
+import Question from "metabase-lib/lib/Question";
 import QuickFilterDrill from "metabase/modes/components/drill/QuickFilterDrill";
-import { ORDERS, PEOPLE } from "__support__/sample_database_fixture";
-
-function setup({ question = ORDERS.question(), clicked } = {}) {
-  return QuickFilterDrill({ question, clicked });
-}
+import { createMockColumn } from "metabase-types/types/mocks/dataset";
+import {
+  ORDERS,
+  PEOPLE,
+  SAMPLE_DATABASE,
+} from "__support__/sample_database_fixture";
 
 const NUMBER_AND_DATE_FILTERS = ["<", ">", "=", "!="];
-
 const OTHER_FILTERS = ["=", "!="];
+
+const DEFAULT_NUMERIC_CELL_VALUE = 42;
+
+const AGGREGATED_QUERY = {
+  aggregation: [["count"]],
+  breakout: ["field", ORDERS.CREATED_AT.id, { "temporal-unit": "month" }],
+  "source-table": ORDERS.id,
+};
+
+const AGGREGATED_QUESTION = {
+  display: "table",
+  dataset_query: {
+    type: "query",
+    query: AGGREGATED_QUERY,
+    database: SAMPLE_DATABASE.id,
+  },
+};
+
+const NESTED_QUESTION_SOURCE_TABLE_ID = "card__58";
+const NESTED_QUESTION = {
+  display: "table",
+  dataset_query: {
+    type: "query",
+    query: {
+      "source-table": NESTED_QUESTION_SOURCE_TABLE_ID,
+    },
+    database: SAMPLE_DATABASE.id,
+  },
+};
+
+function setup({
+  question = ORDERS.question(),
+  column,
+  value = DEFAULT_NUMERIC_CELL_VALUE,
+} = {}) {
+  const actions = QuickFilterDrill({
+    question,
+    clicked: { column, value },
+  });
+  return {
+    actions,
+    cellValue: value,
+  };
+}
 
 describe("QuickFilterDrill", () => {
   it("should not be valid for top level actions", () => {
-    const actions = setup({ question: ORDERS.question() });
+    const actions = QuickFilterDrill({ question: ORDERS.question() });
     expect(actions).toHaveLength(0);
   });
 
   it("should not be valid for PK cells", () => {
-    const actions = setup({
-      clicked: {
-        column: ORDERS.ID.column(),
-        value: 1,
-      },
-    });
+    const { actions } = setup({ column: ORDERS.ID.column() });
     expect(actions).toHaveLength(0);
   });
 
   describe("FK cells", () => {
-    const actions = setup({
-      clicked: {
-        column: ORDERS.PRODUCT_ID.column(),
-        value: 1,
-      },
+    const FK_VALUE = 1;
+    const { actions } = setup({
+      column: ORDERS.PRODUCT_ID.column(),
+      value: FK_VALUE,
     });
 
     it("should return only 'view this records' filter", () => {
@@ -42,19 +81,14 @@ describe("QuickFilterDrill", () => {
       const card = action.question().card();
       expect(card.dataset_query.query).toEqual({
         "source-table": ORDERS.id,
-        filter: ["=", ["field", ORDERS.PRODUCT_ID.id, null], 1],
+        filter: ["=", ORDERS.PRODUCT_ID.reference(), FK_VALUE],
       });
     });
   });
 
   describe("numeric cells", () => {
-    const CELL_VALUE = 42;
-    const actions = setup({
-      clicked: {
-        column: ORDERS.TOTAL.column(),
-        value: CELL_VALUE,
-      },
-    });
+    const clickedField = ORDERS.TOTAL;
+    const { actions } = setup({ column: clickedField.column() });
 
     it("should return correct filters", () => {
       const filters = NUMBER_AND_DATE_FILTERS.map(operator => ({
@@ -69,7 +103,11 @@ describe("QuickFilterDrill", () => {
         const card = action.question().card();
         expect(card.dataset_query.query).toEqual({
           "source-table": ORDERS.id,
-          filter: [operator, ["field", ORDERS.TOTAL.id, null], CELL_VALUE],
+          filter: [
+            operator,
+            clickedField.reference(),
+            DEFAULT_NUMERIC_CELL_VALUE,
+          ],
         });
         expect(card.display).toBe("table");
       });
@@ -77,13 +115,9 @@ describe("QuickFilterDrill", () => {
   });
 
   describe("joined numeric field cell", () => {
-    const CELL_VALUE = 42;
     const joinedFieldRef = ["field", ORDERS.TOTAL.id, { "join-alias": "foo" }];
-    const actions = setup({
-      clicked: {
-        column: ORDERS.TOTAL.column({ field_ref: joinedFieldRef }),
-        value: CELL_VALUE,
-      },
+    const { actions, cellValue } = setup({
+      column: ORDERS.TOTAL.column({ field_ref: joinedFieldRef }),
     });
 
     it("should return correct filters", () => {
@@ -99,7 +133,76 @@ describe("QuickFilterDrill", () => {
         const card = action.question().card();
         expect(card.dataset_query.query).toEqual({
           "source-table": ORDERS.id,
-          filter: [operator, joinedFieldRef, CELL_VALUE],
+          filter: [operator, joinedFieldRef, cellValue],
+        });
+        expect(card.display).toBe("table");
+      });
+    });
+  });
+
+  describe("aggregated numeric cell", () => {
+    const { actions, cellValue } = setup({
+      question: new Question(AGGREGATED_QUESTION),
+      column: createMockColumn({
+        name: "count",
+        field_ref: ["aggregation", 0],
+        base_type: "type/BigInteger",
+        semantic_type: "type/Quantity",
+        source: "aggregation",
+      }),
+    });
+
+    it("should return correct filters", () => {
+      const filters = NUMBER_AND_DATE_FILTERS.map(operator => ({
+        name: operator,
+      }));
+      expect(actions).toMatchObject(filters);
+    });
+
+    actions.forEach((action, i) => {
+      const operator = NUMBER_AND_DATE_FILTERS[i];
+      it(`should correctly apply "${operator}" filter`, () => {
+        const card = action.question().card();
+        expect(card.dataset_query.query).toEqual({
+          "source-query": AGGREGATED_QUERY,
+          filter: [
+            operator,
+            ["field", "count", { "base-type": "type/BigInteger" }],
+            cellValue,
+          ],
+        });
+        expect(card.display).toBe("table");
+      });
+    });
+  });
+
+  describe("numeric field cell from a nested query", () => {
+    const fieldRef = ["field", "count", { "base-type": "type/BigInteger" }];
+    const { actions, cellValue } = setup({
+      question: new Question(NESTED_QUESTION),
+      column: createMockColumn({
+        name: "count",
+        field_ref: fieldRef,
+        base_type: "type/BigInteger",
+        semantic_type: "type/Quantity",
+        source: "aggregation",
+      }),
+    });
+
+    it("should return correct filters", () => {
+      const filters = NUMBER_AND_DATE_FILTERS.map(operator => ({
+        name: operator,
+      }));
+      expect(actions).toMatchObject(filters);
+    });
+
+    actions.forEach((action, i) => {
+      const operator = NUMBER_AND_DATE_FILTERS[i];
+      it(`should correctly apply "${operator}" filter`, () => {
+        const card = action.question().card();
+        expect(card.dataset_query.query).toEqual({
+          "source-table": NESTED_QUESTION_SOURCE_TABLE_ID,
+          filter: [operator, fieldRef, cellValue],
         });
         expect(card.display).toBe("table");
       });
@@ -108,11 +211,9 @@ describe("QuickFilterDrill", () => {
 
   describe("date-time cells", () => {
     const CELL_VALUE = new Date().toISOString();
-    const actions = setup({
-      clicked: {
-        column: ORDERS.CREATED_AT.column(),
-        value: CELL_VALUE,
-      },
+    const { actions } = setup({
+      column: ORDERS.CREATED_AT.column(),
+      value: CELL_VALUE,
     });
 
     it("should return correct filters", () => {
@@ -128,7 +229,7 @@ describe("QuickFilterDrill", () => {
         const card = action.question().card();
         expect(card.dataset_query.query).toEqual({
           "source-table": ORDERS.id,
-          filter: [operator, ["field", ORDERS.CREATED_AT.id, null], CELL_VALUE],
+          filter: [operator, ORDERS.CREATED_AT.reference(), CELL_VALUE],
         });
         expect(card.display).toBe("table");
       });
@@ -137,12 +238,10 @@ describe("QuickFilterDrill", () => {
 
   describe("string cells", () => {
     const CELL_VALUE = "Joe";
-    const actions = setup({
+    const { actions } = setup({
       question: PEOPLE.question(),
-      clicked: {
-        column: PEOPLE.NAME.column(),
-        value: CELL_VALUE,
-      },
+      column: PEOPLE.NAME.column(),
+      value: CELL_VALUE,
     });
 
     it("should return correct filters", () => {
@@ -158,7 +257,7 @@ describe("QuickFilterDrill", () => {
         const card = action.question().card();
         expect(card.dataset_query.query).toEqual({
           "source-table": PEOPLE.id,
-          filter: [operator, ["field", PEOPLE.NAME.id, null], CELL_VALUE],
+          filter: [operator, PEOPLE.NAME.reference(), CELL_VALUE],
         });
         expect(card.display).toBe("table");
       });
@@ -166,13 +265,9 @@ describe("QuickFilterDrill", () => {
   });
 
   describe("numeric cells, but not semantically numbers", () => {
-    const CELL_VALUE = 12345;
-    const actions = setup({
+    const { actions, cellValue } = setup({
       question: PEOPLE.question(),
-      clicked: {
-        column: PEOPLE.ZIP.column(),
-        value: CELL_VALUE,
-      },
+      column: PEOPLE.ZIP.column(),
     });
 
     it("should return correct filters", () => {
@@ -188,7 +283,7 @@ describe("QuickFilterDrill", () => {
         const card = action.question().card();
         expect(card.dataset_query.query).toEqual({
           "source-table": PEOPLE.id,
-          filter: [operator, ["field", PEOPLE.ZIP.id, null], CELL_VALUE],
+          filter: [operator, PEOPLE.ZIP.reference(), cellValue],
         });
         expect(card.display).toBe("table");
       });
