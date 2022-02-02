4f4f90ffea52258aa6e31e58abe98671e046e90b
Anton Kulyk <kuliks.anton@gmail.com>
2022-02-02 13:48:27 +0200

    Use actual model query without data permissions

--- frontend/src/metabase/query_builder/selectors.js
+++ frontend/src/metabase/query_builder/selectors.js
@@ -232,7 +232,13 @@ export const getQuestion = createSelector(
     // When opening a model, we swap it's `dataset_query`
     // with clean query using the model as a source table,
     // to enable "simple mode" like features
-    return question.isDataset() ? question.composeDataset() : question;
+    // This has to be skipped for users without data permissions
+    // as it would be blocked by the backend as an ad-hoc query
+    // see https://github.com/metabase/metabase/issues/20042
+    const hasDataPermission = !!question.database();
+    return question.isDataset() && hasDataPermission
+      ? question.composeDataset()
+      : question;
   },
 );
 
