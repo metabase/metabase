3527d527e12c650f1bb86ae81e53b88808147af7
Cam Saul <1455846+camsaul@users.noreply.github.com>
2022-02-01 17:49:03 -0800

    Fix regression in supporting `user:port@host` app db connection strings (#20131)
    * Fix regression breaking support for user:password@host Postgres connection strings
    
    * Actually we need to handle either Postgres OR MySQL
    
    * Fix the helper `question` function
    
    * Wait for the card query to load before typing
    
    Co-authored-by: Nemanja <31325167+nemanjaglumac@users.noreply.github.com>

--- test/metabase/db/data_source_test.clj
+++ test/metabase/db/data_source_test.clj
@@ -1,18 +1,53 @@
 (ns metabase.db.data-source-test
-  (:require [clojure.java.jdbc :as jdbc]
-            [clojure.test :refer :all]
-            [metabase.db.data-source :as mdb.data-source]
-            [metabase.test :as mt]))
+  (:require
+   [clojure.java.jdbc :as jdbc]
+   [clojure.test :refer :all]
+   [metabase.config :as config]
+   [metabase.connection-pool :as pool]
+   [metabase.db.data-source :as mdb.data-source]
+   [metabase.test :as mt]))
+
+(defn- ->DataSource [s properties]
+  (#'mdb.data-source/->DataSource s (some-> (not-empty properties) pool/map->properties)))
 
 (deftest broken-out-details-test
-  (let [data-source (mdb.data-source/broken-out-details->DataSource
-                     :h2
-                     {:subprotocol "h2"
-                      :db          (format "mem:%s" (mt/random-name))
-                      :classname   "org.h2.Driver"})]
-    (with-open [conn (.getConnection data-source)]
-      (is (= [{:one 1}]
-             (jdbc/query {:connection conn} "SELECT 1 AS one;"))))))
+  (testing :postgres
+    (is (= (->DataSource
+            "jdbc:postgresql://localhost:5432/metabase"
+            {"password"                      "1234"
+             "ApplicationName"               config/mb-version-and-process-identifier
+             "OpenSourceSubProtocolOverride" "true"
+             "user"                          "cam"})
+           (mdb.data-source/broken-out-details->DataSource :postgres {:host     "localhost"
+                                                                      :port     5432
+                                                                      :user     "cam"
+                                                                      :password "1234"
+                                                                      :db       "metabase"}))))
+
+  (testing :h2
+    (is (= (->DataSource
+            "jdbc:h2:file:/metabase/metabase.db"
+            nil)
+           (mdb.data-source/broken-out-details->DataSource :h2 {:db "file:/metabase/metabase.db"}))))
+
+  (testing :mysql
+    (is (= (->DataSource
+            "jdbc:mysql://localhost:3306/metabase"
+            {"user" "root"})
+           (mdb.data-source/broken-out-details->DataSource :mysql {:host "localhost"
+                                                                   :port 3306
+                                                                   :user "root"
+                                                                   :db   "metabase"}))))
+
+  (testing "end-to-end"
+    (let [data-source (mdb.data-source/broken-out-details->DataSource
+                       :h2
+                       {:subprotocol "h2"
+                        :db          (format "mem:%s" (mt/random-name))
+                        :classname   "org.h2.Driver"})]
+      (with-open [conn (.getConnection data-source)]
+        (is (= [{:one 1}]
+               (jdbc/query {:connection conn} "SELECT 1 AS one;")))))))
 
 (deftest connection-string-test
   (let [data-source (mdb.data-source/raw-connection-string->DataSource
@@ -32,8 +67,32 @@
                (jdbc/query {:connection conn} "SELECT 1 AS one;"))))))
 
   (testing "Accept `postgres` as a subprotocol (I think Heroku or whatever does this to screw with us)"
-    (is (= (mdb.data-source/raw-connection-string->DataSource "jdbc:postgresql:localhost:5432/metabase")
-           (mdb.data-source/raw-connection-string->DataSource "postgres:localhost:5432/metabase")))))
+    (is (= (mdb.data-source/raw-connection-string->DataSource "jdbc:postgresql://localhost:5432/metabase")
+           (mdb.data-source/raw-connection-string->DataSource "postgres://localhost:5432/metabase")))))
+
+(deftest wonky-connection-string-test
+  (testing "Should handle malformed user:password@host:port strings (#14678, #20121)"
+    (doseq [subprotocol ["postgresql" "mysql"]]
+      (testing "user AND password"
+        (is (= (->DataSource
+                (str "jdbc:" subprotocol "://localhost:5432/metabase")
+                {"user" "cam", "password" "1234"})
+               (mdb.data-source/raw-connection-string->DataSource (str subprotocol "://cam:1234@localhost:5432/metabase"))))
+        (testing "no port"
+          (is (= (->DataSource
+                  (str "jdbc:" subprotocol "://localhost/metabase")
+                  {"user" "cam", "password" "1234"})
+                 (mdb.data-source/raw-connection-string->DataSource (str subprotocol "://cam:1234@localhost/metabase"))))))
+      (testing "user only"
+        (is (= (->DataSource
+                (str "jdbc:" subprotocol "://localhost:5432/metabase?password=1234")
+                {"user" "cam"})
+               (mdb.data-source/raw-connection-string->DataSource (str subprotocol "://cam@localhost:5432/metabase?password=1234"))))
+        (testing "no port"
+          (is (= (->DataSource
+                  (str "jdbc:" subprotocol "://localhost/metabase?password=1234")
+                  {"user" "cam"})
+                 (mdb.data-source/raw-connection-string->DataSource (str subprotocol "://cam@localhost/metabase?password=1234")))))))))
 
 (deftest equality-test
   (testing "Two DataSources with the same URL should be equal"
