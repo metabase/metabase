4f428c5db6b3930ee815311e27d9bd86f011780a
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-03 13:08:20 +0300

    Migrate from grid-styled in collections (#20178)

---
+++ frontend/src/metabase/collections/components/ItemsTable.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ItemsTableRoot = styled.div`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem;
+  padding: 4rem;
+`;
