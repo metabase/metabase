b0b8bc80526df8dc126c58181b81f034a95e020d
Anton Kulyk <kuliks.anton@gmail.com>
2022-02-04 20:59:32 +0200

    Allow to unmap native model column from a real one (#20219)
    * Add clear behavior to `SelectButton`
    
    * Removed redundant type
    
    * Allow unmapping model column from a real one

--- frontend/src/metabase/core/components/SelectButton/SelectButton.unit.spec.tsx
+++ frontend/src/metabase/core/components/SelectButton/SelectButton.unit.spec.tsx
@@ -25,4 +25,46 @@ describe("SelectButton", () => {
 
     expect(screen.getByRole("button")).not.toHaveFocus();
   });
+
+  describe("clear behavior", () => {
+    it("should not display clear icon when the value is selected, but onClear prop is not provided", () => {
+      render(<SelectButton hasValue>{title}</SelectButton>);
+      expect(screen.queryByLabelText("close icon")).not.toBeInTheDocument();
+    });
+
+    it("should not display clear icon when the value is not selected", () => {
+      render(
+        <SelectButton hasValue={false} onClear={jest.fn()}>
+          {title}
+        </SelectButton>,
+      );
+      expect(screen.queryByLabelText("close icon")).not.toBeInTheDocument();
+    });
+
+    it("should call onClear when close icon is clicked", () => {
+      const onClear = jest.fn();
+      render(
+        <SelectButton hasValue onClear={onClear}>
+          {title}
+        </SelectButton>,
+      );
+
+      userEvent.click(screen.getByLabelText("close icon"));
+
+      expect(onClear).toHaveBeenCalledTimes(1);
+    });
+
+    it("should not call usual onClick when close icon is clicked", () => {
+      const onClick = jest.fn();
+      render(
+        <SelectButton hasValue onClick={onClick} onClear={jest.fn()}>
+          {title}
+        </SelectButton>,
+      );
+
+      userEvent.click(screen.getByLabelText("close icon"));
+
+      expect(onClick).not.toHaveBeenCalled();
+    });
+  });
 });
