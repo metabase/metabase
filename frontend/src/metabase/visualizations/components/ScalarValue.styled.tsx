894ee5598210e3fc6c64fda05e201cb6ae1f81cd
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-04 16:55:06 +0300

    Migrate "visualizations" from grid-styled (#20215)

---
+++ frontend/src/metabase/visualizations/components/ScalarValue.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ScalarRoot = styled.div`
+  position: relative;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
