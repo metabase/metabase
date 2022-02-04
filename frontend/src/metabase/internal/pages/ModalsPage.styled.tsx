914b4c0c8a27d8306cdc343920020e1af1da9338
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-04 18:01:01 +0300

    Migrate internal

---
+++ frontend/src/metabase/internal/pages/ModalsPage.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PageHeader = styled.div`
+  margin: 2rem 0;
+`;
+
+export const SectionRoot = styled.div`
+  margin: 2rem 0;
+  width: 32.5rem;
+`;
