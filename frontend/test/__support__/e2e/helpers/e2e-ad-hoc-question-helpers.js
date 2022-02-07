1b558d7c0b579b40a7e0a23548a9e653329b54ff
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-02-07 21:39:30 +0100

    Handle pivot tables dataset

--- frontend/test/__support__/e2e/helpers/e2e-ad-hoc-question-helpers.js
+++ frontend/test/__support__/e2e/helpers/e2e-ad-hoc-question-helpers.js
@@ -7,8 +7,19 @@ export function adhocQuestionHash(question) {
 }
 
 export function visitQuestionAdhoc(question) {
-  cy.intercept("POST", "/api/dataset").as("dataset");
+  const {
+    display,
+    dataset_query: { type },
+  } = question;
+
+  const isPivotEndpoint = display === "pivot" && type === "query";
+
+  const url = isPivotEndpoint ? "/api/dataset/pivot" : "/api/dataset";
+  const alias = isPivotEndpoint ? "pivotDataset" : "dataset";
+
+  cy.intercept(url).as(alias);
 
   cy.visit("/question#" + adhocQuestionHash(question));
-  cy.wait("@dataset");
+
+  cy.wait("@" + alias);
 }
