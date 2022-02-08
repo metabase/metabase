f39b726c1ba76d8b43165e1c0b01c7e4daf12837
Anton Kulyk <kuliks.anton@gmail.com>
2022-02-08 12:02:45 +0200

    Add Event and EventTimeline types

---
+++ frontend/src/metabase-types/types/Event.ts
@@ -0,0 +1,24 @@
+import { CollectionId } from "./Collection";
+import { User } from "./User";
+
+export interface Event {
+  id: number;
+  timeline_id: number;
+  name: string;
+  description?: string;
+  icon: string;
+  date: string;
+  archived: boolean;
+  creator: User;
+  created_at: string;
+}
+
+export interface EventTimeline {
+  id: number;
+  collection_id: CollectionId;
+  name: string;
+  description?: string;
+  default_icon: string;
+  archived: boolean;
+  events: Event[];
+}
