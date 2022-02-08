0c39e191ecf2c04a8cdcd491378be5fbdbc1d9c7
Anton Kulyk <kuliks.anton@gmail.com>
2022-02-08 12:03:45 +0200

    Add mock factories for Event and EventTimeline

---
+++ frontend/src/metabase-types/types/mocks/event.ts
@@ -0,0 +1,40 @@
+import { Event, EventTimeline } from "../Event";
+import { createMockUser } from "./user";
+
+export const createMockEvent = ({
+  id = 1,
+  timeline_id = 1,
+  name = "Christmas",
+  icon = "star",
+  date = new Date().toISOString(),
+  creator = createMockUser(),
+  created_at = new Date().toISOString(),
+  ...rest
+}: Partial<Event> = {}) => ({
+  id,
+  timeline_id,
+  name,
+  icon,
+  date,
+  creator,
+  created_at,
+  ...rest,
+});
+
+export const createMockEventTimeline = ({
+  id = 1,
+  collection_id = 1,
+  name = "Timeline Foo",
+  default_icon = "star",
+  archived = false,
+  events = [],
+  ...rest
+}: Partial<EventTimeline> = {}): EventTimeline => ({
+  id,
+  collection_id,
+  name,
+  default_icon,
+  archived,
+  events,
+  ...rest,
+});
