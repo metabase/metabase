4ea85e248206b7b24175079f30303e684ddc8b68
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-08 19:55:29 +0300

    Add route

---
+++ frontend/src/metabase/events/routes.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import { ModalRoute } from "metabase/hoc/ModalRoute";
+import ListEventTimelineModal from "./containers/ListEventTimelineModal";
+
+const getRoutes = () => {
+  return (
+    <Fragment>
+      <ModalRoute path="timelines" modal={ListEventTimelineModal} />
+    </Fragment>
+  );
+};
+
+export default getRoutes;
