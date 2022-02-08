9344b53343ca3ec81fd8366c19c2baf523e5ea86
Jeff Evans <jeff.evans@metabase.com>
2022-02-08 15:51:53 -0600

    Add changes to common and verify namespaces back (they are needed to get rid of the `All dirs should be absolute` error when building drivers externally

--- bin/build-drivers/src/build_drivers/common.clj
+++ bin/build-drivers/src/build_drivers/common.clj
@@ -1,7 +1,10 @@
 (ns build-drivers.common
   (:require [clojure.java.io :as io]
             [clojure.tools.deps.alpha :as deps]
-            [metabuild-common.core :as u]))
+            [clojure.tools.deps.alpha.util.dir :as deps.dir]
+            [metabuild-common.core :as u]
+            [yaml.core :as yaml])
+  (:import [java.util.zip ZipEntry ZipFile]))
 
 (def ^:dynamic *driver-project-dir* nil)
 
@@ -33,10 +36,17 @@
 (defn driver-edn-filename [driver]
   (u/filename (driver-project-dir driver) "deps.edn"))
 
-(defn- ->absolute [driver path]
+(defn current-deps-project-root
+  "Returns the root of the current `deps.edn` project (ex: a third party driver, or Metabase itself is running from the
+  core project root).  See [[clojure.tools.deps.alpha/find-edn-maps]] for justification of this impl (we use the same
+  underlying var here)."
+  []
+  deps.dir/*the-dir*)
+
+(defn- ->absolute [_driver path]
   (if (u/absolute? path)
     path
-    (u/filename (driver-project-dir driver) path)))
+    (u/filename (current-deps-project-root) path)))
 
 (defn driver-edn [driver edition]
   (let [edn      (deps/merge-edns ((juxt :root-edn :project-edn) (deps/find-edn-maps (driver-edn-filename driver))))
@@ -44,3 +54,36 @@
     (-> (deps/tool edn combined)
         ;; make sure :paths are absolute
         (update :paths (partial mapv (partial ->absolute driver))))))
+
+(defn get-jar-entry ^ZipEntry [^String jar-path ^String filename]
+  (with-open [zip-file (ZipFile. jar-path)]
+    (first
+      (filter
+        (fn [^ZipEntry zip-entry]
+          (= (str zip-entry) filename))
+        (enumeration-seq (.entries zip-file))))))
+
+(defn jar-contains-file? [^String jar-path ^String filename]
+  (some? (get-jar-entry jar-path filename)))
+
+(defn get-driver-manifest-yaml
+  "For the given `driver`, return the manifest file from its jar file, in the form of a [[ZipEntry]]."
+  ^ZipEntry [driver]
+  (when-let [jar-filename (driver-jar-destination-path driver)]
+    (get-jar-entry jar-filename "metabase-plugin.yaml")))
+
+(defn driver-manifest->yml
+  "For the given `jar-filename`, extract the given `manifest-entry` from the jar file, and return the parsed YAML
+  contents."
+  ([]
+   (let [yaml-file (io/file (current-deps-project-root) "resources/metabase-plugin.yaml")]
+     (if (.exists yaml-file)
+       (yaml/parse-string (slurp yaml-file))
+       (throw (ex-info (format "No resources/metabase-plugin.yaml file found in %s"
+                               (current-deps-project-root))
+                {})))))
+  ([jar-filename manifest-entry]
+   (with-open [zip-file (ZipFile. jar-filename)]
+     (let [entry-is (.getInputStream zip-file manifest-entry)
+           yaml-str (slurp entry-is)]
+       (yaml/parse-string yaml-str)))))
--- bin/build-drivers/src/build_drivers/verify.clj
+++ bin/build-drivers/src/build_drivers/verify.clj
@@ -10,22 +10,11 @@
             [yaml.core :as yaml])
   (:import [java.util.zip ZipEntry ZipFile]))
 
-(defn- get-jar-entry [^String jar-path ^String filename]
-  (with-open [zip-file (ZipFile. jar-path)]
-    (first
-      (filter
-        (fn [^ZipEntry zip-entry]
-          (= (str zip-entry) filename))
-        (enumeration-seq (.entries zip-file))))))
-
-(defn- jar-contains-file? [^String jar-path ^String filename]
-  (some? (get-jar-entry jar-path filename)))
-
 (defn- verify-has-init-class [driver]
   (let [jar-filename               (c/driver-jar-destination-path driver)
         driver-init-class-filename (format "metabase/driver/%s__init.class" (munge (name driver)))]
     (u/step (format "Check %s contains init class file %s" jar-filename driver-init-class-filename)
-      (if (jar-contains-file? jar-filename driver-init-class-filename)
+      (if (c/jar-contains-file? jar-filename driver-init-class-filename)
         (u/announce "Driver init class file found.")
         (throw (ex-info (format "Driver verification failed: init class file %s not found" driver-init-class-filename) {}))))))
 
@@ -35,26 +24,23 @@
       (doseq [file ["clojure/spec/alpha__init.class"
                     "clojure/core__init.class"
                     "clojure/core.clj"]]
-        (when (jar-contains-file? jar-filename file)
+        (when (c/jar-contains-file? jar-filename file)
           (throw (ex-info (format "Driver verification failed: driver contains compiled Clojure core file %s" file)
                           {:file file})))))))
 
 (defn- verify-has-plugin-manifest [driver]
   (let [jar-filename (c/driver-jar-destination-path driver)]
     (u/step (format "Check %s contains metabase-plugin.yaml" jar-filename)
-      (if-let [manifest-entry (get-jar-entry jar-filename "metabase-plugin.yaml")]
-        (with-open [zip-file (ZipFile. jar-filename)]
-          (let [entry-is (.getInputStream zip-file manifest-entry)
-                yaml-str (slurp entry-is)
-                yml      (yaml/parse-string yaml-str)]
-            (u/announce "Plugin manifest found; validating it")
-            (if-not (s/valid? ::lint-manifest-file/plugin-manifest yml)
-              (do
-                ;; print a readable explanation of the spec error
-                (expound/expound ::lint-manifest-file/plugin-manifest yml)
-                (throw (ex-info "Driver verification failed: plugin manifest was invalid; see full explanation above"
-                                {:invalid-driver driver})))
-              (u/announce "Plugin manifest passed spec validation"))))
+      (if-let [manifest-entry (c/get-jar-entry jar-filename "metabase-plugin.yaml")]
+        (let [yml (c/driver-manifest->yml jar-filename manifest-entry)]
+          (u/announce "Plugin manifest found; validating it")
+          (if-not (s/valid? ::lint-manifest-file/plugin-manifest yml)
+            (do
+              ;; print a readable explanation of the spec error
+              (expound/expound ::lint-manifest-file/plugin-manifest yml)
+              (throw (ex-info "Driver verification failed: plugin manifest was invalid; see full explanation above"
+                              {:invalid-driver driver})))
+            (u/announce "Plugin manifest passed spec validation")))
         (throw (ex-info "Driver verification failed: plugin manifest missing" {}))))))
 
 (defn verify-driver
