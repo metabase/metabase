ac198ac5fef7ec6a0b5f9d61ed462623b3d9c6c2
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-08 20:23:18 +0300

    Add icon to collection header

--- frontend/src/metabase/events/components/EventTimelineCard/EventTimelineCard.styled.tsx
+++ frontend/src/metabase/events/components/EventTimelineCard/EventTimelineCard.styled.tsx
@@ -3,11 +3,27 @@ import Link from "metabase/core/components/Link";
 import Icon from "metabase/components/Icon";
 import { color } from "metabase/lib/colors";
 
+export const CardRoot = styled(Link)`
+  display: flex;
+  padding: 1.75rem;
+  align-items: center;
+  border: 1px solid ${color("border")};
+  border-radius: 6px;
+
+  &:hover {
+    border-color: ${color("brand")};
+  }
+`;
+
 export const CardIcon = styled(Icon)`
-  color: ${color("text-light")};
+  color: ${color("bg-medium")};
   width: 1.375rem;
   height: 1.375rem;
   margin-right: 1.75rem;
+
+  ${CardRoot}:hover & {
+    color: ${color("brand")};
+  }
 `;
 
 export const CardBody = styled.span`
@@ -19,25 +35,13 @@ export const CardTitle = styled.span`
   color: ${color("text-dark")};
   font-weight: bold;
   margin-bottom: 0.125rem;
+
+  ${CardRoot}:hover & {
+    color: ${color("brand")};
+  }
 `;
 
 export const CardDescription = styled.span`
   display: block;
   color: ${color("text-dark")};
-`;
-
-export const CardRoot = styled(Link)`
-  display: flex;
-  padding: 1.75rem;
-  align-items: center;
-  border: 1px solid ${color("border")};
-  border-radius: 6px;
-
-  &:hover {
-    border-color: ${color("brand")};
-
-    ${CardIcon}, ${CardTitle} {
-      color: ${color("brand")};
-    }
-  }
 `;
