26183c30b1102f480a9552f8d61df057278e3728
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-08 20:39:23 +0300

    Add icon to collection header

--- frontend/src/metabase/events/routes.jsx
+++ frontend/src/metabase/events/routes.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
 import { ModalRoute } from "metabase/hoc/ModalRoute";
-import ListEventTimelineModal from "./containers/TimelineListModal";
+import TimelineListModal from "./containers/TimelineListModal";
 
 const getRoutes = () => {
   return (
     <Fragment>
-      <ModalRoute path="timelines" modal={ListEventTimelineModal} />
+      <ModalRoute path="timelines" modal={TimelineListModal} />
     </Fragment>
   );
 };
