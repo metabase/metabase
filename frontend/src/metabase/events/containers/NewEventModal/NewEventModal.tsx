92a637fe33ee4c7c492cc7b1c3a1d4b67a0fed1d
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-09 16:12:26 +0300

    Add collection id to timeline modal

--- frontend/src/metabase/events/containers/NewEventModal/NewEventModal.tsx
+++ frontend/src/metabase/events/containers/NewEventModal/NewEventModal.tsx
@@ -1,11 +1,22 @@
 import { connect } from "react-redux";
+import _ from "underscore";
 import { goBack } from "react-router-redux";
+import * as Urls from "metabase/lib/urls";
+import Collections from "metabase/entities/collections";
 import Events from "metabase/entities/events";
 import NewEventModal from "../../components/NewEventModal";
+import { ModalProps } from "../../types";
+
+const collectionProps = {
+  id: (props: ModalProps) => Urls.extractCollectionId(props.params.slug),
+};
 
 const mapDispatchToProps = {
   onSubmit: Events.actions.create,
   onCancel: goBack,
 };
 
-export default connect(null, mapDispatchToProps)(NewEventModal);
+export default _.compose(
+  Collections.load(collectionProps),
+  connect(null, mapDispatchToProps),
+)(NewEventModal);
