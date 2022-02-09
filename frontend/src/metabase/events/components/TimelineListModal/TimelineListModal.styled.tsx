feaaca9b393a3a908e1772fe1b8f9d794f77861e
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-09 12:55:09 +0300

    Add icon to collection header

--- frontend/src/metabase/events/components/TimelineListModal/TimelineListModal.styled.tsx
+++ frontend/src/metabase/events/components/TimelineListModal/TimelineListModal.styled.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import Link from "metabase/core/components/Link";
 import Icon from "metabase/components/Icon";
 import { color } from "metabase/lib/colors";
