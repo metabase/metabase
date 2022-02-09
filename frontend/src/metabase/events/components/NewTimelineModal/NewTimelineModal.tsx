c0af30b21a0357c1cf16c63117e5e80ecd341a64
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-09 16:10:46 +0300

    Add collection id to timeline modal

--- frontend/src/metabase/events/components/NewTimelineModal/NewTimelineModal.tsx
+++ frontend/src/metabase/events/components/NewTimelineModal/NewTimelineModal.tsx
@@ -2,24 +2,30 @@ import React from "react";
 import { t } from "ttag";
 import Form from "metabase/containers/Form";
 import forms from "metabase/entities/event-timelines/forms";
+import { Collection } from "metabase-types/api";
 import ModalHeader from "../ModalHeader";
 import { ModalBody } from "./NewTimelineModal.styled";
 
 export interface NewTimelineModalProps {
+  collection: Collection;
   onSubmit?: () => void;
   onCancel?: () => void;
 }
 
 const NewTimelineModal = ({
+  collection,
   onSubmit,
   onCancel,
 }: NewTimelineModalProps): JSX.Element => {
+  const initialValues = { collection_id: collection.id };
+
   return (
     <div>
       <ModalHeader title={t`New event timeline`} />
       <ModalBody>
         <Form
           form={forms.collection}
+          initialValues={initialValues}
           isModal={true}
           onSubmit={onSubmit}
           onClose={onCancel}
--- frontend/src/metabase/events/containers/NewTimelineModal/NewTimelineModal.tsx
+++ frontend/src/metabase/events/containers/NewTimelineModal/NewTimelineModal.tsx
@@ -1,11 +1,22 @@
 import { connect } from "react-redux";
+import _ from "underscore";
 import { goBack } from "react-router-redux";
+import * as Urls from "metabase/lib/urls";
+import Collections from "metabase/entities/collections";
 import EventTimelines from "metabase/entities/event-timelines";
 import NewTimelineModal from "../../components/NewTimelineModal";
+import { ModalProps } from "../../types";
+
+const collectionProps = {
+  id: (props: ModalProps) => Urls.extractCollectionId(props.params.slug),
+};
 
 const mapDispatchToProps = {
   onSubmit: EventTimelines.actions.create,
   onCancel: goBack,
 };
 
-export default connect(null, mapDispatchToProps)(NewTimelineModal);
+export default _.compose(
+  Collections.load(collectionProps),
+  connect(null, mapDispatchToProps),
+)(NewTimelineModal);
