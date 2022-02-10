31741ba7f462e9e110018b5a1b181c42e17ddd00
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-10 17:16:32 +0300

    Add modals

--- frontend/src/metabase/timelines/actions.ts
+++ frontend/src/metabase/timelines/actions.ts
@@ -1,37 +1,53 @@
+import { createAction } from "redux-actions";
 import { createThunkAction } from "metabase/lib/redux";
 import Timelines from "metabase/entities/timelines";
 import TimelineEvents from "metabase/entities/timeline-events";
 import { Collection, Timeline, TimelineEvent } from "metabase-types/api";
 import { getDefaultTimeline } from "metabase/lib/timeline";
 
-export const CREATE_EVENT = "metabase/events/CREATE_EVENT";
+export const SET_MODE = "metabase/timelines/SET_MODE";
+export const setMode = createAction(SET_MODE);
+
+export const SET_TIMELINE = "metabase/timelines/SET_TIMELINE";
+export const setTimeline = createAction(SET_TIMELINE);
+
+export const SET_EVENT = "metabase/timelines/SET_EVENT";
+export const setEvent = createAction(SET_EVENT);
+
+export const CREATE_EVENT = "metabase/timelines/CREATE_EVENT";
 export const createEvent = createThunkAction(
   CREATE_EVENT,
   (values: Partial<TimelineEvent>) => async (dispatch: Dispatch) => {
-    return await createEventEntity(values, dispatch);
-  },
-);
-
-export const CREATE_TIMELINE = "metabase/events/CREATE_TIMELINE";
-export const createTimeline = createThunkAction(
-  CREATE_TIMELINE,
-  (values: Partial<Timeline>) => async (dispatch: Dispatch) => {
-    return await createTimelineEntity(values, dispatch);
+    const event = await createEventEntity(values, dispatch);
+    dispatch(setMode("timeline-view"));
+    dispatch(setTimeline(event.id));
   },
 );
 
 export const CREATE_EVENT_WITH_TIMELINE =
-  "metabase/events/CREATE_EVENT_WITH_TIMELINE";
+  "metabase/timelines/CREATE_EVENT_WITH_TIMELINE";
 export const createEventWithTimeline = createThunkAction(
   CREATE_EVENT_WITH_TIMELINE,
   (values: Partial<TimelineEvent>, collection: Collection) => async (
     dispatch: Dispatch,
   ) => {
-    const timelineValues = getDefaultTimeline(collection);
-    const timeline = await createTimelineEntity(timelineValues, dispatch);
-    const eventValues = { ...values, timeline_id: timeline.id };
-    await createEventEntity(eventValues, dispatch);
-    return timeline;
+    const timeline = await createEventWithTimelineEntity(
+      values,
+      collection,
+      dispatch,
+    );
+    dispatch(setMode("timeline-view"));
+    dispatch(setTimeline(timeline.id));
+  },
+);
+
+export const CREATE_TIMELINE = "metabase/timelines/CREATE_TIMELINE";
+export const createTimeline = createThunkAction(
+  CREATE_TIMELINE,
+  (values: Partial<Timeline>) => async (dispatch: Dispatch) => {
+    const timeline = await createTimelineEntity(values, dispatch);
+    dispatch(setMode("timeline-view"));
+    dispatch(setTimeline(timeline.id));
   },
 );
 
@@ -41,6 +57,18 @@ const createEventEntity = async (
 ): Promise<TimelineEvent> => {
   const action = await dispatch(TimelineEvents.actions.create(values));
   return TimelineEvents.HACK_getObjectFromAction(action);
+};
+
+const createEventWithTimelineEntity = async (
+  values: Partial<TimelineEvent>,
+  collection: Collection,
+  dispatch: Dispatch,
+): Promise<Timeline> => {
+  const timelineValues = getDefaultTimeline(collection);
+  const timeline = await createTimelineEntity(timelineValues, dispatch);
+  const eventValues = { ...values, timeline_id: timeline.id };
+  await createEventEntity(eventValues, dispatch);
+  return timeline;
 };
 
 const createTimelineEntity = async (
--- frontend/src/metabase/timelines/reducers.ts
+++ frontend/src/metabase/timelines/reducers.ts
@@ -1,24 +1,30 @@
 import { handleActions } from "redux-actions";
-import { CREATE_EVENT_WITH_TIMELINE, CREATE_TIMELINE } from "./actions";
-import { Timeline } from "metabase-types/api";
+import { SET_EVENT, SET_MODE, SET_TIMELINE } from "./actions";
 import { TimelineMode } from "metabase-types/store";
 
-export const mode = handleActions<TimelineMode, Timeline>(
+export const mode = handleActions<TimelineMode>(
   {
-    [CREATE_TIMELINE]: { next: () => "timeline-view" },
-    [CREATE_EVENT_WITH_TIMELINE]: { next: () => "timeline-view" },
+    [SET_MODE]: { next: (state, { payload }) => payload },
   },
   "timeline-list",
 );
 
-export const timelineId = handleActions<number | null, Timeline>(
+export const timelineId = handleActions<number | null>(
   {
-    [CREATE_TIMELINE]: { next: (state, { payload }) => payload.id },
-    [CREATE_EVENT_WITH_TIMELINE]: { next: (state, { payload }) => payload.id },
+    [SET_TIMELINE]: { next: (state, { payload }) => payload },
+  },
+  null,
+);
+
+export const timelineEventId = handleActions<number | null>(
+  {
+    [SET_EVENT]: { next: (state, { payload }) => payload },
   },
   null,
 );
 
 export default {
   mode,
+  timelineId,
+  timelineEventId,
 };
