4b071e760669a1d0698d96d5bcd9dd45b74af3d8
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-10 18:03:42 +0300

    Add modals

--- frontend/src/metabase/timelines/containers/NewEventModal/NewEventModal.tsx
+++ frontend/src/metabase/timelines/containers/NewEventModal/NewEventModal.tsx
@@ -1,10 +1,20 @@
+import { connect } from "react-redux";
 import _ from "underscore";
 import Collections from "metabase/entities/collections";
 import Timelines from "metabase/entities/timelines";
+import { Collection, TimelineEvent } from "metabase-types/api";
 import NewEventModal from "../../components/NewEventModal";
+import { createEvent, setMode } from "../../actions";
 import { getCollectionId, getTimelineId } from "../../selectors";
+
+const mapDispatchToProps = (dispatch: any) => ({
+  onSubmit: (values: Partial<TimelineEvent>, collection: Collection) =>
+    dispatch(createEvent(values, collection)),
+  onClose: () => dispatch(setMode("timeline-view")),
+});
 
 export default _.compose(
   Timelines.load({ id: getTimelineId }),
   Collections.load({ id: getCollectionId }),
+  connect(null, mapDispatchToProps),
 )(NewEventModal);
--- frontend/src/metabase/timelines/containers/NewEventWithTimelineModal/NewEventWithTimelineModal.tsx
+++ frontend/src/metabase/timelines/containers/NewEventWithTimelineModal/NewEventWithTimelineModal.tsx
@@ -1,5 +1,18 @@
+import { connect } from "react-redux";
+import _ from "underscore";
 import Collections from "metabase/entities/collections";
+import { Collection, TimelineEvent } from "metabase-types/api";
 import NewEventModal from "../../components/NewEventModal";
+import { createTimelineWithEvent, setMode } from "../../actions";
 import { getCollectionId } from "../../selectors";
 
-export default Collections.load({ id: getCollectionId })(NewEventModal);
+const mapDispatchToProps = (dispatch: any) => ({
+  onSubmit: (values: Partial<TimelineEvent>, collection: Collection) =>
+    dispatch(createTimelineWithEvent(values, collection)),
+  onClose: () => dispatch(setMode("timeline-view")),
+});
+
+export default _.compose(
+  Collections.load({ id: getCollectionId }),
+  connect(null, mapDispatchToProps),
+)(NewEventModal);
