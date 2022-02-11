3a61a3b452915e5e72dbf201c4e5786279e77d53
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2022-02-11 15:41:40 -0500

    Ensure PNG cropping doesn't clip table content (#20448)

--- src/metabase/pulse/render/png.clj
+++ src/metabase/pulse/render/png.clj
@@ -86,7 +86,11 @@
                                                    RenderingHints/VALUE_FRACTIONALMETRICS_ON))))]
       (.createLayout graphics-engine dimension)
       (let [image         (.getImage graphics-engine)
-            content-width (int (.getMaximalWidth (.getViewport graphics-engine)))]
+            viewport      (.getViewport graphics-engine)
+            ;; CSSBox voodoo -- sometimes maximal width < minimal width, no idea why
+            content-width (max (int (.getMinimalWidth viewport))
+                               (int (.getMaximalWidth viewport)))]
+        ;; Crop the image to the actual size of the rendered content so that tables don't have a ton of whitespace.
         (if (< content-width (.getWidth image))
           (.getSubimage image 0 0 content-width (.getHeight image))
           image)))))
