a0517d411371c9d7f80630a09febb00025745795
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2022-02-14 18:55:49 +0000

    Permissions UI groundwork (#20433)
    * keep click
    
    * safari fixes

--- frontend/src/metabase/admin/permissions/components/PermissionsEditor/PermissionsEditor.styled.js
+++ frontend/src/metabase/admin/permissions/components/PermissionsEditor/PermissionsEditor.styled.js
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 
 export const PermissionsEditorRoot = styled.div`
   flex-grow: 1;
-  overflow: auto;
+  position: relative;
+  overflow: hidden;
   padding: 1rem 0 2rem 0;
 `;
