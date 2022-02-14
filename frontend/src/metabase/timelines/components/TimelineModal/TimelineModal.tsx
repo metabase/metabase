bd812257eb83e704dcb573ab21ba79c593482fd0
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-14 17:07:18 +0300

    Add dyno

--- frontend/src/metabase/timelines/components/TimelineModal/TimelineModal.tsx
+++ frontend/src/metabase/timelines/components/TimelineModal/TimelineModal.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { t } from "ttag";
 import * as Urls from "metabase/lib/urls";
 import Link from "metabase/core/components/Link";
@@ -6,7 +6,7 @@ import EntityMenu from "metabase/components/EntityMenu";
 import { Collection, Timeline } from "metabase-types/api";
 import EventList from "../EventList";
 import ModalHeader from "../ModalHeader";
-import { ModalBody, ModalContent, ModalToolbar } from "./TimelineModal.styled";
+import { ModalBody, ModalRoot, ModalToolbar } from "./TimelineModal.styled";
 
 export interface TimelineModalProps {
   timeline: Timeline;
@@ -19,78 +19,37 @@ const TimelineModal = ({
   collection,
   onClose,
 }: TimelineModalProps): JSX.Element => {
+  const menuItems = getMenuItems(timeline, collection);
+
   return (
-    <div>
+    <ModalRoot>
       <ModalHeader title={timeline.name} onClose={onClose}>
-        <TimelineMenu timeline={timeline} collection={collection} />
+        <EntityMenu items={menuItems} triggerIcon="ellipsis" />
       </ModalHeader>
+      <ModalToolbar>
+        <Link
+          className="Button"
+          to={Urls.newEventInCollection(timeline, collection)}
+        >{t`Add an event`}</Link>
+      </ModalToolbar>
       <ModalBody>
-        <TimelineToolbar timeline={timeline} collection={collection} />
-        <TimelineContent timeline={timeline} collection={collection} />
+        <EventList timeline={timeline} collection={collection} />
       </ModalBody>
-    </div>
+    </ModalRoot>
   );
 };
 
-export interface TimelineMenuProps {
-  timeline: Timeline;
-  collection: Collection;
-}
-
-const TimelineMenu = ({
-  timeline,
-  collection,
-}: TimelineMenuProps): JSX.Element => {
-  const items = useMemo(
-    () => [
-      {
-        title: t`New timeline`,
-        link: Urls.newTimelineInCollection(collection),
-      },
-      {
-        title: t`Edit timeline details`,
-        link: Urls.editTimelineInCollection(timeline, collection),
-      },
-    ],
-    [timeline, collection],
-  );
-
-  return <EntityMenu items={items} triggerIcon="ellipsis" />;
-};
-
-interface TimelineToolbarProps {
-  timeline: Timeline;
-  collection: Collection;
-}
-
-const TimelineToolbar = ({
-  timeline,
-  collection,
-}: TimelineToolbarProps): JSX.Element => {
-  return (
-    <ModalToolbar>
-      <Link
-        className="Button"
-        to={Urls.newEventInCollection(timeline, collection)}
-      >{t`Add an event`}</Link>
-    </ModalToolbar>
-  );
-};
-
-interface TimelineContentProps {
-  timeline: Timeline;
-  collection: Collection;
-}
-
-const TimelineContent = ({
-  timeline,
-  collection,
-}: TimelineContentProps): JSX.Element => {
-  return (
-    <ModalContent>
-      <EventList timeline={timeline} collection={collection} />
-    </ModalContent>
-  );
+const getMenuItems = (timeline: Timeline, collection: Collection) => {
+  return [
+    {
+      title: t`New timeline`,
+      link: Urls.newTimelineInCollection(collection),
+    },
+    {
+      title: t`Edit timeline details`,
+      link: Urls.editTimelineInCollection(timeline, collection),
+    },
+  ];
 };
 
 export default TimelineModal;
