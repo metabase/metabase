3f0e752ce9a6468185a92e4290c2c2e4897670e8
Adam James <adam.vermeer2@gmail.com>
2022-02-15 11:22:45 -0800

    Timeline-events API tests
    Timeline Events API endpoint tests, at least the very basics.

--- test/metabase/api/timeline_event_test.clj
+++ test/metabase/api/timeline_event_test.clj
@@ -7,7 +7,8 @@
             [metabase.models.timeline-event :refer [TimelineEvent]]
             [metabase.server.middleware.util :as middleware.u]
             [metabase.test :as mt]
-            [metabase.util :as u]))
+            [metabase.util :as u]
+            [toucan.db :as db]))
 
 (deftest auth-tests
   (testing "Authentication"
@@ -18,36 +19,49 @@
 
 (deftest get-timeline-event-test
   (testing "GET /api/timeline-event/:id"
-    (mt/with-temp TimelineEvent [event {:name "Very Important Event"
-                                        :timestamp "NOW"}]
-      (testing "check that we get the timeline-event with `id`"
-        (is (= (->> (mt/user-http-request :rasta :get 200 "timeline-event")
-                    (filter #(= (:collection_id %) id))
-                    (map :name)
-                    set)
-               #{"Timeline A" "Timeline B"}))))))
+    (let [defaults {:creator_id (u/the-id (mt/fetch-user :rasta))}]
+      (mt/with-temp* [Collection    [collection {:name "Important Data"}]
+                      Timeline      [timeline (merge defaults {:name          "Important Events"
+                                                               :collection_id (u/the-id collection)})]
+                      TimelineEvent [event (merge defaults {:name         "Very Important Event"
+                                                            :timestamp    (java.time.OffsetDateTime/now)
+                                                            :timezone     "PST"
+                                                            :time_matters false
+                                                            :timeline_id  (u/the-id timeline)})]]
+        (testing "check that we get the timeline-event with `id`"
+          (is (= (->> (mt/user-http-request :rasta :get 200 (str "timeline-event/" (u/the-id event)))
+                      :name)
+                 "Very Important Event")))))))
 
-#_(deftest create-timeline-event-test
+(deftest create-timeline-event-test
   (testing "POST /api/timeline-event"
-    (mt/with-temp Collection [collection {:name "Important Data"}]
-      (let [id          (u/the-id collection)
-            tl-defaults {:creator_id    (u/the-id (mt/fetch-user :rasta))
-                         :collection_id id}]
-        (testing "Create a new timeline"
+    (let [defaults {:creator_id (u/the-id (mt/fetch-user :rasta))}]
+      (mt/with-temp* [Collection    [collection {:name "Important Data"}]
+                      Timeline      [timeline (merge defaults {:name          "Important Events"
+                                                               :collection_id (u/the-id collection)})]]
+        (testing "create a timeline event"
           ;; make an API call to create a timeline
-          (mt/user-http-request :rasta :post 200 "timeline" (merge tl-defaults {:name "Rasta's TL"}))
-          ;; check the collection to see if the timeline is there
-          (is (= (-> (db/select-one Timeline :collection_id id) :name)
-                 "Rasta's TL")))))))
+          (mt/user-http-request :rasta :post 200 "timeline-event" {:name         "Rasta Migrates to Florida for the Winter"
+                                                                   :timestamp    (java.time.OffsetDateTime/now)
+                                                                   :timezone     "PST"
+                                                                   :time_matters false
+                                                                   :timeline_id  (u/the-id timeline)}))
+          ;; check the Timeline to see if the event is there
+          (is (= (-> (db/select-one TimelineEvent :timeline_id (u/the-id timeline)) :name)
+                 "Rasta Migrates to Florida for the Winter"))))))
 
-#_(deftest update-timeline-event-test
+(deftest update-timeline-event-test
   (testing "PUT /api/timeline-event/:id"
-    (mt/with-temp Collection [collection {:name "Important Data"}]
-      (let [id          (u/the-id collection)
-            tl-defaults {:creator_id    (u/the-id (mt/fetch-user :rasta))
-                         :collection_id id}]
-        (mt/with-temp* [Timeline [tl-a (merge tl-defaults {:name "Timeline A" :archived true})]]
-          (testing "check that we successfully updated a timeline"
-            (is (= (->> (mt/user-http-request :rasta :put 200 (str "timeline/" (u/the-id tl-a)) {:archived false})
-                        :archived)
-                   false))))))))
+    (let [defaults {:creator_id (u/the-id (mt/fetch-user :rasta))}]
+      (mt/with-temp* [Collection    [collection {:name "Important Data"}]
+                      Timeline      [timeline (merge defaults {:name          "Important Events"
+                                                               :collection_id (u/the-id collection)})]
+                      TimelineEvent [event (merge defaults {:name         "Very Important Event"
+                                                            :timestamp    (java.time.OffsetDateTime/now)
+                                                            :timezone     "PST"
+                                                            :time_matters false
+                                                            :timeline_id  (u/the-id timeline)})]]
+        (testing "check that we get the timeline-event with `id`"
+          (is (= (->> (mt/user-http-request :rasta :put 200 (str "timeline-event/" (u/the-id event)) {:archived true})
+                      :archived)
+                 true)))))))
