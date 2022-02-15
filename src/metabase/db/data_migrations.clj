718c1400fc45888779a288270d2563025b27af17
Cam Saul <1455846+camsaul@users.noreply.github.com>
2022-02-15 10:36:41 -0800

    Replace `migrate-humanization-setting` with Liquibase migrations (#20404)
    * Replace metabase.db.migrations/migrate-humanization-setting with a Liquibase migration
    
    * Apparently key has to be quoted these days in MySQL/MariaDB
    
    * Need to quote key in the preCondition as well
    
    * Fix merge conflicts

--- src/metabase/db/data_migrations.clj
+++ src/metabase/db/data_migrations.clj
@@ -16,7 +16,6 @@
             [metabase.models.dashboard :refer [Dashboard]]
             [metabase.models.dashboard-card :refer [DashboardCard]]
             [metabase.models.database :refer [Database]]
-            [metabase.models.humanization :as humanization]
             [metabase.models.permissions :as perms :refer [Permissions]]
             [metabase.models.permissions-group :as perm-group :refer [PermissionsGroup]]
             [metabase.models.permissions-group-membership :as perm-membership :refer [PermissionsGroupMembership]]
@@ -121,25 +120,6 @@
 (defmigration ^{:author "camsaul", :added "0.23.0"} copy-site-url-setting-and-remove-trailing-slashes
   (when-let [site-url (db/select-one-field :value Setting :key "-site-url")]
     (public-settings/site-url site-url)))
-
-;; Prior to version 0.28.0 humanization was configured using the boolean setting `enable-advanced-humanization`.
-;; `true` meant "use advanced humanization", while `false` meant "use simple humanization". In 0.28.0, this Setting
-;; was replaced by the `humanization-strategy` Setting, which (at the time of this writing) allows for a choice
-;; between three options: advanced, simple, or none. Migrate any values of the old Setting, if set, to the new one.
-(defmigration ^{:author "camsaul", :added "0.28.0"} migrate-humanization-setting
-  (when-let [enable-advanced-humanization-str (db/select-one-field :value Setting, :key "enable-advanced-humanization")]
-    (when (seq enable-advanced-humanization-str)
-      ;; if an entry exists for the old Setting, it will be a boolean string, either "true" or "false". Try inserting
-      ;; a record for the new setting with the appropriate new value. This might fail if for some reason
-      ;; humanization-strategy has been set already, or enable-advanced-humanization has somehow been set to an
-      ;; invalid value. In that case, fail silently.
-      (u/ignore-exceptions
-        (humanization/humanization-strategy (if (Boolean/parseBoolean enable-advanced-humanization-str)
-                                              "advanced"
-                                              "simple"))))
-    ;; either way, delete the old value from the DB since we'll never be using it again.
-    ;; use `simple-delete!` because `Setting` doesn't have an `:id` column :(
-    (db/simple-delete! Setting {:key "enable-advanced-humanization"})))
 
 ;; Before 0.30.0, we were storing the LDAP user's password in the `core_user` table (though it wasn't used).  This
 ;; migration clears those passwords and replaces them with a UUID. This is similar to a new account setup, or how we
