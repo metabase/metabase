1438584075a3fd5e9fce0c7de02e2714bb895aca
Adam James <adam.vermeer2@gmail.com>
2022-02-17 15:16:54 -0800

    Added Test for timeline event range selection via card API

--- test/metabase/api/card_test.clj
+++ test/metabase/api/card_test.clj
@@ -1282,6 +1282,14 @@
     (mt/user-http-request :rasta :get 200 (str "card/" (u/the-id card) "/timelines") :include "events")
     (mt/user-http-request :rasta :get 200 (str "card/" (u/the-id card) "/timelines"))))
 
+(defn- timelines-range-request
+  [card {:keys [start end]}]
+  (apply mt/user-http-request (concat [:rasta :get 200
+                                       (str "card/" (u/the-id card) "/timelines")
+                                       :include "events"]
+                                      (when start [:start start])
+                                      (when end [:end end]))))
+
 (defn- timeline-names [timelines]
   (->> timelines (map :name) set))
 
@@ -1333,25 +1341,47 @@
           (is (= '()
                  (->> (timelines-request card-c true) first :events)))))))))
 
-(comment
-  (mt/with-temp* [Collection [collection {:name "Collection"}]]
-    (mt/with-temp* [Card [card {:name          "Card A"
-                                :collection_id (u/the-id collection)}]
-                    Timeline [tl-a {:name          "Timeline A"
-                                    :collection_id (u/the-id collection)}]
-                    Timeline [tl-b {:name          "Timeline B"
-                                    :collection_id (u/the-id collection)}]]
-      (mt/with-temp* [TimelineEvent [event-aa {:name        "event-aa"
-                                               :timeline_id (u/the-id tl-a)}]
-                      TimelineEvent [event-ab {:name        "event-ab"
-                                               :timeline_id (u/the-id tl-a)
-                                               :archived    true}]
-                      TimelineEvent [event-ab {:name        "event-ba"
-                                               :timeline_id (u/the-id tl-b)}]
-                      TimelineEvent [event-ab {:name        "event-bb"
-                                               :timeline_id (u/the-id tl-b)}]]
-        (->> (timelines-request card true) first :events))))
-  )
+(deftest timelines-range-test
+  (testing "GET /api/card/:id/timelines?include=events&start=TIME&end=TIME"
+    (mt/with-temp* [Collection [collection {:name "Collection"}]]
+      (mt/with-temp* [Card [card {:name          "Card A"
+                                  :collection_id (u/the-id collection)}]
+                      Timeline [tl-a {:name          "Timeline A"
+                                      :collection_id (u/the-id collection)}]]
+        ;; the temp defaults set {:time_matters true}
+        (mt/with-temp* [TimelineEvent [event-a {:name        "event-a"
+                                                :timeline_id (u/the-id tl-a)
+                                                :timestamp   #t "2020-01-01T10:00:00.0Z"}]
+                        TimelineEvent [event-b {:name        "event-b"
+                                                :timeline_id (u/the-id tl-a)
+                                                :timestamp   #t "2021-01-01T10:00:00.0Z"}]
+                        TimelineEvent [event-c {:name        "event-c"
+                                                :timeline_id (u/the-id tl-a)
+                                                :timestamp   #t "2022-01-01T10:00:00.0Z"}]
+                        TimelineEvent [event-d {:name        "event-d"
+                                                :timeline_id (u/the-id tl-a)
+                                                :timestamp   #t "2023-01-01T10:00:00.0Z"}]]
+          (testing "Events are properly filtered when given only `start=` parameter"
+            (is (= #{"event-c" "event-d"}
+                   (event-names (timelines-range-request card {:start "2022-01-01T10:00:00.0Z"})))))
+          (testing "Events are properly filtered when given only `end=` parameter"
+            (is (= #{"event-a" "event-b" "event-c"}
+                   (event-names (timelines-range-request card {:end "2022-01-01T10:00:00.0Z"})))))
+          (testing "Events are properly filtered when given `start=` and `end=` parameters"
+            (is (= #{"event-b" "event-c"}
+                   (event-names (timelines-range-request card {:start "2020-12-01T10:00:00.0Z"
+                                                               :end   "2022-12-01T10:00:00.0Z"})))))
+          (mt/with-temp TimelineEvent [event-a2 {:name        "event-a2"
+                                                 :timeline_id (u/the-id tl-a)
+                                                 :timestamp   #t "2020-01-01T10:00:00.0Z"
+                                                 :time_matters false}]
+            (testing "Events are properly filtered considering the `time_matters` state."
+              ;; notice that event-a and event-a2 have the same timestamp, but different time_matters states.
+              ;; time_matters = false effectively means "We care only about the DATE of this event", so
+              ;; if a start or end timestamp is on the same DATE (regardless of time), include the event
+              (is (= #{"event-a2"}
+                     (event-names (timelines-range-request card {:start "2020-01-01T11:00:00.0Z"
+                                                                 :end   "2020-12-01T10:00:00.0Z"})))))))))))
 
 ;;; +----------------------------------------------------------------------------------------------------------------+
 ;;; |                                            CSV/JSON/XLSX DOWNLOADS                                             |
