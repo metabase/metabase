5315423b8bed306395092825d8223cb404b8c751
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-17 15:21:25 +0300

    Add thread

--- frontend/src/metabase/timelines/components/TimelineEmptyState/TimelineEmptyState.styled.tsx
+++ frontend/src/metabase/timelines/components/TimelineEmptyState/TimelineEmptyState.styled.tsx
@@ -1,20 +1,50 @@
 import styled from "@emotion/styled";
-import { color } from "metabase/lib/colors";
+import { alpha, color } from "metabase/lib/colors";
+import Icon from "metabase/components/Icon";
 
-export const EmptyStateRoot = styled.div`
+export const StateRoot = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-export const EmptyStateBody = styled.div`
+export const StateBody = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   max-width: 22.5rem;
 `;
 
-export const EmptyStateText = styled.div`
+export const StateThread = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.5rem;
+`;
+
+export const StateThreadStroke = styled.div`
+  margin: 0 0.5rem;
+  width: 11.75rem;
+  height: 1px;
+  background-color: ${alpha("brand", 0.2)};
+`;
+
+export const StateThreadIcon = styled(Icon)`
+  color: ${color("white")};
+  width: 1rem;
+  height: 1rem;
+`;
+
+export const StateThreadIconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 1rem;
+  background-color: ${color("brand")};
+`;
+
+export const StateMessage = styled.div`
   color: ${color("text-dark")};
   line-height: 1.5rem;
   margin-bottom: 2rem;
--- frontend/src/metabase/timelines/components/TimelineEmptyState/TimelineEmptyState.tsx
+++ frontend/src/metabase/timelines/components/TimelineEmptyState/TimelineEmptyState.tsx
@@ -4,9 +4,13 @@ import * as Urls from "metabase/lib/urls";
 import Link from "metabase/core/components/Link";
 import { Collection, Timeline } from "metabase-types/api";
 import {
-  EmptyStateBody,
-  EmptyStateRoot,
-  EmptyStateText,
+  StateBody,
+  StateRoot,
+  StateMessage,
+  StateThread,
+  StateThreadStroke,
+  StateThreadIconContainer,
+  StateThreadIcon,
 } from "./TimelineEmptyState.styled";
 
 export interface TimelineEmptyStateProps {
@@ -23,16 +27,23 @@ const TimelineEmptyState = ({
     : Urls.newEventAndTimelineInCollection(collection);
 
   return (
-    <EmptyStateRoot>
-      <EmptyStateBody>
-        <EmptyStateText>
+    <StateRoot>
+      <StateBody>
+        <StateThread>
+          <StateThreadStroke />
+          <StateThreadIconContainer>
+            <StateThreadIcon name="balloons" />
+          </StateThreadIconContainer>
+          <StateThreadStroke />
+        </StateThread>
+        <StateMessage>
           {t`Add events to Metabase to open important milestones, launches, or anything else, right alongside your data.`}
-        </EmptyStateText>
+        </StateMessage>
         <Link className="Button Button--primary" to={link}>
           {t`Add an event`}
         </Link>
-      </EmptyStateBody>
-    </EmptyStateRoot>
+      </StateBody>
+    </StateRoot>
   );
 };
 
