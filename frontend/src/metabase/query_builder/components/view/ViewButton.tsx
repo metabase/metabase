79aaaa06d6ab8ac3d3c80fa68304da318303926a
Anton Kulyk <kuliks.anton@gmail.com>
2022-02-18 13:03:09 +0400

    Convert `ViewButton` to TypeScript

--- frontend/src/metabase/query_builder/components/view/ViewButton.tsx
+++ frontend/src/metabase/query_builder/components/view/ViewButton.tsx
@@ -1,22 +1,30 @@
+import styled from "@emotion/styled";
 import Button from "metabase/core/components/Button";
 import { color, alpha } from "metabase/lib/colors";
 
-import styled from "@emotion/styled";
+type Props = {
+  active?: boolean;
+  color?: string;
+};
 
 // NOTE: some of this is duplicated from NotebookCell.jsx
-const ViewButton = styled(Button)`
+const ViewButton = styled(Button)<Props>`
   background-color: ${({ active, color = getDefaultColor() }) =>
     active ? color : alpha(color, 0.2)};
+
   color: ${({ active, color = getDefaultColor() }) =>
     active ? "white" : color};
+
   border: none;
+  transition: background 300ms linear, border 300ms linear;
+
   &:hover {
     background-color: ${({ active, color = getDefaultColor() }) =>
       active ? alpha(color, 0.8) : alpha(color, 0.35)};
     color: ${({ active, color = getDefaultColor() }) =>
       active ? "white" : color};
   }
-  transition: background 300ms linear, border 300ms linear;
+
   > .Icon {
     opacity: 0.6;
   }
