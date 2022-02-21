f807c6f78d9e44e0f4bc155191dc757520bf6201
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-21 17:40:32 +0300

    Add date selector

--- frontend/src/metabase/core/components/DateSelector/DateSelector.styled.tsx
+++ frontend/src/metabase/core/components/DateSelector/DateSelector.styled.tsx
@@ -9,7 +9,7 @@ export const SelectorFooter = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 1rem 0.75rem 0;
+  margin: 1rem 0.75rem 0.75rem;
 `;
 
 export const SelectorTimeButton = styled(Button)`
---
+++ frontend/src/metabase/core/components/DateWidget/DateWidget.stories.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Moment } from "moment";
+import { ComponentStory } from "@storybook/react";
+import DateWidget from "./DateWidget";
+
+export default {
+  title: "Core/DateWidget",
+  component: DateWidget,
+};
+
+const Template: ComponentStory<typeof DateWidget> = args => {
+  const [date, setDate] = useState<Moment>();
+  return <DateWidget {...args} date={date} onChangeDate={setDate} />;
+};
+
+export const Default = Template.bind({});
