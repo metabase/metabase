96a0adeb5be4bfaa77c063e24c3d0e5338c70792
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-22 18:58:01 +0300

    Add timezone to forms

--- frontend/src/metabase/entities/timeline-events/forms.js
+++ frontend/src/metabase/entities/timeline-events/forms.js
@@ -1,57 +1,73 @@
 import { t } from "ttag";
-import { getDefaultTimezone } from "metabase/lib/time";
+import {
+  getDefaultTimezone,
+  hasTimePart,
+  parseTimestamp,
+} from "metabase/lib/time";
 import { getTimelineIcons } from "metabase/lib/timeline";
 import validate from "metabase/lib/validate";
 
-const FORM_FIELDS = [
-  {
-    name: "name",
-    title: t`Event name`,
-    placeholder: t`Product launch`,
-    autoFocus: true,
-    validate: validate.required().maxLength(255),
-  },
-  {
-    name: "timestamp",
-    title: t`Date`,
-    type: "date",
-    hasTime: true,
-    hasTimezone: true,
-    timezoneFieldName: "timezone",
-    validate: validate.required(),
-  },
-  {
-    name: "description",
-    title: t`Description`,
-    type: "text",
-    validate: validate.maxLength(255),
-  },
-  {
-    name: "icon",
-    title: t`Icon`,
-    type: "select",
-    initial: "star",
-    options: getTimelineIcons(),
-    validate: validate.required(),
-  },
-  {
-    name: "timezone",
-    type: "hidden",
-    initial: getDefaultTimezone(),
-  },
-  {
-    name: "time_matters",
-    type: "hidden",
-    initial: false,
-  },
-  {
-    name: "timeline_id",
-    type: "hidden",
-  },
-];
+const createForm = () => {
+  return [
+    {
+      name: "name",
+      title: t`Event name`,
+      placeholder: t`Product launch`,
+      autoFocus: true,
+      validate: validate.required().maxLength(255),
+    },
+    {
+      name: "timestamp",
+      title: t`Date`,
+      type: "date",
+      hasTime: true,
+      hasTimezone: true,
+      timezoneFieldName: "timezone",
+      validate: validate.required(),
+    },
+    {
+      name: "description",
+      title: t`Description`,
+      type: "text",
+      validate: validate.maxLength(255),
+    },
+    {
+      name: "icon",
+      title: t`Icon`,
+      type: "select",
+      initial: "star",
+      options: getTimelineIcons(),
+      validate: validate.required(),
+    },
+    {
+      name: "timezone",
+      type: "hidden",
+      initial: getDefaultTimezone(),
+    },
+    {
+      name: "time_matters",
+      type: "hidden",
+      initial: false,
+    },
+    {
+      name: "timeline_id",
+      type: "hidden",
+    },
+  ];
+};
+
+const normalizeForm = timeline => {
+  const timestamp = parseTimestamp(timeline.timestamp);
+
+  return {
+    ...timeline,
+    time_matters: hasTimePart(timestamp),
+  };
+};
 
 export default {
   collection: {
-    fields: FORM_FIELDS,
+    fields: createForm,
+    normalize: normalizeForm,
   },
 };
