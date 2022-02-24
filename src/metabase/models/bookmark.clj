06d4f901e5f90a7716f0b1c58892bf73d510abcc
Adam James <adam.vermeer2@gmail.com>
2022-02-23 16:28:14 -0800

    Made a WIP implementation for grabbing all bookmarks for the user
    Using a honeysql query, it's possible to union the results from the tables:
    
    - card_bookmark
    - dashboard_bookmark
    - collection_bookmark
    
    Where the user_id is the current user.
    
    Then, we can left join this table (called 'bookmark' using :with) with whatever tables make sense. For example, we may
    care about report_card and card table data joined with the card_id, same for dashboards, and collections.
    
    The shape of the returned data is not super clean yet, but the query works as a start.

--- src/metabase/models/bookmark.clj
+++ src/metabase/models/bookmark.clj
@@ -1,5 +1,8 @@
 (ns metabase.models.bookmark
-  (:require [toucan.models :as models]))
+  (:require [metabase.db.connection :as mdb]
+            [metabase.util.honeysql-extensions :as hx]
+            [toucan.db :as db]
+            [toucan.models :as models]))
 
 (models/defmodel CardBookmark :card_bookmark)
 (models/defmodel DashboardBookmark :dashboard_bookmark)
@@ -8,5 +11,62 @@
 (defn bookmarks-for-user
   "Get all bookmarks for a user"
   [id]
-  ;; todo
+  (let [as-null (when (= (mdb/db-type) :postgres) (hx/->integer nil))]
+    (db/query
+     ;; todo: does it make sense to create a 'partial-bookmarks-query' fn?
+     {:with [[:bookmark {:union-all [{:select [:card_id
+                                               [as-null :dashboard_id]
+                                               [as-null :collection_id]]
+                                      :from [:card_bookmark]
+                                      :where [:= :user_id id]}
+                                     {:select [[as-null :card_id]
+                                               :dashboard_id
+                                               [as-null :collection_id]]
+                                      :from [:dashboard_bookmark]
+                                      :where [:= :user_id id]}
+                                     {:select [[as-null :card_id]
+                                               [as-null :dashboard_id]
+                                               :collection_id]
+                                      :from [:collection_bookmark]
+                                      :where [:= :user_id id]}]}]]
+      :select [:*]
+      :from [:bookmark]
+      ;; todo: not certain this is correct yet. Even if it is, the shape of the returned data could be cleaned up I think
+      :left-join [[:report_card :card] [:= :bookmark.card_id :card.id]
+                  [:report_dashboard :dashboard] [:= :bookmark.dashboard_id :dashboard.id]
+                  :collection [:= :bookmark.collection_id :collection.id]]})))
+
+
+(comment
+
+  (db/query {:select [:card_id
+                      [nil :dashboard_id]
+                      [nil :collection_id]]
+             :from CardBookmark
+             :where [:= :user_id 1]})
+  ;; see collection api L405
+  ;; see if I can use the model directly
+  (let [as-null (when (= (mdb/db-type) :postgres) (hx/->integer nil))]
+    (db/query
+     {:with [[:bookmark {:union-all [{:select [:card_id
+                                               [as-null :dashboard_id]
+                                               [as-null :collection_id]]
+                                      :from [:card_bookmark]
+                                      :where [:= :user_id 1]}
+                                     {:select [[as-null :card_id]
+                                               :dashboard_id
+                                               [as-null :collection_id]]
+                                      :from [:dashboard_bookmark]
+                                      :where [:= :user_id 1]}
+                                     {:select [[as-null :card_id]
+                                               [as-null :dashboard_id]
+                                               :collection_id]
+                                      :from [:collection_bookmark]
+                                      :where [:= :user_id 1]}]}]]
+      :select [:*]
+      :from [:bookmark]
+      :left-join [[:report_card :card] [:= :bookmark.card_id :card.id]
+                  [:report_dashboard :dashboard] [:= :bookmark.dashboard_id :dashboard.id]
+                  :collection [:= :bookmark.collection_id :collection.id]]}))
+
   )
