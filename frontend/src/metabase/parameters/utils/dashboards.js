ca8ef992d01559d963496684db80c2d122dbc323
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-02-23 17:37:39 -0800

    use optional chaining

--- frontend/src/metabase/parameters/utils/dashboards.js
+++ frontend/src/metabase/parameters/utils/dashboards.js
@@ -64,7 +64,7 @@ export function isDashboardParameterWithoutMapping(parameter, dashboard) {
 }
 
 export function getMappingTargetField(card, mapping, metadata) {
-  if (!card || !card.dataset_query) {
+  if (!card?.dataset_query) {
     return null;
   }
 
