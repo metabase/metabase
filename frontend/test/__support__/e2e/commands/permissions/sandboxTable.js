2d7f1c2736dfbacd338dd9d68d68444f69255e59
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2022-02-25 12:55:53 +0100

    Global E2E Config: Solve circular dependency issue in helpers (#20741)
    * Add `SAMPLE_DB_TABLES` reference
    
    * Add notes to the cypress data references
    
    * Ensure table ids are correct
    
    Early warning mechanism.
    
    * Use SAMPLE_DB_TABLES in e2e helpers and custom commands
    
    * Use `SAMPLE_DB_ID` in the previously missed helper

--- frontend/test/__support__/e2e/commands/permissions/sandboxTable.js
+++ frontend/test/__support__/e2e/commands/permissions/sandboxTable.js
@@ -1,4 +1,6 @@
-import { USER_GROUPS } from "__support__/e2e/cypress_data";
+import { USER_GROUPS, SAMPLE_DB_TABLES } from "__support__/e2e/cypress_data";
+
+const { STATIC_ORDERS_ID } = SAMPLE_DB_TABLES;
 
 const { COLLECTION_GROUP } = USER_GROUPS;
 
@@ -8,7 +10,7 @@ Cypress.Commands.add(
     attribute_remappings = {},
     card_id = null,
     group_id = COLLECTION_GROUP,
-    table_id = 2,
+    table_id = STATIC_ORDERS_ID,
   } = {}) => {
     // Extract the name of the table, as well as `schema` and `db_id` that we'll need later on for `cy.updatePermissionsSchemas()`
     cy.request("GET", "/api/table").then(({ body: tables }) => {
