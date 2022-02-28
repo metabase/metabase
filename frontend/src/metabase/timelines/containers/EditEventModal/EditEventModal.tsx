e34023ed07cd4258e7db91c1d0752ef8f2779cd3
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-02-28 13:01:10 +0300

    Fix routes for modals

--- frontend/src/metabase/timelines/containers/EditEventModal/EditEventModal.tsx
+++ frontend/src/metabase/timelines/containers/EditEventModal/EditEventModal.tsx
@@ -1,20 +1,12 @@
 import { connect } from "react-redux";
-import { goBack, push } from "react-router-redux";
+import { goBack } from "react-router-redux";
 import _ from "underscore";
 import * as Urls from "metabase/lib/urls";
-import Collections from "metabase/entities/collections";
-import Timelines from "metabase/entities/timelines";
 import TimelineEvents from "metabase/entities/timeline-events";
-import { Collection, Timeline, TimelineEvent } from "metabase-types/api";
+import { TimelineEvent } from "metabase-types/api";
 import { State } from "metabase-types/store";
 import EditEventModal from "../../components/EditEventModal";
 import { ModalProps } from "../../types";
-
-const timelineProps = {
-  id: (state: State, props: ModalProps) =>
-    Urls.extractEntityId(props.params.timelineId),
-  query: { include: "events" },
-};
 
 const timelineEventProps = {
   id: (state: State, props: ModalProps) =>
@@ -22,27 +14,14 @@ const timelineEventProps = {
   entityAlias: "event",
 };
 
-const collectionProps = {
-  id: (state: State, props: ModalProps) =>
-    Urls.extractCollectionId(props.params.slug),
-};
-
 const mapDispatchToProps = (dispatch: any) => ({
-  onSubmit: async (
-    event: TimelineEvent,
-    timeline: Timeline,
-    collection: Collection,
-  ) => {
+  onSubmit: async (event: TimelineEvent) => {
     await dispatch(TimelineEvents.actions.update(event));
-    dispatch(push(Urls.timelineInCollection(timeline, collection)));
+    dispatch(goBack());
   },
-  onArchive: async (
-    event: TimelineEvent,
-    timeline: Timeline,
-    collection: Collection,
-  ) => {
+  onArchive: async (event: TimelineEvent) => {
     await dispatch(TimelineEvents.actions.setArchived(event, true));
-    dispatch(push(Urls.timelineInCollection(timeline, collection)));
+    dispatch(goBack());
   },
   onCancel: () => {
     dispatch(goBack());
@@ -50,8 +29,6 @@ const mapDispatchToProps = (dispatch: any) => ({
 });
 
 export default _.compose(
-  Timelines.load(timelineProps),
   TimelineEvents.load(timelineEventProps),
-  Collections.load(collectionProps),
   connect(null, mapDispatchToProps),
 )(EditEventModal);
