3b56e8d50860c25dee0b1694c859fedb97580eaa
Gustavo Saiani <gustavo@poe.ma>
2022-02-28 15:37:26 -0300

    Add nameOne property to entity

--- frontend/src/metabase/entities/bookmarks.js
+++ frontend/src/metabase/entities/bookmarks.js
@@ -1,16 +1,12 @@
-import { t } from "ttag";
-
 import { createEntity } from "metabase/lib/entities";
 
 import { BookmarkSchema } from "metabase/schema";
 
 const Bookmarks = createEntity({
   name: "bookmarks",
+  nameOne: "bookmark",
   path: "/api/bookmark",
   schema: BookmarkSchema,
-
-  displayNameOne: t`bookmark`,
-  displayNameMany: t`bookmarks`,
 });
 
 export default Bookmarks;
