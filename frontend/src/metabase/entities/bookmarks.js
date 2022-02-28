4868cb029be371f97cdace0c9bd4627c730d9e42
Gustavo Saiani <gustavo@poe.ma>
2022-02-28 20:18:39 -0300

    Comment out some possibilities in entities/bookmarks

--- frontend/src/metabase/entities/bookmarks.js
+++ frontend/src/metabase/entities/bookmarks.js
@@ -1,12 +1,15 @@
 import { createEntity } from "metabase/lib/entities";
 
-import { BookmarkSchema } from "metabase/schema";
+// import { BookmarkSchema } from "metabase/schema";
 
 const Bookmarks = createEntity({
   name: "bookmarks",
   nameOne: "bookmark",
   path: "/api/bookmark",
-  schema: BookmarkSchema,
+  // schema: BookmarkSchema,
+  // objectActions: {
+  //   setArchived: ({ id }, archived, opts) => console.log(id, archived, opts),
+  // },
 });
 
 export default Bookmarks;
