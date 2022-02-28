f77f510983fba85e8dd32e9d8f6d3c4220dd621b
Noah Moss <noahbmoss@gmail.com>
2022-02-28 12:28:44 -0500

    add test namespace

---
+++ enterprise/backend/test/metabase_enterprise/advanced_permissions/models/permissions_test.clj
@@ -0,0 +1,3 @@
+(ns metabase-enterprise.advanced-permissions.models.permissions-test
+  (:require [clojure.test :refer :all]
+            [metabase-enterprise.advanced-permissions.models.permissions :as ee-perms]))
