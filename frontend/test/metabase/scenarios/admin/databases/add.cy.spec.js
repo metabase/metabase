9a6381c1d67694fbf1dbe6737593c7affc121cac
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2022-03-01 11:48:05 +0100

    E2E oss/ee versions helpers cleanup (#20775)
    * Use human readable names for vars
    
    * Apply new version syntax to the related tests

--- frontend/test/metabase/scenarios/admin/databases/add.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/databases/add.cy.spec.js
@@ -3,6 +3,7 @@ import {
   popover,
   describeWithToken,
   mockSessionProperty,
+  isEE,
 } from "__support__/e2e/cypress";
 
 function typeField(label, value) {
@@ -155,7 +156,7 @@ describe("scenarios > admin > databases > add", () => {
   });
 
   it("EE should ship with Oracle and Vertica as options", () => {
-    cy.onlyOn(!!Cypress.env("HAS_ENTERPRISE_TOKEN"));
+    cy.onlyOn(isEE);
 
     cy.visit("/admin/databases/create");
     cy.contains("Database type")
