de017c218c5a0aefd0c548e206b4a8a0aa1fb2f8
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-01 16:51:14 +0300

    Add tests

--- frontend/test/metabase/scenarios/collections/timelines.cy.spec.js
+++ frontend/test/metabase/scenarios/collections/timelines.cy.spec.js
@@ -10,20 +10,30 @@ describe("timelines", () => {
       cy.signInAsAdmin();
     });
 
-    it("should create an event and a default timeline", () => {
+    it("should create the first event and timeline", () => {
       cy.visit("/collection/root");
 
       cy.findByLabelText("calendar icon").click();
-      cy.findByText("Add an event").click();
 
-      cy.findByLabelText("Event name").type("Release");
+      cy.findByText("Add an event").click();
+      cy.findByLabelText("Event name").type("v1.0");
       cy.findByLabelText("Date").type("10/20/2020");
       cy.button("Create").click();
 
-      cy.findByText("Our analytics events");
-      cy.findByText("Release");
+      cy.findByText("v1.0");
       cy.findByText("October 20, 2020");
-      cy.findByText("star icon");
+      cy.findByLabelText("star icon");
+
+      cy.findByText("Add an event").click();
+      cy.findByLabelText("Event name").type("v2.0");
+      cy.findByLabelText("Date").type("5/12/2021");
+      cy.findByText("Star").click();
+      cy.findByText("Balloons").click();
+      cy.button("Create").click();
+
+      cy.findByText("v2.0");
+      cy.findByText("May 12, 2021");
+      cy.findByLabelText("balloons icon");
     });
   });
 });
