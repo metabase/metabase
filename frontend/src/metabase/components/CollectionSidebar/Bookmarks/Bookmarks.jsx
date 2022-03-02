8490c2b0cb00451be96da1b2c4a56778096dc64a
Gustavo Saiani <gustavo@poe.ma>
2022-03-02 16:42:46 -0300

    Render bookmarks roughly like collections

--- frontend/src/metabase/components/CollectionSidebar/Bookmarks/Bookmarks.jsx
+++ frontend/src/metabase/components/CollectionSidebar/Bookmarks/Bookmarks.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 import { t } from "ttag";
 
+import CollectionLink from "metabase/collections/components/CollectionLink";
+import {
+  CollectionListIcon,
+  LabelContainer,
+} from "../Collections/CollectionsList/CollectionsList.styled";
+import BookmarksRoot from "./Bookmarks.styled";
+
 // import CollectionLink from "metabase/collections/components/CollectionLink";
 import { SidebarHeading } from "metabase/components/CollectionSidebar/CollectionSidebar.styled";
 
-const propTypes = {
+const LabelPropTypes = {
+  itemId: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+const BookmarksPropTypes = {
   bookmarks: PropTypes.object,
 };
+
+const Label = ({ itemId, name }) => {
+  return (
+    <LabelContainer>
+      <CollectionListIcon collection={true} targetOffsetX={0} />
+      {name}
+    </LabelContainer>
+  );
+};
+
+Label.propTypes = LabelPropTypes;
 
 const CollectionSidebarBookmarks = ({ bookmarks }) => {
   if (bookmarks.length === 0) {
@@ -15,36 +38,29 @@ const CollectionSidebarBookmarks = ({ bookmarks }) => {
   }
 
   return (
-    <>
+    <BookmarksRoot>
       <SidebarHeading>{t`Bookmarks`}</SidebarHeading>
 
       {bookmarks.map(({ id, name }, index) => {
-        return <div key={id}>Yes</div>;
-        // return (
-        //   <CollectionLink
-        //     key={`bookmark-${id}`}
-        //     to={"https://www.google.com"}
-        //     selected={false}
-        //     depth={0}
-        //     onClick={() => {}}
-        //     hovered={false}
-        //     highlighted={false}
-        //     role="sidebar-bookmark"
-        //   >
-        //     <Label
-        //       action={action}
-        //       collection={collection}
-        //       initialIcon={initialIcon}
-        //       isOpen={isOpen}
-        //       depth={depth}
-        //     />
-        //   </CollectionLink>
-        // );
+        return (
+          <CollectionLink
+            key={`bookmark-${id}`}
+            to={"https://www.google.com"}
+            selected={false}
+            depth={0}
+            onClick={() => {}}
+            hovered={false}
+            highlighted={false}
+            role="sidebar-bookmark"
+          >
+            <Label itemId={id} name={name} />
+          </CollectionLink>
+        );
       })}
-    </>
+    </BookmarksRoot>
   );
 };
 
-CollectionSidebarBookmarks.propTypes = propTypes;
+CollectionSidebarBookmarks.propTypes = BookmarksPropTypes;
 
 export default CollectionSidebarBookmarks;
---
+++ frontend/src/metabase/components/CollectionSidebar/Bookmarks/Bookmarks.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+import { space } from "metabase/styled-components/theme";
+
+const CollectionSidebarBookmarksRoot = styled.div`
+  margin-bottom: ${space(2)};
+`;
+
+export default CollectionSidebarBookmarksRoot;
