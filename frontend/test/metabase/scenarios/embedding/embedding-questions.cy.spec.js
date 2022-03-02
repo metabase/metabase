68409eaac425ec1ab7afca0af333b4a30669579c
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-03-02 20:11:08 +0100

    Add initial embedding test for the regular GUI question

---
+++ frontend/test/metabase/scenarios/embedding/embedding-questions.cy.spec.js
@@ -0,0 +1,85 @@
+import { restore, visitQuestion, popover } from "__support__/e2e/cypress";
+import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
+
+const { ORDERS, ORDERS_ID, PRODUCTS } = SAMPLE_DATABASE;
+
+const questionDetails = {
+  name: "Orders4t#7 t3",
+  description: "Foo",
+  query: {
+    "source-table": ORDERS_ID,
+    limit: 5,
+    expressions: { Math: ["+", 1, 1] },
+  },
+  visualization_settings: {
+    column_settings: {
+      [`["ref",["field",${ORDERS.CREATED_AT},null]]`]: {
+        date_abbreviate: true,
+      },
+      [`["ref",["field",${ORDERS.TOTAL},null]]`]: {
+        column_title: "Billed",
+        number_style: "currency",
+        currency_in_header: false,
+      },
+      [`["ref",["field",${ORDERS.TAX},null]]`]: { show_mini_bar: true },
+    },
+  },
+};
+
+describe("scenarios > embedding > questions ", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+
+    // Remap Product ID -> Product Title
+    cy.request("POST", `/api/field/${ORDERS.PRODUCT_ID}/dimension`, {
+      name: "Product ID as Title",
+      type: "external",
+      human_readable_field_id: PRODUCTS.TITLE,
+    });
+
+    // Do not include Subtotal anywhere
+    cy.request("PUT", `/api/field/${ORDERS.SUBTOTAL}`, {
+      visibility_type: "sensitive",
+    });
+  });
+
+  it("should display the regular GUI question correctly", () => {
+    cy.createQuestion(questionDetails).then(({ body: { id } }) => {
+      cy.request("PUT", `/api/card/${id}`, { enable_embedding: true });
+
+      visitQuestion(id);
+    });
+
+    cy.icon("share").click();
+    cy.findByText("Embed this question in an application").click();
+
+    cy.document().then(doc => {
+      const iframe = doc.querySelector("iframe");
+      cy.visit(iframe.src);
+    });
+
+    cy.findByText(questionDetails.name);
+
+    cy.icon("info").realHover();
+    popover().contains(questionDetails.description);
+
+    // Data model: Renamed column
+    cy.findByText("Product ID as Title");
+    // Data model: Display value changed to show FK
+    cy.findByText("Awesome Concrete Shoes");
+    // Custom column
+    cy.findByText("Math");
+    // Question settings: Renamed column
+    cy.findByText("Billed");
+    // Question settings: Column formating
+    cy.findByText("$39.72");
+    // Question settings: Abbreviated date
+    cy.findByText("Feb 11, 2019, 9:40 PM");
+    // Question settings: Show mini-bar
+    cy.findAllByTestId("mini-bar");
+
+    // Data model: Subtotal is turned off globally
+    cy.findByText("Subtotal").should("not.exist");
+  });
+});
