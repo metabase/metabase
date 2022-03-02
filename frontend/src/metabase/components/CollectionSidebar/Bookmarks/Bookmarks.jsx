3d3de3b56cc9dac57db3160ad1acf0e1cde354e8
Gustavo Saiani <gustavo@poe.ma>
2022-03-02 17:30:06 -0300

    Render bookmarks with icon

--- frontend/src/metabase/components/CollectionSidebar/Bookmarks/Bookmarks.jsx
+++ frontend/src/metabase/components/CollectionSidebar/Bookmarks/Bookmarks.jsx
@@ -3,11 +3,8 @@ import PropTypes from "prop-types";
 import { t } from "ttag";
 
 import CollectionLink from "metabase/collections/components/CollectionLink";
-import {
-  CollectionListIcon,
-  LabelContainer,
-} from "../Collections/CollectionsList/CollectionsList.styled";
-import BookmarksRoot from "./Bookmarks.styled";
+import { LabelContainer } from "../Collections/CollectionsList/CollectionsList.styled";
+import BookmarksRoot, { BookmarkTypeIcon } from "./Bookmarks.styled";
 
 // import CollectionLink from "metabase/collections/components/CollectionLink";
 import { SidebarHeading } from "metabase/components/CollectionSidebar/CollectionSidebar.styled";
@@ -24,7 +21,7 @@ const BookmarksPropTypes = {
 const Label = ({ itemId, name }) => {
   return (
     <LabelContainer>
-      <CollectionListIcon collection={true} targetOffsetX={0} />
+      <BookmarkTypeIcon name="grid" />
       {name}
     </LabelContainer>
   );
@@ -47,10 +44,7 @@ const CollectionSidebarBookmarks = ({ bookmarks }) => {
             key={`bookmark-${id}`}
             to={"https://www.google.com"}
             selected={false}
-            depth={0}
             onClick={() => {}}
-            hovered={false}
-            highlighted={false}
             role="sidebar-bookmark"
           >
             <Label itemId={id} name={name} />
--- frontend/src/metabase/components/CollectionSidebar/Bookmarks/Bookmarks.styled.jsx
+++ frontend/src/metabase/components/CollectionSidebar/Bookmarks/Bookmarks.styled.jsx
@@ -2,8 +2,15 @@ import styled from "@emotion/styled";
 
 import { space } from "metabase/styled-components/theme";
 
+import Icon from "metabase/components/Icon";
+
 const CollectionSidebarBookmarksRoot = styled.div`
   margin-bottom: ${space(2)};
+`;
+
+export const BookmarkTypeIcon = styled(Icon)`
+  margin-right: 6px;
+  opacity: 0.5;
 `;
 
 export default CollectionSidebarBookmarksRoot;
