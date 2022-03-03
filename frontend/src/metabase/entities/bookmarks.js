41b64e61210f5d5677eab9b0c74dd2355cf36cc0
Gustavo Saiani <gustavo@poe.ma>
2022-03-03 20:31:17 -0300

    Load bookmarks from webservice

--- frontend/src/metabase/entities/bookmarks.js
+++ frontend/src/metabase/entities/bookmarks.js
@@ -6,25 +6,6 @@ const Bookmarks = createEntity({
   nameOne: "bookmark",
   path: "/api/bookmark",
   schema: BookmarkSchema,
-
-  api: {
-    list: async () => [
-      {
-        id: 1,
-        name: "A name",
-        type: "question",
-        item_id: "2",
-        slug: "2-a-name",
-      },
-      {
-        id: 2,
-        name: "Another name",
-        type: "question",
-        item_id: "3",
-        slug: "3-another-name",
-      },
-    ],
-  },
 });
 
 export default Bookmarks;
