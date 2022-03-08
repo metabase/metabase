15eb073c4052f91e85f53b13b60825501a89e804
losrebellos <ben@benoitvinay.com>
2022-03-08 16:02:36 +0100

    NoResults component created

---
+++ frontend/src/metabase/static-viz/components/NoResults/constants.ts
@@ -0,0 +1,17 @@
+export const ICON_SIZE = 120;
+export const ICON_MARGIN = 16;
+
+export const LAYOUT = {
+  width: 240,
+  height: 240,
+  margin: 20,
+  font: {
+    family: "Lato, sans-serif",
+    size: 28,
+    weight: 700,
+  },
+  colors: {
+    textLight: "#b8bbc3",
+    textDark: "#4c5773",
+  },
+};
