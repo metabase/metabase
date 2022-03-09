8ad9254d07a0aca2257b312254b81346b7ecc865
Gustavo Saiani <gustavo@poe.ma>
2022-03-09 15:34:04 -0300

    Bookmark questions and display them on sidebar (#20918)

--- frontend/src/metabase/lib/urls.js
+++ frontend/src/metabase/lib/urls.js
@@ -346,3 +346,22 @@ export function extractCollectionId(slug) {
   }
   return extractEntityId(slug);
 }
+
+/*
+ * Will transform a name like `This name has spaces and Uppercases`
+ * into `this-name-has-spaced-and-uppercases`
+ *
+ * then prepend an entity type, say "card" or "dashboard"
+ * plus the passed id.
+ * "
+ */
+export function bookmark({ type, id, name }) {
+  const kebabCaseName = name
+    .toLowerCase()
+    .split(/[ ,]+/)
+    .join("-");
+
+  const [, idInteger] = id.split("-");
+
+  return `${type}/${idInteger}-${kebabCaseName}`;
+}
