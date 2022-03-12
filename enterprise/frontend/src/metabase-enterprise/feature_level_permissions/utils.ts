99e5144ba5021af9b7422664b0f5bbf70a1442bb
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-03-12 00:13:03 -0500

    feature level permission based routing

---
+++ enterprise/frontend/src/metabase-enterprise/feature_level_permissions/utils.ts
@@ -0,0 +1,11 @@
+import { User } from "metabase-types/api";
+
+export const canAccessSettings = (user: User) =>
+  canAccessDataModel(user) || canAccessDatabaseManagement(user);
+
+export const canAccessDataModel = ({ can_access_data_model }: User) =>
+  can_access_data_model;
+
+export const canAccessDatabaseManagement = ({
+  can_access_database_management,
+}: User) => can_access_database_management;
