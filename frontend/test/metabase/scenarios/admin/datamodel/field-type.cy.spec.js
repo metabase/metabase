3d22a1a5dc0d34ff68f4e441c530051e3f9d157c
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-03-14 13:28:08 +0100

    Ensure popover contents loaded before we attempt to scroll

--- frontend/test/metabase/scenarios/admin/datamodel/field-type.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/datamodel/field-type.cy.spec.js
@@ -83,6 +83,9 @@ function setFieldType({ oldValue, newValue } = {}) {
   getFieldType(oldValue).click();
 
   popover().within(() => {
+    cy.findByText(oldValue)
+      .closest(".ReactVirtualized__Grid")
+      .scrollTo(0, 0); // HACK: scroll to the top of the list. Ideally we should probably disable AccordionList virtualization
     searchFieldType(newValue);
     cy.findByText(newValue).click();
   });
@@ -98,7 +101,6 @@ function checkNoFieldType({ oldValue, newValue } = {}) {
 }
 
 function searchFieldType(type) {
-  cy.get(".ReactVirtualized__Grid").scrollTo(0, 0); // HACK: scroll to the top of the list. Ideally we should probably disable AccordionList virtualization
   cy.findByPlaceholderText("Find...").type(type);
 }
 
