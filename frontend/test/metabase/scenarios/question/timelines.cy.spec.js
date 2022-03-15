41b57762dd083cbe511e404342eea4ccd371c567
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-15 15:47:59 +0300

    Add tests

--- frontend/test/metabase/scenarios/question/timelines.cy.spec.js
+++ frontend/test/metabase/scenarios/question/timelines.cy.spec.js
@@ -62,5 +62,49 @@ describe("scenarios > collections > timelines", () => {
       cy.findByText("Releases");
       cy.findByText("RC2");
     });
+
+    it("should archive and unarchive and event", () => {
+      cy.createTimelineWithEvents({
+        timeline: { name: "Releases" },
+        events: [{ name: "RC1", timestamp: "2018-10-20T00:00:00Z" }],
+      });
+
+      cy.visit("/question/3");
+      cy.findByLabelText("calendar icon").click();
+      cy.findByText("Releases").click();
+      cy.findByLabelText("ellipsis icon").click();
+      cy.findByText("Archive event").click();
+      cy.findByText("RC1").should("not.exist");
+
+      cy.findByText("Undo").click();
+      cy.findByText("RC1");
+    });
+  });
+
+  describe("as readonly user", () => {
+    it("should not allow creating default timelines", () => {
+      cy.signIn("readonly");
+      cy.visit("/question/3");
+
+      cy.findByLabelText("calendar icon").click();
+      cy.findByText(/Events in Metabase/);
+      cy.findByText("Add an event").should("not.exist");
+    });
+
+    it("should not allow creating or editing events", () => {
+      cy.signInAsAdmin();
+      cy.createTimelineWithEvents({
+        timeline: { name: "Releases" },
+        events: [{ name: "RC1" }],
+      });
+      cy.signOut();
+      cy.signIn("readonly");
+      cy.visit("/question/3");
+
+      cy.findByLabelText("calendar icon").click();
+      cy.findByText("Releases").click();
+      cy.findByText("Add an event").should("not.exist");
+      cy.findByLabelText("ellipsis icon").should("not.exist");
+    });
   });
 });
