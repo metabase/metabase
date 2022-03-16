07dcb0ee1d876d48470421f91008346b002ce8a7
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-16 21:03:39 +0300

    Add event selection

--- frontend/src/metabase/timelines/questions/components/EventCard/EventCard.styled.tsx
+++ frontend/src/metabase/timelines/questions/components/EventCard/EventCard.styled.tsx
@@ -4,6 +4,8 @@ import { alpha, color } from "metabase/lib/colors";
 
 export const CardRoot = styled.div`
   display: flex;
+  padding-left: 1rem;
+  padding-right: 0.75rem;
 `;
 
 export const CardIcon = styled(Icon)`
