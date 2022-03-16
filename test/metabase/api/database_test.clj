f9f1bf414d7360721d3beebf69dd996afc4eb6fd
Cam Saul <1455846+camsaul@users.noreply.github.com>
2022-03-16 13:05:23 -0700

    Fix noisy backend tests (#20910)
    * Change u/profile from println to log/info
    
    * Change most test printlns to log/info
    
    * Make u/profile message a fn rather than a delay
    
    * Apparently clj-http errors aren't wrapped in :object anymore
    
    * Add running commentary
    
    * Log the amount of time it took to find and run tests
    
    * Sort namespaces
    
    * Update test
    
    Co-authored-by: Diogo Mendes <diogo@metabase.com>

--- test/metabase/api/database_test.clj
+++ test/metabase/api/database_test.clj
@@ -195,9 +195,8 @@
               update! (fn [expected-status-code]
                         (mt/user-http-request :crowberto :put expected-status-code (format "database/%d" db-id) updates))]
           (testing "Should check that connection details are valid on save"
-            (mt/suppress-output
-              (is (= false
-                     (:valid (update! 400))))))
+            (is (= false
+                   (:valid (update! 400)))))
           (testing "If connection details are valid, we should be able to update the Database"
             (with-redefs [driver/can-connect? (constantly true)]
               (is (= nil
@@ -212,17 +211,16 @@
                       :features     (driver.u/features :h2 curr-db)}
                      (into {} curr-db)))))))))
 
-    (mt/with-log-level :info
-      (testing "should be able to set `auto_run_queries`"
-        (testing "when creating a Database"
-          (is (= {:auto_run_queries false}
-                 (select-keys (create-db-via-api! {:auto_run_queries false}) [:auto_run_queries]))))
-        (testing "when updating a Database"
-          (mt/with-temp Database [{db-id :id} {:engine ::test-driver}]
-            (let [updates {:auto_run_queries false}]
-              (mt/user-http-request :crowberto :put 200 (format "database/%d" db-id) updates))
-            (is (= false
-                   (db/select-one-field :auto_run_queries Database, :id db-id)))))))
+    (testing "should be able to set `auto_run_queries`"
+      (testing "when creating a Database"
+        (is (= {:auto_run_queries false}
+               (select-keys (create-db-via-api! {:auto_run_queries false}) [:auto_run_queries]))))
+      (testing "when updating a Database"
+        (mt/with-temp Database [{db-id :id} {:engine ::test-driver}]
+          (let [updates {:auto_run_queries false}]
+            (mt/user-http-request :crowberto :put 200 (format "database/%d" db-id) updates))
+          (is (= false
+                 (db/select-one-field :auto_run_queries Database, :id db-id))))))
     (testing "should be able to unset cache_ttl"
       (mt/with-temp Database [{db-id :id}]
         (let [updates1 {:cache_ttl    1337}
