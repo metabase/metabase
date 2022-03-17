3a85e7fc28b912a08332becf8ece9cfbe9581684
Gustavo Saiani <gustavo@poe.ma>
2022-03-17 10:47:37 -0300

    Test add/remove bookmark in collection page

--- frontend/test/metabase/scenarios/collections/bookmarks.cy.spec.js
+++ frontend/test/metabase/scenarios/collections/bookmarks.cy.spec.js
@@ -1,32 +1,7 @@
 import _ from "underscore";
-import {
-  restore,
-  modal,
-  popover,
-  openOrdersTable,
-  sidebar,
-} from "__support__/e2e/cypress";
-import { displaySidebarChildOf } from "./helpers/e2e-collections-sidebar.js";
-import { USERS, USER_GROUPS } from "__support__/e2e/cypress_data";
-
-const { nocollection } = USERS;
-const { DATA_GROUP } = USER_GROUPS;
+import { restore, sidebar } from "__support__/e2e/cypress";
 
 // Z because the api lists them alphabetically by name, so it makes it easier to check
-const [admin, collection, sub_collection] = [
-  {
-    name: "Robert Tableton's Personal Collection",
-    id: 1,
-  },
-  {
-    name: "Z Collection",
-    id: null, // TBD from a response body
-  },
-  {
-    name: "ZZ Sub-Collection",
-    id: null, // TBD from a response body
-  },
-];
 
 describe("Collection related issues reproductions", () => {
   beforeEach(() => {
@@ -34,31 +9,43 @@ describe("Collection related issues reproductions", () => {
     cy.signInAsAdmin();
   });
 
-  it.only("can bookmark collection from its page", () => {
+  it("can add and remove bookmarks from collection from its page", () => {
     cy.visit("/collection/1");
+
+    // Add bookmark
+    cy.icon("bookmark").click();
+
+    // Bookmark Icon should be blue once Collection is bookmarked
+    cy.icon("bookmark")
+      .should("have.css", "color")
+      .and("eq", "rgb(80, 158, 227)");
+
+    sidebar().within(() => {
+      cy.findByText("Bobby Tables's Personal Collection");
+      cy.findByText("Bookmarks");
+
+      // Once there is a list of bookmarks,
+      // we add a heading to the list of collections below the list of bookmarks
+      cy.findByText("Collections");
+    });
+
+    // Remove bookmark
+    cy.findByTestId("collection-menu").within(() => {
+      cy.icon("bookmark").click();
+    });
+
+    sidebar().within(() => {
+      cy.findByText("Bobby Tables's Personal Collection").should("not.exist");
+      cy.findByText("Bookmarks").should("not.exist");
+
+      // Once there is no list of bookmarks,
+      // we remove the heading for the list of collections
+      cy.findByText("Collections").should("not.exist");
+    });
+
+    // Bookmark Icon should be dark gray once bookmark is removed from Collection
+    cy.icon("bookmark")
+      .should("have.css", "color")
+      .and("eq", "rgb(76, 87, 115)");
   });
 });
-
-function openEllipsisMenuFor(item) {
-  cy.findByText(item)
-    .closest("tr")
-    .find(".Icon-ellipsis")
-    .click({ force: true });
-}
-
-function selectItemUsingCheckbox(item, icon = "table") {
-  cy.findByText(item)
-    .closest("tr")
-    .within(() => {
-      cy.icon(icon).trigger("mouseover");
-      cy.findByRole("checkbox").click();
-    });
-}
-
-function getSidebarCollectionChildrenFor(item) {
-  return sidebar()
-    .findByText(item)
-    .closest("a")
-    .parent()
-    .parent();
-}
