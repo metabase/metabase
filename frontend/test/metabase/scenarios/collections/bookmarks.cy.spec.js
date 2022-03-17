4d5a87518ace8428aa76b459608c96b5eb56d6d5
Gustavo Saiani <gustavo@poe.ma>
2022-03-17 11:14:15 -0300

    Test add/remove bookmark to question in collection page

--- frontend/test/metabase/scenarios/collections/bookmarks.cy.spec.js
+++ frontend/test/metabase/scenarios/collections/bookmarks.cy.spec.js
@@ -3,10 +3,16 @@ import { restore, sidebar } from "__support__/e2e/cypress";
 
 // Z because the api lists them alphabetically by name, so it makes it easier to check
 
-describe("Collection related issues reproductions", () => {
+describe("Bookmarks in a collection page", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+  });
+
+  it("cannot add bookmark to root collection", () => {
+    cy.visit("/collection/root");
+
+    cy.icon("bookmark").should("not.exist");
   });
 
   it("can add and remove bookmarks from collection from its page", () => {
@@ -21,8 +27,8 @@ describe("Collection related issues reproductions", () => {
       .and("eq", "rgb(80, 158, 227)");
 
     sidebar().within(() => {
-      cy.findByText("Bobby Tables's Personal Collection");
       cy.findByText("Bookmarks");
+      cy.findByText("Bobby Tables's Personal Collection");
 
       // Once there is a list of bookmarks,
       // we add a heading to the list of collections below the list of bookmarks
@@ -48,4 +54,37 @@ describe("Collection related issues reproductions", () => {
       .should("have.css", "color")
       .and("eq", "rgb(76, 87, 115)");
   });
+
+  it("can add/remove bookmark from question in collection", () => {
+    cy.visit("/collection/root");
+
+    openEllipsisMenuFor("Orders");
+    cy.findByText("Bookmark").click();
+
+    sidebar().within(() => {
+      cy.findByText("Bookmarks");
+      cy.findByText("Orders");
+    });
+
+    cy.get("td")
+      .contains("Orders")
+      .closest("tr")
+      .find(".Icon-ellipsis")
+      .click({ force: true });
+
+    cy.findByText("Remove Bookmark").click();
+
+    sidebar().within(() => {
+      cy.findByText("Bookmarks").should("not.exist");
+      cy.findByText("Orders").should("not.exist");
+    });
+  });
 });
+
+function openEllipsisMenuFor(item) {
+  cy.get("td")
+    .contains(item)
+    .closest("tr")
+    .find(".Icon-ellipsis")
+    .click({ force: true });
+}
