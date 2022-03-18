4c851c44248ba13707d1b9879533602a00807ad1
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-18 19:58:04 +0000

    Fix data reference layout

--- enterprise/frontend/src/metabase-enterprise/audit_app/containers/AuditApp.jsx
+++ enterprise/frontend/src/metabase-enterprise/audit_app/containers/AuditApp.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
+import fitViewport from "metabase/hoc/FitViewPort";
+
 import SidebarLayout from "../components/SidebarLayoutFixedWidth";
 import AuditSidebar from "../components/AuditSidebar";
 
+const Layout = fitViewport(SidebarLayout);
+
 const AuditApp = ({ children }) => (
-  <SidebarLayout sidebar={<AuditSidebar />}>
+  <Layout sidebar={<AuditSidebar />}>
     <div>{children}</div>
-  </SidebarLayout>
+  </Layout>
 );
 
 export default AuditApp;
