c52da18fb7e1eec41d3443a276a5aa3ff555d2f4
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-22 22:56:35 +0300

    Add delete modal

--- frontend/src/metabase/timelines/collections/routes.tsx
+++ frontend/src/metabase/timelines/collections/routes.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { ModalRoute } from "metabase/hoc/ModalRoute";
 import DeleteEventModal from "./containers/DeleteEventModal";
+import DeleteTimelineModal from "./containers/DeleteTimelineModal";
 import EditEventModal from "./containers/EditEventModal";
 import EditTimelineModal from "./containers/EditTimelineModal";
 import NewEventModal from "./containers/NewEventModal";
@@ -45,6 +46,13 @@ const getRoutes = () => {
         {...{
           path: "timelines/:timelineId/archive",
           modal: TimelineArchiveModal,
+          modalProps: { enableTransition: false },
+        }}
+      />
+      <ModalRoute
+        {...{
+          path: "timelines/:timelineId/delete",
+          modal: DeleteTimelineModal,
           modalProps: { enableTransition: false },
         }}
       />
