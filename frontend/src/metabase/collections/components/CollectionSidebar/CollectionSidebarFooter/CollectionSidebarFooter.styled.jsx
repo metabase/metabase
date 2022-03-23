032d38de42c2ab128d721f55668152e5cc79d947
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2022-03-23 14:48:03 -0400

    fix accidental removal of links (minimalism was a fad anyway)

--- frontend/src/metabase/collections/components/CollectionSidebar/CollectionSidebarFooter/CollectionSidebarFooter.styled.jsx
+++ frontend/src/metabase/collections/components/CollectionSidebar/CollectionSidebarFooter/CollectionSidebarFooter.styled.jsx
@@ -19,7 +19,7 @@ export const Icon = styled(GenericIcon)`
 
 export const Link = styled(GenericLink)`
   align-items: center;
-  color: ${color("brand")};
+  color: ${color("white")};
   display: flex;
   font-weight: 700;
   margin-top: ${space(2)};
