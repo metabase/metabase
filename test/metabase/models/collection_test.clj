8babe80a72b4fa9fef26dc40d6af230bdda1914d
Ngoc Khuat <qn.khuat@gmail.com>
2022-03-23 17:08:11 +0700

    add schema migrations tests

--- test/metabase/models/collection_test.clj
+++ test/metabase/models/collection_test.clj
@@ -1082,8 +1082,8 @@
   (perms-path-ids->names
    (zipmap (map :name collections)
            collections)
-   ;; ignoring subscription permission because it's a permission that added when create a new group and we don't
-   ;; care about it here
+   ;; ignoring subscription permission because it's a permission that is automatically added when creating
+   ;; a new group and we don't care about it here
    (db/select-field :object Permissions
                     {:where [:and
                              [:<> :object (perms/general-perms-path :subscription)]
