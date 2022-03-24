2ab46e2d1493a66cdcfc3bdb55e0c73dd2dfc64d
Cam Saul <github@camsaul.com>
2022-03-23 18:20:04 -0700

    Mark test as ^:parallel

--- test/metabase/driver/common/parameters/dates_test.clj
+++ test/metabase/driver/common/parameters/dates_test.clj
@@ -5,7 +5,7 @@
             [metabase.test :as mt]
             [metabase.util.date-2 :as u.date]))
 
-(deftest date-string->filter-test
+(deftest ^:parallel date-string->filter-test
   (testing "year and month"
     (is (= [:between
             [:field "field" {:base-type :type/DateTime, :temporal-unit :day}]
