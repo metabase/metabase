23118f4f74f123a6d644066159f43804f5ce4381
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2022-03-24 10:51:33 -0400

    search bar color

--- frontend/src/metabase/nav/components/SearchBar.jsx
+++ frontend/src/metabase/nav/components/SearchBar.jsx
@@ -68,9 +68,6 @@ export default class SearchBar extends React.Component {
         >
           <Icon name="search" ml={["10px", 2]} />
           <SearchInput
-            py={2}
-            pr={[0, 2]}
-            pl={1}
             ref={ref => (this.searchInput = ref)}
             value={searchText}
             maxLength={200}
--- frontend/src/metabase/nav/components/SearchBar.styled.jsx
+++ frontend/src/metabase/nav/components/SearchBar.styled.jsx
@@ -1,14 +1,13 @@
 import styled from "@emotion/styled";
-import { space } from "styled-system";
 
 import { color } from "metabase/lib/colors";
+import { space } from "metabase/styled-components/theme";
 
 export const SearchWrapper = styled.div`
   display: flex;
   align-items: center;
   position: relative;
-  background-color: white;
-  border: 1px solid #ddd;
+  background-color: ${color("bg-medium")};
   border-radius: 6px;
   flex: 1 1 auto;
   max-width: 50em;
@@ -17,17 +16,16 @@ export const SearchWrapper = styled.div`
 `;
 
 export const SearchInput = styled.input`
-  ${space};
   background-color: transparent;
   width: 100%;
   border: none;
   color: ${color("text-dark")};
-  font-size: 0.8em;
+  padding: ${space(2)};
   font-weight: 700;
   &:focus {
     outline: none;
   }
   &::placeholder {
-    color: ${color("text-medium")};
+    color: ${color("text-dark")};
   }
 `;
