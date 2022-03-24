678961183f911b313c3e8fb8e5b77e7ea32f2070
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-24 18:56:22 +0300

    Add tests

--- frontend/src/metabase/core/components/Markdown/Markdown.stories.tsx
+++ frontend/src/metabase/core/components/Markdown/Markdown.stories.tsx
@@ -18,5 +18,5 @@ Our first email blast to the mailing list not directly linked to the release
 of a new version. We wanted to see if this would effect visits to landing pages
 for the features in 0.41.
 
-Here’s a [notion doc](https://metabase.com) with the findings.`,
+Here’s a [doc](https://metabase.test) with the findings.`,
 };
