899a0fd4aec9ab47f88c867f82c77e1ef849540e
dan sutton <dan@dpsutton.com>
2022-03-25 18:26:46 -0500

    docstring

--- src/metabase/models/persisted_info.clj
+++ src/metabase/models/persisted_info.clj
@@ -5,7 +5,10 @@
             [metabase.util :as u]
             [toucan.models :as models]))
 
-(def ^:dynamic *allow-persisted-substitution* true)
+(def ^:dynamic *allow-persisted-substitution*
+  "Allow persisted substitution. When refreshing, set this to nil to ensure that all undelrying queries are used to
+  rebuild the persisted table."
+  true)
 
 (defn slug-name
   "A slug from a card suitable for a table name."
