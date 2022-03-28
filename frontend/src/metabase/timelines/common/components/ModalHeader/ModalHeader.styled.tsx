a8af07c267525fa04a63eba80ac8f5ea46f58287
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-28 17:14:18 +0300

    Fix back button in events archive

--- frontend/src/metabase/timelines/common/components/ModalHeader/ModalHeader.styled.tsx
+++ frontend/src/metabase/timelines/common/components/ModalHeader/ModalHeader.styled.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import { color } from "metabase/lib/colors";
 import IconButtonWrapper from "metabase/components/IconButtonWrapper";
+import Icon from "metabase/components/Icon";
+import Ellipsified from "metabase/components/Ellipsified";
 
 export const HeaderRoot = styled.div`
   display: flex;
@@ -8,29 +10,31 @@ export const HeaderRoot = styled.div`
   padding: 2rem 2rem 0;
 `;
 
-export const HeaderTitle = styled.div`
-  flex: 1 1 auto;
-  color: ${color("text-dark")};
+export interface HeaderTitleProps {
+  tooltipMaxWidth?: string;
+}
+
+export const HeaderTitle = styled(Ellipsified)<HeaderTitleProps>`
   font-size: 1.25rem;
   line-height: 1.5rem;
   font-weight: bold;
   margin-right: 1rem;
+`;
+
+export const HeaderLink = styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  align-items: center;
+  color: ${color("text-dark")};
   min-width: 0;
 `;
 
-export const HeaderActions = styled.div`
-  margin-right: 1rem;
+export const HeaderBackIcon = styled(Icon)`
+  margin-right: 0.5rem;
 `;
 
-export const HeaderBackButton = styled(IconButtonWrapper)`
-  flex: 0 0 auto;
-  color: ${color("text-dark")};
-  padding: 0.25rem 0.5rem;
-  margin-left: -0.5rem;
-
-  &:hover {
-    color: ${color("brand")};
-  }
+export const HeaderMenu = styled.div`
+  margin-right: 1rem;
 `;
 
 export const HeaderCloseButton = styled(IconButtonWrapper)`
