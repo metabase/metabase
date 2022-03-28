a5cf3eb6317b2bd33d8bf3260a19be5aa4568f15
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-28 19:14:44 +0100

    Add bookmarks

---
+++ frontend/src/metabase/nav/containers/MainNavbar/BookmarkList/BookmarkList.styled.tsx
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+import { color } from "metabase/lib/colors";
+import { space } from "metabase/styled-components/theme";
+
+import Icon from "metabase/components/Icon";
+
+export const BookmarkListRoot = styled.div`
+  margin: ${space(1)} 0;
+
+  &:hover {
+    button {
+      opacity: 0.5;
+    }
+  }
+
+  button {
+    opacity: 0;
+    color: ${color("brand")};
+    cursor: pointer;
+  }
+`;
+
+export const BookmarkTypeIcon = styled(Icon)`
+  margin-right: 6px;
+  opacity: 0.5;
+`;
