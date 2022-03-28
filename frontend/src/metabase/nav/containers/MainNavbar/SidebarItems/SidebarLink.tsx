4566f15ff409bc5dabb6866f91618c491b317b6a
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-28 19:12:40 +0100

    Add links (data, archive, users' collections)

---
+++ frontend/src/metabase/nav/containers/MainNavbar/SidebarItems/SidebarLink.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import _ from "underscore";
+
+import { TreeNode } from "metabase/components/tree/TreeNode";
+import Icon from "metabase/components/Icon";
+
+import { FullWidthLink } from "./SidebarItems.styled";
+
+interface Props {
+  children: string;
+  url: string;
+  icon: string;
+  isSelected?: boolean;
+  right?: React.ReactNode;
+}
+
+function SidebarLink({
+  children,
+  icon,
+  url,
+  isSelected = false,
+  right = null,
+  ...props
+}: Props) {
+  return (
+    <TreeNode.Root depth={0} isSelected={isSelected} {...props}>
+      <FullWidthLink to={url}>
+        {icon && (
+          <TreeNode.IconContainer>
+            <Icon name={icon} />
+          </TreeNode.IconContainer>
+        )}
+        <TreeNode.NameContainer>{children}</TreeNode.NameContainer>
+      </FullWidthLink>
+      {React.isValidElement(right) && right}
+    </TreeNode.Root>
+  );
+}
+
+export default SidebarLink;
