5ce24d7db69bef329cf57865b26876c9db5d88a3
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-28 19:09:30 +0100

    Fix dragged collection item's z-index

--- frontend/src/metabase/containers/dnd/ItemsDragLayer.jsx
+++ frontend/src/metabase/containers/dnd/ItemsDragLayer.jsx
@@ -43,7 +43,7 @@ export default class ItemsDragLayer extends React.Component {
           transform: `translate(${x}px, ${y}px)`,
           pointerEvents: "none",
           opacity: 0.65,
-          zIndex: 1,
+          zIndex: 999,
         }}
       >
         <DraggedItems
