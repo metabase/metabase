a369b84b26ea4e8141b05d0e51cca807f0333801
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-28 19:08:40 +0100

    Create sidebar and add collections tree

---
+++ frontend/src/metabase/nav/containers/MainNavbar/MainNavbar.styled.tsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+
+import { SIDEBAR_WIDTH } from "metabase/collections/constants";
+
+import { color } from "metabase/lib/colors";
+import { space } from "metabase/styled-components/theme";
+import { breakpointMinSmall } from "metabase/styled-components/theme/media-queries";
+
+export const Sidebar = styled.aside`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding-top: ${space(1)};
+  width: 0;
+
+  overflow-x: hidden;
+  overflow-y: auto;
+
+  background-color: transparent;
+
+  ${breakpointMinSmall} {
+    width: ${SIDEBAR_WIDTH};
+  }
+`;
+
+export const LoadingContainer = styled.div`
+  color: ${color("brand")};
+  text-align: center;
+`;
+
+export const LoadingTitle = styled.h2`
+  color: ${color("text-light")};
+  font-weight: 400;
+  margin-top: ${space(1)};
+`;
---
+++ frontend/src/metabase/nav/containers/MainNavbar/SidebarItems/index.ts
@@ -0,0 +1,1 @@
+export { default as SidebarCollectionLink } from "./SidebarCollectionLink";
