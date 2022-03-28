81b5f8778e0038409e5d15791937289aae48b032
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-28 23:26:02 +0100

    Accept `icon` object as a SidebarLink prop

--- frontend/src/metabase/nav/containers/MainNavbar/SidebarItems/SidebarLink.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/SidebarItems/SidebarLink.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import _ from "underscore";
 
 import { TreeNode } from "metabase/components/tree/TreeNode";
-import Icon from "metabase/components/Icon";
+import Icon, { IconProps } from "metabase/components/Icon";
 
 import { FullWidthLink, NameContainer } from "./SidebarItems.styled";
 
 interface Props {
   children: string;
   url: string;
-  icon: string;
+  icon: string | IconProps;
   isSelected?: boolean;
   right?: React.ReactNode;
 }
@@ -22,12 +22,13 @@ function SidebarLink({
   right = null,
   ...props
 }: Props) {
+  const iconProps = _.isObject(icon) ? icon : { name: icon };
   return (
     <TreeNode.Root depth={0} isSelected={isSelected} {...props}>
       <FullWidthLink to={url}>
-        {icon && (
+        {iconProps && (
           <TreeNode.IconContainer>
-            <Icon name={icon} />
+            <Icon {...iconProps} />
           </TreeNode.IconContainer>
         )}
         <NameContainer>{children}</NameContainer>
