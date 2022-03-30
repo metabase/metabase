91cb2a546beda5a64eab16b31895ac6d9933cb08
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-30 21:18:12 +0100

    Turn SearchBar into functional component

--- frontend/src/metabase/nav/components/SearchBar.jsx
+++ frontend/src/metabase/nav/components/SearchBar.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useEffect, useCallback, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import { t } from "ttag";
 
 import OnClickOutsideWrapper from "metabase/components/OnClickOutsideWrapper";
 
+import { usePrevious } from "metabase/hooks/use-previous";
+import { useToggle } from "metabase/hooks/use-toggle";
 import MetabaseSettings from "metabase/lib/settings";
 
 import { SearchResults } from "./SearchResults";
@@ -19,106 +21,98 @@ import {
 
 const ALLOWED_SEARCH_FOCUS_ELEMENTS = new Set(["BODY", "A"]);
 
-export default class SearchBar extends React.Component {
-  state = {
-    active: false,
-    searchText: "",
-  };
-
-  UNSAFE_componentWillMount() {
-    this._updateSearchTextFromUrl(this.props);
-    window.addEventListener("keyup", this.handleKeyUp);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keyup", this.handleKeyUp);
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (this.props.location.pathname !== nextProps.location.pathname) {
-      this._updateSearchTextFromUrl(nextProps);
-    }
-    // deactivate search on navigation
-    if (this.props.location !== nextProps.location) {
-      this.setInactive();
-    }
-  }
-
-  setActive = () => {
-    this.setState({ active: true });
-  };
-
-  setInactive = () => {
-    this.setState({ active: false });
-  };
-
-  _updateSearchTextFromUrl(props) {
-    const components = props.location.pathname.split("/");
-    if (components[components.length - 1] === "search") {
-      this.setState({ searchText: props.location.query.q });
-    } else {
-      this.setState({ searchText: "" });
-    }
-  }
-
-  onChange = e => {
-    this.setState({ searchText: e.target.value });
-  };
-
-  handleKeyUp = e => {
-    const FORWARD_SLASH_KEY = 191;
-    if (
-      e.keyCode === FORWARD_SLASH_KEY &&
-      ALLOWED_SEARCH_FOCUS_ELEMENTS.has(document.activeElement.tagName)
-    ) {
-      ReactDOM.findDOMNode(this.searchInput).focus();
-      this.setActive();
-    }
-  };
-
-  handleKeyPress = e => {
-    const { searchText } = this.state;
-    const hasSearchQuery =
-      typeof searchText === "string" && searchText.trim().length > 0;
-
-    if (e.key === "Enter" && hasSearchQuery) {
-      this.props.onChangeLocation({
-        pathname: "search",
-        query: { q: searchText.trim() },
-      });
-    }
-  };
-
-  render() {
-    const { active, searchText } = this.state;
-    return (
-      <OnClickOutsideWrapper handleDismissal={this.setInactive}>
-        <SearchWrapper onClick={this.setActive} active={active}>
-          <SearchIcon />
-          <SearchInput
-            value={searchText}
-            placeholder={t`Search` + "…"}
-            maxLength={200}
-            onClick={this.setActive}
-            onChange={this.onChange}
-            onKeyPress={this.handleKeyPress}
-            ref={ref => {
-              this.searchInput = ref;
-            }}
-          />
-          {active && MetabaseSettings.searchTypeaheadEnabled() && (
-            <SearchResultsFloatingContainer>
-              {searchText.trim().length > 0 ? (
-                <SearchResultsContainer>
-                  <SearchResults searchText={searchText.trim()} />
-                </SearchResultsContainer>
-              ) : (
-                <RecentsList />
-              )}
-            </SearchResultsFloatingContainer>
-          )}
-        </SearchWrapper>
-      </OnClickOutsideWrapper>
-    );
-  }
+function isSearchPageLocation(location) {
+  const components = location.pathname.split("/");
+  return components[components.length - 1];
 }
+
+function SearchBar({ location, onChangeLocation }) {
+  const [searchText, setSearchText] = useState(() =>
+    isSearchPageLocation(location) ? location.query.q : "",
+  );
+
+  const [isActive, { turnOn: setActive, turnOff: setInactive }] = useToggle(
+    false,
+  );
+
+  const previousLocation = usePrevious(location);
+  const searchInput = useRef(null);
+
+  const onTextChange = useCallback(e => {
+    setSearchText(e.target.value);
+  }, []);
+
+  useEffect(() => {
+    const FORWARD_SLASH_KEY = 191;
+
+    function focusOnForwardSlashPress(e) {
+      if (
+        e.keyCode === FORWARD_SLASH_KEY &&
+        ALLOWED_SEARCH_FOCUS_ELEMENTS.has(document.activeElement.tagName)
+      ) {
+        ReactDOM.findDOMNode(searchInput.current).focus();
+        setActive();
+      }
+    }
+
+    window.addEventListener("keyup", focusOnForwardSlashPress);
+    return () => window.removeEventListener("keyup", focusOnForwardSlashPress);
+  }, [setActive]);
+
+  useEffect(() => {
+    if (previousLocation?.pathname !== location.pathname) {
+      setSearchText(isSearchPageLocation(location) ? location.query.q : "");
+    }
+  }, [previousLocation, location]);
+
+  useEffect(() => {
+    if (previousLocation !== location) {
+      // deactivate search when page changes
+      setInactive();
+    }
+  }, [previousLocation, location, setInactive]);
+
+  const handleInputKeyPress = useCallback(
+    e => {
+      const hasSearchQuery =
+        typeof searchText === "string" && searchText.trim().length > 0;
+
+      if (e.key === "Enter" && hasSearchQuery) {
+        onChangeLocation({
+          pathname: "search",
+          query: { q: searchText.trim() },
+        });
+      }
+    },
+    [searchText, onChangeLocation],
+  );
+
+  return (
+    <OnClickOutsideWrapper handleDismissal={setInactive}>
+      <SearchWrapper onClick={setActive} active={isActive}>
+        <SearchIcon />
+        <SearchInput
+          value={searchText}
+          placeholder={t`Search` + "…"}
+          maxLength={200}
+          onClick={setActive}
+          onChange={onTextChange}
+          onKeyPress={handleInputKeyPress}
+          ref={searchInput}
+        />
+        {isActive && MetabaseSettings.searchTypeaheadEnabled() && (
+          <SearchResultsFloatingContainer>
+            {searchText.trim().length > 0 ? (
+              <SearchResultsContainer>
+                <SearchResults searchText={searchText.trim()} />
+              </SearchResultsContainer>
+            ) : (
+              <RecentsList />
+            )}
+          </SearchResultsFloatingContainer>
+        )}
+      </SearchWrapper>
+    </OnClickOutsideWrapper>
+  );
+}
+export default SearchBar;
