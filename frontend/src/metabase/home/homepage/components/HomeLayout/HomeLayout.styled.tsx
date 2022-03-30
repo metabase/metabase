15836fca53514825ed2b81ab1be3b5cc44203ef9
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-30 21:32:06 +0300

    Add empty home page

--- frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.styled.tsx
+++ frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.styled.tsx
@@ -6,6 +6,16 @@ export const LayoutRoot = styled.div`
   background-color: ${color("bg-light")};
 `;
 
-export const LayoutBody = styled.div`
-  padding: 4rem 7rem;
+export const LayoutScene = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+`;
+
+export const LayoutSceneImage = styled.img`
+  position: relative;
+  bottom: -3px;
+  mix-blend-mode: multiply;
 `;
--- frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.tsx
+++ frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.tsx
@@ -1,14 +1,15 @@
-import React, { ReactNode } from "react";
-import { LayoutRoot, LayoutBody } from "./HomeLayout.styled";
+import React from "react";
+import { LayoutRoot, LayoutScene, LayoutSceneImage } from "./HomeLayout.styled";
 
-export interface HomeLayoutProps {
-  children?: ReactNode;
-}
-
-const HomeLayout = ({ children }: HomeLayoutProps): JSX.Element => {
+const HomeLayout = (): JSX.Element => {
   return (
     <LayoutRoot>
-      <LayoutBody>{children}</LayoutBody>
+      <LayoutScene>
+        <LayoutSceneImage
+          src="app/img/bridge.png"
+          srcSet="app/img/bridge.png 1x, app/img/bridge@2x.png 2x, app/img/bridge@3x.png 3x"
+        />
+      </LayoutScene>
     </LayoutRoot>
   );
 };
