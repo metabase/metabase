3687025b4fdd5a683c02dad9a2fe3cae23dde01c
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-03-30 21:53:20 +0300

    Rename plugin

--- frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.tsx
+++ frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { LayoutRoot, LayoutScene, LayoutSceneImage } from "./HomeLayout.styled";
 
-const HomeLayout = (): JSX.Element => {
+export interface HomeLayoutProps {
+  showScene?: boolean;
+}
+
+const HomeLayout = ({ showScene }: HomeLayoutProps): JSX.Element => {
   return (
     <LayoutRoot>
-      <LayoutScene>
-        <LayoutSceneImage
-          src="app/img/bridge.png"
-          srcSet="app/img/bridge.png 1x, app/img/bridge@2x.png 2x, app/img/bridge@3x.png 3x"
-        />
-      </LayoutScene>
+      {showScene && (
+        <LayoutScene>
+          <LayoutSceneImage
+            src="app/img/bridge.png"
+            srcSet="app/img/bridge.png 1x, app/img/bridge@2x.png 2x, app/img/bridge@3x.png 3x"
+          />
+        </LayoutScene>
+      )}
     </LayoutRoot>
   );
 };
