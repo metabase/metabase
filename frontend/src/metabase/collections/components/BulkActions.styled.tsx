c96840af460dcc85f776180286676a5429cea550
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-31 22:04:28 +0100

    Remove collection sidebar code

--- frontend/src/metabase/collections/components/BulkActions.styled.tsx
+++ frontend/src/metabase/collections/components/BulkActions.styled.tsx
@@ -2,12 +2,12 @@ import styled from "@emotion/styled";
 import { breakpointMinSmall } from "metabase/styled-components/theme";
 import { color } from "metabase/lib/colors";
 
-import { SIDEBAR_WIDTH } from "metabase/collections/constants";
+import { NAV_SIDEBAR_WIDTH } from "metabase/nav/constants";
 
 export const FixedBottomBar = styled.div<{ isNavbarOpen: boolean }>`
   position: fixed;
   bottom: 0;
-  left: ${props => (props.isNavbarOpen ? SIDEBAR_WIDTH : 0)};
+  left: ${props => (props.isNavbarOpen ? NAV_SIDEBAR_WIDTH : 0)};
   right: 0;
   border-top: 1px solid ${color("border")};
   background-color: ${color("white")};
