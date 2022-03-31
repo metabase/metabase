474bf2ffb610d5becb73c1ee198880655928d3bc
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-03-31 17:39:59 +0200

    Simplify tests

--- frontend/test/metabase/scenarios/dashboard/x-rays.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard/x-rays.cy.spec.js
@@ -26,20 +26,18 @@ describe("scenarios > x-rays", () => {
 
   it("should exist on homepage when person first signs in", () => {
     cy.visit("/");
-    cy.contains("A look at your People table");
+
     cy.contains("A look at your Orders table");
     cy.contains("A look at your Products table");
     cy.contains("A look at your Reviews table");
-  });
 
-  it("should be populated", () => {
-    cy.visit("/");
-    cy.findByText("People table").click();
+    // Let's explore one of our tables
+    cy.contains("A look at your People table").click();
 
-    cy.findByText("Something's gone wrong").should("not.exist");
     cy.findByText("Here's an overview of the people in your People table");
     cy.findByText("Overview");
     cy.findByText("Per state");
+
     cy.get(".Card").should("have.length", 11);
   });
 
@@ -168,8 +166,7 @@ describe("scenarios > x-rays", () => {
   it("should be able to click the title of an x-ray dashcard to see it in the query builder", () => {
     const timeout = { timeout: 10000 };
 
-    cy.visit("/");
-    cy.contains("A look at your Orders table").click();
+    cy.visit(`/auto/dashboard/table/${ORDERS_ID}`);
 
     // confirm results of "Total transactions" card are present
     cy.findByText("18,760", timeout);
