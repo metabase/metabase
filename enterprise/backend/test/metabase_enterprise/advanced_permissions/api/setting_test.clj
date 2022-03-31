6eb8ca7206a4e8408283f391e998ec5d2e31ec85
Ngoc Khuat <qn.khuat@gmail.com>
2022-04-01 00:28:21 +0700

    fix failing test

--- enterprise/backend/test/metabase_enterprise/advanced_permissions/api/setting_test.clj
+++ enterprise/backend/test/metabase_enterprise/advanced_permissions/api/setting_test.clj
@@ -125,7 +125,9 @@
                 (testing (format "set slack setting with %s user" (user->name user))
                   (with-redefs [slack/valid-token? (constantly true)
                                 slack/channel-exists? (constantly true)
-                                slack/refresh-channels-and-usernames! (fn ([]) ([_]))]
+                                slack/slack-token-valid? (constantly true)
+                                slack/refresh-channels-and-usernames! (fn ([]) ([_]))
+                                slack/refresh-channels-and-usernames-when-needed! (fn ([]) ([_]))]
                     (mt/with-temporary-setting-values [slack-app-token nil
                                                        slack-token     "fake-token"]
                       (mt/user-http-request user :put status "slack/settings" {:slack-app-token "fake-token"})))))
