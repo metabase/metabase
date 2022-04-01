b56cb9f6e070d49714cc2adb7509cbb5f941dab4
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-01 10:22:17 +0300

    Add recents

--- frontend/src/metabase/home/homepage/components/HomeContent/HomeContent.tsx
+++ frontend/src/metabase/home/homepage/components/HomeContent/HomeContent.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { isSyncCompleted } from "metabase/lib/syncing";
-import { Database } from "metabase-types/api";
+import { Database, RecentView } from "metabase-types/api";
+import RecentSection from "../../containers/RecentSection";
 import XraySection from "../../containers/XraySection";
 
 export interface HomeContentProps {
   databases: Database[];
+  recents: RecentView[];
 }
 
-const HomeContent = ({ databases }: HomeContentProps): JSX.Element | null => {
-  if (databases.some(isSyncCompleted)) {
+const HomeContent = ({
+  databases,
+  recents,
+}: HomeContentProps): JSX.Element | null => {
+  if (recents.length) {
+    return <RecentSection />;
+  } else if (databases.some(isSyncCompleted)) {
     return <XraySection />;
   } else {
     return null;
---
+++ frontend/src/metabase/home/homepage/components/RecentSection/RecentSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecentSection = (): JSX.Element => {
+  return <div />;
+};
+
+export default RecentSection;
