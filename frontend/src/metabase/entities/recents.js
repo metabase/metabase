df8cea1fe47fa5a52b4e34bac1ba089e62327e22
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-01 10:46:32 +0300

    Add recents

--- frontend/src/metabase/entities/recents.js
+++ frontend/src/metabase/entities/recents.js
@@ -2,6 +2,20 @@ import { createEntity } from "metabase/lib/entities";
 import { entityTypeForObject } from "metabase/lib/schema";
 import { RecentsSchema } from "metabase/schema";
 
+export const getEntity = item => {
+  const entities = require("metabase/entities");
+  return entities[entityTypeForObject(item)];
+};
+
+export const getName = item => {
+  return item.model_object.display_name || item.model_object.name;
+};
+
+export const getIcon = item => {
+  const entity = getEntity(item);
+  return entity.objectSelectors.getIcon(item.model_object);
+};
+
 const Recents = createEntity({
   name: "recents",
   nameOne: "recent",
@@ -18,19 +32,5 @@ const Recents = createEntity({
     getIcon,
   },
 });
-
-export const getEntity = item => {
-  const entities = require("metabase/entities");
-  return entities[entityTypeForObject(item)];
-};
-
-export const getName = item => {
-  return item.model_object.display_name || item.model_object.name;
-};
-
-export const getIcon = item => {
-  const entity = getEntity(item);
-  return entity.objectSelectors.getIcon(item.model_object);
-};
 
 export default Recents;
