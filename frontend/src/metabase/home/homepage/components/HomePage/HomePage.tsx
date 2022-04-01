18c56179b590cabb1fa29356a0dddc80ce82bbb0
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-01 18:29:34 +0300

    Fix e2e tests

--- frontend/src/metabase/home/homepage/components/HomePage/HomePage.tsx
+++ frontend/src/metabase/home/homepage/components/HomePage/HomePage.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeLayout from "../../containers/HomeLayout";
 import HomeContent from "../../containers/HomeContent";
+import { isSmallScreen } from "metabase/lib/dom";
 
-const HomePage = (): JSX.Element => {
+export interface HomePageProps {
+  onOpenNavbar?: () => void;
+}
+
+const HomePage = ({ onOpenNavbar }: HomePageProps): JSX.Element => {
+  useEffect(() => {
+    if (!isSmallScreen()) {
+      onOpenNavbar?.();
+    }
+  }, [onOpenNavbar]);
+
   return (
     <HomeLayout>
       <HomeContent />
