70e2a67d05518f6620fedd73ff287d05070c262e
Noah Moss <noahbmoss@gmail.com>
2022-03-31 23:43:20 -0400

    a bit of refactor

--- enterprise/backend/src/metabase_enterprise/advanced_permissions/query_processor/middleware/permissions.clj
+++ enterprise/backend/src/metabase_enterprise/advanced_permissions/query_processor/middleware/permissions.clj
@@ -1,12 +1,10 @@
 (ns metabase-enterprise.advanced-permissions.query-processor.middleware.permissions
-  (:require [clojure.set :as set]
-            [clojure.string :as str]
+  (:require [clojure.string :as str]
             [metabase.api.common :as api]
             [metabase.models.permissions :as perms]
             [metabase.models.query.permissions :as query-perms]
             [metabase.public-settings.premium-features :as premium-features]
             [metabase.query-processor.error-type :as error-type]
-            [metabase.query-processor.middleware.permissions :as qp.perms]
             [metabase.util.i18n :refer [tru]]))
 
 (def ^:private max-rows-in-limited-downloads 10000)
@@ -16,21 +14,29 @@
   [query]
   (some-> query :info :context name (str/includes? "download")))
 
-(defn- tables->download-perms-set
-  "Given a sequence of `tables-or-ids` referenced by a query, return a set of permissions required to download the
-  results of the query at the level specified by `download-level` (either :full or :limited)."
-  [database-or-id tables-or-ids download-level]
-  (query-perms/tables->permissions-path-set
-   database-or-id
-   tables-or-ids
-   {:table-perms-fn (fn [& path-components] (apply perms/feature-perms-path :download download-level path-components))
-    :native-perms-fn (fn [db-id] (perms/native-feature-perms-path :download download-level db-id))}))
+(defn- table->download-perms-path
+  "Given a table-id referenced by a query, returns the permissions path required to download the results of the query
+  at the level specified by `download-level` (either :full or :limited)."
+  [db-id table-id download-level]
+  (first
+   (query-perms/tables->permissions-path-set
+    db-id
+    #{table-id}
+    {:table-perms-fn (fn [& path-components] (apply perms/feature-perms-path :download download-level path-components))
+     :native-perms-fn (fn [db-id] (perms/native-feature-perms-path :download download-level db-id))})))
 
-(defn- query->source-table-ids
-  [query]
-  ;; Remove the :native key (containing the transpiled MBQL) so that this helper function doesn't think the query is
-  ;; a native query. Actual native queries will still be detected appropriately.
-  (query-perms/query->source-table-ids (dissoc query :native)))
+(defn- table-id->download-perms-level
+  "Given a table-id referenced by a query, returns the level at which the current user can download the data in the
+  table (:full, :limited or :none)."
+  [db-id table-id]
+  (cond (perms/set-has-full-permissions? @api/*current-user-permissions-set* (table->download-perms-path db-id table-id :full))
+        :full
+
+        (perms/set-has-full-permissions? @api/*current-user-permissions-set* (table->download-perms-path db-id table-id :limited))
+        :limited
+
+        :else
+        :none))
 
 (defmulti ^:private current-user-download-perms-level :type)
 
@@ -49,20 +55,12 @@
     :else
     :none))
 
-(defn- table-id->download-perms-level
-  [db-id table-id]
-  (cond (perms/set-has-full-permissions-for-set? @api/*current-user-permissions-set* (tables->download-perms-set db-id #{table-id} :full))
-        :full
-
-        (perms/set-has-full-permissions-for-set? @api/*current-user-permissions-set* (tables->download-perms-set db-id #{table-id} :limited))
-        :limited
-
-        :else
-        :none))
-
 (defmethod current-user-download-perms-level :query
   [{db-id :database, :as query}]
-  (let [table-ids (query->source-table-ids query)]
+  ;; Remove the :native key (containing the transpiled MBQL) so that this helper function doesn't think the query is
+  ;; a native query. Actual native queries are dispatched to a different method by the :type key.
+  (let [table-ids (query-perms/query->source-table-ids (dissoc query :native))]
+    ;; The download perm level for a query should be equal to the lowest perm level of any table referenced by the query.
     (reduce (fn [lowest-seen-perm-level table-id]
               (let [table-perm-level (table-id->download-perms-level db-id table-id)]
                 (cond
