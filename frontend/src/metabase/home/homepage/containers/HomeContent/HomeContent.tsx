1640efd73bd2f349cedc24081345ac091d399f02
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-01 22:02:03 +0300

    Add popular views

--- frontend/src/metabase/home/homepage/containers/HomeContent/HomeContent.tsx
+++ frontend/src/metabase/home/homepage/containers/HomeContent/HomeContent.tsx
@@ -1,9 +1,17 @@
+import { connect } from "react-redux";
 import _ from "underscore";
 import Databases from "metabase/entities/databases";
 import RecentViews from "metabase/entities/recent-views";
+import { getUser } from "metabase/selectors/user";
+import { State } from "metabase-types/store";
 import HomeContent from "../../components/HomeContent";
+
+const mapStateToProps = (state: State) => ({
+  user: getUser(state),
+});
 
 export default _.compose(
   Databases.loadList(),
   RecentViews.loadList({ reload: true }),
+  connect(mapStateToProps),
 )(HomeContent);
