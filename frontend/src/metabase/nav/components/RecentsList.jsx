f62f826788a89e870a3d267a33495f7351684cc5
Anton Kulyk <kuliks.anton@gmail.com>
2022-04-05 15:20:10 +0100

    Mobile friendly search (#21434)
    * Move SearchBarContainer styles to AppBar
    
    * Fix sidebar's border still takes space
    
    * Make search results take full space on mobile
    
    * Minor rename
    
    * Move floating results panel out of input
    
    * Add search clear button for mobile
    
    * Use collapsed search icon for mobile
    
    * Clean up styles
    
    * Add useKeyboardShortcut hook
    
    * Add useOnOutsideClick hook
    
    * Fix search behavior
    
    * Rename `useOnClickOutside`

--- frontend/src/metabase/nav/components/RecentsList.jsx
+++ frontend/src/metabase/nav/components/RecentsList.jsx
@@ -4,7 +4,6 @@ import { t } from "ttag";
 import _ from "underscore";
 
 import RecentViews from "metabase/entities/recent-views";
-import Card from "metabase/components/Card";
 import Text from "metabase/components/type/Text";
 import * as Urls from "metabase/lib/urls";
 import { isSyncCompleted } from "metabase/lib/syncing";
@@ -19,6 +18,7 @@ import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 
 import { getTranslatedEntityName } from "../utils";
 import {
+  Root,
   EmptyStateContainer,
   Header,
   RecentListItemContent,
@@ -51,7 +51,7 @@ function RecentsList({ list, loading }) {
   }
 
   return (
-    <Card py={1}>
+    <Root>
       <Header>{t`Recently viewed`}</Header>
       <LoadingAndErrorWrapper loading={loading} noWrapper>
         <React.Fragment>
@@ -104,7 +104,7 @@ function RecentsList({ list, loading }) {
           )}
         </React.Fragment>
       </LoadingAndErrorWrapper>
-    </Card>
+    </Root>
   );
 }
 
