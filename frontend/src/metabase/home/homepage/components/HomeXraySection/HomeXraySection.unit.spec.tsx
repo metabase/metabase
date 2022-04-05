0da5ab50beae7ae4359967ee1944f5bba08d11c7
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-05 15:38:41 +0400

    Add tests

--- frontend/src/metabase/home/homepage/components/HomeXraySection/HomeXraySection.unit.spec.tsx
+++ frontend/src/metabase/home/homepage/components/HomeXraySection/HomeXraySection.unit.spec.tsx
@@ -29,6 +29,30 @@ describe("HomeXraySection", () => {
     expect(screen.getByText("Orders")).toBeInTheDocument();
     expect(screen.getByText("People")).toBeInTheDocument();
   });
+
+  it("should show x-rays for a user database", () => {
+    const props = getProps({
+      database: createMockDatabase({
+        name: "H2",
+        is_sample: false,
+      }),
+      databaseCandidates: [
+        createMockDatabaseCandidate({
+          tables: [createMockTableCandidate({ title: "Orders" })],
+        }),
+        createMockDatabaseCandidate({
+          tables: [createMockTableCandidate({ title: "People" })],
+        }),
+      ],
+    });
+
+    render(<HomeXraySection {...props} />);
+
+    expect(screen.getByText(/Here are some explorations/)).toBeInTheDocument();
+    expect(screen.getByText("H2")).toBeInTheDocument();
+    expect(screen.getByText("Orders")).toBeInTheDocument();
+    expect(screen.getByText("People")).toBeInTheDocument();
+  });
 });
 
 const getProps = (
