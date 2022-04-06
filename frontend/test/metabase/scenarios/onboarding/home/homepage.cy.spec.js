70c1685d9ff734add358ba63ab7c1411be2b3d3c
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-06 09:24:21 +0400

    Add tests

--- frontend/test/metabase/scenarios/onboarding/home/homepage.cy.spec.js
+++ frontend/test/metabase/scenarios/onboarding/home/homepage.cy.spec.js
@@ -3,14 +3,26 @@ import { restore } from "__support__/e2e/cypress";
 describe("scenarios > home > homepage", () => {
   beforeEach(() => {
     restore("setup");
+    cy.signInAsAdmin();
+
     cy.intercept("GET", "/api/automagic-dashboards/database/**").as("getXrays");
   });
 
   it("should display x-rays for the sample database", () => {
     cy.visit("/");
-    cy.wait("@getXrays");
 
+    cy.wait("@getXrays");
     cy.findByText("Try out these sample x-rays to see what Metabase can do.");
+    cy.findByText("Orders");
+  });
+
+  it("should display x-rays for a user database", () => {
+    cy.addH2SampleDatabase({ name: "H2" });
+    cy.visit("/");
+
+    cy.wait("@getXrays");
+    cy.findByText("Here are some explorations of");
+    cy.findByText("H2");
     cy.findByText("Orders");
   });
 });
