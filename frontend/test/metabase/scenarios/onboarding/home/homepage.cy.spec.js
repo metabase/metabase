266b68f1f971e904d9ed53e2f8df621a3c8600c3
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-06 09:44:58 +0400

    Add tests

--- frontend/test/metabase/scenarios/onboarding/home/homepage.cy.spec.js
+++ frontend/test/metabase/scenarios/onboarding/home/homepage.cy.spec.js
@@ -1,28 +1,35 @@
 import { restore } from "__support__/e2e/cypress";
 
 describe("scenarios > home > homepage", () => {
-  beforeEach(() => {
+  it("should display x-rays for the sample database", () => {
     restore("setup");
     cy.signInAsAdmin();
 
-    cy.intercept("GET", "/api/automagic-dashboards/database/**").as("getXrays");
-  });
-
-  it("should display x-rays for the sample database", () => {
     cy.visit("/");
-
-    cy.wait("@getXrays");
     cy.findByText("Try out these sample x-rays to see what Metabase can do.");
     cy.findByText("Orders");
   });
 
   it("should display x-rays for a user database", () => {
+    restore("setup");
+    cy.signInAsAdmin();
     cy.addH2SampleDatabase({ name: "H2" });
-    cy.visit("/");
 
-    cy.wait("@getXrays");
+    cy.visit("/");
     cy.findByText("Here are some explorations of");
     cy.findByText("H2");
     cy.findByText("Orders");
+  });
+
+  it("should display recent items", () => {
+    restore("default");
+    cy.signInAsAdmin();
+
+    cy.visit("/question/2");
+    cy.findByText("Orders, Count");
+
+    cy.visit("/");
+    cy.findByText("Pick up where you left off");
+    cy.findByText("Orders, Count");
   });
 });
