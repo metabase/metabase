9afe71570e27c027521d894ce3226b4c6143a742
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-04-07 17:40:12 -0400

    fix specs

--- frontend/test/metabase/scenarios/permissions/general-permissions.cy.spec.js
+++ frontend/test/metabase/scenarios/permissions/general-permissions.cy.spec.js
@@ -35,33 +35,12 @@ describeEE("scenarios > admin > permissions > general", () => {
 
       it("revokes ability to create dashboard subscriptions", () => {
         cy.visit("/dashboard/1");
-        cy.icon("subscription")
-          .as("subscriptionsButton")
-          .realHover();
-
-        cy.findByText(
-          "You don't have permission to create a subscription for this dashboard",
-        );
-
-        cy.get("@subscriptionsButton").click();
-        cy.findByText("Create a dashboard subscription").should("not.exist");
+        cy.icon("subscription").should("not.exist");
       });
 
       it("revokes ability to create question alerts", () => {
         cy.visit("/question/1");
-        cy.icon("bell")
-          .as("subscriptionsButton")
-          .realHover();
-        cy.findByText(
-          "You don't have permission to share data from this saved question",
-        );
-
-        cy.findByText(
-          "To send alerts, an admin needs to set up email integration.",
-        ).should("not.exist");
-
-        cy.get("@subscriptionsButton").click();
-        cy.findByText("Create a dashboard subscription").should("not.exist");
+        cy.icon("bell").should("not.exist");
       });
     });
 
