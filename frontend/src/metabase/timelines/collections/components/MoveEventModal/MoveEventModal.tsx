d70a7514dd4d7255f0e6013bd1eebd554887e09c
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-08 16:04:51 +0300

    Reuse ModalBody

--- frontend/src/metabase/timelines/collections/components/MoveEventModal/MoveEventModal.tsx
+++ frontend/src/metabase/timelines/collections/components/MoveEventModal/MoveEventModal.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { t } from "ttag";
 import ModalHeader from "metabase/timelines/common/components/ModalHeader";
+import ModalFooter from "metabase/timelines/common/components/ModalFooter";
 import { Timeline, TimelineEvent } from "metabase-types/api";
+import { ModalRoot, ModalBody } from "./MoveEventModal.styled";
+import Button from "metabase/core/components/Button/Button";
+import TimelinePicker from "metabase/timelines/common/components/TimelinePicker";
 
 export interface MoveEventModalProps {
   event: TimelineEvent;
   timeline: Timeline;
+  timelines: Timeline[];
   onSubmit: (event: TimelineEvent, timeline: Timeline) => void;
   onCancel: () => void;
   onClose?: () => void;
@@ -13,12 +18,33 @@ export interface MoveEventModalProps {
 
 const MoveEventModal = ({
   event,
+  timeline,
+  timelines,
+  onSubmit,
+  onCancel,
   onClose,
 }: MoveEventModalProps): JSX.Element => {
+  const [selection, setSelection] = useState(timeline);
+
+  const handleSubmit = useCallback(() => {
+    onSubmit(event, selection);
+  }, [event, selection, onSubmit]);
+
   return (
-    <div>
+    <ModalRoot>
       <ModalHeader title={t`Move ${event.name}`} onClose={onClose} />
-    </div>
+      <ModalBody>
+        <TimelinePicker
+          value={selection}
+          options={timelines}
+          onChange={setSelection}
+        />
+      </ModalBody>
+      <ModalFooter>
+        <Button onClick={onCancel}>{t`Cancel`}</Button>
+        <Button primary onClick={handleSubmit}>{t`Move`}</Button>
+      </ModalFooter>
+    </ModalRoot>
   );
 };
 
