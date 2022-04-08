ae2557c17f27ba1c7e2120e1195ba758f61266ca
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2022-04-08 13:35:46 -0400

    FE: Settings access global permission + tweaks (#21460)
    * settings global permission
    
    * fix specs
    
    * Enforce Setting permissions (cont) (#21464)
    
    * settings global permission
    
    * more api permissions enforcement
    
    * only admin could call token checks
    
    * address Noah's comments
    
    * clean ns
    
    * clean ns 🤦‍♂️
    
    Co-authored-by: Aleksandr Lesnenko <alxnddr@gmail.com>
    
    * hide subscriptions buttons for users with no permissions
    
    * fix specs
    
    * review fixes
    
    * update spec
    
    Co-authored-by: Ngoc Khuat <qn.khuat@gmail.com>

--- test/metabase/api/ldap_test.clj
+++ test/metabase/api/ldap_test.clj
@@ -6,29 +6,37 @@
             [metabase.test :as mt]
             [metabase.test.integrations.ldap :as ldap.test]))
 
+(defn ldap-test-details
+  []
+  (-> (ldap.test/get-ldap-details)
+      (set/rename-keys (set/map-invert @#'ldap-api/mb-settings->ldap-details))
+      (assoc :ldap-enabled true)))
+
 (deftest ldap-settings-test
   (testing "PUT /api/ldap/settings"
     (ldap.test/with-ldap-server
-      (let [ldap-test-details (-> (ldap.test/get-ldap-details)
-                                  (set/rename-keys (set/map-invert @#'ldap-api/mb-settings->ldap-details))
-                                  (assoc :ldap-enabled true))]
-        (testing "Valid LDAP settings can be saved via an API call"
-          (mt/user-http-request :crowberto :put 200 "ldap/settings" ldap-test-details))
+      (testing "Valid LDAP settings can be saved via an API call"
+        (mt/user-http-request :crowberto :put 200 "ldap/settings" (ldap-test-details)))
 
-        (testing "Invalid LDAP settings return a server error"
-          (mt/user-http-request :crowberto :put 500 "ldap/settings"
-                                (assoc ldap-test-details :ldap-password "wrong-password")))
+      (testing "Invalid LDAP settings return a server error"
+        (mt/user-http-request :crowberto :put 500 "ldap/settings"
+                              (assoc (ldap-test-details) :ldap-password "wrong-password")))
 
-        (testing "Invalid LDAP settings can be saved if LDAP is disabled"
-          (mt/user-http-request :crowberto :put 200 "ldap/settings"
-                                (assoc ldap-test-details :ldap-password "wrong-password" :ldap-enabled false)))
+      (testing "Invalid LDAP settings can be saved if LDAP is disabled"
+        (mt/user-http-request :crowberto :put 200 "ldap/settings"
+                              (assoc (ldap-test-details) :ldap-password "wrong-password" :ldap-enabled false)))
 
-        (testing "Valid LDAP settings can still be saved if port is a integer (#18936)"
-          (mt/user-http-request :crowberto :put 200 "ldap/settings"
-                                (assoc ldap-test-details
-                                       :ldap-port (Integer. (ldap.test/get-ldap-port)))))
+      (testing "Valid LDAP settings can still be saved if port is a integer (#18936)"
+        (mt/user-http-request :crowberto :put 200 "ldap/settings"
+                              (assoc (ldap-test-details)
+                                     :ldap-port (Integer. (ldap.test/get-ldap-port)))))
 
-        (testing "LDAP port is saved as default value if passed as an empty string (#18936)"
-          (mt/user-http-request :crowberto :put 200 "ldap/settings"
-                                (assoc ldap-test-details :ldap-port "" :ldap-enabled false))
-          (is (= 389 (ldap/ldap-port))))))))
+      (testing "LDAP port is saved as default value if passed as an empty string (#18936)"
+        (mt/user-http-request :crowberto :put 200 "ldap/settings"
+                              (assoc (ldap-test-details) :ldap-port "" :ldap-enabled false))
+        (is (= 389 (ldap/ldap-port))))
+
+      (testing "Requires superusers"
+        (is (= "You don't have permissions to do that."
+               (mt/user-http-request :rasta :put 403 "ldap/settings"
+                                     (assoc (ldap-test-details) :ldap-port "" :ldap-enabled false))))))))
