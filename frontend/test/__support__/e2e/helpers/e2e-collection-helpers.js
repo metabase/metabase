e1c77b029850d60bac0fe6f69a651b1f098d4007
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-04-09 23:04:14 +0200

    Remove unused collection helper

--- frontend/test/__support__/e2e/helpers/e2e-collection-helpers.js
+++ frontend/test/__support__/e2e/helpers/e2e-collection-helpers.js
@@ -1,11 +1,5 @@
 import { popover } from "__support__/e2e/cypress";
 
-export function assertCanAddItemsToCollection() {
-  cy.findByTestId("collection-menu").within(() => {
-    cy.icon("add");
-  });
-}
-
 /**
  * Clicks the "+" icon on the collection page and selects one of the menu options
  * @param {"question" | "dashboard" | "collection"} type
