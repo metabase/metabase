17c39156aee8c90e8599f9c35cc937eecd25ffa6
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-13 13:53:53 +0300

    Improve homepage loading speed (#21628)

--- frontend/src/metabase/home/homepage/components/HomeContent/HomeContent.tsx
+++ frontend/src/metabase/home/homepage/components/HomeContent/HomeContent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import moment from "moment";
 import { parseTimestamp } from "metabase/lib/time";
 import { isSyncCompleted } from "metabase/lib/syncing";
+import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 import { Database, PopularItem, RecentItem, User } from "metabase-types/api";
 import HomePopularSection from "../../containers/HomePopularSection";
 import HomeRecentSection from "../../containers/HomeRecentSection";
@@ -9,12 +10,16 @@ import HomeXraySection from "../../containers/HomeXraySection";
 
 export interface HomeContentProps {
   user: User;
-  databases: Database[];
-  recentItems: RecentItem[];
-  popularItems: PopularItem[];
+  databases?: Database[];
+  recentItems?: RecentItem[];
+  popularItems?: PopularItem[];
 }
 
 const HomeContent = (props: HomeContentProps): JSX.Element | null => {
+  if (isLoading(props)) {
+    return <LoadingAndErrorWrapper loading />;
+  }
+
   if (isPopularSection(props)) {
     return <HomePopularSection />;
   }
@@ -30,24 +35,42 @@ const HomeContent = (props: HomeContentProps): JSX.Element | null => {
   return null;
 };
 
-const isPopularSection = ({
+const isLoading = ({
   user,
+  databases,
   recentItems,
   popularItems,
+}: HomeContentProps): boolean => {
+  if (!user.has_question_and_dashboard) {
+    return databases == null;
+  } else if (user.is_installer || !isWithinWeek(user.first_login)) {
+    return databases == null || recentItems == null;
+  } else {
+    return databases == null || recentItems == null || popularItems == null;
+  }
+};
+
+const isPopularSection = ({
+  user,
+  recentItems = [],
+  popularItems = [],
 }: HomeContentProps): boolean => {
   return (
     !user.is_installer &&
     user.has_question_and_dashboard &&
     popularItems.length > 0 &&
-    (isWithinWeek(user.date_joined) || !recentItems.length)
+    (isWithinWeek(user.first_login) || !recentItems.length)
   );
 };
 
-const isRecentSection = ({ user, recentItems }: HomeContentProps): boolean => {
+const isRecentSection = ({
+  user,
+  recentItems = [],
+}: HomeContentProps): boolean => {
   return user.has_question_and_dashboard && recentItems.length > 0;
 };
 
-const isXraySection = ({ databases }: HomeContentProps): boolean => {
+const isXraySection = ({ databases = [] }: HomeContentProps): boolean => {
   return databases.some(isSyncCompleted);
 };
 
