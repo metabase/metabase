f75705945b1ebef68f4915b5e1c2246430064184
Cam Saul <github@camsaul.com>
2022-04-13 16:30:55 -0700

    Use no-op dummy app DB

--- src/metabase/api/testing.clj
+++ src/metabase/api/testing.clj
@@ -7,12 +7,13 @@
             [metabase.api.common :as api]
             [metabase.db.connection :as mdb.connection]
             [metabase.util.files :as files])
-  (:import com.mchange.v2.c3p0.PoolBackedDataSource))
+  (:import com.mchange.v2.c3p0.PoolBackedDataSource
+           metabase.db.connection.ApplicationDB))
 
 ;; EVERYTHING BELOW IS FOR H2 ONLY.
 
-(defn- assert-h2 []
-  (assert (= (:db-type mdb.connection/*application-db*) :h2)
+(defn- assert-h2 [app-db]
+  (assert (= (:db-type app-db) :h2)
           "Snapshot/restore only works for :h2 application databases."))
 
 (defn- snapshot-path-for-name
@@ -24,7 +25,7 @@
 ;;;; SAVE
 
 (defn- save-snapshot! [snapshot-name]
-  (assert-h2)
+  (assert-h2 mdb.connection/*application-db*)
   (let [path (snapshot-path-for-name snapshot-name)]
     (log/infof "Saving snapshot to %s" path)
     (jdbc/query {:datasource mdb.connection/*application-db*} ["SCRIPT TO ?" path]))
@@ -40,51 +41,68 @@
 
 (defn- shut-down-app-db!
   "Shut down the application DB at the H2 level by executing the `SHUTDOWN` statement."
-  []
-  (assert-h2)
+  [^ApplicationDB app-db]
+  (assert-h2 app-db)
   (log/info "Sending H2 application database the SHUTDOWN command")
-  (with-open [conn (.getConnection mdb.connection/*application-db*)
+  (with-open [conn (.getConnection app-db)
               stmt (.createStatement conn)]
     (.execute stmt "SHUTDOWN;")))
 
 (defn- kill-app-db-connection-pool!
   "Immediately destroy all open connections in the app DB connection pool."
-  []
-  (log/info "Destroying application database connection pool")
-  (let [{:keys [data-source]} mdb.connection/*application-db*]
-    (when (instance? PoolBackedDataSource data-source)
-      (.hardReset ^PoolBackedDataSource data-source))))
+  [{:keys [data-source]}]
+  (when (instance? PoolBackedDataSource data-source)
+    (log/info "Destroying application database connection pool")
+    (.hardReset ^PoolBackedDataSource data-source)))
 
 (defn- restore-app-db-from-snapshot!
   "Drop all objects in the application DB, then reload everything from the SQL dump at `snapshot-path`."
-  [^String snapshot-path]
-  (assert-h2)
+  [^ApplicationDB app-db ^String snapshot-path]
+  (assert-h2 app-db)
   (log/infof "Restoring snapshot from %s" snapshot-path)
   (api/check-404 (.exists (java.io.File. snapshot-path)))
-  (with-open [conn (.getConnection mdb.connection/*application-db*)]
+  (with-open [conn (.getConnection app-db)]
     (doseq [sql-args [["SET LOCK_TIMEOUT 180000"]
                       ["DROP ALL OBJECTS"]
                       ["RUNSCRIPT FROM ?" snapshot-path]]]
       (jdbc/execute! {:connection conn} sql-args))))
 
-(defn- increment-app-db-unique-indentifier!
+(defn- increment-app-db-unique-indentifier
   "Increment the [[mdb.connection/unique-identifier]] for the Metabase application DB. This effectively flushes all
   caches using it as a key (including things using [[mdb.connection/memoize-for-application-db]]) such as the Settings
   cache."
-  []
+  [app-db]
   (let [new-id (swap! (var-get #'mdb.connection/application-db-counter) inc)]
     (log/infof "Incrementing application DB unique counter %d -> %d (to flush caches)"
-               (:id mdb.connection/*application-db*)
+               (:id app-db)
                new-id)
-    (alter-var-root #'mdb.connection/*application-db* assoc :id new-id)))
+    (assoc app-db :id new-id)))
+
+(defn- dummy-app-db
+  "Placeholder app DB value of [[mdb.connection/*application-db*]] used during the restore process. Throw an Exception
+  if anyone attempts to use it."
+  []
+  (mdb.connection/application-db
+   :h2
+   (reify javax.sql.DataSource
+     (getConnection [_]
+       (throw (UnsupportedOperationException. "You cannot access the application DB during a snapshot restore!"))))))
 
 (defn- restore-snapshot! [snapshot-name]
-  (assert-h2)
-  (let [path (snapshot-path-for-name snapshot-name)]
-    (shut-down-app-db!)
-    (kill-app-db-connection-pool!)
-    (restore-app-db-from-snapshot! path)
-    (increment-app-db-unique-indentifier!))
+  (assert-h2 mdb.connection/*application-db*)
+  (let [path   (snapshot-path-for-name snapshot-name)
+        app-db mdb.connection/*application-db*]
+    (try
+      (log/info "Temporarily setting *application-db* to no-op dummy app DB")
+      ;; set the app DB to `nil` so nobody else can access it.
+      (alter-var-root #'mdb.connection/*application-db* (constantly (dummy-app-db)))
+      (shut-down-app-db! app-db)
+      (kill-app-db-connection-pool! app-db)
+      (restore-app-db-from-snapshot! app-db path)
+      (finally
+        (let [app-db (increment-app-db-unique-indentifier app-db)]
+          (log/info "Restoring *application-db*")
+          (alter-var-root #'mdb.connection/*application-db* (constantly app-db))))))
   :ok)
 
 (api/defendpoint POST "/restore/:name"
