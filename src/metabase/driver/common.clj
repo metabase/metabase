d13da89336dfb1f3a61ee5e43b308f9ef614e5f9
Cam Saul <1455846+camsaul@users.noreply.github.com>
2022-04-13 09:07:47 -0700

    Fix Snowflake start of week (#21604)

--- src/metabase/driver/common.clj
+++ src/metabase/driver/common.clj
@@ -435,16 +435,17 @@
 (def ^:private ^clojure.lang.PersistentVector days-of-week
   [:monday :tuesday :wednesday :thursday :friday :saturday :sunday])
 
-(defn start-of-week->int
-  "Returns the int value for the current :start-of-week setting value, which ranges from 0 (:monday) to 6 (:sunday).
-  If the :start-of-week setting does not have a value, then `nil` is returned."
+(s/defn start-of-week->int :- (s/pred (fn [n] (and (integer? n) (<= 0 n 6)))
+                                      "Start of week integer")
+  "Returns the int value for the current [[metabase.public-settings/start-of-week]] Setting value, which ranges from
+  `0` (`:monday`) to `6` (`:sunday`). This is guaranteed to return a value."
   {:added "0.42.0"}
   []
-  (when-let [v (setting/get-value-of-type :keyword :start-of-week)]
-    (.indexOf days-of-week v)))
+  (.indexOf days-of-week (setting/get-value-of-type :keyword :start-of-week)))
 
 (s/defn start-of-week-offset :- s/Int
-  "Return the offset for start of week to have the week start on `setting/start-of-week` given  `driver`."
+  "Return the offset for start of week to have the week start on [[metabase.public-settings/start-of-week]] given
+  `driver`."
   [driver]
   (let [db-start-of-week     (.indexOf days-of-week (driver/db-start-of-week driver))
         target-start-of-week (start-of-week->int)
