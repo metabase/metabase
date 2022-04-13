3ce6772b55e59ad0e64e049a2533ff327537f126
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-13 19:23:41 +0300

    Add tests

---
+++ frontend/src/metabase/setup/components/WelcomePage/WelcomePage.unit.spec.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import WelcomePage, { WelcomePageProps } from "./WelcomePage";
+
+describe("WelcomePage", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("should not render until the locale is loaded", () => {
+    const props = getProps({ isLocaleLoaded: false });
+
+    render(<WelcomePage {...props} />);
+
+    expect(screen.queryByText("Welcome to Metabase")).not.toBeInTheDocument();
+  });
+
+  it("should render after some time even if the locale is not loaded", () => {
+    const oldProps = getProps({ isLocaleLoaded: false });
+    const newProps = getProps({ isLocaleLoaded: false });
+
+    const { rerender } = render(<WelcomePage {...oldProps} />);
+    jest.advanceTimersByTime(310);
+    rerender(<WelcomePage {...newProps} />);
+
+    expect(screen.getByText("Welcome to Metabase")).toBeInTheDocument();
+  });
+
+  it("should render before the timeout if the locale is loaded", () => {
+    const oldProps = getProps({ isLocaleLoaded: false });
+    const newProps = getProps({ isLocaleLoaded: true });
+
+    const { rerender } = render(<WelcomePage {...oldProps} />);
+    rerender(<WelcomePage {...newProps} />);
+
+    expect(screen.getByText("Welcome to Metabase")).toBeInTheDocument();
+  });
+});
+
+const getProps = (opts?: Partial<WelcomePageProps>): WelcomePageProps => ({
+  isLocaleLoaded: false,
+  onStepShow: jest.fn(),
+  onStepSubmit: jest.fn(),
+  ...opts,
+});
