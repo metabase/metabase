b6185e2f0b7f5ca7d2cd4635ecd77f67ac3539c5
Noah Moss <noahbmoss@gmail.com>
2022-04-14 13:32:39 -0400

    remove debug code

--- enterprise/backend/src/metabase_enterprise/core.clj
+++ enterprise/backend/src/metabase_enterprise/core.clj
@@ -1,11 +1,2 @@
 (ns metabase-enterprise.core
   "Empty namespace. This is here solely so we can try to require it and see whether or not EE code is on the classpath.")
-
-(metabase.public-settings.premium-features/defenterprise my-sum3
-  "This is my docstring"
-  :feature :embedding
-  :fallback (fn [x y] "fallback!")
-  [x y]
-  (+ x 100))
-
-(my-sum3 100 123)
