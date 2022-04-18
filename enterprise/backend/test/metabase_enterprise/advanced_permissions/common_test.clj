6f26adcae0381f06179e00dbac8c97fb7d58a334
Noah Moss <noahbmoss@gmail.com>
2022-04-18 15:44:28 -0400

    fix test helper

--- enterprise/backend/test/metabase_enterprise/advanced_permissions/common_test.clj
+++ enterprise/backend/test/metabase_enterprise/advanced_permissions/common_test.clj
@@ -22,7 +22,7 @@
           (@#'perms/update-group-permissions! all-users-group-id graph)
           (f))
         (finally
-          (@#'perms/update-group-permissions! all-users-group-id graph))))))
+          (@#'perms/update-group-permissions! all-users-group-id current-graph))))))
 
 (defmacro ^:private with-all-users-data-perms
   "Runs `f` with perms for the All Users group temporarily set to the values in `graph`. Also enables the advanced
