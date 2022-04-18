1e965e8391e953c10134fd9124b60d5c57a86c06
Gustavo Saiani <gustavo@poe.ma>
2022-04-18 14:24:50 +0200

    Reuse styles

--- frontend/src/metabase/query_builder/components/QuestionActionButtons.styled.jsx
+++ frontend/src/metabase/query_builder/components/QuestionActionButtons.styled.jsx
@@ -1,4 +1,27 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+import Button from "metabase/core/components/Button";
+import {
+  shrinkOrExpandOnClick,
+  shrinkOrExpandDuration,
+} from "metabase/styled-components/theme/button.ts";
+
+export const BookmarkButton = styled(Button)`
+  ${shrinkOrExpandOnClick}
+
+  ${props =>
+    props.animation === "expand" &&
+    css`
+      animation: expand linear ${shrinkOrExpandDuration};
+    `}
+
+  ${props =>
+    props.animation === "shrink" &&
+    css`
+      animation: shrink linear ${shrinkOrExpandDuration};
+    `}
+`;
 
 export const Container = styled.div`
   display: flex;
