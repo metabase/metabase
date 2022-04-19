13e9fab3bb213097c09b5df01969b0ec5302dc36
Anton Kulyk <kuliks.anton@gmail.com>
2022-04-19 14:16:03 +0100

    Add `app` to `State` type

--- frontend/src/metabase-types/store/state.ts
+++ frontend/src/metabase-types/store/state.ts
@@ -1,5 +1,6 @@
 import { User } from "metabase-types/api";
 import { AdminState } from "./admin";
+import { AppState } from "./app";
 import { EntitiesState } from "./entities";
 import { FormState } from "./forms";
 import { SettingsState } from "./settings";
@@ -7,6 +8,7 @@ import { SetupState } from "./setup";
 
 export interface State {
   admin: AdminState;
+  app: AppState;
   currentUser: User;
   entities: EntitiesState;
   form: FormState;
