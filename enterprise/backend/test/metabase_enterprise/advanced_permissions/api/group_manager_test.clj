b5545bd30f750f12909cc1334bd02c0acb44d8ee
Ngoc Khuat <qn.khuat@gmail.com>
2022-04-21 12:43:35 +0700

    fix ns

--- enterprise/backend/test/metabase_enterprise/advanced_permissions/api/group_manager_test.clj
+++ enterprise/backend/test/metabase_enterprise/advanced_permissions/api/group_manager_test.clj
@@ -1,11 +1,11 @@
 (ns metabase-enterprise.advanced-permissions.api.group-manager-test
   "Permisisons tests for API that needs to be enforced by Group Manager permisisons."
-  (:require [clojure.test :refer :all]
-            [clojure.set :refer [subset?]]
+  (:require [clojure.set :refer [subset?]]
+            [clojure.test :refer :all]
             [metabase-enterprise.advanced-permissions.models.permissions.group-manager :as gm]
             [metabase.models :refer [PermissionsGroup PermissionsGroupMembership User]]
-            [metabase.models.user :as user]
             [metabase.models.permissions-group :as perms-group]
+            [metabase.models.user :as user]
             [metabase.public-settings.premium-features-test :as premium-features-test]
             [metabase.test :as mt]
             [metabase.util :as u]
