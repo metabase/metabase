4b3eda62605cb9d8dc3d668192dd774098a84fcf
Cam Saul <github@camsaul.com>
2022-04-21 15:59:11 -0700

    rx util support varargs inside `opt`

--- test/metabase/util/regex_test.clj
+++ test/metabase/util/regex_test.clj
@@ -6,4 +6,6 @@
   (let [regex (u.regex/rx (and "^" (or "Cam" "can") (opt #"\s+") #"\d+"))]
     (is (instance? java.util.regex.Pattern regex))
     (is (= (str #"^(?:(?:Cam)|(?:can))(?:\s+)?\d+")
-           (str regex)))))
+           (str regex)))
+    (is (= (str #"^2022-(?:(?:06-30)|(?:07-01))(?:(?:(?:T)|(?:\s))00:00:00(?:Z)?)?")
+           (str (u.regex/rx #"^2022-" (or "06-30" "07-01") (opt (or "T" #"\s") "00:00:00" (opt "Z"))))))))
