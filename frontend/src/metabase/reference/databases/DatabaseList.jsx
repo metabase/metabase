c26a4d5e4a0c5724313b14d0220289f299ac0320
Dalton <daltojohnso@users.noreply.github.com>
2022-04-21 09:57:53 -0700

    Filter out the is_saved_questions db (#21840)

--- frontend/src/metabase/reference/databases/DatabaseList.jsx
+++ frontend/src/metabase/reference/databases/DatabaseList.jsx
@@ -40,6 +40,16 @@ export default class DatabaseList extends Component {
   render() {
     const { entities, style, loadingError, loading } = this.props;
 
+    const databases = Object.values(entities)
+      .filter(database => {
+        const exists = Boolean(database?.id && database?.name);
+        return exists && !database.is_saved_questions;
+      })
+      .sort((a, b) => {
+        const compared = a.name.localeCompare(b.name);
+        return compared !== 0 ? compared : a.engine.localeCompare(b.engine);
+      });
+
     return (
       <div style={style} className="full">
         <ReferenceHeader name={t`Our data`} />
@@ -51,30 +61,18 @@ export default class DatabaseList extends Component {
             Object.keys(entities).length > 0 ? (
               <div className="wrapper">
                 <List>
-                  {Object.values(entities)
-                    .sort((a, b) => {
-                      const compared = a.name.localeCompare(b.name);
-                      return compared !== 0
-                        ? compared
-                        : a.engine.localeCompare(b.engine);
-                    })
-                    .map(
-                      (entity, index) =>
-                        entity &&
-                        entity.id &&
-                        entity.name && (
-                          <li className="relative" key={entity.id}>
-                            <ListItem
-                              id={entity.id}
-                              index={index}
-                              name={entity.display_name || entity.name}
-                              description={entity.description}
-                              url={`/reference/databases/${entity.id}`}
-                              icon="database"
-                            />
-                          </li>
-                        ),
-                    )}
+                  {databases.map((database, index) => (
+                    <li className="relative" key={database.id}>
+                      <ListItem
+                        id={database.id}
+                        index={index}
+                        name={database.display_name || database.name}
+                        description={database.description}
+                        url={`/reference/databases/${database.id}`}
+                        icon="database"
+                      />
+                    </li>
+                  ))}
                 </List>
               </div>
             ) : (
