58bdc382eda7c0dba4fbc9fa787d325a517f5d61
Bryan Maass <bryan.maass@gmail.com>
2022-04-22 12:37:40 -0600

    fix test wording

--- test/metabase/cmd_test.clj
+++ test/metabase/cmd_test.clj
@@ -5,7 +5,7 @@
 (deftest error-message-test
   (is (= ["No command given."] (#'cmd/cmd->fn nil [])))
   (is (= ["Unrecognized command: 'a-command-that-does-not-exist'"] (#'cmd/cmd->fn "a-command-that-does-not-exist" [])))
-  (is (= ["The 'rotate-encryption-key' command requires the following set of arguments: [new-key], but received: []."]
+  (is (= ["The 'rotate-encryption-key' command requires the following arguments: [new-key], but received: []."]
          (#'cmd/cmd->fn "rotate-encryption-key" [])))
   (let [[error? the-fxn] (#'cmd/cmd->fn "rotate-encryption-key" [:some-arg])]
     (is (nil? error?))
