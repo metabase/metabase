fbf3beb1493c33470f799448aa35057b99e9fcb3
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-23 17:25:29 +0300

    Fix auth and home backgrounds

--- frontend/src/metabase/auth/components/AuthLayout/AuthLayout.styled.tsx
+++ frontend/src/metabase/auth/components/AuthLayout/AuthLayout.styled.tsx
@@ -9,18 +9,15 @@ export interface LayoutProps {
 const sceneStyles = css`
   background-color: ${color("bg-light")};
   background-image: url("app/img/bridge.svg");
-  background-size: max(1728px, 150%) auto;
+  background-size: max(2592px, 100%) auto;
   background-repeat: no-repeat;
   background-position: right bottom;
 `;
 
-const colorStyles = css`
-  background-color: ${color("bg-light")};
-`;
-
 export const LayoutRoot = styled.div<LayoutProps>`
   min-height: 100vh;
-  ${props => (props.showScene ? sceneStyles : colorStyles)};
+  background-color: ${color("bg-light")};
+  ${props => props.showScene && sceneStyles};
 `;
 
 export const LayoutBody = styled.div`
