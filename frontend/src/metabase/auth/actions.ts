2fe5330a295e240847ac9e6abfcaa48b01782e15
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-25 16:51:05 +0300

    Clear browser caches when logging out (#22001)

--- frontend/src/metabase/auth/actions.ts
+++ frontend/src/metabase/auth/actions.ts
@@ -78,6 +78,7 @@ export const logout = createThunkAction(LOGOUT, () => {
     trackLogout();
 
     dispatch(push("/auth/login"));
+    window.location.reload(); // clears redux state and browser caches
   };
 });
 
