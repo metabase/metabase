cdd728a198f6594a9231c5412c159bb522b4c56d
losrebellos <ben@benoitvinay.com>
2022-04-26 12:31:41 +0200

    Stickiness cypress tests WIP

--- frontend/test/metabase-visual/dashboard/parameters-widget.cy.spec.js
+++ frontend/test/metabase-visual/dashboard/parameters-widget.cy.spec.js
@@ -1,5 +1,6 @@
 import { restore, visitDashboard } from "__support__/e2e/cypress";
 import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
+import { MAXIMUM_PARAMETERS_FOR_STICKINESS } from "metabase/dashboard/components/Dashboard/stickyParameters";
 
 const { PRODUCTS_ID } = SAMPLE_DATABASE;
 
@@ -14,50 +15,79 @@ const filter = {
   type: "category",
 };
 
-const filters = new Array(12).fill(filter);
+const filters = new Array(MAXIMUM_PARAMETERS_FOR_STICKINESS + 1).fill(filter);
 
 describe("visual tests > dashboard > parameters widget", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
 
-    cy.createQuestionAndDashboard({ questionDetails }).then(
-      ({ body: card }) => {
-        const { dashboard_id } = card;
+    cy.createQuestionAndDashboard({
+      questionDetails,
+    }).then(({ body: card }) => {
+      const { dashboard_id } = card;
 
-        cy.request("PUT", `/api/dashboard/${dashboard_id}`, {
-          parameters: filters,
-        });
-
-        const updatedSize = {
-          sizeX: 12,
-          sizeY: 32,
-        };
-
-        cy.editDashboardCard(card, updatedSize);
-
-        visitDashboard(dashboard_id);
-      },
-    );
-  });
-
-  it("is sticky in view mode", () => {
-    cy.findByText("test question");
-
-    cy.get("main").scrollTo(0, 264);
-
-    cy.percySnapshot();
-  });
-
-  it("is sticky in edit mode", () => {
-    cy.findByText("test question");
-
-    cy.icon("pencil").click();
-
-    cy.findByTestId("dashboard-parameters-and-cards")
-      .scrollTo(0, 464)
-      .then(() => {
-        cy.percySnapshot();
+      cy.request("PUT", `/api/dashboard/${dashboard_id}`, {
+        parameters: filters,
       });
+
+      const updatedSize = {
+        sizeX: 12,
+        sizeY: 32,
+      };
+
+      cy.editDashboardCard(card, updatedSize);
+
+      visitDashboard(dashboard_id);
+    });
+  });
+
+  // desktop
+  describe("Stickiness on desktop", () => {
+    it("is sticky in view mode", () => {
+      cy.findByText("test question");
+
+      cy.get("main").scrollTo(0, 264);
+
+      cy.percySnapshot();
+    });
+
+    it("is sticky in edit mode", () => {
+      cy.findByText("test question");
+
+      cy.icon("pencil").click();
+
+      cy.findByTestId("dashboard-parameters-and-cards")
+        .scrollTo(0, 464)
+        .then(() => {
+          cy.percySnapshot();
+        });
+    });
+  });
+
+  // mobile
+  describe("Stickiness on mobile", () => {
+    // resize window to mobile form factor
+    cy.viewport(480, 800);
+
+    it("is not sticky in view mode", () => {
+      cy.findByText("test question");
+
+      cy.get("main").scrollTo(0, 264);
+
+      cy.percySnapshot();
+    });
+
+    it("is not sticky in edit mode", () => {
+      cy.findByText("test question");
+
+      cy.icon("pencil").click();
+
+      cy.findByTestId("dashboard-parameters-and-cards")
+        .scrollTo(0, 464)
+        .then(() => {
+          cy.percySnapshot();
+        });
+    });
   });
 });
