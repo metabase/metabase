4a7e05c3c8a14265fe526cc9cd5e5c47cfcee436
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-27 18:47:50 +0300

    Revert the fix

--- frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetail.tsx
+++ frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetail.tsx
@@ -156,7 +156,8 @@ export function ObjectDetailFn({
 
   useEffect(() => {
     // if the card changed or table metadata loaded then reload fk references
-    if (tableForeignKeys && (data !== prevData || !prevTableForeignKeys)) {
+    const tableFKsJustLoaded = !prevTableForeignKeys && tableForeignKeys;
+    if (data !== prevData || tableFKsJustLoaded) {
       loadObjectDetailFKReferences();
     }
   }, [
