b66f75812bf78587267f2d1357868435487a0e41
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-04-28 11:31:53 -0700

    Remove value-label-outline text

--- frontend/src/metabase/visualizations/lib/chart_values.js
+++ frontend/src/metabase/visualizations/lib/chart_values.js
@@ -283,11 +283,7 @@ export function onRenderValueLabels(
         return transforms.join(" ");
       });
 
-    // Labels are either white inside the bar or black with white outline.
-    // For outlined labels, Safari had an issue with rendering paint-order: stroke.
-    // To work around that, we create two text labels: one for the the black text
-    // and another for the white outline behind it.
-    ["value-label-outline", "value-label", "value-label-white"].forEach(klass =>
+    ["value-label", "value-label-white"].forEach(klass =>
       labelGroups
         .append("text")
         // only create labels for the correct class(es) given the type of label
@@ -320,7 +316,7 @@ export function onRenderValueLabels(
     addLabels(sample, compactForSeries[index]);
     const totalWidth = chart
       .svg()
-      .selectAll(".value-label-outline")
+      .selectAll(".value-label, .value-label-white")
       .flat()
       .reduce((sum, label) => sum + label.getBoundingClientRect().width, 0);
     const labelWidth = totalWidth / sample.length + LABEL_PADDING;
