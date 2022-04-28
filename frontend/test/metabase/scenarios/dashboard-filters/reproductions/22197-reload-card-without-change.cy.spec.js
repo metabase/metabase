a2b25d9fc84d85cf9a92b73fdc1467bacfaea472
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-28 20:52:12 +0300

    Add cy test

--- frontend/test/metabase/scenarios/dashboard-filters/reproductions/22197-reload-card-without-change.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard-filters/reproductions/22197-reload-card-without-change.cy.spec.js
@@ -23,78 +23,92 @@ const question2Details = {
 
 const dashboardDetails = {
   name: "Filters",
+  parameters: [filterDetails],
 };
 
 describe("issue 22197", () => {
   beforeEach(() => {
     restore();
+    cy.signInAsAdmin();
   });
 
   it("should not reload cards in a dashboard when corresponding filters are not changed (metabase#22197)", () => {
-    createDashboard().then(({ dashboard_id }) => {
-      cy.visit(`/dashboard/${dashboard_id}`);
-      cy.wait("@getDashboard");
-      cy.wait("@getCardQuery1");
-      cy.wait("@getCardQuery2");
-    });
+    createQuestionsAndDashboard().then(
+      ({ dashboard_id, card1_id, card2_id }) => {
+        setFilterMapping({ dashboard_id, card1_id, card2_id });
+        interceptRequests({ dashboard_id, card1_id, card2_id });
+
+        cy.visit(`/dashboard/${dashboard_id}`);
+        cy.wait("@getDashboard");
+        cy.wait("@getCardQuery1");
+        cy.wait("@getCardQuery2");
+
+        // the filter is connected only to the first card
+        cy.findByPlaceholderText(filterDetails.name).type("1{enter}");
+        cy.wait("@getCardQuery1");
+        cy.get("@getCardQuery1.all").should("have.length", 2);
+        cy.get("@getCardQuery2.all").should("have.length", 1);
+      },
+    );
   });
 });
 
-const createDashboard = () => {
+const createQuestionsAndDashboard = () => {
   return cy
-    .createDashboard(dashboardDetails)
-    .then(({ body: { id: dashboard_id } }) => {
-      cy.createQuestion(question1Details).then(({ body: { id: card1_id } }) => {
-        cy.createQuestion(question2Details).then(
-          ({ body: { id: card2_id } }) => {
-            cy.addFilterToDashboard({
-              filter: filterDetails,
-              dashboard_id,
+    .createQuestion(question1Details)
+    .then(({ body: { id: card1_id } }) => {
+      return cy
+        .createQuestion(question2Details)
+        .then(({ body: { id: card2_id } }) => {
+          return cy
+            .createDashboard(dashboardDetails)
+            .then(({ body: { id: dashboard_id } }) => {
+              return { dashboard_id, card1_id, card2_id };
             });
-
-            cy.request("POST", `/api/dashboard/${dashboard_id}/cards`, {
-              cardId: card1_id,
-              sizeX: 8,
-              sizeY: 6,
-              parameter_mappings: [
-                {
-                  parameter_id: filterDetails.id,
-                  card_id: card1_id,
-                  target: ["dimension", ["field", PRODUCTS.ID, null]],
-                },
-              ],
-            });
-
-            cy.request("POST", `/api/dashboard/${dashboard_id}/cards`, {
-              cardId: card2_id,
-              sizeX: 8,
-              sizeY: 6,
-              parameter_mappings: [
-                {
-                  parameter_id: filterDetails.id,
-                  card_id: card1_id,
-                  target: ["dimension", ["field", ORDERS.ID, null]],
-                },
-              ],
-            });
-
-            cy.intercept("GET", `/api/dashboard/${dashboard_id}`).as(
-              "getDashboard",
-            );
-
-            cy.intercept(
-              "POST",
-              `/api/dashboard/${dashboard_id}/dashcard/*/card/${card1_id}/query`,
-            ).as("getCardQuery1");
-
-            cy.intercept(
-              "POST",
-              `/api/dashboard/${dashboard_id}/dashcard/*/card/${card2_id}/query`,
-            ).as("getCardQuery2");
-
-            return { dashboard_id };
-          },
-        );
-      });
+        });
     });
+};
+
+const setFilterMapping = ({ dashboard_id, card1_id, card2_id }) => {
+  cy.request("POST", `/api/dashboard/${dashboard_id}/cards`, {
+    cardId: card1_id,
+    row: 0,
+    col: 0,
+    sizeX: 4,
+    sizeY: 4,
+    parameter_mappings: [
+      {
+        parameter_id: filterDetails.id,
+        card_id: card1_id,
+        target: ["dimension", ["field", PRODUCTS.ID, null]],
+      },
+    ],
+  });
+
+  cy.request("POST", `/api/dashboard/${dashboard_id}/cards`, {
+    cardId: card2_id,
+    row: 0,
+    col: 4,
+    sizeX: 4,
+    sizeY: 4,
+    parameter_mappings: [
+      {
+        parameter_id: filterDetails.id,
+        card_id: card1_id,
+        target: ["dimension", ["field", ORDERS.ID, null]],
+      },
+    ],
+  });
+};
+
+const interceptRequests = ({ dashboard_id, card1_id, card2_id }) => {
+  cy.intercept("GET", `/api/dashboard/${dashboard_id}`).as("getDashboard");
+  cy.intercept(
+    "POST",
+    `/api/dashboard/${dashboard_id}/dashcard/*/card/${card1_id}/query`,
+  ).as("getCardQuery1");
+  cy.intercept(
+    "POST",
+    `/api/dashboard/${dashboard_id}/dashcard/*/card/${card2_id}/query`,
+  ).as("getCardQuery2");
 };
