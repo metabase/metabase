3bffb4f524ee77db97b8ef5f071a1b1263aa5459
Anton Kulyk <kuliks.anton@gmail.com>
2022-04-29 12:38:57 +0100

    Fix dashboard PK drill on filtered data (#22190)
    * Add E2E test
    
    * Exclude undefined query params from question URLs
    
    * Allow passing `objectId` to `getUrlWithParameters`
    
    * Accept PK in `navigateToNewCardFromDashboard`
    
    * Add `extra` viz click action param
    
    * New implementation for dashboard PK drill
    
    * Pass objectId from drill to dashboard nav action
    
    * Fix intercepted route
    
    * Fix test name
    
    * Use URLs for PK dash drill without filters

--- frontend/test/metabase/modes/components/drill/ObjectDetailDrill.unit.spec.js
+++ frontend/test/metabase/modes/components/drill/ObjectDetailDrill.unit.spec.js
@@ -126,25 +126,59 @@ describe("ObjectDetailDrill", () => {
     });
 
     describe("from dashboard", () => {
+      describe("without parameters", () => {
+        const { actions, cellValue } = setup({
+          question: SAVED_QUESTION,
+          column: ORDERS.ID.column(),
+          extraData: {
+            dashboard: { id: 5 },
+          },
+        });
+
+        it("should return object detail filter", () => {
+          expect(actions).toMatchObject([
+            {
+              name: "object-detail",
+              url: expect.any(Function),
+            },
+          ]);
+        });
+
+        it("should return correct URL to object detail", () => {
+          const [action] = actions;
+          expect(action.url()).toBe(
+            `/question/${SAVED_QUESTION.id()}-${SAVED_QUESTION.displayName()}/${cellValue}`,
+          );
+        });
+      });
+    });
+
+    describe("with parameters", () => {
       const { actions, cellValue } = setup({
         question: SAVED_QUESTION,
         column: ORDERS.ID.column(),
         extraData: {
           dashboard: { id: 5 },
+          parameterValuesBySlug: {
+            foo: "bar",
+          },
         },
       });
 
       it("should return object detail filter", () => {
         expect(actions).toMatchObject([
-          { name: "object-detail", url: expect.any(Function) },
+          {
+            name: "object-detail",
+            question: expect.any(Function),
+            extra: expect.any(Function),
+          },
         ]);
       });
 
-      it("should return correct URL to object detail", () => {
+      it("should return correct action", () => {
         const [action] = actions;
-        expect(action.url()).toBe(
-          `/question/${SAVED_QUESTION.id()}-${SAVED_QUESTION.displayName()}/${cellValue}`,
-        );
+        expect(action.question()).toBe(SAVED_QUESTION);
+        expect(action.extra()).toEqual({ objectId: cellValue });
       });
     });
   });
