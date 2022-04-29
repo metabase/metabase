61bec6b708458f0486b9af1b093543a73b56e509
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-04-29 17:45:12 +0300

    Add a test

--- frontend/test/metabase/scenarios/visualizations/reproductions/11249-add-more-series-no-columns.cy.spec.js
+++ frontend/test/metabase/scenarios/visualizations/reproductions/11249-add-more-series-no-columns.cy.spec.js
@@ -1,1 +1,32 @@
-describe("issue 11249", () => {});
+import { restore, visitQuestionAdhoc } from "__support__/e2e/cypress";
+import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
+
+const { ORDERS, ORDERS_ID } = SAMPLE_DATABASE;
+
+const questionDetails = {
+  name: "13960",
+  display: "line",
+  dataset_query: {
+    type: "query",
+    database: 1,
+    query: {
+      "source-table": ORDERS_ID,
+      aggregation: [["count"], ["avg", ["field", ORDERS.TOTAL, null]]],
+      breakout: [["field", ORDERS.CREATED_AT, { "temporal-unit": "month" }]],
+    },
+  },
+  visualization_settings: {
+    "graph.dimensions": ["CREATED_AT"],
+    "graph.metrics": ["avg"],
+  },
+};
+
+describe("issue 11249", () => {
+  beforeEach(() => {
+    restore();
+  });
+
+  it("should not allow adding more series when all columns are used (metabase#11249)", () => {
+    visitQuestionAdhoc(questionDetails);
+  });
+});
