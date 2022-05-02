744d340a62f3f1fa6963d2d985d78af302941618
Noah Moss <noahbmoss@gmail.com>
2022-05-02 18:31:26 -0400

    overrides for certain :unit values

--- src/metabase/query_processor/streaming/xlsx.clj
+++ src/metabase/query_processor/streaming/xlsx.clj
@@ -3,6 +3,7 @@
             [clojure.string :as str]
             [dk.ative.docjure.spreadsheet :as spreadsheet]
             [java-time :as t]
+            [metabase.mbql.schema :as mbql.s]
             [metabase.public-settings :as public-settings]
             [metabase.query-processor.streaming.common :as common]
             [metabase.query-processor.streaming.interface :as qp.si]
@@ -156,55 +157,78 @@
   (let [separator (::mb.viz/date-separator format-settings "/")]
     (str/replace format-string "/" separator)))
 
-(defn- add-time-format
-  [format-settings format-string]
+(defn- time-format
+  [format-settings]
   (let [base-time-format (condp = (::mb.viz/time-enabled format-settings "minutes")
-                           "minutes"
-                           "h:mm"
+                               "minutes"
+                               "h:mm"
 
-                           "seconds"
-                           "h:mm:ss"
+                               "seconds"
+                               "h:mm:ss"
 
-                           "milliseconds"
-                           "h:mm:ss.000"
+                               "milliseconds"
+                               "h:mm:ss.000"
 
-                           ;; {::mb.viz/time-enabled nil} indicates that time is explicitly disabled, rather than
-                           ;; defaulting to "minutes"
-                           nil
-                           nil)
-        time-format      (when base-time-format
-                           (condp = (::mb.viz/time-style format-settings "h:mm A")
-                             "HH:mm"
-                             (str "h" base-time-format)
+                               ;; {::mb.viz/time-enabled nil} indicates that time is explicitly disabled, rather than
+                               ;; defaulting to "minutes"
+                               nil
+                               nil)]
+    (when base-time-format
+      (condp = (::mb.viz/time-style format-settings "h:mm A")
+        "HH:mm"
+        (str "h" base-time-format)
 
-                             ;; Deprecated time style which should be already converted to HH:mm when viz settings are
-                             ;; normalized, but we'll handle it here too just in case. (#18112)
-                             "k:mm"
-                             (str "h" base-time-format)
+        ;; Deprecated time style which should be already converted to HH:mm when viz settings are
+        ;; normalized, but we'll handle it here too just in case. (#18112)
+        "k:mm"
+        (str "h" base-time-format)
 
-                             "h:mm A"
-                             (str base-time-format " am/pm")
+        "h:mm A"
+        (str base-time-format " am/pm")
 
-                             "h A"
-                             "h am/pm"))]
-    (if time-format
+        "h A"
+        "h am/pm"))))
+
+(defn- add-time-format
+  "Adds the appropriate time setting to a date format string if necessary, producing a datetime format string."
+  [format-settings unit format-string]
+  (if (or (not unit) (mbql.s/time-bucketing-units unit))
+    (if-let [time-format (time-format format-settings)]
       (str format-string ", " time-format)
-      format-string)))
+      format-string)
+    format-string))
+
+(def ^:private month-style-overrides
+  "Overrides for the date-style in a column's viz settings if :unit is :month."
+  {"M/D/YYYY"     "m/yyyy"
+   "YYYY/M/D"     "yyyy/m"
+   "MMMM D, YYYY" "mmmm, yyyy"})
+
+(defn- date-format
+  [format-settings unit]
+  (let [base-style (::mb.viz/date-style format-settings "mmmm d, yyyy")
+        unit-style (case unit
+                     :month (get month-style-overrides base-style)
+                     :year "yyyy"
+                     base-style)]
+    (->> unit-style
+         str/lower-case
+         (abbreviate-date-names format-settings)
+         (replace-date-separators format-settings))))
 
 (defn- datetime-format-string
-  [format-settings]
-  (let [merged-settings (merge-global-settings format-settings :type/Temporal)
-        date-style      (::mb.viz/date-style merged-settings "mmmm d, yyyy")]
-    (->> date-style
-         str/lower-case
-         (abbreviate-date-names merged-settings)
-         (replace-date-separators merged-settings)
-         (add-time-format merged-settings))))
+  ([format-settings]
+   (datetime-format-string format-settings nil))
+
+  ([format-settings unit]
+   (let [merged-settings (merge-global-settings format-settings :type/Temporal)]
+     (->> (date-format merged-settings unit)
+          (add-time-format merged-settings unit)))))
 
 (defn- format-settings->format-strings
   "Returns a vector of format strings for a datetime column or number column, corresponding
   to the provided format settings."
-  [format-settings {semantic-type :semantic_type, effective-type :effective_type, :as _col}]
+  [format-settings {semantic-type :semantic_type, effective-type :effective_type, unit :unit}]
   (u/one-or-many
    (cond
      ;; Primary key or foreign key
@@ -214,7 +238,7 @@
      (and (or (some #(contains? datetime-setting-keys %) (keys format-settings))
               (isa? semantic-type :type/Temporal))
           (isa? effective-type :type/Temporal))
-     (datetime-format-string format-settings)
+     (datetime-format-string format-settings unit)
 
      (or (some #(contains? number-setting-keys %) (keys format-settings))
          (isa? semantic-type :type/Currency))
@@ -223,8 +247,8 @@
 (defn- default-format-strings
   "Default strings to use for datetime and number fields if custom format settings are not set."
   []
-  {:datetime (datetime-format-string (merge-global-settings {} :type/Temporal))
-   :date     (datetime-format-string (merge-global-settings {::mb.viz/time-enabled nil} :type/Temporal))
+  {:datetime "mmmm d, yyyy" (datetime-format-string (merge-global-settings {} :type/Temporal))
+   :date     "mmmm d, yyyy" (datetime-format-string (merge-global-settings {::mb.viz/time-enabled nil} :type/Temporal))
    ;; Use a fixed format for time fields since time formatting isn't currently supported (#17357)
    :time     "h:mm am/pm"
    :integer  "#,##0"
