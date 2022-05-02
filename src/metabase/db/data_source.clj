d292b145fdd313c2c5d1a89926e63d897772d1bb
Braden Shepherdson <braden@metabase.com>
2022-05-02 11:29:24 -0400

    Migration progress

--- src/metabase/db/data_source.clj
+++ src/metabase/db/data_source.clj
@@ -1,5 +1,7 @@
 (ns metabase.db.data-source
-  (:require [clojure.set :as set]
+  (:require [clojure.java.io :as io]
+            [clojure.java.jdbc :as jdbc]
+            [clojure.set :as set]
             [clojure.string :as str]
             [clojure.tools.logging :as log]
             [metabase.config :as config]
@@ -9,6 +11,40 @@
             [pretty.core :as pretty])
   (:import java.sql.DriverManager
            java.util.Properties))
+
+(def sample-url "jdbc:h2:file:/Users/braden/mb/metabase/metabase.db")
+
+(defn- h2? [url]
+  (.startsWith url "jdbc:h2:"))
+
+(defn- h2-path [url]
+  (let [[_ path] (re-matches #"jdbc:h2:file:(.*)$" url)]
+    path))
+
+(defn- h2-migration-paths [url]
+  (let [base (h2-path url)]
+    {:v1db   (str base ".mv.db")
+     :v2db   (str base ".v2.mv.db")
+     :script (str base ".v2migration.sql")}))
+
+(comment
+  (h2? sample-url)
+  (h2-path sample-url)
+  (h2-migration-paths sample-url)
+  (.exists (io/file (:v1db (h2-migration-paths sample-url))))
+  )
+
+(defn- h2-v1-db [url]
+  {:dbtype "h2"
+   :dbname (h2-path url)
+   :classname "org.h2_v1_4_197.Driver"})
+
+(defn- h2-v2-db [url]
+  {:dbtype "h2"
+   :dbname (str (h2-path url) ".v2")
+   :classname "org.h2.Driver"})
+
+(def data-source-loading (atom false))
 
 (p/deftype+ DataSource [^String url ^Properties properties]
   pretty/PrettyPrintable
@@ -21,11 +57,38 @@
 
   javax.sql.DataSource
   (getConnection [_]
-    (doto (new org.h2_v1_4_197.jdbcx.JdbcDataSource)
-      (.setURL url))
-    #_(if properties
-      (DriverManager/getConnection url properties)
-      (DriverManager/getConnection url)))
+    (if @data-source-loading
+      (do (log/warn "DSL true")
+          (if properties
+            (DriverManager/getConnection url properties)
+            (DriverManager/getConnection url)))
+      (do (reset! data-source-loading true)
+          (let [conn (delay (if properties
+                              (DriverManager/getConnection url properties)
+                              (DriverManager/getConnection url)))]
+            (if (h2? url)
+              (let [{:keys [v1db v2db script]} (h2-migration-paths url)]
+                (cond
+                  ;; Case 1: v2 database exists - just load it.
+                  (.exists (io/file v2db))   (do
+                                               (log/warn "H2 v2 database exists - using it")
+                                               @conn)
+                  ;; Case 2: Migration script exists - open the database with v2 and import the script.
+                  (.exists (io/file script)) (let [db (h2-v2-db url)]
+                                               (log/warn "H2 v2 database not found, but migration script exists - importing")
+                                               (jdbc/execute! db "RUNSCRIPT FROM ? FROM_1X" script)
+                                               @conn)
+                  ;; Case 3: No upgrade artifacts exist, so load v1 and run the export, then die.
+                  (.exists (io/file v1db))   (let [db (h2-v1-db url)]
+                                               (log/warn "H2 v1 database only - exporting")
+                                               (jdbc/execute! db "SCRIPT TO ?" script)
+                                               (log/fatal "H2 migration in progress! Restart Metabase to complete the import into H2 v2"))
+
+                  ;; Case 4: Nothing at all! Just open a new v2 database.
+                  :else                      (do
+                                               (log/warn "No existing H2 database, just creating a new v2")
+                                               @conn)))
+              @conn)))))
 
   ;; we don't use (.getConnection this url user password) so we don't need to implement it.
   (getConnection [_ _user _password]
