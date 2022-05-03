1dadc5f2c713b618ea0f305f50b1774775428b84
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-03 20:15:20 +0300

    Fix broken plural translations without argument

---
+++ frontend/test/metabase/scenarios/filters/reproductions/22361-relative-filters-translations.cy.spec.js
@@ -0,0 +1,18 @@
+import { restore, openProductsTable } from "__support__/e2e/cypress";
+
+describe("issue 22361", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("should open relative filters for a non-english locale", () => {
+    cy.request("PUT", "/api/user/1", { locale: "de" });
+
+    openProductsTable();
+    cy.findByText("Created At").click();
+    cy.findByText("Nach dieser Spalte filtern").click(); // Filter by this column
+    cy.findByText("Relative Datumswerte...").click(); // Relative filters...
+    cy.findByText("Füge einen Filter hinzu").should("exist"); // Add filter
+  });
+});
