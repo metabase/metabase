191d4b1abbbf46d47187205cf2a64442314fcee8
Anton Kulyk <kuliks.anton@gmail.com>
2022-05-03 15:22:23 +0100

    Refactor FormField

--- frontend/src/metabase/components/form/FormField/FormField.styled.tsx
+++ frontend/src/metabase/components/form/FormField/FormField.styled.tsx
@@ -10,7 +10,7 @@ export const FieldRow = styled.div`
   margin-bottom: 0.5em;
 `;
 
-export const Label = styled.label`
+export const Label = styled.label<{ horizontal?: boolean }>`
   margin-bottom: 0;
   ${props =>
     props.horizontal &&
@@ -37,16 +37,14 @@ export const InfoLabel = styled.span`
   cursor: default;
 `;
 
-export const FieldContainer = styled.div`
+export const FieldContainer = styled.div<{
+  horizontal?: boolean;
+  align?: "left" | "right";
+}>`
   margin-right: ${props => (props.horizontal ? "1rem" : "")};
   margin-left: ${props => (props.align === "left" ? "0.5rem" : "")};
 `;
 
 export const InputContainer = styled.div`
   flex-shrink: 0;
-  ${props =>
-    props.horizontal &&
-    css`
-      margin-left: auto;
-    `}
 `;
