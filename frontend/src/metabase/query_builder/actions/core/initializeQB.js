0f926393963a4d83ae3f99e866c96d3f57d75ed0
Anton Kulyk <kuliks.anton@gmail.com>
2022-05-05 15:08:58 +0100

    Strip some comments

--- frontend/src/metabase/query_builder/actions/core/initializeQB.js
+++ frontend/src/metabase/query_builder/actions/core/initializeQB.js
@@ -105,7 +105,7 @@ export const INITIALIZE_QB = "metabase/qb/INITIALIZE_QB";
 export const initializeQB = (location, params) => {
   return async (dispatch, getState) => {
     const queryParams = location.query;
-    // do this immediately to ensure old state is cleared before the user sees it
+
     dispatch(resetQB());
     dispatch(cancelQuery());
 
@@ -125,9 +125,9 @@ export const initializeQB = (location, params) => {
       ...otherUiControls,
     };
 
-    // load up or initialize the card we'll be working on
     let options = {};
     let serializedCard;
+
     // hash can contain either query params starting with ? or a base64 serialized card
     if (location.hash) {
       const hash = location.hash.replace(/^#/, "");
@@ -141,13 +141,11 @@ export const initializeQB = (location, params) => {
     let preserveParameters = false;
     let snippetFetch;
     if (cardId || serializedCard) {
-      // existing card being loaded
       try {
-        // if we have a serialized card then unpack and use it
         if (serializedCard) {
           card = deserializeCardFromUrl(serializedCard);
-          // if serialized query has database we normalize syntax to support older mbql
           if (card.dataset_query.database != null) {
+            // Ensure older MBQL is supported
             card.dataset_query = normalize(card.dataset_query);
           }
         } else {
@@ -156,11 +154,11 @@ export const initializeQB = (location, params) => {
 
         const deserializedCard = card;
 
-        // load the card either from `cardId` parameter or the serialized card
         if (cardId) {
           card = await loadCard(cardId);
           // when we are loading from a card id we want an explicit clone of the card we loaded which is unmodified
           originalCard = Utils.copy(card);
+
           // for showing the "started from" lineage correctly when adding filters/breakouts and when going back and forth
           // in browser history, the original_card_id has to be set for the current card (simply the id of card itself for now)
           card.original_card_id = card.id;
@@ -184,7 +182,6 @@ export const initializeQB = (location, params) => {
           }
         } else if (card.original_card_id) {
           const deserializedCard = card;
-          // deserialized card contains the card id, so just populate originalCard
           originalCard = await loadCard(card.original_card_id);
 
           if (cardIsEquivalent(deserializedCard, originalCard)) {
@@ -223,17 +220,14 @@ export const initializeQB = (location, params) => {
           card.dataset_query.type,
         );
 
-        // if we have deserialized card from the url AND loaded a card by id then the user should be dropped into edit mode
         uiControls.isEditing = !!options.edit;
 
-        // if this is the users first time loading a saved card on the QB then show them the newb modal
         if (cardId && currentUser.is_qbnewb) {
           uiControls.isShowingNewbModal = true;
           MetabaseAnalytics.trackStructEvent("QueryBuilder", "Show Newb Modal");
         }
 
         if (card.archived) {
-          // use the error handler in App.jsx for showing "This question has been archived" message
           dispatch(setErrorPage(ARCHIVED_ERROR));
           card = null;
         }
@@ -250,8 +244,6 @@ export const initializeQB = (location, params) => {
         dispatch(setErrorPage(error));
       }
     } else {
-      // we are starting a new/empty card
-      // if no options provided in the hash, redirect to the new question flow
       if (
         !options.db &&
         !options.table &&
@@ -265,7 +257,6 @@ export const initializeQB = (location, params) => {
       const databaseId = options.db ? parseInt(options.db) : undefined;
       card = startNewCard("query", databaseId);
 
-      // initialize parts of the query based on optional parameters supplied
       if (card.dataset_query.query) {
         if (options.table != null) {
           card.dataset_query.query["source-table"] = parseInt(options.table);
@@ -277,7 +268,6 @@ export const initializeQB = (location, params) => {
           ];
         }
         if (options.metric != null) {
-          // show the summarize sidebar for metrics
           uiControls.isShowingSummarySidebar = true;
           card.dataset_query.query.aggregation = [
             "metric",
@@ -293,20 +283,17 @@ export const initializeQB = (location, params) => {
       );
     }
 
-    /**** All actions are dispatched here ****/
-
-    // Fetch alerts for the current question if the question is saved
     if (card && card.id != null) {
       dispatch(fetchAlertsForQuestion(card.id));
     }
-    // Fetch the question metadata (blocking)
+
     if (card) {
       await dispatch(loadMetadataForCard(card));
     }
 
     let question = card && new Question(card, getMetadata(getState()));
     if (question && question.isSaved()) {
-      // loading a saved question prevents auto-viz selection
+      // Don't set viz automatically for saved questions
       question = question.lockDisplay();
     }
 
@@ -332,7 +319,6 @@ export const initializeQB = (location, params) => {
 
     const objectId = params?.objectId || queryParams?.objectId;
 
-    // Update the question to Redux state together with the initial state of UI controls
     dispatch({
       type: INITIALIZE_QB,
       payload: {
@@ -344,20 +330,14 @@ export const initializeQB = (location, params) => {
       },
     });
 
-    // if we have loaded up a card that we can run then lets kick that off as well
-    // but don't bother for "notebook" mode
     if (question && uiControls.queryBuilderMode !== "notebook") {
       if (question.canRun()) {
-        // NOTE: timeout to allow Parameters widget to set parameterValues
+        // Timeout to allow Parameters widget to set parameterValues
         setTimeout(
-          () =>
-            // TODO Atte Keinänen 5/31/17: Check if it is dangerous to create a question object without metadata
-            dispatch(runQuestionQuery({ shouldUpdateUrl: false })),
+          () => dispatch(runQuestionQuery({ shouldUpdateUrl: false })),
           0,
         );
       }
-
-      // clean up the url and make sure it reflects our card state
       dispatch(
         updateUrl(card, {
           replaceState: true,
