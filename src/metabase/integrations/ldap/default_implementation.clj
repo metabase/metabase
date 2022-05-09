8574700a64a01f50b3a28f3e23038a501a1c0496
Ngoc Khuat <qn.khuat@gmail.com>
2022-05-09 15:07:35 +0700

    manual backport #22368

--- src/metabase/integrations/ldap/default_implementation.clj
+++ src/metabase/integrations/ldap/default_implementation.clj
@@ -50,7 +50,7 @@
   "Retrieve groups for a supplied DN."
   [ldap-connection         :- LDAPConnectionPool
    dn                      :- su/NonBlankString
-   uid                     :- su/NonBlankString
+   uid                     :- (s/maybe su/NonBlankString)
    {:keys [group-base]}    :- i/LDAPSettings
    group-membership-filter :- su/NonBlankString]
   (when group-base
