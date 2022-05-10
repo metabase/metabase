9b1405e3f64798f96f18b078a9d30b6ed8213b05
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-10 15:56:14 +0300

    Add embed options

--- frontend/src/metabase/lib/urls/admin.ts
+++ frontend/src/metabase/lib/urls/admin.ts
@@ -1,34 +1,33 @@
 import { DatabaseId, UserId } from "metabase-types/api";
-import { getUrl } from "./utils";
 
 export function newUser() {
-  return getUrl(`/admin/people/new`);
+  return `/admin/people/new`;
 }
 
 export function editUser(userId: UserId) {
-  return getUrl(`/admin/people/${userId}/edit`);
+  return `/admin/people/${userId}/edit`;
 }
 
 export function resetPassword(userId: UserId) {
-  return getUrl(`/admin/people/${userId}/reset`);
+  return `/admin/people/${userId}/reset`;
 }
 
 export function newUserSuccess(userId: UserId) {
-  return getUrl(`/admin/people/${userId}/success`);
+  return `/admin/people/${userId}/success`;
 }
 
 export function deactivateUser(userId: UserId) {
-  return getUrl(`/admin/people/${userId}/deactivate`);
+  return `/admin/people/${userId}/deactivate`;
 }
 
 export function reactivateUser(userId: UserId) {
-  return getUrl(`/admin/people/${userId}/reactivate`);
+  return `/admin/people/${userId}/reactivate`;
 }
 
 export function newDatabase() {
-  return getUrl(`/admin/databases/create`);
+  return `/admin/databases/create`;
 }
 
 export function editDatabase(databaseId: DatabaseId) {
-  return getUrl(`/admin/databases/${databaseId}`);
+  return `/admin/databases/${databaseId}`;
 }
