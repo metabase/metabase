23ae84426c38dcef435e262ca3ee3ecbf320528f
Anton Kulyk <kuliks.anton@gmail.com>
2022-05-10 16:07:27 +0100

    Add stub `WritebackForm`

---
+++ frontend/src/metabase/writeback/containers/WritebackForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Table from "metabase-lib/lib/metadata/Table";
+
+export interface WritebackFormProps {
+  table: Table;
+}
+
+function WritebackForm({ table }: WritebackFormProps) {
+  return null;
+}
+
+export default WritebackForm;
