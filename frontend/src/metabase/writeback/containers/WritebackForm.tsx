68164ed2e5d7c97b1ac8e0c1a2bb527bba880c4b
Anton Kulyk <kuliks.anton@gmail.com>
2022-05-10 17:51:48 +0100

    Generate simple form out of field metadata

--- frontend/src/metabase/writeback/containers/WritebackForm.tsx
+++ frontend/src/metabase/writeback/containers/WritebackForm.tsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 
+import Form from "metabase/containers/Form";
+
+import { TYPE } from "metabase/lib/types";
+
+import Field from "metabase-lib/lib/metadata/Field";
 import Table from "metabase-lib/lib/metadata/Table";
 
 export interface WritebackFormProps {
   table: Table;
+  onSubmit?: () => void;
 }
 
-function WritebackForm({ table }: WritebackFormProps) {
-  return null;
+function getFieldTypeProps(field: Field) {
+  if (field.isFK()) {
+    return {
+      type: field.isNumeric() ? "integer" : "input",
+    };
+  }
+  if (field.isNumeric()) {
+    return { type: "integer" };
+  }
+  if (field.isBoolean()) {
+    return { type: "boolean" };
+  }
+  if (field.isDate()) {
+    return { type: "date" };
+  }
+  if (field.semantic_type === TYPE.Email) {
+    return { type: "email" };
+  }
+  if (
+    field.semantic_type === TYPE.Description ||
+    field.semantic_type === TYPE.Comment
+  ) {
+    return { type: "text" };
+  }
+  return { type: "input" };
+}
+
+function WritebackForm({ table, onSubmit }: WritebackFormProps) {
+  const editableFields = useMemo(
+    () => table.fields.filter(field => !field.isPK()),
+    [table],
+  );
+
+  const form = useMemo(
+    () => ({
+      fields: editableFields.map(field => ({
+        name: field.name,
+        title: field.displayName(),
+        description: field.description,
+        ...getFieldTypeProps(field),
+      })),
+    }),
+    [editableFields],
+  );
+
+  const handleSubmit = useCallback(() => {
+    onSubmit?.();
+  }, [onSubmit]);
+
+  return <Form form={form} onSubmit={handleSubmit} />;
 }
 
 export default WritebackForm;
