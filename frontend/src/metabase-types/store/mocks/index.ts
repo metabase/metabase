e299928a8edd91a49af0634982658fb972d95ef1
Anton Kulyk <kuliks.anton@gmail.com>
2022-05-10 13:22:21 +0100

    Add query builder redux state types (#22536)
    * Add `Dataset` mock function
    
    * Add query builder redux state type
    
    * Add query builder redux state mock helper

--- frontend/src/metabase-types/store/mocks/index.ts
+++ frontend/src/metabase-types/store/mocks/index.ts
@@ -2,6 +2,7 @@ export * from "./admin";
 export * from "./app";
 export * from "./entities";
 export * from "./forms";
+export * from "./qb";
 export * from "./settings";
 export * from "./setup";
 export * from "./state";
