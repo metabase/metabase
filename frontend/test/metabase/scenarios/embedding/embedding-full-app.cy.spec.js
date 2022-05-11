d04ac2b75a47e5e29f6874efcee4103433249dad
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-11 15:50:45 +0300

    Add e2e tests

--- frontend/test/metabase/scenarios/embedding/embedding-full-app.cy.spec.js
+++ frontend/test/metabase/scenarios/embedding/embedding-full-app.cy.spec.js
@@ -14,24 +14,24 @@ describe("scenarios > embedding > full app", () => {
   });
 
   describe("navigation", () => {
-    it("should hide top nav by default", () => {
+    it("should hide the top nav by default", () => {
       cy.visit("/", visitOptions);
       cy.findByTestId("main-logo").should("not.exist");
     });
 
-    it("should show top nav when configured", () => {
+    it("should show the top nav by a param", () => {
       cy.visit("/?top_nav=true", visitOptions);
       cy.findAllByTestId("main-logo").should("be.visible");
-      cy.findByRole("button", { name: /New/ }).should("not.exist");
+      cy.button(/New/).should("not.exist");
       cy.findByPlaceholderText("Search").should("not.exist");
     });
 
-    it("should show question creation controls when configured", () => {
+    it("should show question creation controls by a param", () => {
       cy.visit("/?top_nav=true&new_button=true", visitOptions);
-      cy.findByRole("button", { name: /New/ }).should("be.visible");
+      cy.button(/New/).should("be.visible");
     });
 
-    it("should show search controls when configured", () => {
+    it("should show search controls by a param", () => {
       cy.visit("/?top_nav=true&search=true", visitOptions);
       cy.findByPlaceholderText("Search…").should("be.visible");
     });
@@ -41,13 +41,32 @@ describe("scenarios > embedding > full app", () => {
     it("should show the question header by default", () => {
       cy.visit("/question/1", visitOptions);
       cy.wait("@getCardQuery");
+
       cy.findByTestId("qb-header").should("be.visible");
     });
 
-    it("should hide the question header when configured", () => {
+    it("should hide the question header by a param", () => {
       cy.visit("/question/1?header=false", visitOptions);
       cy.wait("@getCardQuery");
+
       cy.findByTestId("qb-header").should("not.exist");
+    });
+
+    it("should hide the question's additional info by a param", () => {
+      cy.visit("/question/1?additional_info=false", visitOptions);
+      cy.wait("@getCardQuery");
+
+      cy.findByText("Our analytics").should("not.exist");
+    });
+
+    it("should hide the question's action buttons by a param", () => {
+      cy.visit("/question/1?action_buttons=false", visitOptions);
+      cy.wait("@getCardQuery");
+
+      cy.button("Summarize").should("not.exist");
+      cy.button("Filter").should("not.exist");
+      cy.icon("notebook").should("not.exist");
+      cy.icon("refresh").should("be.visible");
     });
   });
 });
