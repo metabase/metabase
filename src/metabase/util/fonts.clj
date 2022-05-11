09ee9168d16d1925c5a428ddd2776eaa8a3f8119
Adam James <adam.vermeer2@gmail.com>
2022-05-11 16:51:49 -0700

    Font list derived from font directory/font file names
    This is an attempt to derive the 'Display Name' of a font by comparing the directory name to the file names. '_' is
    replaced with spaces, but '-' exists in the 'Lato' font directory name, and we may have naming confusion in the
    future. Basically, I want to be liberal with what font directory/file names we can receive.
    
    Not certain this is the right approach yet, but it will work and correctly provides names based on the fonts available.

--- src/metabase/util/fonts.clj
+++ src/metabase/util/fonts.clj
@@ -6,28 +6,42 @@
 
 (def ^:private font-path "./resources/frontend_client/app/fonts/")
 
-(defn- normalize-font-dir
-  [path-or-string]
-  (let [s (-> path-or-string
-              str
-              (str/split #"[-_\s]"))]
-      (first (remove #{""} s))))
-
-(defn- available-fonts*
+(defn- available-font-paths
   []
   (log/info (str "Reading available fonts from " font-path))
   (->> font-path
        u.files/get-path
+       u.files/files-seq))
+
+(defn- partial-file-name
+  [parent-path path]
+  (-> (str path)
+      (str/replace (str parent-path "/") "")
+      (str/split #"-")
+      first))
+
+(defn- font-dir->font-name
+  "Compare the names of the actual font file with the directory to try determine the Display Name."
+  [path]
+  (let [potential-name (-> (str path)
+                           (str/replace font-path "")
+                           (str/replace #"_" " "))
+        no-spaces-name (str/replace potential-name #" " "")
+        font-files-partial-name (first (map #(partial-file-name path %) (u.files/files-seq path)))]
+    (if (= (str/lower-case no-spaces-name)
+           (str/lower-case font-files-partial-name))
+      potential-name
+      (str/capitalize font-files-partial-name))))
+
+(defn- available-fonts*
+  []
+  (->> font-path
+       u.files/get-path
        u.files/files-seq
-       (map #(str/replace (str %) font-path ""))
-       set))
+       (map font-dir->font-name)
+       (sort-by #(str/lower-case %))))
 
 (def ^{:arglists '([])} available-fonts
   "Return sorted set of available fonts, as Strings."
   (let [fonts (delay (available-fonts*))]
     (fn [] @fonts)))
-
-(defn available-fonts-with-names
-  "Returns all font directory names and their display names"
-  []
-  (sort-by first (map (juxt identity normalize-font-dir) (available-fonts))))
