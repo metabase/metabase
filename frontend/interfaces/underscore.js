444f46665787a7384b879df632ea93d8ff93ceb1
Ariya Hidayat <ariya@metabase.com>
2022-05-11 15:43:48 -0700

    Use native `Array.isArray` to check for array type

--- frontend/interfaces/underscore.js
+++ frontend/interfaces/underscore.js
@@ -71,7 +71,6 @@ declare module "underscore" {
   declare function isEmpty(o: any): boolean;
   declare function isString(o: any): boolean;
   declare function isObject(o: any): boolean;
-  declare function isArray(o: any): boolean;
 
   declare function groupBy<T>(
     a: Array<T>,
