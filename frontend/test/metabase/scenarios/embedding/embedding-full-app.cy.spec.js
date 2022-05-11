f298aed10e1650ca08d69297846a0fbe32d5b88b
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-11 15:38:27 +0300

    Add e2e tests

--- frontend/test/metabase/scenarios/embedding/embedding-full-app.cy.spec.js
+++ frontend/test/metabase/scenarios/embedding/embedding-full-app.cy.spec.js
@@ -1,38 +1,53 @@
 import { restore } from "__support__/e2e/cypress";
 
+const visitOptions = {
+  onBeforeLoad(window) {
+    window.Cypress = undefined;
+  },
+};
+
 describe("scenarios > embedding > full app", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+    cy.intercept("POST", `/api/card/*/query`).as("getCardQuery");
   });
 
-  it("should hide top nav by default", () => {
-    visitApp("/");
-    cy.findByTestId("main-logo").should("not.exist");
+  describe("navigation", () => {
+    it("should hide top nav by default", () => {
+      cy.visit("/", visitOptions);
+      cy.findByTestId("main-logo").should("not.exist");
+    });
+
+    it("should show top nav when configured", () => {
+      cy.visit("/?top_nav=true", visitOptions);
+      cy.findAllByTestId("main-logo").should("be.visible");
+      cy.findByRole("button", { name: /New/ }).should("not.exist");
+      cy.findByPlaceholderText("Search").should("not.exist");
+    });
+
+    it("should show question creation controls when configured", () => {
+      cy.visit("/?top_nav=true&new_button=true", visitOptions);
+      cy.findByRole("button", { name: /New/ }).should("be.visible");
+    });
+
+    it("should show search controls when configured", () => {
+      cy.visit("/?top_nav=true&search=true", visitOptions);
+      cy.findByPlaceholderText("Search…").should("be.visible");
+    });
   });
 
-  it("should show top nav when enabled", () => {
-    visitApp("/?top_nav=true");
-    cy.findAllByTestId("main-logo").should("be.visible");
-    cy.findByRole("button", { name: /New/ }).should("not.exist");
-    cy.findByPlaceholderText("Search").should("not.exist");
-  });
+  describe("questions", () => {
+    it("should show the question header by default", () => {
+      cy.visit("/question/1", visitOptions);
+      cy.wait("@getCardQuery");
+      cy.findByTestId("qb-header").should("be.visible");
+    });
 
-  it("should show question creation controls when enabled", () => {
-    visitApp("/?top_nav=true&new_button=true");
-    cy.findByRole("button", { name: /New/ }).should("be.visible");
-  });
-
-  it("should show search controls when enabled", () => {
-    visitApp("/?top_nav=true&search=true");
-    cy.findByPlaceholderText("Search…").should("be.visible");
+    it("should hide the question header when configured", () => {
+      cy.visit("/question/1?header=false", visitOptions);
+      cy.wait("@getCardQuery");
+      cy.findByTestId("qb-header").should("not.exist");
+    });
   });
 });
-
-const visitApp = url => {
-  cy.visit(url, {
-    onBeforeLoad(window) {
-      window.Cypress = undefined;
-    },
-  });
-};
