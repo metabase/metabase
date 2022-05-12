8cba0f6baab4cf7c98f3768ba5fde1a0f6a9d0b4
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-12 19:55:33 +0300

    Add icon

--- frontend/src/metabase/core/components/Tab/Tab.tsx
+++ frontend/src/metabase/core/components/Tab/Tab.tsx
@@ -1,18 +1,25 @@
-import React, { forwardRef, HTMLAttributes, ReactNode, Ref } from "react";
+import React, {
+  forwardRef,
+  HTMLAttributes,
+  MouseEvent,
+  ReactNode,
+  Ref,
+} from "react";
 import { TabIcon, TabLabel, TabRoot } from "./Tab.styled";
 
 export interface TabProps extends HTMLAttributes<HTMLButtonElement> {
   icon?: string;
   isActive?: boolean;
   children?: ReactNode;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Tab = forwardRef(function Tab(
-  { icon, isActive, children, ...props }: TabProps,
+  { icon, isActive, children, onClick, ...props }: TabProps,
   ref: Ref<HTMLButtonElement>,
 ) {
   return (
-    <TabRoot {...props} ref={ref} isActive={isActive}>
+    <TabRoot {...props} ref={ref} isActive={isActive} onClick={onClick}>
       {icon && <TabIcon name={icon} />}
       <TabLabel>{children}</TabLabel>
     </TabRoot>
