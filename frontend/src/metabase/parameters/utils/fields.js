00fd2438b40463444ba858fe8d1d8086e1314dc6
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-05-11 17:54:43 -0700

    Move some util fns to utils file

--- frontend/src/metabase/parameters/utils/fields.js
+++ frontend/src/metabase/parameters/utils/fields.js
@@ -1,3 +1,21 @@
 export function hasFieldValues(parameter) {
   return parameter.fields.some(field => field.hasFieldValues());
 }
+
+export function getFields(metadata, parameter) {
+  if (!metadata) {
+    return [];
+  }
+  return (
+    parameter.fields ??
+    getFieldIds(parameter)
+      .map(id => metadata.field(id))
+      .filter(f => f != null)
+  );
+}
+
+export function getFieldIds(parameter) {
+  const { field_ids = [], field_id } = parameter;
+  const fieldIds = field_id ? [field_id] : field_ids;
+  return fieldIds.filter(id => typeof id === "number");
+}
