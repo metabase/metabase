b38dec89f594a5def77e3c269fb2c87ca3ce2592
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-13 12:03:13 +0300

    Add dts types

---
+++ frontend/src/types/react-augment.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+declare module "react" {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function forwardRef<T, P = {}>(
+    render: (props: P, ref: React.Ref<T>) => React.ReactElement | null,
+  ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
+}
