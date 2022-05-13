df337bcaefcc56eae47c436426a6e3a25a26d488
jeff bruemmer <jeff.bruemmer@gmail.com>
2022-05-13 12:45:33 -0400

    generate api docs

--- src/metabase/api/common/internal.clj
+++ src/metabase/api/common/internal.clj
@@ -29,6 +29,23 @@
             (first route)
             route)))
 
+(defn- endpoint-name
+  "Generate a string like `GET /api/meta/db/:id` for a defendpoint route."
+  ([method route]
+   (endpoint-name *ns* method route))
+
+  ([endpoint-namespace method route]
+   (format "%s %s%s"
+           (name method)
+           (-> (.getName (the-ns endpoint-namespace))
+               (str/replace #"^metabase\.api\." "/api/")
+               ;; This is only correct for EE endpoints following the usual convention. Not all of them do. The right
+               ;; way to fix this is to move them -- see #22687
+               (str/replace #"^metabase-enterprise\.([^\.]+)\.api\." "/api/ee/$1/"))
+           (if (vector? route)
+             (first route)
+             route))))
+
 (defn- args-form-flatten
   "A version of `flatten` that will actually flatten a form such as:
 
