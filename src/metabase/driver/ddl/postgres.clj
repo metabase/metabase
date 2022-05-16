9b949bc6ea6890862580968ee92c559cde8b0efc
Case Nelson <case@metabase.com>
2022-05-16 15:37:22 -0600

    Persist model statement_timeout (#22690)
    * Persist model statement_timeout
    
    In order to safeguard against unknowingly or accidentally large models
    being persisted, we can leverage the postgres `statement_timeout`
    setting.
    
    This way, if a particular statement (likely `create table as`) takes too
    long to run, the database will kill it for us and reclaim resources.
    
    The timeout will be the lower of 10 minutes and an existing
    statement_timeout. The thinking here is that if an admin explicitly
    limitted metabase's statement runtime we shouldn't exceed it.
    
    * Rename query to avoid shadow problems

--- src/metabase/driver/ddl/postgres.clj
+++ src/metabase/driver/ddl/postgres.clj
@@ -1,6 +1,8 @@
 (ns metabase.driver.ddl.postgres
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.tools.logging :as log]
+            [honeysql.core :as hsql]
+            [java-time :as t]
             [metabase.driver.ddl.interface :as ddl.i]
             [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
             [metabase.driver.sql.util :as sql.u]
@@ -19,7 +21,7 @@
 (defn- execute! [conn [sql & params]]
   (jdbc/execute! conn (into [(add-remark sql)] params)))
 
-(defn- query [conn [sql & params]]
+(defn- jdbc-query [conn [sql & params]]
   (jdbc/query conn (into [(add-remark sql)] params)))
 
 (defn- create-schema-sql
@@ -42,12 +44,35 @@
             (q :table (ddl.i/schema-name database (public-settings/site-uuid)))
             (q :table table-name))))
 
+(defn- set-statement-timeout!
+  "Must be called within a transaction.
+   Sets the current transaction `statement_timeout` to the minimum
+   of the current (non-zero) value and ten minutes.
+
+   This helps to address unexpectedly large/long running queries."
+  [tx]
+  (let [existing-timeout (->> (hsql/format {:select [:setting]
+                                            :from [:pg_settings]
+                                            :where [:= :name "statement_timeout"]})
+                              (jdbc-query tx)
+                              first
+                              :setting
+                              parse-long)
+        ten-minutes (.toMillis (t/minutes 10))
+        new-timeout (if (zero? existing-timeout)
+                      ten-minutes
+                      (min ten-minutes existing-timeout))]
+    ;; Can't use a prepared parameter with these statements
+    (execute! tx [(format "SET LOCAL statement_timeout TO '%s'" (str new-timeout))])))
+
 (defmethod ddl.i/refresh! :postgres [_driver database definition dataset-query]
   (try
     (let [{:keys [query params]} (qp/compile dataset-query)]
       (jdbc/with-db-connection [conn (sql-jdbc.conn/db->pooled-connection-spec database)]
-        (execute! conn [(drop-table-sql database (:table-name definition))])
-        (execute! conn (into [(create-table-sql database definition query)] params))
+        (jdbc/with-db-transaction [tx conn]
+          (set-statement-timeout! tx)
+          (execute! tx [(drop-table-sql database (:table-name definition))])
+          (execute! tx (into [(create-table-sql database definition query)] params)))
         {:state :success}))
     (catch Exception e
       {:state :error :error (ex-message e)})))
@@ -68,7 +93,7 @@
         steps       [[:persist.check/create-schema
                       (fn check-schema [conn]
                         (let [existing-schemas (->> ["select schema_name from information_schema.schemata"]
-                                                    (query conn)
+                                                    (jdbc-query conn)
                                                     (map :schema_name)
                                                     (into #{}))]
                           (or (contains? existing-schemas schema-name)
@@ -82,25 +107,28 @@
                                                           "values (1)")]))]
                      [:persist.check/read-table
                       (fn read-table [conn]
-                        (query conn [(format "select * from %s.%s"
+                        (jdbc-query conn [(format "select * from %s.%s"
                                              schema-name table-name)]))]
                      [:persist.check/delete-table
                       (fn delete-table [conn]
                         (execute! conn [(drop-table-sql database table-name)]))]]]
     (jdbc/with-db-connection [conn (sql-jdbc.conn/db->pooled-connection-spec database)]
-      (loop [[[step stepfn] & remaining] steps]
-        (let [result (try (stepfn conn)
-                          (log/info (trs "Step {0} was successful for db {1}"
-                                         step (:name database)))
-                          ::valid
-                          (catch Exception e
-                            (log/warn (trs "Error in `{0}` while checking for model persistence permissions." step))
-                            (log/warn e)
-                            step))]
-          (cond (and (= result ::valid) remaining)
-                (recur remaining)
+      (jdbc/with-db-transaction
+        [tx conn]
+        (set-statement-timeout! tx)
+        (loop [[[step stepfn] & remaining] steps]
+          (let [result (try (stepfn tx)
+                            (log/info (trs "Step {0} was successful for db {1}"
+                                           step (:name database)))
+                            ::valid
+                            (catch Exception e
+                              (log/warn (trs "Error in `{0}` while checking for model persistence permissions." step))
+                              (log/warn e)
+                              step))]
+            (cond (and (= result ::valid) remaining)
+                  (recur remaining)
 
-                (= result ::valid)
-                [true :persist.check/valid]
+                  (= result ::valid)
+                  [true :persist.check/valid]
 
-                :else [false step]))))))
+                  :else [false step])))))))
