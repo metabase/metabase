ec23ee9146904035717548c047606a57fa94bc83
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-16 18:02:33 +0300

    Fix nested query case

--- frontend/src/metabase/core/components/ButtonGroup/ButtonGroup.styled.tsx
+++ frontend/src/metabase/core/components/ButtonGroup/ButtonGroup.styled.tsx
@@ -5,13 +5,13 @@ export const ButtonGroupRoot = styled.div`
   display: inline-block;
 
   ${Button.Root} {
-    &:not(:last-child) {
+    &:not(:last-of-type) {
       border-right-width: 0.5px;
       border-top-right-radius: 0;
       border-bottom-right-radius: 0;
     }
 
-    &:not(:first-child) {
+    &:not(:first-of-type) {
       border-left-width: 0.5px;
       border-top-left-radius: 0;
       border-bottom-left-radius: 0;
--- frontend/src/metabase/query_builder/components/filters/modals/BulkFilterSelect/BulkFilterSelect.styled.tsx
+++ frontend/src/metabase/query_builder/components/filters/modals/BulkFilterSelect/BulkFilterSelect.styled.tsx
@@ -5,7 +5,7 @@ import FilterPopover from "../../FilterPopover";
 export const SelectFilterButton = styled(SelectButton)`
   min-height: 2.25rem;
 
-  &:not(:first-child) {
+  &:not(:first-of-type) {
     margin-top: 0.75rem;
   }
 `;
