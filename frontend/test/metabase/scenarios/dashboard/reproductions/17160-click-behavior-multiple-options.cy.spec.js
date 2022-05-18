e141867c015769ea8561d7abfae752fb880fc185
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-05-18 21:10:23 +0400

    add a dashboard test

--- frontend/test/metabase/scenarios/dashboard/reproductions/17160-click-behavior-multiple-options.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard/reproductions/17160-click-behavior-multiple-options.cy.spec.js
@@ -2,7 +2,10 @@ import { restore, popover, visitDashboard } from "__support__/e2e/cypress";
 
 import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
 
-const { PRODUCTS } = SAMPLE_DATABASE;
+const { PRODUCTS, PRODUCTS_ID } = SAMPLE_DATABASE;
+
+const TARGET_DASHBOARD_NAME = "Target dashboard";
+const CATEGORY_FILTER_PARAMETER_ID = "7c9ege62";
 
 describe("issue 17160", () => {
   beforeEach(() => {
@@ -10,7 +13,7 @@ describe("issue 17160", () => {
     cy.signInAsAdmin();
 
     cy.createNativeQuestion({
-      name: `17160`,
+      name: `17160Q`,
       native: {
         query: "SELECT * FROM products WHERE {{CATEGORY}}",
         "template-tags": {
@@ -26,50 +29,56 @@ describe("issue 17160", () => {
         },
       },
     }).then(({ body: { id: questionId } }) => {
-      cy.createDashboard().then(({ body: { id: dashboardId } }) => {
-        // Add the question to the dashboard
-        cy.request("POST", `/api/dashboard/${dashboardId}/cards`, {
-          cardId: questionId,
-        }).then(({ body: { id: dashCardId } }) => {
-          // Add dashboard filter
-          cy.request("PUT", `/api/dashboard/${dashboardId}`, {
-            parameters: [
-              {
-                id: "7c9ege62",
-                name: "Category",
-                slug: "category",
-                type: "category",
-              },
-            ],
-          });
+      cy.createDashboard({ name: "17160D" }).then(
+        ({ body: { id: dashboardId } }) => {
+          // Add the question to the dashboard
+          cy.request("POST", `/api/dashboard/${dashboardId}/cards`, {
+            cardId: questionId,
+          }).then(({ body: { id: dashCardId } }) => {
+            // Add dashboard filter
+            cy.request("PUT", `/api/dashboard/${dashboardId}`, {
+              parameters: [
+                {
+                  id: CATEGORY_FILTER_PARAMETER_ID,
+                  name: "Category",
+                  slug: "category",
+                  sectionId: "string",
+                  type: "string/=",
+                },
+              ],
+            });
 
-          // Create a click behavior and resize the question card
-          cy.request("PUT", `/api/dashboard/${dashboardId}/cards`, {
-            cards: [
-              {
-                id: dashCardId,
-                card_id: questionId,
-                row: 0,
-                col: 0,
-                sizeX: 12,
-                sizeY: 10,
-                parameter_mappings: [
+            createTargetDashboardForClickBehavior().then(targetDashboardId => {
+              // Create a click behavior and resize the question card
+              cy.request("PUT", `/api/dashboard/${dashboardId}/cards`, {
+                cards: [
                   {
-                    parameter_id: "7c9ege62",
-                    card_id: 4,
-                    target: ["dimension", ["template-tag", "CATEGORY"]],
+                    id: dashCardId,
+                    card_id: questionId,
+                    row: 0,
+                    col: 0,
+                    sizeX: 12,
+                    sizeY: 10,
+                    parameter_mappings: [
+                      {
+                        parameter_id: CATEGORY_FILTER_PARAMETER_ID,
+                        card_id: 4,
+                        target: ["dimension", ["template-tag", "CATEGORY"]],
+                      },
+                    ],
+                    visualization_settings: getVisualSettingsWithClickBehavior(
+                      questionId,
+                      targetDashboardId,
+                    ),
                   },
                 ],
-                visualization_settings: getVisualSettingsWithClickBehavior(
-                  questionId,
-                ),
-              },
-            ],
-          });
+              });
 
-          visitDashboard(dashboardId);
-        });
-      });
+              visitDashboard(dashboardId);
+            });
+          });
+        },
+      );
     });
   });
 
@@ -83,34 +92,52 @@ describe("issue 17160", () => {
       cy.button("Add filter").click();
     });
 
-    cy.findAllByText("click-behavior-label")
+    // Check click behavior connected to a question
+    cy.findAllByText("click-behavior-question-label")
       .eq(0)
       .click();
 
     cy.url().should("include", "/question");
 
-    cy.findByText("2 selections").click();
+    assertMultipleValuesFilterState();
 
-    cy.findByTestId("Doohickey-filter-value").within(() =>
-      cy.get("input").should("be.checked"),
-    );
-    cy.findByTestId("Gadget-filter-value").within(() =>
-      cy.get("input").should("be.checked"),
-    );
+    // Go back to the dashboard
+    cy.go("back");
+
+    // Check click behavior connected to a dashboard
+    cy.findAllByText("click-behavior-dashboard-label")
+      .eq(0)
+      .click();
+
+    cy.url().should("include", "/dashboard");
+    cy.findByText(TARGET_DASHBOARD_NAME);
+
+    assertMultipleValuesFilterState();
   });
 });
 
-function getVisualSettingsWithClickBehavior(targetId) {
+function assertMultipleValuesFilterState() {
+  cy.findByText("2 selections").click();
+
+  cy.findByTestId("Doohickey-filter-value").within(() =>
+    cy.get("input").should("be.checked"),
+  );
+  cy.findByTestId("Gadget-filter-value").within(() =>
+    cy.get("input").should("be.checked"),
+  );
+}
+
+function getVisualSettingsWithClickBehavior(questionTarget, dashboardTarget) {
   return {
     column_settings: {
       '["name","ID"]': {
         click_behavior: {
-          targetId,
+          targetId: questionTarget,
           parameterMapping: {
             "6b8b10ef-0104-1047-1e1b-2492d5954322": {
               source: {
                 type: "parameter",
-                id: "7c9ege62",
+                id: CATEGORY_FILTER_PARAMETER_ID,
                 name: "Category",
               },
               target: {
@@ -122,9 +149,84 @@ function getVisualSettingsWithClickBehavior(targetId) {
           },
           linkType: "question",
           type: "link",
-          linkTextTemplate: "click-behavior-label",
+          linkTextTemplate: "click-behavior-question-label",
+        },
+      },
+
+      '["name","EAN"]': {
+        click_behavior: {
+          targetId: dashboardTarget,
+          parameterMapping: {
+            dd19ec03: {
+              source: {
+                type: "parameter",
+                id: CATEGORY_FILTER_PARAMETER_ID,
+                name: "Category",
+              },
+              target: {
+                type: "parameter",
+                id: "dd19ec03",
+              },
+              id: "dd19ec03",
+            },
+          },
+          linkType: "dashboard",
+          type: "link",
+          linkTextTemplate: "click-behavior-dashboard-label",
         },
       },
     },
   };
+}
+
+function createTargetDashboardForClickBehavior() {
+  return cy
+    .createQuestionAndDashboard({
+      dashboardDetails: {
+        name: TARGET_DASHBOARD_NAME,
+      },
+      questionDetails: {
+        query: {
+          "source-table": PRODUCTS_ID,
+        },
+      },
+    })
+    .then(({ body: { id, card_id, dashboard_id } }) => {
+      cy.request("PUT", `/api/dashboard/${dashboard_id}`, {
+        parameters: [
+          {
+            name: "Category",
+            slug: "category",
+            id: "dd19ec03",
+            type: "string/=",
+            sectionId: "string",
+          },
+        ],
+      });
+
+      // Create a click behavior and resize the question card
+      return cy
+        .request("PUT", `/api/dashboard/${dashboard_id}/cards`, {
+          cards: [
+            {
+              id,
+              card_id,
+              row: 0,
+              col: 0,
+              sizeX: 12,
+              sizeY: 10,
+              parameter_mappings: [
+                {
+                  parameter_id: "dd19ec03",
+                  card_id,
+                  target: ["dimension", ["field", PRODUCTS.CATEGORY, null]],
+                },
+              ],
+            },
+          ],
+        })
+        .then(() => {
+          return dashboard_id;
+        });
+    });
 }
