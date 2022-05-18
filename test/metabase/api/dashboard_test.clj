c8bea9c33707fad8878ebe0a1f5ffda10a00b848
Braden Shepherdson <braden@metabase.com>
2022-05-18 16:17:30 -0400

    Include report_dashboardcard with entity_id

--- test/metabase/api/dashboard_test.clj
+++ test/metabase/api/dashboard_test.clj
@@ -226,6 +226,7 @@
                                       :collection_authority_level        nil
                                       :updated_at             true
                                       :created_at             true
+                                      :entity_id              nil
                                       :parameter_mappings     []
                                       :visualization_settings {}
                                       :card                   (merge api.card-test/card-defaults-no-hydrate
@@ -271,6 +272,7 @@
                                             :row                    0
                                             :updated_at             true
                                             :created_at             true
+                                            :entity_id              nil
                                             :collection_authority_level nil
                                             :parameter_mappings     [{:card_id      1
                                                                       :parameter_id "foo"
@@ -705,6 +707,7 @@
                 :series                 []
                 :parameter_mappings     [{:card-id 123, :hash "abc", :target "foo"}]
                 :visualization_settings {}
+                :entity_id              nil
                 :created_at             true
                 :updated_at             true}
                (-> (mt/user-http-request :rasta :post 200 (format "dashboard/%d/cards" dashboard-id)
