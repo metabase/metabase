a170ddf69da3d2228d37a082cf8d0e364cfeaaf9
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-18 14:52:02 +0300

    Fix scrolling for tabs

--- frontend/test/metabase/scenarios/filters/filter-bulk.cy.spec.js
+++ frontend/test/metabase/scenarios/filters/filter-bulk.cy.spec.js
@@ -4,7 +4,7 @@ import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
 
 const { ORDERS_ID, ORDERS } = SAMPLE_DATABASE;
 
-const questionDetails = {
+const questionWithFilter = {
   dataset_query: {
     database: SAMPLE_DB_ID,
     type: "query",
@@ -15,14 +15,26 @@ const questionDetails = {
   },
 };
 
+const questionWithBreakout = {
+  dataset_query: {
+    database: SAMPLE_DB_ID,
+    type: "query",
+    query: {
+      "source-table": ORDERS_ID,
+      breakout: [["field", ORDERS.CREATED_AT, { "temporal-unit": "month" }]],
+      aggregation: [["count"]],
+    },
+  },
+};
+
 describe("scenarios > filters > bulk filtering", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
   });
 
-  it("should modify the filter for a raw query", () => {
-    visitQuestionAdhoc(questionDetails);
+  it("should set filters for a raw query", () => {
+    visitQuestionAdhoc(questionWithFilter);
 
     cy.findByLabelText("Show more filters").click();
     cy.findByLabelText("User ID").within(() => cy.icon("close").click());
@@ -36,5 +48,21 @@ describe("scenarios > filters > bulk filtering", () => {
     cy.findByText("User ID is 1").should("not.exist");
     cy.findByText("Quantity is equal to 20").should("be.visible");
     cy.findByText("Showing 4 rows").should("be.visible");
+  });
+
+  it("should set filters for an aggregated query", () => {
+    visitQuestionAdhoc(questionWithBreakout);
+
+    cy.findByLabelText("Show more filters").click();
+    cy.findByLabelText("Count").click();
+    cy.findByText("Equal to").click();
+    cy.findByText("Greater than").click();
+    cy.findByPlaceholderText("Enter a number").type("500");
+    cy.button("Add filter").click();
+    cy.button("Apply").click();
+    cy.wait("@dataset");
+
+    cy.findByText("Count is greater than 500").should("be.visible");
+    cy.findByText("Showing 21 rows").should("be.visible");
   });
 });
