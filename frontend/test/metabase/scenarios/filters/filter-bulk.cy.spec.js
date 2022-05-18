08bb99cd183981aacf3126ea7e14557d86b12303
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-18 14:19:47 +0300

    Fix scrolling for tabs

--- frontend/test/metabase/scenarios/filters/filter-bulk.cy.spec.js
+++ frontend/test/metabase/scenarios/filters/filter-bulk.cy.spec.js
@@ -1,4 +1,8 @@
-import { restore, openOrdersTable } from "__support__/e2e/cypress";
+import { restore, visitQuestionAdhoc } from "__support__/e2e/cypress";
+import { SAMPLE_DB_ID } from "__support__/e2e/cypress_data";
+import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
+
+const { ORDERS_ID } = SAMPLE_DATABASE;
 
 describe("scenarios > filters > bulk filtering", () => {
   beforeEach(() => {
@@ -7,6 +11,14 @@ describe("scenarios > filters > bulk filtering", () => {
   });
 
   it("should add a new filter in the modal", () => {
-    openOrdersTable();
+    visitQuestionAdhoc({
+      dataset_query: {
+        database: SAMPLE_DB_ID,
+        type: "query",
+        query: {
+          "source-table": ORDERS_ID,
+        },
+      },
+    });
   });
 });
