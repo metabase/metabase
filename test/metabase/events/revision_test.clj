21ba111bc0d70971b1b3bd62cf2d7ed3c76ff6bd
Case Nelson <case@metabase.com>
2022-05-19 15:46:30 -0600

    [Actions] Initial migrations for writeback (#22842)
    * Initial migrations for writeback
    
    * Update for linting
    
    * Fixing tests
    
    * Address review and add missed updated_at column
    
    * Rename readwrite to write
    
    * Fix tests after rename

--- test/metabase/events/revision_test.clj
+++ test/metabase/events/revision_test.clj
@@ -35,6 +35,7 @@
    :cache_ttl              nil
    :query_type             :query
    :table_id               (mt/id :categories)
+   :is_write               false
    :visualization_settings {}})
 
 (defn- dashboard->revision-object [dashboard]
