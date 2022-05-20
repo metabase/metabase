0d1cd29f843de1233228159fa510d9a4d795fe97
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-20 15:40:55 +0300

    Add color picker

--- frontend/src/metabase/core/components/ColorPicker/ColorPicker.tsx
+++ frontend/src/metabase/core/components/ColorPicker/ColorPicker.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-color";
 import { Hue } from "react-color/lib/components/common";
 
-export type ColorValue = ColorState;
+export type ColorChangeEvent = ColorState;
 
 const ColorPicker = (props: CustomPickerInjectedProps): JSX.Element => {
   return (
