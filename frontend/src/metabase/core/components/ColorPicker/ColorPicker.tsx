4c9ef55b8ca1e282d99487f49401f5a8410814bf
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-22 21:58:18 +0300

    Add color picker

--- frontend/src/metabase/core/components/ColorPicker/ColorPicker.tsx
+++ frontend/src/metabase/core/components/ColorPicker/ColorPicker.tsx
@@ -29,6 +29,17 @@ const styles = {
     picker: {
       boxShadow: "none",
       background: "none",
+      fontFamily: "inherit",
+    },
+    body: {
+      marginTop: "16px",
+      padding: "0",
+    },
+    color: {
+      display: "none",
+    },
+    saturation: {
+      borderRadius: "4px",
     },
   },
 };
