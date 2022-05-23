e57571cf398ae2f15f6f8da7b5929b8fac24f8cb
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-23 21:43:12 +0300

    Add forwardRef

--- frontend/src/metabase/core/components/ColorPicker/ColorPickerContent.tsx
+++ frontend/src/metabase/core/components/ColorPicker/ColorPickerContent.tsx
@@ -1,18 +1,23 @@
-import React, { useCallback } from "react";
+import React, { forwardRef, HTMLAttributes, Ref, useCallback } from "react";
 import { ColorState } from "react-color";
 import ColorInput from "metabase/core/components/ColorInput";
 import ColorPickerControls from "./ColorPickerControls";
 import { ContentContainer } from "./ColorPicker.styled";
 
-export interface ColorPickerContentProps {
+export type ColorPickerContentAttributes = Omit<
+  HTMLAttributes<HTMLDivElement>,
+  "onChange"
+>;
+
+export interface ColorPickerContentProps extends ColorPickerContentAttributes {
   color?: string;
   onChange?: (color: string) => void;
 }
 
-const ColorPickerContent = ({
-  color,
-  onChange,
-}: ColorPickerContentProps): JSX.Element => {
+const ColorPickerContent = forwardRef(function ColorPickerContent(
+  { color, onChange, ...props }: ColorPickerContentProps,
+  ref: Ref<HTMLDivElement>,
+) {
   const handleInputChange = useCallback(
     (color?: string) => color && onChange?.(color),
     [onChange],
@@ -24,11 +29,11 @@ const ColorPickerContent = ({
   );
 
   return (
-    <ContentContainer>
+    <ContentContainer {...props} ref={ref}>
       <ColorPickerControls color={color} onChange={handleControlsChange} />
       <ColorInput color={color} fullWidth onChange={handleInputChange} />
     </ContentContainer>
   );
-};
+});
 
 export default ColorPickerContent;
