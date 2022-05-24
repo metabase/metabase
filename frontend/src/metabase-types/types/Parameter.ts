07f2ae4525f549fd2d15a10921f7862e467b1ee3
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-05-24 11:19:41 -0700

    Add basic parameter types

--- frontend/src/metabase-types/types/Parameter.ts
+++ frontend/src/metabase-types/types/Parameter.ts
@@ -1,45 +1,20 @@
 import { CardId } from "./Card";
-import { Field, FieldId } from "./Field";
 import { LocalFieldReference, ForeignFieldReference } from "./Query";
 
 export type ParameterId = string;
 
-// date/*, category, id, etc
 export type ParameterType = string;
 
-// a URL-safe encoding of a parameter value
-export type ParameterValue = string;
-export type ParameterValueOrArray = string | Array<string>;
-
-export type Parameter = {
-  id: ParameterId;
-  name: string;
-  sectionId: string;
-  type: ParameterType;
-  slug: string;
-  default?: string;
-  field_id: FieldId | null;
-  field_ids?: FieldId[];
-  fields: Field[];
-  hasOnlyFieldTargets?: boolean; // true if the parameter is only connected to fields/dimensions rather than variables
-  target?: ParameterTarget;
-  filteringParameters?: ParameterId[];
-};
-
 export type VariableTarget = ["template-tag", string];
-export type DimensionTarget =
-  | ["template-tag", string]
-  | LocalFieldReference
-  | ForeignFieldReference;
+export type ParameterVariableTarget = ["variable", VariableTarget];
+export type DimensionTarget = LocalFieldReference | ForeignFieldReference;
+export type ParameterDimensionTarget = ["dimension", DimensionTarget];
+
+export type ParameterValueOrArray = string | Array<any>;
 
 export type ParameterTarget =
-  | ["variable", VariableTarget]
-  | ["dimension", DimensionTarget];
-
-export type ParameterMappingOption = {
-  name: string;
-  target: ParameterTarget;
-};
+  | ParameterVariableTarget
+  | ParameterDimensionTarget;
 
 export type ParameterMapping = {
   card_id: CardId;
@@ -47,31 +22,25 @@ export type ParameterMapping = {
   target: ParameterTarget;
 };
 
-export type ParameterOption = {
+export type ParameterMappingOptions = {
   name: string;
-  description?: string;
+  sectionId: string;
+  combinedName?: string;
   type: ParameterType;
 };
 
-export type ParameterSection = {
-  id: string;
+export interface Parameter {
+  id: ParameterId;
   name: string;
-  description: string;
-  options: ParameterOption[];
-};
+  type: ParameterType;
+  slug: string;
+  sectionId?: string;
+  default?: any;
+  filteringParameters?: ParameterId[];
+}
 
-export type ParameterInstance = {
+export type ParameterQueryObject = {
   type: ParameterType;
   target: ParameterTarget;
-  value: ParameterValue;
-};
-
-export type ParameterMappingUIOption = ParameterMappingOption & {
-  icon?: string;
-  sectionName: string;
-  isForeign?: boolean;
-};
-
-export type ParameterValues = {
-  [id: ParameterId]: ParameterValue;
+  value: ParameterValueOrArray;
 };
