a8ab9d32e978341225fffdbe6b179e44951928c8
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-25 14:48:30 +0300

    Add color widget

--- enterprise/frontend/src/metabase-enterprise/whitelabel/components/ColorSchemeWidget/ColorSchemeWidget.tsx
+++ enterprise/frontend/src/metabase-enterprise/whitelabel/components/ColorSchemeWidget/ColorSchemeWidget.tsx
@@ -4,6 +4,8 @@ import { debounce } from "lodash";
 import BrandColorSection from "../BrandColorSection";
 import { SectionTitle } from "./ColorSchemeWidget.styled";
 
+const DEBOUNCE_TIMEOUT = 400;
+
 export interface ColorSchemeWidgetProps {
   initialColors?: Record<string, string>;
   originalColors?: Record<string, string>;
@@ -18,7 +20,7 @@ const ColorSchemeWidget = ({
   const [colors, setColors] = useState(initialColors);
 
   const onChangeDebounced = useMemo(() => {
-    return onChange && debounce(onChange, 400);
+    return onChange && debounce(onChange, DEBOUNCE_TIMEOUT);
   }, [onChange]);
 
   const handleChange = useCallback(
