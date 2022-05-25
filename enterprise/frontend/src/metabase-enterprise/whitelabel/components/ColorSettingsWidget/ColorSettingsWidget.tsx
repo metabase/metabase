f0431ad353012b34edf8a1561f19fb2d7142fdbe
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-25 15:41:27 +0300

    Add color widget

--- enterprise/frontend/src/metabase-enterprise/whitelabel/components/ColorSettingsWidget/ColorSettingsWidget.tsx
+++ enterprise/frontend/src/metabase-enterprise/whitelabel/components/ColorSettingsWidget/ColorSettingsWidget.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback, useMemo, useRef } from "react";
+import { debounce } from "lodash";
 import { originalColors } from "../../lib/whitelabel";
 import ColorSettings from "../ColorSettings";
 import { ColorSetting } from "./types";
@@ -8,13 +9,32 @@ export interface ColorSettingsWidget {
   onChange: (value: Record<string, string>) => void;
 }
 
-const ColorSettingsWidget = ({ setting }: ColorSettingsWidget): JSX.Element => {
+const ColorSettingsWidget = ({
+  setting,
+  onChange,
+}: ColorSettingsWidget): JSX.Element => {
+  const onChangeDebounced = useDebounce(onChange, 400);
+
   return (
     <ColorSettings
       initialColors={setting.value}
       originalColors={originalColors}
+      onChange={onChangeDebounced}
     />
   );
+};
+
+const useDebounce = function<T>(func: (value: T) => void, wait: number) {
+  const ref = useRef(func);
+  ref.current = func;
+
+  const callback = useCallback((value: T) => {
+    return ref.current?.(value);
+  }, []);
+
+  return useMemo(() => {
+    return debounce(callback, wait);
+  }, [callback, wait]);
 };
 
 export default ColorSettingsWidget;
