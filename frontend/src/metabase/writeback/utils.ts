1b782de89982f115f543574783f14c3ed2aa7320
Anton Kulyk <kuliks.anton@gmail.com>
2022-05-27 16:59:00 +0100

    Respect DB `actions` setting on the frontend (#22946)
    * Respect DB writeback flag on the frontend
    
    * Fix test

--- frontend/src/metabase/writeback/utils.ts
+++ frontend/src/metabase/writeback/utils.ts
@@ -4,8 +4,8 @@ import { Database as IDatabase } from "metabase-types/types/Database";
 const DB_WRITEBACK_FEATURE = "actions";
 const DB_WRITEBACK_SETTING = "database-enable-actions";
 
-export const isDatabaseWritebackEnabled = (database: IDatabase) =>
+export const isDatabaseWritebackEnabled = (database?: IDatabase | null) =>
   !!database?.settings?.[DB_WRITEBACK_SETTING];
 
-export const isWritebackSupported = (database: Database) =>
+export const isWritebackSupported = (database?: Database | null) =>
   !!database?.hasFeature(DB_WRITEBACK_FEATURE);
