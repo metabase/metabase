24b260eb3f1ef3659789c4fae56254b7e4c43156
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-27 13:12:38 +0300

    Add story

---
+++ frontend/src/metabase/core/components/ColorRange/ColorRange.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ComponentStory } from "@storybook/react";
+import { color } from "metabase/lib/colors";
+import ColorRange from "./ColorRange";
+
+export default {
+  title: "Core/ColorPill",
+  component: ColorRange,
+};
+
+const Template: ComponentStory<typeof ColorRange> = args => {
+  return <ColorRange {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  colors: [color("white"), color("brand")],
+};
+
+export const ThreeColors = Template.bind({});
+ThreeColors.args = {
+  colors: [color("error"), "white", color("success")],
+};
