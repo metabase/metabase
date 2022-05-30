1838c9a40827dba40129cab4312c09a95199ce9e
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-05-30 20:39:17 +0300

    Add accents

--- frontend/src/metabase/visualizations/visualizations/Map.jsx
+++ frontend/src/metabase/visualizations/visualizations/Map.jsx
@@ -31,7 +31,7 @@ import _ from "underscore";
 
 const PIN_MAP_TYPES = new Set(["pin", "heat", "grid"]);
 
-import { getBackgroundColors } from "metabase/lib/colors/groups";
+import { getForegroundColors } from "metabase/lib/colors/groups";
 import ColorRangeSelector from "metabase/core/components/ColorRangeSelector";
 
 export default class Map extends Component {
@@ -257,16 +257,16 @@ export default class Map extends Component {
       title: t`Color`,
       widget: ColorRangeSelector,
       props: {
-        colors: getBackgroundColors(),
+        colors: getForegroundColors(),
         colorMapping: Object.fromEntries(
-          getBackgroundColors().map(color => [
+          getForegroundColors().map(color => [
             color,
             getColorplethColorScale(color),
           ]),
         ),
         isQuantile: true,
       },
-      default: getColorplethColorScale(getBackgroundColors()[0]),
+      default: getColorplethColorScale(getForegroundColors()[0]),
       getHidden: (series, vizSettings) => vizSettings["map.type"] !== "region",
     },
     "map.zoom": {},
