70bdc04133b5b04c08f0958de78d075d06939d89
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-05-31 11:44:33 -0700

    refactor dashboard utils
    Fix e2e test

--- frontend/src/metabase/parameters/utils/fields.unit.spec.js
+++ frontend/src/metabase/parameters/utils/fields.unit.spec.js
@@ -1,10 +1,5 @@
 import Field from "metabase-lib/lib/metadata/Field";
-import {
-  hasFieldValues,
-  getFieldIds,
-  hasFields,
-  isOnlyMappedToFields,
-} from "./fields";
+import { hasFieldValues, hasFields, isOnlyMappedToFields } from "./fields";
 
 describe("parameters/utils/fields", () => {
   describe("hasFieldValues", () => {
@@ -29,29 +24,6 @@ describe("parameters/utils/fields", () => {
 
     it("should handle a parameter with no fields", () => {
       expect(hasFieldValues({})).toBe(false);
-    });
-  });
-
-  describe("getFieldIds", () => {
-    it("should handle a parameter with no fields", () => {
-      expect(getFieldIds({})).toEqual([]);
-    });
-
-    it("should return number field ids", () => {
-      expect(getFieldIds({ field_ids: [1, 2, 3] })).toEqual([1, 2, 3]);
-    });
-
-    it("should filter out virtual field ids", () => {
-      expect(getFieldIds({ field_ids: [1, "two", 3] })).toEqual([1, 3]);
-    });
-
-    it("should favor the field_id prop for whatever reason", () => {
-      expect(
-        getFieldIds({
-          field_id: 1,
-          field_ids: [2, 3],
-        }),
-      ).toEqual([1]);
     });
   });
 
