a60e6e90995fa3810eb936bc8fab4ad3375976e3
Case Nelson <case@metabase.com>
2022-05-31 11:28:18 -0600

    Use transducer as per review

--- src/metabase/query_processor/middleware/resolve_fields.clj
+++ src/metabase/query_processor/middleware/resolve_fields.clj
@@ -16,9 +16,8 @@
   "Resolve all field referenced in the `query`, and store them in the QP Store."
   [query]
   (let [ids (into (set (mbql.u/match (:query query) [:field (id :guard integer?) _] id))
-                  (->> (mbql.u/match (:query query) {:source-metadata source-metadata} source-metadata)
-                       (apply concat)
-                       (keep :id)))]
+                  (comp cat (keep :id))
+                  (mbql.u/match (:query query) {:source-metadata source-metadata} source-metadata))]
     (try
       (u/prog1 query
         (resolve-fields-with-ids! ids))
