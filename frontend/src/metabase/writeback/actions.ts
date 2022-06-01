10c082635569d9225b47afe068fb4fe78837153f
Anton Kulyk <kuliks.anton@gmail.com>
2022-06-01 18:19:16 +0100

    Extract `deleteRow` function

--- frontend/src/metabase/writeback/actions.ts
+++ frontend/src/metabase/writeback/actions.ts
@@ -75,25 +75,29 @@ export type DeleteRowPayload = {
   id: number | string;
 };
 
+export const deleteRow = (payload: DeleteRowPayload) => {
+  const { table, id } = payload;
+  const field = table.fields.find(field => field.isPK());
+  if (!field) {
+    throw new Error("Cannot delete row from table without a primary key");
+  }
+
+  const pk = field.isNumeric() && typeof id === "string" ? parseInt(id) : id;
+  return ActionsApi.delete({
+    type: "query",
+    database: table.db_id,
+    query: {
+      "source-table": table.id,
+      filter: ["=", field.reference(), pk],
+    },
+  });
+};
+
 export const DELETE_ROW_FROM_OBJECT_DETAIL =
   "metabase/qb/DELETE_ROW_FROM_OBJECT_DETAIL";
 export const deleteRowFromObjectDetail = (payload: DeleteRowPayload) => {
   return async (dispatch: any) => {
-    const { table, id } = payload;
-    const field = table.fields.find(field => field.isPK());
-    if (!field) {
-      throw new Error("Cannot delete row from table without a primary key");
-    }
-
-    const pk = field.isNumeric() && typeof id === "string" ? parseInt(id) : id;
-    const result = await ActionsApi.delete({
-      type: "query",
-      database: table.db_id,
-      query: {
-        "source-table": table.id,
-        filter: ["=", field.reference(), pk],
-      },
-    });
+    const result = await deleteRow(payload);
 
     dispatch.action(DELETE_ROW_FROM_OBJECT_DETAIL, payload);
     if (result?.["rows-deleted"]?.length > 0) {
