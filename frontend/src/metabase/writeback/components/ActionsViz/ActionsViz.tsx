995a741730cd2ab8b14a1abdcaadadd14f044703
Anton Kulyk <kuliks.anton@gmail.com>
2022-06-01 17:45:30 +0100

    Connect "Update" form

--- frontend/src/metabase/writeback/components/ActionsViz/ActionsViz.tsx
+++ frontend/src/metabase/writeback/components/ActionsViz/ActionsViz.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { t } from "ttag";
+import { connect } from "react-redux";
 
 import Button from "metabase/core/components/Button";
 import Modal from "metabase/components/Modal";
 
 import { useToggle } from "metabase/hooks/use-toggle";
 
+import { getCardData } from "metabase/dashboard/selectors";
 import WritebackModalForm from "metabase/writeback/containers/WritebackModalForm";
 
 import Metadata from "metabase-lib/lib/metadata/Metadata";
 import Question from "metabase-lib/lib/Question";
-import { DashboardWithCards } from "metabase-types/types/Dashboard";
+
+import { State } from "metabase-types/store";
+import { SavedCard } from "metabase-types/types/Card";
+import { DashboardWithCards, DashCard } from "metabase-types/types/Dashboard";
+import { Dataset } from "metabase-types/types/Dataset";
 import { VisualizationProps } from "metabase-types/types/Visualization";
 
 import { HorizontalAlignmentValue } from "./types";
@@ -76,12 +82,41 @@ const ACTIONS_VIZ_DEFINITION = {
   },
 };
 
-interface ActionsVizProps extends VisualizationProps {
+// { [dashCardId]: { [cardId]: <dataset> } }
+type DashCardData = Record<number, Record<number, Dataset | undefined>>;
+
+interface ActionVizOwnProps extends VisualizationProps {
   dashboard: DashboardWithCards;
+  dashCardData?: DashCardData;
   metadata?: Metadata;
 }
 
-function ActionsViz({ dashboard, metadata, settings }: ActionsVizProps) {
+interface ActionWizStateProps {
+  dashCardData?: DashCardData;
+}
+
+type ActionsVizProps = ActionVizOwnProps & ActionWizStateProps;
+
+function mapStateToProps(state: State) {
+  return {
+    dashCardData: getCardData(state),
+  };
+}
+
+function getObjectDetailViewData(
+  dashCardData: DashCardData,
+  dashCard: DashCard<SavedCard>,
+): unknown[] | undefined {
+  const cardQueryResult = dashCardData[dashCard.id][dashCard.card_id];
+  return cardQueryResult?.data.rows[0];
+}
+
+function ActionsViz({
+  dashboard,
+  dashCardData,
+  metadata,
+  settings,
+}: ActionsVizProps) {
   const [isModalOpen, { turnOn: showModal, turnOff: hideModal }] = useToggle(
     false,
   );
@@ -97,6 +132,13 @@ function ActionsViz({ dashboard, metadata, settings }: ActionsVizProps) {
 
   const isObjectDetailView = question?.display() === "object";
   const table = question?.table();
+  const row =
+    connectedDashCard && isObjectDetailView && dashCardData
+      ? getObjectDetailViewData(
+          dashCardData,
+          connectedDashCard as DashCard<SavedCard>,
+        )
+      : undefined;
 
   const hasCreateButton =
     settings["actions.create_enabled"] && !isObjectDetailView;
@@ -115,18 +157,27 @@ function ActionsViz({ dashboard, metadata, settings }: ActionsVizProps) {
         {hasCreateButton && (
           <Button disabled={!question} onClick={showModal}>{t`New`}</Button>
         )}
-        {hasUpdateButton && <Button disabled={!question}>{t`Edit`}</Button>}
+        {hasUpdateButton && (
+          <Button disabled={!question} onClick={showModal}>{t`Edit`}</Button>
+        )}
         {hasDeleteButton && (
           <Button disabled={!question} danger>{t`Delete`}</Button>
         )}
       </Root>
       {!!table && (
         <Modal isOpen={isModalOpen} onClose={hideModal}>
-          <WritebackModalForm table={table} onClose={hideModal} />
+          <WritebackModalForm table={table} row={row} onClose={hideModal} />
         </Modal>
       )}
     </>
   );
 }
 
-export default Object.assign(ActionsViz, ACTIONS_VIZ_DEFINITION);
+const ConnectedActionsViz = connect<
+  ActionWizStateProps,
+  unknown,
+  ActionVizOwnProps,
+  State
+>(mapStateToProps)(ActionsViz);
+
+export default Object.assign(ConnectedActionsViz, ACTIONS_VIZ_DEFINITION);
