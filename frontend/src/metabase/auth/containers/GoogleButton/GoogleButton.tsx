97026720e5b3fba31843af0125646a0a293616e3
Gustavo Saiani <gustavo@poe.ma>
2022-06-02 14:23:46 +0200

    Update Google Sign In (#22868)

--- frontend/src/metabase/auth/containers/GoogleButton/GoogleButton.tsx
+++ frontend/src/metabase/auth/containers/GoogleButton/GoogleButton.tsx
@@ -1,14 +1,9 @@
 import { connect } from "react-redux";
-import { attachGoogleAuth } from "metabase/lib/auth";
 import GoogleButton from "../../components/GoogleButton";
 import { loginGoogle } from "../../actions";
-
-const mapStateToProps = () => ({
-  onAttach: attachGoogleAuth,
-});
 
 const mapDispatchToProps = {
   onLogin: loginGoogle,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(GoogleButton);
+export default connect(null, mapDispatchToProps)(GoogleButton);
