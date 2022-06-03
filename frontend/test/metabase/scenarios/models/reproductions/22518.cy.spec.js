31f2624f011eddb8ad28b88eb779b43d33f6e7f4
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-06-03 20:55:03 +0200

    Add repro for #22518

---
+++ frontend/test/metabase/scenarios/models/reproductions/22518.cy.spec.js
@@ -0,0 +1,39 @@
+import {
+  restore,
+  openQuestionActions,
+  summarize,
+  sidebar,
+} from "__support__/e2e/cypress";
+
+describe("issue 22518", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+
+    cy.createNativeQuestion(
+      {
+        native: {
+          query: "select 1 id, 'a' foo",
+        },
+        dataset: true,
+      },
+      { visitQuestion: true },
+    );
+  });
+
+  it("UI should immediately reflect model query changes upon saving (metabase#22518)", () => {
+    openQuestionActions();
+    cy.findByText("Edit query definition").click();
+
+    cy.get(".ace_content").type(", 'b' bar");
+
+    cy.findByText("Save changes").click();
+
+    summarize();
+
+    sidebar()
+      .should("contain", "ID")
+      .and("contain", "FOO")
+      .and("contain", "BAR");
+  });
+});
