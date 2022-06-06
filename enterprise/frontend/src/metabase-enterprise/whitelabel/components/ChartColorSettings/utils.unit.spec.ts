a34cb93fb5aff29569aa9342a8a8d2c1e8a9361c
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-06 18:38:07 +0300

    Show button only with custom colors

--- enterprise/frontend/src/metabase-enterprise/whitelabel/components/ChartColorSettings/utils.unit.spec.ts
+++ enterprise/frontend/src/metabase-enterprise/whitelabel/components/ChartColorSettings/utils.unit.spec.ts
@@ -1,3 +1,4 @@
+import Color from "color";
 import { getDefaultChartColors, getAutoChartColors } from "./utils";
 
 describe("getDefaultChartColors", () => {
@@ -32,9 +33,9 @@ describe("getAutoChartColors", () => {
 
     expect(newValues).toEqual({
       brand: "blue",
-      accent1: expect.any(String),
-      accent2: expect.any(String),
-      accent3: expect.any(String),
+      accent1: Color.rgb(138, 230, 230).hex(), // cyan
+      accent2: Color.rgb(0, 128, 0).hex(), // green, unchanged
+      accent3: Color.rgb(138, 161, 230).hex(), // purple
     });
   });
 });
