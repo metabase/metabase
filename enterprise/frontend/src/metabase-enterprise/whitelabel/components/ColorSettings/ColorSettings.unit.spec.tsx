aeb3dbf9a0639a1ec253e9734fc698ce6ad05290
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-06 20:57:05 +0300

    Add tests

--- enterprise/frontend/src/metabase-enterprise/whitelabel/components/ColorSettings/ColorSettings.unit.spec.tsx
+++ enterprise/frontend/src/metabase-enterprise/whitelabel/components/ColorSettings/ColorSettings.unit.spec.tsx
@@ -1,1 +1,55 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { color, colors } from "metabase/lib/colors/palette";
+import ColorSettings from "./ColorSettings";
+
+describe("ColorSettings", () => {
+  const initialColors = {
+    brand: color("success"),
+    accent1: color("text-medium"),
+  };
+
+  it("should update brand colors", () => {
+    const onChange = jest.fn();
+
+    render(
+      <ColorSettings
+        initialColors={initialColors}
+        originalColors={colors}
+        onChange={onChange}
+      />,
+    );
+
+    const input = screen.getByPlaceholderText(color("summarize"));
+    userEvent.clear(input);
+    userEvent.type(input, color("error"));
+
+    expect(onChange).toHaveBeenLastCalledWith({
+      brand: color("success"),
+      summarize: color("error"),
+      accent1: color("text-medium"),
+    });
+  });
+
+  it("should update chart colors", () => {
+    const onChange = jest.fn();
+
+    render(
+      <ColorSettings
+        initialColors={initialColors}
+        originalColors={colors}
+        onChange={onChange}
+      />,
+    );
+
+    const input = screen.getByDisplayValue(color("text-medium"));
+    userEvent.clear(input);
+    userEvent.type(input, color("text-light"));
+
+    expect(onChange).toHaveBeenLastCalledWith({
+      brand: color("success"),
+      accent1: color("text-light"),
+    });
+  });
+});
