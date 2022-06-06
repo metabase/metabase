8dffb466fd820353790d284d7a0f598c8d117475
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-06 18:19:56 +0300

    Add a modal

--- enterprise/frontend/src/metabase-enterprise/whitelabel/components/ChartColorModal/ChartColorModal.tsx
+++ enterprise/frontend/src/metabase-enterprise/whitelabel/components/ChartColorModal/ChartColorModal.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { t } from "ttag";
+import Button from "metabase/core/components/Button/Button";
+import ModalContent from "metabase/components/ModalContent";
 
 export interface ChartColorModalProps {
   onReset?: () => void;
   onClose?: () => void;
 }
 
-const ChartColorModal = ({ onClose }: ChartColorModalProps): JSX.Element => {
-  return <div onClick={onClose} />;
+const ChartColorModal = ({
+  onReset,
+  onClose,
+}: ChartColorModalProps): JSX.Element => {
+  const handleReset = useCallback(() => {
+    onReset?.();
+    onClose?.();
+  }, [onReset, onClose]);
+
+  return (
+    <ModalContent
+      title={t`Reset to default colors?`}
+      footer={[
+        <Button key="close" onClick={onClose}>
+          {t`Cancel`}
+        </Button>,
+        <Button key="delete" danger onClick={handleReset}>
+          {t`Reset`}
+        </Button>,
+      ]}
+      onClose={onClose}
+    >
+      {t`This cannot be undone.`}
+    </ModalContent>
+  );
 };
 
 export default ChartColorModal;
