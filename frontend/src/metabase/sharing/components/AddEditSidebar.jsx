075903102757530f183b5733899eb6858b117ce7
Gustavo Saiani <gustavo@poe.ma>
2022-06-07 16:59:21 -0300

    Toggle private / public

--- frontend/src/metabase/sharing/components/AddEditSidebar.jsx
+++ frontend/src/metabase/sharing/components/AddEditSidebar.jsx
@@ -1,6 +1,6 @@
 /* eslint "react/prop-types": "error" */
 
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import _ from "underscore";
 import cx from "classnames";
@@ -19,6 +19,8 @@ import SendTestPulse from "metabase/components/SendTestPulse";
 import Sidebar from "metabase/dashboard/components/Sidebar";
 import Toggle from "metabase/core/components/Toggle";
 import Select, { Option } from "metabase/core/components/Select";
+import TextInput from "metabase/components/TextInput";
+import Radio from "metabase/core/components/Radio/index.ts";
 
 import { dashboardPulseIsValid } from "metabase/lib/pulse";
 import MetabaseSettings from "metabase/lib/settings";
@@ -410,35 +412,82 @@ function CaveatMessage() {
 }
 
 function ChannelFields({ channel, channelSpec, onChannelPropertyChange }) {
+  const [channelType, setChannelType] = useState("public");
+  const [privateChannelText, setPrivateChannelText] = useState("");
+
+  const channelTypeOptions = [
+    { name: t`A public channel`, value: "public" },
+    { name: t`A private channel`, value: "private" },
+  ];
+
+  const handlePrivateChannelTextChange = (field, value) => {
+    setPrivateChannelText(value);
+
+    onChannelPropertyChange("details", {
+      ...channel.details,
+      [field.name]: value,
+    });
+  };
+
+  const handleChannelTypeChange = channelType => {
+    setChannelType(channelType);
+  };
+
   const valueForField = field => {
-    const value = channel.details && channel.details[field.name];
+    const value = channel?.details?.[field.name];
     return value != null ? value : null; // convert undefined to null so Uncontrollable doesn't ignore changes
   };
+
   return (
     <div>
       {channelSpec.fields.map(field => (
         <div key={field.name} className={field.name}>
-          <span className="text-bold mr1">{field.displayName}</span>
+          <span className="block text-bold pb1">{field.displayName}</span>
+
           {field.type === "select" ? (
-            <Select
-              className="text-bold bg-white inline-block"
-              value={valueForField(field)}
-              placeholder={t`Pick a user or channel...`}
-              searchProp="name"
-              // Address #5799 where `details` object is missing for some reason
-              onChange={o =>
-                onChannelPropertyChange("details", {
-                  ...channel.details,
-                  [field.name]: o.target.value,
-                })
-              }
-            >
-              {field.options.map(option => (
-                <Option key={option} name={option} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
+            <>
+              <Radio
+                variant="bubble"
+                value={channelType}
+                options={channelTypeOptions}
+                onChange={handleChannelTypeChange}
+              />
+
+              {channelType === "public" && (
+                <Select
+                  className="text-bold bg-white inline-block pt2 full-width"
+                  value={valueForField(field)}
+                  placeholder={t`Pick a user or channel...`}
+                  searchProp="name"
+                  // Address #5799 where `details` object is missing for some reason
+                  onChange={o =>
+                    onChannelPropertyChange("details", {
+                      ...channel.details,
+                      [field.name]: o.target.value,
+                    })
+                  }
+                >
+                  {field.options.map(option => (
+                    <Option key={option} name={option} value={option}>
+                      {option}
+                    </Option>
+                  ))}
+                </Select>
+              )}
+
+              {channelType === "private" && (
+                <TextInput
+                  className="pt1"
+                  value={privateChannelText}
+                  placeholder={t`Fill name of private channel`}
+                  autoFocus
+                  aria-autocomplete="list"
+                  onChange={value =>
+                    handlePrivateChannelTextChange(field, value)
+                  }
+                />
+              )}
+            </>
           ) : null}
         </div>
       ))}
