f411b6a1b367ec7375196acc27ff9d23180f48d6
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-09 18:22:26 +0300

    Add area

--- frontend/src/metabase/visualizations/components/ChartSkeleton/BarSkeleton.styled.tsx
+++ frontend/src/metabase/visualizations/components/ChartSkeleton/BarSkeleton.styled.tsx
@@ -2,8 +2,13 @@ import styled from "@emotion/styled";
 import { color } from "metabase/lib/colors";
 import { animationStyles } from "./ChartSkeleton.styled";
 
-export const SkeletonRoot = styled.svg`
+export const SkeletonRoot = styled.div`
   ${animationStyles};
+  height: 100%;
+  padding-top: 2.375rem;
+`;
+
+export const SkeletonImage = styled.svg`
   color: ${color("bg-medium")};
   width: 100%;
   height: 100%;
