f482222ec34694c1aaf6447f1e95905241d0732e
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-09 12:04:27 +0300

    Add scatter

--- frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.tsx
+++ frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.tsx
@@ -3,8 +3,15 @@ import { percentage } from "./utils";
 import { SkeletonRoot, SkeletonCircle } from "./ScatterSkeleton.styled";
 
 const CIRCLES = [
-  [0.08, 0.503, 0.059],
-  [0.08, 0.222, 0.032],
+  [0.081, 0.222, 0.032],
+  [0.081, 0.622, 0.059],
+  [0.272, 0.104, 0.022],
+  [0.288, 0.504, 0.14],
+  [0.431, 0.185, 0.059],
+  [0.431, 0.719, 0.086],
+  [0.612, 0.289, 0.113],
+  [0.854, 0.104, 0.022],
+  [0.862, 0.666, 0.207],
 ];
 
 const ScatterSkeleton = (): JSX.Element => {
