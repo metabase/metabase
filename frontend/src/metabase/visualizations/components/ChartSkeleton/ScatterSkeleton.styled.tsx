ca0de2f2fb2d582d8810e0a201a0278c5e806f16
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-09 12:29:24 +0300

    Add scatter

--- frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.styled.tsx
+++ frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.styled.tsx
@@ -1,21 +1,10 @@
 import styled from "@emotion/styled";
 import { color } from "metabase/lib/colors";
-import { animationStyles, positionStyles } from "./ChartSkeleton.styled";
+import { animationStyles } from "./ChartSkeleton.styled";
 
-export const SkeletonRoot = styled.div`
-  ${positionStyles};
+export const SkeletonRoot = styled.svg`
   ${animationStyles};
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  position: relative;
-  border-left: 1px solid ${color("bg-medium")};
-  border-bottom: 1px solid ${color("bg-medium")};
-`;
-
-export const SkeletonCircle = styled.div`
-  position: absolute;
-  border-radius: 10rem;
-  background-color: ${color("bg-medium")};
-  transform: translate(-50%, -50%);
+  color: ${color("bg-medium")};
+  width: 100%;
+  height: 100%;
 `;
--- frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.tsx
+++ frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.tsx
@@ -1,33 +1,22 @@
 import React from "react";
-import { percentage } from "./utils";
-import { SkeletonRoot, SkeletonCircle } from "./ScatterSkeleton.styled";
-
-const CIRCLES = [
-  [0.081, 0.222, 0.032],
-  [0.081, 0.622, 0.059],
-  [0.272, 0.104, 0.022],
-  [0.288, 0.504, 0.14],
-  [0.431, 0.185, 0.059],
-  [0.431, 0.719, 0.086],
-  [0.612, 0.289, 0.113],
-  [0.854, 0.104, 0.022],
-  [0.862, 0.666, 0.207],
-];
+import { SkeletonRoot } from "./RowSkeleton.styled";
 
 const ScatterSkeleton = (): JSX.Element => {
   return (
-    <SkeletonRoot>
-      {CIRCLES.map(([x, y, radius], index) => (
-        <SkeletonCircle
-          key={index}
-          style={{
-            top: percentage(y),
-            left: percentage(x),
-            width: percentage(radius),
-            paddingBottom: percentage(radius),
-          }}
-        />
-      ))}
+    <SkeletonRoot
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 340 119"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle cx="11" cy="58" r="11" fill="currentColor" />
+      <circle cx="88" cy="58" r="26" fill="currentColor" />
+      <circle cx="208" cy="29" r="21" fill="currentColor" />
+      <circle cx="301.5" cy="80.5" r="38.5" fill="currentColor" />
+      <circle cx="11" cy="20" r="6" fill="currentColor" />
+      <circle cx="82" cy="4" r="4" fill="currentColor" />
+      <circle cx="298" cy="4" r="4" fill="currentColor" />
+      <circle cx="141" cy="15" r="11" fill="currentColor" />
+      <circle cx="141" cy="87" r="16" fill="currentColor" />
     </SkeletonRoot>
   );
 };
