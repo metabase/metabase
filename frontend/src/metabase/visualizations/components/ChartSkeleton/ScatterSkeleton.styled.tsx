89783a00c59675471dbf89722dedd909bbd35d4e
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-10 17:19:59 +0300

    Add axes

--- frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.styled.tsx
+++ frontend/src/metabase/visualizations/components/ChartSkeleton/ScatterSkeleton.styled.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { color } from "metabase/lib/colors";
 import { containerStyles, animationStyles } from "./ChartSkeleton.styled";
 
 export const SkeletonRoot = styled.div`
@@ -9,4 +10,8 @@ export const SkeletonImage = styled.svg`
   ${animationStyles};
   flex: 1 1 0;
   margin-top: 1rem;
+  padding-left: 0.5rem;
+  padding-bottom: 0.5rem;
+  border-left: 1px solid ${color("bg-medium")};
+  border-bottom: 1px solid ${color("bg-medium")};
 `;
