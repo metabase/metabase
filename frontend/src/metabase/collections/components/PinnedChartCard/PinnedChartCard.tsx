fd1722421cff2a0d5767a4e54ca8507fdebb4901
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-10 12:07:09 +0300

    Add area

--- frontend/src/metabase/collections/components/PinnedChartCard/PinnedChartCard.tsx
+++ frontend/src/metabase/collections/components/PinnedChartCard/PinnedChartCard.tsx
@@ -54,6 +54,8 @@ const PinnedChartCard = ({
               rawSeries={rawSeries}
               error={error}
               errorIcon={errorIcon}
+              showTitle
+              isDashboard
             />
           )
         }
