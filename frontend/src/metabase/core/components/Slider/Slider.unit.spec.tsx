518e12a6edf5d2a644e7685fc21a5301c0d7906d
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2022-06-13 16:44:21 -0600

    Slider Component Performance Improvements (#23236)
    * make slider change less

--- frontend/src/metabase/core/components/Slider/Slider.unit.spec.tsx
+++ frontend/src/metabase/core/components/Slider/Slider.unit.spec.tsx
@@ -23,21 +23,42 @@ describe("Slider", () => {
     expect(minInput).toHaveAttribute("max", "412");
   });
 
-  it("should call onChange with the new value on input change", () => {
+  it("should call onChange with the new value on mouseUp", () => {
     const spy = jest.fn();
     render(<Slider value={[10, 20]} onChange={spy} min={0} max={100} />);
 
     const minInput = screen.getByLabelText("min");
     const maxInput = screen.getByLabelText("max");
 
-    // would be nice to use userEvent when we upgrade to v14 so we mock drage events
+    // would be nice to use userEvent when we upgrade to v14 so we can mock drag events
     fireEvent.change(minInput, { target: { value: "5" } });
+    fireEvent.mouseUp(minInput);
     fireEvent.change(maxInput, { target: { value: "15" } });
+    fireEvent.mouseUp(maxInput);
 
     const [firstCall, secondCall] = spy.mock.calls;
 
     expect(firstCall[0]).toEqual([5, 20]);
-    expect(secondCall[0]).toEqual([10, 15]);
+    expect(secondCall[0]).toEqual([5, 15]);
+  });
+
+  // handling input via arrow keys
+  it("should call onChange with the new value on keyup", () => {
+    const spy = jest.fn();
+    render(<Slider value={[10, 20]} onChange={spy} min={0} max={100} />);
+
+    const minInput = screen.getByLabelText("min");
+    const maxInput = screen.getByLabelText("max");
+
+    fireEvent.change(minInput, { target: { value: "5" } });
+    fireEvent.keyUp(minInput);
+    fireEvent.change(maxInput, { target: { value: "15" } });
+    fireEvent.keyUp(maxInput);
+
+    const [firstCall, secondCall] = spy.mock.calls;
+
+    expect(firstCall[0]).toEqual([5, 20]);
+    expect(secondCall[0]).toEqual([5, 15]);
   });
 
   it("should sort input values on mouse up", () => {
@@ -48,5 +69,34 @@ describe("Slider", () => {
 
     fireEvent.mouseUp(minInput);
     expect(spy.mock.calls[0][0]).toEqual([1, 2]);
+  });
+
+  it("should show tooltips on mouseOver", () => {
+    render(<Slider value={[10, 40]} onChange={() => null} min={0} max={100} />);
+
+    const minInput = screen.getByLabelText("min");
+    const minTooltip = screen.getByTestId("min-slider-tooltip");
+    const maxTooltip = screen.getByTestId("max-slider-tooltip");
+
+    expect(minTooltip).toHaveStyle("opacity: 0");
+    expect(maxTooltip).toHaveStyle("opacity: 0");
+    fireEvent.mouseOver(minInput);
+    expect(minTooltip).toHaveStyle("opacity: 1");
+    expect(maxTooltip).toHaveStyle("opacity: 1");
+  });
+
+  it("should hide tooltips on mouseLeave", () => {
+    render(<Slider value={[10, 40]} onChange={() => null} min={0} max={100} />);
+
+    const minInput = screen.getByLabelText("min");
+    const minTooltip = screen.getByTestId("min-slider-tooltip");
+    const maxTooltip = screen.getByTestId("max-slider-tooltip");
+
+    fireEvent.mouseEnter(minInput);
+    expect(minTooltip).toHaveStyle("opacity: 1");
+    expect(maxTooltip).toHaveStyle("opacity: 1");
+    fireEvent.mouseLeave(minInput);
+    expect(minTooltip).toHaveStyle("opacity: 0");
+    expect(maxTooltip).toHaveStyle("opacity: 0");
   });
 });
