1c1c5f4ff0bd20307c5c78d6f396159252d884e3
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-14 16:59:41 +0300

    Add collection preview menu item

--- frontend/src/metabase/collections/utils.ts
+++ frontend/src/metabase/collections/utils.ts
@@ -10,8 +10,8 @@ export type Item = {
   description: string | null;
   copy?: boolean;
   collection_position?: number | null;
-  collection_preview?: boolean;
-  has_required_parameters?: boolean;
+  collection_preview?: boolean | null;
+  has_required_parameters?: boolean | null;
   getIcon: () => { name: string };
   getUrl: () => string;
   setArchived: (isArchived: boolean) => void;
