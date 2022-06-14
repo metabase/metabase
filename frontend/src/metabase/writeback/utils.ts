e14abd0ad81a61f7464daed7bbb34bcac150a0f7
Anton Kulyk <kuliks.anton@gmail.com>
2022-06-14 17:05:51 +0100

    Add action button utils

--- frontend/src/metabase/writeback/utils.ts
+++ frontend/src/metabase/writeback/utils.ts
@@ -1,5 +1,6 @@
 import Database from "metabase-lib/lib/metadata/Database";
 import { Database as IDatabase } from "metabase-types/types/Database";
+import { DashCard } from "metabase-types/types/Dashboard";
 
 const DB_WRITEBACK_FEATURE = "actions";
 const DB_WRITEBACK_SETTING = "database-enable-actions";
@@ -9,3 +10,12 @@ export const isDatabaseWritebackEnabled = (database?: IDatabase | null) =>
 
 export const isWritebackSupported = (database?: Database | null) =>
   !!database?.hasFeature(DB_WRITEBACK_FEATURE);
+
+export const isActionButtonDashCard = (dashCard: DashCard) =>
+  dashCard.visualization_settings?.virtual_card?.display === "action-button";
+
+export const getActionButtonEmitterId = (dashCard: DashCard) =>
+  dashCard.visualization_settings?.click_behavior?.emitter_id;
+
+export const getActionButtonActionId = (dashCard: DashCard) =>
+  dashCard.visualization_settings?.click_behavior?.action;
