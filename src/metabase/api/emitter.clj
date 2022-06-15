b9a65ae7cdf304713af5cefdea6a155c61c857cb
Cam Saul <github@camsaul.com>
2022-06-15 13:20:11 -0700

    Include newly created Emitter in POST /api/emitter response

--- src/metabase/api/emitter.clj
+++ src/metabase/api/emitter.clj
@@ -14,31 +14,6 @@
    [toucan.db :as db]
    [toucan.hydrate :refer [hydrate]]))
 
-(api/defendpoint POST "/"
-  "Endpoint to create an emitter."
-  [:as {emitter :body}]
-  (cond
-    (:dashboard_id emitter)
-    (db/insert! DashboardEmitter emitter)
-
-    (:card_id emitter)
-    (db/insert! CardEmitter emitter)
-
-    :else
-    (throw (ex-info (tru "Unknown emitter type") emitter))))
-
-(api/defendpoint PUT "/:emitter-id"
-  "Endpoint to update an emitter."
-  [emitter-id :as {emitter :body}]
-  (db/update! Emitter emitter-id emitter)
-  api/generic-204-no-content)
-
-(api/defendpoint DELETE "/:emitter-id"
-  "Endpoint to delete an emitter."
-  [emitter-id]
-  (db/delete! Emitter :id emitter-id)
-  api/generic-204-no-content)
-
 (defn- emitter
   "Fetch the Emitter with `emitter-id` and extra info from FK-related tables for custom Emitter execution purposes."
   [emitter-id]
@@ -53,10 +28,36 @@
                                                  [:= :card_emitter.emitter_id :emitter.id]
                                                  [DashboardEmitter :dashboard_emitter]
                                                  [:= :dashboard_emitter.emitter_id :emitter.id]]
-                                     :where     [:= :id emitter-id]})
+                                     :where     [:= :emitter.id emitter-id]})
                           (db/do-post-select Emitter)
                           first)]
     (hydrate emitter [:action :card])))
+
+(api/defendpoint POST "/"
+  "Endpoint to create an emitter."
+  [:as {:keys [body]}]
+  (let [{emitter-id :emitter_id} (cond
+                                   (:dashboard_id body)
+                                   (db/insert! DashboardEmitter body)
+
+                                   (:card_id body)
+                                   (db/insert! CardEmitter body)
+
+                                   :else
+                                   (throw (ex-info (tru "Unknown emitter type") body)))]
+    (emitter emitter-id)))
+
+(api/defendpoint PUT "/:emitter-id"
+  "Endpoint to update an emitter."
+  [emitter-id :as {emitter :body}]
+  (db/update! Emitter emitter-id emitter)
+  api/generic-204-no-content)
+
+(api/defendpoint DELETE "/:emitter-id"
+  "Endpoint to delete an emitter."
+  [emitter-id]
+  (db/delete! Emitter :id emitter-id)
+  api/generic-204-no-content)
 
 (def ^:private CustomActionParametersMap
   (-> {s/Keyword {:type     (apply s/enum (map u/qualified-name (keys mbql.s/parameter-types))) ; type will come in as a string.
