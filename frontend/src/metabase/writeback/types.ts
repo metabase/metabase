0b6315c2eae88e0fa31861a6e9b44209db6262c2
Anton Kulyk <kuliks.anton@gmail.com>
2022-06-15 21:18:42 +0100

    Add `WritebackActionEmitter` type

--- frontend/src/metabase/writeback/types.ts
+++ frontend/src/metabase/writeback/types.ts
@@ -1,5 +1,6 @@
 import Field from "metabase-lib/lib/metadata/Field";
 import { SavedCard, NativeDatasetQuery } from "metabase-types/types/Card";
+import { ParameterId, ParameterTarget } from "metabase-types/types/Parameter";
 
 export interface CategoryWidgetProps {
   field: {
@@ -19,6 +20,18 @@ export interface WritebackAction {
   id: number;
   type: "row";
   card: WritebackActionCard;
+  card_id: number;
   "updated-at": string;
   "created-at": string;
+}
+
+export interface WritebackActionEmitter {
+  id: number;
+  dashboard_id: number;
+  action: WritebackAction & {
+    emitter_id: number;
+  };
+  parameter_mappings: Record<ParameterId, ParameterTarget>;
+  updated_at: string;
+  created_at: string;
 }
