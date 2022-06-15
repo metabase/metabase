3a3ba8524870baa75e4758e7cbc20b2135edb6c0
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-15 18:57:53 +0300

    Adjust skeletons

--- frontend/src/metabase/visualizations/components/skeletons/PieSkeleton/PieSkeleton.tsx
+++ frontend/src/metabase/visualizations/components/skeletons/PieSkeleton/PieSkeleton.tsx
@@ -17,7 +17,7 @@ const PieSkeleton = ({
       <SkeletonCaption name={name} description={description} />
       <SkeletonImage
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 432 138"
+        viewBox="0 0 306 138"
         fill="none"
         preserveAspectRatio="xMidYMid"
       >
@@ -26,7 +26,7 @@ const PieSkeleton = ({
         <rect y="76" width="80" height="11" rx="5.5" fill="currentColor" />
         <rect y="102" width="80" height="11" rx="5.5" fill="currentColor" />
         <path
-          d="M 302 12 a 56.998 56.998 0 0 1 56.961 54.888 M 359 69 a 56.992 56.992 0 0 1 -16.695 40.305 A 56.986 56.986 0 0 1 302 126 M 300.011 125.965 A 56.999 56.999 0 0 1 245 69.123 M 245.035 67.01 a 57 57 0 0 1 55.373 -54.988"
+          d="M237 12a56.998 56.998 0 0 1 56.961 54.888M294 69a56.992 56.992 0 0 1-16.695 40.305A56.986 56.986 0 0 1 237 126M235.011 125.965A56.999 56.999 0 0 1 180 69.123M180.035 67.01a57 57 0 0 1 55.373-54.988"
           stroke="currentColor"
           strokeWidth="24"
         />
