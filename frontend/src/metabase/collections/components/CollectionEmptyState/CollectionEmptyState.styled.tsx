48758e2b0373062f76e7069ae08a35dca8adaa53
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-16 18:11:25 +0300

    Add empty state

---
+++ frontend/src/metabase/collections/components/CollectionEmptyState/CollectionEmptyState.styled.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+import { color } from "metabase/lib/colors";
+
+export const EmptyStateRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const EmptyStateTitle = styled.div`
+  color: ${color("text-dark")};
+  font-size: 1.5rem;
+  line-height: 2rem;
+  margin-bottom: 0.75rem;
+`;
+
+export const EmptyStateDescription = styled.div`
+  color: ${color("text-medium")};
+  font-size: 1rem;
+  line-height: 1.5rem;
+  margin-bottom: 1.5rem;
+`;
--- frontend/src/metabase/collections/components/CollectionEmptyState/CollectionEmptyState.tsx
+++ frontend/src/metabase/collections/components/CollectionEmptyState/CollectionEmptyState.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { t } from "ttag";
+import {
+  EmptyStateDescription,
+  EmptyStateRoot,
+  EmptyStateTitle,
+} from "./CollectionEmptyState.styled";
+import Button from "metabase/core/components/Button";
 
 const CollectionEmptyState = (): JSX.Element => {
-  return <div />;
+  return (
+    <EmptyStateRoot>
+      <EmptyStateTitle>{t`This collection is empty`}</EmptyStateTitle>
+      <EmptyStateDescription>{t`Use collections to organize and group dashboards and questions for your team or yourself`}</EmptyStateDescription>
+      <Button icon="add">{t`Create a new…`}</Button>
+    </EmptyStateRoot>
+  );
 };
 
 export default CollectionEmptyState;
