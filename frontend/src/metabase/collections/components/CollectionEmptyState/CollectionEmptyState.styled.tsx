836d1ebf3655faddc33bead5a16ec2425d687615
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-16 18:22:27 +0300

    Add empty state

--- frontend/src/metabase/collections/components/CollectionEmptyState/CollectionEmptyState.styled.tsx
+++ frontend/src/metabase/collections/components/CollectionEmptyState/CollectionEmptyState.styled.tsx
@@ -11,6 +11,7 @@ export const EmptyStateTitle = styled.div`
   color: ${color("text-dark")};
   font-size: 1.5rem;
   line-height: 2rem;
+  margin-top: 2.5rem;
   margin-bottom: 0.75rem;
 `;
 
@@ -19,4 +20,14 @@ export const EmptyStateDescription = styled.div`
   font-size: 1rem;
   line-height: 1.5rem;
   margin-bottom: 1.5rem;
+`;
+
+export const EmptyStateIconForeground = styled.path`
+  fill: ${color("bg-light")};
+  stroke: ${color("brand")};
+`;
+
+export const EmptyStateIconBackground = styled.path`
+  fill: ${color("brand-light")};
+  stroke: ${color("brand")};
 `;
