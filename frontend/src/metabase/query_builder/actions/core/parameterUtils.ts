5bdde140a7b9165ebbedab07e301876589e0bcd8
Dalton <daltojohnso@users.noreply.github.com>
2022-06-16 08:50:20 -0700

    Refactor card parameter utils (#23063)
    Fix unit tests
    
    Update name to match dashboard util fn name

--- frontend/src/metabase/query_builder/actions/core/parameterUtils.ts
+++ frontend/src/metabase/query_builder/actions/core/parameterUtils.ts
@@ -7,7 +7,7 @@ import { DashboardApi } from "metabase/services";
 import { setErrorPage } from "metabase/redux/app";
 import { getMetadata } from "metabase/selectors/metadata";
 
-import { getValueAndFieldIdPopulatedParametersFromCard } from "metabase/parameters/utils/cards";
+import { getCardUiParameters } from "metabase/parameters/utils/cards";
 import { hasMatchingParameters } from "metabase/parameters/utils/dashboards";
 import { getParameterValuesByIdFromQueryParams } from "metabase/parameters/utils/parameter-values";
 
@@ -100,10 +100,7 @@ export function getParameterValuesForQuestion({
   queryParams?: QueryParams;
   metadata: Metadata;
 }) {
-  const parameters = getValueAndFieldIdPopulatedParametersFromCard(
-    card,
-    metadata,
-  );
+  const parameters = getCardUiParameters(card, metadata);
   return getParameterValuesByIdFromQueryParams(
     parameters,
     queryParams,
