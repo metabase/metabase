129c7fe76d03d939008a9609731824d6dfdd577c
Callum Herries <hi@callumherries.com>
2022-06-21 18:21:37 +1200

    Fix email-api-test

--- enterprise/backend/test/metabase_enterprise/advanced_permissions/api/setting_test.clj
+++ enterprise/backend/test/metabase_enterprise/advanced_permissions/api/setting_test.clj
@@ -74,8 +74,8 @@
                                                                     :email-smtp-username "munchkin"
                                                                     :email-smtp-password "gobble gobble"
                                                                     :email-from-address  "eating@hungry.com"
-                                                                    :email-reply-to      "reply-to@hungry.com"}))))
-
+                                                                    :email-from-name     "Eating"
+                                                                    :email-reply-to      ["reply-to@hungry.com"]}))))
               (delete-email-setting [user status]
                 (testing (format "delete email setting with %s user" (mt/user-descriptor user))
                   (mt/user-http-request user :delete status "email")))
