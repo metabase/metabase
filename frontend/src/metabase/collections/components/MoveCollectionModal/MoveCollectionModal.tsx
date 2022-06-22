9241cb0e2606bd392fcee3acac2ea7c3d6731ca8
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-22 16:52:22 +0300

    Add title

---
+++ frontend/src/metabase/collections/components/MoveCollectionModal/MoveCollectionModal.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from "react";
+import CollectionMoveModal from "metabase/containers/CollectionMoveModal";
+import { Collection } from "metabase-types/api";
+
+export interface MoveCollectionModalProps {
+  collection: Collection;
+  onMove: (source: Collection, destination: Collection) => void;
+  onClose: () => void;
+}
+
+const MoveCollectionModal = ({
+  collection,
+  onMove,
+  onClose,
+}: MoveCollectionModalProps): JSX.Element => {
+  const handleMove = useCallback(
+    async (destination: Collection) => {
+      await onMove(collection, destination);
+    },
+    [collection, onMove],
+  );
+
+  return (
+    <CollectionMoveModal
+      title={`Move ${collection.name}?`}
+      initialCollectionId={collection.id}
+      onMove={handleMove}
+      onClose={onClose}
+    />
+  );
+};
+
+export default MoveCollectionModal;
