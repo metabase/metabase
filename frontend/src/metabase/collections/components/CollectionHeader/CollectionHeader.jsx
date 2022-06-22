821ba3d67df7d5ee30e2b7f454f20fb6c07c9d9e
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-22 15:06:21 +0300

    Add actions

--- frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.jsx
+++ frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.jsx
@@ -1,29 +1,15 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
-import { t } from "ttag";
-
-import * as Urls from "metabase/lib/urls";
-import { isPersonalCollection } from "metabase/collections/utils";
-import Icon, { IconWrapper } from "metabase/components/Icon";
-import Link from "metabase/core/components/Link";
+import React from "react";
 import PageHeading from "metabase/components/type/PageHeading";
-import Tooltip from "metabase/components/Tooltip";
-
-import CollectionEditMenu from "metabase/collections/components/CollectionEditMenu";
-import NewItemMenu from "metabase/containers/NewItemMenu";
-import { color } from "metabase/lib/colors";
 
 import { PLUGIN_COLLECTION_COMPONENTS } from "metabase/plugins";
-import { ANALYTICS_CONTEXT } from "metabase/collections/constants";
 
 import {
-  BookmarkIcon,
-  BookmarkIconWrapper,
   Container,
   DescriptionHeading,
-  MenuContainer,
   TitleContent,
 } from "./CollectionHeader.styled";
+import CollectionActions from "../CollectionActions";
 
 function Title({ collection }) {
   return (
@@ -48,129 +34,11 @@ function Title({ collection }) {
   );
 }
 
-function PermissionsLink({
-  collection,
-  isAdmin,
-  isPersonal,
-  isPersonalCollectionChild,
-}) {
-  const tooltip = t`Edit the permissions for this collection`;
-  const link = `${Urls.collection(collection)}/permissions`;
-
-  const canChangePermissions =
-    isAdmin && !isPersonal && !isPersonalCollectionChild;
-
-  return canChangePermissions ? (
-    <Tooltip tooltip={tooltip}>
-      <Link to={link}>
-        <IconWrapper>
-          <Icon name="lock" />
-        </IconWrapper>
-      </Link>
-    </Tooltip>
-  ) : null;
-}
-
-function TimelinesLink({ collection }) {
-  const title = t`Events`;
-  const link = Urls.timelinesInCollection(collection);
-
-  return (
-    <Tooltip tooltip={title}>
-      <Link to={link}>
-        <IconWrapper>
-          <Icon name="calendar" size={20} />
-        </IconWrapper>
-      </Link>
-    </Tooltip>
-  );
-}
-
-function EditMenu({
-  collection,
-  hasWritePermission,
-  isAdmin,
-  isPersonal,
-  isRoot,
-}) {
-  const tooltip = t`Edit collection`;
-
-  const canEditCollection = hasWritePermission && !isPersonal;
-
-  return canEditCollection ? (
-    <CollectionEditMenu
-      tooltip={tooltip}
-      collection={collection}
-      isAdmin={isAdmin}
-      isRoot={isRoot}
-    />
-  ) : null;
-}
-
-function Bookmark({ isBookmarked, onClickBookmark }) {
-  const title = isBookmarked ? t`Remove from bookmarks` : t`Bookmark`;
-  const iconColor = isBookmarked ? color("brand") : "";
-  const [animation, setAnimation] = useState(null);
-
-  const handleClickBookmark = () => {
-    onClickBookmark();
-    setAnimation(isBookmarked ? "shrink" : "expand");
-  };
-
-  return (
-    <Tooltip tooltip={title}>
-      <BookmarkIconWrapper
-        isBookmarked={isBookmarked}
-        onClick={handleClickBookmark}
-      >
-        <BookmarkIcon
-          name="bookmark"
-          color={iconColor}
-          size={20}
-          animation={animation}
-        />
-      </BookmarkIconWrapper>
-    </Tooltip>
-  );
-}
-
-function Menu(props) {
-  const { collectionId, hasWritePermission } = props;
-
-  const shouldBeBookmarkable = collectionId !== "root";
-
-  return (
-    <MenuContainer data-testid="collection-menu">
-      {hasWritePermission && (
-        <NewItemMenu
-          {...props}
-          collectionId={collectionId}
-          triggerIcon="add"
-          triggerTooltip={t`New…`}
-          analyticsContext={ANALYTICS_CONTEXT}
-        />
-      )}
-      <EditMenu {...props} />
-      <PermissionsLink {...props} />
-      <TimelinesLink {...props} />
-      {shouldBeBookmarkable && <Bookmark {...props} />}
-    </MenuContainer>
-  );
-}
-
 export default function CollectionHeader(props) {
-  const { collection } = props;
-  const isPersonal = isPersonalCollection(collection);
-  const hasWritePermission = collection && collection.can_write;
-
   return (
     <Container>
       <Title {...props} />
-      <Menu
-        {...props}
-        isPersonal={isPersonal}
-        hasWritePermission={hasWritePermission}
-      />
+      <CollectionActions {...props} />
     </Container>
   );
 }
