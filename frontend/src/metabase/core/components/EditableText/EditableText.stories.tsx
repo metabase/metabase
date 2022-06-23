94e351aaca6efc9b37d104d391e3844fbc1a3810
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-23 15:45:32 +0300

    Move editable text

--- frontend/src/metabase/core/components/EditableText/EditableText.stories.tsx
+++ frontend/src/metabase/core/components/EditableText/EditableText.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React from "react";
 import { ComponentStory } from "@storybook/react";
 import { useArgs } from "@storybook/client-api";
 import EditableText from "./EditableText";
@@ -11,8 +11,8 @@ export default {
 const Template: ComponentStory<typeof EditableText> = args => {
   const [{ value }, updateArgs] = useArgs();
 
-  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    updateArgs({ value: event.target.value });
+  const handleChange = (value: string) => {
+    updateArgs({ value });
   };
 
   return <EditableText {...args} value={value} onChange={handleChange} />;
