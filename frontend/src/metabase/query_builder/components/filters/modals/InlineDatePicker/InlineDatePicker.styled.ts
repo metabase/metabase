56357c0423472d51ae7e916b62fcc92c8798bd83
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2022-06-23 17:09:18 -0600

    Inline Date Filters (#23464)
    Add Inline Date picker to bulk filter modal

---
+++ frontend/src/metabase/query_builder/components/filters/modals/InlineDatePicker/InlineDatePicker.styled.ts
@@ -0,0 +1,50 @@
+import styled from "@emotion/styled";
+import { space } from "metabase/styled-components/theme";
+import { color, alpha } from "metabase/lib/colors";
+
+import Button from "metabase/core/components/Button";
+
+export const OptionContainer = styled.div`
+  grid-column: span 2;
+  margin: ${space(2)} 0;
+  padding-bottom: ${space(2)};
+  font-weight: bold;
+  border-bottom: 1px solid ${color("border")};
+`;
+
+type OptionButtonProps = {
+  primaryColor?: string;
+  selected?: boolean;
+};
+
+export const OptionButton = styled(Button)<OptionButtonProps>`
+  border-color: ${({ selected }) =>
+    selected ? color("brand") : color("border")};
+  border-radius: ${space(1)};
+  margin-right: ${space(1)};
+  margin-bottom: ${space(1)};
+
+  background-color: ${({ selected }) =>
+    selected ? alpha("brand", 0.3) : color("white")};
+  color: ${({ selected, primaryColor = color("brand") }) =>
+    selected ? primaryColor : color("text-dark")};
+
+  padding-top: ${space(1)};
+  padding-bottom: ${space(1)};
+
+  &:hover {
+    background-color: ${({ selected }) =>
+      selected ? alpha("brand", 0.3) : color("white")};
+    border-color: ${color("brand")};
+  }
+`;
+
+export const ClearButton = styled.span`
+  color: ${alpha("brand", 0.5)};
+  margin-left: ${space(1)};
+  cursor: pointer;
+
+  &:hover {
+    color: ${color("brand")};
+  }
+`;
---
+++ frontend/src/metabase/query_builder/components/filters/modals/InlineDatePicker/InlineDatePicker.tsx
@@ -0,0 +1,115 @@
+import React, { useMemo, useCallback } from "react";
+import _ from "underscore";
+import { t } from "ttag";
+
+import Filter from "metabase-lib/lib/queries/structured/Filter";
+import { Filter as FilterExpression } from "metabase-types/types/Query";
+
+import Icon from "metabase/components/Icon";
+
+import StructuredQuery from "metabase-lib/lib/queries/StructuredQuery";
+import Dimension from "metabase-lib/lib/Dimension";
+
+import { OPTIONS } from "metabase/query_builder/components/filters/pickers/DatePicker/DatePickerShortcuts";
+
+import { BulkFilterSelect } from "../BulkFilterSelect";
+import {
+  OptionButton,
+  OptionContainer,
+  ClearButton,
+} from "./InlineDatePicker.styled";
+
+const DATE_SHORTCUT_OPTIONS = [
+  OPTIONS.DAY_OPTIONS[0], // Today
+  OPTIONS.DAY_OPTIONS[1], // Yesterday
+  OPTIONS.DAY_OPTIONS[2], // Last Week
+  OPTIONS.MONTH_OPTIONS[0], // Last Month
+];
+
+interface InlineDatePickerProps {
+  query: StructuredQuery;
+  filter?: Filter;
+  newFilter: Filter;
+  dimension: Dimension;
+  onChange: (newFilter: Filter) => void;
+  onClear: () => void;
+}
+
+export function InlineDatePicker({
+  query,
+  filter,
+  newFilter,
+  dimension,
+  onChange,
+  onClear,
+}: InlineDatePickerProps) {
+  const selectedFilterIndex = useMemo(() => {
+    if (!filter) {
+      return null;
+    }
+    const optionIndex = DATE_SHORTCUT_OPTIONS.findIndex(({ init }) =>
+      _.isEqual(filter, init(filter)),
+    );
+    return optionIndex !== -1 ? optionIndex : null;
+  }, [filter]);
+
+  const handleShortcutChange = (init: (filter: Filter) => FilterExpression) => {
+    onChange(new Filter(init(filter ?? newFilter), null, query));
+  };
+
+  const handleClear = useCallback(
+    e => {
+      e.stopPropagation();
+      onClear();
+    },
+    [onClear],
+  );
+
+  const shouldShowShortcutOptions = !filter || selectedFilterIndex !== null;
+
+  return (
+    <OptionContainer
+      data-testid="date-picker"
+      aria-label={dimension?.field()?.displayName()}
+    >
+      {shouldShowShortcutOptions &&
+        DATE_SHORTCUT_OPTIONS.map(({ displayName, init }, index) => (
+          <OptionButton
+            key={displayName}
+            selected={index === selectedFilterIndex}
+            onClick={
+              index === selectedFilterIndex
+                ? onClear
+                : () => handleShortcutChange(init)
+            }
+          >
+            <span aria-selected={index === selectedFilterIndex}>
+              {displayName}
+            </span>
+          </OptionButton>
+        ))}
+
+      <BulkFilterSelect
+        query={query}
+        filter={filter}
+        dimension={dimension}
+        handleChange={onChange}
+        handleClear={onClear}
+        customTrigger={({ onClick }) =>
+          filter && selectedFilterIndex === null ? (
+            <OptionButton selected onClick={onClick}>
+              <span>{filter.displayName({ includeDimension: false })}</span>
+              <ClearButton onClick={handleClear}>
+                <Icon name="close" size={12} />
+              </ClearButton>
+            </OptionButton>
+          ) : (
+            <OptionButton onClick={onClick} aria-label={t`more options`}>
+              <Icon name="ellipsis" size={14} />
+            </OptionButton>
+          )
+        }
+      />
+    </OptionContainer>
+  );
+}
