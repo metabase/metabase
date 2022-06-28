e915d1423094e1d2286d450a8bd5235b6d1d34c8
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-06-28 20:38:09 +0300

    Add desktop version

--- frontend/src/metabase/nav/components/AppBar/AppBarDesktop.styled.tsx
+++ frontend/src/metabase/nav/components/AppBar/AppBarDesktop.styled.tsx
@@ -1,7 +1,8 @@
 import styled from "@emotion/styled";
 import { color } from "metabase/lib/colors";
 import { APP_BAR_HEIGHT } from "metabase/nav/constants";
-import { LogoLink, SidebarButton } from "./AppBarLogo.styled";
+import { LogoLink } from "./AppBarLogo.styled";
+import { SidebarButton } from "./AppBarToggle.styled";
 
 export const AppBarRoot = styled.header`
   display: flex;
