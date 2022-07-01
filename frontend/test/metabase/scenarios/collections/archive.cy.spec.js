028cfcbbfff1d621a11133fdfd12e69e2abfb580
Gustavo Saiani <gustavo@poe.ma>
2022-07-01 13:22:39 -0300

    Use getCollectionIdFromSlug

--- frontend/test/metabase/scenarios/collections/archive.cy.spec.js
+++ frontend/test/metabase/scenarios/collections/archive.cy.spec.js
@@ -1,15 +1,17 @@
-import { restore, visitArchivedQuestion } from "__support__/e2e/helpers";
+import {
+  getCollectionIdFromSlug,
+  restore,
+  visitArchivedQuestion,
+} from "__support__/e2e/helpers";
 import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
 
 const { PEOPLE_ID } = SAMPLE_DATABASE;
 
-const FIRST_COLLECTION_ID = 9;
-
-const questionDetails = {
+const getQuestionDetails = collectionId => ({
   name: "A question",
   query: { "source-table": PEOPLE_ID },
-  collection_id: FIRST_COLLECTION_ID,
-};
+  collection_id: collectionId,
+});
 
 describe("scenarios > collections > archive", () => {
   beforeEach(() => {
@@ -18,11 +20,15 @@ describe("scenarios > collections > archive", () => {
   });
 
   it("shows correct page when visiting page of question that was in archived collection", () => {
-    cy.createQuestion(questionDetails).then(({ body: { id } }) => {
-      cy.request("PUT", `/api/collection/${FIRST_COLLECTION_ID}`, {
-        archived: true,
+    getCollectionIdFromSlug("first_collection", collectionId => {
+      const questionDetails = getQuestionDetails(collectionId);
+
+      cy.createQuestion(questionDetails).then(({ body: { id } }) => {
+        cy.request("PUT", `/api/collection/${collectionId}`, {
+          archived: false,
+        });
+        visitArchivedQuestion(id);
       });
-      visitArchivedQuestion(id);
     });
   });
 });
