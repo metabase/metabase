f02c6c6b7c767ad52c6012ce0e1f61a4927b8b72
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-01 14:30:49 +0300

    Remove lodash

--- enterprise/frontend/src/metabase-enterprise/whitelabel/components/ChartColorPreview/utils.ts
+++ enterprise/frontend/src/metabase-enterprise/whitelabel/components/ChartColorPreview/utils.ts
@@ -1,11 +1,11 @@
 import { color } from "metabase/lib/colors";
-import { times } from "lodash";
+import _ from "underscore";
 
 export const getAccentColorGroups = (palette: Record<string, string>) => {
   const groups = [
-    times(8, i => [`accent${i}`]),
-    times(8, i => [`accent${i}`, `accent${i}-dark`]),
-    times(8, i => [`accent${i}`, `accent${i}-light`, `accent${i}-dark`]),
+    _.times(8, i => [`accent${i}`]),
+    _.times(8, i => [`accent${i}`, `accent${i}-dark`]),
+    _.times(8, i => [`accent${i}`, `accent${i}-light`, `accent${i}-dark`]),
   ];
 
   return groups.map(group => group.flat().map(name => color(name, palette)));
