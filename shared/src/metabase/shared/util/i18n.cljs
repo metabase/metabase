721303e1af8d9fa290ff8c6e5185aed0661a4af5
Noah Moss <noahbmoss@gmail.com>
2022-07-01 11:22:08 -0400

    helper fn to temporarily use site language for translation

--- shared/src/metabase/shared/util/i18n.cljs
+++ shared/src/metabase/shared/util/i18n.cljs
@@ -1,5 +1,6 @@
 (ns metabase.shared.util.i18n
   (:require ["ttag" :as ttag])
+  (:require ["metabase/lib/i18n" :as i18n])
   (:require-macros metabase.shared.util.i18n))
 
 (comment metabase.shared.util.i18n/keep-me
@@ -8,4 +9,5 @@
 (defn js-i18n
   "Format an i18n `format-string` with `args` with a translated string in the user locale."
   [format-string & args]
+  (i18n/withInstanceLanguage (fn [] (js/console.log "asdf")))
   (apply ttag/gettext format-string args))
