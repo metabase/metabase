4024b3fe99eee1c8dd991566276996242ccb3a27
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-04 16:49:42 +0300

    Add global styles

--- frontend/test/metabase-visual/admin/fonts.cy.spec.js
+++ frontend/test/metabase-visual/admin/fonts.cy.spec.js
@@ -1,25 +1,19 @@
 import { restore, describeEE } from "__support__/e2e/helpers";
 
-describeEE.skip("visual tests > admin > fonts", () => {
+describeEE("visual tests > admin > fonts", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+    cy.intercept("GET", "/app/fonts/**").as("getFont");
   });
 
   it("should reload with new fonts", () => {
     cy.visit("/admin/settings/whitelabel");
     cy.findByText("Font");
     cy.percySnapshot("before-font");
-    //Weird way to test that page reloads
-    cy.window().then(w => (w.beforeReload = true));
-    cy.window().should("have.prop", "beforeReload", true);
-    cy.findByTestId("application-font-select-button").click();
-    cy.findByText("Roboto Mono").click();
 
-    // Ensure page reloaded
-    cy.window().should("not.have.prop", "beforeReload");
-    cy.findByText("Font");
-    cy.findByText("Roboto Mono");
+    cy.findByText("Roboto Mono").click();
+    cy.wait("@getFont");
     cy.percySnapshot("after-font");
   });
 });
