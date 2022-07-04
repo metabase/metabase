d728a3c283a34791ddb7f097b7348151855ae7d3
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-04 19:38:01 +0300

    Add cypress test

--- frontend/test/metabase-visual/admin/fonts.cy.spec.js
+++ frontend/test/metabase-visual/admin/fonts.cy.spec.js
@@ -1,4 +1,11 @@
-import { restore, describeEE } from "__support__/e2e/helpers";
+import {
+  restore,
+  describeEE,
+  typeAndBlurUsingLabel,
+} from "__support__/e2e/helpers";
+
+const CUSTOM_FONT_URL =
+  "https://fonts.gstatic.com/s/robotomono/v21/L0xuDF4xlVMF-BfR8bXMIhJHg45mwgGEFl0_3vq_ROW-AJi8SJQt.woff";
 
 describeEE("visual tests > admin > fonts", () => {
   beforeEach(() => {
@@ -7,17 +14,31 @@ describeEE("visual tests > admin > fonts", () => {
     cy.intercept("GET", "/app/fonts/**").as("getFont");
   });
 
-  it("should reload with new fonts", () => {
+  it("should set a built-in font", () => {
     cy.visit("/admin/settings/whitelabel");
-    cy.findByText("Font");
-    cy.percySnapshot("before-font");
 
     cy.findByText("Lato").click();
     cy.findByText("Roboto Mono").click();
-    cy.document()
-      .its("fonts")
-      .invoke("check", "14px Roboto Mono")
-      .should("be.true");
-    cy.percySnapshot("after-font");
+    waitForFont("Roboto Mono");
+
+    cy.percySnapshot();
+  });
+
+  it("should set a custom font", () => {
+    cy.visit("/admin/settings/whitelabel");
+
+    cy.findByText("Lato").click();
+    cy.findByText("Custom…").click();
+    typeAndBlurUsingLabel("Regular", CUSTOM_FONT_URL);
+    waitForFont("Custom");
+
+    cy.percySnapshot();
   });
 });
+
+const waitForFont = font => {
+  cy.document()
+    .its("fonts")
+    .invoke("check", `14px ${font}`)
+    .should("be.true");
+};
