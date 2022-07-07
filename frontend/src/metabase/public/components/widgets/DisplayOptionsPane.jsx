310ef009163d287ac2f0ee30b181ba5cb5b04d5e
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-07 16:15:44 +0300

    Fix copy

--- frontend/src/metabase/public/components/widgets/DisplayOptionsPane.jsx
+++ frontend/src/metabase/public/components/widgets/DisplayOptionsPane.jsx
@@ -84,7 +84,7 @@ const DisplayOptionsPane = ({
               value={displayOptions.font}
               options={[
                 {
-                  name: t`Use default`,
+                  name: t`Use instance font`,
                   value: null,
                 },
                 ...MetabaseSettings.get("available-fonts").map(font => ({
