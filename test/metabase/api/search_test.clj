6852378d1e826549723652b5568d7dc7cf30d8f2
Cal Herries <39073188+calherries@users.noreply.github.com>
2022-07-07 18:02:13 +1200

    Fix databases with no permission being shown with search results (#23678)
    * Update database search query with permissions filter
    
    * Add unit test
    
    * Remove accidentally committed code
    
    * Tidy query
    
    * Refactor query
    
    * Use `check-permissions-for-model` instead for database instead of checking permissions with SQL
    
    * Add extra test before revoking perms

--- test/metabase/api/search_test.clj
+++ test/metabase/api/search_test.clj
@@ -379,7 +379,20 @@
                                  :name     "test segment"}]]
       (perms/revoke-data-perms! (perms-group/all-users) db-id)
       (is (= []
-             (search-request-data :rasta :q "test"))))))
+             (search-request-data :rasta :q "test")))))
+
+  (testing "Databases for which the user does not have access to should not show up in results"
+    (mt/with-temp* [Database [db-1 {:name "db-1"}]
+                    Database [_db-2 {:name "db-2"}]]
+      (is (= #{"db-2" "db-1"}
+             (->> (search-request-data-with sorted-results :rasta :q "db")
+                  (map :name)
+                  set)))
+      (perms/revoke-data-perms! (perms-group/all-users) (:id db-1))
+      (is (= #{"db-2"}
+             (->> (search-request-data-with sorted-results :rasta :q "db")
+                  (map :name)
+                  set))))))
 
 (deftest bookmarks-test
   (testing "Bookmarks are per user, so other user's bookmarks don't cause search results to be altered"
