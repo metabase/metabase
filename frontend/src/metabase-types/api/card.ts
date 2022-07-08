7b979a8c2a0918f9ea0a8b32b73d17f3cb30f4d0
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-08 16:41:20 +0300

    Add question last cached time info (#23787)

--- frontend/src/metabase-types/api/card.ts
+++ frontend/src/metabase-types/api/card.ts
@@ -1,3 +1,21 @@
+import { DatasetQuery } from "./query";
+
+export interface Card extends UnsavedCard {
+  id: CardId;
+  name: string;
+  description: string | null;
+  dataset: boolean;
+  can_write: boolean;
+  cache_ttl: number | null;
+  last_query_start: string | null;
+}
+
+export interface UnsavedCard {
+  display: string;
+  dataset_query: DatasetQuery;
+  visualization_settings: VisualizationSettings;
+}
+
 export type VisualizationSettings = {
   [key: string]: any;
 };
@@ -9,4 +27,5 @@ export interface ModerationReview {
   most_recent: boolean;
 }
 
+export type CardId = number;
 export type ModerationReviewStatus = "verified";
