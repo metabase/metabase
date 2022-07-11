59278f1cad8196fdac028b6612f98a9bf571c0b6
Callum Herries <hi@callumherries.com>
2022-07-11 19:14:38 +1200

    Delete test file

--- enterprise/backend/test/metabase_enterprise/sandbox/api/user_test.clj
+++
@@ -1,3 +0,0 @@
-(ns metabase-enterprise.sandbox.api.user-test
-  "Tests that would logically be included in `metabase.api.user-test` but are separate as they are enterprise only."
-  (:require [clojure.test :refer :all]))
