b0945167778f3c4edf6adf48b22db4d8eba1a0b7
Gustavo Saiani <gustavo@poe.ma>
2022-07-11 13:41:40 -0300

    Add func as propType to reload

--- frontend/src/metabase/entities/containers/EntityListLoader.jsx
+++ frontend/src/metabase/entities/containers/EntityListLoader.jsx
@@ -14,7 +14,7 @@ import { capitalize } from "metabase/lib/formatting";
 const propTypes = {
   entityType: PropTypes.string,
   entityQuery: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  reload: PropTypes.bool,
+  reload: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
   reloadInterval: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
   wrapped: PropTypes.bool,
   debounced: PropTypes.bool,
