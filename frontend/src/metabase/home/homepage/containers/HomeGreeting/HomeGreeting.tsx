dbe4345220e3cde9e320e6ee596984c0b46b4ab8
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-13 22:22:10 +0300

    Fix behavior for settings without the default value

--- frontend/src/metabase/home/homepage/containers/HomeGreeting/HomeGreeting.tsx
+++ frontend/src/metabase/home/homepage/containers/HomeGreeting/HomeGreeting.tsx
@@ -5,7 +5,7 @@ import HomeGreeting from "../../components/HomeGreeting";
 
 const mapStateToProps = (state: State) => ({
   user: getUser(state),
-  showLogo: state.settings.values["show-metabot"],
+  showLogo: state.settings.values["show-metabot"] ?? true,
 });
 
 export default connect(mapStateToProps)(HomeGreeting);
