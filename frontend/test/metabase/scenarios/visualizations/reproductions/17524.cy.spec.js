83d3128baf679385eaf6fcbf00396c87140b1d12
Ryan Laurie <iethree@gmail.com>
2022-07-13 18:15:53 -0600

    more test fixes

--- frontend/test/metabase/scenarios/visualizations/reproductions/17524.cy.spec.js
+++ frontend/test/metabase/scenarios/visualizations/reproductions/17524.cy.spec.js
@@ -1,4 +1,9 @@
-import { restore, filterWidget, filter } from "__support__/e2e/helpers";
+import {
+  restore,
+  filterWidget,
+  filter,
+  filterField,
+} from "__support__/e2e/helpers";
 import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
 
 const { PRODUCTS, PRODUCTS_ID } = SAMPLE_DATABASE;
@@ -70,12 +75,11 @@ describe("issue 17524", () => {
 
       filter();
 
-      cy.findByText("ID").click();
-      cy.findByText("Is").click();
-      cy.findByText("Greater than").click();
-
-      cy.findByPlaceholderText("Enter an ID").type("1");
-      cy.button("Apply").click();
+      filterField("ID", {
+        operator: "Greater than",
+        value: "1",
+      });
+      cy.findByTestId("apply-filters").click();
 
       cy.get("polygon");
     });
