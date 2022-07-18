988e8e399d4961f0a315e5f8f931e28b68c9d558
Anton Kulyk <kuliks.anton@gmail.com>
2022-07-18 15:39:18 +0100

    Run prettier

--- frontend/src/metabase/query_builder/actions/core/metadata.ts
+++ frontend/src/metabase/query_builder/actions/core/metadata.ts
@@ -8,17 +8,15 @@ import Question from "metabase-lib/lib/Question";
 import { Card } from "metabase-types/types/Card";
 import { Dispatch, GetState } from "metabase-types/store";
 
-export const loadMetadataForCard = (card: Card) => (
-  dispatch: Dispatch,
-  getState: GetState,
-) => {
-  const metadata = getMetadata(getState());
-  const question = new Question(card, metadata);
-  const queries = [question.query()];
-  if (question.isDataset()) {
-    queries.push(question.composeDataset().query());
-  }
-  return dispatch(
-    loadMetadataForQueries(queries, question.dependentMetadata()),
-  );
-};
+export const loadMetadataForCard =
+  (card: Card) => (dispatch: Dispatch, getState: GetState) => {
+    const metadata = getMetadata(getState());
+    const question = new Question(card, metadata);
+    const queries = [question.query()];
+    if (question.isDataset()) {
+      queries.push(question.composeDataset().query());
+    }
+    return dispatch(
+      loadMetadataForQueries(queries, question.dependentMetadata()),
+    );
+  };
