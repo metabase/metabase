82efbe0bade8893fb751e2ad0b3ecda6eee82ada
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2022-07-19 02:22:06 +0200

    Refactor `backend.js` #2 - remove unused code (#24062)
    * Remove unused and non-existent ENV
    
    * Export only resources actually used elsewhere
    
    * Remove the default callback implementation
    
    * Remove unused Cypress app db fixture
    
    * Remove `DEFAULT_DB_KEY`
    
    We're never ever going to use `test_db_fixture.db` to spin a Cypress instance up.
    That was the only reason why `DEFAULT_DB_KEY` existed in the first place.
    
    In order to fall back to using that fixture app db `dbKey` would have to be `undefined` or `null`.
    
    * Simplify `get` method
    
    Get the `dbKey` inline, rather than having to pass another callback
    with the separate logic.
    
    * Remove obsolete filter flag (see #19378)
    
    * Remove unused Java flag
    
    * Bring back `MB_USER_DEFAULTS`
    
    This reverts 70ea7bedddb5cc1e221cff934c7c939e96bb43c8
    
    * Link the PR that explains the origin of `MB_USER_DEFAULTS`

--- frontend/test/__runner__/backend.js
+++ frontend/test/__runner__/backend.js
@@ -7,8 +7,6 @@ const { spawn } = require("child_process");
 
 const fetch = require("isomorphic-fetch");
 
-const DEFAULT_DB_KEY = "/test_db_fixture.db";
-
 let testDbId = 0;
 const getDbFile = () =>
   path.join(os.tmpdir(), `metabase-test-${process.pid}-${testDbId++}.db`);
@@ -17,10 +15,7 @@ let port = 4000;
 const getPort = () => port++;
 
 const BackendResource = createSharedResource("BackendResource", {
-  getKey({ dbKey = DEFAULT_DB_KEY }) {
-    return dbKey || {};
-  },
-  create({ dbKey = DEFAULT_DB_KEY }) {
+  create({ dbKey }) {
     const dbFile = getDbFile();
     const absoluteDbKey = dbKey ? __dirname + dbKey : dbFile;
     const e2eHost = process.env["E2E_HOST"];
@@ -51,7 +46,6 @@ const BackendResource = createSharedResource("BackendResource", {
         [
           "-XX:+IgnoreUnrecognizedVMOptions", // ignore options not recognized by this Java version (e.g. Java 8 should ignore Java 9 options)
           "-Dh2.bindAddress=localhost", // fix H2 randomly not working (?)
-          // "-Xmx2g", // Hard limit of 2GB size for the heap since Circle is dumb and the JVM tends to go over the limit
           "-Djava.awt.headless=true", // when running on macOS prevent little Java icon from popping up in Dock
           "-Duser.timezone=US/Pacific",
           `-Dlog4j.configurationFile=file:${__dirname}/log4j2.xml`,
@@ -69,7 +63,6 @@ const BackendResource = createSharedResource("BackendResource", {
               (process.env["MB_EDITION"] === "ee" &&
                 process.env["ENTERPRISE_TOKEN"]) ||
               undefined,
-            MB_FIELD_FILTER_OPERATORS_ENABLED: "true",
             MB_USER_DEFAULTS: JSON.stringify({
               token: "123456",
               user: {
@@ -139,13 +132,7 @@ async function isReady(host) {
 
 function createSharedResource(
   resourceName,
-  {
-    defaultOptions,
-    getKey = options => JSON.stringify(options),
-    create = options => ({}),
-    start = resource => {},
-    stop = resource => {},
-  },
+  { defaultOptions, create, start, stop },
 ) {
   const entriesByKey = new Map();
   const entriesByResource = new Map();
@@ -163,7 +150,8 @@ function createSharedResource(
 
   return {
     get(options = defaultOptions) {
-      const key = getKey(options);
+      const dbKey = options;
+      const key = dbKey || {};
       let entry = entriesByKey.get(key);
       if (!entry) {
         entry = {
@@ -195,4 +183,4 @@ function delay(duration) {
   return new Promise((resolve, reject) => setTimeout(resolve, duration));
 }
 
-module.exports = { DEFAULT_DB_KEY, BackendResource, isReady };
+module.exports = BackendResource;
