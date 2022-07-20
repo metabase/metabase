f97c1ea09402cf7e375a233161fcbb354878141c
Mahatthana (Kelvin) Nomsawadi <mahatthana.n@gmail.com>
2022-07-20 17:07:47 +0700

    Fix SSO users can't change language setting (#24091)
    * Fix changing locale doesn't reload page
    
    * Fix SSO users can't set language
    
    * Address Alexander's feedback

--- frontend/src/metabase/entities/users/forms.js
+++ frontend/src/metabase/entities/users/forms.js
@@ -84,20 +84,18 @@ export default {
       ...PLUGIN_ADMIN_USER_FORM_FIELDS,
     ],
   },
-  user: {
-    fields: user => {
-      const isSsoUser = !PLUGIN_IS_PASSWORD_USER.every(predicate =>
-        predicate(user),
-      );
+  user: user => {
+    const isSsoUser = !PLUGIN_IS_PASSWORD_USER.every(predicate =>
+      predicate(user),
+    );
+    const fields = isSsoUser
+      ? [getLocaleField()]
+      : [...getNameFields(), getEmailField(), getLocaleField()];
 
-      if (isSsoUser) {
-        return [getLocaleField()];
-      }
-
-      // password user
-      return [...getNameFields(), getEmailField(), getLocaleField()];
-    },
-    disablePristineSubmit: true,
+    return {
+      fields,
+      disablePristineSubmit: true,
+    };
   },
   setup: () => ({
     fields: [
