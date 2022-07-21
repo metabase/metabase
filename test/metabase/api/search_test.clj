d3b5f25d760327678aca647cd10495160eb0abd0
Callum Herries <hi@callumherries.com>
2022-07-21 12:20:46 +1200

    Rewrite test

--- test/metabase/api/search_test.clj
+++ test/metabase/api/search_test.clj
@@ -597,10 +597,18 @@
               (is (= nil
                      (search-for-pulses pulse))))))))))
 
-(deftest non-native-card-dataset-query-test
-  (testing "You shouldn't see non-native Cards in the search results that match the keys of the dataset_query"
+(deftest card-dataset-query-test
+  (testing "The dataset_query field should only be searched if the card's query is native SQL"
     ;; https://github.com/metabase/metabase/issues/24132
-    (mt/with-temp Card [card {:name "Round Table" :query_type "query" :dataset_query {:query "select * from round_table"}}]
-      (do-test-users [user [:rasta]]
-         (is (= []
-                (search-request-data user :q "database")))))))
+    (mt/with-temp* [Card [query-card {:name          "Venues Count"
+                                      :query_type    "query"
+                                      :dataset_query (mt/mbql-query venues {:aggregation [[:count]]})}]
+                    Card [native-card {:name          "Another SQL query"
+                                       :query_type    "native"
+                                       :dataset_query {:query "SELECT COUNT(1) AS aggregation FROM venues"}}]]
+      (is (= [(make-result "Another SQL query"
+                           :model "card"
+                           :bookmark false
+                           :dashboardcard_count 0
+                           :dataset_query {:query "SELECT COUNT(1) AS aggregation FROM venues"})]
+             (search-request-data :rasta :q "aggregation"))))))
