8a38ad9ad117940f477a8bc26789652eb02e9398
Anton Kulyk <kuliks.anton@gmail.com>
2022-07-21 16:14:05 +0100

    Remove tailwind from HTTP headers tab

--- frontend/src/metabase/writeback/components/HttpAction/HttpHeaderTab.tsx
+++ frontend/src/metabase/writeback/components/HttpAction/HttpHeaderTab.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import cx from "classnames";
 import { t } from "ttag";
-import { assoc, dissoc } from "icepick";
-import Icon from "metabase/components/Icon";
+import { assoc } from "icepick";
 
 import {
   Input,
   Grid,
   LeftHeader,
   RightHeader,
-  ValueColumn,
+  HeaderRow,
   DeleteButton,
   AddButton,
   Title,
+  TitleRowContainer,
 } from "./HttpHeaderTab.styled";
 
 export type Headers = {
@@ -30,17 +29,13 @@ const HttpHeaderTab: React.FC<Props> = ({ headers, setHeaders }: Props) => {
     setHeaders([...headers, { key: "", value: "" }]);
   };
   return (
-    <Grid className="grid grid-cols-2">
-      <LeftHeader className="py-2 pl-6 text-sm font-semibold">{t`Name`}</LeftHeader>
-      <RightHeader className="flex justify-between h-full text-sm font-semibold align-center">
+    <Grid>
+      <LeftHeader>
+        <Title>{t`Name`}</Title>
+      </LeftHeader>
+      <RightHeader>
         <Title>{t`Value`}</Title>
-
-        <AddButton
-          className="h-full px1 bg-content hover:bg-brand hover:bg-opacity-25 hover:text-brand"
-          onClick={add}
-        >
-          <Icon name="add" />
-        </AddButton>
+        <AddButton primary icon="add" onlyIcon onClick={add} />
       </RightHeader>
       {headers.map(({ key, value }, index) => {
         const setKey = (key: string) =>
@@ -51,28 +46,21 @@ const HttpHeaderTab: React.FC<Props> = ({ headers, setHeaders }: Props) => {
           setHeaders(assoc(headers, index, false).filter(Boolean));
         return (
           <>
-            <Header
-              className="pl-6"
-              key={`${index}-key`}
-              placeholder={t`Header Name`}
-              value={key}
-              setValue={setKey}
-            />
-            <ValueColumn className="flex justify-between align-center">
+            <LeftHeader>
               <Header
-                className="pl-0 pr-6"
-                key={`${index}-value`}
+                placeholder={t`Header Name`}
+                value={key}
+                setValue={setKey}
+              />
+            </LeftHeader>
+            <RightHeader>
+              <Header
                 placeholder={t`Value`}
                 value={value}
                 setValue={setValue}
               />
-              <DeleteButton
-                className="h-full px1 bg-content hover:bg-brand hover:bg-opacity-25 hover:text-brand"
-                onClick={remove}
-              >
-                <Icon name="trash" />
-              </DeleteButton>
-            </ValueColumn>
+              <DeleteButton icon="trash" onlyIcon onClick={remove} />
+            </RightHeader>
           </>
         );
       })}
@@ -81,24 +69,18 @@ const HttpHeaderTab: React.FC<Props> = ({ headers, setHeaders }: Props) => {
 };
 
 type InputProps = {
-  className?: string;
   value: string;
   placeholder: string;
   setValue: (value: string) => void;
 };
 
 const Header: React.FC<InputProps> = ({
-  className,
   value,
   setValue,
   placeholder,
 }: InputProps) => {
   return (
     <Input
-      className={cx(
-        "w-full px1 py-1  text-gray-500 bg-opacity-25 border-transparent placeholder-text-light bg-border focus:ring-transparent focus:border-transparent sm:text-sm",
-        className,
-      )}
       placeholder={placeholder}
       value={value}
       onChange={e => setValue(e.target.value)}
