d11c763fe089e0d35e60146a3ee0accf233855e0
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-21 16:48:17 +0300

    Fix accent7

--- frontend/src/metabase/query_builder/components/DimensionList.styled.tsx
+++ frontend/src/metabase/query_builder/components/DimensionList.styled.tsx
@@ -4,6 +4,6 @@ import { alpha } from "metabase/lib/colors";
 export const FieldListGroupingTrigger = styled.div`
   display: flex;
   visibility: hidden;
-  border-left: 2px solid ${alpha("filter", 0.9)};
-  color: ${alpha("text-white", 0.9)};
+  border-left: 2px solid ${alpha("filter", 0.1)};
+  color: ${alpha("text-white", 0.5)};
 `;
