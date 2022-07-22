5e72cc85a595bb9d4c8c1090a16b2b5fae0f8e47
Anton Kulyk <kuliks.anton@gmail.com>
2022-07-22 23:53:20 +0100

    Remove not used type

--- frontend/src/metabase/containers/FormikForm/FormikFormViewAdapter.tsx
+++ frontend/src/metabase/containers/FormikForm/FormikFormViewAdapter.tsx
@@ -43,7 +43,6 @@ function getMaybeNestedValue<Value = string>(
 }
 
 interface FormikFormViewAdapterOwnProps<Values> {
-  formInitialValues: Values;
   onValuesChange: (values: Values) => void;
 }
 
