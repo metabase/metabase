9e50b918006c3a1c529c2f48953a0da2fb6df72d
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-22 13:53:01 +0300

    Migrate accent2

--- frontend/src/metabase/query_builder/components/expressions/ExpressionEditorSuggestions.styled.jsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionEditorSuggestions.styled.jsx
@@ -1,13 +1,18 @@
 import styled from "@emotion/styled";
+import { alpha, color } from "metabase/lib/colors";
+import TippyPopover from "metabase/components/Popover/TippyPopover";
 
-import { color } from "metabase/lib/colors";
+export const ExpressionPopover = styled(TippyPopover)`
+  border-color: ${alpha("accent2", 0.2)};
+  border-radius: 0;
+`;
 
-export const UlStyled = styled.ul`
+export const ExpressionList = styled.ul`
   min-width: 150px;
   overflow-y: auto;
 `;
 
-export const ListItemStyled = styled.li`
+export const ExpressionListItem = styled.li`
   padding-top: 5px;
   padding-bottom: 5px;
 
