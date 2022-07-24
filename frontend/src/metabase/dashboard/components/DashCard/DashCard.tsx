77361a3a98de75a4a31ac1ab94a42b3a9963b36d
Anton Kulyk <kuliks.anton@gmail.com>
2022-07-24 14:31:45 +0100

    Turn `DashCard` into a functional component

--- frontend/src/metabase/dashboard/components/DashCard/DashCard.tsx
+++ frontend/src/metabase/dashboard/components/DashCard/DashCard.tsx
@@ -1,5 +1,4 @@
-import React, { Component, SyntheticEvent } from "react";
-import ReactDOM from "react-dom";
+import React, { useCallback, useRef, useState } from "react";
 import cx from "classnames";
 import _ from "underscore";
 import { getIn } from "icepick";
@@ -12,6 +11,8 @@ import { IconProps } from "metabase/components/Icon";
 import { IS_EMBED_PREVIEW } from "metabase/lib/embed";
 import { SERVER_ERROR_TYPES } from "metabase/lib/errors";
 import Utils from "metabase/lib/utils";
+
+import { useOnMount } from "metabase/hooks/use-on-mount";
 
 import { isVirtualDashCard } from "metabase/dashboard/utils";
 
@@ -108,243 +109,240 @@ interface DashCardProps {
   onChangeLocation: (location: LocationDescriptor) => void;
 }
 
-function preventDragging(e: SyntheticEvent) {
+function preventDragging(e: React.SyntheticEvent) {
   e.stopPropagation();
 }
 
-class DashCard extends Component<DashCardProps, { isPreviewingCard: boolean }> {
-  async componentDidMount() {
-    const { dashcard, markNewCardSeen } = this.props;
+function DashCard({
+  dashcard,
+  dashcardData,
+  dashboard,
+  slowCards,
+  metadata,
+  parameterValues,
+  gridItemWidth,
+  totalNumGridCols,
+  mode,
+  isEditing = false,
+  isNightMode = false,
+  isFullscreen = false,
+  isMobile = false,
+  isEditingParameter,
+  clickBehaviorSidebarDashcard,
+  headerIcon,
+  onAddSeries,
+  onRemove,
+  navigateToNewCardFromDashboard,
+  markNewCardSeen,
+  showClickBehaviorSidebar,
+  onChangeLocation,
+  onUpdateVisualizationSettings,
+  onReplaceAllVisualizationSettings,
+  dispatch,
+}: DashCardProps) {
+  const [isPreviewingCard, setIsPreviewingCard] = useState(false);
+  const cardRootRef = useRef<HTMLDivElement>(null);
 
-    // HACK: way to scroll to a newly added card
+  const handlePreviewToggle = useCallback(() => {
+    setIsPreviewingCard(wasPreviewingCard => !wasPreviewingCard);
+  }, []);
+
+  useOnMount(() => {
     if (dashcard.justAdded) {
-      const element = ReactDOM.findDOMNode(this);
-      if (element && "scrollIntoView" in element) {
-        element.scrollIntoView({ block: "nearest" });
-      }
+      cardRootRef?.current?.scrollIntoView({
+        block: "nearest",
+      });
       markNewCardSeen(dashcard.id);
     }
+  });
+
+  const mainCard = {
+    ...dashcard.card,
+    visualization_settings: mergeSettings(
+      dashcard.card.visualization_settings,
+      dashcard.visualization_settings,
+    ),
+  } as SavedCard;
+
+  const cards = [mainCard].concat(dashcard.series || []);
+  const dashboardId = dashcard.dashboard_id;
+  const isEmbed = Utils.isJWT(dashboardId);
+  const series = cards.map(card => ({
+    ...getIn(dashcardData, [dashcard.id, card.id]),
+    card: card,
+    isSlow: slowCards[card.id],
+    isUsuallyFast:
+      card.query_average_duration &&
+      card.query_average_duration < DATASET_USUALLY_FAST_THRESHOLD,
+  }));
+
+  const loading =
+    !(series.length > 0 && _.every(series, s => s.data)) &&
+    !isVirtualDashCard(dashcard);
+
+  const expectedDuration = Math.max(
+    ...series.map(s => s.card.query_average_duration || 0),
+  );
+  const usuallyFast = _.every(series, s => s.isUsuallyFast);
+  const isSlow =
+    loading &&
+    _.some(series, s => s.isSlow) &&
+    (usuallyFast ? "usually-fast" : "usually-slow");
+
+  const isAccessRestricted = series.some(
+    s =>
+      s.error_type === SERVER_ERROR_TYPES.missingPermissions ||
+      s.error?.status === 403,
+  );
+
+  const errors = series.map(s => s.error).filter(e => e);
+
+  let errorMessage, errorIcon;
+  if (isAccessRestricted) {
+    errorMessage = ERROR_MESSAGE_PERMISSION;
+    errorIcon = "key";
+  } else if (errors.length > 0) {
+    if (IS_EMBED_PREVIEW) {
+      errorMessage = (errors[0] && errors[0].data) || ERROR_MESSAGE_GENERIC;
+    } else {
+      errorMessage = ERROR_MESSAGE_GENERIC;
+    }
+    errorIcon = "warning";
   }
 
-  handlePreviewToggle = () => {
-    this.setState(prevState => ({
-      isPreviewingCard: !prevState.isPreviewingCard,
-    }));
-  };
+  const parameterValuesBySlug = getParameterValuesBySlug(
+    dashboard.parameters,
+    parameterValues,
+  );
 
-  render() {
-    const {
-      dashcard,
-      dashcardData,
-      dashboard,
-      slowCards,
-      metadata,
-      parameterValues,
-      mode,
-      isEditing = false,
-      isNightMode = false,
-      isFullscreen = false,
-      isMobile = false,
-      isEditingParameter,
-      clickBehaviorSidebarDashcard,
-      headerIcon,
-      onAddSeries,
-      onRemove,
-      navigateToNewCardFromDashboard,
-    } = this.props;
+  const hideBackground =
+    !isEditing &&
+    mainCard.visualization_settings["dashcard.background"] === false;
 
-    const mainCard = {
-      ...dashcard.card,
-      visualization_settings: mergeSettings(
-        dashcard.card.visualization_settings,
-        dashcard.visualization_settings,
-      ),
-    } as SavedCard;
+  const isEditingDashboardLayout =
+    isEditing && clickBehaviorSidebarDashcard == null && !isEditingParameter;
 
-    const cards = [mainCard].concat(dashcard.series || []);
-    const dashboardId = dashcard.dashboard_id;
-    const isEmbed = Utils.isJWT(dashboardId);
-    const series = cards.map(card => ({
-      ...getIn(dashcardData, [dashcard.id, card.id]),
-      card: card,
-      isSlow: slowCards[card.id],
-      isUsuallyFast:
-        card.query_average_duration &&
-        card.query_average_duration < DATASET_USUALLY_FAST_THRESHOLD,
-    }));
+  const gridSize = { width: dashcard.sizeX, height: dashcard.sizeY };
 
-    const loading =
-      !(series.length > 0 && _.every(series, s => s.data)) &&
-      !isVirtualDashCard(dashcard);
-
-    const expectedDuration = Math.max(
-      ...series.map(s => s.card.query_average_duration || 0),
-    );
-    const usuallyFast = _.every(series, s => s.isUsuallyFast);
-    const isSlow =
-      loading &&
-      _.some(series, s => s.isSlow) &&
-      (usuallyFast ? "usually-fast" : "usually-slow");
-
-    const isAccessRestricted = series.some(
-      s =>
-        s.error_type === SERVER_ERROR_TYPES.missingPermissions ||
-        s.error?.status === 403,
-    );
-
-    const errors = series.map(s => s.error).filter(e => e);
-
-    let errorMessage, errorIcon;
-    if (isAccessRestricted) {
-      errorMessage = ERROR_MESSAGE_PERMISSION;
-      errorIcon = "key";
-    } else if (errors.length > 0) {
-      if (IS_EMBED_PREVIEW) {
-        errorMessage = (errors[0] && errors[0].data) || ERROR_MESSAGE_GENERIC;
-      } else {
-        errorMessage = ERROR_MESSAGE_GENERIC;
+  return (
+    <DashCardRoot
+      className="Card rounded flex flex-column hover-parent hover--visibility"
+      style={
+        hideBackground
+          ? { border: 0, background: "transparent", boxShadow: "none" }
+          : undefined
       }
-      errorIcon = "warning";
-    }
-
-    const parameterValuesBySlug = getParameterValuesBySlug(
-      dashboard.parameters,
-      parameterValues,
-    );
-
-    const hideBackground =
-      !isEditing &&
-      mainCard.visualization_settings["dashcard.background"] === false;
-
-    const isEditingDashboardLayout =
-      isEditing && clickBehaviorSidebarDashcard == null && !isEditingParameter;
-
-    const gridSize = { width: dashcard.sizeX, height: dashcard.sizeY };
-
-    return (
-      <DashCardRoot
-        className="Card rounded flex flex-column hover-parent hover--visibility"
-        style={
-          hideBackground
-            ? { border: 0, background: "transparent", boxShadow: "none" }
-            : undefined
-        }
+      isNightMode={isNightMode}
+      isUsuallySlow={isSlow === "usually-slow"}
+      ref={cardRootRef}
+    >
+      {isEditingDashboardLayout ? (
+        <DashboardCardActionsPanel onMouseDown={preventDragging}>
+          <DashCardActionButtons
+            series={series}
+            isLoading={loading}
+            isVirtualDashCard={isVirtualDashCard(dashcard)}
+            hasError={!!errorMessage}
+            onRemove={onRemove}
+            onAddSeries={onAddSeries}
+            onReplaceAllVisualizationSettings={
+              onReplaceAllVisualizationSettings
+            }
+            showClickBehaviorSidebar={() =>
+              showClickBehaviorSidebar(dashcard.id)
+            }
+            isPreviewing={isPreviewingCard}
+            onPreviewToggle={handlePreviewToggle}
+            dashboard={dashboard}
+          />
+        </DashboardCardActionsPanel>
+      ) : null}
+      <WrappedVisualization
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        className={cx("flex-full overflow-hidden", {
+          "pointer-events-none": isEditingDashboardLayout,
+        })}
+        classNameWidgets={isEmbed && "text-light text-medium-hover"}
+        error={errorMessage}
+        headerIcon={headerIcon}
+        errorIcon={errorIcon}
+        isSlow={isSlow}
+        expectedDuration={expectedDuration}
+        rawSeries={series}
+        showTitle
+        isFullscreen={isFullscreen}
         isNightMode={isNightMode}
-        isUsuallySlow={isSlow === "usually-slow"}
-      >
-        {isEditingDashboardLayout ? (
-          <DashboardCardActionsPanel onMouseDown={preventDragging}>
-            <DashCardActionButtons
-              series={series}
-              isLoading={loading}
-              isVirtualDashCard={isVirtualDashCard(dashcard)}
-              hasError={!!errorMessage}
-              onRemove={onRemove}
-              onAddSeries={onAddSeries}
-              onReplaceAllVisualizationSettings={
-                this.props.onReplaceAllVisualizationSettings
-              }
-              showClickBehaviorSidebar={() =>
-                this.props.showClickBehaviorSidebar(dashcard.id)
-              }
-              isPreviewing={this.state.isPreviewingCard}
-              onPreviewToggle={this.handlePreviewToggle}
-              dashboard={dashboard}
+        isDashboard
+        dispatch={dispatch}
+        dashboard={dashboard}
+        dashcard={dashcard}
+        parameterValues={parameterValues}
+        parameterValuesBySlug={parameterValuesBySlug}
+        isEditing={isEditing}
+        isPreviewing={isPreviewingCard}
+        isEditingParameter={isEditingParameter}
+        isMobile={isMobile}
+        gridSize={gridSize}
+        totalNumGridCols={totalNumGridCols}
+        actionButtons={
+          isEmbed ? (
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            <QueryDownloadWidget
+              className="m1 text-brand-hover text-light"
+              classNameClose="hover-child"
+              card={dashcard.card}
+              params={parameterValuesBySlug}
+              dashcardId={dashcard.id}
+              token={dashcard.dashboard_id}
+              icon="download"
             />
-          </DashboardCardActionsPanel>
-        ) : null}
-        <WrappedVisualization
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          className={cx("flex-full overflow-hidden", {
-            "pointer-events-none": isEditingDashboardLayout,
-          })}
-          classNameWidgets={isEmbed && "text-light text-medium-hover"}
-          error={errorMessage}
-          headerIcon={headerIcon}
-          errorIcon={errorIcon}
-          isSlow={isSlow}
-          expectedDuration={expectedDuration}
-          rawSeries={series}
-          showTitle
-          isFullscreen={isFullscreen}
-          isNightMode={isNightMode}
-          isDashboard
-          dispatch={this.props.dispatch}
-          dashboard={dashboard}
-          dashcard={dashcard}
-          parameterValues={parameterValues}
-          parameterValuesBySlug={parameterValuesBySlug}
-          isEditing={isEditing}
-          isPreviewing={this.state.isPreviewingCard}
-          isEditingParameter={isEditingParameter}
-          isMobile={isMobile}
-          gridSize={gridSize}
-          totalNumGridCols={this.props.totalNumGridCols}
-          actionButtons={
-            isEmbed ? (
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-ignore
-              <QueryDownloadWidget
-                className="m1 text-brand-hover text-light"
-                classNameClose="hover-child"
-                card={dashcard.card}
-                params={parameterValuesBySlug}
-                dashcardId={dashcard.id}
-                token={dashcard.dashboard_id}
-                icon="download"
-              />
-            ) : null
-          }
-          onUpdateVisualizationSettings={
-            this.props.onUpdateVisualizationSettings
-          }
-          replacementContent={
-            clickBehaviorSidebarDashcard != null &&
-            isVirtualDashCard(dashcard) ? (
-              <div className="flex full-height align-center justify-center">
-                <h4 className="text-medium">{t`Text card`}</h4>
-              </div>
-            ) : isEditingParameter ? (
-              <DashCardParameterMapper
-                dashcard={dashcard}
-                isMobile={isMobile}
-              />
-            ) : clickBehaviorSidebarDashcard != null ? (
-              <ClickBehaviorSidebarOverlay
-                dashcard={dashcard}
-                dashcardWidth={this.props.gridItemWidth}
-                showClickBehaviorSidebar={this.props.showClickBehaviorSidebar}
-                isShowingThisClickBehaviorSidebar={
-                  clickBehaviorSidebarDashcard?.id === dashcard.id
-                }
-              />
-            ) : null
-          }
-          metadata={metadata}
-          mode={mode}
-          onChangeCardAndRun={
-            navigateToNewCardFromDashboard
-              ? ({
+          ) : null
+        }
+        onUpdateVisualizationSettings={onUpdateVisualizationSettings}
+        replacementContent={
+          clickBehaviorSidebarDashcard != null &&
+          isVirtualDashCard(dashcard) ? (
+            <div className="flex full-height align-center justify-center">
+              <h4 className="text-medium">{t`Text card`}</h4>
+            </div>
+          ) : isEditingParameter ? (
+            <DashCardParameterMapper dashcard={dashcard} isMobile={isMobile} />
+          ) : clickBehaviorSidebarDashcard != null ? (
+            <ClickBehaviorSidebarOverlay
+              dashcard={dashcard}
+              dashcardWidth={gridItemWidth}
+              showClickBehaviorSidebar={showClickBehaviorSidebar}
+              isShowingThisClickBehaviorSidebar={
+                clickBehaviorSidebarDashcard?.id === dashcard.id
+              }
+            />
+          ) : null
+        }
+        metadata={metadata}
+        mode={mode}
+        onChangeCardAndRun={
+          navigateToNewCardFromDashboard
+            ? ({
+                nextCard,
+                previousCard,
+                objectId,
+              }: Omit<NavigateToNewCardFromDashboardOpts, "dashcard">) => {
+                // navigateToNewCardFromDashboard needs `dashcard` for applying active filters to the query
+                navigateToNewCardFromDashboard({
                   nextCard,
                   previousCard,
+                  dashcard,
                   objectId,
-                }: Omit<NavigateToNewCardFromDashboardOpts, "dashcard">) => {
-                  // navigateToNewCardFromDashboard needs `dashcard` for applying active filters to the query
-                  navigateToNewCardFromDashboard({
-                    nextCard,
-                    previousCard,
-                    dashcard,
-                    objectId,
-                  });
-                }
-              : null
-          }
-          onChangeLocation={this.props.onChangeLocation}
-        />
-      </DashCardRoot>
-    );
-  }
+                });
+              }
+            : null
+        }
+        onChangeLocation={onChangeLocation}
+      />
+    </DashCardRoot>
+  );
 }
 
 export default DashCard;
