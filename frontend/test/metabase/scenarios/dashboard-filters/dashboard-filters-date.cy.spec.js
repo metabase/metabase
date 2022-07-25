0a99d27cbdca574ab7857f3afb87fdd5b01fcada
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-07-25 16:23:39 +0200

    Optimize looping logic
    - This will connect all filter types at once
    - We're reducing the number of checks for the default filter value to just one

--- frontend/test/metabase/scenarios/dashboard-filters/dashboard-filters-date.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard-filters/dashboard-filters-date.cy.spec.js
@@ -11,56 +11,75 @@ import {
 import { DASHBOARD_DATE_FILTERS } from "./helpers/e2e-dashboard-filter-data-objects";
 import * as DateFilter from "../native-filters/helpers/e2e-date-filter-helpers";
 
-Object.entries(DASHBOARD_DATE_FILTERS).forEach(
-  ([filter, { value, representativeResult }]) => {
-    describe("scenarios > dashboard > filters > date", () => {
-      beforeEach(() => {
-        cy.intercept("GET", "/api/table/*/query_metadata").as("metadata");
+describe("scenarios > dashboard > filters > date", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "/api/table/*/query_metadata").as("metadata");
 
-        restore();
-        cy.signInAsAdmin();
+    restore();
+    cy.signInAsAdmin();
 
-        visitDashboard(1);
+    visitDashboard(1);
 
-        editDashboard();
-        setFilter("Time", filter);
+    editDashboard();
+  });
 
-        cy.findByText("Select…").click();
-        popover().contains("Created At").first().click();
-      });
+  it(`should work when set through the filter widget`, () => {
+    // Add and connect every single available date filter type
+    Object.entries(DASHBOARD_DATE_FILTERS).forEach(([filter]) => {
+      setFilter("Time", filter);
 
-      it(`should work for "${filter}" when set through the filter widget`, () => {
-        saveDashboard();
-
-        filterWidget().click();
-
-        dateFilterSelector({
-          filterType: filter,
-          filterValue: value,
-        });
-
-        cy.get(".Card").within(() => {
-          cy.findByText(representativeResult);
-        });
-      });
-
-      it(`should work for "${filter}" when set as the default filter`, () => {
-        cy.findByText("Default value").next().click();
-
-        dateFilterSelector({
-          filterType: filter,
-          filterValue: value,
-        });
-
-        saveDashboard();
-
-        cy.get(".Card").within(() => {
-          cy.findByText(representativeResult);
-        });
-      });
+      cy.findByText("Select…").click();
+      popover().contains("Created At").first().click();
     });
-  },
-);
+
+    saveDashboard();
+
+    // Go through each of the filters and make sure they work individually
+    Object.entries(DASHBOARD_DATE_FILTERS).forEach(
+      ([filter, { value, representativeResult }], index) => {
+        filterWidget().eq(index).click();
+
+        dateFilterSelector({
+          filterType: filter,
+          filterValue: value,
+        });
+
+        cy.get(".Card").within(() => {
+          cy.findByText(representativeResult);
+        });
+
+        clearFilter(index);
+      },
+    );
+  });
+
+  // Rather than going through every single filter type,
+  // make sure the default filter works for just one of the available options
+  it(`should work when set as the default filter`, () => {
+    setFilter("Time", "Month and Year");
+    cy.findByText("Default value").next().click();
+
+    DateFilter.setMonthAndYear({
+      month: "November",
+      year: "2016",
+    });
+
+    cy.findByText("Select…").click();
+    popover().contains("Created At").first().click();
+
+    saveDashboard();
+
+    // The default value should immediately be applied
+    cy.get(".Card").within(() => {
+      cy.findByText("85.88");
+    });
+
+    // Make sure we can override the default value
+    cy.findByText("November, 2016").click();
+    popover().contains("June").click();
+    cy.findByText("33.9");
+  });
+});
 
 function dateFilterSelector({ filterType, filterValue } = {}) {
   switch (filterType) {
@@ -93,4 +112,9 @@ function dateFilterSelector({ filterType, filterValue } = {}) {
     default:
       throw new Error("Wrong filter type!");
   }
+}
+
+function clearFilter(index) {
+  filterWidget().eq(index).find(".Icon-close").click();
+  cy.wait("@dashcardQuery1");
 }
