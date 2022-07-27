5fdcd1a2f00bf94597bf2f014e8f7ff19f201d2c
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-07-27 15:30:22 +0300

    Add a tooltip for disabled menu items in collections

--- frontend/src/metabase/components/EntityMenuItem.styled.tsx
+++ frontend/src/metabase/components/EntityMenuItem.styled.tsx
@@ -1,26 +1,31 @@
 import styled from "@emotion/styled";
-
-import ExternalLink from "metabase/core/components/ExternalLink";
+import { Link } from "react-router";
 import { color } from "metabase/lib/colors";
+import ExternalLink from "metabase/core/components/ExternalLink";
+import Icon from "metabase/components/Icon";
 
-export const Item = styled.div`
+export interface MenuItemProps {
+  disabled?: boolean;
+}
+
+export const MenuItemContent = styled.div<MenuItemProps>`
   display: flex;
   align-items: center;
+  border-radius: 0.5em;
   cursor: ${props => (props.disabled ? "not-allowed" : "pointer")};
-  color: ${props => color(props.disabled ? "text-light" : "text-medium")};
+  color: ${props => color(props.disabled ? "text-light" : "text-dark")};
   padding: 0.85em 1.45em;
   text-decoration: none;
-  transition: all 300ms linear;
   :hover {
     color: ${props => !props.disabled && color("brand")};
+    background-color: ${props => !props.disabled && color("bg-light")};
   }
   > .Icon {
-    color: ${color("text-light")};
+    color: ${props => color(props.disabled ? "text-light" : "text-dark")};
     margin-right: 0.65em;
   }
   :hover > .Icon {
     color: ${props => !props.disabled && color("brand")};
-    transition: all 300ms linear;
   },
   /* icon specific tweaks
      the alert icon should be optically aligned  with the x-height of the text */
@@ -42,7 +47,19 @@ export const Item = styled.div`
   },
 `;
 
-export const StyledExternalLink = styled(ExternalLink)`
+export const MenuItemIcon = styled(Icon)`
+  margin-right: 0.5rem;
+`;
+
+export const MenuItemTitle = styled.span`
+  font-weight: bold;
+`;
+
+export const MenuLink = styled(Link)`
+  display: block;
+`;
+
+export const MenuExternalLink = styled(ExternalLink)`
   text-decoration: none;
   display: block;
 `;
