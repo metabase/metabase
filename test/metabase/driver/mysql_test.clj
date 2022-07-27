09cef3996af99df9f551b3b7a58bcd6bfe7047e8
Howon Lee <hlee.howon@gmail.com>
2022-07-27 01:44:55 -0700

    big test in json too

--- test/metabase/driver/mysql_test.clj
+++ test/metabase/driver/mysql_test.clj
@@ -436,11 +436,11 @@
     (mt/dataset json
       (when (not (is-mariadb? (u/id (mt/db))))
         (testing "Nested field column listing, but big"
-          (is (= #{}
-                 (sql-jdbc.sync/describe-nested-field-columns
-                   :mysql
-                   (mt/db)
-                   {:name "big_json"}))))))))
+          (is (= metabase.driver.sql-jdbc.sync.describe-table/max-nested-field-columns
+                 (count (sql-jdbc.sync/describe-nested-field-columns
+                          :mysql
+                          (mt/db)
+                          {:name "big_json"})))))))))
 
 (deftest json-query-test
   (let [boop-identifier (:form (hx/with-type-info (hx/identifier :field "boop" "bleh -> meh") {}))]
