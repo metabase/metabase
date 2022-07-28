26514a850cd06fbe81277a5adb1b64cb9abdde14
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2022-07-28 20:19:49 +0200

    [E2E] Optimize looping logic in tests for dashboard number filters (#24411)
    * Optimize looping logic for GUI and SQL dashboard number filters
    
    - This will connect all filter types at once
    - We're reducing the number of checks for the default filter value to just one

--- frontend/test/metabase/scenarios/dashboard-filters/helpers/e2e-dashboard-filter-data-objects.js
+++ frontend/test/metabase/scenarios/dashboard-filters/helpers/e2e-dashboard-filter-data-objects.js
@@ -36,29 +36,6 @@ export const DASHBOARD_DATE_FILTERS = {
   },
 };
 
-export const DASHBOARD_NUMBER_FILTERS = {
-  "Equal to": {
-    value: "2.07",
-    representativeResult: "37.65",
-  },
-  "Not equal to": {
-    value: "2.07",
-    representativeResult: "110.93",
-  },
-  Between: {
-    value: ["3", "5"],
-    representativeResult: "68.23",
-  },
-  "Greater than or equal to": {
-    value: "6.01",
-    representativeResult: "110.93",
-  },
-  "Less than or equal to": {
-    value: "2",
-    representativeResult: "29.8",
-  },
-};
-
 export const DASHBOARD_LOCATION_FILTERS = {
   Dropdown: {
     value: "Abbeville",
