c35689957dd08b10e0d972cdddfe5ac3717726c8
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-07-29 14:28:45 +0200

    Optimize looping logic for GUI dashboard text/category filters
    - This will connect all filter types at once
    - We're reducing the number of checks for the default filter value to just one

--- frontend/test/metabase/scenarios/dashboard-filters/dashboard-filters-text-category.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard-filters/dashboard-filters-text-category.cy.spec.js
@@ -8,48 +8,64 @@ import {
   visitDashboard,
 } from "__support__/e2e/helpers";
 
-import { DASHBOARD_TEXT_FILTERS } from "./helpers/e2e-dashboard-filter-data-objects";
+import { DASHBOARD_TEXT_FILTERS } from "./dashboard-filters-text-category";
 import { applyFilterByType } from "../native-filters/helpers/e2e-field-filter-helpers";
 
-Object.entries(DASHBOARD_TEXT_FILTERS).forEach(
-  ([filter, { value, representativeResult }]) => {
-    describe("scenarios > dashboard > filters > text/category", () => {
-      beforeEach(() => {
-        restore();
-        cy.signInAsAdmin();
+describe("scenarios > dashboard > filters > text/category", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
 
-        visitDashboard(1);
+    visitDashboard(1);
 
-        editDashboard();
-        setFilter("Text or Category", filter);
+    editDashboard();
+  });
 
-        cy.findByText("Select…").click();
-        popover().contains("Source").click();
-      });
+  it(`should work when set through the filter widget`, () => {
+    Object.entries(DASHBOARD_TEXT_FILTERS).forEach(([filter]) => {
+      cy.log(`Make sure we can connect ${filter} filter`);
+      setFilter("Text or Category", filter);
 
-      it(`should work for "${filter}" when set through the filter widget`, () => {
-        saveDashboard();
-
-        filterWidget().click();
-
-        applyFilterByType(filter, value);
-
-        cy.get(".Card").within(() => {
-          cy.contains(representativeResult);
-        });
-      });
-
-      it(`should work for "${filter}" when set as the default filter`, () => {
-        cy.findByText("Default value").next().click();
-
-        applyFilterByType(filter, value);
-
-        saveDashboard();
-
-        cy.get(".Card").within(() => {
-          cy.contains(representativeResult);
-        });
-      });
+      cy.findByText("Select…").click();
+      popover().contains("Source").click();
     });
-  },
-);
+
+    saveDashboard();
+
+    Object.entries(DASHBOARD_TEXT_FILTERS).forEach(
+      ([filter, { value, representativeResult }], index) => {
+        filterWidget().eq(index).click();
+        applyFilterByType(filter, value);
+
+        cy.log(`Make sure ${filter} filter returns correct result`);
+        cy.get(".Card").within(() => {
+          cy.contains(representativeResult);
+        });
+
+        clearFilter(index);
+      },
+    );
+  });
+
+  it(`should work when set as the default filter`, () => {
+    setFilter("Text or Category", "Dropdown");
+
+    cy.findByText("Select…").click();
+    popover().contains("Source").click();
+
+    cy.findByText("Default value").next().click();
+
+    applyFilterByType("Dropdown", "Organic");
+
+    saveDashboard();
+
+    cy.get(".Card").within(() => {
+      cy.contains("39.58");
+    });
+  });
+});
+
+function clearFilter(index = 0) {
+  filterWidget().eq(index).find(".Icon-close").click();
+  cy.wait("@dashcardQuery1");
+}
