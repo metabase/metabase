42ff07149f41c65043f776e2c1b146947dcabbda
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2022-08-02 10:47:52 -0600

    even tighter bulk filter modal spacing (#24521)

--- frontend/src/metabase/components/TokenFieldItem/TokenFieldItem.styled.ts
+++ frontend/src/metabase/components/TokenFieldItem/TokenFieldItem.styled.ts
@@ -1,17 +1,14 @@
 import styled from "@emotion/styled";
 import { color, alpha, darken } from "metabase/lib/colors";
-import { breakpointMinHeightMedium } from "metabase/styled-components/theme";
 
 export const TokenFieldItem = styled.li<{
   isValid: boolean;
 }>`
   display: flex;
   align-items: center;
-  padding: 0.75rem 1rem;
-  height: 30px;
-  ${breakpointMinHeightMedium} {
-    height: 46px;
-  }
+  padding: 0.5rem 0.75rem;
+
+  height: 46px;
   border-radius: 0.5rem;
   color: ${({ isValid }) => (isValid ? color("brand") : color("error"))};
   background-color: ${alpha("brand", 0.2)};
