e2adb06eba1111b114d5e70f374d80b1b5ccdc3a
Ryan Laurie <iethree@gmail.com>
2022-08-02 10:37:46 -0600

    make search visible

---
+++ frontend/src/metabase/query_builder/components/filters/modals/BulkFilterModal/BulkFilterFieldSearch.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState, useRef } from "react";
+import { t } from "ttag";
+
+import { useOnMount } from "metabase/hooks/use-on-mount";
+
+import {
+  SearchContainer,
+  SearchInput,
+  SearchIcon,
+} from "./BulkFilterModal.styled";
+
+export const FieldSearch = ({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+}): JSX.Element => {
+  const [showSearch, setShowSearch] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useOnMount(() => {
+    const searchToggleListener = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && e.key === "k") {
+        setShowSearch(true);
+      }
+    };
+    window.addEventListener("keydown", searchToggleListener);
+    return () => window.removeEventListener("keydown", searchToggleListener);
+  });
+
+  const shouldClose = () => {
+    const input = inputRef.current;
+    const isFocused = document.activeElement === input;
+
+    if (input && !input.value && !isFocused) {
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (showSearch) {
+      inputRef.current?.focus();
+    }
+  }, [showSearch]);
+
+  return (
+    <SearchContainer
+      isActive={showSearch}
+      onMouseEnter={() => setShowSearch(true)}
+      onMouseLeave={e => shouldClose() && setShowSearch(false)}
+    >
+      <SearchIcon
+        name="search"
+        onClick={() => setShowSearch(lastShowSearch => !lastShowSearch)}
+      />
+      <SearchInput
+        ref={inputRef}
+        isActive={showSearch}
+        type="search"
+        placeholder={t`Search for a column...`}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+        onBlur={() => shouldClose() && setShowSearch(false)}
+      />
+    </SearchContainer>
+  );
+};
--- frontend/src/metabase/query_builder/components/filters/modals/BulkFilterModal/BulkFilterModal.styled.tsx
+++ frontend/src/metabase/query_builder/components/filters/modals/BulkFilterModal/BulkFilterModal.styled.tsx
@@ -10,6 +10,7 @@ import TabList from "metabase/core/components/TabList";
 import TabPanel from "metabase/core/components/TabPanel";
 import Ellipsified from "metabase/core/components/Ellipsified";
 import IconButtonWrapper from "metabase/components/IconButtonWrapper";
+import Icon from "metabase/components/Icon";
 
 interface ModalRootProps {
   hasSideNav?: boolean;
@@ -92,11 +93,55 @@ export const ModalCloseButton = styled(IconButtonWrapper)`
   color: ${color("text-light")};
 `;
 
-export const SearchContainer = styled.div`
-  margin-right: ${space(2)};
+export const SearchIcon = styled(Icon)`
+  margin: 0 ${space(1)};
+  color: ${color("text-light")};
+`;
 
-  input {
-    font-weight: bold;
-    padding: 12px ${space(3)};
+export const SearchContainer = styled.div<{ isActive: boolean }>`
+  display: flex;
+  flex: 1 1 auto;
+  align-items: center;
+  position: relative;
+
+  border: ${props =>
+    props.isActive ? `1px solid ${color("border")}` : "none"};
+  overflow: hidden;
+  transition: max-width 0.2s;
+  margin-right: ${space(3)};
+
+  @media (prefers-reduced-motion) {
+    transition: none;
   }
+
+  justify-content: center;
+  margin-left: auto;
+
+  max-width: ${props => (props.isActive ? "20rem" : "2rem")};
+  height: 2rem;
+  border-radius: 0.5rem;
+
+  ${breakpointMaxSmall} {
+    display: none;
+  }
+`;
+
+export const SearchInput = styled.input<{ isActive: boolean }>`
+  background-color: transparent;
+  border: none;
+  color: ${color("text-medium")};
+  font-weight: 700;
+  margin-right: ${space(1)};
+
+  width: 100%;
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${color("text-light")};
+  }
+
+  width: ${props => (props.isActive ? "100%" : 0)};
 `;
