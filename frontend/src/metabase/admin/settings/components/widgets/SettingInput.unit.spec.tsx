5422664e5f801df64186e5e2b610706a49e8070a
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2022-08-09 12:25:26 +0400

    Allow decimal settings values (#24676)
    * allow decimal settings values
    
    * allow decimal settings values

---
+++ frontend/src/metabase/admin/settings/components/widgets/SettingInput.unit.spec.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SettingInput from "./SettingInput";
+
+describe("SettingInput", () => {
+  it("when type=number should allow decimal values", () => {
+    const onChange = jest.fn();
+    render(
+      <SettingInput
+        onChange={onChange}
+        setting={{
+          key: "test",
+          value: "100",
+          default: "100",
+          placeholder: "numeric value",
+        }}
+        type="number"
+      />,
+    );
+
+    userEvent.type(screen.getByPlaceholderText("numeric value"), ".25");
+    userEvent.tab(); // blur
+
+    expect(onChange).toHaveBeenCalledWith(100.25);
+  });
+});
