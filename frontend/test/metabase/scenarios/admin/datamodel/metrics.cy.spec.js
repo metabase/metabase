metabase/metabase

commit 8d46b4f59a1bc5aa2bae94e5627c77e559773315

Replace redux-form in Metric and Segment forms (#24682)

frontend/src/metabase-types/api/index.ts
@@ -9,11 +9,13 @@ export * from "./dataset";
 export * from "./field";
 export * from "./foreign-key";
 export * from "./group";
+export * from "./metric";
 export * from "./models";
 export * from "./notifications";
 export * from "./permissions";
 export * from "./query";
 export * from "./revision";
+export * from "./segment";
 export * from "./settings";
 export * from "./slack";
 export * from "./table";

frontend/src/metabase-types/api/metric.ts
@@ -1,0 +1,14 @@
+import { StructuredQuery } from "./query";
+import { TableId } from "./table";
+
+export type MetricId = number;
+
+export interface Metric {
+  id: MetricId;
+  name: string;
+  description: string;
+  table_id: TableId;
+  archived: boolean;
+  definition: StructuredQuery;
+  revision_message?: string;
+}

frontend/src/metabase-types/api/mocks/index.ts
@@ -5,8 +5,10 @@ export * from "./collection";
 export * from "./dashboard";
 export * from "./database";
 export * from "./dataset";
+export * from "./metric";
 export * from "./models";
 export * from "./query";
+export * from "./segment";
 export * from "./table";
 export * from "./timeline";
 export * from "./settings";

frontend/src/metabase-types/api/mocks/metric.ts
@@ -1,0 +1,12 @@
+import { Metric } from "metabase-types/api";
+import { createMockStructuredQuery } from "./query";
+
+export const createMockMetric = (opts?: Partial<Metric>): Metric => ({
+  id: 1,
+  name: "Metric",
+  description: "A metric",
+  table_id: 1,
+  archived: false,
+  definition: createMockStructuredQuery(),
+  ...opts,
+});

frontend/src/metabase-types/api/mocks/segment.ts
@@ -1,0 +1,12 @@
+import { Segment } from "metabase-types/api";
+import { createMockStructuredQuery } from "./query";
+
+export const createMockSegment = (opts?: Partial<Segment>): Segment => ({
+  id: 1,
+  name: "Segment",
+  description: "A segment",
+  table_id: 1,
+  archived: false,
+  definition: createMockStructuredQuery(),
+  ...opts,
+});

frontend/src/metabase-types/api/segment.ts
@@ -1,0 +1,14 @@
+import { StructuredQuery } from "./query";
+import { TableId } from "./table";
+
+export type SegmentId = number;
+
+export interface Segment {
+  id: SegmentId;
+  name: string;
+  description: string;
+  table_id: TableId;
+  archived: boolean;
+  definition: StructuredQuery;
+  revision_message?: string;
+}

frontend/src/metabase/admin/datamodel/components/FormInput.jsx
@@ -1,25 +1,0 @@
-/* eslint-disable react/prop-types */
-import React, { Component } from "react";
-
-import cx from "classnames";
-import { formDomOnlyProps } from "metabase/lib/redux";
-
-export default class FormInput extends Component {
-  static propTypes = {};
-
-  render() {
-    const { field, className, placeholder } = this.props;
-    return (
-      <input
-        type="text"
-        placeholder={placeholder}
-        className={cx(
-          "input full",
-          { "border-error": !field.active && field.visited && field.invalid },
-          className,
-        )}
-        {...formDomOnlyProps(field)}
-      />
-    );
-  }
-}

frontend/src/metabase/admin/datamodel/components/FormInput/FormInput.styled.tsx
@@ -1,0 +1,15 @@
+import styled from "@emotion/styled";
+import { color } from "metabase/lib/colors";
+
+export interface FormInputRootProps {
+  touched?: boolean;
+  error?: string | boolean;
+}
+
+export const FormInputRoot = styled.input<FormInputRootProps>`
+  width: 100%;
+
+  &:not(:focus) {
+    border-color: ${props => props.touched && props.error && color("error")};
+  }
+`;

commit dbc02ca15b73995b7e42f974def7e4ad9d8f96b8

[E2E] fix segments/metrics failures that started after #24682

frontend/test/metabase/scenarios/admin/datamodel/metrics.cy.spec.js
@@ -266,7 +266,18 @@ describe("scenarios > admin > datamodel > metrics", () => {
       cy.visit("/admin/datamodel/metrics");
       cy.findByText("New metric").click();
       cy.findByText("Select a table").click();
-      cy.findByText("Orders").click();
+
+      // Ugly hack to prevent failures that started after https://github.com/metabase/metabase/pull/24682 has been merged.
+      // For unknon reasons, popover doesn't open with expanded list of all Sample Database tables. Rather. it shows
+      // Sample Database (collapsed) only. We need to click on it to expand it.
+      // This conditional mechanism prevents failures even if that popover opens expanded in the future.
+      cy.get(".List-section").then($list => {
+        if ($list.length !== 5) {
+          cy.findByText("Sample Database").click();
+        }
+        cy.findByText("Orders").click();
+      });
+
       cy.findByText("Add filters to narrow your answer").click();
       cy.findByText("Custom Expression").click();
       cy.get(".ace_text-input").clear().type("[ID] > 0 OR [ID] < 9876543210");

frontend/test/metabase/scenarios/admin/datamodel/segments.cy.spec.js
@@ -31,9 +31,18 @@ describe("scenarios > admin > datamodel > segments", () => {
       cy.visit("/admin/datamodel/segments");
       cy.findByText("New segment").click();
       cy.findByText("Select a table").click();
-      popover().within(() => {
+
+      // Ugly hack to prevent failures that started after https://github.com/metabase/metabase/pull/24682 has been merged.
+      // For unknon reasons, popover doesn't open with expanded list of all Sample Database tables. Rather. it shows
+      // Sample Database (collapsed) only. We need to click on it to expand it.
+      // This conditional mechanism prevents failures even if that popover opens expanded in the future.
+      cy.get(".List-section").then($list => {
+        if ($list.length !== 5) {
+          cy.findByText("Sample Database").click();
+        }
         cy.findByText("Orders").click();
       });
+
       cy.findByText("Add filters to narrow your answer").click();
 
       cy.log("Fails in v0.36.0 and v0.36.3. It exists in v0.35.4");