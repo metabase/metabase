4c0df23ba1e446d463596cbf3a71cc0d12af96bb
Michiel Borkent <michielborkent@gmail.com>
2022-08-12 12:07:56 +0200

    improve test hook

--- .clj-kondo/hooks/toucan/util/test.clj
+++ .clj-kondo/hooks/toucan/util/test.clj
@@ -4,17 +4,23 @@
 (defn- with-temp-inner [body bindings]
   (let [pairs       (partition 2 bindings)
         db-refs     (map first pairs)
-        let-stream  (for [[_ binding+opts] pairs
-                          part             (:children binding+opts)]
-                      part)]
+        let-stream  (mapcat (fn [[_ bindings+opts]]
+                              (let [children (:children bindings+opts)]
+                                (if (= 1 (count children))
+                                  (concat children [(api/map-node [])])
+                                  children)))
+                            pairs)]
     (api/vector-node [(api/vector-node db-refs)
-                      (api/list-node (list* (api/token-node `let)
-                                            (api/vector-node let-stream)
-                                            body))])))
+                      (with-meta (api/list-node (list* (api/token-node `let)
+                                                       (with-meta (api/vector-node let-stream)
+                                                         (meta body))
+                                                       body))
+                        (meta body))])))
 
 (defn with-temp [{:keys [node]}]
-  (let [[_ db-ref binding+opts & body] (:children node)]
-    {:node (with-temp-inner body [db-ref binding+opts])}))
+  (let [[_ db-ref binding+opts & body] (:children node)
+        node (with-temp-inner body [db-ref binding+opts])]
+    {:node node}))
 
 (defn with-temp* [{:keys [node]}]
   (let [[_ bindings & body]  (:children node)]
