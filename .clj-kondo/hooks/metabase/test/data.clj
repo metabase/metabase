ed3d8163c498277ee43d8d6ebd36208197277b63
Michiel Borkent <michielborkent@gmail.com>
2022-08-12 13:38:11 +0200

    dashboard-test

--- .clj-kondo/hooks/metabase/test/data.clj
+++ .clj-kondo/hooks/metabase/test/data.clj
@@ -1,7 +1,7 @@
 (ns hooks.metabase.test.data
   (:require
    [clj-kondo.hooks-api :as hooks]
-   [clojure.string :as string]
+   [clojure.string :as str]
    [clojure.walk :as walk]))
 
 (defn dataset [{:keys [node]}]
@@ -15,41 +15,45 @@
              (meta dataset))}))
 
 (defn $ids [{:keys [node]}]
-  (let [args (rest (:children node))
-        [table-name & body] args
-        unused-node (hooks/token-node '_)
-        vars (atom #{})
-        _ (walk/postwalk (fn [node]
-                           (when (hooks/token-node? node)
-                             (let [str-node (str (hooks/sexpr node))]
-                               (when (or (string/starts-with? str-node "$")
-                                         (string/starts-with? str-node "!")
-                                         (string/starts-with? str-node "&")
-                                         (string/starts-with? str-node "*")
-                                         (string/starts-with? str-node "%"))
-                                 (swap! vars conj node))))
-                           node)
-                         body)
-        nil-bindings (vec (interpose (hooks/token-node nil) @vars))
-        unused-bindings (vec (interpose unused-node @vars))
-        final-bindings (concat
-                        (when-not (= "nil" (str table-name))
-                          [table-name (hooks/token-node nil)])
-                        [unused-node table-name]
-                        (if (seq nil-bindings)
-                          (conj nil-bindings (hooks/token-node nil))
-                          [])
-                        (if (seq unused-bindings)
-                          (conj (next unused-bindings)
-                                (first unused-bindings)
-                                unused-node)
-                          []))]
-    {:node (with-meta
-             (hooks/list-node
-              (with-meta
-                (list*
-                 (hooks/token-node 'let)
-                 (hooks/vector-node (vec final-bindings))
-                 body)
-                (meta body)))
-             (meta body))}))
+  (let [args (rest (:children node))]
+    (if (= 1 (count args))
+      {:node (update node :children (fn [[ids form]]
+                                      (list* ids (hooks/token-node nil) form)))}
+      (let [args (rest (:children node))
+            [table-name & body] args
+            unused-node (hooks/token-node '_)
+            vars (atom #{})
+            _ (walk/postwalk (fn [node]
+                               (when (hooks/token-node? node)
+                                 (let [str-node (str (hooks/sexpr node))]
+                                   (when (or (str/starts-with? str-node "$")
+                                             (str/starts-with? str-node "!")
+                                             (str/starts-with? str-node "&")
+                                             (str/starts-with? str-node "*")
+                                             (str/starts-with? str-node "%"))
+                                     (swap! vars conj node))))
+                               node)
+                             body)
+            nil-bindings (vec (interpose (hooks/token-node nil) @vars))
+            unused-bindings (vec (interpose unused-node @vars))
+            final-bindings (concat
+                            (when-not (= "nil" (str table-name))
+                              [table-name (hooks/token-node nil)])
+                            [unused-node table-name]
+                            (if (seq nil-bindings)
+                              (conj nil-bindings (hooks/token-node nil))
+                              [])
+                            (if (seq unused-bindings)
+                              (conj (next unused-bindings)
+                                    (first unused-bindings)
+                                    unused-node)
+                              []))]
+        {:node (with-meta
+                 (hooks/list-node
+                  (with-meta
+                    (list*
+                     (hooks/token-node 'let)
+                     (hooks/vector-node (vec final-bindings))
+                     body)
+                    (meta body)))
+                 (meta body))}))))
