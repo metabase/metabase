7bd1ca4b682ed72ffee87dfb83c09f1003ab9402
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-08-18 13:02:50 -0400

    Exit initializeQB early and trigger a metric-specific action, initializeMetricMode

--- frontend/src/metabase/query_builder/actions/newmetrics.ts
+++ frontend/src/metabase/query_builder/actions/newmetrics.ts
@@ -2,12 +2,20 @@ import { t } from "ttag";
 import { push } from "react-router-redux";
 
 import { Dispatch, GetState } from "metabase-types/store";
+import { Card } from "metabase-types/types/Card";
 import { canBeUsedAsMetric } from "metabase-lib/lib/newmetrics/utils";
+import Question from "metabase-lib/lib/Question";
 import { addUndo } from "metabase/redux/undo";
+import { getMetadata } from "metabase/selectors/metadata";
 
 import { getQuestion } from "../selectors";
+import { runQuestionQuery } from "./querying";
+import { loadMetadataForCard } from "./core/metadata";
 
+// avoiding the circular dependency
+const INITIALIZE_QB = "metabase/qb/INITIALIZE_QB";
 export const CREATE_METRIC = "metabase/qb/CREATE_METRIC";
+export const ENTER_QB_METRIC_MODE = "metabase/qb/ENTER_QB_METRIC_MODE";
 
 export const createMetricFromQuestion =
   () => (dispatch: Dispatch, getState: GetState) => {
@@ -23,4 +31,34 @@ export const createMetricFromQuestion =
         }),
       );
     }
+  };
+
+export const initializeMetricMode =
+  (card: Card) => async (dispatch: Dispatch, getState: GetState) => {
+    // this action currently does nothing. is it needed?
+    dispatch({
+      type: ENTER_QB_METRIC_MODE,
+    });
+
+    await dispatch(initializeMetricCard(card));
+  };
+
+// this all comes from the core initializeQB action
+// excluded a few auxilliary things like loading alerts, showing modals,
+// so will need to revisit to ensure there aren't other things that we
+// want to do that's card/qb related
+export const initializeMetricCard =
+  (card: Card) => async (dispatch: Dispatch, getState: GetState) => {
+    await dispatch(loadMetadataForCard(card));
+    const metadata = getMetadata(getState());
+    const question = new Question(card, metadata).lockDisplay();
+    dispatch({
+      type: INITIALIZE_QB,
+      payload: {
+        card: question.card(),
+      },
+    });
+    // no need for setTimeout because there shouldn't be any paremeter widgets
+    // might not always be the case though
+    dispatch(runQuestionQuery({ shouldUpdateUrl: false }));
   };
