a438d0fbd96cb7efae3b4a55f88da2f3e92f9452
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-08-19 16:02:16 -0400

    Add newmetrics to collections page

--- frontend/src/metabase/entities/new-metrics.ts
+++ frontend/src/metabase/entities/new-metrics.ts
@@ -1,4 +1,19 @@
-import { createEntity } from "metabase/lib/entities";
+import { createEntity, undo } from "metabase/lib/entities";
+import { color } from "metabase/lib/colors";
+import { normalizedCollection } from "metabase/entities/collections";
+import { canonicalCollectionId } from "metabase/collections/utils";
+import { Dispatch } from "metabase-types/store";
+
+type Opts = Record<string, any>;
+
+type MetricModel = {
+  description: string | null;
+  entity_id: string | null;
+  model: "newmetric";
+  name: string;
+  id: number;
+  collection?: unknown;
+};
 
 const NewMetrics = createEntity({
   name: "newMetrics",
@@ -10,6 +25,52 @@ const NewMetrics = createEntity({
     getObject: (state: any, { entityId }: { entityId: number }) => {
       return state.entities.newMetrics[entityId];
     },
+  },
+
+  objectActions: {
+    setArchived: ({ id }: { id: number }, archived: boolean, opts: Opts) => {
+      return NewMetrics.actions.update(
+        { id },
+        { archived },
+        undo(opts, "metric", archived ? "archived" : "unarchived"),
+      );
+    },
+
+    setCollection: (
+      { id }: { id: number },
+      collection: { id: number } | null | undefined,
+      opts: Opts,
+    ) => {
+      return NewMetrics.actions.update(
+        { id },
+        { collection_id: canonicalCollectionId(collection?.id) },
+        undo(opts, "metric", "moved"),
+      );
+    },
+
+    setPinned: ({ id }: { id: number }, pinned: boolean, opts: Opts) => {
+      return NewMetrics.actions.update(
+        { id },
+        {
+          collection_position:
+            typeof pinned === "number" ? pinned : pinned ? 1 : null,
+        },
+        opts,
+      );
+    },
+
+    // setCollectionPreview: ({ id }, collection_preview, opts) =>
+    //   NewMetrics.actions.update({ id }, { collection_preview }, opts),
+  },
+
+  objectSelectors: {
+    getName: (metric: MetricModel) => metric?.name,
+    getUrl: (metric: MetricModel) => `/metric/${metric.id}`,
+    getColor: () => color("text-medium"),
+    getCollection: (metric: MetricModel) => {
+      return metric && normalizedCollection(metric.collection);
+    },
+    getIcon: () => ({ name: "star" }),
   },
 });
 
