c3468a854f6f9e0bb1aa75d4413832399f0845b9
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-23 15:05:01 +0100

    Extract collection `name` and `description` fields

--- frontend/src/metabase/entities/collections/forms.js
+++ frontend/src/metabase/entities/collections/forms.js
@@ -12,6 +12,28 @@ import {
 
 import { DEFAULT_COLLECTION_COLOR_ALIAS } from "./constants";
 
+export function createNameField() {
+  return {
+    name: "name",
+    title: t`Name`,
+    placeholder: t`My new fantastic collection`,
+    autoFocus: true,
+    validate: name =>
+      (!name && t`Name is required`) ||
+      (name && name.length > 100 && t`Name must be 100 characters or less`),
+  };
+}
+
+export function createDescriptionField() {
+  return {
+    name: "description",
+    title: t`Description`,
+    type: "text",
+    placeholder: t`It's optional but oh, so helpful`,
+    normalize: description => description || null, // expected to be nil or non-empty string
+  };
+}
+
 function createForm({ extraFields = [] } = {}) {
   return {
     fields: (
@@ -19,22 +41,8 @@ function createForm({ extraFields = [] } = {}) {
         color: color("brand"),
       },
     ) => [
-      {
-        name: "name",
-        title: t`Name`,
-        placeholder: t`My new fantastic collection`,
-        autoFocus: true,
-        validate: name =>
-          (!name && t`Name is required`) ||
-          (name && name.length > 100 && t`Name must be 100 characters or less`),
-      },
-      {
-        name: "description",
-        title: t`Description`,
-        type: "text",
-        placeholder: t`It's optional but oh, so helpful`,
-        normalize: description => description || null, // expected to be nil or non-empty string
-      },
+      createNameField(),
+      createDescriptionField(),
       {
         name: "color",
         title: t`Color`,
