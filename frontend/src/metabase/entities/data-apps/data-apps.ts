79ef6cec2141cc066386ec1b147f8d5eebc2a6c0
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-24 01:52:40 +0100

    Convert data app entity file to TypeScript

--- frontend/src/metabase/entities/data-apps/data-apps.ts
+++ frontend/src/metabase/entities/data-apps/data-apps.ts
@@ -4,10 +4,21 @@ import { createEntity } from "metabase/lib/entities";
 import { DataAppSchema } from "metabase/schema";
 import { CollectionsApi, DataAppsApi } from "metabase/services";
 
+import { Collection, DataApp } from "metabase-types/api";
+
 import { DEFAULT_COLLECTION_COLOR_ALIAS } from "../collections/constants";
 
 import { createForm } from "./forms";
 import { getDataAppIcon } from "./utils";
+
+type EditableDataAppParams = Pick<
+  DataApp,
+  "dashboard_id" | "options" | "nav_items"
+> &
+  Pick<Collection, "name" | "description">;
+
+type CreateDataAppParams = Partial<EditableDataAppParams> &
+  Pick<EditableDataAppParams, "name">;
 
 const DataApps = createEntity({
   name: "dataApps",
@@ -20,7 +31,11 @@ const DataApps = createEntity({
   schema: DataAppSchema,
 
   api: {
-    create: async ({ name, description, ...dataAppProps }) => {
+    create: async ({
+      name,
+      description,
+      ...dataAppProps
+    }: CreateDataAppParams) => {
       const collection = await CollectionsApi.create({
         name,
         description: description || null,
