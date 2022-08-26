872e6c7666aa0ff36db3aa72137c0d9516c9e31d
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-26 13:22:20 +0100

    Turn "Exit app" button into a Link

--- frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbarView.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbarView.tsx
@@ -1,17 +1,14 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useMemo } from "react";
 import _ from "underscore";
 import { t } from "ttag";
-import { LocationDescriptor } from "history";
-import { connect } from "react-redux";
-import { push } from "react-router-redux";
 
 import ButtonGroup from "metabase/core/components/ButtonGroup";
+import Link from "metabase/core/components/Link";
 import Tooltip from "metabase/components/Tooltip";
 
 import * as Urls from "metabase/lib/urls";
 
 import { DataApp } from "metabase-types/api";
-import { State } from "metabase-types/store";
 
 import { MainNavbarProps, SelectedItem } from "./types";
 import {
@@ -25,36 +22,17 @@ import {
   SidebarSection,
 } from "./MainNavbar.styled";
 
-interface OwnProps extends MainNavbarProps {
+interface Props extends MainNavbarProps {
   dataApp: DataApp;
   items: any[];
   selectedItems: SelectedItem[];
 }
 
-interface DispatchProps {
-  onChangeLocation: (location: LocationDescriptor) => void;
-}
-
-type Props = OwnProps & DispatchProps;
-
-const mapDispatchToProps = {
-  onChangeLocation: push,
-};
-
-function DataAppNavbarView({
-  dataApp,
-  items,
-  selectedItems,
-  onChangeLocation,
-}: Props) {
+function DataAppNavbarView({ dataApp, items, selectedItems }: Props) {
   const appPages = useMemo(
     () => items.filter(item => item.model === "dashboard"),
     [items],
   );
-
-  const handleExitApp = useCallback(() => {
-    onChangeLocation(Urls.dataApp(dataApp, { mode: "preview" }));
-  }, [dataApp, onChangeLocation]);
 
   const { "data-app-page": dataAppPage } = _.indexBy(
     selectedItems,
@@ -89,14 +67,12 @@ function DataAppNavbarView({
           </Tooltip>
         </ButtonGroup>
         <ExitDataAppButton
-          onClick={handleExitApp}
+          as={Link}
+          to={Urls.dataApp(dataApp, { mode: "preview" })}
         >{t`Exit app`}</ExitDataAppButton>
       </DataAppActionsContainer>
     </SidebarContentRoot>
   );
 }
 
-export default connect<unknown, DispatchProps, OwnProps, State>(
-  null,
-  mapDispatchToProps,
-)(DataAppNavbarView);
+export default DataAppNavbarView;
