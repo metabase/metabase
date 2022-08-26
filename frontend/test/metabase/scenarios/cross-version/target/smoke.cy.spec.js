c7103ea7c0118c185bf1df2cd917d7379ec6ca76
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2022-08-26 23:06:30 +0200

    Add initial questions

--- frontend/test/metabase/scenarios/cross-version/target/smoke.cy.spec.js
+++ frontend/test/metabase/scenarios/cross-version/target/smoke.cy.spec.js
@@ -4,5 +4,15 @@ describe(`smoke test the migration to the version ${version}`, () => {
   it("should already be set up", () => {
     cy.visit("/");
     cy.findByText("Sign in to Metabase");
+
+    cy.findByLabelText("Email address").type("admin@metabase.test");
+    cy.findByLabelText("Password").type("12341234");
+    cy.button("Sign in").click();
+
+    cy.findByPlaceholderText("Search…");
+
+    cy.visit("/collection/root");
+    cy.findByText("Best Sold Products Rating");
+    cy.findByText("Quarterly Revenue");
   });
 });
