ac2e61367ad621db9a05318cace8d98698f8bdb8
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-27 05:47:22 +0100

    Remove `SidebarItemClasses` and `SidebarItemStyle`

--- frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/SidebarItem.jsx
+++ frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/SidebarItem.jsx
@@ -1,33 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import cx from "classnames";
 
 import {
   BaseSidebarItemRoot,
   SelectableSidebarItemRoot,
 } from "./SidebarItem.styled";
-
-export const SidebarItemClasses =
-  "border-brand-hover bordered border-transparent rounded flex align-center cursor-pointer overflow-hidden";
-
-export const SidebarItemStyle = {
-  paddingTop: 8,
-  paddingBottom: 8,
-  paddingLeft: 12,
-  paddingRight: 12,
-};
-
-export const SidebarItemWrapper = ({ children, onClick, style, disabled }) => (
-  <div
-    className={cx(SidebarItemClasses, { disabled })}
-    onClick={!disabled && onClick}
-    style={{
-      ...style,
-    }}
-  >
-    {children}
-  </div>
-);
 
 export function SidebarItem({ as = BaseSidebarItemRoot, ...props }) {
   const Element = as;
