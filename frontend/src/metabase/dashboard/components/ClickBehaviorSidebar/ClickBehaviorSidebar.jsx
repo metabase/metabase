86b30c0f579d90ccc6b5bbacc50c67ba9679b85b
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-27 03:48:00 +0100

    Extract utils

--- frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/ClickBehaviorSidebar.jsx
+++ frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/ClickBehaviorSidebar.jsx
@@ -31,6 +31,7 @@ import ClickMappings, {
   clickTargetObjectType,
 } from "metabase/dashboard/components/ClickMappings";
 
+import { clickBehaviorOptions, getClickBehaviorOptionName } from "./utils";
 import Column from "./Column";
 import CustomLinkText from "./CustomLinkText";
 import LinkOption from "./LinkOption";
@@ -49,31 +50,6 @@ import {
   SidebarIconWrapper,
   SidebarItem,
 } from "./ClickBehaviorSidebar.styled";
-
-const clickBehaviorOptions = [
-  { value: "menu", icon: "popover" },
-  { value: "link", icon: "link" },
-  { value: "crossfilter", icon: "filter" },
-  { value: "action", icon: "play" },
-];
-
-function getClickBehaviorOptionName(value, dashcard) {
-  if (value === "menu") {
-    return hasActionsMenu(dashcard)
-      ? t`Open the Metabase actions menu`
-      : t`Do nothing`;
-  }
-  if (value === "link") {
-    return t`Go to a custom destination`;
-  }
-  if (value === "crossfilter") {
-    return t`Update a dashboard filter`;
-  }
-  if (value === "action") {
-    return t`Perform action`;
-  }
-  return t`Unknown`;
-}
 
 const BehaviorOption = ({
   option,
