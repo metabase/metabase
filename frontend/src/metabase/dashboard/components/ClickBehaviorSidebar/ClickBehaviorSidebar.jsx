93e613836d1e20074cfe0aa6ab82641d6489375f
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-27 04:08:03 +0100

    Extract `ClickBehaviorSidebarMainView` component

--- frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/ClickBehaviorSidebar.jsx
+++ frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/ClickBehaviorSidebar.jsx
@@ -2,9 +2,6 @@
 import React from "react";
 import { getIn } from "icepick";
 
-import Icon from "metabase/components/Icon";
-
-import { color } from "metabase/lib/colors";
 import {
   isTableDisplay,
   clickBehaviorIsValid,
@@ -13,20 +10,11 @@ import { keyForColumn } from "metabase/lib/dataset";
 
 import Sidebar from "metabase/dashboard/components/Sidebar";
 
-import { clickBehaviorOptions, getClickBehaviorOptionName } from "./utils";
-import ActionOptions from "./ActionOptions";
 import ClickBehaviorSidebarHeader from "./ClickBehaviorSidebarHeader";
-import CrossfilterOptions from "./CrossfilterOptions";
-import LinkOptions from "./LinkOptions";
+import ClickBehaviorSidebarMainView from "./ClickBehaviorSidebarMainView";
 import TableClickBehaviorView from "./TableClickBehaviorView";
 import TypeSelector from "./TypeSelector";
-import { SidebarItemWrapper } from "./SidebarItem";
-import {
-  CloseIconContainer,
-  SidebarContent,
-  SidebarContentBordered,
-  SidebarIconWrapper,
-} from "./ClickBehaviorSidebar.styled";
+import { SidebarContent } from "./ClickBehaviorSidebar.styled";
 
 class ClickBehaviorSidebar extends React.Component {
   state = {
@@ -194,60 +182,12 @@ class ClickBehaviorSidebar extends React.Component {
               />
             </SidebarContent>
           ) : (
-            <div>
-              <SidebarContentBordered>
-                <SidebarItemWrapper
-                  onClick={() => this.setState({ showTypeSelector: true })}
-                  style={{
-                    backgroundColor: color("brand"),
-                    color: color("white"),
-                  }}
-                >
-                  <SidebarIconWrapper
-                    style={{ borderColor: "transparent", paddingLeft: 12 }}
-                  >
-                    <Icon
-                      name={
-                        clickBehaviorOptions.find(
-                          o => o.value === clickBehavior.type,
-                        ).icon
-                      }
-                    />
-                  </SidebarIconWrapper>
-                  <div className="flex align-center full">
-                    <h4>
-                      {getClickBehaviorOptionName(clickBehavior.type, dashcard)}
-                    </h4>
-                    <CloseIconContainer>
-                      <Icon name="close" size={12} />
-                    </CloseIconContainer>
-                  </div>
-                </SidebarItemWrapper>
-              </SidebarContentBordered>
-
-              {clickBehavior.type === "link" ? (
-                <LinkOptions
-                  clickBehavior={clickBehavior}
-                  dashcard={dashcard}
-                  parameters={parameters}
-                  updateSettings={this.updateSettings}
-                />
-              ) : clickBehavior.type === "crossfilter" ? (
-                <CrossfilterOptions
-                  clickBehavior={clickBehavior}
-                  dashboard={dashboard}
-                  dashcard={dashcard}
-                  updateSettings={this.updateSettings}
-                />
-              ) : clickBehavior.type === "action" ? (
-                <ActionOptions
-                  clickBehavior={clickBehavior}
-                  dashcard={dashcard}
-                  parameters={parameters}
-                  updateSettings={this.updateSettings}
-                />
-              ) : null}
-            </div>
+            <ClickBehaviorSidebarMainView
+              clickBehavior={clickBehavior}
+              dashboard={dashboard}
+              dashcard={dashcard}
+              parameters={parameters}
+            />
           )}
         </div>
       </Sidebar>
