cf0959261817b1df2c4e390de74c8d6c1ccf83f6
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-27 04:25:12 +0100

    Untangle `Column`

--- frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/Column.jsx
+++ frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/Column.jsx
@@ -14,27 +14,60 @@ import Questions from "metabase/entities/questions";
 import { SidebarItemWrapper, SidebarItemStyle } from "./SidebarItem";
 import { SidebarIconWrapper } from "./ClickBehaviorSidebar.styled";
 
-const LinkTargetName = ({ clickBehavior: { linkType, targetId } }) => (
-  <span>
-    {linkType === "url" ? (
-      t`URL`
-    ) : linkType === "question" ? (
-      <span>
-        {'"'}
+function Quoted({ children }) {
+  return (
+    <span>
+      {'"'}
+      {children}
+      {'"'}
+    </span>
+  );
+}
+
+const LinkTargetName = ({ clickBehavior: { linkType, targetId } }) => {
+  if (linkType === "url") {
+    return t`URL`;
+  }
+  if (linkType === "question") {
+    return (
+      <Quoted>
         <Questions.Name id={targetId} />
-        {'"'}
-      </span>
-    ) : linkType === "dashboard" ? (
-      <span>
-        {'"'}
+      </Quoted>
+    );
+  }
+  if (linkType === "dashboard") {
+    return (
+      <Quoted>
         <Dashboards.Name id={targetId} />
-        {'"'}
-      </span>
-    ) : (
-      "Unknown"
-    )}
-  </span>
-);
+      </Quoted>
+    );
+  }
+  return t`Unknown`;
+};
+
+function ClickBehaviorDescription({ column, clickBehavior }) {
+  if (!clickBehavior) {
+    return column.display_name;
+  }
+
+  if (clickBehavior.type === "crossfilter") {
+    const parameters = Object.keys(clickBehavior.parameterMapping || {});
+    return (n =>
+      ngettext(
+        msgid`${column.display_name} updates ${n} filter`,
+        `${column.display_name} updates ${n} filters`,
+        n,
+      ))(parameters.length);
+  }
+
+  if (clickBehavior.type === "link") {
+    return jt`${column.display_name} goes to ${(
+      <LinkTargetName clickBehavior={clickBehavior} />
+    )}`;
+  }
+
+  return column.display_name;
+}
 
 const Column = ({ column, clickBehavior, onClick }) => (
   <SidebarItemWrapper onClick={onClick} style={{ ...SidebarItemStyle }}>
@@ -43,18 +76,10 @@ const Column = ({ column, clickBehavior, onClick }) => (
     </SidebarIconWrapper>
     <div>
       <h4>
-        {clickBehavior && clickBehavior.type === "crossfilter"
-          ? (n =>
-              ngettext(
-                msgid`${column.display_name} updates ${n} filter`,
-                `${column.display_name} updates ${n} filters`,
-                n,
-              ))(Object.keys(clickBehavior.parameterMapping || {}).length)
-          : clickBehavior && clickBehavior.type === "link"
-          ? jt`${column.display_name} goes to ${(
-              <LinkTargetName clickBehavior={clickBehavior} />
-            )}`
-          : column.display_name}
+        <ClickBehaviorDescription
+          column={column}
+          clickBehavior={clickBehavior}
+        />
       </h4>
     </div>
   </SidebarItemWrapper>
