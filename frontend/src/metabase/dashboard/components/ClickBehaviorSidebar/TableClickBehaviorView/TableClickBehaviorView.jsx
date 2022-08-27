025b8b91ad5746951e107f9e6c3435c3be91bef0
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-27 04:53:18 +0100

    Untangle `TableClickBehaviorView`

--- frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/TableClickBehaviorView/TableClickBehaviorView.jsx
+++ frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/TableClickBehaviorView/TableClickBehaviorView.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 import { t } from "ttag";
 import _ from "underscore";
 
@@ -10,6 +10,22 @@ import Sidebar from "metabase/dashboard/components/Sidebar";
 import Column from "./Column";
 import { Heading, SidebarHeader } from "../ClickBehaviorSidebar.styled";
 
+const COLUMN_SORTING_ORDER_BY_CLICK_BEHAVIOR_TYPE = [
+  "link",
+  "crossfilter",
+  "actionMenu",
+];
+
+function explainClickBehaviorType(type, dashcard) {
+  return {
+    link: t`Go to custom destination`,
+    crossfilter: t`Update a dashboard filter`,
+    actionMenu: hasActionsMenu(dashcard)
+      ? t`Open the actions menu`
+      : t`Do nothing`,
+  }[type];
+}
+
 function TableClickBehaviorView({
   columns,
   dashcard,
@@ -19,52 +35,59 @@ function TableClickBehaviorView({
   onCancel,
   onClose,
 }) {
+  const groupedColumns = useMemo(() => {
+    const withClickBehaviors = columns.map(column => ({
+      column,
+      clickBehavior: getClickBehaviorForColumn(column),
+    }));
+    const groupedByClickBehavior = _.groupBy(
+      withClickBehaviors,
+      ({ clickBehavior }) => {
+        return clickBehavior?.type || "actionMenu";
+      },
+    );
+
+    const pairs = _.pairs(groupedByClickBehavior);
+    return _.sortBy(pairs, ([type]) =>
+      COLUMN_SORTING_ORDER_BY_CLICK_BEHAVIOR_TYPE.indexOf(type),
+    );
+  }, [columns, getClickBehaviorForColumn]);
+
+  const renderColumn = useCallback(
+    ({ column, clickBehavior }, index) => {
+      return (
+        <Column
+          key={index}
+          column={column}
+          clickBehavior={clickBehavior}
+          onClick={() => onColumnClick(column)}
+        />
+      );
+    },
+    [onColumnClick],
+  );
+
+  const renderColumnGroup = useCallback(
+    group => {
+      const [clickBehaviorType, columnsWithClickBehavior] = group;
+      return (
+        <div key={clickBehaviorType} className="mb2 px4">
+          <h5 className="text-uppercase text-medium my1">
+            {explainClickBehaviorType(clickBehaviorType, dashcard)}
+          </h5>
+          {columnsWithClickBehavior.map(renderColumn)}
+        </div>
+      );
+    },
+    [dashcard, renderColumn],
+  );
+
   return (
     <Sidebar onClose={onClose} onCancel={onCancel} closeIsDisabled={!canClose}>
       <SidebarHeader>
         <Heading className="text-paragraph">{t`On-click behavior for each column`}</Heading>
       </SidebarHeader>
-      <div>
-        {_.chain(columns)
-          .map(column => ({
-            column,
-            clickBehavior: getClickBehaviorForColumn(column),
-          }))
-          .groupBy(({ clickBehavior }) => {
-            const { type = "actionMenu" } = clickBehavior || {};
-            return type;
-          })
-          .pairs()
-          .sortBy(([linkType]) =>
-            ["link", "crossfilter", "actionMenu"].indexOf(linkType),
-          )
-          .map(([linkType, columnsWithClickBehavior]) => (
-            <div key={linkType} className="mb2 px4">
-              <h5 className="text-uppercase text-medium my1">
-                {
-                  {
-                    link: t`Go to custom destination`,
-                    crossfilter: t`Update a dashboard filter`,
-                    actionMenu: hasActionsMenu(dashcard)
-                      ? t`Open the actions menu`
-                      : t`Do nothing`,
-                  }[linkType]
-                }
-              </h5>
-              {columnsWithClickBehavior.map(
-                ({ column, clickBehavior }, index) => (
-                  <Column
-                    key={index}
-                    column={column}
-                    clickBehavior={clickBehavior}
-                    onClick={() => onColumnClick(column)}
-                  />
-                ),
-              )}
-            </div>
-          ))
-          .value()}
-      </div>
+      <div>{groupedColumns.map(renderColumnGroup)}</div>
     </Sidebar>
   );
 }
