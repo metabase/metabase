f2856c6409ea91893e43a81fa54e7c63e339af9f
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-29 20:05:43 +0100

    Extract save dashboard action

--- frontend/src/metabase/dashboard/actions/index.ts
+++ frontend/src/metabase/dashboard/actions/index.ts
@@ -3,6 +3,7 @@ export * from "./core";
 export * from "./data-fetching";
 export * from "./navigation";
 export * from "./parameters";
+export * from "./save";
 export * from "./sharing";
 export * from "./ui";
 export * from "./writeback";
