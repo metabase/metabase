a8ea9b17516a9f0ddf1c95095eea15fb91fd8f64
Anton Kulyk <kuliks.anton@gmail.com>
2022-08-29 19:41:16 +0100

    Introduce data app pages (#25029)
    * Add `is_app_page` to `Dashboard` type
    
    * Add `dataAppPage` form to Dashboard entity
    
    * Use "document" icon for app pages
    
    * Add "Add new page" button
    
    * Drop `isDataApp` dashboard prop
    
    * Hide dashboard-only actions for pages
    
    * Hide fullscreen mode button for pages
    
    * Use "page" word in `DashboardHeader`
    
    * Use "page" word for loading notification UI
    
    * Use "page" word in empty state
    
    * Use "page" word in `DashboardMoveModal`
    
    * Use "page" word in `ArchiveDashboardModal`
    
    * Fix public/embedded dashboard crash
    
    * Fix type error
    
    * Fix type error

--- frontend/src/metabase/dashboard/components/Dashboard/Dashboard.jsx
+++ frontend/src/metabase/dashboard/components/Dashboard/Dashboard.jsx
@@ -38,7 +38,6 @@ class Dashboard extends Component {
     loadDashboardParams: PropTypes.func,
     location: PropTypes.object,
 
-    isDataApp: PropTypes.bool,
     isFullscreen: PropTypes.bool,
     isNightMode: PropTypes.bool,
     isSharing: PropTypes.bool,
@@ -319,10 +318,10 @@ class Dashboard extends Component {
                       {...this.props}
                       isNightMode={shouldRenderAsNightMode}
                       onEditingChange={this.setEditing}
-                      isDataApp={this.props.isDataApp}
                     />
                   ) : (
                     <DashboardEmptyState
+                      isDataApp={dashboard.is_app_page}
                       isNightMode={shouldRenderAsNightMode}
                     />
                   )}
