482b061e6d0abb5b9bb56023e228c22de3f379a4
adam-james <21064735+adam-james-v@users.noreply.github.com>
2022-09-01 11:27:14 -0700

    Format Pie Chart Legend Labels as Month, YYYY (#24890)
    * Format Pie Chart Legend Labels as Month, YYYY
    
    On the backend, the pie chart legend is not rendered in the javascript, it is instead re-created server side as a set
    of html elements. This means "YYYY-MM-DD" formatted dates do not get passed through the frontend code responsible for
    formatting date times (I believe this is done with a library called 'moment.js'). And, since the SVG cannot contain
    html foreignObjects (the backend SVG renederer 'batik' ignores foreign objects), the legend is left as a
    responsiblility for Clojure land.
    
    This PR adds a `format-month` function to handle this case.
    
    Current caveats with this approach:
    
    - doesn't consider localization (yet)
    - adjusted the html to use a table instead of a div full of spans and the formatting could use some tweaks
    - assumes any string that is parse-able into a Jave DateTime object will be displayed in this Month, Year format,
    - which might not be true in all cases
    
    * Improve label formatting by using viz-settings transform fns
    
    * Rework Static Pie Chart legend
    
    Try to make it look a bit nicer:
    - splits legends into 2 tables if there are more than 8 entries
    - larger colored circle
    - use tables to help lay things out more evenly (CSS flexbox doesn't seem to work)
    - handle some legend label formatting cases beyond just timestamps (eg weeks of year, yearly quarters)
    
    Also fixed the failing test
    
    * Adding a month formatter.
    
    Sometimes, the label values are not passed in as timestamps, but just numbers (as strings). In these cases, the
    Datetime formatters won't be useful, so there are some cases built to handle these.
    
    * Remove print statement
    
    * Change temporal format fns to more closely follow viz-settings
    
    Various changes are made to improve static-viz's use of viz-settings from the frontend.
    
    - abbreviation is considered (eg. so "January" is rendered as "Jan" when setting is true)
    - abbreviation also works for Days of week
    - x-of-y cases are handled a bit better
    - separator settings are properly reflected (eg. 2022/05 becomes 2022-05 if separator changed to "-")
    
    * Remove unused key from the let
    
    * Fix some failing tests
    
    * Adding more tests
    
    * Prevent non-temporal legend labels from being rendered via datetime
    
    If a legend's label is some string, we want to pass it unmodified. If it's a number or a timestamp, we can pass it
    through to the date time render function to format the string to match the viz-settings/column units
    
    * Add missed docstring
    
    * Don't lowercase 'D' format, pass correct lowercase 'd' before
    
    'D' format = day of year, which in most human-readable formats is not what we want, but it IS what we want in a
    :day-of-year case.
    
    Instead of always lowercasing, just make sure the lowercase 'd' is in the defaults/overrides
    
    * Better way to handle day of week and month of year
    
    Also got rid of bad use of read-string by using parse-long
    
    * Use a java lib. to format ordinal numbers, respecting site locale

--- src/metabase/pulse/render/body.clj
+++ src/metabase/pulse/render/body.clj
@@ -234,7 +234,7 @@
    :quarter "[Q]Q - YYYY"
    :minute-of-hour "m"
    :day-of-week "dddd"
-   :day-of-month "D"
+   :day-of-month "d"
    :day-of-year "DDD"
    :week-of-year "wo"
    :month-of-year "MMMM"
@@ -247,15 +247,19 @@
                :quarter "YYYY - [Q]Q"}
    "MMMM D, YYYY" {:month "MMMM, YYYY"}
    "D MMMM, YYYY" {:month "MMMM, YYYY"}
-   "dddd, MMMM D, YYYY" {:week "MMMM D, YYYY"
+   "dddd, MMMM D, YYYY" {:day "EEEE, MMMM d, YYYY"
+                         :week "MMMM d, YYYY"
                          :month "MMMM, YYYY"}})
 
 (defn- update-date-style
-  [date-style unit]
+  [date-style unit {::mb.viz/keys [date-abbreviate date-separator]}]
   (let [unit (or unit :default)]
-    (or (get-in override-date-styles [date-style unit])
-        (get-in default-date-styles [unit])
-        date-style)))
+    (cond-> (or (get-in override-date-styles [date-style unit])
+                (get-in default-date-styles [unit])
+                date-style)
+      (not= date-separator "/") (str/replace #"/" date-separator)
+      date-abbreviate (-> (str/replace #"MMMM" "MMM")
+                          (str/replace #"EEEE" "E")))))
 
 (defn- backfill-currency
   [{:keys [number_style currency] :as settings}]
@@ -272,7 +276,7 @@
   [col-settings col]
   (-> (m/map-keys (fn [k] (-> k name (str/replace #"-" "_") keyword)) col-settings)
       (backfill-currency)
-      (u/update-if-exists :date_style update-date-style (:unit col))))
+      (u/update-if-exists :date_style update-date-style (:unit col) col-settings)))
 
 (defn- ->js-viz
   "Include viz settings for js.
@@ -397,8 +401,38 @@
                                    (tru "N/A")
                                    (format-percentage (/ value total)))]))}))
 
+(defn- donut-legend
+  [legend-entries]
+  (letfn [(table-fn [entries]
+            (into [:table {:style (style/style {:color       "#4C5773"
+                                                :font-family "Lato, sans-serif"
+                                                :font-size   "24px"
+                                                :font-weight "bold"
+                                                :box-sizing  "border-box"
+                                                :white-space "nowrap"})}]
+                  (for [{:keys [label percentage color]} entries]
+                    [:tr {:style (style/style {:margin-right "12px"})}
+                     [:td {:style (style/style {:color         color
+                                                :padding-right "7px"
+                                                :line-height   "0"})}
+                      [:span {:style (style/style {:font-size   "2.875rem"
+                                                   :line-height "0"
+                                                   :position    "relative"
+                                                   :top         "-4px"})} "•"]]
+                     [:td {:style (style/style {:padding-right "30px"})}
+                      label]
+                   [:td percentage]])))]
+    (if (< (count legend-entries) 8)
+      (table-fn legend-entries)
+      [:table (into [:tr]
+                    (map (fn [some-entries]
+                           [:td {:style (style/style {:padding-right  "20px"
+                                                      :vertical-align "top"})}
+                            (table-fn some-entries)])
+                         (split-at (/ (count legend-entries) 2) legend-entries)))])))
+
 (s/defmethod render :categorical/donut :- common/RenderedPulseCard
-  [_ render-type _timezone-id :- (s/maybe s/Str) card dashcard {:keys [rows viz-settings] :as data}]
+  [_ render-type timezone-id :- (s/maybe s/Str) card dashcard {:keys [rows cols viz-settings] :as data}]
   (let [viz-settings                (merge viz-settings (:visualization_settings dashcard))
         [x-axis-rowfn y-axis-rowfn] (common/graphing-column-row-fns card data)
         rows                        (map (juxt (comp str x-axis-rowfn) y-axis-rowfn)
@@ -409,8 +443,9 @@
         legend-colors               (merge (zipmap (map first rows) (cycle colors))
                                            (update-keys (:pie.colors viz-settings) name))
         image-bundle                (image-bundle/make-image-bundle
-                                     render-type
-                                     (js-svg/categorical-donut rows legend-colors))]
+                                      render-type
+                                      (js-svg/categorical-donut rows legend-colors))
+        {label-viz-settings :x}     (->js-viz (x-axis-rowfn cols) (y-axis-rowfn cols) viz-settings)]
     {:attachments
      (when image-bundle
        (image-bundle/image-bundle->attachment image-bundle))
@@ -419,17 +454,20 @@
      [:div
       [:img {:style (style/style {:display :block :width :100%})
              :src   (:image-src image-bundle)}]
-      (into [:div {:style (style/style {:clear :both :width "540px" :color "#4C5773"})}]
-            (for [label (map first rows)]
-              [:div {:style (style/style {:float       :left :margin-right "12px"
-                                          :font-family "Lato, sans-serif"
-                                          :font-size   "16px"})}
-               [:span {:style (style/style {:color (legend-colors label)})}
-                "•"]
-               [:span {:style (style/style {:margin-left "6px"})}
-                label]
-               [:span {:style (style/style {:margin-left "6px"})}
-                (percentages label)]]))]}))
+      (donut-legend
+        (mapv (fn [row]
+                (let [label (first row)]
+                  {:percentage (percentages (first row))
+                   :color      (legend-colors (first row))
+                   :label      (if (or (datetime/temporal-string? label)
+                                       (boolean (parse-long label)))
+                                 (datetime/format-temporal-str
+                                   timezone-id
+                                   (first row)
+                                   (x-axis-rowfn cols)
+                                   label-viz-settings)
+                                 label)}))
+              rows))]}))
 
 (s/defmethod render :progress :- common/RenderedPulseCard
   [_ render-type _timezone-id _card dashcard {:keys [cols rows viz-settings] :as _data}]
