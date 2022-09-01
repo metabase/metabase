718939bf12bca185e44de7d4c1fafcbbe821c06a
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-09-01 14:22:59 +0400

    Add support for radio settings with env vars

--- frontend/src/metabase/admin/settings/components/widgets/SettingRadio.jsx
+++ frontend/src/metabase/admin/settings/components/widgets/SettingRadio.jsx
@@ -3,17 +3,24 @@ import React from "react";
 
 import Radio from "metabase/core/components/Radio";
 import cx from "classnames";
+import { SettingPlaceholder } from "./SettingRadio.styled";
 
-const SettingRadio = ({ setting, onChange, disabled }) => (
-  <Radio
-    className={cx({ disabled: disabled })}
-    value={setting.value}
-    onChange={value => onChange(value)}
-    options={Object.entries(setting.options).map(([value, name]) => ({
-      name,
-      value,
-    }))}
-  />
-);
+const SettingRadio = ({ setting, onChange, disabled }) => {
+  if (setting.is_env_setting) {
+    return <SettingPlaceholder>{setting.placeholder}</SettingPlaceholder>;
+  }
+
+  return (
+    <Radio
+      className={cx({ disabled: disabled })}
+      value={setting.value}
+      onChange={value => onChange(value)}
+      options={Object.entries(setting.options).map(([value, name]) => ({
+        name,
+        value,
+      }))}
+    />
+  );
+};
 
 export default SettingRadio;
---
+++ frontend/src/metabase/admin/settings/components/widgets/SettingRadio.styled.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { color } from "metabase/lib/colors";
+
+export const SettingPlaceholder = styled.div`
+  color: ${color("text-light")};
+  font-weight: bold;
+`;
