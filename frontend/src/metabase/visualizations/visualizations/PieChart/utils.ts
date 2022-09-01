c46c03d809aebc5bd7af6a3db83d6f8416bb70b0
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-09-01 17:12:51 +0400

    mute invalid typings

--- frontend/src/metabase/visualizations/visualizations/PieChart/utils.ts
+++ frontend/src/metabase/visualizations/visualizations/PieChart/utils.ts
@@ -2,13 +2,17 @@ export function getMaxLabelDimension(
   d3Arc: d3.svg.Arc<d3.svg.arc.Arc>,
   slice: d3.svg.arc.Arc,
 ) {
-  const index = 0;
-  const innerRadius = d3Arc.innerRadius()(slice, index);
-  const outerRadius = d3Arc.outerRadius()(slice, index);
+  // Invalid typing
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const innerRadius = d3Arc.innerRadius()();
+  // Invalid typing
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const outerRadius = d3Arc.outerRadius()();
   const donutWidth = outerRadius - innerRadius;
 
-  const arcAngle =
-    d3Arc.startAngle()(slice, index) - d3Arc.endAngle()(slice, index);
+  const arcAngle = slice.startAngle - slice.endAngle;
 
   // using law of cosines to calculate the arc length
   // c = sqrt(a^2 + b^2﹣2*a*b * cos(arcAngle))
