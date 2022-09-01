c6a80fe5c7522e7117c0a0611947ee8c5c73d5a8
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2022-09-01 20:59:33 +0200

    [E2E] Handle #17450 failures (#25179)
    * Temporarily skip repro for #17450 for H2 database
    
    * Reproduce #17450 using Postgres

--- frontend/test/metabase/scenarios/admin/databases/add-external.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/databases/add-external.cy.spec.js
@@ -8,12 +8,19 @@ describe("admin > database > add > external databases", () => {
     cy.intercept("POST", "/api/database").as("createDatabase");
   });
 
-  it("should add Postgres database and redirect to listing", () => {
+  it("should add Postgres database and redirect to listing (metabase#17450)", () => {
     cy.visit("/admin/databases/create");
     cy.contains("Database type").closest(".Form-field").find("a").click();
     cy.contains("PostgreSQL").click({ force: true });
+
     cy.findByText("Show advanced options").click();
     cy.contains("Additional JDBC connection string options");
+    // Reproduces metabase#17450
+    cy.findByLabelText("Choose when syncs and scans happen")
+      .click()
+      .should("have.attr", "aria-checked", "true");
+
+    isSyncOptionSelected("Never, I'll do this manually if I need to");
 
     typeAndBlurUsingLabel("Display name", "QA Postgres12");
     typeAndBlurUsingLabel("Host", "localhost");
@@ -22,20 +29,31 @@ describe("admin > database > add > external databases", () => {
     typeAndBlurUsingLabel("Username", "metabase");
     typeAndBlurUsingLabel("Password", "metasample123");
 
-    cy.findByText("Save").should("not.be.disabled").click();
+    cy.button("Save").should("not.be.disabled").click();
 
     cy.wait("@createDatabase");
 
     cy.url().should("match", /\/admin\/databases\?created=true$/);
 
-    cy.findByRole("table").within(() => {
-      cy.findByText("QA Postgres12");
-    });
+    cy.findByText("We're taking a look at your database!");
+    cy.findByLabelText("close icon").click();
 
     cy.findByRole("status").within(() => {
       cy.findByText("Syncing…");
       cy.findByText("Done!");
     });
+
+    cy.findByRole("table").within(() => {
+      cy.findByText("QA Postgres12").click();
+    });
+
+    cy.findByLabelText("Choose when syncs and scans happen").should(
+      "have.attr",
+      "aria-checked",
+      "true",
+    );
+
+    isSyncOptionSelected("Never, I'll do this manually if I need to");
   });
 
   it("should add Mongo database and redirect to listing", () => {
@@ -106,3 +124,8 @@ describe("admin > database > add > external databases", () => {
     });
   });
 });
+
+function isSyncOptionSelected(option) {
+  // This is a really bad way to assert that the text element is selected/active. Can it be fixed in the FE code?
+  cy.findByText(option).parent().should("have.class", "text-brand");
+}
