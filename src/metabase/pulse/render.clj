45299bfd0c689e49e67562381dc2446f445cb580
Adam James <adam.vermeer2@gmail.com>
2022-09-02 14:36:01 -0700

    Remove a few old fns/symbols

--- src/metabase/pulse/render.clj
+++ src/metabase/pulse/render.clj
@@ -58,13 +58,9 @@
                                             :margin-bottom :8px})}
                  description]})))
 
-(defn- number-field?
-  [{base-type :base_type, semantic-type :semantic_type}]
-  (some #(isa? % :type/Number) [base-type semantic-type]))
-
 (defn detect-pulse-chart-type
   "Determine the pulse (visualization) type of a `card`, e.g. `:scalar` or `:bar`."
-  [{display-type :display, card-name :name, :as card} maybe-dashcard {:keys [cols rows insights], :as data}]
+  [{display-type :display, card-name :name, :as card} maybe-dashcard {:keys [cols rows], :as data}]
   (let [col-sample-count          (delay (count (take 3 cols)))
         row-sample-count          (delay (count (take 2 rows)))
         [col-1-rowfn col-2-rowfn] (common/graphing-column-row-fns card data)
@@ -89,11 +85,6 @@
         (and (not= display-type :progress)
              (= @col-sample-count @row-sample-count 1))
         (chart-type :scalar "result has one row and one column")
-
-        #_#_ (and (= display-type :smartscalar)
-                 (= @col-sample-count 2)
-                 (seq insights))
-        (chart-type :smartscalar "result has two columns and insights")
 
         (#{:line
            :area
