9fa48cb05765970139e8afb40b6e24c7972dbe0e
Gustavo Saiani <gustavo@poe.ma>
2022-09-05 11:12:39 -0300

    Extract TablePicker from DataSelector (#25217)

--- frontend/src/metabase/query_builder/components/DataSelector/DataSelector.jsx
+++ frontend/src/metabase/query_builder/components/DataSelector/DataSelector.jsx
@@ -3,7 +3,6 @@ import React, { Component, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { t } from "ttag";
-import cx from "classnames";
 import _ from "underscore";
 
 import {
@@ -14,7 +13,6 @@ import {
 
 import EmptyState from "metabase/components/EmptyState";
 import ListSearchField from "metabase/components/ListSearchField";
-import ExternalLink from "metabase/core/components/ExternalLink";
 import Icon from "metabase/components/Icon";
 import PopoverWithTrigger from "metabase/components/PopoverWithTrigger";
 import AccordionList from "metabase/core/components/AccordionList";
@@ -38,10 +36,9 @@ import {
 } from "./data-search";
 import SavedQuestionPicker from "./saved-question-picker/SavedQuestionPicker";
 import DataSelectorLoading from "./DataSelectorLoading";
-import DataSelectorSectionHeader from "./DataSelectorSectionHeader";
 import FieldPicker from "./DataSelectorFieldPicker";
+import TablePicker from "./DataSelectorTablePicker";
 import {
-  DataSelectorSection,
   DataBucketList,
   DataBucketListItem,
   PickerSpinner,
@@ -1409,111 +1406,4 @@ const DatabaseSchemaPicker = ({
       showItemArrows={hasNextStep}
     />
   );
-};
-
-const TablePicker = ({
-  schemas,
-  tables,
-  selectedDatabase,
-  selectedSchema,
-  selectedTable,
-  onChangeTable,
-  hasNextStep,
-  onBack,
-  isLoading,
-  hasFiltering,
-  minTablesToShowSearch = 10,
-  hasInitialFocus,
-}) => {
-  // In case DataSelector props get reseted
-  if (!selectedDatabase) {
-    if (onBack) {
-      onBack();
-    }
-    return null;
-  }
-
-  const isSavedQuestionList = selectedDatabase.is_saved_questions;
-  const header = (
-    <div className="flex flex-wrap align-center">
-      <span
-        className={cx("flex align-center", {
-          "text-brand-hover cursor-pointer": onBack,
-        })}
-        onClick={onBack}
-      >
-        {onBack && <Icon name="chevronleft" size={18} />}
-        <span className="ml1 text-wrap">{selectedDatabase.name}</span>
-      </span>
-      {selectedSchema?.name && schemas.length > 1 && (
-        <span className="ml1 text-wrap text-slate">
-          - {selectedSchema.displayName()}
-        </span>
-      )}
-    </div>
-  );
-
-  if (tables.length > 0 || isLoading) {
-    const sections = [
-      {
-        name: header,
-        items: tables.filter(Boolean).map(table => ({
-          name: table.displayName(),
-          table: table,
-          database: selectedDatabase,
-        })),
-        loading: tables.length === 0 && isLoading,
-      },
-    ];
-    return (
-      <div
-        style={{ width: 300, overflowY: "auto" }}
-        data-testid="data-selector"
-      >
-        <AccordionList
-          id="TablePicker"
-          key="tablePicker"
-          className="text-brand"
-          hasInitialFocus={hasInitialFocus}
-          sections={sections}
-          maxHeight={Infinity}
-          width="100%"
-          searchable={hasFiltering && tables.length >= minTablesToShowSearch}
-          onChange={item => onChangeTable(item.table)}
-          itemIsSelected={item =>
-            item.table && selectedTable
-              ? item.table.id === selectedTable.id
-              : false
-          }
-          itemIsClickable={item => item.table && isSyncCompleted(item.table)}
-          renderItemIcon={item =>
-            item.table ? <Icon name="table2" size={18} /> : null
-          }
-          showItemArrows={hasNextStep}
-        />
-        {isSavedQuestionList && (
-          <div className="bg-light p2 text-centered border-top">
-            {t`Is a question missing?`}
-            <ExternalLink
-              href={MetabaseSettings.docsUrl(
-                "questions/native-editor/referencing-saved-questions-in-queries",
-              )}
-              target="_blank"
-              className="block link"
-            >
-              {t`Learn more about nested queries`}
-            </ExternalLink>
-          </div>
-        )}
-      </div>
-    );
-  } else {
-    // this is a database with no tables!
-    return (
-      <DataSelectorSection>
-        <DataSelectorSectionHeader header={header} />
-        <div className="p4 text-centered">{t`No tables found in this database.`}</div>
-      </DataSelectorSection>
-    );
-  }
 };
