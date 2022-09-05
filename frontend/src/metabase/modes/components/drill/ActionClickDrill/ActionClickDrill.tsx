208a188abc20ca85e75ad482272ab6de630e9dc2
Anton Kulyk <kuliks.anton@gmail.com>
2022-09-05 13:58:24 +0100

    Open parameter input modal from `ActionClickDrill`

--- frontend/src/metabase/modes/components/drill/ActionClickDrill/ActionClickDrill.tsx
+++ frontend/src/metabase/modes/components/drill/ActionClickDrill/ActionClickDrill.tsx
@@ -1,11 +1,14 @@
 import { getDataFromClicked } from "metabase/lib/click-behavior";
 
-import { executeRowAction } from "metabase/dashboard/actions";
+import {
+  executeRowAction,
+  openActionParametersModal,
+} from "metabase/dashboard/actions";
 
 import type { ParameterMappedForActionExecution } from "metabase-types/api";
 import type { ActionClickObject } from "./types";
 
-import { prepareParameter } from "./utils";
+import { prepareParameter, getNotProvidedActionParameters } from "./utils";
 
 function ActionClickDrill({ clicked }: { clicked: ActionClickObject }) {
   const { dashboard, dashcard } = clicked.extraData;
@@ -17,25 +20,49 @@ function ActionClickDrill({ clicked }: { clicked: ActionClickObject }) {
 
   const parameters: ParameterMappedForActionExecution[] = [];
   const data = getDataFromClicked(clicked);
+  const parameterMappings = dashcard.parameter_mappings || [];
 
-  dashcard.parameter_mappings?.forEach?.(mapping => {
+  parameterMappings.forEach(mapping => {
     const parameter = prepareParameter(mapping, { action, data });
     if (parameter) {
       parameters.push(parameter);
     }
   });
 
+  const missingParameters = getNotProvidedActionParameters(
+    action,
+    parameterMappings,
+    parameters,
+  );
+
+  function clickAction() {
+    if (missingParameters.length > 0) {
+      return openActionParametersModal({
+        dashcardId: dashcard.id,
+        props: {
+          missingParameters,
+          onSubmit: (filledParameters: ParameterMappedForActionExecution[]) =>
+            executeRowAction({
+              dashboard,
+              dashcard,
+              parameters: [...parameters, ...filledParameters],
+            }),
+        },
+      });
+    }
+    return executeRowAction({
+      dashboard,
+      dashcard,
+      parameters,
+    });
+  }
+
   return [
     {
       name: "click_behavior",
       default: true,
       defaultAlways: true,
-      action: () =>
-        executeRowAction({
-          dashboard,
-          dashcard,
-          parameters,
-        }),
+      action: clickAction,
     },
   ];
 }
