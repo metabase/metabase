376b4a83de7c3ad5227a5d0ed8478ad3ccda331f
Gustavo Saiani <gustavo@poe.ma>
2022-09-05 10:43:33 -0300

    Test isLoading

--- frontend/src/metabase/query_builder/components/DataSelector/DataSelectorFieldPicker/DataSelectorFieldPicker.unit.spec.js
+++ frontend/src/metabase/query_builder/components/DataSelector/DataSelectorFieldPicker/DataSelectorFieldPicker.unit.spec.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import DataSelectorFieldPicker from "./DataSelectorFieldPicker";
 
-describe("something", () => {
-  it("something", () => {
-    render(<DataSelectorFieldPicker />);
-    screen.getByText("wrong");
+describe("DataSelectorFieldPicker", () => {
+  describe("when loading", () => {
+    it("uses 'Fields' as title if selectedTable not passed", () => {
+      render(<DataSelectorFieldPicker isLoading={true} />);
+
+      screen.getByText("Fields");
+    });
+
+    it("uses table display name as title if passed", () => {
+      const displayName = "Display name";
+
+      render(
+        <DataSelectorFieldPicker
+          isLoading={true}
+          selectedTable={{ display_name: displayName }}
+        />,
+      );
+
+      screen.getByText(displayName);
+    });
+  });
+
+  it("goes back if clicked", () => {
+    const onBack = jest.fn();
+
+    render(<DataSelectorFieldPicker isLoading={true} onBack={onBack} />);
+
+    fireEvent.click(screen.getByText("Fields"));
+
+    expect(onBack).toHaveBeenCalledTimes(1);
   });
 });
