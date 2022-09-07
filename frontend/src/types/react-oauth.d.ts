256bf3f94a53189f9a6c1256de40a9e326bb8789
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-09-07 20:39:08 +0300

    Fix settings types

--- frontend/src/types/react-oauth.d.ts
+++ frontend/src/types/react-oauth.d.ts
@@ -1,6 +1,6 @@
-declare module "@react-oauth/google" {
-  type ComponentType = import("react").ComponentType;
+import { ComponentType } from "react";
 
+declare module "@react-oauth/google" {
   interface GoogleOAuthProviderProps {
     clientId: string;
   }
