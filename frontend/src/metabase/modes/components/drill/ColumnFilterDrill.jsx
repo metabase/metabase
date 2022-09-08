3c5760a7ee20a359a86c5158a97c1678e232c9af
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-09-08 15:01:11 +0300

    Fix filters not working for columns in multi-stage queries (#25259) (#25289)

--- frontend/src/metabase/modes/components/drill/ColumnFilterDrill.jsx
+++ frontend/src/metabase/modes/components/drill/ColumnFilterDrill.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { t } from "ttag";
 import { TYPE, isa } from "metabase/lib/types";
 
-import Filter from "metabase-lib/lib/queries/structured/Filter";
 import FilterPopover from "metabase/query_builder/components/filters/FilterPopover";
 
 const INVALID_TYPES = [TYPE.Structured];
@@ -22,18 +21,8 @@ export default function ColumnFilterDrill({ question, clicked }) {
     return [];
   }
 
-  const { column } = clicked;
-
-  // if our field ref doesn't match our column id, we have a remapped column
-  // and need to use the field id instead of the ref
-  const fieldRef =
-    column.id !== undefined && column.id !== column.field_ref[1]
-      ? ["field", column.id, null]
-      : column.field_ref;
-
-  const initialFilter = new Filter([], null, query).setDimension(fieldRef, {
-    useDefaultOperator: true,
-  });
+  const { dimension } = clicked;
+  const initialFilter = dimension.defaultFilterForDimension();
 
   return [
     {
