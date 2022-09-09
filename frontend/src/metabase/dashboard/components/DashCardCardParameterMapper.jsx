17d4cea5ebd2314d8b107afa0bd4159b5431ee0b
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-09-09 12:40:28 +0300

    Add a special state for native questions

--- frontend/src/metabase/dashboard/components/DashCardCardParameterMapper.jsx
+++ frontend/src/metabase/dashboard/components/DashCardCardParameterMapper.jsx
@@ -244,7 +244,7 @@ function DashCardCardParameterMapper({
           </Tooltip>
         </>
       )}
-      {onlyAcceptsSingleValue && (
+      {onlyAcceptsSingleValue && !isDisabled && (
         <Warning>
           {t`This field only accepts a single value because it's used in a SQL query.`}
         </Warning>
