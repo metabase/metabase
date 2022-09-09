190f81fbeabfcf5bc3f9082eb6c3ceb5948b7e73
Braden Shepherdson <braden@metabase.com>
2022-09-09 09:54:53 -0400

    Serdes v2: Handle missing User refs by synthesizing dummy users
    Many entities have `creator_id` and similar fields. `User`s are not
    serialized. Foreign keys to users are serialized as email addresses.
    
    During deserialization in a different instance (eg. a local dev instance
    importing a dump from a prod instance) many such users may not exist.
    This change creates new `User` entities on the fly with empty names,
    generated passwords, and the email set.

--- src/metabase/models/user.clj
+++ src/metabase/models/user.clj
@@ -280,6 +280,9 @@
   [new-user :- NewUser]
   (db/insert! User (update new-user :password #(or % (str (UUID/randomUUID))))))
 
+(defn serdes-synthesize-user! [new-user]
+  (insert-new-user! new-user))
+
 (s/defn create-and-invite-user!
   "Convenience function for inviting a new `User` and sending out the welcome email."
   [new-user :- NewUser, invitor :- Invitor, setup? :- s/Bool]
