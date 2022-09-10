79cd445cf0b602ef29c18b7f9d4469ac561c386c
Anton Kulyk <kuliks.anton@gmail.com>
2022-09-10 02:19:17 +0100

    Respect data app homepage setting

--- frontend/src/metabase/entities/data-apps/utils.unit.spec.ts
+++ frontend/src/metabase/entities/data-apps/utils.unit.spec.ts
@@ -1,18 +1,45 @@
-import { createMockDataAppPage } from "metabase-types/api/mocks";
+import {
+  createMockDataApp,
+  createMockDataAppPage,
+} from "metabase-types/api/mocks";
 import { getDataAppHomePageId } from "./utils";
 
 describe("data app utils", () => {
-  describe("getDataAppHomePageId", () => {
-    it("returns fist page in alphabetical order", () => {
-      const page1 = createMockDataAppPage({ id: 1, name: "A" });
-      const page2 = createMockDataAppPage({ id: 2, name: "B" });
-      const page3 = createMockDataAppPage({ id: 3, name: "C" });
+  const dataAppWithoutHomepage = createMockDataApp({ dashboard_id: null });
+  const dataAppWithHomepage = createMockDataApp({ dashboard_id: 3 });
 
-      expect(getDataAppHomePageId([page2, page1, page3])).toEqual(page1.id);
+  const page1 = createMockDataAppPage({ id: 1, name: "A" });
+  const page2 = createMockDataAppPage({ id: 2, name: "B" });
+  const page3 = createMockDataAppPage({ id: 3, name: "C" });
+  const pages = [page1, page2, page3];
+
+  describe("getDataAppHomePageId", () => {
+    describe("with explicit homepage", () => {
+      it("returns data app's dashboard_id", () => {
+        expect(getDataAppHomePageId(dataAppWithHomepage, pages)).toEqual(
+          dataAppWithHomepage.dashboard_id,
+        );
+      });
+
+      it("returns data app's dashboard_id even if page list is empty", () => {
+        expect(getDataAppHomePageId(dataAppWithHomepage, [])).toEqual(
+          dataAppWithHomepage.dashboard_id,
+        );
+      });
     });
 
-    it("returns undefined when there're no pages", () => {
-      expect(getDataAppHomePageId([])).toBeUndefined();
+    describe("without explicit homepage", () => {
+      it("returns fist page in alphabetical order", () => {
+        expect(getDataAppHomePageId(dataAppWithoutHomepage, pages)).toEqual(
+          page1.id,
+        );
+      });
+
+      it("returns undefined when there're no pages", () => {
+        expect(
+          getDataAppHomePageId(dataAppWithoutHomepage, []),
+        ).toBeUndefined();
+      });
     });
   });
 });
