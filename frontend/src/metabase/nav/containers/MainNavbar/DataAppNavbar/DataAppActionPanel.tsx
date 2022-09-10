2975442faf6d81412a6163c8f113228521583f4d
Anton Kulyk <kuliks.anton@gmail.com>
2022-09-10 04:05:36 +0100

    Extract `DataAppActionPanel`

---
+++ frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbar/DataAppActionPanel.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import _ from "underscore";
+import { t } from "ttag";
+
+import ButtonGroup from "metabase/core/components/ButtonGroup";
+import Link from "metabase/core/components/Link";
+import Tooltip from "metabase/components/Tooltip";
+
+import * as Urls from "metabase/lib/urls";
+
+import type { DataApp } from "metabase-types/api";
+
+import {
+  DataAppActionsContainer,
+  DataAppActionButton,
+  ExitDataAppButton,
+} from "../MainNavbar.styled";
+
+interface Props {
+  dataApp: DataApp;
+  onEditAppSettings: () => void;
+}
+
+function DataAppActionPanel({ dataApp, onEditAppSettings }: Props) {
+  return (
+    <DataAppActionsContainer>
+      <ButtonGroup>
+        <Tooltip tooltip={t`Add`}>
+          <DataAppActionButton icon="add" onlyIcon />
+        </Tooltip>
+        <Tooltip tooltip={t`Settings`}>
+          <DataAppActionButton
+            icon="gear"
+            onClick={onEditAppSettings}
+            onlyIcon
+          />
+        </Tooltip>
+      </ButtonGroup>
+      <ExitDataAppButton
+        as={Link}
+        to={Urls.dataApp(dataApp, { mode: "preview" })}
+      >{t`Exit app`}</ExitDataAppButton>
+    </DataAppActionsContainer>
+  );
+}
+
+export default DataAppActionPanel;
