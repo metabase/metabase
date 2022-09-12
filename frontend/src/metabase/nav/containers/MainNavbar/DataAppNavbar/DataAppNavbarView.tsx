fdaa759afa0b0515caefff0e0027163bf590c31a
Anton Kulyk <kuliks.anton@gmail.com>
2022-09-12 18:50:51 +0100

    Group nav sidebar files (#25359)
    * Move `DataAppNavbar` to its own directory
    
    * Move `MainNavbarContainer` to its own directory
    
    * Move `BookmarksList` to `MainNavbarContainer`
    
    * Extract `getSelectedItems`
    
    * Extract `DataAppActionPanel`

--- frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbar/DataAppNavbarView.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbar/DataAppNavbarView.tsx
@@ -2,26 +2,20 @@ import React from "react";
 import _ from "underscore";
 import { t } from "ttag";
 
-import ButtonGroup from "metabase/core/components/ButtonGroup";
-import Link from "metabase/core/components/Link";
-import Tooltip from "metabase/components/Tooltip";
-
 import * as Urls from "metabase/lib/urls";
 
 import type { DataApp } from "metabase-types/api";
 
-import { MainNavbarProps, SelectedItem } from "./types";
+import { MainNavbarProps, SelectedItem } from "../types";
+import DataAppActionPanel from "./DataAppActionPanel";
 import {
-  DataAppActionsContainer,
-  DataAppActionButton,
   DataAppNewButton,
-  ExitDataAppButton,
   PaddedSidebarLink,
   SidebarContentRoot,
   SidebarHeading,
   SidebarHeadingWrapper,
   SidebarSection,
-} from "./MainNavbar.styled";
+} from "../MainNavbar.styled";
 
 interface Props extends MainNavbarProps {
   dataApp: DataApp;
@@ -69,24 +63,10 @@ function DataAppNavbarView({
           >{t`Add new page`}</DataAppNewButton>
         </div>
       </div>
-      <DataAppActionsContainer>
-        <ButtonGroup>
-          <Tooltip tooltip={t`Add`}>
-            <DataAppActionButton icon="add" onlyIcon />
-          </Tooltip>
-          <Tooltip tooltip={t`Settings`}>
-            <DataAppActionButton
-              icon="gear"
-              onClick={onEditAppSettings}
-              onlyIcon
-            />
-          </Tooltip>
-        </ButtonGroup>
-        <ExitDataAppButton
-          as={Link}
-          to={Urls.dataApp(dataApp, { mode: "preview" })}
-        >{t`Exit app`}</ExitDataAppButton>
-      </DataAppActionsContainer>
+      <DataAppActionPanel
+        dataApp={dataApp}
+        onEditAppSettings={onEditAppSettings}
+      />
     </SidebarContentRoot>
   );
 }
