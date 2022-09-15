db7299af80814a638e8b3f91e2384f7f7567ff38
Callum Herries <hi@callumherries.com>
2022-09-15 14:04:10 +0200

    Refactor updateQuestionTagNames

--- frontend/src/metabase-lib/lib/queries/utils/native-query.ts
+++ frontend/src/metabase-lib/lib/queries/utils/native-query.ts
@@ -3,35 +3,33 @@ import _ from "underscore";
 
 import type NativeQuery from "../NativeQuery";
 
-export function updateReferencedQuestionTagName(
-  queryText: string,
-  question: any,
-  oldTagName: string,
-) {
-  const newTagName = `#${question.id}-${slugg(question.name)}`;
-  return queryText.replace(
-    new RegExp(`{{\\s*${oldTagName}\\s*}}`, "g"),
-    `{{${newTagName}}}`,
-  );
-}
-
-export function updateReferencedQuestionNames(
+export function updateQuestionTagNames(
   query: NativeQuery,
   questions: any,
 ): NativeQuery {
   const questionsById = _.indexBy(questions, "id");
   const newQueryText = query
     .templateTags()
-    // only tags for questions
-    .filter(tag => tag.type === "card")
-    // only tags that match given questions
-    .filter(tag => tag["card-id"] && questionsById[tag["card-id"]])
-    // for each tag, update the tag name in the queryText
+    .filter(tag => tag.type === "card") // only tags for questions
+    .filter(tag => tag["card-id"] && questionsById[tag["card-id"]]) // only tags for given questions
     .reduce((qText, tag) => {
-      const question = tag["card-id"] && questionsById[tag["card-id"]];
-      return updateReferencedQuestionTagName(qText, question, tag.name);
+      // reduce over each tag, updating query text
+      const question = tag["card-id"] && questionsById[tag["card-id"]]; // get question for tag
+      const newTagName = `#${question.id}-${slugg(question.name)}`; // calculate tag name for question
+      return replaceTagName(qText, tag.name, newTagName); // update tag name in query text
     }, query.queryText());
-  return newQueryText === query.queryText()
-    ? query
-    : query.setQueryText(newQueryText);
+  return newQueryText !== query.queryText() // return a new query with the updated text
+    ? query.setQueryText(newQueryText)
+    : query;
+}
+
+function replaceTagName(
+  queryText: string,
+  oldTagName: string,
+  newTagName: string,
+) {
+  return queryText.replace(
+    new RegExp(`{{\\s*${oldTagName}\\s*}}`, "g"),
+    `{{${newTagName}}}`,
+  );
 }
