8a3349019f2e1279c482a2faaa6806269dcd2bb3
Sheikh Al Amin Sunoyon <sunoyon@gmail.com>
2022-09-15 15:00:28 +0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,12 @@ set -x
 print_usage_and_exit () {
     echo "Usage: sh deploy.sh IMAGE_VERSION"
     echo "  IMAGE_VERSION (required) can be any docker image tag"
-    echo '  Following variables should be set as part environment variables:'
-    echo "    - REPO_USERNAME"
-    echo "    - REPO_PASSWORD"
     exit 1
 }
 
 if [ -z "$1" ] ; then
     echo 'Error: Argument IMAGE_VERSION is required in position 1.'
     print_usage_and_exit
-fi
-if [ -z "$REPO_USERNAME" ] ; then
-   echo 'Error: REPO_USERNAME is not found as environment variable.'
-   print_usage_and_exit
-fi
-if [ -z "$REPO_PASSWORD" ] ; then
-   echo 'Error: REPO_PASSWORD is not found as environment variable.'
-   print_usage_and_exit
 fi
 
 IMAGE_VERSION=${1}
@@ -34,7 +23,5 @@ cd ./bin/docker/
 docker build -t dsinnovators/metabase:${IMAGE_VERSION} .
 
 echo "Pushing docker image ..."
-docker logout
-docker login -u ${REPO_USERNAME} -p ${REPO_PASSWORD}
 docker push dsinnovators/metabase:${IMAGE_VERSION}
 echo "Done"
