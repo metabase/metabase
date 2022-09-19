f39f6c56bebf31ff94a2aec336cd7dd19691fe53
Anton Kulyk <kuliks.anton@gmail.com>
2022-09-19 11:30:57 +0100

    Load model object

--- frontend/src/metabase/models/containers/ModelDetailPage/ModelDetailPage.tsx
+++ frontend/src/metabase/models/containers/ModelDetailPage/ModelDetailPage.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-function ModelDetailPage() {
-  return <span>Model Detail Page</span>;
+import * as Urls from "metabase/lib/urls";
+
+import Questions from "metabase/entities/questions";
+
+import type { Card } from "metabase-types/api";
+import type { State } from "metabase-types/store";
+
+type OwnProps = {
+  params: {
+    id: string;
+  };
+};
+
+type EntityLoaderProps = {
+  modelCard: Card;
+};
+
+type Props = OwnProps & EntityLoaderProps;
+
+function ModelDetailPage({ modelCard }: Props) {
+  return <span>{modelCard?.name}</span>;
 }
 
-export default ModelDetailPage;
+function getModelId(state: State, props: OwnProps) {
+  return Urls.extractEntityId(props.params.id);
+}
+
+export default Questions.load({ id: getModelId, entityAlias: "modelCard" })(
+  ModelDetailPage,
+);
