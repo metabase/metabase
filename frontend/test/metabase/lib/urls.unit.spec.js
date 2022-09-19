f11c993c4293013a046e6a6b210f840c2d671f41
Ryan Laurie <iethree@gmail.com>
2022-09-19 13:48:37 -0600

    update model bookmark url test

--- frontend/test/metabase/lib/urls.unit.spec.js
+++ frontend/test/metabase/lib/urls.unit.spec.js
@@ -290,7 +290,7 @@ describe("urls", () => {
           name: "Product",
           type: "card",
         }),
-      ).toBe("/model/1-product");
+      ).toBe("/model/1-product/detail");
     });
 
     it("returns dashboard bookmark path", () => {
