c4364dc9c3804453fef3dbb96831fc9c48766c2e
Anton Kulyk <kuliks.anton@gmail.com>
2022-09-19 15:03:32 +0100

    Fix info panel look

--- frontend/src/metabase/models/components/ModelDetailPage/ModelInfoSidePanel/ModelInfoSidePanel.styled.tsx
+++ frontend/src/metabase/models/components/ModelDetailPage/ModelInfoSidePanel/ModelInfoSidePanel.styled.tsx
@@ -1,20 +1,41 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+import EditableText from "metabase/core/components/EditableText";
 
 import { color } from "metabase/lib/colors";
 
+export const ModelInfoSection = styled.div``;
+
 export const ModelInfoPanel = styled.div`
-  padding-left: 3rem;
+  padding-left: 2rem;
   border-left: 1px solid ${color("border")};
-  min-width: 15rem;
+  width: 15rem;
+
+  ${ModelInfoSection}:not(:first-of-type) {
+    margin-top: 1rem;
+  }
 `;
 
-export const ModelInfoTitle = styled.p`
+export const ModelInfoTitle = styled.span`
+  display: block;
   color: ${color("text-dark")};
   font-weight: 600;
 
-  margin: 0;
+  padding-left: 4px;
 `;
 
-export const ModelInfoText = styled.p`
+const commonInfoTextStyle = css`
+  margin-top: 0.5rem;
   color: ${color("text-medium")};
+  padding-left: 4px;
+`;
+
+export const ModelInfoText = styled.span`
+  display: block;
+  ${commonInfoTextStyle}
+`;
+
+export const ModelDescription = styled(EditableText)`
+  ${commonInfoTextStyle}
 `;
--- frontend/src/metabase/models/components/ModelDetailPage/ModelInfoSidePanel/ModelInfoSidePanel.tsx
+++ frontend/src/metabase/models/components/ModelDetailPage/ModelInfoSidePanel/ModelInfoSidePanel.tsx
@@ -1,8 +1,6 @@
 import React, { useMemo } from "react";
 import { t } from "ttag";
 
-import EditableText from "metabase/core/components/EditableText";
-
 import type { Card } from "metabase-types/api";
 import type Question from "metabase-lib/lib/Question";
 
@@ -10,6 +8,8 @@ import {
   ModelInfoPanel,
   ModelInfoTitle,
   ModelInfoText,
+  ModelInfoSection,
+  ModelDescription,
 } from "./ModelInfoSidePanel.styled";
 
 interface Props {
@@ -27,28 +27,30 @@ function ModelInfoSidePanel({ model, onChangeDescription }: Props) {
 
   return (
     <ModelInfoPanel>
-      <ModelInfoTitle>{t`Description`}</ModelInfoTitle>
-      <EditableText
-        initialValue={description}
-        placeholder={
-          !description && !canWrite ? t`No description` : t`Add description`
-        }
-        isOptional
-        isMultiline
-        isDisabled={!canWrite}
-        onChange={onChangeDescription}
-      />
+      <ModelInfoSection>
+        <ModelInfoTitle>{t`Description`}</ModelInfoTitle>
+        <ModelDescription
+          initialValue={description}
+          placeholder={
+            !description && !canWrite ? t`No description` : t`Add description`
+          }
+          isOptional
+          isMultiline
+          isDisabled={!canWrite}
+          onChange={onChangeDescription}
+        />
+      </ModelInfoSection>
       {modelCard.creator && (
-        <>
+        <ModelInfoSection>
           <ModelInfoTitle>{t`Contact`}</ModelInfoTitle>
           <ModelInfoText>{modelCard.creator.common_name}</ModelInfoText>
-        </>
+        </ModelInfoSection>
       )}
       {backingTable && (
-        <>
+        <ModelInfoSection>
           <ModelInfoTitle>{t`Backing table`}</ModelInfoTitle>
           <ModelInfoText>{backingTable.displayName()}</ModelInfoText>
-        </>
+        </ModelInfoSection>
       )}
     </ModelInfoPanel>
   );
