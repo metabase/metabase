21b5920b320688aaa2f41863de32fb2bed803b4c
losrebellos <ben@benoitvinay.com>
2022-09-19 11:20:36 +0200

    Fixed types

--- frontend/src/metabase-lib/lib/metadata/utils.ts
+++ frontend/src/metabase-lib/lib/metadata/utils.ts
@@ -3,9 +3,9 @@ import type Field from "./Field";
 import type Table from "./Table";
 
 export function getUniqueFieldId(
-  id: Pick<Field, "id">,
-  tableId: Pick<Table, "id">,
-): string | Pick<Field, "id"> {
+  id: Field["id"],
+  tableId: Table["id"],
+): string | Field["id"] {
   if (isVirtualCardId(tableId)) {
     return `${tableId}:${id}`;
   }
