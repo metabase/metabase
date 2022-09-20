348d5faf35c5d97077c2ba48ffb6163dba342542
Anton Kulyk <kuliks.anton@gmail.com>
2022-09-20 19:19:16 +0100

    Fix duplicates

--- frontend/src/metabase/models/components/ModelDetailPage/ModelInfoSidePanel/ModelRelationships.tsx
+++ frontend/src/metabase/models/components/ModelDetailPage/ModelInfoSidePanel/ModelRelationships.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from "react";
 import { t } from "ttag";
+import _ from "underscore";
 
 import Icon from "metabase/components/Icon";
 
@@ -18,7 +19,10 @@ function ModelRelationships({ model, mainTable }: Props) {
   const relatedTables = useMemo(() => {
     const tablesMainTablePointsTo = model.table()?.foreignTables() || [];
     const tablesPointingToMainTable = mainTable?.connectedTables() || [];
-    return [...tablesMainTablePointsTo, ...tablesPointingToMainTable];
+    return _.uniq(
+      [...tablesMainTablePointsTo, ...tablesPointingToMainTable],
+      table => table.id,
+    );
   }, [model, mainTable]);
 
   if (relatedTables.length <= 0) {
