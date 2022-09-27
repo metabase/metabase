4f26a50d891e38e0b078f9550e020b07e56d8aa2
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2022-09-27 10:40:03 -0600

    Action Creator 9a: Simplify action click drill (#25579)
    * simplify action click drill
    
    * better validator type

--- frontend/src/metabase/writeback/components/ActionViz/Action.tsx
+++ frontend/src/metabase/writeback/components/ActionViz/Action.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
+import { t } from "ttag";
+import { connect } from "react-redux";
 
 import { isImplicitActionButton } from "metabase/writeback/utils";
 
-import type { ActionDashboardCard, Dashboard } from "metabase-types/api";
+import type {
+  ActionDashboardCard,
+  ArbitraryParameterForActionExecution,
+  ActionParametersMapping,
+  Dashboard,
+  ParameterMappedForActionExecution,
+  WritebackQueryAction,
+} from "metabase-types/api";
 import type { VisualizationProps } from "metabase-types/types/Visualization";
+import type { Dispatch } from "metabase-types/store";
+import type { ParameterValueOrArray } from "metabase-types/types/Parameter";
+
+import {
+  getDashcardParamValues,
+  getNotProvidedActionParameters,
+  prepareParameter,
+} from "metabase/modes/components/drill/ActionClickDrill/utils";
+import { executeRowAction } from "metabase/dashboard/actions";
+import { StyledButton } from "./ActionButton.styled";
 
 import DefaultActionButton from "./DefaultActionButton";
 import ImplicitActionButton from "./ImplicitActionButton";
@@ -11,13 +30,77 @@ import ImplicitActionButton from "./ImplicitActionButton";
 interface ActionProps extends VisualizationProps {
   dashcard: ActionDashboardCard;
   dashboard: Dashboard;
+  dispatch: Dispatch;
+  parameterValues: { [id: string]: ParameterValueOrArray };
 }
 
-function Action({ dashcard, ...props }: ActionProps) {
+function ActionComponent({
+  dashcard,
+  dashboard,
+  dispatch,
+  isSettings,
+  settings,
+  getExtraDataForClick,
+  onVisualizationClick,
+  parameterValues,
+}: ActionProps) {
+  const dashcardParamValues = useMemo(
+    () => getDashcardParamValues(dashcard, parameterValues),
+    [dashcard, parameterValues],
+  );
+
+  const missingParameters = useMemo(() => {
+    if (!dashcard.action) {
+      return [];
+    }
+    return getNotProvidedActionParameters(
+      dashcard.action,
+      dashcardParamValues ?? [],
+    );
+  }, [dashcard, dashcardParamValues]);
+
+  const onSubmit = useCallback(
+    (extra_parameters: ArbitraryParameterForActionExecution[]) =>
+      executeRowAction({
+        dashboard,
+        dashcard,
+        parameters: dashcardParamValues,
+        extra_parameters,
+        dispatch,
+        shouldToast: true,
+      }),
+    [dashboard, dashcard, dashcardParamValues, dispatch],
+  );
+
   if (isImplicitActionButton(dashcard)) {
-    return <ImplicitActionButton {...props} />;
+    return <ImplicitActionButton isSettings={isSettings} settings={settings} />;
   }
-  return <DefaultActionButton {...props} />;
+
+  return (
+    <DefaultActionButton
+      onSubmit={onSubmit}
+      missingParameters={missingParameters}
+      isSettings={isSettings}
+      settings={settings}
+      getExtraDataForClick={getExtraDataForClick}
+      onVisualizationClick={onVisualizationClick}
+    />
+  );
 }
 
-export default Action;
+const ConnectedActionComponent = connect()(ActionComponent);
+
+export default function Action(props: ActionProps) {
+  if (
+    !props.dashcard?.action &&
+    !props.dashcard?.visualization_settings?.click_behavior
+  ) {
+    return (
+      <StyledButton>
+        <strong>{t`Assign an action`}</strong>
+      </StyledButton>
+    );
+  }
+
+  return <ConnectedActionComponent {...props} />;
+}
