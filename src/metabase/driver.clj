bb6889e71c9cb8c0853e3c72019ed41280d2e5a0
Ngoc Khuat <qn.khuat@gmail.com>
2022-09-29 19:45:03 +0700

    temporarily disable a test and bring everything up

--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -476,7 +476,7 @@
 (defmethod supports? [::driver :case-sensitivity-string-filter-options] [_ _] true)
 (defmethod supports? [::driver :date-arithmetics] [_ _] true)
 (defmethod supports? [::driver :temporal-extract] [_ _] true)
-(defmethod supports? [::driver :convert-timezone] [_ _] false)
+(defmethod supports? [::driver :convert-timezone] [_ _] true)
 
 (defmulti database-supports?
   "Does this driver and specific instance of a database support a certain `feature`?
