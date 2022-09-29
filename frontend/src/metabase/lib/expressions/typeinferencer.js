14335899bb10c2a07c03476b98f59f8436f43978
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-09-29 12:00:29 +0300

    Fix case

--- frontend/src/metabase/lib/expressions/typeinferencer.js
+++ frontend/src/metabase/lib/expressions/typeinferencer.js
@@ -35,7 +35,7 @@ export function infer(mbql, env) {
 
   switch (op) {
     case "case":
-      return infer(mbql[1][0], env);
+      return infer(mbql[1][0][1], env);
     case "coalesce":
     case "date-add":
     case "date-subtract":
