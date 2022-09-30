5c289c44e35dfa361a0cde3d41a517afa47ff68e
Ngoc Khuat <qn.khuat@gmail.com>
2022-10-01 00:11:16 +0700

    regex check

--- src/metabase/util/honeysql_extensions.clj
+++ src/metabase/util/honeysql_extensions.clj
@@ -215,9 +215,9 @@
   (is-of-type? expr #\"datetime\") ; -> true"
   [honeysql-form database-type]
   (if-let [db-type (some-> honeysql-form type-info type-info->db-type str/lower-case)]
-    (if (string? database-type)
-      (= db-type (some-> database-type name str/lower-case))
-      (some? (re-find database-type db-type)))
+    (if (instance? java.util.regex.Pattern database-type)
+      (some? (re-find database-type db-type))
+      (= db-type (some-> database-type name str/lower-case)))
     false))
 
 (s/defn with-database-type-info
