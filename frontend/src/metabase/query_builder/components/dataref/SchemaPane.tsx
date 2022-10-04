1592305b37f2476fcd7d69327c0901a4895a5b89
Callum Herries <hi@callumherries.com>
2022-10-04 12:49:34 +0200

    Restyle SchemaPane

--- frontend/src/metabase/query_builder/components/dataref/SchemaPane.tsx
+++ frontend/src/metabase/query_builder/components/dataref/SchemaPane.tsx
@@ -1,45 +1,56 @@
+import { ngettext, msgid } from "ttag";
 import React, { useMemo } from "react";
-import Icon from "metabase/components/Icon";
 import Schemas from "metabase/entities/schemas";
 import { State } from "metabase-types/store";
 import Schema from "metabase-lib/lib/metadata/Schema";
+import {
+  NodeListItemLink,
+  NodeListItemName,
+  NodeListItemIcon,
+  NodeListTitle,
+  NodeListContainer,
+  NodeListIcon,
+  NodeListTitleText,
+} from "./NodeList.styled";
 
-interface Props {
+interface SchemaPaneProps {
   show: (type: string, item: unknown) => void;
   schema: Schema;
 }
 
-const SchemaPaneInner = ({ schema, show }: Props) => {
+const SchemaPaneInner = ({ show, schema }: SchemaPaneProps) => {
   const tables = useMemo(
     () => schema.tables.sort((a, b) => a.name.localeCompare(b.name)),
     [schema.tables],
   );
-  return (
-    <div>
-      <div className="ml1 my2 flex align-center justify-between border-bottom pb1">
-        <div className="flex align-center">
-          <Icon name="table2" className="text-light pr1" size={12} />
-          <span className="text-medium">{tables.length}</span>
-        </div>
-      </div>
+  return tables ? (
+    <NodeListContainer>
+      <NodeListTitle>
+        <NodeListIcon name="table" />
+        <NodeListTitleText>
+          {ngettext(
+            msgid`${tables.length} table`,
+            `${tables.length} tables`,
+            tables.length,
+          )}
+        </NodeListTitleText>
+      </NodeListTitle>
       <ul>
         {tables.map(table => (
           <li key={table.id}>
-            <a
-              className="flex-full flex p1 text-bold text-brand text-wrap no-decoration bg-medium-hover"
-              onClick={() => show("table", table)}
-            >
-              {table.name}
-            </a>
+            <NodeListItemLink onClick={() => show("table", table)}>
+              <NodeListItemIcon name="table" />
+              <NodeListItemName>{table.name}</NodeListItemName>
+            </NodeListItemLink>
           </li>
         ))}
       </ul>
-    </div>
-  );
+    </NodeListContainer>
+  ) : null;
 };
 
 const SchemaPane = Schemas.load({
-  id: (_state: State, { schema }: Props) => schema.id,
+  id: (_state: State, { schema }: SchemaPaneProps) => schema.id,
 })(SchemaPaneInner);
 
 export default SchemaPane;
