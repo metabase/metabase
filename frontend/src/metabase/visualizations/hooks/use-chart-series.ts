6330865e085c3af0e6db2ff80e2bfc41c67dd4c6
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-10-07 03:54:27 +0400

    add a hook for getting chart columns and series

---
+++ frontend/src/metabase/visualizations/hooks/use-chart-series.ts
@@ -0,0 +1,44 @@
+import { useMemo } from "react";
+import { DatasetData, VisualizationSettings } from "metabase-types/api";
+import { getChartColumns } from "metabase/visualizations/lib/graph/columns";
+import { ColumnFormatter } from "metabase/visualizations/types/format";
+import { getSeries } from "../lib/row/data";
+import { getSeriesColors } from "../components/RowChart/utils/colors";
+
+export const useChartSeries = (
+  data: DatasetData,
+  settings: VisualizationSettings,
+  columnFormatter: ColumnFormatter,
+) => {
+  const chartColumns = useMemo(
+    () => getChartColumns(data, settings),
+    [data, settings],
+  );
+
+  const seriesOrder = useMemo(() => {
+    const seriesOrderSettings = settings["graph.series_order"];
+    if (!seriesOrderSettings) {
+      return;
+    }
+
+    return seriesOrderSettings
+      .filter(setting => setting.enabled)
+      .map(setting => setting.name);
+  }, [settings]);
+
+  const series = useMemo(
+    () => getSeries(data, chartColumns, columnFormatter, seriesOrder),
+    [chartColumns, columnFormatter, data, seriesOrder],
+  );
+
+  const seriesColors = useMemo(
+    () => getSeriesColors(settings, series),
+    [series, settings],
+  );
+
+  return {
+    chartColumns,
+    series,
+    seriesColors,
+  };
+};
