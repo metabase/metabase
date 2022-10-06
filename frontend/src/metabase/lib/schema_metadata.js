93bc23cd83d0138c6087a5b62e21a862c2ce2849
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-10-06 15:11:07 -0400

    Move some constants in schema_metadata to metabase-lib

--- frontend/src/metabase/lib/schema_metadata.js
+++ frontend/src/metabase/lib/schema_metadata.js
@@ -6,95 +6,30 @@ import {
   isFK as isTypeFK,
   isPK as isTypePK,
 } from "metabase-lib/lib/types/utils/isa";
-import { TYPE } from "metabase-lib/lib/types/constants";
-
-// primary field types used for picking operators, etc
-export const NUMBER = "NUMBER";
-export const STRING = "STRING";
-export const STRING_LIKE = "STRING_LIKE";
-export const BOOLEAN = "BOOLEAN";
-export const TEMPORAL = "TEMPORAL";
-export const LOCATION = "LOCATION";
-export const COORDINATE = "COORDINATE";
-export const FOREIGN_KEY = "FOREIGN_KEY";
-export const PRIMARY_KEY = "PRIMARY_KEY";
-
-// other types used for various purporses
-export const ENTITY = "ENTITY";
-export const SUMMABLE = "SUMMABLE";
-export const SCOPE = "SCOPE";
-export const CATEGORY = "CATEGORY";
-export const DIMENSION = "DIMENSION";
-
-export const UNKNOWN = "UNKNOWN";
-
-// define various type hierarchies
-// NOTE: be sure not to create cycles using the "other" types
-const TYPES = {
-  [TEMPORAL]: {
-    base: [TYPE.Temporal],
-    effective: [TYPE.Temporal],
-    semantic: [TYPE.Temporal],
-  },
-  [NUMBER]: {
-    base: [TYPE.Number],
-    effective: [TYPE.Number],
-    semantic: [TYPE.Number],
-  },
-  [STRING]: {
-    base: [TYPE.Text],
-    effective: [TYPE.Text],
-    semantic: [TYPE.Text, TYPE.Category],
-  },
-  [STRING_LIKE]: {
-    base: [TYPE.TextLike],
-    effective: [TYPE.TextLike],
-  },
-  [BOOLEAN]: {
-    base: [TYPE.Boolean],
-    effective: [TYPE.Boolean],
-  },
-  [COORDINATE]: {
-    semantic: [TYPE.Coordinate],
-  },
-  [LOCATION]: {
-    semantic: [TYPE.Address],
-  },
-  [ENTITY]: {
-    semantic: [TYPE.FK, TYPE.PK, TYPE.Name],
-  },
-  [FOREIGN_KEY]: {
-    semantic: [TYPE.FK],
-  },
-  [PRIMARY_KEY]: {
-    semantic: [TYPE.PK],
-  },
-  [SUMMABLE]: {
-    include: [NUMBER],
-    exclude: [ENTITY, LOCATION, TEMPORAL],
-  },
-  [SCOPE]: {
-    include: [NUMBER, TEMPORAL, CATEGORY, ENTITY, STRING],
-    exclude: [LOCATION],
-  },
-  [CATEGORY]: {
-    base: [TYPE.Boolean],
-    effective: [TYPE.Boolean],
-    semantic: [TYPE.Category],
-    include: [LOCATION],
-  },
-  // NOTE: this is defunct right now.  see definition of isDimension below.
-  [DIMENSION]: {
-    include: [TEMPORAL, CATEGORY, ENTITY],
-  },
-};
+import {
+  TYPE,
+  TYPE_HIERARCHIES,
+  TEMPORAL,
+  LOCATION,
+  COORDINATE,
+  FOREIGN_KEY,
+  PRIMARY_KEY,
+  STRING,
+  STRING_LIKE,
+  NUMBER,
+  BOOLEAN,
+  UNKNOWN,
+  SUMMABLE,
+  SCOPE,
+  CATEGORY,
+} from "metabase-lib/lib/types/constants";
 
 export function isFieldType(type, field) {
   if (!field) {
     return false;
   }
 
-  const typeDefinition = TYPES[type];
+  const typeDefinition = TYPE_HIERARCHIES[type];
   // check to see if it belongs to any of the field types:
   const props = field.effective_type
     ? ["effective", "semantic"]
