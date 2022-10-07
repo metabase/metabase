4c3b4128b774714f2c094874eb15bd6604d5ab22
Nick Fitzpatrick <nick@metabase.com>
2022-10-07 14:06:30 -0300

    Refactoring ChartSettingFieldsPartition DnD logic

--- frontend/src/metabase/visualizations/components/settings/ChartSettingFieldsPartition.styled.tsx
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingFieldsPartition.styled.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { color, lighten } from "metabase/lib/colors";
 import ColumnItem from "./ColumnItem";
 
 interface FieldPartitionColumnProps {
@@ -9,6 +10,7 @@ export const FieldPartitionColumn = styled(
   ColumnItem,
 )<FieldPartitionColumnProps>`
   padding: 0.25rem 0;
+  margin: 0;
 
   ${props =>
     props.isDisabled &&
@@ -16,4 +18,19 @@ export const FieldPartitionColumn = styled(
         pointer-events: none;
         opacity: 0.4;
       `}
+`;
+
+interface DroppableContainerProps {
+  isDraggingOver: boolean;
+  isDragSource: boolean;
+}
+
+export const DroppableContainer = styled.div<DroppableContainerProps>`
+  background-color: ${({ isDraggingOver, isDragSource }) =>
+    isDraggingOver
+      ? lighten("brand")
+      : isDragSource
+      ? color("border")
+      : "none"};
+  border-radius: 0.5rem;
 `;
