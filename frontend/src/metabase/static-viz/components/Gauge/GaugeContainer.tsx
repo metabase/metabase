metabase/metabase

commit d4cb1e5af75fdc483efbff26882c8f942c3e8a71

Static gauge chart (#25651)
* Basic static gauge chart

* Add gauge segment min max labels

* Add gauge segment label

* Cleanup code

* Fix React list key error

* Remove hard-coded colors

* Make the everything calculates at the gauge center

* Make gauge segment data easier to understand

* Example scaling chart to avoid label overflow

* Avoid long labels overflow by scaling the gauge chart down

* Match gauge needle color with the design

* Fix hard coded gauge segment label anchor calculation

* Fix static gauge chart not rotate gauge needle correctly

* Add E2E for static gauge chart

* Fix `transform-origin: center` not working with BE static chart

* Truncate long segment label so charts can never be too small

* Fix gauge segment not connected to each other

* Static chart refactor part 1

* Final static gauge chart refactoring

* Simplify clojure chart render function

* Fix relative imports

* Make overlapping gauge labels more legible

* Make GaugeLabelData property orders consistent

* Make a function easier to read

* Make the calculation more correct

though the behavior would still be the same

* Separate presentational component out of the Gauge component

* Remove unused tap>

* Fix gauge chart not working on chart tester in internal static viz page

* Fix gauge chart not rendered with Lato font

* Improve internal page static chart tester option clarity

* Support column settings in gauge charts

* Add unit tests for static gauge chart utils

* Address review

* Fix unittest failure

frontend/src/metabase/static-viz/components/Gauge/GaugeContainer.tsx
@@ -1,0 +1,145 @@
+import React, { Fragment } from "react";
+
+import { formatNumber } from "metabase/static-viz/lib/numbers";
+import { truncateText } from "metabase/static-viz/lib/text";
+import type { ColorGetter } from "metabase/static-viz/lib/colors";
+
+import {
+  START_ANGLE,
+  CHART_WIDTH,
+  GAUGE_OUTER_RADIUS,
+  CHART_VERTICAL_MARGIN,
+  GAUGE_INNER_RADIUS,
+  SEGMENT_LABEL_MARGIN,
+  DISTANCE_TO_MIDDLE_LABEL_ANCHOR,
+  MAX_SEGMENT_VALUE_WIDTH,
+  SEGMENT_LABEL_FONT_SIZE,
+} from "./constants";
+import {
+  populateDefaultColumnSettings,
+  removeDuplicateElements,
+  calculateRelativeValueAngle,
+  calculateSegmentLabelPosition,
+  calculateSegmentLabelTextAnchor,
+} from "./utils";
+
+import type { Card, Data, GaugeLabelData, Position } from "./types";
+import Gauge from "./Gauge";
+
+interface GaugeContainerProps {
+  card: Card;
+  data: Data;
+  getColor: ColorGetter;
+}
+
+export default function GaugeContainer({
+  card,
+  data,
+  getColor,
+}: GaugeContainerProps) {
+  const settings = card.visualization_settings;
+  const columnSettings =
+    settings.column_settings &&
+    populateDefaultColumnSettings(Object.values(settings.column_settings)[0]);
+  const segments = settings["gauge.segments"];
+
+  const segmentMinValue = segments[0].min;
+  const segmentMaxValue = segments[segments.length - 1].max;
+
+  const center: Position = [
+    CHART_WIDTH / 2,
+    GAUGE_OUTER_RADIUS + CHART_VERTICAL_MARGIN,
+  ];
+
+  const value = data.rows[0][0];
+
+  const valueFormatter = (value: number) => {
+    return formatNumber(value, columnSettings);
+  };
+
+  const segmentMinMaxLabels: GaugeLabelData[] = segments
+    .flatMap(segmentDatum => {
+      return [segmentDatum.min, segmentDatum.max];
+    })
+    // gauge segments could be continuous i.e. the current max and the next min is the same value.
+    // So we should remove duplicate elements.
+    .reduce(removeDuplicateElements, [])
+    .map((segmentValue, index, segmentValues): GaugeLabelData => {
+      const isMinSegmentValue = index === 0;
+      const isMaxSegmentValue = index === segmentValues.length - 1;
+      const segmentValueAngle =
+        START_ANGLE +
+        calculateRelativeValueAngle(
+          segmentValue,
+          segmentMinValue,
+          segmentMaxValue,
+        );
+
+      if (isMinSegmentValue) {
+        return {
+          position: [
+            -(GAUGE_INNER_RADIUS + GAUGE_OUTER_RADIUS) / 2,
+            SEGMENT_LABEL_MARGIN + DISTANCE_TO_MIDDLE_LABEL_ANCHOR,
+          ],
+          textAnchor: "middle",
+          value: valueFormatter(segmentValue),
+          color: getColor("text-medium"),
+        };
+      }
+
+      if (isMaxSegmentValue) {
+        return {
+          position: [
+            (GAUGE_INNER_RADIUS + GAUGE_OUTER_RADIUS) / 2,
+            SEGMENT_LABEL_MARGIN + DISTANCE_TO_MIDDLE_LABEL_ANCHOR,
+          ],
+          textAnchor: "middle",
+          value: valueFormatter(segmentValue),
+          color: getColor("text-medium"),
+        };
+      }
+
+      return {
+        position: calculateSegmentLabelPosition(segmentValueAngle),
+        textAnchor: calculateSegmentLabelTextAnchor(segmentValueAngle),
+        value: valueFormatter(segmentValue),
+        color: getColor("text-medium"),
+      };
+    });
+
+  const segmentLabels: GaugeLabelData[] = segments
+    .filter(segment => segment.label)
+    .map((segment): GaugeLabelData => {
+      const angle =
+        START_ANGLE +
+        calculateRelativeValueAngle(
+          (segment.max + segment.min) / 2,
+          segmentMinValue,
+          segmentMaxValue,
+        );
+
+      return {
+        position: calculateSegmentLabelPosition(angle),
+        textAnchor: calculateSegmentLabelTextAnchor(angle),
+        value: truncateText(
+          segment.label,
+          MAX_SEGMENT_VALUE_WIDTH,
+          SEGMENT_LABEL_FONT_SIZE,
+        ),
+        color: getColor("text-dark"),
+      };
+    });
+
+  const gaugeLabels = segmentMinMaxLabels.concat(segmentLabels);
+
+  return (
+    <Gauge
+      value={value}
+      valueFormatter={valueFormatter}
+      segments={segments}
+      gaugeLabels={gaugeLabels}
+      center={center}
+      getColor={getColor}
+    />
+  );
+}

frontend/src/metabase/internal/pages/StaticVizPage.jsx
@@ -7,6 +7,11 @@ import {
   STATIC_CHART_DEFAULT_OPTIONS,
 } from "metabase/static-viz/containers/StaticChart/constants";
 import StaticChart from "metabase/static-viz/containers/StaticChart";
+import { GAUGE_CHART_TYPE } from "metabase/static-viz/components/Gauge/constants";
+import {
+  GAUGE_CHART_DEFAULT_OPTIONS,
+  GAUGE_CHART_WITH_COLUMN_SETTINGS_OPTIONS,
+} from "metabase/static-viz/components/Gauge/constants.dev";
 import {
   CATEGORICAL_DONUT_CHART_DEFAULT_OPTIONS,
   CATEGORICAL_DONUT_CHART_TYPE,
@@ -90,11 +95,21 @@ export default function StaticVizPage() {
             }}
           >
             <option id="">---</option>
-            {STATIC_CHART_TYPES.map((chartType, chartTypeIndex) => (
-              <option key={chartType} value={chartTypeIndex}>
-                {chartType}
-              </option>
-            ))}
+            {STATIC_CHART_TYPES.map((chartType, chartTypeIndex) => {
+              if (chartType === WATERFALL_CHART_TYPE) {
+                return (
+                  <option key={chartTypeIndex} value={chartTypeIndex}>
+                    waterfall (
+                    {chartTypeIndex === 1 ? "categorical" : "timeseries"})
+                  </option>
+                );
+              }
+              return (
+                <option key={chartType} value={chartTypeIndex}>
+                  {chartType}
+                </option>
+              );
+            })}
           </select>
 
           {(staticChartCustomOptions ||
@@ -133,6 +148,20 @@ export default function StaticVizPage() {
           <StaticChart
             type={CATEGORICAL_DONUT_CHART_TYPE}
             options={CATEGORICAL_DONUT_CHART_DEFAULT_OPTIONS}
+          />
+        </PageSection>
+        <PageSection>
+          <Subhead>Gauge</Subhead>
+          <StaticChart
+            type={GAUGE_CHART_TYPE}
+            options={GAUGE_CHART_DEFAULT_OPTIONS}
+          />
+        </PageSection>
+        <PageSection>
+          <Subhead>Gauge with column settings</Subhead>
+          <StaticChart
+            type={GAUGE_CHART_TYPE}
+            options={GAUGE_CHART_WITH_COLUMN_SETTINGS_OPTIONS}
           />
         </PageSection>
         <PageSection>

frontend/src/metabase/query_builder/components/notebook/steps/JoinStep.jsx
@@ -54,10 +54,9 @@ const stepShape = {
   revert: PropTypes.func.isRequired,
   clean: PropTypes.func.isRequired,
   actions: PropTypes.array.isRequired,
-
-  previous: stepShape,
-  next: stepShape,
 };
+stepShape.previous = stepShape;
+stepShape.next = stepShape;
 
 const joinStepPropTypes = {
   query: PropTypes.object.isRequired,

frontend/src/metabase/static-viz/components/Gauge/Gauge.tsx
@@ -1,0 +1,168 @@
+import React from "react";
+import { Pie } from "@visx/shape";
+import { Group } from "@visx/group";
+import { PieArcDatum } from "@visx/shape/lib/shapes/Pie";
+
+import { ColorGetter } from "metabase/static-viz/lib/colors";
+
+import {
+  START_ANGLE,
+  END_ANGLE,
+  CHART_WIDTH,
+  GAUGE_OUTER_RADIUS,
+  GAUGE_INNER_RADIUS,
+  SEGMENT_LABEL_FONT_SIZE,
+  CHART_HEIGHT,
+} from "./constants";
+import {
+  limit,
+  calculateChartScale,
+  calculateValueFontSize,
+  calculateRelativeValueAngle,
+  getCirclePositionInSvgCoordinate,
+  gaugeAccessor,
+  gaugeSorter,
+  colorGetter,
+} from "./utils";
+
+import GaugeNeedle from "./GaugeNeedle";
+import GaugeLabel from "./GaugeLabel";
+
+import type { GaugeLabelData, GaugeSegment, Position } from "./types";
+
+interface GaugeProps {
+  value: number;
+  valueFormatter: (value: number) => string;
+  segments: GaugeSegment[];
+  gaugeLabels: GaugeLabelData[];
+  center: Position;
+  getColor: ColorGetter;
+}
+
+export default function Gauge({
+  value,
+  valueFormatter,
+  segments,
+  gaugeLabels,
+  center,
+  getColor,
+}: GaugeProps) {
+  const segmentMinValue = segments[0].min;
+  const segmentMaxValue = segments[segments.length - 1].max;
+  const gaugeNeedleAngle = limit(
+    START_ANGLE +
+      calculateRelativeValueAngle(value, segmentMinValue, segmentMaxValue),
+    START_ANGLE,
+    END_ANGLE,
+  );
+  const gaugeNeedlePosition = getCirclePositionInSvgCoordinate(
+    GAUGE_INNER_RADIUS,
+    gaugeNeedleAngle,
+  );
+
+  const formattedValue = valueFormatter(value);
+  const dynamicValueFontSize = calculateValueFontSize(
+    formattedValue,
+    GAUGE_INNER_RADIUS,
+  );
+  const outlineColor = getColor("white");
+
+  return (
+    <svg width={CHART_WIDTH} height={CHART_HEIGHT} fontFamily="Lato">
+      <g transform={`translate(${CHART_WIDTH / 2}, ${CHART_HEIGHT / 2})`}>
+        {/* `transform-origin: center` doesn't work when rendered with Batik.
+            This <g /> translates the center of the chart to coordinate (0,0),
+            making `scale(number)` using the center of the chart as a transform
+            origin similar to `transform-origin: center` */}
+        <g
+          transform={`scale(${calculateChartScale(gaugeLabels)})
+                      translate(${-CHART_WIDTH / 2}, ${-CHART_HEIGHT / 2})`}
+        >
+          <Group top={center[1]} left={center[0]}>
+            <Pie
+              data={segments}
+              outerRadius={GAUGE_OUTER_RADIUS}
+              innerRadius={GAUGE_INNER_RADIUS}
+              pieValue={gaugeAccessor}
+              pieSort={gaugeSorter}
+              startAngle={START_ANGLE}
+              endAngle={END_ANGLE}
+            >
+              {pie => {
+                // Renders similar to Pie's default children.
+                // https://github.com/airbnb/visx/blob/978c143dae4057e482b0ca909e8c5a16c85dfd1e/packages/visx-shape/src/shapes/Pie.tsx#L86-L98
+                const baseArcPath = pie.path({
+                  startAngle: START_ANGLE,
+                  endAngle: END_ANGLE,
+                } as unknown as PieArcDatum<GaugeSegment>);
+                return (
+                  <Group className="visx-pie-arcs-group">
+                    {baseArcPath && (
+                      <g>
+                        <path d={baseArcPath} fill={getColor("bg-medium")} />
+                      </g>
+                    )}
+                    {pie.arcs.map((arc, index) => {
+                      const arcPath = pie.path({
+                        ...arc,
+                        startAngle:
+                          START_ANGLE +
+                          calculateRelativeValueAngle(
+                            arc.data.min,
+                            segmentMinValue,
+                            segmentMaxValue,
+                          ),
+                        endAngle:
+                          START_ANGLE +
+                          calculateRelativeValueAngle(
+                            arc.data.max,
+                            segmentMinValue,
+                            segmentMaxValue,
+                          ),
+                      });
+                      return (
+                        arcPath && (
+                          <g key={`pie-arc-${index}`}>
+                            <path d={arcPath} fill={colorGetter(arc)} />
+                          </g>
+                        )
+                      );
+                    })}
+                  </Group>
+                );
+              }}
+            </Pie>
+            <GaugeNeedle
+              color={getColor("bg-dark")}
+              outlineColor={outlineColor}
+              position={gaugeNeedlePosition}
+              valueAngle={gaugeNeedleAngle}
+            />
+            {gaugeLabels.map(
+              ({ color, position, textAnchor, value }, index) => {
+                return (
+                  <GaugeLabel
+                    key={index}
+                    fill={color}
+                    stroke={outlineColor}
+                    fontSize={SEGMENT_LABEL_FONT_SIZE}
+                    position={position}
+                    label={value}
+                    textAnchor={textAnchor}
+                  />
+                );
+              },
+            )}
+            <GaugeLabel
+              fill={getColor("text-dark")}
+              stroke={outlineColor}
+              fontSize={dynamicValueFontSize}
+              position={[0, -GAUGE_INNER_RADIUS * 0.4]}
+              label={formattedValue}
+            />
+          </Group>
+        </g>
+      </g>
+    </svg>
+  );
+}

frontend/src/metabase/static-viz/components/Gauge/GaugeLabel.tsx
@@ -1,0 +1,38 @@
+import React from "react";
+
+import OutlinedText from "metabase/static-viz/components/Text/OutlinedText";
+import type { Position, TextAnchor } from "./types";
+
+interface GaugeLabelProps {
+  fill: string;
+  stroke: string;
+  fontSize: number;
+  position: Position;
+  label: string;
+  textAnchor?: TextAnchor;
+}
+
+export default function GaugeLabel({
+  fill,
+  stroke,
+  fontSize,
+  position,
+  label,
+  textAnchor = "middle",
+}: GaugeLabelProps) {
+  return (
+    <OutlinedText
+      fill={fill}
+      fontWeight={700}
+      fontSize={fontSize}
+      stroke={stroke}
+      strokeWidth={fontSize / 4}
+      x={position[0]}
+      y={position[1]}
+      textAnchor={textAnchor}
+      verticalAnchor="middle"
+    >
+      {label}
+    </OutlinedText>
+  );
+}

frontend/src/metabase/static-viz/components/Gauge/GaugeNeedle.tsx
@@ -1,0 +1,88 @@
+import React from "react";
+
+import { getCirclePositionInSvgCoordinate } from "./utils";
+import type { Position } from "./types";
+
+interface GaugeNeedleProps {
+  color: string;
+  outlineColor: string;
+  position: Position;
+  valueAngle: number;
+}
+
+const CIRCLE_ANGLE = 2 * Math.PI;
+const GAUGE_NEEDLE_RADIUS = 14;
+const GAUGE_NEEDLE_OUTLINE_RADIUS = GAUGE_NEEDLE_RADIUS * 1.4;
+const EQUILATERAL_TRIANGLE_ANGLE = CIRCLE_ANGLE / 6;
+
+// Draw equilateral triangle within a circle with radius = `GAUGE_NEEDLE_RADIUS`,
+// that's why I name some variables with `_RADIUS` prefix even though I'm drawing a triangle.
+export default function GaugeNeedle({
+  color,
+  outlineColor,
+  position,
+  valueAngle,
+}: GaugeNeedleProps) {
+  const translationYOffset =
+    GAUGE_NEEDLE_RADIUS * Math.tan(EQUILATERAL_TRIANGLE_ANGLE / 2);
+  return (
+    <g
+      transform={`rotate(${toDegree(valueAngle)} ${toSvgPositionString(
+        position,
+      )}) translate(0 ${-translationYOffset})`}
+    >
+      <Triangle
+        center={position}
+        radius={GAUGE_NEEDLE_OUTLINE_RADIUS}
+        color={outlineColor}
+      />
+      <Triangle center={position} radius={GAUGE_NEEDLE_RADIUS} color={color} />
+    </g>
+  );
+}
+
+function toDegree(radian: number) {
+  return (radian / CIRCLE_ANGLE) * 360;
+}
+
+interface TriangleProps {
+  center: Position;
+  radius: number;
+  color: string;
+}
+
+const TRIANGLE_ANGLE = CIRCLE_ANGLE / 3;
+function Triangle({ center, radius, color }: TriangleProps) {
+  return (
+    <path
+      fill={color}
+      d={`M ${toSvgPositionString(
+        movePosition(
+          center,
+          getCirclePositionInSvgCoordinate(radius, 0 * TRIANGLE_ANGLE),
+        ),
+      )}
+      L ${toSvgPositionString(
+        movePosition(
+          center,
+          getCirclePositionInSvgCoordinate(radius, 1 * TRIANGLE_ANGLE),
+        ),
+      )}
+      L ${toSvgPositionString(
+        movePosition(
+          center,
+          getCirclePositionInSvgCoordinate(radius, 2 * TRIANGLE_ANGLE),
+        ),
+      )}
+      Z`}
+    />
+  );
+}
+
+function toSvgPositionString(position: Position) {
+  return `${position[0]} ${position[1]}`;
+}
+
+function movePosition(origin: Position, difference: Position): Position {
+  return [origin[0] + difference[0], origin[1] + difference[1]];
+}

frontend/src/metabase/static-viz/components/Gauge/constants.dev.ts
@@ -1,0 +1,69 @@
+export const GAUGE_CHART_DEFAULT_OPTIONS = {
+  card: {
+    visualization_settings: {
+      "gauge.segments": [
+        {
+          min: 0,
+          max: 9380,
+          color: "#ED6E6E",
+          label: "Label 1",
+        },
+        {
+          min: 9380,
+          max: 18760,
+          color: "#F9CF48",
+          label: "Label 2",
+        },
+        {
+          min: 18760,
+          max: 37520,
+          color: "#84BB4C",
+          label: "Label 3",
+        },
+      ],
+    },
+  },
+  data: {
+    rows: [[18760]],
+  },
+};
+
+export const GAUGE_CHART_WITH_COLUMN_SETTINGS_OPTIONS = {
+  card: {
+    visualization_settings: {
+      "gauge.segments": [
+        {
+          min: 0,
+          max: 9380,
+          color: "#ED6E6E",
+          label: "Label 1",
+        },
+        {
+          min: 9380,
+          max: 18760,
+          color: "#F9CF48",
+          label: "Label 2",
+        },
+        {
+          min: 18760,
+          max: 37520,
+          color: "#84BB4C",
+          label: "Label 3",
+        },
+      ],
+      column_settings: {
+        '["name","count"]': {
+          number_style: "currency",
+          number_separators: ".’",
+          scale: 2,
+          prefix: "<",
+          suffix: ">",
+          decimals: 1,
+        },
+      },
+    },
+  },
+  data: {
+    rows: [[18760]],
+  },
+};

frontend/src/metabase/static-viz/components/Gauge/constants.tsx
@@ -1,0 +1,32 @@
+export const GAUGE_CHART_TYPE = "gauge";
+
+// Font
+export const BASE_FONT_SIZE = 56;
+export const SEGMENT_LABEL_FONT_SIZE = BASE_FONT_SIZE * 0.3;
+
+// Margins
+const CHART_HORIZONTAL_MARGIN = 10;
+export const CHART_VERTICAL_MARGIN = 40;
+export const VALUE_MARGIN = 30;
+export const SEGMENT_LABEL_MARGIN = SEGMENT_LABEL_FONT_SIZE;
+export const DISTANCE_TO_MIDDLE_LABEL_ANCHOR = SEGMENT_LABEL_FONT_SIZE / 2;
+
+// Sizes
+const GAUGE_THICKNESS = 70;
+export const CHART_WIDTH = 540;
+export const GAUGE_OUTER_RADIUS = CHART_WIDTH / 2 - CHART_HORIZONTAL_MARGIN;
+export const GAUGE_INNER_RADIUS = GAUGE_OUTER_RADIUS - GAUGE_THICKNESS;
+export const CHART_HEIGHT = GAUGE_OUTER_RADIUS + 2 * CHART_VERTICAL_MARGIN;
+export const MAX_SEGMENT_VALUE_WIDTH = 150;
+
+// Angles
+export const GAUGE_ARC_ANGLE = Math.PI;
+export const START_ANGLE = -GAUGE_ARC_ANGLE / 2;
+export const END_ANGLE = GAUGE_ARC_ANGLE / 2;
+// Only allow the bottom of the gauge label to be above the top of the gauge chart.
+// So, the labels don't overlap with the gauge chart, otherwise, uses the label position
+// as a left or right anchor instead of a middle anchor to avoid having the labels protrude
+// the gauge chart.
+export const SEGMENT_LABEL_ANCHOR_THRESHOLD_ANGLE = Math.acos(
+  GAUGE_OUTER_RADIUS / (GAUGE_OUTER_RADIUS + SEGMENT_LABEL_MARGIN),
+);

commit 08c13a980d83c61dd40f0981216edaa884792b78

Fix gauge chart overflow
The bug was introduced by https://github.com/metabase/metabase/pull/25651

frontend/src/metabase/static-viz/components/Gauge/GaugeContainer.tsx
@@ -21,6 +21,7 @@ import {
   calculateRelativeValueAngle,
   calculateSegmentLabelPosition,
   calculateSegmentLabelTextAnchor,
+  gaugeSorter,
 } from "./utils";
 
 import type { Card, Data, GaugeLabelData, Position } from "./types";
@@ -41,7 +42,7 @@ export default function GaugeContainer({
   const columnSettings =
     settings.column_settings &&
     populateDefaultColumnSettings(Object.values(settings.column_settings)[0]);
-  const segments = settings["gauge.segments"];
+  const segments = [...settings["gauge.segments"]].sort(gaugeSorter);
 
   const segmentMinValue = segments[0].min;
   const segmentMaxValue = segments[segments.length - 1].max;