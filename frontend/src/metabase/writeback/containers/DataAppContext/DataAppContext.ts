af738c37d2891a3200147263cf2e98890bbd83e6
Anton Kulyk <kuliks.anton@gmail.com>
2022-10-11 16:17:05 +0100

    Bring `DataAppContext` back

--- frontend/src/metabase/writeback/containers/DataAppContext/DataAppContext.ts
+++ frontend/src/metabase/writeback/containers/DataAppContext/DataAppContext.ts
@@ -1,7 +1,22 @@
 import { createContext, useContext } from "react";
-import _ from "underscore";
+import _ from "lodash";
+
+import { Column } from "metabase-types/types/Dataset";
+
+type FieldName = string;
+type CardName = string;
+
+type ObjectDetailField = {
+  column: Column;
+  value: unknown;
+};
+
+export type FormattedObjectDetail = Record<FieldName, ObjectDetailField>;
+
+export type DataContextType = Record<CardName, FormattedObjectDetail>;
 
 export type DataAppContextType = {
+  data: DataContextType;
   bulkActions: {
     cardId: number | null;
     selectedRowIndexes: number[];
@@ -9,9 +24,12 @@ export type DataAppContextType = {
     removeRow: (index: number) => void;
     clearSelection: () => void;
   };
+  isLoaded: boolean;
+  format: (text: string) => string;
 };
 
 export const DataAppContext = createContext<DataAppContextType>({
+  data: {},
   bulkActions: {
     cardId: null,
     selectedRowIndexes: [],
@@ -19,6 +37,8 @@ export const DataAppContext = createContext<DataAppContextType>({
     removeRow: _.noop,
     clearSelection: _.noop,
   },
+  isLoaded: true,
+  format: (text: string) => text,
 });
 
 export function useDataAppContext() {
