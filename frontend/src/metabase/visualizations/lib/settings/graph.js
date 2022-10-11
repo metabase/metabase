ae496969885ea9dc6e8e50a128041eff3c99aed5
Nick Fitzpatrick <nick@metabase.com>
2022-10-11 13:23:06 -0300

    Adding maxDimensions to waterfall charts (#25714)
    * Adding maxDimensions to waterfall charts
    
    * Adding test
    
    * removing added default column behaviour, adjusting test

--- frontend/src/metabase/visualizations/lib/settings/graph.js
+++ frontend/src/metabase/visualizations/lib/settings/graph.js
@@ -17,7 +17,10 @@ import { getOptionFromColumn } from "metabase/visualizations/lib/settings/utils"
 import { dimensionIsNumeric } from "metabase/visualizations/lib/numeric";
 import { dimensionIsTimeseries } from "metabase/visualizations/lib/timeseries";
 
-import { getMaxMetricsSupported } from "metabase/visualizations";
+import {
+  getMaxMetricsSupported,
+  getMaxDimensionsSupported,
+} from "metabase/visualizations";
 
 import { ChartSettingOrderedSimple } from "metabase/visualizations/components/settings/ChartSettingOrderedSimple";
 import {
@@ -67,7 +70,15 @@ function getDefaultScatterColumns([
 }
 
 function getDefaultLineAreaBarColumns(series) {
-  return getDefaultDimensionsAndMetrics(series);
+  const [
+    {
+      card: { display },
+    },
+  ] = series;
+  return getDefaultDimensionsAndMetrics(
+    series,
+    getMaxDimensionsSupported(display),
+  );
 }
 
 export const GRAPH_DATA_SETTINGS = {
@@ -121,15 +132,16 @@ export const GRAPH_DATA_SETTINGS = {
       ),
     persistDefault: true,
     getProps: ([{ card, data }], vizSettings) => {
-      const value = vizSettings["graph.dimensions"];
+      const addedDimensions = vizSettings["graph.dimensions"];
+      const maxDimensionsSupported = getMaxDimensionsSupported(card.display);
       const options = data.cols
         .filter(vizSettings["graph._dimension_filter"])
         .map(getOptionFromColumn);
       return {
         options,
         addAnother:
-          options.length > value.length &&
-          value.length < 2 &&
+          options.length > addedDimensions.length &&
+          addedDimensions.length < maxDimensionsSupported &&
           vizSettings["graph.metrics"].length < 2
             ? t`Add series breakout`
             : null,
