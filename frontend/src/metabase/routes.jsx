21377189f6c08b4dfaf2e9f50cc69e32d0697581
Anton Kulyk <kuliks.anton@gmail.com>
2022-10-17 18:26:43 +0100

    Disable app routes

--- frontend/src/metabase/routes.jsx
+++ frontend/src/metabase/routes.jsx
@@ -70,8 +70,6 @@ import TableQuestionsContainer from "metabase/reference/databases/TableQuestions
 import FieldListContainer from "metabase/reference/databases/FieldListContainer";
 import FieldDetailContainer from "metabase/reference/databases/FieldDetailContainer";
 
-import DataAppLanding from "metabase/writeback/containers/DataAppLanding";
-
 import getAccountRoutes from "metabase/account/routes";
 import getAdminRoutes from "metabase/admin/routes";
 import getCollectionTimelineRoutes from "metabase/timelines/collections/routes";
@@ -227,32 +225,6 @@ export const getRoutes = store => (
           <ModalRoute path="new_dashboard" modal={CreateDashboardModal} />
           <ModalRoute path="permissions" modal={CollectionPermissionsModal} />
           {getCollectionTimelineRoutes()}
-        </Route>
-
-        <Route path="apps/:slug">
-          <IndexRoute component={DataAppLanding} />
-          <ModalRoute path="move" modal={MoveCollectionModal} />
-          <ModalRoute path="archive" modal={ArchiveCollectionModal} />
-          <ModalRoute path="new_collection" modal={CollectionCreate} />
-          <ModalRoute path="new_dashboard" modal={CreateDashboardModal} />
-          <ModalRoute path="permissions" modal={CollectionPermissionsModal} />
-          {getCollectionTimelineRoutes()}
-        </Route>
-
-        <Route path="a/:slug">
-          <IndexRoute component={DataAppLanding} />
-          <ModalRoute path="move" modal={MoveCollectionModal} />
-          <ModalRoute path="archive" modal={ArchiveCollectionModal} />
-          <ModalRoute path="new_collection" modal={CollectionCreate} />
-          <ModalRoute path="new_dashboard" modal={CreateDashboardModal} />
-          <ModalRoute path="permissions" modal={CollectionPermissionsModal} />
-          {getCollectionTimelineRoutes()}
-
-          <Route path="page/:pageId" component={DashboardApp}>
-            <ModalRoute path="move" modal={DashboardMoveModal} />
-            <ModalRoute path="copy" modal={DashboardCopyModal} />
-            <ModalRoute path="archive" modal={ArchiveDashboardModal} />
-          </Route>
         </Route>
 
         <Route path="activity" component={ActivityApp} />
