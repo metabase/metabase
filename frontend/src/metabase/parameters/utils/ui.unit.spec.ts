936da2fe648f79c192ac536a23be842e28b254bb
Anton Kulyk <kuliks.anton@gmail.com>
2022-10-17 12:36:23 +0100

    Show data app page ID params in editing mode (#25943)

--- frontend/src/metabase/parameters/utils/ui.unit.spec.ts
+++ frontend/src/metabase/parameters/utils/ui.unit.spec.ts
@@ -54,10 +54,6 @@ describe("parameters/utils/ui", () => {
         id: "4",
         slug: "qux",
       }),
-      createMockUiParameter({
-        id: "5",
-        hidden: true,
-      }),
     ];
 
     const hiddenParameterSlugs = "bar,baz";
