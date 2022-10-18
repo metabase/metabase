794e731ffd13da3b65f363e9a784d5304456813c
Callum Herries <hi@callumherries.com>
2022-10-18 11:24:14 +0100

    Add SingleSelectListField

--- frontend/src/metabase/components/SingleSelectListField/types.ts
+++ frontend/src/metabase/components/SingleSelectListField/types.ts
@@ -1,6 +1,6 @@
 export type Option = any[];
 
-export interface ListFieldProps {
+export interface SingleSelectListFieldProps {
   onChange: (value: string[]) => void;
   value: string[];
   options: Option;
