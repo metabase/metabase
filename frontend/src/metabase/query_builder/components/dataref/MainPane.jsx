c46de9a222e1e396ee5b07ac8be6d697e879c3b1
Cal Herries <39073188+calherries@users.noreply.github.com>
2022-10-18 18:39:01 +0100

    Open the data reference when the cursor moves to a card tag in the native query editor (#25801)
    * Refactor DataReference
    
    * Don't show card tags in the sidebar
    
    * Refactor template tag regexes
    
    * Add models to DatabaseSchemasPane
    
    * Restyle SchemaPane
    
    * Anton's suggestions
    
    * Rename show to onItemClick
    
    * Remove another ternary
    
    * Add openDataReferenceAtCard action, but not opening the sidebar yet
    
    * Fix useEffect static analysis
    
    * Types
    
    * Get opening the sidebar at a model working, at the expense of model autocomplete
    
    * Change icon of ModelPane if it's a question
    
    * Tidy
    
    * Refactor DatabasePane
    
    * Move onItemClick and back callback logic to redux actions
    
    * Add e2e test
    
    * Add back autocomplete
    
    * Load question tables correctly
    
    * Restore autocomplete
    
    * Fix card tag autocomplete on the first character of the tag name
    
    * Update template tag editor visibility test to not test for card tags
    
    * Fix tests for cardIdFromTagName
    
    * Tidy tag editor
    
    * Typo
    
    * Fix undefined error
    
    * Tidy NativeQueryEditor.cardTagId
    
    * Tidy nextCompleters
    
    * Tidy
    
    * Update unit tests for tag editor visibility
    
    * Tidy
    
    * Tidy
    
    * Add test for models
    
    * Check fields have loaded before displaying table fields
    
    * Tidy
    
    * Rename ModelPane to QuestionPane
    
    * Rename card to question
    
    * Remove unused files
    
    * Tidy
    
    * Fix renaming
    
    * Fix renaming
    
    * Make variable template tag names non-empty
    
    * Fix test flakiness
    
    * Tidy
    
    * Tidy
    
    * Remove is_saved_questions check
    
    * Rename
    
    * Replace setDataReferenceStack action with selector and fix test
    
    * Initialize dataReferenceStack to null as an "uninitialized" state to fix the back button
    
    * Update e2e tests
    
    * Update e2e tests
    
    * Replace long ternaries

--- frontend/src/metabase/query_builder/components/dataref/MainPane.jsx
+++ frontend/src/metabase/query_builder/components/dataref/MainPane.jsx
@@ -2,38 +2,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { t } from "ttag";
-import Databases from "metabase/entities/databases";
 
+import Databases from "metabase/entities/databases";
+import SidebarContent from "metabase/query_builder/components/SidebarContent";
 import {
   NodeListItemLink,
   NodeListItemName,
   NodeListItemIcon,
 } from "./NodeList.styled";
 
-const MainPane = ({ databases, show }) => (
-  <div>
+const MainPane = ({ databases, onClose, onItemClick }) => (
+  <SidebarContent title={t`Data Reference`} onClose={onClose}>
     <p className="mt2 mb3 text-spaced">
       {t`Browse the contents of your databases, tables, and columns. Pick a database to get started.`}
     </p>
     <ul>
       {databases &&
-        databases
-          .filter(db => !db.is_saved_questions)
-          .map(database => (
-            <li key={database.id}>
-              <NodeListItemLink onClick={() => show("database", database)}>
-                <NodeListItemIcon name="database" />
-                <NodeListItemName>{database.name}</NodeListItemName>
-              </NodeListItemLink>
-            </li>
-          ))}
+        databases.map(database => (
+          <li key={database.id}>
+            <NodeListItemLink onClick={() => onItemClick("database", database)}>
+              <NodeListItemIcon name="database" />
+              <NodeListItemName>{database.name}</NodeListItemName>
+            </NodeListItemLink>
+          </li>
+        ))}
     </ul>
-  </div>
+  </SidebarContent>
 );
 
 MainPane.propTypes = {
-  show: PropTypes.func.isRequired,
   databases: PropTypes.array,
+  onClose: PropTypes.func.isRequired,
+  onItemClick: PropTypes.func.isRequired,
 };
 
 export default Databases.loadList()(MainPane);
--- frontend/src/metabase/query_builder/components/dataref/TableInfoLoader.tsx
+++ frontend/src/metabase/query_builder/components/dataref/TableInfoLoader.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { useSafeAsyncFunction } from "metabase/hooks/use-safe-async-function";
