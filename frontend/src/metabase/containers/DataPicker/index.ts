f1836bdc705585c4d6cfab6bc3b8f69610e7ca0e
Anton Kulyk <kuliks.anton@gmail.com>
2022-10-19 19:59:08 +0100

    Export data picker types

--- frontend/src/metabase/containers/DataPicker/index.ts
+++ frontend/src/metabase/containers/DataPicker/index.ts
@@ -1,2 +1,3 @@
 export { default } from "./DataPickerContainer";
+export type { DataPickerProps, DataPickerFiltersProp } from "./types";
 export { default as useDataPickerValue } from "./useDataPickerValue";
