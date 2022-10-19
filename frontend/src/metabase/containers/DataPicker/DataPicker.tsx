10081532541e7442abd3244f816d05b133e1f11a
Anton Kulyk <kuliks.anton@gmail.com>
2022-10-19 15:46:35 +0100

    Add data type picker step

--- frontend/src/metabase/containers/DataPicker/DataPicker.tsx
+++ frontend/src/metabase/containers/DataPicker/DataPicker.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 
-import type { DataPickerProps } from "./types";
+import { SAVED_QUESTIONS_VIRTUAL_DB_ID } from "metabase-lib/lib/metadata/utils/saved-questions";
+
+import type { DataPickerProps, DataPickerDataType } from "./types";
+
+import DataTypePicker from "./DataTypePicker";
 import RawDataPanePicker from "./RawDataPanePicker";
 
 function DataPicker(props: DataPickerProps) {
+  const { value, onChange } = props;
+
+  const handleDataTypeChange = useCallback(
+    (type: DataPickerDataType) => {
+      const isUsingVirtualTables = type === "models" || type === "questions";
+      onChange({
+        type,
+        databaseId: isUsingVirtualTables
+          ? SAVED_QUESTIONS_VIRTUAL_DB_ID
+          : undefined,
+        schemaId: undefined,
+        tableIds: [],
+      });
+    },
+    [onChange],
+  );
+
+  if (!value.type) {
+    return <DataTypePicker onChange={handleDataTypeChange} />;
+  }
+
   return <RawDataPanePicker {...props} />;
 }
 
