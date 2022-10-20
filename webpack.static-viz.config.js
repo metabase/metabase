c69ff7b2ece896898a2d23b2c14effe0f6b8eec5
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-10-20 18:54:34 +0400

    use watch webpack env var

--- webpack.static-viz.config.js
+++ webpack.static-viz.config.js
@@ -7,49 +7,51 @@ const BABEL_CONFIG = {
   cacheDirectory: process.env.BABEL_DISABLE_CACHE ? null : ".babel_cache",
 };
 
-const shouldDisableMinimization = process.env.DISABLE_MINIMIZATION === 'true'
+module.exports = env => {
+  const shouldDisableMinimization = env.WEBPACK_WATCH === true;
 
-module.exports = {
-  mode: "production",
-  context: SRC_PATH,
+  return {
+    mode: "production",
+    context: SRC_PATH,
 
-  performance: {
-    hints: false,
-  },
+    performance: {
+      hints: false,
+    },
 
-  entry: {
-    "lib-static-viz": {
-      import: "./static-viz/index.js",
-      library: {
-        name: "StaticViz",
-        type: "var",
+    entry: {
+      "lib-static-viz": {
+        import: "./static-viz/index.js",
+        library: {
+          name: "StaticViz",
+          type: "var",
+        },
       },
     },
-  },
 
-  output: {
-    path: BUILD_PATH + "/app/dist",
-    filename: "[name].bundle.js",
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(tsx?|jsx?)$/,
-        exclude: /node_modules|cljs/,
-        use: [{ loader: "babel-loader", options: BABEL_CONFIG }],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".webpack.js", ".web.js", ".js", ".jsx", ".ts", ".tsx"],
-    alias: {
-      metabase: SRC_PATH,
-      cljs: CLJS_SRC_PATH,
-      "metabase-lib": LIB_SRC_PATH,
+    output: {
+      path: BUILD_PATH + "/app/dist",
+      filename: "[name].bundle.js",
     },
-  },
-  optimization: {
-    minimize: !shouldDisableMinimization
-  }
+
+    module: {
+      rules: [
+        {
+          test: /\.(tsx?|jsx?)$/,
+          exclude: /node_modules|cljs/,
+          use: [{ loader: "babel-loader", options: BABEL_CONFIG }],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".webpack.js", ".web.js", ".js", ".jsx", ".ts", ".tsx"],
+      alias: {
+        metabase: SRC_PATH,
+        cljs: CLJS_SRC_PATH,
+        "metabase-lib": LIB_SRC_PATH,
+      },
+    },
+    optimization: {
+      minimize: !shouldDisableMinimization,
+    },
+  };
 };
