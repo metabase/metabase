62e5ba8ec4f3cb91fdf2e2ecee715200bf4854d5
Anton Kulyk <kuliks.anton@gmail.com>
2022-10-21 16:46:44 +0100

    Remove new button from action panel

--- frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbar/DataAppActionPanel.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbar/DataAppActionPanel.tsx
@@ -1,14 +1,7 @@
-import React, { useMemo } from "react";
-import _ from "underscore";
+import React from "react";
 import { t } from "ttag";
 
-import EntityMenu from "metabase/components/EntityMenu";
-import Link from "metabase/core/components/Link";
 import Tooltip from "metabase/components/Tooltip";
-
-import * as Urls from "metabase/lib/urls";
-
-import type { DataApp } from "metabase-types/api";
 
 import {
   ActionGroup,
@@ -17,47 +10,13 @@ import {
 } from "../MainNavbar.styled";
 
 interface Props {
-  dataApp: DataApp;
-  onAddData: () => void;
-  onNewPage: () => void;
   onEditAppSettings: () => void;
 }
 
-function DataAppActionPanel({
-  dataApp,
-  onAddData,
-  onNewPage,
-  onEditAppSettings,
-}: Props) {
-  const addMenuItems = useMemo(
-    () => [
-      {
-        title: t`Data`,
-        icon: "database",
-        action: onAddData,
-      },
-      {
-        title: t`Page`,
-        icon: "pencil",
-        action: onNewPage,
-      },
-    ],
-    [onAddData, onNewPage],
-  );
-
+function DataAppActionPanel({ onEditAppSettings }: Props) {
   return (
     <DataAppActionsContainer>
       <ActionGroup>
-        <ActionGroup.Cell>
-          <EntityMenu
-            items={addMenuItems}
-            trigger={
-              <Tooltip tooltip={t`Add`}>
-                <DataAppActionButton icon="add" />
-              </Tooltip>
-            }
-          />
-        </ActionGroup.Cell>
         <ActionGroup.Cell>
           <Tooltip tooltip={t`Settings`}>
             <DataAppActionButton icon="gear" onClick={onEditAppSettings} />
