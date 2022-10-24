0351dfbdd188666734cd53e235c6258dbfb579ff
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-10-24 14:23:17 +0300

    Add e2e tests

--- frontend/test/metabase/scenarios/admin/settings/sso/saml.cy.spec.js
+++ frontend/test/metabase/scenarios/admin/settings/sso/saml.cy.spec.js
@@ -8,6 +8,7 @@ describeEE("scenarios > admin > settings > SSO > SAML", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+    cy.intercept("PUT", "/api/setting/*").as("updateSetting");
     cy.intercept("PUT", "/api/setting").as("updateSettings");
   });
 
@@ -17,8 +18,9 @@ describeEE("scenarios > admin > settings > SSO > SAML", () => {
     enterSAMLSettings();
     cy.button("Save and enable").click();
     cy.wait("@updateSettings");
-    cy.findAllByRole("link", { name: "Authentication" }).first().click();
+    cy.findByText("Changes saved!").should("exist");
 
+    cy.findAllByRole("link", { name: "Authentication" }).first().click();
     cy.findByRole("switch", { name: "SAML" }).should("be.checked");
   });
 
@@ -28,14 +30,53 @@ describeEE("scenarios > admin > settings > SSO > SAML", () => {
     enterSAMLSettings();
     cy.button("Save but don't enable").click();
     cy.wait("@updateSettings");
-    cy.findAllByRole("link", { name: "Authentication" }).first().click();
+    cy.findByText("Changes saved!").should("exist");
 
+    cy.findAllByRole("link", { name: "Authentication" }).first().click();
     cy.findByRole("switch", { name: "SAML" }).should("not.be.checked");
+  });
+
+  it("should allow to update saml settings", () => {
+    setupSAML();
+    cy.visit("/admin/settings/authentication/saml");
+
+    typeAndBlurUsingLabel("SAML Identity Provider URL", "https://other.test");
+    cy.button("Save changes").click();
+    cy.wait("@updateSettings");
+    cy.findByText("Changes saved!").should("exist");
+
+    cy.findAllByRole("link", { name: "Authentication" }).first().click();
+    cy.findByRole("switch", { name: "SAML" }).should("be.checked");
+  });
+
+  it("should allow to enable and disable saml via the toggle", () => {
+    setupSAML();
+    cy.visit("/admin/settings/authentication");
+
+    cy.findByRole("switch", { name: "SAML" }).click();
+    cy.wait("@updateSetting");
+    cy.findByText("Saved").should("exist");
+    cy.findByRole("switch", { name: "SAML" }).should("not.be.checked");
+
+    cy.findByRole("switch", { name: "SAML" }).click();
+    cy.wait("@updateSetting");
+    cy.findByText("Saved").should("exist");
+    cy.findByRole("switch", { name: "SAML" }).should("be.checked");
   });
 });
 
 const getSAMLCertificate = () => {
   return cy.readFile("test_resources/sso/auth0-public-idp.cert", "utf8");
+};
+
+const setupSAML = () => {
+  getSAMLCertificate().then(certificate => {
+    cy.request("PUT", "/api/setting", {
+      "saml-enabled": true,
+      "saml-identity-provider-uri": "https://example.test",
+      "saml-identity-provider-certificate": certificate,
+    });
+  });
 };
 
 const enterSAMLSettings = () => {
