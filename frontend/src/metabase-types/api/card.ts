b07dd59d5a1e7b7e35678aeef1262b3aa7611654
Cal Herries <39073188+calherries@users.noreply.github.com>
2022-10-25 18:17:29 +0100

    Show collection in data reference for questions/models (#26063)
    * Show collection in data reference
    
    * Fix type check
    
    * Revert "Fix type check"
    
    This reverts commit 3eef7cd21c8c6c1677fc427ac72a581cd8580bb4.
    
    * Use Collections selector instead of question.card().collection
    
    * fetch collection in loadMetadataForCard
    
    * Replace getCollectionFromCard with Collections selector and rename question -> card

--- frontend/src/metabase-types/api/card.ts
+++ frontend/src/metabase-types/api/card.ts
@@ -2,6 +2,7 @@ import { DatasetQuery } from "./query";
 
 export interface Card extends UnsavedCard {
   id: CardId;
+  collection_id: number | null;
   name: string;
   description: string | null;
   dataset: boolean;
