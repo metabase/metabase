da5ad9cfbf420bdfa52dae9e327b629bcc2daf90
Nick Fitzpatrick <nick@metabase.com>
2022-10-25 09:07:06 -0300

    Adding Unit Test

--- frontend/src/metabase/core/components/AutocompleteInput/AutocompleteInput.unit.spec.tsx
+++ frontend/src/metabase/core/components/AutocompleteInput/AutocompleteInput.unit.spec.tsx
@@ -5,9 +5,27 @@ import AutocompleteInput from "./AutocompleteInput";
 
 const OPTIONS = ["Banana", "Orange", "Mango"];
 
-const setup = ({ value = "", onChange = jest.fn() } = {}) => {
+interface setupProps {
+  value?: string;
+  onChange?: (value: string) => void;
+  filterFn?: (value: string | undefined, options: string[]) => string[];
+  onOptionClick?: (value: string) => void;
+}
+
+const setup = ({
+  value = "",
+  onChange = jest.fn(),
+  filterFn,
+  onOptionClick,
+}: setupProps = {}) => {
   const { getByRole, rerender, findAllByRole } = render(
-    <AutocompleteInput value={value} options={OPTIONS} onChange={onChange} />,
+    <AutocompleteInput
+      value={value}
+      options={OPTIONS}
+      onChange={onChange}
+      filterFn={filterFn}
+      onOptionClick={onOptionClick}
+    />,
   );
 
   const input = getByRole("combobox");
@@ -56,5 +74,50 @@ describe("AutocompleteInput", () => {
     fireEvent.click(options[0]);
 
     expect(onChange).toHaveBeenCalledWith("Orange");
+  });
+
+  it("supports custom filtering functions", async () => {
+    const filterFn = (value: string | undefined, options: string[]) => {
+      if (value && options) {
+        return options.filter(
+          option =>
+            !option.toLocaleLowerCase().includes(value.toLocaleLowerCase()),
+        );
+      }
+      return [];
+    };
+
+    const { input, getOptions } = setup({
+      value: "or",
+      filterFn,
+    });
+
+    fireEvent.click(input);
+
+    const options = await getOptions();
+    expect(options).toHaveLength(2);
+
+    expect(options[0]).toHaveTextContent("Banana");
+    expect(options[1]).toHaveTextContent("Mango");
+  });
+
+  it("supports independent option click handler", async () => {
+    const onChange = jest.fn();
+    const onOptionClick = jest.fn();
+
+    const { input, getOptions } = setup({
+      value: "or",
+      onChange,
+      onOptionClick,
+    });
+
+    fireEvent.click(input);
+
+    const options = await getOptions();
+
+    fireEvent.click(options[0]);
+
+    expect(onOptionClick).toHaveBeenCalledWith("Orange");
+    expect(onChange).not.toHaveBeenCalled();
   });
 });
