6cd19a465fb45fbca52402d22af2938b9861fd98
Anton Kulyk <kuliks.anton@gmail.com>
2022-10-26 14:47:05 +0100

    Create nav item when creating blank page

--- frontend/src/metabase/writeback/containers/CreateDataAppPageModalForm.tsx
+++ frontend/src/metabase/writeback/containers/CreateDataAppPageModalForm.tsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { t } from "ttag";
+import { connect } from "react-redux";
 
+import DataApps from "metabase/entities/data-apps";
 import Dashboards from "metabase/entities/dashboards";
 
 import type { DataApp, DataAppPage } from "metabase-types/api";
 
-interface Props {
+interface OwnProps {
   dataApp: DataApp;
   onSave: (dataAppPage: DataAppPage) => void;
   onClose: () => void;
 }
 
-function CreateDataAppPageModalForm({ dataApp, onSave, onClose }: Props) {
+interface DispatchProps {
+  handleUpdateDataApp: (
+    dataApp: Partial<DataApp> & { id: DataApp["id"] },
+  ) => void;
+}
+
+type Props = OwnProps & DispatchProps;
+
+const mapDispatchToProps = {
+  handleUpdateDataApp: DataApps.actions.update,
+};
+
+function CreateDataAppPageModalForm({
+  dataApp,
+  handleUpdateDataApp,
+  onSave,
+  onClose,
+}: Props) {
+  const onSaved = useCallback(
+    (dataAppPage: DataAppPage) => {
+      const navItems = [...dataApp.nav_items, { page_id: dataAppPage.id }];
+      handleUpdateDataApp({ id: dataApp.id, nav_items: navItems });
+      onSave(dataAppPage);
+    },
+    [dataApp, handleUpdateDataApp, onSave],
+  );
+
   return (
     <Dashboards.ModalForm
       form={Dashboards.forms.dataAppPage}
@@ -20,9 +48,9 @@ function CreateDataAppPageModalForm({ dataApp, onSave, onClose }: Props) {
         collection_id: dataApp.collection_id,
       }}
       onClose={onClose}
-      onSaved={onSave}
+      onSaved={onSaved}
     />
   );
 }
 
-export default CreateDataAppPageModalForm;
+export default connect(null, mapDispatchToProps)(CreateDataAppPageModalForm);
