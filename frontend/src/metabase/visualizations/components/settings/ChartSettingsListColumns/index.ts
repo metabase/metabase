62ec886e5fca981cf26735959099a18e8627ee73
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2022-10-27 13:32:15 -0600

    Set List Viz Defaults (#26125)
    * find good default list viz columns
    
    * fix all the broken stuff
    
    * replace link components

--- frontend/src/metabase/visualizations/components/settings/ChartSettingsListColumns/index.ts
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingsListColumns/index.ts
@@ -1,1 +1,2 @@
 export { default } from "./ChartSettingsListColumns";
+export * from "./utils";
---
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingsListColumns/utils.ts
@@ -0,0 +1,38 @@
+import type { Column } from "metabase-types/types/Dataset";
+
+import {
+  isAvatarURL,
+  isEntityName,
+  isCategory,
+  isDescription,
+  isDate,
+  isNumeric,
+  isPK,
+} from "metabase-lib/types/utils/isa";
+
+const getIdOrRef = (column: Column | null) => column?.id ?? column?.field_ref;
+
+const findA = (columns: Column[], predicate: (column: Column) => boolean) => {
+  return columns.find(predicate);
+};
+
+export const getDefaultColumns = (columns: Column[]) => {
+  const findInColumns = (predicate: (column: Column) => boolean) =>
+    findA(columns, predicate);
+
+  const imageCols = [
+    findInColumns(isAvatarURL) ?? findInColumns(isEntityName) ?? null,
+  ];
+  const leftCols = [
+    findInColumns(isEntityName) ?? findInColumns(isPK) ?? null,
+    findInColumns(col => isCategory(col) && !isEntityName(col)) ?? null,
+    findInColumns(isDescription) ?? null,
+  ];
+  const rightCols = [findInColumns(isDate) ?? findInColumns(isNumeric) ?? null];
+
+  return {
+    image: imageCols.filter(Boolean).map(getIdOrRef),
+    left: leftCols.filter(Boolean).map(getIdOrRef),
+    right: rightCols.filter(Boolean).map(getIdOrRef),
+  };
+};
