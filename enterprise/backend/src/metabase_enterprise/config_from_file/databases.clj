a1f0a896293865cc907b4cba5b1040edaba5949f
Cam Saul <1455846+camsaul@users.noreply.github.com>
2022-10-26 19:07:50 -0700

    Support disabling database sync in config files (#26110)
    * Move metabase.config.file code => enterprise directory
    
    * Support setting Settings in config.yml
    
    * Support disabling database sync in config files
    
    * Setting name has to be a keyword since that's how YAML gets parsed
    
    * Test fix :wrench:
    
    * Settings have to be keywords
    
    * Fix Kondo error
    
    * Fix Kondo errors
    
    * Fix Kondo errors
    
    * Remove unused namespace

--- enterprise/backend/src/metabase_enterprise/config_from_file/databases.clj
+++ enterprise/backend/src/metabase_enterprise/config_from_file/databases.clj
@@ -5,9 +5,17 @@
    [metabase-enterprise.config-from-file.interface :as config-from-file.i]
    [metabase.driver.util :as driver.u]
    [metabase.models.database :refer [Database]]
+   [metabase.models.setting :refer [defsetting]]
    [metabase.util :as u]
    [metabase.util.i18n :refer [trs]]
    [toucan.db :as db]))
+
+(defsetting config-from-file-sync-databases
+  "Whether to sync newly created Databases during config-from-file initialization. By default, true, but you can disable
+  this behavior if you want to sync it manually or use SerDes to populate its data model."
+  :visibility :internal
+  :type :boolean
+  :default true)
 
 (s/def :metabase-enterprise.config-from-file.databases.config-file-spec/name
   string?)
@@ -38,7 +46,9 @@
     (do
       (log/info (u/colorize :green (trs "Creating new {0} Database {1}" (:engine database) (pr-str (:name database)))))
       (let [db (db/insert! Database database)]
-        ((requiring-resolve 'metabase.sync/sync-database!) db)))))
+        (if (config-from-file-sync-databases)
+          ((requiring-resolve 'metabase.sync/sync-database!) db)
+          (log/info (trs "Sync on database creation when initializing from file is disabled. Skipping sync.")))))))
 
 (defmethod config-from-file.i/initialize-section! :databases
   [_section-name databases]
