6a17c9425f9507e6341c03e2264716f993c35fbe
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-11-01 13:26:17 +0200

    Fix tests

--- frontend/src/metabase/setup/components/NewsletterForm/NewsletterForm.unit.spec.tsx
+++ frontend/src/metabase/setup/components/NewsletterForm/NewsletterForm.unit.spec.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import NewsletterForm, { NewsletterFormProps } from "./NewsletterForm";
+import NewsletterForm from "./NewsletterForm";
 
 describe("NewsletterForm", () => {
   it("should allow to submit the form with the provided email", async () => {
-    const props = getProps({
-      onSubscribe: jest.fn().mockResolvedValue({}),
-    });
+    const email = "user@metabase.test";
+    const onSubscribe = jest.fn().mockResolvedValue({});
 
-    render(<NewsletterForm {...props} />);
+    render(<NewsletterForm initialEmail={email} onSubscribe={onSubscribe} />);
     userEvent.click(screen.getByText("Subscribe"));
 
-    expect(await screen.findByText(/You're subscribed/)).toBeInTheDocument();
+    await waitFor(() => {
+      expect(onSubscribe).toHaveBeenCalledWith(email);
+      expect(screen.getByText(/You're subscribed/)).toBeInTheDocument();
+    });
   });
-});
-
-const getProps = (
-  opts?: Partial<NewsletterFormProps>,
-): NewsletterFormProps => ({
-  initialEmail: "user@metabase.test",
-  onSubscribe: jest.fn(),
-  ...opts,
 });
