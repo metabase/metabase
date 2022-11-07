1eac235681461c7c2eb01a136ba55457a7dcc8b8
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-11-07 12:56:01 +0200

    Add event form

--- frontend/src/metabase/timelines/common/components/EventForm/EventForm.tsx
+++ frontend/src/metabase/timelines/common/components/EventForm/EventForm.tsx
@@ -2,13 +2,18 @@ import React, { useMemo } from "react";
 import { t } from "ttag";
 import * as Yup from "yup";
 import { getTimelineIcons, getTimelineName } from "metabase/lib/timelines";
-import FormProvider from "metabase/core/components/FormProvider";
+import Button from "metabase/core/components/Button/Button";
 import Form from "metabase/core/components/Form";
+import FormProvider from "metabase/core/components/FormProvider";
 import FormInput from "metabase/core/components/FormInput";
 import FormDateInput from "metabase/core/components/FormDateInput";
 import FormTextArea from "metabase/core/components/FormTextArea";
 import FormSelect from "metabase/core/components/FormSelect";
+import FormSubmitButton from "metabase/core/components/FormSubmitButton";
+import FormErrorMessage from "metabase/core/components/FormErrorMessage";
 import { Timeline, TimelineEventData } from "metabase-types/api";
+import FormArchiveButton from "../FormArchiveButton";
+import { EventFormFooter } from "./EventForm.styled";
 
 const EventSchema = Yup.object({
   name: Yup.string()
@@ -28,6 +33,7 @@ export interface EventFormProps {
   initialValues: TimelineEventData;
   timelines: Timeline[];
   onSubmit: (data: TimelineEventData) => void;
+  onArchive?: () => void;
   onCancel?: () => void;
 }
 
@@ -35,6 +41,7 @@ const EventForm = ({
   initialValues,
   timelines,
   onSubmit,
+  onArchive,
   onCancel,
 }: EventFormProps): JSX.Element => {
   const isNew = initialValues.id == null;
@@ -77,6 +84,22 @@ const EventForm = ({
               options={timelineOptions}
             />
           )}
+          <EventFormFooter>
+            <FormErrorMessage inline />
+            {!isNew && (
+              <FormArchiveButton onClick={onArchive}>
+                {t`Archive event`}
+              </FormArchiveButton>
+            )}
+            <Button type="button" onClick={onCancel}>
+              {t`Cancel`}
+            </Button>
+            <FormSubmitButton
+              title={isNew ? t`Create` : t`Update`}
+              disabled={!dirty}
+              primary
+            />
+          </EventFormFooter>
         </Form>
       )}
     </FormProvider>
