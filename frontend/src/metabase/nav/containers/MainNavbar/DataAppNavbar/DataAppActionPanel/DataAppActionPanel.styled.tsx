f06eb6b1601908884d1ca0f9a8033c6a335c5027
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-11 15:24:43 +0000

    Always keep the "+" button visible in data app navbar (#26415)
    * Always keep the "+" button visible in navbar
    
    * Move `NewButton` to `DataAppActionPanel`

--- frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbar/DataAppActionPanel/DataAppActionPanel.styled.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/DataAppNavbar/DataAppActionPanel/DataAppActionPanel.styled.tsx
@@ -2,6 +2,14 @@ import styled from "@emotion/styled";
 
 export const Root = styled.div`
   display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+`;
+
+export const ActionsContainer = styled.div`
+  display: flex;
   align-items: center;
   gap: 0.5rem;
 `;
