2b801a710cee8eeb1205a0f29fbe6f78ebb2d918
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-14 14:45:24 +0000

    Simplify null checks

--- frontend/src/metabase/selectors/user.js
+++ frontend/src/metabase/selectors/user.js
@@ -7,7 +7,7 @@ export const getUserId = createSelector([getUser], user => user?.id);
 
 export const getUserIsAdmin = createSelector(
   [getUser],
-  user => (user && user.is_superuser) || false,
+  user => user?.is_superuser || false,
 );
 
 export const canManageSubscriptions = createSelector(
@@ -21,10 +21,10 @@ export const canManageSubscriptions = createSelector(
 
 export const getUserAttributes = createSelector(
   [getUser],
-  user => (user && user.login_attributes) || [],
+  user => user?.login_attributes || [],
 );
 
 export const getUserPersonalCollectionId = createSelector(
   [getUser],
-  user => (user && user.personal_collection_id) || null,
+  user => user?.personal_collection_id || null,
 );
