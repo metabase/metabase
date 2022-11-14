283e646291d6c8ca2df3c4262e88a6a9011dc8c2
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-14 12:58:26 +0000

    Extend entities state type

--- frontend/src/metabase-types/store/entities.ts
+++ frontend/src/metabase-types/store/entities.ts
@@ -7,9 +7,16 @@ import {
   Table,
 } from "metabase-types/api";
 
+type EntityList<T> = Record<
+  any, // Usually a JSON stringified list query object or `null`
+  { list: T[]; metadata: { total?: number; limit?: number; offset?: number } }
+>;
+
 export interface EntitiesState {
   collections?: Record<CollectionId, Collection>;
   dataApps?: Record<DataAppId, DataApp>;
+  dataAppCollections?: Record<CollectionId, Collection>;
+  dataAppCollections_list?: EntityList<Collection>;
   databases?: Record<number, Database>;
   tables?: Record<number | string, Table>;
 }
