1ec14463e2c982ae386a646c02b2cfd5ea9de90f
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-14 14:28:12 +0000

    Simplify list endpoint code

--- frontend/src/metabase/entities/collections/collections.js
+++ frontend/src/metabase/entities/collections/collections.js
@@ -32,10 +32,7 @@ const Collections = createEntity({
 
   api: {
     list: async (params, ...args) =>
-      /* Parts of the UI, like ItemPicker don't yet know about the /tree endpoint and break if it's used,
-      so choose which endpoint to use based on the presence of a "tree" param
-      */
-      params && params.tree
+      params?.tree
         ? listCollectionsTree(params, ...args)
         : listCollections(params, ...args),
   },
