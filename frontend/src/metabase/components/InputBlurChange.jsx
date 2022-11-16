b6b156319f80325c11f009b958f5376566255a31
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-11-16 20:45:41 +0200

    Fix resetting inputs on data model page

--- frontend/src/metabase/components/InputBlurChange.jsx
+++ frontend/src/metabase/components/InputBlurChange.jsx
@@ -33,7 +33,9 @@ export default class InputBlurChange extends Component {
   };
 
   UNSAFE_componentWillReceiveProps(newProps) {
-    this.setState({ value: newProps.value });
+    if (newProps.value !== this.props.value) {
+      this.setState({ value: newProps.value });
+    }
   }
 
   onChange(event) {
