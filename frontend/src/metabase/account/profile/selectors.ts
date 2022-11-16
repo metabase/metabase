b9436a7b11e54e2066635772e1608bac700d4b02
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-16 15:17:35 +0000

    Simplify `getIsSsoUser` selector

--- frontend/src/metabase/account/profile/selectors.ts
+++ frontend/src/metabase/account/profile/selectors.ts
@@ -1,16 +1,15 @@
 import { createSelector } from "reselect";
 
-import { checkNotNull } from "metabase/core/utils/types";
-
 import { getUser } from "metabase/selectors/user";
 import { getSettings } from "metabase/selectors/settings";
 
 import { PLUGIN_IS_PASSWORD_USER } from "metabase/plugins";
 
 export const getIsSsoUser = createSelector(getUser, user => {
-  return !PLUGIN_IS_PASSWORD_USER.every(predicate =>
-    predicate(checkNotNull(user)),
-  );
+  if (!user) {
+    return false;
+  }
+  return !PLUGIN_IS_PASSWORD_USER.every(predicate => predicate(user));
 });
 
 export const getLocales = createSelector([getSettings], settings => {
