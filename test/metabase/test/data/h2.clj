33dda60821e693914062f7a2e5f1b410d19c988e
John Cromartie <john@metabase.com>
2022-11-17 21:46:56 +0000

    Removes deprecated SET UNDO_LOG statement from h2 test db

--- test/metabase/test/data/h2.clj
+++ test/metabase/test/data/h2.clj
@@ -65,11 +65,7 @@
 
 (defmethod sql.tx/create-db-sql :h2
   [& _]
-  [;; We don't need to actually do anything to create a database here. Just disable the undo
-   ;; log (i.e., transactions) for this DB session because the bulk operations to load data don't need to be atomic
-   "SET UNDO_LOG = 0;"
-
-   ;; Create a non-admin account 'GUEST' which will be used from here on out
+  [;; Create a non-admin account 'GUEST' which will be used from here on out
    "CREATE USER IF NOT EXISTS GUEST PASSWORD 'guest';"
 
    ;; Set DB_CLOSE_DELAY here because only admins are allowed to do it, so we can't set it via the connection string.
