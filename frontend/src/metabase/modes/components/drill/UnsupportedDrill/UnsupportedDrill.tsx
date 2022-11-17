b994c7a3f15c85475ddf1591ebe29a1a74225f0c
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-11-17 13:57:35 +0200

    Add styles

--- frontend/src/metabase/modes/components/drill/UnsupportedDrill/UnsupportedDrill.tsx
+++ frontend/src/metabase/modes/components/drill/UnsupportedDrill/UnsupportedDrill.tsx
@@ -1,6 +1,14 @@
+import React from "react";
 import { t } from "ttag";
+import MetabaseSettings from "metabase/lib/settings";
+import Icon from "metabase/components/Icon";
 import Question from "metabase-lib/Question";
 import { unsupportedDrill } from "metabase-lib/queries/drills/unsupported-drill";
+import {
+  DrillLearnLink,
+  DrillMessage,
+  DrillRoot,
+} from "./UnsupportedDrill.styled";
 
 interface UnsupportedDrillProps {
   question: Question;
@@ -11,12 +19,24 @@ const UnsupportedDrill = ({ question }: UnsupportedDrillProps) => {
     return [];
   }
 
+  const learnUrl = MetabaseSettings.learnUrl("questions/drill-through");
+
   return [
     {
       name: "unsupported",
       section: "info",
       buttonType: "info",
-      title: t`Drill-through doesn’t work on SQL questions.`,
+      title: (
+        <DrillRoot>
+          <DrillMessage>
+            {t`Drill-through doesn’t work on SQL questions.`}
+          </DrillMessage>
+          <DrillLearnLink href={learnUrl}>
+            <Icon name="reference" />
+            {t`Learn more`}
+          </DrillLearnLink>
+        </DrillRoot>
+      ),
     },
   ];
 };
