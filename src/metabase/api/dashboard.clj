154ca21d11c7c581e384a5a601f38695edf89692
Case Nelson <case@metabase.com>
2022-11-17 13:44:45 -0700

    Actions for public pages (#26554)
    * [Apps] Testing out public dashboards
    
    * Add WIP endpoints for public execution
    
    * Add throttle check to public execution route
    
    * Bump attempts-threshold to 1000
    
    * Bump threshold to 5000
    
    * Add test
    
    * Fix linter
    
    * Review changes
    
    * Fix lint

--- src/metabase/api/dashboard.clj
+++ src/metabase/api/dashboard.clj
@@ -814,6 +814,7 @@
    dashcard-id su/IntGreaterThanZero
    slug su/NonBlankString
    parameters su/JSONString}
+  (api/read-check Dashboard dashboard-id)
   (actions.execution/fetch-values dashboard-id dashcard-id slug (json/parse-string parameters)))
 
 (api/defendpoint POST "/:dashboard-id/dashcard/:dashcard-id/execute/:slug"
@@ -826,7 +827,7 @@
    dashcard-id su/IntGreaterThanZero
    slug su/NonBlankString
    parameters (s/maybe {s/Keyword s/Any})}
-  (action/check-data-apps-enabled)
+  (api/read-check Dashboard dashboard-id)
   ;; Undo middleware string->keyword coercion
   (actions.execution/execute-dashcard! dashboard-id dashcard-id slug (update-keys parameters name)))
 
