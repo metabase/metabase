dfa55039ec16e2d6ff4a8e4084f343b6095729d4
Callum Herries <hi@callumherries.com>
2022-11-17 16:42:04 +0100

    Fix FE type inferencer

--- frontend/src/metabase-lib/expressions/typeinferencer.js
+++ frontend/src/metabase-lib/expressions/typeinferencer.js
@@ -6,6 +6,7 @@ export const MONOTYPE = {
   Number: "number",
   String: "string",
   Boolean: "boolean",
+  DateTime: "datetime",
 };
 
 export function infer(mbql, env) {
@@ -34,6 +35,8 @@ export function infer(mbql, env) {
   }
 
   switch (op) {
+    case "now":
+      return MONOTYPE.DateTime;
     case "case":
       return infer(mbql[1][0][1], env);
     case "coalesce":
