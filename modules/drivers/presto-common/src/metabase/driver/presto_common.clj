f5f0cd6bd5d632d78ec5a6d2edc2c7be3ddab370
Callum Herries <hi@callumherries.com>
2022-11-18 11:18:50 +0100

    Add presto implementation

--- modules/drivers/presto-common/src/metabase/driver/presto_common.clj
+++ modules/drivers/presto-common/src/metabase/driver/presto_common.clj
@@ -168,9 +168,15 @@
             (hh/where [:> :__rownum__ offset])
             (hh/limit items))))))
 
+<<<<<<< HEAD
 (defmethod sql.qp/current-datetime-honeysql-form :presto-common
   [_]
   (hx/with-database-type-info :%now "timestamp with time zone"))
+=======
+(defmethod sql.qp/->honeysql [:presto-common :now]
+  [_driver _clause]
+  (hsql/call :date_trunc (hx/literal :second) (hsql/call :now)))
+>>>>>>> 8a08b3afd1 (Add presto implementation)
 
 (defn- date-diff [unit a b] (hsql/call :date_diff (hx/literal unit) a b))
 (defn- date-trunc [unit x] (hsql/call :date_trunc (hx/literal unit) x))
