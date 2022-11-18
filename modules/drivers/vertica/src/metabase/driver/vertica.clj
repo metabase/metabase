ab68a3c3d0b0b68667b481e629e7a47d88bff857
Callum Herries <hi@callumherries.com>
2022-11-18 17:00:21 +0100

    Fix vertica implementation for report timezones

--- modules/drivers/vertica/src/metabase/driver/vertica.clj
+++ modules/drivers/vertica/src/metabase/driver/vertica.clj
@@ -72,8 +72,8 @@
       (sql-jdbc.common/handle-additional-options details)))
 
 (defmethod sql.qp/->honeysql [:vertica :now]
-  [_driver _clause]
-  (hsql/call :current_timestamp 0))
+  [_ _]
+  (hsql/call :localtimestamp 0))
 
 (defmethod sql.qp/unix-timestamp->honeysql [:vertica :seconds]
   [_ _ expr]
