a2aaf6da10fa389b958142efa3cbdc5e3b91f005
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-11-21 18:13:37 +0700

    Add `getSeriesWithLegends` tests

--- frontend/src/metabase/static-viz/components/LineAreaBarChart/utils/series.unit.spec.ts
+++ frontend/src/metabase/static-viz/components/LineAreaBarChart/utils/series.unit.spec.ts
@@ -1,11 +1,11 @@
-import { merge } from "icepick";
+import { merge, setIn } from "icepick";
 import { colors } from "metabase/lib/colors";
 import type {
   ChartSettings,
   SeriesWithOneOrLessDimensions,
   SeriesWithTwoDimensions,
 } from "../../XYChart/types";
-import { getSeriesWithColors } from "./series";
+import { getSeriesWithColors, getSeriesWithLegends } from "./series";
 
 const settings: ChartSettings = {
   x: {
@@ -620,3 +620,411 @@ function getPalette(instanceColors: Record<string, string>) {
     ...instanceColors,
   };
 }
+
+describe("getSeriesWithLegends", () => {
+  it("should return an empty series given an empty series", () => {
+    const seriesWithLegends = getSeriesWithLegends([], settings);
+
+    expect(seriesWithLegends).toEqual([]);
+  });
+
+  describe("Series without ones or less dimensions", () => {
+    const multipleSeries: SeriesWithOneOrLessDimensions[][] = [
+      [
+        {
+          name: "Count",
+          cardName: "Bar chart",
+          yAxisPosition: "left",
+          type: "bar",
+          data: [
+            ["Doohickey", 3976],
+            ["Gadget", 4939],
+            ["Gizmo", 4784],
+            ["Widget", 5061],
+          ],
+          column: {
+            name: "count",
+            source: "aggregation",
+            display_name: "Count",
+          },
+        },
+      ],
+    ];
+
+    const multipleSeriesDashcard: SeriesWithOneOrLessDimensions[][] = [
+      [
+        {
+          name: "Count",
+          cardName: "Bar chart",
+          yAxisPosition: "left",
+          type: "bar",
+          data: [
+            ["Doohickey", 3976],
+            ["Gadget", 4939],
+            ["Gizmo", 4784],
+            ["Widget", 5061],
+          ],
+          column: {
+            name: "count",
+            source: "aggregation",
+            display_name: "Count",
+          },
+        },
+      ],
+      [
+        {
+          name: "Count",
+          cardName: "Area chart",
+          yAxisPosition: "left",
+          type: "area",
+          data: [
+            ["Doohickey", 3976],
+            ["Gadget", 4939],
+            ["Gizmo", 4784],
+            ["Widget", 5061],
+          ],
+          column: {
+            name: "count",
+            source: "aggregation",
+            display_name: "Count",
+          },
+        },
+      ],
+    ];
+
+    it("should assign legends given series", () => {
+      const seriesWithLegends = getSeriesWithLegends(multipleSeries, settings);
+
+      const expectedSeries = [
+        [
+          {
+            name: "Count",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+          },
+        ],
+      ];
+
+      expect(seriesWithLegends).toEqual(expectedSeries);
+    });
+
+    it("it should assign legends from column custom name", () => {
+      const seriesWithLegends = getSeriesWithLegends(
+        // This might not be apparent, but series' `name` would be set to
+        // custom metric name for series with one or less dimensions.
+        setIn(multipleSeries, [0, 0, "name"], "Custom count"),
+        settings,
+      );
+
+      const expectedSeries = [
+        [
+          {
+            name: "Custom count",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+          },
+        ],
+      ];
+
+      expect(seriesWithLegends).toEqual(expectedSeries);
+    });
+
+    it("it should assign legends on multiple series dashcard", () => {
+      const seriesWithLegends = getSeriesWithLegends(
+        multipleSeriesDashcard,
+        settings,
+      );
+
+      const expectedSeries = [
+        [
+          {
+            name: "Count",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+          },
+        ],
+        [
+          {
+            name: "Area chart",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+          },
+        ],
+      ];
+
+      expect(seriesWithLegends).toEqual(expectedSeries);
+    });
+  });
+
+  describe("Series with 2 dimension", () => {
+    const multipleSeries: SeriesWithTwoDimensions[][] = [
+      [
+        {
+          name: null,
+          cardName: "Area chart",
+          type: "area",
+          data: [
+            ["Doohickey", 177],
+            ["Gadget", 199],
+            ["Gizmo", 158],
+            ["Widget", 210],
+          ],
+          yAxisPosition: "left",
+          column: {
+            semantic_type: "type/CreationTimestamp",
+            unit: "year",
+            name: "CREATED_AT",
+            source: "breakout",
+            display_name: "Created At",
+          },
+          breakoutValue: "2016-01-01T00:00:00Z",
+        },
+        {
+          name: null,
+          cardName: "Area chart",
+          type: "area",
+          data: [
+            ["Doohickey", 1206],
+            ["Gadget", 1505],
+            ["Gizmo", 1592],
+            ["Widget", 1531],
+          ],
+          yAxisPosition: "left",
+          column: {
+            semantic_type: "type/CreationTimestamp",
+            unit: "year",
+            name: "CREATED_AT",
+            source: "breakout",
+            display_name: "Created At",
+          },
+          breakoutValue: "2017-01-01T00:00:00Z",
+        },
+      ],
+    ];
+
+    const multipleSeriesDashcard: SeriesWithTwoDimensions[][] = [
+      [
+        {
+          name: null,
+          cardName: "Area chart",
+          type: "area",
+          data: [
+            ["Doohickey", 177],
+            ["Gadget", 199],
+            ["Gizmo", 158],
+            ["Widget", 210],
+          ],
+          yAxisPosition: "left",
+          column: {
+            semantic_type: "type/CreationTimestamp",
+            unit: "year",
+            name: "CREATED_AT",
+            source: "breakout",
+            display_name: "Created At",
+          },
+          breakoutValue: "2016-01-01T00:00:00Z",
+        },
+        {
+          name: null,
+          cardName: "Area chart",
+          type: "area",
+          data: [
+            ["Doohickey", 1206],
+            ["Gadget", 1505],
+            ["Gizmo", 1592],
+            ["Widget", 1531],
+          ],
+          yAxisPosition: "left",
+          column: {
+            semantic_type: "type/CreationTimestamp",
+            unit: "year",
+            name: "CREATED_AT",
+            source: "breakout",
+            display_name: "Created At",
+          },
+          breakoutValue: "2017-01-01T00:00:00Z",
+        },
+      ],
+
+      [
+        {
+          name: null,
+          cardName: "Bar chart",
+          type: "bar",
+          data: [
+            ["Doohickey", 177],
+            ["Gadget", 199],
+            ["Gizmo", 158],
+            ["Widget", 210],
+          ],
+          yAxisPosition: "left",
+          column: {
+            semantic_type: "type/CreationTimestamp",
+            unit: "year",
+            name: "CREATED_AT",
+            source: "breakout",
+            display_name: "Created At",
+          },
+          breakoutValue: "2016-01-01T00:00:00Z",
+        },
+        {
+          name: null,
+          cardName: "Bar chart",
+          type: "bar",
+          data: [
+            ["Doohickey", 1206],
+            ["Gadget", 1505],
+            ["Gizmo", 1592],
+            ["Widget", 1531],
+          ],
+          yAxisPosition: "left",
+          column: {
+            semantic_type: "type/CreationTimestamp",
+            unit: "year",
+            name: "CREATED_AT",
+            source: "breakout",
+            display_name: "Created At",
+          },
+          breakoutValue: "2017-01-01T00:00:00Z",
+        },
+      ],
+    ];
+
+    it("should assign legends given series", () => {
+      const seriesWithLegends = getSeriesWithLegends(
+        multipleSeries,
+        merge(settings, {
+          x: { type: "timeseries" },
+        }),
+      );
+
+      const expectedSeries = [
+        [
+          {
+            name: "2016",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+          {
+            name: "2017",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+        ],
+      ];
+
+      expect(seriesWithLegends).toEqual(expectedSeries);
+    });
+
+    it("should assign legends from column custom name", () => {
+      const seriesWithLegends = getSeriesWithLegends(
+        multipleSeries,
+        merge(settings, {
+          x: { type: "timeseries" },
+          series_settings: {
+            2017: {
+              title: "custom 2017",
+            },
+          },
+        }),
+      );
+
+      const expectedSeries = [
+        [
+          {
+            name: "2016",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+          {
+            name: "custom 2017",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+        ],
+      ];
+
+      expect(seriesWithLegends).toEqual(expectedSeries);
+    });
+
+    it("it should assign legends on multiple series dashcard", () => {
+      const seriesWithLegends = getSeriesWithLegends(
+        multipleSeriesDashcard,
+        settings,
+      );
+
+      const expectedSeries = [
+        [
+          {
+            name: "Area chart: 2016",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+          {
+            name: "Area chart: 2017",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+        ],
+        [
+          {
+            name: "Bar chart: 2016",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+          {
+            name: "Bar chart: 2017",
+            cardName: expect.anything(),
+            yAxisPosition: expect.anything(),
+            type: expect.anything(),
+            data: expect.anything(),
+            column: expect.anything(),
+            breakoutValue: expect.anything(),
+          },
+        ],
+      ];
+
+      expect(seriesWithLegends).toEqual(expectedSeries);
+    });
+  });
+});
