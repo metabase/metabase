8221b0f8ec65da1ffc69b49aec9e960f66f1a73e
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-21 15:14:06 +0000

    Extract action button components

--- frontend/src/metabase/dashboard/components/DashCard/DashCardActionButtons/DashCardActionButtons.jsx
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardActionButtons/DashCardActionButtons.jsx
@@ -3,125 +3,20 @@ import React from "react";
 import { t } from "ttag";
 
 import Icon from "metabase/components/Icon";
-import ModalWithTrigger from "metabase/components/ModalWithTrigger";
 import Tooltip from "metabase/components/Tooltip";
 
-import visualizations, { getVisualizationRaw } from "metabase/visualizations";
-import { ChartSettingsWithState } from "metabase/visualizations/components/ChartSettings";
+import { getVisualizationRaw } from "metabase/visualizations";
 
 import { isActionCard } from "metabase/writeback/utils";
 
-const HEADER_ICON_SIZE = 16;
+import AddSeriesButton from "./AddSeriesButton";
+import ChartSettingsButton from "./ChartSettingsButton";
+import RemoveButton from "./RemoveButton";
+import ToggleCardPreviewButton from "./ToggleCardPreviewButton";
 
 const HEADER_ACTION_STYLE = {
   padding: 4,
 };
-
-function ChartSettingsButton({
-  series,
-  onReplaceAllVisualizationSettings,
-  dashboard,
-}) {
-  return (
-    <ModalWithTrigger
-      wide
-      tall
-      triggerElement={
-        <Tooltip tooltip={t`Visualization options`}>
-          <Icon
-            name="palette"
-            size={HEADER_ICON_SIZE}
-            style={HEADER_ACTION_STYLE}
-          />
-        </Tooltip>
-      }
-      triggerClasses="text-dark-hover cursor-pointer flex align-center flex-no-shrink mr1 drag-disabled"
-      enableMouseEvents
-    >
-      <ChartSettingsWithState
-        className="spread"
-        series={series}
-        onChange={onReplaceAllVisualizationSettings}
-        isDashboard
-        dashboard={dashboard}
-      />
-    </ModalWithTrigger>
-  );
-}
-
-function RemoveButton({ onRemove }) {
-  return (
-    <a
-      className="text-dark-hover drag-disabled"
-      data-metabase-event="Dashboard;Remove Card Modal"
-      onClick={onRemove}
-      style={HEADER_ACTION_STYLE}
-    >
-      <Icon name="close" size={HEADER_ICON_SIZE} />
-    </a>
-  );
-}
-
-function AddSeriesButton({ series, onAddSeries }) {
-  return (
-    <a
-      data-testid="add-series-button"
-      data-metabase-event="Dashboard;Edit Series Modal;open"
-      className="text-dark-hover cursor-pointer h3 flex-no-shrink relative mr1 drag-disabled"
-      onClick={onAddSeries}
-      style={HEADER_ACTION_STYLE}
-    >
-      <Tooltip tooltip={series.length > 1 ? t`Edit series` : t`Add series`}>
-        <span className="flex align-center">
-          <span className="flex">
-            <Icon
-              className="absolute"
-              name="add"
-              style={{ top: 0, left: 1 }}
-              size={HEADER_ICON_SIZE / 2}
-            />
-            <Icon
-              name={getSeriesIconName(series)}
-              size={HEADER_ICON_SIZE - 2}
-            />
-          </span>
-        </span>
-      </Tooltip>
-    </a>
-  );
-}
-
-function ToggleCardPreviewButton({ isPreviewing, onPreviewToggle }) {
-  return (
-    <a
-      data-metabase-event="Dashboard;Text;edit"
-      className="text-dark-hover cursor-pointer h3 flex-no-shrink relative mr1 drag-disabled"
-      onClick={onPreviewToggle}
-      style={HEADER_ACTION_STYLE}
-    >
-      <Tooltip tooltip={isPreviewing ? t`Edit` : t`Preview`}>
-        <span className="flex align-center">
-          <span className="flex" style={{ width: 18 }}>
-            {isPreviewing ? (
-              <Icon name="edit_document" size={HEADER_ICON_SIZE} />
-            ) : (
-              <Icon name="eye" size={18} />
-            )}
-          </span>
-        </span>
-      </Tooltip>
-    </a>
-  );
-}
-
-function getSeriesIconName(series) {
-  try {
-    const display = series[0].card.display;
-    return visualizations.get(display === "scalar" ? "bar" : display).iconName;
-  } catch (e) {
-    return "bar";
-  }
-}
 
 function DashCardActionButtons({
   card,
