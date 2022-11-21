131a2cf21647cd467e576ba0ddd444c1ab408a7d
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-21 15:38:30 +0000

    Clean up `ChartSettingsButton`

--- frontend/src/metabase/dashboard/components/DashCard/DashCardActionButtons/ChartSettingsButton.tsx
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardActionButtons/ChartSettingsButton.tsx
@@ -1,38 +1,37 @@
-/* eslint-disable react/prop-types */
 import React from "react";
+import _ from "underscore";
 import { t } from "ttag";
 
-import Icon from "metabase/components/Icon";
 import ModalWithTrigger from "metabase/components/ModalWithTrigger";
-import Tooltip from "metabase/components/Tooltip";
 
 import { ChartSettingsWithState } from "metabase/visualizations/components/ChartSettings";
 
-const HEADER_ICON_SIZE = 16;
+import { VisualizationSettings } from "metabase-types/api/card";
+import { DashboardWithCards } from "metabase-types/types/Dashboard";
+import { Series } from "metabase-types/types/Visualization";
 
-const HEADER_ACTION_STYLE = {
-  padding: 4,
-};
+import DashCardActionButton from "./DashCardActionButton";
+
+interface Props {
+  series: Series;
+  dashboard: DashboardWithCards;
+  onReplaceAllVisualizationSettings: (settings: VisualizationSettings) => void;
+}
 
 function ChartSettingsButton({
   series,
-  onReplaceAllVisualizationSettings,
   dashboard,
-}) {
+  onReplaceAllVisualizationSettings,
+}: Props) {
   return (
     <ModalWithTrigger
       wide
       tall
       triggerElement={
-        <Tooltip tooltip={t`Visualization options`}>
-          <Icon
-            name="palette"
-            size={HEADER_ICON_SIZE}
-            style={HEADER_ACTION_STYLE}
-          />
-        </Tooltip>
+        <DashCardActionButton tooltip={t`Visualization options`}>
+          <DashCardActionButton.Icon name="palette" />
+        </DashCardActionButton>
       }
-      triggerClasses="text-dark-hover cursor-pointer flex align-center flex-no-shrink mr1 drag-disabled"
       enableMouseEvents
     >
       <ChartSettingsWithState
