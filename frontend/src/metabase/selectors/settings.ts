5ac1aa4160c159fc40ab79bfeaf292b0d424d688
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-21 13:20:39 +0000

    Make `getSettings` a `reselect` selector

--- frontend/src/metabase/selectors/settings.ts
+++ frontend/src/metabase/selectors/settings.ts
@@ -3,7 +3,10 @@ import { createSelector } from "reselect";
 import type { Settings, SettingKey } from "metabase-types/api";
 import type { State } from "metabase-types/store";
 
-export const getSettings = (state: State) => state.settings.values;
+export const getSettings = createSelector(
+  (state: State) => state.settings,
+  settings => settings.values,
+);
 
 export const getSetting = <T extends SettingKey>(
   state: State,
