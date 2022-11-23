8eea4837465840478a7696acf2f207d287ad4ad7
Tamás Benkő <tamas@metabase.com>
2022-11-23 22:33:55 +0300

    Fix metabase.api.dashboard-test/dashcard-http-action-execution-test

--- test/metabase/api/dashboard_test.clj
+++ test/metabase/api/dashboard_test.clj
@@ -2257,14 +2257,13 @@
 (deftest dashcard-http-action-execution-test
   (mt/test-drivers (mt/normal-drivers-with-feature :actions)
     (actions.test-util/with-actions-test-data-and-actions-enabled
-      (actions.test-util/with-action [{:keys [action-id]} {:type :http}]
+      (actions.test-util/with-action [{:keys [action-id model-id]} {:type :http}]
         (testing "Executing dashcard with action"
-          (mt/with-temp* [Card [{card-id :id} {:dataset true}]
-                          ModelAction [_ {:slug "custom" :card_id card-id :action_id action-id}]
-                          Dashboard [{dashboard-id :id}]
+          (mt/with-temp* [Dashboard [{dashboard-id :id}]
                           DashboardCard [{dashcard-id :id} {:dashboard_id dashboard-id
-                                                            :card_id card-id}]]
-            (let [execute-path (format "dashboard/%s/dashcard/%s/execute/custom"
+                                                            :action_id action-id
+                                                            :card_id model-id}]]
+            (let [execute-path (format "dashboard/%s/dashcard/%s/execute"
                                        dashboard-id
                                        dashcard-id)]
               (testing "Should be able to execute an action"
