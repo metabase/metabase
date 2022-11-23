56382238d5b259d414e1a26d86577b3e87428697
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-23 18:37:33 +0000

    Add hover effect

--- frontend/src/metabase/nav/components/AppBar/DataAppBackButton/DataAppBackButton.styled.tsx
+++ frontend/src/metabase/nav/components/AppBar/DataAppBackButton/DataAppBackButton.styled.tsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 import Link from "metabase/core/components/Link";
 import Icon from "metabase/components/Icon";
 
-import { color } from "metabase/lib/colors";
+import { alpha, color } from "metabase/lib/colors";
 
 export const DataAppLink = styled(Link)`
   display: flex;
@@ -14,14 +14,20 @@ export const DataAppLink = styled(Link)`
   font-size: 0.875rem;
   font-weight: bold;
 
-  background-color: ${color("brand-light")};
+  background-color: ${alpha("brand", 0.2)};
   color: ${color("brand")};
-  border: 1px solid ${color("brand-light")};
+  border: 1px solid ${alpha("brand", 0.2)};
 
   padding: 8px 10px;
   border-radius: 99px;
 
+  transition: background 300ms linear, border 300ms linear;
+
   ${Icon.Root} {
     color: ${color("brand")};
+  }
+
+  &:hover {
+    background-color: ${alpha("brand", 0.35)};
   }
 `;
