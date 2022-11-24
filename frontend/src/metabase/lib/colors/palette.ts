2bf758740336b72296d8ed355e6acd99485515e4
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-11-24 16:48:51 +0300

    increase factor

--- frontend/src/metabase/lib/colors/palette.ts
+++ frontend/src/metabase/lib/colors/palette.ts
@@ -156,7 +156,7 @@ const LIGHT_HSL_RANGES = [
 
 // We intentionally want to return white text color more frequently
 // https://www.notion.so/Maz-notes-on-viz-settings-67aed0e4ddcc4d4a83028992c4301820?d=513f4f7fa9c143cb874c7e4525dfb1e9#277d6b3eeb464eac86088abd144fde9e
-const whiteTextColorPriorityFactor = 1.8;
+const whiteTextColorPriorityFactor = 3;
 
 export const getTextColorForBackground = (backgroundColor: string) => {
   const whiteTextContrast =
@@ -166,7 +166,7 @@ export const getTextColorForBackground = (backgroundColor: string) => {
     Color(color("text-dark")),
   );
 
-  return whiteTextContrast * whiteTextColorPriorityFactor > darkTextContrast
+  return whiteTextContrast > darkTextContrast
     ? color("white")
     : color("text-dark");
 };
