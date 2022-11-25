56c9ffaf1de15894e01a737d8a2bb030cddcc4ea
Nick Fitzpatrick <nick@metabase.com>
2022-11-25 11:52:16 -0400

    Changing to use Button component

--- frontend/src/metabase/visualizations/components/settings/ChartSettingIconRadio.styled.tsx
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingIconRadio.styled.tsx
@@ -1,23 +1,6 @@
 import styled from "@emotion/styled";
-import Icon from "metabase/components/Icon";
+import Button from "metabase/core/components/Button";
 
-import { color } from "metabase/lib/colors";
-
-interface RadioIconProps {
-  isSelected: boolean;
-}
-
-export const RadioIcon = styled(Icon)<RadioIconProps>`
+export const IconButton = styled(Button)`
   margin-left: 0.5rem;
-  cursor: pointer;
-  user-select: none;
-  border: 1px solid ${color("border")};
-  border-radius: 0.5rem;
-  padding: 0.5rem;
-
-  &:hover {
-    color: ${color("brand")};
-  }
-
-  ${props => props.isSelected && `color: ${color("brand")}`}
 `;
