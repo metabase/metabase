af6fdebe980630fa7c0bdcca17c28a75d88aeead
Anton Kulyk <kuliks.anton@gmail.com>
2022-11-30 12:31:02 +0000

    Update `FormCollectionAuthorityLevel`

--- enterprise/frontend/src/metabase-enterprise/collections/components/FormCollectionAuthorityLevel.tsx
+++ enterprise/frontend/src/metabase-enterprise/collections/components/FormCollectionAuthorityLevel.tsx
@@ -1,22 +1,58 @@
-import React from "react";
-import { SegmentedControl } from "metabase/components/SegmentedControl/SegmentedControl";
-import { FormFieldRoot } from "./FormCollectionAuthorityLevel.styled";
+import React, { HTMLAttributes } from "react";
+import { useField } from "formik";
+import { t } from "ttag";
 
-interface Props {
-  field: any;
-  options: any;
+import { useUniqueId } from "metabase/hooks/use-unique-id";
+
+import FormField from "metabase/core/components/FormField";
+import { SegmentedControl } from "metabase/components/SegmentedControl";
+
+import { REGULAR_COLLECTION, OFFICIAL_COLLECTION } from "../constants";
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  name: string;
+  title?: string;
 }
 
-export function FormCollectionAuthorityLevel({ field, options }: Props) {
+const OPTIONS = [
+  {
+    name: REGULAR_COLLECTION.name,
+    value: REGULAR_COLLECTION.type,
+    icon: REGULAR_COLLECTION.icon,
+  },
+  {
+    name: OFFICIAL_COLLECTION.name,
+    value: OFFICIAL_COLLECTION.type,
+    icon: OFFICIAL_COLLECTION.icon,
+    selectedColor: OFFICIAL_COLLECTION.color,
+  },
+];
+
+export function FormCollectionAuthorityLevel({
+  className,
+  style,
+  name = "authority_level",
+  title = t`Collection type`,
+}: Props) {
+  const id = useUniqueId();
+  const [{ value }, { error, touched }, { setValue }] = useField(name);
+
   return (
-    <FormFieldRoot>
+    <FormField
+      className={className}
+      style={style}
+      title={title}
+      htmlFor={id}
+      infoTooltip={t`The contents of Official collections will get a badge by their name and will be more likely to show up in search results.`}
+      error={touched ? error : undefined}
+    >
       <SegmentedControl
-        value={field.value}
-        onChange={field.onChange}
-        options={options}
+        value={value}
+        onChange={setValue}
+        options={OPTIONS}
         variant="fill-background"
         inactiveColor="text-dark"
       />
-    </FormFieldRoot>
+    </FormField>
   );
 }
