17502aad01df1738b3600b4199dbe3dcf6d8b76b
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-11-30 16:18:40 +0200

    Remove special handling for mongo db in the database form (#26819)

--- bin/build-drivers/src/build_drivers/lint_manifest_file.clj
+++ bin/build-drivers/src/build_drivers/lint_manifest_file.clj
@@ -23,7 +23,7 @@
 (s/def ::contact-info (spell/keys :req-un [::name]
                                   :opt-un [::address]))
 
-(def ^:private property-types #{"string" "text" "textFile" "boolean" "secret" "info" "schema-filters"})
+(def ^:private property-types #{"string" "text" "textFile" "boolean" "secret" "info" "schema-filters" "section"})
 
 (s/def ::display-name string?)
 (s/def ::default any?)
