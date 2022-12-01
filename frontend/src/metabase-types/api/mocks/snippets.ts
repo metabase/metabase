3798a419081a86ec4d89d90653b249487f4eff13
Anton Kulyk <kuliks.anton@gmail.com>
2022-12-01 17:00:53 +0000

    Add `NativeQuerySnippet` mocks

---
+++ frontend/src/metabase-types/api/mocks/snippets.ts
@@ -0,0 +1,21 @@
+import { NativeQuerySnippet } from "metabase-types/api";
+import { createMockUser } from "./user";
+
+export const createMockNativeQuerySnippet = ({
+  creator = createMockUser(),
+  creator_id = creator.id,
+  ...opts
+}: Partial<NativeQuerySnippet> = {}): NativeQuerySnippet => ({
+  id: 1,
+  name: "My Snippet",
+  description: null,
+  content: "SELECT * FROM my_table",
+  collection_id: null,
+  creator,
+  creator_id,
+  entity_id: "snippet_entity_id",
+  archived: false,
+  created_at: new Date().toISOString(),
+  updated_at: new Date().toISOString(),
+  ...opts,
+});
