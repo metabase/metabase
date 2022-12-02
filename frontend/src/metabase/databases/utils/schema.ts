1b1d7871c5ff657b08c62f194675caa6488eed91
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-12-02 15:39:07 +0200

    Add to schema

--- frontend/src/metabase/databases/utils/schema.ts
+++ frontend/src/metabase/databases/utils/schema.ts
@@ -23,6 +23,7 @@ export const getValidationSchema = (
     }),
     auto_run_queries: Yup.boolean().default(true),
     refingerprint: Yup.boolean().default(false),
+    cache_ttl: Yup.number().nullable().default(null).positive(Errors.positive),
     is_sample: Yup.boolean().default(false),
     is_full_sync: Yup.boolean().default(true),
     is_on_demand: Yup.boolean().default(false),
