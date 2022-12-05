bac45a4603a96bc5d9681d7331a1bc3b2ef20024
Ryan Laurie <iethree@gmail.com>
2022-12-05 10:35:14 -0700

    use generic type in moveElement

--- frontend/src/metabase/lib/array.ts
+++ frontend/src/metabase/lib/array.ts
@@ -1,9 +1,5 @@
-export const moveElement = (
-  array: unknown[],
-  oldIndex: number,
-  newIndex: number,
-) => {
+export function moveElement<T>(array: T[], oldIndex: number, newIndex: number) {
   const arrayCopy = [...array];
   arrayCopy.splice(newIndex, 0, arrayCopy.splice(oldIndex, 1)[0]);
   return arrayCopy;
-};
+}
