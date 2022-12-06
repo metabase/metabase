49de42aaa1a3ffe7aa92ad9d87957a9aaf595fd5
Anton Kulyk <kuliks.anton@gmail.com>
2022-12-06 23:27:24 +0000

    Update `QuickFilterDrill`

--- frontend/src/metabase/modes/components/drill/QuickFilterDrill.tsx
+++ frontend/src/metabase/modes/components/drill/QuickFilterDrill.tsx
@@ -1,23 +1,23 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import {
   quickFilterDrill,
   quickFilterDrillQuestion,
 } from "metabase-lib/queries/drills/quick-filter-drill";
+import type { Drill } from "../../types";
 
-export default function QuickFilterDrill({ question, clicked }) {
+const QuickFilterDrill: Drill = ({ question, clicked }) => {
   const drill = quickFilterDrill({ question, clicked });
   if (!drill) {
     return [];
   }
 
-  const { operators } = drill;
-
-  return operators.map(({ name, operator }) => ({
+  return drill.operators.map(({ name, operator }) => ({
     name: operator,
+    title: <span className="h2">{name}</span>,
     section: "filter",
     buttonType: "token-filter",
-    title: <span className="h2">{name}</span>,
     question: () => quickFilterDrillQuestion({ question, clicked, operator }),
   }));
-}
+};
+
+export default QuickFilterDrill;
