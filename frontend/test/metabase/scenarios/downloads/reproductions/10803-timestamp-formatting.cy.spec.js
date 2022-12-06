e1c4883cbbd115329c10065c093fc86de967e681
Cam Saul <github@camsaul.com>
2022-12-06 19:53:03 +0000

    Update e2e test: `hh` is 12-hour time, `HH` is 24-hour time

--- frontend/test/metabase/scenarios/downloads/reproductions/10803-timestamp-formatting.cy.spec.js
+++ frontend/test/metabase/scenarios/downloads/reproductions/10803-timestamp-formatting.cy.spec.js
@@ -18,7 +18,7 @@ describe("issue 10803", () => {
         name: "10803",
         native: {
           query:
-            "SELECT PARSEDATETIME('2020-06-03', 'yyyy-MM-dd') AS \"birth_date\", PARSEDATETIME('2020-06-03 23:41:23', 'yyyy-MM-dd hh:mm:ss') AS \"created_at\"",
+            "SELECT PARSEDATETIME('2020-06-03', 'yyyy-MM-dd') AS \"birth_date\", PARSEDATETIME('2020-06-03 23:41:23', 'yyyy-MM-dd HH:mm:ss') AS \"created_at\"",
         },
       },
       { visitQuestion: true, wrapId: true },
