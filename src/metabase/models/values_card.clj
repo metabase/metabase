dd79c6b0f75ebf4e7b0cb542d7d00f97fe415bbb
Tim Macdonald <tim@tsmacdonald.com>
2022-12-07 16:43:10 +0000

    Read filter values from card properly

--- src/metabase/models/values_card.clj
+++ src/metabase/models/values_card.clj
@@ -1,6 +1,7 @@
 (ns metabase.models.values-card
   (:require
    [metabase.models :refer [Card]]
+   [metabase.query-processor :as qp]
    [metabase.util :as u]
    [toucan.db :as db]
    [toucan.models :as models]))
@@ -14,13 +15,21 @@
                         {:properties (constantly {:timestamped? true})
                          :types      (constantly {:parameterized_object_type :keyword})}))
 
-(defn card-for-dashboard
+(defn- query-for-dashboard
   [{dashboard-id :id :as dashboard} param-key]
-  (db/do-post-select Card (db/query
-                           {:select [:*]
-                            :from [:report_card]
-                            :join [:values_card [:= :report_card.id :values_card.card_id]]
-                            :where [:and
-                                    [:= :parameterized_object_id dashboard-id]
-                                    [:= :parameterized_object_type "dashboard"]
-                                    [:= :parameter_id param-key]]})))
+  (->> (db/query {:select [:dataset_query]
+                  :from [:report_card]
+                  :join [:values_card [:= :report_card.id :values_card.card_id]]
+                  :where [:and
+                          [:= :parameterized_object_id dashboard-id]
+                          [:= :parameterized_object_type "dashboard"]
+                          [:= :parameter_id param-key]]})
+       (db/do-post-select Card)
+       first
+       :dataset_query))
+
+(defn values-for-dashboard
+  [dashboard param-key]
+  {:values
+   (map first
+        (qp/process-query (query-for-dashboard dashboard param-key) {:rff (constantly conj)}))})
