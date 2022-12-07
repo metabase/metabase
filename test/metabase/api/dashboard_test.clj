9f7bff23a5b05cebeb61cc8533299945917f7802
Ngoc Khuat <qn.khuat@gmail.com>
2022-12-07 19:20:49 +0700

    add tests for static list

--- test/metabase/api/dashboard_test.clj
+++ test/metabase/api/dashboard_test.clj
@@ -209,6 +209,69 @@
                      (some-> (db/select-one [Dashboard :collection_id :collection_position] :name dashboard-name)
                              (update :collection_id (partial = (u/the-id collection)))))))))))))
 
+(deftest dashboard-with-static-list-parameters-test
+  (testing "A dashboard that has parameters that has static values"
+    (mt/with-model-cleanup [Dashboard]
+      (let [dashboard (mt/user-http-request :rasta :post 200 "dashboard"
+                                            {:name       "a dashboard"
+                                             :parameters [{:id             "_value_",
+                                                           :name           "value",
+                                                           :type           "category",
+                                                           :source         "custom-list"
+                                                           :source-options {"values" [[1] [2] [3]]}}
+                                                          {:id             "_value-with-label_",
+                                                           :name           "value_with_label",
+                                                           :type           "category",
+                                                           :source         "custom-list"
+                                                           :source-options {"values" [[1 "one"] [2 "two"] [3 "three"]]}}]})]
+
+
+        (is (= [{:id             "_value_",
+                 :name           "value",
+                 :type           "category",
+                 :source         "custom-list"
+                 :source-options {:values [[1] [2] [3]]}}
+                {:id             "_value-with-label_",
+                 :name           "value_with_label",
+                 :type           "category",
+                 :source         "custom-list"
+                 :source-options {:values [[1 "one"] [2 "two"] [3 "three"]]}}]
+               (:parameters dashboard)))
+        (testing "make sure we could update and delete the params"
+          (let [dashboard (mt/user-http-request :rasta :put 200 (str "dashboard/" (:id dashboard))
+                                                {:parameters [{:id             "_value_",
+                                                               :name           "value",
+                                                               :type           "category",
+                                                               :source         "custom-list"
+                                                               :source-options {"values" [[1] [2] [3]]}}]})]
+
+
+            (is (= [{:id             "_value_",
+                     :name           "value",
+                     :type           "category",
+                     :source         "custom-list"
+                     :source-options {:values [[1] [2] [3]]}}]
+                   (:parameters dashboard))))))
+
+      (testing "source-options must be a map and source must be a string"
+        (is (= "value may be nil, or if non-nil, value must be an array. Each parameter must be a map with :id and :type keys"
+               (get-in (mt/user-http-request :rasta :post 400 "dashboard"
+                                            {:name       "a dashboard"
+                                             :parameters [{:id             "_value_",
+                                                           :name           "value",
+                                                           :type           "category",
+                                                           :source         []
+                                                           :source-options {"values" [[1] [2] [3]]}}]})
+                      [:errors :parameters])))
+        (is (= "value may be nil, or if non-nil, value must be an array. Each parameter must be a map with :id and :type keys"
+               (get-in (mt/user-http-request :rasta :post 400 "dashboard"
+                                             {:name       "a dashboard"
+                                              :parameters [{:id             "_value_",
+                                                            :name           "value",
+                                                            :type           "category",
+                                                            :source         "custom-list"
+                                                            :source-options []}]})
+                       [:errors :parameters])))))))
 
 ;;; +----------------------------------------------------------------------------------------------------------------+
 ;;; |                                             GET /api/dashboard/:id                                             |
