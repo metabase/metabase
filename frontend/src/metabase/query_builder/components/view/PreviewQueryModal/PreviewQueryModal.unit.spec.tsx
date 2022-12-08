af3fb80162bb843259d44904ec0992c7eaffd17d
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-12-08 20:48:00 +0200

    Add tests

---
+++ frontend/src/metabase/query_builder/components/view/PreviewQueryModal/PreviewQueryModal.unit.spec.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import xhrMock from "xhr-mock";
+import { render, screen } from "@testing-library/react";
+import { createMockCard } from "metabase-types/api/mocks";
+import Question from "metabase-lib/Question";
+import PreviewQueryModal from "./PreviewQueryModal";
+
+const SQL_QUERY = "SELECT 1";
+const SQL_QUERY_ERROR = "Cannot run the query";
+
+describe("PreviewQueryModal", () => {
+  beforeEach(() => {
+    xhrMock.setup();
+  });
+
+  afterEach(() => {
+    xhrMock.teardown();
+  });
+
+  it("should show a fully parameterized native query", async () => {
+    const question = new Question(createMockCard());
+    mockNativeQuery();
+
+    render(<PreviewQueryModal question={question} />);
+
+    expect(await screen.findByText(SQL_QUERY)).toBeInTheDocument();
+  });
+
+  it("should show a native query error", async () => {
+    const question = new Question(createMockCard());
+    mockNativeQueryError();
+
+    render(<PreviewQueryModal question={question} />);
+
+    expect(await screen.findByText(SQL_QUERY_ERROR)).toBeInTheDocument();
+  });
+});
+
+const mockNativeQuery = () => {
+  xhrMock.post("/api/dataset/native", {
+    status: 200,
+    body: JSON.stringify({
+      query: "SELECT 1",
+    }),
+  });
+};
+
+const mockNativeQueryError = () => {
+  xhrMock.post("/api/dataset/native", {
+    status: 500,
+    body: JSON.stringify({
+      message: SQL_QUERY_ERROR,
+    }),
+  });
+};
