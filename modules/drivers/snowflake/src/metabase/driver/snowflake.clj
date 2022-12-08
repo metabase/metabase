d3a984721b1e48d7e3983dead018eae39f9be67c
Bryan Maass <bryan.maass@gmail.com>
2022-12-08 14:15:38 -0700

    only query for db-metadata once per sync operation + scan each table for its PKs (#26810)
    * memoize snowflake describe-database w/ 5 min ttl
    
    * pass in db-metadata where it is used,
    
    - rather than querying for it in random functions
    
    * update callsite of sync-tables-and-database!
    
    * let sync-metabase-metadata! look up db-metadata
    
    - only when needed
    
    * pass catalog (db), and schema to .getPrimaryKeys
    
    * let describe-table* add in the nil table-name
    
    * cleanup
    
    * use :snowflake instead of driver for inheritance
    
    * concurrent-sync-test should call describe-database once
    
    * cleaning up
    
    * call add-table-pks with 3 args
    
    * call add-table-pks with 3 args
    
    * try escaping entity names to appease Oracle Driver
    
    - Oracle wants the table-name to be escaped
    - escape is usually a no-op
    
    * pass driver to escape entity
    
    * pull get-table-pks into a multimethod,
    
    - handle the discrepancy directly in Oracle driver
    
    * remove inline def
    
    * fix colliding consistent-namespace linting
    
    - metabase.driver.sql-jdbc.common and
      metabase.driver.sql-jdbc.sync.common were
      mapped to sql-jdbc.common
    - metabase.driver.sql-jdbc.sync.common is now
      mapped to sql-jdbc.sync.common
    
    * fix reflection warnings
    
    * apply fix for getting oracle pks
    
    * nix an unused arg

--- modules/drivers/snowflake/src/metabase/driver/snowflake.clj
+++ modules/drivers/snowflake/src/metabase/driver/snowflake.clj
@@ -29,11 +29,11 @@
             [metabase.util.honeysql-extensions :as hx]
             [metabase.util.i18n :refer [trs tru]]
             [ring.util.codec :as codec])
-  (:import [java.sql ResultSet Types]
+  (:import java.io.File
+           java.nio.charset.StandardCharsets
+           [java.sql ResultSet Types]
            [java.time OffsetDateTime ZonedDateTime]
-           java.io.File
-           metabase.util.honeysql_extensions.Identifier
-           java.nio.charset.StandardCharsets))
+           metabase.util.honeysql_extensions.Identifier))
 
 (driver/register! :snowflake, :parent #{:sql-jdbc ::sql-jdbc.legacy/use-legacy-classes-for-read-and-set})
 
@@ -357,10 +357,7 @@
                                               (qp.store/fetch-and-store-database! (u/the-id database))
                                               (sql.qp/->honeysql driver table))]}))
 
-(defmethod driver/describe-database :snowflake
-  [driver database]
-  ;; using the JDBC `.getTables` method seems to be pretty buggy -- it works sometimes but other times randomly
-  ;; returns nothing
+(defmethod driver/describe-database :snowflake [driver database]
   (let [db-name          (db-name database)
         excluded-schemas (set (sql-jdbc.sync/excluded-schemas driver))]
     (qp.store/with-store
@@ -395,7 +392,7 @@
       (->> (assoc (select-keys table [:name :schema])
                   :fields (sql-jdbc.sync/describe-table-fields driver conn table (db-name database)))
            ;; find PKs and mark them
-           (sql-jdbc.sync/add-table-pks (.getMetaData conn))))))
+           (sql-jdbc.sync/add-table-pks driver conn (db-name database))))))
 
 (defmethod driver/describe-table-fks :snowflake
   [driver database table]
