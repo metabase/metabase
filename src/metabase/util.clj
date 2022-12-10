95fe2fca6287882bfc17cf4f83cde14ef8cb08d2
Cam Saul <github@camsaul.com>
2022-12-10 05:15:17 +0000

    Remove more misc unused backend stuff

--- src/metabase/util.clj
+++ src/metabase/util.clj
@@ -241,11 +241,6 @@
     (fn [_ x]
       (str x))))
 
-(defn decolorize
-  "Remove ANSI escape sequences from a String `s`."
-  ^String [s]
-  (some-> s (str/replace #"\[[;\d]*m" "")))
-
 (defn format-color
   "With one arg, converts something to a string and colorizes it. With two args, behaves like `format`, but colorizes
   the output.
@@ -345,19 +340,6 @@
   ^Double [^Integer decimal-place, ^Number number]
   {:pre [(integer? decimal-place) (number? number)]}
   (double (.setScale (bigdec number) decimal-place BigDecimal/ROUND_HALF_UP)))
-
-(defn round-to-precision
-  "Round (presumably floating-point) `number` to a precision of `sig-figures`. Returns a `Double`.
-
-  This rounds by significant figures, not decimal places. See [[round-to-decimals]] for that.
-
-    (round-to-precision 4 1234567.89) -> 123500.0"
-  ^Double [^Integer sig-figures ^Number number]
-  {:pre [(integer? sig-figures) (number? number)]}
-  (-> number
-      bigdec
-      (.round (MathContext. sig-figures RoundingMode/HALF_EVEN))
-      double))
 
 (defn real-number?
   "Is `x` a real number (i.e. not a `NaN` or an `Infinity`)?"
@@ -587,6 +569,7 @@
   ^bytes [^String string]
   (.decode base64-decoder string))
 
+;;; TODO -- this is only used [[metabase.analytics.snowplow-test]] these days
 (defn decode-base64
   "Decodes the Base64 string `input` to a UTF-8 string."
   [input]
