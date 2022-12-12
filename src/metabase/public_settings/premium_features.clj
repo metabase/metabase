afcfd4a3592e88da56bbbd18616a2a8fc8a2f2b8
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2022-12-12 11:41:21 -0500

    Update token check URL, using the current one as the fallback (#27090)
    * update token check URL, using the current one as the fallback
    
    * update comment
    
    * fix tests
    
    * update docstring
    
    * update doc string again

--- src/metabase/public_settings/premium_features.clj
+++ src/metabase/public_settings/premium_features.clj
@@ -21,16 +21,21 @@
   "Schema for a valid premium token. Must be 64 lower-case hex characters."
   #"^[0-9a-f]{64}$")
 
-(def store-url
-  "URL to the MetaStore. Hardcoded by default but for development purposes you can use a local server. Specify the env
-  var `METASTORE_DEV_SERVER_URL`."
+(def token-check-url
+  "Base URL to use for token checks. Hardcoded by default but for development purposes you can use a local server.
+  Specify the env var `METASTORE_DEV_SERVER_URL`."
   (or
-   ;; only enable the changing the store url during dev because we don't want people switching it out in production!
+   ;; only enable the changing the token check url during dev because we don't want people switching it out in production!
    (when config/is-dev?
      (some-> (env :metastore-dev-server-url)
              ;; remove trailing slashes
              (str/replace  #"/$" "")))
-   "https://store.metabase.com"))
+   "https://token-check.metabase.com"))
+
+(def store-url
+  "Store URL, used as a fallback for token checks and for fetching the list of cloud gateway IPs."
+  "https://store.metabase.com")
+
 
 ;;; +----------------------------------------------------------------------------------------------------------------+
 ;;; |                                                TOKEN VALIDATION                                                |
@@ -43,9 +48,9 @@
   ;; so we can't import the User model here.
   (db/count 'User :is_active true))
 
-(defn- token-status-url [token]
+(defn- token-status-url [token base-url]
   (when (seq token)
-    (format "%s/api/%s/v2/status" store-url token)))
+    (format "%s/api/%s/v2/status" base-url token)))
 
 (def ^:private ^:const fetch-token-status-timeout-ms 10000) ; 10 seconds
 
@@ -59,6 +64,14 @@
    ;; don't explode in the future if we add more to the response! lol
    schema/Any                           schema/Any})
 
+(defn- fetch-token-and-parse-body
+  [token base-url]
+  (some-> (token-status-url token base-url)
+          (http/get {:query-params {:users     (active-user-count)
+                                    :site-uuid (setting/get :site-uuid-for-premium-features-token-checks)}})
+          :body
+          (json/parse-string keyword)))
+
 (schema/defn ^:private fetch-token-status* :- TokenStatus
   "Fetch info about the validity of `token` from the MetaStore."
   [token :- ValidToken]
@@ -69,22 +82,22 @@
                  (str (subs token 0 4) "..." (subs token 60 64))))
   (deref
    (future
-     (try (some-> (token-status-url token)
-                  (http/get {:query-params {:users     (active-user-count)
-                                            :site-uuid (setting/get :site-uuid-for-premium-features-token-checks)}})
-                  :body
-                  (json/parse-string keyword))
-          ;; if there was an error fetching the token, log it and return a generic message about the
-          ;; token being invalid. This message will get displayed in the Settings page in the admin panel so
-          ;; we do not want something complicated
-          (catch Exception e
-            (log/error e (trs "Error fetching token status:"))
-            (let [body (u/ignore-exceptions (some-> (ex-data e) :body (json/parse-string keyword)))]
-              (or
-               body
-               {:valid         false
-                :status        (tru "Unable to validate token")
-                :error-details (.getMessage e)})))))
+     (try (fetch-token-and-parse-body token token-check-url)
+       (catch Exception e1
+         (log/error e1 (trs "Error fetching token status from {0}:" token-check-url))
+         ;; Try the fallback URL, which was the default URL prior to 45.2
+         (try (fetch-token-and-parse-body token store-url)
+           ;; if there was an error fetching the token from both the normal and fallback URLs, log the first error and
+           ;; return a generic message about the token being invalid. This message will get displayed in the Settings
+           ;; page in the admin panel so we do not want something complicated
+           (catch Exception e2
+             (log/error e2 (trs "Error fetching token status from {0}:" store-url))
+             (let [body (u/ignore-exceptions (some-> (ex-data e1) :body (json/parse-string keyword)))]
+               (or
+                body
+                {:valid         false
+                 :status        (tru "Unable to validate token")
+                 :error-details (.getMessage e1)})))))))
    fetch-token-status-timeout-ms
    {:valid         false
     :status        (tru "Unable to validate token")
