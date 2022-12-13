826432be9bce30d1ceedd831dfd622087195015d
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-12-13 20:54:50 +0200

    Add parameter

--- frontend/src/metabase/parameters/components/ParameterLinkedFilters/ParameterLinkedFilters.styled.tsx
+++ frontend/src/metabase/parameters/components/ParameterLinkedFilters/ParameterLinkedFilters.styled.tsx
@@ -18,3 +18,22 @@ export const SectionMessageLink = styled.span`
   color: ${color("brand")};
   cursor: pointer;
 `;
+
+export const ParameterRoot = styled.div`
+  margin-bottom: 1rem;
+  border-radius: 0.5rem;
+  background-color: ${color("bg-light")};
+`;
+
+export const ParameterBody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+`;
+
+export const ParameterName = styled.div`
+  cursor: pointer;
+  border-bottom: 1px dashed ${color("border")};
+  font-weight: bold;
+`;
