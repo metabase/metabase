e3bdada4320c134b3db86be3f81b4bccb8374502
Cal Herries <39073188+calherries@users.noreply.github.com>
2022-12-14 14:06:59 +0100

    Fix datetime-diff helper text (#27224)

--- frontend/src/metabase-lib/expressions/helper-text-strings.ts
+++ frontend/src/metabase-lib/expressions/helper-text-strings.ts
@@ -384,7 +384,7 @@ const helperTextStrings: HelpText[] = [
   {
     name: "datetime-diff",
     structure:
-      "datetime-diff(" +
+      "datetimeDiff(" +
       t`datetime1` +
       ", " +
       t`datetime2` +
@@ -393,12 +393,12 @@ const helperTextStrings: HelpText[] = [
       ")",
     description: t`Get the difference between two datetime values (datetime2 minus datetime1) using the specified unit of time.`,
     example:
-      "datetime-diff([" +
+      "datetimeDiff([" +
       t`created_at` +
       "], [" +
       t`shipped_at` +
       "], " +
-      t`month` +
+      t`"month"` +
       ")",
     args: [
       {
