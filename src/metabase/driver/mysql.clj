94c747340088f4a31e4f83e99b7b55ac28c6d661
Callum Herries <hi@callumherries.com>
2022-12-14 18:36:02 +0100

    Replace str/upper-case with u/upper-case-en

--- src/metabase/driver/mysql.clj
+++ src/metabase/driver/mysql.clj
@@ -279,7 +279,7 @@
 
         "boolean" json-extract+jsonpath
 
-        (hsql/call :convert json-extract+jsonpath (hsql/raw (str/upper-case field-type)))))))
+        (hsql/call :convert json-extract+jsonpath (hsql/raw (u/upper-case-en field-type)))))))
 
 (defmethod sql.qp/->honeysql [:mysql :field]
   [driver [_ id-or-name opts :as clause]]
@@ -393,7 +393,7 @@
         y (sql.qp/->honeysql driver y)
         disallowed-types (keep
                           (fn [v]
-                            (when-let [db-type (some-> v hx/type-info hx/type-info->db-type str/upper-case keyword)]
+                            (when-let [db-type (some-> v hx/type-info hx/type-info->db-type u/upper-case-en keyword)]
                               (let [base-type (sql-jdbc.sync/database-type->base-type driver db-type)]
                                 (when-not (some #(isa? base-type %) [:type/Date :type/DateTime])
                                   (name db-type)))))
