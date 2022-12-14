9da984597fa02393173386efa8b8bab0fad56124
Anderson Feitosa <andersonmfjr@gmail.com>
2022-12-14 10:04:02 -0300

    Add accelerated by ddf on header

--- frontend/src/metabase/nav/components/AppBar/AppBarLogo.tsx
+++ frontend/src/metabase/nav/components/AppBar/AppBarLogo.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import LogoIcon from "metabase/components/LogoIcon";
 import AppBarToggle from "./AppBarToggle";
-import { LogoLink, LogoRoot, ToggleContainer } from "./AppBarLogo.styled";
+import {
+  DadosferaLogo,
+  LogoLink,
+  LogoRoot,
+  ToggleContainer,
+} from "./AppBarLogo.styled";
 
 export interface AppBarLogoProps {
   isNavBarOpen?: boolean;
@@ -20,6 +25,11 @@ const AppBarLogo = ({
     <LogoRoot>
       <LogoLink to="/" onClick={onLogoClick} data-metabase-event="Navbar;Logo">
         <LogoIcon height={32} />
+
+        <DadosferaLogo>
+          <span>Accelerated By</span>
+          <img src="app/img/ddf-d.svg"></img>
+        </DadosferaLogo>
       </LogoLink>
       {isToggleVisible && (
         <ToggleContainer>
