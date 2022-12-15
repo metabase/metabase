164250687ba74a4e0483d5917c60367f4353305e
Anton Kulyk <kuliks.anton@gmail.com>
2022-12-15 13:06:41 +0000

    Add `VirtualizedSelectList` component

--- frontend/src/metabase/components/VirtualizedList.tsx
+++ frontend/src/metabase/components/VirtualizedList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { List, WindowScroller, AutoSizer } from "react-virtualized";
 
-interface VirtualizedListProps<Item = unknown>
+export interface VirtualizedListProps<Item = unknown>
   extends React.HTMLProps<HTMLUListElement> {
   items: Item[];
   rowHeight: number;
