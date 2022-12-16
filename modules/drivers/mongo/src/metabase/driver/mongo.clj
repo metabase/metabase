c915e8b4994d8c2e7a675023ed36942e5ea1a9dc
Tamás Benkő <tamas@metabase.com>
2022-12-16 12:31:21 +0300

    Remove :truncation-start workaround

--- modules/drivers/mongo/src/metabase/driver/mongo.clj
+++ modules/drivers/mongo/src/metabase/driver/mongo.clj
@@ -315,10 +315,7 @@
 
 (defmethod driver/table-rows-sample :mongo
   [_driver table fields rff opts]
-  (let [mongo-opts {;; setting :truncation-start is needed because of a bug
-                    ;; in our mongo drivers handling of :substring (#27270)
-                    :truncation-start 0
-                    :limit metadata-queries/nested-field-sample-limit
+  (let [mongo-opts {:limit metadata-queries/nested-field-sample-limit
                     :order-by [[:desc [:field (get-id-field-id table) nil]]]}]
     (metadata-queries/table-rows-sample table fields rff (merge mongo-opts opts))))
 
