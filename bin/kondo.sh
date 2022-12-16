7cc572293db9ce1a817d2a862fe67af501e44f67
John Cromartie <john@metabase.com>
2022-12-16 16:20:30 +0000

    Updates bin/kondo.sh to allow non-Docker command

--- bin/kondo.sh
+++ bin/kondo.sh
@@ -2,9 +2,13 @@
 
 # Convenience/CI script for running clj-kondo against all source and test directories using Docker
 
+# To use a local clj-kondo installation instead of Docker, set KONDO_CMD=clj-kondo
+
 set -ex
 
-docker run --rm --volume $PWD:/work --workdir /work cljkondo/clj-kondo:2022.08.03 \
-    clj-kondo --config .clj-kondo/config.edn --config-dir .clj-kondo \
-    --parallel --lint src test \
+kondoCmd="${KONDO_CMD:-docker run --rm --volume $PWD:/work --workdir /work cljkondo/clj-kondo:2022.08.03 clj-kondo}"
+
+$kondoCmd --copy-configs --dependencies --lint "$(clojure -A:dev -Spath)" --skip-lint --parallel
+
+$kondoCmd --config .clj-kondo/config.edn --config-dir .clj-kondo --parallel --lint src test \
     $(find shared modules/drivers enterprise/backend -maxdepth 2 -type d \( -name src -o -name test \))
