90f1e8c52e663d57cb0f4c8b81ea64ff92381ca6
Anton Kulyk <kuliks.anton@gmail.com>
2022-12-16 14:15:11 +0000

    Cherry-pick `moveElement` util move

--- frontend/src/metabase/visualizations/components/settings/ChartSettingFieldsPicker.jsx
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingFieldsPicker.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { t } from "ttag";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-import { moveElement } from "metabase/visualizations/lib/utils";
+import { moveElement } from "metabase/lib/array";
 
 import ChartSettingFieldPicker from "./ChartSettingFieldPicker";
 import { AddAnotherContainer } from "./ChartSettingFieldsPicker.styled";
