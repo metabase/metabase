615c5a4e4368b5a91b631951b31dc6bf51051635
Cal Herries <39073188+calherries@users.noreply.github.com>
2022-12-16 09:08:25 +0100

    Refactor datetime-diff implementations: sqlserver, mysql, bigquery, redshift, snowflake, vertica, postgres (#27262)
    * Refactor bigquery datetime-diff to use multimethod
    
    * Refactor snowflake datetime-diff to use multimethod
    
    * Refactor SQL server datetime-diff to use multimethod
    
    * Refactor mysql datetime-diff to use multimethod
    
    * Refactor redshift datetime-diff to use multimethod
    
    * Simplify vertica datetime-diff
    
    * Tidy postgres datetime-diff
    
    * ; -> ;; in snowflake comment
    
    * Add to bigquery comment

--- modules/drivers/vertica/src/metabase/driver/vertica.clj
+++ modules/drivers/vertica/src/metabase/driver/vertica.clj
@@ -138,88 +138,52 @@
        (reduce (partial hsql/call :concat))))
 
 (defmethod sql.qp/datetime-diff [:vertica :year]
-  [_driver _unit x y]
-  (let [positive-diff (fn [a b] ; precondition: a <= b
-                        (hx/-
-                         (datediff :year a b)
-                         (hx/cast
-                          :integer
-                          (hsql/call
-                           :or
-                           (hsql/call :> (extract :month a) (extract :month b))
-                           (hsql/call
-                            :and
-                            (hsql/call := (extract :month a) (extract :month b))
-                            (hsql/call :> (extract :day a) (extract :day b)))))))]
-    (hsql/call :case
-               (hsql/call :<= (cast-timestamp x) (cast-timestamp y))
-               (positive-diff x y)
-               :else
-               (hx/* -1 (positive-diff y x)))))
+  [driver _unit x y]
+  (let [months (sql.qp/datetime-diff driver :month x y)]
+    (hx/->integer (hsql/call :trunc (hx// months 12)))))
 
 (defmethod sql.qp/datetime-diff [:vertica :quarter]
-  [_driver _unit x y]
-  (let [positive-diff
-        (fn [a b]
-          (hx/cast
-           :integer
-           (hx/floor (hx// (hx/- (datediff :month a b)
-                                 (hx/cast :integer (hsql/call :> (extract :day a) (extract :day b))))
-                           3))))]
-    (hsql/call :case
-               (hsql/call :<= (cast-timestamp x) (cast-timestamp y))
-               (positive-diff x y)
-               :else
-               (hx/* -1 (positive-diff y x)))))
+  [driver _unit x y]
+  (let [months (sql.qp/datetime-diff driver :month x y)]
+    (hx/->integer (hsql/call :trunc (hx// months 3)))))
 
 (defmethod sql.qp/datetime-diff [:vertica :month]
   [_driver _unit x y]
-  (let [positive-diff (fn [a b]
-                        (hx/-
-                         (datediff :month a b)
-                         (hx/cast
-                          :integer
-                          (hsql/call :> (extract :day a) (extract :day b)))))]
-    (hsql/call :case
-               (hsql/call :<= (cast-timestamp x) (cast-timestamp y))
-               (positive-diff x y)
-               :else
-               (hx/* -1 (positive-diff y x)))))
+  (hx/+ (datediff :month x y)
+        ;; datediff counts month boundaries not whole months, so we need to adjust
+        ;; if x<y but x>y in the month calendar then subtract one month
+        ;; if x>y but x<y in the month calendar then add one month
+        (hsql/call
+         :case
+         (hsql/call :and
+                    (hsql/call :< (cast-timestamp x) (cast-timestamp y))
+                    (hsql/call :> (extract :day x) (extract :day y))) -1
+         (hsql/call :and
+                    (hsql/call :> (cast-timestamp x) (cast-timestamp y))
+                    (hsql/call :< (extract :day x) (extract :day y))) 1
+         :else 0)))
 
 (defmethod sql.qp/datetime-diff [:vertica :week]
   [_driver _unit x y]
-  (let [positive-diff
-        (fn [a b]
-          (hx/cast :integer (hx/floor (hx// (datediff :day a b) 7))))]
-    (hsql/call :case
-               (hsql/call :<= (cast-timestamp x) (cast-timestamp y))
-               (positive-diff x y)
-               :else
-               (hx/* -1 (positive-diff y x)))))
+  (hx/->integer (hsql/call :trunc (hx// (datediff :day x y) 7))))
 
 (defmethod sql.qp/datetime-diff [:vertica :day]
   [_driver _unit x y]
   (datediff :day x y))
 
-(defn- sub-day-diff-helper
-  [unit x y]
-  (let [positive-diff
-        (fn [a b]
-          (hx/cast
-           :integer
-           (hx/floor
-            (cond-> (hsql/call :- (extract :epoch b) (extract :epoch a))
-              (not= unit :second)
-              (hx// (case unit :hour 3600 :minute 60))))))]
-    (hsql/call :case
-               (hsql/call :<= (cast-timestamp x) (cast-timestamp y))
-               (positive-diff x y)
-               :else
-               (hx/* -1 (positive-diff y x)))))
+(defmethod sql.qp/datetime-diff [:vertica :hour]
+  [_driver _unit x y]
+  (let [seconds (hx/- (extract :epoch y) (extract :epoch x))]
+    (hx/->integer (hsql/call :trunc (hx// seconds 3600)))))
 
-(defmethod sql.qp/datetime-diff [:vertica :hour] [_driver _unit x y] (sub-day-diff-helper :hour x y))
-(defmethod sql.qp/datetime-diff [:vertica :minute] [_driver _unit x y] (sub-day-diff-helper :minute x y))
-(defmethod sql.qp/datetime-diff [:vertica :second] [_driver _unit x y] (sub-day-diff-helper :second x y))
+(defmethod sql.qp/datetime-diff [:vertica :minute]
+  [_driver _unit x y]
+  (let [seconds (hx/- (extract :epoch y) (extract :epoch x))]
+    (hx/->integer (hsql/call :trunc (hx// seconds 60)))))
+
+(defmethod sql.qp/datetime-diff [:vertica :second]
+  [_driver _unit x y]
+  (hx/->integer (hsql/call :trunc (hx/- (extract :epoch y) (extract :epoch x)))))
 
 (defmethod sql.qp/->honeysql [:vertica :regex-match-first]
   [driver [_ arg pattern]]
