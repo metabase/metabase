7506e953d7ae5e3044d7297926efca7980ab6e55
Bryan Maass <bryan.maass@gmail.com>
2022-12-16 13:49:33 -0700

    adds mu/defn, which behaves like s/defn but for malli schemas
    - includes link to malli.io with your schema and type.🔗
    - making room for some namespaces

---
+++ test/metabase/util/malli_test.clj
@@ -0,0 +1,27 @@
+(ns metabase.util.malli-test
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is testing]]
+   [metabase.util.malli :as um]
+   [clojure.walk :as walk]))
+
+(deftest mu-defn-test
+  (testing "invalid input"
+    (um/defn bar [x :- [:map [:x int?] [:y int?]]] (str x))
+    (is (= [{:x ["missing required key"]
+             :y ["missing required key"]}]
+           (:humanized
+            (try (bar {})
+                 (catch Exception e (ex-data e)))))
+        "when we pass bar an invalid shape um/defn throws")
+    (ns-unmap *ns* 'bar))
+
+  (testing "invalid output"
+    (um/defn baz :- [:map [:x int?] [:y int?]] [] {:x "3"})
+    (is (= {:x ["should be an int"]
+            :y ["missing required key"]}
+           (:humanized
+            (try (baz)
+                 (catch Exception e (ex-data e)))))
+        "when baz returns an invalid form um/defn throws")
+    (ns-unmap *ns* 'baz)))
