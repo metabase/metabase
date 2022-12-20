8e9f93d18ce1ab8e31d72d81998cb1f5552976a1
Case Nelson <case@metabase.com>
2022-12-19 17:39:02 -0700

    Fix reduction

--- modules/drivers/mongo/src/metabase/driver/mongo/query_processor.clj
+++ modules/drivers/mongo/src/metabase/driver/mongo/query_processor.clj
@@ -462,9 +462,7 @@
   ;; division works outside in (/ 1 2 3) => (/ (/ 1 2) 3)
   (let [division (reduce
                    (fn [accum head]
-                     (if accum
-                       {"$divide" [accum head]}
-                       head))
+                     {"$divide" [accum head]})
                    (map ->rvalue args))
         literal-zero? (some #(and (number? %) (zero? %)) divisors)
         non-literal-nil-checks (mapv (fn [divisor] {"$eq" [(->rvalue divisor) 0]}) (remove number? divisors))]
