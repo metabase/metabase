50ca279bdecdfe21042fb42df787fee1332e7dfb
Callum Herries <hi@callumherries.com>
2022-12-21 14:34:41 +0100

    Refactor FE unit tests

--- enterprise/frontend/src/metabase-enterprise/auth/components/SessionTimeoutSetting/SessionTimeoutSetting.unit.spec.tsx
+++ enterprise/frontend/src/metabase-enterprise/auth/components/SessionTimeoutSetting/SessionTimeoutSetting.unit.spec.tsx
@@ -9,7 +9,10 @@ describe("SessionTimeoutSetting", () => {
 
   const TEST_CASES = [
     { value: { amount: 1, unit: "minutes" }, expected: null },
-    { value: { amount: 1, unit: "hours" }, expected: null },
+    {
+      value: { amount: 1, unit: "hours" },
+      expected: null,
+    },
     {
       value: { amount: 0, unit: "minutes" },
       expected: "Timeout must be greater than 0",
@@ -30,10 +33,13 @@ describe("SessionTimeoutSetting", () => {
       value: { amount: 24 * 365.25 * 100, unit: "hours" },
       expected: "Timeout must be less than 100 years",
     },
-    { value: { amount: 24 * 365.25 * 100 - 1, unit: "hours" }, expected: null },
+    {
+      value: { amount: 24 * 365.25 * 100 - 1, unit: "hours" },
+      expected: null,
+    },
   ];
-  it(`validates correctly`, () => {
-    TEST_CASES.map(({ value, expected }) => {
+  TEST_CASES.map(({ value, expected }) => {
+    it(`validates ${value.amount} ${value.unit} correctly`, () => {
       const setting = { value: value, key: "...", default: "..." };
       render(<SessionTimeoutSetting setting={setting} onChange={jest.fn()} />);
       const input = screen.getByTestId("session-timeout-input");
