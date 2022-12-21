dd0775488005f3e660aa06a740b91cf79c30f74d
Anton Kulyk <kuliks.anton@gmail.com>
2022-12-21 12:49:22 +0000

    That wasn't supposed to be here

--- frontend/src/metabase/actions/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Database as IDatabase } from "metabase-types/api";
-
-import type Database from "metabase-lib/metadata/Database";
-
-export const isDatabaseWritebackEnabled = (database?: IDatabase | null) =>
-  !!database?.settings?.["database-enable-actions"];
-
-export const isWritebackSupported = (database?: Database | null) =>
-  !!database?.hasFeature("actions");
