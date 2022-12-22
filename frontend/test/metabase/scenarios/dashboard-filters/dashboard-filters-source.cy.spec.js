e69c2627a6d519bc6575531c4df054dade705337
Alexander Polyankin <alexander.polyankin@metabase.com>
2022-12-22 21:24:21 +0200

    Add e2e test

--- frontend/test/metabase/scenarios/dashboard-filters/dashboard-filters-source.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard-filters/dashboard-filters-source.cy.spec.js
@@ -11,7 +11,7 @@ import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
 
 const { PRODUCTS_ID } = SAMPLE_DATABASE;
 
-const countQuestionDetails = {
+const sampleQuestionDetails = {
   display: "scalar",
   query: {
     "source-table": PRODUCTS_ID,
@@ -19,7 +19,7 @@ const countQuestionDetails = {
   },
 };
 
-const categoriesQuestionDetails = {
+const nativeQuestionDetails = {
   name: "Product categories",
   native: {
     query: "select distinct CATEGORY from PRODUCTS order by CATEGORY limit 2",
@@ -34,79 +34,79 @@ describe("scenarios > dashboard > filters", () => {
   });
 
   it("should be able to use a native card source", () => {
-    cy.createNativeQuestion(categoriesQuestionDetails);
+    cy.createNativeQuestion(nativeQuestionDetails);
     cy.createQuestionAndDashboard({
-      questionDetails: countQuestionDetails,
+      questionDetails: sampleQuestionDetails,
     }).then(({ body: { dashboard_id } }) => {
       visitDashboard(dashboard_id);
     });
 
     editDashboard();
     setFilter("Text or Category", "Dropdown");
-    cy.findByText("Values from a model or question").click();
-    modal().within(() => {
-      cy.findByText("Saved Questions").click();
-      cy.findByText("Product categories").click();
-      cy.button("Select column").click();
-    });
-    modal().within(() => {
-      cy.findByText("Pick a column").click();
-    });
-    popover().within(() => {
-      cy.findByText("CATEGORY").click();
-    });
-    modal().within(() => {
-      cy.button("Done").click();
-    });
-
-    cy.findByText("Select…").click();
-    popover().within(() => cy.findByText("Category").click());
+    setupNativeCard();
+    mapFilterToCard();
     saveDashboard();
-
-    cy.findByText("Text").click();
-    popover().within(() => {
-      cy.findByText("Doohickey").should("be.visible");
-      cy.findByText("Gadget").should("be.visible");
-      cy.findByText("Gizmo").should("not.exist");
-
-      cy.findByPlaceholderText("Search the list").type("Gadget");
-      cy.findByText("Doohickey").should("not.exist");
-      cy.findByText("Gadget").click();
-      cy.button("Add filter").click();
-    });
-    cy.wait("@getCardQuery");
+    filterDashboard();
   });
 
   it("should be able to use a static list source", () => {
     cy.createQuestionAndDashboard({
-      questionDetails: countQuestionDetails,
+      questionDetails: sampleQuestionDetails,
     }).then(({ body: { dashboard_id } }) => {
       visitDashboard(dashboard_id);
     });
 
     editDashboard();
     setFilter("Text or Category", "Dropdown");
-    cy.findByText("Custom list").click();
-    modal().within(() => {
-      cy.findByPlaceholderText(/banana/).type("Apple\nGoogle");
-      cy.button("Done").click();
-    });
-    cy.findByText("Select…").click();
-    popover().within(() => {
-      cy.findByText("Source").click();
-    });
+    setupCustomList();
+    mapFilterToCard();
     saveDashboard();
-
-    cy.findByText("Text").click();
-    popover().within(() => {
-      cy.findByText("Apple").should("be.visible");
-      cy.findByText("Google").should("be.visible");
-
-      cy.findByPlaceholderText("Search the list").type("Goo");
-      cy.findByText("Apple").should("not.exist");
-      cy.findByText("Google").click();
-      cy.button("Add filter").click();
-    });
-    cy.wait("@getCardQuery");
+    filterDashboard();
   });
 });
+
+const setupNativeCard = () => {
+  cy.findByText("Values from a model or question").click();
+  modal().within(() => {
+    cy.findByText("Saved Questions").click();
+    cy.findByText("Product categories").click();
+    cy.button("Select column").click();
+  });
+  modal().within(() => {
+    cy.findByText("Pick a column").click();
+  });
+  popover().within(() => {
+    cy.findByText("CATEGORY").click();
+  });
+  modal().within(() => {
+    cy.button("Done").click();
+  });
+};
+
+const setupCustomList = () => {
+  cy.findByText("Custom list").click();
+  modal().within(() => {
+    cy.findByPlaceholderText(/banana/).type("Doohickey\nGadget");
+    cy.button("Done").click();
+  });
+};
+
+const mapFilterToCard = () => {
+  cy.findByText("Select…").click();
+  popover().within(() => cy.findByText("Category").click());
+};
+
+const filterDashboard = () => {
+  cy.findByText("Text").click();
+  popover().within(() => {
+    cy.findByText("Doohickey").should("be.visible");
+    cy.findByText("Gadget").should("be.visible");
+    cy.findByText("Gizmo").should("not.exist");
+
+    cy.findByPlaceholderText("Search the list").type("Gadget");
+    cy.findByText("Doohickey").should("not.exist");
+    cy.findByText("Gadget").click();
+    cy.button("Add filter").click();
+  });
+  cy.wait("@getCardQuery");
+};
