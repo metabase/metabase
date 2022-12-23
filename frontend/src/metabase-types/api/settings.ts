957b3938950b78e3cb54187f8b7918da718b99e3
Anton Kulyk <kuliks.anton@gmail.com>
2022-12-23 15:47:42 +0000

    Add missing fields to `Settings` type

--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -100,7 +100,19 @@ export interface FontFile {
 export type FontFormat = "woff" | "woff2" | "truetype";
 
 export interface Version {
-  tag: string;
+  tag?: string;
+}
+
+export interface VersionInfoRecord {
+  version?: string; // tag
+  released?: string; // year-month-day
+  patch?: boolean;
+  highlights?: string[];
+}
+
+export interface VersionInfo {
+  latest?: VersionInfoRecord;
+  older?: VersionInfoRecord[];
 }
 
 export type LocaleData = [string, string];
@@ -128,42 +140,65 @@ export interface TokenFeatures {
   whitelabel: boolean;
 }
 
+export type PasswordComplexity = {
+  total?: number;
+  digit?: number;
+};
+
 export interface SettingDefinition {
   key: string;
   env_name: string;
   is_env_setting: boolean;
+  value?: unknown;
 }
 
 export interface Settings {
+  "admin-email": string;
+  "anon-tracking-enabled": boolean;
   "application-font": string;
   "application-font-files": FontFile[] | null;
+  "application-name": string;
   "available-fonts": string[];
   "available-locales": LocaleData[] | null;
+  "cloud-gateway-ips": string[] | null;
   "custom-formatting": FormattingSettings;
   "deprecation-notice-version"?: string;
   "email-configured?": boolean;
   "embedding-secret-key"?: string;
   "enable-embedding": boolean;
+  "enable-enhancements?": boolean;
   "enable-nested-queries": boolean;
   "enable-query-caching"?: boolean;
+  "enable-password-login": boolean;
   "enable-public-sharing": boolean;
   "enable-xrays": boolean;
+  engines: Record<string, Engine>;
   "experimental-enable-actions": boolean;
+  "ga-enabled": boolean;
   "google-auth-auto-create-accounts-domain": string | null;
   "google-auth-client-id": string | null;
   "google-auth-configured": boolean;
   "google-auth-enabled": boolean;
+  "has-user-setup": boolean;
+  "hide-embed-branding?": boolean;
   "is-hosted?": boolean;
   "jwt-enabled"?: boolean;
   "jwt-configured"?: boolean;
   "ldap-configured?": boolean;
   "ldap-enabled": boolean;
   "loading-message": LoadingMessage;
+  "other-sso-enabled?": boolean | null;
+  "password-complexity": PasswordComplexity;
   "persisted-models-enabled": boolean;
+  "premium-embedding-token": string | null;
   "report-timezone-short": string;
   "saml-configured"?: boolean;
   "saml-enabled"?: boolean;
+  "search-typeahead-enabled": boolean;
+  "setup-token": string | null;
   "session-cookies": boolean | null;
+  "snowplow-enabled": boolean;
+  "snowplow-url": string;
   "show-database-syncing-modal": boolean;
   "show-homepage-data": boolean;
   "show-homepage-pin-message": boolean;
@@ -171,15 +206,20 @@ export interface Settings {
   "show-lighthouse-illustration": boolean;
   "show-metabot": boolean;
   "site-locale": string;
+  "site-uuid": string;
   "site-url": string;
   "slack-app-token": string | null;
   "slack-files-channel": string | null;
   "slack-token": string | null;
   "slack-token-valid?": boolean;
+  "subscription-allowed-domains": string | null;
   "token-features": TokenFeatures;
   "token-status": TokenStatus | null;
-  engines: Record<string, Engine>;
+  "user-locale": string | null;
   version: Version;
+  "version-info": VersionInfo | null;
+  "version-info-last-checked": string | null;
 }
 
 export type SettingKey = keyof Settings;
+0;
