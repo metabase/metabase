887f19cdeb7d2b63b6a401857de1ab766c54d5c4
Anton Kulyk <kuliks.anton@gmail.com>
2022-12-24 01:45:35 +0000

    Use `mockSettings` in database edit app tests

--- frontend/src/metabase/admin/databases/containers/DatabaseEditApp.unit.spec.js
+++ frontend/src/metabase/admin/databases/containers/DatabaseEditApp.unit.spec.js
@@ -1,12 +1,15 @@
 import React from "react";
+
 import {
   renderWithProviders,
   screen,
   waitForElementToBeRemoved,
 } from "__support__/ui";
-import admin from "metabase/admin/admin";
-import MetabaseSettings from "metabase/lib/settings";
 import { setupEnterpriseTest } from "__support__/enterprise";
+import { mockSettings } from "__support__/settings";
+
+import admin from "metabase/admin/admin";
+
 import DatabaseEditApp from "./DatabaseEditApp";
 
 const ENGINES_MOCK = {
@@ -34,46 +37,16 @@ jest.mock(
   () => ComponentMock,
 );
 
-function mockSettings({ cachingEnabled = false }) {
-  const original = MetabaseSettings.get.bind(MetabaseSettings);
-  const spy = jest.spyOn(MetabaseSettings, "get");
-  spy.mockImplementation(key => {
-    if (key === "engines") {
-      return ENGINES_MOCK;
-    }
-    if (key === "enable-query-caching") {
-      return cachingEnabled;
-    }
-    if (key === "site-url") {
-      return "http://localhost:3333";
-    }
-    if (key === "application-name") {
-      return "Metabase Test";
-    }
-    if (key === "is-hosted?") {
-      return false;
-    }
-    if (key === "cloud-gateway-ips") {
-      return [];
-    }
-    return original(key);
-  });
-}
-
 async function setup({ cachingEnabled = false } = {}) {
-  mockSettings({ cachingEnabled });
-
-  const settingsReducer = () => ({
-    values: {
-      engines: ENGINES_MOCK,
-      "enable-query-caching": cachingEnabled,
-      "persisted-models-enabled": false,
-    },
+  const settings = mockSettings({
+    engines: ENGINES_MOCK,
+    "enable-query-caching": cachingEnabled,
+    "persisted-models-enabled": false,
   });
 
   renderWithProviders(<DatabaseEditApp />, {
     withRouter: true,
-    reducers: { admin, settings: settingsReducer },
+    reducers: { admin, settings: () => settings.state },
   });
 
   await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
