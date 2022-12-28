1f7a5ba033378dd24d8e1decf2d9097e42699eb6
Ngoc Khuat <qn.khuat@gmail.com>
2022-12-28 08:37:08 +0700

    revert removing ingest-path for card

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -427,3 +427,5 @@
       (when (seq snippets)
         (set (for [snippet-id snippets]
                ["NativeQuerySnippet" snippet-id]))))))
+
+(serdes.base/register-ingestion-path! "Card" (serdes.base/ingestion-matcher-collected "collections" "Card"))
