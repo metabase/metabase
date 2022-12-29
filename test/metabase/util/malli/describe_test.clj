cd061de0c90a444761bd5c0b7320212ac0584c7a
Bryan Maass <bryan.maass@gmail.com>
2022-12-29 13:23:13 -0700

    update alias in test

--- test/metabase/util/malli/describe_test.clj
+++ test/metabase/util/malli/describe_test.clj
@@ -1,48 +1,48 @@
 (ns metabase.util.malli.describe-test
   (:require [clojure.test :refer [deftest is]]
-            [metabase.util.malli.describe :as mallid]))
+            [metabase.util.malli.describe :as umd]))
 
 (deftest descriptor-test
 
   (is (= "a map where {:x -> <integer>}"
-         (mallid/describe [:map [:x int?]])))
+         (umd/describe [:map [:x int?]])))
 
   (is (= "a map where {:x (optional) -> <integer>, :y -> <boolean>}"
-         (mallid/describe [:map [:x {:optional true} int?] [:y :boolean]])))
+         (umd/describe [:map [:x {:optional true} int?] [:y :boolean]])))
 
   (is (= "a map where {:x -> <integer>} with no other keys"
-         (mallid/describe [:map {:closed true} [:x int?]])))
+         (umd/describe [:map {:closed true} [:x int?]])))
 
   (is (= "a map where {:x (optional) -> <integer>, :y -> <boolean>} with no other keys"
-         (mallid/describe [:map {:closed true} [:x {:optional true} int?] [:y :boolean]])))
+         (umd/describe [:map {:closed true} [:x {:optional true} int?] [:y :boolean]])))
 
   (is (= "a function that takes input: [integer] and returns integer"
-         (mallid/describe [:=> [:cat int?] int?])))
+         (umd/describe [:=> [:cat int?] int?])))
 
   (is (= "a map where {:j-code -> <keyword, and has length 4>}"
-         (mallid/describe [:map [:j-code [:and
+         (umd/describe [:map [:j-code [:and
                                           :keyword
                                           [:fn {:description "has length 4"} #(= 4 (count (name %)))]]]])))
 
-  (is (= (mallid/describe [:map-of {:title "dict"} :int :string])
+  (is (= (umd/describe [:map-of {:title "dict"} :int :string])
          "a map (titled: ‘dict’) from <integer> to <string>"))
 
-  (is (= (mallid/describe [:vector [:sequential [:set :int]]])
+  (is (= (umd/describe [:vector [:sequential [:set :int]]])
          "vector of sequence of set of integer"))
 
   (is (= "one of <:dog = a map where {:x -> <integer>} | :cat = anything> dispatched by the type of animal"
-         (mallid/describe [:multi {:dispatch :type
+         (umd/describe [:multi {:dispatch :type
                                    :dispatch-description "the type of animal"}
                            [:dog [:map [:x :int]]]
                            [:cat :any]])))
 
   (is (= "one of <:dog = a map where {:x -> <integer>} | :cat = anything> dispatched by :type"
-         (mallid/describe [:multi {:dispatch :type}
+         (umd/describe [:multi {:dispatch :type}
                            [:dog [:map [:x :int]]]
                            [:cat :any]])))
 
   (is (= "Order which is: <Country is a map where {:name -> <an enum of :FI, :PO>, :neighbors (optional) -> <vector of \"Country\">} with no other keys, Burger is a map where {:name -> <string>, :description (optional) -> <string>, :origin -> <a nullable Country>, :price -> <integer greater than 0>}, OrderLine is a map where {:burger -> <Burger>, :amount -> <integer>} with no other keys, Order is a map where {:lines -> <vector of OrderLine>, :delivery -> <a map where {:delivered -> <boolean>, :address -> <a map where {:street -> <string>, :zip -> <integer>, :country -> <Country>}>} with no other keys>} with no other keys>"
-         (mallid/describe [:schema
+         (umd/describe [:schema
                            {:registry {"Country" [:map
                                                   {:closed true}
                                                   [:name [:enum :FI :PO]]
@@ -73,6 +73,6 @@
                            "Order"])))
 
   (is (= "ConsCell <a nullable a vector with exactly 2 items of type: integer, \"ConsCell\">"
-         (mallid/describe [:schema
+         (umd/describe [:schema
                            {:registry {"ConsCell" [:maybe [:tuple :int [:ref "ConsCell"]]]}}
                            "ConsCell"]))))
