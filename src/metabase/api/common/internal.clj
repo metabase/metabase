3de1d9a961d1a27b94d2e8c91aa63f6ca1b6b4a2
Bryan Maass <bryan.maass@gmail.com>
2023-01-04 16:00:18 -0700

    do not stringify errors

--- src/metabase/api/common/internal.clj
+++ src/metabase/api/common/internal.clj
@@ -286,7 +286,7 @@
       (throw (ex-info (tru "Invalid m field: {0}" field-name)
                       {:status-code 400
                        :errors      {(keyword field-name)
-                                     (str (str/join " " (me/humanize (me/with-spell-checking explained))) ".")}})))))
+                                     (me/humanize (me/with-spell-checking explained))}})))))
 
 (defn validate-param
   "Validate a parameter against its respective schema, or throw an Exception."
