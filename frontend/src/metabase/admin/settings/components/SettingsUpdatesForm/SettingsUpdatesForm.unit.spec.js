f96f83fdb09ff2d0aaba818f9780117ec5cd3751
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-05 15:49:53 +0000

    Extract `setup` helper

--- frontend/src/metabase/admin/settings/components/SettingsUpdatesForm/SettingsUpdatesForm.unit.spec.js
+++ frontend/src/metabase/admin/settings/components/SettingsUpdatesForm/SettingsUpdatesForm.unit.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render as renderRTL, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { setupEnterpriseTest } from "__support__/enterprise";
 import { mockSettings } from "__support__/settings";
 import {
@@ -16,62 +16,56 @@ const elements = [
   },
 ];
 
-const render = () => {
-  renderRTL(<SettingsUpdatesForm elements={elements} />);
-};
+function setup({
+  isHosted = false,
+  currentVersion = "v1.0.0",
+  latestVersion = "v2.0.0",
+} = {}) {
+  const version = currentVersion
+    ? createMockVersion({ tag: currentVersion })
+    : null;
+
+  const versionInfo = currentVersion
+    ? createMockVersionInfo({
+        latest: createMockVersionInfoRecord({ version: latestVersion }),
+      })
+    : null;
+
+  mockSettings({
+    "is-hosted?": isHosted,
+    version,
+    "version-info": versionInfo,
+  });
+
+  render(<SettingsUpdatesForm elements={elements} />);
+}
 
 describe("SettingsUpdatesForm", () => {
   it("shows custom message for Cloud installations", () => {
-    mockSettings({ "is-hosted?": true });
-
-    render();
+    setup({ isHosted: true });
     screen.getByText(/Metabase Cloud keeps your instance up-to-date/);
   });
 
   it("shows correct message when latest version is installed", () => {
-    mockSettings({
-      version: createMockVersion({ tag: "v1.0.0" }),
-      "version-info": createMockVersionInfo({
-        latest: createMockVersionInfoRecord({ version: "v1.0.0" }),
-      }),
-    });
-
-    render();
+    setup({ currentVersion: "v1.0.0", latestVersion: "v1.0.0" });
     screen.getByText(/which is the latest and greatest/);
   });
 
   it("shows correct message when no version checks have been run", () => {
-    mockSettings({
-      version: null,
-      "version-info": null,
-    });
-    render();
+    setup({ currentVersion: null, latestVersion: null });
     screen.getByText("No successful checks yet.");
   });
 
   it("shows upgrade call-to-action if not in Enterprise plan", () => {
-    mockSettings({
-      version: createMockVersion({ tag: "v1.0.0" }),
-      "version-info": createMockVersionInfo({
-        latest: createMockVersionInfoRecord({ version: "v1.0.0" }),
-      }),
-    });
-
-    render();
+    setup({ currentVersion: "v1.0.0", latestVersion: "v1.0.0" });
     screen.getByText("Migrate to Metabase Cloud.");
   });
 
   it("does not show upgrade call-to-action if in Enterprise plan", () => {
     setupEnterpriseTest();
 
-    mockSettings({
-      version: createMockVersion({ tag: "v1.0.0" }),
-      "version-info": createMockVersionInfo({
-        latest: createMockVersionInfoRecord({ version: "v2.0.0" }),
-      }),
-    });
+    setup({ currentVersion: "v1.0.0", latestVersion: "v2.0.0" });
 
-    render();
     expect(screen.queryByText("Migrate to Metabase Cloud.")).toBeNull();
   });
 });
