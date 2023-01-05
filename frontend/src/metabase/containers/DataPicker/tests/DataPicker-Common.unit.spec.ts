9c751305905455700afd5a48e17d287ebfcc72b6
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-05 19:29:24 +0000

    Add data picker tests

---
+++ frontend/src/metabase/containers/DataPicker/tests/DataPicker-Common.unit.spec.ts
@@ -0,0 +1,63 @@
+import nock from "nock";
+
+import { screen, waitFor } from "__support__/ui";
+import { SAMPLE_DATABASE } from "__support__/sample_database_fixture";
+
+import { setup } from "./common";
+
+describe("DataPicker", () => {
+  beforeAll(() => {
+    window.HTMLElement.prototype.scrollIntoView = jest.fn();
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it("shows data type picker by default", async () => {
+    await setup();
+
+    expect(screen.getByText(/Models/i)).toBeInTheDocument();
+    expect(screen.getByText(/Raw Data/i)).toBeInTheDocument();
+    expect(screen.getByText(/Saved Questions/i)).toBeInTheDocument();
+  });
+
+  it("handles no data scenario", async () => {
+    await setup({ hasDataAccess: false });
+
+    expect(
+      screen.getByText(/To pick some data, you'll need to add some first/i),
+    ).toBeInTheDocument();
+    expect(screen.queryByText(/Models/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Raw Data/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Saved Questions/i)).not.toBeInTheDocument();
+  });
+
+  it("allows to disable certain data types", async () => {
+    await setup({
+      filters: { types: type => type !== "raw-data" },
+    });
+
+    expect(screen.queryByText(/Raw Data/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/Models/i)).toBeInTheDocument();
+    expect(screen.getByText(/Saved Questions/i)).toBeInTheDocument();
+  });
+
+  it("automatically picks a data type if there's only one available", async () => {
+    await setup({
+      hasModels: false,
+      filters: { types: type => type !== "questions" },
+    });
+
+    await waitFor(() => screen.getByText(/Orders/i));
+
+    expect(screen.getByText(/Sample Database/i)).toBeInTheDocument();
+    SAMPLE_DATABASE.tables.forEach(table => {
+      expect(screen.getByText(table.displayName())).toBeInTheDocument();
+    });
+
+    expect(screen.queryByText(/Raw Data/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Models/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Saved Questions/i)).not.toBeInTheDocument();
+  });
+});
