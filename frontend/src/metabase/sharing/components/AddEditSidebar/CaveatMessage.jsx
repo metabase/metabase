00075597b715e1369d67e4817b5a6be1c3c3036e
jeff bruemmer <jeff.bruemmer@gmail.com>
2023-01-09 15:00:06 -0500

    subscription chart caveat

--- frontend/src/metabase/sharing/components/AddEditSidebar/CaveatMessage.jsx
+++ frontend/src/metabase/sharing/components/AddEditSidebar/CaveatMessage.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 import { t } from "ttag";
 
-import MetabaseSettings from "metabase/lib/settings";
-
 import Text from "metabase/components/type/Text";
-import ExternalLink from "metabase/core/components/ExternalLink";
 
 function CaveatMessage() {
   return (
     <Text className="mx4 my2 p2 bg-light text-dark rounded">
-      <span className="text-bold">{t`Note`}:&nbsp;</span>
-      {t`charts in your subscription won't look the same as in your dashboard.`}
-      &nbsp;
-      <ExternalLink
-        className="link"
-        target="_blank"
-        href={MetabaseSettings.docsUrl("dashboards/subscriptions")}
-      >
-        {t`Learn more`}
-      </ExternalLink>
-      .
+      {t`Charts delivered via your subscription may look slightly different from the charts in your dashboard, but the data will be the same.`}
     </Text>
   );
 }
