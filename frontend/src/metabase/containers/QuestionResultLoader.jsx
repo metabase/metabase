7a1b13ea7e85ece1324afaabc40c8a0997a51535
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-10 14:28:55 +0000

    Revert "Remove a bunch of question loaders"
    This reverts commit 209dbac68dec92083d6e3bdf34ff42ace76a3870.

---
+++ frontend/src/metabase/containers/QuestionResultLoader.jsx
@@ -0,0 +1,145 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import PropTypes from "prop-types";
+import { defer } from "metabase/lib/promise";
+
+const propTypes = {
+  question: PropTypes.object,
+  children: PropTypes.func,
+  onLoad: PropTypes.func,
+  keepPreviousWhileLoading: PropTypes.bool,
+};
+
+/*
+ * Question result loader
+ *
+ * Handle runninng, canceling, and reloading Question results
+ *
+ * @example
+ * <QuestionResultLoader question={question}>
+ * { ({ result, cancel, reload }) =>
+ *     <div>
+ *       { result && (<Visualization ... />) }
+ *
+ *       <a onClick={() => reload()}>Reload this please</a>
+ *       <a onClick={() => cancel()}>Changed my mind</a>
+ *     </div>
+ * }
+ * </QuestionResultLoader>
+ *
+ */
+export class QuestionResultLoader extends React.Component {
+  state = {
+    results: null,
+    loading: false,
+    error: null,
+  };
+
+  UNSAFE_componentWillMount = () => {
+    this._reload();
+  };
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    const { question, onLoad, keepPreviousWhileLoading } = nextProps;
+    // if the question is different, we need to do a fresh load
+    if (question && !question.isEqual(this.props.question)) {
+      this._loadResult(question, onLoad, keepPreviousWhileLoading);
+    }
+  }
+
+  /*
+   * load the result by calling question.apiGetResults
+   */
+  async _loadResult(question, onLoad, keepPreviousWhileLoading) {
+    const { collectionPreview } = this.props;
+
+    // we need to have a question for anything to happen
+    if (question) {
+      try {
+        // set up a defer for cancelation
+        this._cancelDeferred = defer();
+
+        // begin the request, set cancel in state so the query can be canceled
+        this.setState(prev => ({
+          loading: true,
+          results: keepPreviousWhileLoading ? prev.results : null,
+          error: null,
+        }));
+
+        // call apiGetResults and pass our cancel to allow for cancelation
+        const results = await question.apiGetResults({
+          cancelDeferred: this._cancelDeferred,
+          collectionPreview,
+        });
+
+        // setState with our result, remove our cancel since we've finished
+        this.setState({ loading: false, results });
+
+        // handle onLoad prop
+        if (onLoad) {
+          setTimeout(() => onLoad && onLoad(results));
+        }
+      } catch (error) {
+        this.setState({ loading: false, error });
+      }
+    } else {
+      // if there's not a question we can't do anything so go back to our initial
+      // state
+      this.setState({ loading: false, results: null, error: null });
+    }
+  }
+
+  /*
+   * a function to pass to the child to allow the component to call
+   * load again
+   */
+  _reload = () => {
+    const { question, onLoad, keepPreviousWhileLoading } = this.props;
+    this._loadResult(question, onLoad, keepPreviousWhileLoading);
+  };
+
+  /*
+   * a function to pass to the child to allow the component to interrupt
+   * the query
+   */
+  _cancel = () => {
+    // we only want to do things if cancel has been set
+    if (this.state.loading) {
+      // set loading false
+      this.setState({ loading: false });
+      // call our _cancelDeferred to cancel the query
+      if (this._cancelDeferred) {
+        this._cancelDeferred();
+      }
+    }
+  };
+
+  render() {
+    const { question, children } = this.props;
+    const { results, loading, error } = this.state;
+    return (
+      children &&
+      children({
+        results,
+        result: results && results[0],
+        // convienence for <Visualization /> component. Only support single series for now
+        rawSeries:
+          question && results
+            ? [{ card: question.card(), data: results[0].data }]
+            : null,
+        loading,
+        error,
+        cancel: this._cancel,
+        reload: this._reload,
+      })
+    );
+  }
+}
+
+QuestionResultLoader.defaultProps = {
+  keepPreviousWhileLoading: false,
+};
+
+QuestionResultLoader.propTypes = propTypes;
+
+export default QuestionResultLoader;
---
+++ frontend/src/metabase/containers/SavedQuestionLoader.jsx
@@ -0,0 +1,144 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { connect } from "react-redux";
+
+import { loadMetadataForCard } from "metabase/query_builder/actions";
+import { getMetadata } from "metabase/selectors/metadata";
+
+import Questions from "metabase/entities/questions";
+import Question from "metabase-lib/Question";
+
+// type annotations
+
+/*
+ * SavedQuestionLaoder
+ *
+ * Load a saved quetsion and return it to the calling component
+ *
+ * @example
+ *
+ * Render prop style
+ * import SavedQuestionLoader from 'metabase/containers/SavedQuestionLoader'
+ *
+ * // assuming
+ * class ExampleSavedQuestionFeature extends React.Component {
+ *    render () {
+ *      return (
+ *        <SavedQuestionLoader questionId={this.props.params.questionId}>
+ *        { ({ question, loading, error }) => {
+ *
+ *        }}
+ *        </SavedQuestion>
+ *      )
+ *    }
+ * }
+ *
+ * @example
+ *
+ * The raw un-connected component is also exported so we can unit test it
+ * without the redux store.
+ */
+export class SavedQuestionLoader extends React.Component {
+  state = {
+    // this will store the loaded question
+    question: null,
+    card: null,
+    loading: false,
+    error: null,
+  };
+
+  UNSAFE_componentWillMount() {
+    // load the specified question when the component mounts
+    this._loadQuestion(this.props.questionId);
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    // if the questionId changes (this will most likely be the result of a
+    // url change) then we need to load this new question
+    if (nextProps.questionId !== this.props.questionId) {
+      this._loadQuestion(nextProps.questionId);
+    }
+
+    // if the metadata changes for some reason we need to make sure we
+    // update the question with that metadata
+    if (nextProps.metadata !== this.props.metadata && this.state.card) {
+      this.setState({
+        question: new Question(this.state.card, nextProps.metadata),
+      });
+    }
+  }
+
+  /*
+   * Load a saved question and any required metadata
+   *
+   * 1. Get the card from the api
+   * 2. Load any required metadata into the redux store
+   * 3. Create a new Question object to return to metabase-lib methods can
+   *    be used
+   * 4. Set the component state to the new Question
+   */
+  async _loadQuestion(questionId) {
+    if (questionId == null) {
+      this.setState({
+        loading: false,
+        error: null,
+        question: null,
+        card: null,
+      });
+      return;
+    }
+    try {
+      this.setState({ loading: true, error: null });
+      // get the saved question via the card API
+      const card = await this.props.fetchQuestion(questionId);
+
+      // pass the retrieved card to load any necessary metadata
+      // (tables, source db, segments, etc) into
+      // the redux store, the resulting metadata will be avaliable as metadata on the
+      // component props once it's avaliable
+      await this.props.loadMetadataForCard(card);
+
+      // instantiate a new question object using the metadata and saved question
+      // so we can use metabase-lib methods to retrieve information and modify
+      // the question
+      //
+      const question = new Question(card, this.props.metadata);
+
+      // finally, set state to store the Question object so it can be passed
+      // to the component using the loader, keep a reference to the card
+      // as well
+      this.setState({ loading: false, question, card });
+    } catch (error) {
+      this.setState({ loading: false, error });
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+    const { question, loading, error } = this.state;
+    // call the child function with our loaded question
+    return children && children({ question, loading, error });
+  }
+}
+
+// redux stuff
+function mapStateToProps(state) {
+  return {
+    metadata: getMetadata(state),
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadMetadataForCard: card => dispatch(loadMetadataForCard(card)),
+    fetchQuestion: async id => {
+      const action = await dispatch(Questions.actions.fetch({ id }));
+      return Questions.HACK_getObjectFromAction(action);
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SavedQuestionLoader);
