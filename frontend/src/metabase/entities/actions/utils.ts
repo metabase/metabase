695c2f46b6e8c633e0c3ff690a5bb7fc71a1a606
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-11 18:39:04 +0000

    Cherry-pick actions entity

---
+++ frontend/src/metabase/entities/actions/utils.ts
@@ -0,0 +1,110 @@
+import _ from "underscore";
+
+import { getDefaultFieldSettings } from "metabase/actions/containers/ActionCreator/FormCreator";
+
+import type {
+  ActionFormSettings,
+  FieldType,
+  InputSettingType,
+  Parameter,
+  ParameterType,
+} from "metabase-types/api";
+import type { TemplateTag, TemplateTagType } from "metabase-types/types/Query";
+import type NativeQuery from "metabase-lib/queries/NativeQuery";
+import type Question from "metabase-lib/Question";
+
+import {
+  fieldTypeToParameterTypeMap,
+  dateTypeToParameterTypeMap,
+  fieldTypeToTagTypeMap,
+} from "./constants";
+
+export const removeOrphanSettings = (
+  settings: ActionFormSettings,
+  parameters: Parameter[],
+): ActionFormSettings => {
+  const parameterIds = parameters.map(p => p.id);
+  const fieldIds = Object.keys(settings.fields);
+  const orphanIds = _.difference(fieldIds, parameterIds);
+
+  return {
+    ...settings,
+    fields: _.omit(settings.fields, orphanIds),
+  };
+};
+
+export const addMissingSettings = (
+  settings: ActionFormSettings,
+  parameters: Parameter[],
+): ActionFormSettings => {
+  const parameterIds = parameters.map(p => p.id);
+  const fieldIds = Object.keys(settings.fields);
+  const missingIds = _.difference(parameterIds, fieldIds);
+
+  if (!missingIds.length) {
+    return settings;
+  }
+
+  return {
+    ...settings,
+    fields: {
+      ...settings.fields,
+      ...Object.fromEntries(
+        missingIds.map(id => [id, getDefaultFieldSettings({ id })]),
+      ),
+    },
+  };
+};
+
+const getParameterTypeFromFieldSettings = (
+  fieldType: FieldType,
+  inputType: InputSettingType,
+): ParameterType => {
+  if (fieldType === "date") {
+    return dateTypeToParameterTypeMap[inputType] ?? "date/single";
+  }
+
+  return fieldTypeToParameterTypeMap[fieldType] ?? "string/=";
+};
+
+const getTagTypeFromFieldSettings = (fieldType: FieldType): TemplateTagType => {
+  return fieldTypeToTagTypeMap[fieldType] ?? "text";
+};
+
+export const setParameterTypesFromFieldSettings = (
+  settings: ActionFormSettings,
+  parameters: Parameter[],
+): Parameter[] => {
+  const fields = settings.fields;
+  return parameters.map(parameter => {
+    const field = fields[parameter.id];
+    return {
+      ...parameter,
+      type: field
+        ? getParameterTypeFromFieldSettings(field.fieldType, field.inputType)
+        : "string/=",
+    };
+  });
+};
+
+export const setTemplateTagTypesFromFieldSettings = (
+  settings: ActionFormSettings,
+  question: Question,
+): Question => {
+  const fields = settings.fields;
+
+  (question.query() as NativeQuery)
+    .templateTagsWithoutSnippets()
+    .forEach((tag: TemplateTag) => {
+      question = question.setQuery(
+        (question.query() as NativeQuery).setTemplateTag(tag.name, {
+          ...tag,
+          type: getTagTypeFromFieldSettings(
+            fields[tag.id]?.fieldType ?? "string",
+          ),
+        }),
+      );
+    });
+
+  return question;
+};
