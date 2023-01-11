f0757c398d4e048664a0a5eeab75480c713a1bf1
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-11 12:14:46 +0000

    Add model detail link to `PinnedItemCard`

--- frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.unit.spec.js
+++ frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.unit.spec.js
@@ -81,4 +81,27 @@ describe("PinnedItemCard", () => {
     userEvent.click(getIcon("ellipsis"));
     expect(screen.getByText("Unpin")).toBeInTheDocument();
   });
+
+  it("doesn't show model detail page link", () => {
+    setup();
+    expect(screen.queryByTestId("model-detail-link")).not.toBeInTheDocument();
+  });
+
+  describe("models", () => {
+    const model = getCollectionItem({
+      id: 1,
+      name: "Order",
+      model: "dataset",
+      url: "/model/1",
+    });
+
+    it("should show a model detail page link", () => {
+      setup({ item: model });
+      expect(screen.getByTestId("model-detail-link")).toBeInTheDocument();
+      expect(screen.getByTestId("model-detail-link")).toHaveAttribute(
+        "href",
+        "/model/1-order/detail",
+      );
+    });
+  });
 });
