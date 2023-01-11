c7a33d616680501a7c6ebbd330b9a765702835b1
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-11 12:14:29 +0000

    Clean up `PinnedItemCard's` unit tests

--- frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.unit.spec.js
+++ frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.unit.spec.js
@@ -14,27 +14,37 @@ const defaultCollection = {
   archived: false,
 };
 
-const defaultItem = {
-  id: 1,
-  model: "dashboard",
-  collection_position: 1,
-  name: "Dashboard Foo",
-  description: "description foo foo foo",
-  getIcon: () => ({ name: "dashboard" }),
-  getUrl: () => "/dashboard/1",
-  setArchived: jest.fn(),
-  setPinned: jest.fn(),
-};
+function getCollectionItem({
+  id = 1,
+  model = "dashboard",
+  name = "My Item",
+  description = "description foo foo foo",
+  collection_position = 1,
+  icon = "dashboard",
+  url = "/dashboard/1",
+  setArchived = jest.fn(),
+  setPinned = jest.fn(),
+  ...rest
+} = {}) {
+  return {
+    ...rest,
+    id,
+    model,
+    name,
+    description,
+    collection_position,
+    getIcon: () => ({ name: icon }),
+    getUrl: () => url,
+    setArchived,
+    setPinned,
+  };
+}
 
-function setup({ item, collection } = {}) {
-  item = item || defaultItem;
-  collection = collection || defaultCollection;
+const defaultItem = getCollectionItem();
 
+function setup({ item = defaultItem, collection = defaultCollection } = {}) {
   mockOnCopy.mockReset();
   mockOnMove.mockReset();
-  item.setArchived.mockReset();
-  item.setPinned.mockReset();
-
   return render(
     <PinnedItemCard
       item={item}
@@ -62,12 +72,7 @@ describe("PinnedItemCard", () => {
   });
 
   it("should show a default description if there is no item description", () => {
-    const item = {
-      ...defaultItem,
-      description: null,
-    };
-    setup({ item });
-
+    setup({ item: getCollectionItem({ description: null }) });
     expect(screen.getByText("A dashboard")).toBeInTheDocument();
   });
 
