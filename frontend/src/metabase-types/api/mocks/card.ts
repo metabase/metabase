3dbf23e19bd73ae84de7eda8f502cc6e08be9b44
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-11 19:02:03 +0000

    Fix type errror

--- frontend/src/metabase-types/api/mocks/card.ts
+++ frontend/src/metabase-types/api/mocks/card.ts
@@ -13,7 +13,6 @@ export const createMockCard = (opts?: Partial<Card>): Card => ({
   description: null,
   display: "table",
   dataset_query: createMockStructuredDatasetQuery(),
-  result_metadata: [],
   visualization_settings: createMockVisualizationSettings(),
   result_metadata: [],
   dataset: false,
