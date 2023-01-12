31ebd3a02fc3a4fd2fa463f91e361981b5f77ac9
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-01-12 23:15:10 +0200

    Add modal tests

---
+++ frontend/src/metabase/parameters/components/ValuesSourceModal/ValuesSourceModal.unit.spec.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import nock from "nock";
+import userEvent from "@testing-library/user-event";
+import {
+  createMockCollection,
+  createMockField,
+  createMockFieldValues,
+} from "metabase-types/api/mocks";
+import {
+  setupCollectionsEndpoints,
+  setupFieldValuesEndpoints,
+} from "__support__/server-mocks";
+import { renderWithProviders, screen } from "__support__/ui";
+import Field from "metabase-lib/metadata/Field";
+import { UiParameter } from "metabase-lib/parameters/types";
+import { createMockUiParameter } from "metabase-lib/parameters/mock";
+import ValuesSourceModal from "./ValuesSourceModal";
+
+describe("ValuesSourceModal", () => {
+  describe("fields source", () => {
+    it("should show a message about not connected fields", async () => {
+      setup();
+
+      expect(
+        await screen.findByText(/You haven’t connected a field/),
+      ).toBeInTheDocument();
+    });
+
+    it("should a message about missing field values", async () => {
+      setup({
+        parameter: createMockUiParameter({
+          fields: [new Field(createMockField({ id: 1 }))],
+        }),
+      });
+
+      expect(
+        await screen.findByText(/We don’t have any cached values/),
+      ).toBeInTheDocument();
+    });
+
+    it("should show mapped fields values", async () => {
+      setup({
+        parameter: createMockUiParameter({
+          fields: [new Field(createMockField({ id: 2 }))],
+        }),
+      });
+
+      expect(await screen.findByDisplayValue(/Gadget/)).toBeInTheDocument();
+    });
+  });
+
+  describe("list source", () => {
+    it("should set static list values", () => {
+      const { onSubmit } = setup();
+
+      userEvent.click(screen.getByRole("radio", { name: "Custom list" }));
+      userEvent.type(screen.getByRole("textbox"), "Gadget\nWidget");
+      userEvent.click(screen.getByRole("button", { name: "Done" }));
+
+      expect(onSubmit).toHaveBeenCalledWith("static-list", {
+        values: ["Gadget", "Widget"],
+      });
+    });
+  });
+});
+
+interface SetupOpts {
+  parameter?: UiParameter;
+}
+
+const setup = ({ parameter = createMockUiParameter() }: SetupOpts = {}) => {
+  const scope = nock(location.origin);
+  const onSubmit = jest.fn();
+  const onClose = jest.fn();
+
+  setupCollectionsEndpoints(scope, [
+    createMockCollection({ id: "root", name: "Our analytics" }),
+  ]);
+
+  setupFieldValuesEndpoints(
+    scope,
+    createMockFieldValues({ field_id: 1, values: [] }),
+  );
+
+  setupFieldValuesEndpoints(
+    scope,
+    createMockFieldValues({ field_id: 2, values: [["Gadget"], ["Widget"]] }),
+  );
+
+  renderWithProviders(
+    <ValuesSourceModal
+      parameter={parameter}
+      onSubmit={onSubmit}
+      onClose={onClose}
+    />,
+  );
+
+  return { onSubmit };
+};
--- frontend/test/__support__/server-mocks/field.ts
+++ frontend/test/__support__/server-mocks/field.ts
@@ -1,6 +1,10 @@
 import { Scope } from "nock";
-import { Field } from "metabase-types/api";
+import { Field, FieldValues } from "metabase-types/api";
 
 export function setupFieldEndpoints(scope: Scope, field: Field) {
   scope.get(`/api/field/${field.id}`).reply(200, field);
+}
+
+export function setupFieldValuesEndpoints(scope: Scope, values: FieldValues) {
+  scope.get(`/api/field/${values.field_id}/values`).reply(200, values);
 }
