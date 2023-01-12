9c8aa86898d457cb24e4e1f59a522cdc20567105
dpsutton <dan@dpsutton.com>
2023-01-12 16:38:16 -0600

    restore aliases before annotating (#27637)
    * restore aliases before annotating
    
    * cleanup
    
    * fix tests
    
    * Don't add escaped->original if aliases have not changed
    
    No need to walk and replace the aliases if they are identical. And in
    that case, no need to keep a mapping of identical to identical. Not
    super important but saves some time and complexity, and keeps other
    tests passing since the presence of [:info :alias/escaped->original] in
    the query caused them to trivially fail.
    
    * oracle has a smaller limit and _expected_ mangled
    
    previous testing behavior was "what happened" and not what should
    happen. we fixed the bug but the "expect garbage" behavior was still
    present
    
    * Relax :alias/escaped->original schema
    
    oracle tests use keywords for the alias
    
    ```clojure
    {:alias/escaped->original
     {:test-data-venues--via-a763718f "test_data_venues__via__venue_id",
      :test-data-users--via--user-id "test_data_users__via__user_id"}}}
    ```
    
    No idea why that is keyworded
    
    * relax `:alias/escaped->original` schema
    
    see previous commit

--- test/metabase/query_processor/middleware/escape_join_aliases_test.clj
+++ test/metabase/query_processor/middleware/escape_join_aliases_test.clj
@@ -19,7 +19,8 @@
                                    :condition    [:= [:field 3 nil] [:field 4 {:join-alias "cat_2"}]]}]
                          :fields [[:field 3 nil]
                                   [:field 4 {:join-alias "Cat"}]
-                                  [:field 4 {:join-alias "cat_2"}]]}}
+                                  [:field 4 {:join-alias "cat_2"}]]}
+              :info {:alias/escaped->original {"Cat" "Cat", "cat_2" "cat"}}}
              (escape-join-aliases/escape-join-aliases
               {:database 1
                :type     :query
@@ -31,7 +32,26 @@
                                     :condition    [:= [:field 3 nil] [:field 4 {:join-alias "cat"}]]}]
                           :fields [[:field 3 nil]
                                    [:field 4 {:join-alias "Cat"}]
-                                   [:field 4 {:join-alias "cat"}]]}}))))))
+                                   [:field 4 {:join-alias "cat"}]]}})))))
+  (testing "no need to include alias info if they have no changed"
+    (driver/with-driver :h2
+      (let [query {:database 1
+                   :type     :query
+                   :query    {:joins  [{:source-table 2
+                                        :alias        "cat_1"
+                                        :condition    [:= [:field 3 nil] [:field 4 {:join-alias "Cat"}]]}
+                                       {:source-table 2
+                                        :alias        "Cat_2"
+                                        :condition    [:= [:field 3 nil] [:field 4 {:join-alias "cat"}]]}]
+                              :fields [[:field 3 nil]
+                                       [:field 4 {:join-alias "Cat"}]
+                                       [:field 4 {:join-alias "cat"}]]}}
+            q' (escape-join-aliases/escape-join-aliases query)]
+        (testing "No need for a map with identical mapping"
+          (is (not (contains? (:info q') :alias/escaped->original))))
+        (testing "aliases in the query remain the same"
+          (is (= (#'escape-join-aliases/all-join-aliases query)
+                 (#'escape-join-aliases/all-join-aliases q'))))))))
 
 (driver/register! ::custom-escape :abstract? true)
 
@@ -52,7 +72,9 @@
                                    :condition    [:= [:field 3 nil] [:field 4 {:join-alias "가_50a93035"}]]}]
                          :fields [[:field 3 nil]
                                   [:field 4 {:join-alias "012_68c4f033"}]
-                                  [:field 4 {:join-alias "가_50a93035"}]]}}
+                                  [:field 4 {:join-alias "가_50a93035"}]]}
+              :info {:alias/escaped->original {"가_50a93035" "가나다라마"
+                                               "012_68c4f033" "0123456789abcdef"}}}
              (escape-join-aliases/escape-join-aliases
               {:database 1
                :type     :query
