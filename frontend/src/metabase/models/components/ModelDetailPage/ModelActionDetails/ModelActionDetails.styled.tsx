4b50673fb2d5153dca7542421fb7f976e73c3624
Anton Kulyk <kuliks.anton@gmail.com>
2023-01-12 14:35:50 +0000

    Add "New action" flow

---
+++ frontend/src/metabase/models/components/ModelDetailPage/ModelActionDetails/ModelActionDetails.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const ActionsHeader = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const ActionList = styled.ul`
+  margin-top: 1rem;
+
+  li:not(:first-child) {
+    margin-top: 1rem;
+  }
+`;
--- frontend/src/metabase/models/components/ModelDetailPage/ModelActionDetails/ModelActionDetails.tsx
+++ frontend/src/metabase/models/components/ModelDetailPage/ModelActionDetails/ModelActionDetails.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { t } from "ttag";
+
+import Button from "metabase/core/components/Button";
+
+import { useToggle } from "metabase/hooks/use-toggle";
 
 import Actions from "metabase/entities/actions";
+import ActionCreator from "metabase/actions/containers/ActionCreator";
 
 import type { WritebackAction } from "metabase-types/api";
 import type { State } from "metabase-types/store";
 import type Question from "metabase-lib/Question";
 
 import ModelActionListItem from "./ModelActionListItem";
+import { ActionsHeader, ActionList } from "./ModelActionDetails.styled";
 
 interface OwnProps {
   model: Question;
@@ -18,15 +25,32 @@ interface ActionsLoaderProps {
 
 type Props = OwnProps & ActionsLoaderProps;
 
-function ModelActionDetails({ actions }: Props) {
+function ModelActionDetails({ model, actions }: Props) {
+  const [
+    isActionCreatorOpen,
+    { turnOn: showActionCreator, turnOff: hideActionCreator },
+  ] = useToggle();
+
   return (
-    <ul>
-      {actions.map(action => (
-        <li key={action.id}>
-          <ModelActionListItem action={action} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ActionsHeader>
+        <Button onClick={showActionCreator}>{t`New action`}</Button>
+      </ActionsHeader>
+      <ActionList>
+        {actions.map(action => (
+          <li key={action.id}>
+            <ModelActionListItem action={action} />
+          </li>
+        ))}
+      </ActionList>
+      {isActionCreatorOpen && (
+        <ActionCreator
+          modelId={model.id()}
+          databaseId={model.databaseId()}
+          onClose={hideActionCreator}
+        />
+      )}
+    </>
   );
 }
 
