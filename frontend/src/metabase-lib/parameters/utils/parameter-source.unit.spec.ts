ad52680b9fdb5165083cb6a9c72af1a6556612d2
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-01-13 20:43:54 +0200

    Add tests

--- frontend/src/metabase-lib/parameters/utils/parameter-source.unit.spec.ts
+++ frontend/src/metabase-lib/parameters/utils/parameter-source.unit.spec.ts
@@ -4,6 +4,41 @@ import { createMockUiParameter } from "metabase-lib/parameters/mock";
 import { canListParameterValues } from "./parameter-source";
 
 describe("canListParameterValues", () => {
+  it("should list with fields source", () => {
+    const parameter = createMockUiParameter({
+      fields: [
+        new Field(
+          createMockField({
+            has_field_values: "list",
+          }),
+        ),
+      ],
+      values_query_type: "list",
+    });
+
+    expect(canListParameterValues(parameter)).toBeTruthy();
+  });
+
+  it("should list with card source", () => {
+    const parameter = createMockUiParameter({
+      fields: [
+        new Field(
+          createMockField({
+            has_field_values: "none",
+          }),
+        ),
+      ],
+      values_query_type: "list",
+      values_source_type: "card",
+      values_source_config: {
+        card_id: 1,
+        value_field: ["field", 1, null],
+      },
+    });
+
+    expect(canListParameterValues(parameter)).toBeTruthy();
+  });
+
   it("should list with static list source", () => {
     const parameter = createMockUiParameter({
       fields: [
