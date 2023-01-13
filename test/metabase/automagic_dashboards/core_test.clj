ede7fbf3250fabcf1decca405fea71c2d2f841cc
dan sutton <dan@dpsutton.com>
2023-01-13 17:22:42 -0600

    ignore `metabase.automagic-dashboards.core-test`

--- test/metabase/automagic_dashboards/core_test.clj
+++ test/metabase/automagic_dashboards/core_test.clj
@@ -1,4 +1,4 @@
-(ns metabase.automagic-dashboards.core-test
+(ns ^{:mb/tag :mb.test/once} metabase.automagic-dashboards.core-test
   (:require
    [cheshire.core :as json]
    [clojure.core.async :as a]
