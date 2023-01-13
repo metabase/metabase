265b9e54c3d3e313b5c99fa5f7106fe35b280dd2
Callum Herries <hi@callumherries.com>
2023-01-13 15:32:49 +0100

    Add name field ref test

--- test/metabase/db/schema_migrations_test.clj
+++ test/metabase/db/schema_migrations_test.clj
@@ -824,15 +824,18 @@
 (deftest migrate-legacy-column-settings-field-refs-test
   (testing "Migrations v46.00-044: update visualization_settings.column_settings legacy field refs"
     (impl/test-migrations ["v46.00-044" "v46.00-045"] [migrate!]
-      (let [visualization-settings {"column_settings" {"[\"ref\",[\"field-id\",39]]" {"column_title" "ID1"}
-                                                       "[\"ref\",[\"field\",40,null]]" {"column_title" "ID2"}
-                                                       "[\"ref\",[\"fk->\" [\"field-id\",39] [\"field-id\",40]]]" {"column_title" "ID3"}
-                                                       "[\"ref\",[\"field-literal\",\"column_name\",\"type/Text\"]]" {"column_title" "ID4"}
-                                                       }}
-            expected               {"column_settings" {"[\"ref\",[\"field\",39,null]]" {"column_title" "ID1"}
-                                                       "[\"ref\",[\"field\",40,null]]" {"column_title" "ID2"}
-                                                       "[\"ref\",[\"field\",40,{\"source-field\":39}]]" {"column_title" "ID3"}
-                                                       "[\"ref\",[\"field\",\"column_name\",{\"base-type\":\"type/Text\"}]]" {"column_title" "ID4"}}}
+      (let [visualization-settings
+            {"column_settings" {"[\"ref\",[\"field-id\",39]]"                                 {"column_title" "ID1"}
+                                "[\"ref\",[\"field\",40,null]]"                               {"column_title" "ID2"}
+                                "[\"ref\",[\"fk->\" [\"field-id\",39] [\"field-id\",40]]]"    {"column_title" "ID3"}
+                                "[\"ref\",[\"field-literal\",\"column_name\",\"type/Text\"]]" {"column_title" "ID4"}
+                                "[\"name\",\"column_name\"]"                                  {"column_title" "ID5"}}}
+            expected
+            {"column_settings" {"[\"ref\",[\"field\",39,null]]"                                       {"column_title" "ID1"}
+                                "[\"ref\",[\"field\",40,null]]"                                       {"column_title" "ID2"}
+                                "[\"ref\",[\"field\",40,{\"source-field\":39}]]"                      {"column_title" "ID3"}
+                                "[\"ref\",[\"field\",\"column_name\",{\"base-type\":\"type/Text\"}]]" {"column_title" "ID4"}
+                                "[\"name\",\"column_name\"]"                                          {"column_title" "ID5"}}}
             user-id
             (db/simple-insert! User {:first_name  "Howard"
                                      :last_name   "Hughes"
