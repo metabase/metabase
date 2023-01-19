2bad29e1191befe061eb9910ae6a3962807df2f1
Cam Saul <github@camsaul.com>
2023-01-19 23:03:25 +0000

    Fix some stuff

--- test/metabase/test_runner.clj
+++ test/metabase/test_runner.clj
@@ -1,14 +1,21 @@
 (ns metabase.test-runner
   "The only purpose of this namespace is to make sure all of the other stuff below gets loaded."
   (:require
+   [clojure.java.io :as io]
+   [clojure.set :as set]
+   [clojure.string :as str]
+   [clojure.test :refer :all]
    [hawk.core :as hawk]
    [humane-are.core :as humane-are]
    [metabase.bootstrap]
    [metabase.config :as config]
    [metabase.test-runner.assert-exprs]
+   [metabase.test.data.env :as tx.env]
    [metabase.util.date-2]
    [metabase.util.i18n.impl]
    [pjstadig.humane-test-output :as humane-test-output]))
+
+(set! *warn-on-reflection* true)
 
 ;;; TODO -- consider whether we should just mode all of this stuff to [[user]] instead of doing it here
 
@@ -29,5 +36,59 @@
 ;;; Same for https://github.com/camsaul/humane-are
 (humane-are/install!)
 
-(defn find-and-run-tests-cli [options]
-  (hawk.core/find-and-run-tests-cli options))
+;;; Dynamically generate a `:namespace-pattern` to pass to [[hawk.core]] that will exclude all driver namespaces for
+;;; anything that's not in the [[tx.env/test-drivers]] (i.e., not in `DRIVERS`)
+
+(defn- all-drivers []
+  (into #{:h2 :postgres :mysql}
+        (for [^java.io.File file (.listFiles (io/file "modules/drivers"))
+              :when              (.isDirectory file)]
+          (keyword (.getName file)))))
+
+(defn- excluded-drivers []
+  (set/difference (all-drivers) (tx.env/test-drivers)))
+
+(defn- exclude-driver-pattern [driver]
+  (re-pattern (format "(?!^metabase\\.driver\\.%s)" (name driver))))
+
+(defn- exclude-drivers-pattern []
+  (re-pattern (str/join (map exclude-driver-pattern (excluded-drivers)))))
+
+(defn- namespace-pattern []
+  (re-pattern (str (exclude-drivers-pattern) #"^metabase.*test$")))
+
+(deftest ^:parallel namespace-pattern-test
+  (is (re-matches (namespace-pattern) "metabase.util-test"))
+  (binding [tx.env/*test-drivers* (constantly #{:h2})]
+    (is (re-matches (namespace-pattern) "metabase.driver.h2-test"))
+    (is (not (re-matches (namespace-pattern) "metabase.driver.postgres-test"))))
+  (binding [tx.env/*test-drivers* (constantly #{:postgres})]
+    (is (re-matches (namespace-pattern) "metabase.driver.postgres-test"))
+    (is (not (re-matches (namespace-pattern) "metabase.driver.h2-test")))))
+
+(def ^:private excluded-directories
+  ["classes"
+   "dev"
+   "enterprise/backend/src"
+   "local"
+   "resources"
+   "resources-ee"
+   "shared/src"
+   "src"
+   "target"
+   "test_config"
+   "test_resources"])
+
+(defn- default-options []
+  {:namespace-pattern   (namespace-pattern)
+   :exclude-directories excluded-directories})
+
+(defn find-and-run-tests-repl
+  "Find and run tests from the REPL."
+  [options]
+  (hawk.core/find-and-run-tests-repl (merge (default-options) options)))
+
+(defn find-and-run-tests-cli
+  "Entrypoint for `clojure -X:test`."
+  [options]
+  (hawk.core/find-and-run-tests-cli (merge (default-options) options)))
