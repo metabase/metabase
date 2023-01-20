4a7804fbedc68c6470cdc29d6558b8ad8bca3416
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2023-01-20 13:29:30 -0500

    hacky round trip of link

--- frontend/src/metabase/visualizations/visualizations/Link/Link.tsx
+++ frontend/src/metabase/visualizations/visualizations/Link/Link.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 
-function Link() {
-  return <a>Link</a>;
+function Link(props) {
+  console.log(props);
+  if (props.isPreviewing) {
+    return (
+      <input
+        type="text"
+        placeholder="Add a link"
+        onChange={ev =>
+          props.onUpdateVisualizationSettings({ link: ev.target.value })
+        }
+        autoFocus
+      />
+    );
+  } else {
+    return (
+      props.settings.link && (
+        <a href={props.settings.link}>{props.settings.link}</a>
+      )
+    );
+  }
 }
 
 Link.settings = {
   minSize: { width: 3, height: 1 },
+  link: {
+    value: "https://google.com",
+    description: "",
+  },
 };
 
 Link.identifier = "link";
+Link.supportPreviewing = true;
 
 Link.checkRenderable = () => true;
 
