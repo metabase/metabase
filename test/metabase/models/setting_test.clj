dd0cceefc91f1eb600d7eb937d632b0f8620f11c
Callum Herries <hi@callumherries.com>
2023-01-25 18:02:08 +0200

    Add test

--- test/metabase/models/setting_test.clj
+++ test/metabase/models/setting_test.clj
@@ -334,6 +334,26 @@
             (is (= "TEST SETTING - WITH I18N"
                    (description)))))))))
 
+(defsetting test-dynamic-i18n-setting
+  #(deferred-tru "Test setting - with i18n: {0}" (test-i18n-setting)))
+
+(deftest dynamic-description-test
+  (testing "Descriptions with i18n string returned by function should be able to depend on another setting's value."
+    (mt/with-test-user :crowberto
+      (mt/with-mock-i18n-bundles {"zz" {:messages {"Test setting - with i18n: {0}" "TEST SETTING - WITH I18N: {0}"}}}
+        (letfn [(description []
+                  (some (fn [{:keys [key description]}]
+                          (when (= :test-dynamic-i18n-setting key)
+                            description))
+                        (setting/admin-writable-settings)))]
+          (test-i18n-setting! "test-setting-value!")
+          (is (= "Test setting - with i18n: test-setting-value!"
+                 (description)))
+          (mt/with-user-locale "zz"
+            (is (= "TEST SETTING - WITH I18N: test-setting-value!"
+                   (description))))
+          (test-i18n-setting! nil))))))
+
 
 ;;; ------------------------------------------------ BOOLEAN SETTINGS ------------------------------------------------
 
