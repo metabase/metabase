b2954f77089e2d627ac7503096dec26c628871ab
Ryan Laurie <iethree@gmail.com>
2023-01-31 15:42:11 -0700

    use knex schema in repro for #27745

--- frontend/test/__support__/e2e/test_tables.js
+++ frontend/test/__support__/e2e/test_tables.js
@@ -4,3 +4,21 @@
 // we cannot use knex to define multi-dialect schemas because we can only pass
 // json-serializable data to cypress tasks (which run in node)
 // https://docs.cypress.io/api/commands/task#Arguments
+
+export const colors27745 = async dbClient => {
+  const tableName = "colors27745";
+
+  await dbClient.schema.dropTableIfExists(tableName);
+  await dbClient.schema.createTable(tableName, table => {
+    table.increments("id").primary();
+    table.string("name").unique().notNullable();
+  });
+
+  await dbClient(tableName).insert([
+    { name: "red" },
+    { name: "green" },
+    { name: "blue" },
+  ]);
+
+  return true;
+};
