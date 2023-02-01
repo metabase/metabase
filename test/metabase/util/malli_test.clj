9c0193fd928bc7ee6626ea98938ddc483ef86edc
Bryan Maass <bryan.maass@gmail.com>
2023-01-31 17:18:42 -0700

    revert ns

--- test/metabase/util/malli_test.clj
+++ test/metabase/util/malli_test.clj
@@ -6,111 +6,26 @@
    [metabase.util.malli :as mu]
    [metabase.util.malli.describe :as umd]))
 
-
 (deftest mu-defn-test
-  (testing "function validation on"
-    (testing "invalid input with function validation"
-      (do
-        (mu/set-fn-validation! true)
-        (mu/defn bar [x :- [:map [:x int?] [:y int?]]] (str x))
-        (bar {})
-        (is (= [{:x ["missing required key got: nil"]
-                 :y ["missing required key got: nil"]}]
-               (:humanized
-                (try (bar {})
-                     (catch Exception e (ex-data e)))))
-            "when we pass bar an invalid shape um/defn throws"))
-      (ns-unmap *ns* 'bar))
+  (testing "invalid input"
+    (mu/defn bar [x :- [:map [:x int?] [:y int?]]] (str x))
+    (is (= [{:x ["missing required key"]
+             :y ["missing required key"]}]
+           (:humanized
+            (try (bar {})
+                 (catch Exception e (ex-data e)))))
+        "when we pass bar an invalid shape um/defn throws")
+    (ns-unmap *ns* 'bar))
 
-    (testing "invalid output"
-      (do
-        (mu/set-fn-validation! true)
-        (mu/defn baz :- [:map [:x int?] [:y int?]] [] {:x "3"})
-        (is (= {:x ["should be an int got: \"3\""]
-                :y ["missing required key got: nil"]}
-               (:humanized
-                (try (baz)
-                     (catch Exception e (ex-data e)))))
-            "when baz returns an invalid form um/defn throws"))
-      (ns-unmap *ns* 'baz)))
-
-  (testing "function validation on"
-    (testing "invalid input with function validation"
-      (do
-        (mu/set-fn-validation! false)
-        (mu/defn bar [x :- [:map [:x int?] [:y int?]]] (str x))
-        (is (= [{:x ["missing required key got: nil"]
-                 :y ["missing required key got: nil"]}]
-               (:humanized
-                (try (bar {})
-                     (catch Exception e (ex-data e)))))
-            "when we pass bar an invalid shape um/defn throws"))
-      (ns-unmap *ns* 'bar))
-
-    (testing "invalid output"
-      (do
-        (mu/set-fn-validation! false)
-        (mu/defn baz :- [:map [:x int?] [:y int?]] [] {:x "3"})
-        (is (= {:x ["should be an int got: \"3\""]
-                :y ["missing required key got: nil"]}
-               (:humanized
-                (try (baz)
-                     (catch Exception e (ex-data e)))))
-            "when baz returns an invalid form um/defn throws"))
-      (ns-unmap *ns* 'baz)))
-
-
-
-
-
-;; [(set-fn-validation! false) @*validate-schemas]
-;; ;;=> [true true]
-;; (defn eff [x :- :string])
-
-;; (eff 3)
-;; ;; => nil
-
-
-;; ;; ## WITH ^:always-validate
-
-;; [(set-fn-validation! true) @*validate-schemas]
-;; ;;=> [true true]
-;; (defn ^:always-validate gee [x :- :string])
-;; (try (gee 3)
-;;      (catch Exception e (:type (ex-data e))))
-;; ;;=> :malli.core/invalid-input
-
-;; [(set-fn-validation! false) @*validate-schemas]
-;; ;;=> [false false]
-;; (defn ^:always-validate gee [x :- :string])
-
-;; (try (gee 3)
-;;      (catch Exception e (:type (ex-data e))))
-;; ;;=> :malli.core/invalid-input
-
-
-;; ;; ## WITH ^:never-validate
-
-;; [(set-fn-validation! true) @*validate-schemas]
-;; ;;=> [true true]
-;; (defn ^:never-validate eye [x :- :string])
-;; (eye 3)
-;; ;; => nil
-
-;; [(set-fn-validation! false) @*validate-schemas]
-;; ;; => [false false]
-;; (defn ^:never-validate eye [x :- :string])
-;; (eye 3)
-;; ;; => nil
-
-
-
-
-
-
-  )
-
-
+  (testing "invalid output"
+    (mu/defn baz :- [:map [:x int?] [:y int?]] [] {:x "3"})
+    (is (= {:x ["should be an int"]
+            :y ["missing required key"]}
+           (:humanized
+            (try (baz)
+                 (catch Exception e (ex-data e)))))
+        "when baz returns an invalid form um/defn throws")
+    (ns-unmap *ns* 'baz)))
 
 (deftest with-api-error-message
   (let [less-than-four-fxn (fn [x] (< x 4))]
