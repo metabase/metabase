5afde98aefdc96145e83aadf6d9d205ab353f6f1
Anton Kulyk <kuliks.anton@gmail.com>
2023-02-03 18:03:00 +0000

    Bring e2e tests back
    Manually bringing back tests added in: https://github.com/metabase/metabase/pull/28056

--- frontend/src/metabase-types/api/mocks/actions.ts
+++ frontend/src/metabase-types/api/mocks/actions.ts
@@ -9,18 +9,20 @@ import { createMockNativeDatasetQuery } from "./query";
 import { createMockParameter } from "./parameters";
 import { createMockUserInfo } from "./user";
 
-export const createMockActionParameter = (
-  opts?: Partial<WritebackParameter>,
-): WritebackParameter => ({
-  target: opts?.target || ["variable", ["template-tag", "id"]],
-  ...createMockParameter({
-    id: "id",
+export const createMockActionParameter = ({
+  id = "id",
+  target = ["variable", ["template-tag", id]],
+  ...opts
+}: Partial<WritebackParameter> = {}): WritebackParameter => {
+  const parameter = createMockParameter({
+    id,
     name: "ID",
     type: "type/Integer",
     slug: "id",
     ...opts,
-  }),
-});
+  });
+  return { ...parameter, target };
+};
 
 export const createMockQueryAction = ({
   dataset_query = createMockNativeDatasetQuery(),
