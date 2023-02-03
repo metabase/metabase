d562a1e45d7125b378bef6fe97514af6f0b11213
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2023-02-03 20:24:53 +0100

    Check if staged files include relevant files from `scenarios` folder

--- frontend/test/validate-e2e-test-files.js
+++ frontend/test/validate-e2e-test-files.js
@@ -1,22 +1,34 @@
 #!/usr/bin/env node
+const { execSync } = require("child_process");
 
 const path = require("path");
 const glob = require("glob");
 
-// Will match all files in the scenarios dir, except the helpers
-const PATTERN = "frontend/test/metabase/scenarios/*/{*.js,!(helpers)/*.js}";
+const e2eHome = "frontend/test/metabase/scenarios/";
+const e2eFileExtension = ".cy.spec.js";
 
-const invalidFileNames = glob.sync(PATTERN).filter(fullPath => {
-  const basename = path.basename(fullPath);
-  return !basename.endsWith(".cy.spec.js");
-});
+const stagedFiles = execSync("git diff HEAD --name-only", { encoding: "utf8" });
+const relevantStagedFiles = stagedFiles.includes(e2eHome);
 
-if (invalidFileNames.length > 0) {
-  console.error(
-    "Found Cypress files not ending with .cy.spec.js:\n\n" +
-      invalidFileNames.join("\n"),
-  );
-  return 1;
+if (relevantStagedFiles) {
+  // Will match all files in the scenarios dir, except the helpers
+  const PATTERN = "frontend/test/metabase/scenarios/*/{*.js,!(helpers)/*.js}";
+
+  const invalidFileNames = glob.sync(PATTERN).filter(fullPath => {
+    const basename = path.basename(fullPath);
+    return !basename.endsWith(e2eFileExtension);
+  });
+
+  if (invalidFileNames.length > 0) {
+    console.error(
+      `\nFound Cypress files not ending with '${e2eFileExtension}':\n\n` +
+        invalidFileNames.join("\n"),
+    );
+
+    return 1;
+  }
+
+  return 0;
+} else {
+  return 0;
 }
-
-return 0;
