fa358420e2f8902c119fd21b3f6b16c860faf244
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2023-02-06 09:57:14 +0100

    Update cross-version folder structure

--- frontend/test/metabase/scenarios/cross-version/source/shared/cross-version-source.config.js
+++ frontend/test/metabase/scenarios/cross-version/source/shared/cross-version-source.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require("cypress");
-const {
-  crossVersionSourceConfig,
-} = require("../../../../../__support__/e2e/config");
+const { crossVersionSourceConfig } = require("__support__/e2e/config");
 
 module.exports = defineConfig({ e2e: crossVersionSourceConfig });
--- frontend/test/metabase/scenarios/cross-version/target/shared/cross-version-target.config.js
+++ frontend/test/metabase/scenarios/cross-version/target/shared/cross-version-target.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require("cypress");
-const {
-  crossVersionTargetConfig,
-} = require("../../../../__support__/e2e/config");
+const { crossVersionTargetConfig } = require("__support__/e2e/config");
 
 module.exports = defineConfig({ e2e: crossVersionTargetConfig });
