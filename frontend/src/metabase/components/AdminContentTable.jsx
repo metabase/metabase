cb7af574b1d151426b07c38a7325b6a0da3833e2
Gustavo Saiani <gustavo@poe.ma>
2023-02-08 15:48:50 -0300

    Fit widget in each of the 3 forms

--- frontend/src/metabase/components/AdminContentTable.jsx
+++ frontend/src/metabase/components/AdminContentTable.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import styled from "@emotion/styled";
+
+import { color } from "metabase/lib/colors";
+
+const Th = styled.th`
+  color: ${color("text-dark")};
+`;
 
 const AdminContentTable = ({ columnTitles, children }) => (
   <table className="ContentTable">
     <thead>
       <tr>
         {columnTitles &&
-          columnTitles.map((title, index) => <th key={index}>{title}</th>)}
+          columnTitles.map((title, index) => <Th key={index}>{title}</Th>)}
       </tr>
     </thead>
     <tbody>{children}</tbody>
