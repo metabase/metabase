d4bbd7c642dc1c5a2d43202685db2b0337f90466
Mark Bastian <markbastian@gmail.com>
2023-02-08 08:19:33 -0700

    Updated import

--- test/metabase/query_processor/streaming_test.clj
+++ test/metabase/query_processor/streaming_test.clj
@@ -18,6 +18,8 @@
    [metabase.util :as u]
    [toucan.db :as db])
   (:import
+   (jakarta.servlet AsyncContext ServletOutputStream)
+   (jakarta.servlet.http HttpServletResponse)
    (java.util UUID)))
 
 (defn- maybe-remove-checksum
@@ -82,18 +84,18 @@
                                      (qp.context/reducef (qp.context/rff context) context metadata rows))))
             complete-promise   (promise)]
         (server.protocols/respond streaming-response
-                                  {:response      (reify javax.servlet.http.HttpServletResponse
+                                  {:response      (reify HttpServletResponse
                                                     (setStatus [_ _])
                                                     (setHeader [_ _ _])
                                                     (setContentType [_ _])
                                                     (getOutputStream [_]
-                                                      (proxy [javax.servlet.ServletOutputStream] []
+                                                      (proxy [ServletOutputStream] []
                                                         (write
                                                           ([byytes]
                                                            (.write os ^bytes byytes))
                                                           ([byytes offset length]
                                                            (.write os ^bytes byytes offset length))))))
-                                   :async-context (reify javax.servlet.AsyncContext
+                                   :async-context (reify AsyncContext
                                                     (complete [_]
                                                       (deliver complete-promise true)))})
         (is (= true
