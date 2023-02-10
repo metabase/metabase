52a33907422b9a3fba197bb70aa126be77ac8837
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-02-10 16:25:14 +0200

    Extend cy test

--- frontend/test/metabase/scenarios/models/model-actions.cy.spec.js
+++ frontend/test/metabase/scenarios/models/model-actions.cy.spec.js
@@ -140,6 +140,7 @@ describe("scenarios > models > actions", () => {
 
     cy.findByRole("dialog").within(() => {
       cy.findByRole("button", { name: "Action settings" }).click();
+      cy.findByLabelText("Success message").clear().type("Thanks!");
       cy.findByLabelText("Make public").should("not.be.checked").click();
       cy.findByLabelText("Public action link URL")
         .invoke("val")
@@ -155,7 +156,7 @@ describe("scenarios > models > actions", () => {
 
     cy.findByLabelText(TEST_PARAMETER.name).type("-2");
     cy.findByRole("button", { name: "Submit" }).click();
-    cy.findByText("Thanks for your submission.").should("be.visible");
+    cy.findByText("Thanks!").should("be.visible");
     cy.findByRole("form").should("not.exist");
     cy.findByRole("button", { name: "Submit" }).should("not.exist");
 
