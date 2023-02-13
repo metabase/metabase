8ec7f1856b6c4776d9f9f2f7b525952e27264021
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2023-02-13 10:13:32 -0300

    no native drill fallback when embedded (#28243)

--- frontend/src/metabase/modes/components/drill/NativeDrillFallback/NativeDrillFallback.tsx
+++ frontend/src/metabase/modes/components/drill/NativeDrillFallback/NativeDrillFallback.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { t } from "ttag";
-import MetabaseSettings from "metabase/lib/settings";
+import { IFRAMED } from "metabase/lib/dom";
 import { getEngineNativeType } from "metabase/lib/engine";
-import Icon from "metabase/components/Icon";
 import Question from "metabase-lib/Question";
 import { nativeDrillFallback } from "metabase-lib/queries/drills/native-drill-fallback";
-import {
-  DrillLearnLink,
-  DrillMessage,
-  DrillRoot,
-} from "./NativeDrillFallback.styled";
+
+import { DrillMessage, DrillRoot } from "./NativeDrillFallback.styled";
 
 interface NativeDrillFallbackProps {
   question: Question;
@@ -23,7 +19,10 @@ const NativeDrillFallback = ({ question }: NativeDrillFallbackProps) => {
 
   const { database } = drill;
   const isSql = getEngineNativeType(database.engine) === "sql";
-  const learnUrl = MetabaseSettings.learnUrl("questions/drill-through");
+
+  if (IFRAMED) {
+    return [];
+  }
 
   return [
     {
@@ -37,10 +36,6 @@ const NativeDrillFallback = ({ question }: NativeDrillFallbackProps) => {
               ? t`Drill-through doesn’t work on SQL questions.`
               : t`Drill-through doesn’t work on native questions.`}
           </DrillMessage>
-          <DrillLearnLink href={learnUrl}>
-            <Icon name="reference" />
-            {t`Learn more`}
-          </DrillLearnLink>
         </DrillRoot>
       ),
     },
