7d98ab1f82e0dc28e8ee1227913a7f7a91dc792a
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-02-13 20:11:30 -0300

    rewrite mock

--- frontend/src/metabase/modes/components/drill/NativeDrillFallback/NativeDrillFallback.unit.spec.tsx
+++ frontend/src/metabase/modes/components/drill/NativeDrillFallback/NativeDrillFallback.unit.spec.tsx
@@ -1,35 +1,45 @@
-jest.doMock("metabase/lib/dom");
 import * as dom from "metabase/lib/dom";
+
 import { getCleanNativeQuestion } from "metabase-lib/mocks";
 import NativeDrillFallback from "./NativeDrillFallback";
 
+const mockIframed = (value: boolean) => {
+  const previous = dom.IFRAMED;
+  Object.defineProperty(dom, "IFRAMED", {
+    configurable: true,
+    get() {
+      return value;
+    },
+  });
+
+  return () => {
+    Object.defineProperty(dom, "IFRAMED", {
+      configurable: true,
+      get() {
+        return previous;
+      },
+    });
+  };
+};
+
 describe("NativeDrillFallback", () => {
-  let isWithinIframeSpy: jest.SpyInstance;
-
-  beforeEach(() => {
-    isWithinIframeSpy = jest.spyOn(dom, "isWithinIframe");
-  });
-
-  afterEach(() => {
-    isWithinIframeSpy.mockRestore();
-  });
-
-  it("should return native drill fallback element on native questions", async () => {
-    isWithinIframeSpy.mockReturnValue(false);
-
+  it("should return native drill fallback element on native questions", () => {
+    const unmock = mockIframed(false);
     const question = getCleanNativeQuestion();
     const result = NativeDrillFallback({ question });
 
     expect(result).toHaveLength(1);
     expect(result[0].name).toBe("fallback-native");
+    unmock();
   });
 
-  it("should not return native drill fallback element on native questions when the app is in iframe", async () => {
-    isWithinIframeSpy.mockReturnValue(true);
+  it("should not return native drill fallback element on native questions when the app is in iframe", () => {
+    const unmock = mockIframed(true);
 
     const question = getCleanNativeQuestion();
     const result = NativeDrillFallback({ question });
 
     expect(result).toHaveLength(0);
+    unmock();
   });
 });
