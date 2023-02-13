ee9aa9deaf2c69321eaeb4ed154bd81f3ed7c47b
Ngoc Khuat <qn.khuat@gmail.com>
2023-02-13 21:51:52 +0700

    fix track changes return nil

--- dev/src/dev/db_tracking.clj
+++ dev/src/dev/db_tracking.clj
@@ -6,15 +6,17 @@
 
   > (track! models/Dashboard models/Card models/DashboardCard)
 
-  -- Go on UI and do stuffs like update viz-settings of a dashcard.
+  -- Go on UI and do stuffs like (i.e: update viz-settings of a dashcard).
 
   > (changes)
   ;; => {:update {:report_dashboardcard ...}}
 
-  You can use [[reset-tracking]] to clear our all the current trackings.
-  And untrack! to stop tracking for a list of models."
+  You can use [[reset-tracking!]] to clear our all the current trackings.
+  And [[untrack-all!]] or [[untrack!]] to stop tracking."
   (:require
-   [toucan2.core :as t2]))
+   [methodical.core :as m]
+   [toucan2.core :as t2]
+   [toucan2.tools.before-update :as t2.before-update]))
 
 (def ^:private tracking (atom {}))
 
@@ -31,7 +33,7 @@
   For insert, track the instance as a map.
   For update, only track the changes."
   [action instance]
-  (swap! tracking update-in [action (t2/table-name instance)] conj
+  (swap! tracking update-in [action :report_card] conj
          (case action
            :insert
            (into {} instance)
@@ -43,10 +45,20 @@
   (new-tracking :insert instance)
   instance)
 
+;; TODO: we use before-update but not after-update because `t2/changes` on instance returns empty map on after-update
+(t2/define-before-update ::tracking
+  [instance]
+  (new-tracking :update instance)
+  instance)
+
 (t2/define-after-update ::tracking
   [instance]
   (new-tracking :update instance)
   instance)
+
+(m/prefer-method! #'toucan2.tools.before-update/before-update
+                                :metabase.models.interface/timestamped?
+                                ::tracking)
 
 ;; define-after-delete haven't supported https://github.com/camsaul/toucan2/issues/70
 #_(t2/define-after-delete ::tracking
