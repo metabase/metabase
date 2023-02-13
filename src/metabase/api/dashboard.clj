173f83699b5c71d61db99a7bd6316e0aaff38a03
Mark Bastian <markbastian@gmail.com>
2023-02-13 14:01:26 -0700

    Moving metadata destructure closer to use

--- src/metabase/api/dashboard.clj
+++ src/metabase/api/dashboard.clj
@@ -684,10 +684,10 @@
 (s/defn ^:private mappings->field-ids :- (s/maybe #{su/IntGreaterThanZero})
   [parameter-mappings :- (s/maybe (s/cond-pre #{dashboard-card/ParamMapping} [dashboard-card/ParamMapping]))]
   (set (for [{{:keys [card]} :dashcard :keys [target]} parameter-mappings
-             :let  [field-clause (params/param-target->field-clause target card)
-                    {:keys [result_metadata]} card]
+             :let  [field-clause (params/param-target->field-clause target card)]
              :when field-clause
-             :let  [field-id (or
+             :let  [{:keys [result_metadata]} card
+                    field-id (or
                               ;; Get the field id from the field-clause if it contains it. This is the common case for
                               ;; mbql queries.
                               (mbql.u/match-one field-clause [:field (id :guard integer?) _] id)
