8102e6e2a35fe1aa0fe277009645e61dc206ae73
Gustavo Saiani <gustavo@poe.ma>
2023-02-15 17:45:26 -0300

    Fix issue and add typescript

--- frontend/src/metabase/admin/people/components/GroupSelect/GroupSelect.tsx
+++ frontend/src/metabase/admin/people/components/GroupSelect/GroupSelect.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import { t } from "ttag";
 
@@ -13,7 +12,16 @@ import {
   getGroupColor,
   getGroupNameLocalized,
 } from "metabase/lib/groups";
-import GroupSummary from "./GroupSummary";
+import { GroupIds, UserGroupType, UserGroupsType } from "metabase/admin/types";
+import GroupSummary from "../GroupSummary";
+
+type GroupSelectProps = {
+  groups: UserGroupsType;
+  selectedGroupIds: GroupIds;
+  onGroupChange: (group: UserGroupType, selected: boolean) => void;
+  isCurrentUser: boolean;
+  emptyListMessage: string;
+};
 
 export const GroupSelect = ({
   groups,
@@ -21,7 +29,7 @@ export const GroupSelect = ({
   onGroupChange,
   isCurrentUser = false,
   emptyListMessage = t`No groups`,
-}) => {
+}: GroupSelectProps) => {
   const triggerElement = (
     <div className="flex align-center">
       <span className="mr1 text-medium">
@@ -38,35 +46,50 @@ export const GroupSelect = ({
       </PopoverWithTrigger>
     );
   }
-  const other = groups.filter(g => !isAdminGroup(g) && !isDefaultGroup(g));
+
   const adminGroup = groups.find(isAdminGroup);
   const defaultGroup = groups.find(isDefaultGroup);
   const topGroups = [defaultGroup, adminGroup].filter(g => g != null);
+  const groupsExceptDefaultAndAdmin = groups.filter(
+    g => !isAdminGroup(g) && !isDefaultGroup(g),
+  );
+
+  const sections =
+    topGroups.length > 0
+      ? [
+          { items: topGroups },
+          groupsExceptDefaultAndAdmin.length > 0
+            ? {
+                items: groupsExceptDefaultAndAdmin,
+                name: t`Groups`,
+              }
+            : null,
+        ].filter(_ => _)
+      : [{ items: groupsExceptDefaultAndAdmin }];
 
   return (
     <Select
       triggerElement={triggerElement}
-      onChange={({ target: { value } }) => {
+      onChange={({ target: { value } }: { target: { value: any } }) => {
         groups
           .filter(
             // find the differing groups between the new `value` on previous `selectedGroupIds`
             group =>
-              selectedGroupIds.includes(group.id) ^ value.includes(group.id),
+              (selectedGroupIds.includes(group.id) as any) ^
+              value.includes(group.id),
           )
           .forEach(group => onGroupChange(group, value.includes(group.id)));
       }}
-      optionDisabledFn={group =>
+      optionDisabledFn={(group: UserGroupType) =>
         (isAdminGroup(group) && isCurrentUser) || !canEditMembership(group)
       }
-      optionValueFn={group => group.id}
+      optionValueFn={(group: UserGroupType) => group.id}
       optionNameFn={getGroupNameLocalized}
-      optionStylesFn={group => ({ color: getGroupColor(group) })}
+      optionStylesFn={(group: UserGroupType) => ({
+        color: getGroupColor(group),
+      })}
       value={selectedGroupIds}
-      sections={
-        topGroups.length > 0
-          ? [{ items: topGroups }, { items: other, name: t`Groups` }]
-          : [{ items: other }]
-      }
+      sections={sections}
       multiple
     />
   );
