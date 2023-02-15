5fd1e36314281c6f5ac6e62bc75d0cbe6fb3f789
Ryan Laurie <iethree@gmail.com>
2023-02-15 14:33:17 -0700

    improve virtual card overlays

--- frontend/src/metabase/dashboard/utils.js
+++ frontend/src/metabase/dashboard/utils.js
@@ -62,6 +62,10 @@ export function getVirtualCardType(dashcard) {
   return dashcard?.visualization_settings?.virtual_card?.display;
 }
 
+export function getVirtualCardType(dashcard) {
+  return dashcard?.visualization_settings?.virtual_card?.display;
+}
+
 export function isNativeDashCard(dashcard) {
   return isNative(dashcard.card?.dataset_query);
 }
