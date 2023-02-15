1636dd1869a728b3d5c42d1aff7961bb8e8c62ad
Anton Kulyk <kuliks.anton@gmail.com>
2023-02-15 11:39:11 +0000

    Extract `ImplicitActionIcon` component

--- frontend/src/metabase/actions/components/ImplicitActionIcon/ImplicitActionIcon.styled.tsx
+++ frontend/src/metabase/actions/components/ImplicitActionIcon/ImplicitActionIcon.styled.tsx
@@ -1,12 +1,7 @@
 import styled from "@emotion/styled";
-import Icon from "metabase/components/Icon";
 
 export const Root = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-`;
-
-export const IconSmall = styled(Icon)`
-  margin-left: 24px;
 `;
--- frontend/src/metabase/models/components/ModelDetailPage/ModelActionDetails/ModelActionListItem.tsx
+++ frontend/src/metabase/models/components/ModelDetailPage/ModelActionDetails/ModelActionListItem.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { t } from "ttag";
 
+import ImplicitActionIcon from "metabase/actions/components/ImplicitActionIcon";
+
 import type { WritebackAction, WritebackQueryAction } from "metabase-types/api";
 
 import { isNotNull } from "metabase/core/utils/types";
-import StackedInsightIcon from "./StackedInsightIcon";
 import {
   ActionTitle,
   ActionSubtitle,
@@ -29,7 +30,7 @@ function QueryActionCardContent({ action }: { action: WritebackQueryAction }) {
 function ImplicitActionCardContent() {
   return (
     <ImplicitActionCardContentRoot>
-      <StackedInsightIcon />
+      <ImplicitActionIcon size={32} />
       <ImplicitActionMessage>{t`Auto tracking schema`}</ImplicitActionMessage>
     </ImplicitActionCardContentRoot>
   );
