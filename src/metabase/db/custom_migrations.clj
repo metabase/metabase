9d9ea6f48deeca5e5f44cbe4ca489caa38b07c08
Noah Moss <noahbmoss@gmail.com>
2023-02-16 17:40:53 -0500

    edit docstring

--- src/metabase/db/custom_migrations.clj
+++ src/metabase/db/custom_migrations.clj
@@ -6,26 +6,28 @@
 
 (defmacro def-reversible-migration
   "Define a reversible custom migration. Both the forward and reverse migrations are defined using the same structure,
-  similar to the bodies of multi-arity Clojure functions. The first thing in each migration body must be a one-element
-  vector containing a binding to use for the database object provided by Liquibase, so that migrations have access to
-  it if needed. It is also set automatically as the current connection for Toucan 2.
+  similar to the bodies of multi-arity Clojure functions.
+
+  The first thing in each migration body must be a one-element vector containing a binding to use for the database
+  object provided by Liquibase, so that migrations have access to it if needed. This should typically not be used
+  directly, however, because is also set automatically as the current connection for Toucan 2.
 
   Example:
 
   ```clj
   (def-reversible-migration ExampleMigrationName
-    ([_database]
-     (migration-body))
+   ([_database]
+    (migration-body))
 
-    ([_database]
-     (migration-body)))"
+   ([_database]
+    (migration-body)))"
   [name [[db-binding-1] & migration-body] [[db-binding-2] reverse-migration-body]]
   `(defrecord ~name []
      CustomTaskChange
      (execute [_# database#]
        (binding [toucan2.connection/*current-connectable* (.getWrappedConnection (.getConnection database#))]
-        (let [~db-binding-1 database#]
-          ~@migration-body)))
+         (let [~db-binding-1 database#]
+           ~@migration-body)))
      (getConfirmationMessage [_#]
        (str "Custom migration: " ~name))
      (setUp [_#])
