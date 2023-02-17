246d1eecbd35dbedb97340285b4b3b0e5ee507a2
Ryan Laurie <iethree@gmail.com>
2023-02-17 11:51:00 -0700

    update tests

--- frontend/src/metabase/dashboard/components/Dashboard/DashboardEmptyState/DashboardEmptyState.unit.spec.js
+++ frontend/src/metabase/dashboard/components/Dashboard/DashboardEmptyState/DashboardEmptyState.unit.spec.js
@@ -5,14 +5,19 @@ import DashboardEmptyState from "./DashboardEmptyState";
 
 describe("DashboardEmptyState", () => {
   it("renders", () => {
-    render(<DashboardEmptyState isNightMode={false} />);
+    render(
+      <DashboardEmptyState
+        isNightMode={false}
+        addQuestion={jest.fn()}
+        closeNavbar={jest.fn()}
+      />,
+    );
 
     expect(screen.getByText("?")).toBeInTheDocument();
     expect(
       screen.getByText("This dashboard is looking empty."),
     ).toBeInTheDocument();
-    expect(
-      screen.getByText("Add a question to start making it useful!"),
-    ).toBeInTheDocument();
+    expect(screen.getByText("Add a saved question")).toBeInTheDocument();
+    expect(screen.getByText("ask a new one")).toBeInTheDocument();
   });
 });
