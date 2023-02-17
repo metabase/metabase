82e7b8d72ddfaeb17856aa8ad26e28842dad3e19
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2023-02-17 16:34:42 -0500

    default to docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import "metabase/css/index.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewMode: "docs",
   controls: {
     matchers: {
       color: /(background|color)$/i,
