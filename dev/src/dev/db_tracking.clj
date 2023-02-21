6d4065100294f9f0e3914bfd07d4e3e5330b8d78
Ngoc Khuat <qn.khuat@gmail.com>
2023-02-21 17:47:03 +0700

    bug with before-delete?

--- dev/src/dev/db_tracking.clj
+++ dev/src/dev/db_tracking.clj
@@ -1,11 +1,14 @@
 (ns dev.db-tracking
   "A set of utility function to track database changes.
   Use this when you want to observe changes of database models when doing stuffs on UI.
+
   How to use this?
-  > (track! models/Dashboard models/Card models/DashboardCard)
-  -- Go on UI and do stuffs like (i.e: update viz-settings of a dashcard).
-  > (changes)
-  ;; => {:update {:report_dashboardcard ...}}
+    > (track! models/Dashboard models/Card models/DashboardCard)
+    -- Go on UI and do stuffs like (i.e: update viz-settings of a dashcard).
+
+    > (changes)
+    ;; => {:report_card {:insert ...}}
+
   You can use [[reset-tracking!]] to clear our all the current trackings.
   And [[untrack-all!]] or [[untrack!]] to stop tracking."
   (:require
@@ -26,24 +29,27 @@
 
 (defn new-tracking
   "Add a tracking to the [[tracking]] atom.
+
     > (new-tracking :insert (t2/instance :models/Card {:name \"new card\"}))
     instance
+
     > @tracking
     {:insert {:report_card [{:name \"new card\"}]}]}.
+
   For insert, track the instance as a map.
   For update, only track the changes."
   [action instance]
   (let [track-item (->> (case action
-                          :insert
-                          (into {} instance)
                           :update
-                          (into {} (t2/changes instance)))
+                          (into {} (t2/changes instance))
+                          (into {} instance))
                        clean-tracking)
         path       [(t2/table-name instance) action]]
     ;; ideally this should be debug, but for some reasons this doesn't get logged
     (println (u/colorize 'green :new-tracking) path)
     (pprint/pprint track-item)
-    (swap! tracking update-in path conj track-item)))
+    (swap! tracking update-in path concat [track-item])))
+
 
 (t2/define-after-insert ::tracking
   [instance]
@@ -57,13 +63,15 @@
   instance)
 
 (m/prefer-method! #'toucan2.tools.before-update/before-update
-                                :metabase.models.interface/timestamped?
-                                ::tracking)
+                  :metabase.models.interface/timestamped?
+                  ::tracking)
 
 ;; define-after-delete haven't supported https://github.com/camsaul/toucan2/issues/70
-#_(t2/define-after-delete ::tracking
+#_(t2/define-before-delete ::tracking
     [instance]
-    (new-tracking :delete instance)
+    ;; BUG ?
+    ;; for some reasons instance = `true`?
+    #_(new-tracking :delete instance)
     instance)
 
 (defn reset-tracking!
@@ -101,4 +109,5 @@
 
   (reset-tracking!)
 
-  (untrack! models/Dashboard models/Card models/DashboardCard))
+  (untrack! models/Dashboard models/Card models/DashboardCard)
+  (untrack-all!))
