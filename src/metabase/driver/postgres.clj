d17d5b613fd48f1792c4446522d71c09a8868f22
dan sutton <dan@dpsutton.com>
2023-02-23 16:53:49 -0600

    Let's not do this yet
    i think this is the correct call but want more tests on it when we flip
    the switch

--- src/metabase/driver/postgres.clj
+++ src/metabase/driver/postgres.clj
@@ -575,8 +575,8 @@
    :int4          :type/Integer
    :int8          :type/BigInteger
    :interval      :type/*               ; time span
-   :json          :type/JSON
-   :jsonb         :type/JSON
+   :json          :type/Structured
+   :jsonb         :type/Structured
    :line          :type/*
    :lseg          :type/*
    :macaddr       :type/Structured
