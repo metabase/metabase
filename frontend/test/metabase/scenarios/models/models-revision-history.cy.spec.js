e06a47f07739c5ba5b0cb70e8ed630be00d6c0c2
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2023-02-24 11:04:28 +0100

    Greatly simplify test
    Do not test things out we're covering elsewhere

--- frontend/test/metabase/scenarios/models/models-revision-history.cy.spec.js
+++ frontend/test/metabase/scenarios/models/models-revision-history.cy.spec.js
@@ -1,20 +1,4 @@
-import {
-  restore,
-  modal,
-  filter,
-  filterField,
-  visitQuestion,
-  openQuestionActions,
-  closeQuestionActions,
-  questionInfoButton,
-} from "__support__/e2e/helpers";
-
-import {
-  assertIsModel,
-  assertQuestionIsBasedOnModel,
-  saveQuestionBasedOnModel,
-  assertIsQuestion,
-} from "./helpers/e2e-models-helpers";
+import { restore, questionInfoButton } from "__support__/e2e/helpers";
 
 describe("scenarios > models > revision history", () => {
   beforeEach(() => {
@@ -27,8 +11,6 @@ describe("scenarios > models > revision history", () => {
       name: "Orders Model",
       dataset: true,
     });
-    cy.intercept("PUT", "/api/card/3").as("updateCard");
-    cy.intercept("POST", "/api/revision/revert").as("revertToRevision");
   });
 
   it("should allow reverting to a saved question state", () => {
@@ -38,69 +20,14 @@ describe("scenarios > models > revision history", () => {
     revertTo("You created this");
     cy.wait("@dataset");
 
-    openQuestionActions();
-    assertIsQuestion();
+    cy.location("pathname").should("match", /^\/question\/3/);
     cy.get(".LineAreaBarChart");
 
-    filter();
-    filterField("Discount", {
-      operator: "Not empty",
-    });
+    revertTo("^Turned this into a model");
+    cy.wait("@dataset");
 
-    cy.findByTestId("apply-filters").click();
-
-    cy.findByText("Save").click();
-    modal().within(() => {
-      cy.findByText(/Replace original question/i);
-    });
-  });
-
-  it("should allow reverting to a model state", () => {
-    cy.request("PUT", "/api/card/3", { dataset: false });
-
-    visitQuestion(3);
-    openQuestionActions();
-    assertIsQuestion();
-    closeQuestionActions();
-
-    openRevisionHistory();
-
-    cy.findByText(/Turned this into a model/i)
-      .closest("li")
-      .within(() => {
-        cy.findByTestId("question-revert-button").click();
-      });
-    cy.wait("@revertToRevision");
-
-    openQuestionActions();
-    assertIsModel();
-    closeQuestionActions();
-
-    cy.get(".LineAreaBarChart").should("not.exist");
-
-    filter();
-    filterField("Count", {
-      placeholder: "min",
-      value: "2000",
-    });
-    cy.findByTestId("apply-filters").click();
-
-    assertQuestionIsBasedOnModel({
-      model: "Orders Model",
-      collection: "Our analytics",
-      table: "Orders",
-    });
-
-    saveQuestionBasedOnModel({ modelId: 3, name: "Q1" });
-
-    assertQuestionIsBasedOnModel({
-      questionName: "Q1",
-      model: "Orders Model",
-      collection: "Our analytics",
-      table: "Orders",
-    });
-
-    cy.url().should("not.include", "/question/3");
+    cy.location("pathname").should("match", /^\/model\/3/);
+    cy.get(".cellData");
   });
 });
 
@@ -114,6 +41,7 @@ function openRevisionHistory() {
   cy.intercept("GET", "/api/user").as("user");
   questionInfoButton().click();
   cy.wait("@user");
+
   cy.findByText("History");
 }
 
