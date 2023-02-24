19a94ded9b69830c38f0819e8f443e4868d0d400
Callum Herries <hi@callumherries.com>
2023-02-24 18:32:01 +0200

    Fix test

--- test/metabase/models/database_test.clj
+++ test/metabase/models/database_test.clj
@@ -95,21 +95,20 @@
                (encode-decode pg-db)))))))
 
 (deftest driver-supports-actions-and-database-enable-actions-test
-  (testing "Updating database-enable-actions to true should fail if the engine doesn't support actions"
-    (mt/with-temp Database [{db-id :id :as database} {:engine :sqlite}]
-      (is (= false (driver/database-supports? :sqlite :actions database)))
-      (is (thrown-with-msg?
-           clojure.lang.ExceptionInfo
-           #"The database does not support actions."
-           (db/update! Database db-id :settings {:database-enable-actions true})))))
-  (testing "Updating the engine when database-enable-actions is true should fail if the engine doesn't support actions"
-    (mt/with-temp Database [{db-id :id :as database} {:engine :h2 :settings {:database-enable-actions true}}]
-      (is (= true (driver/database-supports? :h2 :actions database)))
-      (is (= false (driver/database-supports? :sqlite :actions database)))
-      (is (thrown-with-msg?
-           clojure.lang.ExceptionInfo
-           #"The database does not support actions."
-           (db/update! Database db-id :engine :sqlite))))))
+  (mt/with-test-drivers #{:sqlite}
+    (testing "Updating database-enable-actions to true should fail if the engine doesn't support actions"
+      (mt/with-temp Database [database {:engine :sqlite}]
+        (is (= false (driver/database-supports? :sqlite :actions database)))
+        (is (thrown-with-msg?
+             clojure.lang.ExceptionInfo
+             #"The database does not support actions."
+             (db/update! Database (:id database) :settings {:database-enable-actions true})))))
+    (testing "Updating the engine when database-enable-actions is true should fail if the engine doesn't support actions"
+      (mt/with-temp Database [database {:engine :h2 :settings {:database-enable-actions true}}]
+        (is (thrown-with-msg?
+             clojure.lang.ExceptionInfo
+             #"The database does not support actions."
+             (db/update! Database (:id database) :engine :sqlite)))))))
 
 (deftest sensitive-data-redacted-test
   (let [encode-decode (fn [obj] (decode (encode obj)))
