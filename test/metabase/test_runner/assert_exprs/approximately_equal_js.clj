c3a1507359c461b2f36a37f8ac47b841935d66e7
Cam Saul <github@camsaul.com>
2023-02-24 00:45:00 +0000

    Fix cljs test assertions

--- test/metabase/test_runner/assert_exprs/approximately_equal_js.clj
+++ test/metabase/test_runner/assert_exprs/approximately_equal_js.clj
@@ -1,14 +1,14 @@
-(ns metabase.test-runner.assert-exprs.approximately-equal-js
-  (:require [net.cgrand.macrovich :as macros]))
+(ns metabase.test-runner.assert-exprs.approximately-equal-js)
 
-(macros/case
-  :cljs
-  (do
-    (require 'cljs.test)
-    #_{:clj-kondo/ignore [:unresolved-namespace]}
-    (defmethod cljs.test/assert-expr '=?
-      [_env message [_ & form]]
-      (let [[expected actual] (case (count form)
-                                2 form
-                                (throw (ex-info "=? expects exactly 2 arguments" {:form form})))]
-        `(cljs.test/do-report (metabase.test-runner.assert-exprs.approximately-equal/=?-report ~message ~expected ~actual))))))
+;; this is only supposed to be a thing when doing macroexpansion FOR ClojureScript, and `cljs.test` isn't available in
+;; regular Clojure mode. For some reason I couldn't get Macrovich to do what I wanted here
+(try
+  (require 'cljs.test)
+  #_{:clj-kondo/ignore [:unresolved-namespace]}
+  (defmethod cljs.test/assert-expr '=?
+    [_env message [_ & form]]
+    (let [[expected actual] (case (count form)
+                              2 form
+                              (throw (ex-info "=? expects exactly 2 arguments" {:form form})))]
+      `(cljs.test/do-report (metabase.test-runner.assert-exprs.approximately-equal/=?-report ~message ~expected ~actual))))
+  (catch Throwable _))
