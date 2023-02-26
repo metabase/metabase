2d0336af90fe03790c471f5e88fd9b70897f1d42
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2023-02-26 20:31:04 +0100

    Fix integration imports

--- e2e/support/integration/visit-dashboard.js
+++ e2e/support/integration/visit-dashboard.js
@@ -1,4 +1,4 @@
-import { SAMPLE_DATABASE } from "__support__/e2e/cypress_sample_database";
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 
 const { PEOPLE_ID, PRODUCTS_ID, PRODUCTS } = SAMPLE_DATABASE;
 
