a7eee78716eafc3c5aa9759187929e362bf6cf5b
Ryan Laurie <iethree@gmail.com>
2023-02-27 13:14:44 -0700

    reduce some flakes

--- frontend/test/metabase/scenarios/dashboard/actions-on-dashboards.cy.spec.js
+++ frontend/test/metabase/scenarios/dashboard/actions-on-dashboards.cy.spec.js
@@ -494,6 +494,8 @@ function createDashboardWithActionButton({
     cy.button("Pick an action").click();
   });
 
+  cy.wait("@getActions");
+
   cy.findByRole("dialog").within(() => {
     cy.findByText(modelName).click();
     cy.findByText(actionName).click();
