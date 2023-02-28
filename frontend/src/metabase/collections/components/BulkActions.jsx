e384164a73e66d7624b20aa8f9be4b183908939b
Emmad Usmani <emmadusmani@berkeley.edu>
2023-02-28 13:01:51 -0800

    update `BulkActions` to new design

--- frontend/src/metabase/collections/components/BulkActions.jsx
+++ frontend/src/metabase/collections/components/BulkActions.jsx
@@ -3,55 +3,21 @@ import React from "react";
 import { t, msgid, ngettext } from "ttag";
 import _ from "underscore";
 
-import BulkActionBar from "metabase/components/BulkActionBar";
-import Button from "metabase/core/components/Button";
+import { Motion, spring } from "react-motion";
 import Modal from "metabase/components/Modal";
-import StackedCheckBox from "metabase/components/StackedCheckBox";
 
 import CollectionMoveModal from "metabase/containers/CollectionMoveModal";
 import CollectionCopyEntityModal from "metabase/collections/components/CollectionCopyEntityModal";
 
 import { ANALYTICS_CONTEXT } from "metabase/collections/constants";
 import { canArchiveItem, canMoveItem } from "metabase/collections/utils";
+import { NAV_SIDEBAR_WIDTH_HALF } from "metabase/nav/constants";
 import {
-  ActionBarContent,
-  ActionBarText,
-  ActionControlsRoot,
+  BulkActionsToast,
+  CardButton,
+  CardSide,
+  ToastCard,
 } from "./BulkActions.styled";
-
-const BulkActionControls = ({ onArchive, onMove }) => (
-  <ActionControlsRoot>
-    <Button
-      ml={1}
-      medium
-      disabled={!onArchive}
-      onClick={onArchive}
-      data-metabase-event={`${ANALYTICS_CONTEXT};Bulk Actions;Archive Items`}
-    >{t`Archive`}</Button>
-    <Button
-      ml={1}
-      medium
-      disabled={!onMove}
-      onClick={onMove}
-      data-metabase-event={`${ANALYTICS_CONTEXT};Bulk Actions;Move Items`}
-    >{t`Move`}</Button>
-  </ActionControlsRoot>
-);
-
-const SelectionControls = ({
-  selected,
-  hasUnselected,
-  onSelectAll,
-  onSelectNone,
-  size = 18,
-}) =>
-  !hasUnselected ? (
-    <StackedCheckBox checked onChange={onSelectNone} size={size} />
-  ) : selected.length === 0 ? (
-    <StackedCheckBox onChange={onSelectAll} size={size} />
-  ) : (
-    <StackedCheckBox checked indeterminate onChange={onSelectAll} size={size} />
-  );
 
 function BulkActions(props) {
   const {
@@ -67,27 +33,66 @@ function BulkActions(props) {
     isNavbarOpen,
   } = props;
 
-  const canMove = selected.every(item => canMoveItem(item, collection));
-  const canArchive = selected.every(item => canArchiveItem(item, collection));
+  const onMoveClick = selected.every(item => canMoveItem(item, collection))
+    ? onMoveStart
+    : null;
+  const onArchiveClick = selected.every(item =>
+    canArchiveItem(item, collection),
+  )
+    ? onArchive
+    : null;
+
+  const showing = selected.length > 0;
 
   return (
-    <BulkActionBar showing={selected.length > 0} isNavbarOpen={isNavbarOpen}>
+    <>
       {/* NOTE: these padding and grid sizes must be carefully matched
-                   to the main content above to ensure the bulk checkbox lines up */}
-      <ActionBarContent>
-        <SelectionControls {...props} />
-        <BulkActionControls
-          onArchive={canArchive ? onArchive : null}
-          onMove={canMove ? onMoveStart : null}
-        />
-        <ActionBarText>
-          {ngettext(
-            msgid`${selected.length} item selected`,
-            `${selected.length} items selected`,
-            selected.length,
-          )}
-        </ActionBarText>
-      </ActionBarContent>
+      to the main content above to ensure the bulk checkbox lines up */}
+      <Motion
+        defaultStyle={{
+          opacity: 0,
+          translateY: 0,
+        }}
+        style={{
+          opacity: showing ? spring(1) : spring(0),
+          translateY: showing ? spring(0) : spring(100),
+        }}
+      >
+        {({ translateY }) => (
+          <BulkActionsToast
+            style={{
+              transform: `translate(-50%, ${translateY}px)`,
+              marginLeft: isNavbarOpen ? NAV_SIDEBAR_WIDTH_HALF : 0,
+            }}
+          >
+            <ToastCard dark>
+              <CardSide>
+                {ngettext(
+                  msgid`${selected.length} item selected`,
+                  `${selected.length} items selected`,
+                  selected.length,
+                )}
+              </CardSide>
+              <CardSide>
+                <CardButton
+                  medium
+                  purple
+                  disabled={!onMoveClick}
+                  onClick={onMoveClick}
+                  data-metabase-event={`${ANALYTICS_CONTEXT};Bulk Actions;Move Items`}
+                >{t`Move`}</CardButton>
+                <CardButton
+                  medium
+                  purple
+                  disabled={!onArchiveClick}
+                  onClick={onArchiveClick}
+                  data-metabase-event={`${ANALYTICS_CONTEXT};Bulk Actions;Archive Items`}
+                >{t`Archive`}</CardButton>
+              </CardSide>
+            </ToastCard>
+          </BulkActionsToast>
+        )}
+      </Motion>
       {!_.isEmpty(selectedItems) && selectedAction === "copy" && (
         <Modal onClose={onCloseModal}>
           <CollectionCopyEntityModal
@@ -113,7 +118,7 @@ function BulkActions(props) {
           />
         </Modal>
       )}
-    </BulkActionBar>
+    </>
   );
 }
 
--- frontend/src/metabase/nav/constants.ts
+++ frontend/src/metabase/nav/constants.ts
@@ -3,3 +3,4 @@ export const APP_SUBHEADER_HEIGHT = "48px";
 export const APP_BAR_EXTENDED_HEIGHT = "98px";
 export const ADMIN_NAVBAR_HEIGHT = "65px";
 export const NAV_SIDEBAR_WIDTH = "324px";
+export const NAV_SIDEBAR_WIDTH_HALF = "162px";
