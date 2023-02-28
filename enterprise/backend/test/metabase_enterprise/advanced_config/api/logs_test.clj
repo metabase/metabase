6f66d79ecf041a69fe9334bb4a8ec88b8f7145d3
Adam James <adam.vermeer2@gmail.com>
2023-02-27 16:13:22 -0800

    Wrap logs endpoint in proper premium feature

--- enterprise/backend/test/metabase_enterprise/advanced_config/api/logs_test.clj
+++ enterprise/backend/test/metabase_enterprise/advanced_config/api/logs_test.clj
@@ -4,6 +4,7 @@
    [clojure.test :refer :all]
    [java-time :as t]
    [metabase.models.query-execution :refer [QueryExecution]]
+   [metabase.public-settings.premium-features-test :as premium-features.test]
    [metabase.query-processor.util :as qp.util]
    [metabase.test :as mt]))
 
@@ -27,8 +28,9 @@
                       QueryExecution [_qe-b (merge query-execution-defaults
                                                    {:executor_id user-id
                                                     :started_at  (t/minus now (t/days 32))})]]
-        (is (= [(select-keys qe-a [:started_at :id])]
-               (->> (mt/user-http-request test-user :get 200 "ee/logs/query_execution/30")
-                    (filter #(#{user-id} (:executor_id %)))
-                    (map #(select-keys % [:started_at :id]))))
-            "Only Query Executions within `:days` are returned.")))))
+        (premium-features.test/with-premium-features #{:advanced-config}
+          (is (= [(select-keys qe-a [:started_at :id])]
+                 (->> (mt/user-http-request test-user :get 200 "ee/logs/query_execution/30")
+                      (filter #(#{user-id} (:executor_id %)))
+                      (map #(select-keys % [:started_at :id]))))
+              "Only Query Executions within `:days` are returned."))))))
