dd0d65128cf6af168a924e08522ea60a5d1a6ae6
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-02-28 14:51:08 +0200

    Add upgrade url selector

--- frontend/src/metabase/selectors/settings.ts
+++ frontend/src/metabase/selectors/settings.ts
@@ -12,3 +12,29 @@ export const getSetting = <T extends SettingKey>(
   state: State,
   key: T,
 ): Settings[T] => getSettings(state)[key];
+
+interface UpgradeUrlOpts {
+  utm_media: string;
+}
+
+export const getUpgradeUrl = createSelector(
+  (state: State) => getUtmSource(state),
+  (state: State, opts: UpgradeUrlOpts) => opts.utm_media,
+  (source, media) => {
+    const url = new URL("https://www.metabase.com/upgrade/");
+    url.searchParams.append("utm_source", source);
+    url.searchParams.append("utm_media", media);
+
+    return url.toString();
+  },
+);
+
+const getUtmSource = (state: State) => {
+  const features = getSetting(state, "token-features");
+
+  if (features.sso) {
+    return features.hosting ? "pro-cloud" : "pro-self-hosted";
+  } else {
+    return features.hosting ? "starter" : "oss";
+  }
+};
