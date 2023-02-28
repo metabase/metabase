dc5ab242761e1ba00d5b512b01c59490a9d9ad81
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-02-28 16:49:35 +0200

    Add tests

---
+++ frontend/src/metabase/admin/settings/components/widgets/FullAppEmbeddingLinkWidget/FullAppEmbeddingLinkWidget.unit.spec.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { renderWithProviders, screen } from "__support__/ui";
+import FullAppEmbeddingLinkWidget from "./FullAppEmbeddingLinkWidget";
+
+const setup = () => {
+  renderWithProviders(<FullAppEmbeddingLinkWidget />);
+};
+
+describe("FullAppEmbeddingLinkWidget", () => {
+  it("should add utm_media to the upgrade link", () => {
+    setup();
+
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      expect.stringContaining("embed_fullapp"),
+    );
+  });
+});
