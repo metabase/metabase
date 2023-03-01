8bfa0039ae5b25692ee941f7826d2f52b7520fa8
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-01 13:44:13 +0200

    Add tests

--- frontend/src/metabase/selectors/selectors.unit.spec.ts
+++ frontend/src/metabase/selectors/selectors.unit.spec.ts
@@ -7,17 +7,39 @@ import { getUpgradeUrl } from "./settings";
 
 describe("getUpgradeUrl", () => {
   it.each([
-    { hosting: false, sso: false, path: "utm_source=oss" },
-    { hosting: true, sso: false, path: "utm_source=starter" },
-    { hosting: false, sso: true, path: "utm_source=pro-self-hosted" },
-    { hosting: true, sso: true, path: "utm_source=pro-cloud" },
-  ])("should set utm_source", ({ hosting, sso, path }) => {
+    {
+      media: "license",
+      users: 1,
+      features: { hosting: false, sso: false },
+      params: "?utm_media=license&utm_source=oss&utm_users=1",
+    },
+    {
+      media: "permissions_top",
+      users: 2,
+      features: { hosting: true, sso: false },
+      params: "?utm_media=permissions_top&utm_source=starter&utm_users=2",
+    },
+    {
+      media: "license",
+      users: 3,
+      features: { hosting: false, sso: true },
+      params: "?utm_media=license&utm_source=pro-self-hosted&utm_users=3",
+    },
+    {
+      media: "license",
+      users: undefined,
+      features: { hosting: true, sso: true },
+      params: "?utm_media=license&utm_source=pro-cloud",
+    },
+  ])("should set utm_source", ({ media, features, users, params }) => {
     const state = createMockState({
       settings: createMockSettingsState({
-        "token-features": createMockTokenFeatures({ hosting, sso }),
+        "token-features": createMockTokenFeatures(features),
+        "active-users-count": users,
       }),
     });
 
-    expect(getUpgradeUrl(state, { utm_media: "license" })).toContain(path);
+    const url = new URL(getUpgradeUrl(state, { utm_media: media }));
+    expect(url.search).toEqual(params);
   });
 });
