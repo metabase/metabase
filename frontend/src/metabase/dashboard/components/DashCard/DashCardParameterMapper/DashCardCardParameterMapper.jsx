38d6dd7ad58b744f65c28a7bf45f799e2320a445
Ryan Laurie <iethree@gmail.com>
2023-03-01 20:40:10 +0100

    hide action parameter mappings on dashcards

--- frontend/src/metabase/dashboard/components/DashCard/DashCardParameterMapper/DashCardCardParameterMapper.jsx
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardParameterMapper/DashCardCardParameterMapper.jsx
@@ -20,6 +20,7 @@ import {
   showVirtualDashCardInfoText,
 } from "metabase/dashboard/utils";
 
+import { isActionDashCard } from "metabase/actions/utils";
 import Question from "metabase-lib/Question";
 import { isDateParameter } from "metabase-lib/parameters/utils/parameter-type";
 import { isVariableTarget } from "metabase-lib/parameters/utils/targets";
@@ -93,7 +94,7 @@ function DashCardCardParameterMapper({
   const hasSeries = dashcard.series && dashcard.series.length > 0;
   const onlyAcceptsSingleValue =
     isVariableTarget(target) && !isDateParameter(editingParameter);
-  const isDisabled = mappingOptions.length === 0;
+  const isDisabled = mappingOptions.length === 0 || isActionDashCard(dashcard);
   const selectedMappingOption = _.find(mappingOptions, o =>
     _.isEqual(o.target, target),
   );
