b074470cf10c597cd1b762464b73b77d09df7cfa
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-02 14:55:39 +0200

    Type fix

--- frontend/test/__support__/sample_database_fixture.ts
+++ frontend/test/__support__/sample_database_fixture.ts
@@ -64,7 +64,7 @@ export function createMetadata(updateState = (state: EnhancedState) => state) {
   const stateModified = updateState(chain(state)).thaw().value();
 
   stateModified.entities.fields = normalizeFields(
-    (stateModified.entities.fields as Record<string, IField>) || {},
+    stateModified.entities.fields || {},
   );
 
   const metadata = getMetadata(stateModified);
