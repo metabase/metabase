b4b753f86938a265eac3b0e36a2e5b388d888215
Callum Herries <hi@callumherries.com>
2023-03-02 16:48:42 +0200

    Update json-unfolding database setting copy

--- src/metabase/driver/common.clj
+++ src/metabase/driver/common.clj
@@ -174,12 +174,12 @@
 (def json-unfolding
   "Map representing the `json-unfolding` option in a DB connection form"
   {:name         "json-unfolding"
-   :display-name (deferred-tru "Unfold JSON Columns")
+   :display-name (deferred-tru "Unfold JSON Columns by default")
    :type         :boolean
    :visible-if   {"advanced-options" true}
    :description  (deferred-tru
-                   (str "We unfold JSON columns into component fields."
-                        "This is on by default but you can turn it off if performance is slow."))
+                   (str "This enables unfolding JSON columns into component fields by default."
+                        "You can turn this off if performance is slow, and you can override this setting on each field."))
    :default      true})
 
 (def refingerprint
