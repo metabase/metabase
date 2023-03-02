80b236cc965d77af6317c299612c16cefc4c2216
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-02 13:11:56 +0200

    Add utm_media, utm_source, and utm_users to upgrade urls (#28738)

--- frontend/src/metabase/admin/settings/selectors.js
+++ frontend/src/metabase/admin/settings/selectors.js
@@ -14,7 +14,7 @@ import CustomGeoJSONWidget from "./components/widgets/CustomGeoJSONWidget";
 import SettingsLicense from "./components/SettingsLicense";
 import SiteUrlWidget from "./components/widgets/SiteUrlWidget";
 import HttpsOnlyWidget from "./components/widgets/HttpsOnlyWidget";
-import { EmbeddingCustomizationInfo } from "./components/widgets/EmbeddingCustomizationInfo";
+import EmbeddingCustomizationInfo from "./components/widgets/EmbeddingCustomizationInfo";
 import {
   PublicLinksDashboardListing,
   PublicLinksQuestionListing,
@@ -25,7 +25,7 @@ import {
 import SecretKeyWidget from "./components/widgets/SecretKeyWidget";
 import EmbeddingLegalese from "./components/widgets/EmbeddingLegalese";
 import FormattingWidget from "./components/widgets/FormattingWidget";
-import { PremiumEmbeddingLinkWidget } from "./components/widgets/PremiumEmbeddingLinkWidget";
+import FullAppEmbeddingLinkWidget from "./components/widgets/FullAppEmbeddingLinkWidget";
 import ModelCachingScheduleWidget from "./components/widgets/ModelCachingScheduleWidget";
 import SectionDivider from "./components/widgets/SectionDivider";
 import SettingsUpdatesForm from "./components/SettingsUpdatesForm/SettingsUpdatesForm";
@@ -473,7 +473,7 @@ const SECTIONS = updateSectionsWithPlugins({
         },
       },
       {
-        widget: PremiumEmbeddingLinkWidget,
+        widget: FullAppEmbeddingLinkWidget,
         getHidden: (_, derivedSettings) =>
           !derivedSettings["enable-embedding"] ||
           MetabaseSettings.isEnterprise(),
