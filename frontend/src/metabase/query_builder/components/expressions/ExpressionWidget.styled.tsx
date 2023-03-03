f3ef8e67f4af46d11d927fde90ed9f70742ad019
Denis Berezin <denis.berezin@metabase.com>
2023-03-03 22:49:47 +0400

    Implement new UI design, add unit tests (#28776)
    * Add design changes to ExpressionWidget
    
    * Add unit tests

--- frontend/src/metabase/query_builder/components/expressions/ExpressionWidget.styled.tsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionWidget.styled.tsx
@@ -1,5 +1,65 @@
 import styled from "@emotion/styled";
+import { color } from "metabase/lib/colors";
+import Icon from "metabase/components/Icon";
+import Button from "metabase/core/components/Button";
+import IconButtonWrapper from "metabase/components/IconButtonWrapper";
 
-export const ExpressionWidgetWrapper = styled.div`
+export const Container = styled.div`
   width: 445px;
+`;
+
+export const FieldWrapper = styled.div`
+  padding: 0 1.5rem 1.5rem;
+`;
+
+export const ExpressionFieldWrapper = styled.div`
+  padding: 1.5rem 1.5rem 1rem;
+`;
+
+export const FieldTitle = styled.div`
+  display: flex;
+  margin-bottom: 0.5rem;
+
+  font-weight: 700;
+  font-size: 0.83em;
+  text-transform: uppercase;
+  letter-spacing: 0.06em;
+
+  color: ${color("text-light")};
+`;
+
+export const IconWrapper = styled(IconButtonWrapper)`
+  margin-left: 4px;
+`;
+
+export const StyledFieldTitleIcon = styled(Icon)`
+  width: 10px;
+  height: 10px;
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${color("border")};
+  margin-top: 0.5rem;
+`;
+
+export const Footer = styled.div`
+  padding: 1.5rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const RemoveLink = styled(Button)`
+  padding-right: 1rem;
+`;
+
+export const ActionButtonsWrapper = styled.div`
+  margin-left: auto;
+
+  display: flex;
+  gap: 1rem;
 `;
