48ebad9d750835b97536220bd41afe155391cd8a
Anton Kulyk <kuliks.anton@gmail.com>
2023-03-07 17:17:01 +0000

    Add e2e test

--- frontend/src/metabase/containers/UndoListing.jsx
+++ frontend/src/metabase/containers/UndoListing.jsx
@@ -71,7 +71,10 @@ function UndoToast({ undo, onUndo, onDismiss }) {
             </CardContentSide>
             <CardContentSide>
               {undo.actions?.length > 0 && (
-                <UndoButton onClick={onUndo}>{t`Undo`}</UndoButton>
+                <UndoButton
+                  role="button"
+                  onClick={onUndo}
+                >{t`Undo`}</UndoButton>
               )}
               <DismissIcon name="close" onClick={onDismiss} />
             </CardContentSide>
