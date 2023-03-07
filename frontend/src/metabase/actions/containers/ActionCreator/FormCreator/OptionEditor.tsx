31b71da66b7620ab9cf90f4c8182bdb8a42904e7
Anton Kulyk <kuliks.anton@gmail.com>
2023-03-07 18:08:21 +0000

    Validate options for numeric fields with dropdown input

--- frontend/src/metabase/actions/containers/ActionCreator/FormCreator/OptionEditor.tsx
+++ frontend/src/metabase/actions/containers/ActionCreator/FormCreator/OptionEditor.tsx
@@ -5,29 +5,72 @@ import TippyPopoverWithTrigger from "metabase/components/PopoverWithTrigger/Tipp
 import Button from "metabase/core/components/Button";
 import Icon from "metabase/components/Icon";
 
+import type { FieldType } from "metabase-types/api";
+
 import {
   OptionEditorContainer,
   AddMorePrompt,
+  ErrorMessage,
   TextArea,
 } from "./OptionEditor.styled";
 
-type ValueOptions = (string | number)[];
+export type ValueOptions = (string | number)[];
 
 const optionsToText = (options: ValueOptions) => options.join("\n");
 const textToOptions = (text: string): ValueOptions =>
   text.split("\n").map(option => option.trim());
 
-export const OptionPopover = ({
-  options,
-  onChange,
-}: {
+function cleanOptions(options: ValueOptions, fieldType: FieldType) {
+  if (fieldType === "number") {
+    return options.map(option => Number(option));
+  }
+  return options;
+}
+
+function validateOptions(options: ValueOptions, fieldType: FieldType) {
+  if (fieldType === "number") {
+    const isValid = options.every(option => !Number.isNaN(option));
+    return isValid ? undefined : t`Invalid number format`;
+  }
+  return;
+}
+
+export interface OptionEditorProps {
+  fieldType: FieldType;
   options: ValueOptions;
   onChange: (options: ValueOptions) => void;
-}) => {
+}
+
+export const OptionPopover = ({
+  fieldType,
+  options,
+  onChange,
+}: OptionEditorProps) => {
   const [text, setText] = useState(optionsToText(options));
-  const save = (closePopover: () => void) => {
-    onChange(textToOptions(text));
-    closePopover();
+  const [error, setError] = useState<string | null>(null);
+
+  const hasOptions = text.length > 0;
+  const isDirty = text !== optionsToText(options);
+  const hasError = Boolean(error);
+  const canSave = hasOptions && isDirty && !hasError;
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+    if (hasError) {
+      setError(null);
+    }
+  };
+
+  const handleSave = (closePopover: () => void) => {
+    setError(null);
+    const nextOptions = cleanOptions(textToOptions(text), fieldType);
+    const error = validateOptions(nextOptions, fieldType);
+    if (error) {
+      setError(error);
+    } else {
+      onChange(nextOptions);
+      closePopover();
+    }
   };
 
   return (
@@ -41,13 +84,18 @@ export const OptionPopover = ({
         <OptionEditorContainer>
           <TextArea
             value={text}
-            onChange={e => setText(e.target.value)}
+            onChange={handleTextChange}
             placeholder={t`Enter one option per line`}
           />
-          <AddMorePrompt style={{ opacity: text.length ? 1 : 0 }}>
+          <AddMorePrompt isVisible={hasOptions}>
             {t`Press enter to add another option`}
           </AddMorePrompt>
-          <Button onClick={() => save(closePopover)} small>
+          {hasError && <ErrorMessage>{error}</ErrorMessage>}
+          <Button
+            disabled={!canSave}
+            onClick={() => handleSave(closePopover)}
+            small
+          >
             {t`Save`}
           </Button>
         </OptionEditorContainer>
