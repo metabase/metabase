3e6a240bac0188daf69de5c27e9e82ca0d9318ea
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2023-03-08 10:48:49 -0700

    Improved datetime handling in Action Forms 🥴  (#29002)
    * improved datetime handling
    
    * 💪 more tests 💪

--- frontend/src/metabase/actions/containers/ActionParametersInputForm/utils.unit.spec.ts
+++ frontend/src/metabase/actions/containers/ActionParametersInputForm/utils.unit.spec.ts
@@ -1,6 +1,28 @@
-import { getChangedValues, formatValue } from "./utils";
+import { getChangedValues, formatValue, stripTZInfo } from "./utils";
 
 describe("actions > containers > ActionParametersInputForm > utils", () => {
+  describe("stripTZInfo", () => {
+    it("should strip timezone info from dateTimes", () => {
+      const result = stripTZInfo("2020-05-01T03:30:00-02:00");
+      expect(result.format()).toEqual("2020-05-01T03:30:00Z");
+    });
+
+    it("should strip timezone info from times", () => {
+      const result = stripTZInfo("2020-05-01T03:30:00+08:00");
+      expect(result.format()).toEqual("2020-05-01T03:30:00Z");
+    });
+
+    it("should not do anything to strings without time info", () => {
+      const result = stripTZInfo("2020-05-01");
+      expect(result.format()).toEqual("2020-05-01T00:00:00Z");
+    });
+
+    it("should not do anything to strings without timezone info", () => {
+      const result = stripTZInfo("2020-05-01T03:30:00");
+      expect(result.format()).toEqual("2020-05-01T03:30:00Z");
+    });
+  });
+
   describe("formatValue", () => {
     it("ignores null values", () => {
       const result = formatValue(null);
@@ -17,14 +39,28 @@ describe("actions > containers > ActionParametersInputForm > utils", () => {
       expect(result).toEqual("123");
     });
 
-    it("formats dates", () => {
-      const result = formatValue("2020-05-01T00:00:00Z", "date");
-      expect(result).toEqual("2020-05-01");
-    });
+    const timezones = ["-02:00", "-07:00", "+01:00", "+09:00"];
 
-    it("formats datetimes", () => {
-      const result = formatValue("2020-05-01T05:00:00Z", "datetime");
-      expect(result).toEqual("2020-05-01T05:00:00");
+    timezones.forEach(offset => {
+      describe(`with timezone ${offset}`, () => {
+        it("formats dates", () => {
+          const result = formatValue(`2020-05-01T00:00:00${offset}`, "date");
+          expect(result).toEqual("2020-05-01");
+        });
+
+        it("formats datetimes", () => {
+          const result = formatValue(
+            `2020-05-01T05:00:00${offset}`,
+            "datetime",
+          );
+          expect(result).toEqual("2020-05-01T05:00:00");
+        });
+
+        it("formats times", () => {
+          const result = formatValue(`05:25:30${offset}`, "time");
+          expect(result).toEqual("05:25:30");
+        });
+      });
     });
   });
 
