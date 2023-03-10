a0bff4803adfe5e026b07b144d6a46722b9c0f6b
Ngoc Khuat <qn.khuat@gmail.com>
2023-03-10 16:42:13 +0700

    fix to run test against fake db and subscription test

--- enterprise/backend/test/metabase_enterprise/advanced_config/api/logs_test.clj
+++ enterprise/backend/test/metabase_enterprise/advanced_config/api/logs_test.clj
@@ -3,6 +3,7 @@
   (:require
    [clojure.test :refer :all]
    [java-time :as t]
+   [metabase-enterprise.advanced-config.api.logs :as ee.api.logs]
    [metabase.models.query-execution :refer [QueryExecution]]
    [metabase.public-settings.premium-features-test :as premium-features.test]
    [metabase.query-processor.util :as qp.util]
@@ -31,14 +32,22 @@
                         QueryExecution [qe-b (merge query-execution-defaults
                                                     {:executor_id user-id
                                                      :started_at  (t/minus now (t/days 32))})]]
-          (testing "Query Executions within `:yyyy-mm` are returned."
-            (premium-features.test/with-premium-features #{:advanced-config}
+          (premium-features.test/with-premium-features #{:advanced-config}
+            (testing "Query Executions within `:yyyy-mm` are returned."
               (is (= [(select-keys qe-a [:started_at :id])]
-                     (->> (mt/user-http-request test-user :get 200 "ee/logs/query_execution/2023-02")
+                     ;; we're calling the function directly instead of calling the API
+                     ;; because we want the test to run against the empty h2 DB we bound above
+                     ;; Until we figure out how to completely re-bind a database for API calls
+                     ;; this should be enough
+                     (->> (ee.api.logs/query-execution-logs 2023 2)
                           (filter #(#{user-id} (:executor_id %)))
                           (filter #((set (map :id [qe-a qe-b])) (:id %)))
-                          (map #(select-keys % [:started_at :id])))))))
+                          (map #(select-keys % [:started_at :id]))))))
+
+            (testing "require admins"
+              (is (= "You don't have permissions to do that."
+                     (mt/user-http-request :rasta :get 403 "ee/logs/query_execution/2023-02")))))
           (testing "Logs endpoint only works when `:advanced-config` feature is available."
             (premium-features.test/with-premium-features #{}
               (is (= "This API endpoint is only enabled if you have a premium token with the :advanced-config feature."
-                     (mt/user-http-request test-user :get 402 "ee/logs/query_execution/2023-02"))))))))))
+                     (mt/user-http-request :crowberto :get 402 "ee/logs/query_execution/2023-02"))))))))))
