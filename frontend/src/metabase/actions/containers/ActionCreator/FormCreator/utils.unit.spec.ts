27864fed7ce23d35f581703faa3f1dbad5a5f56a
Anton Kulyk <kuliks.anton@gmail.com>
2023-03-17 19:14:23 +0000

    Break down `ActionForm` component (#29290)
    * Move `ActionFormFieldWidget` to action components
    
    * Move `inputTypeHasOptions` to shared utils
    
    * Move `FormFieldEditor` to `FormCreator`
    
    * Clean up `ActionForm` props
    
    * Clean up `FormCreator` props
    
    * Move most of `ActionForm` utils to shared utils
    
    * Move `reorderFields` to `FormCreator`
    
    * Extract `FormCreator` resps from `ActionForm`
    
    * Use default export for `ActionForm`

---
+++ frontend/src/metabase/actions/containers/ActionCreator/FormCreator/utils.unit.spec.ts
@@ -0,0 +1,20 @@
+import { getDefaultFieldSettings } from "metabase/actions/utils";
+import type { FieldSettingsMap } from "metabase-types/api";
+import { reorderFields } from "./utils";
+
+describe("actions > containers > ActionCreator > FormCreator > utils", () => {
+  describe("reorderFields", () => {
+    it("should reorder fields", () => {
+      const fields = {
+        a: getDefaultFieldSettings({ order: 0 }),
+        b: getDefaultFieldSettings({ order: 1 }),
+        c: getDefaultFieldSettings({ order: 2 }),
+      } as FieldSettingsMap;
+      // move b to index 0
+      const reorderedFields = reorderFields(fields, 1, 0);
+      expect(reorderedFields.a.order).toEqual(1);
+      expect(reorderedFields.b.order).toEqual(0);
+      expect(reorderedFields.c.order).toEqual(2);
+    });
+  });
+});
--- frontend/src/metabase/actions/utils.ts
+++ frontend/src/metabase/actions/utils.ts
@@ -1,28 +1,107 @@
 import { t } from "ttag";
+import * as Yup from "yup";
 
-import { getResponseErrorMessage } from "metabase/core/utils/errors";
+import * as Errors from "metabase/core/utils/errors";
 import { slugify, humanize } from "metabase/lib/formatting";
 import { isEmpty } from "metabase/lib/validate";
 
 import type {
   ActionDashboardCard,
+  ActionFormOption,
   ActionFormSettings,
   BaseDashboardOrderedCard,
   Card,
+  FieldType,
   FieldSettings,
   FieldSettingsMap,
+  InputComponentType,
+  InputSettingType,
   Parameter,
   ParameterId,
   ParametersForActionExecution,
   WritebackAction,
   WritebackActionBase,
   WritebackImplicitQueryAction,
+  WritebackParameter,
 } from "metabase-types/api";
 
 import { TYPE } from "metabase-lib/types/constants";
 import Field from "metabase-lib/metadata/Field";
 
-import type { FieldSettings as LocalFieldSettings } from "./types";
+import type {
+  ActionFormProps,
+  ActionFormFieldProps,
+  FieldSettings as LocalFieldSettings,
+} from "./types";
+
+type FieldPropTypeMap = Record<InputSettingType, InputComponentType>;
+
+const fieldPropsTypeMap: FieldPropTypeMap = {
+  string: "text",
+  text: "textarea",
+  date: "date",
+  datetime: "datetime-local",
+  time: "time",
+  number: "number",
+  boolean: "boolean",
+  select: "select",
+  radio: "radio",
+};
+
+const getOptionsFromArray = (
+  options: (number | string)[],
+): ActionFormOption[] => options.map(o => ({ name: o, value: o }));
+
+function getSampleOptions(fieldType: FieldType) {
+  return fieldType === "number"
+    ? getOptionsFromArray([1, 2, 3])
+    : getOptionsFromArray([t`Option One`, t`Option Two`, t`Option Three`]);
+}
+
+const AUTOMATIC_DATE_TIME_FIELDS = [
+  TYPE.CreationDate,
+  TYPE.CreationTemporal,
+  TYPE.CreationTime,
+  TYPE.CreationTimestamp,
+
+  TYPE.DeletionDate,
+  TYPE.DeletionTemporal,
+  TYPE.DeletionTime,
+  TYPE.DeletionTimestamp,
+
+  TYPE.UpdatedDate,
+  TYPE.UpdatedTemporal,
+  TYPE.UpdatedTime,
+  TYPE.UpdatedTimestamp,
+];
+
+const isAutomaticDateTimeField = (field: Field) => {
+  return AUTOMATIC_DATE_TIME_FIELDS.includes(field.semantic_type);
+};
+
+const isEditableField = (field: Field, parameter: Parameter) => {
+  const isRealField = typeof field.id === "number";
+  if (!isRealField) {
+    // Filters out custom, aggregated columns, etc.
+    return false;
+  }
+
+  if (field.isPK()) {
+    // Most of the time PKs are auto-generated,
+    // but there are rare cases when they're not
+    // In this case they're marked as `required`
+    return parameter.required;
+  }
+
+  if (isAutomaticDateTimeField(field)) {
+    return parameter.required;
+  }
+
+  return true;
+};
+
+export const inputTypeHasOptions = (inputType: InputSettingType) =>
+  ["select", "radio"].includes(inputType);
 
 export const sortActionParams =
   (formSettings: ActionFormSettings) => (a: Parameter, b: Parameter) => {
@@ -226,10 +305,106 @@ export function getActionExecutionMessage(
 
 export function getActionErrorMessage(error: unknown) {
   return (
-    getResponseErrorMessage(error) ??
+    Errors.getResponseErrorMessage(error) ??
     t`Something went wrong while executing the action`
   );
 }
+
+const getFormField = (
+  parameter: Parameter,
+  fieldSettings: LocalFieldSettings,
+) => {
+  if (
+    fieldSettings.field &&
+    !isEditableField(fieldSettings.field, parameter as Parameter)
+  ) {
+    return undefined;
+  }
+
+  const fieldProps: ActionFormFieldProps = {
+    name: parameter.id,
+    type: fieldPropsTypeMap[fieldSettings?.inputType] ?? "text",
+    title:
+      fieldSettings.title ||
+      fieldSettings.name ||
+      parameter["display-name"] ||
+      parameter.name ||
+      parameter.id,
+    description: fieldSettings.description ?? "",
+    placeholder: fieldSettings?.placeholder,
+    optional: !fieldSettings.required,
+    field: fieldSettings.field,
+  };
+
+  if (inputTypeHasOptions(fieldSettings.inputType)) {
+    fieldProps.options = fieldSettings.valueOptions?.length
+      ? getOptionsFromArray(fieldSettings.valueOptions)
+      : getSampleOptions(fieldSettings.fieldType);
+  }
+
+  return fieldProps;
+};
+
+export const getForm = (
+  parameters: WritebackParameter[] | Parameter[],
+  fieldSettings: Record<string, FieldSettings> = {},
+): ActionFormProps => {
+  const sortedParams = parameters.sort(
+    sortActionParams({ fields: fieldSettings } as ActionFormSettings),
+  );
+  return {
+    fields: sortedParams
+      ?.map(param => getFormField(param, fieldSettings[param.id] ?? {}))
+      .filter(Boolean) as ActionFormFieldProps[],
+  };
+};
+
+const getFieldValidationType = ({
+  inputType,
+  defaultValue,
+}: FieldSettings): Yup.AnySchema => {
+  switch (inputType) {
+    case "number":
+      return Yup.number()
+        .nullable()
+        .default(defaultValue != null ? Number(defaultValue) : null);
+    case "boolean":
+      return Yup.boolean()
+        .nullable()
+        .default(defaultValue != null ? Boolean(defaultValue) : false);
+    case "date":
+    case "datetime":
+    case "time":
+      return Yup.string()
+        .nullable()
+        .default(defaultValue != null ? String(defaultValue) : null);
+    default:
+      return Yup.string()
+        .nullable()
+        .default(defaultValue != null ? String(defaultValue) : null);
+  }
+};
+
+export const getFormValidationSchema = (
+  parameters: WritebackParameter[] | Parameter[],
+  fieldSettings: FieldSettingsMap = {},
+) => {
+  const schema = Object.values(fieldSettings)
+    .filter(fieldSetting =>
+      // only validate fields that are present in the form
+      parameters.find(parameter => parameter.id === fieldSetting.id),
+    )
+    .map(fieldSetting => {
+      let yupType = getFieldValidationType(fieldSetting);
+
+      if (fieldSetting.required) {
+        yupType = yupType.required(Errors.required);
+      }
+
+      return [fieldSetting.id, yupType];
+    });
+  return Yup.object(Object.fromEntries(schema));
+};
 
 export const getSubmitButtonColor = (action: WritebackAction): string => {
   if (action.type === "implicit" && action.kind === "row/delete") {
