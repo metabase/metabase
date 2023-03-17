63ec4049d07cce7f316c18bf13b3355b7f613aac
Cal Herries <39073188+calherries@users.noreply.github.com>
2023-03-17 16:50:18 +0200

    Optimize DB calls of dashboard card updates some more - 2 (#29252)
    * Transform card data into the same format as that which comes out of the DB
    
    * Use `from-json` instead of toucan transformations
    
    * Remove unused require
    
    * Update docstring and rename to from-parsed-json
    
    * Oops, fixed
    
    * Remove now
    
    * Add docstring
    
    * Fix order of docstring
    
    * Don't update created_at and updated_at

--- src/metabase/models/dashboard_card.clj
+++ src/metabase/models/dashboard_card.clj
@@ -55,6 +55,27 @@
                            :visualization_settings :visualization-settings})
   :pre-insert pre-insert})
 
+(defn from-parsed-json
+  "Convert a map with dashboard-card into a Toucan instance assuming it came from parsed JSON and the map keys have
+   been keywordized. This is useful if the data from a request body inside a `defendpoint` body, and you need it in the
+   same format as if it were selected from the DB with toucan. It doesn't transform the `:created_at` or `:updated_at`
+   fields, as the types of timestamp values differ by the application database driver.
+
+   For example:
+   ```
+   (= dashcard ;; from toucan select, excluding :created_at and :updated_at
+      (-> (json/generate-string dashcard)
+          (json/parse-string true)
+          from-parsed-json))
+   =>
+   true
+   ```"
+  [dashboard-card]
+  (t2/instance DashboardCard
+               (-> dashboard-card
+                   (m/update-existing :parameter_mappings mi/normalize-parameters-list)
+                   (m/update-existing :visualization_settings mi/normalize-visualization-settings))))
+
 (defmethod serdes.hash/identity-hash-fields DashboardCard
   [_dashboard-card]
   [(serdes.hash/hydrated-hash :card "<none>") ; :card is optional, eg. text cards
@@ -164,7 +185,8 @@
                     ;; This is to preserve the existing behavior of questions and card_id
                     ;; I don't know why card_id couldn't be changed for cards though.
                      action_id (conj :card_id))
-         updates (shallow-updates (select-keys dashboard-card update-ks) (select-keys old-dashboard-card update-ks))]
+         updates (shallow-updates (select-keys dashboard-card update-ks)
+                                  (select-keys old-dashboard-card update-ks))]
      (when (seq updates)
        (db/update! DashboardCard id updates))
      (when (not= (:series dashboard-card [])
