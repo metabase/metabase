9ffcfbef1276ebbec600baacede530a99ffb7bc9
Emmad Usmani <emmadusmani@berkeley.edu>
2023-03-21 12:03:30 -0700

    fix color range for null values in table

--- frontend/test/metabase/visualizations/lib/table_format.unit.spec.js
+++ frontend/test/metabase/visualizations/lib/table_format.unit.spec.js
@@ -3,6 +3,7 @@ import {
   canCompareSubstrings,
   OPERATOR_FORMATTER_FACTORIES,
   compileFormatter,
+  extent,
 } from "metabase/visualizations/lib/table_format";
 
 describe("compileFormatter", () => {
@@ -137,5 +138,11 @@ describe("canCompareSubstrings", () => {
     expect(canCompareSubstrings("", "foo")).toBe(false);
     expect(canCompareSubstrings("foo", "")).toBe(false);
     expect(canCompareSubstrings("", "")).toBe(false);
+  });
+});
+
+describe("extent", () => {
+  it("should ignore null and undefined values", () => {
+    expect(extent([[null], [1], [undefined]], 0)).toEqual([1, 1]);
   });
 });
