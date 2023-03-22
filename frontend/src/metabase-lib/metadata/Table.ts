843c7bfadaac17015228c78f02d9039095354be4
Braden Shepherdson <braden@metabase.com>
2023-03-22 09:55:42 -0400

    [MLv2] Round-trip JS <-> CLJS Cards on demand; use for display locking
    This introduces a Malli schema for `Card`, the representation for a
    "question" in Metabase, and supports conversion between the JS map
    `Card` and CLJS maps.
    
    The `dataset_query` field holding the native or MBQL query is special,
    and this PR uses the `->legacy-MBQL` conversion from #29419.
    
    A TS `Question` can be created with either the JS or CLJS rep for the
    card, and the methods `Question.card()` and `Question._cljsCard()`
    lazily convert the other form into the requested form and cache it.
    
    The existing `_getMLv2Query` logic is replaced; it is subsumed by the
    conversion of the whole card including the query.

--- frontend/src/metabase-lib/metadata/Table.ts
+++ frontend/src/metabase-lib/metadata/Table.ts
@@ -58,7 +58,7 @@ class TableInner extends Base {
 
   question() {
     return Question.create({
-      databaseId: this.db && this.db.id,
+      databaseId: (this.db && this.db.id) || this.db_id,
       tableId: this.id,
       metadata: this.metadata,
     });
