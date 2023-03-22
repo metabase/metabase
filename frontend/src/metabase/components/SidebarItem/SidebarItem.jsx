4e6e1f1feaccce9c3145030989c54f9e6a5289e0
Ryan Laurie <iethree@gmail.com>
2023-03-22 15:06:16 -0600

    move component files into directories

---
+++ frontend/src/metabase/components/SidebarItem/SidebarItem.jsx
@@ -0,0 +1,25 @@
+/* eslint "react/prop-types": "warn" */
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router";
+import S from "../Sidebar.css";
+
+import LabelIcon from "../LabelIcon";
+
+const SidebarItem = ({ name, sidebar, icon, href }) => (
+  <li>
+    <Link to={href} className={S.item} activeClassName={S.selected}>
+      <LabelIcon className={S.icon} icon={icon} />
+      <span className={S.name}>{sidebar || name}</span>
+    </Link>
+  </li>
+);
+
+SidebarItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  sidebar: PropTypes.string,
+  icon: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+export default React.memo(SidebarItem);
