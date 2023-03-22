de6c32919fac9144535ce679585d4ca5f911e316
Emmad Usmani <emmadusmani@berkeley.edu>
2023-03-22 10:08:08 -0700

    add another test case

--- frontend/test/metabase/visualizations/lib/table_format.unit.spec.js
+++ frontend/test/metabase/visualizations/lib/table_format.unit.spec.js
@@ -142,6 +142,18 @@ describe("canCompareSubstrings", () => {
 });
 
 describe("extent", () => {
+  it("should correctly compute the extents for each column", () => {
+    const rows = [
+      [-12, 4, 8],
+      [0, -3, 2],
+      [-1, 0, 17],
+    ];
+
+    expect(extent(rows, 0)).toEqual([-12, 0]);
+    expect(extent(rows, 1)).toEqual([-3, 4]);
+    expect(extent(rows, 2)).toEqual([2, 17]);
+  });
+
   it("should ignore null and undefined values", () => {
     expect(extent([[null], [1], [undefined]], 0)).toEqual([1, 1]);
   });
