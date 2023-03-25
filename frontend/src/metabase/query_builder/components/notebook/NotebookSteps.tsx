b068285f1f5ef415a131fffddb94f2a3654e5cf3
Anton Kulyk <kuliks.anton@gmail.com>
2023-03-25 01:40:11 +0000

    Turn `NotebookSteps` into functional component

--- frontend/src/metabase/query_builder/components/notebook/NotebookSteps.tsx
+++ frontend/src/metabase/query_builder/components/notebook/NotebookSteps.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import cx from "classnames";
 
 import type Question from "metabase-lib/Question";
@@ -15,86 +15,86 @@ interface NotebookStepsProps {
   updateQuestion: (question: Question) => void;
 }
 
-interface State {
-  openSteps: { [key: string]: boolean };
-  lastOpenedStep: string | null;
+type OpenSteps = { [key: string]: boolean };
+
+function getInitialOpenSteps(question: Question): OpenSteps {
+  const isNew = !question.table();
+  return isNew
+    ? {
+        "0:filter": true,
+        "0:summarize": true,
+      }
+    : {};
 }
 
-class NotebookSteps extends React.Component<NotebookStepsProps, State> {
-  constructor(props: NotebookStepsProps) {
-    super(props);
-    const isNew = !props.question.table();
-    this.state = {
-      openSteps: isNew
-        ? {
-            "0:filter": true,
-            // "0:aggregate": true,
-            "0:summarize": true,
-          }
-        : {},
-      lastOpenedStep: null,
-    };
-  }
+function NotebookSteps({
+  className,
+  question,
+  sourceQuestion,
+  reportTimezone,
+  updateQuestion,
+}: NotebookStepsProps) {
+  const [openSteps, setOpenSteps] = useState<OpenSteps>(
+    getInitialOpenSteps(question),
+  );
+  const [lastOpenedStep, setLastOpenedStep] = useState<string | null>(null);
 
-  openStep = (id: string) => {
-    this.setState({
-      openSteps: { ...this.state.openSteps, [id]: true },
-      lastOpenedStep: id,
-    });
-  };
-
-  closeStep = (id: string) => {
-    this.setState({
-      openSteps: { ...this.state.openSteps, [id]: false },
-      lastOpenedStep:
-        this.state.lastOpenedStep === id ? null : this.state.lastOpenedStep,
-    });
-  };
-
-  render() {
-    const {
-      question,
-      className,
-      reportTimezone,
-      sourceQuestion,
-      updateQuestion,
-    } = this.props;
-    const { openSteps, lastOpenedStep } = this.state;
-
+  const steps = useMemo(() => {
     if (!question) {
-      return null;
+      return [];
     }
+    return getQuestionSteps(question, openSteps);
+  }, [question, openSteps]);
 
-    const steps = getQuestionSteps(question, openSteps);
+  const handleStepOpen = useCallback((id: string) => {
+    setOpenSteps(openSteps => ({ ...openSteps, [id]: true }));
+    setLastOpenedStep(id);
+  }, []);
 
-    return (
-      <div className={cx(className, "pt3")}>
-        {steps.map((step, index) => {
-          // pass a version of updateQuery that cleans subsequent steps etc
-          const updateQuery = async (query: StructuredQuery) => {
-            const datasetQuery = query.datasetQuery();
-            const updatedQuery = step.update(datasetQuery);
-            await updateQuestion(updatedQuery.question());
-            // mark the step as "closed" since we can assume it's been added or removed by the updateQuery
-            this.closeStep(step.id);
-          };
-          return (
-            <NotebookStep
-              key={step.id}
-              step={step}
-              sourceQuestion={sourceQuestion}
-              updateQuery={updateQuery}
-              openStep={this.openStep}
-              closeStep={this.closeStep}
-              isLastStep={index === steps.length - 1}
-              isLastOpened={lastOpenedStep === step.id}
-              reportTimezone={reportTimezone}
-            />
-          );
-        })}
-      </div>
+  const handleStepClose = useCallback((id: string) => {
+    setOpenSteps(openSteps => ({ ...openSteps, [id]: false }));
+    setLastOpenedStep(lastOpenedStep =>
+      lastOpenedStep === id ? null : lastOpenedStep,
     );
+  }, []);
+
+  if (!question) {
+    return null;
   }
+
+  return (
+    <div className={cx(className, "pt3")}>
+      {steps.map((step, index) => {
+        // pass a version of updateQuery that cleans subsequent steps etc
+        const updateQuery = async (query: StructuredQuery) => {
+          const datasetQuery = query.datasetQuery();
+          const updatedQuery = step.update(datasetQuery);
+          await updateQuestion(updatedQuery.question());
+
+          // mark the step as "closed" since we can assume
+          // it's been added or removed by the updateQuery
+          handleStepClose(step.id);
+        };
+
+        const isLast = index === steps.length - 1;
+        const isLastOpened = lastOpenedStep === step.id;
+
+        return (
+          <NotebookStep
+            key={step.id}
+            step={step}
+            sourceQuestion={sourceQuestion}
+            isLastStep={isLast}
+            isLastOpened={isLastOpened}
+            reportTimezone={reportTimezone}
+            updateQuery={updateQuery}
+            openStep={handleStepOpen}
+            closeStep={handleStepClose}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default NotebookSteps;
