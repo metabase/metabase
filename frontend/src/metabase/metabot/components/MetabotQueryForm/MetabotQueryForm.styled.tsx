141a91ebea4f7f52df4f0404facbbc522ad639cd
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-27 23:02:29 +0300

    Add query form

--- frontend/src/metabase/metabot/components/MetabotQueryForm/MetabotQueryForm.styled.tsx
+++ frontend/src/metabase/metabot/components/MetabotQueryForm/MetabotQueryForm.styled.tsx
@@ -2,19 +2,18 @@ import styled from "@emotion/styled";
 import { color } from "metabase/lib/colors";
 
 export const FormSection = styled.div`
-  margin-top: 1rem;
-  margin-bottom: 1.5rem;
+  margin: 1rem 0 1.5rem;
 `;
 
 export const FormSectionTitle = styled.div`
   color: ${color("text-dark")};
   font-weight: bold;
-  margin-left: 2rem;
-  margin-right: 2rem;
+  padding: 0 2rem;
 `;
 
 export const FormFooter = styled.div`
   display: flex;
   justify-content: end;
   gap: 1rem;
+  padding: 0 2rem 1.5rem;
 `;
