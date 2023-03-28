68114ec4c14292dc6b5e209dd6798d5e09c97457
Bryan Maass <bryan.maass@gmail.com>
2023-03-28 10:42:37 -0600

    restore permissions in test more reliably

--- test/metabase/api/card_test.clj
+++ test/metabase/api/card_test.clj
@@ -2324,57 +2324,64 @@
    (do-with-card-param-values-fixtures nil f))
 
   ([card-values f]
-   (mt/with-temp*
-     [Card [source-card {:database_id   (mt/id)
-                         :table_id      (mt/id :venues)
-                         :dataset_query (mt/mbql-query venues {:limit 5})}]
-      Card [field-filter-card  {:dataset_query
-                                {:database (mt/id)
-                                 :type     :native
-                                 :native   {:query         "SELECT COUNT(*) FROM VENUES WHERE {{NAME}}"
-                                            :template-tags {"NAME" {:id           "name_param_id"
-                                                                    :name         "NAME"
-                                                                    :display_name "Name"
-                                                                    :type         :dimension
-                                                                    :dimension    [:field (mt/id :venues :name) nil]
-                                                                    :required     true}}}}
-                                :name       "native card with field filter"
-                                :parameters [{:id     "name_param_id",
-                                              :type   :string/=,
-                                              :target [:dimension [:template-tag "NAME"]],
-                                              :name   "Name",
-                                              :slug   "NAME"}]}]
-      Card [card        (merge
-                         {:database_id   (mt/id)
-                          :dataset_query (mt/mbql-query venues)
-                          :parameters    [{:name                 "Static Category",
-                                           :slug                 "static_category"
-                                           :id                   "_STATIC_CATEGORY_",
-                                           :type                 "category",
-                                           :values_source_type   "static-list"
-                                           :values_source_config {:values ["African" "American" "Asian"]}}
-                                          {:name                 "Static Category label",
-                                           :slug                 "static_category_label"
-                                           :id                   "_STATIC_CATEGORY_LABEL_",
-                                           :type                 "category",
-                                           :values_source_type   "static-list"
-                                           :values_source_config {:values [["African" "Af"] ["American" "Am"] ["Asian" "As"]]}}
-                                          {:name                 "Card as source"
-                                           :slug                 "card"
-                                           :id                   "_CARD_"
-                                           :type                 "category"
-                                           :values_source_type   "card"
-                                           :values_source_config {:card_id     (:id source-card)
-                                                                  :value_field (mt/$ids $venues.name)}}]
-                          :table_id      (mt/id :venues)}
-                         card-values)]]
-     (f {:source-card       source-card
-         :card              card
-         :field-filter-card field-filter-card
-         :param-keys        {:static-list       "_STATIC_CATEGORY_"
-                             :static-list-label "_STATIC_CATEGORY_LABEL_"
-                             :card              "_CARD_"
-                             :field-values      "name_param_id"}}))))
+   (let [all-permissions (t2/select perms/Permissions)
+         non-admin-perms (remove #(= "/" (:object %)) all-permissions)]
+     (try
+       (mt/with-temp*
+         [Card [source-card {:database_id   (mt/id)
+                             :table_id      (mt/id :venues)
+                             :dataset_query (mt/mbql-query venues {:limit 5})}]
+          Card [field-filter-card  {:dataset_query
+                                    {:database (mt/id)
+                                     :type     :native
+                                     :native   {:query         "SELECT COUNT(*) FROM VENUES WHERE {{NAME}}"
+                                                :template-tags {"NAME" {:id           "name_param_id"
+                                                                        :name         "NAME"
+                                                                        :display_name "Name"
+                                                                        :type         :dimension
+                                                                        :dimension    [:field (mt/id :venues :name) nil]
+                                                                        :required     true}}}}
+                                    :name       "native card with field filter"
+                                    :parameters [{:id     "name_param_id",
+                                                  :type   :string/=,
+                                                  :target [:dimension [:template-tag "NAME"]],
+                                                  :name   "Name",
+                                                  :slug   "NAME"}]}]
+          Card [card        (merge
+                             {:database_id   (mt/id)
+                              :dataset_query (mt/mbql-query venues)
+                              :parameters    [{:name                 "Static Category",
+                                               :slug                 "static_category"
+                                               :id                   "_STATIC_CATEGORY_",
+                                               :type                 "category",
+                                               :values_source_type   "static-list"
+                                               :values_source_config {:values ["African" "American" "Asian"]}}
+                                              {:name                 "Static Category label",
+                                               :slug                 "static_category_label"
+                                               :id                   "_STATIC_CATEGORY_LABEL_",
+                                               :type                 "category",
+                                               :values_source_type   "static-list"
+                                               :values_source_config {:values [["African" "Af"] ["American" "Am"] ["Asian" "As"]]}}
+                                              {:name                 "Card as source"
+                                               :slug                 "card"
+                                               :id                   "_CARD_"
+                                               :type                 "category"
+                                               :values_source_type   "card"
+                                               :values_source_config {:card_id     (:id source-card)
+                                                                      :value_field (mt/$ids $venues.name)}}]
+                              :table_id      (mt/id :venues)}
+                             card-values)]]
+         (f {:source-card       source-card
+             :card              card
+             :field-filter-card field-filter-card
+             :param-keys        {:static-list       "_STATIC_CATEGORY_"
+                                 :static-list-label "_STATIC_CATEGORY_LABEL_"
+                                 :card              "_CARD_"
+                                 :field-values      "name_param_id"}}))
+       (finally
+         (doseq [{:keys [id]} (remove #(= "/" (:object %)) (t2/select perms/Permissions))]
+           (t2/delete! perms/Permissions id))
+         (t2/insert! perms/Permissions non-admin-perms))))))
 
 (defmacro with-card-param-values-fixtures
   "Execute `body` with all needed setup to tests param values on card."
