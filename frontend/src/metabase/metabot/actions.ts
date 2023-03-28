58e7afc2efe6aa87891886f2ca1afcf1062549a8
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-28 21:44:45 +0300

    Add reducers

--- frontend/src/metabase/metabot/actions.ts
+++ frontend/src/metabase/metabot/actions.ts
@@ -1,7 +1,22 @@
 import { createAction } from "redux-actions";
+import { MetabotEntityId, MetabotEntityType } from "metabase-types/store";
+import { MetabotFeedbackType } from "metabase-types/api";
+
+export interface InitPayload {
+  entityId: MetabotEntityId;
+  entityType: MetabotEntityType;
+  initialQueryText?: string;
+}
+
+export const INIT = "metabase/metabot/INIT";
+export const init = createAction<InitPayload>(INIT);
+
+export const RESET = "metabase/metabot/RESET";
+export const reset = createAction(RESET);
 
 export const SET_QUERY_TEXT = "metabase/metabot/SET_QUERY_TEXT";
-export const setQueryText = createAction(SET_QUERY_TEXT);
+export const setQueryText = createAction<string>(SET_QUERY_TEXT);
 
 export const SET_FEEDBACK_TYPE = "metabase/metabot/SET_FEEDBACK_TYPE";
-export const setFeedbackType = createAction(SET_FEEDBACK_TYPE);
+export const setFeedbackType =
+  createAction<MetabotFeedbackType>(SET_FEEDBACK_TYPE);
--- frontend/src/metabase/metabot/reducers.ts
+++ frontend/src/metabase/metabot/reducers.ts
@@ -1,32 +1,74 @@
 import { handleActions } from "redux-actions";
-import { SET_QUERY_TEXT, SET_FEEDBACK_TYPE } from "./actions";
+import { SET_QUERY_TEXT, SET_FEEDBACK_TYPE, RESET } from "./actions";
 
-export const entityId = handleActions({}, null);
-
-export const entityType = handleActions({}, null);
-
-export const card = handleActions({}, null);
-
-export const originalCard = handleActions({}, null);
-
-export const queryText = handleActions(
+export const entityId = handleActions(
   {
-    [SET_QUERY_TEXT]: { next: (state, { payload }) => payload },
-  },
-  "",
-);
-
-export const queryStatus = handleActions({}, "idle");
-
-export const queryResults = handleActions({}, null);
-
-export const queryError = handleActions({}, null);
-
-export const feedbackType = handleActions(
-  {
-    [SET_FEEDBACK_TYPE]: { next: (state, { payload }) => payload },
+    [RESET]: { next: () => null },
   },
   null,
 );
 
-export const feedbackStatus = handleActions({}, null);
+export const entityType = handleActions(
+  {
+    [RESET]: { next: () => null },
+  },
+  null,
+);
+
+export const card = handleActions(
+  {
+    [RESET]: { next: () => null },
+  },
+  null,
+);
+
+export const originalCard = handleActions(
+  {
+    [RESET]: { next: () => null },
+  },
+  null,
+);
+
+export const queryText = handleActions(
+  {
+    [SET_QUERY_TEXT]: { next: (state, { payload }) => payload },
+    [RESET]: { next: () => "" },
+  },
+  "",
+);
+
+export const queryStatus = handleActions(
+  {
+    [RESET]: { next: () => "idle" },
+  },
+  "idle",
+);
+
+export const queryResults = handleActions(
+  {
+    [RESET]: { next: () => null },
+  },
+  null,
+);
+
+export const queryError = handleActions(
+  {
+    [RESET]: { next: () => null },
+  },
+  null,
+);
+
+export const feedbackType = handleActions(
+  {
+    [SET_FEEDBACK_TYPE]: { next: (state, { payload }) => payload },
+    [RESET]: { next: () => null },
+  },
+  null,
+);
+
+export const feedbackStatus = handleActions(
+  {
+    [RESET]: { next: () => "idle" },
+  },
+  "idle",
+);
