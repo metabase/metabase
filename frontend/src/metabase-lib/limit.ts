6764e5f06121d372b7790a308aec1955aa0d33f0
Anton Kulyk <kuliks.anton@gmail.com>
2023-03-28 19:52:56 +0100

    Update v2 `limit` wrapper signatures

--- frontend/src/metabase-lib/limit.ts
+++ frontend/src/metabase-lib/limit.ts
@@ -1,15 +1,21 @@
 import * as ML from "cljs/metabase.lib.limit";
 import type { Query, Limit } from "./types";
 
-export function currentLimit(query: Query): Limit {
-  return ML.current_limit(query);
+const DEFAULT_STAGE_INDEX = -1;
+
+export function currentLimit(
+  query: Query,
+  stageIndex = DEFAULT_STAGE_INDEX,
+): Limit {
+  return ML.current_limit(query, stageIndex);
 }
 
-export function limit(query: Query, limit: Limit): Query {
-  return ML.limit(query, limit);
-}
+declare function LimitFn(query: Query, limit: Limit): Query;
+declare function LimitFn(query: Query, stageIndex: number, limit: Limit): Query;
 
-export function hasLimit(query: Query) {
-  const limit = currentLimit(query);
+export const limit: typeof LimitFn = ML.limit;
+
+export function hasLimit(query: Query, stageIndex = DEFAULT_STAGE_INDEX) {
+  const limit = currentLimit(query, stageIndex);
   return typeof limit === "number" && limit > 0;
 }
