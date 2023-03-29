51492ff8a88503d71cdea18bdb0915950a13abfc
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-29 16:18:35 +0300

    Revert unwanted changes

--- frontend/src/metabase/visualizations/components/skeletons/ChartSkeleton/types.ts
+++
@@ -1,8 +0,0 @@
-import { HTMLAttributes } from "react";
-
-export interface SharedChartSkeletonProps
-  extends HTMLAttributes<HTMLDivElement> {
-  name?: string | null;
-  description?: string | null;
-  isStatic?: boolean;
-}
