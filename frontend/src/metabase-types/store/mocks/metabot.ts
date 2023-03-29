dcc2e95b6eb0297d642b754f3e16fa4716df75f7
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-29 16:30:11 +0300

    Add state mocks

---
+++ frontend/src/metabase-types/store/mocks/metabot.ts
@@ -0,0 +1,17 @@
+import { MetabotState } from "metabase-types/store";
+
+export const createMockMetabotState = (
+  opts?: Partial<MetabotState>,
+): MetabotState => ({
+  entityId: null,
+  entityType: null,
+  card: null,
+  originalCard: null,
+  prompt: "",
+  queryStatus: "idle",
+  queryResults: null,
+  queryError: null,
+  feedbackType: null,
+  feedbackStatus: "idle",
+  ...opts,
+});
