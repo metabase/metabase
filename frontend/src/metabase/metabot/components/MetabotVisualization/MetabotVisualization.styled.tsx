5d582170f5820272d057d7ccf2b4202652b5d2e3
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-29 22:58:34 +0300

    Connect components

--- frontend/src/metabase/metabot/components/MetabotVisualization/MetabotVisualization.styled.tsx
+++ frontend/src/metabase/metabot/components/MetabotVisualization/MetabotVisualization.styled.tsx
@@ -1,9 +1,7 @@
 import styled from "@emotion/styled";
-import { color } from "metabase/lib/colors";
 import Visualization from "metabase/visualizations/components/Visualization";
 
 export const FullVisualization = styled(Visualization)`
   display: flex;
   flex: 1 0 0;
-  border-top: 1px solid ${color("border")};
 `;
