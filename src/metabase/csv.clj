5c3a487ddb2a29b5859e7eeb055412e87ddd81c5
Tim Macdonald <tim@tsmacdonald.com>
2023-03-30 12:38:35 +0100

    Add float_255, int_255

--- src/metabase/csv.clj
+++ src/metabase/csv.clj
@@ -2,21 +2,40 @@
   (:require
    [clojure.data.csv :as csv]
    [clojure.java.io :as io]
+   [clojure.set :as set]
+   [clojure.string :as str]
+   [medley.core :as m]
    [metabase.search.util :as search-util]
    [metabase.util :as u]
-   [metabase.util.i18n :refer [trs]]
-   [clojure.string :as str]))
+   [metabase.util.i18n :refer [trs]]))
 
-(derive ::boolean     ::int)
-(derive ::boolean     ::varchar_255)
-(derive ::boolean     ::text)
+;;
+;;
+;;      ______text________
+;;     /       |          \
+;;    /        |           \
+;; float   varchar_255     int
+;;   \         /   \        /
+;;    \       /     \      /
+;;   float_255       \    /
+;;            \       \  /
+;;             \-----int_255
+;;                     |
+;;                     |
+;;                  boolean
+(derive ::boolean     ::int_255)
+(derive ::int_255     ::int)
+(derive ::int_255     ::float_255)
 (derive ::int         ::float)
-(derive ::int         ::text)
-(derive ::int         ::varchar_255)
-(derive ::int         ::text)
-(derive ::float       ::varchar_255)
+(derive ::float_255   ::varchar_255)
 (derive ::float       ::text)
 (derive ::varchar_255 ::text)
+
+(def intermediate-type->final-type "We use some 'intermediate types' to keep track of information while scanning over the file. This is a map from those
+  intermediate types to the final type they should result in if unaltered."
+  {::int_255   ::int
+   ::float_255 ::float
+   nil         ::text})
 
 (defn value->type
   "The most-specific possible type for a given value. Possibilities are:
@@ -25,15 +44,20 @@
     - ::float
     - ::varchar_255
     - ::text
+    - nil, in which case other functions are expected to replace it with ::text as the catch-all type
 
   NB: There are currently the following gotchas:
     1. ints/floats are assumed to have commas as separators and periods as decimal points
     2. 0 and 1 are assumed to be booleans, not ints."
   [value]
   (cond
+    (str/blank? value)                                      nil
     (re-matches #"(?i)true|t|yes|y|1|false|f|no|n|0" value) ::boolean
-    (re-matches #"[\d,]+"                            value) ::int
-    (re-matches #"[\d,]*\.\d+"                       value) ::float
+    (re-matches #"-?[\d,]{1,255}"                    value) ::int_255
+    (re-matches #"-?[\d,]+"                          value) ::int
+    (and (re-matches #"-?[\d,]*\.\d+"                value)
+         (<= (count value) 255))                            ::float_255
+    (re-matches #"-?[\d,]*\.\d+"                     value) ::float
     (re-matches #".{1,255}"                          value) ::varchar_255
     :else                                                   ::text))
 
@@ -41,16 +65,22 @@
   [column-names row]
   (->> row
        (map vector column-names)
-       (map (fn [[column-name value]] [column-name (value->type value)]))
+       (map (fn [[column-name value]] [column-name (and value (value->type (search-util/normalize value)))]))
        (into {})))
 
 (defn coalesce
   "Returns the 'parent' type (the most general)."
   [type-a type-b]
   (cond
+    (nil? type-a)        type-b
+    (nil? type-b)        type-a
     (isa? type-a type-b) type-b
     (isa? type-b type-a) type-a
-    :else (throw (Exception. (trs "Unexpected type combination in the same column: {0} and {1}" type-a type-b)))))
+    :else
+    (let [common-ancestors (set/intersection (ancestors type-a) (ancestors type-b))]
+      (if (seq common-ancestors)
+        (first common-ancestors)
+        (throw (Exception. (trs "Unexpected type combination in the same column: {0} and {1}" type-a type-b)))))))
 
 (defn- coalesce-types
   [types-so-far new-types]
@@ -59,21 +89,31 @@
               [column-name (coalesce old-type (get new-types column-name))]))
        (into {})))
 
+(defn- normalize-type
+  "We use some intermediate types (int_255, float_255, nil) while looking at the rows, but when the whole file has been
+  scanned they need to be converted into a proper type."
+  [the-type]
+  (get intermediate-type->final-type the-type the-type))
+
 (defn- rows->schema
   [header rows]
-  (let [normalized-header (map u/slugify header)]
+  (let [normalized-header (map (comp u/slugify str/trim) header)]
     (->> rows
          (map (partial row->types normalized-header))
-         (reduce coalesce-types))))
+         (reduce coalesce-types)
+         (m/map-vals normalize-type))))
 
 (defn detect-schema
   "Returns a map of `normalized-column-name -> type` for the given CSV file. The CSV file *must* have headers as the
   first row. Supported types are:
+
     - ::int
     - ::float
     - ::boolean
     - ::varchar_255
-    - ::text"
+    - ::text
+
+  A column that is completely blank is assumed to be of type ::text."
   [csv-file]
   (with-open [reader (io/reader csv-file)]
     (let [[header & rows] (csv/read-csv reader)]
--- test/metabase/csv_test.clj
+++ test/metabase/csv_test.clj
@@ -1,18 +1,23 @@
 (ns metabase.csv-test
   (:require
+   [clojure.string :as str]
    [clojure.test :refer :all]
-   [metabase.csv :as csv]
-   [clojure.string :as str])
+   [metabase.csv :as csv])
   (:import
    [java.io File]))
 
 (set! *warn-on-reflection* true)
 
-(def bool-type  :metabase.csv/boolean)
-(def int-type   :metabase.csv/int)
-(def float-type :metabase.csv/float)
-(def vchar-type :metabase.csv/varchar_255)
-(def text-type  :metabase.csv/text)
+(def bool-type      :metabase.csv/boolean)
+(def int-type-255   :metabase.csv/int_255)
+(def int-type       :metabase.csv/int)
+(def float-type-255 :metabase.csv/float_255)
+(def float-type     :metabase.csv/float)
+(def vchar-type     :metabase.csv/varchar_255)
+(def text-type      :metabase.csv/text)
+
+(def huge-int   (apply str (repeat 256 "1")))
+(def huge-float (str (apply str (repeat 254 "1")) ".0"))
 
 (deftest type-detection-test
   (doseq [[value expected] [["0"                          bool-type]
@@ -27,12 +32,16 @@
                             ["n"                          bool-type]
                             ["yes"                        bool-type]
                             ["NO"                         bool-type]
-                            ["2"                          int-type]
-                            ["9,986,000"                  int-type]
-                            ["3.14"                       float-type]
-                            [".14"                        float-type]
-                            ["0.14"                       float-type]
-                            ["9,986,000.0"                float-type]
+                            ["2"                          int-type-255]
+                            ["-86"                        int-type-255]
+                            ["9,986,000"                  int-type-255]
+                            [huge-int                     int-type]
+                            ["3.14"                       float-type-255]
+                            [".14"                        float-type-255]
+                            ["0.14"                       float-type-255]
+                            ["-9,986.567"                 float-type-255]
+                            ["9,986,000.0"                float-type-255]
+                            [huge-float                   float-type]
                             [(apply str (repeat 255 "x")) vchar-type]
                             [(apply str (repeat 256 "x")) text-type]
                             ["86 is my favorite number"   vchar-type]
@@ -41,17 +50,21 @@
       (is (= expected (csv/value->type value))))))
 
 (deftest type-coalescing-test
-  (doseq [[type-a type-b expected] [[bool-type  int-type   int-type]
-                                    [int-type   bool-type  int-type] ;; ensure arg order doesn't matter
-                                    [int-type   float-type float-type]
-                                    [bool-type  vchar-type vchar-type]
-                                    [bool-type  text-type  text-type]
-                                    [int-type   vchar-type vchar-type]
-                                    [int-type   text-type  text-type]
-                                    [float-type vchar-type vchar-type]
-                                    [float-type text-type  text-type]
-                                    [vchar-type text-type  text-type]]]
-    (is (= expected (csv/coalesce type-a type-b)))))
+  (doseq [[type-a type-b expected] [[bool-type      int-type   int-type]
+                                    [int-type       bool-type  int-type] ;; ensure arg order doesn't matter
+                                    [int-type       float-type float-type]
+                                    [bool-type      vchar-type vchar-type]
+                                    [bool-type      text-type  text-type]
+                                    [int-type       vchar-type text-type]
+                                    [int-type-255   vchar-type vchar-type]
+                                    [int-type       text-type  text-type]
+                                    [float-type-255 vchar-type vchar-type]
+                                    [float-type     vchar-type text-type]
+                                    [float-type     text-type  text-type]
+                                    [vchar-type     text-type  text-type]
+                                    [float-type-255 int-type   text-type]]]
+    (is (= expected (csv/coalesce type-a type-b))
+        (format "%s + %s = %s" (name type-a) (name type-b) (name expected)))))
 
 (defn- csv-file-with
   "Create a temp csv file with the given content and return the file"
@@ -69,4 +82,46 @@
            (csv/detect-schema
             (csv-file-with ["Name, Age, Favorite Pokémon"
                             "Tim, 12, Haunter"
-                            "Ryan, 97, Paras"]))))))
+                            "Ryan, 97, Paras"])))))
+  (testing "CSV missing data"
+    (is (= {"name"       vchar-type
+            "height"     int-type
+            "birth_year" float-type}
+           (csv/detect-schema
+            (csv-file-with ["Name, Height, Birth Year"
+                            "Luke Skywalker, 172, -19"
+                            "Darth Vader, 202, -41.9"
+                            "Watto, 137"          ; missing column
+                            "Sebulba, 112,"]))))) ; comma, but blank column
+  (testing "Type coalescing"
+    (is (= {"name"       vchar-type
+            "height"     float-type
+            "birth_year" vchar-type}
+           (csv/detect-schema
+            (csv-file-with ["Name, Height, Birth Year"
+                            "Rey Skywalker, 170, 15"
+                            "Darth Vader, 202.0, 41.9BBY"])))))
+  (testing "Boolean coalescing"
+    (is (= {"name"          vchar-type
+            "is_jedi_"      bool-type
+            "is_jedi__int_" int-type
+            "is_jedi__vc_"  vchar-type}
+           (csv/detect-schema
+            (csv-file-with ["Name, Is Jedi?, Is Jedi (int), Is Jedi (VC)"
+                            "Rey Skywalker, yes, true, t"
+                            "Darth Vader, YES, TRUE, Y"
+                            "Grogu, 1, 9001, probably?"
+                            "Han Solo, no, FaLsE, 0"])))))
+  (testing "Long numbers don't trip up length limit"
+    (is (= {"int_column"   text-type
+            "float_column" text-type}
+           (csv/detect-schema
+            (csv-file-with ["int column, float column"
+                            (str huge-int "," huge-float)
+                            "not a number, also not a number"]))))
+    (is (= {"int_column"   vchar-type
+            "float_column" vchar-type}
+           (csv/detect-schema
+            (csv-file-with ["int column, float column"
+                            "123, 3.1415"
+                            "not a number, also not a number"]))))))
