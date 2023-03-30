43d1aacd78a51a1d2d5923084a25024ad8d071e3
Tim Macdonald <tim@tsmacdonald.com>
2023-03-30 10:27:07 +0100

    More tests
    [ci skip]

--- src/metabase/csv.clj
+++ src/metabase/csv.clj
@@ -2,18 +2,29 @@
   (:require
    [clojure.data.csv :as csv]
    [clojure.java.io :as io]
+   [clojure.set :as set]
+   [clojure.string :as str]
+   [medley.core :as m]
+   [metabase.search.util :as search-util]
    [metabase.util :as u]
    [metabase.util.i18n :refer [trs]]))
 
+;;           text
+;;            |
+;;            |
+;;       varchar_255
+;;            |
+;;            |
+;;          float
+;;            |
+;;            |
+;;           int
+;;            |
+;;            |
+;;         boolean
 (derive ::boolean     ::int)
-(derive ::boolean     ::varchar_255)
-(derive ::boolean     ::text)
 (derive ::int         ::float)
-(derive ::int         ::text)
-(derive ::int         ::varchar_255)
-(derive ::int         ::text)
 (derive ::float       ::varchar_255)
-(derive ::float       ::text)
 (derive ::varchar_255 ::text)
 
 (defn value->type
@@ -23,15 +34,17 @@
     - ::float
     - ::varchar_255
     - ::text
+    - nil, in which case other functions are expected to replace it with ::text as the catch-all type
 
   NB: There are currently the following gotchas:
     1. ints/floats are assumed to have commas as separators and periods as decimal points
     2. 0 and 1 are assumed to be booleans, not ints."
   [value]
   (cond
+    (str/blank? value)                                      nil
     (re-matches #"(?i)true|t|yes|y|1|false|f|no|n|0" value) ::boolean
-    (re-matches #"[\d,]+"                            value) ::int
-    (re-matches #"[\d,]*\.\d+"                       value) ::float
+    (re-matches #"-?[\d,]+"                          value) ::int
+    (re-matches #"-?[\d,]*\.\d+"                     value) ::float
     (re-matches #".{1,255}"                          value) ::varchar_255
     :else                                                   ::text))
 
@@ -39,16 +52,22 @@
   [column-names row]
   (->> row
        (map vector column-names)
-       (map (fn [[column-name value]] [column-name (value->type value)]))
+       (map (fn [[column-name value]] [column-name (and value (value->type (search-util/normalize value)))]))
        (into {})))
 
 (defn coalesce
   "Returns the 'parent' type (the most general)."
   [type-a type-b]
   (cond
+    (nil? type-a)        type-b
+    (nil? type-b)        type-a
     (isa? type-a type-b) type-b
     (isa? type-b type-a) type-a
-    :else (throw (Exception. (trs "Unexpected type combination in the same column: {0} and {1}" type-a type-b)))))
+    :else
+    (let [common-ancestors (set/intersection (ancestors type-a) (ancestors type-b))]
+      (if (seq common-ancestors)
+        (first common-ancestors)
+        (throw (Exception. (trs "Unexpected type combination in the same column: {0} and {1}" type-a type-b)))))))
 
 (defn- coalesce-types
   [types-so-far new-types]
@@ -59,19 +78,23 @@
 
 (defn- rows->schema
   [header rows]
-  (let [normalized-header (map u/slugify header)]
+  (let [normalized-header (map (comp u/slugify str/trim) header)]
     (->> rows
          (map (partial row->types normalized-header))
-         (reduce coalesce-types))))
+         (reduce coalesce-types)
+         (m/map-vals #(or % ::text)))))
 
 (defn detect-schema
   "Returns a map of `normalized-column-name -> type` for the given CSV file. The CSV file *must* have headers as the
   first row. Supported types are:
+
     - ::int
     - ::float
     - ::boolean
     - ::varchar_255
-    - ::text"
+    - ::text
+
+  A column that is completely blank is assumed to be of type ::text."
   [csv-file]
   (with-open [reader (io/reader csv-file)]
     (let [[header & rows] (csv/read-csv reader)]
