28e7065d260d65b744d27b09e6b2d2cdc10d97f5
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-30 21:19:11 +0300

    Add home widget changes

--- frontend/src/metabase/home/homepage/containers/HomeLayout/HomeLayout.tsx
+++ frontend/src/metabase/home/homepage/containers/HomeLayout/HomeLayout.tsx
@@ -1,22 +1,10 @@
 import { connect } from "react-redux";
-import _ from "underscore";
 import { getSetting } from "metabase/selectors/settings";
-import Search from "metabase/entities/search";
 import type { State } from "metabase-types/store";
 import HomeLayout from "../../components/HomeLayout";
 
 const mapStateToProps = (state: State) => ({
-  showIllustration: getSetting(state, "show-lighthouse-illustration"),
-  isMetabotEnabled: getSetting(state, "is-metabot-enabled"),
+  hasIllustration: getSetting(state, "show-lighthouse-illustration"),
 });
 
-export default _.compose(
-  Search.loadList({
-    query: {
-      models: "dataset",
-      limit: 1,
-    },
-    listName: "models",
-  }),
-  connect(mapStateToProps),
-)(HomeLayout);
+export default connect(mapStateToProps)(HomeLayout);
