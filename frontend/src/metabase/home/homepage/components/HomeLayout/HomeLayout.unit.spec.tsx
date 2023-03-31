bb54507622ada6cebec1aee56cca4202f06e12ea
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-03-31 16:26:33 +0300

    Fix HomeLayout tests

--- frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.unit.spec.tsx
+++ frontend/src/metabase/home/homepage/components/HomeLayout/HomeLayout.unit.spec.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { renderWithProviders, screen } from "__support__/ui";
+import { createMockUser } from "metabase-types/api/mocks";
 import HomeLayout from "./HomeLayout";
 
+const TEST_USER = createMockUser({
+  first_name: "Testy",
+});
+
 const setup = () => {
-  renderWithProviders(<HomeLayout />);
+  renderWithProviders(<HomeLayout />, {
+    storeInitialState: { currentUser: TEST_USER },
+  });
 };
 
 describe("HomeLayout", () => {
   it("should render correctly", () => {
     setup();
-    expect(screen.getByText("Hey there")).toBeInTheDocument();
+    expect(screen.getByText(/Testy/)).toBeInTheDocument();
   });
 });
