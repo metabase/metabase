3324c90963b9b05630fb467fc5723be8d4072760
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2023-04-04 07:34:12 -0600

    CSV / Admin Upload Settings (#29709)
    * allow formData uploads
    
    * basic CSV uploads from collections
    
    * add dropzone support for collections
    
    * allow formData uploads
    
    * basic CSV uploads from collections
    
    * add dropzone support for collections
    
    * Add CSV uploads feature and have postgres and H2 support it (#29636)
    
    * Add csv uploads feature and have postgres support it
    
    * Add h2 support
    
    * Add defsettings
    
    * Add table prefix setting
    
    * add data upload settings
    
    * Add newline
    
    * update settings mock
    
    * Whether or not uploads are enabled
    
    ---------
    
    Co-authored-by: Cal Herries <39073188+calherries@users.noreply.github.com>
    Co-authored-by: Callum Herries <hi@callumherries.com>

---
+++ frontend/src/metabase/admin/settings/components/widgets/UploadSettingsWidget/UploadSettingsWidget.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Database from "metabase/entities/databases";
+import type { Database as DatabaseType } from "metabase-types/types/Database";
+
+import SettingSelect from "../SettingSelect";
+
+const getDatabaseOptions = (databases: DatabaseType[]) =>
+  databases
+    .filter(db => db?.settings?.["database-enable-actions"])
+    .map(db => ({ name: db.name, value: db.id }));
+
+// const enginesWithSchema = ["postgres"];
+// const dbHasSchema = (dbId: number) => (databases: DatabaseType[]) =>
+//   enginesWithSchema.includes(databases.find((db: DatabaseType) => db.id === dbId)?.engine ?? '');
+
+interface UploadSettingProps {
+  databases: DatabaseType[];
+  setting: any;
+  onChange: (value: number) => void;
+}
+
+function UploadDbWidgetView({
+  databases,
+  setting,
+  onChange,
+}: UploadSettingProps) {
+  const databaseOptions = getDatabaseOptions(databases);
+  if (!databaseOptions?.length) {
+    return null;
+  }
+
+  return (
+    <SettingSelect
+      setting={{
+        ...setting,
+        options: databaseOptions,
+      }}
+      onChange={(dbId: number) => onChange(dbId)}
+    />
+  );
+}
+
+export const UploadDbWidget = Database.loadList()(UploadDbWidgetView);
