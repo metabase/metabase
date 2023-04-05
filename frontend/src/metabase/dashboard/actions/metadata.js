fb8017637755298838c0e20995ecd4382461e4a7
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-04-05 16:05:31 +0300

    Load metadata for linked targets

--- frontend/src/metabase/dashboard/actions/metadata.js
+++ frontend/src/metabase/dashboard/actions/metadata.js
@@ -1,16 +1,44 @@
-import { loadMetadataForQueries } from "metabase/redux/metadata";
+import Questions from "metabase/entities/questions";
 import { getMetadata } from "metabase/selectors/metadata";
-
+import { loadMetadataForQueries } from "metabase/redux/metadata";
+import { getLinkTargets } from "metabase/lib/click-behavior";
 import Question from "metabase-lib/Question";
-
 import { isVirtualDashCard } from "../utils";
 
-export const loadMetadataForDashboard = dashCards => (dispatch, getState) => {
+export const loadMetadataForDashboard = dashCards => dispatch => {
+  const cards = dashCards
+    .filter(dc => !isVirtualDashCard(dc)) // exclude text cards
+    .flatMap(dc => [dc.card].concat(dc.series || []));
+
+  dispatch(loadMetadataForCards(cards));
+};
+
+export const loadMetadataForLinkedTargets =
+  visualizationSettings => async (dispatch, getState) => {
+    const linkTargets = getLinkTargets(visualizationSettings);
+    const fetchRequests = linkTargets
+      .map(({ entity, entityId }) =>
+        entity.actions.fetch({ id: entityId }, { noEvent: true }),
+      )
+      .map(action => dispatch(action).catch(e => console.error(e)));
+
+    await Promise.all(fetchRequests);
+
+    const cards = linkTargets
+      .filter(({ entityType }) => entityType === "question")
+      .map(({ entityId }) =>
+        Questions.selectors.getObject(getState(), { entityId }),
+      )
+      .filter(card => card != null);
+
+    await dispatch(loadMetadataForCards(cards));
+  };
+
+const loadMetadataForCards = cards => (dispatch, getState) => {
   const metadata = getMetadata(getState());
 
-  const questions = dashCards
-    .filter(dc => !isVirtualDashCard(dc) && dc.card.dataset_query) // exclude text cards and queries without perms
-    .flatMap(dc => [dc.card].concat(dc.series || []))
+  const questions = cards
+    .filter(card => card.dataset_query) // exclude queries without perms
     .map(card => new Question(card, metadata));
 
   return dispatch(
--- frontend/src/metabase/dashboard/hoc/WithVizSettingsData.js
+++ frontend/src/metabase/dashboard/hoc/WithVizSettingsData.js
@@ -5,38 +5,12 @@ import { withRouter } from "react-router";
 import _ from "underscore";
 
 import { getUserAttributes } from "metabase/selectors/user";
-
-import Questions from "metabase/entities/questions";
-import Dashboards from "metabase/entities/dashboards";
-
-function hasLinkedQuestionOrDashboard({ type, linkType } = {}) {
-  if (type === "link") {
-    return linkType === "question" || linkType === "dashboard";
-  }
-  return false;
-}
-
-function mapLinkedEntityToEntityQuery({ linkType, targetId }) {
-  return {
-    entity: linkType === "question" ? Questions : Dashboards,
-    entityId: targetId,
-  };
-}
+import { getLinkTargets } from "metabase/lib/click-behavior";
+import { loadMetadataForLinkedTargets } from "metabase/dashboard/actions/metadata";
 
 // This HOC give access to data referenced in viz settings.
 // We use it to fetch and select entities needed for dashboard drill actions (e.g. clicking through to a question)
 const WithVizSettingsData = ComposedComponent => {
-  function getLinkTargets(settings) {
-    const { click_behavior, column_settings = {} } = settings || {};
-    return [
-      click_behavior,
-      ...Object.values(column_settings).map(
-        settings => settings.click_behavior,
-      ),
-    ]
-      .filter(hasLinkedQuestionOrDashboard)
-      .map(mapLinkedEntityToEntityQuery);
-  }
   return withRouter(
     connect(
       (state, props) => ({
@@ -67,19 +41,11 @@ const WithVizSettingsData = ComposedComponent => {
       dispatch => ({ dispatch }),
     )(
       class WithVizSettingsData extends React.Component {
-        dashcardSettings(props) {
-          const [firstSeries] = props.rawSeries || [{}];
-          const { visualization_settings } = firstSeries.card || {};
-          return visualization_settings;
-        }
-
         fetch() {
-          getLinkTargets(this.dashcardSettings(this.props)).forEach(
-            ({ entity, entityId }) =>
-              this.props.dispatch(
-                entity.actions.fetch({ id: entityId }, { noEvent: true }),
-              ),
-          );
+          const { rawSeries, dispatch } = this.props;
+          const [firstSeries] = rawSeries || [{}];
+          const { visualization_settings } = firstSeries.card || {};
+          dispatch(loadMetadataForLinkedTargets(visualization_settings));
         }
 
         componentDidMount() {
