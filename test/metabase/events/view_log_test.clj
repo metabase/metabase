7a044e898556d6bdc15656e520effe66448f896b
Adam James <adam.vermeer2@gmail.com>
2023-04-05 16:17:18 -0700

    WIP test for the recent_views user local setting.
    This isn't totally correct yet. I think it doesn't clean up after itself correctly. That's pending.
    
    And, I have to investigate because :table-read isn't working either. Still worth pushing this progress!

--- test/metabase/events/view_log_test.clj
+++ test/metabase/events/view_log_test.clj
@@ -1,9 +1,13 @@
 (ns metabase.events.view-log-test
   (:require
    [clojure.test :refer :all]
+   [java-time :as t]
+   [metabase.api.common :as api]
    [metabase.events.view-log :as view-log]
    [metabase.models :refer [Card Dashboard Table User ViewLog]]
+   [metabase.server.middleware.session :as mw.session]
    [metabase.test :as mt]
+   [metabase.util :as u]
    [toucan2.core :as t2]))
 
 (deftest card-create-test
@@ -64,3 +68,48 @@
             :model_id (:id dashboard)}
            (mt/derecordize
             (t2/select-one [ViewLog :user_id :model :model_id], :user_id (:id user)))))))
+
+(deftest user-recent-views-test
+  (mt/with-temp* [Card      [card1 {:name                   "rand-name"
+                                    :creator_id             (mt/user->id :crowberto)
+                                    :display                "table"
+                                    :visualization_settings {}}]
+                  Card      [archived  {:name                   "archived-card"
+                                        :creator_id             (mt/user->id :crowberto)
+                                        :display                "table"
+                                        :archived               true
+                                        :visualization_settings {}}]
+                  Dashboard [dash {:name        "rand-name2"
+                                   :description "rand-name2"
+                                   :creator_id  (mt/user->id :crowberto)}]
+                  Table     [table1 {:name "rand-name"}]
+                  Table     [hidden-table {:name            "hidden table"
+                                           :visibility_type "hidden"}]
+                  Card      [dataset {:name                   "rand-name"
+                                      :dataset                true
+                                      :creator_id             (mt/user->id :crowberto)
+                                      :display                "table"
+                                      :visualization_settings {}}]]
+    (mt/with-test-user :crowberto
+      (testing "User's recent views are updated when card/dashboard/table-read events occur."
+        (mt/with-test-user :crowberto
+          (view-log/user-recent-views! [])
+          (doseq [event [{:topic :card-read :item dataset}
+                         {:topic :card-read :item card1}
+                         {:topic :card-read :item card1}
+                         {:topic :card-read :item card1}
+                         {:topic :card-read :item card1}
+                         {:topic :card-read :item card1}
+                         {:topic :dashboard-read :item dash}
+                         #_{:topic :table-read :item table1}
+                         #_{:topic :card-read :item archived}
+                         #_{:topic :table-read :item hidden-table}]]
+        #_(mt/with-test-user :crowberto)
+        (view-log/handle-view-event! event))
+          (let [recent-views (mt/with-test-user :crowberto (view-log/user-recent-views))]
+            (is (=
+                 [{:model "card" :model_id (u/the-id dataset)}
+                  {:model "card" :model_id (u/the-id card1)}
+                  {:model "dashboard" :model_id (u/the-id dash)}
+                  #_{:model "table" :model_id (u/the-id table1)}]
+                 recent-views))))))))
