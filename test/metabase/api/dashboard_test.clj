aef49e2b82328b1ed784675ed3e6b300fe062a55
Ngoc Khuat <qn.khuat@gmail.com>
2023-04-07 16:32:35 +0700

    tests fixing

--- test/metabase/api/dashboard_test.clj
+++ test/metabase/api/dashboard_test.clj
@@ -1259,28 +1259,28 @@
                                                 :position         0}]
       (with-dashboards-in-writeable-collection [dashboard-id]
         ;; send a request that update and create and delete some cards at the same time
-        (let [resp (mt/user-http-request :rasta :put 200 (format "dashboard/%d/cards" dashboard-id)
-                                         {:cards [{:id     dashcard-id-1
-                                                   :size_x 4
-                                                   :size_y 4
-                                                   :col    1
-                                                   :row    1
-                                                   ;; update series for card 1
-                                                   :series  [{:id series-id-2}]
-                                                   :card_id card-id-1}
-                                                  {:id     dashcard-id-2
-                                                   :size_x 2
-                                                   :size_y 2
-                                                   :col    2
-                                                   :row    2}
-                                                  ;; remove the dashcard3 and create a new card using negative numbers
-                                                  {:id     -1
-                                                   :size_x 1
-                                                   :size_y 1
-                                                   :col    3
-                                                   :row    3
-                                                   :card_id card-id-2
-                                                   :series  [{:id series-id-1}]}]})
+        (let [cards (:cards (mt/user-http-request :rasta :put 200 (format "dashboard/%d/cards" dashboard-id)
+                                                  {:cards [{:id     dashcard-id-1
+                                                            :size_x 4
+                                                            :size_y 4
+                                                            :col    1
+                                                            :row    1
+                                                            ;; update series for card 1
+                                                            :series  [{:id series-id-2}]
+                                                            :card_id card-id-1}
+                                                           {:id     dashcard-id-2
+                                                            :size_x 2
+                                                            :size_y 2
+                                                            :col    2
+                                                            :row    2}
+                                                           ;; remove the dashcard3 and create a new card using negative numbers
+                                                           {:id     -1
+                                                            :size_x 1
+                                                            :size_y 1
+                                                            :col    3
+                                                            :row    3
+                                                            :card_id card-id-2
+                                                            :series  [{:id series-id-1}]}]}))
               updated-card-1 {:id           dashcard-id-1
                               :card_id      card-id-1
                               :dashboard_id dashboard-id
@@ -1310,9 +1310,9 @@
           (is (=? [updated-card-1
                    updated-card-2
                    new-card]
-                  resp))
-          ;; dashboard 3 is deleted
-          (is (nil? (t2/select-one DashboardCard :id dashcard-id-3))))))))
+                  cards))
+;; dashboard 3 is deleted
+         (is (nil? (t2/select-one DashboardCard :id dashcard-id-3))))))))
 
 (deftest create-tabs-and-dashtabs-test
   (t2.with-temp/with-temp
@@ -1457,15 +1457,15 @@
               (is (partial= [{:visualization_settings {:label "Update"}
                               :action_id              action-id
                               :card_id                nil}]
-                            (mt/user-http-request :crowberto :put 200 (format "dashboard/%s/cards" dashboard-id)
-                                                  {:cards [{:id                     -1
-                                                            :size_x                 1
-                                                            :size_y                 1
-                                                            :row                    1
-                                                            :col                    1
-                                                            :card_id                nil
-                                                            :action_id              action-id
-                                                            :visualization_settings {:label "Update"}}]})))
+                            (:cards (mt/user-http-request :crowberto :put 200 (format "dashboard/%s/cards" dashboard-id)
+                                                          {:cards [{:id                     -1
+                                                                    :size_x                 1
+                                                                    :size_y                 1
+                                                                    :row                    1
+                                                                    :col                    1
+                                                                    :card_id                nil
+                                                                    :action_id              action-id
+                                                                    :visualization_settings {:label "Update"}}]}))))
               (is (partial= {:ordered_cards [{:action (cond-> {:visualization_settings {:hello true}
                                                                :type (name action-type)
                                                                :parameters [{:id "id"}]}
@@ -1493,13 +1493,13 @@
          (testing "If they have data permissions, it should be ok"
            (perms/grant-permissions! (perms-group/all-users) (perms/table-query-path (mt/id :venues)))
            (is (schema= [{:card_id            (s/eq card-id)
-                          :parameter_mappings [(s/one
-                                                {:parameter_id (s/eq "_CATEGORY_ID_")
-                                                 :target       (s/eq ["dimension" ["field" (mt/id :venues :category_id) nil]])
-                                                 s/Keyword     s/Any}
-                                                "mapping")]
-                           s/Keyword           s/Any}]
-                        (add-card! 200)))
+                                  :parameter_mappings [(s/one
+                                                         {:parameter_id (s/eq "_CATEGORY_ID_")
+                                                          :target       (s/eq ["dimension" ["field" (mt/id :venues :category_id) nil]])
+                                                          s/Keyword     s/Any}
+                                                         "mapping")]
+                                  s/Keyword           s/Any}]
+                        (:cards (add-card! 200))))
            (is (schema= [(s/one {:card_id            (s/eq card-id)
                                  :parameter_mappings (s/eq mappings)
                                  s/Keyword           s/Any}
