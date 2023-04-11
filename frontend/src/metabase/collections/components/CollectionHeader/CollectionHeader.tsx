c034c4ea419e79ee8989be8ab4540b12d7e2b2bc
Nick Fitzpatrick <nick@metabase.com>
2023-04-11 11:46:33 -0300

    WIP - 29598 csv status toast (#29757)
    * temp
    
    * generalize StatusListing types
    
    * Putting a pin in this for now
    
    * poorly done typing
    
    * generalizing upload function names
    
    * point to real API
    
    * Fixing Storybook
    
    * Permissions for Status Listing
    
    * use formdata and fix types
    
    ---------
    
    Co-authored-by: Ryan Laurie <iethree@gmail.com>

--- frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.tsx
+++ frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { withRouter } from "react-router";
 import type { Location } from "history";
 
-import { Collection } from "metabase-types/api";
+import { Collection, CollectionId } from "metabase-types/api";
 
 import CollectionCaption from "./CollectionCaption";
 import CollectionBookmark from "./CollectionBookmark";
@@ -21,6 +21,7 @@ export interface CollectionHeaderProps {
   onUpdateCollection: (entity: Collection, values: Partial<Collection>) => void;
   onCreateBookmark: (collection: Collection) => void;
   onDeleteBookmark: (collection: Collection) => void;
+  onUpload: (file: File, collectionId: CollectionId) => void;
 }
 
 const CollectionHeader = ({
@@ -32,6 +33,7 @@ const CollectionHeader = ({
   onUpdateCollection,
   onCreateBookmark,
   onDeleteBookmark,
+  onUpload,
 }: CollectionHeaderProps): JSX.Element => {
   const canUpload = true; // TODO: check settings for upload permissions
 
@@ -49,7 +51,9 @@ const CollectionHeader = ({
           onCreateBookmark={onCreateBookmark}
           onDeleteBookmark={onDeleteBookmark}
         />
-        {canUpload && <CollectionUpload collection={collection} />}
+        {canUpload && (
+          <CollectionUpload collection={collection} onUpload={onUpload} />
+        )}
         <CollectionMenu
           collection={collection}
           isAdmin={isAdmin}
---
+++ frontend/src/metabase/status/components/StatusLarge/StatusLarge.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Ellipsified from "metabase/core/components/Ellipsified";
+import Icon from "../../../components/Icon";
+import useStatusVisibility from "../../hooks/use-status-visibility";
+import {
+  StatusCardRoot,
+  StatusCardIcon,
+  StatusCardBody,
+  StatusCardTitle,
+  StatusCardDescription,
+  StatusCardSpinner,
+  StatusCardIconContainer,
+  StatusRoot,
+  StatusHeader,
+  StatusTitle,
+  StatusToggle,
+  StatusBody,
+} from "./StatusLarge.styled";
+
+type status = {
+  title: string;
+  items: statusItem[];
+};
+
+type statusItem = {
+  id?: number;
+  title: string;
+  icon: string;
+  description?: string | JSX.Element;
+  isInProgress: boolean;
+  isCompleted: boolean;
+  isAborted: boolean;
+};
+
+export interface StatusLargeProps {
+  status: status;
+  isActive?: boolean;
+  onCollapse?: () => void;
+}
+
+const StatusLarge = ({
+  status,
+  isActive,
+  onCollapse,
+}: StatusLargeProps): JSX.Element => {
+  return (
+    <StatusRoot role="status">
+      <StatusHeader>
+        <StatusTitle>{status.title}</StatusTitle>
+        {onCollapse && (
+          <StatusToggle onClick={onCollapse}>
+            <Icon name="chevrondown" />
+          </StatusToggle>
+        )}
+      </StatusHeader>
+      <StatusBody>
+        {status.items.map(item => (
+          <StatusCard item={item} isActive={isActive} key={item.id} />
+        ))}
+      </StatusBody>
+    </StatusRoot>
+  );
+};
+
+interface StatusCardProps {
+  item: statusItem;
+  isActive?: boolean;
+}
+
+const StatusCard = ({
+  item,
+  isActive,
+}: StatusCardProps): JSX.Element | null => {
+  const { id, title, icon, description, isInProgress, isCompleted, isAborted } =
+    item;
+
+  const isVisible = useStatusVisibility(isActive || isInProgress);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <StatusCardRoot key={id}>
+      <StatusCardIcon>
+        <Icon name={icon} />
+      </StatusCardIcon>
+      <StatusCardBody>
+        <StatusCardTitle>
+          <Ellipsified>{title}</Ellipsified>
+        </StatusCardTitle>
+        <StatusCardDescription>{description}</StatusCardDescription>
+      </StatusCardBody>
+      {isInProgress && <StatusCardSpinner size={24} borderWidth={3} />}
+      {isCompleted && (
+        <StatusCardIconContainer>
+          <Icon name="check" size={12} />
+        </StatusCardIconContainer>
+      )}
+      {isAborted && (
+        <StatusCardIconContainer isError={true}>
+          <Icon name="warning" size={12} />
+        </StatusCardIconContainer>
+      )}
+    </StatusCardRoot>
+  );
+};
+
+export default StatusLarge;
