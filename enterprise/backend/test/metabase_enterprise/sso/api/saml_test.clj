4f98a200aa1a88aa797a7c6403a780611c216134
Jerry Huang <jhuang37050@gmail.com>
2023-04-12 11:16:41 -0700

    add test

--- enterprise/backend/test/metabase_enterprise/sso/api/saml_test.clj
+++ enterprise/backend/test/metabase_enterprise/sso/api/saml_test.clj
@@ -13,4 +13,9 @@
       (testing "Invalid SAML settings returns 400"
         (mt/user-http-request :crowberto :put 400 "saml/settings" {:saml-keystore-path "/path/to/keystore"
                                                                    :saml-keystore-password "password"
-                                                                   :saml-keystore-alias "alias"})))))
+                                                                   :saml-keystore-alias "alias"})
+
+        (testing "Blank SAML settings returns 200"
+          (mt/user-http-request :crowberto :put 200 "saml/settings" {:saml-keystore-path nil
+                                                                     :saml-keystore-password nil
+                                                                     :saml-keystore-alias nil}))))))
