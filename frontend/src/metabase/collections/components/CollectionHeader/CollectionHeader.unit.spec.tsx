faefccef4af5f8d2a8cafcafaacf93c10b5ec788
uladzimir <uladzimir.dev@gmail.com>
2023-04-12 11:27:50 +0300

    fixup! Add markdown to EditableText

--- frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.unit.spec.tsx
+++ frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.unit.spec.tsx
@@ -81,6 +81,10 @@ describe("CollectionHeader", () => {
 
       render(<CollectionHeader {...props} />);
 
+      // show input
+      const editableText = screen.getByText("Description");
+      userEvent.click(editableText);
+
       const input = screen.getByDisplayValue("Description");
       userEvent.clear(input);
       userEvent.type(input, "New description");
