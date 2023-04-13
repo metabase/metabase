6e34f9d42fec800a4d62ac7a7cc234e31fa81981
Jerry Huang <jhuang37050@gmail.com>
2023-04-13 11:31:59 -0700

    split tests

--- enterprise/backend/test/metabase_enterprise/sso/api/saml_test.clj
+++ enterprise/backend/test/metabase_enterprise/sso/api/saml_test.clj
@@ -8,14 +8,13 @@
     (testing "Valid SAML settings can be saved via an API call"
       (mt/user-http-request :crowberto :put 200 "saml/settings" {:saml-keystore-path "test_resources/keystore.jks"
                                                                  :saml-keystore-password "123456"
-                                                                 :saml-keystore-alias "sp"})
+                                                                 :saml-keystore-alias "sp"}))
+    (testing "Blank SAML settings returns 200"
+      (mt/user-http-request :crowberto :put 200 "saml/settings" {:saml-keystore-path nil
+                                                                 :saml-keystore-password nil
+                                                                 :saml-keystore-alias nil}))
 
-      (testing "Invalid SAML settings returns 400"
-        (mt/user-http-request :crowberto :put 400 "saml/settings" {:saml-keystore-path "/path/to/keystore"
-                                                                   :saml-keystore-password "password"
-                                                                   :saml-keystore-alias "alias"})
-
-        (testing "Blank SAML settings returns 200"
-          (mt/user-http-request :crowberto :put 200 "saml/settings" {:saml-keystore-path nil
-                                                                     :saml-keystore-password nil
-                                                                     :saml-keystore-alias nil}))))))
+    (testing "Invalid SAML settings returns 400"
+      (mt/user-http-request :crowberto :put 400 "saml/settings" {:saml-keystore-path "/path/to/keystore"
+                                                                 :saml-keystore-password "password"
+                                                                 :saml-keystore-alias "alias"})))
