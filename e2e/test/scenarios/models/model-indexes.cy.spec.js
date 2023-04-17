908b91b35b875a38ad1dc3d260f6f38e0093ab5b
Ryan Laurie <iethree@gmail.com>
2023-04-17 17:05:17 -0600

    e2e test entity indexes

---
+++ e2e/test/scenarios/models/model-indexes.cy.spec.js
@@ -0,0 +1,124 @@
+import {
+  restore,
+  openQuestionActions,
+  popover,
+  sidebar,
+} from "e2e/support/helpers";
+
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import { openColumnOptions } from "./helpers/e2e-models-metadata-helpers";
+
+const { PRODUCTS_ID, PEOPLE_ID } = SAMPLE_DATABASE;
+
+describe("scenarios > model indexes", () => {
+  const pkRef = ["field", 3, null];
+  const valueRef = ["field", 2, null];
+  const modelId = 4;
+
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+    cy.intercept("GET", "/api/search?q=*").as("searchQuery");
+    cy.intercept("POST", "/api/dataset").as("dataset");
+    cy.intercept("POST", "/api/model-index").as("modelIndex");
+
+    cy.createQuestion({
+      name: "Products Model",
+      query: { "source-table": PRODUCTS_ID },
+      dataset: true,
+    });
+  });
+
+  it("should create a model index on product titles", () => {
+    cy.visit(`/model/${modelId}`);
+    cy.wait("@dataset");
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+
+    cy.url().should("include", "/metadata");
+
+    cy.findByTestId("TableInteractive-root").findByTextEnsureVisible("Title");
+
+    openColumnOptions("Title");
+
+    sidebar()
+      .findByLabelText(/surface individual records/i)
+      .click();
+
+    cy.findByTestId("dataset-edit-bar").within(() => {
+      cy.button("Save changes").click();
+    });
+
+    cy.wait("@modelIndex").then(({ request, response }) => {
+      expect(request.body.pk_ref).to.deep.equal(pkRef);
+      expect(request.body.value_ref).to.deep.equal(valueRef);
+      expect(request.body.model_id).to.equal(modelId);
+
+      // this will likely change when this becomes an async process
+      expect(response.body.state).to.equal("indexed");
+      expect(response.body.id).to.equal(1);
+    });
+  });
+
+  it("should be able to search model index values and visit detail records", () => {
+    createModelIndex({ pkRef, valueRef, modelId });
+
+    cy.visit("/");
+
+    cy.findByTestId("app-bar")
+      .findByPlaceholderText("Search…")
+      .type("marble shoes");
+
+    cy.wait("@searchQuery");
+
+    cy.findByTestId("search-results-list")
+      .findByText("Small Marble Shoes")
+      .click();
+
+    cy.wait("@dataset");
+
+    cy.findByTestId("object-detail").within(() => {
+      cy.findByText("Product");
+      cy.findByText("Small Marble Shoes");
+      cy.findByText("Doohickey");
+    });
+  });
+
+  it.skip("should be able to see details of a record outside the first 2000", () => {
+    cy.createQuestion({
+      name: "People Model",
+      query: { "source-table": PEOPLE_ID },
+      dataset: true,
+    });
+    createModelIndex({
+      pkRef: ["field", 32, null],
+      valueRef: ["field", 35, null],
+      modelId: 5,
+    });
+
+    cy.visit("/");
+
+    cy.findByTestId("app-bar").findByPlaceholderText("Search…").type("anais");
+
+    cy.wait("@searchQuery");
+
+    cy.findByTestId("search-results-list").findByText("Anais Zieme").click();
+
+    cy.wait("@dataset");
+
+    cy.findByTestId("object-detail").within(() => {
+      cy.findByText(/We're a little lost/i).should("not.exist");
+    });
+  });
+});
+
+function createModelIndex({ pkRef, valueRef, modelId }) {
+  cy.request("POST", "/api/model-index", {
+    pk_ref: pkRef,
+    value_ref: valueRef,
+    model_id: modelId,
+  }).then(response => {
+    expect(response.body.state).to.equal("indexed");
+    expect(response.body.id).to.equal(1);
+  });
+}
