a52efa4d0e40334d582e8096036ba9dbc5d58e1f
Denis Berezin <denis.berezin@metabase.com>
2023-04-18 00:00:10 +0300

    Self-review, resolved some of todos

--- frontend/src/metabase/modes/components/modes/TimeseriesMode.jsx
+++ frontend/src/metabase/modes/components/modes/TimeseriesMode.jsx
@@ -29,7 +29,6 @@ const TimeseriesModeFooter = props => {
 
 const TimeseriesMode = {
   name: "timeseries",
-  // TODO: should we move "UnderlyingRecordsDrill" to be the first one here?
   drills: [PivotByCategoryDrill, PivotByLocationDrill, ...DefaultMode.drills],
   ModeFooter: TimeseriesModeFooter,
 };
