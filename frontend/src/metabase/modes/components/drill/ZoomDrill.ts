ff6ac197f5f39162e49b827ee3e8ac23af7b5c9f
Denis Berezin <denis.berezin@metabase.com>
2023-04-19 00:41:22 +0300

    Add updated zoom drill title

--- frontend/src/metabase/modes/components/drill/ZoomDrill.ts
+++ frontend/src/metabase/modes/components/drill/ZoomDrill.ts
@@ -1,23 +1,64 @@
 import { t } from "ttag";
+import type { DimensionValue } from "metabase-types/types/Visualization";
+import { LocalFieldReference } from "metabase-types/types/Query";
 import {
-  zoomDrill,
+  getNextZoomDrilldown,
   zoomDrillQuestion,
 } from "metabase-lib/queries/drills/zoom-drill";
+import { FieldDimension } from "metabase-lib/Dimension";
+import { formatBucketing } from "metabase-lib/queries/utils/query-time";
 import type { Drill } from "../../types";
 
+const generateDrillTitle = (
+  dimensions: DimensionValue[],
+  drilldown: { breakouts: LocalFieldReference[] },
+): string => {
+  let currentGranularity;
+  dimensions.some(dimension => {
+    if (dimension.column.field_ref) {
+      const field = FieldDimension.parseMBQL(dimension.column.field_ref);
+      if (field && field.temporalUnit()) {
+        currentGranularity = formatBucketing(
+          field.temporalUnit(),
+        ).toLowerCase();
+        return true;
+      }
+    }
+  });
+
+  let newGranularity;
+  drilldown.breakouts.some(breakout => {
+    const field = FieldDimension.parseMBQL(breakout);
+    if (field && field.temporalUnit()) {
+      newGranularity = formatBucketing(field.temporalUnit()).toLowerCase();
+      return true;
+    }
+  });
+
+  if (currentGranularity && newGranularity) {
+    return t`See this ${currentGranularity} by ${newGranularity}`;
+  }
+
+  return t`Zoom in`;
+};
+
 const ZoomDrill: Drill = ({ question, clicked }) => {
-  if (!zoomDrill({ question, clicked })) {
+  const result = getNextZoomDrilldown(question, clicked);
+
+  if (!result) {
     return [];
   }
+
+  const { dimensions, drilldown } = result;
 
   return [
     {
       name: "timeseries-zoom",
-      title: t`Zoom in`,
+      title: generateDrillTitle(dimensions, drilldown),
       section: "zoom",
       icon: "zoom_in",
       buttonType: "horizontal",
-      question: () => zoomDrillQuestion({ question, clicked }),
+      question: () => zoomDrillQuestion(question, dimensions, drilldown),
     },
   ];
 };
