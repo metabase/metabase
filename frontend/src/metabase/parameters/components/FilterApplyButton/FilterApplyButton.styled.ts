b3988f24bb13ab5bfeadce678525d5853960d6ce
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-04-18 19:05:55 +0100

    Review: Don't animate (un)mount of FilterApplyButton

--- frontend/src/metabase/parameters/components/FilterApplyButton/FilterApplyButton.styled.ts
+++ frontend/src/metabase/parameters/components/FilterApplyButton/FilterApplyButton.styled.ts
@@ -3,9 +3,6 @@ import styled from "@emotion/styled";
 import { space } from "metabase/styled-components/theme";
 import Button from "metabase/core/components/Button/Button";
 
-export const TRANSITION_CLASSNAMES_PREFIX = "filter-apply-button";
-export const BUTTON_TRANSITION_DURATION = 200;
-
 export const ApplyButton = styled(Button)<{ isVisible: boolean }>`
   margin-left: auto;
   margin-top: ${space(1)};
@@ -16,25 +13,5 @@ export const ApplyButton = styled(Button)<{ isVisible: boolean }>`
   &,
   &:hover {
     transition-property: opacity, visibility;
-  }
-
-  &.${TRANSITION_CLASSNAMES_PREFIX}-enter {
-    opacity: 0;
-    transition: none;
-  }
-
-  &.${TRANSITION_CLASSNAMES_PREFIX}-enter-active {
-    opacity: 1;
-    transition: opacity ${BUTTON_TRANSITION_DURATION}ms;
-  }
-
-  &.${TRANSITION_CLASSNAMES_PREFIX}-exit {
-    opacity: 1;
-    transition: none;
-  }
-
-  &.${TRANSITION_CLASSNAMES_PREFIX}-exit-active {
-    opacity: 0;
-    transition: opacity ${BUTTON_TRANSITION_DURATION}ms;
   }
 `;
