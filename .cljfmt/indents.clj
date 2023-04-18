e727142c80a6a4785232a2829210854a4734b1ef
Mark Bastian <markbastian@gmail.com>
2023-04-18 17:09:30 -0600

    First pass at setting cljfmt up for formatting prs (or formatting in general)

--- .cljfmt/indents.clj
+++ .cljfmt/indents.clj
@@ -1,2 +1,21 @@
-{metabase.api.collection/foo [[:inner 0]]
- toucan.db/insert!           [[:block 1]]}
+;; https://github.com/weavejester/cljfmt/blob/master/cljfmt/resources/cljfmt/indents/clojure.clj
+{auto-parse                              [[:inner 0]]
+ check                                   [[:inner 0]]
+ ex-info                                 [[:inner 0]]
+ let-404                                 [[:block 1]]
+ perms/set-has-full-permissions-for-set? [[:inner 0]]
+ perms/set-has-full-permissions?         [[:inner 0]]
+ perms/set-has-partial-permissions?      [[:inner 0]]
+ publish-event!                          [[:block 1]]
+ qp.streaming/streaming-response         [[:inner 0]]
+ ;; Do we want this or not?
+ ;revision/revert!                        [[:inner 0]]
+ t2/select                               [[:block 1]]
+ t2/select-one                           [[:block 1]]
+ t2/select-one-fn                        [[:inner 0]]
+ t2/select-pk->fn                        [[:inner 0]]
+ t2/update!                              [[:inner 0]]
+ toucan.db/insert!                       [[:block 1]]
+ u/prog1                                 [[:inner 0]]
+ u/select-keys-when                      [[:inner 0]]}
+
--- bin/cljfmt.sh
+++ bin/cljfmt.sh
@@ -2,6 +2,17 @@
 
 set -euxo pipefail
 
-clojure -M:cljfmt \
---indents ./.cljfmt/indents.clj \
-src/metabase/api/collection.clj
+ARG=${1:-}
+
+STAGED_FILES=$(git diff --name-only --cached -- "*.clj" "*.cljc" "*.cljs")
+
+if [ "$ARG" == "--all" ]; then
+  clojure -M:cljfmt \
+    --indents ./.cljfmt/indents.clj
+elif [ "${#STAGED_FILES}" -gt 0 ]; then
+  clojure -M:cljfmt \
+    --indents ./.cljfmt/indents.clj \
+    $STAGED_FILES
+else
+  echo "No input. Either pass --all to format all files or stage files to be formatted."
+fi
