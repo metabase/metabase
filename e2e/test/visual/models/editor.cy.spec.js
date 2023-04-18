044723eaf66292d6d357ac432301f581b73f0464
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2023-04-18 11:16:52 +0200

    Update e2e/test/visual/models/editor.cy.spec.js
    Co-authored-by: Tim Macdonald <tim@metabase.com>

--- e2e/test/visual/models/editor.cy.spec.js
+++ e2e/test/visual/models/editor.cy.spec.js
@@ -3,10 +3,10 @@ import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 
 const { ORDERS, ORDERS_ID, PRODUCTS, PRODUCTS_ID } = SAMPLE_DATABASE;
 
-// The card query is failry complex one and it takes a long time to complete
+// The card query is fairly complex one and it takes a long time to complete
 // on slow machines, like the ones used in CI.
-// We've seen timeouts with the Cypress default `requestTimeout` of 5000ms
-// for the `cardQuery` route. Hence, why we need to increase it to 30000ms.
+// We've seen timeouts with the Cypress default `requestTimeout` of 5,000ms
+// for the `cardQuery` route. Hence, why we need to increase it to 30,000ms.
 describe("visual tests > models > editor", { requestTimeout: 30000 }, () => {
   beforeEach(() => {
     restore();
