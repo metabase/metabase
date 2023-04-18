a5583d6eb0942eacbc550f4d95728c0eef02eec9
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-04-18 19:05:55 +0100

    Review: Don't animate (un)mount of FilterApplyButton

--- frontend/src/metabase/parameters/components/FilterApplyButton/FilterApplyButton.tsx
+++ frontend/src/metabase/parameters/components/FilterApplyButton/FilterApplyButton.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useRef } from "react";
-import { CSSTransition } from "react-transition-group";
+import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "metabase/lib/redux";
 
 import {
@@ -7,11 +6,7 @@ import {
   getIsAutoApplyFilters,
 } from "metabase/dashboard/selectors";
 import { applyDraftParameterValues } from "metabase/dashboard/actions";
-import {
-  ApplyButton,
-  BUTTON_TRANSITION_DURATION,
-  TRANSITION_CLASSNAMES_PREFIX,
-} from "./FilterApplyButton.styled";
+import { ApplyButton } from "./FilterApplyButton.styled";
 
 export default function FilterApplyButton() {
   const isAutoApplyFilters = useSelector(getIsAutoApplyFilters);
@@ -24,27 +19,17 @@ export default function FilterApplyButton() {
     dispatch(applyDraftParameterValues());
   }, [dispatch]);
 
-  const applyButtonRef = useRef(null);
+  if (isAutoApplyFilters) {
+    return null;
+  }
 
   return (
-    <CSSTransition
-      in={!isAutoApplyFilters}
-      unmountOnExit
-      classNames={TRANSITION_CLASSNAMES_PREFIX}
-      timeout={{
-        enter: BUTTON_TRANSITION_DURATION,
-        exit: BUTTON_TRANSITION_DURATION,
-      }}
-      nodeRef={applyButtonRef}
+    <ApplyButton
+      primary
+      isVisible={hasUnappliedParameterValues}
+      onClick={handleApplyFilters}
     >
-      <ApplyButton
-        ref={applyButtonRef}
-        primary
-        isVisible={hasUnappliedParameterValues}
-        onClick={handleApplyFilters}
-      >
-        Apply
-      </ApplyButton>
-    </CSSTransition>
+      Apply
+    </ApplyButton>
   );
 }
