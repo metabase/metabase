c395621432a13e15525bf1e79ab9d0513302fd07
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-04-19 15:22:41 +0300

    Change routes

--- frontend/src/metabase/admin/datamodel/metadata/components/MetadataHeader/MetadataHeader.tsx
+++ frontend/src/metabase/admin/datamodel/metadata/components/MetadataHeader/MetadataHeader.tsx
@@ -1,17 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router";
+import { push } from "react-router-redux";
+import _ from "underscore";
 import * as Urls from "metabase/lib/urls";
+import Databases from "metabase/entities/databases";
 import Icon from "metabase/components/Icon/Icon";
 import { DatabaseDataSelector } from "metabase/query_builder/components/DataSelector";
 import { Database, DatabaseId, TableId } from "metabase-types/api";
+import { Dispatch } from "metabase-types/store";
 
-export interface MetadataHeaderProps {
-  databases: Database[];
+interface OwnProps {
   selectedDatabaseId: DatabaseId;
   selectedSchemaName?: string;
   selectedTableId?: TableId;
+}
+
+interface DatabaseLoaderProps {
+  databases: Database[];
+}
+
+interface DispatchProps {
   onSelectDatabase: (databaseId: DatabaseId) => void;
 }
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  onSelectDatabase: databaseId =>
+    dispatch(push(Urls.dataModelDatabase(databaseId))),
+});
+
+type MetadataHeaderProps = OwnProps & DatabaseLoaderProps & DispatchProps;
 
 const MetadataHeader = ({
   databases,
@@ -53,4 +71,7 @@ const MetadataHeader = ({
   );
 };
 
-export default MetadataHeader;
+export default _.compose(
+  Databases.loadList(),
+  connect(null, mapDispatchToProps),
+)(MetadataHeader);
