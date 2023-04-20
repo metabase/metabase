dfaa4a84f62fbea2f8e47d2a30c69da8a3320931
Noah Moss <noahbmoss@gmail.com>
2023-04-20 10:16:09 -0400

    edit migration and get card_create event working

--- src/metabase/models/audit_log.clj
+++ src/metabase/models/audit_log.clj
@@ -1,10 +1,12 @@
 (ns ^{:added "0.47.0"}
- metabase.models.audit_log
+ metabase.models.audit-log
   "Model defenition for the Metabase Audit Log, which tracks (almost) all actions taken by users across the Metabase
   app. This is a distinct from the Activity and View Log models, which predate this namespace, and which power specific
   API endpoints used for in-app functionality, such as the recently-viewed items displayed on the homepage."
   (:require
+   [metabase.events :as events]
    [metabase.models.interface :as mi]
+   [metabase.util :as u]
    [methodical.core :as m]
    [toucan2.core :as t2]))
 
@@ -14,19 +16,31 @@
 
 (defmulti model-details
   "Returns a map with data about an entity that should be included in the `details` column of the Audit Log."
-  {:arglists '([model entity event-type])}
+  {:arglists '([entity event-type])}
   mi/dispatch-on-model)
 
 (defmethod model-details :default
-  [_model _entity _event-type]
+  [_entity _event-type]
   {})
 
-#_(defn- pre-insert [activity]
-    (let [defaults {:timestamp :%now
-                    :details   {}}]
-      (merge defaults activity)))
+(defn record-event!
+  "Record an event in the Audit Log."
+  [topic object]
+  ;; Push an event to the event queue so that activity table is still populated
+  (events/publish-event! topic object)
+  (let [details (model-details object topic)]
+    (t2/insert! :m/audit-log
+                {:topic    topic
+                 :details  details
+                 :model    (name (t2/model object))
+                 :model_id (u/the-id object)})))
 
-#_(mi/define-methods
-    AuditLog
-    {:types      (constantly {:details :json, :topic :keyword})
-     :pre-insert pre-insert})
+(defn- pre-insert [activity]
+  (let [defaults {:timestamp :%now
+                  :details   {}}]
+    (merge defaults activity)))
+
+(mi/define-methods
+  :m/audit-log
+  {:types      (constantly {:details :json, :topic :keyword})
+   :pre-insert pre-insert})
