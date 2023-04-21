c875522bc166699c4b6ee3bb0d62e5283259d2ed
John Cromartie <john@metabase.com>
2023-04-21 15:08:53 -0400

    Moves json-response :error handling outside of postprocess

--- src/metabase/metabot.clj
+++ src/metabase/metabot.clj
@@ -212,35 +212,33 @@
 (defn- postprocess-result
   [{model-id :id :keys [field-id->field-ref mbql_malli_schema database_id]}
    json-response]
-  (if (:error json-response)
-    {:fail json-response}
-    (let [{:keys [breakout aggregation filters]
-           :as   coerced-response} (mc/coerce mbql_malli_schema json-response mtx/json-transformer)]
-      (if (mc/validate mbql_malli_schema coerced-response)
-        (let [inner-mbql (cond-> (assoc
-                                  (select-keys coerced-response [:breakout :aggregation])
-                                  :source-table (format "card__%s" model-id))
-                           breakout
-                           (update :breakout (partial mapv field-id->field-ref))
-                           aggregation
-                           (update :aggregation (partial mapv (fn [[op id]]
-                                                                [op (field-id->field-ref id)])))
-                           filters
-                           (update :filters (fn [filters]
-                                              (mapv
-                                               (fn [[op id m]]
-                                                 (let [{:keys [field_id value]} m]
-                                                   [op
-                                                    (field-id->field-ref id)
-                                                    (if field_id
-                                                      (field-id->field-ref field_id)
-                                                      value)]))
-                                               filters))))]
-          (tap> inner-mbql)
-          {:database database_id
-           :type     :query
-           :query    inner-mbql})
-        {:fail coerced-response}))))
+  (let [{:keys [breakout aggregation filters]
+         :as   coerced-response} (mc/coerce mbql_malli_schema json-response mtx/json-transformer)]
+    (if (mc/validate mbql_malli_schema coerced-response)
+      (let [inner-mbql (cond-> (assoc
+                                (select-keys coerced-response [:breakout :aggregation])
+                                :source-table (format "card__%s" model-id))
+                         breakout
+                         (update :breakout (partial mapv field-id->field-ref))
+                         aggregation
+                         (update :aggregation (partial mapv (fn [[op id]]
+                                                              [op (field-id->field-ref id)])))
+                         filters
+                         (update :filters (fn [filters]
+                                            (mapv
+                                             (fn [[op id m]]
+                                               (let [{:keys [field_id value]} m]
+                                                 [op
+                                                  (field-id->field-ref id)
+                                                  (if field_id
+                                                    (field-id->field-ref field_id)
+                                                    value)]))
+                                             filters))))]
+        (tap> inner-mbql)
+        {:database database_id
+         :type     :query
+         :query    inner-mbql})
+      {:fail coerced-response})))
 
 (defn- ->prompt
   "Returns {:messages [{:role ... :content ...} ...]} prompt map for use in API calls."
@@ -280,7 +278,9 @@
                        (metabot-client/invoke-metabot prompt))]
     (tap> json-response)
     (try
-      (postprocess-result new-model json-response)
+      (if (:error json-response)
+        {:fail json-response}
+        (postprocess-result new-model json-response))
       (catch Exception _
         {:fail   json-response
          :reason :invalid-response}))))
