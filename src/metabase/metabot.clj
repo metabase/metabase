0c309df5406d3c3d2b643fb4a0b743338c49fcf4
Mark Bastian <markbastian@gmail.com>
2023-04-24 10:48:11 -0600

    Fixing postprocessing logic

--- src/metabase/metabot.clj
+++ src/metabase/metabot.clj
@@ -2,20 +2,20 @@
   "The core metabot namespace. Consists primarily of functions named infer-X,
   where X is the thing we want to extract from the bot response."
   (:require
-   [cheshire.core :as json]
-   [clojure.string :as str]
-   [malli.core :as mc]
-   [malli.generator :as mg]
-   [malli.json-schema :as mjs]
-   [malli.transform :as mtx]
-   [metabase.lib.native :as lib-native]
-   [metabase.metabot.client :as metabot-client]
-   [metabase.metabot.settings :as metabot-settings]
-   [metabase.metabot.util :as metabot-util]
-   [metabase.models :refer [Table]]
-   [metabase.query-processor :as qp]
-   [metabase.util.log :as log]
-   [toucan2.core :as t2]))
+    [cheshire.core :as json]
+    [clojure.string :as str]
+    [malli.core :as mc]
+    [malli.generator :as mg]
+    [malli.json-schema :as mjs]
+    [malli.transform :as mtx]
+    [metabase.lib.native :as lib-native]
+    [metabase.metabot.client :as metabot-client]
+    [metabase.metabot.settings :as metabot-settings]
+    [metabase.metabot.util :as metabot-util]
+    [metabase.models :refer [Table]]
+    [metabase.query-processor :as qp]
+    [metabase.util.log :as log]
+    [toucan2.core :as t2]))
 
 (defn infer-viz
   "Determine an 'interesting' visualization for this data."
@@ -143,92 +143,106 @@
   "Returns Malli schema for subset of MBQL constrained to available field IDs"
   [{:keys [result_metadata]}]
   (mc/schema
-   [:map {:registry
-          {::available_fields
-           (into [:enum
-                  {:title       "Field id"
-                   :description "The id for a field to be used in the query"}]
-                 (mapv :id result_metadata))
-           ::available_aggregations
-           (into [:enum
-                  {:title       "Aggregation"
-                   :description "An aggregation that can be performed on data"}]
-                 [:avg :count :count-where :distinct :max :median
-                  :min :percentile :share :stddev :sum :sum-where :var])
-           ::operators
-           (into [:enum
-                  {:title       "Boolean operators"
-                   :description "af"}]
-                 [:< :<= := :>= :>])}}
-    [:aggregation
-     {:optional true}
-     [:vector
-      [:tuple {:title       "Aggregation"
-               :description "A single aggregate operation over a field"}
-       ::available_aggregations
-       ::available_fields]]]
-    [:breakout
-     {:optional true}
-     [:vector {:min 1} ::available_fields]]
-    [:filters
-     {:optional true}
-     [:vector
-      [:tuple {:title       "Filter"
-               :description "A boolean operation that can be used to filter results"}
-       ::operators
-       ::available_fields
-       [:or
-        [:map [:field_id ::available_fields]]
-        [:map [:value [:or :int :double :string]]]]]]]
-    [:limit
-     {:title       "Limit"
-      :description "The number of items to return in a query."
-      :optional true}
-     pos-int?]
-    [:order-by
-     {:title       "Sort order"
-      :description "A sequential set of asc|desc plus field id tuples determining the return data sort order."
-      :optional    true}
-     [:tuple
-      [:enum :asc :desc]
-      ::available_fields]]]))
+    [:map {:registry
+           {::available_fields
+            (into [:enum
+                   {:title       "Field id"
+                    :description "The id for a field to be used in the query"}]
+                  (mapv :id result_metadata))
+            ::available_aggregations
+            (into [:enum
+                   {:title       "Aggregation"
+                    :description "An aggregation that can be performed on data"}]
+                  [:avg :count :count-where :distinct :max :median
+                   :min :percentile :share :stddev :sum :sum-where :var])
+            ::operators
+            (into [:enum
+                   {:title       "Boolean operators"
+                    :description "af"}]
+                  [:< :<= := :>= :>])}}
+     [:aggregation
+      {:optional true}
+      [:vector
+       [:tuple {:title       "Aggregation"
+                :description "A single aggregate operation over a field"}
+        ::available_aggregations
+        ::available_fields]]]
+     [:breakout
+      {:optional true}
+      [:vector {:min 1} ::available_fields]]
+     [:fields
+      {:optional true}
+      [:vector {:title       "Fields"
+                :description "Selected fields from the full set of available fields"
+                :min         1} ::available_fields]]
+     [:filters
+      {:optional true}
+      [:vector
+       [:tuple {:title       "Filter"
+                :description "A boolean operation that can be used to filter results"}
+        ::operators
+        ::available_fields
+        [:or
+         [:map [:field_id ::available_fields]]
+         [:map [:value [:or :int :double :string]]]]]]]
+     [:limit
+      {:title       "Limit"
+       :description "The number of items to return in a query."
+       :optional    true}
+      pos-int?]
+     [:order-by
+      {:title       "Sort order"
+       :description "A sequential set of asc|desc plus field id tuples determining the return data sort order."
+       :optional    true}
+      [:vector
+       [:tuple
+        [:enum :asc :desc]
+        ::available_fields]]]]))
 
 (comment
   ;; This is useful for making sure the above schema is right
-  (let [model               (t2/select-one 'Card :id 1)
+  (let [model            (t2/select-one 'Card :id 1)
         {:keys [result_metadata]} model
-        available-fields    (mapv #(select-keys % [:id :name]) result_metadata)
-        malli-schema        (schema available-fields)]
+        available-fields (mapv #(select-keys % [:id :name]) result_metadata)
+        malli-schema     (schema available-fields)]
     (mg/generate malli-schema)))
 
 (defn- model-field-ref-lookup
   [{:keys [result_metadata]}]
   (zipmap
-   (map :id result_metadata)
-   (map :field_ref result_metadata)))
+    (map :id result_metadata)
+    (map :field_ref result_metadata)))
 
 (defn- postprocess-result
   [{model-id :id :keys [database_id] :as model} json-response]
-  (let [{:keys [breakout aggregation filters]
+  (let [{:keys [breakout aggregation fields filters order-by]
          :as   coerced-response} (mc/coerce (schema model) json-response mtx/json-transformer)
         id->ref    (model-field-ref-lookup model)
         inner-mbql (cond-> (assoc
-                            (select-keys coerced-response [:breakout :aggregation])
-                            :source-table (format "card__%s" model-id))
-                     breakout
-                     (update :breakout (partial mapv id->ref))
+                             coerced-response
+                             :source-table (format "card__%s" model-id))
                      aggregation
                      (update :aggregation (partial mapv (fn [[op id]]
                                                           [op (id->ref id)])))
+                     breakout
+                     (update :breakout (partial mapv id->ref))
+                     fields
+                     (update :fields (partial mapv id->ref))
                      filters
                      (update :filters (fn [filters]
                                         (mapv
-                                         (fn [[op id m]]
-                                           (let [{:keys [field_id value]} m]
-                                             [op
-                                              (id->ref id)
-                                              (or (id->ref field_id) value)]))
-                                         filters))))]
+                                          (fn [[op id m]]
+                                            (let [{:keys [field_id value]} m]
+                                              [op
+                                               (id->ref id)
+                                               (or (id->ref field_id) value)]))
+                                          filters)))
+                     order-by
+                     (update :order-by (fn [clauses]
+                                         (mapv
+                                           (fn [[op id]]
+                                             [op (id->ref id)])
+                                           clauses))))]
     (tap> inner-mbql)
     {:database database_id
      :type     :query
@@ -238,7 +252,7 @@
   "Returns {:messages [{:role ... :content ...} ...]} prompt map for use in API calls."
   [& role-content-pairs]
   {:messages (for [[role content] role-content-pairs]
-               {:role (name role)
+               {:role    (name role)
                 :content (if (sequential? content)
                            (str/join "\n" content)
                            content)})})
@@ -269,23 +283,23 @@
         json-schema   (mjs/transform malli-schema)
         field-info    (mapv #(select-keys % [:id :name]) result_metadata)
         prompt        (->prompt
-                       [:system ["You are a pedantic Metabase query generation assistant."
-                                 "You respond to user queries by building a JSON object that conforms to this json schema:"
-                                 (json-block json-schema)
-                                 "If you are unable to generate a query, return a JSON object like:"
-                                 (json-block {:error "I was unable to generate a query because..."
-                                              :query "<user's original query>"
-                                              :suggestion ["<example natural-language query that might work based on the data model>"]})
-                                 "A JSON description of the fields available in the user's data model:"
-                                 (json-block field-info)
-                                 "Take a natural-language query from the user and construct a query using the supplied schema and available fields."
-                                 "Respond only with schema compliant JSON."]]
-                       [:user user_prompt])
+                        [:system ["You are a pedantic Metabase query generation assistant."
+                                  "You respond to user queries by building a JSON object that conforms to this json schema:"
+                                  (json-block json-schema)
+                                  "If you are unable to generate a query, return a JSON object like:"
+                                  (json-block {:error      "I was unable to generate a query because..."
+                                               :query      "<user's original query>"
+                                               :suggestion ["<example natural-language query that might work based on the data model>"]})
+                                  "A JSON description of the fields available in the user's data model:"
+                                  (json-block field-info)
+                                  "Take a natural-language query from the user and construct a query using the supplied schema and available fields."
+                                  "Respond only with schema compliant JSON."]]
+                        [:user user_prompt])
         json-response (metabot-util/find-result
-                       (fn [message]
-                         (tap> message)
-                         (metabot-util/extract-json message))
-                       (metabot-client/invoke-metabot prompt))]
+                        (fn [message]
+                          (tap> message)
+                          (metabot-util/extract-json message))
+                        (metabot-client/invoke-metabot prompt))]
     (tap> json-response)
     ;; handle cases where the LLM detects its own errors first
     (if (:error json-response)
@@ -301,24 +315,24 @@
 (comment
   (->> (t2/select-one 'Card :id 1) :result_metadata (map :name))
 
-  (let [model (t2/select-one 'Card :id 1)
+  (let [model         (t2/select-one 'Card :id 1)
         json-response {:aggregation [["sum" 41]], :filters [["=" 50 {:value "Boston"}]]}
-        malli-schema (schema model)]
+        malli-schema  (schema model)]
     (mc/coerce malli-schema json-response mtx/json-transformer)
     malli-schema)
 
   ;; This works pretty well
   (let [{:keys [fail] :as mbql} (infer-mbql
-                                 "Provide descriptive stats for sales per state"
-                                 (t2/select-one 'Card :id 1))]
+                                  "Provide descriptive stats for sales per state"
+                                  (t2/select-one 'Card :id 1))]
     (if fail
       fail
       {:mbql mbql
        :data (qp/process-query mbql)}))
 
   (let [{:keys [fail] :as mbql} (infer-mbql
-                                 "What are the 10 highest rated products?"
-                                 (t2/select-one 'Card :id 1))]
+                                  "What are the 10 highest rated products?"
+                                  (t2/select-one 'Card :id 1))]
     (if fail
       [:fail fail]
       mbql))
@@ -348,19 +362,49 @@
   {:aggregation [["sum" 41]], :filters [["=" 50 "Boston"]]}
 
   (let [{:keys [fail] :as mbql} (infer-mbql
-                                 "How many sales were in Idaho?"
-                                 (t2/select-one 'Card :id 1))]
+                                  "How many sales were in Idaho?"
+                                  (t2/select-one 'Card :id 1))]
     (if fail
       fail
       {:mbql mbql
        :data (update-in (qp/process-query mbql) [:data :rows] (fn [rows] (take 10 rows)))}))
 
-  (let [malli-schema (schema (t2/select-one 'Card :id 1))
-        json-response {:aggregation [["count" 53]]
-                       :breakout    [53]
-                       :filters     [["=" 53 {:value "Idaho"}]]}]
-    ;(m/coerce malli-schema json-response mt/json-transformer)
-    (mg/generate malli-schema)
-    )
+  (let [model        (t2/select-one 'Card :id 1)
+        malli-schema (schema model)]
+    (mg/generate malli-schema))
+
+  ;; Generic stats on pricing by category
+  (let [model         (t2/select-one 'Card :id 1)
+        malli-schema  (schema model)
+        json-response {:aggregation [[:min 58]
+                                     [:max 58]
+                                     [:avg 58]],
+                       :breakout    [62]}
+        result        (postprocess-result
+                        model
+                        json-response)]
+    (qp/process-query result))
+
+  (let [model         (t2/select-one 'Card :id 1)
+        malli-schema  (schema model)
+        json-response {:fields   [58]
+                       :limit    10
+                       :order-by [[:asc 58]]}
+        result        (postprocess-result
+                        model
+                        json-response)]
+    (qp/process-query result))
+
+
+  (let [model        (t2/select-one 'Card :id 1)
+        malli-schema (schema model)
+        result       (postprocess-result
+                       model
+                       (mg/generate malli-schema))
+        limit        (get-in result [:query :limit])
+        result       (cond-> result
+                       (nil? limit)
+                       (assoc-in [:query :limit] 2))]
+    (qp/process-query result))
 
   )
