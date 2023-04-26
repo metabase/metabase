87ef4bcb1cdcbe39d3ed7e6af543bf2c1a2b563b
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-04-26 17:39:24 +0100

    Keep the linting config in a single file

--- frontend/src/metabase/lib/redux/hooks.ts
+++ frontend/src/metabase/lib/redux/hooks.ts
@@ -1,11 +1,9 @@
 import type { ThunkDispatch, AnyAction } from "@reduxjs/toolkit";
 import type { TypedUseSelectorHook } from "react-redux";
-/* eslint-disable no-restricted-imports */
 import {
   useDispatch as useDispatchOriginal,
   useSelector as useSelectorOriginal,
 } from "react-redux";
-/* eslint-enable no-restricted-imports */
 
 import type { State } from "metabase-types/store";
 
