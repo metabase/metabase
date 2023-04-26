e562f3e091b8f6fdfee592605f7e6da9b4d0c61c
Emmad Usmani <emmadusmani@berkeley.edu>
2023-04-26 11:59:28 -0700

    add horizontal scrolling

--- frontend/src/metabase/core/components/TabRow/TabRow.tsx
+++ frontend/src/metabase/core/components/TabRow/TabRow.tsx
@@ -1,8 +1,91 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
+import Icon from "metabase/components/Icon/Icon";
+import ExplicitSize from "metabase/components/ExplicitSize";
 import { TabListProps } from "../TabList/TabList";
-import { TabList } from "./TabRow.styled";
+import { ScrollButton, TabList } from "./TabRow.styled";
 
-export default function TabRow<T>({ onChange, ...props }: TabListProps<T>) {
-  return <TabList onChange={onChange as (value: unknown) => void} {...props} />;
+const UNDERSCROLL_PIXELS = 32;
+
+interface TabRowInnerProps<T> extends TabListProps<T> {
+  width: number | null;
+}
+
+function TabRowInner<T>({
+  width,
+  onChange,
+  children,
+  ...props
+}: TabRowInnerProps<T>) {
+  const tabListRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [showScrollRight, setShowScrollRight] = useState(false);
+  const showScrollLeft = scrollPosition > 0;
+
+  useEffect(() => {
+    if (!tabListRef.current || !width) {
+      return;
+    }
+
+    const hideScrollArrows = width === tabListRef.current.scrollWidth;
+    if (hideScrollArrows) {
+      setScrollPosition(0);
+      setShowScrollRight(false);
+      return;
+    }
+
+    setShowScrollRight(scrollPosition + width < tabListRef.current.scrollWidth);
+  }, [width, scrollPosition]);
+
+  const scroll = (direction: "left" | "right") => {
+    if (!tabListRef.current || !width) {
+      return;
+    }
+
+    const scrollDistance =
+      (width - UNDERSCROLL_PIXELS) * (direction === "left" ? -1 : 1);
+    tabListRef.current.scrollBy(scrollDistance, 0);
+    setScrollPosition(
+      Math.max(0, tabListRef.current.scrollLeft + scrollDistance),
+    );
+  };
+
+  return (
+    <TabList
+      onChange={onChange as (value: unknown) => void}
+      ref={tabListRef}
+      {...props}
+    >
+      {children}
+      {showScrollLeft && (
+        <ScrollArrow direction="left" onClick={() => scroll("left")} />
+      )}
+      {showScrollRight && (
+        <ScrollArrow direction="right" onClick={() => scroll("right")} />
+      )}
+    </TabList>
+  );
+}
+
+const TabRowInnerWithSize = ExplicitSize()(TabRowInner);
+
+export default function TabRow<T>(props: TabListProps<T>) {
+  return <TabRowInnerWithSize {...props} />;
+}
+
+interface ScrollArrowProps {
+  direction: "left" | "right";
+  onClick: () => void;
+}
+
+export function ScrollArrow({ direction, onClick }: ScrollArrowProps) {
+  return (
+    <ScrollButton
+      onClick={onClick}
+      direction={direction}
+      aria-label={`scroll tabs ${direction}`}
+    >
+      <Icon name={`chevron${direction}`} color="brand" />
+    </ScrollButton>
+  );
 }
