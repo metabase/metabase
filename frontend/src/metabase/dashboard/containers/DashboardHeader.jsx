485c2ce34841557b44bbc96a46c9628ad6917610
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2023-04-26 18:31:35 +0100

    Don't reset parameter values after modifying dashboards (#29729)
    * Correct a dashboard Redux action signature
    
    * Ensure modifying dashboard doesn't wipe filters
    
    * Address review

--- frontend/src/metabase/dashboard/containers/DashboardHeader.jsx
+++ frontend/src/metabase/dashboard/containers/DashboardHeader.jsx
@@ -145,8 +145,8 @@ class DashboardHeader extends Component {
     );
   }
 
-  async onSave() {
-    await this.props.saveDashboardAndCards(this.props.dashboard.id);
+  async onSave(preserveParameters) {
+    await this.props.saveDashboardAndCards(preserveParameters);
     this.onDoneEditing();
   }
 
@@ -441,7 +441,7 @@ class DashboardHeader extends Component {
         editingButtons={this.getEditingButtons()}
         setDashboardAttribute={setDashboardAttribute}
         onLastEditInfoClick={() => setSidebar({ name: SIDEBAR_NAME.info })}
-        onSave={() => this.onSave()}
+        onSave={() => this.onSave(true)}
       />
     );
   }
