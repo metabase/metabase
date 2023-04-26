6a61b0840c8e09cd244889d4d80724e865515e4c
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-04-26 15:58:18 +0100

    Force import redux hooks from metabase lib redux instead
    This ensures we have correct state type without having to manually type
    our state every time.

--- frontend/src/metabase/lib/redux/hooks.ts
+++ frontend/src/metabase/lib/redux/hooks.ts
@@ -1,9 +1,11 @@
 import type { ThunkDispatch, AnyAction } from "@reduxjs/toolkit";
 import type { TypedUseSelectorHook } from "react-redux";
+/* eslint-disable no-restricted-imports */
 import {
   useDispatch as useDispatchOriginal,
   useSelector as useSelectorOriginal,
 } from "react-redux";
+/* eslint-enable no-restricted-imports */
 
 import type { State } from "metabase-types/store";
 
