80ba18210127f773cdfc96da569035ddaad0f048
uladzimir <uladzimir.dev@gmail.com>
2023-04-27 17:52:59 +0300

    fixup! Use FormProvider in SaveQuestionModal

--- frontend/src/metabase/containers/SaveQuestionModal.jsx
+++ frontend/src/metabase/containers/SaveQuestionModal.jsx
@@ -31,13 +31,13 @@ const getSingleStepTitle = (questionType, showSaveType) => {
 };
 
 const SAVE_QUESTION_SCHEMA = Yup.object({
-  saveType: Yup.string().oneOf(["overwrite", "create"]),
+  saveType: Yup.string(),
   name: Yup.string().when("saveType", {
     // We don't care if the form is valid when overwrite mode is enabled,
     // as original question's data will be submitted instead of the form values
     is: value => value !== "overwrite",
     then: Yup.string().required(Errors.required),
-    otherwise: Yup.string(),
+    otherwise: Yup.string().nullable(true),
   }),
 });
 
