c1ad149efb5fd1b09d9d14b58319ac4314ce4941
Nick Fitzpatrick <nickfitz.582@gmail.com>
2023-04-27 15:22:38 -0300

    updating homepage

--- frontend/src/metabase/home/homepage/components/HomePage/HomePage.tsx
+++ frontend/src/metabase/home/homepage/components/HomePage/HomePage.tsx
@@ -1,19 +1,29 @@
 import React, { useEffect } from "react";
 import { isSmallScreen } from "metabase/lib/dom";
+import Dashboard from "metabase/dashboard/containers/Dashboard";
 import HomeLayout from "../HomeLayout";
 import HomeContent from "../../containers/HomeContent";
 
 export interface HomePageProps {
   hasMetabot: boolean;
+  homepageDashboard: number;
   onOpenNavbar: () => void;
 }
 
-const HomePage = ({ hasMetabot, onOpenNavbar }: HomePageProps): JSX.Element => {
+const HomePage = ({
+  hasMetabot,
+  onOpenNavbar,
+  homepageDashboard,
+}: HomePageProps): JSX.Element => {
   useEffect(() => {
     if (!isSmallScreen()) {
       onOpenNavbar();
     }
   }, [onOpenNavbar]);
+
+  if (homepageDashboard) {
+    return <Dashboard dashboardId={homepageDashboard} />;
+  }
 
   return (
     <HomeLayout hasMetabot={hasMetabot}>
