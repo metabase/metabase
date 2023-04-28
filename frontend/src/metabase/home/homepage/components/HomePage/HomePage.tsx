79755878eddde1b4d27a0863f4fa9c0329791284
Nick Fitzpatrick <nickfitz.582@gmail.com>
2023-04-28 15:57:58 -0300

    permissions

--- frontend/src/metabase/home/homepage/components/HomePage/HomePage.tsx
+++ frontend/src/metabase/home/homepage/components/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { isSmallScreen } from "metabase/lib/dom";
 import Dashboard from "metabase/dashboard/containers/Dashboard";
+import Dashboards from "metabase/entities/dashboards";
 import HomeLayout from "../HomeLayout";
 import HomeContent from "../../containers/HomeContent";
 
@@ -15,14 +16,31 @@ const HomePage = ({
   onOpenNavbar,
   homepageDashboard,
 }: HomePageProps): JSX.Element => {
+  const [showDashboard, setShowDashboard] = useState(true);
   useEffect(() => {
     if (!isSmallScreen()) {
       onOpenNavbar();
     }
   }, [onOpenNavbar]);
 
-  if (homepageDashboard) {
-    return <Dashboard dashboardId={homepageDashboard} />;
+  if (homepageDashboard && showDashboard) {
+    return (
+      <Dashboards.Loader
+        id={homepageDashboard}
+        loadingAndErrorWrapper={false}
+        dispatchApiErrorEvent={false}
+      >
+        {({ loading, error, dashboard }) => {
+          if (!loading && error) {
+            setShowDashboard(false);
+          } else if (dashboard) {
+            return <Dashboard dashboardId={homepageDashboard} />;
+          }
+
+          return null;
+        }}
+      </Dashboards.Loader>
+    );
   }
 
   return (
