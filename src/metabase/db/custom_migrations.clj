589274d0e7f3afaad010458a0c7ac5f9a45291ca
Callum Herries <hi@callumherries.com>
2023-05-02 19:03:22 +0530

    Rework migration to only update field refs

--- src/metabase/db/custom_migrations.clj
+++ src/metabase/db/custom_migrations.clj
@@ -9,12 +9,13 @@
    If you need to use code from elsewhere, consider copying it into this namespace to minimize risk of the code changing behaviour."
   (:require
    [cheshire.core :as json]
+   [clojure.core.match :refer [match]]
    [clojure.set :as set]
    [clojurewerkz.quartzite.jobs :as jobs]
    [clojurewerkz.quartzite.scheduler :as qs]
    [clojurewerkz.quartzite.triggers :as triggers]
+   [medley.core :as m]
    [metabase.db.connection :as mdb.connection]
-   [metabase.models.interface :as mi]
    [metabase.plugins.classloader :as classloader]
    [metabase.util.honey-sql-2 :as h2x]
    [metabase.util.log :as log]
@@ -172,12 +173,36 @@
                      :set    {:json_unfolding true}
                      :where  [:in :metabase_field.id field-ids-to-update]}))))
 
-(define-migration MigrateLegacyVisualizationSettings
+(defn- update-legacy-field-refs [viz-settings]
+  (let [old-to-new (fn [old]
+                     (match old
+                       ["ref" ref] ["ref" (match ref
+                                            ["field-id" x] ["field" x nil]
+                                            ["field-literal" x y] ["field" x {"base-type" y}]
+                                            ["fk->" x y] (let [x (match x
+                                                                   [_x0 x1] x1
+                                                                   x x)
+                                                               y (match y
+                                                                   [_y0 y1] y1
+                                                                   y y)]
+                                                           ["field" y {:source-field x}])
+                                            ref ref)]
+                       k k))]
+    (-> viz-settings
+        (json/parse-string)
+        (m/update-existing "column_settings" update-keys
+                           (fn [k]
+                             (-> k
+                                 (json/parse-string)
+                                 (vec)
+                                 (old-to-new)
+                                 (json/generate-string))))
+        (json/generate-string))))
+
+(define-migration MigrateLegacyColumnSettingsFieldRefs
   (transduce
    (keep (fn [{:keys [id visualization_settings]}]
-           (let [updated (-> visualization_settings
-                             ((:out mi/transform-visualization-settings))
-                             ((:in mi/transform-visualization-settings)))]
+           (let [updated (update-legacy-field-refs visualization_settings)]
              (when (not= visualization_settings updated)
                {:id                     id
                 :visualization_settings updated}))))
