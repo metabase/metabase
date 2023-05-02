a194d1112dad61f0ad9116dd8941ab83c96a7c6c
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-02 20:35:25 +0100

    Unify bucket pickers

--- frontend/src/metabase/common/components/QueryColumnPicker/BinningStrategyPickerPopover.tsx
+++ frontend/src/metabase/common/components/QueryColumnPicker/BinningStrategyPickerPopover.tsx
@@ -1,68 +1,30 @@
-import React, { useMemo } from "react";
-import PopoverWithTrigger from "metabase/components/PopoverWithTrigger/TippyPopoverWithTrigger";
+import React from "react";
+import { t } from "ttag";
 import * as Lib from "metabase-lib";
-import {
-  TriggerButton,
-  TriggerIcon,
-  SelectList,
-  SelectListItem,
-} from "./ColumnPrecisionPickerPopover.styled";
+import BucketPickerPopover, {
+  BucketPickerPopoverProps,
+} from "./BucketPickerPopover";
 
-interface BinningStrategyPickerPopoverProps {
-  selectedItem?: Lib.Bucket | null;
-  query: Lib.Query;
-  items: Lib.Bucket[];
-  onSelect: (item: Lib.Bucket) => void;
+function renderTriggerContent(bucket?: Lib.BucketDisplayInfo) {
+  if (!bucket) {
+    return null;
+  }
+  if (bucket.displayName === "Don''t bin") {
+    return t`Unbinned`;
+  }
+  if (bucket.displayName === "Auto bin") {
+    return t`Auto binned`;
+  }
+  return bucket.displayName;
 }
 
-type BinningStrategyListItem = Lib.BucketDisplayInfo & {
-  binningStrategy: Lib.Bucket;
-};
-
-function BinningStrategyPickerPopover({
-  selectedItem,
-  query,
-  items,
-  onSelect,
-}: BinningStrategyPickerPopoverProps) {
-  const displayableItems: BinningStrategyListItem[] = useMemo(
-    () =>
-      items.map(binningStrategy => ({
-        ...Lib.displayInfo(query, binningStrategy),
-        binningStrategy,
-      })),
-    [query, items],
-  );
-
-  const defaultBinningStrategy = displayableItems.find(item => item.default);
-  const activeBinningStrategy =
-    selectedItem || defaultBinningStrategy?.binningStrategy;
-
-  const selectedItemName = activeBinningStrategy
-    ? Lib.displayInfo(query, activeBinningStrategy).displayName
-    : null;
-
+function BinningStrategyPickerPopover(
+  props: Omit<BucketPickerPopoverProps, "renderTriggerContent">,
+) {
   return (
-    <PopoverWithTrigger
-      renderTrigger={({ onClick }) => (
-        <TriggerButton onClick={onClick}>
-          {selectedItemName}
-          <TriggerIcon name="chevronright" />
-        </TriggerButton>
-      )}
-      popoverContent={
-        <SelectList>
-          {displayableItems.map(item => (
-            <SelectListItem
-              id={item.displayName}
-              key={item.displayName}
-              name={item.displayName}
-              isSelected={item.binningStrategy === selectedItem}
-              onSelect={() => onSelect(item.binningStrategy)}
-            />
-          ))}
-        </SelectList>
-      }
+    <BucketPickerPopover
+      {...props}
+      renderTriggerContent={renderTriggerContent}
     />
   );
 }
