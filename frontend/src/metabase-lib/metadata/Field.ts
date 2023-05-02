a9e6da532540608cb8c1fd67944124172a2550d6
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-02 22:55:32 +0100

    Fix type error

--- frontend/src/metabase-lib/metadata/Field.ts
+++ frontend/src/metabase-lib/metadata/Field.ts
@@ -71,7 +71,7 @@ class FieldInner extends Base {
   table?: Table;
   table_id?: Table["id"];
   target?: Field;
-  fk_target_field_id?: Field["id"];
+  fk_target_field_id?: Field["id"] | null;
   name_field?: Field["id"];
   remapping?: unknown;
   has_field_values?: "list" | "search" | "none";
