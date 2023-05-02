d4ad5dd29a4ad3bc573cbb8087d46e661f5fa03d
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-02 16:16:21 +0100

    Add `BinningStrategyPickerPopover`

---
+++ frontend/src/metabase/common/components/QueryColumnPicker/BinningStrategyPickerPopover.tsx
@@ -0,0 +1,70 @@
+import React, { useMemo } from "react";
+import PopoverWithTrigger from "metabase/components/PopoverWithTrigger/TippyPopoverWithTrigger";
+import * as Lib from "metabase-lib";
+import {
+  TriggerButton,
+  TriggerIcon,
+  SelectList,
+  SelectListItem,
+} from "./ColumnPrecisionPickerPopover.styled";
+
+interface BinningStrategyPickerPopoverProps {
+  selectedItem?: Lib.BinningStrategy | null;
+  query: Lib.Query;
+  items: Lib.BinningStrategy[];
+  onSelect: (item: Lib.BinningStrategy) => void;
+}
+
+type BinningStrategyListItem = Lib.BinningStrategyDisplayInfo & {
+  binningStrategy: Lib.BinningStrategy;
+};
+
+function BinningStrategyPickerPopover({
+  selectedItem,
+  query,
+  items,
+  onSelect,
+}: BinningStrategyPickerPopoverProps) {
+  const displayableItems: BinningStrategyListItem[] = useMemo(
+    () =>
+      items.map(binningStrategy => ({
+        ...Lib.displayInfo(query, binningStrategy),
+        binningStrategy,
+      })),
+    [query, items],
+  );
+
+  const defaultBinningStrategy = displayableItems.find(item => item.default);
+  const activeBinningStrategy =
+    selectedItem || defaultBinningStrategy?.binningStrategy;
+
+  const selectedItemName = activeBinningStrategy
+    ? Lib.displayInfo(query, activeBinningStrategy).displayName
+    : null;
+
+  return (
+    <PopoverWithTrigger
+      renderTrigger={({ onClick }) => (
+        <TriggerButton onClick={onClick}>
+          {selectedItemName}
+          <TriggerIcon name="chevronright" />
+        </TriggerButton>
+      )}
+      popoverContent={
+        <SelectList>
+          {displayableItems.map(item => (
+            <SelectListItem
+              id={item.displayName}
+              key={item.displayName}
+              name={item.displayName}
+              isSelected={item.binningStrategy === selectedItem}
+              onSelect={() => onSelect(item.binningStrategy)}
+            />
+          ))}
+        </SelectList>
+      }
+    />
+  );
+}
+
+export default BinningStrategyPickerPopover;
