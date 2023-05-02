f07d58b3379a316f28c4c0075536ea3b9f50326d
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-05-02 19:24:21 +0100

    Remove puzzle driven development comments

--- frontend/src/metabase/hooks/use-loading-timer.ts
+++ frontend/src/metabase/hooks/use-loading-timer.ts
@@ -9,12 +9,6 @@ export function useLoadingTimer(isLoading: boolean, props: LoadingTimerProps) {
   const { onTimeout, timer } = props;
   useEffect(() => {
     if (isLoading) {
-      // XXX: drive-by refactor
-      // It doesn't make much sense to have `isLoading`
-      // in the timeout because it's guaranteed to be true
-      // since the first `if (isLoading)` block.
-      // In this case we could just pass the `onTimeout` callback
-      // to the `setTimeout` function directly.
       const timeoutId = setTimeout(onTimeout, timer);
       return () => clearTimeout(timeoutId);
     }
