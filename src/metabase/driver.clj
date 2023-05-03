91474aff84dfb7e47f392de5836c0d18a508b23f
Callum Herries <hi@callumherries.com>
2023-05-03 22:29:42 +0530

    Add schemas driver feature flag

--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -493,6 +493,9 @@
     ;; Does the driver support uploading files
     :uploads
 
+    ;; Does the driver support schemas (aka namespaces) for tables
+    :schemas
+
     ;; Does the driver support custom writeback actions. Drivers that support this must
     ;; implement [[execute-write-query!]]
     :actions/custom
