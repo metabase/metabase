f7e4913cbefe2e7a44ab58e14abf4f0b0659874c
Ryan Laurie <iethree@gmail.com>
2023-05-03 05:04:19 -0600

    update e2e tests

--- e2e/test/scenarios/models/model-indexes.cy.spec.js
+++ e2e/test/scenarios/models/model-indexes.cy.spec.js
@@ -11,8 +11,6 @@ import { openColumnOptions } from "./helpers/e2e-models-metadata-helpers";
 const { PRODUCTS_ID, PEOPLE_ID } = SAMPLE_DATABASE;
 
 describe("scenarios > model indexes", () => {
-  const pkRef = ["field", 3, null];
-  const valueRef = ["field", 2, null];
   const modelId = 4;
 
   beforeEach(() => {
@@ -45,8 +43,6 @@ describe("scenarios > model indexes", () => {
     });
 
     cy.wait("@modelIndexCreate").then(({ request, response }) => {
-      expect(request.body.pk_ref).to.deep.equal(pkRef);
-      expect(request.body.value_ref).to.deep.equal(valueRef);
       expect(request.body.model_id).to.equal(modelId);
 
       // this will likely change when this becomes an async process
@@ -71,7 +67,7 @@ describe("scenarios > model indexes", () => {
   });
 
   it("should be able to search model index values and visit detail records", () => {
-    createModelIndex({ pkRef, valueRef, modelId });
+    createModelIndex({ modelId, pkName: "ID", valueName: "TITLE" });
 
     cy.visit("/");
 
@@ -94,17 +90,13 @@ describe("scenarios > model indexes", () => {
     });
   });
 
-  it.skip("should be able to see details of a record outside the first 2000", () => {
+  it("should be able to see details of a record outside the first 2000", () => {
     cy.createQuestion({
       name: "People Model",
       query: { "source-table": PEOPLE_ID },
       dataset: true,
     });
-    createModelIndex({
-      pkRef: ["field", 32, null],
-      valueRef: ["field", 35, null],
-      modelId: 5,
-    });
+    createModelIndex({ modelId: 5, pkName: "ID", valueName: "NAME" });
 
     cy.visit("/");
 
@@ -115,9 +107,11 @@ describe("scenarios > model indexes", () => {
     cy.findByTestId("search-results-list").findByText("Anais Zieme").click();
 
     cy.wait("@dataset");
+    cy.wait("@dataset"); // second query gets the additional record
 
     cy.findByTestId("object-detail").within(() => {
       cy.findByText(/We're a little lost/i).should("not.exist");
+      cy.findAllByText("Anais Zieme").should("have.length", 2);
     });
   });
 });
@@ -125,21 +119,35 @@ describe("scenarios > model indexes", () => {
 function editTitleMetadata() {
   openQuestionActions();
   popover().findByText("Edit metadata").click();
-
   cy.url().should("include", "/metadata");
-
   cy.findByTestId("TableInteractive-root").findByTextEnsureVisible("Title");
 
   openColumnOptions("Title");
 }
 
-function createModelIndex({ pkRef, valueRef, modelId }) {
-  cy.request("POST", "/api/model-index", {
-    pk_ref: pkRef,
-    value_ref: valueRef,
-    model_id: modelId,
-  }).then(response => {
-    expect(response.body.state).to.equal("indexed");
-    expect(response.body.id).to.equal(1);
-  });
+function createModelIndex({ modelId, pkName, valueName }) {
+  // since field ids are non-deterministic, we need to get them from the api
+  cy.request("GET", `/api/table/card__${modelId}/query_metadata`).then(
+    ({ body }) => {
+      const pkRef = [
+        "field",
+        body.fields.find(f => f.name === pkName).id,
+        null,
+      ];
+      const valueRef = [
+        "field",
+        body.fields.find(f => f.name === valueName).id,
+        null,
+      ];
+
+      cy.request("POST", "/api/model-index", {
+        pk_ref: pkRef,
+        value_ref: valueRef,
+        model_id: modelId,
+      }).then(response => {
+        expect(response.body.state).to.equal("indexed");
+        expect(response.body.id).to.equal(1);
+      });
+    },
+  );
 }
