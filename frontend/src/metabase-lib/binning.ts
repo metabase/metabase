8e7c77dbfe4cdc13a838719e44036424655bef1f
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-04 14:31:13 +0100

    Fix bucket methods usage in Jest

--- frontend/src/metabase-lib/binning.ts
+++ frontend/src/metabase-lib/binning.ts
@@ -1,18 +1,22 @@
 import { to_array } from "cljs/cljs.core";
-import * as ML from "cljs/metabase.lib.core";
+import {
+  available_binning_strategies,
+  binning as _binning,
+  with_binning,
+} from "cljs/metabase.lib.core";
 import type { Bucket, ColumnMetadata, Query } from "./types";
 
 export function binning(query: Query, column: ColumnMetadata): Bucket | null {
-  return ML.binning(query, column);
+  return _binning(query, column);
 }
 
 export function availableBinningStrategies(
   query: Query,
   column: ColumnMetadata,
 ): Bucket[] {
-  return to_array(ML.available_binning_strategies(query, column));
+  return to_array(available_binning_strategies(query, column));
 }
 
 export function withBinning(column: ColumnMetadata, binningStrategy: Bucket) {
-  return ML.with_binning(column, binningStrategy);
+  return with_binning(column, binningStrategy);
 }
