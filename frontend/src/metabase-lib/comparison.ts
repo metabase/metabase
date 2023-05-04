ff8b55c11749463e92275ae34c5aa48335cd39c1
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-04 14:31:51 +0100

    Add `isClauseColumn` helper

--- frontend/src/metabase-lib/comparison.ts
+++ frontend/src/metabase-lib/comparison.ts
@@ -1,5 +1,7 @@
 import * as ML from "cljs/metabase.lib.js";
 import type { DatasetQuery } from "metabase-types/api";
+import { displayInfo } from "./metadata";
+import type { Query, Clause, ColumnMetadata } from "./types";
 
 export function areLegacyQueriesEqual(
   query1: DatasetQuery,
@@ -7,4 +9,18 @@ export function areLegacyQueriesEqual(
   fieldIds?: number[],
 ): boolean {
   return ML.query_EQ_(query1, query2, fieldIds);
+}
+
+export function isClauseColumn(
+  query: Query,
+  clause: Clause,
+  column: ColumnMetadata,
+) {
+  const clauseInfo = displayInfo(query, clause);
+  const columnInfo = displayInfo(query, column);
+
+  return (
+    clauseInfo.table?.name === columnInfo.table?.name &&
+    clauseInfo.name === columnInfo.name
+  );
 }
