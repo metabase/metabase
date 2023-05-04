8eae220ec9b08b50d249a2f079b3c9e1175d5ca5
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-04 14:31:13 +0100

    Fix bucket methods usage in Jest

--- frontend/src/metabase-lib/temporal_bucket.ts
+++ frontend/src/metabase-lib/temporal_bucket.ts
@@ -1,24 +1,28 @@
 import { to_array } from "cljs/cljs.core";
-import * as ML from "cljs/metabase.lib.core";
+import {
+  temporal_bucket,
+  available_temporal_buckets,
+  with_temporal_bucket,
+} from "cljs/metabase.lib.core";
 import type { Bucket, ColumnMetadata, Query } from "./types";
 
 export function temporalBucket(
   query: Query,
   column: ColumnMetadata,
 ): Bucket | null {
-  return ML.temporal_bucket(query, column);
+  return temporal_bucket(query, column);
 }
 
 export function availableTemporalBuckets(
   query: Query,
   column: ColumnMetadata,
 ): Bucket[] {
-  return to_array(ML.available_temporal_buckets(query, column));
+  return to_array(available_temporal_buckets(query, column));
 }
 
 export function withTemporalBucket(
   column: ColumnMetadata,
   bucket: Bucket,
 ): ColumnMetadata {
-  return ML.with_temporal_bucket(column, bucket);
+  return with_temporal_bucket(column, bucket);
 }
