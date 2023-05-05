3aa1cc3ce465e8d59ca4a4d3ce16006c81945a56
Bryan Maass <bryan.maass@gmail.com>
2023-05-05 13:41:42 -0600

    clean up audit-db install/update/noop logic

--- enterprise/backend/src/metabase_enterprise/audit_db.clj
+++ enterprise/backend/src/metabase_enterprise/audit_db.clj
@@ -2,28 +2,52 @@
   "This is here so we can try to require it and see whether or not EE code is on the classpath."
   (:require [metabase.models.database :refer [Database]]
             [metabase.sync.schedules :as sync.schedules]
+            [metabase.db.env :as mdb.env]
             [metabase.util :as u]
             [metabase.util.log :as log]
             [toucan2.core :as t2]))
 
 (defn- install-database!
   "Creates the audit db, a clone of the app db used for auditing purposes."
-  []
-  (t2/insert! Database {:is_audit     true
-                        :name         "Audit Database"
-                        :description  "Internal Audit DB used to power metabase analytics."
-                        :engine       "appdb"
-                        :is_full_sync true
-                        :is_on_demand false
-                        :creator_id   nil
+  [engine]
+  (t2/insert! Database {:is_audit         true
+                        :name             "Audit Database"
+                        :description      "Internal Audit DB used to power metabase analytics."
+                        :engine           engine
+                        :is_full_sync     true
+                        :is_on_demand     false
+                        :creator_id       nil
                         :auto_run_queries true}))
 
 (defn ensure-db-installed!
   "Called on app startup to ensure the existance of the audit db in enterprise apps."
   []
-  (if (t2/select-one Database :is_audit true)
-    :metabase-enterprise.audit-db/no-op
+  (let [audit-db (t2/select-one Database :is_audit true)]
+    (cond
+      (nil? audit-db)
+      (u/prog1 ::installed
+        (log/info "Audit DB does not exist, Installing...")
+        (install-database! mdb.env/db-type))
 
-    (u/prog1 :metabase-enterprise.audit-db/installed
-      (log/info "Audit DB does not exist, Installing...")
-      (install-database!))))
+      (not= mdb.env/db-type (:engine audit-db))
+      (u/prog1 ::updated
+        (log/infof "Updating the Audit DB engine to %s." (name mdb.env/db-type))
+        (t2/update! Database :is_audit true {:engine mdb.env/db-type})
+        (ensure-db-installed!))
+
+      :else
+      :metabase-enterprise.audit-db/no-op)))
+
+
+
+
+(comment
+
+  [(t2/delete! Database :is_audit true)
+   (ensure-db-installed!)
+   (ensure-db-installed!)
+
+   (t2/update! Database :is_audit true {:engine "shmostgres"})
+   (ensure-db-installed!)
+   (ensure-db-installed!)]
+  )
--- enterprise/backend/test/metabase_enterprise/audit_db_test.clj
+++ enterprise/backend/test/metabase_enterprise/audit_db_test.clj
@@ -5,18 +5,19 @@
             [metabase.util.log :as log]
             [toucan2.core :as t2]))
 
-(defmacro with-delete-audit-db [& body]
-  `(let [original-audit-db# (t2/select-one Database :is_audit true)]
-     (try
-       (t2/delete! Database :is_audit true)
-       ~@body
-       (finally
-         (t2/delete! Database :is_audit true)
-         (when original-audit-db#
-           (log/fatal (str "Original Audit DB: " (pr-str original-audit-db#)))
-           (#'t2/insert! Database original-audit-db#))))))
-
 (deftest audit-db-is-installed-then-left-alone
-  (with-delete-audit-db
-    (is (= ::audit-db/installed (audit-db/ensure-db-installed!)))
-    (is (= ::audit-db/no-op (audit-db/ensure-db-installed!)))))
+  (let [original-audit-db (t2/select-one Database :is_audit true)]
+    (try
+      (t2/delete! Database :is_audit true)
+      (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-db-installed!)))
+      (is (= :metabase-enterprise.audit-db/no-op (audit-db/ensure-db-installed!)))
+
+      (t2/update! Database :is_audit true {:engine "datomic"})
+      (is (= :metabase-enterprise.audit-db/updated (audit-db/ensure-db-installed!)))
+      (is (= :metabase-enterprise.audit-db/no-op (audit-db/ensure-db-installed!)))
+
+      (finally
+        (t2/delete! Database :is_audit true)
+        (when original-audit-db
+          (log/fatal (str "Original Audit DB: " (pr-str original-audit-db)))
+          (#'t2/insert! Database original-audit-db))))))
