b7c551ad74df97204017e2b90761f8bd030718ed
Maz Ameli <maz@metabase.com>
2023-05-05 16:35:15 -0700

    arrows, formula, snippets

--- frontend/src/metabase/query_builder/components/template_tags/snippet-sidebar/SnippetRow.jsx
+++ frontend/src/metabase/query_builder/components/template_tags/snippet-sidebar/SnippetRow.jsx
@@ -7,8 +7,6 @@ import Icon from "metabase/components/Icon";
 import Snippets from "metabase/entities/snippets";
 import { SnippetButton } from "./SnippetRow.styled";
 
-const ICON_SIZE = 16;
-
 class SnippetRowInner extends React.Component {
   constructor(props) {
     super(props);
@@ -42,21 +40,15 @@ class SnippetRowInner extends React.Component {
                   }
             }
           >
-            <Icon
-              name="snippet"
-              size={ICON_SIZE}
-              className="hover-child--hidden text-light"
-            />
+            <Icon name="snippet" className="hover-child--hidden text-light" />
             <Icon
               name={insertSnippet ? "arrow_left_to_line" : "snippet"}
-              size={ICON_SIZE}
               className="hover-child"
             />
             <span className="flex-full ml1">{snippet.name}</span>
           </div>
           <Icon
             name={isOpen ? "chevronup" : "chevrondown"}
-            size={ICON_SIZE}
             className={cx({ "hover-child": !isOpen })}
           />
         </div>
