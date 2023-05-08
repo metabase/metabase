de77c552cdc9d62068026608d2da7bde4d8fd8cc
Oisin Coveney <oisin@metabase.com>
2023-05-08 10:20:27 +0300

    Add warning message when editing database permissions (#30415)

--- frontend/src/metabase/admin/permissions/pages/DataPermissionsPage/DataPermissionsPage.tsx
+++ frontend/src/metabase/admin/permissions/pages/DataPermissionsPage/DataPermissionsPage.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useCallback, ReactNode } from "react";
 import _ from "underscore";
-import { connect } from "react-redux";
+import { Route } from "react-router";
 
 import Tables from "metabase/entities/tables";
 import Groups from "metabase/entities/groups";
 import Databases from "metabase/entities/databases";
 
+import { DatabaseId } from "metabase-types/api";
+import { useDispatch, useSelector } from "metabase/lib/redux";
 import { getIsDirty, getDiff } from "../../selectors/data-permissions/diff";
 import {
   saveDataPermissions,
@@ -17,51 +18,45 @@ import PermissionsPageLayout from "../../components/PermissionsPageLayout/Permis
 import { DataPermissionsHelp } from "../../components/DataPermissionsHelp";
 import ToolbarUpsell from "../../components/ToolbarUpsell";
 
+type DataPermissionsPageProps = {
+  children: ReactNode;
+  route: typeof Route;
+  params: {
+    databaseId: DatabaseId;
+  };
+};
+
 export const DATA_PERMISSIONS_TOOLBAR_CONTENT = [
   <ToolbarUpsell key="upsell" />,
 ];
 
-const mapDispatchToProps = {
-  loadPermissions: loadDataPermissions,
-  savePermissions: saveDataPermissions,
-  initialize: initializeDataPermissions,
-  fetchTables: dbId =>
-    Tables.actions.fetchList({
-      dbId,
-      include_hidden: true,
-    }),
-};
-
-const mapStateToProps = (state, props) => ({
-  isDirty: getIsDirty(state, props),
-  diff: getDiff(state, props),
-});
-
-const propTypes = {
-  children: PropTypes.node.isRequired,
-  isDirty: PropTypes.bool,
-  diff: PropTypes.object,
-  savePermissions: PropTypes.func.isRequired,
-  loadPermissions: PropTypes.func.isRequired,
-  initialize: PropTypes.func.isRequired,
-  route: PropTypes.object,
-  params: PropTypes.shape({
-    databaseId: PropTypes.string,
-  }),
-  fetchTables: PropTypes.func,
-};
-
 function DataPermissionsPage({
   children,
-  isDirty,
-  diff,
-  savePermissions,
-  loadPermissions,
   route,
   params,
-  initialize,
-  fetchTables,
-}) {
+}: DataPermissionsPageProps) {
+  const isDirty = useSelector(getIsDirty);
+  const diff = useSelector(getDiff);
+
+  const dispatch = useDispatch();
+
+  const loadPermissions = () => dispatch(loadDataPermissions());
+  const savePermissions = () => dispatch(saveDataPermissions());
+  const initialize = useCallback(
+    () => dispatch(initializeDataPermissions()),
+    [dispatch],
+  );
+  const fetchTables = useCallback(
+    (dbId: DatabaseId) =>
+      dispatch(
+        Tables.actions.fetchList({
+          dbId,
+          include_hidden: true,
+        }),
+      ),
+    [dispatch],
+  );
+
   useEffect(() => {
     initialize();
   }, [initialize]);
@@ -89,12 +84,9 @@ function DataPermissionsPage({
   );
 }
 
-DataPermissionsPage.propTypes = propTypes;
-
 export default _.compose(
   Groups.loadList(),
   Databases.loadList({
     selectorName: "getListUnfiltered",
   }),
-  connect(mapStateToProps, mapDispatchToProps),
 )(DataPermissionsPage);
