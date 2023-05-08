c38950635bc67bea804140e07873bcfcd34e3dc1
Ryan Laurie <iethree@gmail.com>
2023-05-08 13:35:49 -0600

    add styling to dropzone overlay

--- frontend/src/metabase/collections/components/UploadOverlay/UploadOverlay.tsx
+++ frontend/src/metabase/collections/components/UploadOverlay/UploadOverlay.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { t } from "ttag";
 
+import type { Collection } from "metabase-types/api";
+import Icon from "metabase/components/Icon";
 import { DragOverlay } from "./UploadOverlay.styled";
 
 export default function UploadOverlay({
   isDragActive,
+  collection,
 }: {
   isDragActive: boolean;
+  collection: Collection;
 }) {
   return (
     <DragOverlay isDragActive={isDragActive}>
-      {t`Drop a CSV file to upload to this collection`}
+      <Icon name="arrow_up" />
+      <div>{t`Drop here to upload to ${collection.name}`}</div>
     </DragOverlay>
   );
 }
--- frontend/src/metabase/collections/containers/CollectionContent.jsx
+++ frontend/src/metabase/collections/containers/CollectionContent.jsx
@@ -203,7 +203,7 @@ function CollectionContent({
 
   const canUpload = uploadsEnabled && collection.can_write;
 
-  const rootProps = canUpload ? getRootProps() : {};
+  const dropzoneProps = canUpload ? getRootProps() : {};
 
   const unpinnedQuery = {
     collection: collectionId,
@@ -232,8 +232,13 @@ function CollectionContent({
         const hasPinnedItems = pinnedItems.length > 0;
 
         return (
-          <CollectionRoot {...rootProps}>
-            {canUpload && <UploadOverlay isDragActive={isDragActive} />}
+          <CollectionRoot {...dropzoneProps}>
+            {canUpload && (
+              <UploadOverlay
+                isDragActive={isDragActive}
+                collection={collection}
+              />
+            )}
             <CollectionMain>
               <Header
                 collection={collection}
