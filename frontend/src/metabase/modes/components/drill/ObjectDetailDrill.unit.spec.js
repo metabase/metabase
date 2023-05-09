cd42f17e5a93972c9f7ee08adb135a6caf553235
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-09 17:09:34 +0100

    Migrate `ObjectDetailDrill`

--- frontend/src/metabase/modes/components/drill/ObjectDetailDrill.unit.spec.js
+++ frontend/src/metabase/modes/components/drill/ObjectDetailDrill.unit.spec.js
@@ -1,19 +1,30 @@
+import { createMockMetadata } from "__support__/metadata";
 import ObjectDetailDrill from "metabase/modes/components/drill/ObjectDetailDrill";
 import { ZOOM_IN_ROW } from "metabase/query_builder/actions";
-import { TYPE as SEMANTIC_TYPE } from "cljs/metabase.types";
 import {
+  createSampleDatabase,
   ORDERS,
+  ORDERS_ID,
   PRODUCTS,
-  SAMPLE_DATABASE,
-  metadata,
-} from "__support__/sample_database_fixture";
+  PRODUCTS_ID,
+  SAMPLE_DB_ID,
+} from "metabase-types/api/mocks/presets";
+import { TYPE as SEMANTIC_TYPE } from "cljs/metabase.types";
 import Question from "metabase-lib/Question";
+
+const metadata = createMockMetadata({
+  databases: [createSampleDatabase()],
+});
+
+const ordersTable = metadata.table(ORDERS_ID);
+const orderIDField = metadata.field(ORDERS.ID);
+const orderIDColumn = metadata.field(ORDERS.ID).column();
 
 const DEFAULT_CELL_VALUE = 1;
 
 function setup({
-  question = ORDERS.question(),
-  column = ORDERS.ID.column(),
+  question = ordersTable.question(),
+  column = orderIDColumn,
   value = DEFAULT_CELL_VALUE,
   extraData,
 } = {}) {
@@ -33,9 +44,9 @@ const SAVED_QUESTION = new Question(
     name: "orders",
     dataset_query: {
       type: "query",
-      database: SAMPLE_DATABASE.id,
+      database: SAMPLE_DB_ID,
       query: {
-        "source-table": ORDERS.id,
+        "source-table": ORDERS_ID,
       },
     },
   },
@@ -44,15 +55,15 @@ const SAVED_QUESTION = new Question(
 
 describe("ObjectDetailDrill", () => {
   it("should not be valid for top level actions", () => {
-    const actions = ObjectDetailDrill({ question: ORDERS.question() });
+    const actions = ObjectDetailDrill({ question: ordersTable.question() });
     expect(actions).toHaveLength(0);
   });
 
   it("should not be valid when clicked value is undefined", () => {
     const actions = ObjectDetailDrill({
-      question: ORDERS.question(),
+      question: ordersTable.question(),
       clicked: {
-        column: ORDERS.ID.column(),
+        column: orderIDColumn,
         value: undefined,
       },
     });
@@ -61,26 +72,26 @@ describe("ObjectDetailDrill", () => {
 
   it("should not be valid non-PK cells", () => {
     const { actions: totalActions } = setup({
-      column: ORDERS.TOTAL.column(),
+      column: metadata.field(ORDERS.TOTAL).column(),
     });
     const { actions: createdAtActions } = setup({
-      column: ORDERS.CREATED_AT.column(),
+      column: metadata.field(ORDERS.CREATED_AT).column(),
     });
     expect(totalActions).toHaveLength(0);
     expect(createdAtActions).toHaveLength(0);
   });
 
   it("should not be available for not editable queries", () => {
-    const question = ORDERS.question();
+    const question = ordersTable.question();
     question.query().isEditable = () => false;
 
     const pk = setup({
       question,
-      column: ORDERS.ID.column(),
+      column: orderIDColumn,
     });
     const fk = setup({
       question,
-      column: ORDERS.PRODUCT_ID.column(),
+      column: metadata.field(ORDERS.PRODUCT_ID).column(),
     });
 
     expect(pk.actions).toHaveLength(0);
@@ -92,7 +103,7 @@ describe("ObjectDetailDrill", () => {
       const mockDispatch = jest.fn();
       const mockGetState = () => ({ qb: { queryResults: {}, card: {} } });
       const { actions, cellValue } = setup({
-        column: ORDERS.ID.column(),
+        column: orderIDColumn,
       });
 
       it("should return object detail filter", () => {
@@ -114,16 +125,16 @@ describe("ObjectDetailDrill", () => {
 
       describe("composite PK", () => {
         describe("default", () => {
-          const question = ORDERS.question();
+          const question = ordersTable.question();
           const orderTotalField = question
             .query()
             .table()
-            .fields.find(field => field.id === ORDERS.TOTAL.id);
+            .fields.find(field => field.id === ORDERS.TOTAL);
           orderTotalField.semantic_type = SEMANTIC_TYPE.PK;
 
           const { actions, cellValue } = setup({
             question,
-            column: ORDERS.ID.column(),
+            column: orderIDColumn,
           });
 
           it("should return object detail filter", () => {
@@ -135,8 +146,8 @@ describe("ObjectDetailDrill", () => {
           it("should apply '=' filter to one of the PKs on click", () => {
             const [action] = actions;
             expect(action.question().datasetQuery().query).toEqual({
-              "source-table": ORDERS.id,
-              filter: ["=", ORDERS.ID.reference(), cellValue],
+              "source-table": ORDERS_ID,
+              filter: ["=", orderIDField.reference(), cellValue],
             });
           });
 
@@ -144,11 +155,11 @@ describe("ObjectDetailDrill", () => {
         });
 
         describe("when table metadata is unavailable", () => {
-          let question = ORDERS.question();
+          let question = ordersTable.question();
           const fields = question.query().table().fields;
           question = question.setResultsMetadata({
             columns: fields.map(field => {
-              if (field.id === ORDERS.TOTAL.id) {
+              if (field.id === ORDERS.TOTAL) {
                 return {
                   ...field,
                   semantic_type: SEMANTIC_TYPE.PK,
@@ -162,14 +173,14 @@ describe("ObjectDetailDrill", () => {
 
           const { actions, cellValue } = setup({
             question,
-            column: ORDERS.ID.column(),
+            column: orderIDColumn,
           });
 
           it("should fallback to result metadata info about columns if table is not available", () => {
             const [action] = actions;
             expect(action.question().datasetQuery().query).toEqual({
-              "source-table": ORDERS.id,
-              filter: ["=", ORDERS.ID.reference(), cellValue],
+              "source-table": ORDERS_ID,
+              filter: ["=", orderIDField.reference(), cellValue],
             });
           });
         });
@@ -180,7 +191,7 @@ describe("ObjectDetailDrill", () => {
       describe("without parameters", () => {
         const { actions } = setup({
           question: SAVED_QUESTION,
-          column: ORDERS.ID.column(),
+          column: orderIDColumn,
           extraData: {
             dashboard: { id: 5 },
           },
@@ -207,7 +218,7 @@ describe("ObjectDetailDrill", () => {
     describe("with parameters", () => {
       const { actions, cellValue } = setup({
         question: SAVED_QUESTION,
-        column: ORDERS.ID.column(),
+        column: orderIDColumn,
         extraData: {
           dashboard: { id: 5 },
           parameterValuesBySlug: {
@@ -237,7 +248,7 @@ describe("ObjectDetailDrill", () => {
   describe("FK cells", () => {
     describe("with a FK column", () => {
       const { actions, cellValue } = setup({
-        column: ORDERS.PRODUCT_ID.column(),
+        column: metadata.field(ORDERS.PRODUCT_ID).column(),
       });
 
       it("should return object detail filter", () => {
@@ -249,8 +260,8 @@ describe("ObjectDetailDrill", () => {
       it("should apply object detail filter correctly", () => {
         const [action] = actions;
         expect(action.question().datasetQuery().query).toEqual({
-          "source-table": PRODUCTS.id,
-          filter: ["=", PRODUCTS.ID.reference(), cellValue],
+          "source-table": PRODUCTS_ID,
+          filter: ["=", metadata.field(PRODUCTS.ID).reference(), cellValue],
         });
       });
 
@@ -264,7 +275,7 @@ describe("ObjectDetailDrill", () => {
       const { actions, cellValue } = setup({
         column: {
           semantic_type: SEMANTIC_TYPE.FK,
-          fk_target_field_id: PRODUCTS.ID.id,
+          fk_target_field_id: PRODUCTS.ID,
         },
       });
 
@@ -277,8 +288,8 @@ describe("ObjectDetailDrill", () => {
       it("should apply object detail filter correctly", () => {
         const [action] = actions;
         expect(action.question().datasetQuery().query).toEqual({
-          "source-table": PRODUCTS.id,
-          filter: ["=", PRODUCTS.ID.reference(), cellValue],
+          "source-table": PRODUCTS_ID,
+          filter: ["=", metadata.field(PRODUCTS.ID).reference(), cellValue],
         });
       });
       it("should supply the foreign key as a return value from the extra() function", () => {
