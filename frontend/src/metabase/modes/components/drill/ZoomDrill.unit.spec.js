5645261fec774ad5f47692aebd76024e3037439a
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-09 17:12:53 +0100

    Migrate `ZoomDrill`

--- frontend/src/metabase/modes/components/drill/ZoomDrill.unit.spec.js
+++ frontend/src/metabase/modes/components/drill/ZoomDrill.unit.spec.js
@@ -1,15 +1,29 @@
-import { ORDERS } from "__support__/sample_database_fixture";
-
+import { createMockMetadata } from "__support__/metadata";
 import ZoomDrill from "metabase/modes/components/drill/ZoomDrill";
+import {
+  createSampleDatabase,
+  ORDERS,
+  ORDERS_ID,
+} from "metabase-types/api/mocks/presets";
+
+const metadata = createMockMetadata({
+  databases: [createSampleDatabase()],
+});
+
+const ordersTable = metadata.table(ORDERS_ID);
 
 describe("ZoomDrill", () => {
   it("should not be valid for top level actions", () => {
-    expect(ZoomDrill({ question: ORDERS.newQuestion() })).toHaveLength(0);
+    const question = ordersTable.newQuestion();
+    const actions = ZoomDrill({ question });
+    expect(actions).toHaveLength(0);
   });
+
   it("should be return correct new for month -> week", () => {
-    const query = ORDERS.query()
+    const query = ordersTable
+      .query()
       .aggregate(["count"])
-      .breakout(["field", ORDERS.CREATED_AT.id, { "temporal-unit": "month" }]);
+      .breakout(["field", ORDERS.CREATED_AT, { "temporal-unit": "month" }]);
 
     const actions = ZoomDrill({
       question: query.question(),
@@ -18,7 +32,7 @@ describe("ZoomDrill", () => {
         value: 42,
         dimensions: [
           {
-            column: ORDERS.CREATED_AT.column({ unit: "month" }),
+            column: metadata.field(ORDERS.CREATED_AT).column({ unit: "month" }),
             value: "2018-01-01T00:00:00Z",
           },
         ],
@@ -27,14 +41,14 @@ describe("ZoomDrill", () => {
     expect(actions).toHaveLength(1);
     const question = actions[0].question();
     expect(question.datasetQuery().query).toEqual({
-      "source-table": ORDERS.id,
+      "source-table": ORDERS_ID,
       aggregation: [["count"]],
       filter: [
         "=",
-        ["field", ORDERS.CREATED_AT.id, { "temporal-unit": "month" }],
+        ["field", ORDERS.CREATED_AT, { "temporal-unit": "month" }],
         "2018-01-01T00:00:00Z",
       ],
-      breakout: [["field", ORDERS.CREATED_AT.id, { "temporal-unit": "week" }]],
+      breakout: [["field", ORDERS.CREATED_AT, { "temporal-unit": "week" }]],
     });
     expect(question.display()).toEqual("line");
   });
