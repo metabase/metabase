41323fafd7a711bb783e810d6364726be35b6c9f
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2023-05-11 11:28:03 +0100

    Milestone 2 of [Epic] Prevent auto-apply filters in slow dashboards #29267 (#30569)

--- e2e/test/scenarios/dashboard-filters/old-parameters.cy.spec.js
+++ e2e/test/scenarios/dashboard-filters/old-parameters.cy.spec.js
@@ -1,12 +1,4 @@
-import {
-  restore,
-  popover,
-  visitDashboard,
-  rightSidebar,
-  filterWidget,
-  editDashboard,
-  saveDashboard,
-} from "e2e/support/helpers";
+import { restore, popover, visitDashboard } from "e2e/support/helpers";
 // NOTE: some overlap with parameters-embedded.cy.spec.js
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 
@@ -71,7 +63,7 @@ describe("scenarios > dashboard > OLD parameters", () => {
     it("should work", () => {
       cy.findAllByText("Doohickey");
 
-      cy.button("Category").click();
+      cy.contains("Category").click();
       popover().within(() => {
         cy.findByText("Gadget").click();
         cy.findByText("Add filter").click();
@@ -79,43 +71,6 @@ describe("scenarios > dashboard > OLD parameters", () => {
 
       // verify that the filter is applied
       cy.findByText("Doohickey").should("not.exist");
-
-      // test prevent auto-apply filters in slow dashboards metabase#29267
-      toggleDashboardSidebar();
-
-      filterWidget().findByText("Gadget").should("be.visible");
-      rightSidebar().findByLabelText("Auto-apply filters").click();
-      cy.button("Apply").should("not.exist");
-
-      cy.button("Category").click();
-      popover().within(() => {
-        cy.findByText("Gadget").click();
-        cy.findByText("Add filter").click();
-      });
-
-      cy.findByText("Rows 1-6 of 200").should("be.visible");
-      cy.button("Apply").click();
-      cy.findByText("Rows 1-6 of 53").should("be.visible");
-
-      // test that the apply button won't showup even when adding a new parameter or removing existing ones
-      toggleDashboardSidebar();
-      editDashboard();
-      cy.icon("filter").click();
-      popover().within(() => {
-        cy.findByText("Text or Category").click();
-        cy.findByText("Is").click();
-      });
-
-      saveDashboard();
-      cy.button("Apply").should("not.exist");
-
-      editDashboard();
-      cy.findByText("Text").within(() => {
-        cy.icon("gear").click();
-      });
-      cy.button("Remove").click();
-      saveDashboard();
-      cy.button("Apply").should("not.exist");
     });
   });
 
@@ -247,9 +202,3 @@ describe("scenarios > dashboard > OLD parameters", () => {
     });
   });
 });
-
-function toggleDashboardSidebar() {
-  cy.get("main header").within(() => {
-    cy.icon("info").click();
-  });
-}
