3a55ccf2f560dc36a771f343c72a3610e9f8c633
Oisin Coveney <oisin@metabase.com>
2023-05-11 11:34:55 +0300

    save changes

--- frontend/test/__support__/server-mocks/dataset.ts
+++ frontend/test/__support__/server-mocks/dataset.ts
@@ -16,6 +16,7 @@ export function setupCardDataset(
   options: MockDatasetOpts = {},
   overwriteRoutes = false,
 ) {
+  console.log(options);
   fetchMock.post("path:/api/dataset", createMockDataset(options), {
     overwriteRoutes,
   });
