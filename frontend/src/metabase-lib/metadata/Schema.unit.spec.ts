219abe8c2457814b1943f2d5ce1c578b05a25e25
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-11 15:34:03 +0300

    Fix schema types

--- frontend/src/metabase-lib/metadata/Schema.unit.spec.ts
+++ frontend/src/metabase-lib/metadata/Schema.unit.spec.ts
@@ -1,16 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
 import Schema from "./Schema";
 
 describe("Schema", () => {
   describe("instantiation", () => {
     it("should create an instance of Schema", () => {
-      expect(new Schema({})).toBeInstanceOf(Schema);
+      expect(new Schema({ id: "1:public", name: "public" })).toBeInstanceOf(
+        Schema,
+      );
     });
     it("should add `object` props to the instance (because it extends Base)", () => {
-      expect(new Schema({ name: "public" })).toBeInstanceOf(Schema);
       expect(
         new Schema({
+          id: "1:public",
           name: "public",
         }),
       ).toHaveProperty("name", "public");
@@ -19,6 +19,7 @@ describe("Schema", () => {
   describe("displayName", () => {
     it("should return a formatted `name` string", () => {
       const schema = new Schema({
+        id: "name: public",
         name: "foo_bar",
       });
       expect(schema.displayName()).toBe("Foo Bar");
