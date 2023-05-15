5189b8265326e53a099e0ed5d54baa70fc581bdb
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-15 17:37:08 +0300

    Fix tests

--- frontend/src/metabase/components/MetadataInfo/TableInfo/ConnectedTables.unit.spec.tsx
+++ frontend/src/metabase/components/MetadataInfo/TableInfo/ConnectedTables.unit.spec.tsx
@@ -1,46 +1,67 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-
-import Table from "metabase-lib/metadata/Table";
-
+import { checkNotNull } from "metabase/core/utils/types";
+import { Table } from "metabase-types/api";
+import {
+  createMockField,
+  createMockForeignKey,
+  createMockTable,
+} from "metabase-types/api/mocks";
+import { createMockState } from "metabase-types/store/mocks";
+import { createMockEntitiesState } from "__support__/store";
+import { getMetadata } from "metabase/selectors/metadata";
+import { renderWithProviders, screen } from "__support__/ui";
 import ConnectedTables from "./ConnectedTables";
 
-function setup(table: Table) {
-  return render(<ConnectedTables table={table} />);
+const EMPTY_TABLE = createMockTable();
+
+const TABLE_WITH_FKS = createMockTable({
+  id: 1,
+  fks: [
+    createMockForeignKey({
+      origin: createMockField({
+        table: createMockTable({
+          id: 2,
+          display_name: "Foo",
+        }),
+      }),
+    }),
+    createMockForeignKey({
+      origin: createMockField({
+        table: createMockTable({
+          id: 3,
+          display_name: "Bar",
+        }),
+      }),
+    }),
+  ],
+});
+
+interface SetupOpts {
+  table: Table;
+}
+
+function setup({ table }: SetupOpts) {
+  const state = createMockState({
+    entities: createMockEntitiesState({
+      tables: [table],
+    }),
+  });
+  const metadata = getMetadata(state);
+
+  return renderWithProviders(
+    <ConnectedTables table={checkNotNull(metadata.table(table.id))} />,
+  );
 }
 
 describe("ConnectedTables", () => {
   it("should show nothing when the table has no fks", () => {
-    const table = new Table();
-    const { container } = setup(table);
+    const { container } = setup({ table: EMPTY_TABLE });
 
     expect(container).toBeEmptyDOMElement();
   });
 
   it("should show a label for each connected table", () => {
-    const table = new Table({
-      id: 1,
-      fks: [
-        {
-          origin: {
-            table: {
-              id: 2,
-              display_name: "Foo",
-            },
-          },
-        },
-        {
-          origin: {
-            table: {
-              id: 3,
-              display_name: "Bar",
-            },
-          },
-        },
-      ],
-    });
-
-    setup(table);
+    setup({ table: TABLE_WITH_FKS });
 
     expect(screen.getByText("Foo")).toBeInTheDocument();
     expect(screen.getByText("Bar")).toBeInTheDocument();
