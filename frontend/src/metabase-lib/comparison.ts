1f9be792cfb94c9c642891c1d54461c1a4f76c07
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-15 14:49:13 +0100

    Add `isSameBucket` utility

--- frontend/src/metabase-lib/comparison.ts
+++ frontend/src/metabase-lib/comparison.ts
@@ -1,7 +1,7 @@
 import * as ML from "cljs/metabase.lib.js";
 import type { DatasetQuery } from "metabase-types/api";
 import { displayInfo } from "./metadata";
-import type { Query, Clause, ColumnMetadata } from "./types";
+import type { Bucket, Clause, ColumnMetadata, Query } from "./types";
 
 export function areLegacyQueriesEqual(
   query1: DatasetQuery,
@@ -23,4 +23,10 @@ export function isClauseColumn(
     clauseInfo.table?.name === columnInfo.table?.name &&
     clauseInfo.name === columnInfo.name
   );
+}
+
+export function isSameBucket(query: Query, bucket1: Bucket, bucket2: Bucket) {
+  const info1 = displayInfo(query, bucket1);
+  const info2 = displayInfo(query, bucket2);
+  return info1.displayName === info2.displayName;
 }
