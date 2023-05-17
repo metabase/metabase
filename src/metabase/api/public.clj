c09053ae3af603080d67c5b17f8c04705089f9ca
Ngoc Khuat <qn.khuat@gmail.com>
2023-05-17 11:23:51 +0700

    hydrate orders-tabs for public/embed dashboard

--- src/metabase/api/public.clj
+++ src/metabase/api/public.clj
@@ -208,7 +208,7 @@
   {:pre [(even? (count conditions))]}
   (binding [params/*ignore-current-user-perms-and-return-all-field-values* true]
     (-> (api/check-404 (apply t2/select-one [Dashboard :name :description :id :parameters :auto_apply_filters], :archived false, conditions))
-        (hydrate [:ordered_cards :card :series :dashcard/action] :param_values :param_fields)
+        (hydrate [:ordered_cards :card :series :dashcard/action] :ordered_tabs :param_values :param_fields)
         api.dashboard/add-query-average-durations
         (update :ordered_cards (fn [dashcards]
                                  (for [dashcard dashcards]
