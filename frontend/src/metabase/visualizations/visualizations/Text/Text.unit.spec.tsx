f2d53c856db91ab97b1d5b2d7b06752309cbf3ae
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-05-18 18:48:29 -0400

    remove comment

--- frontend/src/metabase/visualizations/visualizations/Text/Text.unit.spec.tsx
+++ frontend/src/metabase/visualizations/visualizations/Text/Text.unit.spec.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Text } from "../Text";
 
-// ! FIX TYPESCRIPT ERRORS
 interface Settings {
   text: string | null;
 }
