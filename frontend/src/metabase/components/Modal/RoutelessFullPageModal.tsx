9d67169810b9e9a3511e30951a6cae71bba14aec
Oisin Coveney <oisin@metabase.com>
2023-05-18 11:56:24 +0300

    Move ts refactor code to new branch

--- frontend/src/metabase/components/Modal/RoutelessFullPageModal.tsx
+++
@@ -1,5 +0,0 @@
-// the "routeless" version should only be used for non-inline modals
-import routeless from "metabase/hoc/Routeless";
-import { FullPageModal } from "metabase/components/Modal/FullPageModal";
-
-export const RoutelessFullPageModal = routeless(FullPageModal);
