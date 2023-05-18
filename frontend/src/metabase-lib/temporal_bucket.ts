fd240dc9edfe7a4e60f3eefd5d89c45b959e4404
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-18 14:09:25 +0100

    Use temporal buckets JS wrappers

--- frontend/src/metabase-lib/temporal_bucket.ts
+++ frontend/src/metabase-lib/temporal_bucket.ts
@@ -1,25 +1,20 @@
-import { to_array } from "cljs/cljs.core";
-import {
-  temporal_bucket_option,
-  available_temporal_buckets,
-  with_temporal_bucket,
-} from "cljs/metabase.lib.core";
+import * as ML from "cljs/metabase.lib.js";
 import type { Bucket, ColumnMetadata, Clause, Query } from "./types";
 
 export function temporalBucket(clause: Clause | ColumnMetadata): Bucket | null {
-  return temporal_bucket_option(clause);
+  return ML.temporal_bucket(clause);
 }
 
 export function availableTemporalBuckets(
   query: Query,
   column: ColumnMetadata,
 ): Bucket[] {
-  return to_array(available_temporal_buckets(query, column));
+  return ML.available_temporal_buckets(query, column);
 }
 
 export function withTemporalBucket(
   column: ColumnMetadata,
   bucket: Bucket,
 ): ColumnMetadata {
-  return with_temporal_bucket(column, bucket);
+  return ML.with_temporal_bucket(column, bucket);
 }
