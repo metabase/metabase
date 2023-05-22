66cc724f6875c282e908b62824ceba9f0b43f7ec
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-05-22 17:26:51 -0400

    add small heading unit tests

---
+++ frontend/src/metabase/visualizations/visualizations/Heading/Heading.unit.spec.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import { Heading } from "../Heading";
+
+interface Settings {
+  text: string;
+}
+
+const defaultProps = {
+  onUpdateVisualizationSettings: (text: string) => {
+    return;
+  },
+  dashcard: {},
+  settings: { text: "" },
+  isEditing: false,
+};
+
+describe("Text", () => {
+  it("should be able to render", () => {
+    expect(() =>
+      render(<Heading {...defaultProps} settings={getSettingsWithText("")} />),
+    ).not.toThrow();
+  });
+
+  it("should render text", () => {
+    render(
+      <Heading
+        {...defaultProps}
+        settings={getSettingsWithText("Plain text")}
+      />,
+    );
+
+    expect(screen.getByText("Plain text")).toBeInTheDocument();
+  });
+});
+
+function getSettingsWithText(text: string): Settings {
+  return {
+    text: text,
+  };
+}
