af4da0691827ba41c15a2a9ee9420582467c88ed
dan sutton <dan@dpsutton.com>
2023-05-23 07:42:23 -0500

    remove breakout.
    without breakout
    
    ```sql
     SELECT "source"."id"    AS "ID",
           "source"."title" AS "TITLE"
    FROM   (SELECT "PUBLIC"."products"."id"    AS "ID",
                   "PUBLIC"."products"."title" AS "TITLE"
            FROM   "PUBLIC"."products") AS "source"
    ORDER  BY "source"."title" DESC
    ```
    
    with breakout
    
    ```sql
     SELECT "source"."id"    AS "ID",
           "source"."title" AS "TITLE"
    FROM   (SELECT "PUBLIC"."products"."id"    AS "ID",
                   "PUBLIC"."products"."title" AS "TITLE"
            FROM   "PUBLIC"."products") AS "source"
    GROUP  BY "source"."id",
              "source"."title"
    ORDER  BY "source"."title" DESC,
              "source"."id" ASC
    ```

--- src/metabase/models/model_index.clj
+++ src/metabase/models/model_index.clj
@@ -60,8 +60,7 @@
                      :query    {:source-table (format "card__%d" (:id model))
                                 :fields       [(:pk_ref model-index) (:value_ref model-index)]
                                 :order-by     [[:desc (:value_ref model-index)]]
-                                :limit        (inc max-indexed-values)
-                                :breakout     [(:pk_ref model-index) (:value_ref model-index)]}})
+                                :limit        (inc max-indexed-values)}})
                    :data :rows (filter valid-tuples?))]
          (catch Exception e
            (log/warn (trs "Error fetching indexed values for model {0}" (:id model)) e)
