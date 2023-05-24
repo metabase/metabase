4934e501f8487b2aee7882df6d3fa9d6aeac7685
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-24 14:47:29 +0300

    Fix naming

--- frontend/src/metabase/dashboard/reducers.js
+++ frontend/src/metabase/dashboard/reducers.js
@@ -231,10 +231,10 @@ const dashcards = handleActions(
       ...state,
       [dashcardId]: { ...state[dashcardId], justAdded: false },
     }),
-    [Questions.actionTypes.UPDATE]: (state, { payload: { object } }) =>
+    [Questions.actionTypes.UPDATE]: (state, { payload: { object: card } }) =>
       _.mapObject(state, dashcard =>
-        dashcard.card?.id === object?.id
-          ? assocIn(dashcard, ["card"], object)
+        dashcard.card?.id === card?.id
+          ? assocIn(dashcard, ["card"], card)
           : dashcard,
       ),
   },
@@ -273,8 +273,8 @@ const dashcardData = handleActions(
       next: (state, { payload: { cardId, dashcardId } }) =>
         assocIn(state, [dashcardId, cardId]),
     },
-    [Questions.actionTypes.UPDATE]: (state, { payload: { object } }) =>
-      _.mapObject(state, data => dissoc(data, object.id)),
+    [Questions.actionTypes.UPDATE]: (state, { payload: { object: card } }) =>
+      _.mapObject(state, dashboardData => dissoc(dashboardData, card.id)),
   },
   INITIAL_DASHBOARD_STATE.dashcardData,
 );
