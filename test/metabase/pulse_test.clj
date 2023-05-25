bbb7ca41075d17f4c9f5be6c1e4662017e275b74
Jerry Huang <jhuang37050@gmail.com>
2023-05-24 18:37:19 -0700

    fix test

--- test/metabase/pulse_test.clj
+++ test/metabase/pulse_test.clj
@@ -381,7 +381,7 @@
        (fn [_ _]
          (is (= (into {} (map (fn [user-kwd]
                                 (mt/email-to user-kwd {:subject "Pulse: Pulse Name",
-                                                       :to      #{user-kwd}
+                                                       :to      #{(:email (mt/fetch-user user-kwd))}
                                                        :body    [{"Pulse Name" true}
                                                                  pulse.test-util/png-attachment
                                                                  pulse.test-util/png-attachment]}))
