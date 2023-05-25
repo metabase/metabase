ab7e7317bcc4e20cf7ce2906d5e9834ec5d9aa18
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-05-25 15:39:12 +0700

    Fix dashboard ID type

--- frontend/src/metabase/dashboard/analytics.ts
+++ frontend/src/metabase/dashboard/analytics.ts
@@ -1,6 +1,7 @@
 import { trackSchemaEvent } from "metabase/lib/analytics";
+import type { DashboardId } from "metabase-types/api";
 
-export const trackAutoApplyFiltersDisabled = (dashboardId: number) => {
+export const trackAutoApplyFiltersDisabled = (dashboardId: DashboardId) => {
   trackSchemaEvent("dashboard", "1-0-1", {
     event: "auto_apply_filters_disabled",
     dashboard_id: dashboardId,
