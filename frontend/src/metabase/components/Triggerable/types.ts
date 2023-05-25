1b3c45fe9b1be701052aeec06c9c0631dc0a19e2
Kamil Mielnik <kamil@kamilmielnik.com>
2023-05-25 15:43:39 +0700

    Add generic typing to Triggerable - Extract Children & TriggerElement from Triggerable to avoiding typing   issues when re-assigning variable value (previous implementation)

--- frontend/src/metabase/components/Triggerable/types.ts
+++ frontend/src/metabase/components/Triggerable/types.ts
@@ -3,3 +3,9 @@ import { ReactNode } from "react";
 export type RenderProp<Props extends Record<string, unknown>> = (
   props: Props,
 ) => ReactNode;
+
+export type RenderTriggerElement = (props: {
+  isTriggeredComponentOpen: boolean;
+  open: () => void;
+  close: () => void;
+}) => ReactNode;
