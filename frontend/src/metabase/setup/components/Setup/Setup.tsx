2f2fa2dc315096b7a2c0c9d565120024e02b74e5
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-25 15:21:30 +0300

    Migrate welcome page

--- frontend/src/metabase/setup/components/Setup/Setup.tsx
+++ frontend/src/metabase/setup/components/Setup/Setup.tsx
@@ -1,17 +1,26 @@
 import React, { useEffect } from "react";
+import { useUpdate } from "react-use";
 import { useSelector } from "metabase/lib/redux";
 import { trackStepSeen } from "../../analytics";
 import { WELCOME_STEP } from "../../constants";
-import { getStep } from "../../selectors";
+import { getIsLocaleLoaded, getStep } from "../../selectors";
 import { SettingsPage } from "../SettingsPage";
 import { WelcomePage } from "../WelcomePage";
 
 export const Setup = (): JSX.Element => {
   const step = useSelector(getStep);
+  const isLocaleLoaded = useSelector(getIsLocaleLoaded);
+  const update = useUpdate();
 
   useEffect(() => {
     trackStepSeen(step);
   }, [step]);
+
+  useEffect(() => {
+    if (isLocaleLoaded) {
+      update();
+    }
+  }, [update, isLocaleLoaded]);
 
   if (step === WELCOME_STEP) {
     return <WelcomePage />;
