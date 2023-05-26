d6a8287b4bd169106356a909052a81a5b342b523
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-26 15:58:39 +0300

    Fix tests

--- frontend/test/__support__/server-mocks/session.ts
+++ frontend/test/__support__/server-mocks/session.ts
@@ -4,3 +4,7 @@ import { Settings } from "metabase-types/api";
 export function setupPropertiesEndpoints(settings: Settings) {
   fetchMock.get("path:/api/session/properties", settings);
 }
+
+export function setupForgotPasswordEndpoints() {
+  fetchMock.post("path:/api/session/forgot_password", 200);
+}
