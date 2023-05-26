5d541ee428448b4ed153066a5c488ac16fab529c
Noah Moss <noahbmoss@gmail.com>
2023-05-26 16:22:51 -0400

    fix boolean logic

--- src/metabase/api/permission_graph.clj
+++ src/metabase/api/permission_graph.clj
@@ -87,7 +87,7 @@
    DataPerms
    [:fn {:error/fn (fn [_ _] (trs "Invalid DB permissions: If you have write access for native queries, you must have data access to all schemas."))}
     (fn [{:keys [native schemas]}]
-      (not (and (= native :write) schemas (not= schemas :all :impersonated))))]])
+      (not (and (= native :write) schemas (not (#{:all :impersonated} schemas)))))]])
 
 (def ^:private DbGraph
   [:schema {:registry {"DataPerms" DataPerms}}
