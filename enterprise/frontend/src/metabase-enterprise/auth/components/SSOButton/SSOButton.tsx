9260288249b6bcc79d9c07bdfba2af0221fc3db5
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-26 19:28:51 +0300

    Fix tests

--- enterprise/frontend/src/metabase-enterprise/auth/components/SSOButton/SSOButton.tsx
+++ enterprise/frontend/src/metabase-enterprise/auth/components/SSOButton/SSOButton.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect } from "react";
 import { t } from "ttag";
-import AuthButton from "metabase/auth/components/AuthButton";
+import { AuthButton } from "metabase/auth/components/AuthButton";
 
 export interface SSOButtonProps {
   isCard?: boolean;
