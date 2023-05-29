f0159db26bf6b94d7ddda3fb0da4950166482161
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-05-29 20:39:27 -0300

    fix icons

---
+++ frontend/src/metabase/core/components/Icon/svg/index.ts
@@ -0,0 +1,11 @@
+import dashboard from "./dashboard.svg?jsx";
+import database from "./database.svg?jsx";
+import table from "./table.svg?jsx";
+import unknown from "./unknown.svg?jsx";
+
+export const Icons = {
+  dashboard,
+  database,
+  table,
+  unknown,
+};
