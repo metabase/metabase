81362f7b8b915e46bef034c04e6bcea02121d128
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-29 13:57:36 +0300

    Remove icon, not used

--- frontend/src/metabase/auth/components/AuthButton/AuthButton.tsx
+++ frontend/src/metabase/auth/components/AuthButton/AuthButton.tsx
@@ -1,9 +1,8 @@
 import React, { ReactNode } from "react";
-import { CardIcon, CardLink, CardText, TextLink } from "./AuthButton.styled";
+import { CardLink, CardText, TextLink } from "./AuthButton.styled";
 
 interface AuthButtonProps {
   link?: string;
-  icon?: string;
   isCard?: boolean;
   children?: ReactNode;
   onClick?: () => void;
@@ -11,14 +10,12 @@ interface AuthButtonProps {
 
 export const AuthButton = ({
   link = "",
-  icon,
   isCard,
   children,
   onClick,
 }: AuthButtonProps): JSX.Element => {
   return isCard ? (
     <CardLink to={link} onClick={onClick}>
-      {icon && <CardIcon name={icon} />}
       <CardText>{children}</CardText>
     </CardLink>
   ) : (
--- frontend/src/metabase/auth/components/AuthButton/AuthButton.unit.spec.tsx
+++ frontend/src/metabase/auth/components/AuthButton/AuthButton.unit.spec.tsx
@@ -3,24 +3,18 @@ import { renderWithProviders, screen } from "__support__/ui";
 import { AuthButton } from "./AuthButton";
 
 interface SetupOpts {
-  icon?: string;
   isCard?: boolean;
 }
 
-const setup = ({ icon, isCard }: SetupOpts = {}) => {
-  renderWithProviders(
-    <AuthButton icon={icon} isCard={isCard}>
-      Sign in
-    </AuthButton>,
-  );
+const setup = ({ isCard }: SetupOpts = {}) => {
+  renderWithProviders(<AuthButton isCard={isCard}>Sign in</AuthButton>);
 };
 
 describe("AuthButton", () => {
   it("should render a card", () => {
-    setup({ icon: "google", isCard: true });
+    setup({ isCard: true });
 
     expect(screen.getByText("Sign in")).toBeInTheDocument();
-    expect(screen.getByLabelText("google icon")).toBeInTheDocument();
   });
 
   it("should render a link", () => {
