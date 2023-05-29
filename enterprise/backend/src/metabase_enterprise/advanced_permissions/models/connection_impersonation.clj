0c32e8316cfd70feb3fd62680edec67d34816425
Noah Moss <noahbmoss@gmail.com>
2023-05-29 13:32:13 -0400

    clear impersonations as necessary when perms graphs changes

--- enterprise/backend/src/metabase_enterprise/advanced_permissions/models/connection_impersonation.clj
+++ enterprise/backend/src/metabase_enterprise/advanced_permissions/models/connection_impersonation.clj
@@ -5,6 +5,7 @@
    [medley.core :as m]
    [metabase.models.interface :as mi]
    [metabase.public-settings.premium-features :refer [defenterprise]]
+   [metabase.util.log :as log]
    [methodical.core :as methodical]
    [toucan2.core :as t2]))
 
@@ -37,3 +38,34 @@
       (t2/update! :model/ConnectionImpersonation id impersonation)
       (-> (t2/insert-returning-instances! :model/ConnectionImpersonation impersonation)
           first))))
+
+(defn- delete-impersonations-for-group-database! [{:keys [group-id database-id]} changes]
+  (log/debugf "Deleting unneeded Connection Impersonations for Group %d for Database %d. Graph changes: %s"
+              group-id database-id (pr-str changes))
+  (when (not= :impersonated changes)
+    (log/debugf "Group %d %s for Database %d, deleting all Connection Impersonations for this DB"
+                group-id
+                (case changes
+                  :none  "no longer has any perms"
+                  :all   "now has full data perms"
+                  :block "is now BLOCKED from all non-data-perms access")
+                database-id)
+    (t2/delete! :model/ConnectionImpersonation :group_id group-id :db_id database-id)))
+
+(defn- delete-impersonations-for-group! [{:keys [group-id]} changes]
+  (log/debugf "Deleting unneeded Connection Impersonation policies for Group %d. Graph changes: %s" group-id (pr-str changes))
+  (doseq [database-id (set (keys changes))]
+    (delete-impersonations-for-group-database!
+     {:group-id group-id, :database-id database-id}
+     (get-in changes [database-id :data :schemas]))))
+
+(defenterprise delete-impersonations-if-needed-after-permissions-change!
+  "For use only inside `metabase.models.permissions`; don't call this elsewhere. Delete Connection Impersonations that
+  are no longer needed after the permissions graph is updated. `changes` are the parts of the graph that have changed,
+  i.e. the `things-only-in-new` returned by `clojure.data/diff`."
+  :feature :advanced-permisisons
+  [changes]
+  (log/debug "Permissions updated, deleting unneeded Connection Impersonations...")
+  (doseq [group-id (set (keys changes))]
+    (delete-impersonations-for-group! {:group-id group-id} (get changes group-id)))
+  (log/debug "Done deleting unneeded Connection Impersonations."))
