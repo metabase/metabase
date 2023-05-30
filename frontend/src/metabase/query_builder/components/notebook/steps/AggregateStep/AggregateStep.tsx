ffebb35b3322fc20413a9e840441d4d932a7bbae
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-30 18:06:53 +0100

    Extract `AggregationPopover` in notebook step

--- frontend/src/metabase/query_builder/components/notebook/steps/AggregateStep/AggregateStep.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/AggregateStep/AggregateStep.tsx
@@ -1,6 +1,7 @@
 import { t } from "ttag";
 
 import * as Lib from "metabase-lib";
+import type StructuredQuery from "metabase-lib/queries/StructuredQuery";
 
 import type { NotebookStepUiComponentProps } from "../../types";
 import ClauseStep from "../ClauseStep";
@@ -72,42 +73,84 @@ export function AggregateStep({
       tetherOptions={aggTetherOptions}
       renderName={renderAggregationName}
       renderPopover={(aggregation, index) => (
-        <AggregationPicker
+        <AggregationPopover
           query={topLevelQuery}
-          legacyQuery={legacyQuery}
-          legacyClause={
-            typeof index === "number"
-              ? legacyQuery.aggregations()[index]
-              : undefined
-          }
           stageIndex={stageIndex}
-          operators={
-            aggregation
-              ? Lib.selectedAggregationOperators(operators, aggregation)
-              : operators
-          }
-          onSelect={newAggregation => {
-            const isUpdate = aggregation != null;
-            if (isUpdate) {
-              handleUpdateAggregation(aggregation, newAggregation);
-            } else {
-              handleAddAggregation(newAggregation);
-            }
-          }}
-          onSelectLegacy={newLegacyAggregation => {
-            const isUpdate = aggregation != null && typeof index === "number";
-            if (isUpdate) {
-              updateQuery(
-                legacyQuery.updateAggregation(index, newLegacyAggregation),
-              );
-            } else {
-              updateQuery(legacyQuery.aggregate(newLegacyAggregation));
-            }
-          }}
+          operators={operators}
+          clause={aggregation}
+          clauseIndex={index}
+          legacyQuery={legacyQuery}
+          onAddAggregation={handleAddAggregation}
+          onUpdateAggregation={handleUpdateAggregation}
+          onLegacyQueryChange={updateQuery}
         />
       )}
       onRemove={handleRemoveAggregation}
       data-testid="aggregate-step"
+    />
+  );
+}
+
+interface AggregationPopoverProps {
+  query: Lib.Query;
+  stageIndex: number;
+  operators: Lib.AggregationOperator[];
+  clause?: Lib.AggregationClause;
+  onUpdateAggregation: (
+    currentClause: Lib.AggregationClause,
+    nextClause: Lib.AggregationClause,
+  ) => void;
+  onAddAggregation: (aggregation: Lib.AggregationClause) => void;
+
+  legacyQuery: StructuredQuery;
+  clauseIndex?: number;
+  onLegacyQueryChange: (query: StructuredQuery) => void;
+}
+
+function AggregationPopover({
+  query,
+  stageIndex,
+  operators: baseOperators,
+  clause,
+  clauseIndex,
+  legacyQuery,
+  onAddAggregation,
+  onUpdateAggregation,
+  onLegacyQueryChange,
+}: AggregationPopoverProps) {
+  const isUpdate = clause != null && clauseIndex != null;
+
+  const operators = isUpdate
+    ? Lib.selectedAggregationOperators(baseOperators, clause)
+    : baseOperators;
+
+  const legacyClause = isUpdate
+    ? legacyQuery.aggregations()[clauseIndex]
+    : undefined;
+
+  return (
+    <AggregationPicker
+      query={query}
+      legacyQuery={legacyQuery}
+      legacyClause={legacyClause}
+      stageIndex={stageIndex}
+      operators={operators}
+      onSelect={aggregation => {
+        if (isUpdate) {
+          onUpdateAggregation(clause, aggregation);
+        } else {
+          onAddAggregation(aggregation);
+        }
+      }}
+      onSelectLegacy={newLegacyAggregation => {
+        if (isUpdate) {
+          onLegacyQueryChange(
+            legacyQuery.updateAggregation(clauseIndex, newLegacyAggregation),
+          );
+        } else {
+          onLegacyQueryChange(legacyQuery.aggregate(newLegacyAggregation));
+        }
+      }}
     />
   );
 }
