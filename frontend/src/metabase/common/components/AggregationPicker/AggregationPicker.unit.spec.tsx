c8444d4916690d1f98c3601df6d2fbcbd3740044
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-30 16:18:15 +0100

    Add basic legacy metrics support

--- frontend/src/metabase/common/components/AggregationPicker/AggregationPicker.unit.spec.tsx
+++ frontend/src/metabase/common/components/AggregationPicker/AggregationPicker.unit.spec.tsx
@@ -1,11 +1,31 @@
 import userEvent from "@testing-library/user-event";
+import { createMockMetadata } from "__support__/metadata";
 import { render, screen, within } from "__support__/ui";
+import { checkNotNull } from "metabase/core/utils/types";
+
+import type { Metric, StructuredDatasetQuery } from "metabase-types/api";
+import { createMockMetric } from "metabase-types/api/mocks";
+import {
+  createAdHocCard,
+  createSampleDatabase,
+  createOrdersTable,
+  createPeopleTable,
+  createProductsTable,
+  createReviewsTable,
+  ORDERS,
+  ORDERS_ID,
+  PRODUCTS_ID,
+  PRODUCTS,
+} from "metabase-types/api/mocks/presets";
 import * as Lib from "metabase-lib";
+import Question from "metabase-lib/Question";
+import type StructuredQuery from "metabase-lib/queries/StructuredQuery";
 import {
   createQuery,
   columnFinder,
   findAggregationOperator,
 } from "metabase-lib/test-helpers";
+
 import { AggregationPicker } from "./AggregationPicker";
 
 function createQueryWithCountAggregation() {
@@ -27,17 +47,60 @@ function createQueryWithMaxAggregation() {
   return Lib.aggregate(initialQuery, 0, clause);
 }
 
-function setup({ query = createQuery() } = {}) {
+const TEST_METRIC = createMockMetric({
+  id: 1,
+  table_id: ORDERS_ID,
+  name: "Total Order Value",
+  description: "The total value of all orders",
+  definition: {
+    aggregation: [["sum", ["field", ORDERS.TOTAL, null]]],
+    "source-table": ORDERS_ID,
+  },
+});
+
+const PRODUCT_METRIC = createMockMetric({
+  id: 2,
+  table_id: PRODUCTS_ID,
+  name: "Average Rating",
+  definition: {
+    aggregation: [["avg", ["field", PRODUCTS.RATING, null]]],
+    "source-table": PRODUCTS_ID,
+  },
+});
+
+type SetupOpts = {
+  query?: Lib.Query;
+  metrics?: Metric[];
+};
+
+function setup({ query = createQuery(), metrics = [] }: SetupOpts = {}) {
+  const metadata = createMockMetadata({
+    databases: [
+      createSampleDatabase({
+        tables: [
+          createOrdersTable({ metrics }),
+          createPeopleTable(),
+          createProductsTable({ metrics: [PRODUCT_METRIC] }),
+          createReviewsTable(),
+        ],
+      }),
+    ],
+    metrics: [...metrics, PRODUCT_METRIC],
+  });
+
+  const dataset_query = Lib.toLegacyQuery(query) as StructuredDatasetQuery;
+  const question = new Question(createAdHocCard({ dataset_query }), metadata);
+  const legacyQuery = question.query() as StructuredQuery;
+
   const clause = Lib.aggregations(query, 0)[0];
 
+  const baseOperators = Lib.availableAggregationOperators(query, 0);
   const operators = clause
-    ? Lib.selectedAggregationOperators(
-        Lib.availableAggregationOperators(query, 0),
-        clause,
-      )
-    : Lib.availableAggregationOperators(query, 0);
+    ? Lib.selectedAggregationOperators(baseOperators, clause)
+    : baseOperators;
 
   const onSelect = jest.fn();
+  const onSelectLegacy = jest.fn();
 
   function handleSelect(clause: Lib.AggregationClause) {
     const nextQuery = Lib.aggregate(query, 0, clause);
@@ -49,9 +112,11 @@ function setup({ query = createQuery() } = {}) {
   render(
     <AggregationPicker
       query={query}
+      legacyQuery={legacyQuery}
       stageIndex={0}
       operators={operators}
       onSelect={handleSelect}
+      onSelectLegacy={onSelectLegacy}
     />,
   );
 
@@ -60,10 +125,23 @@ function setup({ query = createQuery() } = {}) {
     return lastCall?.[0];
   }
 
-  return { getRecentClause };
+  return { metadata, getRecentClause, onSelectLegacy };
 }
 
 describe("AggregationPicker", () => {
+  it("should allow switching between aggregation approaches", () => {
+    const { metadata, onSelectLegacy } = setup({
+      query: createQueryWithCountAggregation(),
+      metrics: [TEST_METRIC],
+    });
+    const metric = checkNotNull(metadata.metric(TEST_METRIC.id));
+
+    userEvent.click(screen.getByText("Common Metrics"));
+    userEvent.click(screen.getByText(TEST_METRIC.name));
+
+    expect(onSelectLegacy).toHaveBeenCalledWith(metric.aggregationClause());
+  });
+
   describe("basic operators", () => {
     it("should list basic operators", () => {
       setup();
@@ -205,6 +283,59 @@ describe("AggregationPicker", () => {
           displayName: "Max of Discount",
         }),
       );
+    });
+  });
+
+  describe("metrics", () => {
+    function setupMetrics(opts: SetupOpts = {}) {
+      const result = setup(opts);
+
+      // Expand the metrics section
+      userEvent.click(screen.getByText("Common Metrics"));
+
+      return result;
+    }
+
+    it("shouldn't show the metrics section when there're no metics", () => {
+      setup({ metrics: [] });
+      expect(screen.queryByText("Common Metrics")).not.toBeInTheDocument();
+    });
+
+    it("should list metrics for the query table", () => {
+      setupMetrics({ metrics: [TEST_METRIC] });
+      expect(screen.getByText(TEST_METRIC.name)).toBeInTheDocument();
+    });
+
+    it("shouldn't list metrics for other tables", () => {
+      setupMetrics({ metrics: [TEST_METRIC] });
+      expect(screen.queryByText(PRODUCT_METRIC.name)).not.toBeInTheDocument();
+    });
+
+    it("should show a description for each metric", () => {
+      setupMetrics({ metrics: [TEST_METRIC] });
+
+      const metricOption = screen.getByRole("option", {
+        name: TEST_METRIC.name,
+      });
+      const infoIcon = within(metricOption).getByRole("img", {
+        name: "question icon",
+      });
+      userEvent.hover(infoIcon);
+
+      expect(screen.getByRole("tooltip")).toHaveTextContent(
+        TEST_METRIC.description,
+      );
+    });
+
+    it("should allow picking a metric", () => {
+      const { metadata, onSelectLegacy } = setupMetrics({
+        metrics: [TEST_METRIC],
+      });
+      const metric = checkNotNull(metadata.metric(TEST_METRIC.id));
+
+      userEvent.click(screen.getByText(TEST_METRIC.name));
+
+      expect(onSelectLegacy).toHaveBeenCalledWith(metric.aggregationClause());
     });
   });
 });
