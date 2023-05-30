83773a8bdcf6e7fc8946ed24e51f24ad506058df
Kamil Mielnik <kamil@kamilmielnik.com>
2023-05-30 18:05:36 +0700

    Make NativeQueryEditor testable - Mock just NativeQueryEditor.prototype.loadAceEditor instead of entire NativeQueryEditor

--- frontend/test/register-visualizations.js
+++ frontend/test/register-visualizations.js
@@ -1,4 +1,6 @@
+import _ from "lodash";
 import "metabase/plugins/builtin";
+import { NativeQueryEditor } from "metabase/query_builder/components/NativeQueryEditor";
 
 // We need to mock this *before* registering the visualizations. Otherwise
 // `ChartWithLegend` with already load the real one.
@@ -7,7 +9,9 @@ jest.mock("metabase/components/ExplicitSize");
 // We need to mock this *before* registering the visualizations.
 // Otherwise ActionViz loads the NativeQueryEditor (via ActionCreator)
 // and tests fail because ace is not properly mocked
-jest.mock("metabase/query_builder/components/NativeQueryEditor");
+jest
+  .spyOn(NativeQueryEditor.prototype, "loadAceEditor")
+  .mockImplementation(_.noop);
 
 import registerVisualizations from "metabase/visualizations/register";
 registerVisualizations();
