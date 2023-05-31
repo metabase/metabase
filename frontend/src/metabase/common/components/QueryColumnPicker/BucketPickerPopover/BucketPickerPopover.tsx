71175735a294e76a949e3e238efeacd21aa5c4d8
Anton Kulyk <kuliks.anton@gmail.com>
2023-05-31 16:17:33 +0100

    Remove `React` imports

--- frontend/src/metabase/common/components/QueryColumnPicker/BucketPickerPopover/BucketPickerPopover.tsx
+++ frontend/src/metabase/common/components/QueryColumnPicker/BucketPickerPopover/BucketPickerPopover.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import PopoverWithTrigger from "metabase/components/PopoverWithTrigger/TippyPopoverWithTrigger";
 import * as Lib from "metabase-lib";
 import {
