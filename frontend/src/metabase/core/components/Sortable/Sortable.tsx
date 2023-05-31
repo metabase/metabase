611f36aeb91e10ed9ca5755146357fd5eb571933
Emmad Usmani <emmadusmani@berkeley.edu>
2023-05-31 10:10:03 -0700

    remove react import

--- frontend/src/metabase/core/components/Sortable/Sortable.tsx
+++ frontend/src/metabase/core/components/Sortable/Sortable.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import type { UniqueIdentifier } from "@dnd-kit/core";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
