bfb653cc96d7993aa4245f001f6b66fb4c94e1e9
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-05-31 11:27:50 +0300

    Add entity loader hooks for Search, RecentItems, PopularItems, DatabaseCandidates (#31143)

---
+++ frontend/src/metabase/common/hooks/use-database-candidate-list-query/use-database-candidate-list-query.unit.spec.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
+import { createMockDatabaseCandidate } from "metabase-types/api/mocks";
+import { setupDatabaseCandidatesEndpoint } from "__support__/server-mocks";
+import {
+  renderWithProviders,
+  screen,
+  waitForElementToBeRemoved,
+} from "__support__/ui";
+import { useDatabaseCandidateListQuery } from "./use-database-candidate-list-query";
+
+const TEST_DB_ID = 1;
+const TEST_DB_CANDIDATE = createMockDatabaseCandidate();
+
+const TestComponent = () => {
+  const {
+    data = [],
+    isLoading,
+    error,
+  } = useDatabaseCandidateListQuery({
+    query: { id: TEST_DB_ID },
+  });
+
+  if (isLoading || error) {
+    return <LoadingAndErrorWrapper loading={isLoading} error={error} />;
+  }
+
+  return (
+    <div>
+      {data.map((item, index) => (
+        <div key={index}>{item.schema}</div>
+      ))}
+    </div>
+  );
+};
+
+const setup = () => {
+  setupDatabaseCandidatesEndpoint(TEST_DB_ID, [TEST_DB_CANDIDATE]);
+  renderWithProviders(<TestComponent />);
+};
+
+describe("useDatabaseCandidateListQuery", () => {
+  it("should be initially loading", () => {
+    setup();
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("should show data from the response", async () => {
+    setup();
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+    expect(screen.getByText(TEST_DB_CANDIDATE.schema)).toBeInTheDocument();
+  });
+});
