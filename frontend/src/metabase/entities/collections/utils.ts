ffede40ce2d4e0990765b641434414810fa080aa
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2023-06-01 14:43:50 -0600

    Auditv2 - Frontend for instance analytics questions + models (#31220)
    * mock question-collection type hydration
    
    * show instance analytics icon + tooltips for questions and models
    
    * update request mock

--- frontend/src/metabase/entities/collections/utils.ts
+++ frontend/src/metabase/entities/collections/utils.ts
@@ -21,7 +21,7 @@ export function normalizedCollection(collection: Collection) {
 }
 
 export function getCollectionIcon(
-  collection: Collection,
+  collection: Partial<Collection>,
   { tooltip = "default" } = {},
 ): {
   name: IconName;
@@ -51,10 +51,21 @@ export function getCollectionIcon(
     : { name: "folder" };
 }
 
-export const getCollectionTooltip = (collection: Collection) => {
+const collectionIconTooltipNameMap = {
+  collection: t`collection`,
+  question: t`question`,
+  model: t`model`,
+};
+
+export const getCollectionTooltip = (
+  collection: Partial<Collection>,
+  entity: "collection" | "question" | "model" = "collection",
+) => {
+  const entityText = collectionIconTooltipNameMap[entity];
+
   switch (collection.type) {
     case "instance-analytics":
-      return t`This is a read-only Instance Analytics collection`;
+      return t`This is a read-only Instance Analytics ${entityText}.`;
     default:
       return undefined;
   }
