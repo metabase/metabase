7b15644351516d64f19772be70995c91e214d70d
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-01 12:23:01 +0700

    Manual backport - Fix - Cannot run native queries against MongoDB unless explicitly selecting the table (#31111) (#31198)
    * Update 15946 e2e test
    - modal() expects ModalContent to be present in DOM, but NativeQuerymodal uses ModalBody directly
    - add extra check for run button to be enabled
    
    * Add `collection` to `NativeQuery` type
    
    * Ensure collection is present in dataset query during query conversion
    
    * Unskip e2e test for #15946
    
    * Handle string tableId
    
    ---------
    
    Co-authored-by: Anton Kulyk <kuliks.anton@gmail.com>

--- e2e/test/scenarios/native/reproductions/15946-mongo-pre-select-table.cy.spec.js
+++ e2e/test/scenarios/native/reproductions/15946-mongo-pre-select-table.cy.spec.js
@@ -1,8 +1,8 @@
-import { restore, modal, startNewQuestion } from "e2e/support/helpers";
+import { restore, startNewQuestion } from "e2e/support/helpers";
 
 const MONGO_DB_NAME = "QA Mongo4";
 
-describe.skip("issue 15946", { tags: "@external" }, () => {
+describe("issue 15946", { tags: "@external" }, () => {
   before(() => {
     restore("mongo-4");
     cy.signInAsAdmin();
@@ -15,10 +15,13 @@ describe.skip("issue 15946", { tags: "@external" }, () => {
   it("converting a question to the native query should pre-select a table (metabase#15946)", () => {
     cy.get(".QueryBuilder .Icon-sql").click();
 
-    modal().findByText("Convert this question to a native query").click();
-    modal().should("not.exist");
+    cy.get(".Modal")
+      .findByText("Convert this question to a native query")
+      .click();
+    cy.get(".Modal").should("not.exist");
 
     cy.get(".GuiBuilder-data").contains(MONGO_DB_NAME);
     cy.get(".GuiBuilder-data").contains("Orders");
+    cy.get("aside .RunButton").should("not.be.disabled");
   });
 });
