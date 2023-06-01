a280c559e5a4ac94dbcbc90d7ff9cafe410fe5bc
Emmad Usmani <emmadusmani@berkeley.edu>
2023-06-01 08:55:23 -0700

    Update frontend/src/metabase/core/components/TabRow/TabRow.styled.tsx
    Co-authored-by: Ryan Laurie <30528226+iethree@users.noreply.github.com>

--- frontend/src/metabase/core/components/TabRow/TabRow.styled.tsx
+++ frontend/src/metabase/core/components/TabRow/TabRow.styled.tsx
@@ -4,7 +4,7 @@ import { alpha, color } from "metabase/lib/colors";
 import BaseTabList from "metabase/core/components/TabList";
 import TabLink from "metabase/core/components/TabLink";
 import { TabButton } from "metabase/core/components/TabButton";
-import { SortableDiv } from "metabase/core/components//Sortable";
+import { SortableDiv } from "metabase/core/components/Sortable";
 import { space } from "metabase/styled-components/theme";
 
 export const TabList = styled(BaseTabList)`
