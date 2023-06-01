5106ab29cee48ae94c02116624e90ff8b827a1d9
Anton Kulyk <kuliks.anton@gmail.com>
2023-06-01 12:22:50 +0100

    Use aggregation JS wrappers

--- frontend/src/metabase-lib/aggregation.ts
+++ frontend/src/metabase-lib/aggregation.ts
@@ -1,12 +1,4 @@
-import { to_array } from "cljs/cljs.core";
-import {
-  available_aggregation_operators,
-  aggregation_operator_columns,
-  aggregate as _aggregate,
-  aggregations as _aggregations,
-  aggregation_clause,
-  selected_aggregation_operators,
-} from "cljs/metabase.lib.aggregation";
+import * as ML from "cljs/metabase.lib.js";
 import type {
   AggregationClause,
   AggregationOperator,
@@ -18,20 +10,20 @@ export function availableAggregationOperators(
   query: Query,
   stageIndex: number,
 ): AggregationOperator[] {
-  return to_array(available_aggregation_operators(query, stageIndex));
+  return ML.available_aggregation_operators(query, stageIndex);
 }
 
 export function aggregationOperatorColumns(
   operator: AggregationOperator,
 ): ColumnMetadata[] {
-  return to_array(aggregation_operator_columns(operator));
+  return ML.aggregation_operator_columns(operator);
 }
 
 export function selectedAggregationOperators(
   operators: AggregationOperator[],
   clause: AggregationClause,
 ): AggregationOperator[] {
-  return to_array(selected_aggregation_operators(operators, clause));
+  return ML.selected_aggregation_operators(operators, clause);
 }
 
 export function aggregate(
@@ -39,19 +31,19 @@ export function aggregate(
   stageIndex: number,
   clause: AggregationClause,
 ): Query {
-  return _aggregate(query, stageIndex, clause);
+  return ML.aggregate(query, stageIndex, clause);
 }
 
 export function aggregations(
   query: Query,
   stageIndex: number,
 ): AggregationClause[] {
-  return to_array(_aggregations(query, stageIndex));
+  return ML.aggregations(query, stageIndex);
 }
 
 export function aggregationClause(
   operator: AggregationOperator,
   column?: ColumnMetadata,
 ): AggregationClause {
-  return aggregation_clause(operator, column);
+  return ML.aggregation_clause(operator, column);
 }
