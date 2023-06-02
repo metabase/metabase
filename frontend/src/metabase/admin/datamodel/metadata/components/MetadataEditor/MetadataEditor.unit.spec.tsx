3e4b0253b5c8a85e501bd443849eb448d7254e00
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-02 19:12:37 +0300

    Move tests

--- frontend/src/metabase/admin/datamodel/metadata/components/MetadataEditor/MetadataEditor.unit.spec.tsx
+++ frontend/src/metabase/admin/datamodel/metadata/components/MetadataEditor/MetadataEditor.unit.spec.tsx
@@ -1,5 +1,4 @@
 import { Route } from "react-router";
-import fetchMock from "fetch-mock";
 import userEvent from "@testing-library/user-event";
 import { within } from "@testing-library/react";
 import { Database } from "metabase-types/api";
@@ -20,7 +19,6 @@ import {
 import {
   renderWithProviders,
   screen,
-  waitFor,
   waitForElementToBeRemoved,
 } from "__support__/ui";
 import { getMetadataRoutes } from "../../routes";
@@ -59,6 +57,7 @@ const SAMPLE_DB_MULTI_SCHEMA = createSampleDatabase({
 
 const ORDERS_TABLE_NO_SCHEMA = createOrdersTable({
   id: 10,
+  db_id: 3,
   schema: "",
 });
 
@@ -96,26 +95,6 @@ describe("MetadataEditor", () => {
       expect(
         screen.queryByPlaceholderText("Find a schema"),
       ).not.toBeInTheDocument();
-    });
-
-    it("should allow to navigate to and from table settings", async () => {
-      await setup({ databases: [SAMPLE_DB_NO_SCHEMA] });
-
-      userEvent.click(screen.getByText(ORDERS_TABLE_NO_SCHEMA.display_name));
-      userEvent.click(screen.getByLabelText("Settings"));
-      expect(await screen.findByText("Settings")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(SAMPLE_DB_NO_SCHEMA.name));
-      expect(await screen.findByText("1 Queryable Table")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(ORDERS_TABLE_NO_SCHEMA.display_name));
-      userEvent.click(screen.getByLabelText("Settings"));
-      expect(await screen.findByText("Settings")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(ORDERS_TABLE_NO_SCHEMA.display_name));
-      expect(
-        await screen.findByDisplayValue(ORDERS_TABLE_NO_SCHEMA.display_name),
-      ).toBeInTheDocument();
     });
   });
 
@@ -324,59 +303,6 @@ describe("MetadataEditor", () => {
       );
       expect(section.queryByText("US Dollar")).not.toBeInTheDocument();
     });
-
-    it("should allow to navigate to and from table settings", async () => {
-      await setup();
-
-      userEvent.click(screen.getByText(ORDERS_TABLE.display_name));
-      userEvent.click(screen.getByLabelText("Settings"));
-      expect(await screen.findByText("Settings")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(SAMPLE_DB.name));
-      expect(await screen.findByText("1 Queryable Table")).toBeInTheDocument();
-      expect(screen.getByText("1 Hidden Table")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(ORDERS_TABLE.display_name));
-      userEvent.click(screen.getByLabelText("Settings"));
-      expect(await screen.findByText("Settings")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(ORDERS_TABLE.display_name));
-      expect(
-        await screen.findByDisplayValue(ORDERS_TABLE.display_name),
-      ).toBeInTheDocument();
-    });
-
-    it("should allow to rescan field values", async () => {
-      await setup();
-
-      userEvent.click(screen.getByText(ORDERS_TABLE.display_name));
-      userEvent.click(screen.getByLabelText("Settings"));
-      userEvent.click(
-        await screen.findByRole("button", { name: "Re-scan this table" }),
-      );
-
-      await waitFor(() => {
-        const path = `path:/api/table/${ORDERS_TABLE.id}/rescan_values`;
-        expect(fetchMock.called(path, { method: "POST" })).toBeTruthy();
-      });
-    });
-
-    it("should allow to discard field values", async () => {
-      await setup();
-
-      userEvent.click(screen.getByText(ORDERS_TABLE.display_name));
-      userEvent.click(screen.getByLabelText("Settings"));
-      userEvent.click(
-        await screen.findByRole("button", {
-          name: "Discard cached field values",
-        }),
-      );
-
-      await waitFor(() => {
-        const path = `path:/api/table/${ORDERS_TABLE.id}/discard_values`;
-        expect(fetchMock.called(path, { method: "POST" })).toBeTruthy();
-      });
-    });
   });
 
   describe("multi schema database", () => {
@@ -426,39 +352,6 @@ describe("MetadataEditor", () => {
       ).toBeInTheDocument();
       expect(
         screen.getByText(REVIEWS_TABLE_MULTI_SCHEMA.schema),
-      ).toBeInTheDocument();
-    });
-
-    it("should allow to navigate to and from table settings", async () => {
-      await setup({ databases: [SAMPLE_DB_MULTI_SCHEMA] });
-
-      userEvent.click(screen.getByText(PEOPLE_TABLE_MULTI_SCHEMA.schema));
-      userEvent.click(
-        await screen.findByText(PEOPLE_TABLE_MULTI_SCHEMA.display_name),
-      );
-      userEvent.click(screen.getByLabelText("Settings"));
-      expect(await screen.findByText("Settings")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(SAMPLE_DB_MULTI_SCHEMA.name));
-      expect(await screen.findByText("2 schemas")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(PEOPLE_TABLE_MULTI_SCHEMA.schema));
-      userEvent.click(screen.getByText(PEOPLE_TABLE_MULTI_SCHEMA.display_name));
-      userEvent.click(screen.getByLabelText("Settings"));
-      expect(await screen.findByText("Settings")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(PEOPLE_TABLE_MULTI_SCHEMA.schema));
-      expect(await screen.findByText("1 Queryable Table")).toBeInTheDocument();
-
-      userEvent.click(
-        await screen.findByText(PEOPLE_TABLE_MULTI_SCHEMA.display_name),
-      );
-      userEvent.click(screen.getByLabelText("Settings"));
-      expect(await screen.findByText("Settings")).toBeInTheDocument();
-
-      userEvent.click(screen.getByText(PEOPLE_TABLE_MULTI_SCHEMA.display_name));
-      expect(
-        await screen.findByDisplayValue(PEOPLE_TABLE_MULTI_SCHEMA.display_name),
       ).toBeInTheDocument();
     });
   });
