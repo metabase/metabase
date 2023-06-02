ef6100cee2743cc0b9dfaffc588498f43f717dad
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-02 13:57:33 +0700

    Cover getLeadingText with tests

---
+++ frontend/src/metabase/visualizations/components/skeletons/StaticSkeleton/utils/getLeadingText.unit.spec.ts
@@ -0,0 +1,63 @@
+import { getLeadingText } from "./getLeadingText";
+
+const createHeading = (textContent: string) => {
+  const h1 = document.createElement("h1");
+  h1.textContent = textContent;
+  return h1;
+};
+
+const createParagraph = (textContent: string) => {
+  const p = document.createElement("p");
+  p.textContent = textContent;
+  return p;
+};
+
+const createImage = () => {
+  const img = document.createElement("img");
+  img.src = "https://example.com/img.jpg";
+  return img;
+};
+
+describe("getLeadingText", () => {
+  it("returns text from a sole heading", () => {
+    const h1 = createHeading("Lorem ipsum");
+
+    expect(getLeadingText([h1])).toBe(h1.textContent);
+  });
+
+  it("returns text from a sole p", () => {
+    const p = createParagraph("Lorem ipsum");
+
+    expect(getLeadingText([p])).toBe(p.textContent);
+  });
+
+  it("returns text from a mix of h1 and p", () => {
+    const h1 = createHeading("Lorem ipsum 1");
+    const p = createParagraph("Lorem ipsum 2");
+
+    expect(getLeadingText([h1, p])).toBe(h1.textContent);
+    expect(getLeadingText([p, h1])).toBe(p.textContent);
+  });
+
+  it("skips elements without content", () => {
+    const h1 = createHeading("");
+    const p = createParagraph("Lorem ipsum 2");
+    const img = createImage();
+
+    expect(getLeadingText([p, img, h1])).toBe(p.textContent);
+    expect(getLeadingText([h1, p, img])).toBe(p.textContent);
+    expect(getLeadingText([img, h1, p])).toBe(p.textContent);
+  });
+
+  it("returns an empty string when no element has any content", () => {
+    const h1 = createHeading("");
+    const p = createParagraph("");
+    const img = createImage();
+
+    expect(getLeadingText([h1])).toBe("");
+    expect(getLeadingText([p])).toBe("");
+    expect(getLeadingText([img])).toBe("");
+    expect(getLeadingText([h1, img, p])).toBe("");
+    expect(getLeadingText([])).toBe("");
+  });
+});
