1b856a1108426b9303e15126e9efdd24e4175f73
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-06-02 16:04:53 -0400

    null coalesce minWidth

--- frontend/src/metabase/components/EntityMenu/EntityMenu.jsx
+++ frontend/src/metabase/components/EntityMenu/EntityMenu.jsx
@@ -128,7 +128,7 @@ class EntityMenu extends Component {
                 >
                   <Card>
                     {menuItemContent || (
-                      <ol className="p1" style={{ minWidth: minWidth || 184 }}>
+                      <ol className="p1" style={{ minWidth: minWidth ?? 184 }}>
                         {items.map(item => {
                           if (!item) {
                             return null;
