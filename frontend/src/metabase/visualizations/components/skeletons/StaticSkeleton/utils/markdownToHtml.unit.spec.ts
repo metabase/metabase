7f2ff59bf13c1c30bca1df70c2c1171d0c4c8b58
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-02 14:19:11 +0700

    Add a test case for a mix of h1 and p

--- frontend/src/metabase/visualizations/components/skeletons/StaticSkeleton/utils/markdownToHtml.unit.spec.ts
+++ frontend/src/metabase/visualizations/components/skeletons/StaticSkeleton/utils/markdownToHtml.unit.spec.ts
@@ -2,7 +2,7 @@ import { markdownToHtml } from "./markdownToHtml";
 import { createHeading, createParagraph } from "./test-utils";
 
 describe("markdownToHtml", () => {
-  it("handles empty input", () => {
+  it("handles an empty input", () => {
     expect(markdownToHtml("")).toEqual([]);
   });
 
@@ -13,6 +13,22 @@ describe("markdownToHtml", () => {
 
     expect(markdownToHtml("Lorem ipsum")).toEqual([
       createParagraph("Lorem ipsum"),
+    ]);
+  });
+
+  it("converts a mix of h1 and p", () => {
+    expect(
+      markdownToHtml(["# Lorem ipsum 1", "Lorem ipsum 2"].join("\n")),
+    ).toEqual([
+      createHeading("Lorem ipsum 1"),
+      createParagraph("Lorem ipsum 2"),
+    ]);
+
+    expect(
+      markdownToHtml(["Lorem ipsum 1", "# Lorem ipsum 2"].join("\n")),
+    ).toEqual([
+      createParagraph("Lorem ipsum 1"),
+      createHeading("Lorem ipsum 2"),
     ]);
   });
 });
