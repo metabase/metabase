6d9a10f58e74c70a4e85ccba96ac61207409c6e4
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-05 19:57:54 +0300

    Add test for actions

--- e2e/test/scenarios/dashboard/dashboard-back-navigation.cy.spec.js
+++ e2e/test/scenarios/dashboard/dashboard-back-navigation.cy.spec.js
@@ -1,6 +1,7 @@
 import {
   appBar,
   collectionTable,
+  createAction,
   getDashboardCard,
   getDashboardCardMenu,
   getDashboardCards,
@@ -9,11 +10,13 @@ import {
   queryBuilderHeader,
   restore,
   rightSidebar,
+  setActionsEnabledForDB,
   summarize,
   visitDashboard,
   visualize,
 } from "e2e/support/helpers";
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
 
 const { ORDERS_ID } = SAMPLE_DATABASE;
 
@@ -21,6 +24,8 @@ describe("scenarios > dashboard > dashboard back navigation", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+    setActionsEnabledForDB(SAMPLE_DB_ID);
+
     cy.intercept("POST", `/api/dataset`).as("dataset");
     cy.intercept("POST", `/api/card/*/query`).as("cardQuery");
     cy.intercept("PUT", `/api/card/*`).as("updateCard");
@@ -100,8 +105,7 @@ describe("scenarios > dashboard > dashboard back navigation", () => {
   });
 
   it("should preserve query results when navigating between the dashboard and the query builder", () => {
-    createDashboardWithCards();
-    cy.get("@dashboardId").then(visitDashboard);
+    createAndVisitDashboardWithCards();
     cy.wait("@dashboard");
     cy.wait("@dashcardQuery");
 
@@ -111,24 +115,17 @@ describe("scenarios > dashboard > dashboard back navigation", () => {
       cy.wait("@cardQuery");
     });
 
-    queryBuilderHeader().within(() => {
-      cy.findByLabelText("Back to Test Dashboard").click();
-    });
+    queryBuilderHeader().findByLabelText("Back to Test Dashboard").click();
 
-    getDashboardCard(0).within(() => {
-      cy.findByText("101.04").should("be.visible"); // cached data
-    });
-
-    getDashboardCard(1).within(() => {
-      cy.findByText("Text card").should("be.visible"); // cached data
-    });
+    // cached data
+    getDashboardCard(0).findByText("101.04").should("be.visible");
+    getDashboardCard(1).findByText("Text card").should("be.visible");
+    getDashboardCard(2).findByText("Action card").should("be.visible");
 
     cy.get("@dashboard.all").should("have.length", 1);
     cy.get("@dashcardQuery.all").should("have.length", 1);
 
-    appBar().within(() => {
-      cy.findByText("Our analytics").click();
-    });
+    appBar().findByText("Our analytics").click();
 
     collectionTable().within(() => {
       cy.findByText("Test Dashboard").click();
@@ -181,38 +178,96 @@ describe("scenarios > dashboard > dashboard back navigation", () => {
   });
 });
 
-const createDashboardWithCards = () => {
+const createAndVisitDashboardWithCards = () => {
+  const questionDetails = {
+    name: "Orders",
+    query: { "source-table": ORDERS_ID },
+  };
+
+  const modelDetails = {
+    name: "Orders model",
+    query: { "source-table": ORDERS_ID },
+    dataset: true,
+  };
+
+  const actionDetails = {
+    name: "Update orders quantity",
+    type: "query",
+    database_id: SAMPLE_DB_ID,
+    dataset_query: {
+      database: SAMPLE_DB_ID,
+      native: {
+        query: "UPDATE orders SET quantity = quantity",
+      },
+      type: "native",
+    },
+    parameters: [],
+    visualization_settings: {
+      type: "button",
+    },
+  };
+
+  const questionDashcardDetails = {
+    row: 0,
+    col: 0,
+    size_x: 8,
+    size_y: 8,
+    visualization_settings: {},
+  };
+
+  const textDashcardDetails = {
+    col: 8,
+    row: 0,
+    size_x: 4,
+    size_y: 8,
+    visualization_settings: {
+      virtual_card: {
+        name: null,
+        display: "text",
+        visualization_settings: {},
+        dataset_query: {},
+        archived: false,
+      },
+      text: "Text card",
+    },
+  };
+
+  const actionDashcardDetails = {
+    row: 8,
+    col: 0,
+    size_x: 4,
+    size_y: 1,
+    series: [],
+    visualization_settings: {
+      actionDisplayType: "button",
+      virtual_card: {
+        name: null,
+        display: "action",
+        visualization_settings: {},
+        dataset_query: {},
+        archived: false,
+      },
+      "button.label": "Action card",
+    },
+  };
+
   cy.createDashboard().then(({ body: { id: dashboard_id } }) => {
-    cy.request("PUT", `/api/dashboard/${dashboard_id}/cards`, {
-      cards: [
-        {
-          id: -1,
-          card_id: 1,
-          row: 0,
-          col: 0,
-          size_x: 8,
-          size_y: 8,
-        },
-        {
-          id: -2,
-          card_id: null,
-          col: 8,
-          row: 0,
-          size_x: 4,
-          size_y: 4,
-          visualization_settings: {
-            virtual_card: {
-              name: null,
-              display: "text",
-              visualization_settings: {},
-              dataset_query: {},
-              archived: false,
-            },
-            text: "Text card",
+    cy.createQuestion(questionDetails).then(({ body: { id: question_id } }) => {
+      cy.createQuestion(modelDetails).then(({ body: { id: model_id } }) => {
+        createAction({ ...actionDetails, model_id }).then(
+          ({ body: { id: action_id } }) => {
+            cy.request("PUT", `/api/dashboard/${dashboard_id}/cards`, {
+              cards: [
+                { id: -1, card_id: question_id, ...questionDashcardDetails },
+                { id: -2, ...textDashcardDetails },
+                { id: -3, ...actionDashcardDetails, action_id },
+              ],
+            });
           },
-        },
-      ],
+        );
+      });
     });
-    cy.wrap(dashboard_id).as("dashboardId");
+
+    visitDashboard(dashboard_id);
   });
 };
