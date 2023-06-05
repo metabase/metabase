d61a412da400f3305dc1bc3995e742ac607289bf
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2023-06-05 08:57:36 -0400

    use font-size and weight props for styling text

--- frontend/src/metabase/components/type/Text.jsx
+++ frontend/src/metabase/components/type/Text.jsx
@@ -1,15 +1,21 @@
 import styled from "@emotion/styled";
+import PropTypes from "prop-types";
 import { color } from "metabase/lib/colors";
 
 const Text = styled.div`
   color: ${props => color(`text-${props.color}`)};
-  font-size: ${props => props.fontSize}px;
+  font-size: ${props => props.fontSize};
+  font-weight: ${props => props.fontWeight};
 `;
 
 Text.defaultProps = {
   color: "medium",
 };
 
-Text.propTypes = {};
+Text.propTypes = {
+  color: PropTypes.string,
+  fontSize: PropTypes.string,
+  fontWeight: PropTypes.number,
+};
 
 export default Text;
