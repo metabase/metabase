f42769fce9242c12daed34ba9ee7ba03033c8767
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2023-06-05 12:43:48 -0400

    tweak theme [ci skip]

--- frontend/src/metabase/ui/theme.ts
+++ frontend/src/metabase/ui/theme.ts
@@ -4,10 +4,11 @@ import { color } from "metabase/lib/colors";
 
 export const theme: MantineThemeOverride = {
   colors: {
-    brand: [color("brand")],
-    text: [color("text-dark"), color("text-medium")],
+    brand: [color("brand-light"), color("brand")],
+    text: [color("text-light"), color("text-medium"), color("text-dark")],
   },
   primaryColor: "brand",
+  primaryShade: 1,
   fontFamily: "Lato, sans-serif",
   components: {
     Radio: {
@@ -17,16 +18,10 @@ export const theme: MantineThemeOverride = {
             marginBottom: theme.spacing.xs,
           },
           label: {
-            color: theme.colors.text[0],
+            color: theme.colors.text[2],
             marginLeft: "-2px",
             fontWeight: 700,
             fontSize: "14px",
-          },
-          radio: {
-            "&:checked": {
-              backgroundColor: theme.colors.brand[0],
-              borderColor: theme.colors.brand[0],
-            },
           },
         };
       },
@@ -34,6 +29,10 @@ export const theme: MantineThemeOverride = {
     RadioGroup: {
       styles(theme) {
         return {
+          label: {
+            fontWeight: 700,
+            color: theme.colors.text[2],
+          },
           description: {
             marginBottom: theme.spacing.xs,
           },
