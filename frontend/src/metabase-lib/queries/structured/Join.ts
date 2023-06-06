198aeb42e2afc339f7c6647e2d058b5b29c56593
Tamás Benkő <tamas@metabase.com>
2023-06-06 18:16:45 +0300

    Calculate and set join alias when joining

--- frontend/src/metabase-lib/queries/structured/Join.ts
+++ frontend/src/metabase-lib/queries/structured/Join.ts
@@ -178,6 +178,7 @@ export default class Join extends MBQLObjectClause {
     );
   }
 
+  // TODO migrate to metbase lib
   setDefaultAlias() {
     // The Join alias should be "Table - FK Field" if possible. We need both to disamiguate sitatutions where we have
     // multiple FKs that point to the same Table -- see #8418 and #11452.
