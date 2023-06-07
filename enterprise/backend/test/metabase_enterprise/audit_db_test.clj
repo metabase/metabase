6310d5febdc19b105bfca24736858f2d8d5a00da
Bryan Maass <bryan.maass@gmail.com>
2023-06-07 17:15:32 -0600

    constrain tests to postgres

--- enterprise/backend/test/metabase_enterprise/audit_db_test.clj
+++ enterprise/backend/test/metabase_enterprise/audit_db_test.clj
@@ -4,6 +4,7 @@
             [clojure.test :refer [deftest is]]
             [metabase-enterprise.audit-db :as audit-db]
             [metabase.models.database :refer [Database]]
+            [metabase.test :as mt]
             [toucan2.core :as t2]))
 
 (defmacro with-audit-db-restoration [& body]
@@ -17,32 +18,35 @@
            (#'metabase.core/ensure-audit-db-installed!))))))
 
 (deftest audit-db-is-installed-then-left-alone
-  (with-audit-db-restoration
-    (t2/delete! Database :is_audit true)
-    (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-db-installed!)))
-    (is (= :metabase-enterprise.audit-db/no-op (audit-db/ensure-db-installed!)))
+  (mt/test-drivers #{:postgres}
+    (with-audit-db-restoration
+      (t2/delete! Database :is_audit true)
+      (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-db-installed!)))
+      (is (= :metabase-enterprise.audit-db/no-op (audit-db/ensure-db-installed!)))
 
-    (t2/update! Database :is_audit true {:engine "datomic"})
-    (is (= :metabase-enterprise.audit-db/updated (audit-db/ensure-db-installed!)))
-    (is (= :metabase-enterprise.audit-db/no-op (audit-db/ensure-db-installed!)))))
+      (t2/update! Database :is_audit true {:engine "datomic"})
+      (is (= :metabase-enterprise.audit-db/updated (audit-db/ensure-db-installed!)))
+      (is (= :metabase-enterprise.audit-db/no-op (audit-db/ensure-db-installed!))))))
 
 (deftest audit-db-content-is-not-installed-when-not-found
-  (with-audit-db-restoration
-    (with-redefs [audit-db/analytics-root-dir-resource nil]
-      (is (= nil audit-db/analytics-root-dir-resource))
-      (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-audit-db-installed!)))
-      (is (= 13371337 (t2/select-one-fn :id 'Database {:where [:= :is_audit true]}))
-          "Audit DB is installed.")
-      (is (= [] (t2/select 'Card {:where [:= :database_id 13371337]}))
-          "No cards created for Audit DB."))))
+  (mt/test-drivers #{:postgres}
+    (with-audit-db-restoration
+      (with-redefs [audit-db/analytics-root-dir-resource nil]
+        (is (= nil audit-db/analytics-root-dir-resource))
+        (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-audit-db-installed!)))
+        (is (= 13371337 (t2/select-one-fn :id 'Database {:where [:= :is_audit true]}))
+            "Audit DB is installed.")
+        (is (= [] (t2/select 'Card {:where [:= :database_id 13371337]}))
+            "No cards created for Audit DB.")))))
 
 (deftest audit-db-content-is-installed-when-found
-  (with-audit-db-restoration
-    (with-redefs [audit-db/analytics-root-dir-resource (io/resource "instance_analytics_skip")]
-      (is (str/ends-with? (str audit-db/analytics-root-dir-resource)
-                          "instance_analytics_skip"))
-      (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-audit-db-installed!)))
-      (is (= 13371337 (t2/select-one-fn :id 'Database {:where [:= :is_audit true]}))
-          "Audit DB is installed.")
-      (is (not= 0 (t2/count 'Card {:where [:= :database_id 13371337]}))
-          "Cards should be created for Audit DB when the content is there."))))
+  (mt/test-drivers #{:postgres}
+    (with-audit-db-restoration
+      (with-redefs [audit-db/analytics-root-dir-resource (io/resource "instance_analytics_skip")]
+        (is (str/ends-with? (str audit-db/analytics-root-dir-resource)
+                            "instance_analytics_skip"))
+        (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-audit-db-installed!)))
+        (is (= 13371337 (t2/select-one-fn :id 'Database {:where [:= :is_audit true]}))
+            "Audit DB is installed.")
+        (is (not= 0 (t2/count 'Card {:where [:= :database_id 13371337]}))
+            "Cards should be created for Audit DB when the content is there.")))))
