1c8563019417ce0f6db92a353c85dd7a96eca6dd
Oisin Coveney <oisin@metabase.com>
2023-06-07 17:21:43 +0300

    Reduce test code redundancies and test empty

--- frontend/src/metabase/visualizations/components/skeletons/ChartSkeleton/ChartSkeleton.unit.spec.tsx
+++ frontend/src/metabase/visualizations/components/skeletons/ChartSkeleton/ChartSkeleton.unit.spec.tsx
@@ -1,79 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import {
+  ChartSkeletonDisplayType,
+  chartSkeletonDisplayTypes,
+} from "metabase/visualizations/components/skeletons/util/display-type";
 import ChartSkeleton from "./ChartSkeleton";
 
 describe("ChartSkeleton", () => {
+  // Test empty
   it("should render empty", () => {
-    render(<ChartSkeleton name="Empty" />);
+    render(<ChartSkeleton name={"Empty"} />);
     expect(screen.getByText("Empty")).toBeInTheDocument();
   });
 
-  it("should render area", () => {
-    render(<ChartSkeleton display="area" name="Area" />);
-    expect(screen.getByText("Area")).toBeInTheDocument();
-  });
-
-  it("should render bar", () => {
-    render(<ChartSkeleton display="bar" name="Bar" />);
-    expect(screen.getByText("Bar")).toBeInTheDocument();
-  });
-
-  it("should render funnel", () => {
-    render(<ChartSkeleton display="funnel" name="Funnel" />);
-    expect(screen.getByText("Funnel")).toBeInTheDocument();
-  });
-
-  it("should render gauge", () => {
-    render(<ChartSkeleton display="gauge" name="Gauge" />);
-    expect(screen.getByText("Gauge")).toBeInTheDocument();
-  });
-
-  it("should render line", () => {
-    render(<ChartSkeleton display="line" name="Line" />);
-    expect(screen.getByText("Line")).toBeInTheDocument();
-  });
-
-  it("should render map", () => {
-    render(<ChartSkeleton display="map" name="Map" />);
-    expect(screen.getByText("Map")).toBeInTheDocument();
-  });
-
-  it("should render table", () => {
-    render(<ChartSkeleton display="table" name="Table" />);
-    expect(screen.getByText("Table")).toBeInTheDocument();
-  });
-
-  it("should render pie", () => {
-    render(<ChartSkeleton display="pie" name="Pie" />);
-    expect(screen.getByText("Pie")).toBeInTheDocument();
-  });
-
-  it("should render progress", () => {
-    render(<ChartSkeleton display="progress" name="Progress" />);
-    expect(screen.getByText("Progress")).toBeInTheDocument();
-  });
-
-  it("should render row", () => {
-    render(<ChartSkeleton display="row" name="Row" />);
-    expect(screen.getByText("Row")).toBeInTheDocument();
-  });
-
-  it("should render scalar", () => {
-    render(<ChartSkeleton display="scalar" name="Scalar" />);
-    expect(screen.getByText("Scalar")).toBeInTheDocument();
-  });
-
-  it("should render scatter", () => {
-    render(<ChartSkeleton display="scatter" name="Scatter" />);
-    expect(screen.getByText("Scatter")).toBeInTheDocument();
-  });
-
-  it("should render smartscalar", () => {
-    render(<ChartSkeleton display="smartscalar" name="Trend" />);
-    expect(screen.getByText("Trend")).toBeInTheDocument();
-  });
-
-  it("should render waterfall", () => {
-    render(<ChartSkeleton display="waterfall" name="Waterfall" />);
-    expect(screen.getByText("Waterfall")).toBeInTheDocument();
+  // Test other display types
+  chartSkeletonDisplayTypes.forEach((display: ChartSkeletonDisplayType) => {
+    it(`should render ${display}`, () => {
+      render(<ChartSkeleton display={display} name={display} />);
+      expect(screen.getByText(display)).toBeInTheDocument();
+    });
   });
 });
--- frontend/src/metabase/visualizations/components/skeletons/util/display-type.ts
+++ frontend/src/metabase/visualizations/components/skeletons/util/display-type.ts
@@ -1,18 +1,20 @@
-export type ChartSkeletonDisplayType =
-  | "area"
-  | "bar"
-  | "funnel"
-  | "gauge"
-  | "line"
-  | "map"
-  | "object"
-  | "pivot"
-  | "table"
-  | "pie"
-  | "progress"
-  | "row"
-  | "scalar"
-  | "scatter"
-  | "smartscalar"
-  | "waterfall"
-  | null;
+export const chartSkeletonDisplayTypes = [
+  "area",
+  "bar",
+  "funnel",
+  "gauge",
+  "line",
+  "map",
+  "object",
+  "pivot",
+  "table",
+  "pie",
+  "progress",
+  "row",
+  "scalar",
+  "scatter",
+  "smartscalar",
+  "waterfall",
+] as const;
+
+export type ChartSkeletonDisplayType = typeof chartSkeletonDisplayTypes[number];
