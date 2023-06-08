4f2d6f133c6bb94e91854a4cd7c569909636e27d
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-08 15:13:45 +0700

    Remove rehype-filter from parseMarkdown

--- frontend/src/metabase/lib/markdown/getLeadingText.ts
+++ frontend/src/metabase/lib/markdown/getLeadingText.ts
@@ -1,26 +1,8 @@
 import { parseMarkdown } from "./parseMarkdown";
 import type { Node } from "./types";
 
-const PARSE_OPTIONS = {
-  disallowedElements: [
-    "h1",
-    "h2",
-    "h3",
-    "h4",
-    "h5",
-    "h6",
-    "html",
-    "ol",
-    "ul",
-    "a",
-    "code",
-    "pre",
-  ],
-  unwrapDisallowed: true,
-};
-
 export const getLeadingText = (value: string): string => {
-  const root = parseMarkdown(value, PARSE_OPTIONS);
+  const root = parseMarkdown(value);
   const firstTextChild = root.children.find(child => renderText(child));
   return firstTextChild ? renderText(firstTextChild) : "";
 };
--- frontend/src/metabase/lib/markdown/parseMarkdown.tsx
+++ frontend/src/metabase/lib/markdown/parseMarkdown.tsx
@@ -1,5 +1,3 @@
-import type { Options } from "react-markdown/lib/rehype-filter";
-import rehypeFilter from "react-markdown/lib/rehype-filter";
 import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
@@ -7,12 +5,12 @@ import { unified } from "unified";
 
 const REMARK_PLUGINS = [remarkGfm];
 
-export const parseMarkdown = (value: string, options: Options = {}) => {
+export const parseMarkdown = (value: string) => {
   const processor = unified()
     .use(remarkParse)
     .use(REMARK_PLUGINS)
-    .use(remarkRehype)
-    .use(rehypeFilter, options);
+    .use(remarkRehype);
+
   const file = { value };
   const root = processor.runSync(processor.parse(file), file);
 
