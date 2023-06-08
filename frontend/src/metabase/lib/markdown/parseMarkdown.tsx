153efc9c5b0aaedaeb5d365709be40afb1de43c3
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-08 15:44:00 +0700

    Bring rehypeFilter back as it fixes typing errors in getLeadingText

--- frontend/src/metabase/lib/markdown/parseMarkdown.tsx
+++ frontend/src/metabase/lib/markdown/parseMarkdown.tsx
@@ -1,3 +1,5 @@
+import type { Options } from "react-markdown/lib/rehype-filter";
+import rehypeFilter from "react-markdown/lib/rehype-filter";
 import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
@@ -5,12 +7,12 @@ import { unified } from "unified";
 
 const REMARK_PLUGINS = [remarkGfm];
 
-export const parseMarkdown = (value: string) => {
+export const parseMarkdown = (value: string, options: Options = {}) => {
   const processor = unified()
     .use(remarkParse)
     .use(REMARK_PLUGINS)
-    .use(remarkRehype);
-
+    .use(remarkRehype)
+    .use(rehypeFilter, options);
   const file = { value };
   const root = processor.runSync(processor.parse(file), file);
 
