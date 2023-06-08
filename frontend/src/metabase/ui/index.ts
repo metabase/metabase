e2ba727247be2aea3513ef0379241a0289741412
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-08 17:46:13 +0300

    Add exports for layout components (#31434)

--- frontend/src/metabase/ui/index.ts
+++ frontend/src/metabase/ui/index.ts
@@ -1,8 +1,1 @@
-export { ThemeProvider } from "metabase/ui/components/ThemeProvider";
-export { theme } from "metabase/ui/theme";
-
-// General helpers
-export { Box, Group } from "@mantine/core";
-
-// Inputs
-export { Radio } from "metabase/ui/components/inputs/Radio";
+export * from "./components";
