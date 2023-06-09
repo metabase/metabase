b5939de895f0180ae7e7bd4774e9130b5f31688c
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-06-09 13:00:51 -0300

    fix spec

--- frontend/src/metabase/visualizations/components/settings/ChartSettingFieldsPartition.unit.spec.js
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingFieldsPartition.unit.spec.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import { createMockColumn } from "metabase-types/api/mocks";
 import ChartSettingFieldsPartition from "./ChartSettingFieldsPartition";
