d90731b4c0b2225c377ea1b5c8c1a50b4196b06a
Anton Kulyk <kuliks.anton@gmail.com>
2023-06-12 19:31:53 +0100

    Fix `Icon` import

--- frontend/src/metabase/common/components/AggregationPicker/AggregationPicker.tsx
+++ frontend/src/metabase/common/components/AggregationPicker/AggregationPicker.tsx
@@ -2,7 +2,7 @@ import { useCallback, useMemo, useState } from "react";
 import { t } from "ttag";
 
 import AccordionList from "metabase/core/components/AccordionList";
-import Icon from "metabase/components/Icon";
+import { Icon } from "metabase/core/components/Icon";
 
 import * as Lib from "metabase-lib";
 
