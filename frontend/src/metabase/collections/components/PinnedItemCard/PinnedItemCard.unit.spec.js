39c0a05a21775b8a4ffe88f17cd4085c1308469a
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-12 20:35:45 +0700

    Add description-related unit tests to PinnedItemCard

--- frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.unit.spec.js
+++ frontend/src/metabase/collections/components/PinnedItemCard/PinnedItemCard.unit.spec.js
@@ -1,6 +1,7 @@
-import { Route } from "react-router";
 import userEvent from "@testing-library/user-event";
-import { renderWithProviders, screen, getIcon } from "__support__/ui";
+import { Route } from "react-router";
+
+import { getIcon, renderWithProviders, screen, waitFor } from "__support__/ui";
 
 import PinnedItemCard from "./PinnedItemCard";
 
@@ -13,6 +14,24 @@ const defaultCollection = {
   name: "Collection Foo",
   archived: false,
 };
+
+const HEADING_1_TEXT = "Heading 1";
+const HEADING_1_MARKDOWN = `# ${HEADING_1_TEXT}`;
+const HEADING_2_TEXT = "Heading 2";
+const HEADING_2_MARKDOWN = `## ${HEADING_2_TEXT}`;
+const PARAGRAPH_TEXT = "Paragraph with link";
+const PARAGRAPH_MARKDOWN = "Paragraph with [link](https://example.com)";
+const MARKDOWN = [
+  HEADING_1_MARKDOWN,
+  HEADING_2_MARKDOWN,
+  PARAGRAPH_MARKDOWN,
+].join("\n\n");
+const MARKDOWN_AS_TEXT = [HEADING_1_TEXT, HEADING_2_TEXT, PARAGRAPH_TEXT].join(
+  " ",
+);
+const HEADING_LONG_TEXT =
+  "This is a very long description that will require visual truncation in the user interface";
+const HEADING_LONG_MARKDOWN = `# ${HEADING_LONG_TEXT}`;
 
 function getCollectionItem({
   id = 1,
@@ -108,6 +127,50 @@ describe("PinnedItemCard", () => {
         "href",
         "/model/1-order/detail",
       );
+    });
+  });
+
+  describe("description", () => {
+    it("should show only the first line of description without markdown formatting", () => {
+      setup({ item: getCollectionItem({ description: MARKDOWN }) });
+
+      expect(screen.getByText(HEADING_1_TEXT)).toBeInTheDocument();
+      expect(screen.queryByText(HEADING_1_MARKDOWN)).not.toBeInTheDocument();
+      expect(screen.queryByText(HEADING_2_MARKDOWN)).not.toBeInTheDocument();
+      expect(screen.queryByText(HEADING_2_TEXT)).not.toBeInTheDocument();
+      expect(screen.queryByText(PARAGRAPH_MARKDOWN)).not.toBeInTheDocument();
+      expect(screen.queryByText(PARAGRAPH_TEXT)).not.toBeInTheDocument();
+    });
+
+    it("should show description tooltip with markdown formatting", () => {
+      setup({ item: getCollectionItem({ description: MARKDOWN }) });
+
+      userEvent.hover(screen.getByText(HEADING_1_TEXT));
+
+      const tooltip = screen.getByRole("tooltip");
+
+      expect(tooltip).not.toHaveTextContent(MARKDOWN);
+      expect(tooltip).not.toHaveTextContent(HEADING_1_MARKDOWN);
+      expect(tooltip).not.toHaveTextContent(HEADING_2_MARKDOWN);
+      expect(tooltip).toHaveTextContent(MARKDOWN_AS_TEXT);
+    });
+
+    it("should show description tooltip when ellipis is necessary", async () => {
+      setup({
+        item: getCollectionItem({ description: HEADING_LONG_MARKDOWN }),
+      });
+
+      userEvent.hover(screen.getByText(HEADING_LONG_TEXT));
+
+      await waitFor(() => {
+        expect(screen.getByRole("tooltip")).toBeInTheDocument();
+      });
+
+      const tooltip = screen.getByRole("tooltip");
+
+      expect(tooltip).toHaveAttribute("data-state", "visible");
+      expect(tooltip).not.toHaveTextContent(HEADING_LONG_MARKDOWN);
+      expect(tooltip).toHaveTextContent(HEADING_LONG_TEXT);
     });
   });
 });
