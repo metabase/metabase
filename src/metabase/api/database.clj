f5ebdd8a94b0d4af565f6863fec06d015b13e6ec
Ngoc Khuat <qn.khuat@gmail.com>
2023-06-13 17:09:41 +0700

    Update src/metabase/api/database.clj
    Co-authored-by: Cal Herries <39073188+calherries@users.noreply.github.com>

--- src/metabase/api/database.clj
+++ src/metabase/api/database.clj
@@ -311,8 +311,8 @@
   (if-not include
     db
     (-> (t2/hydrate db (case include
-                        "tables"        :tables
-                        "tables.fields" [:tables [:fields [:target :has_field_values] :has_field_values]]))
+                         "tables"        :tables
+                         "tables.fields" [:tables [:fields [:target :has_field_values] :has_field_values]]))
         (update :tables (fn [tables]
                           (cond->> tables
                             ; filter hidden tables
