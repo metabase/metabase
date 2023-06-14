2de0424212bce5c74e5741ceee7873ffe03b35c1
Emmad Usmani <emmadusmani@berkeley.edu>
2023-06-14 08:05:31 -0700

    fix test after rebasing onto master

--- frontend/src/metabase/dashboard/components/DashboardTabs/DashboardTabs.unit.spec.tsx
+++ frontend/src/metabase/dashboard/components/DashboardTabs/DashboardTabs.unit.spec.tsx
@@ -148,16 +148,14 @@ describe("DashboardTabs", () => {
       it("should automatically select the tab in the slug if valid", async () => {
         setup({
           isEditing: false,
-          slug: getSlug({ tabId: 2, name: "Page 2" }),
+          slug: getSlug({ tabId: 2, name: "Tab 2" }),
         });
 
         expect(selectTab(2)).toHaveAttribute("aria-selected", "true");
         expect(queryTab(1)).toHaveAttribute("aria-selected", "false");
         expect(queryTab(3)).toHaveAttribute("aria-selected", "false");
 
-        expect(
-          await findSlug({ tabId: 2, name: "Page 2" }),
-        ).toBeInTheDocument();
+        expect(await findSlug({ tabId: 2, name: "Tab 2" })).toBeInTheDocument();
       });
 
       it("should automatically select the first tab if slug is invalid", async () => {
@@ -170,9 +168,7 @@ describe("DashboardTabs", () => {
         expect(queryTab(2)).toHaveAttribute("aria-selected", "false");
         expect(queryTab(3)).toHaveAttribute("aria-selected", "false");
 
-        expect(
-          await findSlug({ tabId: 1, name: "Page 1" }),
-        ).toBeInTheDocument();
+        expect(await findSlug({ tabId: 1, name: "Tab 1" })).toBeInTheDocument();
       });
 
       it("should allow you to click to select tabs", async () => {
