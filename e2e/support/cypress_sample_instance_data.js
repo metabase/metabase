2101f28f3b9cfb651b652ba2b055489fbef2755b
Ryan Laurie <iethree@gmail.com>
2023-06-14 08:19:08 -0600

    load snapshot instance data

---
+++ e2e/support/cypress_sample_instance_data.js
@@ -0,0 +1,26 @@
+/**
+ *  This JSON file gets recreated every time Cypress starts.
+ *  See: `e2e/snapshot-creators/default.cy.snap.js:19`
+ *
+ *  - It had to be added to `.gitignore`.
+ *  - It contains extracted metadata from the default instance state (like question and dashboard ids)
+ */
+
+import _ from "underscore";
+// eslint-disable-next-line import/no-unresolved
+
+import SAMPLE_INSTANCE_DATA from "./cypress_sample_instance_data.json";
+
+export const ORDERS_QUESTION_ID = _.findWhere_(SAMPLE_INSTANCE_DATA.questions, {
+  name: "Orders",
+}).id;
+
+export const ORDERS_COUNT_QUESTION_ID = _.findWhere_(
+  SAMPLE_INSTANCE_DATA.questions,
+  { name: "Orders, Count" },
+).id;
+
+export const ORDERS_BY_YEAR_QUESTION_ID = _.findWhere_(
+  SAMPLE_INSTANCE_DATA.questions,
+  { name: "Orders, Count, Grouped by Created At (year)" },
+).id;
