de6ee1def3b6657054ccdb6ae1bd6d956376fce3
Nick Fitzpatrick <nickfitz.582@gmail.com>
2023-06-16 08:37:24 -0300

    Custom Homepage Dashboard Polish

---
+++ frontend/src/metabase/home/components/CustomHomePageModal/CustomHomePageModal.unit.spec.tsx
@@ -0,0 +1,47 @@
+import userEvent from "@testing-library/user-event";
+import { renderWithProviders, screen } from "__support__/ui";
+
+import {
+  setupCollectionsEndpoints,
+  setupCollectionItemsEndpoint,
+} from "__support__/server-mocks";
+
+import {
+  createMockCollection,
+  createMockCollectionItem,
+} from "metabase-types/api/mocks";
+import { CustomHomePageModal } from "./CustomHomePageModal";
+
+const ROOT_COLLECTION = createMockCollection({
+  id: "root",
+  can_write: true,
+  name: "Our Analytics",
+  location: undefined,
+});
+const COLLECTION_ITEM = createMockCollectionItem({
+  name: "Foo",
+  model: "dashboard",
+});
+
+const setup = ({ ...props } = {}) => {
+  const onClose = jest.fn();
+
+  setupCollectionsEndpoints({ collections: [ROOT_COLLECTION] });
+  setupCollectionItemsEndpoint(ROOT_COLLECTION, [COLLECTION_ITEM]);
+
+  renderWithProviders(
+    <CustomHomePageModal onClose={onClose} isOpen={true} {...props} />,
+  );
+};
+
+describe("custom hompage modal", () => {
+  it("should only enable the save button if a dashboard has been selected", async () => {
+    setup();
+    expect(await screen.findByRole("button", { name: /save/i })).toBeDisabled();
+
+    userEvent.click(await screen.findByText("Select a dashboard"));
+    userEvent.click(await screen.findByText("Foo"));
+
+    expect(await screen.findByRole("button", { name: /save/i })).toBeEnabled();
+  });
+});
