d5953460af1b0dc9f4d50ad9055506d89d9756de
Shaun Williams <shaun@metabase.com>
2023-06-15 20:21:36 -0500

    modernize NudgeToPro [skip ci]

--- frontend/src/metabase/admin/people/components/NudgeToPro.tsx
+++ frontend/src/metabase/admin/people/components/NudgeToPro.tsx
@@ -1,25 +1,14 @@
-import { connect } from "react-redux";
-import _ from "underscore";
-
 import ExternalLink from "metabase/core/components/ExternalLink";
 import { getUpgradeUrl } from "metabase/selectors/settings";
-import { State } from "metabase-types/store";
+import { useSelector } from "metabase/lib/redux";
 
-interface NudgeProps {
-  upgradeUrl: string;
-}
-
-const NudgeToPro = (props: NudgeProps) => {
+export const NudgeToPro = () => {
+  const upgradeUrl = useSelector(state =>
+    getUpgradeUrl(state, { utm_media: "admin_people" }),
+  );
   return (
     <div>
-      TODO: Nudge to <ExternalLink href={props.upgradeUrl}>Pro</ExternalLink>
+      TODO: Nudge to <ExternalLink href={upgradeUrl}>Pro</ExternalLink>
     </div>
   );
 };
-
-const mapStateToProps = (state: State) => ({
-  upgradeUrl: getUpgradeUrl(state, { utm_media: "admin_people" }),
-});
-
-// eslint-disable-next-line import/no-default-export -- deprecated usage
-export default _.compose(connect(mapStateToProps))(NudgeToPro);
