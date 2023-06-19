05bb9c3dac506fee3b61a3cc76fcfb61a33c45fd
Shaun Williams <shaun@metabase.com>
2023-06-19 14:29:03 -0500

    add integration test

---
+++ frontend/src/metabase/admin/people/containers/AdminPeopleApp.unit.spec.tsx
@@ -0,0 +1,61 @@
+import {
+  createMockTokenFeatures,
+  createMockUser,
+} from "metabase-types/api/mocks";
+import {
+  createMockSettingsState,
+  createMockState,
+} from "metabase-types/store/mocks";
+import { renderWithProviders, screen } from "__support__/ui";
+import { AdminPeopleApp } from "metabase/admin/people/containers/AdminPeopleApp";
+
+interface SetupOpts {
+  activeUsersCount: number;
+  ssoEnabled: boolean;
+  isSuperUser: boolean;
+}
+
+const setup = ({ activeUsersCount, ssoEnabled, isSuperUser }: SetupOpts) => {
+  const state = createMockState({
+    currentUser: createMockUser({
+      is_superuser: isSuperUser,
+    }),
+    settings: createMockSettingsState({
+      "active-users-count": activeUsersCount,
+      "token-features": createMockTokenFeatures({
+        sso: ssoEnabled,
+      }),
+    }),
+  });
+
+  renderWithProviders(<AdminPeopleApp>empty</AdminPeopleApp>, {
+    storeInitialState: state,
+  });
+};
+
+describe("AdminPeopleApp", () => {
+  describe("nudge to pro", () => {
+    const nudgeText = /Get single-sign on/;
+    const setupOpts = {
+      activeUsersCount: 50,
+      ssoEnabled: false,
+      isSuperUser: true,
+    };
+    it("should be visible when user is admin, has 50 active users, and SSO is not available", () => {
+      setup(setupOpts);
+      expect(screen.getByText(nudgeText)).toBeInTheDocument();
+    });
+    it("should not be visible with less than 50 users", () => {
+      setup({ ...setupOpts, activeUsersCount: 10 });
+      expect(screen.queryByText(nudgeText)).not.toBeInTheDocument();
+    });
+    it("should not be visible when user is not admin", () => {
+      setup({ ...setupOpts, isSuperUser: false });
+      expect(screen.queryByText(nudgeText)).not.toBeInTheDocument();
+    });
+    it("should not be visible when SSO is already available", () => {
+      setup({ ...setupOpts, ssoEnabled: true });
+      expect(screen.queryByText(nudgeText)).not.toBeInTheDocument();
+    });
+  });
+});
