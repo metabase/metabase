01025eecfb0b39642f92f6680d6c1bf162019057
Shaun Williams <shaun@metabase.com>
2023-06-19 09:27:09 -0500

    pixel -> rem

--- frontend/src/metabase/admin/people/components/NudgeToPro.styled.tsx
+++ frontend/src/metabase/admin/people/components/NudgeToPro.styled.tsx
@@ -4,28 +4,28 @@ import ExternalLink from "metabase/core/components/ExternalLink";
 
 export const NudgeCard = styled.div`
   background-color: ${color("bg-light")};
-  border-radius: 6px;
-  padding: 20px 24px;
-  margin-top: 32px;
+  border-radius: 0.375rem;
+  padding: 1.25rem 1.5rem;
+  margin-top: 2rem;
   display: flex;
   flex-direction: column;
 `;
 
 export const Description = styled.div`
-  margin-top: 16px;
+  margin-top: 1rem;
 `;
 
 export const Subtitle = styled.div`
-  margin-top: 8px;
+  margin-top: 0.5rem;
   font-weight: 700;
 `;
 
 export const ProLink = styled(ExternalLink)`
-  margin-top: 16px;
+  margin-top: 1rem;
   font-weight: 700;
-  padding: 12px 16px;
+  padding: 0.75rem 1rem;
   border: 1px solid ${color("brand")};
-  border-radius: 8px;
+  border-radius: 0.5rem;
   color: ${color("brand")};
   width: fit-content;
 
