2a07103c67be62bd031bfe193c07026c169109e6
Callum Herries <hi@callumherries.com>
2023-06-20 12:37:24 +0300

    Add unit test

--- frontend/src/metabase/search/components/SearchResult.unit.spec.js
+++ frontend/src/metabase/search/components/SearchResult.unit.spec.js
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import fetchMock from "fetch-mock";
+import { render, renderWithProviders, screen, fireEvent } from "__support__/ui";
 import { setupEnterpriseTest } from "__support__/enterprise";
 import SearchResult from "./SearchResult";
 
@@ -22,6 +23,47 @@ function collection({
   collection.collection = collection;
   return collection;
 }
+
+describe("SearchResult > Tables", () => {
+  it("only tables with initial_sync_status='complete' are clickable", async () => {
+    const table = { id: 1, display_name: "Table Name" };
+    const database = { id: 1, name: "Database Name" };
+    fetchMock.get("path:/api/table/1", table);
+    fetchMock.get("path:/api/database/1", database);
+    const result = {
+      model: "table",
+      name: "Complete Table",
+      initial_sync_status: "complete",
+      table_id: 1,
+      database_id: 1,
+      getUrl: () => `/table/1`,
+      getIcon: () => ({ name: "table" }),
+    };
+
+    const onClick = jest.fn();
+    renderWithProviders(<SearchResult result={result} onClick={onClick} />);
+    const link = screen.getByText("Complete Table");
+    fireEvent.click(link);
+    expect(onClick).toHaveBeenCalled();
+
+    const incompleteResult = {
+      model: "table",
+      name: "Incomplete Table",
+      initial_sync_status: "incomplete",
+      table_id: 1,
+      database_id: 1,
+      getUrl: () => `/table/1`,
+      getIcon: () => ({ name: "table" }),
+    };
+    const incompleteOnClick = jest.fn();
+    renderWithProviders(
+      <SearchResult result={incompleteResult} onClick={incompleteOnClick} />,
+    );
+    const incompleteLink = screen.getByText("Incomplete Table");
+    fireEvent.click(incompleteLink);
+    expect(incompleteOnClick).not.toHaveBeenCalled();
+  });
+});
 
 describe("SearchResult > Collections", () => {
   const regularCollection = collection();
