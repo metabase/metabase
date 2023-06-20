f44f8cef342a933db103aa2e0258190b66cb59e9
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-20 18:37:31 +0300

    Add e2e test

--- e2e/test/scenarios/question/reproductions/30610-stale-results-metadata.cy.spec.js
+++ e2e/test/scenarios/question/reproductions/30610-stale-results-metadata.cy.spec.js
@@ -1,11 +1,15 @@
 import {
+  modal,
   openNotebook,
   openOrdersTable,
   popover,
+  queryBuilderHeader,
   restore,
   saveQuestion,
+  visitQuestion,
   visualize,
 } from "e2e/support/helpers";
+import { ORDERS_QUESTION_ID } from "e2e/support/cypress_sample_instance_data";
 
 describe("issue 30610", () => {
   beforeEach(() => {
@@ -16,26 +20,50 @@ describe("issue 30610", () => {
   it("should remove stale metadata when saving a new question (metabase#30610)", () => {
     openOrdersTable();
     openNotebook();
-    cy.findByTestId("fields-picker").click();
-    popover().findByText("Select none").click();
-    saveQuestion("Orders with ID");
+    removeSourceColumns();
+    saveQuestion("New orders");
+    createAdHocQuestion("New orders");
+    visualizeAdHocQuestion();
+  });
 
-    cy.findByTestId("app-bar").findByText("New").click();
-    popover().within(() => {
-      cy.findByText("Question").click();
-      cy.findByText("Saved Questions").click();
-      cy.findByText("Orders with ID").click();
-    });
-    cy.findByTestId("fields-picker").click();
-    popover().within(() => {
-      cy.findByText("ID").should("be.visible");
-      cy.findByText("Total").should("not.exist");
-    });
-
-    visualize();
-    cy.findByTestId("TableInteractive-root").within(() => {
-      cy.findByText("ID").should("exist");
-      cy.findByText("Total").should("not.exist");
-    });
+  it("should remove stale metadata when updating an existing question (metabase#30610)", () => {
+    visitQuestion(ORDERS_QUESTION_ID);
+    openNotebook();
+    removeSourceColumns();
+    updateQuestion();
+    createAdHocQuestion("Orders");
+    visualizeAdHocQuestion();
   });
 });
+
+function updateQuestion() {
+  queryBuilderHeader().findByText("Save").click();
+  modal().button("Save").click();
+}
+
+function removeSourceColumns() {
+  cy.findByTestId("fields-picker").click();
+  popover().findByText("Select none").click();
+}
+
+function createAdHocQuestion(questionName) {
+  cy.findByTestId("app-bar").findByText("New").click();
+  popover().within(() => {
+    cy.findByText("Question").click();
+    cy.findByText("Saved Questions").click();
+    cy.findByText(questionName).click();
+  });
+  cy.findByTestId("fields-picker").click();
+  popover().within(() => {
+    cy.findByText("ID").should("be.visible");
+    cy.findByText("Total").should("not.exist");
+  });
+}
+
+function visualizeAdHocQuestion() {
+  visualize();
+  cy.findByTestId("TableInteractive-root").within(() => {
+    cy.findByText("ID").should("exist");
+    cy.findByText("Total").should("not.exist");
+  });
+}
