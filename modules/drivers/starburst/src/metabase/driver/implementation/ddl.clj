b36e7017dda2fb0fbcd48e99cafe131539993c58
Ege İliklier <ege.iliklier@gmail.com>
2023-06-21 18:15:56 +0300

    feat: support persist in catalogs that does not support `CREATE TABLE AS` via `secondary-catalog`

--- modules/drivers/starburst/src/metabase/driver/implementation/ddl.clj
+++ modules/drivers/starburst/src/metabase/driver/implementation/ddl.clj
@@ -18,6 +18,63 @@
   (str "-- Metabase\n"
        sql-str))
 
+(defn- get-catalog
+  "Get catalog value from database object"
+  [database]
+  (get-in database [:details :catalog]))
+
+(defn- assoc-catalog
+  "Return a new database object set with given catalog"
+  [database catalog]
+  (assoc-in database [:details :catalog] catalog))
+
+
+(defn- table-qname
+  [catalog schema table]
+  (format "%s.%s.%s"
+          (quote-fn :table catalog)
+          (quote-fn :table schema)
+          (quote-fn :table table)))
+
+(defn- get-used-catalogs
+  "Get used catalogs"
+  [database]
+  (let [catalog (get-in database [:details :catalog])
+        secondary-catalog (get-in database [:details :secondary-catalog])]
+    (if (some? secondary-catalog) [catalog secondary-catalog] [catalog])))
+
+(defn execute!
+  "Executes sql and params with a standard remark prepended to the statement."
+  [conn [sql & params]]
+  (jdbc/execute! conn (into [(add-remark sql)] params) {:transaction? false}))
+
+(defn- create-table-as-sql
+  "Formats a create table as statement"
+  [table query]
+    (format "create table %s as %s" table query))
+
+(defn- create-table-like-sql
+  "Formats a create table like existing table statement"
+  [table existing-table]
+  (format "create table %s (like %s)" table existing-table))
+
+(defn- insert-into-from-sql
+  "Formats a insert into table from another table statement"
+  [target source]
+  (format "insert into %s select * from %s" target source))
+
+(defn create-schema-sql
+  "Formats a create schema statement"
+  ([database]
+   (format "create schema if not exists %s.%s"
+           (quote-fn :table (get-catalog database))
+           (quote-fn :table (ddl.i/schema-name database (public-settings/site-uuid))))))
+
+(defn drop-table-sql
+  "Formats a drop table statement"
+  [qname]
+    (format "drop table if exists %s" qname))
+
 (defn- create-kv-table-honey-sql-form
   "The honeysql form that creates the persisted schema `cache_info` table."
   [schema-name]
@@ -25,32 +82,40 @@
    :with-columns [[:key :varchar] [:value :varchar]]})
 
 
-(defn execute!
-  "Executes sql and params with a standard remark prepended to the statement."
-  [conn [sql & params]]
-  (jdbc/execute! conn (into [(add-remark sql)] params) {:transaction? false}))
-
-
 (defmethod ddl.i/refresh! :starburst [_driver database definition dataset-query]
   (let [{:keys [query params]} (qp/compile dataset-query)]
     (jdbc/with-db-connection [conn (sql-jdbc.conn/db->pooled-connection-spec database)]
-      (let [schema-name (quote-fn :table (ddl.i/schema-name database (public-settings/site-uuid)))
-            temporary-table (assoc definition :table-name (format "%s_tmp" (:table-name definition)))]
+      (let [table-name (:table-name definition)
+            schema-name (ddl.i/schema-name database (public-settings/site-uuid))
+            main-catalog (get-catalog database)
+            secondary-catalog (get-in database [:details :secondary-catalog])]
 
-          (execute! conn (into [(sql.ddl/create-table-sql database temporary-table query)] params))
-        (try
-          (execute! conn [(sql.ddl/drop-table-sql database (:table-name definition))])
-          (execute! conn [(format "create table %s.%s as select * from %s.%s"
-                                          schema-name (:table-name definition)
-                                          schema-name (:table-name temporary-table))])
-          (catch Exception e
-            (log/warn e)
-            (throw e))
-          (finally
-            (execute! conn [(sql.ddl/drop-table-sql database (:table-name temporary-table))])))
-        )
-      )
-    {:state :success}))
+        (if (some? secondary-catalog)
+          (let [target-table (table-qname main-catalog schema-name table-name)
+                source-table (table-qname secondary-catalog schema-name table-name)]
+            (execute! conn (into [(create-table-as-sql source-table query)] params))
+            (try
+              (execute! conn [(drop-table-sql target-table)])
+              (execute! conn [(create-table-like-sql target-table source-table)])
+              (execute! conn [(insert-into-from-sql target-table source-table)])
+              (catch Exception e
+                (log/warn e)
+                (throw e))
+              (finally
+                (execute! conn [(drop-table-sql source-table)]))))
+          (let [target-table (table-qname main-catalog schema-name table-name)
+                source-table (table-qname main-catalog schema-name (str table-name "_tmp"))]
+            (execute! conn (into [(create-table-as-sql source-table query)] params))
+            (try
+              (execute! conn [(drop-table-sql target-table)])
+              (execute! conn [(create-table-as-sql target-table (format "select * from %s" source-table))])
+              (catch Exception e
+                (log/warn e)
+                (throw e))
+              (finally
+                (execute! conn [(drop-table-sql source-table)]))))))
+      {:state :success})))
+
 
 (defmethod ddl.i/unpersist! :starburst
   [_driver database persisted-info]
@@ -61,41 +126,41 @@
         (log/warn e)
         (throw e)))))
 
+
 (defmethod ddl.i/check-can-persist :starburst
   [database]
   (let [schema-name (ddl.i/schema-name database (public-settings/site-uuid))
         table-name  (format "persistence_check_%s" (rand-int 10000))
+        catalogs    (get-used-catalogs database)
         steps       [[:persist.check/create-schema
                       (fn check-schema [conn]
-                        (let [existing-schemas (->> ["show schemas"]
-                                                    (sql.ddl/jdbc-query conn)
-                                                    (map :schema)
-                                                    (into #{}))]
-                          (or (contains? existing-schemas schema-name)
-                              (execute! conn [(sql.ddl/create-schema-sql database)]))))]
+                        (doseq [catalog catalogs]
+                          (execute! conn [(create-schema-sql (assoc-catalog database catalog))])))]
                      [:persist.check/create-table
                       (fn create-table [conn]
-                        (execute! conn [(sql.ddl/create-table-sql database
-                                                                  {:table-name table-name
-                                                                   :field-definitions [{:field-name "field"
-                                                                                        :base-type :type/Text}]}
-                                                                  "select 1 tmp")]))]
+                        (doseq [catalog catalogs]
+                          (execute! conn [(create-table-as-sql
+                                           (table-qname catalog schema-name table-name)
+                                           "select 1 tmp")])))]
                      [:persist.check/read-table
                       (fn read-table [conn]
-                        (sql.ddl/jdbc-query conn [(format "select * from %s.%s"
-                                                          schema-name table-name)]))]
+                        (doseq [catalog catalogs]
+                          (sql.ddl/jdbc-query conn [(format "select * from %s"
+                                                            (table-qname catalog schema-name table-name))])))]
                      [:persist.check/delete-table
                       (fn delete-table [conn]
-                        (execute! conn [(sql.ddl/drop-table-sql database table-name)]))]
+                        (doseq [catalog catalogs]
+                          (execute! conn [(drop-table-sql
+                                           (table-qname catalog schema-name table-name))])))]
                      [:persist.check/create-kv-table
                       (fn create-kv-table [conn]
                         (execute! conn [(format "drop table if exists %s.cache_info"
-                                                        schema-name)])
+                                                schema-name)])
                         (execute! conn (sql/format
                                         (create-kv-table-honey-sql-form schema-name)
                                         {:dialect :ansi})))]
                      [:persist.check/populate-kv-table
-                      (fn create-kv-table [conn]
+                      (fn populate-kv-table [conn]
                         (execute! conn (sql/format
                                         (ddl.i/populate-kv-table-honey-sql-form
                                          schema-name)
