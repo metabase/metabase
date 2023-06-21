699d7a22dc82356131cf0cd44362be9f724b9127
Ryan Laurie <iethree@gmail.com>
2023-06-21 16:33:59 -0600

    convert searchResult to typescript

--- frontend/src/metabase/search/components/InfoText.tsx
+++ frontend/src/metabase/search/components/InfoText.tsx
@@ -11,7 +11,12 @@ import Database from "metabase/entities/databases";
 import Table from "metabase/entities/tables";
 import { PLUGIN_COLLECTIONS } from "metabase/plugins";
 import { getTranslatedEntityName } from "metabase/nav/utils";
+
+import type { Collection } from "metabase-types/api";
+import type TableType from "metabase-lib/metadata/Table";
+
 import { CollectionBadge } from "./CollectionBadge";
+import type { WrappedResult } from "./types";
 
 const searchResultPropTypes = {
   database_id: PropTypes.number,
@@ -26,7 +31,7 @@ const infoTextPropTypes = {
   result: PropTypes.shape(searchResultPropTypes),
 };
 
-export function InfoText({ result }) {
+function getInfoText(result: WrappedResult) {
   switch (result.model) {
     case "card":
       return jt`Saved question in ${formatCollection(
@@ -57,9 +62,16 @@ export function InfoText({ result }) {
   }
 }
 
+export function InfoText({ result }: { result: WrappedResult }) {
+  return <>{getInfoText(result)}</>;
+}
+
 InfoText.propTypes = infoTextPropTypes;
 
-function formatCollection(result, collection) {
+function formatCollection(
+  result: WrappedResult,
+  collection: Partial<Collection>,
+) {
   return (
     collection.id && (
       <CollectionBadge key={result.model} collection={collection} />
@@ -67,54 +79,63 @@ function formatCollection(result, collection) {
   );
 }
 
-function getCollectionInfoText(collection) {
-  if (PLUGIN_COLLECTIONS.isRegularCollection(collection)) {
+function getCollectionInfoText(collection: Partial<Collection>) {
+  if (
+    PLUGIN_COLLECTIONS.isRegularCollection(collection) ||
+    !collection.authority_level
+  ) {
     return t`Collection`;
   }
   const level = PLUGIN_COLLECTIONS.AUTHORITY_LEVEL[collection.authority_level];
   return `${level.name} ${t`Collection`}`;
 }
 
-function TablePath({ result }) {
-  return jt`Table in ${(
-    <span key="table-path">
-      <Database.Link id={result.database_id} />{" "}
-      {result.table_schema && (
-        <Schema.ListLoader
-          query={{ dbId: result.database_id }}
-          loadingAndErrorWrapper={false}
-        >
-          {({ list }) =>
-            list?.length > 1 ? (
-              <span>
-                <Icon name="chevronright" mx="4px" size={10} />
-                {/* we have to do some {} manipulation here to make this look like the table object that browseSchema was written for originally */}
-                <Link
-                  to={Urls.browseSchema({
-                    db: { id: result.database_id },
-                    schema_name: result.table_schema,
-                  })}
-                >
-                  {result.table_schema}
-                </Link>
-              </span>
-            ) : null
-          }
-        </Schema.ListLoader>
-      )}
-    </span>
-  )}`;
+function TablePath({ result }: { result: WrappedResult }) {
+  return (
+    <>
+      {jt`Table in ${(
+        <span key="table-path">
+          <Database.Link id={result.database_id} />{" "}
+          {result.table_schema && (
+            <Schema.ListLoader
+              query={{ dbId: result.database_id }}
+              loadingAndErrorWrapper={false}
+            >
+              {({ list }: { list: typeof Schema[] }) =>
+                list?.length > 1 ? (
+                  <span>
+                    <Icon name="chevronright" size={10} />
+                    {/* we have to do some {} manipulation here to make this look like the table object that browseSchema was written for originally */}
+                    <Link
+                      to={Urls.browseSchema({
+                        db: { id: result.database_id },
+                        schema_name: result.table_schema,
+                      } as TableType)}
+                    >
+                      {result.table_schema}
+                    </Link>
+                  </span>
+                ) : null
+              }
+            </Schema.ListLoader>
+          )}
+        </span>
+      )}`}
+    </>
+  );
 }
 
 TablePath.propTypes = {
   result: PropTypes.shape(searchResultPropTypes),
 };
 
-function TableLink({ result }) {
+function TableLink({ result }: { result: WrappedResult }) {
   return (
     <Link to={Urls.tableRowsQuery(result.database_id, result.table_id)}>
       <Table.Loader id={result.table_id} loadingAndErrorWrapper={false}>
-        {({ table }) => (table ? <span>{table.display_name}</span> : null)}
+        {({ table }: { table: TableType }) =>
+          table ? <span>{table.display_name}</span> : null
+        }
       </Table.Loader>
     </Link>
   );
--- frontend/src/metabase/search/components/SearchResult.styled.tsx
+++ frontend/src/metabase/search/components/SearchResult.styled.tsx
@@ -6,23 +6,41 @@ import Link from "metabase/core/components/Link";
 import Text from "metabase/components/type/Text";
 import LoadingSpinner from "metabase/components/LoadingSpinner";
 
-function getColorForIconWrapper(props: {
+import type { SearchModelType } from "metabase-types/api";
+
+type SearchEntity = any;
+
+interface ResultStylesProps {
+  compact: boolean;
   active: boolean;
-  type: string;
-  item: { collection_position?: unknown };
+  isSelected: boolean;
+}
+
+function getColorForIconWrapper({
+  item,
+  active,
+  type,
+}: {
+  item: SearchEntity;
+  active: boolean;
+  type: SearchModelType;
 }) {
-  if (!props.active) {
+  if (!active) {
     return color("text-medium");
-  } else if (props.item.collection_position) {
+  } else if (item.collection_position) {
     return color("saturated-yellow");
-  } else if (props.type === "collection") {
+  } else if (type === "collection") {
     return lighten("brand", 0.35);
   } else {
     return color("brand");
   }
 }
 
-export const IconWrapper = styled.div`
+export const IconWrapper = styled.div<{
+  item: SearchEntity;
+  active: boolean;
+  type: SearchModelType;
+}>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -50,13 +68,7 @@ export const Title = styled("h3")<{ active: boolean }>`
   color: ${props => color(props.active ? "text-dark" : "text-medium")};
 `;
 
-interface ResultButtonProps {
-  isSelected: boolean;
-  compact: boolean;
-  active: boolean;
-}
-
-export const ResultButton = styled.button<ResultButtonProps>`
+export const ResultButton = styled.button<ResultStylesProps>`
   ${props => resultStyles(props)}
   padding-right: 0.5rem;
   text-align: left;
@@ -68,27 +80,25 @@ export const ResultButton = styled.button<ResultButtonProps>`
   }
 `;
 
-export const ResultLink = styled(Link)<ResultButtonProps>`
+export const ResultLink = styled(Link)<ResultStylesProps>`
   ${props => resultStyles(props)}
 `;
 
-const resultStyles = (props: ResultButtonProps) => `
+const resultStyles = ({ compact, active, isSelected }: ResultStylesProps) => `
   display: block;
-  background-color: ${
-    props.isSelected ? lighten("brand", 0.63) : "transparent"
-  };
-  min-height: ${props.compact ? "36px" : "54px"};
+  background-color: ${isSelected ? lighten("brand", 0.63) : "transparent"};
+  min-height: ${compact ? "36px" : "54px"};
   padding-top: ${space(1)};
   padding-bottom: ${space(1)};
   padding-left: 14px;
-  padding-right: ${props.compact ? "20px" : space(3)};
-  cursor: ${props.active ? "pointer" : "default"};
+  padding-right: ${compact ? "20px" : space(3)};
+  cursor: ${active ? "pointer" : "default"};
 
   &:hover {
-    background-color: ${props.active ? lighten("brand", 0.63) : ""};
+    background-color: ${active ? lighten("brand", 0.63) : ""};
 
     h3 {
-      color: ${props.active || props.isSelected ? color("brand") : ""};
+      color: ${active || isSelected ? color("brand") : ""};
     }
   }
 
@@ -98,8 +108,8 @@ const resultStyles = (props: ResultButtonProps) => `
     text-decoration-style: dashed;
 
     &:hover {
-      color: ${props.active ? color("brand") : ""};
-      text-decoration-color: ${props.active ? color("brand") : ""};
+      color: ${active ? color("brand") : ""};
+      text-decoration-color: ${active ? color("brand") : ""};
     }
   }
 
@@ -111,11 +121,11 @@ const resultStyles = (props: ResultButtonProps) => `
   }
 
   h3 {
-    font-size: ${props.compact ? "14px" : "16px"};
+    font-size: ${compact ? "14px" : "16px"};
     line-height: 1.2em;
     overflow-wrap: anywhere;
     margin-bottom: 0;
-    color: ${props.active && props.isSelected ? color("brand") : ""};
+    color: ${active && isSelected ? color("brand") : ""};
   }
 
   .Icon-info {
