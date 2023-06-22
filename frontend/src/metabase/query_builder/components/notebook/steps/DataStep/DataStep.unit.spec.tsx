6cd14b02b41016fb5ef6e7cd10bc35a050a6a1b9
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-22 19:59:12 +0300

    Add first test

--- frontend/src/metabase/query_builder/components/notebook/steps/DataStep/DataStep.unit.spec.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/DataStep/DataStep.unit.spec.tsx
@@ -1,13 +1,24 @@
 import userEvent from "@testing-library/user-event";
 import { getIcon, renderWithProviders, screen } from "__support__/ui";
 import * as Lib from "metabase-lib";
+import { columnFinder, createQuery } from "metabase-lib/test-helpers";
 import { createMockNotebookStep } from "../../test-utils";
 import { DataStep } from "./DataStep";
 
+const createQueryWithSingleColumn = (columnName: string) => {
+  const query = createQuery();
+  const columns = Lib.fieldableColumns(query, 0);
+  const findColumn = columnFinder(query, columns);
+  const foundColumn = findColumn("ORDERS", columnName);
+  return Lib.withFields(query, 0, [foundColumn]);
+};
+
+const getCheckbox = (label: string) => {
+  const name = new RegExp(`^((check|dash) icon )?${label}$`);
+  return screen.getByRole("checkbox", { name });
+};
+
 const setup = (step = createMockNotebookStep()) => {
-  const columns = Lib.fieldableColumns(step.topLevelQuery, 0).map(column =>
-    Lib.displayInfo(step.topLevelQuery, 0, column),
-  );
   const updateQuery = jest.fn();
 
   renderWithProviders(
@@ -27,26 +38,26 @@ const setup = (step = createMockNotebookStep()) => {
     return lastCall[0];
   };
 
-  return {
-    columns,
-    getNextQuery,
-  };
+  return { getNextQuery };
 };
 
 describe("DataStep", () => {
   it("should render with all columns selected", () => {
-    const { columns } = setup();
+    setup();
     userEvent.click(getIcon("chevrondown"));
-    expect(
-      screen.getByRole("checkbox", { name: "check icon Select none" }),
-    ).toBeChecked();
 
-    columns.forEach(column => {
-      expect(
-        screen.getByRole("checkbox", {
-          name: `check icon ${column.displayName}`,
-        }),
-      ).toBeChecked();
-    });
+    expect(getCheckbox("Select none")).toBeChecked();
+    expect(getCheckbox("ID")).toBeChecked();
+    expect(getCheckbox("Tax")).toBeChecked();
+  });
+
+  it("should render with a single column selected", () => {
+    const query = createQueryWithSingleColumn("ID");
+    setup(createMockNotebookStep({ topLevelQuery: query }));
+    userEvent.click(getIcon("chevrondown"));
+
+    expect(getCheckbox("Select all")).not.toBeChecked();
+    expect(getCheckbox("ID")).toBeChecked();
+    expect(getCheckbox("Tax")).not.toBeChecked();
   });
 });
