48d4d4e3111ff5b9e04e1fcb664ae98d6d4846df
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-22 20:04:11 +0300

    Add first test

--- frontend/src/metabase/query_builder/components/notebook/steps/DataStep/DataStep.unit.spec.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/DataStep/DataStep.unit.spec.tsx
@@ -5,12 +5,11 @@ import { columnFinder, createQuery } from "metabase-lib/test-helpers";
 import { createMockNotebookStep } from "../../test-utils";
 import { DataStep } from "./DataStep";
 
-const createQueryWithSingleColumn = (columnName: string) => {
+const createQueryWithFields = (columnNames: string[]) => {
   const query = createQuery();
-  const columns = Lib.fieldableColumns(query, 0);
-  const findColumn = columnFinder(query, columns);
-  const foundColumn = findColumn("ORDERS", columnName);
-  return Lib.withFields(query, 0, [foundColumn]);
+  const findColumn = columnFinder(query, Lib.fieldableColumns(query, 0));
+  const columns = columnNames.map(name => findColumn("ORDERS", name));
+  return Lib.withFields(query, 0, columns);
 };
 
 const getCheckbox = (label: string) => {
@@ -48,16 +47,34 @@ describe("DataStep", () => {
 
     expect(getCheckbox("Select none")).toBeChecked();
     expect(getCheckbox("ID")).toBeChecked();
+    expect(getCheckbox("ID")).toBeEnabled();
     expect(getCheckbox("Tax")).toBeChecked();
+    expect(getCheckbox("Tax")).toBeEnabled();
   });
 
   it("should render with a single column selected", () => {
-    const query = createQueryWithSingleColumn("ID");
+    const query = createQueryWithFields(["ID"]);
     setup(createMockNotebookStep({ topLevelQuery: query }));
     userEvent.click(getIcon("chevrondown"));
 
     expect(getCheckbox("Select all")).not.toBeChecked();
     expect(getCheckbox("ID")).toBeChecked();
+    expect(getCheckbox("ID")).toBeDisabled();
     expect(getCheckbox("Tax")).not.toBeChecked();
+    expect(getCheckbox("Tax")).toBeEnabled();
+  });
+
+  it("should render with multiple columns selected", () => {
+    const query = createQueryWithFields(["ID", "TOTAL"]);
+    setup(createMockNotebookStep({ topLevelQuery: query }));
+    userEvent.click(getIcon("chevrondown"));
+
+    expect(getCheckbox("Select all")).not.toBeChecked();
+    expect(getCheckbox("ID")).toBeChecked();
+    expect(getCheckbox("ID")).toBeEnabled();
+    expect(getCheckbox("Tax")).not.toBeChecked();
+    expect(getCheckbox("Tax")).toBeEnabled();
+    expect(getCheckbox("Total")).toBeChecked();
+    expect(getCheckbox("Total")).toBeEnabled();
   });
 });
