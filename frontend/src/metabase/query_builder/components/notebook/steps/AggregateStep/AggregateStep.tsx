253c6fd3d50a651f70a027893e3a4a4c904be538
Anton Kulyk <kuliks.anton@gmail.com>
2023-06-26 14:14:01 +0100

    Migrate notebook aggregations to MLv2 — Operators (1) (#31527)
    * Add aggregation types
    
    * Add aggregation TypeScript wrappers
    
    * Move `AggregateStep` to its own directory
    
    * Port basic features to MLv2
    
    * Deprecate `AggregationPopover`
    
    * Fix MLv1's aggregation clause validation
    
    * Fix import
    
    * `requiresField` → `requiresColumn`
    
    * Add basic tests for `AggregationPicker`
    
    * Add `findAggregationOperator` test utility
    
    * Add basic tests for `AggregateStep`
    
    * Make stage index explicit
    
    * Highlight selections, add picker back button
    
    * Use long display name for aggregation clauses
    
    * Remove `React` imports
    
    * Enable temporal bucketing
    
    * Use aggregation JS wrappers
    
    * Fix `Icon` import
    
    * Patch unit test according to BE changes
    
    * Fix E2E test
    
    * Simplify `Aggregation's` `dimension` method
    
    * Fix formatting

---
+++ frontend/src/metabase/query_builder/components/notebook/steps/AggregateStep/AggregateStep.tsx
@@ -0,0 +1,96 @@
+import { t } from "ttag";
+
+import * as Lib from "metabase-lib";
+
+import type { NotebookStepUiComponentProps } from "../../types";
+import ClauseStep from "../ClauseStep";
+
+import { AggregationPicker } from "./AggregateStep.styled";
+
+const aggTetherOptions = {
+  attachment: "top left",
+  targetAttachment: "bottom left",
+  offset: "0 10px",
+  constraints: [
+    {
+      to: "scrollParent",
+      attachment: "together",
+    },
+  ],
+};
+
+export function AggregateStep({
+  topLevelQuery,
+  step,
+  color,
+  isLastOpened,
+  readOnly,
+  updateQuery,
+}: NotebookStepUiComponentProps) {
+  const { stageIndex } = step;
+
+  const clauses = Lib.aggregations(topLevelQuery, stageIndex);
+  const operators = Lib.availableAggregationOperators(
+    topLevelQuery,
+    stageIndex,
+  );
+
+  const handleAddAggregation = (aggregation: Lib.AggregationClause) => {
+    const nextQuery = Lib.aggregate(topLevelQuery, stageIndex, aggregation);
+    updateQuery(nextQuery);
+  };
+
+  const handleUpdateAggregation = (
+    currentClause: Lib.AggregationClause,
+    nextClause: Lib.AggregationClause,
+  ) => {
+    const nextQuery = Lib.replaceClause(
+      topLevelQuery,
+      stageIndex,
+      currentClause,
+      nextClause,
+    );
+    updateQuery(nextQuery);
+  };
+
+  const handleRemoveAggregation = (aggregation: Lib.AggregationClause) => {
+    const nextQuery = Lib.removeClause(topLevelQuery, stageIndex, aggregation);
+    updateQuery(nextQuery);
+  };
+
+  const renderAggregationName = (aggregation: Lib.AggregationClause) =>
+    Lib.displayInfo(topLevelQuery, stageIndex, aggregation).longDisplayName;
+
+  return (
+    <ClauseStep
+      items={clauses}
+      initialAddText={t`Pick the metric you want to see`}
+      readOnly={readOnly}
+      color={color}
+      isLastOpened={isLastOpened}
+      tetherOptions={aggTetherOptions}
+      renderName={renderAggregationName}
+      renderPopover={aggregation => (
+        <AggregationPicker
+          query={topLevelQuery}
+          stageIndex={stageIndex}
+          operators={
+            aggregation
+              ? Lib.selectedAggregationOperators(operators, aggregation)
+              : operators
+          }
+          onSelect={newAggregation => {
+            const isUpdate = aggregation != null;
+            if (isUpdate) {
+              handleUpdateAggregation(aggregation, newAggregation);
+            } else {
+              handleAddAggregation(newAggregation);
+            }
+          }}
+        />
+      )}
+      onRemove={handleRemoveAggregation}
+      data-testid="aggregate-step"
+    />
+  );
+}
