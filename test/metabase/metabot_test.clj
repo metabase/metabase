8a312e8c07d34191dbdee45f455b41544a19de10
Noah Moss <noahbmoss@gmail.com>
2023-06-26 08:47:21 -0400

    fix BE tests

--- test/metabase/metabot_test.clj
+++ test/metabase/metabot_test.clj
@@ -53,7 +53,7 @@
 
 (deftest infer-sql-test
   (testing "Test the 'plumbing' of the infer-sql function. The actual invocation of the remote bot is dynamically rebound."
-    (mt/with-temporary-setting-values [is-metabot-enabled true]
+    (mt/with-temp-env-var-value [mb-is-metabot-enabled true]
       (mt/dataset sample-dataset
         (t2.with-temp/with-temp
          [Card model {:dataset_query
@@ -61,25 +61,25 @@
                        :type     :query
                        :query    {:source-table (mt/id :orders)}}
                       :dataset true}]
-          (let [bot-sql (format "SELECT * FROM %s" (:name model))]
-            (with-redefs [metabot-client/*create-chat-completion-endpoint* (test-bot-endpoint-single-message bot-sql)
-                          metabot-client/*create-embedding-endpoint*       simple-embedding-stub
-                          metabot-util/*prompt-templates*                  (constantly test-prompt-templates)]
-              (let [{:keys [inner_query] :as denormalized-model} (metabot-util/denormalize-model model)
-                    user_prompt   "Show me all of my data"
-                    context       {:model       denormalized-model
-                                   :user_prompt user_prompt
-                                   :prompt_task :infer_sql}
-                    result        (metabot/infer-sql context)
-                    generated-sql (get-in result [:card :dataset_query :native :query])]
-                (is (true? (str/includes? generated-sql inner_query)))
-                (is (true? (str/ends-with?
-                            (str/replace generated-sql #"\W+" "")
-                            (str/replace bot-sql #"\W+" ""))))))))))))
+         (let [bot-sql (format "SELECT * FROM %s" (:name model))]
+           (with-redefs [metabot-client/*create-chat-completion-endpoint* (test-bot-endpoint-single-message bot-sql)
+                         metabot-client/*create-embedding-endpoint*       simple-embedding-stub
+                         metabot-util/*prompt-templates*                  (constantly test-prompt-templates)]
+             (let [{:keys [inner_query] :as denormalized-model} (metabot-util/denormalize-model model)
+                   user_prompt   "Show me all of my data"
+                   context       {:model       denormalized-model
+                                  :user_prompt user_prompt
+                                  :prompt_task :infer_sql}
+                   result        (metabot/infer-sql context)
+                   generated-sql (get-in result [:card :dataset_query :native :query])]
+               (is (true? (str/includes? generated-sql inner_query)))
+               (is (true? (str/ends-with?
+                           (str/replace generated-sql #"\W+" "")
+                           (str/replace bot-sql #"\W+" ""))))))))))))
 
 (deftest infer-model-test
   (testing "Test the 'plumbing' of the infer-model function. The actual invocation of the remote bot is dynamically rebound."
-    (mt/with-temporary-setting-values [is-metabot-enabled true]
+    (mt/with-temp-env-var-value [mb-is-metabot-enabled true]
       (mt/dataset sample-dataset
         (t2.with-temp/with-temp
          [Card orders-model {:name    "Orders Model"
@@ -100,34 +100,34 @@
                                  :type     :query
                                  :query    {:source-table (mt/id :products)}}
                                 :dataset true}]
-          (let [user_prompt        "Show me all of my orders data"
-                db                 (t2/select-one Database :id (mt/id))
-                {:keys [models] :as denormalized-db} (metabot-util/denormalize-database db)
-                denormalized-model (first (filter (comp #{"Orders Model"} :name) models))
-                context            {:database    denormalized-db
-                                    :user_prompt user_prompt
-                                    :prompt_task :infer_model}
-                bot-message        (format "The best model is probably %s" (:id orders-model))]
-            (with-redefs [metabot-client/*create-chat-completion-endpoint* (test-bot-endpoint-single-message bot-message)
-                          ;; Both the prompt and the model pseudo-ddl containing the text "orders" have better
-                          ;; encoding matches than other model ddls.
-                          metabot-client/*create-embedding-endpoint*       (fn [{:keys [_model input]} _options]
-                                                                             (let [embeddings (if (str/includes?
-                                                                                                   (u/upper-case-en input)
-                                                                                                   "ORDERS")
-                                                                                                [1.0 0.0 0.0 0.0]
-                                                                                                [0.0 0.0 0.0 0.0])]
-                                                                               {:data  [{:embedding embeddings}]
-                                                                                :usage {:prompt_tokens 10}}))
-                          metabot-util/*prompt-templates*                  (constantly test-prompt-templates)]
-              (is
-               (partial=
-                denormalized-model
-                (metabot/match-best-model context))))))))))
+         (let [user_prompt        "Show me all of my orders data"
+               db                 (t2/select-one Database :id (mt/id))
+               {:keys [models] :as denormalized-db} (metabot-util/denormalize-database db)
+               denormalized-model (first (filter (comp #{"Orders Model"} :name) models))
+               context            {:database    denormalized-db
+                                   :user_prompt user_prompt
+                                   :prompt_task :infer_model}
+               bot-message        (format "The best model is probably %s" (:id orders-model))]
+           (with-redefs [metabot-client/*create-chat-completion-endpoint* (test-bot-endpoint-single-message bot-message)
+                         ;; Both the prompt and the model pseudo-ddl containing the text "orders" have better
+                         ;; encoding matches than other model ddls.
+                         metabot-client/*create-embedding-endpoint*       (fn [{:keys [_model input]} _options]
+                                                                            (let [embeddings (if (str/includes?
+                                                                                                  (u/upper-case-en input)
+                                                                                                  "ORDERS")
+                                                                                               [1.0 0.0 0.0 0.0]
+                                                                                               [0.0 0.0 0.0 0.0])]
+                                                                              {:data  [{:embedding embeddings}]
+                                                                               :usage {:prompt_tokens 10}}))
+                         metabot-util/*prompt-templates*                  (constantly test-prompt-templates)]
+             (is
+              (partial=
+               denormalized-model
+               (metabot/match-best-model context))))))))))
 
 (deftest infer-native-sql-test
   (testing "Test the 'plumbing' of the infer-native-sql function. The actual invocation of the remote bot is dynamically rebound."
-    (mt/with-temporary-setting-values [is-metabot-enabled true]
+    (mt/with-temp-env-var-value [mb-is-metabot-enabled true]
       (mt/dataset sample-dataset
         (let [user_prompt "Show me all of my reviews data"
               context     {:database    (metabot-util/denormalize-database (mt/db))
