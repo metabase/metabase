c88f7c77f9b474a5c28325b3a74d314f56cc6b10
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-26 19:51:35 +0700

    Make mock data reflect real data a bit more

--- frontend/src/metabase/visualizations/components/ScalarValue/utils.unit.spec.ts
+++ frontend/src/metabase/visualizations/components/ScalarValue/utils.unit.spec.ts
@@ -45,10 +45,10 @@ describe("findSize", () => {
 
   it("returns the first size with which text width is smaller than the target width", () => {
     measureTextSpy
-      .mockImplementationOnce(createMockMeasureText(120, 50))
-      .mockImplementationOnce(createMockMeasureText(110, 50))
+      .mockImplementationOnce(createMockMeasureText(120, 70))
+      .mockImplementationOnce(createMockMeasureText(110, 60))
       .mockImplementationOnce(createMockMeasureText(100, 50)) // this is the one we want
-      .mockImplementationOnce(createMockMeasureText(90, 50));
+      .mockImplementationOnce(createMockMeasureText(90, 40));
 
     const size = findSize({
       ...defaults,
@@ -94,10 +94,10 @@ describe("findSize", () => {
 
   it("returns the first size with which text height is smaller than the target height", () => {
     measureTextSpy
-      .mockImplementationOnce(createMockMeasureText(50, 120))
-      .mockImplementationOnce(createMockMeasureText(50, 110))
+      .mockImplementationOnce(createMockMeasureText(70, 120))
+      .mockImplementationOnce(createMockMeasureText(60, 110))
       .mockImplementationOnce(createMockMeasureText(50, 100)) // this is the one we want
-      .mockImplementationOnce(createMockMeasureText(50, 90));
+      .mockImplementationOnce(createMockMeasureText(40, 90));
 
     const size = findSize({
       ...defaults,
