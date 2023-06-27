8e0f69136a26b34ac95721b7676d9658aa46d636
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-06-27 19:29:48 -0300

    unused import

--- e2e/test/scenarios/dashboard/dashboard.cy.spec.js
+++ e2e/test/scenarios/dashboard/dashboard.cy.spec.js
@@ -16,7 +16,6 @@ import {
   openQuestionsSidebar,
   describeWithSnowplow,
   expectNoBadSnowplowEvents,
-  expectGoodSnowplowEvents,
   resetSnowplow,
   enableTracking,
   expectGoodSnowplowEvent,
