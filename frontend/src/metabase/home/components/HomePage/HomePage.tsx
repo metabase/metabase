d8b15dc2220d9dfcfb7733e2ccab7ca5f92a4253
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-28 19:53:08 +0300

    Prevent showing spinner when metabot is disabled

--- frontend/src/metabase/home/components/HomePage/HomePage.tsx
+++ frontend/src/metabase/home/components/HomePage/HomePage.tsx
@@ -12,10 +12,11 @@ import {
 } from "metabase/common/hooks";
 import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 import { canUseMetabotOnDatabase } from "metabase/metabot/utils";
-import { getSetting } from "metabase/selectors/settings";
-
+import { CollectionItem } from "metabase-types/api";
+import Database from "metabase-lib/metadata/Database";
 import {
   getCustomHomePageDashboardId,
+  getHasDismissedCustomHomePageToast,
   getIsMetabotEnabled,
 } from "../../selectors";
 import { HomeLayout } from "../HomeLayout";
@@ -24,7 +25,8 @@ import { HomeContent } from "../HomeContent";
 const SEARCH_QUERY = { models: "dataset", limit: 1 } as const;
 
 export const HomePage = (): JSX.Element => {
-  const { hasMetabot, isLoading, error } = useHasMetabot();
+  const { databases, models, isMetabotEnabled, isLoading, error } =
+    useMetabot();
   useNavbar();
   useDashboardPage();
 
@@ -33,7 +35,7 @@ export const HomePage = (): JSX.Element => {
   }
 
   return (
-    <HomeLayout hasMetabot={hasMetabot}>
+    <HomeLayout hasMetabot={getHasMetabot(databases, models, isMetabotEnabled)}>
       <HomeContent />
     </HomeLayout>
   );
@@ -49,38 +51,38 @@ const useNavbar = () => {
   }, [dispatch]);
 };
 
-const useHasMetabot = () => {
+const useMetabot = () => {
   const isMetabotEnabled = useSelector(getIsMetabotEnabled);
-  const {
-    data: databases = [],
-    isLoading: isDatabaseLoading,
-    error: databaseError,
-  } = useDatabaseListQuery({
+  const databaseListQuery = useDatabaseListQuery({
     enabled: isMetabotEnabled,
   });
-  const {
-    data: models = [],
-    isLoading: isModelLoading,
-    error: modelError,
-  } = useSearchListQuery({
+  const searchListQuery = useSearchListQuery({
     query: SEARCH_QUERY,
     enabled: isMetabotEnabled,
   });
 
+  return {
+    databases: databaseListQuery.data ?? [],
+    models: searchListQuery.data ?? [],
+    isMetabotEnabled,
+    isLoading: databaseListQuery.isLoading || searchListQuery.isLoading,
+    error: databaseListQuery.isLoading ?? searchListQuery.isLoading,
+  };
+};
+
+const getHasMetabot = (
+  databases: Database[],
+  models: CollectionItem[],
+  isMetabotEnabled: boolean,
+) => {
   const hasModels = models.length > 0;
   const hasSupportedDatabases = databases.some(canUseMetabotOnDatabase);
-  const hasMetabot = hasModels && hasSupportedDatabases && isMetabotEnabled;
-  const isLoading = isDatabaseLoading || isModelLoading;
-  const error = databaseError ?? modelError;
-
-  return { hasMetabot, isLoading, error };
+  return hasModels && hasSupportedDatabases && isMetabotEnabled;
 };
 
 const useDashboardPage = () => {
   const dashboardId = useSelector(getCustomHomePageDashboardId);
-  const hasDismissedToast = useSelector(state =>
-    getSetting(state, "dismissed-custom-dashboard-toast"),
-  );
+  const hasDismissedToast = useSelector(getHasDismissedCustomHomePageToast);
   const dispatch = useDispatch();
 
   useEffect(() => {
