d8abb5a5dd3498f71a649c04c164ea44b122f0b8
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-30 19:00:24 +0700

    Add a test case for removing columns (resetting to default value)

--- e2e/test/scenarios/visualizations/reproductions/30057-30058-map-crash.cy.spec.js
+++ e2e/test/scenarios/visualizations/reproductions/30057-30058-map-crash.cy.spec.js
@@ -39,7 +39,7 @@ describe("issue 30057", () => {
     cy.signInAsNormalUser();
   });
 
-  it("visualization is still displayed as a map after adding a filter (metabase#30057)", () => {
+  it("should still display visualization as a map after adding a filter (metabase#30057)", () => {
     visitQuestionAdhoc({ dataset_query: testQuery }, { mode: "notebook" });
 
     visualize();
@@ -50,7 +50,7 @@ describe("issue 30057", () => {
     cy.get(".PinMap").should("exist");
   });
 
-  it("visualization is still displayed as a map after adding another column to group by", () => {
+  it("should still display visualization as a map after adding another column to group by", () => {
     visitQuestionAdhoc({ dataset_query: testQuery }, { mode: "notebook" });
 
     visualize();
@@ -60,6 +60,17 @@ describe("issue 30057", () => {
     cy.findByTestId("TableInteractive-root").should("not.exist");
     cy.get(".PinMap").should("exist");
   });
+
+  it("should change display to default after removing a column to group by when map is not sensible anymore", () => {
+    visitQuestionAdhoc({ dataset_query: testQuery }, { mode: "notebook" });
+
+    visualize();
+    removeLatitudeColumn();
+    visualize();
+
+    cy.get(".PinMap").should("not.exist");
+    cy.get(".LineAreaBarChart").should("exist");
+  });
 });
 
 describe("issue 30058", () => {
@@ -68,7 +79,7 @@ describe("issue 30058", () => {
     cy.signInAsNormalUser();
   });
 
-  it("visualization does not crash after adding a filter (metabase#30058)", () => {
+  it("should not crash visualization after adding a filter (metabase#30058)", () => {
     visitQuestionAdhoc({
       dataset_query: testQuery,
       display: "map",
@@ -81,6 +92,14 @@ describe("issue 30058", () => {
     cy.get(".Icon-warning").should("not.exist");
   });
 });
+
+const removeLatitudeColumn = () => {
+  cy.icon("notebook").click();
+  cy.findByTestId("breakout-step")
+    .findByText("Latitude: Auto binned")
+    .icon("close")
+    .click();
+};
 
 const groupByBirthDateColumn = () => {
   cy.icon("notebook").click();
