80c42367b3930ad295de1fb88f73cc5a89d08960
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-06-30 19:42:53 +0300

    Fix between url params

--- e2e/test/scenarios/dashboard/reproductions/31662-between-filter-default-value.cy.spec.js
+++ e2e/test/scenarios/dashboard/reproductions/31662-between-filter-default-value.cy.spec.js
@@ -1,4 +1,4 @@
-import { restore } from "e2e/support/helpers";
+import { filterWidget, popover, restore } from "e2e/support/helpers";
 
 const parameterDetails = {
   name: "Between",
@@ -24,10 +24,15 @@ describe("issue 31662", () => {
   it("should allow setting default values for a not connected between filter (metabase#31662)", () => {
     cy.createDashboard(dashboardDetails).then(
       ({ body: { id: dashboardId } }) => {
-        cy.visit(`dashboard/${dashboardId}?between=3&between=5`);
+        cy.visit(`dashboard/${dashboardId}?between=10&between=20`);
         cy.wait("@dashboard");
       },
     );
     cy.findByTestId("dashboard-empty-state").should("be.visible");
+    filterWidget().findByText("2 selections").click();
+    popover().within(() => {
+      cy.findByDisplayValue("10").should("be.visible");
+      cy.findByDisplayValue("20").should("be.visible");
+    });
   });
 });
