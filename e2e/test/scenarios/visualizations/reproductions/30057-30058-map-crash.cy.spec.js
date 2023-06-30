83bf04551d293d2f1292d74ef274ff44a2a36ea0
Kamil Mielnik <kamil@kamilmielnik.com>
2023-06-30 18:36:53 +0700

    Add test for issue #30057 - use visualize() helper - extract addCountGreaterThan2Filter()

--- e2e/test/scenarios/visualizations/reproductions/30057-30058-map-crash.cy.spec.js
+++ e2e/test/scenarios/visualizations/reproductions/30057-30058-map-crash.cy.spec.js
@@ -1,4 +1,9 @@
-import { popover, restore, visitQuestionAdhoc } from "e2e/support/helpers";
+import {
+  popover,
+  restore,
+  visitQuestionAdhoc,
+  visualize,
+} from "e2e/support/helpers";
 
 import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
@@ -28,6 +33,23 @@ const testQuery = {
   database: SAMPLE_DB_ID,
 };
 
+describe("issue 30057", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("visualization is still displayed as a map after adding a filter (metabase#30057)", () => {
+    visitQuestionAdhoc({ dataset_query: testQuery }, { mode: "notebook" });
+
+    visualize();
+    addCountGreaterThan2Filter();
+    visualize();
+
+    cy.findByTestId("TableInteractive-root").should("not.exist");
+  });
+});
+
 describe("issue 30058", () => {
   beforeEach(() => {
     restore();
@@ -41,21 +63,23 @@ describe("issue 30058", () => {
       displayIsLocked: true,
     });
 
-    cy.icon("notebook").click();
-    cy.button("Filter").click();
-    popover().within(() => {
-      cy.findByText("Count").click();
-      cy.icon("chevrondown").click();
-    });
-    cy.findByTestId("operator-select-list").findByText("Greater than").click();
-    popover().within(() => {
-      cy.findByPlaceholderText("Enter a number").type("2");
-      cy.findByText("Add filter").click();
-    });
-
-    cy.button("Visualize").click();
-    cy.wait("@dataset");
+    addCountGreaterThan2Filter();
+    visualize();
 
     cy.get(".Icon-warning").should("not.exist");
   });
 });
+
+const addCountGreaterThan2Filter = () => {
+  cy.icon("notebook").click();
+  cy.button("Filter").click();
+  popover().within(() => {
+    cy.findByText("Count").click();
+    cy.icon("chevrondown").click();
+  });
+  cy.findByTestId("operator-select-list").findByText("Greater than").click();
+  popover().within(() => {
+    cy.findByPlaceholderText("Enter a number").type("2");
+    cy.findByText("Add filter").click();
+  });
+};
