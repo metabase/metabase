4086d26c8c39026f3c25690eeedce1d9a9472b58
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2023-07-03 10:35:26 +0200

    Generate conditional `describe` blocks

--- e2e/support/helpers/e2e-enterprise-helpers.js
+++ e2e/support/helpers/e2e-enterprise-helpers.js
@@ -1,6 +1,8 @@
 export const isEE = Cypress.env("HAS_ENTERPRISE_TOKEN");
 export const isOSS = !isEE;
 
-export const describeEE = isEE ? describe : describe.skip;
+const conditionalDescribe = cond => (cond ? describe : describe.skip);
 
-export const describeOSS = isOSS ? describe : describe.skip;
+export const describeEE = conditionalDescribe(isEE);
+
+export const describeOSS = conditionalDescribe(isOSS);
