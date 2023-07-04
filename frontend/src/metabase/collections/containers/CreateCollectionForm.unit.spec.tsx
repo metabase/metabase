c8c257279eed6f3c33d062e4b7dab548fd18bebc
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-04 17:21:11 +0300

    Fix tests

--- frontend/src/metabase/collections/containers/CreateCollectionForm.unit.spec.tsx
+++ frontend/src/metabase/collections/containers/CreateCollectionForm.unit.spec.tsx
@@ -1,16 +1,10 @@
 import userEvent from "@testing-library/user-event";
-import { TokenFeatures, User } from "metabase-types/api";
-import {
-  createMockCollection,
-  createMockTokenFeatures,
-  createMockUser,
-} from "metabase-types/api/mocks";
-import { createMockSettingsState } from "metabase-types/store/mocks";
+import { User } from "metabase-types/api";
+import { createMockCollection, createMockUser } from "metabase-types/api/mocks";
 import { setupEnterpriseTest } from "__support__/enterprise";
 import { setupCollectionsEndpoints } from "__support__/server-mocks";
 import { createMockEntitiesState } from "__support__/store";
-import { mockSettings } from "__support__/settings";
-import { renderWithProviders, screen, waitFor } from "__support__/ui";
+import { renderWithProviders, screen } from "__support__/ui";
 import CreateCollectionForm from "./CreateCollectionForm";
 
 const ROOT_COLLECTION = createMockCollection({
@@ -21,52 +15,38 @@ const ROOT_COLLECTION = createMockCollection({
 
 type SetupOpts = {
   user?: User;
-  tokenFeatures?: TokenFeatures;
+  hasToken?: boolean;
 };
 
-async function setup({
+function setup({
   user = createMockUser({ is_superuser: true }),
-  tokenFeatures = createMockTokenFeatures(),
+  hasToken = false,
 }: SetupOpts = {}) {
-  mockSettings({ "token-features": tokenFeatures });
-  setupEnterpriseTest();
   setupCollectionsEndpoints({
     collections: [],
     rootCollection: ROOT_COLLECTION,
   });
 
+  if (hasToken) {
+    setupEnterpriseTest();
+  }
+
   const onCancel = jest.fn();
   renderWithProviders(<CreateCollectionForm onCancel={onCancel} />, {
     storeInitialState: {
       currentUser: user,
-      settings: createMockSettingsState({
-        "token-features": tokenFeatures,
-      }),
       entities: createMockEntitiesState({
         collections: [ROOT_COLLECTION],
       }),
     },
   });
 
-  await waitFor(() => {
-    expect(screen.queryByTestId("loading-spinner")).not.toBeInTheDocument();
-  });
-
   return { onCancel };
 }
 
 describe("CreateCollectionForm", () => {
-  it("shows authority level controls when there is a valid token", async () => {
-    await setup({
-      tokenFeatures: createMockTokenFeatures({ content_management: true }),
-    });
-    expect(screen.getByText(/Collection type/i)).toBeInTheDocument();
-    expect(screen.getByText(/Regular/i)).toBeInTheDocument();
-    expect(screen.getByText(/Official/i)).toBeInTheDocument();
-  });
-
-  it("displays correct blank state", async () => {
-    await setup();
+  it("displays correct blank state", () => {
+    setup();
 
     expect(screen.getByLabelText("Name")).toBeInTheDocument();
     expect(screen.getByLabelText("Name")).toHaveValue("");
@@ -81,29 +61,34 @@ describe("CreateCollectionForm", () => {
     expect(screen.getByRole("button", { name: "Create" })).toBeInTheDocument();
   });
 
-  it("can't submit if name is empty", async () => {
-    await setup();
+  it("can't submit if name is empty", () => {
+    setup();
     expect(screen.getByRole("button", { name: "Create" })).toBeDisabled();
   });
 
-  it("calls onCancel when cancel button is clicked", async () => {
-    const { onCancel } = await setup();
+  it("calls onCancel when cancel button is clicked", () => {
+    const { onCancel } = setup();
     userEvent.click(screen.getByRole("button", { name: "Cancel" }));
-    await waitFor(() => {
-      expect(onCancel).toHaveBeenCalledTimes(1);
-    });
+    expect(onCancel).toHaveBeenCalledTimes(1);
   });
 
-  it("does not show authority level controls when there is no valid token", async () => {
-    await setup();
-    expect(screen.queryByLabelText(/Collection type/i)).not.toBeInTheDocument();
+  it("shows authority level controls when there is a valid token", () => {
+    setup({ hasToken: true });
+    expect(screen.getByText(/Collection type/i)).toBeInTheDocument();
+    expect(screen.getByText(/Regular/i)).toBeInTheDocument();
+    expect(screen.getByText(/Official/i)).toBeInTheDocument();
   });
 
-  it("does not show authority level controls when the user is not an admin", async () => {
-    await setup({
+  it("does not show authority level controls when the user is not an admin", () => {
+    setup({
       user: createMockUser({ is_superuser: false }),
-      tokenFeatures: createMockTokenFeatures({ content_management: true }),
+      hasToken: true,
     });
     expect(screen.queryByText(/Collection type/i)).not.toBeInTheDocument();
+  });
+
+  it("does not show authority level controls when there is no valid token", () => {
+    setup();
+    expect(screen.queryByLabelText(/Collection type/i)).not.toBeInTheDocument();
   });
 });
