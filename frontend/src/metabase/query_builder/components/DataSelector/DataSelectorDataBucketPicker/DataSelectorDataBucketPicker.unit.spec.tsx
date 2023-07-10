d992a10a2456b83b9ba99e0681c31396d855ab0a
Kamil Mielnik <kamil@kamilmielnik.com>
2023-07-10 19:31:54 +0700

    Extract utils unit tests

--- frontend/src/metabase/query_builder/components/DataSelector/DataSelectorDataBucketPicker/DataSelectorDataBucketPicker.unit.spec.tsx
+++ frontend/src/metabase/query_builder/components/DataSelector/DataSelectorDataBucketPicker/DataSelectorDataBucketPicker.unit.spec.tsx
@@ -1,79 +1,42 @@
 import { render, screen } from "@testing-library/react";
 
-import { getDataTypes } from "metabase/containers/DataPicker";
+import { DataTypeInfoItem, getDataTypes } from "metabase/containers/DataPicker";
 
 import DataSelectorDataBucketPicker from "./DataSelectorDataBucketPicker";
 
 describe("DataSelectorDataBucketPicker", () => {
-  describe("when nested queries are enabled", () => {
-    it("should display all non empty buckets - models & saved questions", () => {
-      render(
-        <DataSelectorDataBucketPicker
-          dataTypes={getDataTypes({
-            hasModels: true,
-            hasSavedQuestions: true,
-            hasNestedQueriesEnabled: true,
-          })}
-          onChangeDataBucket={jest.fn()}
-        />,
-      );
-
-      expect(screen.getByText("Models")).toBeInTheDocument();
-      expect(screen.getByText("Raw Data")).toBeInTheDocument();
-      expect(screen.getByText("Saved Questions")).toBeInTheDocument();
+  it("should display all buckets", () => {
+    const dataTypes = getDataTypes({
+      hasModels: true,
+      hasSavedQuestions: true,
+      hasNestedQueriesEnabled: true,
     });
 
-    it("should display all non empty buckets - saved questions", () => {
-      render(
-        <DataSelectorDataBucketPicker
-          dataTypes={getDataTypes({
-            hasModels: false,
-            hasSavedQuestions: true,
-            hasNestedQueriesEnabled: true,
-          })}
-          onChangeDataBucket={jest.fn()}
-        />,
-      );
+    const { container } = render(
+      <DataSelectorDataBucketPicker
+        dataTypes={dataTypes}
+        onChangeDataBucket={jest.fn()}
+      />,
+    );
 
-      expect(screen.queryByText("Models")).not.toBeInTheDocument();
-      expect(screen.getByText("Raw Data")).toBeInTheDocument();
-      expect(screen.getByText("Saved Questions")).toBeInTheDocument();
-    });
-
-    it("should display all non empty buckets - models", () => {
-      render(
-        <DataSelectorDataBucketPicker
-          dataTypes={getDataTypes({
-            hasModels: true,
-            hasSavedQuestions: false,
-            hasNestedQueriesEnabled: true,
-          })}
-          onChangeDataBucket={jest.fn()}
-        />,
-      );
-
-      expect(screen.getByText("Models")).toBeInTheDocument();
-      expect(screen.getByText("Raw Data")).toBeInTheDocument();
-      expect(screen.queryByText("Saved Questions")).not.toBeInTheDocument();
-    });
+    expect(screen.getByText("Models")).toBeInTheDocument();
+    expect(screen.getByText("Raw Data")).toBeInTheDocument();
+    expect(screen.getByText("Saved Questions")).toBeInTheDocument();
+    expect(container.childNodes[0].childNodes.length).toBe(dataTypes.length);
   });
 
-  describe("when nested queries are disabled", () => {
-    it("should not display models nor saved questions", () => {
-      render(
-        <DataSelectorDataBucketPicker
-          dataTypes={getDataTypes({
-            hasModels: true,
-            hasSavedQuestions: true,
-            hasNestedQueriesEnabled: false,
-          })}
-          onChangeDataBucket={jest.fn()}
-        />,
-      );
+  it("should display no buckets", () => {
+    const dataTypes: DataTypeInfoItem[] = [];
+    const { container } = render(
+      <DataSelectorDataBucketPicker
+        dataTypes={dataTypes}
+        onChangeDataBucket={jest.fn()}
+      />,
+    );
 
-      expect(screen.queryByText("Models")).not.toBeInTheDocument();
-      expect(screen.getByText("Raw Data")).toBeInTheDocument();
-      expect(screen.queryByText("Saved Questions")).not.toBeInTheDocument();
-    });
+    expect(screen.queryByText("Models")).not.toBeInTheDocument();
+    expect(screen.queryByText("Raw Data")).not.toBeInTheDocument();
+    expect(screen.queryByText("Saved Questions")).not.toBeInTheDocument();
+    expect(container.childNodes[0].childNodes.length).toBe(dataTypes.length);
   });
 });
