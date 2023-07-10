0ceef75f6540e37590382433a3db6e437d8db34e
Kamil Mielnik <kamil@kamilmielnik.com>
2023-07-10 15:55:47 +0700

    Extract constants out of getDataTypes

--- frontend/src/metabase/containers/DataPicker/utils.ts
+++ frontend/src/metabase/containers/DataPicker/utils.ts
@@ -3,6 +3,27 @@ import { t } from "ttag";
 import { DATA_BUCKET } from "./constants";
 import { DataTypeInfoItem } from "./types";
 
+const DATASETS: DataTypeInfoItem = {
+  id: DATA_BUCKET.DATASETS,
+  icon: "model",
+  name: t`Models`,
+  description: t`The best starting place for new questions.`,
+};
+
+const RAW_DATA: DataTypeInfoItem = {
+  id: DATA_BUCKET.RAW_DATA,
+  icon: "database",
+  name: t`Raw Data`,
+  description: t`Unaltered tables in connected databases.`,
+};
+
+const SAVED_QUESTIONS: DataTypeInfoItem = {
+  id: DATA_BUCKET.SAVED_QUESTIONS,
+  name: t`Saved Questions`,
+  icon: "folder",
+  description: t`Use any question’s results to start a new question.`,
+};
+
 export function getDataTypes({
   hasModels,
   hasNestedQueriesEnabled,
@@ -17,30 +38,15 @@ export function getDataTypes({
   const dataTypes: DataTypeInfoItem[] = [];
 
   if (hasNestedQueriesEnabled && hasModels) {
-    dataTypes.push({
-      id: DATA_BUCKET.DATASETS,
-      icon: "model",
-      name: t`Models`,
-      description: t`The best starting place for new questions.`,
-    });
+    dataTypes.push(DATASETS);
   }
 
   if (hasRawData) {
-    dataTypes.push({
-      id: DATA_BUCKET.RAW_DATA,
-      icon: "database",
-      name: t`Raw Data`,
-      description: t`Unaltered tables in connected databases.`,
-    });
+    dataTypes.push(RAW_DATA);
   }
 
   if (hasNestedQueriesEnabled && hasSavedQuestions) {
-    dataTypes.push({
-      id: DATA_BUCKET.SAVED_QUESTIONS,
-      name: t`Saved Questions`,
-      icon: "folder",
-      description: t`Use any question’s results to start a new question.`,
-    });
+    dataTypes.push(SAVED_QUESTIONS);
   }
 
   return dataTypes;
