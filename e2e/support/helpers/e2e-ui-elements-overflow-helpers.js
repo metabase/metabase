c4020826830146b7edb6f73656102ddbd6d3e20a
Denis Berezin <denis.berezin@metabase.com>
2023-07-11 14:22:40 +0300

    Update e2e helpers file name

--- e2e/support/helpers/e2e-ui-elements-overflow-helpers.js
+++ e2e/support/helpers/e2e-ui-elements-overflow-helpers.js
@@ -43,3 +43,18 @@ export const assertDescendantNotOverflowsContainer = (
     containerRect.right,
   );
 };
+
+export const assertIsEllipsified = element => {
+  expect(isEllipsified(element), "is ellipsified").to.equal(true);
+};
+
+export const assertIsNotEllipsified = element => {
+  expect(isEllipsified(element), "is ellipsified").to.equal(false);
+};
+
+export const isEllipsified = element => {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+};
