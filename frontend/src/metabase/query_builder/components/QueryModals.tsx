05ce892d33002d64d4faf29752239acd32e8422a
Oisin Coveney <oisin@metabase.com>
2023-07-13 15:48:41 +0300

    Make search bar expansion controlled by modal so keyboard shortcut can propagate

--- frontend/src/metabase/query_builder/components/QueryModals.tsx
+++ frontend/src/metabase/query_builder/components/QueryModals.tsx
@@ -232,7 +232,7 @@ class QueryModals extends Component<QueryModalsProps> {
         );
       case MODAL_TYPES.FILTERS:
         return (
-          <Modal fit onClose={onCloseModal} trapFocus={false}>
+          <Modal fit onClose={onCloseModal}>
             <BulkFilterModal
               question={question}
               onQueryChange={this.onQueryChange}
