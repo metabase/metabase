9b56c2f94b447c7dacae9bf465dfa69ac02c6a67
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-13 19:42:51 +0300

    Add basic test

--- frontend/src/metabase-types/api/mocks/settings.ts
+++ frontend/src/metabase-types/api/mocks/settings.ts
@@ -109,6 +109,7 @@ export const createMockTokenFeatures = (
   advanced_permissions: false,
   audit_app: false,
   content_management: false,
+  content_verification: false,
   embedding: false,
   hosting: false,
   official_collections: false,
--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -142,6 +142,7 @@ export interface TokenFeatures {
   advanced_permissions: boolean;
   audit_app: boolean;
   content_management: boolean;
+  content_verification: boolean;
   embedding: boolean;
   hosting: boolean;
   official_collections: boolean;
