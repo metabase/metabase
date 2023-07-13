ce9a89332a423abd9b842ca8a7526a286420f01d
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-13 19:33:08 +0300

    Add basic test

--- frontend/src/metabase/query_builder/components/view/sidebars/QuestionInfoSidebar/tests/setup.tsx
+++ frontend/src/metabase/query_builder/components/view/sidebars/QuestionInfoSidebar/tests/setup.tsx
@@ -9,6 +9,7 @@ import {
 } from "metabase-types/api/mocks";
 import { createSampleDatabase } from "metabase-types/api/mocks/presets";
 import { createMockState } from "metabase-types/store/mocks";
+import { setupEnterprisePlugins } from "__support__/enterprise";
 import { mockSettings } from "__support__/settings";
 import { createMockEntitiesState } from "__support__/store";
 import {
@@ -18,19 +19,21 @@ import {
 } from "__support__/ui";
 import {
   setupCardEndpoints,
+  setupRevisionsEndpoints,
   setupUsersEndpoints,
 } from "__support__/server-mocks";
-import { setupRevisionsEndpoints } from "__support__/server-mocks/revision";
 import { QuestionInfoSidebar } from "../QuestionInfoSidebar";
 
-interface SetupOpts {
+export interface SetupOpts {
   card?: Card;
   settings?: Settings;
+  hasEnterprisePlugins?: boolean;
 }
 
 export const setup = async ({
   card = createMockCard(),
   settings = createMockSettings(),
+  hasEnterprisePlugins,
 }: SetupOpts) => {
   const currentUser = createMockUser();
   setupCardEndpoints(card);
@@ -48,6 +51,10 @@ export const setup = async ({
   const metadata = getMetadata(state);
   const question = checkNotNull(metadata.question(card.id));
   const onSave = jest.fn();
+
+  if (hasEnterprisePlugins) {
+    setupEnterprisePlugins();
+  }
 
   const TestQuestionInfoSidebar = () => (
     <QuestionInfoSidebar question={question} onSave={onSave} />
