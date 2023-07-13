6989cb89a128eec334c37fc6fa53d031dbc4aec6
Jerry Huang <jhuang37050@gmail.com>
2023-07-13 02:20:04 -0400

    update test

--- src/metabase/api/user.clj
+++ src/metabase/api/user.clj
@@ -250,10 +250,12 @@
   [user]
   (let [coll-ids-filter (collection/visible-collection-ids->honeysql-filter-clause
                           :collection_id
-                          (collection/permissions-set->visible-collection-ids @api/*current-user-permissions-set*))]
-    (assoc user :has_question_and_dashboard (and coll-ids-filter
-                                                 (t2/exists? 'Card :creator_id (:id user) :archived false)
-                                                 (t2/exists? 'Dashboard :creator_id (:id user) :archived false)))))
+                          (collection/permissions-set->visible-collection-ids @api/*current-user-permissions-set*))
+        perms-query {:where [:and
+                             [:= :archived false]
+                             coll-ids-filter]}]
+    (assoc user :has_question_and_dashboard (and (t2/exists? 'Card perms-query)
+                                                 (t2/exists? 'Dashboard perms-query)))))
 
 (defn- add-first-login
   "Adds `first_login` key to the `User` with the oldest timestamp from that user's login history. Otherwise give the current time, as it's the user's first login."
