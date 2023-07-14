ea8e8730219442a79fe4aff01fdf6ddfc9f97054
Callum Herries <hi@callumherries.com>
2023-07-14 13:31:47 +0300

    Revert "Remove unused content_management feature from the FE"
    This reverts commit d88ddcbf6c5bc6cc9ee65527e006a58cd3df8335.

--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -142,6 +142,7 @@ export interface TokenFeatures {
   advanced_permissions: boolean;
   audit_app: boolean;
   cache_granular_controls: boolean;
+  content_management: boolean;
   content_verification: boolean;
   embedding: boolean;
   hosting: boolean;
