f20ecd10fd15fe2620dc58fcb1bf1698e56e38d1
Cal Herries <39073188+calherries@users.noreply.github.com>
2023-07-14 13:50:59 +0300

    Use some more granular feature flags for premium features (#32330)

--- enterprise/backend/src/metabase_enterprise/advanced_config/caching.clj
+++ enterprise/backend/src/metabase_enterprise/advanced_config/caching.clj
@@ -5,7 +5,7 @@
 (defenterprise granular-ttl
   "Returns the granular cache ttl (in seconds) for a card. On EE, this first checking whether there is a stored value
    for the card, dashboard, or database (in that order of decreasing preference). Returns nil on OSS."
-  :feature :advanced-config
+  :feature :cache-granular-controls
   [card dashboard database]
   (let [ttls              [(:cache_ttl card) (:cache_ttl dashboard) (:cache_ttl database)]
         most-granular-ttl (first (filter some? ttls))]
