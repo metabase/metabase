b16edd321590725111c40f9c8777033a66468986
Cam Saul <1455846+camsaul@users.noreply.github.com>
2023-07-13 23:33:05 -0700

    Potentially fix StackOverflow in flaky tests (#32375)

--- src/metabase/models/user.clj
+++ src/metabase/models/user.clj
@@ -91,17 +91,20 @@
 (t2/define-after-insert :model/User
   [{user-id :id, superuser? :is_superuser, :as user}]
   (u/prog1 user
-    ;; add the newly created user to the magic perms groups
-    (binding [perms-group-membership/*allow-changing-all-users-group-members* true]
-      (log/info (trs "Adding User {0} to All Users permissions group..." user-id))
-      (t2/insert! PermissionsGroupMembership
-        :user_id  user-id
-        :group_id (:id (perms-group/all-users))))
+    ;; add the newly created user to the magic perms groups.
+    (log/info (trs "Adding User {0} to All Users permissions group..." user-id))
     (when superuser?
-      (log/info (trs "Adding User {0} to Admin permissions group..." user-id))
-      (t2/insert! PermissionsGroupMembership
-        :user_id  user-id
-        :group_id (:id (perms-group/admin))))))
+      (log/info (trs "Adding User {0} to All Users permissions group..." user-id)))
+    (let [groups (filter some? [(perms-group/all-users)
+                                (when superuser? (perms-group/admin))])]
+      (binding [perms-group-membership/*allow-changing-all-users-group-members* true]
+        ;; do a 'simple' insert against the Table name so we don't trigger the after-insert behavior
+        ;; for [[metabase.models.permissions-group-membership]]... we don't want it recursively trying to update
+        ;; the user
+        (t2/insert! (t2/table-name :model/PermissionsGroupMembership)
+                    (for [group groups]
+                      {:user_id  user-id
+                       :group_id (u/the-id group)}))))))
 
 (t2/define-before-update :model/User
   [{:keys [id] :as user}]
