81b571661eba092b7e3a4542913be8a1586b0db9
Cam Saul <1455846+camsaul@users.noreply.github.com>
2023-07-14 11:15:11 -0700

    Fix infinite loop when logging error when fetching `site-locale`  (#32378)
    * Fix infinite loop when logging error when fetching `:site-locale` Setting
    
    * Fix bad performance change
    
    * Remove more dumb optimizations
    
    * Fix references to removed var

--- src/metabase/models/setting.clj
+++ src/metabase/models/setting.clj
@@ -492,18 +492,19 @@
         db-is-set-up? (or (requiring-resolve 'metabase.db/db-is-set-up?)
                           ;; this should never be hit. it is just overly cautious against a NPE here. But no way this
                           ;; cannot resolve
-                          (constantly false))]
+                          (constantly false))
+        db-value      #(t2/select-one-fn :value Setting :key (setting-name setting-definition-or-name))]
     ;; cannot use db (and cache populated from db) if db is not set up
     (when (and (db-is-set-up?) (allows-site-wide-values? setting))
       (let [v (if *disable-cache*
-                (t2/select-one-fn :value Setting :key (setting-name setting-definition-or-name))
+                (db-value)
                 (do
                   (setting.cache/restore-cache-if-needed!)
                   (let [cache (setting.cache/cache)]
                     (if (nil? cache)
                       ;; If another thread is populating the cache for the first time, we will have a nil value for
                       ;; the cache and must hit the db while the cache populates
-                      (t2/select-one-fn :value Setting :key (setting-name setting-definition-or-name))
+                      (db-value)
                       (clojure.core/get cache (setting-name setting-definition-or-name))))))]
         (not-empty v)))))
 
