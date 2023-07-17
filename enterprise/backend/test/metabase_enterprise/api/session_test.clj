cc5ce3ed4cd31309b2dd4b10796210420858c8c4
Ngoc Khuat <qn.khuat@gmail.com>
2023-07-17 16:51:41 +0700

    expose new granular feature flags

--- enterprise/backend/test/metabase_enterprise/api/session_test.clj
+++ enterprise/backend/test/metabase_enterprise/api/session_test.clj
@@ -8,34 +8,50 @@
 (use-fixtures :once (fixtures/initialize :db))
 
 (deftest properties-token-features-test
-  (premium-features-test/with-premium-features #{:audit-app
-                                                 :advanced-permissions
-                                                 :cache-granular-controls
-                                                 :config-text-file
-                                                 :content-management
-                                                 :content-verification
-                                                 :embedding
-                                                 :whitelabel
-                                                 :advanced-config
-                                                 :sso
-                                                 :hosting
-                                                 :sandboxes
-                                                 :snippet-collections
+  (premium-features-test/with-premium-features #{:dashboard-subscription-filters
+                                                 :question-error-logs
                                                  :disable-password-login
-                                                 :official-collections}
-    (is (= {:advanced_config         true
-            :advanced_permissions    true
-            :audit_app               true
-            :cache_granular_controls true
-            :config_text_file        true
-            :content_management      true
-            :content_verification    true
-            :disable_password_login  true
-            :embedding               true
-            :hosting                 true
-            :official_collections    true
-            :sandboxes               true
-            :snippet_collections     true
-            :sso                     true
-            :whitelabel              true}
-          (:token-features (mt/user-http-request :crowberto :get 200 "session/properties"))))))
+                                                 :audit-app
+                                                 :snippet-collections
+                                                 :advanced-permissions
+                                                 :embedding
+                                                 :official-collections
+                                                 :whitelabel
+                                                 :no-upsell
+                                                 :advanced-config
+                                                 :cache-granular-controls
+                                                 :content-verification
+                                                 :serialization
+                                                 :content-management
+                                                 :sso-saml
+                                                 :config-text-file
+                                                 :email-allow-list
+                                                 :hosting
+                                                 :sso
+                                                 :sso-jwt
+                                                 :sandboxes
+                                                 :email-restrict-recipients
+                                                 :sso-ldap
+                                                 :session-timeout-config}
+          (is (= {:advanced_config                true
+                  :advanced_permissions           true
+                  :audit_app                      true
+                  :cache_granular_controls        true
+                  :config_text_file               true
+                  :content_management             true
+                  :content_verification           true
+                  :dashboard_subscription_filters true
+                  :disable_password_login         true
+                  :email_allow_list               true
+                  :email_restrict_recipients      true
+                  :embedding                      true
+                  :hosting                        true
+                  :official_collections           true
+                  :sandboxes                      true
+                  :snippet_collections            true
+                  :sso                            true
+                  :sso_jwt                        true
+                  :sso_saml                       true
+                  :sso_ldap                       true
+                  :whitelabel                     true}
+                 (:token-features (mt/user-http-request :crowberto :get 200 "session/properties"))))))
