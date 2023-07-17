5e4ef215461dfe2d2e9d3ec8c01ab5aba475e9f6
Emmad Usmani <emmadusmani@berkeley.edu>
2023-07-17 12:06:48 -0700

    update unit tests to remove uncessary 'await's

--- frontend/src/metabase/admin/settings/app/components/SettingsEditor/tests/common.unit.spec.tsx
+++ frontend/src/metabase/admin/settings/app/components/SettingsEditor/tests/common.unit.spec.tsx
@@ -15,21 +15,21 @@ describe("SettingsEditor", () => {
         settingValues: createMockSettings({ "enable-embedding": true }),
       });
 
-      userEvent.click(await screen.findByText("Embedding"));
+      userEvent.click(screen.getByText("Embedding"));
       userEvent.click(screen.getByText("Full-app embedding"));
       expect(screen.getByText(/some of our paid plans/)).toBeInTheDocument();
       expect(screen.queryByText("Authorized origins")).not.toBeInTheDocument();
     });
 
     it("should redirect from the full-app embedding page if embedding is not enabled", async () => {
-      setup({
+      await setup({
         settings: [createMockSettingDefinition({ key: "enable-embedding" })],
         settingValues: createMockSettings({ "enable-embedding": false }),
         initialRoute: FULL_APP_EMBEDDING_URL,
       });
 
       expect(
-        await screen.findByText(/Embed dashboards, questions/),
+        screen.getByText(/Embed dashboards, questions/),
       ).toBeInTheDocument();
       expect(screen.queryByText("Full-app embedding")).not.toBeInTheDocument();
     });
