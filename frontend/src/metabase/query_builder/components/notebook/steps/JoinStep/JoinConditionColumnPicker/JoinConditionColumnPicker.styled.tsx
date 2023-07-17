3b7570a5679ec1edcdfd8c56587ec3c27cb3c64c
Anton Kulyk <kuliks.anton@gmail.com>
2023-07-17 16:45:19 +0100

    Use `cursor: pointer` for join column picker

--- frontend/src/metabase/query_builder/components/notebook/steps/JoinStep/JoinConditionColumnPicker/JoinConditionColumnPicker.styled.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/JoinStep/JoinConditionColumnPicker/JoinConditionColumnPicker.styled.tsx
@@ -2,6 +2,13 @@ import styled from "@emotion/styled";
 import { Icon } from "metabase/core/components/Icon";
 import QueryColumnPicker from "metabase/common/components/QueryColumnPicker/QueryColumnPicker";
 import { color } from "metabase/lib/colors";
+import { NotebookCellItem } from "../../../NotebookCell";
+
+export const JoinConditionCellItem = styled(NotebookCellItem)<{
+  readOnly?: boolean;
+}>`
+  cursor: ${props => (props.readOnly ? "default" : "pointer")};
+`;
 
 export const StyledQueryColumnPicker = styled(QueryColumnPicker)`
   color: ${color("brand")};
