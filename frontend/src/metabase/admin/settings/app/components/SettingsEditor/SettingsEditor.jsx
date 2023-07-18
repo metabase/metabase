fb56f9ed61e4c29d557d47de8383c0da9d787236
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-18 18:29:34 +0300

    Migrate VersionUpdateNotice

--- frontend/src/metabase/admin/settings/app/components/SettingsEditor/SettingsEditor.jsx
+++ frontend/src/metabase/admin/settings/app/components/SettingsEditor/SettingsEditor.jsx
@@ -32,6 +32,7 @@ import {
   updateSetting,
   reloadSettings,
 } from "../../../settings";
+import { NewVersionIndicator } from "./SettingsEditor.styled";
 
 const mapStateToProps = (state, props) => {
   return {
@@ -232,12 +233,12 @@ class SettingsEditor extends Component {
           !MetabaseSettings.isHosted();
 
         const newVersionIndicator = shouldDisplayNewVersionIndicator ? (
-          <span
+          <NewVersionIndicator
             style={{ padding: "4px 8px 4px 8px" }}
             className="bg-brand rounded text-white text-bold h6"
           >
             1
-          </span>
+          </NewVersionIndicator>
         ) : null;
 
         return (
