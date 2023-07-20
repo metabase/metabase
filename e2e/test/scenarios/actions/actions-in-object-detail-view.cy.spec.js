1e231228318893f2f285fb5c8f642da6a5b97a3d
Kamil Mielnik <kamil@kamilmielnik.com>
2023-07-20 18:07:45 +0700

    Add second step and extract helpers

--- e2e/test/scenarios/actions/actions-in-object-detail-view.cy.spec.js
+++ e2e/test/scenarios/actions/actions-in-object-detail-view.cy.spec.js
@@ -26,18 +26,75 @@ describe("Model actions in object detail view", () => {
   });
 
   it("scenario", () => {
-    cy.signInAsNormalUser();
-
-    cy.get("@modelId").then(modelId => {
-      cy.visit(`/model/${modelId}/detail`);
-      cy.log("actions tab should not be shown in model page");
-      cy.findByText("Actions").should("not.exist");
-
-      cy.visit(`/model/${modelId}/detail/1`);
-      cy.log("actions dropdown should not be shown in object details modal");
-      cy.findByTestId("actions-menu").should("not.exist");
+    /* Step 1: as a normal user, verify that actions are not visible */
+    asNormalUser(() => {
+      cy.get("@modelId").then(modelId => {
+        assertActionsTabNotExists(modelId);
+        assertActionsDropdownNotExists(modelId);
+      });
     });
 
-    cy.signOut();
+    /* Step 2: as an admin, verify actions are not visible and then enable actions in the model */
+    asAdmin(() => {
+      cy.get("@modelId").then(modelId => {
+        assertActionsTabNotExists(modelId);
+        assertActionsDropdownNotExists(modelId);
+
+        cy.visit(`/admin/databases/${SAMPLE_DB_ID}`);
+        const actionsToggle = cy.findByLabelText("Model actions");
+        cy.log("actions should be disabled in model page");
+        actionsToggle.should("not.be.checked");
+
+        actionsToggle.click();
+        cy.log("actions should be enabled in model detail page");
+        actionsToggle.should("be.checked");
+
+        assertActionsTabExists(modelId);
+        assertActionsDropdownExists(modelId);
+      });
+    });
+
+    asNormalUser(() => {
+      cy.get("@modelId").then(modelId => {
+        assertActionsTabExists(modelId);
+        assertActionsDropdownExists(modelId);
+      });
+    });
   });
 });
+
+function asAdmin(callback) {
+  cy.signInAsAdmin();
+  callback();
+  cy.signOut();
+}
+
+function asNormalUser(callback) {
+  cy.signInAsNormalUser();
+  callback();
+  cy.signOut();
+}
+
+function assertActionsDropdownExists(modelId) {
+  cy.visit(`/model/${modelId}/detail/1`);
+  cy.log("actions dropdown should be shown in object details modal");
+  cy.findByTestId("actions-menu").should("exist");
+}
+
+function assertActionsDropdownNotExists(modelId) {
+  cy.visit(`/model/${modelId}/detail/1`);
+  cy.log("actions dropdown should not be shown in object details modal");
+  cy.findByTestId("actions-menu").should("not.exist");
+}
+
+function assertActionsTabExists(modelId) {
+  cy.visit(`/model/${modelId}/detail`);
+  cy.log("actions tab should be shown in model detail page");
+  cy.findByText("Actions").should("exist");
+}
+
+function assertActionsTabNotExists(modelId) {
+  cy.visit(`/model/${modelId}/detail`);
+  cy.log("actions tab should be shown in model detail page");
+  cy.findByText("Actions").should("not.exist");
+}
