fc911c04b6e975562930109ca1385e8f62f81b6b
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-20 14:06:14 +0300

    Migrate QuestionEmbedWidget

---
+++ frontend/src/metabase/query_builder/components/QuestionEmbedWidget/QuestionEmbedWidget.styled.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+import { color } from "metabase/lib/colors";
+import { Icon } from "metabase/core/components/Icon";
+import { breakpointMinSmall } from "metabase/styled-components/theme";
+
+export const TriggerIcon = styled(Icon)`
+  display: none;
+  margin: 0 0.5rem;
+  cursor: pointer;
+
+  &:hover {
+    color: ${color("brand")};
+  }
+
+  ${breakpointMinSmall} {
+    display: inherit;
+  }
+`;
