0cea2749014e4ede7fdfbce9973f8a3e57716ed0
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-20 13:44:31 +0300

    Fix passing invalid props to Icon and Link

--- frontend/src/metabase/visualizations/components/LegendHeader.styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import LegendItem from "metabase/visualizations/components/LegendItem";
-
-interface LegendHeaderItemProps {
-  isBreakoutSeries?: boolean;
-}
-
-export const LegendHeaderItem = styled(LegendItem)<LegendHeaderItemProps>``;
