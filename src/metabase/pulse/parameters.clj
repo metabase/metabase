90c11f44160c9e51b3a5899c58da37d7810f14ef
Jerry Huang <jhuang37050@gmail.com>
2023-07-20 23:12:07 -0400

    heading cards should not escape characters

--- src/metabase/pulse/parameters.clj
+++ src/metabase/pulse/parameters.clj
@@ -59,4 +59,6 @@
                                        (assoc m tag-name (get param-id->param param-id))))
                                    {}
                                    tag-names)]
-    (update-in dashcard [:visualization_settings :text] shared.params/substitute_tags tag->param (public-settings/site-locale) true)))
+    (update-in dashcard [:visualization_settings :text] shared.params/substitute_tags tag->param (public-settings/site-locale)
+               ; heading cards should not escape characters
+               (not= "heading" (get-in dashcard [:visualization_settings :virtual_card :display])))))
