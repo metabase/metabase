6d73e0925491e651e2127f771ccab350fec42aea
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-07-21 18:33:06 -0300

    revert flag name

--- enterprise/frontend/src/metabase-enterprise/tools/index.js
+++ enterprise/frontend/src/metabase-enterprise/tools/index.js
@@ -8,7 +8,7 @@ import { hasPremiumFeature } from "metabase-enterprise/settings";
 import ErrorOverview from "./containers/ErrorOverview";
 import ErrorDetail from "./containers/ErrorDetail";
 
-if (hasPremiumFeature("question_error_logs")) {
+if (hasPremiumFeature("audit_app")) {
   PLUGIN_ADMIN_TOOLS.INDEX_ROUTE = "errors";
 
   PLUGIN_ADMIN_TOOLS.EXTRA_ROUTES_INFO = [
