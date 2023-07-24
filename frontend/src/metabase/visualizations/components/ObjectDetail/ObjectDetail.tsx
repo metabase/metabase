f260e6b5186191b488dbcb5e08aafc1419000cd2
Kamil Mielnik <kamil@kamilmielnik.com>
2023-07-24 16:39:22 +0700

    Revert "Navigate to sibling row or close the modal on deletion"
    This reverts commit 8b24685eee5cd968f53cc14b7194a66588fb5845.

--- frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetail.tsx
+++ frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetail.tsx
@@ -367,14 +367,6 @@ export function ObjectDetailView({
 
   const handleDeleteSuccess = () => {
     handleActionSuccess();
-
-    if (canZoomNextRow) {
-      viewNextObjectDetail();
-    } else if (canZoomPreviousRow) {
-      viewPreviousObjectDetail();
-    } else {
-      closeObjectDetail();
-    }
   };
 
   if (!data) {
