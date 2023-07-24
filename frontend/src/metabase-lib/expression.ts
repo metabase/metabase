da839777e34e558858759f7e40270da7ee99013b
Anton Kulyk <kuliks.anton@gmail.com>
2023-07-24 15:04:54 +0100

    Add TypeScript wrappers for expression methods

---
+++ frontend/src/metabase-lib/expression.ts
@@ -0,0 +1,25 @@
+import * as ML from "cljs/metabase.lib.js";
+import type { ColumnMetadata, ExpressionClause, Query } from "./types";
+
+export function expression(
+  query: Query,
+  stageIndex: number,
+  clause: ExpressionClause,
+): Query {
+  return ML.expression(query, stageIndex, clause);
+}
+
+export function expressions(
+  query: Query,
+  stageIndex: number,
+): ExpressionClause[] {
+  return ML.expressions(query, stageIndex);
+}
+
+export function expressionableColumns(
+  query: Query,
+  stageIndex: number,
+  expressionPosition: number,
+): ColumnMetadata[] {
+  return ML.expressionable_columns(query, stageIndex, expressionPosition);
+}
