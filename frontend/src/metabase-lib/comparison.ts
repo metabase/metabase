1b2de20da2521a78f76484a96c5efc2758dffcc8
Anton Kulyk <kuliks.anton@gmail.com>
2023-07-25 16:41:31 +0100

    Fix empty column list when need to fetch metadata

--- frontend/src/metabase-lib/comparison.ts
+++ frontend/src/metabase-lib/comparison.ts
@@ -1,5 +1,7 @@
 import * as ML from "cljs/metabase.lib.js";
 import type { DatasetQuery } from "metabase-types/api";
+import type { ColumnMetadata, Query } from "./types";
+import { displayInfo } from "./metadata";
 
 export function areLegacyQueriesEqual(
   query1: DatasetQuery,
@@ -7,4 +9,15 @@ export function areLegacyQueriesEqual(
   fieldIds?: number[],
 ): boolean {
   return ML.query_EQ_(query1, query2, fieldIds);
+}
+
+export function isSameColumn(
+  query: Query,
+  stageIndex: number,
+  column1: ColumnMetadata,
+  column2: ColumnMetadata,
+): boolean {
+  const info1 = displayInfo(query, stageIndex, column1);
+  const info2 = displayInfo(query, stageIndex, column2);
+  return info1.name === info2.name && info1?.table?.name === info2?.table?.name;
 }
