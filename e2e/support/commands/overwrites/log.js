885002251397ce414d27098e27da1b10cab68e49
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2023-07-25 11:52:04 +0200

    Customize and style Cypress `log` command
    This PR will add a background color to the Cypress log command, in order to make it stand out. This should help with reviewing Cypress recorded runs and with the general E2E tests debugging experience.

--- e2e/support/commands/overwrites/log.js
+++ e2e/support/commands/overwrites/log.js
@@ -1,10 +1,12 @@
-Cypress.Commands.overwrite("log", (originalFn, message) => {
-  Cypress.log({
-    displayName: `--- ${window.logCalls}. ${message} ---`,
-    name: `--- ${window.logCalls}. ${message} ---`,
+Cypress.Commands.overwrite("log", (originalFn, text) => {
+  const logConfig = {
+    displayName: `=== ${window.logCalls}. ${text}`,
+    name: "log",
     message: "",
-  });
+  };
 
+  appendStyleIfItDoesntExists(logConfig);
+  Cypress.log(logConfig);
   window.logCalls++;
 });
 
@@ -12,3 +14,27 @@ Cypress.Commands.overwrite("log", (originalFn, message) => {
 beforeEach(() => {
   window.logCalls = 1;
 });
+
+function appendStyleIfItDoesntExists(logConfig) {
+  const headHTML = Cypress.$(window.top.document.head);
+  const allStyles = Array.from(Cypress.$(window.top.document.styleSheets));
+
+  const getCustomStyle = allStyles.filter(s => s.ownerNode.dataset.customLog);
+  const customStyleExists = getCustomStyle.length > 0;
+
+  if (!customStyleExists) {
+    const style = document.createElement("style");
+
+    style.textContent = `
+      .command-name-${logConfig.name} .command-pin-target{
+        color: #ffffff !important;
+        background-color: darkorchid !important;;
+        font-weight: bold !important;
+      }
+    `;
+    style.type = "text/css";
+    style.dataset.customLog = true;
+
+    headHTML.append(style);
+  }
+}
