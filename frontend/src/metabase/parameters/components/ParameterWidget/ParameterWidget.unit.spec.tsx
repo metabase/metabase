1a4e9c7e8c6e2e469fd6a32e05b7d4af837a7aa5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-07-26 15:06:41 +0700

    Add additional test for parameters with connected field

--- frontend/src/metabase/parameters/components/ParameterWidget/ParameterWidget.unit.spec.tsx
+++ frontend/src/metabase/parameters/components/ParameterWidget/ParameterWidget.unit.spec.tsx
@@ -1,17 +1,26 @@
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { createMockParameter } from "metabase-types/api/mocks";
+import { createMockField, createMockParameter } from "metabase-types/api/mocks";
 import { renderWithProviders } from "__support__/ui";
+import type { FieldFilterUiParameter } from "metabase-lib/parameters/types";
+import Field from "metabase-lib/metadata/Field";
 
 import { ParameterWidget } from "./ParameterWidget";
 
-function setup() {
-  const parameter = createMockParameter({
-    id: "1fe8ce3",
-    type: "text/contains",
-    slug: "text_contains",
-    name: "Text contains",
-  });
+interface SetupOpts {
+  connectedField?: Field;
+}
+
+function setup({ connectedField }: SetupOpts = {}) {
+  const parameter: FieldFilterUiParameter = {
+    ...createMockParameter({
+      id: "1fe8ce3",
+      type: "string/contains",
+      slug: "text_contains",
+      name: "Text contains",
+    }),
+    fields: connectedField ? [connectedField] : [],
+  };
 
   const setValue = jest.fn();
 
@@ -40,6 +49,31 @@ describe("ParameterWidget", () => {
 
   it("should not be able to submit empty value (metabase#15462)", () => {
     const { setValue } = setup();
+
+    userEvent.click(screen.getByText("Text contains"));
+
+    const text = "Gadget";
+    const textInput = screen.getByPlaceholderText("Enter some text");
+    userEvent.type(textInput, text);
+    expect(screen.getByRole("button", { name: "Add filter" })).toBeEnabled();
+
+    userEvent.type(textInput, "{backspace}".repeat(text.length));
+    expect(screen.getByRole("button", { name: "Add filter" })).toBeDisabled();
+
+    userEvent.click(screen.getByRole("button", { name: "Add filter" }));
+    expect(setValue).not.toHaveBeenCalled();
+  });
+
+  it("should not be able to submit empty value when parameter has connected field (metabase#15462)", () => {
+    const field = new Field(
+      createMockField({
+        id: 1,
+        base_type: "type/Text",
+        effective_type: "type/Text",
+        semantic_type: "type/Category",
+      }),
+    );
+    const { setValue } = setup({ connectedField: field });
 
     userEvent.click(screen.getByText("Text contains"));
 
