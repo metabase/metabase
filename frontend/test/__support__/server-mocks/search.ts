3b9e4c655d3a6a151e9d30076fbc4986e40409d0
Oisin Coveney <oisin@metabase.com>
2023-07-27 16:47:59 +0100

    Simplify code

--- frontend/test/__support__/server-mocks/search.ts
+++ frontend/test/__support__/server-mocks/search.ts
@@ -4,29 +4,24 @@ import type { CollectionItem, SearchResult } from "metabase-types/api";
 export function setupSearchEndpoints(items: (CollectionItem | SearchResult)[]) {
   fetchMock.get("path:/api/search", uri => {
     const url = new URL(uri);
-    const models = [...new Set(url.searchParams.getAll("models"))];
+    const models = url.searchParams.getAll("models");
     const limit = Number(url.searchParams.get("limit"));
     const offset = Number(url.searchParams.get("offset"));
     const table_db_id = url.searchParams.get("table_db_id") || null;
-    const queryText = url.searchParams.get("q");
+    const queryText = url.searchParams.get("q")?.toLowerCase();
 
-    let matchedItems = items;
-    if (queryText) {
-      const searchString = queryText.toLowerCase();
+    let matchedItems = items.filter(
+      ({ name }) =>
+        !queryText || name.toLowerCase().includes(queryText.toLowerCase()),
+    );
 
-      matchedItems = matchedItems.filter(({ name }) =>
-        name.toLowerCase().includes(searchString),
-      );
-    }
-
-    // available_models: all possible types in a user's database (or in this case, the test data), AFTER the text query filter
     const availableModels = [
       ...new Set(matchedItems.map(({ model }) => model)),
     ];
 
-    if (models && models.length > 0) {
-      matchedItems = matchedItems.filter(({ model }) => models.includes(model));
-    }
+    matchedItems = matchedItems.filter(
+      ({ model }) => !models.length || models.includes(model),
+    );
 
     return {
       data: matchedItems.slice(offset, offset + limit),
