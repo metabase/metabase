a89e5905898e25a9c1d9495d5e6d8d214297fe9e
Oisin Coveney <oisin@metabase.com>
2023-07-27 15:46:54 +0100

    Reduce extraneous code

--- frontend/test/__support__/server-mocks/search.ts
+++ frontend/test/__support__/server-mocks/search.ts
@@ -4,35 +4,29 @@ import type { CollectionItem, SearchResult } from "metabase-types/api";
 export function setupSearchEndpoints(items: (CollectionItem | SearchResult)[]) {
   fetchMock.get("path:/api/search", uri => {
     const url = new URL(uri);
-    const urlModels = [...new Set(url.searchParams.getAll("models"))];
+    const models = [...new Set(url.searchParams.getAll("models"))];
     const limit = Number(url.searchParams.get("limit"));
     const offset = Number(url.searchParams.get("offset"));
     const table_db_id = url.searchParams.get("table_db_id") || null;
     const queryText = url.searchParams.get("q");
 
-    let availableModelItems = items;
+    let matchedItems = items;
     if (queryText) {
       const searchString = queryText.toLowerCase();
 
-      availableModelItems = availableModelItems.filter(({ name }) =>
+      matchedItems = matchedItems.filter(({ name }) =>
         name.toLowerCase().includes(searchString),
       );
     }
 
-    let matchedItems = [...availableModelItems];
-    if (urlModels && urlModels.length > 0) {
-      matchedItems = matchedItems.filter(({ model }) =>
-        urlModels.includes(model),
-      );
-    }
-
-    // available_models: all possible entity types within the test data after the text query filter
+    // available_models: all possible types in a user's database (or in this case, the test data), AFTER the text query filter
     const availableModels = [
-      ...new Set(availableModelItems.map(({ model }) => model)),
+      ...new Set(matchedItems.map(({ model }) => model)),
     ];
 
-    // models: the types that are actually present in the search response
-    const models = [...new Set(matchedItems.map(({ model }) => model))];
+    if (models && models.length > 0) {
+      matchedItems = matchedItems.filter(({ model }) => models.includes(model));
+    }
 
     return {
       data: matchedItems.slice(offset, offset + limit),
