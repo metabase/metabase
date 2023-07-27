785357b299c8924de18bef9636114325310f1109
Oisin Coveney <oisin@metabase.com>
2023-07-27 14:34:24 +0100

    Make sure payload matches expected request

--- e2e/support/helpers/e2e-table-metadata-helpers.js
+++ e2e/support/helpers/e2e-table-metadata-helpers.js
@@ -1,19 +1,29 @@
-export const createMetric = ({ name, description, table_id, field_id }) => {
+export const createMetric = ({
+  name,
+  table_id,
+  definition,
+  description = null,
+}) => {
   cy.log(`Create a metric: ${name}`);
   return cy.request("POST", "/api/metric", {
     name,
     description,
     table_id,
-    field_id,
+    definition,
   });
 };
 
-export const createSegment = ({ name, description, table_id, field_id }) => {
+export const createSegment = ({
+  name,
+  table_id,
+  definition,
+  description = null,
+}) => {
   cy.log(`Create a segment: ${name}`);
   return cy.request("POST", "/api/segment", {
     name,
     description,
     table_id,
-    field_id,
+    definition,
   });
 };
