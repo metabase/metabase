ae973b949240f816ba20eb0402190321dd3a0738
Oisin Coveney <oisin@metabase.com>
2023-07-27 14:57:53 +0100

    Make sure search mock API returns SearchResults and gives us all of the possible information that we need

--- frontend/test/__support__/server-mocks/search.ts
+++ frontend/test/__support__/server-mocks/search.ts
@@ -1,21 +1,43 @@
 import fetchMock from "fetch-mock";
-import type { CollectionItem } from "metabase-types/api";
+import type { CollectionItem, SearchResult } from "metabase-types/api";
 
-export function setupSearchEndpoints(items: CollectionItem[]) {
-  const availableModels = items.map(({ model }) => model);
-
+export function setupSearchEndpoints(items: (CollectionItem | SearchResult)[]) {
   fetchMock.get("path:/api/search", uri => {
     const url = new URL(uri);
-    const models = url.searchParams.getAll("models");
+    const urlModels = [...new Set(url.searchParams.getAll("models"))];
     const limit = Number(url.searchParams.get("limit"));
     const offset = Number(url.searchParams.get("offset"));
     const table_db_id = url.searchParams.get("table_db_id") || null;
-    const matchedItems = items.filter(({ model }) => models.includes(model));
+    const queryText = url.searchParams.get("q");
+
+    let availableModelItems = items;
+    if (queryText) {
+      const searchString = queryText.toLowerCase();
+
+      availableModelItems = availableModelItems.filter(({ name }) =>
+        name.toLowerCase().includes(searchString),
+      );
+    }
+
+    let matchedItems = [...availableModelItems];
+    if (urlModels && urlModels.length > 0) {
+      matchedItems = matchedItems.filter(({ model }) =>
+        urlModels.includes(model),
+      );
+    }
+
+    // available_models: all possible types in a user's database (or in this case, the test data), AFTER the text query filter
+    const availableModels = [
+      ...new Set(availableModelItems.map(({ model }) => model)),
+    ];
+
+    // models: the types that are actually present in the data
+    const models = [...new Set(matchedItems.map(({ model }) => model))];
 
     return {
-      data: matchedItems,
+      data: matchedItems.slice(offset, offset + limit),
       total: matchedItems.length,
-      models, // this should reflect what is in the query param
+      models,
       available_models: availableModels,
       limit,
       offset,
