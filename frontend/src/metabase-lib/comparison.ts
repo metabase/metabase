a6dd5087ea03a0e96fa53c8b9657326d43cba2dc
Anton Kulyk <kuliks.anton@gmail.com>
2023-07-28 15:10:02 +0100

    Remove join on table change

--- frontend/src/metabase-lib/comparison.ts
+++ frontend/src/metabase-lib/comparison.ts
@@ -1,5 +1,7 @@
 import * as ML from "cljs/metabase.lib.js";
 import type { DatasetQuery } from "metabase-types/api";
+import type { Query } from "./types";
+import { toLegacyQuery } from "./query";
 
 export function areLegacyQueriesEqual(
   query1: DatasetQuery,
@@ -7,4 +9,16 @@ export function areLegacyQueriesEqual(
   fieldIds?: number[],
 ): boolean {
   return ML.query_EQ_(query1, query2, fieldIds);
+}
+
+export function areQueriesEqual(
+  query1: Query,
+  query2: Query,
+  fieldIds?: number[],
+): boolean {
+  return areLegacyQueriesEqual(
+    toLegacyQuery(query1),
+    toLegacyQuery(query2),
+    fieldIds,
+  );
 }
