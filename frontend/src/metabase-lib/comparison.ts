86c57315405a26d5ef8a1dc3207d82065a6c374a
Anton Kulyk <kuliks.anton@gmail.com>
2023-07-28 15:51:51 +0100

    Fix race-condition when loading table metadata

--- frontend/src/metabase-lib/comparison.ts
+++ frontend/src/metabase-lib/comparison.ts
@@ -1,7 +1,5 @@
 import * as ML from "cljs/metabase.lib.js";
 import type { DatasetQuery } from "metabase-types/api";
-import type { ColumnMetadata, Query } from "./types";
-import { displayInfo } from "./metadata";
 
 export function areLegacyQueriesEqual(
   query1: DatasetQuery,
@@ -9,15 +7,4 @@ export function areLegacyQueriesEqual(
   fieldIds?: number[],
 ): boolean {
   return ML.query_EQ_(query1, query2, fieldIds);
-}
-
-export function isSameColumn(
-  query: Query,
-  stageIndex: number,
-  column1: ColumnMetadata,
-  column2: ColumnMetadata,
-): boolean {
-  const info1 = displayInfo(query, stageIndex, column1);
-  const info2 = displayInfo(query, stageIndex, column2);
-  return info1.name === info2.name && info1?.table?.name === info2?.table?.name;
 }
