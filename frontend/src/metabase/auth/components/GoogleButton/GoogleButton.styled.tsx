2f03cbb5b50029674dea7b1dcbecca1f7999e65a
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-28 14:25:56 +0300

    Fix google sso in 44

--- frontend/src/metabase/auth/components/GoogleButton/GoogleButton.styled.tsx
+++ frontend/src/metabase/auth/components/GoogleButton/GoogleButton.styled.tsx
@@ -1,6 +1,12 @@
 import styled from "@emotion/styled";
 import { color } from "metabase/lib/colors";
 
+export const GoogleButtonRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const AuthError = styled.div`
   color: ${color("error")};
   text-align: center;
