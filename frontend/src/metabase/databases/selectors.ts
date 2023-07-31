cb58c9413da6a1ed78202195a4386d23d8420175
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-07-31 12:01:47 +0300

    Feature flags integration (#32432)

---
+++ frontend/src/metabase/databases/selectors.ts
@@ -0,0 +1,14 @@
+import { State } from "metabase-types/store";
+import { getSetting } from "metabase/selectors/settings";
+
+export const getEngines = (state: State) => {
+  return getSetting(state, "engines");
+};
+
+export const getIsHosted = (state: State) => {
+  return getSetting(state, "is-hosted?");
+};
+
+export const getIsCachingEnabled = (state: State) => {
+  return getSetting(state, "enable-query-caching") ?? false;
+};
---
+++ frontend/src/metabase/sharing/components/SharingSidebar/tests/premium.unit.spec.ts
@@ -0,0 +1,34 @@
+import userEvent from "@testing-library/user-event";
+import { screen } from "__support__/ui";
+
+import { setup, hasAdvancedFilterOptions } from "./setup";
+
+describe("SharingSidebar Premium Features", () => {
+  const tokenFeatures = {
+    dashboard_subscription_filters: true,
+  };
+
+  describe("Email Subscription sidebar", () => {
+    it("should show advanced filtering options with the correct feature flag", async () => {
+      setup({ email: true, tokenFeatures, hasEnterprisePlugins: true });
+
+      userEvent.click(await screen.findByText("Email it"));
+
+      await screen.findByText("Email this dashboard");
+
+      expect(hasAdvancedFilterOptions(screen)).toBe(true);
+    });
+  });
+
+  describe("Slack Subscription sidebar", () => {
+    it("should show advanced filtering options with the correct feature flag", async () => {
+      setup({ slack: true, tokenFeatures, hasEnterprisePlugins: true });
+
+      userEvent.click(await screen.findByText("Send it to Slack"));
+
+      await screen.findByText("Send this dashboard to Slack");
+
+      expect(hasAdvancedFilterOptions(screen)).toBe(true);
+    });
+  });
+});
