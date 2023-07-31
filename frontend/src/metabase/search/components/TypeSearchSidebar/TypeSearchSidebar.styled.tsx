454a55f6a445bef2b15d1c3c888cab468a694340
Oisin Coveney <oisin@metabase.com>
2023-07-31 22:55:00 +0300

    Add `TypeSearchSidebar` to integration branch (#32747)

---
+++ frontend/src/metabase/search/components/TypeSearchSidebar/TypeSearchSidebar.styled.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import Button from "metabase/core/components/Button";
+import { color } from "metabase/lib/colors";
+
+export const TypeSidebarButton = styled(Button)<{ isActive: boolean }>`
+  color: ${props => (props.isActive ? color("brand") : color("text-medium"))};
+  border: none;
+`;
