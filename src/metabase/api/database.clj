metabase/metabase

commit 8d7ec95708abd084b45707d68c33d672aead4f9f

Migrate database.options to database.settings (#31755)

src/metabase/api/database.clj
@@ -831,14 +831,14 @@
              (tru "Persisting models is not enabled."))
   (api/let-404 [database (t2/select-one Database :id id)]
     (api/write-check database)
-    (if (-> database :options :persist-models-enabled)
+    (if (-> database :settings :persist-models-enabled)
       ;; todo: some other response if already persisted?
       api/generic-204-no-content
       (let [[success? error] (ddl.i/check-can-persist database)
             schema           (ddl.i/schema-name database (public-settings/site-uuid))]
         (if success?
           ;; do secrets require special handling to not clobber them or mess up encryption?
-          (do (t2/update! Database id {:options (assoc (:options database) :persist-models-enabled true)})
+          (do (t2/update! Database id {:settings (assoc (:settings database) :persist-models-enabled true)})
               (task.persist-refresh/schedule-persistence-for-database!
                 database
                 (public-settings/persisted-model-refresh-cron-schedule))
@@ -854,8 +854,8 @@
   {:id su/IntGreaterThanZero}
   (api/let-404 [database (t2/select-one Database :id id)]
     (api/write-check database)
-    (if (-> database :options :persist-models-enabled)
-      (do (t2/update! Database id {:options (dissoc (:options database) :persist-models-enabled)})
+    (if (-> database :settings :persist-models-enabled)
+      (do (t2/update! Database id {:settings (dissoc (:settings database) :persist-models-enabled)})
           (persisted-info/mark-for-pruning! {:database_id id})
           (task.persist-refresh/unschedule-persistence-for-database! database)
           api/generic-204-no-content)

src/metabase/models/database.clj
@@ -31,7 +31,6 @@
 
 (t2/deftransforms :model/Database
   {:details                     mi/transform-encrypted-json
-   :options                     mi/transform-json
    :engine                      mi/transform-keyword
    :metadata_sync_schedule      mi/transform-cron-string
    :cache_field_values_schedule mi/transform-cron-string

enterprise/backend/test/metabase_enterprise/advanced_config/caching_test.clj
@@ -41,7 +41,7 @@
 (defn do-with-persist-models [f]
   (let [two-hours-ago (t/minus (t/local-date-time) (t/hours 2))]
     (t2.with-temp/with-temp
-      [Database db {:options {:persist-models-enabled true}}
+      [Database db {:settings {:persist-models-enabled true}}
        Card     creating  {:dataset true :database_id (u/the-id db)}
        Card     deletable {:dataset true :database_id (u/the-id db)}
        Card     off       {:dataset true :database_id (u/the-id db)}

resources/migrations/000_migrations.yaml
@@ -16021,6 +16021,31 @@ databaseChangeLog:
             onDelete: CASCADE
 
   - changeSet:
+      id: v48.00-001
+      author: qnkhuat
+      comment: Added 0.47.0 - Migrate database.options to database.settings
+      changes:
+        - customChange:
+            class: "metabase.db.custom_migrations.MigrateDatabaseOptionsToSettings"
+
+  - changeSet:
+      id: v48.00-002
+      author: qnkhuat
+      comment: Added 0.47.0 - drop metabase_database.options
+      changes:
+        - dropColumn:
+            tableName: metabase_database
+            columnName: options
+      rollback:
+        - addColumn:
+            tableName: metabase_database
+            columns:
+              - column:
+                  name: options
+                  type: ${text.type}
+                  remarks: "Serialized JSON containing various options like QB behavior."
+
+  - changeSet:
       id: v48.00-003
       author: qnkhuat
       comment: Added 0.48.0 - drop computation_job_result table

src/metabase/api/persist.clj
@@ -133,15 +133,15 @@
 (defn- disable-persisting
   "Disables persistence.
   - update all [[PersistedInfo]] rows to be inactive and deletable
-  - remove `:persist-models-enabled` from relevant [[Database]] options
+  - remove `:persist-models-enabled` from relevant [[Database]] settings
   - schedule a task to [[metabase.driver.ddl.interface/unpersist]] each table"
   []
   (let [id->db      (m/index-by :id (t2/select Database))
-        enabled-dbs (filter (comp :persist-models-enabled :options) (vals id->db))]
+        enabled-dbs (filter (comp :persist-models-enabled :settings) (vals id->db))]
     (log/info (tru "Disabling model persistence"))
     (doseq [db enabled-dbs]
       (t2/update! Database (u/the-id db)
-                  {:options (not-empty (dissoc (:options db) :persist-models-enabled))}))
+                  {:settings (not-empty (dissoc (:settings db) :persist-models-enabled))}))
     (task.persist-refresh/disable-persisting!)))
 
 #_{:clj-kondo/ignore [:deprecated-var]}

src/metabase/db/custom_migrations.clj
@@ -16,6 +16,7 @@
    [clojurewerkz.quartzite.triggers :as triggers]
    [medley.core :as m]
    [metabase.db.connection :as mdb.connection]
+   [metabase.models.interface :as mi]
    [metabase.plugins.classloader :as classloader]
    [metabase.util.honey-sql-2 :as h2x]
    [metabase.util.log :as log]
@@ -675,3 +676,28 @@
                                                      [:like :object "%ref\\\\\",[\\\\\"field%"]
                                                      [:like :object "%ref\\\\\\\",[\\\\\\\"field%"]]
                                                     [:like :object "%join-alias%"]]}))))
+
+(define-reversible-migration MigrateDatabaseOptionsToSettings
+  (let [update-one! (fn [{:keys [id settings options]}]
+                      (let [settings     (mi/encrypted-json-out settings)
+                            options      (mi/json-out-with-keywordization options)
+                            new-settings (mi/encrypted-json-in (merge settings options))]
+                        (t2/query {:update :metabase_database
+                                   :set    {:settings new-settings}
+                                   :where  [:= :id id]})))]
+    (run! update-one! (t2/reducible-query {:select [:id :settings :options]
+                                           :from   [:metabase_database]
+                                           :where  [:and
+                                                    [:not= :options ""]
+                                                    [:not= :options "{}"]
+                                                    [:not= :options nil]]})))
+  (let [rollback-one! (fn [{:keys [id settings options]}]
+                        (let [settings (mi/encrypted-json-out settings)
+                              options  (mi/json-out-with-keywordization options)]
+                          (when (some? (:persist-models-enabled settings))
+                            (t2/query {:update :metabase_database
+                                       :set    {:options (json/generate-string (select-keys settings [:persist-models-enabled]))
+                                                :settings (mi/encrypted-json-in (dissoc settings :persist-models-enabled))}
+                                       :where  [:= :id id]}))))]
+    (run! rollback-one! (t2/reducible-query {:select [:id :settings :options]
+                                             :from   [:metabase_database]}))))

src/metabase/db/liquibase.clj
@@ -62,15 +62,15 @@
    f]
   ;; Custom migrations use toucan2, so we need to make sure it uses the same connection with liquibase
   (binding [t2.conn/*current-connectable* conn-or-data-source]
-   (if (instance? java.sql.Connection conn-or-data-source)
-     (f (-> conn-or-data-source liquibase-connection database liquibase))
-     ;; closing the `LiquibaseConnection`/`Database` closes the parent JDBC `Connection`, so only use it in combination
-     ;; with `with-open` *if* we are opening a new JDBC `Connection` from a JDBC spec. If we're passed in a `Connection`,
-     ;; it's safe to assume the caller is managing its lifecycle.
-     (with-open [conn           (.getConnection ^javax.sql.DataSource conn-or-data-source)
-                 liquibase-conn (liquibase-connection conn)
-                 database       (database liquibase-conn)]
-       (f (liquibase database))))))
+    (if (instance? java.sql.Connection conn-or-data-source)
+      (f (-> conn-or-data-source liquibase-connection database liquibase))
+      ;; closing the `LiquibaseConnection`/`Database` closes the parent JDBC `Connection`, so only use it in combination
+      ;; with `with-open` *if* we are opening a new JDBC `Connection` from a JDBC spec. If we're passed in a `Connection`,
+      ;; it's safe to assume the caller is managing its lifecycle.
+      (with-open [conn           (.getConnection ^javax.sql.DataSource conn-or-data-source)
+                  liquibase-conn (liquibase-connection conn)
+                  database       (database liquibase-conn)]
+        (f (liquibase database))))))
 
 (defmacro with-liquibase
   "Execute body with an instance of a `Liquibase` bound to `liquibase-binding`.

src/metabase/db/setup.clj
@@ -72,31 +72,31 @@
     (log/info (trs "Setting up Liquibase..."))
     (liquibase/with-liquibase [liquibase conn]
       (try
-        (liquibase/consolidate-liquibase-changesets! db-type conn)
-        (log/info (trs "Liquibase is ready."))
-        (case direction
-          :up            (liquibase/migrate-up-if-needed! liquibase)
-          :force         (liquibase/force-migrate-up-if-needed! liquibase)
-          :down          (apply liquibase/rollback-major-version db-type conn liquibase args)
-          :print         (print-migrations-and-quit-if-needed! liquibase)
-          :release-locks (liquibase/force-release-locks! liquibase))
-        ;; Migrations were successful; commit everything and re-enable auto-commit
-        (.commit conn)
-        (.setAutoCommit conn true)
-        :done
-        ;; In the Throwable block, we're releasing the lock assuming we have the lock and we failed while in the
-        ;; middle of a migration. It's possible that we failed because we couldn't get the lock. We don't want to
-        ;; clear the lock in that case, so handle that case separately
-        (catch LockException e
-          (.rollback conn)
-          (throw e))
-        ;; If for any reason any part of the migrations fail then rollback all changes
-        (catch Throwable e
-          (.rollback conn)
-          ;; With some failures, it's possible that the lock won't be released. To make this worse, if we retry the
-          ;; operation without releasing the lock first, the real error will get hidden behind a lock error
-          (liquibase/release-lock-if-needed! liquibase)
-          (throw e))))))
+       (liquibase/consolidate-liquibase-changesets! db-type conn)
+       (log/info (trs "Liquibase is ready."))
+       (case direction
+         :up            (liquibase/migrate-up-if-needed! liquibase)
+         :force         (liquibase/force-migrate-up-if-needed! liquibase)
+         :down          (apply liquibase/rollback-major-version db-type conn liquibase args)
+         :print         (print-migrations-and-quit-if-needed! liquibase)
+         :release-locks (liquibase/force-release-locks! liquibase))
+       ;; Migrations were successful; commit everything and re-enable auto-commit
+       (.commit conn)
+       (.setAutoCommit conn true)
+       :done
+       ;; In the Throwable block, we're releasing the lock assuming we have the lock and we failed while in the
+       ;; middle of a migration. It's possible that we failed because we couldn't get the lock. We don't want to
+       ;; clear the lock in that case, so handle that case separately
+       (catch LockException e
+         (.rollback conn)
+         (throw e))
+       ;; If for any reason any part of the migrations fail then rollback all changes
+       (catch Throwable e
+         (.rollback conn)
+         ;; With some failures, it's possible that the lock won't be released. To make this worse, if we retry the
+         ;; operation without releasing the lock first, the real error will get hidden behind a lock error
+         (liquibase/release-lock-if-needed! liquibase)
+         (throw e))))))
 
 (s/defn ^:private verify-db-connection
   "Test connection to application database with `data-source` and throw an exception if we have any troubles

commit 36250e60a9a34e77721bf888f3bed570adac45e0

Temporarily fix the broken database endpoint (#32799)
* temporarily fix for the broken database endpont

* a more proper fix

* fix indent

---
The bug was introduced in #31755

src/metabase/api/database.clj
@@ -282,12 +282,11 @@
                                                       :include-saved-questions-tables? include-saved-questions-tables?
                                                       :include-editable-data-model?    include_editable_data_model
                                                       :exclude-uneditable-details?     only-editable?
-                                                      :include-analytics?  include_analytics
+                                                      :include-analytics?              include_analytics
                                                       :include-only-uploadable?        include_only_uploadable)
                                             [])]
-    {:data  db-list-res
-     :total (count db-list-res)}))
-
+   {:data  db-list-res
+    :total (count db-list-res)}))
 
 ;;; --------------------------------------------- GET /api/database/:id ----------------------------------------------
 

src/metabase/models/database.clj
@@ -299,12 +299,16 @@
                                      details
                                      (sensitive-fields-for-db db)))))]
      (update db :settings (fn [settings]
-                            (when settings
+                            (when (map? settings)
                               (into {}
                                     (filter (fn [[setting-name _v]]
-                                              (setting/can-read-setting? setting-name
-                                                                         (setting/current-user-readable-visibilities))))
-                                    settings)))))
+                                              (try
+                                               (setting/can-read-setting? setting-name
+                                                                          (setting/current-user-readable-visibilities))
+                                               (catch Throwable _e
+                                                 true)))
+                                            settings))))))
+
    json-generator))
 
 ;;; ------------------------------------------------ Serialization ----------------------------------------------------