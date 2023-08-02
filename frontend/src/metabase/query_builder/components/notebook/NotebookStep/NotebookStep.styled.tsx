8719a49af7c9c45464c9694d1b4e5afd5061a9e2
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-02 13:03:49 +0300

    Fix NotebookStep

--- frontend/src/metabase/query_builder/components/notebook/NotebookStep/NotebookStep.styled.tsx
+++ frontend/src/metabase/query_builder/components/notebook/NotebookStep/NotebookStep.styled.tsx
@@ -1,7 +1,8 @@
 import styled from "@emotion/styled";
 import Button from "metabase/core/components/Button";
-import { alpha, darken, lighten } from "metabase/lib/colors";
+import { alpha, color, darken, lighten } from "metabase/lib/colors";
 import { breakpointMinSmall } from "metabase/styled-components/theme";
+import ActionButton from "./ActionButton";
 
 const getPercentage = (number: number): string => {
   return `${number * 100}%`;
@@ -65,4 +66,19 @@ export const ColorButton = styled(Button)<ColorButtonProps>`
       transparent ? lighten(color, 0.5) : alpha(color, 0.35)};
   }
   transition: background 300ms;
+`;
+
+interface PreviewButtonProps {
+  hasPreviewButton?: boolean;
+}
+
+export const PreviewButton = styled(ActionButton)<PreviewButtonProps>`
+  margin-left: 0.5rem;
+  visibility: ${props => !props.hasPreviewButton && "hidden"};
+  pointer-events: ${props => !props.hasPreviewButton && "none"};
+  opacity: ${props => !props.hasPreviewButton && "0.4"};
+
+  &:hover {
+    color: ${props => props.hasPreviewButton && color("brand")};
+  }
 `;
