0868e43883250e4e6d27ce8e2c1af081c80e7551
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-02 17:31:19 +0300

    Fix QuestionNotebookButton

--- frontend/src/metabase/query_builder/components/view/QuestionNotebookButton/QuestionNotebookButton.styled.tsx
+++ frontend/src/metabase/query_builder/components/view/QuestionNotebookButton/QuestionNotebookButton.styled.tsx
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
+import Button from "metabase/core/components/Button";
 import { color } from "metabase/lib/colors";
 
 interface ButtonRootProps {
   isSelected?: boolean;
 }
 
-export const ButtonRoot = styled.a<ButtonRootProps>`
+export const ButtonRoot = styled(Button)<ButtonRootProps>`
   color: ${props => !props.isSelected && color("text-dark")};
 
   &:hover {
