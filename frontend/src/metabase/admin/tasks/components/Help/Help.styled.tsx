ecb23f025933da43778a4a6c9a6a328c251e8ab0
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-03 13:11:34 +0300

    Fix help

--- frontend/src/metabase/admin/tasks/components/Help/Help.styled.tsx
+++ frontend/src/metabase/admin/tasks/components/Help/Help.styled.tsx
@@ -34,7 +34,7 @@ export const InfoBlockButton = styled.div`
 export const HelpExternalLink = styled(ExternalLink)`
   display: flex;
   padding: 1rem;
-  border: 1px solid ${color("brand")};
+  border: 1px solid ${color("border")};
   border-radius: 0.5rem;
   transition: border 0.3s linear;
   text-decoration: none;
