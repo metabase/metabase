34d82c373e5e8e81cf25a5d0b6a0795ebbc9345b
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-08-03 21:17:49 +0700

    Reuse dashboard E2E util

--- e2e/support/helpers/e2e-dashboard-helpers.js
+++ e2e/support/helpers/e2e-dashboard-helpers.js
@@ -1,5 +1,5 @@
 import { visitDashboard } from "./e2e-misc-helpers";
-import { popover } from "./e2e-ui-elements-helpers";
+import { dashboardHeader, popover } from "./e2e-ui-elements-helpers";
 
 // Metabase utility functions for commonly-used patterns
 export function selectDashboardFilter(selection, filterName) {
@@ -182,4 +182,8 @@ export function resizeDashboardCard({ card, x, y }) {
 
 export function createLinkCard() {
   cy.icon("link").click();
+}
+
+export function toggleDashboardInfoSidebar() {
+  dashboardHeader().icon("info").click();
 }
