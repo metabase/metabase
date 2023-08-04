bd9b44715042a6bf4225824466945fa9650a7579
Nicolo Pretto <info@npretto.com>
2023-08-04 11:25:33 +0200

    test: test for undo -> re-fetch card

--- e2e/test/scenarios/dashboard/reproductions/12926-editing-dashboards-doesnt-cancel-running-queries.cy.spec.js
+++ e2e/test/scenarios/dashboard/reproductions/12926-editing-dashboards-doesnt-cancel-running-queries.cy.spec.js
@@ -2,6 +2,7 @@ import {
   editDashboard,
   restore,
   showDashboardCardActions,
+  undo,
   visitDashboard,
 } from "e2e/support/helpers";
 
@@ -31,15 +32,39 @@ describe("issue 12926", { tags: "@external" }, () => {
         cy.stub(win.XMLHttpRequest.prototype, "abort").as("xhrAbort");
       });
 
-      editDashboard();
-
-      showDashboardCardActions();
-
-      cy.findByTestId("dashboardcard-actions-panel").within(() => {
-        cy.findByLabelText("close icon").click();
-      });
+      removeCard();
 
       cy.get("@xhrAbort").should("have.been.calledOnce");
     });
+
+    it("should re-fetch the query when doing undo on the removal", () => {
+      cy.createNativeQuestionAndDashboard({
+        questionDetails,
+      }).then(({ body: { dashboard_id } }) => {
+        visitDashboard(dashboard_id, { should_wait: false });
+      });
+
+      removeCard();
+
+      // stub the response so that the we can wait the request to see if it has been made
+      // without the stub it would go in timeout as the query is longer than the default timeout
+      cy.intercept("POST", "api/dashboard/*/dashcard/*/card/*/query", req => {
+        req.reply({});
+      }).as("cardQuery");
+
+      undo();
+
+      cy.wait("@cardQuery");
+    });
   });
 });
+
+function removeCard() {
+  editDashboard();
+
+  showDashboardCardActions();
+
+  cy.findByTestId("dashboardcard-actions-panel").within(() => {
+    cy.findByLabelText("close icon").click();
+  });
+}
