93397e7bfaa8b663509f1a588a3b8ff5c0bdf370
Jerry Huang <34140255+qwef@users.noreply.github.com>
2023-08-04 15:03:15 -0700

    🍒 Manual Backport #32458 (#32955)
    * Resolve pivot tables in serialization (#32458)
    
    * initial commit
    
    * remove print
    
    * add fix
    
    * remove load
    
    * fix tests
    
    * fix merge

--- enterprise/backend/test/metabase_enterprise/serialization/load_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/load_test.clj
@@ -36,7 +36,8 @@
    [metabase.util :as u]
    [metabase.util.i18n :refer [trs]]
    [metabase.util.log :as log]
-   [toucan.db :as db])
+   [toucan.db :as db]
+   [toucan2.core :as t2])
   (:import
    (org.apache.commons.io FileUtils)))
 
@@ -150,13 +151,55 @@
           (is (integer? field-id))
           (is (= "latitude" (-> (db/select-one-field :name Field :id field-id)
                               u/lower-case-en)))
-          (is (= {:show_mini_bar true
-                  :column_title "Parallel"} col-val))
-          (is (= "Venue Category" col-name))
-          (is (true? col-enabled))
-          (is (integer? col-field-id) "fieldRef within table.columns was properly serialized and loaded")
-          (is (= "category_id" (-> (db/select-one-field :name Field :id col-field-id)
-                                   u/lower-case-en))))))
+        (is (= {:show_mini_bar true
+                :column_title "Parallel"} col-val))
+        (is (= "Venue Category" col-name))
+        (is (true? col-enabled))
+        (is (integer? col-field-id) "fieldRef within table.columns was properly serialized and loaded")
+        (is (= "category_id" (-> (db/select-one-field :name Field :id col-field-id)
+                                 u/lower-case-en))))))))
+
+(defn- test-loaded-card [card card-name]
+  (when (= "My Nested Card" card-name)
+    (testing "Visualization settings for a Card were persisted correctly"
+      (let [vs (:visualization_settings card)
+            col (-> (:column_settings vs)
+                    first)
+            [col-key col-val] col
+            col-ref (mb.viz/parse-db-column-ref col-key)
+            {:keys [::mb.viz/field-id]} col-ref
+            [{col-name :name col-field-ref :fieldRef col-enabled :enabled :as _tbl-col} & _] (:table.columns vs)
+            [_ col-field-id _] col-field-ref]
+        (is (some? (:table.columns vs)))
+        (is (some? (:column_settings vs)))
+        (is (integer? field-id))
+        (is (= "latitude" (-> (t2/select-one-fn :name Field :id field-id)
+                              u/lower-case-en)))
+        (is (= {:show_mini_bar true
+                :column_title "Parallel"} col-val))
+        (is (= "Venue Category" col-name))
+        (is (true? col-enabled))
+        (is (integer? col-field-id) "fieldRef within table.columns was properly serialized and loaded")
+        (is (= "category_id" (-> (t2/select-one-fn :name Field :id col-field-id)
+                                 u/lower-case-en)))))))
+
+(defn- test-pivot-card [card card-name]
+  (when (= "Pivot Table Card" card-name)
+    (testing "Visualization settings for a Card were persisted correctly"
+      (let [vs      (:visualization_settings card)
+            pivot   (:pivot_table.column_split vs)
+            vecs    (concat (:columns pivot) (:rows pivot))]
+        (is (some? vecs))
+        (doseq [[_ field-id _] vecs]
+          (is (integer? field-id) "fieldRef within pivot table was properly serialized and loaded"))))))
+
+(defmethod assert-loaded-entity Card
+  [{card-name :name :as card} {:keys [query-results collections]}]
+  (testing (format "Card: %s" card-name)
+    (query-res-match query-results card)
+    (collection-names-match collections card)
+    (test-loaded-card card card-name)
+    (test-pivot-card card card-name)
     card))
 
 (defn- collection-parent-name [collection]
@@ -331,7 +374,8 @@
                                                                 card-id-temporal-unit
                                                                 card-id-with-native-snippet
                                                                 card-id-temporal-unit
-                                                                card-join-card-id])
+                                                                card-join-card-id
+                                                                card-id-pivot-table])
                            :collections   (gather-collections [card-id
                                                                card-arch-id
                                                                card-id-root
@@ -345,7 +389,8 @@
                                                                card-id-temporal-unit
                                                                card-id-with-native-snippet
                                                                card-id-temporal-unit
-                                                               card-join-card-id])
+                                                               card-join-card-id
+                                                               card-id-pivot-table])
                            :entities      [[Database           (db/select-one Database :id db-id)]
                                            [Table              (db/select-one Table :id table-id)]
                                            [Table              (db/select-one Table :id table-id-categories)]
@@ -391,7 +436,8 @@
                                            [Collection         (db/select-one Collection :id snippet-nested-collection-id)]
                                            [NativeQuerySnippet (db/select-one NativeQuerySnippet :id nested-snippet-id)]
                                            [Card               (db/select-one Card :id card-id-with-native-snippet)]
-                                           [Card               (db/select-one Card :id card-join-card-id)]]})]
+                                           [Card               (db/select-one Card :id card-join-card-id)]
+                                           [Card               (t2/select-one Card :id card-id-pivot-table)]]})]
         (with-world-cleanup
           (v1-load dump-dir {:on-error :continue :mode :skip})
           (mt/with-db (db/select-one Database :name ts/temp-db-name)
