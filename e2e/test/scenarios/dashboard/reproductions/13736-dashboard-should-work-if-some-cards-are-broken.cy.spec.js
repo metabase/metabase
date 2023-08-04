14d8020e74369cb2508ea44cd4e9a90a42ddc2eb
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-08-04 15:05:53 +0700

    Not hardcoding the question IDs

--- e2e/test/scenarios/dashboard/reproductions/13736-dashboard-should-work-if-some-cards-are-broken.cy.spec.js
+++ e2e/test/scenarios/dashboard/reproductions/13736-dashboard-should-work-if-some-cards-are-broken.cy.spec.js
@@ -5,6 +5,23 @@ import {
   visitDashboard,
 } from "e2e/support/helpers";
 
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+
+const { ORDERS_ID } = SAMPLE_DATABASE;
+
+const questionDetails1 = {
+  name: "Orders question",
+  query: { "source-table": ORDERS_ID },
+};
+
+const questionDetails2 = {
+  name: "Orders count",
+  query: {
+    "source-table": ORDERS_ID,
+    aggregation: [["count"]],
+  },
+};
+
 describe("issue 13736", () => {
   beforeEach(() => {
     restore();
@@ -12,32 +29,52 @@ describe("issue 13736", () => {
   });
 
   it("should work even if some cards are broken (metabase#13736)", () => {
-    cy.intercept("POST", "/api/dashboard/*/dashcard/*/card/1/query", {
-      statusCode: 500,
-      body: {
-        cause: "some error",
-        data: {},
-        message: "some error",
-      },
+    cy.createQuestion(questionDetails1, {
+      wrapId: true,
+      idAlias: "questionId1",
     });
-
+    cy.createQuestion(questionDetails2, {
+      wrapId: true,
+      idAlias: "questionId2",
+    });
     cy.createDashboard({ name: "13736 Dashboard" }).then(
       ({ body: { id: dashboardId } }) => {
-        updateDashboardCards({
-          dashboard_id: dashboardId,
-          cards: [
-            {
-              card_id: 1,
-            },
-            {
-              card_id: 2,
-              col: 11,
-            },
-          ],
-        });
-        visitDashboard(dashboardId);
+        cy.wrap(dashboardId).as("dashboardId");
       },
     );
+
+    cy.then(function () {
+      const dashboardId = this.dashboardId;
+      const questionId1 = this.questionId1;
+      const questionId2 = this.questionId2;
+
+      cy.intercept(
+        "POST",
+        `/api/dashboard/*/dashcard/*/card/${questionId1}/query`,
+        {
+          statusCode: 500,
+          body: {
+            cause: "some error",
+            data: {},
+            message: "some error",
+          },
+        },
+      );
+
+      updateDashboardCards({
+        dashboard_id: dashboardId,
+        cards: [
+          {
+            card_id: questionId1,
+          },
+          {
+            card_id: questionId2,
+            col: 11,
+          },
+        ],
+      });
+      visitDashboard(dashboardId);
+    });
 
     getDashboardCards()
       .eq(0)
