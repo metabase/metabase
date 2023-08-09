8b3e6c8c7fb875ee10ecea10d821e445c1df9fd9
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2023-08-09 11:40:05 +0300

    Address review

--- e2e/test/scenarios/dashboard/reproductions/31274-dashcard-actions-overlap.cy.spec.js
+++ e2e/test/scenarios/dashboard/reproductions/31274-dashcard-actions-overlap.cy.spec.js
@@ -44,57 +44,53 @@ describe("issue 31274", () => {
   });
 
   // cypress automatically scrolls to the element, but we don't need it in this test
-  it(
-    "should not clip dashcard actions (mestabase#31274)",
-    { scrollBehavior: false },
-    () => {
-      cy.createDashboard().then(({ body: dashboard }) => {
-        const cards = createTextCards(3);
-        cy.request("PUT", `/api/dashboard/${dashboard.id}/cards`, {
-          cards,
-        });
-
-        visitDashboard(dashboard.id);
-        editDashboard(dashboard.id);
-
-        secondTextCard().realHover();
-
-        visibleActionsPanel().should("have.length", 1);
-
-        cy.log(
-          "Make sure cypress can click the element, which means it is not covered by another",
-        );
-
-        visibleActionsPanel().within(() => {
-          cy.icon("close").click({
-            position: "top",
-          });
-        });
-      });
-    },
-  );
-
-  it(
-    "renders cross icon on the link card without clipping",
-    { scrollBehavior: false },
-    () => {
-      cy.createDashboard().then(({ body: dashboard }) => {
-        visitDashboard(dashboard.id);
-        editDashboard(dashboard.id);
+  it("should not clip dashcard actions (metabase#31274)", () => {
+    cy.createDashboard().then(({ body: dashboard }) => {
+      const cards = createTextCards(3);
+      cy.request("PUT", `/api/dashboard/${dashboard.id}/cards`, {
+        cards,
       });
 
-      createLinkCard();
-      cy.findByPlaceholderText("https://example.com").realHover();
+      visitDashboard(dashboard.id);
+      editDashboard(dashboard.id);
+
+      secondTextCard().realHover();
+
+      visibleActionsPanel().should("have.length", 1);
 
       cy.log(
         "Make sure cypress can click the element, which means it is not covered by another",
       );
 
-      cy.findByTestId("dashboardcard-actions-panel").within(() => {
-        cy.icon("close").click({ position: "bottom" });
+      visibleActionsPanel().within(() => {
+        cy.icon("close").click({
+          position: "top",
+        });
       });
-    },
-  );
+
+      cy.findAllByTestId("dashcard").should("have.length", 2);
+    });
+  });
+
+  it("renders cross icon on the link card without clipping", () => {
+    cy.createDashboard().then(({ body: dashboard }) => {
+      visitDashboard(dashboard.id);
+      editDashboard(dashboard.id);
+    });
+
+    createLinkCard();
+    cy.findByPlaceholderText("https://example.com").realHover();
+
+    cy.log(
+      "Make sure cypress can click the element, which means it is not covered by another",
+    );
+
+    cy.findByTestId("dashboardcard-actions-panel").within(() => {
+      cy.icon("close").click({ position: "bottom" });
+    });
+
+    cy.findByTestId("dashcard").should("not.exist");
+  });
 });
 
 function visibleActionsPanel() {
