39a29f3b14eb90e475f8230990a3c4622ad281ab
Cam Saul <github@camsaul.com>
2023-08-09 23:59:25 +0000

    Move query->expected-cols

--- test/metabase/query_processor_test.clj
+++ test/metabase/query_processor_test.clj
@@ -4,6 +4,7 @@
   this one. Event-based DBs such as Druid are tested in `metabase.driver.event-query-processor-test`."
   (:require
    [clojure.test :refer :all]
+   [metabase.driver :as driver]
    [metabase.query-processor :as qp]
    [metabase.test :as mt]
    [schema.core :as s]))
@@ -41,3 +42,42 @@
             (testing "preprocess should return same results even when query was cached."
               (is (= expected-results
                      (qp/preprocess query))))))))))
+
+(deftest ^:parallel query->expected-cols-test
+  (testing "field_refs in expected columns have the original join aliases (#30648)"
+    (mt/dataset sample-dataset
+      (binding [driver/*driver* ::custom-escape-spaces-to-underscores]
+        (let [query
+              (mt/mbql-query
+                  products
+                  {:joins
+                   [{:source-query
+                     {:source-table $$orders
+                      :joins
+                      [{:source-table $$people
+                        :alias "People"
+                        :condition [:= $orders.user_id &People.people.id]
+                        :fields [&People.people.address]
+                        :strategy :left-join}]
+                      :fields [$orders.id &People.people.address]}
+                     :alias "Question 54"
+                     :condition [:= $id [:field %orders.id {:join-alias "Question 54"}]]
+                     :fields [[:field %orders.id {:join-alias "Question 54"}]
+                              [:field %people.address {:join-alias "Question 54"}]]
+                     :strategy :left-join}]
+                   :fields
+                   [!default.created_at
+                    [:field %orders.id {:join-alias "Question 54"}]
+                    [:field %people.address {:join-alias "Question 54"}]]})]
+          (is (=? [{:name "CREATED_AT"
+                    :field_ref [:field (mt/id :products :created_at) {:temporal-unit :default}]
+                    :display_name "Created At: Default" #_"Created At"} ; FIXME
+                   {:name "ID"
+                    :field_ref [:field (mt/id :orders :id) {:join-alias "Question 54"}]
+                    :display_name "Question 54 → ID"
+                    :source_alias "Question 54"}
+                   {:name "ADDRESS"
+                    :field_ref [:field (mt/id :people :address) {:join-alias "Question 54"}]
+                    :display_name "Question 54 → Address"
+                    :source_alias "Question 54"}]
+                  (qp/query->expected-cols query))))))))
