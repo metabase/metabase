7207054b64461e76c73df13a11d079134c179135
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-10 18:50:02 +0300

    Add story

--- frontend/src/metabase/ui/components/index.ts
+++ frontend/src/metabase/ui/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./data-display";
 export * from "./inputs";
 export * from "./layout";
+export * from "./layovers";
 export * from "./theme";
 export * from "./typography";
 export * from "./util";
