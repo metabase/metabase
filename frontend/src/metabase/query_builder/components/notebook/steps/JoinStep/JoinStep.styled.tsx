04cb662d0303a711a707dff361fed92b90a497a1
Anton Kulyk <kuliks.anton@gmail.com>
2023-08-11 13:40:10 +0100

    MLv2 Joins 4 — Multiple conditions (#32906)
    * Implement multiple conditions
    
    * Fix tests
    
    * Remove irrelevant test
    
    * Extract `JoinConditionRightPart`
    
    * Fix temporal units sync for join columns

--- frontend/src/metabase/query_builder/components/notebook/steps/JoinStep/JoinStep.styled.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/JoinStep/JoinStep.styled.tsx
@@ -1,7 +1,36 @@
 import styled from "@emotion/styled";
+import { color } from "metabase/lib/colors";
+import IconButtonWrapper from "metabase/components/IconButtonWrapper/IconButtonWrapper";
 import { NotebookCell } from "../../NotebookCell";
 
+export const TablesNotebookCell = styled(NotebookCell)`
+  flex: 1;
+  align-self: start;
+`;
+
 export const ConditionNotebookCell = styled(NotebookCell)`
-  flex: 1 0 auto;
+  flex: 1;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  gap: 8px;
+
   padding: 8px;
+`;
+
+export const ConditionUnionLabel = styled.span`
+  display: block;
+  color: ${color("text-medium")};
+  font-weight: bold;
+  margin-left: 4px;
+`;
+
+export const RemoveConditionButton = styled(IconButtonWrapper)`
+  margin-left: 8px;
+
+  color: ${color("text-light")};
+
+  &:hover {
+    color: ${color("text-medium")};
+  }
 `;
