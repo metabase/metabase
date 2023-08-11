a379a4348229378c2143c8750fbbb4b5a2bf7c46
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-08-11 19:27:37 +0700

    Add missing translations in admin audit details

--- enterprise/frontend/src/metabase-enterprise/audit_app/pages/AuditUserDetail.jsx
+++ enterprise/frontend/src/metabase-enterprise/audit_app/pages/AuditUserDetail.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import { t } from "ttag";
+
 import EntityName from "metabase/entities/containers/EntityName";
 import AuditContent from "../components/AuditContent";
 import AuditDashboard from "../containers/AuditDashboard";
@@ -54,24 +56,24 @@ const AuditUserDownloadsTab = ({ userId }) => (
 );
 
 AuditUserDetail.tabs = [
-  { path: "activity", title: "Activity", component: AuditUserActivityTab },
+  { path: "activity", title: t`Activity`, component: AuditUserActivityTab },
   { path: "details", title: "Account details" },
   { path: "data_permissions", title: "Data permissions" },
   { path: "collection_permissions", title: "Collection permissions" },
   { path: "made_by", title: "Made by them" },
   {
     path: "query_views",
-    title: "Query views",
+    title: t`Query views`,
     component: AuditUserQueryViewsTab,
   },
   {
     path: "dashboard_views",
-    title: "Dashboard views",
+    title: t`Dashboard views`,
     component: AuditUserDashboardViewsTab,
   },
   {
     path: "downloads",
-    title: "Downloads",
+    title: t`Downloads`,
     component: AuditUserDownloadsTab,
   },
 ];
