5d3fec4510d63994f03fd1d925b53612ae0fd4b8
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-11 18:43:57 +0300

    Add realistic menu item content

--- frontend/src/metabase/ui/components/index.ts
+++ frontend/src/metabase/ui/components/index.ts
@@ -2,7 +2,7 @@ export * from "./buttons";
 export * from "./data-display";
 export * from "./inputs";
 export * from "./layout";
-export * from "./layovers";
+export * from "./overlays";
 export * from "./theme";
 export * from "./typography";
 export * from "./util";
