99e1b6405d260be3cda9543fabc33d6038a69f4e
Ryan Laurie <iethree@gmail.com>
2023-08-14 14:32:29 -0600

    Round small values to zero with no min/max difference on the Y scale

--- frontend/src/metabase/visualizations/lib/apply_axis.js
+++ frontend/src/metabase/visualizations/lib/apply_axis.js
@@ -352,7 +352,10 @@ export function applyChartOrdinalXAxis(
 // The tolerance is arbitrarily set to one millionth of the yExtent.
 const TOLERANCE_TO_Y_EXTENT = 1e6;
 export function maybeRoundValueToZero(value, [yMin, yMax]) {
-  const tolerance = Math.abs(yMax - yMin) / TOLERANCE_TO_Y_EXTENT;
+  const tolerance =
+    yMin !== yMax
+      ? Math.abs(yMax - yMin) / TOLERANCE_TO_Y_EXTENT
+      : 1 / TOLERANCE_TO_Y_EXTENT;
   return Math.abs(value) < tolerance ? 0 : value;
 }
 
