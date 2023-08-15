b6062f171e1c45342a55c447a9963be963cfc648
Kamil Mielnik <kamil@kamilmielnik.com>
2023-08-15 17:55:02 +0700

    Add a basic rendering test for TableList

--- frontend/src/metabase/reference/databases/TableList.unit.spec.tsx
+++ frontend/src/metabase/reference/databases/TableList.unit.spec.tsx
@@ -1,20 +1,71 @@
 import { screen } from "@testing-library/react";
 import { Route } from "react-router";
+
+import { createMockEntitiesState } from "__support__/store";
 import { renderWithProviders } from "__support__/ui";
+import { getNextId } from "__support__/utils";
+import { createMockDatabase, createMockTable } from "metabase-types/api/mocks";
+import { createMockState } from "metabase-types/store/mocks";
 
 import TableList from "./TableList";
 
+const databaseId = getNextId();
+
+const incompleteTable = createMockTable({
+  id: getNextId(),
+  db_id: databaseId,
+  display_name: "Incomplete table",
+  initial_sync_status: "incomplete",
+});
+
+const abortedTable = createMockTable({
+  id: getNextId(),
+  db_id: databaseId,
+  display_name: "Aborted table",
+  initial_sync_status: "aborted",
+});
+
+const completeTable = createMockTable({
+  id: getNextId(),
+  db_id: databaseId,
+  display_name: "Complete table",
+  initial_sync_status: "complete",
+});
+
+const database = createMockDatabase({
+  id: databaseId,
+  name: "Test Database",
+  tables: [incompleteTable, abortedTable, completeTable],
+});
+
+const storeInitialState = createMockState({
+  entities: createMockEntitiesState({
+    databases: [database],
+    tables: [incompleteTable, abortedTable, completeTable],
+  }),
+});
+
+const defaultProps = {
+  params: { databaseId },
+  style: {},
+};
+
 function setup({ ...options } = {}) {
   return renderWithProviders(
-    <Route path="/" component={() => <TableList {...options} />} />,
-    { withRouter: true },
+    <Route
+      path="/"
+      component={() => <TableList {...defaultProps} {...options} />}
+    />,
+    { storeInitialState, withRouter: true },
   );
 }
 
 describe("TableList", () => {
-  it("should render", () => {
+  it("should render all tables", () => {
     setup();
 
-    expect(screen.getByText("Tables")).toBeInTheDocument();
+    expect(screen.getByText(incompleteTable.display_name)).toBeInTheDocument();
+    expect(screen.getByText(abortedTable.display_name)).toBeInTheDocument();
+    expect(screen.getByText(completeTable.display_name)).toBeInTheDocument();
   });
 });
