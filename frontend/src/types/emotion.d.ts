975c3837201c940ba2ce925ce027cf941dac6557
Oisin Coveney <oisin@metabase.com>
2023-08-15 15:31:38 +0300

    Add example usage with

--- frontend/src/types/emotion.d.ts
+++ frontend/src/types/emotion.d.ts
@@ -1,48 +1,7 @@
-import "ace-builds";
+import "@emotion/react";
+import type { MantineTheme } from "@mantine/core";
 
-declare module "ace-builds" {
-  namespace Ace {
-    interface CommandManager {
-      // "commandKeyBinding" is not typed, but used in the code, and it works. So, adding an explicit typing here
-      commandKeyBinding: Ace.CommandMap;
-    }
-    interface TextInput {
-      getElement(): HTMLTextAreaElement;
-    }
-    interface Editor {
-      completer?: {
-        popup?: {
-          isOpen: boolean;
-        };
-      };
-    }
-    interface EditSession {
-      $modeId: string;
-      $mode: {
-        $behaviour: unknown;
-        $highlightRules: {
-          $rules: {
-            start: { token: string; regex: string; onMatch: null }[];
-          };
-        };
-        $tokenizer?: unknown;
-
-        getTokenizer: () => Tokenizer;
-      };
-
-      bgTokenizer: Tokenizer & {
-        start: (index: number) => void;
-        setTokenizer: (tokenizer: Tokenizer) => void;
-      };
-
-      gutterRenderer: {
-        getWidth(
-          session: Ace.EditSession,
-          lastLineNumber: number,
-          config: { characterWidth: number },
-        ): number;
-        getText: (session: Ace.EditSession, row: number) => number;
-      };
-    }
-  }
+declare module "@emotion/react" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface Theme extends MantineTheme {}
 }
