223ca53ebc7a13a380dd995ce481a1e1d7488f80
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-15 15:02:13 +0300

    Merge master

--- frontend/src/metabase/ui/components/theme/ThemeProvider/ThemeProvider.tsx
+++ frontend/src/metabase/ui/components/theme/ThemeProvider/ThemeProvider.tsx
@@ -1,12 +1,21 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { MantineProvider } from "@mantine/core";
+import { withEmotionCache } from "@emotion/react";
+import type { EmotionCache } from "@emotion/react";
 import { getThemeOverrides } from "../../../theme";
 
 interface ThemeProviderProps {
   children: ReactNode;
 }
 
-export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const theme = getThemeOverrides();
-  return <MantineProvider theme={theme}>{children}</MantineProvider>;
-};
+export const ThemeProvider = withEmotionCache(
+  ({ children }: ThemeProviderProps, cache: EmotionCache) => {
+    const theme = getThemeOverrides();
+
+    return (
+      <MantineProvider theme={theme} emotionCache={cache}>
+        {children}
+      </MantineProvider>
+    );
+  },
+);
