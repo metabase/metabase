a7ea35e08b551476c0ee56007acc18142e42b476
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-15 12:21:11 +0300

    Move styles to component

---
+++ frontend/src/metabase/ui/components/overlays/Menu/theme.ts
@@ -0,0 +1,53 @@
+import { getStylesRef } from "@mantine/core";
+import type { MantineTheme } from "@mantine/core";
+
+export const getMenuOverrides = () => ({
+  Menu: {
+    defaultProps: {
+      radius: "sm",
+      shadow: "md",
+    },
+    styles: (theme: MantineTheme) => ({
+      dropdown: {
+        padding: "0.75rem !important",
+        minWidth: "11.5rem",
+      },
+      item: {
+        color: theme.colors.text[2],
+        fontSize: theme.fontSizes.md,
+        fontWeight: 700,
+        padding: theme.spacing.md,
+
+        "&:hover, &:focus": {
+          color: theme.colors.brand[1],
+          backgroundColor: theme.colors.bg[0],
+
+          [`& .${getStylesRef("itemRightSection")}`]: {
+            color: theme.colors.brand[1],
+          },
+        },
+      },
+      itemIcon: {
+        marginRight: "0.75rem",
+      },
+      itemRightSection: {
+        ref: getStylesRef("itemRightSection"),
+        color: theme.colors.text[0],
+        marginLeft: "0.75rem",
+      },
+      label: {
+        color: theme.colors.text[0],
+        fontSize: theme.fontSizes.md,
+        fontWeight: 700,
+        padding: `0.375rem ${theme.spacing.md}`,
+      },
+      divider: {
+        marginTop: theme.spacing.sm,
+        marginBottom: theme.spacing.sm,
+        marginLeft: theme.spacing.md,
+        marginRight: theme.spacing.md,
+        borderTopColor: theme.colors.border[0],
+      },
+    }),
+  },
+});
