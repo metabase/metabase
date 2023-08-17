c1d78789fb9d2c21c8243f44de0396ded0203453
Kamil Mielnik <kamil@kamilmielnik.com>
2023-08-17 20:08:21 +0700

    Manually backport #33152

--- frontend/src/metabase/components/ListItem/ListItem.jsx
+++ frontend/src/metabase/components/ListItem/ListItem.jsx
@@ -1,47 +1,68 @@
 /* eslint "react/prop-types": "warn" */
-import { memo } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router";
 import cx from "classnames";
-import Ellipsified from "metabase/core/components/Ellipsified";
+import PropTypes from "prop-types";
+import { memo } from "react";
 import Card from "metabase/components/Card";
 import S from "metabase/components/List/List.css";
 import { Icon } from "metabase/core/components/Icon";
+import { ListItemLink, ListItemName, Root } from "./ListItem.styled";
 
-const ListItem = ({ name, description, placeholder, url, icon }) => (
-  <li className="relative">
-    <Link to={url} className="text-brand-hover">
-      <Card hoverable className="mb2 p3 bg-white rounded bordered">
-        <div className={cx(S.item)}>
-          <div className={S.itemIcons}>
-            {icon && <Icon className={S.chartIcon} name={icon} size={16} />}
-          </div>
-          <div className={S.itemBody}>
-            <div className={S.itemTitle}>
-              <Ellipsified
-                className={S.itemName}
-                tooltip={name}
-                tooltipMaxWidth="100%"
-              >
-                <h3>{name}</h3>
-              </Ellipsified>
-            </div>
-            {(description || placeholder) && (
-              <div className={cx(S.itemSubtitle)}>
-                {description || placeholder}
-              </div>
-            )}
-          </div>
+const ListItem = ({
+  "data-testid": dataTestId,
+  name,
+  url,
+  description,
+  disabled,
+  placeholder,
+  icon,
+}) => {
+  const card = (
+    <Card hoverable className="mb2 p3 bg-white rounded bordered">
+      <div className={cx(S.item)}>
+        <div className={S.itemIcons}>
+          {icon && <Icon className={S.chartIcon} name={icon} size={16} />}
         </div>
-      </Card>
-    </Link>
-  </li>
-);
+        <div className={S.itemBody}>
+          <div className={S.itemTitle}>
+            <ListItemName
+              className={S.itemName}
+              tooltip={name}
+              tooltipMaxWidth="100%"
+            >
+              <h3>{name}</h3>
+            </ListItemName>
+          </div>
+          {(description || placeholder) && (
+            <div className={cx(S.itemSubtitle)}>
+              {description || placeholder}
+            </div>
+          )}
+        </div>
+      </div>
+    </Card>
+  );
+
+  if (disabled) {
+    return (
+      <Root data-testid={dataTestId} disabled>
+        {card}
+      </Root>
+    );
+  }
+
+  return (
+    <Root data-testid={dataTestId}>
+      <ListItemLink to={url}>{card}</ListItemLink>
+    </Root>
+  );
+};
 
 ListItem.propTypes = {
+  "data-testid": PropTypes.string,
   name: PropTypes.string.isRequired,
   url: PropTypes.string,
   description: PropTypes.string,
+  disabled: PropTypes.bool,
   placeholder: PropTypes.string,
   icon: PropTypes.string,
 };
