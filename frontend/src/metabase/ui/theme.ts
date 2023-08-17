9ffc1fa141d8d34843e07b6b2ccdd8145004a530
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-17 18:23:31 +0300

    Add story

--- frontend/src/metabase/ui/theme.ts
+++ frontend/src/metabase/ui/theme.ts
@@ -1,4 +1,5 @@
 import type { MantineThemeOverride } from "@mantine/core";
+import { rem } from "@mantine/core";
 import { color } from "metabase/lib/colors";
 import {
   getAccordionOverrides,
@@ -36,11 +37,31 @@ export const getThemeOverrides = (): MantineThemeOverride => ({
     xl: "40px",
   },
   fontSizes: {
-    xs: "11px",
-    sm: "12px",
-    md: "14px",
-    lg: "17px",
-    xl: "21px",
+    xs: rem(11),
+    sm: rem(12),
+    md: rem(14),
+    lg: rem(17),
+    xl: rem(21),
+  },
+  headings: {
+    sizes: {
+      h1: {
+        fontSize: rem(24),
+        lineHeight: rem(24),
+      },
+      h2: {
+        fontSize: rem(20),
+        lineHeight: rem(24),
+      },
+      h3: {
+        fontSize: rem(14),
+        lineHeight: rem(16),
+      },
+      h4: {
+        fontSize: rem(14),
+        lineHeight: rem(16),
+      },
+    },
   },
   fontFamily: 'Lato, "Helvetica Neue", Helvetica, sans-serif',
   fontFamilyMonospace: "Monaco, monospace",
