a9bf4e52bdf3c614118b340e2758c327dcd3ad3a
Emmad Usmani <emmadusmani@berkeley.edu>
2023-08-21 13:29:02 -0700

    interactive total WIP

--- frontend/src/metabase/visualizations/echarts/visualizations/PieChart/mixins.ts
+++ frontend/src/metabase/visualizations/echarts/visualizations/PieChart/mixins.ts
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import type { EChartsMixin } from "metabase/visualizations/types";
 
 export const pieSeriesMixin: EChartsMixin = ({ option, props }) => {
@@ -13,3 +15,45 @@ export const pieSeriesMixin: EChartsMixin = ({ option, props }) => {
 
   return { option };
 };
+
+// Will later use this changing total on hover
+export function usePieTotalMixin() {
+  const [text, setText] = useState("");
+
+  const pieTotalMixin: EChartsMixin = ({ option }) => {
+    // TODO fix any type
+    const mouseoverHandler = (event: any) => {
+      setText(`${event.data.name} - ${event.data.value}`);
+    };
+    const mouseoutHandler = (event: any) => {
+      setText("Total...");
+    };
+
+    option.graphic = {
+      type: "text",
+      left: "center",
+      top: "center",
+      // TODO styles
+      style: {
+        fill: "#000",
+        font: "bold 26px sans-serif",
+        text: text || "Total...",
+      },
+    };
+    option.hoverLayerThreshold = 0;
+
+    return {
+      option,
+      eventHandlers: [
+        {
+          eventName: "mouseover",
+          query: "series.sunburst",
+          handler: mouseoverHandler,
+        },
+        { eventName: "mouseout", handler: mouseoutHandler },
+      ],
+    };
+  };
+
+  return pieTotalMixin;
+}
