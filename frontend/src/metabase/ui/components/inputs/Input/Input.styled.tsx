a3f0598355788ea38789488ca5f56d2e65e55b02
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-22 14:53:04 +0300

    Add basic input overrides

--- frontend/src/metabase/ui/components/inputs/Input/Input.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Input/Input.styled.tsx
@@ -2,6 +2,29 @@ import type { MantineThemeOverride } from "@mantine/core";
 
 export const getInputOverrides = (): MantineThemeOverride["components"] => ({
   Input: {
+    styles: theme => ({
+      wrapper: {
+        display: "flex",
+        border: `1px solid ${theme.colors.border[0]}`,
+        "&:focus-within": {
+          borderColor: theme.colors.brand[2],
+        },
+      },
+      input: {
+        flex: 1,
+        border: "none",
+      },
+      icon: {
+        display: "block",
+        color: theme.colors.text[2],
+      },
+      rightSection: {
+        display: "block",
+        color: theme.colors.text[0],
+        fontSize: theme.fontSizes.xs,
+        lineHeight: "1rem",
+      },
+    }),
     sizes: {
       md: theme => ({
         input: {
