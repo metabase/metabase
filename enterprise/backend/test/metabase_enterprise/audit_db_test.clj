b13f3d3e342d29ae64d3fd929669c83cc7ce5b13
Case Nelson <case@metabase.com>
2023-08-22 14:09:24 -0600

    [AuditV2] Load instance analytics into h2 (#33286)
    * [AuditV2] Allow loading instance analytics into h2
    
    Part of #31632
    
    This is an interesting run around the problem of h2 normally treating identifiers as case sensitive. H2 v 2 introduced CASE_INSENSITIVE_IDENTIFIERS https://www.h2database.com/javadoc/org/h2/engine/DbSettings.html
    
    Normally, when you create table foobar in H2, that table will be created with uppercase identifiers FOOBAR however, when you select from foobar the table will not be found as identifiers need to match the case in which they are stored during queries by default. This setting allows unquoted identifiers in queries to match table and column names in a case insensitive way.
    
    What this means, is that we can serialize from a postgres db and restore in an h2 db. Table and field names will be loaded as lowercase, and the queries will be built with lowercase identifiers but still match the created uppercase tables and field names. This is good because it should allow native queries to be portable.
    
    Unfortunately, the setting does not work for schema identifiers. So before and after loading we have to update the schema names to lower then upper case.
    
    * Uncomment ia loading
    
    * Stop using the zip
    
    * Cleanup after test
    
    * Remove instance analytics zipfile
    
    * Adjust H2 table and field names before sync and after loading
    
    * Fix queries for updating fields
    
    * Address review comments
    
    * Fix indent
    
    * Fix linter
    
    * Fix tests
    
    * Fix linter

--- enterprise/backend/test/metabase_enterprise/audit_db_test.clj
+++ enterprise/backend/test/metabase_enterprise/audit_db_test.clj
@@ -1,6 +1,5 @@
 (ns metabase-enterprise.audit-db-test
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
             [clojure.test :refer [deftest is]]
             [metabase-enterprise.audit-db :as audit-db]
             [metabase.models.database :refer [Database]]
@@ -18,7 +17,7 @@
            (#'metabase.core/ensure-audit-db-installed!))))))
 
 (deftest audit-db-is-installed-then-left-alone
-  (mt/test-drivers #{:postgres}
+  (mt/test-drivers #{:postgres :h2}
     (with-audit-db-restoration
       (t2/delete! Database :is_audit true)
       (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-db-installed!)))
@@ -29,7 +28,7 @@
       (is (= :metabase-enterprise.audit-db/no-op (audit-db/ensure-db-installed!))))))
 
 (deftest audit-db-content-is-not-installed-when-not-found
-  (mt/test-drivers #{:postgres}
+  (mt/test-drivers #{:postgres :h2}
     (with-audit-db-restoration
       (with-redefs [audit-db/analytics-root-dir-resource nil]
         (is (= nil audit-db/analytics-root-dir-resource))
@@ -40,10 +39,9 @@
             "No cards created for Audit DB.")))))
 
 (deftest audit-db-content-is-installed-when-found
-  (mt/test-drivers #{:postgres}
+  (mt/test-drivers #{:postgres :h2}
     (with-audit-db-restoration
-      (with-redefs [audit-db/analytics-root-dir-resource (io/resource "instance_analytics.zip")]
-        (is (str/ends-with? (str audit-db/analytics-root-dir-resource) ".zip"))
+      (with-redefs [audit-db/analytics-root-dir-resource (io/resource "instance_analytics")]
         (is (= :metabase-enterprise.audit-db/installed (audit-db/ensure-audit-db-installed!)))
         (is (= 13371337 (t2/select-one-fn :id 'Database {:where [:= :is_audit true]}))
             "Audit DB is installed.")
