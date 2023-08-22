9a9e388a1727958636a95c447f8a62ef925c979e
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2023-08-22 18:49:23 -0400

    Ignore internal user when reading has-user-setup setting (#33421)
    * Ignore internal user when reading has-user-setup setting
    
    * clean ns

--- test/metabase/setup_test.clj
+++ test/metabase/setup_test.clj
@@ -1,12 +1,24 @@
 (ns ^:mb/once metabase.setup-test
   (:require
    [clojure.test :refer :all]
+   [metabase.core :as mbc]
    [metabase.db :as mdb]
    [metabase.db.schema-migrations-test.impl
     :as schema-migrations-test.impl]
    [metabase.setup :as setup]
    [metabase.test :as mt]
    [toucan2.core :as t2]))
+
+(deftest has-user-setup-test
+  (testing "The has-user-setup getter should return falsey for an empty instance with only an internal user"
+    ;; create a new completely empty database.
+    (schema-migrations-test.impl/with-temp-empty-app-db [_conn :h2]
+      ;; make sure the DB is set up
+      (mdb/setup-db!)
+      ;; install audit DB, which creates an internal user as a side effect (on EE instances))
+      (mbc/ensure-audit-db-installed!)
+      (is (= false
+             (setup/has-user-setup))))))
 
 (deftest has-user-setup-cached-test
   (testing "The has-user-setup getter should cache truthy results since it can never become falsey"
