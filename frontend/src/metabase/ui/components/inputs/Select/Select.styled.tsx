ec4cc9f04f51f4a61168c36300139d6529297241
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-25 18:54:11 +0300

    Add select

--- frontend/src/metabase/ui/components/inputs/Select/Select.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Select/Select.styled.tsx
@@ -5,5 +5,21 @@ export const getSelectOverrides = (): MantineThemeOverride["components"] => ({
     defaultProps: {
       withinPortal: true,
     },
+    styles: theme => ({
+      itemsWrapper: {
+        padding: "0.75rem",
+      },
+      item: {
+        color: theme.colors.text[2],
+        fontSize: theme.fontSizes.md,
+        fontWeight: 700,
+        lineHeight: theme.lineHeight,
+        padding: theme.spacing.md,
+        "&:hover, &:focus": {
+          color: theme.colors.brand[1],
+          backgroundColor: theme.colors.bg[0],
+        },
+      },
+    }),
   },
 });
