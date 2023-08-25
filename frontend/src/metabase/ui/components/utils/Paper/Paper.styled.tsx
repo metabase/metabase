01efdd17cabb324d45021c099595a0e5a3a17708
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-25 20:40:21 +0300

    Export paper

---
+++ frontend/src/metabase/ui/components/utils/Paper/Paper.styled.tsx
@@ -0,0 +1,15 @@
+import type { MantineThemeOverride, PaperStylesParams } from "@mantine/core";
+
+export const getPaperOverrides = (): MantineThemeOverride["components"] => ({
+  Paper: {
+    styles: (theme, params: PaperStylesParams) => ({
+      root: {
+        color: theme.colors.text[2],
+        backgroundColor: theme.white,
+        "&[data-with-border]": {
+          borderColor: theme.colors.border[0],
+        },
+      },
+    }),
+  },
+});
