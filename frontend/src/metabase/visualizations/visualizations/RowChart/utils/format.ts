45b6424b6f5c8902ac3612b7634f05c4958a41c0
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2023-08-28 08:05:30 -0600

    Fix null series title in line charts (#33331)
    * show empty value for breakouts in settings and legend
    
    * add and update tests
    
    * handle empty column breakouts in rowcharts
    
    * show legend for single cardinality breakouts

--- frontend/src/metabase/visualizations/visualizations/RowChart/utils/format.ts
+++ frontend/src/metabase/visualizations/visualizations/RowChart/utils/format.ts
@@ -12,6 +12,8 @@ import type {
 import { getStackOffset } from "metabase/visualizations/lib/settings/stacking";
 import { getLabelsMetricColumn } from "metabase/visualizations/shared/utils/series";
 import { formatValue } from "metabase/lib/formatting";
+import { NULL_DISPLAY_VALUE } from "metabase/lib/constants";
+import { isEmpty } from "metabase/lib/validate";
 
 export const getFormatters = (
   chartColumns: ChartColumns,
@@ -82,5 +84,7 @@ export const getLabelsFormatter = (
 
 export const getColumnValueFormatter = () => {
   return (value: RowValue, column: DatasetColumn) =>
-    String(formatValue(value, { column }));
+    isEmpty(value)
+      ? NULL_DISPLAY_VALUE
+      : String(formatValue(value, { column }));
 };
