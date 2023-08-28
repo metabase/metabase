0fe19c9e76a402c62b109d2be600d16508159fb6
Cam Saul <github@camsaul.com>
2023-08-28 17:48:30 +0000

    Fix `required-native-extras` for Cljs and add TypeScript wrappers

---
+++ frontend/src/metabase-lib/native.ts
@@ -0,0 +1,34 @@
+import * as ML from "cljs/metabase.lib.js";
+
+import type { DatabaseId } from "metabase-types/api";
+
+import type { MetadataProvider, Query } from "./types";
+
+/**
+ * Returns the extra keys that are required for this database's native queries, for example `:collection` name is
+ *  needed for MongoDB queries.
+ */
+export function requiredNativeExtras(
+  databaseId: DatabaseId,
+  metadata: MetadataProvider,
+): string[] {
+  return ML.required_native_extras(databaseID, metadata);
+}
+
+type NativeExtras = {
+  collection?: string | null;
+};
+
+/**
+ * Returns the extra keys for native queries associated with this query.
+ */
+export function nativeExtras(query: Query): NativeExtras | null;
+
+/**
+ * Updates the extras required for the db to run this query. The first stage must be a native type. Will ignore extras
+ * not in `required-native-extras`.
+ */
+export function withNativeExtras(
+  query: Query,
+  nativeExtras: NativeExtras | null,
+): Query;
