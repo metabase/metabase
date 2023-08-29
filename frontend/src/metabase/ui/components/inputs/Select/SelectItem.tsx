c0891060c24a3b018ce0158d9cedb68c10fc151d
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-29 17:35:09 +0300

    Add stories

--- frontend/src/metabase/ui/components/inputs/Select/SelectItem.tsx
+++ frontend/src/metabase/ui/components/inputs/Select/SelectItem.tsx
@@ -1,16 +1,21 @@
 import { forwardRef } from "react";
+import { Group, Text } from "@mantine/core";
 import type { ComponentPropsWithoutRef } from "react";
+import { Icon } from "metabase/core/components/Icon";
+import type { IconName } from "metabase/core/components/Icon";
 
 interface SelectItemProps extends ComponentPropsWithoutRef<"div"> {
   label: string;
+  icon?: IconName;
 }
 
 export const SelectItem = forwardRef<HTMLDivElement, SelectItemProps>(
-  function SelectItem({ label, ...others }: SelectItemProps, ref) {
+  function SelectItem({ label, icon, ...others }: SelectItemProps, ref) {
     return (
-      <div ref={ref} {...others}>
-        {label}
-      </div>
+      <Group ref={ref} spacing="0.75rem" {...others}>
+        {icon && <Icon name={icon} />}
+        <Text>{label}</Text>
+      </Group>
     );
   },
 );
