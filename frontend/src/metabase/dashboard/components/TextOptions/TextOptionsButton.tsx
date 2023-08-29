e574eacb16af622cfb8314023af99df05fc8e2de
Ryan Laurie <iethree@gmail.com>
2023-08-29 17:01:46 -0600

    Update enterprise tests for DashboardHeader

--- frontend/src/metabase/dashboard/components/TextOptions/TextOptionsButton.tsx
+++ frontend/src/metabase/dashboard/components/TextOptions/TextOptionsButton.tsx
@@ -3,7 +3,7 @@ import { t } from "ttag";
 import { Icon } from "metabase/core/components/Icon";
 import EntityMenu from "metabase/components/EntityMenu";
 
-import { DashboardHeaderButton } from "metabase/dashboard/containers/DashboardHeader.styled";
+import { DashboardHeaderButton } from "metabase/dashboard/components/DashboardHeader/DashboardHeader.styled";
 
 import { IconContainer } from "./TextOptionsButton.styled";
 
