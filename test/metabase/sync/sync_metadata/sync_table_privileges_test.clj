8d475b1d8b5a0d633f7a2bbb6dffc32d5e9f9066
Callum Herries <hi@callumherries.com>
2023-08-29 11:36:58 +0300

    Remove with-model-cleanup

--- test/metabase/sync/sync_metadata/sync_table_privileges_test.clj
+++ test/metabase/sync/sync_metadata/sync_table_privileges_test.clj
@@ -16,19 +16,18 @@
   ;; when table_privileges are implemented for all the actions drivers
   (mt/test-drivers #{:postgres}
     (testing "`TablePrivileges` should store the correct data for current_user and role privileges for postgres"
-      (mt/with-model-cleanup [:model/Field :model/Table :model/Database :model/TablePrivileges]
-       (mt/with-empty-db
-         (let [conn-spec (sql-jdbc.conn/db->pooled-connection-spec (mt/db))]
-           (jdbc/execute! conn-spec (str "CREATE SCHEMA foo; "
-                                         "CREATE TABLE foo.baz (id INTEGER);"))
-           (sync-tables/sync-tables-and-database! (mt/db))
-           (sync-table-privileges/sync-table-privileges! (mt/db))
-           (let [table-id (t2/select-one-pk :model/Table :name "baz" :schema "foo")]
-             (is (=? [{:table_id        table-id
-                       :role            string? ; this should be the current user, but it doesn't matter until we need table_privileges to work with connection impersonation
-                       :is_current_user true
-                       :select          true
-                       :delete          true
-                       :insert          true
-                       :update          true}]
-                     (t2/select :model/TablePrivileges :table_id table-id :is_current_user true))))))))))
+      (mt/with-empty-db
+        (let [conn-spec (sql-jdbc.conn/db->pooled-connection-spec (mt/db))]
+          (jdbc/execute! conn-spec (str "CREATE SCHEMA foo; "
+                                        "CREATE TABLE foo.baz (id INTEGER);"))
+          (sync-tables/sync-tables-and-database! (mt/db))
+          (sync-table-privileges/sync-table-privileges! (mt/db))
+          (let [table-id (t2/select-one-pk :model/Table :name "baz" :schema "foo")]
+            (is (=? [{:table_id        table-id
+                      :role            string? ; this should be the current user, but it doesn't matter until we need table_privileges to work with connection impersonation
+                      :is_current_user true
+                      :select          true
+                      :delete          true
+                      :insert          true
+                      :update          true}]
+                    (t2/select :model/TablePrivileges :table_id table-id :is_current_user true)))))))))
