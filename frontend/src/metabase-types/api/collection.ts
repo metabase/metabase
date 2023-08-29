3125630ee794174e4ad8d32d06c0cb42639dd76f
Ryan Laurie <iethree@gmail.com>
2023-08-29 16:17:08 -0600

    moving instance analytics logic to enterprise (#31484)
    * moving instance analytics logic to enterprise
    
    * tests
    
    * moving the tooltip to a dedicated EE component
    
    * Consolidating Dashboard header test files
    # Conflicts:
    #	enterprise/frontend/src/metabase-enterprise/collections/index.ts
    #	enterprise/frontend/src/metabase-enterprise/collections/utils.ts
    #	frontend/src/metabase-types/api/collection.ts
    #	frontend/src/metabase/collections/components/CollectionHeader/CollectionCaption.tsx
    #	frontend/src/metabase/collections/utils.ts
    #	frontend/src/metabase/dashboard/components/DashboardHeader/DashboardHeader.tsx
    #	frontend/src/metabase/dashboard/containers/DashboardHeader.unit.spec.tsx
    #	frontend/src/metabase/entities/collections/utils.ts
    #	frontend/src/metabase/plugins/index.ts
    #	frontend/src/metabase/search/components/InfoText.jsx

--- frontend/src/metabase-types/api/collection.ts
+++ frontend/src/metabase-types/api/collection.ts
@@ -1,3 +1,4 @@
+import type { IconName } from "metabase/core/components/Icon";
 import type { UserId } from "./user";
 import type { CardDisplayType } from "./card";
 import type { DatabaseId } from "./database";
@@ -10,10 +11,20 @@ export type CollectionContentModel = "card" | "dataset";
 
 export type CollectionAuthorityLevel = "official" | null;
 
+export type CollectionType = "instance-analytics" | null;
+
 export type CollectionAuthorityLevelConfig = {
   type: CollectionAuthorityLevel;
   name: string;
-  icon: string;
+  icon: IconName;
+  color?: string;
+  tooltips?: Record<string, string>;
+};
+
+export type CollectionInstanceAnaltyicsConfig = {
+  type: CollectionType;
+  name: string;
+  icon: IconName;
   color?: string;
   tooltips?: Record<string, string>;
 };
