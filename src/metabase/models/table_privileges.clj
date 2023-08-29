147f5cef669c8f8d13cfdb4b93750289085453f7
Callum Herries <hi@callumherries.com>
2023-08-29 10:55:45 +0300

    Remove TablePrivileges def

--- src/metabase/models/table_privileges.clj
+++ src/metabase/models/table_privileges.clj
@@ -7,11 +7,6 @@
    [methodical.core :as methodical]
    [toucan2.core :as t2]))
 
-(def TablePrivileges
-  "Used to be the toucan1 model name defined using [[toucan.models/defmodel]], now it's a reference to the toucan2 model name.
-  We'll keep this until we replace all the symbols in our codebase."
-  :model/TablePrivileges)
-
 (methodical/defmethod t2/table-name :model/TablePrivileges [_model] :table_privileges)
 
 (derive :model/TablePrivileges :metabase/model)
