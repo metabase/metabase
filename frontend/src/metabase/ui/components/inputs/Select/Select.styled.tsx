67c0bb4c9a46fe5132e59d36b86c02685ed79c1f
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-08-29 15:57:19 +0300

    Add stories

--- frontend/src/metabase/ui/components/inputs/Select/Select.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Select/Select.styled.tsx
@@ -6,8 +6,25 @@ export const getSelectOverrides = (): MantineThemeOverride["components"] => ({
     defaultProps: {
       withinPortal: true,
       itemComponent: SelectItem,
+      clearButtonProps: {
+        color: "text.2",
+      },
     },
     styles: theme => ({
+      rightSection: {
+        svg: {
+          width: "1rem",
+          height: "1rem",
+          "&[data-chevron]": {
+            path: {
+              d: 'path("M1.47 4.47a.75.75 0 0 1 1.06 0L8 9.94l5.47-5.47a.75.75 0 1 1 1.06 1.06l-6 6a.75.75 0 0 1-1.06 0l-6-6a.75.75 0 0 1 0-1.06z")',
+            },
+          },
+          "&:not([data-chevron])": {
+            d: 'path("M4.53 3.47a.75.75 0 0 0-1.06 1.06L6.94 8l-3.47 3.47a.75.75 0 1 0 1.06 1.06L8 9.06l3.47 3.47a.75.75 0 1 0 1.06-1.06L9.06 8l3.47-3.47a.75.75 0 0 0-1.06-1.06L8 6.94 4.53 3.47z")',
+          },
+        },
+      },
       itemsWrapper: {
         padding: "0.75rem",
       },
