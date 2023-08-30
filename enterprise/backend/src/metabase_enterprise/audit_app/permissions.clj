b0d0ebe656b6f4c6e02623a6d894b910beb3f703
Jerry Huang <jhuang37050@gmail.com>
2023-08-30 17:50:38 -0400

    Permissions for audit db

---
+++ enterprise/backend/src/metabase_enterprise/audit_app/permissions.clj
@@ -0,0 +1,29 @@
+(ns metabase-enterprise.audit-app.permissions
+  (:require
+   [metabase-enterprise.audit-db :refer [default-audit-collection-entity-id
+                                         default-audit-db-id]]
+   [metabase.models.permissions :as perms]
+   [metabase.public-settings.premium-features :refer [defenterprise]]
+   [metabase.shared.util.i18n :refer [tru]]
+   [toucan2.core :as t2]))
+
+(def audit-db-view-names
+  "Used for giving granular permissions into the audit db. Instead of granting permissions to
+   all of the audit db, we query the audit db using the names of each view that starts with v_."
+  ["v_users" "v_content" "v_group_members" "v_alerts_subscriptions" "v_activity"])
+
+(defenterprise update-audit-collection-permissions!
+  "Will remove or grant audit db (AppDB) permissions, if the instance analytics permissions changes."
+  :feature :audit-app
+  [group-id changes]
+    (let [audit-collection-id (t2/select-one-fn :id 'Collection :entity_id (default-audit-collection-entity-id))
+          [change-id type]    (first (filter #(= (first %) audit-collection-id) changes))]
+        (when change-id
+          (let [change-permissions! (case type
+                                      :read  perms/grant-permissions!
+                                      :none  perms/delete-related-permissions!
+                                      :write (throw (ex-info (tru (str "Unable to make audit collections writable."))
+                                                             {:status-code 400})))
+                view-tables         (t2/select :model/Table :db_id (default-audit-db-id) :name [:in audit-db-view-names])]
+            (doseq [table view-tables]
+              (change-permissions! group-id (perms/table-query-path table)))))))
