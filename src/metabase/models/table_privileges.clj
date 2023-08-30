3a70010c5fdf28d33702ee7337b7720e3514df77
Cal Herries <39073188+calherries@users.noreply.github.com>
2023-08-30 13:05:11 +0300

    Use role not group in docstring
    Co-authored-by: Ngoc Khuat <qn.khuat@gmail.com>

--- src/metabase/models/table_privileges.clj
+++ src/metabase/models/table_privileges.clj
@@ -1,5 +1,5 @@
 (ns metabase.models.table-privileges
-  "Each row in the `table_privileges` table contains the privileges that the current user or group has on a given table.
+  "Each row in the `table_privileges` table contains the privileges that the current user or role has on a given table.
 
    The `table_privileges` table just a cache of the data returned from driver/table-privileges, but it's stored in the
    database so that we can query it more easily."
