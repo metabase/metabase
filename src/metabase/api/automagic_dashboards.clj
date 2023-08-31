9f365935ae2e91516c29fc4cf1a3b5b988a46a96
dan sutton <dan@dpsutton.com>
2023-08-31 16:47:19 -0500

    better error message for no linked entities
    they used to be in the let-404 so you would get a quite confusing error
    message. Now it's a 400 and will show "No linked entities" if you expand
    the error message. We still need to make this better. But this is an
    obvious improvement on a 404.

--- src/metabase/api/automagic_dashboards.clj
+++ src/metabase/api/automagic_dashboards.clj
@@ -194,7 +194,7 @@
     {query-filter :pk_ref}                       :model-index
     {model-name :name :as model}                 :model
     :keys                                        [linked-tables]}]
-  (if (seq linked-tables)
+  (when (seq linked-tables)
     (let [child-dashboards (map (fn [{:keys [linked-table-id linked-field-id]}]
                                   (let [table (t2/select-one Table :id linked-table-id)
                                         q     (update query-filter
@@ -231,18 +231,7 @@
                    :param_fields  {}})
                 tabs-and-cards)
         (dissoc :transient_name
-                :transient_filters)))
-    {:name          (format "Here's a look at \"%s\" from \"%s\"" indexed-entity-name model-name)
-     :ordered_cards (add-source-model-link
-                      model
-                      [{:row                    0
-                        :col                    0
-                        :size_x                 18
-                        :size_y                 2
-                        :visualization_settings {:text                "# Unfortunately, there's not much else to show right now..."
-                                                 :virtual_card        {:display :text}
-                                                 :dashcard.background false
-                                                 :text.align_vertical :bottom}}])}))
+                :transient_filters)))))
 
 (api/defendpoint GET "/model_index/:model-index-id/primary_key/:pk-id"
   "Return an automagic dashboard for an entity detail specified by `entity`
@@ -250,23 +239,24 @@
   [model-index-id pk-id]
   {model-index-id :int
    pk-id          :int}
-  (api/let-404 [model-index (t2/select-one ModelIndex model-index-id)
-                model (t2/select-one Card (:model_id model-index))
+  (api/let-404 [model-index       (t2/select-one ModelIndex model-index-id)
+                model             (t2/select-one Card (:model_id model-index))
                 model-index-value (t2/select-one ModelIndexValue
                                                  :model_index_id model-index-id
                                                  :model_pk pk-id)]
-               ;; `->entity` does a read check on the model but this is here as well to be extra sure.
-               (let [linked (linked-entities {:model             model
-                                              :model-index       model-index
-                                              :model-index-value model-index-value})]
-                 (api/read-check Card (:model_id model-index))
-                 (or (create-linked-dashboard {:model             model
-                                               :linked-tables     linked
-                                               :model-index       model-index
-                                               :model-index-value model-index-value})
-                     (throw (ex-info (tru "No linked entities")
-                                     {:model-index-id model-index-id
-                                      :status-code    400}))))))
+    ;; `->entity` does a read check on the model but this is here as well to be extra sure.
+    (api/read-check Card (:model_id model-index))
+    ;; don't want to 404 on no linked-entities. Want better error message.
+    (let [linked (linked-entities {:model             model
+                                   :model-index       model-index
+                                   :model-index-value model-index-value})]
+      (or (create-linked-dashboard {:model             model
+                                    :linked-tables     linked
+                                    :model-index       model-index
+                                    :model-index-value model-index-value})
+          (throw (ex-info (tru "No linked entities")
+                          {:model-index-id model-index-id
+                           :status-code    400}))))))
 
 #_{:clj-kondo/ignore [:deprecated-var]}
 (api/defendpoint-schema GET "/:entity/:entity-id-or-query/rule/:prefix/:rule"
