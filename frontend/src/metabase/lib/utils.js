08d65bbcc3348985379da904f6bac98d86d2b03f
Nicolo Pretto <info@npretto.com>
2023-09-01 12:28:52 +0200

    adds typing to compareVersions util function

--- frontend/src/metabase/lib/utils.js
+++ frontend/src/metabase/lib/utils.js
@@ -135,9 +135,15 @@ const MetabaseUtils = {
     return JSON.parse(JSON.stringify(a));
   },
 
-  // this should correctly compare all version formats Metabase uses, e.x.
-  // 0.0.9, 0.0.10-snapshot, 0.0.10-alpha1, 0.0.10-rc1, 0.0.10-rc2, 0.0.10-rc10
-  // 0.0.10, 0.1.0, 0.2.0, 0.10.0, 1.1.0
+  /**
+   * this should correctly compare all version formats Metabase uses, e.x.
+   * 0.0.9, 0.0.10-snapshot, 0.0.10-alpha1, 0.0.10-rc1, 0.0.10-rc2, 0.0.10-rc10
+   * 0.0.10, 0.1.0, 0.2.0, 0.10.0, 1.1.0
+   * @type {{
+   * (aVersion: string, bVersion: string) => number;
+   * (aVersion: string | null | undefined, bVersion: string | null | undefined) => number | null;
+   * }}
+   */
   compareVersions(aVersion, bVersion) {
     if (!aVersion || !bVersion) {
       return null;
