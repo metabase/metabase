b8f14ce2f09ae75a26af4a4156c4e102be798dcf
Noah Moss <noahbmoss@gmail.com>
2023-09-04 15:08:45 -0400

    Don't decode SSO redirect URLs before extracting host

--- enterprise/backend/test/metabase_enterprise/sso/integrations/sso_utils_test.clj
+++ enterprise/backend/test/metabase_enterprise/sso/integrations/sso_utils_test.clj
@@ -9,9 +9,11 @@
       "/test"
       "localhost"
       "localhost:3000"
-      "http://localhost:3000"))
+      "http://localhost:3000"
+      "http://localhost:3000/dashboard/1-test-dashboard?currency=British%20Pound"))
 
   (testing "check-sso-redirect- throws an error for invalid redirect URIs"
     (are [uri] (thrown-with-msg? clojure.lang.ExceptionInfo #"Invalid redirect URL" (sso-utils/check-sso-redirect uri))
       "http://example.com"
-      "//example.com")))
+      "//example.com"
+      "not a url")))
