cc1d1ea07d5329bc5070d4b6a73b1e9f8ff532ca
Nicolo Pretto <info@npretto.com>
2023-09-05 10:41:04 +0200

    moved old test file to be colocated

--- frontend/src/metabase/lib/utils.unit.spec.ts
+++ frontend/src/metabase/lib/utils.unit.spec.ts
@@ -40,7 +40,7 @@ describe("utils", () => {
   });
 });
 
-function shuffle(a) {
+function shuffle(a: string[]) {
   for (let i = a.length; i; i--) {
     const j = Math.floor(Math.random() * i);
     [a[i - 1], a[j]] = [a[j], a[i - 1]];
