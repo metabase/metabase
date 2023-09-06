d9a5ab6b318447d6eea0293477b68f8aece3a38a
Oisin Coveney <oisin@metabase.com>
2023-09-06 17:56:34 +0300

    revert typing for now

--- frontend/src/metabase/hooks/use-list-keyboard-navigation/use-list-keyboard-navigation.ts
+++ frontend/src/metabase/hooks/use-list-keyboard-navigation/use-list-keyboard-navigation.ts
@@ -1,23 +1,20 @@
 import type { MutableRefObject } from "react";
 import { useCallback, useEffect, useRef, useState } from "react";
 
-interface ListKeyboardNavigationInput<T, R> {
-  ref?: MutableRefObject<R | null>;
+interface ListKeyboardNavigationInput<T> {
+  ref?: MutableRefObject<any>;
   list: T[];
   onEnter: (item: T) => void;
   resetOnListChange?: boolean;
 }
 
-export const useListKeyboardNavigation = <
-  T,
-  R extends HTMLElement = HTMLElement,
->({
+export const useListKeyboardNavigation = <T>({
   ref,
   list,
   onEnter,
   resetOnListChange = true,
-}: ListKeyboardNavigationInput<T, R>) => {
-  const selectedRef = useRef<R | null>(null);
+}: ListKeyboardNavigationInput<T>) => {
+  const selectedRef = useRef<HTMLElement | null>();
   const [cursorIndex, setCursorIndex] = useState<number | null>(null);
 
   const reset = useCallback(() => setCursorIndex(null), []);
