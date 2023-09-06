metabase/metabase

commit 00e2565e23ebcd4581c7030b7e0d83d1453560a4

Implement timeseries zoom drill thru (#33574)
* First crack at drill-thru, adding quick-filter-drill

* Adding object detail drill

Needs tests for the multi-PK case

* Add distribution-drill and foreign-key-drill

* Add sort drill

* summarize-column and part of automatic-insights

* Add `display-info` for drill-thrus and the special pivot functions

Clean up and should be ready for FE first crack

* Add `drill-thru` implementations for all current drills

* small fixes

* overhaul of available-drill-thru

Fixes several issues with targeting, merges in Mode logic, fixes
pivot-drills

Still TODO: other drills' mode conditions; lots of checking and testing
the various cases of what should be shown; writing tests for that.

* Add `display-info` for drill-thrus and the special pivot functions

Clean up and should be ready for FE first crack

* Add `display-info` for drill-thrus and the special pivot functions

Clean up and should be ready for FE first crack

* Integrating the basic drill-thru ops, with some debugging hacks

* Fix bad spelling of parameters

* bunch of a progress, :null sentinel, refactor to context

* tests for lots more cases; two missing drills

* adding underlying records drills, not yet working

* fixing up to work with upstream changes

* fixes from Denis' PR

* more progress debugging FE drills and adding tests

* drill tests coming along

* cleanup, test fixes

* display-info-js for recursion

* Add TS typing for displayInfo of drill thrus

* partial changes for underlying records

* hack hack

* splitting out progression logic to a separate file

* First crack at drill-thru, adding quick-filter-drill

* Adding object detail drill

Needs tests for the multi-PK case

* Add distribution-drill and foreign-key-drill

* Add sort drill

* summarize-column and part of automatic-insights

* Add `display-info` for drill-thrus and the special pivot functions

src/metabase/lib/drill_thru/zoom_in_timeseries.cljc
@@ -1,12 +1,66 @@
 (ns metabase.lib.drill-thru.zoom-in-timeseries
   (:require
+   [medley.core :as m]
+   [metabase.lib.breakout :as lib.breakout]
    [metabase.lib.drill-thru.common :as lib.drill-thru.common]
+   [metabase.lib.filter :as lib.filter]
+   [metabase.lib.join :as lib.join]
    [metabase.lib.metadata :as lib.metadata]
-   [metabase.lib.metadata.calculation :as lib.metadata.calculation]
+   [metabase.lib.remove-replace :as lib.remove-replace]
    [metabase.lib.schema :as lib.schema]
+   [metabase.lib.schema.common :as lib.schema.common]
    [metabase.lib.schema.drill-thru :as lib.schema.drill-thru]
+   [metabase.lib.schema.temporal-bucketing
+    :as lib.schema.temporal-bucketing]
+   [metabase.lib.temporal-bucket :as lib.temporal-bucket]
    [metabase.lib.util :as lib.util]
+   [metabase.shared.util.i18n :as i18n]
    [metabase.util.malli :as mu]))
+
+;;; TODO -- we shouldn't include hour and minute for `:type/Date` columns.
+(def ^:private valid-current-units
+  [:year :quarter :month :week :day :hour :minute])
+
+(def ^:private unit->next-unit
+  (zipmap (drop-last valid-current-units)
+          (drop 1 valid-current-units)))
+
+(defn- is-ref-for-source-column? [a-ref column]
+  (and (lib.util/clause-of-type? a-ref :field)
+       (let [[_field _opts id-or-name] a-ref]
+         (if (integer? id-or-name)
+           (= id-or-name (:id column))
+           (and (if-let [join-alias (lib.join/current-join-alias a-ref)]
+                  (= join-alias (lib.join/current-join-alias column))
+                  true)
+                (= id-or-name (:lib/source-column-alias column)))))))
+
+(mu/defn ^:private matching-breakout-ref :- [:maybe :mbql.clause/field]
+  [query        :- ::lib.schema/query
+   stage-number :- :int
+   column       :- lib.metadata/ColumnMetadata]
+  (let [breakouts (lib.breakout/breakouts query stage-number)]
+    (m/find-first (fn [breakout]
+                    (and (is-ref-for-source-column? breakout column)
+                         (= (lib.temporal-bucket/temporal-bucket breakout)
+                            (lib.temporal-bucket/temporal-bucket column))))
+                  breakouts)))
+
+(mu/defn ^:private next-breakout-unit :- [:maybe ::lib.schema.temporal-bucketing/unit.date-time.truncate]
+  [column :- lib.metadata/ColumnMetadata]
+  (when-let [current-unit (lib.temporal-bucket/raw-temporal-bucket column)]
+    (when (contains? (set valid-current-units) current-unit)
+      (unit->next-unit current-unit))))
+
+(mu/defn ^:private describe-next-unit :- ::lib.schema.common/non-blank-string
+  [unit :- ::lib.schema.drill-thru/drill-thru.zoom-in.timeseries.next-unit]
+  (case unit
+    :quarter (i18n/tru "See this year by quarter")
+    :month   (i18n/tru "See this quarter by month")
+    :week    (i18n/tru "See this month by week")
+    :day     (i18n/tru "See this week by day")
+    :hour    (i18n/tru "See this day by hour")
+    :minute  (i18n/tru "See this hour by minute")))
 
 (mu/defn zoom-in-timeseries-drill :- [:maybe ::lib.schema.drill-thru/drill-thru.zoom-in.timeseries]
   "Zooms in on some window, showing it in finer detail.
@@ -15,18 +69,27 @@
 
   This is different from the `:drill-thru/zoom` type, which is for showing the details of a single object."
   ;; TODO: This naming is confusing. Fix it?
-  [query                             :- ::lib.schema/query
-   stage-number                      :- :int
-   {:keys [column dimensions value]} :- ::lib.schema.drill-thru/context]
+  [query                  :- ::lib.schema/query
+   stage-number           :- :int
+   {:keys [column value]} :- ::lib.schema.drill-thru/context]
   (when (and (lib.drill-thru.common/mbql-stage? query stage-number)
              column
              (some? value)
-             (not-empty dimensions))
-    {:lib/type   :metabase.lib.drill-thru/drill-thru
-     :type       :drill-thru/zoom-in.timeseries
-     ;; TODO: This is a bit confused for non-COUNT aggregations. Perhaps it should just always be 10 or something?
-     ;; Note that some languages have different plurals for exactly 2, or for 1, 2-5, and 6+.
-     :row-count  (if (number? value) value 2)
-     :table-name (some->> (lib.util/source-table-id query)
-                          (lib.metadata/table query)
-                          (lib.metadata.calculation/display-name query stage-number))}))
+             (matching-breakout-ref query stage-number column))
+    (when-let [next-unit (next-breakout-unit column)]
+      {:lib/type     :metabase.lib.drill-thru/drill-thru
+       :display-name (describe-next-unit next-unit)
+       :type         :drill-thru/zoom-in.timeseries
+       :column       column
+       :value        value
+       :next-unit    next-unit})))
+
+(mu/defmethod lib.drill-thru.common/drill-thru-method :drill-thru/zoom-in.timeseries
+  [query                            :- ::lib.schema/query
+   stage-number                     :- :int
+   {:keys [column value next-unit]} :- ::lib.schema.drill-thru/drill-thru.zoom-in.timeseries]
+  (let [breakout     (matching-breakout-ref query stage-number column)
+        new-breakout (lib.temporal-bucket/with-temporal-bucket breakout next-unit)]
+    (-> query
+      (lib.filter/filter stage-number (lib.filter/= column value))
+      (lib.remove-replace/replace-clause stage-number breakout new-breakout))))

src/metabase/lib/drill_thru.cljc
@@ -11,7 +11,7 @@
    [metabase.lib.drill-thru.summarize-column :as lib.drill-thru.summarize-column]
    [metabase.lib.drill-thru.summarize-column-by-time :as lib.drill-thru.summarize-column-by-time]
    [metabase.lib.drill-thru.underlying-records :as lib.drill-thru.underlying-records]
-   #_[metabase.lib.drill-thru.zoom-in-timeseries :as lib.drill-thru.zoom-in-timeseries]
+   [metabase.lib.drill-thru.zoom-in-timeseries :as lib.drill-thru.zoom-in-timeseries]
    [metabase.lib.metadata.calculation :as lib.metadata.calculation]
    [metabase.lib.schema :as lib.schema]
    [metabase.lib.schema.drill-thru :as lib.schema.drill-thru]
@@ -51,7 +51,7 @@
           lib.drill-thru.summarize-column/summarize-column-drill
           lib.drill-thru.summarize-column-by-time/summarize-column-by-time-drill
           lib.drill-thru.underlying-records/underlying-records-drill
-          #_lib.drill-thru.zoom-in-timeseries/zoom-in-timeseries-drill])))
+          lib.drill-thru.zoom-in-timeseries/zoom-in-timeseries-drill])))
 
 (mu/defn drill-thru :- ::lib.schema/query
   "`(drill-thru query stage-number drill-thru)`

src/metabase/lib/drill_thru/common.cljc
@@ -34,4 +34,4 @@
   [_query _stage-number drill-thru]
   ;; Several drill-thrus are rendered as a fixed label for that type, with no reference to the column or value,
   ;; so the default is simply the drill-thru type.
-  (select-keys drill-thru [:type]))
+  (select-keys drill-thru [:type :display-name]))

src/metabase/lib/schema/drill_thru.cljc
@@ -120,13 +120,17 @@
     [:lib/type [:= :metabase.lib.drill-thru/drill-thru]]
     [:column   lib.metadata/ColumnMetadata]]])
 
+(mr/def ::drill-thru.zoom-in.timeseries.next-unit
+  [:enum :quarter :month :week :day :hour :minute])
+
 (mr/def ::drill-thru.zoom-in.timeseries
   [:merge
    ::drill-thru.common
    [:map
-    [:type       [:= :drill-thru/zoom-in.timeseries]]
-    [:row-count  number?]
-    [:table-name [:maybe string?]]]])
+    [:type      [:= :drill-thru/zoom-in.timeseries]]
+    [:column    lib.metadata/ColumnMetadata]
+    [:value     some?]
+    [:next-unit [:ref ::drill-thru.zoom-in.timeseries.next-unit]]]])
 
 (mr/def ::drill-thru
   [:and

src/metabase/lib/temporal_bucket.cljc
@@ -138,12 +138,17 @@
   [_x]
   nil)
 
+(mu/defn raw-temporal-bucket :- [:maybe ::lib.schema.temporal-bucketing/unit]
+  "Get the raw temporal bucketing `unit` associated with something e.g. a `:field` ref or a ColumnMetadata."
+  [x]
+  (temporal-bucket-method x))
+
 (mu/defn temporal-bucket :- [:maybe ::lib.schema.temporal-bucketing/option]
   "Get the current temporal bucketing option associated with something, if any."
   [x]
-  (when-let [unit (temporal-bucket-method x)]
+  (when-let [unit (raw-temporal-bucket x)]
     {:lib/type :option/temporal-bucketing
-     :unit unit}))
+     :unit     unit}))
 
 (def ^:private hidden-bucketing-options
   "Options that are technically legal in MBQL, but that should be hidden in the UI."

test/metabase/lib/drill_thru/zoom_in_timeseries_test.cljc
@@ -1,0 +1,81 @@
+(ns metabase.lib.drill-thru.zoom-in-timeseries-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [medley.core :as m]
+   [metabase.lib.core :as lib]
+   [metabase.lib.drill-thru.zoom-in-timeseries
+    :as lib.drill-thru.zoom-in-timeseries]
+   [metabase.lib.temporal-bucket :as lib.temporal-bucket]
+   [metabase.lib.test-metadata :as meta]
+   #?@(:cljs ([metabase.test-runner.assert-exprs.approximately-equal]))))
+
+#?(:cljs (comment metabase.test-runner.assert-exprs.approximately-equal/keep-me))
+
+(deftest ^:parallel zoom-in-timeseries-e2e-test
+  (let [query (-> (lib/query meta/metadata-provider (meta/table-metadata :orders))
+                  (lib/aggregate (lib/count))
+                  (lib/breakout (lib/with-temporal-bucket (meta/field-metadata :orders :created-at) :day))
+                  (lib/breakout (lib/with-temporal-bucket (meta/field-metadata :orders :created-at) :year)))]
+    (is (=? {:stages [{:aggregation [[:count {}]]
+                       :breakout    [[:field {:temporal-unit :day} (meta/id :orders :created-at)]
+                                     [:field {:temporal-unit :year} (meta/id :orders :created-at)]]}]}
+            query))
+    (let [created-at (m/find-first #(and (= (:id %) (meta/id :orders :created-at))
+                                         (= (lib.temporal-bucket/raw-temporal-bucket %) :year))
+                                   (lib/returned-columns query))
+          _          (assert created-at)
+          drill      (lib.drill-thru.zoom-in-timeseries/zoom-in-timeseries-drill query
+                                                                                 -1
+                                                                                 {:column created-at
+                                                                                  :value  2022})]
+      (is (=? {:type         :drill-thru/zoom-in.timeseries
+               :column       {:id                               (meta/id :orders :created-at)
+                              :metabase.lib.field/temporal-unit :year}
+               :value        2022
+               :next-unit    :quarter
+               :display-name "See this year by quarter"}
+              drill))
+      (is (=? {:display-name "See this year by quarter"}
+              (lib/display-info query -1 drill)))
+      (let [query' (lib/drill-thru query drill)]
+        (is (=? {:stages [{:aggregation [[:count {}]]
+                           :breakout    [[:field {:temporal-unit :day} (meta/id :orders :created-at)]
+                                         [:field {:temporal-unit :quarter} (meta/id :orders :created-at)]]
+                           :filters     [[:=
+                                          {}
+                                          [:field {:temporal-unit :year} (meta/id :orders :created-at)]
+                                          2022]]}]}
+                query'))
+        (let [created-at (m/find-first #(and (= (:id %) (meta/id :orders :created-at))
+                                             (= (lib.temporal-bucket/raw-temporal-bucket %) :quarter))
+                                       (lib/returned-columns query'))
+              _          (assert created-at)
+              drill      (lib.drill-thru.zoom-in-timeseries/zoom-in-timeseries-drill query'
+                                                                                     -1
+                                                                                     {:column created-at
+                                                                                      :value  "2022-04-01T00:00:00"})]
+          (is (=? {:type         :drill-thru/zoom-in.timeseries
+                   :column       {:id                               (meta/id :orders :created-at)
+                                  :metabase.lib.field/temporal-unit :quarter}
+                   :value        "2022-04-01T00:00:00"
+                   :next-unit    :month
+                   :display-name "See this quarter by month"}
+                  drill))
+          (is (=? {:display-name "See this quarter by month"}
+                  (lib/display-info query' -1 drill)))
+          (let [query'' (lib/drill-thru query' drill)]
+            (is (=? {:stages [{:aggregation [[:count {}]]
+                               :breakout    [[:field {:temporal-unit :day} (meta/id :orders :created-at)]
+                                             [:field {:temporal-unit :month} (meta/id :orders :created-at)]]
+                               ;; if we were SMART we could remove the first filter clause since it's not adding any
+                               ;; value, but it won't hurt anything other than performance to keep it there. QP can
+                               ;; generate optimal filters anyway
+                               :filters     [[:=
+                                              {}
+                                              [:field {:temporal-unit :year} (meta/id :orders :created-at)]
+                                              2022]
+                                             [:=
+                                              {}
+                                              [:field {:temporal-unit :quarter} (meta/id :orders :created-at)]
+                                              "2022-04-01T00:00:00"]]}]}
+                    query''))))))))

commit 8cb63dfa51b5e8bd3b2703c42aac66c43c55e3ac

Fix unresolved var `lib.join/current-join-alias` (#33747)
The build is failing on master due to :cause "No such var: lib.join/current-join-alias"
This breaking change was introduced in #33574.

Searching for current-join-alias shows that all other references use a different namespace. Namely, lib.join.util.
This PR uses that namespace instead.

The function was moved in #32698. Both PRs have been opened, and have had CI green before the merge, but got out of sync. A merge queue would've prevented this.

src/metabase/lib/drill_thru/zoom_in_timeseries.cljc
@@ -4,7 +4,7 @@
    [metabase.lib.breakout :as lib.breakout]
    [metabase.lib.drill-thru.common :as lib.drill-thru.common]
    [metabase.lib.filter :as lib.filter]
-   [metabase.lib.join :as lib.join]
+   [metabase.lib.join.util :as lib.join.util]
    [metabase.lib.metadata :as lib.metadata]
    [metabase.lib.remove-replace :as lib.remove-replace]
    [metabase.lib.schema :as lib.schema]
@@ -30,8 +30,8 @@
        (let [[_field _opts id-or-name] a-ref]
          (if (integer? id-or-name)
            (= id-or-name (:id column))
-           (and (if-let [join-alias (lib.join/current-join-alias a-ref)]
-                  (= join-alias (lib.join/current-join-alias column))
+           (and (if-let [join-alias (lib.join.util/current-join-alias a-ref)]
+                  (= join-alias (lib.join.util/current-join-alias column))
                   true)
                 (= id-or-name (:lib/source-column-alias column)))))))
 