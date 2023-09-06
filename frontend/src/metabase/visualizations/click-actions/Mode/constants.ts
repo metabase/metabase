50208950907cb68f997562e67a24ceac4b81e1de
Denis Berezin <denis.berezin@metabase.com>
2023-09-06 11:59:58 +0300

    Added MLv2 drills integration for FKFilter and SummarizeColumnByTime drills (#32402)

--- frontend/src/metabase/visualizations/click-actions/Mode/constants.ts
+++ frontend/src/metabase/visualizations/click-actions/Mode/constants.ts
@@ -1,3 +1,8 @@
+import type { Drill } from "metabase/visualizations/types/click-actions";
+import type { DrillThruType } from "metabase-lib";
+import { FKFilterDrill } from "metabase/visualizations/click-actions/drills/mlv2/FKFilterDrill";
+import { SummarizeColumnByTimeDrill } from "metabase/visualizations/click-actions/drills/mlv2/SummarizeColumnByTimeDrill";
+
 export const MODE_TYPE_DEFAULT = "default";
 export const MODE_TYPE_NATIVE = "native";
 export const MODE_TYPE_SEGMENT = "segment";
@@ -15,3 +20,21 @@ export const MODES_TYPES = [
   MODE_TYPE_PIVOT,
   MODE_TYPE_DEFAULT,
 ] as const;
+
+export const DRILL_TYPE_TO_HANDLER_MAP: Record<
+  DrillThruType,
+  Drill<any> | null
+> = {
+  "drill-thru/column-filter": null, // ColumnFilterDrill,
+  "drill-thru/quick-filter": null, // QuickFilterDrill,
+  "drill-thru/pk": null, // PKDetailsDrill,
+  "drill-thru/zoom": null, // ZoomToRowDrill,
+  "drill-thru/fk-details": null, // FKDetailsDrill,
+  "drill-thru/pivot": null,
+  "drill-thru/fk-filter": FKFilterDrill,
+  "drill-thru/distribution": null, // DistributionDrill,
+  "drill-thru/sort": null, // SortDrill,
+  "drill-thru/summarize-column": null, // SummarizeColumnDrill,
+  "drill-thru/summarize-column-by-time": SummarizeColumnByTimeDrill,
+  "drill-thru/underlying-records": null, // UnderlyingRecordsDrill,
+};
