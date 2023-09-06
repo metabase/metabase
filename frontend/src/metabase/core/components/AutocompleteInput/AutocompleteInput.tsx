0c744174a38b6c2548f663bb6c522dbbb621c797
Oisin Coveney <oisinthec@gmail.com>
2023-09-06 17:53:28 +0300

    Discard changes to frontend/src/metabase/core/components/AutocompleteInput/AutocompleteInput.tsx

--- frontend/src/metabase/core/components/AutocompleteInput/AutocompleteInput.tsx
+++ frontend/src/metabase/core/components/AutocompleteInput/AutocompleteInput.tsx
@@ -42,12 +42,12 @@ const AutocompleteInput = ({
   ...rest
 }: AutocompleteInputProps) => {
   const optionsListRef = useRef<HTMLUListElement>(null);
-  const inputRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
   const filteredOptions = useMemo(() => {
     return filterOptions(String(value), options);
   }, [value, options, filterOptions]);
 
-  const { cursorIndex } = useListKeyboardNavigation<string, HTMLDivElement>({
+  const { cursorIndex } = useListKeyboardNavigation({
     list: filteredOptions,
     onEnter: (item: string) => handleOptionSelect(item),
     resetOnListChange: true,
