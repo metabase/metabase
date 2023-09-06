940933a6751ea48f937757ac0788004d58686987
Ngoc Khuat <qn.khuat@gmail.com>
2023-09-06 12:25:21 +0700

    with-temp run in a transaction

--- test/metabase/test/redefs.clj
+++ test/metabase/test/redefs.clj
@@ -5,22 +5,28 @@
    [mb.hawk.parallel]
    [metabase.plugins.classloader :as classloader]
    [methodical.core :as methodical]
+   [toucan2.connection :as t2.connection]
    [toucan2.tools.with-temp :as t2.with-temp]))
 
+(def ^{:dynamic true
+       :private true}
+  *in-with-temp* false)
+
 (methodical/defmethod t2.with-temp/do-with-temp* :around :default
-  "Initialize the DB before doing the other with-temp stuff. Make sure metabase.test.util is loaded. Make sure we are
-  not inside a parallel test."
+  "Initialize the DB before doing the other with-temp stuff. Make sure metabase.test.util is loaded.
+  Make sure the the "
   [model attributes f]
   (classloader/require 'metabase.test.initialize)
   ((resolve 'metabase.test.initialize/initialize-if-needed!) :db)
   ;; so with-temp-defaults are loaded
   (classloader/require 'metabase.test.util)
-  ;; disallow with-temp inside parallel tests.
-  ;;
-  ;; TODO -- there's not really a reason that we can't use with-temp in parallel tests -- it depends on the test -- so
-  ;; once we're a little more comfortable with the current parallel stuff we should remove this restriction.
-  (mb.hawk.parallel/assert-test-is-not-parallel "with-temp")
-  (next-method model attributes f))
+
+  ;; run `f` in a transaction if it's the top-level with-temp
+  (if-not *in-with-temp*
+    (binding [*in-with-temp* true]
+      (t2.connection/with-transaction [_]
+        (next-method model attributes f)))
+    (next-method model attributes f)))
 
 ;;; wrap `with-redefs-fn` (used by `with-redefs`) so it calls `assert-test-is-not-parallel`
 
