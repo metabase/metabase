826c6647b212960c678d8ce802d990f89563db82
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-09-07 22:04:36 +0700

    Remove unsupported timeseries grouping on expression dimension

--- frontend/src/metabase/visualizations/click-actions/components/TimeseriesModeFooter.tsx
+++ frontend/src/metabase/visualizations/click-actions/components/TimeseriesModeFooter.tsx
@@ -11,17 +11,27 @@ export const TimeseriesModeFooter = (props: Props): JSX.Element => {
     const { updateQuestion } = props;
     updateQuestion(question, { run: true });
   };
+  // The first breakout is always a date.
+  // See https://github.com/metabase/metabase/blob/e7363d97d6ed0ec8f5288a642e4990e85df57e79/frontend/src/metabase/visualizations/click-actions/Mode/utils.ts#L41-L44
+  const [dateBreakout] = props.query.breakouts();
+  const shouldHideTimeseriesGroupingWidget = dateBreakout
+    .dimension()
+    .isExpression();
 
   return (
     <div className="flex layout-centered" data-testid="timeseries-mode-bar">
       <span className="mr1">{t`View`}</span>
       <TimeseriesFilterWidget {...props} card={props.lastRunCard} />
-      <span className="mx1">{t`by`}</span>
-      <TimeseriesGroupingWidget
-        {...props}
-        onChange={onChange}
-        card={props.lastRunCard}
-      />
+      {!shouldHideTimeseriesGroupingWidget && (
+        <>
+          <span className="mx1">{t`by`}</span>
+          <TimeseriesGroupingWidget
+            {...props}
+            onChange={onChange}
+            card={props.lastRunCard}
+          />
+        </>
+      )}
     </div>
   );
 };
