d1daf7fab2d630cc888781c57657abf4b2296890
Noah Moss <noahbmoss@gmail.com>
2023-09-07 16:55:38 -0400

    update (some) existing activity log tests to use audit log

--- test/metabase/events/audit_log_test.clj
+++ test/metabase/events/audit_log_test.clj
@@ -2,45 +2,53 @@
   (:require
    [clojure.test :refer :all]
    [metabase.events :as events]
-   [metabase.events.activity-feed :as events.activity-feed]
+   [metabase.events.audit-log :as events.audit-log]
    [metabase.lib.schema.id :as lib.schema.id]
    [metabase.models
-    :refer [Activity Card Dashboard DashboardCard Metric Pulse Segment]]
+    :refer [Card Dashboard DashboardCard Metric Pulse Segment]]
    [metabase.test :as mt]
    [metabase.util :as u]
    [toucan2.core :as t2]
    [toucan2.tools.with-temp :as t2.with-temp]))
 
-;; TODO update tests to use new AuditLog table
-
 (set! *warn-on-reflection* true)
 
-(comment events.activity-feed/keep-me)
+(comment events.audit-log/keep-me)
+
+(defn- event
+  ([topic]
+   (event topic nil))
+
+  ([topic model-id]
+   (t2/select-one [:model/AuditLog :topic :user_id :model :model_id :details]
+                  :topic    topic
+                  :model_id model-id
+                  {:order-by [[:id :desc]]})))
 
 (defn- activity
   ([topic]
    (activity topic nil))
 
   ([topic model-id]
-   (t2/select-one [Activity :topic :user_id :model :model_id :database_id :table_id :details]
+   (t2/select-one [:model/Activity :topic :user_id :model :model_id :details]
                   :topic    topic
                   :model_id model-id
                   {:order-by [[:id :desc]]})))
 
 (deftest card-create-test
   (testing :card-create
-    (t2.with-temp/with-temp [Card card {:name "My Cool Card"}]
-      (mt/with-model-cleanup [Activity]
-        (is (= card
-               (events/publish-event! :event/card-create card)))
-        (is (= {:topic       :card-create
-                :user_id     (mt/user->id :rasta)
-                :model       "card"
-                :model_id    (:id card)
-                :database_id nil
-                :table_id    nil
-                :details     {:name "My Cool Card", :description nil}}
-               (activity "card-create" (:id card))))))))
+    (mt/with-model-cleanup [:model/AuditLog]
+      (mt/with-test-user :rasta
+        (t2.with-temp/with-temp [Card card {:name "My Cool Card"}]
+          (is (= card
+                 (events/publish-event! :event/card-create card)))
+          (is (partial=
+               {:topic    :card-create
+                :user_id  (mt/user->id :rasta)
+                :model    "Card"
+                :model_id (:id card)
+                :details  {:name "My Cool Card", :description nil, :database_id nil, :table_id nil}}
+               (event "card-create" (:id card)))))))))
 
 (deftest card-create-nested-query-test
   (testing :card-create
@@ -53,108 +61,114 @@
                                   :dataset_query {:database lib.schema.id/saved-questions-virtual-database-id
                                                   :type     :query
                                                   :query    {:source-table (str "card__" (u/the-id card-1))}}}]
-        (mt/with-model-cleanup [Activity]
-          (is (= card-2
-                 (events/publish-event! :event/card-create card-2)))
-          (is (= {:topic       :card-create
-                  :user_id     (mt/user->id :rasta)
-                  :model       "card"
-                  :model_id    (:id card-2)
-                  :database_id (mt/id)
-                  :table_id    (mt/id :venues)
-                  :details     {:name "My Cool NESTED Card", :description nil}}
-                 (activity "card-create" (:id card-2)))))))))
+        (mt/with-model-cleanup [:model/AuditLog]
+          (mt/with-test-user :rasta
+            (is (= card-2
+                   (events/publish-event! :event/card-create card-2)))
+            (is (partial=
+                 {:topic    :card-create
+                  :user_id  (mt/user->id :rasta)
+                  :model    "Card"
+                  :model_id (:id card-2)
+                  :details  {:name        "My Cool NESTED Card"
+                             :description nil
+                             :database_id (mt/id)
+                             :table_id    (mt/id :venues)}}
+                 (event "card-create" (:id card-2))))))))))
 
 (deftest card-update-event-test
   (testing :card-update
     (doseq [dataset? [false true]]
       (testing (if dataset? "Dataset" "Card")
         (t2.with-temp/with-temp [Card card {:name "My Cool Card" :dataset dataset?}]
-          (mt/with-model-cleanup [Activity]
-            (is (= card
-                   (events/publish-event! :event/card-update card)))
-            (is (= {:topic       :card-update
-                    :user_id     (mt/user->id :rasta)
-                    :model       (if dataset? "dataset" "card")
-                    :model_id    (:id card)
-                    :database_id nil
-                    :table_id    nil
-                    :details     (cond-> {:name "My Cool Card", :description nil}
-                                   dataset? (assoc :original-model "card"))}
-                   (activity "card-update" (:id card))))))))))
+          (mt/with-model-cleanup [:model/AuditLog]
+           (mt/with-test-user :rasta
+             (is (= card
+                    (events/publish-event! :event/card-update card)))
+             (is (partial=
+                  {:topic    :card-update
+                   :user_id  (mt/user->id :rasta)
+                   :model    "Card"
+                   :model_id (:id card)
+                   :details  (cond-> {:name        "My Cool Card"
+                                      :description nil
+                                      :table_id    nil
+                                      :database_id nil}
+                               dataset? (assoc :model? true))}
+                  (event "card-update" (:id card)))))))))))
 
 (deftest card-delete-event-test
   (testing :card-delete
     (doseq [dataset? [false true]]
       (testing (if dataset? "Dataset" "Card")
         (t2.with-temp/with-temp [Card card {:name "My Cool Card", :dataset dataset?}]
-          (mt/with-model-cleanup [Activity]
-            (is (= card
-                   (events/publish-event! :event/card-delete card)))
-            (is (= {:topic       :card-delete
-                    :user_id     (mt/user->id :rasta)
-                    :model       (if dataset? "dataset" "card")
+          (mt/with-model-cleanup [:model/AuditLog]
+            (mt/with-test-user :rasta
+              (is (= card
+                     (events/publish-event! :event/card-delete card)))
+              (is (partial=
+                   {:topic    :card-delete
+                    :user_id  (mt/user->id :rasta)
+                    :model    "Card"
                     :model_id (:id card)
-                    :database_id nil
-                    :table_id    nil
-                    :details     (cond-> {:name "My Cool Card", :description nil}
-                                   dataset? (assoc :original-model "card"))}
-                   (activity "card-delete" (:id card))))))))))
+                    :details  (cond-> {:name "My Cool Card", :description nil}
+                                dataset? (assoc :model? true))}
+                   (event "card-delete" (:id card)))))))))))
 
 (deftest dashboard-create-event-test
   (testing :dashboard-create
     (t2.with-temp/with-temp [Dashboard dashboard {:name "My Cool Dashboard"}]
-      (mt/with-model-cleanup [Activity]
-        (is (= dashboard
-               (events/publish-event! :event/dashboard-create dashboard)))
-        (is (= {:topic       :dashboard-create
-                :user_id     (mt/user->id :rasta)
-                :model       "dashboard"
-                :model_id    (:id dashboard)
-                :database_id nil
-                :table_id    nil
-                :details     {:name "My Cool Dashboard", :description nil}}
-               (activity "dashboard-create" (:id dashboard))))))))
+      (mt/with-model-cleanup [:model/AuditLog]
+        (mt/with-test-user :rasta
+          (is (= dashboard
+                 (events/publish-event! :event/dashboard-create dashboard)))
+          (is (partial=
+               {:topic    :dashboard-create
+                :user_id  (mt/user->id :rasta)
+                :model    "Dashboard"
+                :model_id (:id dashboard)
+                :details  {:name "My Cool Dashboard", :description nil}}
+               (event "dashboard-create" (:id dashboard)))))))))
 
 (deftest dashboard-delete-event-test
   (testing :dashboard-delete
     (t2.with-temp/with-temp [Dashboard dashboard {:name "My Cool Dashboard"}]
-      (mt/with-model-cleanup [Activity]
-        (is (= dashboard
-               (events/publish-event! :event/dashboard-delete dashboard)))
-        (is (= {:topic       :dashboard-delete
-                :user_id     (mt/user->id :rasta)
-                :model       "dashboard"
-                :model_id    (:id dashboard)
-                :database_id nil
-                :table_id    nil
-                :details     {:name "My Cool Dashboard", :description nil}}
-               (activity "dashboard-delete" (:id dashboard))))))))
+      (mt/with-model-cleanup [:model/AuditLog]
+        (mt/with-test-user :rasta
+          (is (= dashboard
+                 (events/publish-event! :event/dashboard-delete dashboard)))
+          (is (partial=
+               {:topic    :dashboard-delete
+                :user_id  (mt/user->id :rasta)
+                :model    "Dashboard"
+                :model_id (:id dashboard)
+                :details  {:name "My Cool Dashboard", :description nil}}
+               (event "dashboard-delete" (:id dashboard)))))))))
 
 (deftest dashboard-add-cards-event-test
   (testing :dashboard-add-cards
     (mt/with-temp [Dashboard     dashboard {:name "My Cool Dashboard"}
                    Card          card {}
                    DashboardCard dashcard  {:dashboard_id (:id dashboard), :card_id (:id card)}]
-      (mt/with-model-cleanup [Activity]
-        (let [event {:id        (:id dashboard)
-                     :actor_id  (mt/user->id :rasta)
-                     :dashcards [dashcard]}]
-          (is (= event
-                 (events/publish-event! :event/dashboard-add-cards event))))
-        (is (= {:topic       :dashboard-add-cards
-                :user_id     (mt/user->id :rasta)
-                :model       "dashboard"
-                :model_id    (:id dashboard)
-                :database_id nil
-                :table_id    nil
-                :details     {:name        "My Cool Dashboard"
-                              :description nil
-                              :dashcards   [{:description (:description card)
-                                             :name        (:name card)
-                                             :id          (:id dashcard)
-                                             :card_id     (:id card)}]}}
-               (activity "dashboard-add-cards" (:id dashboard))))))))
+      (mt/with-model-cleanup [:model/AuditLog]
+        (mt/with-test-user :rasta
+         (let [event {:id        (:id dashboard)
+                      :actor_id  (mt/user->id :rasta)
+                      :dashcards [dashcard]}]
+           (is (= event
+                  (events/publish-event! :event/dashboard-add-cards event))))
+         (is (partial=
+              {:topic    :dashboard-add-cards
+               :user_id  (mt/user->id :rasta)
+               :model    "Dashboard"
+               :model_id (:id dashboard)
+               :details  {:name        "My Cool Dashboard"
+                          :description nil
+                          :dashcards   [{:description (:description card)
+                                         :name        (:name card)
+                                         :id          (:id dashcard)
+                                         :card_id     (:id card)}]}}
+              (event "dashboard-add-cards" (:id dashboard)))))))))
 
 (deftest dashboard-remove-cards-event-test
   (testing :dashboard-remove-cards
