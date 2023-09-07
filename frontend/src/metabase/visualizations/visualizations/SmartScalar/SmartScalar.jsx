0f991dd0d6e21d21d106e82732de588a22dbada8
Shaun Williams <shaun@metabase.com>
2023-09-07 17:26:06 -0500

    convert class → function component

--- frontend/src/metabase/visualizations/visualizations/SmartScalar/SmartScalar.jsx
+++ frontend/src/metabase/visualizations/visualizations/SmartScalar/SmartScalar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { Component } from "react";
+import { useRef } from "react";
 import { t, jt } from "ttag";
 import _ from "underscore";
 
@@ -48,18 +48,18 @@ import {
   getValueWidth,
 } from "./utils";
 
-export class SmartScalar extends Component {
-  static uiName = t`Trend`;
-  static identifier = "smartscalar";
-  static iconName = "smartscalar";
-  static canSavePng = false;
+const staticProps = {
+  uiName: t`Trend`,
+  identifier: "smartscalar",
+  iconName: "smartscalar",
+  canSavePng: false,
 
-  static minSize = getMinSize("smartscalar");
-  static defaultSize = getDefaultSize("smartscalar");
+  minSize: getMinSize("smartscalar"),
+  defaultSize: getDefaultSize("smartscalar"),
 
-  static noHeader = true;
+  noHeader: true,
 
-  static settings = {
+  settings: {
     ...columnSettings({
       getColumns: (
         [
@@ -83,14 +83,14 @@ export class SmartScalar extends Component {
       inline: true,
     },
     click_behavior: {},
-  };
+  },
 
-  static isSensible({ insights }) {
+  isSensible({ insights }) {
     return insights && insights.length > 0;
-  }
+  },
 
   // Smart scalars need to have a breakout
-  static checkRenderable(
+  checkRenderable(
     [
       {
         data: { insights },
@@ -103,201 +103,203 @@ export class SmartScalar extends Component {
         t`Group by a time field to see how this has changed over time`,
       );
     }
+  },
+};
+
+export function SmartScalar(props) {
+  const {
+    actionButtons,
+    onChangeCardAndRun,
+    onVisualizationClick,
+    isDashboard,
+    settings,
+    visualizationIsClickable,
+    series: [
+      {
+        card,
+        data: { rows, cols },
+      },
+    ],
+    rawSeries,
+    gridSize,
+    width,
+    height,
+    totalNumGridCols,
+    fontFamily,
+  } = props;
+
+  const _scalar = useRef(null);
+
+  const metricIndex = cols.findIndex(col => !isDate(col));
+  const dimensionIndex = cols.findIndex(col => isDate(col));
+
+  const lastRow = rows[rows.length - 1];
+  const value = lastRow?.[metricIndex];
+  const column = cols[metricIndex];
+
+  const insights = rawSeries?.[0].data?.insights;
+  const insight = _.findWhere(insights, { col: column.name });
+  if (!insight) {
+    return null;
   }
 
-  render() {
-    const {
-      actionButtons,
-      onChangeCardAndRun,
-      onVisualizationClick,
-      isDashboard,
-      settings,
-      visualizationIsClickable,
-      series: [
-        {
-          card,
-          data: { rows, cols },
-        },
-      ],
-      rawSeries,
-      gridSize,
-      width,
-      height,
-      totalNumGridCols,
-      fontFamily,
-    } = this.props;
+  const lastValue = insight["last-value"];
+  const formatOptions = settings.column(column);
 
-    const metricIndex = cols.findIndex(col => !isDate(col));
-    const dimensionIndex = cols.findIndex(col => isDate(col));
+  const { displayValue, fullScalarValue } = compactifyValue(
+    lastValue,
+    width,
+    formatOptions,
+  );
 
-    const lastRow = rows[rows.length - 1];
-    const value = lastRow && lastRow[metricIndex];
-    const column = cols[metricIndex];
+  const granularity = Lib.describeTemporalUnit(insight["unit"]).toLowerCase();
 
-    const insights =
-      rawSeries && rawSeries[0].data && rawSeries[0].data.insights;
-    const insight = _.findWhere(insights, { col: column.name });
-    if (!insight) {
-      return null;
-    }
+  const lastChange = insight["last-change"];
+  const previousValue = insight["previous-value"];
 
-    const lastValue = insight["last-value"];
-    const formatOptions = settings.column(column);
+  const isNegative = lastChange < 0;
+  const isSwapped = settings["scalar.switch_positive_negative"];
 
-    const { displayValue, fullScalarValue } = compactifyValue(
-      lastValue,
-      width,
-      formatOptions,
-    );
+  // if the number is negative but thats been identified as a good thing (e.g. decreased latency somehow?)
+  const changeColor = (isSwapped ? !isNegative : isNegative)
+    ? color("error")
+    : color("success");
 
-    const granularity = Lib.describeTemporalUnit(insight["unit"]).toLowerCase();
+  const titleLinesCount = getTitleLinesCount(height);
 
-    const lastChange = insight["last-change"];
-    const previousValue = insight["previous-value"];
+  const changeDisplay = formatChangeAutoPrecision(lastChange, {
+    fontFamily,
+    fontWeight: 900,
+    width: getChangeWidth(width),
+  });
 
-    const isNegative = lastChange < 0;
-    const isSwapped = settings["scalar.switch_positive_negative"];
+  const tooltipSeparator = <Separator>•</Separator>;
+  const previousValueSeparator = (
+    <PreviousValueSeparator>•</PreviousValueSeparator>
+  );
+  const granularityDisplay = jt`last ${granularity}`;
+  const previousValueDisplay = formatValue(
+    previousValue,
+    settings.column(column),
+  );
 
-    // if the number is negative but thats been identified as a good thing (e.g. decreased latency somehow?)
-    const changeColor = (isSwapped ? !isNegative : isNegative)
-      ? color("error")
-      : color("success");
+  const previousValueDisplayInTooltip = jt`${tooltipSeparator} was ${previousValueDisplay} ${granularityDisplay}`;
+  const previousValueDisplayInCard = jt`${previousValueSeparator} was ${previousValueDisplay} ${granularityDisplay}`;
+  const canShowPreviousValue = getCanShowPreviousValue({
+    width,
+    change: changeDisplay,
+    previousValue:
+      typeof previousValueDisplayInTooltip === "string"
+        ? previousValueDisplayInTooltip
+        : previousValueDisplayInTooltip.join(""),
+    fontFamily,
+  });
+  const iconName = isNegative ? "arrow_down" : "arrow_up";
 
-    const titleLinesCount = getTitleLinesCount(height);
-
-    const changeDisplay = formatChangeAutoPrecision(lastChange, {
-      fontFamily,
-      fontWeight: 900,
-      width: getChangeWidth(width),
-    });
-
-    const tooltipSeparator = <Separator>•</Separator>;
-    const previousValueSeparator = (
-      <PreviousValueSeparator>•</PreviousValueSeparator>
-    );
-    const granularityDisplay = jt`last ${granularity}`;
-    const previousValueDisplay = formatValue(
-      previousValue,
-      settings.column(column),
-    );
-
-    const previousValueDisplayInTooltip = jt`${tooltipSeparator} was ${previousValueDisplay} ${granularityDisplay}`;
-    const previousValueDisplayInCard = jt`${previousValueSeparator} was ${previousValueDisplay} ${granularityDisplay}`;
-    const canShowPreviousValue = getCanShowPreviousValue({
-      width,
-      change: changeDisplay,
-      previousValue:
-        typeof previousValueDisplayInTooltip === "string"
-          ? previousValueDisplayInTooltip
-          : previousValueDisplayInTooltip.join(""),
-      fontFamily,
-    });
-    const iconName = isNegative ? "arrow_down" : "arrow_up";
-
-    const clicked = {
+  const clicked = {
+    value,
+    column,
+    dimensions: [
+      {
+        value: rows[rows.length - 1][dimensionIndex],
+        column: cols[dimensionIndex],
+      },
+    ],
+    data: rows[rows.length - 1].map((value, index) => ({
       value,
-      column,
-      dimensions: [
-        {
-          value: rows[rows.length - 1][dimensionIndex],
-          column: cols[dimensionIndex],
-        },
-      ],
-      data: rows[rows.length - 1].map((value, index) => ({
-        value,
-        col: cols[index],
-      })),
-      settings,
-    };
+      col: cols[index],
+    })),
+    settings,
+  };
 
-    const isClickable = visualizationIsClickable(clicked);
+  const isClickable = visualizationIsClickable(clicked);
 
-    return (
-      <ScalarWrapper>
-        <div className="Card-title absolute top right p1 px2">
-          {actionButtons}
-        </div>
-        <ScalarContainer
-          className="fullscreen-normal-text fullscreen-night-text"
-          data-testid="scalar-container"
-          tooltip={fullScalarValue}
-          alwaysShowTooltip={fullScalarValue !== displayValue}
-          isClickable={isClickable}
+  return (
+    <ScalarWrapper>
+      <div className="Card-title absolute top right p1 px2">
+        {actionButtons}
+      </div>
+      <ScalarContainer
+        className="fullscreen-normal-text fullscreen-night-text"
+        data-testid="scalar-container"
+        tooltip={fullScalarValue}
+        alwaysShowTooltip={fullScalarValue !== displayValue}
+        isClickable={isClickable}
+      >
+        <span
+          onClick={
+            isClickable &&
+            (() =>
+              _scalar.current &&
+              onVisualizationClick({ ...clicked, element: _scalar.current }))
+          }
+          ref={_scalar}
         >
-          <span
-            onClick={
-              isClickable &&
-              (() =>
-                this._scalar &&
-                onVisualizationClick({ ...clicked, element: this._scalar }))
-            }
-            ref={scalar => (this._scalar = scalar)}
-          >
-            <ScalarValue
-              fontFamily={fontFamily}
-              gridSize={gridSize}
-              height={getValueHeight(height, canShowPreviousValue)}
-              totalNumGridCols={totalNumGridCols}
-              value={displayValue}
-              width={getValueWidth(width)}
-            />
-          </span>
-        </ScalarContainer>
-        {isDashboard && (
-          <ScalarTitle
-            lines={titleLinesCount}
-            title={settings["card.title"]}
-            description={settings["card.description"]}
-            onClick={
-              onChangeCardAndRun &&
-              (() => onChangeCardAndRun({ nextCard: card }))
-            }
+          <ScalarValue
+            fontFamily={fontFamily}
+            gridSize={gridSize}
+            height={getValueHeight(height, canShowPreviousValue)}
+            totalNumGridCols={totalNumGridCols}
+            value={displayValue}
+            width={getValueWidth(width)}
           />
+        </span>
+      </ScalarContainer>
+      {isDashboard && (
+        <ScalarTitle
+          lines={titleLinesCount}
+          title={settings["card.title"]}
+          description={settings["card.description"]}
+          onClick={
+            onChangeCardAndRun && (() => onChangeCardAndRun({ nextCard: card }))
+          }
+        />
+      )}
+
+      <PreviousValueWrapper data-testid="scalar-previous-value">
+        {lastChange == null || previousValue == null ? (
+          <div
+            className="text-centered text-bold mt1"
+            style={{ color: color("text-medium") }}
+          >{jt`Nothing to compare for the previous ${granularity}.`}</div>
+        ) : lastChange === 0 ? (
+          t`No change from last ${granularity}`
+        ) : (
+          <PreviousValueContainer>
+            <Tooltip
+              isEnabled={!canShowPreviousValue}
+              placement="bottom"
+              tooltip={
+                <VariationTooltip>
+                  <Variation>
+                    <VariationIcon name={iconName} size={TOOLTIP_ICON_SIZE} />
+                    <VariationValue showTooltip={false}>
+                      {formatChange(lastChange)}
+                    </VariationValue>
+                  </Variation>
+
+                  {previousValueDisplayInTooltip}
+                </VariationTooltip>
+              }
+            >
+              <Variation color={changeColor}>
+                <VariationIcon name={iconName} size={ICON_SIZE} />
+                <VariationValue showTooltip={false}>
+                  {changeDisplay}
+                </VariationValue>
+              </Variation>
+            </Tooltip>
+
+            {canShowPreviousValue && (
+              <PreviousValue id="SmartScalar-PreviousValue" responsive>
+                {previousValueDisplayInCard}
+              </PreviousValue>
+            )}
+          </PreviousValueContainer>
         )}
-
-        <PreviousValueWrapper data-testid="scalar-previous-value">
-          {lastChange == null || previousValue == null ? (
-            <div
-              className="text-centered text-bold mt1"
-              style={{ color: color("text-medium") }}
-            >{jt`Nothing to compare for the previous ${granularity}.`}</div>
-          ) : lastChange === 0 ? (
-            t`No change from last ${granularity}`
-          ) : (
-            <PreviousValueContainer>
-              <Tooltip
-                isEnabled={!canShowPreviousValue}
-                placement="bottom"
-                tooltip={
-                  <VariationTooltip>
-                    <Variation>
-                      <VariationIcon name={iconName} size={TOOLTIP_ICON_SIZE} />
-                      <VariationValue showTooltip={false}>
-                        {formatChange(lastChange)}
-                      </VariationValue>
-                    </Variation>
-
-                    {previousValueDisplayInTooltip}
-                  </VariationTooltip>
-                }
-              >
-                <Variation color={changeColor}>
-                  <VariationIcon name={iconName} size={ICON_SIZE} />
-                  <VariationValue showTooltip={false}>
-                    {changeDisplay}
-                  </VariationValue>
-                </Variation>
-              </Tooltip>
-
-              {canShowPreviousValue && (
-                <PreviousValue id="SmartScalar-PreviousValue" responsive>
-                  {previousValueDisplayInCard}
-                </PreviousValue>
-              )}
-            </PreviousValueContainer>
-          )}
-        </PreviousValueWrapper>
-      </ScalarWrapper>
-    );
-  }
+      </PreviousValueWrapper>
+    </ScalarWrapper>
+  );
 }
+
+Object.assign(SmartScalar, staticProps);
