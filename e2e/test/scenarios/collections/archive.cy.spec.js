c92ce8523482c6786f39258fcb725a82b4b21e44
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-09-08 13:01:44 -0400

    Refactor Archive App (#33601)
    * refactor use-list-select hook
    
    - toggleAll(items) felt unintuitive as some items could be selected while simultaneously unselecting others
      - switched to selectOnlyTheseItems(items) to be more explicit on what the function is doing
    - updated consumers of useListSelect to use the new API
    
    * convert to functional component
    
    * convert HoC ListSelect to hook useListSelect
    
    * convert redux connects to useSelectors
    
    * refactor ArchiveApp and improve types
    
    * remove unused eslint disable line
    
    * update useListSelect because of changes
    
    * convert ArchivedItem to TypeScript
    
    * refactor Search.loadList to useSearchListQuery
    
    * add E2E tests for archive page
    
    * refactor
    
    - remove dead code
    - refactor duplicated code
    
    * fix type error
    
    * fix variable usage
    
    * fix broken scroll update

--- e2e/test/scenarios/collections/archive.cy.spec.js
+++ e2e/test/scenarios/collections/archive.cy.spec.js
@@ -1,7 +1,7 @@
 import { getCollectionIdFromSlug, restore } from "e2e/support/helpers";
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 
-const { PEOPLE_ID } = SAMPLE_DATABASE;
+const { ORDERS, ORDERS_ID, PEOPLE_ID } = SAMPLE_DATABASE;
 
 const getQuestionDetails = collectionId => ({
   name: "A question",
@@ -13,6 +13,96 @@ describe("scenarios > collections > archive", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
+  });
+
+  it("should allow user to unarchive, delete, and undo archive of items", () => {
+    const DASHBOARD_NAME = "ARCHIVED DASHBOARD";
+
+    cy.createDashboard({
+      name: DASHBOARD_NAME,
+      dashboardDetails: { collection_id: null },
+    }).then(({ body: { id } }) => {
+      cy.archiveDashboard(id);
+    });
+
+    const COLLECTION_NAME = "ARCHIVED COLLECTION";
+    cy.createCollection({
+      name: COLLECTION_NAME,
+    }).then(({ body: { id } }) => {
+      cy.archiveCollection(id);
+    });
+
+    const QUESTION_NAME = "ARCHIVED QUESTION";
+    cy.createQuestion({
+      name: QUESTION_NAME,
+      query: {
+        "source-table": ORDERS_ID,
+        aggregation: [["count"]],
+        breakout: [
+          ["field", ORDERS.CREATED_AT, { "temporal-unit": "hour-of-day" }],
+        ],
+      },
+    }).then(({ body: { id } }) => {
+      cy.archiveQuestion(id);
+    });
+
+    cy.visit("/archive");
+
+    // test individual archive and undo
+    cy.findByTestId(`archive-item-${DASHBOARD_NAME}`)
+      .findByText(`${DASHBOARD_NAME}`)
+      .realHover()
+      .findByLabelText("unarchive icon")
+      .click();
+    cy.findByTestId(`archive-item-${DASHBOARD_NAME}`).should("not.exist");
+
+    cy.findByTestId("toast-undo").findByText("Undo").click();
+    cy.findByTestId(`archive-item-${DASHBOARD_NAME}`).should("exist");
+
+    // test bulk archive and undo
+    cy.findByTestId(`archive-item-${COLLECTION_NAME}`).within(() => {
+      cy.findByLabelText("archive-item-swapper").realHover().click();
+    });
+
+    cy.findByTestId("bulk-action-bar", { timeout: 5000 }).should("be.visible");
+    cy.findByTestId("bulk-action-bar").within(() => {
+      cy.findByLabelText("bulk-actions-input").click();
+      cy.findByText("Unarchive").click();
+    });
+
+    cy.findByTestId(`archive-item-${DASHBOARD_NAME}`).should("not.exist");
+    cy.findByTestId(`archive-item-${COLLECTION_NAME}`).should("not.exist");
+    cy.findByTestId(`archive-item-${QUESTION_NAME}`).should("not.exist");
+
+    cy.findByTestId("toast-undo").findByText("Undo").click();
+    cy.findByTestId(`archive-item-${DASHBOARD_NAME}`).should("exist");
+    cy.findByTestId(`archive-item-${COLLECTION_NAME}`).should("exist");
+    cy.findByTestId(`archive-item-${QUESTION_NAME}`).should("exist");
+
+    // test individual delete
+    cy.findByTestId(`archive-item-${DASHBOARD_NAME}`)
+      .findByText(`${DASHBOARD_NAME}`)
+      .realHover()
+      .findByLabelText("trash icon")
+      .click();
+    cy.findByTestId(`archive-item-${DASHBOARD_NAME}`).should("not.exist");
+
+    cy.findByTestId("toast-undo").should("not.exist");
+
+    // test bulk delete
+    cy.findByTestId(`archive-item-${COLLECTION_NAME}`)
+      .findByLabelText("archive-item-swapper")
+      .realHover()
+      .click();
+
+    cy.findByTestId("bulk-action-bar", { timeout: 5000 }).should("be.visible");
+    cy.findByTestId("bulk-action-bar").within(() => {
+      cy.findByLabelText("bulk-actions-input").click();
+      cy.findByText("Delete").click();
+    });
+
+    cy.findByTestId(`archive-item-${COLLECTION_NAME}`).should("exist"); // cannot delete collections
+    cy.findByTestId(`archive-item-${QUESTION_NAME}`).should("not.exist");
   });
 
   it("should load initially hidden archived items on scroll (metabase#24213)", () => {
