6d19ba942c9892352976f7f10dc4998ab3acc1d9
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-09-08 13:01:44 -0400

    Refactor Archive App (#33601)
    * refactor use-list-select hook
    
    - toggleAll(items) felt unintuitive as some items could be selected while simultaneously unselecting others
      - switched to selectOnlyTheseItems(items) to be more explicit on what the function is doing
    - updated consumers of useListSelect to use the new API
    
    * convert to functional component
    
    * convert HoC ListSelect to hook useListSelect
    
    * convert redux connects to useSelectors
    
    * refactor ArchiveApp and improve types
    
    * remove unused eslint disable line
    
    * update useListSelect because of changes
    
    * convert ArchivedItem to TypeScript
    
    * refactor Search.loadList to useSearchListQuery
    
    * add E2E tests for archive page
    
    * refactor
    
    - remove dead code
    - refactor duplicated code
    
    * fix type error
    
    * fix variable usage
    
    * fix broken scroll update

--- frontend/src/metabase/components/StackedCheckBox/StackedCheckBox.tsx
+++ frontend/src/metabase/components/StackedCheckBox/StackedCheckBox.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import PropTypes from "prop-types";
+import type { ChangeEvent } from "react";
 
 import {
   DEFAULT_CHECKED_COLOR,
@@ -14,26 +14,31 @@ import {
   Label,
 } from "./StackedCheckBox.styled";
 
-const propTypes = {
-  label: PropTypes.node,
-  checked: PropTypes.bool,
-  disabled: PropTypes.bool,
-  checkedColor: PropTypes.string,
-  uncheckedColor: PropTypes.string,
-  size: PropTypes.number,
-  className: PropTypes.string,
-};
+interface StackedCheckBoxPropTypes {
+  label?: string;
+  ariaLabel?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  checkedColor?: string;
+  uncheckedColor?: string;
+  size?: number;
+  className?: string;
+  indeterminate?: boolean;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
 
-function StackedCheckBox({
+export function StackedCheckBox({
   label,
-  checked,
+  ariaLabel,
+  checked = false,
   disabled = false,
   checkedColor = DEFAULT_CHECKED_COLOR,
   uncheckedColor = DEFAULT_UNCHECKED_COLOR,
   size = DEFAULT_SIZE,
   className,
-  ...props
-}) {
+  indeterminate = false,
+  onChange,
+}: StackedCheckBoxPropTypes) {
   const renderLabel = useCallback(() => {
     if (label == null) {
       return null;
@@ -42,7 +47,11 @@ function StackedCheckBox({
   }, [label]);
 
   return (
-    <StackedCheckBoxRoot className={className} disabled={disabled}>
+    <StackedCheckBoxRoot
+      aria-label={ariaLabel ?? ""}
+      className={className}
+      disabled={disabled}
+    >
       <OpaqueCheckBox
         label={renderLabel()}
         checked={checked}
@@ -50,11 +59,11 @@ function StackedCheckBox({
         checkedColor={checkedColor}
         uncheckedColor={uncheckedColor}
         size={size}
-        {...props}
+        indeterminate={indeterminate}
+        onChange={onChange}
       />
       <StackedBackground
         checked={checked}
-        disabled={disabled}
         checkedColor={checkedColor}
         uncheckedColor={uncheckedColor}
         size={size}
@@ -62,8 +71,3 @@ function StackedCheckBox({
     </StackedCheckBoxRoot>
   );
 }
-
-StackedCheckBox.propTypes = propTypes;
-StackedCheckBox.Label = Label;
-
-export default StackedCheckBox;
