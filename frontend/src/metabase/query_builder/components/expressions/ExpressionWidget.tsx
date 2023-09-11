f003cc21119e2c6d3fba447b7fd7bd88b696eeaf
I.B. Dwi <hi@ibedwi.dev>
2023-09-11 17:42:20 +0700

    fix: prevent space in the expression name

--- frontend/src/metabase/query_builder/components/expressions/ExpressionWidget.tsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionWidget.tsx
@@ -124,10 +124,23 @@ export const ExpressionWidget = (props: ExpressionWidgetProps): JSX.Element => {
             value={name}
             placeholder={t`Something nice and descriptive`}
             fullWidth
-            onChange={event => setName(event.target.value)}
+            onChange={event => {
+              // The expression's name shouldn't contain spaces. This checks
+              // handles if the user pastes a name with spaces.
+              if (event.target.value.includes(" ")) {
+                setName(event.target.value.replace(/\s/g, ""));
+              } else {
+                setName(event.target.value);
+              }
+            }}
             onKeyPress={e => {
               if (e.key === "Enter") {
                 handleCommit(expression);
+              }
+              // Prevent the user to type spaces since the expression's name
+              // can't contain spaces.
+              if (e.key === " " || e.key === "Spacebar") {
+                e.preventDefault();
               }
             }}
           />
