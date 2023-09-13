e4058aee75da725bbee70be184b21a4cf6f75a96
Denis Berezin <denis.berezin@metabase.com>
2023-09-13 23:55:51 +0300

    Add reproduction test (#33873)

--- frontend/src/metabase/query_builder/components/notebook/steps/SortStep/SortStep.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/SortStep/SortStep.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 import { t } from "ttag";
 import { Icon } from "metabase/core/components/Icon";
-import QueryColumnPicker from "metabase/common/components/QueryColumnPicker";
+import { QueryColumnPicker } from "metabase/common/components/QueryColumnPicker";
 import * as Lib from "metabase-lib";
 import type { NotebookStepUiComponentProps } from "../../types";
 import ClauseStep from "../ClauseStep";
