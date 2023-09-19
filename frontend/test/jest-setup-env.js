e610ddc8025cedf2ade3faae45c50479c0b6dc9f
Nicolo Pretto <info@npretto.com>
2023-09-19 17:14:24 +0200

    add custom response for api/database

--- frontend/test/jest-setup-env.js
+++ frontend/test/jest-setup-env.js
@@ -2,5 +2,20 @@ import fetchMock from "fetch-mock";
 
 beforeEach(() => {
   fetchMock.restore();
-  fetchMock.catch({});
+  fetchMock.catch((url, ...args) => {
+    console.log("CATCH FETCH MOCK");
+    console.log(url, args);
+
+    for (const customRule of Object.entries(CUSTOM_RESPONSES)) {
+      if (url.startsWith(customRule[0])) {
+        return customRule[1];
+      }
+    }
+
+    return {};
+  });
 });
+
+const CUSTOM_RESPONSES = {
+  "http://localhost/api/database": { data: [] },
+};
