f054376c17b5d70a809652fb1616432030542d0c
Callum Herries <hi@callumherries.com>
2023-09-20 15:40:12 +0300

    Fix test

--- test/metabase/sync/sync_metadata/sync_table_privileges_test.clj
+++ test/metabase/sync/sync_metadata/sync_table_privileges_test.clj
@@ -12,8 +12,7 @@
 
 (deftest sync-table-privileges!-test
   ;; TODO: The following expression should be replaced by
-  ;; (mt/test-drivers (mt/normal-drivers-with-feature :actions) ...)
-  ;; when table_privileges are implemented for all the actions drivers
+  ;; (mt/test-drivers (mt/normal-drivers-with-feature :table-privileges) ...)
   ;; and we should use more general DDL functions to create tables
   (mt/test-drivers #{:postgres}
     (testing "`TablePrivileges` should store the correct data for current_user and role privileges for postgres"
@@ -32,17 +31,18 @@
                       :update          true}]
                     (t2/select :model/TablePrivileges :table_id table-id :role nil))))))))
   (mt/test-drivers #{:mysql}
-    (testing "`TablePrivileges` should store the correct data for current_user and role privileges for postgres"
-      (mt/with-empty-db
-        (let [conn-spec (sql-jdbc.conn/db->pooled-connection-spec (mt/db))]
-          (jdbc/execute! conn-spec (str "CREATE TABLE baz (id INTEGER);"))
-          (sync-tables/sync-tables-and-database! (mt/db))
-          (sync-table-privileges/sync-table-privileges! (mt/db))
-          (let [table-id (t2/select-one-pk :model/Table :name "baz" :schema nil)]
-            (is (=? [{:table_id        table-id
-                      :role            nil
-                      :select          true
-                      :delete          true
-                      :insert          true
-                      :update          true}]
-                    (t2/select :model/TablePrivileges :table_id table-id :role nil)))))))))
+    (when (-> (mt/db) :dbms_version :flavor (not= "MariaDB"))
+      (testing "`TablePrivileges` should store the correct data for current_user and role privileges for postgres"
+        (mt/with-empty-db
+          (let [conn-spec (sql-jdbc.conn/db->pooled-connection-spec (mt/db))]
+            (jdbc/execute! conn-spec (str "CREATE TABLE baz (id INTEGER);"))
+            (sync-tables/sync-tables-and-database! (mt/db))
+            (sync-table-privileges/sync-table-privileges! (mt/db))
+            (let [table-id (t2/select-one-pk :model/Table :name "baz" :schema nil)]
+              (is (=? [{:table_id        table-id
+                        :role            nil
+                        :select          true
+                        :delete          true
+                        :insert          true
+                        :update          true}]
+                      (t2/select :model/TablePrivileges :table_id table-id :role nil))))))))))
