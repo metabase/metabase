bbafa6e5e05df300755c6a6cbeac895747388e2d
Callum Herries <hi@callumherries.com>
2023-09-20 16:15:28 +0300

    Try fix test in CI

--- test/metabase/driver/mysql_test.clj
+++ test/metabase/driver/mysql_test.clj
@@ -707,9 +707,8 @@
               supports-roles? (or (mysql/mariadb? (mt/db))
                                   (<= 8 (get-db-version spec)))
               get-privileges  (fn []
-                                (let [spec (sql-jdbc.conn/connection-details->spec :mysql (assoc details
-                                                                                                 :user "table_privileges_test_user"
-                                                                                                 :password "password"))]
+                                (sql-jdbc.conn/with-connection-spec-for-testing-connection
+                                  [spec [:mysql (assoc details :user "table_privileges_test_user" :password "password")]]
                                   (with-redefs [sql-jdbc.conn/db->pooled-connection-spec (fn [_] spec)]
                                     (driver/current-user-table-privileges driver/*driver* (assoc (mt/db) :name "table_privileges_test")))))]
           (try
