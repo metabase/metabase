7eb7c93e9013ee41e9e07d7dd3fcd8c200d90b77
Oisin Coveney <oisin@metabase.com>
2023-09-21 19:25:42 +0300

    Added some padding and things

--- frontend/src/metabase/ui/components/inputs/Switch/Switch.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Switch/Switch.styled.tsx
@@ -1,9 +1,36 @@
 import type { MantineThemeOverride } from "@mantine/core";
+import { rem, getSize } from "@mantine/core";
+
+const SIZES = {
+  xs: rem(12),
+  sm: rem(14),
+  md: rem(16),
+  lg: rem(20),
+};
+
+const SWITCH_PADDING = {
+  xs: rem(8),
+  sm: rem(8),
+  md: rem(16),
+  lg: rem(20),
+};
 
 export const getSwitchOverrides = (): MantineThemeOverride["components"] => ({
   Switch: {
     defaultProps: {
       color: "brand",
+    },
+    styles: (theme, params, { size = "md" }) => {
+      return {
+        labelWrapper: {
+          color: theme.colors.text[2],
+        },
+        label: {
+          fontWeight: 700,
+          fontSize: getSize({ size, sizes: SIZES }),
+          paddingLeft: getSize({ size, sizes: SWITCH_PADDING }),
+        },
+      };
     },
   },
 });
