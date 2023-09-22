476a8396d4e3c39f4a66a610246279c026a9cdc3
Oisin Coveney <oisin@metabase.com>
2023-09-22 17:09:43 +0300

    Update frontend/src/metabase/search/utils/enabled-search-type/enabled-search-type.ts
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- frontend/src/metabase/search/utils/enabled-search-type/enabled-search-type.ts
+++ frontend/src/metabase/search/utils/enabled-search-type/enabled-search-type.ts
@@ -2,7 +2,7 @@ import type { EnabledSearchModelType } from "metabase-types/api";
 import { enabledSearchTypes } from "metabase/search/constants";
 
 export function isEnabledSearchModelType(
-  value: any,
+  value: unknown,
 ): value is EnabledSearchModelType {
   return (
     typeof value === "string" && enabledSearchTypes.some(type => type === value)
