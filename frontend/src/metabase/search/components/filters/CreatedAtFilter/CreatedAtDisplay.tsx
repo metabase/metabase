c09c1cdd50b33144b520df5d9dc8d34e6e314cb9
Oisin Coveney <oisin@metabase.com>
2023-09-22 17:09:34 +0300

    Add change to create PR

--- frontend/src/metabase/search/components/filters/CreatedAtFilter/CreatedAtDisplay.tsx
+++ frontend/src/metabase/search/components/filters/CreatedAtFilter/CreatedAtDisplay.tsx
@@ -3,11 +3,13 @@ import type {SearchSidebarFilterComponent} from "metabase/search/types";
 import {CreatedAtFilter} from "metabase/search/components/filters/CreatedAtFilter/CreatedAtFilter";
 import {Text} from "metabase/ui"
 import {getFilterTitle} from "metabase/parameters/utils/date-formatting";
+import {formatParameterValue} from "metabase/parameters/utils/formatting";
 
 export const CreatedAtDisplay: SearchSidebarFilterComponent<"created_at">["DisplayComponent"] = (
   {
     value,
   }) => {
   console.log(value, value && getFilterTitle(value[0]))
-  return <Text>{value ? getFilterTitle(value) : CreatedAtFilter.title}</Text>
+  console.log("formatParameterValue", formatParameterValue(value[0]))
+  return <Text>{value ? getFilterTitle(value[0]) : CreatedAtFilter.title}</Text>
 }
