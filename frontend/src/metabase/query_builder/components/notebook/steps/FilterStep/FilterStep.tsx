efe521613257cddf0233e52af17a4c46a5a91b9f
Anton Kulyk <kuliks.anton@gmail.com>
2023-09-28 11:56:27 +0100

    Use MLv2 in `FilterStep`

--- frontend/src/metabase/query_builder/components/notebook/steps/FilterStep/FilterStep.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/FilterStep/FilterStep.tsx
@@ -1,37 +1,74 @@
+import { useMemo } from "react";
 import { t } from "ttag";
-
-import { FilterPopover } from "metabase/query_builder/components/filters/FilterPopover";
-
+import { FilterPicker } from "metabase/common/components/FilterPicker";
+import * as Lib from "metabase-lib";
 import type { NotebookStepUiComponentProps } from "../../types";
 import ClauseStep from "../ClauseStep";
 
 export function FilterStep({
-  query,
+  topLevelQuery,
+  step,
   color,
   isLastOpened,
   readOnly,
   updateQuery,
 }: NotebookStepUiComponentProps) {
+  const { stageIndex } = step;
+
+  const filters = useMemo(
+    () => Lib.filters(topLevelQuery, stageIndex),
+    [topLevelQuery, stageIndex],
+  );
+
+  const handleAddFilter = (filter: Lib.FilterClause) => {
+    const nextQuery = Lib.filter(topLevelQuery, stageIndex, filter);
+    updateQuery(nextQuery);
+  };
+
+  const handleUpdateFilter = (
+    filter: Lib.FilterClause,
+    nextFilter: Lib.FilterClause,
+  ) => {
+    const nextQuery = Lib.replaceClause(
+      topLevelQuery,
+      stageIndex,
+      filter,
+      nextFilter,
+    );
+    updateQuery(nextQuery);
+  };
+
+  const handleRemoveFilter = (filter: Lib.FilterClause) => {
+    const nextQuery = Lib.removeClause(topLevelQuery, stageIndex, filter);
+    updateQuery(nextQuery);
+  };
+
+  const renderFilterName = (filter: Lib.FilterClause) =>
+    Lib.displayInfo(topLevelQuery, stageIndex, filter).longDisplayName;
+
   return (
     <ClauseStep
-      color={color}
+      items={filters}
       initialAddText={t`Add filters to narrow your answer`}
-      items={query.filters()}
-      renderName={item => item.displayName()}
       readOnly={readOnly}
+      color={color}
+      isLastOpened={isLastOpened}
+      renderName={renderFilterName}
       renderPopover={filter => (
-        <FilterPopover
-          query={query}
+        <FilterPicker
+          query={topLevelQuery}
+          stageIndex={stageIndex}
           filter={filter}
-          onChangeFilter={newFilter =>
-            filter
-              ? updateQuery(filter.replace(newFilter))
-              : updateQuery(query.filter(newFilter))
-          }
+          onSelect={nextFilter => {
+            if (filter) {
+              handleUpdateFilter(filter, nextFilter);
+            } else {
+              handleAddFilter(nextFilter);
+            }
+          }}
         />
       )}
-      isLastOpened={isLastOpened}
-      onRemove={filter => updateQuery(filter.remove())}
+      onRemove={handleRemoveFilter}
     />
   );
 }
