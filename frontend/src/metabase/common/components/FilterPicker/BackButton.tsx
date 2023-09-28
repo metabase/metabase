93253d9717d7f6af1081e3fa924f0a0e4d8552f7
Anton Kulyk <kuliks.anton@gmail.com>
2023-09-28 16:49:23 +0100

    Refactor `BackButton`

--- frontend/src/metabase/common/components/FilterPicker/BackButton.tsx
+++ frontend/src/metabase/common/components/FilterPicker/BackButton.tsx
@@ -1,20 +1,21 @@
 import { t } from "ttag";
 import styled from "@emotion/styled";
 import { color } from "metabase/lib/colors";
-import type { ButtonProps } from "metabase/ui";
-import { Button } from "metabase/ui";
-import { Icon } from "metabase/core/components/Icon";
+import Button from "metabase/core/components/Button";
 
-export const BackButton = styled(Button)<
-  ButtonProps & { "aria-label"?: string; onClick: () => void }
->`
+export const BackButton = styled(Button)`
   padding: 0;
   color: ${color("text-dark")};
   font-size: 1.17em;
+
+  &:hover {
+    color: ${color("brand")};
+  }
 `;
 
 BackButton.defaultProps = {
-  variant: "subtle",
-  leftIcon: <Icon name="chevronleft" />,
+  borderless: true,
+  onlyText: true,
+  icon: "chevronleft",
   "aria-label": t`Back`,
 };
