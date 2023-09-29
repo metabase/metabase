0132ddd3b01050596743811f5677cf95d37380a1
Kamil Mielnik <kamil@kamilmielnik.com>
2023-09-29 17:33:01 +0700

    Fix useCallbackEffect typing

--- frontend/src/metabase/hooks/use-callback-effect.ts
+++ frontend/src/metabase/hooks/use-callback-effect.ts
@@ -2,10 +2,14 @@ import { useCallback, useEffect, useState } from "react";
 
 type Callback = () => void | Promise<void>;
 
+type IsScheduled = boolean;
+
+type ScheduleCallback = (callback: Callback) => void;
+
 /**
  * Will run callback once after a re-render.
  */
-export const useCallbackEffect = () => {
+export const useCallbackEffect = (): [IsScheduled, ScheduleCallback] => {
   const [callback, setCallback] = useState<Callback | null>(null);
   const isScheduled = callback !== null;
 
