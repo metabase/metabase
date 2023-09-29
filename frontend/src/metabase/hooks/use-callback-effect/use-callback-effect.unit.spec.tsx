0c8178c79f760b9750423b3e3e1d978d9b705c71
Kamil Mielnik <kamil@kamilmielnik.com>
2023-09-29 19:43:54 +0700

    Add a test case for scheduling

--- frontend/src/metabase/hooks/use-callback-effect/use-callback-effect.unit.spec.tsx
+++ frontend/src/metabase/hooks/use-callback-effect/use-callback-effect.unit.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import _ from "underscore";
 
 import { useCallbackEffect } from "./use-callback-effect";
@@ -10,13 +11,13 @@ interface Props {
 const TestComponent = ({ callback }: Props) => {
   const [isScheduled, scheduleCallback] = useCallbackEffect();
 
-  if (isScheduled) {
-    return <div>Scheduled</div>;
-  }
+  const handleScheduleClick = () => {
+    scheduleCallback(callback);
+  };
 
   return (
     <div>
-      <button onClick={() => scheduleCallback(callback)}>Schedule</button>
+      <button onClick={handleScheduleClick}>Schedule</button>
       <div>Status: {isScheduled ? "scheduled" : "not scheduled"}</div>
     </div>
   );
@@ -29,5 +30,15 @@ describe("useCallbackEffect", () => {
     render(<TestComponent callback={callback} />);
 
     expect(screen.getByText("Status: not scheduled")).toBeInTheDocument();
+  });
+
+  it("should be scheduled after calling scheduleCallback", () => {
+    const callback = jest.fn(_.noop);
+
+    render(<TestComponent callback={callback} />);
+
+    userEvent.click(screen.getByRole("button", { name: "Schedule" }));
+
+    expect(screen.getByText("Status: scheduled")).toBeInTheDocument();
   });
 });
