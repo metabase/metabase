651cf950b27cac406512a1715220ec9769c8ff17
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2023-09-29 17:42:33 -0400

    view_log migration to new audit_log table, and new recent_views table (#34109)

--- src/metabase/events/audit_log.clj
+++ src/metabase/events/audit_log.clj
@@ -1,8 +1,8 @@
 (ns metabase.events.audit-log
+  "This namespace is responsible for publishing events to the audit log. "
   (:require
    [metabase.api.common :as api]
    [metabase.events :as events]
-   [metabase.models.activity :as activity :refer [Activity]]
    [metabase.models.audit-log :as audit-log]
    [methodical.core :as methodical]
    [toucan2.core :as t2]))
@@ -15,16 +15,31 @@
 (derive :event/card-delete ::card-event)
 
 (methodical/defmethod events/publish-event! ::card-event
-  [topic object]
-  (audit-log/record-event! topic object))
+  [topic card]
+  (audit-log/record-event! topic card))
+
+(derive ::card-read-event :metabase/event)
+(derive :event/card-read ::card-read-event)
+
+(methodical/defmethod events/publish-event! ::card-read-event
+  [topic card]
+  (audit-log/record-event! topic card))
+
+(derive ::card-query-event ::event)
+(derive :event/card-query ::card-query-event)
+
+(methodical/defmethod events/publish-event! ::card-query-event
+  [topic {:keys [card_id] :as object}]
+  (let [details (select-keys object [:cached :ignore_cache :context])]
+    (audit-log/record-event! topic details api/*current-user-id* :model/Card card_id)))
 
 (derive ::dashboard-event ::event)
 (derive :event/dashboard-create ::dashboard-event)
 (derive :event/dashboard-delete ::dashboard-event)
 
 (methodical/defmethod events/publish-event! ::dashboard-event
-  [topic object]
-  (audit-log/record-event! topic object))
+  [topic dashboard]
+  (audit-log/record-event! topic dashboard))
 
 (derive ::dashboard-card-event ::event)
 (derive :event/dashboard-add-cards ::dashboard-card-event)
@@ -40,6 +55,20 @@
                                                (assoc :id id)
                                                (assoc :card_id card_id)))))]
    (audit-log/record-event! topic details api/*current-user-id* :model/Dashboard id)))
+
+(derive ::dashboard-read-event ::event)
+(derive :event/dashboard-read ::dashboard-read-event)
+
+(methodical/defmethod events/publish-event! ::dashboard-read-event
+  [topic dashboard]
+  (audit-log/record-event! topic dashboard))
+
+(derive ::table-read-event ::event)
+(derive :event/table-read ::table-read-event)
+
+(methodical/defmethod events/publish-event! ::table-read-event
+  [topic table]
+  (audit-log/record-event! topic table))
 
 (derive ::metric-event ::event)
 (derive :event/metric-create ::metric-event)
@@ -74,8 +103,8 @@
 (derive :event/segment-delete ::segment-event)
 
 (methodical/defmethod events/publish-event! ::segment-event
-  [topic object]
-  (audit-log/record-event! topic object))
+  [topic segment]
+  (audit-log/record-event! topic segment))
 
 (derive ::user-joined-event ::event)
 (derive :event/user-joined ::user-joined-event)
@@ -90,6 +119,6 @@
 (derive :event/install ::install-event)
 
 (methodical/defmethod events/publish-event! ::install-event
-  [_topic _event]
-  (when-not (t2/exists? Activity :topic "install")
-    (t2/insert! Activity, :topic "install", :model "install")))
+  [topic _event]
+  (when-not (t2/exists? :model/AuditLog :topic "install")
+    (audit-log/record-event! topic {})))
