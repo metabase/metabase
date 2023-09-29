05bca47040420682640dddcf68ea6405b4da4bc7
Oisin Coveney <oisin@metabase.com>
2023-09-29 14:06:44 +0300

    Remove unnecessary date picker filter

--- frontend/src/metabase/search/components/SearchFilterDatePicker/SearchFilterDatePicker.tsx
+++ frontend/src/metabase/search/components/SearchFilterDatePicker/SearchFilterDatePicker.tsx
@@ -1,10 +1,5 @@
-import { DATE_OPERATORS } from "metabase/query_builder/components/filters/pickers/DatePicker/DatePicker";
 import { DATE_SHORTCUT_OPTIONS } from "metabase/query_builder/components/filters/pickers/DatePicker/DatePickerShortcutOptions";
 import { SearchFilterDatePickerWrapper } from "metabase/search/components/SearchFilterDatePicker/SearchFilterDatePicker.styled";
-
-const CREATED_AT_FILTERS = DATE_OPERATORS.filter(
-  ({ name }) => name !== "exclude",
-);
 
 const CREATED_AT_SHORTCUTS = {
   ...DATE_SHORTCUT_OPTIONS,
@@ -23,7 +18,6 @@ export const SearchFilterDatePicker = ({
   <SearchFilterDatePickerWrapper
     value={value}
     setValue={(value: string | null) => onChange(value)}
-    operators={CREATED_AT_FILTERS}
     dateShortcutOptions={CREATED_AT_SHORTCUTS}
     withPadding={false}
   />
