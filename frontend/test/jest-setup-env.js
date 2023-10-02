fd8cb4f558dabfac1c8b40cc2a2ba6b435759262
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2023-10-02 17:30:48 +0300

    Try catch again

--- frontend/test/jest-setup-env.js
+++ frontend/test/jest-setup-env.js
@@ -2,4 +2,11 @@ import fetchMock from "fetch-mock";
 
 beforeEach(() => {
   fetchMock.restore();
+  fetchMock.catch((url, ...args) => {
+    console.error("Caught unmocked request to url: ", url);
+    console.warn(args);
+
+    // consider all unmocked requests are broken
+    return 500;
+  });
 });
