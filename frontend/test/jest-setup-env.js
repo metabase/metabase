33a89e57957071fc5ee1d6846c23504d96b2b927
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2023-10-04 13:59:42 +0300

    Fail CI if not mocked http requests found

--- frontend/test/jest-setup-env.js
+++ frontend/test/jest-setup-env.js
@@ -1,12 +1,11 @@
 import fetchMock from "fetch-mock";
-// jest overrides global.console, it's a workaround to show errors in silent mode (e.g. at CI)
-import * as console from "console";
 
 beforeEach(() => {
   fetchMock.restore();
-  fetchMock.catch((url, ...args) => {
-    console.error("Caught not mocked request to url: ", url);
-    console.warn(args);
+  fetchMock.catch((url, request) => {
+    const errorMessage = `Caught not mocked ${request.method} request to url: ${url}`;
+
+    Promise.reject(errorMessage);
 
     // consider all not mocked requests are broken
     return 500;
