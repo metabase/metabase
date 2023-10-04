c466b382586468edc565d82161ba56cdab6759ad
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2023-10-04 13:09:56 +0300

    Print not mocked http requests to console in silent mode

--- frontend/test/jest-setup-env.js
+++ frontend/test/jest-setup-env.js
@@ -1,12 +1,14 @@
 import fetchMock from "fetch-mock";
+// jest overrides global.console, it's a workaround to show errors in silent mode (e.g. at CI)
+import * as console from "console";
 
 beforeEach(() => {
   fetchMock.restore();
   fetchMock.catch((url, ...args) => {
-    console.error("Caught unmocked request to url: ", url);
+    console.error("Caught not mocked request to url: ", url);
     console.warn(args);
 
-    // consider all unmocked requests are broken
+    // consider all not mocked requests are broken
     return 500;
   });
 });
