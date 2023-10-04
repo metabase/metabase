dc0c13601b33755782e93e908b7cbb3c8ea9ace5
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-10-04 17:13:56 +0300

    Fix csp errors

--- frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/RelativeDatePicker.tsx
+++ frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/RelativeDatePicker.tsx
@@ -1,12 +1,12 @@
-import { Button, Group, Stack } from "metabase/ui";
-import * as Lib from "metabase-lib";
+import { useMemo, useState } from "react";
+import { Select } from "metabase/ui";
 import { BackButton } from "../BackButton";
 import type {
   DatePickerTruncationUnit,
   RelativeDatePickerValue,
 } from "../types";
-import { DEFAULT_VALUE, UNIT_GROUPS } from "./constants";
-import { getCurrentValue } from "./utils";
+import { DEFAULT_VALUE } from "./constants";
+import { getUnitOptions } from "./utils";
 
 interface RelativeDatePickerProps {
   value?: RelativeDatePickerValue;
@@ -14,45 +14,48 @@ interface RelativeDatePickerProps {
   onBack: () => void;
 }
 
-export const RelativeDatePicker = ({
-  value = DEFAULT_VALUE,
+export function RelativeDatePicker({
+  value: initialValue = DEFAULT_VALUE,
   onChange,
   onBack,
-}: RelativeDatePickerProps) => {
-  return (
-    <div>
-      <BackButton onClick={onBack} />
-      <CurrentPicker value={value} onChange={onChange} />
-    </div>
-  );
-};
+}: RelativeDatePickerProps) {
+  const [value, setValue] = useState(initialValue);
 
-interface CurrentPickerProps {
-  value?: RelativeDatePickerValue;
-  onChange: (value: RelativeDatePickerValue) => void;
-}
-
-const CurrentPicker = ({ value, onChange }: CurrentPickerProps) => {
-  const handleClick = (unit: DatePickerTruncationUnit) => {
-    onChange(getCurrentValue(unit));
+  const handleUnitChange = (unit: DatePickerTruncationUnit) => {
+    setValue({ ...value, unit });
   };
 
   return (
-    <Stack p="md">
-      {UNIT_GROUPS.map((group, groupIndex) => (
-        <Group key={groupIndex}>
-          {group.map(unit => (
-            <Button
-              key={unit}
-              variant={unit === value?.unit ? "filled" : "outline"}
-              radius="xl"
-              onClick={() => handleClick(unit)}
-            >
-              {Lib.describeTemporalUnit(unit)}
-            </Button>
-          ))}
-        </Group>
-      ))}
-    </Stack>
+    <div>
+      <BackButton onClick={onBack} />
+      <UnitSelect unit={value.unit} onChange={handleUnitChange} />
+    </div>
   );
-};
+}
+
+interface UnitSelectProps {
+  unit: DatePickerTruncationUnit;
+  onChange: (unit: DatePickerTruncationUnit) => void;
+}
+
+function UnitSelect({ unit, onChange }: UnitSelectProps) {
+  const options = useMemo(() => {
+    return getUnitOptions();
+  }, []);
+
+  const handleChange = (value: string | null) => {
+    const option = options.find(option => option.value === value);
+    if (option) {
+      onChange(option.value);
+    }
+  };
+
+  return (
+    <Select
+      data={options}
+      value={unit}
+      withinPortal={false}
+      onChange={handleChange}
+    />
+  );
+}
--- frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/utils.ts
+++ frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/utils.ts
@@ -1,3 +1,5 @@
+import * as Lib from "metabase-lib";
+import { DATE_PICKER_TRUNCATION_UNITS } from "../constants";
 import type {
   DatePickerTruncationUnit,
   RelativeDatePickerValue,
@@ -13,4 +15,11 @@ export function getCurrentValue(
     offsetUnit: null,
     offsetValue: null,
   };
+}
+
+export function getUnitOptions() {
+  return DATE_PICKER_TRUNCATION_UNITS.map(unit => ({
+    value: unit,
+    label: Lib.describeTemporalUnit(unit),
+  }));
 }
