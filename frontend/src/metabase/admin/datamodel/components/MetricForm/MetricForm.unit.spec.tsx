ae7de7e253a6eae9e325575d52443ef993b0679d
Kamil Mielnik <kamil@kamilmielnik.com>
2023-10-05 16:26:02 +0700

    Make MetricForm and SegmentForm tests consistent

--- frontend/src/metabase/admin/datamodel/components/MetricForm/MetricForm.unit.spec.tsx
+++ frontend/src/metabase/admin/datamodel/components/MetricForm/MetricForm.unit.spec.tsx
@@ -1,15 +1,15 @@
 import userEvent from "@testing-library/user-event";
-import { renderWithProviders, screen, waitFor } from "__support__/ui";
-import MetricApp from "metabase/admin/datamodel/containers/MetricApp";
-import { Route } from "metabase/hoc/Title";
 import { callMockEvent } from "__support__/events";
-import { BEFORE_UNLOAD_UNSAVED_MESSAGE } from "metabase/hooks/use-before-unload";
 import {
   setupDatabasesEndpoints,
   setupSearchEndpoints,
 } from "__support__/server-mocks";
-import { createMockDatabase } from "metabase-types/api/mocks";
+import { renderWithProviders, screen, waitFor } from "__support__/ui";
+import { createSampleDatabase } from "metabase-types/api/mocks/presets";
+import MetricApp from "metabase/admin/datamodel/containers/MetricApp";
 import { checkNotNull } from "metabase/core/utils/types";
+import { Route } from "metabase/hoc/Title";
+import { BEFORE_UNLOAD_UNSAVED_MESSAGE } from "metabase/hooks/use-before-unload";
 
 const TestHome = () => <div />;
 
@@ -20,7 +20,7 @@ interface SetupOpts {
 const setup = ({
   initialRoute = "/admin/datamodel/metric/create",
 }: SetupOpts = {}) => {
-  setupDatabasesEndpoints([createMockDatabase()]);
+  setupDatabasesEndpoints([createSampleDatabase()]);
   setupSearchEndpoints([]);
 
   const { history } = renderWithProviders(
--- frontend/src/metabase/admin/datamodel/components/SegmentForm/SegmentForm.unit.spec.tsx
+++ frontend/src/metabase/admin/datamodel/components/SegmentForm/SegmentForm.unit.spec.tsx
@@ -1,15 +1,15 @@
 import userEvent from "@testing-library/user-event";
-import { renderWithProviders, screen, waitFor } from "__support__/ui";
-import { Route } from "metabase/hoc/Title";
 import { callMockEvent } from "__support__/events";
-import { BEFORE_UNLOAD_UNSAVED_MESSAGE } from "metabase/hooks/use-before-unload";
-import SegmentApp from "metabase/admin/datamodel/containers/SegmentApp";
 import {
   setupDatabasesEndpoints,
   setupSearchEndpoints,
 } from "__support__/server-mocks";
-import { createSampleDatabase } from "metabase-types/api/mocks/presets/sample_database";
+import { renderWithProviders, screen, waitFor } from "__support__/ui";
+import { createSampleDatabase } from "metabase-types/api/mocks/presets";
+import SegmentApp from "metabase/admin/datamodel/containers/SegmentApp";
 import { checkNotNull } from "metabase/core/utils/types";
+import { Route } from "metabase/hoc/Title";
+import { BEFORE_UNLOAD_UNSAVED_MESSAGE } from "metabase/hooks/use-before-unload";
 
 const TestHome = () => <div />;
 
