5293cdc86d1e600079bb5984edfa37aa579841c8
Shaun Williams <shaun@metabase.com>
2023-10-05 12:11:35 -0500

    fix columnSettings default arg

--- frontend/src/metabase/visualizations/lib/settings/column.ts
+++ frontend/src/metabase/visualizations/lib/settings/column.ts
@@ -47,7 +47,7 @@ export function columnSettings({
   ...def
 }: VisualizationSettingDefinition<Series> & {
   getColumns?: GetColumns;
-}) {
+} = {}) {
   return nestedSettings<DatasetColumn>("column_settings", {
     section: t`Formatting`,
     objectName: "column",
