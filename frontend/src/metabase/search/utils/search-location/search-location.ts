e2d50ff5b0b2300134f3524f4e7cf0b7f3c1a987
Oisin Coveney <oisin@metabase.com>
2023-10-05 12:20:00 +0300

    Make initial changes

--- frontend/src/metabase/search/utils/search-location/search-location.ts
+++ frontend/src/metabase/search/utils/search-location/search-location.ts
@@ -6,9 +6,12 @@ import type {
 } from "metabase/search/types";
 import { SearchFilterKeys } from "metabase/search/constants";
 
-export function isSearchPageLocation(location: SearchAwareLocation): boolean {
-  const components = location.pathname.split("/");
-  return components[components.length - 1] === "search";
+export function isSearchPageLocation(location?: SearchAwareLocation): boolean {
+  if (location) {
+    const components = location.pathname.split("/");
+    return components[components.length - 1] === "search";
+  }
+  return false;
 }
 
 export function getSearchTextFromLocation(
