07f8f659d2b1e9cbb432caf47db1e40d9b7cc660
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-10-09 15:49:34 +0300

    Add basic utils

---
+++ frontend/src/metabase/common/components/DatePicker/SpecificDatePicker/utils.ts
@@ -0,0 +1,39 @@
+import moment from "moment-timezone";
+import type {
+  DatePickerOperator,
+  SpecificDatePickerOperator,
+  SpecificDatePickerValue,
+} from "../types";
+import { TABS } from "./constants";
+import type { Tab } from "./types";
+
+export function getTabs(availableOperators: DatePickerOperator): Tab[] {
+  return TABS.filter(tab => availableOperators.includes(tab.operator));
+}
+
+export function getDefaultValue(): SpecificDatePickerValue {
+  const today = moment().startOf("date").toDate();
+
+  return {
+    type: "specific",
+    operator: "between",
+    values: [today, today],
+  };
+}
+
+export function setOperator(
+  value: SpecificDatePickerValue,
+  operator: SpecificDatePickerOperator,
+): SpecificDatePickerValue {
+  switch (operator) {
+    case "=":
+    case "<":
+      return value.operator === "between"
+        ? { ...value, operator, values: [value.values[1]] }
+        : { ...value, operator, values: [value.values[0]] };
+    case ">":
+      return { ...value, operator, values: [value.values[0]] };
+    case "between":
+      return { ...value, operator, values: [value.values[0], value.values[0]] };
+  }
+}
