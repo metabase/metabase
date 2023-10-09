1eaa0c9a08dfec6013689a32168de79bae00dc55
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-10-09 23:38:17 +0300

    Fix types

--- frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/constants.ts
+++ frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/constants.ts
@@ -1,16 +1,10 @@
 import { t } from "ttag";
-import type { DatePickerTruncationUnit } from "../types";
 import type { Tab } from "./types";
 
 export const TABS: Tab[] = [
   { label: t`Past`, direction: "last" },
   { label: t`Current`, direction: "current" },
   { label: t`Next`, direction: "next" },
-];
-
-export const UNIT_GROUPS: DatePickerTruncationUnit[][] = [
-  ["day", "week", "month"],
-  ["quarter", "year"],
 ];
 
 export const DEFAULT_VALUE = {
