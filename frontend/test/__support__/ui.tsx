b6b14902ab054c62115ec755e52e5c96ea4ea5d7
Kamil Mielnik <kamil@kamilmielnik.com>
2023-10-09 19:29:01 +0700

    Rename waitForLoadingSpinnerToDisappear to waitForLoaderToBeRemoved

--- frontend/test/__support__/ui.tsx
+++ frontend/test/__support__/ui.tsx
@@ -196,14 +196,14 @@ export function getBrokenUpTextMatcher(textToFind: string): MatcherFunction {
  * The difference is that waitForElementToBeRemoved expects the element
  * to exist before being removed.
  *
- * The advantage of waitForLoadingSpinnerToDisappear is that it integrates
+ * The advantage of waitForLoaderToBeRemoved is that it integrates
  * better with our async entity framework because it addresses the
  * non-deterministic aspect of when loading states are displayed.
  *
  * @see https://github.com/metabase/metabase/pull/34272#discussion_r1342527087
  * @see https://metaboat.slack.com/archives/C505ZNNH4/p1684753502335459?thread_ts=1684751522.480859&cid=C505ZNNH4
  */
-export const waitForLoadingSpinnerToDisappear = async () => {
+export const waitForLoaderToBeRemoved = async () => {
   await waitFor(() => {
     expect(screen.queryByTestId("loading-spinner")).not.toBeInTheDocument();
   });
