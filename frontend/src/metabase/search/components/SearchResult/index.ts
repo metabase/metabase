423e00ce4d4cd0d120f28c489b90f8868a5af581
Oisin Coveney <oisin@metabase.com>
2023-10-10 13:25:12 +0300

    Split `SearchResult` components into separate files (#34466)

---
+++ frontend/src/metabase/search/components/SearchResult/index.ts
@@ -0,0 +1,9 @@
+export { SearchResult } from "./SearchResult";
+export {
+  ResultLink,
+  ResultButton,
+  ResultSpinner,
+  Title,
+  TitleWrapper,
+} from "./SearchResult.styled";
+export * from "./components";
