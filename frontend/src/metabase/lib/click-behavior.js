d2542d7ff74ca64dd9c31d83ad35c323d2bfe686
Ratul Sharker <ratul.sharker@dsinnovators.com>
2023-10-11 15:46:16 +0000

    - `var` changed to `const.

--- frontend/src/metabase/lib/click-behavior.js
+++ frontend/src/metabase/lib/click-behavior.js
@@ -84,8 +84,8 @@ function getTokenParameter(dashboard) {
 }
 
 function parseJWTPayload(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace('-', '+').replace('_', '/');
   return JSON.parse(atob(base64));
 }
 
