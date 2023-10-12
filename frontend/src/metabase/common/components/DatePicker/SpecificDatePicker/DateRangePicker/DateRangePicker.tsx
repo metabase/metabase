ff7aaf97e8b9cc75e508764faf4b4c00f222d8d3
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-10-12 16:37:36 +0400

    Fix passing date ranges

--- frontend/src/metabase/common/components/DatePicker/SpecificDatePicker/DateRangePicker/DateRangePicker.tsx
+++ frontend/src/metabase/common/components/DatePicker/SpecificDatePicker/DateRangePicker/DateRangePicker.tsx
@@ -46,7 +46,7 @@ export function DateRangePicker({
 
   return (
     <div>
-      <Stack p="md" align="center">
+      <Stack p="md">
         <Group align="center">
           <DateInput
             value={startDate}
