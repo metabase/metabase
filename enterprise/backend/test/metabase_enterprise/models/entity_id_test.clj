b8a1bfa0ba2f4153412a78a468ef105a30046a58
Alexander Solovyov <alexander@solovyov.net>
2023-10-12 13:47:53 +0300

    remove data-migrations ns entirely

--- enterprise/backend/test/metabase_enterprise/models/entity_id_test.clj
+++ enterprise/backend/test/metabase_enterprise/models/entity_id_test.clj
@@ -9,7 +9,6 @@
    [metabase-enterprise.serialization.v2.backfill-ids :as serdes.backfill]
    [metabase-enterprise.serialization.v2.seed-entity-ids :as v2.seed-entity-ids]
    #_{:clj-kondo/ignore [:deprecated-namespace]}
-   [metabase.db.data-migrations]
    [metabase.models]
    [metabase.models.revision-test]
    [metabase.models.serialization :as serdes]))
@@ -17,7 +16,6 @@
 (set! *warn-on-reflection* true)
 
 (comment metabase.models/keep-me
-         metabase.db.data-migrations/keep-me
          metabase.models.revision-test/keep-me)
 
 (def ^:private entities-external-name
