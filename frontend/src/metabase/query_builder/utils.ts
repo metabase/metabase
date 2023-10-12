d46a5219c38c5defbfc35845f30ae64296a5e321
Kamil Mielnik <kamil@kamilmielnik.com>
2023-10-12 14:00:01 +0700

    Make isNavigationAllowed tests pass

--- frontend/src/metabase/query_builder/utils.ts
+++ frontend/src/metabase/query_builder/utils.ts
@@ -92,11 +92,7 @@ export const isNavigationAllowed = ({
     return isQueryTab || isMetadataTab;
   }
 
-  /**
-   * If it's a new question, we're going to deal with it later as part of the epic:
-   * https://github.com/metabase/metabase/issues/33749
-   */
-  if (!isNewQuestion && question.isNative()) {
+  if (question.isNative()) {
     const isRunningQuestion = pathname === "/question" && hash.length > 0;
     return isRunningQuestion;
   }
