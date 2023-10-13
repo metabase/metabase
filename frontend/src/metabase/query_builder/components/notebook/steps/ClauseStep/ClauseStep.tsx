a8356ff23b5389f3441729f95a75ae60db8aa992
Anton Kulyk <kuliks.anton@gmail.com>
2023-10-13 18:25:44 +0100

    Minor `ClauseStep` clean up

--- frontend/src/metabase/query_builder/components/notebook/steps/ClauseStep/ClauseStep.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/ClauseStep/ClauseStep.tsx
@@ -7,54 +7,63 @@ import {
   NotebookCellItem,
 } from "../../NotebookCell";
 
-interface ClauseStepProps<T> {
+export interface ClauseStepProps<T> {
   color: string;
   items: T[];
-  renderName: (item: T, index: number) => JSX.Element | string;
-  renderPopover: (item?: T, index?: number) => JSX.Element | null;
-  canRemove?: (item: T) => boolean;
   isLastOpened?: boolean;
-  onRemove?: ((item: T, index: number) => void) | null;
   initialAddText?: string | null;
   tetherOptions?: Tether.ITetherOptions | null;
   readOnly?: boolean;
+  renderName: (item: T, index: number) => JSX.Element | string;
+  renderPopover: (item?: T, index?: number) => JSX.Element | null;
+  canRemove?: (item: T) => boolean;
+  onRemove?: ((item: T, index: number) => void) | null;
   "data-testid"?: string;
 }
 
 export const ClauseStep = <T,>({
   color,
   items,
-  renderName,
-  renderPopover,
-  canRemove,
-  onRemove = null,
   isLastOpened = false,
   initialAddText = null,
   tetherOptions = null,
   readOnly,
+  renderName,
+  renderPopover,
+  canRemove,
+  onRemove = null,
   ...props
 }: ClauseStepProps<T>): JSX.Element => {
+  const addItemElement = (
+    <NotebookCellAdd
+      color={color}
+      initialAddText={items.length === 0 && initialAddText}
+    />
+  );
+
+  const renderItem = (item: T, index: number) => (
+    <NotebookCellItem color={color} readOnly={readOnly}>
+      {renderName(item, index)}
+      {!readOnly && onRemove && (!canRemove || canRemove(item)) && (
+        <Icon
+          className="ml1"
+          name="close"
+          onClick={e => {
+            e.stopPropagation();
+            onRemove(item, index);
+          }}
+        />
+      )}
+    </NotebookCellItem>
+  );
+
   return (
     <NotebookCell color={color} data-testid={props["data-testid"]}>
       {items.map((item, index) => (
         <PopoverWithTrigger
-          tetherOptions={tetherOptions}
           key={index}
-          triggerElement={
-            <NotebookCellItem color={color} readOnly={readOnly}>
-              {renderName(item, index)}
-              {!readOnly && onRemove && (!canRemove || canRemove(item)) && (
-                <Icon
-                  className="ml1"
-                  name="close"
-                  onClick={e => {
-                    e.stopPropagation();
-                    onRemove(item, index);
-                  }}
-                />
-              )}
-            </NotebookCellItem>
-          }
+          triggerElement={renderItem(item, index)}
+          tetherOptions={tetherOptions}
           sizeToFit
         >
           {renderPopover(item, index)}
@@ -62,15 +71,10 @@ export const ClauseStep = <T,>({
       ))}
       {!readOnly && (
         <PopoverWithTrigger
-          triggerElement={
-            <NotebookCellAdd
-              color={color}
-              initialAddText={items.length === 0 && initialAddText}
-            />
-          }
+          isInitiallyOpen={isLastOpened}
+          triggerElement={addItemElement}
           tetherOptions={tetherOptions}
           sizeToFit
-          isInitiallyOpen={isLastOpened}
         >
           {renderPopover()}
         </PopoverWithTrigger>
