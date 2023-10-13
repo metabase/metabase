e2963f2e62cc2a93f248e1061b2c292e23bdca90
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-10-13 19:40:58 -0300

    rendering context

--- frontend/src/metabase/static-viz/components/ProgressBar/ProgressBar.tsx
+++ frontend/src/metabase/static-viz/components/ProgressBar/ProgressBar.tsx
@@ -2,7 +2,7 @@ import { t } from "ttag";
 import { scaleLinear } from "@visx/scale";
 import { Group } from "@visx/group";
 import { ClipPath } from "@visx/clip-path";
-import type { ColorGetter } from "metabase/static-viz/lib/colors";
+import type { ColorGetter } from "metabase/visualizations/types";
 import { formatNumber } from "../../lib/numbers";
 import { Text } from "../Text";
 import { Pointer } from "./Pointer";
