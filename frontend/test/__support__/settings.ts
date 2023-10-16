1b42f76acb52ac868141859e18e7029002ff5a69
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2023-10-16 07:51:48 -0600

    [AuditV2] Don't show curate permission option for Instance Analytics Collection (#34103)

--- frontend/test/__support__/settings.ts
+++ frontend/test/__support__/settings.ts
@@ -2,8 +2,11 @@ import MetabaseSettings from "metabase/lib/settings";
 import { createMockSettings } from "metabase-types/api/mocks";
 import { createMockSettingsState } from "metabase-types/store/mocks";
 import type { Settings } from "metabase-types/api";
+import type { EnterpriseSettings } from "metabase-enterprise/settings/types";
 
-export function mockSettings(params: Partial<Settings> = {}) {
+export function mockSettings(
+  params: Partial<Settings | EnterpriseSettings> = {},
+) {
   const settings = createMockSettings(params);
   const state = createMockSettingsState(settings);
 
