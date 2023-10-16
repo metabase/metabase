22d1000d0a031f4f3c091e29b480576736b8758f
Emmad Usmani <emmadusmani@berkeley.edu>
2023-10-16 16:06:48 -0700

    fix bug introduced while fixing types

--- frontend/src/metabase/static-viz/containers/StaticChart/utils.ts
+++ frontend/src/metabase/static-viz/containers/StaticChart/utils.ts
@@ -3,9 +3,30 @@ import { createColorGetter } from "metabase/static-viz/lib/colors";
 import { formatStaticValue } from "metabase/static-viz/lib/format";
 import { measureTextWidth } from "metabase/lib/measure-text";
 
-import type { IsomorphicChartProps } from "./types";
+import type { IsomorphicChartProps, StaticChartType } from "./types";
+import { ISOMORPHIC_CHART_TYPES } from "./constants";
 
-export function getIsomorhpicProps(options: any): IsomorphicChartProps {
+export function getIsomorhpicProps({
+  type,
+  options,
+}: {
+  type: StaticChartType;
+  options: any;
+}): IsomorphicChartProps {
+  const renderingContext = {
+    getColor: createColorGetter(options.colors),
+    formatValue: formatStaticValue,
+    measureText: measureTextWidth,
+    fontFamily: "Lato", // TODO make this based on admin settings value
+  };
+
+  // Only the isomorphic, echarts-based charts use rawSeries
+  // and are guranteed to have card and data parameters provided from
+  // resources/frontend_shared/static_viz_interface.js
+  if (!ISOMORPHIC_CHART_TYPES.includes(type)) {
+    return { rawSeries: [], renderingContext };
+  }
+
   // Not a thorough type validation but at least we can make sure
   // these are present.
   if (options.card == null || typeof options.card !== "object") {
@@ -17,11 +38,6 @@ export function getIsomorhpicProps(options: any): IsomorphicChartProps {
 
   return {
     rawSeries: [{ card: options.card, data: options.data }] as RawSeries,
-    renderingContext: {
-      getColor: createColorGetter(options.colors),
-      formatValue: formatStaticValue,
-      measureText: measureTextWidth,
-      fontFamily: "Lato", // TODO make this based on admin settings value
-    },
+    renderingContext,
   };
 }
