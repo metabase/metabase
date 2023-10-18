dc8b203adf82da4751d07993c70638c735dc39c9
Nick Fitzpatrick <nickfitz.582@gmail.com>
2023-10-18 15:59:49 -0300

    Ensure that field picker icon does not get smushed

--- frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
+++ frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
@@ -3,11 +3,13 @@ import { renderWithProviders, screen } from "__support__/ui";
 import {
   createSampleDatabase,
   ORDERS_ID,
+  ORDERS,
   SAMPLE_DB_ID,
 } from "metabase-types/api/mocks/presets";
 import registerVisualizations from "metabase/visualizations/register";
 import ChartSettings from "metabase/visualizations/components/ChartSettings";
-import { createMockColumn } from "metabase-types/api/mocks";
+import { createMockColumn, createMockDataset } from "metabase-types/api/mocks";
+import type { Series } from "metabase-types/api";
 import Question from "metabase-lib/Question";
 
 registerVisualizations();
@@ -16,41 +18,12 @@ const metadata = createMockMetadata({
   databases: [createSampleDatabase()],
 });
 
-const setup = () => {
-  const question = new Question(
-    {
-      dataset_query: {
-        type: "query",
-        query: {
-          "source-table": ORDERS_ID,
-          aggregrations: [["count"]],
-        },
-        database: SAMPLE_DB_ID,
-      },
-      display: "bar",
-      visualization_settings: {},
-    },
-    metadata,
-  );
+interface SetupProps {
+  question: Question;
+  series: Series;
+}
 
-  const series = [
-    {
-      card: question.card(),
-      data: {
-        rows: [[1000]],
-        cols: [
-          createMockColumn({
-            source: "aggregation",
-            field_ref: ["aggregation", 0],
-            name: "count",
-            display_name: "Count",
-            base_type: "type/BigInteger",
-          }),
-        ],
-      },
-    },
-  ];
-
+const setup = ({ series, question }: SetupProps) => {
   renderWithProviders(
     <ChartSettings
       series={series}
@@ -63,10 +36,101 @@ const setup = () => {
 
 describe("barchart", () => {
   it("should not error when rendering for a question with new breakouts", () => {
-    setup();
+    const question = new Question(
+      {
+        dataset_query: {
+          type: "query",
+          query: {
+            "source-table": ORDERS_ID,
+            aggregrations: [["count"]],
+          },
+          database: SAMPLE_DB_ID,
+        },
+        display: "bar",
+        visualization_settings: {},
+      },
+      metadata,
+    );
+
+    const series = [
+      {
+        card: question.card(),
+        ...createMockDataset({
+          data: {
+            cols: [
+              createMockColumn({
+                source: "aggregation",
+                field_ref: ["aggregation", 0],
+                name: "count",
+                display_name: "Count",
+                base_type: "type/BigInteger",
+              }),
+            ],
+          },
+        }),
+      },
+    ];
+    setup({ question, series });
 
     expect(screen.getByText("X-axis")).toBeInTheDocument();
     expect(screen.getByText("No valid fields")).toBeInTheDocument();
     expect(screen.getByText("Count")).toBeInTheDocument();
+  });
+
+  it("should not ellipsify really long field names", () => {
+    const longColumnName =
+      "COLUMN THAT WILL BE BIG BIG BIG VERY BIG IN ORDER TO ACCOMMODAT";
+    const question = new Question(
+      {
+        dataset_query: {
+          type: "query",
+          query: {
+            "source-table": ORDERS_ID,
+            aggregration: [["count"]],
+            breakout: [["expression", longColumnName]],
+            expressions: {
+              longColumnName: [
+                "get-month",
+                ["field", ORDERS.CREATED_AT, { "base-type": "type/DateTime" }],
+              ],
+            },
+          },
+          database: SAMPLE_DB_ID,
+        },
+        display: "bar",
+        visualization_settings: {},
+      },
+      metadata,
+    );
+
+    const series = [
+      {
+        card: question.card(),
+        ...createMockDataset({
+          data: {
+            cols: [
+              createMockColumn({
+                source: "aggregation",
+                field_ref: ["aggregation", 0],
+                name: "count",
+                display_name: "Count",
+                base_type: "type/BigInteger",
+              }),
+              createMockColumn({
+                source: "breakout",
+                field_ref: ["expression", longColumnName],
+                name: longColumnName,
+                display_name: longColumnName,
+                expression_name: longColumnName,
+                base_type: "type/Integer",
+              }),
+            ],
+          },
+        }),
+      },
+    ];
+    setup({ question, series });
+
+    expect(screen.getByText(longColumnName)).toBeInTheDocument();
   });
 });
