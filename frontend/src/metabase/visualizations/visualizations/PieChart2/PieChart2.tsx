595de08c0a4d62de63e3be7937e28fa1eba190f7
Emmad Usmani <emmadusmani@berkeley.edu>
2023-10-18 15:32:00 -0700

    render chart with echarts option object

--- frontend/src/metabase/visualizations/visualizations/PieChart2/PieChart2.tsx
+++ frontend/src/metabase/visualizations/visualizations/PieChart2/PieChart2.tsx
@@ -1,10 +1,14 @@
-import type { VisualizationProps } from "metabase/visualizations/types";
+import type {
+  RenderingContext,
+  VisualizationProps,
+} from "metabase/visualizations/types";
 import { EChartsRenderer } from "metabase/visualizations/components/EChartsRenderer";
 import { getPieChartModel } from "metabase/visualizations/echarts/pie/model";
 import { measureTextWidth } from "metabase/lib/measure-text";
 import { formatValue } from "metabase/lib/formatting/value";
 import { color } from "metabase/lib/colors";
 import type { OptionsType } from "metabase/lib/formatting/types";
+import { getPieChartOption } from "metabase/visualizations/echarts/pie/option";
 import PieChart from "../PieChart/PieChart";
 
 Object.assign(PieChart2, {
@@ -17,27 +21,28 @@ Object.assign(PieChart2, {
 // Only using this for testing, will
 // remove from this branch before merging
 export function PieChart2(props: VisualizationProps) {
-  const model = getPieChartModel(props.rawSeries, props.settings, {
+  const renderingContext: RenderingContext = {
     getColor: color,
     formatValue: (value, options) =>
       formatValue(value, options as OptionsType) as string,
     measureText: measureTextWidth,
     fontFamily: props.fontFamily,
-  });
-  //eslint-disable-next-line
-  console.log("model", model);
+  };
+
+  const chartModel = getPieChartModel(
+    props.rawSeries,
+    props.settings,
+    renderingContext,
+  );
+  const option = getPieChartOption(
+    chartModel,
+    props.settings,
+    renderingContext,
+  );
 
   return (
     <EChartsRenderer
-      option={{
-        series: {
-          type: "sunburst",
-          data: [
-            { name: "slice1", value: 20 },
-            { name: "slice2", value: 30 },
-          ],
-        },
-      }}
+      option={option}
       width={props.width}
       height={props.height}
       eventHandlers={[]}
