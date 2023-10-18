717a54c416fa8c47d5ff1ee86cd950c9018618eb
Oisin Coveney <oisin@metabase.com>
2023-10-18 13:21:14 +0300

    Add colors for collection icons

--- frontend/src/metabase/search/components/SearchResult/components/CollectionIcon.tsx
+++ frontend/src/metabase/search/components/SearchResult/components/CollectionIcon.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "metabase/core/components/Icon";
+import { color } from "metabase/lib/colors";
 import { PLUGIN_COLLECTIONS } from "metabase/plugins";
 import { DEFAULT_ICON_SIZE } from "metabase/search/components/SearchResult/components";
 import type { IconComponentProps } from "metabase/search/components/SearchResult/components/ItemIcon";
@@ -10,7 +11,13 @@ export function CollectionIcon({ item }: { item: IconComponentProps["item"] }) {
     PLUGIN_COLLECTIONS.isRegularCollection(item.collection);
 
   if (isRegular) {
-    return <Icon {...iconProps} size={DEFAULT_ICON_SIZE} />;
+    return (
+      <Icon
+        {...iconProps}
+        size={DEFAULT_ICON_SIZE}
+        color={color("text-light")}
+      />
+    );
   }
 
   return <Icon {...iconProps} width={20} height={24} />;
