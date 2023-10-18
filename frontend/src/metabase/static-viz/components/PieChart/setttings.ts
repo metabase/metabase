838e1f8334cc02c996a839b051a8ccbc63e77e9b
Emmad Usmani <emmadusmani@berkeley.edu>
2023-10-18 14:11:49 -0700

    use `getCommonStaticVizSettings`

--- frontend/src/metabase/static-viz/components/PieChart/setttings.ts
+++ frontend/src/metabase/static-viz/components/PieChart/setttings.ts
@@ -1,33 +1,9 @@
-import type {
-  ComputedVisualizationSettings,
-  RemappingHydratedDatasetColumn,
-} from "metabase/visualizations/types";
-import type {
-  DatasetColumn,
-  PieChartSettings,
-  RawSeries,
-  VisualizationSettings,
-} from "metabase-types/api";
+import type { ComputedVisualizationSettings } from "metabase/visualizations/types";
+import type { PieChartSettings, RawSeries } from "metabase-types/api";
 import { getDefaultDimensionAndMetric } from "metabase/visualizations/lib/utils";
 import { getColorsForValues } from "metabase/lib/colors/charts";
 import { SLICE_THRESHOLD } from "metabase/visualizations/echarts/pie/constants";
-import { normalize } from "metabase-lib/queries/utils/normalize";
-import { getColumnKey } from "metabase-lib/queries/utils/get-column-key";
-
-// TODO move this to base branch in shared location
-const getColumnSettings = (
-  column: DatasetColumn,
-  settings: VisualizationSettings,
-) => {
-  const columnKey = Object.keys(settings.column_settings ?? {}).find(
-    possiblyDenormalizedFieldRef =>
-      normalize(possiblyDenormalizedFieldRef) === getColumnKey(column),
-  );
-  if (!columnKey) {
-    return null;
-  }
-  return settings.column_settings?.[columnKey];
-};
+import { getCommonStaticVizSettings } from "metabase/static-viz/lib/settings";
 
 function getPieChartColors(
   rawSeries: RawSeries,
@@ -50,9 +26,7 @@ function getPieChartColors(
 export function computeStaticPieChartSettings(
   rawSeries: RawSeries,
 ): ComputedVisualizationSettings {
-  const settings: ComputedVisualizationSettings = {
-    ...rawSeries[0].card.visualization_settings,
-  };
+  const settings = getCommonStaticVizSettings(rawSeries);
 
   if (!settings["pie.dimension"] || !settings["pie.metric"]) {
     const defaults = getDefaultDimensionAndMetric(rawSeries);
@@ -65,9 +39,6 @@ export function computeStaticPieChartSettings(
   settings["pie.percent_visibility"] ??= "legend";
   settings["pie.slice_threshold"] ??= SLICE_THRESHOLD * 100;
   settings["pie.colors"] ??= getPieChartColors(rawSeries, settings);
-
-  settings.column = (column: RemappingHydratedDatasetColumn) =>
-    getColumnSettings(column, settings);
 
   return settings;
 }
