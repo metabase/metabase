4585854e2b53f54ac9a2d1907aac16dbc8e2acab
Emmad Usmani <emmadusmani@berkeley.edu>
2023-10-19 16:54:13 -0700

    render pie chart legend

--- frontend/src/metabase/static-viz/components/PieChart/PieChart.tsx
+++ frontend/src/metabase/static-viz/components/PieChart/PieChart.tsx
@@ -1,11 +1,18 @@
 import { init } from "echarts";
+import { Group } from "@visx/group";
+
 import type { IsomorphicStaticChartProps } from "metabase/static-viz/containers/IsomorphicStaticChart/types";
 import { sanitizeSvgForBatik } from "metabase/static-viz/lib/svg";
 
 import { getPieChartModel } from "metabase/visualizations/echarts/pie/model";
 import { getPieChartOption } from "metabase/visualizations/echarts/pie/option";
+
+import { getPieChartFormatters } from "metabase/visualizations/echarts/pie/format";
+import { calculateLegendRows } from "../Legend/utils";
+import { Legend } from "../Legend";
 import { computeStaticPieChartSettings } from "./setttings";
 
+const PADDING_TOP = 16; // TODO confirm with design
 const WIDTH = 540;
 const HEIGHT = 360;
 
@@ -23,11 +30,32 @@ export function PieChart({
     computedVizSettings,
     renderingContext,
   );
-  const option = getPieChartOption(
+  const formatters = getPieChartFormatters(
     chartModel,
     computedVizSettings,
     renderingContext,
   );
+  const option = getPieChartOption(
+    chartModel,
+    formatters,
+    computedVizSettings,
+    renderingContext,
+  );
+
+  const legendRows = calculateLegendRows(
+    chartModel.slices.map(s => ({
+      name: `${s.key} - ${formatters.formatPercent(s.normalizedPercentage)}`,
+      color: s.color,
+    })),
+    WIDTH,
+    24,
+    18,
+    400,
+  );
+  if (!legendRows) {
+    throw Error("Error calculating legend rows");
+  }
+  const { height: legendHeight, items } = legendRows;
 
   const chart = init(null, null, {
     renderer: "svg",
@@ -41,8 +69,14 @@ export function PieChart({
   const svg = sanitizeSvgForBatik(chart.renderToSVGString());
 
   return (
-    <svg width={WIDTH} height={HEIGHT}>
-      <g dangerouslySetInnerHTML={{ __html: svg }}></g>
+    <svg width={WIDTH} height={PADDING_TOP + HEIGHT + legendHeight}>
+      <Group top={PADDING_TOP}>
+        <Legend fontSize={18} fontWeight={400} items={items} />
+      </Group>
+      <Group
+        top={PADDING_TOP + legendHeight}
+        dangerouslySetInnerHTML={{ __html: svg }}
+      ></Group>
     </svg>
   );
 }
