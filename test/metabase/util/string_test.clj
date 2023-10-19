1f77d0ff2e828f11eb9e6d121610492a8b886616
Alexander Solovyov <alexander@solovyov.net>
2023-10-19 16:44:53 +0300

    code review fixes

--- test/metabase/util/string_test.clj
+++ test/metabase/util/string_test.clj
@@ -3,20 +3,28 @@
    [clojure.test :refer :all]
    [metabase.util.string :as u.str]))
 
-
 (deftest mask-test
-  (testing "mask works correctly in general case"
-    (is (= "qwer...uiop"
-           (u.str/mask "qwertyuiop"))))
+  (testing "mask"
+    (testing "works correctly in general case"
+      (is (= "qwer...uiop"
+             (u.str/mask "qwertyuiop"))))
 
-  (testing "mask works correctly with short strings"
-    (is (= "qw..."
-           (u.str/mask "qwer")))
-    (is (= "q..."
-           (u.str/mask "q"))))
+    (testing "works correctly with short strings"
+      (is (= "qw..."
+             (u.str/mask "qwer")))
+      (is (= "q..."
+             (u.str/mask "q"))))
 
-  (testing "mask does not throw errors for empty values"
-    (is (= ""
-           (u.str/mask "")))
-    (is (= nil
-           (u.str/mask nil)))))
+    (testing "does not throw errors for empty values"
+      (is (= ""
+             (u.str/mask "")))
+      (is (= nil
+             (u.str/mask nil))))
+
+    (testing "works with custom start-limit"
+      (is (= "abcd-efgh...-end"
+             (u.str/mask "abcd-efgh-ijkl-end" 9))))
+
+    (testing "works with custom end-limit"
+      (is (= "ab...ra"
+            (u.str/mask "abracadabra" 2 2))))))
