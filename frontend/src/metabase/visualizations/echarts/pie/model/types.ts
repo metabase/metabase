4b2da9a3f26e93304c401d39e9a0d596b5a8764b
Emmad Usmani <emmadusmani@berkeley.edu>
2023-10-19 07:47:28 -0700

    update `PieSlice.key` type to only be `string`

--- frontend/src/metabase/visualizations/echarts/pie/model/types.ts
+++ frontend/src/metabase/visualizations/echarts/pie/model/types.ts
@@ -11,7 +11,7 @@ export interface PieLegendItem {
 }
 
 export interface PieSlice {
-  key: string | number | boolean;
+  key: string;
   value: number;
   normalizedPercentage: number;
   rowIndex?: number;
