3a4848f6ea2ba9d3d562eb81e76dd1cf137dd560
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-10-20 18:32:41 +0400

    Add tests

--- e2e/test/scenarios/question/notebook-filters.cy.spec.js
+++ e2e/test/scenarios/question/notebook-filters.cy.spec.js
@@ -32,117 +32,62 @@ describe("scenarios > question > notebook filters", () => {
     describe("string columns", () => {
       it("equals operator", () => {
         visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
-        filter({ mode: "notebook" });
-        selectColumn("Title");
-        toggleOption("Aerodynamic Concrete Lamp");
-        addFilter();
-        verifyFilterName("Title is Aerodynamic Concrete Lamp");
-        visualize();
-        verifyRowCount(1);
+        addFilterAndVerify({
+          column: "Title",
+          operator: "Is",
+          options: ["Aerodynamic Concrete Lamp"],
+          filterName: "Title is Aerodynamic Concrete Lamp",
+          rowCount: 1,
+        });
+      });
+
+      it("equals operator with multiple options", () => {
+        visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
+        addFilterAndVerify({
+          column: "Title",
+          operator: "Is",
+          options: ["Aerodynamic Concrete Lamp", "Aerodynamic Cotton Bottle"],
+          filterName: "Title is 2 selections",
+          rowCount: 2,
+        });
       });
 
       it("not equals operator", () => {
         visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
-        filter({ mode: "notebook" });
-        selectColumn("Title");
-        selectOperator("Is", "Is not");
-        toggleOption("Aerodynamic Concrete Lamp");
-        addFilter();
-        verifyFilterName("Title is not Aerodynamic Concrete Lamp");
-        visualize();
-        verifyRowCount(199);
-      });
-
-      it("contains operator", () => {
-        visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
-        filter({ mode: "notebook" });
-        selectColumn("Title");
-        selectOperator("Is", "Contains");
-        enterValue("Enter some text", "Al");
-        addFilter();
-        verifyFilterName("Title contains Al");
-        visualize();
-        verifyRowCount(47);
-      });
-
-      it("contains operator with case sensitive option", () => {
-        visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
-        filter({ mode: "notebook" });
-        selectColumn("Title");
-        selectOperator("Is", "Contains");
-        enterValue("Enter some text", "Al");
-        toggleOption("Case sensitive");
-        addFilter();
-        verifyFilterName("Title contains Al");
-        visualize();
-        verifyRowCount(16);
-      });
-
-      it("contains operator without case sensitive option", () => {
-        visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
-        filter({ mode: "notebook" });
-        selectColumn("Title");
-        selectOperator("Is", "Contains");
-        enterValue("Enter some text", "Al");
-        toggleOption("Case sensitive");
-        toggleOption("Case sensitive");
-        addFilter();
-        verifyFilterName("Title contains Al");
-        visualize();
-        verifyRowCount(47);
-      });
-
-      it("does not contain operator", () => {
-        visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
-        filter({ mode: "notebook" });
-        selectColumn("Title");
-        selectOperator("Is", "Does not contain");
-        enterValue("Enter some text", "Al");
-        addFilter();
-        verifyFilterName("Title does not contain Al");
-        visualize();
-        verifyRowCount(153);
-      });
-
-      it("does not contain operator with case sensitive option", () => {
-        visitQuestionAdhoc(tableQuestion, { mode: "notebook" });
-        filter({ mode: "notebook" });
-        selectColumn("Title");
-        selectOperator("Is", "Does not contain");
-        enterValue("Enter some text", "Al");
-        toggleOption("Case sensitive");
-        addFilter();
-        verifyFilterName("Title does not contain Al");
-        visualize();
-        verifyRowCount(184);
+        addFilterAndVerify({
+          column: "Category",
+          operator: "Is not",
+          options: ["Widget"],
+          filterName: "Category is not Widget",
+          rowCount: 146,
+        });
       });
     });
   });
 });
 
-function selectColumn(columnName) {
-  popover().findByText(columnName).click();
-}
-
-function selectOperator(oldOperatorName, newOperatorName) {
-  popover().findByDisplayValue(oldOperatorName).click();
-  cy.findByRole("listbox").findByText(newOperatorName).click();
-}
-
-function toggleOption(optionName) {
-  popover().findByText(optionName).click();
-}
-
-function enterValue(placeholder, value) {
-  cy.findByPlaceholderText(placeholder).clear().type(value);
-}
-
-function addFilter() {
+function addFilterAndVerify({
+  column,
+  operator,
+  value,
+  placeholder,
+  options = [],
+  filterName,
+  rowCount,
+}) {
+  filter({ mode: "notebook" });
+  popover().findByText(column).click();
+  popover().findByDisplayValue("Is").click();
+  cy.findByRole("listbox").findByText(operator).click();
+  if (value) {
+    popover().findByPlaceholderText(placeholder).type(value);
+  } else {
+    options.forEach(option => popover().findByText(option).click());
+  }
   popover().button("Add filter").click();
-}
-
-function verifyFilterName(filterName) {
   getNotebookStep("filter").findByText(filterName).should("be.visible");
+  visualize();
+  verifyRowCount(rowCount);
 }
 
 function verifyRowCount(rowCount) {
