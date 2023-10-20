9f3aac20429ccdcb845052d6aaab959ec20467d5
Kamil Mielnik <kamil@kamilmielnik.com>
2023-10-20 19:39:08 +0700

    Remove unused alias

--- e2e/test/scenarios/dashboard-cards/click-behavior.cy.spec.js
+++ e2e/test/scenarios/dashboard-cards/click-behavior.cy.spec.js
@@ -13,7 +13,6 @@ describe("scenarios > dashboard > dashboard cards > click behavior", () => {
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
-    cy.intercept("POST", "/api/dataset").as("dataset");
   });
 
   describe("visualizations without click behavior", () => {
