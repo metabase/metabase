a069de32c21581ca1bdb1345341933aae1ad10dc
Bryan Maass <bryan.maass@gmail.com>
2023-10-20 14:57:00 -0600

    lint fix -- remove unused temp value

--- test/metabase/api/database_test.clj
+++ test/metabase/api/database_test.clj
@@ -1119,8 +1119,7 @@
     (mt/with-model-cleanup [:model/AuditLog :model/Activity]
       (mt/with-temp [Database    db       {:engine "h2", :details (:details (mt/db))}
                      Table       table  {:db_id (u/the-id db)}
-                     Field       field  {:table_id (u/the-id table)}
-                     FieldValues values {:field_id (u/the-id field), :values [1 2 3 4]}]
+                     Field       field  {:table_id (u/the-id table)}]
         (is (= {:status "ok"} (mt/user-http-request :crowberto :post 200 (format "database/%d/discard_values" (u/the-id db)))))
         (is (= (:id db) (:model_id (mt/latest-audit-log-entry))))))))
 
