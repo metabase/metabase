718d5275d85fa90cabe03c532e3525596ad70c7a
Cam Saul <github@camsaul.com>
2023-10-20 19:26:02 +0000

    Revert FE changes to fix failing tests?

--- frontend/src/metabase-lib/types.ts
+++ frontend/src/metabase-lib/types.ts
@@ -263,7 +263,6 @@ export type DrillThruType =
   | "drill-thru/summarize-column-by-time"
   | "drill-thru/column-filter"
   | "drill-thru/underlying-records"
-  | "drill-thru/zoom-in.geographic"
   | "drill-thru/zoom-in.timeseries";
 
 export type BaseDrillThruInfo<Type extends DrillThruType> = { type: Type };
