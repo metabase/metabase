d66644ad65ac9f0eb6fe3ba17065f0b0ee356257
Nick Fitzpatrick <nickfitz.582@gmail.com>
2023-10-20 16:17:26 -0300

    removing unhelpful test

--- frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
+++ frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
@@ -3,16 +3,12 @@ import { renderWithProviders, screen } from "__support__/ui";
 import {
   createSampleDatabase,
   ORDERS_ID,
-  ORDERS,
   SAMPLE_DB_ID,
 } from "metabase-types/api/mocks/presets";
-import registerVisualizations from "metabase/visualizations/register";
 import ChartSettings from "metabase/visualizations/components/ChartSettings";
 import { createMockColumn, createMockDataset } from "metabase-types/api/mocks";
 import type { Series } from "metabase-types/api";
 import Question from "metabase-lib/Question";
-
-registerVisualizations();
 
 const metadata = createMockMetadata({
   databases: [createSampleDatabase()],
@@ -60,7 +56,7 @@ describe("barchart", () => {
             cols: [
               createMockColumn({
                 source: "aggregation",
-                field_ref: ["aggregation", 0],
+                field_ref: ["aggregation", 0, null],
                 name: "count",
                 display_name: "Count",
                 base_type: "type/BigInteger",
@@ -75,62 +71,5 @@ describe("barchart", () => {
     expect(screen.getByText("X-axis")).toBeInTheDocument();
     expect(screen.getByText("No valid fields")).toBeInTheDocument();
     expect(screen.getByText("Count")).toBeInTheDocument();
-  });
-
-  it("should not ellipsify really long field names", () => {
-    const longColumnName =
-      "COLUMN THAT WILL BE BIG BIG BIG VERY BIG IN ORDER TO ACCOMMODAT";
-    const question = new Question(
-      {
-        dataset_query: {
-          type: "query",
-          query: {
-            "source-table": ORDERS_ID,
-            aggregration: [["count"]],
-            breakout: [["expression", longColumnName]],
-            expressions: {
-              longColumnName: [
-                "get-month",
-                ["field", ORDERS.CREATED_AT, { "base-type": "type/DateTime" }],
-              ],
-            },
-          },
-          database: SAMPLE_DB_ID,
-        },
-        display: "bar",
-        visualization_settings: {},
-      },
-      metadata,
-    );
-
-    const series = [
-      {
-        card: question.card(),
-        ...createMockDataset({
-          data: {
-            cols: [
-              createMockColumn({
-                source: "aggregation",
-                field_ref: ["aggregation", 0],
-                name: "count",
-                display_name: "Count",
-                base_type: "type/BigInteger",
-              }),
-              createMockColumn({
-                source: "breakout",
-                field_ref: ["expression", longColumnName],
-                name: longColumnName,
-                display_name: longColumnName,
-                expression_name: longColumnName,
-                base_type: "type/Integer",
-              }),
-            ],
-          },
-        }),
-      },
-    ];
-    setup({ question, series });
-
-    expect(screen.getByText(longColumnName)).toBeInTheDocument();
   });
 });
