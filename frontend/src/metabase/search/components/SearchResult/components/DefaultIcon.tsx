fb637ade41bef9e48aea084bbd7748b2c55372c9
Oisin Coveney <oisin@metabase.com>
2023-10-20 12:21:34 +0300

    Update frontend/src/metabase/search/components/SearchResult/components/DefaultIcon.tsx
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- frontend/src/metabase/search/components/SearchResult/components/DefaultIcon.tsx
+++ frontend/src/metabase/search/components/SearchResult/components/DefaultIcon.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "metabase/core/components/Icon";
-import type { IconComponentProps } from "metabase/search/components/SearchResult/components/ItemIcon";
+import type { IconComponentProps } from "./ItemIcon";
 import { DEFAULT_ICON_SIZE } from "./constants";
 
 export function DefaultIcon({ item }: { item: IconComponentProps["item"] }) {
