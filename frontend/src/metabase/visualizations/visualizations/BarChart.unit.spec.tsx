0cacd6b6ee4ca32c6c04deefff9d9618138c89a2
Nick Fitzpatrick <nick@metabase.com>
2023-10-25 10:39:57 -0300

    Manual backport of "Ensure that field picker icon does not get smushed" (#35027)
    * Ensure that field picker icon does not get smushed (#34808)
    
    * Ensure that field picker icon does not get smushed
    
    * removing unhelpful test
    
    * register visualizations in test
    
    * remove register visualizations

--- frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
+++ frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
@@ -6,48 +6,20 @@ import {
   SAMPLE_DB_ID,
 } from "metabase-types/api/mocks/presets";
 import ChartSettings from "metabase/visualizations/components/ChartSettings";
-import { createMockColumn } from "metabase-types/api/mocks";
+import { createMockColumn, createMockDataset } from "metabase-types/api/mocks";
+import type { Series } from "metabase-types/api";
 import Question from "metabase-lib/Question";
 
 const metadata = createMockMetadata({
   databases: [createSampleDatabase()],
 });
 
-const setup = () => {
-  const question = new Question(
-    {
-      dataset_query: {
-        type: "query",
-        query: {
-          "source-table": ORDERS_ID,
-          aggregrations: [["count"]],
-        },
-        database: SAMPLE_DB_ID,
-      },
-      display: "bar",
-      visualization_settings: {},
-    },
-    metadata,
-  );
+interface SetupProps {
+  question: Question;
+  series: Series;
+}
 
-  const series = [
-    {
-      card: question.card(),
-      data: {
-        rows: [[1000]],
-        cols: [
-          createMockColumn({
-            source: "aggregation",
-            field_ref: ["aggregation", 0, null],
-            name: "count",
-            display_name: "Count",
-            base_type: "type/BigInteger",
-          }),
-        ],
-      },
-    },
-  ];
-
+const setup = ({ series, question }: SetupProps) => {
   renderWithProviders(
     <ChartSettings
       series={series}
@@ -60,7 +32,41 @@ const setup = () => {
 
 describe("barchart", () => {
   it("should not error when rendering for a question with new breakouts", () => {
-    setup();
+    const question = new Question(
+      {
+        dataset_query: {
+          type: "query",
+          query: {
+            "source-table": ORDERS_ID,
+            aggregrations: [["count"]],
+          },
+          database: SAMPLE_DB_ID,
+        },
+        display: "bar",
+        visualization_settings: {},
+      },
+      metadata,
+    );
+
+    const series = [
+      {
+        card: question.card(),
+        ...createMockDataset({
+          data: {
+            cols: [
+              createMockColumn({
+                source: "aggregation",
+                field_ref: ["aggregation", 0, null],
+                name: "count",
+                display_name: "Count",
+                base_type: "type/BigInteger",
+              }),
+            ],
+          },
+        }),
+      },
+    ];
+    setup({ question, series });
 
     expect(screen.getByText("X-axis")).toBeInTheDocument();
     expect(screen.getByText("No valid fields")).toBeInTheDocument();
