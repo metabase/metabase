3317133fc857c2423bd35d799cd413c4311ee97a
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-10-25 15:12:37 +0700

    Fix dashboard crashing when updating

--- frontend/src/metabase/entities/collections/getExpandedCollectionsById.js
+++ frontend/src/metabase/entities/collections/getExpandedCollectionsById.js
@@ -53,7 +53,6 @@ function getExpandedCollectionsById(
       id: userPersonalCollectionId,
       parent: collectionsById[ROOT_COLLECTION.id],
       children: personalCollection?.children || [],
-      is_personal: true,
     });
   }
 
@@ -62,7 +61,6 @@ function getExpandedCollectionsById(
     ...PERSONAL_COLLECTIONS,
     parent: collectionsById[ROOT_COLLECTION.id],
     children: [],
-    is_personal: true,
   };
   collectionsById[ROOT_COLLECTION.id].children.push(
     collectionsById[PERSONAL_COLLECTIONS.id],
