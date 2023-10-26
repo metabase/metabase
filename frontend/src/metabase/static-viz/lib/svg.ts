bb0e25beed549f85c78749a99e1e2110efcc8495
Emmad Usmani <emmadusmani@berkeley.edu>
2023-10-26 14:26:41 -0700

    add dominant-baseline workaround

--- frontend/src/metabase/static-viz/lib/svg.ts
+++ frontend/src/metabase/static-viz/lib/svg.ts
@@ -1,3 +1,8 @@
+// eslint-disable-next-line import/no-unresolved -- vscode is confused since only @types/hast is installed
+import type { Element } from "hast";
+import { fromHtml } from "hast-util-from-html";
+import { toHtml } from "hast-util-to-html";
+
 // FIXME: instead of Regex parse svg, update, serialize
 const transformSvgForOutline = (svgString: string) => {
   const regex =
@@ -22,9 +27,42 @@ const transformSvgForOutline = (svgString: string) => {
   );
 };
 
-// Removing xmlns attributes that cause Batik failing to convert svg to png
-export const sanitizeSvgForBatik = (svg: string) => {
-  return transformSvgForOutline(svg)
+/**
+ * Fixes `<text>` elements not being vertically centered due to Batik not
+ * supporting the `dominant-baseline` property.
+ */
+export function patchDominantBaseline(svgString: string) {
+  const svgElem = fromHtml(svgString, { fragment: true, space: "svg" })
+    .children[0] as Element;
+
+  svgElem.children.forEach(child => {
+    if (
+      child.type !== "element" ||
+      child.tagName !== "text" ||
+      child.properties.dominantBaseline !== "central"
+    ) {
+      return;
+    }
+
+    child.properties.dy = "0.5em";
+  });
+
+  return toHtml(svgElem, { space: "svg" });
+}
+
+/**
+ * Removes `xmlns` attributes that Batik does not support
+ */
+function removeNamespace(svgString: string) {
+  return svgString
     .replace('xmlns="http://www.w3.org/2000/svg"', "")
     .replace('xmlns:xlink="http://www.w3.org/1999/xlink"', "");
+}
+
+export const sanitizeSvgForBatik = (svgString: string) => {
+  return [
+    transformSvgForOutline,
+    patchDominantBaseline,
+    removeNamespace,
+  ].reduce((currSVGString, transform) => transform(currSVGString), svgString);
 };
