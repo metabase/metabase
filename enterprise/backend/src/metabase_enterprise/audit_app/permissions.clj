3f3171078f49005d93fb31939c93636dbebf7a8a
Noah Moss <noahbmoss@gmail.com>
2023-10-26 17:22:56 -0400

    block unallowed audit queries in qp middleware

--- enterprise/backend/src/metabase_enterprise/audit_app/permissions.clj
+++ enterprise/backend/src/metabase_enterprise/audit_app/permissions.clj
@@ -1,16 +1,36 @@
 (ns metabase-enterprise.audit-app.permissions
   (:require
-   [metabase-enterprise.audit-db :refer [default-audit-collection
-                                         default-audit-db-id]]
+   [metabase-enterprise.audit-db
+    :refer [default-audit-collection default-audit-db-id]]
+   [metabase.lib.metadata :as lib.metadata]
    [metabase.models.permissions :as perms]
+   [metabase.models.query.permissions :as query-perms]
    [metabase.public-settings.premium-features :refer [defenterprise]]
+   [metabase.query-processor.store :as qp.store]
    [metabase.shared.util.i18n :refer [tru]]
    [toucan2.core :as t2]))
 
 (def audit-db-view-names
   "Used for giving granular permissions into the audit db. Instead of granting permissions to
    all of the audit db, we query the audit db using the names of each view that starts with v_."
-  ["v_users" "v_content" "v_group_members" "v_alerts_subscriptions" "v_activity"])
+  #{"v_users" "v_content" "v_group_members" "v_alerts_subscriptions" "v_activity"})
+
+(defenterprise check-audit-db-permissions
+  "Checks that a given query is not a native query, and only includes table IDs corresponding to the audit views
+  listed above. (These should be the only tables present in the DB anyway, but this is an extra check as a fallback measure)."
+  :feature :audit-app
+  [query]
+  ;; query->source-table-ids returns a set of table IDs or the ::query-perms/native keyword
+  (let [table-ids-or-native-kw (query-perms/query->source-table-ids query)]
+    (qp.store/with-metadata-provider (:database-id query)
+      (doseq [table-id table-ids-or-native-kw]
+        (when (= table-id ::query-perms/native)
+          (throw (ex-info (tru "Native queries are not allowed on the audit database")
+                          query)))
+        (when-not (audit-db-view-names
+                   (:name (lib.metadata/table (qp.store/metadata-provider) table-id)))
+          (throw (ex-info (tru "Audit queries are only allowed on audit views")
+                          query)))))))
 
 (defenterprise update-audit-collection-permissions!
   "Will remove or grant audit db (AppDB) permissions, if the instance analytics permissions changes."
--- src/metabase/query_processor/middleware/permissions.clj
+++ src/metabase/query_processor/middleware/permissions.clj
@@ -2,6 +2,7 @@
   "Middleware for checking that the current user has permissions to run the current query."
   (:require
    [clojure.set :as set]
+   [metabase-enterprise.audit-db :as audit-db]
    [metabase.api.common
     :refer [*current-user-id* *current-user-permissions-set*]]
    [metabase.config :as config]
@@ -11,6 +12,7 @@
    [metabase.models.permissions :as perms]
    [metabase.models.query.permissions :as query-perms]
    [metabase.plugins.classloader :as classloader]
+   [metabase.public-settings.premium-features :refer [defenterprise]]
    [metabase.query-processor.error-type :as qp.error-type]
    [metabase.query-processor.store :as qp.store]
    [metabase.query-processor.util.tag-referenced-cards
@@ -98,11 +100,31 @@
   "Used to allow users looking at a dashboard to view (possibly chained) filters."
   false)
 
+;; High-level approach: add a defenterprise with the premise of blocking audit queries that should be
+;; disallowed in all cases.
+;; - OSS implementation always throws — audit DB queries are not allowed
+;;- EE implementation :
+;; IF the database is the audit DB
+;;   - Native query -> throw always
+;;   - Non-native queries
+;;   - Call query->source-table-ids, then fetch the names of the tables from the QP store
+;;      - If they aren't in a hardcoded set of view names, throw an exception
+;;   - Otherwise, do nothing, and let the normal permission checking logic take over (i.e. based on card permissions)
+
+(defenterprise check-audit-db-permissions
+  "OSS implementation always throws an exception since queries over the audit DB are not permitted."
+  metabase-enterprise.audit-app.permissions
+  [query]
+  (throw (ex-info (tru "Querying this database requires the audit-app feature flag")
+                  query)))
+
 (mu/defn ^:private check-query-permissions*
   "Check that User with `user-id` has permissions to run `query`, or throw an exception."
   [{database-id :database, :as outer-query} :- [:map [:database ::lib.schema.id/database]]]
   (when *current-user-id*
     (log/tracef "Checking query permissions. Current user perms set = %s" (pr-str @*current-user-permissions-set*))
+    (when (= (audit-db/default-audit-db-id) database-id)
+     (check-audit-db-permissions outer-query))
     (cond
       *card-id*
       (do
