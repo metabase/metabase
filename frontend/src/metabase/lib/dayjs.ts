2fe7e51023c3bf883f5ccbb970d1989a3b066985
Anton Kulyk <kuliks.anton@gmail.com>
2023-10-26 14:44:16 +0100

    Remove `localeDataPlugin`

--- frontend/src/metabase/lib/dayjs.ts
+++ frontend/src/metabase/lib/dayjs.ts
@@ -1,8 +1,6 @@
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
-import localeDataPlugin from "dayjs/plugin/localeData";
 import updateLocalePlugin from "dayjs/plugin/updateLocale";
 
 dayjs.extend(customParseFormat);
-dayjs.extend(localeDataPlugin);
 dayjs.extend(updateLocalePlugin);
