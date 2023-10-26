530a802bc4e3827385acb6d76d9ce164a248f3f4
Anton Kulyk <kuliks.anton@gmail.com>
2023-10-26 12:09:22 +0100

    Move other plugins to `lib/dayjs`

--- frontend/src/metabase/lib/dayjs.ts
+++ frontend/src/metabase/lib/dayjs.ts
@@ -1,4 +1,8 @@
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
+import localeDataPlugin from "dayjs/plugin/localeData";
+import updateLocalePlugin from "dayjs/plugin/updateLocale";
 
 dayjs.extend(customParseFormat);
+dayjs.extend(localeDataPlugin);
+dayjs.extend(updateLocalePlugin);
--- frontend/src/metabase/lib/i18n.js
+++ frontend/src/metabase/lib/i18n.js
@@ -2,7 +2,6 @@ import { addLocale, useLocale } from "ttag";
 // eslint-disable-next-line no-restricted-imports -- deprecated usage
 import moment from "moment-timezone";
 import dayjs from "dayjs";
-import updateLocalePlugin from "dayjs/plugin/updateLocale";
 
 import MetabaseSettings from "metabase/lib/settings";
 import { DAY_OF_WEEK_OPTIONS } from "metabase/lib/date-time";
@@ -41,7 +40,6 @@ export function updateMomentStartOfWeek() {
 export function updateDayjsStartOfWeek() {
   const startOfWeekDay = getStartOfWeekDay();
   if (startOfWeekDay != null) {
-    dayjs.extend(updateLocalePlugin);
     dayjs.updateLocale(dayjs.locale(), { weekStart: startOfWeekDay });
   }
 }
