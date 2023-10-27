19347e26c623c3daf8c73f6c44bcf553c5fc323f
Oisin Coveney <oisin@metabase.com>
2023-10-27 13:53:04 +0300

    Modify analytics function to handle boolean reduction a little more gracefully

--- frontend/src/metabase/search/analytics.ts
+++ frontend/src/metabase/search/analytics.ts
@@ -1,29 +1,35 @@
 import { trackSchemaEvent } from "metabase/lib/analytics";
 import type { URLSearchFilterQueryParams } from "metabase/search/types";
 
-const isFilterApplied = (value: unknown, defaultValue: unknown = false) => {
-  return !!value ?? defaultValue;
-};
-
 export const trackSearchEvents = ({
   searchFilters,
 }: {
   searchFilters: URLSearchFilterQueryParams;
 }) => {
-  const content_type = searchFilters.type
-    ? Array.isArray(searchFilters.type)
+  let content_type = null;
+
+  if (searchFilters.type) {
+    content_type = Array.isArray(searchFilters.type)
       ? searchFilters.type
-      : [searchFilters.type]
-    : null;
+      : [searchFilters.type];
+  }
+
+  const filterBooleanPayload: { [key: string]: unknown } = {
+    creator: searchFilters.created_by,
+    last_editor: searchFilters.last_edited_by,
+    creation_date: searchFilters.created_at,
+    last_edit_date: searchFilters.last_edited_at,
+    verified_items: searchFilters.verified,
+    search_native_queries: searchFilters.search_native_query,
+  };
+
+  Object.entries(searchFilters).forEach(([key, value]) => {
+    filterBooleanPayload[key] = !!value;
+  });
 
   trackSchemaEvent("search", "1-0-1", {
     event: "search_results_filtered",
     content_type,
-    creator: isFilterApplied(searchFilters.created_by),
-    last_editor: isFilterApplied(searchFilters.last_edited_by),
-    creation_date: isFilterApplied(searchFilters.created_at),
-    last_edit_date: isFilterApplied(searchFilters.last_edited_at),
-    verified_items: isFilterApplied(searchFilters.verified),
-    search_native_queries: isFilterApplied(searchFilters.search_native_query),
+    filterPayload: filterBooleanPayload,
   });
 };
