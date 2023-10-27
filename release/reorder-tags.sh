87ee2bf73823b5250071aebcd90a05cf7885dee0
Ryan Laurie <iethree@gmail.com>
2023-10-27 13:03:26 -0600

    make yesterday work in linux, fix action path

--- release/reorder-tags.sh
+++ release/reorder-tags.sh
@@ -2,6 +2,7 @@
 # for some reason, github always puts our enterprise release notes above our open source
 # release notes on the releases page. We always want the open source release notes to be on top.
 # The hack we've found for this is to force-push yesterday's date into the enterprise release tag.
+# see https://github.com/orgs/community/discussions/8226
 
 # usage: ./reorder-tags.sh <enterprise-tag> <commit-hash>
 
@@ -22,7 +23,12 @@ echo "Reordering commits for $tagName on $commit"
 
 git fetch origin --tags --prune-tags --force
 
-yesterday="$(date -v-1d +%Y-%m-%d)T23:00:00Z"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # osx has to be special: https://stackoverflow.com/questions/9804966/date-command-does-not-follow-linux-specifications-mac-os-x-lion
+  yesterday="$(date -v-1d +%Y-%m-%d)T23:00:00Z"
+else
+  yesterday="$(date --date="yesterday" +%Y-%m-%d)T23:00:00Z"
+fi
 
 GIT_COMMITTER_DATE=$yesterday git tag -f -a -m "$tagName" "$tagName" "$commit"
 
