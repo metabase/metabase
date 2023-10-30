4cc00075e4f9d81683c732ee46e7fac37ff150fd
Nicolò Pretto <info@npretto.com>
2023-10-30 16:52:11 +0100

    refactor: exctracted "DASHBOARD_SCHEMA_VERSION" variable

--- frontend/src/metabase/dashboard/analytics.ts
+++ frontend/src/metabase/dashboard/analytics.ts
@@ -1,15 +1,17 @@
 import { trackSchemaEvent } from "metabase/lib/analytics";
 import type { DashboardId } from "metabase-types/api";
 
+const DASHBOARD_SCHEMA_VERSION = "1-1-2";
+
 export const trackAutoApplyFiltersDisabled = (dashboardId: DashboardId) => {
-  trackSchemaEvent("dashboard", "1-1-2", {
+  trackSchemaEvent("dashboard", DASHBOARD_SCHEMA_VERSION, {
     event: "auto_apply_filters_disabled",
     dashboard_id: dashboardId,
   });
 };
 
 export const trackExportDashboardToPDF = (dashboardId: DashboardId) => {
-  trackSchemaEvent("dashboard", "1-1-2", {
+  trackSchemaEvent("dashboard", DASHBOARD_SCHEMA_VERSION, {
     event: "dashboard_pdf_exported",
     dashboard_id: dashboardId,
   });
@@ -21,14 +23,14 @@ export const trackCardCreated = (type: CardTypes, dashboard_id: number) => {
   if (!type) {
     return;
   }
-  trackSchemaEvent("dashboard", "1-1-2", {
+  trackSchemaEvent("dashboard", DASHBOARD_SCHEMA_VERSION, {
     event: `new_${type}_card_created`,
     dashboard_id,
   });
 };
 
 export const trackCardMoved = (dashboardId: DashboardId) => {
-  trackSchemaEvent("dashboard", "1-1-2", {
+  trackSchemaEvent("dashboard", DASHBOARD_SCHEMA_VERSION, {
     event: `card_moved_to_tab`,
     dashboard_id: dashboardId,
   });
