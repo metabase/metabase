a13d33cb59be9c2d5c95eda1013619bbcd7cceb8
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-10-30 20:28:25 +0200

    Add a test

--- frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/CurrentDatePicker/CurrentDatePicker.unit.spec.tsx
+++ frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/CurrentDatePicker/CurrentDatePicker.unit.spec.tsx
@@ -1,6 +1,6 @@
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "__support__/ui";
-import type { RelativeDatePickerValue } from "../types";
+import type { RelativeDatePickerValue } from "../../types";
 import { CurrentDatePicker } from "./CurrentDatePicker";
 
 const DEFAULT_VALUE: RelativeDatePickerValue = {
@@ -16,12 +16,17 @@ interface SetupOpts {
 function setup({ value = DEFAULT_VALUE }: SetupOpts = {}) {
   const onChange = jest.fn();
 
-  render(<CurrentDatePicker value={DEFAULT_VALUE} onChange={onChange} />);
+  render(<CurrentDatePicker value={value} onChange={onChange} />);
 
   return { onChange };
 }
 
 describe("CurrentDatePicker", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date(2020, 0, 1));
+  });
+
   it("should be able to filter by a current interval", () => {
     const { onChange } = setup();
 
@@ -32,5 +37,15 @@ describe("CurrentDatePicker", () => {
       value: "current",
       unit: "week",
     });
+  });
+
+  it("should show the date range for the selected interval", () => {
+    setup();
+
+    userEvent.hover(screen.getByText("Week"));
+
+    expect(
+      screen.getByText("Right now, this is Dec 29, 2019 – Jan 4, 2020"),
+    ).toBeInTheDocument();
   });
 });
