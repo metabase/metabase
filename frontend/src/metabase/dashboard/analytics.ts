b80c6066459686978ccb6558ba5f60b6291f739f
Nicolò Pretto <info@npretto.com>
2023-10-30 16:51:22 +0100

    update all dashboard schema events to use version 1-1-2

--- frontend/src/metabase/dashboard/analytics.ts
+++ frontend/src/metabase/dashboard/analytics.ts
@@ -2,14 +2,14 @@ import { trackSchemaEvent } from "metabase/lib/analytics";
 import type { DashboardId } from "metabase-types/api";
 
 export const trackAutoApplyFiltersDisabled = (dashboardId: DashboardId) => {
-  trackSchemaEvent("dashboard", "1-0-1", {
+  trackSchemaEvent("dashboard", "1-1-2", {
     event: "auto_apply_filters_disabled",
     dashboard_id: dashboardId,
   });
 };
 
 export const trackExportDashboardToPDF = (dashboardId: DashboardId) => {
-  trackSchemaEvent("dashboard", "1-1-1", {
+  trackSchemaEvent("dashboard", "1-1-2", {
     event: "dashboard_pdf_exported",
     dashboard_id: dashboardId,
   });
@@ -21,7 +21,7 @@ export const trackCardCreated = (type: CardTypes, dashboard_id: number) => {
   if (!type) {
     return;
   }
-  trackSchemaEvent("dashboard", "1-1-1", {
+  trackSchemaEvent("dashboard", "1-1-2", {
     event: `new_${type}_card_created`,
     dashboard_id,
   });
