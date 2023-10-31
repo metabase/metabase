ec724bbb726f4204e2ddff1e765d3c92be3dc899
Ngoc Khuat <qn.khuat@gmail.com>
2023-10-31 18:15:51 +0700

    ee test fixes

--- enterprise/backend/test/metabase_enterprise/serialization/v2/seed_entity_ids_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/v2/seed_entity_ids_test.clj
@@ -54,46 +54,44 @@
            if this test fails, check if it really needs it.
            If yes, makes surethe exported data includes `:entity_id` column
            or the model implements [[serdes/hash-fields]] (#35097)"
-    (is (= (cond-> #{:model/MetricImportantField
-                     :model/ModerationReview
-                     :model/CollectionBookmark
-                     :model/Secret
-                     :model/GroupTableAccessPolicy
-                     :model/FieldValues
-                     :model/ModelIndex
-                     :model/DashboardCardSeries
-                     :model/ParameterCard
-                     :model/QueryAction
-                     :model/ImplicitAction
-                     :model/User
-                     :model/Revision
-                     :model/PermissionsRevision
-                     :model/CardBookmark
-                     :model/CollectionPermissionGraphRevision
-                     :model/BookmarkOrdering
-                     :model/ModelIndexValue
-                     :model/PermissionsGroupMembership
-                     :model/ViewLog
-                     :model/Field
-                     :model/QueryCache
-                     :model/ApplicationPermissionsRevision
-                     :model/LoginHistory
-                     :model/Database
-                     :model/Session
-                     :model/Permissions
-                     :model/TaskHistory
-                     :model/Setting
-                     :model/Activity
-                     :model/PulseChannelRecipient
-                     :model/TablePrivileges
-                     :model/TimelineEvent
-                     :model/PersistedInfo
-                     :model/HTTPAction
-                     :model/QueryExecution
-                     :model/DashboardBookmark
-                     :model/Table
-                     :model/Query
-                     :model/PermissionsGroup}
-             config/ee-available?
-             (conj :model/ConnectionImpersonation))
+    (is (= #{:model/MetricImportantField
+             :model/ModerationReview
+             :model/CollectionBookmark
+             :model/Secret
+             :model/GroupTableAccessPolicy
+             :model/FieldValues
+             :model/ModelIndex
+             :model/DashboardCardSeries
+             :model/DataMigrations
+             :model/ParameterCard
+             :model/QueryAction
+             :model/ImplicitAction
+             :model/User
+             :model/Revision
+             :model/PermissionsRevision
+             :model/CardBookmark
+             :model/CollectionPermissionGraphRevision
+             :model/BookmarkOrdering
+             :model/ModelIndexValue
+             :model/PermissionsGroupMembership
+             :model/ViewLog
+             :model/Field
+             :model/QueryCache
+             :model/ApplicationPermissionsRevision
+             :model/LoginHistory
+             :model/Database
+             :model/Session
+             :model/Permissions
+             :model/TaskHistory
+             :model/Setting
+             :model/Activity
+             :model/PulseChannelRecipient
+             :model/TimelineEvent
+             :model/PersistedInfo
+             :model/HTTPAction
+             :model/QueryExecution
+             :model/DashboardBookmark
+             :model/Table
+             :model/Query
+             :model/PermissionsGroup}
            (set/difference (set (v2.seed-entity-ids/toucan-models)) (#'v2.seed-entity-ids/entity-id-models))))))
