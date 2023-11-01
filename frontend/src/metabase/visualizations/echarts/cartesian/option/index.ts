49f51a3e747fc46b3fbbc5f194bcf99bee5e1dae
Emmad Usmani <emmadusmani@berkeley.edu>
2023-11-01 15:12:14 -0700

    combo chart goal line

--- frontend/src/metabase/visualizations/echarts/cartesian/option/index.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/option/index.ts
@@ -37,7 +37,31 @@ export const getCartesianChartOption = (
   return {
     grid: getChartGrid(chartModel, settings),
     dataset: echartsDataset,
-    series: echartsSeries,
+    series: [
+      {
+        type: "line",
+        markLine: {
+          data: [{ name: "goal-line", yAxis: settings["graph.goal_value"] }], // todo, how does this work with normalization?
+          label: {
+            position: "insideEndTop",
+            formatter: () => settings["graph.goal_label"],
+            fontFamily: renderingContext.fontFamily,
+            fontSize: 14,
+            fontWeight: 700,
+            color: renderingContext.getColor("text-medium"),
+            textBorderWidth: 1,
+            textBorderColor: renderingContext.getColor("white"),
+          },
+          symbol: ["none", "none"],
+          lineStyle: {
+            color: renderingContext.getColor("text-medium"),
+            type: [5, 5],
+            width: 2,
+          },
+        },
+      },
+      ...echartsSeries,
+    ],
     ...buildAxes(chartModel, settings, axesFormatters, renderingContext),
   } as EChartsOption;
 };
