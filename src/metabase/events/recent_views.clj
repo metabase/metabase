7aaf88eb1453e8ffefb8bd5f15ac6350ac5cb49b
Adam James <adam.vermeer2@gmail.com>
2023-11-02 15:48:15 -0700

    Only record card views for :card-read events
    This change alters when card events trigger view logs, namely, they'll only trigger when :card-read events occur.
    
    Prior, it was using :card-query. I don't entirely know why, I think that's a holdover from the older
    view-log/recent-views implementation.
    
    The shape of the view-log schema has also changed so the record-view! function is adjusted to reflect this.
    
    Before, there was a `(complement #{:dashboard :collection})` filter to prevent all pinned questions from being
    counted. Now, we DO want pinned card views to count, but only when they have their visualization turned on. Thus, we
    can remove `:collection` from the filter.
    
    It is notable that switching from `:card-query` to `:card-read` for the topics that get recorded solved a double
    record issue: 2 :card-query events get sent when a user decides to pin a card. Why this happens is TBD (and should be
    fixed because the audit-log does get double written here).
    
    Out of scope for this PR, but the audit-log is still recording :card-update, :card-read, :card-query (x2)... so that
    log can grow quite a bit still.

--- src/metabase/events/recent_views.clj
+++ src/metabase/events/recent_views.clj
@@ -17,12 +17,14 @@
 
 (defn record-view!
   "Simple base function for recording a view of a given `model` and `model-id` by a certain `user`."
-  [model model-id user-id metadata]
+  [{:keys [model model-id user-id context has_access]}]
   (t2/insert! :model/ViewLog
               :user_id  user-id
               :model    (u/lower-case-en model)
               :model_id model-id
-              :metadata metadata))
+              :context context
+              :has_access has_access
+              :metadata {}))
 
 (methodical/defmethod events/publish-event! ::event
   "Handle processing for a single event notification received on the view-log-channel"
@@ -30,17 +32,21 @@
   ;; try/catch here to prevent individual topic processing exceptions from bubbling up.  better to handle them here.
   (try
     (when object
-      (let [model                          (events/topic->model topic)
-            model-id                       (events/object->model-id topic object)
-            user-id                        api/*current-user-id*
+      (let [model                        (events/topic->model topic)
+            model-id                     (events/object->model-id topic object)
+            user-id                      api/*current-user-id*
             ;; `:context` comes
             ;; from [[metabase.query-processor.middleware.process-userland-query/add-and-save-execution-info-xform!]],
             ;; and it should only be present for `:event/card-query`
-            {:keys [context] :as metadata} (events/object->metadata object)]
-        (when (and (#{:event/card-query :event/dashboard-read :event/table-read} topic)
-                   ;; we don't want to count pinned card views
-                   ((complement #{:collection :dashboard}) context))
-          (recent-views/update-users-recent-views! user-id model model-id))
-        (record-view! model model-id user-id metadata)))
+            {:keys [has_access context]} object]
+        (when (and (#{:event/card-read :event/dashboard-read :event/table-read} topic)
+                   ;; we do want to count pinned card views since there is now an option to turn off viz
+                   ((complement #{:dashboard}) context))
+          (recent-views/update-users-recent-views! user-id model model-id)
+          (record-view! {:model      model
+                         :model-id   model-id
+                         :user-id    user-id
+                         :context    (name context)
+                         :has_access has_access}))))
     (catch Throwable e
       (log/warnf e "Failed to process activity event. %s" topic))))
