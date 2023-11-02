9839606a1bdeb6d2680f69a590b957ddb05432fd
Kamil Mielnik <kamil@kamilmielnik.com>
2023-11-02 23:18:33 +0700

    Convert click-behavior utils to TypeScript (#35211)
    * Rename click-behavior.unit.spec.js to click-behavior.unit.spec.ts
    
    * Rename click-behavior.js to click-behavior.ts
    
    * Fix typo
    
    * Type formatDateForParameterType
    
    * Move unit tests close to the file they're testing
    
    * Add type guards for ActionClickBehavior types
    
    * Replace isValidImplicitActionClickBehavior with an actual type guard
    
    * Type notRelativeDateOrRange
    
    * Add missing argument to formatDateForParameterType call
    
    * Type baseTypeFilterForParameterType
    
    * Remove redundant attribute
    
    * Remove unused isAction
    
    * Mark arguments as optional
    
    * Inline getTargetsWithSourceFilters, remove getTargetsForAction
    
    * Type clickBehaviorIsValid
    
    * Add type guards
    
    * Revert "Add type guards"
    
    This reverts commit 16492d1cbfbc2b231c18b91076e0151a59a3ce66.
    
    * Type getDataFromClicked
    - update unit tests for userAttributes
    
    * Make formatValueForLinkTemplate type-safe
    
    * Properly define SourceFilters, move types to top of the file
    
    * Remove redundant condition
    
    * Type getTargetsForDashboard
    
    * Introduce ClickBehaviorParameterSource & ClickBehaviorParameterTarget
    
    * Fix Parameter['type']
    
    * Refactor getTargetForQueryParams & getParameter to TS
    
    * Improve typing
    
    * Type formatSourceForTarget
    
    * Migrate metabase-lib/types/constants to TS
    
    * Improve getTargetsForQuestion typing
    
    * Introduce ClickBehaviorVariableTarget
    
    * Migrate getTargetsForQuestion to TS
    
    * Fix typing errors
    
    * Fix typing in tests
    
    * Fix typing in tests
    
    * Trick GH into giving a nice diff
    
    * Revert "Trick GH into giving a nice diff"
    
    This reverts commit 2adfc1d7232d6e82056756373c154c65c006361a.
    
    * Add an explanatory comment
    
    * Remove redundant checks
    
    * Rename isRecord to isObject
    
    * Remove redundant check
    
    * Allow null value for TemplateTag['default']
    
    * Fix tests
    
    * Remove redundant if
    
    * Fix user.login_attributes type

---
+++ frontend/src/metabase-lib/parameters/utils/click-behavior.ts
@@ -0,0 +1,400 @@
+import _ from "underscore";
+
+import type {
+  ClickBehavior,
+  ClickBehaviorDimensionTarget,
+  ClickBehaviorSource,
+  ClickBehaviorTarget,
+  Dashboard,
+  DashboardCard,
+  DatasetColumn,
+  DatetimeUnit,
+  Parameter,
+  ParameterValueOrArray,
+  UserAttribute,
+} from "metabase-types/api";
+import { isImplicitActionClickBehavior } from "metabase-types/guards";
+import type { ValueAndColumnForColumnNameDate } from "metabase/lib/formatting/link";
+import { parseTimestamp } from "metabase/lib/time";
+import {
+  formatDateTimeForParameter,
+  formatDateToRangeForParameter,
+} from "metabase/lib/formatting/date";
+import {
+  dimensionFilterForParameter,
+  variableFilterForParameter,
+} from "metabase-lib/parameters/utils/filters";
+import type { Dimension as DimensionType } from "metabase-lib/types";
+import { isa, isDate } from "metabase-lib/types/utils/isa";
+import { TYPE } from "metabase-lib/types/constants";
+import TemplateTagVariable from "metabase-lib/variables/TemplateTagVariable";
+import { TemplateTagDimension } from "metabase-lib/Dimension";
+import type Question from "metabase-lib/Question";
+import type { ClickObjectDataRow } from "metabase-lib/queries/drills/types";
+
+interface Target {
+  id: Parameter["id"];
+  name: Parameter["name"] | null | undefined;
+  target: ClickBehaviorTarget;
+  sourceFilters: SourceFilters;
+}
+
+interface SourceFilters {
+  column: (column: DatasetColumn) => boolean;
+  parameter: (parameter: Parameter) => boolean;
+  userAttribute: (userAttribute: string) => boolean;
+}
+
+interface ExtraData {
+  dashboard?: Dashboard;
+  dashboards?: Record<Dashboard["id"], Dashboard>;
+}
+
+export function getDataFromClicked({
+  extraData: { dashboard, parameterValuesBySlug = {}, userAttributes } = {},
+  dimensions = [],
+  data = [],
+}: {
+  extraData?: {
+    dashboard?: Dashboard;
+    parameterValuesBySlug?: Record<string, ParameterValueOrArray>;
+    userAttributes?: Record<UserAttribute, UserAttribute> | null;
+  };
+  dimensions?: DimensionType[];
+  data?: (ClickObjectDataRow & {
+    clickBehaviorValue?: ClickObjectDataRow["value"];
+  })[];
+}): ValueAndColumnForColumnNameDate {
+  const column = [
+    ...dimensions,
+    ...data.map(d => ({
+      column: d.col,
+      // When the data is changed to a display value for use in tooltips, we can set clickBehaviorValue to the raw value for filtering.
+      value: d.clickBehaviorValue || d.value,
+    })),
+  ]
+    .filter(d => d.column != null)
+    .reduce<ValueAndColumnForColumnNameDate["column"]>(
+      (acc, { column, value }) => {
+        if (!column) {
+          return acc;
+        }
+
+        const name = column.name.toLowerCase();
+
+        if (acc[name] === undefined) {
+          return { ...acc, [name]: { value, column } };
+        }
+
+        return acc;
+      },
+      {},
+    );
+
+  const dashboardParameters = (dashboard?.parameters || []).filter(
+    ({ slug }) => parameterValuesBySlug[slug] != null,
+  );
+
+  const parameterByName = Object.fromEntries(
+    dashboardParameters.map(({ name, slug }) => [
+      name.toLowerCase(),
+      { value: parameterValuesBySlug[slug] },
+    ]),
+  );
+
+  const parameterBySlug = _.mapObject(parameterValuesBySlug, value => ({
+    value,
+  }));
+
+  const parameter = Object.fromEntries(
+    dashboardParameters.map(({ id, slug }) => [
+      id,
+      { value: parameterValuesBySlug[slug] },
+    ]),
+  );
+
+  const userAttribute = Object.fromEntries(
+    Object.entries(userAttributes || {}).map(([key, value]) => [
+      key,
+      { value },
+    ]),
+  );
+
+  return { column, parameter, parameterByName, parameterBySlug, userAttribute };
+}
+
+const { Text, Number, Temporal } = TYPE;
+
+function notRelativeDateOrRange({ type }: Parameter) {
+  return type !== "date/range" && type !== "date/relative";
+}
+
+export function getTargetsForQuestion(question: Question): Target[] {
+  const query = question.query();
+  return [...query.dimensionOptions().all(), ...query.variables()].map(o => {
+    let id, target: ClickBehaviorTarget;
+    if (o instanceof TemplateTagVariable || o instanceof TemplateTagDimension) {
+      let name;
+      ({ id, name } = o.tag());
+      target = { type: "variable", id: name };
+    } else if ("mbql" in o) {
+      const dimension: ClickBehaviorDimensionTarget["dimension"] = [
+        "dimension",
+        o.mbql(),
+      ];
+      id = JSON.stringify(dimension);
+      target = { type: "dimension", id, dimension };
+    } else {
+      throw new Error("Unknown target type");
+    }
+    let parentType: string | undefined | null;
+    let parameterSourceFilter: SourceFilters["parameter"] = () => true;
+    if (o instanceof TemplateTagVariable) {
+      const type = o.tag()?.type;
+      parentType = type
+        ? {
+            card: undefined,
+            dimension: undefined,
+            snippet: undefined,
+            text: Text,
+            number: Number,
+            date: Temporal,
+          }[type]
+        : undefined;
+      parameterSourceFilter = parameter =>
+        variableFilterForParameter(parameter)(o);
+    } else {
+      const field = o.field();
+
+      if (field != null) {
+        const { base_type } = field;
+        parentType =
+          [Temporal, Number, Text].find(
+            t => typeof base_type === "string" && isa(base_type, t),
+          ) || base_type;
+        parameterSourceFilter = parameter =>
+          dimensionFilterForParameter(parameter)(o);
+      }
+    }
+
+    return {
+      id,
+      target,
+      name: o.displayName({ includeTable: true }),
+      sourceFilters: {
+        column: column =>
+          Boolean(
+            column.base_type && parentType && isa(column.base_type, parentType),
+          ),
+        parameter: parameterSourceFilter,
+        userAttribute: () => parentType === Text,
+      },
+    };
+  });
+}
+export function getTargetsForDashboard(
+  dashboard: Dashboard,
+  dashcard: DashboardCard,
+): Target[] {
+  if (!dashboard.parameters) {
+    return [];
+  }
+
+  return dashboard.parameters.map(parameter => {
+    const { type, id, name } = parameter;
+    const filter = baseTypeFilterForParameterType(type);
+    return {
+      id,
+      name,
+      target: { type: "parameter", id },
+      sourceFilters: {
+        column: c => notRelativeDateOrRange(parameter) && filter(c.base_type),
+        parameter: sourceParam => {
+          // parameter IDs are generated client-side, so they might not be unique
+          // if dashboard is a clone, it will have identical parameter IDs to the original
+          const isSameParameter =
+            dashboard.id === dashcard.dashboard_id &&
+            parameter.id === sourceParam.id;
+          return parameter.type === sourceParam.type && !isSameParameter;
+        },
+        userAttribute: () => !parameter.type.startsWith("date"),
+      },
+    };
+  });
+}
+
+function baseTypeFilterForParameterType(parameterType: string) {
+  const [typePrefix] = parameterType.split("/");
+  const allowedTypes = {
+    date: [TYPE.Temporal],
+    id: [TYPE.Integer, TYPE.UUID],
+    category: [TYPE.Text, TYPE.Integer],
+    location: [TYPE.Text],
+  }[typePrefix];
+  if (allowedTypes === undefined) {
+    // default to showing everything
+    return () => true;
+  }
+  return (baseType: string | undefined) => {
+    if (typeof baseType === "undefined") {
+      return false;
+    }
+    return allowedTypes.some(allowedType => isa(baseType, allowedType));
+  };
+}
+
+export function clickBehaviorIsValid(
+  clickBehavior: ClickBehavior | undefined | null,
+): boolean {
+  // opens drill-through menu
+  if (clickBehavior == null) {
+    return true;
+  }
+
+  if (clickBehavior.type === "crossfilter") {
+    return Object.keys(clickBehavior.parameterMapping || {}).length > 0;
+  }
+
+  if (clickBehavior.type === "action") {
+    return isImplicitActionClickBehavior(clickBehavior);
+  }
+
+  if (clickBehavior.type === "link") {
+    const { linkType } = clickBehavior;
+
+    // if it's not a crossfilter/action, it's a link
+    if (linkType === "url") {
+      return (clickBehavior.linkTemplate || "").length > 0;
+    }
+
+    // if we're linking to a Metabase entity we just need a targetId
+    if (linkType === "dashboard" || linkType === "question") {
+      return clickBehavior.targetId != null;
+    }
+  }
+
+  // we've picked "link" without picking a link type
+  return false;
+}
+
+export function formatSourceForTarget(
+  source: ClickBehaviorSource,
+  target: ClickBehaviorTarget,
+  {
+    data,
+    extraData,
+    clickBehavior,
+  }: {
+    data: ValueAndColumnForColumnNameDate;
+    extraData: ExtraData;
+    clickBehavior: ClickBehavior;
+  },
+) {
+  const datum = data[source.type][source.id.toLowerCase()] || {};
+  if (
+    "column" in datum &&
+    datum.column &&
+    isDate(datum.column) &&
+    typeof datum.value === "string"
+  ) {
+    const sourceDateUnit = datum.column.unit || null;
+
+    if (target.type === "parameter") {
+      // we should serialize differently based on the target parameter type
+      const parameter = getParameter(target, { extraData, clickBehavior });
+      if (parameter) {
+        return formatDateForParameterType(
+          datum.value,
+          parameter.type,
+          sourceDateUnit,
+        );
+      }
+    } else {
+      // If the target is a dimension or variable, we serialize as a date to remove the timestamp
+
+      if (
+        typeof sourceDateUnit === "string" &&
+        ["week", "month", "quarter", "year"].includes(sourceDateUnit)
+      ) {
+        return formatDateToRangeForParameter(datum.value, sourceDateUnit);
+      }
+
+      return formatDateForParameterType(
+        datum.value,
+        "date/single",
+        sourceDateUnit,
+      );
+    }
+  }
+
+  return datum.value;
+}
+
+function formatDateForParameterType(
+  value: string,
+  parameterType: string,
+  unit: DatetimeUnit | null,
+): string {
+  const m = parseTimestamp(value);
+  if (!m.isValid()) {
+    return String(value);
+  }
+
+  if (parameterType === "date/month-year") {
+    return m.format("YYYY-MM");
+  } else if (parameterType === "date/quarter-year") {
+    return m.format("[Q]Q-YYYY");
+  } else if (parameterType === "date/single") {
+    return m.format("YYYY-MM-DD");
+  } else if (parameterType === "date/all-options") {
+    return formatDateTimeForParameter(value, unit);
+  }
+
+  return value;
+}
+
+export function getTargetForQueryParams(
+  target: ClickBehaviorTarget,
+  {
+    extraData,
+    clickBehavior,
+  }: {
+    extraData: ExtraData;
+    clickBehavior: ClickBehavior;
+  },
+) {
+  if (target.type === "parameter") {
+    const parameter = getParameter(target, { extraData, clickBehavior });
+    return parameter && parameter.slug;
+  }
+  return target.id;
+}
+
+function getParameter(
+  target: ClickBehaviorTarget,
+  {
+    extraData,
+    clickBehavior,
+  }: {
+    extraData: ExtraData;
+    clickBehavior: ClickBehavior;
+  },
+): Parameter | undefined {
+  if (clickBehavior.type === "crossfilter") {
+    const parameters = extraData.dashboard?.parameters || [];
+    return parameters.find(parameter => parameter.id === target.id);
+  }
+
+  if (
+    clickBehavior.type === "link" &&
+    "linkType" in clickBehavior &&
+    (clickBehavior.linkType === "dashboard" ||
+      clickBehavior.linkType === "question")
+  ) {
+    const dashboard = (extraData.dashboards || {})[clickBehavior.targetId];
+    const parameters = dashboard?.parameters || [];
+    return parameters.find(parameter => parameter.id === target.id);
+  }
+
+  return undefined;
+}
