ace7d4fec457bf585ef9887c6e8b6a47df8082e7
Tamás Benkő <tamas@metabase.com>
2023-11-03 01:55:52 +0300

    Create writable connection in metabase.driver.sql-jdbc.execute

--- src/metabase/driver/sql_jdbc/execute.clj
+++ src/metabase/driver/sql_jdbc/execute.clj
@@ -704,7 +704,8 @@
     (do-with-connection-with-options
      driver
      (lib.metadata/database (qp.store/metadata-provider))
-     {:session-timezone (qp.timezone/report-timezone-id-if-supported driver (lib.metadata/database (qp.store/metadata-provider)))}
+     {:write? true
+      :session-timezone (qp.timezone/report-timezone-id-if-supported driver (lib.metadata/database (qp.store/metadata-provider)))}
      (fn [^Connection conn]
        (with-open [stmt (statement-or-prepared-statement driver conn sql params nil)]
          {:rows-affected (if (instance? PreparedStatement stmt)
