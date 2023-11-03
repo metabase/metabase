edb9e16fc5e6770224f8cbd185806d8d0d15263f
Kamil Mielnik <kamil@kamilmielnik.com>
2023-11-03 15:40:54 +0700

    Fix extra content taking too much space

--- frontend/src/metabase/core/components/AccordionList/AccordionListCell.styled.tsx
+++ frontend/src/metabase/core/components/AccordionList/AccordionListCell.styled.tsx
@@ -14,7 +14,7 @@ export const FilterContainer = styled.div`
 `;
 
 export const Content = styled.div<{ isClickable: boolean }>`
-  flex: 0 0 auto;
+  flex: 1 0 auto;
   display: flex;
   align-items: center;
   padding: 0.5rem;
