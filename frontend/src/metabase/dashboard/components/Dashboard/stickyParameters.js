b18f445808c4001c8d68bec3f3b8158c6fb74a06
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2023-11-03 17:12:09 +0300

    Revert back fns order

--- frontend/src/metabase/dashboard/components/Dashboard/stickyParameters.js
+++ frontend/src/metabase/dashboard/components/Dashboard/stickyParameters.js
@@ -2,13 +2,7 @@ import { isSmallScreen } from "metabase/lib/dom";
 
 export const MAXIMUM_PARAMETERS_FOR_STICKINESS = 5;
 
-const checkIfParametersWidgetShouldBeSticky = dashboard =>
-  !(
-    dashboard.state.parametersListLength > MAXIMUM_PARAMETERS_FOR_STICKINESS &&
-    isSmallScreen()
-  );
-
-// Dashboard Filters should always be sticky, except the case with small screen:
+// Dashboard filters container should always be sticky, except the case with small screen:
 // if more than MAXIMUM_PARAMETERS_FOR_STICKINESS parameters exist, we do not stick them to avoid
 // taking to much space on the screen
 export const updateParametersWidgetStickiness = dashboard => {
@@ -22,3 +16,9 @@ export const updateParametersWidgetStickiness = dashboard => {
     });
   }
 };
+
+const checkIfParametersWidgetShouldBeSticky = dashboard =>
+  !(
+    dashboard.state.parametersListLength > MAXIMUM_PARAMETERS_FOR_STICKINESS &&
+    isSmallScreen()
+  );
