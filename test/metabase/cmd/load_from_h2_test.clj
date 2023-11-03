c3ecb1b7370447e6fa4da18fc25c64eb40db9de8
Tamás Benkő <tamas@metabase.com>
2023-11-04 01:10:04 +0300

    Support calculating the major version from Liquibase migration

--- test/metabase/cmd/load_from_h2_test.clj
+++ test/metabase/cmd/load_from_h2_test.clj
@@ -1,6 +1,7 @@
 (ns metabase.cmd.load-from-h2-test
   (:require
    [clojure.test :refer :all]
+   [clojure.tools.logging :as log]
    [metabase.cmd.copy :as copy]
    [metabase.cmd.dump-to-h2 :as dump-to-h2]
    [metabase.cmd.load-from-h2 :as load-from-h2]
@@ -16,7 +17,12 @@
    [metabase.test :as mt]
    [metabase.test.data.interface :as tx]
    [toucan2.connection :as t2.conn]
-   [toucan2.core :as t2]))
+   [toucan2.core :as t2])
+  (:import
+   (liquibase LabelExpression Liquibase)
+   (liquibase.changelog ChangeSet)))
+
+(set! *warn-on-reflection* true)
 
 (defn- load-from-h2-test* [db-name thunk]
   ;; enable this test in the REPL with something like (mt/set-test-drivers! #{:postgres})
@@ -48,7 +54,7 @@
 (deftest ^:parallel load-from-h2-copy-details-enabled-test
   (binding [copy/*copy-h2-database-details* true]
     (load-from-h2-test*
-     "dump-test-2"
+     "dump-test-2"1
      (fn []
        (testing "H2 connection details SHOULD have been copied"
          (is (=? {:db string?}
@@ -68,18 +74,34 @@
   [h2-filename version]
   (str h2-filename "-" version))
 
+(defn- parse-major-version
+  []
+  (t2.conn/with-connection [conn]
+    (liquibase/with-liquibase [liquibase conn]
+      (let [change-sets (.listUnrunChangeSets ^Liquibase liquibase nil (LabelExpression.))
+            size (count change-sets)]
+        (when (pos? size)
+          (when-let [[_ major] (re-find #"v(\d+).*" (.getId ^ChangeSet (.get change-sets (dec size))))]
+            (parse-long major)))))))
+
+(def ^:private current-major-version
+  (delay (or (config/current-major-version)
+             (parse-major-version))))
+
 (defn- create-dumps
   [db-name h2-filename versions]
   (let [db-type driver/*driver*
         db-def {:database-name db-name}
-        current-version (config/current-major-version)
+        current-version @current-major-version
         data-source (get-data-source db-type db-def)]
+    (log/info "creating database")
     (create-current-database db-type db-def data-source)
     (binding [mdb.connection/*application-db* (mdb.connection/application-db db-type data-source)]
       (mt/dataset bird-flocks
         (doseq [version (sort > versions)]
           (let [filename (dump-filename h2-filename version)]
             (when (< version current-version)
+              (log/info "rolling back to version" version)
               (t2.conn/with-connection [conn]
                 (liquibase/with-liquibase [liquibase conn]
                   (liquibase/rollback-major-version db-type conn liquibase version))))
@@ -87,6 +109,7 @@
               (is (= 18 (ffirst (mt/formatted-rows [int]
                                   (mt/run-mbql-query bird
                                     {:aggregation [[:count]]}))))))
+            (log/info "creating dump" filename)
             (dump-to-h2/dump-to-h2! filename)))))))
 
 (defn- load-dump
@@ -100,26 +123,20 @@
         (is (= 200 (ffirst (mt/formatted-rows [int]
                              (mt/run-mbql-query incidents
                                {:aggregation [[:count]]})))))
+        (log/info "loading dump" h2-filename "version" version)
         (load-from-h2/load-from-h2! (dump-filename h2-filename version))
-        (when (= version (config/current-major-version))
+        (when (= version @current-major-version)
           (is (= 18 (ffirst (mt/formatted-rows [int]
                               (mt/run-mbql-query bird
                                 {:aggregation [[:count]]}))))))))))
 
 (deftest down-migrate-and-load-dump-test
-  (mt/test-drivers #{:h2 :mysql :postgres}
+  (mt/test-drivers #{:mysql :postgres}
     (mt/with-temp-dir [dir nil]
       (let [h2-filename (str dir "/dump")
-            current-version (or (config/current-major-version)
-                                (throw (ex-info "Couldn't determine current majore version" {})))
+            current-version (or @current-major-version
+                                (throw (ex-info "Couldn't determine current major version" {})))
             versions (range current-version (- current-version 4) -1)]
         (create-dumps "load-test-source" h2-filename versions)
         (doseq [version versions]
           (load-dump "load-test-target" h2-filename version))))))
-
-(comment
-  ;; $ docker run -name meta-postgres15-sample -d metabase/qa-databases:postgres-sample-15
-  (metabase.test.data.interface/db-test-env-var! :postgresql :user "metabase")
-  (metabase.test.data.interface/db-test-env-var! :postgresql :password "metasample123")
-  (mt/set-test-drivers! #{:postgres})
-  0)
