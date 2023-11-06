f526858d0cedba8f79aced4e4017d9d3dac42fe7
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-06 13:54:05 +0200

    Pass ref

--- frontend/src/metabase/ui/components/overlays/Popover/Popover.tsx
+++ frontend/src/metabase/ui/components/overlays/Popover/Popover.tsx
@@ -1,19 +1,10 @@
-import { useRef } from "react";
 import type { PopoverProps } from "@mantine/core";
 import { Popover as MantinePopover } from "@mantine/core";
-import { PopoverContext } from "./PopoverContext";
 import { PopoverDropdown } from "./PopoverDropdown";
-import { PopoverTarget } from "./PopoverTarget";
 
 export function Popover(props: PopoverProps) {
-  const targetRef = useRef<HTMLDivElement>(null);
-
-  return (
-    <PopoverContext.Provider value={{ targetRef }}>
-      <MantinePopover {...props} />
-    </PopoverContext.Provider>
-  );
+  return <MantinePopover {...props} />;
 }
 
-Popover.Target = PopoverTarget;
+Popover.Target = MantinePopover.Target;
 Popover.Dropdown = PopoverDropdown;
--- frontend/src/metabase/ui/components/overlays/Popover/PopoverContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import type { RefObject } from "react";
-
-export interface PopoverContextType {
-  targetRef?: RefObject<HTMLDivElement>;
-}
-
-export const PopoverContext = createContext<PopoverContextType>({});
