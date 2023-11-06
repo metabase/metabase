f3cefa419cca662bd8db048f80640666d1d78434
Ngoc Khuat <qn.khuat@gmail.com>
2023-11-06 16:09:26 +0700

    filter by 1 day

--- test/metabase/query_processor_test/timezones_test.clj
+++ test/metabase/query_processor_test/timezones_test.clj
@@ -332,7 +332,7 @@
           (doseq [timezone ["UTC" "Asia/Hong_Kong" "US/Hawaii" "America/Puerto_Rico"]]
             (mt/with-temporary-setting-values [report-timezone timezone]
               (let [query (mt/mbql-query relative_filter {:fields [$created]
-                                                          :filter [:time-interval $created -2 :day]})]
+                                                          :filter [:time-interval $created -1 :day]})]
                 (mt/with-native-query-testing-context query
                   (let [results (qp/process-query query)]
                     (is (=? {:status :completed}
