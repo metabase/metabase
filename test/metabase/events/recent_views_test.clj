5b68092807197d07f440aea6132148d9e58feaae
Noah Moss <noahbmoss@gmail.com>
2023-11-06 15:34:26 -0500

    fix more tests

--- test/metabase/events/recent_views_test.clj
+++ test/metabase/events/recent_views_test.clj
@@ -16,7 +16,10 @@
 (deftest card-query-test
   (mt/with-temp [Card card {:creator_id (mt/user->id :rasta)}]
     (mt/with-test-user :rasta
-      (events/publish-event! :event/card-query {:card_id (u/id card) :cached false :ignore_cache true})
+      (events/publish-event! :event/card-query {:card-id      (u/id card)
+                                                :user-id      (mt/user->id :rasta)
+                                                :cached       false
+                                                :ignore_cache true})
       (is (= {:user_id  (mt/user->id :rasta)
               :model    "card"
               :model_id (:id card)}
