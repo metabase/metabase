4937cf7d5e130753c21e9b8bbfb22f16ccb2bb16
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-07 20:56:54 +0200

    Add basic utils

--- frontend/src/metabase/querying/components/TimeseriesFooter/TimeseriesFooter.tsx
+++ frontend/src/metabase/querying/components/TimeseriesFooter/TimeseriesFooter.tsx
@@ -1,6 +1,10 @@
-import type * as Lib from "metabase-lib";
+import { useMemo } from "react";
+import { t } from "ttag";
+import { Group, Text } from "metabase/ui";
+import * as Lib from "metabase-lib";
 import type Question from "metabase-lib/Question";
-import { TimeseriesControls } from "./TimeseriesControls";
+import { TemporalBucketSelect } from "./TemporalBucketSelect";
+import { findBreakoutClause, findColumn } from "./utils";
 
 const STAGE_INDEX = -1;
 
@@ -25,5 +29,49 @@ export function TimeseriesFooter({
       stageIndex={STAGE_INDEX}
       onChange={handleChange}
     />
+  );
+}
+
+interface TimeseriesControlsProps {
+  query: Lib.Query;
+  stageIndex: number;
+  onChange: (query: Lib.Query) => void;
+}
+
+function TimeseriesControls({
+  query,
+  stageIndex,
+  onChange,
+}: TimeseriesControlsProps) {
+  const column = useMemo(
+    () => findColumn(query, stageIndex),
+    [query, stageIndex],
+  );
+
+  const breakout = useMemo(
+    () => column && findBreakoutClause(query, stageIndex, column),
+    [query, stageIndex, column],
+  );
+
+  const handleBreakoutChange = (newColumn: Lib.ColumnMetadata) => {
+    if (breakout) {
+      onChange(Lib.replaceClause(query, stageIndex, breakout, newColumn));
+    }
+  };
+
+  if (!column) {
+    return null;
+  }
+
+  return (
+    <Group p="md" position="center" spacing="sm">
+      <Text>{t`View by`}</Text>
+      <TemporalBucketSelect
+        query={query}
+        stageIndex={stageIndex}
+        column={column}
+        onChange={handleBreakoutChange}
+      />
+    </Group>
   );
 }
