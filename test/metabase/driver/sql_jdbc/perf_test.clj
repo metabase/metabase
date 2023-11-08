f4de79e92ebceedb88e254ff8f7284809bb2ad9b
Tamás Benkő <tamas@metabase.com>
2023-11-08 23:01:36 +0300

    Measure performance with least restrictive isolation level too

--- test/metabase/driver/sql_jdbc/perf_test.clj
+++ test/metabase/driver/sql_jdbc/perf_test.clj
@@ -17,8 +17,12 @@
      (run! :count (jdbc/query {:connection conn} [query])))))
 
 (deftest auto-commit-performance-test
-  (doseq [auto-commit [true false]]
-    (binding [sql-jdbc.execute/*read-only-connection-auto-commit* auto-commit]
+  (doseq [auto-commit [true false]
+          set-isolation-level (if auto-commit
+                                [false]
+                                [false true])]
+    (binding [sql-jdbc.execute/*read-only-connection-auto-commit* auto-commit
+              sql-jdbc.execute/*read-only-connection-set-isolation-level* set-isolation-level]
       (mt/test-drivers (descendants driver/hierarchy :sql-jdbc)
         (mt/dataset sample-dataset
           (let [queries (mapv (fn [i]
@@ -34,12 +38,16 @@
                               (range 1 16))]
             (run-query (first queries))
             #_{:clj-kondo/ignore [:discouraged-var]}
-            (printf "Benchmarking with auto-commit %5s for driver %s%n" auto-commit driver/*driver*)
+            (printf "Benchmarking with auto-commit %s with isolation-level %s for driver %s%n"
+                    auto-commit (if set-isolation-level "set" "unset") driver/*driver*)
             (flush)
             (binding [criterium/*sample-count* 10
                       criterium/*target-execution-time* (* 100 1000 1000)] ; ns
-              (criterium/bench
-                  (->> (for [query queries]
-                         (future (run-query query)))
-                       (run! deref))))
+              (let [results (criterium/benchmark
+                             (->> (for [query queries]
+                                    (future (run-query query)))
+                                  (run! deref))
+                             {})]
+                #_{:clj-kondo/ignore [:discouraged-var]}
+                (prn (dissoc results :samples :runtime-details :results))))
             (flush)))))))
