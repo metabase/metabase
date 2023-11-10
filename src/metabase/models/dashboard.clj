c7588db0648129bd70a1e314831e778caf7450b3
Alexander Solovyov <alexander@solovyov.net>
2023-11-10 17:38:16 +0200

    remove dashcards and tabs not in serialization data when loading (#35590)
    Cards are not touched, just DashboardCards and DashboardTabs are synchronized with loaded data
    
    Backport of #35530

--- src/metabase/models/dashboard.clj
+++ src/metabase/models/dashboard.clj
@@ -595,15 +595,28 @@
          :serdes/meta  [{:model "Dashboard"    :id (:entity_id dashboard)}
                         {:model "DashboardTab" :id (:entity_id tab)}]))
 
+(defn- drop-excessive-nested!
+  "Remove nested entities which are not present in incoming serialization load"
+  [hydration-key ingested local]
+  (let [local-nested    (get (t2/hydrate local hydration-key) hydration-key)
+        ingested-nested (get ingested hydration-key)
+        to-remove       (set/difference (set (map :entity_id local-nested))
+                                        (set (map :entity_id ingested-nested)))
+        model           (t2/model (first local-nested))]
+    (when (seq to-remove)
+      (t2/delete! model :entity_id [:in to-remove]))))
+
 ;; Call the default load-one! for the Dashboard, then for each DashboardCard.
 (defmethod serdes/load-one! "Dashboard" [ingested maybe-local]
   (let [dashboard ((get-method serdes/load-one! :default) (dissoc ingested :ordered_cards :ordered_tabs) maybe-local)]
+    (drop-excessive-nested! :ordered_tabs ingested dashboard)
     (doseq [tab (:ordered_tabs ingested)]
       (serdes/load-one! (dashtab-for tab dashboard)
                         (t2/select-one :model/DashboardTab :entity_id (:entity_id tab))))
+    (drop-excessive-nested! :ordered_cards ingested dashboard)
     (doseq [dashcard (:ordered_cards ingested)]
       (serdes/load-one! (dashcard-for dashcard dashboard)
-                        (t2/select-one 'DashboardCard :entity_id (:entity_id dashcard))))))
+                        (t2/select-one :model/DashboardCard :entity_id (:entity_id dashcard))))))
 
 (defn- serdes-deps-dashcard
   [{:keys [action_id card_id parameter_mappings visualization_settings]}]
