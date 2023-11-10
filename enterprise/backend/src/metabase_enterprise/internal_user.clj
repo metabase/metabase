ae391d9181a48afe2ff56bfb7418e28a3129c433
Noah Moss <noahbmoss@gmail.com>
2023-11-10 13:29:06 -0500

    revert changes to internal user

--- enterprise/backend/src/metabase_enterprise/internal_user.clj
+++ enterprise/backend/src/metabase_enterprise/internal_user.clj
@@ -4,8 +4,10 @@
             [metabase.util.log :as log]
             [toucan2.core :as t2]))
 
-(def ^:private internal-mb-user
-  {:id config/internal-mb-user-id
+(defn- install-internal-user! []
+  (t2/insert-returning-instances!
+   User
+   {:id config/internal-mb-user-id
     :first_name "Metabase"
     :last_name "Internal"
     :email "internal@metabase.com"
@@ -13,25 +15,12 @@
     :is_active false
     :is_superuser false
     :login_attributes nil
-    :sso_source nil})
-
-(defn- install-internal-user! []
-  (t2/insert-returning-instances! User internal-mb-user))
-
-(defn- internal-user-exists? []
-  (if-let [internal-user (t2/select-one User :email (:email internal-mb-user))]
-    (if (not= (:id internal-user) config/internal-mb-user-id)
-      (do
-        (log/info "Internal user already exists with an incorrect ID. Deleting...")
-        (t2/delete! User :email (:email internal-mb-user))
-        false)
-      true)
-    false))
+    :sso_source nil}))
 
 (defn ensure-internal-user-exists!
   "Creates the internal user"
   []
-  (if-not (internal-user-exists?)
+  (if-not (t2/exists? User :id config/internal-mb-user-id)
     (do (log/info "No internal user found, creating now...")
         (install-internal-user!)
         ::installed)
