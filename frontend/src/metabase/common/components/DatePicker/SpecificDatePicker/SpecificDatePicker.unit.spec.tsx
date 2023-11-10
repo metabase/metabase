e2d1948abe7b8655f05b581faea803fc26037f6b
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-10 19:06:59 +0200

    Add tests

--- frontend/src/metabase/common/components/DatePicker/SpecificDatePicker/SpecificDatePicker.unit.spec.tsx
+++ frontend/src/metabase/common/components/DatePicker/SpecificDatePicker/SpecificDatePicker.unit.spec.tsx
@@ -38,11 +38,11 @@ describe("SpecificDatePicker", () => {
   });
 
   it('should be able to set "on" filter', () => {
-    const { onChange } = setup();
+    const { onChange } = setup({ isNew: true });
 
     userEvent.click(screen.getByText("On"));
     userEvent.click(screen.getByText("15"));
-    userEvent.click(screen.getByText("Update filter"));
+    userEvent.click(screen.getByText("Add filter"));
 
     expect(onChange).toHaveBeenCalledWith({
       type: "specific",
@@ -52,11 +52,11 @@ describe("SpecificDatePicker", () => {
   });
 
   it('should be able to set "before" filter', () => {
-    const { onChange } = setup();
+    const { onChange } = setup({ isNew: true });
 
     userEvent.click(screen.getByText("Before"));
     userEvent.click(screen.getByText("15"));
-    userEvent.click(screen.getByText("Update filter"));
+    userEvent.click(screen.getByText("Add filter"));
 
     expect(onChange).toHaveBeenCalledWith({
       type: "specific",
@@ -66,12 +66,12 @@ describe("SpecificDatePicker", () => {
   });
 
   it('should be able to set "after" filter', () => {
-    const { onChange } = setup();
+    const { onChange } = setup({ isNew: true });
 
     userEvent.click(screen.getByText("After"));
     userEvent.clear(screen.getByLabelText("Date"));
     userEvent.type(screen.getByLabelText("Date"), "Feb 15, 2020");
-    userEvent.click(screen.getByText("Update filter"));
+    userEvent.click(screen.getByText("Add filter"));
 
     expect(onChange).toHaveBeenCalledWith({
       type: "specific",
@@ -81,17 +81,36 @@ describe("SpecificDatePicker", () => {
   });
 
   it('should be able to set "between" filter', () => {
-    const { onChange } = setup();
+    const { onChange } = setup({ isNew: true });
 
     const calendars = screen.getAllByRole("table");
     userEvent.click(within(calendars[0]).getByText("12"));
     userEvent.click(within(calendars[1]).getByText("5"));
-    userEvent.click(screen.getByText("Update filter"));
+    userEvent.click(screen.getByText("Add filter"));
 
     expect(onChange).toHaveBeenLastCalledWith({
       type: "specific",
       operator: "between",
       values: [new Date(2019, 11, 12), new Date(2020, 0, 5)],
+    });
+  });
+
+  it('should swap values for the "between" filter when min > max', () => {
+    const { onChange } = setup({ isNew: true });
+
+    const startDateInput = screen.getByLabelText("Start date");
+    userEvent.clear(startDateInput);
+    userEvent.type(startDateInput, "Feb 15, 2020");
+
+    const endDateInput = screen.getByLabelText("End date");
+    userEvent.clear(endDateInput);
+    userEvent.type(endDateInput, "Dec 29, 2019");
+    userEvent.click(screen.getByText("Add filter"));
+
+    expect(onChange).toHaveBeenLastCalledWith({
+      type: "specific",
+      operator: "between",
+      values: [new Date(2019, 11, 29), new Date(2020, 1, 15)],
     });
   });
 });
