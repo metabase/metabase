7664874a16f40681c84b6e20844a1edadac6853f
Tamás Benkő <tamas@metabase.com>
2023-11-12 15:41:24 +0300

    Run tests with joins too

--- test/metabase/driver/sql_jdbc/perf_test.clj
+++ test/metabase/driver/sql_jdbc/perf_test.clj
@@ -1,5 +1,7 @@
 (ns metabase.driver.sql-jdbc.perf-test
   (:require
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
    [clojure.java.jdbc :as jdbc]
    [clojure.test :refer :all]
    [criterium.core :as criterium]
@@ -19,25 +21,16 @@
    (fn [^java.sql.Connection conn]
      (jdbc/query {:connection conn} [query]))))
 
-(deftest auto-commit-performance-test
+(defn- run-auto-commit-performance-test
+  [make-query-fn {:keys [start bound] :as opts}]
   (doseq [auto-commit [true false]]
     (binding [sql-jdbc.execute/*read-only-connection-auto-commit* auto-commit]
       (mt/test-drivers (descendants driver/hierarchy :sql-jdbc)
         (mt/dataset sample-dataset
-          (let [queries (mapv (fn [i]
-                                (-> (mt/mbql-query orders
-                                      {:aggregation [[:count]]
-                                       :condition   [:and
-                                                     [:> $total 12]
-                                                     [:< $quantity i]]
-                                       :breakout    [!month.created_at]
-                                       :order-by    [[:desc $created_at]]})
-                                    qp/compile
-                                    :query))
-                              (range 1 16))]
+          (let [queries (mapv make-query-fn (range start bound))]
             (run-query (first queries))
             #_{:clj-kondo/ignore [:discouraged-var]}
-            (prn {:driver driver/*driver*, :auto-commit auto-commit})
+            (prn (assoc opts :driver driver/*driver* :auto-commit auto-commit))
             (flush)
             (qp.store/with-metadata-provider (:id (mt/db))
               (let [results (criterium/benchmark
@@ -66,3 +59,102 @@
                 #_{:clj-kondo/ignore [:discouraged-var]}
                 (prn (dissoc results :samples :runtime-details :results :os-details))))
             (flush)))))))
+
+(deftest auto-commit-performance-simple-test
+  (run-auto-commit-performance-test
+   (fn [i]
+     (-> (mt/mbql-query orders
+           {:aggregation [[:count]]
+            :condition   [:and
+                          [:> $total 12]
+                          [:< $quantity i]]
+            :breakout    [!month.created_at]
+            :order-by    [[:desc $created_at]]})
+         qp/compile
+         :query))
+   {:test :simple :start 1 :bound 16}))
+
+(deftest auto-commit-performance-join-test
+  (run-auto-commit-performance-test
+   (fn [i]
+     (-> (mt/mbql-query orders
+           {:joins       [{:source-table $$orders
+                           :fields    :all
+                           :alias     "o2"
+                           :strategy  :inner-join
+                           :condition [:< $id &o2.$id]}]
+            :condition   [:and
+                          [:< $id 21]
+                          [:> $total 12]
+                          [:<= $quantity i]
+                          [:< &o2.$quantity 3]
+                          [:< $total &o2.$total]]
+            :aggregation [[:count]]
+            :breakout    [$quantity]
+            :order-by    [[:desc $quantity]]})
+         qp/compile
+         :query))
+   {:test :join :start 13 :bound 16})) ; TODO
+
+(comment
+  (jdbc/with-db-connection [^Connection conn "jdbc:postgresql://vacskamati/metabase?user=metabase&password=metasample123"]
+    (prn conn))
+
+  (metabase.test.data.interface/db-test-env-var! :postgresql :host "vacskamati")
+  (metabase.test.data.interface/db-test-env-var! :postgresql :user "metabase")
+  (metabase.test.data.interface/db-test-env-var! :postgresql :password "metasample123")
+  (metabase.test.data.interface/db-test-env-var! :mysql :host "vacskamati")
+  (metabase.test.data.interface/db-test-env-var! :mysql :user "root")
+  (metabase.test.data.interface/db-test-env-var! :mysql :password "metasample123")
+
+  (metabase.test.data.env/set-test-drivers! #{:postgres :mysql})
+
+  (jdbc/with-db-connection [conn (metabase.driver.sql-jdbc.connection/connection-details->spec
+                                  :postgres
+                                  (metabase.test.data.interface/dbdef->connection-details :postgres nil {}))]
+    (jdbc/query conn ["SELECT * FROM pg_catalog.pg_tables"]))
+
+  (jdbc/with-db-connection [conn (metabase.driver.sql-jdbc.connection/connection-details->spec
+                                  :mysql
+                                  (metabase.test.data.interface/dbdef->connection-details :mysql nil {}))]
+    (jdbc/query conn ["SELECT * FROM pg_catalog.pg_tables"]))
+
+  (doseq [db (toucan2.core/select :metabase_database)]
+    (metabase.driver.sql-jdbc.connection/invalidate-pool-for-db! db))
+
+  (->> (toucan2.core/select :metabase_database :name "sample-dataset")
+       (filter (comp #{"postgres" "mysql"} :engine))
+       (map :id)
+       (run! #(toucan2.core/delete! :metabase_database :id %)))
+
+  (def results
+    (with-open [rdr (java.io.PushbackReader. (io/reader "/Users/metamben/github/metabase/read-only-tr-perf-test-mariadb-10.2-local.edn"))]
+      (loop [s (take-while #(not= % ::eof)
+                           (repeatedly #(edn/read {:readers {'criterium.core.OutlierCount #'identity}
+                                                   :eof ::eof}
+                                                  rdr)))
+             m {}]
+        (if (seq s)
+          (let [db (first s)
+                [now later] (split-with (complement symbol?) (rest s))
+                stats (into {} (map vec) (partition-all 2 now))]
+            (recur later (assoc m db stats)))
+          m))))
+
+  (defn- print-results [results]
+    (println "DB,auto commit,isolation level,samples,calls,mean (s),std-deviation (s²),tail quantile (%),lower quantile (s),upper quantile (s),outlier variance (%)")
+    (doseq [[db test-runs] results
+            [{:keys [auto-commit isolation-level]}
+             {:keys [execution-count sample-count mean
+                     variance outlier-variance
+                     lower-q upper-q tail-quantile]}] test-runs]
+      (printf "%s,%s,%s,%d,%d,%2.6f,%E,%f,%f,%f,%2.4f%n"
+              db,auto-commit,(if auto-commit "" (name isolation-level)),
+              sample-count,execution-count,(first mean),(Math/sqrt (first variance)),
+              (* tail-quantile 100),(first lower-q),(first upper-q),(* outlier-variance 100))))
+
+  (print-results results)
+
+  (criterium/bench (Thread/sleep 1000))
+
+  0)
