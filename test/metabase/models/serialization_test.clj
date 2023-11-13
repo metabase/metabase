29cd80c0bcb0b0282446fb53a6893ec0972d1bad
Bryan Maass <bryan.maass@gmail.com>
2023-11-13 12:31:42 -0700

    wip for loading-with-extra-lines-in-a-yaml-test

--- test/metabase/models/serialization_test.clj
+++ test/metabase/models/serialization_test.clj
@@ -1,8 +1,13 @@
 (ns metabase.models.serialization-test
   (:require
+   [babashka.fs :as fs]
+   [clojure.string :as str]
    [clojure.test :refer [deftest is testing]]
+   [clojure.tools.logging :as log]
+   [metabase-enterprise.serialization.cmd :as serialization.cmd]
    [metabase.models.serialization :as serdes]
-   [metabase.test :as mt]))
+   [metabase.test :as mt]
+   [metabase.util :as u]))
 
 (defmacro with-file-updated [[path f] & body]
   "For the dueration of the macro, the file at `path` is replaced with `(f (slurp path))`, and it will remain
@@ -28,7 +33,25 @@
              (swap! *history assoc :after (slurp filename)))
            @*history))))
 
-;; TODO: run serialization test with an extra
+
+;;
+;; TODOs:
+;; 1. Testing to check that a file with extra stuff in it gets ignored correctly
+;; 2. Since serialization.cmd is enterprise, this test needs to be in an enterprise ns.
+
+
+(deftest loading-with-extra-lines-in-a-yaml-test
+  (let [path "resources/instance_analytics/collections/vG58R8k-QddHWA7_47umn_metabase_analytics/vG58R8k-QddHWA7_47umn_metabase_analytics.yaml"
+        to-append "green_eggs_go_well_with: ham\neaster_bunny: is_real\n"]
+    (is (fs/exists? path))
+    (is (not (str/ends-with? (slurp path) to-append)))
+    (with-file-updated [path #(str % to-append)]
+      (is (str/ends-with? (slurp path) to-append))
+      (serialization.cmd/v2-load-internal "resources/instance_analytics/" {} :token-check? false)
+
+      )
+    (is (not (str/ends-with? (slurp path) to-append)))))
+
 
 (deftest fields-for-table-test
   (testing "fields for table returns table_name -> table_type for all columns"
