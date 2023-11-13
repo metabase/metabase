cd4f340fedfebfb4787ae3e4b05863226e99a1aa
Oisin Coveney <oisin@metabase.com>
2023-11-13 16:02:16 +0200

    Make sure this works

--- frontend/src/metabase/search/utils/use-user-recipients/use-user-recipients.ts
+++ frontend/src/metabase/search/utils/use-user-recipients/use-user-recipients.ts
@@ -1,42 +1,38 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import type { UserListResult } from "metabase-types/api";
 import { UserApi } from "metabase/services";
 
-export const useUserRecipients = (() => {
-  const userListPromise = useRef<Promise<UserListResult[]> | null>(null);
+let userListPromise: Promise<UserListResult[]> | null = null;
 
-  return _useUserRecipients;
+export const useUserRecipients = () => {
+  const [data, setData] = useState<UserListResult[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
 
-  function _useUserRecipients() {
-    const [data, setData] = useState<UserListResult[] | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<Error | null>(null);
-
-    useEffect(() => {
-      if (!userListPromise.current) {
-        userListPromise.current = UserApi.list()
-          .then(response => {
-            return response.data;
-          })
-          .catch(err => {
-            setError(err);
-            userListPromise.current = null;
-            throw err;
-          });
-      }
-
-      setLoading(true);
-      userListPromise.current
-        .then(apiData => {
-          setData(apiData);
-          setLoading(false);
+  useEffect(() => {
+    if (!userListPromise) {
+      userListPromise = UserApi.list()
+        .then(response => {
+          return response.data;
         })
         .catch(err => {
           setError(err);
-          setLoading(false);
+          userListPromise = null;
+          throw err;
         });
-    }, []);
+    }
 
-    return { data, loading, error };
-  }
-})();
+    setLoading(true);
+    userListPromise
+      .then(apiData => {
+        setData(apiData);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return { data, loading, error };
+};
