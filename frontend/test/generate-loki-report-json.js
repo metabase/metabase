4ac303f74da35a6ffac4c758ed7d52e7e3dad80a
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-11-12 23:29:04 -0300

    loki

---
+++ frontend/test/generate-loki-report-json.js
@@ -0,0 +1,31 @@
+const { readdir, writeFile } = require("fs");
+const { promisify } = require("util");
+const { join: joinPath, relative } = require("path");
+
+const asyncReaddir = promisify(readdir);
+const writeFileAsync = promisify(writeFile);
+
+const lokiDir = joinPath(__dirname, "..", "..", ".loki");
+const actualDir = joinPath(lokiDir, "current");
+const expectedDir = joinPath(lokiDir, "reference");
+const diffDir = joinPath(lokiDir, "difference");
+
+(async function main() {
+  const diffs = await asyncReaddir(diffDir);
+
+  await writeFileAsync(
+    joinPath(lokiDir, "report.json"),
+    JSON.stringify({
+      newItems: [],
+      deletedItems: [],
+      passedItems: [],
+      failedItems: diffs,
+      expectedItems: diffs,
+      actualItems: diffs,
+      diffItems: diffs,
+      actualDir: relative(lokiDir, actualDir),
+      expectedDir: relative(lokiDir, expectedDir),
+      diffDir: relative(lokiDir, diffDir),
+    }),
+  );
+})();
