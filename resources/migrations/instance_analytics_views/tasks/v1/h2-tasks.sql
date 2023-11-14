f4385e991071ac17f4a6fbd1dc5143d0a6ee2adf
Luiz Arakaki <luiz.arakaki@metabase.com>
2023-11-14 12:32:52 -0300

    fix double precision types for h2 and mysql

--- resources/migrations/instance_analytics_views/tasks/v1/h2-tasks.sql
+++ resources/migrations/instance_analytics_views/tasks/v1/h2-tasks.sql
@@ -7,6 +7,6 @@ select
     'database_' || db_id as database_qualified_id,
     started_at,
     ended_at,
-    cast(duration as double precision) / 1000 as duration_seconds,
+    cast(duration as double) / 1000 as duration_seconds,
     task_details as details
 from task_history;
