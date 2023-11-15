ce1866524c64e55066c5304c5b6b51b0bc25c0d7
Cam Saul <github@camsaul.com>
2023-11-15 23:11:46 +0000

    Fix Kondo script

--- bin/kondo.sh
+++ bin/kondo.sh
@@ -12,7 +12,7 @@ rm -rf .clj-kondo/metosin/malli-types-clj/
 rm -rf .clj-kondo/.cache
 
 # Run Kondo against all of our Clojure files in the various directories they might live.
-find modules/drivers shared enterprise/backend \
+find modules/drivers enterprise/backend \
      -maxdepth 2 \
      -type d \
      -name src -or -name test \
