775e7a3e55d734b6255cdf84251ec5a29b4553cc
Noah Moss <noahbmoss@gmail.com>
2023-11-16 16:29:28 -0500

    add more spans

--- src/metabase/events/recent_views.clj
+++ src/metabase/events/recent_views.clj
@@ -7,7 +7,8 @@
    [metabase.models.recent-views :as recent-views]
    [metabase.util :as u]
    [metabase.util.log :as log]
-   [methodical.core :as m]))
+   [methodical.core :as m]
+   [steffan-westcott.clj-otel.api.trace.span :as span]))
 
 (derive ::event :metabase/event)
 
@@ -17,14 +18,19 @@
 (m/defmethod events/publish-event! ::event
   "Handle processing for a single event notification which should update the recent views for a user."
   [topic {:keys [object user-id] :as _event}]
-  (try
-    (when object
-      (let [model    (audit-log/model-name object)
-            model-id (u/id object)
-            user-id  (or user-id api/*current-user-id*)]
-        (recent-views/update-users-recent-views! user-id model model-id)))
-    (catch Throwable e
-      (log/warnf e "Failed to process recent_views event: %s" topic))))
+  (span/with-span!
+    {:name (str "recent-views-" (name topic))
+     :topic topic
+     :object object
+     :user-id user-id}
+    (try
+      (when object
+        (let [model    (audit-log/model-name object)
+              model-id (u/id object)
+              user-id  (or user-id api/*current-user-id*)]
+          (recent-views/update-users-recent-views! user-id model model-id)))
+      (catch Throwable e
+        (log/warnf e "Failed to process recent_views event: %s" topic)))))
 
 (derive ::card-query-event :metabase/event)
 (derive :event/card-query ::card-query-event)
@@ -32,11 +38,16 @@
 (m/defmethod events/publish-event! ::card-query-event
   "Handle processing for a single card query event."
   [topic {:keys [card-id user-id context] :as _event}]
-  (try
-    (let [model    "card"
-          user-id  (or user-id api/*current-user-id*)]
-      ;; we don't want to count pinned card views
-      (when-not (#{:collection :dashboard} context)
-        (recent-views/update-users-recent-views! user-id model card-id)))
-    (catch Throwable e
-      (log/warnf e "Failed to process recent_views event: %s" topic))))
+  (span/with-span!
+    {:name (str "recent-views-" (name topic))
+     :topic topic
+     :card-id card-id
+     :user-id user-id}
+    (try
+      (let [model    "card"
+            user-id  (or user-id api/*current-user-id*)]
+        ;; we don't want to count pinned card views
+        (when-not (#{:collection :dashboard} context)
+          (recent-views/update-users-recent-views! user-id model card-id)))
+      (catch Throwable e
+        (log/warnf e "Failed to process recent_views event: %s" topic)))))
