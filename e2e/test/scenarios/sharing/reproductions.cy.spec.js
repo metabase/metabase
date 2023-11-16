90339ce03862c379ad1acd13f5151e040d88792d
Ngoc Khuat <qn.khuat@gmail.com>
2023-11-16 12:58:20 +0700

    remove pulse e2e tests

--- e2e/test/scenarios/sharing/reproductions.cy.spec.js
+++ e2e/test/scenarios/sharing/reproductions.cy.spec.js
@@ -259,49 +259,6 @@ function setUpAlert(questionId) {
   });
 }
 
-describe("issue 16918", () => {
-  const questionDetails = {
-    name: "16918",
-    query: {
-      "source-table": PRODUCTS_ID,
-      aggregation: [["count"]],
-      breakout: [
-        ["field", PRODUCTS.CREATED_AT, { "temporal-unit": "month-of-year" }],
-        ["field", PRODUCTS.CATEGORY, null],
-      ],
-    },
-    display: "line",
-  };
-
-  beforeEach(() => {
-    restore();
-    cy.signInAsAdmin();
-
-    cy.createQuestion(questionDetails).then(({ body }) => {
-      cy.intercept("GET", `/api/pulse/preview_card_info/${body.id}`).as(
-        "cardPreview",
-      );
-    });
-  });
-
-  it(`should load question binned by "Month of year" or similar granularity (metabase#16918)`, () => {
-    cy.visit("/pulse/create");
-
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("Select a question").click();
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("16918").click();
-
-    cy.wait("@cardPreview").then(xhr => {
-      expect(xhr.response.statusCode).not.to.eq(500);
-    });
-
-    // Cypress should be able to find question title in the card preview
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("16918");
-  });
-});
-
 describe("issue 16108", () => {
   beforeEach(() => {
     restore();
