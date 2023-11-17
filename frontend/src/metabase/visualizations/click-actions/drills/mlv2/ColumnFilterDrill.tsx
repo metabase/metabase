a8612e474dc5afe296183ab350982941f4178475
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-17 13:36:01 +0200

    Add mlv2 column filter drill

--- frontend/src/metabase/visualizations/click-actions/drills/mlv2/ColumnFilterDrill.tsx
+++ frontend/src/metabase/visualizations/click-actions/drills/mlv2/ColumnFilterDrill.tsx
@@ -3,6 +3,8 @@ import type { Drill } from "metabase/visualizations/types/click-actions";
 import * as Lib from "metabase-lib";
 import { FilterPickerBody } from "metabase/common/components/FilterPicker";
 
+const STAGE_INDEX = -1;
+
 export const ColumnFilterDrill: Drill<Lib.ColumnFilterDrillThruInfo> = ({
   question,
   drill,
@@ -13,10 +15,10 @@ export const ColumnFilterDrill: Drill<Lib.ColumnFilterDrillThruInfo> = ({
 
   const query = question._getMLv2Query();
   const drillColumn = Lib.drillThruColumn(drill);
-  const filterableColumns = Lib.filterableColumns(query, -1);
+  const filterableColumns = Lib.filterableColumns(query, STAGE_INDEX);
   const filterColumn = Lib.findMatchingColumn(
     query,
-    -1,
+    STAGE_INDEX,
     drillColumn,
     filterableColumns,
   );
@@ -35,10 +37,10 @@ export const ColumnFilterDrill: Drill<Lib.ColumnFilterDrillThruInfo> = ({
         return (
           <FilterPickerBody
             query={query}
-            stageIndex={-1}
+            stageIndex={STAGE_INDEX}
             column={filterColumn}
             onChange={filter => {
-              const nextQuery = Lib.filter(query, -1, filter);
+              const nextQuery = Lib.filter(query, STAGE_INDEX, filter);
               const nextQuestion = question._setMLv2Query(nextQuery);
               const nextCard = nextQuestion.card();
               onChangeCardAndRun({ nextCard });
