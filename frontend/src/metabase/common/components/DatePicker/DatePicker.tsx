5a1ebc542e2a81d14920c5ee6228357c2de8bd67
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-17 20:11:39 +0200

    Add tests

--- frontend/src/metabase/common/components/DatePicker/DatePicker.tsx
+++ frontend/src/metabase/common/components/DatePicker/DatePicker.tsx
@@ -1,8 +1,5 @@
+import type { ReactNode } from "react";
 import { useState } from "react";
-import type { FormEvent, ReactNode } from "react";
-import { t } from "ttag";
-import { Button, Stack } from "metabase/ui";
-import { DateOperatorPicker } from "./DateOperatorPicker";
 import { DateShortcutPicker } from "./DateShortcutPicker";
 import { ExcludeDatePicker } from "./ExcludeDatePicker";
 import { RelativeDatePicker } from "./RelativeDatePicker";
@@ -89,36 +86,4 @@ export function DatePicker({
         />
       );
   }
-}
-
-interface SimpleDatePickerProps {
-  value?: DatePickerValue;
-  availableOperators?: ReadonlyArray<DatePickerOperator>;
-  onChange: (value: DatePickerValue | undefined) => void;
-}
-
-export function SimpleDatePicker({
-  value: initialValue,
-  availableOperators = DATE_PICKER_OPERATORS,
-  onChange,
-}: SimpleDatePickerProps) {
-  const [value, setValue] = useState(initialValue);
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    onChange(value);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <Stack p="md">
-        <DateOperatorPicker
-          value={value}
-          availableOperators={availableOperators}
-          onChange={setValue}
-        />
-        <Button type="submit" variant="filled">{t`Apply`}</Button>
-      </Stack>
-    </form>
-  );
 }
