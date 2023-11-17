3147a75613e992028bc5a94f19746df917ca259a
Anton Kulyk <kuliks.anton@gmail.com>
2023-11-17 11:08:33 +0000

    Add a directory for `ColumnValuesWidget`

--- frontend/src/metabase/common/components/FilterPicker/ColumnValuesWidget/ColumnValuesWidget.tsx
+++ frontend/src/metabase/common/components/FilterPicker/ColumnValuesWidget/ColumnValuesWidget.tsx
@@ -1,6 +1,6 @@
-import type { ColumnValuesWidgetProps } from "../ColumnValuesWidget";
-import { ColumnValuesWidget as BaseColumnValuesWidget } from "../ColumnValuesWidget";
-import { MIN_WIDTH, MAX_WIDTH } from "./constants";
+import type { ColumnValuesWidgetProps } from "../../ColumnValuesWidget";
+import { ColumnValuesWidget as BaseColumnValuesWidget } from "../../ColumnValuesWidget";
+import { MIN_WIDTH, MAX_WIDTH } from "../constants";
 
 export function ColumnValuesWidget<T extends string | number>(
   props: Omit<ColumnValuesWidgetProps<T>, "minWidth" | "maxWidth">,
