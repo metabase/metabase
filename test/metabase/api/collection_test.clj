d8b5a397b59bf6c5485cac0d71e94d87e495aa2e
Jerry Huang <jhuang37050@gmail.com>
2023-11-17 17:30:55 -0600

    move test

--- test/metabase/api/collection_test.clj
+++ test/metabase/api/collection_test.clj
@@ -4,11 +4,7 @@
    [clojure.set :as set]
    [clojure.string :as str]
    [clojure.test :refer :all]
-   [clojure.walk :as walk]
-   [medley.core :as m]
-   [metabase-enterprise.audit-db-test :as audit-db-test]
    [metabase.api.collection :as api.collection]
-   [metabase.config :as config]
    [metabase.models
     :refer [Card Collection Dashboard DashboardCard ModerationReview
             NativeQuerySnippet PermissionsGroup PermissionsGroupMembership Pulse
@@ -68,26 +64,6 @@
 ;;; |                                                GET /collection                                                 |
 ;;; +----------------------------------------------------------------------------------------------------------------+
 
-(defn instance-analytics-collection-names
-  "Gather instance-analytic type collections and their children (who may or may-not have type=instance-analytics)."
-  []
-  (if-not config/ee-available?
-    #{}
-    (let [colls (mapv #(select-keys % [:id :name :location :type]) (t2/select Collection :archived false))
-          id->coll (m/index-by :id colls)
-          collection-tree (collection/collections->tree {} colls)]
-      (->> (loop [[tree & coll-tree] collection-tree
-                  ia-ids #{}]
-             (cond (not tree) ia-ids
-                   (= "instance-analytics" (:type tree)) (let [ids (transient #{})]
-                                                           (walk/postwalk
-                                                            (fn [x] (when (and (map? x) (:id x)) (conj! ids (:id x))) x)
-                                                            tree)
-                                                           (recur coll-tree (into ia-ids (persistent! ids))))
-                   ;; TODO: put my children onto the end of the coll-tree, then recur like normal
-                   :else (recur (concat coll-tree (:children tree)) ia-ids)))
-           (mapv (comp :name id->coll))))))
-
 (deftest list-collections-test
   (testing "GET /api/collection"
     (testing "check that we can get a basic list of collections"
@@ -127,44 +103,6 @@
                     (filter #((set (map mt/user->id [:crowberto :lucky :rasta :trashbird])) (:personal_owner_id %)))
                     (map :name)
                     sort)))))))
-
-(deftest list-collections-visible-collections-test
-  (testing "GET /api/collection"
-    (testing "You should only see your collection and public collections"
-      ;; Set audit-app feature so that we can assert that audit collections are also visible when running EE
-      (premium-features-test/with-premium-features #{:audit-app}
-        (audit-db-test/with-audit-db-restoration
-          (let [admin-user-id  (u/the-id (test.users/fetch-user :crowberto))
-                crowberto-root (t2/select-one Collection :personal_owner_id admin-user-id)]
-            (t2.with-temp/with-temp [Collection collection          {}
-                                     Collection {collection-id :id} {:name "Collection with Items"}
-                                     Collection _                   {:name            "subcollection"
-                                                                     :location        (format "/%d/" collection-id)
-                                                                     :authority_level "official"}
-                                     Collection _                   {:name     "Crowberto's Child Collection"
-                                                                     :location (collection/location-path crowberto-root)}]
-              (let [public-collection-names  #{"Our analytics"
-                                               (:name collection)
-                                               "Collection with Items"
-                                               "subcollection"}
-                    crowbertos               (set (map :name (mt/user-http-request :crowberto :get 200 "collection")))
-                    crowbertos-with-excludes (set (map :name (mt/user-http-request :crowberto :get 200 "collection" :exclude-other-user-collections true)))
-                    luckys                   (set (map :name (mt/user-http-request :lucky :get 200 "collection")))]
-                (is (= (into #{}
-                        (concat (instance-analytics-collection-names)
-                                (t2/select-fn-set :name Collection {:where [:and [:= :type nil] [:= :archived false]]})
-                                public-collection-names))
-                       crowbertos))
-                (is (= (into #{}
-                        (concat public-collection-names
-                                #{"Crowberto Corv's Personal Collection" "Crowberto's Child Collection"}
-                                (instance-analytics-collection-names)))
-                       crowbertos-with-excludes))
-                (is (true? (contains? crowbertos "Lucky Pigeon's Personal Collection")))
-                (is (false? (contains? crowbertos-with-excludes "Lucky Pigeon's Personal Collection")))
-                (is (= (conj public-collection-names (:name collection) "Lucky Pigeon's Personal Collection")
-                       luckys))
-                (is (false? (contains? luckys "Crowberto Corv's Personal Collection")))))))))))
 
 (deftest list-collections-personal-collection-locale-test
   (testing "GET /api/collection"
