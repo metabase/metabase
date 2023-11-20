21a9ded05667515454616b98fc4c61f8fcdefc3d
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-20 14:26:06 +0200

    Add tests

--- frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/utils.unit.spec.ts
+++ frontend/src/metabase/common/components/DatePicker/RelativeDatePicker/utils.unit.spec.ts
@@ -27,4 +27,96 @@ describe("setDirection", () => {
       });
     });
   });
+
+  describe("last", () => {
+    it("should convert a current value", () => {
+      const value: RelativeDatePickerValue = {
+        type: "relative",
+        value: "current",
+        unit: "week",
+      };
+      expect(setDirection(value, "last")).toEqual({
+        type: "relative",
+        value: -30,
+        unit: "week",
+      });
+    });
+
+    it("should convert an interval value", () => {
+      const value: RelativeDatePickerValue = {
+        type: "relative",
+        value: 20,
+        unit: "year",
+      };
+      expect(setDirection(value, "last")).toEqual({
+        type: "relative",
+        value: -20,
+        unit: "year",
+        offsetValue: undefined,
+      });
+    });
+
+    it("should convert an interval value with offset", () => {
+      const value: RelativeDatePickerValue = {
+        type: "relative",
+        value: 20,
+        unit: "day",
+        offsetValue: 10,
+        offsetUnit: "month",
+      };
+      expect(setDirection(value, "last")).toEqual({
+        type: "relative",
+        value: -20,
+        unit: "day",
+        offsetValue: -10,
+        offsetUnit: "month",
+      });
+    });
+  });
+
+  describe("next", () => {
+    it("should convert a current value", () => {
+      const value: RelativeDatePickerValue = {
+        type: "relative",
+        value: "current",
+        unit: "week",
+      };
+      expect(setDirection(value, "next")).toEqual({
+        type: "relative",
+        value: 30,
+        unit: "week",
+      });
+    });
+
+    it("should convert an interval value", () => {
+      const value: RelativeDatePickerValue = {
+        type: "relative",
+        value: -20,
+        unit: "year",
+      };
+      expect(setDirection(value, "next")).toEqual({
+        type: "relative",
+        value: 20,
+        unit: "year",
+        offsetValue: undefined,
+      });
+    });
+
+    it("should convert an interval value with offset", () => {
+      const value: RelativeDatePickerValue = {
+        type: "relative",
+        value: -20,
+        unit: "day",
+        offsetValue: -10,
+        offsetUnit: "month",
+      };
+      expect(setDirection(value, "next")).toEqual({
+        type: "relative",
+        value: 20,
+        unit: "day",
+        offsetValue: 10,
+        offsetUnit: "month",
+      });
+    });
+  });
 });
