244320cdb5b68db9b6679f1c61fdab17d74efc5a
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-21 19:34:27 +0200

    Minor issues

--- frontend/src/metabase/visualizations/click-actions/modes/DefaultMode.ts
+++ frontend/src/metabase/visualizations/click-actions/modes/DefaultMode.ts
@@ -1,4 +1,3 @@
-import { ColumnFilterDrill } from "metabase/visualizations/click-actions/drills/ColumnFilterDrill";
 import type { QueryClickActionsMode } from "../../types";
 import { ColumnFormattingAction } from "../actions/ColumnFormattingAction";
 import { HideColumnAction } from "../actions/HideColumnAction";
@@ -7,7 +6,6 @@ import { DashboardClickAction } from "../actions/DashboardClickAction";
 export const DefaultMode: QueryClickActionsMode = {
   name: "default",
   clickActions: [
-    ColumnFilterDrill,
     HideColumnAction,
     ColumnFormattingAction,
     DashboardClickAction,
