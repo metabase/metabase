5cf48597f84232b0a4a3ad2a7d495b7a5b2e15eb
Shaun Williams <shaun@metabase.com>
2023-11-21 15:24:27 -0600

    Revert "remove unused"
    This reverts commit 8daa24ed2e660ee154e33262f0ab3ac6bcb11cd8.

--- enterprise/frontend/src/metabase-enterprise/auth/index.js
+++ enterprise/frontend/src/metabase-enterprise/auth/index.js
@@ -207,6 +207,12 @@ PLUGIN_ADMIN_SETTINGS_UPDATES.push(sections => ({
         key: "jwt-group-sync",
         display_name: t`Synchronize group memberships`,
         description: null,
+        widget: GroupMappingsWidget,
+        props: {
+          mappingSetting: "jwt-group-mappings",
+          groupHeading: t`Group Name`,
+          groupPlaceholder: "Group Name",
+        },
       },
       {
         key: "jwt-group-mappings",
