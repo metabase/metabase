5e980c8fc087a1f801a07a4b5798d7ed849c295e
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-21 18:48:12 +0200

    Add automatic insights drill (#35991)

--- frontend/src/metabase-lib/urls.ts
+++ frontend/src/metabase-lib/urls.ts
@@ -1,12 +1,6 @@
 import * as Urls from "metabase/lib/urls";
 import { utf8_to_b64url } from "metabase/lib/encoding";
-import type {
-  Filter as RawFilter,
-  ParameterId,
-  ParameterValue,
-} from "metabase-types/api";
-import StructuredQuery from "metabase-lib/queries/StructuredQuery";
-import type Filter from "metabase-lib/queries/structured/Filter";
+import type { ParameterId, ParameterValue } from "metabase-types/api";
 import { isTransientId } from "metabase-lib/queries/utils/card";
 import { getParameterValuesBySlug } from "metabase-lib/parameters/utils/parameter-values";
 import { remapParameterValuesToTemplateTags } from "metabase-lib/parameters/utils/template-tags";
@@ -95,55 +89,35 @@ export function getUrlWithParameters(
   });
 }
 
-export function getAutomaticDashboardUrl(
-  question: Question,
-  filters: (RawFilter | Filter[])[],
-) {
-  let cellQuery = "";
-
-  if (filters.length > 0) {
-    const mbqlFilter = filters.length > 1 ? ["and", ...filters] : filters[0];
-    cellQuery = `/cell/${utf8_to_b64url(JSON.stringify(mbqlFilter))}`;
-  }
-
+export function getAutomaticDashboardUrl(question: Question) {
   const questionId = question.id();
+  const query = question.datasetQuery();
+  const filter = query.type === "query" ? query.query.filter : null;
+  const cellQuery = filter
+    ? `/cell/${utf8_to_b64url(JSON.stringify(filter))}`
+    : "";
 
   if (questionId != null && !isTransientId(questionId)) {
     return `auto/dashboard/question/${questionId}${cellQuery}`;
   } else {
-    const adHocQuery = utf8_to_b64url(
-      JSON.stringify(question.card().dataset_query),
-    );
+    const adHocQuery = utf8_to_b64url(JSON.stringify(query));
     return `auto/dashboard/adhoc/${adHocQuery}${cellQuery}`;
   }
 }
 
-export function getComparisonDashboardUrl(
-  question: Question,
-  filters: (RawFilter | Filter[])[],
-) {
-  let cellQuery = "";
-
-  if (filters.length > 0) {
-    const mbqlFilter = filters.length > 1 ? ["and", ...filters] : filters[0];
-    cellQuery = `/cell/${utf8_to_b64url(JSON.stringify(mbqlFilter))}`;
-  }
-
+export function getComparisonDashboardUrl(question: Question) {
   const questionId = question.id();
-  const query = question.query();
+  const tableId = question.tableId();
+  const query = question.datasetQuery();
+  const filter = query.type === "query" ? query.query.filter : null;
+  const cellQuery = filter
+    ? `/cell/${utf8_to_b64url(JSON.stringify(filter))}`
+    : "";
 
-  if (query instanceof StructuredQuery) {
-    const tableId = query.tableId();
-
-    if (tableId) {
-      if (questionId != null && !isTransientId(questionId)) {
-        return `auto/dashboard/question/${questionId}${cellQuery}/compare/table/${tableId}`;
-      } else {
-        const adHocQuery = utf8_to_b64url(
-          JSON.stringify(question.card().dataset_query),
-        );
-        return `auto/dashboard/adhoc/${adHocQuery}${cellQuery}/compare/table/${tableId}`;
-      }
-    }
+  if (questionId != null && !isTransientId(questionId)) {
+    return `auto/dashboard/question/${questionId}${cellQuery}/compare/table/${tableId}`;
+  } else {
+    const adHocQuery = utf8_to_b64url(JSON.stringify(query));
+    return `auto/dashboard/adhoc/${adHocQuery}${cellQuery}/compare/table/${tableId}`;
   }
 }
