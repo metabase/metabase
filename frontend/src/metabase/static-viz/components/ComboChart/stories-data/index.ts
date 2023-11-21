e7029abdea45002c058ee0f071e6aff3452fb7b9
Emmad Usmani <emmadusmani@berkeley.edu>
2023-11-21 08:30:16 -0800

    add storybook examples

---
+++ frontend/src/metabase/static-viz/components/ComboChart/stories-data/index.ts
@@ -0,0 +1,33 @@
+import autoYSplit from "./auto-y-split.json";
+import messedUpAxis from "./messed-up-axis.json";
+import trendSingleSeriesLine from "./trend-single-series-line.json";
+import trendMultiSeriesLine from "./trend-multi-series-line.json";
+import trendSingleSeriesArea from "./trend-single-series-area.json";
+import trendMultiSeriesArea from "./trend-multi-series-area.json";
+import trendMultiSeriesStackedArea from "./trend-multi-series-stacked-area.json";
+import trendMultiSeriesNormalizedStackedArea from "./trend-multi-series-normalized-stacked-area.json";
+import trendSingleSeriesBar from "./trend-single-series-bar.json";
+import trendMultiSeriesBar from "./trend-multi-series-bar.json";
+import trendMultiSeriesStackedBar from "./trend-multi-series-stacked-bar.json";
+import trendMultiSeriesNormalizedStackedBar from "./trend-multi-series-normalized-stacked-bar.json";
+import trendCombo from "./trend-combo.json";
+import trendComboPower from "./trend-combo-power.json";
+import trendComboLog from "./trend-combo-log.json";
+
+export const data = {
+  autoYSplit,
+  messedUpAxis,
+  trendSingleSeriesLine,
+  trendMultiSeriesLine,
+  trendSingleSeriesArea,
+  trendMultiSeriesArea,
+  trendMultiSeriesStackedArea,
+  trendMultiSeriesNormalizedStackedArea,
+  trendSingleSeriesBar,
+  trendMultiSeriesBar,
+  trendMultiSeriesStackedBar,
+  trendMultiSeriesNormalizedStackedBar,
+  trendCombo,
+  trendComboPower,
+  trendComboLog,
+};
