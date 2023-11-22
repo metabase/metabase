f03617fb64da9703ef39f2f1624cee13d1f94176
Adel <adel@kidoz.net>
2023-11-22 18:13:01 +0200

    KDC-8492-34577-aggregate-nested-id-test

---
+++ e2e/test/scenarios/question/reproductions/34577-mongo-aggregate-nested-id.cy.spec.js
@@ -0,0 +1,43 @@
+import { WRITABLE_DB_ID } from "e2e/support/cypress_data";
+import {
+  restore,
+  visualize,
+  withDatabase,
+  adhocQuestionHash,
+  summarize,
+} from "e2e/support/helpers";
+
+
+describe("issue 34577", { tags: "@mongo" }, () => {
+  beforeEach(() => {
+    restore("mongo-4");
+    cy.signInAsAdmin();
+
+    cy.request(`/api/database/${WRITABLE_DB_ID}/schema/`).then(({ body }) => {
+      const tableId = body.find(table => table.name === "nested_id_collection").id;
+      openTable({
+        database: WRITABLE_DB_ID,
+        table: tableId,
+        mode: "notebook",
+      });
+    });
+  });
+
+  it("should correctly apply distinct count on a nested _id (metabase#34577)", () => {
+
+    cy.findByRole("button", { name: "Summarize" }).click();
+
+    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
+    cy.findByText("Pick the metric you want to see").click();
+    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
+    cy.findByText("Count of rows").click();
+
+    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
+    cy.findByText("Pick a column to group by").click();
+    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
+    cy.findByText("Country").click();
+
+    visualize();
+    cy.get(".ScalarValue").contains("1");
+  });
+});
