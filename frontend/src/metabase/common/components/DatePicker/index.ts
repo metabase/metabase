656c16a1b81fd8b862a65f17ab9dc93f065851ea
Anton Kulyk <kuliks.anton@gmail.com>
2023-11-22 14:40:08 +0000

    Export `getShortcutOptionGroups` from `DatePicker`

--- frontend/src/metabase/common/components/DatePicker/index.ts
+++ frontend/src/metabase/common/components/DatePicker/index.ts
@@ -1,4 +1,8 @@
+import { getShortcutOptionGroups } from "./DateShortcutPicker";
+
 export * from "./DatePicker";
 export * from "./SimpleDatePicker";
 export * from "./types";
 export * from "./utils";
+
+export { getShortcutOptionGroups };
