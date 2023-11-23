9294948918846e60268f397fc8ecb44b9a7fb1d4
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-23 16:49:02 +0200

    Add tests

--- frontend/src/metabase-lib/drills.unit.spec.ts
+++ frontend/src/metabase-lib/drills.unit.spec.ts
@@ -12,18 +12,19 @@ describe("drill-thru/sort", () => {
   const drillType = "drill-thru/sort";
   const initialQuery = createQuery();
   const stageIndex = 0;
+  const column = createOrdersTotalDatasetColumn();
   const findColumn = columnFinder(
     initialQuery,
     Lib.orderableColumns(initialQuery, stageIndex),
   );
 
   describe("availableDrillThrus", () => {
-    it("should return directions for unsorted query", () => {
+    it("should allow to drill when query is not sorted", () => {
       const { drillInfo } = findDrillInfo(
         drillType,
         initialQuery,
         stageIndex,
-        createOrdersTotalDatasetColumn(),
+        column,
       );
       expect(drillInfo).toMatchObject({
         type: drillType,
@@ -31,68 +32,35 @@ describe("drill-thru/sort", () => {
       });
     });
 
-    it("should return directions when the query is sorted ascending", () => {
+    it("should allow to drill when query is sorted ascending", () => {
       const query = Lib.orderBy(
         initialQuery,
         stageIndex,
         findColumn("ORDERS", "TOTAL"),
         "asc",
       );
-      const { drillInfo } = findDrillInfo(
-        drillType,
-        query,
-        stageIndex,
-        createOrdersTotalDatasetColumn(),
-      );
+      const { drillInfo } = findDrillInfo(drillType, query, stageIndex, column);
       expect(drillInfo).toMatchObject({
         type: drillType,
         directions: ["desc"],
       });
     });
 
-    it("should return directions when the query is sorted descending", () => {
+    it("should allow to drill when query is sorted descending", () => {
       const query = Lib.orderBy(
         initialQuery,
         stageIndex,
         findColumn("ORDERS", "TOTAL"),
         "desc",
       );
-      const { drillInfo } = findDrillInfo(
-        drillType,
-        query,
-        stageIndex,
-        createOrdersTotalDatasetColumn(),
-      );
+      const { drillInfo } = findDrillInfo(drillType, query, stageIndex, column);
       expect(drillInfo).toMatchObject({
         type: drillType,
         directions: ["asc"],
       });
     });
-  });
-});
-
-describe("drill-thru/column-filter", () => {
-  const drillType = "drill-thru/column-filter";
-  const initialQuery = createQuery();
-  const stageIndex = 0;
-
-  describe("availableDrillThrus", () => {
-    it("should allow to drill when clicked on a column header", () => {
-      const column = createOrdersTotalDatasetColumn();
-      const { drillInfo } = findDrillInfo(
-        drillType,
-        initialQuery,
-        stageIndex,
-        column,
-      );
-
-      expect(drillInfo).toMatchObject({
-        type: drillType,
-      });
-    });
 
     it("should not allow to drill when clicked on a value", () => {
-      const column = createOrdersTotalDatasetColumn();
       const value = 10;
       const row = [{ col: column, value }];
       const drill = queryDrillThru(
@@ -108,7 +76,58 @@ describe("drill-thru/column-filter", () => {
     });
 
     it("should not allow to drill when clicked on a null value", () => {
-      const column = createOrdersTotalDatasetColumn();
+      const value = null;
+      const row = [{ col: column, value }];
+      const drill = queryDrillThru(
+        drillType,
+        initialQuery,
+        0,
+        column,
+        value,
+        row,
+      );
+
+      expect(drill).toBeNull();
+    });
+  });
+});
+
+describe("drill-thru/column-filter", () => {
+  const drillType = "drill-thru/column-filter";
+  const initialQuery = createQuery();
+  const stageIndex = 0;
+  const column = createOrdersTotalDatasetColumn();
+
+  describe("availableDrillThrus", () => {
+    it("should allow to drill when clicked on a column header", () => {
+      const { drillInfo } = findDrillInfo(
+        drillType,
+        initialQuery,
+        stageIndex,
+        column,
+      );
+
+      expect(drillInfo).toMatchObject({
+        type: drillType,
+      });
+    });
+
+    it("should not allow to drill when clicked on a value", () => {
+      const value = 10;
+      const row = [{ col: column, value }];
+      const drill = queryDrillThru(
+        drillType,
+        initialQuery,
+        stageIndex,
+        column,
+        value,
+        row,
+      );
+
+      expect(drill).toBeNull();
+    });
+
+    it("should not allow to drill when clicked on a null value", () => {
       const value = null;
       const row = [{ col: column, value }];
       const drill = queryDrillThru(
