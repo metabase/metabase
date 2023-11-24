23ff31178b618557fd316722f6f6c2bfd3248c72
Alexander Solovyov <alexander@solovyov.net>
2023-11-24 12:24:32 +0200

    snippets storage-path complexity was not for nothing

--- src/metabase/models/native_query_snippet.clj
+++ src/metabase/models/native_query_snippet.clj
@@ -99,9 +99,15 @@
     [[{:model "Collection" :id collection_id}]]
     []))
 
-(defmethod serdes/storage-path "NativeQuerySnippet" [snippet _ctx]
-  (let [{:keys [id label]} (-> snippet serdes/path last)]
-    ["snippets" (serdes/storage-leaf-file-name id label)]))
+(defmethod serdes/storage-path "NativeQuerySnippet" [snippet ctx]
+  ;; Intended path here is ["snippets" "<nested ... collections>" "<snippet_eid_and_slug>"]
+  ;; We just the default path, then pull it apart.
+  ;; The default is ["collections" "<nested ... collections>" "nativequerysnippets" "<base_name>"]
+  (let [basis  (serdes/storage-default-collection-path snippet ctx)
+        file   (last basis)
+        colls  (->> basis rest (drop-last 2))] ; Drops the "collections" at the start, and the last two.
+    (concat ["snippets"] colls [file])))
+
 
 (defmethod serdes/load-one! "NativeQuerySnippet" [ingested maybe-local]
   ;; if we got local snippet in db and it has same name as incoming one, we can be sure
