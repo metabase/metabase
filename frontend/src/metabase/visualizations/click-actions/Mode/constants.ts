7d401ba429b64815262c271f345cc0e6fd681644
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-28 20:24:51 +0200

    Fix drill type

--- frontend/src/metabase/visualizations/click-actions/Mode/constants.ts
+++ frontend/src/metabase/visualizations/click-actions/Mode/constants.ts
@@ -38,8 +38,8 @@ export const DRILL_TYPE_TO_HANDLER_MAP: Record<
   "drill-thru/summarize-column": null, // SummarizeColumnDrill,
   "drill-thru/summarize-column-by-time": SummarizeColumnByTimeDrill,
   "drill-thru/underlying-records": null, // UnderlyingRecordsDrill,
-  "drill-thru/zoom-in.timeseries": null,
-  "drill-thru/zoom-in.bins": null,
+  "drill-thru/zoom-in.binning": null,
   "drill-thru/zoom-in.geo": null,
+  "drill-thru/zoom-in.timeseries": null,
   "drill-thru/automatic-insights": null,
 };
