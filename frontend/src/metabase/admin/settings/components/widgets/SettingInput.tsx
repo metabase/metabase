7f602aec392975f1ba7e23044deed361852bd648
Nicolò Pretto <info@npretto.com>
2023-11-28 11:42:20 +0100

    refactor: removed unused aria label prop

--- frontend/src/metabase/admin/settings/components/widgets/SettingInput.tsx
+++ frontend/src/metabase/admin/settings/components/widgets/SettingInput.tsx
@@ -16,7 +16,6 @@ interface SettingInputProps {
     value: string | null;
     default?: string;
     placeholder?: string;
-    ariaLabel?: string;
   };
   onChange: (value: string | number | null) => void;
   autoFocus?: boolean;
@@ -55,7 +54,6 @@ const SettingInput = ({
       onChange={fireOnChange ? changeHandler : undefined}
       onBlurChange={!fireOnChange ? changeHandler : undefined}
       autoFocus={autoFocus}
-      aria-label={setting.ariaLabel}
     />
   );
 };
