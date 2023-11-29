7bf05e4aa1dbf75abea13f916872673591d10828
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-29 16:13:44 +0200

    Add tests

--- frontend/src/metabase-lib/tests/drills-summarize-column.unit.spec.ts
+++ frontend/src/metabase-lib/tests/drills-summarize-column.unit.spec.ts
@@ -4,16 +4,18 @@ import {
   createOrdersTable,
   createOrdersTotalDatasetColumn,
   createSampleDatabase,
-  SAMPLE_DB_ID,
 } from "metabase-types/api/mocks/presets";
 import { createMockMetadata } from "__support__/metadata";
 import * as Lib from "metabase-lib";
 import {
+  createColumnClickObject,
   createQuery,
+  createRawCellClickObject,
   findDrillThru,
   queryDrillThru,
 } from "metabase-lib/test-helpers";
 import {
+  createNotEditableQuery,
   createOrdersStructuredColumn,
   createOrdersStructuredField,
 } from "./drills-common";
@@ -26,12 +28,17 @@ describe("drill-thru/summarize-column", () => {
 
   describe("availableDrillThrus", () => {
     it("should allow to drill with a summable column", () => {
+      const clickObject = createColumnClickObject({
+        column: defaultColumn,
+      });
+
       const { drillInfo } = findDrillThru(
-        drillType,
         defaultQuery,
         stageIndex,
-        defaultColumn,
+        clickObject,
+        drillType,
       );
+
       expect(drillInfo).toMatchObject({
         type: drillType,
         aggregations: ["distinct", "sum", "avg"],
@@ -39,13 +46,17 @@ describe("drill-thru/summarize-column", () => {
     });
 
     it("should allow to drill with a non-summable column", () => {
-      const column = createOrdersCreatedAtDatasetColumn();
+      const clickObject = createColumnClickObject({
+        column: createOrdersCreatedAtDatasetColumn(),
+      });
+
       const { drillInfo } = findDrillThru(
-        drillType,
         defaultQuery,
         stageIndex,
-        column,
+        clickObject,
+        drillType,
       );
+
       expect(drillInfo).toMatchObject({
         type: drillType,
         aggregations: ["distinct"],
@@ -53,30 +64,32 @@ describe("drill-thru/summarize-column", () => {
     });
 
     it("should not allow to drill when clicked on a value", () => {
-      const value = 10;
-      const row = [{ col: defaultColumn, value }];
+      const clickObject = createRawCellClickObject({
+        column: defaultColumn,
+        value: 10,
+      });
+
       const drill = queryDrillThru(
-        drillType,
         defaultQuery,
         stageIndex,
-        defaultColumn,
-        value,
-        row,
+        clickObject,
+        drillType,
       );
 
       expect(drill).toBeNull();
     });
 
     it("should not allow to drill when clicked on a null value", () => {
-      const value = null;
-      const row = [{ col: defaultColumn, value }];
+      const clickObject = createRawCellClickObject({
+        column: defaultColumn,
+        value: null,
+      });
+
       const drill = queryDrillThru(
-        drillType,
         defaultQuery,
         stageIndex,
-        defaultColumn,
-        value,
-        row,
+        clickObject,
+        drillType,
       );
 
       expect(drill).toBeNull();
@@ -97,34 +110,20 @@ describe("drill-thru/summarize-column", () => {
 
       const query = createQuery({ metadata });
       const column = createOrdersStructuredColumn();
-      const drill = queryDrillThru(drillType, query, stageIndex, column);
+      const clickObject = createColumnClickObject({ column });
+      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
 
       expect(drill).toBeNull();
     });
 
-    it("should not allow to drill with a native query", () => {
-      const query = createQuery({
-        query: {
-          type: "native",
-          database: SAMPLE_DB_ID,
-          native: { query: "SELECT * FROM ORDERS" },
-        },
+    it("should not allow to drill with a non-editable query (metabase#36125)", () => {
+      const query = createNotEditableQuery(defaultQuery);
+      const clickObject = createColumnClickObject({
+        column: defaultColumn,
       });
-      const column = createOrdersTotalDatasetColumn({
-        id: undefined,
-        field_ref: ["field", "TOTAL", { "base-type": "type/Float" }],
-      });
-      const drill = queryDrillThru(drillType, query, stageIndex, column);
-      expect(drill).toBeNull();
-    });
 
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should not allow to drill with a non-editable query (metabase#36125)", () => {
-      const metadata = createMockMetadata({
-        databases: [createSampleDatabase({ tables: [] })],
-      });
-      const query = createQuery({ metadata });
-      const drill = queryDrillThru(drillType, query, stageIndex, defaultColumn);
+      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+
       expect(drill).toBeNull();
     });
   });
@@ -133,11 +132,14 @@ describe("drill-thru/summarize-column", () => {
     it.each<Lib.SummarizeColumnDrillThruOperator>(["distinct", "sum", "avg"])(
       'should drill with a summable column and "%s" operator',
       operator => {
+        const clickObject = createColumnClickObject({
+          column: defaultColumn,
+        });
         const { drill } = findDrillThru(
-          drillType,
           defaultQuery,
           stageIndex,
-          defaultColumn,
+          clickObject,
+          drillType,
         );
         const newQuery = Lib.drillThru(
           defaultQuery,
@@ -145,23 +147,29 @@ describe("drill-thru/summarize-column", () => {
           drill,
           operator,
         );
+
         expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
       },
     );
 
     it('should drill with a non-summable column and "distinct" operator', () => {
+      const clickedObject = createColumnClickObject({
+        column: createOrdersCreatedAtDatasetColumn(),
+      });
       const { drill } = findDrillThru(
-        drillType,
         defaultQuery,
         stageIndex,
-        createOrdersCreatedAtDatasetColumn(),
+        clickedObject,
+        drillType,
       );
+
       const newQuery = Lib.drillThru(
         defaultQuery,
         stageIndex,
         drill,
         "distinct",
       );
+
       expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
     });
   });
