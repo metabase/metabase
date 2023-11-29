49adbbbee814aa5ce893e5dfb98fe340fd31d899
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-29 23:13:49 +0200

    Add tests

--- frontend/src/metabase-lib/tests/drills-underlying-records.unit.spec.ts
+++ frontend/src/metabase-lib/tests/drills-underlying-records.unit.spec.ts
@@ -33,234 +33,127 @@ describe("drill-thru/underlying-records", () => {
     source: "breakout",
   });
 
-  describe("availableDrillThrus", () => {
-    it("should allow to drill an aggregated query", () => {
-      const clickObject = createAggregatedCellClickObject({
-        aggregationColumn,
-        aggregationColumnValue: 10,
-        breakoutColumn,
-        breakoutColumnValue: "2020-01-01",
-      });
-
-      const { drillInfo } = findDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drillInfo).toMatchObject({
-        type: drillType,
-        rowCount: 10,
-        tableName: "Orders",
-      });
+  it("should drill thru an aggregated cell", () => {
+    const clickObject = createAggregatedCellClickObject({
+      aggregationColumn,
+      aggregationColumnValue: 10,
+      breakoutColumn,
+      breakoutColumnValue: "2020-01-01",
     });
+    const { drill } = findDrillThru(
+      defaultQuery,
+      stageIndex,
+      clickObject,
+      drillType,
+    );
+    const newQuery = Lib.drillThru(defaultQuery, stageIndex, drill);
+    expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(0);
+    expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+  });
 
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should allow to drill when clicked on a pivot cell (metabase#35394)", () => {
-      const query = createAggregatedQueryWithBreakouts({
-        aggregationOperatorName: "count",
-        breakoutColumn1Name: "CREATED_AT",
-        breakoutColumn1TableName: "ORDERS",
-        breakoutColumn2Name: "QUANTITY",
-        breakoutColumn2TableName: "ORDERS",
-      });
-      const clickObject = createPivotCellClickObject({
-        aggregationColumn,
-        aggregationColumnValue: 10,
-        breakoutColumn1: breakoutColumn,
-        breakoutColumn1Value: "2020-01-01",
-        breakoutColumn2: createOrdersQuantityDatasetColumn({
-          source: "breakout",
-        }),
-        breakoutColumn2Value: 0,
-      });
-
-      const { drillInfo } = findDrillThru(
-        query,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drillInfo).toMatchObject({
-        type: drillType,
-        rowCount: 10,
-        tableName: "Orders",
-      });
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip("should drill thru a pivot cell (metabase#35394)", () => {
+    const query = createAggregatedQueryWithBreakouts({
+      aggregationOperatorName: "count",
+      breakoutColumn1Name: "CREATED_AT",
+      breakoutColumn1TableName: "ORDERS",
+      breakoutColumn2Name: "QUANTITY",
+      breakoutColumn2TableName: "ORDERS",
     });
-
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should allow to drill when clicked on a legend item (metabase#35343)", () => {
-      const query = createAggregatedQueryWithBreakouts({
-        aggregationOperatorName: "count",
-        breakoutColumn1Name: "CREATED_AT",
-        breakoutColumn1TableName: "ORDERS",
-        breakoutColumn2Name: "QUANTITY",
-        breakoutColumn2TableName: "ORDERS",
-      });
-      const clickObject = createLegendItemClickObject({
-        breakoutColumn,
-        breakoutColumnValue: 10,
-      });
-
-      const { drillInfo } = findDrillThru(
-        query,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drillInfo).toMatchObject({
-        type: drillType,
-        rowCount: 2,
-        tableName: "Orders",
-      });
+    const clickObject = createPivotCellClickObject({
+      aggregationColumn,
+      aggregationColumnValue: 10,
+      breakoutColumn1: breakoutColumn,
+      breakoutColumn1Value: "2020-01-01",
+      breakoutColumn2: createOrdersQuantityDatasetColumn({
+        source: "breakout",
+      }),
+      breakoutColumn2Value: 0,
     });
+    const { drill } = findDrillThru(query, stageIndex, clickObject, drillType);
+    const newQuery = Lib.drillThru(query, stageIndex, drill);
+    expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(0);
+    expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+  });
 
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should use the default row count for aggregations with negative values (metabase#36143)", () => {
-      const clickObject = createAggregatedCellClickObject({
-        aggregationColumn,
-        aggregationColumnValue: -10,
-        breakoutColumn,
-        breakoutColumnValue: "2020-01-01",
-      });
-
-      const { drillInfo } = findDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drillInfo).toMatchObject({
-        type: drillType,
-        rowCount: 2,
-        tableName: "Orders",
-      });
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip("should drill thru a legend item (metabase#35343)", () => {
+    const query = createAggregatedQueryWithBreakouts({
+      aggregationOperatorName: "count",
+      breakoutColumn1Name: "CREATED_AT",
+      breakoutColumn1TableName: "ORDERS",
+      breakoutColumn2Name: "QUANTITY",
+      breakoutColumn2TableName: "ORDERS",
     });
-
-    it("should allow to drill when clicked on a null value", () => {
-      const clickObject = createAggregatedCellClickObject({
-        aggregationColumn,
-        aggregationColumnValue: null,
-        breakoutColumn,
-        breakoutColumnValue: "2020-01-01",
-      });
-
-      const { drillInfo } = findDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drillInfo).toMatchObject({
-        type: drillType,
-        rowCount: 2,
-        tableName: "Orders",
-      });
+    const clickObject = createLegendItemClickObject({
+      breakoutColumn,
+      breakoutColumnValue: 10,
     });
+    const { drill } = findDrillThru(query, stageIndex, clickObject, drillType);
+    const newQuery = Lib.drillThru(defaultQuery, stageIndex, drill);
+    expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(0);
+    expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+  });
 
-    it("should not allow to drill when there is no aggregation", () => {
-      const query = createQuery();
-      const column = createOrdersTotalDatasetColumn();
-      const clickObject = createRawCellClickObject({ column, value: 10 });
-
-      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
-      expect(drill).toBeNull();
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip("should use the default row count for aggregations with negative values (metabase#36143)", () => {
+    const clickObject = createAggregatedCellClickObject({
+      aggregationColumn,
+      aggregationColumnValue: -10,
+      breakoutColumn,
+      breakoutColumnValue: "2020-01-01",
     });
-
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should not allow to drill with a non-editable query (metabase#36125)", () => {
-      const query = createNotEditableQuery(defaultQuery);
-      const clickObject = createAggregatedCellClickObject({
-        aggregationColumn,
-        aggregationColumnValue: 10,
-        breakoutColumn,
-        breakoutColumnValue: "2020-01-01",
-      });
-
-      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
-      expect(drill).toBeNull();
+    const { drillInfo } = findDrillThru(
+      defaultQuery,
+      stageIndex,
+      clickObject,
+      drillType,
+    );
+    expect(drillInfo).toMatchObject({
+      type: drillType,
+      rowCount: 2,
+      tableName: "Orders",
     });
   });
 
-  describe("drillThru", () => {
-    it("should drill when clicked on an aggregated cell", () => {
-      const clickObject = createAggregatedCellClickObject({
-        aggregationColumn,
-        aggregationColumnValue: 10,
-        breakoutColumn,
-        breakoutColumnValue: "2020-01-01",
-      });
-      const { drill } = findDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      const newQuery = Lib.drillThru(defaultQuery, stageIndex, drill);
-      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(0);
-      expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+  it("should drill thru an aggregated cell with null value", () => {
+    const clickObject = createAggregatedCellClickObject({
+      aggregationColumn,
+      aggregationColumnValue: null,
+      breakoutColumn,
+      breakoutColumnValue: "2020-01-01",
     });
-
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should drill when clicked on a pivot cell (metabase#35394)", () => {
-      const query = createAggregatedQueryWithBreakouts({
-        aggregationOperatorName: "count",
-        breakoutColumn1Name: "CREATED_AT",
-        breakoutColumn1TableName: "ORDERS",
-        breakoutColumn2Name: "QUANTITY",
-        breakoutColumn2TableName: "ORDERS",
-      });
-      const clickObject = createPivotCellClickObject({
-        aggregationColumn,
-        aggregationColumnValue: 10,
-        breakoutColumn1: breakoutColumn,
-        breakoutColumn1Value: "2020-01-01",
-        breakoutColumn2: createOrdersQuantityDatasetColumn({
-          source: "breakout",
-        }),
-        breakoutColumn2Value: 0,
-      });
-      const { drill } = findDrillThru(
-        query,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      const newQuery = Lib.drillThru(query, stageIndex, drill);
-      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(0);
-      expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+    const { drillInfo } = findDrillThru(
+      defaultQuery,
+      stageIndex,
+      clickObject,
+      drillType,
+    );
+    expect(drillInfo).toMatchObject({
+      type: drillType,
+      rowCount: 2,
+      tableName: "Orders",
     });
+  });
 
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should drill when clicked on a legend item (metabase#35343)", () => {
-      const query = createAggregatedQueryWithBreakouts({
-        aggregationOperatorName: "count",
-        breakoutColumn1Name: "CREATED_AT",
-        breakoutColumn1TableName: "ORDERS",
-        breakoutColumn2Name: "QUANTITY",
-        breakoutColumn2TableName: "ORDERS",
-      });
-      const clickObject = createLegendItemClickObject({
-        breakoutColumn,
-        breakoutColumnValue: 10,
-      });
-      const { drill } = findDrillThru(
-        query,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
+  it("should not drill thru a raw query", () => {
+    const query = createQuery();
+    const column = createOrdersTotalDatasetColumn();
+    const clickObject = createRawCellClickObject({ column, value: 10 });
+    const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+    expect(drill).toBeNull();
+  });
 
-      const newQuery = Lib.drillThru(defaultQuery, stageIndex, drill);
-      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(0);
-      expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip("should not drill thru a non-editable query (metabase#36125)", () => {
+    const query = createNotEditableQuery(defaultQuery);
+    const clickObject = createAggregatedCellClickObject({
+      aggregationColumn,
+      aggregationColumnValue: 10,
+      breakoutColumn,
+      breakoutColumnValue: "2020-01-01",
     });
+    const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+    expect(drill).toBeNull();
   });
 });
