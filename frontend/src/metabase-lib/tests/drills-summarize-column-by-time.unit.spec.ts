256066623fce75ea57ee82bfdd46eba07cd53263
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-11-29 23:08:54 +0200

    Add tests

--- frontend/src/metabase-lib/tests/drills-summarize-column-by-time.unit.spec.ts
+++ frontend/src/metabase-lib/tests/drills-summarize-column-by-time.unit.spec.ts
@@ -27,138 +27,105 @@ describe("drill-thru/summarize-column-by-time", () => {
   const stageIndex = 0;
   const defaultColumn = createOrdersTotalDatasetColumn();
 
-  describe("availableDrillThrus", () => {
-    it("should allow to drill with a summable column", () => {
-      const clickObject = createColumnClickObject({
-        column: defaultColumn,
-      });
-
-      const { drillInfo } = findDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drillInfo).toMatchObject({
-        type: drillType,
-      });
+  it("should drill thru a summable column", () => {
+    const clickObject = createColumnClickObject({
+      column: defaultColumn,
     });
-
-    it("should not allow to drill with a non-summable column", () => {
-      const clickObject = createColumnClickObject({
-        column: createOrdersCreatedAtDatasetColumn(),
-      });
-
-      const drill = queryDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drill).toBeNull();
-    });
-
-    it("should not allow to drill when there is no date column", () => {
-      const metadata = createMockMetadata({
-        databases: [
-          createSampleDatabase({
-            tables: [
-              createOrdersTable({
-                fields: [createOrdersIdField(), createOrdersTotalField()],
-              }),
-            ],
-          }),
-        ],
-      });
-      const query = createQuery({ metadata });
-      const clickObject = createColumnClickObject({ column: defaultColumn });
-
-      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
-
-      expect(drill).toBeNull();
-    });
-
-    it("should not allow to drill when clicked on a value", () => {
-      const clickObject = createRawCellClickObject({
-        column: defaultColumn,
-        value: 10,
-      });
-
-      const drill = queryDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drill).toBeNull();
-    });
-
-    it("should not allow to drill when clicked on a null value", () => {
-      const clickObject = createRawCellClickObject({
-        column: defaultColumn,
-        value: null,
-      });
-
-      const drill = queryDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      expect(drill).toBeNull();
-    });
-
-    it('should not allow to drill with "type/Structured" type', () => {
-      const metadata = createMockMetadata({
-        databases: [
-          createSampleDatabase({
-            tables: [
-              createOrdersTable({
-                fields: [createOrdersIdField(), createOrdersStructuredField()],
-              }),
-            ],
-          }),
-        ],
-      });
-
-      const query = createQuery({ metadata });
-      const column = createOrdersStructuredDatasetColumn();
-      const clickObject = createColumnClickObject({ column });
-
-      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
-      expect(drill).toBeNull();
-    });
-
-    it("should not allow to drill with a non-editable query", () => {
-      const query = createNotEditableQuery(defaultQuery);
-      const clickObject = createColumnClickObject({
-        column: defaultColumn,
-      });
-
-      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
-      expect(drill).toBeNull();
-    });
+    const { drill } = findDrillThru(
+      defaultQuery,
+      stageIndex,
+      clickObject,
+      drillType,
+    );
+    const newQuery = Lib.drillThru(defaultQuery, stageIndex, drill);
+    expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
+    expect(Lib.breakouts(newQuery, stageIndex)).toHaveLength(1);
   });
 
-  describe("drillThru", () => {
-    it("should drill with a summable column", () => {
-      const clickObject = createColumnClickObject({
-        column: defaultColumn,
-      });
-      const { drill } = findDrillThru(
-        defaultQuery,
-        stageIndex,
-        clickObject,
-        drillType,
-      );
-
-      const newQuery = Lib.drillThru(defaultQuery, stageIndex, drill);
-      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
-      expect(Lib.breakouts(newQuery, stageIndex)).toHaveLength(1);
+  it("should not drill thru a non-summable column", () => {
+    const clickObject = createColumnClickObject({
+      column: createOrdersCreatedAtDatasetColumn(),
     });
+    const drill = queryDrillThru(
+      defaultQuery,
+      stageIndex,
+      clickObject,
+      drillType,
+    );
+    expect(drill).toBeNull();
+  });
+
+  it("should not drill thru a column when there is no date column", () => {
+    const metadata = createMockMetadata({
+      databases: [
+        createSampleDatabase({
+          tables: [
+            createOrdersTable({
+              fields: [createOrdersIdField(), createOrdersTotalField()],
+            }),
+          ],
+        }),
+      ],
+    });
+    const query = createQuery({ metadata });
+    const clickObject = createColumnClickObject({ column: defaultColumn });
+    const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+    expect(drill).toBeNull();
+  });
+
+  it("should not drill thru a cell", () => {
+    const clickObject = createRawCellClickObject({
+      column: defaultColumn,
+      value: 10,
+    });
+    const drill = queryDrillThru(
+      defaultQuery,
+      stageIndex,
+      clickObject,
+      drillType,
+    );
+    expect(drill).toBeNull();
+  });
+
+  it("should not drill thru a cell with null value", () => {
+    const clickObject = createRawCellClickObject({
+      column: defaultColumn,
+      value: null,
+    });
+    const drill = queryDrillThru(
+      defaultQuery,
+      stageIndex,
+      clickObject,
+      drillType,
+    );
+    expect(drill).toBeNull();
+  });
+
+  it("should not drill thru a JSON column", () => {
+    const metadata = createMockMetadata({
+      databases: [
+        createSampleDatabase({
+          tables: [
+            createOrdersTable({
+              fields: [createOrdersIdField(), createOrdersStructuredField()],
+            }),
+          ],
+        }),
+      ],
+    });
+    const query = createQuery({ metadata });
+    const column = createOrdersStructuredDatasetColumn();
+    const clickObject = createColumnClickObject({ column });
+    const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+    expect(drill).toBeNull();
+  });
+
+  it("should not drill thru a non-editable query", () => {
+    const query = createNotEditableQuery(defaultQuery);
+    const clickObject = createColumnClickObject({
+      column: defaultColumn,
+    });
+    const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+    expect(drill).toBeNull();
   });
 });
