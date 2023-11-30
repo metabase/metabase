df41dbd0063b0e1e1f36a1766c81a96558513a72
Ngoc Khuat <qn.khuat@gmail.com>
2023-11-30 21:23:54 +0700

    Alert/Dashboard subscription: render question title as <a> tag (#36220)

--- test/metabase/pulse/render_test.clj
+++ test/metabase/pulse/render_test.clj
@@ -151,9 +151,19 @@
       (binding [render/*include-description* true]
         (is (= "<p>Card description</p>\n" (last (:content (#'render/make-description-if-needed dc1 card))))))))
 
-    (testing "Test markdown converts to html"
-      (mt/with-temp* [Card          [card {:description "# Card description"}]
-                      Dashboard     [dashboard]
-                      DashboardCard [dc1 {:dashboard_id (:id dashboard) :card_id (:id card)}]]
-        (binding [render/*include-description* true]
-          (is (= "<h1>Card description</h1>\n" (last (:content (#'render/make-description-if-needed dc1 card)))))))))
+  (testing "Test markdown converts to html"
+    (mt/with-temp* [Card          [card {:description "# Card description"}]
+                    Dashboard     [dashboard]
+                    DashboardCard [dc1 {:dashboard_id (:id dashboard) :card_id (:id card)}]]
+      (binding [render/*include-description* true]
+        (is (= "<h1>Card description</h1>\n" (last (:content (#'render/make-description-if-needed dc1 card)))))))))
+
+(deftest title-should-be-an-a-tag-test
+  (testing "the title of the card should be an <a> tag so you can click on title using old outlook clients (#12901)"
+    (mt/with-temp* [Card [card {:name          "A Card"
+                                :dataset_query (mt/mbql-query venues {:limit 1})}]]
+      (mt/with-temp-env-var-value [mb-site-url "https://mb.com"]
+        (let [rendered-card-content (:content (binding [render/*include-title* true]
+                                                (render/render-pulse-card :inline (pulse/defaulted-timezone card) card nil (qp/process-query (:dataset_query card)))))]
+          (is (some? (mbql.u/match-one rendered-card-content
+                                       [:a (_ :guard #(= (format "https://mb.com/question/%d" (:id card)) (:href %))) "A Card"]))))))))
