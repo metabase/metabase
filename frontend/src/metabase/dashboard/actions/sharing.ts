4fcbc1d8c5c01c4baae83771480983b0e06ca493
Oisin Coveney <oisin@metabase.com>
2023-12-01 13:16:46 +0200

    Convert sharing.js to TS

--- frontend/src/metabase/dashboard/actions/sharing.ts
+++ frontend/src/metabase/dashboard/actions/sharing.ts
@@ -1,3 +1,5 @@
+import type { Dashboard } from "metabase-types/api";
+import type { Dispatch, EmbedOptions } from "metabase-types/store";
 import { createAction } from "metabase/lib/redux";
 
 import { SIDEBAR_NAME } from "metabase/dashboard/constants";
@@ -6,7 +8,11 @@ import { DashboardApi } from "metabase/services";
 
 import { setSidebar, closeSidebar } from "./ui";
 
-export const setSharing = isSharing => dispatch => {
+type DashboardIdPayload = {
+  id: Dashboard["id"];
+};
+
+export const setSharing = (isSharing: boolean) => (dispatch: Dispatch) => {
   if (isSharing) {
     dispatch(
       setSidebar({
@@ -22,29 +28,29 @@ export const UPDATE_ENABLE_EMBEDDING =
   "metabase/dashboard/UPDATE_ENABLE_EMBEDDING";
 export const updateEnableEmbedding = createAction(
   UPDATE_ENABLE_EMBEDDING,
-  ({ id }, enable_embedding) => DashboardApi.update({ id, enable_embedding }),
+  ({ id }: DashboardIdPayload, enable_embedding: boolean) =>
+    DashboardApi.update({ id, enable_embedding }),
 );
 
 export const UPDATE_EMBEDDING_PARAMS =
   "metabase/dashboard/UPDATE_EMBEDDING_PARAMS";
 export const updateEmbeddingParams = createAction(
   UPDATE_EMBEDDING_PARAMS,
-  ({ id }, embedding_params) => DashboardApi.update({ id, embedding_params }),
+  ({ id }: DashboardIdPayload, embedding_params: EmbedOptions) =>
+    DashboardApi.update({ id, embedding_params }),
 );
 
 export const CREATE_PUBLIC_LINK = "metabase/dashboard/CREATE_PUBLIC_LINK";
 
 export const createPublicLink = createAction(
   CREATE_PUBLIC_LINK,
-  /**
-   * @param {import("metabase-types/api").Dashboard} payload - The dashboard to create a public link for
-   *
-   * @returns {Promise<{
-   *     id: import("metabase-types/api").DashboardId,
-   *     uuid: string,
-   * }>} Resolves to the dashboard id and its corresponding public uuid
-   */
-  async ({ id }) => {
+  async ({
+    id,
+  }: DashboardIdPayload): Promise<
+    DashboardIdPayload & {
+      uuid: Dashboard["public_uuid"];
+    }
+  > => {
     const { uuid } = await DashboardApi.createPublicLink({ id });
     return { id, uuid };
   },
@@ -53,12 +59,7 @@ export const createPublicLink = createAction(
 export const DELETE_PUBLIC_LINK = "metabase/dashboard/DELETE_PUBLIC_LINK";
 export const deletePublicLink = createAction(
   DELETE_PUBLIC_LINK,
-  /**
-   * @param {import("metabase-types/api").Dashboard} payload - The dashboard to create a public link for
-   *
-   * @returns {Promise<{id: import("metabase-types/api").DashCardId}>} Resolves to the dashboard id
-   */
-  async ({ id }) => {
+  async ({ id }: DashboardIdPayload): Promise<DashboardIdPayload> => {
     await DashboardApi.deletePublicLink({ id });
     return { id };
   },
