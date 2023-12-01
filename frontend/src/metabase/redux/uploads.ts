11a710d99f56c963cfcbcf2dd9122c5f9d276004
Nick Fitzpatrick <nick@metabase.com>
2023-12-01 14:03:42 -0400

    fetching specific collections for file upload toasts (#36241)

--- frontend/src/metabase/redux/uploads.ts
+++ frontend/src/metabase/redux/uploads.ts
@@ -102,7 +102,7 @@ export const uploadFile = createThunkAction(
 interface UploadStartPayload {
   id: number;
   name: string;
-  collectionId: string;
+  collectionId: CollectionId;
 }
 
 interface UploadEndPayload {
