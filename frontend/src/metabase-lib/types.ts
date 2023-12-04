34b87a2a8fa08668da40e78a438d9326eb2914a0
Kamil Mielnik <kamil@kamilmielnik.com>
2023-12-04 18:41:02 +0700

    Remove isNamed attribute from ColumnDisplayInfo

--- frontend/src/metabase-lib/types.ts
+++ frontend/src/metabase-lib/types.ts
@@ -101,7 +101,6 @@ export type ColumnDisplayInfo = {
   name: string;
   displayName: string;
   longDisplayName: string;
-  isNamed: boolean;
 
   fkReferenceName?: string;
   isCalculated: boolean;
@@ -134,8 +133,10 @@ export type MetricDisplayInfo = {
 
 export type ClauseDisplayInfo = Pick<
   ColumnDisplayInfo,
-  "name" | "displayName" | "longDisplayName" | "isNamed" | "table"
->;
+  "name" | "displayName" | "longDisplayName" | "table"
+> & {
+  isNamed: boolean;
+};
 
 export type AggregationClauseDisplayInfo = ClauseDisplayInfo;
 
