7d02a939a852660852424886d2977cb4a2891a54
Alexander Solovyov <alexander@solovyov.net>
2023-12-05 13:25:10 +0200

    dashboard_tab.entity_id is nullable now, makes drop-entity-ids work

--- enterprise/backend/test/metabase_enterprise/serialization/v2/entity_ids_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/v2/entity_ids_test.clj
@@ -2,12 +2,15 @@
   (:require
    [clojure.string :as str]
    [clojure.test :refer :all]
+   [metabase-enterprise.serialization.v2.backfill-ids :as serdes.backfill]
    [metabase-enterprise.serialization.v2.entity-ids :as v2.entity-ids]
    [metabase.models :refer [Collection Dashboard]]
    [metabase.test :as mt]
+   [metabase.util :as u]
    [toucan2.core :as t2])
   (:import
-   (java.time LocalDateTime)))
+   (java.time LocalDateTime)
+   (java.sql DatabaseMetaData)))
 
 (set! *warn-on-reflection* true)
 
@@ -45,24 +48,36 @@
                      (entity-id))))))))))
 
 (deftest drop-entity-ids-test
-  (testing "With a temp Collection with an entity ID"
-    (let [now (LocalDateTime/of 2022 9 1 12 34 56)]
-      (mt/with-temp! [Collection c {:name       "No Entity ID Collection"
-                                    :slug       "no_entity_id_collection"
-                                    :created_at now}]
-        (letfn [(entity-id []
-                  (some-> (t2/select-one-fn :entity_id Collection :id (:id c)) str/trim))]
-          (is (some? (entity-id)))
-          (testing "Should return truthy on success"
-            (is (= true
-                   (v2.entity-ids/drop-entity-ids!))))
-          (is (nil? (entity-id)))))))
-  (testing "empty table"
-    (testing "has no entity ids"
-      (mt/with-temp! [Collection _ {:name       "No Entity ID Collection"
-                                    :slug       "no_entity_id_collection"}]
-        (is (nil? (t2/select-fn-set :entity-id Dashboard)))
-        (testing "but doesn't crash drop-entity-ids"
-          (is (= true
-                 (v2.entity-ids/drop-entity-ids!)))
-          (is (nil? (t2/select-fn-set :entity-id Dashboard))))))))
+  (mt/with-empty-h2-app-db
+    (testing "With a temp Collection with an entity ID"
+      (let [now (LocalDateTime/of 2022 9 1 12 34 56)]
+        (mt/with-temp! [Collection c {:name       "No Entity ID Collection"
+                                      :slug       "no_entity_id_collection"
+                                      :created_at now}]
+                       (letfn [(entity-id []
+                                 (some-> (t2/select-one-fn :entity_id Collection :id (:id c)) str/trim))]
+                         (is (some? (entity-id)))
+                         (testing "Should return truthy on success"
+                           (is (= true
+                                  (v2.entity-ids/drop-entity-ids!))))
+                         (is (nil? (entity-id)))))))
+    (testing "empty table"
+      (testing "has no entity ids"
+        (mt/with-temp! [Collection _ {:name "No Entity ID Collection"
+                                      :slug "no_entity_id_collection"}]
+                       (is (nil? (t2/select-fn-set :entity-id Dashboard)))
+                       (testing "but doesn't crash drop-entity-ids"
+                         (is (= true
+                                (v2.entity-ids/drop-entity-ids!)))
+                         (is (nil? (t2/select-fn-set :entity-id Dashboard)))))))
+    (testing "entity_id field is nullable everywhere so that drop-entity-ids will work for"
+      (t2/with-connection [^java.sql.Connection conn]
+        (doseq [m     (v2.entity-ids/toucan-models)
+                :when (serdes.backfill/has-entity-id? m)
+                :let  [rs (-> (.getMetaData conn)
+                              (.getColumns nil nil (u/upper-case-en (name (t2/table-name m))) "ENTITY_ID"))]]
+          (testing m
+            (if (.next rs)
+              (is (= DatabaseMetaData/columnNullable
+                     ( .getInt rs "NULLABLE")))
+              (is false "cannot get column information"))))))))
