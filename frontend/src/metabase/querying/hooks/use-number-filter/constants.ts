2221fa1b278a843e5fe779ea2c176aed07bb8d3e
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-12-05 18:14:33 +0200

    Extract to a hook

--- frontend/src/metabase/querying/hooks/use-number-filter/constants.ts
+++ frontend/src/metabase/querying/hooks/use-number-filter/constants.ts
@@ -1,6 +1,10 @@
+import type * as Lib from "metabase-lib";
 import type { OperatorOption } from "./types";
 
-export const OPERATOR_OPTIONS: Record<string, OperatorOption> = {
+export const OPERATOR_OPTIONS: Record<
+  Lib.NumberFilterOperatorName,
+  OperatorOption
+> = {
   "=": {
     operator: "=",
     valueCount: 1,
