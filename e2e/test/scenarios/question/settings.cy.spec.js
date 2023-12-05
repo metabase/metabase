13fc7d30fb9ef2f409548bbfaef1c4383ebbbfa8
Nick Fitzpatrick <nickfitz.582@gmail.com>
2023-12-05 13:49:29 -0400

    updating to react 18

--- e2e/test/scenarios/question/settings.cy.spec.js
+++ e2e/test/scenarios/question/settings.cy.spec.js
@@ -249,14 +249,14 @@ describe("scenarios > question > settings", () => {
       cy.findByTestId("viz-settings-button").click(); // open settings sidebar
       // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
       cy.findByText("Conditional Formatting"); // confirm it's open
-      cy.get(".TableInteractive").findByText("Subtotal").click(); // open subtotal column header actions
+      cy.findByTestId("TableInteractive-root").findByText("Subtotal").click(); // open subtotal column header actions
       popover().icon("gear").click(); // open subtotal column settings
 
       //cy.findByText("Table options").should("not.exist"); // no longer displaying the top level settings
       // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
       cy.findByText("Separator style"); // shows subtotal column settings
 
-      cy.get(".TableInteractive").findByText("Created At").click(); // open created_at column header actions
+      cy.findByTestId("TableInteractive-root").findByText("Created At").click(); // open created_at column header actions
       popover().icon("gear").click(); // open created_at column settings
       // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
       cy.findByText("Date style"); // shows created_at column settings
