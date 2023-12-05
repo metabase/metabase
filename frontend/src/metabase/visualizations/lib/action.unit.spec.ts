7a40385de7a276ee5c51eecd15895e5b2a432050
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2023-12-05 19:47:33 +0700

    Add a test to ensure invalid URL won't break navigation

--- frontend/src/metabase/visualizations/lib/action.unit.spec.ts
+++ frontend/src/metabase/visualizations/lib/action.unit.spec.ts
@@ -109,4 +109,43 @@ describe("performAction", () => {
       type: "@@router/CALL_HISTORY_METHOD",
     });
   });
+
+  it("should redirect with invalid URL", () => {
+    MetabaseSettings.set("site-url", "http://localhost/metabase/");
+    const action: UrlClickAction = {
+      buttonType: "horizontal",
+      name: "automatic-insights",
+      section: "auto",
+      url: jest.fn(
+        () =>
+          "invalid_protocol://localhost/metabase/auto/dashboard/adhoc/123Abc",
+      ),
+    };
+
+    const extraProps = {
+      dispatch: jest.fn(),
+      onChangeCardAndRun: jest.fn(),
+    };
+
+    expect(performAction(action, extraProps)).toBe(true);
+
+    expect(action.url).toHaveBeenCalledTimes(1);
+
+    expect(extraProps.dispatch).toHaveBeenCalledTimes(2);
+    expect(extraProps.dispatch).toHaveBeenCalledWith({
+      payload: {
+        args: [
+          {
+            hash: "",
+            pathname:
+              "/invalid_protocol://localhost/metabase/auto/dashboard/adhoc/123Abc",
+            query: {},
+            search: "",
+          },
+        ],
+        method: "push",
+      },
+      type: "@@router/CALL_HISTORY_METHOD",
+    });
+  });
 });
