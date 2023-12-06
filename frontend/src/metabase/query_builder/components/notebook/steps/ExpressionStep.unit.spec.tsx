6c3741ff06c62e9f396e6b7ebefea52dec43c9a4
Kamil Mielnik <kamil@kamilmielnik.com>
2023-12-06 19:41:57 +0700

    Move setup to the top of the file

--- frontend/src/metabase/query_builder/components/notebook/steps/ExpressionStep.unit.spec.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/ExpressionStep.unit.spec.tsx
@@ -15,6 +15,65 @@ import type { NotebookStepUiComponentProps } from "../types";
 import { createMockNotebookStep } from "../test-utils";
 import { ExpressionStep } from "./ExpressionStep";
 
+function setup(
+  additionalProps?: Partial<NotebookStepUiComponentProps>,
+  expressions?: Record<string, Expression>,
+) {
+  const updateQuery = jest.fn();
+  const addExpression = jest.fn();
+  const updateExpression = jest.fn();
+  const removeExpression = jest.fn();
+
+  const query = createMockQueryForExpressions(expressions);
+
+  query.addExpression = addExpression;
+  query.updateExpression = updateExpression;
+  query.removeExpression = removeExpression;
+
+  const step = createMockNotebookStep({
+    type: "expression",
+    query,
+  });
+
+  render(
+    <ExpressionStep
+      step={step}
+      color="#93A1AB"
+      query={query}
+      topLevelQuery={step.topLevelQuery}
+      updateQuery={updateQuery}
+      isLastOpened={false}
+      reportTimezone="UTC"
+      {...additionalProps}
+    />,
+  );
+
+  return {
+    mocks: { addExpression, updateExpression, removeExpression, updateQuery },
+  };
+}
+
+const createMockQueryForExpressions = (
+  expressions?: Record<string, Expression>,
+) => {
+  const state = createMockState({
+    entities: createMockEntitiesState({
+      databases: [createSampleDatabase()],
+    }),
+  });
+
+  const metadata = getMetadata(state);
+  let query = checkNotNull(metadata.table(ORDERS_ID)).query();
+
+  if (expressions) {
+    Object.entries(expressions).forEach(([name, expression]) => {
+      query = query.addExpression(name, expression);
+    });
+  }
+
+  return query;
+};
+
 describe("Notebook Editor > Expression Step", () => {
   it("should handle updating existing expression", async () => {
     const expression: Expression = ["abs", ["field", 17, null]];
@@ -64,62 +123,3 @@ describe("Notebook Editor > Expression Step", () => {
     expect(removeExpression).toHaveBeenCalledWith("expr name");
   });
 });
-
-const createMockQueryForExpressions = (
-  expressions?: Record<string, Expression>,
-) => {
-  const state = createMockState({
-    entities: createMockEntitiesState({
-      databases: [createSampleDatabase()],
-    }),
-  });
-
-  const metadata = getMetadata(state);
-  let query = checkNotNull(metadata.table(ORDERS_ID)).query();
-
-  if (expressions) {
-    Object.entries(expressions).forEach(([name, expression]) => {
-      query = query.addExpression(name, expression);
-    });
-  }
-
-  return query;
-};
-
-function setup(
-  additionalProps?: Partial<NotebookStepUiComponentProps>,
-  expressions?: Record<string, Expression>,
-) {
-  const updateQuery = jest.fn();
-  const addExpression = jest.fn();
-  const updateExpression = jest.fn();
-  const removeExpression = jest.fn();
-
-  const query = createMockQueryForExpressions(expressions);
-
-  query.addExpression = addExpression;
-  query.updateExpression = updateExpression;
-  query.removeExpression = removeExpression;
-
-  const step = createMockNotebookStep({
-    type: "expression",
-    query,
-  });
-
-  render(
-    <ExpressionStep
-      step={step}
-      color="#93A1AB"
-      query={query}
-      topLevelQuery={step.topLevelQuery}
-      updateQuery={updateQuery}
-      isLastOpened={false}
-      reportTimezone="UTC"
-      {...additionalProps}
-    />,
-  );
-
-  return {
-    mocks: { addExpression, updateExpression, removeExpression, updateQuery },
-  };
-}
