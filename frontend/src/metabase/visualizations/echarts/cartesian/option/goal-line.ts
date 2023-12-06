f2bcd15cc0d85c1fdab18250dec6f46ad6c5f036
Emmad Usmani <emmadusmani@berkeley.edu>
2023-12-06 10:37:34 -0800

    combo chart goal line (#35345)
    * combo chart goal line
    
    * refactor to separate function
    
    * fix type error
    
    * remove comment
    
    * handle goal line setting disabled
    
    * handle y-axis split
    
    * improve variable names

---
+++ frontend/src/metabase/visualizations/echarts/cartesian/option/goal-line.ts
@@ -0,0 +1,41 @@
+import type { RegisteredSeriesOption } from "echarts";
+import type {
+  ComputedVisualizationSettings,
+  RenderingContext,
+} from "metabase/visualizations/types";
+import type { CartesianChartModel } from "../model/types";
+
+export function getGoalLineSeriesOption(
+  chartModel: CartesianChartModel,
+  settings: ComputedVisualizationSettings,
+  renderingContext: RenderingContext,
+): RegisteredSeriesOption["line"] | null {
+  if (!settings["graph.show_goal"]) {
+    return null;
+  }
+  const [_leftAxisKeys, rightAxisKeys] = chartModel.yAxisSplit;
+
+  return {
+    type: "line",
+    markLine: {
+      data: [{ name: "goal-line", yAxis: settings["graph.goal_value"] }],
+      label: {
+        position:
+          rightAxisKeys.length === 0 ? "insideEndTop" : "insideStartTop",
+        formatter: () => settings["graph.goal_label"] ?? "",
+        fontFamily: renderingContext.fontFamily,
+        fontSize: 14,
+        fontWeight: 700,
+        color: renderingContext.getColor("text-medium"),
+        textBorderWidth: 1,
+        textBorderColor: renderingContext.getColor("white"),
+      },
+      symbol: ["none", "none"],
+      lineStyle: {
+        color: renderingContext.getColor("text-medium"),
+        type: [5, 5],
+        width: 2,
+      },
+    },
+  };
+}
