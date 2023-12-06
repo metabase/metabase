170394614786dbbe3ae1c7f00c719160703c848e
Emmad Usmani <emmadusmani@berkeley.edu>
2023-12-06 09:32:54 -0800

    add dominant-baseline workaround for echarts (#35118)
    * add dominant-baseline workaround
    
    * patch nodes recursively
    
    * add test

--- frontend/src/metabase/static-viz/lib/svg.ts
+++ frontend/src/metabase/static-viz/lib/svg.ts
@@ -1,3 +1,8 @@
+// eslint-disable-next-line import/no-unresolved -- vscode is confused since only @types/hast is installed
+import type { Element, ElementContent } from "hast";
+import { fromHtml } from "hast-util-from-html";
+import { toHtml } from "hast-util-to-html";
+
 // FIXME: instead of Regex parse svg, update, serialize
 const transformSvgForOutline = (svgString: string) => {
   const regex =
@@ -22,9 +27,50 @@ const transformSvgForOutline = (svgString: string) => {
   );
 };
 
-// Removing xmlns attributes that cause Batik failing to convert svg to png
-export const sanitizeSvgForBatik = (svg: string) => {
-  return transformSvgForOutline(svg)
+/**
+ * Recursive ast traversal helper for `patchDominantBaseline`
+ */
+function patchNode(node: ElementContent) {
+  if (node.type !== "element") {
+    return;
+  }
+
+  if (
+    node.tagName === "text" &&
+    node.properties.dominantBaseline === "central"
+  ) {
+    node.properties.dy = "0.5em";
+  }
+
+  node.children.forEach(child => patchNode(child));
+}
+
+/**
+ * Fixes `<text>` elements not being vertically centered due to Batik not
+ * supporting the `dominant-baseline` property.
+ */
+export function patchDominantBaseline(svgString: string) {
+  const svgElem = fromHtml(svgString, { fragment: true, space: "svg" })
+    .children[0] as Element;
+
+  patchNode(svgElem);
+
+  return toHtml(svgElem, { space: "svg" });
+}
+
+/**
+ * Removes `xmlns` attributes that Batik does not support
+ */
+function removeNamespace(svgString: string) {
+  return svgString
     .replace('xmlns="http://www.w3.org/2000/svg"', "")
     .replace('xmlns:xlink="http://www.w3.org/1999/xlink"', "");
+}
+
+export const sanitizeSvgForBatik = (svgString: string) => {
+  return [
+    transformSvgForOutline,
+    patchDominantBaseline,
+    removeNamespace,
+  ].reduce((currSVGString, transform) => transform(currSVGString), svgString);
 };
