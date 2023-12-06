f8ec8b309eecea8937c81c0509542b5ec05816c7
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-12-06 09:46:51 +0200

    Move drills FE tests to master (#36454)

--- frontend/src/metabase-lib/tests/drills-summarize-column-by-time.unit.spec.ts
+++ frontend/src/metabase-lib/tests/drills-summarize-column-by-time.unit.spec.ts
@@ -1,157 +1,166 @@
 import {
-  ORDERS,
-  ORDERS_ID,
-  SAMPLE_DB_ID,
+  createOrdersCreatedAtDatasetColumn,
+  createOrdersIdField,
+  createOrdersTable,
+  createOrdersTotalDatasetColumn,
+  createOrdersTotalField,
+  createSampleDatabase,
 } from "metabase-types/api/mocks/presets";
+import { createMockMetadata } from "__support__/metadata";
 import * as Lib from "metabase-lib";
-import type { DrillThruType } from "metabase-lib";
-import { drillThru } from "metabase-lib";
-import type {
-  ApplyDrillTestCase,
-  DrillDisplayInfoTestCase,
-} from "metabase-lib/tests/drills-common";
-import { getDrillsQueryParameters } from "metabase-lib/tests/drills-common";
-import { getAvailableDrillByType } from "metabase-lib/test-helpers";
-
-const DRILL_TYPE: DrillThruType = "drill-thru/summarize-column-by-time";
+import {
+  createAggregatedCellClickObject,
+  createColumnClickObject,
+  createQuery,
+  createQueryWithClauses,
+  createRawCellClickObject,
+  findDrillThru,
+  queryDrillThru,
+} from "metabase-lib/test-helpers";
+import {
+  createCountDatasetColumn,
+  createNotEditableQuery,
+  createOrdersStructuredDatasetColumn,
+  createOrdersStructuredField,
+} from "./drills-common";
 
 describe("drill-thru/summarize-column-by-time", () => {
-  describe("availableDrillThrus", () => {
-    it.each<DrillDisplayInfoTestCase>([
-      {
-        clickType: "header",
-        queryType: "unaggregated",
-        columnName: "SUBTOTAL",
-        expectedParameters: {
-          type: "drill-thru/summarize-column-by-time",
-        },
-      },
-      {
-        clickType: "header",
-        queryType: "unaggregated",
-        columnName: "DISCOUNT",
-        expectedParameters: {
-          type: "drill-thru/summarize-column-by-time",
-        },
-      },
-      {
-        clickType: "header",
-        queryType: "unaggregated",
-        columnName: "QUANTITY",
-        expectedParameters: {
-          type: "drill-thru/summarize-column-by-time",
-        },
-      },
-    ])(
-      `should return "${DRILL_TYPE}" drill config for $columnName $clickType in $queryType query`,
-      ({
-        columnName,
-        clickType,
-        queryType,
-        queryTable = "ORDERS",
-        customQuestion,
-        expectedParameters,
-      }) => {
-        const { drillDisplayInfo } = getAvailableDrillByType({
-          drillType: DRILL_TYPE,
-          clickType,
-          clickedColumnName: columnName,
-          ...getDrillsQueryParameters(queryType, queryTable, customQuestion),
-        });
+  const drillType = "drill-thru/summarize-column-by-time";
+  const stageIndex = 0;
 
-        expect(drillDisplayInfo).toEqual(expectedParameters);
-      },
-    );
+  describe("raw query", () => {
+    const defaultQuery = createQuery();
+    const defaultColumn = createOrdersTotalDatasetColumn();
+
+    it("should drill thru a summable column", () => {
+      const clickObject = createColumnClickObject({
+        column: defaultColumn,
+      });
+      const { drill } = findDrillThru(
+        defaultQuery,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      const newQuery = Lib.drillThru(defaultQuery, stageIndex, drill);
+      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
+      expect(Lib.breakouts(newQuery, stageIndex)).toHaveLength(1);
+    });
+
+    it("should not drill thru a non-summable column", () => {
+      const clickObject = createColumnClickObject({
+        column: createOrdersCreatedAtDatasetColumn(),
+      });
+      const drill = queryDrillThru(
+        defaultQuery,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      expect(drill).toBeNull();
+    });
+
+    it("should not drill thru a column header when there is no date column", () => {
+      const metadata = createMockMetadata({
+        databases: [
+          createSampleDatabase({
+            tables: [
+              createOrdersTable({
+                fields: [createOrdersIdField(), createOrdersTotalField()],
+              }),
+            ],
+          }),
+        ],
+      });
+      const query = createQuery({ metadata });
+      const clickObject = createColumnClickObject({ column: defaultColumn });
+      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+      expect(drill).toBeNull();
+    });
+
+    it("should not drill thru a cell", () => {
+      const clickObject = createRawCellClickObject({
+        column: defaultColumn,
+        value: 10,
+      });
+      const drill = queryDrillThru(
+        defaultQuery,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      expect(drill).toBeNull();
+    });
+
+    it("should not drill thru a cell with null value", () => {
+      const clickObject = createRawCellClickObject({
+        column: defaultColumn,
+        value: null,
+      });
+      const drill = queryDrillThru(
+        defaultQuery,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      expect(drill).toBeNull();
+    });
+
+    it("should not drill thru a Structured column", () => {
+      const metadata = createMockMetadata({
+        databases: [
+          createSampleDatabase({
+            tables: [
+              createOrdersTable({
+                fields: [createOrdersIdField(), createOrdersStructuredField()],
+              }),
+            ],
+          }),
+        ],
+      });
+      const query = createQuery({ metadata });
+      const column = createOrdersStructuredDatasetColumn();
+      const clickObject = createColumnClickObject({ column });
+      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+      expect(drill).toBeNull();
+    });
+
+    it("should not drill thru a non-editable query", () => {
+      const query = createNotEditableQuery(defaultQuery);
+      const clickObject = createColumnClickObject({
+        column: defaultColumn,
+      });
+      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+      expect(drill).toBeNull();
+    });
   });
 
-  describe("drillThru", () => {
-    it.each<ApplyDrillTestCase>([
-      {
-        clickType: "header",
-        columnName: "TAX",
-        queryType: "unaggregated",
-        expectedQuery: {
-          aggregation: [
-            [
-              "sum",
-              [
-                "field",
-                ORDERS.TAX,
-                {
-                  "base-type": "type/Float",
-                },
-              ],
-            ],
-          ],
-          breakout: [
-            [
-              "field",
-              ORDERS.CREATED_AT,
-              {
-                "base-type": "type/DateTime",
-                "temporal-unit": "month",
-              },
-            ],
-          ],
-          "source-table": ORDERS_ID,
-        },
-      },
-      {
-        clickType: "header",
-        columnName: "QUANTITY",
-        queryType: "unaggregated",
-        expectedQuery: {
-          aggregation: [
-            [
-              "sum",
-              [
-                "field",
-                ORDERS.QUANTITY,
-                {
-                  "base-type": "type/Integer",
-                },
-              ],
-            ],
-          ],
-          breakout: [
-            [
-              "field",
-              ORDERS.CREATED_AT,
-              {
-                "base-type": "type/DateTime",
-                "temporal-unit": "month",
-              },
-            ],
-          ],
-          "source-table": ORDERS_ID,
-        },
-      },
-    ])(
-      `should return correct result on "${DRILL_TYPE}" drill apply to $columnName on $clickType in $queryType query`,
-      ({
-        columnName,
-        clickType,
-        queryType,
-        queryTable,
-        customQuestion,
-        drillArgs = [],
-        expectedQuery,
-      }) => {
-        const { drill, stageIndex, query } = getAvailableDrillByType({
-          drillType: DRILL_TYPE,
-          clickType,
-          clickedColumnName: columnName,
-          ...getDrillsQueryParameters(queryType, queryTable, customQuestion),
-        });
+  describe("aggregated query", () => {
+    const defaultQuery = createQueryWithClauses({
+      aggregations: [{ operatorName: "count" }],
+      breakouts: [{ columnName: "TOTAL", tableName: "ORDERS" }],
+    });
 
-        const updatedQuery = drillThru(query, stageIndex, drill, ...drillArgs);
-
-        expect(Lib.toLegacyQuery(updatedQuery)).toEqual({
-          database: SAMPLE_DB_ID,
-          query: expectedQuery,
-          type: "query",
-        });
-      },
-    );
+    it("should not drill thru an aggregated column", () => {
+      const clickObject = createAggregatedCellClickObject({
+        aggregation: {
+          column: createCountDatasetColumn(),
+          value: 10,
+        },
+        breakouts: [
+          {
+            column: createOrdersTotalDatasetColumn({ source: "breakout" }),
+            value: 20,
+          },
+        ],
+      });
+      const drill = queryDrillThru(
+        defaultQuery,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      expect(drill).toBeNull();
+    });
   });
 });
--- frontend/src/metabase-lib/tests/drills-zoom-in-timeseries.unit.spec.ts
+++ frontend/src/metabase-lib/tests/drills-zoom-in-timeseries.unit.spec.ts
@@ -1,60 +1,241 @@
-import type { DrillThruType } from "metabase-lib";
-import type { DrillDisplayInfoTestCase } from "metabase-lib/tests/drills-common";
-import { getDrillsQueryParameters } from "metabase-lib/tests/drills-common";
-import { getAvailableDrillByType } from "metabase-lib/test-helpers";
+import {
+  createOrdersCreatedAtDatasetColumn,
+  createOrdersQuantityDatasetColumn,
+} from "metabase-types/api/mocks/presets";
+import * as Lib from "metabase-lib";
+import {
+  createAggregatedCellClickObject,
+  createColumnClickObject,
+  createLegendItemClickObject,
+  createPivotCellClickObject,
+  findDrillThru,
+  queryDrillThru,
+  createQueryWithClauses,
+} from "metabase-lib/test-helpers";
+import {
+  createCountDatasetColumn,
+  createNotEditableQuery,
+} from "./drills-common";
 
-const DRILL_TYPE: DrillThruType = "drill-thru/zoom-in.timeseries";
+// eslint-disable-next-line jest/no-disabled-tests
+describe.skip("drill-thru/zoom-in.timeseries (metabase#36173)", () => {
+  const drillType = "drill-thru/zoom-in.timeseries";
+  const stageIndex = 0;
+  const aggregationColumn = createCountDatasetColumn();
+  const breakoutColumn = createOrdersCreatedAtDatasetColumn({
+    source: "breakout",
+  });
 
-describe("drill-thru/zoom-in.timeseries", () => {
-  describe("availableDrillThrus", () => {
-    it.each<DrillDisplayInfoTestCase>([
-      {
-        // "zoom-in.timeseries" should be returned for aggregated query metric click (metabase#33811)
-        clickType: "cell",
-        queryType: "aggregated",
-        columnName: "count",
-        expectedParameters: {
-          displayName: "See this month by week",
-          type: "drill-thru/zoom-in.timeseries",
+  describe.each([
+    { bucketName: "Year", displayName: "See this year by quarters" },
+    { bucketName: "Quarter", displayName: "See this quarter by months" },
+    { bucketName: "Month", displayName: "See this month by weeks" },
+    { bucketName: "Day", displayName: "See this day by hour" },
+    { bucketName: "Hour", displayName: "See this hour by minute" },
+  ])("$bucketName", ({ bucketName, displayName }) => {
+    it("should drill thru an aggregated cell", () => {
+      const query = createQueryWithClauses({
+        aggregations: [{ operatorName: "count" }],
+        breakouts: [
+          {
+            columnName: breakoutColumn.name,
+            tableName: "ORDERS",
+            temporalBucketName: bucketName,
+          },
+        ],
+      });
+      const clickObject = createAggregatedCellClickObject({
+        aggregation: {
+          column: aggregationColumn,
+          value: 10,
         },
-      },
-      {
-        clickType: "cell",
-        queryType: "aggregated",
-        columnName: "max",
-        expectedParameters: {
-          displayName: "See this month by week",
-          type: "drill-thru/zoom-in.timeseries",
-        },
-      },
-      {
-        clickType: "cell",
-        queryType: "aggregated",
-        columnName: "sum",
-        expectedParameters: {
-          displayName: "See this month by week",
-          type: "drill-thru/zoom-in.timeseries",
-        },
-      },
-    ])(
-      `should return "${DRILL_TYPE}" drill config for $columnName $clickType in $queryType query`,
-      ({
-        columnName,
-        clickType,
-        queryType,
-        queryTable = "ORDERS",
-        customQuestion,
-        expectedParameters,
-      }) => {
-        const { drillDisplayInfo } = getAvailableDrillByType({
-          drillType: DRILL_TYPE,
-          clickType,
-          clickedColumnName: columnName,
-          ...getDrillsQueryParameters(queryType, queryTable, customQuestion),
-        });
+        breakouts: [
+          {
+            column: breakoutColumn,
+            value: "2020-01-01",
+          },
+        ],
+      });
+      const { drill, drillInfo } = findDrillThru(
+        query,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      expect(drillInfo).toMatchObject({
+        displayName,
+      });
 
-        expect(drillDisplayInfo).toEqual(expectedParameters);
+      const newQuery = Lib.drillThru(query, stageIndex, drill);
+      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
+      expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+    });
+
+    it("should drill thru a pivot cell", () => {
+      const query = createQueryWithClauses({
+        aggregations: [{ operatorName: "count" }],
+        breakouts: [
+          {
+            columnName: "CREATED_AT",
+            tableName: "ORDERS",
+            temporalBucketName: bucketName,
+          },
+          {
+            columnName: "QUANTITY",
+            tableName: "ORDERS",
+          },
+        ],
+      });
+      const clickObject = createPivotCellClickObject({
+        aggregation: {
+          column: aggregationColumn,
+          value: 10,
+        },
+        breakouts: [
+          {
+            column: breakoutColumn,
+            value: "2020-01-01",
+          },
+          {
+            column: createOrdersQuantityDatasetColumn({
+              source: "breakout",
+            }),
+            value: 0,
+          },
+        ],
+      });
+      const { drill, drillInfo } = findDrillThru(
+        query,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      expect(drillInfo).toMatchObject({
+        displayName,
+      });
+
+      const newQuery = Lib.drillThru(query, stageIndex, drill);
+      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
+      expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+    });
+
+    it("should drill thru a legend item", () => {
+      const query = createQueryWithClauses({
+        aggregations: [{ operatorName: "count" }],
+        breakouts: [
+          {
+            columnName: "CREATED_AT",
+            tableName: "ORDERS",
+            temporalBucketName: bucketName,
+          },
+          {
+            columnName: "QUANTITY",
+            tableName: "ORDERS",
+          },
+        ],
+      });
+      const clickObject = createLegendItemClickObject({
+        column: breakoutColumn,
+        value: "2020-01-01",
+      });
+      const { drill, drillInfo } = findDrillThru(
+        query,
+        stageIndex,
+        clickObject,
+        drillType,
+      );
+      expect(drillInfo).toMatchObject({
+        displayName,
+      });
+
+      const newQuery = Lib.drillThru(query, stageIndex, drill);
+      expect(Lib.aggregations(newQuery, stageIndex)).toHaveLength(1);
+      expect(Lib.filters(newQuery, stageIndex)).toHaveLength(1);
+    });
+
+    it("should not drill thru a column header", () => {
+      const query = createQueryWithClauses({
+        aggregations: [{ operatorName: "count" }],
+        breakouts: [
+          {
+            columnName: breakoutColumn.name,
+            tableName: "ORDERS",
+            temporalBucketName: bucketName,
+          },
+        ],
+      });
+      const clickObject = createColumnClickObject({
+        column: aggregationColumn,
+      });
+      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+      expect(drill).toBeNull();
+    });
+
+    it("should not drill thru a non-editable query", () => {
+      const query = createNotEditableQuery(
+        createQueryWithClauses({
+          aggregations: [{ operatorName: "count" }],
+          breakouts: [
+            {
+              columnName: breakoutColumn.name,
+              tableName: "ORDERS",
+              temporalBucketName: bucketName,
+            },
+          ],
+        }),
+      );
+      const clickObject = createAggregatedCellClickObject({
+        aggregation: {
+          column: aggregationColumn,
+          value: 10,
+        },
+        breakouts: [
+          {
+            column: breakoutColumn,
+            value: "2020-01-01",
+          },
+        ],
+      });
+      const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+      expect(drill).toBeNull();
+    });
+  });
+
+  it.each([
+    "Minute",
+    "Minute of hour",
+    "Hour of day",
+    "Day of week",
+    "Day of month",
+    "Day of year",
+    "Week of year",
+    "Month of year",
+    "Quarter of year",
+    "Don't bin",
+  ])('should not drill thru with "%s" temporal bucket', bucketName => {
+    const query = createQueryWithClauses({
+      aggregations: [{ operatorName: "count" }],
+      breakouts: [
+        {
+          columnName: breakoutColumn.name,
+          tableName: "ORDERS",
+          temporalBucketName: bucketName,
+        },
+      ],
+    });
+    const clickObject = createAggregatedCellClickObject({
+      aggregation: {
+        column: aggregationColumn,
+        value: 10,
       },
-    );
+      breakouts: [
+        {
+          column: breakoutColumn,
+          value: "2020-01-01",
+        },
+      ],
+    });
+    const drill = queryDrillThru(query, stageIndex, clickObject, drillType);
+    expect(drill).toBeNull();
   });
 });
