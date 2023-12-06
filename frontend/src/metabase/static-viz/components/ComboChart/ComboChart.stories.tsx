9baa737cb4d184bf6a7988ed9a8f0f77796b7e69
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2023-12-06 15:25:49 -0300

    Support Y-axis split (#35307)

---
+++ frontend/src/metabase/static-viz/components/ComboChart/ComboChart.stories.tsx
@@ -0,0 +1,29 @@
+import type { ComponentStory } from "@storybook/react";
+import { color } from "metabase/lib/colors";
+import { formatStaticValue } from "metabase/static-viz/lib/format";
+import { measureTextWidth } from "metabase/static-viz/lib/text";
+import * as questions from "metabase/static-viz/components/ComboChart/stories-data";
+import { ComboChart } from "./ComboChart";
+
+export default {
+  title: "static-viz/ComboChart",
+  component: ComboChart,
+};
+
+const Template: ComponentStory<typeof ComboChart> = args => {
+  return <ComboChart {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  width: 1000,
+  height: 600,
+  rawSeries: questions.autoYSplit as any,
+  dashcardSettings: {},
+  renderingContext: {
+    getColor: color,
+    formatValue: formatStaticValue as any,
+    measureText: measureTextWidth as any,
+    fontFamily: "Lato",
+  },
+};
