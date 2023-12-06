00e44f6052f4c0b3d76fba2c126d72e211429c84
Jerry Huang <jhuang37050@gmail.com>
2023-12-06 12:58:29 -0500

    when passing a resource id as string instead of number, things break with cryptic errors

--- src/metabase/api/embed.clj
+++ src/metabase/api/embed.clj
@@ -332,6 +332,8 @@
 (defn- check-embedding-enabled-for-object
   "Check that embedding is enabled, that `object` exists, and embedding for `object` is enabled."
   ([entity id]
+   (api/check (and (integer? id) (> id 0))
+              [400 (tru "id should be a positive integer")])
    (check-embedding-enabled-for-object (t2/select-one [entity :enable_embedding] :id id)))
 
   ([object]
@@ -416,8 +418,12 @@
      {:resource {:dashboard <dashboard-id>}}"
   [token]
   (let [unsigned (embed/unsign token)]
+    (def unsigned unsigned)
     (check-embedding-enabled-for-dashboard (embed/get-in-unsigned-token-or-throw unsigned [:resource :dashboard]))
     (dashboard-for-unsigned-token unsigned, :constraints [:enable_embedding true])))
+
+(embed/get-in-unsigned-token-or-throw unsigned [:resource :dashboard])
+(check-embedding-enabled-for-dashboard "8")
 
 (defn- dashcard-results-for-signed-token-async
   "Fetch the results of running a Card belonging to a Dashboard using a JSON Web Token signed with the
