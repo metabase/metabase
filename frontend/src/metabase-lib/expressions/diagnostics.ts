02b6fb648201900861cfdc70d1593601fe3c46c0
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2023-12-07 14:21:19 +0300

    Revert "Update diagnostics.ts"
    This reverts commit 9fdf4df70937723b813e683b2d4955e1b9e6a195.

--- frontend/src/metabase-lib/expressions/diagnostics.ts
+++ frontend/src/metabase-lib/expressions/diagnostics.ts
@@ -189,7 +189,6 @@ function isErrorWithMessage(err: unknown): err is ErrorWithMessage {
   return (
     typeof err === "object" &&
     err != null &&
-    "message" in err &&
-    typeof err.message === "string"
+    typeof (err as any).message === "string"
   );
 }
