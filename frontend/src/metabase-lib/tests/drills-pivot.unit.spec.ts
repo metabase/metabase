df7045ad483b1eb4645e8ff488ca3b625a85542f
Braden Shepherdson <braden@metabase.com>
2023-12-08 03:56:50 -0500

    [MLv2] Fix pivot drills integration with the FE (#36509)
    These were broken because `lib.js/pivot-types` was returning CLJS
    keywords rather than strings.
    
    Also makes applying drill-thrus less noisy (`log/info` -> `log/debug`).
    
    Unskips the FE integration tests for pivot drills. Fixes #33559.

--- frontend/src/metabase-lib/tests/drills-pivot.unit.spec.ts
+++ frontend/src/metabase-lib/tests/drills-pivot.unit.spec.ts
@@ -18,8 +18,7 @@ import {
   createNotEditableQuery,
 } from "./drills-common";
 
-// eslint-disable-next-line
-describe.skip("drill-thru/pivot (metabase#33559)", () => {
+describe("drill-thru/pivot (metabase#33559)", () => {
   const drillType = "drill-thru/pivot";
   const stageIndex = 0;
 
