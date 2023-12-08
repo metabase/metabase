164c92eb7640ad7054ec30858b5662db7e13a859
Ryan Laurie <iethree@gmail.com>
2023-12-08 07:27:01 -0700

    more collectionpicker work

--- frontend/src/metabase/common/components/EntityPicker/SpecificEntityPickers/CollectionPicker.tsx
+++ frontend/src/metabase/common/components/EntityPicker/SpecificEntityPickers/CollectionPicker.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import type { Collection } from "metabase-types/api";
 import { CollectionsApi } from "metabase/services";
@@ -8,52 +8,67 @@ import { NestedItemPicker } from "../NestedItemPicker";
 import type { PickerState } from "../types";
 
 interface CollectionPickerProps {
-  onItemSelect: (item: Collection) => void;
-  initialCollectionId?: number;
+  onItemSelect: (item: Partial<Collection>) => void;
+  value?: Partial<Collection>;
 }
 
 export function CollectionPicker({
   onItemSelect,
-  initialCollectionId,
+  value,
 }: CollectionPickerProps) {
   const [ initialState, setInitialState ] = useState<PickerState<Collection>>();
 
-  const onFolderSelect = async (folder?: Partial<Collection>): Promise<Collection[]> => {
-    onItemSelect(folder ?? { id: root });
+  const onFolderSelect = useCallback(async (folder?: Partial<Collection>): Promise<Collection[]> => {
+    onItemSelect(folder ?? { id: 'root' });
     const items = await CollectionsApi.listItems(
       { id: folder?.id ?? "root", models: ["collection"] },
     );
 
+    if (folder?.id === "root") {
+      const ourAnalytics = await CollectionsApi.getRoot();
+      items.data.unshift({ ...ourAnalytics, model: 'collection', id: 'root' } );
+    }
+
     return items.data;
-  };
+  }, [onItemSelect]);
 
   useEffect(() => {
-    if (initialCollectionId) {
+    console.log({ value })
+    if (value?.id) {
       CollectionsApi
-        .listItems({ id: initialCollectionId })
+        .get({ id: value.id })
         .then(async (collection) => {
           const path = [
             "root",
-            ...collection.location.split("/"),
-            initialCollectionId,
+            ...collection.location.split("/").map(Number),
+            value.id,
           ].filter(Boolean);
+
+
 
           const stack = await Promise.all(
             path.map(async (id, index) => {
-              setInitialState({
-                items: await onFolderSelect({ id }),
-                selectedItem: path[index + 1] ?? null,
+              const items = await onFolderSelect({ id });
+              const selectedItem = items.find(item => item.id === path[index + 1]) ?? null;
+              return({
+                items,
+                selectedItem,
               });
             }),
           );
-          return stack;
+
+          console.log({ path, stack })
+
+          setInitialState(stack);
         });
     } else {
       onFolderSelect({ id: "root" }).then(items => {
         setInitialState([{ items, selectedItem: null }]);
       });
     }
-  }, [initialCollectionId]);
+  }, [value?.id, onFolderSelect]);
+
+  console.log({ initialState })
 
 
   if (!initialState) {
