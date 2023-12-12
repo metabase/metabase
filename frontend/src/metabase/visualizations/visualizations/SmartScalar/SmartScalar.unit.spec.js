9bb8179e754a9a10fc47dd5e11fd09be5ad94328
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-12-11 22:20:10 -0500

    add component tests for SmartScalar

--- frontend/src/metabase/visualizations/visualizations/SmartScalar/SmartScalar.unit.spec.js
+++ frontend/src/metabase/visualizations/visualizations/SmartScalar/SmartScalar.unit.spec.js
@@ -5,13 +5,33 @@ import { DateTimeColumn, NumberColumn } from "__support__/visualizations";
 import Visualization from "metabase/visualizations/components/Visualization";
 import { getSettingsWidgetsForSeries } from "metabase/visualizations/lib/settings/visualization";
 import registerVisualizations from "metabase/visualizations/register";
+import { COMPARISON_TYPES } from "./utils";
+
+console.error = jest.fn();
 
 registerVisualizations();
 
-const setup = (series, width) =>
+const setup = (series, width = 800) =>
   renderWithProviders(<Visualization rawSeries={series} width={width} />);
 
-const series = ({ rows, insights, field }) => {
+const PREVIOUS_PERIOD_COMPARISON = {
+  type: COMPARISON_TYPES.PREVIOUS_PERIOD,
+};
+
+const PREVIOUS_VALUE_COMPARISON = {
+  type: COMPARISON_TYPES.PREVIOUS_VALUE,
+};
+
+const getPeriodsAgoComparison = value => ({
+  type: COMPARISON_TYPES.PERIODS_AGO,
+  value,
+});
+
+const series = (
+  { rows, insights, field, comparisonType } = {
+    comparisonType: PREVIOUS_PERIOD_COMPARISON,
+  },
+) => {
   const cols = [
     DateTimeColumn({ name: "Month" }),
     NumberColumn({ name: "Count" }),
@@ -21,7 +41,10 @@ const series = ({ rows, insights, field }) => {
     {
       card: {
         display: "smartscalar",
-        visualization_settings: { "scalar.field": field },
+        visualization_settings: {
+          "scalar.field": field,
+          "scalar.comparisons": comparisonType,
+        },
       },
       data: { cols, rows, insights },
     },
@@ -29,6 +52,260 @@ const series = ({ rows, insights, field }) => {
 };
 
 describe("SmartScalar", () => {
+  describe("Current Metric Display", () => {
+    it("should show metric value and date", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", 100],
+        ["2019-11-01T00:00:00", 120],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }));
+
+      expect(screen.getByText("120")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+    });
+  });
+
+  describe("Comparison Display", () => {
+    it("should show increase", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", 100],
+        ["2019-11-01T00:00:00", 120],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }));
+
+      expect(screen.getByRole("img")).toHaveAttribute(
+        "aria-label",
+        "arrow_up icon",
+      );
+      expect(screen.getByText("20%")).toBeInTheDocument();
+
+      expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
+      expect(screen.getByText("100")).toBeInTheDocument();
+    });
+
+    it("should show ↑ ∞% change", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", 0],
+        ["2019-11-01T00:00:00", 100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }), 400);
+
+      expect(screen.getByText("100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+      expect(screen.getByRole("img")).toHaveAttribute(
+        "aria-label",
+        "arrow_up icon",
+      );
+      expect(screen.getByText("∞%")).toBeInTheDocument();
+      expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
+      expect(screen.getByText("0")).toBeInTheDocument();
+    });
+
+    it("should show decrease", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", 100],
+        ["2019-11-01T00:00:00", 80],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }));
+
+      expect(screen.getByText("80")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+
+      expect(screen.getByRole("img")).toHaveAttribute(
+        "aria-label",
+        "arrow_down icon",
+      );
+      expect(screen.getByText("20%")).toBeInTheDocument();
+
+      expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
+      expect(screen.getByText("100")).toBeInTheDocument();
+    });
+
+    it("should show ↓ ∞% change", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", 0],
+        ["2019-11-01T00:00:00", -100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }), 400);
+
+      expect(screen.getByText("-100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+      expect(screen.getByRole("img")).toHaveAttribute(
+        "aria-label",
+        "arrow_down icon",
+      );
+      expect(screen.getByText("∞%")).toBeInTheDocument();
+      expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
+      expect(screen.getByText("0")).toBeInTheDocument();
+    });
+
+    it("should show 0% change", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", 100],
+        ["2019-11-01T00:00:00", 100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }), 400);
+
+      expect(screen.getByText("100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+      expect(screen.getByText("No change")).toBeInTheDocument();
+      expect(screen.getByText("vs. previous month")).toBeInTheDocument();
+    });
+
+    it("should show when data is missing", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", null],
+        ["2019-11-01T00:00:00", 100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }), 400);
+
+      expect(screen.getByText("100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+      expect(screen.getByText("N/A")).toBeInTheDocument();
+      expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
+      expect(screen.getByText("(No data)")).toBeInTheDocument();
+    });
+
+    it("shouldn't throw an error getting settings for single-column data", () => {
+      const card = { display: "smartscalar", visualization_settings: {} };
+      const data = { cols: [NumberColumn({ name: "Count" })], rows: [[100]] };
+      expect(() => getSettingsWidgetsForSeries([{ card, data }])).not.toThrow();
+    });
+
+    it("shouldn't render compact if normal formatting is <=6 characters", () => {
+      const width = 200;
+      const rows = [
+        ["2019-10-01T00:00:00", 100],
+        ["2019-11-01T00:00:00", 81005],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }), width);
+
+      expect(screen.getByText("81,005")).toBeInTheDocument();
+    });
+
+    it("should render compact if normal formatting is >6 characters and width <250", () => {
+      const width = 200;
+      const rows = [
+        ["2019-10-01T00:00:00", 100],
+        ["2019-11-01T00:00:00", 810750.54],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }), width);
+
+      expect(screen.getByText("810.8k")).toBeInTheDocument();
+    });
+
+    it("should display tooltip with comparison info if card is not wide enough", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", 50],
+        ["2019-11-01T00:00:00", 100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(series({ rows, insights }), 50);
+
+      expect(screen.getByText("100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+
+      // can see arrow and percent change
+      expect(screen.getByRole("img")).toHaveAttribute(
+        "aria-label",
+        "arrow_up icon",
+      );
+      const lastChange = screen.getByText("100%");
+      expect(lastChange).toBeInTheDocument();
+
+      // cannot see comparison period or comparison value b/c they are hidden
+      expect(screen.queryByText("vs. previous month:")).not.toBeInTheDocument();
+      expect(screen.queryByText("50")).not.toBeInTheDocument();
+
+      // show tool-tip
+      userEvent.hover(lastChange);
+      expect(screen.queryAllByLabelText("arrow_up icon")).toHaveLength(2);
+      expect(screen.queryAllByText("100%")).toHaveLength(2);
+      expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
+      expect(screen.getByText("50")).toBeInTheDocument();
+    });
+  });
+
+  describe("comparison: previousValue", () => {
+    it("should skip over rows with null values", () => {
+      const rows = [
+        ["2019-09-01T00:00:00", 100],
+        ["2019-10-01T00:00:00", null],
+        ["2019-11-01T00:00:00", 100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(
+        series({ rows, insights, comparisonType: PREVIOUS_VALUE_COMPARISON }),
+        400,
+      );
+
+      expect(screen.getByText("100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+      expect(screen.getByText("No change")).toBeInTheDocument();
+      expect(screen.getByText("vs. Sep")).toBeInTheDocument();
+    });
+
+    it("should handle no previous value to compare to", () => {
+      const rows = [
+        ["2019-10-01T00:00:00", null],
+        ["2019-11-01T00:00:00", 100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(
+        series({ rows, insights, comparisonType: PREVIOUS_VALUE_COMPARISON }),
+        400,
+      );
+
+      expect(screen.getByText("100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+      expect(screen.getByText("N/A")).toBeInTheDocument();
+      expect(screen.queryByText("vs. Oct:")).not.toBeInTheDocument();
+      expect(screen.getByText("(No data)")).toBeInTheDocument();
+    });
+  });
+
+  describe("comparison: periodsAgo", () => {
+    it("should display exact date", () => {
+      const rows = [
+        ["2019-09-01T00:00:00", 100],
+        ["2019-10-01T00:00:00", null],
+        ["2019-11-01T00:00:00", 100],
+      ];
+      const insights = [{ unit: "month", col: "Count" }];
+
+      setup(
+        series({ rows, insights, comparisonType: getPeriodsAgoComparison(2) }),
+        400,
+      );
+
+      expect(screen.getByText("100")).toBeInTheDocument();
+      expect(screen.getByText("Nov 2019")).toBeInTheDocument();
+      expect(screen.getByText("No change")).toBeInTheDocument();
+      expect(screen.getByText("vs. Sep")).toBeInTheDocument();
+    });
+  });
+
   describe("field selection", () => {
     const rows = [
       ["2019-10-01T00:00:00", 100, 200],
@@ -38,205 +315,37 @@ describe("SmartScalar", () => {
       { unit: "month", col: "Count" },
       { unit: "month", col: "Sum" },
     ];
+
     it("should use first non-date column (Count) by default", () => {
-      setup(series({ rows, insights }));
+      setup(
+        series({
+          rows,
+          insights,
+        }),
+      );
       expect(screen.getByText("120")).toBeInTheDocument();
       expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-      const lastChange = screen.getByText("20%");
-      expect(lastChange).toBeInTheDocument();
-      userEvent.hover(lastChange);
+      expect(screen.getByText("20%")).toBeInTheDocument();
       expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
       expect(screen.getByText("100")).toBeInTheDocument();
     });
+
     it("should use Count when selected", () => {
       setup(series({ rows, insights, field: "Count" }));
       expect(screen.getByText("120")).toBeInTheDocument();
       expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-      const lastChange = screen.getByText("20%");
-      expect(lastChange).toBeInTheDocument();
-      userEvent.hover(lastChange);
+      expect(screen.getByText("20%")).toBeInTheDocument();
       expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
       expect(screen.getByText("100")).toBeInTheDocument();
     });
+
     it("should use Sum when selected", () => {
       setup(series({ rows, insights, field: "Sum" }));
       expect(screen.getByText("220")).toBeInTheDocument();
       expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-      const lastChange = screen.getByText("10%");
-      expect(lastChange).toBeInTheDocument();
-      userEvent.hover(lastChange);
+      expect(screen.getByText("10%")).toBeInTheDocument();
       expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
       expect(screen.getByText("200")).toBeInTheDocument();
     });
-  });
-  it("should show 20% increase", () => {
-    const rows = [
-      ["2019-10-01T00:00:00", 100],
-      ["2019-11-01T00:00:00", 120],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }));
-
-    expect(screen.getByText("120")).toBeInTheDocument();
-    expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-
-    const lastChange = screen.getByText("20%");
-    expect(lastChange).toBeInTheDocument();
-
-    userEvent.hover(lastChange);
-    expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
-    expect(screen.getByText("100")).toBeInTheDocument();
-  });
-
-  it("should show 20% decrease", () => {
-    const rows = [
-      ["2019-10-01T00:00:00", 100],
-      ["2019-11-01T00:00:00", 80],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }));
-
-    expect(screen.getByText("80")).toBeInTheDocument();
-    expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-
-    const lastChange = screen.getByText("20%");
-    expect(lastChange).toBeInTheDocument();
-
-    userEvent.hover(lastChange);
-    expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
-    expect(screen.getByText("100")).toBeInTheDocument();
-  });
-
-  it("should show 0% change", () => {
-    const rows = [
-      ["2019-10-01T00:00:00", 100],
-      ["2019-11-01T00:00:00", 100],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }), 400);
-
-    expect(screen.getByText("100")).toBeInTheDocument();
-    expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-    expect(screen.getByText("No change")).toBeInTheDocument();
-    expect(screen.getByText("vs. previous month")).toBeInTheDocument();
-  });
-
-  it("should show when data is missing", () => {
-    const rows = [
-      ["2019-10-01T00:00:00", null],
-      ["2019-11-01T00:00:00", 100],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }), 400);
-
-    expect(screen.getByText("100")).toBeInTheDocument();
-    expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-    expect(screen.getByText("N/A")).toBeInTheDocument();
-    expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
-    expect(screen.getByText("(empty)")).toBeInTheDocument();
-  });
-
-  it("(comparison: previous value) should skip over rows with null values", () => {
-    const rows = [
-      ["2019-09-01T00:00:00", 100],
-      ["2019-10-01T00:00:00", null],
-      ["2019-11-01T00:00:00", 100],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }), 400);
-
-    expect(screen.getByText("100")).toBeInTheDocument();
-    expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-    expect(screen.getByText("No change")).toBeInTheDocument();
-    expect(screen.getByText("vs. Sep")).toBeInTheDocument();
-  });
-
-  it("should show ↑ ∞% change", () => {
-    const rows = [
-      ["2019-10-01T00:00:00", 0],
-      ["2019-11-01T00:00:00", 100],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }), 400);
-
-    expect(screen.getByText("100")).toBeInTheDocument();
-    expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-    expect(screen.getByRole("img")).toHaveAttribute(
-      "aria-label",
-      "arrow_up icon",
-    );
-    expect(screen.getByText("∞%")).toBeInTheDocument();
-    expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
-    expect(screen.getByText("0")).toBeInTheDocument();
-  });
-
-  it("should show ↓ ∞% change", () => {
-    const rows = [
-      ["2019-10-01T00:00:00", 0],
-      ["2019-11-01T00:00:00", -100],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }), 400);
-
-    expect(screen.getByText("-100")).toBeInTheDocument();
-    expect(screen.getByText("Nov 2019")).toBeInTheDocument();
-    expect(screen.getByRole("img")).toHaveAttribute(
-      "aria-label",
-      "arrow_down icon",
-    );
-    expect(screen.getByText("∞%")).toBeInTheDocument();
-    expect(screen.getByText("vs. previous month:")).toBeInTheDocument();
-    expect(screen.getByText("0")).toBeInTheDocument();
-  });
-
-  it("should show 8000% change", () => {
-    const rows = [
-      ["2019-10-01T00:00:00", 100],
-      ["2019-11-01T00:00:00", 8100],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }));
-
-    expect(screen.getByText("8,000%")).toBeInTheDocument();
-  });
-
-  it("shouldn't throw an error getting settings for single-column data", () => {
-    const card = { display: "smartscalar", visualization_settings: {} };
-    const data = { cols: [NumberColumn({ name: "Count" })], rows: [[100]] };
-    expect(() => getSettingsWidgetsForSeries([{ card, data }])).not.toThrow();
-  });
-
-  it("shouldn't render compact if normal formatting is <=6 characters", () => {
-    const width = 200;
-    const rows = [
-      ["2019-10-01T00:00:00", 100],
-      ["2019-11-01T00:00:00", 81005],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }), width);
-
-    expect(screen.getByText("81,005")).toBeInTheDocument();
-  });
-
-  it("should render compact if normal formatting is >6 characters and width <250", () => {
-    const width = 200;
-    const rows = [
-      ["2019-10-01T00:00:00", 100],
-      ["2019-11-01T00:00:00", 810750.54],
-    ];
-    const insights = [{ unit: "month", col: "Count" }];
-
-    setup(series({ rows, insights }), width);
-
-    expect(screen.getByText("810.8k")).toBeInTheDocument();
   });
 });
