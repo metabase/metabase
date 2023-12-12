67c8d09345a98dbee9cee0b785244b5cf846d812
Anton Kulyk <kuliks.anton@gmail.com>
2023-12-12 18:36:07 +0000

    Simplify things

--- frontend/src/metabase-types/api/mocks/query.ts
+++ frontend/src/metabase-types/api/mocks/query.ts
@@ -5,15 +5,10 @@ import type {
   StructuredQuery,
 } from "metabase-types/api";
 
-// Corresponds to the Sample Database and sample Products table from the built-in preset:
-// https://github.com/metabase/metabase/blob/master/frontend/src/metabase-types/api/mocks/presets/sample_database.ts
-const DEFAULT_DB_ID = 1;
-const DEFAULT_TABLE_ID = 1;
-
 export const createMockStructuredQuery = (
   opts?: Partial<StructuredQuery>,
 ): StructuredQuery => ({
-  "source-table": DEFAULT_TABLE_ID,
+  "source-table": 1,
   ...opts,
 });
 
@@ -28,7 +23,7 @@ export const createMockStructuredDatasetQuery = (
   opts?: Partial<StructuredDatasetQuery>,
 ): StructuredDatasetQuery => ({
   type: "query",
-  database: DEFAULT_DB_ID,
+  database: 1,
   query: createMockStructuredQuery(),
   ...opts,
 });
@@ -37,7 +32,7 @@ export const createMockNativeDatasetQuery = (
   opts?: Partial<NativeDatasetQuery>,
 ): NativeDatasetQuery => ({
   type: "native",
-  database: DEFAULT_DB_ID,
+  database: 1,
   native: createMockNativeQuery(),
   ...opts,
 });
