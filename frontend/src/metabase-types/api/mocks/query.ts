c7b90c12e1c9954a25968531a4913caf5fb0acfa
Anton Kulyk <kuliks.anton@gmail.com>
2023-12-12 18:30:16 +0000

    Use sample products table ID in `createMockStructuredQuery`

--- frontend/src/metabase-types/api/mocks/query.ts
+++ frontend/src/metabase-types/api/mocks/query.ts
@@ -5,9 +5,15 @@ import type {
   StructuredQuery,
 } from "metabase-types/api";
 
+// Corresponds to the Sample Database and sample Products table from the built-in preset:
+// https://github.com/metabase/metabase/blob/master/frontend/src/metabase-types/api/mocks/presets/sample_database.ts
+const DEFAULT_DB_ID = 1;
+const DEFAULT_TABLE_ID = 1;
+
 export const createMockStructuredQuery = (
   opts?: Partial<StructuredQuery>,
 ): StructuredQuery => ({
+  "source-table": DEFAULT_TABLE_ID,
   ...opts,
 });
 
@@ -22,7 +28,7 @@ export const createMockStructuredDatasetQuery = (
   opts?: Partial<StructuredDatasetQuery>,
 ): StructuredDatasetQuery => ({
   type: "query",
-  database: 1,
+  database: DEFAULT_DB_ID,
   query: createMockStructuredQuery(),
   ...opts,
 });
@@ -31,7 +37,7 @@ export const createMockNativeDatasetQuery = (
   opts?: Partial<NativeDatasetQuery>,
 ): NativeDatasetQuery => ({
   type: "native",
-  database: 1,
+  database: DEFAULT_DB_ID,
   native: createMockNativeQuery(),
   ...opts,
 });
