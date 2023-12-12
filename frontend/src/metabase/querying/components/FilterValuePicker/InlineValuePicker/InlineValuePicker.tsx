ea31776c2d0994f9f365eddf0c94d39a983d8f00
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-12-12 18:47:47 +0200

    Use value picker

--- frontend/src/metabase/querying/components/FilterValuePicker/InlineValuePicker/InlineValuePicker.tsx
+++ frontend/src/metabase/querying/components/FilterValuePicker/InlineValuePicker/InlineValuePicker.tsx
@@ -1,8 +1,10 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { t } from "ttag";
-import { Checkbox, SimpleGrid, Stack, TextInput } from "metabase/ui";
+import { Checkbox, SimpleGrid, Stack, Text, TextInput } from "metabase/ui";
+import { Icon } from "metabase/core/components/Icon";
 import type { FieldValue } from "metabase-types/api";
 import { getFieldOptions } from "../utils";
+import { searchOptions } from "./utils";
 
 interface InlineValuePickerProps {
   data: FieldValue[];
@@ -25,21 +27,34 @@ export function InlineValuePicker({
 }
 
 function DefaultValuePicker({ data, value, onChange }: InlineValuePickerProps) {
-  const options = useMemo(() => getFieldOptions(data), [data]);
+  const [searchValue, setSearchValue] = useState("");
+  const options = getFieldOptions(data);
+  const visibleOptions = searchOptions(options, searchValue);
 
   return (
     <Stack>
-      <TextInput placeholder={t`Search the list`} />
+      <TextInput
+        value={searchValue}
+        placeholder={t`Search the list`}
+        onChange={event => setSearchValue(event.currentTarget.value)}
+      />
       <Checkbox.Group value={value} onChange={onChange}>
-        <Stack>
-          {options.map(option => (
-            <Checkbox
-              key={option.value}
-              value={option.value}
-              label={option.label}
-            />
-          ))}
-        </Stack>
+        {visibleOptions.length > 0 ? (
+          <Stack>
+            {visibleOptions.map(option => (
+              <Checkbox
+                key={option.value}
+                value={option.value}
+                label={option.label}
+              />
+            ))}
+          </Stack>
+        ) : (
+          <Stack c="text.0" justify="center" align="center">
+            <Icon name="search" size={40} />
+            <Text c="text.1" mt="lg" fw="bold">{t`Didn't find anything`}</Text>
+          </Stack>
+        )}
       </Checkbox.Group>
     </Stack>
   );
