bf4b0539d771d65e52d0a505657b8da09e04ea9a
Nicolò Pretto <info@npretto.com>
2023-12-14 15:46:41 +0100

    rename component and refactor

--- frontend/src/metabase/admin/settings/components/widgets/EmbeddingSwitchWidget/EmbeddingSwitchWidget.tsx
+++ frontend/src/metabase/admin/settings/components/widgets/EmbeddingSwitchWidget/EmbeddingSwitchWidget.tsx
@@ -1,7 +1,6 @@
 import { t } from "ttag";
 import * as MetabaseAnalytics from "metabase/lib/analytics";
-import { Switch, Title } from "metabase/ui";
-import { Paragraph } from "./EmbeddingSwitchWidget.styled";
+import { Stack, Switch, Text } from "metabase/ui";
 
 interface EmbeddingSwitchWidgetProps {
   setting: {
@@ -14,13 +13,13 @@ interface EmbeddingSwitchWidgetProps {
 export const EmbeddingSwitchWidget = ({
   onChange,
 }: EmbeddingSwitchWidgetProps) => (
-  <div className="text-measure">
-    <Paragraph>
+  <Stack spacing={"md"} className="text-measure">
+    <Text lh={1.5}>
       {t`Embed dashboards, questions, or the entire Metabase app into your application. Integrate with your server code to create a secure environment, limited to specific users or organizations.`}
-    </Paragraph>
+    </Text>
     <Switch
       labelPosition="left"
-      label={<Title order={4}>{t`Embedding Enabled`}</Title>}
+      label={<strong>{t`Embedding Enabled`}</strong>}
       onChange={e => {
         onChange(e.currentTarget.checked);
         MetabaseAnalytics.trackStructEvent(
@@ -29,5 +28,5 @@ export const EmbeddingSwitchWidget = ({
         );
       }}
     />
-  </div>
+  </Stack>
 );
