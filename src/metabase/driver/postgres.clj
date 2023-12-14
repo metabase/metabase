2e1d44cae48d24638c12fbe9ec6116fb721340c7
Callum Herries <calherries@gmail.com>
2023-12-14 22:21:10 +0200

    Implement graceful failure handling for appends

--- src/metabase/driver/postgres.clj
+++ src/metabase/driver/postgres.clj
@@ -841,10 +841,11 @@
                                      :columns     (map keyword column-names)
                                      ::from-stdin "''"}
                                     :quoted true
-                                    :dialect (sql.qp/quote-style driver))]
-       ;; There's nothing magic about 100, but it felt good in testing. There could well be a better number.
-       (doseq [slice-of-values (partition-all 100 values)]
-         (let [tsvs (->> slice-of-values
+                                    :dialect (sql.qp/quote-style driver))
+           ;; There's nothing magic about 100, but it felt good in testing. There could well be a better number.
+           chunks (partition-all (or driver/*insert-chunk-rows* 100) values)]
+       (doseq [chunk chunks]
+         (let [tsvs (->> chunk
                          (map row->tsv)
                          (str/join "\n")
                          (StringReader.))]
