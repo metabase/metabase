5fac5eb04e2ccaf16938a205c0572c28ece4a532
Shaun Williams <shaun@metabase.com>
2023-12-15 15:11:07 -0600

    add reproducing e2e spec

---
+++ e2e/test/scenarios/admin/datamodel/reproductions/36422-show-create-metric-error.cy.spec.js
@@ -0,0 +1,24 @@
+import { popover, restore } from "e2e/support/helpers";
+
+describe("issue 36422", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("should show error and disable save button when create metric fails", () => {
+    cy.visit("/admin/datamodel/metrics");
+    cy.button("New metric").click();
+    selectTable("Orders");
+    cy.get('[name="name"]').type("x");
+    cy.get('[name="description"]').type("x");
+
+    cy.intercept("POST", "/api/metric", req => req.reply(400));
+    cy.button("Save changes").click();
+  });
+});
+
+function selectTable(tableName) {
+  cy.findByText("Select a table").click();
+  popover().findByText(tableName).click();
+}
