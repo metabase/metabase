ddf4c01293de52ab0873aaf72883bea5332181ba
Oleg Gromov <mail@oleggromov.com>
2023-12-20 21:34:19 +0000

    2

--- e2e/test/scenarios/question/notebook.cy.spec.js
+++ e2e/test/scenarios/question/notebook.cy.spec.js
@@ -179,7 +179,8 @@ describe("scenarios > question > notebook", { tags: "@slow" }, () => {
     // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
     cy.findByText("Custom Expression").click();
 
-    cy.get("@formula").clear().type("[Price] > 1 AND [Price] < 5").blur();
+    cy.get("@formula").clear().type("[Price] > 1 AND [Price] < 5");
+    cy.get("@formula").blur();
     cy.button("Done").click();
 
     getNotebookStep("filter")
