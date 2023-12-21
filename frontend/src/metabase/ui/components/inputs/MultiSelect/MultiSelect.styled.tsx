820e4b3c1b5ba05e77841f09341c10109cb2b66c
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-12-21 16:13:57 +0200

    Add styles

--- frontend/src/metabase/ui/components/inputs/MultiSelect/MultiSelect.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/MultiSelect/MultiSelect.styled.tsx
@@ -1,10 +1,19 @@
-import { getStylesRef, px, rem } from "@mantine/core";
+import { getSize, getStylesRef, rem } from "@mantine/core";
 import type {
   MantineThemeOverride,
   MultiSelectStylesParams,
 } from "@mantine/core";
 import { SelectDropdown } from "../Select/SelectDropdown";
 import { SelectItem } from "../Select/SelectItem";
+import {
+  getSelectInputOverrides,
+  getSelectItemsOverrides,
+} from "../Select/Select.styled";
+
+const SIZES = {
+  xs: rem(16),
+  md: rem(24),
+};
 
 export const getMultiSelectOverrides =
   (): MantineThemeOverride["components"] => ({
@@ -16,10 +25,14 @@ export const getMultiSelectOverrides =
         dropdownComponent: SelectDropdown,
         itemComponent: SelectItem,
       },
-      styles: (theme, { invalid }: MultiSelectStylesParams) => ({
-        // input
+      styles: (
+        theme,
+        { invalid }: MultiSelectStylesParams,
+        { size = "md" },
+      ) => ({
+        ...getSelectInputOverrides(theme),
+        ...getSelectItemsOverrides(theme, size),
         wrapper: {
-          position: "relative",
           "&:not(:only-child)": {
             marginTop: theme.spacing.xs,
           },
@@ -38,63 +51,10 @@ export const getMultiSelectOverrides =
         input: {
           ref: getStylesRef("input"),
         },
-        rightSection: {
-          svg: {
-            color: invalid ? theme.colors.error[0] : theme.colors.text[2],
-            width: "1rem !important",
-            height: "1rem !important",
-
-            "&[data-chevron] path": {
-              d: 'path("M 1.3781 4.1906 a 0.7031 0.7031 90 0 1 0.9938 0 L 7.5 9.3187 l 5.1281 -5.1281 a 0.7031 0.7031 90 1 1 0.9938 0.9938 l -5.625 5.625 a 0.7031 0.7031 90 0 1 -0.9938 0 l -5.625 -5.625 a 0.7031 0.7031 90 0 1 0 -0.9938 z")',
-            },
-            "&:not([data-chevron]) path": {
-              d: 'path("4.2469 3.2531 a 0.7031 0.7031 90 0 0 -0.9938 0.9938 L 6.5063 7.5 l -3.2531 3.2531 a 0.7031 0.7031 90 1 0 0.9938 0.9938 L 7.5 8.4938 l 3.2531 3.2531 a 0.7031 0.7031 90 1 0 0.9938 -0.9938 L 8.4938 7.5 l 3.2531 -3.2531 a 0.7031 0.7031 90 0 0 -0.9938 -0.9938 L 7.5 6.5063 L 4.2469 3.2531 z")',
-            },
-          },
-        },
-        // dropdown
-        itemsWrapper: {
-          padding: "0.75rem",
-        },
-        item: {
-          color: theme.colors.text[2],
-          fontSize: theme.fontSizes.md,
-          padding: theme.spacing.sm,
-          "&:hover:not([data-disabled]), &:focus": {
-            color: theme.colors.brand[1],
-            backgroundColor: theme.colors.brand[0],
-          },
-          "&[data-disabled]": {
-            color: theme.colors.text[0],
-          },
-        },
-        separator: {
-          padding: `0 ${theme.spacing.sm}`,
-
-          "&:not(:first-of-type)": {
-            "&::before": {
-              content: '""',
-              display: "block",
-              marginTop: rem(px(theme.spacing.sm) - 1),
-              marginBottom: theme.spacing.xs,
-              borderTop: `1px solid ${theme.colors.border[0]}`,
-            },
-          },
-        },
-        separatorLabel: {
-          color: theme.colors.text[0],
-          marginTop: "0 !important",
-          paddingTop: theme.spacing.xs,
-          paddingBottom: theme.spacing.xs,
-
-          "&::after": {
-            display: "none",
-          },
-        },
-        // values
         values: {
-          gap: theme.spacing.sm,
+          minHeight: getSize({ size, sizes: SIZES }),
           marginLeft: 0,
+          gap: theme.spacing.sm,
         },
         value: {
           margin: 0,
@@ -105,6 +65,7 @@ export const getMultiSelectOverrides =
           },
         },
         defaultValue: {
+          minHeight: getSize({ size, sizes: SIZES }),
           paddingLeft: theme.spacing.sm,
           paddingRight: theme.spacing.sm,
           fontWeight: "normal",
@@ -139,38 +100,6 @@ export const getMultiSelectOverrides =
           input: {
             paddingTop: rem(8),
             paddingBottom: rem(8),
-          },
-        }),
-      },
-      sizes: {
-        xs: theme => ({
-          values: {
-            minHeight: rem(16),
-          },
-          defaultValue: {
-            minHeight: rem(16),
-          },
-          item: {
-            fontSize: theme.fontSizes.sm,
-            lineHeight: theme.lineHeight,
-          },
-          separatorLabel: {
-            fontSize: theme.fontSizes.xs,
-          },
-        }),
-        md: theme => ({
-          values: {
-            minHeight: rem(24),
-          },
-          defaultValue: {
-            minHeight: rem(24),
-          },
-          item: {
-            fontSize: theme.fontSizes.md,
-            lineHeight: "1.5rem",
-          },
-          separatorLabel: {
-            fontSize: theme.fontSizes.sm,
           },
         }),
       },
--- frontend/src/metabase/ui/components/inputs/Select/Select.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Select/Select.styled.tsx
@@ -1,5 +1,9 @@
-import type { MantineThemeOverride } from "@mantine/core";
-import { px, rem, getSize } from "@mantine/core";
+import type {
+  MantineSize,
+  MantineTheme,
+  MantineThemeOverride,
+} from "@mantine/core";
+import { getSize, px, rem } from "@mantine/core";
 import { SelectDropdown } from "./SelectDropdown";
 import { SelectItem } from "./SelectItem";
 
@@ -13,82 +17,95 @@ export const getSelectOverrides = (): MantineThemeOverride["components"] => ({
       maxDropdownHeight: 512,
     },
     styles: (theme, _, { size = "md" }) => ({
-      label: {
-        color: theme.colors.text[1],
-        fontSize: getSize({ size, sizes: theme.fontSizes }),
-      },
-      rightSection: {
-        svg: {
-          color: theme.colors.text[2],
-          width: "1rem",
-          height: "1rem",
-
-          "&[data-chevron] path": {
-            d: 'path("M 1.3781 4.1906 a 0.7031 0.7031 90 0 1 0.9938 0 L 7.5 9.3187 l 5.1281 -5.1281 a 0.7031 0.7031 90 1 1 0.9938 0.9938 l -5.625 5.625 a 0.7031 0.7031 90 0 1 -0.9938 0 l -5.625 -5.625 a 0.7031 0.7031 90 0 1 0 -0.9938 z")',
-          },
-          "&:not([data-chevron]) path": {
-            d: 'path("4.2469 3.2531 a 0.7031 0.7031 90 0 0 -0.9938 0.9938 L 6.5063 7.5 l -3.2531 3.2531 a 0.7031 0.7031 90 1 0 0.9938 0.9938 L 7.5 8.4938 l 3.2531 3.2531 a 0.7031 0.7031 90 1 0 0.9938 -0.9938 L 8.4938 7.5 l 3.2531 -3.2531 a 0.7031 0.7031 90 0 0 -0.9938 -0.9938 L 7.5 6.5063 L 4.2469 3.2531 z")',
-          },
-        },
-      },
-      itemsWrapper: {
-        padding: "0.75rem",
-      },
-      item: {
-        color: theme.colors.text[2],
-        fontSize: theme.fontSizes.md,
-        padding: theme.spacing.sm,
-        "&:hover:not([data-disabled]), &:focus": {
-          color: theme.colors.brand[1],
-          backgroundColor: theme.colors.brand[0],
-        },
-        "&[data-disabled]": {
-          color: theme.colors.text[0],
-        },
-      },
-      separator: {
-        padding: `0 ${theme.spacing.sm}`,
-
-        "&:not(:first-of-type)": {
-          "&::before": {
-            content: '""',
-            display: "block",
-            marginTop: rem(px(theme.spacing.sm) - 1),
-            marginBottom: theme.spacing.xs,
-            borderTop: `1px solid ${theme.colors.border[0]}`,
-          },
-        },
-      },
-      separatorLabel: {
-        color: theme.colors.text[0],
-        marginTop: "0 !important",
-        paddingTop: theme.spacing.xs,
-        paddingBottom: theme.spacing.xs,
-
-        "&::after": {
-          display: "none",
+      ...getSelectInputOverrides(theme),
+      ...getSelectItemsOverrides(theme, size),
+      wrapper: {
+        "&:not(:only-child)": {
+          marginTop: theme.spacing.xs,
         },
       },
     }),
-    sizes: {
-      xs: theme => ({
-        item: {
-          fontSize: theme.fontSizes.sm,
-          lineHeight: theme.lineHeight,
-        },
-        separatorLabel: {
-          fontSize: theme.fontSizes.xs,
-        },
-      }),
-      md: theme => ({
-        item: {
-          fontSize: theme.fontSizes.md,
-          lineHeight: "1.5rem",
-        },
-        separatorLabel: {
-          fontSize: theme.fontSizes.sm,
-        },
-      }),
-    },
   },
 });
+
+export const getSelectInputOverrides = (theme: MantineTheme) => {
+  return {
+    rightSection: {
+      svg: {
+        color: theme.colors.text[2],
+        width: "1rem",
+        height: "1rem",
+
+        "&[data-chevron] path": {
+          d: 'path("M 1.3781 4.1906 a 0.7031 0.7031 90 0 1 0.9938 0 L 7.5 9.3187 l 5.1281 -5.1281 a 0.7031 0.7031 90 1 1 0.9938 0.9938 l -5.625 5.625 a 0.7031 0.7031 90 0 1 -0.9938 0 l -5.625 -5.625 a 0.7031 0.7031 90 0 1 0 -0.9938 z")',
+        },
+        "&:not([data-chevron]) path": {
+          d: 'path("4.2469 3.2531 a 0.7031 0.7031 90 0 0 -0.9938 0.9938 L 6.5063 7.5 l -3.2531 3.2531 a 0.7031 0.7031 90 1 0 0.9938 0.9938 L 7.5 8.4938 l 3.2531 3.2531 a 0.7031 0.7031 90 1 0 0.9938 -0.9938 L 8.4938 7.5 l 3.2531 -3.2531 a 0.7031 0.7031 90 0 0 -0.9938 -0.9938 L 7.5 6.5063 L 4.2469 3.2531 z")',
+        },
+      },
+    },
+  };
+};
+
+const ITEM_FONT_SIZES = {
+  xs: rem(12),
+  md: rem(14),
+};
+
+const SEPARATOR_FONT_SIZES = {
+  xs: rem(12),
+  md: rem(12),
+};
+
+const LINE_HEIGHTS = {
+  xs: rem(16),
+  md: rem(24),
+};
+
+export const getSelectItemsOverrides = (
+  theme: MantineTheme,
+  size: MantineSize | number,
+) => {
+  return {
+    itemsWrapper: {
+      padding: "0.75rem",
+    },
+    item: {
+      color: theme.colors.text[2],
+      fontSize: getSize({ size, sizes: ITEM_FONT_SIZES }),
+      lineHeight: getSize({ size, sizes: LINE_HEIGHTS }),
+      padding: theme.spacing.sm,
+      "&:hover:not([data-disabled]), &:focus": {
+        color: theme.colors.brand[1],
+        backgroundColor: theme.colors.brand[0],
+      },
+      "&[data-disabled]": {
+        color: theme.colors.text[0],
+      },
+    },
+    separator: {
+      padding: `0 ${theme.spacing.sm}`,
+
+      "&:not(:first-of-type)": {
+        "&::before": {
+          content: '""',
+          display: "block",
+          marginTop: rem(px(theme.spacing.sm) - 1),
+          marginBottom: theme.spacing.xs,
+          borderTop: `1px solid ${theme.colors.border[0]}`,
+        },
+      },
+    },
+    separatorLabel: {
+      color: theme.colors.text[0],
+      fontSize: getSize({ size, sizes: SEPARATOR_FONT_SIZES }),
+      marginTop: "0 !important",
+      paddingTop: theme.spacing.xs,
+      paddingBottom: theme.spacing.xs,
+
+      "&::after": {
+        display: "none",
+      },
+    },
+  };
+};
