a4bdded85ad65625e1769c6c401b4b7bd523acc1
Oleg Gromov <mail@oleggromov.com>
2023-12-21 16:49:01 +0000

    Fix e2e notebook / "should process the updated expression when pressing Enter" (#37045)

--- e2e/test/scenarios/question/notebook.cy.spec.js
+++ e2e/test/scenarios/question/notebook.cy.spec.js
@@ -179,7 +179,13 @@ describe("scenarios > question > notebook", { tags: "@slow" }, () => {
     // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
     cy.findByText("Custom Expression").click();
 
-    cy.get("@formula").clear().type("[Price] > 1 AND [Price] < 5{enter}");
+    cy.get("@formula")
+      .invoke("val", "") // this is a more reliable .clear()
+      .type("[Price] > 1 AND [Price] < 5{enter}");
+
+    // In case it does exist, it usually is an error in expression (caused by not clearing
+    // the input properly before typing), and this check helps to highlight that.
+    cy.findByTestId("expression-editor-textfield").should("not.exist");
 
     // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
     cy.contains(/^Price is less than 5/i);
