95737bd7d6009ec2f3076cd1a1c15ee1c5356192
Denis Berezin <denis.berezin@metabase.com>
2023-12-22 15:23:23 +0700

    Code review fixes

--- frontend/src/metabase/ui/components/inputs/SegmentedControl/SegmentedControl.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/SegmentedControl/SegmentedControl.styled.tsx
@@ -2,25 +2,7 @@ import type {
   MantineThemeOverride,
   SegmentedControlStylesParams,
 } from "@mantine/core";
-import { getSize, rem } from "@mantine/core";
-
-const OPTION_PADDING = {
-  xs: rem(8),
-  sm: rem(8),
-  md: rem(8),
-};
-
-const OPTION_PADDING_FULL_WIDTH_V = {
-  xs: rem(8),
-  sm: rem(8),
-  md: rem(8),
-};
-
-const OPTION_PADDING_FULL_WIDTH_H = {
-  xs: rem(16),
-  sm: rem(16),
-  md: rem(16),
-};
+import { rem } from "@mantine/core";
 
 export const getSegmentedControlOverrides =
   (): MantineThemeOverride["components"] => ({
@@ -29,19 +11,10 @@ export const getSegmentedControlOverrides =
         size: "md",
         radius: rem(4),
       },
-      styles: (
-        theme,
-        { fullWidth }: SegmentedControlStylesParams,
-        { size = "md" },
-      ) => {
+      styles: (theme, { fullWidth }: SegmentedControlStylesParams) => {
         return {
           label: {
-            padding: fullWidth
-              ? `${getSize({
-                  size,
-                  sizes: OPTION_PADDING_FULL_WIDTH_V,
-                })} ${getSize({ size, sizes: OPTION_PADDING_FULL_WIDTH_H })}`
-              : getSize({ size, sizes: OPTION_PADDING }),
+            padding: fullWidth ? `${rem(8)} ${rem(16)}` : rem(8),
             fontWeight: "normal",
             lineHeight: "1rem",
             "&[data-active]": {
