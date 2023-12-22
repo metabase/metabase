e57a73a60a1e4e2f6970cf4a32e6888c18f645b8
Emmad Usmani <emmadusmani@berkeley.edu>
2023-12-21 16:34:12 -0800

    create static scatter plot with echarts (#36317)
    * create static scatter plot with echarts
    
    * add storybook examples
    
    * add multi metric series story
    
    * separate dataset computation
    
    * remove hacky default x-axis scale calculation
    
    * remove comment
    
    * improve file structure
    
    * styles
    
    * remove unused `settings` param from series computation
    
    * remove unused export
    
    * remove `scatter2`
    
    * remove redundant animation disable
    
    * remove extra comment

---
+++ frontend/src/metabase/static-viz/components/ScatterPlot/ScatterPlot.stories.tsx
@@ -0,0 +1,78 @@
+import type { ComponentStory } from "@storybook/react";
+import { color } from "metabase/lib/colors";
+import { formatStaticValue } from "metabase/static-viz/lib/format";
+import { measureTextWidth } from "metabase/static-viz/lib/text";
+import type { RenderingContext } from "metabase/visualizations/types";
+
+import { ScatterPlot } from "./ScatterPlot";
+import { data } from "./stories-data";
+
+export default {
+  title: "static-viz/ScatterPlot",
+  component: ScatterPlot,
+};
+
+const Template: ComponentStory<typeof ScatterPlot> = args => {
+  return (
+    <div style={{ border: "1px solid black", display: "inline-block" }}>
+      <ScatterPlot {...args} />
+    </div>
+  );
+};
+
+const renderingContext: RenderingContext = {
+  getColor: color,
+  formatValue: formatStaticValue as any,
+  measureText: (text, style) =>
+    measureTextWidth(text, style.size, style.weight),
+  fontFamily: "Lato",
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  rawSeries: data.default as any,
+  dashcardSettings: {},
+  renderingContext,
+};
+
+export const MultiMetricSeries = Template.bind({});
+MultiMetricSeries.args = {
+  rawSeries: data.multiMetricSeries as any,
+  dashcardSettings: {},
+  renderingContext,
+};
+
+export const PowerXScale = Template.bind({});
+PowerXScale.args = {
+  rawSeries: data.powerXScale as any,
+  dashcardSettings: {},
+  renderingContext,
+};
+
+export const LogXScale = Template.bind({});
+LogXScale.args = {
+  rawSeries: data.logXScale as any,
+  dashcardSettings: {},
+  renderingContext,
+};
+
+export const HistogramXScale = Template.bind({});
+HistogramXScale.args = {
+  rawSeries: data.histogramXScale as any,
+  dashcardSettings: {},
+  renderingContext,
+};
+
+export const OrdinalXScale = Template.bind({});
+OrdinalXScale.args = {
+  rawSeries: data.ordinalXScale as any,
+  dashcardSettings: {},
+  renderingContext,
+};
+
+export const TimeseriesXScale = Template.bind({});
+TimeseriesXScale.args = {
+  rawSeries: data.timeseriesXScale as any,
+  dashcardSettings: {},
+  renderingContext,
+};
