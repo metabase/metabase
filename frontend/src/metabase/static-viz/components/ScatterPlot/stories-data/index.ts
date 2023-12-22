ada365517e4d798b272693e72e8afe46b041da33
Emmad Usmani <emmadusmani@berkeley.edu>
2023-12-21 18:03:06 -0800

    scatter add pow x-scale multi-series story (#36884)

--- frontend/src/metabase/static-viz/components/ScatterPlot/stories-data/index.ts
+++ frontend/src/metabase/static-viz/components/ScatterPlot/stories-data/index.ts
@@ -3,6 +3,7 @@ import multiMetricSeries from "./multi-metric-series.json";
 import multiDimensionBreakout from "./multi-dimension-breakout.json";
 import bubbleSize from "./bubble-size.json";
 import powerXScale from "./power-x-scale.json";
+import powerXScaleMultiSeries from "./power-x-scale-multi-series.json";
 import logXScale from "./log-x-scale.json";
 import logXScaleAtOne from "./log-x-scale-at-one.json";
 import histogramXScale from "./histogram-x-scale.json";
@@ -17,6 +18,7 @@ export const data = {
   multiDimensionBreakout,
   bubbleSize,
   powerXScale,
+  powerXScaleMultiSeries,
   logXScale,
   logXScaleAtOne,
   histogramXScale,
