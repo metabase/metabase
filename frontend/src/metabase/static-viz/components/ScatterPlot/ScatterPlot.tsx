342d253586ab62f67630a3e68369c877d9586545
Emmad Usmani <emmadusmani@berkeley.edu>
2023-12-21 16:34:12 -0800

    create static scatter plot with echarts (#36317)
    * create static scatter plot with echarts
    
    * add storybook examples
    
    * add multi metric series story
    
    * separate dataset computation
    
    * remove hacky default x-axis scale calculation
    
    * remove comment
    
    * improve file structure
    
    * styles
    
    * remove unused `settings` param from series computation
    
    * remove unused export
    
    * remove `scatter2`
    
    * remove redundant animation disable
    
    * remove extra comment

---
+++ frontend/src/metabase/static-viz/components/ScatterPlot/ScatterPlot.tsx
@@ -0,0 +1,60 @@
+import { init } from "echarts";
+import { Group } from "@visx/group";
+
+import { getCartesianChartModel } from "metabase/visualizations/echarts/cartesian/model";
+import { getCartesianChartOption } from "metabase/visualizations/echarts/cartesian/option";
+import { sanitizeSvgForBatik } from "metabase/static-viz/lib/svg";
+import type { IsomorphicStaticChartProps } from "metabase/static-viz/containers/IsomorphicStaticChart/types";
+import { getLegendItems } from "metabase/visualizations/echarts/cartesian/model/legend";
+
+import { calculateLegendRows } from "../Legend/utils";
+import { Legend } from "../Legend";
+import { computeStaticComboChartSettings } from "../ComboChart/settings";
+
+const WIDTH = 540;
+const HEIGHT = 360;
+const LEGEND_PADDING = 8;
+
+export function ScatterPlot({
+  rawSeries,
+  dashcardSettings,
+  renderingContext,
+  width = WIDTH,
+  height = HEIGHT,
+}: IsomorphicStaticChartProps) {
+  const chart = init(null, null, { renderer: "svg", ssr: true, width, height });
+
+  const computedVisualizationSettings = computeStaticComboChartSettings(
+    rawSeries,
+    dashcardSettings,
+    renderingContext,
+  );
+
+  const chartModel = getCartesianChartModel(
+    rawSeries,
+    computedVisualizationSettings,
+    renderingContext,
+  );
+
+  const legendItems = getLegendItems(chartModel);
+  const { height: legendHeight, items: legendLayoutItems } =
+    calculateLegendRows(legendItems, width, LEGEND_PADDING, LEGEND_PADDING);
+
+  const option = getCartesianChartOption(
+    chartModel,
+    computedVisualizationSettings,
+    renderingContext,
+  );
+  chart.setOption(option);
+
+  const chartSvg = sanitizeSvgForBatik(chart.renderToSVGString());
+
+  return (
+    <svg width={width} height={height + legendHeight}>
+      <Legend items={legendLayoutItems} />
+      <Group top={legendHeight}>
+        <g dangerouslySetInnerHTML={{ __html: chartSvg }}></g>
+      </Group>
+    </svg>
+  );
+}
