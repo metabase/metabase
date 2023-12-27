1803cfa1e24a593b8b5940987841c1503e1b2065
Mahmuda-Tani <mahmudatani4709@gmail.com>
2023-12-27 19:56:52 +0600

    parseJWTPayload    function fixed

--- frontend/src/metabase/lib/click-behavior.js
+++ frontend/src/metabase/lib/click-behavior.js
@@ -85,7 +85,7 @@ function getTokenParameter(dashboard) {
 
 function parseJWTPayload(token) {
   const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace('-', '+').replace('_', '/');
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   return JSON.parse(atob(base64));
 }
 
