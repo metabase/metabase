65376ee29df186d571023ff15f7e4d7af94f04e0
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-12-28 18:22:29 +0200

    Add tests

--- frontend/src/metabase/querying/components/FilterModal/StringFilterEditor/StringFilterEditor.unit.spec.tsx
+++ frontend/src/metabase/querying/components/FilterModal/StringFilterEditor/StringFilterEditor.unit.spec.tsx
@@ -1,7 +1,13 @@
 import userEvent from "@testing-library/user-event";
-import { PRODUCT_CATEGORY_VALUES } from "metabase-types/api/mocks/presets";
-import { renderWithProviders, screen } from "__support__/ui";
-import { setupFieldValuesEndpoints } from "__support__/server-mocks";
+import {
+  PEOPLE,
+  PRODUCT_CATEGORY_VALUES,
+} from "metabase-types/api/mocks/presets";
+import { act, renderWithProviders, screen } from "__support__/ui";
+import {
+  setupFieldSearchValuesEndpoints,
+  setupFieldValuesEndpoints,
+} from "__support__/server-mocks";
 import * as Lib from "metabase-lib";
 import { columnFinder, createQuery } from "metabase-lib/test-helpers";
 import { StringFilterEditor } from "./StringFilterEditor";
@@ -46,36 +52,124 @@ describe("StringFilterEditor", () => {
   const findColumn = columnFinder(query, availableColumns);
   const column = findColumn("PRODUCTS", "CATEGORY");
 
-  it("should allow to pick list values", async () => {
-    const { getNextFilter } = setup({
-      query,
-      stageIndex,
-      column,
-    });
-
-    userEvent.click(await screen.findByText("Gadget"));
-
-    const filter = getNextFilter();
-    expect(Lib.displayInfo(query, stageIndex, filter)).toMatchObject({
-      displayName: "Category is Gadget",
-    });
+  beforeAll(() => {
+    jest.useFakeTimers({ advanceTimers: true });
   });
 
-  it("should add a filter with one value", async () => {
-    const { getNextFilter } = setup({
-      query,
-      stageIndex,
-      column,
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  describe("new filter", () => {
+    it("should handle list values", async () => {
+      const { getNextFilter, onInput } = setup({
+        query,
+        stageIndex,
+        column,
+      });
+
+      userEvent.click(await screen.findByText("Gadget"));
+
+      const filter = getNextFilter();
+      expect(Lib.displayInfo(query, stageIndex, filter)).toMatchObject({
+        displayName: "Category is Gadget",
+      });
+      expect(onInput).not.toHaveBeenCalled();
     });
 
-    userEvent.click(screen.getByText("is"));
-    userEvent.click(await screen.findByText("Starts with"));
-    userEvent.type(screen.getByPlaceholderText("Enter some text"), "Ga");
-    userEvent.click(document.body);
+    it("should handle searchable values", async () => {
+      const searchValue = "t";
+      const searchResult = "t@metabase.test";
+      setupFieldSearchValuesEndpoints(PEOPLE.EMAIL, searchValue, [
+        [searchResult],
+      ]);
 
-    const filter = getNextFilter();
-    expect(Lib.displayInfo(query, stageIndex, filter)).toMatchObject({
-      displayName: "Category starts with Ga",
+      const { getNextFilter, onInput } = setup({
+        query,
+        stageIndex,
+        column: findColumn("PEOPLE", "EMAIL"),
+      });
+
+      userEvent.type(
+        screen.getByPlaceholderText("Search by Email"),
+        searchValue,
+      );
+      act(() => jest.advanceTimersByTime(1000));
+      userEvent.click(await screen.findByText(searchResult));
+
+      const filter = getNextFilter();
+      expect(Lib.displayInfo(query, stageIndex, filter)).toMatchObject({
+        displayName: `Email is ${searchResult}`,
+      });
+      expect(onInput).not.toHaveBeenCalled();
+    });
+
+    it("should handle non-searchable values", async () => {
+      const { getNextFilter, onInput } = setup({
+        query,
+        stageIndex,
+        column: findColumn("PEOPLE", "PASSWORD"),
+      });
+
+      userEvent.type(screen.getByPlaceholderText("Enter some text"), "Test");
+      userEvent.click(document.body);
+
+      const filter = getNextFilter();
+      expect(Lib.displayInfo(query, stageIndex, filter)).toMatchObject({
+        displayName: `Password is Test`,
+      });
+      expect(onInput).toHaveBeenCalled();
+    });
+
+    it("should add a filter with one value", async () => {
+      const { getNextFilter, onInput } = setup({
+        query,
+        stageIndex,
+        column,
+      });
+
+      userEvent.click(screen.getByText("is"));
+      userEvent.click(await screen.findByText("Starts with"));
+      userEvent.type(screen.getByPlaceholderText("Enter some text"), "Ga");
+      userEvent.click(document.body);
+
+      const filter = getNextFilter();
+      expect(Lib.displayInfo(query, stageIndex, filter)).toMatchObject({
+        displayName: "Category starts with Ga",
+      });
+      expect(onInput).toHaveBeenCalled();
+    });
+
+    it("should add a filter with no value", async () => {
+      const { getNextFilter } = setup({
+        query,
+        stageIndex,
+        column,
+      });
+
+      userEvent.click(screen.getByText("is"));
+      userEvent.click(await screen.findByText("Is empty"));
+
+      const filter = getNextFilter();
+      expect(Lib.displayInfo(query, stageIndex, filter)).toMatchObject({
+        displayName: "Category is empty",
+      });
+    });
+
+    it("should not accept an empty string as a value", async () => {
+      const { getNextFilter } = setup({
+        query,
+        stageIndex,
+        column,
+      });
+
+      userEvent.click(screen.getByText("is"));
+      userEvent.click(await screen.findByText("Starts with"));
+      expect(getNextFilter()).toBeUndefined();
+
+      userEvent.type(screen.getByPlaceholderText("Enter some text"), "Ga");
+      userEvent.clear(screen.getByPlaceholderText("Enter some text"));
+      expect(getNextFilter()).toBeUndefined();
     });
   });
 });
