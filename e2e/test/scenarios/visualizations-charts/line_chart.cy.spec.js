3043e8ed41f03a49f1dcb2073a23bd159d3119c4
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2023-12-29 16:37:36 +0300

    Revert "Do not set viewport"
    This reverts commit b41a039aa85e98b88aa9dba208c304d7f7680979.

--- e2e/test/scenarios/visualizations-charts/line_chart.cy.spec.js
+++ e2e/test/scenarios/visualizations-charts/line_chart.cy.spec.js
@@ -580,6 +580,8 @@ describe("scenarios > visualizations > line chart", () => {
 
   describe("brush filters", () => {
     it("should apply filters to the series selecting area range", () => {
+      cy.viewport(1280, 800);
+
       visitQuestionAdhoc({
         dataset_query: testQuery,
         display: "line",
