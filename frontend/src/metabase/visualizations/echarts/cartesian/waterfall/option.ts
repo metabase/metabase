ea978febb9e28ed30b8fed427566016ce45b2fe3
Emmad Usmani <emmadusmani@berkeley.edu>
2024-01-03 15:49:02 -0800

    add new args for getCartesianChartOption [ci skip]

--- frontend/src/metabase/visualizations/echarts/cartesian/waterfall/option.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/waterfall/option.ts
@@ -4,19 +4,27 @@ import type {
   ComputedVisualizationSettings,
   RenderingContext,
 } from "metabase/visualizations/types";
+import type { TimelineEventId } from "metabase-types/api";
 
 import type { CartesianChartModel } from "../model/types";
 import { getCartesianChartOption } from "../option";
+import type { TimelineEventsModel } from "../timeline-events/types";
 import { DATASET_DIMENSIONS } from "./constants";
 
 export function getWaterfallOption(
   chartModel: CartesianChartModel,
+  timelineEventsModel: TimelineEventsModel | null,
+  selectedTimelineEventsIds: TimelineEventId[],
   settings: ComputedVisualizationSettings,
+  isAnimated: boolean,
   renderingContext: RenderingContext,
 ): EChartsOption {
   const option = getCartesianChartOption(
     chartModel,
+    timelineEventsModel,
+    selectedTimelineEventsIds,
     settings,
+    isAnimated,
     renderingContext,
   );
 
