9f98477f707257e7d87b2c48356700a003d99462
Emmad Usmani <emmadusmani@berkeley.edu>
2024-01-03 15:47:37 -0800

    add new params to getCartesianChartOption [ci skip]

--- frontend/src/metabase/static-viz/components/WaterfallChart/WaterfallChart.tsx
+++ frontend/src/metabase/static-viz/components/WaterfallChart/WaterfallChart.tsx
@@ -29,7 +29,10 @@ export function WaterfallChart({
   );
   const option = getCartesianChartOption(
     chartModel,
+    null,
+    [],
     computedVisualizationSettings,
+    false,
     renderingContext,
   );
 
