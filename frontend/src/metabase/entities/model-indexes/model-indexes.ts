3d9940266e7316116abb7ef72b5bc5a19068d2f1
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-03 20:57:51 -0500

    When creating a new model, don't call /api/model-indexes because that returns 404

--- frontend/src/metabase/entities/model-indexes/model-indexes.ts
+++ frontend/src/metabase/entities/model-indexes/model-indexes.ts
@@ -20,6 +20,9 @@ export const ModelIndexes = createEntity({
   schema: ModelIndexSchema,
   api: {
     ...ModelIndexApi,
+    list: ({ model_id }: { model_id?: string | null }) =>
+      model_id ? ModelIndexApi.list({ model_id }) : null,
+    // TODO: Write unit tests for the case where there should be an api call and the case where there shouldn't be an api call
   },
   actions,
   utils,
