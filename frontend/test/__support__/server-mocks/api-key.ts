5b1504158804770171c665b1583d8e87604b813f
Ryan Laurie <iethree@gmail.com>
2024-01-05 08:03:34 -0700

    mock api key endpoints in settingsEditor unit tests

--- frontend/test/__support__/server-mocks/api-key.ts
+++ frontend/test/__support__/server-mocks/api-key.ts
@@ -3,5 +3,6 @@ import type { ApiKey } from "metabase-types/api";
 
 export function setupApiKeyEndpoints(apiKeys: ApiKey[]) {
   fetchMock.get("path:/api/api-key", apiKeys);
+  fetchMock.get("path:/api/api-key/count", apiKeys.length);
   fetchMock.post("path:/api/api-key", 200);
 }
