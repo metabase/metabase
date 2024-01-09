d7f2dd9e64ae9a28824db7dca2ccf98c10c718e5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-01-09 21:21:27 +0700

    Hide Metabase link in `FormCreator`

--- frontend/src/metabase/actions/containers/ActionCreator/FormCreator/Description/Description.tsx
+++ frontend/src/metabase/actions/containers/ActionCreator/FormCreator/Description/Description.tsx
@@ -1,18 +1,28 @@
 import { jt, t } from "ttag";
 import ExternalLink from "metabase/core/components/ExternalLink";
-import MetabaseSettings from "metabase/lib/settings";
+import { useSelector } from "metabase/lib/redux";
+import { getDocsUrl } from "metabase/selectors/settings";
+import { getShowMetabaseLinks } from "metabase/selectors/whitelabel";
 import { InfoText } from "./Description.styled";
 
 export function Description() {
-  const docsLink = (
-    <ExternalLink
-      key="learn-more"
-      href={MetabaseSettings.docsUrl("actions/custom")}
-    >{t`Learn more`}</ExternalLink>
+  const docsLink = useSelector(state =>
+    getDocsUrl(state, { page: "actions/custom" }),
   );
+  const showMetabaseLinks = useSelector(getShowMetabaseLinks);
+
   return (
     <InfoText>
-      {jt`Configure your parameters' types and properties here. The values for these parameters can come from user input, or from a dashboard filter. ${docsLink}`}
+      {jt`Configure your parameters' types and properties here. The values for these parameters can come from user input, or from a dashboard filter.`}
+      {showMetabaseLinks && (
+        <>
+          {" "}
+          <ExternalLink
+            key="learn-more"
+            href={docsLink}
+          >{t`Learn more`}</ExternalLink>
+        </>
+      )}
     </InfoText>
   );
 }
