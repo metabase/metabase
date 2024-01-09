f83f88a38f8012039cf287a2397b32afe847cc7c
Ngoc Khuat <qn.khuat@gmail.com>
2024-01-09 19:14:52 +0700

    Unify type of timestamp columns (#37160)

--- enterprise/backend/test/metabase_enterprise/serialization/v2/extract_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/v2/extract_test.clj
@@ -31,7 +31,7 @@
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2])
   (:import
-   (java.time LocalDateTime OffsetDateTime)))
+   (java.time OffsetDateTime)))
 
 (defn- by-model [model-name extraction]
   (->> extraction
@@ -248,14 +248,14 @@
 
                        Card       {c4-id  :id
                                    c4-eid :entity_id}        {:name          "Referenced Question"
-                                   :database_id   db-id
-                                   :table_id      schema-id
-                                   :collection_id coll-id
-                                   :creator_id    mark-id
-                                   :dataset_query
-                                   {:query {:source-table no-schema-id
-                                            :filter [:>= [:field field-id nil] 18]}
-                                    :database db-id}}
+                                                              :database_id   db-id
+                                                              :table_id      schema-id
+                                                              :collection_id coll-id
+                                                              :creator_id    mark-id
+                                                              :dataset_query
+                                                              {:query {:source-table no-schema-id
+                                                                       :filter [:>= [:field field-id nil] 18]}
+                                                               :database db-id}}
                        Card
                        {c5-id  :id
                         c5-eid :entity_id}
@@ -353,7 +353,7 @@
                                                          :filter [:>= [:field ["My Database" nil "Schemaless Table" "Some Field"] nil] 18]
                                                          :aggregation [[:count]]}
                                                  :database "My Database"}
-                   :created_at                  LocalDateTime}
+                   :created_at                  OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
@@ -377,7 +377,7 @@
                                          :card_id      c1-eid
                                          :target [:dimension [:field ["My Database" nil "Schemaless Table" "Some Field"]
                                                               {:source-field ["My Database" "PUBLIC" "Schema'd Table" "Other Field"]}]]}]
-                   :created_at         LocalDateTime}
+                   :created_at         OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
@@ -424,7 +424,7 @@
                       :enabled true}]
                     :column_settings
                     {"[\"ref\",[\"field\",[\"My Database\",\"PUBLIC\",\"Schema'd Table\",\"Other Field\"],null]]" {:column_title "Locus"}}}
-                   :created_at    LocalDateTime}
+                   :created_at    OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
@@ -452,7 +452,7 @@
                    :dataset_query  {:query    {:source-table c4-eid
                                                :aggregation [[:count]]}
                                     :database "My Database"}
-                   :created_at     LocalDateTime}
+                   :created_at     OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
@@ -487,9 +487,9 @@
                                                 :enabled true}]
                                               :column_settings
                                               {"[\"ref\",[\"field\",[\"My Database\",\"PUBLIC\",\"Schema'd Table\",\"Other Field\"],null]]" {:column_title "Locus"}}}
-                     :created_at             LocalDateTime}
+                     :created_at             OffsetDateTime}
                     {:action_id action-eid}]
-                   :created_at             LocalDateTime}
+                   :created_at             OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
@@ -640,7 +640,7 @@
                                           {:model "Field", :id "email"}]]]
                        [:dimensions  [:sequential
                                       [:map
-                                       [:created_at (ms/InstanceOfClass LocalDateTime)]
+                                       [:created_at (ms/InstanceOfClass OffsetDateTime)]
                                        [:human_readable_field_id {:optional true} [:maybe [:sequential [:maybe :string]]]]]]]]
                       ser))
           (is (not (contains? ser :id)))
@@ -666,13 +666,13 @@
                        [:dimensions         [:sequential
                                              [:map
                                               [:human_readable_field_id [:maybe [:sequential [:maybe :string]]]]
-                                              [:created_at              (ms/InstanceOfClass LocalDateTime)]]]]]
+                                              [:created_at              (ms/InstanceOfClass OffsetDateTime)]]]]]
                       ser))
           (is (not (contains? ser :id)))
 
           (testing "dimensions are properly inlined"
             (is (=? [{:human_readable_field_id ["My Database" "PUBLIC" "Customers" "name"]
-                      :created_at              LocalDateTime}]
+                      :created_at              OffsetDateTime}]
                     (:dimensions ser))))
 
           (testing "which depend on the Table and both real and human-readable foreign Fields"
@@ -716,7 +716,7 @@
                    :creator_id  "ann@heart.band"
                    :definition  {:source-table ["My Database" nil "Schemaless Table"]
                                  :aggregation [[:sum [:field ["My Database" nil "Schemaless Table" "Some Field"] nil]]]}
-                   :created_at  LocalDateTime}
+                   :created_at  OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
@@ -881,7 +881,7 @@
                                  :filter       [:< [:field ["My Database" nil
                                                             "Schemaless Table" "Some Field"]
                                                     nil] 18]}
-                   :created_at  LocalDateTime}
+                   :created_at  OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
@@ -1041,7 +1041,7 @@
                                  {:model "Table"    :id "Schemaless Table"}
                                  {:model "Field"    :id "Some Field"}
                                  {:model "FieldValues" :id "0"}] ; Always 0.
-                   :created_at  LocalDateTime
+                   :created_at  OffsetDateTime
                    :values      values}
                   ser))
           (is (not (contains? ser :id)))
@@ -1114,7 +1114,7 @@
                                                               :id    p-none-eid
                                                               :label "pulse_w_o_collection_or_dashboard"}]]]
                        [:creator_id                     [:= "ann@heart.band"]]
-                       [:created_at                     (ms/InstanceOfClass LocalDateTime)]
+                       [:created_at                     (ms/InstanceOfClass OffsetDateTime)]
                        [:dashboard_id {:optional true}  :nil]
                        [:collection_id {:optional true} :nil]]
                       ser))
@@ -1132,7 +1132,7 @@
                                              :label "pulse_with_only_collection"}]]]
                        [:creator_id    [:= "ann@heart.band"]]
                        [:collection_id [:= coll-eid]]
-                       [:created_at    (ms/InstanceOfClass LocalDateTime)]
+                       [:created_at    (ms/InstanceOfClass OffsetDateTime)]
                        [:dashboard_id {:optional true} :nil]]
                       ser))
           (is (not (contains? ser :id)))
@@ -1149,7 +1149,7 @@
                                             :label "pulse_with_only_dashboard"}]]]
                        [:creator_id   [:= "ann@heart.band"]]
                        [:dashboard_id [:= dash-eid]]
-                       [:created_at   (ms/InstanceOfClass LocalDateTime)]
+                       [:created_at   (ms/InstanceOfClass OffsetDateTime)]
                        [:collection_id {:optional true} :nil]]
                       ser))
           (is (not (contains? ser :id)))
@@ -1166,7 +1166,7 @@
                    :creator_id    "ann@heart.band"
                    :dashboard_id  dash-eid
                    :collection_id coll-eid
-                   :created_at    LocalDateTime}
+                   :created_at    OffsetDateTime}
                   ser))
           (is (not (contains? ser :id)))
 
