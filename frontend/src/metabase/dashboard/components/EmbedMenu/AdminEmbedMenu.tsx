190a888db1686bcb26be69e40257a7952b81ba74
Oisin Coveney <oisin@metabase.com>
2024-01-08 17:06:40 -0800

    Remove disabled behavior for the embed menu

--- frontend/src/metabase/dashboard/components/EmbedMenu/AdminEmbedMenu.tsx
+++ frontend/src/metabase/dashboard/components/EmbedMenu/AdminEmbedMenu.tsx
@@ -9,9 +9,7 @@ import {
   DashboardPublicLinkPopover,
   QuestionPublicLinkPopover,
 } from "metabase/dashboard/components/PublicLinkPopover";
-import { useSelector } from "metabase/lib/redux";
-import { getSetting } from "metabase/selectors/settings";
-import { Menu, Title, Text, Stack, Center } from "metabase/ui";
+import { Menu, Title, Center } from "metabase/ui";
 import { Icon } from "metabase/core/components/Icon";
 import { AdminEmbedMenuContainer } from "./AdminEmbedMenu.styled";
 
@@ -22,13 +20,6 @@ export const AdminEmbedMenu = ({
   onModalOpen,
 }: EmbedMenuProps) => {
   const [menuMode, setMenuMode] = useState<EmbedMenuModes>(null);
-
-  const isPublicSharingEnabled = useSelector(state =>
-    getSetting(state, "enable-public-sharing"),
-  );
-  const isEmbeddingEnabled = useSelector(state =>
-    getSetting(state, "enable-embedding"),
-  );
 
   const target = (
     <DashboardEmbedHeaderButton hasBackground={resourceType === "dashboard"} />
@@ -59,27 +50,17 @@ export const AdminEmbedMenu = ({
       <AdminEmbedMenuContainer w="13.75rem" data-testid="embed-header-menu">
         <Menu.Item
           data-testid="embed-menu-public-link-item"
-          py={isPublicSharingEnabled ? "md" : "sm"}
+          py="md"
           icon={
             <Center mr="xs">
               <Icon name="link" />
             </Center>
           }
-          disabled={!isPublicSharingEnabled}
           onClick={() => setMenuMode("public-link-popover")}
         >
-          {isPublicSharingEnabled ? (
-            <Title c="inherit" order={4}>
-              {hasPublicLink ? t`Public link` : t`Create a public link`}
-            </Title>
-          ) : (
-            <Stack spacing="xs">
-              <Title c="inherit" order={4}>
-                {t`Public links are off`}
-              </Title>
-              <Text size="sm" c="inherit">{t`Enable them in settings`}</Text>
-            </Stack>
-          )}
+          <Title c="inherit" order={4}>
+            {hasPublicLink ? t`Public link` : t`Create a public link`}
+          </Title>
         </Menu.Item>
 
         <Menu.Item
@@ -90,21 +71,11 @@ export const AdminEmbedMenu = ({
               <Icon name="embed" />
             </Center>
           }
-          disabled={!isEmbeddingEnabled}
           onClick={onModalOpen}
         >
-          {isEmbeddingEnabled ? (
-            <Title c="inherit" order={4}>
-              {t`Embed`}
-            </Title>
-          ) : (
-            <Stack spacing="xs">
-              <Title c="inherit" order={4}>
-                {t`Embedding is off`}
-              </Title>
-              <Text size="sm" c="inherit">{t`Enable it in settings`}</Text>
-            </Stack>
-          )}
+          <Title c="inherit" order={4}>
+            {t`Embed`}
+          </Title>
         </Menu.Item>
       </AdminEmbedMenuContainer>
     </Menu>
