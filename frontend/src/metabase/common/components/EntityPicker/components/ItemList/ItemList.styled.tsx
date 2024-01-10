b406e8ccaf85aea52d82324f0173a3352c7991ac
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-01-10 17:49:58 -0400

    virtualized lists

--- frontend/src/metabase/common/components/EntityPicker/components/ItemList/ItemList.styled.tsx
+++ frontend/src/metabase/common/components/EntityPicker/components/ItemList/ItemList.styled.tsx
@@ -4,4 +4,5 @@ import { Box } from "metabase/ui";
 export const PickerColumn = styled(Box)`
   flex-basis: 310px;
   padding: 1rem 1rem 1rem 1.5rem;
+  height: 100%;
 `;
--- frontend/src/metabase/common/components/EntityPicker/components/ItemList/ItemList.tsx
+++ frontend/src/metabase/common/components/EntityPicker/components/ItemList/ItemList.tsx
@@ -1,4 +1,10 @@
-import { Text, Box, ScrollArea, NavLink } from "metabase/ui";
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from "react-virtualized";
+import { Text, Box, NavLink } from "metabase/ui";
 import { Icon } from "metabase/core/components/Icon";
 import type { SearchResult } from "metabase-types/api";
 import { getIcon, isSelectedItem } from "../../utils";
@@ -15,6 +21,11 @@ export const ItemList = ({
   selectedItem: SearchResult | null;
   folderModel: string;
 }) => {
+  const cache = new CellMeasurerCache({
+    defaultHeight: 40,
+    fixedWidth: true,
+  });
+
   if (!items) {
     return null;
   }
@@ -30,32 +41,57 @@ export const ItemList = ({
   }
 
   return (
-    <ScrollArea h="100%">
-      <PickerColumn>
-        {items.map(item => {
-          const isFolder = folderModel.includes(item.model);
-          const isSelected = isSelectedItem(item, selectedItem);
-          return (
-            <div key={item.model + item.id}>
-              <NavLink
-                rightSection={
-                  isFolder ? <Icon name="chevronright" size={10} /> : null
-                }
-                label={item.name}
-                active={isSelected}
-                icon={<Icon name={isFolder ? "folder" : getIcon(item)} />}
-                onClick={e => {
-                  e.preventDefault(); // prevent form submission
-                  e.stopPropagation(); // prevent parent onClick
-                  onClick(item);
-                }}
-                variant="light"
-                mb="xs"
-              />
-            </div>
-          );
-        })}
-      </PickerColumn>
-    </ScrollArea>
+    <PickerColumn>
+      <AutoSizer>
+        {({ height, width }) => (
+          <List
+            height={height}
+            width={width}
+            rowHeight={cache.rowHeight}
+            items={items}
+            rowCount={items.length}
+            rowRenderer={({ index, key, style, parent }) => {
+              const item = items[index];
+              const isFolder = folderModel.includes(item.model);
+              const isSelected = isSelectedItem(item, selectedItem);
+              return (
+                <CellMeasurer
+                  key={key}
+                  cache={cache}
+                  columnCount={1}
+                  columnIndex={0}
+                  parent={parent}
+                  rowIndex={index}
+                >
+                  {({ registerChild }) => (
+                    <div ref={registerChild} style={style}>
+                      <NavLink
+                        rightSection={
+                          isFolder ? (
+                            <Icon name="chevronright" size={10} />
+                          ) : null
+                        }
+                        label={item.name}
+                        active={isSelected}
+                        icon={
+                          <Icon name={isFolder ? "folder" : getIcon(item)} />
+                        }
+                        onClick={e => {
+                          e.preventDefault(); // prevent form submission
+                          e.stopPropagation(); // prevent parent onClick
+                          onClick(item);
+                        }}
+                        variant="light"
+                        mb="xs"
+                      />
+                    </div>
+                  )}
+                </CellMeasurer>
+              );
+            }}
+          />
+        )}
+      </AutoSizer>
+    </PickerColumn>
   );
 };
