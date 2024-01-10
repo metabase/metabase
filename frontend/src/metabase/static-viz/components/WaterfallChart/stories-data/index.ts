1d1a845955d45bf38d1207a44705cc2053a7183f
Emmad Usmani <emmadusmani@berkeley.edu>
2024-01-10 14:37:15 -0800

    waterfall handle negatives (#36887)
    * handle negatives [ci skip]
    
    * add 'mixed above 0' example
    
    * add more storybook examples
    
    * negative handling logic
    
    * refactoring
    
    * rename translationConstant -> negativeTranslation [ci skip]

--- frontend/src/metabase/static-viz/components/WaterfallChart/stories-data/index.ts
+++ frontend/src/metabase/static-viz/components/WaterfallChart/stories-data/index.ts
@@ -1,9 +1,23 @@
 import withoutTotal from "./without-total.json";
 import ordinalXScale from "./ordinal-x-scale.json";
 import timeSeriesDataAsOrdinalXScale from "./timeseries-data-as-ordinal-x-scale.json";
+import mixedAboveZero from "./mixed-above-zero.json";
+import mixedBelowZero from "./mixed-below-zero.json";
+import negativeOnly from "./negative-only.json";
+import startsAboveZeroEndsBelow from "./starts-above-zero-ends-below.json";
+import startsBelowZeroEndsAbove from "./starts-below-zero-ends-above.json";
+import startsAboveZeroCrossesEndsAbove from "./starts-above-zero-crosses-ends-above.json";
+import startsBelowZeroCrossesEndsBelow from "./starts-below-zero-crosses-ends-below.json";
 
 export const data = {
   withoutTotal,
   ordinalXScale,
   timeSeriesDataAsOrdinalXScale,
+  mixedAboveZero,
+  mixedBelowZero,
+  negativeOnly,
+  startsAboveZeroEndsBelow,
+  startsBelowZeroEndsAbove,
+  startsAboveZeroCrossesEndsAbove,
+  startsBelowZeroCrossesEndsBelow,
 };
