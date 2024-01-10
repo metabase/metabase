d44989cc961925496cff0bcd2268b787a6112e00
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-01-10 17:38:51 +0100

    Add TS wrapper for CLJS `template-tags`

--- frontend/src/metabase-lib/native.ts
+++ frontend/src/metabase-lib/native.ts
@@ -24,6 +24,10 @@ export function withTemplateTags(query: Query, tags: string[]): Query {
   return ML.with_template_tags(query, tags);
 }
 
+export function templateTags(query: Query): string {
+  return ML.template_tags(query);
+}
+
 /**
  * Returns the extra keys that are required for this database's native queries, for example `:collection` name is
  *  needed for MongoDB queries.
