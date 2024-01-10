c295b82f6403ffd9f75a3bbdf332c6140454d7d0
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2024-01-10 16:05:29 +0100

    Replace `ML` references with `Lib` (#37497)
    * Replace `ML` references with `Lib` in Question class
    
    * Replace `ML` references with `Lib` in StructuredQuery class
    
    * Replace `ML` references with `Lib` in query builder selectors
    
    * Replace `ML` references with `Lib` in unit tests
    
    Fixes #37496.

--- frontend/src/metabase-lib/breakout.unit.spec.ts
+++ frontend/src/metabase-lib/breakout.unit.spec.ts
@@ -1,52 +1,52 @@
+import * as Lib from "metabase-lib";
 import { createQuery, columnFinder } from "./test-helpers";
-import * as ML from "./v2";
 
 describe("breakout", () => {
   describe("add breakout", () => {
     const query = createQuery();
     const findBreakoutableColumn = columnFinder(
       query,
-      ML.breakoutableColumns(query, 0),
+      Lib.breakoutableColumns(query, 0),
     );
 
     it("should handle no breakout clauses", () => {
-      expect(ML.breakouts(query, 0)).toHaveLength(0);
+      expect(Lib.breakouts(query, 0)).toHaveLength(0);
     });
 
     it("should update the query", () => {
       const productTitle = findBreakoutableColumn("PRODUCTS", "TITLE");
-      const nextQuery = ML.breakout(query, 0, productTitle);
-      const breakouts = ML.breakouts(nextQuery, 0);
+      const nextQuery = Lib.breakout(query, 0, productTitle);
+      const breakouts = Lib.breakouts(nextQuery, 0);
 
       expect(breakouts).toHaveLength(1);
-      expect(ML.displayName(nextQuery, breakouts[0])).toBe("Title");
+      expect(Lib.displayName(nextQuery, breakouts[0])).toBe("Title");
     });
 
     it("should preserve breakout positions between v1-v2 roundtrip", () => {
       const query = createQuery();
       const taxColumn = findBreakoutableColumn("ORDERS", "TAX");
-      const nextQuery = ML.breakout(query, 0, taxColumn);
-      const nextQueryColumns = ML.breakoutableColumns(nextQuery, 0);
+      const nextQuery = Lib.breakout(query, 0, taxColumn);
+      const nextQueryColumns = Lib.breakoutableColumns(nextQuery, 0);
       const nextTaxColumn = columnFinder(nextQuery, nextQueryColumns)(
         "ORDERS",
         "TAX",
       );
 
-      expect(ML.displayInfo(nextQuery, 0, nextTaxColumn).breakoutPosition).toBe(
-        0,
-      );
+      expect(
+        Lib.displayInfo(nextQuery, 0, nextTaxColumn).breakoutPosition,
+      ).toBe(0);
 
       const roundtripQuery = createQuery({
-        query: ML.toLegacyQuery(nextQuery),
+        query: Lib.toLegacyQuery(nextQuery),
       });
-      const roundtripQueryColumns = ML.breakoutableColumns(roundtripQuery, 0);
+      const roundtripQueryColumns = Lib.breakoutableColumns(roundtripQuery, 0);
       const roundtripTaxColumn = columnFinder(
         roundtripQuery,
         roundtripQueryColumns,
       )("ORDERS", "TAX");
 
       expect(
-        ML.displayInfo(roundtripQuery, 0, roundtripTaxColumn).breakoutPosition,
+        Lib.displayInfo(roundtripQuery, 0, roundtripTaxColumn).breakoutPosition,
       ).toBe(0);
     });
   });
@@ -55,25 +55,25 @@ describe("breakout", () => {
     const query = createQuery();
     const findBreakoutableColumn = columnFinder(
       query,
-      ML.breakoutableColumns(query, 0),
+      Lib.breakoutableColumns(query, 0),
     );
 
     it("should update the query", () => {
       const productTitle = findBreakoutableColumn("PRODUCTS", "TITLE");
       const productCategory = findBreakoutableColumn("PRODUCTS", "CATEGORY");
 
-      const breakoutQuery = ML.breakout(query, 0, productTitle);
-      const breakouts = ML.breakouts(breakoutQuery, 0);
+      const breakoutQuery = Lib.breakout(query, 0, productTitle);
+      const breakouts = Lib.breakouts(breakoutQuery, 0);
 
       expect(breakouts).toHaveLength(1);
-      const nextQuery = ML.replaceClause(
+      const nextQuery = Lib.replaceClause(
         breakoutQuery,
         0,
         breakouts[0],
         productCategory,
       );
-      const nextBreakouts = ML.breakouts(nextQuery, 0);
-      expect(ML.displayName(nextQuery, nextBreakouts[0])).toBe("Category");
+      const nextBreakouts = Lib.breakouts(nextQuery, 0);
+      expect(Lib.displayName(nextQuery, nextBreakouts[0])).toBe("Category");
       expect(breakouts[0]).not.toEqual(nextBreakouts[0]);
     });
   });
@@ -82,18 +82,18 @@ describe("breakout", () => {
     const query = createQuery();
     const findBreakoutableColumn = columnFinder(
       query,
-      ML.breakoutableColumns(query, 0),
+      Lib.breakoutableColumns(query, 0),
     );
 
     it("should update the query", () => {
       const productTitle = findBreakoutableColumn("PRODUCTS", "TITLE");
 
-      const breakoutQuery = ML.breakout(query, 0, productTitle);
-      const breakouts = ML.breakouts(breakoutQuery, 0);
+      const breakoutQuery = Lib.breakout(query, 0, productTitle);
+      const breakouts = Lib.breakouts(breakoutQuery, 0);
       expect(breakouts).toHaveLength(1);
 
-      const nextQuery = ML.removeClause(breakoutQuery, 0, breakouts[0]);
-      expect(ML.breakouts(nextQuery, 0)).toHaveLength(0);
+      const nextQuery = Lib.removeClause(breakoutQuery, 0, breakouts[0]);
+      expect(Lib.breakouts(nextQuery, 0)).toHaveLength(0);
     });
   });
 });
