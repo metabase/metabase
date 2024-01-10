10a57bc3ffefca7701c659b0173c94369d4e88b8
Emmad Usmani <emmadusmani@berkeley.edu>
2024-01-10 14:44:33 -0800

    waterfall total (#36961)
    * total bar
    
    * add storybook examples
    
    * handle timeseries [ci skip]

--- frontend/src/metabase/visualizations/echarts/cartesian/waterfall/model/dataset.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/waterfall/model/dataset.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import type { RowValues } from "metabase-types/api";
 import {
   assertMultiMetricColumns,
@@ -5,6 +6,7 @@ import {
 } from "metabase/visualizations/lib/graph/columns";
 import { checkNumber } from "metabase/lib/types";
 
+import type { ComputedVisualizationSettings } from "metabase/visualizations/types";
 import type {
   WaterfallDataset,
   WaterfallDatum,
@@ -38,13 +40,16 @@ export function getWaterfallDataset(
   rows: RowValues[],
   cardColumns: CartesianChartColumns,
   negativeTranslation: number,
+  settings: ComputedVisualizationSettings,
 ): WaterfallDataset {
   const columns = assertMultiMetricColumns(cardColumns);
   const dataset: WaterfallDataset = [];
 
+  let total = 0;
   rows.forEach((row, index) => {
     const dimension = String(row[columns.dimension.index]);
     const value = checkNumber(row[columns.metrics[0].index]);
+    total += value;
 
     let increase: number | "-" = "-";
     let decrease: number | "-" = "-";
@@ -96,7 +101,38 @@ export function getWaterfallDataset(
     dataset.push(createDatum({ dimension, barOffset, increase, decrease }));
   });
 
-  // TODO total
+  if (!settings["waterfall.show_total"]) {
+    return dataset;
+  }
+
+  const barOffset =
+    total >= 0 ? negativeTranslation : negativeTranslation + total;
+
+  let dimension = "Total";
+  // For timeseries x-axis ECharts will not allow mixed values,
+  // so we cannot set the dimension value to "Total." As a workaround,
+  // we instead set it to be a date after the final date in the dataset,
+  // then in the x-axis label formatter we will replace the label with the
+  // string "Total."
+  if (settings["graph.x_axis.scale"] === "timeseries") {
+    const lastDimensionValue = rows[rows.length - 1][columns.dimension.index];
+    if (typeof lastDimensionValue === "boolean") {
+      throw Error(
+        "dimension value cannot be boolean with timeseries x-axis scale",
+      );
+    }
+
+    const lastDate = dayjs(lastDimensionValue);
+    dimension = lastDate.add(1, "day").toISOString();
+  }
+
+  dataset.push(
+    createDatum({
+      dimension,
+      barOffset,
+      total: Math.abs(total),
+    }),
+  );
 
   return dataset;
 }
