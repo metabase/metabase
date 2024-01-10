3f57be6541aee3c6e3e69a33571db7df1f6957e9
Emmad Usmani <emmadusmani@berkeley.edu>
2024-01-10 14:41:29 -0800

    waterfall color setting (#36957)
    * add storybook example
    
    * implement series colors
    
    * implement default settings [ci skip]

--- frontend/src/metabase-types/api/card.ts
+++ frontend/src/metabase-types/api/card.ts
@@ -171,6 +171,12 @@ export type VisualizationSettings = {
   // Scatter plot settings
   "scatter.bubble"?: string; // col name
 
+  // Waterfall settings
+  "waterfall.increase_color"?: string;
+  "waterfall.decrease_color"?: string;
+  "waterfall.total_color"?: string;
+  "waterfall.show_total"?: boolean;
+
   // Funnel settings
   "funnel.rows"?: SeriesOrderSetting[];
 
