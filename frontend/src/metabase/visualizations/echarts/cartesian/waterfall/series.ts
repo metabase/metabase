0f5f492479b69658acff6a827d394d5b0e396c9e
Emmad Usmani <emmadusmani@berkeley.edu>
2024-01-10 14:35:13 -0800

    render waterfall series (#36728)
    * create chart model [ci skip]
    
    * create option
    
    * add new args for getCartesianChartOption [ci skip]

--- frontend/src/metabase/visualizations/echarts/cartesian/waterfall/series.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/waterfall/series.ts
@@ -1,18 +1,51 @@
 import type { RegisteredSeriesOption } from "echarts/types/dist/shared";
 
-import type { DataKey, SeriesModel } from "../model/types";
+import { DATASET_DIMENSIONS } from "./constants";
 
-export function buildEChartsWaterfallSeries(
-  seriesModel: SeriesModel,
-  dimensionDataKey: DataKey,
-  yAxisIndex: number,
-): RegisteredSeriesOption["bar"] {
-  return {
-    type: "bar",
-    yAxisIndex,
-    encode: {
-      y: seriesModel.dataKey,
-      x: dimensionDataKey,
+export function buildEChartsWaterfallSeries(): RegisteredSeriesOption["bar"][] {
+  return [
+    {
+      type: "bar",
+      stack: "waterfall_stack",
+      silent: true,
+      itemStyle: {
+        borderColor: "transparent",
+        color: "transparent",
+      },
+      emphasis: {
+        itemStyle: {
+          borderColor: "transparent",
+          color: "transparent",
+        },
+      },
+      encode: {
+        x: DATASET_DIMENSIONS.dimension,
+        y: "barOffset",
+      },
     },
-  };
+    {
+      type: "bar",
+      stack: "waterfall_stack",
+      encode: {
+        x: DATASET_DIMENSIONS.dimension,
+        y: DATASET_DIMENSIONS.increase,
+      },
+    },
+    {
+      type: "bar",
+      stack: "waterfall_stack",
+      encode: {
+        x: DATASET_DIMENSIONS.dimension,
+        y: DATASET_DIMENSIONS.decrease,
+      },
+    },
+    {
+      type: "bar",
+      stack: "waterfall_stack",
+      encode: {
+        x: DATASET_DIMENSIONS.dimension,
+        y: DATASET_DIMENSIONS.total,
+      },
+    },
+  ];
 }
