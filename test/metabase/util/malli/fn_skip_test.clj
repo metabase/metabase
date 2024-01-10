8997340aa4981fbc63802a238d20aa6d988318ae
Bryan Maass <bryan.maass@gmail.com>
2024-01-10 13:06:23 -0700

    eval

--- test/metabase/util/malli/fn_skip_test.clj
+++ test/metabase/util/malli/fn_skip_test.clj
@@ -8,12 +8,14 @@
   ;; Careful not to use binding for *skip-ns-decision-fn*, because the fn macro will evaluate it before it gets bound!
   (testing "when skip-ns-decision-fn returns true, deparameterized form is emitted"
     (alter-var-root #'mu.fn/*skip-ns-decision-fn* (constantly (fn [_ns] true)))
-    (let [f (mu.fn/fn :- :int [] "schemas aren't checked if this is returned")]
-      (is (= "schemas aren't checked if this is returned" (f))))))
+    (eval
+     '(let [f (mu.fn/fn :- :int [] "schemas aren't checked if this is returned")]
+        (is (= "schemas aren't checked if this is returned" (f)))))))
 
 (deftest checks-skipped-iff-skip-ns-decision-fn-returns-false
   ;; Careful not to use binding for *skip-ns-decision-fn*, because the fn macro will evaluate it before it gets bound!
   (testing "when skip-ns-decision-fn returns false, parameterized form is emitted"
     (alter-var-root #'mu.fn/*skip-ns-decision-fn* (constantly (fn [_ns] false)))
-    (let [f (mu.fn/fn :- :int [] "schemas aren't checked if this is returned")]
-      (is (thrown-with-msg? Exception #"Invalid output" (f))))))
+    (eval
+     '(let [f (mu.fn/fn :- :int [] "schemas aren't checked if this is returned")]
+        (is (thrown-with-msg? Exception #"Invalid output" (f)))))))
