678f92316b7cebf87b1dd5747e67a98f4db21b26
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-01-11 18:08:43 +0700

    Hide Metabase link in `CaveatMessage`

--- frontend/src/metabase/sharing/components/AddEditSidebar/CaveatMessage/CaveatMessage.jsx
+++ frontend/src/metabase/sharing/components/AddEditSidebar/CaveatMessage/CaveatMessage.jsx
@@ -2,13 +2,16 @@ import { t } from "ttag";
 
 import MetabaseSettings from "metabase/lib/settings";
 import ExternalLink from "metabase/core/components/ExternalLink";
+import { useSelector } from "metabase/lib/redux";
+import { getShowMetabaseLinks } from "metabase/selectors/whitelabel";
 import { CaveatText } from "./CaveatMessage.styled";
 
 export function CaveatMessage() {
+  const showMetabaseLinks = useSelector(getShowMetabaseLinks);
   return (
     <CaveatText>
       {t`Recipients will see this data just as you see it, regardless of their permissions.`}
-      {
+      {showMetabaseLinks && (
         <>
           &nbsp;
           <ExternalLink
@@ -19,7 +22,7 @@ export function CaveatMessage() {
             {t`Learn more.`}
           </ExternalLink>
         </>
-      }
+      )}
     </CaveatText>
   );
 }
