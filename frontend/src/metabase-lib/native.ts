bb51edb0865ecfbb38c8a92630648c5ddc7129b2
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2024-01-11 06:21:57 +0100

    [MLv2] Add TypeScript wrappers around native CLJS functions (#37518)
    * Add TS wrapper for CLJS `native-query`
    
    * Add TS wrapper for CLJS `raw-native-query`
    
    * Add TS wrapper for CLJS `with-native-query`
    
    * Add TS wrapper for CLJS `with-template-tags`
    
    * Add TS wrapper for CLJS `template-tags`
    
    * Add TS wrapper for CLJS `extract-template-tags`
    
    * Add TS wrapper for CLJS `has-write-permission`
    
    * Add TS wrapper for CLJS `with-different-database`
    
    * Add TS wrapper for CLJS `engine`
    
    * Fix types
    
    * Replace the direct ML reference with a new wrapper
    
    * Simplify the return type for `extractTemplateTags`

--- frontend/src/metabase-lib/native.ts
+++ frontend/src/metabase-lib/native.ts
@@ -1,8 +1,55 @@
 import * as ML from "cljs/metabase.lib.js";
 
-import type { DatabaseId } from "metabase-types/api";
+import type { DatabaseId, TemplateTags } from "metabase-types/api";
 
 import type { MetadataProvider, Query } from "./types";
+
+export function nativeQuery(
+  databaseId: DatabaseId,
+  metadata: MetadataProvider,
+  innerQuery: string,
+): Query {
+  return ML.native_query(databaseId, metadata, innerQuery);
+}
+
+export function rawNativeQuery(query: Query): string {
+  return ML.raw_native_query(query);
+}
+
+export function withNativeQuery(query: Query, innerQuery: string): Query {
+  return ML.with_native_query(query, innerQuery);
+}
+
+export function withTemplateTags(query: Query, tags: TemplateTags): Query {
+  return ML.with_template_tags(query, tags);
+}
+
+export function templateTags(query: Query): TemplateTags {
+  return ML.template_tags(query);
+}
+
+export function extractTemplateTags(
+  queryText: string,
+  existingTags?: TemplateTags,
+): TemplateTags {
+  return ML.extract_template_tags(queryText, existingTags);
+}
+
+export function hasWritePermission(query: Query): boolean {
+  return ML.has_write_permission(query);
+}
+
+export function withDifferentDatabase(
+  query: Query,
+  databaseId: DatabaseId,
+  metadata: MetadataProvider,
+): Query {
+  return ML.with_different_database(query, databaseId, metadata);
+}
+
+export function engine(query: Query): string {
+  return ML.engine(query);
+}
 
 /**
  * Returns the extra keys that are required for this database's native queries, for example `:collection` name is
