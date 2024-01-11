e80a40f7c11f95d74e2162f8241f8aef506d0c56
Kamil Mielnik <kamil@kamilmielnik.com>
2024-01-11 21:00:17 +0700

    Rename hasAnyClauses to hasAnyClausesAtStage, introduce new hasAnyClauses

--- frontend/src/metabase-lib/query-utils.ts
+++ frontend/src/metabase-lib/query-utils.ts
@@ -6,9 +6,17 @@ import { filters } from "./filter";
 import { joins } from "./join";
 import { hasLimit } from "./limit";
 import { orderBys } from "./order_by";
+import { stageCount } from "./query";
 import type { Query } from "./types";
 
-export const hasAnyClauses = (query: Query, stageIndex: number): boolean => {
+export const hasAnyClauses = (query: Query): boolean => {
+  return stageCount(query) > 1 || hasAnyClausesAtStage(query, 0);
+};
+
+export const hasAnyClausesAtStage = (
+  query: Query,
+  stageIndex: number,
+): boolean => {
   const hasJoins = joins(query, stageIndex).length > 0;
   const hasExpressions = expressions(query, stageIndex).length > 0;
   const hasFilters = filters(query, stageIndex).length > 0;
