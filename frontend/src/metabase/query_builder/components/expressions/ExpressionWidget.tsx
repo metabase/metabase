6388b57972dd454f7b8efce89344984846ae52f5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-01-11 20:31:08 +0700

    Hide Metabase link in `ExpressionWidget`

--- frontend/src/metabase/query_builder/components/expressions/ExpressionWidget.tsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionWidget.tsx
@@ -4,13 +4,9 @@ import { t } from "ttag";
 import { isNotNull } from "metabase/lib/types";
 import Button from "metabase/core/components/Button";
 import Input from "metabase/core/components/Input/Input";
-import Tooltip from "metabase/core/components/Tooltip";
 import MetabaseSettings from "metabase/lib/settings";
 import type { Expression } from "metabase-types/api";
 import type * as Lib from "metabase-lib";
-import { useSelector } from "metabase/lib/redux";
-import { getShowMetabaseLinks } from "metabase/selectors/whitelabel";
-import { Box } from "metabase/ui";
 import { isExpression } from "metabase-lib/expressions";
 
 import { ExpressionEditorTextfield } from "./ExpressionEditorTextfield";
@@ -21,12 +17,11 @@ import {
   FieldLabel,
   FieldWrapper,
   Footer,
-  InfoLink,
   RemoveLink,
-  StyledFieldTitleIcon,
 } from "./ExpressionWidget.styled";
+import { ExpressionWidgetInfo } from "./ExpressionWidgetInfo";
 
-const EXPRESSIONS_DOCUMENTATION_URL = MetabaseSettings.docsUrl(
+export const EXPRESSIONS_DOCUMENTATION_URL = MetabaseSettings.docsUrl(
   "questions/query-builder/expressions",
 );
 
@@ -126,39 +121,13 @@ export const ExpressionWidget = <Clause extends object = Lib.ExpressionClause>(
     setError(null);
   };
 
-  const showMetabaseLinks = useSelector(getShowMetabaseLinks);
-
   return (
     <Container>
       {header}
       <ExpressionFieldWrapper>
         <FieldLabel htmlFor="expression-content">
           {t`Expression`}
-          {showMetabaseLinks ? (
-            <Tooltip
-              tooltip={t`You can reference columns here in functions or equations, like: floor([Price] - [Discount]). Click for documentation.`}
-              placement="right"
-              maxWidth={332}
-            >
-              <InfoLink
-                target="_blank"
-                href={EXPRESSIONS_DOCUMENTATION_URL}
-                aria-label={t`Open expressions documentation`}
-              >
-                <StyledFieldTitleIcon name="info" />
-              </InfoLink>
-            </Tooltip>
-          ) : (
-            <Tooltip
-              tooltip={t`You can reference columns here in functions or equations, like: floor([Price] - [Discount]).`}
-              placement="right"
-              maxWidth={332}
-            >
-              <Box ml="0.25rem">
-                <StyledFieldTitleIcon name="info" />
-              </Box>
-            </Tooltip>
-          )}
+          <ExpressionWidgetInfo />
         </FieldLabel>
         <div ref={helpTextTargetRef}>
           <ExpressionEditorTextfield
