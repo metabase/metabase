0971c938ac6e8ec73d738288e358411e9a300ab6
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-01-11 15:08:23 +0700

    Prepare `CaveatMessage` for testing

--- frontend/src/metabase/sharing/components/AddEditSidebar/CaveatMessage/CaveatMessage.jsx
+++ frontend/src/metabase/sharing/components/AddEditSidebar/CaveatMessage/CaveatMessage.jsx
@@ -4,21 +4,22 @@ import MetabaseSettings from "metabase/lib/settings";
 import ExternalLink from "metabase/core/components/ExternalLink";
 import { CaveatText } from "./CaveatMessage.styled";
 
-function CaveatMessage() {
+export function CaveatMessage() {
   return (
     <CaveatText>
       {t`Recipients will see this data just as you see it, regardless of their permissions.`}
-      &nbsp;
-      <ExternalLink
-        className="link"
-        target="_blank"
-        href={MetabaseSettings.docsUrl("dashboards/subscriptions")}
-      >
-        {t`Learn more`}
-      </ExternalLink>
-      .
+      {
+        <>
+          &nbsp;
+          <ExternalLink
+            className="link"
+            target="_blank"
+            href={MetabaseSettings.docsUrl("dashboards/subscriptions")}
+          >
+            {t`Learn more.`}
+          </ExternalLink>
+        </>
+      }
     </CaveatText>
   );
 }
-
-export default CaveatMessage;
