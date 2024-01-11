95c1a19aee0a4c5b4dfe87ada21f4097d2b7bb20
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-01-11 19:24:38 +0700

    Hide Metabase link in `ImpossibleToCreateModelModal`

--- frontend/src/metabase/query_builder/components/ImpossibleToCreateModelModal/ImpossibleToCreateModelModal.tsx
+++ frontend/src/metabase/query_builder/components/ImpossibleToCreateModelModal/ImpossibleToCreateModelModal.tsx
@@ -5,6 +5,11 @@ import ExternalLink from "metabase/core/components/ExternalLink";
 import ModalContent from "metabase/components/ModalContent";
 
 import MetabaseSettings from "metabase/lib/settings";
+import { useSelector } from "metabase/lib/redux";
+import { getShowMetabaseLinks } from "metabase/selectors/whitelabel";
+
+const sqlSnippetLinkText = t`SQL snippets`;
+const referenceLinkText = t`reference the results of another saved question`;
 
 type Props = {
   onClose: () => void;
@@ -12,29 +17,31 @@ type Props = {
 
 function SQLSnippetsDocLink() {
   const href = MetabaseSettings.docsUrl("questions/native-editor/sql-snippets");
-  return <ExternalLink href={href}>{t`SQL snippets`}</ExternalLink>;
+  return <ExternalLink href={href}>{sqlSnippetLinkText}</ExternalLink>;
 }
 
 function ReferencingQuestionsDocLink() {
   const href = MetabaseSettings.docsUrl("questions/native-editor/sql-snippets");
-  return (
-    <ExternalLink
-      href={href}
-    >{t`reference the results of another saved question`}</ExternalLink>
-  );
+  return <ExternalLink href={href}>{referenceLinkText}</ExternalLink>;
 }
 
 export function ImpossibleToCreateModelModal({ onClose }: Props) {
+  const showMetabaseLinks = useSelector(getShowMetabaseLinks);
+
   return (
     <ModalContent
       title={t`Variables in models aren't supported yet`}
       onClose={onClose}
     >
-      <p className="text-paragraph">{jt`To solve this, just remove the variables in this question and try again. (It's okay to use ${(
-        <SQLSnippetsDocLink key="link-1" />
-      )} or ${(
-        <ReferencingQuestionsDocLink key="link-2" />
-      )} in your query.)`}</p>
+      <p className="text-paragraph">
+        {showMetabaseLinks
+          ? jt`To solve this, just remove the variables in this question and try again. (It's okay to use ${(
+              <SQLSnippetsDocLink key="link-1" />
+            )} or ${(
+              <ReferencingQuestionsDocLink key="link-2" />
+            )} in your query.)`
+          : t`To solve this, just remove the variables in this question and try again. (It's okay to use SQL snippets or reference the results of another saved question in your query.)`}
+      </p>
       <div className="flex justify-center py1">
         <Button primary onClick={onClose}>{t`Okay`}</Button>
       </div>
---
+++ frontend/src/metabase/query_builder/components/ImpossibleToCreateModelModal/tests/common.unit.spec.ts
@@ -0,0 +1,48 @@
+import { screen } from "__support__/ui";
+import { setup } from "./setup";
+
+describe("ImpossibleToCreateModelModal (OSS)", () => {
+  it("should show a help link when `show-metabase-links: true`", () => {
+    setup({ showMetabaseLinks: true });
+
+    const sqlSnippetLink = screen.getByRole("link", { name: "SQL snippets" });
+    expect(sqlSnippetLink).toBeInTheDocument();
+    expect(sqlSnippetLink).toHaveProperty(
+      "href",
+      "https://www.metabase.com/docs/latest/questions/native-editor/sql-snippets.html",
+    );
+
+    const referenceLink = screen.getByRole("link", {
+      name: "reference the results of another saved question",
+    });
+    expect(referenceLink).toBeInTheDocument();
+    // TODO: Change this link or remove this comment once the discussion is over.
+    // Discussion: https://metaboat.slack.com/archives/C01LQQ2UW03/p1704975076876289
+    expect(referenceLink).toHaveProperty(
+      "href",
+      "https://www.metabase.com/docs/latest/questions/native-editor/sql-snippets.html",
+    );
+  });
+
+  it("should show a help link when `show-metabase-links: false`", () => {
+    setup({ showMetabaseLinks: false });
+
+    const sqlSnippetLink = screen.getByRole("link", { name: "SQL snippets" });
+    expect(sqlSnippetLink).toBeInTheDocument();
+    expect(sqlSnippetLink).toHaveProperty(
+      "href",
+      "https://www.metabase.com/docs/latest/questions/native-editor/sql-snippets.html",
+    );
+
+    const referenceLink = screen.getByRole("link", {
+      name: "reference the results of another saved question",
+    });
+    expect(referenceLink).toBeInTheDocument();
+    // TODO: Change this link or remove this comment once the discussion is over.
+    // Discussion: https://metaboat.slack.com/archives/C01LQQ2UW03/p1704975076876289
+    expect(referenceLink).toHaveProperty(
+      "href",
+      "https://www.metabase.com/docs/latest/questions/native-editor/sql-snippets.html",
+    );
+  });
+});
