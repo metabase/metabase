d3452176a172dc0cea673f04757b2c357d651524
Anton Kulyk <kuliks.anton@gmail.com>
2024-01-12 17:06:40 +0000

    Add `scalar.comparisons` to `computeSmartScalarSettings`

--- frontend/src/metabase/static-viz/components/SmartScalar/settings.ts
+++ frontend/src/metabase/static-viz/components/SmartScalar/settings.ts
@@ -3,7 +3,7 @@ import { getCommonStaticVizSettings } from "metabase/static-viz/lib/settings";
 import type { ComputedVisualizationSettings } from "metabase/visualizations/types";
 import { getDefaultColumn } from "metabase/visualizations/lib/settings/utils";
 import { VIZ_SETTINGS_DEFAULTS } from "metabase/visualizations/visualizations/SmartScalar/constants";
-// import { getDefaultComparison } from "metabase/visualizations/visualizations/SmartScalar/utils";
+import { getDefaultComparison } from "metabase/visualizations/visualizations/SmartScalar/utils";
 
 export const computeSmartScalarSettings = (
   rawSeries: RawSeries,
@@ -13,7 +13,7 @@ export const computeSmartScalarSettings = (
 
   settings["scalar.field"] ??= getDefaultColumn(rawSeries, settings);
 
-  // settings["scalar.comparisons"] ??= getDefaultComparison(rawSeries, settings);
+  settings["scalar.comparisons"] ??= getDefaultComparison(rawSeries, settings);
 
   settings["scalar.switch_positive_negative"] ??=
     VIZ_SETTINGS_DEFAULTS["scalar.switch_positive_negative"];
