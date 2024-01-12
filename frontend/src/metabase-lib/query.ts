9e038f297575df9fd4dce4241cd0bdfba23091c9
Kamil Mielnik <kamil@kamilmielnik.com>
2024-01-12 17:57:04 +0700

    Move stage-related utils from query.ts to stage.ts

--- frontend/src/metabase-lib/query.ts
+++ frontend/src/metabase-lib/query.ts
@@ -1,5 +1,6 @@
 import * as ML from "cljs/metabase.lib.js";
 import type { DatabaseId, DatasetQuery, TableId } from "metabase-types/api";
+import type LegacyMetadata from "./metadata/Metadata";
 import type {
   CardMetadata,
   Clause,
@@ -11,7 +12,6 @@ import type {
   SegmentMetadata,
   TableMetadata,
 } from "./types";
-import type LegacyMetadata from "./metadata/Metadata";
 
 export function fromLegacyQuery(
   databaseId: DatabaseId,
@@ -42,32 +42,6 @@ export function withDifferentTable(query: Query, tableId: TableId): Query {
 
 export function suggestedName(query: Query): string {
   return ML.suggestedName(query);
-}
-
-export function stageCount(query: Query): number {
-  return ML.stage_count(query);
-}
-
-export function appendStage(query: Query): Query {
-  return ML.append_stage(query);
-}
-
-export function dropStage(query: Query, stageIndex: number): Query {
-  return ML.drop_stage(query, stageIndex);
-}
-
-export function dropStageIfEmpty(query: Query, stageIndex: number): Query {
-  return ML.drop_stage_if_empty(query, stageIndex);
-}
-
-export function dropEmptyStages(query: Query): Query {
-  const stageIndexes = Array.from({ length: stageCount(query) }).map(
-    (_, index) => index,
-  );
-
-  return stageIndexes.reduceRight((query, stageIndex) => {
-    return dropStageIfEmpty(query, stageIndex);
-  }, query);
 }
 
 export function removeClause(
--- frontend/src/metabase-lib/stages.ts
+++ frontend/src/metabase-lib/stages.ts
@@ -1,3 +1,5 @@
+import * as ML from "cljs/metabase.lib.js";
+
 import { aggregations } from "./aggregation";
 import { breakouts } from "./breakout";
 import { expressions } from "./expression";
@@ -6,7 +8,6 @@ import { filters } from "./filter";
 import { joins } from "./join";
 import { hasLimit } from "./limit";
 import { orderBys } from "./order_by";
-import { stageCount } from "./query";
 import type { Query } from "./types";
 
 export const hasAnyClauses = (query: Query): boolean => {
@@ -37,3 +38,29 @@ export const hasAnyClausesAtStage = (
     hasFields
   );
 };
+
+export function stageCount(query: Query): number {
+  return ML.stage_count(query);
+}
+
+export function appendStage(query: Query): Query {
+  return ML.append_stage(query);
+}
+
+export function dropStage(query: Query, stageIndex: number): Query {
+  return ML.drop_stage(query, stageIndex);
+}
+
+export function dropStageIfEmpty(query: Query, stageIndex: number): Query {
+  return ML.drop_stage_if_empty(query, stageIndex);
+}
+
+export function dropEmptyStages(query: Query): Query {
+  const stageIndexes = Array.from({ length: stageCount(query) }).map(
+    (_, index) => index,
+  );
+
+  return stageIndexes.reduceRight((query, stageIndex) => {
+    return dropStageIfEmpty(query, stageIndex);
+  }, query);
+}
