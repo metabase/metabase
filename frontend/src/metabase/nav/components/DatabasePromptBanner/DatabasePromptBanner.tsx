89d33df5c25010ed7d6642c5d465a138e7257650
Gustavo Saiani <gus@metabase.com>
2024-01-12 21:13:24 -0300

    Pass email and siteURL to metabase.com/help/connect (#37571)

--- frontend/src/metabase/nav/components/DatabasePromptBanner/DatabasePromptBanner.tsx
+++ frontend/src/metabase/nav/components/DatabasePromptBanner/DatabasePromptBanner.tsx
@@ -1,6 +1,9 @@
 import { t } from "ttag";
 import type { Location } from "history";
 
+import { getSetting } from "metabase/selectors/settings";
+import { useSelector } from "metabase/lib/redux";
+
 import Link from "metabase/core/components/Link/Link";
 import { trackDatabasePromptBannerClicked } from "metabase/nav/analytics";
 import { useShouldShowDatabasePromptBanner } from "metabase/nav/hooks";
@@ -18,6 +21,13 @@ interface DatabasePromptBannerProps {
 }
 
 export function DatabasePromptBanner({ location }: DatabasePromptBannerProps) {
+  const adminEmail = useSelector(state => getSetting(state, "admin-email"));
+  const siteUrl = useSelector(state => getSetting(state, "site-url"));
+
+  const helpUrl = new URL("https://metabase.com/help/connect");
+  helpUrl.searchParams.set("email", adminEmail || "");
+  helpUrl.searchParams.set("site_url", siteUrl || "");
+
   const shouldShowDatabasePromptBanner = useShouldShowDatabasePromptBanner();
   if (!shouldShowDatabasePromptBanner) {
     return null;
@@ -32,7 +42,7 @@ export function DatabasePromptBanner({ location }: DatabasePromptBannerProps) {
       <Prompt>{t`Connect to your database to get the most from Metabase.`}</Prompt>
       <CallToActions>
         <GetHelpButton
-          href="https://metabase.com/help/connect"
+          href={helpUrl.href}
           onClickCapture={() => {
             trackDatabasePromptBannerClicked("help");
           }}
