93014503f378fd2ee40c48558e54e79441e7ff48
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-01-12 21:08:43 -0300

    fix icons import

--- frontend/src/metabase/visualizations/echarts/cartesian/timeline-events/option.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/timeline-events/option.ts
@@ -1,8 +1,8 @@
 import type { LineSeriesOption } from "echarts";
 import type { MarkLine1DDataItemOption } from "echarts/types/src/component/marker/MarkLineModel";
 import type { RenderingContext } from "metabase/visualizations/types";
-import type { IconName } from "metabase/ui";
-import { Icons } from "metabase/ui";
+import type { IconName } from "metabase/ui/components/icons/Icon/icons";
+import { Icons } from "metabase/ui/components/icons/Icon/icons";
 import { CHART_STYLE } from "metabase/visualizations/echarts/cartesian/constants/style";
 import type { TimelineEventsModel } from "metabase/visualizations/echarts/cartesian/timeline-events/types";
 import type { TimelineEventId } from "metabase-types/api";
