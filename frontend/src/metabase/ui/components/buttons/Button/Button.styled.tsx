b8c116028e816273c6a76e4651e08977715e1415
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-01-15 10:02:11 +0200

    Update frontend/src/metabase/ui/components/buttons/Button/Button.styled.tsx
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- frontend/src/metabase/ui/components/buttons/Button/Button.styled.tsx
+++ frontend/src/metabase/ui/components/buttons/Button/Button.styled.tsx
@@ -37,8 +37,6 @@ export const getButtonOverrides = (): MantineThemeOverride["components"] => ({
           ref: getStylesRef("label"),
           display: "inline-block",
           height: "auto",
-          overflow: "hidden",
-          whiteSpace: "nowrap",
           textOverflow: "ellipsis",
         },
         leftIcon: {
