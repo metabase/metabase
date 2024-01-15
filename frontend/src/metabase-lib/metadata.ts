234b538e2d0d356a2956784dd03582f6c7f69317
Kamil Mielnik <kamil@kamilmielnik.com>
2024-01-15 20:46:44 +0700

    Allow Field to be passed to fromLegacyColumn

--- frontend/src/metabase-lib/metadata.ts
+++ frontend/src/metabase-lib/metadata.ts
@@ -1,6 +1,11 @@
 import * as ML from "cljs/metabase.lib.js";
 import * as ML_MetadataCalculation from "cljs/metabase.lib.metadata.calculation";
-import type { DatabaseId, DatasetColumn, TableId } from "metabase-types/api";
+import type {
+  DatabaseId,
+  DatasetColumn,
+  Field,
+  TableId,
+} from "metabase-types/api";
 import type Metadata from "./metadata/Metadata";
 import type {
   AggregationClause,
@@ -184,7 +189,7 @@ export function returnedColumns(
 export function fromLegacyColumn(
   query: Query,
   stageIndex: number,
-  column: DatasetColumn,
+  columnOrField: DatasetColumn | Field,
 ): ColumnMetadata {
-  return ML.legacy_column__GT_metadata(query, stageIndex, column);
+  return ML.legacy_column__GT_metadata(query, stageIndex, columnOrField);
 }
