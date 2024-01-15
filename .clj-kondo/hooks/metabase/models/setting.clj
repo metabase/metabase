437600322fdcdb7c732bfe6de283dbebe92aa036
Chris Truter <chris@metabase.com>
2024-01-15 15:47:42 +0200

    Add kondo for missing :export? on defsetting

--- .clj-kondo/hooks/metabase/models/setting.clj
+++ .clj-kondo/hooks/metabase/models/setting.clj
@@ -3,6 +3,9 @@
    [clj-kondo.hooks-api :as hooks]
    [hooks.common :as common]))
 
+(def ^:private ignored-implicit-export?
+  '#{engines})
+
 (defn defsetting
   "Rewrite a [[metabase.models.defsetting]] form like
 
@@ -11,14 +14,14 @@
   as
 
     (let [_ \"Description\"
-          _ :type
-          _ :boolean]
+          _ {:type :boolean}]
       (defn my-setting \"Docstring.\" [])
       (defn my-setting! \"Docstring.\" [_value-or-nil]))
 
   for linting purposes."
   [{:keys [node]}]
-  (let [[setting-name & args] (rest (:children node))
+  (let [[setting-name docstring & options] (rest (:children node))
+        anon-binding (common/with-macro-meta (hooks/token-node '_) node)
         ;; (defn my-setting [] ...)
         getter-node           (-> (list
                                    (hooks/token-node 'defn)
@@ -37,11 +40,17 @@
                                    (hooks/vector-node [(hooks/token-node '_value-or-nil)]))
                                   hooks/list-node
                                   (with-meta (update (meta node) :clj-kondo/ignore #(hooks/vector-node (cons :clojure-lsp/unused-public-var (:children %))))))]
+    (when (nil? (second (drop-while (comp not #{[:k :export?]} first) options)))
+      (when-not (contains? ignored-implicit-export? (:value setting-name))
+        (hooks/reg-finding! (assoc (meta node)
+                              :message "Setting definition must provide an explicit value for :export?"
+                              :type :metabase/defsetting-must-specify-export))))
     {:node (-> (list
                 (hooks/token-node 'let)
                 ;; include description and the options map so they can get validated as well.
-                (hooks/vector-node (vec (interleave (repeat (common/with-macro-meta (hooks/token-node '_) node))
-                                                    args)))
+                (hooks/vector-node
+                  [anon-binding docstring
+                   anon-binding (hooks/map-node options)])
                 getter-node
                 setter-node)
                hooks/list-node
@@ -50,15 +59,15 @@
 (comment
   (defn- defsetting* [form]
     (hooks/sexpr
-     (:node
-      (defsetting
-        {:node
-         (hooks/parse-string
-          (with-out-str
-            (clojure.pprint/pprint
-             form)))}))))
+      (:node
+        (defsetting
+          {:node
+           (hooks/parse-string
+             (with-out-str
+               ((resolve 'clojure.pprint/pprint)
+                form)))}))))
 
-  (defn- x []
+  (defn x []
     (defsetting*
       '(defsetting active-users-count
          (deferred-tru "Cached number of active users. Refresh every 5 minutes.")
