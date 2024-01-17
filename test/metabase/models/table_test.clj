a614a0622fa1e03d69f83b2d085418b604cbc6c8
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2024-01-17 16:15:03 -0500

    Set default DB, table, and group perms when new ones are added (#37426)

--- test/metabase/models/table_test.clj
+++ test/metabase/models/table_test.clj
@@ -2,13 +2,15 @@
   (:require
    [clojure.java.jdbc :as jdbc]
    [clojure.test :refer :all]
+   [metabase.models.data-permissions :as data-perms]
    [metabase.models.database :refer [Database]]
-   [metabase.models.permissions :as perms]
+   [metabase.models.permissions-group :as perms-group]
    [metabase.models.serialization :as serdes]
    [metabase.models.table :as table :refer [Table]]
    [metabase.sync :as sync]
    [metabase.test :as mt]
    [metabase.test.data.one-off-dbs :as one-off-dbs]
+   [metabase.util :as u]
    [toucan2.core :as t2]
    [toucan2.tools.with-temp :as t2.with-temp]))
 
@@ -81,16 +83,36 @@
                (serdes/raw-hash ["PUBLIC" "widget" db-hash])
                (serdes/identity-hash table)))))))
 
+(deftest set-new-table-permissions!-test
+  (testing "New permissions are set appropriately for a new table, for all groups"
+    (mt/with-temp [:model/PermissionsGroup {group-id :id} {}
+                   :model/Database         {db-id :id}    {}
+                   :model/Table            {table-id :id} {:db_id  db-id
+                                                           :schema "PUBLIC"}]
+      ;; All Users group should have full data access, full download abilities, and full metadata management
+      (let [all-users-group-id (u/the-id (perms-group/all-users))]
+        (is (partial=
+             {all-users-group-id
+              {db-id
+               {:data-access           {"PUBLIC" {table-id :unrestricted}}
+                :download-results      {"PUBLIC" {table-id :one-million-rows}}
+                :manage-table-metadata {"PUBLIC" {table-id :no}}}}}
+             (data-perms/data-permissions-graph :group-id all-users-group-id :db-id db-id))))
+
+      ;; Other groups should have no-self-service data access and no download abilities or metadata management
+      (is (partial=
+           {group-id
+            {db-id
+             {:data-access           {"PUBLIC" {table-id :no-self-service}}
+              :download-results      {"PUBLIC" {table-id :no}}
+              :manage-table-metadata {"PUBLIC" {table-id :no}}}}}
+           (data-perms/data-permissions-graph :group-id group-id :db-id db-id))))))
+
 (deftest cleanup-permissions-after-delete-table-test
   (mt/with-temp
     [:model/Database         {db-id :id}    {}
-     :model/PermissionsGroup {group-id :id} {}
-     :model/Table            table          {:db_id  db-id
-                                             :active true}]
-    (let [read-perm-id     (:id (first (perms/grant-permissions! group-id (perms/table-read-path table))))
-          download-perm-id (:id (first (perms/grant-permissions!
-                                        group-id (perms/feature-perms-path :download :limited (:db_id table) (:schema table) (:id table)))))]
-      (t2/delete! :model/Table (:id table))
-      (testing "table specific permissions are deleted when we delete the table"
-        (is (false? (t2/exists? :model/permissions read-perm-id)))
-        (is (false? (t2/exists? :model/permissions download-perm-id)))))))
+     :model/Table            {table-id :id} {:db_id  db-id}]
+    (is (true? (t2/exists? :model/DataPermissions :table_id table-id)))
+    (t2/delete! :model/Table table-id)
+    (testing "Table-level permissions are deleted when we delete the table"
+      (is (false? (t2/exists? :model/DataPermissions :table_id table-id))))))
