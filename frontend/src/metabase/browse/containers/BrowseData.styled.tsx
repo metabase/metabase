5cf9fa461a6aab7d99ce5655e4d982c556b6cad0
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-16 22:40:52 -0500

    Style tweaks to models in browse data

--- frontend/src/metabase/browse/containers/BrowseData.styled.tsx
+++ frontend/src/metabase/browse/containers/BrowseData.styled.tsx
@@ -9,6 +9,7 @@ import { GridItem } from "metabase/components/Grid";
 import { Ellipsified } from "metabase/core/components/Ellipsified";
 
 import { Tabs } from "metabase/ui";
+import EmptyState from "metabase/components/EmptyState";
 
 export const DatabaseCard = styled(Card)`
   padding: 1.5rem;
@@ -40,17 +41,15 @@ export const ModelCard = styled(Card)`
   &:hover {
     color: ${color("brand")};
   }
-  // TODO: Use rem 1rem is 16px, so 144px is 9rem
   height: 9rem;
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-between;
   align-items: flex-start;
-  // TODO: Ask Kyle about the box-shadow on the card, which is different between the spec and the defaults for this Card component
+
+  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.06) !important;
 
   // TODO: This feels hacky, is this the right way to do this?
-  & .last-edit-info-label-button {
-  }
   & .last-edit-info-label-button div,
   & .last-edit-info-label-button span {
     white-space: wrap !important;
@@ -73,20 +72,6 @@ export const MultilineEllipsified = styled(Ellipsified)`
   -webkit-box-orient: vertical;
 `;
 
-export const ModelGroupGrid = styled.div`
-  flex: 1;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
-  gap: 1rem;
-  margin-top: 1rem;
-  & > div {
-    height: 144px;
-    &:hover {
-      color: ${color("brand")};
-    }
-  }
-`;
-
 export const BrowseTabs = styled(Tabs)`
   display: flex;
   flex-flow: column nowrap;
@@ -97,15 +82,27 @@ export const BrowseTabsPanel = styled(Tabs.Panel)`
   display: flex;
   flex-flow: column nowrap;
   flex: 1;
+  height: 100%;
 `;
 
 export const BrowseContainer = styled.div`
   display: flex;
   flex: 1;
   flex-flow: column nowrap;
+  height: 100%;
 `;
 
 export const SimpleBrowseHeader = styled.h2`
   margin-bottom: 0.35rem;
   margin-right: 1rem;
+`;
+
+export const BigEmptyState = styled(EmptyState)`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 `;
