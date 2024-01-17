d4e81b497583cc7a55dfd50d13f6f803902a2369
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-17 15:04:00 -0500

    Remove virtualization from BrowseModels

--- frontend/src/metabase/browse/containers/BrowseModels.tsx
+++ frontend/src/metabase/browse/containers/BrowseModels.tsx
@@ -1,15 +1,8 @@
-import { cloneElement, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import _ from "underscore";
 import { t } from "ttag";
 
-import type { GridCellProps } from "react-virtualized";
-
-import {
-  Grid as VirtualizedGrid,
-  WindowScroller,
-  AutoSizer,
-} from "react-virtualized";
 import type { CollectionItemWithLastEditedInfo } from "metabase/components/LastEditInfoLabel/LastEditInfoLabel";
 import type { Collection, CollectionItem } from "metabase-types/api";
 import * as Urls from "metabase/lib/urls";
@@ -42,15 +35,6 @@ interface Model extends CollectionItem {
   created_at?: string | undefined;
 }
 
-type RenderItemFunction = (
-  props: GridCellProps & {
-    columnCount: number;
-    gridGapSize?: number;
-    groupLabel?: string;
-    cells: Cell[];
-  },
-) => JSX.Element | null;
-
 const emptyArray: Model[] = [];
 
 export const BrowseModels = ({
@@ -64,8 +48,6 @@ export const BrowseModels = ({
   const rem = parseInt(space(2));
   const gridGapSize = rem;
   const itemMinWidth = 15 * rem;
-  const defaultItemHeight = 10 * rem;
-  const headerHeight = 3 * rem;
 
   useEffect(() => {
     const configureGrid = () => {
@@ -101,45 +83,12 @@ export const BrowseModels = ({
     );
   }
 
-  const { cells, columnCount } = gridOptions;
-
-  const getRowHeight = ({ index: rowIndex }: { index: number }) => {
-    const cellIndex = rowIndex * columnCount;
-    return cellIsInHeaderRow(cells[cellIndex])
-      ? headerHeight
-      : defaultItemHeight;
-  };
-
-  const cellRenderer = (props: GridCellProps) =>
-    renderItem({
-      ...props,
-      cells,
-      columnCount,
-    });
+  const { cells } = gridOptions;
 
   return (
     <GridContainer>
-      {cells.length && contentViewport ? (
-        <WindowScroller scrollElement={contentViewport}>
-          {({ height, isScrolling, onChildScroll, scrollTop }) => (
-            <AutoSizer disableHeight>
-              {() => (
-                <VirtualizedGrid
-                  data-testid="model-browser"
-                  {...gridOptions}
-                  gap={gridGapSize}
-                  autoHeight
-                  height={height}
-                  isScrolling={isScrolling}
-                  scrollTop={scrollTop}
-                  onScroll={onChildScroll}
-                  rowHeight={getRowHeight}
-                  cellRenderer={cellRenderer}
-                />
-              )}
-            </AutoSizer>
-          )}
-        </WindowScroller>
+      {cells ? (
+        cells
       ) : (
         <CenteredEmptyState
           title={t`No models here yet`}
@@ -281,22 +230,11 @@ const CollectionHeader = ({
   );
 };
 
-const BlankCell = (props: { style?: React.CSSProperties }) => (
-  <div {...props} />
-);
-const BlankCellInHeader = (props: { style?: React.CSSProperties }) => (
-  <div {...props} />
-);
-
 type Cell = React.ReactElement | null;
 
-const makeCells = (models: Model[], columnCount: number): Cell[] => {
+const makeCells = (models: Model[]): Cell[] => {
   const cells: Cell[] = [];
-  for (
-    let i = 0, columnIndex = 0;
-    i < models.length;
-    i++, columnIndex = (columnIndex + 1) % columnCount
-  ) {
+  for (let i = 0; i < models.length; i++) {
     const model = models[i];
 
     const collectionIdChanged =
@@ -309,23 +247,12 @@ const makeCells = (models: Model[], columnCount: number): Cell[] => {
     // add an item that represents the header of the collection
     if (firstModelInItsCollection) {
       const header = <CollectionHeader collection={model.collection} />;
-      // So that the collection header appears at the start of the row,
-      // add zero or more blank items to fill in the rest of the previous row
-      if (columnIndex > 0) {
-        cells.push(...Array(columnCount - columnIndex).fill(<BlankCell />));
-      }
       cells.push(header);
-      // Fill in the rest of the header row with blank items
-      cells.push(...Array(columnCount - 1).fill(<BlankCellInHeader />));
-      columnIndex = 0;
     }
     cells.push(<ModelCell model={model} />);
   }
   return cells;
 };
-
-const cellIsInHeaderRow = (item: Cell) =>
-  item?.type === CollectionHeader || item?.type === BlankCellInHeader;
 
 const getGridOptions = (
   models: Model[],
@@ -360,7 +287,7 @@ const getGridOptions = (
 
   const columnCount = calculateColumnCount(width);
   const columnWidth = calculateItemWidth(width, columnCount);
-  const cells = makeCells(sortedModels, columnCount);
+  const cells = makeCells(sortedModels);
   const rowCount = Math.ceil(cells.length / columnCount);
 
   return {
@@ -370,21 +297,6 @@ const getGridOptions = (
     rowCount,
     width,
   };
-};
-
-const renderItem: RenderItemFunction = ({
-  columnCount,
-  columnIndex,
-  cells,
-  rowIndex,
-  style,
-}) => {
-  const index = rowIndex * columnCount + columnIndex;
-  const cell = cells[index];
-  return cell
-    ? // Render the component with the style prop provided by the grid
-      cloneElement(cell as React.ReactElement, { style })
-    : null;
 };
 
 const addLastEditInfo = (model: Model): CollectionItemWithLastEditedInfo => ({
