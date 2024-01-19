602e292ac30d3166c057d58e3886df29311be009
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-19 15:50:04 -0500

    wip: removing ga

--- frontend/src/metabase/admin/datamodel/table.js
+++ frontend/src/metabase/admin/datamodel/table.js
@@ -1,6 +1,5 @@
 import { createThunkAction } from "metabase/lib/redux";
 
-import * as MetabaseAnalytics from "metabase/lib/analytics";
 import { MetabaseApi } from "metabase/services";
 
 export const RESCAN_TABLE_VALUES = "metabase/admin/tables/RESCAN_TABLE_VALUES";
@@ -10,13 +9,9 @@ export const DISCARD_TABLE_VALUES =
 export const rescanTableFieldValues = createThunkAction(
   RESCAN_TABLE_VALUES,
   function (tableId) {
-    return async function (dispatch, getState) {
+    return async function () {
       try {
         const call = await MetabaseApi.table_rescan_values({ tableId });
-        MetabaseAnalytics.trackStructEvent(
-          "Data Model",
-          "Manual Re-scan Field Values for Table",
-        );
         return call;
       } catch (error) {
         console.error("error manually re-scanning field values", error);
@@ -28,13 +23,9 @@ export const rescanTableFieldValues = createThunkAction(
 export const discardTableFieldValues = createThunkAction(
   DISCARD_TABLE_VALUES,
   function (tableId) {
-    return async function (dispatch, getState) {
+    return async function () {
       try {
         const call = await MetabaseApi.table_discard_values({ tableId });
-        MetabaseAnalytics.trackStructEvent(
-          "Data Model",
-          "Manual Discard Field Values for Table",
-        );
         return call;
       } catch (error) {
         console.error("error discarding field values", error);
