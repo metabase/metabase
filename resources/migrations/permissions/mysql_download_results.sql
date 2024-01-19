836af2327d73d776f71b96d7382160883423e8df
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2024-01-19 16:56:39 -0500

    Namespace permission types (#37934)

--- resources/migrations/permissions/mysql_download_results.sql
+++ resources/migrations/permissions/mysql_download_results.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO data_permissions (group_id, perm_type, db_id, schema_name, table_id, perm_value)
 SELECT pg.id AS group_id,
-       'download-results' AS perm_type,
+       'perms/download-results' AS perm_type,
        md.id AS db_id,
        NULL AS schema_name,
        NULL AS table_id,
@@ -34,7 +34,7 @@ WHERE pg.name != 'Administrators'
      FROM data_permissions dp
      WHERE dp.group_id = pg.id
        AND dp.db_id = md.id
-       AND dp.perm_type = 'download-results')
+       AND dp.perm_type = 'perms/download-results')
   AND CASE
           WHEN EXISTS
                  (SELECT 1
@@ -65,7 +65,7 @@ WITH escaped_schema_table AS (
     FROM metabase_table mt
 )
 SELECT pg.id AS group_id,
-       'download-results' AS perm_type,
+       'perms/download-results' AS perm_type,
        mt.db_id,
        mt.schema AS schema_name,
        mt.id AS table_id,
@@ -93,11 +93,11 @@ WHERE pg.name != 'Administrators'
      WHERE dp.group_id = pg.id
        AND dp.db_id = mt.db_id
        AND dp.table_id = mt.id
-       AND dp.perm_type = 'download-results')
+       AND dp.perm_type = 'perms/download-results')
   AND NOT EXISTS
     (SELECT 1
      FROM data_permissions dp
      WHERE dp.group_id = pg.id
        AND dp.db_id = mt.db_id
        AND dp.table_id IS NULL
-       AND dp.perm_type = 'download-results');
+       AND dp.perm_type = 'perms/download-results');
