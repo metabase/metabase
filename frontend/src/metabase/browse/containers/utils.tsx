cacb6649a75257d2d064cf37095f81c18f263ed2
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-19 07:22:04 -0500

    Fix width of virtualized grid

--- frontend/src/metabase/browse/containers/utils.tsx
+++ frontend/src/metabase/browse/containers/utils.tsx
@@ -1,8 +1,9 @@
+/** This function is in a separate file so it can be straightforwardly mocked in tests */
 export const getPageWidth = (
   contentViewport: HTMLElement,
   gridGapSize: number,
 ) => {
-  const browseAppRoot = contentViewport?.children?.[0];
-  const width = browseAppRoot?.clientWidth - gridGapSize;
+  const browseContainer = contentViewport?.children?.[0].children?.[0];
+  const width = (browseContainer?.clientWidth ?? 500) - gridGapSize;
   return width;
 };
