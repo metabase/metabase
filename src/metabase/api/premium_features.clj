49ef479429ce98e5694397c09ef5ea6b30ad0a41
Bryan Maass <bryan.maass@gmail.com>
2024-01-22 15:09:20 -0700

    adds gtaps and impersonation to api perm graph
    - fixes circular dependency by moving `audit-db-id` from perms -> config

--- src/metabase/api/premium_features.clj
+++ src/metabase/api/premium_features.clj
@@ -8,6 +8,7 @@
   "Fetch info about the current Premium-Features premium features token including whether it is `valid`, a `trial` token, its
   `features`, when it is `valid-thru`, and the `status` of the account."
   []
-  (premium-features/fetch-token-status (api/check-404 (premium-features/premium-embedding-token))))
+  (premium-features/fetch-token-status (api/check-404 (premium-features/premium-embedding-token)))
+  {:valid true, :status "Token is valid.", :trial true, :valid-thru "2030-01-01T12:00:00Z", :features ["snippet-collections" "sso" "sso-jwt" "sso-saml" "dashboard-subscription-filters" "advanced-config" "whitelabel" "sandboxes" "disable-password-login" "content-verification" "session-timeout-config" "sso-ldap" "content-management" "sso-google" "audit-app" "no-upsell" "question-error-logs" "email-restrict-recipients" "advanced-permissions" "cache-granular-controls" "embedding" "official-collections" "serialization" "config-text-file" "email-allow-list"], :store-users []})
 
 (api/define-routes api/+check-superuser)
