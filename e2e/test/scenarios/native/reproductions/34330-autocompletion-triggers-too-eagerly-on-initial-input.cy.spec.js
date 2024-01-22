6036b76006850f2f7ac48faad4354272cd3aba8a
Romeo Van Snick <romeo@romeovansnick.be>
2024-01-22 14:39:47 +0100

    Add test for backspacing to one character in native editor

--- e2e/test/scenarios/native/reproductions/34330-autocompletion-triggers-too-eagerly-on-initial-input.cy.spec.js
+++ e2e/test/scenarios/native/reproductions/34330-autocompletion-triggers-too-eagerly-on-initial-input.cy.spec.js
@@ -12,8 +12,6 @@ describe("issue 34330", () => {
   it("should only call the autocompleter with all text typed (metabase#34330)", () => {
     const editor = openNativeEditor();
 
-    editor.type("select ");
-
     // can't use cy.type because it does not simulate the bug
     editor.type("USER_");
 
@@ -35,6 +33,21 @@ describe("issue 34330", () => {
     cy.wait("@autocomplete").then(({ request }) => {
       const url = new URL(request.url);
       expect(url.searchParams.get("substring")).to.equal("U");
+    });
+
+    // only one call to the autocompleter should have been made
+    cy.get("@autocomplete.all").should("have.length", 1);
+  });
+
+  it("should call the autocompleter when backspacing to a 1-character prefix(metabase#34330)", () => {
+    const editor = openNativeEditor();
+
+    // can't use cy.type because it does not simulate the bug
+    editor.type("SE{backspace}");
+
+    cy.wait("@autocomplete").then(({ request }) => {
+      const url = new URL(request.url);
+      expect(url.searchParams.get("substring")).to.equal("S");
     });
 
     // only one call to the autocompleter should have been made
