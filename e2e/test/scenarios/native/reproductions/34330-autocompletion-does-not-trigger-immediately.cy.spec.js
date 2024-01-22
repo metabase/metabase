3c42595ad59e13b02887557e153d3e696e6ff87f
Romeo Van Snick <romeo@romeovansnick.be>
2024-01-22 08:33:08 +0100

    Remove async from 2e2 tests

--- e2e/test/scenarios/native/reproductions/34330-autocompletion-does-not-trigger-immediately.cy.spec.js
+++ e2e/test/scenarios/native/reproductions/34330-autocompletion-does-not-trigger-immediately.cy.spec.js
@@ -9,7 +9,7 @@ describe("issue 34330", () => {
     );
   });
 
-  it("should only call the autocompleter with all text typed (metabase#34330)", async () => {
+  it("should only call the autocompleter with all text typed (metabase#34330)", () => {
     const editor = openNativeEditor();
 
     editor.type("select ");
@@ -27,7 +27,7 @@ describe("issue 34330", () => {
     cy.get("@autocomplete.all").should("have.length", 1);
   });
 
-  it("should call the autocompleter eventually, even when only 1 character was typed (metabase#34330)", async () => {
+  it("should call the autocompleter eventually, even when only 1 character was typed (metabase#34330)", () => {
     const editor = openNativeEditor();
 
     // can't use cy.type because it does not simulate the bug
