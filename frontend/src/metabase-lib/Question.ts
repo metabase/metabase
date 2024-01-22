fd3819f4e09769dde7e16118493fdd020d277600
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-01-22 21:07:53 +0100

    Make `databaseId` potentially nullable
    Resolves #38010

--- frontend/src/metabase-lib/Question.ts
+++ frontend/src/metabase-lib/Question.ts
@@ -1051,7 +1051,7 @@ class Question {
   }
 
   query(metadata = this._metadata): Query {
-    const databaseId = this.datasetQuery().database;
+    const databaseId = this.datasetQuery()?.database;
 
     // cache the metadata provider we create for our metadata.
     if (metadata === this._metadata) {
