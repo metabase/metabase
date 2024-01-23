c0ca6a098c7c131de5b7de995a49dce5cf6e744f
Chris Truter <chris@metabase.com>
2024-01-23 05:38:31 +0200

    Fix race condition when initializing side-uid for token checks

--- src/metabase/models/setting.clj
+++ src/metabase/models/setting.clj
@@ -511,6 +511,13 @@
                 (db-value setting)
                 (core/get cache (setting-name setting-definition-or-name))))))))))
 
+(defn- underlying-setter
+  "In the case of a read-only setting, this gives raw access to write a new value. Useful for initialization."
+  [{:keys [setter] setting-type :type :as setting}]
+  (if (= :none setter)
+    (partial set-value-of-type! setting-type setting)
+    setter))
+
 (defonce ^:private ^ReentrantLock init-lock (ReentrantLock.))
 
 (defn- init! [setting-definition-or-name]
@@ -527,7 +534,11 @@
             (read-setting setting)
             (when init
               (when-let [init-value (init)]
-                (metabase.models.setting/set! setting init-value :bypass-read-only? true))))
+                ;; We use the underlying setter to avoid triggering the audit logs.
+                ;; This is currently needed to avoid a deadlock when the database is first initialized via a config file.
+                (let [setter (underlying-setter setting)]
+                  (setter init-value))
+                #_(metabase.models.setting/set! setting init-value :bypass-read-only? true))))
           (finally
             (.unlock init-lock)))))))
 
@@ -878,9 +889,9 @@
 
 (defn- set-with-audit-logging!
   "Calls the setting's setter with `new-value`, and then writes the change to the `audit_log` table if necessary."
-  [{:keys [getter audit setter] setting-type :type :as setting} new-value bypass-read-only?]
-  (let [setter (if (and bypass-read-only? (= :none setter))
-                 (partial set-value-of-type! setting-type setting)
+  [{:keys [getter audit setter] :as setting} new-value bypass-read-only?]
+  (let [setter (if bypass-read-only?
+                 (underlying-setter setting)
                  setter)]
     (if (should-audit? setting)
       (let [audit-value-fn #(condp = audit
