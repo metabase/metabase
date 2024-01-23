e9296a2a5d2dc5dbb15ddb7d1223a0bd2c611e12
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2024-01-23 14:29:15 -0500

    data fill

--- frontend/src/metabase/visualizations/components/TableInteractive/TableInteractive.tsx
+++ frontend/src/metabase/visualizations/components/TableInteractive/TableInteractive.tsx
@@ -1,13 +1,35 @@
 import "react-data-grid/lib/styles.css";
 import DataGrid from "react-data-grid";
 import type { FunctionComponent } from "react";
+import type { Row } from "react-data-grid";
 
 type TableInteractiveProps = {
   data: { rows: Array<any>; cols: Array<any> };
 };
+
+const defaultRenderer = (row: Row) => {
+  return <div>{row.row[row.column.idx]}</div>;
+};
+
+const prepareColumns = (columns: Array<any>) => {
+  return columns.map((col: any) => {
+    return {
+      ...col,
+      resizable: true,
+      key: `${col.name}-${col.field}`,
+      width: "max-content",
+      name: col.display_name,
+      frozen: false,
+      renderCell: defaultRenderer,
+    };
+  });
+};
+
 export const TableInteractive: FunctionComponent<TableInteractiveProps> = ({
   data,
 }) => {
   const { rows, cols } = data;
-  return <DataGrid columns={cols} rows={rows} />;
+
+  const columns = prepareColumns(cols);
+  return <DataGrid columns={columns} rows={rows} />;
 };
