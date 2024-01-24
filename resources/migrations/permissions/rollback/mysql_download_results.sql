1c588aa654265444fd458001ca6e42281d1fb596
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2024-01-24 11:11:18 -0500

    Add rollback migration for permissions (#38023)

---
+++ resources/migrations/permissions/rollback/mysql_download_results.sql
@@ -0,0 +1,72 @@
+DELETE FROM permissions WHERE object LIKE '/download/db/%';
+
+INSERT INTO permissions (object, group_id)
+SELECT concat('/download/db/', dp.db_id, '/'),
+dp.group_id
+FROM data_permissions dp
+WHERE dp.perm_type = 'perms/download-results'
+AND dp.perm_value = 'one-million-rows'
+AND dp.table_id IS NULL;
+
+INSERT INTO permissions (object, group_id)
+SELECT concat('/download/limited/db/', dp.db_id, '/'),
+dp.group_id
+FROM data_permissions dp
+WHERE dp.perm_type = 'perms/download-results'
+AND dp.perm_value = 'ten-thousand-rows'
+AND dp.table_id IS NULL;
+
+INSERT INTO permissions (object, group_id)
+SELECT concat('/download/db/',
+              dp.db_id,
+              '/schema/',
+              replace(replace(dp.schema_name, '\\', '\\\\'), '/', '\\/'),
+              '/table/',
+              dp.table_id,
+              '/'),
+       dp.group_id
+FROM data_permissions dp
+WHERE dp.perm_type = 'perms/download-results'
+  AND dp.perm_value = 'one-million-rows'
+AND dp.table_id IS NOT NULL;
+
+INSERT INTO permissions (object, group_id)
+SELECT concat('/download/limited/db/',
+              dp.db_id,
+              '/schema/',
+              replace(replace(dp.schema_name, '\\', '\\\\'), '/', '\\/'),
+              '/table/',
+              dp.table_id,
+              '/'),
+       dp.group_id
+FROM data_permissions dp
+WHERE dp.perm_type = 'perms/download-results'
+  AND dp.perm_value = 'ten-thousand-rows'
+AND dp.table_id IS NOT NULL;
+
+INSERT INTO permissions (object, group_id)
+SELECT
+  concat('/download/limited/db/',  dp.db_id,  '/native/'),
+  dp.group_id
+FROM data_permissions dp
+WHERE dp.perm_type = 'perms/download-results'
+  AND dp.perm_value IN ('ten-thousand-rows', 'one-million-rows')
+  AND NOT EXISTS (
+      -- Check for absence of 'no' permission at table level for the same db_id and group_id
+      SELECT 1
+      FROM data_permissions sub_dp
+      WHERE sub_dp.db_id = dp.db_id
+        AND sub_dp.group_id = dp.group_id
+        AND sub_dp.perm_value = 'no'
+        AND sub_dp.perm_type = 'perms/download-results'
+  )
+  AND NOT EXISTS (
+      -- Check for absence of DB-level permission
+      SELECT 1
+      FROM data_permissions sub_dp
+      WHERE sub_dp.db_id = dp.db_id
+        AND sub_dp.group_id = dp.group_id
+        AND sub_dp.table_id IS NULL
+        AND sub_dp.perm_type = 'perms/download-results'
+  )
+GROUP BY dp.db_id, dp.group_id;
