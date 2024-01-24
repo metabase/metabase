ac0e2b5d7d40f81130961fd7e304711330b737d9
Alexander Solovyov <alexander@solovyov.net>
2024-01-24 12:13:00 +0200

    move check for active table inside the join

--- src/metabase/api/database.clj
+++ src/metabase/api/database.clj
@@ -551,11 +551,11 @@
              :%lower.metabase_field/name     [:like (u/lower-case-en search-string)]
              :metabase_field.visibility_type [:not-in ["sensitive" "retired"]]
              :table.db_id                    db-id
-             :table.active                   true
-             {:order-by  [[[:lower :metabase_field.name] :asc]
-                          [[:lower :table.name] :asc]]
-              :inner-join [[:metabase_table :table] [:= :table.id :metabase_field.table_id]]
-              :limit     limit}))
+             {:order-by   [[[:lower :metabase_field.name] :asc]
+                           [[:lower :table.name] :asc]]
+              :inner-join [[:metabase_table :table] [:and :table.active
+                                                     [:= :table.id :metabase_field.table_id]]]
+              :limit      limit}))
 
 (defn- autocomplete-results [tables fields limit]
   (let [tbl-count   (count tables)
