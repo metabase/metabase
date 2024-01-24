5a909c6e7c5acc237403da49ced17f2706ffbcdd
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-24 15:57:28 -0500

    Move LastEdited to separate component

--- frontend/src/metabase/browse/components/BrowseModels.tsx
+++ frontend/src/metabase/browse/components/BrowseModels.tsx
@@ -1,6 +1,6 @@
 import _ from "underscore";
 import cx from "classnames";
-import { c, t } from "ttag";
+import { t } from "ttag";
 import dayjs from "dayjs";
 import updateLocale from "dayjs/plugin/updateLocale";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -13,8 +13,7 @@ import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 
 import type { useSearchListQuery } from "metabase/common/hooks";
 
-import { Box, Group, Icon, Text, Title, Tooltip } from "metabase/ui";
-import { formatDateTimeWithUnit } from "metabase/lib/formatting";
+import { Box, Group, Icon, Text, Title } from "metabase/ui";
 import { sortModels } from "metabase/browse/utils";
 import NoResults from "assets/img/no_results.svg";
 import { useSelector } from "metabase/lib/redux";
@@ -27,66 +26,10 @@ import {
   ModelCard,
   MultilineEllipsified,
 } from "./BrowseModels.styled";
+import { LastEdited } from "./LastEdited";
 
 dayjs.extend(updateLocale);
 dayjs.extend(relativeTime);
-
-// TODO: Move to separate file, along with LastEdited
-// Move localization to separate PR
-const giveContext = (unit: string) =>
-  c(
-    `Abbreviation for "{0} ${unit}(s)". Keep abbreviations distinct from one another.`,
-  );
-
-let relativeTimeConfig: Record<string, unknown> = {
-  // The following line means: Take the translation of the string "{0}min".
-  // Substitute "%d" for the number. Tell dayjs to use that string when
-  // describing recent dates. For example, in English, the string would
-  // be "%ds". So, if theDate is a Dayjs date that is 5 minutes in the
-  // past, theDate.fromNow will return "5min".
-  // In Swahili, "5min" is "5 dk". "{0}min" translates to "{0} dk".
-  // So "%s dk" will be the string provided to Dayjs.fromNow for
-  // describing dates that are mere minutes in the past.
-  // Given a date 30 minutes in the past, it will return "30 dk".
-  m: giveContext("minute").t`${"%d"}min`,
-  h: giveContext("hour").t`${"%d"}h`,
-  d: giveContext("day").t`${"%d"}d`,
-  M: giveContext("month").t`${"%d"}mo`,
-  y: giveContext("year").t`${"%d"}yr`,
-  // For any number of seconds, just show 1min
-  s: () => giveContext("minute").t`${1}min`,
-  // Don't use "ago"
-  past: "%s",
-  // For the edge case where a model's last-edit date is somehow in the future
-  future: c("{0} is a period of time such as '5 minutes' or '5 months'")
-    .t`${"%s"} from now`,
-};
-
-// Use the same abbreviations for singular and plural
-relativeTimeConfig = {
-  ...relativeTimeConfig,
-  mm: relativeTimeConfig.m,
-  hh: relativeTimeConfig.h,
-  dd: relativeTimeConfig.d,
-  MM: relativeTimeConfig.M,
-  yy: relativeTimeConfig.y,
-};
-
-dayjs.updateLocale(dayjs.locale(), { relativeTime: relativeTimeConfig });
-
-// TODO: Check if this is required:
-// // Use a different dayjs instance to avoid polluting the global one
-// const dayjsWithAbbrevs = dayjs.extend((_, { instance }) => {
-//   return {
-//     updateLocale(localeName, config) {
-//       const locale = (instance.Ls[localeName] = {
-//         ...instance.Ls[localeName],
-//         ...config,
-//       });
-//       return locale;
-//     },
-//   };
-// });
 
 const emptyArray: SearchResult[] = [];
 
@@ -220,48 +163,6 @@ const ModelCell = ({ model, style, collectionHtmlId }: ModelCellProps) => {
         />
       </ModelCard>
     </Link>
-  );
-};
-
-const getHowLongAgo = (timestamp: string) => {
-  const date = dayjs(timestamp);
-  if (timestamp && date.isValid()) {
-    return date.fromNow();
-  } else {
-    return t`(invalid date)`;
-  }
-};
-
-const LastEdited = ({
-  lastEditorFullName,
-  timestamp,
-}: {
-  lastEditorFullName: string | null;
-  timestamp: string;
-}) => {
-  const howLongAgo = getHowLongAgo(timestamp);
-  const timeLabel = timestamp ? getHowLongAgo(timestamp) : "";
-  const formattedDate = formatDateTimeWithUnit(timestamp, "day", {});
-  const time = (
-    <time key="time" dateTime={timestamp}>
-      {formattedDate}
-    </time>
-  );
-  const tooltipLabel = c(
-    "{0} is the full name (or if this is unavailable, the email address) of the last person who edited a model. {1} is a phrase like '5 months ago'",
-  ).jt`Last edited by ${lastEditorFullName}${(<br key="br" />)}${time}`;
-  return (
-    <Tooltip label={tooltipLabel} withArrow disabled={!timeLabel}>
-      <Text role="note" size="small">
-        {lastEditorFullName}
-        {lastEditorFullName && howLongAgo && (
-          <Text span px=".33rem">
-            •
-          </Text>
-        )}
-        {howLongAgo}
-      </Text>
-    </Tooltip>
   );
 };
 
