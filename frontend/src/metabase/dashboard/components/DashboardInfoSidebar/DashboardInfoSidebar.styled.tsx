38e3473b5c04709f94da5383b6f22e6761327ef8
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2024-01-24 15:59:54 -0500

    polish UI for dashboard description suggestion

--- frontend/src/metabase/dashboard/components/DashboardInfoSidebar/DashboardInfoSidebar.styled.tsx
+++ frontend/src/metabase/dashboard/components/DashboardInfoSidebar/DashboardInfoSidebar.styled.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 import EditableText from "metabase/core/components/EditableText";
 import FormField from "metabase/core/components/FormField/FormField";
@@ -59,4 +60,39 @@ export const ContentSection = styled.div`
 
 export const DescriptionHeader = styled.h3`
   margin-bottom: 0.5rem;
+`;
+
+export const StyledEditableText = styled(EditableText)`
+  ${({ loading }) =>
+    loading &&
+    css`
+      border-radius: 0.5rem;
+      --border-size: 2px;
+      --border-angle: 0turn;
+      background-image: conic-gradient(from var(--border-angle), #fff, #fff),
+        conic-gradient(
+          from var(--border-angle),
+          transparent 5%,
+          ${color("brand")},
+          ${color("brand")}
+        );
+      background-size: calc(100% - (var(--border-size) * 2))
+          calc(100% - (var(--border-size) * 2)),
+        cover;
+      background-position: center center;
+      background-repeat: no-repeat;
+
+      animation: bg-spin 2s linear infinite;
+      @keyframes bg-spin {
+        to {
+          --border-angle: 1turn;
+        }
+      }
+
+      @property --border-angle {
+        syntax: "<angle>";
+        inherits: true;
+        initial-value: 0turn;
+      }
+    `}
 `;
