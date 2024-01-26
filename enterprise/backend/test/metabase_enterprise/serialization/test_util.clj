5121b3be36c15447ce2523e6f7a139b5d8e4d6f8
Chris Truter <chris@metabase.com>
2024-01-26 16:15:22 +0200

    Remove legacy macro

--- enterprise/backend/test/metabase_enterprise/serialization/test_util.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/test_util.clj
@@ -93,29 +93,6 @@
      (fn ~db-bindings
        ~@body)))
 
-(defmacro with-source-and-dest-dbs
-  "Creates and sets up two in-memory H2 application databases, a source database and an application database. For
-  testing load/dump/serialization stuff. To use the source DB, use [[with-source-db]], which makes binds it as the
-  current application database; [[with-dest-db]] binds the destination DB as the current application database."
-  {:style/indent 0 :deprecated "0.48.0"}
-  [& body]
-  ;; this is implemented by introducing the anaphors `&source-db` and `&dest-db` which are used by
-  ;; [[with-source-db]] and [[with-dest-db]]
-  `(with-dbs [~'&source-db ~'&dest-db]
-       ~@body))
-
-(defmacro with-source-db
-  "For use with [[with-source-and-dest-dbs]]. Makes the source DB the current application database."
-  {:style/indent 0 :deprecated "0.48.0"}
-  [& body]
-  `(fn [~'source-db] ~@body))
-
-(defmacro with-dest-db
-  "For use with [[with-source-and-dest-dbs]]. Makes the destination DB the current application database."
-  {:style/indent 0 :deprecated "0.48.0"}
-  [& body]
-  `(fn [~'&dest-db] ~@body))
-
 (defn random-dump-dir [prefix]
   (str (u.files/get-path (System/getProperty "java.io.tmpdir") prefix (mt/random-name))))
 
