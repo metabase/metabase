65e1f0c1068a30287118e1f5b92f0fd9a89907ea
Kamil Mielnik <kamil@kamilmielnik.com>
2024-01-26 21:34:31 +0700

    Revert "Remove Question.prototype.isDataset"
    This reverts commit eb152ddcc3a339b94005d69980b9321de8bdca70.

--- frontend/src/metabase-lib/Question.ts
+++ frontend/src/metabase-lib/Question.ts
@@ -266,6 +266,14 @@ class Question {
     return this.setCard(assoc(this.card(), "cache_ttl", cache));
   }
 
+  /**
+   * returns whether this question is a model
+   * @returns boolean
+   */
+  isDataset() {
+    return this._card && this._card.dataset;
+  }
+
   type(): CardType | undefined {
     return this._card && this._card.type;
   }
