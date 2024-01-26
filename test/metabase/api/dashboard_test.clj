db6c23b8fd11fe1ef1fa61b63e70186356cf04c7
Cam Saul <github@camsaul.com>
2024-01-26 21:50:25 +0000

    Chain filter search should check block perms

--- test/metabase/api/dashboard_test.clj
+++ test/metabase/api/dashboard_test.clj
@@ -2844,6 +2844,19 @@
                    (->> (chain-filter-values-url (:id dashboard) (:category-name param-keys))
                         (mt/user-http-request :rasta :get 403))))))))))
 
+(deftest block-data-should-not-expose-field-values-search
+  (testing "block data perms should not allow access to field values (private#196)"
+    (when config/ee-available?
+      (mt/with-premium-features #{:advanced-permissions}
+        (mt/with-temp-copy-of-db
+          (with-chain-filter-fixtures [{:keys [dashboard param-keys]}]
+            (perms/revoke-data-perms! (perms-group/all-users) (mt/id))
+            (perms/update-data-perms-graph! [(:id (perms-group/all-users)) (mt/id) :data]
+                                            {:schemas :block})
+            (is (= "You don't have permissions to do that."
+                   (->> (chain-filter-search-url (:id dashboard) (:category-name param-keys) "BBQ")
+                        (mt/user-http-request :rasta :get 403))))))))))
+
 (deftest dashboard-with-static-list-parameters-test
   (testing "A dashboard that has parameters that has static values"
     (with-chain-filter-fixtures [{:keys [dashboard param-keys]}]
