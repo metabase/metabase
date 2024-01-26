15df6340bec92a1ee998903bde199ebffd35f915
Romeo Van Snick <romeo@romeovansnick.be>
2024-01-26 16:29:49 +0100

    Test actual vizualisation results instead of implementation detauls of dataset

--- e2e/test/scenarios/native/reproductions/16584-run-selected-text-does-not-preserve-parameter-values.cy.spec.ts
+++ e2e/test/scenarios/native/reproductions/16584-run-selected-text-does-not-preserve-parameter-values.cy.spec.ts
@@ -4,7 +4,6 @@ describe("issue 16584", () => {
   beforeEach(() => {
     restore();
     cy.signInAsNormalUser();
-    cy.intercept("POST", "/api/dataset").as("dataset");
   });
 
   it("should pass parameters when running with 'Run select text' (metabase#16584)", () => {
@@ -13,19 +12,21 @@ describe("issue 16584", () => {
     // - the issue is unrelated to using a date filter, using a text filter works too
     // - the issue is unrelated to whether or not the parameter is required or if default value is set
     openNativeEditor()
-      .type("SELECT * FROM ACCOUNTS WHERE COUNTRY = {{ country }} LIMIT 10", {
-        parseSpecialCharSequences: false,
-        delay: 0,
-      })
+      .type(
+        "SELECT COUNTRY FROM ACCOUNTS WHERE COUNTRY = {{ country }} LIMIT 1",
+        {
+          parseSpecialCharSequences: false,
+          delay: 0,
+        },
+      )
       .type("{selectAll}");
 
     cy.findByPlaceholderText("Country").type("NL", { delay: 0 });
 
     runNativeQuery();
 
-    cy.wait("@dataset").then(({ request }) => {
-      const { body } = request;
-      expect(body.parameters[0].value).to.equal("NL");
+    cy.get(".Visualization").within(() => {
+      cy.findByText("NL").should("exist");
     });
   });
 });
