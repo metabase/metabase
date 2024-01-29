859efc8b3cd76810a46cd6b5ecb1989236a632ac
Kamil Mielnik <kamil@kamilmielnik.com>
2024-01-29 19:06:05 +0700

    Add a test case for simple queries

--- frontend/src/metabase-lib/display.unit.spec.ts
+++ frontend/src/metabase-lib/display.unit.spec.ts
@@ -4,16 +4,26 @@ import { getDefaultDisplay } from "./display";
 import { SAMPLE_DATABASE, createQuery } from "./test-helpers";
 
 describe("getDefaultDisplay", () => {
-  it("returns 'table' display for native queries", () => {
-    const nativeQuery: NativeDatasetQuery = {
-      database: SAMPLE_DATABASE.id,
-      type: "native",
-      native: {
-        query: "select 1",
-      },
-    };
-    const query = createQuery({ query: nativeQuery });
+  describe("native queries", () => {
+    it("returns 'table' display for native queries", () => {
+      const nativeQuery: NativeDatasetQuery = {
+        database: SAMPLE_DATABASE.id,
+        type: "native",
+        native: {
+          query: "select 1",
+        },
+      };
+      const query = createQuery({ query: nativeQuery });
 
-    expect(getDefaultDisplay(query)).toEqual({ display: "table" });
+      expect(getDefaultDisplay(query)).toEqual({ display: "table" });
+    });
+  });
+
+  describe("structured queries", () => {
+    it("returns 'table' display for queries without aggregations and breakouts", () => {
+      const query = createQuery();
+
+      expect(getDefaultDisplay(query)).toEqual({ display: "table" });
+    });
   });
 });
