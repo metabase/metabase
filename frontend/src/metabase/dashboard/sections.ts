9541dfbacae6ef7229d133303ffe799af0218283
Anton Kulyk <kuliks.anton@gmail.com>
2024-01-31 18:19:52 +0000

    Update layout names, add "KPI grid"

--- frontend/src/metabase/dashboard/sections.ts
+++ frontend/src/metabase/dashboard/sections.ts
@@ -1,3 +1,4 @@
+import { t } from "ttag";
 import { GRID_WIDTH } from "metabase/lib/dashboard_grid";
 import type { BaseDashboardCard } from "metabase-types/api";
 import {
@@ -110,15 +111,64 @@ export function layout2({ col, row }: LayoutOpts) {
   return [heading, ...scalarCards, largeCard];
 }
 
+function layout3({ col, row }: LayoutOpts) {
+  const heading = createHeadingDashcard({
+    col,
+    row,
+    size_x: GRID_WIDTH,
+    size_y: HEADING_HEIGHT,
+  });
+
+  const nextRow = row + HEADING_HEIGHT;
+
+  const scalarCardWidth = GRID_WIDTH / 2;
+  const scalarCardHeight = 5;
+
+  const scalarCards = [
+    createPlaceholderDashCard({
+      col: 0,
+      row: nextRow,
+      size_x: scalarCardWidth,
+      size_y: scalarCardHeight,
+    }),
+    createPlaceholderDashCard({
+      col: scalarCardWidth,
+      row: nextRow,
+      size_x: scalarCardWidth,
+      size_y: scalarCardHeight,
+    }),
+
+    createPlaceholderDashCard({
+      col: 0,
+      row: nextRow + scalarCardHeight,
+      size_x: scalarCardWidth,
+      size_y: scalarCardHeight,
+    }),
+    createPlaceholderDashCard({
+      col: scalarCardWidth,
+      row: nextRow + scalarCardHeight,
+      size_x: scalarCardWidth,
+      size_y: scalarCardHeight,
+    }),
+  ];
+
+  return [heading, ...scalarCards];
+}
+
 export const layoutOptions: LayoutOption[] = [
   {
     id: 1,
-    label: "Layout 1",
+    label: t`Large chart + KPIs to the right`,
     getLayout: layout1,
   },
   {
     id: 2,
-    label: "Layout 2",
+    label: t`Large chart w/ KPIs above`,
     getLayout: layout2,
+  },
+  {
+    id: 3,
+    label: t`KPI grid`,
+    getLayout: layout3,
   },
 ];
