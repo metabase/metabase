95a6b5847b46536b80bb02b4f030eb1ddbaffba6
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-31 15:14:20 -0500

    wip

---
+++ frontend/src/metabase/palette/components/Palette.styled.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+import { Button, Icon, TextInput } from "metabase/ui";
+import Modal from "metabase/components/Modal";
+
+export const PaletteModal = styled(Modal)`
+  // Stolen from Github
+  position: fixed;
+  margin: 10vh auto;
+  top: 0;
+  z-index: 999;
+  max-height: 80vh;
+  max-width: 90vw;
+  width: 448px;
+  overflow: auto;
+  box-shadow: 0 1px 0.25rem 0 rgba(0, 0, 0, 0.06);
+  border-radius: 0.25rem;
+  min-height: 50vh;
+  // max-height: max(50vh, 570px);
+  padding: 1rem;
+  display: flex;
+
+  // Is this useful? I got it from the palette on notion
+  transform: translate3d(0px, 0px, 0px);
+
+  // fix later
+  & .ModalBody {
+    padding: 0;
+  }
+  button {
+    display: flex;
+    flex-flow: row nowrap;
+  }
+`;
+
+export const PaletteItemDisplay = styled.li`
+  list-style: none;
+  display: flex;
+  padding: 0.5rem;
+`;
+
+export const PaletteInput = styled(TextInput)``;
+
+export const PaletteResultIcon = styled(Icon)`
+  margin-right: 0.5rem;
+`;
+
+export const PaletteResultButton = styled(Button)`
+  // fix later
+  & span {
+    display: flex;
+    align-items: center;
+  }
+`;
