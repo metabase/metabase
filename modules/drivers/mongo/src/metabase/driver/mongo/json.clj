50c889baab029d5554298d09b2cdaf260e356a13
lbrdnk <lbrdnk@users.noreply.github.com>
2024-01-31 18:19:25 +0100

    Cleanup json namespace

--- modules/drivers/mongo/src/metabase/driver/mongo/json.clj
+++ modules/drivers/mongo/src/metabase/driver/mongo/json.clj
@@ -2,22 +2,9 @@
   "This namespace adds mongo specific type encoders to `cheshire`. It is copy of the relevant part of monger's `json`
    namespace.
 
-   TODO !!!! proper docstring that makes sense.
-
-   At the time of writing we are using `cheshire` directly in mongo driver only at 2 places. Running existing tests
-   without those encoders in place yields no failures. Even though, I'm adding encoders to avoid surprises. We should
-   consider removing those completely as part of further refactor.
-   
-   Further rethinking way how we handle mongos serialized
-   With eg. ObjectId parsing we are loosing information here -- ObjectId effectively becomes string.
-
-   Were we using EJSON v1 shell mode, we could generate and parse ObjectId as found in mongo shell. That would require
-   rethink how we work with parameters.
-
-   Parameters in native mongo query as eg `{{x}}` violate json format. So to be able to 
-
-   Refactor? bson package provides functionality to parse EJSON v1. (https://www.mongodb.com/docs/manual/reference/mongodb-extended-json-v1/).
-   Parsing EJSON , we would be "
+   TODO: I believe this namespace should be completely removed. Trying to run tests without those mongo specific
+         encoders yield no failures. Unfortunately I was unable to prove it is not needed yet, hence I'm leaving it
+         in just to be safe. Removal should be considered during further refactoring of our mongo driver."
   (:require
    [cheshire.generate])
   (:import
