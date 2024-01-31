c69b2c6c4675662c6547ff67605cb511c1bb0848
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-31 09:25:30 -0500

    Remove files in browse/containers

---
+++ frontend/src/metabase/browse/containers/TableBrowser/TableBrowser.jsx
@@ -0,0 +1,56 @@
+import { connect } from "react-redux";
+import _ from "underscore";
+import * as Urls from "metabase/lib/urls";
+import { isSyncInProgress } from "metabase/lib/syncing";
+import Tables from "metabase/entities/tables";
+import { getMetadata } from "metabase/selectors/metadata";
+import { getSetting } from "metabase/selectors/settings";
+import { SAVED_QUESTIONS_VIRTUAL_DB_ID } from "metabase-lib/metadata/utils/saved-questions";
+import * as ML_Urls from "metabase-lib/urls";
+import { RELOAD_INTERVAL } from "../../constants";
+import TableBrowser from "../../components/TableBrowser";
+
+const getDatabaseId = (props, { includeVirtual } = {}) => {
+  const { params } = props;
+  const dbId =
+    parseInt(props.dbId) ||
+    parseInt(params.dbId) ||
+    Urls.extractEntityId(params.slug);
+
+  if (!Number.isSafeInteger(dbId)) {
+    return undefined;
+  } else if (dbId === SAVED_QUESTIONS_VIRTUAL_DB_ID && !includeVirtual) {
+    return undefined;
+  } else {
+    return dbId;
+  }
+};
+
+const getSchemaName = props => {
+  return props.schemaName || props.params.schemaName;
+};
+
+const getReloadInterval = (_state, _props, tables = []) =>
+  tables.some(t => isSyncInProgress(t)) ? RELOAD_INTERVAL : 0;
+
+const getTableUrl = (table, metadata) => {
+  const metadataTable = metadata?.table(table.id);
+  return ML_Urls.getUrl(metadataTable?.newQuestion(), { clean: false });
+};
+
+export default _.compose(
+  Tables.loadList({
+    query: (state, props) => ({
+      dbId: getDatabaseId(props, { includeVirtual: true }),
+      schemaName: getSchemaName(props),
+    }),
+    reloadInterval: getReloadInterval,
+  }),
+  connect((state, props) => ({
+    dbId: getDatabaseId(props, { includeVirtual: true }),
+    schemaName: getSchemaName(props),
+    metadata: getMetadata(state),
+    xraysEnabled: getSetting(state, "enable-xrays"),
+    getTableUrl,
+  })),
+)(TableBrowser);
