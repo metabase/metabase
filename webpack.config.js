6657c3cb754e3cb105b44596640158e3ee8ef0e4
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2024-01-31 09:54:11 -0500

    format native queries (#38113)
    * format native queries
    
    * optional clauses, mlv2, specs
    
    * remove unnecessary webpack chunk name directive
    
    * disable formatting for mssql and sqlite, address review comments

--- webpack.config.js
+++ webpack.config.js
@@ -199,9 +199,14 @@ const config = (module.exports = {
     splitChunks: {
       cacheGroups: {
         vendors: {
-          test: /[\\/]node_modules[\\/]/,
+          test: /[\\/]node_modules[\\/](?!sql-formatter[\\/])/,
           chunks: "all",
           name: "vendor",
+        },
+        sqlFormatter: {
+          test: /[\\/]node_modules[\\/]sql-formatter[\\/]/,
+          chunks: "all",
+          name: "sql-formatter",
         },
       },
     },
