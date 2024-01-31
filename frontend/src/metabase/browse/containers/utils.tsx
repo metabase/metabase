fcfbe8d08f5ea672bc44355646fc6e597c8e4487
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-01-31 09:24:17 -0500

    Remove files in browse/containers

--- frontend/src/metabase/browse/containers/utils.tsx
+++
@@ -1,9 +0,0 @@
-/** This function is in a separate file so it can be straightforwardly mocked in tests */
-export const getPageWidth = (
-  contentViewport: HTMLElement,
-  gridGapSize: number,
-) => {
-  const browseContainer = contentViewport?.children?.[0].children?.[0];
-  const width = (browseContainer?.clientWidth ?? 500) - gridGapSize;
-  return width;
-};
