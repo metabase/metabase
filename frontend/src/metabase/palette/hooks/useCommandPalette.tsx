5e6fafcbc8e6dbd3b45024b99336f6c795a545b1
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-01-31 15:49:46 -0500

    simmple hook

--- frontend/src/metabase/palette/hooks/useCommandPalette.tsx
+++ frontend/src/metabase/palette/hooks/useCommandPalette.tsx
@@ -1,5 +1,52 @@
+import { t } from "ttag";
+import { useMemo } from "react";
+import { push } from "react-router-redux";
 import { useDispatch } from "metabase/lib/redux";
+import type { IconName } from "metabase/ui";
+import { setOpenModal } from "metabase/redux/ui";
+import * as Urls from "metabase/lib/urls";
 
-export const useCommandPalette = ({ query }) => {
+type CommandPalletAction = {
+  name: string;
+  icon: IconName;
+  run: (arg?: string) => void;
+};
+
+export const useCommandPalette = ({ query }: { query: string }) => {
   const dispatch = useDispatch();
+
+  const defaultActions = useMemo<CommandPalletAction[]>(
+    () => [
+      {
+        name: t`Create New Collection`,
+        icon: "add",
+        run: () => dispatch(setOpenModal("collection")),
+      },
+      {
+        name: t`Create New Dashboard`,
+        icon: "add",
+        run: () => dispatch(setOpenModal("dashboard")),
+      },
+      {
+        name: t`Create New Question`,
+        icon: "add",
+        run: () =>
+          dispatch(
+            push(
+              Urls.newQuestion({
+                mode: "notebook",
+                creationType: "custom_question",
+              }),
+            ),
+          ),
+      },
+    ],
+    [dispatch],
+  );
+
+  return {
+    results: query
+      ? defaultActions.filter(action => action.name.includes(query))
+      : defaultActions,
+  };
 };
