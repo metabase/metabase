8c2d0a275dbf735e12bdd267b379a7d584d387f5
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-02-01 11:20:37 -0500

    adding settings search

--- frontend/src/metabase/palette/hooks/useCommandPalette.tsx
+++ frontend/src/metabase/palette/hooks/useCommandPalette.tsx
@@ -1,6 +1,7 @@
 import { jt, t } from "ttag";
+import _ from "underscore";
 import type { Dispatch, SetStateAction } from "react";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useEffect } from "react";
 import { push } from "react-router-redux";
 import {
   filterItems,
@@ -11,6 +12,8 @@ import { setOpenModal, closeModal } from "metabase/redux/ui";
 import * as Urls from "metabase/lib/urls";
 import { Icon } from "metabase/ui";
 import { getContextualPaletteActions } from "metabase/selectors/app";
+import { getSections } from "metabase/admin/settings/selectors";
+import { reloadSettings } from "metabase/admin/settings/settings";
 
 export type CommandPalettePageId = "root" | "admin_settings";
 
@@ -24,6 +27,40 @@ export const useCommandPalette = ({
   setQuery: Dispatch<SetStateAction<string>>;
 }) => {
   const dispatch = useDispatch();
+  const adminSections = useSelector(getSections);
+
+  useEffect(() => {
+    dispatch(reloadSettings());
+  }, [dispatch]);
+
+  const adminSectionsSearchMap = useMemo(
+    () =>
+      Object.keys(adminSections).reduce((memo, key) => {
+        const settings = adminSections[key].settings || [];
+        const path = `/admin/settings/${key}`;
+
+        return [
+          ...memo,
+          ...settings
+            .filter(s => s.display_name)
+            .map(s => ({
+              name: s.display_name || "",
+              description: s.description,
+              path,
+              displayName: `${key} > ${s.display_name}`,
+            })),
+        ];
+      }, []),
+    [adminSections],
+  );
+
+  const filteredAdmin = useMemo(
+    () =>
+      adminSectionsSearchMap.filter(x =>
+        x.name.toLowerCase().includes(query.toLowerCase()),
+      ),
+    [query, adminSectionsSearchMap],
+  );
 
   const openNewModal = useCallback(
     (modalId: string) => {
@@ -120,107 +157,31 @@ export const useCommandPalette = ({
     return actions;
   }, [query, dispatch, setPage, openNewModal, setQuery, contextualActions]);
 
-  const adminSettingsActions = useMemo<CommandPaletteActions>(
-    () => [
-      {
-        id: "admin_settings",
-        items: [
-          {
-            id: "back",
-            children: t`Back`,
-            icon: () => <Icon name="arrow_left" />,
-            closeOnSelect: false,
-            onClick: () => {
-              setPage("root");
-            },
+  const adminSettingsActions = [
+    {
+      id: "admin_settings",
+      items: [
+        {
+          id: "back",
+          children: t`Back`,
+          icon: () => <Icon name="arrow_left" />,
+          closeOnSelect: false,
+          onClick: () => {
+            setPage("root");
           },
-          {
-            id: "setup",
-            children: t`Setup`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/setup")),
-          },
-          {
-            id: "general",
-            children: t`General`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/general")),
-          },
-          {
-            id: "updates",
-            children: t`Updates`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/updates")),
-          },
-          {
-            id: "email",
-            children: t`Email`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/email")),
-          },
-          {
-            id: "slack",
-            children: t`Slack`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/slack")),
-          },
-          {
-            id: "authentication",
-            children: t`Authentication`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/authentication")),
-          },
-          {
-            id: "maps",
-            children: t`Maps`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/maps")),
-          },
-          {
-            id: "localization",
-            children: t`Localization`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/localization")),
-          },
-          {
-            id: "uploads",
-            children: t`Uploads`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/uploads")),
-          },
-          {
-            id: "public_sharing",
-            children: t`Public Sharing`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/public-sharing")),
-          },
-          {
-            id: "embedding",
-            children: t`Embedding`,
-            icon: () => <Icon name="gear" />,
-            onClick: () =>
-              dispatch(push("/admin/settings/embedding-in-other-applications")),
-          },
-          {
-            id: "license_and_billing",
-            children: t`License and Billing`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/license")),
-          },
-          {
-            id: "caching",
-            children: t`Caching`,
-            icon: () => <Icon name="gear" />,
-            onClick: () => dispatch(push("/admin/settings/caching")),
-          },
-        ],
-      },
-    ],
-    [dispatch, setPage],
-  );
+        },
+        ...filteredAdmin.map(s => ({
+          id: s.displayName,
+          children: s.displayName,
+          icon: () => <Icon name="gear" />,
+          onClick: () => dispatch(push(s.path)),
+        })),
+      ],
+    },
+  ];
 
   return {
     defaultActions: filterItems(defaultActions, query),
-    adminSettingsActions: filterItems(adminSettingsActions, query),
+    adminSettingsActions: adminSettingsActions,
   };
 };
