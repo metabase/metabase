ac062cabc85b404f190ee5f0d098b080c3fdac86
guo <memeyun2000@126.com>
2024-02-01 16:32:20 +0800

    update

--- bin/build.guo.sh
+++ bin/build.guo.sh
@@ -1,5 +1,4 @@
-#! /usr/bin/env bash
-
+#!/usr/bin/env bash
 set -euo pipefail
 
 # switch to project root directory if we're not already there
@@ -12,4 +11,5 @@ check_clojure_cli
 source "./bin/clear-outdated-cpcaches.sh"
 clear_outdated_cpcaches
 
-clojure -X:build:drivers:build/drivers $@
+clojure -X:drivers:build:build/all "$@"
+
