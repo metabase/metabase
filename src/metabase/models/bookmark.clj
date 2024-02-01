060eb68b89c485fb1f36d180a6caa4562fef1696
John Swanson <john.swanson@metabase.com>
2024-02-01 13:43:06 -0500

    change format for bookmarks

--- src/metabase/models/bookmark.clj
+++ src/metabase/models/bookmark.clj
@@ -38,9 +38,9 @@
   "Shape of a bookmark returned for user. Id is a string because it is a concatenation of the model and the model's
   id. This is required for the frontend entity loading system and does not refer to any particular bookmark id,
   although the compound key can be inferred from it."
-  [:map {:closed true}
-   [:id                               :string]
-   [:type                             [:enum "card" "collection" "dashboard"]]
+  [:map {:closed false}
+   [:id                               ms/PositiveInt]
+   [:model                            [:enum "card" "collection" "dashboard"]]
    [:item_id                          ms/PositiveInt]
    [:name                             ms/NonBlankString]
    [:authority_level {:optional true} [:maybe :string]]
@@ -48,21 +48,16 @@
    [:description     {:optional true} [:maybe :string]]
    [:display         {:optional true} [:maybe :string]]])
 
-(mu/defn ^:private normalize-bookmark-result :- BookmarkResult
+(mu/defn ^:private normalize-bookmark-result
   "Normalizes bookmark results. Bookmarks are left joined against the card, collection, and dashboard tables, but only
   points to one of them. Normalizes it so it has just the desired fields."
   [result]
-  (let [result            (cond-> (into {} (remove (comp nil? second) result))
-                            ;; If not a collection then remove collection properties
-                            ;; to avoid shadowing the "real" properties.
-                            (not= (:type result) "collection")
-                            (dissoc :collection.description :collection.name))
-        normalized-result (zipmap (map unqualify-key (keys result)) (vals result))
-        id-str            (str (:type normalized-result) "-" (:item_id normalized-result))]
-    (-> normalized-result
-        (select-keys [:item_id :type :name :dataset :description :display
-                      :authority_level])
-        (assoc :id id-str))))
+
+  (let [item (into {} (or (:card result) (:dashboard result) (:collection result)))
+        id-str            (str (:type result) "-" (:id item))]
+    (assoc item
+           :model (:type result)
+           :bookmark_id id-str)))
 
 (defn- bookmarks-union-query
   [user-id]
@@ -92,26 +87,48 @@
                   :from   [:collection_bookmark]
                   :where  [:= :user_id user-id]}]}))
 
-(mu/defn bookmarks-for-user :- [:sequential BookmarkResult]
+(defn- add-cards-to-bookmarks [bookmarks]
+  (when (seq bookmarks)
+    (let [card-ids (map :item_id (filter #(= (:type %) "card") bookmarks))
+          cards (when (seq card-ids)
+                  (t2/select :model/Card {:where [:in :id card-ids]}))
+          card-id->card (into {} (map (fn [card] [(:id card) card]) cards))]
+      (for [bookmark bookmarks]
+        (if (= (:type bookmark) "card")
+          (assoc bookmark :card (card-id->card (:item_id bookmark)))
+          bookmark)))))
+
+(defn- add-collections-to-bookmarks [bookmarks]
+  (when (seq bookmarks)
+    (let [collection-ids (map :item_id (filter #(= (:type %) "collection") bookmarks))
+          collections (when (seq collection-ids)
+                        (t2/select :model/Collection {:where [:in :id collection-ids]}))
+          collection-id->collection (into {} (map (fn [collection] [(:id collection) collection]) collections))]
+      (for [bookmark bookmarks]
+        (if (= (:type bookmark) "collection")
+          (assoc bookmark :collection (collection-id->collection (:item_id bookmark)))
+          bookmark)))))
+
+(defn- add-dashboards-to-bookmarks [bookmarks]
+  (when (seq bookmarks)
+    (let [dashboard-ids (map :item_id (filter #(= (:type %) "dashboard") bookmarks))
+          dashboards (when (seq dashboard-ids)
+                        (t2/select :model/Dashboard {:where [:in :id dashboard-ids]}))
+          dashboard-id->dashboard (into {} (map (fn [dashboard] [(:id dashboard) dashboard]) dashboards))]
+      (for [bookmark bookmarks]
+        (if (= (:type bookmark) "dashboard")
+          (assoc bookmark :dashboard (dashboard-id->dashboard (:item_id bookmark)))
+          bookmark)))))
+
+
+(mu/defn bookmarks-for-user
   "Get all bookmarks for a user. Each bookmark will have a string id made of the model and model-id, a type, and
   item_id, name, and description from the underlying bookmarked item."
   [user-id]
   (->> (mdb.query/query
         {:select    [[:bookmark.created_at        :created_at]
                      [:bookmark.type              :type]
-                     [:bookmark.item_id           :item_id]
-                     [:card.name                  (mdb.u/qualify Card :name)]
-                     [:card.dataset               (mdb.u/qualify Card :dataset)]
-                     [:card.display               (mdb.u/qualify Card :display)]
-                     [:card.description           (mdb.u/qualify Card :description)]
-                     [:card.archived              (mdb.u/qualify Card :archived)]
-                     [:dashboard.name             (mdb.u/qualify Dashboard :name)]
-                     [:dashboard.description      (mdb.u/qualify Dashboard :description)]
-                     [:dashboard.archived         (mdb.u/qualify Dashboard :archived)]
-                     [:collection.name            (mdb.u/qualify Collection  :name)]
-                     [:collection.authority_level (mdb.u/qualify Collection :authority_level)]
-                     [:collection.description     (mdb.u/qualify Collection :description)]
-                     [:collection.archived        (mdb.u/qualify Collection :archived)]]
+                     [:bookmark.item_id           :item_id]]
          :from      [[(bookmarks-union-query user-id) :bookmark]]
          :left-join [[:report_card :card]                    [:= :bookmark.card_id :card.id]
                      [:report_dashboard :dashboard]          [:= :bookmark.dashboard_id :dashboard.id]
@@ -132,7 +149,16 @@
                                                     (:postgres :h2) :asc-nulls-last
                                                     :mysql          :asc)]
                      [:created_at :desc]]})
+       (add-cards-to-bookmarks)
+       (add-collections-to-bookmarks)
+       (add-dashboards-to-bookmarks)
        (map normalize-bookmark-result)))
+
+;; type to model
+;; id = underlying item id
+;; bookmark_id = ... bookmark id
+;; bookmark order
+;; display
 
 (defn save-ordering!
   "Saves a bookmark ordering of shape `[{:type, :item_id}]`
