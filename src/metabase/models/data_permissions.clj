25e9ff3b37d64209ca9d93a710a2bfc4b7cb65e0
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2024-02-01 17:22:30 -0500

    Improve performance of adding new table perms (#38294)

--- src/metabase/models/data_permissions.clj
+++ src/metabase/models/data_permissions.clj
@@ -1,5 +1,6 @@
 (ns metabase.models.data-permissions
   (:require
+   [clojure.set :as set]
    [clojure.string :as str]
    [malli.core :as mc]
    [metabase.api.common :as api]
@@ -432,3 +433,42 @@
    perm-type   :- :keyword
    value       :- :keyword]
   (set-table-permissions! group-or-id perm-type {table-or-id value}))
+
+(mu/defn set-new-table-permissions!
+  "Sets permissions for a single table to the specified value in all the provided groups.
+  Does not update any groups where the permission is already set at the database level."
+  [groups-or-ids :- [:sequential TheIdable]
+   table-or-id   :- TheIdable
+   perm-type     :- :keyword
+   value         :- :keyword]
+  (when (not= :model/Table (model-by-perm-type perm-type))
+    (throw (ex-info (tru "Permission type {0} cannot be set on tables." perm-type)
+                    {perm-type (Permissions perm-type)})))
+  (when (= value :block)
+    (throw (ex-info (tru "Block permissions must be set at the database-level only.")
+                    {})))
+  (when (seq groups-or-ids)
+    (t2/with-transaction [_conn]
+      (let [group-ids         (map u/the-id groups-or-ids)
+            table             (if (map? table-or-id)
+                                table-or-id
+                                (t2/select-one [:model/Table :id :db_id :schema] :id table-or-id))
+            db-id             (:db_id table)
+            db-level-perms    (t2/select :model/DataPermissions
+                                         :perm_type (u/qualified-name perm-type)
+                                         :db_id     db-id
+                                         :table_id  nil
+                                         {:where [:in :group_id group-ids]})
+            db-level-group-ids (set (map :group_id db-level-perms))
+            granular-group-ids (set/difference (set group-ids) db-level-group-ids)
+            new-perms (map
+                       (fn [group-id]
+                         (let [{:keys [id db_id schema]} table]
+                           {:perm_type   perm-type
+                            :group_id    group-id
+                            :perm_value  value
+                            :db_id       db_id
+                            :table_id    id
+                            :schema_name schema}))
+                       granular-group-ids)]
+        (t2/insert! :model/DataPermissions new-perms)))))
