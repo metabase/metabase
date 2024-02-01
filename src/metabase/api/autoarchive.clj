c0ad0f96c211cd285bf7c3c97ec788b99ec94bbd
Bryan Maass <bryan.maass@gmail.com>
2024-02-01 09:39:31 -0500

    add query param time-ago + ignore audit cards

--- src/metabase/api/autoarchive.clj
+++ src/metabase/api/autoarchive.clj
@@ -1,12 +1,13 @@
 (ns metabase.api.autoarchive
   (:require
    [compojure.core :refer [DELETE GET POST PUT]]
+   [honey.sql :as sql]
    [metabase.api.common :as api]
+   [metabase.models.permissions :as perms]
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
 
-;; TODO
-;; (def ^:private archive-timing "6 months")
+(def ^:private time-ago #_"6 MONTHS" "5 MINUTES")
 
 ;; TODO
 ;; 1. only worry about orphaned questions
@@ -19,25 +20,57 @@
   (->> q
        ->last-used
        (into {})
-       ;; TODO:
+       ;; FIXME
        #_(select-keys [:id :name :last_used_at :model])))
 
-(defn- auto-archivable-questions []
-  (->> (t2/query {:select    [:rc.id
-                              :rc.name
-                              :rc.created_at
-                              [:vl.timestamp :last_viewed]
-                              [:vl.model :model]]
-                  :from      [[:report_card :rc]]
-                  :left-join [[:view_log :vl] [:= :rc.id :vl.model_id]
-                              [:report_dashboardcard :dc] [:= :rc.id :dc.card_id]]
-                  :where     [:and
-                              [:= :dc.dashboard_id nil]
-                              [:= :vl.model "card"]]})
+;; TODO
+;; - [X] audit,
+;; - [ ] official,
+;; - [ ] alerts,
+;; - [ ] question used by another question
+
+(defn query
+  "This query selects archiavable, orphaned cards, that have been created_at"
+  [{:keys [time-ago collection-id]}]
+  {:select-distinct [:card.id
+                     :card.name
+                     :card.created_at
+                     [:vlog.timestamp :last_viewed]
+                     [:vlog.model :model]]
+   :from      [[:report_card :card]]
+   :left-join [[:view_log :vlog] [:= :card.id :vlog.model_id]
+               [:report_dashboardcard :dc] [:= :card.id :dc.card_id]
+               [:collection :coll] [:= :card.collection_id :coll.id]]
+   :where     [:and
+               (when collection-id [:= :card.collection_id collection-id])
+               [:not [:= :coll.type "instance-analytics"]]
+               [:= :vlog.model "card"]
+               [:not [:= :card.database_id perms/audit-db-id]]
+               [:or
+                [:< :vlog.timestamp {:raw (format "NOW() - INTERVAL '%s'" time-ago)}]
+                [:and
+                 [:= :vlog.timestamp nil]
+                 [:< :card.created_at {:raw (format "NOW() - INTERVAL '%s'" time-ago)}]]]]})
+
+(defn- auto-archivable-questions [{:keys [collection-id time-ago]}]
+  (->> (t2/query (query {:time-ago time-ago
+                         :collection-id collection-id}))
        (mapv ->auto-archivable)))
 
-(api/defendpoint GET "/:collection-id" []
-  {:collection-id ms/PositiveInt}
-  {:archivable (auto-archivable-questions)})
+(api/defendpoint GET "/:collection-id" [collection-id :as {query-params :query-params}]
+  {collection-id [:maybe ms/PositiveInt]
+   query-params [:map [:time-ago {:optional true} :string]]}
+  (let [time-ago (or (:time-ago query-params) time-ago)]
+    {:archivable (auto-archivable-questions
+                  {:collection-id collection-id
+                   :time-ago time-ago})}))
 
 (api/define-routes)
+
+(comment
+
+  (t2/query (query {:time-ago time-ago :collection-id nil}))
+
+  (sql/format (query {:time-ago "5 minutes"}))
+
+  )
