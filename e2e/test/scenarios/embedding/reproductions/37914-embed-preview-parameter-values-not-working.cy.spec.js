8b848a82e4665cec4e3f3d8a6481236f4b2a10a5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-02-01 17:52:16 +0700

    Use an existing util

--- e2e/test/scenarios/embedding/reproductions/37914-embed-preview-parameter-values-not-working.cy.spec.js
+++ e2e/test/scenarios/embedding/reproductions/37914-embed-preview-parameter-values-not-working.cy.spec.js
@@ -5,6 +5,7 @@ import {
   addOrUpdateDashboardCard,
   dashboardHeader,
   modal,
+  getIframeBody,
 } from "e2e/support/helpers";
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 
@@ -142,32 +143,3 @@ describe("issue 37914", () => {
     });
   });
 });
-
-// Code grabbed from https://www.cypress.io/blog/2020/02/12/working-with-iframes-in-cypress
-const getIframeDocument = () => {
-  return (
-    cy
-      .get("iframe")
-      // Cypress yields jQuery element, which has the real
-      // DOM element under property "0".
-      // From the real DOM iframe element we can get
-      // the "document" element, it is stored in "contentDocument" property
-      // Cypress "its" command can access deep properties using dot notation
-      // https://on.cypress.io/its
-      .its("0.contentDocument")
-      .should("exist")
-  );
-};
-
-const getIframeBody = () => {
-  // get the document
-  return (
-    getIframeDocument()
-      // automatically retries until body is loaded
-      .its("body")
-      .should("not.be.undefined")
-      // wraps "body" DOM element to allow
-      // chaining more Cypress commands, like ".find(...)"
-      .then(cy.wrap)
-  );
-};
