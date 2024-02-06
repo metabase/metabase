de43676d9f89c67cc7cfb28c99a51b1bb42844ae
Chris Truter <chris@metabase.com>
2024-02-06 14:35:15 +0200

    Improve tests

--- test/metabase/db/util_test.clj
+++ test/metabase/db/util_test.clj
@@ -2,51 +2,53 @@
   (:require
    [clojure.test :refer [deftest testing is]]
    [metabase.db.util :as mdb.u]
-   [metabase.models.field :refer [Field]]
-   [metabase.models.field-values :refer [FieldValues]]
-   [metabase.util :as u]
+   [metabase.models.setting :refer [Setting]]
    [toucan2.core :as t2]))
 
 (set! *warn-on-reflection* true)
 
 (deftest idempotent-insert!-test
-  ;; We cannot use with-temp, as it starts its own transaction, which stops us setting the isolation level.
-  (let [field-name "test-field"
-        v-field-id (volatile! nil)]
-    (try
-      ;; ensure there is no database detritus
-      (t2/delete! Field :name field-name)
+  (let [columns [:key :value]]
+    (doseq [search-col columns]
+      (testing (format "Testing idempotent insertion where the search key %s a uniqueness constraint in the db"
+                       (if (= :key search-col) "has" "does not have"))
 
-      (let [threads    5
-            promises   (atom [])
-            field-args [:name field-name :base_type :type/Text :table_id 1 :database_type "TEXT"]
-            field-id   (u/prog1 (apply t2/insert-returning-pk! Field field-args) (vreset! v-field-id <>))
-            args       [:field_id field-id :type :linked-filter :hash_key "1234"]
-            thunk      (fn []
-                         (mdb.u/idempotent-insert!
-                           (:id (apply t2/select-one FieldValues args))
-                           ;; Pause to ensure multiple threads hit the mutating path
-                           (do (Thread/sleep 300)
-                               (apply t2/insert-returning-pk! FieldValues args))))]
+        ;; We cannot use with-temp, as it starts its own transaction, which stops us setting the isolation level.
+        (let [search-value (str (random-uuid))
+              other-col    (first (remove #{search-col} columns))]
+          (try
+            ;; ensure there is no database detritus
+            (t2/delete! Setting search-col search-value)
 
-        ;; hit it
-        (dotimes [_ threads]
-          (swap! promises conj (future (thunk))))
+            (let [threads  5
+                  promises (atom [])
+                  thunk    (fn []
+                             (mdb.u/idempotent-insert!
+                               (:key (t2/select-one Setting search-col search-value))
+                               ;; Pause to ensure multiple threads hit the mutating path
+                               (do (Thread/sleep 300)
+                                   (t2/insert-returning-pk! Setting
+                                                            search-col search-value
+                                                            other-col (str (random-uuid))))))]
 
-        ;; This assertion must come first so that be block on the promises
-        (testing "Every call returns the same row"
-          (let [id (:id (apply t2/select-one FieldValues args))]
-            (is (= (repeat threads id)
-                   (map (comp :id deref) @promises)))))
+              ;; hit it
+              (dotimes [_ threads]
+                (swap! promises conj (future (thunk))))
 
-        (testing "We have not inserted any duplicates"
-          (is (= 1 (count (apply t2/select FieldValues args)))))
+              ;; Block on all the futures
+              (doseq [p @promises] @p)
 
-        (testing "Later calls will just return the existing row as well"
-          (is (= (:id (apply t2/select-one FieldValues args)) (thunk))))
-        )
-      ;; Since we couldn't use with-temp, we need to clean up manually.
-      (finally
-        (when-let [field-id @v-field-id]
-          (t2/delete! FieldValues :field_id field-id)
-          (t2/delete! Field :id field-id))))))
+              (testing "Every call returns the same row"
+                (let [id (:key (t2/select-one Setting search-col search-value))]
+                  (is (= (repeat threads id)
+                         (map deref @promises)))))
+
+              (testing "We have not inserted any duplicates"
+                (is (= 1 (count (t2/select Setting search-col search-value)))))
+
+              (testing "Later calls will just return the existing row as well"
+                (is (= (:key (t2/select-one Setting search-col search-value)) (thunk)))))
+
+            ;; Since we couldn't use with-temp, we need to clean up manually.
+            (finally
+              (t2/delete! Setting search-col search-value))))))))
