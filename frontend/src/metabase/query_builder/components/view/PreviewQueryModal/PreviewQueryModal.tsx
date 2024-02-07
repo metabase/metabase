75e3bebf203b0c4ace33075c70a3c9381f97be76
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-02-07 16:48:28 +0700

    Fix ESLint

--- frontend/src/metabase/query_builder/components/view/PreviewQueryModal/PreviewQueryModal.tsx
+++ frontend/src/metabase/query_builder/components/view/PreviewQueryModal/PreviewQueryModal.tsx
@@ -46,7 +46,6 @@ const PreviewQueryModal = ({
 
 const mapStateToProps = (state: State) => ({
   // FIXME: remove the non-null assertion operator
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   question: getQuestion(state)!,
   onLoadQuery: getNativeQueryFn(state),
 });
