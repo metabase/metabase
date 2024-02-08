7f7278f879fca4a2b63407ae3ca2eefebd882a9b
Oleg Gromov <mail@oleggromov.com>
2024-02-08 15:35:12 +0000

    Fix text widget default required value

--- frontend/src/metabase/parameters/components/ParameterValueWidget.jsx
+++ frontend/src/metabase/parameters/components/ParameterValueWidget.jsx
@@ -284,7 +284,7 @@ function Widget({
     return (
       <TextWidget
         value={value}
-        setValue={setValue}
+        setValue={setValueOrDefault}
         className={className}
         isEditing={isEditing}
         commitImmediately={commitImmediately}
