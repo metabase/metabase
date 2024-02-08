b0f8a985b2e09ae184d65621320a9a899b0e3c38
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-02-08 15:27:02 +0100

    Add new route for `/metric/:id`

--- frontend/src/metabase/routes.jsx
+++ frontend/src/metabase/routes.jsx
@@ -224,6 +224,7 @@ export const getRoutes = store => {
             <IndexRoute component={QueryBuilder} />
             <Route path="notebook" component={QueryBuilder} />
             <Route path="query" component={QueryBuilder} />
+            <Route path=":slug" component={QueryBuilder} />
           </Route>
 
           <Route path="browse">
