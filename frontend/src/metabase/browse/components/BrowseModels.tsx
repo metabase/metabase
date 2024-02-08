f094a6d366b6feed467d9414b5ae384a72f6ba67
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-02-08 15:27:46 -0500

    Reset change

--- frontend/src/metabase/browse/components/BrowseModels.tsx
+++ frontend/src/metabase/browse/components/BrowseModels.tsx
@@ -201,7 +201,7 @@ const ModelCell = ({ model, collectionHtmlId }: ModelCellProps) => {
         <Box mb="auto">
           <Icon name="model" size={20} color={color("brand")} />
         </Box>
-        <Title order={4} lh="1rem" mb=".5rem">
+        <Title mb=".25rem" size="1rem">
           <MultilineEllipsified tooltipMaxWidth="20rem" id={headingId}>
             {model.name}
           </MultilineEllipsified>
