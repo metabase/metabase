417768dbba2ea9a6e826d77b6612a5b3e94a9b2c
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-09 12:33:42 +0100

    Add FieldInfoIcon component

---
+++ frontend/src/metabase/components/MetadataInfo/FieldInfoIcon/FieldInfoIcon.tsx
@@ -0,0 +1,25 @@
+import * as Lib from "metabase-lib";
+
+import type { FieldInfoPopoverMLv2Props } from "../FieldInfoPopover";
+import { FieldInfoPopoverMLv2 } from "../FieldInfoPopover";
+
+import { PopoverHoverTarget } from "./FieldInfoIcon.styled";
+
+const LAST_STAGE = -1;
+
+export function FieldInfoIcon(props: FieldInfoPopoverMLv2Props) {
+  const { query, column, stage = LAST_STAGE } = props;
+
+  const { description, semanticType } = Lib.displayInfo(query, stage, column);
+
+  const shouldBeActive =
+    (description && description !== "") || semanticType !== null;
+
+  return (
+    <FieldInfoPopoverMLv2 {...props} delay={[0, 150]}>
+      <PopoverHoverTarget name="info_filled" aria-disabled={!shouldBeActive} />
+    </FieldInfoPopoverMLv2>
+  );
+}
+
+FieldInfoIcon.HoverTarget = PopoverHoverTarget;
