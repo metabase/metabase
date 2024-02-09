032ee95ad7fabce6e99358078e1ab38c5c589a53
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-02-09 13:14:19 -0500

    Browse models: Add banner (#38261)

--- frontend/src/metabase/browse/components/BrowseModels.tsx
+++ frontend/src/metabase/browse/components/BrowseModels.tsx
@@ -20,6 +20,7 @@ import NoResults from "assets/img/no_results.svg";
 import { useDispatch, useSelector } from "metabase/lib/redux";
 import { getLocale } from "metabase/setup/selectors";
 import { isInstanceAnalyticsCollection } from "metabase/collections/utils";
+import { color } from "metabase/lib/colors";
 import { updateSetting } from "metabase/admin/settings/settings";
 import { getHasDismissedBrowseModelsBanner } from "metabase/browse/selectors";
 import { getCollectionName, groupModels } from "../utils";
@@ -85,7 +86,6 @@ export const BrowseModels = ({
             shadow="0"
             radius="0.25rem"
             role="complementary"
-            w="100%"
           >
             <Flex>
               <BannerModelIcon name="model" />
