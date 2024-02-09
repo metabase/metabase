26c040d75da3cc4aad6ad5a7b74863a1f7d423ec
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-02-09 10:11:54 -0500

    Revert "Remove unit tests of bromo default tab (these were failing in CI and the functionality is covered in e2e tests )"
    This reverts commit 5f4c3c1e2c1e9f67c2c43c5423ab87d66d0c78ca.

--- frontend/src/metabase/browse/components/BrowseRedirect.unit.spec.tsx
+++ frontend/src/metabase/browse/components/BrowseRedirect.unit.spec.tsx
@@ -3,6 +3,7 @@ import { renderWithProviders, waitFor } from "__support__/ui";
 import { setupSearchEndpoints } from "__support__/server-mocks";
 import type { SearchResult } from "metabase-types/api";
 import { createMockModelResult } from "metabase-types/api/mocks";
+import { mockLocalStorageGlobally } from "__support__/utils";
 import { BrowseRedirect } from "./BrowseRedirect";
 
 const mockModels: SearchResult[] = [
@@ -18,25 +19,77 @@ const mockModels: SearchResult[] = [
   },
 ].map(model => createMockModelResult(model));
 
-const setup = ({ models }: { models: SearchResult[] }) => {
+const setup = ({
+  models,
+  defaultTab = null,
+}: {
+  models: SearchResult[];
+  defaultTab: string | null;
+}) => {
   setupSearchEndpoints(models);
+  if (defaultTab === null) {
+    localStorage.removeItem("defaultBrowseTab");
+  } else {
+    localStorage.setItem("defaultBrowseTab", defaultTab);
+  }
   return renderWithProviders(<BrowseRedirect />);
 };
 
 describe("BrowseRedirect", () => {
-  it("redirects to /browse/databases if there are no models", async () => {
-    const { store } = setup({ models: [] });
+  let realLocalStorage: Storage;
+  beforeEach(() => {
+    realLocalStorage = mockLocalStorageGlobally().realLocalStorage;
+  });
+  afterEach(() => {
+    global.localStorage = realLocalStorage;
+  });
+
+  it("redirects to /browse/databases if there are no models and no saved setting", async () => {
+    const { store } = setup({ models: [], defaultTab: null });
     const mockDispatch = jest.spyOn(store, "dispatch");
     await waitFor(() => {
       expect(mockDispatch).toHaveBeenCalledWith(replace("/browse/databases"));
     });
   });
-  it("redirects to /browse/models if there are some models", async () => {
-    const { store } = setup({ models: mockModels });
+  it("redirects to /browse/models if there are some models but no saved setting", async () => {
+    const { store } = setup({ models: mockModels, defaultTab: null });
     const mockDispatch = jest.spyOn(store, "dispatch");
     await waitFor(() => {
       expect(mockDispatch).toHaveBeenCalledWith(replace("/browse/models"));
     });
   });
-  // NOTE: Default tab functionality is covered in e2e tests
+
+  it("redirects to /browse/models if the user's defaultBrowseTab setting is 'models'", async () => {
+    const { store, rerender } = setup({
+      models: [],
+      defaultTab: "models",
+    });
+    const mockDispatch = jest.spyOn(store, "dispatch");
+    rerender(<BrowseRedirect />);
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith(replace("/browse/models"));
+    });
+  });
+  it("redirects to /browse/databases if the user's defaultBrowseBab setting is 'databases'", async () => {
+    const { store, rerender } = setup({
+      models: mockModels,
+      defaultTab: "databases",
+    });
+    const mockDispatch = jest.spyOn(store, "dispatch");
+    rerender(<BrowseRedirect />);
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith(replace("/browse/databases"));
+    });
+  });
+  it("redirects to /browse/models if the user has an invalid defaultBrowseTab setting, and some models exist", async () => {
+    const { store, rerender } = setup({
+      models: mockModels,
+      defaultTab: "this is an invalid value",
+    });
+    const mockDispatch = jest.spyOn(store, "dispatch");
+    rerender(<BrowseRedirect />);
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith(replace("/browse/models"));
+    });
+  });
 });
