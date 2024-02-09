46d57b8cd576e48d169bde7069ad5b04f627505d
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-02-09 09:59:16 -0500

    Remove unit tests of bromo default tab (these were failing in CI and the functionality is covered in e2e tests )

--- frontend/test/__support__/utils.ts
+++ frontend/test/__support__/utils.ts
@@ -2,30 +2,3 @@ export const getNextId = (() => {
   let id = 0;
   return () => ++id;
 })();
-
-export const mockLocalStorageGlobally = () => {
-  const store = new Map<string, string>();
-  const mockStorage: Storage = {
-    getItem(key: string) {
-      return store.get(key) || null;
-    },
-    setItem(key: string, value: string) {
-      store.set(key, value);
-    },
-    removeItem(key: string) {
-      store.delete(key);
-    },
-    clear() {
-      store.clear();
-    },
-    key(index: number) {
-      return Array.from(store.keys())[index];
-    },
-    get length() {
-      return store.size;
-    },
-  };
-  const realLocalStorage = localStorage;
-  global.localStorage = mockStorage;
-  return { realLocalStorage };
-};
