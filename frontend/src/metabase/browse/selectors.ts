19f468fad863aeda6ce89b58d1fb55b600f54117
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-02-09 14:33:11 -0500

    Remove dead code

--- frontend/src/metabase/browse/selectors.ts
+++ frontend/src/metabase/browse/selectors.ts
@@ -4,7 +4,3 @@ import type { State } from "metabase-types/store";
 export const getHasDismissedBrowseModelsBanner = (state: State) => {
   return getSetting(state, "dismissed-browse-models-banner");
 };
-
-export const getVerifiedModelsFilterForBrowseModels = (state: State) => {
-  return getSetting(state, "only-show-verified-models-in-browse-data");
-};
