0b06c1daeee86b6abb8dd7db7f5b54435ad5b03a
Nick Fitzpatrick <nick@metabase.com>
2024-02-11 10:50:38 -0400

    Usage insight links on questions and dashboards (#38573)
    * adding links with hardcoded IDs
    
    * add audit-info endpoint
    
    * fix perms
    
    * Fetching Dashboard IDs from the API
    
    * e2e tests
    
    * add BE tests
    
    * kinda busted
    
    * InstanceAnalyticsButton
    
    * removing unused file
    
    * Update enterprise/backend/src/metabase_enterprise/audit_app/api/user.clj
    
    Co-authored-by: Noah Moss <32746338+noahmoss@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jerry Huang <jhuang37050@gmail.com>
    Co-authored-by: Jerry Huang <34140255+qwef@users.noreply.github.com>
    Co-authored-by: Noah Moss <32746338+noahmoss@users.noreply.github.com>

--- enterprise/frontend/src/metabase-enterprise/audit_app/index.js
+++ enterprise/frontend/src/metabase-enterprise/audit_app/index.js
@@ -4,9 +4,16 @@ import {
   PLUGIN_ADMIN_ROUTES,
   PLUGIN_ADMIN_USER_MENU_ITEMS,
   PLUGIN_ADMIN_USER_MENU_ROUTES,
+  PLUGIN_REDUCERS,
+  PLUGIN_DASHBOARD_HEADER,
+  PLUGIN_QUERY_BUILDER_HEADER,
 } from "metabase/plugins";
 import { hasPremiumFeature } from "metabase-enterprise/settings";
 import getAuditRoutes, { getUserMenuRotes } from "./routes";
+import { auditInfo } from "./reducer";
+import { getDashboardOverviewId, getQuestionOverviewId } from "./selectors";
+
+import { InstanceAnalyticsButton } from "./components/InstanceAnalyticsButton/InstanceAnalyticsButton";
 
 if (hasPremiumFeature("audit_app")) {
   PLUGIN_ADMIN_NAV_ITEMS.push({
@@ -24,4 +31,34 @@ if (hasPremiumFeature("audit_app")) {
   ]);
 
   PLUGIN_ADMIN_USER_MENU_ROUTES.push(getUserMenuRotes);
+
+  PLUGIN_REDUCERS.auditInfo = auditInfo;
+
+  PLUGIN_DASHBOARD_HEADER.extraButtons = dashboard => {
+    return [
+      {
+        key: "Usage insights",
+        component: (
+          <InstanceAnalyticsButton
+            entitySelector={getDashboardOverviewId}
+            linkQueryParams={{ dashboard_id: dashboard.id }}
+          />
+        ),
+      },
+    ];
+  };
+
+  PLUGIN_QUERY_BUILDER_HEADER.extraButtons = question => {
+    return [
+      {
+        key: "Usage insights",
+        component: (
+          <InstanceAnalyticsButton
+            entitySelector={getQuestionOverviewId}
+            linkQueryParams={{ question_id: question.id() }}
+          />
+        ),
+      },
+    ];
+  };
 }
