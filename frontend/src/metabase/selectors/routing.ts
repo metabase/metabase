d12b245b9ec635da94e60c82e8d4a4770a8fa530
Kamil Mielnik <kamil@kamilmielnik.com>
2024-02-12 17:07:43 +0700

    Do not export local selector

--- frontend/src/metabase/selectors/routing.ts
+++ frontend/src/metabase/selectors/routing.ts
@@ -1,6 +1,6 @@
 import type { State } from "metabase-types/store";
 
-export const getRouting = (state: State) => state.routing;
+const getRouting = (state: State) => state.routing;
 
 export const getLocation = (state: State) =>
   getRouting(state).locationBeforeTransitions;
