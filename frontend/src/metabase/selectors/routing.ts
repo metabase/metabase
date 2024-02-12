f3adcae889d8ddf46712b9e8cce1fe33eeb1e933
Kamil Mielnik <kamil@kamilmielnik.com>
2024-02-12 18:55:14 +0700

    Update frontend/src/metabase/selectors/routing.ts
    Co-authored-by: Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>

--- frontend/src/metabase/selectors/routing.ts
+++ frontend/src/metabase/selectors/routing.ts
@@ -1,6 +1,4 @@
 import type { State } from "metabase-types/store";
 
-const getRouting = (state: State) => state.routing;
-
 export const getLocation = (state: State) =>
-  getRouting(state).locationBeforeTransitions;
+  state.routing.locationBeforeTransitions;
