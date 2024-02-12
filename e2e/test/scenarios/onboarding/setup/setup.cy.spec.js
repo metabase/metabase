eabed3b66bb38d3f5f88be8a52cbe303b104b330
Nicolò Pretto <info@npretto.com>
2024-02-12 13:00:43 +0100

    should make test pass in release when we have a specific version in the url

--- e2e/test/scenarios/onboarding/setup/setup.cy.spec.js
+++ e2e/test/scenarios/onboarding/setup/setup.cy.spec.js
@@ -368,12 +368,14 @@ describe("scenarios > setup", () => {
       .findByText("Get started with Embedding Metabase in your app")
       .should("exist");
 
-    main().findByRole("link", { name: "Learn more" }).should(
-      "have.attr",
-      "href",
-      // eslint-disable-next-line no-unconditional-metabase-links-render -- this is a test file
-      "https://www.metabase.com/docs/latest/embedding/start.html?utm_media=embed-minimal-homepage",
-    );
+    main()
+      .findByRole("link", { name: "Learn more" })
+      .should("have.attr", "href")
+      .and(
+        "match",
+        // eslint-disable-next-line no-unconditional-metabase-links-render -- this is a test file
+        /https:\/\/www.metabase.com\/docs\/[^\/]*\/embedding\/start\.html\?utm_media=embed-minimal-homepage/,
+      );
 
     cy.icon("close").click();
 
