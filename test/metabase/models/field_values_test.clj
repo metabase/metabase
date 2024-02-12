1dd0c8f689370108a30de19de1da47dd2a333b28
Chris Truter <chris@metabase.com>
2024-02-12 16:48:43 +0200

    improve test formatting and add paranoid final check

--- test/metabase/models/field_values_test.clj
+++ test/metabase/models/field_values_test.clj
@@ -137,12 +137,14 @@
                    FieldValues _                 {:field_id field-id :type :full :values ["a" "b"] :human_readable_values ["A" "B"] :updated_at now}
                    FieldValues _                 {:field_id field-id :type :full :values ["c" "d"] :human_readable_values ["C" "D"] :updated_at then}]
 
-      (testing "When we have two FieldValues rows in the database"
+      (testing "When we have two FieldValues rows in the database, "
         (is (= 2 (count (t2/select FieldValues :field_id field-id :type :full :hash_key nil))))
-        (testing "We always fetch the most recently inserted row"
-          (is (= ["C" "D"] (:human_readable_values (#'field-values/get-latest-full-field-values field-id))))
-          (testing "... and the older rows are deleted"
-            (is (= 1 (count (t2/select FieldValues :field_id field-id :type :full))))))))))
+        (testing "we always return the most recently inserted row"
+          (is (= ["C" "D"] (:human_readable_values (field-values/get-latest-full-field-values field-id))))
+          (testing "... and older rows are implicitly deleted"
+            (is (= 1 (count (t2/select FieldValues :field_id field-id :type :full))))
+            ;; double check that we deleted the correct row
+            (is (= ["C" "D"] (:human_readable_values (field-values/get-latest-full-field-values field-id))))))))))
 
 (deftest get-or-create-full-field-values!-test
   (mt/dataset test-data
