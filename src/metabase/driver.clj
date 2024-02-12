48c43777750049308800640264ae9578abeae2c0
Chris Truter <crisptrutski@users.noreply.github.com>
2024-02-12 10:59:35 +0200

    Revert breaking change to the `active-tables` driver method (#38562)

--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -968,7 +968,7 @@
 (defmethod create-auto-pk-with-append-csv? ::driver [_] false)
 
 (defmulti current-user-table-privileges
-  "Returns the rows of data as arrays needed to populate the tabel_privileges table
+  "Returns the rows of data as arrays needed to populate the table_privileges table
    with the DB connection's current user privileges.
    The data contains the privileges that the user has on the given `database`.
    The privileges include select, insert, update, and delete.
@@ -984,7 +984,7 @@
 
    Either:
    (1) role is null, corresponding to the privileges of the DB connection's current user
-   (2) role is not null, corresponing to the privileges of the role"
-  {:added "0.48.0", :arglists '([driver database])}
+   (2) role is not null, corresponding to the privileges of the role"
+  {:added "0.48.0", :arglists '([driver database & args])}
   dispatch-on-initialized-driver
   :hierarchy #'hierarchy)
