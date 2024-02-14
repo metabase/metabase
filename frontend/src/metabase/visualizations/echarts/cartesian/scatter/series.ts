4375ba3e41db902d662dda3426166550c15bdd50
Emmad Usmani <emmadusmani@berkeley.edu>
2024-02-13 17:13:56 -0800

    improve bubble size scaling (#38623)
    * improve bubble size scaling
    
    * Update Loki Snapshots
    
    ---------
    
    Co-authored-by: Metabase bot <metabase-bot@metabase.com>

--- frontend/src/metabase/visualizations/echarts/cartesian/scatter/series.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/scatter/series.ts
@@ -6,23 +6,42 @@ import type { RenderingContext } from "metabase/visualizations/types";
 import { X_AXIS_DATA_KEY } from "metabase/visualizations/echarts/cartesian/constants/dataset";
 import type { DataKey, Datum, Extent, SeriesModel } from "../model/types";
 
-const BUBBLE_SCALE_FACTOR_MAX = 64;
+const MIN_BUBBLE_DIAMETER = 15;
+const MAX_BUBBLE_DIAMETER = 75;
 
-const MIN_BUBBLE_SIZE = 14;
-
-// TODO: refine the scaling curve when implementing the dynamic scatter plot
-function getBubbleSizeScale(
+/**
+ * Returns a function that takes in a datum, and returns a scaled
+ * diameter size based on the bubble size column the user has selected.
+ * We return diameter since that's what ECharts uses for its `symbolSize` option.
+ *
+ * The function scales linearly based on area to provide an accurate representation
+ * of the data (see https://www.data-to-viz.com/caveat/radius_or_area.html for rationale).
+ */
+function getBubbleDiameterScale(
   bubbleSizeDomain: Extent | null,
   bubbleSizeDataKey: DataKey | undefined,
 ) {
   if (!bubbleSizeDataKey || !bubbleSizeDomain) {
-    return MIN_BUBBLE_SIZE;
+    return MIN_BUBBLE_DIAMETER;
   }
+  const areaRange = [MIN_BUBBLE_DIAMETER, MAX_BUBBLE_DIAMETER].map(
+    diameter => Math.PI * (diameter / 2) ** 2,
+  );
+  // Domain is [0, 1] since the `t` parameteter of the interpolate function below
+  // is normalized to 0-1.
+  const areaScale = d3.scale.linear().domain([0, 1]).range(areaRange);
 
   const scale = d3.scale
-    .sqrt()
-    .domain(bubbleSizeDomain.map(v => v * BUBBLE_SCALE_FACTOR_MAX))
-    .range([MIN_BUBBLE_SIZE, 1024]);
+    .linear()
+    .domain(bubbleSizeDomain)
+    // D3 will take a value from the domain (bubble size column) and normalize it (`t` is between 0,1).
+    // Then we plug the normalized value `t` into the `areaScale` to get the corrseponding area for that diameter.
+    // We then take this area and convert it back to a diameter value
+    // if area = π × (diameter ÷ 2)², then diameter = (2 × √area) ÷ π
+    .interpolate((_, _2) => t => (2 * Math.sqrt(areaScale(t))) / Math.PI)
+    // Finally, D3 linearly maps that value into our defined min/max range.
+    .range([MIN_BUBBLE_DIAMETER, MAX_BUBBLE_DIAMETER]);
+
   return (datum: Datum) => scale(Number(datum[bubbleSizeDataKey]));
 }
 
@@ -40,7 +59,7 @@ export function buildEChartsScatterSeries(
     id: seriesModel.dataKey,
     type: "scatter",
     yAxisIndex,
-    symbolSize: getBubbleSizeScale(bubbleSizeDomain, bubbleSizeDataKey),
+    symbolSize: getBubbleDiameterScale(bubbleSizeDomain, bubbleSizeDataKey),
     encode: {
       y: seriesModel.dataKey,
       x: X_AXIS_DATA_KEY,
