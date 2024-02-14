b1cd232cf6a03e4a76c6f4322b271ef88cf15303
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-14 11:34:19 +0100

    Move to typescript and export as type

--- frontend/src/metabase/components/MetadataInfo/FieldInfo/index.js
+++
@@ -1,1 +0,0 @@
-export { default, FieldInfo, FieldInfoProps } from "./FieldInfo";
---
+++ frontend/src/metabase/components/MetadataInfo/FieldInfo/index.ts
@@ -0,0 +1,3 @@
+// eslint-disable-next-line import/no-default-export
+export { default, FieldInfo } from "./FieldInfo";
+export type { FieldInfoProps } from "./FieldInfo";
