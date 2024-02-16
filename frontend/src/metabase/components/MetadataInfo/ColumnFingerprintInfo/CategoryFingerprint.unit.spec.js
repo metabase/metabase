377c936e0ac4c9936706a9a919b237fdbf018bac
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-16 15:39:28 +0100

    Add info icon to the column picker dropdown in the notebook editor (#38725)
    * Make SemanticTypeLabel not depend on Field but just on the semantic type
    * Add MLv2 version of FieldInfo
    * Add MLv2 props to FieldInfoPopover
    * Convert TableInteractive to use MLv2 version of FieldInfoPopover
    * Support Fingerprints in FieldInfo for MLv2 types
    * Move DateTimeFingerprint to its own file
    * Move NumberFingerprint to its own file
    * Allow Category finderprint info to work with MLv2 types
    * Remove null values in fingerprint info
    * Move move ColumnDescription into separate component
    * Split up fingerprint info component

--- frontend/src/metabase/components/MetadataInfo/ColumnFingerprintInfo/CategoryFingerprint.unit.spec.js
+++ frontend/src/metabase/components/MetadataInfo/ColumnFingerprintInfo/CategoryFingerprint.unit.spec.js
@@ -21,7 +21,7 @@ const state = createMockState({
 function setup({ field }) {
   setupFieldValuesEndpoints(PRODUCT_CATEGORY_VALUES);
 
-  renderWithProviders(<CategoryFingerprint field={field} />, {
+  renderWithProviders(<CategoryFingerprint fieldId={field.id} />, {
     storeInitialState: state,
   });
 }
