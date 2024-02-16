df2b6549ef3f47194182c5a505b66479fce427f3
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-16 12:37:55 +0100

    Fix nesting of ColumnIcon and HoverParent

--- frontend/src/metabase/components/MetadataInfo/ColumnInfoIcon/ColumnInfoIcon.styled.tsx
+++ frontend/src/metabase/components/MetadataInfo/ColumnInfoIcon/ColumnInfoIcon.styled.tsx
@@ -1,17 +1,18 @@
 import styled from "@emotion/styled";
 import { Icon } from "metabase/ui";
 
-export const HoverParent = styled.div``;
-
 export const PopoverHoverTarget = styled(Icon)<{ hasDescription: boolean }>`
   padding: 0.7em 0.65em;
-  opacity: 0;
+  visibility: hidden;
+  opacity: ${props => (props.hasDescription ? 0.6 : 0.3)};
 
-  ${HoverParent}:hover & {
-    opacity: ${props => (props.hasDescription ? 0.6 : 0.3)};
+  &[aria-expanded="true"] {
+    opacity: 1;
+  }
+`;
 
-    &[aria-expanded="true"] {
-      opacity: 1;
-    }
+export const HoverParent = styled.div`
+  &:hover ${PopoverHoverTarget} {
+    visibility: visible;
   }
 `;
