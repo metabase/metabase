9a33882094f9869bcc16482dc5024a784ca80388
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-02-16 12:54:51 +0700

    Combine both container and view component

--- frontend/src/metabase/styled-components/containers/GlobalStyles/GlobalStyles.tsx
+++ frontend/src/metabase/styled-components/containers/GlobalStyles/GlobalStyles.tsx
@@ -1,11 +1,38 @@
-import { connect } from "react-redux";
-import type { State } from "metabase-types/store";
+import { css, Global } from "@emotion/react";
+import { alpha, color } from "metabase/lib/colors";
+import { aceEditorStyles } from "metabase/query_builder/components/NativeQueryEditor/NativeQueryEditor.styled";
+import { saveDomImageStyles } from "metabase/visualizations/lib/save-chart-image";
+import { useSelector } from "metabase/lib/redux";
 import { getFont, getFontFiles } from "../../selectors";
-import { GlobalStylesView } from "../../components/GlobalStylesView";
 
-const mapStateToProps = (state: State) => ({
-  font: getFont(state),
-  fontFiles: getFontFiles(state),
-});
+export const GlobalStyles = (): JSX.Element => {
+  const font = useSelector(getFont);
+  const fontFiles = useSelector(getFontFiles);
 
-export const GlobalStyles = connect(mapStateToProps)(GlobalStylesView);
+  const styles = css`
+    :root {
+      --default-font-family: "${font}";
+      --color-brand: ${color("brand")};
+      --color-brand-alpha-04: ${alpha("brand", 0.04)};
+      --color-brand-alpha-88: ${alpha("brand", 0.88)};
+      --color-focus: ${color("focus")};
+    }
+
+    ${fontFiles?.map(
+      file => css`
+        @font-face {
+          font-family: "${font}";
+          src: url(${encodeURI(file.src)}) format("${file.fontFormat}");
+          font-weight: ${file.fontWeight};
+          font-style: normal;
+          font-display: swap;
+        }
+      `,
+    )}
+
+    ${aceEditorStyles}
+    ${saveDomImageStyles}
+  `;
+
+  return <Global styles={styles} />;
+};
