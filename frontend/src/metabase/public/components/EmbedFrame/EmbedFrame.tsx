785f3fdd8630a2ba0f87abc19d9da01451ffccc4
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2024-02-16 16:27:21 -0500

    use all hidden parameters to determine visibility
    - queryParams: hide_parameters
    - hiddenParameterSlugs

--- frontend/src/metabase/public/components/EmbedFrame/EmbedFrame.tsx
+++ frontend/src/metabase/public/components/EmbedFrame/EmbedFrame.tsx
@@ -165,7 +165,7 @@ function EmbedFrame({
 
   const hasParameters = Array.isArray(parameters) && parameters.length > 0;
   const visibleParameters = hasParameters
-    ? getVisibleParameters(parameters, hiddenParameterSlugs)
+    ? getVisibleParameters(parameters, hideParameters)
     : [];
   const hasVisibleParameters = visibleParameters.length > 0;
 
