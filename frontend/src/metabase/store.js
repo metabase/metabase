d100252bac1ded7ed2b65d3c0474645adf3ba003
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-02-17 22:31:53 +0300

    Disable immer auto freeze

--- frontend/src/metabase/store.js
+++ frontend/src/metabase/store.js
@@ -1,9 +1,18 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { setAutoFreeze } from "immer";
 import { routerReducer as routing, routerMiddleware } from "react-router-redux";
 import promise from "redux-promise";
 import { PLUGIN_REDUX_MIDDLEWARES } from "metabase/plugins";
 
-export function getStore(reducers, history, intialState) {
+/**
+ * MLv2 modifies passed values and adds a new property for caching objects
+ * to speed up calculations. RTK uses immer under the hood and freezes
+ * state automatically, which leads to the error on cljs side.
+ * As a workaround, we disable immer's auto-freeze
+ */
+setAutoFreeze(false);
+
+export function getStore(reducers, history, initialState) {
   const reducer = combineReducers({
     ...reducers,
     routing,
@@ -11,7 +20,7 @@ export function getStore(reducers, history, intialState) {
 
   return configureStore({
     reducer,
-    preloadedState: intialState,
+    preloadedState: initialState,
     middleware: getDefaultMiddleware =>
       getDefaultMiddleware({
         immutableCheck: false,
