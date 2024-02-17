0b74fb16f8ae20c9c4a1b8d24425472a2c3beaea
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-02-17 22:31:53 +0300

    Disable immer auto freeze

--- frontend/src/metabase/store.js
+++ frontend/src/metabase/store.js
@@ -1,9 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { setAutoFreeze } from "immer";
 import { routerReducer as routing, routerMiddleware } from "react-router-redux";
 import promise from "redux-promise";
 import { PLUGIN_REDUX_MIDDLEWARES } from "metabase/plugins";
 
-export function getStore(reducers, history, intialState) {
+setAutoFreeze(false);
+
+export function getStore(reducers, history, initialState) {
   const reducer = combineReducers({
     ...reducers,
     routing,
@@ -11,7 +14,7 @@ export function getStore(reducers, history, intialState) {
 
   return configureStore({
     reducer,
-    preloadedState: intialState,
+    preloadedState: initialState,
     middleware: getDefaultMiddleware =>
       getDefaultMiddleware({
         immutableCheck: false,
