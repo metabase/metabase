9deb8f7be3b1ea02a8ad8d3931bb7b19579ba2e1
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-19 11:06:53 +0100

    Add FilterIcon to BooleanFilterEditor

--- frontend/src/metabase/querying/components/FilterModal/BooleanFilterEditor/BooleanFilterEditor.tsx
+++ frontend/src/metabase/querying/components/FilterModal/BooleanFilterEditor/BooleanFilterEditor.tsx
@@ -7,6 +7,7 @@ import { useBooleanOperatorFilter } from "metabase/querying/hooks/use-boolean-op
 import type * as Lib from "metabase-lib";
 import { FilterColumnName } from "../FilterColumnName";
 import { FilterOperatorPicker } from "../FilterOperatorPicker";
+import { InfoIcon, HoverParent } from "../InfoIcon.styled";
 import type { FilterEditorProps } from "../types";
 
 export function BooleanFilterEditor({
@@ -53,45 +54,48 @@ export function BooleanFilterEditor({
   };
 
   return (
-    <Grid grow>
-      <Grid.Col span="auto">
-        <Flex h="100%" align="center" gap="sm">
-          <Icon name={columnIcon} />
-          <FilterColumnName
-            query={query}
-            stageIndex={stageIndex}
-            column={column}
-            isSearching={isSearching}
-          />
-          {isExpanded && (
-            <FilterOperatorPicker
-              value={operator}
-              options={availableOptions}
-              onChange={handleOperatorChange}
+    <HoverParent>
+      <Grid grow>
+        <Grid.Col span="auto">
+          <Flex h="100%" align="center" gap="sm">
+            <InfoIcon query={query} stageIndex={stageIndex} column={column} />
+            <Icon name={columnIcon} />
+            <FilterColumnName
+              query={query}
+              stageIndex={stageIndex}
+              column={column}
+              isSearching={isSearching}
             />
-          )}
-        </Flex>
-      </Grid.Col>
-      <Grid.Col span={4}>
-        <Group spacing="md">
-          <Checkbox
-            label={t`True`}
-            checked={values.length > 0 ? values[0] : false}
-            indeterminate={valueCount === 0}
-            onChange={event =>
-              handleValuesChange(event.target.checked ? [true] : [])
-            }
-          />
-          <Checkbox
-            label={t`False`}
-            checked={values.length > 0 ? !values[0] : false}
-            indeterminate={valueCount === 0}
-            onChange={event =>
-              handleValuesChange(event.target.checked ? [false] : [])
-            }
-          />
-        </Group>
-      </Grid.Col>
-    </Grid>
+            {isExpanded && (
+              <FilterOperatorPicker
+                value={operator}
+                options={availableOptions}
+                onChange={handleOperatorChange}
+              />
+            )}
+          </Flex>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Group spacing="md">
+            <Checkbox
+              label={t`True`}
+              checked={values.length > 0 ? values[0] : false}
+              indeterminate={valueCount === 0}
+              onChange={event =>
+                handleValuesChange(event.target.checked ? [true] : [])
+              }
+            />
+            <Checkbox
+              label={t`False`}
+              checked={values.length > 0 ? !values[0] : false}
+              indeterminate={valueCount === 0}
+              onChange={event =>
+                handleValuesChange(event.target.checked ? [false] : [])
+              }
+            />
+          </Group>
+        </Grid.Col>
+      </Grid>
+    </HoverParent>
   );
 }
