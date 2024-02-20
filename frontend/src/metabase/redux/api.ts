78382410722836314bde74ce198d40746aafdb58
Sloan Sparger <sloansparger@gmail.com>
2024-02-20 16:47:31 -0600

    reorganizes files and impls util to createEntityApis more easily

--- frontend/src/metabase/redux/api.ts
+++ frontend/src/metabase/redux/api.ts
@@ -4,62 +4,31 @@ import type {
   ApiKey,
   CreateApiKeyInput,
   CreateApiKeyResponse,
-  EditApiKeyInput,
+  UpdateApiKeyInput,
+  UpdateApiKeyOutput,
   RegenerateApiKeyResponse,
 } from "metabase-types/api/admin";
 
-import { customBaseQuery, getListTag, providesList } from "./api-utils";
+import { createEntityApi, getListTag } from "metabase/redux/utils";
 
-const API_KEY_TAG = "ApiKey" as const;
-const LIST_TAG = getListTag(API_KEY_TAG);
-const COUNT_TAG = { type: API_KEY_TAG, id: "COUNT" as const };
-
-// Define a service using a base URL and expected endpoints
-export const ApiKeysApi = createApi({
-  reducerPath: "apiKeys",
-  tagTypes: [API_KEY_TAG],
-  baseQuery: customBaseQuery,
+export const ApiKeysApi = createEntityApi<
+  ApiKey,
+  "ApiKey",
+  CreateApiKeyInput,
+  CreateApiKeyResponse,
+  UpdateApiKeyInput,
+  UpdateApiKeyOutput
+>({
+  entityName: "ApiKey",
+  apiPath: "api-key",
+}).injectEndpoints({
   endpoints: builder => ({
-    list: builder.query<ApiKey[], void>({
-      query: () => `/api/api-key`,
-      providesTags: result => providesList(result, API_KEY_TAG),
-    }),
     count: builder.query<number, void>({
       query: () => `/api/api-key/count`,
-      // TODO: invalide on create, delete
     }),
-    create: builder.mutation<CreateApiKeyResponse, CreateApiKeyInput>({
-      query: input => ({
-        method: "POST",
-        url: `/api/api-key`,
-        body: input,
-      }),
-      invalidatesTags: [LIST_TAG, COUNT_TAG],
-    }),
-    delete: builder.mutation<void, number>({
-      query: id => ({
-        method: "DELETE",
-        url: `/api/api-key/${id}`,
-      }),
-      invalidatesTags: [LIST_TAG, COUNT_TAG],
-    }),
-    edit: builder.mutation<void, EditApiKeyInput>({
-      query: ({ id, ...body }) => ({
-        method: "PUT",
-        url: `/api/api-key/${id}`,
-        body,
-      }),
-      invalidatesTags: [LIST_TAG],
-    }),
-    regenerate: builder.mutation<RegenerateApiKeyResponse, number>({
-      query: id => ({
-        method: "PUT",
-        url: `/api/api-key/${id}/regenerate`,
-      }),
-      invalidatesTags: [LIST_TAG],
+    regenerate: builder.mutation<RegenerateApiKeyResponse, ApiKey["id"]>({
+      query: id => ({ method: "PUT", url: `/api/api-key/${id}/regenerate` }),
+      invalidatesTags: [getListTag("ApiKey")],
     }),
   }),
 });
-
-// TODO: can you force a refetch on create and edit?
-// TODO: make sure I'm error handling correctly, errors as values are great, but maybe not with existing patterns?
