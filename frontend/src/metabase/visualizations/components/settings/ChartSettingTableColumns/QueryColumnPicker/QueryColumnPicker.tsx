ef65e933eec16a6792fbda62ebea62d0a282449c
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-02-20 09:19:38 +0200

    Add query picker

---
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingTableColumns/QueryColumnPicker/QueryColumnPicker.tsx
@@ -0,0 +1,94 @@
+import { useMemo, useState } from "react";
+import { t } from "ttag";
+import { Box, Checkbox, Flex, Icon, Text, TextInput } from "metabase/ui";
+import { getColumnIcon } from "metabase/common/utils/columns";
+import type * as Lib from "metabase-lib";
+import {
+  getColumnGroupItems,
+  searchColumnGroupItems,
+  toggleColumnGroupInQuery,
+  toggleColumnInQuery,
+} from "./utils";
+import type { ColumnGroupItem, ColumnItem } from "./types";
+
+interface QueryColumnPickerProps {
+  query: Lib.Query;
+  stageIndex: number;
+  onChange: (query: Lib.Query) => void;
+}
+
+export const QueryColumnPicker = ({
+  query,
+  stageIndex,
+  onChange,
+}: QueryColumnPickerProps) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const groupItems = useMemo(() => {
+    return getColumnGroupItems(query, stageIndex);
+  }, [query, stageIndex]);
+
+  const visibleGroupItems = useMemo(
+    () => searchColumnGroupItems(groupItems, searchValue),
+    [groupItems, searchValue],
+  );
+
+  const handleColumnToggle = (columnItem: ColumnItem) => {
+    onChange(toggleColumnInQuery(query, stageIndex, columnItem));
+  };
+
+  const handleGroupToggle = (groupItem: ColumnGroupItem) => {
+    onChange(toggleColumnGroupInQuery(query, stageIndex, groupItem));
+  };
+
+  return (
+    <div>
+      <TextInput
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+        rightSection={<Icon name="search" />}
+        placeholder={t`Search for a column...`}
+        mb="1rem"
+      />
+      {visibleGroupItems.map((groupItem, groupIndex) => {
+        return (
+          <div role="list" key={groupIndex}>
+            <Text fz="lg" fw={700} lh="1.5rem" mb="1rem" mt="1.75rem">
+              {groupItem.displayName}
+            </Text>
+            <Box mb="1.5rem">
+              <Checkbox
+                variant="stacked"
+                size="xs"
+                label={
+                  <Text fw={700} ml="0.375rem" lh="1rem">{t`Remove all`}</Text>
+                }
+                checked={groupItem.isSelected}
+                onClick={() => handleGroupToggle(groupItem)}
+              />
+            </Box>
+            {groupItem.columnItems.map((columnItem, columnIndex) => (
+              <Box mb="1rem" key={columnIndex}>
+                <Checkbox
+                  label={
+                    <Flex ml="0.25rem" align="center">
+                      <Icon name={getColumnIcon(columnItem.column)} />
+                      <Text span ml="0.5rem" lh="1rem" fw={400}>
+                        {columnItem.displayName}
+                      </Text>
+                    </Flex>
+                  }
+                  checked={columnItem.isSelected}
+                  disabled={columnItem.isDisabled}
+                  mb="1.5rem"
+                  size="xs"
+                  onClick={() => handleColumnToggle(columnItem)}
+                />
+              </Box>
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
---
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingTableColumns/QueryColumnPicker/utils.ts
@@ -0,0 +1,74 @@
+import * as Lib from "metabase-lib";
+import type { ColumnGroupItem, ColumnItem } from "./types";
+
+export function getColumnGroupItems(
+  query: Lib.Query,
+  stageIndex: number,
+): ColumnGroupItem[] {
+  const columns = Lib.visibleColumns(query, stageIndex);
+  const groups = Lib.groupColumns(columns);
+
+  return groups.map(group => {
+    const groupInfo = Lib.displayInfo(query, stageIndex, group);
+    const columnItems = Lib.getColumnsFromColumnGroup(group).map(column => {
+      const columnInfo = Lib.displayInfo(query, stageIndex, column);
+
+      return {
+        column,
+        displayName: columnInfo.displayName,
+        isSelected: columnInfo.selected ?? false,
+        isDisabled: columnInfo.isAggregation || columnInfo.isBreakout,
+      };
+    });
+
+    return {
+      columnItems,
+      displayName: groupInfo.displayName,
+      isSelected: columnItems.every(columnItem => columnItem.isSelected),
+      isDisabled: columnItems.some(columnItem => columnItem.isDisabled),
+    };
+  });
+}
+
+export function searchColumnGroupItems(
+  groupItems: ColumnGroupItem[],
+  searchValue: string,
+): ColumnGroupItem[] {
+  const searchString = searchValue.trim().toLowerCase();
+  if (searchString.length === 0) {
+    return groupItems;
+  }
+
+  return groupItems
+    .map(groupItem => ({
+      ...groupItem,
+      columnItems: groupItem.columnItems.filter(columnItem =>
+        columnItem.displayName.toLowerCase().includes(searchString),
+      ),
+    }))
+    .filter(groupItem => groupItem.columnItems.length > 0);
+}
+
+export function toggleColumnInQuery(
+  query: Lib.Query,
+  stageIndex: number,
+  { column, isSelected }: ColumnItem,
+) {
+  return isSelected
+    ? Lib.removeField(query, stageIndex, column)
+    : Lib.addField(query, stageIndex, column);
+}
+
+export function toggleColumnGroupInQuery(
+  query: Lib.Query,
+  stageIndex: number,
+  { columnItems, isSelected }: ColumnGroupItem,
+) {
+  return columnItems.reduce(
+    (query, { column }) =>
+      isSelected
+        ? Lib.removeField(query, stageIndex, column)
+        : Lib.addField(query, stageIndex, column),
+    query,
+  );
+}
