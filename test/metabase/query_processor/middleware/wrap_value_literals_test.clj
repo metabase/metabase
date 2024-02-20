7d411367e3b31abf02de44a41d5d205102301cd0
Case Nelson <case@metabase.com>
2024-02-20 12:19:10 -0700

    Address pr feedback

--- test/metabase/query_processor/middleware/wrap_value_literals_test.clj
+++ test/metabase/query_processor/middleware/wrap_value_literals_test.clj
@@ -211,12 +211,17 @@
               {:source-query {:source-table $$checkins
                               :filter       [:> $date "2014-01-01"]}}))))))
 
-(deftest ^:parallel wrap-literals-around-expressions
+(deftest ^:parallel wrap-literals-around-expressions-test
   (testing "does wrapping literals work against expressions? (#27185)"
-    (wrap-value-literals
-      (lib.tu.macros/mbql-query checkins
-        {:expressions {"foo" $date}
-         :filter       [:> [:expression "foo" {:base-type :type/DateTime}] "2014-01-01"]}))))
+    (is (= (lib.tu.macros/mbql-query checkins
+             {:expressions {"foo" $date}
+              :filter      [:>
+                            [:expression "foo" {:base-type :type/DateTime}]
+                            [:absolute-datetime #t "2014-01-01T00:00Z[UTC]" :default]]})
+           (wrap-value-literals
+             (lib.tu.macros/mbql-query checkins
+               {:expressions {"foo" $date}
+                :filter      [:> [:expression "foo" {:base-type :type/DateTime}] "2014-01-01"]}))))))
 
 (deftest ^:parallel other-clauses-test
   (testing "Make sure we apply the transformation to predicates in all parts of the query, not only `:filter`"
