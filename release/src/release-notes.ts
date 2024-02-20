5d1e1959c53b57affa0c8c14e260a6fb4737beda
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2024-02-20 16:31:57 -0700

    Update release notes template (#38865)

--- release/src/release-notes.ts
+++ release/src/release-notes.ts
@@ -8,14 +8,6 @@ import {
 import type { ReleaseProps, Issue } from "./types";
 
 const releaseTemplate = `# NOTE: clean up 'Enhancements' and 'Bug fixes' sections and remove this line before publishing!
-**Enhancements**
-
-{{enhancements}}
-
-**Bug fixes**
-
-{{bug-fixes}}
-
 **Upgrading**
 
 You can download a .jar of the release, or get the latest on Docker. Make sure to back up your Metabase
@@ -31,6 +23,20 @@ SHA-256 checksum for the {{version}} JAR:
 \`\`\`
 {{checksum}}
 \`\`\`
+
+<details>
+<summary><h2>Changelog</h2></summary>
+
+**Enhancements**
+
+{{enhancements}}
+
+**Bug fixes**
+
+{{bug-fixes}}
+
+</details>
+
 `;
 
 const isBugIssue = (issue: Issue) =>
