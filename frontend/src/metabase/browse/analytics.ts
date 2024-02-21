6c1d3dbb0ff2402c6a9688e35d4908755e1e2a9d
Ryan Laurie <iethree@gmail.com>
2024-02-21 13:36:59 -0700

    fix event tracking

--- frontend/src/metabase/browse/analytics.ts
+++ frontend/src/metabase/browse/analytics.ts
@@ -2,12 +2,12 @@ import { trackSchemaEvent } from "metabase/lib/analytics";
 
 export const trackModelClick = (modelId: number) =>
   trackSchemaEvent("browse_data", "1-0-0", {
-    event: "browse_data_model_click",
+    event: "browse_data_model_clicked",
     model_id: modelId,
   });
 
 export const trackTableClick = (tableId: number) =>
   trackSchemaEvent("browse_data", "1-0-0", {
-    event: "browse_data_table_click",
+    event: "browse_data_table_clicked",
     table_id: tableId,
   });
