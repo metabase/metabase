e40a17d476e78102a36735654a40c487c844963e
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-21 17:08:15 +0100

    Fix tests that rely on binning label

--- e2e/test/scenarios/binning/binning-reproductions.cy.spec.js
+++ e2e/test/scenarios/binning/binning-reproductions.cy.spec.js
@@ -164,10 +164,12 @@ describe("binning related reproductions", () => {
     popover().findByText("18646").click();
 
     popover().within(() => {
-      cy.findByRole("option", { name: "CREATED_AT" })
+      cy.findByRole("option", { name: /CREATED_AT/ })
         .findByText("by month")
         .should("exist");
-      cy.findByRole("option", { name: "CREATED_AT" }).click();
+      cy.findByRole("option", { name: /CREATED_AT/ }).click({
+        position: "left",
+      });
     });
 
     getNotebookStep("summarize").findByText(
