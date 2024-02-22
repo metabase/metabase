274d89b973732fc8e48570394ad6ccc69ece568c
John Swanson <john.swanson@metabase.com>
2024-02-22 08:19:38 -0800

    re-add deleted test on segmented perms users

--- enterprise/backend/test/metabase_enterprise/sandbox/api/card_test.clj
+++ enterprise/backend/test/metabase_enterprise/sandbox/api/card_test.clj
@@ -3,9 +3,53 @@
    [clojure.test :refer :all]
    [metabase-enterprise.test :as met]
    [metabase.api.card-test :as api.card-test]
+   [metabase.models.data-permissions :as data-perms]
+   [metabase.models.permissions :as perms]
    [metabase.query-processor :as qp]
    [metabase.test :as mt]
+   [metabase.util :as u]
    [toucan2.tools.with-temp :as t2.with-temp]))
+
+(deftest users-with-segmented-perms-test
+  (testing "Users with segmented permissions should be able to save cards"
+    (let [card-name (mt/random-name)]
+      (mt/with-model-cleanup [:model/Card]
+        (mt/with-temp [:model/Database                   db {}
+                       :model/Collection                 collection {}
+                       :model/Table                      table {:db_id (u/the-id db)}
+                       :model/PermissionsGroup           group {}
+                       :model/PermissionsGroupMembership _ {:user_id (mt/user->id :rasta)
+                                                            :group_id (u/the-id group)}
+                       :model/GroupTableAccessPolicy     _ {:group_id (u/the-id group)
+                                                            :table_id (u/the-id table)}]
+          (mt/with-db db
+            (mt/with-no-data-perms-for-all-users!
+              (data-perms/set-table-permission! group table :perms/data-access :unrestricted)
+              (perms/grant-collection-readwrite-permissions! group collection)
+              (is (some? (mt/user-http-request :rasta :post 200 "card"
+                                               (assoc (api.card-test/card-with-name-and-query card-name (api.card-test/mbql-count-query db table))
+                                                      :collection_id (u/the-id collection))))))))))
+
+    (testing "Users with segmented permissions should be able to update the query associated to a card"
+      (mt/with-model-cleanup [:model/Card]
+        (mt/with-temp [:model/Database                   db {}
+                       :model/Collection                 collection {}
+                       :model/Table                      table {:db_id (u/the-id db)}
+                       :model/PermissionsGroup           group {}
+                       :model/PermissionsGroupMembership _ {:user_id (mt/user->id :rasta)
+                                                            :group_id (u/the-id group)}
+                       :model/Card                       card {:name "Some Name"
+                                                               :collection_id (u/the-id collection)}
+                       :model/GroupTableAccessPolicy     _    {:group_id (u/the-id group)
+                                                               :table_id (u/the-id table)}]
+          (mt/with-db db
+            (mt/with-no-data-perms-for-all-users!
+              (data-perms/set-table-permission! group table :perms/data-access :unrestricted)
+              (perms/grant-collection-readwrite-permissions! group collection)
+              (is (= "Another Name"
+                     (:name (mt/user-http-request :rasta :put 200 (str "card/" (u/the-id card))
+                                                  {:name          "Another Name"
+                                                   :dataset_query (api.card-test/mbql-count-query db table)})))))))))))
 
 (deftest parameters-with-source-is-card-test
   (testing "a card with a parameter whose source is a card should respect sandboxing"
