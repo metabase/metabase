80784e413375ac9f4b6244c39972282e48b0feff
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-22 14:36:15 +0100

    Make list item title ellipsified

--- frontend/src/metabase/common/components/QueryColumnPicker/QueryColumnPicker.styled.tsx
+++ frontend/src/metabase/common/components/QueryColumnPicker/QueryColumnPicker.styled.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 
 import AccordionList from "metabase/core/components/AccordionList";
+import { Ellipsified } from "metabase/core/components/Ellipsified";
 import { color } from "metabase/lib/colors";
 import type { ColorName } from "metabase/lib/colors/types";
 
@@ -16,13 +17,19 @@ export const NameAndBucketing = styled.div`
   flex-shrink: 0;
   flex-grow: 1;
   white-space: nowrap;
+  overflow: hidden;
 
   ${TriggerButton} {
     height: 100%;
     padding: 0;
+    flex-shrink: 1;
   }
 `;
 
-export const ItemName = styled.div`
+export const ItemName = styled(Ellipsified)`
   margin-right: 0.25rem;
+  flex-shrink: 1;
+  min-width: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
