62a8c2aefe65c3644e08c2a93d7b6e19561c579e
Mark Bastian <markbastian@gmail.com>
2024-02-22 13:35:40 -0700

    Update frontend/src/metabase/core/components/Input/Input.tsx
    Co-authored-by: Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>

--- frontend/src/metabase/core/components/Input/Input.tsx
+++ frontend/src/metabase/core/components/Input/Input.tsx
@@ -27,7 +27,6 @@ export type InputAttributes = Omit<
 export interface InputProps extends InputAttributes {
   inputRef?: Ref<HTMLInputElement>;
   size?: InputSize;
-  loading?: boolean;
   error?: boolean;
   fullWidth?: boolean;
   leftIcon?: IconName;
