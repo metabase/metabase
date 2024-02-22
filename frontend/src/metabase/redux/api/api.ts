2014689cede578b6b56324578b45aa8da7f1b700
Sloan Sparger <sloansparger@gmail.com>
2024-02-22 12:16:25 -0600

    clean up

--- frontend/src/metabase/redux/api/api.ts
+++ frontend/src/metabase/redux/api/api.ts
@@ -2,11 +2,11 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 
 import { apiQuery } from "metabase/redux/utils";
 
-import { API_KEY_TAG_TYPES } from "./api-keys";
+import { tagTypes } from "./tags";
 
 export const Api = createApi({
   reducerPath: "metabase-api",
-  tagTypes: [...API_KEY_TAG_TYPES], // TODO:
+  tagTypes,
   baseQuery: apiQuery,
   endpoints: () => ({}),
 });
