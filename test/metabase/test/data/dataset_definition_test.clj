279b3a309bb310882799f2e1884a7bb53d712b61
Ngoc Khuat <qn.khuat@gmail.com>
2024-02-22 16:02:25 +0700

    text instead of uuid

--- test/metabase/test/data/dataset_definition_test.clj
+++ test/metabase/test/data/dataset_definition_test.clj
@@ -10,21 +10,21 @@
   (mt/test-drivers (mt/sql-jdbc-drivers)
     (mt/dataset (mt/dataset-definition "custom-pk"
                   ["user"
-                   [{:field-name "uuid" :base-type :type/UUID :pk? true}]
-                   [[#uuid "201014e3-81bc-4349-a64f-d5519571297b"]]]
+                   [{:field-name "email" :base-type :type/Text :pk? true}]
+                   [["ngoc@metabase.com"]]]
                   ["group"
-                   [{:field-name "user_uuid" :base-type :type/UUID :fk "user"}]
-                   [[#uuid "201014e3-81bc-4349-a64f-d5519571297b"]]])
+                   [{:field-name "user_email" :base-type :type/Text :fk "user"}]
+                   [["ngoc@metabase.com"]]])
       (let [user-fields  (t2/select [:model/Field :name :semantic_type :fk_target_field_id] :table_id (mt/id :user))
             group-fields (t2/select [:model/Field :name :semantic_type :fk_target_field_id] :table_id (mt/id :group))]
-        (testing "user.uuid is PK"
-          (is (= [{:name               (ddl.i/format-name driver/*driver* "uuid")
+        (testing "user.email is a PK"
+          (is (= [{:name               (ddl.i/format-name driver/*driver* "email")
                    :fk_target_field_id nil
                    :semantic_type      :type/PK}]
                  user-fields)))
-        (testing "user_uuid is FK of user table"
-          (is (= #{{:name               (ddl.i/format-name driver/*driver* "user_uuid")
-                    :fk_target_field_id (mt/id :user :uuid)
+        (testing "user_email is a FK non user.email"
+          (is (= #{{:name               (ddl.i/format-name driver/*driver* "user_email")
+                    :fk_target_field_id (mt/id :user :email)
                     :semantic_type      :type/FK}
                    {:name               (ddl.i/format-name driver/*driver* "id")
                     :fk_target_field_id nil
