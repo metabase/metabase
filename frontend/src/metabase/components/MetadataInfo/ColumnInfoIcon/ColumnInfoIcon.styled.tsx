45372031bebe6e31dc0974cd7f16a10e34e1de2f
Romeo Van Snick <romeo@romeovansnick.be>
2024-02-22 14:36:36 +0100

    Never shrink icon

--- frontend/src/metabase/components/MetadataInfo/ColumnInfoIcon/ColumnInfoIcon.styled.tsx
+++ frontend/src/metabase/components/MetadataInfo/ColumnInfoIcon/ColumnInfoIcon.styled.tsx
@@ -5,6 +5,7 @@ import { Icon } from "metabase/ui";
 export const PopoverHoverTarget = styled(Icon)<{ hasDescription: boolean }>`
   padding: 0.7em 0.65em;
   visibility: hidden;
+  flex-shrink: 0;
   opacity: ${props => (props.hasDescription ? 0.6 : 0.3)};
 
   &[aria-expanded="true"] {
