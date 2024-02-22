b00fe00f44b7a7727d1752d09693fc02333392de
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-02-22 14:21:08 +0700

    Fix can't select dashboard tabs in interactive embedding with parameter `header=false` (#39007)
    * Remove full-app to a newer term
    
    * Fix dashboard not loading in interactive embedding with header=false
    header=false
    
    * Fix can't choose dashboard tabs in interactive embedding w/ header=false

--- frontend/src/metabase/dashboard/components/DashboardHeader/DashboardHeaderView.tsx
+++ frontend/src/metabase/dashboard/components/DashboardHeader/DashboardHeaderView.tsx
@@ -4,14 +4,16 @@ import { t } from "ttag";
 import cx from "classnames";
 import type { Location } from "history";
 
-import { color } from "metabase/lib/colors";
-import type { Collection, Dashboard } from "metabase-types/api";
-
 import EditBar from "metabase/components/EditBar";
-import { PLUGIN_COLLECTION_COMPONENTS } from "metabase/plugins";
-import { useDispatch, useSelector } from "metabase/lib/redux";
 import { updateDashboard } from "metabase/dashboard/actions";
-import { getIsSidebarOpen } from "metabase/dashboard/selectors";
+import {
+  getIsHeaderVisible,
+  getIsSidebarOpen,
+} from "metabase/dashboard/selectors";
+import { color } from "metabase/lib/colors";
+import { useDispatch, useSelector } from "metabase/lib/redux";
+import { PLUGIN_COLLECTION_COMPONENTS } from "metabase/plugins";
+import type { Collection, Dashboard } from "metabase-types/api";
 
 import {
   EditWarning,
@@ -71,6 +73,7 @@ export function DashboardHeaderComponent({
   const dispatch = useDispatch();
 
   const isSidebarOpen = useSelector(getIsSidebarOpen);
+  const isDashboardHeaderVisible = useSelector(getIsHeaderVisible);
 
   const _headerButtons = useMemo(
     () => (
@@ -121,52 +124,54 @@ export function DashboardHeaderComponent({
         isFixedWidth={dashboard?.width === "fixed"}
         isSidebarOpen={isSidebarOpen}
       >
-        <HeaderRow
-          className={cx("QueryBuilder-section", headerClassName)}
-          data-testid="dashboard-header"
-          ref={header}
-        >
-          <HeaderFixedWidthContainer
-            data-testid="fixed-width-dashboard-header"
-            isNavBarOpen={isNavBarOpen}
-            isFixedWidth={dashboard?.width === "fixed"}
+        {isDashboardHeaderVisible && (
+          <HeaderRow
+            className={cx("QueryBuilder-section", headerClassName)}
+            data-testid="dashboard-header"
+            ref={header}
           >
-            <HeaderContent
-              role="heading"
-              hasSubHeader
-              showSubHeader={showSubHeader}
+            <HeaderFixedWidthContainer
+              data-testid="fixed-width-dashboard-header"
+              isNavBarOpen={isNavBarOpen}
+              isFixedWidth={dashboard?.width === "fixed"}
             >
-              <HeaderCaptionContainer>
-                <HeaderCaption
-                  key={dashboard.name}
-                  initialValue={dashboard.name}
-                  placeholder={t`Add title`}
-                  isDisabled={!dashboard.can_write}
-                  data-testid="dashboard-name-heading"
-                  onChange={handleUpdateCaption}
-                />
-                <PLUGIN_COLLECTION_COMPONENTS.CollectionInstanceAnalyticsIcon
-                  color={color("brand")}
-                  collection={collection}
-                  entity="dashboard"
-                />
-              </HeaderCaptionContainer>
-              <HeaderBadges>
-                {isLastEditInfoVisible && (
-                  <HeaderLastEditInfoLabel
-                    item={dashboard}
-                    onClick={onLastEditInfoClick}
-                    className=""
+              <HeaderContent
+                role="heading"
+                hasSubHeader
+                showSubHeader={showSubHeader}
+              >
+                <HeaderCaptionContainer>
+                  <HeaderCaption
+                    key={dashboard.name}
+                    initialValue={dashboard.name}
+                    placeholder={t`Add title`}
+                    isDisabled={!dashboard.can_write}
+                    data-testid="dashboard-name-heading"
+                    onChange={handleUpdateCaption}
                   />
-                )}
-              </HeaderBadges>
-            </HeaderContent>
+                  <PLUGIN_COLLECTION_COMPONENTS.CollectionInstanceAnalyticsIcon
+                    color={color("brand")}
+                    collection={collection}
+                    entity="dashboard"
+                  />
+                </HeaderCaptionContainer>
+                <HeaderBadges>
+                  {isLastEditInfoVisible && (
+                    <HeaderLastEditInfoLabel
+                      item={dashboard}
+                      onClick={onLastEditInfoClick}
+                      className=""
+                    />
+                  )}
+                </HeaderBadges>
+              </HeaderContent>
 
-            <HeaderButtonsContainer isNavBarOpen={isNavBarOpen}>
-              {_headerButtons}
-            </HeaderButtonsContainer>
-          </HeaderFixedWidthContainer>
-        </HeaderRow>
+              <HeaderButtonsContainer isNavBarOpen={isNavBarOpen}>
+                {_headerButtons}
+              </HeaderButtonsContainer>
+            </HeaderFixedWidthContainer>
+          </HeaderRow>
+        )}
         <HeaderRow>
           <HeaderFixedWidthContainer
             data-testid="fixed-width-dashboard-tabs"
