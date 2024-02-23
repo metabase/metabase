e999c737c417cd53839fba2780c6e965eb98d704
Alexander Solovyov <alexander@solovyov.net>
2024-02-23 12:33:35 +0200

    handle review comments

--- enterprise/backend/src/metabase_enterprise/task/caching.clj
+++ enterprise/backend/src/metabase_enterprise/task/caching.clj
@@ -14,49 +14,45 @@
 
 (set! *warn-on-reflection* true)
 
+(defn- select-ready-to-run [strategy]
+  (t2/select :model/CacheConfig :strategy strategy {:where [:or
+                                                            [:= :next_run_at nil]
+                                                            [:<= :next_run_at (t/offset-date-time)]]}))
+
+(defn- calc-next-run [^String schedule]
+  (let [^MutableTrigger cron (cron/finalize (cron/cron-schedule schedule))]
+    ;; needed by the tests, or the cron will use its own current date
+    (.setStartTime cron (t/java-date))
+    (-> (.getFireTimeAfter cron (t/java-date (t/offset-date-time)))
+        (t/offset-date-time (t/zone-offset)))))
+
 (defn- refresh-schedule-configs []
-  (let [now (t/offset-date-time)]
-    (doseq [item (t2/select :model/CacheConfig :strategy :schedule {:where [:or
-                                                                            [:= :next_run_at nil]
-                                                                            [:<= :next_run_at [:now]]]})
-            :let [cron     (-> (cron/cron-schedule (-> item :config :schedule))
-                               cron/finalize)
-                  ;; needed by the tests, or the cron will use it's own current date
-                  _        (.setStartTime ^MutableTrigger cron (t/java-date))
-                  next-run (-> (.getFireTimeAfter ^MutableTrigger cron (t/java-date (t/offset-date-time)))
-                               (t/offset-date-time (t/zone-offset)))]]
-      (t2/update! :model/CacheConfig {:id (:id item)} {:updated_at  now
-                                                       :next_run_at next-run}))))
+  (count
+   (for [{:keys [id config]} (select-ready-to-run :schedule)]
+     (t2/update! :model/CacheConfig {:id id}
+                 {:next_run_at     (calc-next-run (:schedule config))
+                  :last_expired_at (t/offset-date-time)}))))
 
 (defn- refresh-query-configs []
-  (let [now     (t/offset-date-time)
-        configs (t2/select :model/CacheConfig :strategy :query {:where [:or
-                                                                        [:= :next_run_at nil]
-                                                                        [:<= :next_run_at [:now]]]})
-        fields  (m/index-by :id
-                            (t2/select :model/Field :id [:in (map #(-> % :config :field_id) configs)]))
-        tables  (m/index-by :id
-                            (t2/select :model/Table :id [:in (map :table_id (vals fields))]))]
-    (doseq [item configs
-            :let [config   (:config item)
-                  field    (get fields (:field_id config))
-                  table    (get tables (:table_id field))
-                  cron     (-> (cron/cron-schedule (:schedule config))
-                               cron/finalize)
-                  ;; needed by the tests, or the cron will use it's own current date
-                  _        (.setStartTime ^MutableTrigger cron (t/java-date))
-                  next-run (-> (.getFireTimeAfter ^MutableTrigger cron (t/java-date (t/offset-date-time)))
-                               (t/offset-date-time (t/zone-offset)))
-                  query    {:database (:db_id table)
-                            :type     :query
-                            :query    {:source-table (:table_id field)
-                                       :aggregation  [(:aggregation config)
-                                                      [:field (:id field) nil]]}}
-                  result   (-> (qp/process-query query) :data :rows ffirst)]]
-      (when (not= result (:payload item))
-        (t2/update! :model/CacheConfig {:id (:id item)} {:updated_at  now
-                                                         :next_run_at next-run
-                                                         :payload     result})))))
+  (if-let [configs (seq (select-ready-to-run :query))]
+    (let [fields (m/index-by :id (t2/select :model/Field :id [:in (map #(-> % :config :field_id) configs)]))
+          tables (m/index-by :id (t2/select :model/Table :id [:in (map :table_id (vals fields))]))]
+      (count
+       (for [{:keys [id config] :as item} configs
+             :let [field       (get fields (:field_id config))
+                   table       (get tables (:table_id field))
+                   query       {:database (:db_id table)
+                                :type     :query
+                                :query    {:source-table (:table_id field)
+                                           :aggregation  [(:aggregation config) [:field (:id field) nil]]}}
+                   result      (-> (qp/process-query query) :data :rows ffirst)
+                   next-run-at (calc-next-run (:schedule config))
+                   marker      (:marker (:state item))]]
+         (t2/update! :model/CacheConfig {:id id}
+                     (cond-> {:next_run_at next-run-at}
+                       (not= marker result) (assoc :state {:marker result}
+                                                   :last_expired_at (t/offset-date-time)))))))
+    0))
 
 (jobs/defjob ^{org.quartz.DisallowConcurrentExecution true
                :doc                                   "Refresh 'schedule' caches"}
