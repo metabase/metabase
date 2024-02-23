0e8739bb00ddf22feb14e465b5cc1f7aa84b1c5f
Ngoc Khuat <qn.khuat@gmail.com>
2024-02-23 13:08:35 +0700

    don't limit drivers :)

--- test/metabase/test/data/dataset_definition_test.clj
+++ test/metabase/test/data/dataset_definition_test.clj
@@ -6,8 +6,6 @@
    [metabase.test :as mt]
    [toucan2.core :as t2]))
 
-(mt/set-test-drivers! #{:h2 :postgres :oracle})
-
 (deftest dataset-with-custom-pk-test
   (mt/test-drivers (mt/sql-jdbc-drivers)
     (mt/dataset (mt/dataset-definition "custom-pk"
