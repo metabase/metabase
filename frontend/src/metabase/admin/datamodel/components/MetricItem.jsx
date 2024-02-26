74b73d9f91fc7089afe407c83d03d1690b506eb6
Ryan Laurie <iethree@gmail.com>
2024-02-26 08:36:59 -0700

    import order fix

--- frontend/src/metabase/admin/datamodel/components/MetricItem.jsx
+++ frontend/src/metabase/admin/datamodel/components/MetricItem.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Component } from "react";
-
 import { t } from "ttag";
+
 import { Icon } from "metabase/ui";
 
 import ObjectActionSelect from "./ObjectActionSelect";
