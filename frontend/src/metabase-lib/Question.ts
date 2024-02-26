bcc488574043c6094cfa9fc53ebad466df615f51
Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>
2024-02-26 22:43:57 +0300

    Fix applying default values for a pinned sql question (#39156)
    * Fix applying default values for a pinned question
    
    * fix typecheck
    
    * Add a comment

--- frontend/src/metabase-lib/Question.ts
+++ frontend/src/metabase-lib/Question.ts
@@ -770,11 +770,13 @@ class Question {
     return question;
   }
 
-  parameters(): ParameterObject[] {
+  parameters({ collectionPreview } = {}): ParameterObject[] {
     return getCardUiParameters(
       this.card(),
       this.metadata(),
       this._parameterValues,
+      undefined,
+      collectionPreview,
     );
   }
 
