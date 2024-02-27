6ca69f039bd6542da0e7e65bcf2902d44d6d9582
Nicolò Pretto <info@npretto.com>
2024-02-27 18:25:49 +0100

    fix: border color not red for unable_to_valiadate case

--- frontend/src/metabase/setup/components/LicenseTokenStep/LicenseTokenForm.tsx
+++ frontend/src/metabase/setup/components/LicenseTokenStep/LicenseTokenForm.tsx
@@ -52,7 +52,7 @@ export const LicenseTokenForm = ({
               oldState !== "loading" ? "idle" : "loading",
             );
           }}
-          error={status === "invalid_token"}
+          error={["invalid_token", "unable_to_validate"].includes(status)}
         />
         {status === "invalid_token" && (
           <Text color="error">{t`This token doesn’t seem to be valid. Double-check it, then contact support if you think it should be working`}</Text>
