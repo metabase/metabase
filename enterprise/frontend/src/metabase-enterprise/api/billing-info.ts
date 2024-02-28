fb8b3df01b0e034646fa4a0b69ba4ad4601da0f9
Ryan Laurie <iethree@gmail.com>
2024-02-28 13:52:49 -0700

    use rtk for billing info
    # Conflicts:
    #	enterprise/frontend/src/metabase-enterprise/api/index.ts

---
+++ enterprise/frontend/src/metabase-enterprise/api/billing-info.ts
@@ -0,0 +1,13 @@
+import { Api } from "metabase/api";
+import type { BillingInfo } from "metabase-types/api";
+
+export const billingInfoApi = Api.injectEndpoints({
+  endpoints: builder => ({
+    getBillingInfo: builder.query<BillingInfo, void>({
+      query: () => ({
+        method: "GET",
+        url: "/api/ee/billing",
+      }),
+    }),
+  }),
+});
