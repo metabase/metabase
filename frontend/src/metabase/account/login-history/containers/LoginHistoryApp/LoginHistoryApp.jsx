631036fb4179e549885232a575df03913bff6ac4
Ryan Laurie <iethree@gmail.com>
2024-02-28 14:16:37 -0700

    replace trivial loginHistory entity

--- frontend/src/metabase/account/login-history/containers/LoginHistoryApp/LoginHistoryApp.jsx
+++ frontend/src/metabase/account/login-history/containers/LoginHistoryApp/LoginHistoryApp.jsx
@@ -1,5 +1,18 @@
-import LoginHistory from "metabase/entities/loginHistory";
+import { loginHistoryApi } from "metabase/api/login-history";
+import { LoadingAndErrorWrapper } from "metabase/public/containers/PublicAction/PublicAction.styled";
 
 import LoginHistoryList from "../../components/LoginHistory";
 
-export default LoginHistory.loadList()(LoginHistoryList);
+export default function LoginHistoryApp() {
+  const {
+    data: loginHistory,
+    error,
+    isLoading,
+  } = loginHistoryApi.useGetLoginHistoryQuery();
+
+  if (isLoading || error) {
+    return <LoadingAndErrorWrapper loading={isLoading} error={error} />;
+  }
+
+  return <LoginHistoryList loginHistory={loginHistory} />;
+}
---
+++ frontend/src/metabase/api/login-history.ts
@@ -0,0 +1,13 @@
+import { Api } from "metabase/api";
+import type { UserLoginHistory } from "metabase-types/api";
+
+export const loginHistoryApi = Api.injectEndpoints({
+  endpoints: builder => ({
+    getLoginHistory: builder.query<UserLoginHistory, void>({
+      query: () => ({
+        method: "GET",
+        url: "/api/login-history/current",
+      }),
+    }),
+  }),
+});
