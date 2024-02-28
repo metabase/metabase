8d4f57ec35a111dd45daf485b8805a338772a6c0
Case Nelson <case@metabase.com>
2024-02-28 16:54:12 -0700

    Fix snowflake ssh tunnel

--- modules/drivers/snowflake/src/metabase/driver/snowflake.clj
+++ modules/drivers/snowflake/src/metabase/driver/snowflake.clj
@@ -557,12 +557,15 @@
   #{"INFORMATION_SCHEMA"})
 
 (defmethod driver/can-connect? :snowflake
-  [driver {:keys [db], :as details}]
-  (and ((get-method driver/can-connect? :sql-jdbc) driver details)
-       (sql-jdbc.conn/with-connection-spec-for-testing-connection [spec [driver details]]
-         ;; jdbc/query is used to see if we throw, we want to ignore the results
-         (jdbc/query spec (format "SHOW OBJECTS IN DATABASE \"%s\";" db))
-         true)))
+  [driver {:keys [db host port account], :as details}]
+  (let [details (cond-> details
+                  (not host) (assoc :host (str account ".snowflakecomputing.com"))
+                  (not port) (assoc :port 443))]
+    (and ((get-method driver/can-connect? :sql-jdbc) driver details)
+         (sql-jdbc.conn/with-connection-spec-for-testing-connection [spec [driver details]]
+           ;; jdbc/query is used to see if we throw, we want to ignore the results
+           (jdbc/query spec (format "SHOW OBJECTS IN DATABASE \"%s\";" db))
+           true))))
 
 (defmethod driver/normalize-db-details :snowflake
   [_ database]
