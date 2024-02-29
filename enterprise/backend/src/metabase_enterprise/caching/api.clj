cf516b891a61f6563c99ce30eb37c1ffa4a13334
Alexander Solovyov <alexander@solovyov.net>
2024-02-29 06:58:16 -0800

    caching get api should have correct shape

--- enterprise/backend/src/metabase_enterprise/caching/api.clj
+++ enterprise/backend/src/metabase_enterprise/caching/api.clj
@@ -9,27 +9,51 @@
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
 
+(defn- assert-valid-model [model model-id]
+  (if (= model "root")
+    (when-not (zero? model-id)
+      (throw (ex-info (tru "Root configuration is only valid with model_id = 0") {:status-code 400
+                                                                                  :model_id    model-id})))
+    (api/check-404 (t2/select-one (case model
+                                    "database"  :model/Database
+                                    "dashboard" :model/Dashboard
+                                    "question"  :model/Card)
+                                  :id model-id))))
+
 (api/defendpoint GET "/"
-   "Return cache configuration."
+  "Return cache configuration."
   [:as {{:strs [model collection]
          :or   {model "root"}}
         :query-params}]
   {model      [:set {:decode/string (fn [x] (cond (set? x)    x
                                                   (vector? x) (set x)
                                                   (some? x)   #{x}))}
-               [:enum "root" "database" "collection" "dashboard" "question"]]
+               [:enum "root" "database" "dashboard" "question"]]
    collection [:maybe ms/PositiveInt]}
   (validation/check-has-application-permission :setting)
-
-  (let [model (cond-> model
-                (some #{"dashboard" "question"} model) (conj "collection"))
-        items (t2/select :model/CacheConfig :model [:in model] :collection_id collection)]
-    {:items items}))
+  (let [items (t2/select :model/CacheConfig
+                         :model [:in model]
+                         {:left-join [:report_card      [:and
+                                                         [:= :model [:inline "question"]]
+                                                         [:= :model_id :report_card.id]
+                                                         [:= :report_card.collection_id collection]]
+                                      :report_dashboard [:and
+                                                         [:= :model [:inline "dashboard"]]
+                                                         [:= :model_id :report_dashboard.id]
+                                                         [:= :report_dashboard.collection_id collection]]]
+                          :where     [:case
+                                      [:= :model [:inline "question"]]  [:!= :report_card.id nil]
+                                      [:= :model [:inline "dashboard"]] [:!= :report_dashboard.id nil]
+                                      :else                             true]})]
+    {:items (for [item items]
+              {:model    (:model item)
+               :model_id (:model_id item)
+               :strategy (assoc (:config items) :type (:strategy item))})}))
 
 (api/defendpoint PUT "/"
   "Store cache configuration."
   [:as {{:keys [model model_id strategy]} :body}]
-  {model    [:enum "root" "database" "collection" "dashboard" "question"]
+  {model    [:enum "root" "database" "dashboard" "question"]
    model_id ms/IntGreaterThanOrEqualToZero
    strategy [:and
              [:map
@@ -54,35 +78,20 @@
                           [:aggregation [:enum "max" "count"]]
                           [:schedule u.cron/CronScheduleString]]]]]}
   (validation/check-has-application-permission :setting)
-  (when (and (= model "root") (not= model_id 0))
-    (throw (ex-info (tru "Root configuration is only valid with model_id = 0") {:status-code 400
-                                                                                :model_id    model_id})))
-  (let [entity (when-not (= model "root")
-                 (api/check-404 (t2/select-one (case model
-                                                 "database"   :model/Database
-                                                 "collection" :model/Collection
-                                                 "dashboard"  :model/Dashboard
-                                                 "question"   :model/Card)
-                                               :id model_id)))
-        cid    (if (= model "collection")
-                 (:parent_id (t2/hydrate entity :parent_id))
-                 (:collection_id entity))
-        data   {:model         model
-                :model_id      model_id
-                :collection_id cid
-                :strategy      (:type strategy)
-                :config        (dissoc strategy :type)}]
+  (assert-valid-model model model_id)
+  (let [data {:model    model
+              :model_id model_id
+              :strategy (:type strategy)
+              :config   (dissoc strategy :type)}]
     {:id (or (first (t2/update-returning-pks! :model/CacheConfig {:model model :model_id model_id} data))
              (t2/insert-returning-pk! :model/CacheConfig data))}))
 
 (api/defendpoint DELETE "/"
   [:as {{:keys [model model_id]} :body}]
-  {model    [:enum "root" "database" "collection" "dashboard" "question"]
+  {model    [:enum "root" "database" "dashboard" "question"]
    model_id ms/PositiveInt}
   (validation/check-has-application-permission :setting)
-  (when (and (= model "root") (not= model_id 0))
-    (throw (ex-info (tru "Root configuration is only valid with model_id = 0") {:status-code 400
-                                                                                :model_id model_id})))
+  (assert-valid-model model model_id)
   (t2/delete! :model/CacheConfig :model model :model_id model_id)
   nil)
 
