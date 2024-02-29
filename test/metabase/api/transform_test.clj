ed45f2caf3e082adec9e96ab9b61b2ec7a9d256c
John Swanson <john.swanson@metabase.com>
2024-02-29 10:28:54 -0800

    enforce new permissions on transform

--- test/metabase/api/transform_test.clj
+++ test/metabase/api/transform_test.clj
@@ -3,11 +3,14 @@
    [clojure.test :refer :all]
    [metabase.models.card :refer [Card]]
    [metabase.models.collection :refer [Collection]]
+   [metabase.models.data-permissions :as data-perms]
+   [metabase.models.permissions-group :as perms-group]
    [metabase.query-processor :as qp]
    [metabase.test :as mt]
    [metabase.test.domain-entities :refer [with-test-domain-entity-specs]]
    [metabase.test.fixtures :as fixtures]
-   [metabase.test.transforms :refer [with-test-transform-specs]]))
+   [metabase.test.transforms :refer [with-test-transform-specs]]
+   [metabase.util :as u]))
 
 (use-fixtures :once (fixtures/initialize :db))
 
@@ -18,17 +21,26 @@
   (testing "GET /api/transform/:db-id/:schema/:transform-name"
     (testing "Run the transform and make sure it produces the correct result"
       (mt/with-test-user :crowberto
-        (with-test-transform-specs
-          (with-test-domain-entity-specs
-            (mt/with-model-cleanup [Card Collection]
-              (is (= [[1 "Red Medicine" 4 10.065 -165.374 3 1.5 4 3 2 1]
-                      [2 "Stout Burgers & Beers" 11 34.1 -118.329 2 1.1 11 2 1 1]
-                      [3 "The Apple Pan" 11 34.041 -118.428 2 1.1 11 2 1 1]]
-                     (mt/formatted-rows [int str int 3.0 3.0 int 1.0 int int int int]
-                       (-> (mt/user-http-request :rasta :get 200 (test-endpoint))
-                           first
-                           :dataset_query
-                           qp/process-query)))))))))))
+        (mt/with-restored-data-perms!
+          (data-perms/set-database-permission! (u/the-id (perms-group/all-users))
+                                               (mt/id)
+                                               :perms/view-data
+                                               :unrestricted)
+          (data-perms/set-database-permission! (u/the-id (perms-group/all-users))
+                                               (mt/id)
+                                               :perms/create-queries
+                                               :query-builder-and-native)
+          (with-test-transform-specs
+            (with-test-domain-entity-specs
+              (mt/with-model-cleanup [Card Collection]
+                (is (= [[1 "Red Medicine" 4 10.065 -165.374 3 1.5 4 3 2 1]
+                        [2 "Stout Burgers & Beers" 11 34.1 -118.329 2 1.1 11 2 1 1]
+                        [3 "The Apple Pan" 11 34.041 -118.428 2 1.1 11 2 1 1]]
+                       (mt/formatted-rows [int str int 3.0 3.0 int 1.0 int int int int]
+                         (-> (mt/user-http-request :rasta :get 200 (test-endpoint))
+                             first
+                             :dataset_query
+                             qp/process-query))))))))))))
 
 (deftest permissions-test
   (testing "GET /api/transform/:db-id/:schema/:transform-name"
