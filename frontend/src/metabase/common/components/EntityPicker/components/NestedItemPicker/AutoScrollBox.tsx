e0eae86d5b9044d037bc6d7d958ea40084732965
Ryan Laurie <iethree@gmail.com>
2024-02-29 13:26:19 -0700

    ok raffi was right about the scrolling

--- frontend/src/metabase/common/components/EntityPicker/components/NestedItemPicker/AutoScrollBox.tsx
+++ frontend/src/metabase/common/components/EntityPicker/components/NestedItemPicker/AutoScrollBox.tsx
@@ -1,37 +1,18 @@
 import type React from "react";
-import { useEffect, useRef } from "react";
+import { useRef, useEffect } from "react";
 import { usePrevious } from "react-use";
 
 import { HorizontalScrollBox } from "./NestedItemPicker.styled";
 
-const gradualScroll = (container: HTMLDivElement | null) => {
+const scrollRight = (
+  container: HTMLDivElement | null,
+  behavior: "smooth" | "auto",
+) => {
   if (!container) {
     return;
   }
-  const intervalId = setInterval(() => {
-    // This is actually a fairly fussy check. It's possible for the scrollWidth to be higher
-    // than what the scrollLeft + clientWidth can ever be, causing you to constantly scroll
-    // right.
-    if (
-      container.scrollLeft + container.clientWidth <=
-      container.scrollWidth * 0.99
-    ) {
-      container.scrollLeft += 25;
-    } else {
-      clearInterval(intervalId);
-    }
-  }, 10);
-};
-
-const scrollAllTheWay = (container: HTMLDivElement | null) => {
-  if (!container) {
-    return;
-  }
-
-  if (container.clientWidth < container.scrollWidth) {
-    const diff = container.scrollWidth - container.clientWidth;
-    container.scrollLeft += diff;
-  }
+  const diff = container.scrollWidth - container.clientWidth;
+  container.scrollBy({ left: diff, behavior });
 };
 
 export const AutoScrollBox = ({
@@ -44,26 +25,21 @@ export const AutoScrollBox = ({
   props?: React.HTMLAttributes<HTMLDivElement>;
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const containerWidth = containerRef.current?.scrollWidth;
-  const previousContainerWidth = usePrevious(containerWidth);
+  const previousContainerRef = usePrevious(containerRef.current);
   const previousContentHash = usePrevious(contentHash);
 
   useEffect(() => {
-    if (!containerWidth) {
+    if (!containerRef?.current || !contentHash) {
       return;
     }
 
-    if (!previousContainerWidth) {
-      scrollAllTheWay(containerRef.current);
-    } else if (contentHash !== previousContentHash) {
-      gradualScroll(containerRef.current);
+    if (contentHash !== previousContentHash) {
+      scrollRight(
+        containerRef.current,
+        !previousContainerRef ? "auto" : "smooth",
+      );
     }
-  }, [
-    containerWidth,
-    previousContainerWidth,
-    contentHash,
-    previousContentHash,
-  ]);
+  }, [previousContentHash, contentHash, previousContainerRef]);
 
   return (
     <HorizontalScrollBox h="100%" {...props} ref={containerRef}>
