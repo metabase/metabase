fe3238193053a8730310de5183cf8e574613a253
Kamil Mielnik <kamil@kamilmielnik.com>
2024-03-01 20:30:21 +0700

    Add a repro for #34688

--- e2e/test/scenarios/binning/reproductions/34690-time-series-footer.cy.spec.js
+++ e2e/test/scenarios/binning/reproductions/34690-time-series-footer.cy.spec.js
@@ -4,64 +4,87 @@ import { restore } from "e2e/support/helpers";
 
 const { PRODUCTS_ID, PRODUCTS } = SAMPLE_DATABASE;
 
-const question = {
-  type: "query",
-  query: {
-    "source-table": PRODUCTS_ID,
-    expressions: {
-      "Custom column": [
-        "case",
+const BASE_QUERY = {
+  "source-table": PRODUCTS_ID,
+  expressions: {
+    "Custom column": [
+      "case",
+      [
         [
           [
+            "<",
             [
-              "<",
-              [
-                "field",
-                PRODUCTS.ID,
-                {
-                  "base-type": "type/BigInteger",
-                },
-              ],
-              10,
+              "field",
+              PRODUCTS.ID,
+              {
+                "base-type": "type/BigInteger",
+              },
             ],
-            "Foo",
+            10,
           ],
+          "Foo",
         ],
-        {
-          default: "Bar",
-        },
       ],
-    },
-    aggregation: [["count"]],
-    breakout: [
-      [
-        "field",
-        PRODUCTS.CREATED_AT,
-        {
-          "base-type": "type/DateTime",
-          "temporal-unit": "month",
-        },
-      ],
-      [
-        "expression",
-        "Custom column",
-        {
-          "base-type": "type/Text",
-        },
-      ],
+      {
+        default: "Bar",
+      },
     ],
   },
-  database: SAMPLE_DB_ID,
+  aggregation: [["count"]],
 };
 
-describe("issue 34690", () => {
+const CREATED_AT_BREAKOUT = [
+  "field",
+  PRODUCTS.CREATED_AT,
+  {
+    "base-type": "type/DateTime",
+    "temporal-unit": "month",
+  },
+];
+
+const CUSTOM_COLUMN_BREAKOUT = [
+  "expression",
+  "Custom column",
+  {
+    "base-type": "type/Text",
+  },
+];
+
+describe("issues 34688 and 34690", () => {
   beforeEach(() => {
     restore();
     cy.signInAsNormalUser();
   });
 
+  it("shows time series footer when there is a category breakout (metabase#34688)", () => {
+    cy.createQuestion(
+      {
+        database: SAMPLE_DB_ID,
+        type: "query",
+        query: {
+          ...BASE_QUERY,
+          breakout: [CUSTOM_COLUMN_BREAKOUT, CREATED_AT_BREAKOUT],
+        },
+      },
+      { visitQuestion: true },
+    );
+
+    cy.findByTestId("timeseries-filter-button").should("exist");
+    cy.findByTestId("timeseries-bucket-button").should("exist");
+  });
+
   it("shows time series footer when there is a category breakout (metabase#34690)", () => {
-    cy.createQuestion(question, { visitQuestion: true });
+    cy.createQuestion(
+      {
+        database: SAMPLE_DB_ID,
+        type: "query",
+        query: {
+          ...BASE_QUERY,
+          breakout: [CREATED_AT_BREAKOUT, CUSTOM_COLUMN_BREAKOUT],
+        },
+      },
+      { visitQuestion: true },
+    );
 
     cy.findByTestId("timeseries-filter-button").should("exist");
     cy.findByTestId("timeseries-bucket-button").should("exist");
