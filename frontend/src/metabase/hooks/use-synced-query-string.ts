0400f6399401c30ffec036b9c07ca340c7a20698
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2024-03-01 13:12:38 -0500

    setup two way sync for parameter values

--- frontend/src/metabase/hooks/use-synced-query-string.ts
+++ frontend/src/metabase/hooks/use-synced-query-string.ts
@@ -1,13 +1,35 @@
 import querystring from "querystring";
 import { useEffect } from "react";
+import { useLocation, usePrevious } from "react-use";
+import _ from "underscore";
 
+import { setParameterValuesFromQueryParams } from "metabase/dashboard/actions";
 import { IS_EMBED_PREVIEW } from "metabase/lib/embed";
+import { useDispatch } from "metabase/lib/redux";
 
 export function useSyncedQueryString(
   fn: () => Record<string, any>,
-  deps?: any[],
+  deps: any[],
 ) {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const previousLocation = usePrevious(location);
+  const previousDeps = usePrevious(deps);
+
   useEffect(() => {
+    // if the dependencies have not changed, but the location has,
+    // update the parameters to reflect the new URL state
+    if (
+      _.isEqual(previousDeps, deps) &&
+      previousLocation &&
+      previousLocation.search !== location.search
+    ) {
+      const search = location?.search?.substring(1) || "";
+      const query = querystring.parse(search);
+      dispatch(setParameterValuesFromQueryParams(query));
+      return;
+    }
+
     /**
      * We don't want to sync the query string to the URL because when previewing,
      * this changes the URL of the iframe by appending the query string to the src.
@@ -17,19 +39,22 @@ export function useSyncedQueryString(
     if (IS_EMBED_PREVIEW) {
       return;
     }
+
+    // if the dependencies have changed and the location does not match
+    // the dependency state,
+    // update the URL state to reflect the new parameter state
     const object = fn();
     const searchString = buildSearchString(object);
-
-    if (searchString !== window.location.search) {
+    if (!_.isEqual(previousDeps, deps) && searchString !== location.search) {
       history.replaceState(
         null,
         document.title,
-        window.location.pathname + searchString + window.location.hash,
+        location.pathname + searchString + location.hash,
       );
     }
     // exhaustive-deps is enabled for useSyncedQueryString so we don't need to include `fn` as a dependency
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [deps]);
+  }, [...deps, location]);
 }
 
 const QUERY_PARAMS_ALLOW_LIST = ["objectId", "tab"];
