889291a86b7e43063919e3c7c448329c6b5893f3
Anton Kulyk <kuliks.anton@gmail.com>
2024-03-01 17:25:23 +0000

    Convert `dashboard/actions/core` to TypeScript

--- frontend/src/metabase/dashboard/actions/core.ts
+++ frontend/src/metabase/dashboard/actions/core.ts
@@ -1,4 +1,11 @@
 import { createAction } from "metabase/lib/redux";
+import type {
+  DashCardId,
+  DashCardVisualizationSettings,
+  Dashboard,
+  DashboardCard,
+  DashboardId,
+} from "metabase-types/api";
 
 export const INITIALIZE = "metabase/dashboard/INITIALIZE";
 export const initialize = createAction(INITIALIZE);
@@ -7,16 +14,31 @@ export const RESET = "metabase/dashboard/RESET";
 export const reset = createAction(RESET);
 
 export const SET_EDITING_DASHBOARD = "metabase/dashboard/SET_EDITING_DASHBOARD";
-export const setEditingDashboard = createAction(SET_EDITING_DASHBOARD);
+export const setEditingDashboard = createAction<Dashboard | false>(
+  SET_EDITING_DASHBOARD,
+);
 
+export type SetDashboardAttributesOpts = {
+  id: DashboardId;
+  attributes: Partial<Dashboard>;
+};
 export const SET_DASHBOARD_ATTRIBUTES =
   "metabase/dashboard/SET_DASHBOARD_ATTRIBUTES";
-export const setDashboardAttributes = createAction(SET_DASHBOARD_ATTRIBUTES);
+export const setDashboardAttributes = createAction<SetDashboardAttributesOpts>(
+  SET_DASHBOARD_ATTRIBUTES,
+);
 
+export type SetDashCardAttributesOpts = {
+  id: DashCardId;
+  attributes: Partial<DashboardCard>;
+};
 export const SET_DASHCARD_ATTRIBUTES =
   "metabase/dashboard/SET_DASHCARD_ATTRIBUTES";
-export const setDashCardAttributes = createAction(SET_DASHCARD_ATTRIBUTES);
+export const setDashCardAttributes = createAction<SetDashCardAttributesOpts>(
+  SET_DASHCARD_ATTRIBUTES,
+);
 
+export type SetMultipleDashCardAttributesOpts = SetDashCardAttributesOpts[];
 export const SET_MULTIPLE_DASHCARD_ATTRIBUTES =
   "metabase/dashboard/SET_MULTIPLE_DASHCARD_ATTRIBUTES";
 export const setMultipleDashCardAttributes = createAction(
@@ -36,19 +58,29 @@ export const UPDATE_DASHCARD_VISUALIZATION_SETTINGS =
   "metabase/dashboard/UPDATE_DASHCARD_VISUALIZATION_SETTINGS";
 export const onUpdateDashCardVisualizationSettings = createAction(
   UPDATE_DASHCARD_VISUALIZATION_SETTINGS,
-  (id, settings) => ({ id, settings }),
+  (id: DashCardId, settings: DashCardVisualizationSettings) => ({
+    id,
+    settings,
+  }),
 );
 
 export const UPDATE_DASHCARD_VISUALIZATION_SETTINGS_FOR_COLUMN =
   "metabase/dashboard/UPDATE_DASHCARD_VISUALIZATION_SETTINGS_FOR_COLUMN";
 export const onUpdateDashCardColumnSettings = createAction(
   UPDATE_DASHCARD_VISUALIZATION_SETTINGS_FOR_COLUMN,
-  (id, column, settings) => ({ id, column, settings }),
+  (
+    id: DashCardId,
+    column: string,
+    settings?: Record<string, unknown> | null,
+  ) => ({ id, column, settings }),
 );
 
 export const REPLACE_ALL_DASHCARD_VISUALIZATION_SETTINGS =
   "metabase/dashboard/REPLACE_ALL_DASHCARD_VISUALIZATION_SETTINGS";
 export const onReplaceAllDashCardVisualizationSettings = createAction(
   REPLACE_ALL_DASHCARD_VISUALIZATION_SETTINGS,
-  (id, settings) => ({ id, settings }),
+  (id: DashCardId, settings: DashCardVisualizationSettings) => ({
+    id,
+    settings,
+  }),
 );
