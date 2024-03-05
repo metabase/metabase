d3a82a6b30adfd637ee00e3362091ceafe47734a
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-03-05 15:26:27 +0200

    Add condition

---
+++ frontend/src/metabase/query_builder/components/notebook/steps/JoinStep/JoinConditionDraft/JoinConditionDraft.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+import { Box, Flex } from "metabase/ui";
+import * as Lib from "metabase-lib";
+
+import { JoinConditionColumnPicker } from "../JoinConditionColumnPicker";
+import { JoinConditionOperatorPicker } from "../JoinConditionOperatorPicker";
+
+import { JoinConditionRoot } from "./JoinConditionDraft.styled";
+import { getDefaultJoinConditionOperator } from "./utils";
+
+interface JoinConditionDraftProps {
+  query: Lib.Query;
+  stageIndex: number;
+  joinable: Lib.Joinable;
+  isReadOnly: boolean;
+  onChange: (condition: Lib.JoinCondition) => void;
+}
+
+export function JoinConditionDraft({
+  query,
+  stageIndex,
+  joinable,
+  isReadOnly,
+  onChange,
+}: JoinConditionDraftProps) {
+  const [operator, setOperator] = useState(() =>
+    getDefaultJoinConditionOperator(query, stageIndex),
+  );
+  const [lhsColumn, setLhsColumn] = useState<Lib.ColumnMetadata>();
+  const [rhsColumn, setRhsColumn] = useState<Lib.ColumnMetadata>();
+
+  const handleColumnChange = (
+    lhsColumn: Lib.ColumnMetadata | undefined,
+    rhsColumn: Lib.ColumnMetadata | undefined,
+  ) => {
+    if (lhsColumn != null && rhsColumn != null) {
+      onChange(
+        Lib.joinConditionClause(
+          query,
+          stageIndex,
+          operator,
+          lhsColumn,
+          rhsColumn,
+        ),
+      );
+    }
+  };
+
+  const handleLhsColumnChange = (newLhsColumn: Lib.ColumnMetadata) => {
+    setLhsColumn(newLhsColumn);
+    handleColumnChange(newLhsColumn, rhsColumn);
+  };
+
+  const handleRhsColumnChange = (newRhsColumn: Lib.ColumnMetadata) => {
+    setRhsColumn(newRhsColumn);
+    handleColumnChange(lhsColumn, newRhsColumn);
+  };
+
+  return (
+    <JoinConditionRoot>
+      <Flex align="center" gap="4px" mih="47px" p="4px">
+        <Box ml={!lhsColumn ? "4px" : undefined}>
+          <JoinConditionColumnPicker
+            query={query}
+            stageIndex={stageIndex}
+            joinable={joinable}
+            lhsColumn={lhsColumn}
+            rhsColumn={rhsColumn}
+            isLhsColumn={true}
+            isReadOnly={isReadOnly}
+            onChange={handleLhsColumnChange}
+          />
+        </Box>
+        <JoinConditionOperatorPicker
+          query={query}
+          stageIndex={stageIndex}
+          operator={operator}
+          isReadOnly={isReadOnly}
+          onChange={setOperator}
+        />
+        <Box mr={!rhsColumn ? "4px" : undefined}>
+          <JoinConditionColumnPicker
+            query={query}
+            stageIndex={stageIndex}
+            joinable={joinable}
+            lhsColumn={lhsColumn}
+            rhsColumn={rhsColumn}
+            isLhsColumn={false}
+            isReadOnly={isReadOnly}
+            onChange={handleRhsColumnChange}
+          />
+        </Box>
+      </Flex>
+    </JoinConditionRoot>
+  );
+}
