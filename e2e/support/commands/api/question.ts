4d066429508371f0e581b32a388ad955b4aa44d7
Kamil Mielnik <kamil@kamilmielnik.com>
2024-03-05 14:49:33 +0700

    Type question function

--- e2e/support/commands/api/question.ts
+++ e2e/support/commands/api/question.ts
@@ -33,23 +33,23 @@ type QueryType = "query" | "native";
 
 interface QuestionDetails {
   /**
-   * Defaults to "test question"
+   * Defaults to "test question".
    */
   name?: Card["name"];
   description?: Card["description"];
   /**
    * Entity type.
-   * Defaults to "question"
+   * Defaults to "question".
    */
   type?: Card["type"];
   native: any; // TODO: make its presence depend on type
   query: any; // TODO: make its presence depend on type
   /**
-   * Defaults to SAMPLE_DB_ID
+   * Defaults to SAMPLE_DB_ID.
    */
   database?: DatasetQuery["database"];
   /**
-   * Defaults to "table"
+   * Defaults to "table".
    */
   display?: Card["display"];
   parameters?: Card["parameters"];
@@ -62,36 +62,41 @@ interface QuestionDetails {
    * Used on the frontend to determine whether the question is pinned or not.
    */
   collection_position?: Card["collection_position"];
-  embedding_params?: Card["embedding_params"]; // TODO: make its presence depend on type - only for models
+  embedding_params?: Card["embedding_params"]; // TODO: make its presence depend on type only for models
   /**
-   * Defaults to false
+   * Defaults to false.
    */
   enable_embedding?: Card["enable_embedding"];
 }
 
-/**
- *
- * @param {("query"|"native")} queryType
- *
- * @param {object} questionDetails
- * @param {string} [questionDetails.name="test question"]
- * @param {string} questionDetails.description
- * @param {("question"|"model")} questionDetails.type Entity type
- * @param {object} questionDetails.native
- * @param {object} questionDetails.query
- * @param {number} [questionDetails.database=1]
- * @param {string} [questionDetails.display="table"]
- * @param {object} [questionDetails.visualization_settings={}]
- * @param {number} [questionDetails.collection_id] - Parent collection in which to store this question.
- * @param {number} [questionDetails.collection_position] - used on the frontend to determine whether the question is pinned or not.
- *
- * @param {object} customOptions
- * @param {boolean} customOptions.loadMetadata - Whether to visit the question in order to load its metadata.
- * @param {boolean} customOptions.visitQuestion - Whether to visit the question after the creation or not.
- * @param {boolean} customOptions.wrapId - Whether to wrap a question id, to make it available outside of this scope.
- * @param {string} customOptions.idAlias - Alias a question id in order to use it later with `cy.get("@" + alias).
- * @param {string} customOptions.interceptAlias - We need distinctive endpoint aliases for cases where we have multiple questions or nested questions.
- */
+interface Options {
+  /**
+   * Whether to visit the question in order to load its metadata.
+   * Defaults to false.
+   */
+  loadMetadata?: boolean;
+  /**
+   * Whether to visit the question after the creation or not.
+   * Defaults to false.
+   */
+  visitQuestion?: boolean;
+  /**
+   * Whether to wrap a question id, to make it available outside of this scope.
+   * Defaults to false.
+   */
+  wrapId?: boolean;
+  /**
+   * Alias a question id in order to use it later with `cy.get("@" + alias).
+   * Defaults to "questionid".
+   */
+  idAlias?: string;
+  /**
+   * We need distinctive endpoint aliases for cases where we have multiple questions or nested questions.
+   * Defaults to "cardQuery".
+   */
+  interceptAlias?: string;
+}
+
 function question(
   queryType: QueryType,
   {
@@ -108,14 +113,14 @@ function question(
     collection_position,
     embedding_params,
     enable_embedding = false,
-  } = {},
+  }: QuestionDetails,
   {
     loadMetadata = false,
     visitQuestion = false,
     wrapId = false,
     idAlias = "questionId",
     interceptAlias = "cardQuery",
-  } = {},
+  }: Options = {},
 ) {
   return cy
     .request("POST", "/api/card", {
