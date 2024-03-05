b5d1fd01408a3ed867457953b694986e00122f9d
Kamil Mielnik <kamil@kamilmielnik.com>
2024-03-05 16:59:51 +0700

    Add boilerplate for 11994 repro

---
+++ e2e/test/scenarios/permissions/reproductions/11994-dirty-question.cy.spec.ts
@@ -0,0 +1,57 @@
+import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import { restore, visitQuestion } from "e2e/support/helpers";
+import type {
+  ConcreteFieldReference,
+  StructuredQuery,
+} from "metabase-types/api";
+
+const { ORDERS, ORDERS_ID } = SAMPLE_DATABASE;
+
+const ORDERS_TOTAL_FIELD: ConcreteFieldReference = [
+  "field",
+  ORDERS.TOTAL,
+  {
+    "base-type": "type/Float",
+  },
+];
+
+const CREATED_AT_MONTH_BREAKOUT: ConcreteFieldReference = [
+  "field",
+  ORDERS.CREATED_AT,
+  {
+    "base-type": "type/DateTime",
+    "temporal-unit": "month",
+  },
+];
+
+const QUERY: StructuredQuery = {
+  "source-table": ORDERS_ID,
+  aggregation: [["count"], ["sum", ORDERS_TOTAL_FIELD]],
+  breakout: [CREATED_AT_MONTH_BREAKOUT],
+};
+
+describe("issue 11994", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+    cy.createQuestion(
+      { database: SAMPLE_DB_ID, query: QUERY, display: "pivot" },
+      { wrapId: true, idAlias: "pivotQuestionId" },
+    );
+    cy.createQuestion(
+      { database: SAMPLE_DB_ID, query: QUERY, display: "combo" },
+      { wrapId: true, idAlias: "comboQuestionId" },
+    );
+    cy.signInAsSandboxedUser();
+  });
+
+  it.only("does not show raw data toggle for pivot questions (metabase#11994)", () => {
+    // TODO: refactor visitQuestion to accept alias or id.
+    cy.get("@pivotQuestionId").then(pivotQuestionId => {
+      visitQuestion(pivotQuestionId);
+    });
+  });
+
+  it("does not offer to save combo question viewed in raw mode (metabase#11994)", () => {});
+});
