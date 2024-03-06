d520af7dc20c68cd39837685ab713edd31a58207
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-03-06 12:03:04 +0300

    Allow passing theme to the SDK components

--- frontend/src/embedding-sdk/SdkProvider.tsx
+++ frontend/src/embedding-sdk/SdkProvider.tsx
@@ -2,13 +2,14 @@ import type * as React from "react";
 import { memo, useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import styled from "@emotion/styled";
+import type { MantineThemeOverride } from "@mantine/core";
 import { ThemeProvider } from "metabase/ui/components/theme/ThemeProvider";
 import { getStore } from "metabase/store";
 import reducers from "metabase/reducers-main";
-import { alpha, color } from "metabase/lib/colors";
 import { aceEditorStyles } from "metabase/query_builder/components/NativeQueryEditor/NativeQueryEditor.styled";
 import { saveDomImageStyles } from "metabase/visualizations/lib/save-chart-image";
 import { setOptions } from "metabase/redux/embed";
+import { color, alpha } from "metabase/ui/theme";
 
 import { SdkEmotionCacheProvider } from "./SdkEmotionCacheProvider";
 import { EmbeddingContext } from "./context";
@@ -21,9 +22,11 @@ import { useInitData } from "./hooks";
 const MetabaseProviderInternal = ({
   children,
   config,
+  theme,
 }: {
   children: React.ReactNode;
   config: SDKConfigType;
+  theme: MantineThemeOverride;
 }): JSX.Element => {
   const store = getStore(reducers);
 
@@ -52,7 +55,7 @@ const MetabaseProviderInternal = ({
     >
       <Provider store={store}>
         <SdkEmotionCacheProvider>
-          <ThemeProvider>
+          <ThemeProvider theme={theme}>
             <ContentWrapper id={SDK_CONTEXT_CLASS_NAME} font={font}>
               {!isInitialized ? <div>Initializing...</div> : children}
             </ContentWrapper>
