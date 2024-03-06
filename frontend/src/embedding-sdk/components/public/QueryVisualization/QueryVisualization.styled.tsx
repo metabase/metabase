1931e4207abe43f4bd28a2edd235d73a9b8b6d9b
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-03-06 12:03:25 +0300

    Fix SDK question visualization don't have background color
    Without the background color, the background from parents could leak
    into the SDK components.

--- frontend/src/embedding-sdk/components/public/QueryVisualization/QueryVisualization.styled.tsx
+++ frontend/src/embedding-sdk/components/public/QueryVisualization/QueryVisualization.styled.tsx
@@ -2,5 +2,6 @@ import styled from "@emotion/styled";
 import QueryVisualization from "metabase/query_builder/components/QueryVisualization";
 
 export const QueryVisualizationSdkWrapper = styled(QueryVisualization)`
+  background: white;
   flex: 1;
 `;
