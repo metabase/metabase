ccebcc5ca2e379b7ed4f67a55a1b1f254eec94c6
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-03-06 23:29:47 +0300

    Fix leaking styles

--- frontend/src/embedding-sdk/components/public/QueryVisualization/QueryVisualization.styled.tsx
+++ frontend/src/embedding-sdk/components/public/QueryVisualization/QueryVisualization.styled.tsx
@@ -1,7 +1,9 @@
 import styled from "@emotion/styled";
 import QueryVisualization from "metabase/query_builder/components/QueryVisualization";
+import { color } from "metabase/ui/utils/colors";
 
 export const QueryVisualizationSdkWrapper = styled(QueryVisualization)`
-  background: white;
+  background: ${color("white")};
+  color: ${color("text-dark")};
   flex: 1;
 `;
