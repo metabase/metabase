2bf68c1381649a7c7ef9daad8f5e884db5532484
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-03-06 14:30:52 -0500

    Disable animation on dashboard header popover

--- frontend/src/metabase/dashboard/components/DashboardHeader/DashboardHeader.tsx
+++ frontend/src/metabase/dashboard/components/DashboardHeader/DashboardHeader.tsx
@@ -590,6 +590,7 @@ export const DashboardHeader = (props: DashboardHeaderProps) => {
             items={extraButtons}
             triggerIcon="ellipsis"
             tooltip={t`Move, archive, and more...`}
+            transitionDuration={0}
           />,
         );
       }
