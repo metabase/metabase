58befcb018bd02006e55f8d4815f5c64693bd0f6
Braden Shepherdson <braden@metabase.com>
2024-03-07 09:24:36 -0500

    [MLv2] Introduce `swap-clauses` to reorder aggregations, etc.

--- src/metabase/lib/js.cljs
+++ src/metabase/lib/js.cljs
@@ -333,6 +333,17 @@
    (lib.core/normalize (js->clj target-clause :keywordize-keys true))
    (lib.core/normalize (js->clj new-clause :keywordize-keys true))))
 
+(defn ^:export swap-clauses
+  "Exchanges the positions of two clauses in a list. Can be used for filters, aggregations, breakouts, and expressions.
+
+  Returns the updated query. If it can't find both clauses in a single list (therefore also in the same stage), emits a
+  warning and returns the query unchanged."
+  [a-query stage-number source-clause target-clause]
+  (lib.core/swap-clauses
+   a-query stage-number
+   (lib.core/normalize (js->clj source-clause :keywordize-keys true))
+   (lib.core/normalize (js->clj target-clause :keywordize-keys true))))
+
 (defn- prep-query-for-equals [a-query field-ids]
   (-> a-query
       mbql.js/normalize-cljs
