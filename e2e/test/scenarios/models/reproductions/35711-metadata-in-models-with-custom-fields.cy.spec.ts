0780cf98b4bbd7b39df242f8a003b83588d4d80b
Kamil Mielnik <kamil@kamilmielnik.com>
2024-03-07 21:38:18 +0700

    Format code

--- e2e/test/scenarios/models/reproductions/35711-metadata-in-models-with-custom-fields.cy.spec.ts
+++ e2e/test/scenarios/models/reproductions/35711-metadata-in-models-with-custom-fields.cy.spec.ts
@@ -32,32 +32,40 @@ describe("issue 35711", () => {
       },
       { visitQuestion: true },
     );
+
     openQuestionActions();
     popover().findByText("Edit metadata").click();
-
-    cy.findAllByTestId("header-cell")
-      .contains("Total")
-      .then(totalColumn => {
-        const rect = totalColumn[0].getBoundingClientRect();
-
-        // drag & drop the Total column 100 px to the left to switch it with Tax column
-        cy.wrap(totalColumn)
-          .trigger("mousedown")
-          .trigger("mousemove", { clientX: rect.x - 100, clientY: rect.y })
-          .trigger("mouseup");
-      });
-
-    cy.button("Get Answer").should("not.exist");
-    cy.get("main")
-      .findByText("There was a problem with your question")
-      .should("not.exist");
-    cy.get(".cellData").should("contain", "37.65");
+    reorderTaxAndTotalColumns();
+    assertNoError();
 
     cy.findByTestId("editor-tabs-query-name").click();
-    cy.button("Get Answer").should("not.exist");
-    cy.get("main")
-      .findByText("There was a problem with your question")
-      .should("not.exist");
-    cy.get(".cellData").should("contain", "37.65");
+    assertNoError();
   });
 });
+
+function reorderTaxAndTotalColumns() {
+  cy.findAllByTestId("header-cell").eq(4).should("have.text", "Tax");
+  cy.findAllByTestId("header-cell").eq(5).should("have.text", "Total");
+
+  // drag & drop the Total column 100 px to the left to switch it with Tax column
+  cy.findAllByTestId("header-cell")
+    .contains("Total")
+    .then(totalColumn => {
+      const rect = totalColumn[0].getBoundingClientRect();
+      cy.wrap(totalColumn)
+        .trigger("mousedown")
+        .trigger("mousemove", { clientX: rect.x - 100, clientY: rect.y })
+        .trigger("mouseup");
+    });
+
+  cy.findAllByTestId("header-cell").eq(4).should("have.text", "Total");
+  cy.findAllByTestId("header-cell").eq(5).should("have.text", "Tax");
+}
+
+function assertNoError() {
+  cy.button("Get Answer").should("not.exist");
+  cy.get("main")
+    .findByText("There was a problem with your question")
+    .should("not.exist");
+  cy.get(".cellData").should("contain", "37.65");
+}
