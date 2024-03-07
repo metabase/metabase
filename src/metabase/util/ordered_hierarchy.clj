ef61713f574db22edb944b349f1af06be955c72b
Chris Truter <chris@metabase.com>
2024-03-07 11:19:41 -0700

    Use hiccup syntax for value-type DAG

--- src/metabase/util/ordered_hierarchy.clj
+++ src/metabase/util/ordered_hierarchy.clj
@@ -5,14 +5,39 @@
    [flatland.ordered.set :refer [ordered-set]]
    [medley.core :as m]))
 
+(declare derive)
+
+(defn- assert-no-child-repeats [visited-children [parent & children]]
+  (when (contains? @visited-children parent)
+    (assert (empty? children) (format "You must only list the children of %s at its first occurrence." parent))
+    (swap! visited-children conj parent)))
+
+(defn- derive-children
+  ([h root]
+   (derive-children (atom #{}) h root))
+  ([visited-children h [parent & children]]
+   (reduce (fn [h child]
+             (if (keyword? child)
+               (derive h child parent)
+               (do (assert-no-child-repeats visited-children child)
+                   (derive-children visited-children
+                                    (derive h (first child) parent)
+                                    child))))
+           h
+           children)))
+
 (defn make-hierarchy
   "Similar to [[clojure.core/make-hierarchy]], but the returned hierarchy will supports ordered derivations.
 
+  Can take arguments to be treated as roots, defined using hiccup syntax. NOTE: we do not check that they are roots.
+
   !! WARNING !!
   Using [[clojure.core/derive]] with this will corrupt the ordering - you must use the implementation from this ns."
-  []
-  (-> (clojure.core/make-hierarchy)
-      (with-meta {::ordered? true})))
+  ([]
+   (-> (clojure.core/make-hierarchy)
+       (with-meta {::ordered? true})))
+  ([& roots]
+   (reduce derive-children (make-hierarchy) roots)))
 
 (defn ancestors
   "Returns the immediate and indirect parents of tag, as established via derive. Earlier derivations are shown first.
