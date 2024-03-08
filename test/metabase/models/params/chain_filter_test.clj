7cfe35327ff615bb67bd41b5545f5d4697bb87fc
Alexander Solovyov <alexander@solovyov.net>
2024-03-08 12:20:17 +0200

    check that marking PK inactive will disable chained filters

--- test/metabase/models/params/chain_filter_test.clj
+++ test/metabase/models/params/chain_filter_test.clj
@@ -671,9 +671,24 @@
             (is (= true
                    (:has_more_values (chain-filter venues.latitude {venues.price 4} :limit 1))))))))))
 
+;; TODO: make this test parallel, but clj-kondo complains about t2/update! being destructive and no amount of
+;; :clj-kondo/ignore convinces it.
 (deftest chain-filter-inactive-test
   (testing "Inactive fields are not used to generate joins"
-    (mt/dataset avian-singles
+    ;; custom dataset so that destructive operations (especially marking PK inactive) won't have any effect on other
+    ;; tests
+    (mt/with-temp-test-data [["users"
+                              [{:field-name "name"
+                                :base-type :type/Text}]
+                              []]
+                             ["messages"
+                              [{:field-name "receiver_id"
+                                :base-type :type/Integer
+                                :fk :users}
+                               {:field-name "sender_id"
+                                :base-type :type/Integer
+                                :fk :users}]
+                              []]]
       (mt/$ids nil
         (mt/with-dynamic-redefs [chain-filter/database-fk-relationships @#'chain-filter/database-fk-relationships*
                                  chain-filter/find-joins                (fn
@@ -693,4 +708,9 @@
                        :rhs {:table $$users, :field %users.id}}]
                      (#'chain-filter/find-joins (mt/id) $$messages $$users))))
             (finally
-              (t2/update! :model/Field {:id %messages.receiver_id} {:active true}))))))))
+              (t2/update! :model/Field {:id %messages.receiver_id} {:active true})))
+
+          ;; mark field
+          (t2/update! :model/Field {:id %users.id} {:active false})
+          (testing "there are no connections when PK is inactive"
+            (is (nil? (#'chain-filter/find-joins (mt/id) $$messages $$users)))))))))
