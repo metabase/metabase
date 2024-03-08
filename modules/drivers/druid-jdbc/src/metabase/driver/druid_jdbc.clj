5ec44440483b8e966b3b65bfcbdf2af3f6feff08
lbrdnk <lbrdnk@users.noreply.github.com>
2024-03-08 19:00:57 +0100

    clojure.java.jdbc spec supports connection-uri

--- modules/drivers/druid-jdbc/src/metabase/driver/druid_jdbc.clj
+++ modules/drivers/druid-jdbc/src/metabase/driver/druid_jdbc.clj
@@ -10,8 +10,15 @@
 
 (defmethod sql-jdbc.conn/connection-details->spec :druid-jdbc
   [_driver _db-details]
-  {:connection (next.jdbc/get-connection "jdbc:avatica:remote:url=http://localhost:8888/druid/v2/sql/avatica/;transparent_reconnection=true")})
+  {:connection-uri "jdbc:avatica:remote:url=http://localhost:8888/druid/v2/sql/avatica/;transparent_reconnection=true"})
 
 (comment
   (driver/can-connect? :druid-jdbc nil)
+
+  (def x (next.jdbc/get-connection "jdbc:avatica:remote:url=http://localhost:8888/druid/v2/sql/avatica/;transparent_reconnection=true"))
+  (type x)
+  (instance? java.sql.Connection x)
+  (.getMetaData x)
+  (instance? java.sql.DatabaseMetaData (.getMetaData x))
+  (-> (.getMetaData x) .getURL)
   )
