a3b36c96a6e2a2323049e40d13a36d2d161a0c3d
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-03-12 19:17:06 -0400

    Reset changes to Menu

--- frontend/src/metabase/ui/components/overlays/Menu/Menu.tsx
+++ frontend/src/metabase/ui/components/overlays/Menu/Menu.tsx
@@ -1,22 +1,11 @@
 import type { MenuProps } from "@mantine/core";
 import { Menu as MantineMenu } from "@mantine/core";
 
-import { useSelector } from "metabase/lib/redux";
-import { getWritingDirection } from "metabase/selectors/app";
-
 import { MenuDropdown } from "./MenuDropdown";
 import { MenuItem } from "./MenuItem";
 
 export function Menu(props: MenuProps) {
-  let position = props.position;
-  const writingDirection = useSelector(getWritingDirection);
-  if (writingDirection === "rtl" && position === "bottom-start") {
-    position = "bottom-end";
-  }
-  if (writingDirection === "rtl" && position === "bottom-end") {
-    position = "bottom-start";
-  }
-  return <MantineMenu {...props} position={position} />;
+  return <MantineMenu {...props} />;
 }
 
 Menu.Target = MantineMenu.Target;
