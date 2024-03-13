54e4773cb29cae017c062944bcda286f03d2b2da
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2024-03-13 10:39:54 -0300

    fix stacked series order (#40073)

--- frontend/src/metabase/visualizations/echarts/cartesian/option/trend-line.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/option/trend-line.ts
@@ -182,9 +182,6 @@ export function getTrendLineOptionsAndDatasets(
   const rawDatasets = chartModel.insights.map(insight =>
     getSingleSeriesTrendDataset(insight, chartModel),
   );
-  if (settings["stackable.stack_type"] != null) {
-    rawDatasets.reverse(); // stacked series order is reversed in order to render from top to bottom
-  }
 
   const normalizedDatasets = normalizeTrendDatasets(
     rawDatasets,
