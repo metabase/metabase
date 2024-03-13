metabase/metabase

commit 180cf7870b91c722375d6842ceb19d1b223df294

Fix order of actions in question, dashboard and collection item menus (#39782)

frontend/src/metabase/components/EntityItem/EntityItem.jsx
@@ -103,72 +103,101 @@ function EntityItemMenu({
   const isXrayShown = isModel && isXrayEnabled;
   const isMetabotShown = isModel && canUseMetabot;
 
-  const actions = useMemo(
-    () =>
-      [
-        onToggleBookmark && {
-          title: isBookmarked ? t`Remove from bookmarks` : t`Bookmark`,
-          icon: "bookmark",
-          action: onToggleBookmark,
-        },
-        onPin && {
-          title: isPinned ? t`Unpin` : t`Pin this`,
-          icon: isPinned ? "unpin" : "pin",
-          action: onPin,
-        },
-        isMetabotShown && {
-          title: t`Ask Metabot`,
-          link: Urls.modelMetabot(item.id),
-          icon: "insight",
-        },
-        isXrayShown && {
-          title: t`X-ray this`,
-          link: Urls.xrayModel(item.id),
-          icon: "bolt",
-        },
-        onTogglePreview && {
-          title: isPreviewed
-            ? t`Don’t show visualization`
-            : t`Show visualization`,
-          icon: isPreviewed ? "eye_crossed_out" : "eye",
-          action: onTogglePreview,
-          tooltip: !isParameterized
-            ? t`Open this question and fill in its variables to see it.`
-            : undefined,
-          disabled: !isParameterized,
-        },
-        onMove && {
-          title: t`Move`,
-          icon: "move",
-          action: onMove,
-        },
-        onCopy && {
-          title: t`Duplicate`,
-          icon: "clone",
-          action: onCopy,
-        },
-        onArchive && {
-          title: t`Archive`,
-          icon: "archive",
-          action: onArchive,
-        },
-      ].filter(action => action),
-    [
-      item.id,
-      isPinned,
-      isXrayShown,
-      isMetabotShown,
-      isPreviewed,
-      isParameterized,
-      isBookmarked,
-      onPin,
-      onMove,
-      onCopy,
-      onArchive,
-      onTogglePreview,
-      onToggleBookmark,
-    ],
-  );
+  const actions = useMemo(() => {
+    const result = [];
+
+    const bookmarkAction = {
+      title: isBookmarked ? t`Remove from bookmarks` : t`Bookmark`,
+      icon: "bookmark",
+      action: onToggleBookmark,
+    };
+
+    if (isPinned) {
+      result.push({
+        title: t`Unpin`,
+        icon: "unpin",
+        action: onPin,
+      });
+      result.push(bookmarkAction);
+    } else {
+      result.push(bookmarkAction);
+      result.push({
+        title: t`Pin this`,
+        icon: "pin",
+        action: onPin,
+      });
+    }
+
+    if (isMetabotShown) {
+      result.push({
+        title: t`Ask Metabot`,
+        link: Urls.modelMetabot(item.id),
+        icon: "insight",
+      });
+    }
+
+    if (isXrayShown) {
+      result.push({
+        title: t`X-ray this`,
+        link: Urls.xrayModel(item.id),
+        icon: "bolt",
+      });
+    }
+
+    if (onTogglePreview) {
+      result.push({
+        title: isPreviewed
+          ? t`Don’t show visualization`
+          : t`Show visualization`,
+        icon: isPreviewed ? "eye_crossed_out" : "eye",
+        action: onTogglePreview,
+        tooltip: !isParameterized
+          ? t`Open this question and fill in its variables to see it.`
+          : undefined,
+        disabled: !isParameterized,
+      });
+    }
+
+    if (onMove) {
+      result.push({
+        title: t`Move`,
+        icon: "move",
+        action: onMove,
+      });
+    }
+
+    if (onCopy) {
+      result.push({
+        title: t`Duplicate`,
+        icon: "clone",
+        action: onCopy,
+      });
+    }
+
+    if (onArchive) {
+      result.push({
+        title: t`Archive`,
+        icon: "archive",
+        action: onArchive,
+      });
+    }
+
+    return result;
+  }, [
+    item.id,
+    isPinned,
+    isXrayShown,
+    isMetabotShown,
+    isPreviewed,
+    isParameterized,
+    isBookmarked,
+    onPin,
+    onMove,
+    onCopy,
+    onArchive,
+    onTogglePreview,
+    onToggleBookmark,
+  ]);
   if (actions.length === 0) {
     return null;
   }

frontend/src/metabase/dashboard/components/DashboardHeader/DashboardHeader.tsx
@@ -518,13 +518,6 @@ export const DashboardHeader = (props: DashboardHeaderProps) => {
       });
 
       extraButtons.push({
-        title: t`Duplicate`,
-        icon: "clone",
-        link: `${location.pathname}/copy`,
-        event: "Dashboard;Copy",
-      });
-
-      extraButtons.push({
         title:
           Array.isArray(dashboard.tabs) && dashboard.tabs.length > 1
             ? t`Export tab as PDF`
@@ -543,6 +536,17 @@ export const DashboardHeader = (props: DashboardHeaderProps) => {
           link: `${location.pathname}/move`,
           event: "Dashboard;Move",
         });
+      }
+
+      extraButtons.push({
+        title: t`Duplicate`,
+        icon: "clone",
+        link: `${location.pathname}/copy`,
+        event: "Dashboard;Copy",
+      });
+
+      if (canEdit) {
+        extraButtons.push(...PLUGIN_DASHBOARD_HEADER.extraButtons(dashboard));
 
         extraButtons.push({
           title: t`Archive`,
@@ -550,8 +554,6 @@ export const DashboardHeader = (props: DashboardHeaderProps) => {
           link: `${location.pathname}/archive`,
           event: "Dashboard;Archive",
         });
-
-        extraButtons.push(...PLUGIN_DASHBOARD_HEADER.extraButtons(dashboard));
       }
     }
 

frontend/src/metabase/query_builder/components/view/ViewHeader/components/QuestionActions/QuestionActions.tsx
@@ -189,6 +189,19 @@ export const QuestionActions = ({
       action: () => onOpenModal(MODAL_TYPES.MOVE),
       testId: MOVE_TESTID,
     });
+  }
+
+  const { isEditable } = Lib.queryDisplayInfo(question.query());
+  if (isEditable) {
+    extraButtons.push({
+      title: t`Duplicate`,
+      icon: "clone",
+      action: () => onOpenModal(MODAL_TYPES.CLONE),
+      testId: CLONE_TESTID,
+    });
+  }
+
+  if (canWrite) {
     if (isQuestion) {
       extraButtons.push({
         title: t`Turn into a model`,
@@ -206,15 +219,7 @@ export const QuestionActions = ({
     }
   }
 
-  const { isEditable } = Lib.queryDisplayInfo(question.query());
-  if (isEditable) {
-    extraButtons.push({
-      title: t`Duplicate`,
-      icon: "clone",
-      action: () => onOpenModal(MODAL_TYPES.CLONE),
-      testId: CLONE_TESTID,
-    });
-  }
+  extraButtons.push(...PLUGIN_QUERY_BUILDER_HEADER.extraButtons(question));
 
   if (canWrite) {
     extraButtons.push({
@@ -224,8 +229,6 @@ export const QuestionActions = ({
       testId: ARCHIVE_TESTID,
     });
   }
-
-  extraButtons.push(...PLUGIN_QUERY_BUILDER_HEADER.extraButtons(question));
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 

commit a16963aeab4a952e5afe50ba8c1879bb8565e845

Don't include blank actions in the entitymenu
Fixes a problem introduced in #39782

frontend/src/metabase/components/EntityItem/EntityItem.jsx
@@ -106,26 +106,36 @@ function EntityItemMenu({
   const actions = useMemo(() => {
     const result = [];
 
-    const bookmarkAction = {
-      title: isBookmarked ? t`Remove from bookmarks` : t`Bookmark`,
-      icon: "bookmark",
-      action: onToggleBookmark,
-    };
+    const bookmarkAction = onToggleBookmark
+      ? {
+          title: isBookmarked ? t`Remove from bookmarks` : t`Bookmark`,
+          icon: "bookmark",
+          action: onToggleBookmark,
+        }
+      : null;
 
     if (isPinned) {
-      result.push({
-        title: t`Unpin`,
-        icon: "unpin",
-        action: onPin,
-      });
-      result.push(bookmarkAction);
+      if (onPin) {
+        result.push({
+          title: t`Unpin`,
+          icon: "unpin",
+          action: onPin,
+        });
+      }
+      if (bookmarkAction) {
+        result.push(bookmarkAction);
+      }
     } else {
-      result.push(bookmarkAction);
-      result.push({
-        title: t`Pin this`,
-        icon: "pin",
-        action: onPin,
-      });
+      if (bookmarkAction) {
+        result.push(bookmarkAction);
+      }
+      if (onPin) {
+        result.push({
+          title: t`Pin this`,
+          icon: "pin",
+          action: onPin,
+        });
+      }
     }
 
     if (isMetabotShown) {