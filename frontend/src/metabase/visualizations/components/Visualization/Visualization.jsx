2e9e95c0852694efc5ad82688c41ade65f886e1c
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-03-13 17:19:30 +0200

    Extract action column placement (#39958)

--- frontend/src/metabase/visualizations/components/Visualization/Visualization.jsx
+++ frontend/src/metabase/visualizations/components/Visualization/Visualization.jsx
@@ -302,13 +302,23 @@ class Visualization extends PureComponent {
   };
 
   // Add the underlying card of current series to onChangeCardAndRun if available
-  handleOnChangeCardAndRun = ({ nextCard, seriesIndex, objectId }) => {
+  handleOnChangeCardAndRun = ({
+    nextCard,
+    seriesIndex,
+    objectId,
+    settingsSyncOptions,
+  }) => {
     const { series, clicked } = this.state;
 
     const index = seriesIndex || (clicked && clicked.seriesIndex) || 0;
     const previousCard = series && series[index] && series[index].card;
 
-    this.props.onChangeCardAndRun({ nextCard, previousCard, objectId });
+    this.props.onChangeCardAndRun({
+      nextCard,
+      previousCard,
+      objectId,
+      settingsSyncOptions,
+    });
   };
 
   onRender = ({ yAxisSplit, warnings = [] } = {}) => {
