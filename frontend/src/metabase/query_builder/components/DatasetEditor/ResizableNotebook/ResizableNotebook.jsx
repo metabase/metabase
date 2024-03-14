1b2be09f37749861c792714436ffe92bfb148b3d
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-03-14 15:29:53 +0100

    Use Mantine instead of styled components

--- frontend/src/metabase/query_builder/components/DatasetEditor/ResizableNotebook/ResizableNotebook.jsx
+++ frontend/src/metabase/query_builder/components/DatasetEditor/ResizableNotebook/ResizableNotebook.jsx
@@ -3,15 +3,47 @@ import PropTypes from "prop-types";
 import { ResizableBox } from "react-resizable";
 
 import CS from "metabase/css/core/index.css";
+import { color, darken } from "metabase/lib/colors";
 import Notebook from "metabase/query_builder/components/notebook/Notebook";
-
-import { NotebookContainer, Handle } from "./ResizableNotebook.styled";
+import { Flex, Box } from "metabase/ui";
 
 const propTypes = {
   isResizing: PropTypes.bool.isRequired,
   resizableBoxProps: PropTypes.object.isRequired,
   onResizeStop: PropTypes.func.isRequired,
 };
+
+/**
+ * Prevents automatic scroll effect on queries with lots of steps.
+ * When overflow is 'scroll' and the notebook is being resized,
+ * its height changes and it scrolls automatically.
+ * Setting the overflow to "hidden" while resizing fixes that behavior.
+ * @link Demo: https://github.com/metabase/metabase/pull/19103#issuecomment-981935878
+ */
+const getOverflow = isResizing => (isResizing ? "hidden" : "scroll");
+
+const Handle = () => (
+  <Flex
+    align="center"
+    justify="center"
+    w="100%"
+    h="8px"
+    pos="absolute"
+    bottom="-4px"
+    style={{
+      cursor: "row-resize",
+    }}
+  >
+    <Box
+      w="100px"
+      h="5px"
+      bg={darken(color("border"), 0.03)}
+      style={{
+        borderRadius: "4px",
+      }}
+    ></Box>
+  </Flex>
+);
 
 function ResizableNotebook({
   isResizing,
@@ -30,9 +62,9 @@ function ResizableNotebook({
         onResizeStop(...args);
       }}
     >
-      <NotebookContainer isResizing={isResizing}>
+      <Box w="100%" style={{ overflowY: getOverflow(isResizing) }}>
         <Notebook {...notebookProps} hasVisualizeButton={false} />
-      </NotebookContainer>
+      </Box>
     </ResizableBox>
   );
 }
