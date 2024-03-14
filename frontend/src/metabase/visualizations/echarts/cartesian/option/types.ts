f62de70c5b5b3c15c54e4d39920196b2f7402a66
Emmad Usmani <emmadusmani@berkeley.edu>
2024-03-14 12:00:57 -0700

    fix insights error (#40104)
    * fix insights error
    
    * Update Loki Snapshots
    
    ---------
    
    Co-authored-by: Metabase bot <metabase-bot@metabase.com>

--- frontend/src/metabase/visualizations/echarts/cartesian/option/types.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/option/types.ts
@@ -1,6 +1,15 @@
 import type { RegisteredSeriesOption } from "echarts";
+import type { RowValue } from "metabase-types/api";
+
+import type { DataKey } from "../model/types";
+import type { TREND_LINE_DATA_KEY } from "../constants/dataset";
 
 export type EChartsSeriesOption =
   | RegisteredSeriesOption["line"]
   | RegisteredSeriesOption["bar"]
   | RegisteredSeriesOption["scatter"];
+
+export type TrendDataset = {
+  [key: DataKey]: RowValue;
+  [TREND_LINE_DATA_KEY]: number;
+}[];
