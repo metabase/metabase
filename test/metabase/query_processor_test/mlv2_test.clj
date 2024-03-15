109324b25288697e0f7b57e7079aa07778f56f01
Cam Saul <github@camsaul.com>
2024-03-15 00:53:54 +0000

    Hopefully the last few test fixes :wrench:

--- test/metabase/query_processor_test/mlv2_test.clj
+++ test/metabase/query_processor_test/mlv2_test.clj
@@ -8,6 +8,8 @@
    [metabase.util :as u]
    [metabase.util.malli :as mu]))
 
+(def ^:dynamic *enable-mlv2-normalization-tests* true)
+
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (mu/defn test-mlv2-normalization :- :map
   "Preprocessing middleware that tests that [[metabase.lib.normalize]] handles everything coming back from the
@@ -15,18 +17,19 @@
   in [[metabase.query-processor.preprocess/test-mlv2-normalization]]."
   [query :- :map]
   (u/prog1 query
+    (when *enable-mlv2-normalization-tests*
     ;; we only want to run on top-level preprocessing steps, we don't want to do this on recursive preprocesses because
     ;; lots of extra keys get added and moved around that we don't need to worry about normalizing.
-    (when-not (qp.store/miscellaneous-value [::tested])
-      (qp.store/store-miscellaneous-value! [::tested] true)
-      (testing (str "\n\n"
-                    `test-mlv2-normalization
-                    "\nTest pMBQL normalization from JSON-serialized queries")
+      (when-not (qp.store/miscellaneous-value [::tested])
+        (qp.store/store-miscellaneous-value! [::tested] true)
+        (testing (str "\n\n"
+                      `test-mlv2-normalization
+                      "\nTest pMBQL normalization from JSON-serialized queries")
         ;; remove keys that aren't supposed to get serialized anyway (these are added at runtime by the QP or things
         ;; that call it)
-        (let [query        (lib.serialize/prepare-for-serialization query)
-              deserialized (json/parse-string (json/generate-string query))
-              normalized   (lib/normalize :metabase.lib.schema/query deserialized {:throw? true})]
-          (testing (str "\nquery = \n" (u/pprint-to-str query))
-            (is (= query
-                   normalized))))))))
+          (let [query        (lib.serialize/prepare-for-serialization query)
+                deserialized (json/parse-string (json/generate-string query))
+                normalized   (lib/normalize :metabase.lib.schema/query deserialized {:throw? true})]
+            (testing (str "\nquery = \n" (u/pprint-to-str query))
+              (is (= query
+                     normalized)))))))))
