33cebb7a71c7466f7d0a2976eac30ddf831ae3a5
lbrdnk <lbrdnk@users.noreply.github.com>
2024-03-15 15:42:04 +0100

    Add test extensions

--- modules/drivers/druid-jdbc/test/metabase/test/data/druid_jdbc.clj
+++ modules/drivers/druid-jdbc/test/metabase/test/data/druid_jdbc.clj
@@ -1,1 +1,29 @@
-(ns metabase.test.data.druid-jdbc)
+(ns metabase.test.data.druid-jdbc
+  (:require
+   [cheshire.core :as json]
+   [clj-http.client :as http]
+   [metabase.test.data.interface :as tx]))
+
+(tx/add-test-extensions! :druid-jdbc)
+
+(defmethod tx/dbdef->connection-details :druid-jdbc
+  [& _]
+  {:host "localhost"
+   :port "8888"})
+
+;; TODO: Use tx/dbdef->connection-details.
+(defn- already-loaded []
+  (set (json/parse-string (:body (http/get "http://localhost:8888/druid/v2/datasources")))))
+
+(defmethod tx/create-db! :druid-jdbc
+  [_ dbdef & _]
+  (let [{:keys [database-name], :as _dbdef} (tx/get-dataset-definition dbdef)]
+    (assert (= database-name "checkins")
+            "Druid tests currently only support the flattened test-data dataset.")
+    (assert (contains? (already-loaded) "checkins")
+            "Expected 'checkins' dataset to be present in Druid datasources. (This should be loaded as part of building Docker image)")
+    nil))
+
+(defmethod tx/destroy-db! :druid-jdbc
+  [& _]
+  nil)
