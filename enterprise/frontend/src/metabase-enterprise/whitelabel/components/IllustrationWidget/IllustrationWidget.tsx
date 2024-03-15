0006f6cbdbd39c9233be74732b9458cd2fbda0fc
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-03-16 00:10:00 +0700

    Add login page illustration (#39299)
    * Move Metabot setting to a new place
    
    * Add setting for login page illustration
    
    * fix UI styling
    
    * Fix not able to upload the same file twice
    
    * Update the admin > appearance > conceal metabase copy
    
    * Prepare the component for tests
    
    * Add some unit tests
    
    * Prevent settings not saved sometimes
    
    After setting the value to/from `custom` one of the setting could be
    failed to save, I suspect this happens because the race condition from
    trying to save the setting values relatively at the same time.
    
    * Make unit tests easier to read
    
    * Add all unit test test cases
    
    * Add basic E2E test
    
    * Show fallback icon for broken images
    
    * Make use of the proper component
    
    * Update the comment to affect the reality
    
    * Add an error message when file size is over 2MB
    
    * Show an error message when the file size > 2MB
    
    * Add the error message tests
    
    * Preserve the preview image aspect-ratio
    
    * Detect corrupted images
    
    * Fix failing unit tests
    
    * Address feedback: Type
    
    * Feedback: Create meaningful variables
    
    * Feedback: Make selector API more sensible
    
    * Prevent race condition
    
    Makes sure we save the settings completely before making other actions
    that also change settings otherwise, there could be race conditions.
    
    * Feedback: tighten up a type
    
    * Feedback: Fix inconsistency callback calls

---
+++ enterprise/frontend/src/metabase-enterprise/whitelabel/components/IllustrationWidget/IllustrationWidget.tsx
@@ -0,0 +1,215 @@
+import type { ChangeEvent } from "react";
+import { useMemo, useState, useRef } from "react";
+import { createPortal } from "react-dom";
+import { t } from "ttag";
+
+import { color } from "metabase/lib/colors";
+import { Button, Flex, Icon, Paper, Select, Text } from "metabase/ui";
+import type {
+  EnterpriseSettingKey,
+  EnterpriseSettings,
+  IllustrationSettingValue,
+} from "metabase-enterprise/settings/types";
+
+import { LighthouseImage, PreviewImage } from "./IllustrationWidget.styled";
+
+export interface StringSetting {
+  value: IllustrationSettingValue | null;
+  default: IllustrationSettingValue;
+}
+
+type IllustrationWidgetProps = {
+  id?: string;
+  setting: StringSetting;
+  onChange: (value: IllustrationSettingValue) => Promise<void>;
+  onChangeSetting: (key: EnterpriseSettingKey, value: unknown) => Promise<void>;
+  settingValues: Partial<EnterpriseSettings>;
+  defaultIllustrationLabel: string;
+  customIllustrationSetting:
+    | "login-page-illustration-custom"
+    | "landing-page-illustration-custom"
+    | "no-question-results-illustration-custom"
+    | "no-search-results-illustration-custom";
+  errorMessageContainerId: string;
+};
+
+const MB = 1024 * 1024;
+const IMAGE_SIZE_LIMIT = 2 * MB;
+
+export function IllustrationWidget({
+  id,
+  setting,
+  onChange,
+  onChangeSetting,
+  settingValues,
+  defaultIllustrationLabel,
+  customIllustrationSetting,
+  errorMessageContainerId,
+}: IllustrationWidgetProps) {
+  const value = setting.value ?? setting.default;
+  const [fileName, setFileName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const data = useMemo(
+    () => [
+      { label: defaultIllustrationLabel, value: "default" },
+      { label: t`No illustration`, value: "no-illustration" },
+      { label: t`Custom`, value: "custom" },
+    ],
+    [defaultIllustrationLabel],
+  );
+
+  async function handleChange(value: IllustrationSettingValue) {
+    setErrorMessage("");
+    // Avoid saving the same value
+    // When setting.value is set to the default value its value would be `null`
+    if (value === (setting.value ?? setting.default)) {
+      return;
+    }
+
+    if (value === "custom" && settingValues[customIllustrationSetting]) {
+      await onChange("custom");
+    } else if (value === "custom") {
+      fileInputRef.current?.click();
+    } else {
+      await onChange(value);
+      await onChange("default");
+    }
+  }
+
+  function handleFileUpload(fileEvent: ChangeEvent<HTMLInputElement>) {
+    if (fileEvent.target.files && fileEvent.target.files.length > 0) {
+      const file = fileEvent.target.files[0];
+      if (file.size > IMAGE_SIZE_LIMIT) {
+        setErrorMessage(
+          t`The image you chose is larger than 2MB. Please choose another one.`,
+        );
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = async readerEvent => {
+        const dataUri = readerEvent.target?.result as string;
+        if (!(await isFileIntact(dataUri))) {
+          setErrorMessage(
+            t`The image you chose is corrupted. Please choose another one.`,
+          );
+          return;
+        }
+        setFileName(file.name);
+        // Setting 2 setting values at the same time could result in one of them not being saved
+        await onChange("custom");
+        await onChangeSetting(customIllustrationSetting, dataUri);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  async function handleRemoveCustomIllustration() {
+    if (fileInputRef.current?.value) {
+      fileInputRef.current.value = "";
+    }
+    setFileName("");
+    // Setting 2 setting values at the same time could result in one of them not being saved
+    await onChange("default");
+    await onChangeSetting(customIllustrationSetting, null);
+  }
+
+  const isCustomIllustration = value === "custom";
+  const errorMessageContainer = document.getElementById(
+    errorMessageContainerId,
+  );
+
+  return (
+    <Paper withBorder shadow="none">
+      <Flex>
+        <Flex
+          align="center"
+          justify="center"
+          w="7.5rem"
+          style={{ borderRight: `1px solid ${color("border")}` }}
+        >
+          {getPreviewImage({
+            value,
+            customSource: settingValues[customIllustrationSetting] ?? undefined,
+          })}
+        </Flex>
+        <Flex p="lg" w="25rem" align="center" gap="sm">
+          <Select
+            id={id}
+            data={data}
+            value={value}
+            onChange={handleChange}
+            w={isCustomIllustration && fileName ? "8.25rem" : "100%"}
+            style={{
+              flexShrink: isCustomIllustration && fileName ? 0 : undefined,
+            }}
+            error={Boolean(errorMessage)}
+          />
+          {isCustomIllustration && (
+            <Text truncate="end" ml="auto">
+              {fileName}
+            </Text>
+          )}
+          {/**
+           * <Select /> has an annoying 0.25rem top margin which I don't have time to fix yet.
+           * This makes sure the X button is aligned with the Select component.
+           */}
+          {isCustomIllustration && (
+            <Button
+              leftIcon={<Icon name="close" />}
+              mt="0.25rem"
+              variant="subtle"
+              c="text-dark"
+              compact
+              onClick={handleRemoveCustomIllustration}
+              aria-label={t`Remove custom illustration`}
+            />
+          )}
+          <input
+            data-testid="file-input"
+            hidden
+            ref={fileInputRef}
+            onChange={handleFileUpload}
+            type="file"
+            accept="image/jpeg,image/png,image/svg+xml"
+            multiple={false}
+          />
+        </Flex>
+      </Flex>
+      {errorMessage &&
+        errorMessageContainer &&
+        createPortal(errorMessage, errorMessageContainer)}
+    </Paper>
+  );
+}
+
+async function isFileIntact(dataUri: string) {
+  return new Promise(resolve => {
+    const image = document.createElement("img");
+    image.src = dataUri;
+    image.onerror = () => resolve(false);
+    image.onload = () => resolve(true);
+  });
+}
+
+interface GetPreviewImageProps {
+  value: IllustrationSettingValue;
+  customSource: string | undefined;
+}
+
+function getPreviewImage({ value, customSource }: GetPreviewImageProps) {
+  if (value === "default") {
+    return <LighthouseImage />;
+  }
+
+  if (value === "no-illustration") {
+    return null;
+  }
+
+  if (value === "custom") {
+    return <PreviewImage src={customSource} />;
+  }
+
+  return null;
+}
