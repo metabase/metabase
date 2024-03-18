971b4ef18b539471d761fe7d5bc78f3e1164cc85
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2024-03-18 14:50:56 -0400

    replace magic string with constant

--- frontend/src/metabase/lib/formatting/link.ts
+++ frontend/src/metabase/lib/formatting/link.ts
@@ -3,6 +3,8 @@ import { isDate } from "metabase-lib/v1/types/utils/isa";
 import type { ParameterValueOrArray } from "metabase-types/api";
 import type { DatasetColumn, RowValue } from "metabase-types/api/dataset";
 
+import { NULL_DISPLAY_VALUE } from "../constants";
+
 import { formatDateTimeForParameter } from "./date";
 
 type Value = ParameterValueOrArray | RowValue | undefined;
@@ -49,7 +51,7 @@ export function renderLinkURLForClick(
     ({ value, column }: TemplateForClickFormatFunctionParamsType) => {
       const valueForLinkTemplate = formatValueForLinkTemplate(value, column);
 
-      if ([null, "(empty)"].includes(valueForLinkTemplate)) {
+      if ([null, NULL_DISPLAY_VALUE].includes(valueForLinkTemplate)) {
         return "";
       }
 
