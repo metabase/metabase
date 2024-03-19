25c4c7057276ae31b54e9ddecb584e4933c81446
Bryan Maass <bryan.maass@gmail.com>
2024-03-19 13:16:48 -0600

    move ee features into ee tests

---
+++ enterprise/backend/test/metabase_enterprise/airgap_test.clj
@@ -0,0 +1,36 @@
+(ns metabase-enterprise.airgap-test
+  (:require    [clojure.java.io :as io]
+               [clojure.string :as str]
+               [clojure.test :refer :all]
+               [metabase-enterprise.airgap :as airgap]
+               [metabase.public-settings.premium-features :as premium-features]))
+
+(defn- test-fake-features [& {:keys [token-fn pubk-fn]}]
+  (with-redefs
+    [premium-features/premium-embedding-token (constantly (-> (io/resource "fake_ag_token.txt")
+                                                              slurp
+                                                              str/trim
+                                                              ((or token-fn identity))))
+     airgap/pubkey-reader (fn [] (-> (io/reader (io/resource "fake_pubkey.pem"))
+                                     ((or pubk-fn identity))))]
+    (#'airgap/decode-token (premium-features/premium-embedding-token))))
+
+(deftest ag-token-decryption-test
+  ;; Checks for a specific feature encoded into the fake token:
+  (is (contains? (set (map hash (:features (test-fake-features)))) 2117420126)))
+
+(deftest ag-token-valid-now-test
+  (testing "Token time is valid"
+    ;; the test token is valid until 2054
+    (is (true? (#'airgap/valid-now? (test-fake-features))))))
+
+(deftest ag-invalid-token-test
+  (testing "Invalid token format"
+    (is (thrown-with-msg? Exception
+         #"Message seems corrupt or manipulated."
+         (test-fake-features :token-fn (fn [token] (str token "x"))))))
+  (testing "Invalid public key format"
+    (is (thrown-with-msg? Exception
+         #"unable to convert key pair: encoded key spec not recognized: invalid info structure in RSA public key"
+         (test-fake-features
+          :pubk-fn (fn [_] (io/reader (io/resource "broken_pubkey.pem"))))))))
--- test/metabase/api/premium_features_test.clj
+++ test/metabase/api/premium_features_test.clj
@@ -1,9 +1,6 @@
 (ns metabase.api.premium-features-test
   (:require
-   [clojure.java.io :as io]
-   [clojure.string :as str]
    [clojure.test :refer :all]
-   [metabase-enterprise.airgap :as airgap]
    [metabase.public-settings.premium-features :as premium-features]
    [metabase.public-settings.premium-features-test :as premium-features-test]
    [metabase.test :as mt]))
@@ -28,34 +25,3 @@
         (testing "requires superusers"
           (is (= "You don't have permissions to do that."
                  (mt/user-http-request :rasta :get 403 "premium-features/token/status"))))))))
-
-(defn- test-fake-features [& {:keys [token-fn pubk-fn]}]
-  (with-redefs
-    [premium-features/premium-embedding-token (constantly
-                                               (-> (str/trim (slurp (io/resource "fake_ag_token.txt")))
-                                                   ((or token-fn identity))))
-     airgap/pubkey-reader (fn [] (-> (io/reader (io/resource "fake_pubkey.pem"))
-                                     ((or pubk-fn identity))))]
-    (#'airgap/decode-token (premium-features/premium-embedding-token))))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Airgap
-
-(deftest ag-token-decryption-test
-  (is (contains? (set (map hash (:features (test-fake-features)))) 2117420126)))
-
-(deftest ag-token-valid-now-test
-  (testing "Token time is valid"
-    ;; the test token is valid until 2054
-    (is (true? (#'airgap/valid-now? (test-fake-features))))))
-
-(deftest ag-invalid-token-test
-  (testing "Invalid token format"
-    (is (thrown-with-msg? Exception
-         "Message seems corrupt or manipulated."
-         (test-fake-features :token-fn (fn [token] (str token "x"))))))
-  (testing "Invalid public key format"
-    (is (thrown-with-msg? Exception
-         #"unable to convert key pair: encoded key spec not recognized: invalid info structure in RSA public key"
-         (test-fake-features
-          :pubk-fn (fn [_] (io/reader (io/resource "broken_pubkey.pem"))))))))
