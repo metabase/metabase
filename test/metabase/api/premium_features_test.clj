7f2170f900dc9fe433c520a7c468a6dc32b6cb88
Bryan Maass <bryan.maass@gmail.com>
2024-03-19 12:28:03 -0600

    fix tests and add tests

--- test/metabase/api/premium_features_test.clj
+++ test/metabase/api/premium_features_test.clj
@@ -5,9 +5,10 @@
    [clojure.test :refer :all]
    [metabase-enterprise.airgap :as airgap]
    [metabase.public-settings.premium-features :as premium-features]
-   [metabase.public-settings.premium-features-test
-    :as premium-features-test]
+   [metabase.public-settings.premium-features-test :as premium-features-test]
    [metabase.test :as mt]))
+
+(set! *warn-on-reflection* true)
 
 (deftest get-token-status-test
   (testing "GET /api/premium-features/token/status"
@@ -28,26 +29,33 @@
           (is (= "You don't have permissions to do that."
                  (mt/user-http-request :rasta :get 403 "premium-features/token/status"))))))))
 
-(deftest ag-token-status-test
-  (testing "Invalid token format"
-    (with-redefs [premium-features/premium-embedding-token (constantly "airgap_qwerty")]
-      (testing "returns an error"
-        (is (= {:valid         false
-                :status        "invalid"
-                :error-details "Token should be 64 hexadecimal characters."}
-               (mt/user-http-request :crowberto :get 200 "premium-features/token/status"))))))  )
-
-(defn- test-fake-features []
+(defn- test-fake-features [& {:keys [token-fn pubk-fn]}]
   (with-redefs
-    [premium-features/premium-embedding-token (constantly (str/trim (slurp "test_resources/fake_ag_token.txt")))
-     airgap/pubkey-reader (fn [] (io/reader (io/resource "fake_pubkey.pem")))]
+    [premium-features/premium-embedding-token (constantly
+                                               (-> (str/trim (slurp (io/resource "fake_ag_token.txt")))
+                                                   ((or token-fn identity))))
+     airgap/pubkey-reader (fn [] (-> (io/reader (io/resource "fake_pubkey.pem"))
+                                     ((or pubk-fn identity))))]
     (#'airgap/decode-token (premium-features/premium-embedding-token))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Airgap
+
 (deftest ag-token-decryption-test
-  ;; (is (contains? (set (:features (test-fake-features))) "dan"))
   (is (contains? (set (map hash (:features (test-fake-features)))) 2117420126)))
 
 (deftest ag-token-valid-now-test
   (testing "Token time is valid"
     ;; the test token is valid until 2054
     (is (true? (#'airgap/valid-now? (test-fake-features))))))
+
+(deftest ag-invalid-token-test
+  (testing "Invalid token format"
+    (is (thrown-with-msg? Exception
+         "Message seems corrupt or manipulated."
+         (test-fake-features :token-fn (fn [token] (str token "x"))))))
+  (testing "Invalid public key format"
+    (is (thrown-with-msg? Exception
+         #"unable to convert key pair: encoded key spec not recognized: invalid info structure in RSA public key"
+         (test-fake-features
+          :pubk-fn (fn [_] (io/reader (io/resource "broken_pubkey.pem"))))))))
