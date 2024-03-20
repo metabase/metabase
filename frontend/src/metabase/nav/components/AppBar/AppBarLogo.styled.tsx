0efe263a46bded4b4d02fae5e63a95df38fbe6cc
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-03-20 12:34:22 -0400

    Impose max width on custom icon and center vertically to match hamburger menu

--- frontend/src/metabase/nav/components/AppBar/AppBarLogo.styled.tsx
+++ frontend/src/metabase/nav/components/AppBar/AppBarLogo.styled.tsx
@@ -10,6 +10,9 @@ export const LogoLink = styled(Link)<{ isSmallAppBar: boolean }>`
   justify-content: center;
   border-radius: 0.375rem;
   height: 3.25rem;
+  max-width: 12rem;
+  overflow-x: hidden;
+  line-height: 0;
   opacity: 1;
   ${props =>
     !props.isSmallAppBar &&
