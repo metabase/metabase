8e9eeac11dc393446f0d16f4be6143d210b05514
Nicolò Pretto <info@npretto.com>
2024-03-20 10:51:21 +0100

    export List from metabase/ui

---
+++ frontend/src/metabase/ui/components/typography/List/List.styled.tsx
@@ -0,0 +1,17 @@
+import type { MantineThemeOverride } from "@mantine/core";
+
+export const getListOverrides = (): MantineThemeOverride["components"] => ({
+  List: {
+    styles: () => {
+      return {
+        root: {
+          // to revert "none" from the reset
+          listStyleType: "revert",
+        },
+        item: {
+          lineHeight: "1.5",
+        },
+      };
+    },
+  },
+});
