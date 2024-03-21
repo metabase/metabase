e31d0f3236e362bd0f49c172e722b8a8028b1d6a
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-03-21 12:46:11 +0200

    Add tests

--- e2e/test/scenarios/models/reproductions/23421-visualization-settins-breaks-ui.cy.spec.js
+++ e2e/test/scenarios/models/reproductions/23421-visualization-settins-breaks-ui.cy.spec.js
@@ -1,4 +1,4 @@
-import { restore, openQuestionActions } from "e2e/support/helpers";
+import { restore, openQuestionActions, popover } from "e2e/support/helpers";
 
 const query = 'SELECT 1 AS "id", current_timestamp::timestamp AS "created_at"';
 
@@ -9,6 +9,32 @@ const emptyColumnsQuestionDetails = {
   displayIsLocked: true,
   visualization_settings: {
     "table.columns": [],
+    "table.pivot_column": "orphaned1",
+    "table.cell_column": "orphaned2",
+  },
+  type: "model",
+};
+
+const hiddenColumnsQuestionDetails = {
+  native: {
+    query,
+  },
+  displayIsLocked: true,
+  visualization_settings: {
+    "table.columns": [
+      {
+        name: "id",
+        key: '["name","id"]',
+        enabled: false,
+        fieldRef: ["field", "id", { "base-type": "type/Integer" }],
+      },
+      {
+        name: "created_at",
+        key: '["name","created_at"]',
+        enabled: false,
+        fieldRef: ["field", "created_at", { "base-type": "type/DateTime" }],
+      },
+    ],
     "table.pivot_column": "orphaned1",
     "table.cell_column": "orphaned2",
   },
@@ -26,12 +52,25 @@ describe("issue 23421", () => {
       visitQuestion: true,
     });
     openQuestionActions();
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("Edit query definition").click();
+    popover().findByText("Edit query definition").click();
 
     cy.get(".ace_content").should("contain", query);
     cy.findByRole("columnheader", { name: "id" }).should("be.visible");
     cy.findByRole("columnheader", { name: "created_at" }).should("be.visible");
+    cy.button("Save changes").should("be.disabled");
+  });
+
+  it("`visualization_settings` with hidden columns should not break UI (metabase#23421)", () => {
+    cy.createNativeQuestion(hiddenColumnsQuestionDetails, {
+      visitQuestion: true,
+    });
+    openQuestionActions();
+    popover().findByText("Edit query definition").click();
+
+    cy.get(".ace_content").should("contain", query);
+    cy.findByTestId("visualization-root")
+      .findByText("Every field is hidden right now")
+      .should("be.visible");
     cy.button("Save changes").should("be.disabled");
   });
 });
