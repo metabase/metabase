b86b72dd385b9b811bfa97ee87dfb0f2d3c60080
Ngoc Khuat <qn.khuat@gmail.com>
2024-03-21 12:10:35 +0700

    fix get items n+1

--- src/metabase/api/collection.clj
+++ src/metabase/api/collection.clj
@@ -100,7 +100,7 @@
                ;; Order NULL collection types first so that audit collections are last
                :order-by [[[[:case [:= :type nil] 0 :else 1]] :asc]
                           [:%lower.name :asc]]})
-    exclude-other-user-collections (remove-other-users-personal-subcollections api/*current-user-id*)))
+   exclude-other-user-collections (remove-other-users-personal-subcollections api/*current-user-id*)))
 
 (api/defendpoint GET "/"
   "Fetch a list of all Collections that the current user has read permissions for (`:can_write` is returned as an
@@ -549,20 +549,11 @@
               ;; when fetching root collection, we might have personal collection
               (assoc row :name (collection/user->personal-collection-name (:personal_owner_id row) :user))
               (dissoc row :personal_owner_id)))]
-      (let [visible-collection-ids (collection/permissions-set->visible-collection-ids
-                                  @api/*current-user-permissions-set*)]
-        (for [row rows]
-          ;; Go through this rigamarole instead of hydration because we
-          ;; don't get models back from ulterior over-query
-          ;; Previous examination with logging to DB says that there's no N+1 query for this.
-          ;; However, this was only tested on H2 and Postgres
-          (-> row
-              (assoc :can_write (mi/can-write? Collection (:id row)))
-              (assoc :effective_location
-                      (collection/effective-location-path (:location row) visible-collection-ids))
-              (dissoc :collection_position :display :moderated_status :icon
-                      :collection_preview :dataset_query :table_id :query_type :is_upload)
-              update-personal-collection)))))
+    (for [row rows]
+      (-> (t2/hydrate (t2/instance :model/Collection row) :can_write :effective_location)
+          (dissoc :collection_position :display :moderated_status :icon
+                  :collection_preview :dataset_query :table_id :query_type :is_upload)
+          update-personal-collection))))
 
 (mu/defn ^:private coalesce-edit-info :- last-edit/MaybeAnnotated
   "Hoist all of the last edit information into a map under the key :last-edit-info. Considers this information present
