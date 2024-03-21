36865860478b228d1216e4f67377764d5e9019a5
Kamil Mielnik <kamil@kamilmielnik.com>
2024-03-21 19:23:36 +0700

    Fix anys

--- frontend/src/metabase/common/components/CollectionPicker/components/CollectionPicker.unit.spec.tsx
+++ frontend/src/metabase/common/components/CollectionPicker/components/CollectionPicker.unit.spec.tsx
@@ -3,16 +3,18 @@ import fetchMock from "fetch-mock";
 import { setupCollectionItemsEndpoint } from "__support__/server-mocks";
 import {
   act,
-  renderWithProviders,
-  screen,
   mockGetBoundingClientRect,
   mockScrollBy,
+  renderWithProviders,
+  screen,
 } from "__support__/ui";
 import type { CollectionId } from "metabase-types/api";
 import {
   createMockCollection,
   createMockCollectionItem,
 } from "metabase-types/api/mocks";
+
+import type { CollectionPickerItem } from "../types";
 
 import { CollectionPicker } from "./CollectionPicker";
 
@@ -85,7 +87,7 @@ const flattenCollectionTree = (
   ].concat(...node.map(n => flattenCollectionTree(n.collections)));
 };
 
-const walkForCollectionItems = (node: any[]) => {
+const walkForCollectionItems = (node: MockCollection[]) => {
   node.forEach(n => {
     const collectionItems = n.collections.map((c: MockCollection) =>
       createMockCollectionItem({
@@ -108,18 +110,18 @@ const walkForCollectionItems = (node: any[]) => {
   });
 };
 
-interface setupProps {
+interface SetupOpts {
   initialValue?: {
     id: CollectionId;
     model: "collection";
   };
-  onItemSelect?: (item: any) => void;
+  onItemSelect?: (item: CollectionPickerItem) => void;
 }
 
 const setup = ({
   initialValue = { id: "root", model: "collection" },
-  onItemSelect = jest.fn(),
-}: setupProps = {}) => {
+  onItemSelect = jest.fn<void, [CollectionPickerItem]>(),
+}: SetupOpts = {}) => {
   mockGetBoundingClientRect();
   mockScrollBy();
 
