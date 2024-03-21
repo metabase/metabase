2c09721538250041aa1f5874d124cee7f942fdcd
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-03-21 14:08:23 +0700

    Feedback: Make file name consistent with the codebase

--- frontend/src/metabase/ui/components/data-display/Image/index.tsx
+++ frontend/src/metabase/ui/components/data-display/Image/index.tsx
@@ -1,16 +1,8 @@
 import type { ImageProps as MantineImageProps } from "@mantine/core";
-import { Image as MantineImage } from "@mantine/core";
 import type { CSSProperties } from "react";
 
 export interface ImageProps extends MantineImageProps {
   position: CSSProperties["position"];
 }
 
-export function Image({ position, ...mantineImageProps }: ImageProps) {
-  return (
-    <MantineImage
-      {...mantineImageProps}
-      styles={{ image: { objectPosition: position } }}
-    />
-  );
-}
+export { Image } from "./Image";
