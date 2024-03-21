8feb08ba8c8c97f6f327ec3f2d2193ea1fb59b9c
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-03-21 14:59:32 +0200

    Add metrics to DataSelector

--- frontend/src/metabase/query_builder/components/DataSelector/DataSelector.jsx
+++ frontend/src/metabase/query_builder/components/DataSelector/DataSelector.jsx
@@ -173,7 +173,8 @@ export class UnconnectedDataSelector extends Component {
     delete: PropTypes.func,
     reload: PropTypes.func,
     list: PropTypes.arrayOf(PropTypes.object),
-    search: PropTypes.arrayOf(PropTypes.object),
+    models: PropTypes.arrayOf(PropTypes.object),
+    metrics: PropTypes.arrayOf(PropTypes.object),
     allDatabases: PropTypes.arrayOf(PropTypes.object),
   };
 
@@ -417,14 +418,28 @@ export class UnconnectedDataSelector extends Component {
 
   isLoadingDatasets = () => this.props.loading;
 
-  hasDatasets = () => {
-    const { search, loaded } = this.props;
-    return loaded && search?.length > 0;
+  hasModels = () => {
+    const { models, loaded } = this.props;
+    return loaded && models && models.length > 0;
   };
 
-  hasUsableDatasets = () => {
-    // As datasets are actually saved questions, nested queries must be enabled
-    return this.hasDatasets() && this.props.hasNestedQueriesEnabled;
+  hasUsableModels = () => {
+    // As models are actually saved questions, nested queries must be enabled
+    return this.hasModels() && this.props.hasNestedQueriesEnabled;
+  };
+
+  hasMetrics = () => {
+    const { metrics, loaded } = this.props;
+    return loaded && metrics && metrics.length > 0;
+  };
+
+  hasUsableMetrics = () => {
+    // As metrics are actually saved questions, nested queries must be enabled
+    return this.hasMetrics() && this.props.hasNestedQueriesEnabled;
+  };
+
+  hasUsableModelsOrMetrics = () => {
+    return this.hasUsableModels() || this.hasUsableMetrics();
   };
 
   hasSavedQuestions = () => {
@@ -438,7 +453,7 @@ export class UnconnectedDataSelector extends Component {
     // "Saved Questions" are presented in a different picker step
     // So it should be excluded from a regular databases list
     const shouldRemoveSavedQuestionDatabaseFromList =
-      !this.props.hasNestedQueriesEnabled || this.hasDatasets();
+      !this.props.hasNestedQueriesEnabled || this.hasUsableModelsOrMetrics();
 
     return shouldRemoveSavedQuestionDatabaseFromList
       ? databases.filter(db => !db.is_saved_questions)
@@ -464,7 +479,10 @@ export class UnconnectedDataSelector extends Component {
       await this.switchToStep(TABLE_STEP);
     } else if (this.state.selectedDatabaseId && steps.includes(SCHEMA_STEP)) {
       await this.switchToStep(SCHEMA_STEP);
-    } else if (steps[0] === DATA_BUCKET_STEP && !this.hasUsableDatasets()) {
+    } else if (
+      steps[0] === DATA_BUCKET_STEP &&
+      !this.hasUsableModelsOrMetrics()
+    ) {
       await this.switchToStep(steps[1]);
     } else {
       await this.switchToStep(steps[0]);
@@ -534,8 +552,8 @@ export class UnconnectedDataSelector extends Component {
       index -= 1;
     }
 
-    // data bucket step doesn't make a lot of sense when there're no datasets
-    if (steps[index] === DATA_BUCKET_STEP && !this.hasUsableDatasets()) {
+    // data bucket step doesn't make a lot of sense when there're no models or metrics
+    if (steps[index] === DATA_BUCKET_STEP && !this.hasUsableModelsOrMetrics()) {
       return null;
     }
 
@@ -790,9 +808,12 @@ export class UnconnectedDataSelector extends Component {
 
   handleSavedEntityPickerClose = () => {
     const { selectedDataBucketId } = this.state;
+    if (selectedDataBucketId === DATA_BUCKET.MODELS || this.hasUsableModels()) {
+      this.previousStep();
+    }
     if (
-      selectedDataBucketId === DATA_BUCKET.MODELS ||
-      this.hasUsableDatasets()
+      selectedDataBucketId === DATA_BUCKET.METRICS ||
+      this.hasUsableMetrics()
     ) {
       this.previousStep();
     }
@@ -825,9 +846,10 @@ export class UnconnectedDataSelector extends Component {
         return (
           <DataBucketPicker
             dataTypes={getDataTypes({
-              hasModels: this.hasDatasets(),
+              hasModels: this.hasModels(),
               hasNestedQueriesEnabled,
               hasSavedQuestions: this.hasSavedQuestions(),
+              hasMetrics: this.hasMetrics(),
             })}
             {...props}
           />
@@ -836,7 +858,7 @@ export class UnconnectedDataSelector extends Component {
         return combineDatabaseSchemaSteps ? (
           <DatabaseSchemaPicker
             {...props}
-            hasBackButton={this.hasUsableDatasets() && props.onBack}
+            hasBackButton={this.hasUsableModelsOrMetrics() && props.onBack}
           />
         ) : (
           <DatabasePicker {...props} />
@@ -845,7 +867,7 @@ export class UnconnectedDataSelector extends Component {
         return combineDatabaseSchemaSteps ? (
           <DatabaseSchemaPicker
             {...props}
-            hasBackButton={this.hasUsableDatasets() && props.onBack}
+            hasBackButton={this.hasUsableModelsOrMetrics() && props.onBack}
           />
         ) : (
           <SchemaPicker {...props} />
@@ -927,16 +949,17 @@ export class UnconnectedDataSelector extends Component {
     if (!this.props.hasNestedQueriesEnabled) {
       return ["table"];
     }
-    if (!this.hasUsableDatasets()) {
+    if (!this.hasUsableModelsOrMetrics()) {
       return isSavedEntityPickerShown ? ["card"] : ["card", "table"];
     }
     if (!selectedDataBucketId) {
-      return ["card", "dataset", "table"];
+      return ["card", "dataset", "table", "metric"];
     }
     return {
       [DATA_BUCKET.MODELS]: ["dataset"],
       [DATA_BUCKET.RAW_DATA]: ["table"],
       [DATA_BUCKET.SAVED_QUESTIONS]: ["card"],
+      [DATA_BUCKET.METRICS]: ["metric"],
     }[selectedDataBucketId];
   };
 
@@ -1054,14 +1077,23 @@ const DataSelector = _.compose(
     loadingAndErrorWrapper: false,
     listName: "allDatabases",
   }),
+  // If there is at least one model or metric,
+  // we want to display a slightly different data picker view
+  // (see DATA_BUCKET step)
   Search.loadList({
-    // If there is at least one dataset,
-    // we want to display a slightly different data picker view
-    // (see DATA_BUCKET step)
     query: {
       models: "dataset",
       limit: 1,
     },
+    listName: "models",
+    loadingAndErrorWrapper: false,
+  }),
+  Search.loadList({
+    query: {
+      models: "metric",
+      limit: 1,
+    },
+    listName: "metrics",
     loadingAndErrorWrapper: false,
   }),
   connect(
