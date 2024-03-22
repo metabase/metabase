ed0f27cacb1f9df29c6a6dd72bfd1ece6838b2bc
Noah Moss <noahbmoss@gmail.com>
2024-03-22 10:29:47 -0400

    fix deletion of sandboxes and impersonations

--- enterprise/backend/test/metabase_enterprise/sandbox/api/permissions_test.clj
+++ enterprise/backend/test/metabase_enterprise/sandbox/api/permissions_test.clj
@@ -18,50 +18,33 @@
    [toucan2.tools.with-temp :as t2.with-temp]))
 
 (defn- db-graph-keypath [group]
-  [:groups (u/the-id group) (mt/id) :data])
+  [:groups (u/the-id group) (mt/id) :view-data])
 
 (defn- venues-perms-graph-keypath [group]
   (concat
    (db-graph-keypath group)
-   [:schemas :PUBLIC (mt/id :venues)]))
+   [:PUBLIC (mt/id :venues)]))
 
 (deftest revoke-perms-delete-gtaps-test
   (testing "PUT /api/permissions/graph"
     (testing "removing sandboxed permissions for a group should delete the associated GTAP (#16190)"
       (doseq [[message {:keys [updated-db-perms expected-perms]}]
-              {"when revoking all permissions for DB"
-               {:updated-db-perms (constantly {:native :none, :schemas :none})
-                :expected-perms   (constantly nil)}
-
-               "when revoking all permissions for schema"
-               {:updated-db-perms (constantly {:native :none, :schemas {:PUBLIC :none}})
-                :expected-perms   (constantly nil)}
-
-               "when revoking all permissions for Table"
-               {:updated-db-perms (fn []
-                                    {:native :none, :schemas {:PUBLIC {(mt/id :venues) :none
-                                                                       (mt/id :users)  :all}}})
-                :expected-perms   (fn []
-                                    {:schemas {:PUBLIC {(mt/id :users) "all"}}})}
-
-               "when changing permissions for DB to unrestricted access"
-               {:updated-db-perms (constantly {:native :none, :schemas :all})
-                :expected-perms   (constantly {:schemas "all"})}
+              {"when changing permissions for DB to unrestricted access"
+               {:updated-db-perms (constantly :unrestricted)
+                :expected-perms   (constantly "unrestricted")}
 
                "when changing permissions for schema to unrestricted access"
-               {:updated-db-perms (constantly {:native :none, :schemas {:PUBLIC :all}})
-                :expected-perms   (constantly {:schemas {:PUBLIC "all"}})}
+               {:updated-db-perms (constantly {:PUBLIC :unrestricted})
+                :expected-perms   (constantly "unrestricted")}
 
-               "when changing permissions for Table to :query [grant full query perms]"
-               {:updated-db-perms (fn []
-                                    {:native :none, :schemas {:PUBLIC {(mt/id :venues) {:query :all}}}})
-                :expected-perms   (fn []
-                                    {:schemas {:PUBLIC {(mt/id :venues) "all"}}})}}]
+               "when changing permissions for table to unrestricted access"
+               {:updated-db-perms (fn [] {:PUBLIC {(mt/id :venues) :unrestricted}})
+                :expected-perms   (fn [] "unrestricted")}}]
         (met/with-gtaps! {:gtaps {:venues {}}}
           (testing message
             (testing "sanity check"
               (testing "perms graph endpoint should return segmented perms for Venues table"
-                (is (= {:read "all" :query "segmented"}
+                (is (= "sandboxed"
                        (get-in (mt/user-http-request :crowberto :get 200 "permissions/graph")
                                (venues-perms-graph-keypath &group)))))
               (testing "GTAP should exist in application DB"
@@ -127,10 +110,7 @@
                                                            :table_id (mt/id :venues)}]
           (let [graph  (mt/user-http-request :crowberto :get 200 "permissions/graph")
                 graph' (assoc-in graph (db-graph-keypath (perms-group/all-users))
-                                 {:schemas
-                                  {"PUBLIC"
-                                   {(mt/id :venues)
-                                    {:read :all, :query :segmented}}}})]
+                                 {"PUBLIC" {(mt/id :venues) "sandboxed"}})]
             (mt/user-http-request :crowberto :put 200 "permissions/graph" graph')
             (testing "GTAP should not have been deleted"
               (is (t2/exists? GroupTableAccessPolicy :group_id (u/the-id (perms-group/all-users)), :table_id (mt/id :venues))))))))))
