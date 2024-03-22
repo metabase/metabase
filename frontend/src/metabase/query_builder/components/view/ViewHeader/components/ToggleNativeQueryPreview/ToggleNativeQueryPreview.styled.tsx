ca03549b11d1cfde641cc841b4f5af578d75c900
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-03-22 16:00:02 +0100

    Style the toggle icon

--- frontend/src/metabase/query_builder/components/view/ViewHeader/components/ToggleNativeQueryPreview/ToggleNativeQueryPreview.styled.tsx
+++ frontend/src/metabase/query_builder/components/view/ViewHeader/components/ToggleNativeQueryPreview/ToggleNativeQueryPreview.styled.tsx
@@ -3,12 +3,21 @@ import styled from "@emotion/styled";
 import IconButtonWrapper from "metabase/components/IconButtonWrapper";
 import { color } from "metabase/lib/colors";
 
-export const SqlButton = styled(IconButtonWrapper)`
-  color: ${color("text-dark")};
-  padding: 0.5rem;
+interface SqlButtonProps {
+  isSelected?: boolean;
+}
+
+export const SqlButton = styled(IconButtonWrapper)<SqlButtonProps>`
+  color: ${({ isSelected }) =>
+    isSelected ? color("white") : color("text-dark")};
+  background-color: ${({ isSelected }) => isSelected && color("brand")};
+  height: 2rem;
+  width: 2rem;
 
   &:hover {
     color: ${color("brand")};
     background-color: ${color("bg-medium")};
+    border: 1px solid ${color("brand")};
+    transition: all 200ms linear;
   }
 `;
