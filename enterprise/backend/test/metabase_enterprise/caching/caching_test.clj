815b2a7308f4d536bbaaf46e3c938045ab017ee4
Alexander Solovyov <alexander@solovyov.net>
2024-03-25 17:57:33 +0300

    make cache config available for OSS version (#40512)

--- enterprise/backend/test/metabase_enterprise/caching/caching_test.clj
+++ enterprise/backend/test/metabase_enterprise/caching/caching_test.clj
@@ -1,15 +1,11 @@
-(ns metabase-enterprise.caching.api-test
+(ns metabase-enterprise.caching.caching-test
   (:require
    [clojure.test :refer :all]
-   [metabase-enterprise.caching.api :as caching.api]
    [metabase.public-settings :as public-settings]
    [metabase.query-processor.card :as qp.card]
    [metabase.test :as mt]
    [metabase.util :as u]
    [toucan2.core :as t2]))
-
-(comment
-  caching.api/keep-me)
 
 (defn last-audit-event []
   (t2/select-one [:model/AuditLog :topic :user_id :model :model_id :details]
@@ -20,10 +16,6 @@
   (mt/discard-setting-changes [enable-query-caching]
     (public-settings/enable-query-caching! true)
     (mt/with-model-cleanup [:model/CacheConfig]
-      (testing "Caching requires premium token with `:caching`"
-        (mt/with-premium-features #{}
-          (is (= "Caching is a paid feature not currently available to your instance. Please upgrade to use it. Learn more at metabase.com/upgrade/"
-                 (mt/user-http-request :crowberto :get 402 "ee/caching/")))))
       (testing "Caching API"
         (mt/with-premium-features #{:cache-granular-controls :audit-app}
           (mt/with-temp [:model/Database      db     {}
@@ -40,15 +32,15 @@
 
             (testing "No access from regular users"
               (is (= "You don't have permissions to do that."
-                     (mt/user-http-request :rasta :get 403 "ee/caching/"))))
+                     (mt/user-http-request :rasta :get 403 "caching/"))))
 
             (testing "Can configure root"
-              (is (mt/user-http-request :crowberto :put 200 "ee/caching/"
+              (is (mt/user-http-request :crowberto :put 200 "caching/"
                                         {:model    "root"
                                          :model_id 0
                                          :strategy {:type "nocache" :name "root"}}))
               (is (=? {:data [{:model "root" :model_id 0}]}
-                      (mt/user-http-request :crowberto :get 200 "ee/caching/"
+                      (mt/user-http-request :crowberto :get 200 "caching/"
                                             :model "root")))
               (testing "Is audited"
                 (is (=? {:topic    :caching-update
@@ -62,28 +54,28 @@
                         (last-audit-event)))))
 
             (testing "Can configure others"
-              (is (mt/user-http-request :crowberto :put 200 "ee/caching/"
+              (is (mt/user-http-request :crowberto :put 200 "caching/"
                                         {:model    "database"
                                          :model_id (:id db)
                                          :strategy {:type "nocache" :name "db"}}))
-              (is (mt/user-http-request :crowberto :put 200 "ee/caching/"
+              (is (mt/user-http-request :crowberto :put 200 "caching/"
                                         {:model    "dashboard"
                                          :model_id (:id dash1)
                                          :strategy {:type "nocache" :name "dash"}}))
-              (is (mt/user-http-request :crowberto :put 200 "ee/caching/"
+              (is (mt/user-http-request :crowberto :put 200 "caching/"
                                         {:model    "question"
                                          :model_id (:id card1)
                                          :strategy {:type "nocache" :name "card1"}})))
 
             (testing "HTTP responds with correct listings"
               (is (=? {:data [{:model "root" :model_id 0}]}
-                      (mt/user-http-request :crowberto :get 200 "ee/caching/")))
+                      (mt/user-http-request :crowberto :get 200 "caching/")))
               (is (=? {:data [{:model "database" :model_id (:id db)}]}
-                      (mt/user-http-request :crowberto :get 200 "ee/caching/" {}
+                      (mt/user-http-request :crowberto :get 200 "caching/" {}
                                             :model :database)))
               (is (=? {:data [{:model "dashboard" :model_id (:id dash1)}
-                               {:model "question" :model_id (:id card1)}]}
-                      (mt/user-http-request :crowberto :get 200 "ee/caching/" {}
+                              {:model "question" :model_id (:id card1)}]}
+                      (mt/user-http-request :crowberto :get 200 "caching/" {}
                                             :collection (:id col1) :model :dashboard :model :question))))
 
             (testing "We select correct config for something from a db"
@@ -102,13 +94,20 @@
                         (:cache-strategy (#'qp.card/query-for-card card3 {} {} {} {}))))))
 
             (testing "It's possible to delete a configuration"
-              (is (nil? (mt/user-http-request :crowberto :delete 204 "ee/caching"
+              (is (nil? (mt/user-http-request :crowberto :delete 204 "caching/"
                                               {:model    "database"
                                                :model_id (:id db)})))
               (testing "Listing for databases becomes empty"
                 (is (=? {:data []}
-                        (mt/user-http-request :crowberto :get 200 "ee/caching/" {}
+                        (mt/user-http-request :crowberto :get 200 "caching/" {}
                                               :model :database))))
               (testing "And card2 gets root config"
                 (is (=? {:type :nocache :name "root"}
-                        (:cache-strategy (#'qp.card/query-for-card card2 {} {} {} {}))))))))))))
+                        (:cache-strategy (#'qp.card/query-for-card card2 {} {} {} {}))))))
+
+            (testing "It's possible to use advanced caching strategies"
+              (is (mt/user-http-request :crowberto :put 200 "caching/"
+                                        {:model    "root"
+                                         :model_id 0
+                                         :strategy {:type     "schedule"
+                                                    :schedule "0/2 * * * * ?"}})))))))))
