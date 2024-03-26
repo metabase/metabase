98f6cd824e162b03cf335903e91b338b601940a6
Denis Berezin <denis.berezin@metabase.com>
2024-03-27 02:10:40 +0300

    Embedding SDK - integration commit (#40198)
    * Minimal SDK code
    
    * Fix viz, styles
    
    * Move SDK code to enterprise folder
    
    * Fix files structure
    
    * Clean-up
    
    * Review fixes
    
    * Review fixes
    
    * Review fixes
    
    * Remove elementid
    
    * Reuse some options from main webpack config
    
    * Actualize package.json
    
    * Actualize package.json

---
+++ enterprise/frontend/src/embedding-sdk/components/private/SdkContentWrapper.tsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+import { alpha, color } from "metabase/lib/colors";
+import { aceEditorStyles } from "metabase/query_builder/components/NativeQueryEditor/NativeQueryEditor.styled";
+import { saveDomImageStyles } from "metabase/visualizations/lib/save-chart-image";
+
+export const SdkContentWrapper = styled.div<{ font: string }>`
+  --default-font-family: "${({ font }) => font}";
+  --color-brand: ${color("brand")};
+  --color-brand-alpha-04: ${alpha("brand", 0.04)};
+  --color-brand-alpha-88: ${alpha("brand", 0.88)};
+  --color-focus: ${color("focus")};
+
+  ${aceEditorStyles}
+  ${saveDomImageStyles}
+
+  --default-font-size: 0.875em;
+  --default-font-color: var(--color-text-dark);
+  --default-bg-color: var(--color-bg-light);
+
+  font-family: var(--default-font-family), sans-serif;
+  font-size: var(--default-font-size);
+  font-weight: 400;
+  font-style: normal;
+  color: var(--color-text-dark);
+  margin: 0;
+  height: 100%; /* ensure the entire page will fill the window */
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-bg-light);
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
