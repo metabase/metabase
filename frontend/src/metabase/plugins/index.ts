114d47bb01d488f32434a19ad56be2503394473d
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-03-26 22:50:46 +0700

    Fix not able to build static viz

--- frontend/src/metabase/plugins/index.ts
+++ frontend/src/metabase/plugins/index.ts
@@ -1,7 +1,6 @@
 import type { ComponentType, HTMLAttributes, ReactNode } from "react";
 import { t } from "ttag";
 
-import noResultsSource from "assets/img/no_results.svg";
 import { UNABLE_TO_CHANGE_ADMIN_PERMISSIONS } from "metabase/admin/permissions/constants/messages";
 import type {
   DataPermission,
@@ -147,10 +146,12 @@ export const PLUGIN_SELECTORS = {
     };
   },
   getNoDataIllustration: (_state: State): string => {
-    return noResultsSource;
+    // Can't import any SVG file because it would fail to build static viz
+    return "app/img/no_results.svg";
   },
   getNoObjectIllustration: (_state: State): string => {
-    return noResultsSource;
+    // Can't import any SVG file because it would fail to build static viz
+    return "app/img/no_results.svg";
   },
 };
 
