622c5e453662cb8abee49f3934075b9f4d082306
Ryan Laurie <iethree@gmail.com>
2024-03-26 15:21:44 -0600

    keep old question picker in add-card dashboard sidebar

--- frontend/src/metabase/dashboard/components/AddCardSidebar/AddCardSidebar.jsx
+++ frontend/src/metabase/dashboard/components/AddCardSidebar/AddCardSidebar.jsx
@@ -1,30 +1,18 @@
 import PropTypes from "prop-types";
-import { t } from "ttag";
 
-import { QuestionPickerModal } from "metabase/common/components/EntityPicker";
 import { Sidebar } from "metabase/dashboard/components/Sidebar";
 
+import { QuestionPicker } from "../QuestionPicker";
 
 AddCardSidebar.propTypes = {
   onSelect: PropTypes.func.isRequired,
   initialCollection: PropTypes.number,
 };
 
-export function AddCardSidebar({
-  onClose,
-  onSelect,
-}) {
+export function AddCardSidebar(props) {
   return (
     <Sidebar data-testid="add-card-sidebar">
-      <QuestionPickerModal
-        title={t`Add a question to this dashboard`}
-        value={{ model: "collection", id: 'root' }}
-        onChange={question => {
-          onSelect(question.id);
-          onClose();
-        }}
-        onClose={onClose}
-      />
+      <QuestionPicker {...props} />
     </Sidebar>
   );
 }
