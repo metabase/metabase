cbd652882a9e87d436a5e835bc57c112b9d42ca8
Tim Macdonald <tim@tsmacdonald.com>
2024-03-26 10:59:46 +0000

    Parse wildcards in native queries
    Add query_field.direct_reference

--- src/metabase/native_query_analyzer.clj
+++ src/metabase/native_query_analyzer.clj
@@ -5,6 +5,7 @@
   1. Translate Metabase-isms into generic SQL that Macaw can understand.
   2. Contain Metabase-specific business logic."
   (:require
+   [clojure.set :as set]
    [clojure.string :as str]
    [macaw.core :as mac]
    [metabase.config :as config]
@@ -37,35 +38,70 @@
       (str/replace "\"" "")
       u/lower-case-en))
 
-(defn- field-ids-for-query
+(def ^:private field-and-table-fragment
+  "HoneySQL fragment to get the Field and Table"
+  {:from [[(t2/table-name :model/Field) :f]]
+   :join [[(t2/table-name :model/Table) :t] [:= :table_id :t.id]]})
+
+(defn- direct-field-ids-for-query
   "Very naively selects the IDs of Fields that could be used in the query. Improvements to this are planned for Q2 2024,
   c.f. Milestone 3 of https://github.com/metabase/metabase/issues/36911"
-  [q db-id]
-  (try
-    (let [{:keys [columns tables]} (mac/query->components (mac/parsed-query q))]
-      (t2/select-pks-set :model/Field {:from [[(t2/table-name :model/Field) :f]]
-                                       :join [[(t2/table-name :model/Table) :t] [:= :table_id :t.id]]
-                                       :where [:and
-                                               [:= :t.db_id db-id]
-                                               (if (seq tables)
-                                                 [:in :%lower.t/name (map normalize-name tables)]
-                                                 true)
-                                               (if (seq columns)
-                                                 [:in :%lower.f/name (map normalize-name columns)]
-                                                 true)]}))
-    (catch JSQLParserException e
-      (log/error e "Error parsing native query"))))
+  [{:keys [columns tables] :as _parsed-query} db-id]
+  (t2/select-pks-set :model/Field (merge field-and-table-fragment
+                                         {:where [:and
+                                                  [:= :t.db_id db-id]
+                                                  (if (seq tables)
+                                                    [:in :%lower.t/name (map normalize-name tables)]
+                                                    ;; if we don't know what tables it's from, look everywhere
+                                                    true)
+                                                  (if (seq columns)
+                                                    [:in :%lower.f/name (map normalize-name columns)]
+                                                    ;; if there are no columns, it must be a select * or similar
+                                                    false)]})))
+
+(defn- indirect-field-ids-for-query
+  "Similar to direct-field-ids-for-query, but for wildcard selects"
+  [{:keys [table-wildcards has-wildcard? tables] :as _parsed-query} db-id]
+  (let [active-fields-from-tables
+        (fn [table-names]
+          (t2/select-pks-set :model/Field (merge field-and-table-fragment
+                                                 {:where [:and
+                                                          [:= :t.db_id db-id]
+                                                          [:= true :f.active]
+                                                          (if (seq table-names)
+                                                            [:in :%lower.t/name (map normalize-name table-names)]
+                                                            ;; if no tables, nothing is selectable
+                                                            false)]})))]
+
+    (if has-wildcard?
+      ;; select * from ...
+      ;; so, get everything in all the tables
+      (active-fields-from-tables tables)
+      ;; select foo.* from ..
+      ;; limit to the named tables
+      (active-fields-from-tables table-wildcards))))
 
 (defn- field-ids-for-card
-  "Returns a list of field IDs that (may) be referenced in the given cards's query. Errs on the side of optimism:
+  "Returns a `{:direct #{...} :indirect #{...}}` map with field IDs that (may) be referenced in the given cards's query. Errs on the side of optimism:
   i.e., it may return fields that are *not* in the query, and is unlikely to fail to return fields that are in the
   query.
 
+  Direct references are columns that are named in the query; indirect ones are from wildcards. If a field could be both direct and indirect, it will *only* show up in the `:direct` set.
+
   Returns `nil` (and logs the error) if there was a parse error."
   [card]
-  (let [{native-query :native
-         db-id        :database} (:dataset_query card)]
-    (field-ids-for-query (:query native-query) db-id)))
+  (try
+    (let [{native-query :native
+           db-id        :database} (:dataset_query card)
+          parsed-query             (mac/query->components (mac/parsed-query (:query native-query)))
+          direct-ids               (direct-field-ids-for-query parsed-query db-id)
+          indirect-ids             (set/difference
+                                    (indirect-field-ids-for-query parsed-query db-id)
+                                    direct-ids)]
+      {:direct   direct-ids
+       :indirect indirect-ids})
+    (catch JSQLParserException e
+      (log/error e "Error parsing native query"))))
 
 (defn update-query-fields-for-card!
   "Clears QueryFields associated with this card and creates fresh, up-to-date-ones.
@@ -76,8 +112,13 @@
   [{card-id :id, query :dataset_query :as card}]
   (when (and (active?)
              (= :native (:type query)))
-    (let [query-field-records (map (fn [field-id] {:card_id  card-id :field_id field-id})
-                                   (field-ids-for-card card))]
+    (let [{:keys [direct indirect]} (field-ids-for-card card)
+          id->record (fn [direct? field-id] {:card_id          card-id
+                                             :field_id         field-id
+                                             :direct_reference direct?})
+          query-field-records (concat
+                               (map (partial id->record true) direct)
+                               (map (partial id->record false) indirect))]
       ;; This feels inefficient at first glance, but the number of records should be quite small and doing some sort
       ;; of upsert-or-delete would involve comparisons in Clojure-land that are more expensive than just "killing and
       ;; filling" the records.
