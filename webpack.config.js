a2b12666479ad9e290ee4e03f3ef8383a914074e
Oisin Coveney <oisin@metabase.com>
2024-03-28 13:53:49 +0200

    Attempt another plugin

--- webpack.config.js
+++ webpack.config.js
@@ -117,7 +117,7 @@ const config = (module.exports = {
               publicPath: "./",
             },
           },
-          'css-modules-typescript-loader',
+          // 'css-modules-typescript-loader',
           { loader: "css-loader", options: CSS_CONFIG },
           { loader: "postcss-loader" },
         ],
