5f5a05c61ee2ed626ca7905c02613174d76bfe33
Chris Truter <chris@metabase.com>
2024-03-28 14:36:46 +0200

    Separate type considerations

--- test/metabase/upload_test.clj
+++ test/metabase/upload_test.clj
@@ -7,7 +7,6 @@
    [clojure.string :as str]
    [clojure.test :refer :all]
    [flatland.ordered.map :as ordered-map]
-   [java-time.api :as t]
    [metabase.analytics.snowplow-test :as snowplow-test]
    [metabase.driver :as driver]
    [metabase.driver.ddl.interface :as ddl.i]
@@ -23,25 +22,23 @@
    [metabase.test.data.sql :as sql.tx]
    [metabase.upload :as upload]
    [metabase.upload.parsing :as upload-parsing]
+   [metabase.upload.types :as upload-types]
    [metabase.util :as u]
-   [metabase.util.ordered-hierarchy :as ordered-hierarchy]
    [toucan2.core :as t2])
   (:import
    (java.io File)))
 
 (set! *warn-on-reflection* true)
 
-(def ^:private bool-type         ::upload/boolean)
-(def ^:private int-type          ::upload/int)
-(def ^:private bool-int-type     ::upload/*boolean-int*)
-(def ^:private float-type        ::upload/float)
-(def ^:private float-or-int-type ::upload/*float-or-int*)
-(def ^:private vchar-type        ::upload/varchar-255)
-(def ^:private date-type         ::upload/date)
-(def ^:private datetime-type     ::upload/datetime)
-(def ^:private offset-dt-type    ::upload/offset-datetime)
-(def ^:private text-type         ::upload/text)
-(def ^:private auto-pk-type      ::upload/auto-incrementing-int-pk)
+(def ^:private bool-type         ::upload-types/boolean)
+(def ^:private int-type          ::upload-types/int)
+(def ^:private float-type        ::upload-types/float)
+(def ^:private vchar-type        ::upload-types/varchar-255)
+(def ^:private date-type         ::upload-types/date)
+(def ^:private datetime-type     ::upload-types/datetime)
+(def ^:private offset-dt-type    ::upload-types/offset-datetime)
+(def ^:private text-type         ::upload-types/text)
+(def ^:private auto-pk-type      ::upload-types/auto-incrementing-int-pk)
 
 (defn- local-infile-on? []
   (= "ON" (-> (sql-jdbc.conn/db->pooled-connection-spec (mt/db))
@@ -114,189 +111,6 @@
     (binding [upload/*sync-synchronously?* true]
       (#'upload/scan-and-sync-table! database table))
     (t2/select-one :model/Table (:id table))))
-
-(deftest type-detection-and-parse-test
-  (doseq [[string-value expected-value expected-type seps]
-          ;; Number-related
-          [["0.0"        0              float-or-int-type "."]
-           ["0.0"        0              float-or-int-type ".,"]
-           ["0,0"        0              float-or-int-type ",."]
-           ["0,0"        0              float-or-int-type ", "]
-           ["0.0"        0              float-or-int-type ".’"]
-           ["-0.0"       -0.0           float-or-int-type "."]
-           ["-0.0"       -0.0           float-or-int-type ".,"]
-           ["-0,0"       -0.0           float-or-int-type ",."]
-           ["-0,0"       -0.0           float-or-int-type ", "]
-           ["-0.0"       -0.0           float-or-int-type ".’"]
-           ["(0.0)"      -0.0           float-or-int-type "."]
-           ["(0.0)"      -0.0           float-or-int-type ".,"]
-           ["(0,0)"      -0.0           float-or-int-type ",."]
-           ["(0,0)"      -0.0           float-or-int-type ", "]
-           ["(0.0)"      -0.0           float-or-int-type ".’"]
-           ["-4300.00€"  -4300          float-or-int-type ".,"]
-           ["£1,000.00"  1000           float-or-int-type]
-           ["£1,000.00"  1000           float-or-int-type "."]
-           ["£1,000.00"  1000           float-or-int-type ".,"]
-           ["£1.000,00"  1000           float-or-int-type ",."]
-           ["£1 000,00"  1000           float-or-int-type ", "]
-           ["£1’000.00"  1000           float-or-int-type ".’"]
-           ["$2"         2              int-type]
-           ["$ 3"        3              int-type]
-           ["-43€"       -43            int-type]
-           ["(86)"       -86            int-type]
-           ["($86)"      -86            int-type]
-           ["£1000"      1000           int-type]
-           ["£1000"      1000           int-type "."]
-           ["£1000"      1000           int-type ".,"]
-           ["£1000"      1000           int-type ",."]
-           ["£1000"      1000           int-type ", "]
-           ["£1000"      1000           int-type ".’"]
-           ["-¥9"        -9             int-type]
-           ["₹ -13"      -13            int-type]
-           ["₪13"        13             int-type]
-           ["₩-13"       -13            int-type]
-           ["₿42"        42             int-type]
-           ["-99¢"       -99            int-type]
-           ["2"          2              int-type]
-           ["-86"        -86            int-type]
-           ["9,986,000"  9986000        int-type]
-           ["9,986,000"  9986000        int-type "."]
-           ["9,986,000"  9986000        int-type ".,"]
-           ["9.986.000"  9986000        int-type ",."]
-           ["9’986’000"  9986000        int-type ".’"]
-           ["$0"         0              int-type]
-           ["-1"         -1             int-type]
-           ["0"          false          bool-int-type]
-           ["1"          true           bool-int-type]
-           ["9.986.000"  "9.986.000"    vchar-type ".,"]
-           ["3.14"       3.14           float-type]
-           ["3.14"       3.14           float-type "."]
-           ["3.14"       3.14           float-type ".,"]
-           ["3,14"       3.14           float-type ",."]
-           ["3,14"       3.14           float-type ", "]
-           ["(3.14)"     -3.14          float-type]
-           ["3.14"       3.14           float-type ".’"]
-           [".14"        ".14"          vchar-type ".,"] ;; TODO: this should be a float type
-           ["0.14"       0.14           float-type ".,"]
-           ["-9986.567"  -9986.567      float-type ".,"]
-           ["$2.0"       2              float-or-int-type ".,"]
-           ["$ 3.50"     3.50           float-type ".,"]
-           ["-4300.23€"  -4300.23       float-type ".,"]
-           ["£1,000.23"  1000.23        float-type]
-           ["£1,000.23"  1000.23        float-type "."]
-           ["£1,000.23"  1000.23        float-type ".,"]
-           ["£1.000,23"  1000.23        float-type ",."]
-           ["£1 000,23"  1000.23        float-type ", "]
-           ["£1’000.23"  1000.23        float-type ".’"]
-           ["-¥9.99"     -9.99          float-type ".,"]
-           ["₹ -13.23"   -13.23         float-type ".,"]
-           ["₪13.01"     13.01          float-type ".,"]
-           ["₩13.33"     13.33          float-type ".,"]
-           ["₿42.243646" 42.243646      float-type ".,"]
-           ["-99.99¢"    -99.99         float-type ".,"]
-           ["."          "."            vchar-type]
-           ;; String-related
-           [(apply str (repeat 255 "x")) (apply str (repeat 255 "x")) vchar-type]
-           [(apply str (repeat 256 "x")) (apply str (repeat 256 "x")) text-type]
-           ["86 is my favorite number"   "86 is my favorite number"   vchar-type]
-           ["My favorite number is 86"   "My favorite number is 86"   vchar-type]
-           ;; Date-related
-           [" 2022-01-01 "                    #t "2022-01-01"             date-type]
-           [" 2022-02-30 "                    " 2022-02-30 "              vchar-type]
-           [" -2022-01-01 "                   #t "-2022-01-01"            date-type]
-           [" Jan 1 2018"                     #t "2018-01-01"             date-type]
-           [" Jan 02 2018"                    #t "2018-01-02"             date-type]
-           [" Jan 30 -2018"                   #t "-2018-01-30"            date-type]
-           [" Jan 1, 2018"                    #t "2018-01-01"             date-type]
-           [" Jan 02, 2018"                   #t "2018-01-02"             date-type]
-           [" Feb 30, 2018"                   " Feb 30, 2018"             vchar-type]
-           [" 1 Jan 2018"                     #t "2018-01-01"             date-type]
-           [" 02 Jan 2018"                    #t "2018-01-02"             date-type]
-           [" 1 Jan, 2018"                    #t "2018-01-01"             date-type]
-           [" 02 Jan, 2018"                   #t "2018-01-02"             date-type]
-           [" January 1 2018"                 #t "2018-01-01"             date-type]
-           [" January 02 2018"                #t "2018-01-02"             date-type]
-           [" January 1, 2018"                #t "2018-01-01"             date-type]
-           [" January 02, 2018"               #t "2018-01-02"             date-type]
-           [" 1 January 2018"                 #t "2018-01-01"             date-type]
-           [" 02 January 2018"                #t "2018-01-02"             date-type]
-           [" 1 January, 2018"                #t "2018-01-01"             date-type]
-           [" 02 January, 2018"               #t "2018-01-02"             date-type]
-           [" Saturday, January 1 2000"       #t "2000-01-01"             date-type]
-           [" Sunday, January 02 2000"        #t "2000-01-02"             date-type]
-           [" Saturday, January 1, 2000"      #t "2000-01-01"             date-type]
-           [" Sunday, January 02, 2000"       #t "2000-01-02"             date-type]
-           [" 2022-01-01T01:00 "              #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01t01:00 "              #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01 01:00 "              #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01T01:00:00 "           #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01t01:00:00 "           #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01 01:00:00 "           #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01T01:00:00.00 "        #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01t01:00:00.00 "        #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01 01:00:00.00 "        #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01T01:00:00.000000000 " #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01t01:00:00.000000000 " #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01 01:00:00.000000000 " #t "2022-01-01T01:00"       datetime-type]
-           [" 2022-01-01T01:00:00.00-07 "     #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01t01:00:00.00-07 "     #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01 01:00:00.00-07 "     #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01T01:00:00.00-07:00 "  #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01t01:00:00.00-07:00 "  #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01 01:00:00.00-07:00 "  #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01T01:00:00.00-07:00 "  #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01t01:00:00.00-07:00 "  #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01 01:00:00.00-07:00 "  #t "2022-01-01T01:00-07:00" offset-dt-type]
-           [" 2022-01-01T01:00:00.00Z "       (t/offset-date-time "2022-01-01T01:00+00:00") offset-dt-type]
-           [" 2022-01-01t01:00:00.00Z "       (t/offset-date-time "2022-01-01T01:00+00:00") offset-dt-type]
-           [" 2022-01-01 01:00:00.00Z "       (t/offset-date-time "2022-01-01T01:00+00:00") offset-dt-type]]]
-    (let [settings    {:number-separators (or seps ".,")}
-          type->check (#'upload/settings->type->check settings)
-          value-type  (#'upload/value->type type->check string-value)
-          ;; get the type of the column, if we created it based on only that value
-          col-type    (first (upload/column-types-from-rows settings nil [[string-value]]))
-          parser      (upload-parsing/upload-type->parser col-type settings)]
-      (testing (format "\"%s\" is a %s" string-value value-type)
-        (is (= expected-type
-               value-type)))
-      (testing (format "\"%s\" is parsed into %s" string-value expected-value)
-        (is (= expected-value
-               (parser string-value)))))))
-
-(deftest ^:parallel type-coalescing-test
-  (doseq [[type-a            type-b           expected]
-          [[bool-type        bool-type        bool-type]
-           [bool-type        int-type         vchar-type]
-           [bool-type        bool-int-type    bool-type]
-           [bool-type        date-type        vchar-type]
-           [bool-type        datetime-type    vchar-type]
-           [bool-type        vchar-type       vchar-type]
-           [bool-type        text-type        text-type]
-           [int-type         bool-type        vchar-type]
-           [int-type         float-type       float-type]
-           [int-type         date-type        vchar-type]
-           [int-type         datetime-type    vchar-type]
-           [int-type         vchar-type       vchar-type]
-           [int-type         text-type        text-type]
-           [int-type         bool-int-type    int-type]
-           [bool-int-type    bool-int-type    bool-int-type]
-           [float-type       vchar-type       vchar-type]
-           [float-type       text-type        text-type]
-           [float-type       date-type        vchar-type]
-           [float-type       datetime-type    vchar-type]
-           [float-type       text-type        text-type]
-           [float-type       date-type        vchar-type]
-           [float-type       datetime-type    vchar-type]
-           [date-type        datetime-type    datetime-type]
-           [date-type        vchar-type       vchar-type]
-           [date-type        text-type        text-type]
-           [datetime-type    vchar-type       vchar-type]
-           [offset-dt-type   vchar-type       vchar-type]
-           [datetime-type    text-type        text-type]
-           [offset-dt-type   text-type        text-type]
-           [vchar-type       text-type        text-type]]]
-    (is (= expected (#'ordered-hierarchy/first-common-ancestor @#'upload/h type-a type-b))
-        (format "%s + %s = %s" (name type-a) (name type-b) (name expected)))))
 
 (defn csv-file-with
   "Create a temp csv file with the given content and return the file"
@@ -1878,36 +1692,6 @@
                           (append!)))))
                 (io/delete-file file)))))))))
 
-(def ^:private concretize @#'upload/concretize)
-
-(deftest initial-column-type-test
-  (let [column-type (partial concretize nil)]
-    (testing "Unknown value types are treated as text"
-      (is (= ::upload/text (column-type nil))))
-    (testing "Non-abstract value types resolve to themselves"
-      (let [ts @#'upload/column-types]
-        (is (= (zipmap ts ts)
-               (zipmap (map column-type ts) ts)))))
-    (testing "Abstract values are resolved using an explicit map, which is consistent with the hierarchy"
-      (doseq [[value-type expected-column-type] @#'upload/abstract->concrete]
-        (is (= expected-column-type (column-type value-type)))
-        ;; Strictly speaking we only require that there is a route from each abstract node to its column type which only
-        ;; traverses through abstract nodes, but it's much simpler to enforce this stronger condition.
-        (is (contains? (parents @#'upload/h value-type) expected-column-type))))))
-
-(deftest append-column-type-test
-  (doseq [existing-type @#'upload/value-types
-          value-type    @#'upload/value-types]
-    (case [existing-type value-type]
-      [::upload/int ::upload/*float-or-int*]
-      (testing "We coerce floats with fractional part to plan integers when appending into an existing integer column"
-        (is (= ::upload/int (concretize existing-type value-type))))
-
-      ;; This is unsatisfying, would be good if this interface also covered promoting columns and rejecting values.
-      (testing (format "We append %s values to %s columns as if we were inserting them into new columns" existing-type value-type)
-        (is (= (concretize nil value-type)
-               (concretize existing-type value-type)))))))
-
 (deftest create-from-csv-int-and-float-test
   (testing "Creation should handle a mix of int and float-or-int values in any order"
     (mt/test-drivers (mt/normal-drivers-with-feature :uploads)
@@ -1991,18 +1775,3 @@
                    table-rows)))
 
           (io/delete-file file))))))
-
-(deftest coercion-soundness-test
-  (testing "Every coercion maps to a stricter type that is a direct descendant"
-    (is (empty?
-         ;; Build a set of all type-pairs that violate this constraint
-         (into (sorted-set)
-               (comp (mapcat (fn [[column-type value-types]]
-                               (for [value-type value-types]
-                                 [column-type value-type])))
-                     (remove (fn [[column-type value-type]]
-                               ;; Strictly speaking we only require that there is a route which only traverses
-                               ;; through abstract nodes, but it's much simpler to enforce this stronger condition:
-                               ;; that `column-type` is a child of `value-type`
-                               (contains? (ordered-hierarchy/children @#'upload/h value-type) column-type))))
-               @#'upload/column-type->coercible-value-types)))))
