5201f3d61452e5195a63911b6183b7184c1d44c6
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-03-29 11:16:57 -0400

    Fix type

--- enterprise/frontend/src/metabase-enterprise/caching/index.tsx
+++ enterprise/frontend/src/metabase-enterprise/caching/index.tsx
@@ -1,7 +1,4 @@
-import { t } from "ttag";
-
 import { PLUGIN_CACHING, PLUGIN_FORM_WIDGETS } from "metabase/plugins";
-import { Stack, Title } from "metabase/ui";
 import { hasPremiumFeature } from "metabase-enterprise/settings";
 
 import CacheTTLField from "./components/CacheTTLField";
@@ -12,9 +9,9 @@ import QuestionCacheSection from "./components/QuestionCacheSection";
 import QuestionCacheTTLField from "./components/QuestionCacheTTLField";
 import {
   getQuestionsImplicitCacheTTL,
-  validateCacheTTL,
-  normalizeCacheTTL,
   hasQuestionCacheSection,
+  normalizeCacheTTL,
+  validateCacheTTL,
 } from "./utils";
 
 if (hasPremiumFeature("cache_granular_controls")) {
@@ -23,26 +20,13 @@ if (hasPremiumFeature("cache_granular_controls")) {
     validate: validateCacheTTL,
     normalize: normalizeCacheTTL,
   };
-
   PLUGIN_FORM_WIDGETS.dashboardCacheTTL = CacheTTLField;
   PLUGIN_FORM_WIDGETS.databaseCacheTTL = DatabaseCacheTTLField;
   PLUGIN_FORM_WIDGETS.questionCacheTTL = QuestionCacheTTLField;
-
   PLUGIN_CACHING.getQuestionsImplicitCacheTTL = getQuestionsImplicitCacheTTL;
   PLUGIN_CACHING.DatabaseCacheTimeField = DatabaseCacheTimeField;
   PLUGIN_CACHING.DashboardCacheSection = DashboardCacheSection;
   PLUGIN_CACHING.QuestionCacheSection = QuestionCacheSection;
   PLUGIN_CACHING.isEnabled = () => true;
   PLUGIN_CACHING.hasQuestionCacheSection = hasQuestionCacheSection;
-
-  PLUGIN_CACHING.canOverrideRootCacheInvalidationStrategy = true;
-  PLUGIN_CACHING.showAd = false;
-  PLUGIN_CACHING.explanation = (
-    <Stack spacing="xl">
-      {t`Cache the results of queries to have them display instantly. Here you can choose when cached results should be invalidated. You can set up one rule for all your databases, or apply more specific settings to each database.`}
-      <Title
-        order={4}
-      >{t`Pick the policy for when cached query results should be invalidated.`}</Title>
-    </Stack>
-  );
 }
