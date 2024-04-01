8fbd0761cdafc04d02d6adcacd5a256a108fca68
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-04-01 19:34:30 -0400

    Use new enum for hours

--- frontend/src/metabase/admin/performance/components/StrategyEditorForDatabases.tsx
+++ frontend/src/metabase/admin/performance/components/StrategyEditorForDatabases.tsx
@@ -28,342 +28,344 @@ import type {
   Strategy,
   StrategyType,
 } from "../types";
-import { Strategies } from "../types";
+import { DurationUnit, Strategies } from "../types";
 
 import { ResetAllToDefaultButton } from "./ResetAllToDefaultButton";
 import { Panel, TabWrapper } from "./StrategyEditorForDatabases.styled";
 import { StrategyForm } from "./StrategyForm";
 import { StrategyFormLauncher } from "./StrategyFormLauncher";
 
-export const StrategyEditorForDatabases = withRouter(
-  ({
-    canOverrideRootStrategy,
+const StrategyEditorForDatabases_Base = ({
+  canOverrideRootStrategy,
+  router,
+  route,
+}: {
+  canOverrideRootStrategy: boolean;
+  router: InjectedRouter;
+  route?: Route;
+}) => {
+  const [
+    // The targetId is the id of the model that is currently being edited
+    targetId,
+    setTargetId,
+  ] = useState<number | null>(null);
+
+  const databasesResult = useDatabaseListQuery();
+  const databases = databasesResult.data;
+
+  const shouldShowStrategyFormLaunchers = canOverrideRootStrategy;
+
+  const configsResult = useAsync(async () => {
+    const rootConfigsFromAPI = (
+      (await CacheConfigApi.list({ model: "root" })) as CacheConfigAPIResponse
+    ).data;
+    const rootConfig = rootConfigsFromAPI[0] ?? {
+      model: "root",
+      model_id: rootId,
+      strategy: { type: "nocache" },
+    };
+    const dbConfigsFromAPI = canOverrideRootStrategy
+      ? (
+          (await CacheConfigApi.list({
+            model: "database",
+          })) as CacheConfigAPIResponse
+        ).data
+      : [];
+    const configs = [rootConfig, ...dbConfigsFromAPI];
+    return configs;
+  }, []);
+
+  const configsFromAPI = configsResult.value;
+
+  const [configs, setConfigs] = useState<Config[]>([]);
+
+  const rootStrategyOverriddenOnce = configs.some(
+    config => config.model_id !== rootId,
+  );
+
+  const [rootStrategyRecentlyOverridden] = useRecentlyTrue(
+    rootStrategyOverriddenOnce,
+    3000,
+  );
+  const shouldShowResetButton =
+    rootStrategyOverriddenOnce || rootStrategyRecentlyOverridden;
+
+  // When reset button is hidden, create a new version of the form to avoid carrying old context over
+  useEffect(() => {
+    if (!rootStrategyRecentlyOverridden) {
+      setResetFormVersionNumber(n => n + 1);
+    }
+  }, [rootStrategyRecentlyOverridden]);
+
+  useEffect(() => {
+    if (configsFromAPI) {
+      setConfigs(configsFromAPI);
+    }
+  }, [configsFromAPI]);
+
+  /** The config for the model currently being edited */
+  const targetConfig = findWhere(configs, {
+    model_id: targetId ?? undefined,
+  });
+  const savedStrategy = targetConfig?.strategy;
+
+  if (savedStrategy?.type === "duration") {
+    savedStrategy.unit = DurationUnit.Hours;
+  }
+
+  const [nextLocation, setNextLocation] = useState<Location>();
+
+  const { show: askConfirmation, modalContent: confirmationModal } =
+    useConfirmation();
+
+  const askBeforeDiscardingChanges = useCallback(
+    (onConfirm: () => void) =>
+      askConfirmation({
+        title: t`Discard your changes?`,
+        message: t`Your changes haven’t been saved, so you’ll lose them if you navigate away.`,
+        confirmButtonText: t`Discard`,
+        onConfirm,
+      }),
+    [askConfirmation],
+  );
+
+  const [isStrategyFormDirty, setIsStrategyFormDirty] = useState(false);
+  const showStrategyForm = targetId !== null;
+
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  useEffect(() => {
+    if (!route) {
+      return;
+    }
+    const removeLeaveHook = router.setRouteLeaveHook(route, location => {
+      if (isStrategyFormDirty && !isConfirmed) {
+        askBeforeDiscardingChanges(() => {
+          setIsConfirmed(true);
+          setNextLocation(location);
+        });
+        return false;
+      }
+    });
+    return removeLeaveHook;
+  }, [
     router,
     route,
-  }: {
-    canOverrideRootStrategy: boolean;
-    router: InjectedRouter;
-    route?: Route;
-  }) => {
-    const [
-      // The targetId is the id of the model that is currently being edited
-      targetId,
-      setTargetId,
-    ] = useState<number | null>(null);
+    isConfirmed,
+    isStrategyFormDirty,
+    askBeforeDiscardingChanges,
+  ]);
 
-    const databasesResult = useDatabaseListQuery();
-    const databases = databasesResult.data;
+  const dispatch = useDispatch();
 
-    const shouldShowStrategyFormLaunchers = canOverrideRootStrategy;
-
-    const configsResult = useAsync(async () => {
-      const rootConfigsFromAPI = (
-        (await CacheConfigApi.list({ model: "root" })) as CacheConfigAPIResponse
-      ).data;
-      const rootConfig = rootConfigsFromAPI[0] ?? {
-        model: "root",
-        model_id: rootId,
-        strategy: { type: "nocache" },
-      };
-      const dbConfigsFromAPI = canOverrideRootStrategy
-        ? (
-            (await CacheConfigApi.list({
-              model: "database",
-            })) as CacheConfigAPIResponse
-          ).data
-        : [];
-      const configs = [rootConfig, ...dbConfigsFromAPI];
-      return configs;
-    }, []);
-
-    const configsFromAPI = configsResult.value;
-
-    const [configs, setConfigs] = useState<Config[]>([]);
-
-    const rootStrategyOverriddenOnce = configs.some(
-      config => config.model_id !== rootId,
-    );
-
-    const [rootStrategyRecentlyOverridden] = useRecentlyTrue(
-      rootStrategyOverriddenOnce,
-      3000,
-    );
-    const shouldShowResetButton =
-      rootStrategyOverriddenOnce || rootStrategyRecentlyOverridden;
-
-    // When reset button is hidden, create a new version of the form to avoid carrying old context over
-    useEffect(() => {
-      if (!rootStrategyRecentlyOverridden) {
-        setResetFormVersionNumber(n => n + 1);
-      }
-    }, [rootStrategyRecentlyOverridden]);
-
-    useEffect(() => {
-      if (configsFromAPI) {
-        setConfigs(configsFromAPI);
-      }
-    }, [configsFromAPI]);
-
-    /** The config for the model currently being edited */
-    const targetConfig = findWhere(configs, {
-      model_id: targetId ?? undefined,
-    });
-    const savedStrategy = targetConfig?.strategy;
-
-    if (savedStrategy?.type === "duration") {
-      savedStrategy.unit = "hours";
+  useEffect(() => {
+    if (nextLocation) {
+      dispatch(push(nextLocation));
     }
+  }, [dispatch, nextLocation]);
 
-    const [nextLocation, setNextLocation] = useState<Location>();
+  /** Update the targetId (the id of the currently edited model) but confirm if the form is unsaved */
+  const safelyUpdateTargetId: SafelyUpdateTargetId = (
+    newTargetId,
+    isFormDirty,
+  ) => {
+    if (targetId === newTargetId) {
+      return;
+    }
+    const update = () => setTargetId(newTargetId);
+    if (isFormDirty) {
+      askBeforeDiscardingChanges(update);
+    } else {
+      update();
+    }
+  };
 
-    const { show: askConfirmation, modalContent: confirmationModal } =
-      useConfirmation();
+  useEffect(() => {
+    if (!canOverrideRootStrategy && targetId === null) {
+      setTargetId(rootId);
+    }
+  }, [canOverrideRootStrategy, targetId]);
 
-    const askBeforeDiscardingChanges = useCallback(
-      (onConfirm: () => void) =>
-        askConfirmation({
-          title: t`Discard your changes?`,
-          message: t`Your changes haven’t been saved, so you’ll lose them if you navigate away.`,
-          confirmButtonText: t`Discard`,
-          onConfirm,
-        }),
-      [askConfirmation],
-    );
-
-    const [isStrategyFormDirty, setIsStrategyFormDirty] = useState(false);
-    const showStrategyForm = targetId !== null;
-
-    const [isConfirmed, setIsConfirmed] = useState(false);
-
-    useEffect(() => {
-      if (!route) {
+  const saveStrategy = useCallback(
+    async (values: Strategy) => {
+      if (targetId === null) {
         return;
       }
-      const removeLeaveHook = router.setRouteLeaveHook(route, location => {
-        if (isStrategyFormDirty && !isConfirmed) {
-          askBeforeDiscardingChanges(() => {
-            setIsConfirmed(true);
-            setNextLocation(location);
-          });
-          return false;
-        }
-      });
-      return removeLeaveHook;
-    }, [
-      router,
-      route,
-      isConfirmed,
-      isStrategyFormDirty,
-      askBeforeDiscardingChanges,
-    ]);
 
-    const dispatch = useDispatch();
+      const baseConfig: Pick<Config, "model" | "model_id"> = {
+        model: targetId === rootId ? "root" : "database",
+        model_id: targetId,
+      };
 
-    useEffect(() => {
-      if (nextLocation) {
-        dispatch(push(nextLocation));
-      }
-    }, [dispatch, nextLocation]);
-
-    /** Update the targetId (the id of the currently edited model) but confirm if the form is unsaved */
-    const safelyUpdateTargetId: SafelyUpdateTargetId = (
-      newTargetId,
-      isFormDirty,
-    ) => {
-      if (targetId === newTargetId) {
-        return;
-      }
-      const update = () => setTargetId(newTargetId);
-      if (isFormDirty) {
-        askBeforeDiscardingChanges(update);
+      const otherConfigs = configs.filter(
+        config => config.model_id !== targetId,
+      );
+      if (values.type === "inherit") {
+        await CacheConfigApi.delete(baseConfig, { hasBody: true });
+        setConfigs(otherConfigs);
       } else {
-        update();
-      }
-    };
+        const validFields = getFieldsForStrategyType(values.type);
+        const newStrategy = pick(values, validFields) as Strategy;
 
-    useEffect(() => {
-      if (!canOverrideRootStrategy && targetId === null) {
-        setTargetId(rootId);
-      }
-    }, [canOverrideRootStrategy, targetId]);
+        const validatedStrategy =
+          Strategies[values.type].validateWith.validateSync(newStrategy);
 
-    const saveStrategy = useCallback(
-      async (values: Strategy) => {
-        if (targetId === null) {
-          return;
-        }
-
-        const baseConfig: Pick<Config, "model" | "model_id"> = {
-          model: targetId === rootId ? "root" : "database",
-          model_id: targetId,
+        const newConfig = {
+          ...baseConfig,
+          strategy: validatedStrategy,
         };
 
-        const otherConfigs = configs.filter(
-          config => config.model_id !== targetId,
-        );
-        if (values.type === "inherit") {
-          await CacheConfigApi.delete(baseConfig, { hasBody: true });
-          setConfigs(otherConfigs);
-        } else {
-          const validFields = getFieldsForStrategyType(values.type);
-          const newStrategy = pick(values, validFields) as Strategy;
-
-          const validatedStrategy =
-            Strategies[values.type].validateWith.validateSync(newStrategy);
-
-          const newConfig = {
-            ...baseConfig,
-            strategy: validatedStrategy,
-          };
-
-          await CacheConfigApi.update(newConfig);
-          setConfigs([...otherConfigs, newConfig]);
-        }
-      },
-      [configs, setConfigs, targetId],
-    );
-
-    const databaseIds = useMemo(
-      () =>
-        configs.reduce<number[]>(
-          (acc, config) =>
-            config.model === "database" ? [...acc, config.model_id] : acc,
-          [],
-        ),
-      [configs],
-    );
-
-    const [resetFormVersionNumber, setResetFormVersionNumber] = useState(0);
-
-    const resetAllToDefault = useCallback(async () => {
-      const originalConfigs = [...configs];
-      if (databaseIds.length === 0) {
-        return;
+        await CacheConfigApi.update(newConfig);
+        setConfigs([...otherConfigs, newConfig]);
       }
-      try {
-        await CacheConfigApi.delete(
-          { model_id: databaseIds, model: "database" },
-          { hasBody: true },
-        );
-        setConfigs((configs: Config[]) =>
-          configs.filter(({ model }) => model !== "database"),
-        );
-      } catch (e) {
-        setConfigs(originalConfigs);
-        throw e;
-      }
-    }, [configs, setConfigs, databaseIds]);
+    },
+    [configs, setConfigs, targetId],
+  );
 
-    const formPanelRef = useRef<HTMLDivElement>(null);
-    const [formPanelHasVerticalScrollbar, setFormPanelHasVerticalScrollbar] =
-      useState(false);
+  const databaseIds = useMemo(
+    () =>
+      configs.reduce<number[]>(
+        (acc, config) =>
+          config.model === "database" ? [...acc, config.model_id] : acc,
+        [],
+      ),
+    [configs],
+  );
 
-    useEffect(() => {
-      const formPanel = formPanelRef.current;
-      if (!formPanel) {
-        return;
-      }
-      setFormPanelHasVerticalScrollbar(
-        formPanel.scrollHeight > formPanel.clientHeight,
-      );
-    }, [
-      formPanelRef.current?.scrollHeight,
-      formPanelRef.current?.clientHeight,
-      formPanelRef,
-      setFormPanelHasVerticalScrollbar,
-    ]);
+  const [resetFormVersionNumber, setResetFormVersionNumber] = useState(0);
 
-    const showSpinner = useDelayedLoadingSpinner();
-    const error = databasesResult.error || configsResult.error;
-    const loading = databasesResult.isLoading || configsResult.loading;
-
-    if (error || loading) {
-      return showSpinner ? (
-        <LoadingAndErrorWrapper error={error} loading={loading} />
-      ) : (
-        <></>
-      );
+  const resetAllToDefault = useCallback(async () => {
+    const originalConfigs = [...configs];
+    if (databaseIds.length === 0) {
+      return;
     }
+    try {
+      await CacheConfigApi.delete(
+        { model_id: databaseIds, model: "database" },
+        { hasBody: true },
+      );
+      setConfigs((configs: Config[]) =>
+        configs.filter(({ model }) => model !== "database"),
+      );
+    } catch (e) {
+      setConfigs(originalConfigs);
+      throw e;
+    }
+  }, [configs, setConfigs, databaseIds]);
 
-    const rootConfig = findWhere(configs, { model_id: rootId });
+  const formPanelRef = useRef<HTMLDivElement>(null);
+  const [formPanelHasVerticalScrollbar, setFormPanelHasVerticalScrollbar] =
+    useState(false);
 
-    const rootConfigLabel =
-      (rootConfig?.strategy.type
-        ? Strategies[rootConfig?.strategy.type].shortLabel
-        : null) ?? "default";
+  useEffect(() => {
+    const formPanel = formPanelRef.current;
+    if (!formPanel) {
+      return;
+    }
+    setFormPanelHasVerticalScrollbar(
+      formPanel.scrollHeight > formPanel.clientHeight,
+    );
+  }, [
+    formPanelRef.current?.scrollHeight,
+    formPanelRef.current?.clientHeight,
+    formPanelRef,
+    setFormPanelHasVerticalScrollbar,
+  ]);
 
-    return (
-      <TabWrapper role="region" aria-label={t`Data caching settings`}>
-        <Stack spacing="xl" lh="1.5rem" maw="32rem" mb="1.5rem">
-          <aside>{PLUGIN_CACHING.explanation}</aside>
-        </Stack>
-        {confirmationModal}
-        <Box
-          style={{
-            display: "grid",
-            gridTemplateColumns: "minmax(5rem, 30rem) minmax(5rem, auto)",
-            overflow: "hidden",
-          }}
-          w="100%"
-          mx="0"
-          mb="1rem"
-        >
-          {shouldShowStrategyFormLaunchers && (
-            <Panel role="group" style={{ backgroundColor: color("bg-light") }}>
-              <Box
-                p="lg"
-                style={{ borderBottom: `1px solid ${color("border")}` }}
-              >
+  const showSpinner = useDelayedLoadingSpinner();
+  const error = databasesResult.error || configsResult.error;
+  const loading = databasesResult.isLoading || configsResult.loading;
+
+  if (error || loading) {
+    return showSpinner ? (
+      <LoadingAndErrorWrapper error={error} loading={loading} />
+    ) : (
+      <></>
+    );
+  }
+
+  const rootConfig = findWhere(configs, { model_id: rootId });
+
+  const rootConfigLabel =
+    (rootConfig?.strategy.type
+      ? Strategies[rootConfig?.strategy.type].shortLabel
+      : null) ?? "default";
+
+  return (
+    <TabWrapper role="region" aria-label={t`Data caching settings`}>
+      <Stack spacing="xl" lh="1.5rem" maw="32rem" mb="1.5rem">
+        <aside>{PLUGIN_CACHING.explanation}</aside>
+      </Stack>
+      {confirmationModal}
+      <Box
+        style={{
+          display: "grid",
+          gridTemplateColumns: "minmax(5rem, 30rem) minmax(5rem, auto)",
+          overflow: "hidden",
+        }}
+        w="100%"
+        mx="0"
+        mb="1rem"
+      >
+        {shouldShowStrategyFormLaunchers && (
+          <Panel role="group" style={{ backgroundColor: color("bg-light") }}>
+            <Box
+              p="lg"
+              style={{ borderBottom: `1px solid ${color("border")}` }}
+            >
+              <StrategyFormLauncher
+                forId={rootId}
+                title={t`Default policy`}
+                configs={configs}
+                targetId={targetId}
+                safelyUpdateTargetId={safelyUpdateTargetId}
+                isFormDirty={isStrategyFormDirty}
+              />
+            </Box>
+            <Stack p="lg" spacing="md">
+              {databases?.map(db => (
                 <StrategyFormLauncher
-                  forId={rootId}
-                  title={t`Default policy`}
+                  forId={db.id}
+                  title={db.name}
                   configs={configs}
                   targetId={targetId}
                   safelyUpdateTargetId={safelyUpdateTargetId}
                   isFormDirty={isStrategyFormDirty}
+                  key={`database_${db.id}`}
                 />
-              </Box>
-              <Stack p="lg" spacing="md">
-                {databases?.map(db => (
-                  <StrategyFormLauncher
-                    forId={db.id}
-                    title={db.name}
-                    configs={configs}
-                    targetId={targetId}
-                    safelyUpdateTargetId={safelyUpdateTargetId}
-                    isFormDirty={isStrategyFormDirty}
-                    key={`database_${db.id}`}
-                  />
-                ))}
-              </Stack>
-              <FormProvider
-                initialValues={{}}
-                onSubmit={resetAllToDefault}
-                key={resetFormVersionNumber} // To avoid using stale context
-              >
-                {shouldShowResetButton && (
-                  <ResetAllToDefaultButton rootConfigLabel={rootConfigLabel} />
-                )}
-              </FormProvider>
-            </Panel>
-          )}
-          <Panel
-            ref={formPanelRef}
-            hasVerticalScrollbar={formPanelHasVerticalScrollbar}
-          >
-            {showStrategyForm && (
-              <StrategyForm
-                targetId={targetId}
-                setIsDirty={setIsStrategyFormDirty}
-                saveStrategy={saveStrategy}
-                savedStrategy={savedStrategy}
-              />
-            )}
+              ))}
+            </Stack>
+            <FormProvider
+              initialValues={{}}
+              onSubmit={resetAllToDefault}
+              key={resetFormVersionNumber} // To avoid using stale context
+            >
+              {shouldShowResetButton && (
+                <ResetAllToDefaultButton rootConfigLabel={rootConfigLabel} />
+              )}
+            </FormProvider>
           </Panel>
-        </Box>
-      </TabWrapper>
-    );
-  },
+        )}
+        <Panel
+          ref={formPanelRef}
+          hasVerticalScrollbar={formPanelHasVerticalScrollbar}
+        >
+          {showStrategyForm && (
+            <StrategyForm
+              targetId={targetId}
+              setIsDirty={setIsStrategyFormDirty}
+              saveStrategy={saveStrategy}
+              savedStrategy={savedStrategy}
+            />
+          )}
+        </Panel>
+      </Box>
+    </TabWrapper>
+  );
+};
+
+export const StrategyEditorForDatabases = withRouter(
+  StrategyEditorForDatabases_Base,
 );
 
 const getFieldsForStrategyType = (strategyType: StrategyType) => {
