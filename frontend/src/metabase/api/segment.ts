e892dc6218251391c3c4c6d793cf76c19cd18853
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-04-01 16:09:47 +0300

    Use RTK for segments

--- frontend/src/metabase/api/segment.ts
+++ frontend/src/metabase/api/segment.ts
@@ -1,7 +1,13 @@
-import type { Segment, SegmentId } from "metabase-types/api";
+import type {
+  CreateSegmentRequest,
+  DeleteSegmentRequest,
+  Segment,
+  SegmentId,
+  UpdateSegmentRequest,
+} from "metabase-types/api";
 
 import { Api } from "./api";
-import { idTag, listTag } from "./tags";
+import { idTag, invalidateTags, listTag } from "./tags";
 
 export const segmentApi = Api.injectEndpoints({
   endpoints: builder => ({
@@ -22,7 +28,40 @@ export const segmentApi = Api.injectEndpoints({
       }),
       providesTags: segment => (segment ? [idTag("segment", segment.id)] : []),
     }),
+    createSegment: builder.mutation<Segment, CreateSegmentRequest>({
+      query: body => ({
+        method: "POST",
+        url: "/api/segment",
+        body,
+      }),
+      invalidatesTags: (_, error) =>
+        invalidateTags(error, [listTag("segment")]),
+    }),
+    updateSegment: builder.mutation<Segment, UpdateSegmentRequest>({
+      query: ({ id, ...body }) => ({
+        method: "PUT",
+        url: `/api/segment/${id}`,
+        body,
+      }),
+      invalidatesTags: (_, error, { id }) =>
+        invalidateTags(error, [listTag("segment"), idTag("segment", id)]),
+    }),
+    deleteSegment: builder.mutation<Segment, DeleteSegmentRequest>({
+      query: ({ id, ...body }) => ({
+        method: "DELETE",
+        url: `/api/segment/${id}`,
+        body,
+      }),
+      invalidatesTags: (_, error, { id }) =>
+        invalidateTags(error, [listTag("segment"), idTag("segment", id)]),
+    }),
   }),
 });
 
-export const { useListSegmentsQuery, useGetSegmentQuery } = segmentApi;
+export const {
+  useListSegmentsQuery,
+  useGetSegmentQuery,
+  useCreateSegmentMutation,
+  useUpdateSegmentMutation,
+  useDeleteSegmentMutation,
+} = segmentApi;
