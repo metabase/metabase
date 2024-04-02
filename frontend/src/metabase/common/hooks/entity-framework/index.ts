e9976c5d4c17c5a40e3b6d046a0620aa05684f1a
Kamil Mielnik <kamil@kamilmielnik.com>
2024-04-02 21:35:24 +0700

    Remove useSchemaListQuery

--- frontend/src/metabase/common/hooks/entity-framework/index.ts
+++ frontend/src/metabase/common/hooks/entity-framework/index.ts
@@ -19,7 +19,6 @@ export * from "./use-question-query";
 export * from "./use-recent-item-list-query";
 export * from "./use-revision-list-query";
 export * from "./use-search-list-query";
-export * from "./use-schema-list-query";
 export * from "./use-table-list-query";
 export * from "./use-table-query";
 export * from "./use-user-list-query";
