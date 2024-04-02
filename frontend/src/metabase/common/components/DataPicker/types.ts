1fc62305ba76aaba978a9d21245f2660a71f23d6
Kamil Mielnik <kamil@kamilmielnik.com>
2024-04-02 19:44:24 +0700

    Remove unused types

--- frontend/src/metabase/common/components/DataPicker/types.ts
+++ frontend/src/metabase/common/components/DataPicker/types.ts
@@ -2,14 +2,9 @@ import type {
   Card,
   Collection,
   Database,
-  DatabaseListQuery,
   NormalizedSchema,
-  SchemaListQuery,
   Table,
-  TableListQuery,
 } from "metabase-types/api";
-
-import type { PickerState } from "../EntityPicker";
 
 export type CollectionItem = {
   id: Collection["id"];
@@ -47,49 +42,15 @@ export type ModelItem = {
   model: "dataset";
 };
 
-export type NotebookDataPickerFolderItem =
-  | CollectionItem
-  | DatabaseItem
-  | SchemaItem;
-
 export type Value = {
   id: Table["id"];
   db_id: Table["db_id"];
   schema?: Table["schema"];
 };
 
-export type TablePickerFolderItem = DatabaseItem | SchemaItem;
+export type NotebookDataPickerFolderItem =
+  | CollectionItem
+  | DatabaseItem
+  | SchemaItem;
 
 export type NotebookDataPickerValueItem = TableItem | QuestionItem | ModelItem;
-
-export type NotebookDataPickerItem =
-  | NotebookDataPickerFolderItem
-  | NotebookDataPickerValueItem;
-
-export type NotebookDataPickerModel = NotebookDataPickerItem["model"];
-
-// export type Value = NotebookDataPickerValueItem["id"];
-
-export type PathEntry<Model extends NotebookDataPickerFolderItem["model"]> =
-  PickerState<Model, NotebookDataPickerItem, NotebookDataPickerQuery<Model>>;
-
-// TODO
-export type NotebookDataPickerQuery<Model> = Model extends "database"
-  ? DatabaseListQuery
-  : Model extends "schema"
-  ? SchemaListQuery
-  : TableListQuery;
-
-// export type NotebookDataPickerQuery =
-//   | {
-//       model: "table";
-//       query: TableListQuery;
-//     }
-//   | {
-//       model: "schema";
-//       query: SchemaListQuery;
-//     }
-//   | {
-//       model: "database";
-//       query: DatabaseListQuery;
-//     };
