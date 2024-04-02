15f5157e8b86d85af1bd4d3d093986e9d22988af
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-04-02 21:04:18 +0700

    Fix default export

--- frontend/src/metabase/nav/containers/MainNavbar/SidebarItems/DraggableSidebarLink.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/SidebarItems/DraggableSidebarLink.tsx
@@ -7,9 +7,6 @@ interface Props extends Omit<SidebarLinkProps, "left"> {
   isDragging: boolean;
 }
 
-function DraggableSidebarLink(props: Props) {
+export function DraggableSidebarLink(props: Props) {
   return <StyledSidebarLink {...props} left={<DragIcon name="grabber" />} />;
 }
-
-// eslint-disable-next-line import/no-default-export -- deprecated usage
-export default DraggableSidebarLink;
