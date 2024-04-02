44e1883f2d0d107566ef842b07363d5790ea8bb2
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-04-02 09:06:27 -0400

    Reset file

--- e2e/test/scenarios/admin/settings/cache.cy.spec.js
+++ e2e/test/scenarios/admin/settings/cache.cy.spec.js
@@ -6,59 +6,63 @@ const nativeQuery = "select (random() * random() * random()), pg_sleep(2)";
  * Disabled and quarantined until we fix the caching issues, and especially:
  * https://github.com/metabase/metabase/issues/13262
  */
-describe("scenarios > admin > settings > cache", { tags: "@external" }, () => {
-  beforeEach(() => {
-    cy.intercept("POST", "/api/dataset").as("dataset");
-    cy.intercept("POST", "/api/card/*/query").as("cardQuery");
-
-    restore("postgres-12");
-    cy.signInAsAdmin();
-  });
-
-  describe("issue 18458", () => {
+describe.skip(
+  "scenarios > admin > settings > cache",
+  { tags: "@external" },
+  () => {
     beforeEach(() => {
-      cy.visit("/admin/settings/caching");
+      cy.intercept("POST", "/api/dataset").as("dataset");
+      cy.intercept("POST", "/api/card/*/query").as("cardQuery");
 
-      enableCaching();
-
-      setCachingValue("Minimum Query Duration", "1");
-      setCachingValue("Cache Time-To-Live (TTL) multiplier", "2");
-
-      // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-      cy.findByText("Saved");
-
-      // Run the query and save the question
-      openNativeEditor({ databaseName: "QA Postgres12" }).type(nativeQuery);
-      runNativeQuery();
-
-      getCellText().then(res => {
-        cy.wrap(res).as("tempResult");
-      });
-
-      saveQuestion("18458");
+      restore("postgres-12");
+      cy.signInAsAdmin();
     });
 
-    it("should respect previously set cache duration (metabase#18458)", () => {
-      refreshUntilCached();
+    describe("issue 18458", () => {
+      beforeEach(() => {
+        cy.visit("/admin/settings/caching");
 
-      cy.get("@cachedResult").then(cachedValue => {
-        /**
-         * 5s is longer than what we set the cache to last:
-         * Approx 2s for an Average Runtime x multiplier of 2.
-         *
-         * The cache should expire after 4s and we should see a new random result.
-         */
-        cy.wait(5000);
+        enableCaching();
 
-        refresh();
+        setCachingValue("Minimum Query Duration", "1");
+        setCachingValue("Cache Time-To-Live (TTL) multiplier", "2");
 
-        getCellText().then(newValue => {
-          expect(newValue).to.not.eq(cachedValue);
+        // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
+        cy.findByText("Saved");
+
+        // Run the query and save the question
+        openNativeEditor({ databaseName: "QA Postgres12" }).type(nativeQuery);
+        runNativeQuery();
+
+        getCellText().then(res => {
+          cy.wrap(res).as("tempResult");
+        });
+
+        saveQuestion("18458");
+      });
+
+      it("should respect previously set cache duration (metabase#18458)", () => {
+        refreshUntilCached();
+
+        cy.get("@cachedResult").then(cachedValue => {
+          /**
+           * 5s is longer than what we set the cache to last:
+           * Approx 2s for an Average Runtime x multiplier of 2.
+           *
+           * The cache should expire after 4s and we should see a new random result.
+           */
+          cy.wait(5000);
+
+          refresh();
+
+          getCellText().then(newValue => {
+            expect(newValue).to.not.eq(cachedValue);
+          });
         });
       });
     });
-  });
-});
+  },
+);
 
 function enableCaching() {
   cy.findByText("Disabled")
