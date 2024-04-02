cad018c969ab0bd575b0d7a8e8e3011a7ebd1475
Nicolò Pretto <info@npretto.com>
2024-04-02 15:10:54 +0200

    use "named" setters and getters as suggested by Noah

--- src/metabase/embed/settings.clj
+++ src/metabase/embed/settings.clj
@@ -7,6 +7,7 @@
    [metabase.api.common :as api]
    [metabase.models.setting :as setting :refer [defsetting]]
    [metabase.public-settings :as public-settings]
+   [metabase.util.embed :as embed]
    [metabase.util.i18n :as i18n :refer [deferred-tru]]
    [toucan2.core :as t2]))
 
@@ -27,8 +28,8 @@
   :setter     (fn [new-value]
                 (when (not= new-value (setting/get-value-of-type :boolean :enable-embedding))
                   (setting/set-value-of-type! :boolean :enable-embedding new-value)
-                  (when (and new-value (str/blank? (setting/get :embedding-secret-key)))
-                    (setting/set! :embedding-secret-key (crypto-random/hex 32)))
+                  (when (and new-value (str/blank? (embed/embedding-secret-key)))
+                    (embed/embedding-secret-key! (crypto-random/hex 32)))
                   (let [snowplow-payload {:embedding-app-origin-set   (boolean (embedding-app-origin))
                                           :number-embedded-questions  (t2/count :model/Card :enable_embedding true)
                                           :number-embedded-dashboards (t2/count :model/Dashboard :enable_embedding true)}]
