0ac94456db84bc9827051e16da448d687fa4cdbe
Ryan Laurie <iethree@gmail.com>
2024-04-02 16:30:14 -0600

    auto-translate works!

--- release/src/auto-translate.ts
+++ release/src/auto-translate.ts
@@ -1,28 +1,65 @@
 import 'zx/globals';
+$.verbose = false;
 
 const baseUrl = 'https://api.poeditor.com/v2'
 const POEDITOR_API_TOKEN = process.env.POEDITOR_API_TOKEN;
-const POEDITOR_PROJECT_ID = "200535"; // Metabae POEditor project
-const SOURCE_LANGUAGE = "english";
-const PROVIDER = "deepl"; // TODO: fallback to google,
+const POEDITOR_PROJECT_ID = "200535"; // Metabase POEditor project
+const SOURCE_LANGUAGE = "en";
+const DEFAULT_PROVIDER = "deepl";
+const url = `${baseUrl}/translations/automatic`;
 
-const languages = [
-  'es',
-  'pl',
+// deepl doesn't support these languages
+const google_only_languages = [
+  'ar',
+  'ar-SA',
+  'ca',
+  'fa',
+  'he',
+  'id',
+  'sq',
+  'sr',
+  'vi',
 ];
+
+// alias from our file names to poeditor's language codes
+const aliases: Record<string, string> = {
+  "zh": 'zh-Hans',
+};
+
+// alias from poeditor's language codes to deepl or google's language codes
+const providerAliases: Record<string, string> = {
+  'he': 'iw', // google
+  "zh-CN": "zh", // deepl
+  "zh-HK": "zh", // deepl
+  "zh-TW": "zh", // deepl
+  "zh-Hans": "zh", // deepl
+};
 
 // https://poeditor.com/docs/api#translations_automatic
 const autoTranslate = async (language: string) => {
-  const url = `${baseUrl}/translations/automatic`;
+  if (language in aliases) {
+    language = aliases[language];
+  }
+
+  const provider = google_only_languages.includes(language) ? 'google' : DEFAULT_PROVIDER;
+
+  const config = {
+    source_language: SOURCE_LANGUAGE,
+    provider_source_language: provider === 'deepl'
+      ? SOURCE_LANGUAGE.toUpperCase() // deepl uses uppercase
+      : SOURCE_LANGUAGE,
+    provider,
+    target_languages: [{
+      project_language: language,
+      provider_language: provider === 'deepl'
+        ? (providerAliases[language] ?? language).toUpperCase() // deepl uses uppercase
+        : (providerAliases[language] ?? language),
+    }],
+  };
 
   // poeditor doesn't like the escaping that UrlSearchParams does
-  const encodedData = `api_token=${POEDITOR_API_TOKEN}&id=${POEDITOR_PROJECT_ID}&source_language="${SOURCE_LANGUAGE}"&provider_source_language="${SOURCE_LANGUAGE}"&provider="${PROVIDER}"`
-     + `&target_languages=${JSON.stringify([{
-        project_language: language,
-        provider_language: language,
-      }]).replaceAll('"', '\"')}`;
-
-  console.log(encodedData)
+  const encodedData = `api_token=${POEDITOR_API_TOKEN}&id=${POEDITOR_PROJECT_ID}&source_language=${config.source_language}&provider_source_language=${config.provider_source_language}&provider=${provider}`
+    + `&target_languages=${JSON.stringify(config.target_languages).replaceAll('"', '\"')}`;
 
   const response = await fetch(url, {
     method: 'POST',
@@ -32,18 +69,22 @@ const autoTranslate = async (language: string) => {
     body: encodedData,
   });
 
-  console.log(await response.json())
+  const message = (await response.json())?.result;
+
+  console.log(message);
 }
 
-await autoTranslate('es');
-/**
+function getExistingLanguages() {
+  const localeFiles = fs.readdirSync("../locales");
 
-curl -X POST https://api.poeditor.com/v2/translations/automatic \
-     -d api_token="xxx" \
-     -d id="200535" \
-     -d source_language="en" \
-     -d provider_source_language="en" \
-     -d provider="deepl" \
-     -d target_languages="[{\"project_language\":\"es\",\"provider_language\":\"es\"}]"
- */
+  return localeFiles.filter(f => /\.po$/.test(f))
+    .map(f => f.replace(/\.po$/, ""));
+}
 
+const supportedLanguages = getExistingLanguages();
+
+for (const language of supportedLanguages) {
+  // do this in a for/await loop to avoid rate limiting
+  console.log(chalk.blue(`\nTranslating ${language}`))
+  await autoTranslate(language);
+}
