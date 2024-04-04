5670664bbff9b52b8db6d16d4483c53c97f43cbb
Anton Kulyk <kuliks.anton@gmail.com>
2024-04-04 19:25:22 +0100

    Try using a separate canvas object for each cartesian chart (#41048)
    * Extract `createTextMeasurer`
    
    * Use separate canvas for each cartesian chart

--- frontend/src/metabase/lib/measure-text.ts
+++ frontend/src/metabase/lib/measure-text.ts
@@ -3,31 +3,26 @@ import type {
   TextMeasurer,
 } from "metabase/visualizations/shared/types/measure-text";
 
-let canvas: HTMLCanvasElement | null = null;
+export const createTextMeasurer =
+  (canvas: HTMLCanvasElement): TextMeasurer =>
+  (text, style) => {
+    const context = canvas.getContext("2d");
+    if (!context) {
+      throw new Error("Could not create canvas context");
+    }
 
-export const measureText: TextMeasurer = (text: string, style: FontStyle) => {
-  canvas ??= document.createElement("canvas");
-  const context = canvas.getContext("2d");
+    context.font = `${style.weight} ${style.size} ${style.family}`;
+    const textMetrics = context.measureText(text);
 
-  // Temporary here to debug
-  // https://github.com/metabase/metabase/issues/40206
-  // @ts-expect-error — doing a bad thing here
-  measureTextWidth.canvasContext = context;
-
-  if (!context) {
-    throw new Error("Could not create canvas context");
-  }
-
-  context.font = `${style.weight} ${style.size} ${style.family}`;
-  const textMetrics = context.measureText(text);
-
-  return {
-    width: textMetrics.width,
-    height:
-      textMetrics.actualBoundingBoxAscent +
-      textMetrics.actualBoundingBoxDescent,
+    return {
+      width: textMetrics.width,
+      height:
+        textMetrics.actualBoundingBoxAscent +
+        textMetrics.actualBoundingBoxDescent,
+    };
   };
-};
+
+export const measureText = createTextMeasurer(document.createElement("canvas"));
 
 export const measureTextWidth = (text: string, style: FontStyle) =>
   measureText(text, style).width;
