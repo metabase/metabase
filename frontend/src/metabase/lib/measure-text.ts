8d67550e836e6383b4fccbfcd398f64781e2ef5b
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-04-04 17:34:47 -0300

    Revert "Try using a separate canvas object for each cartesian chart (#41048)"
    This reverts commit 8859c10b183c980fb1ef47b1cd4c8653a6a6c9b9.

--- frontend/src/metabase/lib/measure-text.ts
+++ frontend/src/metabase/lib/measure-text.ts
@@ -3,26 +3,31 @@ import type {
   TextMeasurer,
 } from "metabase/visualizations/shared/types/measure-text";
 
-export const createTextMeasurer =
-  (canvas: HTMLCanvasElement): TextMeasurer =>
-  (text, style) => {
-    const context = canvas.getContext("2d");
-    if (!context) {
-      throw new Error("Could not create canvas context");
-    }
+let canvas: HTMLCanvasElement | null = null;
 
-    context.font = `${style.weight} ${style.size} ${style.family}`;
-    const textMetrics = context.measureText(text);
+export const measureText: TextMeasurer = (text: string, style: FontStyle) => {
+  canvas ??= document.createElement("canvas");
+  const context = canvas.getContext("2d");
 
-    return {
-      width: textMetrics.width,
-      height:
-        textMetrics.actualBoundingBoxAscent +
-        textMetrics.actualBoundingBoxDescent,
-    };
+  // Temporary here to debug
+  // https://github.com/metabase/metabase/issues/40206
+  // @ts-expect-error — doing a bad thing here
+  measureTextWidth.canvasContext = context;
+
+  if (!context) {
+    throw new Error("Could not create canvas context");
+  }
+
+  context.font = `${style.weight} ${style.size} ${style.family}`;
+  const textMetrics = context.measureText(text);
+
+  return {
+    width: textMetrics.width,
+    height:
+      textMetrics.actualBoundingBoxAscent +
+      textMetrics.actualBoundingBoxDescent,
   };
-
-export const measureText = createTextMeasurer(document.createElement("canvas"));
+};
 
 export const measureTextWidth = (text: string, style: FontStyle) =>
   measureText(text, style).width;
