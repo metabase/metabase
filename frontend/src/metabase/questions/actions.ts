ab48fd205bef69fa91e73de84fb654cc040b777e
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-04-05 15:05:44 +0300

    Fix tracking for notebook native preview

--- frontend/src/metabase/questions/actions.ts
+++ frontend/src/metabase/questions/actions.ts
@@ -2,7 +2,6 @@ import { loadMetadataForDependentItems } from "metabase/redux/metadata";
 import { getMetadata } from "metabase/selectors/metadata";
 import * as Lib from "metabase-lib";
 import Question from "metabase-lib/v1/Question";
-import { getQuestionVirtualTableId } from "metabase-lib/v1/metadata/utils/saved-questions";
 import type { Card } from "metabase-types/api";
 import type { Dispatch, GetState } from "metabase-types/store";
 
@@ -14,16 +13,11 @@ export const loadMetadataForCard =
   (card: Card, options?: LoadMetadataOptions) =>
   async (dispatch: Dispatch, getState: GetState) => {
     const question = new Question(card, getMetadata(getState()));
-    const loadAdhocMetadata =
-      question.isSaved() && question.type() !== "question";
-    const dependencies = [...Lib.dependentMetadata(question.query())];
-    if (loadAdhocMetadata) {
-      const tableId = getQuestionVirtualTableId(question.id());
-      dependencies.push({ id: tableId, type: "table" });
-    }
+    const dependencies = Lib.dependentMetadata(question.query());
     await dispatch(loadMetadataForDependentItems(dependencies, options));
 
-    if (loadAdhocMetadata) {
+    // metadata for an ad-hoc question based on this question
+    if (question.isSaved() && question.type() !== "question") {
       const questionWithMetadata = new Question(card, getMetadata(getState()));
       const adhocQuestion = questionWithMetadata.composeQuestionAdhoc();
       const adhocDependencies = Lib.dependentMetadata(adhocQuestion.query());
