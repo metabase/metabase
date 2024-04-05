edba1ac186f4585e2b3b000d45eb743ad742a685
Romeo Van Snick <romeo@romeovansnick.be>
2024-04-05 11:23:15 -0400

    Split out help text content component

--- frontend/src/metabase/query_builder/components/expressions/ExpressionEditorHelpText/ExpressionEditorHelpText.tsx
+++ frontend/src/metabase/query_builder/components/expressions/ExpressionEditorHelpText/ExpressionEditorHelpText.tsx
@@ -21,23 +21,88 @@ import {
   LearnMoreIcon,
 } from "./ExpressionEditorHelpText.styled";
 
-export interface ExpressionEditorHelpTextProps {
+export type ExpressionEditorHelpTextContentProps = {
   helpText: HelpText | null | undefined;
-  width: number | undefined;
-  target: React.RefObject<HTMLElement>;
-}
+};
+
+export type ExpressionEditorHelpTextProps =
+  ExpressionEditorHelpTextContentProps & {
+    target: React.RefObject<HTMLElement>;
+    width: number | undefined;
+  };
+
+export const ExpressionEditorHelpTextContent = ({
+  helpText,
+}: ExpressionEditorHelpTextContentProps) => {
+  const showMetabaseLinks = useSelector(getShowMetabaseLinks);
+
+  if (!helpText) {
+    return null;
+  }
+
+  const { description, structure, args } = helpText;
+
+  return (
+    <>
+      {/* Prevent stealing focus from input box causing the help text to be closed (metabase#17548) */}
+      <Container
+        onMouseDown={e => e.preventDefault()}
+        data-testid="expression-helper-popover"
+      >
+        <FunctionHelpCode data-testid="expression-helper-popover-structure">
+          {structure}
+          {args != null && (
+            <>
+              (
+              {args.map(({ name }, index) => (
+                <span key={name}>
+                  <FunctionHelpCodeArgument>{name}</FunctionHelpCodeArgument>
+                  {index + 1 < args.length && ", "}
+                </span>
+              ))}
+              )
+            </>
+          )}
+        </FunctionHelpCode>
+        <Divider />
+
+        <div>{description}</div>
+
+        {args != null && (
+          <ArgumentsGrid data-testid="expression-helper-popover-arguments">
+            {args.map(({ name, description: argDescription }) => (
+              <React.Fragment key={name}>
+                <ArgumentTitle>{name}</ArgumentTitle>
+                <div>{argDescription}</div>
+              </React.Fragment>
+            ))}
+          </ArgumentsGrid>
+        )}
+
+        <BlockSubtitleText>{t`Example`}</BlockSubtitleText>
+        <ExampleCode>{helpText.example}</ExampleCode>
+        {showMetabaseLinks && (
+          <DocumentationLink
+            href={MetabaseSettings.docsUrl(getHelpDocsUrl(helpText))}
+            target="_blank"
+          >
+            <LearnMoreIcon name="reference" size={12} />
+            {t`Learn more`}
+          </DocumentationLink>
+        )}
+      </Container>
+    </>
+  );
+};
 
 export const ExpressionEditorHelpText = ({
   helpText,
   width,
   target,
 }: ExpressionEditorHelpTextProps) => {
-  const showMetabaseLinks = useSelector(getShowMetabaseLinks);
   if (!helpText) {
     return null;
   }
-
-  const { description, structure, args } = helpText;
 
   return (
     <TippyPopover
@@ -46,59 +111,7 @@ export const ExpressionEditorHelpText = ({
       placement="bottom-start"
       visible
       zIndex={300}
-      content={
-        <>
-          {/* Prevent stealing focus from input box causing the help text to be closed (metabase#17548) */}
-          <Container
-            onMouseDown={e => e.preventDefault()}
-            data-testid="expression-helper-popover"
-          >
-            <FunctionHelpCode data-testid="expression-helper-popover-structure">
-              {structure}
-              {args != null && (
-                <>
-                  (
-                  {args.map(({ name }, index) => (
-                    <span key={name}>
-                      <FunctionHelpCodeArgument>
-                        {name}
-                      </FunctionHelpCodeArgument>
-                      {index + 1 < args.length && ", "}
-                    </span>
-                  ))}
-                  )
-                </>
-              )}
-            </FunctionHelpCode>
-            <Divider />
-
-            <div>{description}</div>
-
-            {args != null && (
-              <ArgumentsGrid data-testid="expression-helper-popover-arguments">
-                {args.map(({ name, description: argDescription }) => (
-                  <React.Fragment key={name}>
-                    <ArgumentTitle>{name}</ArgumentTitle>
-                    <div>{argDescription}</div>
-                  </React.Fragment>
-                ))}
-              </ArgumentsGrid>
-            )}
-
-            <BlockSubtitleText>{t`Example`}</BlockSubtitleText>
-            <ExampleCode>{helpText.example}</ExampleCode>
-            {showMetabaseLinks && (
-              <DocumentationLink
-                href={MetabaseSettings.docsUrl(getHelpDocsUrl(helpText))}
-                target="_blank"
-              >
-                <LearnMoreIcon name="reference" size={12} />
-                {t`Learn more`}
-              </DocumentationLink>
-            )}
-          </Container>
-        </>
-      }
+      content={<ExpressionEditorHelpTextContent helpText={helpText} />}
     />
   );
 };
