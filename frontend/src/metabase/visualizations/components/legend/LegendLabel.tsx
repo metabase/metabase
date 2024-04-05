20dadeaaea24d11fc1a980e1d3494e655ed5b46a
Kamil Mielnik <kamil@kamilmielnik.com>
2024-04-05 18:14:57 +0700

    Use project convention

--- frontend/src/metabase/visualizations/components/legend/LegendLabel.tsx
+++ frontend/src/metabase/visualizations/components/legend/LegendLabel.tsx
@@ -1,9 +1,9 @@
-import classNames from "classnames";
+import cx from "classnames";
 import type { ReactNode } from "react";
 import type { LinkProps } from "react-router";
 import { Link } from "react-router";
 
-import styles from "./LegendLabel.module.css";
+import S from "./LegendLabel.module.css";
 
 interface Props {
   children: ReactNode;
@@ -13,11 +13,11 @@ interface Props {
 
 export const LegendLabel = ({ children, className, href }: Props) => {
   if (!href) {
-    return <div className={classNames(styles.text, className)}>{children}</div>;
+    return <div className={cx(S.text, className)}>{children}</div>;
   }
 
   return (
-    <Link className={classNames(styles.text, styles.link, className)} to={href}>
+    <Link className={cx(S.text, S.link, className)} to={href}>
       {children}
     </Link>
   );
