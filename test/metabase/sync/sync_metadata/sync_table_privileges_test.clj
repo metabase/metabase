5334d240832ab0550a4aef51814216ac0467e5e2
Cal Herries <calherries@gmail.com>
2024-04-08 16:02:51 +0300

    fix test

--- test/metabase/sync/sync_metadata/sync_table_privileges_test.clj
+++ test/metabase/sync/sync_metadata/sync_table_privileges_test.clj
@@ -12,7 +12,8 @@
 (set! *warn-on-reflection* true)
 
 (deftest sync-table-privileges!-test
-  (mt/test-drivers (disj (set/intersection (mt/normal-drivers-with-feature :schemas)))
+  (mt/test-drivers (set/intersection (mt/normal-drivers-with-feature :table-privileges)
+                                     (mt/normal-drivers-with-feature :schemas))
     (testing "`TablePrivileges` should store the correct data for current_user and role privileges for databases with schemas"
       (mt/with-empty-db
         (let [conn-spec (sql-jdbc.conn/db->pooled-connection-spec (mt/db))]
