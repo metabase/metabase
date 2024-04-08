f85e17245acfc55ded752edbf7a683c94f6eac6e
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2024-04-08 11:19:09 +0200

    Make notebook native preview sidebar resizable (#40984)
    * Display sql preview on sizes less than 1280px
    
    * Handle min notebook width using Mantine responsitve style prop
    
    * Display the resizable sidebar on sizes >= 1280px
    
    * Set sidebar width on resize
    
    * Add custom handle
    
    * Simplify window resizing with `useWindowSize` hook
    
    * Use "white" from the theme colors
    
    * Improve sidebar semantics with `aside`
    
    * Define the `NativeQueryPreviewSidebar` as `aside`
    
    * Use Mantine style props instead of CSS module
    
    * Run the E2E test again using regular browser
    
    We removed the nested media query that ReplayIO doesn't support.
    Now this test can run using Replay Chromium as all other tests do.
    
    * Add some breathing room between the scrollbar and the resizable sidebar

--- e2e/test/scenarios/question/notebook-native-preview-sidebar.cy.spec.ts
+++ e2e/test/scenarios/question/notebook-native-preview-sidebar.cy.spec.ts
@@ -53,54 +53,48 @@ describe("scenarios > question > notebook > native query preview sidebar", () =>
     });
   });
 
-  it(
-    "smoke test: should show the preview sidebar, update it, persist it and close it",
-    // Replay Chromium does not respect a media query for the sidebar, which crashes this test.
-    // We have to use a different browser until that upstream bug is fixed.
-    { browser: "firefox" },
-    () => {
-      const defaultRowLimit = 1048575;
-      const queryLimit = 2;
+  it("smoke test: should show the preview sidebar, update it, persist it and close it", () => {
+    const defaultRowLimit = 1048575;
+    const queryLimit = 2;
 
-      cy.intercept("POST", "/api/dataset/native").as("nativeDataset");
+    cy.intercept("POST", "/api/dataset/native").as("nativeDataset");
 
-      openReviewsTable({ mode: "notebook", limit: queryLimit });
-      cy.findByLabelText("View the SQL").click();
-      cy.wait("@nativeDataset");
-      cy.findByTestId("native-query-preview-sidebar").within(() => {
-        cy.findByText("SQL for this question").should("exist");
-        cy.get(".ace_content")
-          .should("contain", "SELECT")
-          .and("contain", queryLimit);
-        cy.button("Convert this question to SQL").should("exist");
-      });
-
-      cy.log(
-        "Sidebar state should be persisted when navigating away from the notebook",
-      );
-      visualize();
-      cy.findAllByTestId("header-cell").should("contain", "Rating");
-      cy.findByTestId("native-query-preview-sidebar")
-        .should("exist")
-        .and("not.be.visible");
-
-      openNotebook();
-      cy.findByTestId("native-query-preview-sidebar").should("be.visible");
-
-      cy.log("Modifying GUI query should update the SQL preview");
-      cy.findByTestId("step-limit-0-0").icon("close").click({ force: true });
-      cy.wait("@nativeDataset");
-      cy.findByTestId("native-query-preview-sidebar")
-        .get(".ace_content")
+    openReviewsTable({ mode: "notebook", limit: queryLimit });
+    cy.findByLabelText("View the SQL").click();
+    cy.wait("@nativeDataset");
+    cy.findByTestId("native-query-preview-sidebar").within(() => {
+      cy.findByText("SQL for this question").should("exist");
+      cy.get(".ace_content")
         .should("contain", "SELECT")
-        .and("contain", defaultRowLimit)
-        .and("not.contain", queryLimit);
+        .and("contain", queryLimit);
+      cy.button("Convert this question to SQL").should("exist");
+    });
 
-      cy.log("It should be possible to close the sidebar");
-      cy.findByLabelText("Hide the SQL").click();
-      cy.findByTestId("native-query-preview-sidebar").should("not.exist");
-    },
-  );
+    cy.log(
+      "Sidebar state should be persisted when navigating away from the notebook",
+    );
+    visualize();
+    cy.findAllByTestId("header-cell").should("contain", "Rating");
+    cy.findByTestId("native-query-preview-sidebar")
+      .should("exist")
+      .and("not.be.visible");
+
+    openNotebook();
+    cy.findByTestId("native-query-preview-sidebar").should("be.visible");
+
+    cy.log("Modifying GUI query should update the SQL preview");
+    cy.findByTestId("step-limit-0-0").icon("close").click({ force: true });
+    cy.wait("@nativeDataset");
+    cy.findByTestId("native-query-preview-sidebar")
+      .get(".ace_content")
+      .should("contain", "SELECT")
+      .and("contain", defaultRowLimit)
+      .and("not.contain", queryLimit);
+
+    cy.log("It should be possible to close the sidebar");
+    cy.findByLabelText("Hide the SQL").click();
+    cy.findByTestId("native-query-preview-sidebar").should("not.exist");
+  });
 
   it("should not offer the sidebar preview for a user without native permissions", () => {
     cy.signIn("nosql");
