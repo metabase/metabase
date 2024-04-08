1ca5539dfd3e753535d057729e980662809292a6
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2024-04-08 16:06:22 -0400

    add ECharts e2e test helpers from PR #39919

--- e2e/support/helpers/e2e-visual-tests-helpers.js
+++ e2e/support/helpers/e2e-visual-tests-helpers.js
@@ -1,3 +1,20 @@
 export function ensureDcChartVisibility() {
   cy.get(".dc-chart");
 }
+
+export function echartsContainer() {
+  return cy.findByTestId("chart-container");
+}
+
+export function chartPathWithColor(color) {
+  return echartsContainer().find(`path[fill="${color}"]`);
+}
+
+export function chartPathsWithColors(colors) {
+  return colors.map(color => chartPathWithColor(color));
+}
+
+const CIRCLE_PATH = "M1 0A1 1 0 1 1 1 -0.0001";
+export function lineChartCircle() {
+  return echartsContainer().find(`path[d="${CIRCLE_PATH}"]`);
+}
