1a2d2ff053107affa40f7cf662e193c2051c779b
Ryan Laurie <iethree@gmail.com>
2024-04-10 14:35:19 +0100

    sweet sweet unit tests

--- frontend/src/metabase/common/components/QuestionPicker/components/QuestionPicker.unit.spec.tsx
+++ frontend/src/metabase/common/components/QuestionPicker/components/QuestionPicker.unit.spec.tsx
@@ -1,111 +1,141 @@
+import userEvent from "@testing-library/user-event";
 import fetchMock from "fetch-mock";
+import _ from "underscore";
 
 import { setupCollectionItemsEndpoint } from "__support__/server-mocks";
 import {
-  act,
   mockGetBoundingClientRect,
   mockScrollBy,
   renderWithProviders,
   screen,
+  waitFor,
 } from "__support__/ui";
-import type { CollectionId } from "metabase-types/api";
+import type { CollectionId, CollectionItem } from "metabase-types/api";
 import {
+  createMockCard,
   createMockCollection,
   createMockCollectionItem,
 } from "metabase-types/api/mocks";
 
-import type { QuestionPickerItem } from "../types";
+import type { QuestionPickerItem, QuestionPickerValueModel } from "../types";
 
-import { QuestionPicker } from "./QuestionPicker";
+import { QuestionPicker, defaultOptions } from "./QuestionPicker";
+import { QuestionPickerModal } from "./QuestionPickerModal";
 
-type MockCollection = {
-  id: CollectionId;
-  name: string;
-  location: string | null;
-  is_personal: boolean;
-  collections: MockCollection[];
+type NestedCollectionItem = Partial<CollectionItem> & {
+  id: any;
+  is_personal?: boolean;
+  descendants: NestedCollectionItem[];
 };
 
-const collectionTree: MockCollection[] = [
+const myQuestion = createMockCard({
+  id: 100,
+  name: "My Question",
+  collection_id: 3,
+});
+
+const myModel = createMockCard({
+  id: 101,
+  name: "My Model",
+  collection_id: 3,
+  type: "model",
+});
+
+const collectionTree: NestedCollectionItem[] = [
   {
-    id: "root",
+    id: "root" as any,
+    model: "collection",
     name: "Our Analytics",
-    location: null,
-    is_personal: false,
-    collections: [
+    location: "",
+    can_write: true,
+    descendants: [
       {
         id: 4,
         name: "Collection 4",
+        model: "collection",
         location: "/",
-        is_personal: false,
-        collections: [
+        can_write: true,
+        descendants: [
           {
             id: 3,
             name: "Collection 3",
-            collections: [],
+            model: "collection",
+            descendants: [
+              {
+                ...myQuestion,
+                model: "card",
+              } as unknown as NestedCollectionItem,
+              {
+                ...myModel,
+                model: "dataset",
+              } as unknown as NestedCollectionItem,
+            ],
             location: "/4/",
+            can_write: true,
             is_personal: false,
           },
         ],
       },
       {
         id: 2,
+        model: "collection",
         is_personal: false,
         name: "Collection 2",
         location: "/",
-        collections: [],
+        can_write: true,
+        descendants: [],
       },
     ],
   },
   {
     name: "My personal collection",
     id: 1,
+    model: "collection",
     location: "/",
     is_personal: true,
-    collections: [
+    can_write: true,
+    descendants: [
       {
         id: 5,
+        model: "collection",
         location: "/1/",
         name: "personal sub_collection",
         is_personal: true,
-        collections: [],
+        can_write: true,
+        descendants: [],
       },
     ],
   },
 ];
 
 const flattenCollectionTree = (
-  node: MockCollection[],
-): Omit<MockCollection, "collections">[] => {
-  return [
-    ...node.map(n => ({
-      name: n.name,
-      id: n.id,
-      is_personal: !!n.is_personal,
-      location: n.location,
-    })),
-  ].concat(...node.map(n => flattenCollectionTree(n.collections)));
+  node: NestedCollectionItem[],
+): Omit<NestedCollectionItem, "descendants">[] => {
+  if (!node) {
+    return [];
+  }
+  return [...node.map(n => _.omit(n, "descendants"))].concat(
+    ...node.map(n => flattenCollectionTree(n.descendants)),
+  );
 };
 
-const walkForCollectionItems = (node: MockCollection[]) => {
+const walkForCollectionItems = (node: NestedCollectionItem[]) => {
   node.forEach(n => {
-    const collectionItems = n.collections.map((c: MockCollection) =>
-      createMockCollectionItem({
-        id: c.id as number,
-        name: c.name,
-        model: "collection",
-        location: c.location || "/",
-      }),
+    if (!n.descendants) {
+      return;
+    }
+    const collectionItems = n.descendants.map((c: NestedCollectionItem) =>
+      createMockCollectionItem(c),
     );
 
     setupCollectionItemsEndpoint({
       collection: createMockCollection({ id: n.id }),
       collectionItems,
-      models: ["collection"],
+      models: ["collection", "dataset", "card"],
     });
 
     if (collectionItems.length > 0) {
-      walkForCollectionItems(n.collections);
+      walkForCollectionItems(n.descendants);
     }
   });
 };
@@ -113,48 +143,81 @@ const walkForCollectionItems = (node: MockCollection[]) => {
 interface SetupOpts {
   initialValue?: {
     id: CollectionId;
-    model: "collection";
+    model: "collection" | "card" | "dataset";
   };
-  onItemSelect?: (item: QuestionPickerItem) => void;
+  onChange?: (item: QuestionPickerItem) => void;
+  models?: [QuestionPickerValueModel, ...QuestionPickerValueModel[]];
 }
 
-const setup = ({
-  initialValue = { id: "root", model: "collection" },
-  onItemSelect = jest.fn<void, [QuestionPickerItem]>(),
-}: SetupOpts = {}) => {
+const commonSetup = () => {
   mockGetBoundingClientRect();
   mockScrollBy();
 
-  const allCollections =
-    flattenCollectionTree(collectionTree).map(createMockCollection);
+  const allItems = flattenCollectionTree(collectionTree).map(
+    createMockCollectionItem,
+  );
 
-  //Setup individual collection mocks
-  allCollections.forEach(collection => {
-    fetchMock.get(`path:/api/collection/${collection.id}`, collection);
+  allItems.forEach(item => {
+    if (item.model !== "collection") {
+      fetchMock.get(`path:/api/card/${item.id}`, item);
+    } else {
+      fetchMock.get(`path:/api/collection/${item.id}`, item);
+    }
   });
 
-  //Setup collection items mocks
-
+  // Setup collection items mocks
   walkForCollectionItems(collectionTree);
+};
 
-  return renderWithProviders(
+const setupPicker = async ({
+  initialValue = { id: "root", model: "collection" },
+  onChange = jest.fn<void, [QuestionPickerItem]>(),
+}: SetupOpts = {}) => {
+  commonSetup();
+
+  renderWithProviders(
     <QuestionPicker
-      onItemSelect={onItemSelect}
+      onItemSelect={onChange}
       initialValue={initialValue}
-      options={{}}
+      models={["card"]}
+      options={defaultOptions}
     />,
+  );
+
+  await waitFor(() =>
+    expect(screen.queryByTestId("loading-spinner")).not.toBeInTheDocument(),
   );
 };
 
-describe("CollectionPicker", () => {
+const setupModal = async ({
+  initialValue,
+  models = ["card", "dataset"],
+  onChange = jest.fn<void, [QuestionPickerItem]>(),
+}: SetupOpts = {}) => {
+  commonSetup();
+
+  renderWithProviders(
+    <QuestionPickerModal
+      onChange={onChange}
+      value={initialValue}
+      onClose={jest.fn()}
+      models={models}
+    />,
+  );
+
+  await waitFor(() =>
+    expect(screen.queryByTestId("loading-spinner")).not.toBeInTheDocument(),
+  );
+};
+
+describe("QuestionPicker", () => {
   afterAll(() => {
     jest.restoreAllMocks();
   });
 
   it("should select the root collection by default", async () => {
-    act(() => {
-      setup();
-    });
+    await setupPicker();
+
     expect(
       await screen.findByRole("button", { name: /Our Analytics/ }),
     ).toHaveAttribute("data-active", "true");
@@ -168,10 +231,24 @@ describe("CollectionPicker", () => {
     ).toBeInTheDocument();
   });
 
-  it("should render the path to the value provided", async () => {
-    act(() => {
-      setup({ initialValue: { id: 3, model: "collection" } });
-    });
+  it("should render the path to the collection provided", async () => {
+    await setupPicker({ initialValue: { id: 3, model: "collection" } });
+    expect(
+      await screen.findByRole("button", { name: /Our Analytics/ }),
+    ).toHaveAttribute("data-active", "true");
+
+    expect(
+      await screen.findByRole("button", { name: /Collection 4/ }),
+    ).toHaveAttribute("data-active", "true");
+
+    expect(
+      await screen.findByRole("button", { name: /Collection 3/ }),
+    ).toHaveAttribute("data-active", "true");
+  });
+
+  it("should render the path to the question provided", async () => {
+    await setupPicker({ initialValue: { id: 100, model: "card" } });
+
     expect(
       await screen.findByRole("button", { name: /Our Analytics/ }),
     ).toHaveAttribute("data-active", "true");
@@ -184,19 +261,135 @@ describe("CollectionPicker", () => {
       await screen.findByRole("button", { name: /Collection 3/ }),
     ).toHaveAttribute("data-active", "true");
 
-    expect(await screen.findByLabelText("empty")).toBeInTheDocument();
+    // question itself should start selected
+    expect(
+      await screen.findByRole("button", { name: /My Question/ }),
+    ).toHaveAttribute("data-active", "true");
+  });
+});
+
+describe("QuestionPickerModal", () => {
+  afterAll(() => {
+    jest.restoreAllMocks();
   });
 
-  it("should render the path back to personal collection", async () => {
-    act(() => {
-      setup({ initialValue: { id: 5, model: "collection" } });
-    });
-    expect(
-      await screen.findByRole("button", { name: /My personal collection/ }),
-    ).toHaveAttribute("data-active", "true");
+  it("should render the modal", async () => {
+    await setupModal();
 
     expect(
-      await screen.findByRole("button", { name: /personal sub_collection/ }),
+      await screen.findByText(/choose a question or model/i),
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByRole("button", { name: /Our Analytics/ }),
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByRole("button", { name: /Select/ }),
+    ).toBeInTheDocument();
+  });
+
+  it("should render model and question tabs by default", async () => {
+    await setupModal();
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByRole("tab", { name: /Models/ }),
+    ).toBeInTheDocument();
+  });
+
+  it("can render a single tab (which hides the tab bar)", async () => {
+    await setupModal({ models: ["dataset"] });
+
+    expect(
+      await screen.findByText(/choose a question or model/i),
+    ).toBeInTheDocument();
+    expect(screen.queryByRole("tablist")).not.toBeInTheDocument();
+    expect(screen.queryByRole("tab")).not.toBeInTheDocument();
+  });
+
+  it("should auto-select the question tab when a question is selected", async () => {
+    await setupModal({
+      initialValue: { id: 100, model: "card" },
+    });
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toHaveAttribute("aria-selected", "true");
+    expect(await screen.findByRole("tab", { name: /Models/ })).toHaveAttribute(
+      "aria-selected",
+      "false",
+    );
+
+    expect(
+      await screen.findByRole("button", { name: /My Question/ }),
     ).toHaveAttribute("data-active", "true");
+  });
+
+  it("should auto-select the model tab when a model is selected", async () => {
+    await setupModal({
+      initialValue: { id: 101, model: "dataset" },
+    });
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toHaveAttribute("aria-selected", "false");
+    expect(await screen.findByRole("tab", { name: /Models/ })).toHaveAttribute(
+      "aria-selected",
+      "true",
+    );
+
+    expect(
+      await screen.findByRole("button", { name: /My Model/ }),
+    ).toHaveAttribute("data-active", "true");
+  });
+
+  it("should automatically switch to the search tab when a search query is provided", async () => {
+    await setupModal();
+
+    const searchInput = await screen.findByPlaceholderText(/search/i);
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toHaveAttribute("aria-selected", "true");
+
+    await userEvent.type(searchInput, "sizzlipede");
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toHaveAttribute("aria-selected", "false");
+    expect(await screen.findByRole("tab", { name: /Search/ })).toHaveAttribute(
+      "aria-selected",
+      "true",
+    );
+  });
+
+  it("should switch back to the default tab when the search query is cleared", async () => {
+    await setupModal();
+
+    const searchInput = await screen.findByPlaceholderText(/search/i);
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toHaveAttribute("aria-selected", "true");
+
+    await userEvent.type(searchInput, "sizzlipede");
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toHaveAttribute("aria-selected", "false");
+    expect(await screen.findByRole("tab", { name: /Search/ })).toHaveAttribute(
+      "aria-selected",
+      "true",
+    );
+
+    await userEvent.clear(searchInput);
+
+    expect(
+      await screen.findByRole("tab", { name: /Questions/ }),
+    ).toHaveAttribute("aria-selected", "true");
+    expect(
+      screen.queryByRole("tab", { name: /Search/ }),
+    ).not.toBeInTheDocument();
   });
 });
