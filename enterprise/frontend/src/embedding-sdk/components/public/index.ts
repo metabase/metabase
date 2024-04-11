3e656da0e35d7339b773ca8366319a0175a8ba9c
Oisin Coveney <oisin@metabase.com>
2024-04-11 15:44:43 +0300

    Add eslint-disable-line import/no-duplicates for ee imports

--- enterprise/frontend/src/embedding-sdk/components/public/index.ts
+++ enterprise/frontend/src/embedding-sdk/components/public/index.ts
@@ -1,5 +1,5 @@
-import "ee-overrides";
-import "ee-plugins";
+import "ee-overrides"; // eslint-disable-line import/no-duplicates
+import "ee-plugins"; // eslint-disable-line import/no-duplicates
 
 export { QueryVisualizationSdk } from "./QueryVisualization";
 export { MetabaseProvider } from "./MetabaseProvider";
