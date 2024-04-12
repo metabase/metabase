9130b4e27f2f7c8eabb482f16ab36e2e86618275
Oisin Coveney <oisin@metabase.com>
2024-04-12 12:21:28 +0300

    Convert Formula to TS

---
+++ frontend/src/metabase/reference/components/Formula.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+import cx from "classnames";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { t } from "ttag";
+
+import { QueryDefinition } from "metabase/admin/datamodel/components/QueryDefinition";
+import { Icon } from "metabase/ui";
+import type { Metric, Segment } from "metabase-types/api";
+
+import S from "./Formula.module.css";
+
+export const Formula = ({
+  type,
+  entity,
+  isExpanded,
+  expandFormula,
+  collapseFormula,
+}: {
+  type: "segment" | "metric";
+  entity: Segment | Metric;
+  isExpanded: boolean;
+  expandFormula: () => void;
+  collapseFormula: () => void;
+}) => (
+  <div
+    className={cx(S.formula)}
+    onClick={isExpanded ? collapseFormula : expandFormula}
+  >
+    <div className={S.formulaHeader}>
+      <Icon name="beaker" size={24} />
+      <span className={S.formulaTitle}>{t`View the ${type} formula`}</span>
+    </div>
+    <TransitionGroup>
+      {isExpanded && (
+        <CSSTransition
+          key="formulaDefinition"
+          classNames="formulaDefinition"
+          timeout={{
+            enter: 300,
+            exit: 300,
+          }}
+        >
+          <div className="formulaDefinition">
+            <QueryDefinition
+              className={S.formulaDefinitionInner}
+              object={entity}
+            />
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
+  </div>
+);
