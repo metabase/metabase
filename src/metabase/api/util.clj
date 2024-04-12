ae25f459a1584234a9493b2dbc0b0bea819d399b
Nicolò Pretto <info@npretto.com>
2024-04-12 11:31:30 +0200

    product-feedback endpoint (#40974)
    * mvp of the proxy endpoint for the product feedback
    
    * cleanup and fixes
    
    * email :string -> ms/NonBlankString
    
    * revert my solution
    
    * Dan diff file
    
    * makes fields other than source optional
    
    * don't use default value in dev, to not spam the prod endpoint
    
    * change from setting to function for product-feedback-url, as it's not meant to be changed by end users

--- src/metabase/api/util.clj
+++ src/metabase/api/util.clj
@@ -2,14 +2,20 @@
   "Random utilty endpoints for things that don't belong anywhere else in particular, e.g. endpoints for certain admin
   page tasks."
   (:require
+   [cheshire.core :as json]
+   [clj-http.client :as http]
    [compojure.core :refer [GET POST]]
    [crypto.random :as crypto-random]
+   [environ.core :refer [env]]
    [metabase.analytics.prometheus :as prometheus]
    [metabase.analytics.stats :as stats]
    [metabase.api.common :as api]
    [metabase.api.common.validation :as validation]
+   [metabase.config :as config]
    [metabase.logger :as logger]
    [metabase.troubleshooting :as troubleshooting]
+   [metabase.util.log :as log]
+   [metabase.util.malli :as mu]
    [metabase.util.malli.schema :as ms]
    [ring.util.response :as response]))
 
@@ -37,6 +43,37 @@
    Intended for use when creating a value for `embedding-secret-key`."
   []
   {:token (crypto-random/hex 32)})
+
+(defn- product-feedback-url
+  "Product feedback url. When not prod, reads `MB_PRODUCT_FEEDBACK_URL` from the environment to prevent development
+  feedback from hitting the endpoint."
+  []
+  (if config/is-prod?
+    "https://store-api.metabase.com/api/v1/crm/product-feedback"
+    (env :mb-product-feedback-url)))
+
+(mu/defn send-feedback!
+  "Sends the feedback to the api endpoint"
+  [comments :- [:maybe ms/NonBlankString]
+   source :- ms/NonBlankString
+   email :- [:maybe ms/NonBlankString]]
+  (try (http/post (product-feedback-url)
+                  {:content-type :json
+                   :body         (json/generate-string {:comments comments
+                                                        :source   source
+                                                        :email    email})})
+       (catch Exception e
+         (log/warn e)
+         (throw e))))
+
+(api/defendpoint POST "/product-feedback"
+  "Endpoint to provide feedback from the product"
+  [:as {{:keys [comments source email]} :body}]
+  {comments [:maybe ms/NonBlankString]
+   source ms/NonBlankString
+   email [:maybe ms/NonBlankString]}
+  (future (send-feedback! comments source email))
+  api/generic-204-no-content)
 
 (api/defendpoint GET "/bug_report_details"
   "Returns version and system information relevant to filing a bug report against Metabase."
