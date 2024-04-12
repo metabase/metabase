afff3c5fa8098e3b809462e14c769a33604d2892
Oisin Coveney <oisin@metabase.com>
2024-04-12 11:28:02 +0300

    Fix lint error

--- enterprise/frontend/src/embedding-sdk/components/public/MetabaseProvider.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/MetabaseProvider.tsx
@@ -1,5 +1,5 @@
-import type { ReactNode } from "react";
-import { memo, JSX } from "react";
+import type { ReactNode, JSX } from "react";
+import { memo } from "react";
 import { Provider } from "react-redux";
 
 import { AppInitializeController } from "embedding-sdk/components/private/AppInitializeController";
