bde86a7306928ce0642b24fb21ead5e2a8f23026
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-04-15 23:20:14 +0200

    Throw for unused Tasks endpoints

--- frontend/src/metabase/entities/tasks.js
+++ frontend/src/metabase/entities/tasks.js
@@ -6,4 +6,16 @@ import { createEntity } from "metabase/lib/entities";
 export default createEntity({
   name: "tasks",
   path: "/api/task",
+
+  api: {
+    create: () => {
+      throw new TypeError("Tasks.api.create is not supported");
+    },
+    update: () => {
+      throw new TypeError("Tasks.api.update is not supported");
+    },
+    delete: () => {
+      throw new TypeError("Tasks.api.delete is not supported");
+    },
+  },
 });
