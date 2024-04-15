a0beab004d0c585f0e8dde868cb72cd320bf3d5b
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-04-15 23:34:47 +0200

    RTK Query `taskApi`

---
+++ frontend/src/metabase/api/task.ts
@@ -0,0 +1,27 @@
+import { Api } from "./api";
+
+export const taskApi = Api.injectEndpoints({
+  endpoints: builder => ({
+    listTasks: builder.query<unknown, void>({
+      query: () => ({
+        method: "GET",
+        url: "/api/task",
+      }),
+    }),
+    getTask: builder.query<unknown, number>({
+      query: id => ({
+        method: "GET",
+        url: `/api/task/${id}}`,
+      }),
+    }),
+    getTasksInfo: builder.query<unknown, void>({
+      query: () => ({
+        method: "GET",
+        url: "/api/task/info",
+      }),
+    }),
+  }),
+});
+
+export const { useListTasksQuery, useGetTaskQuery, useGetTasksInfoQuery } =
+  taskApi;
