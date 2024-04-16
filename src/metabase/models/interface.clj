ca80c305e6eae87dfb93ead4aa83de69d3db434a
Ngoc Khuat <qn.khuat@gmail.com>
2024-04-16 13:38:21 +0700

    missing mu

--- src/metabase/models/interface.clj
+++ src/metabase/models/interface.clj
@@ -19,6 +19,7 @@
    [metabase.util.encryption :as encryption]
    [metabase.util.i18n :refer [tru]]
    [metabase.util.log :as log]
+   [metabase.util.malli :as mu]
    [metabase.util.malli.registry :as mr]
    [methodical.core :as methodical]
    [potemkin :as p]
