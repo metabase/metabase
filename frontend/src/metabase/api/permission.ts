56a0e110c175a6510443c76df4ed7587722431b0
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-04-16 20:08:57 +0200

    Define RTK Query 5 main endpoints for `permissionApi` (group)

---
+++ frontend/src/metabase/api/permission.ts
@@ -0,0 +1,64 @@
+import type { GroupId, Group } from "metabase-types/api";
+
+import { Api } from "./api";
+import {
+  idTag,
+  listTag,
+  invalidateTags,
+  providePermissionsGroupListTags,
+  providePermissionsGroupTags,
+} from "./tags";
+
+export const permissionApi = Api.injectEndpoints({
+  endpoints: builder => ({
+    listPermissionsGroups: builder.query<Group[], void>({
+      query: body => ({
+        method: "GET",
+        url: "/api/permissions/group",
+        body,
+      }),
+      providesTags: (groups = []) => providePermissionsGroupListTags(groups),
+    }),
+    getPermissionsGroup: builder.query<Group, GroupId>({
+      query: id => ({
+        method: "GET",
+        url: `/api/permissions/group/${id}`,
+      }),
+      providesTags: group => (group ? providePermissionsGroupTags(group) : []),
+    }),
+    createPermissionsGroup: builder.mutation<unknown, unknown>({
+      query: body => ({
+        method: "POST",
+        url: "/api/permissions/group",
+        body,
+      }),
+      invalidatesTags: (_, error) =>
+        invalidateTags(error, [listTag("permissions-group")]),
+    }),
+    updatePermissionsGroup: builder.mutation<unknown, any>({
+      query: ({ id, ...body }) => ({
+        method: "PUT",
+        url: `/api/permissions/group/${id}`,
+        body,
+      }),
+      invalidatesTags: (_, error, { id }) =>
+        invalidateTags(error, [idTag("permissions-group", id)]),
+    }),
+    deletePermissionsGroup: builder.mutation<unknown, GroupId>({
+      query: id => ({
+        method: "DELETE",
+        url: `/api/permissions/group/${id}`,
+      }),
+      invalidatesTags: (_, error, id) =>
+        invalidateTags(error, [idTag("permissions-group", id)]),
+    }),
+  }),
+});
+
+export const {
+  useListPermissionsGroupsQuery,
+  useGetPermissionsGroupQuery,
+  useCreatePermissionsGroupMutation,
+  useUpdatePermissionsGroupMutation,
+  useDeletePermissionsGroupMutation,
+} = permissionApi;
