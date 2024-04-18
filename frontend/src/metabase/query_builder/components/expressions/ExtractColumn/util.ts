23823b0a8d4649569633dd33e1f017f2423e6679
Romeo Van Snick <romeo@romeovansnick.be>
2024-04-18 16:05:12 +0200

    Add examples for column extraction

--- frontend/src/metabase/query_builder/components/expressions/ExtractColumn/util.ts
+++ frontend/src/metabase/query_builder/components/expressions/ExtractColumn/util.ts
@@ -1,11 +1,29 @@
-import * as Lib from "metabase-lib";
+import type * as Lib from "metabase-lib";
 
-// TODO: use Lib.displayInfo instead
-export function mock_displayInfo(
-  query: Lib.Query,
-  stageIndex: number,
-  extraction: Lib.ColumnExtraction,
-) {
-  /// HERE
-  return {};
+export function getExample(extraction: Lib.ColumnExtraction) {
+  // TODO: this should eventually be moved into Lib.displayInfo
+  // to avoid the keys going out of sync with the MLv2-defined extractions.
+  const tag = extraction.tag as unknown as string;
+  switch (tag) {
+    case "hour-of-day":
+      return "0, 1";
+    case "day-of-month":
+      return "1, 2";
+    case "day-of-week":
+      return "Monday, Tuesday";
+    case "month-of-year":
+      return "Jan, Feb";
+    case "quarter-of-year":
+      return "Q1, Q2";
+    case "year":
+      return "2023, 2024";
+    case "domain":
+      return "example.com, online.com";
+    case "host":
+      return "example, online";
+    case "subdomain":
+      return "www, maps";
+  }
+
+  return undefined;
 }
