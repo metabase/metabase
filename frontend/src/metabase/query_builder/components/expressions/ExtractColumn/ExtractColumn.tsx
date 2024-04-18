bfd45b50b0eb7e93f147976679b3a3b689c5aab9
Romeo Van Snick <romeo@romeovansnick.be>
2024-04-18 16:08:14 +0200

    Use Lib.columnExtractions to create extraction picker

--- frontend/src/metabase/query_builder/components/expressions/ExtractColumn/ExtractColumn.tsx
+++ frontend/src/metabase/query_builder/components/expressions/ExtractColumn/ExtractColumn.tsx
@@ -2,13 +2,13 @@ import { useState, useMemo } from "react";
 import { t } from "ttag";
 
 import { QueryColumnPicker } from "metabase/common/components/QueryColumnPicker";
-import { Box } from "metabase/ui";
+import { Box, Stack } from "metabase/ui";
 import * as Lib from "metabase-lib";
 
 import { ExpressionWidgetHeader } from "../ExpressionWidgetHeader";
 
-import { ExtractDateTime } from "./ExtractDateTime";
-import { mock_displayInfo } from "./util";
+import { Button } from "./Button";
+import { getExample } from "./util";
 
 type Props = {
   query: Lib.Query;
@@ -20,94 +20,123 @@ type Props = {
 export function ExtractColumn({ query, stageIndex, onCancel }: Props) {
   const [column, setColumn] = useState<Lib.ColumnMetadata | null>(null);
 
-  function handleSubmit() {
-    // TODO
-  }
-
   function handleSelect(column: Lib.ColumnMetadata) {
     setColumn(column);
   }
 
-  const extractableColumns = useMemo(() => {
-    const columns = Lib.expressionableColumns(query, stageIndex);
-    return columns
-      .map(column => {
-        const extractions = Lib.columnExtractions(query, column);
-        return {
-          column,
-          extractions: extractions.map(extraction =>
-            mock_displayInfo(query, stageIndex, extraction),
-          ),
-        };
-      })
-      .filter(info => info.extractions.length > 0);
-  }, [query, stageIndex]);
-
   if (!column) {
-    const columnGroups = Lib.groupColumns(
-      extractableColumns.map(x => x.column),
-    );
-
     return (
-      <>
-        <ExpressionWidgetHeader
-          title={t`Select column to extract from`}
-          onBack={onCancel}
-        />
-        <Box py="sm">
-          <QueryColumnPicker
-            query={query}
-            stageIndex={stageIndex}
-            columnGroups={columnGroups}
-            onSelect={handleSelect}
-            checkIsColumnSelected={item => item.column === column}
-            width="100%"
-            alwaysExpanded
-            disableSearch
-          />
-        </Box>
-      </>
+      <ColumnPicker
+        query={query}
+        stageIndex={stageIndex}
+        column={column}
+        onCancel={onCancel}
+        onSelect={handleSelect}
+      />
     );
   }
 
+  function handleSubmit(_extraction: Lib.ColumnExtraction) {
+    // TODO
+  }
+
+  return (
+    <ExtractionPicker
+      query={query}
+      stageIndex={stageIndex}
+      column={column}
+      onSelect={handleSubmit}
+      onCancel={() => setColumn(null)}
+    />
+  );
+}
+
+function ColumnPicker({
+  query,
+  stageIndex,
+  column,
+  onSelect,
+  onCancel,
+}: {
+  query: Lib.Query;
+  stageIndex: number;
+  column: Lib.ColumnMetadata | null;
+  onSelect: (column: Lib.ColumnMetadata) => void;
+  onCancel: () => void;
+}) {
+  const extractableColumns = useMemo(
+    () =>
+      Lib.expressionableColumns(query, stageIndex).filter(
+        column => Lib.columnExtractions(query, column).length > 0,
+      ),
+    [query, stageIndex],
+  );
+  const columnGroups = Lib.groupColumns(extractableColumns);
+
+  return (
+    <>
+      <ExpressionWidgetHeader
+        title={t`Select column to extract from`}
+        onBack={onCancel}
+      />
+      <Box py="sm">
+        <QueryColumnPicker
+          query={query}
+          stageIndex={stageIndex}
+          columnGroups={columnGroups}
+          onSelect={onSelect}
+          checkIsColumnSelected={item => item.column === column}
+          width="100%"
+          alwaysExpanded
+          disableSearch
+        />
+      </Box>
+    </>
+  );
+}
+
+function ExtractionPicker({
+  query,
+  stageIndex,
+  column,
+  onSelect,
+  onCancel,
+}: {
+  query: Lib.Query;
+  stageIndex: number;
+  column: Lib.ColumnMetadata;
+  onSelect: (extraction: Lib.ColumnExtraction) => void;
+  onCancel: () => void;
+}) {
   const info = Lib.displayInfo(query, stageIndex, column);
-  const Component = component(info);
+
+  const extractions = useMemo(
+    () =>
+      Lib.columnExtractions(query, column).map(extraction => ({
+        extraction,
+        info: Lib.displayInfo(query, stageIndex, extraction),
+      })),
+    [query, stageIndex, column],
+  );
 
   return (
     <>
       <ExpressionWidgetHeader
         title={t`Select part of '${info.longDisplayName}' to extract`}
-        onBack={() => setColumn(null)}
+        onBack={onCancel}
       />
-      <form onSubmit={handleSubmit}>
-        <Box p="sm">
-          <Component />
-        </Box>
-      </form>
+      <Box p="sm">
+        <Stack spacing={0}>
+          {extractions.map(extraction => (
+            <Button
+              key={extraction.info.key}
+              title={extraction.info.displayName}
+              example={getExample(extraction.info)}
+              onClick={() => onSelect(extraction.extraction)}
+            />
+          ))}
+        </Stack>
+      </Box>
     </>
   );
-}
-
-function component(info: Lib.ColumnDisplayInfo) {
-  if (info.semanticType === "type/Email") {
-    return ExtractEmail;
-  }
-
-  if (info.semanticType === "type/URL") {
-    return ExtractUrl;
-  }
-
-  if (info.effectiveType === "type/DateTime") {
-    return ExtractDateTime;
-  }
-
-  return null;
-}
-
-function ExtractEmail() {
-  return <div>Email</div>;
-}
-
-function ExtractUrl() {
-  return <div>Url</div>;
 }
