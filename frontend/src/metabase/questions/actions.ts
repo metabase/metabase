ab8a52a563c49e24d75ffbcc4674dac7f3c549b3
Kamil Mielnik <kamil@kamilmielnik.com>
2024-04-19 17:15:15 +0700

    Add naive implementation of loading metadata until dependentMetadata gives no more new results

--- frontend/src/metabase/questions/actions.ts
+++ frontend/src/metabase/questions/actions.ts
@@ -1,3 +1,5 @@
+import _ from "underscore";
+
 import { loadMetadataForDependentItems } from "metabase/redux/metadata";
 import { getMetadata } from "metabase/selectors/metadata";
 import * as Lib from "metabase-lib";
@@ -14,19 +16,57 @@ export const loadMetadataForCard =
   (card: Card, options?: LoadMetadataOptions) =>
   async (dispatch: Dispatch, getState: GetState) => {
     const question = new Question(card, getMetadata(getState()));
-    const loadAdhocMetadata =
-      question.isSaved() && question.type() !== "question";
-    const dependencies = [...Lib.dependentMetadata(question.query())];
-    if (loadAdhocMetadata) {
-      const tableId = getQuestionVirtualTableId(question.id());
-      dependencies.push({ id: tableId, type: "table" });
-    }
+    const dependencies = questionDependentMetadata(question);
+
     await dispatch(loadMetadataForDependentItems(dependencies, options));
 
-    if (loadAdhocMetadata) {
+    if (shouldLoadAdhocMetadata(question)) {
       const questionWithMetadata = new Question(card, getMetadata(getState()));
       const adhocQuestion = questionWithMetadata.composeQuestionAdhoc();
-      const adhocDependencies = Lib.dependentMetadata(adhocQuestion.query());
+      const adhocDependencies = questionDependentMetadata(adhocQuestion);
       await dispatch(loadMetadataForDependentItems(adhocDependencies, options));
+
+      const updatedQuestion = new Question(card, getMetadata(getState()));
+      const updatedAdHocQuestion = updatedQuestion.composeQuestionAdhoc();
+      const updatedDependencies =
+        questionDependentMetadata(updatedAdHocQuestion);
+
+      if (!areDependenciesEqual(adhocDependencies, updatedDependencies)) {
+        await dispatch(loadMetadataForCard(card, options));
+      }
+    } else {
+      const updatedQuestion = new Question(card, getMetadata(getState()));
+      const updatedDependencies = questionDependentMetadata(updatedQuestion);
+
+      if (!areDependenciesEqual(dependencies, updatedDependencies)) {
+        await dispatch(loadMetadataForCard(card, options));
+      }
     }
   };
+
+const shouldLoadAdhocMetadata = (question: Question): boolean => {
+  return question.isSaved() && question.type() !== "question";
+};
+
+const questionDependentMetadata = (question: Question): Lib.DependentItem[] => {
+  const dependencies = [...Lib.dependentMetadata(question.query())];
+
+  if (shouldLoadAdhocMetadata(question)) {
+    const tableId = getQuestionVirtualTableId(question.id());
+    dependencies.push({ id: tableId, type: "table" });
+  }
+
+  return dependencies;
+};
+
+const areDependenciesEqual = (
+  items1: Lib.DependentItem[],
+  items2: Lib.DependentItem[],
+): boolean => {
+  const stringifyItem = ({ id, type }: Lib.DependentItem) => `${id}-${type}`;
+  const difference = _.difference(
+    items1.map(stringifyItem),
+    items2.map(stringifyItem),
+  );
+  return difference.length === 0;
+};
