38da6996e7d4c6dd4dab8d43da9d296fa52557e8
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-04-22 13:25:43 +0200

    Add boilerplate `perist` API

---
+++ frontend/src/metabase/api/persist.ts
@@ -0,0 +1,64 @@
+import type {
+  ListPersistedInfoResponse,
+  CardId,
+  ListPersistedInfoRequest,
+  ModelCacheRefreshStatus,
+  PersistedInfoId,
+} from "metabase-types/api";
+
+import { Api } from "./api";
+
+export const persistApi = Api.injectEndpoints({
+  endpoints: builder => ({
+    listPersistedInfo: builder.query<
+      ListPersistedInfoResponse,
+      ListPersistedInfoRequest | void
+    >({
+      query: params => ({
+        method: "GET",
+        url: "/api/persist",
+        params,
+      }),
+    }),
+    getPersistedInfo: builder.query<ModelCacheRefreshStatus, PersistedInfoId>({
+      query: id => ({
+        method: "GET",
+        url: `/api/persist/${id}`,
+      }),
+    }),
+    getPersistedInfoByCard: builder.query<ModelCacheRefreshStatus, CardId>({
+      query: id => ({
+        method: "GET",
+        url: `/api/persist/card/${id}`,
+      }),
+    }),
+    enablePersist: builder.query<unknown, void>({
+      query: () => ({
+        method: "POST",
+        url: "/api/persist/enable",
+      }),
+    }),
+    disablePersist: builder.query<unknown, void>({
+      query: () => ({
+        method: "POST",
+        url: "/api/persist/disable",
+      }),
+    }),
+    setRefreshSchedule: builder.mutation<unknown, unknown>({
+      query: body => ({
+        method: "POST",
+        url: "/api/persist/set-refresh/schedule",
+        body,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useListPersistedInfoQuery,
+  useGetPersistedInfoQuery,
+  useGetPersistedInfoByCardQuery,
+  useEnablePersistQuery,
+  useDisablePersistQuery,
+  useSetRefreshScheduleMutation,
+} = persistApi;
