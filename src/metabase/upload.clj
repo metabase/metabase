208bdc9a1e459459b8574acfdd1766b7ca12e0db
Chris Truter <chris@metabase.com>
2024-04-23 15:01:59 +0200

    Slightly more direct

--- src/metabase/upload.clj
+++ src/metabase/upload.clj
@@ -521,9 +521,8 @@
   "Returns true if `query` has no joins in it, otherwise false."
   [query]
   (->> (range (lib/stage-count query))
-       (mapcat (fn [stage]
-                 (lib/joins query stage)))
-       (empty?)))
+       (every? (fn [stage]
+                 (empty? (lib/joins query stage))))))
 
 (defn- only-table-id
   "For models that depend on only one table, return its id, otherwise return nil. Doesn't support native queries."
