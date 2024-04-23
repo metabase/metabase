d5f4222dc353eac7269c2b820cc19a9f5963ce1e
Case Nelson <case@metabase.com>
2024-04-23 09:10:26 -0600

    [Metrics v2] Query processor joining metrics

--- src/metabase/query_processor/middleware/metrics.clj
+++ src/metabase/query_processor/middleware/metrics.clj
@@ -1,14 +1,29 @@
 (ns metabase.query-processor.middleware.metrics
   (:require
+   [medley.core :as m]
    [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
    [metabase.lib.util :as lib.util]
    [metabase.lib.util.match :as lib.util.match]
-   [metabase.lib.walk :as lib.walk]))
+   [metabase.lib.walk :as lib.walk]
+   [metabase.util :as u]))
 
 (defn- expression-with-name-from-source
   [query [_ {:lib/keys [expression-name]} :as expression]]
   (lib/expression query expression-name expression))
+
+(defn- replace-metric-aggregation-refs [aggregation metric-name lookup]
+  (lib.util.match/replace
+   aggregation
+    [:metric & (_ :guard (fn [[{:keys [join-alias]} metric-id]]
+                           (contains? lookup [join-alias metric-id])))]
+    (let [[_ {:keys [join-alias]} metric-id] &match]
+      (update (lib.util/fresh-uuids
+                (get lookup [join-alias metric-id]))
+              1
+              merge
+              {:name (u/slugify (or join-alias metric-name))}
+              (select-keys (get &match 1) [:lib/uuid :name])))))
 
 (defn expand
   "Expand `:source-card` of `:type` `:metric` into an expanded query combining this query with sources.
@@ -25,27 +40,40 @@
    ```
    "
   [query]
-  (lib.walk/walk-stages
-   query
-   (fn [_query _path {:keys [source-card joins filters aggregation expressions breakout order-by] :as stage}]
-     (let [source-metadata (some->> source-card (lib.metadata/card query))]
-       (if (= (:type source-metadata) :metric)
-         (let [source-query (expand (-> query
-                                        (lib/query (:dataset-query source-metadata))
-                                        lib.util/fresh-query-instance))
-               metric-aggregation (-> source-query lib/aggregations first)
-               new-aggregations (lib.util.match/replace aggregation
-                                  [:metric {} source-card]
-                                  (assoc-in (lib.util/fresh-uuids metric-aggregation)
-                                            [1 :lib/uuid]
-                                            (get-in &match [1 :lib/uuid])))]
-           (as-> source-query $q
-             (lib.util/update-query-stage $q -1 dissoc :aggregation :breakout :order-by :fields)
-             (reduce lib/join $q joins)
-             (reduce lib/filter $q filters)
-             (reduce lib/breakout $q breakout)
-             (reduce lib/aggregate $q new-aggregations)
-             (reduce expression-with-name-from-source $q expressions)
-             (reduce lib/order-by $q order-by)
-             (:stages $q)))
-         stage)))))
+  (let [metric-refs (volatile! {})
+        stage-metric (volatile! [])]
+    (lib.walk/walk
+      query
+      (fn [_query path-type _path stage-or-join]
+        (cond
+          (= path-type :lib.walk/join)
+          (let [[source-card aggregation] @stage-metric]
+            (vswap! metric-refs assoc [(:alias stage-or-join) source-card] aggregation)
+            (vreset! stage-metric [])
+            stage-or-join)
+
+          (= path-type :lib.walk/stage)
+          (let [{:keys [source-card filters joins aggregation expressions breakout order-by]} stage-or-join
+                source-metadata (some->> source-card (lib.metadata/card query))
+                metric-name (:name source-metadata)]
+            (if (= (:type source-metadata) :metric)
+              (let [source-query (expand (-> query
+                                             (lib/query (:dataset-query source-metadata))
+                                             lib.util/fresh-query-instance))
+                    metric-aggregation (-> source-query lib/aggregations first)
+                    _ (vreset! stage-metric [source-card metric-aggregation])
+                    new-aggregations (replace-metric-aggregation-refs
+                                       aggregation
+                                       metric-name
+                                       (assoc @metric-refs [nil source-card] metric-aggregation))]
+                (as-> source-query $q
+                  (lib.util/update-query-stage $q -1 dissoc :aggregation :breakout :order-by :fields)
+
+                  (reduce lib/join $q joins)
+                  (reduce lib/filter $q filters)
+                  (reduce lib/breakout $q breakout)
+                  (reduce lib/aggregate $q new-aggregations)
+                  (reduce expression-with-name-from-source $q expressions)
+                  (reduce lib/order-by $q order-by)
+                  (:stages $q)))
+              (m/update-existing stage-or-join :aggregation replace-metric-aggregation-refs metric-name @metric-refs))))))))
--- test/metabase/query_processor/middleware/metrics_test.clj
+++ test/metabase/query_processor/middleware/metrics_test.clj
@@ -1,6 +1,7 @@
 (ns metabase.query-processor.middleware.metrics-test
   (:require
    [clojure.test :refer [deftest is]]
+   [medley.core :as m]
    [metabase.lib.convert :as lib.convert]
    [metabase.lib.core :as lib]
    [metabase.lib.equality :as lib.equality]
@@ -11,13 +12,14 @@
    [metabase.lib.test-util :as lib.tu]
    [metabase.query-processor :as qp]
    [metabase.query-processor.middleware.metrics :as metrics]
-   [metabase.test :as mt]))
+   [metabase.test :as mt]
+   [metabase.util :as u]))
 
 (def counter (atom (rand-int 2000)))
 
 (defn- basic-metric-query []
   (-> (lib/query meta/metadata-provider (meta/table-metadata :products))
-      (lib/aggregate (lib/avg (meta/field-metadata :products :rating)))))
+      (lib/aggregate (update (lib/avg (meta/field-metadata :products :rating)) 1 assoc :name (u/slugify "Mock metric")))))
 
 (defn- mock-metric
   ([]
@@ -28,7 +30,7 @@
    (let [metric {:lib/type :metadata/card
                  :id (swap! counter inc)
                  :database-id (meta/id)
-                 :name "Mock metric - Avertage Product Rating"
+                 :name "Mock metric"
                  :type :metric
                  :dataset-query query}]
      [metric (lib/composed-metadata-provider
@@ -123,6 +125,18 @@
                                [:= {} [:field {} (meta/id :products :category)] "Widget"]]}]}
           (metrics/expand query)))))
 
+(deftest ^:parallel expand-joined-metric-test
+  (let [[source-metric mp] (mock-metric)
+        query (as-> (lib/query mp (meta/table-metadata :orders)) $q
+                (lib/join $q (lib/join-clause (lib.metadata/card mp (:id source-metric))
+                                              [(lib/= (meta/field-metadata :orders :product-id)
+                                                      (meta/field-metadata :products :id))]))
+                (lib/aggregate $q (lib.options/ensure-uuid [:metric {:join-alias "Mock metric - Product"} (:id source-metric)])))]
+    (is (=?
+          {:stages [{:joins [{:stages [{:source-table (meta/id :products)}]}]
+                     :aggregation [[:avg {} [:field {} (meta/id :products :rating)]]]}]}
+          (metrics/expand query)))))
+
 (deftest ^:parallel e2e-results-test
   (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))
         source-query (-> (lib/query mp (lib.metadata/table mp (mt/id :products)))
@@ -135,3 +149,61 @@
         (is (=
              (mt/rows (qp/process-query source-query))
              (mt/rows (qp/process-query query))))))))
+
+(deftest ^:parallel e2e-join-to-table-test
+  (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))
+        source-query (-> (lib/query mp (lib.metadata/table mp (mt/id :orders)))
+                         (lib/aggregate (lib/count)))
+        find-by-id (fn [id columns]
+                     (m/find-first (comp #{id} :id) columns))]
+    (mt/with-temp [:model/Card source-metric {:dataset_query (lib.convert/->legacy-MBQL source-query)
+                                              :database_id (mt/id)
+                                              :name "new_metric"
+                                              :type :metric}]
+
+      (let [source-metric-id (:id source-metric)
+            metric (lib.metadata/card mp source-metric-id)
+            join-alias (:name metric)
+            q (as-> (lib/query mp (lib.metadata/table mp (mt/id :products))) $q
+                (lib/join $q (lib/join-clause
+                               metric
+                               [(lib/= (lib/with-join-alias
+                                         (find-by-id (mt/id :orders :id) (lib/joinable-columns $q -1 metric))
+                                         join-alias)
+                                       (lib.metadata/field mp (mt/id :products :id)))]))
+                ;; TODO how to make new metric reference?
+                (lib/aggregate $q (lib.options/ensure-uuid [:metric {:base-type :type/Float :join-alias join-alias} source-metric-id]))
+                (lib/breakout $q (lib/with-temporal-bucket
+                                   (find-by-id (mt/id :orders :created_at) (lib/breakoutable-columns $q))
+                                   :month))
+                (lib/breakout $q (find-by-id (mt/id :products :vendor) (lib/breakoutable-columns $q)))
+                (lib/filter $q (lib/between (find-by-id (mt/id :orders :quantity) (lib/filterable-columns $q))
+                                            1 5))
+                (lib/append-stage $q)
+                (lib/filter $q (lib/= (find-by-id (mt/id :products :vendor) (lib/filterable-columns $q))
+                                      "Americo Sipes and Sons"
+                                      "Aufderhar-Boehm"
+                                      "Balistreri-Muller"))
+                (lib/filter $q (lib/between
+                                 (m/find-first (comp #{"new_metric"} :name) (lib/filterable-columns $q))
+                                 10 100)))]
+        (is (= [["2016-05-01T00:00:00Z" "Americo Sipes and Sons" 19]
+                ["2016-05-01T00:00:00Z" "Aufderhar-Boehm" 19]
+                ["2016-05-01T00:00:00Z" "Balistreri-Muller" 19]
+                ["2016-06-01T00:00:00Z" "Americo Sipes and Sons" 16]
+                ["2016-06-01T00:00:00Z" "Aufderhar-Boehm" 16]
+                ["2016-06-01T00:00:00Z" "Balistreri-Muller" 16]
+                ["2016-07-01T00:00:00Z" "Americo Sipes and Sons" 50]
+                ["2016-07-01T00:00:00Z" "Aufderhar-Boehm" 50]
+                ["2016-07-01T00:00:00Z" "Balistreri-Muller" 50]
+                ["2016-08-01T00:00:00Z" "Americo Sipes and Sons" 21]
+                ["2016-08-01T00:00:00Z" "Aufderhar-Boehm" 21]
+                ["2016-08-01T00:00:00Z" "Balistreri-Muller" 21]
+                ["2016-09-01T00:00:00Z" "Americo Sipes and Sons" 74]
+                ["2016-09-01T00:00:00Z" "Aufderhar-Boehm" 74]
+                ["2016-09-01T00:00:00Z" "Balistreri-Muller" 74]
+                ["2016-10-01T00:00:00Z" "Americo Sipes and Sons" 83]
+                ["2016-10-01T00:00:00Z" "Aufderhar-Boehm" 83]
+                ["2016-10-01T00:00:00Z" "Balistreri-Muller" 83]]
+               (mt/rows
+                 (qp/process-query q))))))))
