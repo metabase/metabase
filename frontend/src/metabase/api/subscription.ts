bd2b8e31a097ceb52e5d507e5653c726ec50997d
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-04-24 16:12:21 +0200

    Add types

--- frontend/src/metabase/api/subscription.ts
+++ frontend/src/metabase/api/subscription.ts
@@ -1,27 +1,44 @@
+import type {
+  ListSubscriptionsRequest,
+  DashboardSubscription,
+  CreateSubscriptionRequest,
+  UpdateSubscriptionRequest,
+} from "metabase-types/api";
+
 import { Api } from "./api";
 
 export const subscriptionApi = Api.injectEndpoints({
   endpoints: builder => ({
-    listSubscriptions: builder.query<unknown, unknown>({
-      query: () => ({
+    listSubscriptions: builder.query<
+      DashboardSubscription[],
+      ListSubscriptionsRequest
+    >({
+      query: parameters => ({
         method: "GET",
         url: "/api/pulse",
+        parameters,
       }),
     }),
-    getSubscription: builder.query<unknown, number>({
+    getSubscription: builder.query<DashboardSubscription, number>({
       query: id => ({
         method: "GET",
         url: `/api/pulse/${id}`,
       }),
     }),
-    createSubscription: builder.mutation<unknown, unknown>({
+    createSubscription: builder.mutation<
+      DashboardSubscription,
+      CreateSubscriptionRequest
+    >({
       query: body => ({
         method: "POST",
         url: "/api/pulse",
         body,
       }),
     }),
-    updateSubscription: builder.mutation<unknown, unknown>({
+    updateSubscription: builder.mutation<
+      DashboardSubscription,
+      UpdateSubscriptionRequest
+    >({
       query: ({ id, ...body }) => ({
         method: "PUT",
         url: `/api/pulse/${id}`,
