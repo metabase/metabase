03a518303a63b8106aa3fc68cf33d03a987db92b
Noah Moss <noahbmoss@gmail.com>
2024-04-24 16:27:33 -0400

    Revert "test"
    This reverts commit e3be0107c5c67f9c6892929463ff90d9df8cb812.

--- test/metabase/api/setup_test.clj
+++ test/metabase/api/setup_test.clj
@@ -7,6 +7,7 @@
    [metabase.analytics.snowplow-test :as snowplow-test]
    [metabase.api.setup :as api.setup]
    [metabase.config :as config]
+   [metabase.core :as mbc]
    [metabase.db :as mdb]
    [metabase.driver.h2 :as h2]
    [metabase.events :as events]
@@ -421,7 +422,7 @@
   (testing "Internally created state like Metabase Analytics shouldn't affect the checklist"
     (mt/with-temp-empty-app-db [_conn :h2]
       (mdb/setup-db! :create-sample-content? true)
-      #_(mbc/ensure-audit-db-installed!)
+      (mbc/ensure-audit-db-installed!)
       (testing "Sense check: internal content exists"
         (is (true? (t2/exists? :model/User)))
         (is (true? (t2/exists? :model/Database)))
