04a2045c6e22e30b84c59711360aeca428820b5e
Romeo Van Snick <romeo@romeovansnick.be>
2024-04-24 18:45:39 +0200

    Fix typo in event key

--- frontend/src/metabase/query_builder/components/expressions/analytics.ts
+++ frontend/src/metabase/query_builder/components/expressions/analytics.ts
@@ -3,6 +3,6 @@ import { trackSchemaEvent } from "metabase/lib/analytics";
 export const trackColumnCombineViaShortcut = () => {
   trackSchemaEvent("question", "1-0-4", {
     event: "column_combine_via_shortcut",
-    custom_expression_uses: ["concat"],
+    custom_expressions_used: ["concat"],
   });
 };
