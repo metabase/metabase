4c72721dfa1697e4656bb0c969e1db9b6ff6cddc
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-04-24 15:10:52 +0200

    Add boilerplate `subscriptionApi`

---
+++ frontend/src/metabase/api/subscription.ts
@@ -0,0 +1,39 @@
+import { Api } from "./api";
+
+export const subscriptionApi = Api.injectEndpoints({
+  endpoints: builder => ({
+    listSubscriptions: builder.query<unknown, unknown>({
+      query: () => ({
+        method: "GET",
+        url: "/api/pulse",
+      }),
+    }),
+    getSubscription: builder.query<unknown, number>({
+      query: id => ({
+        method: "GET",
+        url: `/api/pulse/${id}`,
+      }),
+    }),
+    createSubscription: builder.mutation<unknown, unknown>({
+      query: body => ({
+        method: "POST",
+        url: "/api/pulse",
+        body,
+      }),
+    }),
+    updateSubscription: builder.mutation<unknown, unknown>({
+      query: ({ id, ...body }) => ({
+        method: "PUT",
+        url: `/api/pulse/${id}`,
+        body,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useListSubscriptionsQuery,
+  useGetSubscriptionQuery,
+  useCreateSubscriptionMutation,
+  useUpdateSubscriptionMutation,
+} = subscriptionApi;
