1ac4b35a9569c1de9b2d44340dc1cd522128d0cd
Case Nelson <case@metabase.com>
2024-04-25 16:33:08 -0600

    Throw on leftover metrics

--- src/metabase/query_processor/middleware/metrics.clj
+++ src/metabase/query_processor/middleware/metrics.clj
@@ -5,8 +5,7 @@
    [metabase.lib.util :as lib.util]
    [metabase.lib.util.match :as lib.util.match]
    [metabase.lib.walk :as lib.walk]
-   [metabase.util :as u]
-   [metabase.util.log :as log]))
+   [metabase.util :as u]))
 
 (defn- replace-metric-aggregation-refs [x lookup]
   (lib.util.match/replace
@@ -115,4 +114,5 @@
         @metric-ref-lookup)
       (when-let [match (lib.util.match/match-one <>
                          [:metric {} _] &match)]
-        (log/warn "Failed to replace metric" (pr-str match) (pr-str @metric-ref-lookup))))))
+        (throw (ex-info "Failed to replace metric" {:match match
+                                                    :lookup @metric-ref-lookup}))))))
