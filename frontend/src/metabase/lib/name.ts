62c90fead7d8ad3ab3db0fea852ff532a3a6cd43
Ryan Laurie <iethree@gmail.com>
2024-04-25 16:19:10 -0600

    fix display name

--- frontend/src/metabase/lib/name.ts
+++ frontend/src/metabase/lib/name.ts
@@ -1,8 +1,8 @@
 type ItemWithName = {
   name: string;
-  displayName?: string;
+  display_name?: string;
 };
 
 /** items may or may not have display names, but we should prefer displaying them when they do */
 export const getName = (item: ItemWithName): string =>
-  item.displayName || item.name;
+  item.display_name || item.name;
