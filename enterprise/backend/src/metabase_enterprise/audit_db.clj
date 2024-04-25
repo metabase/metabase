5ec0dd285d004581cc0893234b5d002453f7111d
Noah Moss <noahbmoss@gmail.com>
2024-04-25 09:41:15 -0400

    change default of last-analytics-checksum to nil instead of 0

--- enterprise/backend/src/metabase_enterprise/audit_db.clj
+++ enterprise/backend/src/metabase_enterprise/audit_db.clj
@@ -25,7 +25,6 @@
   "A place to save the analytics-checksum, to check between app startups. If set to -1, skips the checksum process
   entirely to avoid calculating checksums in environments (e2e tests) where we don't care."
   :type       :integer
-  :default    0
   :visibility :internal
   :audit      :never
   :doc        false
