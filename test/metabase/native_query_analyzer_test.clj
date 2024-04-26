48c4945132709224b7e5146adf617e3aea2498dc
Alexander Solovyov <alexander@solovyov.net>
2024-04-26 17:04:05 +0300

    woah mysql you disappoint me once again

--- test/metabase/native_query_analyzer_test.clj
+++ test/metabase/native_query_analyzer_test.clj
@@ -1,6 +1,7 @@
 (ns metabase.native-query-analyzer-test
   (:require
    [clojure.test :refer :all]
+   [metabase.db.connection :as mdb.connection]
    [metabase.native-query-analyzer :as query-analyzer]
    [metabase.public-settings :as public-settings]
    [metabase.test :as mt]))
@@ -33,28 +34,32 @@
   (binding [query-analyzer/*parse-queries-in-test?* true]
     (let [q (fn [sql]
               (#'query-analyzer/field-ids-for-sql (mt/native-query {:query sql})))]
-      (mt/$ids nil
-        (testing "simple query matches"
-          (is (= {:direct #{%venues.id} :indirect nil}
-                 (q "select id from venues"))))
-        (testing "quotes stop case matching"
-          (is (= {:direct nil :indirect nil}
+      (testing "simple query matches"
+        (is (= {:direct #{(mt/id :venues :id)} :indirect nil}
+               (q "select id from venues"))))
+      (testing "quotes stop case matching"
+        ;; MySQL does case-insensitive string comparisons by default; quoting does not make it conside case
+        ;; in field names either, so it's consistent behavior
+        (if (= (mdb.connection/db-type) :mysql)
+          (is (= {:direct #{(mt/id :venues :id)} :indirect nil}
                  (q "select \"id\" from venues")))
-          (is (= {:direct #{%venues.id} :indirect nil}
-                 (q "select \"ID\" from venues"))))
-        (testing "you can mix quoted and unquoted names"
-          (is (= {:direct #{%venues.id %venues.name} :indirect nil}
-                 (q "select v.\"ID\", v.name from venues"))))
-        (testing "It will find all relevant columns if query is not specific"
-          (is (= {:direct #{%venues.id %checkins.id} :indirect nil}
-                 (q "select id from venues join checkins"))))
-        (testing "But if you are specific - then it's a concrete field"
-          (is (= {:direct #{%venues.id} :indirect nil}
-                 (q "select v.id from venues v join checkins"))))
-        (testing "And wildcards are matching everything"
-          (is (= 10
-                 (-> (q "select * from venues v join checkins")
-                     :indirect count)))
-          (is (= 6
-                 (-> (q "select v.* from venues v join checkins")
-                     :indirect count))))))))
+          (is (= {:direct nil :indirect nil}
+                 (q "select \"id\" from venues"))))
+        (is (= {:direct #{(mt/id :venues :id)} :indirect nil}
+               (q "select \"ID\" from venues"))))
+      (testing "you can mix quoted and unquoted names"
+        (is (= {:direct #{(mt/id :venues :id) (mt/id :venues :name)} :indirect nil}
+               (q "select v.\"ID\", v.name from venues"))))
+      (testing "It will find all relevant columns if query is not specific"
+        (is (= {:direct #{(mt/id :venues :id) (mt/id :checkins :id)} :indirect nil}
+               (q "select id from venues join checkins"))))
+      (testing "But if you are specific - then it's a concrete field"
+        (is (= {:direct #{(mt/id :venues :id)} :indirect nil}
+               (q "select v.id from venues v join checkins"))))
+      (testing "And wildcards are matching everything"
+        (is (= 10
+               (-> (q "select * from venues v join checkins")
+                   :indirect count)))
+        (is (= 6
+               (-> (q "select v.* from venues v join checkins")
+                   :indirect count)))))))
