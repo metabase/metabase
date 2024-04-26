a4ec1dd33269386cc2e7b3ea6f4c083d09d2662f
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-04-26 16:15:16 -0400

    Trivial change to create a separate PR

--- frontend/src/metabase/browse/components/BrowseModels.tsx
+++ frontend/src/metabase/browse/components/BrowseModels.tsx
@@ -29,7 +29,7 @@ export const BrowseModels = ({
     getCollectionViewPreferences,
   );
 
-  if (error || isLoading) {
+  if (isLoading || error) {
     return (
       <LoadingAndErrorWrapper
         error={error}
