0da8100558340c745e6dfdc48a11eb48190ca170
Alexander Solovyov <alexander@solovyov.net>
2024-04-26 15:25:49 +0300

    Store fields used in MBQL cards in QueryField (#41821)

--- src/metabase/native_query_analyzer.clj
+++ src/metabase/native_query_analyzer.clj
@@ -17,7 +17,6 @@
    [metabase.native-query-analyzer.parameter-substitution :as nqa.sub]
    [metabase.public-settings :as public-settings]
    [metabase.util :as u]
-   [metabase.util.log :as log]
    [toucan2.core :as t2]))
 
 (def ^:dynamic *parse-queries-in-test?*
@@ -94,53 +93,25 @@
       ;; limit to the named tables
       (seq table-wildcards)            (active-fields-from-tables table-wildcards))))
 
-(defn- field-ids-for-card
+(defn field-ids-for-sql
   "Returns a `{:direct #{...} :indirect #{...}}` map with field IDs that (may) be referenced in the given cards's
   query. Errs on the side of optimism: i.e., it may return fields that are *not* in the query, and is unlikely to fail
   to return fields that are in the query.
 
   Direct references are columns that are named in the query; indirect ones are from wildcards. If a field could be
   both direct and indirect, it will *only* show up in the `:direct` set."
-  [card]
-  (let [query        (:dataset_query card)
-        db-id        (:database query)
-        sql-string   (:query (nqa.sub/replace-tags query))
-        parsed-query (macaw/query->components (macaw/parsed-query sql-string))
-        direct-ids   (direct-field-ids-for-query parsed-query db-id)
-        indirect-ids (set/difference
-                      (indirect-field-ids-for-query parsed-query db-id)
-                      direct-ids)]
-    {:direct   direct-ids
-     :indirect indirect-ids}))
-
-(defn update-query-fields-for-card!
-  "Clears QueryFields associated with this card and creates fresh, up-to-date-ones.
-
-  Any card is accepted, but this functionality only works for ones with a native query.
-
-  If you're invoking this from a test, be sure to turn on [[*parse-queries-in-test?*]].
-
-  Returns `nil` (and logs the error) if there was a parse error."
-  [{card-id :id, query :dataset_query :as card}]
+  [query]
   (when (and (active?)
-             (= :native (:type query)))
-    (try
-      (let [{:keys [direct indirect]} (field-ids-for-card card)
-            id->record                (fn [direct? field-id]
-                                        {:card_id          card-id
-                                         :field_id         field-id
-                                         :direct_reference direct?})
-            query-field-records       (concat
-                                       (map (partial id->record true) direct)
-                                       (map (partial id->record false) indirect))]
-        ;; This feels inefficient at first glance, but the number of records should be quite small and doing some sort
-        ;; of upsert-or-delete would involve comparisons in Clojure-land that are more expensive than just "killing and
-        ;; filling" the records.
-        (t2/with-transaction [_conn]
-          (t2/delete! :model/QueryField :card_id card-id)
-          (t2/insert! :model/QueryField query-field-records)))
-      (catch Exception e
-        (log/error e "Error parsing native query")))))
+             (:native query))
+    (let [db-id        (:database query)
+          sql-string   (:query (nqa.sub/replace-tags query))
+          parsed-query (macaw/query->components (macaw/parsed-query sql-string))
+          direct-ids   (direct-field-ids-for-query parsed-query db-id)
+          indirect-ids (set/difference
+                        (indirect-field-ids-for-query parsed-query db-id)
+                        direct-ids)]
+      {:direct   direct-ids
+       :indirect indirect-ids})))
 
 ;; TODO: does not support template tags
 (defn replace-names
