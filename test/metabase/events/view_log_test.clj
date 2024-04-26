70cf20d5419f5add481701898778e0c91de62175
Cal Herries <calherries@gmail.com>
2024-04-26 14:07:13 +0100

    Reuse view_log namespace instead

--- test/metabase/events/view_log_test.clj
+++ test/metabase/events/view_log_test.clj
@@ -106,6 +106,33 @@
                 :context    "dashboard"}
                (latest-view (u/id user) (u/id card)))))))))
 
+(deftest card-read-view-count-test
+  (mt/with-temp [:model/User user {}
+                 :model/Card card {:creator_id (u/id user)}]
+    (testing "A card read events are recorded by a card's view_count"
+      (is (= 0 (:view_count card))
+          "view_count should be 0 before the event is published")
+      (events/publish-event! :event/card-read {:object card :user-id (u/the-id user)})
+      (is (= 1 (t2/select-one-fn :view_count :model/Card (:id card)))
+          "view_count should be incremented")
+      (events/publish-event! :event/card-read {:object card :user-id (u/the-id user)})
+      (is (= 2 (t2/select-one-fn :view_count :model/Card (:id card)))
+          "view_count should be incremented"))))
+
+(deftest dashboard-read-view-count-test
+  (mt/with-temp [:model/User user {}
+                 :model/Dashboard dashboard {:creator_id (u/id user)}]
+    (testing "A dashboard read events are recorded by a dashboard's view_count"
+      (is (= 0 (:view_count dashboard))
+          "view_count should be 0 before the event is published")
+      (events/publish-event! :event/dashboard-read {:object dashboard :user-id (u/the-id user)})
+      (is (= 1 (t2/select-one-fn :view_count :model/Dashboard (:id dashboard)))
+          "view_count should be incremented")
+      (events/publish-event! :event/dashboard-read {:object dashboard :user-id (u/the-id user)})
+      (is (= 2 (t2/select-one-fn :view_count :model/Dashboard (:id dashboard)))
+          "view_count should be incremented"))))
+
+
 ;;; ---------------------------------------- API tests begin -----------------------------------------
 
 (deftest get-collection-view-log-test
