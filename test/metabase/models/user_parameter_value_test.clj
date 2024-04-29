0283e859384ddd84c79f0a10bcafba451e2f2612
Adam James <adam.vermeer2@gmail.com>
2024-04-29 11:04:44 -0700

    Add idx to FK for user parameter values table + fix tests
    PG needs idx to be manually added to foreign keys, so I added that migration.
    
    Additionally, I think I fixed the tests failing by using a temp empty db. Hopefully that fixes the failures.

--- test/metabase/models/user_parameter_value_test.clj
+++ test/metabase/models/user_parameter_value_test.clj
@@ -6,18 +6,18 @@
    [toucan2.core :as t2]))
 
 (deftest user-parameter-value-crud-test
-  (mt/initialize-if-needed! :db)
-  (mt/with-current-user (mt/user->id :rasta)
-    (mt/with-model-cleanup [:model/UserParameterValue]
-      (let [upv-count (t2/count :model/UserParameterValue)]
-        (testing "Upsert creates new user parameter value entry if the param_id user_id pair doesn't exist"
-          (upv/upsert! (mt/user->id :rasta) "some-param" "A")
-          (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
-          (is (= "A" (:value (t2/select-one :model/UserParameterValue :user_id 1 :parameter_id "some-param")))))
-        (testing "Upsert updates user parameter value entry if the param_id user_id pair already exists"
-          (upv/upsert! (mt/user->id :rasta) "some-param" "B")
-          (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
-          (is (= "B" (:value (t2/select-one :model/UserParameterValue :user_id 1 :parameter_id "some-param")))))
-        (testing "Upsert deletes user parameter value entry if value is `nil`."
-          (upv/upsert! (mt/user->id :rasta) "some-param" nil)
-          (is (= upv-count (t2/count :model/UserParameterValue))))))))
+  (mt/with-temp [:model/Database _ {}]
+    (mt/with-current-user (mt/user->id :rasta)
+      (mt/with-model-cleanup [:model/UserParameterValue]
+        (let [upv-count (t2/count :model/UserParameterValue)]
+          (testing "Upsert creates new user parameter value entry if the param_id user_id pair doesn't exist"
+            (upv/upsert! (mt/user->id :rasta) "some-param" "A")
+            (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
+            (is (= "A" (:value (t2/select-one :model/UserParameterValue :user_id 1 :parameter_id "some-param")))))
+          (testing "Upsert updates user parameter value entry if the param_id user_id pair already exists"
+            (upv/upsert! (mt/user->id :rasta) "some-param" "B")
+            (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
+            (is (= "B" (:value (t2/select-one :model/UserParameterValue :user_id 1 :parameter_id "some-param")))))
+          (testing "Upsert deletes user parameter value entry if value is `nil`."
+            (upv/upsert! (mt/user->id :rasta) "some-param" nil)
+            (is (= upv-count (t2/count :model/UserParameterValue)))))))))
