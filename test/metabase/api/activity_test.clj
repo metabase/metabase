93661f994f055e3312b09e867052e735993a3e9a
Case Nelson <case@metabase.com>
2024-04-29 14:15:46 -0600

    [Metrics V2] Fix model on activity endpoint (#41998)
    Fixes #41993

--- test/metabase/api/activity_test.clj
+++ test/metabase/api/activity_test.clj
@@ -106,6 +106,11 @@
                                     :type                   :model
                                     :creator_id             (mt/user->id :crowberto)
                                     :display                "table"
+                                    :visualization_settings {}}
+                 Card      metric  {:name                   "rand-name"
+                                    :type                   :metric
+                                    :creator_id             (mt/user->id :crowberto)
+                                    :display                "table"
                                     :visualization_settings {}}]
     (testing "recent_views endpoint shows the current user's recently viewed items."
       (mt/with-model-cleanup [ViewLog]
@@ -120,12 +125,14 @@
                                          {:topic :event/dashboard-read :event {:object dash}}
                                          {:topic :event/table-read :event {:object table1}}
                                          {:topic :event/card-query :event {:card-id (:id archived)}}
-                                         {:topic :event/table-read :event {:object hidden-table}}]]
+                                         {:topic :event/table-read :event {:object hidden-table}}
+                                         {:topic :event/card-query :event {:card-id (:id metric)}}]]
             (events/publish-event! topic (assoc event :user-id (mt/user->id :crowberto))))
           (testing "No duplicates or archived items are returned."
             (let [recent-views (mt/user-http-request :crowberto :get 200 "activity/recent_views")]
-              (is (partial=
-                   [{:model "table" :model_id (u/the-id table1)}
+              (is (=?
+                   [{:model "metric" :model_id (u/the-id metric)}
+                    {:model "table" :model_id (u/the-id table1)}
                     {:model "dashboard" :model_id (u/the-id dash)}
                     {:model "card" :model_id (u/the-id card1)}
                     {:model "dataset" :model_id (u/the-id dataset)}]
@@ -186,8 +193,13 @@
                                     :type                   :model
                                     :creator_id             (mt/user->id :crowberto)
                                     :display                "table"
+                                    :visualization_settings {}}
+                 Card      metric  {:name                   "rand-name"
+                                    :type                   :metric
+                                    :creator_id             (mt/user->id :crowberto)
+                                    :display                "table"
                                     :visualization_settings {}}]
-    (let [test-ids (set (map :id [card1 archived dash1 dash2 table1 hidden-table dataset]))]
+    (let [test-ids (set (map :id [card1 archived dash1 dash2 table1 hidden-table dataset metric]))]
       (testing "Items viewed by multiple users are not duplicated in the popular items list."
         (mt/with-model-cleanup [ViewLog QueryExecution]
           (create-views! [[(mt/user->id :rasta)     "dashboard" (:id dash1)]
@@ -205,10 +217,10 @@
           (create-views! [[(mt/user->id :rasta) "dashboard" (:id dash1)]
                           [(mt/user->id :rasta) "card"      (:id card1)]
                           [(mt/user->id :rasta) "table"     (:id table1)]
-                          [(mt/user->id :rasta) "card"      (:id dataset)]])
+                          [(mt/user->id :rasta) "card"      (:id metric)]])
           (is (= [["dashboard" (:id dash1)]
                   ["card" (:id card1)]
-                  ["dataset" (:id dataset)]
+                  ["metric" (:id metric)]
                   ["table" (:id table1)]]
                  ;; all views are from :rasta, but :crowberto can still see popular items
                  (->> (mt/user-http-request :crowberto :get 200 "activity/popular_items")
