67e9e7b9836d14f00f438c99aae71ac5f40e5e2a
Nicolò Pretto <info@npretto.com>
2024-04-29 19:12:40 +0200

    add "Setup embedding" to the admin setup checklist (#41638)
    * add setup embedding to the admin setup checklist
    
    * restore whitespace as it was before
    
    * i miss prettier (whitespace again)
    
    * Clean up check list for embedding
    
    Largely making a new entry for the embedding info just to fight long
    lines. `:embedding-homepage-dismissed-as-done` just made the maps really
    long. If we have a `:done?` under an `:embedding` keyword it reads much
    more naturally.
    
    Also, The previous diff was calling `boolean` on a var rather than
    invoking the var:
    
    ```clojure
    :embedding-app-origin (boolean embed.settings/embedding-app-origin)
    ;;                             ^^^^^^^^^^^^^^ needs to be (invoked)
    ```
    
    ---------
    
    Co-authored-by: dan sutton <dan@dpsutton.com>

--- src/metabase/api/setup.clj
+++ src/metabase/api/setup.clj
@@ -8,7 +8,9 @@
    [metabase.config :as config]
    [metabase.db :as mdb]
    [metabase.email :as email]
+   [metabase.embed.settings :as embed.settings]
    [metabase.events :as events]
+   [metabase.integrations.google :as google]
    [metabase.integrations.slack :as slack]
    [metabase.models.card :refer [Card]]
    [metabase.models.collection :refer [Collection]]
@@ -149,9 +151,14 @@
   [:map {:closed true}
    [:db-type [:enum :h2 :mysql :postgres]]
    [:hosted? :boolean]
+   [:embedding [:map
+                [:interested? :boolean]
+                [:done? :boolean]
+                [:app-origin :boolean]]]
    [:configured [:map
                  [:email :boolean]
-                 [:slack :boolean]]]
+                 [:slack :boolean]
+                 [:sso :boolean]]]
    [:counts [:map
              [:user :int]
              [:card :int]
@@ -162,26 +169,33 @@
              [:dashboard :boolean]
              [:pulse :boolean]
              [:hidden-table :boolean]
-             [:collection :boolean]]]])
+             [:collection :boolean]
+             [:embedded-resource :boolean]]]])
 
 (mu/defn ^:private state-for-checklist :- ChecklistState
   []
   {:db-type    (mdb/db-type)
    :hosted?    (premium-features/is-hosted?)
+   :embedding  {:interested? (not (= (embed.settings/embedding-homepage) :hidden))
+                :done?       (= (embed.settings/embedding-homepage) :dismissed-done)
+                :app-origin  (boolean (embed.settings/embedding-app-origin))}
    :configured {:email (email/email-configured?)
-                :slack (slack/slack-configured?)}
+                :slack (slack/slack-configured?)
+                :sso   (google/google-auth-enabled)}
    :counts     {:user  (t2/count User)
                 :card  (t2/count Card)
                 :table (t2/count Table)}
-   :exists     {:non-sample-db (t2/exists? Database, :is_sample false)
-                :dashboard     (t2/exists? Dashboard)
-                :pulse         (t2/exists? Pulse)
-                :hidden-table  (t2/exists? Table, :visibility_type [:not= nil])
-                :collection    (t2/exists? Collection)
-                :model         (t2/exists? Card :type :model)}})
+   :exists     {:non-sample-db     (t2/exists? Database, :is_sample false)
+                :dashboard         (t2/exists? Dashboard)
+                :pulse             (t2/exists? Pulse)
+                :hidden-table      (t2/exists? Table, :visibility_type [:not= nil])
+                :collection        (t2/exists? Collection)
+                :model             (t2/exists? Card :type :model)
+                :embedded-resource (or (t2/exists? Card :enable_embedding true)
+                                       (t2/exists? Dashboard :enable_embedding true))}})
 
 (defn- get-connected-tasks
-  [{:keys [configured counts exists] :as _info}]
+  [{:keys [configured counts exists embedding] :as _info}]
   [{:title       (tru "Add a database")
     :group       (tru "Get connected")
     :description (tru "Connect to your data so your whole team can start to explore.")
@@ -200,6 +214,13 @@
     :link        "/admin/settings/slack"
     :completed   (configured :slack)
     :triggered   :always}
+   {:title       (tru "Setup embedding")
+    :group       (tru "Get connected")
+    :link        "/admin/settings/embedding-in-other-applications"
+    :completed   (or (embedding :done?)
+                     (and (configured :sso) (embedding :app-origin))
+                     (exists :embedded-resource))
+    :triggered   (embedding :interested?)}
    {:title       (tru "Invite team members")
     :group       (tru "Get connected")
     :description (tru "Share answers and data with the rest of your team.")
