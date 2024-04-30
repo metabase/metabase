8f5691fd0acdd69d366606d9d4c77b213a8efedf
Phoomparin Mano <phoomparin@gmail.com>
2024-04-30 23:29:52 +0700

    add more descriptions on what the theme overrides are for

--- frontend/src/metabase/ui/components/theme/ThemeProvider/ThemeProvider.tsx
+++ frontend/src/metabase/ui/components/theme/ThemeProvider/ThemeProvider.tsx
@@ -10,11 +10,18 @@ import { DatesProvider } from "../DatesProvider";
 
 interface ThemeProviderProps {
   children: ReactNode;
+
+  /**
+   * Extend Metabase's theme overrides.
+   * This is primarily used in the React embedding SDK
+   * to allow SDK users to customize the theme.
+   */
   theme?: MantineThemeOverride;
 }
 
 export const ThemeProvider = withEmotionCache(
   (props: ThemeProviderProps, cache: EmotionCache) => {
+    // Merge default theme overrides with user-provided theme overrides
     const theme = useMemo(() => {
       return merge(getThemeOverrides(), props.theme);
     }, [props.theme]);
