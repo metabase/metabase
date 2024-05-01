11ced06cd0e31127cc16548e21520cf1fdf81b73
Romeo Van Snick <romeo@romeovansnick.be>
2024-05-01 09:35:46 +0200

    Set up extractions metadata helper

--- frontend/src/metabase-lib/extractions.ts
+++ frontend/src/metabase-lib/extractions.ts
@@ -9,3 +9,40 @@ export function extract(
 ): Query {
   return ML.extract(query, stageIndex, extraction);
 }
+
+const extractions = {
+  "hour-of-day": {
+    functions: ["hour"],
+  },
+  "day-of-month": {
+    functions: ["day"],
+  },
+  "day-of-week": {
+    functions: ["weekday"],
+  },
+  "month-of-year": {
+    functions: ["month"],
+  },
+  "quarter-of-year": {
+    functions: ["quarter"],
+  },
+  year: {
+    functions: ["year"],
+  },
+  domain: {
+    functions: ["domain"],
+  },
+  host: {
+    functions: ["host"],
+  },
+  subdomain: {
+    functions: ["subdomain"],
+  },
+};
+
+/**
+ * Return the functions used by a specific column extraction.
+ */
+export function functionsUsedByExtraction(tag: string): string[] {
+  return extractions[tag as keyof typeof extractions]?.functions ?? [];
+}
