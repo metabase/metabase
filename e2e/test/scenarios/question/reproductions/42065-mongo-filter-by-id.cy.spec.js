f6cf4b1c3019b3ff4130f2bc99fde6dec87ca1e7
lbrdnk <lbrdnk@users.noreply.github.com>
2024-05-01 11:26:52 +0200

    Update e2e test to use id gathered from actual data

--- e2e/test/scenarios/question/reproductions/42065-mongo-filter-by-id.cy.spec.js
+++ e2e/test/scenarios/question/reproductions/42065-mongo-filter-by-id.cy.spec.js
@@ -20,27 +20,39 @@ describe(
     });
 
     it("should be possible to filter by Mongo _id column (metabase#42010)", () => {
-      cy.log("Check notebook editor");
-      cy.findByRole("button", { name: "Filter" }).click();
-      cy.get("[data-element-id=list-item-title]")
-        .eq(0)
-        .should("have.text", "ID");
-      cy.get("[data-element-id=list-item-title]").eq(0).click();
-      cy.findByPlaceholderText("Search by ID").type("663167e513bd481495a98bd0");
-      cy.get("button").contains("Add filter").click();
       cy.get("button").contains("Visualize").click();
-      cy.get("button").contains("Showing 1 row");
+      cy.get("[data-testid=cell-data]")
+        .eq(10)
+        .then(elm => {
+          // Get the id of some row and go back to notebook editor. I suppose it differs per snapshot, hence we are
+          // unable to hardcode the value.
+          const id = elm.text();
+          cy.go("back");
 
-      cy.log("Remove current filter");
-      cy.findByRole("button", { name: "Remove" }).click();
+          // Check the notebook editor
+          cy.log("Check notebook editor");
+          cy.findByRole("button", { name: "Filter" }).click();
+          // Following selects first item from filter column picker
+          cy.get("[data-element-id=list-item-title]")
+            .eq(0)
+            .should("have.text", "ID");
+          cy.get("[data-element-id=list-item-title]").eq(0).click();
+          cy.findByPlaceholderText("Search by ID").type(id);
+          cy.get("button").contains("Add filter").click();
+          cy.get("button").contains("Visualize").click();
+          cy.get("button").contains("Showing 1 row");
 
-      cy.log("Check chill mode");
-      cy.get("[data-testid=cell-data]").eq(0).should("have.text", "ID");
-      cy.get("[data-testid=cell-data]").eq(0).click();
-      cy.get("button").contains("Filter by this column").click();
-      cy.findByPlaceholderText("Search by ID").type("663167e513bd481495a98bd0");
-      cy.get("button").contains("Add filter").click();
-      cy.get("button").contains("Showing 1 row");
+          // Now remove the filter
+          cy.log("Remove current filter");
+          cy.findByRole("button", { name: "Remove" }).click();
+
+          // And add the filter through chill mode
+          cy.log("Check chill mode");
+          cy.get("button").contains("Filter").click();
+          cy.findByPlaceholderText("Search by ID").type(id);
+          cy.get("button").contains("Apply filter").click();
+          cy.get("button").contains("Showing 1 row");
+        });
     });
   },
 );
