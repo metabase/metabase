e98b9c7dd780f56b71d2d5fad04f116366dd0ae6
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-05-01 16:44:52 +0300

    Cleanup parameters popover

--- frontend/src/metabase/dashboard/components/ParametersPopover.jsx
+++ frontend/src/metabase/dashboard/components/ParametersPopover.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import styled from "@emotion/styled";
 import cx from "classnames";
-import { Component } from "react";
 import { t } from "ttag";
 import _ from "underscore";
 
@@ -23,40 +22,30 @@ const PopoverBody = styled.div`
 `;
 
 const defaultOptionForParameterSection = getDefaultOptionForParameterSection();
+const PARAMETER_SECTIONS = getDashboardParameterSections();
 
-export class ParametersPopover extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {};
-  }
+export const ParametersPopover = ({ onClose, onAddParameter }) => {
+  return (
+    <ParameterOptionsSectionsPane
+      sections={PARAMETER_SECTIONS}
+      onSelectSection={selectedSection => {
+        const parameterSection = _.findWhere(PARAMETER_SECTIONS, {
+          id: selectedSection.id,
+        });
 
-  PARAMETER_SECTIONS = getDashboardParameterSections();
+        if (parameterSection) {
+          const defaultOption =
+            defaultOptionForParameterSection[parameterSection.id];
 
-  render() {
-    const { onClose, onAddParameter } = this.props;
+          onAddParameter(defaultOption);
+          onClose();
+        }
+      }}
+    />
+  );
+};
 
-    return (
-      <ParameterOptionsSectionsPane
-        sections={this.PARAMETER_SECTIONS}
-        onSelectSection={selectedSection => {
-          const parameterSection = _.findWhere(this.PARAMETER_SECTIONS, {
-            id: selectedSection.id,
-          });
-
-          if (parameterSection) {
-            const defaultOption =
-              defaultOptionForParameterSection[parameterSection.id];
-
-            onAddParameter(defaultOption);
-            onClose();
-          }
-        }}
-      />
-    );
-  }
-}
-
-export const ParameterOptionsSection = ({ section, onClick }) => (
+const ParameterOptionsSection = ({ section, onClick }) => (
   <OptionItemRoot onClick={onClick}>
     <OptionItemTitle
       className={cx(CS.textBold, CS.flex, CS.alignCenter)}
@@ -73,7 +62,7 @@ export const ParameterOptionsSection = ({ section, onClick }) => (
   </OptionItemRoot>
 );
 
-export const ParameterOptionsSectionsPane = ({ sections, onSelectSection }) => (
+const ParameterOptionsSectionsPane = ({ sections, onSelectSection }) => (
   <PopoverBody className={CS.pb2}>
     <h3
       className={cx(CS.pb2, CS.pt3, CS.px3)}
@@ -86,31 +75,6 @@ export const ParameterOptionsSectionsPane = ({ sections, onSelectSection }) => (
           onClick={() => onSelectSection(section)}
         />
       ))}
-    </ul>
-  </PopoverBody>
-);
-
-export const ParameterOptionItem = ({ option, onClick }) => (
-  <OptionItemRoot onClick={onClick}>
-    <OptionItemTitle className={CS.textBold} style={{ marginBottom: 4 }}>
-      {option.menuName || option.name}
-    </OptionItemTitle>
-    <OptionItemDescription>{option.description}</OptionItemDescription>
-  </OptionItemRoot>
-);
-
-export const ParameterOptionsPane = ({ options, onSelectOption }) => (
-  <PopoverBody className={CS.pb2}>
-    <h3 className={cx(CS.pb2, CS.pt3, CS.px3)}>{t`What kind of filter?`}</h3>
-    <ul>
-      {options &&
-        options.map(option => (
-          <ParameterOptionItem
-            key={option.menuName || option.name}
-            option={option}
-            onClick={() => onSelectOption(option)}
-          />
-        ))}
     </ul>
   </PopoverBody>
 );
