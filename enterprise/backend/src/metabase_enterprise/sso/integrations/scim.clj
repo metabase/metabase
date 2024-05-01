e7fc937934116b0dff8043942ecbdc8b0b45cace
Noah Moss <noahbmoss@gmail.com>
2024-04-30 20:34:42 -0400

    drop null constraint on api_key.user_id and create new scim ns

---
+++ enterprise/backend/src/metabase_enterprise/sso/integrations/scim.clj
@@ -0,0 +1,9 @@
+(ns metabase-enterprise.sso.integrations.scim
+  (:require [toucan2.core :as t2]))
+
+;; TODO: should we allow multiple SCIM API keys at once?
+(defn refresh-scim-api-key!
+  "Generates a new SCIM API key and deletes any that already exist."
+  []
+  (t2/with-transaction [_conn]
+    (t2/delete :model/ApiKey :scope :scim)))
--- src/metabase/models/api_key.clj
+++ src/metabase/models/api_key.clj
@@ -29,9 +29,8 @@
                                         [:pg.id :group-id]
                                         [:api_key.id :api-key-id]]
                                :from   [[:permissions_group :pg]]
-                               :join   [[:permissions_group_membership :pgm]
-                                      [:= :pgm.group_id :pg.id]
-                                      :api_key [:= :api_key.user_id :pgm.user_id]]
+                               :join   [[:permissions_group_membership :pgm] [:= :pgm.group_id :pg.id]
+                                        :api_key [:= :api_key.user_id :pgm.user_id]]
                                :where  [:in :api_key.id (map u/the-id api-keys)]}))
           api-key-id->group
           (fn [api-key-id]
