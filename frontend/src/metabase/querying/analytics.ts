6e75be433ed8b90772eec965bad3bbe767e9688c
Romeo Van Snick <romeo@romeovansnick.be>
2024-05-02 20:08:49 +0200

    Add snowplow event for column combine via header

---
+++ frontend/src/metabase/querying/analytics.ts
@@ -0,0 +1,15 @@
+import { trackSchemaEvent } from "metabase/lib/analytics";
+import * as Lib from "metabase-lib";
+import type Question from "metabase-lib/v1/Question";
+
+export const trackColumnCombineViaColumnHeader = (
+  query: Lib.Query,
+  question?: Question,
+) => {
+  trackSchemaEvent("question", "1-0-4", {
+    event: "column_combine_via_column_header",
+    custom_expressions_used: ["concat"],
+    database_id: Lib.databaseID(query),
+    question_id: question?.id() ?? 0,
+  });
+};
