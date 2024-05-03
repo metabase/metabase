1ee51081fcfaf1658fb493d2cd4925adfbb3ab6e
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-05-03 15:34:30 +0200

    Remove unused `getEngineLogo` function

--- frontend/src/metabase/lib/engine.js
+++ frontend/src/metabase/lib/engine.js
@@ -24,35 +24,6 @@ export function getEngineNativeAceMode(engine) {
   }
 }
 
-export function getEngineLogo(engine) {
-  const path = `app/assets/img/drivers`;
-
-  switch (engine) {
-    case "bigquery":
-    case "druid":
-    case "h2":
-    case "mongo":
-    case "mysql":
-    case "oracle":
-    case "postgres":
-    case "redshift":
-    case "snowflake":
-    case "sparksql":
-    case "sqlite":
-    case "sqlserver":
-    case "vertica":
-      return `${path}/${engine}.svg`;
-    case "bigquery-cloud-sdk":
-      return `${path}/bigquery.svg`;
-    case "presto-jdbc":
-      return `${path}/presto.svg`;
-    case "starburst":
-      return `${path}/starburst.svg`;
-    case "materialize":
-      return `${path}/materialize.svg`;
-  }
-}
-
 function formatJsonQuery(query) {
   return JSON.stringify(query, null, 2);
 }
