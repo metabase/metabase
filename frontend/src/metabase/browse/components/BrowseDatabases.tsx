a3cd45d5417b75aff9d94853cae72e643975d039
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-05-03 14:33:31 -0400

    Remove the tabs from Browse (#41955)

--- frontend/src/metabase/browse/components/BrowseDatabases.tsx
+++ frontend/src/metabase/browse/components/BrowseDatabases.tsx
@@ -1,26 +1,32 @@
+import { Link } from "react-router";
 import { t } from "ttag";
 
 import NoResults from "assets/img/no_results.svg";
-import type { useDatabaseListQuery } from "metabase/common/hooks";
+import { useListDatabasesQuery } from "metabase/api";
 import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 import CS from "metabase/css/core/index.css";
 import { color } from "metabase/lib/colors";
 import * as Urls from "metabase/lib/urls";
-import { Box, Icon, Title } from "metabase/ui";
+import { Box, Flex, Group, Icon, Text, Title } from "metabase/ui";
 
-import { CenteredEmptyState } from "./BrowseApp.styled";
+import {
+  BrowseContainer,
+  BrowseHeader,
+  BrowseMain,
+  BrowseSection,
+  CenteredEmptyState,
+  LearnAboutDataIcon,
+} from "./BrowseApp.styled";
 import {
   DatabaseCard,
   DatabaseCardLink,
   DatabaseGrid,
 } from "./BrowseDatabases.styled";
+import { BrowseHeaderIconContainer } from "./BrowseHeader.styled";
 
-export const BrowseDatabases = ({
-  databasesResult,
-}: {
-  databasesResult: ReturnType<typeof useDatabaseListQuery>;
-}) => {
-  const { data: databases, error, isLoading } = databasesResult;
+export const BrowseDatabases = () => {
+  const { data, isLoading, error } = useListDatabasesQuery();
+  const databases = data?.data;
 
   if (error) {
     return <LoadingAndErrorWrapper error />;
@@ -30,34 +36,76 @@ export const BrowseDatabases = ({
     return <LoadingAndErrorWrapper loading />;
   }
 
-  return databases?.length ? (
-    <DatabaseGrid data-testid="database-browser">
-      {databases.map(database => (
-        <div key={database.id}>
-          <DatabaseCardLink to={Urls.browseDatabase(database)}>
-            <DatabaseCard>
-              <Icon
-                name="database"
-                color={color("accent2")}
-                className={CS.mb3}
-                size={32}
-              />
-              <Title order={2} size="1rem" lh="1rem" color="inherit">
-                {database.name}
-              </Title>
-            </DatabaseCard>
-          </DatabaseCardLink>
-        </div>
-      ))}
-    </DatabaseGrid>
-  ) : (
-    <CenteredEmptyState
-      title={<Box mb=".5rem">{t`No databases here yet`}</Box>}
-      illustrationElement={
-        <Box mb=".5rem">
-          <img src={NoResults} />
-        </Box>
-      }
-    />
+  if (!databases?.length) {
+    return (
+      <CenteredEmptyState
+        title={<Box mb=".5rem">{t`No databases here yet`}</Box>}
+        illustrationElement={
+          <Box mb=".5rem">
+            <img src={NoResults} />
+          </Box>
+        }
+      />
+    );
+  }
+
+  return (
+    <BrowseContainer>
+      <BrowseHeader>
+        <BrowseSection>
+          <Flex w="100%" direction="row" justify="space-between" align="center">
+            <Title order={1} color="text-dark">
+              <Group spacing="sm">
+                <Icon color={color("brand")} name="database" />
+                {t`Databases`}
+              </Group>
+            </Title>
+            <LearnAboutDataLink />
+          </Flex>
+        </BrowseSection>
+      </BrowseHeader>
+      <BrowseMain>
+        <BrowseSection>
+          <DatabaseGrid data-testid="database-browser">
+            {databases.map(database => (
+              <div key={database.id}>
+                <DatabaseCardLink to={Urls.browseDatabase(database)}>
+                  <DatabaseCard>
+                    <Icon
+                      name="database"
+                      color={color("accent2")}
+                      className={CS.mb3}
+                      size={32}
+                    />
+                    <Title order={2} size="1rem" lh="1rem" color="inherit">
+                      {database.name}
+                    </Title>
+                  </DatabaseCard>
+                </DatabaseCardLink>
+              </div>
+            ))}
+          </DatabaseGrid>
+        </BrowseSection>
+      </BrowseMain>
+    </BrowseContainer>
   );
 };
+
+const LearnAboutDataLink = () => (
+  <Flex
+    ml="auto"
+    p=".75rem"
+    justify="right"
+    align="center"
+    style={{ flexBasis: "40.0%" }}
+  >
+    <Link to="reference">
+      <BrowseHeaderIconContainer>
+        <LearnAboutDataIcon size={14} name="reference" />
+        <Text size="md" lh="1" fw="bold" ml=".5rem" c="inherit">
+          {t`Learn about our data`}
+        </Text>
+      </BrowseHeaderIconContainer>
+    </Link>
+  </Flex>
+);
--- frontend/src/metabase/browse/components/BrowseModels.tsx
+++ frontend/src/metabase/browse/components/BrowseModels.tsx
@@ -1,43 +1,132 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { t } from "ttag";
+import _ from "underscore";
 
 import NoResults from "assets/img/no_results.svg";
-import type { useSearchListQuery } from "metabase/common/hooks";
+import { useSearchQuery } from "metabase/api";
 import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
+import { color } from "metabase/lib/colors";
 import { useSelector } from "metabase/lib/redux";
+import { PLUGIN_CONTENT_VERIFICATION } from "metabase/plugins";
 import { getLocale } from "metabase/setup/selectors";
-import { Box } from "metabase/ui";
-import type { SearchResult, CollectionId } from "metabase-types/api";
+import { Box, Flex, Group, Icon, Stack, Title } from "metabase/ui";
+import type { CollectionId } from "metabase-types/api";
 
 import { BROWSE_MODELS_LOCALSTORAGE_KEY } from "../constants";
-import { getCollectionViewPreferences, groupModels } from "../utils";
+import {
+  filterModels,
+  getCollectionViewPreferences,
+  groupModels,
+  type ActualModelFilters,
+} from "../utils";
 
-import { CenteredEmptyState } from "./BrowseApp.styled";
+import {
+  BrowseContainer,
+  BrowseHeader,
+  BrowseMain,
+  BrowseSection,
+  CenteredEmptyState,
+} from "./BrowseApp.styled";
 import { ModelGrid } from "./BrowseModels.styled";
 import { ModelExplanationBanner } from "./ModelExplanationBanner";
 import { ModelGroup } from "./ModelGroup";
 
-export const BrowseModels = ({
-  modelsResult,
+const { availableModelFilters } = PLUGIN_CONTENT_VERIFICATION;
+
+export const BrowseModels = () => {
+  const getInitialModelFilters = useCallback(() => {
+    return _.reduce(
+      availableModelFilters,
+      (acc, filter, filterName) => {
+        const storedFilterStatus = localStorage.getItem(
+          `browseFilters.${filterName}`,
+        );
+        const shouldFilterBeActive =
+          storedFilterStatus === null
+            ? filter.activeByDefault
+            : storedFilterStatus === "on";
+        return {
+          ...acc,
+          [filterName]: shouldFilterBeActive,
+        };
+      },
+      {},
+    );
+  }, []);
+
+  const [actualModelFilters, setActualModelFilters] =
+    useState<ActualModelFilters>({});
+
+  useEffect(() => {
+    const initialModelFilters = getInitialModelFilters();
+    setActualModelFilters(initialModelFilters);
+  }, [getInitialModelFilters, setActualModelFilters]);
+
+  const handleModelFilterChange = useCallback(
+    (modelFilterName: string, active: boolean) => {
+      localStorage.setItem(
+        `browseFilters.${modelFilterName}`,
+        active ? "on" : "off",
+      );
+      setActualModelFilters((prev: ActualModelFilters) => {
+        return { ...prev, [modelFilterName]: active };
+      });
+    },
+    [setActualModelFilters],
+  );
+
+  return (
+    <BrowseContainer>
+      <BrowseHeader>
+        <BrowseSection>
+          <Flex w="100%" direction="row" justify="space-between" align="center">
+            <Title order={1} color="text-dark">
+              <Group spacing="sm">
+                <Icon size={18} color={color("brand")} name="model" />
+                {t`Models`}
+              </Group>
+            </Title>
+            <PLUGIN_CONTENT_VERIFICATION.ModelFilterControls
+              actualModelFilters={actualModelFilters}
+              handleModelFilterChange={handleModelFilterChange}
+            />
+          </Flex>
+        </BrowseSection>
+      </BrowseHeader>
+      <BrowseMain>
+        <BrowseSection>
+          <BrowseModelsBody actualModelFilters={actualModelFilters} />
+        </BrowseSection>
+      </BrowseMain>
+    </BrowseContainer>
+  );
+};
+
+export const BrowseModelsBody = ({
+  actualModelFilters,
 }: {
-  modelsResult: ReturnType<typeof useSearchListQuery<SearchResult>>;
+  actualModelFilters: ActualModelFilters;
 }) => {
-  const { data: models = [], error, isLoading } = modelsResult;
+  const { data, error, isLoading } = useSearchQuery({
+    models: ["dataset"],
+    filter_items_in_personal_collection: "exclude",
+  });
+  const unfilteredModels = data?.data;
   const locale = useSelector(getLocale);
   const localeCode: string | undefined = locale?.code;
   const [collectionViewPreferences, setCollectionViewPreferences] = useState(
     getCollectionViewPreferences,
   );
 
-  if (error || isLoading) {
-    return (
-      <LoadingAndErrorWrapper
-        error={error}
-        loading={isLoading}
-        style={{ display: "flex", flex: 1 }}
-      />
-    );
-  }
+  const models = useMemo(
+    () =>
+      filterModels(
+        unfilteredModels || [],
+        actualModelFilters,
+        availableModelFilters,
+      ),
+    [unfilteredModels, actualModelFilters],
+  );
 
   const handleToggleCollectionExpand = (collectionId: CollectionId) => {
     const newPreferences = {
@@ -71,11 +160,21 @@ export const BrowseModels = ({
     );
   };
 
+  if (error || isLoading) {
+    return (
+      <LoadingAndErrorWrapper
+        error={error}
+        loading={isLoading}
+        style={{ display: "flex", flex: 1 }}
+      />
+    );
+  }
+
   const groupsOfModels = groupModels(models, localeCode);
 
   if (models.length) {
     return (
-      <>
+      <Stack spacing="md" mb="lg">
         <ModelExplanationBanner />
         <ModelGrid role="grid">
           {groupsOfModels.map(groupOfModels => {
@@ -99,7 +198,7 @@ export const BrowseModels = ({
             );
           })}
         </ModelGrid>
-      </>
+      </Stack>
     );
   }
 
