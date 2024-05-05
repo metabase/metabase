0e17570a9f1a766bad8165dafdeb3fe365d4089c
Romeo Van Snick <romeo@romeovansnick.be>
2024-05-05 16:37:19 +0200

    Add ExtractColumn action

---
+++ frontend/src/metabase/visualizations/click-actions/actions/ExtractColumn/ExtractColumn.tsx
@@ -0,0 +1,63 @@
+import { t } from "ttag";
+
+import { ExtractColumn } from "metabase/query_builder/components/expressions/ExtractColumn";
+import type { LegacyDrill } from "metabase/visualizations/types";
+import type { ClickActionPopoverProps } from "metabase/visualizations/types/click-actions";
+import * as Lib from "metabase-lib";
+
+export const ExtractColumnAction: LegacyDrill = ({ question, clicked }) => {
+  const { isEditable } = Lib.queryDisplayInfo(question.query());
+
+  if (
+    !clicked ||
+    clicked.value !== undefined ||
+    !clicked.columnShortcuts ||
+    clicked?.extraData?.isRawTable ||
+    !isEditable
+  ) {
+    return [];
+  }
+
+  const Popover = ({
+    onChangeCardAndRun,
+    onClose,
+  }: ClickActionPopoverProps) => {
+    const query = question.query();
+    const stageIndex = -1;
+
+    function handleSubmit(
+      _clause: Lib.Clause,
+      _name: string,
+      extraction: Lib.ColumnExtraction,
+    ) {
+      const newQuery = Lib.extract(query, stageIndex, extraction);
+
+      const nextQuestion = question.setQuery(newQuery);
+      const nextCard = nextQuestion.card();
+
+      onChangeCardAndRun({ nextCard });
+      onClose();
+    }
+
+    return (
+      <ExtractColumn
+        query={query}
+        stageIndex={stageIndex}
+        onSubmit={handleSubmit}
+      />
+    );
+  };
+
+  return [
+    {
+      name: "column-extract",
+      title: t`Extract column`,
+      buttonType: "horizontal",
+      icon: "extract",
+      tooltip: t`Extract column`,
+      default: true,
+      section: "extract",
+      popover: Popover,
+    },
+  ];
+};
