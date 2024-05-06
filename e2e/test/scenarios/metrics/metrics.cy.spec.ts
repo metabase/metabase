ce6b871310f63adb935272e4c7e4f73cd382d914
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-06 15:24:14 -0400

    Basic e2e test

--- e2e/test/scenarios/metrics/metrics.cy.spec.ts
+++ e2e/test/scenarios/metrics/metrics.cy.spec.ts
@@ -1,14 +1,33 @@
-import { popover, restore } from "e2e/support/helpers";
+import { getNotebookStep, modal, popover, restore } from "e2e/support/helpers";
 
 describe("scenarios > metrics", () => {
   beforeEach(() => {
     restore();
-    cy.signInAsAdmin();
+    cy.signInAsNormalUser();
   });
 
   it("should create a metric for a table", () => {
     cy.visit("/");
     cy.findByTestId("app-bar").findByText("New").click();
-    popover().findByText("Metric");
+    popover().findByText("Metric").click();
+    popover().findByText("Raw Data").click();
+    popover().findByText("Orders").click();
+    getNotebookStep("summarize")
+      .findByText("Pick the metric you want to see")
+      .click();
+    popover().findByText("Count of rows").click();
+    cy.button("Save").click();
+    modal().within(() => {
+      cy.findByLabelText("Name").clear().type("My metric");
+      cy.button("Save").click();
+    });
+    cy.findByTestId("query-builder-main").icon("play").click();
+    cy.findByTestId("saved-question-header-title").should(
+      "have.value",
+      "My metric",
+    );
+    cy.findByTestId("scalar-container")
+      .findByText("18,760")
+      .should("be.visible");
   });
 });
--- frontend/src/metabase/query_builder/components/DatasetEditor/DatasetEditor.jsx
+++ frontend/src/metabase/query_builder/components/DatasetEditor/DatasetEditor.jsx
@@ -74,6 +74,7 @@ const propTypes = {
   onCancelCreateNewModel: PropTypes.func.isRequired,
   onCancelDatasetChanges: PropTypes.func.isRequired,
   handleResize: PropTypes.func.isRequired,
+  updateQuestion: PropTypes.func.isRequired,
   runQuestionQuery: PropTypes.func.isRequired,
   onOpenModal: PropTypes.func.isRequired,
 
@@ -209,6 +210,7 @@ function DatasetEditor(props) {
     onCancelDatasetChanges,
     onCancelCreateNewModel,
     onSave,
+    updateQuestion,
     handleResize,
     onOpenModal,
   } = props;
@@ -351,18 +353,27 @@ function DatasetEditor(props) {
   const handleSave = useCallback(async () => {
     const canBeDataset = checkCanBeModel(question);
     const isBrandNewDataset = !question.id();
+    const questionWithDisplay = question.setDefaultDisplay();
 
     if (canBeDataset && isBrandNewDataset) {
+      await updateQuestion(questionWithDisplay, { rerunQuery: false });
       onOpenModal(MODAL_TYPES.SAVE);
     } else if (canBeDataset) {
-      await onSave(question, { rerunQuery: false });
+      await onSave(questionWithDisplay, { rerunQuery: false });
       await setQueryBuilderMode("view");
       runQuestionQuery();
     } else {
       onOpenModal(MODAL_TYPES.CAN_NOT_CREATE_MODEL);
       throw new Error(t`Variables in models aren't supported yet`);
     }
-  }, [question, onSave, setQueryBuilderMode, runQuestionQuery, onOpenModal]);
+  }, [
+    question,
+    updateQuestion,
+    onSave,
+    setQueryBuilderMode,
+    runQuestionQuery,
+    onOpenModal,
+  ]);
 
   const handleColumnSelect = useCallback(
     column => {
