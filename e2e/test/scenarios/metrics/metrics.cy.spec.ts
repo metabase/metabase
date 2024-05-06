d624cd60cb75052e7ea3961b9e2d7eb33278c419
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-06 15:50:48 -0400

    Basic e2e test

--- e2e/test/scenarios/metrics/metrics.cy.spec.ts
+++ e2e/test/scenarios/metrics/metrics.cy.spec.ts
@@ -1,12 +1,21 @@
-import { getNotebookStep, modal, popover, restore } from "e2e/support/helpers";
+import {
+  echartsContainer,
+  getNotebookStep,
+  modal,
+  popover,
+  queryBuilderMain,
+  restore,
+} from "e2e/support/helpers";
 
 describe("scenarios > metrics", () => {
   beforeEach(() => {
     restore();
     cy.signInAsNormalUser();
+    cy.intercept("POST", "/api/dataset").as("dataset");
+    cy.intercept("POST", "/api/card").as("createCard");
   });
 
-  it("should create a metric for a table", () => {
+  it("should create a scalar metric", () => {
     cy.visit("/");
     cy.findByTestId("app-bar").findByText("New").click();
     popover().findByText("Metric").click();
@@ -17,17 +26,40 @@ describe("scenarios > metrics", () => {
       .click();
     popover().findByText("Count of rows").click();
     cy.button("Save").click();
-    modal().within(() => {
-      cy.findByLabelText("Name").clear().type("My metric");
-      cy.button("Save").click();
-    });
-    cy.findByTestId("query-builder-main").icon("play").click();
-    cy.findByTestId("saved-question-header-title").should(
-      "have.value",
-      "My metric",
-    );
+    modal().button("Save").click();
+    cy.wait("@createCard");
+    queryBuilderMain().findByTestId("run-button").click();
+    cy.wait("@dataset");
     cy.findByTestId("scalar-container")
       .findByText("18,760")
       .should("be.visible");
+  });
+
+  it("should create a timeseries metric", () => {
+    cy.visit("/");
+    cy.findByTestId("app-bar").findByText("New").click();
+    popover().findByText("Metric").click();
+    popover().findByText("Raw Data").click();
+    popover().findByText("Orders").click();
+    getNotebookStep("summarize")
+      .findByText("Pick the metric you want to see")
+      .click();
+    popover().within(() => {
+      cy.findByText("Sum of ...").click();
+      cy.findByText("Total").click();
+    });
+    getNotebookStep("summarize")
+      .findByText("Pick a column to group by")
+      .click();
+    popover().findByText("Created At").click();
+    cy.button("Save").click();
+    modal().button("Save").click();
+    cy.wait("@createCard");
+    queryBuilderMain().findByTestId("run-button").click();
+    cy.wait("@dataset");
+    echartsContainer().within(() => {
+      cy.findByText("Sum of Total").should("be.visible");
+      cy.findByText("Created At").should("be.visible");
+    });
   });
 });
