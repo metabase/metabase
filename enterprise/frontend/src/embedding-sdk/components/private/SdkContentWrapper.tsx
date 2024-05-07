1060973956b85712e55ff15465736928b01aea63
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2024-05-07 22:53:37 +0700

    Support custom font in SDK

--- enterprise/frontend/src/embedding-sdk/components/private/SdkContentWrapper.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/SdkContentWrapper.tsx
@@ -1,12 +1,21 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import type { HTMLAttributes } from "react";
 
 import { getRootStyle } from "metabase/css/core/base.styled";
 import { defaultFontFiles } from "metabase/css/core/fonts.styled";
 import { alpha, color } from "metabase/lib/colors";
+import { useSelector } from "metabase/lib/redux";
 import { aceEditorStyles } from "metabase/query_builder/components/NativeQueryEditor/NativeQueryEditor.styled";
+import { getFontFiles } from "metabase/styled-components/selectors";
 import { saveDomImageStyles } from "metabase/visualizations/lib/save-chart-image";
+import type { FontFile } from "metabase-types/api";
 
-export const SdkContentWrapper = styled.div<{ font: string; baseUrl?: string }>`
+const SdkContentWrapperInner = styled.div<
+  SdkContentWrapperProps & {
+    fontFiles: FontFile[] | null;
+  }
+>`
   --mb-default-font-family: "${({ font }) => font}";
   --mb-color-brand: ${color("brand")};
   --mb-color-brand-alpha-04: ${alpha("brand", 0.04)};
@@ -18,8 +27,39 @@ export const SdkContentWrapper = styled.div<{ font: string; baseUrl?: string }>`
   ${({ theme }) => getRootStyle(theme)}
 
   ${({ baseUrl }) => defaultFontFiles({ baseUrl })}
+  ${({ fontFiles }) =>
+    fontFiles?.map(
+      file => css`
+        @font-face {
+          font-family: "Custom";
+          src: url(${encodeURI(file.src)}) format("${file.fontFormat}");
+          font-weight: ${file.fontWeight};
+          font-style: normal;
+          font-display: swap;
+        }
+      `,
+    )}
 
   svg {
     display: inline;
   }
 `;
+interface SdkContentWrapperProps {
+  font: string;
+  baseUrl?: string;
+}
+export function SdkContentWrapper({
+  font,
+  baseUrl,
+  ...divProps
+}: SdkContentWrapperProps & HTMLAttributes<HTMLDivElement>) {
+  const fontFiles = useSelector(getFontFiles);
+  return (
+    <SdkContentWrapperInner
+      font={font}
+      baseUrl={baseUrl}
+      fontFiles={fontFiles}
+      {...divProps}
+    />
+  );
+}
