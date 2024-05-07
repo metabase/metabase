fc0d335cf409f96d04ce22b4473146be410ca041
Kamil Mielnik <kamil@kamilmielnik.com>
2024-05-07 14:31:03 +0700

    Use models param to speed up the query

--- frontend/src/metabase/common/components/DataPicker/hooks/useAvailableData.ts
+++ frontend/src/metabase/common/components/DataPicker/hooks/useAvailableData.ts
@@ -1,7 +1,7 @@
 import { useSearchQuery } from "metabase/api";
 
 export const useAvailableData = () => {
-  const { data } = useSearchQuery({ limit: 0 });
+  const { data } = useSearchQuery({ models: ["card"], limit: 0 });
   const availableModels = data?.available_models ?? [];
   const hasModels = availableModels.includes("dataset");
   const hasQuestions = availableModels.includes("card");
