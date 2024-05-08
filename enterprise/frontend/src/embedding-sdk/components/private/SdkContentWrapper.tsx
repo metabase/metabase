e678ed6de0e5f4486f7854ce8ea2a21829e33191
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-05-08 22:31:14 +0700

    Support custom font in SDK (#42342)
    * Support custom font in SDK
    
    * Update component after merging master

--- enterprise/frontend/src/embedding-sdk/components/private/SdkContentWrapper.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/SdkContentWrapper.tsx
@@ -1,15 +1,40 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import type { HTMLAttributes } from "react";
 
 import { DEFAULT_FONT } from "embedding-sdk/config";
 import type { EmbeddingTheme } from "embedding-sdk/types/theme/private";
 import { getRootStyle } from "metabase/css/core/base.styled";
 import { defaultFontFiles } from "metabase/css/core/fonts.styled";
 import { alpha, color } from "metabase/lib/colors";
+import { useSelector } from "metabase/lib/redux";
 import { aceEditorStyles } from "metabase/query_builder/components/NativeQueryEditor/NativeQueryEditor.styled";
+import { getFontFiles } from "metabase/styled-components/selectors";
 import { saveDomImageStyles } from "metabase/visualizations/lib/save-chart-image";
+import type { FontFile } from "metabase-types/api";
 
-export const SdkContentWrapper = styled.div<{ baseUrl?: string }>`
+interface SdkContentWrapperProps {
+  baseUrl?: string;
+}
+export function SdkContentWrapper({
+  baseUrl,
+  ...divProps
+}: SdkContentWrapperProps & HTMLAttributes<HTMLDivElement>) {
+  const fontFiles = useSelector(getFontFiles);
+  return (
+    <SdkContentWrapperInner
+      baseUrl={baseUrl}
+      fontFiles={fontFiles}
+      {...divProps}
+    />
+  );
+}
+
+const SdkContentWrapperInner = styled.div<
+  SdkContentWrapperProps & {
+    fontFiles: FontFile[] | null;
+  }
+>`
   --mb-default-font-family: "${({ theme }) => getFontFamily(theme)}";
   --mb-color-brand: ${color("brand")};
   --mb-color-brand-alpha-04: ${alpha("brand", 0.04)};
@@ -22,6 +47,18 @@ export const SdkContentWrapper = styled.div<{ baseUrl?: string }>`
   ${({ theme }) => getWrapperStyle(theme)}
 
   ${({ baseUrl }) => defaultFontFiles({ baseUrl })}
+  ${({ fontFiles }) =>
+    fontFiles?.map(
+      file => css`
+        @font-face {
+          font-family: "Custom";
+          src: url(${encodeURI(file.src)}) format("${file.fontFormat}");
+          font-weight: ${file.fontWeight};
+          font-style: normal;
+          font-display: swap;
+        }
+      `,
+    )}
 
   svg {
     display: inline;
