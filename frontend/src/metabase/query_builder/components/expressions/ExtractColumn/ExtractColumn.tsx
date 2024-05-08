653de372d1052556e94ac86262cb86827380dbcd
Romeo Van Snick <romeo@romeovansnick.be>
2024-05-08 10:30:23 +0200

    Move the extract examples inline

--- frontend/src/metabase/query_builder/components/expressions/ExtractColumn/ExtractColumn.tsx
+++ frontend/src/metabase/query_builder/components/expressions/ExtractColumn/ExtractColumn.tsx
@@ -2,7 +2,7 @@ import { useState, useMemo } from "react";
 import { t } from "ttag";
 
 import { QueryColumnPicker } from "metabase/common/components/QueryColumnPicker";
-import { Text, Box, Stack, Button } from "metabase/ui";
+import { Text, Box, Stack, Button, Flex } from "metabase/ui";
 import * as Lib from "metabase-lib";
 
 import { ExpressionWidgetHeader } from "../ExpressionWidgetHeader";
@@ -180,15 +180,18 @@ function ExtractColumnButton({
       className={styles.button}
       classNames={{
         inner: styles.inner,
+        label: styles.label,
       }}
       onClick={onClick}
     >
-      <Text color="text-dark" className={styles.content} weight="bold" p={0}>
-        {title}
-      </Text>
-      <Text color="text-light" size="sm" className={styles.example}>
-        {example}
-      </Text>
+      <Flex align="center" justify="space-between" gap="1rem">
+        <Text color="text-dark" className={styles.content} weight="bold" p={0}>
+          {title}
+        </Text>
+        <Text color="text-light" size="sm" className={styles.example}>
+          {example}
+        </Text>
+      </Flex>
     </Button>
   );
 }
