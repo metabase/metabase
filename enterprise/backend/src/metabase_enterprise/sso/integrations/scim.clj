aa3b0a2c66bb99439fb7838e44ed6c6607a86e19
Noah Moss <noahbmoss@gmail.com>
2024-05-08 15:21:43 -0400

    some feedback from slack

--- enterprise/backend/src/metabase_enterprise/sso/integrations/scim.clj
+++ enterprise/backend/src/metabase_enterprise/sso/integrations/scim.clj
@@ -1,24 +1,23 @@
 (ns metabase-enterprise.sso.integrations.scim
   (:require
-   [metabase.api.common :as api]
    [metabase.models.api-key :as api-key]
    [toucan2.core :as t2]))
 
-(def ^:private scim-api-key-name "Metabase SCIM API Key (internal)")
+(defn- scim-api-key-name
+  []
+  (format "Metabase SCIM API Key - %s" (random-uuid)))
 
 (defn refresh-scim-api-key!
   "Generates a new SCIM API key and deletes any that already exist."
-  []
+  [user-id]
   (t2/with-transaction [_conn]
     (t2/delete! :model/ApiKey :scope :scim)
     (t2/insert-returning-instance! :model/ApiKey {:user_id       nil
                                                   :scope         :scim
-                                                  :name          scim-api-key-name
+                                                  :name          (scim-api-key-name)
                                                   :unhashed_key  (api-key/generate-key)
-                                                  :creator_id    api/*current-user-id*
-                                                  :updated_by_id api/*current-user-id*})))
+                                                  :creator_id    user-id
+                                                  :updated_by_id user-id})))
 
 (comment
- (require '[metabase.test :as mt])
- (mt/with-current-user 1
-   (refresh-scim-api-key!)))
+ (refresh-scim-api-key! 1))
