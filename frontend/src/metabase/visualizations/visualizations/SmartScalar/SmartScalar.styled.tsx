9c8aa59a1c74201b57681ff4f21c9766fd8093da
Phoomparin Mano <phoomparin@gmail.com>
2024-05-08 16:53:12 +0700

    remove todo comment

--- frontend/src/metabase/visualizations/visualizations/SmartScalar/SmartScalar.styled.tsx
+++ frontend/src/metabase/visualizations/visualizations/SmartScalar/SmartScalar.styled.tsx
@@ -4,14 +4,6 @@ import { Ellipsified } from "metabase/core/components/Ellipsified";
 import { space } from "metabase/styled-components/theme";
 import { Icon } from "metabase/ui";
 
-/**
- * TODO: Embedding Dark Mode
- * PreviousValueNumber is lighten(text-medium, 0.45)
- * PreviousValueDetails is lighten(text-light, 0.25)
- * Separator is lighten(text-medium, 0.15)
- * Variation when changeColor empty is lighten(text-medium, 0.3)
- */
-
 export const VariationIcon = styled(Icon)`
   display: flex;
   align-items: center;
