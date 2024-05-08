38ca253a03842654cb945dfc69a64b68dd173fa8
Cam Saul <github@camsaul.com>
2024-05-08 18:36:04 +0000

    Check ns shapes

--- .clj-kondo/hooks/clojure/core.clj
+++ .clj-kondo/hooks/clojure/core.clj
@@ -205,21 +205,6 @@
 
  (do (non-thread-safe-form-should-end-with-exclamation* (hooks/parse-string form)) nil))
 
-(defn- ns-form-node->ns-symb [ns-form-node]
-  (some-> (some (fn [node]
-                  (when (and (hooks/token-node? node)
-                             (not= (hooks/sexpr node) 'ns))
-                    node))
-                (:children ns-form-node))
-          hooks/sexpr))
-
-(defn- module
-  "E.g.
-
-    (module 'metabase.qp.middleware.wow) => 'metabase.qp"
-  [ns-symb]
-  (some-> (re-find #"^metabase\.[^.]+" (str ns-symb)) symbol))
-
 (defn- ns-form-node->require-node [ns-form-node]
   (some (fn [node]
           (when (and (hooks/list-node? node)
@@ -228,6 +213,23 @@
                             (= (hooks/sexpr first-child) :require))))
             node))
         (:children ns-form-node)))
+
+(defn- lint-require-shapes [ns-form-node]
+  (doseq [node (-> ns-form-node
+                   ns-form-node->require-node
+                   :children
+                   rest)]
+    (println "(pr-str (hooks/sexpr node)):" (pr-str (hooks/sexpr node))) ; NOCOMMIT
+    (cond
+      (not (hooks/vector-node? node))
+      (hooks/reg-finding! (assoc (meta node)
+                                 :message "All :required namespaces should be wrapped in vectors [:metabase/require-shape-checker]"
+                                 :type    :metabase/require-shape-checker))
+
+      (hooks/vector-node? (second (:children node)))
+      (hooks/reg-finding! (assoc (meta node)
+                                 :message "Don't use prefix forms inside :require [:metabase/require-shape-checker]"
+                                 :type    :metabase/require-shape-checker)))))
 
 (defn- require-node->namespace-symb-nodes [require-node]
   (let [[_ns & args] (:children require-node)]
@@ -244,6 +246,21 @@
                     :else
                     (printf "Don't know how to figure out what namespace is being required in %s\n" (pr-str node)))))
           args)))
+
+(defn- ns-form-node->ns-symb [ns-form-node]
+  (some-> (some (fn [node]
+                  (when (and (hooks/token-node? node)
+                             (not= (hooks/sexpr node) 'ns))
+                    node))
+                (:children ns-form-node))
+          hooks/sexpr))
+
+(defn- module
+  "E.g.
+
+    (module 'metabase.qp.middleware.wow) => 'metabase.qp"
+  [ns-symb]
+  (some-> (re-find #"^metabase\.[^.]+" (str ns-symb)) symbol))
 
 (defn- ignored-namespace? [ns-symb config]
   (some
@@ -289,5 +306,6 @@
 
 
 (defn lint-ns [x]
+  (lint-require-shapes (:node x))
   (lint-modules (:node x) (get-in x [:config :linters :metabase/ns-module-checker]))
   x)
