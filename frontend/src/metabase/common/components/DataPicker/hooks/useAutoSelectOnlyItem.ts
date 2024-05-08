b5f2b2ec8aad5cc2b035bc1154b04791901daecf
Kamil Mielnik <kamil@kamilmielnik.com>
2024-05-08 14:01:43 +0700

    Table does not appear selected in single-schema dbs (#42336)
    * Convert useAutoSelectOnlyItem arguments to param object
    
    * Disable auto-selection when there already is a selection

--- frontend/src/metabase/common/components/DataPicker/hooks/useAutoSelectOnlyItem.ts
+++ frontend/src/metabase/common/components/DataPicker/hooks/useAutoSelectOnlyItem.ts
@@ -1,16 +1,23 @@
 import { useEffect } from "react";
 import { useLatest } from "react-use";
 
+interface Props<Item> {
+  disabled: boolean;
+  items: Item[] | undefined;
+  onChange: (item: Item) => void;
+}
+
 /**
  * Automatically selects the only item on the list.
  * Does nothing if there's 0 items or more than 1.
  *
  * @returns true when there's only 1 item.
  */
-export const useAutoSelectOnlyItem = <Item>(
-  items: Item[] | undefined,
-  onChange: (item: Item) => void,
-): boolean => {
+export const useAutoSelectOnlyItem = <Item>({
+  disabled,
+  items,
+  onChange,
+}: Props<Item>): boolean => {
   // use ref to avoid triggering the effect too often
   const onChangeRef = useLatest(onChange);
   const hasOnly1Item = items?.length === 1;
@@ -18,11 +25,11 @@ export const useAutoSelectOnlyItem = <Item>(
 
   useEffect(
     function autoSelectOnlyItem() {
-      if (onlyItem) {
+      if (!disabled && onlyItem) {
         onChangeRef.current(onlyItem);
       }
     },
-    [onlyItem, onChangeRef],
+    [disabled, onlyItem, onChangeRef],
   );
 
   // let consumer component know when to not render itself
