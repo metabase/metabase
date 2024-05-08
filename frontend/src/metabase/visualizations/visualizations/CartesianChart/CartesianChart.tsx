cf46f8e131754ab4fbf4673e2cc4bd15afbd62be
Emmad Usmani <emmadusmani@berkeley.edu>
2024-05-08 11:32:19 -0700

    refactor legend components to use items prop

--- frontend/src/metabase/visualizations/visualizations/CartesianChart/CartesianChart.tsx
+++ frontend/src/metabase/visualizations/visualizations/CartesianChart/CartesianChart.tsx
@@ -106,8 +106,7 @@ function _CartesianChart(props: VisualizationProps) {
       <CartesianChartLegendLayout
         isReversed={settings["legend.is_reversed"]}
         hasLegend={hasLegend}
-        labels={legendItems.map(item => item.name)}
-        colors={legendItems.map(item => item.color)}
+        items={legendItems}
         actionButtons={!hasTitle ? actionButtons : undefined}
         hovered={hovered}
         isFullscreen={isFullscreen}
