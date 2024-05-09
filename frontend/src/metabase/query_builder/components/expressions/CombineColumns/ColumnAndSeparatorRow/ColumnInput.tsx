1c80d7346563121161a8b7886d9d590f293caf26
Romeo Van Snick <romeo@romeovansnick.be>
2024-05-09 09:35:40 +0200

    Set up sequenced close handler in combine columns

--- frontend/src/metabase/query_builder/components/expressions/CombineColumns/ColumnAndSeparatorRow/ColumnInput.tsx
+++ frontend/src/metabase/query_builder/components/expressions/CombineColumns/ColumnAndSeparatorRow/ColumnInput.tsx
@@ -125,7 +125,9 @@ export function ColumnInput({
             {text}
           </Button>
         </Popover.Target>
-        <Popover.Dropdown>{dropdown}</Popover.Dropdown>
+        <Popover.Dropdown setupSequencedCloseHandler>
+          {dropdown}
+        </Popover.Dropdown>
       </Popover>
     </Input.Wrapper>
   );
