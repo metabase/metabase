aeb32fa053f48495c0ac9d930162b6976c10aad0
Anton Kulyk <kuliks.anton@gmail.com>
2024-05-09 15:32:32 +0100

    Basic select question + query + combine rows flow

--- frontend/src/metabase/visualizer/Visualizer.tsx
+++ frontend/src/metabase/visualizer/Visualizer.tsx
@@ -1,12 +1,58 @@
+import { useEffect, useMemo, useState } from "react";
+import { useMap, usePrevious } from "react-use";
 import _ from "underscore";
 
+import { skipToken, useCardQueryQuery } from "metabase/api";
 import { QuestionPicker } from "metabase/dashboard/components/QuestionPicker";
-import { Card, Grid } from "metabase/ui";
-import type { CardId } from "metabase-types/api";
+import { Card, Center, Grid, Loader } from "metabase/ui";
+import type { CardId, Dataset, RowValues } from "metabase-types/api";
 
 export function Visualizer() {
-  const handleQuestionSelected = (_questionId: CardId) => {
-    // wip
+  const [cardDataMap, cardDataMapActions] = useMap<Record<CardId, Dataset>>({});
+  const [fetchedCardId, setFetchedCardId] = useState<CardId | null>(null);
+
+  const cardQuery = useCardQueryQuery(fetchedCardId ?? skipToken);
+  const wasFetching = usePrevious(cardQuery.isFetching);
+
+  useEffect(() => {
+    if (
+      fetchedCardId &&
+      !cardDataMap[fetchedCardId] &&
+      cardQuery.data &&
+      !cardQuery.isFetching &&
+      wasFetching
+    ) {
+      cardDataMapActions.set(fetchedCardId, cardQuery.data);
+      setFetchedCardId(null);
+    }
+  }, [fetchedCardId, cardDataMap, cardDataMapActions, cardQuery, wasFetching]);
+
+  const combinedRows = useMemo(() => {
+    const rows: RowValues[] = [];
+
+    Object.values(cardDataMap).map(dataset => {
+      const [lastRow] = dataset.data.rows.toReversed();
+      if (lastRow) {
+        rows.push(lastRow);
+      }
+    });
+
+    return rows;
+  }, [cardDataMap]);
+
+  const isLoading =
+    cardQuery.isFetching || (fetchedCardId && !cardDataMap[fetchedCardId]);
+
+  const handleQuestionSelected = (questionId: CardId) => {
+    if (isLoading) {
+      return;
+    }
+    if (cardDataMap[questionId]) {
+      setFetchedCardId(null);
+      cardDataMapActions.remove(questionId);
+    } else {
+      setFetchedCardId(questionId);
+    }
   };
 
   return (
@@ -16,8 +62,26 @@ export function Visualizer() {
       </Grid.Col>
       <Grid.Col span={9}>
         <Card withBorder w="100%" h="100%">
-          Funnel visualizer here! Expected data shape: 1 numeric column + 1
-          string column
+          {isLoading ? (
+            <Center w="100%" h="100%">
+              <Loader size="xl" />
+            </Center>
+          ) : (
+            <div>
+              {combinedRows.map(
+                (row, index) =>
+                  row && (
+                    <div key={index} style={{ marginTop: "0.5rem" }}>
+                      {row.map((value, index) => (
+                        <span key={index} style={{ padding: "0.25rem" }}>
+                          {value}
+                        </span>
+                      ))}
+                    </div>
+                  ),
+              )}
+            </div>
+          )}
         </Card>
       </Grid.Col>
     </Grid>
