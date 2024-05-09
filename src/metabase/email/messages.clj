d16edffc2eb2d3e5603f491ccb3ef4edbe2a6657
Adam James <adam.vermeer2@gmail.com>
2024-05-09 11:01:31 -0700

    Don't use the slugify util for filenames

--- src/metabase/email/messages.clj
+++ src/metabase/email/messages.clj
@@ -380,7 +380,7 @@
     {:type         :attachment
      :content-type content-type
      :file-name    (format "%s_%s.%s"
-                           (or (u/slugify card-name) "query_result")
+                           (or card-name "query_result")
                            (u.date/format (t/zoned-date-time))
                            (name export-type))
      :content      (-> attachment-file .toURI .toURL)
