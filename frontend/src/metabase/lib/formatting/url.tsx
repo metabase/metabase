bbdcb28b02f602a48d33209e2319301cbbe33f04
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2024-05-08 22:50:29 -0300

    allow using urls from other columns in link templates (#42438)

--- frontend/src/metabase/lib/formatting/url.tsx
+++ frontend/src/metabase/lib/formatting/url.tsx
@@ -12,7 +12,7 @@ function isSafeProtocol(protocol: string) {
   );
 }
 
-function isDefaultLinkProtocol(protocol: string) {
+export function isDefaultLinkProtocol(protocol: string) {
   return (
     protocol === "http:" || protocol === "https:" || protocol === "mailto:"
   );
