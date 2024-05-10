0442440737a61d4c4ba7ceac2fb50844963e7d23
Kamil Mielnik <kamil@kamilmielnik.com>
2024-05-10 16:38:43 +0700

    Add a hack to populate query metadata with info that table is a model

--- frontend/src/metabase/query_builder/components/notebook/steps/JoinStep/JoinTablePicker/JoinTablePicker.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/JoinStep/JoinTablePicker/JoinTablePicker.tsx
@@ -7,10 +7,12 @@ import {
   DataPickerModal,
   dataPickerValueFromJoinable,
 } from "metabase/common/components/DataPicker";
+import Questions from "metabase/entities/questions";
 import Tables from "metabase/entities/tables";
 import { useDispatch } from "metabase/lib/redux";
 import { Icon, Popover, Tooltip } from "metabase/ui";
 import * as Lib from "metabase-lib";
+import { getQuestionIdFromVirtualTableId } from "metabase-lib/v1/metadata/utils/saved-questions";
 import type { TableId } from "metabase-types/api";
 
 import { NotebookCellItem } from "../../../NotebookCell";
@@ -53,6 +55,15 @@ export function JoinTablePicker({
   const handleTableChange = async (tableId: TableId) => {
     // we need to populate query metadata with selected table
     await dispatch(Tables.actions.fetchMetadata({ id: tableId }));
+
+    if (typeof tableId === "string") {
+      await dispatch(
+        Questions.actions.fetch({
+          id: getQuestionIdFromVirtualTableId(tableId),
+        }),
+      );
+    }
+
     onChangeRef.current?.(Lib.tableOrCardMetadata(queryRef.current, tableId));
   };
 
