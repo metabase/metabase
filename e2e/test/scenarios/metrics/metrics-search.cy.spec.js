899a2d8b68ce376caa01f430274587022195ae19
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-13 16:27:50 -0400

    Fix tests

--- e2e/test/scenarios/metrics/metrics-search.cy.spec.js
+++ e2e/test/scenarios/metrics/metrics-search.cy.spec.js
@@ -1,9 +1,13 @@
+import _ from "underscore";
+
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import {
   appBar,
   createQuestion,
+  describeEE,
   navigationSidebar,
   restore,
+  setTokenFeatures,
   visitMetric,
 } from "e2e/support/helpers";
 
@@ -19,28 +23,64 @@ const ORDERS_SCALAR_METRIC = {
   display: "scalar",
 };
 
-describe("scenarios > metrics > question", () => {
+describe("scenarios > metrics > search", () => {
   beforeEach(() => {
     restore();
     cy.signInAsNormalUser();
+    cy.intercept("POST", "/api/dataset").as("dataset");
   });
 
-  it("should see metrics in recent items", () => {
-    createQuestion(ORDERS_SCALAR_METRIC).then(({ body: card }) =>
-      visitMetric(card.id),
-    );
-
-    cy.log("Global search");
+  it("should see metrics in recent items in global search", () => {
+    createQuestion(ORDERS_SCALAR_METRIC).then(({ body: card }) => {
+      visitMetric(card.id);
+      cy.wait("@dataset");
+    });
+    navigationSidebar().findByText("Home").click();
     appBar().findByPlaceholderText("Search…").click();
     cy.findByTestId("search-results-floating-container")
       .findByText(ORDERS_SCALAR_METRIC.name)
-      .should("be.visible");
+      .click();
+    cy.wait("@dataset");
+    cy.findByTestId("scalar-container").should("be.visible");
+  });
 
-    cy.log("Home page");
+  it("should see metrics in recent items on the home page", () => {
+    createQuestion(ORDERS_SCALAR_METRIC).then(({ body: card }) => {
+      visitMetric(card.id);
+      cy.wait("@dataset");
+    });
     navigationSidebar().findByText("Home").click();
     cy.findByTestId("home-page").within(() => {
       cy.findByText("Pick up where you left off").should("be.visible");
-      cy.findByText(ORDERS_SCALAR_METRIC.name).should("be.visible");
+      cy.findByText(ORDERS_SCALAR_METRIC.name).click();
+      cy.wait("@dataset");
     });
+    cy.findByTestId("scalar-container").should("be.visible");
+  });
+});
+
+describeEE("scenarios > metrics > search", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+    cy.intercept("POST", "/api/dataset").as("dataset");
+    setTokenFeatures("all");
+  });
+
+  it.skip("should see metrics in popular items on the homepage", () => {
+    createQuestion({ ...ORDERS_SCALAR_METRIC, type: "question" }).then(
+      ({ body: card }) => {
+        visitMetric(card.id);
+        cy.wait("@dataset");
+      },
+    );
+    cy.signInAsNormalUser();
+    cy.visit("/");
+    cy.findByTestId("home-page").within(() => {
+      cy.findByText("Here are some popular questions").should("be.visible");
+      cy.findByText(ORDERS_SCALAR_METRIC.name).click();
+      cy.wait("@dataset");
+    });
+    cy.findByTestId("scalar-container").should("be.visible");
   });
 });
