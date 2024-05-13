87853210df7e59c5e410d6db9432a4d74d8efda5
Romeo Van Snick <romeo@romeovansnick.be>
2024-05-13 09:44:00 +0200

    Render item icon inline with column icon

--- frontend/src/metabase/components/MetadataInfo/InfoIcon/InfoIcon.styled.tsx
+++ frontend/src/metabase/components/MetadataInfo/InfoIcon/InfoIcon.styled.tsx
@@ -3,18 +3,31 @@ import styled from "@emotion/styled";
 import { Icon } from "metabase/ui";
 
 export const PopoverHoverTarget = styled(Icon)<{ hasDescription: boolean }>`
-  padding: 0.7em 0.65em;
-  visibility: hidden;
   flex-shrink: 0;
-  opacity: ${props => (props.hasDescription ? 0.6 : 0.3)};
+  display: none;
 
-  &[aria-expanded="true"] {
+  opacity: ${props => (props.hasDescription ? 0.75 : 0.5)};
+
+  [aria-expanded="true"] & {
+    display: block;
     opacity: 1;
+  }
+`;
+
+export const PopoverDefaultIcon = styled(Icon)`
+  display: block;
+
+  [aria-expanded="true"] & {
+    display: none;
   }
 `;
 
 export const HoverParent = styled.div`
   &:hover ${PopoverHoverTarget} {
-    visibility: visible;
+    display: block;
+  }
+
+  &:hover ${PopoverDefaultIcon} {
+    display: none;
   }
 `;
