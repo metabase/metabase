3d376d10b7fb0c924a45b50254153e1f4f25196f
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-13 15:20:17 -0400

    Fix tests

--- e2e/test/scenarios/metrics/metrics-question.cy.spec.js
+++ e2e/test/scenarios/metrics/metrics-question.cy.spec.js
@@ -1,12 +1,14 @@
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import {
+  assertQueryBuilderRowCount,
   createQuestion,
   modal,
+  popover,
   restore,
   visitMetric,
 } from "e2e/support/helpers";
 
-const { ORDERS_ID } = SAMPLE_DATABASE;
+const { ORDERS_ID, ORDERS } = SAMPLE_DATABASE;
 
 const ORDERS_SCALAR_METRIC = {
   name: "Count of orders",
@@ -16,6 +18,23 @@ const ORDERS_SCALAR_METRIC = {
     aggregation: [["count"]],
   },
   display: "scalar",
+};
+
+const ORDERS_TIMESERIES_METRIC = {
+  name: "Count of orders over time",
+  type: "metric",
+  query: {
+    "source-table": ORDERS_ID,
+    aggregation: [["count"]],
+    breakout: [
+      [
+        "field",
+        ORDERS.CREATED_AT,
+        { "base-type": "type/DateTime", "temporal-unit": "month" },
+      ],
+    ],
+  },
+  display: "line",
 };
 
 describe("scenarios > metrics > question", () => {
@@ -37,5 +56,20 @@ describe("scenarios > metrics > question", () => {
     cy.findByTestId("scalar-container")
       .findByText("4,939")
       .should("be.visible");
+  });
+
+  it("should be able to change the temporal unit when consuming a timeseries metric", () => {
+    createQuestion(ORDERS_TIMESERIES_METRIC).then(({ body: card }) =>
+      visitMetric(card.id),
+    );
+    assertQueryBuilderRowCount(49);
+    cy.findByTestId("qb-header-action-panel").button("Summarize").click();
+    cy.findByTestId("sidebar-content")
+      .findByTestId("pinned-dimensions")
+      .findByLabelText("Created At")
+      .findByText("by month")
+      .click();
+    popover().findByText("Year").click();
+    assertQueryBuilderRowCount(5);
   });
 });
