8e92422f323507a6e154dba51caa4adfb94e2e37
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-05-13 17:09:55 -0300

    replace search bar with command palette toggle

--- e2e/support/helpers/e2e-command-palette-helpers.js
+++ e2e/support/helpers/e2e-command-palette-helpers.js
@@ -5,5 +5,19 @@ export const pressPageUp = () => cy.get("body").type("{pageup}");
 export const pressPageDown = () => cy.get("body").type("{pagedown}");
 export const pressHome = () => cy.get("body").type("{home}");
 export const pressEnd = () => cy.get("body").type("{end}");
-export const commandPaletteSearch = () =>
-  cy.findByPlaceholderText("Jump to...");
+export const commandPaletteInput = () => cy.findByPlaceholderText("Jump to...");
+
+export const commandPaletteSearch = (query, viewAll = true) => {
+  cy.intercept("GET", "/api/search?q=*").as("paletteSearch");
+  cy.findByTestId("app-bar")
+    .findByRole("button", { name: /Search/ })
+    .click();
+  commandPaletteInput().type(query);
+  cy.wait("@paletteSearch");
+
+  if (viewAll) {
+    commandPalette()
+      .findByRole("option", { name: /View and filter/ })
+      .click();
+  }
+};
