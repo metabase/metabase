c559e5fc3f7b70ba2e2f8caca91ee745270e578d
Phoomparin Mano <phoomparin@gmail.com>
2024-05-13 22:30:38 +0700

    remove unused use-embedding-theme hook

--- frontend/src/metabase/hooks/use-embedding-theme.ts
+++
@@ -1,8 +0,0 @@
-import type { EmbeddingTheme } from "embedding-sdk/types/theme/private";
-import { useMantineTheme } from "metabase/ui";
-
-/**
- * Get the Mantine theme from context.
- * The embedding theme options are available in `theme.other`
- */
-export const useEmbeddingTheme: () => EmbeddingTheme = useMantineTheme;
