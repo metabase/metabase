db414f66d868a2f019c2e1288bfc59d4e6cdc2da
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-05-14 13:06:42 -0300

    e2e test part one

--- e2e/support/helpers/e2e-command-palette-helpers.js
+++ e2e/support/helpers/e2e-command-palette-helpers.js
@@ -1,5 +1,7 @@
 export const commandPalette = () => cy.findByTestId("command-palette");
 export const openCommandPalette = () => cy.get("body").type("{ctrl+k}{cmd+k}");
+export const commandPaletteButton = () =>
+  cy.findByTestId("app-bar").findByRole("button", { name: /Search/ });
 export const closeCommandPalette = () => cy.get("body").type("{esc}");
 export const pressPageUp = () => cy.get("body").type("{pageup}");
 export const pressPageDown = () => cy.get("body").type("{pagedown}");
