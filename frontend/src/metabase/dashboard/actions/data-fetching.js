c520460595fb322d664b978fabdcada6ebfcd2ab
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-14 15:11:34 -0400

    Fix metadata loading issues after tabs #42655

--- frontend/src/metabase/dashboard/actions/data-fetching.js
+++ frontend/src/metabase/dashboard/actions/data-fetching.js
@@ -37,6 +37,7 @@ import {
   getDashCardById,
   getSelectedTabId,
   getQuestions,
+  getInitialSelectedTabId,
 } from "../selectors";
 import {
   expandInlineDashboard,
@@ -227,10 +228,11 @@ export const fetchDashboard = createAsyncThunk(
       fetchDashboardCancellation = null;
 
       if (dashboardType === "normal" || dashboardType === "transient") {
-        const selectedTabId = getSelectedTabId(getState());
+        const selectedTabId =
+          getSelectedTabId(getState()) ?? getInitialSelectedTabId(result);
 
         const cards =
-          selectedTabId === undefined
+          selectedTabId == null
             ? result.dashcards
             : result.dashcards.filter(
                 c => c.dashboard_tab_id === selectedTabId,
--- frontend/src/metabase/dashboard/actions/data-fetching.unit.spec.js
+++ frontend/src/metabase/dashboard/actions/data-fetching.unit.spec.js
@@ -1,44 +1,160 @@
-import { configureStore } from "@reduxjs/toolkit";
+import fetchMock from "fetch-mock";
 
-import { setupDashboardEndpoints } from "__support__/server-mocks";
+import { getStore } from "__support__/entities-store";
+import {
+  setupDashboardsEndpoints,
+  setupDatabaseEndpoints,
+} from "__support__/server-mocks";
 import { createMockEntitiesState } from "__support__/store";
 import {
+  createMockCard,
   createMockDashboard,
-  createMockDatabase,
+  createMockDashboardCard,
+  createMockDashboardTab,
   createMockSettings,
+  createMockStructuredDatasetQuery,
 } from "metabase-types/api/mocks";
+import {
+  createSampleDatabase,
+  ORDERS_ID,
+  PRODUCTS_ID,
+} from "metabase-types/api/mocks/presets";
 import { createMockDashboardState } from "metabase-types/store/mocks";
 
 import { dashboardReducers } from "../reducers";
 
 import { fetchDashboard } from "./data-fetching";
 
+function setup({ dashboards = [] }) {
+  const database = createSampleDatabase();
+  const state = {
+    dashboard: createMockDashboardState(),
+    entities: createMockEntitiesState({
+      databases: [database],
+    }),
+    settings: createMockSettings(),
+  };
+
+  const store = getStore(
+    {
+      dashboard: dashboardReducers,
+      entities: (state = {}) => state,
+      settings: (state = {}) => state,
+    },
+    state,
+  );
+
+  setupDatabaseEndpoints(database);
+  setupDashboardsEndpoints(dashboards);
+
+  return store;
+}
+
 describe("fetchDashboard", () => {
-  let store;
-
-  beforeEach(() => {
-    const state = {
-      dashboard: createMockDashboardState(),
-      entities: createMockEntitiesState({
-        databases: [createMockDatabase()],
-      }),
-      settings: createMockSettings(),
-    };
-
-    store = configureStore({
-      reducer: {
-        dashboard: dashboardReducers,
-        entities: (state = {}) => state,
-        settings: (state = {}) => state,
-      },
-      preloadedState: state,
+  it("should fetch metadata for all cards when there are no tabs", async () => {
+    const dashboard = createMockDashboard({
+      dashcards: [
+        createMockDashboardCard({
+          card: createMockCard({
+            dataset_query: createMockStructuredDatasetQuery({
+              query: {
+                "source-table": PRODUCTS_ID,
+              },
+            }),
+          }),
+        }),
+        createMockDashboardCard({
+          card: createMockCard({
+            dataset_query: createMockStructuredDatasetQuery({
+              query: {
+                "source-table": ORDERS_ID,
+              },
+            }),
+          }),
+        }),
+      ],
+    });
+    const store = setup({
+      dashboards: [dashboard],
     });
 
-    setupDashboardEndpoints(createMockDashboard({ id: 1 }));
-    setupDashboardEndpoints(createMockDashboard({ id: 2 }));
+    await store.dispatch(
+      fetchDashboard({
+        dashId: dashboard.id,
+        queryParams: {},
+        options: {},
+      }),
+    );
+
+    expect(
+      fetchMock.calls(`path:/api/table/${PRODUCTS_ID}/query_metadata`),
+    ).toHaveLength(1);
+    expect(
+      fetchMock.calls(`path:/api/table/${ORDERS_ID}/query_metadata`),
+    ).toHaveLength(1);
+  });
+
+  it("should fetch metadata for cards on the first tab when there are tabs", async () => {
+    const dashboard = createMockDashboard({
+      dashcards: [
+        createMockDashboardCard({
+          card: createMockCard({
+            dataset_query: createMockStructuredDatasetQuery({
+              query: {
+                "source-table": PRODUCTS_ID,
+              },
+            }),
+          }),
+          dashboard_tab_id: 1,
+        }),
+        createMockDashboardCard({
+          card: createMockCard({
+            dataset_query: createMockStructuredDatasetQuery({
+              query: {
+                "source-table": ORDERS_ID,
+              },
+            }),
+          }),
+          dashboard_tab_id: 2,
+        }),
+      ],
+      tabs: [
+        createMockDashboardTab({
+          id: 1,
+        }),
+        createMockDashboardTab({
+          id: 2,
+        }),
+      ],
+    });
+    const store = setup({
+      dashboards: [dashboard],
+    });
+
+    await store.dispatch(
+      fetchDashboard({
+        dashId: dashboard.id,
+        queryParams: {},
+        options: {},
+      }),
+    );
+
+    expect(
+      fetchMock.calls(`path:/api/table/${PRODUCTS_ID}/query_metadata`),
+    ).toHaveLength(1);
+    expect(
+      fetchMock.calls(`path:/api/table/${ORDERS_ID}/query_metadata`),
+    ).toHaveLength(0);
   });
 
   it("should cancel previous dashboard fetch when a new one is initiated (metabase#35959)", async () => {
+    const store = setup({
+      dashboards: [
+        createMockDashboard({ id: 1 }),
+        createMockDashboard({ id: 2 }),
+      ],
+    });
+
     const firstFetch = store.dispatch(
       fetchDashboard({
         dashId: 1,
