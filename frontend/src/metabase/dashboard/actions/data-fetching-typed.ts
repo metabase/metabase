4adec6a12084b17d881e238ce5a54c4ba87d0c26
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-14 13:28:15 -0400

    Fix initial metadata loading for dashboard with tabs

--- frontend/src/metabase/dashboard/actions/data-fetching-typed.ts
+++ frontend/src/metabase/dashboard/actions/data-fetching-typed.ts
@@ -4,6 +4,7 @@ import { loadMetadataForDashboard } from "metabase/dashboard/actions/metadata";
 import {
   getDashboardById,
   getDashCardById,
+  getInitialSelectedTabId,
   getParameterValues,
   getQuestions,
   getSelectedTabId,
@@ -126,7 +127,8 @@ export const fetchDashboard = createAsyncThunk(
       fetchDashboardCancellation = null;
 
       if (dashboardType === "normal" || dashboardType === "transient") {
-        const selectedTabId = getSelectedTabId(getState());
+        const selectedTabId =
+          getSelectedTabId(getState()) ?? getInitialSelectedTabId(result);
 
         const cards =
           selectedTabId === undefined
