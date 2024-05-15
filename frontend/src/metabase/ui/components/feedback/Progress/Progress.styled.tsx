31bc854ad1ae042998bd8ffafb6a0560a916dab6
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-05-15 16:08:57 -0300

    update progress styling

--- frontend/src/metabase/ui/components/feedback/Progress/Progress.styled.tsx
+++ frontend/src/metabase/ui/components/feedback/Progress/Progress.styled.tsx
@@ -2,8 +2,15 @@ import type { MantineThemeOverride } from "@mantine/core";
 
 export const getProgressOverrides = (): MantineThemeOverride["components"] => ({
   Progress: {
+    styles: theme => {
+      return {
+        root: {
+          border: `1px solid ${theme.fn.themeColor("brand")}`,
+        },
+      };
+    },
     defaultProps: {
-      animate: true,
+      size: 10,
     },
   },
 });
