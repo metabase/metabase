0dde5a67d7877405feff9ce061a968567a4ad856
Cal Herries <calherries@gmail.com>
2024-05-15 11:43:36 -0600

    More upload test changes for clickhouse (#42531)

--- src/metabase/upload.clj
+++ src/metabase/upload.clj
@@ -110,10 +110,14 @@
   (let [external-type (keyword "metabase.upload" (name column-type))]
     (driver/upload-type->database-type driver external-type)))
 
+(defn- defaulting-database-type [driver upload-type]
+  (or (driver/upload-type->database-type driver upload-type)
+      (driver/upload-type->database-type driver ::varchar-255)))
+
 (defn- column-definitions
   "Returns a map of column-name -> column-definition from a map of column-name -> upload-type."
   [driver col->upload-type]
-  (update-vals col->upload-type (partial database-type driver)))
+  (update-vals col->upload-type (partial defaulting-database-type driver)))
 
 (defn current-database
   "The database being used for uploads (as per the `uploads-database-id` setting)."
