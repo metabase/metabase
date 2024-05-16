c4a2e45e9ac76d669513720e52f0f2ca1d78f2f8
Ryan Laurie <iethree@gmail.com>
2024-05-16 10:31:26 -0600

    Hosting Upsell

---
+++ frontend/src/metabase/admin/upsells/UpsellHosting.tsx
@@ -0,0 +1,30 @@
+import { t, jt } from "ttag";
+
+const RocketGlobeIllustrationSrc = "app/assets/img/rocket-globe.svg";
+import { useSelector } from "metabase/lib/redux";
+import { getIsHosted } from "metabase/setup/selectors";
+
+import { UpsellCard } from "./components";
+
+export const UpsellHosting = ({ source }: { source: string }) => {
+  const isHosted = useSelector(getIsHosted);
+
+  if (isHosted) {
+    return null;
+  }
+
+  return (
+    <UpsellCard
+      title={t`Minimize maintenance`}
+      campaign="hosting"
+      buttonText={t`Learn more`}
+      buttonLink="https://www.metabase.com/cloud"
+      illustrationSrc={RocketGlobeIllustrationSrc}
+      source={source}
+    >
+      {jt`${(
+        <strong>{t`Migrate to Metabase cloud`}</strong>
+      )} for fast, reliable, and secure deployment.`}
+    </UpsellCard>
+  );
+};
