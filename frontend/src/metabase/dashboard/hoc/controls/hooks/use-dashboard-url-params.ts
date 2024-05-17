d96a5d30e90d72bd7c606bcdba4f56852f1d7463
Oisin Coveney <oisin@metabase.com>
2024-05-17 13:53:40 +0300

    Modify dashboard url params to ensure that we keep hash options that haven't changed

--- frontend/src/metabase/dashboard/hoc/controls/hooks/use-dashboard-url-params.ts
+++ frontend/src/metabase/dashboard/hoc/controls/hooks/use-dashboard-url-params.ts
@@ -1,7 +1,7 @@
 import type { Location } from "history";
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { replace } from "react-router-redux";
-import { useMount, usePrevious } from "react-use";
+import { useMount, usePrevious, useUpdateEffect } from "react-use";
 import { isEqual } from "underscore";
 
 import { useDashboardDisplayOptions } from "metabase/dashboard/hoc/controls/hooks/use-dashboard-display-options";
@@ -9,11 +9,11 @@ import type { DashboardDisplayOptionControls } from "metabase/dashboard/hoc/cont
 import type { DashboardUrlHashOptions } from "metabase/dashboard/hoc/controls/types/hash-options";
 import { parseHashOptions, stringifyHashOptions } from "metabase/lib/browser";
 import { useDispatch } from "metabase/lib/redux";
-import { isNotFalsy } from "metabase/lib/types";
+import { isNullOrUndefined } from "metabase/lib/types";
 
 const removeEmptyOptions = (obj: Record<string, unknown>) => {
   return Object.fromEntries(
-    Object.entries(obj).filter(([_, v]) => isNotFalsy(v)),
+    Object.entries(obj).filter(([_, v]) => !isNullOrUndefined(v)),
   );
 };
 
@@ -57,66 +57,67 @@ export const useDashboardUrlParams = ({
     ) as DashboardUrlHashOptions;
   }, [location.hash]);
 
-  const previousHashOptions = usePrevious(hashOptions);
+  const stateOptions = useMemo(() => {
+    return removeEmptyOptions({
+      fullscreen: isFullscreen,
+      theme,
+      hide_parameters: hideParameters,
+      refresh: refreshPeriod,
+      titled,
+      bordered,
+      hide_download_button: hideDownloadButton,
+    }) as DashboardUrlHashOptions;
+  }, [
+    bordered,
+    hideDownloadButton,
+    hideParameters,
+    isFullscreen,
+    refreshPeriod,
+    theme,
+    titled,
+  ]);
 
-  const stateOptions: DashboardUrlHashOptions = useMemo(
-    () =>
-      removeEmptyOptions({
-        fullscreen: isFullscreen,
-        theme,
-        hide_parameters: hideParameters,
-        refresh: refreshPeriod,
-      }),
-    [hideParameters, isFullscreen, refreshPeriod, theme],
-  );
+  const prevStateOptions = usePrevious(stateOptions);
 
   const loadDashboardParams = useCallback(() => {
     onFullscreenChange(hashOptions.fullscreen || false);
     setTheme(hashOptions.theme || null);
     setHideParameters(hashOptions.hide_parameters || null);
     onRefreshPeriodChange(hashOptions.refresh || null);
+    setTitled(hashOptions.titled ?? false);
+    setBordered(hashOptions.bordered ?? false);
+    setHideDownloadButton(hashOptions.hide_download_button ?? false);
   }, [
-    hashOptions,
-    setHideParameters,
     onFullscreenChange,
-    onRefreshPeriodChange,
+    hashOptions.fullscreen,
+    hashOptions.theme,
+    hashOptions.hide_parameters,
+    hashOptions.refresh,
+    hashOptions.titled,
+    hashOptions.bordered,
+    hashOptions.hide_download_button,
     setTheme,
+    setHideParameters,
+    onRefreshPeriodChange,
+    setTitled,
+    setBordered,
+    setHideDownloadButton,
   ]);
 
   useMount(() => {
     loadDashboardParams();
   });
 
-  useEffect(() => {
-    if (!isEqual(stateOptions, hashOptions)) {
-      let hash;
-      if (!isEqual(hashOptions, previousHashOptions)) {
-        // if the hash options have changed, use them
-        hash = stringifyHashOptions(hashOptions);
-        // ensure that we keep the state and hash options synced
-        loadDashboardParams();
-      } else {
-        // otherwise, the state options have changed, so use them
-        hash = stringifyHashOptions(stateOptions);
-      }
-
-      if (hash !== location.hash) {
-        dispatch(
-          replace({
-            ...location,
-            hash: hash ? "#" + hash : "",
-          }),
-        );
-      }
+  useUpdateEffect(() => {
+    if (!isEqual(stateOptions, prevStateOptions)) {
+      dispatch(
+        replace({
+          ...location,
+          hash: stringifyHashOptions(stateOptions),
+        }),
+      );
     }
-  }, [
-    dispatch,
-    hashOptions,
-    loadDashboardParams,
-    location,
-    previousHashOptions,
-    stateOptions,
-  ]);
+  }, [dispatch, prevStateOptions, stateOptions]);
 
   return {
     isFullscreen,
