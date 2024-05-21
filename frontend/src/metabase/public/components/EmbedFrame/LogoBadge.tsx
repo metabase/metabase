cf3447334dd01f597064db8f13b24c649461f156
Denis Berezin <denis.berezin@metabase.com>
2024-05-21 20:37:05 +0300

    Add utm params to  link

--- frontend/src/metabase/public/components/EmbedFrame/LogoBadge.tsx
+++ frontend/src/metabase/public/components/EmbedFrame/LogoBadge.tsx
@@ -5,13 +5,13 @@ import LogoIcon from "metabase/components/LogoIcon";
 import type { Variant } from "./LogoBadge.styled";
 import { MetabaseLink, MetabaseName, Message } from "./LogoBadge.styled";
 
-function LogoBadge({
+export const LogoBadge = ({
   dark,
   variant = "default",
 }: {
   dark: boolean;
   variant?: Variant;
-}) {
+}) => {
   const logoSize = variant === "large" ? 42 : 28;
   const Metabase = (
     // eslint-disable-next-line no-literal-metabase-strings -- This embedding badge which we don't want to show the whitelabeled name
@@ -21,7 +21,7 @@ function LogoBadge({
   );
   return (
     <MetabaseLink
-      href="https://metabase.com/"
+      href="https://www.metabase.com/powered-by-metabase?utm_medium=referral&utm_source=product&utm_campaign=powered_by_metabase&utm_content=embedded_banner"
       target="_blank"
       variant={variant}
     >
@@ -29,7 +29,4 @@ function LogoBadge({
       <Message variant={variant}>{jt`Powered by ${Metabase}`}</Message>
     </MetabaseLink>
   );
-}
-
-// eslint-disable-next-line import/no-default-export -- deprecated usage
-export default LogoBadge;
+};
