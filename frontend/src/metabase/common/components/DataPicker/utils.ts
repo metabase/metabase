66eb1449858b1552f8843423b45b491730e6a18b
Kamil Mielnik <kamil@kamilmielnik.com>
2024-05-21 22:20:23 +0700

    Generate different shouldShowItem for different pickers

--- frontend/src/metabase/common/components/DataPicker/utils.ts
+++ frontend/src/metabase/common/components/DataPicker/utils.ts
@@ -6,6 +6,7 @@ import { getSchemaName } from "metabase-lib/v1/metadata/utils/schema";
 import type {
   Card,
   CollectionItem,
+  CollectionItemModel,
   Database,
   DatabaseId,
   SchemaName,
@@ -191,16 +192,16 @@ export const isValidValueItem = (model: SearchModel): boolean => {
   return ["table", "card", "dataset", "metric"].includes(model);
 };
 
-export const createShouldShowItem = (databaseId?: DatabaseId) => {
+export const createShouldShowItem = (
+  models: CollectionItemModel[],
+  databaseId?: DatabaseId,
+) => {
   return (item: QuestionPickerItem) => {
     if (item.model === "collection") {
       const below = item.below ?? [];
       const here = item.here ?? [];
       const contents = [...below, ...here];
-      const hasCards =
-        contents.includes("card") ||
-        contents.includes("dataset") ||
-        contents.includes("metric");
+      const hasCards = models.some(model => contents.includes(model));
 
       if (item.id !== "root" && !item.is_personal && !hasCards) {
         return false;
