80f9aaa42cce2ebe99ac7e8309b908f80a3f9040
Ryan Laurie <iethree@gmail.com>
2024-05-21 15:48:49 -0600

    simplify strategy editor markup

--- frontend/src/metabase/admin/performance/components/StrategyEditorForDatabases.styled.tsx
+++ frontend/src/metabase/admin/performance/components/StrategyEditorForDatabases.styled.tsx
@@ -1,32 +1,29 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 import { color } from "metabase/lib/colors";
 
-export const Panel = styled.section<{ verticallyOverflows?: boolean }>`
+export const Panel = styled.section`
   overflow-y: auto;
   display: flex;
   flex-flow: column nowrap;
   background-color: ${color("white")};
-  border-style: solid;
-  border-color: ${color("border")};
-  border-block-width: 2px;
-  border-inline-end-width: 1px;
-  border-inline-start-width: 0;
-  :first-child {
-    border-inline-start-width: 2px;
-    border-start-start-radius: 1rem;
-    border-end-start-radius: 1rem;
+  height: 100%;
+  &:last-child {
+    border-left: 1px solid ${color("border")};
   }
-  :last-child {
-    border-inline-end-width: 2px;
-    ${props =>
-      !props.verticallyOverflows &&
-      css`
-        border-start-end-radius: 1rem;
-        border-end-end-radius: 1rem;
-      `}
-  }
+`;
+
+export const RoundedBox = styled.div`
+  margin-bottom: 1rem;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: minmax(5rem, 30rem) minmax(5rem, auto);
+
+  overflow: hidden;
+
+  border-radius: 1rem;
+  border: 2px solid ${color("border")};
 `;
 
 export const TabWrapper = styled.div`
