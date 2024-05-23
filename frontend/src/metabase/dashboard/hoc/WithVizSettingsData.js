d4ba4506ef0dc2dca239a09cffb1cb4c53b2004f
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-23 13:00:51 -0400

    Fix memoization

--- frontend/src/metabase/dashboard/hoc/WithVizSettingsData.js
+++ frontend/src/metabase/dashboard/hoc/WithVizSettingsData.js
@@ -1,52 +1,70 @@
+import createCachedSelector from "re-reselect";
 import { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router";
 import _ from "underscore";
 
 import { getLinkTargets } from "metabase/lib/click-behavior";
 import { getUserAttributes } from "metabase/selectors/user";
+
+function getExtraDataForClick(
+  entities,
+  userAttributes,
+  dashboard,
+  dashcard,
+  parameterValuesBySlug,
+) {
+  return clicked => {
+    const entitiesByTypeAndId = _.chain(getLinkTargets(clicked.settings))
+      .groupBy(target => target.entity.name)
+      .mapObject(targets =>
+        _.chain(targets)
+          .map(({ entity, entityType, entityId }) =>
+            entityType === "question"
+              ? entity.selectors.getObject({ entities }, { entityId })?.card()
+              : entity.selectors.getObject({ entities }, { entityId }),
+          )
+          .filter(object => object != null)
+          .indexBy(object => object.id)
+          .value(),
+      )
+      .value();
+    return {
+      ...entitiesByTypeAndId,
+      parameterValuesBySlug,
+      dashboard,
+      dashcard,
+      userAttributes,
+    };
+  };
+}
+
+const createGetExtraDataForClick = createCachedSelector(
+  [
+    state => state.entities,
+    state => getUserAttributes(state),
+    (_state, props) => props.dashboard,
+    (_state, props) => props.dashcard,
+    (_state, props) => props.parameterValuesBySlug,
+  ],
+  getExtraDataForClick,
+)((_state, props) => props.dashcard.id);
 
 /**
  * This HOC gives access to data referenced in viz settings.
  * @deprecated HOCs are deprecated
  */
 export const WithVizSettingsData = ComposedComponent => {
-  return withRouter(
-    connect(
-      (state, props) => ({
-        getExtraDataForClick: clicked => {
-          const entitiesByTypeAndId = _.chain(getLinkTargets(clicked.settings))
-            .groupBy(target => target.entity.name)
-            .mapObject(targets =>
-              _.chain(targets)
-                .map(({ entity, entityType, entityId }) =>
-                  entityType === "question"
-                    ? entity.selectors.getObject(state, { entityId })?.card()
-                    : entity.selectors.getObject(state, { entityId }),
-                )
-                .filter(object => object != null)
-                .indexBy(object => object.id)
-                .value(),
-            )
-            .value();
-          return {
-            ...entitiesByTypeAndId,
-            location: props.location,
-            routerParams: props.params,
-            parameterValuesBySlug: props.parameterValuesBySlug,
-            dashboard: props.dashboard,
-            dashcard: props.dashcard,
-            userAttributes: getUserAttributes(state, props),
-          };
-        },
-      }),
-      dispatch => ({ dispatch }),
-    )(
-      class WithVizSettingsData extends Component {
-        render() {
-          return <ComposedComponent {..._.omit(this.props, "dispatch")} />;
-        }
-      },
-    ),
+  return connect(
+    (state, props) => {
+      const getExtraDataForClick = createGetExtraDataForClick(state, props);
+      return { getExtraDataForClick };
+    },
+    dispatch => ({ dispatch }),
+  )(
+    class WithVizSettingsData extends Component {
+      render() {
+        return <ComposedComponent {..._.omit(this.props, "dispatch")} />;
+      }
+    },
   );
 };
