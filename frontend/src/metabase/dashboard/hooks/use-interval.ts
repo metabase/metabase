5d2eea372f33d4e293e3157a139f139d70d5d16e
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-05-27 12:23:21 +0300

    Comment

--- frontend/src/metabase/dashboard/hooks/use-interval.ts
+++ frontend/src/metabase/dashboard/hooks/use-interval.ts
@@ -1,5 +1,8 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
+/**
+ * it's a copy of mantine's useInterval, but with memoization of the callbacks
+ */
 export function useInterval(fn: () => void, interval: number) {
   const [active, setActive] = useState(false);
   const intervalRef = useRef<number>();
