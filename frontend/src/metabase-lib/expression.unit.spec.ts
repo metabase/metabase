b7a586fe3e387be172215d3154d7571e20017a97
Kamil Mielnik <kamil@kamilmielnik.com>
2024-05-27 18:22:04 +0700

    Unwrap tests

--- frontend/src/metabase-lib/expression.unit.spec.ts
+++ frontend/src/metabase-lib/expression.unit.spec.ts
@@ -12,30 +12,97 @@ import { SAMPLE_DATABASE, createQueryWithClauses } from "./test-helpers";
 
 const offset = -1;
 
-describe("expression", () => {
-  describe("offsetClause", () => {
-    it("offsets Count aggregation without breakout", () => {
-      const stageIndex = -1;
-      const query = createQueryWithClauses({
-        aggregations: [{ operatorName: "count" }],
-      });
-      const [aggregationClause] = aggregations(query, stageIndex);
-      const offsettedClause = offsetClause(
-        query,
-        stageIndex,
-        aggregationClause,
-        offset,
-      );
-      const finalQuery = aggregate(query, stageIndex, offsettedClause);
+describe("offsetClause", () => {
+  it("offsets Count aggregation without breakout", () => {
+    const stageIndex = -1;
+    const query = createQueryWithClauses({
+      aggregations: [{ operatorName: "count" }],
+    });
+    const [aggregationClause] = aggregations(query, stageIndex);
+    const offsettedClause = offsetClause(
+      query,
+      stageIndex,
+      aggregationClause,
+      offset,
+    );
+    const finalQuery = aggregate(query, stageIndex, offsettedClause);
 
-      expect(
-        displayInfo(finalQuery, stageIndex, offsettedClause).displayName,
-      ).toBe("Count (previous period)");
+    expect(
+      displayInfo(finalQuery, stageIndex, offsettedClause).displayName,
+    ).toBe("Count (previous period)");
 
-      expect(toLegacyQuery(finalQuery)).toMatchObject({
-        database: SAMPLE_DATABASE.id,
-        query: {
-          aggregation: [
+    expect(toLegacyQuery(finalQuery)).toMatchObject({
+      database: SAMPLE_DATABASE.id,
+      query: {
+        aggregation: [
+          ["count"],
+          [
+            "offset",
+            {
+              name: "Count (previous period)",
+              "display-name": "Count (previous period)",
+            },
+            ["count"],
+            offset,
+          ],
+        ],
+        "source-table": ORDERS_ID,
+      },
+      type: "query",
+    });
+  });
+
+  it("generates proper name for Count aggregation with a Month breakout", () => {
+    const stageIndex = -1;
+    const query = createQueryWithClauses({
+      aggregations: [{ operatorName: "count" }],
+      breakouts: [
+        {
+          columnName: "CREATED_AT",
+          tableName: "ORDERS",
+          temporalBucketName: "Month",
+        },
+      ],
+    });
+    const [aggregationClause] = aggregations(query, stageIndex);
+    const offsettedClause = offsetClause(
+      query,
+      stageIndex,
+      aggregationClause,
+      offset,
+    );
+    const finalQuery = aggregate(query, stageIndex, offsettedClause);
+
+    expect(
+      displayInfo(finalQuery, stageIndex, offsettedClause).displayName,
+    ).toBe("Count (previous month)");
+  });
+});
+
+describe("diffOffsetClause", () => {
+  it("offsets Count aggregation without breakout", () => {
+    const stageIndex = -1;
+    const query = createQueryWithClauses({
+      aggregations: [{ operatorName: "count" }],
+    });
+    const [aggregationClause] = aggregations(query, stageIndex);
+    const offsettedClause = diffOffsetClause(
+      query,
+      stageIndex,
+      aggregationClause,
+      offset,
+    );
+    const finalQuery = aggregate(query, stageIndex, offsettedClause);
+
+    // TODO: displayInfo's name assertion
+
+    expect(toLegacyQuery(finalQuery)).toMatchObject({
+      database: SAMPLE_DATABASE.id,
+      query: {
+        aggregation: [
+          ["count"],
+          [
+            "-",
             ["count"],
             [
               "offset",
@@ -47,63 +114,42 @@ describe("expression", () => {
               offset,
             ],
           ],
-          "source-table": ORDERS_ID,
-        },
-        type: "query",
-      });
-    });
-
-    it("generates proper name for Count aggregation with a Month breakout", () => {
-      const stageIndex = -1;
-      const query = createQueryWithClauses({
-        aggregations: [{ operatorName: "count" }],
-        breakouts: [
-          {
-            columnName: "CREATED_AT",
-            tableName: "ORDERS",
-            temporalBucketName: "Month",
-          },
         ],
-      });
-      const [aggregationClause] = aggregations(query, stageIndex);
-      const offsettedClause = offsetClause(
-        query,
-        stageIndex,
-        aggregationClause,
-        offset,
-      );
-      const finalQuery = aggregate(query, stageIndex, offsettedClause);
-
-      expect(
-        displayInfo(finalQuery, stageIndex, offsettedClause).displayName,
-      ).toBe("Count (previous month)");
+        "source-table": ORDERS_ID,
+      },
+      type: "query",
     });
   });
 
-  describe("diffOffsetClause", () => {
-    it("offsets Count aggregation without breakout", () => {
-      const stageIndex = -1;
-      const query = createQueryWithClauses({
-        aggregations: [{ operatorName: "count" }],
-      });
-      const [aggregationClause] = aggregations(query, stageIndex);
-      const offsettedClause = diffOffsetClause(
-        query,
-        stageIndex,
-        aggregationClause,
-        offset,
-      );
-      const finalQuery = aggregate(query, stageIndex, offsettedClause);
+  // TODO: test for name
+});
 
-      // TODO: displayInfo's name assertion
+describe("percentDiffOffsetClause", () => {
+  it("offsets Count aggregation without breakout", () => {
+    const stageIndex = -1;
+    const query = createQueryWithClauses({
+      aggregations: [{ operatorName: "count" }],
+    });
+    const [aggregationClause] = aggregations(query, stageIndex);
+    const offsettedClause = percentDiffOffsetClause(
+      query,
+      stageIndex,
+      aggregationClause,
+      offset,
+    );
+    const finalQuery = aggregate(query, stageIndex, offsettedClause);
 
-      expect(toLegacyQuery(finalQuery)).toMatchObject({
-        database: SAMPLE_DATABASE.id,
-        query: {
-          aggregation: [
-            ["count"],
+    // TODO: displayInfo's name assertion
+
+    expect(toLegacyQuery(finalQuery)).toMatchObject({
+      database: SAMPLE_DATABASE.id,
+      query: {
+        aggregation: [
+          ["count"],
+          [
+            "-",
             [
-              "-",
+              "/",
               ["count"],
               [
                 "offset",
@@ -115,62 +161,14 @@ describe("expression", () => {
                 offset,
               ],
             ],
+            1,
           ],
-          "source-table": ORDERS_ID,
-        },
-        type: "query",
-      });
+        ],
+        "source-table": ORDERS_ID,
+      },
+      type: "query",
     });
-
-    // TODO: test for name
   });
 
-  describe("percentDiffOffsetClause", () => {
-    it("offsets Count aggregation without breakout", () => {
-      const stageIndex = -1;
-      const query = createQueryWithClauses({
-        aggregations: [{ operatorName: "count" }],
-      });
-      const [aggregationClause] = aggregations(query, stageIndex);
-      const offsettedClause = percentDiffOffsetClause(
-        query,
-        stageIndex,
-        aggregationClause,
-        offset,
-      );
-      const finalQuery = aggregate(query, stageIndex, offsettedClause);
-
-      // TODO: displayInfo's name assertion
-
-      expect(toLegacyQuery(finalQuery)).toMatchObject({
-        database: SAMPLE_DATABASE.id,
-        query: {
-          aggregation: [
-            ["count"],
-            [
-              "-",
-              [
-                "/",
-                ["count"],
-                [
-                  "offset",
-                  {
-                    name: "Count (previous period)",
-                    "display-name": "Count (previous period)",
-                  },
-                  ["count"],
-                  offset,
-                ],
-              ],
-              1,
-            ],
-          ],
-          "source-table": ORDERS_ID,
-        },
-        type: "query",
-      });
-    });
-
-    // TODO: test for name
-  });
+  // TODO: test for name
 });
