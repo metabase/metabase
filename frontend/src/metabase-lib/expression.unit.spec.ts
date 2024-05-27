907d1b16b7129d1f1a754275ad3a8abed2b3c3cd
Kamil Mielnik <kamil@kamilmielnik.com>
2024-05-27 20:20:48 +0700

    Add tests for percentDiffOffsetClause

--- frontend/src/metabase-lib/expression.unit.spec.ts
+++ frontend/src/metabase-lib/expression.unit.spec.ts
@@ -280,38 +280,122 @@ describe("diffOffsetClause", () => {
 });
 
 describe("percentDiffOffsetClause", () => {
-  it("offsets Count aggregation without breakout", () => {
-    const offset = -1;
-    const stageIndex = -1;
-    const query = createQueryWithClauses({
-      aggregations: [{ operatorName: "count" }],
-    });
-    const [aggregationClause] = aggregations(query, stageIndex);
+  const stageIndex = -1;
+
+  const setup = (query: Query, offset: number) => {
+    const [clause] = aggregations(query, stageIndex);
     const offsettedClause = percentDiffOffsetClause(
       query,
       stageIndex,
-      aggregationClause,
+      clause,
       offset,
     );
     const finalQuery = aggregate(query, stageIndex, offsettedClause);
 
-    expect(toLegacyQuery(finalQuery)).toMatchObject({
-      database: SAMPLE_DATABASE.id,
-      query: {
-        aggregation: [
-          ["count"],
-          [
-            "aggregation-options",
-            ["-", ["/", ["count"], ["offset", {}, ["count"], offset]], 1],
-            {
-              name: "Count (% vs previous period)",
-              "display-name": "Count (% vs previous period)",
-            },
-          ],
-        ],
-        "source-table": ORDERS_ID,
-      },
-      type: "query",
+    return {
+      clause: offsettedClause,
+      query: finalQuery,
+    };
+  };
+
+  describe("offset = -1", () => {
+    const offset = -1;
+
+    describe("no breakout", () => {
+      const { query, clause } = setup(queryNoBreakout, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs previous period)");
+      });
+
+      it("produces correct aggregation clause", () => {
+        expect(toLegacyQuery(query)).toMatchObject({
+          database: SAMPLE_DATABASE.id,
+          query: {
+            aggregation: [
+              ["count"],
+              [
+                "aggregation-options",
+                ["-", ["/", ["count"], ["offset", {}, ["count"], offset]], 1],
+                {
+                  name: "Count (% vs previous period)",
+                  "display-name": "Count (% vs previous period)",
+                },
+              ],
+            ],
+            "source-table": ORDERS_ID,
+          },
+          type: "query",
+        });
+      });
+    });
+
+    describe("breakout on binned datetime column", () => {
+      const { query, clause } = setup(queryDateBreakoutBinning, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs previous month)");
+      });
+    });
+
+    describe("breakout on non-binned datetime column", () => {
+      const { query, clause } = setup(queryDateBreakoutNoBinning, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs previous period)");
+      });
+    });
+
+    describe("breakout on non-datetime column", () => {
+      const { query, clause } = setup(queryCategoryBreakout, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs previous value)");
+      });
+    });
+  });
+
+  describe("offset < -1", () => {
+    const offset = -2;
+
+    describe("no breakout", () => {
+      const { query, clause } = setup(queryNoBreakout, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs 2 periods ago)");
+      });
+    });
+
+    describe("breakout on binned datetime column", () => {
+      const { query, clause } = setup(queryDateBreakoutBinning, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs 2 months ago)");
+      });
+    });
+
+    describe("breakout on non-binned datetime column", () => {
+      const { query, clause } = setup(queryDateBreakoutNoBinning, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs 2 periods ago)");
+      });
+    });
+
+    describe("breakout on non-datetime column", () => {
+      const { query, clause } = setup(queryCategoryBreakout, offset);
+
+      it("produces correct aggregation name", () => {
+        const info = displayInfo(query, stageIndex, clause);
+        expect(info.displayName).toBe("Count (% vs 2 rows above)");
+      });
     });
   });
 });
