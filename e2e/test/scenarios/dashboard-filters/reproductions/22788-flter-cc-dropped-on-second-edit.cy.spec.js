1712e03e7943a2b4d1dd44636128d5e36645d096
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-28 14:36:15 -0400

    Fix parameter matching with model columns (#43204)

--- e2e/test/scenarios/dashboard-filters/reproductions/22788-flter-cc-dropped-on-second-edit.cy.spec.js
+++ e2e/test/scenarios/dashboard-filters/reproductions/22788-flter-cc-dropped-on-second-edit.cy.spec.js
@@ -7,6 +7,7 @@ import {
   saveDashboard,
   sidebar,
   getDashboardCard,
+  popover,
 } from "e2e/support/helpers";
 
 const { PRODUCTS_ID, PRODUCTS } = SAMPLE_DATABASE;
@@ -97,11 +98,16 @@ describe("issue 22788", () => {
 
 function addFilterAndAssert() {
   filterWidget().click();
-  cy.findByPlaceholderText("Enter some text").type("Gizmo{enter}");
-  cy.button("Add filter").click();
+  popover().within(() => {
+    cy.findByText("Gizmo").click();
+    cy.button("Add filter").click();
+  });
 
-  cy.findAllByText("Gizmo");
-  cy.findAllByText("Doohickey").should("not.exist");
+  filterWidget()
+    .findByDisplayValue("Gizmo")
+    .should("not.exist")
+    .findByDisplayValue("Doohickey")
+    .should("not.exist");
 }
 
 function openFilterSettings() {
