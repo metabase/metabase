77f86c500f67653b7d4a8aa9eb3b0a78a21fd865
John Swanson <john.swanson@metabase.com>
2024-05-29 09:15:57 -0700

    Use a migration script (postgres only for now)

---
+++ resources/migrations/trash/postgres_rollback.sql
@@ -0,0 +1,17 @@
+UPDATE report_dashboard
+SET collection_id = (SELECT id FROM collection WHERE type = 'trash'),
+    trashed_from_collection_id = collection_id
+WHERE trashed_directly = true;
+
+-- Next: set `trashed_directly`.
+UPDATE report_card
+SET collection_id = (SELECT id FROM collection WHERE type = 'trash'),
+    trashed_from_collection_id = collection_id
+WHERE trashed_directly = true;
+
+UPDATE collection AS c1
+SET
+  trashed_from_location = c1.location,
+  location = CONCAT('/', trash.id, '/')
+FROM (SELECT id FROM collection WHERE type = 'trash') AS trash
+WHERE c1.trashed_directly = true;
