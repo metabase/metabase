3dc15c75bfe092e7f10be7fe2036bea7243cf247
Romeo Van Snick <romeo@romeovansnick.be>
2024-05-29 11:08:42 +0200

    Correctly place "Add column" button and allow using it when summarizing (#43189)
    * Do not render 'Add column' shortcut when viewing a raw table
    
    * Do not show 'Add column' shortcut when a query has aggregations
    
    * List shortcut guards out of component
    
    * Make 'Add column' icon sticky in the header
    
    * Add test for hiding the column shortcut
    
    * Remove Number wrapper
    
    * Move padding to a constant
    
    * Use actions directly to determine if there are any shortcuts
    
    * Make sure there are expressionable columns for the combine columns shortcut
    
    * Make sure there are extractable columns for the extract column shortcut
    
    * Remove padding when not overflowing
    
    * Do not shadow query in popover components
    
    * Allow combine and extract for raw tables
    
    * Add test for combining summarization columns
    
    * Add test for combining summarization columns
    
    * Use a more precise way of calculating the offset for the 'Add column' button
    
    * Handle null question
    
    * Change to the outline button
    
    * Use actual column widths to calculate total width
    
    * Handle empty mode
    
    * Remove extra measure call
    
    * Move shortcutColumn to separate method and avoid looping over all available actions when possible

--- frontend/src/metabase/visualizations/click-actions/actions/ExtractColumn/ExtractColumn.tsx
+++ frontend/src/metabase/visualizations/click-actions/actions/ExtractColumn/ExtractColumn.tsx
@@ -9,14 +9,21 @@ import type { ClickActionPopoverProps } from "metabase/visualizations/types/clic
 import * as Lib from "metabase-lib";
 
 export const ExtractColumnAction: LegacyDrill = ({ question, clicked }) => {
-  const { isEditable } = Lib.queryDisplayInfo(question.query());
+  const { query, stageIndex } = Lib.asReturned(question.query(), -1);
+
+  const { isEditable } = Lib.queryDisplayInfo(query);
+  const expressionableColumns = Lib.expressionableColumns(query, stageIndex);
+  const isExtractable =
+    expressionableColumns.reduce(function (sum, column) {
+      return sum + Lib.columnExtractions(query, column).length;
+    }, 0) > 0;
 
   if (
     !clicked ||
     clicked.value !== undefined ||
     !clicked.columnShortcuts ||
-    clicked?.extraData?.isRawTable ||
-    !isEditable
+    !isEditable ||
+    !isExtractable
   ) {
     return [];
   }
@@ -25,8 +32,6 @@ export const ExtractColumnAction: LegacyDrill = ({ question, clicked }) => {
     onChangeCardAndRun,
     onClose,
   }: ClickActionPopoverProps) => {
-    const query = question.query();
-    const stageIndex = -1;
     const dispatch = useDispatch();
 
     function handleSubmit(
