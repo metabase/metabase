80084df9160bbc2425ba3adaf7c031c28069afd2
Kamil Mielnik <kamil@kamilmielnik.com>
2024-05-29 21:34:34 +0700

    Extract utils

--- frontend/src/metabase/query_builder/components/CompareAggregations/components/OffsetInput/OffsetInput.tsx
+++ frontend/src/metabase/query_builder/components/CompareAggregations/components/OffsetInput/OffsetInput.tsx
@@ -1,11 +1,10 @@
 import { useMemo } from "react";
-import { t } from "ttag";
 
-import { pluralize } from "metabase/lib/formatting";
 import { Flex, NumberInput, Text } from "metabase/ui";
-import * as Lib from "metabase-lib";
+import type * as Lib from "metabase-lib";
 
 import S from "./OffsetInput.module.css";
+import { getHelp, getLabel, parsePeriodValue } from "./utils";
 
 interface Props {
   query: Lib.Query;
@@ -40,62 +39,4 @@ export const OffsetInput = ({ query, stageIndex, value, onChange }: Props) => {
       </Text>
     </Flex>
   );
-};
-
-const parsePeriodValue = (value: string): number | "" => {
-  const number = parseInt(value, 10);
-  return Number.isNaN(number) ? "" : Math.max(Math.abs(number), 1);
-};
-
-const getLabel = (query: Lib.Query, stageIndex: number): string => {
-  const firstBreakout = Lib.breakouts(query, stageIndex)[0];
-
-  if (firstBreakout) {
-    const firstBreakoutColumn = Lib.breakoutColumn(
-      query,
-      stageIndex,
-      firstBreakout,
-    );
-
-    if (!Lib.isDate(firstBreakoutColumn)) {
-      return t`Row for comparison`;
-    }
-  }
-
-  return t`Previous period`;
-};
-
-const getHelp = (query: Lib.Query, stageIndex: number): string => {
-  const firstBreakout = Lib.breakouts(query, stageIndex)[0];
-
-  if (!firstBreakout) {
-    return t`periods ago based on grouping`;
-  }
-
-  const firstBreakoutColumn = Lib.breakoutColumn(
-    query,
-    stageIndex,
-    firstBreakout,
-  );
-  const firstBreakoutColumnInfo = Lib.displayInfo(
-    query,
-    stageIndex,
-    firstBreakoutColumn,
-  );
-
-  if (!Lib.isDate(firstBreakoutColumn)) {
-    return t`rows above based on “${firstBreakoutColumnInfo.displayName}”`;
-  }
-
-  const bucket = Lib.temporalBucket(firstBreakout);
-
-  if (!bucket) {
-    return t`periods ago based on “${firstBreakoutColumnInfo.displayName}”`;
-  }
-
-  const bucketInfo = Lib.displayInfo(query, stageIndex, bucket);
-
-  return t`${pluralize(bucketInfo.displayName.toLowerCase())} ago based on “${
-    firstBreakoutColumnInfo.displayName
-  }”`;
 };
---
+++ frontend/src/metabase/query_builder/components/CompareAggregations/components/OffsetInput/utils.ts
@@ -0,0 +1,62 @@
+import { t } from "ttag";
+
+import { pluralize } from "metabase/lib/formatting";
+import * as Lib from "metabase-lib";
+
+export const parsePeriodValue = (value: string): number | "" => {
+  const number = parseInt(value, 10);
+  return Number.isNaN(number) ? "" : Math.max(Math.abs(number), 1);
+};
+
+export const getLabel = (query: Lib.Query, stageIndex: number): string => {
+  const firstBreakout = Lib.breakouts(query, stageIndex)[0];
+
+  if (firstBreakout) {
+    const firstBreakoutColumn = Lib.breakoutColumn(
+      query,
+      stageIndex,
+      firstBreakout,
+    );
+
+    if (!Lib.isDate(firstBreakoutColumn)) {
+      return t`Row for comparison`;
+    }
+  }
+
+  return t`Previous period`;
+};
+
+export const getHelp = (query: Lib.Query, stageIndex: number): string => {
+  const firstBreakout = Lib.breakouts(query, stageIndex)[0];
+
+  if (!firstBreakout) {
+    return t`periods ago based on grouping`;
+  }
+
+  const firstBreakoutColumn = Lib.breakoutColumn(
+    query,
+    stageIndex,
+    firstBreakout,
+  );
+  const firstBreakoutColumnInfo = Lib.displayInfo(
+    query,
+    stageIndex,
+    firstBreakoutColumn,
+  );
+
+  if (!Lib.isDate(firstBreakoutColumn)) {
+    return t`rows above based on “${firstBreakoutColumnInfo.displayName}”`;
+  }
+
+  const bucket = Lib.temporalBucket(firstBreakout);
+
+  if (!bucket) {
+    return t`periods ago based on “${firstBreakoutColumnInfo.displayName}”`;
+  }
+
+  const bucketInfo = Lib.displayInfo(query, stageIndex, bucket);
+
+  return t`${pluralize(bucketInfo.displayName.toLowerCase())} ago based on “${
+    firstBreakoutColumnInfo.displayName
+  }”`;
+};
