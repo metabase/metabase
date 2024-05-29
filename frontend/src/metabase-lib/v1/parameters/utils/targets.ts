e8f6510cb0bc40163f0840e006575be532ac83e8
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-05-29 10:39:55 -0400

    Try simple approach

--- frontend/src/metabase-lib/v1/parameters/utils/targets.ts
+++ frontend/src/metabase-lib/v1/parameters/utils/targets.ts
@@ -60,38 +60,11 @@ export function getParameterTargetField(
   }
 
   if (isConcreteFieldReference(fieldRef)) {
-    const query = question.query();
-    const stageIndex = -1;
-    const columns = Lib.visibleColumns(query, stageIndex);
-
-    if (columns.length === 0) {
-      // query and metadata are not available: 1) no data permissions 2) embedding
-      // we cannot correctly find a field in all cases this way
-      const [_, fieldIdOrName] = fieldRef;
-      const fields = metadata.fieldsList();
-      return fields.find(
-        field => field.id === fieldIdOrName || field.name === fieldIdOrName,
-      );
-    }
-
-    const [columnIndex] = Lib.findColumnIndexesFromLegacyRefs(
-      query,
-      stageIndex,
-      columns,
-      [fieldRef],
+    const [_, fieldIdOrName] = fieldRef;
+    const fields = metadata.fieldsList();
+    return fields.find(
+      field => field.id === fieldIdOrName || field.name === fieldIdOrName,
     );
-    if (columnIndex < 0) {
-      return null;
-    }
-
-    const column = columns[columnIndex];
-    const fieldValuesInfo = Lib.fieldValuesSearchInfo(query, column);
-    if (fieldValuesInfo.fieldId == null) {
-      // the column does not represent to a database field, e.g. coming from an aggregation clause
-      return null;
-    }
-
-    return metadata.field(fieldValuesInfo.fieldId);
   }
 
   return null;
