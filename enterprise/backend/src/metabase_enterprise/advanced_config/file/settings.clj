e5a335704c8b9b64c34a7169fe51e8dccc21552b
Cal Herries <calherries@gmail.com>
2024-05-30 17:36:12 -0600

    + log error from incorrectly set setting instead of throwing error

--- enterprise/backend/src/metabase_enterprise/advanced_config/file/settings.clj
+++ enterprise/backend/src/metabase_enterprise/advanced_config/file/settings.clj
@@ -3,7 +3,10 @@
    [clojure.spec.alpha :as s]
    [metabase-enterprise.advanced-config.file.interface :as advanced-config.file.i]
    [metabase.models.setting :as setting]
-   [metabase.util.log :as log]))
+   [metabase.util :as u]
+   [metabase.util.log :as log])
+  (:import
+   (clojure.lang ExceptionInfo)))
 
 (defmethod advanced-config.file.i/section-spec :settings
   [_section-name]
@@ -14,4 +17,9 @@
   (log/info "Setting setting values from config file")
   (doseq [[setting-name setting-value] settings]
     (log/infof "Setting value for Setting %s" setting-name)
-    (setting/set! setting-name setting-value)))
+    (try
+      (setting/set! setting-name setting-value)
+      (catch ExceptionInfo e
+        (if (true? (:metabase.models.setting/unknown-setting-error (ex-data e)))
+          (log/warn (u/format-color :yellow "Ignoring unknown setting in config: %s." (name setting-name)))
+          (throw e))))))
