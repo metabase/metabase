9da9c957cdcf88ef1677000f923963c4caee3b06
John Swanson <john.swanson@metabase.com>
2024-05-30 10:22:04 -0700

    fix h2 rollback

--- resources/migrations/trash/h2-rollback.sql
+++ resources/migrations/trash/h2-rollback.sql
@@ -9,9 +9,8 @@ SET collection_id = (SELECT id FROM collection WHERE type = 'trash'),
     trashed_from_collection_id = collection_id
 WHERE trashed_directly = true;
 
-WITH trash AS (SELECT id FROM collection WHERE type = 'trash')
 UPDATE collection AS c1
 SET
   trashed_from_location = c1.location,
-  location = CONCAT('/', trash.id, '/')
-WHERE c1.trashed_directly = true;
+  location = CONCAT('/', (SELECT id FROM collection WHERE type = 'trash'), '/')
+WHERE c1.trashed_directly = true AND c1.archived = true AND c1.namespace IS NULL;
