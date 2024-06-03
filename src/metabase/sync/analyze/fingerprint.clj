bc5bc7c804172ebf37ab35b37b32bef3da075052
Cal Herries <calherries@gmail.com>
2024-06-03 11:24:25 -0600

    + don't refingerprint type/*

--- src/metabase/sync/analyze/fingerprint.clj
+++ src/metabase/sync/analyze/fingerprint.clj
@@ -161,6 +161,7 @@
     [:not (mdb.query/isa :semantic_type :type/PK)]
     [:= :semantic_type nil]]
    [:not-in :visibility_type ["retired" "sensitive"]]
+   [:not= :base_type (u/qualified-name :type/*)]
    [:not (mdb.query/isa :base_type :type/Structured)]])
 
 (def ^:dynamic *refingerprint?*
