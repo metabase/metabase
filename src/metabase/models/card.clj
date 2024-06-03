70ff785090df34d9988dc6aaaa76803fbc445214
Tamás Benkő <tamas@metabase.com>
2024-06-03 18:33:59 +0300

    Add a standard can-write check to with-can-write-query

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -149,27 +149,27 @@
   "Adds can_write_query to each card."
   [cards]
   (mi/instances-with-hydrated-data
-    cards :can_write_query
-    (fn []
-      (mu/disable-enforcement
-        (let [db->mp (into {}
-                           (comp
-                             (keep :database_id)
-                             (map (juxt identity
-                                        lib.metadata.jvm/application-database-metadata-provider)))
-                           cards)]
-          (into {}
-                (comp
-                  (remove nil?)
-                  (filter (comp not-empty :dataset_query))
-                  (map
-                    (fn [{card-id :id :keys [database_id dataset_query]}]
-                      (let [query (lib/query (get db->mp database_id) dataset_query)]
-                        [card-id
-                         (:is-editable (lib/display-info query query))]))))
-                cards))))
-    :id
-    {:default false}))
+   cards :can_write_query
+   (fn []
+     (mu/disable-enforcement
+       (let [db->mp (into {}
+                          (comp
+                           (keep :database_id)
+                           (map (juxt identity
+                                      lib.metadata.jvm/application-database-metadata-provider)))
+                          cards)]
+         (into {}
+               (comp
+                (filter (comp seq :dataset_query))
+                (map
+                 (fn [{card-id :id :keys [database_id dataset_query] :as card}]
+                   (let [query (lib/query (get db->mp database_id) dataset_query)]
+                     [card-id
+                      (:is-editable (and (mi/can-write? card)
+                                         (lib/display-info query query)))]))))
+               cards))))
+   :id
+   {:default false}))
 
 (mi/define-batched-hydration-method add-can-write-query
   :can_write_query
