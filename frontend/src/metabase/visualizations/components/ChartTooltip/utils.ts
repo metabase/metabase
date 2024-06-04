cb58da6bfc7a84a524961ce7d30ca512ea253c71
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2024-06-04 17:17:35 -0400

    fix tooltip values being double scaled

--- frontend/src/metabase/visualizations/components/ChartTooltip/utils.ts
+++ frontend/src/metabase/visualizations/components/ChartTooltip/utils.ts
@@ -1,4 +1,5 @@
 import { formatValue } from "metabase/lib/formatting";
+import type { OptionsType } from "metabase/lib/formatting/types";
 import type {
   ComputedVisualizationSettings,
   RemappingHydratedDatasetColumn,
@@ -9,15 +10,24 @@ export const formatValueForTooltip = ({
   value,
   column,
   settings,
+  isAlreadyScaled,
 }: {
   value?: unknown;
   column?: RemappingHydratedDatasetColumn | DatasetColumn | null;
   settings?: ComputedVisualizationSettings;
-}) =>
-  formatValue(value, {
+  isAlreadyScaled?: boolean;
+}) => {
+  const options: OptionsType = {
     ...(settings && settings.column && column
       ? settings.column(column)
       : { column }),
     type: "tooltip",
     majorWidth: 0,
-  });
+  };
+
+  if (isAlreadyScaled) {
+    options.scale = 1;
+  }
+
+  return formatValue(value, options);
+};
