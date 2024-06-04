a3a5acd808147ba0fbb5186010a548bd6990e853
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-06-04 10:04:46 -0400

    rebase

--- frontend/src/metabase/browse/test-utils.ts
+++ frontend/src/metabase/browse/test-utils.ts
@@ -2,6 +2,6 @@ import type { ModelResult } from "metabase-types/api";
 import { createMockSearchResult } from "metabase-types/api/mocks";
 
 export const createMockModelResult = (
-  model: Partial<ModelResult>,
+  model: Partial<ModelResult> = {},
 ): ModelResult =>
   createMockSearchResult({ ...model, model: "dataset" }) as ModelResult;
