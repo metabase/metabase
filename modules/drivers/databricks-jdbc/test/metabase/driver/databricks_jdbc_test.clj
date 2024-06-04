800d09128d5704d243a7f913564fb794538814d1
lbrdnk <lbrdnk@users.noreply.github.com>
2024-06-04 22:45:25 +0200

    WIP data loading -- it works, further cleanup needed

--- modules/drivers/databricks-jdbc/test/metabase/driver/databricks_jdbc_test.clj
+++ modules/drivers/databricks-jdbc/test/metabase/driver/databricks_jdbc_test.clj
@@ -1,3 +1,19 @@
-(ns metabase.driver.databricks-jdbc-test)
+(ns metabase.driver.databricks-jdbc-test
+  (:require
+   [clojure.test :refer :all]
+   [metabase.test :as mt]))
 
 ;; can connect with details...
+(deftest dummy-test
+  (mt/test-driver
+   :databricks-jdbc
+   (mt/dataset
+    places-cam-likes
+    (mt/db))))
+
+(deftest dummy-test-2
+  (mt/test-driver
+   :databricks-jdbc
+   (mt/dataset
+    test-data
+    (mt/db))))
---
+++ modules/drivers/databricks-jdbc/test/metabase/test/data/databricks_jdbc.clj
@@ -0,0 +1,109 @@
+(ns metabase.test.data.databricks-jdbc
+  (:require
+   [clojure.java.jdbc :as jdbc]
+   [metabase.driver.ddl.interface :as ddl.i]
+   [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
+   [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
+   [metabase.test.data.interface :as tx]
+   [metabase.test.data.sql :as sql.tx]
+   [metabase.test.data.sql-jdbc :as sql-jdbc.tx]
+   [metabase.test.data.sql-jdbc.execute :as execute]
+   [metabase.test.data.sql-jdbc.load-data :as load-data]
+   [metabase.test.data.sql.ddl :as ddl]
+   [metabase.util :as u]
+   [metabase.util.log :as log]
+   ))
+
+(set! *warn-on-reflection* true)
+
+(sql-jdbc.tx/add-test-extensions! :databricks-jdbc)
+
+(doseq [[base-type database-type] {:type/BigInteger     "BIGINT"
+                                   :type/Boolean        "BOOLEAN"
+                                   :type/Date           "DATE"
+                                   :type/DateTime       "TIMESTAMP"
+                                   ;; TODO: Verify following is ok
+                                   :type/DateTimeWithTZ "TIMESTAMP"
+                                   :type/Decimal        "DECIMAL"
+                                   :type/Float          "DOUBLE"
+                                   :type/Integer        "INTEGER"
+                                   :type/Text           "STRING"}]
+  (defmethod sql.tx/field-base-type->sql-type [:databricks-jdbc base-type] [_ _] database-type))
+
+;; TODO: Naming schema vs database!
+(defmethod tx/dbdef->connection-details :databricks-jdbc
+  [_driver _connection-type {:keys [database-name]}]
+  (merge
+   {:host      (tx/db-test-env-var-or-throw :databricks-jdbc :host)
+    :token     (tx/db-test-env-var-or-throw :databricks-jdbc :token)
+    :http-path (tx/db-test-env-var-or-throw :databricks-jdbc :http-path)
+    :catalog   (tx/db-test-env-var-or-throw :databricks-jdbc :catalog)
+    :schema    database-name}))
+
+;; TODO: Make this work so already loaded databases are not removed.
+(defn- existing-databases
+  "Set of databases that already exist. Used to avoid creating those"
+  []
+  (sql-jdbc.execute/do-with-connection-with-options
+   :databricks-jdbc
+   (->> (tx/dbdef->connection-details :databricks-jdbc nil nil)
+        ;; TODO: use db->pooled-connection-spec
+        (sql-jdbc.conn/connection-details->spec :databricks-jdbc))
+   nil
+   (fn [^java.sql.Connection conn]
+     (into #{} (map :databasename) (jdbc/query {:connection conn} ["SHOW DATABASES;"])))))
+
+(defmethod tx/create-db! :athena
+  [driver {:keys [schema], :as db-def} & options]
+  (let [schema (ddl.i/format-name driver schema)]
+    (if (contains? (existing-databases) schema)
+      (log/infof "Databricks database %s already exists, skipping creation" (pr-str schema))
+      (do
+        (log/infof "Creating Databricks database %s" (pr-str schema))
+        (apply (get-method tx/create-db! :sql-jdbc/test-extensions) driver db-def options)))))
+
+;; TODO: Verify this is ready to go!
+(defmethod load-data/do-insert! :databricks-jdbc
+  [driver spec table-identifier row-or-rows]
+  (let [statements (ddl/insert-rows-ddl-statements driver table-identifier row-or-rows)]
+    (sql-jdbc.execute/do-with-connection-with-options
+     driver
+     spec
+     {:write? true}
+     (fn [^java.sql.Connection conn]
+       (try
+         ;; Not supported!
+         #_(.setAutoCommit conn false)
+         (doseq [sql+args statements]
+           (jdbc/execute! {:connection conn} sql+args {:transaction? false}))
+         (catch #_java.sql.SQLException Throwable e
+                (log/infof "Error inserting data: %s" (u/pprint-to-str 'red statements))
+                (jdbc/print-sql-exception-chain e)
+                (throw e)))))))
+
+;; TODO: It seems ids have to be added!!!
+(defmethod load-data/load-data! :databricks-jdbc [& args]
+  (try
+    ;; TODO: Following may be redundant!
+    (apply load-data/load-data-AND-add-ids! args)
+    (catch Throwable t
+      (throw t))))
+
+#_(defmethod load-data/load-data! :databricks-jdbc [& args]
+  (apply (get-method load-data/load-data! :sql-jdbc) args))
+
+;; TODO: Verify!
+(defmethod execute/execute-sql! :databricks-jdbc [& args]
+  (apply execute/sequentially-execute-sql! args))
+
+;; TODO: Is INT OK? Probably.
+(defmethod sql.tx/pk-sql-type :databricks-jdbc [_] "INT")
+
+;; TODO: Verify!
+(defmethod tx/supports-time-type? :databricks-jdbc [_driver] false)
+(defmethod tx/supports-timestamptz-type? :databricks-jdbc [_driver] false)
+
+;; Following is necessary!
+(defmethod sql.tx/drop-db-if-exists-sql :databricks-jdbc
+  [driver {:keys [database-name]}]
+  (format "DROP DATABASE IF EXISTS %s CASCADE" (sql.tx/qualify-and-quote driver database-name)))
