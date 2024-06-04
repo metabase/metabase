e617925e1bbb8db9233ce923a90b38fdd37823ec
Emmad Usmani <emmadusmani@berkeley.edu>
2024-06-04 11:19:26 -0700

    compute viz settings for static pie chart (#43548)
    * compute viz settings for static pie chart
    
    * create chartModel for pie chart (#43549)
    
    * create chartModel for pie chart
    
    * render static pie chart (#43550)
    
    * render static pie chart
    
    * render static pie chart legend (#43551)

--- frontend/src/metabase/static-viz/components/PieChart/PieChart.tsx
+++ frontend/src/metabase/static-viz/components/PieChart/PieChart.tsx
@@ -1,16 +1,74 @@
+import { Group } from "@visx/group";
+import { init } from "echarts";
+
+import { sanitizeSvgForBatik } from "metabase/static-viz/lib/svg";
+import { DIMENSIONS } from "metabase/visualizations/echarts/pie/constants";
+import { getPieChartFormatters } from "metabase/visualizations/echarts/pie/format";
+import { getPieChartModel } from "metabase/visualizations/echarts/pie/model";
+import { getPieChartOption } from "metabase/visualizations/echarts/pie/option";
+
 import type { StaticChartProps } from "../StaticVisualization";
 
-export function PieChart(props: StaticChartProps) {
+import { getPieChartLegend } from "./legend";
+import { computeStaticPieChartSettings } from "./settings";
+
+export function PieChart({
+  rawSeries,
+  dashcardSettings,
+  renderingContext,
+  isStorybook,
+}: StaticChartProps) {
+  const computedVizSettings = computeStaticPieChartSettings(
+    rawSeries,
+    dashcardSettings,
+  );
+  const chartModel = getPieChartModel(
+    rawSeries,
+    computedVizSettings,
+    renderingContext,
+  );
+  const formatters = getPieChartFormatters(
+    chartModel,
+    computedVizSettings,
+    renderingContext,
+  );
+  const option = getPieChartOption(
+    chartModel,
+    formatters,
+    computedVizSettings,
+    renderingContext,
+  );
+  const { legendHeight, Legend } = getPieChartLegend(
+    chartModel,
+    formatters,
+    computedVizSettings,
+    DIMENSIONS.sideLen,
+    DIMENSIONS.paddingTop,
+  );
+
+  const chart = init(null, null, {
+    renderer: "svg",
+    ssr: true,
+    width: DIMENSIONS.sideLen,
+    height: DIMENSIONS.sideLen,
+  });
+  chart.setOption(option);
+  const chartSvg = sanitizeSvgForBatik(
+    chart.renderToSVGString(),
+    isStorybook ?? false,
+  );
+
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={500} height={500}>
-      <text
-        id="outer-text"
-        fill="black"
-        dominantBaseline="central"
-        transform="translate(50 50)"
-      >
-        Placeholder
-      </text>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={DIMENSIONS.sideLen}
+      height={DIMENSIONS.sideLen + DIMENSIONS.paddingTop + legendHeight}
+    >
+      <Legend />
+      <Group
+        top={DIMENSIONS.paddingTop + legendHeight}
+        dangerouslySetInnerHTML={{ __html: chartSvg }}
+      ></Group>
     </svg>
   );
 }
