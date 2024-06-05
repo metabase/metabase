0fc86aae24bf707bd5dbad1b801be67a2a3e77ad
Ryan Laurie <iethree@gmail.com>
2024-06-05 05:30:45 -0600

    add another flake

--- e2e/test/scenarios/dashboard/dashboard-reproductions.cy.spec.js
+++ e2e/test/scenarios/dashboard/dashboard-reproductions.cy.spec.js
@@ -696,33 +696,37 @@ describe("issue 31274", () => {
   });
 
   // cypress automatically scrolls to the element, but we don't need it in this test
-  it("should not clip dashcard actions (metabase#31274)", () => {
-    cy.createDashboard().then(({ body: dashboard }) => {
-      const dashcards = createTextCards(3);
-      cy.request("PUT", `/api/dashboard/${dashboard.id}`, {
-        dashcards,
-      });
-
-      visitDashboard(dashboard.id);
-      editDashboard(dashboard.id);
-
-      secondTextCard().realHover();
-
-      visibleActionsPanel().should("have.length", 1);
-
-      cy.log(
-        "Make sure cypress can click the element, which means it is not covered by another",
-      );
-
-      visibleActionsPanel().within(() => {
-        cy.icon("close").click({
-          position: "top",
+  it(
+    "should not clip dashcard actions (metabase#31274)",
+    { tags: "@flaky" },
+    () => {
+      cy.createDashboard().then(({ body: dashboard }) => {
+        const dashcards = createTextCards(3);
+        cy.request("PUT", `/api/dashboard/${dashboard.id}`, {
+          dashcards,
         });
-      });
 
-      cy.findAllByTestId("dashcard").should("have.length", 2);
-    });
-  });
+        visitDashboard(dashboard.id);
+        editDashboard(dashboard.id);
+
+        secondTextCard().realHover();
+
+        visibleActionsPanel().should("have.length", 1);
+
+        cy.log(
+          "Make sure cypress can click the element, which means it is not covered by another",
+        );
+
+        visibleActionsPanel().within(() => {
+          cy.icon("close").click({
+            position: "top",
+          });
+        });
+
+        cy.findAllByTestId("dashcard").should("have.length", 2);
+      });
+    },
+  );
 
   it("renders cross icon on the link card without clipping", () => {
     cy.createDashboard().then(({ body: dashboard }) => {
