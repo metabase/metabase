e536ba68f16684a0ab729e299fd0a61c8f72d383
Denis Berezin <denis.berezin@metabase.com>
2024-06-06 00:41:40 +0300

    Fix CI

--- frontend/src/metabase/databases/components/DatabaseEngineField/DatabaseEngineWidget.styled.tsx
+++ frontend/src/metabase/databases/components/DatabaseEngineField/DatabaseEngineWidget.styled.tsx
@@ -37,8 +37,7 @@ export const EngineCardRoot = styled.li<EngineCardRootProps>`
   border-radius: 0.375rem;
   background-color: ${color("white")};
   cursor: pointer;
-  outline: ${props =>
-    props.isActive ? `2px solid var(--mb-color-focus)` : ""};
+  outline: ${props => props.isActive && `2px solid var(--mb-color-focus)`};
 
   &:hover {
     border-color: var(--mb-color-brand);
