77cd3bbd8bbc0e6b00a10f4fac978ae6ab12efc4
Kamil Mielnik <kamil@kamilmielnik.com>
2024-06-05 18:32:30 +0700

    Implement test for notebook editor

--- e2e/test/scenarios/question/column-compare.cy.spec.ts
+++ e2e/test/scenarios/question/column-compare.cy.spec.ts
@@ -1,17 +1,85 @@
-describe("scenarios > question > column compare", () => {
+import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import {
+  createQuestion,
+  describeWithSnowplow,
+  expectGoodSnowplowEvent,
+  getNotebookStep,
+  openNotebook,
+  popover,
+  resetSnowplow,
+  restore,
+} from "e2e/support/helpers";
+import type { FieldReference } from "metabase-types/api";
+
+const { PRODUCTS_ID, PRODUCTS } = SAMPLE_DATABASE;
+
+const BREAKOUT_CREATED_AT_MONTH: FieldReference = [
+  "field",
+  PRODUCTS.CREATED_AT,
+  { "base-type": "type/DateTime", "temporal-unit": "month" },
+];
+
+describeWithSnowplow("scenarios > question > column compare", () => {
+  beforeEach(() => {
+    restore();
+    resetSnowplow();
+    cy.signInAsAdmin();
+
+    createQuestion(
+      {
+        query: {
+          "source-table": PRODUCTS_ID,
+          aggregation: [["count"]],
+          breakout: [BREAKOUT_CREATED_AT_MONTH],
+        },
+      },
+      { visitQuestion: true, wrapId: true, idAlias: "questionId" },
+    );
+  });
+
   describe("notebook editor", () => {
+    it("should create a snowplow event for the column compare action", () => {
+      openNotebook();
+      getNotebookStep("summarize")
+        .findByTestId("aggregate-step")
+        .icon("add")
+        .click();
+
+      popover().findByText("Compare “Count” to previous months ...").click();
+      toggleColumnPicker();
+      toggleColumnPickerItem("Percentage difference");
+      toggleColumnPicker();
+      popover().button("Done").click();
+
+      cy.get("@questionId").then(questionId => {
+        expectGoodSnowplowEvent({
+          event: "column_compare_via_shortcut",
+          custom_expressions_used: ["offset", "count"],
+          database_id: SAMPLE_DB_ID,
+          question_id: questionId,
+        });
+      });
+    });
+  });
+
+  describe.skip("chill mode - column header drill", () => {
     it("works", () => {});
   });
 
-  describe("chill mode - column header drill", () => {
+  describe.skip("chill mode - plus button", () => {
     it("works", () => {});
   });
 
-  describe("chill mode - plus button", () => {
-    it("works", () => {});
-  });
-
-  describe("chill mode - summarize", () => {
+  describe.skip("chill mode - summarize", () => {
     it("works", () => {});
   });
 });
+
+function toggleColumnPicker() {
+  cy.findByTestId("column-picker").click({ force: true });
+}
+
+function toggleColumnPickerItem(name: string) {
+  cy.findAllByTestId("column-picker-item").contains(name).click();
+}
