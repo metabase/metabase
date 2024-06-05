17045b5ae5b1d7c1d2342a31dcf5ab7d4f02207d
Kamil Mielnik <kamil@kamilmielnik.com>
2024-06-05 21:45:57 +0700

    Add more tests

--- frontend/src/metabase/query_builder/components/CompareAggregations/CompareAggregations.unit.spec.tsx
+++ frontend/src/metabase/query_builder/components/CompareAggregations/CompareAggregations.unit.spec.tsx
@@ -1,3 +1,5 @@
+import userEvent from "@testing-library/user-event";
+
 import { renderWithProviders, screen } from "__support__/ui";
 import type * as Lib from "metabase-lib";
 import { createQueryWithClauses } from "metabase-lib/test-helpers";
@@ -21,16 +23,87 @@ const setup = ({ query }: SetupOpts) => {
       onSubmit={onSubmit}
     />,
   );
+
+  return { onClose };
 };
 
 describe("CompareAggregations", () => {
-  describe("1 aggregation", () => {
-    it("displays the correct title", () => {
+  describe("single aggregation", () => {
+    it("does not show step to pick aggregation", () => {
       setup({ query: createQueryWithCountAggregation() });
 
       expect(
         screen.getByText("Compare “Count” to previous period"),
       ).toBeInTheDocument();
+      expect(
+        screen.queryByText("Compare one of these to the previous period"),
+      ).not.toBeInTheDocument();
+    });
+
+    it("calls onClose when going back to previous step", async () => {
+      const { onClose } = setup({ query: createQueryWithCountAggregation() });
+
+      expect(
+        screen.getByText("Compare “Count” to previous period"),
+      ).toBeInTheDocument();
+      expect(
+        screen.queryByText("Compare one of these to the previous period"),
+      ).not.toBeInTheDocument();
+
+      await userEvent.click(
+        screen.getByText("Compare “Count” to previous period"),
+      );
+
+      expect(onClose).toHaveBeenCalled();
+    });
+  });
+
+  describe("multiple aggregations", () => {
+    it("shows step to pick aggregation", async () => {
+      setup({ query: createQueryWithCountAndSumAggregations() });
+
+      expect(
+        screen.getByText("Compare one of these to the previous period"),
+      ).toBeInTheDocument();
+      expect(
+        screen.queryByText("Compare “Count” to previous period"),
+      ).not.toBeInTheDocument();
+      expect(screen.getByText("Count")).toBeInTheDocument();
+      expect(screen.getByText("Sum of Price")).toBeInTheDocument();
+    });
+
+    it("allows to go back and forth between steps", async () => {
+      const { onClose } = setup({
+        query: createQueryWithCountAndSumAggregations(),
+      });
+
+      await userEvent.click(screen.getByText("Count"));
+
+      expect(
+        screen.getByText("Compare “Count” to previous period"),
+      ).toBeInTheDocument();
+
+      await userEvent.click(
+        screen.getByText("Compare “Count” to previous period"),
+      );
+
+      await userEvent.click(screen.getByText("Sum of Price"));
+
+      expect(
+        screen.getByText("Compare “Sum of Price” to previous period"),
+      ).toBeInTheDocument();
+
+      await userEvent.click(
+        screen.getByText("Compare “Sum of Price” to previous period"),
+      );
+
+      expect(onClose).not.toHaveBeenCalled();
+
+      await userEvent.click(
+        screen.getByText("Compare one of these to the previous period"),
+      );
+
+      expect(onClose).toHaveBeenCalled();
     });
   });
 });
@@ -38,5 +111,14 @@ describe("CompareAggregations", () => {
 function createQueryWithCountAggregation() {
   return createQueryWithClauses({
     aggregations: [{ operatorName: "count" }],
+  });
+}
+
+function createQueryWithCountAndSumAggregations() {
+  return createQueryWithClauses({
+    aggregations: [
+      { operatorName: "count" },
+      { operatorName: "sum", columnName: "PRICE", tableName: "PRODUCTS" },
+    ],
   });
 }
