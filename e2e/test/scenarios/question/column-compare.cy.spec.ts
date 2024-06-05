d3872697c9e9765ea8940036a7dc0dfbcb3bc40b
Kamil Mielnik <kamil@kamilmielnik.com>
2024-06-05 17:03:30 +0700

    Add tests boilerplate

---
+++ e2e/test/scenarios/question/column-compare.cy.spec.ts
@@ -0,0 +1,17 @@
+describe("scenarios > question > column compare", () => {
+  describe("notebook editor", () => {
+    it("works", () => {});
+  });
+
+  describe("chill mode - column header drill", () => {
+    it("works", () => {});
+  });
+
+  describe("chill mode - plus button", () => {
+    it("works", () => {});
+  });
+
+  describe("chill mode - summarize", () => {
+    it("works", () => {});
+  });
+});
