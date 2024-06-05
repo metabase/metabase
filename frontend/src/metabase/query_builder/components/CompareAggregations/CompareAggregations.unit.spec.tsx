01d7d93a708bb2c9c6c457ffad53c46f213d9c58
Kamil Mielnik <kamil@kamilmielnik.com>
2024-06-05 19:58:01 +0700

    Add boilerplate for CompareAggregations unit tests

---
+++ frontend/src/metabase/query_builder/components/CompareAggregations/CompareAggregations.unit.spec.tsx
@@ -0,0 +1,42 @@
+import { renderWithProviders, screen } from "__support__/ui";
+import type * as Lib from "metabase-lib";
+import { createQueryWithClauses } from "metabase-lib/test-helpers";
+
+import { CompareAggregations } from "./CompareAggregations";
+
+interface SetupOpts {
+  query: Lib.Query;
+}
+
+const setup = ({ query }: SetupOpts) => {
+  const stageIndex = -1;
+  const onClose = jest.fn();
+  const onSubmit = jest.fn();
+
+  renderWithProviders(
+    <CompareAggregations
+      query={query}
+      stageIndex={stageIndex}
+      onClose={onClose}
+      onSubmit={onSubmit}
+    />,
+  );
+};
+
+describe("CompareAggregations", () => {
+  describe("1 aggregation", () => {
+    it("displays the correct title", () => {
+      setup({ query: createQueryWithCountAggregation() });
+
+      expect(
+        screen.getByText("Compare “Count” to previous period ..."),
+      ).toBeInTheDocument();
+    });
+  });
+});
+
+function createQueryWithCountAggregation() {
+  return createQueryWithClauses({
+    aggregations: [{ operatorName: "count" }],
+  });
+}
