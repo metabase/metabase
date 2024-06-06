5e72037735a352aaa2580157fd5a0b3f2e757e32
Romeo Van Snick <romeo@romeovansnick.be>
2024-06-06 17:59:40 +0200

    Fix sharing test

--- e2e/test/scenarios/sharing/sharing-reproductions.cy.spec.js
+++ e2e/test/scenarios/sharing/sharing-reproductions.cy.spec.js
@@ -469,7 +469,8 @@ describeEE("issue 24223", () => {
     cy.findByLabelText("subscriptions").click();
     // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
     cy.findByText("Email it").click();
-    multiAutocompleteInput()
+    cy.findByPlaceholderText("Enter user names or email addresses")
+      .click()
       .type(`${admin.first_name} ${admin.last_name}{enter}`)
       .blur(); // blur is needed to close the popover
 
