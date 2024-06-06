b6fc6b8474069bcc2e4503e8005f3aba5bd07480
Phoomparin Mano <phoomparin@gmail.com>
2024-06-06 22:06:12 +0700

    add simple table test data

--- frontend/src/metabase/visualizations/components/TableSimple/TableSimple.stories.tsx
+++ frontend/src/metabase/visualizations/components/TableSimple/TableSimple.stories.tsx
@@ -1,14 +1,13 @@
 import type { Story } from "@storybook/react";
 
 import { VisualizationWrapper } from "__support__/storybook";
-import { NumberColumn, StringColumn } from "__support__/visualizations";
 import { Box } from "metabase/ui";
 import { registerVisualization } from "metabase/visualizations";
 import Visualization from "metabase/visualizations/components/Visualization";
 import Table from "metabase/visualizations/visualizations/Table";
-import { createMockCard } from "metabase-types/api/mocks";
 
 import { TableSimple } from "./TableSimple";
+import RAW_SERIES from "./stories-data/table-simple-orders-with-people.json";
 
 // @ts-expect-error: incompatible prop types with registerVisualization
 registerVisualization(Table);
@@ -18,26 +17,10 @@ export default {
   component: TableSimple,
 };
 
-const MOCK_SERIES = [
-  {
-    card: createMockCard(),
-    data: {
-      cols: [
-        StringColumn({ name: "Dimension" }),
-        NumberColumn({ name: "Count" }),
-      ],
-      rows: [
-        ["foo", 1],
-        ["bar", 2],
-      ],
-    },
-  },
-];
-
 export const Default: Story = () => (
   <VisualizationWrapper>
     <Box h={500}>
-      <Visualization rawSeries={MOCK_SERIES} width={500} isDashboard />
+      <Visualization rawSeries={RAW_SERIES} />,
     </Box>
   </VisualizationWrapper>
 );
