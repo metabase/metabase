084055e2f56e9259871931f632abafbabe40257b
Emmad Usmani <emmadusmani@berkeley.edu>
2024-06-05 17:38:06 -0700

    add loki specs

--- frontend/src/metabase/static-viz/components/PieChart/stories-data/index.ts
+++ frontend/src/metabase/static-viz/components/PieChart/stories-data/index.ts
@@ -1,5 +1,43 @@
+import allNegative from "./all-negative.json";
+import allSettings from "./all-settings.json";
+import binnedDimension from "./binned-dimension.json";
+import booleanDimension from "./boolean-dimension.json";
+import colors from "./colors.json";
+import columnFormattingPercentagesOnChart from "./column-formatting-percentages-on-chart.json";
+import columnFormatting from "./column-formatting.json";
+import dateDimension from "./date-dimension.json";
 import defaultSettings from "./default-settings.json";
+import hideLegend from "./hide-legend.json";
+import hideTotal from "./hide-total.json";
+import largeMinimumSlicePercentage from "./large-min-slice-percentage.json";
+import mixedPositiveNegative from "./mixed-positive-negative.json";
+import numericDimension from "./numeric-dimension.json";
+import relativeDateDimension from "./relative-date-dimension.json";
+import showPercentagesOff from "./show-percentages-off.json";
+import showPercentagesOnChart from "./show-percentages-on-chart.json";
+import smallMinimumSlicePercentage from "./small-min-slice-percentage.json";
+import sortedMetricCol from "./sorted-metric-col.json";
+import zeroMinimumSlicePercentage from "./zero-min-slice-percentage.json";
 
 export const data = {
   defaultSettings,
+  allSettings,
+  colors,
+  hideLegend,
+  hideTotal,
+  smallMinimumSlicePercentage,
+  largeMinimumSlicePercentage,
+  zeroMinimumSlicePercentage,
+  showPercentagesOff,
+  showPercentagesOnChart,
+  allNegative,
+  mixedPositiveNegative,
+  columnFormatting,
+  columnFormattingPercentagesOnChart,
+  booleanDimension,
+  numericDimension,
+  binnedDimension,
+  dateDimension,
+  relativeDateDimension,
+  sortedMetricCol,
 };
