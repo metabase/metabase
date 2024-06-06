2f47b575c2a98a9e45aa37fef14a7a82a12d40ad
Alexander Solovyov <alexander@solovyov.net>
2024-06-06 17:03:06 +0300

    do not output exceptions used only for tests (#43723)

--- enterprise/backend/src/metabase_enterprise/serialization/api.clj
+++ enterprise/backend/src/metabase_enterprise/serialization/api.clj
@@ -26,6 +26,10 @@
 
 (set! *warn-on-reflection* true)
 
+(def ^:dynamic *additive-logging*
+  "If custom loggers should pass logs to parent loggers (to system Metabase logs), used to clean up test output."
+  true)
+
 ;;; Storage
 
 (def parent-dir "Dir for storing serialization API export-in-progress and archives."
@@ -69,7 +73,8 @@
         dst      (io/file (str (.getPath path) ".tar.gz"))
         log-file (io/file path "export.log")
         err      (atom nil)
-        report   (with-open [_logger (logger/for-ns 'metabase-enterprise.serialization log-file)]
+        report   (with-open [_logger (logger/for-ns 'metabase-enterprise.serialization log-file
+                                                    {:additive *additive-logging*})]
                    (try                 ; try/catch inside logging to log errors
                      (let [report (serdes/with-cache
                                     (-> (extract/extract opts)
@@ -96,7 +101,8 @@
   (let [dst      (io/file parent-dir (u.random/random-name))
         log-file (io/file dst "import.log")
         err      (atom nil)
-        report   (with-open [_logger (logger/for-ns 'metabase-enterprise.serialization log-file)]
+        report   (with-open [_logger (logger/for-ns 'metabase-enterprise.serialization log-file
+                                                    {:additive *additive-logging*})]
                    (try                 ; try/catch inside logging to log errors
                      (log/infof "Serdes import, size %s" size)
                      (let [path (u.compress/untgz file dst)]
