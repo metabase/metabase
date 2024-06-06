f38cf23fa67290b11a8e79ec8128b789ba5619bb
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-06-06 16:25:54 -0400

    recents

--- frontend/src/metabase/browse/components/BrowseModels.tsx
+++ frontend/src/metabase/browse/components/BrowseModels.tsx
@@ -2,13 +2,15 @@ import { useMemo } from "react";
 import { t } from "ttag";
 
 import NoResults from "assets/img/no_results.svg";
+import { useListRecentItemsQuery } from "metabase/api";
 import { useFetchModels } from "metabase/common/hooks/use-fetch-models";
 import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 import { color } from "metabase/lib/colors";
 import { PLUGIN_CONTENT_VERIFICATION } from "metabase/plugins";
 import { Box, Flex, Group, Icon, Stack, Title } from "metabase/ui";
-import type { ModelResult } from "metabase-types/api";
 
+import type { ModelResult } from "../types";
+import { isRecentModel } from "../types";
 import { filterModels } from "../utils";
 
 import {
@@ -20,22 +22,26 @@ import {
 } from "./BrowseContainer.styled";
 import { ModelExplanationBanner } from "./ModelExplanationBanner";
 import { ModelsTable } from "./ModelsTable";
+import { RecentModels } from "./RecentModels";
+import { getMaxRecentModelCount } from "./utils";
 
 const { availableModelFilters, useModelFilterSettings, ModelFilterControls } =
   PLUGIN_CONTENT_VERIFICATION;
 
 export const BrowseModels = () => {
+  /** Mapping of filter names to true if the filter is active or false if it is inactive */
   const [actualModelFilters, setActualModelFilters] = useModelFilterSettings();
 
-  const result = useFetchModels({ model_ancestors: true });
+  const modelsResult = useFetchModels({ model_ancestors: true });
 
   const { allModels, doVerifiedModelsExist } = useMemo(() => {
-    const allModels = (result.data?.data as ModelResult[] | undefined) ?? [];
+    const allModels =
+      (modelsResult.data?.data as ModelResult[] | undefined) ?? [];
     const doVerifiedModelsExist = allModels.some(
       model => model.moderated_status === "verified",
     );
     return { allModels, doVerifiedModelsExist };
-  }, [result]);
+  }, [modelsResult]);
 
   const { filteredModels } = useMemo(() => {
     // If no models are verified, don't filter them
@@ -44,6 +50,25 @@ export const BrowseModels = () => {
       : allModels;
     return { filteredModels };
   }, [allModels, actualModelFilters, doVerifiedModelsExist]);
+
+  const recentModelsResult = useListRecentItemsQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+  });
+
+  const filteredRecentModels = useMemo(
+    () =>
+      filterModels(
+        recentModelsResult.data?.filter(isRecentModel),
+        actualModelFilters,
+        availableModelFilters,
+      ),
+    [recentModelsResult.data, actualModelFilters],
+  );
+
+  const recentModels = useMemo(() => {
+    const cap = getMaxRecentModelCount(allModels.length);
+    return filteredRecentModels.slice(0, cap);
+  }, [filteredRecentModels, allModels.length]);
 
   return (
     <BrowseContainer>
@@ -73,50 +98,33 @@ export const BrowseModels = () => {
       </BrowseHeader>
       <BrowseMain>
         <BrowseSection>
-          <BrowseModelsBody result={result} models={filteredModels} />
+          <LoadingAndErrorWrapper
+            error={modelsResult.error || recentModelsResult.error}
+            loading={modelsResult.isLoading || recentModelsResult.isLoading}
+            style={{ flex: 1 }}
+          >
+            {filteredModels.length ? (
+              <Stack mb="lg" spacing="md">
+                <ModelExplanationBanner />
+                <RecentModels models={recentModels} />
+                <ModelsTable models={filteredModels} />
+              </Stack>
+            ) : (
+              <CenteredEmptyState
+                title={<Box mb=".5rem">{t`No models here yet`}</Box>}
+                message={
+                  <Box maw="24rem">{t`Models help curate data to make it easier to find answers to questions all in one place.`}</Box>
+                }
+                illustrationElement={
+                  <Box mb=".5rem">
+                    <img src={NoResults} />
+                  </Box>
+                }
+              />
+            )}
+          </LoadingAndErrorWrapper>
         </BrowseSection>
       </BrowseMain>
     </BrowseContainer>
-  );
-};
-
-export const BrowseModelsBody = ({
-  models,
-  result,
-}: {
-  models: ModelResult[];
-  result: { error?: any; isLoading: boolean };
-}) => {
-  if (result.error || result.isLoading) {
-    return (
-      <LoadingAndErrorWrapper
-        error={result.error}
-        loading={result.isLoading}
-        style={{ flex: 1 }}
-      />
-    );
-  }
-
-  if (models.length) {
-    return (
-      <Stack mb="lg" spacing="md">
-        <ModelExplanationBanner />
-        <ModelsTable models={models} />
-      </Stack>
-    );
-  }
-
-  return (
-    <CenteredEmptyState
-      title={<Box mb=".5rem">{t`No models here yet`}</Box>}
-      message={
-        <Box maw="24rem">{t`Models help curate data to make it easier to find answers to questions all in one place.`}</Box>
-      }
-      illustrationElement={
-        <Box mb=".5rem">
-          <img src={NoResults} />
-        </Box>
-      }
-    />
   );
 };
