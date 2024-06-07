786ec1a04439338db566a157dc0ef1a16e0403db
Anton Kulyk <kuliks.anton@gmail.com>
2024-06-07 16:18:35 +0100

    Prevent unnecessary redirects to home dashboard

--- frontend/src/metabase/nav/components/AppBar/AppBarLogo.tsx
+++ frontend/src/metabase/nav/components/AppBar/AppBarLogo.tsx
@@ -1,4 +1,9 @@
+import type { MouseEvent } from "react";
+import { useLocation } from "react-use";
+
 import LogoIcon from "metabase/components/LogoIcon";
+import { useSelector } from "metabase/lib/redux";
+import { getCustomHomePageDashboardId } from "metabase/selectors/app";
 
 import { LogoLink } from "./AppBarLogo.styled";
 
@@ -15,15 +20,32 @@ export function AppBarLogo({
   isNavBarEnabled,
   onLogoClick,
 }: AppBarLogoProps): JSX.Element | null {
+  const location = useLocation();
+  const homepageDashboardId = useSelector(getCustomHomePageDashboardId);
+
   if (!isLogoVisible) {
     return null;
   }
+
+  const isAtDashboardHomepage = Boolean(
+    homepageDashboardId &&
+      location.pathname?.startsWith(`/dashboard/${homepageDashboardId}`),
+  );
+
+  const handleClick = (event: MouseEvent) => {
+    // Prevent navigating to the dashboard homepage when a user is already there
+    // https://github.com/metabase/metabase/issues/43800
+    if (isAtDashboardHomepage) {
+      event.preventDefault();
+    }
+    onLogoClick?.();
+  };
 
   return (
     <LogoLink
       to="/"
       isSmallAppBar={Boolean(isSmallAppBar)}
-      onClick={onLogoClick}
+      onClick={handleClick}
       disabled={!isNavBarEnabled}
       data-testid="main-logo-link"
     >
