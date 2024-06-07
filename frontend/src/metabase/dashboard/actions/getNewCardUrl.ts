b6114f3fffee5ac97e4973f99b258ca94d2cbb35
Kamil Mielnik <kamil@kamilmielnik.com>
2024-06-07 16:46:36 +0700

    Extract getNewCardUrl and move getParametersMappedToDashcard closer to it

---
+++ frontend/src/metabase/dashboard/actions/getNewCardUrl.ts
@@ -0,0 +1,88 @@
+import _ from "underscore";
+
+import { getCardAfterVisualizationClick } from "metabase/visualizations/lib/utils";
+import Question from "metabase-lib/v1/Question";
+import type Metadata from "metabase-lib/v1/metadata/Metadata";
+import type { ParameterWithTarget } from "metabase-lib/v1/parameters/types";
+import * as ML_Urls from "metabase-lib/v1/urls";
+import type {
+  Card,
+  Dashboard,
+  ParameterId,
+  ParameterValueOrArray,
+  QuestionDashboardCard,
+} from "metabase-types/api";
+import type { StoreDashboard } from "metabase-types/store";
+
+/**
+ * All navigation URLs from dashboards to cards (e.x. clicking a title, drill through)
+ * should come from this function, which merges any currently applied dashboard filters
+ * into the new card / URL parameters.
+ */
+export const getNewCardUrl = ({
+  metadata,
+  dashboard,
+  parameterValues,
+  nextCard,
+  previousCard,
+  dashcard,
+  objectId,
+}: {
+  metadata: Metadata;
+  dashboard: Dashboard | StoreDashboard;
+  parameterValues: Record<ParameterId, ParameterValueOrArray>;
+  nextCard: Card;
+  previousCard: Card;
+  dashcard: QuestionDashboardCard;
+  objectId?: number | string;
+}): string | undefined => {
+  const cardAfterClick = getCardAfterVisualizationClick(nextCard, previousCard);
+
+  const previousQuestion = new Question(previousCard, metadata);
+  const nextQuestion = previousQuestion.canRunAdhocQuery()
+    ? new Question(cardAfterClick, metadata)
+        .setDisplay(cardAfterClick.display || previousCard.display)
+        .setSettings(dashcard.card.visualization_settings)
+        .lockDisplay()
+    : new Question(dashcard.card, metadata).setDashboardProps({
+        dashboardId: dashboard.id,
+        dashcardId: dashcard.id,
+      });
+
+  const parametersMappedToCard = getParametersMappedToDashcard(
+    dashboard.parameters,
+    dashcard,
+  );
+
+  const url = ML_Urls.getUrlWithParameters(
+    nextQuestion,
+    parametersMappedToCard,
+    parameterValues,
+    {
+      objectId,
+    },
+  );
+
+  return url;
+};
+
+export function getParametersMappedToDashcard(
+  parameters: Dashboard["parameters"],
+  dashcard: QuestionDashboardCard,
+): ParameterWithTarget[] {
+  const { parameter_mappings } = dashcard;
+  return (parameters || [])
+    .map(parameter => {
+      const mapping = _.findWhere(parameter_mappings || [], {
+        parameter_id: parameter.id,
+      });
+
+      if (mapping) {
+        return {
+          ...parameter,
+          target: mapping.target,
+        };
+      }
+    })
+    .filter((parameter): parameter is ParameterWithTarget => parameter != null);
+}
