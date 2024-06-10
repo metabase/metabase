84afb90cc52e6fd07ab3f14c02d9e406bf6ab245
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-06-10 21:19:18 +0200

    Remove the `flaky` tag

--- e2e/test/scenarios/organization/bookmarks-question.cy.spec.js
+++ e2e/test/scenarios/organization/bookmarks-question.cy.spec.js
@@ -15,66 +15,62 @@ describe("scenarios > question > bookmarks", () => {
     cy.signInAsAdmin();
   });
 
-  it(
-    "should add, update bookmark name when question name is updated, then remove bookmark from question page",
-    { tags: "@flaky" },
-    () => {
-      visitQuestion(ORDERS_QUESTION_ID);
-      toggleBookmark();
+  it("should add, update bookmark name when question name is updated, then remove bookmark from question page", () => {
+    visitQuestion(ORDERS_QUESTION_ID);
+    toggleBookmark();
 
-      openNavigationSidebar();
-      navigationSidebar().within(() => {
-        getSectionTitle(/Bookmarks/);
-        cy.findByText("Orders");
-      });
+    openNavigationSidebar();
+    navigationSidebar().within(() => {
+      getSectionTitle(/Bookmarks/);
+      cy.findByText("Orders");
+    });
 
-      // Rename bookmarked question
-      cy.findByTestId("saved-question-header-title").click().type(" 2").blur();
+    // Rename bookmarked question
+    cy.findByTestId("saved-question-header-title").click().type(" 2").blur();
 
-      navigationSidebar().within(() => {
-        cy.findByText("Orders 2");
-      });
+    navigationSidebar().within(() => {
+      cy.findByText("Orders 2");
+    });
 
-      cy.log("Turn the question into a model");
-      openQuestionActions();
-      cy.findByRole("dialog").contains("Turn into a model").click();
-      cy.findByRole("dialog").contains("Turn this into a model").click();
-      cy.findByRole("status")
-        .should("contain", "This is a model now.")
-        // Close this toast as soon we confim it exists!
-        // It lingers in the UI far too long which is causing flakiness later on
-        // when we assert on the next toast (when we turn the model back to the question).
-        .icon("close")
-        .click();
+    cy.log("Turn the question into a model");
+    openQuestionActions();
+    cy.findByRole("dialog").contains("Turn into a model").click();
+    cy.findByRole("dialog").contains("Turn this into a model").click();
+    cy.findByRole("status")
+      .should("contain", "This is a model now.")
+      // Close this toast as soon we confim it exists!
+      // It lingers in the UI far too long which is causing flakiness later on
+      // when we assert on the next toast (when we turn the model back to the question).
+      .icon("close")
+      .click();
 
-      navigationSidebar().within(() => {
-        cy.findByLabelText(/Bookmarks/)
-          .icon("model")
-          .should("exist");
-      });
+    navigationSidebar().within(() => {
+      cy.findByLabelText(/Bookmarks/)
+        .icon("model")
+        .should("exist");
+    });
 
-      cy.log("Turn the model back into a question");
-      openQuestionActions();
-      cy.findByRole("dialog").contains("Turn back to saved question").click();
-      cy.findByRole("status").should("contain", "This is a question now.");
+    cy.log("Turn the model back into a question");
+    openQuestionActions();
+    cy.findByRole("dialog").contains("Turn back to saved question").click();
+    cy.findByRole("status").should("contain", "This is a question now.");
 
-      openNavigationSidebar();
-      cy.log("Should not find bookmark");
-      navigationSidebar().within(() => {
-        cy.findByLabelText(/Bookmarks/)
-          .icon("model")
-          .should("not.exist");
-      });
+    openNavigationSidebar();
+    cy.log("Should not find bookmark");
+    navigationSidebar().within(() => {
+      cy.findByLabelText(/Bookmarks/)
+        .icon("model")
+        .should("not.exist");
+    });
 
-      // Remove bookmark
-      toggleBookmark({ wasSelected: true });
+    // Remove bookmark
+    toggleBookmark({ wasSelected: true });
 
-      navigationSidebar().within(() => {
-        getSectionTitle(/Bookmarks/).should("not.exist");
-        cy.findByText("Orders 2").should("not.exist");
-      });
-    },
-  );
+    navigationSidebar().within(() => {
+      getSectionTitle(/Bookmarks/).should("not.exist");
+      cy.findByText("Orders 2").should("not.exist");
+    });
+  });
 });
 
 function toggleBookmark({ wasSelected = false } = {}) {
