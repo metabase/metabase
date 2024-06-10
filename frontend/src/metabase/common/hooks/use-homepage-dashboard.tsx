ae631f74d3be3377db394d17f2530c41409079b9
Anton Kulyk <kuliks.anton@gmail.com>
2024-06-10 14:50:02 +0100

    Handle sub-root Metabase URLs

--- frontend/src/metabase/common/hooks/use-homepage-dashboard.tsx
+++ frontend/src/metabase/common/hooks/use-homepage-dashboard.tsx
@@ -5,13 +5,18 @@ import { useSelector } from "metabase/lib/redux";
 import { getCustomHomePageDashboardId } from "metabase/selectors/app";
 import { getSettingsLoading } from "metabase/selectors/settings";
 
+import { useSetting } from "./use-setting";
+
 export const useHomepageDashboard = () => {
   const dashboardId = useSelector(getCustomHomePageDashboardId);
   const isLoadingSettings = useSelector(getSettingsLoading);
-  const { pathname } = useLocation();
+  const siteUrl = useSetting("site-url");
+  const location = useLocation();
 
   const { data: dashboard, isLoading: isLoadingDashboard } =
     useGetDashboardQuery(dashboardId ? { id: dashboardId } : skipToken);
+
+  const pathname = location?.href?.replace(siteUrl, "");
 
   const isAtHomepageDashboard = Boolean(
     dashboardId && pathname?.startsWith(`/dashboard/${dashboardId}`),
