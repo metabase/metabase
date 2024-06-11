1f7a45428d029e54c85b389981d7bed20b7a05d7
Phoomparin Mano <phoomparin@gmail.com>
2024-06-11 23:49:54 +0700

    fix static dashboard theming

--- frontend/src/metabase/visualizations/components/TableSimple/TableSimple.stories.tsx
+++ frontend/src/metabase/visualizations/components/TableSimple/TableSimple.stories.tsx
@@ -6,37 +6,37 @@ import {
 } from "__support__/storybook";
 import type { MetabaseTheme } from "embedding-sdk";
 import { Box } from "metabase/ui";
-import { registerVisualization } from "metabase/visualizations";
 import Visualization from "metabase/visualizations/components/Visualization";
-import Table from "metabase/visualizations/visualizations/Table";
 
-import { TableSimple } from "./TableSimple";
 import RAW_SERIES from "./stories-data/table-simple-orders-with-people.json";
 
-// @ts-expect-error: incompatible prop types with registerVisualization
-registerVisualization(Table);
-
 export default {
-  title: "visualizations/TableSimple",
-  component: TableSimple,
+  title: "viz/TableSimple",
 };
 
 export const Default: Story = () => (
   <VisualizationWrapper>
     <Box h={500}>
-      <Visualization rawSeries={RAW_SERIES} />,
+      <Visualization rawSeries={RAW_SERIES} isDashboard />,
     </Box>
   </VisualizationWrapper>
 );
 
 export const EmbeddingTheme: Story = () => {
   const theme: MetabaseTheme = {
+    colors: {
+      brand: "#eccc68",
+      "text-primary": "#ffffff",
+      "text-secondary": "#ffffff",
+      "text-tertiary": "#ffffff",
+    },
     components: {
       dashboard: {
-        card: { backgroundColor: "#2d2d30" },
+        card: { backgroundColor: "#2f3542" },
       },
       table: {
-        cell: { backgroundColor: "#2f3640" },
+        cell: { textColor: "#dfe4ea", backgroundColor: "#2f3640" },
+        idColumn: { textColor: "#fff", backgroundColor: "#eccc6855" },
       },
     },
   };
@@ -44,7 +44,7 @@ export const EmbeddingTheme: Story = () => {
   return (
     <SdkVisualizationWrapper theme={theme}>
       <Box h={500}>
-        <Visualization rawSeries={RAW_SERIES} />,
+        <Visualization rawSeries={RAW_SERIES} isDashboard />,
       </Box>
     </SdkVisualizationWrapper>
   );
