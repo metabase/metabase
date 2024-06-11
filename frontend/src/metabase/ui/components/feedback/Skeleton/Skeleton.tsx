d8dd37547709102aa38426e571a4289282fe55b3
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-06-11 12:18:19 -0400

    fix

--- frontend/src/metabase/ui/components/feedback/Skeleton/Skeleton.tsx
+++ frontend/src/metabase/ui/components/feedback/Skeleton/Skeleton.tsx
@@ -2,20 +2,19 @@ import type { SkeletonProps } from "@mantine/core";
 import { Skeleton as MantineSkeleton } from "@mantine/core";
 import { useMemo } from "react";
 
-export const Skeleton = (
-  props: SkeletonProps & {
-    /** Automatically assign a natural-looking, random width to the skeleton */
-    natural?: boolean;
-  },
-) => {
+export const Skeleton = ({
+  natural,
+  ...props
+}: SkeletonProps & {
+  /** Automatically assign a natural-looking, random width to the skeleton */
+  natural?: boolean;
+}) => {
   // FIXME: remove this
-  props.natural &&= (
-    window as { noNaturalSkeletons?: boolean }
-  ).noNaturalSkeletons;
+  natural &&= (window as { noNaturalSkeletons?: boolean }).noNaturalSkeletons;
 
   const width = useMemo(
-    () => (props.natural ? `${Math.random() * 30 + 50}%` : props.width),
-    [props.natural, props.width],
+    () => (natural ? `${Math.random() * 30 + 50}%` : props.width),
+    [natural, props.width],
   );
   return <MantineSkeleton width={width} {...props} />;
 };
