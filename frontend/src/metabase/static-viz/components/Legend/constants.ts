b8ce2c2aba9383bf181c78527b776b211542b32f
Emmad Usmani <emmadusmani@berkeley.edu>
2024-06-11 10:52:29 -0700

    make dot margin smaller

--- frontend/src/metabase/static-viz/components/Legend/constants.ts
+++ frontend/src/metabase/static-viz/components/Legend/constants.ts
@@ -1,5 +1,5 @@
 export const LEGEND_CIRCLE_SIZE = 10;
-export const LEGEND_CIRCLE_MARGIN_RIGHT = 8;
+export const LEGEND_CIRCLE_MARGIN_RIGHT = 4;
 export const LEGEND_ITEM_MARGIN_RIGHT = 16;
 
 export const DEFAULT_LEGEND_FONT_SIZE = 14;
