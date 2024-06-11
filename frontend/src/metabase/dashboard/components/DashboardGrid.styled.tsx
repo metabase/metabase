d41a4006e45f104fc4caf483ee68119357130357
Phoomparin Mano <poom@metabase.com>
2024-06-11 17:29:06 +0700

    use the shadow CSS variable
    Co-authored-by: Denis Berezin <denis.berezin@metabase.com>

--- frontend/src/metabase/dashboard/components/DashboardGrid.styled.tsx
+++ frontend/src/metabase/dashboard/components/DashboardGrid.styled.tsx
@@ -94,6 +94,6 @@ function getDashboardCardShadowOrBorder({ theme }: { theme: MantineTheme }) {
   }
 
   return css`
-    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.13);
+    box-shadow: 0px 1px 3px var(--mb-color-shadow);
   `;
 }
