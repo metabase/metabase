65ea62a6e76cf32b530e2932e5733fcc509051fe
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-06-11 12:52:06 -0400

    Add shimmer shortcut

--- frontend/src/metabase/ui/components/feedback/Skeleton/Skeleton.styled.tsx
+++ frontend/src/metabase/ui/components/feedback/Skeleton/Skeleton.styled.tsx
@@ -9,22 +9,37 @@ const shimmerAnimation = keyframes`
 }
 `;
 
-export const getSkeletonOverrides = (): MantineThemeOverride["components"] => ({
-  Skeleton: {
-    styles: _theme => {
-      return {
-        root: {
-          "background-color": "rgba(0, 0, 0, .03)",
-          "&::before": {
-            background:
-              "linear-gradient(100deg, transparent, rgba(0, 0, 0, .03) 50%, transparent) ! important",
-            animation: `${shimmerAnimation} 1.2s linear infinite`,
-          },
-          "&::after": {
-            display: "none",
-          },
-        },
-      };
+export const getSkeletonOverrides = (): MantineThemeOverride["components"] => {
+  const win = window as {
+    shimmerSkeletons?: boolean;
+  };
+  win.shimmerSkeletons ??= true;
+  return {
+    Skeleton: {
+      styles: _theme => {
+        return {
+          root: win.shimmerSkeletons
+            ? {
+                "background-color": "rgba(0, 0, 0, .03)",
+                "&::before": {
+                  background:
+                    "linear-gradient(100deg, transparent, rgba(0, 0, 0, .03) 50%, transparent) ! important",
+                  animation: `${shimmerAnimation} 1.2s linear infinite`,
+                },
+                "&::after": {
+                  display: "none",
+                },
+              }
+            : {
+                "&::before": {
+                  background: "rgba(0, 0, 0, .03) ! important",
+                },
+                "&::after": {
+                  background: "rgba(0, 0, 0, .06) ! important",
+                },
+              },
+        };
+      },
     },
-  },
-});
+  };
+};
