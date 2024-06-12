888556a83e38d175db745c7faf9012b568375f33
Anton Kulyk <kuliks.anton@gmail.com>
2024-06-12 18:23:00 +0100

    Fix type errors

--- frontend/src/metabase/visualizations/lib/sync-settings.unit.spec.ts
+++ frontend/src/metabase/visualizations/lib/sync-settings.unit.spec.ts
@@ -1,8 +1,13 @@
 import { getColumnKey } from "metabase-lib/v1/queries/utils/get-column-key";
+import type {
+  DatasetColumn,
+  TableColumnOrderSetting,
+} from "metabase-types/api";
+import { createMockDataset } from "metabase-types/api/mocks";
 
 import { syncVizSettingsWithQueryResults } from "./sync-settings";
 
-const cols = [
+const cols: DatasetColumn[] = [
   {
     display_name: "num",
     source: "native",
@@ -31,26 +36,25 @@ const cols = [
   },
 ];
 
+const vizSettingColumns: TableColumnOrderSetting[] = cols.map(column => ({
+  key: getColumnKey(column),
+  name: column.name,
+  fieldRef: column.field_ref,
+  enabled: true,
+}));
+
 const noVizSettings = {};
 const vizSettings = {
-  "table.columns": [
-    {
-      name: "num",
-      fieldRef: ["field", "num", { "base-type": "type/Float" }],
-      enabled: true,
-    },
-    {
-      name: "text",
-      fieldRef: ["field", "text", { "base-type": "type/Text" }],
-      enabled: true,
-    },
-  ],
+  "table.columns": vizSettingColumns,
 };
 
 describe("syncVizSettingsWithQueryResults", () => {
   describe("when 'table.columns' setting is not defined", () => {
     it("should do nothing when given empty query results", () => {
-      const queryResults = {};
+      const queryResults = createMockDataset({
+        error: { status: 500 },
+        data: { cols: [], rows: [] },
+      });
       const syncedSettings = syncVizSettingsWithQueryResults(
         noVizSettings,
         queryResults,
@@ -59,7 +63,7 @@ describe("syncVizSettingsWithQueryResults", () => {
     });
 
     it("should do nothing when given query results with no columns", () => {
-      const queryResults = { data: { cols: [] } };
+      const queryResults = createMockDataset({ data: { cols: [], rows: [] } });
       const syncedSettings = syncVizSettingsWithQueryResults(
         noVizSettings,
         queryResults,
@@ -68,7 +72,7 @@ describe("syncVizSettingsWithQueryResults", () => {
     });
 
     it("should do nothing when given query results with columns", () => {
-      const queryResults = { data: { cols } };
+      const queryResults = createMockDataset({ data: { cols } });
       const syncedSettings = syncVizSettingsWithQueryResults(
         noVizSettings,
         queryResults,
@@ -81,7 +85,7 @@ describe("syncVizSettingsWithQueryResults", () => {
     // Adding a column with same name is covered as well,
     // as name is generated at FE and it will be unique (e.g. foo -> foo_2)
     it("should handle the addition and removal of columns", () => {
-      const addedColumn = {
+      const addedColumn: DatasetColumn = {
         name: "foo",
         display_name: "foo",
         source: "native",
@@ -94,7 +98,9 @@ describe("syncVizSettingsWithQueryResults", () => {
         ],
         base_type: "type/Float",
       };
-      const queryResults = { data: { cols: [...cols.slice(1), addedColumn] } };
+      const queryResults = createMockDataset({
+        data: { cols: [...cols.slice(1), addedColumn] },
+      });
 
       const syncedSettings = syncVizSettingsWithQueryResults(
         vizSettings,
@@ -115,7 +121,7 @@ describe("syncVizSettingsWithQueryResults", () => {
     });
 
     it("should handle extraneous column props mutation", () => {
-      const updatedColumn = {
+      const updatedColumn: DatasetColumn = {
         display_name: "num with mutated display_name",
         source: "native",
         field_ref: [
@@ -128,9 +134,9 @@ describe("syncVizSettingsWithQueryResults", () => {
         name: "foo",
         base_type: "type/Float",
       };
-      const queryResults = {
+      const queryResults = createMockDataset({
         data: { cols: [updatedColumn, ...cols.slice(1)] },
-      };
+      });
 
       const syncedSettings = syncVizSettingsWithQueryResults(
         vizSettings,
@@ -151,7 +157,7 @@ describe("syncVizSettingsWithQueryResults", () => {
     });
 
     it("should handle existing column's field_ref mutation", () => {
-      const updatedColumn = {
+      const updatedColumn: DatasetColumn = {
         name: "foo",
         display_name: "foo",
         source: "native",
@@ -164,9 +170,9 @@ describe("syncVizSettingsWithQueryResults", () => {
         ],
         base_type: "type/Integer",
       };
-      const queryResults = {
+      const queryResults = createMockDataset({
         data: { cols: [updatedColumn, ...cols.slice(1)] },
-      };
+      });
 
       const syncedSettings = syncVizSettingsWithQueryResults(
         vizSettings,
@@ -187,7 +193,9 @@ describe("syncVizSettingsWithQueryResults", () => {
     });
 
     it("shouldn't update settings if order of columns has changed", () => {
-      const queryResults = { data: { cols: [cols[1], cols[0]] } };
+      const queryResults = createMockDataset({
+        data: { cols: [cols[1], cols[0]] },
+      });
 
       const syncedSettings = syncVizSettingsWithQueryResults(
         vizSettings,
