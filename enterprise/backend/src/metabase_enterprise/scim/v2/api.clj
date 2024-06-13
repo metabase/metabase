8a6eaf475972f8362eec28db2e91440701cda2fe
Noah Moss <noahbmoss@gmail.com>
2024-06-13 14:15:47 -0400

    GET user endpoint

--- enterprise/backend/src/metabase_enterprise/scim/v2/api.clj
+++ enterprise/backend/src/metabase_enterprise/scim/v2/api.clj
@@ -4,11 +4,53 @@
   (:require
    [compojure.core :refer [GET]]
    [metabase.api.common :as api :refer [defendpoint]]
+   [metabase.util.malli :as mu]
+   [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
+
+#_(def ^:private group-schema-uri "urn:ietf:params:scim:schemas:core:2.0:Group")
+(def ^:private user-schema-uri "urn:ietf:params:scim:schemas:core:2.0:User")
+
+(def SCIMUser
+  "Malli schema for a SCIM user"
+  [:map
+   [:schemas [:sequential ms/NonBlankString]]
+   [:id ms/NonBlankString]
+   [:userName ms/NonBlankString]
+   [:name [:map
+           [:givenName string?]
+           [:familyName string?]]]
+   [:emails [:sequential
+             [:map
+              [:value ms/NonBlankString]
+              [:type {:optional true} [:enum "work" "home" "other"]]
+              [:primary {:optional true} boolean?]]]]
+   [:active boolean?]])
+
+(mu/defn mb-user->scim :- SCIMUser
+  "Given a Metabase user, returns a SCIM user"
+  [user]
+  {:schemas  [user-schema-uri]
+   :id       (:entity_id user)
+   :userName (:email user)
+   :name     {:givenName  (:first_name user)
+              :familyName (:last_name user)}
+   :emails   [{:value (:email user)}]
+   :active   (:is_active user)
+   :groups   {}})
 
 (defendpoint GET "/Users"
   "Fetches a list of users."
   []
   (t2/select :model/User))
+
+(defendpoint GET "/Users/:id"
+  "Fetches a single user"
+  [id]
+  {id ms/PositiveInt}
+  (-> (t2/select-one [:model/User :id :first_name :last_name :email :is_active :entity_id]
+                     :id id)
+      (t2/hydrate :user_group_memberships)
+      mb-user->scim))
 
 (api/define-routes)
--- enterprise/backend/test/metabase_enterprise/scim/v2/api_test.clj
+++ enterprise/backend/test/metabase_enterprise/scim/v2/api_test.clj
@@ -3,23 +3,42 @@
    [clojure.test :refer :all]
    [metabase-enterprise.scim.api :as scim]
    [metabase.http-client :as client]
-   [metabase.test :as mt]))
+   [metabase.test :as mt]
+   [toucan2.core :as t2]))
+
+(def ^:private scim-api-key
+  (-> (#'scim/refresh-scim-api-key! (mt/user->id :crowberto)) :unmasked_key))
+
+(defn- scim-client
+  "Wrapper for `metabase.http-client/client` which includes the SCIM key in the Authorization header"
+  [method expected-status-code endpoint]
+  (client/client method
+                 expected-status-code
+                 endpoint
+                 {:request-options
+                  {:headers {"authorization" (format "Bearer %s" scim-api-key)}}}))
 
 (deftest scim-authentication-test
   (mt/with-premium-features #{:scim}
-    (let [scim-api-key (-> (#'scim/refresh-scim-api-key! (mt/user->id :crowberto)) :unmasked_key)]
-      (testing "SCIM endpoints require a valid SCIM API key passed in the authorization header"
-        (client/client :get 200 "ee/scim/v2/Users"
-                       {:request-options
-                        {:headers {"authorization" (format "Bearer %s" scim-api-key)}}}))
+    (testing "SCIM endpoints require a valid SCIM API key passed in the authorization header"
+      (scim-client :get 200 "ee/scim/v2/Users"))
 
-      (testing "The SCIM API key cannot be used for non-SCIM endpoints"
-        (client/client :get 401 "user"
-                       {:request-options
-                        {:headers {"authorization" (format "Bearer %s" scim-api-key)}}}))
+    (testing "The SCIM API key cannot be used for non-SCIM endpoints"
+      (scim-client :get 401 "user"))
 
-      (testing "SCIM endpoints do not allow normal auth"
-        (mt/user-http-request :crowberto :get 401 "ee/scim/v2/Users"))
+    (testing "SCIM endpoints do not allow normal auth"
+      (mt/user-http-request :crowberto :get 401 "ee/scim/v2/Users"))
 
-      (testing "A SCIM API key cannot be passed via the x-api-key header"
-        (client/client :get 401 "ee/scim/v2/Users" {:request-options {:headers {"x-api-key" scim-api-key}}})))))
+    (testing "A SCIM API key cannot be passed via the x-api-key header"
+      (client/client :get 401 "ee/scim/v2/Users" {:request-options {:headers {"x-api-key" scim-api-key}}}))))
+
+(deftest fetch-user-test
+  (mt/with-premium-features #{:scim}
+    (testing "A single user can be fetched in the SCIM format"
+      (is (= {:schemas  ["urn:ietf:params:scim:schemas:core:2.0:User"]
+              :id       (t2/select-one-fn :entity_id :model/User :id (mt/user->id :rasta))
+              :userName "rasta@metabase.com"
+              :name     {:givenName "Rasta" :familyName "Toucan"}
+              :emails   [{:value "rasta@metabase.com"}]
+              :active   true}
+             (scim-client :get 200 (format "ee/scim/v2/Users/%d" (mt/user->id :rasta))))))))
