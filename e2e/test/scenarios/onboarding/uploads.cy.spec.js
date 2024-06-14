00c1bcaf969b4995751a95937bebae7aa6a3040f
losrebellos <ben@benoitvinay.com>
2024-06-14 22:01:19 +0200

    Misc for Snowplow

--- e2e/test/scenarios/onboarding/uploads.cy.spec.js
+++ e2e/test/scenarios/onboarding/uploads.cy.spec.js
@@ -9,36 +9,40 @@ import {
   enableUploads,
 } from "e2e/support/helpers";
 
-describe("CSV Uploading", { tags: ["@external", "@actions"] }, () => {
-  before(() => {
-    restore("postgres-12");
-    cy.signInAsAdmin();
+describeWithSnowplow(
+  "Upload CSV button in Sidebar",
+  { tags: ["@external", "@actions"] },
+  () => {
+    before(() => {
+      restore("postgres-12");
+      cy.signInAsAdmin();
 
-    enableUploads("postgres");
-  });
+      enableUploads("postgres");
+    });
 
-  beforeEach(() => {
-    cy.intercept({ method: "GET", url: "/api/session/properties" }, request => {
-      request.on("response", response => {
-        if (typeof response.body === "object") {
-          // Setting the DWH feature
-          const tokenFeatures = response.body["token-features"];
-          response.body = {
-            ...response.body,
-            "token-features": { ...tokenFeatures, attached_dwh: true },
-          };
-        }
-      });
-    }).as("getSessionProperties");
-    cy.intercept("POST", "/api/card/from-csv").as("uploadCSV");
+    beforeEach(() => {
+      cy.intercept(
+        { method: "GET", url: "/api/session/properties" },
+        request => {
+          request.on("response", response => {
+            if (typeof response.body === "object") {
+              // Setting the DWH feature
+              const tokenFeatures = response.body["token-features"];
+              response.body = {
+                ...response.body,
+                "token-features": { ...tokenFeatures, attached_dwh: true },
+              };
+            }
+          });
+        },
+      ).as("getSessionProperties");
 
-    resetSnowplow();
-    cy.signInAsAdmin();
-    enableTracking();
-    cy.visit("/");
-  });
+      resetSnowplow();
+      cy.signInAsAdmin();
+      enableTracking();
+      cy.visit("/");
+    });
 
-  describeWithSnowplow("Upload CSV button in Sidebar", () => {
     CSV_FILES.forEach(testFile => {
       it(`${testFile.valid ? "Can" : "Cannot"} upload ${
         testFile.fileName
@@ -61,5 +65,5 @@ describe("CSV Uploading", { tags: ["@external", "@actions"] }, () => {
         });
       });
     });
-  });
-});
+  },
+);
