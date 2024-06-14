55378ea0c8d1532d99c44b31be829da55be2554c
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2024-06-14 15:40:22 -0400

    menu item styling

--- frontend/src/metabase/visualizer/components/VisualizerMenuItem.tsx
+++ frontend/src/metabase/visualizer/components/VisualizerMenuItem.tsx
@@ -1,4 +1,10 @@
-import { Box } from "metabase/ui";
+/* eslint-disable*/
+
+/* Hacking in some styling, TODO - remove this */
+
+import { ActionIcon } from "@mantine/core";
+import { color } from "metabase/lib/colors";
+import { Box, Flex, Icon } from "metabase/ui";
 
 export function VisualizerMenuItem({
   item,
@@ -8,6 +14,35 @@ export function VisualizerMenuItem({
   onClick: (item: any) => void;
 }) {
   return (
-    <Box onClick={() => onClick(item)}>{item.displayName || item.name}</Box>
+    <Flex
+      align="center"
+      sx={{
+        ".Icon": {
+          opacity: "0",
+        },
+        ":hover": {
+          backgroundColor: color("brand-lighter"),
+          color: color("brand"),
+          borderRadius: "4px",
+          cursor: "pointer",
+
+          ".Icon": {
+            opacity: "1",
+          },
+        },
+      }}
+      onClick={() => onClick(item)}
+    >
+      <Box py="sm" px="md">
+        {item.displayName || item.name}
+      </Box>
+      {/* TODO - Only show this if the item is addable */}
+      <ActionIcon ml="auto">
+        <Icon
+          name="add"
+          style={{ borderLeft: `1px solid ${color("brand-lighter")}` }}
+        />
+      </ActionIcon>
+    </Flex>
   );
 }
