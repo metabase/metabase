be4b35a3af0c56505511af5c85fa0b67468f8696
lbrdnk <lbrdnk@users.noreply.github.com>
2024-06-17 16:57:17 +0200

    Add Databricks to broken drivers for timezone tests

--- test/metabase/query_processor_test/timezones_test.clj
+++ test/metabase/query_processor_test/timezones_test.clj
@@ -26,7 +26,11 @@
     :oracle
     :redshift
     :sparksql
-    :vertica})
+    :vertica
+    ;; TODO: Modify tests so databricks-jdbc does not have to be here.
+    ;;       `timezones-test/filter-test`
+    ;;       `timezones-test/sql-time-timezone-handling-test`
+    :databricks-jdbc})
 
 (defn- set-timezone-drivers
   "Drivers that support setting a Session timezone."
