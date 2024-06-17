d835e20a7a7bf6ebdf374c9ff701c829483909f2
Emmad Usmani <emmadusmani@berkeley.edu>
2024-06-17 15:00:26 -0700

    fix missing currency formatting

--- frontend/src/metabase/visualizations/lib/settings/column.js
+++ frontend/src/metabase/visualizations/lib/settings/column.js
@@ -22,7 +22,6 @@ import {
   isDate,
   isDateWithoutTime,
   isNumber,
-  isPercentage,
 } from "metabase-lib/v1/types/utils/isa";
 import { getColumnKey } from "metabase-lib/v1/queries/utils/get-column-key";
 import {
@@ -31,6 +30,14 @@ import {
   getColumnSettingKey,
 } from "metabase-lib/v1/queries/utils/dataset";
 import { nestedSettings } from "./nested";
+import {
+  getCurrency,
+  getDefaultCurrency,
+  getDefaultCurrencyInHeader,
+  getDefaultCurrencyStyle,
+  getDefaultNumberSeparators,
+  getDefaultNumberStyle,
+} from "metabase/visualizations/shared/settings/column";
 
 // HACK: cyclical dependency causing errors in unit tests
 // import { getVisualizationRaw } from "metabase/visualizations";
@@ -245,17 +252,6 @@ export const DATE_COLUMN_SETTINGS = {
   },
 };
 
-function getCurrency(currency, currencyStyle) {
-  return (0)
-    .toLocaleString("en", {
-      style: "currency",
-      currency: currency,
-      currencyDisplay: currencyStyle,
-    })
-    .replace(/0([.,]0+)?/, "")
-    .trim(); // strip off actual number
-}
-
 export const NUMBER_COLUMN_SETTINGS = {
   number_style: {
     title: t`Style`,
@@ -268,17 +264,7 @@ export const NUMBER_COLUMN_SETTINGS = {
         { name: t`Currency`, value: "currency" },
       ],
     },
-    getDefault: (column, settings) => {
-      if (isCurrency(column) && settings["currency"]) {
-        return "currency";
-      }
-
-      if (isPercentage(column)) {
-        return "percent";
-      }
-
-      return "decimal";
-    },
+    getDefault: getDefaultNumberStyle,
     // hide this for currency
     getHidden: (column, settings) =>
       isCurrency(column) && settings["number_style"] === "currency",
@@ -296,7 +282,7 @@ export const NUMBER_COLUMN_SETTINGS = {
       searchProp: "name",
       searchCaseSensitive: false,
     },
-    default: "USD",
+    getDefault: getDefaultCurrency,
     getHidden: (column, settings) => settings["number_style"] !== "currency",
   },
   currency_style: {
@@ -328,12 +314,7 @@ export const NUMBER_COLUMN_SETTINGS = {
         ],
       };
     },
-    getDefault: (column, settings) => {
-      const c = settings["currency"] || "USD";
-      return getCurrencySymbol(c) !== getCurrency(c, "code")
-        ? "symbol"
-        : "code";
-    },
+    getDefault: getDefaultCurrencyStyle,
     getHidden: (column, settings) => settings["number_style"] !== "currency",
     readDependencies: ["number_style"],
   },
@@ -346,7 +327,7 @@ export const NUMBER_COLUMN_SETTINGS = {
         { name: t`In every table cell`, value: false },
       ],
     },
-    default: true,
+    getDefault: getDefaultCurrencyInHeader,
     getHidden: (_column, settings, { series, forAdminSettings }) => {
       if (forAdminSettings === true) {
         return false;
@@ -372,7 +353,7 @@ export const NUMBER_COLUMN_SETTINGS = {
         { name: "100’000.00", value: ".’" },
       ],
     },
-    default: ".,",
+    getDefault: getDefaultNumberSeparators,
   },
   decimals: {
     title: t`Minimum number of decimal places`,
