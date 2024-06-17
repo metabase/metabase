4d2fc486bbe914e26cf85d866887fcd44007f216
Emmad Usmani <emmadusmani@berkeley.edu>
2024-06-17 16:23:02 -0700

    fix graalvm crashing

--- frontend/src/metabase/visualizations/lib/settings/column.js
+++ frontend/src/metabase/visualizations/lib/settings/column.js
@@ -31,7 +31,6 @@ import {
 } from "metabase-lib/v1/queries/utils/dataset";
 import { nestedSettings } from "./nested";
 import {
-  getCurrency,
   getDefaultCurrency,
   getDefaultCurrencyInHeader,
   getDefaultCurrencyStyle,
@@ -43,6 +42,17 @@ import {
 // import { getVisualizationRaw } from "metabase/visualizations";
 function getVisualizationRaw(...args) {
   return require("metabase/visualizations").getVisualizationRaw(...args);
+}
+
+function getCurrency(currency, currencyStyle) {
+  return (0)
+    .toLocaleString("en", {
+      style: "currency",
+      currency: currency,
+      currencyDisplay: currencyStyle,
+    })
+    .replace(/0([.,]0+)?/, "")
+    .trim(); // strip off actual number
 }
 
 const DEFAULT_GET_COLUMNS = (series, vizSettings) =>
--- frontend/src/metabase/visualizations/shared/settings/column.ts
+++ frontend/src/metabase/visualizations/shared/settings/column.ts
@@ -1,21 +1,5 @@
-import { getCurrencySymbol } from "metabase/lib/formatting";
 import { isCurrency, isPercentage } from "metabase-lib/v1/types/utils/isa";
 import type { DatasetColumn, VisualizationSettings } from "metabase-types/api";
-
-/**
- * Helper used for `getDefault` and `getProps` calculations in
- * frontend/src/metabase/visualizations/lib/settings/column.js
- */
-export function getCurrency(currency: string, currencyStyle: string) {
-  return (0)
-    .toLocaleString("en", {
-      style: "currency",
-      currency: currency,
-      currencyDisplay: currencyStyle,
-    })
-    .replace(/0([.,]0+)?/, "")
-    .trim(); // strip off actual number
-}
 
 export function getDefaultNumberStyle(
   column: DatasetColumn,
@@ -36,12 +20,35 @@ export function getDefaultCurrency() {
   return "USD";
 }
 
+// This list should be the same as the one in `supports-symbol?` in
+// currency.cljc. We copied it manually because importing it would lead to a
+// `window` usage in a dependent import, which will break static viz since it's
+// not supported by GraalVM.
+const CURRENCIES_WITH_SYMBOLS = new Set([
+  "USD",
+  "CAD",
+  "EUR",
+  "AUD",
+  "BRL",
+  "CNY",
+  "GBP",
+  "HKD",
+  "ILS",
+  "INR",
+  "JPY",
+  "KRW",
+  "MXN",
+  "NZD",
+  "TWD",
+  "VND",
+]);
+
 export function getDefaultCurrencyStyle(
   _column: any,
   settings: VisualizationSettings,
 ) {
   const c = settings["currency"] || "USD";
-  return getCurrencySymbol(c) !== getCurrency(c, "code") ? "symbol" : "code";
+  return CURRENCIES_WITH_SYMBOLS.has(c) ? "symbol" : "code";
 }
 
 export function getDefaultCurrencyInHeader() {
