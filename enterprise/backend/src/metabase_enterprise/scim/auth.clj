3a5f4cfe4f8f1cefaa2c747e5e09354c536111cd
Noah Moss <noahbmoss@gmail.com>
2024-06-18 17:58:14 -0400

    a couple settings and another test

--- enterprise/backend/src/metabase_enterprise/scim/auth.clj
+++ enterprise/backend/src/metabase_enterprise/scim/auth.clj
@@ -1,5 +1,6 @@
 (ns metabase-enterprise.scim.auth
   (:require
+   [metabase-enterprise.scim.api :as scim]
    [metabase.server.middleware.session :as mw.session]
    [metabase.util.password :as u.password]
    [toucan2.core :as t2]))
@@ -17,12 +18,13 @@
 (def ^:private error-schema-uri "urn:ietf:params:scim:api:messages:2.0:Error")
 
 (defn +scim-auth
-  "Middleware that returns a 401 response if `request` does not have a valid SCIM API key"
+  "Middleware that returns a 401 response if `request` does not have a valid SCIM API key, and/or if
+  SCIM is not enabled."
   [handler]
   (fn [request respond raise]
     (let [authorization-header (get-in request [:headers "authorization"])
           [_ api-key]          (re-matches #"Bearer (.*)" (or authorization-header ""))]
-      (if (validate-scim-api-key api-key)
+      (if (and (scim/scim-enabled) (validate-scim-api-key api-key))
         (handler request respond raise)
         (respond {:status 401
                   :body   {:schemas [error-schema-uri]
