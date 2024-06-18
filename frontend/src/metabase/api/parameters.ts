b85b43e2b816af771e685937b4dbf214ba1e2540
Romeo Van Snick <romeo@romeovansnick.be>
2024-06-18 16:23:33 +0200

    Move types to metabase-types

--- frontend/src/metabase/api/parameters.ts
+++ frontend/src/metabase/api/parameters.ts
@@ -1,37 +1,17 @@
-import { getNonVirtualFields } from "metabase-lib/v1/parameters/utils/parameter-fields";
-import { normalizeParameter } from "metabase-lib/v1/parameters/utils/parameter-values";
-import type { Parameter, ParameterValues } from "metabase-types/api";
+import type {
+  GetParameterValuesRequest,
+  ParameterValues,
+  SearchParameterValuesRequest,
+} from "metabase-types/api";
 
-import { Api, skipToken } from "./api";
+import { Api } from "./api";
 import { idTag } from "./tags";
 
-type NormalizedParameter = ReturnType<typeof normalizeParameter>;
-
-export type LoadParameterValuesParams = {
-  parameter: Parameter;
-};
-
-export type LoadParameterValuesInternalParams = {
-  parameter: NormalizedParameter;
-  field_ids: number[];
-};
-
-export type SearchParameterValuesParams = {
-  parameter: Parameter;
-  query: string;
-};
-
-export type SearchParameterValuesInternalParams = {
-  parameter: NormalizedParameter;
-  field_ids: number[];
-  query: string;
-};
-
-const parametersApi = Api.injectEndpoints({
+export const parametersApi = Api.injectEndpoints({
   endpoints: builder => ({
-    loadParameterValues: builder.query<
+    getParameterValues: builder.query<
       ParameterValues,
-      LoadParameterValuesInternalParams
+      GetParameterValuesRequest
     >({
       query: params => ({
         method: "POST",
@@ -44,52 +24,13 @@ const parametersApi = Api.injectEndpoints({
     }),
     searchParameterValues: builder.query<
       ParameterValues,
-      SearchParameterValuesInternalParams
+      SearchParameterValuesRequest
     >({
       query: params => ({
         method: "POST",
         url: `/api/dataset/parameter/search/${params.query}`,
         params,
       }),
-      // TODO: can we provide tags for this?
     }),
   }),
 });
-
-export const useLoadParameterValuesQuery: typeof parametersApi.useLoadParameterValuesQuery =
-  (params, options) => {
-    if (params === skipToken) {
-      return parametersApi.useLoadParameterValuesQuery(skipToken, options);
-    }
-
-    return parametersApi.useLoadParameterValuesQuery(
-      expandParameters(params),
-      options,
-    );
-  };
-
-export const useSearchParameterValuesQuery: typeof parametersApi.useSearchParameterValuesQuery =
-  (params, options) => {
-    if (params === skipToken) {
-      return parametersApi.useSearchParameterValuesQuery(skipToken, options);
-    }
-
-    return parametersApi.useSearchParameterValuesQuery(
-      {
-        ...expandParameters(params),
-        query: params.query,
-      },
-      options,
-    );
-  };
-
-function expandParameters({
-  parameter,
-  ...rest
-}: LoadParameterValuesParams | SearchParameterValuesParams) {
-  return {
-    ...rest,
-    parameter: normalizeParameter(parameter),
-    field_ids: getNonVirtualFields(parameter).map(field => Number(field.id)),
-  };
-}
