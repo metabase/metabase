7d44202a8637a4e5807c37c995a5aa35ae1f925f
John Swanson <john.swanson@metabase.com>
2024-06-18 12:37:14 -0700

    fix tests

--- src/metabase/models/dashboard.clj
+++ src/metabase/models/dashboard.clj
@@ -244,7 +244,8 @@
    ;;   lower-numbered positions appearing before higher numbered ones.
    ;; TODO: querying on stats we don't have any dashboard that has a position, maybe we could just drop it?
    :public_uuid :made_public_by_id
-   :position :initially_published_at :view_count])
+   :position :initially_published_at :view_count
+   :last_viewed_at])
 
 (def ^:private excluded-columns-for-dashcard-revision
   [:entity_id :created_at :updated_at :collection_authority_level])
