b4bf0de13614e592c2731690aca7a4bbe54d4208
Adam James <adam.vermeer2@gmail.com>
2024-06-19 12:08:47 -0700

    Add a little more test coverage for different value types

--- test/metabase/models/user_parameter_value_test.clj
+++ test/metabase/models/user_parameter_value_test.clj
@@ -9,14 +9,22 @@
   (mt/with-empty-db
     (let [user-id   (mt/user->id :rasta)
           upv-count (t2/count :model/UserParameterValue)]
-      (testing "Upsert creates new user parameter value entry if the param_id user_id pair doesn't exist"
-        (upv/upsert! user-id "some-param" "A")
-        (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
-        (is (= "A" (:value (t2/select-one :model/UserParameterValue :user_id user-id :parameter_id "some-param")))))
-      (testing "Upsert updates user parameter value entry if the param_id user_id pair already exists"
-        (upv/upsert! user-id "some-param" "B")
-        (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
-        (is (= "B" (:value (t2/select-one :model/UserParameterValue :user_id user-id :parameter_id "some-param")))))
-      (testing "Upsert deletes user parameter value entry if value is `nil`."
-        (upv/upsert! user-id "some-param" nil)
-        (is (= upv-count (count (t2/select :model/UserParameterValue))))))))
+      (doseq [[test-str value-in value-out value-update value-update-out] [["string" "A" "A" "B" "B"]
+                                                                           ["key" :A "A" :B "B"]
+                                                                           ["vectors"
+                                                                            ["A" "B" "C"] ["A" "B" "C"]
+                                                                            ["A" "B" "C" "D"] ["A" "B" "C" "D"]]]]
+        (testing (format "User Parameter Value for %s values" test-str)
+          (testing (format "Upsert creates new user parameter value entry if the param_id user_id pair doesn't exist")
+            (upv/upsert! user-id "some-param" value-in)
+            (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
+            (is (= value-out (:value (t2/select-one :model/UserParameterValue :user_id user-id :parameter_id "some-param")))))
+
+          (testing "Upsert updates user parameter value entry if the param_id user_id pair already exists"
+            (upv/upsert! user-id "some-param" value-update)
+            (is (= (inc upv-count) (t2/count :model/UserParameterValue)))
+            (is (= value-update-out (:value (t2/select-one :model/UserParameterValue :user_id user-id :parameter_id "some-param")))))
+
+          (testing "Upsert deletes user parameter value entry if value is `nil`."
+            (upv/upsert! user-id "some-param" nil)
+            (is (= upv-count (count (t2/select :model/UserParameterValue))))))))))
