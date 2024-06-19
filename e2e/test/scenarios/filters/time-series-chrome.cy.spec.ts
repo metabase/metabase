7097dc69fa8fb4a69f548c27aeeed98f0ba6e45e
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-06-19 21:14:54 +0200

    Add E2E smoke tests

---
+++ e2e/test/scenarios/filters/time-series-chrome.cy.spec.ts
@@ -0,0 +1,140 @@
+import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import { restore, visitQuestionAdhoc } from "e2e/support/helpers";
+
+const { PRODUCTS, PRODUCTS_ID } = SAMPLE_DATABASE;
+
+describe("time-series chrome filter widget", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  describe("smoke tests", () => {
+    beforeEach(() => {
+      visitQuestionAdhoc({
+        dataset_query: {
+          database: SAMPLE_DB_ID,
+          query: {
+            "source-table": PRODUCTS_ID,
+            aggregation: [["count"]],
+            breakout: [
+              ["field", PRODUCTS.CREATED_AT, { "temporal-unit": "month" }],
+            ],
+          },
+          type: "query",
+        },
+      });
+    });
+
+    it("should properly display the component and all its operators", () => {
+      const operators = [
+        "Previous",
+        "Next",
+        "Current",
+        "Before",
+        "After",
+        "On",
+        "Between",
+      ];
+
+      cy.log(
+        "should display 'All time' as the initialy selected operator (metabase#22247)",
+      );
+      cy.findByTestId("timeseries-filter-button")
+        .should("have.text", "All time")
+        .click();
+
+      cy.findByTestId("datetime-filter-picker")
+        .findByDisplayValue("All time")
+        .click();
+
+      cy.findByRole("listbox").within(() => {
+        cy.findByRole("option", { name: "All time" }).should(
+          "have.attr",
+          "aria-selected",
+          "true",
+        );
+
+        cy.log("Make sure we display all the operators");
+        operators.forEach(operator => {
+          cy.findByRole("option", { name: operator }).should("be.visible");
+        });
+      });
+
+      cy.findByTestId("datetime-filter-picker").within(() => {
+        cy.log("Include 'current' interval switch should not be displayed");
+        cy.findByLabelText(/^Include/).should("not.exist");
+        cy.button("Apply").should("not.be.disabled");
+      });
+    });
+
+    it("should stay in sync with the relative date filter", () => {
+      cy.findByTestId("timeseries-filter-button").click();
+      cy.findByTestId("datetime-filter-picker")
+        .findByDisplayValue("All time")
+        .click();
+
+      cy.log(
+        "Choose the 'Previous' operator and check the state of the time-series chrome",
+      );
+      cy.findByRole("listbox")
+        .findByRole("option", { name: "Previous" })
+        .click();
+      cy.findByTestId("datetime-filter-picker").within(() => {
+        // Top row
+        cy.findByDisplayValue("Previous").should("be.visible");
+        cy.findByDisplayValue("30").should("be.visible");
+        cy.findByDisplayValue("days").should("be.visible");
+
+        cy.log("Toggle should be always off initially");
+        // This is targeting the input checkbox that is hidden
+        cy.findByLabelText("Include today").should(
+          "have.attr",
+          "aria-checked",
+          "false",
+        );
+
+        // This is clicking on an actual label in the UI
+        cy.findByText("Include today").click();
+
+        cy.findByLabelText("Include today").should(
+          "have.attr",
+          "aria-checked",
+          "true",
+        );
+        cy.button("Apply").click();
+        cy.wait("@dataset");
+      });
+
+      cy.findByTestId("filter-pill")
+        .should("have.text", "Created At is in the previous 30 days")
+        .click();
+
+      cy.log(
+        "Make sure the relative date picker reflects the state of the time-series chrome",
+      );
+      cy.findByTestId("datetime-filter-picker").within(() => {
+        cy.findByRole("tab", { name: "Previous" }).should(
+          "have.attr",
+          "aria-selected",
+          "true",
+        );
+
+        cy.findByLabelText("Include today").should(
+          "have.attr",
+          "aria-checked",
+          "true",
+        );
+
+        cy.log("Swith should preserve its state after we switch the direction");
+        cy.findByRole("tab", { name: "Next" }).click();
+        cy.findByLabelText("Include today").should(
+          "have.attr",
+          "aria-checked",
+          "true",
+        );
+      });
+    });
+  });
+});
