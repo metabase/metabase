c006657dce101c3a3d8fc989a837d4624b0ad916
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-06-19 18:38:05 -0400

    feat: Use skeletons to indicate that the main navigation sidebar is loading (#44010)

--- frontend/src/metabase/nav/containers/MainNavbar/NavbarLoadingView.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/NavbarLoadingView.tsx
@@ -1,20 +1,48 @@
-import { t } from "ttag";
+import { Skeleton, Flex } from "metabase/ui";
+import { Repeat } from "metabase/ui/components/feedback/Skeleton/Repeat";
 
-import LoadingSpinner from "metabase/components/LoadingSpinner";
+import { SidebarSection } from "./MainNavbar.styled";
 
-import {
-  LoadingAndErrorContainer,
-  LoadingAndErrorContent,
-  LoadingAndErrorTitle,
-} from "./MainNavbar.styled";
+const NavLinkSkeleton = () => (
+  <Flex my="1rem" gap="sm" px="1rem">
+    <Skeleton radius="100%" w="1rem" h="1rem" />
+    <Skeleton h="1rem" natural />
+  </Flex>
+);
+
+const SectionTitleSkeleton = () => (
+  <Skeleton
+    h=".75rem"
+    mt="2rem"
+    mb="1rem"
+    w="8rem"
+    style={{ marginInlineStart: "1rem" }}
+  />
+);
 
 export function NavbarLoadingView() {
   return (
-    <LoadingAndErrorContainer>
-      <LoadingAndErrorContent>
-        <LoadingSpinner />
-        <LoadingAndErrorTitle>{t`Loading…`}</LoadingAndErrorTitle>
-      </LoadingAndErrorContent>
-    </LoadingAndErrorContainer>
+    <div aria-busy data-testid="loading-spinner">
+      <SidebarSection>
+        <NavLinkSkeleton />
+      </SidebarSection>
+      <SidebarSection>
+        <SectionTitleSkeleton />
+        <NavLinkSkeleton />
+        <NavLinkSkeleton />
+      </SidebarSection>
+      <SidebarSection>
+        <SectionTitleSkeleton />
+        <Repeat times={3}>
+          <NavLinkSkeleton />
+        </Repeat>
+      </SidebarSection>
+      <SidebarSection>
+        <SectionTitleSkeleton />
+        <Repeat times={7}>
+          <NavLinkSkeleton />
+        </Repeat>
+      </SidebarSection>
+    </div>
   );
 }
