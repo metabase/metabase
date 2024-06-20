761772c84db543952a7e7362b2acfbcd45b9c5b1
Phoomparin Mano <phoomparin@gmail.com>
2024-06-20 17:39:45 +0700

    always return custom font family

--- frontend/src/metabase/styled-components/selectors.ts
+++ frontend/src/metabase/styled-components/selectors.ts
@@ -17,15 +17,6 @@ export const getFont = createSelector(
   },
 );
 
-export const getFontFiles = createSelector(
-  [getSettings, getEmbedOptions],
-  (settings, embedOptions) => {
-    // If a font is set in the embed options, we don't need to load any font files.
-    // However, we still need to load the custom font files if the font is set to "Custom".
-    if (embedOptions.font && embedOptions.font !== "Custom") {
-      return [];
-    } else {
-      return settings["application-font-files"];
-    }
-  },
-);
+export const getFontFiles = createSelector([getSettings], settings => {
+  return settings["application-font-files"];
+});
