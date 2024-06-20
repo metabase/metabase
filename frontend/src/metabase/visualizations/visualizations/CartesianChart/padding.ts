abc499e992d733483af6ffe5801b355b23218bde
Phoomparin Mano <phoomparin@gmail.com>
2024-06-20 18:14:23 +0700

    link to discussion on query builder extra spacing

--- frontend/src/metabase/visualizations/visualizations/CartesianChart/padding.ts
+++ frontend/src/metabase/visualizations/visualizations/CartesianChart/padding.ts
@@ -13,6 +13,8 @@ export const getChartPadding = ({
     return padding;
   }
 
+  // Extra spacing is required on question pages.
+  // Refer to https://github.com/metabase/metabase/pull/17552#issuecomment-904945088
   if (isQueryBuilder) {
     return "1rem 1rem 1rem 2rem";
   }
