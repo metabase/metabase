48993f0563f6eebfb43de58d5861ab21e992dcdb
Noah Moss <noahbmoss@gmail.com>
2024-06-20 09:49:46 -0400

    cleanup

--- test/metabase/db/schema_migrations_test.clj
+++ test/metabase/db/schema_migrations_test.clj
@@ -2024,30 +2024,26 @@
                                                                                      :created_at :%now
                                                                                      :updated_at :%now
                                                                                      :details    "{}"}))
-            table-id      (first (t2/insert-returning-pks! (t2/table-name Table) {:db_id      db-id}
-                                                                               :schema     "SchemaName"
-                                                                               :name       "Table"
-                                                                               :created_at :%now
-                                                                               :updated_at :%now
-                                                                               :active     true))
-            permission-id (t2/insert-returning-pk! (t2/table-name :model/Permissions)
-                                                   {:object "/db/fake-permission/"
-                                                    :group_id 1})
+            table-id      (first (t2/insert-returning-pks! (t2/table-name Table) {:db_id      db-id
+                                                                                  :schema     "SchemaName"
+                                                                                  :name       "Table"
+                                                                                  :created_at :%now
+                                                                                  :updated_at :%now
+                                                                                  :active     true}))
+            permission-id (t2/insert-returning-pk! (t2/table-name :model/Permissions) {:object "/db/fake-permission/"
+                                                                                       :group_id 1})
             _             (t2/query-one {:insert-into :sandboxes
                                          :values      [{:group_id             1
                                                         :table_id             table-id
                                                         :attribute_remappings "{\"foo\", 1}"
-                                                        :permission_id        permission-id}]})]
+                                                        :permission_id        permission-id}]})
+            expected        {:group_id             1
+                             :table_id             table-id
+                             :attribute_remappings "{\"foo\", 1}"}]
         (migrate!)
-        (is (=? {:group_id             1
-                 :table_id             table-id
-                 :attribute_remappings "{\"foo\", 1}"}
-                (t2/select-one :sandboxes :table_id table-id)))
+        (is (=? expected (t2/select-one :sandboxes :table_id table-id)))
         (migrate! :down 49)
-        (is (=? {:group_id             1
-                 :table_id             table-id
-                 :attribute_remappings "{\"foo\", 1}"}
-                (t2/select-one :sandboxes :table_id table-id)))))))
+        (is (=? expected (t2/select-one :sandboxes :table_id table-id)))))))
 
 (deftest view-count-test
   (testing "report_card.view_count and report_dashboard.view_count should be populated"
