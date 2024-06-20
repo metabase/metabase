8a7370118d00869cbc479a3a37f8d410538426f3
Romeo Van Snick <romeo@romeovansnick.be>
2024-06-20 09:57:14 +0200

    Setup parameter mocks in embed test

--- frontend/src/metabase/public/components/EmbedModal/StaticEmbedSetupPane/tests/common.unit.spec.tsx
+++ frontend/src/metabase/public/components/EmbedModal/StaticEmbedSetupPane/tests/common.unit.spec.tsx
@@ -1,6 +1,7 @@
 import { screen, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+import { setupParameterValuesEndpoints } from "__support__/server-mocks";
 import { getBrokenUpTextMatcher } from "__support__/ui";
 import { createMockDashboard } from "metabase-types/api/mocks";
 
@@ -12,6 +13,13 @@ const DATE_PARAMETER_MOCK = {
   slug: "month_and_year",
   type: "date/month-year",
 };
+
+beforeEach(() => {
+  setupParameterValuesEndpoints({
+    values: [],
+    has_more_values: false,
+  });
+});
 
 describe("Static Embed Setup phase", () => {
   describe.each([
