d5bf66922b86afe9ca1b59f6f8ee872e06b3c348
Cam Saul <1455846+camsaul@users.noreply.github.com>
2024-06-20 18:44:34 -0700

    Make the App DB transaction isolation level READ_COMMITTED for MySQL [MEGA PERFORMANCE BOOST] (#44505)
    * Make the App DB transaction isolation level READ_COMMITTED for MySQL/MariaDB [MEGA PERFORMANCE BOOST]
    
    * Add test

--- test/metabase/db/data_source_test.clj
+++ test/metabase/db/data_source_test.clj
@@ -12,7 +12,7 @@
 (defn- ->DataSource [s properties]
   (#'mdb.data-source/->DataSource s (some-> (not-empty properties) connection-pool/map->properties)))
 
-(deftest broken-out-details-test
+(deftest ^:parallel broken-out-details-test
   (testing :postgres
     (is (= (->DataSource
             "jdbc:postgresql://localhost:5432/metabase"
@@ -51,7 +51,7 @@
         (is (= [{:one 1}]
                (jdbc/query {:connection conn} "SELECT 1 AS one;")))))))
 
-(deftest connection-string-test
+(deftest ^:parallel connection-string-test
   (let [data-source (mdb.data-source/raw-connection-string->DataSource
                      (format "jdbc:h2:mem:%s" (mt/random-name)))]
     (with-open [conn (.getConnection data-source)]
@@ -72,7 +72,7 @@
     (is (= (mdb.data-source/raw-connection-string->DataSource "jdbc:postgresql://localhost:5432/metabase")
            (mdb.data-source/raw-connection-string->DataSource "postgres://localhost:5432/metabase")))))
 
-(deftest wonky-connection-string-test
+(deftest ^:parallel wonky-connection-string-test
   (testing "Should handle malformed user:password@host:port strings (#14678, #20121)"
     (doseq [subprotocol ["postgresql" "mysql"]]
       (testing "user AND password"
@@ -96,7 +96,7 @@
                   {"user" "cam"})
                  (mdb.data-source/raw-connection-string->DataSource (str subprotocol "://cam@localhost/metabase?password=1234")))))))))
 
-(deftest raw-connection-string-with-separate-username-and-password-test
+(deftest ^:parallel raw-connection-string-with-separate-username-and-password-test
   (testing "Raw connection string should support separate username and/or password (#20122)"
     (testing "username and password"
       (is (= (->DataSource
@@ -116,7 +116,7 @@
              (mdb.data-source/raw-connection-string->DataSource "postgres://metabase" nil "1234")
              (mdb.data-source/raw-connection-string->DataSource "postgres://metabase" "" "1234"))))))
 
-(deftest equality-test
+(deftest ^:parallel equality-test
   (testing "Two DataSources with the same URL should be equal"
     (is (= (mdb.data-source/raw-connection-string->DataSource "ABCD")
            (mdb.data-source/raw-connection-string->DataSource "ABCD"))))
