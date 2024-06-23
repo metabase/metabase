9d2007426514ca301511a5b9d2c28cdb19bc9ba6
lbrdnk <lbrdnk@users.noreply.github.com>
2024-06-24 01:16:13 +0200

    Adjust alternative date tests

--- test/metabase/query_processor_test/alternative_date_test.clj
+++ test/metabase/query_processor_test/alternative_date_test.clj
@@ -352,7 +352,8 @@
           (mt/test-drivers (mt/normal-drivers-with-feature ::iso-8601-test-fields-are-queryable ::parse-string-to-date)
             (is (= 1
                    (->> (mt/run-mbql-query times
-                          {:filter [:= !day.d "2008-10-19"]})
+                          {:fields [$d]
+                           :filter [:= !day.d "2008-10-19"]})
                         mt/rows
                         count)))))))))
 
