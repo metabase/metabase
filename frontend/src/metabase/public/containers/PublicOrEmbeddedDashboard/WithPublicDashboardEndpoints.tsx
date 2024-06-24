103f257b6affd1b5601bbaa6a722a3e2583d018d
Nicolò Pretto <info@npretto.com>
2024-06-24 17:01:01 +0200

    use usePublicDashboardEndpoints inside WithPublicDashboardEndpoints

--- frontend/src/metabase/public/containers/PublicOrEmbeddedDashboard/WithPublicDashboardEndpoints.tsx
+++ frontend/src/metabase/public/containers/PublicOrEmbeddedDashboard/WithPublicDashboardEndpoints.tsx
@@ -11,21 +11,8 @@ import type { DashboardId } from "metabase-types/api";
 export const WithPublicDashboardEndpoints = <T extends WithRouterProps>(
   Component: ComponentType<T>,
 ): ComponentType<T & { dashboardId: DashboardId }> => {
-  function DashboardEndpointsInner({
-    params: { uuid, token },
-    ...props
-  }: {
-    params: {
-      uuid?: string;
-      token?: string;
-    };
-  }) {
-    if (uuid) {
-      setPublicDashboardEndpoints();
-    } else if (token) {
-      setEmbedDashboardEndpoints();
-    }
-    const dashboardId = uuid || token;
+  function DashboardEndpointsInner(props: WithRouterProps) {
+    const { dashboardId } = usePublicDashboardEndpoints(props);
     return <Component {...(props as T)} dashboardId={dashboardId} />;
   }
 
