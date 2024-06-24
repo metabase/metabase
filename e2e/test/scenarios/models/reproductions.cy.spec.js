cc317f6d3d2f475593d1a3dd34aef9b1699580aa
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-06-24 08:37:29 -0400

    Fix model metadata editing for SQL models with manually sorted columns (#44574)

--- e2e/test/scenarios/models/reproductions.cy.spec.js
+++ e2e/test/scenarios/models/reproductions.cy.spec.js
@@ -50,6 +50,7 @@ import {
   visualize,
   focusNativeEditor,
   onlyOnOSS,
+  visitModel,
 } from "e2e/support/helpers";
 import {
   createMockActionParameter,
@@ -1811,5 +1812,59 @@ describe("issue 40252", () => {
     cy.wait("@dataset");
 
     cy.findAllByTestId("header-cell").contains("Model B - A1 → B1Upd");
+  });
+});
+
+describe("issue 42355", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("should allow overriding database fields for models with manually ordered columns (metabase#42355)", () => {
+    createNativeQuestion({
+      type: "model",
+      native: { query: "SELECT ID, PRODUCT_ID FROM ORDERS" },
+      visualization_settings: {
+        "table.columns": [
+          {
+            name: "PRODUCT_ID",
+            key: '["name","PRODUCT_ID"]',
+            enabled: true,
+            fieldRef: ["field", "PRODUCT_ID", { "base-type": "type/Integer" }],
+          },
+          {
+            name: "ID",
+            key: '["name","ID"]',
+            enabled: true,
+            fieldRef: ["field", "ID", { "base-type": "type/BigInteger" }],
+          },
+        ],
+        "table.cell_column": "ID",
+      },
+    }).then(({ body: card }) => visitModel(card.id));
+
+    cy.log("update metadata");
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+    rightSidebar()
+      .findByText("Database column this maps to")
+      .next()
+      .findByText("None")
+      .click();
+    popover().within(() => {
+      cy.findByText("Orders").click();
+      cy.findByText("ID").click();
+    });
+    cy.button("Save changes").click();
+
+    cy.log("check metadata changes are visible");
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+    rightSidebar()
+      .findByText("Database column this maps to")
+      .next()
+      .findByText("Orders → ID")
+      .should("be.visible");
   });
 });
