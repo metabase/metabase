55ba25879a8e7379b1076f53d9dacad11915c64c
Romeo Van Snick <romeo@romeovansnick.be>
2024-06-24 17:26:35 +0200

    Add e2e test to verify that step previews only care about the step being previewed

--- e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
@@ -34,7 +34,7 @@ import {
   queryBuilderFooter,
 } from "e2e/support/helpers";
 
-const { ORDERS, ORDERS_ID, PRODUCTS } = SAMPLE_DATABASE;
+const { ORDERS, ORDERS_ID, PRODUCTS, PRODUCTS_ID } = SAMPLE_DATABASE;
 
 describe("issue 32625, issue 31635", () => {
   const CC_NAME = "Is Promotion";
@@ -909,6 +909,70 @@ describe("issue 43294", () => {
     echartsContainer().within(() => {
       cy.findByText("Count").should("be.visible");
       cy.findByText("Created At").should("be.visible");
+    });
+  });
+});
+
+describe("issue 40399", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("should not show results from other stages in a stages preview (metabase#40399)", () => {
+    createQuestion(
+      {
+        name: "40399",
+
+        query: {
+          "source-table": PRODUCTS_ID,
+          joins: [
+            {
+              fields: "all",
+              alias: "Orders",
+              "source-table": ORDERS_ID,
+              strategy: "left-join",
+              condition: [
+                "=",
+                ["field", PRODUCTS.ID, null],
+                ["field", ORDERS.PRODUCT_ID, { "join-alias": "Orders" }],
+              ],
+            },
+          ],
+          filter: ["=", ["field", PRODUCTS.CATEGORY, null], "Widget"],
+        },
+      },
+      {
+        visitQuestion: true,
+      },
+    );
+
+    openNotebook();
+
+    cy.findByTestId("step-filter-0-0").within(() => {
+      cy.findByTestId("step-preview-button").click();
+      cy.findByTestId("preview-root")
+        .findAllByText("Widget")
+        .should("be.visible");
+    });
+
+    cy.findByTestId("step-join-0-0").within(() => {
+      cy.findByTestId("step-preview-button").click();
+      cy.findByTestId("preview-root")
+        .findAllByText("Gizmo")
+        .should("be.visible");
+
+      cy.findByTestId("preview-root").findByText("Widget").should("not.exist");
+    });
+
+    cy.findByTestId("step-data-0-0").within(() => {
+      cy.findByTestId("step-preview-button").click();
+      cy.findByTestId("preview-root")
+        .findAllByText("Gizmo")
+        .should("be.visible");
+
+      cy.findByTestId("preview-root").findAllByText("Gizmo").should("exist");
+      cy.findByTestId("preview-root").findAllByText("Widget").should("exist");
     });
   });
 });
