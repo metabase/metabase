03aa4c8c3108f5fa6eb2d67ca4bb505340a0dd5c
Ngoc Khuat <qn.khuat@gmail.com>
2024-06-24 16:00:12 +0700

    move test to slack

--- test/metabase/pulse_test.clj
+++ test/metabase/pulse_test.clj
@@ -5,7 +5,6 @@
    [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.test :refer :all]
-   [metabase.channel.slack :as channel.slack]
    [metabase.email :as email]
    [metabase.integrations.slack :as slack]
    [metabase.models
@@ -697,49 +696,6 @@
        (metabase.pulse/send-pulse! (pulse/retrieve-notification pulse-id))
        (is (mt/received-email-body? :rasta #"Manage your subscriptions"))
        (is (mt/received-email-body? "nonuser@metabase.com" #"Unsubscribe"))))))
-
-(deftest create-and-upload-slack-attachments!-test
-  (let [slack-uploader (fn [storage]
-                         (fn [_bytes attachment-name _channel-id]
-                           (swap! storage conj attachment-name)
-                           (str "http://uploaded/" attachment-name)))]
-    (testing "Uploads files"
-      (let [titles         (atom [])
-            attachments    [{:title           "a"
-                             :attachment-name "a.png"
-                             :rendered-info   {:attachments nil
-                                               :content     [:div "hi"]}
-                             :channel-id      "FOO"}
-                            {:title           "b"
-                             :attachment-name "b.png"
-                             :rendered-info   {:attachments nil
-                                               :content     [:div "hi again"]}
-                             :channel-id      "FOO"}]
-            processed      (with-redefs [slack/upload-file! (slack-uploader titles)]
-                            (#'channel.slack/create-and-upload-slack-attachments! attachments (slack-uploader titles)))]
-        (is (= [{:title "a", :image_url "http://uploaded/a.png"}
-                {:title "b", :image_url "http://uploaded/b.png"}]
-               processed))
-        (is (= @titles ["a.png" "b.png"]))))
-    (testing "Uses the raw text when present"
-      (let [titles         (atom [])
-            attachments    [{:title           "a"
-                             :attachment-name "a.png"
-                             :rendered-info   {:attachments nil
-                                               :content     [:div "hi"]}
-                             :channel-id      "FOO"}
-                            {:title           "b"
-                             :attachment-name "b.png"
-                             :rendered-info   {:attachments nil
-                                               :content     [:div "hi again"]
-                                               :render/text "hi again"}
-                             :channel-id      "FOO"}]
-            processed      (with-redefs [slack/upload-file! (slack-uploader titles)]
-                            (#'channel.slack/create-and-upload-slack-attachments! attachments (slack-uploader titles)))]
-        (is (= [{:title "a", :image_url "http://uploaded/a.png"}
-                {:title "b", :text "hi again"}]
-               processed))
-        (is (= @titles ["a.png"]))))))
 
 (deftest pulse-permissions-test
   (testing "Pulses should be sent with the Permissions of the user that created them."
