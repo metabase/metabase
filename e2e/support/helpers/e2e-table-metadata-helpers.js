522869938c1fb3d67d35f0bf89316e8f3a1dcd7a
Ryan Laurie <iethree@gmail.com>
2024-06-25 17:44:54 +0200

    restore legacy metrics

--- e2e/support/helpers/e2e-table-metadata-helpers.js
+++ e2e/support/helpers/e2e-table-metadata-helpers.js
@@ -12,3 +12,18 @@ export const createSegment = ({
     definition,
   });
 };
+
+export const createMetric = ({
+  name,
+  table_id,
+  definition,
+  description = null,
+}) => {
+  cy.log(`Create a metric: ${name}`);
+  return cy.request("POST", "/api/legacy-metric", {
+    name,
+    description,
+    table_id,
+    definition,
+  });
+};
