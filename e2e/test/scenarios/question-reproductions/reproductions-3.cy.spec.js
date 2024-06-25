8297bcb64d7f022efeeba8b93b805d96406b26b9
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-06-25 17:49:02 -0400

    E2E test for custom date expression error (#44694)

--- e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
@@ -953,6 +953,26 @@ describe("issue 44532", () => {
   });
 });
 
+describe("issue 33441", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("should show an error message for an incorrect date expression (metabase#33441)", () => {
+    openOrdersTable({ mode: "notebook" });
+    addCustomColumn();
+    enterCustomColumnDetails({
+      formula: 'datetimeDiff([Created At] , now, "days")',
+      name: "Date",
+    });
+    popover().within(() => {
+      cy.findByText("Invalid expression").should("be.visible");
+      cy.button("Done").should("be.disabled");
+    });
+  });
+});
+
 describe("issue 43294", () => {
   const questionDetails = {
     display: "line",
