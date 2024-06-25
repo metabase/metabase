11c2ef53c6245123eca79e22b54936eaa11bf13f
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-06-25 09:25:42 -0400

    Fix expressions with the same name as their base column (#44641)
    * Pass expressionPosition
    
    * Add a test
    
    * Update e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>
    
    * Rename expressionPosition to expressionIndex
    
    ---------
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
@@ -34,6 +34,7 @@ import {
   queryBuilderFooter,
   enterCustomColumnDetails,
   addCustomColumn,
+  tableInteractive,
 } from "e2e/support/helpers";
 
 const { ORDERS, ORDERS_ID, PRODUCTS, PRODUCTS_ID } = SAMPLE_DATABASE;
@@ -657,6 +658,51 @@ describe("issue 42957", () => {
       );
 
       cy.findByText("Collection without models").should("not.exist");
+    });
+  });
+});
+
+describe("issue 40064", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("should be able to edit a custom column with the same name as one of the columns used in the expression (metabase#40064)", () => {
+    createQuestion(
+      {
+        query: {
+          "source-table": ORDERS_ID,
+          expressions: {
+            Tax: ["*", ["field", ORDERS.TAX, { "base-type": "type/Float" }], 2],
+          },
+          limit: 1,
+        },
+      },
+      { visitQuestion: true },
+    );
+
+    cy.log("check the initial expression value");
+    tableInteractive().findByText("4.14").should("be.visible");
+
+    cy.log("update the expression and check the value");
+    openNotebook();
+    getNotebookStep("expression").findByText("Tax").click();
+    enterCustomColumnDetails({ formula: "[Tax] * 3" });
+    popover().button("Update").click();
+    visualize();
+    tableInteractive().findByText("6.21").should("be.visible");
+
+    cy.log("rename the expression and make sure you cannot create a cycle");
+    openNotebook();
+    getNotebookStep("expression").findByText("Tax").click();
+    enterCustomColumnDetails({ formula: "[Tax] * 3", name: "Tax3" });
+    popover().button("Update").click();
+    getNotebookStep("expression").findByText("Tax3").click();
+    enterCustomColumnDetails({ formula: "[Tax3] * 3", name: "Tax3" });
+    popover().within(() => {
+      cy.findByText("Unknown Field: Tax3").should("be.visible");
+      cy.button("Update").should("be.disabled");
     });
   });
 });
