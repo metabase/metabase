20dc5aeeaa581efcc879e35ae04ed118793143ae
Phoomparin Mano <phoomparin@gmail.com>
2024-06-27 00:45:02 +0700

    create an isolated redux store for each public component

--- enterprise/frontend/src/embedding-sdk/components/private/PublicComponentWrapper/withPublicComponentWrapper.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/PublicComponentWrapper/withPublicComponentWrapper.tsx
@@ -1,15 +1,33 @@
-import type { ComponentType } from "react";
+import { useEffect, useRef, type ComponentType } from "react";
+import { Provider } from "react-redux";
+
+import { sdkReducers, store, useSdkSelector } from "embedding-sdk/store";
+import { getSessionTokenState } from "embedding-sdk/store/selectors";
+import { getStore } from "metabase/store";
 
 import { PublicComponentWrapper } from "./PublicComponentWrapper";
+
+type Store = ReturnType<typeof getStore>;
 
 export function withPublicComponentWrapper<P extends object>(
   WrappedComponent: ComponentType<P>,
 ): React.FC<P> {
   const WithPublicComponentWrapper: React.FC<P> = props => {
+    const token = useSdkSelector(getSessionTokenState);
+
+    const isolatedStore = useRef<Store>();
+
+    // HACK: refresh the internal store when the token changes
+    useEffect(() => {
+      isolatedStore.current = getStore(sdkReducers, null, store.getState());
+    }, [token]);
+
     return (
-      <PublicComponentWrapper>
-        <WrappedComponent {...props} />
-      </PublicComponentWrapper>
+      <Provider store={isolatedStore.current}>
+        <PublicComponentWrapper>
+          <WrappedComponent {...props} />
+        </PublicComponentWrapper>
+      </Provider>
     );
   };
 
