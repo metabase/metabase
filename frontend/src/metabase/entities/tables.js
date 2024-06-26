c9f400e755e3a22bed3df49fe6d625677c8bdea3
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-06-26 12:36:10 -0400

    Fix broken field normalization in metadata (#44721) (#44764)
    * Fix broken field normalization in metadata
    
    * Handle Fields.actionTypes.UPDATE action in tables reducer to keep table's original_fields in sync
    
    * Add an e2e test for fields
    
    * Format code
    
    * Use a different table, since accounts table is not available in e2e tests
    
    ---------
    
    Co-authored-by: Alexander Polyankin <alexander.polyankin@metabase.com>
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- frontend/src/metabase/entities/tables.js
+++ frontend/src/metabase/entities/tables.js
@@ -196,6 +196,24 @@ const Tables = createEntity({
   },
 
   reducer: (state = {}, { type, payload, error }) => {
+    if (type === Fields.actionTypes.UPDATE && !error) {
+      const updatedField = payload.field;
+      const tableId = updatedField.table_id;
+      const table = state[tableId];
+
+      if (table) {
+        return {
+          ...state,
+          [tableId]: {
+            ...table,
+            original_fields: table.original_fields?.map(field => {
+              return field.id === updatedField.id ? updatedField : field;
+            }),
+          },
+        };
+      }
+    }
+
     if (type === Questions.actionTypes.CREATE && !error) {
       const card = payload.question;
       const virtualQuestionTable = convertSavedQuestionToVirtualTable(card);
