74e71e453583087dd197f1bf58b73fad212c760f
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-06-26 20:15:37 +0300

    Add a repro for 'Cannot change question data source to raw data after selecting saved question' (#44757)
    * Add a repro for 'Cannot change question data source to raw data after selecting saved question'
    
    * Update e2e/test/scenarios/question-reproductions/reproductions-2.cy.spec.js
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>
    
    * Update e2e/test/scenarios/question-reproductions/reproductions-2.cy.spec.js
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>
    
    * Update e2e/test/scenarios/question-reproductions/reproductions-2.cy.spec.js
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>
    
    ---------
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

---
+++ e2e/test/scenarios/question/reproductions/36669-change-datasource-saved-question.cy.spec.js
@@ -0,0 +1,47 @@
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import {
+  restore,
+  startNewQuestion,
+  entityPickerModal,
+  entityPickerModalTab,
+  createQuestion,
+  getNotebookStep,
+} from "e2e/support/helpers";
+
+const { ORDERS_ID } = SAMPLE_DATABASE;
+
+describe("issue 36669", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("should be able to change question data source to raw data after selecting saved question (metabase#36669)", () => {
+    const questionDetails = {
+      name: "Orders 36669",
+      query: {
+        "source-table": ORDERS_ID,
+        limit: 5,
+      },
+    };
+
+    createQuestion(questionDetails).then(() => {
+      startNewQuestion();
+    });
+
+    entityPickerModal().within(() => {
+      cy.findByPlaceholderText("Search…").type("Orders 36669");
+
+      cy.findByRole("tabpanel").findByText("Orders 36669").click();
+    });
+
+    getNotebookStep("data").findByText("Orders 36669").click();
+
+    entityPickerModal().within(() => {
+      entityPickerModalTab("Tables").click();
+
+      cy.log("verify Tables are listed");
+      cy.findByRole("tabpanel").should("contain", "Orders");
+    });
+  });
+});
