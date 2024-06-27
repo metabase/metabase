5015a8cd3d8a9286055a5c0097017a7c03071046
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2024-06-27 15:50:07 +0200

    Consolidate models TS reproductions for the `release-x.50.x` branch (#44818)
    * Port TS reproductions
    
    * Revert React 18 changes from master
    
    * Incorporate 41785
    
    * Fix typo [ci skip]

---
+++ e2e/test/scenarios/models/reproductions.cy.spec.ts
@@ -0,0 +1,442 @@
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import {
+  createQuestion,
+  openQuestionActions,
+  popover,
+  restore,
+  hovercard,
+  createNativeQuestion,
+  openNotebook,
+  enterCustomColumnDetails,
+  visualize,
+  saveQuestion,
+  entityPickerModal,
+  entityPickerModalTab,
+  join,
+  modal,
+  queryBuilderMain,
+  startNewModel,
+} from "e2e/support/helpers";
+import type { FieldReference } from "metabase-types/api";
+
+const { ORDERS, ORDERS_ID, PRODUCTS, PRODUCTS_ID } = SAMPLE_DATABASE;
+
+describe("issue 29943", () => {
+  function reorderTotalAndCustomColumns() {
+    getHeaderCell(1, "Total").should("exist");
+    getHeaderCell(2, "Custom").should("exist");
+
+    // drag & drop the Custom column 100 px to the left to switch it with Total column
+    cy.findAllByTestId("header-cell")
+      .contains("Custom")
+      .then(customColumn => {
+        const rect = customColumn[0].getBoundingClientRect();
+        cy.wrap(customColumn)
+          .trigger("mousedown")
+          .trigger("mousemove", { clientX: rect.x - 100, clientY: rect.y })
+          .trigger("mouseup");
+      });
+
+    getHeaderCell(1, "Custom").should("exist");
+    getHeaderCell(2, "Total").should("exist");
+  }
+
+  function assertColumnSelected(columnIndex: number, name: string) {
+    getHeaderCell(columnIndex, name)
+      .find("div")
+      .should("have.css", "background-color")
+      .and("eq", "rgb(80, 158, 227)");
+
+    cy.findByLabelText("Display name").should("have.value", name);
+  }
+
+  function getHeaderCell(columnIndex: number, name: string) {
+    return cy
+      .findAllByTestId("header-cell")
+      .eq(columnIndex)
+      .should("have.text", name);
+  }
+
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+    cy.intercept("POST", "/api/dataset").as("dataset");
+  });
+
+  it("selects the right column when clicking a column header (metabase#29943)", () => {
+    createQuestion(
+      {
+        type: "model",
+        query: {
+          "source-table": ORDERS_ID,
+          expressions: {
+            Custom: ["+", 1, 1],
+          },
+          fields: [
+            ["field", ORDERS.ID, { "base-type": "type/BigInteger" }],
+            ["field", ORDERS.TOTAL, { "base-type": "type/Float" }],
+            ["expression", "Custom", { "base-type": "type/Integer" }],
+          ],
+          limit: 5, // optimization
+        },
+      },
+      { visitQuestion: true },
+    );
+
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+
+    reorderTotalAndCustomColumns();
+    cy.button("Save changes").click();
+    cy.wait("@dataset");
+
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+
+    assertColumnSelected(0, "ID");
+
+    getHeaderCell(1, "Custom").click();
+    assertColumnSelected(1, "Custom");
+
+    getHeaderCell(2, "Total").click();
+    assertColumnSelected(2, "Total");
+
+    getHeaderCell(0, "ID").click();
+    assertColumnSelected(0, "ID");
+  });
+});
+
+describe("issue 35711", () => {
+  const { ORDERS_ID, ORDERS } = SAMPLE_DATABASE;
+
+  const DISCOUNT_FIELD_REF: FieldReference = [
+    "field",
+    ORDERS.DISCOUNT,
+    {
+      "base-type": "type/Float",
+    },
+  ];
+
+  function reorderTaxAndTotalColumns() {
+    cy.findAllByTestId("header-cell").eq(4).should("have.text", "Tax");
+    cy.findAllByTestId("header-cell").eq(5).should("have.text", "Total");
+
+    // drag & drop the Total column 100 px to the left to switch it with Tax column
+    cy.findAllByTestId("header-cell")
+      .contains("Total")
+      .then(totalColumn => {
+        const rect = totalColumn[0].getBoundingClientRect();
+        cy.wrap(totalColumn)
+          .trigger("mousedown")
+          .trigger("mousemove", { clientX: rect.x - 100, clientY: rect.y })
+          .trigger("mouseup");
+      });
+
+    cy.findAllByTestId("header-cell").eq(4).should("have.text", "Total");
+    cy.findAllByTestId("header-cell").eq(5).should("have.text", "Tax");
+  }
+
+  function assertNoError() {
+    cy.button("Get Answer").should("not.exist");
+    cy.get("main")
+      .findByText("There was a problem with your question")
+      .should("not.exist");
+    cy.get("[data-testid=cell-data]").should("contain", "37.65");
+  }
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("can edit metadata of a model with a custom column (metabase#35711)", () => {
+    createQuestion(
+      {
+        type: "model",
+        query: {
+          "source-table": ORDERS_ID,
+          expressions: {
+            "Custom column": ["-", DISCOUNT_FIELD_REF, 1],
+          },
+          limit: 5, // optimization
+        },
+      },
+      { visitQuestion: true },
+    );
+
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+    reorderTaxAndTotalColumns();
+    assertNoError();
+
+    cy.findByTestId("editor-tabs-query-name").click();
+    assertNoError();
+  });
+});
+
+describe("issues 25884 and 34349", () => {
+  const ID_DESCRIPTION =
+    "This is a unique ID for the product. It is also called the “Invoice number” or “Confirmation number” in customer facing emails and screens.";
+
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("should show empty description input for columns without description in metadata (metabase#25884, metabase#34349)", () => {
+    createQuestion(
+      {
+        type: "model",
+        query: {
+          "source-table": ORDERS_ID,
+          expressions: {
+            Country: ["substring", "United States", 1, 20],
+          },
+          fields: [
+            ["field", ORDERS.ID, { "base-type": "type/BigInteger" }],
+            ["expression", "Country", { "base-type": "type/Text" }],
+          ],
+          limit: 5, // optimization
+        },
+      },
+      { visitQuestion: true },
+    );
+
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+
+    cy.findByLabelText("Description").should("have.text", ID_DESCRIPTION);
+
+    cy.findAllByTestId("header-cell").contains("Country").click();
+    cy.findByLabelText("Description").should("have.text", "");
+
+    cy.findAllByTestId("header-cell").contains("ID").click();
+    cy.findByLabelText("Description").should("have.text", ID_DESCRIPTION);
+  });
+});
+
+describe("issue 23103", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+    cy.intercept("PUT", "/api/card/*").as("updateModel");
+  });
+
+  it("shows correct number of distinct values (metabase#23103)", () => {
+    createNativeQuestion(
+      {
+        type: "model",
+        native: {
+          query: "select * from products limit 5",
+        },
+      },
+      { visitQuestion: true },
+    );
+
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+
+    cy.findAllByTestId("header-cell").contains("CATEGORY").click();
+    cy.findAllByTestId("select-button").contains("None").click();
+    popover().within(() => {
+      cy.findByText("Products").click();
+      cy.findByText("Category").click();
+    });
+
+    cy.button("Save changes").click();
+    cy.wait("@updateModel");
+    cy.button("Saving…").should("not.exist");
+
+    cy.findAllByTestId("header-cell").contains("Category").trigger("mouseover");
+
+    hovercard().findByText("4 distinct values").should("exist");
+  });
+});
+
+describe("issue 39150", { viewportWidth: 1600 }, () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("allows custom columns with the same name in nested models (metabase#39150-1)", () => {
+    const ccName = "CC Rating";
+
+    createQuestion({
+      name: "Source Model",
+      type: "model",
+      query: {
+        "source-table": PRODUCTS_ID,
+        expressions: {
+          [ccName]: [
+            "ceil",
+            [
+              "field",
+              PRODUCTS.RATING,
+              {
+                "base-type": "type/Float",
+              },
+            ],
+          ],
+        },
+        limit: 2,
+      },
+    }).then(({ body: { id: sourceModelId } }) => {
+      createQuestion(
+        {
+          name: "Nested Model",
+          type: "model",
+          query: {
+            "source-table": `card__${sourceModelId}`,
+          },
+        },
+        { visitQuestion: true },
+      );
+    });
+
+    openNotebook();
+    cy.findByTestId("action-buttons").findByText("Custom column").click();
+
+    enterCustomColumnDetails({
+      formula: "floor([Rating])",
+      name: ccName,
+      blur: true,
+    });
+
+    cy.button("Done").click();
+
+    visualize();
+
+    cy.findAllByTestId("header-cell")
+      .filter(`:contains('${ccName}')`)
+      .should("have.length", 2);
+  });
+
+  it("allows custom columns with the same name as the aggregation column from the souce model (metabase#39150-2)", () => {
+    createQuestion({
+      name: "Source Model",
+      type: "model",
+      query: {
+        "source-table": PRODUCTS_ID,
+        aggregation: [["count"]],
+        breakout: [
+          [
+            "field",
+            PRODUCTS.CATEGORY,
+            {
+              "base-type": "type/Text",
+            },
+          ],
+        ],
+        limit: 2,
+      },
+    }).then(({ body: { id: sourceModelId } }) => {
+      createQuestion(
+        {
+          type: "model",
+          query: {
+            "source-table": `card__${sourceModelId}`,
+          },
+        },
+        { visitQuestion: true },
+      );
+    });
+
+    openNotebook();
+    cy.findByTestId("action-buttons").findByText("Custom column").click();
+
+    enterCustomColumnDetails({
+      formula: "[Count] + 1",
+      name: "Count",
+      blur: true,
+    });
+
+    cy.button("Done").click();
+
+    visualize();
+
+    cy.findAllByTestId("header-cell")
+      .filter(":contains('Count')")
+      .should("have.length", 2);
+
+    saveQuestion("Nested Model", { wrapId: true, idAlias: "nestedModelId" });
+
+    cy.log("Make sure this works for the deeply nested models as well");
+    cy.get("@nestedModelId").then(nestedModelId => {
+      createQuestion(
+        {
+          type: "model",
+          query: {
+            "source-table": `card__${nestedModelId}`,
+          },
+        },
+        { visitQuestion: true },
+      );
+    });
+
+    openNotebook();
+    cy.findByTestId("action-buttons").findByText("Custom column").click();
+
+    enterCustomColumnDetails({
+      formula: "[Count] + 5",
+      name: "Count",
+      blur: true,
+    });
+
+    cy.button("Done").click();
+
+    visualize();
+
+    cy.findAllByTestId("header-cell")
+      .filter(":contains('Count')")
+      .should("have.length", 3);
+  });
+});
+
+describe("issue 41785", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+    cy.intercept("POST", "/api/dataset").as("dataset");
+  });
+
+  it("does not break the question when removing column with the same mapping as another column (metabase#41785)", () => {
+    // it's important to create the model through UI to reproduce this issue
+    startNewModel();
+    entityPickerModal().within(() => {
+      entityPickerModalTab("Tables").click();
+      cy.findByText("Orders").click();
+    });
+    join();
+    entityPickerModal().within(() => {
+      entityPickerModalTab("Tables").click();
+      cy.findByText("Orders").click();
+    });
+    popover().findByText("ID").click();
+    popover().findByText("ID").click();
+
+    cy.findByTestId("run-button").click();
+    cy.wait("@dataset");
+
+    cy.button("Save").click();
+    modal().button("Save").click();
+
+    cy.findAllByTestId("cell-data").should("contain", "37.65");
+    cy.findByTestId("loading-indicator").should("not.exist");
+
+    cy.findByTestId("viz-settings-button").click();
+    cy.findByTestId("chartsettings-sidebar").within(() => {
+      cy.findAllByText("Tax").should("have.length", 1);
+      cy.findAllByText("Orders → Tax").should("have.length", 1);
+
+      cy.findByRole("button", { name: "Add or remove columns" }).click();
+      cy.findAllByText("Tax").should("have.length", 1);
+      cy.findAllByText("Orders → Tax").should("have.length", 1).click();
+    });
+
+    cy.wait("@dataset");
+
+    queryBuilderMain()
+      .findByText("There was a problem with your question")
+      .should("not.exist");
+  });
+});
