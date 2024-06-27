b162f80aff285238a0997170318aa5771522199b
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-06-27 08:47:30 +0200

    Port changes from #43660

--- e2e/test/scenarios/question-reproductions/reproductions-2.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-2.cy.spec.js
@@ -21,8 +21,10 @@ import {
   chartPathWithFillColor,
   openQuestionActions,
   queryBuilderHeader,
-  describeOSS,
   saveQuestion,
+  onlyOnOSS,
+  entityPickerModalItem,
+  newButton,
 } from "e2e/support/helpers";
 
 const { ORDERS, ORDERS_ID, PRODUCTS, PRODUCTS_ID, PEOPLE } = SAMPLE_DATABASE;
@@ -274,61 +276,60 @@ describe("issue 25016", () => {
 });
 
 // this is only testable in OSS because EE always has models from auditv2
-describeOSS("issue 25144", { tags: "@OSS" }, () => {
+describe("issue 25144", { tags: "@OSS" }, () => {
   beforeEach(() => {
+    onlyOnOSS();
     restore("setup");
     cy.signInAsAdmin();
     cy.intercept("POST", "/api/card").as("createCard");
     cy.intercept("PUT", "/api/card/*").as("updateCard");
   });
 
-  it("should show Saved Questions section after creating the first question (metabase#25144)", () => {
+  it("should show Saved Questions tab after creating the first question (metabase#25144)", () => {
     cy.visit("/");
 
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("New").click();
-    popover().findByText("Question").click();
-    popover().findByText("Orders").click();
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("Save").click();
-    modal().findByLabelText("Name").clear().type("Orders question");
-    modal().button("Save").click();
-    cy.wait("@createCard");
-    cy.wait(100);
-    modal().button("Not now").click();
+    newButton("Question").click();
 
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("New").click();
-    popover().findByText("Question").click();
-    popover().findByText("Saved Questions").click();
-    popover().findByText("Orders question").should("be.visible");
+    entityPickerModal().within(() => {
+      cy.findByText("Saved questions").should("not.exist");
+      entityPickerModalItem(2, "Orders").click();
+    });
+
+    saveQuestion("Orders question");
+
+    newButton("Question").click();
+
+    entityPickerModal().within(() => {
+      entityPickerModalTab("Saved questions").should("be.visible").click();
+      entityPickerModalItem(1, "Orders question").should("be.visible");
+    });
   });
 
-  it("should show Models section after creation the first model (metabase#24878)", () => {
+  it("should show Models tab after creation the first model (metabase#24878)", () => {
     cy.visit("/");
 
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("New").click();
-    popover().findByText("Question").click();
-    popover().findByText("Orders").click();
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("Save").click();
-    modal().findByLabelText("Name").clear().type("Orders model");
-    modal().button("Save").click();
+    newButton("Model").click();
+    cy.findByTestId("new-model-options")
+      .findByText(/use the notebook/i)
+      .click();
+    entityPickerModal().within(() => {
+      entityPickerModalItem(2, "Orders").click();
+    });
+
+    cy.findByTestId("dataset-edit-bar").button("Save").click();
+
+    modal().within(() => {
+      cy.findByLabelText("Name").clear().type("Orders model");
+      cy.button("Save").click();
+    });
     cy.wait("@createCard");
-    cy.wait(100);
-    modal().button("Not now").click();
 
-    cy.findByLabelText("Move, archive, and more...").click();
-    popover().findByText("Turn into a model").click();
-    modal().button("Turn this into a model").click();
-    cy.wait("@updateCard");
+    newButton("Question").click();
 
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("New").click();
-    popover().findByText("Question").click();
-    popover().findByText("Models").click();
-    popover().findByText("Orders model").should("be.visible");
+    entityPickerModal().within(() => {
+      entityPickerModalTab("Models").should("be.visible").click();
+      entityPickerModalItem(1, "Orders model").should("be.visible");
+    });
   });
 });
 
