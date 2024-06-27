f117d9d78cbf82ff923bfcddb3e47e22926fb259
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2024-06-27 18:56:04 +0200

    Unskip repro for #14957: Saving a question before query has been executed can be slow with UI "hanging" (#44825)

--- e2e/test/scenarios/question-reproductions/reproductions-1.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-1.cy.spec.js
@@ -316,9 +316,9 @@ describe("postgres > user > query", { tags: "@external" }, () => {
   });
 });
 
-const PG_DB_NAME = "QA Postgres12";
+describe("issue 14957", { tags: "@external" }, () => {
+  const PG_DB_NAME = "QA Postgres12";
 
-describe.skip("issue 14957", { tags: "@external" }, () => {
   beforeEach(() => {
     restore("postgres-12");
     cy.signInAsAdmin();
@@ -327,12 +327,7 @@ describe.skip("issue 14957", { tags: "@external" }, () => {
   it("should save a question before query has been executed (metabase#14957)", () => {
     openNativeEditor({ databaseName: PG_DB_NAME }).type("select pg_sleep(60)");
 
-    // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
-    cy.findByText("Save").click();
-
-    cy.findByLabelText("Name").type("14957");
-    cy.button("Save").click();
-
+    saveQuestion("14957");
     modal().should("not.exist");
   });
 });
