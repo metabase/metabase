789eca57a23b074a40c1af9d4c2e7c25c97e1807
Phoomparin Mano <phoomparin@gmail.com>
2024-06-27 16:23:22 +0700

    cleanup redux store logic

--- enterprise/frontend/src/embedding-sdk/components/private/IsolatedStoreProvider.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/IsolatedStoreProvider.tsx
@@ -1,60 +1,35 @@
-import { ComponentType, ReactNode, useEffect, useMemo, useState } from "react";
+import { type ComponentType, type ReactNode, type FC, useMemo } from "react";
 import { Provider } from "react-redux";
-import { getStore } from "metabase/store";
+
 import { useSdkSelector, store, sdkReducers } from "embedding-sdk/store";
-import { nanoid } from "@reduxjs/toolkit";
+import { getStore } from "metabase/store";
 
 interface Props {
   children: ReactNode;
 }
 
-type Store = ReturnType<typeof getStore>;
-
 export const IsolatedStoreProvider = ({ children }: Props) => {
-  const token = useSdkSelector(s => s.sdk.token.token?.id);
   const isLoggedIn = useSdkSelector(s => s.sdk.loginStatus.status);
 
-  const [instanceId, setInstanceId] = useState("");
-  const [isolatedStore, setIsolatedStore] = useState<Store>();
-
-  useEffect(() => {
-    const id = nanoid();
-    setInstanceId(id);
-
-    console.log("(fx:mount)", id);
-
-    return () => {
-      console.log("(fx:cleanup)", id);
-    };
+  const isolatedStore = useMemo(() => {
+    return getStore(sdkReducers, null, store.getState());
   }, []);
 
-  useEffect(() => {
-    if (!instanceId) return;
-
-    const inheritedState = store.getState();
-    console.log("(fx:set:store)", instanceId);
-
-    setIsolatedStore(getStore(sdkReducers, null, inheritedState));
-  }, [instanceId]);
-
-  if (isLoggedIn !== "success") return null;
-  if (!isolatedStore) return null;
-
-  console.log("(fx:render)");
+  if (isLoggedIn !== "success" || !isolatedStore) {
+    return null;
+  }
 
   return <Provider store={isolatedStore}>{children}</Provider>;
 };
 
 export function withIsolatedStore<P extends object>(
   WrappedComponent: ComponentType<P>,
-): React.FC<P> {
-  const withIsolatedStore: React.FC<P> = props => {
-    return (
-      <IsolatedStoreProvider>
-        <WrappedComponent {...props} />
-      </IsolatedStoreProvider>
-    );
-  };
+): FC<P> {
+  const withIsolatedStore: FC<P> = props => (
+    <IsolatedStoreProvider>
+      <WrappedComponent {...props} />
+    </IsolatedStoreProvider>
+  );
 
   withIsolatedStore.displayName = `withIsolatedStore(${
     WrappedComponent.displayName || WrappedComponent.name || "Component"
