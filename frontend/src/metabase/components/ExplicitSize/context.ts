06509d07dd52defa67b8eb400464d96d7758d24f
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-06-28 22:21:52 +0700

    Clarify the use of ExplicitSize context

--- frontend/src/metabase/components/ExplicitSize/context.ts
+++ frontend/src/metabase/components/ExplicitSize/context.ts
@@ -2,6 +2,7 @@ import { createContext } from "react";
 
 import type { RefreshMode } from "./ExplicitSize";
 
+// Only used for Loki snapshots
 export const explicitSizeRefreshModeContext = createContext<RefreshMode | null>(
   null,
 );
