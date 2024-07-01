67e4e6eb28aaaceec1da44387960d86e2cd45996
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-07-01 12:54:37 +0200

    Regroup (metric v1) repro for 36422

--- e2e/test/scenarios/admin/datamodel/metrics.cy.spec.js
+++ e2e/test/scenarios/admin/datamodel/metrics.cy.spec.js
@@ -341,6 +341,25 @@ describe("scenarios > admin > datamodel > metrics", () => {
   });
 });
 
+describe("metrics v1 reproductions", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsAdmin();
+  });
+
+  it("should show error and disable save button when create metric fails", () => {
+    cy.visit("/admin/datamodel/metrics");
+    cy.button("New metric").click();
+    selectTable("Orders");
+    cy.findByLabelText("Name Your Metric").type("x");
+    cy.findByLabelText("Describe Your Metric").type("x");
+
+    cy.intercept("POST", "/api/legacy-metric", req => req.reply(400));
+    cy.button("Save changes").click();
+    cy.findByRole("alert", { name: "An error occurred" }).should("be.visible");
+  });
+});
+
 function selectTable(tableName) {
   cy.findByText("Select a table").click();
   popover().findByText(tableName).click();
