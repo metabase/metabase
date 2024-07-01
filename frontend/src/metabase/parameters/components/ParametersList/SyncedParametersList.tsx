3878ee8ae046d5bfc27d8541a673b3b97af25559
Anton Kulyk <kuliks.anton@gmail.com>
2024-07-01 17:58:43 +0100

    Fix `SyncedParametersList`

--- frontend/src/metabase/parameters/components/ParametersList/SyncedParametersList.tsx
+++ frontend/src/metabase/parameters/components/ParametersList/SyncedParametersList.tsx
@@ -1,19 +1,18 @@
-import { useMemo } from "react";
-import { withRouter, type WithRouterProps } from "react-router";
+import querystring from "querystring";
+import { useEffect, useMemo } from "react";
 import _ from "underscore";
 
-import { useSyncedQueryString } from "metabase/hooks/use-synced-query-string";
+import { IS_EMBED_PREVIEW } from "metabase/lib/embed";
 import type { ParametersListProps } from "metabase/parameters/components/ParametersList/types";
 import { getParameterValuesBySlug } from "metabase-lib/v1/parameters/utils/parameter-values";
 
 import { ParametersList } from "./ParametersList";
 
-const _SyncedParametersList = ({
+export const SyncedParametersList = ({
   parameters,
   editingParameter,
   question,
   dashboard,
-  location,
 
   className,
   hideParameters,
@@ -28,13 +27,31 @@ const _SyncedParametersList = ({
   setEditingParameter,
   setParameterValueToDefault,
   enableParameterRequiredBehavior,
-}: ParametersListProps & WithRouterProps) => {
+}: ParametersListProps) => {
   const queryParams = useMemo(
     () => getParameterValuesBySlug(parameters),
     [parameters],
   );
 
-  useSyncedQueryString(queryParams, location);
+  useEffect(() => {
+    /**
+     * We don't want to sync the query string to the URL because when previewing,
+     * this changes the URL of the iframe by appending the query string to the src.
+     * This causes the iframe to reload when changing the preview hash from appearance
+     * settings because now the base URL (including the query string) is different.
+     */
+    if (IS_EMBED_PREVIEW) {
+      return;
+    }
+    const searchString = buildSearchString(queryParams);
+    if (searchString !== window.location.search) {
+      window.history.replaceState(
+        null,
+        document.title,
+        window.location.pathname + searchString + window.location.hash,
+      );
+    }
+  }, [queryParams]);
 
   return (
     <ParametersList
@@ -57,6 +74,21 @@ const _SyncedParametersList = ({
   );
 };
 
-export const SyncedParametersList = withRouter<ParametersListProps>(
-  _SyncedParametersList,
-);
+const QUERY_PARAMS_ALLOW_LIST = ["objectId"];
+
+function buildSearchString(object: Record<string, any>) {
+  const currentSearchParams = querystring.parse(
+    window.location.search.replace("?", ""),
+  );
+  const filteredSearchParams = Object.fromEntries(
+    Object.entries(currentSearchParams).filter(entry =>
+      QUERY_PARAMS_ALLOW_LIST.includes(entry[0]),
+    ),
+  );
+
+  const search = querystring.stringify({
+    ...filteredSearchParams,
+    ...object,
+  });
+  return search ? `?${search}` : "";
+}
