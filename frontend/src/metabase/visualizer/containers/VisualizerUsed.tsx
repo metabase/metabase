e979ba86611e2dd7732c74a2c7f5016c6a6548e7
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2024-07-02 13:18:03 -0400

    add skeleton content for being used section items

--- frontend/src/metabase/visualizer/containers/VisualizerUsed.tsx
+++ frontend/src/metabase/visualizer/containers/VisualizerUsed.tsx
@@ -1,15 +1,77 @@
+/* eslint-disable */
+/* Use ActionIcon */
 import { t } from "ttag";
 
-import { Card, Title } from "metabase/ui";
+import { Menu, Flex, Card, Title, Icon, IconName, Text } from "metabase/ui";
 import type { Card as ICard } from "metabase-types/api";
+import { ActionIcon } from "@mantine/core";
 
 export function VisualizerUsed({ cards }: { cards?: ICard[] }) {
   return (
     <Card h="100%">
       <Title order={4}>{t`Being used`}</Title>
-      {cards?.map((card, index) => (
-        <div key={index}>{card.name}</div>
-      ))}
+      {cards?.map((card, index) => {
+        return (
+          <Flex key={index} py="sm" align="center">
+            {/* TODO - create a dark variant  */}
+            <Menu>
+              <Menu.Target>
+                <ActionIcon mr="sm">
+                  <Icon name={card.display as IconName} />
+                  <Icon name="chevrondown" size={8} />
+                </ActionIcon>
+              </Menu.Target>
+              <Menu.Dropdown bg="black.0">
+                <Menu.Item>
+                  <Flex align="center">
+                    <Icon name="bar" color="white" />
+                    <Text ml="sm" color="white">{t`Bar`}</Text>
+                  </Flex>
+                </Menu.Item>
+                <Menu.Item>
+                  <Flex align="center">
+                    <Icon name="line" color="white" />
+                    <Text ml="sm" color="white">{t`Line`}</Text>
+                  </Flex>
+                </Menu.Item>
+                <Menu.Item>
+                  <Flex align="center">
+                    <Icon name="pie" color="white" />
+                    <Text ml="sm" color="white">{t`Pie`}</Text>
+                  </Flex>
+                </Menu.Item>
+                <Menu.Item>
+                  <Flex align="center">
+                    <Icon name="ellipsis" />
+                    <Text ml="sm">{t`More`}</Text>
+                  </Flex>
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+            <div>{card.name}</div>
+            <Flex ml="auto">
+              <ActionIcon mr="sm">
+                <Icon
+                  name={
+                    card.dataset_query.type === "native" ? "sql" : "notebook"
+                  }
+                />
+              </ActionIcon>
+              <Menu>
+                <Menu.Target>
+                  <ActionIcon>
+                    <Icon name="ellipsis" />
+                  </ActionIcon>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item>{t`Refresh`}</Menu.Item>
+                  <Menu.Item>{t`Remove`}</Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Flex>
+          </Flex>
+        );
+      })}
     </Card>
   );
 }
