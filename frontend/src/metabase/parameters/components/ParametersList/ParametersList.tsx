871549d24711ff02df2d3a7bf8bf0f53ebe55cb3
R1tter <marcelosori@hotmail.com>
2024-07-02 18:32:52 -0300

    includes logic

--- frontend/src/metabase/parameters/components/ParametersList/ParametersList.tsx
+++ frontend/src/metabase/parameters/components/ParametersList/ParametersList.tsx
@@ -46,7 +46,7 @@ export const ParametersList = ({
   const visibleValuePopulatedParameters = useMemo(() => {
     const visibleParams = getVisibleParameters(parameters, hideParameters);
     return visibleParams.filter(
-      parameter => isEditing || !parameter.name.includes("#hide"), //testing
+      parameter => isEditing || !parameter.name.includes("#hide"),
     );
   }, [parameters, hideParameters, isEditing]);
 
