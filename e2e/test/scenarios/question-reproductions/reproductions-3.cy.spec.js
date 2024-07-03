1de13707705dca76516de8a72ca62a3e4b5186d7
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-03 18:41:43 +0700

    Fix - Ellipsis tooltip cut off/not visible in GUI editor (#45030)
    * Fix #39771
    
    * Add repro for #39771
    
    * Add explanatory comment
    
    * Fix z-index
    
    * Use Mantine Tooltip in Ellipsified to fix z-index issues
    
    * Revert "Fix z-index"
    
    This reverts commit 951e3dca86be73ba2123930fbfaaae45c5fa1f0e.
    
    * Revert "Fix #39771"
    
    This reverts commit 4165da5022d566253e875938a84bd1f137c92357.
    
    * Update repro to new tooltip
    
    * Update assertions

--- e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
@@ -1633,3 +1633,68 @@ describe("issue 38989", () => {
       .should("exist");
   });
 });
+
+describe("issue 39771", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("should show tooltip for ellipsified text (metabase#39771)", () => {
+    createQuestion(
+      {
+        query: {
+          aggregation: [["count"]],
+          breakout: [
+            [
+              "field",
+              "CREATED_AT",
+              {
+                "base-type": "type/DateTime",
+                "temporal-unit": "quarter-of-year",
+              },
+            ],
+          ],
+          "source-query": {
+            "source-table": ORDERS_ID,
+            aggregation: [["count"]],
+            breakout: [
+              [
+                "field",
+                ORDERS.CREATED_AT,
+                {
+                  "base-type": "type/DateTime",
+                  "temporal-unit": "month",
+                },
+              ],
+            ],
+          },
+        },
+      },
+      { visitQuestion: true },
+    );
+
+    openNotebook();
+    getNotebookStep("summarize", { stage: 1 })
+      .findByTestId("breakout-step")
+      .findByText("Created At: Month: Quarter of year")
+      .click();
+
+    popover().findByText("by quarter of year").realHover();
+
+    popover().then(([$popover]) => {
+      const popoverStyle = window.getComputedStyle($popover);
+      const popoverZindex = parseInt(popoverStyle.zIndex, 10);
+
+      cy.findByTestId("ellipsified-tooltip").within(([$tooltip]) => {
+        cy.findByText("by quarter of year").should("be.visible");
+
+        const tooltipStyle = window.getComputedStyle($tooltip);
+        const tooltipZindex = parseInt(tooltipStyle.zIndex, 10);
+
+        // resort to asserting zIndex because should("be.visible") passes unexpectedly
+        expect(tooltipZindex).to.be.gte(popoverZindex);
+      });
+    });
+  });
+});
