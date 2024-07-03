7326c6620152032ec88e71bdcff6629ae8c24385
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-07-03 08:05:23 -0400

    Add repro for #43216 (#44972) (#45080)
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- e2e/support/helpers/e2e-command-palette-helpers.js
+++ e2e/support/helpers/e2e-command-palette-helpers.js
@@ -1,12 +1,20 @@
 export const commandPalette = () => cy.findByTestId("command-palette");
+
 export const openCommandPalette = () => cy.get("body").type("{ctrl+k}{cmd+k}");
+
 export const commandPaletteButton = () =>
   cy.findByTestId("app-bar").findByRole("button", { name: /Search/ });
+
 export const closeCommandPalette = () => cy.get("body").type("{esc}");
+
 export const pressPageUp = () => cy.get("body").type("{pageup}");
+
 export const pressPageDown = () => cy.get("body").type("{pagedown}");
+
 export const pressHome = () => cy.get("body").type("{home}");
+
 export const pressEnd = () => cy.get("body").type("{end}");
+
 export const commandPaletteInput = () =>
   cy.findByPlaceholderText("Search for anything or jump somewhere…");
 
