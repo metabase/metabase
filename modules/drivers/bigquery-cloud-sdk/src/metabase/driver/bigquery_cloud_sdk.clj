5fe9f12fbd1171bb40197f557083cfa51353574b
lbrdnk <lbrdnk@users.noreply.github.com>
2024-07-04 20:04:27 +0200

    Set key-constraints to false for bigquery

--- modules/drivers/bigquery-cloud-sdk/src/metabase/driver/bigquery_cloud_sdk.clj
+++ modules/drivers/bigquery-cloud-sdk/src/metabase/driver/bigquery_cloud_sdk.clj
@@ -474,16 +474,17 @@
 ;;; |                                           Other Driver Method Impls                                            |
 ;;; +----------------------------------------------------------------------------------------------------------------+
 
-(doseq [[feature supported?] {:convert-timezone        true
-                              :datetime-diff           true
-                              :expressions             true
-                              :now                     true
-                              :percentile-aggregations true
+(doseq [[feature supported?] {:convert-timezone         true
+                              :datetime-diff            true
+                              :expressions              true
+                              :now                      true
+                              :percentile-aggregations  true
+                              :metadata/key-constraints false
                               ;; BigQuery uses timezone operators and arguments on calls like extract() and
                               ;; timezone_trunc() rather than literally using SET TIMEZONE, but we need to flag it as
                               ;; supporting set-timezone anyway so that reporting timezones are returned and used, and
                               ;; tests expect the converted values.
-                              :set-timezone            true}]
+                              :set-timezone             true}]
   (defmethod driver/database-supports? [:bigquery-cloud-sdk feature] [_driver _feature _db] supported?))
 
 ;; BigQuery is always in UTC
