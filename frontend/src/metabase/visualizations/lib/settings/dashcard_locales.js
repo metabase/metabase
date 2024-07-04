c4e407f4488ade36d67cfc5d54107921a8511982
ashahrior <ashahrior.cse.mbstu@gmail.com>
2024-07-04 16:54:56 +0600

    titla changed. version upgraded

--- frontend/src/metabase/visualizations/lib/settings/dashcard_locales.js
+++ frontend/src/metabase/visualizations/lib/settings/dashcard_locales.js
@@ -701,8 +701,8 @@ const TITLE_LOCALES_BN = {
     'bn' : 'সার্টিফিকেট কনফিগারেশন অনুযায়ী প্রশিক্ষণের পরিসংখ্যান'
   },
   'total.trainee.count.based.on.obtaining.certificate' : {
-    'en' : 'Trainee Statistics Based on Attainment of Certificate of Completed Training',
-    'bn' : 'সম্পন্ন প্রশিক্ষণ সমূহের সার্টিফিকেট অর্জন ভিত্তিক প্রশিক্ষণার্থীর পরিসংখ্যান'
+    'en' : 'Statistics of Trainees Based on Certification of Trainings',
+    'bn' : 'প্রশিক্ষণ সমূহের সার্টিফিকেট অর্জন ভিত্তিক প্রশিক্ষণার্থীর পরিসংখ্যান'
   },
   'admin.dashboard.ppe.total.school' : {
     'en' : 'Total Pre-Primary School',
