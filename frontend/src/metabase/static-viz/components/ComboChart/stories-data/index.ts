0f1a93ef8e99c94a0389633924ee39cf9465130b
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-07-04 18:23:37 -0400

    fix incorrect y-axis extent computation

--- frontend/src/metabase/static-viz/components/ComboChart/stories-data/index.ts
+++ frontend/src/metabase/static-viz/components/ComboChart/stories-data/index.ts
@@ -92,6 +92,7 @@ import numberOfInsightsError39608 from "./number-of-insights-error-39608.json";
 import numericXAxisIncludesZero37082 from "./numeric-x-axis-includes-zero-37082.json";
 import powYScaleCustomYAxisRange from "./pow-y-scale-custom-y-axis-range.json";
 import safariNonIanaTimezoneRepro44128 from "./safari-non-iana-timezone-repro-44128.json";
+import stackedChartCustomYAxisRange from "./stacked-chart-custom-y-axis-range.json";
 import ticksNativeWeekWithGapLongRange from "./ticks-native-week-with-gap-long-range.json";
 import ticksNativeWeekWithGapShortRange from "./ticks-native-week-with-gap-short-range.json";
 import timeSeriesTicksCompactFormattingMixedTimezones from "./time-series-ticks-compact-formatting-mixed-timezones.json";
@@ -228,4 +229,5 @@ export const data = {
   areaChartSteppedNullsSkipped,
   safariNonIanaTimezoneRepro44128,
   combinedWithInvalidSettings,
+  stackedChartCustomYAxisRange,
 };
