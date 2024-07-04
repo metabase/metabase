ad8e3d76416295fcd52d7031b68b4fc029b65e8d
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-04 12:14:59 -0400

    Revert broken changes

--- frontend/src/metabase-lib/v1/queries/utils/dataset.ts
+++ frontend/src/metabase-lib/v1/queries/utils/dataset.ts
@@ -9,32 +9,41 @@ import type {
 export const datasetContainsNoResults = (data: DatasetData) =>
   data.rows == null || data.rows.length === 0;
 
-export function getColumnSettingKey({
-  key,
-  name,
-  fieldRef,
-}: TableColumnOrderSetting) {
+export function getColumnSettingKey(
+  { key, name, fieldRef }: TableColumnOrderSetting,
+  ignoreBaseType = false,
+) {
+  if (ignoreBaseType) {
+    return getColumnKey({ name, field_ref: normalize(fieldRef) }, true);
+  }
+
   return key ?? getColumnKey({ name, field_ref: normalize(fieldRef) });
 }
 
 export function findColumnIndexesForColumnSettings(
-  columns: Pick<DatasetColumn, "name" | "field_ref">[],
+  columns: DatasetColumn[],
   columnSettings: TableColumnOrderSetting[],
 ) {
-  const columnIndexByName = new Map(
-    columns.map((column, index) => [column.name, index]),
+  const columnIndexByKey = new Map(
+    columns.map((column, index) => [getColumnKey(column, true), index]),
   );
   return columnSettings.map(
-    columnSetting => columnIndexByName.get(columnSetting.name) ?? -1,
+    columnSetting =>
+      columnIndexByKey.get(getColumnSettingKey(columnSetting, true)) ?? -1,
   );
 }
 
 export function findColumnSettingIndexesForColumns(
-  columns: Pick<DatasetColumn, "name" | "field_ref">[],
+  columns: DatasetColumn[],
   columnSettings: TableColumnOrderSetting[],
 ) {
   const columnSettingIndexByKey = new Map(
-    columnSettings.map((columnSetting, index) => [columnSetting.name, index]),
+    columnSettings.map((columnSetting, index) => [
+      getColumnSettingKey(columnSetting, true),
+      index,
+    ]),
   );
-  return columns.map(column => columnSettingIndexByKey.get(column.name) ?? -1);
+  return columns.map(
+    column => columnSettingIndexByKey.get(getColumnKey(column, true)) ?? -1,
+  );
 }
