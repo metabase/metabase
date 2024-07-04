0b9bdb7e74303803a2c0bee4417c33271fa96b6e
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-07-04 19:12:06 +0700

    Use `--mb-color-text-primary` for light theme dashboard

--- frontend/src/metabase/visualizations/components/FunnelNormal.styled.tsx
+++ frontend/src/metabase/visualizations/components/FunnelNormal.styled.tsx
@@ -64,7 +64,7 @@ export const FunnelStart = styled.div<SharedProps>`
 
   ${Title} {
     font-weight: bold;
-    color: var(--mb-color-text-dark);
+    color: var(--mb-color-text-primary);
     ${props =>
       props.isNarrow
         ? css`
