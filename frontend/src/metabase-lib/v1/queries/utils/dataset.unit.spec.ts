29b8637e5b79585ba1d517441bf16c9f28620393
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-04 16:17:44 -0400

    Add unit tests

--- frontend/src/metabase-lib/v1/queries/utils/dataset.unit.spec.ts
+++ frontend/src/metabase-lib/v1/queries/utils/dataset.unit.spec.ts
@@ -11,7 +11,7 @@ import {
 
 describe("dataset utils", () => {
   describe("findColumnIndexesForColumnSettings", () => {
-    it("finds columnIndex for ColumnSettings without base-type", () => {
+    it("should find indexes for columns without base-type", () => {
       const column = createMockColumn({
         id: ORDERS.TOTAL,
         name: "TOTAL",
@@ -35,7 +35,7 @@ describe("dataset utils", () => {
       ).toEqual([0]);
     });
 
-    it("finds columnIndex for ColumnSettings with base-type", () => {
+    it("should find indexes for columns with base-type", () => {
       const column = createMockColumn({
         id: ORDERS.TOTAL,
         name: "TOTAL",
@@ -59,7 +59,7 @@ describe("dataset utils", () => {
       ).toEqual([0]);
     });
 
-    it("finds findColumnIndexesForColumnSettings for Columns with different base-type", () => {
+    it("should find indexes for columns with different base-type", () => {
       const column = createMockColumn({
         id: ORDERS.TOTAL,
         name: "TOTAL",
@@ -85,7 +85,7 @@ describe("dataset utils", () => {
   });
 
   describe("findColumnSettingIndexesForColumns", () => {
-    it("finds columnSettingsIndex for Column without base-type", () => {
+    it("should find indexes for column settings without base-type", () => {
       const column = createMockColumn({
         id: ORDERS.TOTAL,
         name: "TOTAL",
@@ -103,7 +103,7 @@ describe("dataset utils", () => {
       ).toEqual([0]);
     });
 
-    it("finds columnSettingsIndex for Columns with base-type", () => {
+    it("should find indexes for column settings with base-type", () => {
       const column = createMockColumn({
         id: ORDERS.TOTAL,
         name: "TOTAL",
@@ -127,7 +127,7 @@ describe("dataset utils", () => {
       ).toEqual([0]);
     });
 
-    it("finds columnSettingsIndex for Columns with different base-type", () => {
+    it("should find indexes for column settings with different base-type", () => {
       const column = createMockColumn({
         id: ORDERS.TOTAL,
         name: "TOTAL",
@@ -149,6 +149,66 @@ describe("dataset utils", () => {
       expect(
         findColumnSettingIndexesForColumns([column], [columnSetting]),
       ).toEqual([0]);
+    });
+
+    it("should find indexes for column settings when the field ref changes from a string (metabase#42049)", () => {
+      const columns = [
+        createMockColumn({
+          id: ORDERS.TOTAL,
+          name: "TOTAL",
+          field_ref: ["field", "TOTAL", { "base-type": "type/Float" }],
+        }),
+        createMockColumn({
+          id: ORDERS.SUBTOTAL,
+          name: "SUBTOTAL",
+          field_ref: ["field", "SUBTOTAL", { "base-type": "type/Float" }],
+        }),
+      ];
+      const columnSettings = [
+        createMockTableColumnOrderSetting({
+          name: "SUBTOTAL",
+          fieldRef: ["field", ORDERS.SUBTOTAL, { "base-type": "type/Float" }],
+          enabled: true,
+        }),
+        createMockTableColumnOrderSetting({
+          name: "TOTAL",
+          fieldRef: ["field", ORDERS.TOTAL, { "base-type": "type/Float" }],
+          enabled: true,
+        }),
+      ];
+      expect(
+        findColumnSettingIndexesForColumns(columns, columnSettings),
+      ).toEqual([1, 0]);
+    });
+
+    it("should find indexes for column settings when the field ref changes from an expression to a field (metabase#39993)", () => {
+      const columns = [
+        createMockColumn({
+          id: ORDERS.TOTAL,
+          name: "TOTAL",
+          field_ref: ["field", "TOTAL", { "base-type": "type/Float" }],
+        }),
+        createMockColumn({
+          id: undefined,
+          name: "expr",
+          field_ref: ["field", "expr", { "base-type": "type/Float" }],
+        }),
+      ];
+      const columnSettings = [
+        createMockTableColumnOrderSetting({
+          name: "TOTAL",
+          fieldRef: ["field", ORDERS.TOTAL, { "base-type": "type/Float" }],
+          enabled: true,
+        }),
+        createMockTableColumnOrderSetting({
+          name: "expr",
+          fieldRef: ["expression", "expr"],
+          enabled: true,
+        }),
+      ];
+      expect(
+        findColumnSettingIndexesForColumns(columns, columnSettings),
+      ).toEqual([0, 1]);
     });
   });
 });
