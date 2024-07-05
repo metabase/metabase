352e94eb13dad29ef79e1127d6d67b931242c5a6
Ngoc Khuat <qn.khuat@gmail.com>
2024-07-05 16:31:45 +0700

    toucan2 dev tool to record all queries count

---
+++ dev/src/dev/toucan2_monitor.clj
@@ -0,0 +1,74 @@
+(ns dev.toucan2-monitor
+  "Utilities to track and monitor queries using toucan2
+
+  Usage:
+    (start!) ;; start tracking
+    ;; do some query using toucan 2 or via UI
+    (queries) ;; get all queries and its execution time
+    ;; => [[[\"SELECT * FROM report_card\"] 100]]
+    (stop!)"
+  (:require
+   [methodical.core :as methodical]
+   [toucan2.pipeline :as t2.pipeline]))
+
+(set! *warn-on-reflection* true)
+
+(def queries*
+  "An atom to store all the queries and its execution time."
+  (atom []))
+
+(defn queries
+  "Get all the queries and its execution time in ms.
+
+  Usage:
+    (queries)
+    ;; => [[[\"SELECT * FROM report_card\"] 100]]"
+  []
+  @queries*)
+
+(defn reset-queries!
+  "Reset all the queries and its execution time."
+  []
+  (reset! queries* []))
+
+(defn- track-query-execution-fn
+  [next-method rf conn query-type model query]
+  (let [start (System/currentTimeMillis)
+        result (next-method rf conn query-type model query)
+        end (System/currentTimeMillis)]
+    (swap! queries* (fnil conj []) [query (- end start)])
+    result))
+
+(defn start!
+  "Start tracking queries."
+  []
+  (methodical/add-aux-method-with-unique-key!
+   #'t2.pipeline/transduce-execute-with-connection
+   :around
+   :default
+   track-query-execution-fn
+   ::monitor))
+
+(defn stop!
+  "Stop tracking queries."
+  []
+  (methodical/remove-aux-method-with-unique-key!
+   #'t2.pipeline/transduce-execute-with-connection
+   :around
+   :default
+   ::tracking))
+
+(defn total-execution-time-ms
+  "Get the total execution time of all queries in ms."
+  []
+  (->> (queries)
+       (map second)
+       (apply +)))
+
+(comment
+ (start!)
+ (queries)
+ (stop!)
+ (reset-queries!)
+ (count (queries))
+ (total-execution-time-ms))
