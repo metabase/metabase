6660e9b7da3157cfabbd6d27b9e4e34a268c2f3e
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-08 13:38:57 -0400

    Add tests

--- frontend/src/metabase/querying/utils/viz-settings/sync-viz-settings.unit.spec.tsx
+++ frontend/src/metabase/querying/utils/viz-settings/sync-viz-settings.unit.spec.tsx
@@ -1,9 +1,15 @@
+import * as Lib from "metabase-lib";
+import { columnFinder, createQuery } from "metabase-lib/test-helpers";
 import {
   createMockTableColumnOrderSetting,
   createMockVisualizationSettings,
 } from "metabase-types/api/mocks";
 
-import { syncVizSettings, type ColumnInfo } from "./sync-viz-settings";
+import {
+  syncVizSettings,
+  syncVizSettingsWithQuery,
+  type ColumnInfo,
+} from "./sync-viz-settings";
 
 describe("syncVizSettings", () => {
   describe("table.columns", () => {
@@ -30,9 +36,7 @@ describe("syncVizSettings", () => {
         { name: "ID", desiredColumnAlias: "ID" },
         { name: "ID_2", desiredColumnAlias: "PEOPLE__ID" },
       ];
-
       const newColumns: ColumnInfo[] = [oldColumns[1], oldColumns[0]];
-
       const oldSettings = createMockVisualizationSettings({
         "table.columns": [
           createMockTableColumnOrderSetting({
@@ -55,7 +59,6 @@ describe("syncVizSettings", () => {
         { name: "ID", desiredColumnAlias: "ID" },
         { name: "ID_2", desiredColumnAlias: "PEOPLE__ID" },
       ];
-
       const newColumns: ColumnInfo[] = [
         { name: "ID", desiredColumnAlias: "ID" },
         { name: "ID_2", desiredColumnAlias: "PRODUCTS__ID" },
@@ -92,13 +95,11 @@ describe("syncVizSettings", () => {
         { name: "ID_2", desiredColumnAlias: "PRODUCTS__ID" },
         { name: "ID_3", desiredColumnAlias: "PEOPLE__ID" },
       ];
-
       const newColumns: ColumnInfo[] = [
         { name: "ID", desiredColumnAlias: "ID" },
         { name: "TOTAL", desiredColumnAlias: "TOTAL" },
         { name: "ID_2", desiredColumnAlias: "PEOPLE__ID" },
       ];
-
       const oldSettings = createMockVisualizationSettings({
         "table.columns": [
           createMockTableColumnOrderSetting({
@@ -136,7 +137,6 @@ describe("syncVizSettings", () => {
         { name: "ID_2", desiredColumnAlias: "PRODUCTS__ID" },
         { name: "ID_3", desiredColumnAlias: "PEOPLE__ID" },
       ];
-
       const oldSettings = createMockVisualizationSettings({
         "table.columns": [
           createMockTableColumnOrderSetting({
@@ -156,6 +156,51 @@ describe("syncVizSettings", () => {
           { name: "ID", enabled: true },
           { name: "ID_2", enabled: false },
           { name: "ID_3", enabled: true },
+        ],
+      });
+    });
+  });
+});
+
+describe("syncVizSettingsWithQuery", () => {
+  describe("table.columns", () => {
+    it("should handle adding new columns with column.name changes", () => {
+      const baseQuery = createQuery();
+      const stageIndex = -1;
+      const availableColumns = Lib.visibleColumns(baseQuery, stageIndex);
+      const findColumn = columnFinder(baseQuery, availableColumns);
+      const oldQuery = Lib.withFields(baseQuery, stageIndex, [
+        findColumn("ORDERS", "ID"),
+        findColumn("PEOPLE", "ID"),
+      ]);
+      const newQuery = Lib.withFields(baseQuery, stageIndex, [
+        findColumn("ORDERS", "ID"),
+        findColumn("PRODUCTS", "ID"),
+        findColumn("PEOPLE", "ID"),
+      ]);
+      const oldSettings = createMockVisualizationSettings({
+        "table.columns": [
+          createMockTableColumnOrderSetting({
+            name: "ID",
+            enabled: true,
+          }),
+          createMockTableColumnOrderSetting({
+            name: "ID_2",
+            enabled: false,
+          }),
+        ],
+      });
+
+      const newSettings = syncVizSettingsWithQuery(
+        oldSettings,
+        newQuery,
+        oldQuery,
+      );
+      expect(newSettings).toEqual({
+        "table.columns": [
+          { name: "ID", enabled: true },
+          { name: "ID_3", enabled: false },
+          { name: "ID_2", enabled: true },
         ],
       });
     });
