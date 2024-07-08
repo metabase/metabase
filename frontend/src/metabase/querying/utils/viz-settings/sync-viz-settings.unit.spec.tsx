f87d7c0db319b44be42623aca60a27baecf3b481
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-08 13:21:24 -0400

    Add tests

--- frontend/src/metabase/querying/utils/viz-settings/sync-viz-settings.unit.spec.tsx
+++ frontend/src/metabase/querying/utils/viz-settings/sync-viz-settings.unit.spec.tsx
@@ -6,7 +6,43 @@ import {
 import { syncVizSettings, type ColumnInfo } from "./sync-viz-settings";
 
 describe("syncVizSettings", () => {
-  it("should handle column.name updates", () => {
+  it("should handle adding new columns with column.name changes", () => {
+    const oldColumns: ColumnInfo[] = [
+      { name: "ID", desiredColumnAlias: "ID" },
+      { name: "ID_2", desiredColumnAlias: "PEOPLE__ID" },
+    ];
+
+    const newColumns: ColumnInfo[] = [
+      { name: "ID", desiredColumnAlias: "ID" },
+      { name: "ID_2", desiredColumnAlias: "PRODUCTS__ID" },
+      { name: "ID_3", desiredColumnAlias: "PEOPLE__ID" },
+    ];
+
+    const oldSettings = createMockVisualizationSettings({
+      "table.columns": [
+        createMockTableColumnOrderSetting({
+          name: "ID",
+          enabled: true,
+        }),
+        createMockTableColumnOrderSetting({
+          name: "ID_2",
+          enabled: false,
+        }),
+      ],
+    });
+
+    const newSettings = syncVizSettings(oldSettings, newColumns, oldColumns);
+
+    expect(newSettings).toEqual({
+      "table.columns": [
+        { name: "ID", enabled: true },
+        { name: "ID_3", enabled: false },
+        { name: "ID_2", enabled: true },
+      ],
+    });
+  });
+
+  it("should handle removing columns with column.name changes", () => {
     const oldColumns: ColumnInfo[] = [
       { name: "ID", desiredColumnAlias: "ID" },
       { name: "TOTAL", desiredColumnAlias: "TOTAL" },
