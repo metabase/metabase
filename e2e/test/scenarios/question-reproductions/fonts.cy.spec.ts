bed8c21a163b3097fe996a136a9749916de8415c
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-09 19:57:23 +0700

    Bring back working assertions

--- e2e/test/scenarios/question-reproductions/fonts.cy.spec.ts
+++ e2e/test/scenarios/question-reproductions/fonts.cy.spec.ts
@@ -13,29 +13,20 @@ describe("fonts", () => {
   it("works", () => {
     openOrdersTable({ mode: "notebook" });
 
-    cy.wait(1000);
-
-    // cy.get("@font-regular.all").should("have.length", 1);
-    // cy.get("@font-regular").should(({ response }) => {
-    //   expect(response).to.include({ statusCode: 200 });
-    // });
-
-    // cy.get("@font-bold.all").should("have.length", 1);
-    // cy.get("@font-bold").should(({ response }) => {
-    //   expect(response).to.include({ statusCode: 200 });
-    // });
-
-    // cy.document()
-    // .then(document => document.fonts.ready)
-    // .then(fonts => {
-    //   cy.wrap(fonts).invoke("check", "16px Lato").should("be.true");
-    // });
-
-    cy.get("head").then(([$head]) => {
-      const index = $head.innerHTML.indexOf(
-        "app/fonts/Lato/lato-v16-latin-regular.woff2",
-      );
-      expect(index).to.be.greaterThan(0);
+    cy.get("@font-regular.all").should("have.length", 1);
+    cy.get("@font-regular").should(({ response }) => {
+      expect(response).to.include({ statusCode: 200 });
     });
+
+    cy.get("@font-bold.all").should("have.length", 1);
+    cy.get("@font-bold").should(({ response }) => {
+      expect(response).to.include({ statusCode: 200 });
+    });
+
+    cy.document()
+      .then(document => document.fonts.ready)
+      .then(fonts => {
+        cy.wrap(fonts).invoke("check", "16px Lato").should("be.true");
+      });
   });
 });
