1aa6155a4374d63f74d91629be3a7ab987eb0a4e
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-09 15:30:56 -0400

    Fix hydration

--- src/metabase/models/params.clj
+++ src/metabase/models/params.clj
@@ -136,12 +136,13 @@
   no more."
   [fields]
   (let [table-id->name-field (fields->table-id->name-field (pk-fields fields))]
-    (for [field fields
-          :when (isa? (:semantic_type field) :type/PK)
-          :let [name-field (table-id->name-field (:table_id field))]]
-      (-> field
-          (assoc :name_field name-field)
-          (assoc :name_field_id (when name-field (u/the-id name-field)))))))
+    (for [field fields]
+      ;; add matching `:name_field` if it's a PK
+      (let [name-field (when (isa? (:semantic_type field) :type/PK)
+                         (table-id->name-field (:table_id field)))]
+        (-> field
+            (assoc :name_field name-field)
+            (assoc :name_field_id (when name-field (u/the-id name-field))))))))
 
 ;; We hydrate the `:human_readable_field` for each Dimension using the usual hydration logic, so it contains columns we
 ;; don't want to return. The two functions below work to remove the unneeded ones.
