58120d364712a831aa01c92a5dec4c803ce55301
jeff bruemmer <jeff.bruemmer@gmail.com>
2024-07-10 14:30:49 -0400

    tests

--- src/metabase/cmd/config_file_gen.clj
+++ src/metabase/cmd/config_file_gen.clj
@@ -1,81 +1,82 @@
 (ns metabase.cmd.config-file-gen
-  "Generates basic configuration file for Metabase, with all settings."
+  "Generates a docs page with an example configuration file for Metabase.
+  The example config includes all configurable settings and their default values."
   (:require
    [clj-yaml.core :as yaml]
    [clojure.java.io :as io]
    [metabase.cmd.env-var-dox :as dox]))
 
-;;;; Get Markdown intro, outro, and base template
-
-(defn- config-base-template
-  "Gets the base configuration template, to which we'll add the settings."
-  []
-  (yaml/parse-string (slurp (io/resource "metabase/cmd/resources/config-template.yaml"))))
-
-(defn- config-template-intro
-  "Gets the markdown intro for the configuration file."
-  []
-  (slurp (io/resource "metabase/cmd/resources/config-file-intro.md")))
-
-(defn- config-file-outro
-  "The Markdown to follow the config file template."
-  []
-  (slurp (io/resource "metabase/cmd/resources/config-file-outro.md")))
-
 ;;;; Get settings and their default values
 
-(defn- get-settings
+(defn get-settings
   "Gets valid config settings."
   []
   (dox/filter-env-vars (dox/get-settings)))
 
-(defn- get-name-and-default
+(defn get-name-and-default
   "Get a setting's name and its default."
   [{:keys [munged-name default]}]
   {(keyword munged-name) default})
 
-;;;; Add settings to YAML template and build Markdown file
+;;;; Add settings to YAML template
 
-(defn- create-settings-map
+(defn create-settings-map
   "Creates a sorted map of settings from their names and defaults."
-  []
-  (->> (get-settings)
+  [settings]
+  (->> settings
        (map get-name-and-default)
        (into (sorted-map))))
 
 (defn- add-settings
   "Adds settings to the configuration template."
   [config]
-  (assoc-in config [:config :settings] (create-settings-map)))
+  (assoc-in config [:config :settings] (create-settings-map (get-settings))))
+
+;;;; Build Markdown file
+
+(def markdown-intro
+  "Used as header for config file doc."
+  "metabase/cmd/resources/config-file-intro.md")
+
+(def markdown-outro
+  "Used as footer for config file doc."
+  "metabase/cmd/resources/config-file-outro.md")
 
 (defn- build-markdown-page
   "Take a YAML string and builds a Markdown page for docs on a configuration file."
   [config-yaml]
-  (str (config-template-intro)
+  (str (slurp (io/resource markdown-intro))
        config-yaml
-       (config-file-outro)))
+       (slurp (io/resource markdown-outro))))
+
+(defn- format-yaml
+  "Takes configuration map that includes settings data and preps YAML for embedding in Markdown doc."
+  [config-with-settings]
+  (yaml/generate-string config-with-settings :dumper-options {:flow-style :block}))
+
+(defn- create-config-doc
+  "Generates a configuration file Markdown doc with config template for Metabase
+   with settings and their default values."
+  [yaml-template]
+  (-> yaml-template
+      (io/resource)
+      (slurp)
+      (yaml/parse-string)
+      (add-settings)
+      (format-yaml)
+      (build-markdown-page)))
 
 (def config-file-path
   "Docs location for the config file template."
   "docs/configuring-metabase/config-template.md")
 
-(defn format-yaml
-  "Takes configuration map that includes settings data and preps YAML for embedding in Markdown doc."
-  [config-with-settings]
-  (yaml/generate-string config-with-settings :dumper-options {:flow-style :block}))
-
-(defn create-config-doc
-  "Generates a configuration file Markdown doc with config template for Metabase
-   with settings and their default values."
-  []
-  (-> (config-base-template)
-      (add-settings)
-      (format-yaml)
-      (build-markdown-page)))
+(def yaml-template
+  "Base template that we'll add the settings to."
+  "metabase/cmd/resources/config-template.yaml")
 
 (defn generate-config-file-doc!
   "Generates a configuration file doc with template and saves to docs directory."
   []
   (println "Creating config file doc with template...")
-  (spit (io/file config-file-path) (create-config-doc))
+  (spit (io/file config-file-path) (create-config-doc yaml-template))
   (println (str "Config doc file created: `" config-file-path "`.")))
