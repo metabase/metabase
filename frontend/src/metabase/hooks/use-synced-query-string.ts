e727328d8f4b134faf575ea685f1a902cf471930
Romeo Van Snick <romeo@romeovansnick.be>
2024-07-11 14:16:35 +0200

    Use useDeepCompareEffect to avoid flicker

--- frontend/src/metabase/hooks/use-synced-query-string.ts
+++ frontend/src/metabase/hooks/use-synced-query-string.ts
@@ -1,11 +1,10 @@
-import { useEffect } from "react";
-import { useUnmount } from "react-use";
+import { useDeepCompareEffect } from "react-use";
 
 import { IS_EMBED_PREVIEW } from "metabase/lib/embed";
 import { buildSearchString } from "metabase/lib/urls";
 
 export function useSyncedQueryString(object: Record<string, any>) {
-  useEffect(() => {
+  useDeepCompareEffect(() => {
     /**
      * We don't want to sync the query string to the URL because when previewing,
      * this changes the URL of the iframe by appending the query string to the src.
@@ -27,23 +26,23 @@ export function useSyncedQueryString(object: Record<string, any>) {
         window.location.pathname + searchString + window.location.hash,
       );
     }
+
+    return () => {
+      // Remove every previously-synced keys from the query string when the component is unmounted.
+      // This is a workaround to clear the parameter list state when [SyncedParametersList] unmounts.
+      const searchString = buildSearchString({
+        filterFn: key => !(key in object),
+      });
+
+      if (searchString !== window.location.search) {
+        history.replaceState(
+          null,
+          document.title,
+          window.location.pathname + searchString + window.location.hash,
+        );
+      }
+    };
   }, [object]);
-
-  useUnmount(() => {
-    // Remove every previously-synced keys from the query string when the component is unmounted.
-    // This is a workaround to clear the parameter list state when [SyncedParametersList] unmounts.
-    const searchString = buildSearchString({
-      filterFn: key => !(key in object),
-    });
-
-    if (searchString !== window.location.search) {
-      history.replaceState(
-        null,
-        document.title,
-        window.location.pathname + searchString + window.location.hash,
-      );
-    }
-  });
 }
 
 const QUERY_PARAMS_ALLOW_LIST = ["objectId", "tab"];
