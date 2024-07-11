1127f2988fd6dd3b359dcf3be4a5c5670988908a
Oisin Coveney <oisin@metabase.com>
2024-07-11 16:40:30 +0300

    Attempt to fix types

--- frontend/src/metabase/dashboard/components/DashboardHeader/buttons/OverflowMenu.tsx
+++ frontend/src/metabase/dashboard/components/DashboardHeader/buttons/OverflowMenu.tsx
@@ -1,17 +1,22 @@
 import type { MouseEvent, ReactNode } from "react";
+import { P, match } from "ts-pattern";
 
 import { Icon, type IconName, Menu, Text } from "metabase/ui";
 
-export type OverflowMenuItem = {
-  enabled?: boolean;
-  title?: string | ReactNode;
-  icon?: IconName;
-  action?: ((e: MouseEvent) => void) | (() => void);
-  link?: string;
-  event?: string;
-  testId?: string;
-  component?: ReactNode;
-};
+export type OverflowMenuItem =
+  | {
+      enabled?: boolean;
+      title?: string | ReactNode;
+      icon?: IconName;
+      action?: ((e: MouseEvent) => void) | (() => void);
+      link?: string;
+      event?: string;
+      testId?: string;
+    }
+  | {
+      component?: ReactNode;
+    };
+
 export const OverflowMenu = ({
   items,
   target,
@@ -22,27 +27,23 @@ export const OverflowMenu = ({
   <Menu>
     <Menu.Target>{target}</Menu.Target>
     <Menu.Dropdown>
-      {items.map((item, index) => {
-        if (item.enabled === false) {
-          return null;
-        }
-
-        if (item.component) {
-          return item.component;
-        }
-
-        return (
-          <Menu.Item
-            key={index}
-            onClick={item.action}
-            data-metabase-event={item.event}
-            data-testid={item.testId}
-            icon={item.icon && <Icon name={item.icon} />}
-          >
-            <Text fw="bold">{item.title}</Text>
-          </Menu.Item>
-        );
-      })}
+      {items.map((item, index) =>
+        match(item)
+          .returnType<ReactNode | null>()
+          .with({ enabled: P.when(enabled => enabled === false) }, () => null)
+          .with({ component: P.not(P.nullish) }, item => item.component)
+          .with({ title: P.not(P.nullish) }, item => (
+            <Menu.Item
+              key={index}
+              onClick={item.action}
+              data-testid={item.testId}
+              icon={item.icon && <Icon name={item.icon} />}
+            >
+              <Text fw="bold">{item.title}</Text>
+            </Menu.Item>
+          ))
+          .otherwise(() => null),
+      )}
     </Menu.Dropdown>
   </Menu>
 );
