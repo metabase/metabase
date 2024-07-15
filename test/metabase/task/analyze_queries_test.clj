30827bb881d94f3be9095294d2112e9988abc33a
Chris Truter <chris@metabase.com>
2024-07-15 14:10:24 +0200

    DRY up test a bit

--- test/metabase/task/analyze_queries_test.clj
+++ test/metabase/task/analyze_queries_test.clj
@@ -33,40 +33,34 @@
                               :query_type    "native"
                               :dataset_query (mt/native-query {:query "SELECT id FROM venues"})}]
 
-      ;; emulate "cards created before QueryField existed"
-      (t2/delete! :model/QueryField :card_id [:in (map :id [c1 c2 c3 c4 arch])])
+      (let [card-ids (map :id [c1 c2 c3 c4 arch])]
 
-      ;; `(first (vals %))` is necessary since h2 generates `:count(id)` as a name for the column
-      (let [get-count #(t2/select-one-fn (comp first vals) [:model/QueryField [[:count :id]]] :card_id %)]
-        (testing "QueryField is empty - queries weren't analyzed"
-          (is (zero? (get-count (:id c1))))
-          (is (zero? (get-count (:id c2))))
-          (is (zero? (get-count (:id c3))))
-          (is (zero? (get-count (:id c4))))
-          (is (zero? (get-count (:id arch)))))
+        ;; emulate "cards created before QueryField existed"
+        (t2/delete! :model/QueryField :card_id [:in card-ids])
 
-        ;; queue the cards
-        (query-analysis/analyze-async! (:id c1))
-        (query-analysis/analyze-async! (:id c2))
-        (query-analysis/analyze-async! (:id c3))
-        (query-analysis/analyze-async! (:id c4))
-        (query-analysis/analyze-async! (:id arch))
+        ;; `(first (vals %))` is necessary since h2 generates `:count(id)` as a name for the column
+        (let [get-count #(t2/select-one-fn (comp first vals) [:model/QueryField [[:count :id]]] :card_id %)]
+          (testing "QueryField is empty - queries weren't analyzed"
+            (is (every? zero? (map get-count card-ids))))
 
-        ;; run the analysis for 1s
-        (try
-          (binding [query-analysis/*parse-queries-in-test?* true]
-            (u/with-timeout 10000
-              (#'task.analyze-queries/analyzer-loop!)))
-          (catch Exception _))
+          ;; queue the cards
+          (run! query-analysis/analyze-async! card-ids)
 
-        (testing "QueryField is filled now"
-          (testing "for a native query"
-            (is (pos? (get-count (:id c1)))))
-          (testing "for a native query with template tags"
-            (is (pos? (get-count (:id c2)))))
-          (testing "for an MBQL"
-            (is (pos? (get-count (:id c3)))))
-          (testing "for an MLv2"
-            (is (pos? (get-count (:id c4)))))
-          (testing "but not for an archived card"
-            (is (zero? (get-count (:id arch))))))))))
+          ;; run the analysis for 1s
+          (try
+            (binding [query-analysis/*parse-queries-in-test?* true]
+              (u/with-timeout 1000
+                (#'task.analyze-queries/analyzer-loop!)))
+            (catch Exception _))
+
+          (testing "QueryField is filled now"
+            (testing "for a native query"
+              (is (pos? (get-count (:id c1)))))
+            (testing "for a native query with template tags"
+              (is (pos? (get-count (:id c2)))))
+            (testing "for an MBQL"
+              (is (pos? (get-count (:id c3)))))
+            (testing "for an MLv2"
+              (is (pos? (get-count (:id c4)))))
+            (testing "but not for an archived card"
+              (is (zero? (get-count (:id arch)))))))))))
