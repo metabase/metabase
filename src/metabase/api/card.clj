df67330e5da463372e6a929f3f31e49318917013
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2024-07-16 18:36:48 -0400

    Refactor perms middleware to enforce view-data at the table-level (#45615)

--- src/metabase/api/card.clj
+++ src/metabase/api/card.clj
@@ -411,7 +411,7 @@
   {:pre [(map? query)]}
   (when-not (query-perms/can-run-query? query)
     (let [required-perms (try
-                           (query-perms/required-perms query :throw-exceptions? true)
+                           (query-perms/required-perms-for-query query :throw-exceptions? true)
                            (catch Throwable e
                              e))]
       (throw (ex-info (tru "You cannot save this Question because you do not have permissions to run its query.")
