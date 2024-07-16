1f87ee4f74a477f7fda10417be822b6179e4742f
Ryan Laurie <iethree@gmail.com>
2024-07-16 11:52:17 -0600

    update imports

--- release/src/milestones.ts
+++ release/src/milestones.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 import _ from "underscore";
 
 import { nonUserFacingLabels } from "./constants";
--- release/src/slack.ts
+++ release/src/slack.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import fs from 'fs';
 
-
 import { WebClient } from '@slack/web-api';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
