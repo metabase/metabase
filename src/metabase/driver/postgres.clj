1f269ace13084ef677f0074373ac80f5d795eeae
Case Nelson <case@metabase.com>
2024-07-16 17:15:49 -0600

    Fix tests

--- src/metabase/driver/postgres.clj
+++ src/metabase/driver/postgres.clj
@@ -66,6 +66,7 @@
                               :now                      true
                               :persist-models           true
                               :schemas                  true
+                              :identifiers-with-spaces  true
                               :uploads                  true}]
   (defmethod driver/database-supports? [:postgres feature] [_driver _feature _db] supported?))
 
