d392a4984b9402df2d2785fdd46620d19d61e92d
Chris Truter <crisptrutski@users.noreply.github.com>
2024-07-16 18:46:46 +0200

    Implement SQL Analysis worker (#45549)

--- src/metabase/query_analysis.clj
+++ src/metabase/query_analysis.clj
@@ -12,29 +12,59 @@
    [metabase.query-analysis.native-query-analyzer.replacement :as nqa.replacement]
    [metabase.util :as u]
    [metabase.util.log :as log]
+   [metabase.util.queue :as queue]
    [toucan2.core :as t2]))
 
-(def ^:dynamic *parse-queries-in-test?*
-  "Normally, a native card's query is parsed on every create/update. For most tests, this is an unnecessary
-  expense. Therefore, we skip parsing while testing unless this variable is turned on.
+(set! *warn-on-reflection* true)
 
-  c.f. [[native-analysis-active?]]"
-  false)
+(def ^:private realtime-queue-capacity 1000)
 
-(defn- native-analysis-active?
-  "Should the query run? Either we're not testing or it's been explicitly turned on.
+(defonce ^:private queue (queue/bounded-transfer-queue realtime-queue-capacity {:dedupe? true}))
 
-  c.f. [[*parse-queries-in-test?*]], [[public-settings/sql-parsing-enabled]]"
-  []
-  (and (public-settings/sql-parsing-enabled)
-       (or (not config/is-test?)
-           *parse-queries-in-test?*)))
+(def ^:dynamic *analyze-execution-in-dev?*
+  "Managing a background thread in the REPL is likely to confound and infuriate, especially when we're using it to run
+  tests. For this reason we run analysis on the main thread by default."
+  ::immediate)
 
-(defn enabled?
+(def ^:dynamic *analyze-execution-in-test?*
+  "A card's query is normally analyzed on every create/update. For most tests, this is an unnecessary expense, hence
+  we disable analysis by default."
+  ::disabled)
+
+(defmacro with-execution*
+  "Override the default execution mode, except in prod."
+  [execution & body]
+  `(binding [*analyze-execution-in-dev?* ~execution
+             *analyze-execution-in-test?* ~execution]
+     ~@body))
+
+(defmacro with-queued-analysis
+  "Override the default execution mode to always use the queue. Does nothing in prod - only use this in tests."
+  [& body]
+  `(with-execution* ::queued ~@body))
+
+(defmacro with-immediate-analysis
+  "Override the default execution mode to always use the current thread. Does nothing in prod - only use this in tests."
+  [& body]
+  `(with-execution* ::immediate ~@body))
+
+(defmacro without-analysis
+  "Override the default execution mode to always use the current thread. Does nothing in prod - only use this in tests."
+  [& body]
+  `(with-execution* ::disabled ~@body))
+
+(defn- execution []
+  (case config/run-mode
+    :prod ::queued
+    :dev  *analyze-execution-in-dev?*
+    :test *analyze-execution-in-test?*))
+
+
+(defn enabled-type?
   "Is analysis of the given query type enabled?"
   [query-type]
   (case query-type
-    :native     (native-analysis-active?)
+    :native     (public-settings/sql-parsing-enabled)
     :query      true
     :mbql/query true
     false))
@@ -46,7 +76,7 @@
   Does not track wildcards for queries rendered as tables afterwards."
   [query]
   (let [query-type (lib/normalized-query-type query)]
-    (when (enabled? query-type)
+    (when (enabled-type? query-type)
       (case query-type
         :native     (try
                       (nqa/field-ids-for-native query)
@@ -58,24 +88,19 @@
 (defn update-query-analysis-for-card!
   "Clears QueryFields associated with this card and creates fresh, up-to-date-ones.
 
-  If you're invoking this from a test, be sure to turn on [[*parse-queries-in-test?*]].
-
   Returns `nil` (and logs the error) if there was a parse error."
   [{card-id :id, query :dataset_query}]
-  (try
-    (let [{:keys [explicit implicit] :as res} (query-field-ids query)
-          id->row                             (fn [explicit? field-id]
-                                                {:card_id            card-id
-                                                 :field_id           field-id
-                                                 :explicit_reference explicit?})
-          query-field-rows                    (concat
-                                               (map (partial id->row true) explicit)
-                                               (map (partial id->row false) implicit))]
-      ;; when the response is `nil`, it's a disabled parser, not unknown columns
-      (when (some? res)
-        (query-field/update-query-fields-for-card! card-id query-field-rows)))
-    (catch Exception e
-      (log/error e "Error updating query fields"))))
+  (let [{:keys [explicit implicit] :as res} (query-field-ids query)
+        id->row          (fn [explicit? field-id]
+                           {:card_id            card-id
+                            :field_id           field-id
+                            :explicit_reference explicit?})
+        query-field-rows (concat
+                          (map (partial id->row true) explicit)
+                          (map (partial id->row false) implicit))]
+    ;; when the response is `nil`, it's a disabled parser, not unknown columns
+    (when (some? res)
+      (query-field/update-query-fields-for-card! card-id query-field-rows))))
 
 (defn- replaced-inner-query-for-native-card
   [query {:keys [fields tables] :as _replacement-ids}]
@@ -126,3 +151,35 @@
     :native (replaced-inner-query-for-native-card q replacements)
     (throw (ex-info "We don't (yet) support replacing field and table refs in cards with MBQL queries"
                     {:card card :replacements replacements}))))
+
+(defn analyze-card!
+  "Update the analysis for the given card, if it is active."
+  [card-id]
+  (let [card (t2/select-one [:model/Card :id :archived :dataset_query] card-id)]
+      (cond
+        (not card)       (log/warnf "Card not found: %s" card-id)
+        (:archived card) (log/warnf "Skipping archived card: %s" card-id)
+        :else            (log/infof "Performing query analysis for card %s" card-id))
+      (when (and card (not (:archived card)))
+        (update-query-analysis-for-card! card))))
+
+(defn next-card-id!
+  "Get the id of the next card id to be analyzed. May block indefinitely, relies on producer."
+  []
+  (queue/blocking-take! queue))
+
+(defn- queue-or-analyze! [offer-fn! card-or-id]
+  (case (execution)
+    ::immediate (analyze-card! (u/the-id card-or-id))
+    ::queued    (offer-fn! queue (u/the-id card-or-id))
+    ::disabled  nil))
+
+(defn analyze-async!
+  "Asynchronously hand-off the given card for analysis, at a high priority."
+  [card-or-id]
+  (queue-or-analyze! queue/maybe-put! card-or-id))
+
+(defn analyze-sync!
+  "Synchronously hand-off the given card for analysis, at a low priority. May block indefinitely, relies on consumer."
+  [card-or-id]
+  (queue-or-analyze! queue/blocking-put! card-or-id))
--- test/metabase/task/analyze_queries_test.clj
+++ test/metabase/task/analyze_queries_test.clj
@@ -1,25 +1,23 @@
-(ns metabase.task.backfill-query-fields-test
+(ns metabase.task.analyze-queries-test
   (:require
-   [clojure.set :as set]
    [clojure.test :refer :all]
    [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
    [metabase.lib.metadata.jvm :as lib.metadata.jvm]
    [metabase.models :refer [Card]]
    [metabase.query-analysis :as query-analysis]
-   [metabase.task.backfill-query-fields :as backfill]
+   [metabase.task.analyze-queries :as task.analyze-queries]
    [metabase.test :as mt]
+   [metabase.util :as u]
    [metabase.util.queue :as queue]
    [toucan2.core :as t2]))
 
-
-(deftest backfill-query-field-test
+(deftest analyzer-loop-test
   (let [metadata-provider (lib.metadata.jvm/application-database-metadata-provider (mt/id))
         venues            (lib.metadata/table metadata-provider (mt/id :venues))
         venues-name       (lib.metadata/field metadata-provider (mt/id :venues :name))
         mlv2-query        (-> (lib/query metadata-provider venues)
                               (lib/aggregate (lib/distinct venues-name)))]
-
     (mt/with-temp [Card c1   {:query_type    "native"
                               :dataset_query (mt/native-query {:query "SELECT id FROM venues"})}
                    Card c2   {:query_type    "native"
@@ -36,31 +34,35 @@
                               :query_type    "native"
                               :dataset_query (mt/native-query {:query "SELECT id FROM venues"})}]
 
-      ;; Make sure there is no existing analysis for the relevant cards
-      (t2/delete! :model/QueryField :card_id [:in (map :id [c1 c2 c3 c4 arch])])
+      (let [card-ids (map :id [c1 c2 c3 c4 arch])]
 
-      ;; Make sure some other card has analysis
-      (testing "There is at least one card with existing analysis"
-        (query-analysis/analyze-card! (:id c3))
-        (is (pos? (count (t2/select :model/QueryField :card_id (:id c3))))))
+        ;; emulate "cards created before QueryField existed"
+        (t2/delete! :model/QueryField :card_id [:in card-ids])
+        (queue/clear! @#'query-analysis/queue)
 
-      (let [queued-ids   (atom #{})
-            expected-ids (into #{} (map :id) [c1 c2 c4])]
+        ;; `(first (vals %))` is necessary since h2 generates `:count(id)` as a name for the column
+        (let [get-count #(t2/select-one-fn (comp first vals) [:model/QueryField [[:count :id]]] :card_id %)]
+          (testing "QueryField is empty - queries weren't analyzed"
+            (is (every? zero? (map get-count card-ids))))
 
-        ;; Run the backfill with a mocked out publisher
-        (#'backfill/backfill-missing-query-fields!
-         #(swap! queued-ids conj (:id %)))
+          ;; queue the cards
+          (query-analysis/with-queued-analysis
+            (run! query-analysis/analyze-async! card-ids))
 
-        (testing "The expected cards were all sent to the analyzer"
-          (is (= expected-ids (set/intersection expected-ids @queued-ids))))
+          ;; run the analysis for 1s
+          (try
+            (u/with-timeout 1000
+              (#'task.analyze-queries/analyzer-loop!))
+            (catch Exception _))
 
-        (testing "The card with existing analysis was not sent to the analyzer again"
-          (is (not (@queued-ids (:id c3)))))))))
-
-
-(comment
-  (set! *warn-on-reflection* true)
-  (queue/clear! @#'query-analysis/queue)
-  (.-queued-set @#'query-analysis/queue)
-  (.peek (.-async-queue @#'query-analysis/queue))
-  (.peek (.-sync-queue @#'query-analysis/queue)))
+          (testing "QueryField is filled now"
+            (testing "for a native query"
+              (is (pos? (get-count (:id c1)))))
+            (testing "for a native query with template tags"
+              (is (pos? (get-count (:id c2)))))
+            (testing "for an MBQL"
+              (is (pos? (get-count (:id c3)))))
+            (testing "for an MLv2"
+              (is (pos? (get-count (:id c4)))))
+            (testing "but not for an archived card"
+              (is (zero? (get-count (:id arch)))))))))))
