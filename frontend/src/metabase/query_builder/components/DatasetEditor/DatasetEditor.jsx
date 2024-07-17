7d84225c1a7c15ea446568a2d1ad593631f7bcac
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-17 20:44:19 +0700

    Refactor EditorTabs component (#45705)
    * Refactor EditoTabs
    - get rid of options prop
    - use named export
    
    * Remove unused data-testid
    
    * Remove redundant id and aria-labelledby
    
    * Remove redundant props spread
    
    * Bring back data-testid in use
    - Thanks VSCode for randomly showing me search results or not...

--- frontend/src/metabase/query_builder/components/DatasetEditor/DatasetEditor.jsx
+++ frontend/src/metabase/query_builder/components/DatasetEditor/DatasetEditor.jsx
@@ -53,7 +53,7 @@ import {
 } from "./DatasetEditor.styled";
 import DatasetFieldMetadataSidebar from "./DatasetFieldMetadataSidebar";
 import DatasetQueryEditor from "./DatasetQueryEditor";
-import EditorTabs from "./EditorTabs";
+import { EditorTabs } from "./EditorTabs";
 import { TabHintToast } from "./TabHintToast";
 import { EDITOR_TAB_INDEXES } from "./constants";
 
@@ -470,16 +470,8 @@ function DatasetEditor(props) {
         center={
           <EditorTabs
             currentTab={datasetEditorTab}
+            disabledMetadata={!resultsMetadata}
             onChange={onChangeEditorTab}
-            options={[
-              { id: "query", name: t`Query`, icon: "notebook" },
-              {
-                id: "metadata",
-                name: t`Metadata`,
-                icon: "label",
-                disabled: !resultsMetadata,
-              },
-            ]}
           />
         }
         buttons={[
