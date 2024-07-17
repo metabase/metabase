9ff11322a67c066b28896e2d4ed1f1930ef42d7c
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-07-17 17:09:33 +0200

    Do not render if there is no time to format

--- frontend/src/metabase/query_builder/components/view/ExecutionTime/ExecutionTime.tsx
+++ frontend/src/metabase/query_builder/components/view/ExecutionTime/ExecutionTime.tsx
@@ -5,11 +5,17 @@ import { Tooltip } from "metabase/ui";
 import { formatDuration } from "./utils";
 
 interface Props {
-  time: number;
+  time?: number;
 }
 
-export const ExecutionTime = ({ time }: Props) => (
-  <Tooltip label={t`Query execution time`}>
-    <span data-testid="execution-time">{formatDuration(time)}</span>
-  </Tooltip>
-);
+export const ExecutionTime = ({ time }: Props) => {
+  if (!time) {
+    return null;
+  }
+
+  return (
+    <Tooltip label={t`Query execution time`}>
+      <span data-testid="execution-time">{formatDuration(time)}</span>
+    </Tooltip>
+  );
+};
