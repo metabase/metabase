77ce9a20604dbdace353f07dab14a7051f9bc41f
Anton Kulyk <kuliks.anton@gmail.com>
2024-07-17 16:56:16 +0100

    Fix outdated import

--- frontend/src/metabase/static-viz/components/ScalarChart/ScalarChart.tsx
+++ frontend/src/metabase/static-viz/components/ScalarChart/ScalarChart.tsx
@@ -1,12 +1,12 @@
 import { ComboChart } from "metabase/static-viz/components/ComboChart";
 import type { StaticChartProps } from "metabase/static-viz/components/StaticVisualization";
-import { scalarToBarTransform } from "metabase/visualizations/visualizations/Scalar/scalars-bar-transform";
+import { scalarToCartesianTransform } from "metabase/visualizations/visualizations/Scalar/scalars-cartesian-transform";
 
 export const ScalarChart = (props: StaticChartProps) => {
   return (
     <ComboChart
       {...props}
-      rawSeries={scalarToBarTransform(
+      rawSeries={scalarToCartesianTransform(
         props.rawSeries,
         {},
         props.renderingContext,
