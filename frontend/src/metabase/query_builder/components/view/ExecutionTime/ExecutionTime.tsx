e59317811e87a7de3385e57af6831f0e4fd38d23
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-07-18 14:13:50 +0200

    45749 follow up: guard agains the potential `null`

--- frontend/src/metabase/query_builder/components/view/ExecutionTime/ExecutionTime.tsx
+++ frontend/src/metabase/query_builder/components/view/ExecutionTime/ExecutionTime.tsx
@@ -4,12 +4,17 @@ import { Tooltip } from "metabase/ui";
 
 import { formatDuration } from "./utils";
 
+/**
+ * `time` can most likely never be `null`
+ * but we don't have type safety in the parent of this component
+ * so we're guarding against it here preemptively!
+ */
 interface Props {
-  time?: number;
+  time?: number | null;
 }
 
 export const ExecutionTime = ({ time }: Props) => {
-  if (time === undefined) {
+  if (time == null) {
     return null;
   }
 
