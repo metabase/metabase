f3ba39de01f9ea6fb5f62c791050a4b561344ae2
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-19 15:46:41 +0700

    Refactor test

--- e2e/test/scenarios/question-reproductions/reproductions.cy.spec.ts
+++ e2e/test/scenarios/question-reproductions/reproductions.cy.spec.ts
@@ -1,47 +1,46 @@
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import { createQuestion, popover, restore } from "e2e/support/helpers";
 
+import type { Filter, LocalFieldReference } from "metabase-types/api";
+
 const { ORDERS, ORDERS_ID } = SAMPLE_DATABASE;
 
 describe("issue 39487", () => {
+  const CREATED_AT_FIELD: LocalFieldReference = [
+    "field",
+    ORDERS.CREATED_AT,
+    {
+      "base-type": "type/DateTime",
+    },
+  ];
+
+  const MONTH_WITH_4_DAY_ROWS = "2015-02-01";
+  const MONTH_WITH_5_DAY_ROWS = "2024-05-01";
+  const MONTH_WITH_6_DAY_ROWS = "2024-06-01";
+
+  // TODO:
+  // - [] before/after/on (single calendar)
+  // - [] between (two calendars)
   beforeEach(() => {
     restore();
     cy.signInAsAdmin();
-
-    createQuestion(
-      {
-        query: {
-          "source-table": ORDERS_ID,
-          aggregation: [["count"]],
-          breakout: [["field", ORDERS.CREATED_AT, { "temporal-unit": "year" }]],
-          filter: [
-            "between",
-            [
-              "field",
-              ORDERS.CREATED_AT,
-              {
-                "base-type": "type/DateTime",
-              },
-            ],
-            // "2015-02-01", // has 4 rows of days
-            "2024-05-01", // has 4 rows of days
-            "2024-06-30", // has 6 rows of days
-          ],
-        },
-        display: "line",
-      },
-      { visitQuestion: true },
-    );
   });
 
   it("calendar has constant size (metabase#39487", () => {
+    createTimeSeriesQuestionWithFilter([
+      "between",
+      CREATED_AT_FIELD,
+      MONTH_WITH_5_DAY_ROWS,
+      MONTH_WITH_6_DAY_ROWS,
+    ]);
+
     cy.findByTestId("timeseries-filter-button").click();
 
     measureDatetimeFilterPicker().then(initialHeight => {
       cy.wrap(initialHeight).as("initialHeight");
     });
 
-    popover().findByRole("tab", { name: "Next" }).click();
+    nextMonth().click();
 
     cy.get("@initialHeight").then(initialHeight => {
       measureDatetimeFilterPicker().then(height => {
@@ -50,10 +49,29 @@ describe("issue 39487", () => {
     });
   });
 
+  function createTimeSeriesQuestionWithFilter(filter: Filter) {
+    createQuestion(
+      {
+        query: {
+          "source-table": ORDERS_ID,
+          aggregation: [["count"]],
+          breakout: [["field", ORDERS.CREATED_AT, { "temporal-unit": "year" }]],
+          filter,
+        },
+        display: "line",
+      },
+      { visitQuestion: true },
+    );
+  }
+
   function measureDatetimeFilterPicker() {
     return cy.findByTestId("datetime-filter-picker").then(([$element]) => {
       const { height } = $element.getBoundingClientRect();
       return height;
     });
+  }
+
+  function nextMonth() {
+    return popover().get("button[data-next]");
   }
 });
