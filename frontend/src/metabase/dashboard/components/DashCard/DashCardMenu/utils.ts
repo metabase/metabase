e99511639dfbfbf4a128974430906c2947d83f4a
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-19 09:01:04 -0400

    Remove can_run_adhoc_query from the FE

--- frontend/src/metabase/dashboard/components/DashCard/DashCardMenu/utils.ts
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardMenu/utils.ts
@@ -1,9 +1,16 @@
 import { PLUGIN_FEATURE_LEVEL_PERMISSIONS } from "metabase/plugins";
+import * as Lib from "metabase-lib";
 import type Question from "metabase-lib/v1/Question";
 import type { Dataset } from "metabase-types/api";
 
 export const canEditQuestion = (question: Question) => {
-  return question.canWrite() && question.canRunAdhocQuery();
+  // question.query() is an expensive call, short circuit when possible
+  if (!question.canWrite()) {
+    return false;
+  }
+
+  const { isEditable } = Lib.queryDisplayInfo(question.query());
+  return isEditable;
 };
 
 export const canDownloadResults = (result?: Dataset) => {
