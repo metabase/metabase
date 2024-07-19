0ec80edb8c514e5bd9f37f72dd2b98234bbee003
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-19 18:18:59 +0700

    Group tests

--- e2e/test/scenarios/question-reproductions/reproductions.cy.spec.ts
+++ e2e/test/scenarios/question-reproductions/reproductions.cy.spec.ts
@@ -18,52 +18,54 @@ describe("issue 39487", () => {
     cy.signInAsAdmin();
   });
 
-  it("calendar has constant size when using single date picker filter (metabase#39487)", () => {
-    createTimeSeriesQuestionWithFilter(["=", CREATED_AT_FIELD, "2015-01-01"]); // 5 day rows
+  describe("timeseries filter button", () => {
+    it("calendar has constant size when using single date picker filter (metabase#39487)", () => {
+      createTimeSeriesQuestionWithFilter(["=", CREATED_AT_FIELD, "2015-01-01"]); // 5 day rows
 
-    cy.findByTestId("timeseries-filter-button").click();
+      cy.findByTestId("timeseries-filter-button").click();
 
-    measureInitialValues();
+      measureInitialValues();
 
-    nextButton().click(); // go to 2015-02 - 4 day rows
-    assertNoLayoutShift();
+      nextButton().click(); // go to 2015-02 - 4 day rows
+      assertNoLayoutShift();
 
-    nextButton().click(); // go to 2015-03 - 5 day rows
-    assertNoLayoutShift();
+      nextButton().click(); // go to 2015-03 - 5 day rows
+      assertNoLayoutShift();
 
-    nextButton().click(); // go to 2015-04 - 5 day rows
-    assertNoLayoutShift();
+      nextButton().click(); // go to 2015-04 - 5 day rows
+      assertNoLayoutShift();
 
-    nextButton().click(); // go to 2015-05 - 6 day rows
-    assertNoLayoutShift();
+      nextButton().click(); // go to 2015-05 - 6 day rows
+      assertNoLayoutShift();
 
-    popover().button("May 2015").click(); // go to year view
-    assertNoLayoutShift();
+      popover().button("May 2015").click(); // go to year view
+      assertNoLayoutShift();
 
-    popover().button("2015").click(); // go to decade view
-    assertNoLayoutShift();
-  });
+      popover().button("2015").click(); // go to decade view
+      assertNoLayoutShift();
+    });
 
-  it("calendar has constant size when using date range filter (metabase#39487)", () => {
-    createTimeSeriesQuestionWithFilter([
-      "between",
-      CREATED_AT_FIELD,
-      "2024-05-01", // 5 day rows
-      "2024-06-01", // 6 day rows
-    ]);
+    it("calendar has constant size when using date range filter (metabase#39487)", () => {
+      createTimeSeriesQuestionWithFilter([
+        "between",
+        CREATED_AT_FIELD,
+        "2024-05-01", // 5 day rows
+        "2024-06-01", // 6 day rows
+      ]);
 
-    cy.findByTestId("timeseries-filter-button").click();
+      cy.findByTestId("timeseries-filter-button").click();
 
-    measureInitialValues();
+      measureInitialValues();
 
-    nextButton().click(); // go to 2024-07 - 5 day rows
-    assertNoLayoutShift();
+      nextButton().click(); // go to 2024-07 - 5 day rows
+      assertNoLayoutShift();
 
-    popover().button("July 2024").click(); // go to year view
-    assertNoLayoutShift();
+      popover().button("July 2024").click(); // go to year view
+      assertNoLayoutShift();
 
-    popover().button("2024").click(); // go to decade view
-    assertNoLayoutShift();
+      popover().button("2024").click(); // go to decade view
+      assertNoLayoutShift();
+    });
   });
 
   function createTimeSeriesQuestionWithFilter(filter: Filter) {
