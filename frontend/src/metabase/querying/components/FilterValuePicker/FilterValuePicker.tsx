954208bfecf73a590a47c93fad3530f5b39bbdce
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-22 10:30:50 -0400

    Fix search field values with has_more_values: true (#45922)

--- frontend/src/metabase/querying/components/FilterValuePicker/FilterValuePicker.tsx
+++ frontend/src/metabase/querying/components/FilterValuePicker/FilterValuePicker.tsx
@@ -89,6 +89,7 @@ function FilterValuePicker({
       <SearchValuePicker
         fieldId={checkNotNull(fieldInfo.fieldId)}
         searchFieldId={checkNotNull(fieldInfo.searchFieldId)}
+        fieldValues={fieldData?.values ?? []}
         selectedValues={selectedValues}
         columnDisplayName={columnInfo.displayName}
         shouldCreate={shouldCreate}
