cd0fc17f78bed9d837906bef144a03f4a29e6b25
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-23 20:42:00 +0700

    Move min height to Calendar

--- frontend/src/metabase/ui/components/inputs/DatePicker/DatePicker.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/DatePicker/DatePicker.styled.tsx
@@ -5,12 +5,6 @@ export const getDatePickerOverrides =
     DatePicker: {
       defaultProps: {
         size: "md",
-        /**
-         * Months have different number of day rows (4, 5 or 6). This causes date picker height to change when
-         * navigating between months, and the "next" & "previous" buttons will shift their positions (metabase#39487).
-         * This value should be the same as the default height of the calendar when 6 day rows are displayed.
-         */
-        mih: 314,
       },
     },
   });
