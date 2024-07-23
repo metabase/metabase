971b28857bd4f9a0de7681a6e2bd3c26451d5dd0
Tamás Benkő <tamas@metabase.com>
2024-07-23 20:48:32 +0300

    Adjust tests

--- test/metabase/auth_provider_test.clj
+++ test/metabase/auth_provider_test.clj
@@ -11,44 +11,48 @@
 
 (defmethod auth-provider/fetch-auth ::test-me
   [_provider _db-id details]
-  (is (= {:key "testing"} details))
+  (is (= "testing" (:key details)))
   {:password "qux"})
 
 (deftest ^:parallel simple-test
-  (mt/with-temp [:model/Database db {:details {:username "test"
-                                               :password "ignored"
-                                               :auth-provider ::test-me
-                                               :auth-provider-details {:key "testing"}}}]
-    (is (=? {:password "qux" :username "test"}
-            (auth-provider/fetch-and-incorporate-auth-provider-details
-              (:engine db)
-              (:id db)
-              (:details db))))))
+  (let [details {:username "test"
+                 :password "ignored"
+                 :auth-provider ::test-me
+                 :key "testing"}]
+    (mt/with-temp [:model/Database db {:details details}]
+      (is (=? (assoc details :password "qux")
+              (auth-provider/fetch-and-incorporate-auth-provider-details
+               (:engine db)
+               (:id db)
+               (:details db)))))))
 
 (deftest http-provider-tests
   (let [original-details (:details (mt/db))
-        http-provider-details {:url (client/build-url "/testing/echo"
-                                                      {:body (json/encode original-details)})}]
+        http-provider-details {:auth-provider "http"
+                               :http-auth-url (client/build-url "/testing/echo"
+                                                                {:body (json/encode original-details)})}]
     (is (= original-details (auth-provider/fetch-auth :http nil http-provider-details)))
-    (is (= original-details (auth-provider/fetch-and-incorporate-auth-provider-details
-                              (tx/driver)
-                              {:auth-provider :http
-                               :auth-provider-details http-provider-details})))))
+    (is (= (merge http-provider-details original-details)
+           (auth-provider/fetch-and-incorporate-auth-provider-details
+            (tx/driver)
+            http-provider-details)))))
+
 (deftest oauth-provider-tests
-  (let [oauth-provider-details {:token-url (client/build-url "/testing/echo"
-                                                             {:body (json/encode {:access_token "foobar"})})}]
+  (let [oauth-provider-details {:auth-provider :oauth
+                                :oauth-token-url (client/build-url "/testing/echo"
+                                                                   {:body (json/encode {:access_token "foobar"})})}]
     (is (= {:access_token "foobar"} (auth-provider/fetch-auth :oauth nil oauth-provider-details)))
-    (is (= {:password "foobar"} (auth-provider/fetch-and-incorporate-auth-provider-details
-                                  (tx/driver)
-                                  {:auth-provider :oauth
-                                   :auth-provider-details oauth-provider-details})))))
+    (is (= (merge oauth-provider-details {:password "foobar"})
+           (auth-provider/fetch-and-incorporate-auth-provider-details
+            (tx/driver)
+            oauth-provider-details)))))
 
 (deftest auth-integration-test
   (mt/test-drivers #{:postgres :mysql}
     (let [original-details (:details (mt/db))
           auth-details {:auth-provider :http
-                        :auth-provider-details {:url (client/build-url "/testing/echo"
-                                                                       {:body (json/encode original-details)})}}]
+                        :http-auth-url (client/build-url "/testing/echo"
+                                                         {:body (json/encode original-details)})}]
       (mt/with-temp [:model/Database db
                      {:engine (tx/driver),
                       :details auth-details}]
