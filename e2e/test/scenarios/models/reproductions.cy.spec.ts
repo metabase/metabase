196e2bf6d7edb89bd188528ae8d20fe51df1b14d
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-23 14:53:17 -0400

    E2E repro

--- e2e/test/scenarios/models/reproductions.cy.spec.ts
+++ e2e/test/scenarios/models/reproductions.cy.spec.ts
@@ -775,6 +775,72 @@ describe("issue 25885", () => {
   });
 });
 
+describe("issue 33844", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+    cy.request("PUT", `/api/card/${ORDERS_QUESTION_ID}`, { type: "model" });
+    cy.intercept("POST", "/api/dataset").as("dataset");
+    cy.intercept("POST", "/api/card").as("createModel");
+    cy.intercept("PUT", "/api/card/*").as("updateModel");
+  });
+
+  function testModelMetadata(isNew: boolean) {
+    cy.log("make a column visible only in detail views");
+    cy.findByTestId("detail-shortcut").should("not.exist");
+    tableHeaderClick("ID");
+    cy.findByLabelText("Detail views only").click();
+    cy.button(isNew ? "Save" : "Save changes").click();
+    if (isNew) {
+      modal().button("Save").click();
+      cy.wait("@createModel");
+    } else {
+      cy.wait("@updateModel");
+    }
+    tableInteractive().findByText("ID").should("not.exist");
+    cy.findAllByTestId("detail-shortcut").first().click();
+    modal().within(() => {
+      cy.findByText("Order").should("be.visible");
+      cy.findByText("ID").should("be.visible");
+      cy.findByTestId("object-detail-close-button").click();
+    });
+
+    cy.log("make the column visible in table views");
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+    tableHeaderClick("ID");
+    cy.findByLabelText("Detail views only").should("be.checked");
+    cy.findByLabelText("Table and details views").click();
+    cy.button("Save changes").click();
+    cy.wait("@updateModel");
+    tableInteractive().findByText("ID").should("be.visible");
+  }
+
+  it("should show hidden PKs in model metadata editor and object details after creating a model (metabase#33844)", () => {
+    cy.visit("/");
+    newButton("Model").click();
+    cy.findByTestId("new-model-options")
+      .findByText("Use the notebook editor")
+      .click();
+    entityPickerModal().within(() => {
+      entityPickerModalTab("Tables").click();
+      cy.findByText("Orders").click();
+    });
+    cy.findByTestId("run-button").click();
+    cy.wait("@dataset");
+    cy.findByTestId("dataset-edit-bar").findByText("Metadata").click();
+    testModelMetadata(true);
+  });
+
+  it("should show hidden PKs in model metadata editor and object details after updating a model (metabase#33844,metabase#45924)", () => {
+    visitModel(ORDERS_QUESTION_ID);
+    cy.wait("@dataset");
+    openQuestionActions();
+    popover().findByText("Edit metadata").click();
+    testModelMetadata(false);
+  });
+});
+
 describe("issue 45924", () => {
   beforeEach(() => {
     restore();
