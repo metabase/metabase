62bb5bc9d2a590498e916345296900de3657ebb8
Oisin Coveney <oisin@metabase.com>
2024-07-23 10:59:02 +0200

    fix(sdk) - fix popover errors on dashboard parameters (#45671)

--- e2e/test/scenarios/embedding/embedding-native.cy.spec.js
+++ e2e/test/scenarios/embedding/embedding-native.cy.spec.js
@@ -90,7 +90,7 @@ describe("scenarios > embedding > native questions", () => {
 
       // Created At: Q2 2023
       filterWidget().contains("Created At").click();
-      cy.findByTestId("select-button").click();
+      cy.findByTestId("select-year-picker").click();
       popover().last().contains("2023").click();
       // eslint-disable-next-line no-unscoped-text-selectors -- deprecated usage
       cy.findByText("Q2").click();
