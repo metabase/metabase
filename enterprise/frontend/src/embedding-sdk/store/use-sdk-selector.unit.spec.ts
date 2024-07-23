30b7fd68698732cf5bc1dce95c613afc7db6aa06
Phoomparin Mano <phoomparin@gmail.com>
2024-07-23 23:08:11 +0700

    fix import type error

--- enterprise/frontend/src/embedding-sdk/store/use-sdk-selector.unit.spec.ts
+++ enterprise/frontend/src/embedding-sdk/store/use-sdk-selector.unit.spec.ts
@@ -1,9 +1,9 @@
 import { renderHook } from "@testing-library/react";
 
 import {
-  USE_OUTSIDE_OF_CONTEXT_MESSAGE,
   useSdkSelector,
-} from "embedding-sdk/store";
+  USE_OUTSIDE_OF_CONTEXT_MESSAGE,
+} from "./use-sdk-selector";
 
 describe("useSdkSelector", () => {
   it("should throw an error if used outside of the MetabaseProvider", () => {
