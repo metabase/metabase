d924325a3da58acbb975c21b609390e678ea0887
Phoomparin Mano <phoomparin@gmail.com>
2024-07-24 01:24:44 +0700

    adds the shim

--- enterprise/frontend/src/embedding-sdk/lib/polyfill-use-sync-external-store.ts
+++ enterprise/frontend/src/embedding-sdk/lib/polyfill-use-sync-external-store.ts
@@ -1,7 +1,9 @@
 import React from "react";
-import { useSyncExternalStore } from "use-sync-external-store";
+import { useSyncExternalStore } from "use-sync-external-store/shim";
 
-// Patches useSyncExternalStore if we are in React 17.
+// Monkey-patches useSyncExternalStore if we are in React 17,
+// where useSyncExternalStore is not available.
+// This is used in react-redux v8, until they've dropped the shim in v9.
 if (!("useSyncExternalStore" in React)) {
   // @ts-expect-error - we're patching for React 17, which doesn't have this method.
   React.useSyncExternalStore = useSyncExternalStore;
