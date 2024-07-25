c80ba16cf4ed2aec6fb3cc52d0a59f33eab6f989
Anton Kulyk <kuliks.anton@gmail.com>
2024-07-25 13:18:36 +0100

    Revert "Remove redundant dataset error checks"
    This reverts commit fc7810f04598cae4238792b2e74754ae524fc059.

--- frontend/src/metabase/dashboard/components/DashCard/DashCardMenu/utils.ts
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardMenu/utils.ts
@@ -16,6 +16,7 @@ export const canEditQuestion = (question: Question) => {
 export const canDownloadResults = (result?: Dataset) => {
   return (
     !!result?.data &&
+    !result.error &&
     PLUGIN_FEATURE_LEVEL_PERMISSIONS.canDownloadResults(result)
   );
 };
