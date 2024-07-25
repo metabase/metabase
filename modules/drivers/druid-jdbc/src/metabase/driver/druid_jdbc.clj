7b932b8da4100107573b475a23110478e4c157f3
Cal Herries <calherries@gmail.com>
2024-07-25 16:59:21 +0300

    ~ don't implement druid jdbc just yet

--- modules/drivers/druid-jdbc/src/metabase/driver/druid_jdbc.clj
+++ modules/drivers/druid-jdbc/src/metabase/driver/druid_jdbc.clj
@@ -7,7 +7,6 @@
    [java-time.api :as t]
    [metabase.driver :as driver]
    [metabase.driver.common :as driver.common]
-   [metabase.driver.sql :as driver.sql]
    [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
    [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
    [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]
@@ -186,7 +185,3 @@
            {:version "0.0"
             :semantic-version [0 0]
             :flavor "fallback"}))))
-
-(defmethod driver.sql/json-field-length :druid-jdbc
-  [_ json-field-identifier]
-  [:strlen [:cast json-field-identifier :varchar]])
