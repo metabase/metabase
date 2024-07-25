927d6c04e515b67ddfd3432577cce7bb976291b3
Emmad Usmani <emmadusmani@berkeley.edu>
2024-07-25 11:06:32 -0700

    remove SUNBURST_SERIES_OPTION constant

--- frontend/src/metabase/visualizations/echarts/pie/option.ts
+++ frontend/src/metabase/visualizations/echarts/pie/option.ts
@@ -1,6 +1,5 @@
 import Color from "color";
 import type { EChartsOption } from "echarts";
-import cloneDeep from "lodash.clonedeep";
 
 import { getTextColorForBackground } from "metabase/lib/colors";
 import type {
@@ -8,11 +7,9 @@ import type {
   RenderingContext,
 } from "metabase/visualizations/types";
 
-import { DIMENSIONS, TOTAL_TEXT } from "../constants";
-import type { PieChartFormatters } from "../format";
-import type { PieChartModel, PieSlice, PieSliceData } from "../model/types";
-
-import { SUNBURST_SERIES_OPTION } from "./constants";
+import { DIMENSIONS, TOTAL_TEXT } from "./constants";
+import type { PieChartFormatters } from "./format";
+import type { PieChartModel, PieSlice, PieSliceData } from "./model/types";
 
 function getSliceByKey(key: PieSliceData["key"], slices: PieSlice[]) {
   const slice = slices.find(s => s.data.key === key);
@@ -38,6 +35,17 @@ function getTotalGraphicOption(
   if (outerRadius * 2 < DIMENSIONS.totalDiameterThreshold) {
     return undefined;
   }
+  const valueText = formatters.formatMetric(
+    hoveredIndex != null
+      ? chartModel.slices[hoveredIndex].data.displayValue
+      : chartModel.total,
+  );
+  const labelText =
+    hoveredIndex != null
+      ? formatters
+          .formatDimension(chartModel.slices[hoveredIndex].data.key)
+          .toUpperCase()
+      : TOTAL_TEXT;
 
   return {
     type: "group",
@@ -53,11 +61,7 @@ function getTotalGraphicOption(
           textAlign: "center",
           fontFamily: renderingContext.fontFamily,
           fill: renderingContext.getColor("text-dark"),
-          text: formatters.formatMetric(
-            hoveredIndex != null
-              ? chartModel.slices[hoveredIndex].data.displayValue
-              : chartModel.total,
-          ),
+          text: valueText,
         },
       },
       {
@@ -70,12 +74,7 @@ function getTotalGraphicOption(
           textAlign: "center",
           fontFamily: renderingContext.fontFamily,
           fill: renderingContext.getColor("text-light"),
-          text:
-            hoveredIndex != null
-              ? formatters
-                  .formatDimension(chartModel.slices[hoveredIndex].data.key)
-                  .toUpperCase()
-              : TOTAL_TEXT,
+          text: labelText,
         },
       },
     ],
@@ -136,28 +135,19 @@ export function getPieChartOption(
   hoveredIndex?: number,
 ): EChartsOption {
   // Sizing
-  const seriesOption = cloneDeep(SUNBURST_SERIES_OPTION); // deep clone to avoid sharing assigned properties with other instances
-  if (!seriesOption.label) {
-    throw Error(`"seriesOption.label" is undefined`);
-  }
-
   const innerSideLength = Math.min(
     sideLength - DIMENSIONS.padding.side * 2,
     DIMENSIONS.maxSideLength,
   );
   const { outerRadius, innerRadius } = getRadiusOption(innerSideLength);
-  seriesOption.radius = [innerRadius, outerRadius];
 
-  seriesOption.itemStyle = {
-    borderWidth:
-      (Math.PI * innerSideLength) / DIMENSIONS.slice.borderProportion, // arc length formula: s = 2πr(θ/360°), we want border to be 1 degree
-  };
+  const borderWidth =
+    (Math.PI * innerSideLength) / DIMENSIONS.slice.borderProportion; // arc length formula: s = 2πr(θ/360°), we want border to be 1 degree
 
   const fontSize = Math.max(
     DIMENSIONS.slice.maxFontSize * (innerSideLength / DIMENSIONS.maxSideLength),
     DIMENSIONS.slice.minFontSize,
   );
-  seriesOption.label.fontSize = fontSize;
 
   // "Show total" setting
   const graphicOption = settings["pie.show_total"]
@@ -182,6 +172,49 @@ export function getPieChartOption(
     );
   };
 
+  // Series data
+  const data = chartModel.slices.map(s => {
+    const labelColor = getTextColorForBackground(
+      s.data.color,
+      renderingContext.getColor,
+    );
+    const label = formatSlicePercent(s.data.key);
+    const isLabelVisible = getIsLabelVisible(
+      label,
+      s,
+      innerRadius,
+      outerRadius,
+      fontSize,
+      renderingContext,
+    );
+
+    return {
+      value: s.data.value,
+      name: s.data.key,
+      itemStyle: { color: s.data.color },
+      label: {
+        color: labelColor,
+        formatter: () => (isLabelVisible ? label : " "),
+      },
+      emphasis: { itemStyle: { color: s.data.color } },
+      blur: {
+        itemStyle: {
+          // We have to fade the slices through `color` rather than `opacity`
+          // becuase echarts' will apply the opacity to the white border,
+          // causing the underlying color to leak. It is safe to use non-hex
+          // values here, since this value will never be used in batik
+          // (there's no emphasis/blur for static viz).
+          color: Color(s.data.color).fade(0.7).rgb().string(),
+          opacity: 1,
+        },
+        label: {
+          opacity:
+            labelColor === renderingContext.getColor("text-dark") ? 0.3 : 1,
+        },
+      },
+    };
+  });
+
   return {
     // Unlike the cartesian chart, `animationDuration: 0` does not prevent the
     // chart from animating on initial render, so we unfortunately have to
@@ -192,48 +225,23 @@ export function getPieChartOption(
     },
     graphic: graphicOption,
     series: {
-      ...seriesOption,
-      data: chartModel.slices.map(s => {
-        const labelColor = getTextColorForBackground(
-          s.data.color,
-          renderingContext.getColor,
-        );
-        const label = formatSlicePercent(s.data.key);
-        const isLabelVisible = getIsLabelVisible(
-          label,
-          s,
-          innerRadius,
-          outerRadius,
-          fontSize,
-          renderingContext,
-        );
-
-        return {
-          value: s.data.value,
-          name: s.data.key,
-          itemStyle: { color: s.data.color },
-          label: {
-            color: labelColor,
-            formatter: () => (isLabelVisible ? label : " "),
-          },
-          emphasis: { itemStyle: { color: s.data.color } },
-          blur: {
-            itemStyle: {
-              // We have to fade the slices through `color` rather than `opacity`
-              // becuase echarts' will apply the opacity to the white border,
-              // causing the underlying color to leak. It is safe to use non-hex
-              // values here, since this value will never be used in batik
-              // (there's no emphasis/blur for static viz).
-              color: Color(s.data.color).fade(0.7).rgb().string(),
-              opacity: 1,
-            },
-            label: {
-              opacity:
-                labelColor === renderingContext.getColor("text-dark") ? 0.3 : 1,
-            },
-          },
-        };
-      }),
+      type: "sunburst",
+      sort: undefined,
+      nodeClick: false,
+      radius: [innerRadius, outerRadius],
+      itemStyle: {
+        borderWidth,
+      },
+      label: {
+        rotate: 0,
+        overflow: "none",
+        fontSize,
+        fontWeight: DIMENSIONS.slice.label.fontWeight,
+      },
+      labelLayout: {
+        hideOverlap: true,
+      },
+      data,
     },
   };
 }
