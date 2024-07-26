dc760a8bfaf38a3b28f2d10f0bb8b0066a81519f
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-07-26 15:35:27 -0400

    Remove unused dimension options from query_metadata (#46195)
    * Remove unused dimension options
    
    * Fix test

--- test/metabase/api/dashboard_test.clj
+++ test/metabase/api/dashboard_test.clj
@@ -4606,8 +4606,7 @@
                                  {:id (mt/id :checkins)}
                                  {:id (mt/id :reviews)}
                                  {:id (mt/id :products)
-                                  :fields sequential?
-                                  :dimension_options map?}
+                                  :fields sequential?}
                                  {:id (mt/id :venues)}])
            :cards [{:id link-card}]
            :databases [{:id (mt/id) :engine string?}]
