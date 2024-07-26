b4fe972049929752e8a2b5bcf0152f3baf555d9c
Bryan Maass <bryan.maass@gmail.com>
2024-07-26 15:45:43 -0600

    rough sketch of how load testing could work

---
+++ dev/src/dev/add_load.clj
@@ -0,0 +1,58 @@
+(ns src.dev.add-load
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk]
+            [metabase-enterprise.serialization.cmd :as cmd]
+            [metabase.test :as mt]))
+
+(defn unwrap [data]
+  (vec (mapcat
+        #(cond
+           (keyword? (first %))
+           [%]
+           (number? (first %))
+           (repeat (first %) (second %))
+           :else (throw (ex-info "Invalid data" {})))
+        data)))
+
+(defn undata [data]
+  (vec (flatten
+        (walk/postwalk
+         (fn [x] (if (and (keyword? x)
+                         (str/starts-with? (name x) "?"))
+                  (symbol (subs (name x) 1))
+                  x))
+         data))))
+
+(defn find-collection-ids [script]
+  (let [ids (atom [])]
+    (walk/prewalk
+     (fn [x]
+       (if (and (keyword? x) (str/starts-with? (name x) "?"))
+         (swap! ids conj (symbol (subs (name x) 1)))
+         x))
+     script)
+    (vec (distinct @ids))))
+
+
+(defmacro load-up [script]
+  `(do (mt/with-temp ~(undata (unwrap #_{:clj-kondo/ignore [:discouraged-var]}
+                                      (eval script)))
+         (cmd/v2-dump! "output_" {:collection-ids ~(find-collection-ids
+                                                    #_{:clj-kondo/ignore [:discouraged-var]}
+                                                    (eval script))}))
+       (cmd/v2-load! "output_" {:backfill? false})))
+
+;; Load scenarios cases look like this
+  (def one-coll-with-300-cards [[:model/Collection {:?coll-id :id} {}]
+                                [300 [:model/Card {} {:collection_id :?coll-id}]]])
+
+(comment
+
+  (time (load-up one-coll-with-300-cards))
+  ;; 300 cards in 1 collection takes about 15 seconds
+
+
+  ;; Caveat, this blows the stack at 400+ entities being inserted, I think due to how with-temp is implemented. To do
+  ;; more, we could switch it to using a loop instead of straight recursion.
+
+  )
