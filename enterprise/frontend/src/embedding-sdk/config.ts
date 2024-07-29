ac9e820c1b048372e4f676e51aedd9ea89e7ffd2
Denis Berezin <denis.berezin@metabase.com>
2024-07-29 16:43:48 +0300

    Refactor to inject version in build step

--- enterprise/frontend/src/embedding-sdk/config.ts
+++ enterprise/frontend/src/embedding-sdk/config.ts
@@ -1,10 +1,4 @@
-import packageJson from "embedding-sdk/package.template.json";
-
 export const DEFAULT_FONT = "Lato";
 export const EMBEDDING_SDK_ROOT_ELEMENT_ID = "metabase-sdk-root";
 
-export let EMBEDDING_SDK_VERSION = "unknown";
-
-try {
-  EMBEDDING_SDK_VERSION = packageJson?.version ?? "unknown";
-} catch (e) {}
+export const EMBEDDING_SDK_VERSION = window.EMBEDDING_SDK_VERSION ?? "unknown";
