1650a4c9326788aee21a3e2d32bee63e13da65e3
Kamil Mielnik <kamil@kamilmielnik.com>
2024-07-30 15:11:14 +0700

    Add test for sql filters - date

--- e2e/test/scenarios/native-filters/sql-filters-reset-clear.cy.spec.ts
+++ e2e/test/scenarios/native-filters/sql-filters-reset-clear.cy.spec.ts
@@ -38,7 +38,7 @@ describe("scenarios > filters > sql filters > reset & clear", () => {
       },
     });
 
-    checkNativeParameters({
+    checkNativeParametersInput({
       defaultValueFormatted: "a",
       otherValue: "{backspace}b",
       otherValueFormatted: "b",
@@ -73,12 +73,50 @@ describe("scenarios > filters > sql filters > reset & clear", () => {
       },
     });
 
-    checkNativeParameters({
+    checkNativeParametersInput({
       defaultValueFormatted: "1",
       otherValue: "{backspace}2",
       otherValueFormatted: "2",
       setValue: (label, value) => {
         filter(label).focus().clear().type(value).blur();
+      },
+    });
+  });
+
+  it("date parameters", () => {
+    createNativeQuestionWithParameters({
+      no_default_non_required: {
+        name: "no_default_non_required",
+        "display-name": NO_DEFAULT_NON_REQUIRED,
+        id: "fed1b918",
+        type: "date",
+      },
+      default_non_required: {
+        name: "default_non_required",
+        "display-name": DEFAULT_NON_REQUIRED,
+        id: "75d67d38",
+        type: "date",
+        default: "2024-01-01",
+      },
+      default_required: {
+        name: "default_required",
+        "display-name": DEFAULT_REQUIRED,
+        id: "60f12ac8",
+        type: "date",
+        required: true,
+        default: "2024-01-01",
+      },
+    });
+
+    checkNativeParametersDropdown({
+      defaultValueFormatted: "January 1, 2024",
+      otherValue: "01/01/2020",
+      otherValueFormatted: "January 1, 2020",
+      setValue: (label, value) => {
+        addDateFilter(label, value);
+      },
+      updateValue: (label, value) => {
+        updateDateFilter(label, value);
       },
     });
   });
@@ -107,14 +145,14 @@ describe("scenarios > filters > sql filters > reset & clear", () => {
     /**
      * Use 'none' when no icon should be visible.
      */
-    icon: "reset" | "clear" | "none",
+    icon: "chevron" | "reset" | "clear" | "none",
   ) {
     clearIcon(label).should(icon === "clear" ? "be.visible" : "not.exist");
     resetIcon(label).should(icon === "reset" ? "be.visible" : "not.exist");
-    chevronIcon(label).should("not.exist");
+    chevronIcon(label).should(icon === "chevron" ? "be.visible" : "not.exist");
   }
 
-  function checkNativeParameters<T = string>({
+  function checkNativeParametersInput<T = string>({
     defaultValueFormatted,
     otherValue,
     otherValueFormatted,
@@ -172,6 +210,75 @@ describe("scenarios > filters > sql filters > reset & clear", () => {
     checkStatusIcon(DEFAULT_REQUIRED, "none");
 
     checkParameterSidebarDefaultValue({
+      defaultValueFormatted,
+      otherValue,
+      otherValueFormatted,
+      setValue,
+      updateValue,
+    });
+  }
+
+  function checkNativeParametersDropdown({
+    defaultValueFormatted,
+    otherValue,
+    otherValueFormatted,
+    setValue,
+    updateValue = setValue,
+  }: {
+    defaultValueFormatted: string;
+    otherValue: string;
+    otherValueFormatted: string;
+    setValue: (label: string, value: string) => void;
+    updateValue?: (label: string, value: string) => void;
+  }) {
+    cy.log("no default value, non-required, no current value");
+    checkStatusIcon(NO_DEFAULT_NON_REQUIRED, "chevron");
+
+    cy.log("no default value, non-required, has current value");
+    setValue(NO_DEFAULT_NON_REQUIRED, otherValue);
+    filter(NO_DEFAULT_NON_REQUIRED).should("have.text", otherValueFormatted);
+    checkStatusIcon(NO_DEFAULT_NON_REQUIRED, "clear");
+    clearButton(NO_DEFAULT_NON_REQUIRED).click();
+    filter(NO_DEFAULT_NON_REQUIRED).should(
+      "have.text",
+      NO_DEFAULT_NON_REQUIRED,
+    );
+    checkStatusIcon(NO_DEFAULT_NON_REQUIRED, "chevron");
+
+    cy.log("has default value, non-required, current value same as default");
+    checkStatusIcon(DEFAULT_NON_REQUIRED, "clear");
+    filter(DEFAULT_NON_REQUIRED).should("have.text", defaultValueFormatted);
+    clearButton(DEFAULT_NON_REQUIRED).click();
+    filter(DEFAULT_NON_REQUIRED).should("have.text", DEFAULT_NON_REQUIRED);
+
+    cy.log("has default value, non-required, no current value");
+    checkStatusIcon(DEFAULT_NON_REQUIRED, "reset");
+    resetButton(DEFAULT_NON_REQUIRED).click();
+    filter(DEFAULT_NON_REQUIRED).should("have.text", defaultValueFormatted);
+    checkStatusIcon(DEFAULT_NON_REQUIRED, "clear");
+
+    cy.log(
+      "has default value, non-required, current value different than default",
+    );
+    updateValue(DEFAULT_NON_REQUIRED, otherValue);
+    filter(DEFAULT_NON_REQUIRED).should("have.text", otherValueFormatted);
+    checkStatusIcon(DEFAULT_NON_REQUIRED, "reset");
+    resetButton(DEFAULT_NON_REQUIRED).click();
+    filter(DEFAULT_NON_REQUIRED).should("have.text", defaultValueFormatted);
+    checkStatusIcon(DEFAULT_NON_REQUIRED, "clear");
+
+    cy.log("has default value, required, value same as default");
+    checkStatusIcon(DEFAULT_REQUIRED, "none");
+
+    cy.log("has default value, required, current value different than default");
+    updateValue(DEFAULT_REQUIRED, otherValue);
+    filter(DEFAULT_REQUIRED).should("have.text", otherValueFormatted);
+    checkStatusIcon(DEFAULT_REQUIRED, "reset");
+    resetButton(DEFAULT_REQUIRED).click();
+    filter(DEFAULT_REQUIRED).should("have.text", defaultValueFormatted);
+    checkStatusIcon(DEFAULT_REQUIRED, "none");
+
+    checkParameterSidebarDateDefaultValue({
       defaultValueFormatted,
       otherValue,
       otherValueFormatted,
@@ -259,8 +366,106 @@ describe("scenarios > filters > sql filters > reset & clear", () => {
     });
   }
 
-  function filter(placeholder: string) {
-    return cy.findByPlaceholderText(placeholder);
+  function checkParameterSidebarDateDefaultValue({
+    defaultValueFormatted,
+    otherValue,
+    otherValueFormatted,
+    setValue,
+    updateValue,
+  }: {
+    defaultValueFormatted: string;
+    otherValue: string;
+    otherValueFormatted: string;
+    setValue: (label: string, value: string) => void;
+    updateValue: (label: string, value: string) => void;
+  }) {
+    cy.log("parameter sidebar");
+
+    cy.findByTestId("visibility-toggler").click();
+    cy.icon("variable").click();
+
+    cy.log(NO_DEFAULT_NON_REQUIRED);
+    filterSection("no_default_non_required").within(() => {
+      filter("Select a default value…").scrollIntoView();
+      filter("Select a default value…").should("have.value", "");
+      checkStatusIcon("Select a default value…", "chevron");
+      filter("Select a default value…").click();
+    });
+
+    popover().findByRole("textbox").clear().type(otherValue).blur();
+    popover().button("Add filter").click();
+
+    filterSection("no_default_non_required").within(() => {
+      filter("Select a default value…").should(
+        "have.value",
+        otherValueFormatted,
+      );
+      checkStatusIcon("Select a default value…", "clear");
+
+      clearIcon("Select a default value…").click();
+      filter("Select a default value…").should("have.value", "");
+      checkStatusIcon("Select a default value…", "chevron");
+    });
+
+    cy.log(DEFAULT_NON_REQUIRED);
+    filterSection("default_non_required").within(() => {
+      filter("Select a default value…").scrollIntoView();
+      filter("Select a default value…").should(
+        "have.value",
+        defaultValueFormatted,
+      );
+      checkStatusIcon("Select a default value…", "clear");
+
+      clearIcon("Select a default value…").click();
+      filter("Select a default value…").should("have.value", "");
+      checkStatusIcon("Select a default value…", "chevron");
+      filter("Select a default value…").click();
+    });
+
+    popover().findByRole("textbox").clear().type(otherValue).blur();
+    popover().button("Add filter").click();
+
+    filterSection("default_non_required").within(() => {
+      filter("Select a default value…").should(
+        "have.value",
+        otherValueFormatted,
+      );
+      checkStatusIcon("Select a default value…", "clear");
+    });
+
+    cy.log(DEFAULT_REQUIRED);
+    filterSection("default_required").within(() => {
+      filter("Select a default value…").scrollIntoView();
+      filter("Select a default value…").should(
+        "have.value",
+        defaultValueFormatted,
+      );
+      checkStatusIcon("Select a default value…", "clear");
+
+      clearIcon("Select a default value…").click();
+      filter("Select a default value…").should("have.value", "");
+      checkStatusIcon("Select a default value…", "chevron");
+      filter("Select a default value…").click();
+    });
+
+    popover().findByRole("textbox").clear().type(otherValue).blur();
+    popover().button("Add filter").click();
+
+    filterSection("default_required").within(() => {
+      filter("Select a default value…").should(
+        "have.value",
+        otherValueFormatted,
+      );
+      checkStatusIcon("Select a default value…", "clear");
+    });
+  }
+
+  function filter(labelOrPlaceholder: string) {
+    // simple filters (text/number) can be found by placeholders
+    // date filters can be found by labels
+    return cy.get(
+      `[placeholder="${labelOrPlaceholder}"],[aria-label="${labelOrPlaceholder}"]`,
+    );
   }
 
   function filterSection(id: string) {
