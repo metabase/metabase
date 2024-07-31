04c81905cfe264c4a29c19b728fb831d68025146
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2024-07-31 09:59:40 -0600

    🍒 Manual Backport: Run without Replay (#45799) (#46305)
    * ci: Run without Replay (#45799)
    
    * Uninstall ReplayIO
    
    * Remove Replay config
    
    * Install and use custom Chrome v111
    
    * Simplify comment
    
    * Set a specific Chrome version commit
    
    * Skip repro for #22517
    # Conflicts:
    #	.github/actions/prepare-cypress/action.yml
    #	.github/workflows/e2e-stress-test-flake-fix.yml
    #	.github/workflows/e2e-tests.yml
    #	.github/workflows/pre-release.yml
    #	e2e/support/config.js
    #	e2e/support/cypress.js
    #	e2e/test/scenarios/models/reproductions.cy.spec.js
    #	package.json
    #	yarn.lock
    
    * sort out download utils
    
    * fix another import

--- e2e/support/config.js
+++ e2e/support/config.js
@@ -1,32 +1,44 @@
+import path from "node:path";
+
+import {
+  removeDirectory,
+  verifyDownloadTasks,
+} from "./commands/downloads/downloadUtils";
 import * as dbTasks from "./db_tasks";
 
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor"); // This function is called when a project is opened or re-opened (e.g. due to the project's config changing)
 const {
   NodeModulesPolyfillPlugin,
 } = require("@esbuild-plugins/node-modules-polyfill");
-const replay = require("@replayio/cypress");
-const { verifyDownloadTasks } = require("cy-verify-downloads");
-
-const {
-  removeDirectory,
-} = require("./commands/downloads/deleteDownloadsFolder");
 
 const isEnterprise = process.env["MB_EDITION"] === "ee";
 
 const hasSnowplowMicro = process.env["MB_SNOWPLOW_AVAILABLE"];
 const snowplowMicroUrl = process.env["MB_SNOWPLOW_URL"];
 
-const isQaDatabase = process.env["QA_DB_ENABLED"];
+const isQaDatabase = process.env["QA_DB_ENABLED"] === "true";
 
 const sourceVersion = process.env["CROSS_VERSION_SOURCE"];
 const targetVersion = process.env["CROSS_VERSION_TARGET"];
 
-const runWithReplay = process.env["CYPRESS_REPLAYIO_ENABLED"];
-
 const feHealthcheckEnabled = process.env["CYPRESS_FE_HEALTHCHECK"] === "true";
 
-const convertStringToInt = string =>
-  string.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0);
+// docs say that tsconfig paths should handle aliases, but they don't
+const assetsResolverPlugin = {
+  name: "assetsResolver",
+  setup(build) {
+    // Redirect all paths starting with "assets/" to "resources/"
+    build.onResolve({ filter: /^assets\// }, args => {
+      return {
+        path: path.join(
+          __dirname,
+          "../../resources/frontend_client/app",
+          args.path,
+        ),
+      };
+    });
+  },
+};
 
 const defaultConfig = {
   // This is the functionality of the old cypress-plugins.js file
@@ -37,40 +49,13 @@ const defaultConfig = {
      **                        PREPROCESSOR                            **
      ********************************************************************/
 
-    if (runWithReplay) {
-      on = replay.wrapOn(on);
-      replay.default(on, config, {
-        upload: true,
-        apiKey: process.env.REPLAY_API_KEY,
-        filter: r => {
-          const hasCrashed = r.status === "crashed";
-          const hasFailed = r.metadata.test?.result === "failed";
-          const isFlaky =
-            r.metadata.test?.result === "passed" &&
-            r.metadata.test.tests.some(r => r.result === "failed");
-          const randomlyUploadAll =
-            r.metadata.source.branch === "master" &&
-            convertStringToInt(r.metadata.test.run.id) % 10 === 1;
-
-          console.log("upload replay ::", {
-            hasCrashed,
-            hasFailed,
-            isFlaky,
-            randomlyUploadAll,
-            branch: r.metadata.source.branch,
-            result: r.metadata.test?.result,
-            status: r.status,
-            runId: r.metadata.test.run.id,
-          });
-          return hasCrashed || hasFailed || isFlaky || randomlyUploadAll;
-        },
-      });
-    }
-
     on(
       "file:preprocessor",
       createBundler({
-        plugins: [NodeModulesPolyfillPlugin()],
+        loader: {
+          ".svg": "text",
+        },
+        plugins: [NodeModulesPolyfillPlugin(), assetsResolverPlugin],
         sourcemap: "inline",
       }),
     );
@@ -147,13 +132,13 @@ const defaultConfig = {
   //   1. testFiles and
   //   2. integrationFolder
   specPattern: "e2e/test/**/*.cy.spec.{js,ts}",
+  viewportHeight: 800,
+  viewportWidth: 1280,
 };
 
 const mainConfig = {
   ...defaultConfig,
   projectId: "ywjy9z",
-  viewportHeight: 800,
-  viewportWidth: 1280,
   numTestsKeptInMemory: process.env["CI"] ? 1 : 50,
   reporter: "cypress-multi-reporters",
   reporterOptions: {
