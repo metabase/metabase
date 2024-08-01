a1312976a7224853619641cf578147b7c58e6083
Romeo Van Snick <romeo@romeovansnick.be>
2024-08-01 18:47:20 +0200

    Move imported test files to cy.spec and barrel to ci.cy.spec

---
+++ e2e/test/scenarios/question-reproductions/question-reproductions.ci.cy.spec.ts
@@ -0,0 +1,28 @@
+import "./4482.cy.spec";
+import "./6239.cy.spec";
+import "./9027.cy.spec";
+import "./13097.cy.spec";
+import "./13263.cy.spec";
+import "./14957.cy.spec";
+import "./15714.cy.spec";
+import "./15876.cy.spec";
+import "./17512.cy.spec";
+import "./17514.cy.spec";
+import "./17910.cy.spec";
+import "./17963.cy.spec";
+import "./18207.cy.spec";
+import "./18978.cy.spec";
+import "./19341.cy.spec";
+import "./19742.cy.spec";
+import "./19893.cy.spec";
+import "./20627.cy.spec";
+import "./20809.cy.spec";
+
+import "./reproductions-2.cy.spec";
+import "./reproductions-3.cy.spec";
+
+describe("question reproductions", () => {
+  it("should run a test", () => {
+    // this needs to be here for Cypress to pick up on this file.
+  });
+});
