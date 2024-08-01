b843c6eb9e993de97cef0cab1d22cb44274d2959
Nicolò Pretto <info@npretto.com>
2024-08-01 10:31:55 +0200

    update `dashboard_pdf_exported` and use it in embedding context (#46076)
    * copy schema for better diff
    
    * adds `dashboard_accessed_via` to the `dashboard_pdf_exported` event

--- e2e/test/scenarios/sharing/downloads/downloads.cy.spec.js
+++ e2e/test/scenarios/sharing/downloads/downloads.cy.spec.js
@@ -362,6 +362,7 @@ describeWithSnowplow("scenarios > dashboard > download pdf", () => {
       expectGoodSnowplowEvent({
         event: "dashboard_pdf_exported",
         dashboard_id: dashboard.id,
+        dashboard_accessed_via: "internal",
       });
     });
   });
