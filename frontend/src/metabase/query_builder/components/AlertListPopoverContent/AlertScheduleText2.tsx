ba60225e1dee7408c9969977eaec6211f223aaca
Oisin Coveney <oisin@metabase.com>
2024-08-01 21:58:34 +0300

    save progress

--- frontend/src/metabase/query_builder/components/AlertListPopoverContent/AlertScheduleText2.tsx
+++ frontend/src/metabase/query_builder/components/AlertListPopoverContent/AlertScheduleText2.tsx
@@ -1,3 +1,12 @@
+import { match } from "ts-pattern";
+import { ScheduleSettings } from "metabase-types/api";
+import _ from "underscore";
+import {
+  AM_PM_OPTIONS,
+  getDayOfWeekOptions,
+  HOUR_OPTIONS,
+} from "metabase/lib/date-time";
+
 export const AlertScheduleText2 = ({
   schedule,
   verbose,
@@ -6,4 +15,32 @@ export const AlertScheduleText2 = ({
   verbose?: boolean;
 }) => {
   const scheduleType = schedule.schedule_type;
+
+  if (!scheduleType) {
+    return null;
+  }
+
+  const hourOfDay = schedule.schedule_hour;
+  const dayOfWeekOptions = getDayOfWeekOptions();
+
+  const day = _.find(
+    dayOfWeekOptions,
+    o => o.value === schedule.schedule_day,
+  )?.name;
+  const hour = _.find(HOUR_OPTIONS, opt => opt.value === hourOfDay % 12)?.name;
+  const amPm = _.find(
+    AM_PM_OPTIONS,
+    opt => hourOfDay && opt.value === (hourOfDay >= 12 ? 1 : 0),
+  )?.name;
+
+  match([scheduleType, verbose])
+    .with(["hourly", true], () => "hourly")
+    .with(["hourly", false], () => "Hourly")
+    .with(["daily", true], () => "daily at " + hour + " " + amPm)
+    .with(["daily", false], () => "Daily, " + hour + " " + amPm)
+    .with(["weekly", true], () => `weekly on ${day}s at ${hour} ${amPm}`)
+    .with(
+      ["weekly", false],
+      () => `${day}s, ${hour.substr(0, hour.indexOf(":"))} ${amPm}`,
+    );
 };
