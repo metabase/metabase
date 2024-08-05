275d4b44f832b25cdde8f9d75edc3d1dd8367b47
Ryan Laurie <iethree@gmail.com>
2024-08-05 17:37:32 -0600

    allow edits to uers with empty name fields

--- frontend/src/metabase/admin/people/containers/EditUserModal.tsx
+++ frontend/src/metabase/admin/people/containers/EditUserModal.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 import type { Params } from "react-router/lib/Router";
 
-import { useUserQuery } from "metabase/common/hooks";
+import { skipToken, useGetUserQuery } from "metabase/api";
 import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 import ModalContent from "metabase/components/ModalContent";
 import Users from "metabase/entities/users";
@@ -17,15 +17,21 @@ interface EditUserModalProps {
 
 export const EditUserModal = ({ onClose, params }: EditUserModalProps) => {
   const dispatch = useDispatch();
-
-  const { data: user, isLoading } = useUserQuery({
-    id: parseInt(params.userId),
-  });
+  const userId = params.userId ? parseInt(params.userId) : null;
+  const { data: user, isLoading } = useGetUserQuery(userId ?? skipToken);
 
   const initialValues = useMemo(() => getInitialValues(user), [user]);
 
-  const handleSubmit = async (val: Partial<UserType>) => {
-    await dispatch(Users.actions.update({ id: user?.id, ...val }));
+  const handleSubmit = async (newValues: Partial<UserType>) => {
+    // first name and last name keys need to be present so they can
+    // potentially be removed
+    const submitValues = {
+      first_name: null,
+      last_name: null,
+      ...newValues,
+    };
+    // can't use metabase/api hook here until the people list uses it too
+    await dispatch(Users.actions.update({ id: user?.id, ...submitValues }));
     onClose();
   };
 
@@ -48,8 +54,8 @@ export const EditUserModal = ({ onClose, params }: EditUserModalProps) => {
 
 const getInitialValues = (user?: UserType) => {
   return {
-    first_name: user?.first_name ?? "",
-    last_name: user?.last_name ?? "",
+    first_name: user?.first_name,
+    last_name: user?.last_name,
     email: user?.email,
     user_group_memberships: user?.user_group_memberships || [],
     login_attributes: user?.login_attributes || {},
