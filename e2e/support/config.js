a22a647697404f4a27b7121ae2924c4f9b402b0a
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-08-05 16:38:45 +0300

    enable video recording

--- e2e/support/config.js
+++ e2e/support/config.js
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import path from "node:path";
 
 import installLogsPrinter from "cypress-terminal-report/src/installLogsPrinter";
@@ -100,6 +101,21 @@ const defaultConfig = {
       removeDirectory,
     });
 
+    // this is an official workaround to keep recordings of the failed specs only
+    // https://docs.cypress.io/guides/guides/screenshots-and-videos#Delete-videos-for-specs-without-failing-or-retried-tests
+    on("after:spec", (spec, results) => {
+      if (results && results.video) {
+        // Do we have failures for any retry attempts?
+        const failures = results.tests.some(test =>
+          test.attempts.some(attempt => attempt.state === "failed"),
+        );
+        if (!failures) {
+          // delete the video if the spec passed and no tests retried
+          fs.unlinkSync(results.video);
+        }
+      }
+    });
+
     /********************************************************************
      **                          CONFIG                                **
      ********************************************************************/
@@ -139,6 +155,9 @@ const defaultConfig = {
   specPattern: "e2e/test/**/*.cy.spec.{js,ts}",
   viewportHeight: 800,
   viewportWidth: 1280,
+  // enable video recording in run mode
+  video: true,
+  videoCompression: true,
 };
 
 const mainConfig = {
