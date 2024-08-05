5500e5d4a481618c7a8219c88414e1b9d80507cf
Phoomparin Mano <phoomparin@gmail.com>
2024-08-06 00:09:58 +0700

    refactor asking for database connection info

--- enterprise/frontend/src/embedding-sdk/cli/steps/add-database-connection.ts
+++ enterprise/frontend/src/embedding-sdk/cli/steps/add-database-connection.ts
@@ -1,16 +1,12 @@
-import fs from "fs/promises";
-
-import { select, input, password, number } from "@inquirer/prompts";
-import chalk from "chalk";
-import fileSelector from "inquirer-file-selector";
+import { select } from "@inquirer/prompts";
 import toggle from "inquirer-toggle";
-import { match } from "ts-pattern";
-import _ from "underscore";
 
 import type { CliStepMethod } from "embedding-sdk/cli/types/cli";
-import type { EngineField, Settings } from "metabase-types/api/settings";
+import type { Settings } from "metabase-types/api/settings";
 
+import { CLI_SHOWN_DB_ENGINES } from "../constants/database";
 import { addDatabaseConnection } from "../utils/add-database-connection";
+import { askForDatabaseConnectionInfo } from "../utils/ask-for-db-connection-info";
 import { fetchInstanceSettings } from "../utils/fetch-instance-settings";
 import { printError } from "../utils/print";
 
@@ -38,100 +34,13 @@ export const addDatabaseConnectionStep: CliStepMethod = async state => {
 
     const engine = settings.engines[engineKey];
     const engineName = engine["driver-name"];
-    const fields = engine["details-fields"] ?? [];
-
-    const connection: Record<string, string | boolean | number> = {};
-
-    for (const field of fields) {
-      // Skip fields that are not shown in the CLI
-      if (!SHOWN_DB_FIELDS.includes(field.name)) {
-        continue;
-      }
-
-      const visibleIf = field["visible-if"];
-
-      const shouldShowField =
-        !visibleIf ||
-        Object.entries(visibleIf).every(
-          ([key, expected]) => connection[key] === expected,
-        );
-
-      // Skip fields that should be hidden
-      if (!shouldShowField) {
-        continue;
-      }
-
-      const name = field["display-name"];
-      const helperText = field["helper-text"];
-
-      const message = `${name}:`;
-
-      if (helperText) {
-        console.log(`  ${chalk.gray(helperText)}`);
-      }
-
-      const value = await match(field.type)
-        .with("boolean", () =>
-          toggle({ message, default: Boolean(field.default) }),
-        )
-        .with("password", () =>
-          password({
-            message,
-            mask: true,
-          }),
-        )
-        .with("integer", () =>
-          number({
-            message,
-            required: field.required ?? false,
-            ...getIntegerFieldDefault(field, engineKey),
-          }),
-        )
-        .with("textFile", async () => {
-          const path = await fileSelector({ message });
-
-          return fs.readFile(path, "utf-8");
-        })
-        .with("section", async () => {
-          if (field.name === "use-hostname") {
-            const choice = await select({
-              message: "Do you want to connect with hostname or account name?",
-              choices: [
-                { name: "Hostname", value: "hostname" },
-                { name: "Account name", value: "account" },
-              ],
-            });
-
-            return choice === "hostname";
-          }
-
-          if (field.name === "use-conn-uri") {
-            const choice = await select({
-              message:
-                "Do you want to connect with hostname or connection string?",
-              choices: [
-                { name: "Hostname", value: "hostname" },
-                { name: "Connection String", value: "conn-uri" },
-              ],
-            });
-
-            return choice === "conn-uri";
-          }
-        })
-        .otherwise(() =>
-          input({
-            message,
-            required: field.required ?? false,
-            ...(!!field.default && { default: String(field.default) }),
-          }),
-        );
-
-      if (value !== undefined) {
-        connection[field.name] = value;
-      }
-    }
 
     try {
+      const connection = await askForDatabaseConnectionInfo({
+        engine,
+        engineKey,
+      });
+
       await addDatabaseConnection({
         name: engineName,
         engine: engineKey,
@@ -152,63 +61,7 @@ export const addDatabaseConnectionStep: CliStepMethod = async state => {
   return [{ type: "done" }, { ...state, settings }];
 };
 
-/** Show popular database engines in the CLI */
-const SHOWN_DB_ENGINES = [
-  "postgres",
-  "mysql",
-  "sqlserver",
-  "bigquery-cloud-sdk",
-  "snowflake",
-  "redshift",
-  "mongo",
-  "athena",
-];
-
-/** Database connection fields that are shown in the CLI */
-const SHOWN_DB_FIELDS = [
-  // Common connection fields for all databases
-  "host",
-  "port",
-  "dbname",
-  "user",
-  "pass",
-  "password",
-  "ssl",
-
-  // Snowflake fields
-  "use-hostname",
-  "account",
-
-  // BigQuery fields
-  "project-id",
-  "service-account-json",
-
-  // Amazon Athena fields
-  "region",
-  "workgroup",
-  "s3_staging_dir",
-  "access_key",
-  "secret_key",
-
-  // MongoDB fields
-  "use-conn-uri",
-  "conn-uri",
-  "authdb",
-];
-
 const getEngineChoices = (settings: Settings) =>
   Object.entries(settings.engines)
     .map(([key, engine]) => ({ name: engine["driver-name"], value: key }))
-    .filter(engine => SHOWN_DB_ENGINES.includes(engine.value));
-
-const getIntegerFieldDefault = (field: EngineField, engine: string) => {
-  if (field.default) {
-    return { default: Number(field.default) };
-  }
-
-  if (field.name === "port") {
-    if (engine === "postgres") {
-      return { default: 5432 };
-    }
-  }
-};
+    .filter(engine => CLI_SHOWN_DB_ENGINES.includes(engine.value));
---
+++ enterprise/frontend/src/embedding-sdk/cli/utils/ask-for-db-connection-info.ts
@@ -0,0 +1,130 @@
+import fs from "fs/promises";
+
+import { input, number, password, select } from "@inquirer/prompts";
+import chalk from "chalk";
+import fileSelector from "inquirer-file-selector";
+import toggle from "inquirer-toggle";
+import { match } from "ts-pattern";
+
+import type { Engine, EngineField } from "metabase-types/api";
+
+import { CLI_SHOWN_DB_FIELDS } from "../constants/database";
+
+interface Options {
+  engine: Engine;
+  engineKey: string;
+}
+
+export async function askForDatabaseConnectionInfo(options: Options) {
+  const { engine, engineKey } = options;
+
+  const fields = engine["details-fields"] ?? [];
+
+  const connection: Record<string, string | boolean | number> = {};
+
+  for (const field of fields) {
+    // Skip fields that are not shown in the CLI
+    if (!CLI_SHOWN_DB_FIELDS.includes(field.name)) {
+      continue;
+    }
+
+    const visibleIf = field["visible-if"];
+
+    const shouldShowField =
+      !visibleIf ||
+      Object.entries(visibleIf).every(
+        ([key, expected]) => connection[key] === expected,
+      );
+
+    // Skip fields that should be hidden
+    if (!shouldShowField) {
+      continue;
+    }
+
+    const name = field["display-name"];
+    const helperText = field["helper-text"];
+
+    const message = `${name}:`;
+
+    if (helperText) {
+      console.log(`  ${chalk.gray(helperText)}`);
+    }
+
+    const value = await match(field.type)
+      .with("boolean", () =>
+        toggle({ message, default: Boolean(field.default) }),
+      )
+      .with("password", () =>
+        password({
+          message,
+          mask: true,
+        }),
+      )
+      .with("integer", () =>
+        number({
+          message,
+          required: field.required ?? false,
+          ...getIntegerFieldDefault(field, engineKey),
+        }),
+      )
+      .with("textFile", async () => {
+        const path = await fileSelector({ message });
+
+        return fs.readFile(path, "utf-8");
+      })
+      .with("section", () => askSectionChoice(field))
+      .otherwise(() =>
+        input({
+          message,
+          required: field.required ?? false,
+          ...(!!field.default && { default: String(field.default) }),
+        }),
+      );
+
+    if (value !== undefined) {
+      connection[field.name] = value;
+    }
+  }
+
+  return connection;
+}
+
+const getIntegerFieldDefault = (field: EngineField, engine: string) => {
+  if (field.default) {
+    return { default: Number(field.default) };
+  }
+
+  if (field.name === "port") {
+    if (engine === "postgres") {
+      return { default: 5432 };
+    }
+  }
+};
+
+const askSectionChoice = async (field: EngineField) => {
+  // Snowflake allows to connect with either hostname or account name.
+  if (field.name === "use-hostname") {
+    const choice = await select({
+      message: "Do you want to connect with hostname or account name?",
+      choices: [
+        { name: "Hostname", value: "hostname" },
+        { name: "Account name", value: "account" },
+      ],
+    });
+
+    return choice === "hostname";
+  }
+
+  // MongoDB allows to connect with either hostname or connection string.
+  if (field.name === "use-conn-uri") {
+    const choice = await select({
+      message: "Do you want to connect with hostname or connection string?",
+      choices: [
+        { name: "Hostname", value: "hostname" },
+        { name: "Connection String", value: "conn-uri" },
+      ],
+    });
+
+    return choice === "conn-uri";
+  }
+};
