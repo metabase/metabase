3e479328966149b7eb1979b5e20325219828bc19
Oisin Coveney <oisin@metabase.com>
2024-08-05 13:09:59 +0300

    fix e2e test

--- frontend/src/metabase/query_builder/components/AlertListPopoverContent/AlertScheduleText.tsx
+++ frontend/src/metabase/query_builder/components/AlertListPopoverContent/AlertScheduleText.tsx
@@ -1,72 +1,89 @@
+import { useCallback } from "react";
 import _ from "underscore";
 
 import {
   AM_PM_OPTIONS,
-  getDayOfWeekOptions,
   HOUR_OPTIONS,
+  getDayOfWeekOptions,
 } from "metabase/lib/date-time";
-import { Box, Text } from "metabase/ui";
+import { isNullOrUndefined } from "metabase/lib/types";
 import type { Channel } from "metabase-types/api";
-
-const getScheduleText = ({ schedule, verbose }: AlertScheduleTextProps) => {
-  const scheduleType = schedule.schedule_type;
-  const hourOfDay = schedule.schedule_hour;
-
-  // these are pretty much copy-pasted from SchedulePicker
-  if (scheduleType === "hourly") {
-    return verbose ? "hourly" : "Hourly";
-  } else if (scheduleType === "daily" && hourOfDay) {
-    const hour = _.find(
-      HOUR_OPTIONS,
-      opt => opt.value === hourOfDay % 12,
-    )?.name;
-    const amPm = _.find(
-      AM_PM_OPTIONS,
-      opt => opt.value === (hourOfDay >= 12 ? 1 : 0),
-    )?.name;
-
-    return `${verbose ? "daily at " : "Daily, "} ${hour} ${amPm}`;
-  } else if (scheduleType === "weekly" && hourOfDay) {
-    const dayOfWeekOptions = getDayOfWeekOptions();
-
-    const day = _.find(
-      dayOfWeekOptions,
-      o => o.value === schedule.schedule_day,
-    )?.name;
-    const hour = _.find(
-      HOUR_OPTIONS,
-      opt => opt.value === hourOfDay % 12,
-    )?.name;
-    const amPm = _.find(
-      AM_PM_OPTIONS,
-      opt => opt.value === (hourOfDay >= 12 ? 1 : 0),
-    )?.name;
-
-    if (!hour) {
-      return;
-    }
-
-    if (verbose) {
-      return `weekly on ${day}s at ${hour} ${amPm}`;
-    } else {
-      // omit the minute part of time
-      return `${day}s, ${hour.substring(0, hour.indexOf(":"))} ${amPm}`;
-    }
-  }
-};
 
 type AlertScheduleTextProps = {
   schedule: Channel;
-  verbose: boolean;
+  verbose?: boolean;
 };
 
 export const AlertScheduleText = ({
   schedule,
   verbose,
-}: AlertScheduleTextProps) => (
-  <Box component="span">
-    <Text fw={verbose ? "bold" : "normal"}>
-      {getScheduleText({ schedule, verbose })}
-    </Text>
-  </Box>
-);
+}: AlertScheduleTextProps) => {
+  const getScheduleText = useCallback(() => {
+    const scheduleType = schedule.schedule_type;
+
+    if (!scheduleType) {
+      return null;
+    }
+
+    // these are pretty much copy-pasted from SchedulePicker
+    if (scheduleType === "hourly") {
+      return verbose ? "hourly" : "Hourly";
+    } else if (scheduleType === "daily") {
+      const hourOfDay = schedule.schedule_hour;
+
+      if (isNullOrUndefined(hourOfDay)) {
+        return null;
+      }
+
+      const hour = _.find(
+        HOUR_OPTIONS,
+        opt => opt.value === hourOfDay % 12,
+      )?.name;
+      const amPm = _.find(
+        AM_PM_OPTIONS,
+        opt => opt.value === (hourOfDay >= 12 ? 1 : 0),
+      )?.name;
+
+      return `${verbose ? "daily at " : "Daily, "} ${hour} ${amPm}`;
+    } else if (scheduleType === "weekly") {
+      const hourOfDay = schedule.schedule_hour;
+      if (isNullOrUndefined(hourOfDay)) {
+        return null;
+      }
+
+      const dayOfWeekOptions = getDayOfWeekOptions();
+
+      const day = _.find(
+        dayOfWeekOptions,
+        o => o.value === schedule.schedule_day,
+      )?.name;
+      const hour = _.find(
+        HOUR_OPTIONS,
+        opt => opt.value === hourOfDay % 12,
+      )?.name;
+      const amPm = _.find(
+        AM_PM_OPTIONS,
+        opt => opt.value === (hourOfDay >= 12 ? 1 : 0),
+      )?.name;
+
+      if (verbose) {
+        return `weekly on ${day}s at ${hour} ${amPm}`;
+      } else {
+        // omit the minute part of time
+        return `${day}s, ${hour?.substring(0, hour.indexOf(":"))} ${amPm}`;
+      }
+    }
+  }, [schedule, verbose]);
+
+  const scheduleText = getScheduleText();
+
+  if (verbose) {
+    return (
+      <span>
+        Checking <b>{scheduleText}</b>
+      </span>
+    );
+  } else {
+    return <span>{scheduleText}</span>;
+  }
+};
