89cf23742d404b4b465973a6a7abd399223d94ef
Oisin Coveney <oisin@metabase.com>
2024-08-08 15:24:33 +0300

    Convert DashboardHeaderDivider to metabase/ui Divider + Remove DashboardHeader.styled.tsx

--- frontend/src/metabase/dashboard/components/DashboardHeader/DashboardHeader.styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const DashboardHeaderActionDivider = styled.div`
-  height: 1.25rem;
-  padding-left: 0.5rem;
-  margin-left: 0.5rem;
-  width: 0;
-  border-left: 1px solid var(--mb-color-border);
-`;
