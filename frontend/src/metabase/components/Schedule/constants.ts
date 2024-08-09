25c758b111de1c59e9f9897f420b3978b7262fc3
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-08-09 11:12:37 -0400

    Delete stale comment

--- frontend/src/metabase/components/Schedule/constants.ts
+++ frontend/src/metabase/components/Schedule/constants.ts
@@ -56,8 +56,7 @@ export const getScheduleStrings = () => {
   const scheduleOptionNames = {
     // The context is needed because 'hourly' can be an adjective ('hourly
     // rate') or adverb ('update hourly'). Same with 'daily', 'weekly', and
-    // 'monthly'. This also provides the sort order: hourly < daily < weekly <
-    // monthly.
+    // 'monthly'.
     hourly: c("adverb").t`hourly`,
     daily: c("adverb").t`daily`,
     weekly: c("adverb").t`weekly`,
