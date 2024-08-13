733dde5ebe9cc06a00bc638eb32ae850cd6fea8c
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-08-13 11:58:40 -0400

    Speed up permissions migrations (round 2) (#46393) (#46778)
    * new version of data_access migration + run ANALYZE beforehand
    
    * relax constraints on migration timestamps
    
    * more migration rewrites
    
    * fix bug in migration
    
    * fix another bug and remove extraneous logging
    
    * reorder migrations to fix ID validation
    
    * start adapting the changes to mysql as well
    
    * wip
    
    * add more ANALYZE calls
    
    * revert logging changes
    
    * finish duplicating work for mysql + a bit of clean up
    
    * create H2 versions of migrations without the ANALYZE calls
    
    * remove ANALYZE call from view_data.sql (probably not necessary)
    
    * fix mysql syntax
    
    * fix another mysql bug
    
    * another mysql fix
    
    * Don't exclude inactive tables for data_access
    
    * Don't write rows for Administrators group
    
    * Don't write rows for Admins (H2/MySQL)
    
    * disable triggers before perms migration
    
    ---------
    
    Co-authored-by: Noah Moss <32746338+noahmoss@users.noreply.github.com>
    Co-authored-by: John Swanson <john.swanson@metabase.com>

--- resources/migrations/permissions/mysql_manage_table_metadata.sql
+++ resources/migrations/permissions/mysql_manage_table_metadata.sql
@@ -39,43 +39,61 @@ WHERE pg.name != 'Administrators'
           ELSE FALSE
       END;
 
--- Insert table-level permissions only where no DB-level permissions exist
+ANALYZE TABLE data_permissions;
+
+-- Insert 'yes' permissions based on existing permissions
 INSERT INTO data_permissions (group_id, perm_type, db_id, schema_name, table_id, perm_value)
 WITH escaped_schema_table AS (
-SELECT mt.id,
-       mt.db_id,
-       mt.schema,
-       replace(replace(mt.schema, '\\', '\\\\'), '/', '\\/') AS escaped_schema
-    FROM metabase_table mt
+  SELECT
+    mt.id as table_id,
+    mt.db_id,
+    mt.schema,
+    CONCAT('/data-model/db/', mt.db_id, '/schema/', REPLACE(REPLACE(mt.schema, '\\', '\\\\'), '/', '\\/'), '/') AS schema_path,
+    CONCAT('/data-model/db/', mt.db_id, '/schema/', REPLACE(REPLACE(mt.schema, '\\', '\\\\'), '/', '\\/'), '/table/', mt.id, '/') AS table_path
+  FROM metabase_table mt
 )
-SELECT pg.id AS group_id,
-       'perms/manage-table-metadata' AS perm_type,
-       mt.db_id,
-       mt.schema AS schema_name,
-       mt.id AS table_id,
-       CASE
-           WHEN EXISTS
-                  (SELECT 1
-                   FROM permissions p
-                   WHERE p.group_id = pg.id
-                     AND (p.object = concat('/data-model/db/', mt.db_id, '/schema/', mt.escaped_schema, '/')
-                          OR p.object = concat('/data-model/db/', mt.db_id, '/schema/', mt.escaped_schema, '/table/', mt.id, '/'))) THEN 'yes'
-           ELSE 'no'
-       END AS perm_value
+SELECT
+    p.group_id,
+    'perms/manage-table-metadata' AS perm_type,
+    est.db_id,
+    est.schema AS schema_name,
+    est.table_id,
+    'yes' AS perm_value
+FROM escaped_schema_table est
+JOIN permissions p
+ON p.object IN (
+    est.schema_path,
+    est.table_path
+)
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM data_permissions dp
+    WHERE dp.group_id = p.group_id
+      AND dp.db_id = est.db_id
+      AND dp.table_id = est.table_id
+      AND dp.perm_type = 'perms/manage-table-metadata'
+);
+
+ANALYZE TABLE data_permissions;
+
+-- Insert 'no' permissions for any table and group combinations that weren't covered by the previous query
+INSERT INTO data_permissions (group_id, perm_type, db_id, schema_name, table_id, perm_value)
+SELECT
+    pg.id AS group_id,
+    'perms/manage-table-metadata' AS perm_type,
+    mt.db_id,
+    mt.schema AS schema_name,
+    mt.id AS table_id,
+    'no' AS perm_value
 FROM permissions_group pg
-CROSS JOIN escaped_schema_table mt
-WHERE pg.name != 'Administrators'
-  AND NOT EXISTS
-    (SELECT 1
-     FROM data_permissions dp
-     WHERE dp.group_id = pg.id
-       AND dp.db_id = mt.db_id
-       AND dp.table_id = mt.id
-       AND dp.perm_type = 'perms/manage-table-metadata')
-  AND NOT EXISTS
-    (SELECT 1
-     FROM data_permissions dp
-     WHERE dp.group_id = pg.id
-       AND dp.db_id = mt.db_id
-       AND dp.table_id IS NULL
-       AND dp.perm_type = 'perms/manage-table-metadata');
+CROSS JOIN metabase_table mt
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM data_permissions dp
+    WHERE dp.group_id = pg.id
+      AND dp.db_id = mt.db_id
+      AND (dp.table_id = mt.id
+           OR dp.table_id IS NULL)
+      AND dp.perm_type = 'perms/manage-table-metadata'
+)
+AND pg.name != 'Administrators';
