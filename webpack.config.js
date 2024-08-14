f5ed18e797f1399b48d25f232220a7f3f9a79278
Jordi Giron <jordi@revanguard.com>
2024-08-14 17:14:02 +0200

    Added envs

--- webpack.config.js
+++ webpack.config.js
@@ -309,8 +309,8 @@ const config = {
     new webpack.EnvironmentPlugin({
       WEBPACK_BUNDLE: "development",
       MB_LOG_ANALYTICS: "false",
-      REACT_APP_WEBSOCKET_SERVER: process.env.REACT_APP_WEBSOCKET_SERVER || "ws://localhost:8090",
-      REACT_APP_SEMANTIC: process.env.REACT_APP_SEMANTIC || "http://localhost:3001"
+      REACT_APP_WEBSOCKET_SERVER: process.env.REACT_APP_WEBSOCKET_SERVER || "wss://dev.chat.omniloy.com",
+      REACT_APP_SEMANTIC: process.env.REACT_APP_SEMANTIC || "https://dev.semantic.omniloy.com"
     }),
     // https://github.com/remarkjs/remark/discussions/903
     new webpack.ProvidePlugin({ process: "process/browser.js" }),
