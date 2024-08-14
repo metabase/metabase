e1a19f9680a9660854ed75acc5696efcc99cd0ab
Phoomparin Mano <phoomparin@gmail.com>
2024-08-14 15:11:23 +0800

    activate metabase license

--- enterprise/frontend/src/embedding-sdk/cli/steps/setup-license.ts
+++ enterprise/frontend/src/embedding-sdk/cli/steps/setup-license.ts
@@ -6,6 +6,7 @@ import open from "open";
 import type { CliStepMethod } from "embedding-sdk/cli/types/cli";
 
 import { printEmptyLines, printInfo } from "../utils/print";
+import { propagateErrorResponse } from "../utils/propagate-error-response";
 
 const trialUrl = `https://store.metabase.com/checkout?plan=pro&deployment=self-hosted`;
 const trialUrlWithUtm = `${trialUrl}&utm_source=product&utm_medium=checkout&utm_campaign=embedding-sdk&utm_content=embedding-sdk-cli`;
@@ -42,5 +43,26 @@ export const setupLicense: CliStepMethod = async state => {
     required: true,
   });
 
-  return [{ type: "success" }, { ...state, token }];
+  // Activate the license
+  try {
+    const res = await fetch(
+      `${state.instanceUrl}/api/setting/premium-embedding-token`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          value: token.trim(),
+        }),
+        headers: { "content-type": "json", cookie: state.cookie ?? "" },
+      },
+    );
+
+    await propagateErrorResponse(res);
+
+    return [{ type: "success" }, { ...state, token }];
+  } catch (error) {
+    const reason = error instanceof Error ? error.message : String(error);
+    const message = `Failed to activate license. Reason: ${reason}`;
+
+    return [{ type: "error", message }, state];
+  }
 };
