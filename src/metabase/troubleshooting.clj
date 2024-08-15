406e03f4c1fc80d69b9e62903afb482e2de13acb
bryan <bryan.maass@gmail.com>
2024-08-15 13:16:18 -0600

    skip some troubleshooting stats when hosted (#46749)
    * skip some troubleshooting stats when hosted
    
    * fix typo
    
    * just skip "system-info" and "app db details"

--- src/metabase/troubleshooting.clj
+++ src/metabase/troubleshooting.clj
@@ -30,21 +30,22 @@
   "Make it easy for the user to tell us what they're using"
   []
   (merge
-   {:databases                    (->> (t2/select :model/Database) (map :engine) distinct)
-    :hosting-env                  (stats/environment-type)
-    :application-database         (mdb/db-type)
-    :application-database-details (t2/with-connection [^java.sql.Connection conn]
-                                    (let [metadata (.getMetaData conn)]
-                                      {:database    {:name    (.getDatabaseProductName metadata)
-                                                     :version (.getDatabaseProductVersion metadata)}
-                                       :jdbc-driver {:name    (.getDriverName metadata)
-                                                     :version (.getDriverVersion metadata)}}))
-    :run-mode                     (config/config-kw :mb-run-mode)
+   {:databases  (->> (t2/select :model/Database) (map :engine) distinct)
+    :run-mode   (config/config-kw :mb-run-mode)
     :plan-alias (or (some-> (premium-features/premium-embedding-token) premium-features/fetch-token-status :plan-alias) "")
-    :version                      config/mb-version-info
-    :settings                     {:report-timezone (driver/report-timezone)}}
+    :version    config/mb-version-info
+    :settings   {:report-timezone (driver/report-timezone)}
+    :hosting-env                  (stats/environment-type)
+    :application-database         (mdb/db-type)}
+   (when-not (premium-features/is-hosted?)
+     {:application-database-details (t2/with-connection [^java.sql.Connection conn]
+                                      (let [metadata (.getMetaData conn)]
+                                        {:database    {:name    (.getDatabaseProductName metadata)
+                                                       :version (.getDatabaseProductVersion metadata)}
+                                         :jdbc-driver {:name    (.getDriverName metadata)
+                                                       :version (.getDriverVersion metadata)}}))})
    (when (premium-features/is-airgapped?)
-     {:airgap-token :enabled
-      :max-users (premium-features/max-users-allowed)
+     {:airgap-token       :enabled
+      :max-users          (premium-features/max-users-allowed)
       :current-user-count (premium-features/cached-active-users-count)
-      :valid-thru (some-> (premium-features/premium-embedding-token) premium-features/fetch-token-status :valid-thru)})))
+      :valid-thru         (some-> (premium-features/premium-embedding-token) premium-features/fetch-token-status :valid-thru)})))
