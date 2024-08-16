a7977ad11ffc215d9bbfbb76a4121beb0808b221
Oisin Coveney <oisin@metabase.com>
2024-08-16 13:22:59 +0300

    Add comment for useSdkElementSize

--- enterprise/frontend/src/embedding-sdk/hooks/private/use-sdk-element-size.ts
+++ enterprise/frontend/src/embedding-sdk/hooks/private/use-sdk-element-size.ts
@@ -3,5 +3,9 @@ import { useElementSize } from "@mantine/hooks";
 export const useSdkElementSize = () => {
   const { height, ref, width } = useElementSize();
 
-  return { height: height || (width ? width / 9 : 500), ref, width };
+  // Some of our components by default don't have a specified height (i.e. the Visualization component)
+  // and are rendering with a height of 0. This is a workaround to set a default height for those components.
+  const elementHeight = height || (width ? width / 9 : 500);
+
+  return { height: elementHeight, ref, width };
 };
