f99a7e9ec3b98046802f2f643a49218f1a7091ec
Sloan Sparger <sloansparger@users.noreply.github.com>
2024-08-19 11:21:02 -0700

    [Permissions] Prevent "Granular" option in DB View Data options from changing permissions to unrestricted (#46976)
    * fix
    
    * adds back most of the code and limits it to only happen with impersonations, updates test to handle differing logic between the two flows
    
    * removes test that is not longer needed

--- enterprise/frontend/src/metabase-enterprise/advanced_permissions/index.js
+++ enterprise/frontend/src/metabase-enterprise/advanced_permissions/index.js
@@ -89,16 +89,12 @@ if (hasPremiumFeature("advanced_permissions")) {
     value === BLOCK_PERMISSION_OPTION.value;
 
   PLUGIN_ADVANCED_PERMISSIONS.getDatabaseLimitedAccessPermission = value => {
-    if (
-      value === BLOCK_PERMISSION_OPTION.value ||
-      value === IMPERSONATED_PERMISSION_OPTION.value
-    ) {
+    if (value === IMPERSONATED_PERMISSION_OPTION.value) {
       return DataPermissionValue.UNRESTRICTED;
     }
 
     return null;
   };
-
   PLUGIN_ADVANCED_PERMISSIONS.isAccessPermissionDisabled = (value, subject) => {
     if (subject === "tables" || subject === "fields") {
       return value === DataPermissionValue.IMPERSONATED;
