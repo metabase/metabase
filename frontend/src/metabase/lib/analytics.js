bb696759f5a46e2d574eba8b8c6fe54f37b78062
Thomas Schmidt <thomas.heinz.schmidt@gmail.com>
2024-08-19 13:41:55 +0200

    Add structured action event

--- frontend/src/metabase/lib/analytics.js
+++ frontend/src/metabase/lib/analytics.js
@@ -37,7 +37,21 @@ export const trackStructEvent = (category, action, label, value) => {
   }
 };
 
-export const trackSchemaEvent = (schema, version, data) => {
+/**
+ * @deprecated This should not be used for new events. We introduced a generic `trackActionEvent` instead. There is also `trackSchemaEvent` but it should only be used as a last resort.
+ */
+export const trackLegacySchemaEvent = (schema, version, data) => {
+  trackSchemaEvent(schema, version, data);
+};
+
+export const trackSchemaEvent = (schema, version, data, contextEntities) => {
+  /*
+  IMPORTANT NOTE:
+  Only use this function directly if `trackActionEvent` doesn't fit your needs.
+  We want to use the generic structured action event as much as possible to avoid to create or update schemas.
+  If you need to track event specific context that does not fit the structured action event, you can pass context entities
+  that are designed in a reusable way (https://docs.snowplow.io/docs/understanding-your-pipeline/entities/)
+  */
   if (shouldLogAnalytics) {
     const { event, ...other } = data;
     // eslint-disable-next-line no-console
@@ -54,8 +68,37 @@ export const trackSchemaEvent = (schema, version, data) => {
   }
 
   if (Settings.snowplowEnabled()) {
-    trackSnowplowSchemaEvent(schema, version, data);
+    trackSnowplowSchemaEvent(schema, version, data, contextEntities);
   }
+};
+
+/**
+ * @param {string} name - Name of the action. Noun (target) + Verb in the past (action) which define the action taken - e.g. question-created, dashboard-updated, dashboard-auto-apply-filter-enabled
+ * @param {string} triggeredFrom - From where the action was taken. This can be generic like 'dashboard' or also more specific like 'dashboard_top_nav'.
+ * @param {string} targetId - (Optional) ID of the entity that the action was performed on. E.g. the ID of the question that was created in a question_created event.
+ * @param {number} durationMs - (Optional) Duration in milliseconds
+ * @param {string} result - (Optional) The outcome of the action (e.g. success, failure, …)
+ * @param {string} eventDetail - (Optional) Can be used for additional details that describe the event, e.g. the type of question that was created in a question_created event.
+ * @param {object} contextEntities - (Optional) Additional context entities (https://docs.snowplow.io/docs/understanding-your-pipeline/entities/). We send the Metabase Instance context by default with each event.
+ */
+export const trackActionEvent = (
+  name,
+  triggeredFrom,
+  targetId,
+  durationMs,
+  result,
+  eventDetail,
+  contextEntities,
+) => {
+  const event = {
+    name,
+    triggered_from: triggeredFrom,
+    target_id: targetId,
+    duration_ms: durationMs,
+    result,
+    event_detail: eventDetail,
+  };
+  trackSchemaEvent("structured_action", "1-0-0", event, contextEntities);
 };
 
 const createSnowplowTracker = store => {
@@ -106,12 +149,13 @@ const trackSnowplowPageView = url => {
   Snowplow.trackPageView();
 };
 
-const trackSnowplowSchemaEvent = (schema, version, data) => {
+const trackSnowplowSchemaEvent = (schema, version, data, contextEntities) => {
   Snowplow.trackSelfDescribingEvent({
     event: {
       schema: `iglu:com.metabase/${schema}/jsonschema/${version}`,
       data,
     },
+    context: contextEntities, // Optional - allows to add additional context entities
   });
 };
 
