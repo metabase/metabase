58b2f0cf37331612858420fd1a34a80ed6d0e89d
Nicolò Pretto <info@npretto.com>
2024-08-20 14:25:23 +0200

    split the jest tests files to simulate pro cloud, pro selfhosted and starter cloud scenarios

--- frontend/src/metabase/setup/tests/premium-pro-selfhosted.unit.spec.tsx
+++ frontend/src/metabase/setup/tests/premium-pro-selfhosted.unit.spec.tsx
@@ -1,6 +1,7 @@
 /* eslint jest/expect-expect: ["error", { "assertFunctionNames": ["expect", "expectSectionToHaveLabel", "expectSectionsToHaveLabelsInOrder"] }] */
 
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { createMockTokenFeatures } from "metabase-types/api/mocks";
 
@@ -21,29 +22,29 @@ const setupPremium = (opts?: SetupOpts) => {
   return setup({
     ...opts,
     hasEnterprisePlugins: true,
-    tokenFeatures: createMockTokenFeatures({ hosting: true, embedding: true }),
+    tokenFeatures: createMockTokenFeatures({ embedding: true }),
   });
 };
 
-describe("setup (EE, hosting and embedding feature)", () => {
-  it("default step order should be correct, without the commercial step", async () => {
+describe("setup (EE build, `embedding` feature but no `hosting` to simulate pro self-hosted)", () => {
+  it("default step order should be correct, without the commercial step but with the data usage step", async () => {
     await setupPremium();
     await skipWelcomeScreen();
     expectSectionToHaveLabel("What's your preferred language?", "1");
     expectSectionToHaveLabel("What should we call you?", "2");
     expectSectionToHaveLabel("What will you use Metabase for?", "3");
     expectSectionToHaveLabel("Add your data", "4");
+    // no "Activate your commercial license" as this has token-features
+    expectSectionToHaveLabel("Usage data preferences", "5");
 
     expectSectionsToHaveLabelsInOrder();
   });
 
-  it("should not show the analytics opt out (because of token-feature 'hosting')", async () => {
+  it("should show the analytics opt out", async () => {
     await setupPremium();
     await skipWelcomeScreen();
 
-    expect(
-      screen.queryByText("Usage data preferences"),
-    ).not.toBeInTheDocument();
+    expect(screen.getByText("Usage data preferences")).toBeInTheDocument();
   });
 
   it("should not render the license activation step", async () => {
@@ -54,7 +55,7 @@ describe("setup (EE, hosting and embedding feature)", () => {
     ).not.toBeInTheDocument();
   });
 
-  it("should set 'setup-license-active-at-setup' to true even when skipping the data usage step", async () => {
+  it("should set 'setup-license-active-at-setup' to true", async () => {
     await setupPremium();
     await skipWelcomeScreen();
     await skipLanguageStep();
@@ -62,6 +63,8 @@ describe("setup (EE, hosting and embedding feature)", () => {
 
     await selectUsageReason("embedding");
     await clickNextStep();
+
+    await userEvent.click(screen.getByText("Finish"));
 
     expect(await screen.findByText("Take me to Metabase")).toBeInTheDocument();
 
