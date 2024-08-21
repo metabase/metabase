2cec7777bdeaa5964ae782a273f9c30da2a52843
Ngoc Khuat <qn.khuat@gmail.com>
2024-08-21 14:32:10 +0700

    [notification] Fix test pulse endpoint does not work properly for http channels (#46474) (#47050)

--- test/metabase/channel/http_test.clj
+++ test/metabase/channel/http_test.clj
@@ -48,8 +48,7 @@
   (fn [req]
     (-> req
         (m/update-existing :body #(-> % slurp (json/parse-string true)))
-        handler
-        (m/update-existing :body json/generate-string))))
+        handler)))
 
 (def middlewares [json-mw wrap-params])
 
@@ -64,7 +63,10 @@
       (finally
         (.stop server)))))
 
-(defn- make-route
+(defn make-route
+  "Create a route to be used with [[with-server]].
+
+  (make-route :get \"/test\" (fn [req] {:status 200 :body \"Hello, world!\"}))"
   [method path handler]
   {:path  path
    :route (compojure/make-route method path handler)})
@@ -140,16 +142,16 @@
         (is (true? (can-connect?* get-302-redirect-200))))
       (testing "failed to connect with a 302 that redirects to 400"
         (is (= {:request-status 400
-                :request-body   "\"Bad request\""}
+                :request-body   "Bad request"}
                (exception-data (can-connect?* get-302-redirect-400)))))
       (testing "failed to conenct to a 400"
         (is (= {:request-status 400
-                :request-body   "\"Bad request\""}
+                :request-body   "Bad request"}
                (exception-data (can-connect?* get-400)))))
       (is (=? {:request-status 500
                ;; not sure why it's returns a response map is nil body
                ;; looks like a jetty bug: https://stackoverflow.com/q/46299061
-               #_:request-body   #_"\"Internal server error\""}
+               #_:request-body   #_"Internal server error"}
              (exception-data (can-connect?* get-500)))))))
 
 (deftest ^:parallel can-connect-header-auth-test
@@ -168,7 +170,7 @@
 
     (testing "fail to connect with header auth"
       (is (= {:request-status 401
-              :request-body   "\"Unauthorized\""}
+              :request-body   "Unauthorized"}
              (exception-data (can-connect? {:url         (str url "/user")
                                             :method      "get"
                                             :auth-method "header"
@@ -191,7 +193,7 @@
                                               :password "secretpassword"}}))))
     (testing "fail to connect with query-param auth"
       (is (= {:request-status 401
-              :request-body   "\"Unauthorized\""}
+              :request-body   "Unauthorized"}
              (exception-data (can-connect? {:url         (str url "/user")
                                             :method      "get"
                                             :auth-method "query-param"
@@ -214,7 +216,7 @@
                                 :auth-info   {:token "SECRET_TOKEN"}}))))
     (testing "fail to connect with request-body auth"
       (is (= {:request-status 401
-              :request-body   "\"Unauthorized\""}
+              :request-body   "Unauthorized"}
              (exception-data (can-connect? {:url         (str url "/user")
                                             :method      "post"
                                             :auth-method "request-body"
@@ -236,7 +238,7 @@
              (exception-data (can-connect? {:xyz "hello world"})))))
 
     (with-server [url [get-400]]
-      (is (= {:request-body   "\"Bad request\""
+      (is (= {:request-body   "Bad request"
               :request-status 400}
              (exception-data (can-connect? {:url         (str url (:path get-400))
                                             :method      "get"
