6d9751b75d5a7245b67ffb22af585cd1047f471d
Bryan Maass <bryan.maass@gmail.com>
2024-08-21 16:30:55 -0600

    add api level test

--- test/metabase/api/util_test.clj
+++ test/metabase/api/util_test.clj
@@ -80,3 +80,9 @@
                                :email    "happy_user@test.com"
                                :source   "Analytics Inc"})
         (is (true? (deref sent? 2000 ::timedout)))))))
+
+(deftest entity-id-translation-test
+  (mt/with-temp [:model/Card {card-id :id card-eid :entity_id} {}]
+    (is (= {card-eid card-id}
+           (-> (mt/user-http-request :crowberto :post 200 "util/entity_id" {:entity_ids {"report_card" [card-eid]}})
+               (update-keys name))))))
