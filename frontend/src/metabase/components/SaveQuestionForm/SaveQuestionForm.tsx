0a6d0a1928722839067d0ba8ca71a966b9c3ea37
Oisin Coveney <oisin@metabase.com>
2024-08-21 18:15:53 +0300

    feat(sdk): Edit Question (#46894)

--- frontend/src/metabase/components/SaveQuestionForm/SaveQuestionForm.tsx
+++ frontend/src/metabase/components/SaveQuestionForm/SaveQuestionForm.tsx
@@ -1,4 +1,3 @@
-import { TransitionGroup } from "react-transition-group";
 import { t } from "ttag";
 
 import FormCollectionPicker from "metabase/collections/containers/FormCollectionPicker";
@@ -39,27 +38,25 @@ export const SaveQuestionForm = ({ onCancel }: SaveQuestionFormProps) => {
           vertical
         />
       )}
-      <TransitionGroup>
-        {values.saveType === "create" && (
-          <div className={CS.overflowHidden}>
-            <FormInput
-              name="name"
-              title={t`Name`}
-              placeholder={nameInputPlaceholder}
-            />
-            <FormTextArea
-              name="description"
-              title={t`Description`}
-              placeholder={t`It's optional but oh, so helpful`}
-            />
-            <FormCollectionPicker
-              name="collection_id"
-              title={t`Which collection should this go in?`}
-              zIndex={DEFAULT_MODAL_Z_INDEX + 1}
-            />
-          </div>
-        )}
-      </TransitionGroup>
+      {values.saveType === "create" && (
+        <div className={CS.overflowHidden}>
+          <FormInput
+            name="name"
+            title={t`Name`}
+            placeholder={nameInputPlaceholder}
+          />
+          <FormTextArea
+            name="description"
+            title={t`Description`}
+            placeholder={t`It's optional but oh, so helpful`}
+          />
+          <FormCollectionPicker
+            name="collection_id"
+            title={t`Which collection should this go in?`}
+            zIndex={DEFAULT_MODAL_Z_INDEX + 1}
+          />
+        </div>
+      )}
       <FormFooter>
         <FormErrorMessage inline />
         <Button type="button" onClick={onCancel}>{t`Cancel`}</Button>
