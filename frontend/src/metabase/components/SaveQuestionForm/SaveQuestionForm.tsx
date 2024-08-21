4defe8314637fcfd8ca332e06cd50d0a67806eaa
Oisin Coveney <oisin@metabase.com>
2024-08-21 12:37:24 +0300

    Convert to mantine components

--- frontend/src/metabase/components/SaveQuestionForm/SaveQuestionForm.tsx
+++ frontend/src/metabase/components/SaveQuestionForm/SaveQuestionForm.tsx
@@ -1,4 +1,3 @@
-import { TransitionGroup } from "react-transition-group";
 import { t } from "ttag";
 
 import FormCollectionPicker from "metabase/collections/containers/FormCollectionPicker";
@@ -6,13 +5,15 @@ import { getPlaceholder } from "metabase/components/SaveQuestionForm/util";
 import Button from "metabase/core/components/Button";
 import FormErrorMessage from "metabase/core/components/FormErrorMessage";
 import FormFooter from "metabase/core/components/FormFooter";
-import FormInput from "metabase/core/components/FormInput";
-import FormRadio from "metabase/core/components/FormRadio";
 import FormSubmitButton from "metabase/core/components/FormSubmitButton";
-import FormTextArea from "metabase/core/components/FormTextArea";
 import CS from "metabase/css/core/index.css";
-import { Form } from "metabase/forms";
-import { DEFAULT_MODAL_Z_INDEX } from "metabase/ui";
+import {
+  Form,
+  FormRadioGroup,
+  FormTextInput,
+  FormTextarea,
+} from "metabase/forms";
+import { DEFAULT_MODAL_Z_INDEX, Radio, Stack } from "metabase/ui";
 
 import { useSaveQuestionContext } from "./context";
 import type { SaveQuestionFormProps } from "./types";
@@ -26,40 +27,40 @@ export const SaveQuestionForm = ({ onCancel }: SaveQuestionFormProps) => {
   return (
     <Form>
       {showSaveType && (
-        <FormRadio
+        <FormRadioGroup
           name="saveType"
-          title={t`Replace or save as new?`}
-          options={[
-            {
-              name: t`Replace original question, "${originalQuestion?.displayName()}"`,
-              value: "overwrite",
-            },
-            { name: t`Save as new question`, value: "create" },
-          ]}
-          vertical
-        />
+          label={t`Replace or save as new?`}
+          mb="md"
+        >
+          <Radio
+            value={"overwrite"}
+            label={t`Replace original question, "${originalQuestion?.displayName()}"`}
+          />
+          <Radio value={"create"} label={t`Save as new question`} />
+        </FormRadioGroup>
       )}
-      <TransitionGroup>
-        {values.saveType === "create" && (
-          <div className={CS.overflowHidden}>
-            <FormInput
-              name="name"
-              title={t`Name`}
-              placeholder={nameInputPlaceholder}
-            />
-            <FormTextArea
-              name="description"
-              title={t`Description`}
-              placeholder={t`It's optional but oh, so helpful`}
-            />
-            <FormCollectionPicker
-              name="collection_id"
-              title={t`Which collection should this go in?`}
-              zIndex={DEFAULT_MODAL_Z_INDEX + 1}
-            />
-          </div>
-        )}
-      </TransitionGroup>
+
+      {values.saveType === "create" && (
+        <Stack className={CS.overflowHidden}>
+          <FormTextInput
+            name="name"
+            label={t`Name`}
+            placeholder={nameInputPlaceholder}
+          />
+
+          <FormTextarea
+            name="description"
+            label={t`Description`}
+            placeholder={t`It's optional but oh, so helpful`}
+          />
+
+          <FormCollectionPicker
+            name="collection_id"
+            title={t`Which collection should this go in?`}
+            zIndex={DEFAULT_MODAL_Z_INDEX + 1}
+          />
+        </Stack>
+      )}
       <FormFooter>
         <FormErrorMessage inline />
         <Button type="button" onClick={onCancel}>{t`Cancel`}</Button>
