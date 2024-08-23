35681422c48b5a9f168127f45bb8d1d101567080
lbrdnk <lbrdnk@users.noreply.github.com>
2024-08-23 16:03:41 +0200

    cljfmt

--- modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
+++ modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
@@ -858,43 +858,43 @@
 ;; prior to dataset generation.
 (deftest ^:synchronized correct-timestamp-type-querying-test
   (mt/test-driver
-   :snowflake
-   (let [original-set-current-user-timezone! @#'test.data.snowflake/set-user-timezone!
-         original-dbdef->connection-details (get-method tx/dbdef->connection-details :snowflake)]
-     (with-redefs [test.data.snowflake/set-user-timezone!
-                   (fn [user _timezone]
-                     (original-set-current-user-timezone! user "America/Belize"))
-                   tx/dbdef->connection-details
-                   (fn [driver connection-type database-definition]
-                     (-> (original-dbdef->connection-details driver connection-type database-definition)
-                         (assoc :user "BELIZE_PERSON")))]
-       (mt/dataset
-         good-datetimes-in-belize
-         (testing "Expected data is returned using yesterday filter"
-           (let [belize-offset       (t/zone-offset "-06:00")
-                 yesterday-first     (t/- (t/truncate-to (t/offset-date-time belize-offset) :days) (t/days 1))
-                 yesterday-last      (t/+ yesterday-first (t/hours 18))
-                 yesterday-first-str (t/format :iso-offset-date-time yesterday-first)
-                 yesterday-last-str  (t/format :iso-offset-date-time yesterday-last)]
-             (doseq [[tested-field-kw base-type database-type]
-                     [[:IN_Z_OFFSET        :type/DateTimeWithLocalTZ "timestamptz"]
-                      [:IN_VARIOUS_OFFSETS :type/DateTimeWithLocalTZ "timestamptz"]
-                      [:JUST_NTZ           :type/DateTime            "timestampntz"]
-                      [:JUST_LTZ           :type/DateTimeWithTZ      "timestampltz"]]
-                     :let [tested-field [:field (mt/id :GOOD_DATETIMES tested-field-kw) {:base-type base-type}]]]
-               (testing (str "on column type " database-type)
-                 (let [rows (mt/rows (qp/process-query
-                                      {:database (mt/id)
-                                       :type     :query
-                                       :query {:source-table (mt/id :GOOD_DATETIMES)
-                                               :fields [tested-field]
-                                               :filter [:time-interval tested-field -1 :day]
-                                               :order-by [[tested-field :asc]]}}))]
-                   (testing "Correct rows count returned"
-                     (is (= 4 (count rows))))
-                   (testing "First row has expected values"
-                     (is (= yesterday-first-str
-                            (ffirst rows))))
-                   (testing "Last row has expected values"
-                     (is (= yesterday-last-str
-                            (ffirst (reverse rows)))))))))))))))
+    :snowflake
+    (let [original-set-current-user-timezone! @#'test.data.snowflake/set-user-timezone!
+          original-dbdef->connection-details (get-method tx/dbdef->connection-details :snowflake)]
+      (with-redefs [test.data.snowflake/set-user-timezone!
+                    (fn [user _timezone]
+                      (original-set-current-user-timezone! user "America/Belize"))
+                    tx/dbdef->connection-details
+                    (fn [driver connection-type database-definition]
+                      (-> (original-dbdef->connection-details driver connection-type database-definition)
+                          (assoc :user "BELIZE_PERSON")))]
+        (mt/dataset
+          good-datetimes-in-belize
+          (testing "Expected data is returned using yesterday filter"
+            (let [belize-offset       (t/zone-offset "-06:00")
+                  yesterday-first     (t/- (t/truncate-to (t/offset-date-time belize-offset) :days) (t/days 1))
+                  yesterday-last      (t/+ yesterday-first (t/hours 18))
+                  yesterday-first-str (t/format :iso-offset-date-time yesterday-first)
+                  yesterday-last-str  (t/format :iso-offset-date-time yesterday-last)]
+              (doseq [[tested-field-kw base-type database-type]
+                      [[:IN_Z_OFFSET        :type/DateTimeWithLocalTZ "timestamptz"]
+                       [:IN_VARIOUS_OFFSETS :type/DateTimeWithLocalTZ "timestamptz"]
+                       [:JUST_NTZ           :type/DateTime            "timestampntz"]
+                       [:JUST_LTZ           :type/DateTimeWithTZ      "timestampltz"]]
+                      :let [tested-field [:field (mt/id :GOOD_DATETIMES tested-field-kw) {:base-type base-type}]]]
+                (testing (str "on column type " database-type)
+                  (let [rows (mt/rows (qp/process-query
+                                       {:database (mt/id)
+                                        :type     :query
+                                        :query {:source-table (mt/id :GOOD_DATETIMES)
+                                                :fields [tested-field]
+                                                :filter [:time-interval tested-field -1 :day]
+                                                :order-by [[tested-field :asc]]}}))]
+                    (testing "Correct rows count returned"
+                      (is (= 4 (count rows))))
+                    (testing "First row has expected values"
+                      (is (= yesterday-first-str
+                             (ffirst rows))))
+                    (testing "Last row has expected values"
+                      (is (= yesterday-last-str
+                             (ffirst (reverse rows)))))))))))))))
