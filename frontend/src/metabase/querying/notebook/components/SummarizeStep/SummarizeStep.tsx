ae37db06c9785b0ed7b6ce6691ba3cea513a28b0
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-08-23 21:56:28 -0400

    Remove reverting for metrics

--- frontend/src/metabase/querying/notebook/components/SummarizeStep/SummarizeStep.tsx
+++ frontend/src/metabase/querying/notebook/components/SummarizeStep/SummarizeStep.tsx
@@ -7,11 +7,13 @@ import { AggregateStep } from "../AggregateStep";
 import { BreakoutStep } from "../BreakoutStep";
 
 export function SummarizeStep({
+  step,
   color,
   isLastOpened,
-  step,
   ...props
 }: NotebookStepUiComponentProps) {
+  const isMetric = step.question.type() === "metric";
+
   return (
     <Flex align="center" direction={{ base: "column", md: "row" }} gap="sm">
       <Box w={{ base: "100%", md: "50%" }}>
@@ -22,8 +24,21 @@ export function SummarizeStep({
           {...props}
         />
       </Box>
-      <Box c={color} fw="bold">{t`by`}</Box>
+      {!isMetric && <Box c={color} fw="bold">{t`by`}</Box>}
       <Box w={{ base: "100%", md: "50%" }}>
+        {isMetric && (
+          <Box pos="relative">
+            <Box
+              pos={{ md: "absolute" }}
+              bottom={0}
+              mb="sm"
+              c="summarize"
+              fw="bold"
+            >
+              {t`Default time dimension`}
+            </Box>
+          </Box>
+        )}
         <BreakoutStep
           step={step}
           color={color}
