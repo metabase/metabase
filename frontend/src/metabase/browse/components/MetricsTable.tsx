b68f37dcd88ae87ff37e58e37b1cd3de590df21a
Romeo Van Snick <romeo@romeovansnick.be>
2024-08-26 16:20:52 +0200

    Simplify MetricsTable component

--- frontend/src/metabase/browse/components/MetricsTable.tsx
+++ frontend/src/metabase/browse/components/MetricsTable.tsx
@@ -1,4 +1,5 @@
-import { type CSSProperties, type PropsWithChildren, useState } from "react";
+import type React from "react";
+import { useCallback, useState } from "react";
 import { push } from "react-router-redux";
 import { t } from "ttag";
 
@@ -18,15 +19,7 @@ import type { ResponsiveProps } from "metabase/components/ItemsTable/utils";
 import Link from "metabase/core/components/Link";
 import { useDispatch } from "metabase/lib/redux";
 import * as Urls from "metabase/lib/urls";
-import {
-  Box,
-  FixedSizeIcon,
-  Flex,
-  Icon,
-  type IconName,
-  type IconProps,
-  Skeleton,
-} from "metabase/ui";
+import { Box, FixedSizeIcon, Flex, Icon, Skeleton } from "metabase/ui";
 import { Repeat } from "metabase/ui/components/feedback/Skeleton/Repeat";
 import { SortDirection, type SortingOptions } from "metabase-types/api/sorting";
 
@@ -72,7 +65,7 @@ export function MetricsTable({
   const handleSortingOptionsChange = skeleton ? undefined : setSortingOptions;
 
   return (
-    <Table aria-label={skeleton ? undefined : t`Table of models`}>
+    <Table aria-label={skeleton ? undefined : t`Table of metrics`}>
       <thead>
         <tr>
           <SortableColumnHeader
@@ -113,11 +106,11 @@ export function MetricsTable({
       <TBody>
         {skeleton ? (
           <Repeat times={7}>
-            <TBodyRowSkeleton />
+            <MetricRow />
           </Repeat>
         ) : (
           sortedMetrics.map((metric: MetricResult) => (
-            <TBodyRow metric={metric} key={`${metric.model}-${metric.id}`} />
+            <MetricRow metric={metric} key={`${metric.model}-${metric.id}`} />
           ))
         )}
       </TBody>
@@ -125,145 +118,72 @@ export function MetricsTable({
   );
 }
 
-function TBodyRow({
-  metric,
-  skeleton,
-}: {
-  metric: MetricResult;
-  skeleton?: boolean;
-}) {
-  const icon = getIcon(metric);
+function MetricRow({ metric }: { metric?: MetricResult }) {
   const dispatch = useDispatch();
-  const { id, name } = metric;
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent) => {
+      if (!metric) {
+        return;
+      }
+
+      // do not trigger click when selecting text
+      const selection = document.getSelection();
+      if (selection?.type === "Range") {
+        event.stopPropagation();
+        return;
+      }
+
+      const { id, name } = metric;
+      const url = Urls.metric({ id, name });
+      const subpathSafeUrl = Urls.getSubpathSafeUrl(url);
+
+      // TODO:
+      // trackMetricClick(metric.id);
+
+      if ((event.ctrlKey || event.metaKey) && event.button === 0) {
+        Urls.openInNewTab(subpathSafeUrl);
+      } else {
+        dispatch(push(url));
+      }
+    },
+    [metric, dispatch],
+  );
 
   return (
-    <tr
-      onClick={(e: React.MouseEvent) => {
-        if (skeleton) {
-          return;
-        }
-        const url = Urls.metric({ id, name });
-        const subpathSafeUrl = Urls.getSubpathSafeUrl(url);
-
-        if ((e.ctrlKey || e.metaKey) && e.button === 0) {
-          Urls.openInNewTab(subpathSafeUrl);
-        } else {
-          dispatch(push(url));
-        }
-      }}
-      tabIndex={0}
-      key={metric.id}
-    >
-      {/* Name */}
-      <NameCell
-        metric={metric}
-        icon={icon}
-        onClick={() => {
-          if (skeleton) {
-            return;
-          }
-          // TODO
-          // trackModelClick(model.id);
-        }}
-      />
-
-      {/* Collection */}
-      <td
-        data-testid={`path-for-collection: ${
-          metric.collection
-            ? getCollectionName(metric.collection)
-            : t`Untitled collection`
-        }`}
-        {...collectionProps}
-      >
-        <Link
-          // className={S.collectionLink}
-          to={Urls.collection(metric.collection)}
-          onClick={e => e.stopPropagation()}
-        >
-          <Flex gap="sm">
-            <FixedSizeIcon name="folder" />
-            <Box w="calc(100% - 1.5rem)">
-              <EllipsifiedCollectionPath collection={metric.collection} />
-            </Box>
-          </Flex>
-        </Link>
-      </td>
-
-      {/* Description */}
-      <td {...descriptionProps}>
-        <EllipsifiedWithMarkdownTooltip>
-          {getMetricDescription(metric) || ""}
-        </EllipsifiedWithMarkdownTooltip>
-      </td>
-
-      {/* Adds a border-radius to the table */}
+    <tr onClick={handleClick} tabIndex={0}>
+      <NameCell metric={metric} />
+      <CollectionCell metric={metric} />
+      <DescriptionCell metric={metric} />
       <Columns.RightEdge.Cell />
     </tr>
   );
 }
 
-const TBodyRowSkeleton = ({ style }: { style?: CSSProperties }) => {
-  const icon = { name: "metric" as IconName };
-
-  return (
-    <tr style={style}>
-      {/* Name */}
-      <NameCell icon={icon}>
-        <CellTextSkeleton />
-      </NameCell>
-
-      {/* Collection */}
-      <td {...collectionProps}>
-        <Flex gap=".5rem">
-          <FixedSizeIcon name="folder" />
-          <CellTextSkeleton />
-        </Flex>
-      </td>
-
-      {/* Description */}
-      <td {...descriptionProps}>
-        <CellTextSkeleton />
-      </td>
-
-      {/* Adds a border-radius to the table */}
-      <Columns.RightEdge.Cell />
-    </tr>
-  );
-};
-
-const CellTextSkeleton = () => {
+function SkeletonText() {
   return <Skeleton natural h="16.8px" />;
-};
+}
 
-function NameCell({
-  metric,
-  testIdPrefix = "table",
-  onClick,
-  icon,
-  children,
-}: PropsWithChildren<{
-  metric?: MetricResult;
-  testIdPrefix?: string;
-  onClick?: () => void;
-  icon: IconProps;
-}>) {
-  const headingId = `model-${metric?.id || "dummy"}-heading`;
+function stopPropagation(event: React.MouseEvent) {
+  event.stopPropagation();
+}
+
+function NameCell({ metric }: { metric?: MetricResult }) {
+  const headingId = `metric-${metric?.id ?? "dummy"}-heading`;
+  const icon = getIcon(metric);
+
   return (
-    <ItemNameCell
-      data-testid={`${testIdPrefix}-name`}
-      aria-labelledby={headingId}
-    >
+    <ItemNameCell data-testid="table-name" aria-labelledby={headingId}>
       <MaybeItemLink
         to={
           metric ? Urls.metric({ id: metric.id, name: metric.name }) : undefined
         }
-        onClick={onClick}
         style={{
           // To align the icons with "Name" in the <th>
           paddingInlineStart: "1.4rem",
           paddingInlineEnd: ".5rem",
         }}
+        onClick={stopPropagation}
       >
         <Icon
           size={16}
@@ -271,14 +191,64 @@ function NameCell({
           color={"var(--mb-color-brand)"}
           style={{ flexShrink: 0 }}
         />
-        {children || (
+        {metric ? (
           <EntityItem.Name
             name={metric?.name || ""}
             variant="list"
             id={headingId}
           />
+        ) : (
+          <SkeletonText />
         )}
       </MaybeItemLink>
     </ItemNameCell>
+  );
+}
+
+function CollectionCell({ metric }: { metric?: MetricResult }) {
+  const collectionName = metric?.collection
+    ? getCollectionName(metric.collection)
+    : t`Untitled collection`;
+
+  const content = (
+    <Flex gap="sm">
+      <FixedSizeIcon name="folder" />
+      <Box w="calc(100% - 1.5rem)">
+        {metric ? (
+          <EllipsifiedCollectionPath collection={metric.collection} />
+        ) : (
+          <SkeletonText />
+        )}
+      </Box>
+    </Flex>
+  );
+
+  return (
+    <td
+      data-testid={`path-for-collection: ${collectionName}`}
+      {...collectionProps}
+    >
+      {metric?.collection ? (
+        <Link to={Urls.collection(metric.collection)} onClick={stopPropagation}>
+          {content}
+        </Link>
+      ) : (
+        content
+      )}
+    </td>
+  );
+}
+
+function DescriptionCell({ metric }: { metric?: MetricResult }) {
+  return (
+    <td {...descriptionProps}>
+      {metric ? (
+        <EllipsifiedWithMarkdownTooltip>
+          {getMetricDescription(metric) || ""}
+        </EllipsifiedWithMarkdownTooltip>
+      ) : (
+        <SkeletonText />
+      )}
+    </td>
   );
 }
