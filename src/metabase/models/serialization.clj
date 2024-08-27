d4fb20ebe05ec7fca6add22ee29db763aa6cbc6f
Chris Truter <chris@metabase.com>
2024-08-27 09:07:17 +0200

    More logging around serialization error, to debug flake

--- src/metabase/models/serialization.clj
+++ src/metabase/models/serialization.clj
@@ -960,9 +960,13 @@
   The input might be nil, in which case so is the output. This is legal for a native question."
   [[db-name schema table-name :as table-id]]
   (when table-id
-    (or (t2/select-one-fn :id 'Table :name table-name :schema schema :db_id (t2/select-one-fn :id 'Database :name db-name))
-        (throw (ex-info (format "table id present, but no table found: %s" table-id)
-                        {:table-id table-id})))))
+    (if-let [db-id (t2/select-one-fn :id 'Database :name db-name)]
+      (or (t2/select-one-fn :id 'Table :name table-name :schema schema :db_id db-id)
+          (throw (ex-info (format "table id present, but no table found: %s" table-id)
+                          {:table-id table-id})))
+      (throw (ex-info (format "table id present, but database not found: %s" table-id)
+                      {:table-id table-id
+                       :database-names (sort (t2/select-fn-vec :name 'Table))})))))
 
 (defn table->path
   "Given a `table_id` as exported by [[export-table-fk]], turn it into a `[{:model ...}]` path for the Table.
