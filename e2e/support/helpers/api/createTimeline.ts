a050634464e1293c98606a40e9e1284f9027c570
Kamil Mielnik <kamil@kamilmielnik.com>
2024-08-27 14:46:41 +0700

    Make createTimeline argument optional

--- e2e/support/helpers/api/createTimeline.ts
+++ e2e/support/helpers/api/createTimeline.ts
@@ -6,7 +6,9 @@ export const createTimeline = ({
   default: isDefault = false,
   archived = false,
   ...params
-}: CreateTimelineRequest): Cypress.Chainable<Cypress.Response<Timeline>> => {
+}: Partial<CreateTimelineRequest> = {}): Cypress.Chainable<
+  Cypress.Response<Timeline>
+> => {
   return cy.request("POST", "/api/timeline", {
     ...params,
     name,
