c964053f80d37a7b76b3de4f6aeb008281e0e8c1
Kamil Mielnik <kamil@kamilmielnik.com>
2024-08-27 15:18:48 +0700

    Remove duplicated ensureFilterStage after merge

--- frontend/src/metabase-lib/filter.ts
+++ frontend/src/metabase-lib/filter.ts
@@ -3,8 +3,6 @@ import moment from "moment-timezone"; // eslint-disable-line no-restricted-impor
 import * as ML from "cljs/metabase.lib.js";
 import type { DatasetColumn, TemporalUnit } from "metabase-types/api";
 
-import { aggregations } from "./aggregation";
-import { breakouts } from "./breakout";
 import {
   isBoolean,
   isCoordinate,
@@ -29,7 +27,7 @@ import {
 import { expressionClause, expressionParts } from "./expression";
 import { isColumnMetadata } from "./internal";
 import { displayInfo } from "./metadata";
-import { appendStage, removeClause, stageCount } from "./query";
+import { removeClause, stageCount } from "./query";
 import {
   availableTemporalBuckets,
   temporalBucket,
@@ -100,27 +98,6 @@ export function removeFilters(query: Query, stageIndex: number): Query {
     (newQuery, filter) => removeClause(newQuery, stageIndex, filter),
     query,
   );
-}
-
-/**
- * In order to be able to filter on columns created by aggregations and breakouts
- * we need to append an extra stage to the query.
- *
- * Technically we should be checking for presence of "aggregations OR breakouts"
- * but we're actually checking for presence of "aggregations AND breakouts" for
- * unknown legacy reasons. Filtering a query with aggregations and no breakouts
- * does not make much sense but filtering a query with breakouts and no aggregations
- * could be potentially useful.
- *
- * This function should live in metabase/querying/utils/filters/utils.ts
- * but needs to be used in metabase-lib/v1 which would cause a forbidden import.
- * TODO: Move it there when metabase-lib/v1 is no more.
- */
-export function ensureFilterStage(query: Query): Query {
-  const isSummarized =
-    aggregations(query, -1).length > 0 && breakouts(query, -1).length > 0;
-
-  return isSummarized ? appendStage(query) : query;
 }
 
 /**
