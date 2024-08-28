2649c1c8154b99c75be6fd1401afe9eef59c0f73
Anton Kulyk <kuliks.anton@gmail.com>
2024-08-28 12:34:03 +0100

    Use `text-medium` for outer circle background

--- frontend/src/metabase/visualizations/components/legend/LegendItemDot.styled.tsx
+++ frontend/src/metabase/visualizations/components/legend/LegendItemDot.styled.tsx
@@ -7,7 +7,7 @@ export const OuterCircle = styled.div`
   width: 12px;
   height: 12px;
   border-radius: 50%;
-  background-color: var(--mb-color-border);
+  background-color: var(--mb-color-text-medium);
   transition: all 0.2s;
 `;
 
