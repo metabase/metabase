5fa6782d05b70d5718ca090e57f78e959e16481f
Romeo Van Snick <romeo@romeovansnick.be>
2024-08-28 15:03:23 +0200

    Simplify useHasMetrics

--- frontend/src/metabase/common/hooks/use-has-metrics.tsx
+++ frontend/src/metabase/common/hooks/use-has-metrics.tsx
@@ -2,17 +2,18 @@ import type { SearchRequest } from "metabase-types/api";
 
 import { useFetchMetrics } from "./use-fetch-metrics";
 
-/** NOTE: hasModels is undefined when the request is pending */
 export const useHasMetrics = (req: Partial<SearchRequest> = {}) => {
-  const metricsResult = useFetchMetrics({
-    limit: 1,
-    model_ancestors: false,
+  const { data, isLoading, error } = useFetchMetrics({
+    limit: 0,
     ...req,
   });
-  const metricsLength = metricsResult.data?.data.length;
+
+  const availableModels = data?.available_models ?? [];
+  const hasMetrics = availableModels.includes("metric");
+
   return {
-    hasMetrics: metricsLength === undefined ? undefined : metricsLength !== 0,
-    isLoading: metricsResult.isLoading,
-    error: metricsResult.error,
+    hasMetrics,
+    isLoading,
+    error,
   };
 };
