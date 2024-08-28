e335d67a11811fd92f98d4d945845e8b0534506b
Anton Kulyk <kuliks.anton@gmail.com>
2024-08-28 15:01:15 +0100

    Update dot style

--- frontend/src/metabase/visualizations/components/legend/LegendItemDot.styled.tsx
+++ frontend/src/metabase/visualizations/components/legend/LegendItemDot.styled.tsx
@@ -7,7 +7,7 @@ export const OuterCircle = styled.div`
   width: 12px;
   height: 12px;
   border-radius: 50%;
-  background-color: var(--mb-color-text-medium);
+  background-color: var(--mb-color-border);
   transition: all 0.2s;
 `;
 
@@ -22,7 +22,7 @@ export const InnerCircle = styled.div<{ isVisible: boolean }>`
   background-color: ${props =>
     props.isVisible ? props.color : "var(--mb-color-background)"};
   border: 2px solid
-    ${props => (props.isVisible ? props.color : "var(--mb-color-border)")};
+    ${props => (props.isVisible ? props.color : "var(--mb-color-text-medium)")};
   transition: all 0.2s;
 `;
 
@@ -37,7 +37,6 @@ export const Button = styled.button`
     `&:hover {
       ${InnerCircle} {
         transform: scale(0.8);
-        border-width: 0;
       }
       ${OuterCircle} {
         transform: scale(1.3);
