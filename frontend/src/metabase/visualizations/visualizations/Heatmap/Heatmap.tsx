c26a5f0d4816d5ddb1115ce7c3f817e111437a19
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-08-28 00:40:29 -0400

    render real data

--- frontend/src/metabase/visualizations/visualizations/Heatmap/Heatmap.tsx
+++ frontend/src/metabase/visualizations/visualizations/Heatmap/Heatmap.tsx
@@ -1,34 +1,74 @@
-import * as echarts from "echarts";
 import { t } from "ttag";
 import _ from "underscore";
 
+import { findWithIndex } from "metabase/lib/arrays";
 import { ResponsiveEChartsRenderer } from "metabase/visualizations/components/EChartsRenderer";
+import { columnSettings } from "metabase/visualizations/lib/settings/column";
+import {
+  dimensionSetting,
+  metricSetting,
+} from "metabase/visualizations/lib/settings/utils";
 import type { VisualizationProps } from "metabase/visualizations/types";
+import type { RowValue } from "metabase-types/api";
 
-export const Heatmap = (_props: VisualizationProps) => {
-  function getVirtualData(year: string) {
-    const date = +echarts.time.parse(year + "-01-01");
-    const end = +echarts.time.parse(+year + 1 + "-01-01");
-    const dayTime = 3600 * 24 * 1000;
-    const data = [];
-    for (let time = date; time < end; time += dayTime) {
-      data.push([
-        echarts.time.format(time, "{yyyy}-{MM}-{dd}", false),
-        Math.floor(Math.random() * 10000),
-      ]);
-    }
-    return data;
-  }
-  const option = {
-    title: {
-      top: 30,
-      left: "center",
-      text: "Daily Step Count",
+// Defines supported visualization settings
+const SETTINGS_DEFINITIONS = {
+  // Column formatting settings
+  ...columnSettings({ hidden: true }),
+  // Heatmap dimension column
+  ...dimensionSetting("heatmap.dimension", {
+    section: t`Data`,
+    title: t`Dimension`,
+    showColumnSetting: true,
+  }),
+  // Heatmap metric column
+  ...metricSetting("heatmap.metric", {
+    section: t`Data`,
+    title: t`Measure`,
+    showColumnSetting: true,
+  }),
+};
+
+export const Heatmap = ({ rawSeries, settings }: VisualizationProps) => {
+  // Assuming Heatmaps can use one card only, getting the dataset
+  const [{ data }] = rawSeries;
+
+  // Find dimension and metric columns
+  const dimension = findWithIndex(
+    data.cols,
+    col => col.name === settings["heatmap.dimension"],
+  );
+  const metric = findWithIndex(
+    data.cols,
+    col => col.name === settings["heatmap.metric"],
+  );
+
+  // Create a dataset in the format expected by ECharts
+  const echartsData = data.rows.map(row => {
+    // Get dimension and metric values from a row
+    const dimensionValue = row[dimension.index];
+    const metricValue = row[metric.index];
+
+    return [dimensionValue, metricValue];
+  });
+
+  // Finding latest year that exists in the dataset to select it in the calendar.
+  // Ideally, users should be able to select it manually.
+  const datasetLatestYear = echartsData.reduce(
+    (latestYear: RowValue, row: RowValue[]) => {
+      const currentDate = row[0];
+      const currentDateYear = new Date(String(currentDate)).getFullYear();
+      if (typeof latestYear !== "number") {
+        return currentDateYear;
+      }
+      return Math.max(latestYear, currentDateYear);
     },
-    tooltip: {},
+    null,
+  );
+
+  // Define ECharts option object that describes how to render the chart
+  const option = {
     visualMap: {
-      min: 0,
-      max: 10000,
       type: "piecewise",
       orient: "horizontal",
       left: "center",
@@ -36,19 +76,19 @@ export const Heatmap = (_props: VisualizationProps) => {
     },
     calendar: {
       top: 120,
-      left: 30,
-      right: 30,
-      cellSize: ["auto", 13],
-      range: "2016",
+      left: 80,
+      right: 80,
+      bottom: 120,
+      cellSize: ["auto", 20],
+      range: datasetLatestYear,
       itemStyle: {
         borderWidth: 0.5,
       },
-      yearLabel: { show: false },
     },
     series: {
       type: "heatmap",
       coordinateSystem: "calendar",
-      data: getVirtualData("2016"),
+      data: echartsData,
     },
   };
 
@@ -60,4 +100,5 @@ Object.assign(Heatmap, {
   identifier: "Heatmap",
   iconName: "grid",
   noun: t`Heatmap`,
+  settings: SETTINGS_DEFINITIONS,
 });
