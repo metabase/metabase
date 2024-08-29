8d2eb5b1a49c85cfed9852364436ebb8d66c62c6
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-08-28 23:04:17 -0400

    Add event validation

--- frontend/src/metabase-types/analytics/action.ts
+++ frontend/src/metabase-types/analytics/action.ts
@@ -1,31 +1,43 @@
-import type {
-  WritebackActionId,
-  WritebackActionType,
-} from "metabase-types/api";
+import type { ValidateSchema } from "./utils";
 
-export type ActionCreatedEvent = {
+type ActionEventSchema = {
+  event: string;
+  type: string;
+  action_id: number;
+  num_parameters?: number | null;
+  context?: string | null;
+};
+
+type ValidateEvent<T extends ActionEventSchema> = ValidateSchema<
+  T,
+  ActionEventSchema
+>;
+
+type ActionType = "http" | "query" | "implicit";
+
+export type ActionCreatedEvent = ValidateEvent<{
   event: "action_created";
-  type: WritebackActionType;
-  action_id: WritebackActionId;
-};
+  type: ActionType;
+  action_id: number;
+}>;
 
-export type ActionUpdatedEvent = {
+export type ActionUpdatedEvent = ValidateEvent<{
   event: "action_updated";
-  type: WritebackActionType;
-  action_id: WritebackActionId;
-};
+  type: ActionType;
+  action_id: number;
+}>;
 
-export type ActionDeletedEvent = {
+export type ActionDeletedEvent = ValidateEvent<{
   event: "action_deleted";
-  type: WritebackActionType;
-  action_id: WritebackActionId;
-};
+  type: ActionType;
+  action_id: number;
+}>;
 
-export type ActionExecutedEvent = {
+export type ActionExecutedEvent = ValidateEvent<{
   event: "action_executed";
-  type: WritebackActionType;
-  action_id: WritebackActionId;
-};
+  type: ActionType;
+  action_id: number;
+}>;
 
 export type ActionEvent =
   | ActionCreatedEvent
