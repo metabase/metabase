629f6a380287c192508b15b2861077922a13bf19
Sloan Sparger <sloansparger@gmail.com>
2024-08-28 23:43:38 -0500

    hacking linking to dashboard for question and books marks - does not handle navigating to the correct tab yet

--- frontend/src/metabase/lib/urls/bookmarks.ts
+++ frontend/src/metabase/lib/urls/bookmarks.ts
@@ -4,16 +4,27 @@ import type { Bookmark } from "metabase-types/api";
 
 import { appendSlug } from "./utils";
 
-function getBookmarkBasePath(bookmark: Bookmark) {
+function getBookmarkBasePath(bookmark: Pick<Bookmark, "type" | "card_type">) {
   if (bookmark.type === "card") {
     return bookmark.card_type;
   }
   return bookmark.type;
 }
 
-export function bookmark(bookmark: Bookmark) {
-  const [, itemId] = bookmark.id.split("-");
-  const basePath = getBookmarkBasePath(bookmark);
-  const itemPath = appendSlug(itemId, slugg(bookmark.name));
+export function bookmark(
+  bm: Pick<Bookmark, "id" | "type" | "card_type" | "name" | "dashboard_id">,
+) {
+  // HACK: to make bookmarks for dashboard questions to take you to their dashboard pages
+  if (bm.type === "card" && typeof bm.dashboard_id === "number") {
+    return bookmark({
+      id: `dashboard-${bm.dashboard_id}`,
+      type: "dashboard",
+      name: "",
+    });
+  }
+
+  const [, itemId] = bm.id.split("-");
+  const basePath = getBookmarkBasePath(bm);
+  const itemPath = appendSlug(itemId, slugg(bm.name));
   return `/${basePath}/${itemPath}`;
 }
