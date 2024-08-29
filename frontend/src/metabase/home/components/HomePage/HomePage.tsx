cf4a46390bd3ac8f0b669b0957e7dc3f9e1497f0
Ryan Laurie <iethree@gmail.com>
2024-08-29 13:21:35 -0600

    small styling updates

--- frontend/src/metabase/home/components/HomePage/HomePage.tsx
+++ frontend/src/metabase/home/components/HomePage/HomePage.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { replace } from "react-router-redux";
 import { t } from "ttag";
 
-import { TestTabbedSidesheet } from "metabase/common/components/Sidesheet/Sidesheet.samples";
 import {
   useDatabaseListQuery,
   useSearchListQuery,
@@ -43,7 +42,6 @@ export const HomePage = (): JSX.Element => {
 
   return (
     <HomeLayout hasMetabot={getHasMetabot(databases, models, isMetabotEnabled)}>
-      <TestTabbedSidesheet />
       <HomeContent />
     </HomeLayout>
   );
