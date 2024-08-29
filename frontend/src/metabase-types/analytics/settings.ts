41ff8f6fc70960b6f1a3c7409aa732f01cf98f34
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-08-28 22:12:32 -0400

    Add event validation

--- frontend/src/metabase-types/analytics/settings.ts
+++ frontend/src/metabase-types/analytics/settings.ts
@@ -1,17 +1,29 @@
-export type TrackingPermissionEnabledEvent = {
+import type { ValidateSchema } from "./utils";
+
+type SettingsEventSchema = {
+  event: string;
+  source?: string | null;
+};
+
+type ValidateEvent<T extends SettingsEventSchema> = ValidateSchema<
+  T,
+  SettingsEventSchema
+>;
+
+export type TrackingPermissionEnabledEvent = ValidateEvent<{
   event: "tracking_permission_enabled";
   source: "setup" | "admin";
-};
+}>;
 
-export type TrackingPermissionDisabledEvent = {
+export type TrackingPermissionDisabledEvent = ValidateEvent<{
   event: "tracking_permission_disabled";
   source: "setup" | "admin";
-};
+}>;
 
-export type HomepageDashboardEnabledEvent = {
+export type HomepageDashboardEnabledEvent = ValidateEvent<{
   event: "homepage_dashboard_enabled";
   source: "admin" | "homepage";
-};
+}>;
 
 export type SettingsEvent =
   | TrackingPermissionEnabledEvent
