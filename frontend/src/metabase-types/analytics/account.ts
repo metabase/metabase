0d4092598ae611c5a713c026141b0bbd925d90fc
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-08-28 23:05:42 -0400

    Add event validation

--- frontend/src/metabase-types/analytics/account.ts
+++ frontend/src/metabase-types/analytics/account.ts
@@ -1,9 +1,22 @@
-export type NewUserCreatedEvent = {
-  event: "new_user_created";
+import type { ValidateSchema } from "./utils";
+
+type AccountEventSchema = {
+  event: string;
+  version?: string | null;
+  link?: string | null;
 };
 
-export type NewInstanceCreatedEvent = {
+type ValidateEvent<T extends AccountEventSchema> = ValidateSchema<
+  T,
+  AccountEventSchema
+>;
+
+export type NewUserCreatedEvent = ValidateEvent<{
+  event: "new_user_created";
+}>;
+
+export type NewInstanceCreatedEvent = ValidateEvent<{
   event: "new_instance_created";
-};
+}>;
 
 export type AccountEvent = NewUserCreatedEvent | NewInstanceCreatedEvent;
