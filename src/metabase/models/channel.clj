46491dc402503650a85967974ed340aabcdfc2f6
Ngoc Khuat <qn.khuat@gmail.com>
2024-08-29 17:35:41 +0700

    [notification] channel serdes spec (#47386)

--- src/metabase/models/channel.clj
+++ src/metabase/models/channel.clj
@@ -25,6 +25,11 @@
   [_database]
   [:name :type])
 
+(defmethod serdes/make-spec "Channel"
+  [_model-name _opts]
+  {:copy      [:name :description :type :details :active]
+   :transform {:created_at (serdes/date)}})
+
 (doto :model/Channel
   (derive :metabase/model)
   (derive :hook/timestamped?))
