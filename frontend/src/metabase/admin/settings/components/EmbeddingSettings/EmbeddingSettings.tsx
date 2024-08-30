03fece72ec8160c2c299ce2cf034c3b17f383098
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-08-30 22:47:21 +0700

    Fix E2E tests

--- frontend/src/metabase/admin/settings/components/EmbeddingSettings/EmbeddingSettings.tsx
+++ frontend/src/metabase/admin/settings/components/EmbeddingSettings/EmbeddingSettings.tsx
@@ -27,19 +27,21 @@ export function EmbeddingSettings({
 
   return (
     <Box p="0.5rem 1rem 0">
-      <SettingHeader
-        id="enable-embedding"
-        setting={{
-          display_name: t`Embedding`,
-        }}
-      />
       <Stack spacing="2.5rem">
-        <SetByEnvVarWrapper setting={enableEmbeddingSetting}>
-          <EmbeddingSwitchWidget
-            setting={enableEmbeddingSetting}
-            onChange={onChangeEnableEmbedding}
+        <Box data-testid="enable-embedding-setting">
+          <SettingHeader
+            id="enable-embedding"
+            setting={{
+              display_name: t`Embedding`,
+            }}
           />
-        </SetByEnvVarWrapper>
+          <SetByEnvVarWrapper setting={enableEmbeddingSetting}>
+            <EmbeddingSwitchWidget
+              setting={enableEmbeddingSetting}
+              onChange={onChangeEnableEmbedding}
+            />
+          </SetByEnvVarWrapper>
+        </Box>
         <StaticEmbeddingOptionCard />
         <EmbeddingSdkOptionCard />
         <InteractiveEmbeddingOptionCard />
