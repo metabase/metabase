50c71a846c66e50e9568abc84c26ce512a79b7fa
Chris Truter <crisptrutski@users.noreply.github.com>
2024-08-30 10:57:14 +0200

    Add test against serialization YAML format changing (#47349)

--- enterprise/backend/test/metabase_enterprise/serialization/v2/extract_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/v2/extract_test.clj
@@ -7,6 +7,7 @@
    [java-time.api :as t]
    [metabase-enterprise.serialization.test-util :as ts]
    [metabase-enterprise.serialization.v2.extract :as extract]
+   [metabase-enterprise.serialization.v2.round-trip-test :as round-trip-test]
    [metabase.audit :as audit]
    [metabase.core :as mbc]
    [metabase.models
@@ -34,6 +35,10 @@
    [toucan2.core :as t2])
   (:import
    (java.time OffsetDateTime)))
+
+(comment
+  ;; Use this spell in your test body to add the given fixtures to the round trip baseline.
+  (round-trip-test/add-to-baseline!))
 
 (defn- by-model [model-name extraction]
   (->> extraction
@@ -333,6 +338,7 @@
                        _
                        {:action_id action-id
                         :dashboard_id other-dash-id}]
+
       (testing "table and database are extracted as [db schema table] triples"
         (let [ser (serdes/extract-one "Card" {} (t2/select-one Card :id c1-id))]
           (is (=? {:serdes/meta                 [{:model "Card" :id c1-eid :label "some_question"}]
