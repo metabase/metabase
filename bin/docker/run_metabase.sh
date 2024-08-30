1c3a9343c895edf5af3b5c7d498af6004e7ffe8c
Jordi Giron <jordi@revanguard.com>
2024-08-30 14:57:01 +0200

    reverted test

--- bin/docker/run_metabase.sh
+++ bin/docker/run_metabase.sh
@@ -51,7 +51,6 @@ docker_setup_env() {
     file_env 'REACT_APP_SEMANTIC'
     file_env 'REACT_APP_WEBSOCKET_SERVER'
     file_env 'HOSTNAME_APP'
-    file_env 'SERVICE_APP'
 }
 
 # detect if the container is started as root or not
