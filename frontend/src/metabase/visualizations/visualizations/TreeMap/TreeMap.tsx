aa9ef3f33998798a3894f31306be56b6f5ccfd99
Denis Tikhomirov <d.tikhomirov@mcplat.ru>
2024-09-04 01:05:24 +0300

    add treemap settings

--- frontend/src/metabase/visualizations/visualizations/TreeMap/TreeMap.tsx
+++ frontend/src/metabase/visualizations/visualizations/TreeMap/TreeMap.tsx
@@ -1,15 +1,57 @@
 import { t } from "ttag";
 
+import { getOptionFromColumn } from "metabase/visualizations/lib/settings/utils";
+import {
+  getDefaultDimensions,
+  getDefaultMetrics,
+} from "metabase/visualizations/shared/settings/cartesian-chart";
 import type { VisualizationProps } from "metabase/visualizations/types";
+import { isDimension, isMetric } from "metabase-lib/v1/types/utils/isa";
 
 import { ChartRenderer } from "./TreeMap.styled";
+
+const __DIMENSIONS = "treemap.dimensions";
+const __MEASURES = "treemap.measures";
 
 Object.assign(TreeMap, {
   uiName: t`Treemap`,
   identifier: "treemap",
   iconName: "ai",
   placeholderSeries: [],
-  settings: {},
+  settings: {
+    [__DIMENSIONS]: {
+      section: t`Columns`,
+      title: t`Dimensions`,
+      widget: "fields",
+      getDefault: (series, vizSettings) =>
+        getDefaultDimensions(series, vizSettings),
+      getProps: ([{ _card, data }], vizSettings) => {
+        const addedDimensions = vizSettings[__DIMENSIONS] || [];
+        const options = data.cols.filter(isDimension).map(getOptionFromColumn);
+        return {
+          options,
+          addAnother:
+            options.length > addedDimensions.length
+              ? t`Add series breakout`
+              : null,
+          columns: data.cols,
+        };
+      },
+    },
+    [__MEASURES]: {
+      section: t`Columns`,
+      title: t`Measures`,
+      widget: "field",
+      getDefault: (series, vizSettings) =>
+        getDefaultMetrics(series, vizSettings),
+      getProps: ([{ _card, data }], _vizSettings) => {
+        const options = data.cols.filter(isMetric).map(getOptionFromColumn);
+        return {
+          options,
+        };
+      },
+    },
+  },
 });
 
 export function TreeMap(_props: VisualizationProps) {
