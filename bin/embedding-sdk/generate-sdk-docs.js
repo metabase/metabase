7352c39fcaf7af12a08eb6b536022ad7fd52356c
Phoomparin Mano <phoomparin@gmail.com>
2024-09-04 23:48:59 +0700

    update api-extractor

--- bin/embedding-sdk/generate-sdk-docs.js
+++ bin/embedding-sdk/generate-sdk-docs.js
@@ -5,28 +5,37 @@ const {
   ExtractorResult,
 } = require("@microsoft/api-extractor");
 
+console.log("Extracting APIs from codebase...");
+
 const apiExtractorJsonPath = path.join(__dirname, "./api-extractor.jsonc");
 
-// Load and parse the api-extractor.json file
-const extractorConfig =
-  ExtractorConfig.loadFileAndPrepare(apiExtractorJsonPath);
+try {
+  // Load and parse the api-extractor.json file
+  const extractorConfig =
+    ExtractorConfig.loadFileAndPrepare(apiExtractorJsonPath);
 
-// Invoke API Extractor
-const extractorResult = Extractor.invoke(extractorConfig, {
-  // Equivalent to the "--local" command-line parameter
-  localBuild: true,
+  // Invoke API Extractor
+  const extractorResult = Extractor.invoke(extractorConfig, {
+    // Equivalent to the "--local" command-line parameter
+    localBuild: true,
 
-  // Equivalent to the "--verbose" command-line parameter
-  showVerboseMessages: true,
-});
+    // Equivalent to the "--verbose" command-line parameter
+    showVerboseMessages: true,
+  });
 
-if (extractorResult.succeeded) {
-  console.log(`API Extractor completed successfully`);
-  process.exitCode = 0;
-} else {
-  console.error(
-    `API Extractor completed with ${extractorResult.errorCount} errors` +
+  if (extractorResult.succeeded) {
+    console.log(`API Extractor completed successfully`);
+    process.exitCode = 0;
+  } else {
+    console.error(
+      `API Extractor completed with ${extractorResult.errorCount} errors` +
       ` and ${extractorResult.warningCount} warnings`,
-  );
-  process.exitCode = 1;
+    );
+    process.exitCode = 1;
+  }
+} catch (error) {
+  console.error("API Extractor raised an error:");
+  console.error(error);
 }
+
+
