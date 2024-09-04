483303a9f3e3c40871d7120fd34d61c28939daf9
Tamás Benkő <tamas@metabase.com>
2024-09-04 23:55:47 +0300

    Add more splice test cases

--- test/metabase/query_processor/pivot_test.clj
+++ test/metabase/query_processor/pivot_test.clj
@@ -696,6 +696,14 @@
            (splice [0 1] {1 0, 4 3})))
     (is (= [0 1 3 4]
            (splice [0 2] {1 0, 4 3})))
-    (testing "chained remapping (is that something we allow?)"
+    (testing "chained remapping"
+      (is (= [1 2 3 5]
+             (splice [1 2] {1 2, 2 5})))
+      (is (= [1 2 3 4 5]
+             (splice [1 2 3] {1 2, 2 5})))
+      (is (= [1 2 3 5 6]
+             (splice [1 2 4] {1 2, 2 5})))
       (is (= [1 2 3 5 7]
-             (splice [1 2 5] {1 2, 2 5}))))))
+             (splice [1 2 5] {1 2, 2 5})))
+      (is (= [1 2 3 5 8]
+             (splice [1 2 6] {1 2, 2 5}))))))
