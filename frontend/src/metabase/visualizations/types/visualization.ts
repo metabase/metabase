c54d07592acdd685478f4991bfbd56a4eec38b36
Phoomparin Mano <phoomparin@gmail.com>
2024-09-06 21:10:29 +0700

    remove second query argument

--- frontend/src/metabase/visualizations/types/visualization.ts
+++ frontend/src/metabase/visualizations/types/visualization.ts
@@ -236,8 +236,7 @@ export type VisualizationDefinition = {
   placeHolderSeries?: Series;
 
   transformSeries?: (series: Series) => TransformedSeries;
-  // TODO: remove dependency on metabase-lib
-  isSensible: (data: DatasetData, query?: Query) => boolean;
+  isSensible: (data: DatasetData) => boolean;
   // checkRenderable throws an error if a visualization is not renderable
   checkRenderable: (
     series: Series,
