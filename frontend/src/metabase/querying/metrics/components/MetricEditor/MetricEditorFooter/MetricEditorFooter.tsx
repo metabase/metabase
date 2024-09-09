c4c6de5aad3507957b23a41e45586ee0fd6a146f
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-09-09 13:46:26 -0400

    Add frame

--- frontend/src/metabase/querying/metrics/components/MetricEditor/MetricEditorFooter/MetricEditorFooter.tsx
+++ frontend/src/metabase/querying/metrics/components/MetricEditor/MetricEditorFooter/MetricEditorFooter.tsx
@@ -1,8 +1,10 @@
+import DebouncedFrame from "metabase/components/DebouncedFrame";
 import QueryVisualization from "metabase/query_builder/components/QueryVisualization";
 import * as Lib from "metabase-lib";
 import type Question from "metabase-lib/v1/Question";
 import type { Dataset, RawSeries } from "metabase-types/api";
 
+import S from "./MetricEditorFooter.module.css";
 import { MetricEmptyState } from "./MetricEmptyState";
 
 type MetricEditorFooterProps = {
@@ -25,24 +27,24 @@ export function MetricEditorFooter({
 }: MetricEditorFooterProps) {
   const isRunnable = Lib.canRun(question.query(), "metric");
 
-  if (!result && !isRunning) {
-    return (
-      <MetricEmptyState
-        isRunnable={isRunnable}
-        runQuestionQuery={runQuestionQuery}
-      />
-    );
-  }
-
   return (
-    <QueryVisualization
-      question={question}
-      result={result}
-      rawSeries={rawSeries}
-      isRunnable={isRunnable}
-      isRunning={isRunning}
-      isResultDirty={isResultDirty}
-      runQuestionQuery={runQuestionQuery}
-    />
+    <DebouncedFrame className={S.frame}>
+      {result != null || isRunning ? (
+        <QueryVisualization
+          question={question}
+          result={result}
+          rawSeries={rawSeries}
+          isRunnable={isRunnable}
+          isRunning={isRunning}
+          isResultDirty={isResultDirty}
+          runQuestionQuery={runQuestionQuery}
+        />
+      ) : (
+        <MetricEmptyState
+          isRunnable={isRunnable}
+          runQuestionQuery={runQuestionQuery}
+        />
+      )}
+    </DebouncedFrame>
   );
 }
