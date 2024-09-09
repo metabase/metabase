7a1cae2fe3862b5548766b48f5f433d8bcbef707
Chris Truter <chris@metabase.com>
2024-09-09 15:19:59 +0200

    Avoid depending on sample data

--- test/metabase/search/postgres/core_test.clj
+++ test/metabase/search/postgres/core_test.clj
@@ -18,26 +18,17 @@
     (mt/dataset test-data
       (search.postgres/init! true)
       (testing "consistent results between all searches for certain queries\n"
-        (doseq [term ["satisfaction" "e-commerce" "example" "rasta" "new" "revenue"]]
+        (doseq [term ["satisfaction" "e-commerce" "example" "rasta" "new" "revenue" "collection"]]
           (testing (str "consistent results, but not ordering" term)
             (is (= (legacy-results term)
-                   (hybrid term))))))
-      (testing "But sometimes the order is inconsistent"
-        (let [hybrid (hybrid "collection")
-              legacy (legacy-results "collection")]
-          (is (= (set hybrid) (set legacy)))
-          (is (not= hybrid legacy)))))))
+                   (hybrid term)))))))))
 
 (deftest ^:synchronized hybrid-multi-test
   (when (is-postgres?)
     (mt/dataset test-data
       (search.postgres/init! true)
-      (testing "consistent results between "
-        (doseq [term ["satisfaction" "e-commerce" "example" "rasta" "new" "revenue"]]
+      (testing "consistent results between both hybrid implementations"
+        (doseq [term ["satisfaction" "e-commerce" "example" "rasta" "new" "revenue" "collection"]]
           (testing term
             (is (= (hybrid term)
-                   (hybrid-multi term)))))
-        (testing "But sometimes the order is inconsistent"
-          (let [basic (hybrid "collection")
-                multi (hybrid-multi "collection")]
-            (is (= (set basic) (set multi)))))))))
+                   (hybrid-multi term)))))))))
