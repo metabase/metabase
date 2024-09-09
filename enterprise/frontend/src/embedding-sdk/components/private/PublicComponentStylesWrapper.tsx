91fad45f1a6fb712c8cbe73e42680b9275be1645
Nicolò Pretto <info@npretto.com>
2024-09-09 16:30:11 +0200

    clean up duplicated css

--- enterprise/frontend/src/embedding-sdk/components/private/PublicComponentStylesWrapper.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/PublicComponentStylesWrapper.tsx
@@ -26,8 +26,6 @@ export const PublicComponentStylesWrapper = styled.div`
   // end of RESET
 
   font-style: normal;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
 
   width: 100%;
   height: 100%;
