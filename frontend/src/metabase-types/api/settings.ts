5aed175f005716ea31884b31e9039b237ce2b59a
Romeo Van Snick <romeo@romeovansnick.be>
2024-09-11 16:52:50 +0200

    Add user setting for verified metrics

--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -333,6 +333,7 @@ export type UserSettings = {
   "expand-browse-in-nav"?: boolean;
   "expand-bookmarks-in-nav"?: boolean;
   "browse-filter-only-verified-models"?: boolean;
+  "browse-filter-only-verified-metrics"?: boolean;
   "show-updated-permission-modal": boolean;
   "show-updated-permission-banner": boolean;
 };
