82ee478dd59655be4be5ff3f6ba03bd122954318
lbrdnk <lbrdnk@users.noreply.github.com>
2024-09-11 16:37:28 +0200

    Update driver to use schema filters and remove now redundant sync implemnetations

--- modules/drivers/databricks-jdbc/src/metabase/driver/databricks_jdbc.clj
+++ modules/drivers/databricks-jdbc/src/metabase/driver/databricks_jdbc.clj
@@ -1,6 +1,5 @@
 (ns metabase.driver.databricks-jdbc
   (:require
-   [clojure.string :as str]
    [java-time.api :as t]
    [metabase.driver :as driver]
    [metabase.driver.hive-like :as driver.hive-like]
@@ -8,8 +7,6 @@
    [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
    [metabase.driver.sql-jdbc.execute.legacy-impl :as sql-jdbc.legacy]
    [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]
-   [metabase.driver.sql-jdbc.sync.describe-database :as sql-jdbc.describe-database]
-   [metabase.driver.sql-jdbc.sync.interface :as sql-jdbc.sync.interface]
    [metabase.driver.sql.query-processor :as sql.qp]
    [metabase.query-processor.timezone :as qp.timezone]
    [metabase.util :as u]
@@ -48,7 +45,7 @@
   "SET TIME ZONE %s;")
 
 (defmethod sql-jdbc.conn/connection-details->spec :databricks-jdbc
-  [_driver {:keys [catalog host http-path log-level schema token] :as _details}]
+  [_driver {:keys [catalog host http-path log-level token additional-options] :as _details}]
   (assert (string? (not-empty catalog)) "Catalog is mandatory.")
   (merge
    {:classname        "com.databricks.client.jdbc.Driver"
@@ -58,112 +55,19 @@
     ;; https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.40/docs/release-notes.txt
     :subname          (str "//" host ":443/;EnableArrow=0"
                            ";ConnCatalog=" (codec/url-encode catalog)
-                           (when (string? (not-empty schema))
-                             (str ";ConnSchema=" (codec/url-encode schema))))
-    :transportMode    "http"
-    :ssl              1
-    :AuthMech         3
-    :HttpPath         http-path
-    :uid              "token"
-    :pwd              token
+                           (when (string? (not-empty additional-options))
+                             additional-options))
+    :transportMode  "http"
+    :ssl            1
+    :AuthMech       3
+    :HttpPath       http-path
+    :uid            "token"
+    :pwd            token
     :UseNativeQuery 1}
    ;; Following is used just for tests. See the [[metabase.driver.sql-jdbc.connection-test/perturb-db-details]]
    ;; and test that is using the function.
    (when log-level
      {:LogLevel log-level})))
-
-(defmethod driver/describe-database :databricks-jdbc
-  [driver db-or-id-or-spec]
-  {:tables
-   (sql-jdbc.execute/do-with-connection-with-options
-    driver
-    db-or-id-or-spec
-    nil
-    (fn [^Connection conn]
-      (let [database                 (sql-jdbc.describe-database/db-or-id-or-spec->database db-or-id-or-spec)
-            {:keys [catalog schema]} (:details database)
-            dbmeta                   (.getMetaData conn)]
-        (with-open [rs (.getTables dbmeta catalog schema nil
-                                   (into-array String ["TABLE" "VIEW"]))]
-          (let [rs-meta (.getMetaData rs)
-                col-count (.getColumnCount rs-meta)
-                rows (loop [rows []]
-                       (.next rs)
-                       (if (.isAfterLast rs)
-                         rows
-                         (recur (conj rows (mapv (fn [idx]
-                                                   (.getObject rs ^long idx))
-                                                 (map inc (range col-count)))))))
-                fields (map (fn [[_catalog schema table-name _table-type remarks]]
-                              {:name table-name
-                               :schema schema
-                               :description remarks})
-                            rows)
-                fields* (filter (comp (partial sql-jdbc.sync.interface/have-select-privilege?
-                                               :databricks-jdbc
-                                               conn
-                                               schema)
-                                      :name)
-                                fields)]
-            (set fields*))))))})
-
-(def ^:private ^:dynamic *database*
-  "Used to get `[:details :catalog]` in `sql-jdbc.sync/describe-table-fields :databricks-jdbc`. Bound in
-  `driver/describe-table :databricks-jdbc`. Catalog is used as parameter of [[table-fields-sql-str]]."
-  nil)
-
-(defmethod driver/describe-table :databricks-jdbc
-  [driver database table]
-  (binding [*database* database]
-    ((get-method driver/describe-table :sql-jdbc) driver database table)))
-
-(def ^:private describe-table-field-keys
-  #{:column_name :column_default :comment :is_nullable :data_type})
-
-#_{:clj-kondo/ignore true}
-(defn- row-map->field-metadata
-  [{:keys [column_name column_default comment is_nullable data_type]
-    :as row-map}]
-  (doseq [key describe-table-field-keys]
-    (assert (contains? row-map key)))
-  (merge {:name column_name
-          :database-type data_type
-          ;; At the moment there is no way to decide whether column is auto increment with Databricks.
-          ;; `IS_IDENTITY` column is always "NO". Docs say it is reserved for future use. See the following:
-          ;; https://docs.databricks.com/en/sql/language-manual/information-schema/columns.html. Then, with regards
-          ;; to alternative ways of fetching that information, `describe ...` does not contain that and rows that
-          ;; are result of `.getColumns` have corresponding column always set to nil.
-          :database-is-autoincrement false
-          :database-required? (and (nil? column_default)
-                                   (= "NO" is_nullable))}
-         (when comment
-           {:comment comment})))
-
-#_{:clj-kondo/ignore true}
-(def ^:private table-fields-sql-str
-  (str/join "\n"
-            ["select *"
-             "  from information_schema.columns"
-             "  where"
-             "    table_catalog = ?"
-             "    AND table_schema = ?"
-             "    AND table_name = ?"]))
-
-#_(defmethod sql-jdbc.sync/describe-table-fields :databricks-jdbc
-  [driver ^Connection _conn table _db-name-or-nil]
-  (let [catalog-name (get-in *database* [:details :catalog])
-        schema-name  (get-in *database* [:details :schema])
-        table-name   (:name table)]
-    (into
-     #{}
-     (comp
-      (map row-map->field-metadata)
-      (sql-jdbc.sync/describe-table-fields-xf driver *database*))
-     ;; TODO: Maybe I could swap this for plain jdbc version! hence I'd be able to use existing connection!
-     (sql-jdbc.execute/reducible-query *database* [table-fields-sql-str
-                                                   catalog-name
-                                                   schema-name
-                                                   table-name]))))
 
 (defmethod sql.qp/quote-style :databricks-jdbc
   [_driver]
