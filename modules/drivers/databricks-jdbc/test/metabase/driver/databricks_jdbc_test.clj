c8d0af106278d075b53cf95437e88048cb107f06
lbrdnk <lbrdnk@users.noreply.github.com>
2024-09-11 16:37:45 +0200

    Update tests

--- modules/drivers/databricks-jdbc/test/metabase/driver/databricks_jdbc_test.clj
+++ modules/drivers/databricks-jdbc/test/metabase/driver/databricks_jdbc_test.clj
@@ -4,6 +4,7 @@
    [java-time.api :as t]
    [metabase.driver :as driver]
    [metabase.driver.databricks-jdbc :as databricks-jdbc]
+   [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
    [metabase.test :as mt]))
 
 (deftest ^:parallel sync-test
@@ -11,14 +12,14 @@
     (mt/test-driver
       :databricks-jdbc
       (is (= {:tables
-              #{{:name "venues", :schema "test-data", :description ""}
-                {:name "checkins", :schema "test-data", :description ""}
-                {:name "users", :schema "test-data", :description ""}
-                {:name "people", :schema "test-data", :description ""}
-                {:name "categories", :schema "test-data", :description ""}
-                {:name "reviews", :schema "test-data", :description ""}
-                {:name "orders", :schema "test-data", :description ""}
-                {:name "products", :schema "test-data", :description ""}}}
+              #{{:name "venues", :schema "test-data", :description nil}
+                {:name "checkins", :schema "test-data", :description nil}
+                {:name "users", :schema "test-data", :description nil}
+                {:name "people", :schema "test-data", :description nil}
+                {:name "categories", :schema "test-data", :description nil}
+                {:name "reviews", :schema "test-data", :description nil}
+                {:name "orders", :schema "test-data", :description nil}
+                {:name "products", :schema "test-data", :description nil}}}
              (driver/describe-database :databricks-jdbc (mt/db)))))))
 
 (deftest ^:synchronized date-time->results-local-date-time-test
@@ -36,3 +37,19 @@
           (testing "ZonedDateTime is shifted by results timezone"
             (is (= expected
                    (#'databricks-jdbc/date-time->results-local-date-time (t/zoned-date-time 2024 8 29 17 20 30))))))))))
+
+(deftest additional-options-test
+  (mt/test-driver
+   :databricks-jdbc
+   (testing "Additional options are added to :subname key of generated spec"
+     (is (re-find #";IgnoreTransactions=0$"
+                  (->> {:http-path "p/a/t/h",
+                        :schema-filters-type "inclusion",
+                        :schema-filters-patterns "xix",
+                        :access-token "xixix",
+                        :host "localhost",
+                        :engine "databricks-jdbc",
+                        :catalog "ccc"
+                        :additional-options ";IgnoreTransactions=0"}
+                       (sql-jdbc.conn/connection-details->spec :databricks-jdbc)
+                       :subname))))))
