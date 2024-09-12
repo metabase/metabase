4b382f6d9eba84592b5f351586c7ec3247b5a85c
Kamil Mielnik <kamil@kamilmielnik.com>
2024-09-13 01:03:47 +0700

    Update e2e/test/scenarios/admin-2/whitelabel.cy.spec.js

--- e2e/test/scenarios/admin-2/whitelabel.cy.spec.js
+++ e2e/test/scenarios/admin-2/whitelabel.cy.spec.js
@@ -486,9 +486,9 @@ describeEE("formatting > whitelabel", () => {
             cy.findByAltText("No results").should("not.exist");
 
             cy.log("test search not found illustration");
-            cy.findByPlaceholderText(
-              "Search this collection or everywhere…",
-            ).type("This aren't the objects you're looking for");
+            cy.findByPlaceholderText("Search…").type(
+              "This aren't the objects you're looking for",
+            );
             cy.findByAltText("No results").should("not.exist");
           });
         });
