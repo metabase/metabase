6fe5f84460b8c595f95d9fc3983fad1a06c276b7
Ryan Laurie <iethree@gmail.com>
2024-09-12 17:24:08 -0600

    create dashboard settings sidebar

--- frontend/src/metabase/dashboard/components/DashboardHeader/buttons/DashboardActionMenu.tsx
+++ frontend/src/metabase/dashboard/components/DashboardHeader/buttons/DashboardActionMenu.tsx
@@ -51,6 +51,17 @@ export const getExtraButtons = ({
     action: (e: MouseEvent) => onFullscreenChange(!isFullscreen, !e.altKey),
   });
 
+  extraButtons.push({
+    title: t`Edit settings`,
+    icon: "gear",
+    action: openSettingsSidebar,
+  });
+
+  extraButtons.push({
+    separator: true,
+    key: "separator-after-edit-settings",
+  });
+
   if (canEdit) {
     extraButtons.push({
       title: t`Edit settings`,
