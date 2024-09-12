2b0dc0b0c6c13e466f71b21f2d809720045633a2
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-09-12 11:27:09 +0700

    Clarify todo comment

--- frontend/src/metabase/sharing/components/SharingMenu/MenuItems/EmbedMenuItem.tsx
+++ frontend/src/metabase/sharing/components/SharingMenu/MenuItems/EmbedMenuItem.tsx
@@ -7,6 +7,7 @@ import { getUserIsAdmin } from "metabase/selectors/user";
 import { Center, Icon, Menu, Stack, Text, Title } from "metabase/ui";
 
 export function EmbedMenuItem({ onClick }: { onClick: () => void }) {
+  // TODO: Change this to `enable-embedding-static` once the BE is implemented.
   const isStaticEmbeddingEnabled = useSetting("enable-embedding");
   const isAdmin = useSelector(getUserIsAdmin);
 
