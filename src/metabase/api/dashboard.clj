ca927c2c07520998ea132bd07dfbac345d64f0ac
John Swanson <john.swanson@metabase.com>
2024-09-13 13:30:30 -0700

    cljfmt

--- src/metabase/api/dashboard.clj
+++ src/metabase/api/dashboard.clj
@@ -626,10 +626,10 @@
 (defn- assert-no-invalid-dashboard-internal-dashcards [dashboard to-create]
   (when-let [card-ids (seq (keep :card_id to-create))]
     (api/check-400 (not (t2/exists? :model/Card
-                         {:where [:and
-                                  [:not= :dashboard_id (u/the-id dashboard)]
-                                  [:not= :dashboard_id nil]
-                                  [:in :id card-ids]]})))))
+                                    {:where [:and
+                                             [:not= :dashboard_id (u/the-id dashboard)]
+                                             [:not= :dashboard_id nil]
+                                             [:in :id card-ids]]})))))
 
 (defn- do-update-dashcards!
   [dashboard current-cards new-cards]
