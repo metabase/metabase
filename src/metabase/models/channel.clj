a088dea03754ed125c3d9384b80a1e72d55c9e72
Ngoc Khuat <qn.khuat@gmail.com>
2024-09-13 19:14:35 +0700

    channel template schema

--- src/metabase/models/channel.clj
+++ src/metabase/models/channel.clj
@@ -5,6 +5,7 @@
    [metabase.models.permissions :as perms]
    [metabase.models.serialization :as serdes]
    [metabase.util :as u]
+   [metabase.util.malli :as mu]
    [methodical.core :as methodical]
    [toucan2.core :as t2]))
 
@@ -13,30 +14,7 @@
 (methodical/defmethod t2/table-name :model/Channel         [_model] :channel)
 (methodical/defmethod t2/table-name :model/ChannelTemplate [_model] :channel_template)
 
-(defmethod mi/can-write? :model/Channel
-  [& _]
-  (or (mi/superuser?)
-      (perms/current-user-has-application-permissions? :setting)))
 
-(defmethod serdes/entity-id "Channel" [_ {:keys [name]}] name)
-
-(defmethod serdes/hash-fields :model/Channel         [_instance] [:name :type])
-(defmethod serdes/hash-fields :model/ChannelTemplate [_instance] [:name :channel_type :details])
-
-(defmethod mi/can-write? :model/ChannelTemplate
-  [& _]
-  (or (mi/superuser?)
-      (perms/current-user-has-application-permissions? :setting)))
-
-(defmethod serdes/make-spec "Channel"
-  [_model-name _opts]
-  {:copy      [:name :description :type :details :active]
-   :transform {:created_at (serdes/date)}})
-
-(defmethod serdes/make-spec "ChannelTemplate"
-  [_model-name _opts]
-  {:copy      [:name :channel_type :details :entity_id]
-   :transform {:created_at (serdes/date)}})
 
 (doto :model/Channel
   (derive :metabase/model)
@@ -47,13 +25,18 @@
   (derive :hook/entity-id)
   (derive :hook/timestamped?))
 
+;; ------------------------------------------------------------------------------------------------;;
+;;                                           :model/Channel                                        ;;
+;; ------------------------------------------------------------------------------------------------;;
+
 (t2/deftransforms :model/Channel
   {:type    (mi/transform-validator mi/transform-keyword (partial mi/assert-namespaced "channel"))
    :details mi/transform-encrypted-json})
 
-(t2/deftransforms :model/ChannelTemplate
-  {:channel_type  (mi/transform-validator mi/transform-keyword (partial mi/assert-namespaced "channel"))
-   :details       mi/transform-json})
+(defmethod mi/can-write? :model/Channel
+  [& _]
+  (or (mi/superuser?)
+      (perms/current-user-has-application-permissions? :setting)))
 
 (t2/define-before-update :model/Channel
   [instance]
@@ -70,3 +53,72 @@
 (defmethod audit-log/model-details :model/Channel
   [channel _event-type]
   (select-keys channel [:id :name :description :type :active]))
+
+
+(defmethod serdes/entity-id "Channel" [_ {:keys [name]}] name)
+
+(defmethod serdes/hash-fields :model/Channel         [_instance] [:name :type])
+
+(defmethod serdes/make-spec "Channel"
+  [_model-name _opts]
+  {:copy      [:name :description :type :details :active]
+   :transform {:created_at (serdes/date)}})
+;; ------------------------------------------------------------------------------------------------;;
+;;                                       :model/ChannelTemplate                                    ;;
+;; ------------------------------------------------------------------------------------------------;;
+
+(t2/deftransforms :model/ChannelTemplate
+  {:channel_type  (mi/transform-validator mi/transform-keyword (partial mi/assert-namespaced "channel"))
+   :details       mi/transform-json})
+
+(def ^:private channel-template-details-type
+  #{:email/mustache
+    :email/resource})
+
+(def ^:private ChannelTemplateEmailDetails
+  [:merge
+   [:map
+    [:type    (into [:enum] (concat channel-template-details-type (map u/qualified-name channel-template-details-type)))]
+    [:subject string?]]
+   [:multi {:dispatch (comp keyword :type)}
+    [:email/resource
+     [:map
+      [:path string?]]]
+    [:email/mustache
+     [:map
+      [:body string?]]]]])
+
+(def ^:private ChannelTemplate
+  [:merge
+   [:map
+    [:channel_type [:fn #(= "channel" (-> % keyword namespace))]]]
+   [:multi {:dispatch :channel_type}
+    [:channel/email
+     [:map
+      [:details ChannelTemplateEmailDetails]]]]])
+
+(defn- check-valid-channel-template
+  [channel-template]
+  (mu/validate-throw ChannelTemplate channel-template))
+
+(t2/define-before-insert :model/ChannelTemplate
+  [instance]
+  (check-valid-channel-template instance)
+  instance)
+
+(t2/define-before-update :model/ChannelTemplate
+  [instance]
+  (check-valid-channel-template instance)
+  instance)
+
+(defmethod mi/can-write? :model/ChannelTemplate
+  [& _]
+  (or (mi/superuser?)
+      (perms/current-user-has-application-permissions? :setting)))
+
+(defmethod serdes/hash-fields :model/ChannelTemplate [_instance] [:name :channel_type :details])
+
+(defmethod serdes/make-spec "ChannelTemplate"
+  [_model-name _opts]
+  {:copy      [:name :channel_type :details :entity_id]
+   :transform {:created_at (serdes/date)}})
