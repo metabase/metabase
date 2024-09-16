0907d4001c2a17c93f151e9cebe551a40b0b6165
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-09-16 20:14:32 +0300

    update viz and settings buttons

--- frontend/src/metabase/query_builder/components/view/ViewButton.tsx
+++ frontend/src/metabase/query_builder/components/view/ViewButton.tsx
@@ -6,6 +6,7 @@ import { alpha, color } from "metabase/lib/colors";
 type Props = {
   active?: boolean;
   color?: string;
+  iconWithText?: boolean;
 };
 
 // NOTE: some of this is duplicated from NotebookCell.jsx
@@ -30,6 +31,10 @@ const ViewButton = styled(Button)<Props>`
 
   > .Icon {
     opacity: 0.6;
+  }
+
+  .Icon + div {
+    ${props => props.iconWithText && "margin-left: 0.25rem"};
   }
 `;
 
