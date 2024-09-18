1a57048f2fe103db27bec2abe3c5b7c934788a05
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-09-18 22:01:38 +0300

    WIP

--- frontend/src/metabase/querying/notebook/components/NotebookStepList/NotebookStepList.unit.spec.tsx
+++ frontend/src/metabase/querying/notebook/components/NotebookStepList/NotebookStepList.unit.spec.tsx
@@ -8,13 +8,33 @@ import {
   createMockCard,
   createMockStructuredDatasetQuery,
 } from "metabase-types/api/mocks";
-import { createSampleDatabase } from "metabase-types/api/mocks/presets";
+import { ORDERS, PEOPLE, PRODUCTS, PRODUCTS_ID, createSampleDatabase } from "metabase-types/api/mocks/presets";
 import {
   createMockQueryBuilderState,
   createMockState,
 } from "metabase-types/store/mocks";
 
 import { NotebookStepList } from "./NotebookStepList";
+
+const ORDERS_PRODUCT_JOIN_CONDITION = [
+  "=",
+  ["field", ORDERS.PRODUCT_ID, null],
+  ["field", PRODUCTS.ID, { "join-alias": "Products" }],
+];
+
+const ORDERS_PEOPLE_JOIN_CONDITION = [
+  "=",
+  ["field", ORDERS.USER_ID, null],
+  ["field", PEOPLE.ID, { "join-alias": "People" }],
+];
+const RANDOM_ORDER_ID = 155;
+
+const PRODUCTS_JOIN = {
+  alias: "Products",
+  condition: ORDERS_PRODUCT_JOIN_CONDITION,
+  "source-table": PRODUCTS_ID,
+};
+const ORDERS_PK_FILTER = ["=", ["field", ORDERS.ID, null], RANDOM_ORDER_ID];
 
 type SetupOpts = Partial<ComponentProps<typeof NotebookStepList>>;
 
@@ -23,7 +43,13 @@ function setup(opts: SetupOpts = {}) {
   const reportTimezone = "UTC";
 
   const card = createMockCard({
-    dataset_query: createMockStructuredDatasetQuery(),
+    dataset_query: createMockStructuredDatasetQuery({
+      query: {
+        "source-table": 1,
+        // joins: [PRODUCTS_JOIN],
+        filter: ["and", ORDERS_PK_FILTER],
+      }
+    }),
   });
   const state = createMockState({
     qb: createMockQueryBuilderState({
@@ -53,10 +79,12 @@ describe("NotebookStepList", () => {
   it("renders a list of actions in correct order", () => {
     setup();
 
-    const actionButtonsContainer = screen.getByTestId(
+    const actionButtonsContainer = screen.getAllByTestId(
       "action-buttons",
-    ) as HTMLElement;
+    ).at(-1) as HTMLElement;
     const buttons = actionButtonsContainer.querySelectorAll("button");
+
+    screen.logTestingPlaygroundURL();
 
     expect(buttons[0]).toHaveTextContent("Join data");
     expect(buttons[1]).toHaveTextContent("Custom column");
