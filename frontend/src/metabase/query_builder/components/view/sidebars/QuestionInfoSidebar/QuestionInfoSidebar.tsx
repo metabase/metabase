bd8ab56fe8de2c8bfac93fb02fc78b24b3a3e1e9
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-09-18 15:05:43 -0400

    Reset question info sidebar entirely

--- frontend/src/metabase/query_builder/components/view/sidebars/QuestionInfoSidebar/QuestionInfoSidebar.tsx
+++ frontend/src/metabase/query_builder/components/view/sidebars/QuestionInfoSidebar/QuestionInfoSidebar.tsx
@@ -1,5 +1,4 @@
-import { type Dispatch, type SetStateAction, useRef, useState } from "react";
-import { match } from "ts-pattern";
+import { useState } from "react";
 import { t } from "ttag";
 
 import EditableText from "metabase/core/components/EditableText";
@@ -7,7 +6,7 @@ import Link from "metabase/core/components/Link";
 import * as Urls from "metabase/lib/urls";
 import { PLUGIN_CACHING, PLUGIN_MODERATION } from "metabase/plugins";
 import { QuestionActivityTimeline } from "metabase/query_builder/components/QuestionActivityTimeline";
-import { Button, Flex, Icon, Stack, Textarea } from "metabase/ui";
+import { Stack } from "metabase/ui";
 import type Question from "metabase-lib/v1/Question";
 
 import ModelCacheManagementSection from "../ModelCacheManagementSection";
@@ -23,77 +22,10 @@ interface QuestionInfoSidebarProps {
   onSave: (question: Question) => Promise<Question>;
 }
 
-type AnalysisPageId = "default" | "describe" | "caching" | "edit_viz";
-
 export const QuestionInfoSidebar = ({
   question,
   onSave,
 }: QuestionInfoSidebarProps) => {
-  const [pageId, setPageId] = useState<AnalysisPageId>("edit_viz");
-  const scrollableStackRef = useRef<HTMLDivElement | null>(null);
-
-  const page = match(pageId)
-    .with("default", () => (
-      <DefaultPage question={question} onSave={onSave} setPageId={setPageId} />
-    ))
-    .with("describe", () => <DescribePage />)
-    .with("caching", () => (
-      <CachingPage question={question} setPage={setPageId} />
-    ))
-    .otherwise(() => null);
-
-  return (
-    <>
-      <Root>
-        <ContentSection style={{ height: "100%", overflowY: "hidden" }}>
-          <Stack
-            spacing="md"
-            h="100%"
-            style={{
-              padding: "0rem",
-              overflowY: "auto",
-            }}
-            ref={scrollableStackRef}
-          >
-            <Flex gap="sm" p="1rem">
-              <Button
-                c={pageId === "default" ? "var(--mb-color-brand)" : undefined}
-                onClick={() => setPageId("default")}
-              >
-                <Icon name="info" />
-              </Button>
-            </Flex>
-            {page}
-          </Stack>
-        </ContentSection>
-      </Root>
-    </>
-  );
-};
-
-const DescribePage = () => {
-  return (
-    <Stack p="md">
-      <Textarea
-        autosize
-        rows={1}
-        maxRows={4}
-        placeholder={t`Describe yourself and your goals`}
-        id="user-description"
-      />
-    </Stack>
-  );
-};
-
-const DefaultPage = ({
-  question,
-  onSave,
-  setPageId,
-}: {
-  question: Question;
-  onSave: (question: Question) => Promise<Question>;
-  setPageId: Dispatch<SetStateAction<AnalysisPageId>>;
-}) => {
   const description = question.description();
   const canWrite = question.canWrite() && !question.isArchived();
   const isPersisted = question.isPersisted();
@@ -107,69 +39,68 @@ const DefaultPage = ({
     }
   };
 
+  const [page, setPage] = useState<"default" | "caching">("default");
+
   return (
-    <Stack p="lg">
-      <Root>
-        <ContentSection>
-          <HeaderContainer>
-            <h3>{t`About`}</h3>
-            {question.type() === "model" && !question.isArchived() && (
-              <Link
-                variant="brand"
-                to={Urls.modelDetail(question.card())}
-              >{t`Model details`}</Link>
-            )}
-          </HeaderContainer>
-          <EditableText
-            initialValue={description}
-            placeholder={
-              !description && !canWrite ? t`No description` : t`Add description`
-            }
-            isOptional
-            isMultiline
-            isMarkdown
-            isDisabled={!canWrite}
-            onChange={handleSave}
-          />
-          <PLUGIN_MODERATION.QuestionModerationSection question={question} />
-        </ContentSection>
-
-        {question.type() === "model" && isPersisted && (
-          <ContentSection extraPadding>
-            <ModelCacheManagementSection model={question} />
+    <>
+      {page === "default" && (
+        <Root>
+          <ContentSection>
+            <HeaderContainer>
+              <h3>{t`About`}</h3>
+              {question.type() === "model" && !question.isArchived() && (
+                <Link
+                  variant="brand"
+                  to={Urls.modelDetail(question.card())}
+                >{t`Model details`}</Link>
+              )}
+            </HeaderContainer>
+            <EditableText
+              initialValue={description}
+              placeholder={
+                !description && !canWrite
+                  ? t`No description`
+                  : t`Add description`
+              }
+              isOptional
+              isMultiline
+              isMarkdown
+              isDisabled={!canWrite}
+              onChange={handleSave}
+            />
+            <PLUGIN_MODERATION.QuestionModerationSection question={question} />
           </ContentSection>
-        )}
 
-        {hasCacheSection && (
+          {question.type() === "model" && isPersisted && (
+            <ContentSection extraPadding>
+              <ModelCacheManagementSection model={question} />
+            </ContentSection>
+          )}
+
+          {hasCacheSection && (
+            <ContentSection extraPadding>
+              <Stack spacing="0.5rem">
+                <PLUGIN_CACHING.SidebarCacheSection
+                  model="question"
+                  item={question}
+                  setPage={setPage}
+                />
+              </Stack>
+            </ContentSection>
+          )}
           <ContentSection extraPadding>
-            <Stack spacing="0.5rem">
-              <PLUGIN_CACHING.SidebarCacheSection
-                model="question"
-                item={question}
-                setPage={setPageId}
-              />
-            </Stack>
+            <QuestionActivityTimeline question={question} />
           </ContentSection>
-        )}
-        <ContentSection extraPadding>
-          <QuestionActivityTimeline question={question} />
-        </ContentSection>
-      </Root>
-    </Stack>
+        </Root>
+      )}
+      {page === "caching" && (
+        <PLUGIN_CACHING.SidebarCacheForm
+          item={question}
+          model="question"
+          setPage={setPage}
+          pt="md"
+        />
+      )}
+    </>
   );
 };
-
-const CachingPage = ({
-  question,
-  setPage,
-}: {
-  question: Question;
-  setPage: Dispatch<SetStateAction<AnalysisPageId>>;
-}) => (
-  <PLUGIN_CACHING.SidebarCacheForm
-    item={question}
-    model="question"
-    setPage={setPage}
-    pt="md"
-  />
-);
