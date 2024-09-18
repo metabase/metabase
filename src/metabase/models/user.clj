59b1cc8552d4a616fe9c128d545fd1ccf5090b73
Tim Macdonald <tim@metabase.com>
2024-09-18 19:54:30 +0100

    This might be it for avatars, Kelvin (#48004)

--- src/metabase/models/user.clj
+++ src/metabase/models/user.clj
@@ -204,7 +204,7 @@
 
 (def ^:private default-user-columns
   "Sequence of columns that are normally returned when fetching a User from the DB."
-  [:id :email :date_joined :first_name :last_name :last_login :is_superuser :is_qbnewb])
+  [:id :avatar :email :date_joined :first_name :last_name :last_login :is_superuser :is_qbnewb])
 
 (def admin-or-self-visible-columns
   "Sequence of columns that we can/should return for admins fetching a list of all Users, or for the current user
