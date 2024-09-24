1def1148921d9f0189e1dd2f02f55cb24909764d
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-09-25 00:05:40 +0300

    add more tests

--- frontend/src/metabase/querying/notebook/components/NotebookStepList/NotebookStepList.unit.spec.tsx
+++ frontend/src/metabase/querying/notebook/components/NotebookStepList/NotebookStepList.unit.spec.tsx
@@ -1,14 +1,22 @@
+/* eslint jest/expect-expect: ["error", { "assertFunctionNames": ["assertActionButtonsOrder"] }] */
 import type { ComponentProps } from "react";
 
 import { createMockEntitiesState } from "__support__/store";
 import { renderWithProviders, screen } from "__support__/ui";
 import { getMetadata } from "metabase/selectors/metadata";
 import Question from "metabase-lib/v1/Question";
+import type { Card, Filter, JoinCondition } from "metabase-types/api";
 import {
   createMockCard,
   createMockStructuredDatasetQuery,
 } from "metabase-types/api/mocks";
-import { ORDERS, PEOPLE, PRODUCTS, PRODUCTS_ID, createSampleDatabase } from "metabase-types/api/mocks/presets";
+import {
+  ORDERS,
+  ORDERS_ID,
+  PRODUCTS,
+  PRODUCTS_ID,
+  createSampleDatabase,
+} from "metabase-types/api/mocks/presets";
 import {
   createMockQueryBuilderState,
   createMockState,
@@ -16,41 +24,25 @@ import {
 
 import { NotebookStepList } from "./NotebookStepList";
 
-const ORDERS_PRODUCT_JOIN_CONDITION = [
+const ORDERS_PRODUCT_JOIN_CONDITION: JoinCondition = [
   "=",
   ["field", ORDERS.PRODUCT_ID, null],
   ["field", PRODUCTS.ID, { "join-alias": "Products" }],
 ];
-
-const ORDERS_PEOPLE_JOIN_CONDITION = [
-  "=",
-  ["field", ORDERS.USER_ID, null],
-  ["field", PEOPLE.ID, { "join-alias": "People" }],
-];
-const RANDOM_ORDER_ID = 155;
 
 const PRODUCTS_JOIN = {
   alias: "Products",
   condition: ORDERS_PRODUCT_JOIN_CONDITION,
   "source-table": PRODUCTS_ID,
 };
-const ORDERS_PK_FILTER = ["=", ["field", ORDERS.ID, null], RANDOM_ORDER_ID];
+const ORDERS_FILTER: Filter = [">", ["field", ORDERS.TAX, null], 10];
 
 type SetupOpts = Partial<ComponentProps<typeof NotebookStepList>>;
 
-function setup(opts: SetupOpts = {}) {
+function setup(opts: SetupOpts = {}, card: Card) {
   const database = createSampleDatabase();
   const reportTimezone = "UTC";
 
-  const card = createMockCard({
-    dataset_query: createMockStructuredDatasetQuery({
-      query: {
-        "source-table": 1,
-        // joins: [PRODUCTS_JOIN],
-        filter: ["and", ORDERS_PK_FILTER],
-      }
-    }),
-  });
   const state = createMockState({
     qb: createMockQueryBuilderState({
       card,
@@ -76,21 +68,109 @@ function setup(opts: SetupOpts = {}) {
 }
 
 describe("NotebookStepList", () => {
-  it("renders a list of actions in correct order", () => {
-    setup();
+  it("renders a list of actions for data step", () => {
+    const card = createMockCard({
+      dataset_query: createMockStructuredDatasetQuery({
+        query: {
+          "source-table": ORDERS_ID,
+        },
+      }),
+    });
 
-    const actionButtonsContainer = screen.getAllByTestId(
-      "action-buttons",
-    ).at(-1) as HTMLElement;
-    const buttons = actionButtonsContainer.querySelectorAll("button");
+    setup({}, card);
 
-    screen.logTestingPlaygroundURL();
+    assertActionButtonsOrder([
+      "Join data",
+      "Custom column",
+      "Filter",
+      "Summarize",
+      "Sort",
+      "Row limit",
+    ]);
+  });
 
-    expect(buttons[0]).toHaveTextContent("Join data");
-    expect(buttons[1]).toHaveTextContent("Custom column");
-    expect(buttons[2]).toHaveTextContent("Filter");
-    expect(buttons[3]).toHaveTextContent("Summarize");
-    expect(buttons[4]).toHaveTextContent("Sort");
-    expect(buttons[5]).toHaveTextContent("Row limit");
+  it("renders a list of actions for join step", () => {
+    const card = createMockCard({
+      dataset_query: createMockStructuredDatasetQuery({
+        query: {
+          "source-table": ORDERS_ID,
+          joins: [PRODUCTS_JOIN],
+        },
+      }),
+    });
+    setup({}, card);
+
+    assertActionButtonsOrder([
+      "Join data",
+      "Custom column",
+      "Filter",
+      "Summarize",
+      "Sort",
+      "Row limit",
+    ]);
+  });
+
+  it("renders a list of actions for Custom column step", () => {
+    const card = createMockCard({
+      dataset_query: createMockStructuredDatasetQuery({
+        query: {
+          "source-table": ORDERS_ID,
+          expressions: {
+            "Custom column": ["+", 1, 1],
+          },
+        },
+      }),
+    });
+    setup({}, card);
+
+    assertActionButtonsOrder(["Filter", "Summarize", "Sort", "Row limit"]);
+  });
+
+  it("renders a list of actions for Filter step", () => {
+    const card = createMockCard({
+      dataset_query: createMockStructuredDatasetQuery({
+        query: {
+          "source-table": ORDERS_ID,
+          filter: ORDERS_FILTER,
+        },
+      }),
+    });
+    setup({}, card);
+
+    assertActionButtonsOrder(["Summarize", "Sort", "Row limit"]);
+  });
+
+  it("renders a list of actions for Summarize step", () => {
+    const card = createMockCard({
+      dataset_query: createMockStructuredDatasetQuery({
+        query: {
+          "source-table": ORDERS_ID,
+          aggregation: [["count"]],
+          breakout: [["field", ORDERS.CREATED_AT, null]],
+        },
+      }),
+    });
+    setup({}, card);
+
+    assertActionButtonsOrder([
+      "Sort",
+      "Row limit",
+      "Join data",
+      "Custom column",
+      "Filter",
+      "Summarize",
+    ]);
   });
 });
+
+function assertActionButtonsOrder(buttonNames: string[]) {
+  const actionButtonsContainer = screen
+    .getAllByTestId("action-buttons")
+    .at(-1) as HTMLElement;
+  const buttons = actionButtonsContainer.querySelectorAll("button");
+
+  expect(buttons.length).toBe(buttonNames.length);
+  buttonNames.forEach((name, index) => {
+    expect(buttons[index]).toHaveTextContent(name);
+  });
+}
