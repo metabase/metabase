bbdd0fe0cf82d05e9a7c933e6dd08d242c0c7324
Nicolò Pretto <info@npretto.com>
2024-09-25 09:51:02 +0200

    move public embed card to its own file

--- frontend/src/metabase/public/components/EmbedModal/SelectEmbedTypePane/SelectEmbedTypePane.tsx
+++ frontend/src/metabase/public/components/EmbedModal/SelectEmbedTypePane/SelectEmbedTypePane.tsx
@@ -1,38 +1,23 @@
 import cx from "classnames";
 import { useState } from "react";
-import { useAsync } from "react-use";
-import { jt, t } from "ttag";
+import { t } from "ttag";
 
 import { getPlan } from "metabase/common/utils/plan";
 import Link from "metabase/core/components/Link";
 import CS from "metabase/css/core/index.css";
 import { useSelector } from "metabase/lib/redux";
 import { PLUGIN_EMBEDDING } from "metabase/plugins";
-import {
-  trackPublicEmbedCodeCopied,
-  trackPublicLinkRemoved,
-} from "metabase/public/lib/analytics";
+import { trackPublicLinkRemoved } from "metabase/public/lib/analytics";
 import { getPublicEmbedHTML } from "metabase/public/lib/code";
 import type {
   EmbedResource,
   EmbedResourceType,
 } from "metabase/public/lib/types";
 import { getSetting } from "metabase/selectors/settings";
-import { PublicLinkCopyPanel } from "metabase/sharing/components/PublicLinkPopover/PublicLinkCopyPanel";
 import type { ExportFormatType } from "metabase/sharing/components/PublicLinkPopover/types";
-import {
-  Box,
-  Button,
-  Center,
-  Group,
-  Icon,
-  List,
-  Loader,
-  Popover,
-  Stack,
-  Text,
-} from "metabase/ui";
+import { Box, Group, Icon, List, Stack, Text } from "metabase/ui";
 
+import { PublicEmbedCard } from "./PublicEmbedCard";
 import { SharingPaneButton } from "./SharingPaneButton/SharingPaneButton";
 import SdkIllustration from "./illustrations/embedding-sdk.svg?component";
 import InteractiveEmbeddingIllustration from "./illustrations/interactive-embedding.svg?component";
@@ -195,75 +180,6 @@ export function SelectEmbedTypePane({
     </Stack>
   );
 }
-
-const PublicEmbedCard = ({
-  publicEmbedCode,
-  createPublicLink,
-  deletePublicLink,
-  resourceType,
-}: any) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const { loading } = useAsync(async () => {
-    if (isOpen && !publicEmbedCode) {
-      return createPublicLink();
-    }
-    return null;
-  }, [publicEmbedCode, isOpen]);
-
-  return (
-    <Group spacing="xs">
-      <Text>
-        {jt`Use ${(
-          <Text span fw="bold" key="bold">
-            {t`public embedding`}
-          </Text>
-        )} to add a publicly-visible iframe embed to your web page or blog
-    post.`}
-      </Text>
-      <Popover
-        width={200}
-        position="bottom"
-        withArrow
-        shadow="md"
-        opened={isOpen}
-      >
-        <Popover.Target>
-          <Button
-            variant="subtle"
-            p={0}
-            onClick={() => setIsOpen(value => !value)}
-          >{t`Get embedding code`}</Button>
-        </Popover.Target>
-        <Popover.Dropdown>
-          <Stack p="lg" w="28rem" mih="7.5rem" justify="center">
-            {loading ? (
-              <Center>
-                <Loader />
-              </Center>
-            ) : (
-              <PublicLinkCopyPanel
-                url={publicEmbedCode}
-                onRemoveLink={e => {
-                  setIsOpen(false);
-                  deletePublicLink(e);
-                }}
-                removeButtonLabel={t`Remove public link`}
-                removeTooltipLabel={t`Affects both public link and embed URL for this dashboard`}
-                onCopy={() =>
-                  trackPublicEmbedCodeCopied({
-                    artifact: resourceType,
-                    source: "public-embed",
-                  })
-                }
-              />
-            )}
-          </Stack>
-        </Popover.Dropdown>
-      </Popover>
-    </Group>
-  );
-};
 
 export const useInteractiveEmbeddingCta = () => {
   const isInteractiveEmbeddingEnabled = useSelector(
