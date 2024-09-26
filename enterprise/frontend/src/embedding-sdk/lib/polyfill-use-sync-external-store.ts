1ab2ce59483b6de9e3aee4f219e14eae71e40337
Oisin Coveney <oisin@metabase.com>
2024-09-26 13:08:58 +0300

    use shim check for shimming

--- enterprise/frontend/src/embedding-sdk/lib/polyfill-use-sync-external-store.ts
+++ enterprise/frontend/src/embedding-sdk/lib/polyfill-use-sync-external-store.ts
@@ -1,13 +1,11 @@
 import React from "react";
 import { useSyncExternalStore } from "use-sync-external-store/shim";
 
-import { getMajorReactVersion } from "metabase/lib/compat/check-version";
-
 // Monkey-patches useSyncExternalStore if we are in React 17,
 // where useSyncExternalStore is not available.
 // This is used in react-redux v8, until they've dropped the shim in v9.
-const reactVersion = getMajorReactVersion();
+export const shouldShimExternalStore = () => !("useSyncExternalStore" in React);
 
-if (reactVersion <= 17) {
+if (shouldShimExternalStore()) {
   React.useSyncExternalStore = useSyncExternalStore;
 }
