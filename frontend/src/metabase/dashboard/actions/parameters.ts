c54064ea078a2ff467603dd547f5bfc5bef3761c
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-09-27 11:25:01 -0400

    Reset the dashboard parameter when setting the default value (#48089) (#48165)
    * Reset the dashboard parameter when setting the default value
    
    * Fix test for filter default
    
    * Add test for setting parameters when editing the defaults
    
    * Use plain dispatch
    
    
    
    * Use cy.log instead of comments
    
    * Use icon selector
    
    ---------
    
    Co-authored-by: Romeo Van Snick <romeo@romeovansnick.be>
    Co-authored-by: Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>

--- frontend/src/metabase/dashboard/actions/parameters.ts
+++ frontend/src/metabase/dashboard/actions/parameters.ts
@@ -417,6 +417,7 @@ export const setParameterDefaultValue = createThunkAction(
       ...parameter,
       default: defaultValue,
     }));
+    dispatch(setParameterValue(parameterId, defaultValue));
     return { id: parameterId, defaultValue };
   },
 );
