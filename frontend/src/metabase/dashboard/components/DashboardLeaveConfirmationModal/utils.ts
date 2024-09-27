32109288234f4261634a17164729c34c2bf6bc17
Sloan Sparger <sloansparger@gmail.com>
2024-09-27 18:32:51 -0500

    fix unit tests

--- frontend/src/metabase/dashboard/components/DashboardLeaveConfirmationModal/utils.ts
+++ frontend/src/metabase/dashboard/components/DashboardLeaveConfirmationModal/utils.ts
@@ -1,9 +1,8 @@
 import type { Location } from "history";
 
-import { IS_LOCATION_ALLOWED } from "metabase/components/LeaveConfirmationModal/LeaveConfirmationModal";
 import { deserializeCard, parseHash } from "metabase/query_builder/actions";
 
-const isNavigatingToCreateADashboardQuestion = (
+export const isNavigatingToCreateADashboardQuestion = (
   location?: Location,
 ): boolean => {
   const isQuestionCreationUrl =
@@ -22,17 +21,4 @@ const isNavigatingToCreateADashboardQuestion = (
   }
 
   return false;
-};
-
-export const isNavigatingToCreateADashboardQuestionGuard = (
-  location?: Location,
-) => {
-  return !isNavigatingToCreateADashboardQuestion(location);
-};
-
-export const isNavigatingElsewhereGuard = (location?: Location) => {
-  if (isNavigatingToCreateADashboardQuestion(location)) {
-    return false;
-  }
-  return IS_LOCATION_ALLOWED(location);
 };
