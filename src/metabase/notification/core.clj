a8112c9c14a8eae1c43bad2c4bfcc8d8d3c51e81
Ngoc Khuat <qn.khuat@gmail.com>
2024-09-30 22:38:11 +0700

    send notifications in a pooled executors

--- src/metabase/notification/core.clj
+++ src/metabase/notification/core.clj
@@ -5,7 +5,10 @@
    [metabase.util.log :as log]
    [metabase.util.malli :as mu]
    [metabase.util.malli.schema :as ms]
-   [toucan2.core :as t2]))
+   [toucan2.core :as t2])
+  (:import
+   (java.util.concurrent Callable Executors ExecutorService)
+   (org.apache.commons.lang3.concurrent BasicThreadFactory$Builder)))
 
 (set! *warn-on-reflection* true)
 
@@ -37,8 +40,15 @@
               :template
               :recipients))
 
-(mu/defn send-notification!
-  "Send the notification to all handlers synchronously."
+(defonce ^:private pool
+  (Executors/newFixedThreadPool
+   10 ;; TODO: this should be configurable
+   (.build
+    (doto (BasicThreadFactory$Builder.)
+      (.namingPattern "send-notification-thread-pool-%d")))))
+
+(mu/defn- send-notification-sync!
+  "Send the notification to all handlers synchronously. Do not use this directly, use *send-notification!* instead."
   [notification-info :- NotificationInfo]
   (let [noti-handlers (hydrate-notification-handler (t2/select :model/NotificationHandler :notification_id (:id notification-info)))]
     (log/infof "[Notification %d] Found %d handlers" (:id notification-info) (count noti-handlers))
@@ -52,4 +62,17 @@
         (log/infof "[Notification %d] Got %d messages for channel %s" (:id notification-info) (count messages) (:channel_type handler))
         (doseq [message messages]
           (channel/send! (or (:channel handler)
-                             {:type channel-type}) message))))))
+                             {:type channel-type}) message)))))
+  nil)
+
+(defn- send-notification-async!
+  "Send a notification asynchronously."
+  [notification]
+  (let [task (bound-fn []
+               (send-notification-sync! notification))]
+    (.submit ^ExecutorService pool ^Callable task))
+  nil)
+
+(def ^:dynamic send-notification!
+  "The function to send a notification. Defaults to `send-notification-async!`."
+  send-notification-async!)
