69e960cebe34f8e92a85dd7cd66bc52f73d740e8
Bryan Maass <bryan.maass@gmail.com>
2024-10-01 11:35:04 -0600

    migration should work on mysql+h2+pg

--- test/metabase/db/schema_migrations_test.clj
+++ test/metabase/db/schema_migrations_test.clj
@@ -2627,3 +2627,28 @@
                       (map #(select-keys % [:collection_id :perm_type :perm_value :object]))))))
         (testing "the invalid permissions (for a nonexistent table) were deleted"
           (is (empty? (t2/select :model/Permissions :object [:in [nonexistent-path nonexistent-read-path]]))))))))
+
+(deftest populate-enabled-embedding-settings-works
+  (testing "Check that embedding settings are nil when enable-embedding is nil"
+    (impl/test-migrations "v51.2024-09-26T03:01:00" [migrate!]
+      (t2/delete! :model/Setting :key "enable-embedding")
+      (migrate!)
+      (is (= nil (t2/select-one :model/Setting :key "enable-embedding-interactive")))))
+  (testing "Check that embedding settings are true when enable-embedding is true"
+    (impl/test-migrations ["v51.2024-09-26T03:01:00" "v51.2024-09-26T03:03:00"] [migrate!]
+      (t2/delete! :model/Setting :key "enable-embedding")
+      (t2/insert! :model/Setting {:key "enable-embedding" :value "true"})
+      (migrate!)
+      (is (= "true" (t2/select-one-fn :value :model/Setting :key "enable-embedding-interactive")))))
+  (testing "Check that embedding settings are false when enable-embedding is false"
+    (impl/test-migrations ["v51.2024-09-26T03:01:00" "v51.2024-09-26T03:03:00"] [migrate!]
+      (t2/delete! :model/Setting :key "enable-embedding")
+      (t2/insert! :model/Setting {:key "enable-embedding" :value "false"})
+      (migrate!)
+      (is (= "false" (t2/select-one-fn :value :model/Setting :key "enable-embedding-interactive"))))))
+
+;; string set to a value
+;; string unset
+
+#_(deftest populate-embedding-origins-settings-works
+    )
