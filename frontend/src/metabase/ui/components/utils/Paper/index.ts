f8fd8abb7ecd7221928877c550283acddcc7fffb
Sloan Sparger <sloansparger@gmail.com>
2024-10-08 13:08:04 -0500

    port paper component

--- frontend/src/metabase/ui/components/utils/Paper/index.ts
+++ frontend/src/metabase/ui/components/utils/Paper/index.ts
@@ -1,3 +1,3 @@
 export { Paper } from "@mantine/core";
 export type { PaperProps } from "@mantine/core";
-export { getPaperOverrides } from "./Paper.styled";
+export { paperOverrides } from "./Paper.styled";
