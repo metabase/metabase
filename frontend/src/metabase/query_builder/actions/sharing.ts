3c52bc2d117f1d5791e6d304f9ea133b8fac1775
Oisin Coveney <oisin@metabase.com>
2024-10-08 16:39:50 +0300

    Remove intermediate actions and use hooks directly

--- frontend/src/metabase/query_builder/actions/sharing.ts
+++ frontend/src/metabase/query_builder/actions/sharing.ts
@@ -1,31 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import { createCardPublicLink, deleteCardPublicLink } from "metabase/api";
-import { createThunkAction } from "metabase/lib/redux";
 import type { EmbeddingParameters } from "metabase/public/lib/types";
 import { CardApi } from "metabase/services";
 import type { Card, CardId } from "metabase-types/api";
-
-export const CREATE_PUBLIC_LINK = "metabase/card/CREATE_PUBLIC_LINK";
-
-export const createPublicLink = createThunkAction(
-  CREATE_PUBLIC_LINK,
-  ({ id }: Card) =>
-    async dispatch => {
-      const { data } = await (dispatch(
-        createCardPublicLink.initiate({ id }),
-      ) as Promise<{ data: { uuid: string }; error: unknown }>);
-      return { id, uuid: data.uuid };
-    },
-);
-
-export const DELETE_PUBLIC_LINK = "metabase/card/DELETE_PUBLIC_LINK";
-
-export const deletePublicLink = createThunkAction(
-  DELETE_PUBLIC_LINK,
-  (card: Card) => async dispatch =>
-    await dispatch(deleteCardPublicLink.initiate(card)),
-);
 
 export const updateEnableEmbedding = createAsyncThunk<
   Pick<Card, "id" | "enable_embedding"> & { uuid: Card["public_uuid"] },
