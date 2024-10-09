644420162600f6300450e99b27d7c32b023708b6
Sloan Sparger <sloansparger@gmail.com>
2024-10-09 15:22:08 -0500

    port Anchor

--- frontend/src/metabase/ui/components/typography/Anchor/index.ts
+++ frontend/src/metabase/ui/components/typography/Anchor/index.ts
@@ -1,3 +1,3 @@
 export { Anchor } from "@mantine/core";
 export type { AnchorProps } from "@mantine/core";
-export { getAnchorOverrides } from "./Anchor.styled";
+export { anchorOverrides } from "./Anchor.styled";
