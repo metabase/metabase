f576f53172972471b3949faff4a2627d87118887
Sloan Sparger <sloansparger@gmail.com>
2024-10-09 15:22:08 -0500

    port Anchor

--- frontend/src/metabase/ui/components/typography/Anchor/Anchor.styled.tsx
+++ frontend/src/metabase/ui/components/typography/Anchor/Anchor.styled.tsx
@@ -1,17 +1,11 @@
-import type { MantineThemeOverride } from "@mantine/core";
+import { Anchor, type MantineThemeOverride } from "@mantine/core";
 
-export const getAnchorOverrides = (): MantineThemeOverride["components"] => ({
-  Anchor: {
-    styles: theme => {
-      return {
-        root: {
-          color: theme.fn.themeColor("brand"),
-          "&:active": {
-            color: theme.fn.themeColor("text-dark"),
-            textDecoration: "underline",
-          },
-        },
-      };
+import AnchorStyles from "./Anchor.module.css";
+
+export const anchorOverrides: MantineThemeOverride["components"] = {
+  Anchor: Anchor.extend({
+    classNames: {
+      root: AnchorStyles.root,
     },
-  },
-});
+  }),
+};
