6f6b5929e913aac10e669f26b2bcbec746f6c20d
Sloan Sparger <sloansparger@gmail.com>
2024-10-09 16:30:40 -0500

    port Textarea

--- frontend/src/metabase/ui/components/inputs/Textarea/Textarea.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Textarea/Textarea.styled.tsx
@@ -1,12 +1,12 @@
-import type { MantineThemeOverride } from "@mantine/core";
+import { type MantineThemeOverride, Textarea } from "@mantine/core";
 
-export const getTextareaOverrides = (): MantineThemeOverride["components"] => ({
-  Textarea: {
+export const textareaOverrides: MantineThemeOverride["components"] = {
+  Textarea: Textarea.extend({
     defaultProps: {
       size: "md",
       autosize: true,
       minRows: 2,
       maxRows: 6,
     },
-  },
-});
+  }),
+};
