c3446ac97670a7852ba276f7547d6c5b596341a3
Ryan Laurie <iethree@gmail.com>
2024-10-09 13:20:28 -0600

    Input + TextInput

--- frontend/src/metabase/ui/components/inputs/TextInput/index.ts
+++ frontend/src/metabase/ui/components/inputs/TextInput/index.ts
@@ -1,3 +1,3 @@
 export { TextInput } from "@mantine/core";
 export type { TextInputProps } from "@mantine/core";
-export { getTextInputOverrides } from "./TextInput.styled";
+export { textInputOverrides } from "./TextInput.styled";
