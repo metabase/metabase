999468f154c550574688567efb0370da15fc99ad
Ryan Laurie <iethree@gmail.com>
2024-10-09 15:34:27 -0600

    File inputs

--- frontend/src/metabase/ui/components/inputs/FileInput/FileInputValue.styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-import type { TextProps } from "@mantine/core";
-import { Text } from "@mantine/core";
-
-export const FileInputText = styled(Text)<TextProps>`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
