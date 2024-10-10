3a01a0cc1ebae012c5cf61e8ee4bbabc3b7a5c08
John Swanson <john.swanson@metabase.com>
2024-10-10 13:54:39 -0700

    Add the setting back with a deprecation notice
    A bit awkwardly, we never set `:deprecated` on the setting before. We
    can retroactively deprecate this as of v50.
    
    I'm adding back the setting purely to emit the warning message on startup.

--- src/metabase/server/middleware/auth.clj
+++ src/metabase/server/middleware/auth.clj
@@ -2,6 +2,7 @@
   "Middleware related to enforcing authentication/API keys (when applicable). Unlike most other middleware most of this
   is not used as part of the normal `app`; it is instead added selectively to appropriate routes."
   (:require
+   [metabase.models.setting :refer [defsetting]]
    [metabase.server.request.util :as req.util]))
 
 (defn enforce-authentication
@@ -13,3 +14,13 @@
        (handler request respond raise)
        (respond req.util/response-unauthentic)))
    (meta handler)))
+
+(defsetting api-key
+  "When set, this API key is required for all API requests."
+  :deprecated "0.50.0"
+  :encryption :when-encryption-key-set
+  :visibility :internal
+  :doc "Decommissioned setting that previously allowed specifying a static API key required for all API requests to
+        the `/notify` endpoints. No longer has any effect, as these endpoints are now protected by normal auth (with
+        normal API keys set via the admin panel); purely keeping this around to emit a WARN log message on startup. We
+        can remove this in a version or two.")
