9228b0787a989dc5392d5951672d124b65bdb328
Romeo Van Snick <romeo@romeovansnick.be>
2024-10-10 15:51:55 +0200

    Move setup to before and find summarize block using helper

--- e2e/test/scenarios/metrics/reproductions/metrics-reproductions.cy.spec.ts
+++ e2e/test/scenarios/metrics/reproductions/metrics-reproductions.cy.spec.ts
@@ -1,10 +1,15 @@
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
-import { createQuestion, restore } from "e2e/support/helpers";
+import {
+  createQuestion,
+  getNotebookStep,
+  main,
+  restore,
+} from "e2e/support/helpers";
 
 const { ORDERS_ID, ORDERS } = SAMPLE_DATABASE;
 
 describe("issue 47058", () => {
-  it("should show the loading page while the question metadata is being fetched (metabase#47058)", () => {
+  before(() => {
     restore();
     cy.signInAsNormalUser();
     cy.intercept("GET", "/api/card/*/query_metadata", req =>
@@ -18,7 +23,7 @@ describe("issue 47058", () => {
         "source-table": ORDERS_ID,
         aggregation: [["count"]],
       },
-    }).then(({ body: { id: metricId, name: metricName } }) => {
+    }).then(({ body: { id: metricId } }) => {
       createQuestion({
         name: "Question 47058",
         type: "question",
@@ -33,20 +38,26 @@ describe("issue 47058", () => {
         },
       }).then(({ body: { id: questionId } }) => {
         cy.visit(`/question/${questionId}/notebook`);
-
-        cy.findByText("Loading...").should("be.visible");
-        cy.findByText(metricName).should("not.exist");
-        cy.findByText("[Unknown Metric]").should("not.exist");
-
-        cy.wait("@metadata");
-        cy.log(
-          "Only renders the notebook editor (with the summarize button that has the metrics' name on it) after the metadata is loaded",
-        );
-
-        cy.findByText("Loading...").should("not.exist");
-        cy.findByText("[Unknown Metric]").should("not.exist");
-        cy.findByText(metricName).should("be.visible");
       });
+    });
+  });
+
+  it("should show the loading page while the question metadata is being fetched (metabase#47058)", () => {
+    main().within(() => {
+      cy.findByText("Loading...").should("be.visible");
+      getNotebookStep("summarize").should("not.exist");
+
+      cy.findByText("[Unknown Metric]").should("not.exist");
+
+      cy.wait("@metadata");
+      cy.log(
+        "Only renders the notebook editor (with the summarize button that has the metrics' name on it) after the metadata is loaded",
+      );
+
+      cy.findByText("Loading...").should("not.exist");
+      getNotebookStep("summarize").should("be.visible");
+
+      cy.findByText("[Unknown Metric]").should("not.exist");
     });
   });
 });
