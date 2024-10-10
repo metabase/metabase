ba419eec13ca81bcd2072f41c02cd9240434f293
Noah Moss <noahbmoss@gmail.com>
2024-10-10 17:04:47 -0400

    add test

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -574,8 +574,8 @@
       ;; change for a native query, populate-result-metadata removes it (set to nil) unless prevented by the
       ;; verified-result-metadata? flag (see #37009).
       (cond-> #_changes
-       (or (empty? (:result_metadata card))
-           (not verified-result-metadata?))
+        (or (empty? (:result_metadata card))
+            (not verified-result-metadata?))
         card.metadata/populate-result-metadata)
       pre-update
       populate-query-fields
