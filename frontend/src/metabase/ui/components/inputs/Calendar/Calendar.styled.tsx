86605f5b2da1cd015010ceaf531d4d5a6aff0bcd
Ryan Laurie <iethree@gmail.com>
2024-10-10 15:02:40 -0600

    Calendar and DatePicker

--- frontend/src/metabase/ui/components/inputs/Calendar/Calendar.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Calendar/Calendar.styled.tsx
@@ -1,8 +1,19 @@
-import type { MantineThemeOverride } from "@mantine/core";
-import { getStylesRef, rem } from "@mantine/core";
+import {
+  Calendar,
+  CalendarHeader,
+  Day,
+  Month,
+  MonthLevel,
+  MonthsList,
+  PickerControl,
+  WeekdaysRow,
+  YearsList,
+} from "@mantine/dates";
 
-export const getCalendarOverrides = (): MantineThemeOverride["components"] => ({
-  Calendar: {
+import Styles from "./Calendar.module.css";
+
+export const calendarOverrides = {
+  Calendar: Calendar.extend({
     defaultProps: {
       /**
        * Months have different number of day rows (4, 5 or 6). This causes date picker height to change when
@@ -11,197 +22,53 @@ export const getCalendarOverrides = (): MantineThemeOverride["components"] => ({
        */
       mih: 314,
     },
-  },
-  Day: {
-    styles: theme => ({
-      day: {
-        width: rem(40),
-        height: rem(40),
-        color: theme.fn.themeColor("text-dark"),
-        fontSize: theme.fontSizes.md,
-        lineHeight: rem(24),
-        borderRadius: theme.radius.xs,
-
-        "&:hover": {
-          backgroundColor: "var(--mb-color-bg-light)",
-        },
-        "&[data-disabled]": {
-          color: theme.fn.themeColor("bg-dark"),
-        },
-        "&[data-weekend]": {
-          color: theme.fn.themeColor("text-dark"),
-        },
-        "&[data-outside]": {
-          color: theme.fn.themeColor("bg-dark"),
-        },
-        "&[data-in-range]": {
-          color: "var(--mb-color-text-hover)",
-          borderRadius: 0,
-          backgroundColor: "var(--mb-color-background-hover)",
-          "&:hover": {
-            backgroundColor: "var(--mb-color-background-hover)",
-          },
-        },
-        "&[data-first-in-range]": {
-          borderTopLeftRadius: theme.radius.xs,
-          borderBottomLeftRadius: theme.radius.xs,
-        },
-        "&[data-last-in-range]": {
-          borderTopRightRadius: theme.radius.xs,
-          borderBottomRightRadius: theme.radius.xs,
-        },
-        "&[data-selected]": {
-          color: theme.white,
-          backgroundColor: "var(--mb-color-background-brand)",
-          "&:hover": {
-            backgroundColor: "var(--mb-color-background-brand)",
-          },
-        },
-      },
-    }),
-  },
-  WeekdaysRow: {
-    styles: theme => ({
-      weekday: {
-        width: rem(40),
-        height: rem(32),
-        color: theme.fn.themeColor("text-light"),
-        fontSize: theme.fontSizes.sm,
-        lineHeight: rem(24),
-        textAlign: "center",
-        paddingBottom: 0,
-      },
-    }),
-  },
-  PickerControl: {
-    styles: theme => ({
-      pickerControl: {
-        color: theme.fn.themeColor("text-dark"),
-        fontSize: theme.fontSizes.md,
-        lineHeight: rem(24),
-        width: rem(80),
-        height: rem(32),
-        borderRadius: theme.radius.sm,
-
-        "&:hover": {
-          backgroundColor: theme.fn.themeColor("bg-light"),
-        },
-        "&[data-disabled]": {
-          color: theme.fn.themeColor("bg-dark"),
-        },
-        "&[data-weekend]": {
-          color: theme.fn.themeColor("text-dark"),
-        },
-        "&[data-outside]": {
-          color: theme.fn.themeColor("bg-dark"),
-        },
-        "&[data-in-range]": {
-          color: theme.fn.themeColor("text-medium"),
-          borderRadius: 0,
-          backgroundColor: theme.fn.themeColor("brand-lighter"),
-          "&:hover": {
-            backgroundColor: theme.fn.themeColor("brand-lighter"),
-          },
-        },
-      },
-    }),
-  },
-  Month: {
-    styles: () =>
-      getListStyles({
-        rowClass: "monthRow",
-        cellClass: "monthCell",
-        horizontalPadding: rem(1),
-        verticalPadding: rem(1),
-      }),
-  },
-  MonthsList: {
-    styles: theme =>
-      getListStyles({
-        rowClass: "monthsListRow",
-        cellClass: "monthsListCell",
-        horizontalPadding: theme.spacing.sm,
-        verticalPadding: theme.spacing.xs,
-      }),
-  },
-  YearsList: {
-    styles: theme =>
-      getListStyles({
-        rowClass: "yearsListRow",
-        cellClass: "yearsListCell",
-        horizontalPadding: theme.spacing.sm,
-        verticalPadding: theme.spacing.xs,
-      }),
-  },
-  CalendarHeader: {
-    styles: theme => ({
-      calendarHeader: {
-        marginBottom: 0,
-      },
-      calendarHeaderLevel: {
-        height: rem(32),
-        color: theme.fn.themeColor("text-dark"),
-        fontSize: theme.fontSizes.md,
-        fontWeight: "bold",
-        lineHeight: rem(24),
-
-        "&:hover": {
-          backgroundColor: theme.fn.themeColor("bg-light"),
-        },
-      },
-      calendarHeaderControl: {
-        width: rem(32),
-        height: rem(32),
-        borderRadius: theme.radius.xs,
-        color: theme.fn.themeColor("bg-dark"),
-        "&:hover": {
-          backgroundColor: theme.fn.themeColor("bg-light"),
-        },
-      },
-    }),
-  },
-  MonthLevel: {
-    styles: () => ({
-      calendarHeader: {
-        marginBottom: 0,
-      },
-    }),
-  },
-});
-
-interface ListStylesParams {
-  rowClass: string;
-  cellClass: string;
-  horizontalPadding: string;
-  verticalPadding: string;
-}
-
-const getListStyles = ({
-  rowClass,
-  cellClass,
-  horizontalPadding,
-  verticalPadding,
-}: ListStylesParams) => ({
-  [cellClass]: {
-    ref: getStylesRef(cellClass),
-
-    "&[data-with-spacing]": {
-      padding: 0,
-
-      "&:not(:first-of-type)": {
-        paddingLeft: horizontalPadding,
-      },
-      "&:not(:last-of-type)": {
-        paddingRight: horizontalPadding,
-      },
+  }),
+  Day: Day.extend({
+    classNames: {
+      day: Styles.day,
     },
-  },
-  [rowClass]: {
-    [`&:not(:first-of-type) .${getStylesRef(cellClass)}`]: {
-      paddingTop: verticalPadding,
+  }),
+  WeekdaysRow: WeekdaysRow.extend({
+    classNames: {
+      weekdaysRow: Styles.weekdaysRow,
     },
-    [`&:not(:last-of-type) .${getStylesRef("monthCell")}`]: {
-      paddingBottom: verticalPadding,
+  }),
+  PickerControl: PickerControl.extend({
+    classNames: {
+      pickerControl: Styles.pickerControl,
     },
-  },
-});
+  }),
+  Month: Month.extend({
+    classNames: {
+      month: Styles.month,
+      monthRow: Styles.row,
+      monthCell: Styles.cell,
+    },
+  }),
+  MonthsList: MonthsList.extend({
+    classNames: {
+      monthsList: Styles.monthsList,
+      monthsListRow: Styles.row,
+      monthsListCell: Styles.cell,
+    },
+  }),
+  YearsList: YearsList.extend({
+    classNames: {
+      yearsList: Styles.yearsList,
+      yearsListRow: Styles.row,
+      yearsListCell: Styles.cell,
+    },
+  }),
+  CalendarHeader: CalendarHeader.extend({
+    classNames: {
+      calendarHeader: Styles.calendarHeader,
+      calendarHeaderLevel: Styles.calendarHeaderLevel,
+      calendarHeaderControl: Styles.calendarHeaderControl,
+    },
+  }),
+  MonthLevel: MonthLevel.extend({
+    classNames: {
+      calendarHeader: Styles.calendarHeader,
+    },
+  }),
+};
