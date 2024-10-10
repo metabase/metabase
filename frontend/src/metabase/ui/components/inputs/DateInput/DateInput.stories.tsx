30dee89e5fa702edb53e02a6111d9ac127c18384
Ryan Laurie <iethree@gmail.com>
2024-10-10 16:07:34 -0600

    Date Input

--- frontend/src/metabase/ui/components/inputs/DateInput/DateInput.stories.tsx
+++ frontend/src/metabase/ui/components/inputs/DateInput/DateInput.stories.tsx
@@ -61,7 +61,7 @@ const VariantTemplate = (args: DateInputProps) => (
 );
 
 const IconTemplate = (args: DateInputProps) => (
-  <VariantTemplate {...args} icon={<Icon name="calendar" />} />
+  <VariantTemplate {...args} leftSection={<Icon name="calendar" />} />
 );
 
 export default {
