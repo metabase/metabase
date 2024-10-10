1cb448a2869372558ae8e7b38bb43aeeaf2a6f25
John Swanson <john.swanson@metabase.com>
2024-10-10 10:28:41 -0700

    fix a couple tests

--- test/metabase/util/autoplace_test.clj
+++ test/metabase/util/autoplace_test.clj
@@ -10,7 +10,7 @@
   (autoplace/get-position-for-new-dashcard cards test-card-width test-card-height test-grid-width))
 
 (defn- pos [m]
-  (merge {:x_size 2 :y_size 2 :dashboard_tab_id nil} m))
+  (merge {:size_x 2 :size_y 2 :dashboard_tab_id nil} m))
 
 (deftest get-position-for-new-dashcard-works
   (testing "place first card at 0,0"
@@ -37,11 +37,11 @@
                           (pos {:col 4 :row 0})]))))
   (testing "place card correctly with non-default sizes"
     (is (= (pos {:col 3 :row 2})
-           (get-position [(pos {:col 1 :row 0 :x_size 2 :y_size 4})
+           (get-position [(pos {:col 1 :row 0 :size_x 2 :size_y 4})
                           (pos {:col 4 :row 0})])))
     (is (= (pos {:col 0 :row 1})
-           (get-position [(pos {:col 0 :row 0 :x_size 3 :y_size 1})
-                          (pos {:col 4 :row 0 :x_size 2 :y_size 1})]))))
+           (get-position [(pos {:col 0 :row 0 :size_x 3 :size_y 1})
+                          (pos {:col 4 :row 0 :size_x 2 :size_y 1})]))))
   (testing "should not place card over the right edge of the grid"
     (is (= (pos {:col 0 :row 1})
-           (get-position [(pos {:col 0 :row 0 :x_size 5 :y_size 1})])))))
+           (get-position [(pos {:col 0 :row 0 :size_x 5 :size_y 1})])))))
