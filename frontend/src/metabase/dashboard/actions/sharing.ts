5045bed9dd025b45f1db7e624fe5f595d0b2e6ed
Oisin Coveney <oisin@metabase.com>
2024-10-10 11:43:34 +0300

    Use rtk hooks for public dashboards (#48513)

--- frontend/src/metabase/dashboard/actions/sharing.ts
+++ frontend/src/metabase/dashboard/actions/sharing.ts
@@ -1,18 +1,10 @@
-import {
-  createDashboardPublicLink,
-  deleteDashboardPublicLink,
-} from "metabase/api";
 import { SIDEBAR_NAME } from "metabase/dashboard/constants";
 import { createAsyncThunk } from "metabase/lib/redux";
 import { DashboardApi } from "metabase/services";
-import type { Dashboard, DashboardId } from "metabase-types/api";
+import type { Dashboard } from "metabase-types/api";
 import type { Dispatch } from "metabase-types/store";
 
 import { closeSidebar, setSidebar } from "./ui";
-
-type DashboardIdPayload = {
-  id: DashboardId;
-};
 
 export const setSharing = (isSharing: boolean) => (dispatch: Dispatch) => {
   if (isSharing) {
@@ -49,29 +41,4 @@ export const updateEmbeddingParams = createAsyncThunk<
   UPDATE_EMBEDDING_PARAMS,
   async ({ id, embedding_params }) =>
     await DashboardApi.update({ id, embedding_params }),
-);
-
-export const CREATE_PUBLIC_LINK = "metabase/dashboard/CREATE_PUBLIC_LINK";
-export const createPublicLink = createAsyncThunk(
-  CREATE_PUBLIC_LINK,
-  async ({ id }: DashboardIdPayload, { dispatch }) => {
-    const { data = null } = await dispatch(
-      createDashboardPublicLink.initiate({ id }),
-    );
-    return { id, uuid: data?.uuid };
-  },
-);
-
-export const DELETE_PUBLIC_LINK = "metabase/dashboard/DELETE_PUBLIC_LINK";
-export const deletePublicLink = createAsyncThunk(
-  DELETE_PUBLIC_LINK,
-  async ({ id }: DashboardIdPayload, { dispatch }) => {
-    await dispatch(
-      deleteDashboardPublicLink.initiate({
-        id,
-      }),
-    );
-
-    return { id };
-  },
 );
