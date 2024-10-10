ad59a648cce39d69a3adecc0e80d49a96fe7d4db
Sloan Sparger <sloansparger@gmail.com>
2024-10-10 17:01:46 -0500

    port ActionIcon

---
+++ frontend/src/metabase/ui/components/buttons/ActionIcon/ActionButton.stories.tsx
@@ -0,0 +1,47 @@
+import { Icon } from "../../";
+
+import { ActionIcon, type ActionIconProps } from "./";
+
+export default {
+  title: "Buttons/ActionIcon",
+  component: ActionIcon,
+  argTypes: {
+    color: {
+      control: {
+        type: "select",
+        options: ["brand"],
+      },
+    },
+    disabled: {
+      constrol: {
+        type: "boolean",
+      },
+    },
+    size: {
+      control: {
+        type: "select",
+        options: ["xs", "sm", "md", "lg", "xl"],
+      },
+    },
+    variant: {
+      control: {
+        type: "select",
+        options: ["filled", "subtle", "viewFooter", "viewHeader"],
+      },
+    },
+  },
+};
+
+export const Default = {
+  render: (args: ActionIconProps) => (
+    <ActionIcon {...args}>
+      <Icon name="ai" />
+    </ActionIcon>
+  ),
+  args: {
+    color: "brand",
+    size: "md",
+    variant: "filled",
+    disabled: false,
+  },
+};
