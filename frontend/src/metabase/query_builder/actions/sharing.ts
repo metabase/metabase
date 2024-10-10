6959f3e969873d1d61c4435bb8916267e06857fa
Oisin Coveney <oisin@metabase.com>
2024-10-10 14:10:46 +0300

    Fix types on dashboard API, and add types for dashboard redux (#48399)

--- frontend/src/metabase/query_builder/actions/sharing.ts
+++ frontend/src/metabase/query_builder/actions/sharing.ts
@@ -1,47 +1,26 @@
-import { createAction } from "redux-actions";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import { createCardPublicLink, deleteCardPublicLink } from "metabase/api";
-import { createThunkAction } from "metabase/lib/redux";
+import type { EmbeddingParameters } from "metabase/public/lib/types";
 import { CardApi } from "metabase/services";
-import type { Card } from "metabase-types/api";
-import type { EmbedOptions } from "metabase-types/store";
+import type { Card, CardId } from "metabase-types/api";
 
-type CardIdPayload = Pick<Card, "id">;
+export const updateEnableEmbedding = createAsyncThunk<
+  Pick<Card, "id" | "enable_embedding"> & { uuid: Card["public_uuid"] },
+  Pick<Card, "id" | "enable_embedding">
+>("metabase/card/UPDATE_ENABLE_EMBEDDING", async ({ id, enable_embedding }) => {
+  const response = await CardApi.update({
+    id,
+    enable_embedding,
+  });
 
-export const CREATE_PUBLIC_LINK = "metabase/card/CREATE_PUBLIC_LINK";
+  return { id, uuid: response.uuid, enable_embedding };
+});
 
-export const createPublicLink = createThunkAction(
-  CREATE_PUBLIC_LINK,
-  ({ id }: Card) =>
-    async dispatch => {
-      const { data } = await (dispatch(
-        createCardPublicLink.initiate({ id }),
-      ) as Promise<{ data: { uuid: string }; error: unknown }>);
-      return { id, uuid: data.uuid };
-    },
-);
-
-export const DELETE_PUBLIC_LINK = "metabase/card/DELETE_PUBLIC_LINK";
-
-export const deletePublicLink = createThunkAction(
-  DELETE_PUBLIC_LINK,
-  (card: Card) => async dispatch =>
-    await dispatch(deleteCardPublicLink.initiate(card)),
-);
-
-export const UPDATE_ENABLE_EMBEDDING = "metabase/card/UPDATE_ENABLE_EMBEDDING";
-export const updateEnableEmbedding = createAction(
-  UPDATE_ENABLE_EMBEDDING,
-  ({ id }: CardIdPayload, enable_embedding: boolean) =>
-    CardApi.update({
-      id,
-      enable_embedding,
-    }),
-);
-
-export const UPDATE_EMBEDDING_PARAMS = "metabase/card/UPDATE_EMBEDDING_PARAMS";
-export const updateEmbeddingParams = createAction(
-  UPDATE_EMBEDDING_PARAMS,
-  ({ id }: CardIdPayload, embedding_params: EmbedOptions) =>
-    CardApi.update({ id, embedding_params }),
+export const updateEmbeddingParams = createAsyncThunk<
+  Card,
+  { id: CardId; embedding_params: EmbeddingParameters }
+>(
+  "metabase/card/UPDATE_EMBEDDING_PARAMS",
+  async ({ id, embedding_params }) =>
+    await CardApi.update({ id, embedding_params }),
 );
