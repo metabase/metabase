88e41e7fa6d68774f05cbbe10f9714ae33fd8d87
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-10-11 15:50:26 +0300

    js -> ts

--- frontend/src/metabase/query_builder/actions/core/card.ts
+++ frontend/src/metabase/query_builder/actions/core/card.ts
@@ -1,7 +1,12 @@
 import Questions from "metabase/entities/questions";
+import type { CardId } from "metabase-types/api";
+import type { Dispatch, GetState } from "metabase-types/store";
 
 // load a card either by ID or from a base64 serialization.  if both are present then they are merged, which the serialized version taking precedence
-export async function loadCard(cardId, { dispatch, getState }) {
+export async function loadCard(
+  cardId: CardId,
+  { dispatch, getState }: { dispatch: Dispatch; getState: GetState },
+) {
   try {
     await dispatch(
       Questions.actions.fetch(
