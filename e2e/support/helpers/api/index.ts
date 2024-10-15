e09354fd8de3508adbcb36f35d03b76dcd5db495
Kamil Mielnik <kamil@kamilmielnik.com>
2024-10-15 21:36:25 +0700

    Backport #48672 - Refactor remaining e2e API commands to TS helper functions (v50) (#48724)
    * Refactor remaining e2e API commands to TS helper functions (#48672)
    
    * Refactor createDashboardWithQuestions, createQuestionAndAddToDashboard and editDashboardCard to TS function helpers
    
    * Fix createNativeQuestionAndDashboard that got out of sync
    
    * Add missing export
    
    * Fix import

--- e2e/support/helpers/api/index.ts
+++ e2e/support/helpers/api/index.ts
@@ -7,9 +7,11 @@ export { createApiKey } from "./createApiKey";
 export { createCollection } from "./createCollection";
 export { createDashboard } from "./createDashboard";
 export type { DashboardDetails } from "./createDashboard";
+export { createDashboardWithQuestions } from "./createDashboardWithQuestions";
 export { createDashboardWithTabs } from "./createDashboardWithTabs";
 export { createModerationReview } from "./createModerationReview";
 export { createNativeQuestion } from "./createNativeQuestion";
+export { createNativeQuestionAndDashboard } from "./createNativeQuestionAndDashboard";
 export type { NativeQuestionDetails } from "./createNativeQuestion";
 export { createPulse } from "./createPulse";
 export { createQuestion } from "./createQuestion";
@@ -17,10 +19,12 @@ export type {
   QuestionDetails,
   StructuredQuestionDetails,
 } from "./createQuestion";
+export { createQuestionAndAddToDashboard } from "./createQuestionAndAddToDashboard";
 export { createQuestionAndDashboard } from "./createQuestionAndDashboard";
 export { createTimeline } from "./createTimeline";
 export { createTimelineEvent } from "./createTimelineEvent";
 export { createTimelineWithEvents } from "./createTimelineWithEvents";
+export { editDashboardCard } from "./editDashboardCard";
 export { getCurrentUser } from "./getCurrentUser";
 export { remapDisplayValueToFK } from "./remapDisplayValueToFK";
 export { updateDashboardCards } from "./updateDashboardCards";
