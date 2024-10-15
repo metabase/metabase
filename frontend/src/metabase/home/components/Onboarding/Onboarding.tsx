c9ad94ad7a2e4a336dc3205018ce49d7b5fb5fa6
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-10-15 15:45:25 +0200

    Add first pass of the page structure

--- frontend/src/metabase/home/components/Onboarding/Onboarding.tsx
+++ frontend/src/metabase/home/components/Onboarding/Onboarding.tsx
@@ -1,3 +1,295 @@
+import { jt, t } from "ttag";
+
+import { getPlan } from "metabase/common/utils/plan";
+import ExternalLink from "metabase/core/components/ExternalLink";
+import Link from "metabase/core/components/Link";
+import { useSelector } from "metabase/lib/redux";
+import * as Urls from "metabase/lib/urls";
+import { getSetting } from "metabase/selectors/settings";
+import {
+  getApplicationName,
+  getShowMetabaseLinks,
+} from "metabase/selectors/whitelabel";
+import {
+  Accordion,
+  Box,
+  Button,
+  Group,
+  Icon,
+  Stack,
+  Text,
+  Title,
+  rem,
+} from "metabase/ui";
+
+import S from "./Onboarding.module.css";
+
 export const Onboarding = () => {
-  return <h1>"Hello, world!"</h1>;
+  const applicationName = useSelector(getApplicationName);
+  const showMetabaseLinks = useSelector(getShowMetabaseLinks);
+  const plan = useSelector(state =>
+    getPlan(getSetting(state, "token-features")),
+  );
+
+  const shouldConfigureCommunicationChannels =
+    plan === "oss" || plan === "pro-self-hosted";
+
+  const newQuestionUrl = Urls.newQuestion({
+    mode: "notebook",
+    creationType: "custom_question",
+    collectionId: "root",
+    cardType: "question",
+  });
+
+  const lastUsedDatabaseId = useSelector(state =>
+    getSetting(state, "last-used-native-database-id"),
+  );
+
+  const newNativeQuestionUrl = Urls.newQuestion({
+    type: "native",
+    creationType: "native_question",
+    collectionId: "root",
+    cardType: "question",
+    databaseId: lastUsedDatabaseId || undefined,
+  });
+
+  return (
+    <Box px={rem(48)} py="xl" bg="var(--mb-base-color-white)" mih="100%">
+      <Box maw={592}>
+        <Accordion
+          defaultValue="set-up"
+          classNames={{
+            item: S.item,
+            content: S.content,
+            control: S.control,
+            label: S.label,
+            icon: S.icon,
+          }}
+        >
+          <Box mb={64}>
+            <Title order={2} mb={24}>{t`Set up your ${applicationName}`}</Title>
+            <Accordion.Item value="set-up">
+              <Accordion.Control icon={<Icon name="add_data" />}>
+                {t`Connect to your database`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  <Text>
+                    {t`You can connect multiple databases, and query them directly with the query builder or the Native/SQL editor.`}
+                  </Text>
+                  <Link to="/admin/databases/create">
+                    <Button variant="outline">{t`Add Database`}</Button>
+                  </Link>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+            <Accordion.Item value="invite">
+              <Accordion.Control icon={<Icon name="group" />}>
+                {t`Invite people`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  {/* TODO: different copy for different plans? */}
+                  <Text>{t`Don't be shy with the invites.`}</Text>
+
+                  <Group spacing={0}>
+                    <Link to="/admin/people">
+                      <Button variant="outline">{t`Invite people`}</Button>
+                    </Link>
+                    <Link to="/admin/settings/authentication">
+                      <Button variant="subtle">{t`Set up Single Sign-on`}</Button>
+                    </Link>
+                  </Group>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Box>
+
+          <Box mb={64}>
+            <Title order={2} mb={24}>{t`Start visualizing your data`}</Title>
+            <Accordion.Item value="x-rays">
+              <Accordion.Control icon={<Icon name="bolt" />}>
+                {t`Create automatic dashboards`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  <Text>
+                    {jt`Hover over a table and click the yellow lightning bolt ${(
+                      <Icon
+                        name="bolt_filled"
+                        size={14}
+                        c="var(--mb-color-warning)"
+                        className={S.inlineIcon}
+                      />
+                    )}. ${applicationName} will create a bunch of charts based on that data and arrange them on a dashboard.`}
+                  </Text>
+                  <Link to="/browse/databases">
+                    <Button variant="outline">{t`Browse data`}</Button>
+                  </Link>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+            <Accordion.Item value="notebook">
+              <Accordion.Control icon={<Icon name="notebook" />}>
+                {t`Make an interactive chart with the query builder`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  <Text>
+                    {jt`Filter and summarize data, add custom columns, join data from other tables, and more - ${(<b>{t`all without SQL`}</b>)}. And when you build a chart with the query builder, people will be able to ${(
+                      <b>{t`drill-through the chart`}</b>
+                    )} to explore the data further.`}
+                  </Text>
+                  <Link to={newQuestionUrl}>
+                    <Button variant="outline">{t`New question`}</Button>
+                  </Link>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item value="sql">
+              <Accordion.Control icon={<Icon name="sql" />}>
+                {t`Query with SQL`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  <Text>
+                    {jt`Use the ${(<b>{t`native query editor`}</b>)} to query data with SQL or the query language native to your database. You can insert variables in your code to create ${
+                      showMetabaseLinks ? (
+                        <ExternalLink href="https://www.metabase.com/docs/latest/questions/native-editor/sql-parameters">{t`SQL templates`}</ExternalLink>
+                      ) : (
+                        t`SQL templates`
+                      )
+                    }, and reference the results of models or other saved question in your code.`}
+                  </Text>
+                  <Link to={newNativeQuestionUrl}>
+                    <Button variant="outline">{t`New native query`}</Button>
+                  </Link>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+            <Accordion.Item value="dashboard">
+              <Accordion.Control icon={<Icon name="dashboard" />}>
+                {t`Create and share a dashboard`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  <Text>
+                    {jt`You can organize questions into a ${
+                      showMetabaseLinks ? (
+                        <ExternalLink href="https://www.metabase.com/docs/latest/dashboards/introduction#dashboard-tabs">{t`dashboard with tabs`}</ExternalLink>
+                      ) : (
+                        t`dashboard with tabs`
+                      )
+                    } and add text cards.`}
+                  </Text>
+                  <VideoPlaceholder />
+                  <Text>
+                    {jt`You can add ${(<b>{t`filters`}</b>)} to dashboards and connect them to fields on questions to narrow the results.`}
+                  </Text>
+                  <VideoPlaceholder />
+                  <Text>
+                    {t`You can drill-through your dashboard and charts to see more detailed data underneath.`}
+                  </Text>
+                  <Link to="/dashboard/1">
+                    <Button variant="outline">{t`Edit a sample dashboard`}</Button>
+                  </Link>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Box>
+          <Box mb={64}>
+            <Title order={2} mb={24}>{t`Get email updates and alerts`}</Title>
+            <Accordion.Item value="subscription">
+              <Accordion.Control icon={<Icon name="subscription" />}>
+                {t`Get dashboard updates by email`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  {shouldConfigureCommunicationChannels && (
+                    <Text>
+                      {jt`${(<Link to="/admin/settings/email/smtp">{t`Set up email`}</Link>)} or ${(<Link to="/admin/settings/notifications">{t`Slack`}</Link>)} first.`}
+                    </Text>
+                  )}
+                  <Text>
+                    {jt`To set up a subscription to a dashboard, click on the ${(<Icon name="subscription" className={S.inlineIcon} />)} ${(<i>{t`subscriptions`}</i>)} icon on the top bar. On a sidebar on the right set up a dashboard subscription via email or Slack.`}
+                  </Text>
+                  <Link to="/dashboard/1">
+                    <Button variant="outline">{t`Set up subscriptions for a sample dashboard`}</Button>
+                  </Link>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+            <Accordion.Item value="alert">
+              <Accordion.Control icon={<Icon name="alert" />}>
+                {t`Get alerts when metrics behave unexpectedly`}
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack spacing="lg">
+                  <VideoPlaceholder />
+                  {shouldConfigureCommunicationChannels && (
+                    <Text>
+                      {jt`${(<Link to="/admin/settings/email/smtp">{t`Set up email`}</Link>)} or ${(<Link to="/admin/settings/notifications">{t`Slack`}</Link>)} first.`}
+                    </Text>
+                  )}
+                  <Text>
+                    {jt`Go to a question and click on the ${(<Icon name="alert" className={S.inlineIcon} />)} ${(<i>{t`bell`}</i>)} icon in the bottom right of the screen.`}
+                  </Text>
+                  <Text>
+                    {t`There are three kinds of things you can get alerted about in ${applicationName}:`}
+                    <ul className={S.list}>
+                      <li>{jt`${
+                        showMetabaseLinks ? (
+                          <ExternalLink href="https://www.metabase.com/docs/latest/questions/sharing/alerts#goal-line-alerts">{t`Goal line alerts`}</ExternalLink>
+                        ) : (
+                          t`Goal line alerts`
+                        )
+                      }: when a time series crosses a goal line.`}</li>
+                      <li>{jt`${
+                        showMetabaseLinks ? (
+                          <ExternalLink href="https://www.metabase.com/docs/latest/questions/sharing/alerts#progress-bar-alerts">{t`Progress bar alerts`}</ExternalLink>
+                        ) : (
+                          t`Progress bar alerts`
+                        )
+                      }: when a progress bar reaches or goes below its goal.`}</li>
+                      <li>{jt`${
+                        showMetabaseLinks ? (
+                          <ExternalLink href="https://www.metabase.com/docs/latest/questions/sharing/alerts#results-alerts">{t`Results alerts`}</ExternalLink>
+                        ) : (
+                          t`Results alerts`
+                        )
+                      }: when a question returns any result.`}</li>
+                    </ul>
+                  </Text>
+                  <Link to="/question/12">
+                    <Button variant="outline">{t`Set up alert for a sample question`}</Button>
+                  </Link>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Box>
+        </Accordion>
+      </Box>
+    </Box>
+  );
 };
+
+function VideoPlaceholder() {
+  return (
+    <Box
+      bg="aliceblue"
+      w="100%"
+      style={{
+        aspectRatio: "16/9",
+        borderRadius: 8,
+      }}
+    ></Box>
+  );
+}
