6e9748350eefcbea1d3692a3d5364714bee19674
Kamil Mielnik <kamil@kamilmielnik.com>
2024-10-15 17:22:47 +0700

    Refactor remaining e2e API commands to TS helper functions (#48672)
    * Refactor createDashboardWithQuestions, createQuestionAndAddToDashboard and editDashboardCard to TS function helpers
    
    * Fix createNativeQuestionAndDashboard that got out of sync

--- e2e/support/helpers/api/index.ts
+++ e2e/support/helpers/api/index.ts
@@ -7,6 +7,7 @@ export { createApiKey } from "./createApiKey";
 export { createCollection } from "./createCollection";
 export { createDashboard } from "./createDashboard";
 export type { DashboardDetails } from "./createDashboard";
+export { createDashboardWithQuestions } from "./createDashboardWithQuestions";
 export { createDashboardWithTabs } from "./createDashboardWithTabs";
 export { createModerationReview } from "./createModerationReview";
 export { createNativeQuestion } from "./createNativeQuestion";
@@ -18,10 +19,12 @@ export type {
   QuestionDetails,
   StructuredQuestionDetails,
 } from "./createQuestion";
+export { createQuestionAndAddToDashboard } from "./createQuestionAndAddToDashboard";
 export { createQuestionAndDashboard } from "./createQuestionAndDashboard";
 export { createTimeline } from "./createTimeline";
 export { createTimelineEvent } from "./createTimelineEvent";
 export { createTimelineWithEvents } from "./createTimelineWithEvents";
+export { editDashboardCard } from "./editDashboardCard";
 export { getCurrentUser } from "./getCurrentUser";
 export { remapDisplayValueToFK } from "./remapDisplayValueToFK";
 export { updateDashboardCards } from "./updateDashboardCards";
