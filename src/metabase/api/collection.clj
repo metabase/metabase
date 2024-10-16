f8e7cbebc604810e4c370c3ef42e3f77144f8b76
eskrm <eskrm@pm.me>
2024-10-16 15:56:11 -0700

    FAI-7404: Do not allow users to modify custom reports collections

--- src/metabase/api/collection.clj
+++ src/metabase/api/collection.clj
@@ -1168,6 +1168,17 @@
     :parent_id (move-collection! collection-before-update collection-updates)
     :no-op))
 
+(defn- is-custom-reports-collection
+  "Check if the collection is a custom reports collection for a pipeline."
+  [collection]
+  (if (= (:name collection) "Custom reports")
+    (let [ancestors (some->> (collection/location-path->ids (:location collection))
+                             (map #(:name (t2/select-one Collection :id %))))]
+      (and (= (count ancestors) 4)
+           (= (take 2 ancestors) ["Aion", "Pipelines"])
+           (= (last ancestors) "Reports")))
+    false))
+
 (api/defendpoint PUT "/:id"
   "Modify an existing Collection, including archiving or unarchiving it, or moving it."
   [id, :as {{:keys [name description archived parent_id authority_level], :as collection-updates} :body}]
@@ -1184,6 +1195,9 @@
                (not= (keyword authority_level) (:authority_level collection-before-update)))
       (premium-features/assert-has-feature :official-collections (tru "Official Collections"))
       (api/check-403 api/*is-superuser?*))
+    ;; faros: block regular users from editing the custom reports collection
+    (when (not api/*is-superuser?*)
+      (api/check-403 (not (is-custom-reports-collection collection-before-update))))
     ;; ok, go ahead and update it! Only update keys that were specified in the `body`. But not `parent_id` since
     ;; that's not actually a property of Collection, and since we handle moving a Collection separately below.
     (let [updates (u/select-keys-when collection-updates :present [:name :description :authority_level])]
