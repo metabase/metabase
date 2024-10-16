a7a6a31d1b9aef14f1249c8f80e5cc95454ced11
Phoomparin Mano <phoomparin@gmail.com>
2024-10-16 23:58:21 +0700

    cancel request on unmount in static question

--- enterprise/frontend/src/embedding-sdk/lib/load-static-question.ts
+++ enterprise/frontend/src/embedding-sdk/lib/load-static-question.ts
@@ -1,24 +1,29 @@
+import type { Deferred } from "metabase/lib/promise";
 import { CardApi } from "metabase/services";
 import type { Card, Dataset, ParameterQueryObject } from "metabase-types/api";
 
 interface Options {
   questionId: number;
   parameterValues?: Record<string, string | number>;
+  cancelDeferred?: Deferred;
 }
 
 type ParameterQueryInput = { id: string } & ParameterQueryObject;
 
+// @ts-expect-error - foo bar
+window.CardApi = CardApi;
+
 export async function loadStaticQuestion(options: Options) {
-  const { questionId, parameterValues } = options;
+  const { questionId, parameterValues, cancelDeferred: cancelled } = options;
 
   let card: Card | null;
   let result: Dataset | null;
 
   [card, result] = await Promise.all([
-    CardApi.get({ cardId: questionId }),
+    CardApi.get({ cardId: questionId }, { cancelled }),
 
     // Query the card in parallel when no parameters are provided.
-    !parameterValues && CardApi.query({ cardId: questionId }),
+    !parameterValues && CardApi.query({ cardId: questionId }, { cancelled }),
   ]);
 
   if (parameterValues && card?.parameters) {
@@ -31,10 +36,13 @@ export async function loadStaticQuestion(options: Options) {
         value: parameterValues[parameter.slug],
       }));
 
-    result = await CardApi.query({
-      cardId: questionId,
-      parameters,
-    });
+    result = await CardApi.query(
+      {
+        cardId: questionId,
+        parameters,
+      },
+      { cancelled },
+    );
   }
 
   return { card, result };
