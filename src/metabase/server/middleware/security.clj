d850922e45f9f4092ccc208eecf091789c6a9b8a
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-10-16 12:27:10 -0400

    add allowed iframe host setting wip

--- src/metabase/server/middleware/security.clj
+++ src/metabase/server/middleware/security.clj
@@ -93,7 +93,7 @@
                                  (when config/is-dev?
                                    "http://localhost:9630")
                                  "https://accounts.google.com"]
-                  :frame-src     ["*"]
+                  :frame-src    ["*"]
                   :font-src     ["*"]
                   :img-src      ["*"
                                  "'self' data:"]
