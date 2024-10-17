4f703c35597f5e2da7409e9ebc4fa829c941169a
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-10-17 10:43:38 -0400

    Add a new prometheus metrics in m.qp.middleware.metrics/adjust (#48598) (#48811)
    * Add new promethues counter for :metabase-metrics/adjust-errors
    
    To record errors in metabase.query-processor.middleware.metrics/adjust
    
    * Add prometheus metrics test to metrics middleware tests
    
    * Also inc :metabase-metrics/adjust-errors when throwing an exception
    
    * Make splice-compatible-metrics private
    
    It doesn't seem to be used outside of metabase.query-processor.middleware.metrics.
    
    * Add test for :metabase-metrics/adjust-errors when exception is thrown
    
    * Move inc-and-throw! helper into metabase.analytics.prometheus
    
    * Add inc-and-throw! test to prometheus tests
    
    * Rename inc-test to inc!-test and use approx= and metric-value helpers
    
    * Add try/catch to metrics adjust and record metric errors in a single place
    
    Record promethues metrics in a single place in metabase.query-processor.middleware.metrics and ensure we record
    failures for any exception throw.
    
    * Improve testing description string
    
    * Rename :metabase-metrics/adjust-errors to :metabase-query-processor/metrics-errors
    
    * Move metrics adjustment try/catch into a new helper
    
    This ensures we count all exceptions generated by adjust-metric-stages.
    
    * Add new prometheus metric to count number of MB metrics seen by the QP
    
    This new metric is intended to count the number of metrics that the query processor metrics middleware sees and
    attempts to adjust.
    
    * Add more metrics metrics tests
    
    - Ensure that queries with no metrics do not incremement either counter
    - Ensure that successful calls to adjust do not increment the error counter
    - Ensure that exceptions throw from other libs also increment the error counter
    
    * Rename test to adjust-prometheus-metrics-test and add helper to reduce code dup
    
    * If no metrics are found immediately return query in metrics middleware adjust
    
    Don't attempt to adjust query if we don't find any :metric clauses.
    
    * Appease cljfmt
    
    * Improve metric description
    
    
    
    * Improve epsilon test in approx=
    
    
    
    * Remove adjust-metric-stages-counting-errors
    
    This function was an attempt to avoid double-counting errors, but probably not worth the effort.
    
    See
    
    https://github.com/metabase/metabase/pull/48598#discussion_r1800197964
    https://github.com/metabase/metabase/pull/48598#discussion_r1800200421
    
    * Count metrics directly when mocking prometheus/inc!
    
    Based on PR feedback here:
    
    https://github.com/metabase/metabase/pull/48598#discussion_r1800207674
    
    * Remove prometheus/inc-and-throw!
    
    Value prop was always questionable, but now that it's only called from a single place, just remove it and inline the
    call to prometheus/inc!. Easy to add back later if it proves generally useful.
    
    PR discussion thread
    
    https://github.com/metabase/metabase/pull/48598#discussion_r1799728596
    
    * Rename match-one-metric to find-first-metric
    
    https://github.com/metabase/metabase/pull/48598#discussion_r1803321025
    
    * Rename :metabase-query-processor/metrics-errors to metric-errors
    
    https://github.com/metabase/metabase/pull/48598#discussion_r1803313818
    
    * Throw an exception rather than logging a warning if we fail to adjust a metric
    
    https://github.com/metabase/metabase/pull/48598#discussion_r1803328482
    
    ---------
    
    Co-authored-by: appleby <86076+appleby@users.noreply.github.com>
    Co-authored-by: metamben <103100869+metamben@users.noreply.github.com>

--- test/metabase/query_processor/middleware/metrics_test.clj
+++ test/metabase/query_processor/middleware/metrics_test.clj
@@ -3,6 +3,7 @@
    [clojure.test :refer [deftest is testing]]
    [mb.hawk.assert-exprs.approximately-equal :as =?]
    [medley.core :as m]
+   [metabase.analytics.prometheus :as prometheus]
    [metabase.lib.convert :as lib.convert]
    [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
@@ -59,6 +60,76 @@
 
 (def adjust
   (comp #'metrics/adjust #'fetch-source-query/resolve-source-cards))
+
+(defn- check-prometheus-metrics!
+  [& {expected-metrics-count  :metabase-query-processor/metrics
+      expected-metrics-errors :metabase-query-processor/metric-errors
+      metric-and-mp           :metric-and-mp
+      query-fn                :query-fn
+      check-fn                :check-fn}]
+  (let [[source-metric mp] (or metric-and-mp (mock-metric))
+        query              (if query-fn
+                             (query-fn mp source-metric)
+                             (lib/query mp source-metric))
+        metrics              (atom {})
+        read-metric          #(% @metrics 0)]
+    (with-redefs [prometheus/inc! #(swap! metrics update % (fnil inc 0))]
+      (check-fn query)
+      (is (= expected-metrics-count (read-metric :metabase-query-processor/metrics)))
+      (is (= expected-metrics-errors (read-metric :metabase-query-processor/metric-errors))))))
+
+(deftest adjust-prometheus-metrics-test
+  (testing "adjustment of query with no metrics does not increment either counter"
+    (check-prometheus-metrics!
+     :metabase-query-processor/metrics 0
+     :metabase-query-processor/metric-errors 0
+     :query-fn (fn [_mp _metric]
+                 (-> (lib/query meta/metadata-provider (meta/table-metadata :products))
+                     (lib/aggregate (lib/avg (meta/field-metadata :products :rating)))))
+     :check-fn #(is (=? {:stages [{:source-table (meta/id :products)
+                                   :aggregation  [[:avg {} [:field {} (meta/id :products :rating)]]]}]}
+                        (adjust %)))))
+  (testing "successful adjustment does not increment error counter"
+    (check-prometheus-metrics!
+     :metabase-query-processor/metrics 1
+     :metabase-query-processor/metric-errors 0
+     :check-fn #(is (=? {:stages [{:source-table (meta/id :products)
+                                   :aggregation  [[:avg {} [:field {} (meta/id :products :rating)]]]}]}
+                        (adjust %)))))
+  (testing "failure to adjust :metric clauses increments error counter"
+    (check-prometheus-metrics!
+     :metabase-query-processor/metrics 1
+     :metabase-query-processor/metric-errors 1
+     :check-fn (fn [query]
+                 (with-redefs [metrics/adjust-metric-stages (fn [_ _ stages] stages)]
+                   (is (thrown-with-msg?
+                        clojure.lang.ExceptionInfo
+                        #"Failed to replace metric"
+                        (adjust query)))))))
+  (testing "exceptions from other libs also increment error counter"
+    (check-prometheus-metrics!
+     :metabase-query-processor/metrics 1
+     :metabase-query-processor/metric-errors 1
+     :check-fn (fn [query]
+                 (with-redefs [lib.metadata/bulk-metadata-or-throw (fn [& _] (throw (Exception. "Test exception")))]
+                   (is (thrown-with-msg?
+                        java.lang.Exception
+                        #"Test exception"
+                        (adjust query)))))))
+  (testing "metric missing aggregation increments counter and throws exception"
+    (check-prometheus-metrics!
+     :metabase-query-processor/metrics 1
+     :metabase-query-processor/metric-errors 1
+     :metric-and-mp (mock-metric (-> (lib/query meta/metadata-provider (meta/table-metadata :products))))
+     :query-fn (fn [mp metric]
+                 (-> (lib/query mp (meta/table-metadata :products))
+                     (lib/aggregate (lib/+ (lib.options/ensure-uuid
+                                            [:metric {} (:id metric)]) 1))))
+     :check-fn (fn [query]
+                 (is (thrown-with-msg?
+                      clojure.lang.ExceptionInfo
+                      #"Source metric missing aggregation"
+                      (adjust query)))))))
 
 (deftest ^:parallel no-metric-should-result-in-exact-same-query
   (let [query (lib/query meta/metadata-provider (meta/table-metadata :products))]
