10f87bde9fd692011ec159ddc03acef7675e097a
Romeo Van Snick <romeo@romeovansnick.be>
2024-10-17 17:27:20 +0200

    Allow passing utm to docs url

--- frontend/src/metabase/common/hooks/use-docs-url/use-docs-url.ts
+++ frontend/src/metabase/common/hooks/use-docs-url/use-docs-url.ts
@@ -1,10 +1,27 @@
 import { useSelector } from "metabase/lib/redux";
-import { getDocsUrl } from "metabase/selectors/settings";
+import { type UtmProps, getDocsUrl } from "metabase/selectors/settings";
 import { getShowMetabaseLinks } from "metabase/selectors/whitelabel";
 
-export const useDocsUrl = (page: string, anchor?: string) => {
+type DocsUtmProps = Omit<UtmProps, "utm_medium"> & {
+  utm_medium?: UtmProps["utm_medium"];
+};
+
+export const useDocsUrl = (
+  page: string,
+  anchor?: string,
+  utm?: DocsUtmProps,
+) => {
   const showMetabaseLinks = useSelector(getShowMetabaseLinks);
-  const url = useSelector(state => getDocsUrl(state, { page, anchor }));
+  const url = useSelector(state =>
+    getDocsUrl(state, {
+      page,
+      anchor,
+      utm: utm && {
+        ...utm,
+        utm_medium: utm.utm_medium ?? "docs",
+      },
+    }),
+  );
 
   return { url, showMetabaseLinks };
 };
