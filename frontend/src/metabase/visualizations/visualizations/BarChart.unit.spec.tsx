6a0b46e54dac7591497a096520271c5ded5d4f9b
Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>
2024-10-18 20:48:10 +0300

    rework ChartSettings to typescript (#48877)
    * rework ChartSettings to typescript
    
    * lint

--- frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
+++ frontend/src/metabase/visualizations/visualizations/BarChart.unit.spec.tsx
@@ -1,6 +1,6 @@
 import { createMockMetadata } from "__support__/metadata";
 import { renderWithProviders, screen } from "__support__/ui";
-import ChartSettings from "metabase/visualizations/components/ChartSettings";
+import { ChartSettings } from "metabase/visualizations/components/ChartSettings";
 import registerVisualizations from "metabase/visualizations/register";
 import Question from "metabase-lib/v1/Question";
 import type { Series } from "metabase-types/api";
