4c99a4250f78e3fd2edd1904d422c9213b401aff
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-10-21 14:54:51 -0300

    Adding moderation status to Dashboards

--- enterprise/frontend/src/metabase-enterprise/moderation/utils.ts
+++ enterprise/frontend/src/metabase-enterprise/moderation/utils.ts
@@ -1,9 +1,16 @@
 import { t } from "ttag";
 
 import type Question from "metabase-lib/v1/Question";
+import { Dashboard, entityIsDashboard } from "metabase-types/api";
 
-export const getVerifyQuestionTitle = (question: Question): string => {
-  const type = question.type();
+export const getVerifyQuestionTitle = (
+  question: Question | Dashboard,
+): string => {
+  const type = entityIsDashboard(question) ? "dashboard" : question.type();
+
+  if (type === "dashboard") {
+    return t`Verify this dashboard`;
+  }
 
   if (type === "question") {
     return t`Verify this question`;
