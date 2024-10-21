9c71ce697a11c3af1b3386310f40cd22787b0ed2
John Swanson <john.swanson@metabase.com>
2024-10-21 14:15:05 -0700

    Playing around with other tools

---
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/who_is_your_favorite.clj
@@ -0,0 +1,9 @@
+(ns metabase-enterprise.metabot-v3.tools.who-is-your-favorite
+  (:require
+   [metabase-enterprise.metabot-v3.reactions :as metabot-v3.reactions]
+   [metabase-enterprise.metabot-v3.tools.interface :as metabot-v3.tools.interface]
+   [metabase.util.malli :as mu]))
+
+(mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/who-is-your-favorite :- [:sequential ::metabot-v3.reactions/reaction]
+  [_tool-name _arg-map]
+  [{:type  :metabot.reaction/your-favorite}])
