620a2e60528acf028352f3316b6228838942a229
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-10-22 12:36:40 -0400

    On master, there is a generic 'retire' action list and modal that originally let users retire segments and metrics. Since it now just retires metrics, I've simplified the code and renamed the files accordingly.

--- frontend/src/metabase/admin/datamodel/components/SegmentActionSelect.jsx
+++ frontend/src/metabase/admin/datamodel/components/SegmentActionSelect.jsx
@@ -7,13 +7,12 @@ import ModalWithTrigger from "metabase/components/ModalWithTrigger";
 import PopoverWithTrigger from "metabase/components/PopoverWithTrigger";
 import AdminS from "metabase/css/admin.module.css";
 import CS from "metabase/css/core/index.css";
-import { capitalize } from "metabase/lib/formatting";
 import { Icon } from "metabase/ui";
 
-import { ActionLink, TriggerIconContainer } from "./ObjectActionSelect.styled";
-import ObjectRetireModal from "./ObjectRetireModal";
+import { ActionLink, TriggerIconContainer } from "./SegmentActionSelect.styled";
+import SegmentRetireModal from "./SegmentRetireModal";
 
-export default class ObjectActionsSelect extends Component {
+export default class SegmentActionSelect extends Component {
   constructor(props) {
     super(props);
 
@@ -22,8 +21,6 @@ export default class ObjectActionsSelect extends Component {
 
   static propTypes = {
     object: PropTypes.object.isRequired,
-    objectType: PropTypes.string.isRequired,
-    objectTypeLocalized: PropTypes.string.isRequired,
     onRetire: PropTypes.func.isRequired,
   };
 
@@ -33,7 +30,7 @@ export default class ObjectActionsSelect extends Component {
   }
 
   render() {
-    const { object, objectType, objectTypeLocalized } = this.props;
+    const { object } = this.props;
     return (
       <div>
         <PopoverWithTrigger
@@ -45,29 +42,21 @@ export default class ObjectActionsSelect extends Component {
         >
           <ul className={AdminS.UserActionsSelect}>
             <li>
-              <ActionLink
-                to={"/admin/datamodel/" + objectType + "/" + object.id}
-              >
-                {t`Edit`} {capitalize(objectType)}
+              <ActionLink to={`/admin/datamodel/segment/${object.id}`}>
+                {t`Edit segment`}
               </ActionLink>
             </li>
             <li>
               <ActionLink
-                to={
-                  "/admin/datamodel/" +
-                  objectType +
-                  "/" +
-                  object.id +
-                  "/revisions"
-                }
+                to={`/admin/datamodel/segment/${object.id}/revisions`}
               >
-                {t`Revision History`}
+                {t`Revision history`}
               </ActionLink>
             </li>
             <li className={cx(CS.mt1, CS.borderTop)}>
               <ModalWithTrigger
                 ref={this.retireModal}
-                triggerElement={t`Retire ${objectTypeLocalized}`}
+                triggerElement={t`Retire segment`}
                 triggerClasses={cx(
                   CS.block,
                   CS.p2,
@@ -77,9 +66,8 @@ export default class ObjectActionsSelect extends Component {
                   CS.cursorPointer,
                 )}
               >
-                <ObjectRetireModal
+                <SegmentRetireModal
                   object={object}
-                  objectType={objectType}
                   onRetire={this.onRetire.bind(this)}
                   onClose={() => this.retireModal.current.close()}
                 />
