423be24e75cb49eb4fe34027404ed4abc7b0487b
Anton Kulyk <kuliks.anton@gmail.com>
2024-10-22 17:40:24 +0100

    Don't update setting without changes

--- frontend/src/metabase/admin/settings/components/widgets/SettingText.jsx
+++ frontend/src/metabase/admin/settings/components/widgets/SettingText.jsx
@@ -26,7 +26,17 @@ const SettingText = ({
     defaultValue={setting.value || setting.default || ""}
     placeholder={setting.placeholder}
     onChange={fireOnChange ? e => onChange(e.target.value) : null}
-    onBlur={!fireOnChange ? e => onChange(e.target.value) : null}
+    onBlur={
+      !fireOnChange
+        ? e => {
+            const value = setting.value || setting.default || "";
+            const nextValue = e.target.value;
+            if (nextValue !== value) {
+              onChange(nextValue);
+            }
+          }
+        : null
+    }
     autoFocus={autoFocus}
   />
 );
