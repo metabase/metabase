1224a80f55618ed91243448da197de2789d23ba5
lbrdnk <lbrdnk@users.noreply.github.com>
2024-10-22 19:41:48 +0200

    Adjust temporal-unit-parameters.cy.spec.js

--- e2e/test/scenarios/dashboard-filters/temporal-unit-parameters.cy.spec.js
+++ e2e/test/scenarios/dashboard-filters/temporal-unit-parameters.cy.spec.js
@@ -528,7 +528,7 @@ describe("scenarios > dashboard > temporal unit parameters", () => {
         .click({ force: true });
       sidebar().within(() => {
         cy.log("datetime columns cannot be mapped");
-        cy.findByText("Created At").click();
+        cy.findByText("Created At: Month").click();
         cy.findByText("Update a dashboard filter").click();
         cy.findByText("No available targets").should("be.visible");
         cy.icon("chevronleft").click();
