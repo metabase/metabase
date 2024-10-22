353c8067b1dc45fcbc94aee347a0b18dcd21ed9d
lbrdnk <lbrdnk@users.noreply.github.com>
2024-10-22 16:35:08 +0200

    formatColumn to displayNameForColumn

--- frontend/src/metabase/lib/formatting/column.ts
+++ frontend/src/metabase/lib/formatting/column.ts
@@ -1,13 +1,5 @@
-import { getFriendlyName } from "metabase/visualizations/lib/utils";
 import type { DatasetColumn } from "metabase-types/api/dataset";
 
-export function formatColumn(column: DatasetColumn): string {
-  if (!column) {
-    return "";
-  } else if (column.remapped_to_column != null) {
-    // remapped_to_column is a special field added by Visualization.jsx
-    return formatColumn(column.remapped_to_column);
-  } else {
-    return getFriendlyName(column);
-  }
+export function displayNameForColumn(column: DatasetColumn): string {
+  return column?.remapped_to_column?.display_name ?? column?.display_name ?? "";
 }
