dbdef76e639d3f5857f8e787d5435530f0ebaac7
Anton Kulyk <kuliks.anton@gmail.com>
2024-10-23 16:58:07 +0100

    Allow grouping bar chart series into "Other" category (#48265)
    * Add `graph.max_categories` viz setting
    
    * Allow grouping series into "other" category
    
    * Add tooltip
    
    * Toggle "other" series visibility
    
    * WIP: new implementation for `graph.series_order`
    
    * Remove not used function
    
    * Make "other" series a regular `SeriesModel`
    
    * Fix stacked bar charts
    
    * Remove colors from "other" tooltip
    
    * Sort "other" tooltip values
    
    * Update `graph.max_categories` setting
    
    - negative number validation
    - 0 value turns off grouping
    
    * Fix Loki test
    
    * Add basic loki test
    
    * Add loki test for stacked chart
    
    * Add loki test for stacked normalized chart
    
    * slight refactor of series_order setting
    
    * Add screenshots
    
    * Handle different kinds of aggregations for "other"
    
    * Handle different aggregation kinds in tooltips
    
    * Add `graph.other_series_aggregation_fn` viz setting
    
    * Fix type errors
    
    * Fix unit tests
    
    * settings ui
    
    * remove sorting by value to match series order setting, other color
    
    * hide grouped series controls
    
    * Add e2e test
    
    * Update screenshots
    
    * Fix incorrect series grouping for stacked charts
    
    * group series into other settings
    
    * Update e2e test to work with new viz settings
    
    * Disable drills for the "Other" series
    
    * fix max_categories setting popover positioning
    
    * fix total row has misaligned columns on other series values
    
    * Remove redundant `click({ force: true })`
    
    * Rename aggregation fn viz setting
    
    * Move aggregation fn setting to the popover
    
    * WIP
    
    * Update popover
    
    * Fix `it.only`
    
    * Fix legend sync issue
    
    * Disable "Other" category by default (temporary)
    
    * Enable aggregation fn picker for MBQL queries (temporary)
    
    * Move "Other" category summary to a tooltip row
    
    * Add `graph.max_categories_enabled` to viz settings type
    
    * Reuse already computed "Other" value in tooltips
    
    * Add null check
    
    * Fix series length check
    
    ---------
    
    Co-authored-by: Aleksandr Lesnenko <alxnddr@gmail.com>

--- frontend/src/metabase-types/api/card.ts
+++ frontend/src/metabase-types/api/card.ts
@@ -151,6 +151,15 @@ export type VisualizationSettings = {
   "graph.show_values"?: boolean;
   "stackable.stack_type"?: StackType;
   "graph.show_stack_values"?: StackValuesDisplay;
+  "graph.max_categories_enabled"?: boolean;
+  "graph.max_categories"?: number;
+  "graph.other_category_aggregation_fn"?:
+    | "sum"
+    | "avg"
+    | "min"
+    | "max"
+    | "stddev"
+    | "median";
 
   // Table
   "table.columns"?: TableColumnOrderSetting[];
