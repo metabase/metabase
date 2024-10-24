5fc286e5b1bf41403aff4582d51af67692e24676
Nicolò Pretto <info@npretto.com>
2024-10-24 17:43:49 +0200

    use default line height in the embedding homepage

--- frontend/src/metabase/home/components/EmbedHomepage/StaticEmbedContent.tsx
+++ frontend/src/metabase/home/components/EmbedHomepage/StaticEmbedContent.tsx
@@ -18,13 +18,12 @@ export const StaticEmbedContent = ({
 }: StaticEmbedContentProps) => (
   <Box>
     <Text
-      lh={1.57}
       fw="bold"
       mb="sm"
       size="lg"
       color="text-medium"
     >{t`Static embedding`}</Text>
-    <Text lh={1.57} mb="md">
+    <Text mb="md">
       {/* eslint-disable-next-line no-literal-metabase-strings -- This string only shows for admins. */}
       {t`Embed a dashboard in a 'Powered by Metabase' iframe with interactivity limited to filters and tooltips, and a few customization options. The iframe loads a Metabase URL secured with a signed JSON Web Token (JWT). Appears with "Powered by Metabase", on Open Source and Starter plans, with the option to remove on Pro and Enterprise. As the simplest form of embedding, you can add a dashboard into your app in a few minutes with just a snippet.`}
     </Text>
