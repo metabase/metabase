b1a4b93a7f98b82d141f032e6f7f3c9bee5a391f
Noah Moss <noahbmoss@gmail.com>
2024-10-24 14:28:50 -0400

    simplify solution; a little refactor and cleanup

--- src/metabase/task/persist_refresh.clj
+++ src/metabase/task/persist_refresh.clj
@@ -13,9 +13,7 @@
    [metabase.email.messages :as messages]
    [metabase.models.card :refer [Card]]
    [metabase.models.database :refer [Database]]
-   [metabase.models.persisted-info
-    :as persisted-info
-    :refer [PersistedInfo]]
+   [metabase.models.persisted-info :as persisted-info]
    [metabase.models.task-history :as task-history]
    [metabase.public-settings :as public-settings]
    [metabase.query-processor.middleware.limit :as limit]
@@ -37,11 +35,11 @@
 
 (p/defprotocol+ Refresher
   "This protocol is just a wrapper of the ddl.interface multimethods to ease for testing. Rather than defing some
-   multimethods on fake engine types, just work against this, and it will dispatch to the ddl.interface normally, or
-   allow for easy to control custom behavior in tests."
-  (refresh! [this database definition dataset-query]
-    "Refresh a persisted model. Returns a map with :state that is :success or :error. If :state is :error, includes a
-    key :error with a string message. See [[metabase.driver.ddl.interface/refresh!]] for more information.")
+  multimethods on fake engine types, just work against this, and it will dispatch to the ddl.interface normally, or
+  allow for easy to control custom behavior in tests."
+  (refresh! [this database definition dataset-query])
+  "Refresh a persisted model. Returns a map with :state that is :success or :error. If :state is :error, includes a
+  key :error with a string message. See [[metabase.driver.ddl.interface/refresh!]] for more information."
   (unpersist! [this database persisted-info]))
 
 (def ^:private dispatching-refresher
@@ -56,12 +54,12 @@
 
 (defn- refresh-with-stats! [refresher database stats persisted-info]
   ;; Since this could be long running, double check state just before refreshing
-  (when (contains? (persisted-info/refreshable-states) (t2/select-one-fn :state PersistedInfo :id (:id persisted-info)))
+  (when (contains? (persisted-info/refreshable-states) (t2/select-one-fn :state :model/PersistedInfo :id (:id persisted-info)))
     (log/infof "Attempting to refresh persisted model %s." (:card_id persisted-info))
     (let [card (t2/select-one Card :id (:card_id persisted-info))
           definition (persisted-info/metadata->definition (:result_metadata card)
                                                           (:table_name persisted-info))
-          _ (t2/update! PersistedInfo (u/the-id persisted-info)
+          _ (t2/update! :model/PersistedInfo (u/the-id persisted-info)
                         {:definition definition,
                          :query_hash (persisted-info/query-hash (:dataset_query card))
                          :active false,
@@ -75,7 +73,7 @@
                                     (log/infof e "Error refreshing persisting model with card-id %s"
                                                (:card_id persisted-info))
                                     {:state :error :error (ex-message e)}))]
-      (t2/update! PersistedInfo (u/the-id persisted-info)
+      (t2/update! :model/PersistedInfo (u/the-id persisted-info)
                   {:active (= state :success),
                    :refresh_end :%now,
                    :state (if (= state :success) "persisted" "error")
@@ -98,7 +96,7 @@
   (try
     (let [error-details       (error-details task-details)
           error-details-by-id (m/index-by :persisted-info-id error-details)
-          persisted-infos     (->> (t2/hydrate (t2/select PersistedInfo :id [:in (keys error-details-by-id)])
+          persisted-infos     (->> (t2/hydrate (t2/select :model/PersistedInfo :id [:in (keys error-details-by-id)])
                                                [:card :collection] :database)
                                    (map #(assoc % :error (get-in error-details-by-id [(:id %) :error]))))]
       (messages/send-persistent-model-error-email!
@@ -130,7 +128,7 @@
           unpersist-fn (fn []
                          (reduce (fn [stats persisted-info]
                                    ;; Since this could be long running, double check state just before deleting
-                                   (let [current-state (t2/select-one-fn :state PersistedInfo :id (:id persisted-info))
+                                   (let [current-state (t2/select-one-fn :state :model/PersistedInfo :id (:id persisted-info))
                                          card-info     (t2/select-one [Card :archived :type]
                                                                       :id (:card_id persisted-info))]
                                      (if (or (contains? (persisted-info/prunable-states) current-state)
@@ -141,7 +139,7 @@
                                          (try
                                            (unpersist! refresher database persisted-info)
                                            (when-not (= "off" current-state)
-                                             (t2/delete! PersistedInfo :id (:id persisted-info)))
+                                             (t2/delete! :model/PersistedInfo :id (:id persisted-info)))
                                            (update stats :success inc)
                                            (catch Exception e
                                              (log/infof e "Error unpersisting model with card-id %s" (:card_id persisted-info))
@@ -154,13 +152,13 @@
 (defn- deletable-models
   "Returns persisted info records that can be unpersisted. Will select records that have moved into a deletable state
   after a sufficient delay to ensure no queries are running against them and to allow changing mind. Also selects
-  persisted info records pointing to cards that are no longer models and archived cards/models."
+  persisted info records pointing to cards that are no longer models, archived cards/models, and records where the corresponding
+  card or database has been permanently deleted."
   []
-  (t2/select PersistedInfo
+  (t2/select :model/PersistedInfo
              {:select    [:p.*]
               :from      [[:persisted_info :p]]
-              :left-join [[:report_card :c] [:= :c.id :p.card_id]
-                          [:report_database :d] [:= :d.id :p.database_id]]
+              :left-join [[:report_card :c] [:= :c.id :p.card_id]]
               :where     [:or
                           [:and
                            [:in :state (persisted-info/prunable-states)]
@@ -172,18 +170,16 @@
                             (sql.qp/add-interval-honeysql-form (mdb/db-type) :%now -1 :hour)]]
                           [:= :c.type "question"]
                           [:= :c.archived true]
-                          ;; card_id and dashboard_id are set to null when the corresponding card/dashboard is deleted
+                          ;; card_id is set to null when the corresponding card is deleted
                           [:= :p.card_id nil]
-                          [:= :p.dashboard_id nil]
-                          ;; if we do have a card or dashboard ID, but can't join it to a real card or dashboard, let's
-                          ;; delete it as well (cleaning up any possible orphans)
-                          [:= :c.id nil]
-                          [:= :d.id nil]]}))
+                          ;; if we do have a card ID in persisted_info but the card doesn't exist, delete it as well
+                          ;; (cleaning up any possible orphans)
+                          [:= :c.id nil]]}))
 
 (defn- refreshable-models
   "Returns refreshable models for a database id. Must still be models and not archived."
   [database-id]
-  (t2/select PersistedInfo
+  (t2/select :model/PersistedInfo
              {:select    [:p.* :c.type :c.archived :c.name]
               :from      [[:persisted_info :p]]
               :left-join [[:report_card :c] [:= :c.id :p.card_id]]
@@ -219,7 +215,7 @@
 (defn- refresh-individual!
   "Refresh an individual model based on [[PersistedInfo]]."
   [persisted-info-id refresher]
-  (let [persisted-info (t2/select-one PersistedInfo :id persisted-info-id)
+  (let [persisted-info (t2/select-one :model/PersistedInfo :id persisted-info-id)
         database       (when persisted-info
                          (t2/select-one Database :id (:database_id persisted-info)))]
     (if (and persisted-info database)
