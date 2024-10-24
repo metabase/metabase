ca0a2b552781ca057725409578a214610d18dcbd
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-10-24 20:47:46 +0200

    Add E2E Snowplow tests

---
+++ e2e/test/scenarios/onboarding/onboarding-checklist.cy.spec.ts
@@ -0,0 +1,94 @@
+import {
+  describeWithSnowplow,
+  enableTracking,
+  expectGoodSnowplowEvent,
+  expectGoodSnowplowEvents,
+  expectNoBadSnowplowEvents,
+  resetSnowplow,
+  restore,
+} from "e2e/support/helpers";
+import type { ChecklistItemValue } from "metabase/home/components/Onboarding/types";
+
+describeWithSnowplow("Onboarding checklist events", () => {
+  beforeEach(() => {
+    restore("setup");
+    cy.signInAsAdmin();
+
+    resetSnowplow();
+    enableTracking();
+  });
+
+  afterEach(() => {
+    expectNoBadSnowplowEvents();
+  });
+
+  it('should track clicking on "How to use Metabase" button', () => {
+    cy.visit("/");
+    cy.findByTestId("main-navbar-root")
+      .findByRole("listitem", { name: "How to use Metabase" })
+      .click();
+    cy.location("pathname").should("eq", "/getting-started");
+    expectGoodSnowplowEvent({
+      event: "onboarding_checklist_opened",
+    });
+  });
+
+  describe("Onboarding checklist page", () => {
+    it("should track each item when expanded", () => {
+      const PAGE_VIEW = 1;
+      const items: ChecklistItemValue[] = [
+        "invite",
+        "database",
+        "x-ray",
+        "notebook",
+        "sql",
+        "dashboard",
+        "subscription",
+        "alert",
+      ];
+
+      cy.visit("/getting-started");
+      cy.log(
+        "The default open accordion item is not tracked - only the page view is",
+      );
+      expectGoodSnowplowEvents(PAGE_VIEW);
+
+      items.forEach(i => {
+        cy.findByTestId(`${i}-item`).click();
+        expectGoodSnowplowEvent({
+          event: "onboarding_checklist_item_expanded",
+          triggered_from: i,
+        });
+      });
+    });
+
+    it("should track individual items' cta(s) when clicked", () => {
+      cy.visit("/getting-started");
+      cy.findByTestId("database-cta").button("Add Database").click();
+      expectGoodSnowplowEvent({
+        event: "onboarding_checklist_cta_clicked",
+        triggered_from: "database",
+        event_detail: "primary",
+      });
+
+      cy.go("back");
+
+      cy.findByTestId("invite-item").click();
+      cy.findByTestId("invite-cta").button("Invite people").click();
+      expectGoodSnowplowEvent({
+        event: "onboarding_checklist_cta_clicked",
+        triggered_from: "invite",
+        event_detail: "primary",
+      });
+
+      cy.go("back");
+
+      cy.findByTestId("invite-cta").button("Set up Single Sign-on").click();
+      expectGoodSnowplowEvent({
+        event: "onboarding_checklist_cta_clicked",
+        triggered_from: "invite",
+        event_detail: "secondary",
+      });
+    });
+  });
+});
