35cfec55fd36fa493b5d04f9d73a40c9ba28a432
Oisin Coveney <oisin@metabase.com>
2024-10-24 13:08:28 +0300

    I was trying to use vim

--- frontend/src/metabase/visualizations/components/ChartSettings/ChartSettings.tsx
+++ frontend/src/metabase/visualizations/components/ChartSettings/ChartSettings.tsx
@@ -1,5 +1,5 @@
 import { assocIn } from "icepick";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { t } from "ttag";
 import _ from "underscore";
 
