257261536433b09a0e2cf9cae570d287cd631e50
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-10-25 22:18:03 +0700

    test question download with params

--- e2e/test/scenarios/embedding/embed-resource-downloads.cy.spec.ts
+++ e2e/test/scenarios/embedding/embed-resource-downloads.cy.spec.ts
@@ -6,6 +6,7 @@ import {
 import {
   addOrUpdateDashboardCard,
   createDashboardWithQuestions,
+  createNativeQuestion,
   describeWithSnowplowEE,
   expectGoodSnowplowEvent,
   expectNoBadSnowplowEvents,
@@ -303,6 +304,92 @@ describeWithSnowplowEE(
           resource_type: "question",
           accessed_via: "static-embed",
           export_type: "csv",
+        });
+      });
+
+      describe("with native question parameters", () => {
+        beforeEach(() => {
+          cy.signInAsAdmin();
+
+          setTokenFeatures("all");
+
+          // Can't figure out the type if I extracted `questionDetails` to a variable.
+          createNativeQuestion(
+            {
+              name: "25374",
+              native: {
+                "template-tags": {
+                  num: {
+                    id: "f7672b4d-1e84-1fa8-bf02-b5e584cd4535",
+                    name: "num",
+                    "display-name": "Num",
+                    type: "number",
+                    default: null,
+                  },
+                },
+                query: "select {{num}}",
+              },
+              parameters: [
+                {
+                  id: "f7672b4d-1e84-1fa8-bf02-b5e584cd4535",
+                  type: "number/=",
+                  target: ["variable", ["template-tag", "num"]],
+                  name: "Num",
+                  slug: "num",
+                  default: null,
+                },
+              ],
+              enable_embedding: true,
+              embedding_params: {
+                num: "enabled",
+              },
+            },
+            {
+              idAlias: "questionId",
+              wrapId: true,
+            },
+          );
+
+          cy.signOut();
+        });
+
+        it("should be able to download a static embedded dashcard as CSV", () => {
+          const value = 9999;
+          cy.get("@questionId").then(questionId => {
+            visitEmbeddedPage(
+              {
+                resource: { question: Number(questionId) },
+                params: {
+                  num: value,
+                },
+              },
+              {
+                pageStyle: {
+                  downloads: true,
+                },
+              },
+            );
+          });
+
+          waitLoading();
+
+          main().findByText(value).should("exist");
+
+          cy.findByTestId("download-button").click();
+
+          popover().within(() => {
+            cy.findByText(".csv").click();
+            cy.findByTestId("download-results-button").click();
+          });
+
+          cy.verifyDownload(".csv", { contains: true });
+
+          expectGoodSnowplowEvent({
+            event: "download_results_clicked",
+            resource_type: "question",
+            accessed_via: "static-embed",
+            export_type: "csv",
+          });
         });
       });
     });
