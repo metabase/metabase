286dad02238174a67ccdc88c5471ba1c73d8832b
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-10-28 13:26:16 -0400

    Add tests

--- e2e/test/scenarios/custom-column/cc-boolean-functions.cy.spec.ts
+++ e2e/test/scenarios/custom-column/cc-boolean-functions.cy.spec.ts
@@ -25,16 +25,7 @@ describe("scenarios > custom column > boolean functions", () => {
   });
 
   describe("expression editor", () => {
-    type ExpressionTestCase = {
-      name: string;
-      questionDetails: StructuredQuestionDetails;
-      questionColumns: string[];
-      newExpression: string;
-      newExpressionRows: string[][];
-      modifiedExpression: string;
-      modifiedExpressionRows: string[][];
-    };
-
+    const stringQuestionColumns = ["Category"];
     const stringQuestionDetails: StructuredQuestionDetails = {
       query: {
         "source-table": PRODUCTS_ID,
@@ -43,8 +34,8 @@ describe("scenarios > custom column > boolean functions", () => {
         limit: 1,
       },
     };
-    const stringQuestionColumns = ["Category"];
 
+    const numberQuestionColumns = ["Total"];
     const numberQuestionDetails: StructuredQuestionDetails = {
       query: {
         "source-table": ORDERS_ID,
@@ -53,8 +44,8 @@ describe("scenarios > custom column > boolean functions", () => {
         limit: 1,
       },
     };
-    const numberQuestionColumns = ["Total"];
 
+    const dateQuestionColumns = ["Created At"];
     const dateQuestionDetails: StructuredQuestionDetails = {
       query: {
         "source-table": ORDERS_ID,
@@ -63,128 +54,142 @@ describe("scenarios > custom column > boolean functions", () => {
         limit: 1,
       },
     };
-    const dateQuestionColumns = ["Created At"];
 
-    const expressionTestCases: ExpressionTestCase[] = [
-      {
-        name: "isNull",
+    function testExpression({
+      questionDetails,
+      questionColumns,
+      newExpression,
+      newExpressionRows,
+      modifiedExpression,
+      modifiedExpressionRows,
+    }: {
+      questionDetails: StructuredQuestionDetails;
+      questionColumns: string[];
+      newExpression: string;
+      newExpressionRows: string[][];
+      modifiedExpression: string;
+      modifiedExpressionRows: string[][];
+    }) {
+      createQuestion(questionDetails, { visitQuestion: true });
+
+      cy.log("add a new custom column");
+      openNotebook();
+      getNotebookStep("data").button("Custom column").click();
+      enterCustomColumnDetails({
+        formula: newExpression,
+        name: expressionName,
+      });
+      popover().button("Done").click();
+      visualize();
+      cy.wait("@dataset");
+      assertTableData({
+        columns: [...questionColumns, expressionName],
+        firstRows: newExpressionRows,
+      });
+
+      cy.log("modify an existing custom column");
+      openNotebook();
+      getNotebookStep("expression").findByText(expressionName).click();
+      enterCustomColumnDetails({
+        formula: modifiedExpression,
+        name: expressionName,
+      });
+      popover().button("Update").click();
+      visualize();
+      cy.wait("@dataset");
+      assertTableData({
+        columns: [...questionColumns, expressionName],
+        firstRows: modifiedExpressionRows,
+      });
+    }
+
+    it("isNull", () => {
+      testExpression({
         questionDetails: stringQuestionDetails,
         questionColumns: stringQuestionColumns,
         newExpression: "isNull([Category])",
         newExpressionRows: [["Gizmo", "false"]],
         modifiedExpression: "notNull([Category])",
         modifiedExpressionRows: [["Gizmo", "true"]],
-      },
-      {
-        name: "isEmpty",
+      });
+    });
+
+    it("isEmpty", () => {
+      testExpression({
         questionDetails: stringQuestionDetails,
         questionColumns: stringQuestionColumns,
         newExpression: "isEmpty([Category])",
         newExpressionRows: [["Gizmo", "false"]],
         modifiedExpression: "notEmpty([Category])",
         modifiedExpressionRows: [["Gizmo", "true"]],
-      },
-      {
-        name: "startsWith",
+      });
+    });
+
+    it("startsWith", () => {
+      testExpression({
         questionDetails: stringQuestionDetails,
         questionColumns: stringQuestionColumns,
         newExpression: 'startsWith([Category], "Gi")',
         newExpressionRows: [["Gizmo", "true"]],
         modifiedExpression: 'startsWith([Category], "mo")',
         modifiedExpressionRows: [["Gizmo", "false"]],
-      },
-      {
-        name: "endsWith",
+      });
+    });
+
+    it("endsWith", () => {
+      testExpression({
         questionDetails: stringQuestionDetails,
         questionColumns: stringQuestionColumns,
         newExpression: 'endsWith([Category], "Gi")',
         newExpressionRows: [["Gizmo", "false"]],
         modifiedExpression: 'endsWith([Category], "mo")',
         modifiedExpressionRows: [["Gizmo", "true"]],
-      },
-      {
-        name: "contains",
+      });
+    });
+
+    it("contains", () => {
+      testExpression({
         questionDetails: stringQuestionDetails,
         questionColumns: stringQuestionColumns,
         newExpression: 'contains([Category], "zm")',
         newExpressionRows: [["Gizmo", "true"]],
         modifiedExpression: 'contains([Category], "mz")',
         modifiedExpressionRows: [["Gizmo", "false"]],
-      },
-      {
-        name: "doesNotContain",
+      });
+    });
+
+    it("doesNotContain", () => {
+      testExpression({
         questionDetails: stringQuestionDetails,
         questionColumns: stringQuestionColumns,
         newExpression: 'doesNotContain([Category], "zm")',
         newExpressionRows: [["Gizmo", "false"]],
         modifiedExpression: 'doesNotContain([Category], "mz")',
         modifiedExpressionRows: [["Gizmo", "true"]],
-      },
-      {
-        name: "between",
+      });
+    });
+
+    it("between", () => {
+      testExpression({
         questionDetails: numberQuestionDetails,
         questionColumns: numberQuestionColumns,
         newExpression: "between([Total], 20, 30)",
         newExpressionRows: [["39.72", "false"]],
         modifiedExpression: "between([Total], 30, 40)",
         modifiedExpressionRows: [["39.72", "true"]],
-      },
-      {
-        name: "timeInterval",
+      });
+    });
+
+    it("timeInterval", () => {
+      testExpression({
         questionDetails: dateQuestionDetails,
         questionColumns: dateQuestionColumns,
         newExpression: 'interval([Created At], -30, "year")',
         newExpressionRows: [["April 30, 2022, 6:56 PM", "true"]],
         modifiedExpression: 'interval([Created At], 2, "month")',
         modifiedExpressionRows: [["April 30, 2022, 6:56 PM", "false"]],
-      },
-    ];
-
-    expressionTestCases.forEach(
-      ({
-        name,
-        questionDetails,
-        questionColumns,
-        newExpression,
-        newExpressionRows,
-        modifiedExpression,
-        modifiedExpressionRows,
-      }) => {
-        it(name, () => {
-          createQuestion(questionDetails, { visitQuestion: true });
-
-          cy.log("add a new custom column");
-          openNotebook();
-          getNotebookStep("data").button("Custom column").click();
-          enterCustomColumnDetails({
-            formula: newExpression,
-            name: expressionName,
-          });
-          popover().button("Done").click();
-          visualize();
-          cy.wait("@dataset");
-          assertTableData({
-            columns: [...questionColumns, expressionName],
-            firstRows: newExpressionRows,
-          });
-
-          cy.log("modify an existing custom column");
-          openNotebook();
-          getNotebookStep("expression").findByText(expressionName).click();
-          enterCustomColumnDetails({
-            formula: modifiedExpression,
-            name: expressionName,
-          });
-          popover().button("Update").click();
-          visualize();
-          cy.wait("@dataset");
-          assertTableData({
-            columns: [...questionColumns, expressionName],
-            firstRows: modifiedExpressionRows,
-          });
-        });
-      },
-    );
+      });
+    });
   });
 
   describe("same stage", () => {
