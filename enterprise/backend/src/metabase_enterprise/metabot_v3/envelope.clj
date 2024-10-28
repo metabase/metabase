a28d3d4c7b4ef10cb38ef4eac1dbce768b2bc7a9
John Swanson <john.swanson@metabase.com>
2024-10-28 10:13:37 -0700

    Round trips to the LLM

---
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/envelope.clj
@@ -0,0 +1,127 @@
+(ns metabase-enterprise.metabot-v3.envelope
+  "(Because 'context' was already taken.)
+
+  The 'envelope' holds the context for our conversation with the LLM. Specifically, it bundles up the history, the
+  reactions, and the context into one convenient location, with a simple API for querying and modifying."
+  (:require [metabase.util :as u]))
+
+(def ^:constant max-round-trips
+  "The maximum number of times we'll make a request to the LLM before responding to the user. Currently we'll just throw
+  an exception if this limit is exceeded."
+  10)
+
+(defn create
+  "Create a fresh envelope from a context and history. This envelope should be used for the lifetime of the request."
+  [context history]
+  {:history history
+   :context context
+   :reactions []
+   :max-round-trips max-round-trips
+   :round-trips-remaining max-round-trips})
+
+(defn decrement-round-trips
+  "Decrement the remaining allowed round trips to the LLM, erroring if the maximum was exceeded."
+  [e]
+  (update e :round-trips-remaining (fn [v]
+                                     (u/prog1 (dec v)
+                                       (when (neg? <>)
+                                         (throw (ex-info "Error: too many round trips." {:envelope e})))))))
+
+(defn history
+  "Gets the history from the envelope."
+  [e]
+  (:history e))
+
+(defn context
+  "Gets the context from the envelope."
+  [e]
+  (:context e))
+
+(defn reactions
+  "Gets the reactions from the envelope."
+  [e]
+  (:reactions e))
+
+(defn add-reactions
+  "Add new reactions to the envelope."
+  [e reactions]
+  (update e :reactions into reactions))
+
+(defn add-user-message
+  "Given a user message (a string) adds it to the envelope."
+  [e msg]
+  (update e :history conj {:role :user :content msg}))
+
+(defn add-message
+  "Given a raw message, add it to the envelope."
+  [e msg]
+  (update e :history conj msg))
+
+(defn add-tool-response
+  "Given an output string and a collection of reactions, adds them to the envelope."
+  [e tool-call-id output reactions]
+  (-> e
+      (add-message {:role :tool
+                    :tool-call-id tool-call-id
+                    :content output})
+      (add-reactions reactions)))
+
+(defn is-tool-call?
+  "Is this message a tool call?"
+  [{:keys [tool-calls]}]
+  (boolean (seq tool-calls)))
+
+(defn is-tool-call-response?
+  "Is this message a response to a tool call?"
+  [{:keys [role tool-call-id]}]
+  (and (= role :tool)
+       tool-call-id))
+
+(defn requires-tool-invocation?
+  "Does this envelope require tool call invocation?"
+  [e]
+  (->> e history peek is-tool-call?))
+
+(defn is-user-message?
+  "Is this message from the user?"
+  [{:keys [role]}]
+  (= role :user))
+
+(defn is-assistant-message?
+  "Is this message from the assistant (i.e. the LLM)?"
+  [{:keys [role]}]
+  (= role :assistant))
+
+(defn requires-llm-response?
+  "Does this envelope require a new response from the LLM?"
+  [e]
+  (let [last-message (->> e history peek)]
+    (or (is-tool-call-response? last-message)
+        (is-user-message? last-message))))
+
+(defn tool-calls-requiring-invocation
+  "Gets a list of all the tool calls in the chat history that have not yet been responded to."
+  [e]
+  (let [tool-call-id->response (->> e
+                                    history
+                                    (filter is-tool-call-response?)
+                                    (map :tool-call-id)
+                                    (into #{}))]
+
+    (->> (history e)
+         (filter is-tool-call?)
+         (mapcat :tool-calls)
+         (remove #(tool-call-id->response (:id %))))))
+
+(defn last-assistant-message->reaction
+  "This is a bit hacky. Right now we only respond to the user with reactions. So we take the last assistant message and
+  turn it into a reaction."
+  [e]
+  {:type :metabot.reaction/message
+   :repl/message-color :green
+   :repl/message-emoji "🤖"
+   :message (->> e
+                 history
+                 (filter is-assistant-message?)
+                 last
+                 :content)})
--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/who_is_your_favorite.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/who_is_your_favorite.clj
@@ -1,10 +1,10 @@
 (ns metabase-enterprise.metabot-v3.tools.who-is-your-favorite
   (:require
-   [metabase-enterprise.metabot-v3.reactions :as metabot-v3.reactions]
    [metabase-enterprise.metabot-v3.tools.interface :as metabot-v3.tools.interface]
    [metabase.util.malli :as mu]))
 
-(mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/who-is-your-favorite :- [:sequential ::metabot-v3.reactions/reaction]
+(mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/who-is-your-favorite
   [_tool-name _arg-map]
-  [{:type :metabot.reaction/message
-    :message "You are... but don't tell anyone!"}])
+  {:reactions [{:type :metabot.reaction/message
+                :message "You are... but don't tell anyone!"}]
+   :output "This current user is my favorite."})
