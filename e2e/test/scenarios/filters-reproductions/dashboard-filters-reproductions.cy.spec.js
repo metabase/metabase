9f97dceac4ba3599ea61133eccb4f94a827604d6
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-10-28 10:58:33 -0400

    Invert exclude filter checkbox state (#49178) (#49236)
    Co-authored-by: Alexander Polyankin <alexander.polyankin@metabase.com>

--- e2e/test/scenarios/filters-reproductions/dashboard-filters-reproductions.cy.spec.js
+++ e2e/test/scenarios/filters-reproductions/dashboard-filters-reproductions.cy.spec.js
@@ -1405,13 +1405,13 @@ describe("issue 24235", () => {
     popover().within(() => {
       cy.findByText("Exclude...").click();
       cy.findByText("Days of the week...").click();
-      cy.findByText("Select none...").click();
+      cy.findByText("Select all").click();
       cy.findByText("Add filter").click();
     });
 
     filterWidget().click();
     popover().within(() => {
-      cy.findByText("Select all...").click();
+      cy.findByText("Select none").click();
       cy.findByText("Update filter").click();
     });
 
@@ -3003,9 +3003,10 @@ describe("issue 27579", () => {
     popover().within(() => {
       cy.findByText("Exclude...").click();
       cy.findByText("Hours of the day...").click();
+      cy.findByText("Select all").click();
       cy.findByLabelText("12 AM").should("be.checked");
 
-      cy.findByText("Select none...").click();
+      cy.findByText("Select none").click();
       cy.findByLabelText("12 AM").should("not.be.checked");
     });
   });
