fa31b664270ede275b7f1b3f86e76064b6a7bcf4
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-10-29 09:31:12 +0100

    Convert the Banner to TS

---
+++ frontend/src/metabase/components/Banner/Banner.tsx
@@ -0,0 +1,18 @@
+import Markdown from "metabase/core/components/Markdown";
+import { Flex, type FlexProps } from "metabase/ui";
+
+export const Banner = ({ children, ...props }: FlexProps) => {
+  const content =
+    typeof children === "string" ? <Markdown>{children}</Markdown> : children;
+
+  return (
+    <Flex
+      bg="var(--mb-color-bg-light)"
+      c="var(--mb-color-text-medium)"
+      p="0.75rem"
+      {...props}
+    >
+      {content}
+    </Flex>
+  );
+};
