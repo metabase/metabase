2b3b6cc8727254cff4f9dae6ec34b0417623bdea
Case Nelson <case@metabase.com>
2024-10-30 12:45:36 -0600

    Make format string inline

--- src/metabase/driver/postgres.clj
+++ src/metabase/driver/postgres.clj
@@ -290,7 +290,7 @@
                         [[:not= :pk.column_name nil] :pk?]
 
                         [[:col_description
-                          [:cast [:cast [:format "%I.%I" [:cast :c.table_schema :text] [:c.table_name :text]] :regclass] :oid]
+                          [:cast [:cast [:format [:inline "%I.%I"] [:cast :c.table_schema :text] [:cast :c.table_name :text]] :regclass] :oid]
                           :c.ordinal_position]
                          :field-comment]
                         [[:and
