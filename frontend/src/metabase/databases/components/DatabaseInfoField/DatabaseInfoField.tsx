41d551805864336adbf163fb4e1d5c9291b048d2
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-10-30 09:39:07 -0400

    tech: Refactor  and improve `AppBanner` (#49273) (#49339)
    * Remove unused component
    
    * Remove extra space
    
    * Remove cross-component dependency on the `Banner`
    
    * Use named export
    
    * Convert the Banner to TS
    
    * Make `ReadOnlyBanner` use `Banner` under the hood
    
    * Add test id to the banner
    
    * Simplify render method and tests
    
    * Add the missing test for ReadOnly banner
    
    * Avoid using special ASCII characters for the common apostrophe
    
    * Remove E2E spec
    
    * Use `Banner` for `DatabaseInfoField`
    
    * Use colors defined in the theme
    
    * Remove the dependency on the `Banner` component again!
    
    * Add a comment
    
    * Use pattern matching
    
    Co-authored-by: Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>

--- frontend/src/metabase/databases/components/DatabaseInfoField/DatabaseInfoField.tsx
+++ frontend/src/metabase/databases/components/DatabaseInfoField/DatabaseInfoField.tsx
@@ -1,4 +1,5 @@
-import { InfoBanner } from "./DatabaseInfoField.styled";
+import Markdown from "metabase/core/components/Markdown";
+import { Flex } from "metabase/ui";
 
 export interface DatabaseInfoFieldProps {
   placeholder?: string;
@@ -6,8 +7,19 @@ export interface DatabaseInfoFieldProps {
 
 const DatabaseInfoField = ({
   placeholder,
-}: DatabaseInfoFieldProps): JSX.Element => {
-  return <InfoBanner>{placeholder}</InfoBanner>;
+}: DatabaseInfoFieldProps): JSX.Element | null => {
+  return placeholder ? (
+    <Flex
+      bg="bg-light"
+      c="text-medium"
+      data-testid="app-banner"
+      mb="sm"
+      p="0.75rem"
+      style={{ borderRadius: "0.375rem" }}
+    >
+      <Markdown>{placeholder}</Markdown>
+    </Flex>
+  ) : null;
 };
 
 // eslint-disable-next-line import/no-default-export -- deprecated usage
