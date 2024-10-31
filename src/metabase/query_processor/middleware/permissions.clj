882731060428a59105e58d08f7a980c76734130b
Noah Moss <noahbmoss@gmail.com>
2024-10-31 14:18:58 -0400

    undo a change to see if it fixes some things

--- src/metabase/query_processor/middleware/permissions.clj
+++ src/metabase/query_processor/middleware/permissions.clj
@@ -99,8 +99,9 @@
       (cond
         card-id
         (do
-          (check-block-permissions outer-query)
-          (query-perms/check-card-read-perms database-id card-id))
+          (query-perms/check-card-read-perms database-id card-id)
+          (when-not (query-perms/has-perm-for-query? outer-query :perms/view-data required-perms)
+            (throw (query-perms/perms-exception required-perms))))
 
         ;; set when querying for field values of dashboard filters, which only require
         ;; collection perms for the dashboard and not ad-hoc query perms
