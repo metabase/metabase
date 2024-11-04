59dfdf49a2eeceed949e5463ca0381325625451e
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-11-04 10:38:06 -0500

    Handle migration

--- frontend/src/metabase-lib/v1/queries/utils/pivot.js
+++ frontend/src/metabase-lib/v1/queries/utils/pivot.js
@@ -3,21 +3,21 @@ import _ from "underscore";
 import * as Lib from "metabase-lib";
 
 export function getPivotColumnSplit(question) {
-  const setting = question.setting("pivot_table.column_split");
+  const setting = question.setting("pivot_table.column_split") ?? {};
   const query = question.query();
   const stageIndex = -1;
-  const columns = Lib.returnedColumns(query, stageIndex);
-  const breakoutColumnNames = columns
+  const returnedColumns = Lib.returnedColumns(query, stageIndex);
+  const breakoutColumnNames = returnedColumns
     .map(column => Lib.displayInfo(query, stageIndex, column))
     .filter(columnInfo => columnInfo.isBreakout)
     .map(columnInfo => columnInfo.name);
 
   const { rows: pivot_rows, columns: pivot_cols } = _.mapObject(
-    setting,
+    _.pick(setting, "rows", "columns"),
     columnNames => {
       return columnNames
-        .map(columnName => breakoutColumnNames.indexOf(columnName))
-        .filter(columnIndex => columnIndex >= 0);
+        ?.map(columnName => breakoutColumnNames.indexOf(columnName))
+        ?.filter(columnIndex => columnIndex >= 0);
     },
   );
 
