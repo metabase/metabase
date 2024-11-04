de559f49d6dc245bc35d73d373a3cbba3c8062e4
Phoomparin Mano <poom@metabase.com>
2024-11-04 20:49:13 +0700

    feat(sdk): ability to enforce the destination collection to save to and hide the collection picker (#49251)
    * add ability to hide the collection picker
    
    * add save options
    
    * make sure collectionId is fresh
    
    * update sdk question form component
    
    * make collectionId accept null
    
    * apply saveOptions to sdk components
    
    * revert to use initialCollectionId as context name
    
    * target the correct collection id
    
    * add e2e tests
    
    * update e2e tests
    
    * move into within
    
    * update test
    
    * remove collectionId from default args
    
    * revert e2e of hide columns
    
    * make e2e helper more generic
    
    * use saveToCollectionId prop to save to a specific collection
    
    * update e2e tests
    
    * update comment formatting
    
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
    
    * clean up leftover changes from an earlier version
    
    * readme update
    
    * indicate that saveToCollectionId is only applicable to static questions
    
    * revert QuestionEditor.isSaveEnabled defaults
    
    * update comment format
    
    * remove unused initialCollectionId prop
    
    * experiment: try to fix failing custom-columns e2e
    
    ---------
    
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>

--- enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/context/types.ts
+++ enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/context/types.ts
@@ -3,20 +3,24 @@ import type { PropsWithChildren } from "react";
 import type { SdkPluginsConfig } from "embedding-sdk";
 import type { LoadQuestionHookResult } from "embedding-sdk/hooks/private/use-load-question";
 import type { LoadSdkQuestionParams } from "embedding-sdk/types/question";
+import type { SaveQuestionProps } from "metabase/components/SaveQuestionForm/types";
 import type { NotebookProps as QBNotebookProps } from "metabase/querying/notebook/components/Notebook";
 import type { Mode } from "metabase/visualizations/click-actions/Mode";
 import type Question from "metabase-lib/v1/Question";
 import type { CardEntityId, CardId } from "metabase-types/api";
 
 export type EntityTypeFilterKeys = "table" | "question" | "model" | "metric";
-export type InteractiveQuestionConfig = {
+
+type InteractiveQuestionConfig = {
   componentPlugins?: SdkPluginsConfig;
   onNavigateBack?: () => void;
   onBeforeSave?: (question?: Question) => Promise<void>;
   onSave?: (question?: Question) => void;
-  isSaveEnabled?: boolean;
   entityTypeFilter?: EntityTypeFilterKeys[];
-};
+
+  /** Is the save question button visible? */
+  isSaveEnabled?: boolean;
+} & Pick<SaveQuestionProps, "saveToCollectionId">;
 
 export type QuestionMockLocationParameters = {
   location: { search: string; hash: string; pathname: string };
@@ -36,7 +40,10 @@ export type InteractiveQuestionContextType = Omit<
   LoadQuestionHookResult,
   "loadQuestion"
 > &
-  Pick<InteractiveQuestionConfig, "onNavigateBack" | "isSaveEnabled"> &
+  Pick<
+    InteractiveQuestionConfig,
+    "onNavigateBack" | "isSaveEnabled" | "saveToCollectionId"
+  > &
   Pick<QBNotebookProps, "modelsFilterList"> & {
     plugins: InteractiveQuestionConfig["componentPlugins"] | null;
     mode: Mode | null | undefined;
