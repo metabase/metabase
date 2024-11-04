b645396db29a07750c48043ec490cb43fbf906f2
Oisin Coveney <oisin@metabase.com>
2024-11-04 15:42:55 +0200

    Fix the lint issue by converting the hook to CSS and use semantic colors

--- frontend/src/metabase/admin/settings/components/widgets/EmbeddingOption/StaticEmbeddingOptionCard/StaticEmbeddingIcon.tsx
+++ frontend/src/metabase/admin/settings/components/widgets/EmbeddingOption/StaticEmbeddingOptionCard/StaticEmbeddingIcon.tsx
@@ -1,32 +1,30 @@
-import {
-  type UseEmbeddingSettingsIconColorsProps,
-  useEmbeddingSettingsIconColors,
-} from "../use-embedding-settings-icon-colors";
+import EmbeddingOptionS from "../EmbeddingOption.module.css";
+import type { EmbeddingOptionIconProps } from "../types";
 
-export const StaticEmbeddingIcon = ({
-  disabled,
-}: UseEmbeddingSettingsIconColorsProps) => {
-  const { primary, secondary } = useEmbeddingSettingsIconColors({ disabled });
-
-  return (
-    <svg width="42" height="34" fill="white" xmlns="http://www.w3.org/2000/svg">
-      <rect
-        x="1.25"
-        y="1.25"
-        width="37.5"
-        height="29.5"
-        rx="2.75"
-        stroke={secondary}
-        strokeWidth="2.5"
-      />
-      <path
-        d="M14 12a2 2 0 0 1 2-2h17a2 2 0 0 1 2 2v5H14v-5ZM14 19h9v9h-7a2 2 0 0 1-2-2v-7ZM25 19h10v7a2 2 0 0 1-2 2h-8v-9Z"
-        fill={primary}
-      />
-      <path
-        d="M5 10h6v18H7a2 2 0 0 1-2-2V10ZM5 6a2 2 0 0 1 2-2h26a2 2 0 0 1 2 2v2H5V6Z"
-        fill={secondary}
-      />
-    </svg>
-  );
-};
+export const StaticEmbeddingIcon = ({ disabled }: EmbeddingOptionIconProps) => (
+  <svg
+    width="42"
+    height="34"
+    fill="white"
+    xmlns="http://www.w3.org/2000/svg"
+    className={disabled && EmbeddingOptionS.Disabled}
+  >
+    <rect
+      x="1.25"
+      y="1.25"
+      width="37.5"
+      height="29.5"
+      rx="2.75"
+      strokeWidth="2.5"
+      className={EmbeddingOptionS.SecondaryStroke}
+    />
+    <path
+      d="M14 12a2 2 0 0 1 2-2h17a2 2 0 0 1 2 2v5H14v-5ZM14 19h9v9h-7a2 2 0 0 1-2-2v-7ZM25 19h10v7a2 2 0 0 1-2 2h-8v-9Z"
+      className={EmbeddingOptionS.Primary}
+    />
+    <path
+      d="M5 10h6v18H7a2 2 0 0 1-2-2V10ZM5 6a2 2 0 0 1 2-2h26a2 2 0 0 1 2 2v2H5V6Z"
+      className={EmbeddingOptionS.SecondaryFill}
+    />
+  </svg>
+);
