8ebb8c2d01100224ed81be371eaa28d456a223cd
Nicolò Pretto <info@npretto.com>
2024-11-05 18:43:15 +0100

    fix(49482): don't create a new store on each MetabaseProvider render (#49492)
    * fix(49482): don't create a new store on each MetabaseProvider render
    
    * remove store prop as it seems to not be used

--- enterprise/frontend/src/embedding-sdk/test/auth-flow.unit.spec.tsx
+++ enterprise/frontend/src/embedding-sdk/test/auth-flow.unit.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import fetchMock from "fetch-mock";
 
 import {
@@ -7,6 +7,7 @@ import {
   setupCurrentUserEndpoint,
   setupPropertiesEndpoints,
 } from "__support__/server-mocks";
+import { waitForLoaderToBeRemoved } from "__support__/ui";
 import { waitForRequest } from "__support__/utils";
 import {
   MetabaseProvider,
@@ -26,7 +27,7 @@ const AUTH_PROVIDER_URL = "http://auth-provider:3000/sso/metabase";
 const MOCK_API_KEY = "mock-api-key";
 const USER_CURRENT_URL = `${METABASE_INSTANCE_URL}/api/user/current`;
 const MOCK_SESSION = {
-  exp: 1729761473,
+  exp: Number.MAX_SAFE_INTEGER,
   iat: 1729760873,
   id: "5e03fb0a-2398-423f-98b3-0c4abaf0c47a",
 };
@@ -70,6 +71,37 @@ describe("SDK auth flow", () => {
 
     setupCardEndpoints(MOCK_CARD);
     setupCardQueryEndpoints(MOCK_CARD, {} as any);
+  });
+
+  it("should initialize the auth flow only once, not on rerenders", async () => {
+    const sdkConfig = defineEmbeddingSdkConfig({
+      metabaseInstanceUrl: METABASE_INSTANCE_URL,
+      jwtProviderUri: AUTH_PROVIDER_URL,
+    });
+
+    const { rerender } = setup(sdkConfig);
+
+    expect(fetchMock.calls(AUTH_PROVIDER_URL)).toHaveLength(1);
+
+    rerender(
+      <MetabaseProvider
+        config={{
+          ...sdkConfig,
+        }}
+      >
+        <StaticQuestion questionId={1} />
+      </MetabaseProvider>,
+    );
+
+    await waitForLoaderToBeRemoved();
+
+    expect(fetchMock.calls(AUTH_PROVIDER_URL)).toHaveLength(1);
+
+    expect(screen.queryByText("Initializing...")).not.toBeInTheDocument();
+    expect(
+      // this is just something we know it's on the screen when everything is ok
+      screen.getByTestId("query-visualization-root"),
+    ).toBeInTheDocument();
   });
 
   describe("when using jwtProvider", () => {
