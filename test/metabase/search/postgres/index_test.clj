fa3f57a08f984400e794d5f9148a2889f329764c
Chris Truter <crisptrutski@users.noreply.github.com>
2024-11-05 14:09:28 +0200

    Basic tsquery escaping (#49520)

--- test/metabase/search/postgres/index_test.clj
+++ test/metabase/search/postgres/index_test.clj
@@ -164,29 +164,41 @@
 (def search-expr #'search.index/to-tsquery-expr)
 
 (deftest to-tsquery-expr-test
-  (is (= "a & b & c:*"
+  (is (= "'a' & 'b' & 'c':*"
          (search-expr "a b c")))
 
-  (is (= "a & b & c:*"
+  (is (= "'a' & 'b' & 'c':*"
          (search-expr "a AND b AND c")))
 
-  (is (= "a & b & c"
+  (is (= "'a' & 'b' & 'c'"
          (search-expr "a b \"c\"")))
 
-  (is (= "a & b | c:*"
+  (is (= "'a' & 'b' | 'c':*"
          (search-expr "a b or c")))
 
-  (is (= "this & !that:*"
+  (is (= "'this' & !'that':*"
          (search-expr "this -that")))
 
-  (is (= "a & b & c <-> d & e | b & e:*"
+  (is (= "'a' & 'b' & 'c' <-> 'd' & 'e' | 'b' & 'e':*"
          (search-expr "a b \" c d\" e or b e")))
 
-  (is  (= "ab <-> and <-> cde <-> f | !abc & def & ghi | jkl <-> mno <-> or <-> pqr"
+  (is  (= "'ab' <-> 'and' <-> 'cde' <-> 'f' | !'abc' & 'def' & 'ghi' | 'jkl' <-> 'mno' <-> 'or' <-> 'pqr'"
           (search-expr "\"ab and cde f\" or -abc def AND ghi OR \"jkl mno OR pqr\"")))
 
-  (is (= "big & data | business <-> intelligence | data & wrangling:*"
+  (is (= "'big' & 'data' | 'business' <-> 'intelligence' | 'data' & 'wrangling':*"
          (search-expr "Big Data oR \"Business Intelligence\" OR data and wrangling")))
 
-  (is (= "partial <-> quoted <-> and <-> or <-> -split:*"
-         (search-expr "\"partial quoted AND OR -split"))))
+  (testing "unbalanced quotes"
+    (is (= "'big' <-> 'data' & 'big' <-> 'mistake':*"
+           (search-expr "\"Big Data\" \"Big Mistake"))))
+
+  (is (= "'partial' <-> 'quoted' <-> 'and' <-> 'or' <-> '-split':*"
+         (search-expr "\"partial quoted AND OR -split")))
+
+  (testing "dangerous characters"
+    (is (= "'you' & '<-' & 'pointing':*"
+           (search-expr "you <- pointing"))))
+
+  (testing "single quotes"
+    (is (= "'you''re':*"
+           (search-expr "you're")))))
