b6a09d3f8413eaaeacfe4114bbe17971c176c450
John Swanson <john.swanson@metabase.com>
2024-11-05 11:53:08 -0800

    fix confirmation flow

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/confirm_invite_user.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/confirm_invite_user.clj
@@ -9,7 +9,7 @@
   api/*is-superuser?*)
 
 (mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/confirm-invite-user
-  [_tool-name {:keys [email], :as args}]
+  [_tool-name {:keys [email], :as _argument-map}]
   {:reactions [{:type :metabot.reaction/confirmation
                 :description (format "Invite a user with email '%s' to Metabase?" email)
                 :options {:yes [{:type :metabot.reaction/api-call
@@ -17,9 +17,7 @@
                                             :url "/api/user"
                                             :body {:email email}}}
                                 {:type :metabot.reaction/writeback
-                                 :message (format "Tool call %s is now complete. The user confirmed the operation and the specified user has been invited to Metabase."
-                                                  (:metabot-v3/tool-call-id args))}]
+                                 :message "<system message>The user confirmed the operation and the specified user has been invited to Metabase.</system message>"}]
                           :no [{:type :metabot.reaction/writeback
-                                :message (format "Tool call %s is now complete. The user refused the operation. Ask if they need anything else."
-                                                 (:metabot-v3/tool-call-id args))}]}}]
+                                :message "<system message>The user refused the operation. Ask if they need anything else.</system message>"}]}}]
    :output "Confirmation required - awaiting user input."})
