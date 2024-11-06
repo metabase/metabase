2bb81d2cc909c54ec71b01115d51e21055266d25
Bryan Maass <bryan.maass@gmail.com>
2024-11-06 12:56:01 -0700

    migrations: expiration_date +  notified_at insert function: add stuff.
    todo: make them consistent + add api

--- src/metabase/models/moderation_review.clj
+++ src/metabase/models/moderation_review.clj
@@ -2,10 +2,12 @@
   "TODO -- this should be moved to `metabase-enterprise.content-verification.models.moderation-review` since it's a
   premium-only model."
   (:require
+   [malli.experimental.time]
    [metabase.db.query :as mdb.query]
    [metabase.models.interface :as mi]
    [metabase.models.permissions :as perms]
    [metabase.moderation :as moderation]
+   [metabase.util.i18n :refer [tru]]
    [metabase.util.malli :as mu]
    [metabase.util.malli.schema :as ms]
    [methodical.core :as methodical]
@@ -13,18 +15,18 @@
 
 (def statuses
   "Schema enum of the acceptable values for the `status` column"
-  #{"verified" nil})
+  #{"verified" "neutral" "expired" "flagged" nil})
 
 (def Statuses
   "Schema of valid statuses"
   [:maybe (into [:enum] statuses)])
 
 (def ^:private reasons
-  {:no-updates        "This won't be updated"
-   :temp-maintenance  "In temporary maintenance"
-   :data-problem      "Problems with the data"
-   :needs-replacement "Model needs replacing"
-   :other             "Other"})
+  {:no-updates        (tru "This won't be updated")
+   :temp-maintenance  (tru "In temporary maintenance")
+   :data-problem      (tru "Problems with the data")
+   :needs-replacement (tru "Model needs replacing")
+   :other             (tru "Other")})
 
 (def Reasons
   "Reasons that a verifiable entity would be flagged"
@@ -64,8 +66,8 @@
   10)
 
 (mu/defn delete-extra-reviews!
-  "Delete extra reviews to maintain an invariant of only `max-moderation-reviews`. Called before inserting so actuall
-  insures there are one fewer than that so you can add afterwards."
+  "Delete extra reviews to maintain an invariant of only `max-moderation-reviews`. Called before inserting so actually
+  ensures there are one fewer than that so you can add one afterwards."
   [item-id   :- :int
    item-type :- :string]
   (let [ids (into #{} (comp (map :id)
@@ -75,24 +77,25 @@
                                     :where    [:and
                                                [:= :moderated_item_id item-id]
                                                [:= :moderated_item_type item-type]]
-                                    ;; cannot put the offset in this query as mysql doesnt place nice. It requires a limit
-                                    ;; as well which we do not want to give. The offset is only 10 though so its not a huge
-                                    ;; savings and we run this on every entry so the max number is 10, delete the extra,
-                                    ;; and insert a new one to arrive at 10 again, our invariant.
+                                    ;; cannot put the offset in this query as mysql doesnt play nice. It requires a
+                                    ;; limit as well which we do not want to give. The offset is only 10 though so its
+                                    ;; not a huge savings and we run this on every entry so the max number is 10,
+                                    ;; delete the extra, and insert a new one to arrive at 10 again, our invariant.
                                     :order-by [[:id :desc]]}))]
     (when (seq ids)
       (t2/delete! ModerationReview :id [:in ids]))))
 
 (mu/defn create-review!
   "Create a new ModerationReview"
+  ;; TODO: once we add "flagged" status, check that reason is only allowed for those.
   [review :- [:map
               [:moderated_item_id   ms/PositiveInt]
               [:moderated_item_type moderation/moderated-item-types]
               [:moderator_id        ms/PositiveInt]
               [:status              {:optional true} Statuses]
               [:reason              {:optional true} Reasons]
-              [:text                {:optional true} [:maybe :string]]]]
-  ;; TODO: once we add "flagged" status, check that reason is only allowed for those.
+              [:text                {:optional true} [:maybe :string]]
+              [:expiration-date     :time/local-date]]]
   (t2/with-transaction [_conn]
     (delete-extra-reviews! (:moderated_item_id review) (:moderated_item_type review))
     (t2/update! ModerationReview {:moderated_item_id   (:moderated_item_id review)
