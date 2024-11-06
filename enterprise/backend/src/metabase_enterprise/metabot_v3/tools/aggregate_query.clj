bf214dadd7e6d28a587083c841b5384da6f5f270
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-11-06 13:43:59 -0500

    Fix lint

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/aggregate_query.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/aggregate_query.clj
@@ -1,7 +1,7 @@
 (ns metabase-enterprise.metabot-v3.tools.aggregate-query
   (:require
-    [metabase-enterprise.metabot-v3.tools.interface :as metabot-v3.tools.interface]
-    [metabase.util.malli :as mu]))
+   [metabase-enterprise.metabot-v3.tools.interface :as metabot-v3.tools.interface]
+   [metabase.util.malli :as mu]))
 
 (mu/defmethod metabot-v3.tools.interface/*tool-applicable?* :metabot.tool/aggregate-query
   [_tool-name context]
