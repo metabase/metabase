aa9124be3bd23ac15bbf36387a4b18bc0bb78067
Sloan Sparger <sloansparger@gmail.com>
2024-11-06 14:06:23 -0600

    FE - change error messages

--- enterprise/frontend/src/metabase-enterprise/metabot/reactions/errors.ts
+++ enterprise/frontend/src/metabase-enterprise/metabot/reactions/errors.ts
@@ -15,10 +15,3 @@ export const notifyUnknownReaction: ReactionHandler<
     dispatch(addUserMessage(t`Whoops, I actually can’t do this. Sorry.`));
   };
 };
-
-export const notifyUnkownError: ReactionHandler<void> = () => {
-  return ({ dispatch }) => {
-    dispatch(clearUserMessages());
-    dispatch(addUserMessage(t`I can’t do that, unfortunately.`));
-  };
-};
