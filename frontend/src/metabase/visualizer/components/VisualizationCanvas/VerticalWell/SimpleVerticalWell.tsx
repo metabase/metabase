7a0457c49626bc9db0b9d3841028e7284c622003
Anton Kulyk <kuliks.anton@gmail.com>
2024-11-06 16:23:26 +0000

    Add hover state to vertical well

--- frontend/src/metabase/visualizer/components/VisualizationCanvas/VerticalWell/SimpleVerticalWell.tsx
+++ frontend/src/metabase/visualizer/components/VisualizationCanvas/VerticalWell/SimpleVerticalWell.tsx
@@ -6,11 +6,16 @@ import { DROPPABLE_ID } from "metabase/visualizer/dnd/constants";
 import { WellItem } from "../WellItem";
 
 interface SimpleVerticalWellProps {
-  name: string;
+  name?: string;
 }
 
 export function SimpleVerticalWell({ name }: SimpleVerticalWellProps) {
-  const { setNodeRef } = useDroppable({ id: DROPPABLE_ID.Y_AXIS_WELL });
+  const { active, isOver, setNodeRef } = useDroppable({
+    id: DROPPABLE_ID.Y_AXIS_WELL,
+  });
+
+  const borderStyle = name ? "solid" : "dashed";
+  const borderColor = active ? "var(--mb-color-brand)" : "var(--border-color)";
 
   return (
     <Flex
@@ -18,17 +23,21 @@ export function SimpleVerticalWell({ name }: SimpleVerticalWellProps) {
       pos="relative"
       align="center"
       justify="center"
-      bg="var(--mb-color-bg-light)"
+      bg={active ? "var(--mb-color-brand-light)" : "bg-light"}
       p="md"
       wrap="nowrap"
       style={{
         borderRadius: "var(--border-radius-xl)",
-        border: `1px solid var(--mb-color-border)`,
+        border: `1px ${borderStyle} ${borderColor}`,
+        transform: active ? "scale(1.025)" : "scale(1)",
+        transition:
+          "transform 0.2s ease-in-out 0.2s, border-color 0.2s ease-in-out 0.2s, background 0.2s ease-in-out 0.2s",
+        outline: isOver ? "1px solid var(--mb-color-brand)" : "none",
       }}
       ref={setNodeRef}
     >
       <WellItem style={{ position: "absolute", transform: "rotate(-90deg)" }}>
-        <Text truncate>{name}</Text>
+        <Text truncate>{name ?? "Metric"}</Text>
       </WellItem>
     </Flex>
   );
