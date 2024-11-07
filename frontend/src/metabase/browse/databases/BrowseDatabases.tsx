862612a773ea98ea1e97edc0cef521d9aa8fcecf
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-11-07 13:11:02 +0100

    Implement "Add database" button

--- frontend/src/metabase/browse/databases/BrowseDatabases.tsx
+++ frontend/src/metabase/browse/databases/BrowseDatabases.tsx
@@ -1,28 +1,41 @@
+import cx from "classnames";
+import { Link } from "react-router";
 import { t } from "ttag";
 
 import NoResults from "assets/img/no_results.svg";
 import { useListDatabasesQuery } from "metabase/api";
 import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 import CS from "metabase/css/core/index.css";
+import { getEngineLogo } from "metabase/databases/utils/engine";
 import { color } from "metabase/lib/colors";
+import { useSelector } from "metabase/lib/redux";
 import * as Urls from "metabase/lib/urls";
-import { Box, Icon, Title } from "metabase/ui";
+import { newDatabase } from "metabase/lib/urls";
+import { getUserIsAdmin } from "metabase/selectors/user";
+import {
+  Box,
+  Group,
+  Icon,
+  Stack,
+  type StackProps,
+  Text,
+  Title,
+} from "metabase/ui";
 
 import {
   BrowseContainer,
+  BrowseGrid,
   BrowseMain,
   BrowseSection,
   CenteredEmptyState,
 } from "../components/BrowseContainer.styled";
 import { BrowseDataHeader } from "../components/BrowseDataHeader";
 
-import {
-  DatabaseCard,
-  DatabaseCardLink,
-  DatabaseGrid,
-} from "./BrowseDatabases.styled";
+import DB from "./BrowseDatabases.module.css";
 
 export const BrowseDatabases = () => {
+  const isAdmin = useSelector(getUserIsAdmin);
+
   const { data, isLoading, error } = useListDatabasesQuery();
   const databases = data?.data;
 
@@ -52,27 +65,72 @@ export const BrowseDatabases = () => {
       <BrowseDataHeader />
       <BrowseMain>
         <BrowseSection>
-          <DatabaseGrid data-testid="database-browser">
+          <BrowseGrid data-testid="database-browser">
             {databases.map(database => (
-              <div key={database.id}>
-                <DatabaseCardLink to={Urls.browseDatabase(database)}>
-                  <DatabaseCard>
-                    <Icon
-                      name="database"
-                      color={color("accent2")}
-                      className={CS.mb3}
-                      size={32}
-                    />
-                    <Title order={2} size="1rem" lh="1rem" color="inherit">
-                      {database.name}
-                    </Title>
-                  </DatabaseCard>
-                </DatabaseCardLink>
-              </div>
+              <Link to={Urls.browseDatabase(database)} key={database.id}>
+                <DatabaseCard
+                  bg="bg-white"
+                  className={cx(CS.rounded, CS.bordered, DB.dbCard)}
+                >
+                  <Icon name="database" color={color("accent2")} size={32} />
+                  <Title order={2} size="md" lh="md" color="inherit">
+                    {database.name}
+                  </Title>
+                </DatabaseCard>
+              </Link>
             ))}
-          </DatabaseGrid>
+            {isAdmin && (
+              <Link to={newDatabase()}>
+                <DatabaseCard
+                  className={cx(CS.rounded, CS.bordered, DB.addCard)}
+                >
+                  <Group spacing="xs">
+                    <CardImageWrapper database={"postgres"} />
+                    <CardImageWrapper database={"mysql"} />
+                    <CardImageWrapper database={"snowflake"} />
+                  </Group>
+                  <div>
+                    <Title order={2} size="md" lh={1} color="inherit">
+                      {t`Add a database`}
+                    </Title>
+                    <Text
+                      color="inherit"
+                      fz="sm"
+                      lh={1}
+                      span
+                    >{t`20+ data connectors. Start exploring in minutes.`}</Text>
+                  </div>
+                </DatabaseCard>
+              </Link>
+            )}
+          </BrowseGrid>
         </BrowseSection>
       </BrowseMain>
     </BrowseContainer>
+  );
+};
+
+const CardImageWrapper = ({ database }: { database: string }) => {
+  return (
+    <Box
+      bg="white"
+      h="xl"
+      w="xl"
+      className={CS.rounded}
+      style={{
+        boxShadow:
+          "0px 0px 0px 1px rgba(0, 0, 0, 0.05), 0px 1px 4px 0px rgba(0, 0, 0, 0.10)",
+      }}
+    >
+      <img src={getEngineLogo(database)} alt={t`${database} database logo`} />
+    </Box>
+  );
+};
+
+const DatabaseCard = ({ children, ...props }: StackProps) => {
+  return (
+    <Stack h="8.5rem" justify="space-between" p="lg" {...props}>
+      {children}
+    </Stack>
   );
 };
