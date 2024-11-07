39ef03f9c279fa528e8d3f7907c7e1f081a7cbee
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2024-11-07 17:01:55 -0500

    change axes names tool (#49640)

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools.clj
@@ -4,6 +4,7 @@
    [clojure.string :as str]
    [malli.core :as mc]
    [malli.transform :as mtx]
+   [metabase-enterprise.metabot-v3.tools.change-axes-labels]
    [metabase-enterprise.metabot-v3.tools.change-display-type]
    [metabase-enterprise.metabot-v3.tools.change-query]
    [metabase-enterprise.metabot-v3.tools.change-table-visualization-settings]
@@ -25,6 +26,7 @@
   metabase-enterprise.metabot-v3.tools.confirm-invite-user/keep-me
   metabase-enterprise.metabot-v3.tools.change-display-type/keep-me
   metabase-enterprise.metabot-v3.tools.change-query/keep-me
+  metabase-enterprise.metabot-v3.tools.change-axes-labels/keep-me
   metabase-enterprise.metabot-v3.tools.change-table-visualization-settings/keep-me
   metabase-enterprise.metabot-v3.tools.who-is-your-favorite/keep-me)
 
