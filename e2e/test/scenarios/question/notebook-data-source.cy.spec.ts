84883041b431ae643375df787c471cccd3a65e10
Kamil Mielnik <kamil@kamilmielnik.com>
2024-11-08 20:49:20 +0700

    Fix flaky test

--- e2e/test/scenarios/question/notebook-data-source.cy.spec.ts
+++ e2e/test/scenarios/question/notebook-data-source.cy.spec.ts
@@ -435,13 +435,7 @@ describe("issue 28106", () => {
           .findByTestId("scroll-container")
           .as("schemasList");
 
-        // The list is virtualized and the scrollbar height changes during scrolling (metabase#44966)
-        // that's why we need to scroll and wait multiple times.
-        // Test is flaky because of this - that's why there are 3 attempts.
-        for (let i = 0; i < 3; ++i) {
-          cy.get("@schemasList").scrollTo("bottom");
-          cy.wait(100);
-        }
+        scrollAllTheWayDown();
 
         // assert scrolling worked and the last item is visible
         entityPickerModalItem(1, "Public").should("be.visible");
@@ -460,6 +454,22 @@ describe("issue 28106", () => {
       });
     },
   );
+
+  // The list is virtualized and the scrollbar height changes during scrolling (metabase#44966)
+  // that's why we need to scroll multiple times.
+  function scrollAllTheWayDown() {
+    cy.get("@schemasList").realMouseWheel({ deltaY: 100 });
+
+    cy.get("@schemasList").then($element => {
+      const list = $element[0];
+      const isScrolledAllTheWayDown =
+        list.scrollHeight - list.scrollTop === list.clientHeight;
+
+      if (!isScrolledAllTheWayDown) {
+        scrollAllTheWayDown();
+      }
+    });
+  }
 });
 
 describe("issue 32252", () => {
