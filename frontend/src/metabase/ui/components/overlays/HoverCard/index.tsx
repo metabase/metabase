9645b6e6ca1a1d53c6b5c284c55cad6e4ae73720
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-11-08 10:19:51 -0500

    Stacking: HoverCards

--- frontend/src/metabase/ui/components/overlays/HoverCard/index.tsx
+++ frontend/src/metabase/ui/components/overlays/HoverCard/index.tsx
@@ -1,3 +1,12 @@
-export { HoverCard } from "@mantine/core";
+import {
+  type HoverCardProps,
+  HoverCard as MantineHoverCard,
+} from "@mantine/core";
+
+import { withLazyPortal } from "../utils";
 export type { HoverCardProps } from "@mantine/core";
 export { getHoverCardOverrides } from "./HoverCard.styled";
+
+export const HoverCard = (props: HoverCardProps) => {
+  return <MantineHoverCard {...withLazyPortal(props)} />;
+};
