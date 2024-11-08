f493f05f63530324a374812c49fcfac9ce35d270
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-11-07 22:16:47 -0500

    group into other

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/change_chart_appearance.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/change_chart_appearance.clj
@@ -4,7 +4,7 @@
    [metabase.util.malli :as mu]))
 
 (mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/change-chart-appearance
-  [_tool-name {goal :goal, trend-line :trend-line, data-labels :data-labels, total :total, stack-type :stack-type}]
+  [_tool-name {goal :goal, trend-line :trend-line, data-labels :data-labels, total :total, stack-type :stack-type, max-series-count :max-series-count}]
   {:reactions [{:type :metabot.reaction/change-chart-appearance
                 :goal (when goal
                        {:goal_value (:goal_value goal)
@@ -16,5 +16,6 @@
                               :data_label_format (:data_label_format data-labels)
                               :pie_chart_percent_visibility (:pie_chart_percent_visibility data-labels)})
                 :total total
-                :stack_type stack-type}]
+                :stack_type stack-type
+                :max_series_count max-series-count}]
    :output "success"})
