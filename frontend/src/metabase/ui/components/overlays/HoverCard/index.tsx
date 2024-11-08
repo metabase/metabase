1cc46e6ee6b7e101e9bf2c130e55e6b56f6b16af
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-11-08 10:51:30 -0500

    Fix Hovercards

--- frontend/src/metabase/ui/components/overlays/HoverCard/index.tsx
+++ frontend/src/metabase/ui/components/overlays/HoverCard/index.tsx
@@ -4,9 +4,10 @@ import {
 } from "@mantine/core";
 
 import { withLazyPortal } from "../utils";
-export type { HoverCardProps } from "@mantine/core";
 export { getHoverCardOverrides } from "./HoverCard.styled";
 
 export const HoverCard = (props: HoverCardProps) => {
   return <MantineHoverCard {...withLazyPortal(props)} />;
 };
+HoverCard.Target = MantineHoverCard.Target;
+HoverCard.Dropdown = MantineHoverCard.Dropdown;
