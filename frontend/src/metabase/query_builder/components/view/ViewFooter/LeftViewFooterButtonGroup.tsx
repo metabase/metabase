58c406eba497784635765402ee4503b75f71a2aa
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-11-08 11:09:03 +0300

    update color

--- frontend/src/metabase/query_builder/components/view/ViewFooter/LeftViewFooterButtonGroup.tsx
+++ frontend/src/metabase/query_builder/components/view/ViewFooter/LeftViewFooterButtonGroup.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { t } from "ttag";
 
 import { EditorViewControl } from "embedding-sdk/components/private/EditorViewControl";
@@ -10,7 +11,10 @@ import {
 } from "metabase/query_builder/actions";
 // import ViewButton from "metabase/query_builder/components/view/ViewButton";
 // import { FooterButtonGroup } from "metabase/query_builder/components/view/ViewFooter.styled";
-import { getIsShowingRawTable, getUiControls } from "metabase/query_builder/selectors";
+import {
+  getIsShowingRawTable,
+  getUiControls,
+} from "metabase/query_builder/selectors";
 import { Button, Flex, Icon, Tooltip } from "metabase/ui";
 import { getIconForVisualizationType } from "metabase/visualizations";
 import type Question from "metabase-lib/v1/Question";
@@ -20,41 +24,68 @@ interface LeftViewFooterButtonGroupProps {
   question: Question;
 }
 
-export const LeftViewFooterButtonGroup = ({ question }: LeftViewFooterButtonGroupProps) => {
-  const {
-    isShowingChartSettingsSidebar
-  }: QueryBuilderUIControls = useSelector(getUiControls);
+export const LeftViewFooterButtonGroup = ({
+  question,
+}: LeftViewFooterButtonGroupProps) => {
+  const { isShowingChartSettingsSidebar }: QueryBuilderUIControls =
+    useSelector(getUiControls);
 
   const dispatch = useDispatch();
   const isShowingRawTable = useSelector(getIsShowingRawTable);
   const vizIcon = getIconForVisualizationType(question.display());
 
-  const data = [
-    {
-      value: "editor",
-      label: <Tooltip label={t`Editor`}><Icon name="notebook" onClick={() => {
-        dispatch(setQueryBuilderMode("notebook"));
-      }} /></Tooltip>,
-    },
-    {
-      value: "table",
-      label: <Tooltip label={t`Results`}><Icon name="table2" onClick={() => {
-        dispatch(setUIControls({ isShowingRawTable: true }));
-      }} /></Tooltip>,
-    },
-    {
-      value: "visualization",
-      // also we need to add a spinner :boom:
-      label: <Tooltip label={t`Visualization`}><Icon name={vizIcon} onClick={() => {
-        dispatch(setUIControls({ isShowingRawTable: false }));
-      }} /></Tooltip>,
-    },
-  ];
-
+  const data = useMemo(
+    () => [
+      {
+        value: "editor",
+        label: (
+          <Tooltip label={t`Editor`}>
+            <Icon
+              name="notebook"
+              onClick={() => {
+                dispatch(setQueryBuilderMode("notebook"));
+              }}
+            />
+          </Tooltip>
+        ),
+      },
+      {
+        value: "table",
+        label: (
+          <Tooltip label={t`Results`}>
+            <Icon
+              name="table2"
+              onClick={() => {
+                dispatch(setUIControls({ isShowingRawTable: true }));
+              }}
+            />
+          </Tooltip>
+        ),
+      },
+      {
+        value: "visualization",
+        // also we need to add a spinner :boom:
+        label: (
+          <Tooltip label={t`Visualization`}>
+            <Icon
+              name={vizIcon}
+              onClick={() => {
+                dispatch(setUIControls({ isShowingRawTable: false }));
+              }}
+            />
+          </Tooltip>
+        ),
+      },
+    ],
+    [dispatch, vizIcon],
+  );
 
   return (
     <Flex gap="0.75rem">
-      <EditorViewControl value={isShowingRawTable ? "table" : "visualization"} data={data} />
+      <EditorViewControl
+        value={isShowingRawTable ? "table" : "visualization"}
+        data={data}
+      />
       {/* <ViewButton
             medium
             labelBreakpoint="sm"
@@ -83,25 +114,33 @@ export const LeftViewFooterButtonGroup = ({ question }: LeftViewFooterButtonGrou
             }
           >{isShowingChartSettingsSidebar ? t`Done` : t`Settings`}</ViewButton> */}
       {/* TODO: mah is a hack for 32px height button */}
-      {!isShowingRawTable && <Button radius="xl" variant={isShowingChartSettingsSidebar ? "filled" : "default"} styles={
-        {
-          ...(!isShowingChartSettingsSidebar && {
-            root: {
-              backgroundColor: "var(--mb-color-brand-lighter)",
-              color: "var(--mb-color-brand)",
-              border: 0,
+      {!isShowingRawTable && (
+        <Button
+          radius="xl"
+          variant={isShowingChartSettingsSidebar ? "filled" : "default"}
+          styles={{
+            ...(!isShowingChartSettingsSidebar && {
+              root: {
+                backgroundColor: "var(--mb-color-brand-lighter)",
+                color: "var(--mb-color-brand)",
+                border: 0,
 
-              "&:hover": {
-                backgroundColor: "var(--mb-base-color-blue-20)",
-              }
-            }
-          })
-        }
-      } mah="xl" onClick={isShowingChartSettingsSidebar
-        ? () => dispatch(onCloseChartSettings())
-        : () => dispatch(onOpenChartSettings())
-      }>{isShowingChartSettingsSidebar ? t`Done` : t`Chart settings`}</Button>
-      }
+                "&:hover": {
+                  backgroundColor: "var(--mb-color-focus)",
+                },
+              },
+            }),
+          }}
+          mah="xl"
+          onClick={
+            isShowingChartSettingsSidebar
+              ? () => dispatch(onCloseChartSettings())
+              : () => dispatch(onOpenChartSettings())
+          }
+        >
+          {isShowingChartSettingsSidebar ? t`Done` : t`Chart settings`}
+        </Button>
+      )}
     </Flex>
   );
 };
