be1910d00377b07843dbc8921383f9c9173047b1
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-11-08 13:33:47 -0500

    Fix Guard

--- frontend/src/metabase/ui/components/overlays/Guard.tsx
+++ frontend/src/metabase/ui/components/overlays/Guard.tsx
@@ -1,6 +1,9 @@
 import type { ModalBaseProps } from "@mantine/core";
 
-/** Prevent eager portal rendering */
+/** Prevent eager portal rendering.
+ *
+ * The point of doing this is to stop any floating element from popping up
+ * underneath another one already on the page */
 export const Guard = <
   T extends Partial<
     Pick<ModalBaseProps, "withinPortal" | "opened" | "children">
@@ -8,8 +11,8 @@ export const Guard = <
 >(
   props: T,
 ) => {
-  const { withinPortal, opened, children } = props;
-  if (withinPortal && opened) {
+  const { withinPortal = true, opened, children } = props;
+  if (withinPortal && !opened) {
     return null;
   }
   return <>{children}</>;
