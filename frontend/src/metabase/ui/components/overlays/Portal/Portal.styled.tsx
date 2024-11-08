96f777cecd50944b4b66b56b4e724b70f4b6d2e2
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-11-08 12:43:16 -0500

    Set portal z-index via classname and add !important

--- frontend/src/metabase/ui/components/overlays/Portal/Portal.styled.tsx
+++ frontend/src/metabase/ui/components/overlays/Portal/Portal.styled.tsx
@@ -4,10 +4,6 @@ import ZIndex from "metabase/css/core/z-index.module.css";
 
 export const getPortalOverrides = (): MantineThemeOverride["components"] => ({
   Portal: {
-    // FIXME: Do just one of these
-    defaultProps: {
-      zIndex: "var(--mb-floating-element-z-index)",
-    },
     classNames: { dropdown: ZIndex.FloatingElement },
   },
 });
