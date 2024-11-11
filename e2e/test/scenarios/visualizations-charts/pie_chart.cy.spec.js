6ad7afdf57a3d70802cb98c51d3d9fd3d2cc9789
lbrdnk <lbrdnk@users.noreply.github.com>
2024-11-11 15:03:55 +0100

    Update pie_chart.cy.spec.js

--- e2e/test/scenarios/visualizations-charts/pie_chart.cy.spec.js
+++ e2e/test/scenarios/visualizations-charts/pie_chart.cy.spec.js
@@ -389,7 +389,7 @@ describe("scenarios > visualizations > pie chart", () => {
     });
 
     assertEChartsTooltip({
-      header: "Created At",
+      header: "Created At: Day of week",
       rows: [
         {
           color: "#51528D",
