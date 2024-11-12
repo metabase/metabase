5c2e2301b8e396827d939e665a11017790f5dd05
Nicolò Pretto <info@npretto.com>
2024-11-12 18:20:41 +0100

    use the alert component for the default sdk error component

--- enterprise/frontend/src/embedding-sdk/components/private/PublicComponentWrapper/SdkError.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/PublicComponentWrapper/SdkError.tsx
@@ -2,6 +2,7 @@ import { useSdkSelector } from "embedding-sdk/store";
 import { getErrorComponent } from "embedding-sdk/store/selectors";
 import type { SdkErrorComponentProps } from "embedding-sdk/store/types";
 import Alert from "metabase/core/components/Alert";
+import { color } from "metabase/lib/colors";
 import { Box, Center } from "metabase/ui";
 
 export const SdkError = ({ message }: SdkErrorComponentProps) => {
@@ -16,8 +17,15 @@ export const SdkError = ({ message }: SdkErrorComponentProps) => {
   );
 };
 
+const FORCE_DARK_TEXT_COLOR = {
+  // The Alert component has a light background, we need to force a dark text
+  // color. The sdk aliases text-dark to the primary color, which in dark themes
+  // is a light color, making the text un-readable
+  "--mb-color-text-dark": color("text-dark"),
+} as React.CSSProperties;
+
 const DefaultErrorMessage = ({ message }: SdkErrorComponentProps) => (
-  <Box p="sm">
+  <Box p="sm" style={FORCE_DARK_TEXT_COLOR}>
     <Alert variant="error" icon="warning">
       {message}
     </Alert>
