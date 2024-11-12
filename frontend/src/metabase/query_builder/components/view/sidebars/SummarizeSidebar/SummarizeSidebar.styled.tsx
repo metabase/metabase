6f0d3aac8deba46a075e0ea6dfd0201f57982066
Oisin Coveney <oisin@metabase.com>
2024-11-12 09:38:38 +0200

    Remove some styled components in Summarize (#49836)

--- frontend/src/metabase/query_builder/components/view/sidebars/SummarizeSidebar/SummarizeSidebar.styled.tsx
+++ frontend/src/metabase/query_builder/components/view/sidebars/SummarizeSidebar/SummarizeSidebar.styled.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 
-import { AggregationPicker as BaseAggregationPicker } from "metabase/common/components/AggregationPicker";
 import SidebarContent from "metabase/query_builder/components/SidebarContent";
 
 export const SidebarView = styled(SidebarContent)`
@@ -9,28 +8,4 @@ export const SidebarView = styled(SidebarContent)`
   left: 0;
   right: 0;
   bottom: 0;
-`;
-
-const Section = styled.section`
-  padding: 1.5rem;
-`;
-
-export const SectionTitle = styled.h3`
-  font-weight: 900;
-  margin-bottom: 1rem;
-`;
-
-export const AggregationsContainer = styled(Section)`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0.5rem;
-  padding-top: 0;
-`;
-
-export const AggregationPicker = styled(BaseAggregationPicker)`
-  color: var(--mb-color-summarize);
-`;
-
-export const ColumnListContainer = styled(Section)`
-  border-top: 1px solid var(--mb-color-border);
 `;
