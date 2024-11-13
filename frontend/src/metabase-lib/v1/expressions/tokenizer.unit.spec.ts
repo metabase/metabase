e52307c8bc0f92a769e408e8804fa244bee54788
Kamil Mielnik <kamil@kamilmielnik.com>
2024-11-13 20:43:14 +0700

    Add basic test case

--- frontend/src/metabase-lib/v1/expressions/tokenizer.unit.spec.ts
+++ frontend/src/metabase-lib/v1/expressions/tokenizer.unit.spec.ts
@@ -1,8 +1,8 @@
 import { TOKEN, tokenize } from "./tokenizer";
 
 describe("tokenizer", () => {
-  it("takes commas into account when dealing with incomplete tokens", () => {
-    const { tokens } = tokenize('case([Total] > 200, [to, "Nothing")');
+  it("tokenizes valid expression", () => {
+    const { tokens } = tokenize('case([Total] > 200, [T], "Nothing")');
 
     expect(tokens).toEqual([
       { type: TOKEN.Identifier, start: 0, end: 4 }, // case
@@ -11,9 +11,26 @@ describe("tokenizer", () => {
       { type: TOKEN.Operator, op: ">", start: 13, end: 14 }, // >
       { type: TOKEN.Number, start: 15, end: 18 }, // 200
       { type: TOKEN.Operator, op: ",", start: 18, end: 19 }, // ,
-      { type: TOKEN.Identifier, start: 20, end: 23 }, // [to
-      { type: TOKEN.Operator, op: ",", start: 23, end: 34 }, // ,
-      { type: TOKEN.Identifier, start: 25, end: 34 }, // "Nothing"
+      { type: TOKEN.Identifier, start: 20, end: 23 }, // [T]
+      { type: TOKEN.Operator, op: ",", start: 23, end: 24 }, // ,
+      { type: TOKEN.String, start: 25, end: 34, value: "Nothing" }, // "Nothing"
+      { type: TOKEN.Operator, op: ")", start: 34, end: 35 }, // )
+    ]);
+  });
+
+  it("takes commas into account when dealing with incomplete tokens", () => {
+    const { tokens } = tokenize('case([Total] > 200, [To, "Nothing")');
+
+    expect(tokens).toEqual([
+      { type: TOKEN.Identifier, start: 0, end: 4 }, // case
+      { type: TOKEN.Operator, op: "(", start: 4, end: 5 }, // (
+      { type: TOKEN.Identifier, start: 5, end: 12 }, // [Total]
+      { type: TOKEN.Operator, op: ">", start: 13, end: 14 }, // >
+      { type: TOKEN.Number, start: 15, end: 18 }, // 200
+      { type: TOKEN.Operator, op: ",", start: 18, end: 19 }, // ,
+      { type: TOKEN.Identifier, start: 20, end: 23 }, // [To
+      { type: TOKEN.Operator, op: ",", start: 23, end: 24 }, // ,
+      { type: TOKEN.String, start: 25, end: 34, value: "Nothing" }, // "Nothing"
       { type: TOKEN.Operator, op: ")", start: 34, end: 35 }, // )
     ]);
   });
