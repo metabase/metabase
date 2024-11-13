c93fd7a215f83c4f59450cf9041a7b811b000977
Kamil Mielnik <kamil@kamilmielnik.com>
2024-11-13 20:40:48 +0700

    Format code

--- frontend/src/metabase-lib/v1/expressions/tokenizer.unit.spec.ts
+++ frontend/src/metabase-lib/v1/expressions/tokenizer.unit.spec.ts
@@ -2,7 +2,9 @@ import { TOKEN, tokenize } from "./tokenizer";
 
 describe("tokenizer", () => {
   it("takes commas into account when dealing with incomplete tokens", () => {
-    expect(tokenize('case([Total] > 200, [to, "Nothing")').tokens).toEqual([
+    const { tokens } = tokenize('case([Total] > 200, [to, "Nothing")');
+
+    expect(tokens).toEqual([
       { type: TOKEN.Identifier, start: 0, end: 4 }, // case
       { type: TOKEN.Operator, op: "(", start: 4, end: 5 }, // (
       { type: TOKEN.Identifier, start: 5, end: 12 }, // [Total]
