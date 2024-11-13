358f47e153271fbb4ef2496c514670ea47e5ed1f
Ngoc Khuat <qn.khuat@gmail.com>
2024-11-13 20:14:39 +0700

    cljfmt

--- src/metabase/channel/template/handlebars.clj
+++ src/metabase/channel/template/handlebars.clj
@@ -6,17 +6,17 @@
   (:import
    (com.github.jknack.handlebars
     Parser Handlebars Template)
+   (com.github.jknack.handlebars.cache TemplateCache)
    (com.github.jknack.handlebars.io
     TemplateSource
-    ClassPathTemplateLoader)
-   (com.github.jknack.handlebars.cache TemplateCache)))
+    ClassPathTemplateLoader)))
 
 (set! *warn-on-reflection* true)
 
 ;; This is a similar implementation of com.github.jknack.handlebars.cache.ConcurrentMapTemplateCache but with Atom.
 ;; It fixes a bug where the cache is not reloaded if you use the source only once.
 (deftype AtomTemplateCache
-  [cache ^:volatile-mutable reload]
+         [cache ^:volatile-mutable reload]
   TemplateCache
   (^void clear [_]
     (reset! cache {})
@@ -27,27 +27,27 @@
   (^Template get [^TemplateCache this ^TemplateSource source ^Parser parser]
     (let [[cached-source cached-template :as entry] (get @cache source)]
       (cond
-       (nil? entry)
-       (let [template (.parse parser source)]
-         ;; this is the fix for the pre-mentioned bug
-         ;; try uncomment this and run the metabase.channel.template.handlebars-test/reload-template-if-it's-changed test
-         ;; TODO: fix this upstream for the original ConcurrentMapTemplateCache
-         (.lastModified source)
-         (log/debugf "Caching template %s" source)
-         (swap! cache assoc source [source template])
-         template)
-
-       (and reload (not= (.lastModified source) (.lastModified ^TemplateSource cached-source)))
-       (do
-        (log/debugf "Reloading template %s" source)
-        (.evict this source)
+        (nil? entry)
         (let [template (.parse parser source)]
+          ;; this is the fix for the pre-mentioned bug
+          ;; try uncomment this and run the metabase.channel.template.handlebars-test/reload-template-if-it's-changed test
+          ;; TODO: fix this upstream for the original ConcurrentMapTemplateCache
+          (.lastModified source)
+          (log/debugf "Caching template %s" source)
           (swap! cache assoc source [source template])
-          template))
-       :else
-       (do
-        (log/debugf "Using cached template %s" source)
-        cached-template))))
+          template)
+
+        (and reload (not= (.lastModified source) (.lastModified ^TemplateSource cached-source)))
+        (do
+          (log/debugf "Reloading template %s" source)
+          (.evict this source)
+          (let [template (.parse parser source)]
+            (swap! cache assoc source [source template])
+            template))
+        :else
+        (do
+          (log/debugf "Using cached template %s" source)
+          cached-template))))
 
   (setReload [this value]
     (set! reload value)
