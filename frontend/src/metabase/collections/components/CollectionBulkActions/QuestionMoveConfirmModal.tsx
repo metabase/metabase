86325d6a7259ec68dac2635db34ca66bdc353eb9
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-11-13 09:04:17 -0400

    loading state

--- frontend/src/metabase/collections/components/CollectionBulkActions/QuestionMoveConfirmModal.tsx
+++ frontend/src/metabase/collections/components/CollectionBulkActions/QuestionMoveConfirmModal.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Flex, List, Modal, Text } from "metabase/ui";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  List,
+  Loader,
+  Modal,
+  Text,
+} from "metabase/ui";
 import type { CardId, CollectionItem, DashboardId } from "metabase-types/api";
 
 export type CardDashboards = {
@@ -10,53 +19,67 @@ export type CardDashboards = {
 };
 
 export const QuestionMoveConfirmModal = ({
-  cardDashboards,
+  cardDashboards = [],
   selectedItems = [],
   onConfirm,
   onClose,
+  isLoading = false,
 }: {
-  cardDashboards: CardDashboards[];
+  cardDashboards?: CardDashboards[];
   selectedItems: Pick<CollectionItem, "id" | "model" | "name">[];
   onConfirm: () => void;
   onClose: () => void;
+  isLoading?: boolean;
 }) => {
   return (
     <Modal
-      opened={cardDashboards.length > 0}
-      title="Moving a question into a dashboard will remove it from all other dashboards"
+      opened={cardDashboards.length > 0 || isLoading}
+      title={
+        !isLoading &&
+        "Moving a question into a dashboard will remove it from all other dashboards"
+      }
       onClose={onClose}
       size="lg"
+      withCloseButton={!isLoading}
     >
-      {cardDashboards.map(cd => {
-        const card = selectedItems.find(
-          item => item.id === cd.cardId && item.model === "card",
-        );
-        return (
-          <Box mt="1rem" key={`card-${cd.cardId}`}>
-            <Text>
-              Moving{" "}
-              <Text span fw={700}>
-                {card?.name}
-              </Text>{" "}
-              will remove it from the following dashboards
-            </Text>
-            <List>
-              {cd.dashboards.map(dashboard => (
-                <List.Item key={`dashboard-${dashboard.dashboardId}`}>
-                  {dashboard.name}
-                </List.Item>
-              ))}
-            </List>
-          </Box>
-        );
-      })}
+      {isLoading ? (
+        <Center>
+          <Loader size="lg" />
+        </Center>
+      ) : (
+        <>
+          {cardDashboards.map(cd => {
+            const card = selectedItems.find(
+              item => item.id === cd.cardId && item.model === "card",
+            );
+            return (
+              <Box mt="1rem" key={`card-${cd.cardId}`}>
+                <Text>
+                  Moving{" "}
+                  <Text span fw={700}>
+                    {card?.name}
+                  </Text>{" "}
+                  will remove it from the following dashboards
+                </Text>
+                <List>
+                  {cd.dashboards.map(dashboard => (
+                    <List.Item key={`dashboard-${dashboard.dashboardId}`}>
+                      {dashboard.name}
+                    </List.Item>
+                  ))}
+                </List>
+              </Box>
+            );
+          })}
 
-      <Flex justify="end" gap="1rem" mt="1rem">
-        <Button onClick={onClose}>Cancel</Button>
-        <Button variant="filled" onClick={onConfirm}>
-          Confirm
-        </Button>
-      </Flex>
+          <Flex justify="end" gap="1rem" mt="1rem">
+            <Button onClick={onClose}>Cancel</Button>
+            <Button variant="filled" onClick={onConfirm}>
+              Confirm
+            </Button>
+          </Flex>
+        </>
+      )}
     </Modal>
   );
 };
