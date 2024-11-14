c1f142811d06b0227f43e206454b243370927350
Mike Appleby <86076+appleby@users.noreply.github.com>
2024-11-14 15:36:59 -0800

    Add test for dataset query_metadata with deleted source card

--- test/metabase/api/dataset_test.clj
+++ test/metabase/api/dataset_test.clj
@@ -753,3 +753,47 @@
                                                      :dimension    [:field (mt/id :people :id) nil]
                                                      :widget-type  :id
                                                      :default      nil}}}})))))
+
+(deftest dataset-query-metadata-with-archived-and-deleted-source-card-test
+  (testing "Don't throw an error if source card is deleted (#48461)"
+    (mt/with-temp
+      [Card {card-id-1 :id} {:dataset_query (mt/mbql-query products)
+                             :database_id   (mt/id)}
+       Card {card-id-2 :id} {:dataset_query
+                             {:type     :query
+                              :query    {:source-table (str "card__" card-id-1)}
+                              :database (mt/id)}
+                             :database_id (mt/id)}]
+      (let [query {:type     :query
+                   :query    {:source-table (str "card__" card-id-2)}
+                   :database (mt/id)}]
+        (testing "Archive source card"
+          (is (=?
+               {:archived    true
+                :can_delete  true
+                :id          card-id-1
+                :database_id (mt/id)
+                :table_id    (mt/id :products)}
+               (-> (mt/user-http-request :crowberto :put 200 (str "card/" card-id-1) {:archived true})))))
+        (testing "Before delete"
+          (is (=?
+               {:fields    empty?
+                :tables    [{:id (str "card__" card-id-2)}]
+                :databases [{:id (mt/id) :engine string?}]}
+               (-> (mt/user-http-request :crowberto :post 200 "dataset/query_metadata" query)
+                   ;; The output is so large, these help debugging
+                   (update :fields #(map (fn [x] (select-keys x [:id])) %))
+                   (update :databases #(map (fn [x] (select-keys x [:id :engine])) %))
+                   (update :tables #(map (fn [x] (select-keys x [:id :name])) %))))))
+        (testing "Delete source card"
+          (is (nil? (mt/user-http-request :crowberto :delete 204 (str "card/" card-id-1)))))
+        (testing "After delete"
+          (is (=?
+               {:fields    empty?
+                :tables    empty?
+                :databases [{:id (mt/id) :engine string?}]}
+               (-> (mt/user-http-request :crowberto :post 200 "dataset/query_metadata" query)
+                   ;; The output is so large, these help debugging
+                   (update :fields #(map (fn [x] (select-keys x [:id])) %))
+                   (update :databases #(map (fn [x] (select-keys x [:id :engine])) %))
+                   (update :tables #(map (fn [x] (select-keys x [:id :name])) %))))))))))
