d86bf3f164d7d439672501285734946c22e75647
lbrdnk <lbrdnk@users.noreply.github.com>
2024-11-14 17:31:01 +0100

    Use NULL_DISPLAY_VALUE

--- frontend/src/metabase/lib/formatting/column.ts
+++ frontend/src/metabase/lib/formatting/column.ts
@@ -1,5 +1,10 @@
+import { NULL_DISPLAY_VALUE } from "metabase/lib/constants";
 import type { DatasetColumn } from "metabase-types/api/dataset";
 
 export function displayNameForColumn(column: DatasetColumn): string {
-  return column?.remapped_to_column?.display_name ?? column?.display_name ?? "";
+  return (
+    column?.remapped_to_column?.display_name ??
+    column?.display_name ??
+    NULL_DISPLAY_VALUE
+  );
 }
