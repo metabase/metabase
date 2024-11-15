3a98e2d44f3512ed8734c8319f4adca4dfbe6afb
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-11-15 15:51:22 -0500

    Bring back change-chart-appearance tool (#50104)

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools.clj
@@ -4,6 +4,7 @@
    [clojure.string :as str]
    [malli.core :as mc]
    [malli.transform :as mtx]
+   [metabase-enterprise.metabot-v3.tools.change-chart-appearance]
    [metabase-enterprise.metabot-v3.tools.change-display-type]
    [metabase-enterprise.metabot-v3.tools.change-query]
    [metabase-enterprise.metabot-v3.tools.interface :as metabot-v3.tools.interface]
@@ -20,6 +21,7 @@
 (set! *warn-on-reflection* true)
 
 (comment
+  metabase-enterprise.metabot-v3.tools.change-chart-appearance/keep-me
   metabase-enterprise.metabot-v3.tools.change-display-type/keep-me
   metabase-enterprise.metabot-v3.tools.change-query/keep-me
   metabase-enterprise.metabot-v3.tools.who-is-your-favorite/keep-me)
--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/change_chart_appearance.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/change_chart_appearance.clj
@@ -4,25 +4,11 @@
    [metabase.util.malli :as mu]))
 
 (mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/change-chart-appearance
-  [_tool-name {goal :goal, trend-line :trend-line, data-labels :data-labels, total :total, stack-type :stack-type, max-series-count :max-series-count, axes-labels :axes-labels, y-axis-range :y-axis-range}]
-  {:reactions [{:type :metabot.reaction/change-chart-appearance
-                :goal (when goal
-                        {:goal_value (:goal_value goal)
-                         :show_goal (:show_goal goal)
-                         :goal_label (:goal_label goal)})
-                :trend_line trend-line
-                :data_labels (when data-labels
-                               {:show_data_labels (:show_data_labels data-labels)
-                                :data_label_format (:data_label_format data-labels)
-                                :pie_chart_percent_visibility (:pie_chart_percent_visibility data-labels)})
-                :total total
-                :stack_type stack-type
-                :max_series_count max-series-count
-                :axes_labels (when axes-labels
-                               {:x_axis_label (:x_axis_label axes-labels)
-                                :y_axis_label (:y_axis_label axes-labels)})
-                :y_axis_range (when y-axis-range
-                                {:auto_range (:auto_range y-axis-range)
-                                 :min (:min y-axis-range)
-                                 :max (:max y-axis-range)})}]
+  [_tool-name arguments _context]
+  {:reactions [(merge {:type :metabot.reaction/change-chart-appearance}
+                       arguments)]
    :output "success"})
+
+(mu/defmethod metabot-v3.tools.interface/*tool-applicable?* :metabot.tool/change-chart-appearance
+  [_tool-name {:keys [display_type]}]
+  (some? display_type))
