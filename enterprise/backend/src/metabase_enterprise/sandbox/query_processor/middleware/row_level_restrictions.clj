0368b4d2e4dc6ad997d27cbeaf10429f6df236b5
Noah Moss <noahbmoss@gmail.com>
2024-11-15 15:26:50 -0500

    cljfmt

--- enterprise/backend/src/metabase_enterprise/sandbox/query_processor/middleware/row_level_restrictions.clj
+++ enterprise/backend/src/metabase_enterprise/sandbox/query_processor/middleware/row_level_restrictions.clj
@@ -179,11 +179,11 @@
         ;; to a custom handler, and we don't want to accidentally terminate the stream here!
         (binding [qp.pipeline/*result* qp.pipeline/default-result-handler]
           (mw.session/as-admin
-           ((requiring-resolve 'metabase.query-processor/process-query)
-            {:database (u/the-id (lib.metadata/database (qp.store/metadata-provider)))
-             :type     :query
-             :query    {:source-query source-query
-                        :limit        0}})))]
+            ((requiring-resolve 'metabase.query-processor/process-query)
+             {:database (u/the-id (lib.metadata/database (qp.store/metadata-provider)))
+              :type     :query
+              :query    {:source-query source-query
+                         :limit        0}})))]
     (or (-> result :data :results_metadata :columns not-empty)
         (throw (ex-info (tru "Error running query to determine metadata")
                         {:source-query source-query
