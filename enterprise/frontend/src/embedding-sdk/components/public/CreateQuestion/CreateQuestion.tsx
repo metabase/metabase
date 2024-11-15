fe95a7800295218af2d2447f9b051b399c76e473
Phoomparin Mano <phoomparin@gmail.com>
2024-11-15 23:23:11 +0700

    revamp the create question component

--- enterprise/frontend/src/embedding-sdk/components/public/CreateQuestion/CreateQuestion.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/CreateQuestion/CreateQuestion.tsx
@@ -1,23 +1,104 @@
-import { QuestionEditor } from "embedding-sdk/components/private/QuestionEditor";
+import { useState } from "react";
 
-import type { InteractiveQuestionProps } from "../InteractiveQuestion";
+import { FlexibleSizeComponent } from "embedding-sdk";
+import { useInteractiveQuestionContext } from "embedding-sdk/components/private/InteractiveQuestion/context";
+import { SaveQuestionModal } from "metabase/containers/SaveQuestionModal";
+import { Button, Flex } from "metabase/ui";
 
-type CreateQuestionProps = Omit<InteractiveQuestionProps, "questionId">;
+import {
+  InteractiveQuestion,
+  type InteractiveQuestionProps,
+} from "../InteractiveQuestion";
+
+type CreateQuestionProps = Omit<
+  InteractiveQuestionProps,
+  "questionId" | "children"
+>;
 
 export const CreateQuestion = ({
-  plugins,
   onSave,
-  onBeforeSave,
-  entityTypeFilter,
-  isSaveEnabled,
-  saveToCollectionId,
-}: CreateQuestionProps = {}) => (
-  <QuestionEditor
-    plugins={plugins}
-    onBeforeSave={onBeforeSave}
-    onSave={onSave}
-    entityTypeFilter={entityTypeFilter}
-    isSaveEnabled={isSaveEnabled}
-    saveToCollectionId={saveToCollectionId}
-  />
-);
+  isSaveEnabled = true,
+  ...props
+}: CreateQuestionProps = {}) => {
+  // The questionId is undefined at first.
+  // Once the question is saved, we set the questionId to the saved question's id.
+  const [questionId, setQuestionId] = useState<number | undefined>(undefined);
+
+  const [isSaveModalOpen, setSaveModalOpen] = useState(false);
+
+  return (
+    <InteractiveQuestion
+      {...props}
+      questionId={questionId}
+      isSaveEnabled={isSaveEnabled}
+      onSave={question => {
+        if (question) {
+          setQuestionId(question.id());
+          setSaveModalOpen(false);
+          onSave?.(question);
+        }
+      }}
+    >
+      <CreateQuestionDefaultView
+        isSaveModalOpen={isSaveModalOpen}
+        setSaveModalOpen={setSaveModalOpen}
+      />
+    </InteractiveQuestion>
+  );
+};
+
+export const CreateQuestionDefaultView = ({
+  isSaveModalOpen,
+  setSaveModalOpen,
+}: {
+  isSaveModalOpen: boolean;
+  setSaveModalOpen: (isOpen: boolean) => void;
+}) => {
+  const [isVisualizationView, setIsVisualizationView] = useState(false);
+
+  const { isSaveEnabled, question, originalQuestion, onSave, onCreate } =
+    useInteractiveQuestionContext();
+
+  return (
+    <FlexibleSizeComponent>
+      <Flex w="100%" justify="space-between">
+        <Flex>
+          <InteractiveQuestion.Title />
+        </Flex>
+
+        <Flex>
+          <Button onClick={() => setIsVisualizationView(!isVisualizationView)}>
+            Show {isVisualizationView ? "editor" : "visualization"}
+          </Button>
+
+          <Button onClick={() => setSaveModalOpen(true)}>Save</Button>
+        </Flex>
+      </Flex>
+
+      {isVisualizationView && (
+        <Flex h="500px">
+          <InteractiveQuestion.QuestionVisualization />
+        </Flex>
+      )}
+
+      {!isVisualizationView && (
+        <InteractiveQuestion.Editor
+          onApply={() => setIsVisualizationView(true)}
+        />
+      )}
+
+      {/* Refer to the SaveQuestionProvider for context on why we have to do it like this */}
+      {isSaveEnabled && isSaveModalOpen && question && (
+        <SaveQuestionModal
+          question={question}
+          originalQuestion={originalQuestion ?? null}
+          opened={true}
+          closeOnSuccess={true}
+          onClose={() => setSaveModalOpen(false)}
+          onCreate={onCreate}
+          onSave={onSave}
+        />
+      )}
+    </FlexibleSizeComponent>
+  );
+};
