0cc3b4d998d67e8955a5590857aa152f019537a6
Oisin Coveney <oisin@metabase.com>
2024-11-15 17:40:35 +0200

    Add Summarize component

--- frontend/src/metabase/query_builder/utils/get-aggregation-items.ts
+++ frontend/src/metabase/query_builder/utils/get-aggregation-items.ts
@@ -2,10 +2,19 @@ import * as Lib from "metabase-lib";
 
 import type { UpdateQueryHookProps } from "../hooks/types";
 
+export type AggregationItem = {
+  aggregation: Lib.AggregationClause;
+  aggregationIndex: number;
+  operators: Lib.AggregationOperator[];
+  displayName: string;
+};
+
+export type AggregationItems = AggregationItem[];
+
 export const getAggregationItems = ({
   query,
   stageIndex,
-}: Pick<UpdateQueryHookProps, "query" | "stageIndex">) => {
+}: Pick<UpdateQueryHookProps, "query" | "stageIndex">): AggregationItems => {
   const aggregations = Lib.aggregations(query, stageIndex);
 
   return aggregations.map((aggregation, aggregationIndex) => {
