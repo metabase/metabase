639e98b7af3e6b73284ec2837cf9ecf837185501
Anton Kulyk <kuliks.anton@gmail.com>
2024-11-15 14:57:15 +0000

    Support multiple pie dimensions

--- frontend/src/metabase/visualizer/visualizations/pie.ts
+++ frontend/src/metabase/visualizer/visualizations/pie.ts
@@ -3,7 +3,9 @@ import type { DragEndEvent } from "@dnd-kit/core";
 import { DROPPABLE_ID } from "metabase/visualizer/constants";
 import {
   addColumnMapping,
+  checkColumnMappingExists,
   cloneColumnProperties,
+  createDimensionColumn,
   createVisualizerColumnReference,
   extractReferencedColumns,
   isDraggedColumnItem,
@@ -42,17 +44,41 @@ export const pieDropHandler = (
   }
 
   if (over.id === DROPPABLE_ID.PIE_DIMENSION) {
-    const [dimensionColumnName] = state.settings["pie.dimension"] ?? [];
-    if (dimensionColumnName) {
-      const index = state.columns.findIndex(col => col.name === "DIMENSION_1");
+    const dimensions = state.settings["pie.dimension"] ?? [];
+
+    const isInUse = Object.values(state.columnValuesMapping).some(
+      valueSources => checkColumnMappingExists(valueSources, columnRef),
+    );
+    if (isInUse) {
+      return;
+    }
+
+    const index = state.columns.findIndex(col => col.name === dimensions[0]);
+    const dimension = state.columns[index];
+    const isDimensionMappedToValues =
+      state.columnValuesMapping[dimension.name]?.length > 0;
+
+    if (dimensions.length === 1 && dimension && !isDimensionMappedToValues) {
       state.columns[index] = cloneColumnProperties(
         state.columns[index],
         column,
       );
-      state.columnValuesMapping[dimensionColumnName] = addColumnMapping(
-        state.columnValuesMapping[dimensionColumnName],
+      state.columnValuesMapping[dimension.name] = addColumnMapping(
+        state.columnValuesMapping[dimension.name],
         columnRef,
       );
+    } else {
+      const nameIndex = dimensions.length + 1;
+      const newDimension = cloneColumnProperties(
+        createDimensionColumn({ name: `DIMENSION_${nameIndex}` }),
+        column,
+      );
+      state.columns.push(newDimension);
+      state.columnValuesMapping[newDimension.name] = [columnRef];
+      state.settings = {
+        ...state.settings,
+        "pie.dimension": [...dimensions, newDimension.name],
+      };
     }
   }
 };
