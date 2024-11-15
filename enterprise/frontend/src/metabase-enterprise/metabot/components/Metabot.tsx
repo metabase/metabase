ec4d40a067e34e82f092f18b850f92edc4eec606
Sloan Sparger <sloansparger@users.noreply.github.com>
2024-11-15 15:05:37 -0600

    [Metabot] UI Access improvements (#49859)
    * auto close the metabot input on route changes
    
    * prevents auto-closing metabot if the base path segment is the same
    
    * pr feedback
    
    * makes metabot only visible while the user is signed in
    
    * add test coverage
    
    * fix infinite type instantiation errors

--- enterprise/frontend/src/metabase-enterprise/metabot/components/Metabot.tsx
+++ enterprise/frontend/src/metabase-enterprise/metabot/components/Metabot.tsx
@@ -2,21 +2,32 @@ import { useCallback, useEffect } from "react";
 import { tinykeys } from "tinykeys";
 
 import ErrorBoundary from "metabase/ErrorBoundary";
+import { getCurrentUser } from "metabase/admin/datamodel/selectors";
+import { useSelector } from "metabase/lib/redux";
 
 import { useMetabotAgent } from "../hooks";
 
 import { MetabotChat } from "./MetabotChat";
 
 export const Metabot = () => {
+  const currentUser = useSelector(getCurrentUser);
+
   const { visible, setVisible } = useMetabotAgent();
 
   const onClose = useCallback(() => setVisible(false), [setVisible]);
 
   useEffect(() => {
+    if (!currentUser) {
+      return () => {};
+    }
+
     return tinykeys(window, {
-      "$mod+b": () => setVisible(!visible),
+      "$mod+b": e => {
+        e.preventDefault(); // prevent FF from opening bookmark menu
+        setVisible(!visible);
+      },
     });
-  }, [visible, setVisible]);
+  }, [visible, setVisible, currentUser]);
 
   if (!visible) {
     return null;
