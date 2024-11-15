52541c5184325ae11a28d31cf098b68e8d387937
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-11-15 14:06:40 -0500

    Disable viz settings

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/query.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/query.clj
@@ -1,7 +1,8 @@
 (ns metabase-enterprise.metabot-v3.tools.query
   (:require
-    [metabase.lib.core :as lib]
-    [metabase.lib.metadata.jvm :as lib.metadata.jvm]))
+   [metabase.lib.core :as lib]
+   [metabase.lib.metadata.jvm :as lib.metadata.jvm]
+   [metabase.lib.types.isa :as lib.types.isa]))
 
 (defn column-id
   "Column id."
@@ -10,10 +11,16 @@
       (:name column)))
 
 (defn column-info
-  "Column id and name."
+  "Column id, name, type."
   [query column]
   {:id (column-id column)
-   :name (-> (lib/display-info query column) :long-display-name)})
+   :name (-> (lib/display-info query column) :long-display-name)
+   :type (cond
+           (lib.types.isa/boolean? column) :boolean
+           (lib.types.isa/date-or-datetime? column) :date
+           (lib.types.isa/numeric? column) :number
+           (lib.types.isa/string-or-string-like? column) :string
+           :else :unknown)})
 
 (defn operator-name
   "Operator name."
