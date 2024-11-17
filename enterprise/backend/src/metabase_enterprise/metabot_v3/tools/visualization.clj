90e1ad0a83446441b642650d69e281705d4ba19e
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-11-16 23:22:46 -0500

    Cleanup context

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/visualization.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/visualization.clj
@@ -2,24 +2,14 @@
   (:require
    [medley.core :as m]))
 
-(defn column-id
-  "Visualization column id."
-  [column]
-  (:name column))
-
-(defn column-info
-  "Visualization column id and display name."
-  [column]
-  {:id (column-id column)
-   :name (:display_name column)})
-
 (defn- table-columns
   [dataset-columns viz-settings]
   (when-let [column-settings (:table.columns viz-settings)]
     (let [name->column (m/index-by :name dataset-columns)]
       (mapv (fn [{:keys [name enabled]}]
-              (merge (column-info (get name->column name))
-                     {:enabled enabled}))
+              {:id name
+               :name (-> (get name->column name) :display_name)
+               :enabled enabled})
             column-settings))))
 
 (defn visualization-context
