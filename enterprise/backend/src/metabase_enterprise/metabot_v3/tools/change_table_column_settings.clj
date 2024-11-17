1ff2f6bd29f8d1a287bb812d76b9a16266879966
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-11-16 23:33:45 -0500

    Cleanup context

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/change_table_column_settings.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/change_table_column_settings.clj
@@ -5,10 +5,15 @@
    [metabase.util.malli :as mu]))
 
 (mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/change-table-column-settings
-  [_tool-name {:keys [table-columns]} _context]
-  {:output "success"
-   :reactions [{:type :metabot.reaction/change-table-column-settings
-                :settings {:table.columns (mapv #(set/rename-keys % {:id :name}) table-columns)}}]})
+  [_tool-name {:keys [table-columns]} {:keys [visualization_settings]}]
+  (let [table-columns    (mapv #(set/rename-keys % {:id :name}) table-columns)
+        new-column-names (into #{} (map :name) table-columns)
+        old-column-names (into #{} (map :name) (:table.columns visualization_settings))]
+    (if (= old-column-names new-column-names)
+      {:output "success"
+       :reactions [{:type :metabot.reaction/change-table-column-settings
+                    :settings {:table.columns table-columns}}]}
+      {:output "Invalid `table_columns` change. You can only change `enabled` property of each column or reorder columns in the list. Do not add new columns and do not remove existing columns from this list."})))
 
 (mu/defmethod metabot-v3.tools.interface/*tool-applicable?* :metabot.tool/change-table-column-settings
   [_tool-name {:keys [dataset_columns visualization_settings]}]
