20dbcb59d2415ae5c6f25523f5c5b58f48111ba6
Oisin Coveney <oisin@metabase.com>
2024-11-18 11:43:35 +0200

    revert change for better diff

--- enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/components/Title.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/components/Title.tsx
@@ -1,6 +1,5 @@
+import { useInteractiveQuestionContext } from "embedding-sdk/components/private/InteractiveQuestion/context";
 import { QuestionTitle } from "embedding-sdk/components/private/QuestionTitle";
-
-import { useInteractiveQuestionContext } from "../context";
 
 export const Title = () => {
   const { question } = useInteractiveQuestionContext();
