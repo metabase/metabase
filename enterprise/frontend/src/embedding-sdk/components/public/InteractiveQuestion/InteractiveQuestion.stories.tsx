e85fc8d079cc9104ab7b361f4d892d59715c6335
Phoomparin Mano <phoomparin@gmail.com>
2024-11-18 23:14:16 +0700

    update storybook stories

--- enterprise/frontend/src/embedding-sdk/components/public/InteractiveQuestion/InteractiveQuestion.stories.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/InteractiveQuestion/InteractiveQuestion.stories.tsx
@@ -1,8 +1,7 @@
 import type { StoryFn } from "@storybook/react";
-import { type ComponentProps, useState } from "react";
+import type { ComponentProps } from "react";
 
 import { CommonSdkStoryWrapper } from "embedding-sdk/test/CommonSdkStoryWrapper";
-import { Stack } from "metabase/ui";
 
 import { InteractiveQuestion } from "./InteractiveQuestion";
 
@@ -32,40 +31,5 @@ export const Default = {
     questionId: QUESTION_ID,
     isSaveEnabled: true,
     saveToCollectionId: undefined,
-  },
-};
-
-export const WithCustomSaveForm = {
-  render(args: InteractiveQuestionComponentProps) {
-    const [isSaved, setIsSaved] = useState(false);
-    const [isBeforeSaveCalled, setBeforeSaveCalled] = useState(false);
-
-    return (
-      <InteractiveQuestion
-        onSave={() => {
-          setIsSaved(true);
-        }}
-        onBeforeSave={async () => {
-          setBeforeSaveCalled(true);
-        }}
-        {...args}
-      >
-        <InteractiveQuestion.QuestionVisualization />
-
-        <Stack h="200px">
-          <InteractiveQuestion.SaveQuestionForm />
-        </Stack>
-
-        <div>
-          {isBeforeSaveCalled && <div>Before save called!</div>}
-          {isSaved && <div>Saved!</div>}
-        </div>
-      </InteractiveQuestion>
-    );
-  },
-
-  args: {
-    questionId: QUESTION_ID,
-    isSaveEnabled: true,
   },
 };
