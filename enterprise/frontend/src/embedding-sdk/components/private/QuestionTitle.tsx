ab0e75a640491c1f4791d57721bd6318b588fe48
Oisin Coveney <oisin@metabase.com>
2024-11-18 11:16:39 +0200

    Move title logic to function and use that in SDK

--- enterprise/frontend/src/embedding-sdk/components/private/QuestionTitle.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/QuestionTitle.tsx
@@ -1,33 +1,51 @@
 import cx from "classnames";
-import type React from "react";
 import { t } from "ttag";
 
+import type { PropsWithHTMLStyle } from "embedding-sdk/types/default-style-props";
 import CS from "metabase/css/core/index.css";
-import { AdHocQuestionDescription } from "metabase/query_builder/components/view/ViewHeader/components/AdHocQuestionDescription";
+import {
+  getAdHocQuestionDescription,
+  shouldRenderAdhocDescription,
+} from "metabase/query_builder/components/view/ViewHeader/components/AdHocQuestionDescription/AdHocQuestionDescription";
 import * as Lib from "metabase-lib";
 import type Question from "metabase-lib/v1/Question";
 
-interface QuestionTitleProps {
+type QuestionTitleProps = {
   question: Question;
-}
+};
 
-export const QuestionTitle = ({
-  question,
-}: QuestionTitleProps): React.JSX.Element => {
+const getQuestionTitle = ({ question }: QuestionTitleProps): string => {
   const isSaved = question.isSaved();
+  const displayName = question.displayName();
+
+  if (isSaved && displayName) {
+    return displayName ?? null;
+  }
 
   const query = question.query();
   const { isNative } = Lib.queryDisplayInfo(query);
+  const adhocDescription = getAdHocQuestionDescription({ question });
+  if (
+    !isNative &&
+    shouldRenderAdhocDescription({ question }) &&
+    adhocDescription
+  ) {
+    return adhocDescription;
+  }
+
+  return t`New question`;
+};
+
+export const QuestionTitle = ({
+  question,
+  className,
+  style,
+}: PropsWithHTMLStyle<QuestionTitleProps>) => {
+  const questionTitle = getQuestionTitle({ question });
 
   return (
-    <h2 className={cx(CS.h2, CS.textWrap)}>
-      {isSaved ? (
-        question.displayName()
-      ) : !isNative && AdHocQuestionDescription.shouldRender(question) ? (
-        <AdHocQuestionDescription question={question} />
-      ) : (
-        t`New question`
-      )}
+    <h2 className={cx(CS.h2, CS.textWrap, className)} style={style}>
+      {questionTitle}
     </h2>
   );
 };
