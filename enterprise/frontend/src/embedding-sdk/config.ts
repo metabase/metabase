ceba6e407caba565e893cbc605be37bfa8ae3d04
Nicolò Pretto <info@npretto.com>
2024-11-19 09:08:15 +0100

    feat(sdk): detect mismatch between sdk version and mb version (#50032)
    * feat(sdk): detect mismatch between sdk version and mb version
    
    * refactor: log utils to print the big headers
    
    * Update enterprise/frontend/src/embedding-sdk/lib/version-utils.unit.spec.ts
    
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
    
    * refactor: call -> callArguments
    
    * add link to the docs
    
    ---------
    
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>

--- enterprise/frontend/src/embedding-sdk/config.ts
+++ enterprise/frontend/src/embedding-sdk/config.ts
@@ -4,5 +4,5 @@ export const EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID = "metabase-sdk-portal-root";
 export const EMBEDDING_SDK_FULL_PAGE_PORTAL_ROOT_ELEMENT_ID =
   "metabase-sdk-full-page-portal-root";
 
-export const getEmbeddingSdkVersion = () =>
-  process.env.EMBEDDING_SDK_VERSION ?? "unknown";
+export const getEmbeddingSdkVersion = (): string | "unknown" =>
+  (process.env.EMBEDDING_SDK_VERSION as string) ?? "unknown";
