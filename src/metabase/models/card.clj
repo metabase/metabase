5e0a0f74357c00897b2f236883ab8bbc2309e8df
Chris Truter <chris@metabase.com>
2024-11-19 09:28:31 +0200

    Index cards by their database

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -1017,6 +1017,7 @@
                   :dashboardcard-count {:select [:%count.*]
                                         :from   [:report_dashboardcard]
                                         :where  [:= :report_dashboardcard.card_id :this.id]}
+                  :database-id         :database_id
                   :last-viewed-at      :last_used_at
                   :native-query        [:case [:= "native" :query_type] :dataset_query]
                   :official-collection [:= "official" :collection.authority_level]
