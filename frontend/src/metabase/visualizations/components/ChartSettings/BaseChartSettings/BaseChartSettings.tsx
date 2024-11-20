c991f857db2de151648cdc34c727fb5720829894
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-11-20 11:39:28 +0300

    align tab items on the left

--- frontend/src/metabase/visualizations/components/ChartSettings/BaseChartSettings/BaseChartSettings.tsx
+++ frontend/src/metabase/visualizations/components/ChartSettings/BaseChartSettings/BaseChartSettings.tsx
@@ -2,8 +2,8 @@ import { type ReactNode, useCallback, useMemo, useState } from "react";
 import { t } from "ttag";
 import _ from "underscore";
 
-import Radio from "metabase/core/components/Radio";
 import CS from "metabase/css/core/index.css";
+import { Box, Tabs } from "metabase/ui";
 import { updateSeriesColor } from "metabase/visualizations/lib/series";
 import {
   getComputedSettings,
@@ -18,13 +18,11 @@ import ChartSettingsWidgetList from "../../ChartSettingsWidgetList";
 import ChartSettingsWidgetPopover from "../../ChartSettingsWidgetPopover";
 import type { Widget } from "../types";
 
-import {
-  ChartSettingsListContainer,
-  ChartSettingsMenu,
-  SectionContainer,
-} from "./BaseChartSettings.styled";
+import { ChartSettingsMenu } from "./BaseChartSettings.styled";
 import { useChartSettingsSections } from "./hooks";
 import type { BaseChartSettingsProps } from "./types";
+
+const maxTabsNumber = 4;
 
 export const BaseChartSettings = ({
   initial,
@@ -194,19 +192,26 @@ export const BaseChartSettings = ({
     <>
       <ChartSettingsMenu data-testid="chartsettings-sidebar">
         {showSectionPicker && (
-          <SectionContainer>
-            <Radio
-              value={chartSettingCurrentSection ?? undefined}
-              onChange={handleShowSection}
-              options={sectionNames}
-              optionNameFn={v => v}
-              optionValueFn={v => v}
-              optionKeyFn={v => v}
-              variant="underlined"
-            />
-          </SectionContainer>
+          <Tabs
+            radius={0}
+            defaultValue={chartSettingCurrentSection}
+            onTabChange={handleShowSection}
+            styles={{
+              tab: {
+                flexBasis: `${100 / maxTabsNumber}%`,
+              },
+            }}
+          >
+            <Tabs.List>
+              {sectionNames.map(sectionName => (
+                <Tabs.Tab key={sectionName} value={sectionName}>
+                  {sectionName}
+                </Tabs.Tab>
+              ))}
+            </Tabs.List>
+          </Tabs>
         )}
-        <ChartSettingsListContainer className={CS.scrollShow}>
+        <Box pos="relative" py="lg" className={CS.scrollShow}>
           {isChartPicker ? (
             chartTypeSettings
           ) : (
@@ -215,7 +220,7 @@ export const BaseChartSettings = ({
               extraWidgetProps={extraWidgetProps}
             />
           )}
-        </ChartSettingsListContainer>
+        </Box>
       </ChartSettingsMenu>
       <ChartSettingsWidgetPopover
         anchor={popoverRef as HTMLElement}
