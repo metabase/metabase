86d3ed3458b1a64a7eaec9ce96c1be84f3451174
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-11-20 16:03:16 +0700

    Refactor: extract a setup function as a shared util

--- frontend/src/metabase/public/containers/PublicOrEmbeddedQuestion/PublicOrEmbeddedQuestion/PublicOrEmbeddedQuestion.unit.spec.tsx
+++ frontend/src/metabase/public/containers/PublicOrEmbeddedQuestion/PublicOrEmbeddedQuestion/PublicOrEmbeddedQuestion.unit.spec.tsx
@@ -1,114 +1,38 @@
 import userEvent from "@testing-library/user-event";
-import { Route } from "react-router";
 
-import { setupEnterprisePlugins } from "__support__/enterprise";
-import {
-  setupPublicCardQueryEndpoints,
-  setupPublicQuestionEndpoints,
-} from "__support__/server-mocks";
-import { mockSettings } from "__support__/settings";
 import {
   getIcon,
   queryIcon,
-  renderWithProviders,
   screen,
   waitForLoaderToBeRemoved,
   within,
 } from "__support__/ui";
-import registerVisualizations from "metabase/visualizations/register";
-import type { VisualizationProps } from "metabase/visualizations/types";
-import {
-  createMockEmbedDataset,
-  createMockPublicCard,
-  createMockTokenFeatures,
-} from "metabase-types/api/mocks";
-import { createMockState } from "metabase-types/store/mocks";
+import { createMockTokenFeatures } from "metabase-types/api/mocks";
 
-import { PublicOrEmbeddedQuestion } from "./PublicOrEmbeddedQuestion";
-
-registerVisualizations();
+import { type SetupOpts, setup } from "./SetupOpts";
 
 const FAKE_UUID = "123456";
 
 const QUESTION_NAME = "Public question";
 
-const VisualizationMock = ({
-  onUpdateVisualizationSettings,
-  rawSeries,
-}: VisualizationProps) => {
-  const [
-    {
-      card,
-      data: { rows },
-    },
-  ] = rawSeries;
-
-  return (
-    <div>
-      <div>
-        {rows[0].map((value, i) => (
-          <span key={i}>{value}</span>
-        ))}
-      </div>
-      <div data-testid="settings">
-        {JSON.stringify(card.visualization_settings)}
-      </div>
-      <button onClick={() => onUpdateVisualizationSettings({ foo: "bar" })}>
-        update settings
-      </button>
-    </div>
-  );
-};
-
-jest.mock(
-  "metabase/visualizations/components/Visualization",
-  () => VisualizationMock,
-);
-
-async function setup({
-  hash,
-}: {
-  hash?: string;
-} = {}) {
-  setupPublicQuestionEndpoints(
-    FAKE_UUID,
-    createMockPublicCard({ name: QUESTION_NAME }),
-  );
-  setupPublicCardQueryEndpoints(
-    FAKE_UUID,
-    createMockEmbedDataset({
-      data: { rows: [["John W."]] },
-    }),
-  );
-
-  renderWithProviders(
-    <Route path="public/question/:uuid" component={PublicOrEmbeddedQuestion} />,
-    {
-      storeInitialState: createMockState(),
-      withRouter: true,
-      initialRoute: `public/question/${FAKE_UUID}${hash ? "#" + hash : ""}`,
-    },
-  );
-  expect(await screen.findByText(QUESTION_NAME)).toBeInTheDocument();
+function setupPremium(opts?: Partial<SetupOpts>) {
+  return setup({
+    ...opts,
+    hasEnterprisePlugins: true,
+    tokenFeatures: createMockTokenFeatures({ whitelabel: true }),
+    questionName: QUESTION_NAME,
+    uuid: FAKE_UUID,
+  });
 }
 
 describe("PublicOrEmbeddedQuestion", () => {
-  beforeAll(() => {
-    mockSettings({
-      // the `whitelabel` feature is needed to test #downloads=false
-      "token-features": createMockTokenFeatures({ whitelabel: true }),
-    });
-
-    setupEnterprisePlugins();
-  });
-
   it("should render data", async () => {
-    await setup();
+    await setupPremium();
     expect(await screen.findByText("John W.")).toBeInTheDocument();
   });
 
   it("should update card settings when visualization component changes them (metabase#37429)", async () => {
-    await setup();
+    await setupPremium();
 
     await waitForLoaderToBeRemoved();
 
@@ -127,7 +51,7 @@ describe("PublicOrEmbeddedQuestion", () => {
 
   describe("downloads flag", () => {
     it("should allow downloading the results when downloads are enabled", async () => {
-      await setup({ hash: "downloads=true" });
+      await setupPremium({ hash: { downloads: "true" } });
       await waitForLoaderToBeRemoved();
 
       await userEvent.click(getIcon("download"));
@@ -140,7 +64,7 @@ describe("PublicOrEmbeddedQuestion", () => {
     });
 
     it("should not allow downloading results when downloads are enabled", async () => {
-      await setup({ hash: "downloads=false" });
+      await setupPremium({ hash: { downloads: "false" } });
 
       await waitForLoaderToBeRemoved();
 
