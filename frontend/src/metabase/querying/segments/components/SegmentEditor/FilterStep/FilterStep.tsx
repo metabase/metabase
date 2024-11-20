abe661f0e42bbe9a6cd4d601495c81c64a784366
Alexander Polyankin <alexander.polyankin@metabase.com>
2024-11-20 16:27:06 -0500

    Add data step

--- frontend/src/metabase/querying/segments/components/SegmentEditor/FilterStep/FilterStep.tsx
+++ frontend/src/metabase/querying/segments/components/SegmentEditor/FilterStep/FilterStep.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import { t } from "ttag";
 
 import { FilterPill } from "metabase/querying/filters/components/FilterPanel/FilterPill";
 import { FilterPicker } from "metabase/querying/filters/components/FilterPicker";
 import { ClauseStep } from "metabase/querying/segments/components/SegmentEditor/ClauseStep";
-import { Flex, Popover } from "metabase/ui";
+import { Button, Flex, Popover } from "metabase/ui";
 import * as Lib from "metabase-lib";
 
 type FilterStepProps = {
@@ -17,49 +18,99 @@ export function FilterStep({ query, stageIndex, onChange }: FilterStepProps) {
 
   return (
     <ClauseStep label={t`Filters`}>
-      <Flex>
-        {filters.map((filter, filterIndex) => (
-          <FilterPopover
-            key={filterIndex}
+      {query && (
+        <Flex>
+          {filters.map((filter, filterIndex) => (
+            <ChangeFilterPopover
+              key={filterIndex}
+              query={query}
+              stageIndex={stageIndex}
+              filter={filter}
+              onChange={onChange}
+            />
+          ))}
+          <AddFilterPopover
             query={query}
             stageIndex={stageIndex}
-            filter={filter}
             onChange={onChange}
           />
-        ))}
-      </Flex>
+        </Flex>
+      )}
     </ClauseStep>
   );
 }
 
-type FilterPopoverProps = {
-  query: Lib.Query | undefined;
+type AddFilterPopoverProps = {
+  query: Lib.Query;
+  stageIndex: number;
+  onChange: (query: Lib.Query) => void;
+};
+
+function AddFilterPopover({
+  query,
+  stageIndex,
+  onChange,
+}: AddFilterPopoverProps) {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleSelect = (newFilter: Lib.Filterable) => {
+    const newQuery = Lib.filter(query, stageIndex, newFilter);
+    onChange(newQuery);
+    setIsOpened(false);
+  };
+
+  return (
+    <Popover opened={isOpened} onChange={setIsOpened}>
+      <Popover.Target>
+        <Button onClick={() => setIsOpened(!isOpened)}>{t`Add filter`}</Button>
+      </Popover.Target>
+      <Popover.Dropdown>
+        <FilterPicker
+          query={query}
+          stageIndex={stageIndex}
+          onSelect={handleSelect}
+        />
+      </Popover.Dropdown>
+    </Popover>
+  );
+}
+
+type ChangeFilterPopoverProps = {
+  query: Lib.Query;
   stageIndex: number;
   filter: Lib.FilterClause;
   onChange: (query: Lib.Query) => void;
 };
 
-function FilterPopover({
+function ChangeFilterPopover({
   query,
   stageIndex,
   filter,
   onChange,
-}: FilterPopoverProps) {
-  if (!query) {
-    return null;
-  }
-
+}: ChangeFilterPopoverProps) {
+  const [isOpened, setIsOpened] = useState(false);
   const filterInfo = Lib.displayInfo(query, stageIndex, filter);
 
   const handleSelect = (newFilter: Lib.Filterable) => {
-    const newQuery = Lib.filter(query, stageIndex, newFilter);
+    const newQuery = Lib.replaceClause(query, stageIndex, filter, newFilter);
+    onChange(newQuery);
+    setIsOpened(false);
+  };
+
+  const handleRemove = () => {
+    const newQuery = Lib.removeClause(query, stageIndex, filter);
     onChange(newQuery);
   };
 
   return (
-    <Popover>
+    <Popover opened={isOpened}>
       <Popover.Target>
-        <FilterPill>{filterInfo.displayName}</FilterPill>
+        <FilterPill
+          onClick={() => setIsOpened(!isOpened)}
+          onRemoveClick={handleRemove}
+        >
+          {filterInfo.displayName}
+        </FilterPill>
       </Popover.Target>
       <Popover.Dropdown>
         <FilterPicker
