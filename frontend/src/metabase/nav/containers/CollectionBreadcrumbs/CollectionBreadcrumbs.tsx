f566ad9b27e6433e475d78b9ec2a235800fd44b2
Nick Fitzpatrick <nickfitz.582@gmail.com>
2024-11-20 16:03:36 -0400

    unit tests for new breadcumb functionality

--- frontend/src/metabase/nav/containers/CollectionBreadcrumbs/CollectionBreadcrumbs.tsx
+++ frontend/src/metabase/nav/containers/CollectionBreadcrumbs/CollectionBreadcrumbs.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-use";
+import { type WithRouterProps, withRouter } from "react-router";
 import _ from "underscore";
 
 import {
@@ -17,54 +17,56 @@ import {
   type CollectionBreadcrumbsProps as BreadcrumbsProps,
 } from "../../components/CollectionBreadcrumbs/CollectionBreadcrumbs";
 
-interface CollectionBreadcrumbsProps
-  extends Omit<
-    BreadcrumbsProps,
-    "collection" | "dashboard" | "baseCollectionId"
-  > {
-  collectionId?: CollectionId;
-  baseCollectionId?: CollectionId | null;
-}
+type CollectionBreadcrumbsProps = WithRouterProps &
+  Omit<BreadcrumbsProps, "collection" | "dashboard" | "baseCollectionId"> & {
+    collectionId?: CollectionId;
+    baseCollectionId?: CollectionId | null;
+  };
 
 // TODO: not sure how these changes will work for the embedding folks
-export const CollectionBreadcrumbs = (props: CollectionBreadcrumbsProps) => {
-  const viewedItem = useSelector(getViewedItem);
+export const CollectionBreadcrumbs = withRouter(
+  (props: CollectionBreadcrumbsProps) => {
+    const viewedItem = useSelector(getViewedItem);
 
-  const { currentData: viewedQuestion } = useGetCardQuery(
-    viewedItem.model === "question" && viewedItem.question?.id
-      ? { id: viewedItem.question.id() }
-      : skipToken,
-  );
+    const { currentData: viewedQuestion } = useGetCardQuery(
+      viewedItem.model === "question" && viewedItem.question?.id
+        ? { id: viewedItem.question.id() }
+        : skipToken,
+    );
 
-  const { currentData: viewedDashboard } = useGetDashboardQuery(
-    viewedItem.model === "dashboard" && viewedItem.dashboard?.id
-      ? { id: viewedItem.dashboard.id }
-      : skipToken,
-  );
+    const { currentData: viewedDashboard } = useGetDashboardQuery(
+      viewedItem.model === "dashboard" && viewedItem.dashboard?.id
+        ? { id: viewedItem.dashboard.id }
+        : skipToken,
+    );
 
-  const viewedItemParentCollectionId =
-    viewedDashboard?.collection_id ?? viewedQuestion?.collection_id;
-  const collectionId =
-    props.collectionId ?? viewedItemParentCollectionId ?? ROOT_COLLECTION.id;
-  const { data: collection } = useGetCollectionQuery({ id: collectionId });
+    const viewedItemParentCollectionId =
+      viewedDashboard?.collection_id ?? viewedQuestion?.collection_id;
+    const collectionId =
+      props.collectionId ?? viewedItemParentCollectionId ?? ROOT_COLLECTION.id;
+    const { data: collection } = useGetCollectionQuery({ id: collectionId });
 
-  const dashboardId = viewedQuestion?.dashboard_id;
-  const isDashboardQuestion = _.isNumber(dashboardId);
-  const isQuestionPage = useLocation().pathname?.startsWith("/question");
-  const shouldShowDashboard = isDashboardQuestion && isQuestionPage;
+    const dashboardId = viewedQuestion?.dashboard_id;
+    const isDashboardQuestion = _.isNumber(dashboardId);
+    const isQuestionPage = props.location.pathname.startsWith("/question");
 
-  const { currentData: viewedDashboardQuestionDashboard } =
-    useGetDashboardQuery(shouldShowDashboard ? { id: dashboardId } : skipToken);
+    const shouldShowDashboard = isDashboardQuestion && isQuestionPage;
 
-  return (
-    <Breadcrumbs
-      {...props}
-      collection={collection}
-      dashboard={viewedDashboardQuestionDashboard}
-      baseCollectionId={props.baseCollectionId ?? null}
-    />
-  );
-};
+    const { currentData: viewedDashboardQuestionDashboard } =
+      useGetDashboardQuery(
+        shouldShowDashboard ? { id: dashboardId } : skipToken,
+      );
+
+    return (
+      <Breadcrumbs
+        {...props}
+        collection={collection}
+        dashboard={viewedDashboardQuestionDashboard}
+        baseCollectionId={props.baseCollectionId ?? null}
+      />
+    );
+  },
+);
 
 // eslint-disable-next-line import/no-default-export -- deprecated usage
 export default CollectionBreadcrumbs;
