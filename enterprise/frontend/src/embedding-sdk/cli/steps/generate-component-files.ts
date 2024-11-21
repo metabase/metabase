7d287538be97787d52c8327c212ce7deb018a11b
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-11-21 04:04:27 -0500

    fix(sdk): emit typescript files in the embedding cli when in a typescript project (#50278) (#50316)
    * emit typescript when in a typescript project
    
    * also check dependencies for typescript dep
    
    Co-authored-by: Phoomparin Mano <poom@metabase.com>

--- enterprise/frontend/src/embedding-sdk/cli/steps/generate-component-files.ts
+++ enterprise/frontend/src/embedding-sdk/cli/steps/generate-component-files.ts
@@ -6,6 +6,7 @@ import { GENERATED_COMPONENTS_DEFAULT_PATH } from "../constants/config";
 import { getGeneratedComponentFilesMessage } from "../constants/messages";
 import { ANALYTICS_CSS_SNIPPET } from "../snippets/analytics-css-snippet";
 import type { CliStepMethod } from "../types/cli";
+import { checkIsInTypeScriptProject } from "../utils/check-typescript-project";
 import { getComponentSnippets } from "../utils/get-component-snippets";
 import { printError, printSuccess } from "../utils/print";
 
@@ -49,9 +50,13 @@ export const generateReactComponentFiles: CliStepMethod = async state => {
     userSwitcherEnabled: !!token,
   });
 
+  const isInTypeScriptProject = await checkIsInTypeScriptProject();
+  const fileExtension = isInTypeScriptProject ? "ts" : "js";
+  const componentExtension = isInTypeScriptProject ? "tsx" : "jsx";
+
   // Generate sample components files in the specified directory.
   for (const { name, content } of sampleComponents) {
-    await fs.writeFile(`${path}/${name}.jsx`, content);
+    await fs.writeFile(`${path}/${name}.${componentExtension}`, content);
   }
 
   // Generate analytics.css sample styles.
@@ -63,7 +68,7 @@ export const generateReactComponentFiles: CliStepMethod = async state => {
     .join("\n")
     .trim();
 
-  await fs.writeFile(`${path}/index.js`, exportIndexContent);
+  await fs.writeFile(`${path}/index.${fileExtension}`, exportIndexContent);
 
   printSuccess(getGeneratedComponentFilesMessage(path));
 
