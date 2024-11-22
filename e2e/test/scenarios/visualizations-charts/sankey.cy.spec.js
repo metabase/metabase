a9841dad20f51a01b2eb13f20750033d860af484
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2024-11-22 12:52:09 -0500

    sankey chart (#46245)
    * sankey prototype
    
    * formatting, tooltips
    
    * layout, formatting
    
    * handle invalid settings
    
    * use metabase color palette
    
    * add missing data for click behavior, hovers, add clicks support
    
    * smart sankey default columns selection
    
    * check more columns
    
    * fix tooltip shows incorrect values for nodes
    
    * polish, specs
    
    * extend the viz settings type with sankey settings
    
    * add isSensible to SankeyChart
    
    * specs on sankey default columns computation
    
    * import correct column predicates
    
    * revert outdated changes in graph.js
    
    * sankey colors
    
    * format values in sankey tooltip
    
    * change default sankey edge color to source
    
    * increase the default size of sankey dashboard cards
    
    * improve error messages
    
    * use echarts option to hide overlapping labels
    
    * increase padding
    
    * click behavior, tooltip values
    
    * fixes
    
    * fix rebase: getFriendlyName got removed
    
    * layout, labels
    
    * polish
    
    * remove non-existing viz type
    
    * update spec
    
    * polish

---
+++ e2e/test/scenarios/visualizations-charts/sankey.cy.spec.js
@@ -0,0 +1,125 @@
+import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
+import {
+  assertEChartsTooltip,
+  chartPathWithFillColor,
+  echartsContainer,
+  modal,
+  restore,
+  sankeyEdge,
+  visitQuestionAdhoc,
+} from "e2e/support/helpers";
+
+const SANKEY_QUERY = `
+SELECT 'Social Media' AS source, 'Landing Page' AS target, 30000 AS metric
+UNION ALL
+SELECT 'Email Campaign', 'Landing Page', 20000
+UNION ALL
+SELECT 'Paid Search', 'Landing Page', 25000
+UNION ALL
+SELECT 'Landing Page', 'Signup Form', 60000
+UNION ALL
+SELECT 'Signup Form', 'Free Trial', 40000
+UNION ALL
+SELECT 'Signup Form', 'Abandoned Signup', 20000
+UNION ALL
+SELECT 'Free Trial', 'Onboarding', 30000
+UNION ALL
+SELECT 'Free Trial', 'Churned During Trial', 10000
+UNION ALL
+SELECT 'Onboarding', 'Active Users', 25000
+UNION ALL
+SELECT 'Onboarding', 'Churned After Onboarding', 5000
+UNION ALL
+SELECT 'Active Users', 'Paid Subscription', 20000
+UNION ALL
+SELECT 'Active Users', 'Cancelled Subscription', 5000;
+`;
+
+describe("scenarios > visualizations > sankey", () => {
+  beforeEach(() => {
+    restore();
+    cy.signInAsNormalUser();
+  });
+
+  it("should render sankey charts in query builder", () => {
+    visitQuestionAdhoc({
+      display: "table",
+      dataset_query: {
+        type: "native",
+        native: {
+          query: SANKEY_QUERY,
+        },
+        database: SAMPLE_DB_ID,
+      },
+    });
+
+    // Select Sankey viz type
+    cy.findByTestId("viz-type-button").click();
+    cy.findByTestId("Sankey-button").click();
+
+    // Ensure it shows node labels
+    echartsContainer().findByText("Social Media");
+
+    // Edit viz settings
+    cy.findByTestId("viz-settings-button").click();
+    cy.findByTestId("chartsettings-sidebar")
+      .as("settings-sidebar")
+      .findByText("Display")
+      .click();
+
+    // Shows colored edges by default
+    sankeyEdge("#509EE3");
+
+    // Set edge colors to Gray
+    cy.get("@settings-sidebar").findByText("Gray").click();
+
+    // Ensure it shows gray edges
+    sankeyEdge("#81898e");
+
+    // Ensure it does not show edge labels by default
+    echartsContainer().findByText("60,000").should("not.exist");
+
+    // Enable edge labels
+    cy.get("@settings-sidebar").findByLabelText("Show edge labels").click();
+
+    // Ensure it shows edge labels
+    echartsContainer().findByText("60,000");
+
+    // Apply compact formatting
+    cy.get("@settings-sidebar").findByText("Compact").click();
+
+    // Ensure it shows compact labels
+    echartsContainer().findByText("60.0k");
+
+    // Ensure tooltip shows correct values
+    sankeyEdge("#81898e").eq(0).realHover();
+    assertEChartsTooltip({
+      header: "Social Media → Landing Page",
+      rows: [
+        {
+          name: "METRIC",
+          value: "30,000",
+        },
+      ],
+    });
+
+    chartPathWithFillColor("#509EE3").realHover();
+    assertEChartsTooltip({
+      header: "Social Media",
+      rows: [
+        {
+          name: "METRIC",
+          value: "0",
+        },
+      ],
+    });
+
+    // Ensure saving the question works
+    cy.findByTestId("qb-save-button").click();
+    cy.findByPlaceholderText("What is the name of your question?").type(
+      "My Sankey chart",
+    );
+    cy.findByTestId("save-question-modal").findByText("Save").click();
+    modal().findByText("Saved! Add this to a dashboard?");
+  });
+});
