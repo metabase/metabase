ad2b6cd6921eba6fd1beb17a64a649b160715c9c
Denis Berezin <denis.berezin@metabase.com>
2024-11-22 16:40:05 +0300

    feat(sdk): Run e2e tests using published SDK package related to target release (#49196)
    * Move FlexibleSizeComponent to public context
    
    * Refactor SDK e2e tests to component testing approach
    
    * Fix CI
    
    * Fix CI
    
    * Migrate new test
    
    * Review fixes
    
    * Review fixes, fix failing e2e test
    
    * migrate new test, fix CI
    
    * Enable skipped tests
    
    * Fix CI script
    
    * Fix CI

---
+++ e2e/support/component-webpack.config.js
@@ -0,0 +1,104 @@
+const fs = require("fs");
+
+const path = require("path");
+const webpack = require("webpack");
+
+const mainConfig = require("../../webpack.config");
+
+const { isEmbeddingSdkPackageInstalled } = resolveEmbeddingSdkPackage();
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".css", ".svg"],
+    alias: {
+      ...mainConfig.resolve.alias,
+      ...(!isEmbeddingSdkPackageInstalled
+        ? {
+            "@metabase/embedding-sdk-react": path.resolve(
+              __dirname,
+              "../../resources/embedding-sdk/dist/main.bundle.js",
+            ),
+          }
+        : null),
+    },
+    fallback: { path: false, fs: false }, // FIXME: this might break file download tests, we might need to implement this properly
+  },
+  entry: [path.join(__dirname, "src", "index.js")],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg/,
+        type: "asset/source",
+        resourceQuery: { not: [/component/] }, // *.svg?source
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: /component/, // *.svg?component
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              ref: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(tsx?|jsx?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+            targets: "last 5 Chrome versions",
+            configFile: false,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
+};
+
+function resolveEmbeddingSdkPackage() {
+  let isEmbeddingSdkPackageInstalled = false;
+  let embeddingSdkVersion;
+
+  try {
+    embeddingSdkVersion =
+      require("@metabase/embedding-sdk-react/package.json").version;
+    isEmbeddingSdkPackageInstalled = true;
+  } catch (err) {
+    const sdkPackageTemplateJson = fs.readFileSync(
+      path.resolve(
+        "../../enterprise/frontend/src/embedding-sdk/package.template.json",
+      ),
+      "utf-8",
+    );
+    const sdkPackageTemplateJsonContent = JSON.parse(sdkPackageTemplateJson);
+    embeddingSdkVersion = JSON.stringify(sdkPackageTemplateJsonContent.version);
+  }
+
+  return {
+    isEmbeddingSdkPackageInstalled,
+    embeddingSdkVersion,
+  };
+}
