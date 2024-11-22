849cc9e5325fb9dfa851c4a80a6550f4961f92b5
Phoomparin Mano <phoomparin@gmail.com>
2024-11-23 00:21:11 +0700

    check for loading indicator

--- e2e/test/scenarios/embedding-sdk/create-question.cy.spec.ts
+++ e2e/test/scenarios/embedding-sdk/create-question.cy.spec.ts
@@ -20,8 +20,16 @@ describeSDK("scenarios > embedding-sdk > create-question", () => {
     cy.signOut();
   });
 
-  it("can create questions via the CreateQuestion component", () => {
+  it("can create a question via the CreateQuestion component", () => {
     cy.intercept("POST", "/api/card").as("createCard");
+
+    cy.intercept("POST", "/api/dataset", req => {
+      // throttling makes the loading indicator shows up
+      // reliably when we click on visualize.
+      req.on("response", res => {
+        res.setThrottle(1000);
+      });
+    });
 
     visitFullAppEmbeddingUrl({
       url: EMBEDDING_SDK_STORY_HOST,
@@ -46,11 +54,17 @@ describeSDK("scenarios > embedding-sdk > create-question", () => {
 
       cy.findByRole("button", { name: "Visualize" }).click();
 
+      // Should show a loading indicator (metabase#47564)
+      cy.findByTestId("loading-indicator").should("exist");
+
       // Should be able to go back to the editor view
       cy.findByRole("button", { name: "Show editor" }).click();
 
       // Should be able to visualize the question again
       cy.findByRole("button", { name: "Visualize" }).click();
+
+      // Should not show a loading indicator again as the question has not changed (metabase#47564)
+      cy.findByTestId("loading-indicator").should("not.exist");
 
       // Should be able to save to a new question right away
       cy.findByRole("button", { name: "Save" }).click();
