e39591fbcb39284d2728238b2f514d8d977b6ff1
Ngoc Khuat <qn.khuat@gmail.com>
2024-11-25 16:42:13 +0700

    add a test for accounts scenario

--- test/metabase/search/postgres/index_test.clj
+++ test/metabase/search/postgres/index_test.clj
@@ -220,7 +220,7 @@
                           [:= :name entity-name]
                           [:= :model model]]}))
 
-(def default-index-entity
+(def blank-index-entity
   {:model               nil
    :model_id            nil
    :name                nil
@@ -239,7 +239,7 @@
 
 (defn- index-entity
   [entity]
-  (merge default-index-entity entity))
+  (merge blank-index-entity entity))
 
 (deftest card-complex-ingestion-test
   (search.tu/with-temp-index-table
---
+++ test/metabase/search/scenarios_test.clj
@@ -0,0 +1,54 @@
+(ns metabase.search.scenarios-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [metabase.config :as config]
+   [metabase.search.postgres.ingestion :as search.ingestion]
+   [metabase.search.spec :as search.spec]
+   [metabase.search.test-util :as search.tu]
+   [metabase.test :as mt]))
+
+(defn ingest!
+  "Ingest any objects with name contains `search-term`"
+  [search-term]
+  (doseq [model (keys (methods search.spec/spec))]
+    (#'search.ingestion/batch-update!
+     (#'search.ingestion/spec-index-reducible model [:like :this.name (str "%" search-term "%")]))))
+
+(defn fulltext-search
+  [search-string]
+  (map (juxt :model :id :name #_:total_score #_:scores) (search.tu/search-results search-string {:search-engine "fulltext"})))
+
+(deftest e2e-accounts-test
+  ;; https://metaboat.slack.com/archives/C072YM78NGK/p1731616354121189
+  (when config/ee-available?
+    (mt/with-premium-features #{:official-collections}
+      (search.tu/with-temp-index-table
+        (let [search-term* (mt/random-name)
+              search-term  #(str search-term* " " %)]
+          (mt/with-temp
+            [:model/Database   {db-id :id}             {:name "Test Database"}
+             :model/Table      {table-id :id}          {:name       (search-term "table")
+                                                        :view_count 40
+                                                        :db_id      db-id}
+             :model/Card       {card-id :id}           {:name       (search-term "card")
+                                                        :view_count 41}
+             :model/Card       {model-id :id}          {:name       (search-term "model")
+                                                        :type       :model
+                                                        :view_count 42}
+             :model/Collection {official-col-id :id}   {:name            "Official Collection"
+                                                        :authority_level "official"}
+             :model/Card       {official-card-id :id}  {:name          (search-term "Card Official")
+                                                        :collection_id official-col-id
+                                                        :view_count    41}
+             :model/Card       {official-model-id :id} {:name          (search-term "Model Official")
+                                                        :type          :model
+                                                        :collection_id official-col-id
+                                                        :view_count    42}]
+            (ingest! search-term*)
+            (testing "official items are preferred, models > card even though card has slightly higher view count"
+              (is (= [["dataset"  official-model-id (search-term "Model Official")]
+                      ["card"     official-card-id  (search-term "Card Official")]
+                      ["dataset"  model-id          (search-term "model")]
+                      ["card"     card-id           (search-term "card")]
+                      ["table"    table-id          (search-term "table")]]
+                     (fulltext-search search-term*))))))))))
