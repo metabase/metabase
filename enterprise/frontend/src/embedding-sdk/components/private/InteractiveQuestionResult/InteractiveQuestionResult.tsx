315fa734d710c5248a8dad2822d9317551cc88cd
Phoomparin Mano <poom@metabase.com>
2024-11-26 02:37:57 +0700

    fix(sdk): show loader right after visualizing in notebook editor for the first time (#50411)
    * show sdk loading state when visualizing for the first time
    
    * rename to isQueryResultLoading
    
    * check for loading indicator
    
    * add notes on slow 3G

--- enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestionResult/InteractiveQuestionResult.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestionResult/InteractiveQuestionResult.tsx
@@ -78,11 +78,14 @@ export const InteractiveQuestionResult = ({
   const [isSaveModalOpen, { open: openSaveModal, close: closeSaveModal }] =
     useDisclosure(false);
 
-  if (isQuestionLoading) {
+  // When visualizing a question for the first time, there is no query result yet.
+  const isQueryResultLoading = question && !queryResults;
+
+  if (isQuestionLoading || isQueryResultLoading) {
     return <SdkLoader />;
   }
 
-  if (!question || !queryResults) {
+  if (!question) {
     return <SdkError message={t`Question not found`} />;
   }
 
