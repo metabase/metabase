a23b9908bcfc179e28daac42e2c5add4ab2d92fd
Adam James <adam.vermeer2@gmail.com>
2024-11-26 06:30:11 -0800

    fix tests

--- test/metabase/api/downloads_exports_test.clj
+++ test/metabase/api/downloads_exports_test.clj
@@ -274,8 +274,7 @@
                                                :type     :query
                                                :query
                                                {:source-table (mt/id :products)
-                                                :aggregation  [[:sum [:field (mt/id :products :price) {:base-type :type/Float}]]
-                                                               [:avg [:field (mt/id :products :price) {:base-type :type/Float}]]]
+                                                :aggregation  [[:sum [:field (mt/id :products :price) {:base-type :type/Float}]]]
                                                 :breakout     [[:field (mt/id :products :category) {:base-type :type/Text}]
                                                                [:field (mt/id :products :created_at) {:base-type :type/DateTime :temporal-unit :year}]]}}}]
         (testing "formatted"
@@ -333,33 +332,33 @@
                                                 :breakout     [[:field (mt/id :products :category) {:base-type :type/Text}]
                                                                [:field (mt/id :products :created_at) {:base-type :type/DateTime :temporal-unit :year}]]}}}]
         (testing "formatted"
-          (is (= [["Category" "2016" "2016" "2017" "2017" "2018" "2018" "2019" "2019" "Row totals" "Row totals"]
-                  ["Category"
-                   "Sum of Price"
-                   "Average of Price"
-                   "Sum of Price"
-                   "Average of Price"
-                   "Sum of Price"
-                   "Average of Price"
-                   "Sum of Price"
-                   "Average of Price"
-                   ""
-                   ""]
-                  ["Doohickey" "$632.14" "48.63" "$854.19" "50.25" "$496.43" "62.05" "$203.13" "50.78" "$2,185.89" "52.93"]
-                  ["Gadget" "$679.83" "52.29" "$1,059.11" "55.74" "$844.51" "60.32" "$435.75" "62.25" "$3,019.20" "57.65"]
-                  ["Gizmo" "$529.70" "58.86" "$1,080.18" "51.44" "$997.94" "58.7" "$227.06" "56.77" "$2,834.88" "56.44"]
-                  ["Widget" "$987.39" "51.97" "$1,014.68" "56.37" "$912.20" "65.16" "$195.04" "65.01" "$3,109.31" "59.63"]
-                  ["Grand totals"
-                   "$2,829.06"
-                   "52.94"
-                   "$4,008.16"
-                   "53.45"
-                   "$3,251.08"
-                   "61.56"
-                   "$1,060.98"
-                   "58.7"
-                   "$11,149.28"
-                   "56.66"]
+          (is (= [[["Category" "2016" "2016" "2017" "2017" "2018" "2018" "2019" "2019" "Row totals" "Row totals"]
+                   ["Category"
+                    "Sum of Price"
+                    "Average of Price"
+                    "Sum of Price"
+                    "Average of Price"
+                    "Sum of Price"
+                    "Average of Price"
+                    "Sum of Price"
+                    "Average of Price"
+                    ""
+                    ""]
+                   ["Doohickey" "$632.14" "48.63" "$854.19" "50.25" "$496.43" "62.05" "$203.13" "50.78" "$2,185.89" "52.93"]
+                   ["Gadget" "$679.83" "52.29" "$1,059.11" "55.74" "$844.51" "60.32" "$435.75" "62.25" "$3,019.20" "57.65"]
+                   ["Gizmo" "$529.70" "58.86" "$1,080.18" "51.44" "$997.94" "58.7" "$227.06" "56.77" "$2,834.88" "56.44"]
+                   ["Widget" "$987.39" "51.97" "$1,014.68" "56.37" "$912.20" "65.16" "$195.04" "65.01" "$3,109.31" "59.63"]
+                   ["Grand totals"
+                    "$2,829.06"
+                    "52.94"
+                    "$4,008.16"
+                    "53.45"
+                    "$3,251.08"
+                    "61.56"
+                    "$1,060.98"
+                    "58.7"
+                    "$11,149.28"
+                    "56.66"]]
                   #{:unsaved-card-download :card-download :dashcard-download
                     :alert-attachment :subscription-attachment
                     :public-question-download :public-dashcard-download}]
@@ -586,7 +585,7 @@
                                                     :source-table (format "card__%s" pivot-data-card-id)}}}]
             (let [result (card-download pivot-card {:export-format :csv :pivot true})]
               (is
-               (= [[ "B" "C" "3" "4" "Row totals"]
+               (= [["B" "C" "3" "4" "Row totals"]
                    ["BA" "3" "1" "1" "2"]
                    ["BA" "4" "1" "1" "2"]
                    ;; Without the fix in pr#50380, this would incorrectly look like:
