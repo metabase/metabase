2e7f1a9c48e14e14006e7077a698b5b3e5e73175
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2024-11-26 10:08:47 +0100

    Add new selectors

--- frontend/src/metabase/home/selectors.ts
+++ frontend/src/metabase/home/selectors.ts
@@ -1,4 +1,9 @@
+import { createSelector } from "@reduxjs/toolkit";
+import dayjs from "dayjs";
+
+import { getIsEmbedded } from "metabase/selectors/embed";
 import { getSetting } from "metabase/selectors/settings";
+import { getIsWhiteLabeling } from "metabase/selectors/whitelabel";
 import type { State } from "metabase-types/store";
 
 export const getIsXrayEnabled = (state: State) => {
@@ -8,3 +13,16 @@ export const getIsXrayEnabled = (state: State) => {
 export const getHasMetabotLogo = (state: State) => {
   return getSetting(state, "show-metabot");
 };
+
+const getIsNewInstance = (state: State) => {
+  const instanceCreated = getSetting(state, "instance-creation");
+  const daysSinceCreation = dayjs().diff(dayjs(instanceCreated), "days");
+  return daysSinceCreation <= 30;
+};
+
+export const getShouldShowOnboardingLink = createSelector(
+  [getIsNewInstance, getIsEmbedded, getIsWhiteLabeling],
+  (isNewInstance, isEmbedded, isWhiteLabelled) => {
+    return isNewInstance && !isEmbedded && !isWhiteLabelled;
+  },
+);
