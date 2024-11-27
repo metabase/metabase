66cc57c3938eb05830889563e0bb54c451592c59
Chris Truter <chris@metabase.com>
2024-11-27 14:58:19 +0200

    Avoid renaming tables, embrace randomness

--- src/metabase/search/appdb/index.clj
+++ src/metabase/search/appdb/index.clj
@@ -13,36 +13,36 @@
 (comment
   postgres/keep-me)
 
+(set! *warn-on-reflection* true)
+
 (def ^:private insert-batch-size 150)
 
-(def ^:dynamic *active-table*
-  "The table against which we should currently make search queries. Dynamic for testing."
-  :search_index)
+(def ^:dynamic ^:private *active-table* (atom nil))
 
-(def ^:private pending-table :search_index_next)
+(def ^:dynamic ^:private *pending-table* (atom nil))
 
-(def ^:private retired-table :search_index_retired)
+(defn gen-table-name
+  "Generate a unique table name to use as a search index table."
+  []
+  (keyword (str/replace (str "search_index_" (random-uuid)) #"-" "_")))
 
-(defonce ^:private initialized? (atom false))
+(defn active-table
+  "The table against which we should currently make search queries."
+  []
+  @*active-table*)
 
-(defonce ^:private reindexing? (atom false))
-
-(defn- random-prefix []
-  (str/replace (str (name *active-table*) "_" (random-uuid)) #"-" "_"))
+(defn pending-table
+  "A partially populated table that will take over from [[active-table]] when it is done."
+  []
+  @*pending-table*)
 
 (defn- exists? [table-name]
   (t2/exists? :information_schema.tables :table_name (name table-name)))
 
 (defn- drop-table! [table-name]
   (boolean
-   (when (exists? table-name)
+   (when (and table-name (exists? table-name))
      (t2/query (sql.helpers/drop-table table-name)))))
-
-(defn- rename-table! [old new]
-  (when (and (exists? old) (not (exists? new)))
-    (-> (sql.helpers/alter-table old)
-        (sql.helpers/rename-table new)
-        t2/query)))
 
 (defn create-table!
   "Create an index table with the given name. Should fail if it already exists."
@@ -50,33 +50,38 @@
   (-> (sql.helpers/create-table table-name)
       (sql.helpers/with-columns (specialization/table-schema))
       t2/query)
-  (let [idx-prefix (random-prefix)
-        table-name (name table-name)]
-    (doseq [stmt (specialization/post-create-statements idx-prefix table-name)]
+  (let [table-name (name table-name)]
+    (doseq [stmt (specialization/post-create-statements table-name table-name)]
       (t2/query stmt))))
 
 (defn maybe-create-pending!
   "Create a search index table."
   ([]
-   (when (not @reindexing?)
-     (maybe-create-pending! pending-table)
-     (reset! reindexing? true)))
+   (when (not (pending-table))
+     (let [tn (gen-table-name)]
+       (maybe-create-pending! tn)
+       (reset! *pending-table* tn))))
   ([table-name]
    (when-not (exists? table-name)
      (create-table! table-name))))
 
-(defn activate-pending!
+(defn activate-table!
   "Make the pending index active if it exists. Returns true if it did so."
-  []
-  ;; ... just in case it wasn't cleaned up last time.
-  (drop-table! retired-table)
-  (when (exists? pending-table)
-    (t2/with-transaction [_conn]
-      (rename-table! *active-table* retired-table)
-      (rename-table! pending-table *active-table*))
-    (reset! reindexing? false)
-    (drop-table! retired-table)
-    true))
+  ([]
+   (activate-table! (pending-table)))
+  ([table-name]
+   (boolean
+    (when (exists? table-name)
+      (let [active (active-table)]
+        (when (not= active table-name)
+          (reset! *active-table* table-name)
+          (swap! *pending-table* #(when (not= % table-name) %))
+          (when active
+            ;; TODO we need more graceful way to drop these
+            (-> (Thread. (fn []
+                           (Thread/sleep 1000)
+                           (drop-table! active)))
+                (.run)))))))))
 
 (defn- document->entry [entity]
   (-> entity
@@ -98,36 +103,33 @@
   [id search-models]
   ;; In practice, we expect this to be 1-1, but the data model does not preclude it.
   (when (seq search-models)
-    (when @initialized?
-      (t2/delete! *active-table* :model_id id :model [:in search-models]))
-    (when @reindexing?
-      (t2/delete! pending-table :model_id id :model [:in search-models]))))
+    (doseq [table-name [(active-table) (pending-table)] :when table-name]
+      (t2/delete! table-name :model_id id :model [:in search-models]))))
 
 (defn- safe-batch-upsert! [table-name entries]
-  (try
-    (specialization/batch-upsert! table-name entries)
-    (catch Exception e
-      ;; ignore database errors, the table likely doesn't exist, or has a stale schema.
-      (when-not (instance? PSQLException (ex-cause e))
-        (throw e)))))
+  ;; For convenience, if we're given a non-existing table, gracefully no-op.
+  (when table-name
+    (try
+      (specialization/batch-upsert! table-name entries)
+      (catch Exception e
+        ;; ignore database errors, the table likely doesn't exist, or has a stale schema.
+        (when-not (instance? PSQLException (ex-cause e))
+          (throw e))))))
 
 (defn- batch-update!
   "Create the given search index entries in bulk"
   [documents]
   (let [entries          (map document->entry documents)
-        ;; When stubbing the table in tests, always treat it as active
-        active?          (or @initialized? (not= :search_index *active-table*))
         ;; Ideally, we would reset these atoms if the corresponding tables don't exist.
         ;; We're about to rework this area, so just leaving this as a note for now.
-        active-updated?  (when active? (safe-batch-upsert! *active-table* entries))
-        pending-updated? (when @reindexing?  (safe-batch-upsert! pending-table entries))]
+        active-updated?  (safe-batch-upsert! (active-table) entries)
+        pending-updated? (safe-batch-upsert! (pending-table) entries)]
     (when (or active-updated? pending-updated?)
       (->> entries (map :model) frequencies))))
 
 (defmethod search.engine/reset-tracking! :search.engine/fulltext [_]
-  ;; TODO we'll make this safe when we switch to the metadata table.
-  (reset! initialized? false)
-  (reset! reindexing? false))
+  (reset! *active-table* nil)
+  (reset! *pending-table* nil))
 
 (defmethod search.engine/consume! :search.engine/fulltext [_engine document-reducible]
   (transduce (comp (partition-all insert-batch-size)
@@ -140,7 +142,8 @@
   ([search-term search-ctx]
    (search-query search-term search-ctx [:model_id :model]))
   ([search-term search-ctx select-items]
-   (specialization/base-query *active-table* search-term search-ctx select-items)))
+   (when-let [index-table (active-table)]
+     (specialization/base-query index-table search-term search-ctx select-items))))
 
 (defn search
   "Use the index table to search for records."
@@ -151,21 +154,26 @@
 (defn reset-index!
   "Ensure we have a blank slate; in case the table schema or stored data format has changed."
   []
-  ;; Moving to random tables will clean this up
-  (let [testing?   (not= *active-table* :search_index)
-        table-name (if testing? *active-table* pending-table)]
-    (when-not testing?
-      (reset! reindexing? false))
-    (drop-table! table-name)
-    (maybe-create-pending! table-name)
-    (when-not testing?
-      (activate-pending!)
-      (reset! initialized? true))
-    true))
+  (drop-table! (pending-table))
+  (maybe-create-pending!)
+  (activate-table!))
 
 (defn ensure-ready!
   "Ensure the index is ready to be populated. Return false if it was already ready."
   [force-recreation?]
-  (if (or force-recreation? (not (exists? *active-table*)))
+  (if (or force-recreation? (not (exists? (active-table))))
     (reset-index!)
-    (reset! initialized? true)))
+    true))
+
+#_{:clj-kondo/ignore [:metabase/test-helpers-use-non-thread-safe-functions]}
+(defmacro with-temp-index-table
+  "Create a temporary index table for the duration of the body."
+  [& body]
+  `(let [table-name# (gen-table-name)]
+     (binding [*pending-table* (atom nil)
+               *active-table*  (atom table-name#)]
+       (try
+         (create-table! table-name#)
+         ~@body
+         (finally
+           (#'drop-table! table-name#))))))
