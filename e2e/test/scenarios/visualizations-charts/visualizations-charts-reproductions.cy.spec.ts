9d6e86eb615feb0bf3f1327110b56dc091b7bdf4
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-11-27 14:41:16 +0300

    fix new test

--- e2e/test/scenarios/visualizations-charts/visualizations-charts-reproductions.cy.spec.ts
+++ e2e/test/scenarios/visualizations-charts/visualizations-charts-reproductions.cy.spec.ts
@@ -176,7 +176,7 @@ describe("issue 49529", () => {
 
     visitQuestionAdhoc(question);
 
-    cy.findByTestId("viz-settings-button").click();
+    openVizType("Data");
 
     cy.findAllByTestId("select-button").eq(0).as("dimensionSelect").click();
     popover().findByText("ID").click();
