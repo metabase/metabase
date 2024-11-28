fbbaba0ee118f2a2c02428d31d80a8105964990f
Romeo Van Snick <romeo@romeovansnick.be>
2024-11-29 09:22:12 +1100

    Update test helpers

--- e2e/support/helpers/e2e-native-editor-helpers.ts
+++ e2e/support/helpers/e2e-native-editor-helpers.ts
@@ -1,18 +1,18 @@
 export function nativeEditor() {
   cy.findAllByTestId("loading-indicator").should("not.exist");
-  return cy.get("[data-testid=native-query-editor] .ace_content");
+  return cy.get("[data-testid=native-query-editor] .cm-content");
 }
 
 export function focusNativeEditor() {
   nativeEditor().should("be.visible").click();
 
-  return cy
-    .findByTestId("native-query-editor")
-    .should("have.class", "ace_focus");
+  nativeEditor().get(".cm-editor").should("have.class", "cm-focused");
+
+  return nativeEditor();
 }
 
 export function blurNativeEditor() {
-  cy.get(".ace_text-input").blur();
+  nativeEditor().get(".cm-editor").blur();
 }
 
 export function nativeEditorCompletions() {
