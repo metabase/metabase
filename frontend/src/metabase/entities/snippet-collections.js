a2a9eaba89900afa2fc3bc9562387b2271604e11
Kamil Mielnik <kamil@kamilmielnik.com>
2024-11-28 20:22:44 +0700

    Revert "Enable RTK for snippet collections"
    This reverts commit e1439cb28bcc89c99feac1d6fe6c51a0ecba1aae.

--- frontend/src/metabase/entities/snippet-collections.js
+++ frontend/src/metabase/entities/snippet-collections.js
@@ -2,7 +2,6 @@ import { createSelector } from "@reduxjs/toolkit";
 import { t } from "ttag";
 import _ from "underscore";
 
-import { useGetCollectionQuery } from "metabase/api";
 import { canonicalCollectionId } from "metabase/collections/utils";
 import NormalCollections, {
   getExpandedCollectionsById,
@@ -19,12 +18,6 @@ const SnippetCollections = createEntity({
 
   displayNameOne: t`snippet collection`,
   displayNameMany: t`snippet collections`,
-
-  rtk: {
-    getUseGetQuery: () => ({
-      useGetQuery,
-    }),
-  },
 
   api: _.mapObject(
     NormalCollections.api,
@@ -71,12 +64,5 @@ const SnippetCollections = createEntity({
     return undefined; // not tracking
   },
 });
-
-const useGetQuery = opts => {
-  return useGetCollectionQuery({
-    namespace: "snippets",
-    ...opts,
-  });
-};
 
 export default SnippetCollections;
