f5e63b40018a166e01765831006d2c53a1db24f0
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2024-12-03 19:23:38 +0100

    Introduce the non-admin Onboarding Checklist (#50516)
    * Add new selectors
    
    * Adjust the onboarding checklist for non-admins
    
    * Add unit tests for non-admins
    
    * Rename the control knob
    
    * Fix the forgotten test
    
    * Stop guarding the route for admins only
    
    * Translate `alt` tags
    
    Co-authored-by: Raphael Krut-Landau <raphael.kl@gmail.com>
    
    * Translate `alt` tags
    
    Co-authored-by: Raphael Krut-Landau <raphael.kl@gmail.com>
    
    * Add e2e coverage for non-admins
    
    * Refine selectors
    
    * Improve the route guard
    
    * Increase E2E coverage for route guards
    
    ---------
    
    Co-authored-by: Raphael Krut-Landau <raphael.kl@gmail.com>

--- frontend/src/metabase/home/selectors.ts
+++ frontend/src/metabase/home/selectors.ts
@@ -1,4 +1,9 @@
+import { createSelector } from "@reduxjs/toolkit";
+import dayjs from "dayjs";
+
+import { getIsEmbedded } from "metabase/selectors/embed";
 import { getSetting } from "metabase/selectors/settings";
+import { getIsWhiteLabeling } from "metabase/selectors/whitelabel";
 import type { State } from "metabase-types/store";
 
 export const getIsXrayEnabled = (state: State) => {
@@ -8,3 +13,16 @@ export const getIsXrayEnabled = (state: State) => {
 export const getHasMetabotLogo = (state: State) => {
   return getSetting(state, "show-metabot");
 };
+
+export const getIsNewInstance = (state: State) => {
+  const instanceCreated = getSetting(state, "instance-creation");
+  const daysSinceCreation = dayjs().diff(dayjs(instanceCreated), "days");
+  return daysSinceCreation <= 30;
+};
+
+export const getCanAccessOnboardingPage = createSelector(
+  [getIsEmbedded, getIsWhiteLabeling],
+  (isEmbedded, isWhiteLabelled) => {
+    return !isEmbedded && !isWhiteLabelled;
+  },
+);
