deaa8704c3961d34d4857befbe2d1411070770ac
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-12-03 17:12:02 +0300

    more css

--- frontend/src/metabase/query_builder/components/view/PreviewQueryButton/PreviewQueryButton.tsx
+++ frontend/src/metabase/query_builder/components/view/PreviewQueryButton/PreviewQueryButton.tsx
@@ -1,14 +1,16 @@
 import { useCallback } from "react";
 import { t } from "ttag";
 
+import IconButtonWrapper from "metabase/components/IconButtonWrapper";
 import Tooltip from "metabase/core/components/Tooltip";
 import type { QueryModalType } from "metabase/query_builder/constants";
 import { MODAL_TYPES } from "metabase/query_builder/constants";
+import { Icon } from "metabase/ui";
 import * as Lib from "metabase-lib";
 import type Question from "metabase-lib/v1/Question";
 import type NativeQuery from "metabase-lib/v1/queries/NativeQuery";
 
-import { PreviewButton, PreviewButtonIcon } from "./PreviewQueryButton.styled";
+import PreviewQueryButtonS from "./PreviewQueryButton.module.css";
 
 interface PreviewQueryButtonProps {
   onOpenModal?: (modalType: QueryModalType) => void;
@@ -23,9 +25,16 @@ export const PreviewQueryButton = ({
 
   return (
     <Tooltip tooltip={t`Preview the query`}>
-      <PreviewButton aria-label={t`Preview the query`} onClick={handleClick}>
-        <PreviewButtonIcon name="eye_filled" />
-      </PreviewButton>
+      <IconButtonWrapper
+        className={PreviewQueryButtonS.PreviewButton}
+        aria-label={t`Preview the query`}
+        onClick={handleClick}
+      >
+        <Icon
+          className={PreviewQueryButtonS.PreviewButtonIcon}
+          name="eye_filled"
+        />
+      </IconButtonWrapper>
     </Tooltip>
   );
 };
