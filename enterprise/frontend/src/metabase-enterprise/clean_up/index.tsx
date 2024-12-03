9fdcce03953649700f8c063894521a218bdbb85c
Sloan Sparger <sloansparger@gmail.com>
2024-12-03 14:53:39 -0600

    pr feedback

--- enterprise/frontend/src/metabase-enterprise/clean_up/index.tsx
+++ enterprise/frontend/src/metabase-enterprise/clean_up/index.tsx
@@ -1,9 +1,5 @@
 import { t } from "ttag";
 
-import {
-  isInstanceAnalyticsCustomCollection,
-  isTrashedCollection,
-} from "metabase/collections/utils";
 import { ModalRoute } from "metabase/hoc/ModalRoute";
 import * as Urls from "metabase/lib/urls";
 import { PLUGIN_COLLECTIONS } from "metabase/plugins";
@@ -11,19 +7,13 @@ import { hasPremiumFeature } from "metabase-enterprise/settings";
 
 import { CleanupCollectionModal } from "./CleanupCollectionModal";
 import { CollectionCleanupAlert } from "./CollectionCleanupAlert";
+import { canCleanUp } from "./utils";
 
 if (hasPremiumFeature("collection_cleanup")) {
-  PLUGIN_COLLECTIONS.canCleanUp = collection => {
-    return Boolean(
-      !isInstanceAnalyticsCustomCollection(collection) &&
-        !isTrashedCollection(collection) &&
-        !collection.is_sample &&
-        collection.can_write,
-    );
-  };
+  PLUGIN_COLLECTIONS.canCleanUp = canCleanUp;
 
   PLUGIN_COLLECTIONS.getCleanUpMenuItems = (collection, itemCount) => {
-    if (!PLUGIN_COLLECTIONS.canCleanUp(collection) || itemCount === 0) {
+    if (!canCleanUp(collection) || itemCount === 0) {
       return [];
     }
 
