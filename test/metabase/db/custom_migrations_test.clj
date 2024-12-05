fc9458183d90dd207f631eebe88e19c98df148ad
bryan <bryan.maass@gmail.com>
2024-12-05 09:54:29 -0700

    Apply suggestions from code review
    Co-authored-by: Chris Truter <crisptrutski@users.noreply.github.com>

--- test/metabase/db/custom_migrations_test.clj
+++ test/metabase/db/custom_migrations_test.clj
@@ -1992,9 +1992,7 @@
           (binding [custom-migrations/*create-sample-content* create?]
             (is (false? (sample-content-created?)))
             (migrate!)
-            (if create?
-              (is (true? (sample-content-created?)))
-              (is (false? (sample-content-created?)))))
+            (is (= create? sample-content-created?))
 
           (when (true? create?)
             (testing "The Examples collection has permissions set to grant read-write access to all users"
