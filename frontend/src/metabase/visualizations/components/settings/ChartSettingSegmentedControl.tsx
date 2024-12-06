e39c1b20ed6fdd56f81f96de1cce858bc5af8802
Oisin Coveney <oisin@metabase.com>
2024-12-06 12:56:26 +0200

    Save progress

---
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingSegmentedControl.tsx
@@ -0,0 +1,14 @@
+import { SegmentedControl as OldSegmentedControl } from "metabase/components/SegmentedControl";
+import { SegmentedControl, Stack } from "metabase/ui";
+
+function ChartSettingSegmentedControl(props) {
+  // __AUTO_GENERATED_PRINT_VAR_START__
+  console.log("ChartSettingSegmentedControl   : ", props); // __AUTO_GENERATED_PRINT_VAR_END__
+  return (
+    <Stack>
+      <OldSegmentedControl fullWidth {...props} />
+    </Stack>
+  );
+}
+
+export default ChartSettingSegmentedControl;
