23e0c4d38af95473a0b4a6092d7574dbf8c0cce2
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-12-06 13:25:56 -0500

    Remove todo

--- frontend/src/metabase/status/components/StatusListing/StatusListing.styled.tsx
+++ frontend/src/metabase/status/components/StatusListing/StatusListing.styled.tsx
@@ -4,6 +4,5 @@ export const StatusListingRoot = styled.div`
   position: fixed;
   right: 1.5rem;
   bottom: 1.5rem;
-  /* TODO: Use one of the existing floating item types and remove this */
   z-index: 2;
 `;
