215690c1889986c907f901e343dfe94609c5e453
Kamil Mielnik <kamil@kamilmielnik.com>
2024-12-09 20:39:08 +0700

    Get rid of Collection.Link usage in DashboardMoveModal

--- frontend/src/metabase/dashboard/components/DashboardMoveModal.tsx
+++ frontend/src/metabase/dashboard/components/DashboardMoveModal.tsx
@@ -2,8 +2,10 @@ import { connect } from "react-redux";
 import { c, t } from "ttag";
 import _ from "underscore";
 
+import { useGetCollectionQuery } from "metabase/api";
 import { MoveModal } from "metabase/containers/MoveModal";
-import Collection, { ROOT_COLLECTION } from "metabase/entities/collections";
+import Link from "metabase/core/components/Link";
+import { ROOT_COLLECTION } from "metabase/entities/collections";
 import Dashboards from "metabase/entities/dashboards";
 import { color } from "metabase/lib/colors";
 import * as Urls from "metabase/lib/urls";
@@ -55,24 +57,31 @@ const DashboardMoveToast = ({
   collectionId,
 }: {
   collectionId: CollectionId;
-}) => (
-  <ToastRoot>
-    <Icon
-      name="collection"
-      style={{ marginInlineEnd: "0.25rem" }}
-      color="text-white"
-    />
-    {c("{0} is a location where the dashboard was moved to")
-      .jt`Dashboard moved to ${(
-      <Collection.Link
-        key="link"
-        id={collectionId}
-        style={{ marginInlineStart: ".25em" }}
-        color={color("brand")}
+}) => {
+  const { data: collection } = useGetCollectionQuery({ id: collectionId });
+
+  return (
+    <ToastRoot>
+      <Icon
+        name="collection"
+        style={{ marginInlineEnd: "0.25rem" }}
+        color="text-white"
       />
-    )}`}
-  </ToastRoot>
-);
+      {c("{0} is a location where the dashboard was moved to")
+        .jt`Dashboard moved to ${
+        collection ? (
+          <Link
+            to={Urls.collection(collection)}
+            style={{ marginInlineStart: ".25em" }}
+            color={color("brand")}
+          >
+            {collection.name}
+          </Link>
+        ) : null
+      }`}
+    </ToastRoot>
+  );
+};
 
 export const DashboardMoveModalConnected = _.compose(
   connect(null, mapDispatchToProps),
