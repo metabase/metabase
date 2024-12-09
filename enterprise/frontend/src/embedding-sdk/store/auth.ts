5d0621883415e9d9784afe6a7356d6c663fbd36a
Phoomparin Mano <phoomparin@gmail.com>
2024-12-10 00:28:28 +0800

    fix lint

--- enterprise/frontend/src/embedding-sdk/store/auth.ts
+++ enterprise/frontend/src/embedding-sdk/store/auth.ts
@@ -183,4 +183,3 @@ const sessionSchema = Yup.object({
   // We should also receive `iat` and `status` in the response, but we don't actually need them
   // as we don't use them, so we don't throw an error if they are missing
 });
-
