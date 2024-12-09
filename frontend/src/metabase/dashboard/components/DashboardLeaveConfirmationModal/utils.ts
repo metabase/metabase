d0d7f416d99a27906911dfc40c1c60462a391d33
Sloan Sparger <sloansparger@gmail.com>
2024-12-09 16:06:30 -0600

    finish feedback from FE code review

--- frontend/src/metabase/dashboard/components/DashboardLeaveConfirmationModal/utils.ts
+++ frontend/src/metabase/dashboard/components/DashboardLeaveConfirmationModal/utils.ts
@@ -3,15 +3,15 @@ import type { Location } from "history";
 import { deserializeCard, parseHash } from "metabase/query_builder/actions";
 
 export const isNavigatingToCreateADashboardQuestion = (
-  location?: Location,
+  nextLocation?: Location,
 ): boolean => {
   const isQuestionCreationUrl =
-    (location?.pathname === "/question/notebook" ||
-      location?.pathname === "/question/query") &&
-    location?.hash;
+    (nextLocation?.pathname === "/question/notebook" ||
+      nextLocation?.pathname === "/question/query") &&
+    nextLocation?.hash;
 
   if (isQuestionCreationUrl) {
-    const { serializedCard } = parseHash(location.hash);
+    const { serializedCard } = parseHash(nextLocation.hash);
     const deserializedCard = serializedCard && deserializeCard(serializedCard);
 
     const isDashboardQuestion =
