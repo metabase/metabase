d29e5130ef15e290e34f023d27c868944251f042
Kamil Mielnik <kamil@kamilmielnik.com>
2024-12-10 16:57:23 +0700

    Use database from the store if it's already there

--- frontend/src/metabase/browse/tables/TableBrowser/useDatabaseCrumb.ts
+++ frontend/src/metabase/browse/tables/TableBrowser/useDatabaseCrumb.ts
@@ -1,12 +1,24 @@
 import { t } from "ttag";
 
 import { skipToken, useGetDatabaseQuery } from "metabase/api";
+import Databases from "metabase/entities/databases";
+import { useSelector } from "metabase/lib/redux";
 import * as Urls from "metabase/lib/urls";
 import { SAVED_QUESTIONS_VIRTUAL_DB_ID } from "metabase-lib/v1/metadata/utils/saved-questions";
 import type { DatabaseId } from "metabase-types/api";
 
 export const useDatabaseCrumb = (id: DatabaseId) => {
-  const { data: database } = useGetDatabaseQuery(
+  // We display what we already have in store to avoid showing loading state.
+  // It's possible because GET /api/database is always dispatched on app launch.
+  // We still re-fetch the database to get new data, just in case it changed.
+  const legacyDatabase = useSelector(state => {
+    return Databases.selectors.getObject(state, {
+      entityId: id,
+      requestType: "fetch",
+    });
+  });
+
+  const { data: fetchedDatabase } = useGetDatabaseQuery(
     id === SAVED_QUESTIONS_VIRTUAL_DB_ID ? skipToken : { id },
   );
 
@@ -17,8 +29,13 @@ export const useDatabaseCrumb = (id: DatabaseId) => {
     };
   }
 
-  if (!database) {
-    return null;
+  const database = fetchedDatabase ?? legacyDatabase;
+
+  if (database) {
+    return {
+      title: null,
+      to: Urls.browseDatabase({ id }),
+    };
   }
 
   return {
