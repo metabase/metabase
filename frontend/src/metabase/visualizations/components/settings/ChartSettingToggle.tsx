555dc166f970dbbca9b8352ebc1a49b3fad02b3a
Oisin Coveney <oisin@metabase.com>
2024-12-11 16:42:25 +0200

    Add label

--- frontend/src/metabase/visualizations/components/settings/ChartSettingToggle.tsx
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingToggle.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable react/prop-types */
-import { Switch } from "metabase/ui";
+import CS from "metabase/css/core/index.css";
+import { Switch, Text } from "metabase/ui";
 
 export const ChartSettingToggle = ({ value, onChange, id, title }) => (
   <Switch
-    label={title}
+    label={
+      <Text truncate fw="bold">
+        {title}
+      </Text>
+    }
     labelPosition="left"
     checked={value}
     onChange={e => onChange(e.currentTarget.checked)}
     id={id}
+    w="100%"
+    size="sm"
+    classNames={{
+      labelWrapper: CS.fullWidth,
+    }}
   />
 );
