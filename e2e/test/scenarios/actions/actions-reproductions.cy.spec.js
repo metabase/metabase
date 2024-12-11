0065965a41d99f1ed4df0379f696f4ce481ddd44
Kamil Mielnik <kamil@kamilmielnik.com>
2024-12-11 20:21:59 +0700

    Add repro for when primary key is not called 'id'

--- e2e/test/scenarios/actions/actions-reproductions.cy.spec.js
+++ e2e/test/scenarios/actions/actions-reproductions.cy.spec.js
@@ -1,5 +1,5 @@
 import { H } from "e2e/support";
-import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
+import { SAMPLE_DB_ID, WRITABLE_DB_ID } from "e2e/support/cypress_data";
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import {
   ORDERS_DASHBOARD_ID,
@@ -226,41 +226,45 @@ describe("Issue 32974", { tags: ["@external", "@actions"] }, () => {
 describe("issue 51020", () => {
   beforeEach(() => {
     H.restore();
+    H.restore("postgres-writable");
     cy.signInAsAdmin();
     H.setActionsEnabledForDB(SAMPLE_DB_ID);
+  });
 
-    H.visitModel(ORDERS_MODEL_ID);
+  function setupBasicActionsInModel() {
     H.questionInfoButton().click();
     H.modal().findByText("See more about this model").click();
     cy.findByRole("tab", { name: "Actions" }).click();
     cy.button(/Create basic actions/).click();
+  }
 
+  function setupDashboard({ questionName, modelName, columnName }) {
     H.newButton("Dashboard").click();
     H.modal().findByLabelText("Name").type("Dash");
     H.modal().button("Create").click();
 
     cy.button("Add a saved question").click();
-    cy.findByTestId("add-card-sidebar").findByText("Orders Model").click();
+    cy.findByTestId("add-card-sidebar").findByText(questionName).click();
 
     cy.findByLabelText("Add a filter or parameter").click();
     H.popover().findByText("ID").click();
     H.getDashboardCard().findByText("Select…").click();
-    H.popover().findAllByText("ID").eq(0).click();
+    H.popover().findAllByText(columnName).eq(0).click();
     cy.button("Done").click();
 
     H.getDashboardCard().realHover().icon("click").click();
     cy.get("aside").within(() => {
-      cy.findByText("ID").click();
+      cy.findByText(columnName).click();
       cy.findByText("Update a dashboard filter").click();
       cy.findByTestId("click-target-column").click();
     });
-    H.popover().findByText("ID").click();
+    H.popover().findByText(columnName).click();
     cy.button("Done").click();
 
     cy.findByLabelText("Add action").click();
     cy.button("Pick an action").click();
     H.modal().within(() => {
-      cy.findByText("Orders Model").click();
+      cy.findByText(modelName).click();
       cy.findByText("Update").click();
       cy.findAllByText("Ask the user").eq(0).click();
     });
@@ -268,25 +272,128 @@ describe("issue 51020", () => {
     cy.button("Done").click();
 
     H.saveDashboard();
+  }
+
+  describe("when primary key is called 'id'", () => {
+    beforeEach(() => {
+      H.visitModel(ORDERS_MODEL_ID);
+      setupBasicActionsInModel();
+      setupDashboard({
+        modelName: "Orders Model",
+        questionName: "Orders Model",
+        columnName: "ID",
+      });
+    });
+
+    it("should pass primary key attribute to execute action endpoint when it's populated with click behavior or URL (metabase#51020)", () => {
+      cy.log(
+        "check when primary key parameter is populated with click behavior",
+      );
+      H.getDashboardCard(0).findAllByText("1").eq(0).click();
+      H.getDashboardCard(1).findByText("Click Me").click();
+      H.modal().findByLabelText("Discount").type("5");
+      H.modal().button("Update").click();
+
+      H.modal().should("not.exist");
+      H.undoToast().findByText("Successfully updated").should("be.visible");
+
+      cy.log("check when primary key parameter is populated with URL");
+      cy.reload();
+      H.getDashboardCard(1).findByText("Click Me").click();
+      H.modal().findByLabelText("Discount").type("{backspace}");
+      H.modal().button("Update").click();
+
+      H.modal().should("not.exist");
+      H.undoToast().findByText("Successfully updated").should("be.visible");
+    });
   });
 
-  it("should pass id attribute to execute action endpoint when it's populated with click behavior or URL (metabase#51020)", () => {
-    cy.log("check when id parameter is populated with click behavior");
-    H.getDashboardCard(0).findAllByText("1").eq(0).click();
-    H.getDashboardCard(1).findByText("Click Me").click();
-    H.modal().findByLabelText("Discount").type("5");
-    H.modal().button("Update").click();
+  describe("when primary key is not called 'id'", () => {
+    function createTemporaryTable() {
+      H.queryWritableDB(
+        "CREATE TABLE IF NOT EXISTS foo (foo INT PRIMARY KEY, name VARCHAR)",
+      );
+      H.queryWritableDB(
+        "INSERT INTO foo (foo, name) VALUES (1, 'Foo'), (2, 'Bar')",
+      );
+    }
 
-    H.modal().should("not.exist");
-    H.undoToast().findByText("Successfully updated").should("be.visible");
+    function dropTemporaryTable() {
+      H.queryWritableDB("ALTER TABLE IF EXISTS foo DROP CONSTRAINT foo_pkey");
+      H.queryWritableDB("DROP TABLE IF EXISTS foo");
+    }
 
-    cy.log("check when id parameter is populated with URL");
-    cy.reload();
-    H.getDashboardCard(1).findByText("Click Me").click();
-    H.modal().findByLabelText("Discount").type("{backspace}");
-    H.modal().button("Update").click();
+    beforeEach(() => {
+      cy.intercept("POST", "/api/dataset").as("dataset");
+      cy.intercept("POST", "/api/card").as("createCard");
+      cy.intercept("GET", "/api/card/*").as("getCard");
 
-    H.modal().should("not.exist");
-    H.undoToast().findByText("Successfully updated").should("be.visible");
+      dropTemporaryTable();
+      createTemporaryTable();
+      H.resyncDatabase({ dbId: WRITABLE_DB_ID, tableName: "foo" });
+
+      cy.visit("/");
+      H.newButton("Model").click();
+      cy.findByTestId("new-model-options")
+        .findByText("Use the notebook editor")
+        .click();
+      H.entityPickerModal().within(() => {
+        cy.findByPlaceholderText("Search this collection or everywhere…").type(
+          "foo",
+        );
+        cy.findByText("Everywhere").click();
+        cy.findByText("Foo").click();
+      });
+      cy.findByTestId("run-button").click();
+      cy.wait("@dataset");
+      cy.button("Save").click();
+      H.modal()
+        .findByLabelText("Name")
+        .type("{backspace}{backspace}{backspace}Model 51020");
+      H.modal().button("Save").click();
+      cy.wait("@createCard");
+      cy.wait("@getCard");
+      setupBasicActionsInModel();
+
+      H.newButton("Question").click();
+      H.entityPickerModalTab("Models").click();
+      H.entityPickerModalItem(1, "Model 51020").click();
+      H.saveQuestion("Question 51020", undefined, {
+        tab: "Browse",
+        path: ["Our analytics"],
+      });
+
+      setupDashboard({
+        modelName: "Model 51020",
+        questionName: "Question 51020",
+        columnName: "Foo",
+      });
+    });
+
+    afterEach(() => {
+      dropTemporaryTable();
+    });
+
+    it("should pass primary key attribute to execute action endpoint when it's populated with click behavior or URL (metabase#51020)", () => {
+      cy.log(
+        "check when primary key parameter is populated with click behavior",
+      );
+      H.getDashboardCard(0).findAllByText("1").eq(0).click();
+      H.getDashboardCard(1).findByText("Click Me").click();
+      H.modal().findByLabelText("Name").type(" Baz");
+      H.modal().button("Update").click();
+
+      H.modal().should("not.exist");
+      H.undoToast().findByText("Successfully updated").should("be.visible");
+
+      cy.log("check when primary key parameter is populated with URL");
+      cy.reload();
+      H.getDashboardCard(1).findByText("Click Me").click();
+      H.modal().findByLabelText("Name").type(" Baz");
+      H.modal().button("Update").click();
+
+      H.modal().should("not.exist");
+      H.undoToast().findByText("Successfully updated").should("be.visible");
+    });
   });
 });
