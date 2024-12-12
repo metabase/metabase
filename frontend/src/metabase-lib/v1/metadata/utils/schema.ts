6b8af2fe4ec40874645ad7218b08080c2c87136e
Kamil Mielnik <kamil@kamilmielnik.com>
2024-12-12 21:29:19 +0700

    Convert metabase-lib/v1/metadata/utils/schema.js to TS

--- frontend/src/metabase-lib/v1/metadata/utils/schema.ts
+++ frontend/src/metabase-lib/v1/metadata/utils/schema.ts
@@ -1,18 +1,37 @@
-export const getSchemaName = id => {
+import type { DatabaseId, SchemaId, SchemaName } from "metabase-types/api";
+
+export const getSchemaName = (id: string | null | undefined): SchemaId => {
   return parseSchemaId(id)[1];
 };
 
-export const parseSchemaId = id => {
+type ParseSchemaIdResultOptions = {
+  isDatasets?: boolean;
+};
+
+type ParseSchemaIdResult =
+  | [DatabaseId, SchemaId]
+  | [DatabaseId, SchemaId, ParseSchemaIdResultOptions];
+
+export const parseSchemaId = (
+  id: string | null | undefined,
+): ParseSchemaIdResult => {
   const schemaId = String(id || "");
   const [databaseId, schemaName, encodedPayload] = schemaId.split(":");
-  const result = [databaseId, decodeURIComponent(schemaName)];
+  const result: ParseSchemaIdResult = [
+    parseInt(databaseId, 10),
+    decodeURIComponent(schemaName),
+  ];
   if (encodedPayload) {
     result.push(JSON.parse(decodeURIComponent(encodedPayload)));
   }
   return result;
 };
 
-export const generateSchemaId = (dbId, schemaName, payload) => {
+export const generateSchemaId = (
+  dbId: DatabaseId,
+  schemaName: SchemaName | undefined | null,
+  payload?: unknown,
+): string => {
   // Schema ID components are separated with colons
   // Schema name should be encoded to escape colon characters
   // so parseSchemaId can work correctly
