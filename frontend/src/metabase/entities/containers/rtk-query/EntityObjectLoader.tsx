9b689131a7be0c0af2c8c2947b7cc1caf6d2d804
Kamil Mielnik <kamil@kamilmielnik.com>
2024-12-12 18:43:26 +0700

    Remove old EntityObjectLoader and replace it with new one

--- frontend/src/metabase/entities/containers/rtk-query/EntityObjectLoader.tsx
+++ frontend/src/metabase/entities/containers/rtk-query/EntityObjectLoader.tsx
@@ -53,7 +53,7 @@ export interface Props<Entity, EntityWrapper> {
   entityType: EntityType | EntityTypeSelector;
   fetchType?: FetchType;
   loadingAndErrorWrapper?: boolean;
-  LoadingAndErrorWrapper: ComponentType<LoadingAndErrorWrapperProps>;
+  LoadingAndErrorWrapper?: ComponentType<LoadingAndErrorWrapperProps>;
   reload?: boolean;
   requestType?: RequestType;
   selectorName?: string;
@@ -69,7 +69,7 @@ const defaultTransformResponse = (data: unknown, _query: EntityQuery) => data;
  *
  * @deprecated use "metabase/api" instead
  */
-export function EntityObjectLoaderRtkQuery<Entity, EntityWrapper>({
+export function EntityObjectLoader<Entity, EntityWrapper>({
   ComposedComponent,
   dispatchApiErrorEvent = true,
   entityAlias,
@@ -265,12 +265,12 @@ export function EntityObjectLoaderRtkQuery<Entity, EntityWrapper>({
 /**
  * @deprecated HOCs are deprecated
  */
-export const entityObjectLoaderRtkQuery =
+export const entityObjectLoader =
   (eolProps: any) =>
   (ComposedComponent: (props: any) => ReactNode) =>
   // eslint-disable-next-line react/display-name
   (props: any): ReactNode => (
-    <EntityObjectLoaderRtkQuery
+    <EntityObjectLoader
       ComposedComponent={ComposedComponent}
       {...props}
       {...eolProps}
