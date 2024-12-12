897e93273100feb17a42349006676431eade7802
John Swanson <john.swanson@metabase.com>
2024-12-12 11:23:04 -0800

    Skeleton for `generate-insights` tool
    For now I just implemented the easy things: tables, metrics, and
    reports (which I think equal cards on our end?), and totally skipped
    `query_id`.

---
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/generate_insights.clj
@@ -0,0 +1,16 @@
+(ns metabase-enterprise.metabot-v3.tools.generate-insights
+  (:require
+   [metabase-enterprise.metabot-v3.tools.interface :as metabot-v3.tools.interface]
+   [metabase.public-settings :as public-settings]
+   [metabase.util.malli :as mu]))
+
+(mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/generate-insights
+  [_tool-name {what-for :for :as _arguments} context]
+  (let [[k id] (some #(find what-for %) [:metric_id :table_id :report_id :query_id])
+        entity-type (case k
+                      :metric_id "metric"
+                      :table_id "table"
+                      :report_id "card"
+                      :query_id (throw (ex-info "Unhandled" {})))]
+    {:output (str (public-settings/site-url) "/auto/dashboard/" entity-type "/" id)
+     :context context}))
