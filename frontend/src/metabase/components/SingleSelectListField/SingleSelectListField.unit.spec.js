28dd682433a4acaff2cfd81bcb29ea1105fa1486
Romeo Van Snick <romeo@romeovansnick.be>
2024-12-13 09:49:25 +1100

    Fix unit test

--- frontend/src/metabase/components/SingleSelectListField/SingleSelectListField.unit.spec.js
+++ frontend/src/metabase/components/SingleSelectListField/SingleSelectListField.unit.spec.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import ValueComponent from "metabase/components/Value";
 
@@ -57,22 +56,10 @@ describe("SingleSelectListField", () => {
     expect(screen.getByPlaceholderText("Find...")).toBeInTheDocument();
   });
 
-  it("displays options when alwaysShowOptions is true", () => {
+  it("displays options", () => {
     setup({ alwaysShowOptions: true });
 
     expect(screen.getByText(firstOption)).toBeInTheDocument();
     expect(screen.getByText(secondOption)).toBeInTheDocument();
-  });
-
-  it("does not display options when alwaysShowOptions is false", async () => {
-    setup();
-
-    expect(screen.queryByText(firstOption)).not.toBeInTheDocument();
-    expect(screen.queryByText(secondOption)).not.toBeInTheDocument();
-
-    await userEvent.type(screen.getByPlaceholderText("Find..."), "AK");
-
-    expect(await screen.findByText(firstOption)).toBeInTheDocument();
-    expect(screen.queryByText(secondOption)).not.toBeInTheDocument();
   });
 });
