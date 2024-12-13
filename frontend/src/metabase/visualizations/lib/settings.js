c153597500b2aae8ff576cdef72e7b59478a3e85
Oisin Coveney <oisin@metabase.com>
2024-12-13 18:06:34 +0200

    Convert ChartSettingsRadio to mantine (#51210)

--- frontend/src/metabase/visualizations/lib/settings.js
+++ frontend/src/metabase/visualizations/lib/settings.js
@@ -9,7 +9,7 @@ import ChartSettingInput from "metabase/visualizations/components/settings/Chart
 import ChartSettingInputGroup from "metabase/visualizations/components/settings/ChartSettingInputGroup";
 import { ChartSettingInputNumeric } from "metabase/visualizations/components/settings/ChartSettingInputNumeric";
 import { ChartSettingMultiSelect } from "metabase/visualizations/components/settings/ChartSettingMultiSelect";
-import ChartSettingRadio from "metabase/visualizations/components/settings/ChartSettingRadio";
+import { ChartSettingRadio } from "metabase/visualizations/components/settings/ChartSettingRadio";
 import { ChartSettingSegmentedControl } from "metabase/visualizations/components/settings/ChartSettingSegmentedControl";
 import ChartSettingSelect from "metabase/visualizations/components/settings/ChartSettingSelect";
 import ChartSettingToggle from "metabase/visualizations/components/settings/ChartSettingToggle";
