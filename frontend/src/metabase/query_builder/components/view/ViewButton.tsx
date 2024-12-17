7125dd002fe12a240980cf2538342dae14ad752f
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-12-17 15:45:53 +0300

    fix view button styling

--- frontend/src/metabase/query_builder/components/view/ViewButton.tsx
+++ frontend/src/metabase/query_builder/components/view/ViewButton.tsx
@@ -2,6 +2,8 @@ import cx from "classnames";
 import type { CSSProperties } from "react";
 
 import Button, { type ButtonProps } from "metabase/core/components/Button";
+import { alpha } from "metabase/lib/colors";
+import { useMantineTheme } from "metabase/ui";
 
 import S from "./ViewButton.module.css";
 
@@ -11,17 +13,33 @@ interface Props extends ButtonProps {
 }
 
 // NOTE: some of this is duplicated from NotebookCell.jsx
-const ViewButton = ({ className, active, color, ...props }: Props) => (
-  <Button
-    className={cx(S.ViewButton, { [S.inactive]: !active }, className)}
-    style={
-      {
-        "--view-button-color": color,
-      } as CSSProperties
-    }
-    {...props}
-  />
-);
+const ViewButton = ({ className, active, color, ...props }: Props) => {
+  const theme = useMantineTheme();
+
+  return (
+    <Button
+      className={cx(S.ViewButton, { [S.active]: active }, className)}
+      style={
+        {
+          "--view-button-color": color ?? theme.fn.themeColor("brand"),
+          "--view-button-background-color": alpha(
+            color ?? theme.fn.themeColor("brand"),
+            0.2,
+          ),
+          "--view-button-background-color-active": alpha(
+            color ?? theme.fn.themeColor("brand"),
+            0.8,
+          ),
+          "--view-button-background-color-hover": alpha(
+            color ?? theme.fn.themeColor("brand"),
+            0.35,
+          ),
+        } as CSSProperties
+      }
+      {...props}
+    />
+  );
+};
 
 // eslint-disable-next-line import/no-default-export -- deprecated usage
 export default ViewButton;
