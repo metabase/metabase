832a73119218eb052391926dbe80c67c9764bd7e
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-12-17 16:59:02 -0500

    Fix another e2e test in custom-column

--- e2e/test/scenarios/custom-column/custom-column.cy.spec.js
+++ e2e/test/scenarios/custom-column/custom-column.cy.spec.js
@@ -773,7 +773,7 @@ describe("scenarios > question > custom column", () => {
     cy.log("custom columns - in");
     H.getNotebookStep("data").button("Custom column").click();
     H.popover()
-      .first()
+      .findByTestId("expression-editor")
       .within(() => {
         H.enterCustomColumnDetails({
           formula: 'in("Gadget", [Vendor], [Category])',
