f223c4f95fe19352d1e50f601de423cbc478cb68
Sloan Sparger <sloansparger@gmail.com>
2024-12-17 16:29:59 -0600

    allow collection clean up banner to be dismissable

--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -389,6 +389,7 @@ interface PublicSettings {
 }
 
 export type UserSettings = {
+  "dismissed-collection-cleanup-banner"?: boolean;
   "dismissed-browse-models-banner"?: boolean;
   "dismissed-custom-dashboard-toast"?: boolean;
   "dismissed-onboarding-sidebar-link"?: boolean;
