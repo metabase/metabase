9c8a5672c5474ca3b07ec610d48d2bad9bcece47
Phoomparin Mano <poom@metabase.com>
2024-12-17 22:08:59 +0700

    feat(sdk): move non-auth config options to provider (#50585) (#51067)
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
    Co-authored-by: Nicolò Pretto <info@npretto.com>

--- e2e/test-component/scenarios/embedding-sdk/styles-tests.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/styles-tests.cy.spec.tsx
@@ -8,7 +8,7 @@ import {
 import { ORDERS_QUESTION_ID } from "e2e/support/cypress_sample_instance_data";
 import { describeEE, modal, updateSetting } from "e2e/support/helpers";
 import {
-  DEFAULT_SDK_PROVIDER_CONFIG,
+  DEFAULT_SDK_AUTH_PROVIDER_CONFIG,
   sdkJwtSignIn,
   signInAsAdminAndEnableEmbeddingSdkForComponentTests,
 } from "e2e/support/helpers/component-testing-sdk";
@@ -36,7 +36,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
             <h1>This is outside of the provider</h1>
           </div>
 
-          <MetabaseProvider config={DEFAULT_SDK_PROVIDER_CONFIG}>
+          <MetabaseProvider authConfig={DEFAULT_SDK_AUTH_PROVIDER_CONFIG}>
             <div style={{ border: "1px solid black" }}>
               <h1>This is inside of the provider</h1>
             </div>
@@ -76,7 +76,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
           </div>
 
           <MetabaseProvider
-            config={{
+            authConfig={{
               apiKey: "TEST",
               metabaseInstanceUrl: "http://fake-host:1234",
             }}
@@ -118,7 +118,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
     it("should use the font from the theme if set", () => {
       cy.mount(
         <MetabaseProvider
-          config={DEFAULT_SDK_PROVIDER_CONFIG}
+          authConfig={DEFAULT_SDK_AUTH_PROVIDER_CONFIG}
           theme={{ fontFamily: "Impact" }}
         >
           <StaticQuestion questionId={ORDERS_QUESTION_ID} />
@@ -148,7 +148,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
             <h1>This is outside of the provider</h1>
           </div>
 
-          <MetabaseProvider config={DEFAULT_SDK_PROVIDER_CONFIG}>
+          <MetabaseProvider authConfig={DEFAULT_SDK_AUTH_PROVIDER_CONFIG}>
             <div style={{ border: "1px solid black" }}>
               <h1>This is inside of the provider</h1>
             </div>
@@ -195,7 +195,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
             <h1>This is outside of the provider</h1>
           </div>
 
-          <MetabaseProvider config={DEFAULT_SDK_PROVIDER_CONFIG}>
+          <MetabaseProvider authConfig={DEFAULT_SDK_AUTH_PROVIDER_CONFIG}>
             <div style={{ border: "1px solid black" }}>
               <h1>This is inside of the provider</h1>
             </div>
@@ -223,7 +223,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
     it("legacy WindowModal modals should render with our styles", () => {
       // this test renders a create dashboard modal that, at this time, is using the legacy WindowModal
       cy.mount(
-        <MetabaseProvider config={DEFAULT_SDK_PROVIDER_CONFIG}>
+        <MetabaseProvider authConfig={DEFAULT_SDK_AUTH_PROVIDER_CONFIG}>
           <CreateDashboardModal />
         </MetabaseProvider>,
       );
@@ -238,7 +238,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
 
     it("mantine modals should render with our styles", () => {
       cy.mount(
-        <MetabaseProvider config={DEFAULT_SDK_PROVIDER_CONFIG}>
+        <MetabaseProvider authConfig={DEFAULT_SDK_AUTH_PROVIDER_CONFIG}>
           <InteractiveQuestion questionId={ORDERS_QUESTION_ID} />
         </MetabaseProvider>,
       );
@@ -288,7 +288,7 @@ describeEE("scenarios > embedding-sdk > styles", () => {
       cy.mount(
         <div>
           {elements.map(({ jsx }) => jsx)}
-          <MetabaseProvider config={DEFAULT_SDK_PROVIDER_CONFIG}>
+          <MetabaseProvider authConfig={DEFAULT_SDK_AUTH_PROVIDER_CONFIG}>
             <StaticQuestion questionId={ORDERS_QUESTION_ID} />
           </MetabaseProvider>
         </div>,
--- enterprise/frontend/src/embedding-sdk/components/public/InteractiveQuestion/InteractiveQuestion.unit.spec.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/InteractiveQuestion/InteractiveQuestion.unit.spec.tsx
@@ -114,7 +114,7 @@ const setup = ({
     {
       mode: "sdk",
       sdkProviderProps: {
-        config: createMockAuthProviderUriConfig({
+        authConfig: createMockAuthProviderUriConfig({
           authProviderUri: "http://TEST_URI/sso/metabase",
         }),
       },
