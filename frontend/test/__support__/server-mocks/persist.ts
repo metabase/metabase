4f9bcfa293ec2e177be1ec75b69b7f1960688cec
Kamil Mielnik <kamil@kamilmielnik.com>
2024-12-18 20:47:10 +0700

    Update unit tests

--- frontend/test/__support__/server-mocks/persist.ts
+++ frontend/test/__support__/server-mocks/persist.ts
@@ -3,14 +3,23 @@ import fetchMock from "fetch-mock";
 import type { ModelCacheRefreshStatus } from "metabase-types/api";
 
 export function setupModelPersistenceEndpoints(
-  persistedModel: ModelCacheRefreshStatus,
+  persistedModels: ModelCacheRefreshStatus[],
 ) {
-  fetchMock.get(
-    `path:/api/persist/card/${persistedModel.card_id}`,
-    persistedModel,
-  );
-  fetchMock.post(
-    `path:/api/card/${persistedModel.card_id}/refresh`,
-    persistedModel,
-  );
+  fetchMock.get(`path:/api/persist`, {
+    data: persistedModels,
+    limit: 20,
+    offset: 0,
+    total: persistedModels.length,
+  });
+
+  for (const persistedModel of persistedModels) {
+    fetchMock.get(
+      `path:/api/persist/card/${persistedModel.card_id}`,
+      persistedModel,
+    );
+    fetchMock.post(
+      `path:/api/card/${persistedModel.card_id}/refresh`,
+      persistedModel,
+    );
+  }
 }
