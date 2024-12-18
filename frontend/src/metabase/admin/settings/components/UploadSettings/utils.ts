07fb6000d6b24b5bd6cdae726fe8d17ecdfebbad
Kamil Mielnik <kamil@kamilmielnik.com>
2024-12-18 20:32:43 +0700

    Improve typing

--- frontend/src/metabase/admin/settings/components/UploadSettings/utils.ts
+++ frontend/src/metabase/admin/settings/components/UploadSettings/utils.ts
@@ -1,9 +1,10 @@
 import type Database from "metabase-lib/v1/metadata/Database";
+import type { SchemaName } from "metabase-types/api";
 
 export const getDatabaseOptions = (databases: Database[]) =>
   databases.map(db => ({ name: db.name, value: db.id }));
 
-export const getSchemaOptions = (schemas: string[]) =>
+export const getSchemaOptions = (schemas: SchemaName[]) =>
   schemas.map(schema => ({ name: schema, value: schema }));
 
 export const dbHasSchema = (databases: Database[], dbId: number): boolean =>
