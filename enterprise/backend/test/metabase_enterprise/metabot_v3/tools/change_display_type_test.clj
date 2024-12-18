50c17bac99b3729494dfa14fab24ff418067feac
John Swanson <john.swanson@metabase.com>
2024-12-18 12:23:52 -0800

    green CI? (#51459)
    * green CI?
    
    * don't swap in test context for dummy tools
    
    * convert non-string tool responses to json
    
    * CR feedback and fix circular dep
    
    * clean up reaction handling in envelope
    
    * fix another test

--- enterprise/backend/test/metabase_enterprise/metabot_v3/tools/change_display_type_test.clj
+++
@@ -1,9 +0,0 @@
-(ns metabase-enterprise.metabot-v3.tools.change-display-type-test
-  (:require [clojure.test :refer :all]
-            [metabase-enterprise.metabot-v3.tools.interface :as tools.interface]))
-
-(deftest it-generates-a-reaction
-  (is (= {:output "success",
-          :reactions [{:display-type "foo"
-                       :type :metabot.reaction/change-display-type}]}
-         (tools.interface/*invoke-tool* :metabot.tool/change-display-type {:type "foo"} {}))))
