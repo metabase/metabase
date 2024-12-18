6d4b695b5f845a08dfa84450ed055da975feaf3b
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2024-12-18 11:47:11 -0500

    add percentages to sankey tooltip (#51193)
    * add percentages to sankey tooltip
    
    * another tooltip variant
    
    * specs
    
    * lint

--- e2e/test/scenarios/visualizations-charts/sankey.cy.spec.js
+++ e2e/test/scenarios/visualizations-charts/sankey.cy.spec.js
@@ -85,27 +85,48 @@ describe("scenarios > visualizations > sankey", () => {
     // Ensure it shows compact labels
     H.echartsContainer().findByText("60.0k");
 
-    // Ensure tooltip shows correct values
-    H.sankeyEdge("#81898e").eq(0).realHover();
+    // Ensure tooltip shows correct values for edges
+    H.sankeyEdge("#81898e").eq(8).realHover();
     H.assertEChartsTooltip({
-      header: "Social Media → Landing Page",
+      header: "Onboarding → Active Users",
       rows: [
         {
-          name: "METRIC",
-          value: "30,000",
+          color: "#F7C41F",
+
+          name: "Active Users",
+          value: "25,000",
+          secondaryValue: "83.33 %",
+        },
+        {
+          color: "#F2A86F",
+          name: "Churned After Onboarding",
+          value: "5,000",
+          secondaryValue: "16.67 %",
         },
       ],
+      footer: { name: "Total", value: "30,000", secondaryValue: "100 %" },
     });
 
-    H.chartPathWithFillColor("#509EE3").realHover();
+    // Ensure tooltip shows correct values for nodes
+    H.chartPathWithFillColor("#E75454").realHover();
     H.assertEChartsTooltip({
-      header: "Social Media",
+      header: "Onboarding",
       rows: [
         {
-          name: "METRIC",
-          value: "30,000",
+          color: "#F7C41F",
+          name: "Active Users",
+          value: "25,000",
+          secondaryValue: "83.33 %",
+        },
+        {
+          color: "#F2A86F",
+          name: "Churned After Onboarding",
+          value: "5,000",
+          secondaryValue: "16.67 %",
         },
       ],
+      footer: { name: "Total", value: "30,000", secondaryValue: "100 %" },
+      blurAfter: true,
     });
 
     // Ensure saving the question works
