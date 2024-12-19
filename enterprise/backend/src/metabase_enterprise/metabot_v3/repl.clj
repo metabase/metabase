83dbe5eace7330d7333ef7a042f2b3fffcfe5a2d
John Swanson <john.swanson@metabase.com>
2024-12-19 05:23:23 -0800

    QOL improvements for the REPL namespace

--- enterprise/backend/src/metabase_enterprise/metabot_v3/repl.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/repl.clj
@@ -36,33 +36,10 @@
   (doseq [reaction reactions]
     (handle-reaction reaction)))
 
-(defn- test-context
-  []
-  (let [test-query {:dataset_query {:database 1, :type "query", :query {:source-table 27}},
-                    :display "table",
-                    :visualization_settings {},
-                    :type "question"}]
-    {:user_is_viewing [{:type :dashboard
-                        :id 5
-                        :parameters []
-                        :is-embedded false}
-                       {:type :table
-                        :id 27}
-                       {:type :model
-                        :id 18}
-                       {:type :metric
-                        :id 135}
-                       {:type :report
-                        :id 43}
-                       {:type :adhoc
-                        :query test-query}]}))
-
 (defn user-repl
   "REPL for interacting with MetaBot."
-  ([]
-   (user-repl [] (str (random-uuid))))
 
-  ([history session-id]
+  ([history context session-id]
    (when-let [history' (try
                          (when-let [input (try
                                             #_{:clj-kondo/ignore [:discouraged-var]}
@@ -74,14 +51,14 @@
                            (println "🗨 " (u/colorize :blue input))
                            (when (and (not (#{"quit" "exit" "bye" "goodbye" "\\q"} input))
                                       (not (str/blank? input)))
-                             (let [env (api/request input (test-context) history session-id)]
+                             (let [env (api/request input context history session-id)]
                                (handle-reactions (metabot-v3.envelope/reactions env))
                                (metabot-v3.envelope/history env))))
                          (catch Throwable e
                            #_{:clj-kondo/ignore [:discouraged-var]}
                            (println (u/pprint-to-str :red e))
                            history))]
-     (recur history' session-id))))
+     (recur history' context session-id))))
 
 (defn user-repl-cli
   "CLI entrypoint for using the MetaBot REPL.
@@ -91,7 +68,7 @@
   (mdb/setup-db! :create-sample-content? false)
   #_{:clj-kondo/ignore [:discouraged-var]}
   (println "Starting MetaBot REPL... 🤖")
-  (user-repl)
+  (user-repl [] {} (str (random-uuid)))
   (System/exit 0))
 
 (comment
@@ -100,4 +77,50 @@
             metabase.api.common/*current-user-id* 2
             metabase.api.common/*is-superuser?* true]
     (user-repl))
-  -)
+
+  (defn- test-context
+    []
+    (let [test-query {:dataset_query {:database 1, :type "query", :query {:source-table 27}},
+                      :display "table",
+                      :visualization_settings {},
+                      :type "question"}]
+      {:user_is_viewing [{:type :dashboard
+                          :id 5
+                          :parameters []
+                          :is-embedded false}
+                         {:type :table
+                          :id 27}
+                         {:type :model
+                          :id 18}
+                         {:type :metric
+                          :id 135}
+                         {:type :report
+                          :id 43}
+                         {:type :adhoc
+                          :query test-query}]}))
+
+  (defn- user-repl-with-context [context]
+    (user-repl [] context (str (random-uuid))))
+
+  (require '[metabase.test :as mt])
+
+  ;; Discounted orders only
+  (mt/with-test-user :crowberto
+    (user-repl-with-context
+     {:user-is-viewing [{:type :table :ref 5}]}))
+
+  ;; subscribe me to this dashboard every week on monday
+  (mt/with-test-user :crowberto
+    (user-repl-with-context
+     {:user-is-viewing [{:type :dashboard :ref 11}]}))
+
+  ;; filter on the last 6 months
+  ;; actually, only filter for current year
+  (mt/with-test-user :crowberto
+    (user-repl-with-context
+     {:user-is-viewing [{:type :report :ref 111}]}))
+
+  ;; breakout by plan
+  (mt/with-test-user :crowberto
+    (user-repl-with-context
+     {:user-is-viewing [{:type :metric :ref 122}]})))
