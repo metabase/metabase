973a8c522b0e4be7e772af9117c3a77ec856405b
John Swanson <john.swanson@metabase.com>
2024-12-19 11:25:30 -0800

    Adjust response format for `invite-user`

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/invite_user.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/invite_user.clj
@@ -7,17 +7,8 @@
 (mu/defmethod metabot-v3.tools.interface/*invoke-tool* :metabot.tool/invite-user
   [_tool-name {:keys [email]} _env]
   (let [output (try
-                 (let [{:keys [id first_name last_name email]} (api.user/invite-user {:email email})]
-                   {:id id
-                    :name (cond
-                            (and (nil? first_name) (nil? last_name)) email
-
-                            (or (nil? first_name) (nil? last_name))
-                            (str first_name last_name)
-
-                            :else
-                            (str first_name " " last_name))
-                    :email_address email})
+                 (api.user/invite-user {:email email})
+                 (format "%s has been invited to metabase" email)
                  (catch Exception e
                    {:error (ex-message e)}))]
     {:output output}))
