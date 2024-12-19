18227640bedb99a08e5c5e49da7a9682371c362f
William Zimrin <william@metabase.com>
2024-12-19 16:18:06 -0600

    athena doesn't run uuid tests

--- test/metabase/query_processor_test/uuid_test.clj
+++ test/metabase/query_processor_test/uuid_test.clj
@@ -2,10 +2,20 @@
   "Tests for queries involving uuids."
   (:require
    [clojure.test :refer :all]
+   [metabase.driver :as driver]
    [metabase.test :as mt]))
 
+(defmethod driver/database-supports? [::driver/driver ::uuids-in-create-table-statements]
+  [_driver _feature _database]
+  true)
+
+;; Athena supports uuids in queries, but not in create table
+(defmethod driver/database-supports? [:athena ::uuids-in-create-table-statements]
+  [_driver _feature _database]
+  false)
+
 (deftest ^:parallel simple-uuid-query-test
-  (mt/test-drivers (mt/normal-drivers-with-feature :uuid-type)
+  (mt/test-drivers (mt/normal-drivers-with-feature :uuid-type ::uuids-in-create-table-statements)
     (testing "Simple query"
       (mt/dataset uuid-dogs
         (is (= [[#uuid "d6b02fa2-bf7b-4b32-80d5-060b649c9859" "Tim"]]
@@ -16,7 +26,7 @@
                             "d6b02fa2-bf7b-4b32-80d5-060b649c9859"]}))))))))
 
 (deftest ^:parallel joined-uuid-query-test
-  (mt/test-drivers (mt/normal-drivers-with-feature :uuid-type)
+  (mt/test-drivers (mt/normal-drivers-with-feature :uuid-type ::uuids-in-create-table-statements)
     (testing "Query with joins"
       (mt/dataset uuid-dogs
         (is (= [[#uuid "d6b02fa2-bf7b-4b32-80d5-060b649c9859" "Tim"]]
