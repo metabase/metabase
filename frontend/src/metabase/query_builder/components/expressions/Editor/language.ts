cb018a9fab5032fca651b36624ef52ad1eb400bd
Romeo Van Snick <romeo@romeovansnick.be>
2024-12-19 15:26:38 +1100

    Add linter to custom expression language

--- frontend/src/metabase/query_builder/components/expressions/Editor/language.ts
+++ frontend/src/metabase/query_builder/components/expressions/Editor/language.ts
@@ -1,5 +1,10 @@
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
+import { type Diagnostic, linter } from "@codemirror/lint";
+import type { EditorView } from "@codemirror/view";
 import { styleTags, tags as t } from "@lezer/highlight";
+
+import type * as Lib from "metabase-lib";
+import { diagnose } from "metabase-lib/v1/expressions/diagnostics";
 
 import { parser } from "./parser";
 
@@ -28,6 +33,35 @@ const expressionLanguage = LRLanguage.define({
   languageData: {},
 });
 
-export function customExpression() {
-  return new LanguageSupport(expressionLanguage, []);
+type LintOptions = {
+  startRule: "expression" | "aggregation" | "boolean";
+  query: Lib.Query;
+  stageIndex: number;
+  name?: string | null;
+  expressionIndex: number | undefined;
+};
+
+const lint = (options: LintOptions) =>
+  linter((view: EditorView): Diagnostic[] => {
+    const source = view.state.doc.toString();
+    const error = diagnose({
+      source,
+      ...options,
+    });
+    if (!error || error.pos == null || error.len == null) {
+      return [];
+    }
+
+    return [
+      {
+        from: error.pos,
+        to: error.pos + error.len,
+        severity: "error",
+        message: error.message,
+      },
+    ];
+  });
+
+export function customExpression(options: LintOptions) {
+  return new LanguageSupport(expressionLanguage, [lint(options)]);
 }
