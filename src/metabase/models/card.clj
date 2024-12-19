4e451b4692b34f37a84c8182ecdc3ef7c041cd0b
John Swanson <john.swanson@metabase.com>
2024-12-19 04:12:43 -0800

    fix error copy when trying to move card->dash (#51233)

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -493,8 +493,7 @@
                                 (into {}))]
     (when (and (:dashboard_id changes) (seq dashboard-id->name))
       (throw (ex-info
-              (tru "Cannot convert to dashboard question: appears in other dashboards ({0})"
-                   (str/join "," (vals dashboard-id->name)))
+              (tru "Can't move question into dashboard. Questions saved in dashboards can't appear in other dashboards.")
               {:status-code 400
                :other-dashboards dashboard-id->name}))))
   (when-let [reason (invalid-dashboard-internal-card-update-reason? card changes)]
