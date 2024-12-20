567788ad60bed2465f5b08f1ec1c4a0985f56550
Nathan Voxland <nathan@metabase.com>
2024-12-20 15:28:12 -0600

    Updated remove_encryption command for changed code and added test

--- src/metabase/cmd/remove_encryption.clj
+++ src/metabase/cmd/remove_encryption.clj
@@ -8,6 +8,7 @@
    The current encryption key must be set in the `MB_ENCRYPTION_SECRET_KEY` env var."
   []
   (when-not (mdb/db-is-set-up?)
-    (throw (Exception. "Not database configured")))
+    (log/warnf "Database not found. Metabase will create a new database at %s and proceed encrypting." "2")
+    (mdb/setup-db! :create-sample-content? true))
   (log/infof "Connected to: %s | %s" (mdb/db-type) (mdb/db-file))
   (mdb/encrypt-db (mdb/db-type) (mdb/data-source) ""))
