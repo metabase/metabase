6f6526c338d871ccac4f3e0613848bc8de45ff11
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2024-12-20 11:34:49 -0500

    fix(sdk): add supports for xx-YY locales in embed/public-links and sdk (#51002) (#51572)
    * fix: add supports for xx-YY locales in embed/public-links
    
    * Generate locales.json
    
    * [WIP] Read locales.json
    
    * wip FE implementation
    
    * refactor + unit and sdk component tests
    
    * fixes after the merge, plus tests and cleand up logic
    
    * fix file name in string
    
    * change FE logic to use locales from the instance settings
    
    * revert json generation
    
    * remove leftover comment
    
    * add comment about the code to keep in sync
    
    * fix kondo error
    
    
    * fix unit test
    
    * fix more jest tests
    
    ---------
    
    Co-authored-by: Nicolò Pretto <info@npretto.com>
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>

--- frontend/src/metabase/public/LocaleProvider.tsx
+++ frontend/src/metabase/public/LocaleProvider.tsx
@@ -1,29 +1,92 @@
 import { type PropsWithChildren, useEffect, useState } from "react";
 
+import { useSetting } from "metabase/common/hooks";
 import { setLocaleHeader } from "metabase/lib/api";
 import { loadLocalization, setUserLocale } from "metabase/lib/i18n";
+
+interface LocaleProviderProps {
+  locale?: string | null;
+  shouldWaitForLocale?: boolean;
+}
 
 export const LocaleProvider = ({
   children,
   locale,
-}: PropsWithChildren<{ locale?: string | null }>) => {
-  // The state is not used explicitly, but we need to trigger a re-render when the locale changes
-  // as changing the locale in ttag doesn't trigger react components to update
-  const [_isLoadingLocale, setIsLoadingLocale] = useState(false);
+  shouldWaitForLocale,
+}: PropsWithChildren<LocaleProviderProps>) => {
+  const shouldLoadLocale = Boolean(locale);
+  const [isLocaleLoading, setIsLocaleLoading] = useState(shouldLoadLocale);
+
+  const availableLocalesData = useSetting("available-locales");
 
   useEffect(() => {
-    if (locale) {
-      setIsLoadingLocale(true);
-      setLocaleHeader(locale);
-      loadLocalization(locale).then(translatedObject => {
-        setIsLoadingLocale(false);
-        setUserLocale(translatedObject);
-      });
+    if (shouldLoadLocale && availableLocalesData) {
+      const localeToLoad = getLocaleToUse(
+        locale ?? null,
+        availableLocalesData.map(([locale]) => locale) ?? [],
+      );
+
+      setLocaleHeader(localeToLoad);
+      loadLocalization(localeToLoad)
+        .then(translatedObject => {
+          setIsLocaleLoading(false);
+          setUserLocale(translatedObject);
+        })
+        .catch(() => {
+          setIsLocaleLoading(false);
+        });
     }
-  }, [locale]);
+  }, [locale, shouldLoadLocale, availableLocalesData]);
+
+  if (shouldWaitForLocale && isLocaleLoading) {
+    return null;
+  }
 
   // note: we may show a loader here while loading, this would prevent race
   // conditions and things being rendered for some time with the wrong locale
   // downside is that it would make the initial load slower
-  return <>{children}</>;
+  return children;
+};
+
+// Re-implementation of the fallback logic of the backend.
+// Should be kept in sync with `fallback-locale` in src/metabase/util/i18n/impl.clj
+export const getLocaleToUse = (
+  // locale is in the format `en-US` or `zh-TW`
+  locale: string | null,
+  // availableLocales is an array of strings in the format `en` or `zh_TW`
+  // note: the backend uses `_` instead of `-`
+  availableLocales: string[],
+) => {
+  if (!locale) {
+    return "en";
+  }
+  const [language, country] = locale.split("-");
+
+  const normalizedLanguage = language.toLowerCase();
+  const normalizedCountry = country?.toUpperCase();
+
+  const normalizedLocale = country
+    ? `${normalizedLanguage}_${normalizedCountry}`
+    : normalizedLanguage;
+
+  // given a locale aa_BB
+
+  // return the locale aa_BB if it's available
+  if (availableLocales.includes(normalizedLocale)) {
+    return normalizedLocale;
+  }
+
+  // return the `aa` if it's available
+  if (availableLocales.includes(normalizedLanguage)) {
+    return normalizedLanguage;
+  }
+
+  // fallback to the first `aa-XX` locale found, if any
+  for (const availableLocale of availableLocales) {
+    if (availableLocale.startsWith(normalizedLanguage)) {
+      return availableLocale;
+    }
+  }
+
+  return "en";
 };
