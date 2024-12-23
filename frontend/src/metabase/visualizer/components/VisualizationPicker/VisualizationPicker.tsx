99f8a7ddc6638bb5f5a44db67413f26512d1d4d2
Anton Kulyk <kuliks.anton@gmail.com>
2024-12-23 16:52:53 +0000

    Update viz type picker

--- frontend/src/metabase/visualizer/components/VisualizationPicker/VisualizationPicker.tsx
+++ frontend/src/metabase/visualizer/components/VisualizationPicker/VisualizationPicker.tsx
@@ -1,53 +1,94 @@
 import { useMemo } from "react";
+import { t } from "ttag";
+import _ from "underscore";
 
 import IconButtonWrapper from "metabase/components/IconButtonWrapper";
-import { Flex, Icon, type IconName, Menu, Text } from "metabase/ui";
+import { useSelector } from "metabase/lib/redux";
+import { Flex, Icon, Menu, Text } from "metabase/ui";
 import visualizations from "metabase/visualizations";
-import type { DatasetData, VisualizationDisplay } from "metabase-types/api";
+import { getVisualizerRawSeries } from "metabase/visualizer/selectors";
+import type { VisualizationDisplay } from "metabase-types/api";
+
+import S from "./VisualizationPicker.module.css";
 
 interface VisualizationPickerProps {
   value: VisualizationDisplay | null;
-  data?: DatasetData;
   onChange: (vizType: string) => void;
 }
 
 export function VisualizationPicker({
   value,
-  data,
   onChange,
 }: VisualizationPickerProps) {
+  const series = useSelector(getVisualizerRawSeries);
+
   const vizOptions = useMemo(() => {
+    const [mainSeries] = series ?? [];
+    const { data } = mainSeries ?? {};
     return Array.from(visualizations)
-      .filter(([, viz]) => {
-        if (viz.hidden) {
-          return false;
-        }
-        if (data && typeof viz.isSensible === "function") {
-          return viz.isSensible(data);
-        }
-        return true;
-      })
-      .map(([vizType, viz]) => ({
-        label: viz.uiName,
-        value: vizType,
-        icon: viz.iconName,
-      }));
-  }, [data]);
+      .filter(([, viz]) => !viz.hidden)
+      .map(([vizType, viz]) => {
+        return {
+          label: viz.uiName,
+          value: vizType,
+          icon: viz.iconName,
+          isSensible: Boolean(data && viz.isSensible?.(data)),
+        };
+      });
+  }, [series]);
+
+  const [sensibleOptions, nonsensibleOptions] = useMemo(
+    () => _.partition(vizOptions, option => option.isSensible),
+    [vizOptions],
+  );
+
+  const selectedOption = useMemo(
+    () => vizOptions.find(option => option.value === value),
+    [value, vizOptions],
+  );
 
   return (
     <Menu>
       <Menu.Target>
         <IconButtonWrapper style={{ marginRight: "4px" }}>
-          <Icon name={(value ?? "area") as IconName} />
+          <Icon name={selectedOption?.icon ?? "empty"} />
           <Icon name="chevrondown" size={8} />
         </IconButtonWrapper>
       </Menu.Target>
       <Menu.Dropdown>
-        {vizOptions.map(({ label, value, icon }) => (
-          <Menu.Item key={value} onClick={() => onChange(value)}>
+        {sensibleOptions.map(({ label, value, icon }) => (
+          <Menu.Item
+            key={value}
+            className={S.ListItem}
+            aria-selected={value === selectedOption?.value}
+            onClick={() => onChange(value)}
+          >
             <Flex align="center">
               <Icon name={icon} />
-              <Text ml="sm">{label}</Text>
+              <Text className={S.ListItemLabel} ml="sm">
+                {label}
+              </Text>
+            </Flex>
+          </Menu.Item>
+        ))}
+        {nonsensibleOptions.length > 0 && (
+          <>
+            <Menu.Divider />
+            <Menu.Label>{t`Other charts`}</Menu.Label>
+          </>
+        )}
+        {nonsensibleOptions.map(({ label, value, icon }) => (
+          <Menu.Item
+            key={value}
+            className={S.ListItem}
+            aria-selected={value === selectedOption?.value}
+            onClick={() => onChange(value)}
+          >
+            <Flex align="center">
+              <Icon name={icon} />
+              <Text className={S.ListItemLabel} ml="sm">
+                {label}
+              </Text>
             </Flex>
           </Menu.Item>
         ))}
