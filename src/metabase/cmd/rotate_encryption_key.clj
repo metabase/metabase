b8fc60752cd2108b08835391578890d6fbc731f5
Nathan Voxland <nathan@metabase.com>
2024-12-23 16:18:30 -0600

    Don't need "" vs nil check

--- src/metabase/cmd/rotate_encryption_key.clj
+++ src/metabase/cmd/rotate_encryption_key.clj
@@ -7,11 +7,10 @@
   "Rotate the current configured db using the current `MB_ENCRYPTION_SECRET_KEY` env var and `to-key` argument.
   An empty to-key will decrypt the database."
   [to-key]
-  (let [to-key (or to-key "")]
-    (when-not (mdb/db-is-set-up?)
-      (log/warnf "Database not found. Metabase will create a new database at %s and proceed encrypting." "2")
-      (mdb/setup-db! :create-sample-content? true))
-    (log/infof "Connected to: %s | %s" (mdb/db-type) (mdb/db-file))
-    (if (empty? to-key)
-      (mdb/decrypt-db (mdb/db-type) (mdb/data-source))
-      (mdb/encrypt-db (mdb/db-type) (mdb/data-source) to-key))))
+  (when-not (mdb/db-is-set-up?)
+    (log/warnf "Database not found. Metabase will create a new database at %s and proceed encrypting." "2")
+    (mdb/setup-db! :create-sample-content? true))
+  (log/infof "Connected to: %s | %s" (mdb/db-type) (mdb/db-file))
+  (if (empty? to-key)
+    (mdb/decrypt-db (mdb/db-type) (mdb/data-source))
+    (mdb/encrypt-db (mdb/db-type) (mdb/data-source) to-key)))
