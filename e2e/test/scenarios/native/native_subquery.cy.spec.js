6967228adf7a1cf28ceb4cf1bc168e1fe28137d5
Romeo Van Snick <romeo@romeovansnick.be>
2024-12-24 11:44:16 +1100

    Fix formatting

--- e2e/test/scenarios/native/native_subquery.cy.spec.js
+++ e2e/test/scenarios/native/native_subquery.cy.spec.js
@@ -78,234 +78,243 @@ describe("scenarios > question > native subquery", () => {
             cy.button("Move").click();
           });
 
-        H.openNativeEditor();
-        cy.reload(); // Refresh the state, so previously created questions need to be loaded again.
-        H.NativeEditor.focus().type(" {{#people");
+          H.openNativeEditor();
+          cy.reload(); // Refresh the state, so previously created questions need to be loaded again.
+          H.NativeEditor.focus().type(" {{#people");
 
           // Wait until another explicit autocomplete is triggered
           // (slightly longer than AUTOCOMPLETE_DEBOUNCE_DURATION)
           // See https://github.com/metabase/metabase/pull/20970
           cy.wait(1000);
 
-        H.NativeEditor.completions().within(() => {
-          H.NativeEditor.completion(`${questionId2}-a-`)
-            .should("be.visible")
-            .findByText("Model in Bobby Tables's Personal Collection")
-            .should("be.visible");
+          H.NativeEditor.completions().within(() => {
+            H.NativeEditor.completion(`${questionId2}-a-`)
+              .should("be.visible")
+              .findByText("Model in Bobby Tables's Personal Collection")
+              .should("be.visible");
 
-          H.NativeEditor.completion(`${questionId1}-a-`)
-            .should("be.visible")
-            .findByText("Question in Our analytics")
-            .should("be.visible");
+            H.NativeEditor.completion(`${questionId1}-a-`)
+              .should("be.visible")
+              .findByText("Question in Our analytics")
+              .should("be.visible");
+          });
         });
+      });
+
+      it("autocomplete should work for columns from referenced questions", () => {
+        // Create two saved questions, the first will be referenced in the query when it is opened, and the second will be added to the query after it is opened.
+        cy.createNativeQuestion({
+          name: "A People Question 1",
+          native: {
+            query: "SELECT id AS a_unique_column_name FROM PEOPLE",
+          },
+        }).then(({ body: { id: questionId1 } }) => {
+          cy.createNativeQuestion({
+            name: "A People Question 2",
+            native: {
+              query: "SELECT id AS another_unique_column_name FROM PEOPLE",
+            },
+          }).then(({ body: { id: questionId2 } }) => {
+            const tagID = `#${questionId1}`;
+
+            // create a question with a template tag
+            cy.createNativeQuestion({
+              name: "Count of People",
+              native: {
+                query: `select COUNT(*) from {{#${questionId1}}}`,
+                "template-tags": {
+                  [tagID]: {
+                    id: "10422a0f-292d-10a3-fd90-407cc9e3e20e",
+                    name: tagID,
+                    "display-name": tagID,
+                    type: "card",
+                    "card-id": questionId1,
+                  },
+                },
+              },
+            }).then(({ body: { id: questionId3 } }) => {
+              cy.wrap(questionId3).as("toplevelQuestionId");
+              cy.visit(`/question/${questionId3}`);
+
+              // Refresh the state, so previously created questions need to be loaded again.
+              cy.reload();
+              cy.findByText("Open Editor").click();
+              H.NativeEditor.type(" a_");
+
+              H.NativeEditor.completion("A_UNIQUE_COLUMN_NAME").should(
+                "be.visible",
+              );
+
+              // For some reason, typing `{{#${questionId2}}}` in one go isn't deterministic,
+              // so type it in two parts
+              H.NativeEditor.type(` {{#${questionId2}}}`);
+
+              // Again, typing in in one go doesn't always work
+              // so type it in two parts
+              H.NativeEditor.type(" another");
+
+              H.NativeEditor.completion("ANOTHER_UNIQUE_COLUMN_NAME").should(
+                "be.visible",
+              );
+            });
+          });
+        });
+      });
+
+      it("card reference tags should update when the name of the card changes", () => {
+        cy.createNativeQuestion({
+          name: "A People Question 1",
+          native: {
+            query: "SELECT id AS a_unique_column_name FROM PEOPLE",
+          },
+        }).then(({ body: { id: questionId1 } }) => {
+          cy.wrap(questionId1).as("questionId");
+          const tagID = `#${questionId1}`;
+          cy.createNativeQuestion({
+            name: "Count of People",
+            native: {
+              query: `select COUNT(*) from {{#${questionId1}}}`,
+              "template-tags": {
+                [tagID]: {
+                  id: "10422a0f-292d-10a3-fd90-407cc9e3e20e",
+                  name: tagID,
+                  "display-name": tagID,
+                  type: "card",
+                  "card-id": questionId1,
+                },
+              },
+            },
+          }).then(({ body: { id: questionId2 } }) => {
+            // check the original name is in the query
+            cy.visit(`/question/${questionId2}`);
+            cy.findByText("Open Editor").click();
+            cy.get("@questionId").then(questionId => {
+              H.NativeEditor.get()
+                .should("be.visible")
+                .and("contain", `{{#${questionId}-a-people-question-1}}`);
+            });
+
+            // change the name
+            cy.visit(`/question/${questionId1}`);
+            cy.findByText("A People Question 1").type(" changed");
+            // unfocus the input
+            cy.findByText("Open Editor").click();
+
+            // check the name has changed
+            cy.visit(`/question/${questionId2}`);
+            cy.findByText("Open Editor").click();
+            cy.get("@questionId").then(questionId => {
+              H.NativeEditor.get()
+                .should("be.visible")
+                .and(
+                  "contain",
+                  `{{#${questionId}-a-people-question-1-changed}}`,
+                );
+            });
+          });
+        });
+      });
+
+      it("should allow a user with no data access to execute a native subquery", () => {
+        // Create the initial SQL question and followup nested question
+        cy.createNativeQuestion({
+          name: "People in WA",
+          native: {
+            query: "select * from PEOPLE where STATE = 'WA'",
+          },
+        })
+          .then(response => {
+            cy.wrap(response.body.id).as("nestedQuestionId");
+            const tagID = `#${response.body.id}`;
+
+            cy.createNativeQuestion({
+              name: "Count of People in WA",
+              native: {
+                query: `select COUNT(*) from {{#${response.body.id}}}`,
+                "template-tags": {
+                  [tagID]: {
+                    id: "10422a0f-292d-10a3-fd90-407cc9e3e20e",
+                    name: tagID,
+                    "display-name": tagID,
+                    type: "card",
+                    "card-id": response.body.id,
+                  },
+                },
+              },
+            });
+          })
+          .then(response => {
+            cy.wrap(response.body.id).as("toplevelQuestionId");
+
+            cy.visit(`/question/${response.body.id}`);
+            cy.contains("41");
+          });
+
+        // Now sign in as a user w/no data access
+        cy.signIn("nodata");
+
+        // They should be able to access both questions
+        H.visitQuestion("@nestedQuestionId");
+        cy.findByTestId("question-row-count").should(
+          "have.text",
+          "Showing 41 rows",
+        );
+
+        H.visitQuestion("@toplevelQuestionId");
+        cy.get("#main-data-grid [data-testid=cell-data]").should(
+          "have.text",
+          "41",
+        );
+      });
+
+      it("should be able to reference a nested question (metabase#25988)", () => {
+        const questionDetails = {
+          name: "Nested GUI question",
+          query: {
+            "source-table": `card__${ORDERS_QUESTION_ID}`,
+            limit: 2,
+          },
+        };
+
+        H.createQuestion(questionDetails).then(
+          ({ body: { id: nestedQuestionId } }) => {
+            const tagID = `#${nestedQuestionId}`;
+            cy.intercept("GET", `/api/card/${nestedQuestionId}`).as(
+              "loadQuestion",
+            );
+
+            H.startNewNativeQuestion();
+            H.NativeEditor.focus().type(`SELECT * FROM {{${tagID}`);
+            cy.wait("@loadQuestion");
+            cy.findByTestId("sidebar-header-title").should(
+              "have.text",
+              questionDetails.name,
+            );
+
+            H.runNativeQuery();
+            cy.findAllByTestId("cell-data").should("contain", "37.65");
+          },
+        );
+      });
+
+      it("should be able to reference a saved native question that ends with a semicolon `;` (metabase#28218)", () => {
+        const questionDetails = {
+          name: "28218",
+          native: { query: "select 1;" }, // semicolon is important here
+        };
+
+        H.createNativeQuestion(questionDetails).then(
+          ({ body: { id: baseQuestionId } }) => {
+            const tagID = `#${baseQuestionId}`;
+
+            H.startNewNativeQuestion();
+            H.NativeEditor.focus().type(`SELECT * FROM {{${tagID}`);
+
+            H.runNativeQuery();
+            cy.findAllByTestId("cell-data").should("contain", "1");
+          },
+        );
       });
     },
   );
-
-  it("autocomplete should work for columns from referenced questions", () => {
-    // Create two saved questions, the first will be referenced in the query when it is opened, and the second will be added to the query after it is opened.
-    cy.createNativeQuestion({
-      name: "A People Question 1",
-      native: {
-        query: "SELECT id AS a_unique_column_name FROM PEOPLE",
-      },
-    }).then(({ body: { id: questionId1 } }) => {
-      cy.createNativeQuestion({
-        name: "A People Question 2",
-        native: {
-          query: "SELECT id AS another_unique_column_name FROM PEOPLE",
-        },
-      }).then(({ body: { id: questionId2 } }) => {
-        const tagID = `#${questionId1}`;
-
-        // create a question with a template tag
-        cy.createNativeQuestion({
-          name: "Count of People",
-          native: {
-            query: `select COUNT(*) from {{#${questionId1}}}`,
-            "template-tags": {
-              [tagID]: {
-                id: "10422a0f-292d-10a3-fd90-407cc9e3e20e",
-                name: tagID,
-                "display-name": tagID,
-                type: "card",
-                "card-id": questionId1,
-              },
-            },
-          },
-        }).then(({ body: { id: questionId3 } }) => {
-          cy.wrap(questionId3).as("toplevelQuestionId");
-          cy.visit(`/question/${questionId3}`);
-
-          // Refresh the state, so previously created questions need to be loaded again.
-          cy.reload();
-          cy.findByText("Open Editor").click();
-          H.NativeEditor.type(" a_");
-
-          H.NativeEditor.completion("A_UNIQUE_COLUMN_NAME").should(
-            "be.visible",
-          );
-
-          // For some reason, typing `{{#${questionId2}}}` in one go isn't deterministic,
-          // so type it in two parts
-          H.NativeEditor.type(` {{#${questionId2}}}`);
-
-          // Again, typing in in one go doesn't always work
-          // so type it in two parts
-          H.NativeEditor.type(" another");
-
-          H.NativeEditor.completion("ANOTHER_UNIQUE_COLUMN_NAME").should(
-            "be.visible",
-          );
-        });
-      });
-    });
-  });
-
-  it("card reference tags should update when the name of the card changes", () => {
-    cy.createNativeQuestion({
-      name: "A People Question 1",
-      native: {
-        query: "SELECT id AS a_unique_column_name FROM PEOPLE",
-      },
-    }).then(({ body: { id: questionId1 } }) => {
-      cy.wrap(questionId1).as("questionId");
-      const tagID = `#${questionId1}`;
-      cy.createNativeQuestion({
-        name: "Count of People",
-        native: {
-          query: `select COUNT(*) from {{#${questionId1}}}`,
-          "template-tags": {
-            [tagID]: {
-              id: "10422a0f-292d-10a3-fd90-407cc9e3e20e",
-              name: tagID,
-              "display-name": tagID,
-              type: "card",
-              "card-id": questionId1,
-            },
-          },
-        },
-      }).then(({ body: { id: questionId2 } }) => {
-        // check the original name is in the query
-        cy.visit(`/question/${questionId2}`);
-        cy.findByText("Open Editor").click();
-        cy.get("@questionId").then(questionId => {
-          H.NativeEditor.get()
-            .should("be.visible")
-            .and("contain", `{{#${questionId}-a-people-question-1}}`);
-        });
-
-        // change the name
-        cy.visit(`/question/${questionId1}`);
-        cy.findByText("A People Question 1").type(" changed");
-        // unfocus the input
-        cy.findByText("Open Editor").click();
-
-        // check the name has changed
-        cy.visit(`/question/${questionId2}`);
-        cy.findByText("Open Editor").click();
-        cy.get("@questionId").then(questionId => {
-          H.NativeEditor.get()
-            .should("be.visible")
-            .and("contain", `{{#${questionId}-a-people-question-1-changed}}`);
-        });
-      });
-    });
-  });
-
-  it("should allow a user with no data access to execute a native subquery", () => {
-    // Create the initial SQL question and followup nested question
-    cy.createNativeQuestion({
-      name: "People in WA",
-      native: {
-        query: "select * from PEOPLE where STATE = 'WA'",
-      },
-    })
-      .then(response => {
-        cy.wrap(response.body.id).as("nestedQuestionId");
-        const tagID = `#${response.body.id}`;
-
-        cy.createNativeQuestion({
-          name: "Count of People in WA",
-          native: {
-            query: `select COUNT(*) from {{#${response.body.id}}}`,
-            "template-tags": {
-              [tagID]: {
-                id: "10422a0f-292d-10a3-fd90-407cc9e3e20e",
-                name: tagID,
-                "display-name": tagID,
-                type: "card",
-                "card-id": response.body.id,
-              },
-            },
-          },
-        });
-      })
-      .then(response => {
-        cy.wrap(response.body.id).as("toplevelQuestionId");
-
-        cy.visit(`/question/${response.body.id}`);
-        cy.contains("41");
-      });
-
-    // Now sign in as a user w/no data access
-    cy.signIn("nodata");
-
-    // They should be able to access both questions
-    H.visitQuestion("@nestedQuestionId");
-    cy.findByTestId("question-row-count").should(
-      "have.text",
-      "Showing 41 rows",
-    );
-
-    H.visitQuestion("@toplevelQuestionId");
-    cy.get("#main-data-grid [data-testid=cell-data]").should("have.text", "41");
-  });
-
-  it("should be able to reference a nested question (metabase#25988)", () => {
-    const questionDetails = {
-      name: "Nested GUI question",
-      query: {
-        "source-table": `card__${ORDERS_QUESTION_ID}`,
-        limit: 2,
-      },
-    };
-
-    H.createQuestion(questionDetails).then(
-      ({ body: { id: nestedQuestionId } }) => {
-        const tagID = `#${nestedQuestionId}`;
-        cy.intercept("GET", `/api/card/${nestedQuestionId}`).as("loadQuestion");
-
-        H.startNewNativeQuestion();
-        H.NativeEditor.focus().type(`SELECT * FROM {{${tagID}`);
-        cy.wait("@loadQuestion");
-        cy.findByTestId("sidebar-header-title").should(
-          "have.text",
-          questionDetails.name,
-        );
-
-        H.runNativeQuery();
-        cy.findAllByTestId("cell-data").should("contain", "37.65");
-      },
-    );
-  });
-
-  it("should be able to reference a saved native question that ends with a semicolon `;` (metabase#28218)", () => {
-    const questionDetails = {
-      name: "28218",
-      native: { query: "select 1;" }, // semicolon is important here
-    };
-
-    H.createNativeQuestion(questionDetails).then(
-      ({ body: { id: baseQuestionId } }) => {
-        const tagID = `#${baseQuestionId}`;
-
-        H.startNewNativeQuestion();
-        H.NativeEditor.focus().type(`SELECT * FROM {{${tagID}`);
-
-        H.runNativeQuery();
-        cy.findAllByTestId("cell-data").should("contain", "1");
-      },
-    );
-  });
 });
