d7e0951dbb5606421fe289aa19592229037328e3
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-12-26 16:15:10 +0700

    Add a story to theme date filters missing dark theme

---
+++ frontend/src/metabase/querying/filters/components/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,175 @@
+import type { ComponentMeta, Story } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
+import cx from "classnames";
+import { merge } from "icepick";
+import type { ComponentProps } from "react";
+
+import { Popover } from "metabase/ui";
+
+import { DatePicker } from "./DatePicker";
+import StoryStyle from "./DatePicker.stories.module.css";
+
+export default {
+  title: "Parameters/DatePicker",
+  component: DatePicker,
+} as ComponentMeta<typeof DatePicker>;
+
+type CustomStoryProps = {
+  theme?: "light" | "dark";
+};
+const Template: Story<
+  ComponentProps<typeof DatePicker> & CustomStoryProps
+> = args => {
+  const isDarkTheme = args.theme === "dark";
+
+  return (
+    <Popover opened position="bottom-start" withinPortal={false}>
+      <Popover.Target>
+        <div></div>
+      </Popover.Target>
+      <Popover.Dropdown
+        className={cx({
+          [StoryStyle.darkTheme]: isDarkTheme,
+        })}
+      >
+        <DatePicker {...args} />
+      </Popover.Dropdown>
+    </Popover>
+  );
+};
+
+export const AllOptions = {
+  render: Template,
+  play: async ({ canvasElement }: { canvasElement: HTMLCanvasElement }) => {
+    const canvas = within(canvasElement);
+    const yesterday = canvas.getByRole("button", {
+      name: "Yesterday",
+    });
+    yesterday.classList.add("pseudo-hover");
+  },
+};
+export const AllOptionsDarkTheme = merge(AllOptions, {
+  args: {
+    theme: "dark",
+  },
+});
+
+export const Exclude = {
+  render: Template,
+  play: async ({ canvasElement }: { canvasElement: HTMLCanvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(
+      canvas.getByRole("button", {
+        name: "Exclude…",
+      }),
+    );
+  },
+};
+export const ExcludeDarkTheme = merge(Exclude, {
+  args: {
+    theme: "dark",
+  },
+});
+
+export const ExcludeDayOfWeek = {
+  render: Template,
+  args: {
+    value: {
+      type: "exclude",
+      operator: "!=",
+      unit: "day-of-week",
+      values: [3, 4, 5],
+    },
+  },
+};
+export const ExcludeDayOfWeekDarkTheme = merge(ExcludeDayOfWeek, {
+  args: {
+    theme: "dark",
+  },
+});
+
+export const RelativeCurrent = {
+  render: Template,
+  args: {
+    value: { type: "relative", unit: "day", value: "current" },
+  },
+  play: async ({ canvasElement }: { canvasElement: HTMLCanvasElement }) => {
+    const canvas = within(canvasElement);
+    const week = canvas.getByRole("button", {
+      name: "Week",
+    });
+    week.classList.add("pseudo-hover");
+    const next = canvas.getByRole("tab", {
+      name: "Next",
+    });
+    next.classList.add("pseudo-hover");
+  },
+};
+export const RelativeCurrentDarkTheme = merge(RelativeCurrent, {
+  args: {
+    theme: "dark",
+  },
+});
+
+export const RelativePrevious = {
+  render: Template,
+  args: {
+    value: { type: "relative", unit: "month", value: -12 },
+  },
+  play: async ({ canvasElement }: { canvasElement: HTMLCanvasElement }) => {
+    const canvas = within(canvasElement);
+    const next = canvas.getByRole("tab", {
+      name: "Next",
+    });
+    next.classList.add("pseudo-hover");
+  },
+};
+export const RelativePreviousDarkTheme = merge(RelativePrevious, {
+  args: {
+    theme: "dark",
+  },
+});
+
+export const RelativeNext = {
+  render: Template,
+  args: {
+    value: { type: "relative", unit: "day", value: 30 },
+  },
+  play: async ({ canvasElement }: { canvasElement: HTMLCanvasElement }) => {
+    const canvas = within(canvasElement);
+    const next = canvas.getByRole("tab", {
+      name: "Next",
+    });
+    next.classList.add("pseudo-hover");
+  },
+};
+export const RelativeNextDarkTheme = merge(RelativeNext, {
+  args: {
+    theme: "dark",
+  },
+});
+
+export const SpecificBetween = {
+  render: Template,
+  args: {
+    value: {
+      type: "specific",
+      operator: "between",
+      values: [
+        new Date("2024-11-11T17:00:00.000Z"),
+        new Date("2024-11-20T17:00:00.000Z"),
+      ],
+      hasTime: false,
+    },
+  },
+  play: async ({ canvasElement }: { canvasElement: HTMLCanvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole("button", { name: "Add time" }));
+  },
+};
+
+export const SpecificBetweenDarkTheme = merge(SpecificBetween, {
+  args: {
+    theme: "dark",
+  },
+});
