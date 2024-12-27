0e752135ba59e7b9ba02aaf431d00d09957f9070
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-12-27 19:56:13 +0700

    [debug] EditableDashboard stories

--- enterprise/frontend/src/embedding-sdk/components/public/InteractiveDashboard/EditableDashboard.stories.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/InteractiveDashboard/EditableDashboard.stories.tsx
@@ -7,7 +7,13 @@ import {
   type EditableDashboardProps,
 } from "./EditableDashboard";
 
-const DASHBOARD_ID = (window as any).DASHBOARD_ID || 1;
+const ENTITY_ID = "xBLdW9FsgRuB2HGhWiBa_";
+const ONE_TOO_MANY_ENTITY_ID = ENTITY_ID + "1";
+const WRONG_ENTITY_ID = ENTITY_ID.slice(0, -1) + "1";
+const NUMBER_ID = 1;
+const WRONG_NUMBER_ID = 99999999;
+
+const DASHBOARD_ID = ENTITY_ID;
 
 const darkTheme = getSdkStorybookDarkTheme();
 
@@ -16,6 +22,27 @@ export default {
   component: EditableDashboard,
   parameters: {
     layout: "fullscreen",
+  },
+  argTypes: {
+    dashboardId: {
+      options: [
+        ENTITY_ID,
+        ONE_TOO_MANY_ENTITY_ID,
+        WRONG_ENTITY_ID,
+        NUMBER_ID,
+        WRONG_NUMBER_ID,
+      ],
+      control: {
+        type: "select",
+        labels: {
+          [ENTITY_ID]: "Entity ID",
+          [ONE_TOO_MANY_ENTITY_ID]: "One Too Many Entity ID",
+          [WRONG_ENTITY_ID]: "Wrong Entity ID",
+          [NUMBER_ID]: "Number ID",
+          [WRONG_NUMBER_ID]: "Wrong Number ID",
+        },
+      },
+    },
   },
 };
 
