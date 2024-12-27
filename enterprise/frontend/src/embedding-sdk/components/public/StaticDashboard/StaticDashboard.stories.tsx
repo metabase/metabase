631ddfe92a1a9c09fc35ed18e36f0f5ce8e99430
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-12-27 19:48:47 +0700

    [debug] StaticDashboard stories

--- enterprise/frontend/src/embedding-sdk/components/public/StaticDashboard/StaticDashboard.stories.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/StaticDashboard/StaticDashboard.stories.tsx
@@ -5,7 +5,13 @@ import { CommonSdkStoryWrapper } from "embedding-sdk/test/CommonSdkStoryWrapper"
 
 import type { StaticDashboardProps } from "./StaticDashboard";
 
-const DASHBOARD_ID = (window as any).DASHBOARD_ID || "1";
+const ENTITY_ID = "xBLdW9FsgRuB2HGhWiBa_";
+const ONE_TOO_MANY_ENTITY_ID = ENTITY_ID + "1";
+const WRONG_ENTITY_ID = ENTITY_ID.slice(0, -1) + "1";
+const NUMBER_ID = 1;
+const WRONG_NUMBER_ID = 99999999;
+
+const DASHBOARD_ID = ENTITY_ID;
 
 export default {
   title: "EmbeddingSDK/StaticDashboard",
@@ -14,6 +20,27 @@ export default {
     layout: "fullscreen",
   },
   decorators: [CommonSdkStoryWrapper],
+  argTypes: {
+    dashboardId: {
+      options: [
+        ENTITY_ID,
+        ONE_TOO_MANY_ENTITY_ID,
+        WRONG_ENTITY_ID,
+        NUMBER_ID,
+        WRONG_NUMBER_ID,
+      ],
+      control: {
+        type: "select",
+        labels: {
+          [ENTITY_ID]: "Entity ID",
+          [ONE_TOO_MANY_ENTITY_ID]: "One Too Many Entity ID",
+          [WRONG_ENTITY_ID]: "Wrong Entity ID",
+          [NUMBER_ID]: "Number ID",
+          [WRONG_NUMBER_ID]: "Wrong Number ID",
+        },
+      },
+    },
+  },
 };
 
 const Template: StoryFn<StaticDashboardProps> = args => {
