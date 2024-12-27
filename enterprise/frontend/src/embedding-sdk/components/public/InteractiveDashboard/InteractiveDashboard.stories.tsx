b7523181747b252053d5596aa936bc7e6a196a3b
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-12-27 18:25:40 +0700

    [debug] InteractiveDashboard Stories

--- enterprise/frontend/src/embedding-sdk/components/public/InteractiveDashboard/InteractiveDashboard.stories.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/InteractiveDashboard/InteractiveDashboard.stories.tsx
@@ -9,7 +9,13 @@ import {
   type InteractiveDashboardProps,
 } from "./InteractiveDashboard";
 
-const DASHBOARD_ID = (window as any).DASHBOARD_ID || 1;
+const ENTITY_ID = "xBLdW9FsgRuB2HGhWiBa_";
+const ONE_TOO_MANY_ENTITY_ID = ENTITY_ID + "1";
+const WRONG_ENTITY_ID = ENTITY_ID.slice(0, -1) + "1";
+const NUMBER_ID = 1;
+const WRONG_NUMBER_ID = 99999999;
+
+const DASHBOARD_ID = ENTITY_ID;
 
 export default {
   title: "EmbeddingSDK/InteractiveDashboard",
@@ -18,6 +24,27 @@ export default {
     layout: "fullscreen",
   },
   decorators: [CommonSdkStoryWrapper],
+  argTypes: {
+    dashboardId: {
+      options: [
+        ENTITY_ID,
+        ONE_TOO_MANY_ENTITY_ID,
+        WRONG_ENTITY_ID,
+        NUMBER_ID,
+        WRONG_NUMBER_ID,
+      ],
+      control: {
+        type: "select",
+        labels: {
+          [ENTITY_ID]: "Entity ID",
+          [ONE_TOO_MANY_ENTITY_ID]: "One Too Many Entity ID",
+          [WRONG_ENTITY_ID]: "Wrong Entity ID",
+          [NUMBER_ID]: "Number ID",
+          [WRONG_NUMBER_ID]: "Wrong Number ID",
+        },
+      },
+    },
+  },
 };
 
 const Template: StoryFn<InteractiveDashboardProps> = args => {
