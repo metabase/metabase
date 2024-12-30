7181004e156b4b1b3bcc9a84f587f9f4633c1c50
Filip Hric <filip@filiphric.sk>
2024-12-30 14:06:39 +0100

    group helpers in UI

--- e2e/support/index.ts
+++ e2e/support/index.ts
@@ -1,4 +1,73 @@
 // H is for helpers 🤗
-import * as H from "./helpers";
+import * as OriginalHelpers from "./helpers";
 
-export { H };
+const formatArgs = (args: any[]) => {
+  try {
+    return args.length > 0
+      ? args
+          .map(arg =>
+            typeof arg === "object"
+              ? JSON.stringify(arg, null, 2)
+              : String(arg),
+          )
+          .join(", ")
+      : "";
+  } catch (e) {
+    return "Unable to stringify args";
+  }
+};
+
+// these helpers are not wrapped because they return modified mocha functions
+const UNWRAPPED_HELPERS = [
+  "describeEE",
+  "describeWithSnowplow",
+  "describeWithSnowplowEE",
+];
+
+const shouldWrapHelper = (key: string) => !UNWRAPPED_HELPERS.includes(key);
+
+// Create wrapped versions of all helper functions
+const wrappedHelpers = Object.entries(OriginalHelpers).reduce(
+  (acc, [key, helper]) => {
+    // not all helpers are functions, we need to check
+    // @ts-expect-error helpers are not typed
+    acc[key] =
+      typeof helper === "function" && shouldWrapHelper(key)
+        ? (...args: any[]) => {
+            return cy.then(() => {
+              const log = Cypress.log({
+                name: `H.${key}`,
+                message: formatArgs(args),
+                // @ts-expect-error groupStart api is not public and typed
+                groupStart: true,
+                consoleProps: () => ({
+                  Function: key,
+                  Arguments: args,
+                }),
+              });
+
+              const result = helper(...args);
+
+              // If the result is a Cypress chain, attach the log end
+              if (result && typeof result.then === "function") {
+                return result.then((value: any) => {
+                  // @ts-expect-error groupStart api is not public and typed
+                  log.endGroup();
+                  return value;
+                });
+              }
+
+              // @ts-expect-error groupStart api is not public and typed
+              log.endGroup();
+              return result;
+            });
+          }
+        : helper;
+
+    return acc;
+  },
+  {} as typeof OriginalHelpers,
+);
+
+// Export the wrapped version but maintain the same usage pattern
+export { wrappedHelpers as H };
