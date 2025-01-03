19feac8baa66e5acf127535eaabb3b95cbef99d2
Ngoc Khuat <qn.khuat@gmail.com>
2025-01-03 20:30:17 +0700

    clean up properly

--- src/metabase/notification/storage/disk.clj
+++ src/metabase/notification/storage/disk.clj
@@ -6,15 +6,9 @@
    [metabase.util.random :as random])
   (:import
    (java.io File)
-   (java.nio.file Files Path)
-   (java.time Instant Duration)
-   (java.util.concurrent Executors ScheduledFuture TimeUnit)))
+   (java.util.concurrent Executors ScheduledThreadPoolExecutor TimeUnit)))
 
 (set! *warn-on-reflection* true)
-
-(def ^:private ^:const expiry-minutes
-  "How long to keep files before deletion"
-  5)
 
 (def ^:private temp-dir
   (delay
@@ -25,52 +19,62 @@
       dir)))
 
 (def ^:private deletion-scheduler
-  (Executors/newScheduledThreadPool 1))
+  (delay
+    (Executors/newScheduledThreadPool 1)))
 
 (defn- schedule-deletion!
-  "Schedule file for deletion after expiry-minutes"
-  [^File file]
-  (.schedule deletion-scheduler
-             (fn []
-               (when (.exists file)
-                 (io/delete-file file true)))
-             expiry-minutes
-             TimeUnit/MINUTES))
+  "Schedule file for deletion after specified seconds."
+  [^File file seconds]
+  (.schedule ^ScheduledThreadPoolExecutor @deletion-scheduler
+             ^Callable (fn []
+                         (when (.exists file)
+                           (io/delete-file file true)))
+             ^Long seconds
+             TimeUnit/SECONDS))
 
-(deftype DiskStorage [^:volatile-mutable path]
+(defn- temp-file
+  []
+  (doto (File/createTempFile "notification-" ".edn" @temp-dir)
+    (.deleteOnExit)))
+
+(deftype DiskStorage [file]
   storage/NotificationStorage
   (store! [this data]
-    (let [file (doto (File/createTempFile "notification-" ".edn" @temp-dir)
-                 (.deleteOnExit))]
-      (set! path file)
-      (spit file (pr-str data))
-      this))
+    (spit file (pr-str data))
+    this)
 
   (retrieve [this]
-    (let [^File file (.path this)]
+    (let [^File file (.file this)]
       (when (.exists file)
         (read-string (slurp file)))))
 
   (cleanup! [this]
-    (let [^File file (.path this)]
+    (let [^File file (.file this)]
       (when (.exists file)
         (.delete file))))
 
   Object
   (toString [this]
-    (str "#<DiskStorage@" (.path this) ">")))
+    (str "#<DiskStorage@" (.file this) ">")))
 
 (defn to-disk-storage!
-  "Store data in a temporary file and return a reference to it"
-  [data]
-  (let [s (DiskStorage. nil)]
-    (storage/store! s data)
-    s))
+  "Store data in a temporary file and return a reference to it.
+  Optional expiry-seconds parameter specifies how long to keep the file before deletion (defaults to 5 minutes)"
+  ([]
+   (to-disk-storage! nil))
+  ([data]
+   (to-disk-storage! data (* 5 60)))
+  ([data expired-seconds]
+   (let [f (temp-file)
+         s (DiskStorage. f)]
+     (u/prog1 (storage/store! s data)
+       (-> f (schedule-deletion! expired-seconds))))))
 
 (comment
-  (store! {:foo "bar"})
-  #_@(.getPath (store! {:foo "bar"})))
+  (to-disk-storage! {:foo "bar"} 20))
 
-;; Ensure the scheduler is shut down when the JVM exits
-#_(.addShutdownHook (Runtime/getRuntime)
-                    (Thread. ^Runnable #(.shutdown deletion-scheduler)))
+(.addShutdownHook
+ (Runtime/getRuntime)
+ (Thread. ^Runnable (fn []
+                      (when @deletion-scheduler
+                        (.shutdown ^ScheduledThreadPoolExecutor @deletion-scheduler)))))
