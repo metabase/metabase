3cb81842c3e7528e406c6d629fcb1a273dc1d4e5
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-03 16:16:18 -0500

    Add picker

--- frontend/src/metabase/ui/components/inputs/QuarterPicker/QuarterPicker.tsx
+++ frontend/src/metabase/ui/components/inputs/QuarterPicker/QuarterPicker.tsx
@@ -1,37 +1,115 @@
-import { MonthPicker, type MonthPickerProps } from "@mantine/dates";
+import { Box, SimpleGrid } from "@mantine/core";
+import {
+  type DatePickerValue,
+  MonthPicker,
+  type MonthPickerProps,
+  PickerControl,
+} from "@mantine/dates";
 import { useUncontrolled } from "@mantine/hooks";
-import cx from "classnames";
 import dayjs from "dayjs";
 import { type Ref, forwardRef } from "react";
 
 import S from "./QuarterPicker.module.css";
 
+const QUARTERS = [1, 2, 3, 4];
+
 export type QuarterPickerProps = MonthPickerProps;
 
 export const QuarterPicker = forwardRef(function QuarterPicker(
-  { classNames, value: valueProp, onChange, ...props }: QuarterPickerProps,
+  {
+    value: valueProp,
+    defaultValue,
+    date: dateProp,
+    defaultDate,
+    level: levelProp,
+    defaultLevel,
+    onChange,
+    onDateChange,
+    onLevelChange,
+    ...props
+  }: QuarterPickerProps,
   ref: Ref<HTMLDivElement>,
 ) {
   const [value, setValue] = useUncontrolled({
     value: valueProp,
+    defaultValue,
     onChange,
   });
 
+  const [date, setDate] = useUncontrolled({
+    value: dateProp,
+    defaultValue: defaultDate,
+    finalValue: new Date(),
+    onChange: onDateChange,
+  });
+
+  const [level, setLevel] = useUncontrolled({
+    value: levelProp,
+    defaultValue: defaultLevel,
+    finalValue: "year" as const,
+    onChange: onLevelChange,
+  });
+
   return (
-    <MonthPicker
-      {...props}
-      ref={ref}
-      classNames={{
-        monthsList: cx(S.monthsList, classNames?.monthsList),
-        monthsListCell: cx(S.monthsListCell, classNames?.monthsListCell),
-      }}
-      value={value ? getStartOfQuarter(value) : null}
-      monthsListFormat="[Q]Q"
-      onChange={setValue}
-    />
+    <Box ref={ref} {...props}>
+      <MonthPicker
+        classNames={{
+          yearLevel: S.yearLevel,
+          monthsList: S.monthsList,
+        }}
+        value={value}
+        date={date}
+        level={level}
+        miw="11rem"
+        onChange={setValue}
+        onDateChange={setDate}
+        onLevelChange={setLevel}
+      />
+      {level === "year" && (
+        <SimpleGrid cols={2} spacing="sm">
+          {QUARTERS.map(quarter => (
+            <QuarterPickerControl
+              key={quarter}
+              value={value}
+              date={date}
+              quarter={quarter}
+              onChange={setValue}
+            />
+          ))}
+        </SimpleGrid>
+      )}
+    </Box>
   );
 });
 
-function getStartOfQuarter(value: Date) {
-  return dayjs(value).startOf("quarter").toDate();
+type QuarterPickerControlProps = {
+  value: DatePickerValue;
+  date: Date;
+  quarter: number;
+  onChange: (value: Date) => void;
+};
+
+function QuarterPickerControl({
+  value,
+  date,
+  quarter,
+  onChange,
+}: QuarterPickerControlProps) {
+  const isSelected =
+    value != null && isSameYearQuarter(value, date.getFullYear(), quarter);
+
+  const handleClick = () => {
+    onChange(dayjs(date).quarter(quarter).startOf("quarter").toDate());
+  };
+
+  return (
+    <PickerControl selected={isSelected} onClick={handleClick}>
+      Q{quarter}
+    </PickerControl>
+  );
+}
+
+function isSameYearQuarter(value: Date, year: number, quarter: number) {
+  const date = dayjs(value);
+  return date.year() === year && date.quarter() === quarter;
 }
