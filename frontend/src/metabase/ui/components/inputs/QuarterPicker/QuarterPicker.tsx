abbdd31838fb15c5fc50268e1fbd79866e3c444d
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-03 14:48:13 -0500

    Add quarter picker

--- frontend/src/metabase/ui/components/inputs/QuarterPicker/QuarterPicker.tsx
+++ frontend/src/metabase/ui/components/inputs/QuarterPicker/QuarterPicker.tsx
@@ -1,5 +1,7 @@
 import { MonthPicker, type MonthPickerProps } from "@mantine/dates";
+import { useUncontrolled } from "@mantine/hooks";
 import cx from "classnames";
+import dayjs from "dayjs";
 import { type Ref, forwardRef } from "react";
 
 import S from "./QuarterPicker.module.css";
@@ -7,20 +9,29 @@ import S from "./QuarterPicker.module.css";
 export type QuarterPickerProps = MonthPickerProps;
 
 export const QuarterPicker = forwardRef(function QuarterPicker(
-  { classNames, ...props }: QuarterPickerProps,
+  { classNames, value: valueProp, onChange, ...props }: QuarterPickerProps,
   ref: Ref<HTMLDivElement>,
 ) {
+  const [value, setValue] = useUncontrolled({
+    value: valueProp,
+    onChange,
+  });
+
   return (
     <MonthPicker
-      ref={ref}
       {...props}
+      ref={ref}
       classNames={{
         monthsList: cx(S.monthsList, classNames?.monthsList),
-        monthsListRow: cx(S.monthsListRow, classNames?.monthsListRow),
         monthsListCell: cx(S.monthsListCell, classNames?.monthsListCell),
-        pickerControl: cx(S.pickerControl, classNames?.pickerControl),
       }}
+      value={value ? getStartOfQuarter(value) : null}
       monthsListFormat="[Q]Q"
+      onChange={setValue}
     />
   );
 });
+
+function getStartOfQuarter(value: Date) {
+  return dayjs(value).startOf("quarter").toDate();
+}
