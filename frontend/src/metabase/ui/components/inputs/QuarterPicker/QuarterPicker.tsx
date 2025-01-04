20bd836fa4b73483b2f0370842e89c7e50e26a54
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-04 10:30:06 -0500

    Fix styles

--- frontend/src/metabase/ui/components/inputs/QuarterPicker/QuarterPicker.tsx
+++ frontend/src/metabase/ui/components/inputs/QuarterPicker/QuarterPicker.tsx
@@ -8,16 +8,25 @@ import { useUncontrolled } from "@mantine/hooks";
 import cx from "classnames";
 import dayjs from "dayjs";
 import { type Ref, forwardRef } from "react";
+import { c } from "ttag";
 
 import S from "./QuarterPicker.module.css";
 
-export type QuarterPickerProps = MonthPickerProps;
+export type QuarterPickerProps = Omit<
+  MonthPickerProps,
+  "monthsListFormat" | "getMonthControlProps"
+> & {
+  quarterListFormat?: string;
+  getQuarterControlProps?: (date: Date) => Partial<PickerControlProps>;
+};
 
 export const QuarterPicker = forwardRef(function QuarterPicker(
   {
     classNames,
     value: valueProp,
     defaultValue,
+    quarterListFormat = getQuarterFormat(),
+    getQuarterControlProps,
     onChange,
     ...props
   }: QuarterPickerProps,
@@ -39,15 +48,24 @@ export const QuarterPicker = forwardRef(function QuarterPicker(
         monthsListCell: cx(S.monthsListCell, classNames?.monthsListCell),
       }}
       value={getQuarterValue(value)}
-      monthsListFormat="[Q]Q"
+      monthsListFormat={quarterListFormat}
       onChange={setValue}
-      getMonthControlProps={getMonthControlProps}
+      getMonthControlProps={date => ({
+        ...getMonthControlProps(date),
+        ...getQuarterControlProps?.(date),
+      })}
     />
   );
 });
 
 function getQuarterValue(value: DatePickerValue): DatePickerValue {
   return value ? dayjs(value).startOf("quarter").toDate() : null;
+}
+
+function getQuarterFormat() {
+  return c(
+    'This is a "dayjs" format string (https://day.js.org/docs/en/plugin/advanced-format). It should include "Q" for the quarter number, and raw text can be escaped by brackets. For example, "[Quarter] Q" will be rendered as "Quarter 1".',
+  ).t`[Q]Q`;
 }
 
 function getMonthControlProps(date: Date): Partial<PickerControlProps> {
