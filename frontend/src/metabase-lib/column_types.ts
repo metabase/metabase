e692ee4a964eaafd34139b6b3a5bfd229737e05d
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-07 15:33:04 -0500

    Type checks

--- frontend/src/metabase-lib/column_types.ts
+++ frontend/src/metabase-lib/column_types.ts
@@ -11,21 +11,18 @@ export const isTemporal: TypeFn = TYPES.temporal_QMARK_;
 export const isDateOrDateTime: TypeFn = TYPES.date_or_datetime_QMARK_;
 export const isDateWithoutTime: TypeFn = TYPES.date_without_time_QMARK_;
 export const isInteger: TypeFn = TYPES.integer_QMARK_;
+export const isNumeric: TypeFn = TYPES.numeric_QMARK_;
 export const isString: TypeFn = TYPES.string_QMARK_;
 export const isStringLike: TypeFn = TYPES.string_like_QMARK_;
 export const isStringOrStringLike: TypeFn = TYPES.string_or_string_like_QMARK_;
 export const isTime: TypeFn = TYPES.time_QMARK_;
-
-// Legacy helpers that mix effective and semantic types.
-export const isCategory: TypeFn = TYPES.category_QMARK_;
-export const isNumber: TypeFn = TYPES.number_QMARK_;
-export const isNumeric: TypeFn = TYPES.numeric_QMARK_;
 
 // Semantic type checks. A semantic type can be assigned to a column with an
 // unrelated effective type. Do not imply any effective type when checking for a
 // semantic type.
 export const isAddress: TypeFn = TYPES.address_QMARK_;
 export const isAvatarURL: TypeFn = TYPES.avatar_URL_QMARK_;
+export const isCategory: TypeFn = TYPES.category_QMARK_;
 export const isCity: TypeFn = TYPES.city_QMARK_;
 export const isComment: TypeFn = TYPES.comment_QMARK_;
 export const isCoordinate: TypeFn = TYPES.coordinate_QMARK_;
