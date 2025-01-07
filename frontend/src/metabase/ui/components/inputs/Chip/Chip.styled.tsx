27083ac6aaadbde9278026d43535edbace6ede97
Nick Fitzpatrick <nickfitz.582@gmail.com>
2025-01-07 16:34:15 -0400

    initial story

--- frontend/src/metabase/ui/components/inputs/Chip/Chip.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/Chip/Chip.styled.tsx
@@ -5,32 +5,38 @@ export const getChipOverrides = (): MantineThemeOverride["components"] => ({
     defaultProps: {
       size: 14,
     },
-    variants: {
-      brand: (_theme, _props, context) => {
-        return {
-          iconWrapper: {
-            display: "none",
-          },
 
-          label: {
+    styles: (_theme, _props, context) => {
+      return {
+        iconWrapper: {
+          display: "none",
+        },
+        root: {
+          width: "fit-content",
+        },
+        label: {
+          backgroundColor: "var(--mb-color-brand-lighter)",
+          color: "var(--mb-color-brand)",
+          padding: "0.5rem 1rem ",
+          display: "block",
+          height: "auto",
+          lineHeight: `calc(${rem(context.size)})`,
+          fontWeight: 700,
+
+          "&:hover": {
             backgroundColor: "var(--mb-color-brand-light)",
-            color: "var(--mb-color-brand)",
-            padding: "0.5rem 1rem ",
-            display: "block",
-            height: "auto",
-            lineHeight: `calc(${rem(context.size)})`,
+          },
 
-            "&[data-checked=true]": {
-              backgroundColor: "var(--mb-color-brand)",
-              color: "white",
-              paddingInline: "1rem",
-            },
+          "&[data-checked=true]": {
+            backgroundColor: "var(--mb-color-brand)",
+            color: "white",
+            paddingInline: "1rem",
           },
-          input: {
-            display: "block",
-          },
-        };
-      },
+        },
+        input: {
+          display: "block",
+        },
+      };
     },
   },
 });
