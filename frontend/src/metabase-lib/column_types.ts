6b2f3fa65a2df17324361317faf5d490bbf58118
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-07 16:09:36 -0500

    Preserve behavior

--- frontend/src/metabase-lib/column_types.ts
+++ frontend/src/metabase-lib/column_types.ts
@@ -19,7 +19,7 @@ export const isTime: TypeFn = TYPES.time_QMARK_;
 // Checks for both effective and semantic types. This hack is required to
 // support numbers stored as strings in MySQL until there is a proper
 // coercion strategy. `isString` and `isNumeric` would be both `true` for such
-// columns; that's why `isNumeric` needs to be called first.
+// columns; that's why `isNumeric` needs to be called first. See #44431.
 export const isNumeric: TypeFn = TYPES.numeric_QMARK_;
 
 // Semantic type checks. A semantic type can be assigned to a column with an
--- src/metabase/lib/types/constants.cljc
+++ src/metabase/lib/types/constants.cljc
@@ -40,7 +40,7 @@
   {::temporal    {:effective-type [:type/Temporal]}
    ;; Checks for both effective and semantic types. This hack is required to
    ; support numbers stored as strings in MySQL until there is a proper
-   ; coercion strategy.
+   ; coercion strategy. See #44431.
    ::number      {:effective-type [:type/Number]
                   :semantic-type  [:type/Number]}
    ::integer     {:effective-type [:type/Integer]}
