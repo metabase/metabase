1e06aa7a64b1b20defc61b0bd34268b22afc8743
Bryan Maass <bryan.maass@gmail.com>
2025-01-08 13:39:27 -0700

    update add load examples

--- dev/src/dev/add_load.clj
+++ dev/src/dev/add_load.clj
@@ -5,7 +5,7 @@
 
 (def ^:private logical-kw
   "A keyword that starts with a question mark, used to track values created by from-script."
-  [:and :keyword [:fn #(= "?"(namespace %))]])
+  [:and :keyword [:fn #(= "?" (namespace %))]])
 
 (mu/defn- extract-bindings
   "Extracts the bindings from the script and inserts the values into the ids map. This is the data oriented equivalent
@@ -23,94 +23,87 @@
    (fn [x] (if-let [value (get ids x)] value x))
    attrs))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; PUBLIC API ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def ^:private BindingForm [:or [:= :?] logical-kw :map])
 
+(def ^:private NormalizedTuple [:tuple :keyword BindingForm :map])
 
-(mu/defn from-script
+(def ^:private Tuple [:or
+            [:tuple :keyword]
+            [:tuple :keyword BindingForm]
+            NormalizedTuple])
+
+(mu/defn- normalize-tuple :- NormalizedTuple
+  [next-kw-thunk :- fn? t :- Tuple]
+  (let [[model bindings attrs] t]
+    [model
+     (if (or (nil? bindings) (= :? bindings)) (next-kw-thunk) bindings)
+     (or attrs {})]))
+
+(mu/defn- from-script*
   "Takes a script and inserts the entities into the database. Returns a map of the ids of the entities inserted.
 
   The script is similar to the one used by [[mt/with-temp]], but instead of code to be executed, it contains the
   entities to be inserted, and values to be bound _as data_. The entities are inserted in the order they appear in the
-  script, and their bound values are saved in a map.
-
-  Example:
-
-  - Bind the coll-id, kind of like with-temp:
-  ```clojure
-  (from-script [[:model/Collection {:?/coll-id :id} {}]])
-  ;;=> {:coll-id 407}
-  ```
-
-    - Bind the entire collection:
-  ```clojure
-  (from-script [[:model/Collection :?/my-coll {}]])
-  ;;=> {:my-coll (toucan2.instance/instance
-                  :model/Collection
-                  {:authority_level nil,
-                   :description nil,
-                   :archived false,
-                   :slug \"matrfcjkeesgtgspbbej\",
-                   :archive_operation_id nil,
-                   :name \"MATRFCJKEESGTGSPBBEJ\",
-                   :personal_owner_id nil,
-                   :type nil,
-                   :is_sample false,
-                   :id 456,
-                   :archived_directly nil,
-                   :entity_id \"VYHXF-EitdLBqGkFUdl4s\",
-                   :location \"/\",
-                   :namespace nil,
-                   :created_at #t \"2024-08-06T15:26:27.515507Z\"})}
-  ```
-
-  - Insert a card and have it reference the collection:
-  ```clojure
-  (from-script
-    [[:model/Collection {:?/coll-id :id} {}]
-     [:model/Collection {:?/coll-id-two :id} {}]
-     [:model/Card {:?/card-id :id
-                   :?/entity_id :entity-id}]])
-  ;;=> {:coll-id 408, :coll-id-two 409, :card-id 9550, :entity-id \"BZiPJnAlnm69pUvP7rCXY\"}
-  ```
-
-  Because the script itself is just data, we can use it as an api for inserting generated load-testing data.
-
-  - Example with n cards that keep track of their card and entity ids.
-
-  ```clojure
-  (defn two-collections-and-n-cards [n]
-    (reduce into
-            [[:model/Collection {:?/coll-id :id} {}]
-             [:model/Collection {:?/coll-id-two :id} {}]]
-            (vec (for [i (range 1 (inc n))]
-                   [[:model/Card
-                     {(keyword \"?\" (str \"card-id-\" i)) :id
-                      (keyword \"?\" (str \"entity-id-\" i)) :entity_id}
-                     {:collection_id :?/coll-id}]]))))
-
-  (from-script (two-collections-and-n-cards 5))
-  ;;=>
-  {:coll-id 452
-   :coll-id-two 453
-
-   :card-id-1 36667
-   :entity-id-1 \"f7H5BjF5F2Y-doCek40-1\"
-   :card-id-2 36668
-   :entity-id-2 \"-1y3Rc3HmBomcVfzt7wyk\"
-   :card-id-3 36669
-   :entity-id-3 \"Xqj0Q3O0mNtCkIEq4Mr6b\"
-   :card-id-4 36670
-   :entity-id-4 \"Pd9ZsZNI0YbnEj8M4TOFp\"}
-  ```"
-  [script :- [:sequential [:tuple :keyword [:or :map logical-kw] :map]]]
-  (let [ids (atom {})
-        _ (add-watch ids ::logger (fn [_k _r _o n] (when (< 0.99 (rand))
-                                                     #_:clj-kondo/ignore
-                                                     (println "Inserted" (count n) "entities."))))]
+  script, and their bound values are saved in a map."
+  [script :- [:sequential Tuple]]
+  (let [ids (atom {})]
     (doseq [[modelable bindings attrs] script]
       (let [inserted (perm/with-perm modelable (fill-attrs @ids attrs))]
         (swap! ids merge (extract-bindings bindings inserted))
         inserted))
     (update-keys @ids (comp keyword name))))
+
+(defn- make-next-kw-thunk []
+  (let [kw-idx (atom 0)]
+    (fn [] (keyword "?" (str "_" (swap! kw-idx inc))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PUBLIC API ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(mu/defn from-script :- :map
+  "Takes a script and inserts the entities into the database. Returns a map of the ids of the entities inserted."
+  [script :- [:sequential Tuple]]
+  (->>
+   script
+   (mapv (partial normalize-tuple (make-next-kw-thunk)))
+   ;; TODO build a dag and insert in topological order, batched
+   ;; ((fn [x] (def normalized-tuples x) x))
+   from-script*))
+
+(comment
+  ;; ============================== Examples ==============================
+  ;; One random card
+  (from-script [[:model/Card]])
+
+  ;; ignore the return value:
+  (from-script [[:model/Card {}]])
+
+  ;; Two random cards
+  (from-script [[:model/Card] [:model/Card]])
+
+  ;; One random card but give it a name
+  (from-script [[:model/Card :? {:name "My Card 1"
+                                 :description "This is a card"}]])
+
+  ;; ignore the return value
+  (from-script [[:model/Card {} {:name "My Card 5"}]])
+
+  ;; One random card, given the key my-card
+  (from-script [[:model/Card :?/my-card {:name "My Card 2"}]])
+
+  ;; Desctructure card id
+  (from-script [[:model/Card {:?/card-id :id} {:name "My Card 4"}]])
+
+  ;; You can reuse ids from previous steps
+  (from-script [[:model/Card {:?/card-id :id} {:name "My Card (w/ dashboard)"}]
+                [:model/Dashboard {:?/dash-id :id} {:name "My Dashboard"}]
+                [:model/DashboardCard :? {:card_id :?/card-id :dashboard_id :?/dash-id}]
+                [:model/Card {:?/card-id :id} {:name "My Card (w/ dashboard)"}]
+                [:model/Dashboard {:?/dash-id :id} {:name "My Dashboard"}]
+                [:model/DashboardCard :? {:card_id :?/card-id :dashboard_id :?/dash-id}]
+                [:model/Card {:?/card-id :id} {:name "My Card (w/ dashboard)"}]
+                [:model/Dashboard {:?/dash-id :id} {:name "My Dashboard"}]
+                [:model/DashboardCard :? {:card_id :?/card-id :dashboard_id :?/dash-id}]])
+  )
