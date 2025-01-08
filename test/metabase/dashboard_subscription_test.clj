136bf4f9ba3fa077e094bff9dfc9f0e230938e6d
Ngoc Khuat <qn.khuat@gmail.com>
2025-01-08 15:53:41 +0700

    test fixes

--- test/metabase/dashboard_subscription_test.clj
+++ test/metabase/dashboard_subscription_test.clj
@@ -991,7 +991,7 @@
 (deftest dashboard-subscription-attachments-test
   (testing "Dashboard subscription attachments respect dashcard viz settings."
     (mt/with-fake-inbox
-      (notification.tu/with-notification-testing-setup
+      (notification.tu/with-notification-testing-setup!
         (mt/with-temp [:model/Card {card-id :id :as c} (pulse.test-util/checkins-query-card {:breakout [!day.date]})
                        :model/Dashboard {dash-id :id} {:name "just dash"}]
           (let [;; with the helper `metadata->field-ref` we turn column metadata into column field refs
