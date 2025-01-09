421ebe6242932c6721f7a8a34932574da524634b
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-01-09 11:42:02 -0500

    Remove schema from model details page

--- frontend/src/metabase/models/components/ModelDetailPage/ModelSchemaDetails/ModelSchemaDetails.styled.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const SchemaHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
