9f5f35e12e22d59c999c145f50962b3f42cdf3b4
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-10 10:03:41 -0500

    Add tests

--- frontend/src/metabase/querying/filters/utils/dates.unit.spec.ts
+++ frontend/src/metabase/querying/filters/utils/dates.unit.spec.ts
@@ -21,6 +21,22 @@ describe("getDateFilterClause", () => {
       value: { type: "relative", value: "current", unit: "day" },
       displayName: "Created At is today",
     },
+    {
+      value: { type: "month", year: 2024, month: 1 },
+      displayName: "Created At is Jan 1–31, 2024",
+    },
+    {
+      value: { type: "month", year: 2024, month: 12 },
+      displayName: "Created At is Dec 1–31, 2024",
+    },
+    {
+      value: { type: "quarter", year: 2020, quarter: 1 },
+      displayName: "Created At is Jan 1 – Mar 31, 2020",
+    },
+    {
+      value: { type: "quarter", year: 2020, quarter: 4 },
+      displayName: "Created At is Oct 1 – Dec 31, 2020",
+    },
   ])(
     "should convert a filter value to a filter clause",
     ({ value, displayName }) => {
