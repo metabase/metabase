f28a8b8bd8fb8fef083976d0c8ec14115f0c1dd8
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-10 19:48:09 -0500

    Remove unused code

--- frontend/src/metabase/parameters/utils/date-formatting.ts
+++ frontend/src/metabase/parameters/utils/date-formatting.ts
@@ -1,38 +1,5 @@
-import { t } from "ttag";
-
 import { formatDateFilter } from "metabase/querying/filters/utils/dates";
 import { deserializeDateFilter } from "metabase/querying/parameters/utils/dates";
-import {
-  DATE_OPERATORS,
-  generateTimeFilterValuesDescriptions,
-} from "metabase-lib/v1/queries/utils/query-time";
-
-function getFilterOperator(filter: any[] = []) {
-  return DATE_OPERATORS.find(op => op.test(filter as any));
-}
-
-const prefixedOperators = new Set([
-  "exclude",
-  "before",
-  "after",
-  "on",
-  "empty",
-  "not-empty",
-]);
-
-export function getFilterTitle(filter: any[]) {
-  const values = generateTimeFilterValuesDescriptions(filter);
-  const desc =
-    values.length > 2
-      ? t`${values.length} selections`
-      : values.join(filter[0] === "!=" ? ", " : " - ");
-  const op = getFilterOperator(filter);
-  const prefix =
-    op && prefixedOperators.has(op.name)
-      ? `${op.displayPrefix ?? op.displayName} `
-      : "";
-  return prefix + desc;
-}
 
 export function formatDateValue(value: string): string | null {
   const filterValue = deserializeDateFilter(value);
