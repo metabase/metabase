f55eec68964bca0423e5b68149335076d265c038
Bryan Maass <bryan.maass@gmail.com>
2025-01-13 11:22:24 -0700

    update test for service accounts

--- enterprise/backend/test/metabase_enterprise/api/gsheets_test.clj
+++ enterprise/backend/test/metabase_enterprise/api/gsheets_test.clj
@@ -1,6 +1,6 @@
 (ns metabase-enterprise.api.gsheets-test
   (:require [clojure.test :refer [deftest is testing]]
-            [metabase.api.gsheets :as gsheets]
+            [metabase-enterprise.gsheets :as gsheets.api]
             [metabase.test.util :as tu]))
 
 (deftest validate-link-format-test)
@@ -11,7 +11,7 @@
       [api-key "mb_api_key_123"
        store-api-url "http://store-api-url.com"]
       (is (= {:store-api-url "http://store-api-url.com", :api-key "mb_api_key_123"}
-             (#'gsheets/->config))))))
+             (#'gsheets.api/->config))))))
 
 (deftest ->config-missing-api-key-test
   (tu/with-temporary-setting-values
@@ -20,7 +20,7 @@
     (is (thrown-with-msg?
          Exception
          #"Missing api-key."
-         (#'gsheets/->config)))))
+         (#'gsheets.api/->config)))))
 
 (deftest ->config-missing-both-test
   (tu/with-temporary-setting-values
@@ -29,24 +29,24 @@
     (is (thrown-with-msg?
          Exception
          #"Missing api-key."
-         (#'gsheets/->config)))))
+         (#'gsheets.api/->config)))))
 
-(deftest oauth-not-setup?-test
+(deftest service-account-not-setup?-test
   (let [*hm-call-count (atom 0)]
-    (with-redefs [gsheets/hm-oauth-setup? (fn [] (swap! *hm-call-count inc) false)]
+    (with-redefs [gsheets.api/hm-service-account-setup? (fn [] (swap! *hm-call-count inc) false)]
       (tu/with-temporary-setting-values
         [store-api-url "http://store-api-url"
          api-key "mb_api_key_123"]
-        (is (false? (#'gsheets/oauth-setup? :no-auth)))
+        (is (false? (#'gsheets.api/hm-service-account-setup?)))
         (is (= 1 @*hm-call-count))))))
 
-(deftest oauth-is-setup?-test
+(deftest service-account-is-setup?-test
   (let [*hm-call-count (atom 0)]
-    (with-redefs [gsheets/hm-oauth-setup? (fn [] (swap! *hm-call-count inc) true)]
+    (with-redefs [gsheets.api/hm-service-account-setup? (fn [] (swap! *hm-call-count inc) true)]
       (tu/with-temporary-setting-values
         [store-api-url "http://store-api-url"
          api-key "mb_api_key_123"]
-        (is (true? (#'gsheets/oauth-setup? :auth-complete)))
-        (is (= 0 @*hm-call-count))
-        (is (true? (#'gsheets/oauth-setup? :folder-saved)))
-        (is (= 0 @*hm-call-count))))))
+        (is (true? (#'gsheets.api/hm-service-account-setup?)))
+        (is (= 1 @*hm-call-count))
+        (is (true? (#'gsheets.api/hm-service-account-setup?)))
+        (is (= 2 @*hm-call-count))))))
