280d7f2554551206e9a2b556a8f7285bd2588a0f
Nicolò Pretto <info@npretto.com>
2025-01-14 17:35:20 +0100

    refactor sdk imports (#52020)
    * move sdk config
    
    * extracts mode: "sdk" to renderWithSDKProviders
    
    * move MetabasePluginsConfig
    
    * move theme types
    
    * move theming stuff
    
    * moved plugins, added eslint rule
    
    * remove duplicated file
    
    * clean up ui.tsx
    
    * move defineMetabaseTheme
    
    * fix lint issue

--- enterprise/frontend/src/embedding-sdk/components/public/CollectionBrowser/CollectionBrowser.unit.spec.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/CollectionBrowser/CollectionBrowser.unit.spec.tsx
@@ -4,8 +4,9 @@ import {
   setupCollectionItemsEndpoint,
   setupCollectionsEndpoints,
 } from "__support__/server-mocks";
-import { renderWithProviders, screen } from "__support__/ui";
+import { screen } from "__support__/ui";
 import { CollectionBrowserInner } from "embedding-sdk/components/public/CollectionBrowser/CollectionBrowser";
+import { renderWithSDKProviders } from "embedding-sdk/test/__support__/ui";
 import { createMockAuthProviderUriConfig } from "embedding-sdk/test/mocks/config";
 import { setupSdkState } from "embedding-sdk/test/server-mocks/sdk-init";
 import { ROOT_COLLECTION } from "metabase/entities/collections";
@@ -64,8 +65,7 @@ async function setup({
 
   const state = setupSdkState();
 
-  renderWithProviders(<CollectionBrowserInner {...props} />, {
-    mode: "sdk",
+  renderWithSDKProviders(<CollectionBrowserInner {...props} />, {
     sdkProviderProps: {
       authConfig: createMockAuthProviderUriConfig({
         authProviderUri: "http://TEST_URI/sso/metabase",
