ea301c3d7010ce3d4dbbee697238dfe9dd472473
Kamil Mielnik <kamil@kamilmielnik.com>
2025-01-15 21:30:28 +0700

    Simplify unit tests

--- frontend/src/metabase-lib/v1/expressions/tokenizer.unit.spec.ts
+++ frontend/src/metabase-lib/v1/expressions/tokenizer.unit.spec.ts
@@ -36,42 +36,20 @@ describe("tokenizer", () => {
   });
 
   it("tokenizes incomplete bracket identifier followed by whitespace (metabase#50925)", () => {
-    const { tokens } = tokenize("case([ID] = 1, [Price] * 1.21, [Pr [Price])");
+    const { tokens } = tokenize("[Pr [Price]");
 
     expect(tokens).toEqual([
-      { type: 4, start: 0, end: 4 }, // case
-      { type: 1, op: "(", start: 4, end: 5 }, // (
-      { type: 4, start: 5, end: 9 }, // [ID]
-      { type: 1, op: "=", start: 10, end: 11 }, // =
-      { type: 2, start: 12, end: 13 }, // 1
-      { type: 1, op: ",", start: 13, end: 14 }, // ,
-      { type: 4, start: 15, end: 22 }, // [Price]
-      { type: 1, op: "*", start: 23, end: 24 }, // *
-      { type: 2, start: 25, end: 29 }, // 1.21
-      { type: 1, op: ",", start: 29, end: 30 }, // ,
-      { type: 4, start: 31, end: 34 }, // [Pr
-      { type: 4, start: 35, end: 42 }, // [Price]
-      { type: 1, op: ")", start: 42, end: 43 }, // )
+      { type: TOKEN.Identifier, start: 0, end: 3 }, // [Pr
+      { type: TOKEN.Identifier, start: 4, end: 11 }, // [Price]
     ]);
   });
 
   it("tokenizes incomplete bracket identifier followed by bracket identifier (metabase#50925)", () => {
-    const { tokens } = tokenize("case([ID] = 1, [Price] * 1.21, [Pr[Price])");
+    const { tokens } = tokenize("[Pr[Price]");
 
     expect(tokens).toEqual([
-      { type: 4, start: 0, end: 4 }, // case
-      { type: 1, op: "(", start: 4, end: 5 }, // (
-      { type: 4, start: 5, end: 9 }, // [ID]
-      { type: 1, op: "=", start: 10, end: 11 }, // =
-      { type: 2, start: 12, end: 13 }, // 1
-      { type: 1, op: ",", start: 13, end: 14 }, // ,
-      { type: 4, start: 15, end: 22 }, // [Price]
-      { type: 1, op: "*", start: 23, end: 24 }, // *
-      { type: 2, start: 25, end: 29 }, // 1.21
-      { type: 1, op: ",", start: 29, end: 30 }, // ,
-      { type: 4, start: 31, end: 34 }, // [Pr
-      { type: 4, start: 34, end: 41 }, // [Price]
-      { type: 1, op: ")", start: 41, end: 42 }, // )
+      { type: TOKEN.Identifier, start: 0, end: 3 }, // [Pr
+      { type: TOKEN.Identifier, start: 3, end: 10 }, // [Price]
     ]);
   });
 });
