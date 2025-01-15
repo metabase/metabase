7ea92e6d5725aa5f3b53a67d73bf2c4173faa902
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-01-15 12:59:03 +0100

    Add initial hacky structure

--- frontend/src/metabase/browse/models/ModelExplanationBanner.tsx
+++ frontend/src/metabase/browse/models/ModelExplanationBanner.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { t } from "ttag";
 
-import { useUserSetting } from "metabase/common/hooks";
-import { Flex, Icon, Paper, Text } from "metabase/ui";
-
-import { BannerCloseButton, BannerModelIcon } from "./BrowseModels.styled";
+import { useDocsUrl, useUserSetting } from "metabase/common/hooks";
+import ExternalLink from "metabase/core/components/ExternalLink";
+import {
+  Button,
+  Flex,
+  Group,
+  Modal,
+  Paper,
+  Stack,
+  Text,
+  Title,
+} from "metabase/ui";
 
 export const ModelExplanationBanner = () => {
   const [hasDismissedBanner, setHasDismissedBanner] = useUserSetting(
     "dismissed-browse-models-banner",
   );
+
+  const [opened, setOpened] = useState(false);
+
+  const { showMetabaseLinks, url } = useDocsUrl("data-modeling/models");
+
   const dismissBanner = () => {
     setHasDismissedBanner(true);
   };
@@ -19,23 +33,60 @@ export const ModelExplanationBanner = () => {
 
   return (
     <Paper
-      p="1rem"
       color="text-dark"
-      bg="brand-lighter"
+      bg="transparent"
       shadow="0"
       radius="0.25rem"
       role="complementary"
-      w="100%"
+      w="80%"
+      mb="xl"
     >
       <Flex>
-        <BannerModelIcon name="model" />
-        <Text size="md" lh="1rem" style={{ marginInlineEnd: "1rem" }}>
-          {t`Models help curate data to make it easier to find answers to questions all in one place.`}
-        </Text>
-        <BannerCloseButton onClick={dismissBanner}>
-          <Icon name="close" />
-        </BannerCloseButton>
+        <div
+          onClick={() => setOpened(true)}
+          style={{
+            cursor: "pointer",
+            background: "pink",
+            width: "120px",
+            height: "80px",
+            marginRight: "16px",
+          }}
+        />
+        <Stack spacing="md">
+          <Title
+            order={2}
+            size="md"
+            lh={1}
+            m={0}
+          >{t`Create models to clean up and combine tables to make your data easier to explore`}</Title>
+          <Text size="md" lh="1.5">
+            {t`Models are somewhat like virtual tables: do all your joins and custom columns once, save it as a model, then query it like a table.`}
+          </Text>
+          <Group spacing="md">
+            {showMetabaseLinks && (
+              <Button variant="subtle" p={0}>
+                <ExternalLink
+                  key="model-caching-link"
+                  href={url}
+                >{t`Read the docs`}</ExternalLink>
+              </Button>
+            )}
+            <Button variant="subtle" p={0} onClick={dismissBanner}>
+              Dismiss
+            </Button>
+          </Group>
+        </Stack>
       </Flex>
+      <Modal opened={opened} size="80%" onClose={() => setOpened(false)}>
+        <iframe
+          width="100%"
+          style={{ aspectRatio: "16/9", border: 0, borderRadius: "8px" }}
+          src="https://www.youtube.com/embed/Cb7-wLAgSCA?si=gPukXurSJAM8asGJ&autoplay=1"
+          // eslint-disable-next-line no-literal-metabase-strings -- It's just a title for the a11y purposes
+          title="Use Models in Metabase | Getting started with Metabase"
+          referrerPolicy="strict-origin-when-cross-origin"
+        ></iframe>
+      </Modal>
     </Paper>
   );
 };
