d1cc061245c0f7895ea158a0208ff7f54d0a6ffe
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-01-15 23:19:40 -0500

    Remove dynamic-dictionary from user settings

--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -403,7 +403,6 @@ export type UserSettings = {
   "show-updated-permission-modal": boolean;
   "show-updated-permission-banner": boolean;
   "trial-banner-dismissal-timestamp"?: string | null;
-  "dynamic-dictionary"?: string | null;
 };
 
 /**
