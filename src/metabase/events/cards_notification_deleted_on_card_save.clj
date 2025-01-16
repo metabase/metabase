7155d8189abbaac4b93315cea648640c49970499
Ngoc Khuat <qn.khuat@gmail.com>
2025-01-16 17:12:03 +0700

    let's make sure it does not fail if sending email fails

--- src/metabase/events/cards_notification_deleted_on_card_save.clj
+++ src/metabase/events/cards_notification_deleted_on_card_save.clj
@@ -2,6 +2,7 @@
   (:require
    [metabase.email.messages :as messages]
    [metabase.events :as events]
+   [metabase.util.log :as log]
    [methodical.core :as methodical]))
 
 (derive ::event :metabase/event)
@@ -13,22 +14,25 @@
   time of this writing this is triggered by [[metabase.models.card/delete-alerts-if-needed!]] and
   by [[metabase.api.collection/maybe-send-archived-notifications!]]."
   [topic {:keys [notifications card actor], :as _event}]
-  (let [send-message! (case topic
-                        :event/card-update.notification-deleted.card-archived
-                        messages/send-alert-stopped-because-archived-email!
+  (try
+    (let [send-message! (case topic
+                          :event/card-update.notification-deleted.card-archived
+                          messages/send-alert-stopped-because-archived-email!
 
-                        :event/card-update.notification-deleted.card-changed
-                        messages/send-alert-stopped-because-changed-email!)
-        recipients (->> notifications
-                        (mapcat :handlers)
-                        (filter #(= :channel/email (:channel_type %)))
-                        (mapcat :recipients)
-                        (keep (fn [recipient]
-                                (case (:type recipient)
-                                  :notification-recipient/user
-                                  (-> recipient :user :email)
-                                  :notification-recipient/raw-value
-                                  (-> recipient :details :value)
-                                  (throw (ex-info "Unknown recipient type" {:recipient recipient}))))))]
-    (when (seq recipients)
-      (send-message! card recipients actor))))
+                          :event/card-update.notification-deleted.card-changed
+                          messages/send-alert-stopped-because-changed-email!)
+          recipients (->> notifications
+                          (mapcat :handlers)
+                          (filter #(= :channel/email (:channel_type %)))
+                          (mapcat :recipients)
+                          (keep (fn [recipient]
+                                  (case (:type recipient)
+                                    :notification-recipient/user
+                                    (-> recipient :user :email)
+                                    :notification-recipient/raw-value
+                                    (-> recipient :details :value)
+                                    (throw (ex-info "Unknown recipient type" {:recipient recipient}))))))]
+      (when (seq recipients)
+        (send-message! card recipients actor)))
+    (catch Throwable e
+      (log/error e "Error sending notification email"))))
