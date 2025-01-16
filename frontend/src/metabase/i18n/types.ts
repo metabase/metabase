53ef15aa12b37b1d7016aa52b3033f1afd053110
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-01-15 23:22:11 -0500

    Remove dead code

--- frontend/src/metabase/i18n/types.ts
+++ frontend/src/metabase/i18n/types.ts
@@ -8,31 +8,6 @@ export type ContentTranslationDictionaryEntry = {
 /** A dictionary used for content translation */
 export type ContentTranslationDictionary = ContentTranslationDictionaryEntry[];
 
-export const isValidContentTranslationDictionary = (
-  obj: unknown,
-): obj is ContentTranslationDictionary => {
-  if (!Array.isArray(obj)) {
-    return false;
-  }
-  for (const entry of obj) {
-    if (
-      !Array.isArray(entry) ||
-      entry.length !== 3 ||
-      typeof entry[0] !== "string" ||
-      typeof entry[1] !== "string" ||
-      typeof entry[2] !== "string"
-    ) {
-      return false;
-    }
-  }
-  return true;
-};
-
-export type LocalizationPattern = {
-  actionTypePattern: RegExp;
-  fieldPattern: RegExp;
-};
-
 export type ContentTranslationContextObject = {
   shouldLocalize: boolean;
   locale?: string;
