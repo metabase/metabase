04c6cc39d51fa8a25107d51d57c6736c43846098
Phoomparin Mano <phoomparin@gmail.com>
2025-01-17 16:24:26 +0700

    scaffold test setup for tooltips

---
+++ e2e/test-component/scenarios/embedding-sdk/tooltip-reproductions.cy.spec.tsx
@@ -0,0 +1,69 @@
+import { InteractiveDashboard } from "@metabase/embedding-sdk-react";
+
+import { H } from "e2e/support";
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import { describeEE } from "e2e/support/helpers";
+import {
+  mockAuthProviderAndJwtSignIn,
+  mountSdkContent,
+  signInAsAdminAndEnableEmbeddingSdk,
+} from "e2e/support/helpers/component-testing-sdk";
+
+const { ORDERS, ORDERS_ID } = SAMPLE_DATABASE;
+
+describeEE("scenarios > embedding-sdk > tooltip-reproductions", () => {
+  beforeEach(() => {
+    signInAsAdminAndEnableEmbeddingSdk();
+
+    H.createQuestion({
+      name: "Tooltip test",
+      query: {
+        "source-table": ORDERS_ID,
+        aggregation: [["count"]],
+        breakout: [["field", ORDERS.CREATED_AT, { "temporal-unit": "year" }]],
+      },
+      display: "bar",
+    })
+      .then(({ body: { id: ordersQuestionId } }) =>
+        H.createDashboard({
+          dashcards: [
+            {
+              id: 1,
+              size_x: 10,
+              size_y: 20,
+              row: 0,
+              col: 0,
+              card_id: ordersQuestionId,
+            },
+            {
+              id: 2,
+              size_x: 10,
+              size_y: 5,
+              row: 0,
+              col: 0,
+              card_id: ordersQuestionId,
+            },
+          ],
+        }),
+      )
+      .then(dashboard => {
+        cy.wrap(dashboard.body.id).as("dashboardId");
+      });
+
+    cy.signOut();
+    mockAuthProviderAndJwtSignIn();
+
+    cy.intercept("GET", "/api/dashboard/*").as("getDashboard");
+    cy.intercept("POST", "/api/dashboard/*/dashcard/*/card/*/query").as(
+      "dashcardQuery",
+    );
+  });
+
+  it("should render tooltips below the screen's height", () => {
+    cy.get("@dashboardId").then(dashboardId => {
+      mountSdkContent(<InteractiveDashboard dashboardId={dashboardId} />);
+
+      H;
+    });
+  });
+});
