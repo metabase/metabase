1069b41af317ff5979b3a9f57a2892d3d1fe6947
Cam Saul <github@camsaul.com>
2025-01-17 01:03:35 +0000

    Auto rewrite defendpoint ones

--- src/metabase/api/action.clj
+++ src/metabase/api/action.clj
@@ -1,11 +1,11 @@
 (ns metabase.api.action
   "`/api/action/` endpoints."
   (:require
-   [compojure.core :refer [POST]]
    [metabase.actions.core :as actions]
    [metabase.analytics.snowplow :as snowplow]
    [metabase.api.common :as api]
    [metabase.api.common.validation :as validation]
+   [metabase.api.macros :as api.macros]
    [metabase.models.action :as action]
    [metabase.models.card :as card]
    [metabase.models.collection :as collection]
@@ -47,12 +47,12 @@
    [:parameters         {:optional true} [:maybe [:sequential map?]]]
    [:parameter_mappings {:optional true} [:maybe map?]]])
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint GET "/"
+(api.macros/defendpoint :get "/"
   "Returns actions that can be used for QueryActions. By default lists all viewable actions. Pass optional
   `?model-id=<model-id>` to limit to actions on a particular model."
-  [model-id]
-  {model-id [:maybe ms/PositiveInt]}
+  [_route-params
+   {:keys [model-id]} :- [:map
+                          [:model-id {:optional true} [:maybe ms/PositiveInt]]]]
   (letfn [(actions-for [models]
             (if (seq models)
               (t2/hydrate (action/select-actions models
@@ -71,28 +71,25 @@
                                             (collection/visible-collection-filter-clause)]}))]
       (actions-for models))))
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint GET "/public"
+(api.macros/defendpoint :get "/public"
   "Fetch a list of Actions with public UUIDs. These actions are publicly-accessible *if* public sharing is enabled."
   []
   (validation/check-has-application-permission :setting)
   (validation/check-public-sharing-enabled)
   (t2/select [:model/Action :name :id :public_uuid :model_id], :public_uuid [:not= nil], :archived false))
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint GET "/:action-id"
+(api.macros/defendpoint :get "/:action-id"
   "Fetch an Action."
-  [action-id]
-  {action-id ms/PositiveInt}
+  [{:keys [action-id]} :- [:map
+                           [:action-id ms/PositiveInt]]]
   (-> (action/select-action :id action-id :archived false)
       (t2/hydrate :creator)
       api/read-check))
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint DELETE "/:action-id"
+(api.macros/defendpoint :delete "/:action-id"
   "Delete an Action."
-  [action-id]
-  {action-id ms/PositiveInt}
+  [{:keys [action-id]} :- [:map
+                           [:action-id ms/PositiveInt]]]
   (let [action (api/write-check :model/Action action-id)]
     (snowplow/track-event! ::snowplow/action
                            {:event     :action-deleted
@@ -101,26 +98,25 @@
   (t2/delete! :model/Action :id action-id)
   api/generic-204-no-content)
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint POST "/"
+(api.macros/defendpoint :post "/"
   "Create a new action."
-  [:as {{:keys [type name description model_id parameters parameter_mappings visualization_settings
-                kind
-                database_id dataset_query
-                template response_handle error_handle] :as action} :body}]
-  {name                   :string
-   model_id               ms/PositiveInt
-   type                   [:maybe supported-action-type]
-   description            [:maybe :string]
-   parameters             [:maybe [:sequential map?]]
-   parameter_mappings     [:maybe map?]
-   visualization_settings [:maybe map?]
-   kind                   [:maybe implicit-action-kind]
-   database_id            [:maybe ms/PositiveInt]
-   dataset_query          [:maybe map?]
-   template               [:maybe http-action-template]
-   response_handle        [:maybe json-query-schema]
-   error_handle           [:maybe json-query-schema]}
+  [_route-params
+   _query-params
+   {:keys [type model_id parameters database_id]
+    :as action} :- [:map
+                    [:name                   :string]
+                    [:model_id               ms/PositiveInt]
+                    [:type                   {:optional true} [:maybe supported-action-type]]
+                    [:description            {:optional true} [:maybe :string]]
+                    [:parameters             {:optional true} [:maybe [:sequential map?]]]
+                    [:parameter_mappings     {:optional true} [:maybe map?]]
+                    [:visualization_settings {:optional true} [:maybe map?]]
+                    [:kind                   {:optional true} [:maybe implicit-action-kind]]
+                    [:database_id            {:optional true} [:maybe ms/PositiveInt]]
+                    [:dataset_query          {:optional true} [:maybe map?]]
+                    [:template               {:optional true} [:maybe http-action-template]]
+                    [:response_handle        {:optional true} [:maybe json-query-schema]]
+                    [:error_handle           {:optional true} [:maybe json-query-schema]]]]
   (when (and (nil? database_id)
              (= "query" type))
     (throw (ex-info (tru "Must provide a database_id for query actions")
@@ -146,26 +142,26 @@
       ;; so we return the most recently updated http action.
       (last (action/select-actions nil :type type)))))
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint PUT "/:id"
+(api.macros/defendpoint :put "/:id"
   "Update an Action."
-  [id :as {action :body}]
-  {id     ms/PositiveInt
-   action [:map
-           [:archived               {:optional true} [:maybe :boolean]]
-           [:database_id            {:optional true} [:maybe ms/PositiveInt]]
-           [:dataset_query          {:optional true} [:maybe :map]]
-           [:description            {:optional true} [:maybe :string]]
-           [:error_handle           {:optional true} [:maybe json-query-schema]]
-           [:kind                   {:optional true} [:maybe implicit-action-kind]]
-           [:model_id               {:optional true} [:maybe ms/PositiveInt]]
-           [:name                   {:optional true} [:maybe :string]]
-           [:parameter_mappings     {:optional true} [:maybe :map]]
-           [:parameters             {:optional true} [:maybe [:sequential :map]]]
-           [:response_handle        {:optional true} [:maybe json-query-schema]]
-           [:template               {:optional true} [:maybe http-action-template]]
-           [:type                   {:optional true} [:maybe supported-action-type]]
-           [:visualization_settings {:optional true} [:maybe :map]]]}
+  [{:keys [id]} :- [:map
+                    [:id ms/PositiveInt]]
+   _query-params
+   action :- [:map
+              [:archived               {:optional true} [:maybe :boolean]]
+              [:database_id            {:optional true} [:maybe ms/PositiveInt]]
+              [:dataset_query          {:optional true} [:maybe :map]]
+              [:description            {:optional true} [:maybe :string]]
+              [:error_handle           {:optional true} [:maybe json-query-schema]]
+              [:kind                   {:optional true} [:maybe implicit-action-kind]]
+              [:model_id               {:optional true} [:maybe ms/PositiveInt]]
+              [:name                   {:optional true} [:maybe :string]]
+              [:parameter_mappings     {:optional true} [:maybe :map]]
+              [:parameters             {:optional true} [:maybe [:sequential :map]]]
+              [:response_handle        {:optional true} [:maybe json-query-schema]]
+              [:template               {:optional true} [:maybe http-action-template]]
+              [:type                   {:optional true} [:maybe supported-action-type]]
+              [:visualization_settings {:optional true} [:maybe :map]]]]
   (actions/check-actions-enabled! id)
   (let [existing-action (api/write-check :model/Action id)]
     (action/update! (assoc action :id id) existing-action))
@@ -177,13 +173,12 @@
                             :num_parameters (count parameters)})
     action))
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint POST "/:id/public_link"
+(api.macros/defendpoint :post "/:id/public_link"
   "Generate publicly-accessible links for this Action. Returns UUID to be used in public links. (If this
   Action has already been shared, it will return the existing public link rather than creating a new one.) Public
   sharing must be enabled."
-  [id]
-  {id ms/PositiveInt}
+  [{:keys [id]} :- [:map
+                    [:id ms/PositiveInt]]]
   (api/check-superuser)
   (validation/check-public-sharing-enabled)
   (let [action (api/read-check :model/Action id :archived false)]
@@ -194,11 +189,10 @@
                              {:public_uuid <>
                               :made_public_by_id api/*current-user-id*})))}))
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint DELETE "/:id/public_link"
+(api.macros/defendpoint :delete "/:id/public_link"
   "Delete the publicly-accessible link to this Dashboard."
-  [id]
-  {id ms/PositiveInt}
+  [{:keys [id]} :- [:map
+                    [:id ms/PositiveInt]]]
   ;; check the /application/setting permission, not superuser because removing a public link is possible from /admin/settings
   (validation/check-has-application-permission :setting)
   (validation/check-public-sharing-enabled)
@@ -207,25 +201,26 @@
   (t2/update! :model/Action id {:public_uuid nil, :made_public_by_id nil})
   {:status 204, :body nil})
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint GET "/:action-id/execute"
+(api.macros/defendpoint :get "/:action-id/execute"
   "Fetches the values for filling in execution parameters. Pass PK parameters and values to select."
-  [action-id parameters]
-  {action-id  ms/PositiveInt
-   parameters ms/JSONString}
+  [{:keys [action-id]} :- [:map
+                           [:action-id ms/PositiveInt]]
+   {:keys [parameters]} :- [:map
+                            [:parameters ms/JSONString]]]
   (actions/check-actions-enabled! action-id)
   (-> (action/select-action :id action-id :archived false)
       api/read-check
       (actions/fetch-values (json/decode parameters))))
 
-#_{:clj-kondo/ignore [:deprecated-var]}
-(api/defendpoint POST "/:id/execute"
+(api.macros/defendpoint :post "/:id/execute"
   "Execute the Action.
 
    `parameters` should be the mapped dashboard parameters with values."
-  [id :as {{:keys [parameters], :as _body} :body}]
-  {id         ms/PositiveInt
-   parameters [:maybe [:map-of :keyword any?]]}
+  [{:keys [id]} :- [:map
+                    [:id ms/PositiveInt]]
+   _query-params
+   {:keys [parameters], :as _body} :- [:map
+                                       [:parameters {:optional true} [:maybe [:map-of :keyword any?]]]]]
   (let [{:keys [type] :as action} (api/check-404 (action/select-action :id id :archived false))]
     (snowplow/track-event! ::snowplow/action
                            {:event     :action-executed
