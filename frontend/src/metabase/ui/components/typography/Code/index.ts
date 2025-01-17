0d6186ad08c76d6c65bd60945c5bfc8fe2a5698a
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-01-17 10:09:48 -0500

    Port Code to Mantine v7

--- frontend/src/metabase/ui/components/typography/Code/index.ts
+++ frontend/src/metabase/ui/components/typography/Code/index.ts
@@ -1,3 +1,3 @@
 export { Code } from "@mantine/core";
 export type { CodeProps } from "@mantine/core";
-export { getCodeOverrides } from "./Code.styled";
+export { codeOverrides } from "./Code.config";
