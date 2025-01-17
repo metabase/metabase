889416a9161462e7e6e8f48901fb739fcdd28005
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-01-17 14:06:45 -0500

    Port MonthPicker to Mantine v7 (#52318)

--- frontend/src/metabase/ui/theme.ts
+++ frontend/src/metabase/ui/theme.ts
@@ -24,7 +24,7 @@ import {
   listOverrides,
   menuOverrides,
   modalOverrides,
-  //TODO: monthPickerOverrides
+  monthPickerOverrides,
   multiSelectOverrides,
   navLinkOverrides,
   overlayOverrides,
@@ -133,6 +133,7 @@ export const getThemeOverrides = (): MantineThemeOverride => ({
     ...inputOverrides,
     ...menuOverrides,
     ...modalOverrides,
+    ...monthPickerOverrides,
     ...multiSelectOverrides,
     ...navLinkOverrides,
     ...radioOverrides,
