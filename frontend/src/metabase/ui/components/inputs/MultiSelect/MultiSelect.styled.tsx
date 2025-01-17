72501e16e386608d100855d8448586ff7b32e076
Oisin Coveney <oisin@metabase.com>
2025-01-17 11:15:00 -0800

    Change order of input wrapper elements (#52235)

--- frontend/src/metabase/ui/components/inputs/MultiSelect/MultiSelect.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/MultiSelect/MultiSelect.styled.tsx
@@ -39,6 +39,7 @@ export const getMultiSelectOverrides =
           color: "text-dark",
         },
         "data-testid": "multi-select",
+        inputWrapperOrder: ["label", "description", "input", "error"],
       },
       styles: (
         theme,
@@ -126,6 +127,9 @@ export const getMultiSelectOverrides =
           backgroundColor: "var(--mb-color-background)",
           borderColor: "var(--mb-color-border)",
           overflow: "auto",
+        },
+        error: {
+          "&::first-letter": { textTransform: "capitalize" },
         },
       }),
       variants: {
