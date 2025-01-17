0bf5ca2bc826d4de3c2fbf96a7c75b9ceb2d84f2
Tyler Plude <tsplude@gmail.com>
2025-01-17 09:52:50 -0800

    Raising download-row-limit min to 1048576

--- src/metabase/public_settings.clj
+++ src/metabase/public_settings.clj
@@ -1066,7 +1066,7 @@ See [fonts](../configuring-metabase/fonts.md).")
   :default    nil
   :getter     (fn []
                 (let [limit (setting/get-value-of-type :integer :download-row-limit)]
-                  (max limit 10000))))
+                  (max limit 1048576))))
 
 ;;; TODO -- move the search-related settings into the `:search` module. Only settings used across the entire application
 ;;; should live in this namespace.
