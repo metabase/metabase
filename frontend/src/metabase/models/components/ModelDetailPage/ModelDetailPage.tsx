eaaf4b2f1f4d0ff0e93c4b0e661bfe56fcb37195
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-01-17 09:48:56 -0500

    Remove tabs from model detail page

--- frontend/src/metabase/models/components/ModelDetailPage/ModelDetailPage.tsx
+++ frontend/src/metabase/models/components/ModelDetailPage/ModelDetailPage.tsx
@@ -1,41 +1,27 @@
 import { t } from "ttag";
 
-import TabContent from "metabase/core/components/TabContent";
-import TabLink from "metabase/core/components/TabLink";
-import * as Urls from "metabase/lib/urls";
 import type Question from "metabase-lib/v1/Question";
 import type { CollectionId } from "metabase-types/api";
 
 import ModelActionDetails from "./ModelActionDetails";
 import ModelDetailHeader from "./ModelDetailHeader";
-import {
-  ModelMain,
-  RootLayout,
-  TabPanel,
-  TabPanelContent,
-  TabRow,
-} from "./ModelDetailPage.styled";
-import ModelSchemaDetails from "./ModelSchemaDetails";
+import { ModelMain, RootLayout } from "./ModelDetailPage.styled";
 
 interface Props {
   model: Question;
-  tab: string;
   hasDataPermissions: boolean;
-  hasActionsTab: boolean;
   onChangeName: (name?: string) => void;
   onChangeCollection: ({ id }: { id: CollectionId }) => void;
+  shouldShowActionsUI: boolean;
 }
 
 function ModelDetailPage({
   model,
-  tab,
   hasDataPermissions,
-  hasActionsTab,
+  shouldShowActionsUI,
   onChangeName,
   onChangeCollection,
 }: Props) {
-  const modelCard = model.card();
-
   return (
     <RootLayout>
       <ModelMain>
@@ -45,39 +31,11 @@ function ModelDetailPage({
           onChangeName={onChangeName}
           onChangeCollection={onChangeCollection}
         />
-        <TabContent value={tab}>
-          <TabRow>
-            <TabLink
-              value="usage"
-              to={Urls.modelDetail(modelCard, "usage")}
-            >{t`Used by`}</TabLink>
-            <TabLink
-              value="schema"
-              to={Urls.modelDetail(modelCard, "schema")}
-            >{t`Schema`}</TabLink>
-            {hasActionsTab && (
-              <TabLink
-                value="actions"
-                to={Urls.modelDetail(modelCard, "actions")}
-              >{t`Actions`}</TabLink>
-            )}
-          </TabRow>
-          <TabPanel value="schema">
-            <TabPanelContent>
-              <ModelSchemaDetails
-                model={model}
-                hasEditMetadataLink={hasDataPermissions}
-              />
-            </TabPanelContent>
-          </TabPanel>
-          {hasActionsTab && (
-            <TabPanel value="actions">
-              <TabPanelContent>
-                <ModelActionDetails model={model} />
-              </TabPanelContent>
-            </TabPanel>
-          )}
-        </TabContent>
+        {shouldShowActionsUI ? (
+          <ModelActionDetails model={model} />
+        ) : (
+          <>{t`Actions are not enabled for this model.`}</>
+        )}
       </ModelMain>
     </RootLayout>
   );
