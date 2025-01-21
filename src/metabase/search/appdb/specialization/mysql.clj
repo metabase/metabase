1d1320b2ad33da0f58f337a3dc1273b2065e0b00
Alexander Solovyov <alexander@solovyov.net>
2025-01-21 14:39:37 +0200

    mysql ngram works just as well but faster

--- src/metabase/search/appdb/specialization/mysql.clj
+++ src/metabase/search/appdb/specialization/mysql.clj
@@ -2,24 +2,41 @@
   (:require
    [clojure.math :as math]
    [clojure.string :as str]
+   [honey.sql :as sql]
    [honey.sql.helpers :as sql.helpers]
    [metabase.search.appdb.specialization.api :as specialization]
-   [metabase.util :as u]
+   [metabase.search.appdb.specialization.postgres :as postgres]
    [toucan2.core :as t2]))
+
+;;; honeysql extensions
+
+(defn- format-match-against [_ [cols pattern]]
+  (let [cols-sql (if (vector? cols)
+                   (str/join ", " (map sql/sql-kw cols))
+                   (sql/sql-kw cols))
+        [pattern-sql & pattern-params] (sql/format-expr pattern)]
+    (into [(str "MATCH(" cols-sql ") AGAINST(" pattern-sql " IN BOOLEAN MODE)")]
+          pattern-params)))
+
+(sql/register-fn! :match-against format-match-against)
+
+;;; mysql-specific logic
 
 (defmethod specialization/->db-type :mysql [t]
   (get {:pk :int, :timestamp (keyword "timestamp(6)")} t t))
 
 (defmethod specialization/table-schema :mysql [base-schema]
   (into [[:id :bigint :auto-increment :primary-key]
-         [:search_terms :text]
-         [:native_search_terms :text]]
+         [:search_text :text]
+         [:native_query_text :text]]
         base-schema))
 
 (defmethod specialization/post-create-statements :mysql [prefix table-name]
   (mapv
    (fn [template] (format template prefix table-name))
-   ["CREATE UNIQUE INDEX %s_identity_idx ON %s (model, model_id)"]))
+   ["CREATE UNIQUE INDEX %s_identity_idx ON %s (model, model_id)"
+    "CREATE FULLTEXT INDEX %s_search_text_idx ON %s (search_text) WITH PARSER ngram"
+    "CREATE FULLTEXT INDEX %s_native_query_idx ON %s (native_query_text) WITH PARSER ngram"]))
 
 (defmethod specialization/batch-upsert! :mysql [table entries]
   (when (seq entries)
@@ -31,26 +48,44 @@
                                        (for [k update-keys]
                                          [k [:values k]]))}))))
 
-(defn- wildcard-tokens [search-term]
-  (->> (str/split search-term #"\s+")
-       (map #(u/lower-case-en (str/trim %)))
-       (remove str/blank?)
-       (map (fn [s] (str "%" s "%")))))
+(defn- quote* [s]
+  (str "\""
+       (-> (str/replace s #"^\"|\"$" "")
+           (str/replace "\"" "\"\""))
+       "\""))
+
+(defn- process-phrase [phrase]
+  (cond
+    ;; trailing quotation mark
+    (= phrase "\"")               nil
+    ;; negation
+    (str/starts-with? phrase "-") (str "-" (quote* (subs phrase 1)))
+    ;; quoted or unquoted
+    :else                         (quote* phrase)))
+
+(defn- to-mysql-expr
+  "Given the user input, construct a query in MySQL query language."
+  [input]
+  (str
+   (when-let [input (some-> input str/trim not-empty)]
+     (->> (postgres/split-preserving-quotes input)
+          (remove str/blank?)
+          (map process-phrase)
+          (str/join " ")))))
 
 (defmethod specialization/base-query :mysql
-  [active-table search-term search-ctx select-items]
+  [active-table search-string search-ctx select-items]
   (let [search-column (if (:search-native-query search-ctx)
-                        :search_index.native_search_terms
-                        :search_index.search_terms)]
+                        [:search_index.search_text :search_index.native_query_text]
+                        :search_index.search_text)]
     (cond-> {:select select-items
              :from   [[active-table :search_index]]}
-      (not (str/blank? search-term))
-      (sql.helpers/where (into [:and] (for [pattern (wildcard-tokens search-term)]
-                                        [:like [:lower search-column] pattern]))))))
+      (not (str/blank? search-string))
+      (sql.helpers/where [:match-against search-column (to-mysql-expr search-string)]))))
 
 (defmethod specialization/extra-entry-fields :mysql [entity]
-  {:search_terms        (or (:searchable_text entity) "")
-   :native_search_terms (str/join " " (keep entity [:searchable_text :native_query]))})
+  {:search_text       (or (:searchable_text entity) "")
+   :native_query_text (or (:native_query entity) "")})
 
 (defmethod specialization/text-score :mysql [_search-ctx]
   [:inline 1])
