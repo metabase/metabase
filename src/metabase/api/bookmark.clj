284164d9a3352a8530a432c4a9f626c78c51a728
Cam Saul <github@camsaul.com>
2025-01-21 22:43:32 +0000

    Revert changes to bookmark

--- src/metabase/api/bookmark.clj
+++ src/metabase/api/bookmark.clj
@@ -5,8 +5,8 @@
   underlying tables the id on the actual bookmark itself is not unique among \"bookmarks\" and is not a good
   identifier for using in the API."
   (:require
+   [compojure.core :refer [DELETE GET POST]]
    [metabase.api.common :as api]
-   [metabase.api.macros :as api.macros]
    [metabase.models.bookmark :as bookmark]
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
@@ -27,18 +27,20 @@
    "dashboard"  [:model/Dashboard  :model/DashboardBookmark  :dashboard_id]
    "collection" [:model/Collection :model/CollectionBookmark :collection_id]})
 
-(api.macros/defendpoint :get "/"
+#_{:clj-kondo/ignore [:deprecated-var]}
+(api/defendpoint GET "/"
   "Fetch all bookmarks for the user"
   []
   ;; already sorted by created_at in query. Can optionally use user sort preferences here and not in the function
   ;; below
   (bookmark/bookmarks-for-user api/*current-user-id*))
 
-(api.macros/defendpoint :post "/:model/:id"
+#_{:clj-kondo/ignore [:deprecated-var]}
+(api/defendpoint POST "/:model/:id"
   "Create a new bookmark for user."
-  [{:keys [model id]} :- [:map
-                          [:model Models]
-                          [:id    ms/PositiveInt]]]
+  [model id]
+  {model Models
+   id    ms/PositiveInt}
   (let [[item-model bookmark-model item-key] (lookup model)]
     (api/read-check item-model id)
     (api/check (not (t2/exists? bookmark-model item-key id
@@ -46,11 +48,12 @@
                [400 "Bookmark already exists"])
     (first (t2/insert-returning-instances! bookmark-model {item-key id :user_id api/*current-user-id*}))))
 
-(api.macros/defendpoint :delete "/:model/:id"
+#_{:clj-kondo/ignore [:deprecated-var]}
+(api/defendpoint DELETE "/:model/:id"
   "Delete a bookmark. Will delete a bookmark assigned to the user making the request by model and id."
-  [{:keys [model id]} :- [:map
-                          [:model Models]
-                          [:id    ms/PositiveInt]]]
+  [model id]
+  {model Models
+   id    ms/PositiveInt}
   ;; todo: allow admins to include an optional user id to delete for so they can delete other's bookmarks.
   (let [[_ bookmark-model item-key] (lookup model)]
     (t2/delete! bookmark-model
@@ -58,12 +61,11 @@
                 item-key id)
     api/generic-204-no-content))
 
-(api.macros/defendpoint :put "/ordering"
+#_{:clj-kondo/ignore [:deprecated-var]}
+(api/defendpoint PUT "/ordering"
   "Sets the order of bookmarks for user."
-  [_route-params
-   _query-params
-   {:keys [orderings]} :- [:map
-                           [:orderings BookmarkOrderings]]]
+  [:as {{:keys [orderings]} :body}]
+  {orderings BookmarkOrderings}
   (bookmark/save-ordering! api/*current-user-id* orderings)
   api/generic-204-no-content)
 
