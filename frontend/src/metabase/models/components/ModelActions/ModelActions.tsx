5fc641383dfcfc1111ceb63484959388fe7b5566
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-01-21 09:16:56 -0500

    Improve appearance of actions page

--- frontend/src/metabase/models/components/ModelActions/ModelActions.tsx
+++ frontend/src/metabase/models/components/ModelActions/ModelActions.tsx
@@ -1,6 +1,8 @@
-import { t } from "ttag";
+import { jt, t } from "ttag";
 
-import { Stack } from "metabase/ui";
+import Link from "metabase/core/components/Link";
+import * as Urls from "metabase/lib/urls";
+import { Group, Icon, Paper, Stack, Title } from "metabase/ui";
 import type Question from "metabase-lib/v1/Question";
 
 import ModelActionDetails from "./ModelActionDetails";
@@ -14,7 +16,23 @@ function ModelActions({ model, shouldShowActionsUI }: Props) {
   return (
     <Stack p="3rem 4rem" mih="90vh">
       {shouldShowActionsUI ? (
-        <ModelActionDetails model={model} />
+        <Stack spacing="lg">
+          <Title>
+            <Group spacing="sm">
+              {jt`Actions for ${(
+                <Group spacing="xs">
+                  <Icon name="model" size={24} />
+                  <Link variant="brand" to={Urls.model({ id: model.id() })}>
+                    {model.displayName()}
+                  </Link>
+                </Group>
+              )}`}
+            </Group>
+          </Title>
+          <Paper p="lg">
+            <ModelActionDetails model={model} />
+          </Paper>
+        </Stack>
       ) : (
         <>{t`Actions are not enabled for this model.`}</>
       )}
