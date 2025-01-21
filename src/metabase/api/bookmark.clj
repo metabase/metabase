aac05e9b4047e24df5b5886bd215b26f4066416f
Cam Saul <github@camsaul.com>
2025-01-21 22:23:20 +0000

    Manual cleanup

--- src/metabase/api/bookmark.clj
+++ src/metabase/api/bookmark.clj
@@ -5,8 +5,7 @@
   underlying tables the id on the actual bookmark itself is not unique among \"bookmarks\" and is not a good
   identifier for using in the API."
   (:require
-   [compojure.core :refer [DELETE GET POST]]
-   [metabase.api.common :as api] 
+   [metabase.api.common :as api]
    [metabase.api.macros :as api.macros]
    [metabase.models.bookmark :as bookmark]
    [metabase.util.malli.schema :as ms]
@@ -38,8 +37,8 @@
 (api.macros/defendpoint :post "/:model/:id"
   "Create a new bookmark for user."
   [{:keys [model id]} :- [:map
-          [:model Models]
-          [:id    ms/PositiveInt]]]
+                          [:model Models]
+                          [:id    ms/PositiveInt]]]
   (let [[item-model bookmark-model item-key] (lookup model)]
     (api/read-check item-model id)
     (api/check (not (t2/exists? bookmark-model item-key id
@@ -50,8 +49,8 @@
 (api.macros/defendpoint :delete "/:model/:id"
   "Delete a bookmark. Will delete a bookmark assigned to the user making the request by model and id."
   [{:keys [model id]} :- [:map
-          [:model Models]
-          [:id    ms/PositiveInt]]]
+                          [:model Models]
+                          [:id    ms/PositiveInt]]]
   ;; todo: allow admins to include an optional user id to delete for so they can delete other's bookmarks.
   (let [[_ bookmark-model item-key] (lookup model)]
     (t2/delete! bookmark-model
@@ -64,7 +63,7 @@
   [_route-params
    _query-params
    {:keys [orderings]} :- [:map
-          [:orderings BookmarkOrderings]]]
+                           [:orderings BookmarkOrderings]]]
   (bookmark/save-ordering! api/*current-user-id* orderings)
   api/generic-204-no-content)
 
