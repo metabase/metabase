57fc8f190a68568e1fc52115fdc463915c96d91c
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-21 22:00:36 -0500

    Fix numeric parameter value parsing

--- frontend/src/metabase-lib/v1/parameters/utils/parameter-parsing.unit.spec.js
+++ frontend/src/metabase-lib/v1/parameters/utils/parameter-parsing.unit.spec.js
@@ -326,6 +326,8 @@ describe("parameters/utils/parameter-values", () => {
       { value: "123,234", expectedValue: ["123,234"] },
       { value: "123,abc", expectedValue: null },
       { value: "123,234abc", expectedValue: ["123,234"] },
+      { value: 123, expectedValue: [123] },
+      { value: [1, 2, 3], expectedValue: [1, 2, 3] },
     ])(
       "should parse number parameter value $value",
       ({ value, expectedValue }) => {
