796fb5e7437bff6d26da1b34d6260e871f3603a6
Nicolò Pretto <info@npretto.com>
2025-01-23 16:48:28 +0100

    Revert "revert unwanted changes"
    This reverts commit f6ce10ac7b278244eb7a80c1a065fe53a0b216c7.

--- frontend/src/metabase/routes.jsx
+++ frontend/src/metabase/routes.jsx
@@ -214,30 +214,33 @@ export const getRoutes = store => {
           {getModelRoutes()}
 
           <Route path="/model">
-            <IndexRoute component={QueryBuilder} />
+            <IndexRoute component={QueryBuilderWithEntityId} />
             <Route
               path="new"
               title={t`New Model`}
               component={NewModelOptions}
             />
-            <Route path=":slug" component={QueryBuilder} />
-            <Route path=":slug/notebook" component={QueryBuilder} />
-            <Route path=":slug/query" component={QueryBuilder} />
-            <Route path=":slug/metadata" component={QueryBuilder} />
-            <Route path=":slug/metabot" component={QueryBuilder} />
-            <Route path=":slug/:objectId" component={QueryBuilder} />
-            <Route path="query" component={QueryBuilder} />
-            <Route path="metabot" component={QueryBuilder} />
+            <Route path=":slug" component={QueryBuilderWithEntityId} />
+            <Route path=":slug/notebook" component={QueryBuilderWithEntityId} />
+            <Route path=":slug/query" component={QueryBuilderWithEntityId} />
+            <Route path=":slug/metadata" component={QueryBuilderWithEntityId} />
+            <Route path=":slug/metabot" component={QueryBuilderWithEntityId} />
+            <Route
+              path=":slug/:objectId"
+              component={QueryBuilderWithEntityId}
+            />
+            <Route path="query" component={QueryBuilderWithEntityId} />
+            <Route path="metabot" component={QueryBuilderWithEntityId} />
           </Route>
 
           {/* METRICS V2 */}
           <Route path="/metric">
-            <IndexRoute component={QueryBuilder} />
-            <Route path="notebook" component={QueryBuilder} />
-            <Route path="query" component={QueryBuilder} />
-            <Route path=":slug" component={QueryBuilder} />
-            <Route path=":slug/notebook" component={QueryBuilder} />
-            <Route path=":slug/query" component={QueryBuilder} />
+            <IndexRoute component={QueryBuilderWithEntityId} />
+            <Route path="notebook" component={QueryBuilderWithEntityId} />
+            <Route path="query" component={QueryBuilderWithEntityId} />
+            <Route path=":slug" component={QueryBuilderWithEntityId} />
+            <Route path=":slug/notebook" component={QueryBuilderWithEntityId} />
+            <Route path=":slug/query" component={QueryBuilderWithEntityId} />
           </Route>
 
           <Route path="browse">
