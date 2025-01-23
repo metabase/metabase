64a886418d39d31d7de8fc9cdfb1578b6fa7bf7b
Phoomparin Mano <phoomparin@gmail.com>
2025-01-23 15:07:24 +0700

    refactor sdk mappings

--- frontend/src/metabase/embedding-sdk/theme/sdk-theme-to-css-vars.ts
+++ frontend/src/metabase/embedding-sdk/theme/sdk-theme-to-css-vars.ts
@@ -3,6 +3,36 @@ import type { FlattenObjectKeys } from "../types/utils";
 import type { MetabaseComponentTheme } from "./MetabaseTheme";
 
 type MetabaseComponentThemeKey = FlattenObjectKeys<MetabaseComponentTheme>;
+type CssVariableToThemeMap = Record<string, MetabaseComponentThemeKey>;
+
+// SDK > Interactive Question > Notebook Editor Button
+export const NOTEBOOK_EDITOR_THEME_OPTIONS = {
+  "--mb-color-border-sdk-editor-button":
+    "questionEditor.editorButton.borderColor",
+
+  // Inactive
+  "--mb-color-icon-sdk-editor-button": "questionEditor.editorButton.iconColor",
+  "--mb-color-bg-sdk-editor-button":
+    "questionEditor.editorButton.backgroundColor",
+
+  // Active
+  "--mb-color-icon-sdk-editor-button-active":
+    "questionEditor.editorButton.activeIconColor",
+  "--mb-color-bg-sdk-editor-button-active":
+    "questionEditor.editorButton.activeBackgroundColor",
+} satisfies CssVariableToThemeMap;
+
+// SDK > Collection Browser > Breadcrumbs > Expand Button
+export const COLLECTION_BROWSER_THEME_OPTIONS = {
+  "--mb-color-text-collection-browser-expand-button":
+    "collectionBrowser.breadcrumbs.expandButton.textColor",
+  "--mb-color-bg-collection-browser-expand-button":
+    "collectionBrowser.breadcrumbs.expandButton.backgroundColor",
+  "--mb-color-text-collection-browser-expand-button-hover":
+    "collectionBrowser.breadcrumbs.expandButton.hoverTextColor",
+  "--mb-color-bg-collection-browser-expand-button-hover":
+    "collectionBrowser.breadcrumbs.expandButton.hoverBackgroundColor",
+} satisfies CssVariableToThemeMap;
 
 /** Maps the CSS variable name to the corresponding theme key in the Embedding SDK theme. */
 export const CSS_VARIABLES_TO_SDK_THEME_MAP = {
@@ -25,27 +55,10 @@ export const CSS_VARIABLES_TO_SDK_THEME_MAP = {
   // Notebook Editor > Action Button
   "--mb-color-notebook-step-bg": "questionEditor.actionButton.backgroundColor",
 
-  // Embedding SDK > Interactive Question > Toolbar (Default Layout)
+  // SDK > Interactive Question > Toolbar (Default Layout)
   "--mb-color-bg-sdk-question-toolbar": "question.toolbar.backgroundColor",
 
-  // Embedding SDK > Interactive Question > Notebook Editor Button
-  "--mb-color-bg-sdk-editor-button":
-    "questionEditor.editorButton.backgroundColor",
-  "--mb-color-bg-sdk-editor-button-active":
-    "questionEditor.editorButton.activeBackgroundColor",
-  "--mb-color-icon-sdk-editor-button": "questionEditor.editorButton.iconColor",
-  "--mb-color-icon-sdk-editor-button-active":
-    "questionEditor.editorButton.activeIconColor",
-  "--mb-color-border-sdk-editor-button":
-    "questionEditor.editorButton.borderColor",
-
-  // Embedding SDK > Collection Browser
-  "--mb-color-text-collection-browser-expand-button":
-    "collectionBrowser.breadcrumbs.expandButton.textColor",
-  "--mb-color-bg-collection-browser-expand-button":
-    "collectionBrowser.breadcrumbs.expandButton.backgroundColor",
-  "--mb-color-text-collection-browser-expand-button-hover":
-    "collectionBrowser.breadcrumbs.expandButton.hoverTextColor",
-  "--mb-color-bg-collection-browser-expand-button-hover":
-    "collectionBrowser.breadcrumbs.expandButton.hoverBackgroundColor",
-} satisfies Record<string, MetabaseComponentThemeKey>;
+  // Mappings for SDK elements
+  ...NOTEBOOK_EDITOR_THEME_OPTIONS,
+  ...COLLECTION_BROWSER_THEME_OPTIONS,
+} satisfies CssVariableToThemeMap;
