cfd5738e52d5e45f9025d538589363e0cf1ed78a
Kamil Mielnik <kamil@kamilmielnik.com>
2025-01-24 19:04:17 +0700

    Inline FilterTabContent into FilterModalBody

--- frontend/src/metabase/querying/filters/components/FilterModal/FilterModalBody/FilterModalBody.tsx
+++ frontend/src/metabase/querying/filters/components/FilterModal/FilterModalBody/FilterModalBody.tsx
@@ -1,14 +1,30 @@
+import { Flex, Tabs } from "metabase/ui";
+
 import { useFilterModalContext } from "../context";
 
-import { FilterTabContent } from "./FilterTabContent";
 import { FilterTabEmptyState } from "./FilterTabEmptyState";
+import { FilterTabList } from "./FilterTabList";
+import { FilterTabPanel } from "./FilterTabPanel";
 
 export const FilterModalBody = () => {
-  const { groupItems } = useFilterModalContext();
+  const { groupItems, setTab, tab, version, visibleItems } =
+    useFilterModalContext();
 
   if (groupItems.length === 0) {
     return <FilterTabEmptyState />;
   }
 
-  return <FilterTabContent />;
+  return (
+    <Tabs value={tab} onTabChange={setTab} orientation="vertical" h="100%">
+      <Flex direction="row" w="100%">
+        {visibleItems.length > 1 && <FilterTabList groupItems={visibleItems} />}
+        {visibleItems.map(groupItem => (
+          <FilterTabPanel
+            key={`${groupItem.key}:${version}`}
+            groupItem={groupItem}
+          />
+        ))}
+      </Flex>
+    </Tabs>
+  );
 };
