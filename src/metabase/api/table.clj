36fefd9c843f440397f1b480c64b4c07b833c537
Cam Saul <1455846+camsaul@users.noreply.github.com>
2025-01-24 12:20:02 -0800

    Update src/metabase/api/table.clj
    Co-authored-by: bryan <bryan.maass@gmail.com>

--- src/metabase/api/table.clj
+++ src/metabase/api/table.clj
@@ -406,10 +406,11 @@
    These options are provided for use in the Admin Edit Metadata page."
   [{:keys [id]} :- [:map
                     [:id ms/PositiveInt]]
-   {:keys [include_sensitive_fields include_hidden_fields include_editable_data_model]} :- [:map
-                                                                                            [:include_sensitive_fields    {:default false} [:maybe ms/BooleanValue]]
-                                                                                            [:include_hidden_fields       {:default false} [:maybe ms/BooleanValue]]
-                                                                                            [:include_editable_data_model {:default false} [:maybe ms/BooleanValue]]]]
+   {:keys [include_sensitive_fields include_hidden_fields include_editable_data_model]}
+   :- [:map
+       [:include_sensitive_fields    {:default false} [:maybe ms/BooleanValue]]
+       [:include_hidden_fields       {:default false} [:maybe ms/BooleanValue]]
+       [:include_editable_data_model {:default false} [:maybe ms/BooleanValue]]]]
   (fetch-table-query-metadata id {:include-sensitive-fields?    include_sensitive_fields
                                   :include-hidden-fields?       include_hidden_fields
                                   :include-editable-data-model? include_editable_data_model}))
