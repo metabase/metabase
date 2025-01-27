851e6ff7b2cbf38778c0cc4364f48be56b4e9570
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-01-27 09:54:32 -0500

    Fix types

--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -1,7 +1,5 @@
 import type { ReactNode } from "react";
 
-import type { AutocompleteMatchStyle } from "./autocomplete";
-
 export interface FormattingSettings {
   "type/Temporal"?: DateFormattingSettings;
   "type/Number"?: NumberFormattingSettings;
@@ -245,6 +243,8 @@ export interface OpenAiModel {
 }
 
 export type HelpLinkSetting = "metabase" | "hidden" | "custom";
+
+export type AutocompleteMatchStyle = "off" | "prefix" | "substring";
 
 export interface UploadsSettings {
   db_id: number | null;
