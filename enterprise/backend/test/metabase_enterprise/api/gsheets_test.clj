844c15b288d4819d1e24ffba83698181267b5722
Bryan Maass <bryan.maass@gmail.com>
2025-01-28 16:21:39 -0700

    indentation

--- enterprise/backend/test/metabase_enterprise/api/gsheets_test.clj
+++ enterprise/backend/test/metabase_enterprise/api/gsheets_test.clj
@@ -23,15 +23,15 @@
 (deftest ->config-good-test
   (testing "Both needed values are present and pulled from settings"
     (tu/with-temporary-setting-values
-        [api-key "mb_api_key_123"
-         store-api-url "http://store-api-url.com"]
+      [api-key "mb_api_key_123"
+       store-api-url "http://store-api-url.com"]
       (is (= {:store-api-url "http://store-api-url.com", :api-key "mb_api_key_123"}
              (#'gsheets.api/->config))))))
 
 (deftest ->config-missing-api-key-test
   (tu/with-temporary-setting-values
-      [api-key nil
-       store-api-url "http://store-api-url.com"]
+    [api-key nil
+     store-api-url "http://store-api-url.com"]
     (is (thrown-with-msg?
          Exception
          #"Missing api-key."
@@ -39,8 +39,8 @@
 
 (deftest ->config-missing-both-test
   (tu/with-temporary-setting-values
-      [api-key ""
-       store-api-url nil]
+    [api-key ""
+     store-api-url nil]
     (is (thrown-with-msg?
          Exception
          #"Missing api-key."
