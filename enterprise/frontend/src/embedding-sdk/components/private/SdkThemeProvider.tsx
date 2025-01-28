a419fdc884b9ad2de83aaa2140044807bd87b2c3
Phoomparin Mano <phoomparin@gmail.com>
2025-01-28 17:38:26 +0700

    memoize css variables

--- enterprise/frontend/src/embedding-sdk/components/private/SdkThemeProvider.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/SdkThemeProvider.tsx
@@ -45,5 +45,10 @@ function GlobalSdkCssVariables() {
   // the default is needed for when the sdk can't connect to the instance and get the default from there
   const font = useSelector(getFont) ?? DEFAULT_FONT;
 
-  return <Global styles={getMetabaseSdkCssVariables(theme, font)} />;
+  const styles = useMemo(
+    () => getMetabaseSdkCssVariables(theme, font),
+    [theme, font],
+  );
+
+  return <Global styles={styles} />;
 }
