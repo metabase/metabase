2fc40ef7bbd052e3d5424ab6a09b35b9c64fefb1
Sébastien <sebastien@metabase.com>
2025-01-28 11:01:07 +0100

    fixed unit test but should it be

--- frontend/src/metabase/visualizations/lib/text.unit.spec.ts
+++ frontend/src/metabase/visualizations/lib/text.unit.spec.ts
@@ -1,41 +1,50 @@
 import { measureTextWidth as measureDynamic } from "metabase/lib/measure-text";
 import { measureTextWidth as measureStatic } from "metabase/static-viz/lib/text";
 
-import type { TextWidthMeasurer } from "../shared/types/measure-text";
+import type { FontStyle } from "../shared/types/measure-text";
 
 import { truncateText } from "./text";
 
-const fontStyle = {
+const fontStyle: FontStyle = {
   size: 11,
   weight: 400,
   family: "Lato",
 };
 
-const tests: { name: string; measurer: TextWidthMeasurer }[] = [
-  { name: "truncateText - dynamic viz measurer", measurer: measureDynamic },
-  {
-    name: "truncateText - static viz measurer",
-    measurer: (text, style) =>
-      measureStatic(text, Number(style.size), Number(style.weight)),
-  },
-];
+describe("truncateText", () => {
+  const staticFn = (text: string, style: FontStyle) =>
+    measureStatic(text, +style.size, +style.weight);
 
-tests.map(test => {
-  describe(test.name, () => {
-    it("should not truncate text with ellipses if there is no overflow", () => {
-      expect(truncateText("John Doe", 48, test.measurer, fontStyle)).toBe(
-        "John Doe",
-      );
-    });
+  const dynamicFn = (text: string, style: FontStyle) =>
+    measureDynamic(text, style);
 
-    it("should truncate text with ellipses if there is overflow", () => {
-      expect(truncateText("John Doe", 48, test.measurer, fontStyle)).toBe(
-        "John Doe",
-      );
-    });
+  beforeEach(() => {
+    jest
+      .spyOn(HTMLElement.prototype, "clientWidth", "get")
+      .mockImplementation(function (this: HTMLElement) {
+        return this.innerHTML.length * 6;
+      });
+  });
 
-    it("should use ellipses in case there is no space for text at all", () => {
-      expect(truncateText("John Doe", 0, test.measurer, fontStyle)).toBe("…");
-    });
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("should not truncate text with ellipses if there is no overflow", () => {
+    expect(truncateText("John Doe", 48, dynamicFn, fontStyle)).toBe("John Doe");
+
+    expect(truncateText("John Doe", 48, staticFn, fontStyle)).toBe("John Doe");
+  });
+
+  it("should truncate text with ellipses if there is overflow", () => {
+    expect(truncateText("John Doe", 40, dynamicFn, fontStyle)).toBe("John…");
+
+    expect(truncateText("John Doe", 40, staticFn, fontStyle)).toBe("John…");
+  });
+
+  it("should use ellipses in case there is no space for text at all", () => {
+    expect(truncateText("John Doe", 0, dynamicFn, fontStyle)).toBe("…");
+
+    expect(truncateText("John Doe", 0, staticFn, fontStyle)).toBe("…");
   });
 });
