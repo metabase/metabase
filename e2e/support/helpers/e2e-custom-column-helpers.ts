ffe41ae603c21d248b23e9f39c4203ff402a4ea0
Romeo Van Snick <romeo@romeovansnick.be>
2025-01-29 16:15:24 +0100

    Fix custom expression helper

--- e2e/support/helpers/e2e-custom-column-helpers.ts
+++ e2e/support/helpers/e2e-custom-column-helpers.ts
@@ -8,7 +8,6 @@ export function expressionEditorWidget() {
  * @param {string=} option.name
  * @param {boolean} option.blur true by default. However, if you need to examine the popover in the test, it should be set to false so the popover is not dismissed
  */
-
 export function enterCustomColumnDetails({
   formula,
   name,
@@ -18,16 +17,24 @@ export function enterCustomColumnDetails({
   name?: string;
   blur?: boolean;
 }) {
-  cy.get(".ace_text-input")
+  cy.findByTestId("custom-expression-query-editor")
     .first()
     .as("formula")
-    .should("exist")
-    .focus()
-    .clear()
-    .type(formula);
+    .click();
+
+  // clear the expression editor
+  cy.realPress(["Meta", "A"]);
+  cy.realPress(["Backspace"]);
+
+  // Enter the formula.
+  // HACK: we do invoke("text") instead of type() because type() does not work on
+  // CodeMirror elements in Cypress. realType() would work but some of the formulas
+  // contain special characters that are not supported by realType().
+  cy.get("@formula").findByRole("textbox").invoke("text", formula);
 
   if (blur) {
-    cy.get("@formula").blur();
+    // click outside the expression editor
+    cy.get("label[for='expression-content']").click();
   }
 
   if (name) {
