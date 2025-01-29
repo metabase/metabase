6dbd1372448ddc29dc101ddddf2b83862f8e1166
Ryan Laurie <iethree@gmail.com>
2025-01-29 10:14:34 -0700

    another test order fix

--- e2e/test/scenarios/question/notebook-data-source.cy.spec.ts
+++ e2e/test/scenarios/question/notebook-data-source.cy.spec.ts
@@ -128,9 +128,9 @@ describe("scenarios > notebook > data source", () => {
         const schemaName = "Wild";
         const tableName = "Animals";
 
+        H.restore(`${dialect}-writable`);
         H.resetTestTable({ type: dialect, table: testTable1 });
         H.resetTestTable({ type: dialect, table: testTable2 });
-        H.restore(`${dialect}-writable`);
 
         cy.signInAsAdmin();
 
