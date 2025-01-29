323fec245ccb51d943c024bac98804f1416648d3
Phoomparin Mano <phoomparin@gmail.com>
2025-01-29 12:27:33 +0700

    make color tests dynamic and not hardcoded

--- e2e/test-component/scenarios/embedding-sdk/interactive-question-theming.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/interactive-question-theming.cy.spec.tsx
@@ -2,6 +2,7 @@ import {
   InteractiveQuestion,
   type MetabaseTheme,
 } from "@metabase/embedding-sdk-react";
+import Color from "color";
 
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import { createQuestion, describeEE } from "e2e/support/helpers";
@@ -14,6 +15,12 @@ import { getSdkRoot } from "e2e/support/helpers/e2e-embedding-sdk-helpers";
 import { Box } from "metabase/ui";
 
 const { ORDERS, ORDERS_ID } = SAMPLE_DATABASE;
+
+const darken = (color: string | undefined, amount: number) =>
+  Color(color).darken(amount).rgb().toString();
+
+const lighten = (color: string | undefined, amount: number) =>
+  Color(color).lighten(amount).rgb().toString();
 
 describeEE(
   "scenarios > embedding-sdk > interactive-question > theming",
@@ -63,35 +70,36 @@ describeEE(
       getSdkRoot().within(() => {
         cy.findByText("Product ID").should("be.visible");
 
-        const buttonHoverBg = "rgb(33, 39, 43)";
+        const buttonHoverBg = lighten(theme.colors?.background, 0.5);
+
         const customColumn = "[aria-label='Custom column']";
 
         // Should be the lightened version of the background color
         cy.findByTestId("notebook-button")
           .should("be.visible")
           .realHover()
-          .should("have.css", "background-color", buttonHoverBg);
+          .should($el => haveBackgroundColor($el, buttonHoverBg));
 
         // Should be the lightened version of the background color
         cy.findByTestId("chart-type-selector-button")
           .should("be.visible")
           .realHover()
-          .should("have.css", "background-color", buttonHoverBg);
+          .should($el => haveBackgroundColor($el, buttonHoverBg));
 
         cy.findByTestId("notebook-button").click();
 
         // Should be the lightened version of the background color, same as the notebook button hover.
-        cy.get(customColumn).should(
-          "have.css",
-          "background-color",
-          buttonHoverBg,
+        cy.get(customColumn).should($el =>
+          haveBackgroundColor($el, buttonHoverBg),
         );
 
         // Hover should be a less lightened version of the background color.
         cy.get(customColumn)
           .should("be.visible")
           .realHover()
-          .should("have.css", "background-color", "rgb(31, 36, 41)");
+          .should($el =>
+            haveBackgroundColor($el, lighten(theme.colors?.background, 0.4)),
+          );
       });
     });
 
@@ -123,18 +131,32 @@ describeEE(
         cy.findByTestId("notebook-button").click();
 
         // Should be the slightly darker version of the background color, same as the notebook button hover
-        cy.get(customColumn).should(
-          "have.css",
-          "background-color",
-          "rgb(242, 242, 242)",
+        cy.get(customColumn).should($el =>
+          haveBackgroundColor($el, darken(theme.colors?.background, 0.05)),
         );
 
         // Hover should be an even darker version of the background color
         cy.get(customColumn)
           .should("be.visible")
           .realHover()
-          .should("have.css", "background-color", "rgb(230, 230, 230)");
+          .should($el =>
+            haveBackgroundColor($el, darken(theme.colors?.background, 0.1)),
+          );
       });
     });
   },
 );
+
+/**
+ * Using should("have.css", "background-color") causes off-by-one error that causes the test to fail.
+ * For some reason, using getComputedStyle() directly produces the correct result.
+ **/
+export function haveBackgroundColor(
+  $element: JQuery,
+  expected: string,
+): boolean {
+  const element = $element[0];
+  const style = window.getComputedStyle(element);
+
+  return style.backgroundColor === expected;
+}
