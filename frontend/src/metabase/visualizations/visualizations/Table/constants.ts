b81a8b32a9f13c9ed65f0cfbb95ae127a2a70d9f
Aleksandr Lesnenko <alxnddr@gmail.com>
2025-01-28 23:09:04 -0500

    controlled column sizing state, add basic object detail, smooth resizing

--- frontend/src/metabase/visualizations/visualizations/Table/constants.ts
+++ frontend/src/metabase/visualizations/visualizations/Table/constants.ts
@@ -1,2 +1,4 @@
 export const ROW_HEIGHT = 36;
+export const HEADER_HEIGHT = 36;
 export const MIN_COLUMN_WIDTH = 60;
+export const INDEX_COLUMN_ID = `\0_INDEX`;
--- frontend/src/metabase/visualizations/visualizations/Table/hooks/use-virtual-grid.tsx
+++ frontend/src/metabase/visualizations/visualizations/Table/hooks/use-virtual-grid.tsx
@@ -1,45 +1,26 @@
-import type { ColumnDef, Table as ReactTable } from "@tanstack/react-table";
+import type { Table as ReactTable } from "@tanstack/react-table";
 import { useVirtualizer } from "@tanstack/react-virtual";
 import type React from "react";
-import { useCallback, useLayoutEffect, useMemo } from "react";
+import { useCallback, useLayoutEffect } from "react";
 import _ from "underscore";
 
-import type { DatasetData, RowValue, RowValues } from "metabase-types/api";
+import type { RowValues } from "metabase-types/api";
 
 import { ROW_HEIGHT } from "../constants";
-
-import type { CellMeasurer } from "./use-cell-measure";
-
-interface PivotedDatasetData extends DatasetData {
-  sourceRows: RowValues[];
-}
 
 interface UseVirtualGridProps {
   bodyRef: React.RefObject<HTMLDivElement>;
   table: ReactTable<RowValues>;
-  columns: ColumnDef<RowValues, RowValue>[];
-  data: DatasetData | PivotedDatasetData;
-  columnFormatters: ((value: RowValue) => React.ReactNode)[];
-  measureBodyCellDimensions: CellMeasurer;
-  isPivoted?: boolean;
+  measureRowHeight: (rowIndex: number) => number;
 }
 
 export const useVirtualGrid = ({
   bodyRef,
   table,
-  data,
-  columns,
-  columnFormatters,
-  measureBodyCellDimensions,
-  isPivoted = false,
+  measureRowHeight,
 }: UseVirtualGridProps) => {
-  const wrappedColumns = useMemo(() => {
-    return columns.filter(col => col.meta?.wrap);
-  }, [columns]);
-
   const { rows: tableRows } = table.getRowModel();
   const visibleColumns = table.getVisibleLeafColumns();
-
   const columnVirtualizer = useVirtualizer({
     count: visibleColumns.length,
     getScrollElement: () => bodyRef.current,
@@ -53,44 +34,14 @@ export const useVirtualGrid = ({
     getScrollElement: () => bodyRef.current,
     estimateSize: () => ROW_HEIGHT,
     overscan: 5,
-    getItemKey: index => tableRows[index].id,
     measureElement: element => {
       const rowIndexRaw = element?.getAttribute("data-index");
       const rowIndex = rowIndexRaw != null ? parseInt(rowIndexRaw, 10) : null;
-
-      if (
-        rowIndex == null ||
-        !isFinite(rowIndex) ||
-        wrappedColumns.length === 0
-      ) {
+      if (rowIndex == null || !isFinite(rowIndex)) {
         return ROW_HEIGHT;
       }
 
-      const height = Math.max(
-        ...wrappedColumns.map(column => {
-          const datasetIndex = column.meta?.datasetIndex;
-          if (!datasetIndex) {
-            return 0;
-          }
-
-          const value =
-            isPivoted && "sourceRows" in data
-              ? data.sourceRows[rowIndex][datasetIndex]
-              : data.rows[rowIndex][datasetIndex];
-          const formattedValue = columnFormatters[datasetIndex](value);
-          if (!formattedValue || _.isEmpty(formattedValue)) {
-            return ROW_HEIGHT;
-          }
-          const formattedString = formattedValue?.toString() ?? "";
-          const cellDimensions = measureBodyCellDimensions(
-            formattedString,
-            column.size,
-          );
-          return cellDimensions.height;
-        }, ROW_HEIGHT),
-      );
-
-      return height;
+      return measureRowHeight(rowIndex);
     },
   });
 
@@ -116,7 +67,7 @@ export const useVirtualGrid = ({
 
   useLayoutEffect(() => {
     measureGrid();
-  }, [columns, measureGrid]);
+  }, [measureRowHeight, measureGrid]);
 
   return {
     virtualColumns,
