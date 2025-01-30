cfdf668df7d3c0d56539e7d7980d27f56dc833b2
Kamil Mielnik <kamil@kamilmielnik.com>
2025-01-30 20:55:05 +0700

    Use translation

--- frontend/src/metabase/reference/components/Field.jsx
+++ frontend/src/metabase/reference/components/Field.jsx
@@ -119,7 +119,7 @@ const Field = ({ field, foreignKeys, url, icon, isEditing, formField }) => (
               <input
                 className={cx(F.fieldTextInput, CS.mb2, CS.mt1)}
                 type="text"
-                placeholder="Description"
+                placeholder={t`Description`}
                 {...formField.description}
                 defaultValue={field.description ?? ""}
               />
