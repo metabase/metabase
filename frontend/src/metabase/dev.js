962d38fb5b30f9c7df0342de8b150296479251ed
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2025-01-30 22:05:22 +0300

    fixup

--- frontend/src/metabase/dev.js
+++ frontend/src/metabase/dev.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
-if (process.env.ENABLE_CLJS_HOT_RELOAD !== "false") {
+if (process.env.ENABLE_CLJS_HOT_RELOAD === "true") {
   import("cljs/metabase.util.devtools");
 }
