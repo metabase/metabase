7eee6c1a154417935be37e2a4fb00748810a400e
Chris Truter <chris@metabase.com>
2025-01-30 18:05:57 +0200

    More robust handling of search negation

--- src/metabase/search/appdb/specialization/postgres.clj
+++ src/metabase/search/appdb/specialization/postgres.clj
@@ -55,7 +55,7 @@
       (str/join " <-> " <>))
 
     ;; negation
-    (str/starts-with? word-or-phrase "-")
+    (and (re-find #"^-[^-]" word-or-phrase))
     (str "!" (quote* (subs word-or-phrase 1)))
 
     ;; just a regular word
