d0b70d40956fb03dec42907e7ccfaa87c01d6db1
Kamil Mielnik <kamil@kamilmielnik.com>
2025-01-31 21:16:37 +0700

    Fix - Empty tooltip shown when save button is disabled (#53033)
    * Fix #51035
    
    * Add repro for #51035

--- e2e/test/scenarios/native/native-reproductions.cy.spec.ts
+++ e2e/test/scenarios/native/native-reproductions.cy.spec.ts
@@ -172,8 +172,14 @@ describe("issue 49454", () => {
     });
   });
 
-  it("should be possible to use metrics in native queries (metabase#49454)", () => {
-    H.startNewNativeQuestion().type("select * from {{ #test");
+  it("should be possible to use metrics in native queries (metabase#49454, metabase#51035)", () => {
+    H.startNewNativeQuestion();
+
+    cy.log("should not show empty tooltip (metabase#51035)");
+    cy.button("Save").realHover();
+    H.tooltip().should("not.exist");
+
+    H.nativeEditor().type("select * from {{ #test");
 
     H.nativeEditorCompletions().within(() => {
       cy.findByText("-question-49454").should("be.visible");
