137699ac8776eb6a23d279fb35e670219b0cec0a
Kamil Mielnik <kamil@kamilmielnik.com>
2025-01-31 16:14:13 +0700

    Fix #50266

--- frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetailView.tsx
+++ frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetailView.tsx
@@ -4,10 +4,8 @@ import { t } from "ttag";
 import _ from "underscore";
 
 import { ActionExecuteModal } from "metabase/actions/containers/ActionExecuteModal";
-import {
-  useActionListQuery,
-  useDatabaseListQuery,
-} from "metabase/common/hooks";
+import { skipToken, useListActionsQuery } from "metabase/api";
+import { useDatabaseListQuery } from "metabase/common/hooks";
 import { NotFound } from "metabase/components/ErrorPages";
 import LoadingSpinner from "metabase/components/LoadingSpinner";
 import Modal from "metabase/components/Modal";
@@ -250,10 +248,13 @@ export function ObjectDetailView({
     question.type() === "model" &&
     question.supportsImplicitActions();
 
-  const { data: actions = [] } = useActionListQuery({
-    enabled: areImplicitActionsEnabled,
-    query: { "model-id": question?.id() },
-  });
+  const modelId = question?.type() === "model" ? question.id() : undefined;
+
+  const { data: actions = [] } = useListActionsQuery(
+    areImplicitActionsEnabled && modelId != null
+      ? { "model-id": modelId }
+      : skipToken,
+  );
 
   const { data: databases = [] } = useDatabaseListQuery({
     enabled: areImplicitActionsEnabled,
