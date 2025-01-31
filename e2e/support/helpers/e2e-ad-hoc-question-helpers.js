c36d1e67d2e8c39c1919c5b680069ed7ef8fb15d
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-01-31 10:31:02 -0500

    Fix - Empty tooltip shown when save button is disabled (#53033) (#53039)
    * Fix #51035
    
    * Add repro for #51035
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- e2e/support/helpers/e2e-ad-hoc-question-helpers.js
+++ e2e/support/helpers/e2e-ad-hoc-question-helpers.js
@@ -96,7 +96,7 @@ function newNativeCardHash(
  *
  * @example
  * H.startNewNativeQuestion({ query: "SELECT * FROM ORDERS" });
- * @param {object} config
+ * @param {object=} config
  * @param {number} config.database
  * @param {string} config.query
  * @param {number} config.collection_id
