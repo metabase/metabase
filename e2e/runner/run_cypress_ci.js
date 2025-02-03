df12e253cb153bdd6394dba59837e662d76b2439
Ryan Laurie <iethree@gmail.com>
2025-02-03 13:03:52 -0700

    fix component test ci

--- e2e/runner/run_cypress_ci.js
+++ e2e/runner/run_cypress_ci.js
@@ -1,31 +1,28 @@
 const CypressBackend = require("./cypress-runner-backend");
 const runCypress = require("./cypress-runner-run-tests");
 const { printBold } = require("./cypress-runner-utils");
-const mode = process.argv[2];
+
+const mode = process.argv?.[2]?.trim();
+
+const availableModes = ["start", "snapshot", "e2e", "component"];
+
+if (!availableModes.includes(mode)) {
+  console.error(`Invalid mode: ${mode}`);
+  process.exit(1);
+}
 
 const startServer = async () => {
   printBold("Starting backend");
   await CypressBackend.start();
 };
 
-const snapshot = async () => {
-  printBold("Generating snapshots");
-  await runCypress("snapshot");
-};
-
-const runTests = async () => {
-  printBold("Running Cypress Tests");
-  await runCypress("e2e");
+const runTests = async suite => {
+  printBold(`Running ${suite} Cypress Tests`);
+  await runCypress(suite);
 };
 
 if (mode === "start") {
   startServer();
-}
-
-if (mode === "snapshot") {
-  snapshot();
-}
-
-if (mode === "test") {
-  runTests();
+} else {
+  runTests(mode);
 }
