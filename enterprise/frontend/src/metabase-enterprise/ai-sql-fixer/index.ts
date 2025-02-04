c6007af3337ef65146ec6134c0fe9f315041210c
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-02-04 10:41:40 -0500

    Fixes

--- enterprise/frontend/src/metabase-enterprise/ai-sql-fixer/index.ts
+++ enterprise/frontend/src/metabase-enterprise/ai-sql-fixer/index.ts
@@ -3,6 +3,6 @@ import { hasPremiumFeature } from "metabase-enterprise/settings";
 
 import { FixNativeQueryButton } from "./components/FixNativeQueryButton";
 
-if (hasPremiumFeature("metabot_v3")) {
+if (hasPremiumFeature("ai_sql_fixer")) {
   PLUGIN_AI_SQL_FIXER.FixNativeQueryButton = FixNativeQueryButton;
 }
