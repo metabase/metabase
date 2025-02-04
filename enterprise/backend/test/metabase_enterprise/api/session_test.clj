dc6538a4ccd5193dd26cf42f3ad0a94233658d8f
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-02-04 11:15:42 -0500

    Fix test

--- enterprise/backend/test/metabase_enterprise/api/session_test.clj
+++ enterprise/backend/test/metabase_enterprise/api/session_test.clj
@@ -57,7 +57,7 @@
             :hosting                        true
             :llm_autodescription            true
             :metabot_v3                     true
-            :ai-sql-fixer                   true
+            :ai_sql_fixer                   true
             :official_collections           true
             :query_reference_validation     true
             :sandboxes                      true
