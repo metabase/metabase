6c10cada98c9b5a4e89ce2bdc5258813525e4582
Tyler Plude <tsplude@gmail.com>
2025-02-05 15:09:10 -0400

    Working replication of valuesByKey map construction

--- src/metabase/pivot/js.cljs
+++ src/metabase/pivot/js.cljs
@@ -27,3 +27,14 @@
         col-settings (js->clj col-settings :keywordize-keys true)
         collapsed-subtotals (js->clj collapsed-subtotals)]
     (pivot/build-pivot-trees rows col-indexes row-indexes col-settings collapsed-subtotals)))
+
+(defn ^:export build-values-by-key
+  "TODO"
+  [rows col-indexes row-indexes val-indexes col-settings cols]
+  (let [rows (js->clj rows)
+        col-indexes (js->clj col-indexes)
+        row-indexes (js->clj row-indexes)
+        val-indexes (js->clj val-indexes)
+        col-settings (js->clj col-settings :keywordize-keys true)
+        cols (js->clj cols :keywordize-keys true)]
+    (clj->js (pivot/build-values-by-key rows col-indexes row-indexes val-indexes col-settings cols))))
