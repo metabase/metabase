077a2bda827e564324d3c9597ba1f3bf749ab1e5
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-02-05 18:57:24 +0100

    Use rem

--- frontend/src/metabase/admin/permissions/components/PermissionsEditor/PermissionsEditorContent.styled.tsx
+++ frontend/src/metabase/admin/permissions/components/PermissionsEditor/PermissionsEditorContent.styled.tsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const PermissionEditorContentRoot = styled.div`
   display: flex;
   flex-direction: column;
-  padding-left: 32px;
+  padding-left: 2rem;
   height: 100%;
 `;
 
