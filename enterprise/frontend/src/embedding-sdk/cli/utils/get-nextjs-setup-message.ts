745977aab5c0e6ee0a03ec5fc981d899bf04e171
Phoomparin Mano <phoomparin@gmail.com>
2025-02-10 19:48:26 +0700

    cleanup messages helper

--- enterprise/frontend/src/embedding-sdk/cli/utils/get-nextjs-setup-message.ts
+++ enterprise/frontend/src/embedding-sdk/cli/utils/get-nextjs-setup-message.ts
@@ -20,29 +20,29 @@ export const getNextJsSetupMessages = async ({
   const isInTypeScriptProject = await checkIsInTypeScriptProject();
   const componentExtension = isInTypeScriptProject ? "tsx" : "js";
 
-  const snippets = [];
+  const messages = [];
 
-  let componentSnippet = `Added an analytics-demo route to your "${green(router)}" directory.\n`;
+  const layoutFile = `${router === "app" ? "app/layout" : "pages/_app"}.${componentExtension}`;
+
+  let componentMessage = `Added an analytics-demo route to your "${green(router)}" directory.\n`;
 
   if (hasNextJsCustomAppOrRootLayout) {
     const layoutSnippet =
       await getNextJsCustomAppOrRootLayoutSnippet(componentPath);
 
-    const layoutFile = `${router === "app" ? "app/layout" : "pages/_app"}.${componentExtension}`;
-
     // If the user already has an _app.tsx or layout.tsx, we need to show them the snippet,
     // so they can add the example providers and the example CSS to their file.
-    componentSnippet += `Next, add the example providers and the example CSS to your ${green(layoutFile)} file. For example:
+    componentMessage += `  Next, add the example providers and the example CSS to your ${green(layoutFile)} file. For example:
 
   ${green(layoutSnippet)}`;
   } else {
     // Otherwise, we tell them that we've generated the needed files.
-    componentSnippet += `An example Next.js ${router === "app" ? "root layout" : "custom app"} is also added to your ${router} directory.`;
+    componentMessage += `  An example Next.js ${layoutFile} is also added to your ${router} directory.`;
   }
 
-  snippets.push(componentSnippet);
+  messages.push(componentMessage);
 
-  snippets.push(`
+  messages.push(`
   Instead of having a separate Express.js server, you can create API routes
   for them. See the examples from ${green(LINK_TO_NEXT_JS_SAMPLE)}.
 
@@ -50,5 +50,5 @@ export const getNextJsSetupMessages = async ({
   ${green(LINK_TO_NEXT_JS_GUIDE)}
   `);
 
-  return snippets.map(snippet => snippet.trim());
+  return messages.map(snippet => snippet.trim());
 };
