74837f0a240c45a3381bd9dd6b0f0f5444815b50
Bryan Maass <bryan.maass@gmail.com>
2025-02-10 10:59:01 -0700

    cleanup

--- dev/src/dev/modularization_help.clj
+++ dev/src/dev/modularization_help.clj
@@ -2,7 +2,10 @@
   (:require
    [clojure.string :as str]))
 
-(defn potempkin-ns! []
+(defn potempkin-ns!
+  "Call this inside a namespace to gather the public vars, sort them, and return them as a vector, usually this is
+  exactly what you put into potempkin's `import-vars`."
+  []
   (let [ns-symbol (symbol (str *ns*))]
     (into [(symbol (last (str/split (str ns-symbol) #"\.")))]
           (sort (keys (ns-publics ns-symbol))))))
