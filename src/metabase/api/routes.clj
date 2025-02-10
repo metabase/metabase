60e08be85713fd172d58c20cb81b9f1cc946fe80
Cam Saul <1455846+camsaul@users.noreply.github.com>
2025-02-10 09:41:54 -0800

    [Backend modularization] `user-key-value` module (#53416)

--- src/metabase/api/routes.clj
+++ src/metabase/api/routes.clj
@@ -45,7 +45,6 @@
    [metabase.api.testing]
    [metabase.api.tiles]
    [metabase.api.user]
-   [metabase.api.user-key-value]
    [metabase.api.util]
    [metabase.api.util.handlers :as handlers]
    [metabase.channel.api]
@@ -55,6 +54,7 @@
    [metabase.setup.api]
    [metabase.sync.api]
    [metabase.timeline.api]
+   [metabase.user-key-value.api]
    [metabase.util.i18n :refer [deferred-tru]]))
 
 (comment metabase.api.action/keep-me
@@ -92,10 +92,10 @@
          metabase.api.testing/keep-me
          metabase.api.tiles/keep-me
          metabase.api.user/keep-me
-         metabase.api.user-key-value/keep-me
          metabase.api.util/keep-me
          metabase.permissions.api/keep-me
-         metabase.setup.api/keep-me)
+         metabase.setup.api/keep-me
+         metabase.user-key-value.api/keep-me)
 
 (def ^:private ^{:arglists '([request respond raise])} pass-thru-handler
   "Always 'falls thru' to the next handler."
@@ -180,7 +180,7 @@
    "/timeline"             (+auth metabase.timeline.api/timeline-routes)
    "/timeline-event"       (+auth metabase.timeline.api/timeline-event-routes)
    "/user"                 (+auth 'metabase.api.user)
-   "/user-key-value"       (+auth 'metabase.api.user-key-value)
+   "/user-key-value"       (+auth 'metabase.user-key-value.api)
    "/util"                 'metabase.api.util})
 ;;; ↑↑↑ KEEP THIS SORTED OR ELSE ↑↑↑
 
