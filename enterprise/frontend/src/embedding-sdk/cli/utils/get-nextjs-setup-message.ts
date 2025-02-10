2942e5d16d1028a796b757528b25f48a14de785a
Phoomparin Mano <phoomparin@gmail.com>
2025-02-10 20:07:25 +0700

    add more info on the default provider

--- enterprise/frontend/src/embedding-sdk/cli/utils/get-nextjs-setup-message.ts
+++ enterprise/frontend/src/embedding-sdk/cli/utils/get-nextjs-setup-message.ts
@@ -20,11 +20,11 @@ export const getNextJsSetupMessages = async ({
   const isInTypeScriptProject = await checkIsInTypeScriptProject();
   const componentExtension = isInTypeScriptProject ? "tsx" : "js";
 
-  const messages = [];
+  const snippets = [];
 
   const layoutFile = `${router === "app" ? "app/layout" : "pages/_app"}.${componentExtension}`;
 
-  let componentMessage = `Added an analytics-demo route to your "${green(router)}" directory.\n`;
+  let componentSnippet = `Added an analytics-demo route to your "${green(router)}" directory.\n`;
 
   if (hasNextJsCustomAppOrRootLayout) {
     const layoutSnippet =
@@ -32,17 +32,17 @@ export const getNextJsSetupMessages = async ({
 
     // If the user already has an _app.tsx or layout.tsx, we need to show them the snippet,
     // so they can add the example providers and the example CSS to their file.
-    componentMessage += `  Next, add the example providers and the example CSS to your ${green(layoutFile)} file. For example:
+    componentSnippet += `  Next, add the example providers and CSS stylesheet to your ${green(layoutFile)} file. For example:
 
   ${green(layoutSnippet)}`;
   } else {
     // Otherwise, we tell them that we've generated the needed files.
-    componentMessage += `  An example Next.js ${layoutFile} is also added to your ${router} directory.`;
+    componentSnippet += `  An example Next.js ${layoutFile} is also added with the example providers and CSS stylesheet.`;
   }
 
-  messages.push(componentMessage);
+  snippets.push(componentSnippet);
 
-  messages.push(`
+  snippets.push(`
   Instead of having a separate Express.js server, you can create API routes
   for them. See the examples from ${green(LINK_TO_NEXT_JS_SAMPLE)}.
 
@@ -50,5 +50,5 @@ export const getNextJsSetupMessages = async ({
   ${green(LINK_TO_NEXT_JS_GUIDE)}
   `);
 
-  return messages.map(snippet => snippet.trim());
+  return snippets.map(snippet => snippet.trim());
 };
