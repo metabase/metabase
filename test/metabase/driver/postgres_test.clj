98ca1bcecb59079e2cc2daf016bcdcdaf86c484b
Riley Thompson <rileythompson99@gmail.com>
2025-02-11 10:36:06 -0500

    remove let

--- test/metabase/driver/postgres_test.clj
+++ test/metabase/driver/postgres_test.clj
@@ -1669,14 +1669,13 @@
   (testing "An aggregated array column should be returned in a readable format"
     (mt/test-driver :postgres
       (mt/dataset test-data
-        (let [rows (->> (mt/native-query {:query "select category_id, array_agg(name)
-                                                  from venues
-                                                  group by category_id
-                                                  order by 1 asc
-                                                  limit 2;"})
-                        mt/process-query
-                        mt/rows
-                        (map second))]
-          (is (= [["The Gorbals" "The Misfit Restaurant + Bar" "Marlowe" "Yamashiro Hollywood" "Musso & Frank Grill" "Pacific Dining Car" "Chez Jay" "Rush Street"]
-                  ["Greenblatt's Delicatessen & Fine Wine Shop" "Handy Market"]]
-                 rows)))))))
+        (is (= [["The Gorbals" "The Misfit Restaurant + Bar" "Marlowe" "Yamashiro Hollywood" "Musso & Frank Grill" "Pacific Dining Car" "Chez Jay" "Rush Street"]
+                ["Greenblatt's Delicatessen & Fine Wine Shop" "Handy Market"]]
+               (->> (mt/native-query {:query "select category_id, array_agg(name)
+                                              from venues
+                                              group by category_id
+                                              order by 1 asc
+                                              limit 2;"})
+                    mt/process-query
+                    mt/rows
+                    (map second))))))))
