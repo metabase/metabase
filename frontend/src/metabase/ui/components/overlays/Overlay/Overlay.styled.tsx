02cfb17bced439f66c6b60dd885c6b23da3459ea
Sébastien <sebastien@metabase.com>
2025-02-11 10:25:11 +0100

    an attempt te reduce the spam in the dev console (#53214)

--- frontend/src/metabase/ui/components/overlays/Overlay/Overlay.styled.tsx
+++ frontend/src/metabase/ui/components/overlays/Overlay/Overlay.styled.tsx
@@ -1,12 +1,13 @@
-import type { MantineThemeOverride } from "@mantine/core";
+import type { MantineThemeOverride, OverlayProps } from "@mantine/core";
 
 import ZIndex from "metabase/css/core/z-index.module.css";
 
+// Putting this here ensures it's typed correctly
+const defaultProps: OverlayProps = {};
+
 export const getOverlayOverrides = (): MantineThemeOverride["components"] => ({
   Overlay: {
-    defaultProps: {
-      withinPortal: true,
-    },
+    defaultProps,
     classNames: { root: ZIndex.Overlay },
   },
 });
