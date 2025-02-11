68b79ba103fcccdb60a78a0d82e1b47c4a3dbb73
Phoomparin Mano <poom@metabase.com>
2025-02-11 10:06:28 +0700

    update cli startup message
    Co-authored-by: Jeff Bruemmer <jeff.bruemmer@gmail.com>

--- enterprise/frontend/src/embedding-sdk/cli/constants/messages.ts
+++ enterprise/frontend/src/embedding-sdk/cli/constants/messages.ts
@@ -7,13 +7,11 @@ import {
 } from "./config";
 
 export const SHOW_ON_STARTUP_MESSAGE = `
-  This will spin up a local Metabase instance via Docker and help you get
-  an embedded dashboard in your app. You cannot connect to an existing
-  Metabase instance using this tool.
-
-  Note that the default setup with API keys won’t work in production.
-  It's only intended for you to quickly try out the SDK on your local machine.
-  A production setup requires a Pro/Enterprise license and SSO with JWT.
+  This tool will spin up a local Metabase instance via Docker and help you get
+  an embedded dashboard in your app.
+  
+  - You can't use this tool to connect to an existing Metabase instance.
+  - The tool's default setup (which uses API keys) won’t work in production. It's only intended for you to quickly try out the SDK on your local machine. A production setup requires a Pro/Enterprise license and SSO with JWT.
 `;
 
 export const PACKAGE_JSON_NOT_FOUND_MESSAGE = `
