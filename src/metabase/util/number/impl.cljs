0709a43cef55138ecb72f1ca4b7b2c7112b8480a
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-02-10 21:04:07 -0500

    Fix bigint issue

--- src/metabase/util/number/impl.cljs
+++ src/metabase/util/number/impl.cljs
@@ -1,6 +1,7 @@
 (ns metabase.util.number.impl
   "CLJS implementation of the number utilities.
-  See [[metabase.util.number]] for the public interface.")
+  See [[metabase.util.number]] for the public interface."
+  (:refer-clojure :exclude [bigint]))
 
 (defn bigint
   "Coerce a number or a string to BigInt. Throws if coercion cannot be made."
