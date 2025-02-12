22dfd36a102a564f776aa4ca968ba12cf55f71cd
Sébastien <sebastien@metabase.com>
2025-02-12 12:12:09 +0100

    fixed test

--- frontend/src/metabase/visualizations/echarts/tooltip/index.unit.spec.ts
+++ frontend/src/metabase/visualizations/echarts/tooltip/index.unit.spec.ts
@@ -16,6 +16,7 @@ describe("useCloseTooltipOnScroll", () => {
     chartRefMock = {
       current: {
         dispatchAction: jest.fn(),
+        isDisposed: () => false,
       } as unknown as EChartsType,
     };
     jest.clearAllMocks();
