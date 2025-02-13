2bccb68c91ae23f764859a96a62f6b57ffba511d
Phoomparin Mano <phoomparin@gmail.com>
2025-02-13 19:08:35 +0700

    use people, orders and products

--- enterprise/frontend/src/embedding-sdk/cli/constants/config.ts
+++ enterprise/frontend/src/embedding-sdk/cli/constants/config.ts
@@ -23,8 +23,4 @@ export const SANDBOXED_GROUP_NAMES = ["Customer A", "Customer B", "Customer C"];
 export const GENERATED_COMPONENTS_DEFAULT_PATH = "./src/components/metabase";
 
 // Tables from the sample database that are selected by default.
-export const SAMPLE_DATABASE_SELECTED_TABLES = [
-  "ORDERS",
-  "PRODUCTS",
-  "ACCOUNTS",
-];
+export const SAMPLE_DATABASE_SELECTED_TABLES = ["PEOPLE", "ORDERS", "PRODUCTS"];
