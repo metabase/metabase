842fa1a0fe8201b68db5a23ef7ac036213c25fad
Kamil Mielnik <kamil@kamilmielnik.com>
2025-02-13 18:10:50 +0700

    Fix no-unused-vars ESLint rule config (#53648)
    * Fix no-unused-vars config by adding sole underscore checks
    
    * Fix unused underscores

--- frontend/src/metabase/query_builder/components/template_tags/TagEditorParamParts/DefaultRequiredValueControl.tsx
+++ frontend/src/metabase/query_builder/components/template_tags/TagEditorParamParts/DefaultRequiredValueControl.tsx
@@ -1,5 +1,4 @@
 import { t } from "ttag";
-import _ from "underscore";
 
 import { RequiredParamToggle } from "metabase/parameters/components/RequiredParamToggle";
 import { Flex, Text } from "metabase/ui";
