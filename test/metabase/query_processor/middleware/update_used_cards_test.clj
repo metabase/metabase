60cd1ba664a63c18f7b360b56b2a16b6cdda7e83
Edward Paget <ed.paget@gmail.com>
2025-02-13 09:28:29 -0600

    fixup! use temporary settings helper

--- test/metabase/query_processor/middleware/update_used_cards_test.clj
+++ test/metabase/query_processor/middleware/update_used_cards_test.clj
@@ -110,15 +110,12 @@
                (-> (t2/select-one-fn :last_used_at :model/Card card-id-2)
                    t/offset-date-time
                    (.withNano 0))))))
-    (testing "if metabase is in read only mode still set the last_used_at timestamp"
+    (testing "if metabase is in read only mode still sets the last_used_at timestamp"
       (mt/with-temp
         [:model/Card {card-id-1 :id} {:last_used_at one-hour-ago}]
-        (try
-          (cloud-migration/read-only-mode! true)
+        (mt/with-temporary-setting-values [read-only-mode true]
           (#'qp.update-used-cards/update-used-cards!* [{:id card-id-1 :timestamp now}])
-          (cloud-migration/read-only-mode! false)
           (is (= now
                  (-> (t2/select-one-fn :last_used_at :model/Card card-id-1)
                      t/offset-date-time
-                     (.withNano 0))))
-          (finally (cloud-migration/read-only-mode! false)))))))
+                     (.withNano 0)))))))))
