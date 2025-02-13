f1f611c7799891fc6f740492b9c4592d344df54b
Nathan Voxland <nathan@metabase.com>
2025-02-13 09:28:51 -0600

    Added test for database caching

--- test/metabase/api/database_test.clj
+++ test/metabase/api/database_test.clj
@@ -876,23 +876,23 @@
 (deftest ^:parallel databases-caching
   (testing "GET /api/database"
     (testing "Testing that listing all databases does not make excessive queries with multiple databases"
-        (mt/with-temp [:model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}
-                       :model/Database _ {:engine ::test-driver}]
-          (t2/with-call-count [call-count]
-            (mt/user-http-request :rasta :get 200 "database")
-            (is (< (call-count) 10)))))))
+      (mt/with-temp [:model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}
+                     :model/Database _ {:engine ::test-driver}]
+        (t2/with-call-count [call-count]
+          (mt/user-http-request :rasta :get 200 "database")
+          (is (< (call-count) 10)))))))
 
 (deftest ^:parallel databases-list-test-2
   (testing "GET /api/database"
