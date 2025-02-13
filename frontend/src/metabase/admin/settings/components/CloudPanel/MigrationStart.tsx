9cf3bb1d1a378ae5850fa05b7af76589fad20b4f
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-02-13 15:52:22 +0100

    Use the cloud upsell instead of the previous CTA

--- frontend/src/metabase/admin/settings/components/CloudPanel/MigrationStart.tsx
+++ frontend/src/metabase/admin/settings/components/CloudPanel/MigrationStart.tsx
@@ -1,12 +1,10 @@
 import { t } from "ttag";
 
-import { getPlan } from "metabase/common/utils/plan";
+import { UpsellCloud } from "metabase/admin/upsells/UpsellCloud";
 import ExternalLink from "metabase/core/components/ExternalLink";
 import { useToggle } from "metabase/hooks/use-toggle";
 import { color } from "metabase/lib/colors";
-import { useSelector } from "metabase/lib/redux";
-import { getSetting } from "metabase/selectors/settings";
-import { Box, Button, Icon, Modal, Text } from "metabase/ui";
+import { Button, Icon, Modal, Text } from "metabase/ui";
 
 interface MigrationStartProps {
   startNewMigration: () => void;
@@ -20,27 +18,9 @@ export const MigrationStart = ({
   const [isModalOpen, { turnOn: openModal, turnOff: closeModal }] =
     useToggle(false);
 
-  const plan = useSelector(state =>
-    getPlan(getSetting(state, "token-features")),
-  );
-  const isProSelfHosted = plan === "pro-self-hosted";
-
   return (
     <>
-      <Box mt="1rem">
-        <Text size="md">
-          {isProSelfHosted
-            ? t`Migrate this instance to Metabase Cloud at no extra cost and get high availability, automatic upgrades, backups, and enterprise grade compliance.`
-            : t`Migrate this instance to Metabase Cloud with a free 14-day trial and get high availability, automatic upgrades, backups, and official support.`}{" "}
-          <ExternalLink href="https://www.metabase.com/cloud/">{t`Learn More.`}</ExternalLink>
-        </Text>
-
-        <Button
-          mt="2rem"
-          variant="filled"
-          onClick={openModal}
-        >{t`Get started`}</Button>
-      </Box>
+      <UpsellCloud onOpenModal={openModal} source="settings-cloud" />
 
       <Modal.Root
         opened={isModalOpen}
