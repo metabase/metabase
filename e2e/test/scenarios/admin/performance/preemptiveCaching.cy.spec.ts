fad73bee12aa7acfebfd033585101cbf09215334
Noah Moss <noahbmoss@gmail.com>
2025-02-13 19:30:24 -0500

    Refactor cypress test

--- e2e/test/scenarios/admin/performance/preemptiveCaching.cy.spec.ts
+++ e2e/test/scenarios/admin/performance/preemptiveCaching.cy.spec.ts
@@ -1,14 +1,17 @@
 const { H } = cy;
 
 import { WRITABLE_DB_ID } from "e2e/support/cypress_data";
-import { createQuestion, getTableId, visitQuestion } from "e2e/support/helpers";
+import {
+  createNativeQuestion,
+  createQuestion,
+  getTableId,
+  visitQuestion,
+} from "e2e/support/helpers";
 
-import { TEST_TABLE, sampleNativeQuestion } from "./helpers/constants";
 import {
   advanceServerClockBy,
   interceptPerformanceRoutes,
   resetServerTime,
-  setupQuestionTest,
 } from "./helpers/e2e-performance-helpers";
 import {
   durationRadioButton,
@@ -32,6 +35,7 @@ describe(
         H.restore("postgres-writable");
         cy.signInAsAdmin();
         H.setTokenFeatures("all");
+
         H.resetTestTable({ type: "postgres", table: WRITABLE_TEST_TABLE });
         H.resyncDatabase({
           dbId: WRITABLE_DB_ID,
@@ -39,8 +43,49 @@ describe(
         });
       });
 
+      function visitCachedQuestion(questionId: string) {
+        cy.then(function () {
+          cy.log("Visiting the question");
+          visitQuestion(questionId);
+        });
+      }
+
+      function runPreemptiveCachingTest(questionId: string) {
+        visitCachedQuestion(questionId);
+        cy.findByTestId("visualization-root").findByText("Amorous Aardvarks");
+        openSidebarCacheStrategyForm("question");
+        cy.findByLabelText(/Use default/).click();
+        durationRadioButton().click();
+        enablePreemptiveCaching();
+        saveCacheStrategyForm({
+          strategyType: "duration",
+          model: "question",
+        });
+
+        // Refreshing the question causes us to cache it
+        visitCachedQuestion(questionId);
+        cy.findByTestId("visualization-root").findByText("Amorous Aardvarks");
+
+        // Value changes in the DB
+        H.queryWritableDB(
+          `UPDATE ${WRITABLE_TEST_TABLE} SET team_name = 'New Value' where id = 1;`,
+        );
+
+        // Check that old value is still cached
+        advanceServerClockBy(CACHE_DURATION_MS - 1000);
+        visitCachedQuestion(questionId);
+        cy.findByTestId("visualization-root").findByText("Amorous Aardvarks");
+
+        // Advance server clock more, and then trigger automatic cache refresh
+        advanceServerClockBy(2000);
+        cy.log("Triggering cache refresh task");
+        cy.request("POST", "/api/testing/refresh-caches");
+
+        visitCachedQuestion(questionId);
+        cy.findByTestId("visualization-root").findByText("New Value");
+      }
+
       it("Returns matching results when an MBQL query is cached preemptively", () => {
-        // Using WRITABLE_TEST_TABLE because creating a question using TEST_TABLE doesn't work...
         getTableId({
           databaseId: WRITABLE_DB_ID,
           name: WRITABLE_TEST_TABLE,
@@ -56,80 +101,30 @@ describe(
             },
             {
               wrapId: true,
-              idAlias: "questionId",
+              idAlias: "mbqlQuestionId",
             },
           );
-          const visitItem = () =>
-            cy.then(function () {
-              cy.log("Visiting the question");
-              visitQuestion("@questionId");
-            });
-
-          visitItem();
-
-          cy.findByTestId("visualization-root").findByText("Amorous Aardvarks");
-          openSidebarCacheStrategyForm("question");
-          cy.findByLabelText(/Use default/).click();
-          durationRadioButton().click();
-          enablePreemptiveCaching();
-          saveCacheStrategyForm({
-            strategyType: "duration",
-            model: "question",
-          });
-
-          // Refreshing the question causes us to cache it
-          visitItem();
-          cy.findByTestId("visualization-root").findByText("Amorous Aardvarks");
-
-          // Value changes in the DB
-          H.queryWritableDB(
-            `UPDATE ${WRITABLE_TEST_TABLE} SET team_name = 'New Value' where id = 1;`,
-          );
-
-          // Check that old value is still cached
-          advanceServerClockBy(CACHE_DURATION_MS - 1000);
-          visitItem();
-          cy.findByTestId("visualization-root").findByText("Amorous Aardvarks");
-
-          // Advance server clock more, and then trigger automatic cache refresh
-          advanceServerClockBy(2000);
-          cy.log("Triggering cache refresh task");
-          cy.request("POST", "/api/testing/refresh-caches");
-
-          visitItem();
-          cy.findByTestId("visualization-root").findByText("New Value");
         });
+
+        runPreemptiveCachingTest("@mbqlQuestionId");
       });
 
       it("Returns matching results when a native query is cached preemptively", () => {
-        const { visitItem } = setupQuestionTest(sampleNativeQuestion);
-        visitItem();
-        cy.findByTestId("visualization-root").findByText("Old Value");
-        openSidebarCacheStrategyForm("question");
-        cy.findByLabelText(/Use default/).click();
-        durationRadioButton().click();
-        enablePreemptiveCaching();
-        saveCacheStrategyForm({ strategyType: "duration", model: "question" });
+        createNativeQuestion(
+          {
+            name: "Cached question",
+            database: WRITABLE_DB_ID,
+            native: {
+              query: `SELECT * FROM ${WRITABLE_TEST_TABLE};`,
+            },
+          },
+          {
+            wrapId: true,
+            idAlias: "nativeQuestionId",
+          },
+        );
 
-        // Refreshing the question causes us to cache it
-        visitItem();
-        cy.findByTestId("visualization-root").findByText("Old Value");
-
-        // Value changes in the DB
-        H.queryWritableDB(`UPDATE ${TEST_TABLE} SET my_text='New Value';`);
-
-        // Check that old value is still cached
-        advanceServerClockBy(CACHE_DURATION_MS - 1000);
-        visitItem();
-        cy.findByTestId("visualization-root").findByText("Old Value");
-
-        // Advance server clock more, and then trigger automatic cache refresh
-        advanceServerClockBy(2000);
-        cy.log("Triggering cache refresh task");
-        cy.request("POST", "/api/testing/refresh-caches");
-
-        visitItem();
-        cy.findByTestId("visualization-root").findByText("New Value");
+        runPreemptiveCachingTest("@nativeQuestionId");
       });
     });
   },
