12789706f606ce45c2ff23eb6893e2e7a30fa870
Cam Saul <github@camsaul.com>
2025-02-15 01:44:24 +0000

    One more pass

--- src/metabase/public_settings.clj
+++ src/metabase/public_settings.clj
@@ -790,15 +790,6 @@ See [fonts](../configuring-metabase/fonts.md).")
   :default    false
   :export?    false)
 
-(defn remove-public-uuid-if-public-sharing-is-disabled
-  "If public sharing is *disabled* and `object` has a `:public_uuid`, remove it so people don't try to use it (since it
-  won't work). Intended for use as part of a `post-select` implementation for Cards and Dashboards."
-  [object]
-  (if (and (:public_uuid object)
-           (not (enable-public-sharing)))
-    (assoc object :public_uuid nil)
-    object))
-
 (defsetting available-fonts
   "Available fonts"
   :visibility :public
--- test/metabase/public_sharing/api_test.clj
+++ test/metabase/public_sharing/api_test.clj
@@ -10,7 +10,6 @@
    [metabase.analytics.stats :as stats]
    [metabase.api.card-test :as api.card-test]
    [metabase.api.dashboard-test :as api.dashboard-test]
-   [metabase.public-sharing.api :as api.public]
    [metabase.config :as config]
    [metabase.http-client :as client]
    [metabase.models.interface :as mi]
@@ -18,6 +17,7 @@
    [metabase.models.params.chain-filter-test :as chain-filter-test]
    [metabase.permissions.models.permissions :as perms]
    [metabase.permissions.models.permissions-group :as perms-group]
+   [metabase.public-sharing.api :as api.public]
    [metabase.query-processor :as qp]
    [metabase.query-processor.middleware.process-userland-query-test :as process-userland-query-test]
    [metabase.query-processor.pivot.test-util :as api.pivots]
@@ -1649,7 +1649,7 @@
                (let [result (results)]
                  (is (=? {:status "completed"}
                          result))
-                      ;; [[metabase.api.public/transform-results]] should remove `row_count`
+                      ;; [[metabase.public-sharing.api/transform-results]] should remove `row_count`
                  (testing "row_count isn't included in public endpoints"
                    (is (nil? (:row_count result))))
                  (is (= 6 (count (get-in result [:data :cols]))))
