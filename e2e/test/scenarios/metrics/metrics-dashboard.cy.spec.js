e380d347a6e655d0a4f81d8d28da0d3f29e8b7fb
Filip Hric <filip@filiphric.sk>
2025-02-17 09:58:51 +0100

    Dev 344 remove flaky tag from tests that are stable (#53669)
    * add eslint rule that prevents incorrect order of helpers
    
    * remove flaky tags and stabilize tests

--- e2e/test/scenarios/metrics/metrics-dashboard.cy.spec.js
+++ e2e/test/scenarios/metrics/metrics-dashboard.cy.spec.js
@@ -116,32 +116,28 @@ describe("scenarios > metrics > dashboard", () => {
     );
   });
 
-  it(
-    "should be possible to add metrics to a dashboard",
-    { tags: "@flaky" },
-    () => {
-      H.createQuestion(ORDERS_SCALAR_METRIC);
-      H.createQuestion(ORDERS_TIMESERIES_METRIC);
-      H.visitDashboard(ORDERS_DASHBOARD_ID);
-      H.editDashboard();
-      H.openQuestionsSidebar();
-      cy.findByTestId("add-card-sidebar").within(() => {
-        cy.findByText(ORDERS_SCALAR_METRIC.name).click();
-        cy.findByPlaceholderText("Search…").type(ORDERS_TIMESERIES_METRIC.name);
-        cy.wait("@search");
-        cy.findByText(ORDERS_SCALAR_METRIC.name).should("not.exist");
-        cy.findByText(ORDERS_TIMESERIES_METRIC.name).click();
-      });
-      H.getDashboardCard(1).within(() => {
-        cy.findByText(ORDERS_SCALAR_METRIC.name).should("be.visible");
-        cy.findByText("18,760").should("be.visible");
-      });
-      H.getDashboardCard(2).within(() => {
-        cy.findByText(ORDERS_TIMESERIES_METRIC.name).should("be.visible");
-        H.echartsContainer().should("be.visible");
-      });
-    },
-  );
+  it("should be possible to add metrics to a dashboard", () => {
+    H.createQuestion(ORDERS_SCALAR_METRIC);
+    H.createQuestion(ORDERS_TIMESERIES_METRIC);
+    H.visitDashboard(ORDERS_DASHBOARD_ID);
+    H.editDashboard();
+    H.openQuestionsSidebar();
+    cy.findByTestId("add-card-sidebar").within(() => {
+      cy.findByText(ORDERS_SCALAR_METRIC.name).click();
+      cy.findByPlaceholderText("Search…").type(ORDERS_TIMESERIES_METRIC.name);
+      cy.wait("@search");
+      cy.findByText(ORDERS_SCALAR_METRIC.name).should("not.exist");
+      cy.findByText(ORDERS_TIMESERIES_METRIC.name).click();
+    });
+    H.getDashboardCard(1).within(() => {
+      cy.findByText(ORDERS_SCALAR_METRIC.name).should("be.visible");
+      cy.findByText("18,760").should("be.visible");
+    });
+    H.getDashboardCard(2).within(() => {
+      cy.findByText(ORDERS_TIMESERIES_METRIC.name).should("be.visible");
+      H.echartsContainer().should("be.visible");
+    });
+  });
 
   it("should be able to add a filter and drill thru", () => {
     H.createDashboardWithQuestions({
