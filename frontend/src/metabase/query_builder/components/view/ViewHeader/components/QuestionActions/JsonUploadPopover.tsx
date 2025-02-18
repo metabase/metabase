b019ad7c22d131088ef631a8bf1c52dec46ff623
Ryan Laurie <iethree@gmail.com>
2025-02-18 15:53:37 -0700

    fix curl example

--- frontend/src/metabase/query_builder/components/view/ViewHeader/components/QuestionActions/JsonUploadPopover.tsx
+++ frontend/src/metabase/query_builder/components/view/ViewHeader/components/QuestionActions/JsonUploadPopover.tsx
@@ -1,8 +1,9 @@
 import { jt, t } from "ttag";
 
+import { CopyButton } from "metabase/components/CopyButton";
 import { CopyTextInput } from "metabase/components/CopyTextInput";
 import Link from "metabase/core/components/Link";
-import { Box, Code, Popover, Text } from "metabase/ui";
+import { Box, Code, Flex, Popover, Text } from "metabase/ui";
 import type Question from "metabase-lib/v1/Question";
 
 export function JsonUploadPopover({
@@ -16,27 +17,30 @@ export function JsonUploadPopover({
 }) {
   const url = `http://${window.location.host}/api/table/${question?._card?.table_id}/append-json`;
 
+  const curlContent = `curl -X POST \\
+  -H "Content-Type: application/json" \\
+  -H "X-Api-Key: YOUR_API_KEY" \\
+  -d '[{ "column1"\: "value1", "column2": "value2" }]' \\
+  ${url}`;
+
   return (
     <Popover opened={isOpen} onChange={onClose} closeOnClickOutside>
       <Popover.Target>
         <span />
       </Popover.Target>
       <Popover.Dropdown>
-        <Box p="lg" w="28rem">
+        <Box p="lg" w="30rem">
           <Text pb="sm" fw="bold">{t`Upload JSON`}</Text>
           <Text mb="md" fz="sm" lh="md" c="text-medium">
             {jt`You can upload JSON data to this model by making a POST request to this URL with the JSON data in the request body and an ${(<Link to="/admin/settings/authentication/api-keys" key="api-key" variant="brandBold">{t`API Key`}</Link>)} in the x-api-key header.`}
           </Text>
           <CopyTextInput value={url} />
-          <Box mt="md">
+          <Flex mt="md" p="sm" bg="bg-light" justify="space-between">
             <Code block style={{ whiteSpace: "pre-wrap" }}>
-              {`curl -X POST \\
-    -H "Content-Type: application/json" \\
-    -H "X-Api-Key: YOUR_API_KEY" \\
-    -d '{ "column1"\: "value1", "column2": "value2" }' \\
-    ${url}`}
+              {curlContent}
             </Code>
-          </Box>
+            <CopyButton value={curlContent} />
+          </Flex>
         </Box>
       </Popover.Dropdown>
     </Popover>
