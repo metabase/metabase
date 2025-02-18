107f97ca4dfed07e5ccb53954b8eedd5dd382411
Edward Paget <edpaget@users.noreply.github.com>
2025-02-18 10:52:26 -0600

    fix: allow password reset if google sso disabled (#53865)
    * fix: allow password reset if google sso disabled
    
    * fixup! correct arg type to keyword in message test
    
    * fixup! remove empty line
    
    * fixup! add public docstring
    
    * fixup! add temp google client id

--- src/metabase/session/api.clj
+++ src/metabase/session/api.clj
@@ -163,6 +163,14 @@
   {:email      (throttle/make-throttler :email :attempts-threshold 3 :attempt-ttl-ms 1000)
    :ip-address (throttle/make-throttler :email :attempts-threshold 50)})
 
+(defn- password-reset-disabled?
+  "Disable password reset for all users created with SSO logins, unless those Users were created with Google SSO
+  in which case disable reset for them as long as the Google SSO feature is enabled."
+  [sso-source]
+  (if (and (= sso-source :google) (not (public-settings/sso-enabled?)))
+    (public-settings/google-auth-enabled?)
+    (some? sso-source)))
+
 (defn- forgot-password-impl
   [email]
   (future
@@ -172,8 +180,9 @@
                (t2/select-one [:model/User :id :sso_source :is_active]
                               :%lower.email
                               (u/lower-case-en email))]
-      (if (some? sso-source)
-        ;; If user uses any SSO method to log in, no need to generate a reset token
+      (if (password-reset-disabled? sso-source)
+        ;; If user uses any SSO method to log in, no need to generate a reset token. Some cases for Google SSO
+        ;; are exempted see `password-reset-allowed?`
         (messages/send-password-reset-email! email sso-source nil is-active?)
         (let [reset-token        (user/set-password-reset-token! user-id)
               password-reset-url (str (public-settings/site-url) "/auth/reset_password/" reset-token)]
