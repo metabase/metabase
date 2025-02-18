50dac909e0009394e0117a7668b4ed02847ea8e0
Phoomparin Mano <poom@metabase.com>
2025-02-18 17:44:32 +0700

    feat(sdk): add Next.js compatibility to embedding cli (#50281) (#53692)
    * check if the project is a nextjs project
    
    * add nextjs imports and exports for nextjs projects
    
    * add comments
    
    * add notice on analytics.css and link to guide
    
    * fix typo
    
    * update snippet
    
    * normalize path
    
    * quote import path
    
    * update generated directory
    
    * use fileName for sampleComponents
    
    * remove getImportSnippet
    
    * use nextjs imports from the compatibility layer
    
    * remove use client and export default from snippets
    
    * fix checkIsInNextJsProject import
    
    * add use client directive to pages with useReducer
    
    * add suggestions on module resolution
    
    * make import paths reasonable for next.js
    
    * add example _app.tsx snippet
    
    * trim snippets for Next.js
    
    * show custom app snippets separately for next
    
    * auto-generate custom app
    
    * fix file extension
    
    * do not show the react import step in nextjs
    
    * add default root layout or app
    
    * refine root layout generation logic
    
    * add comments on root layout generation
    
    * add next.js route to app and pages router
    
    * add notes on API routes
    
    * only generate AnalyticsPage when not using nextjs
    
    * use writeFileSync
    
    * create directory if it does not exist
    
    * add messages on route created
    
    * remove AppProps type from snippets
    
    * inform users that a custom root layout has been added
    
    * wait for root layout snippets to be ready
    
    * use the correct layout file name in message
    
    * cleanup snippet message
    
    * highlight the layout file
    
    * temporarily remove the moduleResolution warning
    
    * cleanup messages helper
    
    * cleanup exports
    
    * cleanup analytics page snippet
    
    * restore analytics.css
    
    * add more info on the default provider
    
    * fix whitespace in imports
    
    * add a use-client directive to EmbeddingProvider
    
    * highlight the analytics demo route
    
    * add a sans-serif font by default
    
    * do not show the Express server message if Express server is not there
    
    * use glob to simplify path matching
    
    * make analytics page snippet function pure
    
    * make custom app snippet function pure
    
    * add support for src directory
    
    * add source prefix in router resolution
    
    * trim layout snippet header and add eta
    
    * strip the source prefix from import paths
    
    * make src dir a constant
    
    * add lightweight margin reset to sample analytics.css
    
    * consistent source prefix
    
    * parameterize the route name
    
    * cleanup snippet whitespace
    
    
    
    * rename to getNextJsPagesWrapperOrAppWrapperSnippet
    
    * consolidate snippets file
    
    * inline hasNextJsCustomAppOrRootLayout
    
    
    
    ---------
    
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>

--- enterprise/frontend/src/embedding-sdk/cli/utils/get-suggested-import-path.unit.spec.ts
+++ enterprise/frontend/src/embedding-sdk/cli/utils/get-suggested-import-path.unit.spec.ts
@@ -6,6 +6,40 @@ describe("CLI > getSuggestedImportPath", () => {
     [".", "."],
     ["./src/components", "./components"],
   ])("suggests a reasonable import path", (input, suggestion) => {
-    expect(getSuggestedImportPath(input)).toBe(suggestion);
+    expect(
+      getSuggestedImportPath({
+        isNextJs: false,
+        isUsingSrcDirectory: true,
+        componentPath: input,
+      }),
+    ).toBe(suggestion);
+  });
+
+  it("suggests a reasonable default import path for Next.js without src directory", () => {
+    expect(
+      getSuggestedImportPath({ isNextJs: true, isUsingSrcDirectory: false }),
+    ).toBe("../components/metabase");
+
+    expect(
+      getSuggestedImportPath({
+        isNextJs: true,
+        isUsingSrcDirectory: false,
+        componentPath: "foo/bar",
+      }),
+    ).toBe("../foo/bar");
+  });
+
+  it("suggests a reasonable default import path for Next.js with src directory", () => {
+    expect(
+      getSuggestedImportPath({ isNextJs: true, isUsingSrcDirectory: true }),
+    ).toBe("./metabase");
+
+    expect(
+      getSuggestedImportPath({
+        isNextJs: true,
+        isUsingSrcDirectory: true,
+        componentPath: "foo/bar",
+      }),
+    ).toBe("./bar");
   });
 });
