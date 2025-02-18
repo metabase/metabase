d2f735131c293821670e7e50e3b77101010ca954
Noah Moss <noahbmoss@gmail.com>
2025-02-18 10:22:46 -0500

    Fix lint/formatting errors

--- frontend/src/metabase/visualizations/visualizations/PivotTable/PivotTable.tsx
+++ frontend/src/metabase/visualizations/visualizations/PivotTable/PivotTable.tsx
@@ -135,7 +135,7 @@ const PivotTableInner = forwardRef<HTMLDivElement, VisualizationProps>(
     const topHeaderRef = useRef(null);
 
     const getColumnTitle = useCallback(
-      function(columnIndex: number) {
+      function (columnIndex: number) {
         const column = data.cols.filter(col => !isPivotGroupColumn(col))[
           columnIndex
         ];
