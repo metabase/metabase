bb4565576de1fc86d8d8f63e0c72291a1904cc76
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-02-19 21:38:23 +0700

    Remove dependency from an unrelated module
    Co-Authored-By: Alexander Polyankin <alexander.polyankin@metabase>

--- enterprise/frontend/src/embedding-sdk/components/private/SimpleDataPicker/SimpleDataPicker.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/SimpleDataPicker/SimpleDataPicker.tsx
@@ -2,7 +2,6 @@ import { useDisclosure } from "@mantine/hooks";
 import { type ReactNode, useMemo } from "react";
 
 import { useSearchQuery } from "metabase/api";
-import { isModel } from "metabase/browse/models/utils";
 import { DelayedLoadingAndErrorWrapper } from "metabase/components/LoadingAndErrorWrapper/DelayedLoadingAndErrorWrapper";
 import { Box, Popover } from "metabase/ui";
 import { getQuestionVirtualTableId } from "metabase-lib/v1/metadata/utils/saved-questions";
@@ -42,9 +41,10 @@ export function SimpleDataPicker({
       data.data.map(entity => {
         return {
           ...entity,
-          id: isModel(entity)
-            ? getQuestionVirtualTableId(entity.id)
-            : entity.id,
+          id:
+            entity.model === "dataset"
+              ? getQuestionVirtualTableId(entity.id)
+              : entity.id,
         };
       }),
       { sort_column: "name", sort_direction: SortDirection.Asc },
