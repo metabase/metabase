7a5ea00e0fe671ba431533204c5416a25b2439e8
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-20 10:05:30 +0100

    Document utils

--- frontend/src/metabase-lib/v1/expressions/formatter/utils.ts
+++ frontend/src/metabase-lib/v1/expressions/formatter/utils.ts
@@ -7,6 +7,10 @@ type Lifted<T> = {
   [K in keyof T]: (path: AstPath<unknown>) => path is AstPath<Assertion<T[K]>>;
 };
 
+/**
+ * Lift all matchers to work on Prettier's AstPath instead of on
+ * single Expression nodes.
+ */
 function lift<T>(matchers: T): Lifted<T> {
   // @ts-expect-error: map is empty
   const res: Lifted<T> = {};
@@ -14,16 +18,28 @@ function lift<T>(matchers: T): Lifted<T> {
   for (const key in matchers) {
     const matcher = matchers[key];
     // @ts-expect-error: matcher is valid
-    res[key] = checker(matcher);
+    res[key] = liftMatcher(matcher);
   }
   return res;
 }
 
-function checker<T>(
+/**
+ * Lift a single matcher to work on Prettier's AstPath instead of on
+ * a single Expression node.
+ */
+function liftMatcher<T>(
   matcher: (expr: unknown) => expr is T,
 ): (path: AstPath<unknown>) => path is AstPath<T> {
   return (path: AstPath<unknown>): path is AstPath<T> => matcher(path.node);
 }
 
-// Lift matchers to work on AstPath instead of the corresponding node.
+/**
+ * Lift all matchers to work on Prettier's AstPath instead of on
+ * single Expression nodes.
+ *
+ * For example, this will verify that path.node is a string literal and
+ * asserts that path is AstPath<string>.
+ * @example
+ *   pathMatchers.isStringLiteral(path)
+ */
 export const pathMatchers = lift(nodeMatchers);
