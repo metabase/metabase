e0ddf9e883e06339dcabee2af70d258c0e24e8d0
Bryan Maass <bryan.maass@gmail.com>
2025-02-20 09:20:11 -0800

    refine tests + check github action output

---
+++ test/mage/core_test.clj
@@ -0,0 +1,28 @@
+(ns mage.core-test
+  (:require
+   [clojure.string :as str]
+   [mage.util :as u]))
+
+(def excluded-public-tasks
+  "Private tasks aren't tested, and do not have the same level of reliability!!"
+  #{"nrepl"})
+
+(defn bb-tasks-list []
+  (->> "bb tasks"
+       u/shl!
+       (drop 2)
+       (map (comp first #(str/split % #"\s+")))
+       (remove excluded-public-tasks)
+       vec))
+
+(defn- bb-task-has-example? [task-name]
+  (doseq [cmd [(str "bb " task-name " -h")
+               (str "bb " task-name " -help")]]
+    (println (str "Testing that task has examples with '" cmd "'"))
+    (when-not (str/includes? (u/sh! (str "bb " task-name " -h")) "Examples:")
+      (System/exit 1))))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (mapv bb-task-has-example? (bb-tasks-list))
+  (println "All tests passed")
+  (System/exit 0))
