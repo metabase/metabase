5f5eaa30961126ee227bbd210cb4d36e3d6acdc5
Oisin Coveney <oisin@metabase.com>
2025-02-20 14:38:29 -0800

    Remove changes from ColorSelector

--- frontend/src/metabase/visualizations/components/settings/ChartSettingsTableFormatting/RuleEditor.tsx
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingsTableFormatting/RuleEditor.tsx
@@ -173,9 +173,6 @@ export const RuleEditor = ({
             value={rule.color}
             colors={COLORS}
             onChange={color => onChange({ ...rule, color })}
-            popoverProps={{
-              withinPortal: false,
-            }}
           />
           {canHighlightRow && (
             <>
