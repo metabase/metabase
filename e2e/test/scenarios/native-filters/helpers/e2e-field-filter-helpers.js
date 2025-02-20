78077ba2e067766ae54e43f719525292ab450ebf
Case Nelson <case@metabase.com>
2025-02-20 09:06:32 -0700

    feat: Reduce cost of initial field scanning (#53387)
    * feat: Reduce cost of initial field scanning
    
    Setting a Field `has_field_values` to `:autolist` moves from classify
    based on fingerprinting to the initial Field creation only based on
    semantic and base types.
    
    Querying for field values and receiving too many values or too many
    bytes will no longer delete FieldValues or change Field
    `has_field_values` but rather set the `has_more_values` to true.
    
    During sync, if a `has_field_values` is `:autolist` or `:list` but
    FieldValues is nil, we will not fetch a new cache of FieldValues. This
    way, the only way to cache new FieldValues would be by triggering an
    adhoc request to `GET /api/field/:id/values`.
    
    * Unmark FKs as auto-list
    
    * Fix tests to manually activate field values for param_values
    
    * Add search to e2e helper when filter has more
    
    * Fix e2e tests
    
    * Fix helper
    
    * Fixing more tests
    
    * Fix e2e test

--- e2e/test/scenarios/native-filters/helpers/e2e-field-filter-helpers.js
+++ e2e/test/scenarios/native-filters/helpers/e2e-field-filter-helpers.js
@@ -47,9 +47,12 @@ export function setWidgetStringFilter(value) {
 
 export function selectFilterValueFromList(
   value,
-  { addFilter = true, buttonLabel = "Add filter" } = {},
+  { addFilter = true, buttonLabel = "Add filter", search = false } = {},
 ) {
   popover().within(() => {
+    if (search) {
+      cy.findByPlaceholderText("Search the list").type(`${value}{enter}`);
+    }
     cy.findByText(value).click();
 
     if (addFilter) {
@@ -68,10 +71,10 @@ export function selectFilterValueFromList(
 export function applyFilterByType(
   filter,
   value,
-  { buttonLabel = "Add filter" } = {},
+  { buttonLabel = "Add filter", search = false } = {},
 ) {
   if (["Is", "Is not"].includes(filter)) {
-    selectFilterValueFromList(value, { buttonLabel });
+    selectFilterValueFromList(value, { buttonLabel, search });
   } else {
     addWidgetStringFilter(value, { buttonLabel });
   }
