cb1ffdbb1ed2322340a90b5876f7c3a23fa7373d
Cam Saul <github@camsaul.com>
2025-02-19 18:13:31 -0800

    Update some Kondo configs

--- .clj-kondo/macros/metabase/query_analysis/task/test_setup.clj
+++ .clj-kondo/macros/metabase/query_analysis/task/test_setup.clj
@@ -1,13 +1,18 @@
-(ns macros.metabase.query-analysis.task.test-setup
-  (:require [macros.common]))
+(ns macros.metabase.query-analysis.task.test-setup)
 
 (defmacro with-test-setup!
   "Set up the data required to test the Query Analysis related tasks"
   [& body]
-  `(let [~(macros.common/ignore-unused 'c1) 1
-         ~(macros.common/ignore-unused 'c2) 2
-         ~(macros.common/ignore-unused 'c3) 3
-         ~(macros.common/ignore-unused 'c4) 4
-         ~(macros.common/ignore-unused 'archived) 5
-         ~(macros.common/ignore-unused 'invalid) 6]
+  `(let [~'c1 1
+         ~'c2 2
+         ~'c3 3
+         ~'c4 4
+         ~'archived 5
+         ~'invalid 6]
+     ~'c1
+     ~'c2
+     ~'c3
+     ~'c4
+     ~'archived
+     ~'invalid
      ~@body))
