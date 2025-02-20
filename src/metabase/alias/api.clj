d1217195cbb7702383fe63d074339d2617b8afef
dan sutton <dan@dpsutton.com>
2025-02-20 10:16:41 -0800

    remove drafts and olds from api/aliases api

--- src/metabase/alias/api.clj
+++ src/metabase/alias/api.clj
@@ -3,7 +3,8 @@
    [metabase.api.common :as api]
    [metabase.api.macros :as api.macros]
    [metabase.util :as u]
-   [toucan2.core :as t2]))
+   [toucan2.core :as t2]
+   [clojure.string :as str]))
 
 (api.macros/defendpoint :get "/"
   "Fetch recent logins for the current user."
@@ -11,15 +12,21 @@
   (t2/query {:union-all
              [{:select [:name [[:inline "question"] :model] :id :alias]
                :from [:report_card]
-               :where [:not= :alias nil]}
+               :where [:and
+                       [:not= :alias nil]
+                       [:= 0 [:strpos :alias "@"]]]}
               {:select [:name [[:inline "dashboard"] :model] :id :alias]
                :from [:report_dashboard]
-               :where [:not= :alias nil]}]
+               :where [:and
+                       [:not= :alias nil]
+                       [:= 0 [:strpos :alias "@"]]]}]
              :order-by [[:id :desc]]}))
 
 (api.macros/defendpoint :get "/:alias"
   "Fetch recent logins for the current user."
   [{:keys [alias]}]
+  (when (str/includes? alias "@")
+    (throw (ex-info "Cannot get draft and old aliases this way" {:alias alias})))
   (or (some-> (t2/select-one [:model/Dashboard :id :alias] :alias alias)
               (assoc :model "dashboard"))
       (some-> (t2/select-one [:model/Card :id :alias] :alias alias)
