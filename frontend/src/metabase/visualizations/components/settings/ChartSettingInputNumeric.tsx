e6d4c90df374fd86dc3f1393028d332b2a08d68f
Oisin Coveney <oisin@metabase.com>
2025-02-20 16:58:28 -0800

    Attempt to fix type errors again

--- frontend/src/metabase/visualizations/components/settings/ChartSettingInputNumeric.tsx
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingInputNumeric.tsx
@@ -22,7 +22,8 @@ const ALLOWED_CHARS = new Set([
 
 // Note: there are more props than these that are provided by the viz settings
 // code, we just don't have types for them here.
-interface ChartSettingInputProps extends ChartSettingWidgetProps<number> {
+interface ChartSettingInputProps
+  extends Omit<ChartSettingWidgetProps<number>, "onChangeSettings"> {
   options?: {
     isInteger?: boolean;
     isNonNegative?: boolean;
--- frontend/src/metabase/visualizations/components/settings/types.ts
+++ frontend/src/metabase/visualizations/components/settings/types.ts
@@ -3,5 +3,5 @@ import type { VisualizationSettings } from "metabase-types/api";
 export interface ChartSettingWidgetProps<TValue> {
   value: TValue | undefined;
   onChange: (value?: TValue | null) => void;
-  onChangeSettings?: (settings: Partial<VisualizationSettings>) => void;
+  onChangeSettings: (settings: Partial<VisualizationSettings>) => void;
 }
