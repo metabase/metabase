08938b15f055051dd2f768e12745b4963a62920c
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-21 08:35:09 +0100

    Use diagnoseAndCompileExpression in lint extension too

--- frontend/src/metabase/query_builder/components/expressions/Editor/language.ts
+++ frontend/src/metabase/query_builder/components/expressions/Editor/language.ts
@@ -3,8 +3,13 @@ import { type Diagnostic, linter } from "@codemirror/lint";
 import type { EditorView } from "@codemirror/view";
 
 import type * as Lib from "metabase-lib";
-import { diagnose } from "metabase-lib/v1/expressions/diagnostics";
+import type { ErrorWithMessage } from "metabase-lib/v1/expressions";
 import { parser } from "metabase-lib/v1/expressions/tokenizer/parser";
+import type Metadata from "metabase-lib/v1/metadata/Metadata";
+
+import type { StartRule } from "../types";
+
+import { diagnoseAndCompileExpression } from "./utils";
 
 const expressionLanguage = LRLanguage.define({
   parser,
@@ -12,31 +17,31 @@ const expressionLanguage = LRLanguage.define({
 });
 
 type LintOptions = {
-  startRule: "expression" | "aggregation" | "boolean";
+  startRule: StartRule;
   query: Lib.Query;
   stageIndex: number;
-  name?: string | null;
-  expressionIndex: number | undefined;
+  name?: string;
+  expressionIndex?: number | undefined;
+  metadata: Metadata;
 };
 
 const lint = (options: LintOptions) =>
   linter(
     (view: EditorView): Diagnostic[] => {
       const source = view.state.doc.toString();
-      const error = diagnose({
-        source,
-        ...options,
-      });
-      if (!error || error.pos == null || error.len == null) {
+      const { error } = diagnoseAndCompileExpression(source, options);
+
+      if (!error) {
         return [];
       }
 
+      const position = getErrorPosition(error);
+
       return [
         {
-          from: error.pos,
-          to: error.pos + error.len,
           severity: "error",
           message: error.message,
+          ...position,
         },
       ];
     },
@@ -44,6 +49,22 @@ const lint = (options: LintOptions) =>
       tooltipFilter: () => [],
     },
   );
+
+function getErrorPosition(error: ErrorWithMessage | Error) {
+  if ("pos" in error && "len" in error) {
+    const pos = error.pos ?? 0;
+    const len = error.len ?? 0;
+    return {
+      from: pos,
+      to: pos + len,
+    };
+  }
+
+  return {
+    from: 0,
+    to: 0,
+  };
+}
 
 export function customExpression(options: LintOptions) {
   return new LanguageSupport(expressionLanguage, [lint(options)]);
