a7ea0912f72a7645a8d5399d432b64a747137863
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-24 20:13:39 +0100

    Add fuzz testing for compiler

---
+++ frontend/src/metabase-lib/v1/expressions/fuzz.compiler.unit.spec.ts
@@ -0,0 +1,48 @@
+import process from "process";
+import _ from "underscore";
+
+import { query } from "./__support__/shared";
+import { compileExpression } from "./compiler";
+import { generateExpression } from "./test/generator";
+
+const fuzz = process.env.MB_FUZZ ? describe : _.noop;
+const MAX_SEED = 10_000;
+
+function compile(expression: string, startRule: string = "expression") {
+  const result = compileExpression({
+    source: expression,
+    query,
+    stageIndex: -1,
+    startRule,
+    resolve: false,
+  });
+  if ("error" in result) {
+    throw result.error;
+  }
+}
+
+describe("metabase-lib/v1/expressions/compiler", () => {
+  // quick sanity check before the real fuzzing
+  it("should parse custom expresssion", () => {
+    expect(() => compile("CASE([Deal],[Price]*7e-1,[Price])")).not.toThrow();
+  });
+});
+
+fuzz("FUZZING metabase-lib/v1/expressions/recursive-parser", () => {
+  for (let seed = 1; seed < MAX_SEED; ++seed) {
+    it("should parse generated number expression from seed " + seed, () => {
+      const { expression } = generateExpression(seed, "number");
+      expect(() => compile(expression)).not.toThrow();
+    });
+
+    it("should parse generated string expression from seed " + seed, () => {
+      const { expression } = generateExpression(seed, "string");
+      expect(() => compile(expression)).not.toThrow();
+    });
+
+    it("should parse generated boolean expression from seed " + seed, () => {
+      const { expression } = generateExpression(seed, "boolean");
+      expect(() => compile(expression, "boolean")).not.toThrow();
+    });
+  }
+});
