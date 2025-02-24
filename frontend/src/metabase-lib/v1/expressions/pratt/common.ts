b8a2994981ad399b5f5b81fc6a15fec613d218a3
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-24 20:42:54 +0100

    Remove compare and compareSeed

--- frontend/src/metabase-lib/v1/expressions/pratt/common.ts
+++ frontend/src/metabase-lib/v1/expressions/pratt/common.ts
@@ -8,9 +8,7 @@ import {
   adjustOptions,
   useShorthands,
 } from "../passes";
-import { parse as oldParser } from "../recursive-parser";
 import { resolve } from "../resolver";
-import { generateExpression } from "../test/generator";
 
 import { lexify, compile as newCompile, parse } from ".";
 
@@ -46,44 +44,6 @@ export function compile(source: string, type: Type, opts: Opts = {}) {
   );
 }
 
-export function mockResolve(_kind: any, name: string): Expression {
+function mockResolve(_kind: any, name: string): Expression {
   return ["dimension", name];
-}
-
-export function oracle(source: string, type: Type) {
-  let mbql = null;
-  try {
-    mbql = oldParser(source);
-  } catch (e) {
-    let err = e as any;
-    if (err.length && err.length > 0) {
-      err = err[0];
-      if (typeof err.message === "string") {
-        err = err.message;
-      }
-    }
-    throw err;
-  }
-  return resolve({ expression: mbql, type, fn: mockResolve });
-}
-
-export function compare(
-  source: string,
-  type: Type,
-  opts: Opts = {},
-): { oracle: any; compiled: any } {
-  const _oracle = oracle(source, type);
-  const compiled = compile(source, type, opts);
-  return { oracle: _oracle, compiled };
-}
-
-export function compareSeed(
-  seed: number,
-  type: Type,
-  opts: Opts = {},
-): { oracle: any; compiled: any } {
-  const { expression } = generateExpression(seed, type);
-  const _oracle = oracle(expression, type);
-  const compiled = compile(expression, type, opts);
-  return { oracle: _oracle, compiled };
 }
