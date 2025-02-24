81b013f41fc6b379b4fa45d7e636debcbed84eee
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-24 20:40:09 +0100

    Remove need for compare

--- frontend/src/metabase-lib/v1/expressions/pratt/compiler.unit.spec.ts
+++ frontend/src/metabase-lib/v1/expressions/pratt/compiler.unit.spec.ts
@@ -1,12 +1,16 @@
-import { compare, compile } from "./common";
+import { compile } from "./common";
 
 describe("pratt/compiler", () => {
   function expr(
     source: string,
-    opts: { throwOnError?: boolean; resolverPass?: boolean } = {},
+    opts: {
+      throwOnError?: boolean;
+      resolverPass?: boolean;
+      type?: "expression" | "boolean";
+    } = {},
   ) {
-    const { throwOnError = true, resolverPass } = opts;
-    return compile(source, "expression", { throwOnError, resolverPass });
+    const { throwOnError = true, type = "expression", resolverPass } = opts;
+    return compile(source, type, { throwOnError, resolverPass });
   }
 
   describe("(for an expression)", () => {
@@ -83,59 +87,124 @@ describe("pratt/compiler", () => {
 
     // note, changed from original to accommodate validation of substring args
     it(`Seed 10099: CONtAinS ( [OF4wuV], SUbstriNG("_", 1, lENGtH("s Mfg7" ) ) )`, () => {
-      const { oracle, compiled } = compare(
+      const compiled = expr(
         `CONtAinS ( [OF4wuV], SUbstriNG("_", 1, lENGtH("s Mfg7" ) ) )`,
-        "boolean",
-        { throwOnError: true, resolverPass: false },
+        { throwOnError: true, resolverPass: false, type: "boolean" },
       );
-      expect(compiled).toEqual(oracle);
+      expect(compiled).toEqual([
+        "contains",
+        ["dimension", "OF4wuV"],
+        ["substring", "_", 1, ["length", "s Mfg7"]],
+      ]);
     });
 
     it(`Seed 10092:  NOT ( NOT   (isNUll ([T0q → n_M_O])))`, () => {
-      const { oracle, compiled } = compare(
-        ` NOT ( NOT   (isNUll ([T0q → n_M_O])))`,
-        "expression",
-        {
-          throwOnError: true,
-          resolverPass: false,
-        },
-      );
-      expect(compiled).toEqual(oracle);
+      const compiled = expr(` NOT ( NOT   (isNUll ([T0q → n_M_O])))`, {
+        throwOnError: true,
+        resolverPass: false,
+      });
+      expect(compiled).toEqual([
+        "not",
+        ["not", ["is-null", ["dimension", "T0q → n_M_O"]]],
+      ]);
     });
 
     it(`Seed 10082: SUbstriNg( cOncat("BaK2    ", [__m_4], rTrim(coNcAt (replACE ([Av5Wtbz], regeXextRACt( [_1I → g], "H NVB84_"), rEGexextract ( [__8], " _ 2" )  ) , SUbStRiNG("qb0  ", (power( LeNgTh ( rtrim ( "YyCe_2" )) * 0e+77, 1 ) ), 1 + 0e-54 / 374719e-64) , cOncaT( "    F9 _O", "_a5_", " 5 _U_ ", " bE", rEPlACe (BXj3O, " ", [Z → X9]) ) )  )  ), (1), log ( 1E-26 )  )`, () => {
-      const { oracle, compiled } = compare(
+      const compiled = expr(
         `SUbstriNg( cOncat("BaK2    ", [__m_4], rTrim(coNcAt (replACE ([Av5Wtbz], regeXextRACt( [_1I → g], "H NVB84_"), rEGexextract ( [__8], " _ 2" )  ) , SUbStRiNG("qb0  ", (power( LeNgTh ( rtrim ( "YyCe_2" )) * 0e+77, 1 ) ), 1 + 0e-54 / 374719e-64) , cOncaT( "    F9 _O", "_a5_", " 5 _U_ ", " bE", rEPlACe (BXj3O, " ", [Z → X9]) ) )  )  ), (1), log ( 1E-26 )  )`,
-        "expression",
         { throwOnError: true, resolverPass: false },
       );
-      expect(compiled).toEqual(oracle);
+      expect(compiled).toEqual([
+        "substring",
+        [
+          "concat",
+          "BaK2    ",
+          ["dimension", "__m_4"],
+          [
+            "rtrim",
+            [
+              "concat",
+              [
+                "replace",
+                ["dimension", "Av5Wtbz"],
+                ["regex-match-first", ["dimension", "_1I → g"], "H NVB84_"],
+                ["regex-match-first", ["dimension", "__8"], " _ 2"],
+              ],
+              [
+                "substring",
+                "qb0  ",
+                ["power", ["*", ["length", ["rtrim", "YyCe_2"]], 0], 1],
+                ["+", 1, ["/", 0, 3.74719e-59]],
+              ],
+              [
+                "concat",
+                "    F9 _O",
+                "_a5_",
+                " 5 _U_ ",
+                " bE",
+                [
+                  "replace",
+                  ["dimension", "BXj3O"],
+                  " ",
+                  ["dimension", "Z → X9"],
+                ],
+              ],
+            ],
+          ],
+        ],
+        1,
+        ["log", 1e-26],
+      ]);
     });
 
     it(`Seed 57808:  (( Abs (  (exP( cEil(  - 1e+48) )) )  * -1e31 ) * ( poWeR( (( - 0e+67) *lengTh ( "8" )  ) ,  ( -1 ) *lengTh( "Q  P2c n" ) / powEr(1, N) ) )  )`, () => {
-      const { oracle, compiled } = compare(
+      const compiled = expr(
         `(( Abs (  (exP( cEil(  - 1e+48) )) )  * -1e31 ) * ( poWeR( (( - 0e+67) *lengTh ( "8" )  ) ,  ( -1 ) *lengTh( "Q  P2c n" ) / powEr(1, N) ) )  )`,
-        "expression",
         { throwOnError: true, resolverPass: false },
       );
-      expect(compiled).toEqual(oracle);
+      expect(compiled).toEqual([
+        "*",
+        ["abs", ["exp", ["ceil", -1e48]]],
+        -1e31,
+        [
+          "power",
+          ["*", -0, ["length", "8"]],
+          [
+            "/",
+            ["*", -1, ["length", "Q  P2c n"]],
+            ["power", 1, ["dimension", "N"]],
+          ],
+        ],
+      ]);
     });
 
     // Checks that `x - (y - z)` doesn't get merged into `x - y -z`
     it(`seed 10144: eNDsWith( "NTP", replacE( [V2FFf → r_8ZFu], coalescE(repLACe([cf → l], sUbStriNg ( Replace( [A], "L ", "b"), coAlEsCE(953925E-38, 307355.510173e+32 ), pOwEr (1e+15, 0 )) , caSe ( IsEMpTy( [_ → _3H_6b]) , cOncat ("n_F e_B n" ) , isEmptY([E → _3R6p6_]), conCat(" D 2h", " 4 9u ", "A_9_M_9_", " q _")) ) , RegExeXtRact ([_PI9], "K43s 6") ) , sUBstriNg (CaSE (intervAl( [_OU9c], - 632269.595767E-79, CoalESce ("u__0_71", "c ")), CoalesCe( suBstriNG ( "XPHC0 li_", 500924.700063e-10, 341369) )), - 1E+47 - (424024.827478-1 ), - 1) ) )`, () => {
-      const { oracle, compiled } = compare(
+      const compiled = expr(
         `(( Abs (  (exP( cEil(  - 1e+48) )) )  * -1e31 ) * ( poWeR( (( - 0e+67) *lengTh ( "8" )  ) ,  ( -1 ) *lengTh( "Q  P2c n" ) / powEr(1, N) ) )  )`,
-        "expression",
         { throwOnError: true, resolverPass: false },
       );
-      expect(compiled).toEqual(oracle);
+      expect(compiled).toEqual([
+        "*",
+        ["abs", ["exp", ["ceil", -1e48]]],
+        -1e31,
+        [
+          "power",
+          ["*", -0, ["length", "8"]],
+          [
+            "/",
+            ["*", -1, ["length", "Q  P2c n"]],
+            ["power", 1, ["dimension", "N"]],
+          ],
+        ],
+      ]);
     });
 
     it(`Merging subtraction`, () => {
-      const { oracle, compiled } = compare(`1 - (0 - -10)`, "expression", {
+      const compiled = expr(`1 - (0 - -10)`, {
         throwOnError: true,
       });
-      expect(compiled).toEqual(oracle);
+      expect(compiled).toEqual(["-", 1, ["-", 0, -10]]);
     });
   });
 });
