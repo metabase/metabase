9c4ef5a454f8a145bb6b424863f5f7e76c5f28e8
Nick Fitzpatrick <nickfitz.582@gmail.com>
2025-02-24 17:30:34 -0400

    ui regressions

--- frontend/src/metabase/ui/components/inputs/MonthPicker/MonthPicker.config.tsx
+++ frontend/src/metabase/ui/components/inputs/MonthPicker/MonthPicker.config.tsx
@@ -1,11 +1,19 @@
 import type { MantineThemeOverride } from "@mantine/core";
 import { MonthPicker } from "@mantine/dates";
 
+import Styles from "./MonthPicker.module.css";
+
 export const monthPickerOverrides: MantineThemeOverride["components"] = {
   MonthPicker: MonthPicker.extend({
     defaultProps: {
       size: "md",
       mih: 0, // overwrite Calendar's default value
+    },
+    classNames: {
+      levelsGroup: Styles.LevelsGroup,
+      calendarHeaderLevel: Styles.CalendarHeaderLevel,
+      monthsListCell: Styles.MonthListCell,
+      monthsListRow: Styles.MonthListRow,
     },
   }),
 };
