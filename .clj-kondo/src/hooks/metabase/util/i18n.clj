13a933dca65ea8247b5119b769972ff3533a731c
John Swanson <john.swanson@metabase.com>
2025-02-24 09:09:56 -0800

    CR feedback response
    - more efficient linter
    
    - add linter for "it's a string or call to str"
    
    - remove unnecessary `(next chars)` check
    
    - also, unrelated, fix yet another broken test

--- .clj-kondo/src/hooks/metabase/util/i18n.clj
+++ .clj-kondo/src/hooks/metabase/util/i18n.clj
@@ -24,17 +24,35 @@
               ;; Technically a string like "foo bar's happy but i'm not" is totally fine, but we'll say it's invalid
               ;; because... that inner string literal is probably not what we actually want. So since we've never done
               ;; this intentionally as far as I can tell, let's just remove the possibility to avoid ambiguity.
-            (and (next chars) (= (second chars) \{))
-            (let [rest-str (apply str (rest chars)) ; From '{
-                  closing-index (str/index-of rest-str "'")]
-              (if closing-index
-                (recur (drop (+ closing-index 2) chars)) ; Skip past }'
-                false)) ; No closing }' found
-
+            (= (second chars) \{)
+            (let [at-closing (drop-while #(not= \' %) (next chars))]
+              (if (seq at-closing)
+                (recur (next at-closing))
+                false))
               ;; Unpaired single quote: invalid
             :else
             false)
           (recur (rest chars)))))))
+
+(defn format-str-arg-node->str
+  [node]
+  (cond
+    ;; it's just a string literal
+    (api/string-node? node) (api/sexpr node)
+    ;; it's `(str "some" "strings")`
+    (and (api/list-node? node)
+         (= 'str (api/sexpr (first (:children node))))
+         (every? api/string-node? (rest (:children node))))
+    (apply str (map api/sexpr (rest (:children node))))
+
+    ;; it's something else, no good
+    :else
+    (do
+      (api/reg-finding!
+       (assoc (meta node)
+              :message "Must be a string literal or a call to `str` with string literal arguments."
+              :type :metabase/validate-string-or-str-args-to-i18n))
+      nil)))
 
 (defn strict-apostrophes
   "Checks the node to make sure it's not a i18n call that will result in dropped apostrophes."
@@ -42,10 +60,7 @@
   (let [format-str-arg (second (:children node))
         ;; `format-str-arg` must be either a `"string"` or `(str "a call " "to" "str")` (to allow for splitting long
         ;; strings into shorter lines)
-        format-str (if (api/string-node? format-str-arg)
-                     (api/sexpr format-str-arg)
-                     ;; (str "a" "b" "c") => a list-node with children "str", "a", "b", and "c"
-                     (apply str (map api/sexpr (rest (:children format-str-arg)))))]
+        format-str (format-str-arg-node->str format-str-arg)]
     (when-not (valid-apostrophes? format-str)
       (api/reg-finding!
        (assoc (meta node)
