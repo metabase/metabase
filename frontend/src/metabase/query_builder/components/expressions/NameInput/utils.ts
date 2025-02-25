70317dd7a7b291291991ef79bb6f0e3d8666a997
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-25 11:47:58 +0100

    Use actual ellipsis in placeholder

--- frontend/src/metabase/query_builder/components/expressions/NameInput/utils.ts
+++ frontend/src/metabase/query_builder/components/expressions/NameInput/utils.ts
@@ -4,11 +4,11 @@ import type { StartRule } from "../types";
 
 export function getPlaceholder(startRule: StartRule) {
   if (startRule === "expression") {
-    return t`Give your column a name...`;
+    return t`Give your column a name…`;
   } else if (startRule === "aggregation") {
-    return t`Give your aggregation a name...`;
+    return t`Give your aggregation a name…`;
   } else if (startRule === "boolean") {
-    return t`Give your filter a name...`;
+    return t`Give your filter a name…`;
   }
-  return t`Give your expression a name...`;
+  return t`Give your expression a name…`;
 }
