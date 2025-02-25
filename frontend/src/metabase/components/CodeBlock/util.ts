cee4450768708f8c9aed41de3f17c0bfe734e358
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-25 16:09:01 +0100

    Add erb support

--- frontend/src/metabase/components/CodeBlock/util.ts
+++ frontend/src/metabase/components/CodeBlock/util.ts
@@ -6,21 +6,18 @@ import { StreamLanguage } from "@codemirror/language";
 import { clojure } from "@codemirror/legacy-modes/mode/clojure";
 import { pug } from "@codemirror/legacy-modes/mode/pug";
 import { ruby } from "@codemirror/legacy-modes/mode/ruby";
+import type { Extension } from "@codemirror/state";
 import { handlebarsLanguage as handlebars } from "@xiechao/codemirror-lang-handlebars";
 
 import type { CodeLanguage } from "./types";
 
-export function getLanguageExtension(language: CodeLanguage) {
+export function getLanguageExtension(language: CodeLanguage): Extension | null {
   switch (language) {
     case "clojure":
       return StreamLanguage.define(clojure);
+    case "erb":
     case "html":
       return html();
-    case "javascript":
-      return javascript({
-        jsx: true,
-        typescript: false,
-      });
     case "json":
       return json();
     case "python":
@@ -32,11 +29,11 @@ export function getLanguageExtension(language: CodeLanguage) {
       return StreamLanguage.define(pug);
     case "ruby":
       return StreamLanguage.define(ruby);
+    case "javascript":
     case "typescript":
       return javascript({
         jsx: true,
-        typescript: true,
+        typescript: language === "typescript",
       });
   }
-  // "erb"
 }
