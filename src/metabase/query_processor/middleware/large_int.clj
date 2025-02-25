27c05bfff9740191806f86138bce2db59bc3e92f
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-02-25 14:37:49 -0500

    First pass

--- src/metabase/query_processor/middleware/large_int.clj
+++ src/metabase/query_processor/middleware/large_int.clj
@@ -6,22 +6,41 @@
    [metabase.query-processor.store :as qp.store]
    [metabase.util.performance :as perf]))
 
-(defn- integer-column?
-  [{:keys [base_type] :as _column-metadata}]
-  (isa? base_type :type/Integer))
-
-(defn- column-index-mask
-  [cols]
-  (mapv integer-column? cols))
+(def ^:private min-long -9007199254740991)
+(def ^:private max-long 9007199254740991)
+(def ^:private min-bigint (bigint min-long))
+(def ^:private max-bigint (bigint max-long))
+(def ^:private min-biginteger (biginteger min-long))
+(def ^:private max-biginteger (biginteger max-long))
+(def ^:private min-bigdecimal (bigdec min-long))
+(def ^:private max-bigdecimal (bigdec max-long))
 
 (defn- large-long?
-  [^Long n]
-  (or (< n -9007199254740991)
-      (> n 9007199254740991)))
+  [n]
+  (and (instance? Long n)
+       (or (< n min-long) (> n max-long))))
+
+(defn- large-bigint?
+  [n]
+  (and (instance? clojure.lang.BigInt n)
+       (or (< n min-bigint) (> n max-bigint))))
+
+(defn- large-biginteger?
+  [n]
+  (and (instance? java.math.BigInteger n)
+       (or (< n min-biginteger) (> n max-biginteger))))
+
+(defn- large-bigdecimal?
+  [n]
+  (and (instance? java.math.BigDecimal n)
+       (or (< n min-bigdecimal) (> n max-bigdecimal))))
 
 (defn- large-integer?
   [n]
-  (and (instance? Long n) (large-long? n)))
+  (or (large-long? n)
+      (large-bigint? n)
+      (large-biginteger? n)
+      (large-bigdecimal? n)))
 
 (defn- large-int->string [x]
   (if (large-integer? x)
@@ -34,10 +53,19 @@
           (perf/mapv #(if %2 (large-int->string %1) %1) row field-mask)))
    rf))
 
+(defn- maybe-integer-column?
+  [{:keys [base_type] :as _column-metadata}]
+  (or (isa? base_type :type/Integer)
+      (isa? base_type :type/Decimal)))
+
+(defn- column-index-mask
+  [cols]
+  (mapv maybe-integer-column? cols))
+
 (defn convert-large-int-to-string
-  "Converts any big integer in a result to a string to handle a number > 2^51 or < -2^51, the JavaScript float mantissa.
-  This will allow proper display of large integers, like IDs from services like social media. NULLs are converted to
-  Clojure nil/JS null."
+  "Converts any large integer in a result to a string to handle a number > 2^51 or < -2^51, the JavaScript float
+  mantissa. This will allow proper display of large integers, like IDs from services like social media. NULLs are
+  converted to Clojure nil/JS null."
   [{{:keys [js-int-to-string?] :or {js-int-to-string? false}} :middleware, :as query} rff]
   (let [rff' (when js-int-to-string?
                (fn [metadata]
