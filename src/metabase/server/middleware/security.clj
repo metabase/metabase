f000425e05c0f464bee75a3e31cbbeb9a373019e
Kamil Mielnik <kamil@kamilmielnik.com>
2025-02-25 20:54:05 +0700

    Remove trailing slashes just to be sure

--- src/metabase/server/middleware/security.clj
+++ src/metabase/server/middleware/security.clj
@@ -119,7 +119,7 @@
                   :script-src   (concat
                                  ["'self'"
                                   "https://maps.google.com"
-                                  "https://*.maze.co/"
+                                  "https://*.maze.co"
                                   "https://accounts.google.com"
                                   (when (public-settings/anon-tracking-enabled)
                                     "https://www.google-analytics.com")
@@ -149,15 +149,15 @@
                                  (when config/is-dev?
                                    "http://localhost:9630")
                                  "https://accounts.google.com"
-                                 "https://*.maze.co/"]
-                  :frame-src    (conj (parse-allowed-iframe-hosts (public-settings/allowed-iframe-hosts)) "https://*.maze.co/")
+                                 "https://*.maze.co"]
+                  :frame-src    (conj (parse-allowed-iframe-hosts (public-settings/allowed-iframe-hosts)) "https://*.maze.co")
                   :font-src     ["*"]
                   :img-src      ["*"
                                  "'self' data:"]
                   :connect-src  ["'self'"
                                  ;; Google Identity Services
                                  "https://accounts.google.com"
-                                 "https://*.maze.co/"
+                                 "https://*.maze.co"
                                  ;; MailChimp. So people can sign up for the Metabase mailing list in the sign up process
                                  "metabase.us10.list-manage.com"
                                  ;; Snowplow analytics
