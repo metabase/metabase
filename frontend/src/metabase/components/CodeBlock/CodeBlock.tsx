0da2da48cf7fd7bd42dedaeaa0b6180fa84f0534
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-25 13:18:19 +0100

    Use custom highlighing

--- frontend/src/metabase/components/CodeBlock/CodeBlock.tsx
+++ frontend/src/metabase/components/CodeBlock/CodeBlock.tsx
@@ -1,7 +1,9 @@
+import { syntaxHighlighting } from "@codemirror/language";
 import CodeMirror from "@uiw/react-codemirror";
 import { useMemo } from "react";
 
 import { isNotNull } from "metabase/lib/types";
+import { metabaseSyntaxHighlighting } from "metabase/ui/syntax";
 
 import type { CodeLanguage } from "./types";
 import { getLanguageExtension } from "./util";
@@ -9,13 +11,18 @@ import { getLanguageExtension } from "./util";
 export type CodeBlockProps = {
   code: string;
   language: CodeLanguage;
+  className?: string;
 };
 
-export function CodeBlock({ code, language }: CodeBlockProps) {
+export function CodeBlock({ code, language, ...props }: CodeBlockProps) {
   const extensions = useMemo(() => {
     const lang = getLanguageExtension(language);
-    return [lang].filter(isNotNull);
+    return [syntaxHighlighting(metabaseSyntaxHighlighting), lang].filter(
+      isNotNull,
+    );
   }, [language]);
 
-  return <CodeMirror value={code} extensions={extensions} readOnly />;
+  return (
+    <CodeMirror value={code} extensions={extensions} readOnly {...props} />
+  );
 }
