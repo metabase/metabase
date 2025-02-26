1dc0fbcdb71c01f7a4a0ec5f1c216766a144edd8
Phoomparin Mano <phoomparin@gmail.com>
2025-02-26 16:21:03 +0700

    use question=new in layout

--- enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/context/types.ts
+++ enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/context/types.ts
@@ -43,11 +43,13 @@ export type InteractiveQuestionProviderWithLocationProps = PropsWithChildren<
   InteractiveQuestionConfig & QuestionMockLocationParameters
 >;
 
+// eslint-disable-next-line @typescript-eslint/ban-types -- this is needed to allow any Entity ID string but keep autocomplete for "new", for creating new questions.
+export type InteractiveQuestionId = CardId | "new" | (string & {});
+
 export type InteractiveQuestionProviderProps = PropsWithChildren<
   InteractiveQuestionConfig &
     Omit<LoadSdkQuestionParams, "cardId"> & {
-      // eslint-disable-next-line @typescript-eslint/ban-types -- this is needed to allow any Entity ID string but keep autocomplete for "new", for creating new questions.
-      cardId: CardId | "new" | (string & {});
+      cardId: InteractiveQuestionId;
     }
 >;
 
@@ -68,4 +70,5 @@ export type InteractiveQuestionContextType = Omit<
     onSave: (question: Question) => Promise<void>;
   } & {
     isCardIdError: boolean;
+    originalId: InteractiveQuestionId;
   };
