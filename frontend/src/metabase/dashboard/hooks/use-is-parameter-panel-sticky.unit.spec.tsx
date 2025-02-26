f2e4c593e9fcaefabe76f6444cb4ecce0f63271b
Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>
2025-02-26 12:09:21 +0300

    ci: fix flaky test about sticky panel (#54125)
    * fix flaky test about sticky panel
    
    * rework fix
    
    * drop waitFor
    
    * fixup

--- frontend/src/metabase/dashboard/hooks/use-is-parameter-panel-sticky.unit.spec.tsx
+++ frontend/src/metabase/dashboard/hooks/use-is-parameter-panel-sticky.unit.spec.tsx
@@ -1,17 +1,11 @@
 import { act, renderHook, waitFor } from "@testing-library/react";
-import type { RefObject } from "react";
 
 import { useIsParameterPanelSticky } from "./use-is-parameter-panel-sticky";
 
-type IOCallback = (
-  entries: IntersectionObserverEntry[],
-  observer: IntersectionObserver,
-) => void;
-
 const setup = () => {
-  const parameterPanelRef = {
+  const parameterPanelRef: React.RefObject<HTMLDivElement> = {
     current: document.createElement("div"),
-  } as RefObject<HTMLElement>;
+  };
   const { result, unmount } = renderHook(() =>
     useIsParameterPanelSticky({ parameterPanelRef }),
   );
@@ -27,10 +21,10 @@ describe("useIsParameterPanelSticky", () => {
   let originalIntersectionObserver: typeof IntersectionObserver;
   let mockObserve: jest.Mock;
   let mockDisconnect: jest.Mock;
-  let intersectionCallback: IOCallback | null = null;
+  let intersectionCallback: IntersectionObserverCallback | null = null;
 
-  const invokeIntersection = async (ratio: number) => {
-    await act(() => {
+  const invokeIntersection = (ratio: number) => {
+    act(() => {
       intersectionCallback?.(
         [{ intersectionRatio: ratio } as IntersectionObserverEntry],
         {} as IntersectionObserver,
@@ -48,24 +42,27 @@ describe("useIsParameterPanelSticky", () => {
 
     intersectionCallback = null;
 
-    (global as any).IntersectionObserver = jest.fn(function (
-      callback: IOCallback,
-    ) {
-      intersectionCallback = callback;
+    global.IntersectionObserver = class MockedIntersectionObserver
+      implements IntersectionObserver
+    {
+      constructor(callback: IntersectionObserverCallback) {
+        intersectionCallback = callback;
+      }
 
-      return {
-        observe: mockObserve,
-        disconnect: mockDisconnect,
-      };
-    });
+      observe = mockObserve;
+      disconnect = mockDisconnect;
+      root: Element | null = null;
+      rootMargin: string = "";
+      thresholds: ReadonlyArray<number> = [];
+      takeRecords(): IntersectionObserverEntry[] {
+        return [];
+      }
+      unobserve() {}
+    };
   });
 
-  afterAll(() => {
-    if (originalIntersectionObserver) {
-      global.IntersectionObserver = originalIntersectionObserver;
-    } else {
-      delete (global as any).IntersectionObserver;
-    }
+  afterEach(() => {
+    global.IntersectionObserver = originalIntersectionObserver;
   });
 
   it("returns initial state", () => {
@@ -82,7 +79,7 @@ describe("useIsParameterPanelSticky", () => {
       expect(mockObserve).toHaveBeenCalledTimes(1);
     });
 
-    await invokeIntersection(0.5);
+    invokeIntersection(0.5);
 
     expect(result.current.isSticky).toBe(true);
   });
@@ -94,11 +91,11 @@ describe("useIsParameterPanelSticky", () => {
       expect(mockObserve).toHaveBeenCalledTimes(1);
     });
 
-    await invokeIntersection(0.7);
+    invokeIntersection(0.7);
 
     expect(result.current.isSticky).toBe(true);
 
-    await invokeIntersection(1);
+    invokeIntersection(1);
 
     expect(result.current.isSticky).toBe(false);
   });
@@ -106,18 +103,23 @@ describe("useIsParameterPanelSticky", () => {
   it("sets isStickyStateChanging to true and false before and after isSticky is changed", async () => {
     const { result } = setup();
 
+    const unmockRaf = mockRaf();
+
     await waitFor(() => {
       expect(mockObserve).toHaveBeenCalledTimes(1);
     });
 
-    await invokeIntersection(0.7);
+    invokeIntersection(0.7);
 
     expect(result.current.isStickyStateChanging).toBe(true);
+
     expect(result.current.isSticky).toBe(true);
 
     await waitFor(() => {
       expect(result.current.isStickyStateChanging).toBe(false);
     });
+
+    unmockRaf();
   });
 
   it("disconnects the observer on unmount", () => {
@@ -127,3 +129,15 @@ describe("useIsParameterPanelSticky", () => {
     expect(mockDisconnect).toHaveBeenCalledTimes(1);
   });
 });
+
+// JSDOM uses its own implementation of requestAnimationFrame, which appears to
+// be flaky in our tests. This mock is attempt to make results more consistent.
+function mockRaf() {
+  const originalRaf = global.requestAnimationFrame;
+
+  global.requestAnimationFrame = callback => setTimeout(callback, 0);
+
+  return function unmockRaf() {
+    global.requestAnimationFrame = originalRaf;
+  };
+}
