477cf50f3f594f89bc1ee9ad212ffd7493ca7309
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-02-26 17:02:23 +0100

    Prevent header from visually jumping
    Resolve CLO-3660

--- frontend/src/metabase/timelines/common/components/ModalHeader/ModalHeader.styled.tsx
+++ frontend/src/metabase/timelines/common/components/ModalHeader/ModalHeader.styled.tsx
@@ -37,7 +37,13 @@ export const HeaderMenu = styled.div`
   margin-right: 1rem;
 `;
 
+/**
+ * Both the `height` and the `width` need to be in sync with `EntityMenuIconButton`
+ * in order to prevent the header from visually jumping. See: CLO-3660
+ */
 export const HeaderCloseButton = styled(IconButtonWrapper)`
+  height: 36px;
+  width: 36px;
   flex: 0 0 auto;
   color: var(--mb-color-text-light);
 `;
