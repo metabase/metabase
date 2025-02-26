e9944f7158501a3a8a1cea61cb2627c287f40b43
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-02-25 22:46:49 -0500

    Basic test

--- test/metabase/query_processor/postprocess_test.clj
+++ test/metabase/query_processor/postprocess_test.clj
@@ -14,13 +14,13 @@
     (mt/test-drivers (mt/normal-drivers)
       (testing "All QP middleware should work regardless of the type of each row (#13475)"
         (doseq [rows [[[1]
-                       [Integer/MAX_VALUE]]
+                       [Long/MAX_VALUE]]
                       [(list 1)
-                       (list Integer/MAX_VALUE)]
+                       (list Long/MAX_VALUE)]
                       [(cons 1 nil)
-                       (cons Integer/MAX_VALUE nil)]
+                       (cons Long/MAX_VALUE nil)]
                       [(lazy-seq [1])
-                       (lazy-seq [Integer/MAX_VALUE])]]]
+                       (lazy-seq [Long/MAX_VALUE])]]]
           (testing (format "rows = ^%s %s" (.getCanonicalName (class rows)) (pr-str rows))
             (letfn [(process-query [& {:as additional-options}]
                       (let [query (merge
@@ -33,9 +33,9 @@
                         (qp.setup/with-qp-setup [_query query]
                           (transduce identity (rff nil) rows))))]
               (is (= [[1]
-                      [2147483647]]
+                      [Long/MAX_VALUE]]
                      (process-query)))
               (testing "Should work with the middleware options used by API requests as well"
-                (is (= [["1"]
-                        ["2147483647"]]
+                (is (= [[1]
+                        [(str Long/MAX_VALUE)]]
                        (process-query :middleware @api-qp-middleware-options)))))))))))
