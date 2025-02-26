9bf33818a412a5f67166130419864ba3b20edf18
Tamás Benkő <tamas@metabase.com>
2025-02-26 22:54:29 +0300

    Fix API test

--- enterprise/backend/test/metabase_enterprise/metabot_v3/api_test.clj
+++ enterprise/backend/test/metabase_enterprise/metabot_v3/api_test.clj
@@ -12,22 +12,23 @@
     (with-redefs [metabot-v3.client/request-v2 (fn [e]
                                                  (swap! ai-requests conj e)
                                                  e)]
-      (let [response (mt/user-http-request :rasta :post 200 "ee/metabot-v3/v2/agent"
-                                           {:message question
-                                            :context {}
-                                            :conversation_id conversation-id
-                                            :history []
-                                            :state {}})]
-        (is (=? {:reactions []
-                 :history
-                 [{:role "user", :content "what can you do?"}
-                  {:content nil
-                   :role "assistant"
-                   :tool-calls [{:id (=?/same :tool-call-id), :name "get-current-user", :arguments {}}]}
-                  {:role "tool"
-                   :tool-call-id (=?/same :tool-call-id)
-                   :content "{\"id\":2,\"name\":\"Rasta Toucan\",\"email-address\":\"rasta@metabase.com\"}"}
-                  {:role "user", :content question}]
-                 :state {}
-                 :conversation_id conversation-id}
-                response))))))
+      (mt/with-premium-features #{:metabot-v3}
+        (let [response (mt/user-http-request :rasta :post 200 "ee/metabot-v3/v2/agent"
+                                             {:message question
+                                              :context {}
+                                              :conversation_id conversation-id
+                                              :history []
+                                              :state {}})]
+          (is (=? {:reactions []
+                   :history
+                   [{:role "user", :content "what can you do?"}
+                    {:content nil
+                     :role "assistant"
+                     :tool-calls [{:id (=?/same :tool-call-id), :name "get-current-user", :arguments {}}]}
+                    {:role "tool"
+                     :tool-call-id (=?/same :tool-call-id)
+                     :content "{\"id\":2,\"name\":\"Rasta Toucan\",\"email-address\":\"rasta@metabase.com\"}"}
+                    {:role "user", :content question}]
+                   :state {}
+                   :conversation_id conversation-id}
+                  response)))))))
