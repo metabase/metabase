415dadd5f05a7c536790860594935604b6321970
Oisin Coveney <oisin@metabase.com>
2025-02-26 13:06:16 -0800

    Convert ChartSettingsTableFormatting components to TS (#54103)

--- frontend/src/metabase/visualizations/components/settings/ChartSettingsTableFormatting/index.ts
+++ frontend/src/metabase/visualizations/components/settings/ChartSettingsTableFormatting/index.ts
@@ -1,1 +1,3 @@
-export * from "./ChartSettingsTableFormatting";
+export { ChartSettingsTableFormatting } from "./ChartSettingsTableFormatting";
+export { isFormattable } from "./util";
+export { ALL_OPERATOR_NAMES } from "./get-operators-for-columns";
