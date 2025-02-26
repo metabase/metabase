c164fdfdb0b83f0854e0a05b47d5aaf65d8466e7
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2025-02-26 19:16:54 +0300

    fix monthpicker styles after migration to mantine v7

--- frontend/src/metabase/ui/components/inputs/MonthPicker/MonthPicker.styled.tsx
+++ frontend/src/metabase/ui/components/inputs/MonthPicker/MonthPicker.styled.tsx
@@ -1,11 +1,27 @@
 import type { MantineThemeOverride } from "@mantine/core";
 import { MonthPicker } from "@mantine/dates";
 
+import CalendarS from "../Calendar/Calendar.module.css";
+
+import S from "./MonthPicker.module.css";
+
 export const monthPickerOverrides: MantineThemeOverride["components"] = {
   MonthPicker: MonthPicker.extend({
     defaultProps: {
       size: "md",
       mih: 0, // overwrite Calendar's default value
+    },
+    classNames: {
+      monthsList: CalendarS.monthsList,
+      yearsList: CalendarS.yearsList,
+      yearsListRow: CalendarS.row,
+      yearsListCell: CalendarS.cell,
+      calendarHeader: S.calendarHeader,
+      calendarHeaderLevel: CalendarS.calendarHeaderLevel,
+      calendarHeaderControl: CalendarS.calendarHeaderControl,
+      monthsListControl: S.monthsListControl,
+      monthsListCell: S.monthsListCell,
+      monthsListRow: S.monthsListRow,
     },
   }),
 };
