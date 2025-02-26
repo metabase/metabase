66cc474834fb1df399eb6c34ddc6759217fc27d9
John Swanson <john.swanson@metabase.com>
2025-02-26 09:23:51 -0800

    Rebase fixes

--- enterprise/backend/src/metabase_enterprise/database_routing/api.clj
+++ enterprise/backend/src/metabase_enterprise/database_routing/api.clj
@@ -1,6 +1,6 @@
 (ns metabase-enterprise.database-routing.api
   (:require
-   [metabase.analytics.snowplow :as snowplow]
+   [metabase.analytics.core :as analytics]
    [metabase.api.common :as api]
    [metabase.api.database :as api.database]
    [metabase.api.macros :as api.macros]
@@ -24,7 +24,7 @@
                                                [:name               ms/NonBlankString]
                                                [:details            ms/Map]]]]]]
   (api/check-superuser)
-  (api/check-400 (t2/exists? :model/DatabaseRouter :db_id router_database_id))
+  (api/check-400 (t2/exists? :model/DatabaseRouter :database_id router_database_id))
   (let [{:keys [engine auto_run_queries is_on_demand]} (t2/select-one :model/Database :id router_database_id)
         details-or-errors                              (map (fn [{:keys [details] n :name}]
                                                               {:details-or-error (api.database/test-connection-details (name engine) details)
@@ -35,29 +35,29 @@
       (u/prog1 (t2/insert-returning-instances!
                 :model/Database
                 (map (fn [{:keys [name details-or-error]}]
-                       {:name                name
-                        :engine              engine
-                        :details             details-or-error
-                        :auto_run_queries    auto_run_queries
-                        :is_full_sync        false
-                        :is_on_demand        is_on_demand
-                        :cache_ttl           nil
-                        :primary_database_id router_database_id
-                        :creator_id          api/*current-user-id*})
+                       {:name               name
+                        :engine             engine
+                        :details            details-or-error
+                        :auto_run_queries   auto_run_queries
+                        :is_full_sync       false
+                        :is_on_demand       is_on_demand
+                        :cache_ttl          nil
+                        :router_database_id router_database_id
+                        :creator_id         api/*current-user-id*})
                      details-or-errors))
         (doseq [database <>]
           (events/publish-event! :event/database-create {:object database :user-id api/*current-user-id*})
-          (snowplow/track-event! ::snowplow/database
-                                 {:event        :database-connection-successful
-                                  :database     engine
-                                  :database-id  (u/the-id database)
-                                  :source       :admin
-                                  :dbms-version (:version (driver/dbms-version (keyword engine) database))})))
+          (analytics/track-event! :snowplow/database
+                                  {:event        :database-connection-successful
+                                   :database     engine
+                                   :database-id  (u/the-id database)
+                                   :source       :admin
+                                   :dbms-version (:version (driver/dbms-version (keyword engine) database))})))
       (do
-        (snowplow/track-event! ::snowplow/database
-                               {:event    :database-connection-failed
-                                :database engine
-                                :source   :admin})
+        (analytics/track-event! :snowplow/database
+                                {:event    :database-connection-failed
+                                 :database engine
+                                 :source   :admin})
         {:status 400
          :body   (->> details-or-errors
                       (remove is-valid?)
@@ -74,8 +74,8 @@
    {:keys [user_attribute]} :- [:map
                                 [:user_attribute ms/NonBlankString]]]
   (api/check-404 (t2/exists? :model/Database :id id))
-  (api/check-400 (not (t2/exists? :model/DatabaseRouter :db_id id)))
-  (t2/insert-returning-instance! :model/DatabaseRouter :user_attribute user_attribute :db_id id))
+  (api/check-400 (not (t2/exists? :model/DatabaseRouter :database_id id)))
+  (t2/insert-returning-instance! :model/DatabaseRouter :user_attribute user_attribute :database_id id))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/database-routing` routes"
--- enterprise/backend/test/metabase_enterprise/database_routing/api_test.clj
+++ enterprise/backend/test/metabase_enterprise/database_routing/api_test.clj
@@ -1,8 +1,9 @@
 (ns metabase-enterprise.database-routing.api-test
-  (:require [metabase.test :as mt]
-            [clojure.test :refer [deftest testing is use-fixtures]]
-            [toucan2.core :as t2]
-            [metabase.driver :as driver]))
+  (:require [clojure.test :refer [deftest testing is use-fixtures]]
+            [metabase.driver :as driver]
+            [metabase.driver.h2]
+            [metabase.test :as mt]
+            [toucan2.core :as t2]))
 
 (defn with-h2-fixture [f]
   (binding [metabase.driver.h2/*allow-testing-h2-connections* true]
@@ -13,7 +14,7 @@
     (f)))
 
 (defn with-test-db-is-router-db [f]
-  (mt/with-temp [:model/DatabaseRouter _ {:db_id (mt/id)
+  (mt/with-temp [:model/DatabaseRouter _ {:database_id (mt/id)
                                           :user_attribute "meow"}]
     (f)))
 
@@ -26,7 +27,7 @@
                                                :mirrors [{:name "mirror database"
                                                           :details (:details (mt/db))}]})]
       (is (t2/exists? :model/Database
-                      :primary_database_id (mt/id)
+                      :router_database_id (mt/id)
                       :id db-id)))))
 
 (deftest invalid-details-returns-an-error
@@ -55,24 +56,70 @@
                                                  :details (:details (mt/db))}
                                                 {:name "mirror database 2"
                                                  :details (:details (mt/db))}]})))
-        (is (not (t2/exists? :model/Database :primary_database_id (mt/id))))))))
+        (is (not (t2/exists? :model/Database :router_database_id (mt/id))))))))
 
 (deftest we-can-mark-an-existing-database-as-being-a-router-database
   (mt/with-temp [:model/Database {db-id :id} {}]
     (mt/with-model-cleanup [:model/DatabaseRouter]
       (mt/user-http-request :crowberto :put 200 (str "ee/database-routing/database/" db-id)
                             {:user_attribute "foo"})
-      (is (t2/exists? :model/DatabaseRouter :db_id db-id :user_attribute "foo")))))
+      (is (t2/exists? :model/DatabaseRouter :database_id db-id :user_attribute "foo")))))
 
 (deftest marking-a-nonexistent-database-as-a-router-database-fails
   (let [nonexistent-id 123456789]
     (mt/with-model-cleanup [:model/DatabaseRouter]
       (mt/user-http-request :crowberto :put 404 (str "ee/database-routing/database/" nonexistent-id)
                             {:user_attribute "foo"})
-      (is (not (t2/exists? :model/DatabaseRouter :db_id nonexistent-id :user_attribute "foo"))))))
+      (is (not (t2/exists? :model/DatabaseRouter :database_id nonexistent-id :user_attribute "foo"))))))
 
 (deftest marking-something-that-is-already-a-router-database-fails
   (mt/with-temp [:model/Database {db-id :id} {}
-                 :model/DatabaseRouter _ {:db_id db-id :user_attribute "foo"}]
+                 :model/DatabaseRouter _ {:database_id db-id :user_attribute "foo"}]
     (mt/user-http-request :crowberto :put 400 (str "ee/database-routing/database/" db-id)
                           {:user_attribute "bar"})))
+
+(deftest mirror-databases-are-hidden-from-regular-database-api
+  (mt/with-temp [:model/Database {db-id :id} {}
+                 :model/DatabaseRouter _ {:database_id db-id :user_attribute "foo"}
+                 :model/Database {mirror-db-id :id} {:router_database_id db-id}]
+    (testing "GET /database/:id"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id))
+      (testing "If we pass the `include_mirror_databases` param, it is included"
+        (mt/user-http-request :crowberto :get 200 (str "database/" mirror-db-id "?include_mirror_databases=true")))
+      (testing "If a regular user passees `include_mirror_databases` it is hidden"
+        (mt/user-http-request :rasta :get 404 (str "database/" mirror-db-id "?include_mirror_databases=true"))))
+    (testing "GET /database/"
+      (is (not-any? #(= (:id %) mirror-db-id)
+                    (:data (mt/user-http-request :crowberto :get 200 "database/"))))
+      (testing "If we pass the `include_mirror_databases` param it is included"
+        (is (some #(= (:id %) mirror-db-id)
+                  (:data (mt/user-http-request :crowberto :get 200 "database/?include_mirror_databases=true")))))
+      (testing "Regular users can't do this"
+        (is (not-any? #(= (:id %) mirror-db-id)
+                      (:data (mt/user-http-request :rasta :get 200 "database/?include_mirror_databases=true"))))))
+    (testing "PUT /database/:id should work normally"
+      (mt/user-http-request :crowberto :put 200 (str "database/" mirror-db-id)))
+    (testing "GET /database/:id/usage_info"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/usage_info")))
+    (testing "GET /database/:id/metadata"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/metadata")))
+    (testing "GET /database/:id/autocomplete_suggestions"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/autocomplete_suggestions")))
+    (testing "GET /database/:id/card_autocomplete_suggestions"
+      (mt/user-http-request :crowberto :get 400 (str "database/" mirror-db-id "/card_autocomplete_suggestions")))
+    (testing "GET /database/:id/fields"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/fields")))
+    (testing "GET /database/:id/idfields"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/idfields")))
+    (testing "POST /database/:id/sync_schema"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/sync_schema")))
+    (testing "POST /database/:id/dismiss_spinner"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/dismiss_spinner")))
+    (testing "POST /database/:id/rescan_values"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/rescan_values")))
+    (testing "POST /database/:id/discard_values"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/discard_values")))
+    (testing "POST /database/:id/syncable_schemas"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/syncable_schemas")))
+    (testing "GET /database/:id/schemas"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/schemas")))))
