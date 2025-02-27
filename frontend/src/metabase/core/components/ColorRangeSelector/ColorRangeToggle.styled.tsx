fb418334ae6967daa2e0ae836bcc32515e35221c
Oisin Coveney <oisin@metabase.com>
2025-02-27 15:05:43 -0800

    Convert ColorRange + RuleBackground to Mantine

--- frontend/src/metabase/core/components/ColorRangeSelector/ColorRangeToggle.styled.tsx
+++ frontend/src/metabase/core/components/ColorRangeSelector/ColorRangeToggle.styled.tsx
@@ -2,17 +2,9 @@
 import styled from "@emotion/styled";
 
 import Button from "metabase/core/components/Button";
-import ColorRange from "metabase/core/components/ColorRange";
-
-import type { ColorRangeProps } from "../ColorRange/ColorRange";
 
 export const ToggleRoot = styled.div`
   display: flex;
-`;
-
-export const ToggleColorRange = styled(ColorRange)<ColorRangeProps>`
-  flex: 1 1 auto;
-  cursor: ${props => (props.onSelect ? "pointer" : " default")};
 `;
 
 export const ToggleButton = styled(Button)`
