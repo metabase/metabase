ce73bf4b9f4678ec793b6c3ac4aaa69b3c8dfc26
Romeo Van Snick <romeo@romeovansnick.be>
2025-02-27 11:51:16 +0100

    Use explicit classes in highlight

--- frontend/src/metabase/ui/syntax/highlight.ts
+++ frontend/src/metabase/ui/syntax/highlight.ts
@@ -1,40 +1,29 @@
 import { HighlightStyle } from "@codemirror/language";
-import type { Tag } from "@lezer/highlight";
 import { tags } from "@lezer/highlight";
 
 import S from "./highlight.module.css";
 
-const defaultTags = Object.values(tags)
-  .filter((tag): tag is Tag => typeof tag !== "function")
-  .map((tag: Tag) => ({
-    tag,
-    class: classNameForTag(tag),
-  }))
-  .filter(tag => tag.class !== "");
-
-const styles = [
-  ...defaultTags,
-  { tag: tags.special(tags.string), class: S.string },
+export const metabaseSyntaxHighlighting = HighlightStyle.define([
+  { tag: tags.arithmeticOperator, class: S.arithmeticOperator },
+  { tag: tags.blockComment, class: S.blockComment },
+  { tag: tags.bool, class: S.bool },
+  { tag: tags.brace, class: S.brace },
+  { tag: tags.comment, class: S.comment },
+  { tag: tags.compareOperator, class: S.compareOperator },
+  { tag: tags.constant(tags.variableName), class: S.constant },
   { tag: tags.definition(tags.propertyName), class: S.propertyNameDefinition },
+  { tag: tags.escape, class: S.escape },
   { tag: tags.function(tags.variableName), class: S.variableName },
-];
-
-export const metabaseSyntaxHighlighting = HighlightStyle.define(styles);
-
-/**
- * Returns the correct css class name for a @lezer/highlight tag.
- */
-export function classNameForTag(tag: Tag | Tag[]): string {
-  if (Array.isArray(tag)) {
-    return tag.map(classNameForTag).join(" ");
-  }
-
-  // lezer generates tags like function(variableName) for combined tags
-  // we split them up here so we generate classNames like .function.variableName
-  const names = tag.toString().split(/[\(\)]/);
-
-  return names
-    .map(name => S[name] ?? "")
-    .filter(x => x !== "")
-    .join(" ");
-}
+  { tag: tags.keyword, class: S.keyword },
+  { tag: tags.lineComment, class: S.lineComment },
+  { tag: tags.logicOperator, class: S.logicOperator },
+  { tag: tags.number, class: S.number },
+  { tag: tags.paren, class: S.paren },
+  { tag: tags.processingInstruction, class: S.processingInstruction },
+  { tag: tags.propertyName, class: S.propertyName },
+  { tag: tags.special(tags.string), class: S.string },
+  { tag: tags.squareBracket, class: S.squareBracket },
+  { tag: tags.string, class: S.string },
+  { tag: tags.typeName, class: S.typeName },
+  { tag: tags.variableName, class: S.variableName },
+]);
