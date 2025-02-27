59dff635fdc740818b1e91d518e3fdd1d2c093fd
Sébastien <sebastien@metabase.com>
2025-02-27 15:27:45 +0100

    :x

--- frontend/src/metabase/core/components/ColorSelector/ColorSelector.tsx
+++ frontend/src/metabase/core/components/ColorSelector/ColorSelector.tsx
@@ -1,8 +1,8 @@
-import { useDisclosure } from "@mantine/hooks";
 import type { HTMLAttributes } from "react";
 
 import type { PillSize } from "metabase/core/components/ColorPill";
 import { ColorPill } from "metabase/core/components/ColorPill";
+import { useToggle } from "metabase/hooks/use-toggle";
 import { Center, Popover } from "metabase/ui";
 
 import ColorSelectorPopover from "./ColorSelectorPopover";
@@ -27,14 +27,15 @@ export const ColorSelector = ({
   withinPortal = true,
   ...props
 }: ColorSelectorProps) => {
-  const [opened, { toggle, close }] = useDisclosure(false);
+  const [opened, { turnOff: close, toggle }] = useToggle(false);
 
   return (
     <Popover
       withinPortal={withinPortal}
       opened={opened}
-      onClose={close}
+      onDismiss={close}
       position="bottom-start"
+      trapFocus
     >
       <Popover.Target>
         <Center data-testid="color-selector-button">
