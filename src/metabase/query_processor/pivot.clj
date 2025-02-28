dad51f169f80a96433aba07c3bedb714c414724d
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-02-28 17:04:38 -0500

    fix: nested field refs from join-alias with spaces (#54231) (#54535)
    * fix: nested field refs from join-alias with spaces
    
    Fixes: #37600
    
    Join aliases get escaped by drivers (truncation and/or tr space to
    underscore). However, when join fields are referenced in future stages,
    the references were not being renamed and so the query was being created
    with invalid columns from the previous stage.
    
    * Fix tests, drop methods namespace
    
    * Re-assoc info after conversion
    
    * Just change query, none of the mw keys
    
    * Add before/after mw for escape join alias to be able to use and capture converted pmbql queries
    
    * Fix kondo errors
    
    * Fix nest-breakouts
    
    * Fix breakouts tests
    
    * Field rename should happen in add-alias-info.
    
    * Fix tests
    
    * Don't use card name for collection name
    
    * Format rows for sqlite
    
    * Uncomment assertion
    
    Co-authored-by: Case Nelson <case@metabase.com>

--- src/metabase/query_processor/pivot.clj
+++ src/metabase/query_processor/pivot.clj
@@ -8,7 +8,6 @@
    [metabase.lib.convert :as lib.convert]
    [metabase.lib.core :as lib]
    [metabase.lib.equality :as lib.equality]
-   [metabase.lib.metadata :as lib.metadata]
    [metabase.lib.metadata.jvm :as lib.metadata.jvm]
    [metabase.lib.query :as lib.query]
    [metabase.lib.schema :as lib.schema]
@@ -360,7 +359,7 @@
         metadata-provider  (or (:lib/metadata query)
                                (lib.metadata.jvm/application-database-metadata-provider (:database query)))
         query              (lib/query metadata-provider query)
-        unique-name-fn     (lib.util/unique-name-generator (lib.metadata/->metadata-provider query))
+        unique-name-fn     (lib.util/unique-name-generator)
         returned-columns   (->> (lib/returned-columns query)
                                 (mapv #(update % :name unique-name-fn)))
         {:source/keys [aggregations breakouts]} (group-by :lib/source returned-columns)
