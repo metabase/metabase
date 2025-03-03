8c455ba41a0b5bd86026655d4fcabb887a773c3f
John Swanson <john.swanson@metabase.com>
2025-03-03 09:52:23 -0800

    Don't test database conn for mirror dbs

--- enterprise/backend/src/metabase_enterprise/database_routing/api.clj
+++ enterprise/backend/src/metabase_enterprise/database_routing/api.clj
@@ -1,18 +1,19 @@
 (ns metabase-enterprise.database-routing.api
   (:require
-   [metabase.analytics.core :as analytics]
    [metabase.api.common :as api]
-   [metabase.api.database :as api.database]
    [metabase.api.macros :as api.macros]
    [metabase.api.routes.common :refer [+auth]]
-   [metabase.driver :as driver]
    [metabase.events :as events]
    [metabase.util :as u]
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
 
 (api.macros/defendpoint :post "/mirror-database"
-  "Create new Mirror Databases."
+  "Create new Mirror Databases.
+
+  Note that unlike the normal `POST /api/database` endpoint, does NOT check the details before adding the Database.
+
+  This is OK, it's not an invariant that all database details are always valid, but it's something to note."
   [_route-params
    _query-params
    {:keys [router_database_id mirrors]} :- [:map
@@ -23,46 +24,22 @@
                                                [:name               ms/NonBlankString]
                                                [:details            ms/Map]]]]]]
   (api/check-400 (t2/exists? :model/DatabaseRouter :database_id router_database_id))
-  (let [{:keys [engine auto_run_queries is_on_demand]} (t2/select-one :model/Database :id router_database_id)
-        details-or-errors                              (map (fn [{:keys [details] n :name}]
-                                                              {:details-or-error (api.database/test-connection-details (name engine) details)
-                                                               :name             n})
-                                                            mirrors)
-        is-valid?                                      #(not= (:valid (:details-or-error %)) false)]
-    (if (every? is-valid? details-or-errors)
-      (u/prog1 (t2/insert-returning-instances!
-                :model/Database
-                (map (fn [{:keys [name details-or-error]}]
-                       {:name               name
-                        :engine             engine
-                        :details            details-or-error
-                        :auto_run_queries   auto_run_queries
-                        :is_full_sync       false
-                        :is_on_demand       is_on_demand
-                        :cache_ttl          nil
-                        :router_database_id router_database_id
-                        :creator_id         api/*current-user-id*})
-                     details-or-errors))
-        (doseq [database <>]
-          (events/publish-event! :event/database-create {:object database :user-id api/*current-user-id*})
-          (analytics/track-event! :snowplow/database
-                                  {:event        :database-connection-successful
-                                   :database     engine
-                                   :database-id  (u/the-id database)
-                                   :source       :admin
-                                   :dbms-version (:version (driver/dbms-version (keyword engine) database))})))
-      (do
-        (analytics/track-event! :snowplow/database
-                                {:event    :database-connection-failed
-                                 :database engine
-                                 :source   :admin})
-        {:status 400
-         :body   (->> details-or-errors
-                      (remove is-valid?)
-                      (map (fn [{:keys [details-or-error name]}]
-                             (-> details-or-error
-                                 (dissoc :valid)
-                                 (merge {:name name})))))}))))
+  (let [{:keys [engine auto_run_queries is_on_demand]} (t2/select-one :model/Database :id router_database_id)]
+    (u/prog1 (t2/insert-returning-instances!
+              :model/Database
+              (map (fn [{:keys [name details]}]
+                     {:name               name
+                      :engine             engine
+                      :details            details
+                      :auto_run_queries   auto_run_queries
+                      :is_full_sync       false
+                      :is_on_demand       is_on_demand
+                      :cache_ttl          nil
+                      :router_database_id router_database_id
+                      :creator_id         api/*current-user-id*})
+                   mirrors))
+      (doseq [database <>]
+        (events/publish-event! :event/database-create {:object database :user-id api/*current-user-id*})))))
 
 (api.macros/defendpoint :post "/router"
   "Creates a new Router by marking an existing Database as a router."
--- enterprise/backend/test/metabase_enterprise/database_routing/api_test.clj
+++ enterprise/backend/test/metabase_enterprise/database_routing/api_test.clj
@@ -31,33 +31,17 @@
                       :router_database_id (mt/id)
                       :id db-id)))))
 
-(deftest invalid-details-returns-an-error
+(deftest invalid-details-doesnt-matter
   (mt/with-model-cleanup [:model/Database]
-    (with-redefs [driver/can-connect? (fn [& _]
+    (with-redefs [driver/can-connect? (fn [_]
                                         (throw (ex-info "nope" {})))]
-      (is (= [{:message "nope" :name "mirror database"}]
-             (mt/user-http-request :crowberto :post 400 "ee/database-routing/mirror-database"
-                                   {:router_database_id (mt/id)
-                                    :mirrors [{:name "mirror database"
-                                               :details {:db "not gonna work"}}]}))))))
-
-(deftest creation-is-atomic
-  (mt/with-model-cleanup [:model/Database]
-    (let [calls (atom 0)]
-      (with-redefs [driver/can-connect? (fn [& _]
-                                          (if (<= @calls 0)
-                                            (do
-                                              (swap! calls inc)
-                                              true)
-                                            (throw (ex-info "nope" {}))))]
-        (is (= [{:message "nope" :name "mirror database 2"}]
-               (mt/user-http-request :crowberto :post 400 "ee/database-routing/mirror-database"
-                                     {:router_database_id (mt/id)
-                                      :mirrors [{:name "mirror database"
-                                                 :details (:details (mt/db))}
-                                                {:name "mirror database 2"
-                                                 :details (:details (mt/db))}]})))
-        (is (not (t2/exists? :model/Database :router_database_id (mt/id))))))))
+      (let [[{db-id :id}] (mt/user-http-request :crowberto :post 200 "ee/database-routing/mirror-database"
+                                                {:router_database_id (mt/id)
+                                                 :mirrors [{:name "mirror database"
+                                                            :details {:db "meow"}}]})]
+        (is (t2/exists? :model/Database
+                        :router_database_id (mt/id)
+                        :id db-id))))))
 
 (deftest we-can-mark-an-existing-database-as-being-a-router-database
   (mt/with-temp [:model/Database {db-id :id} {}]
