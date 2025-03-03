f143a4ec9bfba4b9d8efc54e3a145919077a0122
Sloan Sparger <sloansparger@gmail.com>
2025-03-03 15:46:05 -0600

    flow rewired together mostly, connection details form is now in a modal

--- frontend/src/metabase/admin/databases/containers/DatabaseEditApp.tsx
+++ frontend/src/metabase/admin/databases/containers/DatabaseEditApp.tsx
@@ -1,21 +1,18 @@
-import type { Location, LocationDescriptor } from "history";
+import type { Location } from "history";
 import type { ComponentType } from "react";
-import type { Route } from "react-router";
-import { push } from "react-router-redux";
 import { useMount } from "react-use";
 import { t } from "ttag";
 import _ from "underscore";
 
 import ErrorBoundary from "metabase/ErrorBoundary";
 import Breadcrumbs from "metabase/components/Breadcrumbs";
-import { GenericError } from "metabase/components/ErrorPages";
+import { GenericError, NotFound } from "metabase/components/ErrorPages";
 import CS from "metabase/css/core/index.css";
 import title from "metabase/hoc/Title";
-import { useModal } from "metabase/hooks/use-modal";
 import { connect } from "metabase/lib/redux";
 import { getSetting } from "metabase/selectors/settings";
 import { getUserIsAdmin } from "metabase/selectors/user";
-import { Box, Divider, Modal } from "metabase/ui";
+import { Box, Divider } from "metabase/ui";
 import Database from "metabase-lib/v1/metadata/Database";
 import type {
   DatabaseData,
@@ -26,10 +23,6 @@ import type { State } from "metabase-types/store";
 
 import { DatabaseConnectionInfoSectionContent } from "../components/DatabaseConnectionInfoSectionContent";
 import { DatabaseDangerZoneSectionContent } from "../components/DatabaseDangerZoneSectionContent";
-import {
-  DatabaseEditConnectionForm,
-  type DatabaseEditErrorType,
-} from "../components/DatabaseEditConnectionForm";
 import { DatabaseInfoSection } from "../components/DatabaseInfoSection";
 import { DatabaseModelFeaturesSectionContent } from "../components/DatabaseModelFeaturesSectionContent";
 import { ExistingDatabaseHeader } from "../components/ExistingDatabaseHeader";
@@ -43,10 +36,11 @@ import {
 import { getEditingDatabase, getInitializeError } from "../selectors";
 
 interface DatabaseEditAppProps {
+  children: React.ReactNode;
   database?: Database;
   params: { databaseId: DatabaseId };
   reset: () => void;
-  initializeDatabase: (databaseId: DatabaseId) => void;
+  initializeDatabase: (databaseId: DatabaseId) => Promise<void>;
   dismissSyncSpinner: (databaseId: DatabaseId) => Promise<void>;
   updateDatabase: (
     database: { id: DatabaseId } & Partial<DatabaseType>,
@@ -55,10 +49,6 @@ interface DatabaseEditAppProps {
   location: Location;
   isAdmin: boolean;
   isModelPersistenceEnabled: boolean;
-  // TODO: remove
-  initializeError?: DatabaseEditErrorType;
-  route: Route;
-  onChangeLocation: (location: LocationDescriptor) => void;
 }
 
 const mapStateToProps = (state: State) => {
@@ -78,39 +68,40 @@ const mapDispatchToProps = {
   updateDatabase,
   dismissSyncSpinner,
   selectEngine,
-  onChangeLocation: push,
 };
 
 function DatabaseEditAppInner({
+  children,
   database,
   updateDatabase,
-  initializeError,
   dismissSyncSpinner,
   isAdmin,
   isModelPersistenceEnabled,
   reset,
   initializeDatabase,
   params,
-  route,
-  onChangeLocation,
 }: DatabaseEditAppProps) {
-  const editingExistingDatabase = database?.id != null;
-  const addingNewDatabase = !editingExistingDatabase;
-
-  const dbDetailsModal = useModal(false);
-
   useMount(async () => {
-    await reset();
+    reset();
     await initializeDatabase(params.databaseId);
   });
 
-  const crumbs = [
+  const dbNotFound = !database?.id;
+
+  const crumbs = _.compact([
     [t`Databases`, "/admin/databases"],
-    [addingNewDatabase ? t`Add Database` : database.name],
-  ];
+    dbNotFound ? null : [t`Add Database`],
+  ]);
+
   // TODO: handle this on a new page
-  if (addingNewDatabase) {
-    return <div>TODO: b0rked this on purpose for the time being</div>;
+  if (dbNotFound) {
+    return (
+      <ErrorBoundary errorComponent={GenericError as ComponentType}>
+        <Box w="100%" maw="64.25rem" mx="auto" py="4rem">
+          <NotFound />
+        </Box>
+      </ErrorBoundary>
+    );
   }
 
   // TODO: && a check for if any section in the models section should be shown
@@ -118,65 +109,54 @@ function DatabaseEditAppInner({
   const shouldShowDangerZone = !database.is_attached_dwh;
 
   return (
-    <ErrorBoundary errorComponent={GenericError as ComponentType}>
-      <Box w="100%" maw="64.25rem" mx="auto" px="2rem">
-        <Breadcrumbs className={CS.py4} crumbs={crumbs} />
+    <>
+      <ErrorBoundary errorComponent={GenericError as ComponentType}>
+        <Box w="100%" maw="64.25rem" mx="auto" px="2rem">
+          <Breadcrumbs className={CS.py4} crumbs={crumbs} />
 
-        <ExistingDatabaseHeader database={database} />
+          <ExistingDatabaseHeader database={database} />
 
-        <Divider mb="3.25rem" />
+          <Divider mb="3.25rem" />
 
-        <DatabaseInfoSection
-          name={t`Connection and sync`}
-          description={t`Manage details about the database connection and when Metabase ingests new data.`}
-          condensed
-        >
-          <DatabaseConnectionInfoSectionContent
-            database={database}
-            dismissSyncSpinner={dismissSyncSpinner}
-            openDbDetailsModal={dbDetailsModal.open}
-          />
-        </DatabaseInfoSection>
-
-        {shouldShowModelFeaturesSection && (
           <DatabaseInfoSection
-            name={t`Model features`}
-            description={t`Choose whether to enable features related to Metabase models. These will often require a write connection.`}
+            name={t`Connection and sync`}
+            description={t`Manage details about the database connection and when Metabase ingests new data.`}
+            condensed
           >
-            <DatabaseModelFeaturesSectionContent
+            <DatabaseConnectionInfoSectionContent
               database={database}
-              isModelPersistenceEnabled={isModelPersistenceEnabled}
-              updateDatabase={updateDatabase}
+              dismissSyncSpinner={dismissSyncSpinner}
             />
           </DatabaseInfoSection>
-        )}
 
-        {shouldShowDangerZone && (
-          <DatabaseInfoSection
-            name={t`Danger zone`}
-            description={t`Remove this database and other destructive actions`}
-          >
-            <DatabaseDangerZoneSectionContent
-              isAdmin={isAdmin}
-              database={database}
-            />
-          </DatabaseInfoSection>
-        )}
+          {shouldShowModelFeaturesSection && (
+            <DatabaseInfoSection
+              name={t`Model features`}
+              description={t`Choose whether to enable features related to Metabase models. These will often require a write connection.`}
+            >
+              <DatabaseModelFeaturesSectionContent
+                database={database}
+                isModelPersistenceEnabled={isModelPersistenceEnabled}
+                updateDatabase={updateDatabase}
+              />
+            </DatabaseInfoSection>
+          )}
 
-        <Modal
-          title={t`Edit connection details`}
-          opened={dbDetailsModal.opened}
-          onClose={dbDetailsModal.close}
-        >
-          <DatabaseEditConnectionForm
-            database={database}
-            initializeError={initializeError}
-            onChangeLocation={onChangeLocation}
-            route={route}
-          />
-        </Modal>
-      </Box>
-    </ErrorBoundary>
+          {shouldShowDangerZone && (
+            <DatabaseInfoSection
+              name={t`Danger zone`}
+              description={t`Remove this database and other destructive actions`}
+            >
+              <DatabaseDangerZoneSectionContent
+                isAdmin={isAdmin}
+                database={database}
+              />
+            </DatabaseInfoSection>
+          )}
+        </Box>
+      </ErrorBoundary>
+      {children}
+    </>
   );
 }
 
