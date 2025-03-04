51fdff0ebd32ed9678e6785890b284cab607b831
Denis Berezin <denis.berezin@metabase.com>
2025-03-04 22:14:26 +0300

    Add proper feature toggle name

--- enterprise/frontend/src/metabase-enterprise/data_editing/index.ts
+++ enterprise/frontend/src/metabase-enterprise/data_editing/index.ts
@@ -4,7 +4,7 @@ import { PLUGIN_DATA_EDITING } from "metabase/plugins";
 import { TableDataContainer } from "./tables";
 
 // TODO[WRK]: enable this check after this feature token is added on the BE
-// if (hasPremiumFeature("data_editing")) {
+// if (hasPremiumFeature("table-data-editing")) {
 PLUGIN_DATA_EDITING.isEnabled = () => true;
 PLUGIN_DATA_EDITING.PAGE_COMPONENT = TableDataContainer;
 // }
