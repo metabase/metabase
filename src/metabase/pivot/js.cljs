0edf1f436b4d9fd9979c609d950a76cdcfa8f022
Noah Moss <noahbmoss@gmail.com>
2025-03-05 15:43:17 -0500

    Fix lint issues

--- src/metabase/pivot/js.cljs
+++ src/metabase/pivot/js.cljs
@@ -43,6 +43,6 @@
         cols         (js->clj cols :keywordize-keys true)
         settings     (js->clj settings :keywordize-keys true)
         col-settings (js->clj col-settings :keywordize-keys true)
-        ;; On the FE, always format rows — `false` only applies to downloads
+        ;; On the FE, always format rows (false only applies to downloads)
         format-rows? true]
-    (clj->js (pivot/process-pivot-table data row-indexes col-indexes val-indexes cols top-formatters left-formatters  format-rows? settings col-settings make-color-getter))))
+    (clj->js (pivot/process-pivot-table data row-indexes col-indexes val-indexes cols top-formatters left-formatters value-formatters format-rows? settings col-settings make-color-getter))))
