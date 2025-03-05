fe707e4c5ee0909e973e2b0b1ca709b33f0e815c
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-03-05 13:38:40 -0500

    Do no singularize or pluralize table and column names in drills (#54659)

--- e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-3.cy.spec.js
@@ -132,7 +132,7 @@ describe("issue 33079", () => {
     H.createQuestion(questionDetails, { visitQuestion: true });
     H.cartesianChartCircle().eq(1).click({ force: true });
     H.popover()
-      .findByText(/Order/) // See these Orders
+      .findByText("Siehe diese Einträge") // See these records
       .click();
     cy.wait("@dataset");
     cy.findByTestId("question-row-count").should("contain", "19");
