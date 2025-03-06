de730d4e85162585af25c2741e6025046399a9aa
Sébastien <sebastien@metabase.com>
2025-03-06 11:23:53 +0100

    Closes VIZ-522: crash when column is unknown in settings

--- frontend/src/metabase/visualizer/utils/index.ts
+++ frontend/src/metabase/visualizer/utils/index.ts
@@ -2,6 +2,7 @@ export * from "./column";
 export * from "./dashboard-card-supports-visualizer";
 export * from "./data-source";
 export * from "./drag-and-drop";
+export * from "./get-initial-state-for-card-data-source";
 export * from "./is-visualizer-dashboard-card";
 export * from "./merge-data";
 export * from "./misc";
