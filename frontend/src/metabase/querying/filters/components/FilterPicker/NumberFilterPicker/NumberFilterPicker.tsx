9f625a4dd2f8f5aaa05f31ed833c8004780686c8
Kamil Mielnik <kamil@kamilmielnik.com>
2025-03-06 07:02:39 -0500

    Menu

--- frontend/src/metabase/querying/filters/components/FilterPicker/NumberFilterPicker/NumberFilterPicker.tsx
+++ frontend/src/metabase/querying/filters/components/FilterPicker/NumberFilterPicker/NumberFilterPicker.tsx
@@ -1,20 +1,19 @@
-import { Button, Chip } from "@mantine/core";
+import { Chip } from "@mantine/core";
 import type { FormEvent } from "react";
 import { useMemo, useState } from "react";
+import { usePreviousDistinct } from "react-use";
 
 import { useNumberFilter } from "metabase/querying/filters/hooks/use-number-filter";
-import { Box, Flex, Icon, Menu } from "metabase/ui";
+import { Box, Flex, Menu } from "metabase/ui";
 import * as Lib from "metabase-lib";
 
-import { FilterOperatorPicker } from "../FilterOperatorPicker";
 import { FilterPickerFooter } from "../FilterPickerFooter";
-import { FilterPickerHeader } from "../FilterPickerHeader";
 import { WIDTH } from "../constants";
 import type { FilterPickerWidgetProps } from "../types";
 
+import { Chart } from "./Chart";
 import { NumberValueInput } from "./NumberValueInput";
 import { RangePicker } from "./RangePicker";
-import { Chart } from "./Chart";
 
 export function NumberFilterPicker({
   query,
@@ -51,22 +50,34 @@ export function NumberFilterPicker({
     filter,
   });
 
+  const previousOperator = usePreviousDistinct(operator);
+
   const availableOptions = useMemo(() => {
     return availableOptions2.sort((a, b) => {
+      // if (previousOperator === 'between' && a.operator === '=') {
+      //   return 0;
+      // }
+      // if (a.operator === "between") {
+      //   return -1;
+      // }
+      // if (a.operator === "=") {
+      //   return -1;
+      // }
       if (a.operator === "between") {
-        return -1;
-      }
-      if (a.operator === "=") {
         return -1;
       }
 
       return 0;
     });
-  }, [availableOptions2]);
+  }, [availableOptions2, operator, previousOperator]);
   const selectedOption = availableOptions.find(
     option => option.operator === operator,
   );
 
+  console.log({
+    operator,
+    previousOperator,
+  });
   const handleOperatorChange = (newOperator: Lib.NumberFilterOperator) => {
     setOperator(newOperator);
     setValues(getDefaultValues(newOperator, values));
@@ -95,6 +106,10 @@ export function NumberFilterPicker({
                 // c={"#4C5773"}
                 // ta={"left"}
                 // style={{textAlign:'left'}}
+                onClick={() => {
+                  handleOperatorChange(option.operator);
+                  setIsOpen(false);
+                }}
               >
                 {option.name}
               </Menu.Item>
@@ -114,38 +129,70 @@ export function NumberFilterPicker({
     >
       <Flex gap="sm" p="md" pb={0}>
         <Chip.Group multiple={false} value={selectedOption?.operator}>
-          {availableOptions.slice(0, 2).map(option => (
-            <Chip
-              radius={"xl"}
-              variant={
-                selectedOption?.operator === option.operator
-                  ? "filled"
-                  : "outline"
+          {availableOptions
+            .filter(option => {
+              if (operator === "between") {
+                return option.operator === "between" || option.operator === "=";
               }
-              value={option.operator}
-              key={option.name}
-              styles={
-                selectedOption?.operator === option.operator
-                  ? {
-                      label: {
-                        // color: "#696E7B",
-                        // borderColor: "#dee2e6",
-                        // fontWeight: "bold",
-                      },
-                    }
-                  : {
-                      label: {
-                        color: "#696E7B",
-                        borderColor: "#dee2e6",
-                        fontWeight: "bold",
-                      },
-                    }
+
+              if (previousOperator === "between" && operator === "=") {
+                return option.operator === "between" || option.operator === "=";
               }
-              onChange={() => handleOperatorChange(option.operator)}
-            >
-              {option.name}
-            </Chip>
-          ))}
+
+              if (operator === "=" || operator === "!=") {
+                return option.operator === "=" || option.operator === "!=";
+              }
+
+              if (operator === "is-null" || operator === "not-null") {
+                return (
+                  option.operator === "is-null" ||
+                  option.operator === "not-null"
+                );
+              }
+
+              if (operator === "<" || operator === "<=") {
+                return option.operator === "<" || option.operator === "<=";
+              }
+
+              if (operator === ">" || operator == ">=") {
+                return option.operator === ">" || option.operator === ">=";
+              }
+
+              return true;
+            })
+            .slice(0, 2)
+            .map(option => (
+              <Chip
+                radius={"xl"}
+                variant={
+                  selectedOption?.operator === option.operator
+                    ? "filled"
+                    : "outline"
+                }
+                value={option.operator}
+                key={option.name}
+                styles={
+                  selectedOption?.operator === option.operator
+                    ? {
+                        label: {
+                          // color: "#696E7B",
+                          // borderColor: "#dee2e6",
+                          // fontWeight: "bold",
+                        },
+                      }
+                    : {
+                        label: {
+                          color: "#696E7B",
+                          borderColor: "#dee2e6",
+                          fontWeight: "bold",
+                        },
+                      }
+                }
+                onClick={() => handleOperatorChange(option.operator)}
+              >
+                {option.name}
+              </Chip>
+            ))}
           <Chip
             radius={"xl"}
             variant="outline"
