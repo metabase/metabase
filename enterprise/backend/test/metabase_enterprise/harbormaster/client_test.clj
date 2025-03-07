2731328399456b550180df938146ad188f42bfa0
Bryan Maass <bryan.maass@gmail.com>
2025-03-07 12:08:06 -0700

    switch off flaky test setup

--- enterprise/backend/test/metabase_enterprise/harbormaster/client_test.clj
+++ enterprise/backend/test/metabase_enterprise/harbormaster/client_test.clj
@@ -13,13 +13,22 @@
       (is (= {:store-api-url "http://store-api-url.com", :api-key "mb_api_key_123"}
              (#'hm.client/->config))))))
 
+(defn- +value-for-setting [grv setting-kw value]
+  (fn
+    ([k] (grv k))
+    ([k pred parse-fn]
+     (if (= k setting-kw) value (grv k pred parse-fn)))))
+
 (deftest ->config-blank-api-key-test
-  (mt/with-temporary-raw-setting-values [api-key ""]
-    (is (thrown-with-msg? Exception
-                          #"Missing api-key."
-                          (#'hm.client/->config))))
   (let [grv setting/get-raw-value]
-    (with-redefs [setting/get-raw-value (fn ([k] (grv k)) ([k pred parse-fn] (if (= k :api-key) nil (grv k pred parse-fn))))]
+    ;; mt/with-temporary-setting-values is not used here because we want to test the behavior of the function when the
+    ;; api-key is blank or nil, and mt/with-temporary-setting-values will not allow us to set the api-key to blank
+    ;; or nil.
+    (with-redefs [setting/get-raw-value (+value-for-setting grv :api-key "")]
+      (is (thrown-with-msg? Exception
+                            #"Missing api-key."
+                            (#'hm.client/->config))))
+    (with-redefs [setting/get-raw-value (+value-for-setting grv :api-key nil)]
       (is (thrown-with-msg? Exception
                             #"Missing api-key."
                             (#'hm.client/->config))))))
