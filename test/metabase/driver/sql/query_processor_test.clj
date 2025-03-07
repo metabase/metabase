7bc9aeee6828d24a74f53aa97f9e201572df9804
Mike Appleby <86076+appleby@users.noreply.github.com>
2025-03-07 14:17:15 +0300

    Extend BE test with more literal values

--- test/metabase/driver/sql/query_processor_test.clj
+++ test/metabase/driver/sql/query_processor_test.clj
@@ -1137,15 +1137,23 @@
       (is (= ["SELECT * FROM \"venues\" WHERE \"venues\".\"name\" = [my-string]"]
              (sql.qp/format-honeysql ::inline-value-test honeysql))))))
 
-(deftest ^:parallel query-with-wrapped-literal-expression-test
-  (testing "wrapped literal expression support"
-    (is (= [[1 "foo" 12345]
-            [2 "foo" 12345]]
+(deftest ^:parallel query-with-literal-custom-expression-test
+  (testing "literal custom expression support"
+    (is (= [[1 "" "foo" 0 12345 true false]
+            [2 "" "foo" 0 12345 true false]]
            (mt/rows
             (mt/run-mbql-query orders
-              {:expressions {"wrapped string" [:value "foo" nil]
-                             "wrapped int"    [:value 12345 nil]}
+              {:expressions {"empty"  [:value ""    nil]
+                             "foo"    [:value "foo" nil]
+                             "zero"   [:value 0     nil]
+                             "12345"  [:value 12345 nil]
+                             "True"   [:value true  nil]
+                             "False"  [:value false nil]}
                :fields [$id
-                        [:expression "wrapped string" nil]
-                        [:expression "wrapped int" nil]]
+                        [:expression "empty" nil]
+                        [:expression "foo"   nil]
+                        [:expression "zero"  nil]
+                        [:expression "12345" nil]
+                        [:expression "True"  nil]
+                        [:expression "False" nil]]
                :limit 2}))))))
