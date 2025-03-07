6c55ce87c3b7ece6536ec9367b1c7d64d8375989
Ryan Laurie <iethree@gmail.com>
2025-03-07 16:28:06 -0700

    add boxprops

--- frontend/src/metabase/admin/settings/components/SettingHeader/SettingHeader.tsx
+++ frontend/src/metabase/admin/settings/components/SettingHeader/SettingHeader.tsx
@@ -1,17 +1,19 @@
 import type React from "react";
 
+import type { BoxProps } from "metabase/ui";
 import { Box, Text } from "metabase/ui";
 
 export const SettingHeader = ({
   id,
   description,
   title,
+  ...boxProps
 }: {
   id: string;
   description?: string | React.ReactNode;
   title?: string;
-}) => (
-  <Box>
+} & BoxProps) => (
+  <Box {...boxProps}>
     <SettingTitle id={id}>{title}</SettingTitle>
     <Text my="sm" maw="38rem" lh="xl" c="text-medium">
       {description}
