40cd0b4df12e79cf900bfbab3f4611a3262a0568
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-10 20:14:17 +0100

    Add isCompilerPass helper

--- frontend/src/metabase-lib/v1/expressions/compiler.ts
+++ frontend/src/metabase-lib/v1/expressions/compiler.ts
@@ -1,6 +1,5 @@
 import { t } from "ttag";
 
-import { isNotNull } from "metabase/lib/types";
 import * as Lib from "metabase-lib";
 import type Database from "metabase-lib/v1/metadata/Database";
 import type { Expression } from "metabase-types/api";
@@ -12,6 +11,7 @@ import {
   adjustMultiArgOptions,
   adjustOffset,
   adjustOptions,
+  isCompilerPass,
 } from "./passes";
 import { compile, lexify, parse } from "./pratt";
 import type { ErrorWithMessage, StartRule } from "./types";
@@ -64,7 +64,7 @@ export function compileExpression({
       startRule,
     }),
     adjustBooleans,
-  ].filter(isNotNull);
+  ].filter(isCompilerPass);
 
   try {
     let expression = compile(root);
