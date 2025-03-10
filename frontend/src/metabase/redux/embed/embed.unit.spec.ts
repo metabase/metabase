4830499fa249b0947b43da973289bfafd97c061e
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-03-10 19:50:19 +0700

    Add redux slice tests

--- frontend/src/metabase/redux/embed/embed.unit.spec.ts
+++ frontend/src/metabase/redux/embed/embed.unit.spec.ts
@@ -40,6 +40,123 @@ describe("embed reducer", () => {
         "invalid_option",
       );
     });
+
+    describe("entity_types", () => {
+      it('should accept multiple "entity_types" options', () => {
+        const store = createMockStore();
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "entity_types=table&entity_types=model",
+          }),
+        );
+
+        // The default value is `["model", "table"]`, so we know these 2 types are set correctly.
+        expect(store.getState().embed.options.entity_types).toEqual([
+          "table",
+          "model",
+        ]);
+      });
+
+      it('should accept single "entity_types" option', () => {
+        const store = createMockStore();
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "entity_types=model",
+          }),
+        );
+
+        expect(store.getState().embed.options.entity_types).toEqual(["model"]);
+      });
+
+      it('should accept comma separated "entity_types" option', () => {
+        const store = createMockStore();
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "entity_types=model,table",
+          }),
+        );
+
+        expect(store.getState().embed.options.entity_types).toEqual([
+          "model",
+          "table",
+        ]);
+      });
+
+      it('should accept comma separated "entity_types" option with space', () => {
+        const store = createMockStore();
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "entity_types= table , model",
+          }),
+        );
+
+        expect(store.getState().embed.options.entity_types).toEqual([
+          "table",
+          "model",
+        ]);
+      });
+
+      it('should ignore invalid "entity_types" option', () => {
+        const store = createMockStore();
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "entity_types=not_a_valid_type",
+          }),
+        );
+
+        // Default value
+        expect(store.getState().embed.options.entity_types).toEqual([
+          "model",
+          "table",
+        ]);
+      });
+
+      it('should set "entity_types" option to the default value `["model", "table"]` when "entity_types" are empty', () => {
+        const store = createMockStore();
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "entity_types=",
+          }),
+        );
+
+        // Default value
+        expect(store.getState().embed.options.entity_types).toEqual([
+          "model",
+          "table",
+        ]);
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "",
+          }),
+        );
+
+        // Default value
+        expect(store.getState().embed.options.entity_types).toEqual([
+          "model",
+          "table",
+        ]);
+      });
+
+      it('should ignore "entity_types" that is not the first option when the first option has comma', () => {
+        const store = createMockStore();
+
+        store.dispatch(
+          setInitialUrlOptions({
+            search: "entity_types= table, invalid&entity_types=model",
+          }),
+        );
+
+        // Default value
+        expect(store.getState().embed.options.entity_types).toEqual(["table"]);
+      });
+    });
   });
 });
 
