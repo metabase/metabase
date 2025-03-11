7f220a98f0a487e3937b449529c515cf45661f95
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-03-11 16:46:05 +0700

    Put tests file in a proper folder
    So the SDK build process doesn't check test file types

--- frontend/src/metabase/querying/notebook/components/NotebookDataPicker/EmbeddingDataPicker/EmbeddingDataPicker.tsx
+++ frontend/src/metabase/querying/notebook/components/NotebookDataPicker/EmbeddingDataPicker/EmbeddingDataPicker.tsx
@@ -9,7 +9,7 @@ import { getMetadata } from "metabase/selectors/metadata";
 import * as Lib from "metabase-lib";
 import type { TableId } from "metabase-types/api";
 
-import { DataPickerTarget } from "./NotebookDataPicker";
+import { DataPickerTarget } from "../NotebookDataPicker";
 
 type EmbeddingDataPickerProps = {
   query: Lib.Query;
