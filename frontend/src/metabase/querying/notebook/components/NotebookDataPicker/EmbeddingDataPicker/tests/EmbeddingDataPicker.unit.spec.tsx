09717292d5e8b50d568bfae1ec5e56609d67714b
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-03-11 19:43:20 +0700

    Add more assertions to make sure more paths are tested

--- frontend/src/metabase/querying/notebook/components/NotebookDataPicker/EmbeddingDataPicker/tests/EmbeddingDataPicker.unit.spec.tsx
+++ frontend/src/metabase/querying/notebook/components/NotebookDataPicker/EmbeddingDataPicker/tests/EmbeddingDataPicker.unit.spec.tsx
@@ -1,3 +1,5 @@
+import userEvent from "@testing-library/user-event";
+
 import { screen } from "__support__/ui";
 
 import { setup } from "./setup";
@@ -7,18 +9,34 @@ describe("EmbeddingDataPicker", () => {
     it("should show tables when there is no models", async () => {
       setup({ hasModels: false });
 
-      expect(await screen.findByText("Sample Database")).toBeInTheDocument();
+      const databaseOption = await screen.findByText("Sample Database");
+      expect(databaseOption).toBeInTheDocument();
+
       expect(screen.queryByText("Models")).not.toBeInTheDocument();
       expect(screen.queryByText("Raw Data")).not.toBeInTheDocument();
+
+      await userEvent.click(databaseOption);
+      expect(await screen.findByText("Orders")).toBeInTheDocument();
+      expect(screen.getByText("People")).toBeInTheDocument();
+      expect(screen.getByText("Products")).toBeInTheDocument();
+      expect(screen.getByText("Reviews")).toBeInTheDocument();
     });
 
     it('should show "BUCKET" step when there are both models and tables', async () => {
       setup();
 
       expect(await screen.findByText("Models")).toBeInTheDocument();
-      expect(screen.getByText("Raw Data")).toBeInTheDocument();
+      const rawDataOption = screen.getByText("Raw Data");
+      expect(rawDataOption).toBeInTheDocument();
 
       expect(screen.queryByText("Sample Database")).not.toBeInTheDocument();
+
+      await userEvent.click(rawDataOption);
+      await userEvent.click(screen.getByText("Sample Database"));
+      expect(await screen.findByText("Orders")).toBeInTheDocument();
+      expect(screen.getByText("People")).toBeInTheDocument();
+      expect(screen.getByText("Products")).toBeInTheDocument();
+      expect(screen.getByText("Reviews")).toBeInTheDocument();
     });
 
     describe("entity_types", () => {
