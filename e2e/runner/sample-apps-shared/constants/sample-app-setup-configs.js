ce95346855e0fb89ba19b123a8cc3a43e6f55db9
Timofei <timofei@metabase.com>
2025-03-11 12:54:14 +0400

    Update CI logic

--- e2e/runner/sample-apps-shared/constants/sample-app-setup-configs.js
+++ e2e/runner/sample-apps-shared/constants/sample-app-setup-configs.js
@@ -1,4 +1,4 @@
-const BRANCH_NAME = "release-x.52.x";
+const BRANCH_NAME = "release-x.52.x"; // Affects the `local` testing only. On CI is passed as an ENV variable.
 
 const BASE_ENV = {
   PREMIUM_EMBEDDING_TOKEN: process.env.CYPRESS_ALL_FEATURES_TOKEN ?? "",
