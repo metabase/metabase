eb813b80544b6c64cfa4ba5ff330aa4e4abb9314
William Zimrin <william@metabase.com>
2025-03-11 16:27:42 -0500

    SQL Server doesn't cast when compiling uuid field filters
    Apparently, a filter like `blah in (cast([uuid] as uuid))` is fine in postgres, but sqlserver doesn't like it very
    much.  This gives sql server the syntax it needs.

--- test/metabase/query_processor_test/uuid_test.clj
+++ test/metabase/query_processor_test/uuid_test.clj
@@ -3,6 +3,7 @@
   (:require
    [clojure.test :refer :all]
    [metabase.driver :as driver]
+   [metabase.query-processor :as qp]
    [metabase.test :as mt]))
 
 (defmethod driver/database-supports? [::driver/driver ::uuids-in-create-table-statements]
@@ -38,3 +39,23 @@
                    :filter [:=
                             [:field (mt/id :dogs :id) {:base-type "type/UUID"}]
                             "27e164bc-54f8-47a0-a85a-9f0e90dd7667"]}))))))))
+
+(deftest ^:parallel field-filter-uuid-test
+  (mt/test-drivers (mt/normal-drivers-with-feature :native-parameters :uuid-type
+                                                   ::uuids-in-create-table-statements)
+    (testing "uuid field filters"
+      (mt/dataset uuid-dogs
+        (let [query {:database   (mt/id)
+                     :type       :native
+                     :native     (assoc (mt/count-with-field-filter-query driver/*driver* :people :id "27e164bc-54f8-47a0-a85a-9f0e90dd7667")
+                                        :template-tags {"id" {:name "id"
+                                                              :display-name "id"
+                                                              :type         :dimension
+                                                              :widget-type  :id
+                                                              :dimension    [:field (mt/id :people :id) nil]}})
+                     :parameters [{:type   :id
+                                   :name   "id"
+                                   :target [:dimension [:template-tag "id"]]
+                                   :value  "d6b02fa2-bf7b-4b32-80d5-060b649c9859"}]}]
+          (mt/with-native-query-testing-context query
+            (is (= 1 (ffirst (mt/rows (qp/process-query query)))))))))))
