281d9f5aa4e1acebb68d1393c23d2c87bd53b696
Konstantin Darutkin <spalt08@mail.ru>
2025-03-11 15:11:14 +0000

    fix: review issues

--- frontend/src/metabase/data-grid/utils/cell.ts
+++
@@ -1,4 +0,0 @@
-// should be in sync with node_modules/@tanstack/table-core/src/core/cell.ts logic
-export const getGridCellId = (rowIndex: number, columnName: string): string => {
-  return `${rowIndex}_${columnName}`;
-};
