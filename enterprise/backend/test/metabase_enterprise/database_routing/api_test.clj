196ea2820af781456a8fa7b6532cff52268028ab
John Swanson <john.swanson@metabase.com>
2025-03-11 10:37:01 -0700

    CRUD API for database routing (#54102)
    * Migrations for database sandboxing
    
    Two changes here:
    
    - add a self-referential foreign key,
    `metabase_database.primary_database_id`, to allow marking a mirror
    database's primary database. Databases with this set are mirror
    databases, databases without it are primary databases.
    
    - add a `db_sandbox` table with only three columns:
    
      - `id`, primary ID,
    
      - `db_id`, the `metabase_database.id` this sandbox is for, and
    
      - `user_attribute`, the user attribute used to map users to a
        particular mirror database.
    
    * Bulk endpoint for creating mirror dbs
    
    The error case is a bit wonky right now, it just returns the errors for
    the details that failed... without the details or name.
    
    Co-authored-by: Ed Paget <ed@metabase.com>
    
    * Add endpoint for marking a database as a RouterDB
    
    Co-authored-by: Ed Paget <ed@metabase.com>
    
    * Rebase fixes
    
    * Exclude mirrordbs from regular db endpoints
    
    * kondo fixes
    
    * mark `DatabaseRouter` as not serialized
    
    * serdes fix
    
    * copy fix
    
    * delete and update routers
    
    Also reorganized things to be a bit less cattywompus.
    
    Now we have:
    
    - `POST api/ee/database-routing/mirror-database` to create a new mirror database
    - `POST api/ee/database-routing/router` to create a new router using an
      existing database
    - `PUT api/ee/database-routing/router/:id` to update a router to use a
      new user attribute
    - `DELETE api/ee/database-routing/router/:id` to delete a router and all
      of the associated mirror databases.
    
    * GET endpoint for mirrors of a particular router db
    
    * Don't test database conn for mirror dbs
    
    ---------
    
    Co-authored-by: Ed Paget <ed@metabase.com>

---
+++ enterprise/backend/test/metabase_enterprise/database_routing/api_test.clj
@@ -0,0 +1,146 @@
+(ns metabase-enterprise.database-routing.api-test
+  (:require
+   [clojure.test :refer [deftest testing is use-fixtures]]
+   [metabase.driver :as driver]
+   [metabase.driver.h2]
+   [metabase.test :as mt]
+   [toucan2.core :as t2]))
+
+(defn with-h2-fixture [f]
+  (binding [metabase.driver.h2/*allow-testing-h2-connections* true]
+    (f)))
+
+(defn with-premium-feature-fixture [f]
+  (mt/with-premium-features #{:database-routing}
+    (f)))
+
+(defn with-test-db-is-router-db [f]
+  (mt/with-temp [:model/DatabaseRouter _ {:database_id (mt/id)
+                                          :user_attribute "meow"}]
+    (f)))
+
+(use-fixtures :each with-h2-fixture with-premium-feature-fixture with-test-db-is-router-db)
+
+(deftest creation-works
+  (mt/with-model-cleanup [:model/Database]
+    (let [[{db-id :id}] (mt/user-http-request :crowberto :post 200 "ee/database-routing/mirror-database"
+                                              {:router_database_id (mt/id)
+                                               :mirrors [{:name "mirror database"
+                                                          :details (:details (mt/db))}]})]
+      (is (t2/exists? :model/Database
+                      :router_database_id (mt/id)
+                      :id db-id)))))
+
+(deftest invalid-details-doesnt-matter
+  (mt/with-model-cleanup [:model/Database]
+    (with-redefs [driver/can-connect? (fn [_]
+                                        (throw (ex-info "nope" {})))]
+      (let [[{db-id :id}] (mt/user-http-request :crowberto :post 200 "ee/database-routing/mirror-database"
+                                                {:router_database_id (mt/id)
+                                                 :mirrors [{:name "mirror database"
+                                                            :details {:db "meow"}}]})]
+        (is (t2/exists? :model/Database
+                        :router_database_id (mt/id)
+                        :id db-id))))))
+
+(deftest we-can-mark-an-existing-database-as-being-a-router-database
+  (mt/with-temp [:model/Database {db-id :id} {}]
+    (mt/with-model-cleanup [:model/DatabaseRouter]
+      (mt/user-http-request :crowberto :post 200 "ee/database-routing/router"
+                            {:user_attribute "foo"
+                             :database_id db-id})
+      (is (t2/exists? :model/DatabaseRouter :database_id db-id :user_attribute "foo")))))
+
+(deftest marking-a-nonexistent-database-as-a-router-database-fails
+  (let [nonexistent-id 123456789]
+    (mt/with-model-cleanup [:model/DatabaseRouter]
+      (mt/user-http-request :crowberto :post 404 "ee/database-routing/router"
+                            {:user_attribute "foo"
+                             :database_id nonexistent-id})
+      (is (not (t2/exists? :model/DatabaseRouter :database_id nonexistent-id :user_attribute "foo"))))))
+
+(deftest marking-something-that-is-already-a-router-database-fails
+  (mt/with-temp [:model/Database {db-id :id} {}
+                 :model/DatabaseRouter _ {:database_id db-id :user_attribute "foo"}]
+    (mt/user-http-request :crowberto :post 400 "ee/database-routing/router"
+                          {:user_attribute "bar"
+                           :database_id db-id})))
+
+(deftest we-can-update-existing-router-databases-to-point-to-a-new-user-attribute
+  (mt/with-temp [:model/Database {db-id :id} {}
+                 :model/DatabaseRouter {router-id :id} {:database_id db-id :user_attribute "foo"}]
+    (mt/user-http-request :crowberto :put 200 (str "ee/database-routing/router/" router-id)
+                          {:user_attribute "bar"})))
+
+(deftest deleting-database-routers-works
+  (mt/with-temp [:model/Database {db-id :id} {}
+                 :model/DatabaseRouter {router-id :id} {:database_id db-id :user_attribute "foo"}
+                 :model/Database {mirror-db-id :id} {:router_database_id db-id}]
+    (mt/user-http-request :crowberto :delete 204 (str "ee/database-routing/router/" router-id))
+    (is (not (t2/exists? :model/Database :id mirror-db-id)))
+    (is (not (t2/exists? :model/DatabaseRouter :id router-id)))))
+
+(deftest endpoint-are-locked-down-to-superusers-only
+  (testing "POST /api/ee/database-routing/mirror-database"
+    (mt/with-model-cleanup [:model/Database]
+      (mt/user-http-request :rasta :post 403 "ee/database-routing/mirror-database"
+                            {:router_database_id (mt/id)
+                             :mirrors [{:name "mirror database"
+                                        :details (:details (mt/db))}]})))
+  (testing "POST /api/ee/database-routing/router"
+    (mt/with-temp [:model/Database {db-id :id} {}]
+      (mt/with-model-cleanup [:model/DatabaseRouter]
+        (mt/user-http-request :rasta :post 403 "ee/database-routing/router"
+                              {:user_attribute "foo"
+                               :database_id db-id}))))
+  (testing "PUT /api/ee/database-routing/router"
+    (mt/with-temp [:model/Database {db-id :id} {}
+                   :model/DatabaseRouter {router-id :id} {:database_id db-id :user_attribute "foo"}]
+      (mt/user-http-request :rasta :put 403 (str "ee/database-routing/router/" router-id)
+                            {:user_attribute "bar"}))))
+
+(deftest mirror-databases-are-hidden-from-regular-database-api
+  (mt/with-temp [:model/Database {db-id :id} {}
+                 :model/DatabaseRouter _ {:database_id db-id :user_attribute "foo"}
+                 :model/Database {mirror-db-id :id} {:router_database_id db-id}]
+    (testing "GET /database/:id"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id))
+      (testing "If we pass the `include_mirror_databases` param, it is included"
+        (mt/user-http-request :crowberto :get 200 (str "database/" mirror-db-id "?include_mirror_databases=true")))
+      (testing "If a regular user passees `include_mirror_databases` it is hidden"
+        (mt/user-http-request :rasta :get 404 (str "database/" mirror-db-id "?include_mirror_databases=true"))))
+    (testing "GET /database/"
+      (is (not-any? #(= (:id %) mirror-db-id)
+                    (:data (mt/user-http-request :crowberto :get 200 "database/"))))
+      (testing "If we pass the `include_mirror_databases` param it is included"
+        (is (some #(= (:id %) mirror-db-id)
+                  (:data (mt/user-http-request :crowberto :get 200 (str "database/?include_mirror_databases=" db-id))))))
+      (testing "Regular users can't do this"
+        (is (not-any? #(= (:id %) mirror-db-id)
+                      (:data (mt/user-http-request :rasta :get 200 (str "database/?include_mirror_databases=" db-id)))))))
+    (testing "PUT /database/:id should work normally"
+      (mt/user-http-request :crowberto :put 200 (str "database/" mirror-db-id)))
+    (testing "GET /database/:id/usage_info"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/usage_info")))
+    (testing "GET /database/:id/metadata"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/metadata")))
+    (testing "GET /database/:id/autocomplete_suggestions"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/autocomplete_suggestions")))
+    (testing "GET /database/:id/card_autocomplete_suggestions"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/card_autocomplete_suggestions?query=foobar")))
+    (testing "GET /database/:id/fields"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/fields")))
+    (testing "GET /database/:id/idfields"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/idfields")))
+    (testing "POST /database/:id/sync_schema"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/sync_schema")))
+    (testing "POST /database/:id/dismiss_spinner"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/dismiss_spinner")))
+    (testing "POST /database/:id/rescan_values"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/rescan_values")))
+    (testing "POST /database/:id/discard_values"
+      (mt/user-http-request :crowberto :post 404 (str "database/" mirror-db-id "/discard_values")))
+    (testing "POST /database/:id/syncable_schemas"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/syncable_schemas")))
+    (testing "GET /database/:id/schemas"
+      (mt/user-http-request :crowberto :get 404 (str "database/" mirror-db-id "/schemas")))))
