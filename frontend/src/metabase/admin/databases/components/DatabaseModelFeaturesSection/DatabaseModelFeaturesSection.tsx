3c470e34ba26db3196d77128620ccaddf9a57506
Denis Berezin <denis.berezin@metabase.com>
2025-03-13 00:08:00 +0300

    Adapt to new design
    (cherry picked from commit 3cac55139c87424cf4084d3b2432919f8b7c3acd)

--- frontend/src/metabase/admin/databases/components/DatabaseModelFeaturesSection/DatabaseModelFeaturesSection.tsx
+++ frontend/src/metabase/admin/databases/components/DatabaseModelFeaturesSection/DatabaseModelFeaturesSection.tsx
@@ -1,7 +1,6 @@
 import { useCallback } from "react";
 import { t } from "ttag";
 
-import { PLUGIN_DATA_EDITING } from "metabase/plugins";
 import { Flex } from "metabase/ui";
 import type Database from "metabase-lib/v1/metadata/Database";
 import type { DatabaseData, DatabaseId } from "metabase-types/api";
@@ -10,7 +9,6 @@ import { DatabaseInfoSection } from "../DatabaseInfoSection";
 
 import { ModelActionsSection } from "./ModelActionsSection";
 import { ModelCachingControl } from "./ModelCachingControl";
-import { TableEditingSection } from "./TableEditingSection";
 
 export const DatabaseModelFeaturesSection = ({
   database,
@@ -25,13 +23,10 @@ export const DatabaseModelFeaturesSection = ({
 }) => {
   const isEditingDatabase = !!database.id;
 
-  const showModelActions = isEditingDatabase && database.supportsActions();
   const contentVisibility = {
-    showModelActions,
+    showModelActions: isEditingDatabase && database.supportsActions(),
     showModelCachingSection:
       isModelPersistenceEnabled && database.supportsPersistence(),
-    showTableEditingSection:
-      showModelActions && PLUGIN_DATA_EDITING.isEnabled(),
   };
 
   const hasNoContent = Object.values(contentVisibility).every(x => x === false);
@@ -41,15 +36,6 @@ export const DatabaseModelFeaturesSection = ({
       updateDatabase({
         id: database.id,
         settings: { "database-enable-actions": nextValue },
-      }),
-    [database.id, updateDatabase],
-  );
-
-  const handleToggleTableEditingEnabled = useCallback(
-    (nextValue: boolean) =>
-      updateDatabase({
-        id: database.id,
-        settings: { "database-enable-table-editing": nextValue },
       }),
     [database.id, updateDatabase],
   );
@@ -74,13 +60,6 @@ export const DatabaseModelFeaturesSection = ({
 
         {contentVisibility.showModelCachingSection && (
           <ModelCachingControl database={database} />
-        )}
-
-        {contentVisibility.showTableEditingSection && (
-          <TableEditingSection
-            isEnabled={database.hasTableEditingEnabled()}
-            onToggle={handleToggleTableEditingEnabled}
-          />
         )}
       </Flex>
     </DatabaseInfoSection>
---
+++ frontend/src/metabase/admin/databases/components/DatabaseTableEditingSection.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { t } from "ttag";
+
+import Toggle from "metabase/core/components/Toggle";
+import { getResponseErrorMessage } from "metabase/lib/errors";
+import { PLUGIN_DATA_EDITING } from "metabase/plugins";
+import { Box, Flex } from "metabase/ui";
+import type Database from "metabase-lib/v1/metadata/Database";
+import type { DatabaseData, DatabaseId } from "metabase-types/api";
+
+import { DatabaseInfoSection } from "./DatabaseInfoSection";
+import {
+  Description,
+  Error,
+  Label,
+} from "./DatabaseModelFeaturesSection/ModelFeatureToggles.styled";
+
+export function DatabaseTableEditingSection({
+  database,
+  updateDatabase,
+}: {
+  database: Database;
+  updateDatabase: (
+    database: { id: DatabaseId } & Partial<DatabaseData>,
+  ) => Promise<void>;
+}) {
+  const isEditingDatabase = !!database.id;
+  const showTableEditingSection =
+    isEditingDatabase &&
+    database.supportsActions() &&
+    PLUGIN_DATA_EDITING.isEnabled();
+
+  const [error, setError] = useState<string | null>(null);
+
+  const handleToggle = async (enabled: boolean) => {
+    try {
+      setError(null);
+
+      await updateDatabase({
+        id: database.id,
+        settings: { "database-enable-table-editing": enabled },
+      });
+    } catch (err) {
+      setError(getResponseErrorMessage(err) || t`An error occurred`);
+    }
+  };
+
+  if (!showTableEditingSection) {
+    return null;
+  }
+
+  return (
+    <DatabaseInfoSection
+      name={t`Editable tables`}
+      description={""}
+      data-testid="database-table-editing-section"
+    >
+      <div>
+        <Flex align="center" justify="space-between" mb="xs">
+          <Label htmlFor="table-editing-toggle">{t`Editable tables`}</Label>
+          <Toggle
+            id="table-editing-toggle"
+            value={database.hasTableEditingEnabled()}
+            onChange={handleToggle}
+          />
+        </Flex>
+        <Box maw="22.5rem">
+          {error ? <Error>{error}</Error> : null}
+          <Description>
+            {t`Allow Admins to edit tables in this database.`}
+            <br />
+            {t`Your database connection will need Write permissions.`}
+          </Description>
+        </Box>
+      </div>
+    </DatabaseInfoSection>
+  );
+}
