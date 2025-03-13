6be233d7461221050e687e26e490aa7586a911ec
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-13 16:47:20 +0100

    Add HighlightExpression component

--- frontend/src/metabase/query_builder/components/expressions/HighlightExpression/HighlightExpression.tsx
+++ frontend/src/metabase/query_builder/components/expressions/HighlightExpression/HighlightExpression.tsx
@@ -1,11 +1,40 @@
 import { type HTMLAttributes, useMemo } from "react";
 import { useAsync } from "react-use";
 
-import { formatExample } from "metabase-lib/v1/expressions";
+import type * as Lib from "metabase-lib";
+import { format, formatExample } from "metabase-lib/v1/expressions";
 import type { Expression } from "metabase-types/api";
 
 import S from "./HighlightExpression.module.css";
 import { highlight } from "./utils";
+
+export function HighlightExpression({
+  expression,
+  query,
+  stageIndex,
+  expressionIndex,
+  printWidth = Infinity,
+  ...props
+}: {
+  expression: Expression;
+  query: Lib.Query;
+  stageIndex: number;
+  expressionIndex?: number;
+  printWidth?: number;
+} & HTMLAttributes<HTMLPreElement>) {
+  const { value: formattedExpression } = useAsync(
+    () =>
+      format(expression, { query, stageIndex, expressionIndex, printWidth }),
+    [expression, query, stageIndex, expressionIndex, printWidth],
+  );
+
+  return (
+    <HighlightExpressionSource
+      expression={formattedExpression ?? ""}
+      {...props}
+    />
+  );
+}
 
 export function HighlightExampleExpression({
   expression,
