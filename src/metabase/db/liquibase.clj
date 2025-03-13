849500329f150250ba916b192701c7b3afbb10c2
Nathan Voxland <nathan@metabase.com>
2025-03-13 16:08:00 -0500

    Improved code and added tests

--- src/metabase/db/liquibase.clj
+++ src/metabase/db/liquibase.clj
@@ -485,8 +485,8 @@
   (->> liquibase
        (.getDatabaseChangeLog)
        (.getChangeSets)
-       (map #(.getId ^ChangeSet %))
        last
+       (.getId)
        extract-numbers
        first))
 
@@ -494,35 +494,37 @@
   "Gets the latest version applied to the database."
   [conn ^Database database]
   (when-not (fresh-install? conn database)
-    (let [changeset-query (format "SELECT id FROM %s WHERE id LIKE 'v%%' ORDER BY ORDEREXECUTED DESC LIMIT 1"
+    (let [changeset-query (format "SELECT id FROM %s WHERE id LIKE 'v%%' ORDER BY id DESC LIMIT 1"
                                   (.getDatabaseChangeLogTableName database))
           changeset-id (last (map :id (jdbc/query {:connection conn} [changeset-query])))]
       (some-> changeset-id extract-numbers first))))
 
 (defn rollback-major-version
-  "Roll back migrations later than given Metabase major version"
+  "Roll back migrations later than given Metabase major version. If force is true, it will ignore any checks and always roll back"
   ;; default rollback to previous version
-  ([conn liquibase]
+  ([conn liquibase force]
    ;; get current major version of Metabase we are running
-   (rollback-major-version conn liquibase (dec (config/current-major-version))))
+   (rollback-major-version conn liquibase force (dec (config/current-major-version))))
 
   ;; with explicit target version
-  ([conn ^Liquibase liquibase target-version]
+  ([conn ^Liquibase liquibase force target-version]
    (when (or (not (integer? target-version)) (< target-version 44))
      (throw (IllegalArgumentException.
              (format "target version must be a number between 44 and the previous major version (%d), inclusive"
                      (config/current-major-version)))))
    (with-scope-locked liquibase
     ;; count and rollback only the applied change set ids which come after the target version (only the "v..." IDs need to be considered)
-     (let [changeset-query (format "SELECT id FROM %s WHERE id LIKE 'v%%' ORDER BY ORDEREXECUTED ASC" (changelog-table-name liquibase))
+     (let [changeset-query (format "SELECT id FROM %s WHERE id LIKE 'v%%' ORDER BY ID ASC" (changelog-table-name liquibase))
            changeset-ids   (map :id (jdbc/query {:connection conn} [changeset-query]))
            ;; IDs in changesets do not include the leading 0/1 digit, so the major version is the first number
            ids-to-drop     (drop-while #(not= (inc target-version) (first (extract-numbers %))) changeset-ids)
            latest-available (latest-available-major-version liquibase)
            latest-applied   (latest-applied-major-version conn (.getDatabase liquibase))]
-       (when (> latest-applied latest-available)
-         (throw (IllegalArgumentException.
+       (when (and (not force) (> latest-applied latest-available))
+         (throw (ex-info
                  (format "Cannot downgrade a database at version %d from Metabase version %d. You must run 'migrate down' from Metabase version >= %d."
-                         latest-applied latest-available latest-applied))))
+                         latest-applied latest-available latest-applied)
+                 {:latest-available latest-available
+                  :latest-applied   latest-applied})))
        (log/infof "Rolling back app database schema to version %d" target-version)
        (.rollback liquibase (count ids-to-drop) "")))))
