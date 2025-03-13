7f5d39953177e35c9c9607e80870fcfe33e38c10
Chris Truter <chris@metabase.com>
2025-03-13 09:22:55 +0200

    Bring across the more complex example

--- enterprise/backend/src/metabase_enterprise/data_editing/foreign_keys.clj
+++ enterprise/backend/src/metabase_enterprise/data_editing/foreign_keys.clj
@@ -1,4 +1,6 @@
-(ns metabase-enterprise.data-editing.foreign-keys)
+(ns metabase-enterprise.data-editing.foreign-keys
+  (:require
+   [clojure.java.jdbc :as jdbc]))
 
 (defn take-with-sentinel
   "Similar to the [[take]] transducer, but emits a final sentinel if there were remaining items."
@@ -18,3 +20,49 @@
             (if (>= nn 0)
               result
               (ensure-reduced (rf result sentinel))))))))))
+
+(defn pop-queue [{:keys [queue] :as state}]
+  (if-let [nxt (first queue)]
+    [nxt (update state :queue subvec 1)]
+    [nil state]))
+
+(defn lookup-children-in-db [{:keys [table fk pk]} parents]
+  (jdbc/query
+   'db
+   {:select pk
+    :from   [table]
+    :where  (if (= 1 (count fk))
+              [:in (key (first fk)) (map (val (first fk)) parents)]
+              (into [:or] (for [p parents]
+                            (into [:and] (for [[fk-col pk-col] fk]
+                                           [:= fk-col (get p pk-col)])))))
+    :limit  501}))
+
+(defn- queue-items [state item-type items]
+  (if-not (seq items)
+    state
+    (let [new-items (remove (set (get-in state [:results item-type])) items)]
+      (-> state
+          (update :queue conj [item-type new-items])
+          (update-in [:results item-type] (fnil into #{}) new-items)))))
+
+(defn step [metadata children-fn state]
+  (let [[[parent-type items] state'] (pop-queue state)]
+    (if-not parent-type
+      state'
+      (let [type-metadata (get metadata parent-type)
+            child-keys    (for [relationship type-metadata]
+                            (children-fn relationship items))]
+        (reduce (fn [state [item-type items]]
+                  (queue-items state item-type items))
+                state'
+                child-keys)))))
+
+(defn walk [item-type items metadata children-fn & {:keys [max-queries]
+                                                    :or   {max-queries 100}}]
+  (:results
+   (reduce
+    (fn [state _]
+      (step metadata children-fn state))
+    {:queue [[item-type items]]}
+    (range max-queries))))
--- enterprise/backend/test/metabase_enterprise/data_editing/foreign_keys_test.clj
+++ enterprise/backend/test/metabase_enterprise/data_editing/foreign_keys_test.clj
@@ -1,8 +1,7 @@
 (ns metabase-enterprise.data-editing.foreign-keys-test
   (:require
    [clojure.test :refer :all]
-   [metabase-enterprise.data-editing.foreign-keys :as fks]
-   [metabase-enterprise.data-editing.foreign-keys2 :as fks2]))
+   [metabase-enterprise.data-editing.foreign-keys :as fks]))
 
 (deftest take-with-sentinel-test
   (is (= [0 1 2]
@@ -12,27 +11,28 @@
   (is (= [0 1 2 3 :truncated]
          (sequence (fks/take-with-sentinel 4 :truncated) (range 5)))))
 
-(def graph
-  {:user/ceo              {:reports-to [:user/cto
-                                        :user/cpo]}
-   :user/cto              {:belongs-to [:team/alpha
-                                        :team/bravo]
-                           :reports-to [:team/em]}
-   :user/cpo              {:lead-by    [:programme/skunk-works]
-                           :reports-to [:user/pm]}
-   :team/alpha            {:member-of [:user/em
-                                       :user/alice
-                                       :user/bob
-                                       :user/clarence]
-                           :owned-by  [:programme/skunk-works]}
-   :programme/skunk-works {:part-of [:project/gamma]}
-   :project/gamma         {:belongs-to [:task/foo]}
-   :user/em               {:managed-by [:team/alpha]}
-   :user/alice            {:assigned-to [:task/foo]}})
+(def metadata
+  {:orders     [{:table :order-items, :fk {:order-id :id}, :pk [:id]}]
 
-(def metadata {:orders [{:table :order-items, :fk {:order-id :id}, :pk [:id]}]
-               :people [{:table :people, :fk {:father :id}, :pk [:id]}
-                        {:table :people, :fk {:mother :id}, :pk [:id]}]})
+   ;; family tree
+
+   :people     [{:table :people, :fk {:father_id :id}, :pk [:id]}
+                {:table :people, :fk {:mother_id :id}, :pk [:id]}]
+
+   ;; project tracker
+
+   :users      [{:table :teams, :fk {:belongs-to :id}, :pk [:id]}
+                {:table :users, :fk {:reports-to :id}, :pk [:id]}
+                {:table :programmes, :fk {:guided-by :id}, :pk [:id]}]
+
+   :teams      [{:table :users, :fk {:managed-by :id}, :pk [:id]}
+                {:table :programmes, :fk {:focus :id}, :pk [:id]}]
+
+   :programmes [{:table :projects, :fk {:part-of :id}, :pk [:id]}]
+
+   :projects   [{:table :tasks, :fk {:project :id} :pk [:id]}]
+
+   :tasks      [{:table :users, :fk {:assigned-to :id}, :pk [:id]}]})
 
 (def db
   {:orders      [{:id 42}
@@ -40,13 +40,62 @@
    :order-items [{:id 1337, :order-id 42}
                  {:id 1338, :order-id 42}
                  {:id 1339, :order-id 43}]
+
+   ;; family tree
+
    :people      [{:id 1}
                  {:id 2}
-                 {:id 3 :father 1 :mother 2}
-                 {:id 4 :father 1 :mother 3}
-                 ;; it's a scandal, nobody knows
-                 {:id 5 :father nil :mother 3}]})
+                 {:id 3 :father_id 1 :mother_id 2}
+                 {:id 4 :father_id 1 :mother_id 3}
+                 {:id 5 :father_id nil :mother_id 3}]
+
+   ;; project tracker
+
+   :users       [{:id :user/ceo}
+                 {:id :user/cto, :reports-to :user/ceo}
+                 {:id :user/cpo, :reports-to :user/ceo}
+                 {:id :user/em, :reports-to :user/cto, :belongs-to :team/alpha}
+                 {:id :user/pm, :belongs-to :team/alpha}
+                 {:id :user/alice, :belongs-to :team/alpha}
+                 {:id :user/bob, :belongs-to :team/alpha}
+                 {:id :user/clarence, :belongs-to :team/alpha}]
+
+   :teams       [{:id :team/alpha, :managed-by :user/em, :focus :programme/skunk-works}
+                 {:id :team/bravo, :managed-by :user/cto}]
+
+   :programmes  [{:id :programme/skunk-works, :guided-by :user/cpo}]
+
+   :projects    [{:id :project/gamma, :part-of :programme/skunk-works}]
+
+   :tasks       [{:id :task/foo, :project :project/gamma, :assigned-to :user/alice}]})
+
+(defn matches? [parent fks child]
+  (= (map parent (vals fks))
+     (map child (keys fks))))
+
+(defn lookup-children [relationship parents]
+  [(:table relationship)
+   (for [child  (get db (:table relationship))
+         parent parents
+         :when (matches? parent (:fk relationship) child)]
+     (select-keys child (:pk relationship)))])
 
 (deftest walk-test
-  (fks2/walk :orders [{:id 42}] metadata)
-  (fks2/walk :people [{:id 1}] metadata))
+  (is (= {:order-items #{{:id 1338} {:id 1337}}}
+         (fks/walk :orders [{:id 42}] metadata lookup-children)))
+  (is (= {:people #{{:id 5} {:id 4} {:id 3}}}
+         (fks/walk :people [{:id 1}] metadata lookup-children)))
+  (is (= {:users      #{{:id :user/ceo}
+                        {:id :user/cto}
+                        {:id :user/cpo}
+                        {:id :user/em}
+                        {:id :user/pm}
+                        {:id :user/bob}
+                        {:id :user/clarence}
+                        {:id :user/alice}}
+          :teams      #{{:id :team/alpha}
+                        {:id :team/bravo}}
+          :programmes #{{:id :programme/skunk-works}}
+          :projects   #{{:id :project/gamma}}
+          :tasks      #{{:id :task/foo}}}
+         (fks/walk :users [{:user/ceo 1}] metadata lookup-children))))
