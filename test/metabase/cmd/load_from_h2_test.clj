b70817ccb4a0cd7d975baf539efb676763cd11e4
Nathan Voxland <nathan@metabase.com>
2025-03-14 09:48:52 -0500

    Resolved conflicts

--- test/metabase/cmd/load_from_h2_test.clj
+++ test/metabase/cmd/load_from_h2_test.clj
@@ -119,7 +119,7 @@
             (log/info "rolling back to version" version)
             (t2.conn/with-connection [conn]
               (liquibase/with-liquibase [liquibase conn]
-                (liquibase/rollback-major-version conn liquibase version))))
+                (liquibase/rollback-major-version conn liquibase false version))))
           (log/info "creating dump" filename)
           ;; this migrates the DB back to the newest and creates a dump
           (dump-to-h2/dump-to-h2! filename)
