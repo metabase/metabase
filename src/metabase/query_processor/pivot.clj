ea73a601b7c8d26ee2d55e8b0b176e58c853d300
Noah Moss <32746338+noahmoss@users.noreply.github.com>
2025-03-14 18:49:11 -0400

    Shared FE/BE pivots (#53000)
    * POC - moving two pivot functions into CLJC
    
    * Debug logs in frontend pivot impl
    
    * POC continued
    
    * setting isCollapsed consistently
    
    * Working replication of valuesByKey map construction
    
    * sorting logic
    
    * hook up new code to the FE impl
    
    * Fixing small potential bug in valuesByKey
    
    * Fixing bug in clojure vs javascript string key for valuesByKey
    
    * fix valueColumns key name
    
    * Some performance improvements + cleanup
    
    * A couple more attempts at perf improvements
    
    * Fix bugs
    
    * port format-values-in-tree
    
    * WIP add-subtotals
    
    * @tsp - Improved build-values-by-key perf; some debug perf lines in frontend land
    
    * @tsp - in progress conversion of add-subtotals
    
    * @tsp - working converted Pivot.add_subtotals
    
    * @tsp - fixing bug in add-subtotal mapcat recursion
    
    * @tsp - add-subtotal restoring map to mapcat (oops)
    
    * @tsp - adding working Pivot.add_value_column_nodes to replace addValueColumnNodes
    
    * @tsp - removing debug logs & perf timing logs
    
    * Appease whitespace linter
    
    * More formatting code ported, cleanup
    
    * Fix bugs, cleanup
    
    * Fix some tests
    
    * Start hooking up the BE, fix some bugs
    
    * Fix lint/formatting errors
    
    * Fix comparator + i18n
    
    * Fix another module issue
    
    * Test using js-specific i18n function
    
    * Revert "Test using js-specific i18n function"
    
    This reverts commit 9a8604b583885f1c35f48507112e3a142ca0d3a0.
    
    * Bump up timeout on cypress assertion
    
    * WIP - migrate row section getter to cljc
    
    * fix tests
    
    * Fix e2e test
    
    * Switch exports over to fully using CLJC code paths, delete now-unused code
    
    * Delete outdated postprocess tests
    
    * Bug fix
    
    * Fix build
    
    * cleanup, consolidation
    
    * fix function call
    
    * more cleanup
    
    * empty test ns
    
    * misc cleanup/docstrings
    
    * WIP tests
    
    * more tests and refactor
    
    * small fix
    
    * fix getting column title for value columns
    
    * Fix some tests
    
    * Fix another test
    
    * Revert a couple changes
    
    * Fix some more tests
    
    * Fix more tests, fix default values for pivot viz settings
    
    * Ensure headers respect format-rows? settings in exports, and fix more
    tests
    
    * Fix lint issues
    
    * fix another test
    
    * Fix a test, a little cleanup
    
    * Try to fix another test by setting format-rows middleware to false for subscription attachments
    
    * Revert "Try to fix another test by setting format-rows middleware to false for subscription attachments"
    
    This reverts commit d860522a6d17c27e67afabccf119039508c96cca.
    
    * Update formatting logic to fix another test
    
    * Fix conditional formatting
    
    * Fix last BE test
    
    * remove test not needed anymore
    
    * Fix drill through + more cleanup
    
    * clean ns
    
    * remove old code from .clj-kondo/config.edn
    
    * add a comment + tweak keywords
    
    * perf improvements and refactoring the CSV export path
    
    * Performance improvements
    
    * Fix bad rebase
    
    * more perf improvements
    
    * fix test
    
    * lint
    
    * oops, fix test
    
    * more optimizations, memoize backend formatting
    
    * fix range-based conditional formatting
    
    * remove unused function
    
    * add some new backend unit tests
    
    * fix FE type checks - caused by an experiment that I didn't revert fully
    
    * address PR review feedback
    
    * fix up tests
    
    ---------
    
    Co-authored-by: Tyler Plude <tsplude@gmail.com>

--- src/metabase/query_processor/pivot.clj
+++ src/metabase/query_processor/pivot.clj
@@ -605,7 +605,8 @@
                                 (pivot-options query (get query :viz-settings))
                                 (pivot-options query (get-in query [:info :visualization-settings]))
                                 (not-empty (select-keys query [:pivot-rows :pivot-cols :pivot-measures])))
-             query             (assoc-in query [:middleware :pivot-options] pivot-opts)
+             query             (-> query
+                                   (assoc-in [:middleware :pivot-options] pivot-opts))
              all-queries       (generate-queries query pivot-opts)
              column-mapping-fn (make-column-mapping-fn query)]
          (process-multiple-queries all-queries rff column-mapping-fn))))))
