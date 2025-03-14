04aff663c29db6c71524daddc70328cf68e686a0
Denis Berezin <denis.berezin@metabase.com>
2025-03-14 13:29:23 +0300

    use real setting value

--- frontend/src/metabase/databases/utils/settings.ts
+++ frontend/src/metabase/databases/utils/settings.ts
@@ -1,8 +1,5 @@
 import type { Database } from "metabase-types/api";
 
-export function hasDatabaseTableEditingEnabled(_database: Database) {
-  return true;
-
-  // TODO [WRK]: enable this check after it is supported on the BE
-  // return database.settings?.["database-enable-table-editing"] ?? false;
+export function hasDatabaseTableEditingEnabled(database: Database) {
+  return database.settings?.["database-enable-table-editing"] ?? false;
 }
