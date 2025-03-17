3fb87c022a6d726a1ca830f083479a2dc2167016
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-03-17 15:06:54 -0400

    Literals in filters tests

--- e2e/test/scenarios/custom-column/cc-literals.cy.spec.ts
+++ e2e/test/scenarios/custom-column/cc-literals.cy.spec.ts
@@ -6,6 +6,40 @@ describe("scenarios > custom column > literals", () => {
     cy.signInAsNormalUser();
   });
 
+  it("should support literals in custom columns", () => {
+    function removeTableFields() {
+      H.getNotebookStep("data").button("Pick columns").click();
+      H.popover().findByText("Select all").click();
+    }
+
+    function addCustomColumn({
+      expression,
+      name,
+      isFirst,
+    }: {
+      expression: string;
+      name: string;
+      isFirst?: boolean;
+    }) {
+      if (isFirst) {
+        H.getNotebookStep("data").button("Custom column").click();
+      } else {
+        H.getNotebookStep("expression").icon("add").click();
+      }
+      H.enterCustomColumnDetails({ formula: expression, name });
+      H.popover().button("Done").click();
+    }
+
+    H.openProductsTable({ mode: "notebook" });
+    removeTableFields();
+    addCustomColumn({ expression: "True", name: "True" });
+    addCustomColumn({ expression: "False", name: "False" });
+    addCustomColumn({ expression: "10", name: "Number" });
+    addCustomColumn({ expression: '"abc"', name: "Text" });
+    addCustomColumn({ expression: "[Number]", name: "Value" });
+    addCustomColumn({ expression: "[Number] + [Number]", name: "Expression" });
+  });
+
   it("should support literals in filters", () => {
     function testFilterLiteral({
       filterExpression,
