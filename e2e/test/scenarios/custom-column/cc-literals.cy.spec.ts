449fdd7274188eda216a4f877a04e43eec3a2c3d
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-03-17 15:24:24 -0400

    Literals in custom column tests

--- e2e/test/scenarios/custom-column/cc-literals.cy.spec.ts
+++ e2e/test/scenarios/custom-column/cc-literals.cy.spec.ts
@@ -6,6 +6,44 @@ describe("scenarios > custom column > literals", () => {
     cy.signInAsNormalUser();
   });
 
+  it("should support literals in custom columns", () => {
+    const columns = [
+      { name: "True", expression: "True", value: "true" },
+      // { name: "false", expression: "False", value: "false" },
+      { name: "Text", expression: '"abc"', value: "abc" },
+      { name: "Number", expression: "10", value: "10" },
+      { name: "Column", expression: "[Number]", value: "10" },
+      { name: "Expression", expression: "[Number] + [Number]", value: "20" },
+    ];
+
+    function removeTableFields() {
+      H.getNotebookStep("data").button("Pick columns").click();
+      H.popover().findByText("Select all").click();
+      H.getNotebookStep("data").button("Pick columns").click();
+    }
+
+    function addCustomColumns() {
+      columns.forEach(({ name, expression }, index) => {
+        if (index === 0) {
+          H.getNotebookStep("data").button("Custom column").click();
+        } else {
+          H.getNotebookStep("expression").icon("add").click();
+        }
+        H.enterCustomColumnDetails({ formula: expression, name });
+        H.popover().button("Done").click();
+      });
+    }
+
+    H.openProductsTable({ mode: "notebook" });
+    removeTableFields();
+    addCustomColumns();
+    H.visualize();
+    H.assertTableData({
+      columns: ["ID", ...columns.map(({ name }) => name)],
+      firstRows: [["1", ...columns.map(({ value }) => value)]],
+    });
+  });
+
   it("should support literals in filters", () => {
     function testFilterLiteral({
       filterExpression,
