48376969deaedff50ef4cf68d518d48bf35ca697
Timofei <timofei@metabase.com>
2025-03-18 00:26:31 +0400

    Extract more snippets

---
+++ docs/embedding/sdk/snippets/questions/disable-question-save.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  InteractiveQuestion,
+  MetabaseProvider,
+  defineMetabaseAuthConfig,
+} from "@metabase/embedding-sdk-react";
+
+const authConfig = defineMetabaseAuthConfig({
+  // [<ignore>]
+  metabaseInstanceUrl: "https://your-metabase.example.com",
+  authProviderUri: "https://your-app.example.com/sso/metabase",
+  // [<endignore>]
+});
+
+export default function App() {
+  return (
+    <MetabaseProvider authConfig={authConfig}>
+      <InteractiveQuestion questionId={1} isSaveEnabled={false} />
+    </MetabaseProvider>
+  );
+}
