da586d032673faeecdafef80a4a207b3b1dd5089
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-17 16:53:13 +0100

    Add tests for FunctionBrowser

---
+++ frontend/src/metabase/query_builder/components/expressions/FunctionBrowser/utils.unit.spec.ts
@@ -0,0 +1,129 @@
+import { createMockMetadata } from "__support__/metadata";
+import { createSampleDatabase } from "metabase-types/api/mocks/presets";
+
+import type { StartRule } from "../types";
+
+import { getFilteredClauses } from "./utils";
+
+function setup({
+  filter = "",
+  startRule = "expression",
+}: {
+  filter?: string;
+  startRule?: StartRule;
+} = {}) {
+  const sampleDatabase = createSampleDatabase();
+  const metadata = createMockMetadata({ databases: [sampleDatabase] });
+  const database = metadata.database(sampleDatabase.id);
+  if (!database) {
+    throw new Error("Error in test: sample database not found");
+  }
+  database.hasFeature = jest.fn().mockReturnValue(true);
+
+  return getFilteredClauses({
+    filter,
+    startRule,
+    database,
+  });
+}
+
+describe("getFilteredClauses", () => {
+  it("should return all clauses when no filter is passed", () => {
+    const results = setup();
+
+    // The array should be sorted
+    expect(results.map(group => group.category)).toEqual(
+      ["date", "logical", "math", "string"].sort(),
+    );
+
+    const dateFunctions = results[0];
+
+    // The array should be sorted
+    expect(dateFunctions.clauses.map(clause => clause.name)).toEqual(
+      [
+        "convert-timezone",
+        "datetime-add",
+        "datetime-diff",
+        "datetime-subtract",
+        "day-name",
+        "get-day",
+        "get-day-of-week",
+        "get-hour",
+        "get-minute",
+        "get-month",
+        "get-quarter",
+        "get-second",
+        "get-week",
+        "get-year",
+        "interval",
+        "month-name",
+        "now",
+        "quarter-name",
+        "relative-datetime",
+        "relative-time-interval",
+        "time-interval",
+      ].sort(),
+    );
+  });
+
+  it("should filter clauses", () => {
+    const results = setup({
+      filter: "no",
+    });
+
+    // The array should be sorted
+    expect(results.map(group => group.category)).toEqual(
+      ["date", "logical", "string"].sort(),
+    );
+
+    // The array should be sorted
+    expect(results[0].clauses.map(clause => clause.name)).toEqual(["now"]);
+    expect(results[1].clauses.map(clause => clause.name)).toEqual([
+      "not-in",
+      "not-null",
+    ]);
+    expect(results[2].clauses.map(clause => clause.name)).toEqual([
+      "does-not-contain",
+      "not-empty",
+    ]);
+  });
+
+  it("should find case", () => {
+    const results = setup({
+      filter: "case",
+    });
+
+    // The array should be sorted
+    expect(results.map(group => group.category)).toEqual(["logical"].sort());
+
+    // The array should be sorted
+    expect(results[0].clauses[0]).toEqual({
+      args: [
+        {
+          description: "Something that should evaluate to true or false.",
+          example: "[Weight] > 200",
+          name: "condition",
+        },
+        {
+          description:
+            "The value that will be returned if the preceding condition is true.",
+          example: '"Large"',
+          name: "output",
+        },
+        {
+          description: "You can add more conditions to test.",
+          example: '[Weight] > 150, "Medium", "Small"',
+          name: "…",
+        },
+      ],
+      category: "logical",
+      description:
+        "Alias for if(). Tests an expression against a list of cases and returns the corresponding value of the first matching case, with an optional default value if nothing else is met.",
+      docsPage: "case",
+      example:
+        'case([Weight] > 200, "Large", [Weight] > 150, "Medium", "Small")',
+      name: "case",
+      structure: "case",
+    });
+  });
+});
