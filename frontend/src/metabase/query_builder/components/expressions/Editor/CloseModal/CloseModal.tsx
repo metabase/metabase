9e96ef75a496f3dcac5f6cdf13773858981c427e
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-17 12:06:38 +0100

    Remove second message in closemodal

--- frontend/src/metabase/query_builder/components/expressions/Editor/CloseModal/CloseModal.tsx
+++ frontend/src/metabase/query_builder/components/expressions/Editor/CloseModal/CloseModal.tsx
@@ -15,6 +15,7 @@ export function CloseModal({
       data-ignore-editor-clicks="true"
       title={t`Keep editing your custom expression?`}
       content={t`You have changes that haven't been saved to your custom expression. You can continue editing it or discard the changes.`}
+      message=""
       onConfirm={onDiscardChanges}
       onClose={onKeepEditing}
       confirmButtonText={t`Discard changes`}
