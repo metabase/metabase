b4798c2fc48890867b13cc3b3208b12f783822ea
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-18 16:41:24 +0100

    Add is-segment-metadata helper to lib

--- src/metabase/lib/js.cljs
+++ src/metabase/lib/js.cljs
@@ -1208,6 +1208,14 @@
   [arg]
   (and (map? arg) (= :metadata/column (:lib/type arg))))
 
+(defn ^:export is-segment-metadata
+  "Returns true if arg is an MLv2 segment, ie. has `:lib/type :metadata/segment`.
+
+  > **Code health:** Smelly. When is this called and why does the FE need to know? The values are supposed to be opaque,
+  and we should see if there's a better way to get the needed information."
+  [arg]
+  (and (map? arg) (= :metadata/segment (:lib/type arg))))
+
 ;; # Field selection
 ;; Queries can specify a subset of fields to return from their source table or previous stage. There are several
 ;; functions provided to inspect and manage that list of fields.
