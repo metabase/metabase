12ecd951014394fa22e2a74866e9184dfe317cd1
Edward Paget <ed.paget@gmail.com>
2025-03-18 10:10:40 -0500

    fixup! add sts check to 404 response tests

--- test/metabase/api/common_test.clj
+++ test/metabase/api/common_test.clj
@@ -25,7 +25,7 @@
              "Content-Type"                      "text/plain"
              "Expires"                           "Tue, 03 Jul 2001 06:00:00 GMT"
              "Last-Modified"                     true ; this will be current date, so do update-in ... string?
-             "Strict-Transport-Security"         "max-age=31536000"
+             "Strict-Transport-Security"         "max-age=31536000; includeSubDomains"
              "X-Content-Type-Options"            "nosniff"
              "X-Frame-Options"                   "DENY"
              "X-Permitted-Cross-Domain-Policies" "none"
