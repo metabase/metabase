ee8a88842b7961fb961ef5ebed2e6a6c7c98278f
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-18 16:42:48 +0100

    Add Lib.isSegmentMetadata helper

--- frontend/src/metabase-lib/internal.ts
+++ frontend/src/metabase-lib/internal.ts
@@ -1,7 +1,11 @@
 import * as ML from "cljs/metabase.lib.js";
 
-import type { ColumnMetadata } from "./types";
+import type { ColumnMetadata, SegmentMetadata } from "./types";
 
 export function isColumnMetadata(arg: unknown): arg is ColumnMetadata {
   return ML.is_column_metadata(arg);
+}
+
+export function isSegmentMetadata(arg: unknown): arg is SegmentMetadata {
+  return ML.is_segment_metadata(arg);
 }
