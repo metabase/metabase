6b55c57f5e5b0f3b798928cc748c010dd201c13b
lbrdnk <lbrdnk@users.noreply.github.com>
2025-03-19 12:49:23 +0100

    WIP using context to store info

--- test/metabase/query_processor_test/generative_test.clj
+++ test/metabase/query_processor_test/generative_test.clj
@@ -20,7 +20,8 @@
 
 (comment
   (alter-var-root #_:clj-kondo/ignore #'environ.core/env assoc
-                  :mb-gentest-run "true")
+                  :mb-gentest-run "true"
+                  )
   
   #_:clj-kondo/ignore
   (select-keys environ.core/env (filter #(re-find #"mb-gentest" (name %))
@@ -79,3 +80,140 @@
               (or (is (true? (apply (every-pred :name :base_type :display_name) (mt/cols result))))
                   (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
                                                                                :result result})))))))))))
+
+(deftest query-execution-with-cards-WITH-CONTEXT-test
+  (when (config/config-bool :mb-gentest-run)
+    (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))]
+      (gentest/with-gentest
+        {:gentest.default-limit/iterations 1}
+        []
+        (tu.gen.jvm/with-random-cards mp 6
+          (let [query (lib.tu.gen/random-query mp)
+                result (qp/process-query (lib/query mp query))]
+            (testing "Successful query execution"
+              (or (is (= :completed
+                         (:status result)))
+                  (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                               :result result})))))
+            (testing "At least one column returned"
+              (or (is (<= 1 (count (mt/cols result))))
+                  (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                               :result result}))))
+              (or (is (true? (apply (every-pred :name :base_type :display_name) (mt/cols result))))
+                  (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                               :result result})))))))))))
+
+(comment
+  
+  (alter-var-root #'environ.core/env assoc
+                  :mb-gentest-seed "9131285353844560210")
+
+  (def ccc nil)
+  )
+
+(gentest/defgentest qwer-test
+  (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))]
+    (def ccon metabase.test.gentest/*context*)
+    (tu.gen.jvm/with-random-cards mp 6
+      (gentest/iterate
+       {:gentest.default-limit/iterations 1}
+       [base-query (lib/query mp @(def aa (tu.rng/rand-nth (concat (lib.metadata/tables mp) &cards))))
+        limit (inc (tu.rng/rand-int lib.tu.gen/sane-iterations-limit))]
+       (doseq [query (lib.tu.gen/random-queries-from base-query limit)]
+         (def qq query)
+         (mt/with-temp
+           [:model/Card
+            {id :id}
+            {:dataset_query (lib.convert/->legacy-MBQL query)}]
+           (let [result (qp/process-query (lib/query mp (lib.metadata/card mp id)))]
+             (testing "Successful query execution"
+               (or (is (= :completed
+                          (:status result)))
+                   (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                                :result result})))))
+             (testing "At least one column returned"
+               (or (is (<= 1 (count (mt/cols result))))
+                   (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                                :result result}))))
+               (or (is (true? (apply (every-pred :name :base_type :display_name) (mt/cols result))))
+                   (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                                :result result}))))))))))))
+
+(comment
+  
+  (clojure.walk/macroexpand-all '(tu.gen.jvm/with-random-cards mp 2
+                                   (def ccc &cards))
+                                #_(gentest/defgentest cards-and-context-test
+                                    (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))]
+                                      )))
+  
+  (tu.gen.jvm/with-random-cards (metabase.lib.metadata.jvm/application-database-metadata-provider (mt/id)) 2
+    &cards)
+  
+  
+  )
+
+(gentest/defgentest maxi-test
+  (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))]
+    (tu.gen.jvm/with-random-cards mp 6
+      (def ccc &cards))))
+
+(gentest/defgentest minisasdf-testisx-test
+  (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))]
+    (def ccon metabase.test.gentest/*context*)
+    (tu.gen.jvm/with-random-cards mp 6
+      (gentest/iterate
+       {:gentest.default-limit/iterations 1}
+       [base-query (lib/query mp @(def aa (tu.rng/rand-nth (concat (lib.metadata/tables mp) &cards))))
+        limit (inc (tu.rng/rand-int lib.tu.gen/sane-iterations-limit))]
+       #_(doseq [query (lib.tu.gen/random-queries-from base-query limit)]
+         (def qq query)
+         (mt/with-temp
+           [:model/Card
+            {id :id}
+            {:dataset_query (lib.convert/->legacy-MBQL query)}]
+           (let [result (qp/process-query (lib/query mp (lib.metadata/card mp id)))]
+             (testing "Successful query execution"
+               (or (is (= :completed
+                          (:status result)))
+                   (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                                :result result})))))
+             (testing "At least one column returned"
+               (or (is (<= 1 (count (mt/cols result))))
+                   (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                                :result result}))))
+               (or (is (true? (apply (every-pred :name :base_type :display_name) (mt/cols result))))
+                   (log/error (with-out-str #_:clj-kondo/ignore (pprint/pprint {:query query
+                                                                                :result result}))))))))))))
+
+(comment
+  (alter-var-root #'environ.core/env assoc
+                  :mb-gentest-context-seed "9131285353844560210"
+                  :mb-gentest-iteration-seed "1")
+  (alter-var-root #'environ.core/env dissoc :mb-gentest-iteration-seed)
+  )
+
+(gentest/defgentest minisasdf-testisx-test
+  (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))]
+    (tu.gen.jvm/with-random-cards mp 3
+      (gentest/spit-context! :context/cards &cards)
+      (gentest/iterate
+       {:gentest.default-limit/iterations 1}
+       [base-query (lib/query mp (tu.rng/rand-nth (concat #_(lib.metadata/tables mp)
+                                                          &cards)))
+        limit      1 #_(inc (tu.rng/rand-int lib.tu.gen/sane-iterations-limit))]
+       (doseq [query (lib.tu.gen/random-queries-from base-query limit)]
+         (gentest/spit-context! :execution/qut query)
+         (mt/with-temp
+           [:model/Card
+            {id :id}
+            {:dataset_query (lib.convert/->legacy-MBQL query)}]
+           (let [result (qp/process-query (lib/query mp (lib.metadata/card mp id)))]
+             (gentest/spit-context! :execution/result result)
+             (testing "Successful query execution"
+               (is (= 1 #_:completed
+                      (:status result))))
+             (testing "At least one column returned"
+               (is (<= 1 (count (mt/cols result))))
+               (is (true? (apply (every-pred :name :base_type :display_name)
+                                 (mt/cols result))))))))))))
