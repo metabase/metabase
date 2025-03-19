abc3b79ad51affa07bd9c86e572f2eb4a2c08a19
Phoomparin Mano <phoomparin@gmail.com>
2025-03-19 20:05:02 +0700

    cleanup version change script

--- bin/embedding-sdk/change-react-version.bash
+++ bin/embedding-sdk/change-react-version.bash
@@ -1,29 +1,43 @@
 #!/bin/bash
 
-function backup_packages() {
+function backup_package_files() {
   cp yarn.lock yarn.lock.backup
   cp package.json package.json.backup
 }
 
-function restore_packages() {
+function restore_package_files() {
   mv yarn.lock.backup yarn.lock
   mv package.json.backup package.json
 }
 
-function use_react_19() {
-  yarn add react@^19 react-dom@^19
+function use_react_version() {
+  local version=$1
+
+  if [[ ! $version =~ ^[0-9]+$ ]]; then
+    echo "Error: Version must be a number"
+    exit 1
+  fi
+
+  yarn add react@^$version react-dom@^$version
 }
 
-if [ "$1" == "19" ]; then
-  backup_packages
-  use_react_19
+function print_usage() {
+  echo "Usage: ./bin/embedding-sdk/change-react-version.bash [VERSION|restore]"
+  echo
+  echo "Examples:"
+  echo "  ./bin/embedding-sdk/change-react-version.bash 18    # Switch to React 18"
+  echo "  ./bin/embedding-sdk/change-react-version.bash 19    # Switch to React 19"
+  echo "  ./bin/embedding-sdk/change-react-version.bash restore    # Restore original package files"
+}
+
+if [[ "$1" =~ ^[0-9]+$ ]]; then
+  backup_package_files
+  use_react_version "$1"
   exit 0
 elif [ "$1" == "restore" ]; then
-  restore_packages
+  restore_package_files
   exit 0
-elif [ "$1" == "" ]; then
-  echo "Usage: ./bin/embedding-sdk/change-react-version.bash [19|restore]"
+else
+  print_usage
   exit 1
 fi
-
-
