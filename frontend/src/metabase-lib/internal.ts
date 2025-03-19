ef7db0856a402e50d9d32d1c63a73164e16f5de5
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-19 11:09:04 +0100

    Add isMetricMetadata to Lib

--- frontend/src/metabase-lib/internal.ts
+++ frontend/src/metabase-lib/internal.ts
@@ -1,7 +1,11 @@
 import * as ML from "cljs/metabase.lib.js";
 
-import type { ColumnMetadata } from "./types";
+import type { ColumnMetadata, MetricMetadata } from "./types";
 
 export function isColumnMetadata(arg: unknown): arg is ColumnMetadata {
+  return ML.is_column_metadata(arg);
+}
+
+export function isMetricMetadata(arg: unknown): arg is MetricMetadata {
   return ML.is_column_metadata(arg);
 }
