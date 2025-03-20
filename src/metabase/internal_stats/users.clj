95ee555daa9a34263de0259baae8920e55133bae
Edward Paget <ed.paget@gmail.com>
2025-03-20 13:12:29 -0500

    feat: add embedding stats to token request
    Adds the embedding_users, internal_users, domains, and query_execution_*
    stats to the token check request in order to track embedding usage.
    
    This creates a new internal_stats module to share code between the
    anonymously reported stats the stats for the token check request to
    avoid some circular depedencies. This also seperates creation of a stat
    and how it is reported, so we could move over other stats calculated for
    the phone home metric.

---
+++ src/metabase/internal_stats/users.clj
@@ -0,0 +1,19 @@
+(ns metabase.internal-stats.users
+  (:require
+   [metabase.db :as db]
+   [toucan2.core :as t2]))
+
+(defn email-domain-count
+  "Count all unique normalized domains found in active user emails"
+  []
+  (:count (t2/query-one {:select [[:%count.* :count]]
+                         :from [[{:select-distinct (condp contains? (db/db-type)
+                                                     #{:postgres}  [[[:split_part :email [:inline "@"] [:inline 2]]]]
+                                                     #{:h2 :mysql} [[[:substring :email [:locate "@" :email]]]])
+                                  :from [:core_user]
+                                  :where [:= :is_active true]} :distinct_emails]]})))
+
+(defn external-users-count
+  "Number of users with sso-source: JWT as a proxy for external users of embedded views"
+  []
+  (t2/count :model/User :is_active true :sso_source :jwt))
