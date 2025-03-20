b68090ffdce35245339a911deb43fe1d2a6b8d2c
William Zimrin <william@metabase.com>
2025-03-20 16:08:45 -0500

    error when calculating entity id while missing fields

--- src/metabase/models/serialization.clj
+++ src/metabase/models/serialization.clj
@@ -185,6 +185,14 @@
   {:arglists '([model-or-instance])}
   mi/dispatch-on-model)
 
+(defmulti hash-required-fields
+  {:arglists '([model-or-instance])}
+  mi/dispatch-on-model)
+
+(defmethod hash-required-fields :default
+  [_model]
+  [])
+
 (defn- increment-hash-values
   "Potenially adds a new value to the list of input seq based on increment.  Used to 'increment' a hash value to avoid duplicates."
   [values increment]
@@ -204,10 +212,12 @@
    (identity-hash entity 0))
   ([entity increment]
    {:pre [(some? entity)]}
-   (-> (for [f (hash-fields entity)]
-         (f entity))
-       (increment-hash-values increment)
-       raw-hash)))
+   (if (every? #(contains? entity %) (hash-required-fields entity))
+     (-> (for [f (hash-fields entity)]
+           (f entity))
+         (increment-hash-values increment)
+         raw-hash)
+     (throw (Exception. (str "Entity " entity " missing required fields " (hash-required-fields entity)))))))
 
 (defn backfill-entity-id
   "Given an entity with a (possibly empty) `:entity_id` field:
