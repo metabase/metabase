0f56608b5aba7a20058961f037c8feb8dc95d669
Edward Paget <ed.paget@gmail.com>
2025-03-21 09:12:57 -0500

    fixup! exclude api-key email

--- src/metabase/internal_stats/users.clj
+++ src/metabase/internal_stats/users.clj
@@ -6,17 +6,16 @@
 (defn email-domain-count
   "Count all unique normalized domains found in active user emails"
   []
-  #p (t2/query {:select-distinct (condp contains? (db/db-type)
-                                   #{:postgres}  [[[:split_part :email [:inline "@"] [:inline 2]]]]
-                                   #{:h2 :mysql} [[[:substring :email [:locate "@" :email]]]])
-                :from [:core_user]
-                :where [:= :is_active true]})
   (:count (t2/query-one {:select [[:%count.* :count]]
                          :from [[{:select-distinct (condp contains? (db/db-type)
                                                      #{:postgres}  [[[:split_part :email [:inline "@"] [:inline 2]]]]
                                                      #{:h2 :mysql} [[[:substring :email [:locate "@" :email]]]])
                                   :from [:core_user]
-                                  :where [:= :is_active true]} :distinct_emails]]})))
+                                  :where [:and
+                                          [:= :is_active true]
+                                          ;; Exclude @api-key because we probably don't want to count if it exists
+                                          ;; and it causes test flakes
+                                          [:not [:like :email [:inline "%@api-key.invalid"]]]]} :distinct_emails]]})))
 
 (defn external-users-count
   "Number of users with sso-source: JWT as a proxy for external users of embedded views"
