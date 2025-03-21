dd3da7b0924a63693a54a2d305cd0036f370557d
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-03-21 14:31:09 -0400

    Remove FilterModal

--- frontend/src/metabase/query_builder/components/QueryVisualization.jsx
+++ frontend/src/metabase/query_builder/components/QueryVisualization.jsx
@@ -206,5 +206,5 @@ export const VisualizationDirtyState = ({
 };
 
 function getRunQueryShortcut() {
-  return isMac() ? t`⌘ + return` : t`Ctrl + Enter`;
+  return isMac() ? t`⌘ + return` : t`Ctrl + enter`;
 }
