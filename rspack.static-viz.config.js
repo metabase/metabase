7ba7b6c7304eb17325cde275708a5a17b53eb3e9
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2025-03-21 10:34:27 -0400

    use whitelabel colors in the static viz (#55231)
    * use whitelabel colors in the static viz
    
    * Fix backend whitelabel test for app colors
    
    * update CI
    
    ---------
    
    Co-authored-by: Tyler Plude <tsplude@gmail.com>

--- rspack.static-viz.config.js
+++ rspack.static-viz.config.js
@@ -10,6 +10,8 @@ const CLJS_SRC_PATH_DEV = __dirname + "/target/cljs_dev";
 const LIB_SRC_PATH = __dirname + "/frontend/src/metabase-lib";
 const TYPES_SRC_PATH = __dirname + "/frontend/src/metabase-types";
 const SDK_SRC_PATH = __dirname + "/enterprise/frontend/src/embedding-sdk";
+const ENTERPRISE_SRC_PATH =
+  __dirname + "/enterprise/frontend/src/metabase-enterprise";
 
 const WEBPACK_BUNDLE = process.env.WEBPACK_BUNDLE || "development";
 const devMode = WEBPACK_BUNDLE !== "production";
@@ -109,11 +111,16 @@ module.exports = env => {
       alias: {
         assets: ASSETS_PATH,
         metabase: SRC_PATH,
+        "metabase-enterprise": ENTERPRISE_SRC_PATH,
         cljs: devMode ? CLJS_SRC_PATH_DEV : CLJS_SRC_PATH,
         "metabase-lib": LIB_SRC_PATH,
         "metabase-types": TYPES_SRC_PATH,
         "embedding-sdk": SDK_SRC_PATH,
         "process/browser": require.resolve("process/browser"),
+        "ee-overrides":
+          process.env.MB_EDITION === "ee"
+            ? ENTERPRISE_SRC_PATH + "/static-viz-overrides"
+            : SRC_PATH + "/lib/noop",
       },
       fallback: {
         crypto: require.resolve("crypto-browserify"),
--- test/metabase/channel/render/body_test.clj
+++ test/metabase/channel/render/body_test.clj
@@ -8,6 +8,7 @@
    [hickory.select :as hik.s]
    [metabase.channel.render.body :as body]
    [metabase.channel.render.core :as channel.render]
+   [metabase.config :as config]
    [metabase.formatter :as formatter]
    [metabase.notification.payload.execute :as notification.execute]
    [metabase.public-settings :as public-settings]
@@ -1034,3 +1035,23 @@
                               first)]
               (testing "Renders with correct day of week first"
                 (is (= "2017/1" label))))))))))
+
+(deftest render-correct-whitelabel-colors
+  (when config/ee-available?
+    (testing "The static-viz respects custom whitelabel colors"
+      (mt/with-premium-features #{:whitelabel}
+        (mt/with-temporary-setting-values [public-settings/application-colors {:accent0 "#0005FF"}]
+          (mt/dataset test-data
+            (let [q    (mt/mbql-query products
+                         {:aggregation [[:count]]
+                          :breakout    [!month.created_at]})
+                  card {:name                   "bar-test"
+                        :display                :bar
+                        :dataset_query          q
+                        :visualization_settings {:graph.dimensions ["CREATED_AT"]
+                                                 :graph.metrics ["count"]}}]
+              (mt/with-temp [:model/Card {card-id :id} card]
+                (let [doc    (render.tu/render-card-as-hickory! card-id)
+                      svg    (html doc)]
+                  (testing "Renders with custom whitelabel color"
+                    (is (str/includes? svg "#0005FF"))))))))))))
