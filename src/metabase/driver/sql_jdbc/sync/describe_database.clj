a0a91c0536d6ca987dda9feae53d82a03cd78249
lbrdnk <lbrdnk@users.noreply.github.com>
2025-03-21 14:04:32 +0100

    Arg order

--- src/metabase/driver/sql_jdbc/sync/describe_database.clj
+++ src/metabase/driver/sql_jdbc/sync/describe_database.clj
@@ -107,7 +107,7 @@
        (let [name (.getString rset "TABLE_NAME")
              schema (.getString rset "TABLE_SCHEM")
              ttype (.getString rset "TABLE_TYPE")]
-         (log/debugf "jdbc-get-tables: Fetched object: type `%s` name `%s` schema `%s`" name schema ttype)
+         (log/debugf "jdbc-get-tables: Fetched object: type `%s` name `%s` schema `%s`" ttype name schema)
          {:name        name
           :schema      schema
           :description (when-let [remarks (.getString rset "REMARKS")]
