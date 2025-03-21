9fca9c0d620eba3ee347f4dda4807ae4ee49dc2a
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-21 15:24:42 +0100

    Add test case for 1 + 1

--- frontend/src/metabase-lib/v1/expressions/formatter/formatter.unit.spec.ts
+++ frontend/src/metabase-lib/v1/expressions/formatter/formatter.unit.spec.ts
@@ -48,6 +48,9 @@ describe("format", () => {
     it("formats nested arithmetic expressions", async () => {
       await isFormatted([
         expression`
+          1 + 1
+        `,
+        expression`
           1 + 2 - 3 + 4 / 5
         `,
         expression`
