d756c569a6d5cda9c7d5dbb319bf87ffe9b0a835
Sébastien <sebastien@metabase.com>
2025-03-21 18:05:33 +0100

    closes viz-358 precision for scientific notation (#55542)

--- frontend/src/metabase/lib/formatting/numbers.unit.spec.ts
+++ frontend/src/metabase/lib/formatting/numbers.unit.spec.ts
@@ -10,31 +10,12 @@ describe("formatNumber", () => {
     });
 
     const compactResult = formatNumber(-500000, {
-      jsx: true,
-      remap: true,
-      field: "-500000",
       currency: "USD",
       number_style: "currency",
       currency_style: "symbol",
       currency_in_header: true,
       number_separators: ".,",
       _numberFormatter: numberFormatter,
-      _header_unit: "$",
-      column: {
-        display_name: "-500000",
-        source: "native",
-        field_ref: [
-          "field",
-          "-500000",
-          {
-            "base-type": "type/Integer",
-          },
-        ],
-        name: "-500000",
-        base_type: "type/Integer",
-        effective_type: "type/Integer",
-      },
-      _column_title_full: "-500000 ($)",
       compact: true,
     });
     expect(compactResult).toEqual("-$500.0k");
@@ -42,31 +23,12 @@ describe("formatNumber", () => {
     const fullResult = formatNumber(-500000, {
       compact: false,
       maximumFractionDigits: 2,
-      jsx: true,
-      remap: true,
-      field: "-500000",
       currency: "USD",
       number_style: "currency",
       currency_style: "symbol",
       currency_in_header: true,
       number_separators: ".,",
       _numberFormatter: numberFormatter,
-      _header_unit: "$",
-      column: {
-        display_name: "-500000",
-        source: "native",
-        field_ref: [
-          "field",
-          "-500000",
-          {
-            "base-type": "type/Integer",
-          },
-        ],
-        name: "-500000",
-        base_type: "type/Integer",
-        effective_type: "type/Integer",
-      },
-      _column_title_full: "-500000 ($)",
     });
     expect(fullResult).toEqual("-$500,000.00");
   });
@@ -89,5 +51,25 @@ describe("formatNumber", () => {
         number_style: "duration",
       }),
     ).toEqual("1d 3h 57m");
+  });
+
+  it("should work with scientific notation (metabase#25222)", () => {
+    expect(
+      formatNumber(0.000000000015, {
+        number_style: "scientific",
+      }),
+    ).toEqual("1.5e-11");
+
+    expect(
+      formatNumber(1.000000015, {
+        number_style: "scientific",
+      }),
+    ).toEqual("1.000000015e+0");
+
+    expect(
+      formatNumber(1.000015, {
+        number_style: "scientific",
+      }),
+    ).toEqual("1.000015e+0");
   });
 });
