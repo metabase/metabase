e169212488f36a4a5e43e8f6e6b25cead280ef9a
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-03-21 10:53:47 -0400

    reduce diff size

--- e2e/test/scenarios/permissions/sandboxing/helpers/types.ts
+++ e2e/test/scenarios/permissions/sandboxing/helpers/types.ts
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 import type { Dataset } from "metabase-types/api";
-||||||| parent of ee9077ecd5 (Test that sandboxing plays well with caching)
-import type Question from "metabase-lib/v1/Question";
-import type { Dashboard, Dataset } from "metabase-types/api";
-=======
-import type Question from "metabase-lib/v1/Question";
-import type { Dashboard, Dataset } from "metabase-types/api";
-import type Question from "metabase-lib/v1/Question";
-import type { Dashboard, DatasetData } from "metabase-types/api";
-import type { CollectionItem, Dashboard, Dataset } from "metabase-types/api";
->>>>>>> ee9077ecd5 (Test that sandboxing plays well with caching)
 
 export type DatasetResponse = {
   body: Dataset;
@@ -20,21 +9,7 @@ export type DatasetResponse = {
 
 export type DashcardQueryResponse = {
   body: Dataset;
-<<<<<<< HEAD
   url: string;
   headers: any;
   statusCode: number;
-||||||| parent of ee9077ecd5 (Test that sandboxing plays well with caching)
-};
-
-export type SandboxableItems = {
-  dashboard: Dashboard;
-  questions: Question[];
-=======
-};
-
-export type SandboxableItems = {
-  dashboard: Dashboard;
-  questions: CollectionItem[];
->>>>>>> ee9077ecd5 (Test that sandboxing plays well with caching)
 };
