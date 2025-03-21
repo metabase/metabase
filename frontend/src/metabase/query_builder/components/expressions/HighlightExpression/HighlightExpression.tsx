4b5b07be0347020d3d0ae0a2a692091cd25ae767
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-21 13:29:33 +0100

    Build expressionParts for examples manually

--- frontend/src/metabase/query_builder/components/expressions/HighlightExpression/HighlightExpression.tsx
+++ frontend/src/metabase/query_builder/components/expressions/HighlightExpression/HighlightExpression.tsx
@@ -3,7 +3,6 @@ import { useAsync } from "react-use";
 
 import type * as Lib from "metabase-lib";
 import { format, formatExample } from "metabase-lib/v1/expressions";
-import type { Expression } from "metabase-types/api";
 
 import S from "./HighlightExpression.module.css";
 import { highlight } from "./utils";
@@ -41,7 +40,7 @@ export function HighlightExampleExpression({
   printWidth = Infinity,
   ...props
 }: {
-  expression: Expression;
+  expression: Lib.ExpressionParts;
   printWidth?: number;
 } & HTMLAttributes<HTMLPreElement>) {
   const { value: formattedExpression } = useAsync(
