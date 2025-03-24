0b28628a73d40c989703bf5e65758f235022edcb
Tyler Plude <tsplude@gmail.com>
2025-03-24 14:05:15 -0700

    Add row index support

--- src/metabase/channel/render/table.clj
+++ src/metabase/channel/render/table.clj
@@ -14,6 +14,8 @@
 
 (def ^:private max-bar-width 106)
 (def ^:private font-size 12)
+(def ^:private td-x-padding-em 1)
+(def ^:private td-y-padding-em 0.75)
 
 (defn- bar-th-style []
   (merge
@@ -33,7 +35,18 @@
     :color          style/color-text-dark
     :border-bottom  (str "1px solid " style/color-border)
     :border-right   (str "1px solid " style/color-border)
-    :padding        "0.75em 1em"}))
+    :padding        (format "%sem %sem" td-y-padding-em td-x-padding-em)}))
+
+(defn- bar-td-row-index-style []
+  (merge
+   (style/font-style)
+   {:font-size      (format "%spx" font-size)
+    :font-weight    400
+    :text-align     :right
+    :color          style/color-text-light
+    :border-bottom  (str "1px solid " style/color-border)
+    :border-right   (str "1px solid " style/color-border)
+    :padding        (format "%sem %sem" td-y-padding-em td-x-padding-em)}))
 
 (defn- bar-th-style-numeric []
   (merge (style/font-style) (bar-th-style) {:text-align :right}))
@@ -84,25 +97,30 @@
     (str (str/trim (subs text 0 max-column-character-length)) "...")
     text))
 
-(defn- render-table-head [column-names {:keys [bar-width row]} columns col->styles]
+(defn- render-table-head [column-names {:keys [bar-width row]} columns col->styles row-index?]
   [:thead
-   (conj (into [:tr]
-               (map-indexed
-                (fn [idx header-cell]
-                  (let [title (get column-names idx)
-                        ;; TSP TODO - Rework index alignment assumption
-                        ;; Also should simplify / unify the styling functions
-                        col   (nth columns idx)]
-                    [:th {:style (style/style
-                                  (row-style-for-type header-cell)
-                                  (heading-style-for-type header-cell)
-                                  {:min-width :42px}
-                                  (get col->styles (:name col)))
-                          :title title}
-                     (truncate-text (h title))]))
-                row))
-         (when bar-width
-           [:th {:style (style/style (bar-td-style) (bar-th-style) {:width (str bar-width "%")})}]))])
+   [:tr
+    (when row-index?
+      [:th {:style (style/style
+                    (heading-style-for-type "#")
+                    {:font-weight 400
+                     :color       style/color-text-light})
+            :title "Row Index"}
+       (h "#")])
+    (map-indexed
+     (fn [idx header-cell]
+       (let [title (get column-names idx)
+             col   (nth columns idx)]
+         [:th {:style (style/style
+                       (row-style-for-type header-cell)
+                       (heading-style-for-type header-cell)
+                       {:min-width :42px}
+                       (get col->styles (:name col)))
+               :title title}
+          (truncate-text (h title))]))
+     row)
+    (when bar-width
+      [:th {:style (style/style (bar-td-style) (bar-th-style) {:width (str bar-width "%")})}])]])
 
 (defn- render-bar
   [bar-width normalized-zero]
@@ -128,10 +146,16 @@
   `get-background-color` is a function that returned the background color for the current cell; it is invoked like
 
     (get-background-color cell-value column-name row-index)"
-  [get-background-color normalized-zero column-names rows columns col->styles]
+  [get-background-color normalized-zero column-names rows columns col->styles row-index?]
   [:tbody
    (for [[row-idx {:keys [row bar-width]}] (m/indexed rows)]
      [:tr {:style (style/style {:color style/color-gray-3})}
+      (when row-index?
+        [:td {:style (style/style
+                      (bar-td-row-index-style)
+                      (when (= row-idx (dec (count rows)))
+                        {:border-bottom 0}))}
+         (inc row-idx)])
       (for [[col-idx cell] (m/indexed row)]
         (let [col (nth columns col-idx)]
           [:td {:style (style/style
@@ -147,12 +171,13 @@
            (h cell)]))
       (some-> bar-width (render-bar normalized-zero))])])
 
-;; TSP TODO - fix the assumption that padding is 1em
 (defn- get-min-width
-  [col-width]
-  (let [col-width (if col-width col-width 780) ;; Default to 780px
-        y-padding 1]
-    (- col-width (* font-size y-padding 2))))
+  "Get the min width for a column with text wrapping enabled. In the happy path, column widths are supplied in viz settings
+  and the correct index into column widths is found by grabbing the index of the column in the column definitions list whose
+  name matches the column-name key of the text wrap viz setting. If this breaks down we default to an arbitrary 100px"
+  [col-idx column-widths]
+  (let [col-width (if col-idx (nth column-widths col-idx) 100)]
+    (- col-width (* font-size td-x-padding-em 2))))
 
 (defn- get-text-align
   [text-align]
@@ -174,8 +199,11 @@
          (cond-> acc
            ;; text wrapping
            (::mb.viz/text-wrapping col-setting)
-           (assoc column-name {:min-width (format "%spx" (get-min-width (nth column-widths column-index)))
-                               :white-space "normal"})
+           (assoc column-name (merge {:whitespace "normal"}
+                                     (if column-widths
+                                       {:min-width (format "%spx" (get-min-width column-index column-widths))}
+                                       ;; Text wrapping enabled but no column widths supplied, default to 780px
+                                       {:max-width "780px"})))
 
            ;; text alignment
            (::mb.viz/text-align col-setting)
@@ -197,6 +225,7 @@
 
   ([color-selector normalized-zero {:keys [col-names cols-for-color-lookup]} [header & rows] columns viz-settings format-rows?]
    (let [col->styles        (column->viz-setting-styles columns viz-settings)
+         row-index?         (:table.row_index viz-settings)
          pivot-grouping-idx (get (zipmap col-names (range)) "pivot-grouping")
          col-names          (cond->> col-names
                               pivot-grouping-idx (m/remove-nth pivot-grouping-idx))
@@ -207,19 +236,25 @@
                                                          (let [group (:num-value (nth (:row row) pivot-grouping-idx))]
                                                            (when (= 0 group)
                                                              (update row :row #(m/remove-nth pivot-grouping-idx %)))))))
-         thead              (render-table-head (vec col-names) header columns col->styles)
+         thead              (render-table-head (vec col-names) header columns col->styles row-index?)
          tbody              (render-table-body
                              (partial js.color/get-background-color color-selector)
                              normalized-zero
                              cols-for-color-lookup
                              rows
                              columns
-                             col->styles)]
+                             col->styles
+                             row-index?)]
+     (def col->styles col->styles)
+     (def viz-settings viz-settings)
+     (def thead thead)
+     (def tbody tbody)
      [:table {:style       (style/style {:max-width     "100%"
                                          :white-space   "nowrap"
                                          :border        (str "1px solid " style/color-border)
                                          :width         "1%"
-                                         :border-radius :6px})
+                                         :border-radius "6px"
+                                         :margin-bottom "6px"})
               :cellpadding "0"
               :cellspacing "0"}
       thead
