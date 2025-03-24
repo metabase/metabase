d08a800821c880fb4a8d96876ba4b82f30013eeb
Sébastien <sebastien@metabase.com>
2025-03-24 10:24:01 +0100

    .

--- frontend/src/metabase/lib/formatting/numbers.unit.spec.ts
+++ frontend/src/metabase/lib/formatting/numbers.unit.spec.ts
@@ -4,31 +4,7 @@ describe("formatNumber", () => {
   it("should respect the decimals setting even when compact is true (metabase#54063)", () => {
     const result = formatNumber(4.271250189320243, {
       compact: true,
-      maximumFractionDigits: 0,
-      minimumFractionDigits: 0,
-      jsx: true,
-      remap: true,
-      field: "stddev",
-      currency: "USD",
-      number_style: "decimal",
-      currency_style: "symbol",
-      currency_in_header: true,
-      number_separators: ", ",
       decimals: 0,
-      column: {
-        database_type: "DOUBLE PRECISION",
-        semantic_type: "type/Quantity",
-        name: "stddev",
-        settings: null,
-        source: "aggregation",
-        field_ref: ["aggregation", 0],
-        effective_type: "type/Float",
-        aggregation_index: 0,
-        ident: "I7WK_71R1PnM2AiixvLvd",
-        display_name: "Standard deviation of Quantity",
-        base_type: "type/Float",
-      },
-      _column_title_full: "Standard deviation of Quantity",
     });
 
     expect(result).toEqual("4");
