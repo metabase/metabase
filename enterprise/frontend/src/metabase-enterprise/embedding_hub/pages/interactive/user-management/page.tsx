b5418381925ab03483269ebd3c00df3923c1c801
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2025-03-25 15:42:52 -0400

    add user management page content blocks

--- enterprise/frontend/src/metabase-enterprise/embedding_hub/pages/interactive/user-management/page.tsx
+++ enterprise/frontend/src/metabase-enterprise/embedding_hub/pages/interactive/user-management/page.tsx
@@ -1,7 +1,78 @@
 import { t } from "ttag";
 
-import { Title } from "metabase/ui";
+import { color } from "metabase/lib/colors";
+import {
+  Anchor,
+  Box,
+  Button,
+  Card,
+  Group,
+  Select,
+  Text,
+  Title,
+} from "metabase/ui";
 
 export function UserManagementPage() {
-  return <Title>{t`User management`}</Title>;
+  return (
+    <>
+      <Title>{t`User management`}</Title>
+      <Card p="lg" mt="xl" withBorder>
+        <Title order={2}>{t`Strategy`}</Title>
+        <Text color={color("text-medium")}>{t`
+          If users of your embeds are all part of the same org, single tenant is fine, otherwise you  can choose Multi tenant to manage resources easily for multiple organizations. Fix this copy Kyle, it sucks. Halp jeff.
+          `}</Text>
+        <Select
+          defaultValue="single-tenant"
+          data={[
+            { value: "single-tenant", label: t`Single tenant` },
+            { value: "multi-tenant", label: t`Multi tenant` },
+          ]}
+        />
+      </Card>
+      <Box mt="xl">
+        <Title order={2}>{t`Multi tenant features`}</Title>
+
+        <Box mt="lg">
+          <Title order={4}>{t`Tenants`}</Title>
+
+          <Card p="lg" mt="sm" withBorder>
+            <Group justify="space-between">
+              <Title order={4}>0</Title>
+              <Anchor>{t`View and manage`}</Anchor>
+            </Group>
+          </Card>
+        </Box>
+        <Box mt="lg">
+          <Group justify="space-between">
+            <Box>
+              <Title order={4}>{t`Tenant groups`}</Title>
+              <Text>{t`Tenant groups help you manage patterns for data access and content permissions across all your tenants.`}</Text>
+            </Box>
+            <Button>{t`Create a tenant group`}</Button>
+          </Group>
+
+          <Card p="lg" mt="sm" withBorder>
+            <Group justify="space-between">
+              <Text>Basic editors</Text>
+              <Anchor>{t`Manage permissions`}</Anchor>
+            </Group>
+            <Group justify="space-between">
+              <Text>Basic viewers</Text>
+              <Anchor>{t`Manage permissions`}</Anchor>
+            </Group>
+          </Card>
+        </Box>
+
+        <Box mt="xl">
+          <Title order={2} mb="sm">{t`Provisioning`}</Title>
+          <Card p="lg" mt="sm" withBorder>
+            <Group justify="space-between">
+              <Text>JWT - Disabled</Text>
+              <Button>{t`Set up JWT`}</Button>
+            </Group>
+          </Card>
+        </Box>
+      </Box>
+    </>
+  );
 }
