62656cdcac931258cdf271ed57012b9727e31aed
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2025-03-25 17:20:01 -0400

    highlight active nav + nav border

--- enterprise/frontend/src/metabase-enterprise/embedding_hub/routes.tsx
+++ enterprise/frontend/src/metabase-enterprise/embedding_hub/routes.tsx
@@ -22,37 +22,55 @@ function Sidebar() {
   return (
     <>
       <NavLink
+        variant="default"
         label="Overview"
         component={Link}
         to="/admin/embedding/overview"
         leftSection={<Icon name="star" />}
+        active={/^\/admin\/embedding\/overview/.test(location.pathname)}
       />
       <Text size="sm" mt="lg" mb="sm">{t`Embedding types`}</Text>
       <NavLink
+        variant="default"
         label="Static"
         component={Link}
         to="/admin/embedding/static"
         leftSection={<Icon name="snippet" />}
+        active={/^\/admin\/embedding\/static/.test(location.pathname)}
       />
-      <NavLink label="Interactive" leftSection={<Icon name="click" />}>
+      <NavLink
+        variant="default"
+        label="Interactive"
+        leftSection={<Icon name="click" />}
+      >
         <NavLink
+          variant="default"
           label="Settings"
           component={Link}
           to="/admin/embedding/interactive/settings"
+          active={/^\/admin\/embedding\/interactive\/settings/.test(
+            location.pathname,
+          )}
         />
         <NavLink
+          variant="default"
           label="User management"
           component={Link}
           to="/admin/embedding/interactive/user-management"
+          active={/^\/admin\/embedding\/interactive\/user-management/.test(
+            location.pathname,
+          )}
         />
-        <NavLink label="Frameworks" />
+        <NavLink variant="default" label="Frameworks" />
       </NavLink>
       <Text size="sm" mt="lg" mb="sm">{t`Shared settings`}</Text>
       <NavLink
+        variant="default"
         label="Appearance"
         component={Link}
         to="/admin/embedding/appearance"
         leftSection={<Icon name="palette" />}
+        active={/^\/admin\/embedding\/appearance/.test(location.pathname)}
       />
     </>
   );
@@ -65,7 +83,10 @@ function StaticEmbedding() {
 function EmbeddingLayout({ children }: { children: React.ReactNode }) {
   return (
     <AppShell navbar={{ width: 300, breakpoint: "md" }} header={{ height: 66 }}>
-      <AppShell.Navbar p="md">
+      <AppShell.Navbar
+        p="md"
+        style={{ borderRight: `1px solid ${color("border")}` }}
+      >
         <Sidebar />
       </AppShell.Navbar>
       <AppShell.Main bg={color("bg-light")}>
