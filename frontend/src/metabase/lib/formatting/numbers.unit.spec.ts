8cdf00f3c0fccac29cc0755d11a5afbd4d333883
Sébastien <sebastien@metabase.com>
2025-03-25 13:43:21 +0100

    Closes viz-456: decimals setting not being honored compact formatting (#55181)
    * closes viz-456 decimals not being honored compact formatting
    
    * fixup
    
    * .

--- frontend/src/metabase/lib/formatting/numbers.unit.spec.ts
+++ frontend/src/metabase/lib/formatting/numbers.unit.spec.ts
@@ -1,6 +1,15 @@
 import { formatNumber, numberFormatterForOptions } from "./numbers";
 
 describe("formatNumber", () => {
+  it("should respect the decimals setting even when compact is true (metabase#54063)", () => {
+    const result = formatNumber(4.271250189320243, {
+      compact: true,
+      decimals: 0,
+    });
+
+    expect(result).toEqual("4");
+  });
+
   it("should show the correct currency format (metabase#34242)", () => {
     const numberFormatter = numberFormatterForOptions({
       number_style: "currency",
