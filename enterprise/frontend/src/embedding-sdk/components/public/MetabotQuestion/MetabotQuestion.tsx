21e124f4790b4da41da9d6b87469550e65562685
Phoomparin Mano <phoomparin@gmail.com>
2025-03-25 12:48:26 +0400

    fix

--- enterprise/frontend/src/embedding-sdk/components/public/MetabotQuestion/MetabotQuestion.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/MetabotQuestion/MetabotQuestion.tsx
@@ -3,6 +3,9 @@ import { MetabotChat } from "metabase-enterprise/metabot/components/MetabotChat"
 import { MetabotProvider } from "metabase-enterprise/metabot/context";
 
 import "metabase-enterprise/metabot";
+import { useMemo, useState } from "react";
+
+import { InteractiveAdHocQuestion } from "embedding-sdk/components/private/InteractiveAdHocQuestion";
 
 interface Props {
   visible: boolean;
@@ -10,9 +13,26 @@ interface Props {
 }
 
 export const MetabotQuestion = ({ visible, onClose }: Props) => {
+  const [result, setResult] = useState<Record<string, any> | null>(null);
+
+  const redirectUrl = useMemo(() => {
+    return result?.payload?.payload?.data?.reactions?.find(
+      (reaction: { type: string; url: string }) =>
+        reaction.type === "metabot.reaction/redirect",
+    )?.url;
+  }, [result]);
+
   return (
     <MetabotProvider>
-      {visible && <MetabotChat onClose={onClose} />}
+      {redirectUrl && (
+        <InteractiveAdHocQuestion
+          questionPath={redirectUrl}
+          title={false}
+          onNavigateBack={() => {}}
+        />
+      )}
+
+      {visible && <MetabotChat onClose={onClose} onResult={setResult} />}
     </MetabotProvider>
   );
 };
