fd7f3a2d6bd4aba0a06360a9442db9acb651d339
Eric Normand <ericwnormand@gmail.com>
2025-03-25 11:46:04 -0500

    Remove unsupported postgres tests

--- test/metabase/driver/postgres_test.clj
+++ test/metabase/driver/postgres_test.clj
@@ -2002,11 +2002,8 @@
    (check-integer-query query db-type uncasted-field "\"subquery\".\"INTCAST\""))
   ([query db-type uncasted-field casted-field]
    (mt/native-query {:query (str "SELECT " casted-field ", "
-                                 (case db-type
-                                   ;; need to do regex because some strings have 0 in front
-                                   "TEXT"    (str (name uncasted-field) " ~ '^0*' || " "CAST(" casted-field " AS " db-type ") || '$'")
-                                   "INTEGER" (str "CAST(" casted-field " AS " db-type ") = " (name uncasted-field))
-                                   "FLOAT"   (str "ABS(CAST(" casted-field " AS " db-type ") - " (name uncasted-field) ") < 1"))
+                                 ;; need to do regex because some strings have 0 in front
+                                 (str (name uncasted-field) " ~ '^0*' || " "CAST(" casted-field " AS " db-type ") || '$'")
                                  ", "
                                  (name uncasted-field) " "
                                  "FROM ( "
@@ -2018,11 +2015,7 @@
   (mt/test-driver :postgres
     (mt/dataset test-data
       (let [mp (mt/metadata-provider)]
-        (doseq [[table fields] [[:people [{:field :longitude :db-type "FLOAT"}
-                                          {:field :id :db-type "INTEGER"}
-                                          {:field :zip :db-type "TEXT"}]]
-                                [:orders [{:field :user_id :db-type "INTEGER"}
-                                          {:field :subtotal :db-type "FLOAT"}]]]
+        (doseq [[table fields] [[:people [{:field :zip :db-type "TEXT"}]]]
                 {:keys [field db-type]} fields]
           (testing (str "casting " table "." field "(" db-type ") to integer")
             (let [field-md (lib.metadata/field mp (mt/id table field))
@@ -2045,13 +2038,7 @@
         (doseq [[table expressions] [[:people [{:expression (lib/expression-clause :concat
                                                                                    [(lib.metadata/field mp (mt/id :people :id))
                                                                                     (lib.metadata/field mp (mt/id :people :zip))] nil)
-                                                :db-type "TEXT"}
-                                               {:expression (lib/expression-clause :get-day-of-week
-                                                                                   [(lib.metadata/field mp (mt/id :people :birth_date))] nil)
-                                                :db-type "INTEGER"}]]
-                                     [:orders [{:expression (lib/+ (lib.metadata/field mp (mt/id :orders :total))
-                                                                   (lib.metadata/field mp (mt/id :orders :quantity)))
-                                                :db-type "FLOAT"}]]]
+                                                :db-type "TEXT"}]]]
                 {:keys [expression db-type]} expressions]
           (testing (str "Casting " db-type " to integer")
             (let [query (-> (lib/query mp (lib.metadata/table mp (mt/id table)))
@@ -2072,10 +2059,8 @@
   (mt/test-driver :postgres
     (mt/dataset test-data
       (let [mp (mt/metadata-provider)]
-        (doseq [[_table expressions] [[:people [{:expression 1 :db-type "INTEGER"}
-                                                {:expression "'123'" :db-type "TEXT"}
-                                                {:expression "'-123'" :db-type "TEXT"}
-                                                {:expression 4.5 :db-type "FLOAT"}]]]
+        (doseq [[_table expressions] [[:people [{:expression "'123'" :db-type "TEXT"}
+                                                {:expression "'-123'" :db-type "TEXT"}]]]
                 {:keys [expression db-type]} expressions]
           (testing (str "Casting " db-type " to integer from native query")
             (let [native-query (mt/native-query {:query (str "SELECT " expression " AS UNCASTED")})]
@@ -2100,11 +2085,7 @@
   (mt/test-driver :postgres
     (mt/dataset test-data
       (let [mp (mt/metadata-provider)]
-        (doseq [[table fields] [[:people [{:field :longitude :db-type "FLOAT"}
-                                          {:field :id :db-type "INTEGER"}
-                                          {:field :zip :db-type "TEXT"}]]
-                                [:orders [{:field :user_id :db-type "INTEGER"}
-                                          {:field :subtotal :db-type "FLOAT"}]]]
+        (doseq [[table fields] [[:people [{:field :zip :db-type "TEXT"}]]]
                 {:keys [field db-type]} fields]
           (let [nested-query (lib/query mp (lib.metadata/table mp (mt/id table)))]
             (testing (str "Casting " db-type " to integer")
@@ -2134,13 +2115,7 @@
         (doseq [[table expressions] [[:people [{:expression (lib/expression-clause :concat
                                                                                    [(lib.metadata/field mp (mt/id :people :id))
                                                                                     (lib.metadata/field mp (mt/id :people :zip))] nil)
-                                                :db-type "TEXT"}
-                                               {:expression (lib/expression-clause :get-day-of-week
-                                                                                   [(lib.metadata/field mp (mt/id :people :birth_date))] nil)
-                                                :db-type "INTEGER"}]]
-                                     [:orders [{:expression (lib/+ (lib.metadata/field mp (mt/id :orders :total))
-                                                                   (lib.metadata/field mp (mt/id :orders :quantity)))
-                                                :db-type "FLOAT"}]]]
+                                                :db-type "TEXT"}]]]
                 {:keys [expression db-type]} expressions]
           (let [nested-query (-> (lib/query mp (lib.metadata/table mp (mt/id table)))
                                  (lib/with-fields [])
@@ -2175,17 +2150,7 @@
                                                              (lib/expression-clause :concat
                                                                                     [(lib.metadata/field mp (mt/id :people :id))
                                                                                      (lib.metadata/field mp (mt/id :people :zip))] nil)]
-                                                :db-type "TEXT"}
-                                               {:expression [(lib/expression-clause :get-day-of-week
-                                                                                    [(lib.metadata/field mp (mt/id :people :birth_date))] nil)
-                                                             (lib/expression-clause :get-day-of-week
-                                                                                    [(lib.metadata/field mp (mt/id :people :birth_date))] nil)]
-                                                :db-type "INTEGER"}]]
-                                     [:orders [{:expression [(lib/+ (lib.metadata/field mp (mt/id :orders :total))
-                                                                    (lib.metadata/field mp (mt/id :orders :quantity)))
-                                                             (lib/+ (lib.metadata/field mp (mt/id :orders :total))
-                                                                    (lib.metadata/field mp (mt/id :orders :quantity)))]
-                                                :db-type "FLOAT"}]]]
+                                                :db-type "TEXT"}]]]
                 {db-type :db-type [e1 e2] :expression} expressions]
           (let [query (-> (lib/query mp (lib.metadata/table mp (mt/id table)))
                           (lib/expression "UNCASTED" e1)
@@ -2203,11 +2168,7 @@
   (mt/test-driver :postgres
     (mt/dataset test-data
       (let [mp (mt/metadata-provider)]
-        (doseq [[table fields] [[:people [{:field :longitude :db-type "FLOAT"}
-                                          {:field :id :db-type "INTEGER"}
-                                          {:field :zip :db-type "TEXT"}]]
-                                [:orders [{:field :user_id :db-type "INTEGER"}
-                                          {:field :subtotal :db-type "FLOAT"}]]]
+        (doseq [[table fields] [[:people [{:field :zip :db-type "TEXT"}]]]
                 {:keys [field db-type]} fields]
           (testing (str "aggregating " table "." field "(" db-type ") and casting to integer")
             (let [field-md (lib.metadata/field mp (mt/id table field))
