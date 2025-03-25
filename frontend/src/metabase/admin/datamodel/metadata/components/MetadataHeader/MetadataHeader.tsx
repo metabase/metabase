18b702e13839d47e1cd147ab23053c85a420e7ab
Kamil Mielnik <kamil@kamilmielnik.com>
2025-03-25 21:28:13 +0700

    Add new 'Table settings' button - remove unused CSS classes

--- frontend/src/metabase/admin/datamodel/metadata/components/MetadataHeader/MetadataHeader.tsx
+++ frontend/src/metabase/admin/datamodel/metadata/components/MetadataHeader/MetadataHeader.tsx
@@ -1,5 +1,6 @@
 import cx from "classnames";
 import { useLayoutEffect } from "react";
+import { Link } from "react-router";
 import { push, replace } from "react-router-redux";
 import { t } from "ttag";
 import _ from "underscore";
@@ -10,12 +11,10 @@ import { connect } from "metabase/lib/redux";
 import * as Urls from "metabase/lib/urls";
 import { PLUGIN_FEATURE_LEVEL_PERMISSIONS } from "metabase/plugins";
 import { DatabaseDataSelector } from "metabase/query_builder/components/DataSelector";
-import { Icon } from "metabase/ui";
+import { Button, Flex, Icon } from "metabase/ui";
 import type Database from "metabase-lib/v1/metadata/Database";
 import type { DatabaseId, SchemaId, TableId } from "metabase-types/api";
 import type { Dispatch } from "metabase-types/store";
-
-import { TableSettingsLink } from "./MetadataHeader.styled";
 
 interface OwnProps {
   selectedDatabaseId?: DatabaseId;
@@ -62,19 +61,13 @@ const MetadataHeader = ({
   return (
     <div
       data-testid="admin-metadata-header"
-      className={cx(
-        "MetadataEditor-header",
-        CS.flex,
-        CS.alignCenter,
-        CS.flexNoShrink,
-        CS.py4,
-      )}
+      className={cx(CS.flex, CS.alignCenter, CS.flexNoShrink, CS.py4)}
     >
       <Icon
         className={cx(CS.flex, CS.alignCenter, CS.flexNoShrink, CS.textMedium)}
         name="database"
       />
-      <div className={cx("MetadataEditor-headerSection", CS.h2)}>
+      <div className={CS.h2}>
         <DatabaseDataSelector
           databases={databases}
           selectedDatabaseId={selectedDatabaseId}
@@ -82,29 +75,19 @@ const MetadataHeader = ({
           style={{ padding: 0, paddingLeft: 8 }}
         />
       </div>
+
       {selectedDatabaseId && selectedSchemaId && selectedTableId && (
-        <div
-          className={cx(
-            "MetadataEditor-headerSection",
-            CS.flex,
-            CS.flexAlignRight,
-            CS.alignCenter,
-            CS.flexNoShrink,
-          )}
-        >
-          <span className={cx(CS.ml4, CS.mr3)}>
-            <TableSettingsLink
-              aria-label={t`Settings`}
-              to={Urls.dataModelTableSettings(
-                selectedDatabaseId,
-                selectedSchemaId,
-                selectedTableId,
-              )}
-            >
-              <Icon name="gear" />
-            </TableSettingsLink>
-          </span>
-        </div>
+        <Flex align="center" justify="flex-end" flex="1 0 auto" mx="xl">
+          <Button
+            component={Link}
+            leftSection={<Icon name="gear" />}
+            to={Urls.dataModelTableSettings(
+              selectedDatabaseId,
+              selectedSchemaId,
+              selectedTableId,
+            )}
+          >{t`Table settings`}</Button>
+        </Flex>
       )}
     </div>
   );
