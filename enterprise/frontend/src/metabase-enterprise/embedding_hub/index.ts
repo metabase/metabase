30706bb8dbada15cba56dd94cfeb2bfd204f266b
Kyle Doherty <5248953+kdoh@users.noreply.github.com>
2025-03-25 15:50:49 -0400

    add nav item

--- enterprise/frontend/src/metabase-enterprise/embedding_hub/index.ts
+++ enterprise/frontend/src/metabase-enterprise/embedding_hub/index.ts
@@ -1,6 +1,13 @@
-import { PLUGIN_ADMIN_ROUTES } from "metabase/plugins";
+import { t } from "ttag";
+
+import { PLUGIN_ADMIN_NAV_ITEMS, PLUGIN_ADMIN_ROUTES } from "metabase/plugins";
 
 import { getRoutes } from "./routes";
 
 // Don't know why this gives a ts error
 PLUGIN_ADMIN_ROUTES.push(getRoutes);
+PLUGIN_ADMIN_NAV_ITEMS.push({
+  name: t`Embedding`,
+  path: "admin/embedding",
+  key: "embedding",
+});
