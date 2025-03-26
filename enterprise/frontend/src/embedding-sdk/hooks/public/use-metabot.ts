69f4a32f2601c50628cbde6fb5c637460e442386
Phoomparin Mano <phoomparin@gmail.com>
2025-03-26 16:37:22 +0400

    add image summarization

--- enterprise/frontend/src/embedding-sdk/hooks/public/use-metabot.ts
+++ enterprise/frontend/src/embedding-sdk/hooks/public/use-metabot.ts
@@ -14,9 +14,11 @@ export interface MetabotMessage {
 
 export interface UseMetabotHookResult {
   status: MetabotStatus;
-  sendMessage(message: string): void;
   messages: MetabotMessage[];
   latestQuestionPath: string | null;
+
+  sendMessage(message: string): void;
+  summarizeChartFromElement(element: HTMLElement): void;
 }
 
 export function useMetabot(): UseMetabotHookResult {
@@ -68,10 +70,72 @@ export function useMetabot(): UseMetabotHookResult {
     });
   }
 
+  async function summarizeChartFromElement(element: HTMLElement) {
+    const blob = await captureImageFromElementAsBlob(element);
+
+    if (!blob) {
+      return;
+    }
+
+    const model = "openai/gpt-4o-2024-11-20";
+    const body = new FormData();
+    body.append("image", blob, "chart.png");
+
+    // TODO: proxy via the backend instead of calling ai-service directly
+    const response = await fetch(
+      `http://localhost:8000/experimental/summarize-chart?model=${model}`,
+      {
+        body,
+        method: "POST",
+        headers: {
+          // !!!!!!!!!!!!!!!!!! do not commit the token - this is for demo only
+          "x-metabase-instance-token": "<YOUR_MB_INSTANCE_TOKEN_UNSAFE>",
+        },
+      },
+    );
+
+    const output = await response.json();
+
+    setMessages(prevMessages => [
+      ...prevMessages,
+      {
+        id: `bot-${Date.now()}`,
+        content: output?.summary,
+        sender: "bot",
+        timestamp: Date.now(),
+      },
+    ]);
+
+    return output?.summary;
+  }
+
   return {
     status,
-    sendMessage,
     messages,
     latestQuestionPath,
+
+    sendMessage,
+    summarizeChartFromElement,
   };
+}
+
+export async function captureImageFromElementAsBlob(
+  element: HTMLElement,
+): Promise<Blob | null> {
+  const { default: html2canvas } = await import("html2canvas-pro");
+
+  return new Promise(resolve => {
+    if (!element || !(element instanceof HTMLElement)) {
+      return resolve(null);
+    }
+
+    html2canvas(element, {
+      scale: 2,
+      useCORS: true,
+      onclone: (doc: Document, node: HTMLElement) => {
+        node.style.borderRadius = "0px";
+        node.style.border = "none";
+      },
+    }).then(canvas => canvas.toBlob(blob => resolve(blob)));
+  });
 }
