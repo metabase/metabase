af2327fc89ce51142f1dc183c5b91619ee8c1a1b
Riley Thompson <riley@metabase.com>
2025-03-26 10:39:19 -0400

    Remove JS from mongo queries (#55499)

--- modules/drivers/mongo/src/metabase/driver/mongo/query_processor.clj
+++ modules/drivers/mongo/src/metabase/driver/mongo/query_processor.clj
@@ -417,7 +417,7 @@
     (isa? base-type :type/MongoBSONID)
     (ObjectId. (str value))
 
-    (isa? base-type :type/UUID)
+    (= base-type :type/*)
     (try
       (-> (str value)
           java.util.UUID/fromString
@@ -741,16 +741,10 @@
 (defn- str-match-pattern [field options prefix value suffix]
   (if (mbql.u/is-clause? ::not value)
     {$not (str-match-pattern field options prefix (second value) suffix)}
-    (let [base-type (get-in field [2 :base-type])]
+    (do
       (assert (and (contains? #{nil "^"} prefix) (contains? #{nil "$"} suffix))
               "Wrong prefix or suffix value.")
-      {$regexMatch {"input" (if (= :type/UUID base-type)
-                              ;; TODO: Update this to use $toString once all supported
-                              ;; Mongo versions have $toString available (>= 8.0)
-                              {"$function" {"body" "function(uuid) { return uuid.toString().substring(6, 42) }",
-                                            "args" [(->rvalue field)],
-                                            "lang" "js"}}
-                              (->rvalue field))
+      {$regexMatch {"input" (->rvalue field)
                     "regex" (if (= (first value) :value)
                               (str prefix (->rvalue value) suffix)
                               {$concat (into [] (remove nil?) [(when (some? prefix) {$literal prefix})
