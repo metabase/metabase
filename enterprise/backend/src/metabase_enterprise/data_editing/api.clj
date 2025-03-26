c21e9db004059b86165674db9f63ade30135750f
Ngoc Khuat <qn.khuat@gmail.com>
2025-03-26 12:41:22 +0700

    [Notification] tables notification working e2e (#55444)

--- enterprise/backend/src/metabase_enterprise/data_editing/api.clj
+++ enterprise/backend/src/metabase_enterprise/data_editing/api.clj
@@ -1,11 +1,18 @@
 (ns metabase-enterprise.data-editing.api
   (:require
+   [clojure.data :refer [diff]]
+   [medley.core :as m]
    [metabase.actions.core :as actions]
    [metabase.api.common :as api]
    [metabase.api.macros :as api.macros]
    [metabase.api.routes.common :refer [+auth]]
    [metabase.driver :as driver]
+   [metabase.events :as events]
+   [metabase.events.notification :as events.notification]
+   [metabase.lib.core :as lib]
+   [metabase.lib.metadata :as lib.metadata]
    [metabase.query-processor :as qp]
+   [metabase.query-processor.store :as qp.store]
    [metabase.upload :as-alias upload]
    [metabase.util.i18n :as i18n]
    [metabase.util.malli.schema :as ms]
@@ -17,47 +24,102 @@
                            {:database (api/check-404 (t2/select-one-fn :db_id [:model/Table :db_id] table-id))
                             :table-id table-id
                             :arg      rows}
-                           :policy :data-editing))
+                           :policy   :data-editing))
+
+(doseq [topic [:event/data-editing-row-create
+               :event/data-editing-row-update
+               :event/data-editing-row-delete]]
+  (defmethod events.notification/notification-filter-for-topic topic
+    [_topic event-info]
+    (assert (:table_id event-info) "Event info must contain :table_id")
+    [:= :table_id (:table_id event-info)]))
+
+(defn- qp-result->row-map
+  [{:keys [rows cols]}]
+  ;; rows from the request are keywordized
+  (let [col-names (map (comp keyword :name) cols)]
+    (map #(zipmap col-names %) rows)))
+
+(defn- table-id->pk
+  [table-id]
+  ;; TODO: support composite PKs
+  (let [pks (api/check-404 (t2/select :model/Field :table_id table-id :semantic_type :type/PK))]
+    (api/check-500 (= 1 (count pks)))
+    (first pks)))
+
+(defn- get-row-pk
+  [pk-field row]
+  (get row (keyword (:name pk-field))))
+
+(defn- query-db-rows
+  [table-id pk-field rows]
+  (let [{:keys [db_id]} (api/check-404 (t2/select-one :model/Table table-id))]
+    (assert pk-field "Table must have a primary key")
+    (assert (every? (partial get-row-pk pk-field) rows) "All rows must have the primary key")
+    (when-let [pk-values (seq (map (partial get-row-pk pk-field) rows))]
+      (qp.store/with-metadata-provider db_id
+        (let [mp    (qp.store/metadata-provider)
+              query (-> (lib/query mp (lib.metadata/table mp table-id))
+                        (lib/filter (apply lib/in (lib.metadata/field mp (:id pk-field)) pk-values))
+                        qp/userland-query-with-default-constraints)]
+          (->> (qp/process-query query)
+               :data
+               qp-result->row-map
+               (m/index-by #(get-row-pk pk-field %))))))))
 
 (api.macros/defendpoint :post "/table/:table-id"
   "Insert row(s) into the given table."
   [{:keys [table-id]} :- [:map [:table-id ms/PositiveInt]]
    {}
    {:keys [rows]} :- [:map [:rows [:sequential {:min 1} :map]]]]
-  (perform-bulk-action! :bulk/create table-id rows))
+  (let [res (perform-bulk-action! :bulk/create table-id rows)]
+    (doseq [row (:created-rows res)]
+      (events/publish-event! :event/data-editing-row-create
+                             {:table_id    table-id
+                              :created_row row
+                              :actor_id    api/*current-user-id*}))
+    res))
 
 (api.macros/defendpoint :put "/table/:table-id"
   "Update row(s) within the given table."
   [{:keys [table-id]} :- [:map [:table-id ms/PositiveInt]]
    {}
    {:keys [rows]} :- [:map [:rows [:sequential {:min 1} :map]]]]
-  (perform-bulk-action! :bulk/update table-id rows)
   (if (empty? rows)
     {:updated []}
-    (let [db-id          (t2/select-one-fn :db_id [:model/Table :db_id] table-id)
-          pk-fields      (t2/select [:model/Field :id :name] :table_id table-id :semantic_type :type/PK)
-          key-match-expr (fn [row]
-                           (->> (for [{field-name :name, id :id} pk-fields]
-                                  [:= [:field id] (get row (keyword field-name))])
-                                (into [:and])))
-          ;; note there is no bound set on the number of clauses
-          ;; we are dependent on
-          ;; a. the optimiser doing something sensible re index scans
-          ;; b. there being no limits on the number of OR/AND clauses
-          ;; revisit this policy later
-          q-filter       (into [:or] (map key-match-expr) rows)
-          q              {:source-table table-id, :filter q-filter}
-          {:keys [data]} (qp/process-query {:database db-id, :type :query, :query q})
-          col-names      (mapv :name (:cols data))
-          updated        (mapv #(zipmap col-names %) (:rows data))]
-      {:updated updated})))
+    (let [pk-field   (table-id->pk table-id)
+          id->db-row (query-db-rows table-id pk-field rows)
+          updated-rows (volatile! [])]
+      (doseq [row rows]
+        (let [;; well, this is a trick, but I haven't figured out how to do single row update
+              result        (:rows-updated (perform-bulk-action! :bulk/update table-id [row]))
+              after-row     (-> (query-db-rows table-id pk-field [row]) vals first)
+              row-before    (get id->db-row (get-row-pk pk-field row))
+              [_ changes _] (diff row-before row)]
+          (vswap! updated-rows conj after-row)
+          (when (pos-int? result)
+            (events/publish-event! :event/data-editing-row-update
+                                   {:table_id table-id
+                                    :after    after-row
+                                    :before   row-before
+                                    :update   changes
+                                    :actor_id api/*current-user-id*}))))
+      {:updated @updated-rows})))
 
 (api.macros/defendpoint :post "/table/:table-id/delete"
   "Delete row(s) from the given table"
   [{:keys [table-id]} :- [:map [:table-id ms/PositiveInt]]
    {}
    {:keys [rows]} :- [:map [:rows [:sequential {:min 1} :map]]]]
-  (perform-bulk-action! :bulk/delete table-id rows))
+  (let [pk-field    (table-id->pk table-id)
+        id->db-rows (query-db-rows table-id pk-field rows)
+        res         (perform-bulk-action! :bulk/delete table-id rows)]
+    (doseq [row rows]
+      (events/publish-event! :event/data-editing-row-delete
+                             {:table_id    table-id
+                              :deleted_row (get id->db-rows (get-row-pk pk-field row))
+                              :actor_id    api/*current-user-id*}))
+    res))
 
 ;; might later be changed, or made driver specific, we might later drop the requirement depending on admin trust
 ;; model (e.g are admins trusted with writing arbitrary SQL cases anyway, will non admins ever call this?)
---
+++ enterprise/backend/test/metabase_enterprise/data_editing/notification_test.clj
@@ -0,0 +1,169 @@
+(ns metabase-enterprise.data-editing.notification-test
+  (:require
+   [clojure.test :refer :all]
+   [metabase-enterprise.data-editing.test-util :as data-editing.tu]
+   [metabase.driver :as driver]
+   [metabase.notification.test-util :as notification.tu]
+   [metabase.sync.core :as sync]
+   [metabase.test :as mt]
+   [metabase.test.fixtures :as fixtures]
+   [metabase.util :as u]
+   [toucan2.core :as t2]))
+
+(use-fixtures :each (fn [thunk]
+                      (mt/with-premium-features #{:table-data-editing}
+                        (thunk))))
+
+(defn all-handlers
+  [http-channel-id]
+  [{:channel_type :channel/email
+    :recipients [{:type :notification-recipient/user
+                  :user_id (mt/user->id :crowberto)}]}
+   {:channel_type :channel/slack
+    :recipients [{:type :notification-recipient/raw-value
+                  :details {:value "#test-pulse"}}]}
+   {:channel_type :channel/http
+    :channel_id   http-channel-id}])
+
+(defn test-row-notification!
+  [event-name request-fn channel-type->assert-fns]
+  (data-editing.tu/with-temp-test-db!
+    (mt/with-temp [:model/Channel chn {:type :channel/http}]
+      (notification.tu/with-system-event-notification!
+        [_notification {:subscriptions [{:event_name event-name
+                                         :table_id   (mt/id :categories)}]
+                        :handlers      (all-handlers (:id chn))}]
+        (notification.tu/with-channel-fixtures [:channel/email :channel/slack]
+          (let [channel-type->captured-message (notification.tu/with-captured-channel-send!
+                                                 (request-fn))]
+            (doseq [[channel-type assert-fn] channel-type->assert-fns]
+              (testing (format "channel-type = %s" channel-type)
+                (assert-fn (get channel-type->captured-message channel-type))))))))))
+
+(deftest create-row-notification-test
+  (test-row-notification!
+   :event/data-editing-row-create
+   (fn []
+     (mt/user-http-request
+      :crowberto
+      :post
+      (data-editing.tu/table-url (mt/id :categories))
+      {:rows [{:NAME "New Category"}]}))
+
+   {:channel/slack (fn [[message :as msgs]]
+                     (is (= 1 (count msgs)))
+                     (is (=? {:attachments [{:blocks
+                                             [{:type "section",
+                                               :text
+                                               {:type "mrkdwn",
+                                                :text "*Crowberto Corv has created a row for CATEGORIES*\n• ID : 76\n• NAME : New Category"}}]}]
+                              :channel-id "#test-pulse"}
+                             message)))
+    :channel/email (fn [[email :as emails]]
+                     (is (= 1 (count emails)))
+                     (is (=? {:subject "Table CATEGORIES has a new row"
+                              :message [{"Crowberto Corv has created a row for CATEGORIES" true
+                                         "NAME: New Category" true}]}
+                             (mt/summarize-multipart-single-email
+                              email
+                              #"Crowberto Corv has created a row for CATEGORIES"
+                              #"NAME: New Category"))))
+    :channel/http    (fn [[req :as reqs]]
+                       (is (= 1 (count reqs)))
+                       (is (=? {:body {:event_info {:actor       {:common_name "Crowberto Corv"
+                                                                  :email "crowberto@metabase.com"
+                                                                  :first_name "Crowberto"
+                                                                  :last_name "Corv"}
+                                                    :actor_id    (mt/user->id :crowberto),
+                                                    :created_row {"ID" (mt/malli=? :int) "NAME" "New Category"}
+                                                    :table       {:name "CATEGORIES"}
+                                                    :table_id    (mt/id :categories)}
+                                       :event_name :event/data-editing-row-create,
+                                       :type "system_event"}}
+                               req)))}))
+
+(deftest update-row-notification-test
+  (test-row-notification!
+   :event/data-editing-row-update
+   (fn []
+     (mt/user-http-request
+      :crowberto
+      :put
+      (data-editing.tu/table-url (mt/id :categories))
+      {:rows [{:ID 1 :NAME "Updated Category"}]}))
+
+   {:channel/slack (fn [[message :as msgs]]
+                     (is (= 1 (count msgs)))
+                     (is (=? {:attachments [{:blocks
+                                             [{:type "section",
+                                               :text
+                                               {:type "mrkdwn",
+                                                :text "*Crowberto Corv has updated a from CATEGORIES*\n*Update:*\n• NAME : Updated Category"}}]}]
+                              :channel-id "#test-pulse"}
+                             message)))
+    :channel/email (fn [[email :as emails]]
+                     (is (= 1 (count emails)))
+                     (is (=? {:subject "Table CATEGORIES has been updated"
+                              :message [{"Crowberto Corv has updated a row in CATEGORIES" true
+                                         "NAME: Updated Category" true}]}
+                             (mt/summarize-multipart-single-email
+                              email
+                              #"Crowberto Corv has updated a row in CATEGORIES"
+                              #"NAME: Updated Category"))))
+    :channel/http  (fn [[req :as reqs]]
+                     (is (= 1 (count reqs)))
+                     (is (=? {:body {:event_info {:actor    {:common_name "Crowberto Corv"
+                                                             :email "crowberto@metabase.com"
+                                                             :first_name "Crowberto"
+                                                             :last_name "Corv"}
+                                                  :actor_id (mt/user->id :crowberto),
+                                                  :after    {:ID 1 :NAME "Updated Category"}
+                                                  :before   {:ID 1 :NAME (mt/malli=? :string)}
+                                                  :update   {:NAME "Updated Category"}
+                                                  :table    {:name "CATEGORIES"}
+                                                  :table_id (mt/id :categories)}
+                                     :event_name :event/data-editing-row-update,
+                                     :type "system_event"}}
+                             req)))}))
+
+(deftest delete-row-notification-test
+  (test-row-notification!
+   :event/data-editing-row-delete
+   (fn []
+     (mt/user-http-request
+      :crowberto
+      :post
+      (format "%s/delete" (data-editing.tu/table-url (mt/id :categories)))
+      {:rows [{:ID 1}]}))
+
+   {:channel/slack (fn [[message :as msgs]]
+                     (is (= 1 (count msgs)))
+                     (is (=? {:attachments [{:blocks
+                                             [{:type "section",
+                                               :text
+                                               {:type "mrkdwn",
+                                                :text "*Crowberto Corv has deleted a from CATEGORIES*\n• ID : 1\n• NAME : African"}}]}]
+                              :channel-id "#test-pulse"}
+                             message)))
+    :channel/email (fn [[email :as emails]]
+                     (is (= 1 (count emails)))
+                     (is (=? {:subject "Table CATEGORIES has a row deleted"
+                              :message [{"Crowberto Corv has deleted a row from CATEGORIES" true
+                                         "NAME: African" true}]}
+                             (mt/summarize-multipart-single-email
+                              email
+                              #"Crowberto Corv has deleted a row from CATEGORIES"
+                              #"NAME: African"))))
+    :channel/http  (fn [[req :as reqs]]
+                     (is (= 1 (count reqs)))
+                     (is (=? {:body {:event_info {:actor       {:common_name "Crowberto Corv"
+                                                                :email "crowberto@metabase.com"
+                                                                :first_name "Crowberto"
+                                                                :last_name "Corv"}
+                                                  :actor_id    (mt/user->id :crowberto),
+                                                  :deleted_row {:ID 1 :NAME "African"}
+                                                  :table       {:name "CATEGORIES"}
+                                                  :table_id    (mt/id :categories)}
+                                     :event_name :event/data-editing-row-delete,
+                                     :type "system_event"}}
+                             req)))}))
