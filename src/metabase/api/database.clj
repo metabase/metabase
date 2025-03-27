59fdb34e28d90136f7c3da49534a142deb141754
John Swanson <john.swanson@metabase.com>
2025-03-27 13:17:27 -0700

    fix deletion of router dbs with dest dbs (#55831)

--- src/metabase/api/database.clj
+++ src/metabase/api/database.clj
@@ -1010,6 +1010,7 @@
   (api/check-superuser)
   (api/let-404 [db (t2/select-one :model/Database :id id)]
     (api/check-403 (mi/can-write? db))
+    (t2/delete! :model/Database :router_database_id id)
     (t2/delete! :model/Database :id id)
     (events/publish-event! :event/database-delete {:object db :user-id api/*current-user-id*}))
   api/generic-204-no-content)
