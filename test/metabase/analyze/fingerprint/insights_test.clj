92f407d4c0e61bc176f7e129c7393273dd2cce2b
Noah Moss <noahbmoss@gmail.com>
2025-03-27 16:42:20 -0400

    add unit test

--- test/metabase/analyze/fingerprint/insights_test.clj
+++ test/metabase/analyze/fingerprint/insights_test.clj
@@ -201,7 +201,23 @@
                     (insights/insights [{:base_type :type/DateTime}
                                         {:base_type :type/Number}
                                         ;; Any column with a base type that is not number or temporal previously
-                                        ;; prevented timeseries insights from being calculated
+                                        ;; prevented timeseries insights from being callulated
                                         {:base_type :type/Text}])
                     [["2024-08-09" 10.0 "weekday"]
                      ["2024-08-10" 20.0 "weekend"]])))))
+
+(deftest datetime-unit-insights
+  (testing "A timeseries column with a :type/Text base type can still produce insights if it has a valid :unit (#12388)"
+    (are [assertion datetime-col] (assertion
+                                   (transduce identity
+                                              (insights/insights [datetime-col
+                                                                  {:base_type :type/Number}])
+                                              [["2024-08-09" 10.0]
+                                               ["2024-08-10" 20.0]]))
+      nil?  {:base_type :type/Text}
+         ;; Extraction unit (day-of-week) is classified as a numeric column and doesn't produce insights here
+      nil?  {:base_type :type/Text :unit :day-of-week}
+         ;; Spot check truncation units — should all generate insights
+      some? {:base_type :type/Text :unit :day}
+      some? {:base_type :type/Text :unit :month}
+      some? {:base_type :type/Text :unit :year})))
