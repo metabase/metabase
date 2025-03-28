77e4657c4f056172a3f2ca4601579da2a06db8b0
Sloan Sparger <sloansparger@gmail.com>
2025-03-28 11:37:25 +0700

    make dest db validate on creation, invalidate cache of user attributes after user is updated

--- enterprise/frontend/src/metabase-enterprise/api/db-routing.ts
+++ enterprise/frontend/src/metabase-enterprise/api/db-routing.ts
@@ -1,3 +1,5 @@
+import _ from "underscore";
+
 import { idTag, invalidateTags, listTag } from "metabase/api/tags";
 import type {
   CreateDestinationDatabaseRequest,
@@ -18,17 +20,33 @@ export const dbRoutingApi = EnterpriseApi.injectEndpoints({
       invalidatesTags: (_, error, { id }) =>
         invalidateTags(error, [listTag("database"), idTag("database", id)]),
     }),
+    // this API endpoint is a bulk endpoint but the FE never needs this capability
+    // for ease of use and consistency with other DB creation endpoints the request
+    // and responses types are mapped over to act as if it only can create a single db
     createDestinationDatabase: builder.mutation<
       Database,
       CreateDestinationDatabaseRequest
     >({
-      query: (body) => ({
+      query: ({ router_database_id, destination_database }) => ({
         method: "POST",
-        url: "/api/ee/database-routing/mirror-database",
-        body,
+        url: "/api/ee/database-routing/mirror-database?check_connection_details=true",
+        body: {
+          router_database_id,
+          mirrors: [destination_database],
+        },
       }),
-      transformResponse: (response: [Database]): Database => {
-        return response[0];
+      transformResponse: (response: [Database]): Database => response[0],
+      transformErrorResponse: (response) => {
+        // response shape from api should be:
+        // Record<db_name, { message: string; errors: Record<string, string>; }>;
+        if (_.isObject(response) && _.isObject(response.data)) {
+          const maybeDbEditErrorType = Object.values(response.data)?.[0];
+          if (_.isObject(maybeDbEditErrorType)) {
+            return maybeDbEditErrorType;
+          }
+        }
+
+        return response;
       },
       invalidatesTags: (db, error) =>
         invalidateTags(error, [
