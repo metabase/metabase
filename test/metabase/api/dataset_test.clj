e3b9f3a208640d6b68a3d5f65984af2678a8f0a1
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-03-28 13:46:18 -0400

    Add e2e test

--- test/metabase/api/dataset_test.clj
+++ test/metabase/api/dataset_test.clj
@@ -123,14 +123,24 @@
                  (format-response (most-recent-query-execution-for-query query)))))))))
 
 (deftest dashboard-ad-hoc-context-test
-  (let [query  (mt/mbql-query checkins {:aggregation [[:count]]})
-        result (mt/user-http-request :crowberto :post 202 "dataset" {:query query :dashboard_id 1})]
-    (testing "should return correct query results"
-      (is (=? {:data {:rows [[1000]]}}
-              (format-response result))))
-    (testing "should set `:context` to `:dashboard-ad-hoc` when `:dashboard_id` is passed"
-      (is (=? {:context :dashboard-ad-hoc}
-              (format-response (most-recent-query-execution-for-query query)))))))
+  (testing "POST /api/dataset"
+    (let [query  (mt/mbql-query checkins {:aggregation [[:count]]})
+          result (mt/user-http-request :crowberto :post 202 "dataset" {:query query :dashboard_id 1})]
+      (testing "should return correct query results"
+        (is (=? {:data {:rows [[1000]]}}
+                (format-response result))))
+      (testing "should set `:context` to `:dashboard-ad-hoc` when `:dashboard_id` is passed"
+        (is (=? {:context :dashboard-ad-hoc}
+                (format-response (most-recent-query-execution-for-query query)))))))
+  (testing "POST /api/dataset/pivot"
+    (let [query  (api.pivots/pivot-query)
+          result (mt/user-http-request :crowberto :post 202 "dataset/pivot" {:query query :dashboard_id 1})
+          rows   (mt/rows result)]
+      (testing "should return correct query results"
+        (is (= 1144 (count rows))))
+      (testing "should set `:context` to `:dashboard-ad-hoc` when `:dashboard_id` is passed"
+        (is (=? {:context :dashboard-ad-hoc}
+                (format-response (most-recent-query-execution-for-query query))))))))
 
 (deftest failure-test
   ;; clear out recent query executions!
