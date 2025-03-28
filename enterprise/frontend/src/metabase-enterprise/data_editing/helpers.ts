92eb22ecae96009196b6b04badc1b561b2a3179f
Konstantin Darutkin <spalt08@mail.ru>
2025-03-28 12:49:59 +0000

    fix: review issues

--- enterprise/frontend/src/metabase-enterprise/data_editing/helpers.ts
+++ enterprise/frontend/src/metabase-enterprise/data_editing/helpers.ts
@@ -1,6 +1,6 @@
-import type { Field } from "metabase-types/api";
+import type { FieldWithMetadata } from "metabase-types/api";
 
-export function canEditField(field?: Field) {
+export function canEditField(field?: FieldWithMetadata) {
   return (
     field && field.semantic_type !== "type/PK" && !field.database_is_generated
   );
