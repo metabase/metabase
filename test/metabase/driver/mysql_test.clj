f9e9673ba4dd283f0585d643baeb799527b00455
Eric Normand <ericwnormand@gmail.com>
2025-03-31 11:51:15 -0500

    Remove mysql split-part-test

--- test/metabase/driver/mysql_test.clj
+++ test/metabase/driver/mysql_test.clj
@@ -996,29 +996,3 @@
               (is (= :type/BigInteger (-> cols first :base_type)))
               (doseq [[casted-value _equals? _uncasted-value] rows]
                 (is (int? casted-value))))))))))
-
-(deftest ^:parallel split-part-test
-  (mt/test-driver :mysql
-    (let [mp (mt/metadata-provider)
-          main-strings [(lib.metadata/field mp (mt/id :people :name))
-                        (lib.metadata/field mp (mt/id :people :zip))
-                        (lib.metadata/field mp (mt/id :people :password))
-                        (lib.metadata/field mp (mt/id :people :address))]
-          delimiters [" "
-                      "-"
-                      "7"
-                      (lib/concat "-" " ")]
-          indexes [1 2 (lib/+ 0 2)]]
-      (doseq [main-string main-strings
-              delimiter delimiters
-              index indexes]
-        (testing "split part"
-          (let [query (-> (lib/query mp (lib.metadata/table mp (mt/id :people)))
-                          (lib/expression "SPLITPART" (lib/split-part main-string delimiter index))
-                          (lib/limit 100))
-                result (-> query qp/process-query)
-                cols (mt/cols result)
-                rows (mt/rows result)]
-            (is (= :type/Text (-> cols last :base_type)))
-            (doseq [row rows]
-              (is (string? (last row))))))))))
