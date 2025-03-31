7fec4930631658eb8d3f886921fed4f00e786936
Romeo Van Snick <romeo@romeovansnick.be>
2025-03-31 12:14:41 +0200

    Add repro for #55984

--- e2e/test/scenarios/custom-column/custom-column-reproductions.cy.spec.js
+++ e2e/test/scenarios/custom-column/custom-column-reproductions.cy.spec.js
@@ -1531,3 +1531,52 @@ describe("issue #55940", () => {
       .should("contain", "Offset(Sum([Total]), -1)");
   });
 });
+
+describe("issue #55984", () => {
+  beforeEach(() => {
+    H.restore();
+    cy.signInAsNormalUser();
+    H.openOrdersTable({ mode: "notebook" });
+  });
+
+  it("should not overflow the suggestion tooltip when a suggestion name is too long (metabase#55984)", () => {
+    H.addCustomColumn();
+    H.enterCustomColumnDetails({
+      formula: "[Total]",
+      name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+    });
+    cy.button("Done").click();
+
+    H.summarize({ mode: "notebook" });
+    H.popover().findByText("Custom Expression").click();
+    H.CustomExpressionEditor.type("[lo");
+    H.CustomExpressionEditor.completions().should(($el) => {
+      const scrollWidth = $el[0].scrollWidth;
+      const offsetWidth = $el[0].offsetWidth;
+      // there should be no scroll bars
+      expect(scrollWidth).to.be.lt(offsetWidth);
+    });
+  });
+
+  it("should not overflow the suggestion tooltip when a suggestion name is too long and has no spaces (metabase#55984)", () => {
+    H.addCustomColumn();
+    H.enterCustomColumnDetails({
+      formula: "[Total]",
+      name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt".replaceAll(
+        " ",
+        "_",
+      ),
+    });
+    cy.button("Done").click();
+
+    H.summarize({ mode: "notebook" });
+    H.popover().findByText("Custom Expression").click();
+    H.CustomExpressionEditor.type("[lo");
+    H.CustomExpressionEditor.completions().should(($el) => {
+      const scrollWidth = $el[0].scrollWidth;
+      const offsetWidth = $el[0].offsetWidth;
+      // there should be no scroll bars
+      expect(scrollWidth).to.be.lt(offsetWidth);
+    });
+  });
+});
