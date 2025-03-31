feaab8c9f41bae19192c33cf03636b7c512970b6
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-03-31 11:51:34 -0400

    Case-sensitivity arg name vs example disambiguation (#55965) (#56013)
    * Rename caseInsensitive to caseSensitivity
    
    * Use literal string for case-insensitive template
    
    * Update argument description to be more clear
    
    * Support inline expression highlights
    
    * Render help text description with Markdown
    
    * Add more markdown to help text strings
    
    * Use markdown code blocks to mark code in help text descriptions
    
    * Highlight argument references in help text descriptions
    
    * Fix arg alignment
    
    * Fix HelpText unit tests
    
    * Fix FunctionBrowser tests
    
    * Render FunctionBrowser descriptions using Markdown too
    
    * Mark true
    
    * Fix e2e tests
    
    * Fix e2e tests
    
    * Remove commented-out code
    
    
    
    ---------
    
    Co-authored-by: Romeo Van Snick <romeo@romeovansnick.be>
    Co-authored-by: Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>

--- frontend/src/metabase/query_builder/components/expressions/FunctionBrowser/FunctionBrowser.tsx
+++ frontend/src/metabase/query_builder/components/expressions/FunctionBrowser/FunctionBrowser.tsx
@@ -1,6 +1,8 @@
 import {
   type ChangeEvent,
+  Children,
   type MouseEvent,
+  type ReactNode,
   useCallback,
   useMemo,
   useState,
@@ -8,14 +10,30 @@ import {
 import { t } from "ttag";
 
 import EmptyState from "metabase/components/EmptyState";
+import Markdown from "metabase/core/components/Markdown";
 import { useSelector } from "metabase/lib/redux";
 import { getMetadata } from "metabase/selectors/metadata";
 import { Box, Flex, Icon, Input, Text } from "metabase/ui";
 import type * as Lib from "metabase-lib";
 import type { HelpText, StartRule } from "metabase-lib/v1/expressions";
 
+import { HighlightExpressionSource } from "../HighlightExpression";
+
 import S from "./FunctionBrowser.module.css";
 import { getDatabase, getFilteredClauses, getSearchPlaceholder } from "./utils";
+
+const components = {
+  code(props: { children: ReactNode }) {
+    const children = Children.toArray(props.children);
+    if (!children.every((child) => typeof child === "string")) {
+      return <code>{children}</code>;
+    }
+
+    const source = children.join("").replace(/^\$/, "");
+
+    return <HighlightExpressionSource inline expression={source} />;
+  },
+};
 
 export function FunctionBrowser({
   startRule,
@@ -129,7 +147,7 @@ function FunctionBrowserItem({
       </dt>
       <dd className={S.description}>
         <Text size="sm" c="var(--mb-color-text-medium)">
-          {clause.description}
+          <Markdown components={components}>{clause.description}</Markdown>
         </Text>
       </dd>
     </Box>
