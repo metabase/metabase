af3e3e552dccf40420398f8f1e49c42a5202a39c
lbrdnk <lbrdnk@users.noreply.github.com>
2025-04-01 14:13:03 +0200

    Update partitioning logic of sync-all-indexes!

--- src/metabase/sync/sync_metadata/indexes.clj
+++ src/metabase/sync/sync_metadata/indexes.clj
@@ -65,6 +65,10 @@
    ;; parameter limit of 65,535. See #52746 for details.
    (partition-all 5000 indexes)))
 
+(def ^:dynamic *update-partition-size*
+  "Size of the partition of indexes to update using one `t2/update!` call. Dynamic for testing purposes."
+  1000)
+
 (defn- sync-all-indexes!
   [database]
   (sync-util/with-error-handling "Error syncing Indexes"
@@ -79,28 +83,27 @@
                                                                         :where [:= :t.db_id database-id]}]
                                                         :parent_id nil
                                                         :database_indexed true)
-          [removing adding]          (data/diff existing-indexed-field-ids indexed-field-ids)]
-      (log/infof "Unmarking %d fields from indexed" (count removing))
-      (doseq [field-ids (partition 10 removing)]
-        (log/tracef "Unmarking Field %s as indexed" (pr-str field-ids)))
-      (log/infof "Marking %d fields as indexed" (count adding))
-      (doseq [field-ids (partition 10 adding)]
-        (log/tracef "Marking Field %s as indexed" (pr-str field-ids)))
+          [removing adding]           (data/diff existing-indexed-field-ids indexed-field-ids)
+          removing-count              (count removing)
+          adding-count                (count adding)]
+      ;; Null database_indexed of fields having NO index.
+      (log/infof "Unmarking %d fields from indexed" removing-count)
+      (doseq [field-ids (partition-all 100 removing)]
+        (log/tracef "Unmarking Fields as indexed: %s" (pr-str field-ids)))
+      (doseq [field-ids (partition-all *update-partition-size* removing)]
+        (log/infof "Executing batch update of at most %d fields" *update-partition-size*)
+        (t2/update! :model/Field :parent_id nil :id [:in field-ids] {:database_indexed false}))
+      ;; Set database_indexed of fields having index.
+      (log/infof "Marking %d fields as indexed" adding-count)
+      (doseq [field-ids (partition-all 100 adding)]
+        (log/tracef "Marking Fields as indexed: %s" (pr-str field-ids)))
+      (doseq [field-ids (partition-all *update-partition-size* adding)]
+        (log/infof "Executing batch update of at most %d fields" *update-partition-size*)
+        (t2/update! :model/Field :parent_id nil :id [:in field-ids] {:database_indexed true}))
       (if (or (seq adding) (seq removing))
-        (do
-          (doseq [indexed-field-ids-part (partition 1000 indexed-field-ids)]
-            (log/info "Executing batch update of at most 1000 fields")
-            (t2/update! :model/Field
-                        :table_id [:in {:select [[:t.id]]
-                                        :from [[(t2/table-name :model/Table) :t]]
-                                        :where [:= :t.db_id database-id]}]
-                        :parent_id nil
-                        {:database_indexed (if (seq indexed-field-ids-part)
-                                             [:case [:in :id indexed-field-ids-part] true :else false]
-                                             false)}))
-          {:total-indexes   (count indexed-field-ids)
-           :added-indexes   (count adding)
-           :removed-indexes (count removing)})
+        {:total-indexes   (count indexed-field-ids)
+         :added-indexes   adding-count
+         :removed-indexes removing-count}
         empty-stats))))
 
 (defn maybe-sync-indexes!
