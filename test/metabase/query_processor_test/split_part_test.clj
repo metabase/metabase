b22fb7843baf1ac805f84c06a8e1726a7a8fe9a4
Eric Normand <ericwnormand@gmail.com>
2025-04-01 14:20:39 -0500

    WIP new examples tests + corner cases

--- test/metabase/query_processor_test/split_part_test.clj
+++ test/metabase/query_processor_test/split_part_test.clj
@@ -65,3 +65,54 @@
                          (str/split (re-pattern delimiter))
                          (get (dec index) ""))
                      split-string)))))))))
+
+(deftest ^:parallel split-part-test-examples
+  (mt/test-drivers (mt/normal-drivers-with-feature :split-part)
+    (let [mp (mt/metadata-provider)
+          examples [{:text "ABC-123" :delimiter "-" :position 1 :expected "ABC" :msg "Easy case."}
+                    {:text "ABC-123" :delimiter "-" :position 2 :expected "123" :msg "Easy case."}
+                    ;;{:text "ABC-123" :delimiter "-" :position 0 :expected ""    :msg "Position too low."}
+                    {:text "ABC-123" :delimiter "-" :position 3 :expected ""    :msg "Position too high."}
+
+                    {:text "/ABC/123/" :delimiter "/" :position 1 :expected "" :msg "Empty part when delimiter is first char."}
+                    {:text "/ABC/123/" :delimiter "/" :position 2 :expected "ABC" :msg "First part of path."}
+                    {:text "/ABC/123/" :delimiter "/" :position 3 :expected "123" :msg "Second part of path."}
+                    {:text "/ABC/123/" :delimiter "/" :position 4 :expected "" :msg "Empty part when delimiter is last char."}
+                    {:text "/ABC/123/" :delimiter "/" :position 40 :expected "" :msg "Empty part when position out of bounds."}
+
+                    {:text "ABC-123" :delimiter "," :position 1 :expected "ABC-123"    :msg "Delimiter doesn't exist."}
+
+                    {:text "ABC-123" :delimiter "ABC-123" :position 1 :expected ""    :msg "Delimiter matches whole string."}
+                    {:text "ABC-123" :delimiter "ABC-123" :position 2 :expected ""    :msg "Delimiter matches whole string."}
+
+                    {:text "ABC-123" :delimiter "ABC-1235" :position 1 :expected "ABC-123"    :msg "Delimiter longer than whole string."}]]
+      (doseq [{:keys [text delimiter position expected msg]} examples]
+        (testing (str "split part: " msg)
+          (let [query (-> (lib/query mp (lib.metadata/table mp (mt/id :people)))
+                          (lib/with-fields [(lib.metadata/field mp (mt/id :people :id))])
+                          (lib/expression "SPLITPART" (lib/split-part text delimiter position))
+                          (lib/limit 1))
+                result (-> query qp/process-query)
+                cols (mt/cols result)
+                rows (mt/rows result)]
+            (is (= :type/Text (-> cols last :base_type)))
+            (doseq [[_id split-string] rows]
+              (is (string? split-string))
+              (is (= expected split-string)
+                  (str "Full field: " (pr-str text) ", Delimiter: " (pr-str delimiter) ", position: " position "; " msg)))))))))
+
+(deftest ^:parallel split-part-test-corner-cases
+  (mt/test-drivers (mt/normal-drivers-with-feature :split-part)
+    (let [mp (mt/metadata-provider)]
+      (testing "split part: empty delimiter should be empty string"
+        (let [query (-> (lib/query mp (lib.metadata/table mp (mt/id :people)))
+                        (lib/with-fields [(lib.metadata/field mp (mt/id :people :id))])
+                        (lib/expression "SPLITPART" (lib/split-part "ABC-123-XYZ" "-" (lib/- 0 1)))
+                        (lib/limit 1))
+              result (-> query qp/process-query)
+              cols (mt/cols result)
+              rows (mt/rows result)]
+          (is (= :type/Text (-> cols last :base_type)))
+          (doseq [[_id split-string] rows]
+            (is (string? split-string))
+            (is (= "" split-string))))))))
