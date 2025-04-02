087d9fd2d7a4957bd0178239bca23b81e6646929
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-04-02 21:36:49 +0700

    Add MetabaseQuestion and its storybook

---
+++ enterprise/frontend/src/embedding-sdk/components/public/MetabotQuestion/MetabotQuestion.stories.tsx
@@ -0,0 +1,41 @@
+import type { StoryFn } from "@storybook/react";
+import type { ComponentProps } from "react";
+
+import { CommonSdkStoryWrapper } from "embedding-sdk/test/CommonSdkStoryWrapper";
+import { questionIds } from "embedding-sdk/test/storybook-id-args";
+import { Box } from "metabase/ui";
+
+import { MetabotQuestion } from "./MetabotQuestion";
+
+const QUESTION_ID = (window as any).QUESTION_ID || questionIds.numberId;
+
+type MetabotQuestionProps = ComponentProps<typeof MetabotQuestion>;
+
+export default {
+  title: "EmbeddingSDK/MetabotQuestion",
+  component: MetabotQuestion,
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [CommonSdkStoryWrapper],
+};
+
+const Template: StoryFn<MetabotQuestionProps> = () => {
+  return (
+    <Box bg="var(--mb-color-background)" mih="100vh">
+      <MetabotQuestion visible={true} onClose={() => {}} />
+    </Box>
+  );
+};
+
+export const Default = {
+  render: Template,
+
+  args: {
+    questionId: QUESTION_ID,
+    isSaveEnabled: true,
+    targetCollection: undefined,
+    title: true,
+    withResetButton: true,
+  },
+};
---
+++ enterprise/frontend/src/embedding-sdk/components/public/MetabotQuestion/MetabotQuestion.tsx
@@ -0,0 +1,37 @@
+import { useMemo, useState } from "react";
+
+import { InteractiveAdHocQuestion } from "embedding-sdk/components/private/InteractiveAdHocQuestion";
+import { MetabotChat } from "metabase-enterprise/metabot/components/MetabotChat";
+import { MetabotProvider } from "metabase-enterprise/metabot/context";
+
+interface Props {
+  visible: boolean;
+  onClose: () => void;
+}
+
+export const MetabotQuestion = ({ visible, onClose }: Props) => {
+  const [result, setResult] = useState<Record<string, any> | null>(null);
+
+  const redirectUrl = useMemo(() => {
+    return result?.payload?.payload?.data?.reactions?.find(
+      (reaction: { type: string; url: string }) =>
+        reaction.type === "metabot.reaction/redirect",
+    )?.url;
+  }, [result]);
+
+  return (
+    <MetabotProvider>
+      {redirectUrl && (
+        <InteractiveAdHocQuestion
+          questionPath={redirectUrl}
+          title={false}
+          onNavigateBack={() => {}}
+        />
+      )}
+
+      {visible && (
+        <MetabotChat onClose={onClose} onResult={setResult} withMicrophone />
+      )}
+    </MetabotProvider>
+  );
+};
