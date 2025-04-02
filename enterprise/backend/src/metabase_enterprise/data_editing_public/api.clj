c228954ffe115fc322f6d484221a96c74b13d918
Dan Stone <wotbrew@gmail.com>
2025-04-02 13:41:43 +0100

    Add webhook ingestion support (#56024)
    Added ability to create unauthenticated webhook endpoints that can
    insert rows into the database.
    
    Hooks are expected to fire a single record or multiple records at the
    generated endpoint.
    
    Moved some logic around (adding data_editing.clj) in order to accomodate
    new public endpoints that share behaviour with the existing insert
    endpoint.

---
+++ enterprise/backend/src/metabase_enterprise/data_editing_public/api.clj
@@ -0,0 +1,30 @@
+(ns metabase-enterprise.data-editing-public.api
+  (:require
+   [metabase-enterprise.data-editing.data-editing :as data-editing]
+   [metabase.api.common :as api]
+   [metabase.api.macros :as api.macros]
+   [toucan2.core :as t2]))
+
+(api.macros/defendpoint :post "/webhook/:token/data"
+  "Inserts rows into the table associated with the token. See routes for ee/data-editing/webhook for token CRUD.
+
+  Accepts either a single row or multiple rows in an array.
+  Expects all required columns to be provided.
+
+  The provided row JSON must be compatible with the underlying types of the table.
+  e.g if you have set up a casting rule for a unix epoch seconds field to present it as a datetime, callers must provide the integer value to this API.
+
+  Callers should expect 400 errors on constraint violation (e.g the primary key already exists) or the schema is not met."
+  [{:keys [token]}
+   _
+   row-or-rows]
+  (let [table-id (api/check-404 (t2/select-one-fn :table_id :table_webhook_token :token token))
+        rows     (if (map? row-or-rows) [row-or-rows] row-or-rows)]
+    (when (seq rows)
+      (api/check-400 (every? seq rows))
+      (data-editing/insert! table-id rows))
+    {}))
+
+(def ^{:arglists '([request respond raise])} routes
+  "`/api/ee/data-editing routes."
+  (api.macros/ns-handler *ns*))
