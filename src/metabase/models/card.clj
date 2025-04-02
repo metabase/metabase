e4e18123497e0261516832350b78fad11a8e217b
Chris Truter <crisptrutski@users.noreply.github.com>
2025-04-02 14:27:24 +0200

    Make editables models instead of questions (#56135)
    * Make editables models instead of questions
    
    * seems to be fine
    
    * Make it more defensive

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -553,7 +553,14 @@
   (let [correct-collection-id (t2/select-one-fn :collection_id [:model/Dashboard :collection_id] (:dashboard_id card))
         invalid? (or (and (contains? card :collection_id)
                           (not= correct-collection-id (:collection_id card)))
-                     (not (contains? #{:question "question" nil} (:type card)))
+                     ;(not (contains? #{:question "question" nil} (:type card)))
+                     (case (some-> (:type card) name)
+                       nil false
+                       "question" false
+                       ;; allow editables
+                       ;; TODO there may be other places in the dashboard-question code paths that break with models
+                       "model" (= "editable-table" (:display card))
+                       true)
                      (some? (:collection_position card)))]
     (when invalid?
       (throw (ex-info (tru "Invalid dashboard-internal card")
