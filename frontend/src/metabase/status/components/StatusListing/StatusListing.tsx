2c43600fd85231c000c088b3ba4d46e0e1adc953
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-02 17:34:30 -0400

    wip

--- frontend/src/metabase/status/components/StatusListing/StatusListing.tsx
+++ frontend/src/metabase/status/components/StatusListing/StatusListing.tsx
@@ -25,6 +25,9 @@ const StatusListing = () => {
 
   useCheckActiveDownloadsBeforeUnload();
 
+  // FIXME: temporary work around for silly problem
+  return null;
+
   return (
     <StatusListingRoot data-testid="status-root-container">
       {isAdmin && <DatabaseStatus />}
