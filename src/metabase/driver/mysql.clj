79caedfb08792387be3f230063feaca34c55554c
Eric Normand <ericwnormand@gmail.com>
2025-04-02 10:39:50 -0500

    Rename :cast in tier 2 databases

--- src/metabase/driver/mysql.clj
+++ src/metabase/driver/mysql.clj
@@ -70,7 +70,9 @@
                               :schemas                                false
                               :uploads                                true
                               :identifiers-with-spaces                true
-                              :cast                                   true
+                              :expressions/text                       true
+                              :expressions/integer                    true
+                              :expressions/date                       true
                               ;; MySQL doesn't let you have lag/lead in the same part of a query as a `GROUP BY`; to
                               ;; fully support `offset` we need to do some kooky query transformations just for MySQL
                               ;; and make this work.
