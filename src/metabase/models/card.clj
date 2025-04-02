6ab330f0f08e1da40c281255da02681d483f3947
Chris Truter <chris@metabase.com>
2025-04-02 14:26:57 +0200

    Make it more defensive

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -554,8 +554,13 @@
         invalid? (or (and (contains? card :collection_id)
                           (not= correct-collection-id (:collection_id card)))
                      ;(not (contains? #{:question "question" nil} (:type card)))
-                     ;; TODO this might cause havoc somewhere?
-                     (not (contains? #{:question "question" :model "model" nil} (:type card)))
+                     (case (some-> (:type card) name)
+                       nil false
+                       "question" false
+                       ;; allow editables
+                       ;; TODO there may be other places in the dashboard-question code paths that break with models
+                       "model" (= "editable-table" (:display card))
+                       true)
                      (some? (:collection_position card)))]
     (when invalid?
       (throw (ex-info (tru "Invalid dashboard-internal card")
