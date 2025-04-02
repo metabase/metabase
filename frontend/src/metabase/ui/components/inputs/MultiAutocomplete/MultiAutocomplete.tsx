660127218eae4a9af1d1cdfe8a5a85495e2bcfb0
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-04-02 15:27:38 -0400

    PoC

--- frontend/src/metabase/ui/components/inputs/MultiAutocomplete/MultiAutocomplete.tsx
+++ frontend/src/metabase/ui/components/inputs/MultiAutocomplete/MultiAutocomplete.tsx
@@ -28,9 +28,8 @@ export function MultiAutocomplete({
 }: MultiAutocompleteProps) {
   const combobox = useCombobox();
   const {
-    visibleValues,
+    pillValues,
     fieldValue,
-    fieldValueIndex,
     handleFieldChange,
     handleFieldFocus,
     handleFieldBlur,
@@ -38,28 +37,13 @@ export function MultiAutocomplete({
     handlePillRemoveClick,
   } = useMultiAutocomplete({ values, shouldCreate, onChange });
 
-  const field = (
-    <Combobox.EventsTarget>
-      <PillsInput.Field
-        value={fieldValue}
-        placeholder={placeholder}
-        autoFocus={autoFocus || fieldValueIndex != null}
-        onChange={handleFieldChange}
-        onFocus={handleFieldFocus}
-        onBlur={handleFieldBlur}
-      />
-    </Combobox.EventsTarget>
-  );
-
   return (
     <Combobox store={combobox}>
       <Combobox.DropdownTarget>
         <PillsInput>
           <Pill.Group>
-            {visibleValues.map((value, valueIndex) =>
-              fieldValueIndex === valueIndex ? (
-                field
-              ) : (
+            {pillValues.map((value, valueIndex) =>
+              value !== FIELD_PLACEHOLDER ? (
                 <Pill
                   key={valueIndex}
                   withRemoveButton
@@ -68,9 +52,19 @@ export function MultiAutocomplete({
                 >
                   {value}
                 </Pill>
+              ) : (
+                <Combobox.EventsTarget key="field">
+                  <PillsInput.Field
+                    value={fieldValue}
+                    placeholder={placeholder}
+                    autoFocus={autoFocus}
+                    onChange={handleFieldChange}
+                    onFocus={handleFieldFocus}
+                    onBlur={handleFieldBlur}
+                  />
+                </Combobox.EventsTarget>
               ),
             )}
-            {fieldValueIndex == null && field}
           </Pill.Group>
         </PillsInput>
       </Combobox.DropdownTarget>
@@ -90,75 +84,75 @@ export function MultiAutocomplete({
   );
 }
 
+const DELIMITERS = [",", "\t", "\n"];
+const FIELD_PLACEHOLDER = null;
+
 type UseMultiAutocompleteProps = {
   values: string[];
   shouldCreate: (newValue: string) => boolean;
   onChange: (newValues: string[]) => void;
 };
 
+type FieldSelection = {
+  index: number;
+  length: number;
+};
+
 function useMultiAutocomplete({
-  values: effectiveValues,
+  values,
   shouldCreate,
   onChange,
 }: UseMultiAutocompleteProps) {
-  const [isFocused, setIsFocused] = useState(false);
-  const [pillValues, setPillValues] = useState<string[]>([]);
   const [fieldValue, setFieldValue] = useState("");
-  const [fieldValueIndex, setFieldValueIndex] = useState<number>();
-  const visibleValues = isFocused ? pillValues : effectiveValues;
+  const [fieldSelection, setFieldSelection] = useState<FieldSelection>({
+    index: values.length,
+    length: 0,
+  });
 
   const handleFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value;
-    const parsedValues = parseValues(newValue).filter(shouldCreate);
-    const isMultiValue = parsedValues.length > 1;
-    const isDelimiter = DELIMITERS.some((delimiter) =>
-      newValue.endsWith(delimiter),
+    const rawValue = event.target.value;
+    const parsedValues = parseValues(rawValue).filter(shouldCreate);
+    const newValues = getValuesAfterChange(
+      values,
+      parsedValues,
+      fieldSelection,
     );
-
-    const newValues = pillValues.concat(parsedValues);
-    const newPillValues = isMultiValue || isDelimiter ? newValues : pillValues;
-    const newFieldValue = isMultiValue || isDelimiter ? "" : newValue;
+    const { fieldValue: newFieldValue, fieldSelection: newFieldSelection } =
+      getFieldStateAfterChange(fieldSelection, rawValue, parsedValues);
     onChange(newValues);
-    setPillValues(newPillValues);
     setFieldValue(newFieldValue);
+    setFieldSelection(newFieldSelection);
   };
 
   const handleFieldFocus = () => {
-    setIsFocused(true);
-    setPillValues(effectiveValues);
+    setFieldSelection({ index: values.length, length: 0 });
   };
 
   const handleFieldBlur = () => {
-    setIsFocused(false);
-    setPillValues([]);
     setFieldValue("");
-    setFieldValueIndex(undefined);
+    setFieldSelection({ index: values.length, length: 0 });
   };
 
   const handlePillDoubleClick = (valueIndex: number) => {
-    setFieldValue(visibleValues[valueIndex]);
-    setFieldValueIndex(valueIndex);
+    setFieldValue(values[valueIndex]);
+    setFieldSelection({ index: valueIndex, length: 1 });
   };
 
   const handlePillRemoveClick = (valueIndex: number) => {
-    const newValues = [...effectiveValues];
-    const removeValueIndex =
-      fieldValueIndex != null &&
-      valueIndex > fieldValueIndex &&
-      !shouldCreate(fieldValue)
-        ? valueIndex - 1
-        : valueIndex;
-    newValues.splice(removeValueIndex, 1);
+    const newValues = [...values];
+    newValues.splice(valueIndex, 1);
     onChange(newValues);
-    setPillValues(newValues);
     setFieldValue("");
-    setFieldValueIndex(undefined);
+    setFieldSelection({ index: newValues.length, length: 0 });
   };
 
   return {
-    visibleValues,
+    pillValues: getValuesAfterChange(
+      values,
+      [FIELD_PLACEHOLDER],
+      fieldSelection,
+    ),
     fieldValue,
-    fieldValueIndex,
     handleFieldChange,
     handleFieldFocus,
     handleFieldBlur,
@@ -167,11 +161,49 @@ function useMultiAutocomplete({
   };
 }
 
-const DELIMITERS = [",", "\t", "\n"];
+function getValuesAfterChange<T>(
+  values: T[],
+  parsedValues: T[],
+  fieldSelection: FieldSelection,
+) {
+  return [
+    ...values.slice(0, fieldSelection.index),
+    ...parsedValues,
+    ...values.slice(fieldSelection.index + fieldSelection.length),
+  ];
+}
 
-function parseValues(str: string): string[] {
+function getFieldStateAfterChange(
+  fieldSelection: FieldSelection,
+  fieldValue: string,
+  parsedValues: string[],
+) {
+  const isDelimiter = DELIMITERS.some((delimiter) =>
+    fieldValue.endsWith(delimiter),
+  );
+
+  if (parsedValues.length > 1 || isDelimiter) {
+    return {
+      fieldValue: "",
+      fieldSelection: {
+        index: fieldSelection.index + parsedValues.length,
+        length: 0,
+      },
+    };
+  } else {
+    return {
+      fieldValue,
+      fieldSelection: {
+        index: fieldSelection.index,
+        length: parsedValues.length > 0 ? 1 : 0,
+      },
+    };
+  }
+}
+
+function parseValues(fieldValue: string): string[] {
   try {
-    return parse(str, {
+    return parse(fieldValue, {
       delimiter: DELIMITERS,
       skip_empty_lines: true,
       relax_column_count: true,
