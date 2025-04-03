dc2bc0a393cf412b05a231d7edaba4fd5cef3959
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-04-03 09:26:42 -0400

    PoC

--- frontend/src/metabase/ui/components/inputs/MultiAutocomplete/MultiAutocomplete.tsx
+++ frontend/src/metabase/ui/components/inputs/MultiAutocomplete/MultiAutocomplete.tsx
@@ -4,15 +4,10 @@ import {
   OptionsDropdown,
   Pill,
   PillsInput,
-  useCombobox,
 } from "@mantine/core";
-import { parse } from "csv-parse/browser/esm/sync";
-import {
-  type ChangeEvent,
-  type KeyboardEvent,
-  type ReactNode,
-  useState,
-} from "react";
+import type { ReactNode } from "react";
+
+import { useMultiAutocomplete } from "./use-multi-autocomplete";
 
 export type MultiAutocompleteProps = {
   values: string[];
@@ -29,14 +24,14 @@ export function MultiAutocomplete({
   values,
   options,
   placeholder,
-  shouldCreate = defaultShouldCreate,
+  shouldCreate,
   autoFocus,
   nothingFoundMessage,
   onChange,
   onSearchChange,
 }: MultiAutocompleteProps) {
-  const combobox = useCombobox();
   const {
+    combobox,
     pillValues,
     fieldValue,
     handleFieldChange,
@@ -45,15 +40,16 @@ export function MultiAutocomplete({
     handleFieldBlur,
     handlePillDoubleClick,
     handlePillRemoveClick,
+    handleOptionSubmit,
   } = useMultiAutocomplete({ values, shouldCreate, onChange, onSearchChange });
 
   return (
-    <Combobox store={combobox}>
+    <Combobox store={combobox} onOptionSubmit={handleOptionSubmit}>
       <Combobox.DropdownTarget>
-        <PillsInput>
+        <PillsInput onClick={() => combobox.openDropdown()}>
           <Pill.Group>
             {pillValues.map((value, valueIndex) =>
-              value !== FIELD_PLACEHOLDER ? (
+              value !== null ? (
                 <Pill
                   key={valueIndex}
                   withRemoveButton
@@ -94,182 +90,4 @@ export function MultiAutocomplete({
       />
     </Combobox>
   );
-}
-
-const DELIMITERS = [",", "\t", "\n"];
-const QUOTE_CHAR = '"';
-const ESCAPE_CHAR = "\\";
-const SPECIAL_CHARS_REGEX = /[,\t\n"\\]/g;
-const FIELD_PLACEHOLDER = null;
-
-type UseMultiAutocompleteProps = {
-  values: string[];
-  shouldCreate: (newValue: string) => boolean;
-  onChange: (newValues: string[]) => void;
-  onSearchChange?: (newValue: string) => void;
-};
-
-type FieldSelection = {
-  index: number;
-  length: number;
-};
-
-function useMultiAutocomplete({
-  values,
-  shouldCreate,
-  onChange,
-  onSearchChange,
-}: UseMultiAutocompleteProps) {
-  const [fieldValue, setFieldValue] = useState("");
-  const [fieldSelection, setFieldSelection] = useState<FieldSelection>({
-    index: values.length,
-    length: 0,
-  });
-
-  const setFieldValueAndNotify = (newFieldValue: string) => {
-    setFieldValue(newFieldValue);
-    onSearchChange?.(newFieldValue);
-  };
-
-  const removeValue = (
-    valueIndex: number,
-    { preserveSelection }: { preserveSelection: boolean },
-  ) => {
-    const newValues = [...values];
-    newValues.splice(valueIndex, 1);
-    onChange(newValues);
-    setFieldValueAndNotify("");
-    setFieldSelection({
-      index: preserveSelection
-        ? fieldSelection.index + fieldSelection.length - 1
-        : newValues.length,
-      length: 0,
-    });
-  };
-
-  const handleFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const rawValue = event.target.value;
-    const parsedValues = parseCsv(rawValue).filter(shouldCreate);
-    const newValues = getValuesAfterChange(
-      values,
-      parsedValues,
-      fieldSelection,
-    );
-    const { fieldValue: newFieldValue, fieldSelection: newFieldSelection } =
-      getFieldStateAfterChange(fieldSelection, rawValue, parsedValues);
-    onChange(newValues);
-    setFieldValueAndNotify(newFieldValue);
-    setFieldSelection(newFieldSelection);
-  };
-
-  const handleFieldKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (
-      event.key === "Backspace" &&
-      fieldValue.length === 0 &&
-      fieldSelection.index > 0 &&
-      fieldSelection.length === 0
-    ) {
-      removeValue(fieldSelection.index - 1, { preserveSelection: true });
-    }
-  };
-
-  const handleFieldFocus = () => {
-    setFieldSelection({ index: values.length, length: 0 });
-  };
-
-  const handleFieldBlur = () => {
-    setFieldValueAndNotify("");
-    setFieldSelection({ index: values.length, length: 0 });
-  };
-
-  const handlePillDoubleClick = (valueIndex: number) => {
-    setFieldValueAndNotify(formatCsv(values[valueIndex]));
-    setFieldSelection({ index: valueIndex, length: 1 });
-  };
-
-  const handlePillRemoveClick = (valueIndex: number) => {
-    removeValue(valueIndex, { preserveSelection: false });
-  };
-
-  return {
-    pillValues: getValuesAfterChange(
-      values,
-      [FIELD_PLACEHOLDER],
-      fieldSelection,
-    ),
-    fieldValue,
-    handleFieldChange,
-    handleFieldKeyDown,
-    handleFieldFocus,
-    handleFieldBlur,
-    handlePillDoubleClick,
-    handlePillRemoveClick,
-  };
-}
-
-function getValuesAfterChange<T>(
-  values: T[],
-  parsedValues: T[],
-  fieldSelection: FieldSelection,
-) {
-  return [
-    ...values.slice(0, fieldSelection.index),
-    ...parsedValues,
-    ...values.slice(fieldSelection.index + fieldSelection.length),
-  ];
-}
-
-function getFieldStateAfterChange(
-  fieldSelection: FieldSelection,
-  fieldValue: string,
-  parsedValues: string[],
-) {
-  const isDelimiter = DELIMITERS.some((delimiter) =>
-    fieldValue.endsWith(delimiter),
-  );
-
-  if (parsedValues.length > 1 || (isDelimiter && parsedValues.length > 0)) {
-    return {
-      fieldValue: "",
-      fieldSelection: {
-        index: fieldSelection.index + parsedValues.length,
-        length: 0,
-      },
-    };
-  } else {
-    return {
-      fieldValue,
-      fieldSelection: {
-        index: fieldSelection.index,
-        length: parsedValues.length > 0 ? 1 : 0,
-      },
-    };
-  }
-}
-
-function parseCsv(rawValue: string): string[] {
-  try {
-    return parse(rawValue, {
-      delimiter: DELIMITERS,
-      skip_empty_lines: true,
-      relax_column_count: true,
-      relax_quotes: true,
-      trim: true,
-      quote: QUOTE_CHAR,
-      escape: ESCAPE_CHAR,
-    }).flat();
-  } catch (err) {
-    return [];
-  }
-}
-
-function formatCsv(value: string): string {
-  if (SPECIAL_CHARS_REGEX.test(value)) {
-    return `${QUOTE_CHAR}${value.replaceAll(SPECIAL_CHARS_REGEX, (s) => `${ESCAPE_CHAR}${s}`)}${QUOTE_CHAR}`;
-  }
-  return value;
-}
-
-function defaultShouldCreate(value: string) {
-  return value.trim().length > 0;
 }
---
+++ frontend/src/metabase/ui/components/inputs/MultiAutocomplete/use-multi-autocomplete.ts
@@ -0,0 +1,210 @@
+import { useCombobox } from "@mantine/core";
+import { parse } from "csv-parse/dist/esm/sync";
+import { type ChangeEvent, type KeyboardEvent, useState } from "react";
+
+const DELIMITERS = [",", "\t", "\n"];
+const QUOTE_CHAR = '"';
+const ESCAPE_CHAR = "\\";
+const SPECIAL_CHARS_REGEX = /[,\t\n"\\]/g;
+const FIELD_PLACEHOLDER = null;
+
+type UseMultiAutocompleteProps = {
+  values: string[];
+  shouldCreate?: (newValue: string) => boolean;
+  onChange: (newValues: string[]) => void;
+  onSearchChange?: (newValue: string) => void;
+};
+
+type FieldState = {
+  fieldValue: string;
+  fieldSelection: FieldSelection;
+};
+
+type FieldSelection = {
+  index: number;
+  length: number;
+};
+
+export function useMultiAutocomplete({
+  values,
+  shouldCreate = defaultShouldCreate,
+  onChange,
+  onSearchChange,
+}: UseMultiAutocompleteProps) {
+  const combobox = useCombobox();
+  const [fieldValue, setFieldValue] = useState("");
+  const [fieldSelection, setFieldSelection] = useState<FieldSelection>({
+    index: values.length,
+    length: 0,
+  });
+
+  const setFieldState = ({ fieldValue, fieldSelection }: FieldState) => {
+    setFieldValue(fieldValue);
+    setFieldSelection(fieldSelection);
+    onSearchChange?.(fieldValue);
+  };
+
+  const handleFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const rawValue = event.target.value;
+    const parsedValues = parseCsv(rawValue).filter(shouldCreate);
+    const newValues = getValuesAfterChange(
+      values,
+      parsedValues,
+      fieldSelection,
+    );
+    onChange(newValues);
+    const newFieldState = getFieldStateAfterChange(
+      fieldSelection,
+      rawValue,
+      parsedValues,
+    );
+    setFieldState(newFieldState);
+  };
+
+  const handleFieldKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (
+      event.key === "Backspace" &&
+      fieldValue.length === 0 &&
+      fieldSelection.index > 0 &&
+      fieldSelection.length === 0
+    ) {
+      const newValues = [...values];
+      newValues.splice(fieldSelection.index - 1, 1);
+      onChange(newValues);
+      setFieldState({
+        fieldValue: "",
+        fieldSelection: {
+          index: fieldSelection.index + fieldSelection.length - 1,
+          length: 0,
+        },
+      });
+    }
+  };
+
+  const handleFieldFocus = () => {
+    setFieldSelection({ index: values.length, length: 0 });
+  };
+
+  const handleFieldBlur = () => {
+    setFieldState({
+      fieldValue: "",
+      fieldSelection: { index: values.length, length: 0 },
+    });
+  };
+
+  const handlePillDoubleClick = (valueIndex: number) => {
+    setFieldState({
+      fieldValue: formatCsv(values[valueIndex]),
+      fieldSelection: { index: valueIndex, length: 1 },
+    });
+  };
+
+  const handlePillRemoveClick = (valueIndex: number) => {
+    const newValues = [...values];
+    newValues.splice(valueIndex, 1);
+    onChange(newValues);
+    setFieldState({
+      fieldValue: "",
+      fieldSelection: {
+        index: newValues.length,
+        length: 0,
+      },
+    });
+  };
+
+  const handleOptionSubmit = (value: string) => {
+    const newValues = getValuesAfterChange(values, [value], fieldSelection);
+    onChange(newValues);
+    setFieldState({
+      fieldValue: "",
+      fieldSelection: {
+        index: fieldSelection.index + fieldSelection.length,
+        length: 0,
+      },
+    });
+  };
+
+  return {
+    combobox,
+    pillValues: getValuesAfterChange(
+      values,
+      [FIELD_PLACEHOLDER],
+      fieldSelection,
+    ),
+    fieldValue,
+    handleFieldChange,
+    handleFieldKeyDown,
+    handleFieldFocus,
+    handleFieldBlur,
+    handlePillDoubleClick,
+    handlePillRemoveClick,
+    handleOptionSubmit,
+  };
+}
+
+function getValuesAfterChange<T>(
+  values: T[],
+  parsedValues: T[],
+  fieldSelection: FieldSelection,
+) {
+  return [
+    ...values.slice(0, fieldSelection.index),
+    ...parsedValues,
+    ...values.slice(fieldSelection.index + fieldSelection.length),
+  ];
+}
+
+function getFieldStateAfterChange(
+  fieldSelection: FieldSelection,
+  fieldValue: string,
+  parsedValues: string[],
+) {
+  const isDelimiter = DELIMITERS.some((delimiter) =>
+    fieldValue.endsWith(delimiter),
+  );
+
+  if (parsedValues.length > 1 || (isDelimiter && parsedValues.length > 0)) {
+    return {
+      fieldValue: "",
+      fieldSelection: {
+        index: fieldSelection.index + parsedValues.length,
+        length: 0,
+      },
+    };
+  } else {
+    return {
+      fieldValue,
+      fieldSelection: {
+        index: fieldSelection.index,
+        length: parsedValues.length > 0 ? 1 : 0,
+      },
+    };
+  }
+}
+
+function parseCsv(rawValue: string): string[] {
+  try {
+    return parse(rawValue, {
+      delimiter: DELIMITERS,
+      skip_empty_lines: true,
+      relax_column_count: true,
+      relax_quotes: true,
+      trim: true,
+      quote: QUOTE_CHAR,
+      escape: ESCAPE_CHAR,
+    }).flat();
+  } catch (err) {
+    return [];
+  }
+}
+
+function formatCsv(value: string): string {
+  if (SPECIAL_CHARS_REGEX.test(value)) {
+    return `${QUOTE_CHAR}${value.replaceAll(SPECIAL_CHARS_REGEX, (s) => `${ESCAPE_CHAR}${s}`)}${QUOTE_CHAR}`;
+  }
+  return value;
+}
+
+function defaultShouldCreate(value: string) {
+  return value.trim().length > 0;
+}
