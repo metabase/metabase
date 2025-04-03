896c488d2a6597ecf799f0fbbaadaaba67bb2e92
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-04-03 15:27:35 -0400

    PoC

--- frontend/src/metabase/parameters/components/widgets/NumberInputWidget/NumberInputWidget.tsx
+++ frontend/src/metabase/parameters/components/widgets/NumberInputWidget/NumberInputWidget.tsx
@@ -73,32 +73,6 @@ export function NumberInputWidget({
     values.map(getOption).filter((item): item is SelectItem => item !== null) ??
     [];
 
-  const valueOptions = unsavedArrayValue
-    .map((item): SelectItem | null => {
-      const option = parameter?.values_source_config?.values?.find(
-        (option) => getValue(option)?.toString() === item?.toString(),
-      );
-
-      if (!option) {
-        return null;
-      }
-
-      const value = getValue(option)?.toString();
-      if (typeof value !== "string") {
-        return null;
-      }
-
-      return {
-        label: getLabel(option),
-        value,
-      };
-    })
-    .filter(isNotNull);
-
-  const customLabelOptions = options.filter(
-    (option) => option.label !== option.value,
-  );
-
   function shouldCreate(value: string) {
     const res = parseNumber(value);
     return res !== null;
@@ -119,7 +93,7 @@ export function NumberInputWidget({
             placeholder={placeholder}
             shouldCreate={shouldCreate}
             autoFocus={autoFocus}
-            options={customLabelOptions.concat(valueOptions)}
+            options={options}
           />
         </TokenFieldWrapper>
       ) : (
