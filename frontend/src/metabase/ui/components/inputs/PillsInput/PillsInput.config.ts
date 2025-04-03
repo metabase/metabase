a2d3b8fd091b4594d09d77813b66526073bf59ac
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-04-03 12:35:31 -0400

    PoC

--- frontend/src/metabase/ui/components/inputs/PillsInput/PillsInput.config.ts
+++ frontend/src/metabase/ui/components/inputs/PillsInput/PillsInput.config.ts
@@ -1,24 +1,14 @@
-import {
-  type MantineThemeOverride,
-  PillsInput,
-  PillsInputField,
-} from "@mantine/core";
+import { type MantineThemeOverride, PillsInput } from "@mantine/core";
 
 import S from "./PillsInput.module.css";
 
 export const pillsInputOverrides: MantineThemeOverride["components"] = {
   PillsInput: PillsInput.extend({
     defaultProps: {
-      size: "md",
+      variant: "default",
     },
     classNames: {
-      wrapper: S.wrapper,
       input: S.input,
-    },
-  }),
-  PillsInputField: PillsInputField.extend({
-    classNames: {
-      field: S.field,
     },
   }),
 };
