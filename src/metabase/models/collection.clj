0868aed492200197afe2e9cab9783b41779c32cb
Alexander Solovyov <alexander@solovyov.net>
2025-04-03 10:19:37 +0300

    sorry i messed up (#56202)

--- src/metabase/models/collection.clj
+++ src/metabase/models/collection.clj
@@ -23,7 +23,6 @@
    ;; Trying to use metabase.search would cause a circular reference ;_;
    [metabase.search.spec :as search.spec]
    [metabase.util :as u]
-   [metabase.util.date-2 :as u.date]
    [metabase.util.honey-sql-2 :as h2x]
    [metabase.util.i18n :refer [trs tru]]
    [metabase.util.log :as log]
