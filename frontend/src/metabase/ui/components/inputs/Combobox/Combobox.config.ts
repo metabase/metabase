541da5c96923e367919b10fb7bfc44969fb28fb6
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-04-03 10:00:18 -0400

    Basic styling

--- frontend/src/metabase/ui/components/inputs/Combobox/Combobox.config.ts
+++ frontend/src/metabase/ui/components/inputs/Combobox/Combobox.config.ts
@@ -1,9 +1,14 @@
 import { Combobox, type MantineThemeOverride } from "@mantine/core";
 
+import S from "./Combobox.module.css";
+
 export const comboboxOverrides: MantineThemeOverride["components"] = {
   Combobox: Combobox.extend({
     defaultProps: {
       size: "md",
+    },
+    classNames: {
+      empty: S.empty,
     },
   }),
 };
