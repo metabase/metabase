f8ec46f76f4347608a181b863cc747da829a4a2b
lbrdnk <lbrdnk@users.noreply.github.com>
2025-04-03 15:47:25 +0200

    Add var to std dev ags

--- test/metabase/query_processor/middleware/metrics_test.clj
+++ test/metabase/query_processor/middleware/metrics_test.clj
@@ -885,7 +885,6 @@
    [:max       (aggregate-col-1st-arg-fn lib/max)]
    [:min       (aggregate-col-1st-arg-fn lib/min)]
    [:sum       (aggregate-col-1st-arg-fn lib/sum)]
-   [:var       (aggregate-col-1st-arg-fn lib/var)]
    ;; special
    [:count-where
     (fn [query]
@@ -919,7 +918,10 @@
                                                      (lib/visible-columns query)))))]
    [:standard-deviation-aggregations
     :stddev
-    (aggregate-col-1st-arg-fn lib/stddev)]])
+    (aggregate-col-1st-arg-fn lib/stddev)]
+   [:standard-deviation-aggregations
+    :var
+    (aggregate-col-1st-arg-fn lib/var)]])
 
 (deftest filtered-metric-comparison-test
   (mt/test-drivers
