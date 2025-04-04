293f5b80aafb7b7799eda25edebb2fcadfe2e36b
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-04 14:24:14 -0400

    Simplify - don't localize FieldValuesWidget in this branch

--- frontend/src/metabase/components/FieldValuesWidget/FieldValuesWidget.tsx
+++ frontend/src/metabase/components/FieldValuesWidget/FieldValuesWidget.tsx
@@ -14,6 +14,7 @@ import type { LayoutRendererArgs } from "metabase/components/TokenField/TokenFie
 import ValueComponent from "metabase/components/Value";
 import CS from "metabase/css/core/index.css";
 import Fields from "metabase/entities/fields";
+import { useTranslateContent2 } from "metabase/i18n/components/ContentTranslationContext";
 import { parseNumber } from "metabase/lib/number";
 import { defer } from "metabase/lib/promise";
 import { connect, useDispatch } from "metabase/lib/redux";
@@ -55,7 +56,6 @@ import {
   shouldList,
   showRemapping,
 } from "./utils";
-import { useTranslateContent2 } from "metabase/i18n/components/ContentTranslationContext";
 
 const MAX_SEARCH_RESULTS = 100;
 
@@ -242,7 +242,6 @@ export const FieldValuesWidgetInner = forwardRef<
             entityId: field.getUniqueId(),
           }),
       );
-      console.log("@m925uhk0", "fieldValues", fieldValues);
 
       return dedupeValues(fieldValues);
     } else {
