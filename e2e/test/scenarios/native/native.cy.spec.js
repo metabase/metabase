3a506fc347a9ef05395f1ad645c657b002b72d23
Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>
2025-04-04 15:45:16 +0300

    test: wait for data in a flaky test (#56248)
    * test: wait for data in a flaky test
    
    * one more check
    
    * set smaller viewport
    
    * add wait
    
    * add a helper

--- e2e/test/scenarios/native/native.cy.spec.js
+++ e2e/test/scenarios/native/native.cy.spec.js
@@ -428,18 +428,28 @@ describe("scenarios > question > native", () => {
         },
       };
 
+      function setViewport(width, height) {
+        cy.viewport(width, height);
+        cy.wait(100); // wait for UI to re-render to avoid flakiness
+      }
+
       H.createNativeQuestion(questionDetails, { visitQuestion: true });
 
       cy.log("open editor on a normal screen size");
       cy.findByTestId("visibility-toggler").click();
 
-      dataReferenceSidebar().should("be.visible");
+      dataReferenceSidebar()
+        .should("be.visible")
+        // means data is loaded
+        .should("contain", "Sample Database");
 
       cy.findByTestId("visibility-toggler").click();
+      dataReferenceSidebar().should("not.be.visible");
 
       cy.log("open editor on a small screen size");
-      cy.viewport(1279, 800);
+      setViewport(1279, 800);
 
+      cy.log("try to open data reference sidebar on a mid size screen");
       cy.findByTestId("visibility-toggler").click();
       dataReferenceSidebar().should("not.be.visible");
 
@@ -450,7 +460,7 @@ describe("scenarios > question > native", () => {
       cy.findByTestId("native-query-editor-sidebar").icon("reference").click();
 
       cy.log("set small viewport");
-      cy.viewport(800, 800);
+      setViewport(800, 800);
 
       cy.findByTestId("sidebar-left").invoke("width").should("be.gt", 350);
       cy.findByTestId("sidebar-right").invoke("width").should("be.gt", 350);
