b9e3f2346fc3c1b67b4a6e786422db760ff216cf
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-04 14:16:49 -0400

    Simplify - don't localize BrowseDatabases in this branch

--- frontend/src/metabase/browse/databases/BrowseDatabases.tsx
+++ frontend/src/metabase/browse/databases/BrowseDatabases.tsx
@@ -2,10 +2,6 @@ import cx from "classnames";
 import { Link } from "react-router";
 import { t } from "ttag";
 
-import {
-  ContentTranslationProvider,
-  useTranslateContent2,
-} from "metabase/i18n/components/ContentTranslationContext";
 import NoResults from "assets/img/no_results.svg";
 import { useListDatabasesQuery } from "metabase/api";
 import { LoadingAndErrorWrapper } from "metabase/components/LoadingAndErrorWrapper";
@@ -37,14 +33,12 @@ import { BrowseDataHeader } from "../components/BrowseDataHeader";
 
 import DB from "./BrowseDatabases.module.css";
 import { trackAddDatabaseDBList } from "./analytics";
-import { useTranslateContent } from "metabase/i18n/components/ContentTranslationContext";
 
 export const BrowseDatabases = () => {
   const isAdmin = useSelector(getUserIsAdmin);
 
   const { data, isLoading, error } = useListDatabasesQuery();
   const databases = data?.data;
-  const tc = useTranslateContent2();
 
   if (error) {
     return <LoadingAndErrorWrapper error />;
@@ -68,64 +62,55 @@ export const BrowseDatabases = () => {
   }
 
   return (
-    <ContentTranslationProvider>
-      <BrowseContainer>
-        <BrowseDataHeader />
-        <BrowseMain>
-          <BrowseSection>
-            <SimpleGrid data-testid="database-browser" cols={3}>
-              {databases &&
-                databases.length > 0 &&
-                databases.map((database) => (
-                  <Link to={Urls.browseDatabase(database)} key={database.id}>
-                    <DatabaseCard
-                      bg="bg-white"
-                      className={cx(CS.rounded, CS.bordered, DB.dbCard)}
-                    >
-                      <Icon
-                        name="database"
-                        color={color("accent2")}
-                        size={32}
-                      />
-                      <Title order={2} size="md" lh={1.2} c="inherit">
-                        {tc(database.name)}
-                      </Title>
-                    </DatabaseCard>
-                  </Link>
-                ))}
-
-              {isAdmin && (
-                <Link
-                  to={newDatabase()}
-                  onClick={() => trackAddDatabaseDBList()}
-                >
+    <BrowseContainer>
+      <BrowseDataHeader />
+      <BrowseMain>
+        <BrowseSection>
+          <SimpleGrid data-testid="database-browser" cols={3}>
+            {databases &&
+              databases.length > 0 &&
+              databases.map((database) => (
+                <Link to={Urls.browseDatabase(database)} key={database.id}>
                   <DatabaseCard
-                    className={cx(CS.rounded, CS.bordered, DB.addCard)}
+                    bg="bg-white"
+                    className={cx(CS.rounded, CS.bordered, DB.dbCard)}
                   >
-                    <Group gap="xs">
-                      <CardImageWrapper database={"postgres"} />
-                      <CardImageWrapper database={"mysql"} />
-                      <CardImageWrapper database={"snowflake"} />
-                    </Group>
-                    <div>
-                      <Title order={2} size="md" lh={1.2} c="inherit">
-                        {t`Add a database`}
-                      </Title>
-                      <Text
-                        color="inherit"
-                        fz="sm"
-                        lh={1}
-                        span
-                      >{t`20+ data connectors. Start exploring in minutes.`}</Text>
-                    </div>
+                    <Icon name="database" color={color("accent2")} size={32} />
+                    <Title order={2} size="md" lh={1.2} c="inherit">
+                      {database.name}
+                    </Title>
                   </DatabaseCard>
                 </Link>
-              )}
-            </SimpleGrid>
-          </BrowseSection>
-        </BrowseMain>
-      </BrowseContainer>
-    </ContentTranslationProvider>
+              ))}
+
+            {isAdmin && (
+              <Link to={newDatabase()} onClick={() => trackAddDatabaseDBList()}>
+                <DatabaseCard
+                  className={cx(CS.rounded, CS.bordered, DB.addCard)}
+                >
+                  <Group gap="xs">
+                    <CardImageWrapper database={"postgres"} />
+                    <CardImageWrapper database={"mysql"} />
+                    <CardImageWrapper database={"snowflake"} />
+                  </Group>
+                  <div>
+                    <Title order={2} size="md" lh={1.2} c="inherit">
+                      {t`Add a database`}
+                    </Title>
+                    <Text
+                      color="inherit"
+                      fz="sm"
+                      lh={1}
+                      span
+                    >{t`20+ data connectors. Start exploring in minutes.`}</Text>
+                  </div>
+                </DatabaseCard>
+              </Link>
+            )}
+          </SimpleGrid>
+        </BrowseSection>
+      </BrowseMain>
+    </BrowseContainer>
   );
 };
 
