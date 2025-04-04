14e980bc9e7aa05ca82b20f4a9b614da96970b0f
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-04 14:41:08 -0400

    Simplify in this branch - don't localize HeaderCollectionBadge

--- frontend/src/metabase/query_builder/components/view/ViewHeader/components/HeaderCollectionBadge/HeaderCollectionBadge.tsx
+++ frontend/src/metabase/query_builder/components/view/ViewHeader/components/HeaderCollectionBadge/HeaderCollectionBadge.tsx
@@ -1,6 +1,5 @@
 import { t } from "ttag";
 
-import { useTranslateContent2 } from "metabase/i18n/components/ContentTranslationContext";
 import * as Urls from "metabase/lib/urls";
 import type Question from "metabase-lib/v1/Question";
 
@@ -9,10 +8,9 @@ import { HeadBreadcrumbs } from "../HeaderBreadcrumbs/HeaderBreadcrumbs";
 export function HeaderCollectionBadge({ question }: { question: Question }) {
   const { collection } = question.card();
   const icon = question.type();
-  const tc = useTranslateContent2();
   return (
     <HeadBreadcrumbs.Badge to={Urls.collection(collection)} icon={icon}>
-      {tc(collection?.name) || t`Our analytics`}
+      {collection?.name || t`Our analytics`}
     </HeadBreadcrumbs.Badge>
   );
 }
