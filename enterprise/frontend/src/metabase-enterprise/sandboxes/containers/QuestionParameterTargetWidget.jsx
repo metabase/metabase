1ca8358ff21b9f5c9240d7e27e008820adea6fad
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-04 14:14:07 -0400

    simplifying

--- enterprise/frontend/src/metabase-enterprise/sandboxes/containers/QuestionParameterTargetWidget.jsx
+++ enterprise/frontend/src/metabase-enterprise/sandboxes/containers/QuestionParameterTargetWidget.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable react/prop-types */
-import { Component } from "react";
 
 import { QuestionLoaderHOC } from "metabase/containers/QuestionLoader";
+import { useTranslateContent2 } from "metabase/i18n/components/ContentTranslationContext";
 import { getParameterMappingOptions } from "metabase/parameters/utils/mapping-options";
 
 import ParameterTargetWidget from "../components/ParameterTargetWidget";
-import { useTranslateContent2 } from "metabase/i18n/components/ContentTranslationContext";
 
 export const QuestionParameterTargetWidget = ({ question, ...props }) => {
   const tc = useTranslateContent2();
   const mappingOptions = question
     ? getParameterMappingOptions(question, null, question.card(), tc)
     : [];
-  console.log("@m9216654", "mappingOptions", mappingOptions);
 
   return (
     <ParameterTargetWidget
