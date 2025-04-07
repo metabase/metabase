44623d88846b5015422e2352cb8da48ec0f6dfbb
Edward Paget <ed.paget@gmail.com>
2025-04-07 16:32:10 -0500

    fixup! remove #p

--- src/metabase/query_processor/middleware/results_metadata.clj
+++ src/metabase/query_processor/middleware/results_metadata.clj
@@ -31,7 +31,7 @@
                ;; don't want to update metadata when we use a Card as a source Card.
                (not (:qp/source-card-id query))
                ;; Only update changed metadata
-               (not= #p metadata #p (qp.store/miscellaneous-value [::qp.card/card-stored-metadata])))
+               (not= metadata (qp.store/miscellaneous-value [::qp.card/card-stored-metadata])))
       (t2/update! :model/Card card-id {:result_metadata metadata
                                        :updated_at      :updated_at}))
     ;; if for some reason we weren't able to record results metadata for this query then just proceed as normal
