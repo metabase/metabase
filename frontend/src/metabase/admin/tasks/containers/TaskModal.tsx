e2cb347ab993de5c8135309d479c97cdfbdc76c0
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-07 21:11:13 +0700

    Add new buttons

--- frontend/src/metabase/admin/tasks/containers/TaskModal.tsx
+++ frontend/src/metabase/admin/tasks/containers/TaskModal.tsx
@@ -1,11 +1,13 @@
+import { useMemo } from "react";
 import { goBack } from "react-router-redux";
 import { t } from "ttag";
 
 import { useGetTaskQuery } from "metabase/api";
-import Code from "metabase/components/Code";
+import { CodeBlock } from "metabase/components/CodeBlock";
 import { LoadingAndErrorWrapper } from "metabase/components/LoadingAndErrorWrapper";
 import ModalContent from "metabase/components/ModalContent";
 import { useDispatch } from "metabase/lib/redux";
+import { Button, Flex, Icon } from "metabase/ui";
 
 type TaskModalProps = {
   params: { taskId: number };
@@ -13,19 +15,32 @@ type TaskModalProps = {
 
 export const TaskModal = ({ params }: TaskModalProps) => {
   const dispatch = useDispatch();
-  const { data, isLoading, error } = useGetTaskQuery(params.taskId);
+  const { data: task, isLoading, error } = useGetTaskQuery(params.taskId);
 
-  if (isLoading || error) {
+  const code = useMemo(() => {
+    return task ? JSON.stringify(task.task_details, null, 2) : "";
+  }, [task]);
+
+  const handleClose = () => {
+    dispatch(goBack());
+  };
+
+  if (isLoading || error || !task) {
     return <LoadingAndErrorWrapper loading={isLoading} error={error} />;
   }
 
-  if (!data) {
-    return null;
-  }
-
   return (
-    <ModalContent title={t`Task details`} onClose={() => dispatch(goBack())}>
-      <Code>{JSON.stringify(data.task_details)}</Code>
+    <ModalContent title={t`Task details`} onClose={handleClose}>
+      <CodeBlock language="json" code={code} />
+
+      <Flex gap="md" justify="space-between" mt="xl">
+        <Button leftSection={<Icon name="audit" />}>{t`See logs`}</Button>
+
+        <Flex gap="md">
+          <Button leftSection={<Icon name="download" />}>{t`Download`}</Button>
+          <Button leftSection={<Icon name="clipboard" />}>{t`Copy`}</Button>
+        </Flex>
+      </Flex>
     </ModalContent>
   );
 };
