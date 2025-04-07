ad09dfa3c8defa3db202bbcc8c851a92291f0254
Konstantin Darutkin <spalt08@mail.ru>
2025-04-07 13:19:22 +0100

    fix: empty viz state typecheck

--- frontend/src/metabase/visualizations/components/EmptyVizState/utils.ts
+++ frontend/src/metabase/visualizations/components/EmptyVizState/utils.ts
@@ -22,7 +22,10 @@ import type { CardDisplayType } from "metabase-types/api";
  * The "table" and the "object" (detail) charts can always display the data
  * using the raw table alone, so they don't need an empty state.
  */
-export type ExcludedEmptyVizDisplayTypes = "table" | "object";
+export type ExcludedEmptyVizDisplayTypes =
+  | "table"
+  | "object"
+  | "table-editable";
 type SupportedDisplayType = Exclude<
   CardDisplayType,
   ExcludedEmptyVizDisplayTypes
