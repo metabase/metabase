94b76960aca24df13151283b4e0c6cace650984d
Edward Paget <ed.paget@gmail.com>
2025-04-07 16:34:24 -0500

    fixup! remove cyclic dep

--- src/metabase/query_processor/middleware/results_metadata.clj
+++ src/metabase/query_processor/middleware/results_metadata.clj
@@ -7,7 +7,6 @@
    [metabase.driver :as driver]
    [metabase.driver.util :as driver.u]
    [metabase.lib.metadata :as lib.metadata]
-   [metabase.query-processor.card :as qp.card]
    [metabase.query-processor.reducible :as qp.reducible]
    [metabase.query-processor.schema :as qp.schema]
    [metabase.query-processor.store :as qp.store]
@@ -31,7 +30,7 @@
                ;; don't want to update metadata when we use a Card as a source Card.
                (not (:qp/source-card-id query))
                ;; Only update changed metadata
-               (not= metadata (qp.store/miscellaneous-value [::qp.card/card-stored-metadata])))
+               (not= metadata (qp.store/miscellaneous-value [:metadata.query-processor.card/card-stored-metadata])))
       (t2/update! :model/Card card-id {:result_metadata metadata
                                        :updated_at      :updated_at}))
     ;; if for some reason we weren't able to record results metadata for this query then just proceed as normal
