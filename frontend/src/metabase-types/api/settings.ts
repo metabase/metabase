f88f3c02b1c441d77cce14fc03ba959f64bbfb78
Riley Thompson <riley@metabase.com>
2025-04-08 16:31:57 -0400

    Remove partner drivers (#56403)

--- frontend/src/metabase-types/api/settings.ts
+++ frontend/src/metabase-types/api/settings.ts
@@ -67,7 +67,7 @@ export interface EngineFieldOption {
 }
 
 export interface EngineSource {
-  type: "official" | "community" | "partner";
+  type: "official" | "community";
   contact: EngineSourceContact | null;
 }
 
