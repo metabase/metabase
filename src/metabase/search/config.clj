56bd595f34475837b3267e002f70352614b32366
Nathan Voxland <nathan@metabase.com>
2025-04-09 15:01:38 -0500

    Added include_metadata argument to search API (#56397)

--- src/metabase/search/config.clj
+++ src/metabase/search/config.clj
@@ -256,7 +256,8 @@
    ;; true to search for verified items only, nil will return all items
    [:verified                            {:optional true} true?]
    [:ids                                 {:optional true} [:set {:min 1} ms/PositiveInt]]
-   [:include-dashboard-questions?        {:optional true} :boolean]])
+   [:include-dashboard-questions?        {:optional true} :boolean]
+   [:include-metadata?                   {:optional true} :boolean]])
 
 (defmulti column->string
   "Turn a complex column into a string"
