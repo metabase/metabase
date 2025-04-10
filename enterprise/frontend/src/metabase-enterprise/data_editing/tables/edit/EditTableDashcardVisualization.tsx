24ff3463ec2a91d463b27ffe9e04cd6d7a3e86a5
Konstantin Darutkin <spalt08@mail.ru>
2025-04-10 14:58:29 +0100

    feat(enterprise): show create row on editable dashcard

--- enterprise/frontend/src/metabase-enterprise/data_editing/tables/edit/EditTableDashcardVisualization.tsx
+++ enterprise/frontend/src/metabase-enterprise/data_editing/tables/edit/EditTableDashcardVisualization.tsx
@@ -1,5 +1,9 @@
 import cx from "classnames";
+import { t } from "ttag";
 
+import { Box, Button, Flex, Icon, Stack, Text } from "metabase/ui";
+import { HARD_ROW_LIMIT } from "metabase-lib/v1/queries/utils";
+import { formatRowCount } from "metabase-lib/v1/queries/utils/row-count";
 import type {
   ConcreteTableId,
   DatasetData,
@@ -54,14 +58,35 @@ export const EditTableDashcardVisualization = ({
   );
 
   return (
-    <div className={cx(S.tableRoot, className)}>
-      <EditTableDataGrid
-        data={data}
-        fieldMetadataMap={tableFieldMetadataMap}
-        onCellValueUpdate={handleCellValueUpdate}
-        onRowExpandClick={handleModalOpenAndExpandedRow}
-        columnsConfig={columnsConfig}
-      />
+    <Stack className={cx(S.container, className)} gap={0}>
+      <Box pos="relative" className={S.gridWrapper}>
+        <EditTableDataGrid
+          data={data}
+          fieldMetadataMap={tableFieldMetadataMap}
+          onCellValueUpdate={handleCellValueUpdate}
+          onRowExpandClick={handleModalOpenAndExpandedRow}
+          columnsConfig={columnsConfig}
+        />
+      </Box>
+
+      <Flex
+        p="xs"
+        pr="1rem"
+        justify="space-between"
+        align="center"
+        className={S.gridFooterDashcardVisualization}
+      >
+        <Button
+          variant="subtle"
+          size="xs"
+          fz="sm"
+          leftSection={<Icon name="add" />}
+          onClick={() => handleModalOpenAndExpandedRow()}
+        >{t`New record`}</Button>
+        <Text fz="sm" fw="bold">
+          {getEditTableRowCountMessage(data)}
+        </Text>
+      </Flex>
       <EditingBaseRowModal
         opened={isCreateRowModalOpen}
         onClose={closeCreateRowModal}
@@ -78,6 +103,18 @@ export const EditTableDashcardVisualization = ({
         fieldMetadataMap={tableFieldMetadataMap}
         isLoading={isInserting}
       />
-    </div>
+    </Stack>
   );
 };
+
+function getEditTableRowCountMessage(data: DatasetData): string {
+  const rowCount = data.rows.length;
+
+  if (data.rows_truncated > 0) {
+    return t`Showing first ${formatRowCount(rowCount)}`;
+  }
+  if (rowCount === HARD_ROW_LIMIT) {
+    return t`Showing first ${HARD_ROW_LIMIT} rows`;
+  }
+  return t`Showing ${formatRowCount(rowCount)}`;
+}
