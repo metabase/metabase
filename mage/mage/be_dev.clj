47f366c5aad1bb4b2e1bf95f2754bcebf4d1ae59
Bryan Maass <bryan.maass@gmail.com>
2025-04-10 11:38:33 -0600

    teach it how to behave when there's no repl

--- mage/mage/be_dev.clj
+++ mage/mage/be_dev.clj
@@ -2,27 +2,28 @@
   (:require
    [bencode.core :as bencode]
    [clojure.string :as str]
+   [mage.color :as c]
    [mage.util :as u]))
 
 (set! *warn-on-reflection* true)
 
-(defn safe-parse-int [s]
-  (try
-    (Integer/parseInt s)
-    (catch NumberFormatException e
-      (println "Error parsing long:" s)
-      nil)))
-
 (defn parse-int [s-or-i]
-  (if (instance? Integer s-or-i)
-    s-or-i
-    (safe-parse-int s-or-i)))
+  (if (string? s-or-i)
+    (let [s (str/trim s-or-i)]
+      (try
+        (Integer/parseInt s)
+        (catch Exception _ nil)))
+    s-or-i))
 
 (defn nrepl-port []
-  (or
-   (safe-parse-int (slurp ".nrepl-port"))
-   (safe-parse-int (System/getenv "MB_DEV_PORT"))
-   (throw (ex-info "No port found in .nrepl-port or MB_DEV_PORT" {}))))
+  (try
+    (parse-int (slurp ".nrepl-port"))
+    (catch java.io.FileNotFoundException _
+      (throw (ex-info (str "Metabase backend is not running. To start it, run:\n\n"
+                           (c/green "  clj -M:dev:ee:ee-dev:drivers:drivers-dev:dev-start\n\n")
+                           "If you prefer a different way to start the backend, please use that instead.\n"
+                           "The REPL server creates a .nrepl-port file when it starts.")
+                      {:cause :backend-not-running})))))
 
 (defn bootstrap-code
   "Capture output and return it as strings along with the value from the orignal code."
@@ -38,24 +39,38 @@
 
 (defn nrepl-eval
   ([code]
-   (u/debug [(nrepl-port) code])
-   (nrepl-eval (nrepl-port) code))
+   (try
+     (let [port (nrepl-port)]
+       (u/debug [port code])
+       (nrepl-eval port code))
+     (catch clojure.lang.ExceptionInfo e
+       (if (= :backend-not-running (:cause (ex-data e)))
+         (println (.getMessage e))
+         (throw e)))))
   ([port code]
-   (try (let [s (java.net.Socket. "localhost" (parse-int port))
-              out (.getOutputStream s)
-              in (java.io.PushbackInputStream. (.getInputStream s))
-              _ (bencode/write-bencode out {"op" "eval"
-                                            "code" (bootstrap-code code)})
-              return (update-vals (bencode/read-bencode in) slurp)]
-          #_:clj-kondo/ignore
-          ;; (prn ["Repl Response:" output])
-          (doseq [[k v] return]
-            (if (= k "value")
-              ;; try to read v, which is a map but comes back as a string:
-              (if-let [v (read-string v)]
-                (do
-                  (println "value: " (pr-str (:value v)))
-                  (println "stdout: " (str/trim (:stdout v)))
-                  (println "stderr: " (str/trim (:stderr v))))
-                (println "value: " v))
-              (println (str k ":") v)))))))
+   (try
+     (let [port (parse-int port)
+           s (java.net.Socket. "localhost" port)
+           out (.getOutputStream s)
+           in (java.io.PushbackInputStream. (.getInputStream s))
+           _ (bencode/write-bencode out {"op" "eval"
+                                         "code" (bootstrap-code code)})
+           return (update-vals (bencode/read-bencode in) slurp)]
+       #_:clj-kondo/ignore
+       ;; (prn ["Repl Response:" output])
+       (doseq [[k v] return]
+         (if (= k "value")
+           ;; try to read v, which is a map but comes back as a string:
+           (if-let [v (read-string v)]
+             (do
+               (println "value: " (pr-str (:value v)))
+               (println "stdout: " (str/trim (:stdout v)))
+               (println "stderr: " (str/trim (:stderr v))))
+             (println "value: " v))
+           (println (str k ":") v))))
+     (catch java.net.ConnectException _
+       (println (str "Could not connect to the REPL server on port: " (c/red port) " (found port number in .nrepl-port).\n"
+                     "Is the Metabase backend running?\n\n"
+                     "To start it, run:\n"
+                     (c/green "  clj -M:dev:ee:ee-dev:drivers:drivers-dev:dev-start\n\n")
+                     "If you prefer a different way to start the backend, please use that instead."))))))
