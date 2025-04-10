403ecfd728e39d9408f23ac0317ecafcc339b881
Nick Fitzpatrick <nickfitz.582@gmail.com>
2025-04-10 23:35:40 +0530

    adding unit tests

--- frontend/src/metabase/visualizations/components/Visualization/Visualization.unit.spec.js
+++ frontend/src/metabase/visualizations/components/Visualization/Visualization.unit.spec.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
 
-import { renderWithProviders, screen } from "__support__/ui";
+import { renderWithProviders, screen, within } from "__support__/ui";
 import { delay } from "__support__/utils";
 import { NumberColumn, StringColumn } from "__support__/visualizations";
 import { color } from "metabase/lib/colors";
+import { Icon } from "metabase/ui";
 import { registerVisualization } from "metabase/visualizations";
 import Visualization from "metabase/visualizations/components/Visualization";
 import registerVisualizations from "metabase/visualizations/register";
@@ -96,6 +97,31 @@ describe("Visualization", () => {
 
       expect(screen.getByTestId("scalar-value")).toHaveTextContent("1");
     });
+
+    // Can unskip once we decide what to do with this viz
+    // eslint-disable-next-line
+    it.skip("should support labelRightSection", async () => {
+      await renderViz(
+        [
+          {
+            card: createMockCard({
+              display: "scalar",
+              visualization_settings: createMockVisualizationSettings({
+                "card.title": "Foo Question",
+              }),
+            }),
+            data: { rows: [[1]], cols: [NumberColumn({ name: "Count" })] },
+          },
+        ],
+        {
+          labelRightSection: <Icon name="check" />,
+          isDashboard: true,
+          showTitle: true,
+        },
+      );
+      expect(screen.getByText("Foo Question")).toBeInTheDocument();
+      expect(screen.getByRole("img", { name: /check/ })).toBeInTheDocument();
+    });
   });
 
   describe("bar", () => {
@@ -118,6 +144,38 @@ describe("Visualization", () => {
         ]);
 
         expect(chartPathsWithColor(color("brand"))).toHaveLength(2);
+      });
+
+      it("should support leftLabelSection", async () => {
+        await renderViz(
+          [
+            {
+              card: createMockCard({
+                display: "bar",
+                visualization_settings: createMockVisualizationSettings({
+                  "card.title": "Foo Question",
+                }),
+              }),
+              data: {
+                cols: [
+                  StringColumn({ name: "Dimension" }),
+                  NumberColumn({ name: "Count" }),
+                ],
+                rows: [
+                  ["foo", 1],
+                  ["bar", 2],
+                ],
+              },
+            },
+          ],
+          {
+            labelRightSection: <Icon name="check" />,
+            isDashboard: true,
+            showTitle: true,
+          },
+        );
+        expect(screen.getByText("Foo Question")).toBeInTheDocument();
+        expect(screen.getByRole("img", { name: /check/ })).toBeInTheDocument();
       });
     });
 
@@ -205,6 +263,138 @@ describe("Visualization", () => {
         expect(chartPathsWithColor(color("brand"))).toHaveLength(2); // "count"
         expect(chartPathsWithColor(color("accent2"))).toHaveLength(2); // "Card2"
       });
+    });
+  });
+
+  describe("pie", () => {
+    it("should support leftLabelSection", async () => {
+      await renderViz(
+        [
+          {
+            card: createMockCard({
+              display: "pie",
+              visualization_settings: createMockVisualizationSettings({
+                "card.title": "Foo Question",
+              }),
+            }),
+            data: {
+              cols: [
+                StringColumn({ name: "Dimension" }),
+                NumberColumn({ name: "Count" }),
+              ],
+              rows: [
+                ["foo", 1],
+                ["bar", 2],
+              ],
+            },
+          },
+        ],
+        {
+          labelRightSection: <Icon name="check" />,
+          isDashboard: true,
+          showTitle: true,
+        },
+      );
+      expect(screen.getByText("Foo Question")).toBeInTheDocument();
+      expect(screen.getByRole("img", { name: /check/ })).toBeInTheDocument();
+    });
+  });
+
+  describe("row", () => {
+    it("should support leftLabelSection", async () => {
+      await renderViz(
+        [
+          {
+            card: createMockCard({
+              display: "row",
+              visualization_settings: createMockVisualizationSettings({
+                "card.title": "Foo Question",
+              }),
+            }),
+            data: {
+              cols: [
+                StringColumn({ name: "Dimension" }),
+                NumberColumn({ name: "Count" }),
+              ],
+              rows: [
+                ["foo", 1],
+                ["bar", 2],
+              ],
+            },
+          },
+        ],
+        {
+          labelRightSection: <Icon name="check" />,
+          isDashboard: true,
+          showTitle: true,
+        },
+      );
+      expect(screen.getByText("Foo Question")).toBeInTheDocument();
+      expect(screen.getByRole("img", { name: /check/ })).toBeInTheDocument();
+    });
+  });
+
+  describe("table", () => {
+    it("should support leftLabelSection", async () => {
+      await renderViz(
+        [
+          {
+            card: createMockCard({
+              display: "table",
+              visualization_settings: createMockVisualizationSettings({
+                "card.title": "Foo Question",
+              }),
+            }),
+            data: {
+              cols: [
+                StringColumn({ name: "Dimension" }),
+                NumberColumn({ name: "Count" }),
+              ],
+              rows: [
+                ["foo", 1],
+                ["bar", 2],
+              ],
+            },
+          },
+        ],
+        {
+          labelRightSection: <Icon name="check" />,
+          isDashboard: true,
+          showTitle: true,
+        },
+      );
+      expect(screen.getByText("Foo Question")).toBeInTheDocument();
+      expect(screen.getByRole("img", { name: /check/ })).toBeInTheDocument();
+    });
+  });
+
+  describe("progress", () => {
+    it("should support leftLabelSection", async () => {
+      await renderViz(
+        [
+          {
+            card: createMockCard({
+              display: "progress",
+              visualization_settings: createMockVisualizationSettings({
+                "card.title": "Foo Question",
+              }),
+            }),
+            data: { rows: [[1]], cols: [NumberColumn({ name: "Count" })] },
+          },
+        ],
+        {
+          labelRightSection: <Icon name="check" />,
+          isDashboard: true,
+          showTitle: true,
+        },
+      );
+
+      expect(screen.getByText("Foo Question")).toBeInTheDocument();
+      expect(
+        within(screen.getByTestId("legend-caption")).getByRole("img", {
+          name: /check/,
+        }),
+      ).toBeInTheDocument();
     });
   });
 });
