7d9b4dd49e81af42024a3776dc3bb2af34ead019
Bryan Maass <bryan.maass@gmail.com>
2025-04-10 11:10:53 -0600

    Add solid instructions for calling into the repl

--- mage/mage/be_dev.clj
+++ mage/mage/be_dev.clj
@@ -1,6 +1,7 @@
 (ns mage.be-dev
   (:require
    [bencode.core :as bencode]
+   [clojure.string :as str]
    [mage.util :as u]))
 
 (set! *warn-on-reflection* true)
@@ -23,6 +24,18 @@
    (safe-parse-int (System/getenv "MB_DEV_PORT"))
    (throw (ex-info "No port found in .nrepl-port or MB_DEV_PORT" {}))))
 
+(defn bootstrap-code
+  "Capture output and return it as strings along with the value from the orignal code."
+  [code-string]
+  (str "
+(let [o# (new java.io.StringWriter)
+      e# (new java.io.StringWriter)]
+  (binding [*out* o#
+            *err* e#]
+    {:value (do " code-string ")
+     :stdout (str o#)
+     :stderr (str e#)}))"))
+
 (defn nrepl-eval
   ([code]
    (u/debug [(nrepl-port) code])
@@ -31,10 +44,18 @@
    (try (let [s (java.net.Socket. "localhost" (parse-int port))
               out (.getOutputStream s)
               in (java.io.PushbackInputStream. (.getInputStream s))
-              _ (bencode/write-bencode out {"op" "eval" "code" (str "(do " code ")")})
-              output (update-vals (bencode/read-bencode in) slurp)]
+              _ (bencode/write-bencode out {"op" "eval"
+                                            "code" (bootstrap-code code)})
+              return (update-vals (bencode/read-bencode in) slurp)]
           #_:clj-kondo/ignore
-          (prn ["Repl Response:" output])
-          (when-let [v (get output "value")]
-            (prn (try (read-string v)
-                      (catch Exception _ v))))))))
+          ;; (prn ["Repl Response:" output])
+          (doseq [[k v] return]
+            (if (= k "value")
+              ;; try to read v, which is a map but comes back as a string:
+              (if-let [v (read-string v)]
+                (do
+                  (println "value: " (pr-str (:value v)))
+                  (println "stdout: " (str/trim (:stdout v)))
+                  (println "stderr: " (str/trim (:stderr v))))
+                (println "value: " v))
+              (println (str k ":") v)))))))
