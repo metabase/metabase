37930966b81e833004883f002dc0d23675aeb2a6
Sébastien <sebastien@metabase.com>
2025-04-11 17:11:28 +0200

    mostly removed add series related tests

--- e2e/test/scenarios/visualizations-charts/legend.cy.spec.js
+++ e2e/test/scenarios/visualizations-charts/legend.cy.spec.js
@@ -396,8 +396,7 @@ describe("scenarios > visualizations > legend", () => {
       cy.realPress("Escape");
     });
 
-    H.showDashboardCardActions(0);
-    H.getDashboardCard(0).findByLabelText("Add series").click();
+    H.showDashcardVisualizerModal(0);
 
     H.modal().within(() => {
       ensureCanNotToggleSeriesVisibility();
