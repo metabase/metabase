6c1ebb6d0eeff30cdcd1cab374608cbd6073fd0f
Dan Stone <wotbrew@gmail.com>
2025-04-11 14:55:22 +0100

    Run data editing tests against postgres as well as H2 (#56481)
    * Delete app db table record when dropping table
    
    * Remove empty-h2-app-db calls
    
    * Add auto-inc test for create table
    
    * Use driver test db instead of app db
    
    * Fix local whitespace
    
    * Do not lose the cleanup ex in open-test-table!
    
    * Fix postgres test issues

--- enterprise/backend/test/metabase_enterprise/data_editing/test_util.clj
+++ enterprise/backend/test/metabase_enterprise/data_editing/test_util.clj
@@ -14,29 +14,40 @@
 
 (set! *warn-on-reflection* true)
 
-(defn- create-test-table! [driver db table-name column-map create-table-opts]
-  (let [_     (driver/create-table! driver
-                                    (mt/id)
-                                    table-name
-                                    column-map
-                                    create-table-opts)
-        table (sync/create-table! db
-                                  {:name         table-name
-                                   :schema       nil
-                                   :display_name table-name})]
+(defn sync-new-table!
+  "Syncs a new table by name, returns the table id."
+  [db table-name]
+  (let [table (sync/create-table! db {:name table-name
+                                      ;; todo figure out how to determine default schema from driver
+                                      :schema (case (:engine db) :postgres "public" nil)
+                                      :display_name table-name})]
     (sync/sync-fields-for-table! db table)
     (:id table)))
+
+(defn- create-test-table! [db table-name column-map create-table-opts]
+  (let [_  (driver/create-table! (:engine db)
+                                 (:id db)
+                                 table-name
+                                 column-map
+                                 create-table-opts)]
+    (sync-new-table! db table-name)))
 
 (defn table-url
   "Returns the URL for data editing of the table with the given ID."
   [table-id]
   (format "ee/data-editing/table/%d" table-id))
 
+(defn alter-appdb-settings! [f & args]
+  (let [id           (mt/id)
+        settings     (t2/select-one-fn :settings :model/Database id)
+        new-settings (apply f settings args)]
+    (t2/update! :model/Database id {:settings new-settings})))
+
 (defn toggle-data-editing-enabled! [on-or-off]
-  (t2/update! :model/Database (mt/id) {:settings {:database-enable-table-editing (boolean on-or-off)}}))
+  (alter-appdb-settings! assoc :database-enable-table-editing (boolean on-or-off)))
 
 (defn open-test-table!
-  "Sets up an anonymous table in the appdb. Return a box that can be deref'd for the table-id.
+  "Sets up an anonymous table in the test db (mt/id). Return a box that can be deref'd for the table-id.
 
   Optionally accepts the column map and opts inputs to driver/create-table!.
   The symbol auto-inc-type can be used to denote the driver-specific auto-incrementing type for primary keys.
@@ -51,20 +62,22 @@
      :song  [:text]}
     {:primary-key [:id]}))
   (^Closeable [column-map create-table-opts]
-   (let [driver        :h2
+   (let [db            (t2/select-one :model/Database (mt/id))
+         driver        (:engine db)
          auto-inc-type (driver/upload-type->database-type driver :metabase.upload/auto-incrementing-int-pk)
          column-map    (walk/postwalk-replace {'auto-inc-type auto-inc-type} column-map)
-         db            (t2/select-one :model/Database (mt/id))
          table-name    (str "temp_table_" (str/replace (random-uuid) "-" "_"))
-         cleanup       #(try (driver/drop-table! driver (mt/id) table-name) (catch Exception _))]
+         cleanup       (fn []
+                         (driver/drop-table! driver (mt/id) table-name)
+                         (t2/delete! :model/Table :name table-name))]
      (try
-       (let [table-id (create-test-table! driver db table-name column-map create-table-opts)]
+       (let [table-id (create-test-table! db table-name column-map create-table-opts)]
          (reify Closeable
            IDeref
            (deref [_] table-id)
            (close [_] (cleanup))))
        (catch Exception e
-         (cleanup)
+         (try (cleanup) (catch Exception cleanup-ex (.addSuppressed e cleanup-ex)))
          (throw e))))))
 
 (defmacro with-temp-test-db!
@@ -73,5 +86,5 @@
   Use (mt/id), (mt/db) etc to get the database id and database object."
   [& body]
   `(actions.tu/with-actions-test-data
-     (t2/update! :model/Database (mt/id) {:settings {:database-enable-table-editing true}})
+     (toggle-data-editing-enabled! true)
      ~@body))
