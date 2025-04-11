b17057236c3ce19fc2493863939f91d7344c9f2e
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-04-11 21:42:26 +0700

    Add question details to Metabot

--- enterprise/frontend/src/embedding-sdk/components/public/MetabotQuestion/MetabotQuestion.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/MetabotQuestion/MetabotQuestion.tsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { t } from "ttag";
 
 import { InteractiveAdHocQuestion } from "embedding-sdk/components/private/InteractiveAdHocQuestion";
+import { InteractiveQuestionDefaultView } from "embedding-sdk/components/private/InteractiveQuestionDefaultView";
 import { withPublicComponentWrapper } from "embedding-sdk/components/private/PublicComponentWrapper";
-import { Flex, Text } from "metabase/ui";
+import { Flex, Stack, Text } from "metabase/ui";
 
 import { MetabotChatEmbedding } from "./MetabotChatEmbedding";
+import { QuestionDetails } from "./QuestionDetails";
+import { QuestionTitle } from "./QuestionTitle";
 
 const MetabotQuestionInner = () => {
   const [redirectUrl, setRedirectUrl] = useState<string>("");
@@ -19,7 +22,16 @@ const MetabotQuestionInner = () => {
           title={false}
           onNavigateBack={() => {}}
           isSaveEnabled={false}
-        />
+        >
+          <InteractiveQuestionDefaultView
+            title={
+              <Stack gap="sm" mb="1rem">
+                <QuestionTitle />
+                <QuestionDetails />
+              </Stack>
+            }
+          />
+        </InteractiveAdHocQuestion>
       )}
       <Disclaimer />
     </Flex>
