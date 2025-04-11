1284bcf7cf58c2e9117bc6b6056a5c79c21e2fcc
jeff bruemmer <jeff.bruemmer@gmail.com>
2025-04-11 18:23:47 -0400

    path and formatting

--- .typedoc/typedoc-plugin-head.js
+++ .typedoc/typedoc-plugin-head.js
@@ -3,7 +3,10 @@ import { Renderer } from "typedoc";
 export function load(app) {
   app.renderer.on(Renderer.EVENT_END_PAGE, (page) => {
     if (page.contents) {
-      page.contents = page.contents.replace(/<meta\s+name="description"[^>]*>/, '{% include embedded-analytics-sdk-metadata.html %}');
+      page.contents = page.contents.replace(
+        /<meta\s+name="description"[^>]*>/,
+        "{% include docs/embedded-analytics-sdk-metadata.html %}",
+      );
     }
   });
 }
