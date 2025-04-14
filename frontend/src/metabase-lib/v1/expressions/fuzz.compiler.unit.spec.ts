29b72e46dae6aa579789e209ee7bb4202a83ef31
Romeo Van Snick <romeo@romeovansnick.be>
2025-04-14 17:30:05 +0200

    Fix dissappearing string escapes (#56605)
    * Add repro for #56596
    
    * Allow escaping backslash
    
    * Use unquoteString everywhere to parse strings and references
    
    * Do string quoting and unquoting more consistently
    
    * Add repro for #56596
    
    * Make string parsing more straightforward and consistent
    
    * Use quoteString to find errors with quoting
    
    * Type string helpers more strictly
    
    * Add fuzz tests for string quoting
    
    * Move fuzz helper to test folder
    
    * Make string fuzzing repeatable
    
    * Add sanity check
    
    * Fuzz all three quoting modalities
    
    * Add [-quote tests
    
    * Rename quote to delimiter
    
    * Fix path for lexifier fuzz testing
    
    * Fix name for compiler fuzz testing
    
    * Use constant to store delimiter pairs
    
    * Fix option name in formatter
    
    * Add more cases around half-quoted strings
    
    * Rename quote to delimiter in comment
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>
    
    ---------
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- frontend/src/metabase-lib/v1/expressions/fuzz.compiler.unit.spec.ts
+++ frontend/src/metabase-lib/v1/expressions/fuzz.compiler.unit.spec.ts
@@ -1,8 +1,6 @@
-import process from "process";
-import _ from "underscore";
-
 import { query } from "./__support__/shared";
 import { compileExpression } from "./compiler";
+import { fuzz } from "./test/fuzz";
 import { generateExpression } from "./test/generator";
 import type { StartRule } from "./types";
 
@@ -16,7 +14,6 @@ jest.mock("metabase-lib", () => {
   };
 });
 
-const fuzz = process.env.MB_FUZZ ? describe : _.noop;
 const MAX_SEED = 10_000;
 
 function compile(expression: string, startRule: StartRule = "expression") {
@@ -39,7 +36,7 @@ describe("metabase-lib/v1/expressions/compiler", () => {
   });
 });
 
-fuzz("FUZZING metabase-lib/v1/expressions/recursive-parser", () => {
+fuzz("FUZZING metabase-lib/v1/expressions/compiler", () => {
   for (let seed = 1; seed < MAX_SEED; ++seed) {
     it("should parse generated number expression from seed " + seed, () => {
       const { expression } = generateExpression(seed, "number");
