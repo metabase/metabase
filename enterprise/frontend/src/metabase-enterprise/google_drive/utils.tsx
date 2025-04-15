0ba920f81853990afc5e9081b8ccd965aa714659
Ryan Laurie <iethree@gmail.com>
2025-04-15 10:19:39 -0600

    handle error states in all components

--- enterprise/frontend/src/metabase-enterprise/google_drive/utils.tsx
+++ enterprise/frontend/src/metabase-enterprise/google_drive/utils.tsx
@@ -1,8 +1,11 @@
+import { P, match } from "ts-pattern";
+
 import { skipToken } from "metabase/api";
 import { useHasTokenFeature, useSetting } from "metabase/common/hooks";
 import { useSelector } from "metabase/lib/redux";
 import { getUserIsAdmin } from "metabase/selectors/user";
 import { useGetServiceAccountQuery } from "metabase-enterprise/api";
+import type { GdrivePayload } from "metabase-types/api";
 
 export function useShowGdrive() {
   const gSheetsEnabled = useSetting("show-google-sheets-integration");
@@ -21,3 +24,16 @@ export function useShowGdrive() {
 
   return showGdrive;
 }
+
+export const getStatus = ({
+  status,
+  error,
+}: {
+  status: GdrivePayload["status"] | undefined;
+  error: unknown | undefined;
+}): GdrivePayload["status"] =>
+  match({ error: !!error, status })
+    .returnType<GdrivePayload["status"]>()
+    .with({ error: true }, () => "error")
+    .with({ status: P.string.minLength(1) }, ({ status }) => status)
+    .otherwise(() => "not-connected");
