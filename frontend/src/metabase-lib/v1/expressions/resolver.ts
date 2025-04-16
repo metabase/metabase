633a4565ff049d137c61f3fd14af5d3f28b467d7
Romeo Van Snick <romeo@romeovansnick.be>
2025-04-16 20:52:26 +0200

    Convert compiler to lib (#56426)
    * Convert pratt compiler to use ExpressionParts
    
    * Simplify compile by using compileNode helper
    
    * Add todo's about casts
    
    * Simplify error creation
    
    * Make assertions more consistent and translate error messages
    
    * Remove any check
    
    * Simplify compiler by moving stuff into infix/unary helpers
    
    * Remove unused assertions
    
    * Remove casts on constant operator names
    
    * Add assertions for operators
    
    * Wrap top-level literals in a value clause
    
    * Handle bigints in pratt compiler
    
    * Accept ExpressionParts in Lib.expressionClause
    
    * Remove null from ExpressionArgs
    
    * Add field and metric to expression operator
    
    * Simplify pratt compiler types
    
    * Add more tests for equivalent clauses
    
    * Return metadata objects directly from field-resolver
    
    * Add more operators to type
    
    * Create type for field type
    
    * Remove resolverPass
    
    * Convert resolver to Lib.ExpressionParts
    
    * Convert compiler to expressionParts
    
    * Fix syntax error
    
    * Fix types
    
    * Remove unused passes
    
    * Fix resolver tests
    
    * Add types to top-level literals
    
    * WIP: convert untagged expression-parts from #js
    
    * Manually unwrap field, segment and metric metadata
    
    * Unwrap options in pratt compiler
    
    * Remove empty options
    
    * Handle normalized expression when diagnosing
    
    * Allow customizing field resolver
    
    * Fix fuzz tests
    
    * Add value to ExpressionOperator
    
    * Remove unused TODO's
    
    * Use constants in test so types match
    
    * Wrap clauses in defineClause and extract list of defined clauses
    
    * Forbid setting name on clause config
    
    * Use defined clause name as ExpressionOperator
    
    * Use getClauseDefinition helper to get clause definitions
    
    * Ignore type in filters
    
    * Move expression-parts transformation to lib.js
    
    * Add type-of method implementations for field, metrics and segments
    
    * Trigger change when fast setting custom expression
    
    * Remove superflouous casts
    
    * Simplify resolver return type
    
    * Use reference for top-level fields
    
    * Be more specific about types in compiler
    
    * Convert operator to string
    
    * Add unit test for specific expressions
    
    * Fix literals icons
    
    * Remove unused matchers
    
    * Remove unused option from compiler
    
    * Negate number literals directly in compiler
    
    * Add expression-clause implementation that takes one argument
    
    * Use single-arg version of Lib.expressionParts
    
    * Remove TODO
    
    * Use helper to compile top level literal
    
    * Add effective-type to top-level literals
    
    * Remove unused MBQLCLauseMap type
    
    * Remove unused import
    
    * Add two-argument version of expressionClause
    
    * Wait for button to be visible
    
    * Add operator to invalid operator assertion
    
    * Remove incorrect operator check
    
    * Add more specific type to clause name
    
    * Always throw if function name is not found
    
    * Add overloads to getDefinedClause for known function names
    
    * Document the allowFastSet typing hack
    
    * Lift operator check
    
    * Convert nil to {} in body
    
    Co-authored-by: metamben <103100869+metamben@users.noreply.github.com>
    
    * Make expression-parts js conversion more clear
    
    * Pass through lib.metadata.calculation/type-of for the type-of implementations
    
    * Always throw assertion errors
    
    * Use helper to check query invariants
    
    * Remove lib.schema.expression/type-of methods
    
    * Add test for normalization of uppercase keyword arguments
    
    * Allow some normalizers to ignore case
    
    * Use threading for lowercase converter
    
    Co-authored-by: metamben <103100869+metamben@users.noreply.github.com>
    
    * Structure test better
    
    Co-authored-by: metamben <103100869+metamben@users.noreply.github.com>
    
    * Remove unused var
    
    ---------
    
    Co-authored-by: metamben <103100869+metamben@users.noreply.github.com>

--- frontend/src/metabase-lib/v1/expressions/resolver.ts
+++ frontend/src/metabase-lib/v1/expressions/resolver.ts
@@ -1,15 +1,9 @@
 import { t } from "ttag";
 
-import type { Expression, ExpressionOperand } from "metabase-types/api";
+import * as Lib from "metabase-lib";
 
-import { FIELD_MARKERS, MBQL_CLAUSES, getMBQLName } from "./config";
+import { getClauseDefinition, getMBQLName } from "./config";
 import { ResolverError } from "./errors";
-import {
-  isCallExpression,
-  isCaseOrIfOperator,
-  isOptionsObject,
-  isValue,
-} from "./matchers";
 import type { ExpressionType } from "./types";
 import { getNode } from "./utils";
 
@@ -21,31 +15,36 @@ const MAP_TYPE = {
 type ResolverFunction = (
   kind: "field" | "segment" | "metric",
   name: string,
-  expression?: Expression,
-) => Expression;
+  expression?: Lib.ExpressionParts,
+) => Lib.ColumnMetadata | Lib.SegmentMetadata | Lib.MetricMetadata;
 
-type Options<T> = {
-  expression: T;
-  type: ExpressionType;
-  fn: ResolverFunction;
-};
-
-export function resolve(options: Options<Expression>): Expression;
-export function resolve(options: Options<ExpressionOperand>): ExpressionOperand;
 export function resolve({
   expression,
   type = "expression",
   fn,
-}: Options<Expression | ExpressionOperand>): Expression | ExpressionOperand {
-  if (!isCallExpression(expression) || isValue(expression)) {
+}: {
+  expression:
+    | Lib.ExpressionParts
+    | Lib.ColumnMetadata
+    | Lib.MetricMetadata
+    | Lib.SegmentMetadata;
+  type: ExpressionType;
+  fn: ResolverFunction;
+}):
+  | Lib.ExpressionParts
+  | Lib.ColumnMetadata
+  | Lib.MetricMetadata
+  | Lib.SegmentMetadata {
+  if (!Lib.isExpressionParts(expression) || expression.operator === "value") {
     return expression;
   }
 
-  const [op, ...operands] = expression;
+  const { operator, options, args } = expression;
 
-  if (FIELD_MARKERS.has(op)) {
+  // @ts-expect-error: we use dimension internally
+  if (operator === "dimension") {
     const kind = MAP_TYPE[type as keyof typeof MAP_TYPE] ?? "dimension";
-    const [name] = operands;
+    const [name] = args;
     if (typeof name !== "string") {
       throw new ResolverError(t`Invalid field name`, getNode(expression));
     }
@@ -54,109 +53,43 @@ export function resolve({
     } catch (err) {
       // A second chance when field is not found:
       // maybe it is a function with zero argument (e.g. Count, CumulativeCount)
-      const func = getMBQLName(name);
-      if (func && MBQL_CLAUSES[func].args.length === 0) {
-        return [func];
+      const operator = getMBQLName(name);
+      const clause = operator && getClauseDefinition(operator);
+      if (clause && clause?.args.length === 0) {
+        return {
+          operator,
+          options: {},
+          args: [],
+        };
       }
       throw err;
     }
   }
 
-  const clause = MBQL_CLAUSES[op];
+  const clause = getClauseDefinition(operator);
   if (!clause) {
-    throw new ResolverError(t`Unknown function ${op}`, getNode(expression));
+    throw new ResolverError(
+      t`Unknown function ${operator}`,
+      getNode(expression),
+    );
   }
 
-  return [
-    op,
-    ...map(op, operands, expression, (operand, index, args) => {
-      if (
-        (index >= clause.args.length && !clause.multiple) ||
-        isOptionsObject(operand)
-      ) {
+  return {
+    operator,
+    options,
+    args: args.map((operand, index, args) => {
+      if (index >= clause.args.length && !clause.multiple) {
         // as-is, optional object for e.g. ends-with, time-interval, etc
         return operand;
       }
-
+      if (!Lib.isExpressionParts(operand)) {
+        return operand;
+      }
       return resolve({
         expression: operand,
         type: clause.argType?.(index, args, type) ?? clause.args[index],
         fn,
       });
     }),
-  ];
-}
-
-// Map over operands of case/if expressions, but marshal them first and unmarshal them after
-function map(
-  op: string,
-  operands: (Expression | ExpressionOperand)[],
-  expression: Expression,
-  fn: (
-    operand: Expression | ExpressionOperand,
-    index: number,
-    args: (Expression | ExpressionOperand)[],
-  ) => Expression | ExpressionOperand,
-): (Expression | ExpressionOperand)[] {
-  const marshalled = marshalOperands(op, operands);
-  const mapped = marshalled.map(fn);
-  return unmarshalOperands(op, mapped, expression);
-}
-
-// Flatten operands of case/if expressions so they can be easily mapped over
-function marshalOperands(
-  op: string,
-  operands: (Expression | ExpressionOperand)[],
-): (Expression | ExpressionOperand)[] {
-  if (!isCaseOrIfOperator(op)) {
-    return operands;
-  }
-
-  const pairs = operands[0] as [Expression, Expression][];
-  const options = operands[1];
-  const res = pairs.flat();
-
-  if (
-    isOptionsObject(options) &&
-    "default" in options &&
-    options.default !== undefined
-  ) {
-    res.push(options.default as Expression);
-  }
-  return res;
-}
-
-// Unflatten the operands of case/if expressions, as flattened by marshalOperands
-function unmarshalOperands(
-  op: string,
-  operands: (Expression | ExpressionOperand)[],
-  expression: Expression,
-): (Expression | ExpressionOperand)[] {
-  if (!isCaseOrIfOperator(op)) {
-    return operands;
-  }
-
-  const pairs: [Expression, Expression][] = [];
-
-  const pairCount = operands.length >> 1;
-  for (let i = 0; i < pairCount; ++i) {
-    const tst = operands[i * 2];
-    const val = operands[i * 2 + 1];
-    if (isOptionsObject(tst) || isOptionsObject(val)) {
-      throw new ResolverError(
-        t`Unsupported case/if options`,
-        getNode(expression),
-      );
-    }
-    pairs.push([tst, val]);
-  }
-  if (operands.length > 2 * pairCount) {
-    const lastOperand = operands[operands.length - 1];
-    const options = isOptionsObject(lastOperand)
-      ? lastOperand
-      : { default: lastOperand };
-    return [pairs, options] as (Expression | ExpressionOperand)[];
-  }
-
-  return [pairs] as (Expression | ExpressionOperand)[];
+  };
 }
