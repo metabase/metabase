161ae67606911b6498c4123caa0f657a1591af59
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-16 17:05:59 +0700

    Add url state to logs

--- frontend/src/metabase/admin/tasks/components/Logs/Logs.tsx
+++ frontend/src/metabase/admin/tasks/components/Logs/Logs.tsx
@@ -1,17 +1,26 @@
 import cx from "classnames";
+import type { Location } from "history";
 import * as React from "react";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import reactAnsiStyle from "react-ansi-style";
+import { withRouter } from "react-router";
 import { t } from "ttag";
 
+import { useUrlState } from "metabase/common/hooks/use-url-state";
 import Select, { Option } from "metabase/core/components/Select";
 import CS from "metabase/css/core/index.css";
 
 import { LogsContainer, LogsContent } from "./Logs.styled";
 import { usePollingLogsQuery, useTailLogs } from "./hooks";
-import { filterLogs, formatLog, getAllProcessUUIDs } from "./utils";
+import {
+  filterLogs,
+  formatLog,
+  getAllProcessUUIDs,
+  urlStateConfig,
+} from "./utils";
 
 interface LogsProps {
+  location: Location;
   // NOTE: fetching logs could come back from any machine if there's multiple machines backing a MB isntance
   // make this frequent enough that you will most likely get every log from every machine in some reasonable
   // amount of time
@@ -20,15 +29,19 @@ interface LogsProps {
 
 export const DEFAULT_POLLING_DURATION_MS = 1000;
 
-export const Logs = ({
+const LogsBase = ({
+  location,
   pollingDurationMs = DEFAULT_POLLING_DURATION_MS,
 }: LogsProps) => {
-  const [selectedProcessUUID, setSelectedProcessUUID] = useState("ALL");
+  const [{ process, query }, { patchUrlState }] = useUrlState(
+    location,
+    urlStateConfig,
+  );
   const { loaded, error, logs } = usePollingLogsQuery(pollingDurationMs);
   const processUUIDs = useMemo(() => getAllProcessUUIDs(logs), [logs]);
   const filteredLogs = useMemo(
-    () => filterLogs(logs, selectedProcessUUID),
-    [logs, selectedProcessUUID],
+    () => filterLogs(logs, { process, query }),
+    [logs, process, query],
   );
   const { scrollRef, onScroll, refollow } = useTailLogs(filteredLogs);
 
@@ -42,28 +55,30 @@ export const Logs = ({
 
   return (
     <LogsContainer loading={!loaded} error={error}>
-      {processUUIDs.length > 1 && (
-        <div className={CS.pb1}>
-          <label>{t`Select Metabase process:`}</label>
-          <Select
-            defaultValue="ALL"
-            value={selectedProcessUUID}
-            onChange={(e: { target: { value: string } }) => {
-              refollow();
-              setSelectedProcessUUID(e.target.value);
-            }}
-            className={cx(CS.inlineBlock, CS.ml1)}
-            width={400}
-          >
-            <Option value="ALL" key="ALL">{t`All Metabase processes`}</Option>
-            {processUUIDs.map((uuid) => (
-              <Option key={uuid} value={uuid}>
-                <code>{uuid}</code>
-              </Option>
-            ))}
-          </Select>
-        </div>
-      )}
+      <div className={CS.pb1}>
+        {processUUIDs.length > 1 && (
+          <>
+            <label>{t`Select Metabase process:`}</label>
+            <Select
+              defaultValue="ALL"
+              value={process}
+              onChange={(e: { target: { value: string } }) => {
+                refollow();
+                patchUrlState({ process: e.target.value });
+              }}
+              className={cx(CS.inlineBlock, CS.ml1)}
+              width={400}
+            >
+              <Option value="ALL" key="ALL">{t`All Metabase processes`}</Option>
+              {processUUIDs.map((uuid) => (
+                <Option key={uuid} value={uuid}>
+                  <code>{uuid}</code>
+                </Option>
+              ))}
+            </Select>
+          </>
+        )}
+      </div>
 
       <LogsContent id="logs-content" ref={scrollRef} onScroll={onScroll}>
         {displayLogs}
@@ -71,3 +86,5 @@ export const Logs = ({
     </LogsContainer>
   );
 };
+
+export const Logs = withRouter(LogsBase);
