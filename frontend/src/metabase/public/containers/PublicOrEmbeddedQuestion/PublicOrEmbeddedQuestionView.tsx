d19e34761d670cc98b45912ee3b20db8164d0fe2
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-17 12:48:56 -0400

    Content translation project integration branch

--- frontend/src/metabase/public/containers/PublicOrEmbeddedQuestion/PublicOrEmbeddedQuestionView.tsx
+++ frontend/src/metabase/public/containers/PublicOrEmbeddedQuestion/PublicOrEmbeddedQuestionView.tsx
@@ -4,6 +4,10 @@ import type { Dispatch, SetStateAction } from "react";
 
 import { LoadingAndErrorWrapper } from "metabase/components/LoadingAndErrorWrapper";
 import CS from "metabase/css/core/index.css";
+import {
+  maybeTranslateDisplayNames,
+  useTranslateContent,
+} from "metabase/i18n/hooks";
 import { EmbedFrame } from "metabase/public/components/EmbedFrame";
 import type { DisplayTheme } from "metabase/public/lib/types";
 import { PublicOrEmbeddedQuestionDownloadPopover } from "metabase/query_builder/components/QuestionDownloadPopover/QuestionDownloadPopover";
@@ -76,6 +80,9 @@ export function PublicOrEmbeddedQuestionView({
         floating={!titled}
       />
     ) : null;
+  const rawSeries = [{ card, data: result?.data }] as RawSeries;
+  const tc = useTranslateContent();
+  const maybeTranslatedRawSeries = maybeTranslateDisplayNames(rawSeries, tc);
 
   return (
     <EmbedFrame
@@ -107,7 +114,7 @@ export function PublicOrEmbeddedQuestionView({
           <Visualization
             isNightMode={theme === "night"}
             error={result?.error?.toString()}
-            rawSeries={[{ card, data: result?.data }] as RawSeries}
+            rawSeries={maybeTranslatedRawSeries}
             className={cx(CS.full, CS.flexFull, CS.z1)}
             onUpdateVisualizationSettings={(
               settings: VisualizationSettings,
