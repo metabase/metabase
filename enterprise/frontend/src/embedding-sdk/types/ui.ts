b997f06d49375cdf0b9f8fb6b3a7c8ac9b5055c5
Timofei <timofei@metabase.com>
2025-04-17 22:33:31 +0400

    Hide internal MetabaseQuestion from docs

--- enterprise/frontend/src/embedding-sdk/types/ui.ts
+++ enterprise/frontend/src/embedding-sdk/types/ui.ts
@@ -3,6 +3,7 @@ import type { IconName as InternalIconName } from "metabase/ui";
 /**
  * Inline wrapper to properly display the `IconName` type without referencing the `internal` type
  *
+ * @hidden
  * @inline
  */
 type _IconName = InternalIconName;
