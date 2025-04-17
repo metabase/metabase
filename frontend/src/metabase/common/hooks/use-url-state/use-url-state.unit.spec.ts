62072477d9bc61ea5604099ff20202fbe755304a
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-17 18:09:30 +0700

    Add unit tests for useUrlState

---
+++ frontend/src/metabase/common/hooks/use-url-state/use-url-state.unit.spec.ts
@@ -0,0 +1,71 @@
+import type { Location } from "history";
+
+import { renderHookWithProviders } from "__support__/ui";
+import { createMockLocation } from "metabase-types/store/mocks";
+
+import {
+  type QueryParam,
+  type UrlStateConfig,
+  useUrlState,
+} from "./use-url-state";
+
+type UrlState = {
+  name: string | null;
+  score: number | null;
+};
+
+interface SetupOpts {
+  location?: Location;
+}
+
+const setup = ({ location = createMockLocation() }: SetupOpts = {}) => {
+  const parseName = (param: QueryParam): UrlState["name"] => {
+    const value = Array.isArray(param) ? param[0] : param;
+    return value ?? null;
+  };
+
+  const parseScore = (param: QueryParam): UrlState["score"] => {
+    const value = Array.isArray(param) ? param[0] : param;
+    if (!value) {
+      return null;
+    }
+    const parsed = parseInt(value, 10);
+    return Number.isFinite(parsed) ? parsed : null;
+  };
+
+  const config: UrlStateConfig<UrlState> = {
+    parse: (query) => ({
+      name: parseName(query.name),
+      score: parseScore(query.score),
+    }),
+    serialize: ({ name, score }) => ({
+      name: name == null ? undefined : name,
+      score: score == null ? undefined : String(score),
+    }),
+  };
+
+  return renderHookWithProviders(() => useUrlState(location, config), {
+    initialRoute: `${location.pathname}${location.search}`,
+  });
+};
+
+describe("useUrlState", () => {
+  it("works with missing query params", () => {
+    const { result } = setup();
+    const [state] = result.current;
+    expect(state).toEqual({ name: null, score: null });
+  });
+
+  it("parses query params", () => {
+    const location = createLocation("?name=abc&score=123");
+    const { result } = setup({ location });
+    const [state] = result.current;
+    expect(state).toEqual({ name: "abc", score: 123 });
+  });
+});
+
+function createLocation(search: string) {
+  const params = new URLSearchParams(search);
+  const query = Object.fromEntries(params.entries());
+  return createMockLocation({ search, query });
+}
