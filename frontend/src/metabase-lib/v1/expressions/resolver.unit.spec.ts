ead2ace60fd1a8a64c80497a2e7680839bb9a909
Romeo Van Snick <romeo@romeovansnick.be>
2025-04-21 14:48:36 +0200

    Add tests for resolver

---
+++ frontend/src/metabase-lib/v1/expressions/resolver.unit.spec.ts
@@ -0,0 +1,350 @@
+import { resolver as makeResolver } from "./resolver";
+import {
+  expressions,
+  fields,
+  metrics,
+  query,
+  segments,
+  stageIndex,
+} from "./test/shared";
+
+describe("resolver", () => {
+  describe.each(["expression", "boolean"])("startRule = %s", (startRule) => {
+    const resolve = makeResolver({
+      query,
+      stageIndex,
+      startRule,
+    });
+
+    describe("type = boolean", () => {
+      const boolean = (name: string) => resolve("boolean", name);
+
+      it("should resolve segments", () => {
+        expect(boolean("Expensive Things")).toEqual(segments.EXPENSIVE_THINGS);
+      });
+
+      it("should resolve columns of type: boolean", () => {
+        expect(boolean("bool")).toEqual(expressions.BOOL);
+      });
+
+      it("should not resolve fields with non-boolean types", () => {
+        expect(() => boolean("stringly")).toThrow("Unknown Segment: stringly");
+        expect(() => boolean("Total")).toThrow("Unknown Segment: Total");
+      });
+
+      it("should not resolve metrics", () => {
+        expect(() => boolean("Foo Metric")).toThrow(
+          "Unknown Segment: Foo Metric",
+        );
+      });
+    });
+
+    describe("type = string", () => {
+      const string = (name: string) => resolve("string", name);
+
+      it("should resolve dimensions of type: string", () => {
+        expect(string("Product → Category")).toEqual(fields.products.CATEGORY);
+        expect(string("User → Address")).toEqual(fields.people.ADDRESS);
+        expect(string("User.Address")).toEqual(fields.people.ADDRESS);
+        expect(string("stringly")).toEqual(expressions.STRINGLY);
+      });
+
+      it("should resolve dimensions of types that can be stringly typed", () => {
+        expect(string("Total")).toEqual(fields.orders.TOTAL);
+        expect(string("Product → Price")).toEqual(fields.products.PRICE);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => string("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should not resolve metrics", () => {
+        expect(() => string("Foo Metric")).toThrow("Unknown Field: Foo Metric");
+      });
+    });
+
+    describe("type = number", () => {
+      const number = (name: string) => resolve("number", name);
+
+      it("should resolve dimensions of type: number", () => {
+        expect(number("ID")).toEqual(fields.orders.ID);
+        expect(number("Total")).toEqual(fields.orders.TOTAL);
+        expect(number("Product → Price")).toEqual(fields.products.PRICE);
+        expect(number("Product.Price")).toEqual(fields.products.PRICE);
+        expect(number("foo")).toEqual(expressions.FOO);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => number("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should not resolve metrics", () => {
+        expect(() => number("Foo Metric")).toThrow("Unknown Field: Foo Metric");
+      });
+    });
+
+    describe("type = datetime", () => {
+      const datetime = (name: string) => resolve("datetime", name);
+
+      it("should resolve dimensions of type: datetime", () => {
+        expect(datetime("Created At")).toEqual(fields.orders.CREATED_AT);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => datetime("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should not resolve metrics", () => {
+        expect(() => datetime("Foo Metric")).toThrow(
+          "Unknown Field: Foo Metric",
+        );
+      });
+    });
+
+    describe("type = any", () => {
+      const any_ = (name: string) => resolve("any", name);
+
+      it("should resolve dimensions of type: any", () => {
+        expect(any_("Created At")).toEqual(fields.orders.CREATED_AT);
+        expect(any_("ID")).toEqual(fields.orders.ID);
+        expect(any_("Product → Price")).toEqual(fields.products.PRICE);
+        expect(any_("bool")).toEqual(expressions.BOOL);
+      });
+
+      it("should not resolve segments", () => {
+        // TODO: should we resolve segments here?
+        expect(() => any_("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should not resolve metrics", () => {
+        expect(() => any_("Foo Metric")).toThrow("Unknown Field: Foo Metric");
+      });
+    });
+
+    describe("type = expression", () => {
+      const expression = (name: string) => resolve("expression", name);
+
+      it("should resolve dimensions of type: expression", () => {
+        expect(expression("Created At")).toEqual(fields.orders.CREATED_AT);
+        expect(expression("ID")).toEqual(fields.orders.ID);
+        expect(expression("Product → Price")).toEqual(fields.products.PRICE);
+        expect(expression("bool")).toEqual(expressions.BOOL);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => expression("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should not resolve metrics", () => {
+        expect(() => expression("Foo Metric")).toThrow(
+          "Unknown Field: Foo Metric",
+        );
+      });
+    });
+
+    describe("type = aggregation", () => {
+      const aggregation = (name: string) => resolve("aggregation", name);
+
+      it("should not resolve fields", () => {
+        expect(() => aggregation("Unknown")).toThrow("Unknown Metric: Unknown");
+        expect(() => aggregation("Created At")).toThrow(
+          "No aggregation found in: Created At. Use functions like Sum() or custom Metrics",
+        );
+        expect(() => aggregation("Product → Price")).toThrow(
+          "No aggregation found in: Product → Price. Use functions like Sum() or custom Metrics",
+        );
+        expect(() => aggregation("bool")).toThrow(
+          "No aggregation found in: bool. Use functions like Sum() or custom Metrics",
+        );
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => aggregation("Expensive Things")).toThrow(
+          "Unknown Metric: Expensive Things",
+        );
+      });
+
+      it("should resolve metrics", () => {
+        expect(aggregation("Foo Metric")).toEqual(metrics.FOO);
+      });
+    });
+  });
+
+  describe("startRule = aggregation", () => {
+    const resolve = makeResolver({
+      query,
+      stageIndex,
+      startRule: "aggregation",
+    });
+
+    describe("type = boolean", () => {
+      const boolean = (name: string) => resolve("boolean", name);
+
+      it("should resolve segments", () => {
+        expect(boolean("Expensive Things")).toEqual(segments.EXPENSIVE_THINGS);
+      });
+
+      it("should resolve columns of type: boolean", () => {
+        expect(boolean("bool")).toEqual(expressions.BOOL);
+      });
+
+      it("should not resolve fields with non-boolean types", () => {
+        expect(() => boolean("stringly")).toThrow("Unknown Segment: stringly");
+        expect(() => boolean("Total")).toThrow("Unknown Segment: Total");
+      });
+
+      it("should not resolve metrics", () => {
+        // TODO: should this resolve metrics?
+        expect(() => boolean("Foo Metric")).toThrow(
+          "Unknown Segment: Foo Metric",
+        );
+      });
+    });
+
+    describe("type = string", () => {
+      const string = (name: string) => resolve("string", name);
+
+      it("should resolve dimensions of type: string", () => {
+        expect(string("Product → Category")).toEqual(fields.products.CATEGORY);
+        expect(string("User → Address")).toEqual(fields.people.ADDRESS);
+        expect(string("User.Address")).toEqual(fields.people.ADDRESS);
+        expect(string("stringly")).toEqual(expressions.STRINGLY);
+      });
+
+      it("should resolve dimensions of types that can be stringly typed", () => {
+        expect(string("Total")).toEqual(fields.orders.TOTAL);
+        expect(string("Product → Price")).toEqual(fields.products.PRICE);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => string("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should resolve metrics", () => {
+        expect(string("Foo Metric")).toEqual(metrics.FOO);
+      });
+    });
+
+    describe("type = number", () => {
+      const number = (name: string) => resolve("number", name);
+
+      it("should resolve dimensions of type: number", () => {
+        expect(number("ID")).toEqual(fields.orders.ID);
+        expect(number("Total")).toEqual(fields.orders.TOTAL);
+        expect(number("Product → Price")).toEqual(fields.products.PRICE);
+        expect(number("Product.Price")).toEqual(fields.products.PRICE);
+        expect(number("foo")).toEqual(expressions.FOO);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => number("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should resolve metrics", () => {
+        expect(number("Foo Metric")).toEqual(metrics.FOO);
+      });
+    });
+
+    describe("type = datetime", () => {
+      const datetime = (name: string) => resolve("datetime", name);
+
+      it("should resolve dimensions of type: datetime", () => {
+        expect(datetime("Created At")).toEqual(fields.orders.CREATED_AT);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => datetime("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should resolve metrics", () => {
+        expect(datetime("Foo Metric")).toEqual(metrics.FOO);
+      });
+    });
+
+    describe("type = any", () => {
+      const any_ = (name: string) => resolve("any", name);
+
+      it("should resolve dimensions of type: any", () => {
+        expect(any_("Created At")).toEqual(fields.orders.CREATED_AT);
+        expect(any_("ID")).toEqual(fields.orders.ID);
+        expect(any_("Product → Price")).toEqual(fields.products.PRICE);
+        expect(any_("bool")).toEqual(expressions.BOOL);
+      });
+
+      it("should not resolve segments", () => {
+        // TODO: should we resolve segments here?
+        expect(() => any_("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should resolve metrics", () => {
+        expect(any_("Foo Metric")).toEqual(metrics.FOO);
+      });
+    });
+
+    describe("type = expression", () => {
+      const expression = (name: string) => resolve("expression", name);
+
+      it("should resolve dimensions of type: expression", () => {
+        expect(expression("Created At")).toEqual(fields.orders.CREATED_AT);
+        expect(expression("ID")).toEqual(fields.orders.ID);
+        expect(expression("Product → Price")).toEqual(fields.products.PRICE);
+        expect(expression("bool")).toEqual(expressions.BOOL);
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => expression("Expensive Things")).toThrow(
+          "Unknown Field: Expensive Things",
+        );
+      });
+
+      it("should resolve metrics", () => {
+        expect(expression("Foo Metric")).toEqual(metrics.FOO);
+      });
+    });
+
+    describe("type = aggregation", () => {
+      const aggregation = (name: string) => resolve("aggregation", name);
+
+      it("should not resolve fields", () => {
+        expect(() => aggregation("Unknown")).toThrow("Unknown Metric: Unknown");
+        expect(() => aggregation("Created At")).toThrow(
+          "No aggregation found in: Created At. Use functions like Sum() or custom Metrics",
+        );
+        expect(() => aggregation("Product → Price")).toThrow(
+          "No aggregation found in: Product → Price. Use functions like Sum() or custom Metrics",
+        );
+        expect(() => aggregation("bool")).toThrow(
+          "No aggregation found in: bool. Use functions like Sum() or custom Metrics",
+        );
+      });
+
+      it("should not resolve segments", () => {
+        expect(() => aggregation("Expensive Things")).toThrow(
+          "Unknown Metric: Expensive Things",
+        );
+      });
+
+      it("should resolve metrics", () => {
+        expect(aggregation("Foo Metric")).toEqual(metrics.FOO);
+      });
+    });
+  });
+});
