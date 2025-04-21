f309296861dcdad85624f5176eec25bbf00bfb99
Edward Paget <ed.paget@gmail.com>
2025-04-21 09:35:14 -0500

    fixup! remove when-class

--- test/metabase/util/jdbc_exceptions_test.clj
+++ test/metabase/util/jdbc_exceptions_test.clj
@@ -7,26 +7,13 @@
 
 (set! *warn-on-reflection* true)
 
-(defmacro ^:private when-class
-  ^{:clj-kondo/lint-as :when}
-  [k & body]
-  (let [resolved? (try (Class/forName (str k))
-                       true
-                       (catch ClassNotFoundException _ false))]
-    (when resolved?
-      `(do ~@body))))
-
 (deftest query-canceled?-test
   (testing "Looks throughout chain"
     (let [e (Exception. "bad" (Exception. "bad" (Exception. "bad" (java.sql.SQLTimeoutException. "timeout"))))]
       (is (sut/query-canceled? nil e))))
-  (when-class net.snowflake.client.jdbc.SnowflakeSQLException
-              (testing "recognizes snowflake exception"
-                (let [e (net.snowflake.client.jdbc.SnowflakeSQLException. "SQL execution canceled" "42S02")]
-                  (is (sut/query-canceled? :snowflake e)))
-                (let [e (net.snowflake.client.jdbc.SnowflakeSQLException. "SQL execution canceled" "42S02")]
-                  (is (sut/query-canceled? :snowflake e)))))
-  (when-class org.postgresql.util.PSQLException
-              (testing "recognizes postgres exception"
-                (let [e (org.postgresql.util.PSQLException. "ERROR: canceling statement due to user request" PSQLState/QUERY_CANCELED)]
-                  (is (sut/query-canceled? :postgres e))))))
+  (testing "recognizes snowflake exception"
+    (let [e (java.sql.SQLException. "SQL execution canceled" "42S02")]
+      (is (sut/query-canceled? :snowflake e))))
+  (testing "recognizes postgres exception"
+    (let [e (org.postgresql.util.PSQLException. "ERROR: canceling statement due to user request" PSQLState/QUERY_CANCELED)]
+      (is (sut/query-canceled? :postgres e)))))
