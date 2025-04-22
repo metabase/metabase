f33802fccefe7520005fb9bd867b20ceafbc2626
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-22 22:00:28 +0700

    SEM-111 Support sorting by duration, start and end time (#56791)
    * Use Mantine instead of styled components
    
    * Fix centering
    
    * Add task and status params
    
    * Add TaskStatusPicker
    
    * Make picker clearable
    
    * Add task picker
    
    * Put filters and pagination together
    
    * Translate task status
    
    * Break down logic
    
    * Move loading and error state
    
    * Always render something
    
    * Make task picker searchable
    
    * Add empty state to the table
    
    * Fix clear button being transparent
    
    * Add useUrlState
    
    * Use useUrlState in TasksApp
    
    * Move UrlState to its own file
    
    * Sort
    
    * Rename file
    
    * Rename
    
    * Clean invalid query params on mount
    
    * Prettier
    
    * Use Action
    
    * Ignore invalid pages
    
    * Reuse variable
    
    * Fix tests
    
    * Add test
    
    * Add label to clear button
    
    * Remove redundant condition
    
    * Add assertions
    
    * Improve typing
    
    * Be explicit about empty string handling
    
    * Add sorting params to ListTasksRequest
    
    * Add sorting params to url state
    
    * Render icon for the first column
    
    * Add SortableColumnHeader
    
    * Extract TasksTable
    
    * Move use-url-state to its own directory
    
    * Add unit tests for useUrlState
    
    * Add test
    
    * Refactor
    
    * Add more tests
    
    * Fix missing import
    
    * Add task endpoints mocks
    
    * Add TasksApp tests boilerplate
    
    * Add pagination test
    
    * Add tests for filters
    
    * Add more tests
    
    * Tmp
    
    * Make SortColumn generic
    
    * Use SortingOptions
    
    * Remove unused type
    
    * Make BaseItemsTableProps non-generic
    
    * Integrate sortingOptions
    
    * Prevent CLS
    
    * Prevent table content from disappearing on sort or filter
    
    * Improve typing
    
    * Use isLoading
    
    * Get fresh data
    
    * Update comment
    
    * Reset page on sorting change
    
    * Update tests
    
    * Add tests for page reset
    
    * Update tests
    
    * Mark test as flaky
    
    * Reuse SortingOptions
    
    * Extract getFirstParamValue

--- frontend/src/metabase-types/api/task.ts
+++ frontend/src/metabase-types/api/task.ts
@@ -1,5 +1,6 @@
 import type { DatabaseId } from "./database";
 import type { PaginationRequest, PaginationResponse } from "./pagination";
+import type { SortingOptions } from "./sorting";
 
 // "unknown" status is only expected for historical tasks (before Task['status'] was introduced)
 export type TaskStatus = "success" | "started" | "failed" | "unknown";
@@ -7,18 +8,21 @@ export type TaskStatus = "success" | "started" | "failed" | "unknown";
 export interface Task {
   id: number;
   db_id: DatabaseId | null;
-  duration: number;
+  duration: number | null;
   started_at: string;
-  ended_at: string;
+  ended_at: string | null;
   task: string;
   task_details: Record<string, unknown> | null;
   status: TaskStatus;
 }
 
+export type ListTasksSortColumn = "started_at" | "ended_at" | "duration";
+
 export type ListTasksRequest = {
   status?: TaskStatus;
   task?: string;
-} & PaginationRequest;
+} & PaginationRequest &
+  Partial<SortingOptions<ListTasksSortColumn>>;
 
 export type ListTasksResponse = {
   data: Task[];
