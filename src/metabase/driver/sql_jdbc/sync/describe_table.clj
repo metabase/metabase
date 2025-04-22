f2551f6712930304c0a525f05abf18844ac871cc
lbrdnk <lbrdnk@users.noreply.github.com>
2025-04-22 18:40:18 +0200

    Add schema, table and field to missing database type to base type mapping (#56843)
    * Adjust missing base type logging for jdbc drivers
    
    * Adjust base type logging for Athena
    
    * Add schema and table to missing base type logging
    
    * Comments
    
    * Update test
    
    * Drop only preceding nils

--- src/metabase/driver/sql_jdbc/sync/describe_table.clj
+++ src/metabase/driver/sql_jdbc/sync/describe_table.clj
@@ -54,12 +54,15 @@
   (with-open [rs (.getCatalogs metadata)]
     (set (map :table_cat (jdbc/metadata-result rs)))))
 
-(defn- database-type->base-type-or-warn
+(defn database-type->base-type-or-warn
   "Given a `database-type` (e.g. `VARCHAR`) return the mapped Metabase type (e.g. `:type/Text`)."
-  [driver database-type]
+  [driver namespaced-col database-type]
   (or (sql-jdbc.sync.interface/database-type->base-type driver (keyword database-type))
-      (do (log/warnf "Don't know how to map column type '%s' to a Field base_type, falling back to :type/*."
-                     database-type)
+      (do (let [pretty-column (str/join "." (map #(str "'" % "'")
+                                                 (drop-while nil? namespaced-col)))]
+            (log/warnf "Don't know how to map column type '%s' to a Field base_type for %s, falling back to :type/*."
+                       database-type
+                       pretty-column))
           :type/*)))
 
 (defn- calculated-semantic-type
@@ -170,11 +173,22 @@
          init
          [jdbc-metadata fallback-metadata])))))
 
+(def ^:private ^:dynamic *table-info*
+  "To be bound in [[describe-table-fields]] to convey table schema and name into [[describe-fields-xf]]. Reason
+  being, that describe-fields-xf is used in driver/describe-fields and driver/describe-table-fields, where transducer's
+  `col` arg is missing this information for the latter."
+  {})
+
 (defn describe-fields-xf
   "Returns a transducer for computing metadata about the fields in `db`."
   [driver db]
   (map (fn [col]
-         (let [base-type (or (:base-type col) (database-type->base-type-or-warn driver (:database-type col)))
+         (let [base-type (or (:base-type col) (database-type->base-type-or-warn
+                                               driver
+                                               [((some-fn :table-schema) col *table-info*)
+                                                ((some-fn :table-name)   col *table-info*)
+                                                (:name col)]
+                                               (:database-type col)))
                semantic-type (calculated-semantic-type driver (:name col) (:database-type col))
                json? (isa? base-type :type/JSON)
                database-position (some-> (:database-position col) int)]
@@ -213,10 +227,13 @@
 
 (defmethod describe-table-fields :sql-jdbc
   [driver conn table db-name-or-nil]
-  (into
-   #{}
-   (describe-table-fields-xf driver (table/database table))
-   (fields-metadata driver conn table db-name-or-nil)))
+  (binding [*table-info* (merge {:table-name (:name table)}
+                                (when (:schema table)
+                                  {:table-schema (:schema table)}))]
+    (into
+     #{}
+     (describe-table-fields-xf driver (table/database table))
+     (fields-metadata driver conn table db-name-or-nil))))
 
 ;;; TODO -- it seems like in practice we usually call this without passing in a DB name, so `db-name-or-nil` is almost
 ;;; always just `nil`. There's currently not a great driver-agnostic way to determine the actual physical Database name
