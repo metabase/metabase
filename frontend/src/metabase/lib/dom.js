4e893156f296f2431f60b6f1f8756ac4c2a415ad
mmmsssttt404 <931121963@qq.com>
2025-04-22 16:43:46 +0800

    Update dom.js

--- frontend/src/metabase/lib/dom.js
+++ frontend/src/metabase/lib/dom.js
@@ -451,7 +451,7 @@ export function removeAllChildren(element) {
 
 export function parseDataUri(url) {
   const match =
-    url && url.match(/^data:(?:([^;]+)(?:;([^;]+))?)?(;base64)?,(.*)$/);
+    url && url.match(/^data:(?:([^;]+)(?:;([^;]+))?)?(;base64)?,((?:(?!\1|,).)*)$/);
   if (match) {
     let [, mimeType, charset, base64, data] = match;
     if (charset === "base64" && !base64) {
