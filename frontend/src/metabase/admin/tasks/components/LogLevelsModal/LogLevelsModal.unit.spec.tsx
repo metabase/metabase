4ee9fafcaebc492372f26ada1958970fc68969e4
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-23 19:57:38 +0700

    Add tests

--- frontend/src/metabase/admin/tasks/components/LogLevelsModal/LogLevelsModal.unit.spec.tsx
+++ frontend/src/metabase/admin/tasks/components/LogLevelsModal/LogLevelsModal.unit.spec.tsx
@@ -1,4 +1,7 @@
+import userEvent from "@testing-library/user-event";
+import fetchMock from "fetch-mock";
 import { Route } from "react-router";
+import { dedent } from "ts-dedent";
 
 import {
   setupDeleteLoggerAdjustmentEndpoint,
@@ -9,8 +12,10 @@ import {
   renderWithProviders,
   screen,
   waitForLoaderToBeRemoved,
+  within,
 } from "__support__/ui";
 import { ModalRoute } from "metabase/hoc/ModalRoute";
+import { checkNotNull } from "metabase/lib/types";
 import type { LoggerPreset } from "metabase-types/api";
 import { createMockLoggerPreset } from "metabase-types/api/mocks/logger";
 
@@ -27,7 +32,7 @@ const PRESET_A = createMockLoggerPreset({
 const PRESET_B = createMockLoggerPreset({
   display_name: "Preset H2",
   loggers: [
-    { name: "metabase.driver", level: "debug" },
+    { name: "metabase.driver", level: "error" },
     { name: "metabase.driver.h2", level: "info" },
   ],
 });
@@ -75,5 +80,102 @@ describe("LogLevelsModal", () => {
     expect(screen.getByTestId("loading-indicator")).toBeInTheDocument();
     await waitForLoaderToBeRemoved();
     expect(screen.getByText("An error occurred")).toBeInTheDocument();
+  });
+
+  it("auto-applies the first preset on load", async () => {
+    setup();
+    await waitForLoaderToBeRemoved();
+
+    const [_durationUnitPicker, codeMirror] = screen.getAllByRole("textbox");
+    expect(codeMirror).toHaveValue(dedent`{
+      "metabase.driver": "debug",
+      "metabase.driver.mysql": "info"
+    }`);
+  });
+
+  it("allows to load preset", async () => {
+    setup();
+    await waitForLoaderToBeRemoved();
+
+    await userEvent.click(screen.getByText("Load preset"));
+    const popover = screen.getByRole("menu");
+    await userEvent.click(within(popover).getByText(PRESET_B.display_name));
+
+    const [_durationUnitPicker, codeMirror] = screen.getAllByRole("textbox");
+    expect(codeMirror).toHaveValue(dedent`{
+      "metabase.driver": "error",
+      "metabase.driver.h2": "info"
+    }`);
+  });
+
+  it("allows to reset changes to defaults", async () => {
+    setup();
+    await waitForLoaderToBeRemoved();
+    expect(screen.getByRole("dialog")).toBeInTheDocument();
+
+    await userEvent.click(screen.getByText("Reset to defaults"));
+
+    expect(
+      fetchMock.calls("path:/api/logger/adjustment", { method: "DELETE" }),
+    ).toHaveLength(1);
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+
+  it("allows to save changes", async () => {
+    setup();
+    await waitForLoaderToBeRemoved();
+    expect(screen.getByRole("dialog")).toBeInTheDocument();
+
+    await userEvent.clear(screen.getByPlaceholderText("Duration"));
+    await userEvent.type(screen.getByPlaceholderText("Duration"), "24");
+    await userEvent.click(screen.getByPlaceholderText("Unit"));
+    await userEvent.click(screen.getByText("Hours"));
+    await userEvent.click(screen.getByText("Load preset"));
+    const popover = screen.getByRole("menu");
+    await userEvent.click(within(popover).getByText(PRESET_B.display_name));
+    await userEvent.click(screen.getByText("Save"));
+
+    const calls = fetchMock.calls("path:/api/logger/adjustment", {
+      method: "POST",
+    });
+
+    expect(calls).toHaveLength(1);
+    const [_url, options] = calls[0];
+    const body = await checkNotNull(options).body;
+
+    if (typeof body !== "string") {
+      throw new Error("body should be a string");
+    }
+
+    expect(JSON.parse(body)).toEqual({
+      duration: 24,
+      duration_unit: "hours",
+      log_levels: {
+        "metabase.driver": "error",
+        "metabase.driver.h2": "info",
+      },
+    });
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+
+  it("should disable save button when there is no duration", async () => {
+    setup();
+    await waitForLoaderToBeRemoved();
+    expect(screen.getByRole("button", { name: "Save" })).toBeEnabled();
+
+    await userEvent.clear(screen.getByPlaceholderText("Duration"));
+
+    expect(screen.getByRole("button", { name: "Save" })).toBeDisabled();
+  });
+
+  it("should disable save button when json is invalid", async () => {
+    setup();
+    await waitForLoaderToBeRemoved();
+    expect(screen.getByRole("button", { name: "Save" })).toBeEnabled();
+
+    const [_durationUnitPicker, codeMirror] = screen.getAllByRole("textbox");
+    await userEvent.type(codeMirror, "gibberish");
+
+    expect(screen.getByRole("button", { name: "Save" })).toBeDisabled();
   });
 });
--- frontend/test/__support__/server-mocks/logger.ts
+++ frontend/test/__support__/server-mocks/logger.ts
@@ -11,19 +11,19 @@ export function setupLoggerPresetsEndpoint(
   response: LoggerPreset[] | MockResponse,
   options?: MockOptionsMethodGet,
 ) {
-  fetchMock.get(`path:/api/logger/presets`, response, options);
+  fetchMock.get("path:/api/logger/presets", response, options);
 }
 
 export function setupPostLoggerAdjustmentEndpoint(
   response: MockResponse = { status: 200 },
   options?: MockOptionsMethodPost,
 ) {
-  fetchMock.post(`path:/api/logger/adjustment`, response, options);
+  fetchMock.post("path:/api/logger/adjustment", response, options);
 }
 
 export function setupDeleteLoggerAdjustmentEndpoint(
   response: MockResponse = { status: 200 },
   options?: MockOptionsMethodDelete,
 ) {
-  fetchMock.delete(`path:/api/logger/adjustment`, response, options);
+  fetchMock.delete("path:/api/logger/adjustment", response, options);
 }
