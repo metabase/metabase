a12f58110506bde2402bb741d9ef95da30cb0954
Tony-metabase <110378427+Tony-metabase@users.noreply.github.com>
2025-04-23 18:26:25 +0200

    Update alert.cy.spec.js
    Replaced "You're only allowed to email alerts to addresses ending in {0}"
    With "Only allowed domains are supported"

--- e2e/test/scenarios/sharing/alert/alert.cy.spec.js
+++ e2e/test/scenarios/sharing/alert/alert.cy.spec.js
@@ -146,8 +146,8 @@ describe("scenarios > alert", () => {
       const allowedDomain = "metabase.test";
       const deniedDomain = "metabase.example";
       const deniedEmail = `mailer@${deniedDomain}`;
-      const subscriptionError = `You're only allowed to email subscriptions to addresses ending in ${allowedDomain}`;
-      const alertError = `You're only allowed to email alerts to addresses ending in ${allowedDomain}`;
+      const subscriptionError = `Only allowed domains are supported`;
+      const alertError = `Only allowed domains are supported`;
 
       function addEmailRecipient(email) {
         cy.findByRole("textbox").click().type(`${email}`).blur();
