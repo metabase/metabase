b6b4ea3215c138cc9767b2bca18b2980d3c4a4e3
Sloan Sparger <sloansparger@gmail.com>
2025-04-24 14:36:20 -0500

    copy feedback

--- enterprise/frontend/src/metabase-enterprise/database_routing/DatabaseRoutingSection/DatabaseRoutingSection.tsx
+++ enterprise/frontend/src/metabase-enterprise/database_routing/DatabaseRoutingSection/DatabaseRoutingSection.tsx
@@ -104,7 +104,7 @@ export const DatabaseRoutingSection = ({
     <DatabaseInfoSection
       name={t`Database routing`}
       // eslint-disable-next-line no-literal-metabase-strings -- This string only shows for admins.
-      description={t`When someone views a question using data from this database, Metabase will send the queries to the destination database set by the person's user attribute. Each destination database must have identical schema.`}
+      description={t`When someone views a question using data from this database, Metabase will send the queries to the destination database set by the person's user attribute. Each destination database must have identical schemas.`}
       data-testid="database-routing-section"
     >
       <Flex justify="space-between" align="center">
