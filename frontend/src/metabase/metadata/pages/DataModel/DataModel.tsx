3def1160eefbfcdc2769dc16fd800783fe016bfc
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-24 18:53:49 +0700

    Use DataModelApp in DataModel

--- frontend/src/metabase/metadata/pages/DataModel/DataModel.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/DataModel.tsx
@@ -1,3 +1,14 @@
-import { Box } from "metabase/ui";
+import type { Location } from "history";
+import { withRouter } from "react-router";
 
-export const DataModel = () => <Box>Well begun is half done</Box>;
+import { DataModelApp } from "metabase/admin/datamodel/containers/DataModelApp";
+
+interface Props {
+  location: Location;
+}
+
+const DataModelBase = ({ location }: Props) => (
+  <DataModelApp location={location}>Well begun is half done</DataModelApp>
+);
+
+export const DataModel = withRouter(DataModelBase);
