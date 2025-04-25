84dd56136019464bdb98163147d7fc81eb5b2dda
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-25 21:36:30 +0700

    Get rid of entity framework workarounds

--- frontend/src/metabase/metadata/pages/DataModel/components/FieldOrder/FieldOrder.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/components/FieldOrder/FieldOrder.tsx
@@ -1,6 +1,5 @@
 import { PointerSensor, useSensor } from "@dnd-kit/core";
-import { useMemo, useState } from "react";
-import { useDeepCompareEffect } from "react-use";
+import { useMemo } from "react";
 
 import {
   type DragEndEvent,
@@ -21,28 +20,14 @@ export const FieldOrder = ({ table, onChange }: Props) => {
   const pointerSensor = useSensor(PointerSensor, {
     activationConstraint: { distance: 15 },
   });
-  const initialItems = useMemo(() => getItems(table?.fields), [table?.fields]);
-  const initialOrder = useMemo(
-    () => getItemsOrder(initialItems),
-    [initialItems],
-  );
-  const [items, setItems] = useState(initialItems);
-  const [order, setOrder] = useState(initialOrder);
+  const items = useMemo(() => getItems(table?.fields), [table?.fields]);
+  const order = useMemo(() => getItemsOrder(items), [items]);
   const sortedItems = useMemo(() => sortItems(items, order), [items, order]);
   const isDragDisabled = sortedItems.length <= 1;
 
   const handleSortEnd = ({ itemIds }: DragEndEvent) => {
-    setOrder(itemIds);
     onChange(itemIds);
   };
-
-  useDeepCompareEffect(() => {
-    setOrder(initialOrder);
-  }, [initialOrder]);
-
-  useDeepCompareEffect(() => {
-    setItems(initialItems);
-  }, [initialItems]);
 
   return (
     <Flex direction="column" gap="sm">
