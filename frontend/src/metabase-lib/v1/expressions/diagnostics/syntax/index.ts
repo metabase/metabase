bf2d148954c794f96145b72abd400b85e8196525
Romeo Van Snick <romeo@romeovansnick.be>
2025-04-25 09:50:57 +0200

    Fix expression diagnostic for mismatched siblings (#57170)
    * Check all mismatched sibling tokens, not just missing comma's
    
    * Rename siblings check to checkMismatchedSiblings
    
    * Merge imports
    
    * Also catch call expressions

--- frontend/src/metabase-lib/v1/expressions/diagnostics/syntax/index.ts
+++ frontend/src/metabase-lib/v1/expressions/diagnostics/syntax/index.ts
@@ -3,7 +3,7 @@ import type { Token } from "../../pratt";
 import { checkBadTokens } from "./check-bad-tokens";
 import { checkFieldQuotes } from "./check-field-quotes";
 import { checkMatchingParentheses } from "./check-matching-parenthesis";
-import { checkMissingCommasInArgumentList } from "./check-missing-comma-in-argument-list";
+import { checkMismatchedSiblings } from "./check-mismatched-siblings";
 import { checkNumberExponent } from "./check-number-exponent";
 import { checkOpenParenthesisAfterFunction } from "./check-open-parenthesis-after-function";
 import { checkStringQuotes } from "./check-string-quotes";
@@ -11,10 +11,10 @@ import { checkStringQuotes } from "./check-string-quotes";
 export const syntaxChecks = [
   checkOpenParenthesisAfterFunction,
   checkMatchingParentheses,
-  checkMissingCommasInArgumentList,
   checkNumberExponent,
   checkStringQuotes,
   checkFieldQuotes,
+  checkMismatchedSiblings,
   checkBadTokens,
 ];
 
