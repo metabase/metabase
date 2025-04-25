0220b98b51a580a51fa62d5bf8c0dac5151db721
Edward Paget <edpaget@users.noreply.github.com>
2025-04-25 13:09:48 -0500

    feat: upgrade ring-jetty to 1.14.1 (#57139)
    * feat: upgrade ring-jetty to 1.14.1 (#55921)
    
    * feat: upgrade ring-jetty to 1.14.1
    
    Moves us onto Jetty 12 which has some features (pthreading, QoSHanlder)
    that we may want to take advantage of.
    
    * fixup! customize jetty metrics
    
    * fixup! license info for jetty ee9
    
    * fixup! make a var private
    
    * fixup! don't use deprecated method
    
    * fixup! create custom statistics handler
    
    Replicate the Jetty 11 statistics handler that is able to report on
    asynchronously handled requests separately from synchronously handled
    ones. Also ports it to directly report statistics to our prometheus
    rather than needing to build a connector between the stats handler
    object and our metrics reporting
    
    Co-Authored-By: John Swanson <john.swanson@metabase.com>
    
    * fixup! fix race condition in stats test
    
    * fixup! missing private annotation
    
    * fixup! use normal jetty release with logging fix
    
    ---------
    
    Co-authored-by: John Swanson <john.swanson@metabase.com>
    
    * fix: request max time stat reports seconds (#57121)
    
    Forgot to divide millis by 1000 before saving so this stat was reporting
    some silly numbers on stats. Also adds tests around this metric
    
    * fix: test flake in jetty stats (#57141)
    
    ---------
    
    Co-authored-by: John Swanson <john.swanson@metabase.com>

--- src/metabase/analytics/prometheus.clj
+++ src/metabase/analytics/prometheus.clj
@@ -16,7 +16,6 @@
    [jvm-hiccup-meter.core :as hiccup-meter]
    [metabase.analytics.settings :refer [prometheus-server-port]]
    [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
-   [metabase.server.core :as server]
    [metabase.util :as u]
    [metabase.util.i18n :refer [trs]]
    [metabase.util.log :as log]
@@ -30,7 +29,6 @@
     MemoryPoolsExports
     StandardExports
     ThreadExports)
-   (io.prometheus.client.jetty JettyStatisticsCollector)
    (java.util ArrayList List)
    (javax.management ObjectName)
    (org.eclipse.jetty.server Server)))
@@ -194,11 +192,41 @@
 
 (defn- jetty-collectors
   []
-  ;; when in dev you might not have a server setup
-  (when (server/instance)
-    [(collector/named {:namespace "metabase_webserver"
-                       :name      "jetty_stats"}
-                      (JettyStatisticsCollector. (.getHandler (server/instance))))]))
+  [(prometheus/counter :jetty/requests-total
+                       {:description "Number of requests"})
+   (prometheus/gauge :jetty/requests-active
+                     {:description "Number of requests currently active"})
+   (prometheus/gauge :jetty/requests-max
+                     {:description "Maximum number of requests that have been active at once"})
+   (prometheus/gauge :jetty/request-time-max-seconds
+                     {:description "Maximum time spent executing a request"})
+   (prometheus/counter :jetty/request-time-seconds-total
+                       {:description "Total time spent executing requests"})
+   (prometheus/counter :jetty/dispatched-total
+                       {:description "Number of requests handled"})
+   (prometheus/gauge :jetty/dispatched-active
+                     {:description "Number of active requests being handled"})
+   (prometheus/gauge :jetty/dispatched-active-max
+                     {:descrption "Maximum number of active requests handled"})
+   (prometheus/gauge :jetty/dispatched-time-max
+                     {:description "Maximum time spent dispatching a request"})
+   (prometheus/counter :jetty/dispatched-time-seconds-total
+                       {:descrption "Total time spent handling requests"})
+   (prometheus/counter :jetty/async-requests-total
+                       {:descrption "Totql number of async requests"})
+   (prometheus/gauge :jetty/async-requests-waiting
+                     {:description "Currently waiting async requests"})
+   (prometheus/gauge :jetty/async-requests-waiting-max
+                     {:description "Maximum number of waiting async requests"})
+   (prometheus/counter :jetty/async-dispatches-total
+                       {:description "Number of requests that have been asynchronously dispatched"})
+   (prometheus/counter :jetty/expires-total
+                       {:descpription "Number of async requests that have expired"})
+   (prometheus/counter :jetty/responses-total
+                       {:descrption "Total response grouped by status code"
+                        :labels [:code]})
+   (prometheus/counter :jetty/responses-bytes-total
+                       {:description "Total number of bytes across all responses"})])
 
 (defn- product-collectors
   []
