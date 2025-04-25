46f7348be91a1a784c6377b984ce6b212a894138
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-04-25 14:08:42 -0400

    Fix back button

--- frontend/src/metabase/querying/filters/components/FilterPicker/TimeFilterPicker/TimeFilterPicker.unit.spec.tsx
+++ frontend/src/metabase/querying/filters/components/FilterPicker/TimeFilterPicker/TimeFilterPicker.unit.spec.tsx
@@ -61,22 +61,28 @@ function setup({
     />,
   );
 
-  function getNextFilterParts() {
+  const getNextFilterParts = () => {
     const [filter] = onChange.mock.lastCall;
     return Lib.timeFilterParts(query, 0, filter);
-  }
+  };
 
-  function getNextFilterColumnName() {
+  const getNextFilterColumnName = () => {
     const parts = getNextFilterParts();
     const column = checkNotNull(parts?.column);
     return Lib.displayInfo(query, 0, column).longDisplayName;
-  }
+  };
+
+  const getNextFilterChangeOpts = () => {
+    const [_filter, opts] = onChange.mock.lastCall;
+    return opts;
+  };
 
   return {
     query,
     column,
     getNextFilterParts,
     getNextFilterColumnName,
+    getNextFilterChangeOpts,
     onChange,
     onBack,
   };
@@ -272,6 +278,18 @@ describe("TimeFilterPicker", () => {
       expect(onBack).toHaveBeenCalled();
       expect(onChange).not.toHaveBeenCalled();
     });
+
+    it.each([
+      { label: "Apply filter", source: "default" },
+      { label: "Add another filter", source: "add-button" },
+    ])(
+      'should add a filter via the "$label" button when the add button is enabled',
+      async ({ label, source }) => {
+        const { getNextFilterChangeOpts } = setup({ withAddButton: true });
+        await userEvent.click(screen.getByRole("button", { name: label }));
+        expect(getNextFilterChangeOpts()).toMatchObject({ source });
+      },
+    );
   });
 
   describe("existing filter", () => {
