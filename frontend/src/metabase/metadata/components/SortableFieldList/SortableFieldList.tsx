0f857396df3c671c8d88652b348371a84df334ea
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-28 21:25:48 +0700

    Implement getFieldIcon boilerplate

--- frontend/src/metabase/metadata/components/SortableFieldList/SortableFieldList.tsx
+++ frontend/src/metabase/metadata/components/SortableFieldList/SortableFieldList.tsx
@@ -5,6 +5,8 @@ import {
   type DragEndEvent,
   SortableList,
 } from "metabase/core/components/Sortable";
+import { useSelector } from "metabase/lib/redux";
+import { getMetadata } from "metabase/selectors/metadata";
 import { Flex } from "metabase/ui";
 import type { Table } from "metabase-types/api";
 
@@ -18,10 +20,11 @@ interface Props {
 }
 
 export const SortableFieldList = ({ table, onChange }: Props) => {
+  const metadata = useSelector(getMetadata);
   const pointerSensor = useSensor(PointerSensor, {
     activationConstraint: { distance: 15 },
   });
-  const items = useMemo(() => getItems(table?.fields), [table?.fields]);
+  const items = useMemo(() => getItems(metadata, table), [metadata, table]);
   const order = useMemo(() => getItemsOrder(items), [items]);
   const sortedItems = useMemo(() => sortItems(items, order), [items, order]);
   const isDragDisabled = sortedItems.length <= 1;
--- frontend/src/metabase/metadata/components/SortableFieldList/utils.ts
+++ frontend/src/metabase/metadata/components/SortableFieldList/utils.ts
@@ -1,9 +1,12 @@
 import _ from "underscore";
 
+import { getColumnIcon } from "metabase/common/utils/columns";
 import type { DragEndEvent } from "metabase/core/components/Sortable";
 import { NULL_DISPLAY_VALUE } from "metabase/lib/constants";
 import { type IconName, isValidIconName } from "metabase/ui";
-import type { Field } from "metabase-types/api";
+import * as Lib from "metabase-lib";
+import type Metadata from "metabase-lib/v1/metadata/Metadata";
+import type { DimensionReference, Field, Table } from "metabase-types/api";
 
 interface Item {
   id: DragEndEvent["id"];
@@ -16,9 +19,13 @@ export function getId(item: Item): Item["id"] {
   return item.id;
 }
 
-export function getItems(fields: Field[] = []): Item[] {
-  return fields.map((field) => {
-    const icon = getFieldIcon(field);
+export function getItems(metadata: Metadata, table: Table): Item[] {
+  if (!table.fields) {
+    return [];
+  }
+
+  return table.fields.map((field) => {
+    const icon = getFieldIcon(metadata, table, field);
 
     return {
       id: getFieldId(field),
@@ -51,7 +58,33 @@ function getFieldDisplayName(field: Field): string {
   return field.dimensions?.[0]?.name || field.display_name || field.name;
 }
 
-function getFieldIcon(field: Field): IconName {
-  // TODO: use `getColumnIcon` from "metabase/common/utils/columns"
-  return "calendar";
+function getFieldIcon(
+  metadata: Metadata,
+  table: Table,
+  field: Field,
+): IconName {
+  const databaseId = table.db_id;
+  const metadataProvider = Lib.metadataProvider(databaseId, metadata);
+  const query = Lib.fromLegacyQuery(databaseId, metadataProvider, {
+    type: "query",
+    database: databaseId,
+    query: {
+      "source-table": field.table_id,
+    },
+  });
+  const columns = Lib.visibleColumns(query, 0);
+  const fieldId = getFieldId(field);
+
+  // fieldId should never be a string in this context because it's a raw table field
+  if (typeof fieldId === "string") {
+    return "list"; // the same fallback as in getColumnIcon
+  }
+
+  const fieldRef: DimensionReference = ["field", fieldId, null];
+  const [index] = Lib.findColumnIndexesFromLegacyRefs(query, 0, columns, [
+    fieldRef,
+  ]);
+  const column = columns[index];
+
+  return getColumnIcon(column);
 }
