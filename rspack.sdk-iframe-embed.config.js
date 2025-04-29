45659be180252b8c59812097c2b9466db4ab0fbb
Phoomparin Mano <phoomparin@gmail.com>
2025-04-29 23:40:31 +0700

    reformat rspack.sdk-iframe-embed.config.js

--- rspack.sdk-iframe-embed.config.js
+++ rspack.sdk-iframe-embed.config.js
@@ -3,14 +3,14 @@ const ENTERPRISE_SRC_PATH =
   __dirname + "/enterprise/frontend/src/metabase-enterprise";
 
 module.exports = {
-  name: 'iframe_sdk_embed_v1',
+  name: "iframe_sdk_embed_v1",
   entry: ENTERPRISE_SRC_PATH + "/embedding_iframe_sdk/embed.v1.ts",
   output: {
     path: BUILD_PATH + "/app",
-    filename: 'embed.v1.js',
-    library: 'metabase.embed',
-    libraryTarget: 'umd',
-    globalObject: 'this',
+    filename: "embed.v1.js",
+    library: "metabase.embed",
+    libraryTarget: "umd",
+    globalObject: "this",
 
     // disabling HMR with devServer.hot still results in hot update files,
     // so we must disable them explicitly
@@ -26,7 +26,11 @@ module.exports = {
           {
             loader: "builtin:swc-loader",
             options: {
-              jsc: { loose: true, transform: {}, parser: { syntax: "typescript" }, },
+              jsc: {
+                loose: true,
+                transform: {},
+                parser: { syntax: "typescript" },
+              },
               sourceMaps: false,
               minify: false,
               env: { targets: ["defaults"] },
@@ -39,4 +43,4 @@ module.exports = {
   },
   optimization: { splitChunks: false, runtimeChunk: false },
   devtool: false,
-}
+};
