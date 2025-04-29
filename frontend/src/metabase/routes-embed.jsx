53a9f98b7b29d8d262e3bd91b446b1e53a9185ac
Phoomparin Mano <phoomparin@gmail.com>
2025-04-29 19:31:30 +0700

    update route to embed/sdk/v1

--- frontend/src/metabase/routes-embed.jsx
+++ frontend/src/metabase/routes-embed.jsx
@@ -11,7 +11,7 @@ export const getRoutes = (store) => (
   <Route>
     {PLUGIN_EMBEDDING_IFRAME_SDK.SdkIframeEmbedRoute && (
       <Route
-        path="embed/v2"
+        path="embed/sdk/v1"
         component={PLUGIN_EMBEDDING_IFRAME_SDK.SdkIframeEmbedRoute}
       />
     )}
