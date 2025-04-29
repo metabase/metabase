cf858da66643c8447c0a2f8ec190a91ffdfb12cb
Cam Saul <github@camsaul.com>
2025-04-29 21:16:24 +0000

    Fix Search Settings

--- src/metabase/search/settings.clj
+++ src/metabase/search/settings.clj
@@ -1,7 +1,17 @@
 (ns metabase.search.settings
   (:require
    [metabase.settings.core :refer [defsetting]]
+   [metabase.settings.deprecated-grab-bag :as public-settings]
    [metabase.util.i18n :as i18n]))
+
+(defsetting search-typeahead-enabled
+  (i18n/deferred-tru "Enable typeahead search in the {0} navbar?"
+                     (public-settings/application-name-for-setting-descriptions))
+  :type       :boolean
+  :default    true
+  :visibility :authenticated
+  :export?    true
+  :audit      :getter)
 
 (defsetting search-engine
   (i18n/deferred-tru "Which engine to use when performing search. Supported values are :in-place and :appdb")
