a73d72df5a28efb24e84af544ee4eb2efdd1a9c1
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-30 21:51:40 +0700

    CoercionStrategyPicker error state

--- frontend/src/metabase/metadata/components/CoercionStrategyPicker/CoercionStrategyPicker.tsx
+++ frontend/src/metabase/metadata/components/CoercionStrategyPicker/CoercionStrategyPicker.tsx
@@ -1,9 +1,11 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { t } from "ttag";
 
 import { coercions_for_type } from "cljs/metabase.types";
 import { Select, type SelectProps } from "metabase/ui";
 import type { Field } from "metabase-types/api";
+
+import S from "./CoercionStrategyPicker.module.css";
 
 interface Props extends Omit<SelectProps, "data" | "value" | "onChange"> {
   baseType: Field["base_type"];
@@ -17,10 +19,16 @@ export const CoercionStrategyPicker = ({
   onChange,
   ...props
 }: Props) => {
+  const [isTouched, setIsTouched] = useState(false);
   const data = useMemo(() => coercions_for_type(baseType), [baseType]);
+  const error =
+    value == null ? t`To enable casting, please select a data type` : null;
 
   return (
     <Select
+      classNames={{
+        root: S.root,
+      }}
       comboboxProps={{
         middlewares: {
           flip: true,
@@ -31,8 +39,10 @@ export const CoercionStrategyPicker = ({
         position: "bottom-start",
       }}
       data={data}
+      error={isTouched ? error : null}
       placeholder={t`Select data type`}
       value={value}
+      onBlur={() => setIsTouched(true)}
       onChange={onChange}
       {...props}
     />
