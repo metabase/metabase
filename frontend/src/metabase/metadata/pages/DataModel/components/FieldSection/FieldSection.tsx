7c98254e6defca4df9140ad52641546414f0dec1
Kamil Mielnik <kamil@kamilmielnik.com>
2025-04-30 17:00:31 +0700

    Load field, display name

--- frontend/src/metabase/metadata/pages/DataModel/components/FieldSection/FieldSection.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/components/FieldSection/FieldSection.tsx
@@ -1,18 +1,25 @@
+import { useGetFieldQuery } from "metabase/api";
+import { LoadingAndErrorWrapper } from "metabase/components/LoadingAndErrorWrapper";
+import { NULL_DISPLAY_VALUE } from "metabase/lib/constants";
 import { Box, Title } from "metabase/ui";
 import type { FieldId } from "metabase-types/api";
 
 interface Props {
-  fieldId?: FieldId;
+  fieldId: FieldId;
 }
 
 export const FieldSection = ({ fieldId }: Props) => {
+  const { data: field, error, isLoading } = useGetFieldQuery({ id: fieldId });
+
+  if (error || isLoading || !field) {
+    return <LoadingAndErrorWrapper error={error} loading={isLoading} />;
+  }
+
   return (
     <Box>
       <Title mb="md" order={2}>
-        Category
+        {field.display_name || field.name || NULL_DISPLAY_VALUE}
       </Title>
-
-      <Box>Field: {fieldId}</Box>
     </Box>
   );
 };
