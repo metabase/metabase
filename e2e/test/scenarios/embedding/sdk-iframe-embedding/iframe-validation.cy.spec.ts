a22ec54a6d7e28ecea83a7bebc3a6d3ea83487d9
Phoomparin Mano <phoomparin@gmail.com>
2025-04-30 20:47:01 +0700

    add more iframe validation tests

--- e2e/test/scenarios/embedding/sdk-iframe-embedding/iframe-validation.cy.spec.ts
+++ e2e/test/scenarios/embedding/sdk-iframe-embedding/iframe-validation.cy.spec.ts
@@ -6,7 +6,7 @@ describe("scenarios > embedding > sdk iframe embedding > iframe validation", () 
     cy.signOut();
   });
 
-  it("raises an error when target element is not found", () => {
+  it("throws when target element is not found", () => {
     H.loadSdkIframeEmbedTestPage({
       target: "#not-existent-target",
       expectErrors: true,
@@ -18,7 +18,7 @@ describe("scenarios > embedding > sdk iframe embedding > iframe validation", () 
     );
   });
 
-  it("raises an error when target element is not provided", () => {
+  it("throws when target element is undefined", () => {
     H.loadSdkIframeEmbedTestPage({
       target: undefined,
       expectErrors: true,
@@ -30,7 +30,7 @@ describe("scenarios > embedding > sdk iframe embedding > iframe validation", () 
     );
   });
 
-  it("raises an error when api key is not provided", () => {
+  it("throws when api key is not provided", () => {
     H.loadSdkIframeEmbedTestPage({
       apiKey: undefined,
       expectErrors: true,
@@ -39,6 +39,58 @@ describe("scenarios > embedding > sdk iframe embedding > iframe validation", () 
     cy.get("@consoleError").should(
       "be.calledWith",
       "[metabase.embed] api key and instance url must be provided",
+    );
+  });
+
+  it("throws when instance url is not provided", () => {
+    H.loadSdkIframeEmbedTestPage({
+      apiKey: "foobar",
+      instanceUrl: undefined,
+      expectErrors: true,
+    });
+
+    cy.get("@consoleError").should(
+      "be.calledWith",
+      "[metabase.embed] api key and instance url must be provided",
+    );
+  });
+
+  it("throws when both question id and dashboard id are provided", () => {
+    H.loadSdkIframeEmbedTestPage({
+      questionId: 10,
+      dashboardId: 10,
+      expectErrors: true,
+    });
+
+    cy.get("@consoleError").should(
+      "be.calledWith",
+      "[metabase.embed] can't use both dashboardId and questionId at the same time",
+    );
+  });
+
+  it("throws when question id is provided in the exploration template", () => {
+    H.loadSdkIframeEmbedTestPage({
+      template: "exploration",
+      questionId: 10,
+      expectErrors: true,
+    });
+
+    cy.get("@consoleError").should(
+      "be.calledWith",
+      "[metabase.embed] the exploration template can't be used with dashboardId or questionId",
+    );
+  });
+
+  it("throws when dashboard id is provided in the exploration template", () => {
+    H.loadSdkIframeEmbedTestPage({
+      template: "exploration",
+      dashboardId: 10,
+      expectErrors: true,
+    });
+
+    cy.get("@consoleError").should(
+      "be.calledWith",
+      "[metabase.embed] the exploration template can't be used with dashboardId or questionId",
     );
   });
 });
