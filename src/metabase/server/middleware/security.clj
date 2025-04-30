3b43b52c3cc1b9dbf85a353211dd3ce8a2b217ca
Timofey Kachalov <timofei@metabase.com>
2025-04-30 11:55:52 +0400

    Add Access-Control-Max-Age header to cache CORS preflight requests made by SDK (#57383)

--- src/metabase/server/middleware/security.clj
+++ src/metabase/server/middleware/security.clj
@@ -231,7 +231,9 @@
         "Vary"                        "Origin"})
      {"Access-Control-Allow-Headers"  "*"
       "Access-Control-Allow-Methods"  "*"
-      "Access-Control-Expose-Headers" "X-Metabase-Anti-CSRF-Token"})))
+      "Access-Control-Expose-Headers" "X-Metabase-Anti-CSRF-Token"
+      ;; Needed for Embedding SDK. Should cache preflight requests for the specified number of seconds.
+      "Access-Control-Max-Age"  "60"})))
 
 (defn security-headers
   "Fetch a map of security headers that should be added to a response based on the passed options."
