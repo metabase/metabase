3b31164e9c4b28325183399db0866a82b019e72b
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-01 17:04:33 -0400

    Support translated question names in static embedding

--- frontend/src/metabase/components/TitleAndDescription/TitleAndDescription.jsx
+++ frontend/src/metabase/components/TitleAndDescription/TitleAndDescription.jsx
@@ -3,17 +3,21 @@ import cx from "classnames";
 import { memo } from "react";
 
 import CS from "metabase/css/core/index.css";
+import { useTranslateContent } from "metabase/i18n/hooks";
 import { Icon, Tooltip } from "metabase/ui";
 
-const TitleAndDescription = ({ title, description, className }) => (
-  <div className={cx(CS.flex, CS.alignCenter, className)}>
-    <h2 className={cx(CS.h2, CS.mr1, CS.textWrap)}>{title}</h2>
-    {description && (
-      <Tooltip label={description} maw="22em">
-        <Icon name="info" className={CS.mx1} />
-      </Tooltip>
-    )}
-  </div>
-);
+const TitleAndDescription = ({ title, description, className }) => {
+  const tc = useTranslateContent();
+  return (
+    <div className={cx(CS.flex, CS.alignCenter, className)}>
+      <h2 className={cx(CS.h2, CS.mr1, CS.textWrap)}>{tc(title)}</h2>
+      {description && (
+        <Tooltip label={description} maw="22em">
+          <Icon name="info" className={CS.mx1} />
+        </Tooltip>
+      )}
+    </div>
+  );
+};
 
 export default memo(TitleAndDescription);
