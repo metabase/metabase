9c673f0995e86a3863e9f206d9b672cdffb4ef29
Oisin Coveney <oisin@metabase.com>
2025-05-01 08:09:35 +0100

    TS Conversion: Automatic Dashboard App (#57075)

--- frontend/src/metabase-types/api/dashboard.ts
+++ frontend/src/metabase-types/api/dashboard.ts
@@ -79,7 +79,25 @@ export interface Dashboard {
 
   /* Indicates whether static embedding for this dashboard has been published */
   enable_embedding: boolean;
+
+  /* For x-ray dashboards */
+  transient_name?: string;
+  related?: RelatedDashboardXRays;
+  more?: string | null;
 }
+
+export type RelatedDashboardXRays = {
+  related?: RelatedDashboardXRayItem[];
+  "zoom-in"?: RelatedDashboardXRayItem[];
+  "zoom-out"?: RelatedDashboardXRayItem[];
+  compare?: RelatedDashboardXRayItem[];
+};
+
+export type RelatedDashboardXRayItem = {
+  description: string;
+  title: string;
+  url: string;
+};
 
 /** Dashboards with string ids, like x-rays, cannot have cache configurations */
 export type CacheableDashboard = Omit<Dashboard, "id"> & { id: number };
--- frontend/src/metabase/dashboard/containers/AutomaticDashboardApp/AutomaticDashboardApp.tsx
+++ frontend/src/metabase/dashboard/containers/AutomaticDashboardApp/AutomaticDashboardApp.tsx
@@ -1,42 +1,54 @@
-/* eslint-disable react/prop-types */
 import cx from "classnames";
 import { dissoc } from "icepick";
 import { Component } from "react";
+import type { ConnectedProps } from "react-redux";
+import type { WithRouterProps } from "react-router";
 import { t } from "ttag";
 import _ from "underscore";
 
 import ActionButton from "metabase/components/ActionButton";
-import Card from "metabase/components/Card";
 import Button from "metabase/core/components/Button";
 import Link from "metabase/core/components/Link";
 import CS from "metabase/css/core/index.css";
 import { DashboardTabs } from "metabase/dashboard/components/DashboardTabs";
 import { DASHBOARD_PARAMETERS_PDF_EXPORT_NODE_ID } from "metabase/dashboard/constants";
 import { Dashboard } from "metabase/dashboard/containers/Dashboard";
-import { DashboardData } from "metabase/dashboard/hoc/DashboardData";
+import {
+  DashboardData,
+  type DashboardDataReturnedProps,
+} from "metabase/dashboard/hoc/DashboardData";
 import { getIsHeaderVisible, getTabs } from "metabase/dashboard/selectors";
 import Collections from "metabase/entities/collections";
 import Dashboards from "metabase/entities/dashboards";
 import title from "metabase/hoc/Title";
-import { color } from "metabase/lib/colors";
 import { connect } from "metabase/lib/redux";
 import * as Urls from "metabase/lib/urls";
 import { ParametersList } from "metabase/parameters/components/ParametersList";
 import { addUndo } from "metabase/redux/undo";
 import { getMetadata } from "metabase/selectors/metadata";
-import { Box, Flex, Icon, Title, Tooltip } from "metabase/ui";
+import { Box } from "metabase/ui";
 import { getValuePopulatedParameters } from "metabase-lib/v1/parameters/utils/parameter-values";
+import type { Dashboard as IDashboard } from "metabase-types/api";
+import type { State } from "metabase-types/store";
 
-import { FixedWidthContainer } from "../components/Dashboard/DashboardComponents";
-import { useDashboardUrlQuery } from "../hooks/use-dashboard-url-query";
+import { FixedWidthContainer } from "../../components/Dashboard/DashboardComponents";
+import { useDashboardUrlQuery } from "../../hooks/use-dashboard-url-query";
+import { XrayIcon } from "../XrayIcon";
 
 import S from "./AutomaticDashboardApp.module.css";
-import { XrayIcon } from "./XrayIcon";
+import { SuggestionsSidebar } from "./SuggestionsSidebar";
 
-const getDashboardId = (state, { params: { splat }, location: { hash } }) =>
-  `/auto/dashboard/${splat}${hash.replace(/^#?/, "?")}`;
+type AutomaticDashboardAppRouterProps = WithRouterProps<{ splat: string }>;
 
-const mapStateToProps = (state, props) => ({
+const getDashboardId = (
+  _state: State,
+  { params: { splat }, location: { hash } }: AutomaticDashboardAppRouterProps,
+) => `/auto/dashboard/${splat}${hash.replace(/^#?/, "?")}`;
+
+const mapStateToProps = (
+  state: State,
+  props: AutomaticDashboardAppRouterProps,
+) => ({
   metadata: getMetadata(state),
   dashboardId: getDashboardId(state, props),
   isHeaderVisible: getIsHeaderVisible(state),
@@ -49,12 +61,19 @@ const mapDispatchToProps = {
   addUndo,
 };
 
-class AutomaticDashboardAppInner extends Component {
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type ReduxProps = ConnectedProps<typeof connector>;
+
+type AutomaticDashboardAppInnerProps = ReduxProps &
+  AutomaticDashboardAppRouterProps &
+  DashboardDataReturnedProps;
+
+class AutomaticDashboardAppInner extends Component<AutomaticDashboardAppInnerProps> {
   state = {
     savedDashboardId: null,
   };
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps: AutomaticDashboardAppInnerProps) {
     // scroll to the top when the pathname changes
     if (prevProps.location.pathname !== this.props.location.pathname) {
       window.scrollTo(0, 0);
@@ -87,7 +106,7 @@ class AutomaticDashboardAppInner extends Component {
     this.setState({ savedDashboardId: newDashboard.id });
   };
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps: AutomaticDashboardAppInnerProps) {
     // clear savedDashboardId if changing to a different dashboard
     if (this.props.location.pathname !== nextProps.location.pathname) {
       this.setState({ savedDashboardId: null });
@@ -150,7 +169,7 @@ class AutomaticDashboardAppInner extends Component {
                       </ActionButton>
                     )}
                   </div>
-                  {this.props.tabs.length > 1 && (
+                  {dashboard && this.props.tabs.length > 1 && (
                     <div className={cx(CS.wrapper, CS.flex, CS.alignCenter)}>
                       <DashboardTabs dashboardId={dashboard.id} />
                     </div>
@@ -179,7 +198,19 @@ class AutomaticDashboardAppInner extends Component {
                 </FixedWidthContainer>
               </div>
             )}
-            <Dashboard isXray {...this.props} />
+            {this.props.dashboard && (
+              <Dashboard
+                isXray
+                dashboard={this.props.dashboard}
+                selectedTabId={this.props.selectedTabId}
+                slowCards={this.props.slowCards}
+                clickBehaviorSidebarDashcard={null}
+                downloadsEnabled={false}
+                navigateToNewCardFromDashboard={
+                  this.props.navigateToNewCardFromDashboard ?? undefined
+                }
+              />
+            )}
           </div>
           {more && (
             <div className={cx(CS.flex, CS.justifyEnd, CS.px4, CS.pb4)}>
@@ -208,104 +239,26 @@ class AutomaticDashboardAppInner extends Component {
 }
 
 export const AutomaticDashboardAppConnected = _.compose(
-  connect(mapStateToProps, mapDispatchToProps),
+  connector,
   DashboardData,
-  title(({ dashboard }) => dashboard && dashboard.name),
+  title(
+    ({ dashboard }: { dashboard: IDashboard }) => dashboard && dashboard.name,
+  ),
 )(AutomaticDashboardAppInner);
 
-const TransientTitle = ({ dashboard }) =>
+const TransientTitle = ({ dashboard }: { dashboard: IDashboard }) =>
   dashboard.transient_name ? (
     <span>{dashboard.transient_name}</span>
   ) : dashboard.name ? (
     <span>{dashboard.name}</span>
   ) : null;
 
-const RELATED_CONTENT = {
-  compare: {
-    get title() {
-      return t`Compare`;
-    },
-    icon: "compare",
-  },
-  "zoom-in": {
-    get title() {
-      return t`Zoom in`;
-    },
-    icon: "zoom_in",
-  },
-  "zoom-out": {
-    get title() {
-      return t`Zoom out`;
-    },
-    icon: "zoom_out",
-  },
-  related: {
-    get title() {
-      return t`Related`;
-    },
-    icon: "connections",
-  },
-};
-
-const SuggestionsList = ({ suggestions, section }) => (
-  <Box component="ol" my="sm">
-    {Object.keys(suggestions).map((s, i) => (
-      <li key={i} className={CS.my2}>
-        <SuggestionSectionHeading>
-          {RELATED_CONTENT[s].title}
-        </SuggestionSectionHeading>
-        {suggestions[s].length > 0 &&
-          suggestions[s].map((item, itemIndex) => (
-            <Link
-              key={itemIndex}
-              to={item.url}
-              className={cx(CS.hoverParent, CS.hoverVisibility, S.ItemLink)}
-            >
-              <Card className={CS.p2} hoverable>
-                <Flex align="center">
-                  <Icon
-                    name={RELATED_CONTENT[s].icon}
-                    color={color("accent4")}
-                    className={CS.mr1}
-                  />
-                  <h4 className={CS.textWrap}>{item.title}</h4>
-                  <Box ml="auto" className={CS.hoverChild}>
-                    <Tooltip label={item.description}>
-                      <Icon name="info_outline" color={color("bg-dark")} />
-                    </Tooltip>
-                  </Box>
-                </Flex>
-              </Card>
-            </Link>
-          ))}
-      </li>
-    ))}
-  </Box>
-);
-
-const SuggestionSectionHeading = ({ children }) => (
-  <h5
-    style={{
-      fontWeight: 900,
-      textTransform: "uppercase",
-      color: color("text-medium"),
-    }}
-    className={CS.mb1}
-  >
-    {children}
-  </h5>
-);
-
-const SuggestionsSidebar = ({ related }) => (
-  <Flex direction="column" py="md" px="xl">
-    <Title py="sm" px={0} order={2}>{t`More X-rays`}</Title>
-    <SuggestionsList suggestions={related} />
-  </Flex>
-);
-
 // Workaround until AutomaticDashboardApp is refactored to be a function component
 // (or even better, merged/generalized with DashboardApp)
-const AutomaticDashboardQueryParamsSync = ({ router, location }) => {
+const AutomaticDashboardQueryParamsSync = ({
+  router,
+  location,
+}: Pick<WithRouterProps, "router" | "location">) => {
   useDashboardUrlQuery(router, location);
   return null;
 };
