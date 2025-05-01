0deec7ed8bced7a6c7dfc1f149f764d6071d8028
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-30 23:17:27 -0400

    Use Formik approach to success/active/failure modes for upload input button

--- enterprise/frontend/src/metabase-enterprise/content_translation/components/ContentTranslationConfiguration.tsx
+++ enterprise/frontend/src/metabase-enterprise/content_translation/components/ContentTranslationConfiguration.tsx
@@ -1,24 +1,27 @@
-import { type ReactNode } from "react";
+import {
+  type ChangeEvent,
+  type ReactNode,
+  useCallback,
+  useRef,
+  useState,
+} from "react";
 import { t } from "ttag";
 
+import { UploadInput } from "metabase/components/upload";
 import ExternalLink from "metabase/core/components/ExternalLink";
-import FormFileInput from "metabase/core/components/FormFileInput";
 import Markdown from "metabase/core/components/Markdown";
-import { Form, FormProvider, FormSubmitButton } from "metabase/forms";
+import {
+  Form,
+  FormProvider,
+  FormSubmitButton,
+  useFormContext,
+} from "metabase/forms";
 import { useSelector } from "metabase/lib/redux";
 import { getDocsUrl } from "metabase/selectors/settings";
-import { Icon, Stack, Text } from "metabase/ui";
+import { Group, Icon, Loader, Stack, Text } from "metabase/ui";
 import { useUploadContentTranslationDictionaryMutation } from "metabase-enterprise/api";
-import { UploadInput } from "metabase/components/upload";
-
-interface FormValues {
-  file: File | null;
-}
 
 export const ContentTranslationConfiguration = () => {
-  const [uploadContentTranslationDictionary] =
-    useUploadContentTranslationDictionaryMutation();
-
   const availableLocalesDocsUrl = useSelector((state) =>
     // eslint-disable-next-line no-unconditional-metabase-links-render -- This is used in admin settings
     getDocsUrl(state, {
@@ -26,14 +29,6 @@ export const ContentTranslationConfiguration = () => {
       anchor: "supported-languages",
     }),
   );
-
-  const handleSubmit = async (values: FormValues) => {
-    if (!values.file) {
-      throw new Error(t`No file selected`);
-    }
-
-    await uploadContentTranslationDictionary({ file: values.file }).unwrap();
-  };
 
   return (
     <Stack gap="sm" maw="38rem">
@@ -51,28 +46,107 @@ export const ContentTranslationConfiguration = () => {
         </Markdown>
       </Text>
 
-      <FormProvider initialValues={{ file: null }} onSubmit={handleSubmit}>
-        <Form>
-          <Stack gap="md">
-            <FormSubmitButton
-              leftSection={<Icon name="upload" />}
-              label={t`Upload translation dictionary`}
-              successLabel={t`Dictionary uploaded`}
-              failedLabel={t`Could not upload dictionary`}
-              onClick={triggerUpload}
-              maw="20rem"
-            />
-            <UploadInput
-              id="content-translation-dictionary-upload-input"
-              style={{ display: "none" }}
-              ref={inputRef}
-              accept="text/csv"
-              onChange={handleFileChange}
-              key={uploadInputReactKey}
-            />
-          </Stack>
-        </Form>
+      <FormProvider initialValues={{}} onSubmit={() => {}}>
+        <UploadForm />
       </FormProvider>
     </Stack>
+  );
+};
+
+const UploadForm = () => {
+  const [uploadContentTranslationDictionary] =
+    useUploadContentTranslationDictionaryMutation();
+
+  // This is used to reset the input so that the same file can be uploaded
+  // multiple times
+  const [uploadInputReactKey, setUploadInputReactKey] = useState(0);
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files?.length) {
+      const file = event.target.files[0];
+      await uploadFile(file);
+      setUploadInputReactKey((n) => n + 1);
+    }
+  };
+
+  const { status, setStatus } = useFormContext();
+
+  const uploadFile = useCallback(
+    async (file: File) => {
+      if (!file) {
+        console.error("No file selected");
+        return;
+      }
+      setStatus("pending");
+      await uploadContentTranslationDictionary({ file })
+        .unwrap()
+        .then(() => {
+          setStatus("fulfilled");
+        })
+        .catch(() => {
+          setStatus("rejected");
+        });
+    },
+    [uploadContentTranslationDictionary, setStatus],
+  );
+
+  const triggerUpload = () => {
+    const input = inputRef.current;
+    if (!input) {
+      return;
+    }
+    input.style.display = "block";
+    input.click();
+    input.style.display = "none";
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <Form>
+      <Stack gap="md">
+        <FormSubmitButton
+          disabled={status === "pending"}
+          label={
+            <Group gap="sm">
+              <Icon name="upload" opacity=".8" />
+              <Text c="white">{t`Upload translation dictionary`}</Text>
+            </Group>
+          }
+          successLabel={
+            <Group gap="sm" role="alert">
+              <Icon name="check" opacity=".8" />
+              <Text c="white">{t`Dictionary uploaded`}</Text>
+            </Group>
+          }
+          failedLabel={
+            <Group gap="sm" role="alert">
+              <Icon name="warning" opacity=".8" />
+              <Text c="white">{t`Could not upload dictionary`}</Text>
+            </Group>
+          }
+          activeLabel={
+            <Group gap="md" role="alert">
+              <Loader size="xs" opacity=".8" />
+              <Text>{t`Uploading dictionary...`}</Text>
+            </Group>
+          }
+          maw="20rem"
+          variant="filled"
+          onClick={(e) => {
+            triggerUpload();
+            e.preventDefault();
+          }}
+        />
+        <UploadInput
+          id="content-translation-dictionary-upload-input"
+          style={{ display: "none" }}
+          ref={inputRef}
+          accept="text/csv"
+          onChange={handleFileChange}
+          key={uploadInputReactKey}
+        />
+      </Stack>
+    </Form>
   );
 };
