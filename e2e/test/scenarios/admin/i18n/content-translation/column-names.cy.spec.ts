d386e313debb8ded3692ee03f3a6d8404e59cd0e
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-01 14:24:34 -0400

    Support column name translations

---
+++ e2e/test/scenarios/admin/i18n/content-translation/column-names.cy.spec.ts
@@ -0,0 +1,84 @@
+import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
+import { NORMAL_USER_ID } from "e2e/support/cypress_sample_instance_data";
+
+import { germanFieldNames } from "./constants";
+import { uploadTranslationDictionary } from "./helpers/e2e-content-translation-helpers";
+
+const { PRODUCTS_ID } = SAMPLE_DATABASE;
+
+const { H } = cy;
+
+describe("scenarios > admin > localization > content translation of column names", () => {
+  describe("ee", () => {
+    describe("after uploading related German translations", () => {
+      before(() => {
+        H.restore();
+        cy.signInAsAdmin();
+        H.setTokenFeatures("all");
+        H.createQuestion(
+          {
+            name: "Products question",
+            query: {
+              "source-table": PRODUCTS_ID,
+            },
+          },
+          { wrapId: true, idAlias: "productsQuestionId" },
+        );
+        uploadTranslationDictionary(germanFieldNames);
+        H.snapshot("translations-uploaded--normal-user-locale-is-en");
+        cy.request("PUT", `/api/user/${NORMAL_USER_ID}`, {
+          locale: "de",
+        });
+        H.snapshot("translations-uploaded--normal-user-locale-is-de");
+
+        cy.intercept("POST", "api/ee/content-translation/upload-dictionary").as(
+          "uploadDictionary",
+        );
+      });
+
+      describe("on the question page", () => {
+        let productsQuestionId = null as unknown as number;
+
+        before(() => {
+          cy.get<number>("@productsQuestionId").then((id) => {
+            productsQuestionId = id;
+          });
+        });
+
+        describe("when locale is English, column names are NOT localized in", () => {
+          beforeEach(() => {
+            H.restore("translations-uploaded--normal-user-locale-is-en" as any);
+            cy.signInAsNormalUser();
+          });
+
+          it("column headers", () => {
+            H.visitQuestion(productsQuestionId);
+            cy.findByTestId("table-header").within(() => {
+              germanFieldNames.forEach((row) => {
+                cy.findByText(row.msgid).should("be.visible");
+                cy.findByText(row.msgstr).should("not.exist");
+              });
+            });
+          });
+        });
+
+        describe("when locale is German, column names ARE localized in", () => {
+          beforeEach(() => {
+            H.restore("translations-uploaded--normal-user-locale-is-de" as any);
+            cy.signInAsNormalUser();
+          });
+
+          it("column headers", () => {
+            H.visitQuestion(productsQuestionId);
+            cy.findByTestId("table-header").within(() => {
+              germanFieldNames.forEach((row) => {
+                cy.findByText(row.msgid).should("not.exist");
+                cy.findByText(row.msgstr).should("be.visible");
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
