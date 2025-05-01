d3047e5d5f4e54130e4e11590f204df47d997d42
Timofei <timofei@metabase.com>
2025-05-01 13:44:04 +0400

    Add tsup/esbuild config for building Embedding SDK

---
+++ frontend/build/embedding-sdk/utils/get-css-modules-inject-code.mjs
@@ -0,0 +1,41 @@
+import { transform as esbuildTransform } from "esbuild";
+
+import { CSS_MODULE_INJECT_FUNCTION_NAME } from "../constants/css-module-inject-function-name.mjs";
+
+export const getCssModulesInjectCode = async () => {
+  const code = `
+    function ${CSS_MODULE_INJECT_FUNCTION_NAME}(css, { insertAt } = {}) {
+      if (!css || typeof document === 'undefined') return
+
+      const head = document.head || document.getElementsByTagName('head')[0]
+      const style = document.createElement('style')
+      style.type = 'text/css'
+
+      if (insertAt === 'top') {
+        if (head.firstChild) {
+          head.insertBefore(style, head.firstChild)
+        } else {
+          head.appendChild(style)
+        }
+      } else {
+        head.appendChild(style)
+      }
+
+      if (style.styleSheet) {
+        style.styleSheet.cssText = css
+      } else {
+        style.appendChild(document.createTextNode(css))
+      }
+    }
+  `;
+
+  return (
+    await esbuildTransform(code, {
+      minify: true,
+      minifyIdentifiers: true,
+      minifySyntax: true,
+      minifyWhitespace: true,
+      loader: "js",
+    })
+  ).code;
+};
