faa2345f4a8abf61045917660c18a0ca90fa99b8
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-02 06:27:52 -0400

    wip: fixing e2e tests where token seems missing

--- e2e/test/scenarios/admin/i18n/content-translation/column-names.cy.spec.ts
+++ e2e/test/scenarios/admin/i18n/content-translation/column-names.cy.spec.ts
@@ -18,201 +18,194 @@ const usedGermanFieldNames = germanFieldNames.filter(
   (row) => row.msgstr !== "Kahl",
 );
 
-describe("scenarios > admin > localization > content translation", () => {
+describe("scenarios > admin > localization > content translation of column names", () => {
   describe("ee", () => {
-    beforeEach(() => {
+    before(() => {
       H.restore();
+    });
+    beforeEach(() => {
       cy.signInAsAdmin();
       H.setTokenFeatures("all");
     });
-
-    describe("column names", () => {
-      describe("after uploading related German translations", () => {
-        before(() => {
-          H.restore();
-          cy.signInAsAdmin();
-          H.createQuestion(
-            {
-              name: "Products question",
-              query: {
-                "source-table": PRODUCTS_ID,
-              },
+    describe("after uploading related German translations", () => {
+      before(() => {
+        cy.signInAsAdmin();
+        H.setTokenFeatures("all");
+        H.createQuestion(
+          {
+            name: "Products question",
+            query: {
+              "source-table": PRODUCTS_ID,
             },
-            { wrapId: true, idAlias: "productsQuestionId" },
-          );
-          uploadTranslationDictionary(germanFieldNames);
-          H.snapshot("translations-uploaded--normal-user-locale-is-en");
-          cy.request("PUT", `/api/user/${NORMAL_USER_ID}`, {
-            locale: "de",
+          },
+          { wrapId: true, idAlias: "productsQuestionId" },
+        );
+        uploadTranslationDictionary(germanFieldNames);
+        H.snapshot("translations-uploaded--normal-user-locale-is-en");
+        cy.request("PUT", `/api/user/${NORMAL_USER_ID}`, {
+          locale: "de",
+        });
+        H.snapshot("translations-uploaded--normal-user-locale-is-de");
+      });
+
+      describe("on the question page", () => {
+        let productsQuestionId = null as unknown as number;
+
+        before(() => {
+          cy.get<number>("@productsQuestionId").then((id) => {
+            productsQuestionId = id;
           });
-          H.snapshot("translations-uploaded--normal-user-locale-is-de");
         });
 
-        describe("on the question page", () => {
-          let productsQuestionId = null as unknown as number;
+        describe("when locale is English, column names are NOT localized in", () => {
+          beforeEach(() => {
+            H.restore("translations-uploaded--normal-user-locale-is-en" as any);
+            cy.signInAsNormalUser();
+          });
 
-          before(() => {
-            cy.get<number>("@productsQuestionId").then((id) => {
-              productsQuestionId = id;
+          it.only("column headers", () => {
+            H.visitQuestion(productsQuestionId);
+            cy.findByTestId("table-header").within(() => {
+              usedGermanFieldNames.forEach((row) => {
+                cy.findByText(row.msgid).should("be.visible");
+                cy.findByText(row.msgstr).should("not.exist");
+              });
             });
           });
 
-          describe("when locale is English, column names are NOT localized in", () => {
-            beforeEach(() => {
-              H.restore(
-                "translations-uploaded--normal-user-locale-is-en" as any,
-              );
-              cy.signInAsNormalUser();
-            });
+          it("filter popover", () => {
+            H.visitQuestion(productsQuestionId);
 
-            it("column headers", () => {
-              H.visitQuestion(productsQuestionId);
-              cy.findByTestId("table-header").within(() => {
-                usedGermanFieldNames.forEach((row) => {
-                  cy.findByText(row.msgid).should("be.visible");
-                  cy.findByText(row.msgstr).should("not.exist");
-                });
-              });
-            });
+            H.filter();
 
-            it("filter popover", () => {
-              H.visitQuestion(productsQuestionId);
-
-              H.filter();
-
-              Object.values(usedGermanFieldNames).forEach((row) => {
-                H.popover().within(() => {
-                  cy.findByText(row.msgstr).should("not.exist");
-                  cy.findByText(row.msgid).click();
-                });
-
-                // Within subsequent popover
-                H.popover().within(() => {
-                  cy.findByText(row.msgstr).should("not.exist");
-                  cy.findByText(row.msgid).click();
-                  // Clicking the column name returns to the main popover
-                });
-              });
-            });
-
-            it("summarize sidebar", () => {
-              H.visitQuestion(productsQuestionId);
-              cy.log("Open summarize sidebar");
-              H.summarize();
-              H.rightSidebar().within(() => {
-                cy.log(
-                  "Summarize sidebar includes all column names in English",
-                );
-                Object.values(usedGermanFieldNames).forEach((row) => {
-                  cy.findByText(row.msgstr).should("not.exist");
-                  cy.findByText(row.msgid).should("be.visible");
-                });
-              });
-            });
-
-            it("dashcard parameter mapper", () => {
-              openDashCardCardParameterMapper();
-
+            Object.values(usedGermanFieldNames).forEach((row) => {
               H.popover().within(() => {
-                Object.values(usedGermanFieldNames)
-                  .filter((tr) => columnNamesWithTypeText.includes(tr.msgid))
-                  .forEach((row) => {
-                    cy.findByText(row.msgstr).should("not.exist");
-                    cy.findByText(row.msgid).should("be.visible");
-                  });
+                cy.findByText(row.msgstr).should("not.exist");
+                cy.findByText(row.msgid).click();
+              });
+
+              // Within subsequent popover
+              H.popover().within(() => {
+                cy.findByText(row.msgstr).should("not.exist");
+                cy.findByText(row.msgid).click();
+                // Clicking the column name returns to the main popover
               });
             });
           });
 
-          describe("when locale is German, column names ARE localized in", () => {
-            beforeEach(() => {
-              H.restore(
-                "translations-uploaded--normal-user-locale-is-de" as any,
-              );
-              cy.signInAsNormalUser();
+          it("summarize sidebar", () => {
+            H.visitQuestion(productsQuestionId);
+            cy.log("Open summarize sidebar");
+            H.summarize();
+            H.rightSidebar().within(() => {
+              cy.log("Summarize sidebar includes all column names in English");
+              Object.values(usedGermanFieldNames).forEach((row) => {
+                cy.findByText(row.msgstr).should("not.exist");
+                cy.findByText(row.msgid).should("be.visible");
+              });
             });
+          });
 
-            it("column headers", () => {
-              H.visitQuestion(productsQuestionId);
-              cy.findByTestId("table-header").within(() => {
-                usedGermanFieldNames.forEach((row) => {
-                  cy.findByText(row.msgid).should("not.exist");
-                  cy.findByText(row.msgstr).should("be.visible");
+          it("dashcard parameter mapper", () => {
+            openDashCardCardParameterMapper();
+
+            H.popover().within(() => {
+              Object.values(usedGermanFieldNames)
+                .filter((tr) => columnNamesWithTypeText.includes(tr.msgid))
+                .forEach((row) => {
+                  cy.findByText(row.msgstr).should("not.exist");
+                  cy.findByText(row.msgid).should("be.visible");
                 });
+            });
+          });
+        });
+
+        describe("when locale is German, column names ARE localized in", () => {
+          beforeEach(() => {
+            H.restore("translations-uploaded--normal-user-locale-is-de" as any);
+            cy.signInAsNormalUser();
+          });
+
+          it("column headers", () => {
+            H.visitQuestion(productsQuestionId);
+            cy.findByTestId("table-header").within(() => {
+              usedGermanFieldNames.forEach((row) => {
+                cy.findByText(row.msgid).should("not.exist");
+                cy.findByText(row.msgstr).should("be.visible");
+              });
+            });
+          });
+
+          it("filter popover", () => {
+            H.visitQuestion(productsQuestionId);
+            // Filter is 'Filter' in German
+            H.initiateAction("Filter");
+
+            Object.values(usedGermanFieldNames).forEach((row) => {
+              H.popover().within(() => {
+                cy.findByText(row.msgid).should("not.exist");
+                cy.findByText(row.msgstr).click();
+              });
+
+              // Within subsequent popover
+              H.popover().within(() => {
+                cy.findByText(row.msgid).should("not.exist");
+                cy.findByText(row.msgstr).click();
+                // Clicking the column name returns to the main popover
               });
             });
 
-            it("filter popover", () => {
-              H.visitQuestion(productsQuestionId);
-              // Filter is 'Filter' in German
-              H.initiateAction("Filter");
+            cy.log("Column names should be searchable");
+            H.popover().within(() => {
+              cy.findByPlaceholderText(/Finden/).type("er");
+              const translatedColumnNames = usedGermanFieldNames.map(
+                (n) => n.msgstr,
+              );
+              const nonMatches = translatedColumnNames.filter(
+                (n) => !/er/i.test(n),
+              );
+              const matches = translatedColumnNames.filter((n) =>
+                /er/i.test(n),
+              );
+              expect(nonMatches).to.have.length.greaterThan(0);
+              expect(matches).to.have.length.greaterThan(0);
+              nonMatches.forEach((n) =>
+                cy.findByText(new RegExp(n)).should("not.exist"),
+              );
+              matches.forEach((n) =>
+                cy.findByText(new RegExp(n)).should("be.visible"),
+              );
+            });
+          });
 
+          it("summarize sidebar", () => {
+            H.visitQuestion(productsQuestionId);
+            cy.log("Open summarize sidebar");
+            H.initiateAction("Zusammenfassen" as "Summarize");
+            H.rightSidebar().within(() => {
+              cy.log("Summarize sidebar includes all column names in German");
               Object.values(usedGermanFieldNames).forEach((row) => {
-                H.popover().within(() => {
-                  cy.findByText(row.msgid).should("not.exist");
-                  cy.findByText(row.msgstr).click();
-                });
-
-                // Within subsequent popover
-                H.popover().within(() => {
-                  cy.findByText(row.msgid).should("not.exist");
-                  cy.findByText(row.msgstr).click();
-                  // Clicking the column name returns to the main popover
-                });
+                cy.findByText(row.msgid).should("not.exist");
+                cy.findByText(row.msgstr).should("be.visible");
               });
 
               cy.log("Column names should be searchable");
-              H.popover().within(() => {
-                cy.findByPlaceholderText(/Finden/).type("er");
-                const translatedColumnNames = usedGermanFieldNames.map(
-                  (n) => n.msgstr,
-                );
-                const nonMatches = translatedColumnNames.filter(
-                  (n) => !/er/i.test(n),
-                );
-                const matches = translatedColumnNames.filter((n) =>
-                  /er/i.test(n),
-                );
-                expect(nonMatches).to.have.length.greaterThan(0);
-                expect(matches).to.have.length.greaterThan(0);
-                nonMatches.forEach((n) =>
-                  cy.findByText(new RegExp(n)).should("not.exist"),
-                );
-                matches.forEach((n) =>
-                  cy.findByText(new RegExp(n)).should("be.visible"),
-                );
-              });
+              cy.findByPlaceholderText(/Finden/).type("kat");
+              cy.findByText("Erstellt am").should("not.exist");
+              cy.findByText("Kategorie").should("be.visible");
             });
+          });
 
-            it("summarize sidebar", () => {
-              H.visitQuestion(productsQuestionId);
-              cy.log("Open summarize sidebar");
-              H.initiateAction("Zusammenfassen" as "Summarize");
-              H.rightSidebar().within(() => {
-                cy.log("Summarize sidebar includes all column names in German");
-                Object.values(usedGermanFieldNames).forEach((row) => {
+          it("dashcard parameter mapper", () => {
+            openDashCardCardParameterMapper();
+
+            H.popover().within(() => {
+              Object.values(usedGermanFieldNames)
+                .filter((tr) => columnNamesWithTypeText.includes(tr.msgid))
+                .forEach((row) => {
                   cy.findByText(row.msgid).should("not.exist");
                   cy.findByText(row.msgstr).should("be.visible");
                 });
-
-                cy.log("Column names should be searchable");
-                cy.findByPlaceholderText(/Finden/).type("kat");
-                cy.findByText("Erstellt am").should("not.exist");
-                cy.findByText("Kategorie").should("be.visible");
-              });
-            });
-
-            it("dashcard parameter mapper", () => {
-              openDashCardCardParameterMapper();
-
-              H.popover().within(() => {
-                Object.values(usedGermanFieldNames)
-                  .filter((tr) => columnNamesWithTypeText.includes(tr.msgid))
-                  .forEach((row) => {
-                    cy.findByText(row.msgid).should("not.exist");
-                    cy.findByText(row.msgstr).should("be.visible");
-                  });
-              });
             });
           });
         });
