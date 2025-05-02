dd239e128d99bba548ab60a4f449bdee395c52dc
Tyler Plude <tsplude@gmail.com>
2025-05-02 01:30:21 -0700

    [VISUALIZER] Fix card duplication spinning (#57510)
    * [VISUALIZER] Fix spinning card duplication
    
    * ts fix
    
    ---------
    
    Co-authored-by: Sébastien <sebastien@metabase.com>

--- frontend/src/metabase/dashboard/components/DashCard/DashCardActionsPanel/use-duplicate-dashcard.ts
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardActionsPanel/use-duplicate-dashcard.ts
@@ -13,10 +13,12 @@ import {
 } from "metabase/dashboard/selectors";
 import {
   generateTemporaryDashcardId,
+  isQuestionDashCard,
   isVirtualDashCard,
 } from "metabase/dashboard/utils";
 import { getPositionForNewDashCard } from "metabase/lib/dashboard_grid";
 import { useDispatch, useSelector } from "metabase/lib/redux";
+import { isVisualizerDashboardCard } from "metabase/visualizer/utils";
 import type { Dashboard, DashboardCard } from "metabase-types/api";
 
 export function useDuplicateDashCard({
@@ -59,7 +61,32 @@ export function useDuplicateDashCard({
 
     // We don't have card (question) data for virtual dashcards (text, heading, link, action)
     if (!isVirtualDashCard(dashcard)) {
+      // Fetch data for the main card
       dispatch(fetchCardData(dashcard.card, { ...dashcard, id: newId }));
+
+      // For visualizer cards, also fetch data for all referenced data sources
+      if (isVisualizerDashboardCard(dashcard)) {
+        // Get the visualization definition
+        const visualizerDef = dashcard.visualization_settings.visualization;
+        if (visualizerDef && visualizerDef.columnValuesMapping) {
+          // Fetch data for each referenced card in the visualizer
+          const seriesCards = dashcard.series || [];
+
+          // Fetch data for all series cards
+          seriesCards.forEach((card) => {
+            dispatch(fetchCardData(card, { ...dashcard, id: newId }));
+          });
+        }
+      } else if (
+        isQuestionDashCard(dashcard) &&
+        dashcard.series &&
+        dashcard.series.length > 0
+      ) {
+        // For standard multi-series cards, fetch data for each series card
+        dashcard.series.forEach((card) => {
+          dispatch(fetchCardData(card, { ...dashcard, id: newId }));
+        });
+      }
     }
 
     trackDashcardDuplicated(dashboard.id);
