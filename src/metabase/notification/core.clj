7ead7d96e3c864d0fde8c808405483ba1104f58f
Ngoc Khuat <qn.khuat@gmail.com>
2025-05-02 13:52:02 +0700

    Sample event info with table value (#57474)
    * sample with table info
    
    * remove debugging code
    
    * wiring up
    
    * missing imports

--- src/metabase/notification/core.clj
+++ src/metabase/notification/core.clj
@@ -2,6 +2,7 @@
   "Core functionality for notifications."
   (:require
    [metabase.notification.payload.core :as notification.payload]
+   [metabase.notification.payload.impl.system-event :as notification.payload.system-event]
    [metabase.notification.seed :as notification.seed]
    [metabase.notification.send :as notification.send]
    [metabase.notification.task.send :as notification.task.send]
@@ -15,6 +16,8 @@
  [notification.payload
   notification-payload
   notification-payload-schema]
+ [notification.payload.system-event
+  sample-payload]
  [notification.task.send
   update-send-notification-triggers-timezone!]
  [notification.seed
