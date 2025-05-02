5983fea1edd8b093240d31b364812b562abf8f9a
Braden Shepherdson <braden@metabase.com>
2025-05-02 08:50:47 -0400

    cleanup

--- src/metabase/lib_be/init.clj
+++ src/metabase/lib_be/init.clj
@@ -3,7 +3,3 @@
    [metabase.lib-be.settings]
    [metabase.lib-be.task.backfill-card-metadata-analysis]
    [metabase.lib-be.task.backfill-entity-ids]))
-
-;; XXX: START HERE: Figure out how to log COUNT(*) by metadata_analysis_state to Prometheus periodically.
-;; How often is the right amount? Should I be logging it after each run? Does it poll these metrics and I just have
-;; to report them?
