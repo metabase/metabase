d38c64105e42b265bcb11111cb5e31031259a03b
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-02 19:57:16 -0400

    wip: writing unit tests for TitleAndDescription

--- frontend/src/metabase/components/TitleAndDescription/tests/premium.unit.spec.tsx
+++ frontend/src/metabase/components/TitleAndDescription/tests/premium.unit.spec.tsx
@@ -2,28 +2,28 @@ import userEvent from "@testing-library/user-event";
 
 import { screen } from "__support__/ui";
 
-import { setup as baseSetup } from "./setup";
+import { type SetupOpts, setup as baseSetup } from "./setup";
 
 function setup(opts: SetupOpts = {}) {
   return baseSetup({
     hasEnterprisePlugins: true,
-    tokenFeatures: { "content-translation": true },
+    tokenFeatures: { content_translation: true },
     ...opts,
   });
 }
 
 describe("TitleAndDescription (with mocked useTranslateContent)", () => {
-  it("displays 'Sample text-translated' and 'Sample description-translated' correctly", async () => {
-    setup();
+  it("displays translated question title and description", async () => {
+    setup({ localeCode: "de" });
 
     expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
-      "Sample text", // FIXME use translated text here
+      "Bespieltext",
     );
 
     await userEvent.hover(screen.getByLabelText("info icon"));
     expect(
       await screen.findByRole("tooltip", {
-        name: "Sample description", // FIXME use translated text here
+        name: "Beispielbeschreibung",
       }),
     ).toBeInTheDocument();
   });
