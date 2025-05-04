d2b18782dff58ab1663a6dbc47a289b3abf14d5e
Timofei <timofei@metabase.com>
2025-05-04 13:08:48 +0400

    Esbuild poc

--- frontend/src/metabase/visualizations/lib/exports-branding-utils.tsx
+++ frontend/src/metabase/visualizations/lib/exports-branding-utils.tsx
@@ -1,7 +1,7 @@
 import ReactDOMServer from "react-dom/server";
 import { t } from "ttag";
 
-import BrandingLogo from "metabase/public/components/EmbedFrame/LogoBadge/metabase_logo_with_text.svg?component";
+import BrandingLogo from "metabase/public/components/EmbedFrame/LogoBadge/metabase_logo_with_text.svg";
 
 type BrandingSize = "xs" | "s" | "m" | "l" | "xl" | "xxl" | "xxxl";
 
