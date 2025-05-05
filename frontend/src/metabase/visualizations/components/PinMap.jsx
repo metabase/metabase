8c962b1bb5aabc1270923ae7a59a7e42d64ea1ba
Nicolò Pretto <info@npretto.com>
2025-05-05 16:30:54 +0200

    fix(sdk): manual fixes for module level ttag calls causing broken translations

--- frontend/src/metabase/visualizations/components/PinMap.jsx
+++ frontend/src/metabase/visualizations/components/PinMap.jsx
@@ -30,7 +30,7 @@ const MAP_COMPONENTS_BY_TYPE = {
 };
 
 export default class PinMap extends Component {
-  static uiName = t`Pin Map`;
+  static getUiName = () => t`Pin Map`;
   static identifier = "pin_map";
   static iconName = "pinmap";
 
--- frontend/src/metabase/visualizations/visualizations/Scalar/Scalar.jsx
+++ frontend/src/metabase/visualizations/visualizations/Scalar/Scalar.jsx
@@ -39,7 +39,7 @@ function legacyScalarSettingsToFormatOptions(settings) {
 // Scalar visualization shows a single number
 // Multiseries Scalar is transformed to a Funnel
 export class Scalar extends Component {
-  static uiName = t`Number`;
+  static getUiName = () => t`Number`;
   static identifier = "scalar";
   static iconName = "number";
   static canSavePng = false;
