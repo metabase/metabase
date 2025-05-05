6645f5cd6ea5abed80d0e11c292387c6305e2b13
Nick Fitzpatrick <nickfitz.582@gmail.com>
2025-05-05 15:31:28 +0530

    adding unit test

--- frontend/src/metabase/visualizations/components/Visualization/Watermark/Watermark.tsx
+++ frontend/src/metabase/visualizations/components/Visualization/Watermark/Watermark.tsx
@@ -2,11 +2,18 @@ import { isVirtualDashCard } from "metabase/dashboard/utils";
 import type { BaseDashboardCard, Card } from "metabase-types/api";
 
 import S from "./Watermark.module.css";
+import watermark from "./watermark.svg";
 
 export const Watermark = ({ card }: { card: Card | BaseDashboardCard }) => {
   if (isVirtualDashCard(card)) {
     return null;
   }
 
-  return <div className={S.Root} data-testid="visualization-watermark" />;
+  return (
+    <div
+      className={S.Root}
+      data-testid="visualization-watermark"
+      style={{ backgroundImage: `url(${watermark})` }}
+    />
+  );
 };
