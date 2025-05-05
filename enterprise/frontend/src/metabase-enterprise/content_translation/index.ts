919f2ebbf93823e6d5a5d7d94bee195b16f7626f
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-05 10:44:16 -0400

    wip

--- enterprise/frontend/src/metabase-enterprise/content_translation/index.ts
+++ enterprise/frontend/src/metabase-enterprise/content_translation/index.ts
@@ -2,7 +2,7 @@ import { PLUGIN_CONTENT_TRANSLATION } from "metabase/plugins";
 import { hasPremiumFeature } from "metabase-enterprise/settings";
 
 import { ContentTranslationConfiguration } from "./components";
-import { useTranslateContent } from "./hooks";
+import { useTranslateContent } from "./use-translate-content";
 
 if (hasPremiumFeature("content_translation")) {
   Object.assign(PLUGIN_CONTENT_TRANSLATION, {
