c5e465fa78aa56c37c6be94703257108160fdd01
Phoomparin Mano <phoomparin@gmail.com>
2025-05-07 01:11:04 +0700

    use getSdkStore instead

---
+++ resources/frontend_client/app/main.457b853515e10c35.hot-update.js
@@ -0,0 +1,9 @@
+"use strict";
+this["webpackHotUpdatemetabase_embed"]('main', {},function(__webpack_require__) {
+// webpack/runtime/get_full_hash
+!function() {
+__webpack_require__.h = function() { return "c1a30a72af4792e6"; }
+}();
+
+}
+);
