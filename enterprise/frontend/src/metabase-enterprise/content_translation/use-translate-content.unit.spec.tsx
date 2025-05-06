e5fff641d42e6c1f530dc5b3ffb6c86eba58cbe2
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-06 10:06:46 -0400

    Use setupContentTranslationEndpoints

--- enterprise/frontend/src/metabase-enterprise/content_translation/use-translate-content.unit.spec.tsx
+++ enterprise/frontend/src/metabase-enterprise/content_translation/use-translate-content.unit.spec.tsx
@@ -1,7 +1,22 @@
+<<<<<<< HEAD
 import { setupEnterprisePlugins } from "__support__/enterprise";
 import { setupContentTranslationEndpoints } from "__support__/server-mocks/content-translation";
+||||||| parent of 61061e6f165 (Use setupContentTranslationEndpoints)
+import { setupEnterprisePlugins } from "__support__/enterprise";
+=======
+import { setupContentTranslationEndpoints } from "__support__/server-mocks/content-translation";
+>>>>>>> 61061e6f165 (Use setupContentTranslationEndpoints)
 import { mockSettings } from "__support__/settings";
+<<<<<<< HEAD
 import { renderWithProviders, screen } from "__support__/ui";
+||||||| parent of 61061e6f165 (Use setupContentTranslationEndpoints)
+import { renderHookWithProviders } from "__support__/ui";
+import { useListContentTranslationsQuery } from "metabase-enterprise/api";
+import type { TokenFeatures } from "metabase-types/api";
+=======
+import { renderHookWithProviders } from "__support__/ui";
+import type { TokenFeatures } from "metabase-types/api";
+>>>>>>> 61061e6f165 (Use setupContentTranslationEndpoints)
 import type { RetrievedDictionaryArrayRow } from "metabase-types/api/content-translation";
 import {
   createMockTokenFeatures,
@@ -16,6 +31,13 @@ interface SetupOpts {
   /** An untranslated string */
   msgid: string | null | undefined;
   translations: RetrievedDictionaryArrayRow[] | undefined;
+<<<<<<< HEAD
+||||||| parent of 61061e6f165 (Use setupContentTranslationEndpoints)
+  tokenFeatures?: Partial<TokenFeatures>;
+  hasEnterprisePlugins?: boolean;
+=======
+  tokenFeatures?: Partial<TokenFeatures>;
+>>>>>>> 61061e6f165 (Use setupContentTranslationEndpoints)
 }
 
 const sampleSpanishDictionary: RetrievedDictionaryArrayRow[] = [
@@ -27,6 +49,7 @@ const sampleSpanishDictionary: RetrievedDictionaryArrayRow[] = [
   },
 ];
 
+<<<<<<< HEAD
 const TestComponent = ({ msgid }: { msgid?: string | null }) => {
   const tc = useTranslateContent();
   const msgstr = tc(msgid);
@@ -36,6 +59,24 @@ const TestComponent = ({ msgid }: { msgid?: string | null }) => {
 function setup({ locale, msgid, translations }: SetupOpts) {
   setupEnterprisePlugins();
   setupContentTranslationEndpoints({ dictionary: translations });
+||||||| parent of 61061e6f165 (Use setupContentTranslationEndpoints)
+function setup({
+  locale,
+  msgid,
+  translations,
+  tokenFeatures = {},
+  hasEnterprisePlugins = true,
+}: SetupOpts) {
+  if (hasEnterprisePlugins) {
+    setupEnterprisePlugins();
+  }
+  mockUseListContentTranslationsQuery.mockReturnValue({
+    data: translations ? { data: translations } : undefined,
+  });
+=======
+function setup({ locale, msgid, translations, tokenFeatures = {} }: SetupOpts) {
+  setupContentTranslationEndpoints({ dictionary: translations });
+>>>>>>> 61061e6f165 (Use setupContentTranslationEndpoints)
 
   const storeInitialState = createMockState({
     settings: mockSettings({
@@ -50,8 +91,20 @@ function setup({ locale, msgid, translations }: SetupOpts) {
 }
 
 describe("useTranslateContent", () => {
+<<<<<<< HEAD
   it("should return the original string when dictionary is undefined", async () => {
     setup({
+||||||| parent of 61061e6f165 (Use setupContentTranslationEndpoints)
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return the original string when dictionary is undefined", () => {
+    const { result } = setup({
+=======
+  it("should return the original string when dictionary is undefined", () => {
+    const { result } = setup({
+>>>>>>> 61061e6f165 (Use setupContentTranslationEndpoints)
       msgid: "Hello World",
       locale: "es",
       translations: undefined,
@@ -86,8 +139,34 @@ describe("useTranslateContent", () => {
     expect(await screen.findByText("Hello? World?")).toBeInTheDocument();
   });
 
+<<<<<<< HEAD
   it("should return the original string when translation is an empty string", async () => {
     setup({
+||||||| parent of 61061e6f165 (Use setupContentTranslationEndpoints)
+  it("should return the original string when no translation is found", () => {
+    const { result } = setup({
+      msgid: "Hello World!!!",
+      locale: "es",
+      translations: sampleSpanishDictionary,
+    });
+    expect(result.current).toBe("Hello World!!!");
+  });
+
+  it("should return the original string when translation is an empty string", () => {
+    const { result } = setup({
+=======
+  it("should return the original string when no translation is found", () => {
+    const { result } = setup({
+      msgid: "Hello? World?",
+      locale: "es",
+      translations: sampleSpanishDictionary,
+    });
+    expect(result.current).toBe("Hello? World?");
+  });
+
+  it("should return the original string when translation is an empty string", () => {
+    const { result } = setup({
+>>>>>>> 61061e6f165 (Use setupContentTranslationEndpoints)
       msgid: "Hello World",
       locale: "es",
       translations: [
