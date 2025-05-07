ad120b2f803d5422e7427819e34b5d728606cf78
Timofei <timofei@metabase.com>
2025-05-07 19:06:27 +0400

    Remove empty-state images from bundle

--- frontend/src/metabase/visualizations/components/EmptyVizState/EmptyVizState.tsx
+++ frontend/src/metabase/visualizations/components/EmptyVizState/EmptyVizState.tsx
@@ -67,13 +67,15 @@ export const EmptyVizState = ({
       justify="center"
       data-testid="visualization-placeholder"
     >
-      <Box maw="20rem" mb="3rem">
-        <img
-          src={imgSrc}
-          alt={c("{0} refers to the chart type")
-            .t`${emptyVizChart} chart example illustration`}
-        />
-      </Box>
+      {imgSrc && (
+        <Box maw="20rem" mb="3rem">
+          <img
+            src={imgSrc}
+            alt={c("{0} refers to the chart type")
+              .t`${emptyVizChart} chart example illustration`}
+          />
+        </Box>
+      )}
       <Stack gap="0.75rem" maw="25rem" ta="center" align="center">
         {isDocsCTA && (
           <>
