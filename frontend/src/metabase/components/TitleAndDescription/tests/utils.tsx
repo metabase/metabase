d0daa363d965a351cbc1a555112d3ed41ccb920a
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-06 23:01:14 -0400

    Refactor, divide test file

---
+++ frontend/src/metabase/components/TitleAndDescription/tests/utils.tsx
@@ -0,0 +1,74 @@
+import userEvent from "@testing-library/user-event";
+
+import { setupEnterprisePlugins } from "__support__/enterprise";
+import { setupContentTranslationEndpoints } from "__support__/server-mocks/content-translation";
+import { mockSettings } from "__support__/settings";
+import { renderWithProviders, screen } from "__support__/ui";
+import type {
+  RetrievedDictionaryArrayRow,
+  TokenFeatures,
+} from "metabase-types/api";
+import {
+  createMockTokenFeatures,
+  createMockUser,
+} from "metabase-types/api/mocks";
+import { createMockState } from "metabase-types/store/mocks";
+
+import TitleAndDescription from "../TitleAndDescription";
+
+import { type SetupOpts, sampleDictionary } from "./constants";
+
+export interface SetupOpts {
+  localeCode: string;
+  hasEnterprisePlugins?: boolean;
+  tokenFeatures?: Partial<TokenFeatures>;
+  dictionary?: RetrievedDictionaryArrayRow[];
+}
+
+export const setup = ({
+  localeCode,
+  hasEnterprisePlugins,
+  tokenFeatures = {},
+  dictionary = [],
+}: SetupOpts) => {
+  const storeInitialState = createMockState({
+    settings: mockSettings({
+      "token-features": createMockTokenFeatures(tokenFeatures),
+    }),
+    currentUser: createMockUser({ locale: localeCode }),
+  });
+
+  if (hasEnterprisePlugins) {
+    setupEnterprisePlugins();
+  }
+
+  setupContentTranslationEndpoints({ dictionary });
+
+  return renderWithProviders(
+    <TitleAndDescription
+      title={dictionary[0].msgid}
+      description={dictionary[1].msgid}
+    />,
+    { storeInitialState },
+  );
+};
+
+export const assertStringsArePresent = async ({
+  shouldBeTranslated,
+}: {
+  shouldBeTranslated: boolean;
+}) => {
+  const key = shouldBeTranslated ? "msgstr" : "msgid";
+  expect(
+    await screen.findByRole("heading", {
+      name: sampleDictionary[0][key],
+    }),
+  ).toBeInTheDocument();
+
+  await userEvent.hover(screen.getByLabelText("info icon"));
+  expect(
+    await screen.findByRole("tooltip", {
+      name: sampleDictionary[1][key],
+    }),
+  ).toBeInTheDocument();
+};
