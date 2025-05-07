ea77125b51e543c0e087cefa865d5fa51965e30f
Phoomparin Mano <phoomparin@gmail.com>
2025-05-07 23:07:11 +0700

    remove hot files

--- resources/frontend_client/app/main.457b853515e10c35.hot-update.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-this["webpackHotUpdatemetabase_embed"]('main', {},function(__webpack_require__) {
-// webpack/runtime/get_full_hash
-!function() {
-__webpack_require__.h = function() { return "c1a30a72af4792e6"; }
-}();
-
-}
-);
--- resources/frontend_client/app/main.ce27633267cc1f69.hot-update.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-this["webpackHotUpdatemetabase_embed"]('main', {},function(__webpack_require__) {
-// webpack/runtime/get_full_hash
-!function() {
-__webpack_require__.h = function() { return "b55eb2b0e1d416c6"; }
-}();
-
-}
-);
