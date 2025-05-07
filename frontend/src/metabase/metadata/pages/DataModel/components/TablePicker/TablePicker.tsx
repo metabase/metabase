e120852f8a773217910cefec0e03ba814d258328
Romeo Van Snick <romeo@romeovansnick.be>
2025-05-07 15:27:09 +0200

    Enable virtualization

--- frontend/src/metabase/metadata/pages/DataModel/components/TablePicker/TablePicker.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/components/TablePicker/TablePicker.tsx
@@ -1,5 +1,6 @@
+import { useVirtualizer } from "@tanstack/react-virtual";
 import cx from "classnames";
-import { useDeferredValue, useState } from "react";
+import { useDeferredValue, useRef, useState } from "react";
 
 import Link from "metabase/core/components/Link";
 import { Box, Flex, Icon, Stack } from "metabase/ui";
@@ -21,6 +22,7 @@ export function TablePicker(props: {
 
   const { isExpanded, toggle } = useExpandedState(props);
   const { tree } = useTableLoader(props);
+
   const flat = flatten(tree, isExpanded);
 
   return (
@@ -29,13 +31,13 @@ export function TablePicker(props: {
         <SearchInput value={searchValue} onChange={setSearchValue} />
       </Box>
 
-      <Box className={S.tablePickerContent} px="xl" pb="lg">
-        {deferredSearchValue === "" ? (
-          <Results items={flat} toggle={toggle} isExpanded={isExpanded} />
-        ) : (
+      {deferredSearchValue === "" ? (
+        <Results items={flat} toggle={toggle} isExpanded={isExpanded} />
+      ) : (
+        <Box className={S.tablePickerContent} px="xl" pb="lg">
           <SearchResults searchValue={searchValue} />
-        )}
-      </Box>
+        </Box>
+      )}
     </Stack>
   );
 }
@@ -49,38 +51,78 @@ function Results({
   toggle: (key: string) => void;
   isExpanded: (key: string) => boolean;
 }) {
-  return items.map((item) => {
-    if (item.type === "root") {
-      return null;
-    }
-    return (
-      <Box className={cx(S.item, S[item.type])} key={item.key}>
-        <Link
-          to={getUrl({
-            databaseId: undefined,
-            fieldId: undefined,
-            schemaId: undefined,
-            tableId: undefined,
-            ...item.value,
-          })}
-          onClick={() => toggle(item.key)}
-        >
-          <Flex align="center" gap="sm" direction="row" mb="xs">
-            {hasChildren(item.type) ? (
-              <Icon
-                name="chevronright"
-                size={10}
-                color="var(--mb-color-text-light)"
-                className={cx(S.chevron, {
-                  [S.expanded]: isExpanded(item.key),
-                })}
-              />
-            ) : null}
-            <Icon name={getIconForType(item.type)} />
-            {item.label}
-          </Flex>
-        </Link>
-      </Box>
-    );
+  const ref = useRef<HTMLDivElement>(null);
+
+  const virtual = useVirtualizer({
+    estimateSize: () => 36,
+    count: items.length,
+    getScrollElement: () => ref.current,
+    overscan: 5,
   });
+
+  return (
+    <Box
+      ref={ref}
+      style={{
+        position: "relative",
+        overflow: "auto",
+        height: "100%",
+      }}
+      px="xl"
+      pb="lg"
+    >
+      <Box
+        style={{
+          height: virtual.getTotalSize(),
+        }}
+      >
+        {virtual.getVirtualItems().map((virtualItem) => {
+          const item = items[virtualItem.index];
+          if (item.type === "root") {
+            return null;
+          }
+          return (
+            <Box
+              className={cx(S.item, S[item.type])}
+              key={item.key}
+              style={{
+                position: "absolute",
+                top: virtualItem.start,
+                minHeight: virtualItem.size,
+              }}
+            >
+              <Link
+                to={getUrl({
+                  databaseId: undefined,
+                  fieldId: undefined,
+                  schemaId: undefined,
+                  tableId: undefined,
+                  ...item.value,
+                })}
+                onClick={() => {
+                  toggle(item.key);
+                  virtual.measure();
+                }}
+              >
+                <Flex align="center" gap="sm" direction="row" mb="xs">
+                  {hasChildren(item.type) ? (
+                    <Icon
+                      name="chevronright"
+                      size={10}
+                      color="var(--mb-color-text-light)"
+                      className={cx(S.chevron, {
+                        [S.expanded]: isExpanded(item.key),
+                      })}
+                    />
+                  ) : null}
+                  <Icon name={getIconForType(item.type)} />
+                  {item.label}
+                </Flex>
+              </Link>
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 }
