4a7c7f4e199ab12888536664a42344229696e162
Phoomparin Mano <phoomparin@gmail.com>
2025-05-07 20:40:08 +0700

    setup a clean entry point

---
+++ frontend/src/metabase/app-embed-sdk.js
@@ -0,0 +1,7 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE-EMBEDDING.txt', which is part of this source code package.
+ */
+
+// eslint-disable-next-line no-console -- for debugging only
+console.log("app-embed-sdk entry point");
