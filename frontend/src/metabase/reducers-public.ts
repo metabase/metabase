a615227eee56a885627b4a5f92e7428ea44c185b
Phoomparin Mano <phoomparin@gmail.com>
2025-05-07 22:55:02 +0700

    revert sdk plugin fields

--- frontend/src/metabase/reducers-public.ts
+++ frontend/src/metabase/reducers-public.ts
@@ -1,9 +1,7 @@
 // Reducers needed for public questions and dashboards
 
-import { PLUGIN_EMBEDDING_IFRAME_SDK } from "./plugins";
 import { commonReducers } from "./reducers-common";
 
 export const publicReducers = {
   ...commonReducers,
-  ...PLUGIN_EMBEDDING_IFRAME_SDK.additionalPublicReducerPlugins,
 };
