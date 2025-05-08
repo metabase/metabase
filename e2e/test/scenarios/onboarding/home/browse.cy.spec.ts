6fa79dd66f24089c6704ab56ac201c8763fdfecb
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-05-08 11:51:54 +0200

    Cover initiating a new model with e2e tests

--- e2e/test/scenarios/onboarding/home/browse.cy.spec.ts
+++ e2e/test/scenarios/onboarding/home/browse.cy.spec.ts
@@ -102,6 +102,20 @@ H.describeWithSnowplow("scenarios > browse", () => {
     });
   });
 
+  it("tracks when a new model creation is initiated", () => {
+    cy.visit("/browse/models");
+    cy.findByTestId("browse-models-header")
+      .findByLabelText("Create a new model")
+      .should("be.visible")
+      .click();
+    cy.location("pathname").should("eq", "/model/new");
+    H.expectNoBadSnowplowEvents();
+    H.expectGoodSnowplowEvent({
+      event: "plus_button_clicked",
+      triggered_from: "model",
+    });
+  });
+
   it("browsing to a database only triggers a request for schemas for that specific database", () => {
     cy.intercept("GET", `/api/database/${SAMPLE_DB_ID}/schemas`).as(
       "schemasForSampleDatabase",
