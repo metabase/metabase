0bc5c5d03526da1a0910fde890b190a25d855ea7
Mike Appleby <86076+appleby@users.noreply.github.com>
2025-05-09 10:35:11 -0700

    Add some tests for non-booleans to boolean_to_comparison_test.clj

--- test/metabase/driver/sql/query_processor/boolean_to_comparison_test.clj
+++ test/metabase/driver/sql/query_processor/boolean_to_comparison_test.clj
@@ -7,25 +7,40 @@
 
 (def ^:private true-value  [:value true  {:base_type :type/Boolean}])
 (def ^:private false-value [:value false {:base_type :type/Boolean}])
+(def ^:private int-value   [:value 1     {:base_type :type/Integer}])
 
 (defn- basic-filter-query [expression]
   (mt/mbql-query venues
-    {:expressions {"T" true-value, "F" false-value}
-     :fields      [[:expression "T"] [:expression "F"]]
+    {:expressions {"T" true-value, "F" false-value, "I" int-value}
+     :fields      [[:expression "T"] [:expression "F"] [:expression "I"]]
      :filter      expression}))
 
 (deftest ^:parallel boolean->comparison-test
-  (are [expression] (binding [sql.qp/*inner-query* (:query (basic-filter-query expression))]
-                      (= [:= expression true]
-                         (sql.qp.boolean-to-comparison/boolean->comparison expression)))
-    false
-    true
-    true-value
-    false-value
-    [:expression "T"]
-    [:expression "F"]
-    [:field "some-bool" {:base-type :type/Boolean}]
-    [:field 123 {:base-type :type/Boolean}]))
+  (testing "boolean expressions"
+    (are [clause] (binding [sql.qp/*inner-query* (:query (basic-filter-query clause))]
+                    (= [:= clause true]
+                       (sql.qp.boolean-to-comparison/boolean->comparison clause)))
+      false
+      true
+      true-value
+      false-value
+      [:value true nil]
+      [:expression "T"]
+      [:expression "F"]
+      [:field "some-bool" {:base-type :type/Boolean}]
+      [:field 123 {:base-type :type/Boolean}]))
+
+  (testing "non-boolean expressions"
+    (are [clause] (binding [sql.qp/*inner-query* (:query (basic-filter-query clause))]
+                    (= clause
+                       (sql.qp.boolean-to-comparison/boolean->comparison clause)))
+      0
+      1
+      "not a boolean"
+      [:value 1 nil]
+      [:expression "I"]
+      [:field "some-int" {:base-type :type/Integer}]
+      [:field 123 {:base-type :type/Integer}])))
 
 (deftest ^:parallel case-boolean->comparison
   (are [clause expected] (= expected
@@ -34,9 +49,13 @@
      [[true true]
       [true-value true]
       [false false]
-      [false-value false]]]
+      [false-value false]
+      [[:= true false] false]
+      [[:= 1 2] false]]]
     [:case
      [[[:= true true] true]
       [[:= true-value true] true]
       [[:= false true] false]
-      [[:= false-value true] false]]]))
+      [[:= false-value true] false]
+      [[:= true false] false]
+      [[:= 1 2] false]]]))
