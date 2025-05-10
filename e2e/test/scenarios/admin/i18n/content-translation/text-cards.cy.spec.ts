548c50defbff1aab04e817484cf3ed7829974c0f
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-10 09:16:00 -0400

    Simplify text card e2e tests

--- e2e/test/scenarios/admin/i18n/content-translation/text-cards.cy.spec.ts
+++ e2e/test/scenarios/admin/i18n/content-translation/text-cards.cy.spec.ts
@@ -1,14 +1,9 @@
-import { DictionaryArray } from "metabase/i18n/types";
-import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
-import {
-  NORMAL_USER_ID,
-  ORDERS_DASHBOARD_ID,
-} from "e2e/support/cypress_sample_instance_data";
+import { NORMAL_USER_ID } from "e2e/support/cypress_sample_instance_data";
+import type { DictionaryArray } from "metabase/i18n/types";
+
 import { uploadTranslationDictionary } from "./helpers/e2e-content-translation-helpers";
 
 const { H } = cy;
-
-// NOTE: Claude wrote most of this. I still need to check that it works
 
 describe("scenarios > dashboard > content translation of text cards and headings", () => {
   const textCardTranslations: DictionaryArray = [
@@ -29,7 +24,7 @@ describe("scenarios > dashboard > content translation of text cards and headings
     cy.signInAsAdmin();
     H.setTokenFeatures("all");
 
-    cy.log("Upload translation dictionary with our test translations");
+    cy.log("Upload sample translation dictionary");
     uploadTranslationDictionary(textCardTranslations);
 
     cy.request("PUT", `/api/user/${NORMAL_USER_ID}`, { locale: "de" });
@@ -54,6 +49,7 @@ describe("scenarios > dashboard > content translation of text cards and headings
     });
   });
 
+  // TODO: Double check that this is correct
   it("should translate text in dashboard heading cards", () => {
     H.createDashboard().then(({ body: { id: dashboardId } }) => {
       H.visitDashboard(dashboardId);
@@ -76,59 +72,6 @@ describe("scenarios > dashboard > content translation of text cards and headings
 
       // Cancel editing without saving changes
       cy.findByText("Cancel").click();
-    });
-  });
-
-  it("should preserve parameter substitutions when translating text cards", () => {
-    // Create a new dashboard with a parameter
-    H.createDashboard().then(({ body: { id: dashboardId } }) => {
-      H.visitDashboard(dashboardId);
-      H.editDashboard();
-
-      // Add a text card with a category parameter
-      H.addTextBoxWhileEditing("Category is {{category}}", {
-        parseSpecialCharSequences: false,
-      });
-
-      // Add a heading with a parameter
-      H.addHeadingWhileEditing("Category is {{category}}", {
-        parseSpecialCharSequences: false,
-      });
-
-      // Set up a filter
-      H.setFilter("Text", "Is");
-
-      // Connect filter to text cards
-      H.getDashboardCard(0).findByText("Select…").click();
-      H.popover().findByText("category").click();
-
-      H.getDashboardCard(1).findByText("Select…").click();
-      H.popover().findByText("category").click();
-
-      // Save the dashboard
-      H.saveDashboard();
-
-      // Add a filter value
-      H.filterWidget().click();
-      H.dashboardParametersPopover().within(() =>
-        H.fieldValuesCombobox().type("Gadget"),
-      );
-      cy.button("Add filter").click();
-
-      // Verify the translation preserves the parameter
-      H.getDashboardCard(0)
-        .findByText("Kategorie ist Gadget")
-        .should("be.visible");
-      H.getDashboardCard(1)
-        .findByText("Kategorie ist Gadget")
-        .should("be.visible");
-
-      // When we edit, the original text is shown
-      H.editDashboard();
-      H.getDashboardCard(0).click();
-      cy.get("textarea").should("have.value", "Category is {{category}}");
-      H.getDashboardCard(1).click();
-      cy.get("input").should("have.value", "Category is {{category}}");
     });
   });
 });
