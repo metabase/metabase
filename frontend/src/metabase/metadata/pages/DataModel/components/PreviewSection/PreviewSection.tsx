ca74aa4e3079eef06ba29d6765422838fc187226
Romeo Van Snick <romeo@romeovansnick.be>
2025-05-12 16:47:13 +0200

    Add preview type selector

--- frontend/src/metabase/metadata/pages/DataModel/components/PreviewSection/PreviewSection.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/components/PreviewSection/PreviewSection.tsx
@@ -1,15 +1,44 @@
-import { Box, Card, Text } from "metabase/ui";
+import { useMemo, useState } from "react";
+import { t } from "ttag";
+
+import { Box, Card, SegmentedControl, Text } from "metabase/ui";
 import type { FieldId } from "metabase-types/api";
 
 interface Props {
   fieldId?: FieldId;
 }
 
-export const PreviewSection = ({ fieldId }: Props) => {
+type PreviewType = ReturnType<typeof getTypeSelectorData>[number]["value"];
+
+export const PreviewSection = (_props: Props) => {
+  const [previewType, setPreviewType] = useState<PreviewType>("table");
+
   return (
     <Card bg="white" h="100%" px="lg" py="md" shadow="xs">
       <Text fw="bold">Field preview</Text>
-      <Box>Field: {fieldId}</Box>
+      <PreviewTypeSelector value={previewType} onChange={setPreviewType} />
+
+      {previewType === "table" && <Box></Box>}
     </Card>
   );
 };
+
+function getTypeSelectorData() {
+  return [
+    { label: t`Table`, value: "table" as const },
+    { label: t`Detail`, value: "detail" as const },
+    { label: t`Filtering`, value: "filtering" as const },
+  ];
+}
+
+function PreviewTypeSelector({
+  value,
+  onChange,
+}: {
+  value: PreviewType;
+  onChange: (value: PreviewType) => void;
+}) {
+  const data = useMemo(getTypeSelectorData, []);
+
+  return <SegmentedControl data={data} value={value} onChange={onChange} />;
+}
