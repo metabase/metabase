be9271fb0395aeb04f64b3e032e51a15486f4478
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-05-13 15:26:58 -0400

    Fix params used in dashboard endpoints

--- src/metabase/api/dashboard.clj
+++ src/metabase/api/dashboard.clj
@@ -1124,72 +1124,23 @@
   (when-let [[_ tag] (mbql.u/check-clause :template-tag dimension)]
     (get-in card [:dataset_query :native :template-tags (u/qualified-name tag)])))
 
-(defn- param-type->op [type]
+(defn- param-type->op
+  [type]
   (if (get-in lib.schema.parameter/types [type :operator])
     (keyword (name type))
     :=))
 
-;; TODO needs to call [[lib/ensure-filter-stage]] and take `stage-number` from the parameter mapping into account
-;; TODO duplicates code in params.clj
-;; TODO needs to wrap models and metrics properly!
-(mu/defn- param->fields
-  [{:keys [mappings] :as param} :- mbql.s/Parameter]
-  (let [cards (into {}
-                    (map (fn [mapping]
-                           (let [card (get-in mapping [:dashcard :card])]
-                             [(:id card) card])))
-                    mappings)
-        metadata-providers (->>
-                            cards
-                            vals
-                            (map :database_id)
-                            distinct
-                            (into {}
-                                  (map (fn [database-id]
-                                         [database-id
-                                          (lib.metadata.jvm/application-database-metadata-provider database-id)]))))
+(defn- param-type->default-options
+  [type]
+  (when (#{:string/contains :string/does-not-contain :string/starts-with :string/ends-with} type)
+    {:case-sensitive false}))
 
-        filterable-columns (into {}
-                                 (map (fn [[card-id card]]
-                                        (let [dataset-query (:dataset_query card)]
-                                          [card-id
-                                           (if (seq dataset-query)
-                                             (->> dataset-query
-                                                  (lib/query (metadata-providers (:database_id card)))
-                                                  lib/filterable-columns)
-                                             [])])))
-                                 cards)]
-    (for [{:keys [target] {:keys [card]} :dashcard} mappings
-          :let  [[_ dimension] (->> (mbql.normalize/normalize-tokens target :ignore-path)
-                                    (mbql.u/check-clause :dimension))]
-          :when dimension
-          :let  [ttag      (get-template-tag dimension card)
-                 dimension (condp mbql.u/is-clause? dimension
-                             :field        dimension
-                             :expression   dimension
-                             :template-tag (:dimension ttag)
-                             (log/error "cannot handle this dimension" {:dimension dimension}))
-                 field-id  (or
-                            ;; Get the field id from the field-clause if it contains it. This is the common case
-                            ;; for mbql queries.
-                            (lib.util.match/match-one dimension [:field (id :guard integer?) _] id)
-                            ;; Attempt to get the field clause from the model metadata corresponding to the field.
-                            ;; This is the common case for native queries in which mappings from original columns
-                            ;; have been performed using model metadata.
-                            (:id (qp.util/field->field-info dimension (:result_metadata card)))
-                            ;; Look through the card's filterable columns and see if any of them match. This is common
-                            ;; when the query has an aggregation and you want to filter on something pre-aggregation.
-                            (lib.util.match/match-one dimension [:field (field-name :guard string?) _]
-                              (->> card
-                                   :id
-                                   filterable-columns
-                                   (lib/find-matching-column (lib/->pMBQL dimension))
-                                   :id)))]
-          :when field-id]
-      {:field-id field-id
-       :op       (param-type->op (:type param))
-       :options  (merge (:options ttag)
-                        (:options param))})))
+(mu/defn- param->fields
+  [param :- mbql.s/Parameter]
+  (for [field-id (params/dashboard-param->field-ids param)]
+    {:field-id field-id
+     :op       (param-type->op (:type param))
+     :options  (or (:options param) (param-type->default-options (:type param)))}))
 
 (mu/defn- chain-filter-constraints :- chain-filter/Constraints
   [dashboard                   :- :map
