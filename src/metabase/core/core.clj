3265976568be5a3479d9af8cbeafa5b3a7c693a6
Edward Paget <ed.paget@gmail.com>
2025-05-13 13:13:07 -0500

    fixup! put the comment back in the right spot

--- src/metabase/core/core.clj
+++ src/metabase/core/core.clj
@@ -145,10 +145,10 @@
   (init-status/set-progress! 0.5)
   (premium-features/airgap-check-user-count)
   (init-status/set-progress! 0.55)
-  ;; run a very quick check to see if we are doing a first time installation
-  ;; the test we are using is if there is at least 1 User in the database
   (task/init-scheduler!)
   (analytics/add-listeners-to-scheduler!)
+  ;; run a very quick check to see if we are doing a first time installation
+  ;; the test we are using is if there is at least 1 User in the database
   (let [new-install? (not (setup/has-user-setup))]
     ;; initialize Metabase from an `config.yml` file if present (Enterprise Edition™ only)
     (config-from-file/init-from-file-if-code-available!)
