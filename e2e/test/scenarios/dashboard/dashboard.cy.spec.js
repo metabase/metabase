caffffd8021c84bb72b376d2a4f7e0a6fdc531f2
John Swanson <john.swanson@metabase.com>
2025-05-13 08:49:00 -0700

    Fix snowplow helpers to be less flaky (#57750)
    Remove `expectGoodSnowplowEvents` entirely - it was an antipattern.
    
    Replace it with a good set of snowplow helper functions that actually
    accomplish what we want:
    
    - `expectSnowplowEvent`, which matches on the *entire* event rather than
    assuming the event is an unstructured event (most of ours are, but some
    are not).
    
    - `expectUnstructuredSnowplowEvent`, which behaves the same as
      `expectGoodSnowplowEvent` used to, except I removed an unused behavior
      where it took a function instead of a partial-match object.
    
    - `assertNoUnstructuredSnowplowEvent`, which asserts the inverse of
      `expectGoodSnowplowEvent` - there are zero matching snowplow events.
    
    I removed `Good` from all these names as I think that's implied - if you
    want to make an assertion that a bad event is being received you can
    write your own helper :)

--- e2e/test/scenarios/dashboard/dashboard.cy.spec.js
+++ e2e/test/scenarios/dashboard/dashboard.cy.spec.js
@@ -1345,7 +1345,7 @@ H.describeWithSnowplow("scenarios > dashboard", () => {
       H.saveDashboard();
       validateIFrame("https://example.com");
 
-      H.expectGoodSnowplowEvent({
+      H.expectUnstructuredSnowplowEvent({
         event: "new_iframe_card_created",
         target_id: id,
         event_detail: "example.com",
@@ -1359,7 +1359,7 @@ H.describeWithSnowplow("scenarios > dashboard", () => {
     const newTitle = "New title";
     cy.findByTestId("dashboard-name-heading").clear().type(newTitle).blur();
     H.saveDashboard();
-    H.expectGoodSnowplowEvent({
+    H.expectUnstructuredSnowplowEvent({
       event: "dashboard_saved",
     });
   });
@@ -1391,7 +1391,7 @@ H.describeWithSnowplow("scenarios > dashboard", () => {
       /orders in a dashboard/i,
     );
 
-    H.expectGoodSnowplowEvent({
+    H.expectUnstructuredSnowplowEvent({
       event: "new_link_card_created",
     });
   });
@@ -1412,7 +1412,7 @@ H.describeWithSnowplow("scenarios > dashboard", () => {
         .click({ force: true }) // disable
         .click({ force: true }); // enable
 
-      H.expectGoodSnowplowEvent(
+      H.expectUnstructuredSnowplowEvent(
         {
           event: "card_set_to_hide_when_no_results",
           dashboard_id: ORDERS_DASHBOARD_ID,
@@ -1474,7 +1474,7 @@ H.describeWithSnowplow("scenarios > dashboard", () => {
     cy.findByLabelText("Toggle width").click();
     H.popover().findByText("Full width").click();
     H.assertDashboardFullWidth();
-    H.expectGoodSnowplowEvent({
+    H.expectUnstructuredSnowplowEvent({
       event: "dashboard_width_toggled",
       full_width: true,
     });
@@ -1489,7 +1489,7 @@ H.describeWithSnowplow("scenarios > dashboard", () => {
     cy.findByLabelText("Toggle width").click();
     H.popover().findByText("Full width").click();
     H.assertDashboardFixedWidth();
-    H.expectGoodSnowplowEvent({
+    H.expectUnstructuredSnowplowEvent({
       event: "dashboard_width_toggled",
       full_width: false,
     });
