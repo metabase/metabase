0cadbdf007ddfd6b90d66ff4a7ca98524fa08a7e
Stas Gavrylov <stas@metabase.com>
2025-05-13 18:12:14 +0300

    WRK-380, WRK-387: Custom templates improvements (#57893)
    * Fix Slack template onChange method. Remove Slack markdown highlighting. Remove email template from alerts
    
    * WRK-387: Add possibility to send alerts to multiple channels
    
    * Some renaming in custom templates components
    
    * Fix bug when template editor is cleared and immediately repopulated
    
    * Fix lint

--- frontend/src/metabase/notifications/channels/SlackChannelFieldNew.tsx
+++ frontend/src/metabase/notifications/channels/SlackChannelFieldNew.tsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
+import { useCallback, useMemo } from "react";
 import { t } from "ttag";
 
-import CS from "metabase/css/core/index.css";
 import { useSelector } from "metabase/lib/redux";
 import { getApplicationName } from "metabase/selectors/whitelabel";
-import { Autocomplete } from "metabase/ui";
+import { MultiSelect } from "metabase/ui";
 import type { ChannelSpec, NotificationHandlerSlack } from "metabase-types/api";
 
 const CHANNEL_FIELD_NAME = "channel";
 const CHANNEL_PREFIX = "#";
 const USER_PREFIX = "@";
-
-const ALLOWED_PREFIXES = [CHANNEL_PREFIX, USER_PREFIX];
 
 interface SlackChannelFieldProps {
   channel: NotificationHandlerSlack;
@@ -25,67 +22,68 @@ export const SlackChannelFieldNew = ({
   channelSpec,
   onChange,
 }: SlackChannelFieldProps) => {
-  const [hasPrivateChannelWarning, setHasPrivateChannelWarning] =
-    useState(false);
-
   const channelField = channelSpec.fields?.find(
     (field) => field.name === CHANNEL_FIELD_NAME,
   );
-  const value = channel.recipients[0]?.details.value ?? "";
 
-  const updateChannel = (value: string) => {
-    onChange({
-      ...channel,
-      recipients: [
-        {
+  const value = useMemo(
+    () => channel.recipients.map((r) => r.details.value),
+    [channel],
+  );
+
+  const updateChannel = useCallback(
+    (value: string[]) => {
+      onChange({
+        ...channel,
+        recipients: value.map((v) => ({
           type: "notification-recipient/raw-value",
           details: {
-            value,
+            value: v,
           },
-        },
-      ],
+        })),
+      });
+    },
+    [channel, onChange],
+  );
+
+  const handleBlur = useCallback(() => {
+    const formattedValues = value.map((v) => {
+      if (v.startsWith(USER_PREFIX)) {
+        return v;
+      }
+      if (v.startsWith(CHANNEL_PREFIX)) {
+        return v;
+      }
+      return `${CHANNEL_PREFIX}${v}`;
     });
-  };
 
-  const handleChange = (value: string) => {
-    updateChannel(value);
-    setHasPrivateChannelWarning(false);
-  };
-
-  const handleBlur = () => {
-    const shouldAddPrefix =
-      value.length > 0 && !ALLOWED_PREFIXES.includes(value[0]);
-    const fullChannelName = shouldAddPrefix
-      ? `${CHANNEL_PREFIX}${value}`
-      : value;
-
-    if (shouldAddPrefix) {
-      updateChannel(fullChannelName);
-    }
-
-    const isPrivate =
-      value.trim().length > 0 && !channelField?.options?.includes(value);
-
-    setHasPrivateChannelWarning(isPrivate);
-  };
+    updateChannel(formattedValues);
+  }, [updateChannel, value]);
 
   const applicationName = useSelector(getApplicationName);
 
   return (
     <div>
-      <Autocomplete
+      <MultiSelect
+        multiple
+        searchable
+        hidePickedOptions
         data={channelField?.options || []}
         value={value}
         placeholder={t`Pick a user or channel...`}
         limit={300}
+        styles={{
+          input: {
+            paddingRight: "0.25rem",
+          },
+          section: {
+            display: "none",
+          },
+        }}
         onBlur={handleBlur}
-        onChange={handleChange}
+        onChange={updateChannel}
+        nothingFoundMessage={t`In order to send subscriptions and alerts to private Slack channels, you must first add the ${applicationName} bot to them.`}
       />
-      {hasPrivateChannelWarning && (
-        <div
-          className={CS.mt1}
-        >{t`In order to send subscriptions and alerts to private Slack channels, you must first add the ${applicationName} bot to them.`}</div>
-      )}
     </div>
   );
 };
