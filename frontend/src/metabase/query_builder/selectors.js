cb1b6f78562af02122d2a6b5e9522737a8d2ed6e
Aleksandr Lesnenko <alxnddr@users.noreply.github.com>
2025-05-14 14:04:03 -0400

    track first non-table chart generated (#56858)
    * track first non-table chart generated
    
    * specs
    
    * fix types
    
    * review
    
    * fix serialization
    
    * eliminate chart rerender when instance settings change
    
    * fix prop name
    
    * rename chart-generated setting
    
    * update e2e spec

--- frontend/src/metabase/query_builder/selectors.js
+++ frontend/src/metabase/query_builder/selectors.js
@@ -673,15 +673,10 @@ export const getShouldShowUnsavedChangesWarning = createSelector(
  * Returns the card and query results data in a format that `Visualization.jsx` expects
  */
 export const getRawSeries = createSelector(
-  [
-    getQuestion,
-    getFirstQueryResult,
-    getLastRunDatasetQuery,
-    getIsShowingRawTable,
-  ],
-  (question, queryResult, lastRunDatasetQuery, isShowingRawTable) => {
+  [getCard, getFirstQueryResult, getLastRunDatasetQuery, getIsShowingRawTable],
+  (card, queryResult, lastRunDatasetQuery, isShowingRawTable) => {
     const rawSeries = createRawSeries({
-      question,
+      card,
       queryResult,
       datasetQuery: lastRunDatasetQuery,
     });
@@ -1078,7 +1073,7 @@ export function getEmbeddedParameterVisibility(state, slug) {
 
 export const getSubmittableQuestion = (state, question) => {
   const rawSeries = createRawSeries({
-    question: getQuestion(state),
+    card: getCard(state),
     queryResult: getFirstQueryResult(state),
     datasetQuery: getLastRunDatasetQuery(state),
   });
