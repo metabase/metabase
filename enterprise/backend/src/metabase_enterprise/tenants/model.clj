138364d721cf990208268a1c11ea69f54236eed1
John Swanson <john.swanson@metabase.com>
2025-05-14 06:15:44 -0700

    fix tests, slight change to tenant creation API

--- enterprise/backend/src/metabase_enterprise/tenants/model.clj
+++ enterprise/backend/src/metabase_enterprise/tenants/model.clj
@@ -1,20 +1,16 @@
 (ns metabase-enterprise.tenants.model
   (:require
-   [metabase.util :as u]
    [methodical.core :as methodical]
    [toucan2.core :as t2]))
 
 (methodical/defmethod t2/table-name :model/Tenant [_model] :tenant)
 
-(defn name-exists?
+(defn tenant-exists?
   "Given a tenant name, returns truthy if the name (or its slugified version) is already reserved."
-  [n]
+  [{n :name slug :slug}]
   (t2/exists? :model/Tenant {:where [:or
-                                     [:= :slug (u/slugify n)]
+                                     [:= :slug slug]
                                      [:= :name n]]}))
-
-(t2/define-before-insert :model/Tenant [tenant]
-  (assoc tenant :slug (u/slugify (:name tenant))))
 
 (doto :model/Tenant
   (derive :metabase/model))
