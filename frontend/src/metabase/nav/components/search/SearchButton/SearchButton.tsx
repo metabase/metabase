6c285bf161839745ccd5f4a23a382f8a3038c41b
Sloan Sparger <sloansparger@gmail.com>
2025-05-14 14:30:05 -0500

    add metabot icon to searchbar, clicking icon opens metabot instead of search

--- frontend/src/metabase/nav/components/search/SearchButton/SearchButton.tsx
+++ frontend/src/metabase/nav/components/search/SearchButton/SearchButton.tsx
@@ -4,10 +4,15 @@ import { t } from "ttag";
 
 import useIsSmallScreen from "metabase/hooks/use-is-small-screen";
 import { METAKEY } from "metabase/lib/browser";
-import { Button, Icon, Tooltip } from "metabase/ui";
+import { Button, Flex, Icon, Tooltip, UnstyledButton } from "metabase/ui";
+/* eslint-disable no-restricted-imports */
+import { useMetabotAgent } from "metabase-enterprise/metabot/hooks";
 
+// TODO: make the changes to this file EE only
 export const SearchButton = () => {
   const kbar = useKBar();
+  const metabot = useMetabotAgent();
+
   const { setVisualState } = kbar.query;
 
   const handleClick = useCallback(() => {
@@ -16,36 +21,48 @@ export const SearchButton = () => {
 
   const isSmallScreen = useIsSmallScreen();
 
+  const label = t`Ask Metabot or search`;
+
   if (isSmallScreen) {
     return (
       <Button
         h="36px"
-        leftSection={<Icon name="search" />}
+        leftSection={<Icon name="metabot" />}
         variant="subtle"
         onClick={handleClick}
         color="text-medium"
-        aria-label="Search"
+        aria-label={label}
       />
     );
   } else {
     return (
-      <Tooltip label={`${t`Search...`} (${METAKEY}+k)`}>
-        <Button
-          h="36px"
-          w="240px"
-          leftSection={<Icon name="search" />}
-          onClick={handleClick}
-          // TODO: Adjust this with Mantine V7
-          styles={{
-            inner: {
-              justifyContent: "start",
-            },
-          }}
-          aria-label="Search"
-        >
-          {t`Search`}
-        </Button>
-      </Tooltip>
+      <Flex
+        h="36px"
+        w="240px"
+        justify="flex-start"
+        align="center"
+        style={{
+          borderRadius: "var(--mantine-radius-default)",
+          border: "1px solid var(--mb-color-border)",
+        }}
+      >
+        <Tooltip label={`${t`Metabot...`} (${METAKEY}+b)`}>
+          <UnstyledButton
+            onClick={() => metabot.setVisible(true)}
+            h="1rem"
+            pl="md"
+            pr="xs"
+            aria-label={t`Open metabot`}
+          >
+            <Icon name="metabot" />
+          </UnstyledButton>
+        </Tooltip>
+        <Tooltip label={`${t`Search...`} (${METAKEY}+k)`}>
+          <UnstyledButton pl="xs" onClick={handleClick} aria-label={label}>
+            {label}
+          </UnstyledButton>
+        </Tooltip>
+      </Flex>
     );
   }
 };
