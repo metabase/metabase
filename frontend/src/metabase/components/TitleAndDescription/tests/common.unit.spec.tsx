898ea413b2dd9adb5005d051195b4cf3c8eab895
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-15 15:42:08 -0400

    Simplify tests, remove some abstractions

--- frontend/src/metabase/components/TitleAndDescription/tests/common.unit.spec.tsx
+++ frontend/src/metabase/components/TitleAndDescription/tests/common.unit.spec.tsx
@@ -1,26 +1,49 @@
+import userEvent from "@testing-library/user-event";
+
+import { screen } from "__support__/ui";
+import { assertNeverPasses } from "__support__/utils";
+
 import { sampleDictionary } from "./constants";
-import {
-  type SetupOpts,
-  assertStringsArePresent,
-  assertStringsDoNotBecomePresent,
-  setup as baseSetup,
-} from "./utils";
+import { type SetupOpts, setup as baseSetup } from "./utils";
 
 describe("TitleAndDescription component (OSS)", () => {
   const setup = (opts: SetupOpts) =>
     baseSetup({ hasEnterprisePlugins: false, ...opts });
 
   describe("when a German content translation dictionary is provided", () => {
-    it("displays untranslated question title and description when locale is English", async () => {
-      setup({ localeCode: "en", dictionary: sampleDictionary });
-      assertStringsArePresent("msgid");
-      await assertStringsDoNotBecomePresent("msgstr");
-    });
+    ["en", "de"].forEach((locale) => {
+      it(`displays untranslated question title and description when locale is ${locale}`, async () => {
+        setup({ localeCode: "en", dictionary: sampleDictionary });
+        expect(
+          await screen.findByRole("heading", {
+            name: "Sample text",
+          }),
+        ).toBeInTheDocument();
 
-    it("displays untranslated question title and description when locale is German", async () => {
-      setup({ localeCode: "de", dictionary: sampleDictionary });
-      assertStringsArePresent("msgid");
-      await assertStringsDoNotBecomePresent("msgstr");
+        await userEvent.hover(screen.getByLabelText("info icon"));
+        expect(
+          await screen.findByRole("tooltip", {
+            name: "Sample description",
+          }),
+        ).toBeInTheDocument();
+
+        await assertNeverPasses(async () => {
+          expect(
+            await screen.findByRole("heading", {
+              name: "Beispieltext",
+            }),
+          ).toBeInTheDocument();
+        });
+
+        await assertNeverPasses(async () => {
+          await userEvent.hover(screen.getByLabelText("info icon"));
+          expect(
+            await screen.findByRole("tooltip", {
+              name: "Beispielbeschreibung",
+            }),
+          ).toBeInTheDocument();
+        });
+      });
     });
   });
 });
