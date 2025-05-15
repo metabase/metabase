beac61695206f6a79ead739f8c379c28dbb08ae0
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-05-15 18:55:01 +0300

    add yet another linter, no literal strings so we don't forget to translate everything (#57785) (#57964)
    Co-authored-by: Nicolò Pretto <info@npretto.com>

--- frontend/src/metabase/lib/formatting/numbers.tsx
+++ frontend/src/metabase/lib/formatting/numbers.tsx
@@ -306,6 +306,7 @@ function formatNumberScientific(
     const [m, n] = exp.split("e");
     return (
       <span>
+        {/* eslint-disable-next-line i18next/no-literal-string */}
         {m}×10<sup>{n.replace(/^\+/, "")}</sup>
       </span>
     );
