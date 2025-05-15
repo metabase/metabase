eb3cfda4911966fbc3cad41c48594783478b0f38
Phoomparin Mano <phoomparin@gmail.com>
2025-05-15 17:00:30 +0700

    press escape instead of clicking outside of the popover

--- e2e/test-component/scenarios/embedding-sdk/interactive-question.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/interactive-question.cy.spec.tsx
@@ -461,10 +461,12 @@ describe("scenarios > embedding-sdk > interactive-question", () => {
       cy.log("click on the button to switch target collection");
       cy.findByText("use second collection").click();
       cy.findByText(`id = ${SECOND_COLLECTION_ENTITY_ID}`).should("exist");
+    });
 
-      cy.log("close any existing open popovers to reduce flakes");
-      cy.findByText("Data").click();
+    cy.log("close any existing open popovers to reduce flakes");
+    cy.get("body").type("{esc}");
 
+    getSdkRoot().within(() => {
       cy.log("open the data picker");
       cy.findByText("Pick your starting data").click();
 
