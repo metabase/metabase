0cd9e7175636a1fcf758958a018371b4180531ec
Timofei <timofei@metabase.com>
2025-05-15 13:48:06 +0400

    Remove loader

--- frontend/src/metabase/public/components/EmbedModal/StaticEmbedSetupPane/PreviewPane/PreviewPane.tsx
+++ frontend/src/metabase/public/components/EmbedModal/StaticEmbedSetupPane/PreviewPane/PreviewPane.tsx
@@ -1,7 +1,6 @@
 import cx from "classnames";
 
 import CS from "metabase/css/core/index.css";
-import { Flex, Loader } from "metabase/ui";
 
 import { PreviewPaneContainer } from "./PreviewPane.styled";
 
@@ -30,12 +29,6 @@ export function PreviewPane({
       backgroundType={backgroundType}
       className={cx(className, CS.flex, CS.relative)}
     >
-      {!previewUrl && (
-        <Flex justify="center" align="center" w="100%">
-          <Loader size="lg" />
-        </Flex>
-      )}
-
       {previewUrl && (
         <iframe
           data-testid="embed-preview-iframe"
