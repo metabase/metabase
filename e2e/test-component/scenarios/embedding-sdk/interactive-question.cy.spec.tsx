871c98e12f6f17ab93a54273093c9006a81b9c42
Phoomparin Mano <phoomparin@gmail.com>
2025-05-15 15:20:40 +0700

    wait for interactive question to finish loading before popover

--- e2e/test-component/scenarios/embedding-sdk/interactive-question.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/interactive-question.cy.spec.tsx
@@ -460,6 +460,10 @@ describe("scenarios > embedding-sdk > interactive-question", () => {
       cy.findByText("use second collection").click();
       cy.findByText(`id = ${SECOND_COLLECTION_ENTITY_ID}`).should("exist");
 
+      cy.wait("@cardQuery").then(({ response }) => {
+        expect(response?.statusCode).to.equal(202);
+      });
+
       cy.log("ensure that the interactive question still works");
       H.popover().findByRole("link", { name: "Orders" }).click();
       cy.findByRole("button", { name: "Visualize" }).should("be.visible");
