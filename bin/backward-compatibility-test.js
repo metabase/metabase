e51db7b42852bd8ad417098f9a714abf6a53c1a0
Nicolò Pretto <info@npretto.com>
2025-05-16 13:34:20 +0200

    run all question tests again to see which one fails

--- bin/backward-compatibility-test.js
+++ bin/backward-compatibility-test.js
@@ -29,9 +29,10 @@ const JAR_PATH = path.join(BE_FOLDER, "target/uberjar/metabase.jar");
 // Test files configuration
 const TEST_TO_RUN = [
   "e2e/test/scenarios/dashboard/dashboard.cy.spec.js",
-  "e2e/test/scenarios/question/caching.cy.spec.js",
-  "e2e/test/scenarios/question/native-query-drill.cy.spec.ts",
-  "e2e/test/scenarios/question/offset.cy.spec.ts",
+  // "e2e/test/scenarios/question/caching.cy.spec.js",
+  // "e2e/test/scenarios/question/native-query-drill.cy.spec.ts",
+  // "e2e/test/scenarios/question/offset.cy.spec.ts",
+  "e2e/test/scenarios/question/*",
 ];
 const COMMA_SEPARATED_TESTS_TO_RUN = TEST_TO_RUN.join(",");
 
