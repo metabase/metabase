6dcabbb0b4735fb9750c244939bd4b78efc7fb15
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2025-05-19 15:34:44 +0300

    add tests for mapping of native query to temporal unit parameter

--- e2e/test/scenarios/dashboard-filters/temporal-unit-parameters.cy.spec.js
+++ e2e/test/scenarios/dashboard-filters/temporal-unit-parameters.cy.spec.js
@@ -1030,6 +1030,56 @@ describe("scenarios > dashboard > temporal unit parameters", () => {
       H.getDashboardCard().findByText("Created At: Year").should("be.visible");
     });
   });
+
+  describe("native queries", () => {
+    it("should be able to use temporal unit parameters in a native query", () => {
+      const questionWithoutDefaultValue = {
+        name: "Saved question with time grouping",
+        native: {
+          query: `
+        SELECT
+          count(*),
+          {{mb.time_grouping("unit", "created_at")}} as unit
+        FROM
+          ORDERS
+        GROUP BY
+          unit
+        `,
+          "template-tags": {
+            unit: {
+              type: "temporal-unit",
+              name: "unit",
+              id: "eb345703-001c-4b2a-b7d5-71cb3efe4beb",
+              "display-name": "Unit",
+              required: true,
+            },
+          },
+        },
+      };
+
+      H.createDashboardWithQuestions({
+        dashboardDetails,
+        questions: [questionWithoutDefaultValue],
+      }).then(({ dashboard }) => H.visitDashboard(dashboard.id));
+
+      H.getDashboardCard().should(
+        "contain",
+        "There was a problem displaying this chart.",
+      );
+
+      H.editDashboard();
+      addTemporalUnitParameter();
+      H.selectDashboardFilter(H.getDashboardCard(), "Unit");
+
+      H.dashboardParameterSidebar()
+        .get("[aria-labelledby='default-value-label'")
+        .click();
+
+      H.popover().findByText("Year").click();
+      H.saveDashboard();
+      H.getDashboardCard().should("contain", "January 1, 2022, 12:00 AM");
+    });
+  });
 });
 
 function backToDashboard() {
