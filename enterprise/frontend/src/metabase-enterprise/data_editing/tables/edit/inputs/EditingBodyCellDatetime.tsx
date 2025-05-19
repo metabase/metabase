fcd9cc8878d01ce0f13bb6ef1c21bf408901a56a
Konstantin Darutkin <spalt08@mail.ru>
2025-05-19 11:35:14 +0100

    fix: do not switch timezones when editing a date (#58090)
    * fix: do not switch timezones when editing a date
    
    * fix: reivew fixes

--- enterprise/frontend/src/metabase-enterprise/data_editing/tables/edit/inputs/EditingBodyCellDatetime.tsx
+++ enterprise/frontend/src/metabase-enterprise/data_editing/tables/edit/inputs/EditingBodyCellDatetime.tsx
@@ -1,9 +1,13 @@
-import { type KeyboardEvent, useCallback, useState } from "react";
+import { DatesProvider, useDatesContext } from "@mantine/dates";
+import dayjs from "dayjs";
+import { type KeyboardEvent, useCallback, useMemo, useState } from "react";
 
 import {
   DEFAULT_DATE_STYLE,
   DEFAULT_TIME_STYLE,
 } from "metabase/lib/formatting/datetime-utils";
+import { useSelector } from "metabase/lib/redux";
+import { getSetting } from "metabase/selectors/settings";
 import { DateInput } from "metabase/ui";
 
 import type { EditingBodyPrimitiveProps } from "./types";
@@ -24,21 +28,38 @@ export const EditingBodyCellDatetime = ({
     datasetColumn.effective_type === "type/DateTime" ||
     datasetColumn.effective_type === "type/DateTimeWithLocalTZ";
 
-  const initialDateValue = initialValue
-    ? new Date(initialValue?.toString())
-    : null;
+  const { locale, firstDayOfWeek, consistentWeeks } = useDatesContext();
+  const reportTimezone = useSelector((state) =>
+    getSetting(state, "report-timezone-long"),
+  );
+
+  const { restoreTimezone, initialDate } = useMemo(
+    () => ({
+      initialDate: initialValue ? new Date(initialValue.toString()) : null,
+      restoreTimezone: (date: Date | null) => {
+        if (!date) {
+          return null;
+        }
+
+        return dayjs(date)
+          .tz(reportTimezone)
+          .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+      },
+    }),
+    [initialValue, reportTimezone],
+  );
 
   const valueFormat = isDateTime ? DEFAULT_DATETIME_STYLE : DEFAULT_DATE_STYLE;
 
-  const [value, setValue] = useState<Date | null>(initialDateValue);
+  const [value, setValue] = useState<Date | null>(initialDate);
   const [isFocused, setFocused] = useState(false);
 
   const handleChange = useCallback(
     (value: Date | null) => {
       setValue(value);
-      onChangeValue?.(value ? value.toISOString() : null);
+      onChangeValue?.(restoreTimezone(value));
     },
-    [onChangeValue],
+    [onChangeValue, restoreTimezone],
   );
 
   const handleFocus = useCallback(() => {
@@ -47,36 +68,45 @@ export const EditingBodyCellDatetime = ({
 
   const handleBlur = useCallback(() => {
     setFocused(false);
-    onSubmit(value ? value.toISOString() : null);
-  }, [value, onSubmit]);
+    onSubmit(restoreTimezone(value));
+  }, [value, onSubmit, restoreTimezone]);
 
   const handleKeyUp = useCallback(
     (event: KeyboardEvent) => {
       if (event.key === "Escape") {
         onCancel();
       } else if (event.key === "Enter") {
-        onSubmit(value ? value.toISOString() : null);
+        onSubmit(restoreTimezone(value));
       }
     },
-    [value, onCancel, onSubmit],
+    [value, onCancel, onSubmit, restoreTimezone],
   );
 
   return (
-    <DateInput
-      autoFocus={autoFocus}
-      value={value}
-      valueFormat={valueFormat}
-      classNames={{
-        wrapper: classNames?.wrapper,
-        input: classNames?.dateTextInputElement,
+    <DatesProvider
+      settings={{
+        timezone: reportTimezone,
+        locale,
+        firstDayOfWeek,
+        consistentWeeks,
       }}
-      // Keeps popover mounted when focused to improve time editing UX
-      popoverProps={{ opened: isFocused }}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      onFocus={handleFocus}
-      onKeyUp={handleKeyUp}
-      {...inputProps}
-    />
+    >
+      <DateInput
+        autoFocus={autoFocus}
+        value={value}
+        valueFormat={valueFormat}
+        classNames={{
+          wrapper: classNames?.wrapper,
+          input: classNames?.dateTextInputElement,
+        }}
+        // Keeps popover mounted when focused to improve time editing UX
+        popoverProps={{ opened: isFocused }}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+        onKeyUp={handleKeyUp}
+        {...inputProps}
+      />
+    </DatesProvider>
   );
 };
