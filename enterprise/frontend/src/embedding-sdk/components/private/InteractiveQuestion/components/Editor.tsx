097b7388de898c2b755220d98b7c45ccca120b2b
Oisin Coveney <oisin@metabase.com>
2025-05-19 11:46:32 +0200

    Revert using onApply

--- enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/components/Editor.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/InteractiveQuestion/components/Editor.tsx
@@ -34,6 +34,7 @@ export type InteractiveQuestionEditorProps = {
  * @param props
  */
 export const Editor = ({
+  onApply,
   hasVisualizeButton = true,
 }: InteractiveQuestionEditorProps) => {
   // Loads databases and metadata so we can show notebook steps for the selected data source
@@ -74,6 +75,7 @@ export const Editor = ({
             await updateQuestion(nextQuestion, { run: false })
           }
           runQuestionQuery={async () => {
+            onApply?.();
             await queryQuestion();
           }}
           setQueryBuilderMode={() => {}}
