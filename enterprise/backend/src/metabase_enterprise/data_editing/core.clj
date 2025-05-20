9b9019274ea18ab8ed384afbbe8e778126a4ecb5
Chris Truter <chris@metabase.com>
2025-05-20 10:40:05 +0200

    Gah whats up GH

--- enterprise/backend/src/metabase_enterprise/data_editing/core.clj
+++ enterprise/backend/src/metabase_enterprise/data_editing/core.clj
@@ -1,8 +1,6 @@
 (ns metabase-enterprise.data-editing.core
   (:require
-   [metabase-enterprise.data-editing.data-editing :as data-editing]
    [potemkin :as p]))
 
 (comment
-  p/keep-me
-  data-editing/keep-me)
+  p/keep-me)
