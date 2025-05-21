9de445a734edb2ce4407a5ba059044c7273031e0
Phoomparin Mano <phoomparin@gmail.com>
2025-05-22 03:43:13 +0700

    use plugins to copy to app/embed.v1.js

--- rspack.iframe-sdk-embed.config.js
+++ rspack.iframe-sdk-embed.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-commonjs */
 /* eslint-disable no-undef */
 
+const fs = require("fs");
 const path = require("path");
 
 const ENTERPRISE_SRC_PATH =
@@ -13,14 +14,17 @@ const SCRIPT_TAG_PATH = path.resolve(
 
 const BUILD_PATH = __dirname + "/resources/frontend_client";
 
+const OUT_FILE_NAME = "embed.v1.js";
+const OUT_TEMP_PATH = path.resolve(BUILD_PATH, "app/tmp-embed-js");
+
 module.exports = {
   name: "iframe_sdk_embed_v1",
   entry: SCRIPT_TAG_PATH,
   output: {
-    // we must use a different directory than the main rspack (app/dist),
+    // we must use a different directory than the main rspack config,
     // otherwise the path conflicts and the output bundle will not appear.
-    path: BUILD_PATH + "/app",
-    filename: "embed.v1.js",
+    path: OUT_TEMP_PATH,
+    filename: OUT_FILE_NAME,
     library: "metabase.embed",
     libraryTarget: "umd",
     globalObject: "this",
@@ -51,4 +55,22 @@ module.exports = {
   },
   optimization: { splitChunks: false, runtimeChunk: false },
   devtool: false,
+  plugins: [
+    {
+      name: "copy-embed-js-to-app-path",
+      apply(compiler) {
+        compiler.hooks.afterEmit.tap("copy-embed-js-to-app-path", () => {
+          const tempPath = path.join(OUT_TEMP_PATH, OUT_FILE_NAME);
+          const appPath = path.join(BUILD_PATH, "app/", OUT_FILE_NAME);
+
+          // copy embed.v1.js from the temp directory to the resources directory
+          fs.mkdirSync(path.dirname(appPath), { recursive: true });
+          fs.copyFileSync(tempPath, appPath);
+
+          // cleanup the temp directory to prevent bloat.
+          fs.rmSync(tempPath, { recursive: true });
+        });
+      },
+    },
+  ],
 };
