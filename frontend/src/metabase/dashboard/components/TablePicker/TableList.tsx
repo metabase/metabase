c6825b0461bbc3b2b587826dd27719ba64d2499d
Denis Berezin <denis.berezin@metabase.com>
2025-05-21 19:25:29 +0300

    WRK-419: filter available editable tables list based on db setting

--- frontend/src/metabase/dashboard/components/TablePicker/TableList.tsx
+++ frontend/src/metabase/dashboard/components/TablePicker/TableList.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { t } from "ttag";
 
 import { useListTablesQuery } from "metabase/api";
@@ -11,13 +12,30 @@ type TableListProps = {
 export const TableList = ({ onSelect }: TableListProps) => {
   const { data: tables } = useListTablesQuery();
 
-  if (!tables) {
+  const filteredTables = useMemo(() => {
+    return tables
+      ?.filter(({ db }) => db?.settings?.["database-enable-table-editing"])
+      .sort((a, b) => {
+        // sort by db name, then table name
+        const aDbName = a.db?.name || "";
+        const bDbName = b.db?.name || "";
+
+        const dbOrder = aDbName.localeCompare(bDbName);
+        if (dbOrder !== 0) {
+          return dbOrder;
+        }
+
+        return a.display_name.localeCompare(b.display_name);
+      });
+  }, [tables]);
+
+  if (!filteredTables) {
     return <div>{t`Nothing found`}</div>;
   }
 
   return (
     <SelectList>
-      {tables.map((item) => (
+      {filteredTables.map((item) => (
         <SelectList.Item
           key={item.id}
           id={item.id}
