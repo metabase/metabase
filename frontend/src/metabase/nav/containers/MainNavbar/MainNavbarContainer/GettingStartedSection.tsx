60aff2ad2e0ed7542054261f49c7a7dbb87f2938
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-05-22 09:22:51 +0200

    Move "How to use Metabase" to the new section

---
+++ frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/GettingStartedSection.tsx
@@ -0,0 +1,60 @@
+import { useDisclosure } from "@mantine/hooks";
+import { c, t } from "ttag";
+
+import CS from "metabase/css/core/index.css";
+import { getCanAccessOnboardingPage } from "metabase/home/selectors";
+import { useSelector } from "metabase/lib/redux";
+import { Collapse, Group, Icon, UnstyledButton } from "metabase/ui";
+
+import {
+  PaddedSidebarLink,
+  SidebarHeading,
+  SidebarSection,
+} from "../MainNavbar.styled";
+import { trackOnboardingChecklistOpened } from "../analytics";
+import type { SelectedItem } from "../types";
+
+export const GettingStartedSection = ({
+  nonEntityItem,
+}: {
+  nonEntityItem: SelectedItem;
+}) => {
+  const [opened, { toggle }] = useDisclosure(true);
+
+  const canAccessOnboarding = useSelector(getCanAccessOnboardingPage);
+
+  const ONBOARDING_URL = "/getting-started";
+  const isOnboardingPageSelected = nonEntityItem?.url === ONBOARDING_URL;
+
+  return (
+    <SidebarSection aria-selected={opened} role="tab">
+      <Group
+        align="center"
+        gap="sm"
+        onClick={toggle}
+        component={UnstyledButton}
+        c="text-medium"
+        mb="sm"
+        className={CS.cursorPointer}
+      >
+        <SidebarHeading>{c("A verb, shown in the sidebar")
+          .t`Getting Started`}</SidebarHeading>
+        <Icon name={opened ? "chevrondown" : "chevronright"} size={8} />
+      </Group>
+
+      <Collapse in={opened} transitionDuration={0} role="tabpanel">
+        {canAccessOnboarding && (
+          <PaddedSidebarLink
+            icon="learn"
+            url={ONBOARDING_URL}
+            isSelected={isOnboardingPageSelected}
+            onClick={() => trackOnboardingChecklistOpened()}
+          >
+            {/* eslint-disable-next-line no-literal-metabase-strings -- We only show this to non-whitelabelled instances */}
+            {t`How to use Metabase`}
+          </PaddedSidebarLink>
+        )}
+      </Collapse>
+    </SidebarSection>
+  );
+};
