9878d407e93c4317514009caa3b9246d6a931252
Cam Saul <github@camsaul.com>
2025-05-22 20:55:38 +0000

    cljfmt

--- test/metabase/app_db/schema_migrations_test/impl.clj
+++ test/metabase/app_db/schema_migrations_test/impl.clj
@@ -79,8 +79,7 @@
       ;; open
      (with-open [conn (.getConnection data-source)]
        (binding [mdb.connection/*application-db* (mdb.connection/application-db driver data-source)
-                 custom-migrations.util/*allow-temp-scheduling* false
-                 ]
+                 custom-migrations.util/*allow-temp-scheduling* false]
          (f conn))))))
 
 (defmacro with-temp-empty-app-db
