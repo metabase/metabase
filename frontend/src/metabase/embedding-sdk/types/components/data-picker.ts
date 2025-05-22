2cd19962d4df508d573d255ba3a0e95e8ab373f4
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-05-22 20:36:56 +0700

    Clarify a new prop

--- frontend/src/metabase/embedding-sdk/types/components/data-picker.ts
+++ frontend/src/metabase/embedding-sdk/types/components/data-picker.ts
@@ -2,6 +2,7 @@ import type { TableId } from "metabase-types/api";
 
 export interface DataSourceSelectorProps {
   isInitiallyOpen: boolean;
+  /** Whether the first stage of the query uses a model */
   isQuerySourceModel: boolean;
   /** false when joining data, true otherwise */
   canChangeDatabase: boolean;
