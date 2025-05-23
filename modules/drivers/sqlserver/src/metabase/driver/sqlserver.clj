ef25bbccbbbfc39893c815a859c0ae3dfe59f201
Riley Thompson <rileythompson99@gmail.com>
2025-05-23 15:40:36 -0400

    wording and case insensitive check

--- modules/drivers/sqlserver/src/metabase/driver/sqlserver.clj
+++ modules/drivers/sqlserver/src/metabase/driver/sqlserver.clj
@@ -59,7 +59,7 @@
 
 (defmethod driver/database-supports? [:sqlserver :connection-impersonation-requires-role]
   [_driver _feature db]
-  (= "sa" (get-in db [:details :user])))
+  (= (str/lower-case (-> db :details :user)) "sa"))
 
 (defmethod driver/database-supports? [:sqlserver :percentile-aggregations]
   [_ _ db]
@@ -926,7 +926,7 @@
   ;; Use a "role" (sqlserver user) if it exists, otherwise use
   ;; the user if it can be impersonated (ie not the 'sa' user).
   (let [{:keys [role user]} (:details database)]
-    (or role (when-not (= user "sa") user))))
+    (or role (when-not (= (str/lower-case user) "sa") user))))
 
 (defmethod driver.sql/set-role-statement :sqlserver
   [_driver role]
