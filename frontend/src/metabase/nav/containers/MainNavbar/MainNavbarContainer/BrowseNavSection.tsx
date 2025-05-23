1f94e8c4175753d106e2cfedf1c15a4c548cd039
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-05-23 14:36:43 +0200

    Trigger the modal from the browse data section

--- frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/BrowseNavSection.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/BrowseNavSection.tsx
@@ -14,10 +14,12 @@ import type { SelectedItem } from "../types";
 export const BrowseNavSection = ({
   nonEntityItem,
   onItemSelect,
+  onModalOpen,
   hasDataAccess,
 }: {
   nonEntityItem: SelectedItem;
   onItemSelect: () => void;
+  onModalOpen: () => void;
   hasDataAccess: boolean;
 }) => {
   const BROWSE_MODELS_URL = "/browse/models";
@@ -60,6 +62,7 @@ export const BrowseNavSection = ({
           component={UnstyledButton}
           c="brand"
           className={CS.cursorPointer}
+          onClick={onModalOpen}
         >
           <Icon name="add_data" />
           <Text fw={700} c="inherit">{t`Add`}</Text>
--- frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/MainNavbarView.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/MainNavbarView.tsx
@@ -1,3 +1,4 @@
+import { useDisclosure } from "@mantine/hooks";
 import type { MouseEvent } from "react";
 import { useCallback, useMemo } from "react";
 import { t } from "ttag";
@@ -42,6 +43,7 @@ import { DwhUploadMenu } from "../SidebarItems/DwhUpload";
 import { trackNewCollectionFromNavInitiated } from "../analytics";
 import type { SelectedItem } from "../types";
 
+import { AddDataModal } from "./AddDataModal";
 import BookmarkList from "./BookmarkList";
 import { BrowseNavSection } from "./BrowseNavSection";
 import { GettingStartedSection } from "./GettingStartedSection";
@@ -87,6 +89,9 @@ export function MainNavbarView({
   );
 
   const isAtHomepageDashboard = useIsAtHomepageDashboard();
+
+  const [modalOpened, { open: openModal, close: closeModal }] =
+    useDisclosure(false);
 
   const {
     card: cardItem,
@@ -235,6 +240,7 @@ export function MainNavbarView({
                 nonEntityItem={nonEntityItem}
                 onItemSelect={onItemSelect}
                 hasDataAccess={hasDataAccess}
+                onModalOpen={openModal}
               />
             </ErrorBoundary>
           </SidebarSection>
@@ -262,6 +268,8 @@ export function MainNavbarView({
         </div>
         <WhatsNewNotification />
       </SidebarContentRoot>
+
+      <AddDataModal opened={modalOpened} onClose={closeModal} />
     </ErrorBoundary>
   );
 }
