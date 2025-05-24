21702861350a6622eae7173169b5eb599104dc00
dan sutton <dan@dpsutton.com>
2025-05-23 20:24:25 -0500

    download dictionary

--- src/metabase/content_translation/api.clj
+++ src/metabase/content_translation/api.clj
@@ -1,7 +1,9 @@
 (ns metabase.content-translation.api
   (:require
+   [clojure.data.csv :as csv]
    [metabase.api.macros :as api.macros]
    [metabase.premium-features.core :as premium-features]
+   [ring.util.response :as resp]
    [toucan2.core :as t2]))
 
 (api.macros/defendpoint :get "/dictionary"
@@ -14,6 +16,25 @@
         {:data (t2/select :model/ContentTranslation :locale locale)}
         {:data (t2/select :model/ContentTranslation)}))
     (throw (ex-info "Content translation is not enabled" {:status-code 400}))))
+
+(api.macros/defendpoint :post "/download"
+  [_ _ _]
+  (if (premium-features/enable-content-translation?)
+
+    (let [sw (java.io.StringWriter.)
+          cols [:locale :msgid :msgstr]
+          csv (with-open [w (java.io.StringWriter.)]
+                (csv/write-csv w (into [(map name cols)] (map (fn [row]
+                                                     (map row cols)))
+                                       (t2/select :model/ContentTranslation)))
+                (str w))
+          safe-filename "download.csv"]
+      (-> (resp/response csv)
+          (resp/header "Content-Type" "text/csv; charset=utf-8")
+          (resp/header "Content-Disposition" (str "attachment; filename=\"" safe-filename "\""))
+          (resp/header "Cache-Control" "no-cache, no-store, must-revalidate")
+          (resp/header "Pragma" "no-cache")
+          (resp/header "Expires" "0")))))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/content-translation-dictionary` routes"
