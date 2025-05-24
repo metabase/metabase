f1cdbb280ea862cfa2d2f3294837483f1c3741a2
dan sutton <dan@dpsutton.com>
2025-05-23 19:48:40 -0500

    make sure no enterprise in oss

--- src/metabase/content_translation/api.clj
+++ src/metabase/content_translation/api.clj
@@ -1,16 +1,19 @@
 (ns metabase.content-translation.api
   (:require
-   [metabase-enterprise.content-translation.models :as ct]
-   [metabase.api.macros :as api.macros]))
+   [metabase.api.macros :as api.macros]
+   [metabase.premium-features.core :as premium-features]
+   [toucan2.core :as t2]))
 
 (api.macros/defendpoint :get "/dictionary"
   "Provides translations of user-generated content. This is a public route so that logged-out viewers of static-embedded questions and dashboards can retrieve translations"
   ;; TODO: Secure this route against shenanigans
   [_route-params query-params _body]
-  (let [locale (:locale query-params)]
-    (if locale
-      {:data (ct/get-translations locale)}
-      {:data (ct/get-translations)})))
+  (if (premium-features/enable-content-translation?)
+    (let [locale (:locale query-params)]
+      (if locale
+        {:data (t2/select :model/ContentTranslation :locale locale)}
+        {:data (t2/select :model/ContentTranslation)}))
+    (throw (ex-info "Content translation is not enabled" {:status-code 400}))))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/content-translation-dictionary` routes"
