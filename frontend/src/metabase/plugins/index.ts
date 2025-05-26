d27d1ec42ac6dea4f1af3e1a199699e502459688
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-26 18:14:09 -0400

    Fix unit tests

--- frontend/src/metabase/plugins/index.ts
+++ frontend/src/metabase/plugins/index.ts
@@ -745,9 +745,7 @@ export const PLUGIN_CONTENT_TRANSLATION = {
   translateDisplayNames: <T extends object>(
     obj: T,
     _tc: ContentTranslationFunction,
-  ) => ({
-    ...obj,
-  }),
+  ) => obj,
 };
 
 export const PLUGIN_DB_ROUTING = {
