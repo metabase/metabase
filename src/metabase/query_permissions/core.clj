b60e850ccebcb98c7f29cb60f2a013897fb7b029
Cam Saul <github@camsaul.com>
2025-05-27 22:47:11 +0000

    PR feedback

--- src/metabase/query_permissions/core.clj
+++ src/metabase/query_permissions/core.clj
@@ -1,4 +1,7 @@
 (ns metabase.query-permissions.core
+  "`query-permissions` is a module for calculating permissions for ad-hoc queries. This is separate from the main
+  `permissions` module because it has a dependency on the `query-processor` and we want to keep the dependencies of
+  `permissions` itself to a minimum."
   (:require
    [metabase.query-permissions.impl]
    [potemkin :as p]))
@@ -16,12 +19,13 @@
   query->source-table-ids
   required-perms-for-query])
 
-(defmacro ^:deprecated with-card-instances
+(defmacro with-card-instances
   "Provide a map of Card ID -> instance to use instead of hitting a metadata provider for more efficient perms
   calculation.
 
   TODO -- we should just rework the functions in question to take an explicit metadata provider that you can pre-warm
-  as needed. -- Cam"
+  as needed. I guess the one thing stopping is us that 'transient dashboards' create Cards that don't even have
+  integer IDs yet. Maybe we can think of a way to decouple this stuff. -- Cam"
   [card-id->instance & body]
   `(binding [metabase.query-permissions.impl/*card-instances* ~card-id->instance]
      ~@body))
