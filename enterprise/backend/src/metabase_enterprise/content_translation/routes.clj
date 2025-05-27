66c90434e5903be5190676633c0608700418d975
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-27 18:28:18 -0400

    Move some files around

--- enterprise/backend/src/metabase_enterprise/content_translation/routes.clj
+++ enterprise/backend/src/metabase_enterprise/content_translation/routes.clj
@@ -1,17 +1,13 @@
 (ns metabase-enterprise.content-translation.api.routes
   "Endpoints relating to the translation of user-generated content"
   (:require
+   [metabase-enterprise.content-translation.dictionary :as dictionary]
    [metabase.api.macros :as api.macros]
    [metabase.api.routes.common :refer [+auth]]
-   [metabase.content-translation.api.dictionary :as dictionary]
-   [metabase.util.i18n :refer [tru]]
    [metabase.util.json :as json]
    [metabase.util.malli.schema :as ms]))
 
-(set! *warn-on-reflection* true)
-
 (def ^:private http-status-ok 200)
-(def ^:private http-status-unprocessable 422)
 
 (api.macros/defendpoint :post
   "/upload-dictionary"
@@ -27,23 +23,11 @@
                                                    [:map
                                                     [:filename :string]
                                                     [:tempfile (ms/InstanceOfClass java.io.File)]]]]]]]
-  (try
-    (dictionary/import-translations! {:filename (get-in multipart-params ["file" :filename])
-                                      :file     (get-in multipart-params ["file" :tempfile])})
-    {:status http-status-ok
-     :headers {"Content-Type" "application/json"}
-     :body (json/encode {:success true
-                         :message (tru "Import was successful")})}
-    (catch clojure.lang.ExceptionInfo e
-      ; If the exception contains a list of validation errors, return them properly formatted
-      (if-let [errors (get (ex-data e) :errors)]
-        {:status (or (:status-code (ex-data e)) http-status-unprocessable)
-         :headers {"Content-Type" "application/json"}
-         :body (json/encode {:success false
-                             :message (.getMessage e)
-                             :errors errors})}
-        ; For other types of exceptions, rethrow
-        (throw e)))))
+  (dictionary/import-translations! {:filename (get-in multipart-params ["file" :filename])
+                                    :file     (get-in multipart-params ["file" :tempfile])})
+  {:status http-status-ok
+   :headers {"Content-Type" "application/json"}
+   :body (json/encode {:success true})})
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/content-translation` routes."
