1e144f1255b6ed7f93468bac22d2933295ebe1dd
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-05-27 11:48:12 +0200

    Add some basic modal structure

--- frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/AddDataModal/AddDataModal.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/AddDataModal/AddDataModal.tsx
@@ -1,4 +1,9 @@
-import { Modal } from "metabase/ui";
+import { t } from "ttag";
+
+import CS from "metabase/css/core/index.css";
+import { Box, Icon, Modal, Tabs, Title } from "metabase/ui";
+
+import S from "./AddDataModal.module.css";
 
 export const AddDataModal = ({
   onClose,
@@ -7,5 +12,51 @@ export const AddDataModal = ({
   onClose: () => void;
   opened: boolean;
 }) => {
-  return <Modal opened={opened} onClose={onClose} title="Add data" />;
+  return (
+    <Modal.Root opened={opened} onClose={onClose} size="auto">
+      <Modal.Overlay />
+      <Modal.Content h="30rem">
+        <Tabs
+          variant="none"
+          defaultValue="db"
+          orientation="vertical"
+          classNames={{
+            list: S.list,
+            tab: S.tab,
+          }}
+          h="100%"
+        >
+          <Box component="nav" w="14rem" className={CS.borderRight}>
+            <Box component="header" className={S.header}>
+              <Title order={2} size="lg">{t`Add data`}</Title>
+            </Box>
+            <Tabs.List px="md" pb="lg">
+              <Tabs.Tab value="db" leftSection={<Icon name="database" />}>
+                {t`Database`}
+              </Tabs.Tab>
+              <Tabs.Tab
+                value="csv"
+                leftSection={<Icon name="table2" />}
+                disabled
+              >
+                {t`CSV`}
+              </Tabs.Tab>
+              <Tabs.Tab
+                value="gsheet"
+                leftSection={<Icon name="document" />}
+                disabled
+              >
+                {t`Google Sheets`}
+              </Tabs.Tab>
+            </Tabs.List>
+          </Box>
+          <Box component="main" w="30rem">
+            <Tabs.Panel value="db">{"Database content"}</Tabs.Panel>
+            <Tabs.Panel value="csv">{"CSV content"}</Tabs.Panel>
+            <Tabs.Panel value="gsheet">{"Google Sheets content"}</Tabs.Panel>
+          </Box>
+        </Tabs>
+      </Modal.Content>
+    </Modal.Root>
+  );
 };
