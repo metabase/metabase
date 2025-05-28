76a0a40cc0e2368e4d53b4bd8cd1fd6e6b580ac7
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-28 17:33:02 -0400

    wip

--- frontend/src/metabase/plugins/index.ts
+++ frontend/src/metabase/plugins/index.ts
@@ -779,4 +779,5 @@ export const PLUGIN_API = {
     parameterId: ParameterId,
   ) =>
     `/api/dashboard/${dashboardId}/params/${encodeURIComponent(parameterId)}/remapping`,
+  getContentTranslationDictionaryUrl: null as string | null,
 };
