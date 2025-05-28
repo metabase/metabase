431460c46b486bccb58c8af794c3ecf4452556da
dan sutton <dan@dpsutton.com>
2025-05-27 23:19:05 -0500

    bit of cleanup in the dictionary creation namespace

--- enterprise/backend/src/metabase_enterprise/content_translation/dictionary.clj
+++ enterprise/backend/src/metabase_enterprise/content_translation/dictionary.clj
@@ -7,28 +7,33 @@
 
 (set! *warn-on-reflection* true)
 
-(def ^:private http-status-unprocessable 422)
+(defn- translation-key
+  "The identity of a translation. It's locale and source string so we can identify if the same translation is present
+  multiple times."
+  [t]
+  (select-keys t [:locale :msgid]))
 
-(defn- collect-row-format-error
-  "Returns an error message if a row does not have the expected format."
-  [row-index row]
-  (when-not (= (count row) 3)
-    (tru "Row {0}: Invalid format. Expected exactly 3 columns (Language, String, Translation)" (+ row-index 2))))
+(defn- adjust-index
+  "Adjust index: increment once for the header row that was chopped off and again to go to 1-based indexing for human
+  consumption."
+  [i]
+  (+ i 2))
 
 (defn- collect-locale-error
   "Returns an error message if a row does not have a valid locale."
-  [row-index locale]
+  [_state index {:keys [locale]}]
   (when (not (i18n/available-locale? locale))
-    (tru "Row {0}: Invalid locale: {1}" (+ row-index 2) locale)))
+    (tru "Row {0}: Invalid locale: {1}" (adjust-index index) locale)))
 
 (defn- collect-duplication-error
-  "Returns an error message if this translation key has already been seen in the file. A translation key is a string like 'de,Category'"
-  [seen-keys translation-key row-index locale trimmed-msgid]
-  (when (contains? seen-keys translation-key)
+  "Returns an error message if this translation key has already been seen in the file. A translation key is a string
+  like 'de,Category'"
+  [{:keys [seen] :as _state} index {:keys [msgid locale] :as translation}]
+  (when (-> translation translation-key seen)
     (tru "Row {0}: The string \"{1}\" is translated into locale \"{2}\" earlier in the file"
-         (+ row-index 2) trimmed-msgid locale)))
+         (adjust-index index) msgid locale)))
 
-(defn is-msgstr-usable
+(defn- is-msgstr-usable
   "Check if the translation string is usable. It should not be blank or contain only commas, whitespace, or semicolons."
   [msgstr]
   (not
@@ -36,44 +41,51 @@
     (str/blank? msgstr)
     (re-matches #"^[,;\s]*$" msgstr))))
 
-(defn- validate-row
-  "Validate a single row of CSV data, returning errors and the updated set of seen translation keys."
-  [row-index row seen-keys]
-  (let [[locale msgid _msgstr] row
-        trimmed-msgid (str/trim msgid)
-        translation-key (str locale "," trimmed-msgid)]
-    (if-let [format-error (collect-row-format-error row-index row)]
-      ;; If the row format is invalid, just return that error
-      {:errors [format-error] :seen-keys (conj seen-keys translation-key)}
-      ;; Otherwise check for other potential errors
-      (let [errors (remove nil?
-                           [(collect-duplication-error seen-keys translation-key row-index locale trimmed-msgid)
-                            (collect-locale-error row-index locale)])]
-        {:errors errors :seen-keys (conj seen-keys translation-key)}))))
+(defn- row-errors
+  [state index translation]
+  (keep (fn [f] (f state index translation))
+        [collect-duplication-error collect-locale-error]))
+
+(defn- wrong-row-shape
+  [index]
+  (tru "Row {0}: Invalid format. Expected exactly 3 columns (Language, String, Translation)"
+       (adjust-index index)))
+
+(defn- process-rows
+  "Format, trim, validate rows. Takes the vectors from a csv and returns a map with the shape
+  {:translations [{:locale :msgid :msgstr}]
+   :errors       [string]}.
+  The :seen set is returned but not meant for consumption."
+  [rows]
+  (reduce (fn [state [index row]]
+            (let [[locale msgid msgstr & extra] row
+                  translation                   {:locale locale
+                                                 :msgid  (str/trim msgid)
+                                                 :msgstr (str/trim msgstr)}
+                  errors                        (cond-> (row-errors state index translation)
+                                                  (seq extra) (conj (wrong-row-shape index)))]
+              (cond-> (-> state
+                          (update :seen conj (dissoc translation :msgstr))
+                          (update :translations conj translation))
+                (seq errors) (update :errors into errors))))
+          {:seen         #{}
+           :errors       []
+           :translations []}
+          (map-indexed vector rows)))
 
 (defn import-translations!
   "Import translations from CSV and insert or update rows in the content_translation table."
   [rows]
-  (let [errors (:errors (reduce (fn [{:keys [seen-keys] :as m} [row-index row]]
-                                  (let [{row-errors :errors updated-seen-keys :seen-keys} (validate-row row-index row seen-keys)]
-                                    (-> m
-                                        (update :seen-keys #(apply conj %1 %2) updated-seen-keys)
-                                        (update :errors #(apply conj %1 %2) row-errors))))
-                                {:seen-keys #{}
-                                 :errors []}
-                                (map-indexed vector rows)))
-        _ (when (seq errors)
-            (throw (ex-info (tru "The file could not be uploaded due to the following error(s):")
-                            {:status-code http-status-unprocessable
-                             :errors errors})))
-        usable-rows (for [[locale msgid msgstr] rows
-                          :let [trimmed-msgid (str/trim msgid)
-                                trimmed-msgstr (str/trim msgstr)]
-                          :when (is-msgstr-usable trimmed-msgstr)]
-                      {:locale locale :msgid trimmed-msgid :msgstr trimmed-msgstr})]
-    (t2/with-transaction [_tx]
-      ;; Replace all existing entries
-      (t2/delete! :model/ContentTranslation)
-      ;; Insert all usable rows at once
-      (when-not (empty? usable-rows)
-        (t2/insert! :model/ContentTranslation usable-rows)))))
+  (let [{:keys [translations errors]} (process-rows rows)]
+    (when (seq errors)
+      (throw (ex-info (tru "The file could not be uploaded due to the following error(s):")
+                      {:status-code 422
+                       :errors errors})))
+    ;; remove bad msgstrs after error generator for line number reporting reasons
+    (let [usable-rows (filter (comp is-msgstr-usable :msgstr) translations)]
+      (t2/with-transaction [_tx]
+        ;; Replace all existing entries
+        (t2/delete! :model/ContentTranslation)
+        ;; Insert all usable rows at once
+        (when-not (empty? usable-rows)
+          (t2/insert! :model/ContentTranslation usable-rows))))))
