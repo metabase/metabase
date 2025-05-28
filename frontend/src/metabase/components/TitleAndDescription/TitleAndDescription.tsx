b61ebe42a03347818930acf17eaa441836747ea9
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-28 15:57:01 -0400

    Content translation: localize question names and descriptions in static embedding (#57564)

--- frontend/src/metabase/components/TitleAndDescription/TitleAndDescription.tsx
+++ frontend/src/metabase/components/TitleAndDescription/TitleAndDescription.tsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import { useTranslateContent } from "metabase/i18n/hooks";
 
 import { Box, type BoxProps, Icon, Space, Title, Tooltip } from "metabase/ui";
 
@@ -6,25 +7,28 @@ const TitleAndDescriptionInner = ({
   title,
   description,
   ...boxProps
-}: { title: string; description?: string | null } & BoxProps) => (
-  <Box {...boxProps}>
-    <Title
-      role="heading"
-      size="xl"
-      component="span"
-      lh="normal"
-      order={2}
-      textWrap="wrap"
-    >
-      {title}
-    </Title>
-    <Space component="span" mx="xs" />
-    {description && (
-      <Tooltip label={description} maw="22em">
-        <Icon name="info" />
-      </Tooltip>
-    )}
-  </Box>
-);
+}: { title: string; description?: string | null } & BoxProps) => {
+  const tc = useTranslateContent();
+  return (
+    <Box {...boxProps}>
+      <Title
+        role="heading"
+        size="xl"
+        component="span"
+        lh="normal"
+        order={2}
+        textWrap="wrap"
+      >
+        {tc(title)}
+      </Title>
+      <Space component="span" mx="xs" />
+      {description && (
+        <Tooltip label={description} maw="22em">
+          <Icon name="info" />
+        </Tooltip>
+      )}
+    </Box>
+  );
+};
 
 export const TitleAndDescription = memo(TitleAndDescriptionInner);
