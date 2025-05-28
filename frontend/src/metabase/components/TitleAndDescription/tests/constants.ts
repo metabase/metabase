afe3f679ff82fdd8239a1762d0226ec7d6e1268e
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-05-28 15:57:01 -0400

    Content translation: localize question names and descriptions in static embedding (#57564)

---
+++ frontend/src/metabase/components/TitleAndDescription/tests/constants.ts
@@ -0,0 +1,11 @@
+import type { RetrievedDictionaryArrayRow } from "metabase-types/api/content-translation";
+
+export const sampleDictionary: RetrievedDictionaryArrayRow[] = [
+  { id: 0, locale: "de", msgid: "Sample text", msgstr: "Beispieltext" },
+  {
+    id: 1,
+    locale: "de",
+    msgid: "Sample description",
+    msgstr: "Beispielbeschreibung",
+  },
+];
