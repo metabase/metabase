ae85b4467b88acc2700288af1f93ff94295c2eb1
Timofei <timofei@metabase.com>
2025-05-28 20:44:04 +0400

    Fix lint

--- e2e/test-component/scenarios/embedding-sdk/locale-on-provider.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/locale-on-provider.cy.spec.tsx
@@ -6,12 +6,6 @@ import {
 import { ORDERS_QUESTION_ID } from "e2e/support/cypress_sample_instance_data";
 import { METABASE_INSTANCE_URL } from "e2e/support/helpers";
 import { updateSetting } from "e2e/support/helpers/api";
-import {
-  AUTH_PROVIDER_URL,
-  METABASE_INSTANCE_URL,
-  mockAuthProviderAndJwtSignIn,
-  signInAsAdminAndEnableEmbeddingSdk,
-} from "e2e/support/helpers/component-testing-sdk";
 import { getSdkRoot } from "e2e/support/helpers/e2e-embedding-sdk-helpers";
 import { signInAsAdminAndEnableEmbeddingSdk } from "e2e/support/helpers/embedding-sdk-testing";
 import { mockAuthProviderAndJwtSignIn } from "e2e/support/helpers/embedding-sdk-testing/embedding-sdk-helpers";
