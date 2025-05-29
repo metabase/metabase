92249b46f55c8df7ca206374ea9f18c3a5908dae
Romeo Van Snick <romeo@romeovansnick.be>
2025-05-29 11:32:32 +0200

    Add e2e test for metabase#57685

--- e2e/test/scenarios/visualizations-tabular/visualizations-tabular-reproductions.cy.spec.js
+++ e2e/test/scenarios/visualizations-tabular/visualizations-tabular-reproductions.cy.spec.js
@@ -1508,3 +1508,23 @@ describe("issue 55637", () => {
     cy.findByTestId("column-info").should("not.exist");
   });
 });
+
+describe("issue 57685", () => {
+  beforeEach(() => {
+    H.restore();
+    cy.signInAsNormalUser();
+    H.createNativeQuestion(
+      {
+        display: "table",
+        native: {
+          query: 'SELECT id as "" FROM PRODUCTS',
+        },
+      },
+      { visitQuestion: true },
+    );
+  });
+
+  it("should handle empty column names without error (metabase#57685)", () => {
+    cy.findByTestId("visualization-root").icon("warning").should("not.exist");
+  });
+});
