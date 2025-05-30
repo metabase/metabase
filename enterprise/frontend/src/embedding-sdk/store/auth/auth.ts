e6afa64e52d0cf6ff4318d047f7fdc672f38d2a7
Phoomparin Mano <phoomparin@gmail.com>
2025-05-30 19:48:38 +0700

    move validateSessionToken into auth-common

--- enterprise/frontend/src/embedding-sdk/store/auth/auth.ts
+++ enterprise/frontend/src/embedding-sdk/store/auth/auth.ts
@@ -1,10 +1,9 @@
-import * as Yup from "yup";
-
 import {
   connectToInstanceAuthSso,
   jwtDefaultRefreshTokenFunction,
   openSamlLoginPopup,
   samlTokenStorage,
+  validateSessionToken,
 } from "embedding/auth-common";
 import type {
   MetabaseAuthConfig,
@@ -119,33 +118,8 @@ export const refreshTokenAsync = createAsyncThunk(
       authMethod,
       fetchRequestToken: customGetRefreshToken,
     });
+    validateSessionToken(session);
 
-    if (!session || typeof session !== "object") {
-      throw MetabaseError.INVALID_SESSION_OBJECT({
-        expected: "{ jwt: string }",
-        actual: JSON.stringify(session, null, 2),
-      });
-    }
-    if ("status" in session && session.status !== "ok") {
-      if ("message" in session && typeof session.message === "string") {
-        throw MetabaseError.INVALID_SESSION_OBJECT({
-          expected: "{ jwt: string }",
-          actual: session.message,
-        });
-      }
-      if (typeof session.status === "string") {
-        throw MetabaseError.INVALID_SESSION_OBJECT({
-          expected: "{ jwt: string }",
-          actual: session.status,
-        });
-      }
-    }
-    if (!sessionSchema.isValidSync(session)) {
-      throw MetabaseError.INVALID_SESSION_SCHEMA({
-        expected: "{ id: string, exp: number, iat: number, status: string }",
-        actual: JSON.stringify(session, null, 2),
-      });
-    }
     return session;
   },
 );
@@ -178,13 +152,6 @@ const getRefreshToken = async ({
     `Unknown or missing method: ${method}, response: ${JSON.stringify(urlResponseJson, null, 2)}`,
   );
 };
-
-const sessionSchema = Yup.object({
-  id: Yup.string().required(),
-  exp: Yup.number().required(),
-  // We should also receive `iat` and `status` in the response, but we don't actually need them
-  // as we don't use them, so we don't throw an error if they are missing
-});
 
 export function getSdkRequestHeaders(hash?: string): Record<string, string> {
   return {
