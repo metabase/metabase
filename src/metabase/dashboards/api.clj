2a42f773cf49d8d96e28d27983da6a6e3f003bd8
Dan Stone <wotbrew@gmail.com>
2025-05-30 11:16:35 +0100

    Fix not unsetting the action_id on dashboard edit (#58695)

--- src/metabase/dashboards/api.clj
+++ src/metabase/dashboards/api.clj
@@ -987,7 +987,9 @@
         (assoc-in dashcard [:visualization_settings :primitive_action] action-id)
         ;; Unpack encoded actions
         (neg-int? action-id)
-        (let [[op param] (actions/unpack-encoded-action-id action-id)]
+        (let [[op param] (actions/unpack-encoded-action-id action-id)
+              ;; very import we assoc nil rather than dissoc as otherwise shallow-updates will ignore this
+              dashcard   (assoc dashcard :action_id nil)]
           ;; This would be much better handled as multimethod, but hopefully the hacks don't live much longer.
           (if (isa? op :table.row/common)
             (assoc-in dashcard [:visualization_settings :table_action] {:kind (u/qualified-name op), :table_id param})
