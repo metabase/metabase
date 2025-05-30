ee33943abbbd08a80c1732d225f3a274a77d6280
Phoomparin Mano <phoomparin@gmail.com>
2025-05-30 15:47:30 +0700

    do not store saml tokens on host page

--- enterprise/frontend/src/embedding-sdk/store/auth/index.ts
+++ enterprise/frontend/src/embedding-sdk/store/auth/index.ts
@@ -1,1 +1,2 @@
 export * from "./auth";
+export * from "./saml-token-storage";
--- enterprise/frontend/src/embedding/auth-common/index.ts
+++ enterprise/frontend/src/embedding/auth-common/index.ts
@@ -1,5 +1,4 @@
 export { connectToInstanceAuthSso } from "./connect-to-instance-auth-sso";
 export { jwtDefaultRefreshTokenFunction } from "./jwt";
 export { openSamlLoginPopup } from "./saml";
-export { samlTokenStorage } from "./saml-token-storage";
 export { validateSessionToken } from "./validate-session-token";
