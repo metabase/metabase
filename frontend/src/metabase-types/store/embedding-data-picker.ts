7a05c79ae3cc8e6b21381f44db898d53ca6f25fd
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-05-30 15:38:10 +0700

    Fix the picker name typo

--- frontend/src/metabase-types/store/embedding-data-picker.ts
+++ frontend/src/metabase-types/store/embedding-data-picker.ts
@@ -8,7 +8,7 @@ export const DEFAULT_EMBEDDING_ENTITY_TYPES: EmbeddingEntityType[] = [
 ];
 
 /**
- * `question` only works on multi-staged data picker, not the simple data picker.
+ * `question` only works on multi-stage data picker, not the simple data picker.
  * The reason being that we want to streamline user experience for simple embedding
  * use cases, but `question` was later added to support users who are used to
  * selecting Saved questions in interactive embedding, so this is special case.
