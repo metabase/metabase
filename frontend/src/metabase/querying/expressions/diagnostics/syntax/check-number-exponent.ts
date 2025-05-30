c36afe26e4ab9b753c1d9191ef5c63c394e3e3c6
Romeo Van Snick <romeo@romeovansnick.be>
2025-05-30 10:48:46 +0200

    Use token text instead of slicing source

--- frontend/src/metabase/querying/expressions/diagnostics/syntax/check-number-exponent.ts
+++ frontend/src/metabase/querying/expressions/diagnostics/syntax/check-number-exponent.ts
@@ -3,19 +3,13 @@ import { t } from "ttag";
 import { NUMBER, type Token } from "../../pratt";
 import { error } from "../utils";
 
-export function checkNumberExponent({
-  source,
-  tokens,
-}: {
-  source: string;
-  tokens: Token[];
-}) {
+export function checkNumberExponent({ tokens }: { tokens: Token[] }) {
   for (const token of tokens) {
     if (token.type !== NUMBER) {
       continue;
     }
 
-    const value = source.slice(token.from, token.to).toLowerCase();
+    const value = token.text.toLowerCase();
     const [, exponent, ...rest] = value.split("e");
 
     if (typeof exponent === "string" && !exponent.match(/[0-9]$/)) {
