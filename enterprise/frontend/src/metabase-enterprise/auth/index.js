8dd23025c878928a1f0797459fb2694a74517192
Ryan Laurie <iethree@gmail.com>
2025-05-30 20:12:11 -0600

    update JWT settings

--- enterprise/frontend/src/metabase-enterprise/auth/index.js
+++ enterprise/frontend/src/metabase-enterprise/auth/index.js
@@ -1,9 +1,6 @@
-import { t } from "ttag";
-
 import { LOGIN, LOGIN_GOOGLE } from "metabase/auth/actions";
 import MetabaseSettings from "metabase/lib/settings";
 import {
-  PLUGIN_ADMIN_SETTINGS_UPDATES,
   PLUGIN_AUTH_PROVIDERS,
   PLUGIN_IS_PASSWORD_USER,
   PLUGIN_LDAP_FORM_FIELDS,
@@ -18,70 +15,9 @@ import {
   LdapGroupMembershipFilter,
   LdapUserProvisioning,
 } from "./components/Ldap";
-import SettingsJWTForm from "./components/SettingsJWTForm";
+import { SettingsJWTForm } from "./components/SettingsJWTForm";
 import { SettingsSAMLForm } from "./components/SettingsSAMLForm";
 import { SsoButton } from "./components/SsoButton";
-
-PLUGIN_ADMIN_SETTINGS_UPDATES.push((sections) => ({
-  ...sections,
-  "authentication/jwt": {
-    component: SettingsJWTForm,
-    getHidden: () => !hasPremiumFeature("sso_jwt"),
-    settings: [
-      {
-        key: "jwt-enabled",
-        display_name: t`JWT Authentication`,
-        type: "boolean",
-      },
-      {
-        key: "jwt-user-provisioning-enabled?",
-        display_name: t`User Provisioning`,
-        // eslint-disable-next-line no-literal-metabase-strings -- This string only shows for admins.
-        description: t`When a user logs in via JWT, create a Metabase account for them automatically if they don't have one.`,
-        type: "boolean",
-      },
-      {
-        key: "jwt-identity-provider-uri",
-        display_name: t`JWT Identity Provider URI`,
-        placeholder: "https://jwt.yourdomain.org",
-        type: "string",
-        required: false,
-        autoFocus: true,
-        getHidden: (_, derivedSettings) => !derivedSettings["jwt-enabled"],
-      },
-      {
-        key: "jwt-shared-secret",
-        display_name: t`String used by the JWT signing key`,
-        type: "text",
-        required: true,
-        getHidden: (_, derivedSettings) => !derivedSettings["jwt-enabled"],
-      },
-      {
-        key: "jwt-attribute-email",
-        display_name: t`Email attribute`,
-        type: "string",
-      },
-      {
-        key: "jwt-attribute-firstname",
-        display_name: t`First name attribute`,
-        type: "string",
-      },
-      {
-        key: "jwt-attribute-lastname",
-        display_name: t`Last name attribute`,
-        type: "string",
-      },
-      {
-        key: "jwt-group-sync",
-        display_name: t`Synchronize group memberships`,
-        description: null,
-      },
-      {
-        key: "jwt-group-mappings",
-      },
-    ],
-  },
-}));
 
 const SSO_PROVIDER = {
   name: "sso",
@@ -90,6 +26,10 @@ const SSO_PROVIDER = {
 
 if (hasPremiumFeature("sso_saml")) {
   PLUGIN_AUTH_PROVIDERS.SettingsSAMLForm = SettingsSAMLForm;
+}
+
+if (hasPremiumFeature("sso_jwt")) {
+  PLUGIN_AUTH_PROVIDERS.SettingsJWTForm = SettingsJWTForm;
 }
 
 PLUGIN_AUTH_PROVIDERS.AuthSettingsPage = AuthSettingsPage;
