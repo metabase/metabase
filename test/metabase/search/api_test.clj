bea21dd0ba8cc8f5f53c46f13728c0095fcc0a9a
Nathan Voxland <nathan@metabase.com>
2025-06-02 10:32:22 -0500

    Re-index again

--- test/metabase/search/api_test.clj
+++ test/metabase/search/api_test.clj
@@ -1757,7 +1757,7 @@
 
 (deftest ^:synchronized multiple-limits
   (search.tu/with-sync-search-indexing
-    ;(search/init-index! {:force-reset? false :re-populate? false})
+    (search/init-index!)
     (testing "Multiple `limit` query args should be handled correctly (#45345)"
       (is (= {} (mt/user-real-request :crowberto :get 500 "search?q=product")))
       ;; This test is failing with "no index" for some reason, forcing the reindex
