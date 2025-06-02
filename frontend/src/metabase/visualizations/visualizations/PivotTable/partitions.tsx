97135a0b64e7c151a701a96a89e025f30a98fc17
Noah Moss <noahbmoss@gmail.com>
2025-06-02 14:30:38 -0400

    add new native pivot split setting

--- frontend/src/metabase/visualizations/visualizations/PivotTable/partitions.tsx
+++ frontend/src/metabase/visualizations/visualizations/PivotTable/partitions.tsx
@@ -8,7 +8,7 @@ import { PivotTableSettingLabel } from "./PivotTable.styled";
 
 export interface Partition {
   name: "rows" | "columns" | "values";
-  columnFilter: (col: DatasetColumn | undefined) => boolean;
+  columnFilter?: (col: DatasetColumn | undefined) => boolean;
   title: React.ReactNode;
 }
 
@@ -32,6 +32,30 @@ export const partitions: Partition[] = [
   {
     name: "values",
     columnFilter: (col) => !isDimension(col),
+    title: (
+      // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
+      <PivotTableSettingLabel data-testid="pivot-table-setting">{t`Measures`}</PivotTableSettingLabel>
+    ),
+  },
+];
+
+export const nativePartitions: Partition[] = [
+  {
+    name: "rows",
+    title: (
+      // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
+      <PivotTableSettingLabel data-testid="pivot-table-setting">{t`Rows`}</PivotTableSettingLabel>
+    ),
+  },
+  {
+    name: "columns",
+    title: (
+      // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
+      <PivotTableSettingLabel data-testid="pivot-table-setting">{t`Columns`}</PivotTableSettingLabel>
+    ),
+  },
+  {
+    name: "values",
     title: (
       // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
       <PivotTableSettingLabel data-testid="pivot-table-setting">{t`Measures`}</PivotTableSettingLabel>
