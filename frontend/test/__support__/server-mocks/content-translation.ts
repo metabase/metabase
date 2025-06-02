e1639c3ec24d3d7daecfc22c64eeae03d35d41bc
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-06-02 13:01:56 -0400

    simplify unit tests involving content translations

--- frontend/test/__support__/server-mocks/content-translation.ts
+++ frontend/test/__support__/server-mocks/content-translation.ts
@@ -1,34 +1,27 @@
 import fetchMock from "fetch-mock";
 
 import * as EnterpriseContentTranslationUtilsModule from "metabase-enterprise/content_translation/utils";
-import type { DictionaryResponse } from "metabase-types/api";
 
-export function setupContentTranslationEndpoints({
-  dictionary = [],
-  uploadSuccess = true,
-}: {
-  dictionary?: DictionaryResponse["data"];
-  uploadSuccess?: boolean;
-} = {}) {
-  fetchMock.get(
-    "path:/api/content-translation/dictionary",
-    (url): DictionaryResponse => {
-      const localeCode = new URL(url).searchParams.get("locale");
-      const data = localeCode
-        ? dictionary.filter((row) => row.locale === localeCode)
-        : dictionary;
-      return { data };
-    },
-  );
+export function setupContentTranslationEndpoints() {
   fetchMock.post("path:/api/ee/content-translation/upload-dictionary", () => ({
-    success: uploadSuccess,
+    success: true,
   }));
 }
 
-/** To check that no content translation was performed, use this spy to assert
- * that the translateContentString utility function was not invoked */
+/**
+ * One of the utility functions that makes the content translation feature tick
+ * is translateContentString. It takes a msgid and returns a msgstr.
+ * setupTranslateContentStringSpy mocks out this utility function. By default,
+ * it also mocks out the implementation, so that `translated_${msgid}` is
+ * returned. This makes unit testing easier.
+ *
+ * To check that no content translation was performed, use this spy to assert
+ * that the translateContentString utility function was not invoked
+ * */
 export const setupTranslateContentStringSpy = (
-  mockImplementation?: EnterpriseContentTranslationUtilsModule.TranslateContentStringFunction,
+  mockImplementation: EnterpriseContentTranslationUtilsModule.TranslateContentStringFunction = (
+    ...[_dictionary, _locale, msgid]
+  ) => `translated_${msgid}`,
 ) => {
   let translateContentStringSpy: jest.SpyInstance;
 
