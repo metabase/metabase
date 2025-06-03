c5dcf705296f6463aeff06f1e7a1464af48eb3fc
William Zimrin <william@metabase.com>
2025-06-03 11:49:47 -0500

    fix more tests

--- test/metabase/driver/common/parameters/temporal_units_test.clj
+++ test/metabase/driver/common/parameters/temporal_units_test.clj
@@ -10,8 +10,8 @@
 
 (defn- ->local-date-time [t]
   (cond-> t
+    (= (count t) 10) (str "T00:00:00")
     (str/includes? t " ") (str/replace " " "T")
-    (str/includes? t "T") (t/zoned-date-time)
     true t/local-date-time))
 
 (defn- run-sample-query [query]
@@ -35,7 +35,7 @@
                                                      :name   "time-unit"
                                                      :target [:variable [:template-tag "time-unit"]]}])
             date-types ["minute" "hour" "day" "week" "month" "quarter" "year"]
-            count-types ["second-of-minute" "minute-of-hour" "hour-of-day" "day-of-month"
+            count-types ["minute-of-hour" "hour-of-day" "day-of-month"
                          "day-of-year" "month-of-year" "quarter-of-year"]
             ;; The original dates are 2019-02-11T21:40:27.892 and 2018-05-15T08:04:04.580
             expected-dates [[[#t "2019-02-11T21:40:00"] [#t "2018-05-15T08:04:00"]]
@@ -45,8 +45,7 @@
                             [[#t "2019-02-01T00:00:00"] [#t "2018-05-01T00:00:00"]]
                             [[#t "2019-01-01T00:00:00"] [#t "2018-04-01T00:00:00"]]
                             [[#t "2019-01-01T00:00:00"] [#t "2018-01-01T00:00:00"]]]
-            expected-counts [[[27] [4]]
-                             [[40] [4]]
+            expected-counts [[[40] [4]]
                              [[21] [8]]
                              [[11] [15]]
                              [[42] [135]]
