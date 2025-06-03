55f25402e50e46c63ac2e20257da179aa208178f
Kamil Mielnik <kamil@kamilmielnik.com>
2025-06-03 20:54:44 +0700

    Use FilterPickerBody

--- frontend/src/metabase/metadata/pages/DataModel/components/PreviewSection/FilteringPreview.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/components/PreviewSection/FilteringPreview.tsx
@@ -1,3 +1,12 @@
+import _ from "underscore";
+
+import { createMockMetadata } from "__support__/metadata";
+import {
+  useGetDatabaseQuery,
+  useGetTableQueryMetadataQuery,
+} from "metabase/api";
+import { FilterPickerBody } from "metabase/querying/filters/components/FilterPicker/FilterPickerBody";
+import * as Lib from "metabase-lib";
 import type { DatabaseId, Field, FieldId, TableId } from "metabase-types/api";
 
 interface Props {
@@ -7,6 +16,41 @@ interface Props {
   tableId: TableId;
 }
 
-export function FilteringPreview(props: Props) {
-  return <>filtering</>;
+export function FilteringPreview({
+  databaseId,
+  field,
+  fieldId,
+  tableId,
+}: Props) {
+  const { data: database } = useGetDatabaseQuery({ id: databaseId }); // TODO plugins
+  const { data: table } = useGetTableQueryMetadataQuery({ id: tableId }); // TODO plugins
+  const metadataProvider = Lib.metadataProvider(
+    databaseId,
+    createMockMetadata({
+      databases: database ? [database] : [],
+      fields: field ? [field] : [],
+      tables: table ? [table] : [],
+    }),
+  );
+
+  const query = Lib.fromLegacyQuery(databaseId, metadataProvider, {
+    type: "query",
+    database: databaseId,
+    query: {
+      "source-table": tableId,
+    },
+  });
+  const stageIndex = 0;
+  const column = Lib.fromLegacyColumn(query, stageIndex, field);
+
+  // const column = Lib.legacyColumnTypeInfo(field);
+
+  return (
+    <FilterPickerBody
+      column={column}
+      query={query}
+      stageIndex={stageIndex}
+      onChange={_.noop}
+    />
+  );
 }
