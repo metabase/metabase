c046a49822256d443c379473b680290f1d2eaeb4
Kamil Mielnik <kamil@kamilmielnik.com>
2025-06-03 21:15:05 +0700

    Another attempt

--- frontend/src/metabase/metadata/pages/DataModel/components/PreviewSection/FilteringPreview.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/components/PreviewSection/FilteringPreview.tsx
@@ -1,13 +1,25 @@
 import _ from "underscore";
 
-import { createMockMetadata } from "__support__/metadata";
 import {
-  useGetDatabaseQuery,
+  useGetDatabaseMetadataQuery,
   useGetTableQueryMetadataQuery,
 } from "metabase/api";
+import { LoadingAndErrorWrapper } from "metabase/components/LoadingAndErrorWrapper";
+import { useSelector } from "metabase/lib/redux";
+import { getRawTableFieldId } from "metabase/metadata/utils/field";
 import { FilterPickerBody } from "metabase/querying/filters/components/FilterPicker/FilterPickerBody";
+import { getMetadata } from "metabase/selectors/metadata";
 import * as Lib from "metabase-lib";
-import type { DatabaseId, Field, FieldId, TableId } from "metabase-types/api";
+import type Metadata from "metabase-lib/v1/metadata/Metadata";
+import type {
+  Database,
+  DatabaseId,
+  Field,
+  FieldId,
+  FieldReference,
+  Table,
+  TableId,
+} from "metabase-types/api";
 
 interface Props {
   databaseId: DatabaseId;
@@ -22,16 +34,19 @@ export function FilteringPreview({
   fieldId,
   tableId,
 }: Props) {
-  const { data: database } = useGetDatabaseQuery({ id: databaseId }); // TODO plugins
-  const { data: table } = useGetTableQueryMetadataQuery({ id: tableId }); // TODO plugins
-  const metadataProvider = Lib.metadataProvider(
-    databaseId,
-    createMockMetadata({
-      databases: database ? [database] : [],
-      fields: field ? [field] : [],
-      tables: table ? [table] : [],
-    }),
-  );
+  const { data: database, isFetching: isDatabaseFetching } =
+    useGetDatabaseMetadataQuery({ id: databaseId }); // TODO plugins
+  const { data: table, isFetching: isTableFetching } =
+    useGetTableQueryMetadataQuery({ id: tableId }); // TODO plugins
+
+  const metadata = useSelector(getMetadata);
+  // const metadata = createMockMetadata({
+  //   databases: database ? [database] : [],
+  //   fields: field ? [field] : [],
+  //   tables: table ? [table] : [],
+  // });
+  // const metadata = createMinimumMetadata(database, table, field);
+  const metadataProvider = Lib.metadataProvider(databaseId, metadata);
 
   const query = Lib.fromLegacyQuery(databaseId, metadataProvider, {
     type: "query",
@@ -41,9 +56,22 @@ export function FilteringPreview({
     },
   });
   const stageIndex = 0;
-  const column = Lib.fromLegacyColumn(query, stageIndex, field);
-
+  const fieldRef: FieldReference = ["field", fieldId, null];
+  const columns = Lib.visibleColumns(query, stageIndex);
+  const [index] = Lib.findColumnIndexesFromLegacyRefs(
+    query,
+    stageIndex,
+    columns,
+    [fieldRef],
+  );
+  const column = columns[index];
   // const column = Lib.legacyColumnTypeInfo(field);
+
+  if (isTableFetching || isDatabaseFetching || !database || !table) {
+    return (
+      <LoadingAndErrorWrapper loading={isTableFetching || isDatabaseFetching} />
+    );
+  }
 
   return (
     <FilterPickerBody
@@ -53,4 +81,23 @@ export function FilteringPreview({
       onChange={_.noop}
     />
   );
+}
+
+function createMinimumMetadata(
+  database: Database,
+  table: Table,
+  field: Field,
+): Metadata {
+  const metadata: Metadata = {
+    // @ts-expect-error now we know
+    databases: { [database.id]: database },
+    // @ts-expect-error now we know
+    tables: { [table.id]: table },
+    // @ts-expect-error we know now
+    fields: Object.fromEntries(
+      (table.fields ?? []).map((field) => [getRawTableFieldId(field), field]),
+    ),
+  };
+
+  return metadata;
 }
