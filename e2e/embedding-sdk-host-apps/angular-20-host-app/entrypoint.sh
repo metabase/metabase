4890f87f6ac6b204b3280ac29a560cb51b83a7b3
Timofei <timofei@metabase.com>
2025-06-04 12:38:57 +0400

    Add audit checks for Host Apps

--- e2e/embedding-sdk-host-apps/angular-20-host-app/entrypoint.sh
+++ e2e/embedding-sdk-host-apps/angular-20-host-app/entrypoint.sh
@@ -2,7 +2,10 @@
 set -e
 
 rm -rf dist
+
 yarn install --frozen-lockfile
+# To fail for audit warnings and prevent triggering warnings on the Github
+yarn audit --level low
 
 if [ "$WATCH" = "true" ]; then
   yarn watch --port $CLIENT_PORT
