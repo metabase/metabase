a78df3ddda34f31866b3c8b21521fc95767852f7
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-06-04 21:09:38 +0700

    Add an option to opt in to multi-staged data picker (#58575)
    * Add an option to force multi-staged data picker
    
    * Move type to a proper location
    
    * Add `question` to `entity_types`
    
    * Add ability to select saved questions back to multi-staged data picker
    
    * Auto select Questions or Models when there is only a single option
    
    * Prevent accidental types
    
    * Remove unreachable code
    
    * Default the join step after selecting a question to the question step
    
    * Fix type errors
    
    * Improve typing + fix unit tests
    
    * Remove unnecessary await
    
    * Add unit tests
    
    * Update the default simple data picker value
    
    * Fix the picker name typo
    
    * Ignore `question` in Simple data picker
    
    * Add E2E tests
    
    * Move a constant out of a type file
    
    * Fix types
    
    * Address review. Make code more sane
    
    * Address review. Improve variable name clarity.
    
    ---------
    
    Co-authored-by: Oisin Coveney <oisin@metabase.com>

--- frontend/src/metabase/plugins/index.ts
+++ frontend/src/metabase/plugins/index.ts
@@ -34,7 +34,6 @@ import type {
 } from "metabase/browse/models";
 import type { LinkProps } from "metabase/core/components/Link";
 import type { DashCardMenuItem } from "metabase/dashboard/components/DashCard/DashCardMenu/DashCardMenu";
-import type { EmbeddingEntityType } from "metabase/embedding-sdk/store";
 import type { DataSourceSelectorProps } from "metabase/embedding-sdk/types/components/data-picker";
 import { getIconBase } from "metabase/lib/icon";
 import type { MetabotContext } from "metabase/metabot";
@@ -84,6 +83,7 @@ import type {
   Dispatch,
   State,
 } from "metabase-types/store";
+import type { EmbeddingEntityType } from "metabase-types/store/embedding-data-picker";
 
 import type { GetAuthProviders, PluginGroupManagersType } from "./types";
 
