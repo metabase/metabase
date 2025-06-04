b92e15aa6186a28191f6daedbcffa50d0a621bed
Edward Paget <ed.paget@gmail.com>
2025-06-04 15:03:59 -0500

    fix: improve effective children performance
    on the /items endpoint by reusing the permission CTE
    
    Co-Authored-by: John Swanson <john.swanson@metabase.com>

--- src/metabase/collections/api.clj
+++ src/metabase/collections/api.clj
@@ -623,6 +623,7 @@
   (-> (assoc
        (collection/effective-children-query
         collection
+        {:cte-name :visible_collection_ids}
         (if archived?
           [:or
            [:= :archived true]
@@ -908,7 +909,8 @@
                           (update select-clause-type add-model-ranking model)))
         viz-config  {:include-archived-items :all
                      :archive-operation-id nil
-                     :permission-level (if archived? :write :read)}
+                     :permission-level (if archived? :write :read)
+                     :include-trash-collection? archived?}
         rows-query  {:with     [[:visible_collection_ids (collection/visible-collection-query viz-config)]]
                      :select   [:* [[:over [[:count :*] {} :total_count]]]]
                      :from     [[{:union-all queries} :dummy_alias]]
