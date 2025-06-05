dc9df1d8120212c5fb78470ebd1d4df55b531eb4
Nicolò Pretto <info@npretto.com>
2025-06-05 15:53:03 +0200

    welcome step and final step

--- frontend/src/metabase/setup/components/EmbeddingSetup/steps/WelcomeStep.tsx
+++ frontend/src/metabase/setup/components/EmbeddingSetup/steps/WelcomeStep.tsx
@@ -1,24 +1,67 @@
 import { Link } from "react-router";
-import { t } from "ttag";
+import { c, t } from "ttag";
 
-import { Box, Button, Text } from "metabase/ui";
+import { useSelector } from "metabase/lib/redux";
+import { getUser } from "metabase/selectors/user";
+import { Box, Button, List, Space, Text, Title } from "metabase/ui";
 
 export const WelcomeStep = () => {
+  const user = useSelector(getUser);
+
   return (
-    <Box>
-      <Text size="xl" fw="bold" mb="md">
-        {t`Welcome to Embedding Setup`}
+    <Box
+      p="2xl"
+      style={{ maxWidth: 580, borderRadius: 16, boxShadow: "var(--shadow-md)" }}
+      // mx="auto"
+      my="xxl"
+      bg="white"
+    >
+      <Title order={2} mb="lg">
+        {c("{0} is the first name of the logged in user")
+          .t`Howdy, ${user?.first_name}`}
+      </Title>
+      <Title order={2} mb="lg">
+        {t`Let's get you up and running with a starting setup for embedded analytics`}
+      </Title>
+
+      <Text size="lg" mb="md">
+        {t`You'll get to add working starter content to your app based on your real data.`}
       </Text>
-      <Text mb="xl">
-        {t`Let's get your database connected and set up embedding for your application.`}
+      <Text size="lg" mb="md">
+        {t`This will give you a solid base to customize and keep building off of on your way to production.`}
       </Text>
+
+      <Space h="xl" />
+
+      <Text size="lg" mb="xs">{t`Requirements:`}</Text>
+      <Box mb="xl" pl="lg" style={{ paddingLeft: 24 }}>
+        <List size="lg">
+          <List.Item>
+            {t`Access to your app or a sample app you want to use to experiment`}
+          </List.Item>
+        </List>
+      </Box>
+
+      <Space h="xl" />
+
       <Button
         component={Link}
         to="/setup/embedding/data-connection"
         variant="filled"
+        mb="md"
+        miw={"12rem"}
       >
-        {t`Get Started`}
+        {t`Start`}
       </Button>
+
+      <Space h="lg" />
+
+      <Text>
+        <Link
+          to="/setup/embedding/manual"
+          style={{ color: "#888" }}
+        >{t`Set up manually`}</Link>
+      </Text>
     </Box>
   );
 };
