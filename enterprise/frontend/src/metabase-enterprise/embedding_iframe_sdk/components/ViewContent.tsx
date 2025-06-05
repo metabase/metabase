eceece6f56f2fe50ae7584651b29df33bf14db6d
Phoomparin Mano <phoomparin@gmail.com>
2025-06-05 20:09:20 +0700

    basic setup for ViewContent

--- enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk/components/ViewContent.tsx
+++ enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk/components/ViewContent.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { P, match } from "ts-pattern";
 
 import { CollectionBrowser } from "embedding-sdk/components/public/CollectionBrowser";
 import { InteractiveDashboard } from "embedding-sdk/components/public/InteractiveDashboard";
 import { InteractiveQuestion } from "embedding-sdk/components/public/InteractiveQuestion";
+import { ActionIcon, Icon, Stack } from "metabase/ui";
 
 import type { SdkIframeEmbedSettings } from "../types/embed";
 
@@ -11,39 +13,54 @@ interface ViewContentProps {
 }
 
 export function ViewContent({ settings }: ViewContentProps) {
-  const [selected, setSelected] = useState<
+  const [selectedEntity, setSelectedEntity] = useState<
     | { type: "dashboard"; id: number | string }
     | { type: "question"; id: number | string }
     | null
   >(null);
 
-  if (selected) {
-    if (selected.type === "dashboard") {
-      return <InteractiveDashboard dashboardId={selected.id} withDownloads />;
-    }
-    if (selected.type === "question") {
-      return (
-        <InteractiveQuestion
-          questionId={selected.id}
-          height="100%"
-          withDownloads
-        />
-      );
-    }
-  }
-
-  return (
-    <CollectionBrowser
-      collectionId={settings.initialCollection}
-      visibleEntityTypes={settings.entityTypes ?? ["dashboard", "question"]}
-      onClick={(item) => {
-        if (item.model === "dashboard") {
-          setSelected({ type: "dashboard", id: item.id });
-        } else if (item.model === "card") {
-          setSelected({ type: "question", id: item.id });
-        }
-      }}
-      style={{ height: "100%" }}
-    />
+  const getNavBar = () => (
+    <Stack pt="md" px="lg" onClick={() => setSelectedEntity(null)}>
+      <ActionIcon variant="outline" radius="xl" size="2.625rem" color="border">
+        <Icon c="brand" name="arrow_left" />
+      </ActionIcon>
+    </Stack>
   );
+
+  return match(selectedEntity)
+    .with({ type: "dashboard", id: P.nonNullable }, ({ id }) => (
+      <Stack h="100%">
+        {getNavBar()}
+
+        <InteractiveDashboard dashboardId={id} withDownloads />
+      </Stack>
+    ))
+    .with({ type: "question", id: P.nonNullable }, ({ id }) => (
+      <Stack h="100%">
+        {getNavBar()}
+
+        <InteractiveQuestion questionId={id} height="100%" withDownloads />
+      </Stack>
+    ))
+    .otherwise(() => (
+      <Stack p="lg" maw="60rem" mx="auto">
+        <CollectionBrowser
+          collectionId={settings.initialCollection}
+          visibleEntityTypes={
+            settings.entityTypes ?? ["dashboard", "question", "collection"]
+          }
+          onClick={(item) =>
+            match(item)
+              .with({ model: "dashboard" }, ({ id }) =>
+                setSelectedEntity({ type: "dashboard", id }),
+              )
+              .with({ model: "card" }, ({ id }) =>
+                setSelectedEntity({ type: "question", id }),
+              )
+              .otherwise(() => {})
+          }
+          style={{ height: "100%" }}
+        />
+      </Stack>
+    ));
 }
