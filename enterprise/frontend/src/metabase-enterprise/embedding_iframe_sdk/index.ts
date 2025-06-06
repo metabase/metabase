44e8f207b138995a9426f1dfd9e412e9f3c5e357
Phoomparin Mano <phoomparin@gmail.com>
2025-06-06 18:02:58 +0700

    set config at entrypoint instead of ee plugin

--- enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk/index.ts
+++ enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk/index.ts
@@ -1,11 +1,7 @@
-import { EMBEDDING_SDK_IFRAME_EMBEDDING_CONFIG } from "metabase/embedding-sdk/config";
 import { PLUGIN_EMBEDDING_IFRAME_SDK } from "metabase/plugins";
 import { hasPremiumFeature } from "metabase-enterprise/settings";
 
 import { SdkIframeEmbedRoute } from "./components/SdkIframeEmbedRoute";
-
-// Use the iframe embedding auth flow instead of the regular auth flow.
-EMBEDDING_SDK_IFRAME_EMBEDDING_CONFIG.isSdkIframeEmbedAuth = true;
 
 // We allow users try out the iframe embedding without valid license on localhost.
 // This should always be usable on an EE instance regardless of license.
