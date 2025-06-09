fb6e21689aee23ed9efef090738e2eab72a786b0
Kamil Mielnik <kamil@kamilmielnik.com>
2025-06-09 19:22:44 +0700

    Get rid of second page component

--- frontend/src/metabase/metadata/pages/DataModel/DataModel.tsx
+++ frontend/src/metabase/metadata/pages/DataModel/DataModel.tsx
@@ -48,10 +48,28 @@ export const DataModel = ({
   location: Location;
   children: ReactNode;
 }) => {
-  const { databaseId, tableId, schemaId } = parseRouteParams(params);
+  const { databaseId, fieldId, tableId, schemaId } = parseRouteParams(params);
+  const isSegments = location.pathname.startsWith("/admin/datamodel/segment");
   const [isResizing, setIsResizing] = useState(false);
   const [navWidth, setNavWidth] = useState(columnSizesConfig.nav.initial);
   const { height, ref } = useElementSize();
+  const isEmptyStateShown =
+    databaseId == null || tableId == null || fieldId == null;
+  const {
+    data: table,
+    error,
+    isLoading,
+  } = useGetTableQueryMetadataQuery(
+    tableId == null
+      ? skipToken
+      : {
+          id: tableId,
+          include_sensitive_fields: true,
+          ...PLUGIN_FEATURE_LEVEL_PERMISSIONS.dataModelQueryProps,
+        },
+  );
+  const field = table?.fields?.find((field) => field.id === fieldId);
+  const [previewType, setPreviewType] = useState<PreviewType>("table");
 
   return (
     <Flex
@@ -86,127 +104,100 @@ export const DataModel = ({
           />
 
           <Box className={S.footer} mx="xl" py="sm">
-            <SegmentsLink
-              active={
-                location.pathname.startsWith("/admin/datamodel/segments") ||
-                location.pathname.startsWith("/admin/datamodel/segment/")
-              }
-              to="/admin/datamodel/segments"
-            />
+            <SegmentsLink active={isSegments} to="/admin/datamodel/segments" />
           </Box>
         </Stack>
       </ResizableBox>
 
-      {children}
-    </Flex>
-  );
-};
+      {isSegments && children}
 
-export function DataModelEditor({ params }: { params: RouteParams }) {
-  const { databaseId, tableId, fieldId } = parseRouteParams(params);
-  const isEmptyStateShown =
-    databaseId == null || tableId == null || fieldId == null;
-  const {
-    data: table,
-    error,
-    isLoading,
-  } = useGetTableQueryMetadataQuery(
-    tableId == null
-      ? skipToken
-      : {
-          id: tableId,
-          include_sensitive_fields: true,
-          ...PLUGIN_FEATURE_LEVEL_PERMISSIONS.dataModelQueryProps,
-        },
-  );
-  const field = table?.fields?.find((field) => field.id === fieldId);
-  const [previewType, setPreviewType] = useState<PreviewType>("table");
-
-  return (
-    <>
-      {tableId && (
-        <Box
-          bg="bg-white"
-          className={cx(S.column, S.rightBorder)}
-          flex="0 0 25%"
-          h="100%"
-          miw={rem(400)}
-        >
-          <LoadingAndErrorWrapper error={error} loading={isLoading}>
-            {table && (
-              <TableSection
-                /**
-                 * Make sure internal component state is reset when changing tables.
-                 * This is to avoid state mix-up with optimistic updates.
-                 */
-                key={table.id}
-                params={params}
-                table={table}
-              />
-            )}
-          </LoadingAndErrorWrapper>
-        </Box>
-      )}
-
-      {isEmptyStateShown && (
-        <Flex align="center" bg="bg-white" flex="1" justify="center">
-          <Box maw={rem(320)}>
-            <EmptyState
-              illustrationElement={<img src={EmptyDashboardBot} />}
-              title={
-                tableId
-                  ? t`Edit the table and fields`
-                  : t`Start by selecting data to model`
-              }
-              message={
-                tableId
-                  ? t`Select a field to edit it. Then change the display name, semantic type or filtering behavior.`
-                  : t`Browse your databases to find the table you’d like to edit.`
-              }
-            />
-          </Box>
-        </Flex>
-      )}
-
-      {!isEmptyStateShown && (
+      {!isSegments && (
         <>
-          <Box
-            bg="bg-white"
-            className={S.column}
-            flex="0 0 25%"
-            h="100%"
-            miw={rem(400)}
-          >
-            <LoadingAndErrorWrapper error={error} loading={isLoading}>
-              {field && (
-                <FieldSection
-                  databaseId={databaseId}
-                  field={field}
-                  /**
-                   * Make sure internal component state is reset when changing fields.
-                   * This is to avoid state mix-up with optimistic updates.
-                   */
-                  key={getRawTableFieldId(field)}
-                />
-              )}
-            </LoadingAndErrorWrapper>
-          </Box>
-
-          {field && table && (
-            <Box flex={`1 1 ${rem(200)}`} miw={0} p="xl">
-              <PreviewSection
-                databaseId={databaseId}
-                field={field}
-                fieldId={fieldId}
-                previewType={previewType}
-                table={table}
-                tableId={tableId}
-                onPreviewTypeChange={setPreviewType}
-              />
+          {tableId && (
+            <Box
+              bg="bg-white"
+              className={cx(S.column, S.rightBorder)}
+              flex="0 0 25%"
+              h="100%"
+              miw={rem(400)}
+            >
+              <LoadingAndErrorWrapper error={error} loading={isLoading}>
+                {table && (
+                  <TableSection
+                    /**
+                     * Make sure internal component state is reset when changing tables.
+                     * This is to avoid state mix-up with optimistic updates.
+                     */
+                    key={table.id}
+                    params={params}
+                    table={table}
+                  />
+                )}
+              </LoadingAndErrorWrapper>
             </Box>
+          )}
+
+          {isEmptyStateShown && (
+            <Flex align="center" bg="bg-white" flex="1" justify="center">
+              <Box maw={rem(320)}>
+                <EmptyState
+                  illustrationElement={<img src={EmptyDashboardBot} />}
+                  title={
+                    tableId
+                      ? t`Edit the table and fields`
+                      : t`Start by selecting data to model`
+                  }
+                  message={
+                    tableId
+                      ? t`Select a field to edit it. Then change the display name, semantic type or filtering behavior.`
+                      : t`Browse your databases to find the table you’d like to edit.`
+                  }
+                />
+              </Box>
+            </Flex>
+          )}
+
+          {!isEmptyStateShown && (
+            <>
+              <Box
+                bg="bg-white"
+                className={S.column}
+                flex="0 0 25%"
+                h="100%"
+                miw={rem(400)}
+              >
+                <LoadingAndErrorWrapper error={error} loading={isLoading}>
+                  {field && (
+                    <FieldSection
+                      databaseId={databaseId}
+                      field={field}
+                      /**
+                       * Make sure internal component state is reset when changing fields.
+                       * This is to avoid state mix-up with optimistic updates.
+                       */
+                      key={getRawTableFieldId(field)}
+                    />
+                  )}
+                </LoadingAndErrorWrapper>
+              </Box>
+
+              {field && table && (
+                <Box flex={`1 1 ${rem(200)}`} miw={0} p="xl">
+                  <PreviewSection
+                    databaseId={databaseId}
+                    field={field}
+                    fieldId={fieldId}
+                    previewType={previewType}
+                    table={table}
+                    tableId={tableId}
+                    onPreviewTypeChange={setPreviewType}
+                  />
+                </Box>
+              )}
+            </>
           )}
         </>
       )}
-    </>
+    </Flex>
   );
-}
+};
