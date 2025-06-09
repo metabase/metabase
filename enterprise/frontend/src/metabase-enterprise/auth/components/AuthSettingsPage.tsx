97c217e905a395d25df3bd169c1bfb940b8748a8
Fraser McIntosh <fraser@metabase.com>
2025-06-09 14:13:34 +1200

    Remove tabs from auth pages

--- enterprise/frontend/src/metabase-enterprise/auth/components/AuthSettingsPage.tsx
+++ enterprise/frontend/src/metabase-enterprise/auth/components/AuthSettingsPage.tsx
@@ -1,4 +1,3 @@
-import { push } from "react-router-redux";
 import { t } from "ttag";
 
 import { GoogleAuthCard } from "metabase/admin/settings/auth/containers/GoogleAuthCard";
@@ -7,9 +6,8 @@ import { ManageApiKeys } from "metabase/admin/settings/components/ApiKeys/Manage
 import { SettingsSection } from "metabase/admin/settings/components/SettingsSection";
 import { AdminSettingInput } from "metabase/admin/settings/components/widgets/AdminSettingInput";
 import { useHasTokenFeature } from "metabase/common/hooks";
-import { useDispatch } from "metabase/lib/redux";
 import { PLUGIN_AUTH_PROVIDERS } from "metabase/plugins";
-import { Stack, Tabs } from "metabase/ui";
+import { Stack } from "metabase/ui";
 
 import { JwtAuthCard } from "../containers/JwtAuthCard";
 import { SamlAuthCard } from "../containers/SamlAuthCard";
@@ -20,44 +18,15 @@ import { SessionTimeoutSetting } from "./SessionTimeoutSetting";
 type Tabs = "authentication" | "user-provisioning" | "api-keys";
 
 export function AuthSettingsPage({ tab = "authentication" }: { tab?: Tabs }) {
-  const dispatch = useDispatch();
+  if (tab === "api-keys") {
+    return <ManageApiKeys />;
+  }
 
-  const handleTabChange = (newTab: Tabs | "") => {
-    dispatch(push(`/admin/settings/authentication/${newTab}`));
-  };
+  if (tab === "user-provisioning") {
+    return <PLUGIN_AUTH_PROVIDERS.UserProvisioningSettings />;
+  }
 
-  const hasScim = useHasTokenFeature("scim");
-
-  return (
-    <Tabs mx="md" value={tab} maw="80rem">
-      <Tabs.List mb="lg">
-        <Tabs.Tab value="authentication" onClick={() => handleTabChange("")}>
-          {t`Authentication`}
-        </Tabs.Tab>
-        {hasScim && (
-          <Tabs.Tab
-            value="user-provisioning"
-            onClick={() => handleTabChange("user-provisioning")}
-          >
-            {t`User Provisioning`}
-          </Tabs.Tab>
-        )}
-        <Tabs.Tab value="api-keys" onClick={() => handleTabChange("api-keys")}>
-          {t`API Keys`}
-        </Tabs.Tab>
-      </Tabs.List>
-
-      <Tabs.Panel value="authentication">
-        <AuthenticationTab />
-      </Tabs.Panel>
-      <Tabs.Panel value="user-provisioning">
-        <PLUGIN_AUTH_PROVIDERS.UserProvisioningSettings />
-      </Tabs.Panel>
-      <Tabs.Panel value="api-keys">
-        <ManageApiKeys />
-      </Tabs.Panel>
-    </Tabs>
-  );
+  return <AuthenticationTab />;
 }
 
 function AuthenticationTab() {
