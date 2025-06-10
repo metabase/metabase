96123576cf64b13ac42b9b81b6d3340d06caec3f
Kamil Mielnik <kamil@kamilmielnik.com>
2025-06-10 20:43:35 +0700

    Add comment

--- frontend/src/metabase/metadata/pages/DataModel/constants.ts
+++ frontend/src/metabase/metadata/pages/DataModel/constants.ts
@@ -3,6 +3,6 @@ import type { Column, ColumnSizeConfig } from "./types";
 export const COLUMN_CONFIG: Record<Column, ColumnSizeConfig> = {
   nav: { initial: 320, min: 240, max: 440 },
   table: { initial: 320, min: 240, max: 640 },
-  field: { initial: 480, min: 280, max: 640 },
+  field: { initial: 480, min: 280, max: 640 }, // this is field + preview container
   preview: { initial: 400, min: 400, max: 640 },
 };
