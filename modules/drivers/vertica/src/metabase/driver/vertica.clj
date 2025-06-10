8e161473ed5f3c5ac0aee79ed7a55e827d2fa3b5
Case Nelson <case@metabase.com>
2025-06-09 21:07:49 -0600

    refactor: introduce driver-api (#59011)
    * refactor: introduce driver-api
    
    * Add kondo linting
    
    * Handle kondo
    
    * self review
    
    * Clean up imported vars
    
    * Rename qp.error-types
    
    * Fix missed clickhouse ref
    
    * Fix init settings
    
    * Fix table alias error
    
    * Fix kondo error
    
    * Fix mistake mbql.s
    
    * Fix var ref for get-method and kondo module config
    
    * Fix dynamic vars
    
    * Ignore exported multifns in changelog check
    
    * Fix desired-alias in rewrite clause
    
    * Use snowflake-jdbc-thin to avoid registering the embedded bouncycastle provider
    
    * Update merged code for driver-api

--- modules/drivers/vertica/src/metabase/driver/vertica.clj
+++ modules/drivers/vertica/src/metabase/driver/vertica.clj
@@ -5,6 +5,7 @@
    [honey.sql :as sql]
    [java-time.api :as t]
    [metabase.driver :as driver]
+   [metabase.driver-api.core :as driver-api]
    [metabase.driver.sql-jdbc :as sql-jdbc]
    [metabase.driver.sql-jdbc.common :as sql-jdbc.common]
    [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
@@ -15,7 +16,6 @@
    [metabase.driver.sql.query-processor.empty-string-is-null
     :as sql.qp.empty-string-is-null]
    [metabase.driver.sql.util :as sql.u]
-   [metabase.query-processor.timezone :as qp.timezone]
    [metabase.util :as u]
    [metabase.util.date-2 :as u.date]
    [metabase.util.honey-sql-2 :as h2x]
@@ -136,7 +136,7 @@
     (sql.u/validate-convert-timezone-args timestamptz? target-timezone source-timezone)
     (-> (if timestamptz?
           expr
-          (h2x/at-time-zone expr (or source-timezone (qp.timezone/results-timezone-id))))
+          (h2x/at-time-zone expr (or source-timezone (driver-api/results-timezone-id))))
         (h2x/at-time-zone target-timezone)
         (h2x/with-database-type-info "timestamp"))))
 
