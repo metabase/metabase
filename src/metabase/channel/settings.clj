45e7564f43bbf1edd04bf04be1261e5e60e681f3
Nathan Voxland <nathan@metabase.com>
2025-06-11 11:42:01 -0500

    Lint fix

--- src/metabase/channel/settings.clj
+++ src/metabase/channel/settings.clj
@@ -3,8 +3,7 @@
    [clojure.string :as str]
    [java-time.api :as t]
    [metabase.settings.core :as setting :refer [defsetting]]
-   [metabase.util.i18n :refer [deferred-tru]]
-   [metabase.util.i18n :refer [tru]]
+   [metabase.util.i18n :refer [deferred-tru tru]]
    [metabase.util.malli.registry :as mr]
    [metabase.util.malli.schema :as ms]
    [metabase.util.string :as u.str]))
