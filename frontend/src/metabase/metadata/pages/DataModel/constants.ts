39a9d3d0d9ad12dfd8394b5b1089e0a66e3f472b
Kamil Mielnik <kamil@kamilmielnik.com>
2025-06-11 16:57:21 +0700

    Update default constraints

--- frontend/src/metabase/metadata/pages/DataModel/constants.ts
+++ frontend/src/metabase/metadata/pages/DataModel/constants.ts
@@ -1,8 +1,8 @@
 import type { Column, ColumnSizeConfig } from "./types";
 
 export const COLUMN_CONFIG: Record<Column, ColumnSizeConfig> = {
-  nav: { initial: 320, min: 240, max: 440 },
-  table: { initial: 320, min: 240, max: 640 },
+  nav: { initial: 376, min: 280, max: 440 },
+  table: { initial: 376, min: 280, max: 640 },
   field: { initial: 480, min: 280, max: 640 }, // this is field + preview container
-  preview: { initial: 400, min: 400, max: 640 },
+  preview: { initial: 440, min: 440, max: 640 },
 };
