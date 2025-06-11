4c34b2de574a638a5e0cf0563e83d2195dfb19cf
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2025-06-11 14:44:58 +0300

    chore: cleanup native editor after refactoring

--- frontend/src/metabase/query_builder/components/NativeQueryEditor/types.tsx
+++ frontend/src/metabase/query_builder/components/NativeQueryEditor/types.tsx
@@ -7,3 +7,10 @@ export type SelectionRange = {
   start: Location;
   end: Location;
 };
+
+export type Features = {
+  dataReference?: boolean;
+  variables?: boolean;
+  snippets?: boolean;
+  promptInput?: boolean;
+};
