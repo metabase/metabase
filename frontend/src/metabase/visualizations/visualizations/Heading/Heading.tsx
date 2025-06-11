e8cbb9bf493bf84934855d084c0afe8287b7daa0
Sébastien <sebastien@metabase.com>
2025-06-11 18:36:02 +0200

    added back DashCardParameterMapper

--- frontend/src/metabase/visualizations/visualizations/Heading/Heading.tsx
+++ frontend/src/metabase/visualizations/visualizations/Heading/Heading.tsx
@@ -4,10 +4,13 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { t } from "ttag";
 
 import { ToolbarButton } from "metabase/components/ToolbarButton";
+import { DashCardParameterMapper } from "metabase/dashboard/components/DashCard/DashCardParameterMapper/DashCardParameterMapper";
 import { DashboardParameterList } from "metabase/dashboard/components/DashboardParameterList";
 import {
   getDashCardInlineValuePopulatedParameters,
+  getDashcardParameterMappingOptions,
   getEditingParameter,
+  getIsEditingParameter,
   getParameterValues,
 } from "metabase/dashboard/selectors";
 import { useTranslateContent } from "metabase/i18n/hooks";
@@ -45,6 +48,7 @@ export function Heading({
   isEditing,
   isFullscreen,
   onUpdateVisualizationSettings,
+  isMobile,
 }: HeadingProps) {
   const inlineParameters = useSelector((state) =>
     getDashCardInlineValuePopulatedParameters(state, dashcard?.id),
@@ -63,6 +67,16 @@ export function Heading({
   const preventDragging = (e: MouseEvent<HTMLInputElement>) => {
     e.stopPropagation();
   };
+
+  const isEditingParameter = useSelector(getIsEditingParameter);
+
+  const mappingOptions = useSelector((state) =>
+    getDashcardParameterMappingOptions(state, {
+      card: dashcard.card,
+      dashcard,
+    }),
+  );
+  const hasVariables = mappingOptions.length > 0;
 
   const container = useRef<HTMLDivElement>(null);
   const [isNarrow, setIsNarrow] = useState(false);
@@ -105,6 +119,43 @@ export function Heading({
   const hasContent = !isEmpty(settings.text);
   const placeholder = t`You can connect widgets to {{variables}} in heading cards.`;
 
+  let leftContent: JSX.Element | null;
+
+  if (hasVariables && isEditingParameter) {
+    leftContent = (
+      <DashCardParameterMapper dashcard={dashcard} isMobile={isMobile} />
+    );
+  } else if (isPreviewing) {
+    leftContent = (
+      <HeadingContent
+        data-testid="editing-dashboard-heading-preview"
+        isEditing={isEditing}
+        onMouseDown={preventDragging}
+        hasFilters={inlineParameters.length > 0}
+      >
+        {hasContent ? content : placeholder}
+      </HeadingContent>
+    );
+  } else {
+    leftContent = (
+      <HeadingTextInput
+        name="heading"
+        data-testid="editing-dashboard-heading-input"
+        placeholder={placeholder}
+        value={textValue}
+        autoFocus={justAdded || isFocused}
+        onChange={(e) => setTextValue(e.target.value)}
+        onMouseDown={preventDragging}
+        onBlur={() => {
+          toggleFocusOff();
+
+          if (settings.text !== textValue) {
+            onUpdateVisualizationSettings({ text: textValue });
+          }
+        }}
+      />
+    );
+  }
   if (isEditing) {
     return (
       <InputContainer
@@ -114,33 +165,7 @@ export function Heading({
         onClick={toggleFocusOn}
         ref={container}
       >
-        {isPreviewing ? (
-          <HeadingContent
-            data-testid="editing-dashboard-heading-preview"
-            isEditing={isEditing}
-            onMouseDown={preventDragging}
-            hasFilters={inlineParameters.length > 0}
-          >
-            {hasContent ? settings.text : placeholder}
-          </HeadingContent>
-        ) : (
-          <HeadingTextInput
-            name="heading"
-            data-testid="editing-dashboard-heading-input"
-            placeholder={placeholder}
-            value={textValue}
-            autoFocus={justAdded || isFocused}
-            onChange={(e) => setTextValue(e.target.value)}
-            onMouseDown={preventDragging}
-            onBlur={() => {
-              toggleFocusOff();
-
-              if (settings.text !== textValue) {
-                onUpdateVisualizationSettings({ text: textValue });
-              }
-            }}
-          />
-        )}
+        {leftContent}
         {inlineParameters.length > 0 && (
           <ParametersList
             isNarrow={isNarrow}
