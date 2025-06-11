510f0c36ed120b7f4d241e08592ae9884e2188f9
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-06-12 00:57:19 +0200

    Reflect changes from #58405

--- frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/BrowseNavSection.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/BrowseNavSection.tsx
@@ -55,18 +55,6 @@ export const BrowseNavSection = ({
       </Group>
 
       <Collapse in={opened} transitionDuration={0} role="tabpanel">
-        {(!isEmbeddingIframe || entityTypes.includes("model")) && (
-          <PaddedSidebarLink
-            icon="model"
-            url={BROWSE_MODELS_URL}
-            isSelected={nonEntityItem?.url?.startsWith(BROWSE_MODELS_URL)}
-            onClick={onItemSelect}
-            aria-label={t`Browse models`}
-          >
-            {t`Models`}
-          </PaddedSidebarLink>
-        )}
-
         {hasDataAccess &&
           (!isEmbeddingIframe || entityTypes.includes("table")) && (
             <PaddedSidebarLink
