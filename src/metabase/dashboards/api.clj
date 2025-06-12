770df0dd26fa47983ee77aea0600c551628dba53
Chris Truter <chris@metabase.com>
2025-06-12 13:55:59 +0200

    More readable with format

--- src/metabase/dashboards/api.clj
+++ src/metabase/dashboards/api.clj
@@ -951,13 +951,12 @@
     ;; new action, give it an id
     ;; currently the frontend is generating its own ids... we need to replace those
     (or (not id) (not (str/starts-with? id "dashcard:")))
-    (str "dashcard:"
-         (let [dashcard-id (:id dashcard "unknown")]
-           (if (pos-int? dashcard-id)
-             dashcard-id
-             "unknown"))
-         ":"
-         (u/generate-nano-id))
+    (format "dashcard:%s:%s"
+            (let [dashcard-id (:id dashcard "unknown")]
+              (if (pos-int? dashcard-id)
+                dashcard-id
+                "unknown"))
+            (u/generate-nano-id))
     ;; chicken-and-egg resulted in a suboptimal id, fix it
     (and (str/starts-with? id "dashcard:unknown:") (:id dashcard))
     (str/replace id #"dashcard:unknown" (str "dashcard:" (:id dashcard)))
