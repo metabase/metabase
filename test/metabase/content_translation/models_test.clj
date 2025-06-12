e16e1b141b22eef4513e4f024547a413bfe6fe56
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-06-12 15:51:08 -0400

    Fix be unit tests

--- test/metabase/content_translation/models_test.clj
+++ test/metabase/content_translation/models_test.clj
@@ -6,9 +6,21 @@
    [metabase.test :as mt]
    [toucan2.core :as t2]))
 
+(defmacro with-clean-translations!
+  "Macro to reset the content translation table to an empty state before a test and restore it after the test runs."
+  [& body]
+  `(let [original-entities# (t2/select [:model/ContentTranslation])]
+     (try
+       (t2/delete! :model/ContentTranslation)
+       ~@body
+       (finally
+         (t2/delete! :model/ContentTranslation)
+         (when (seq original-entities#)
+           (t2/insert! :model/ContentTranslation original-entities#))))))
+
 (deftest get-translations-test
   (mt/with-premium-features #{:content-translation}
-    (mt/with-empty-h2-app-db
+    (with-clean-translations!
       (mt/with-temp [:model/ContentTranslation _ {:locale "fr"
                                                   :msgid "Hello"
                                                   :msgstr "Bonjour"}
@@ -24,7 +36,6 @@
             (is (every? #(contains? % :locale) all-translations))
             (is (every? #(contains? % :msgid) all-translations))
             (is (every? #(contains? % :msgstr) all-translations))))
-
         (testing "get-translations with locale parameter filters by locale"
           (let [fr-translations (ct.models/get-translations "fr")
                 es-translations (ct.models/get-translations "es")]
@@ -32,24 +43,11 @@
             (is (= 1 (count es-translations)))
             (is (every? #(= "fr" (:locale %)) fr-translations))
             (is (every? #(= "es" (:locale %)) es-translations))))
-
         (testing "get-translations with empty locale returns empty collection"
           (let [nonexistent-translations (ct.models/get-translations "ja")]
             (is (empty? nonexistent-translations))))
-
         (testing "get-translations with nil locale behaves same as no parameter"
           (let [all-translations (ct.models/get-translations)
                 nil-translations (ct.models/get-translations nil)]
             (is (= (count all-translations) (count nil-translations)))
-            (is (= (set all-translations) (set nil-translations)))))))
-
-    (testing "get-translations handles complex unicode"
-      (mt/with-temp [:model/ContentTranslation _ {:locale "zh_CN"
-                                                  :msgid "Eat while it's hot"
-                                                  :msgstr "趁热吃"}]
-        (let [translations (ct.models/get-translations "zh_CN")]
-          (is (= 1 (count translations)))
-          (let [translation (first translations)]
-            (is (= "zh_CN" (:locale translation)))
-            (is (= "Eat while it's hot" (:msgid translation)))
-            (is (= "趁热吃" (:msgstr translation)))))))))
+            (is (= (set all-translations) (set nil-translations)))))))))
