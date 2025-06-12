ba89dc7998c1a2e4632a35aa89f1c0d426feb98c
Ryan Laurie <iethree@gmail.com>
2025-06-12 17:40:10 -0600

    fix e2e test, cleanup

--- frontend/src/metabase/admin/people/components/PeopleNav.tsx
+++ frontend/src/metabase/admin/people/components/PeopleNav.tsx
@@ -1,18 +1,13 @@
 import { t } from "ttag";
 
 import { shouldNudgeToPro } from "metabase/admin/people/selectors";
-import { AdminNavWrapper } from "metabase/admin/settings/components/AdminNav";
-import { UpsellSSO } from "metabase/admin/upsells";
-import Link from "metabase/core/components/Link";
-import { useSelector } from "metabase/lib/redux";
-import { getLocation } from "metabase/selectors/routing";
 import {
-  Icon,
-  type IconName,
-  NavLink,
-  type NavLinkProps,
-  Stack,
-} from "metabase/ui";
+  AdminNavItem,
+  AdminNavWrapper,
+} from "metabase/admin/settings/components/AdminNav";
+import { UpsellSSO } from "metabase/admin/upsells";
+import { useSelector } from "metabase/lib/redux";
+import { Stack } from "metabase/ui";
 
 export function PeopleNav() {
   const shouldNudge = useSelector(shouldNudgeToPro) as boolean;
@@ -20,33 +15,20 @@ export function PeopleNav() {
   return (
     <AdminNavWrapper justify="space-between">
       <Stack gap="xs">
-        <SettingsNavItem path="people" label={t`People`} icon="person" />
-        <SettingsNavItem path="people/groups" label={t`Groups`} icon="group" />
+        <AdminNavItem
+          path="/admin/people"
+          data-testid="nav-item"
+          label={t`People`}
+          icon="person"
+        />
+        <AdminNavItem
+          path="/admin/people/groups"
+          data-testid="nav-item"
+          label={t`Groups`}
+          icon="group"
+        />
       </Stack>
       {shouldNudge && <UpsellSSO source="people-groups-settings" />}
     </AdminNavWrapper>
-  );
-}
-
-function SettingsNavItem({
-  path,
-  label,
-  icon,
-  ...navLinkProps
-}: { path: string; icon?: IconName } & Omit<NavLinkProps, "href">) {
-  const location = useSelector(getLocation);
-  const subpath = location?.pathname?.replace?.("/admin/", "");
-
-  return (
-    <NavLink
-      component={Link}
-      to={`/admin/${path}`}
-      defaultOpened={subpath.includes(path)}
-      active={path === subpath}
-      variant="admin-nav"
-      label={label}
-      {...(icon ? { leftSection: <Icon name={icon} /> } : undefined)}
-      {...navLinkProps}
-    />
   );
 }
