974c620e0a249b5a2eb15b9fbc10a874cf9cf822
Timofei <timofei@metabase.com>
2025-06-12 18:09:11 +0400

    Add more deps to ignored by sdk

--- frontend/build/embedding-sdk/constants/ignored-by-sdk-packages.mjs
+++ frontend/build/embedding-sdk/constants/ignored-by-sdk-packages.mjs
@@ -1,9 +1,23 @@
 export const IGNORED_NOT_USED_BY_SDK_PACKAGES = [
   // Used in Admin panel only
+  "lodash.orderby",
   "password-generator",
   "react-ansi-style",
   "scroll-into-view-if-needed",
 
   // Used on the login page
   "@react-oauth/google",
+
+  // Used in static-viz
+  "fast-text-encoding",
+  "hast-util-from-html",
+  "hast-util-to-html",
+  "resize-observer-polyfill",
+  "server-text-width",
+  "stream-browserify",
+
+  // Polyfills
+  "buffer",
+  "process",
+  "regenerator-runtime",
 ];
