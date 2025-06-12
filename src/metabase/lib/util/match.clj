98926b25c6e5a23473a963f2c9db07cc4c915aeb
Oleksandr Yakushev <alex@bytopia.org>
2025-06-12 17:50:04 +0300

    [m.lib.fe-util] Introduce match-lite to reduce CLJS bundle size

--- src/metabase/lib/util/match.clj
+++ src/metabase/lib/util/match.clj
@@ -211,6 +211,161 @@
 ;;      (&match :guard (every-pred map? :source-table))
 ;;      (:source-table &match))
 
+(defn- parse-pattern
+  "Parse a pattern vector into bindings and conditions"
+  [pattern]
+  (cond
+    ;; Symbol binding
+    (symbol? pattern)
+    {:type :symbol
+     :symbol pattern}
+
+    ;; Guard pattern
+    (and (list? pattern) (>= (count pattern) 3))
+    (let [preds (apply hash-map (rest pattern))]
+      (cond-> {:type :guard
+               :symbol (nth pattern 0)
+               :predicate (nth pattern 2)}
+        (:guard preds) (assoc :predicate (:guard preds))
+        (:len preds) (assoc :length (:len preds))))
+
+    ;; Vector pattern
+    (vector? pattern)
+    (let [[main-parts rest-parts] (vec (split-with (complement #{'&}) pattern))
+          rest-part (second rest-parts)]
+      {:type :vector
+       :parts main-parts
+       :rest-part rest-part
+       :has-rest (some? rest-part)})
+
+    (or (number? pattern) (keyword? pattern))
+    {:type :equality
+     :value pattern}
+
+    (set? pattern)
+    {:type :set
+     :value pattern}
+
+    :else
+    (throw (ex-info "Invalid pattern" {:pattern pattern}))))
+
+(defn- process-pattern
+  ([pattern value dont-ensure-vectors?]
+   (let [bindings (volatile! []), conditions (volatile! [])]
+     (process-pattern pattern value bindings conditions dont-ensure-vectors?)
+     {:bindings @bindings
+      :conditions @conditions}))
+  ([pattern value bindings conditions dont-ensure-vectors?]
+   (let [{:keys [parts rest-part predicate] :as parsed} (parse-pattern pattern)]
+     (case (:type parsed)
+       :symbol (when-not (= (:symbol parsed) '_)
+                 (vswap! bindings conj [(:symbol parsed) value]))
+       :vector (let [s (if (symbol? value) value (gensym "vec"))
+                     cnt (count parts)]
+                 (when-not dont-ensure-vectors?
+                   (vswap! bindings conj [s `(metabase.lib.util.match.impl/vector! ~value)]))
+                 (dorun (map-indexed #(process-pattern %2 (list `nth s %1 nil) bindings conditions false) parts))
+                 (if rest-part
+                   (process-pattern rest-part (list `drop cnt value) bindings conditions false)
+                   (vswap! conditions conj (list `metabase.lib.util.match.impl/count= s cnt))))
+       :guard (let [s (:symbol parsed)
+                    bind (when-not (= s '_) s)]
+                (when bind (vswap! bindings conj [bind value]))
+                (when (:predicate parsed)
+                  (vswap! conditions conj (with-meta (if (and (seq? predicate) (not= (first predicate) 'fn*))
+                                                       predicate
+                                                       (list predicate (or bind value)))
+                                                     {:depends-on s})))
+                (when (:length parsed)
+                  (vswap! conditions conj (with-meta (list `metabase.lib.util.match.impl/count= (or bind value) (:length parsed))
+                                                     {:depends-on s}))))
+       :equality (vswap! conditions conj (list `= value (:value parsed)))
+       :set (vswap! conditions conj (list (:value parsed) value))))))
+
+(defn- seq-contains? [coll item]
+  (some #(= % item) coll))
+
+(defn- collect-common [processed-patterns]
+  (let [all-bindings (mapv :bindings processed-patterns)
+        common-bindings (filter (fn [bind] (every? #(seq-contains? % bind) all-bindings))
+                                (first all-bindings))
+
+        all-conditions (mapv :conditions processed-patterns)
+        common-conditions (filter (fn [condition] (and (every? #(seq-contains? % condition) all-conditions)
+                                                       ;; Ensure that variable for common condition is already bound.
+                                                       (seq-contains? (map first common-bindings)
+                                                                      (:depends-on (meta condition)))))
+                                  (first all-conditions))]
+    {:common-bindings common-bindings
+     :common-conditions (->> common-conditions
+                             (mapv #(vary-meta % dissoc :depends-on)))
+     :all-bindings (mapv (fn [bindings]
+                           (remove #(seq-contains? common-bindings %) bindings))
+                         all-bindings)
+     :all-conditions (->> all-conditions
+                          (mapv (fn [conditions]
+                                  (->> conditions
+                                       (remove #(seq-contains? common-conditions %))
+                                       (mapv #(vary-meta % dissoc :depends-on))))))}))
+
+(defn- expand-conditions [combiner conditions body & [bool?]]
+  (case (count conditions)
+    0 body
+    1 (if bool?
+        (first conditions)
+        `(when ~(first conditions) ~body))
+    (if bool?
+      `(~combiner ~@conditions)
+      `(when (~combiner ~@conditions) ~body))))
+
+(defn- expand-bindings [bindings body]
+  (if (empty? bindings) body
+      `(let ~(vec (mapcat identity bindings)) ~body)))
+
+(defmacro match-lite
+  "Pattern matching macro, simplified version of `clojure.core.match`.
+
+  Usage:
+  (match-lite value
+    pattern1 result1
+    pattern2 result2
+    ...)
+
+  Patterns can be:
+  - symbol - binds the entire value
+  - keyword - must match exactly
+  - set - must be one of the set items
+  - (sym :guard pred :len size) - bind with predicate check. Can optionally check for collection length.
+  - vector - binds positional values inside a sequence against other patterns. Can have & to bind remaining elements."
+  {:style/indent :defn}
+  [value & clauses]
+  (when (odd? (count clauses))
+    (throw (ex-info "match-lite requires even number of clauses" {})))
+  (let [value-sym (if (symbol? value) value (gensym "value"))
+        pairs (partition 2 clauses)
+        all-vectors? (every? vector? (map first pairs))
+        processed-patterns (for [[pattern _] pairs]
+                             (process-pattern pattern value-sym all-vectors?))
+        {:keys [common-bindings common-conditions all-bindings all-conditions]}
+        (collect-common processed-patterns)
+        same-result? (apply = (map second pairs))]
+    `(let [~@(when-not (= value-sym value)
+               [value-sym value])
+           ~@(when all-vectors?
+               [value-sym `(metabase.lib.util.match.impl/vector! ~value-sym)])
+           ~@(vec (mapcat identity common-bindings))]
+       ~(expand-conditions
+         `and common-conditions
+         (if (and same-result? (> (count pairs) 1))
+           `(when (or ~@(mapv (fn [bindings conditions]
+                                (expand-bindings bindings (expand-conditions `and conditions true true)))
+                              all-bindings all-conditions))
+              ~(second (first pairs)))
+           `(or ~@(mapv (fn [bindings conditions [_ return-expr]]
+                          (expand-bindings bindings (expand-conditions `and conditions return-expr)))
+                        all-bindings all-conditions pairs)
+                nil))))))
+
 (defmacro replace*
   "Internal implementation for `replace`. Generate a pattern-matching function with `core.match`, and use it to replace
   matching values in `form`."
