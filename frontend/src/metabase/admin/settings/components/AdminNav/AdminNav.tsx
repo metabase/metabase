370e782cdfab4507a1d521cb91fd24bb3e47e17d
Ryan Laurie <iethree@gmail.com>
2025-06-12 15:09:33 -0600

    type fix

--- frontend/src/metabase/admin/settings/components/AdminNav/AdminNav.tsx
+++ frontend/src/metabase/admin/settings/components/AdminNav/AdminNav.tsx
@@ -17,8 +17,7 @@ export const AdminNavWrapper = ({
   ...stackProps
 }: {
   children: React.ReactNode;
-  stackProps?: StackProps;
-}) => {
+} & StackProps) => {
   return (
     <Stack w="16rem" gap="xs" bg="white" p="md" h="100%" {...stackProps}>
       {children}
