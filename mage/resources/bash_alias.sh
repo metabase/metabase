f2da06b3b7cf0ef70f29b18d1565cf54eff556a6
Bryan Maass <bryan.maass@gmail.com>
2025-06-13 11:31:28 -0600

    edit bash script

--- mage/resources/bash_alias.sh
+++ mage/resources/bash_alias.sh
@@ -9,6 +9,5 @@ mage() {
 _bb_tasks() {
     COMPREPLY=( $(compgen -W "$(bb tasks |tail -n +3 |cut -f1 -d ' ')" -- ${COMP_WORDS[COMP_CWORD]}) );
 }
-# autocomplete filenames as well
 complete -f -F _bb_tasks mage
 ## END MAGE [auto-installed]
