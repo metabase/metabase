db6e7afb4b53b29ee19f67993c3bab80abb21daf
Nicolò Pretto <info@npretto.com>
2025-06-13 15:46:16 +0200

    make database widget keyboard friendly

--- frontend/src/metabase/databases/components/DatabaseEngineField/DatabaseEngineWidget.tsx
+++ frontend/src/metabase/databases/components/DatabaseEngineField/DatabaseEngineWidget.tsx
@@ -218,7 +218,7 @@ const EngineCard = ({
 
   return (
     <EngineCardRoot
-      role="option"
+      role="listitem"
       id={getListOptionId(rootId, option)}
       isActive={isActive}
       onClick={handleClick}
