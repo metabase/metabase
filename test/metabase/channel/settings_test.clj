65da45c0989f45c0aef65caec8929b1291a77eb5
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-06-14 02:19:26 +0300

    Added validation for email-from-name (#59330) (#59500)
    Co-authored-by: Nathan Voxland <nathan@metabase.com>

--- test/metabase/channel/settings_test.clj
+++ test/metabase/channel/settings_test.clj
@@ -3,7 +3,8 @@
   (:require
    [clojure.test :refer :all]
    [metabase.channel.settings :as channel.settings]
-   [metabase.test :as mt]))
+   [metabase.test :as mt])
+  (:import (clojure.lang ExceptionInfo)))
 
 (deftest slack-app-token-truncation-test
   (testing "slack-app-token is truncated when fetched by the setting's custom getter"
@@ -15,3 +16,27 @@
   (mt/with-temporary-setting-values [slack-channels-and-usernames-last-updated nil]
     (is (= channel.settings/zoned-time-epoch
            (channel.settings/slack-channels-and-usernames-last-updated)))))
+
+(deftest email-from-name-validation
+  (mt/with-temporary-setting-values [email-from-name nil]
+    (testing "valid names"
+      (doseq [valid-name [nil
+                          "Test Name"
+                          "Test-Name"
+                          "Test 123 !#$%&'*+-=?^`~"
+                          "\uD83D\uDC7E"
+                          "بسمة"]]
+        (testing valid-name
+          (channel.settings/email-from-name! valid-name)
+          (is (= (channel.settings/email-from-name) valid-name)))))
+    (testing "invalid names"
+      (doseq [invalid-name ["Bad :"
+                            ":"
+                            "Bad \""
+                            "Bad ("
+                            "Bad )"
+                            "Bad ("
+                            "Bad @"]]
+        (testing invalid-name
+          (is (thrown-with-msg? ExceptionInfo #"Invalid special character included."
+                                (channel.settings/email-from-name! invalid-name))))))))
