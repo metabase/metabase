e63da3229e853457cf3e4ec4d72ca60ad773819b
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-06-13 13:13:57 +0700

    Add a basic E2E test

---
+++ e2e/test/scenarios/onboarding/setup/setup-embedding.cy.spec.ts
@@ -0,0 +1,149 @@
+describe("scenarios > setup embedding (EMB-477)", () => {
+  beforeEach(() => H.restore("blank"));
+
+  it("should allow users to use existing setup flow", () => {
+    cy.visit("/setup/embedding");
+    assertEmbeddingOnboardingPageLoaded();
+
+    cy.findByRole("link", { name: "Set up manually" })
+      .should("be.visible")
+      .click();
+
+    cy.findByRole("main").within(() => {
+      cy.findByRole("heading", { name: "Welcome to Metabase" }).should(
+        "be.visible",
+      );
+      cy.findByText(
+        "Looks like everything is working. Now let’s get to know you, connect to your data, and start finding you some answers!",
+      ).should("be.visible");
+      cy.button("Let's get started").should("be.visible");
+    });
+
+    cy.location("pathname").should("eq", "/setup");
+  });
+
+  it("should allow users to go through the embedding setup and onboarding flow", () => {
+    cy.visit("/setup/embedding");
+
+    cy.log("0: Welcome step");
+    assertEmbeddingOnboardingPageLoaded();
+    step().within(() => {
+      cy.findByRole("heading", {
+        name: "Let's get you up and running with a starting setup for embedded analytics",
+      }).should("be.visible");
+      cy.button("Start").should("be.visible").click();
+    });
+
+    cy.log("1: User creation step");
+    sidebar().within(() => {
+      cy.findByRole("listitem", { current: "step" }).should(
+        "have.text",
+        "Set up your account",
+      );
+    });
+    step().within(() => {
+      cy.findByRole("heading", { name: "What should we call you?" }).should(
+        "be.visible",
+      );
+
+      fillOutUserForm();
+
+      cy.button("Next").should("be.enabled").click();
+    });
+
+    cy.log("2: Data connection step");
+    sidebar().within(() => {
+      cy.findByRole("listitem", { current: "step" }).should(
+        "have.text",
+        "Connect to your data",
+      );
+    });
+    step().within(() => {
+      cy.findByRole("heading", { name: "Connect to your data" }).should(
+        "be.visible",
+      );
+
+      cy.findByRole("option", { name: "PostgreSQL" }).click();
+      fillOutDatabaseForm();
+
+      cy.button("Connect database").should("be.enabled").click();
+    });
+
+    cy.log("3: Content generation step");
+    sidebar().within(() => {
+      cy.findByRole("listitem", { current: "step" }).should(
+        "have.text",
+        "Generate starter content",
+      );
+    });
+    step().within(() => {
+      cy.findByRole("heading", { name: "Select Tables to Embed" }).should(
+        "be.visible",
+      );
+
+      cy.findByRole("checkbox", { name: "feedback" })
+        .should("be.enabled")
+        .check();
+      cy.findByRole("checkbox", { name: "orders" })
+        .should("be.enabled")
+        .check();
+      cy.findByRole("checkbox", { name: "products" })
+        .should("be.enabled")
+        .check();
+      cy.log(
+        "Others non-selected options should be disabled after selecting 3 options (max limit)",
+      );
+      cy.findByRole("checkbox", { name: "reviews" }).should("be.disabled");
+
+      cy.button("Continue").should("be.enabled").click();
+    });
+
+    cy.log("4: Embed in your app step");
+    sidebar().within(() => {
+      cy.findByRole("listitem", { current: "step" }).should(
+        "have.text",
+        "Add to your app",
+      );
+    });
+    step().within(() => {
+      cy.findByRole("heading", { name: "Add to your app" }).should(
+        "be.visible",
+      );
+
+      // TODO: Add assertions
+    });
+  });
+});
+
+function assertEmbeddingOnboardingPageLoaded() {
+  cy.findByLabelText("Embedding Setup Sidebar")
+    .findByText("Embedded Analytics")
+    .should("be.visible");
+}
+
+function step() {
+  return cy.findByRole("region", { name: "Embedding setup current step" });
+}
+
+function sidebar() {
+  return cy.findByRole("complementary");
+}
+
+function fillOutUserForm() {
+  cy.findByLabelText("First name").type("Testy");
+  cy.findByLabelText("Last name").type("McTestface");
+  cy.findByLabelText("Email").type("testy@metabase.test");
+  cy.findByLabelText("Company or team name").type("Epic Team");
+
+  cy.findByLabelText("Create a password").type("metabase123");
+  cy.findByLabelText("Confirm your password").type("metabase123");
+}
+
+function fillOutDatabaseForm() {
+  cy.findByLabelText("Display name").type("Postgres Database");
+  cy.findByLabelText("Host").type("localhost");
+  cy.findByLabelText("Port").type("5404");
+  cy.findByLabelText("Database name").type("sample");
+  cy.findByLabelText("Username").type("metabase");
+  cy.findByLabelText("Password").type("metasample123");
+}
