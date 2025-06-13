a53b1fb1492fee087caa74b3cc8c3c9f8afb2722
Nathan Voxland <nathan@metabase.com>
2025-06-13 14:08:49 -0500

    Added /api/email/cloud API and backing settings

--- test/metabase/channel/settings_test.clj
+++ test/metabase/channel/settings_test.clj
@@ -15,3 +15,42 @@
   (mt/with-temporary-setting-values [slack-channels-and-usernames-last-updated nil]
     (is (= channel.settings/zoned-time-epoch
            (channel.settings/slack-channels-and-usernames-last-updated)))))
+
+(deftest cloud-email-reply-to
+  (mt/with-temporary-setting-values [cloud-email-reply-to nil]
+    (testing "requires cloud-custom-smtp feature to be enabled"
+      (is (thrown-with-msg? Exception #"Setting cloud-email-reply-to is not enabled because feature :cloud-custom-smtp is not available"
+                            (channel.settings/cloud-email-reply-to! "test@example.com"))))
+    (mt/with-premium-features [:cloud-custom-smtp]
+      (testing "invalid email is not allowed"
+        (is (thrown-with-msg? Exception #"Invalid reply-to address"
+                              (channel.settings/cloud-email-reply-to! "invalid"))))
+      (testing "correctly sets the setting"
+        (channel.settings/cloud-email-reply-to! ["test@example.com"])
+        (is (= '("test@example.com") (channel.settings/cloud-email-reply-to)))))))
+
+(deftest cloud-email-smtp-port
+  (mt/with-temporary-setting-values [cloud-email-smtp-port nil]
+    (testing "requires cloud-custom-smtp feature to be enabled"
+      (is (thrown-with-msg? Exception #"Setting cloud-email-smtp-port is not enabled because feature :cloud-custom-smtp is not available"
+                            (channel.settings/cloud-email-smtp-port! 465))))
+    (mt/with-premium-features [:cloud-custom-smtp]
+      (testing "invalid port is not allowed"
+        (is (thrown-with-msg? AssertionError #"Invalid custom email-smtp-port"
+                              (channel.settings/cloud-email-smtp-port! 25))))
+      (testing "correctly sets the setting"
+        (channel.settings/cloud-email-smtp-port! 465)
+        (is (= 465 (channel.settings/cloud-email-smtp-port)))))))
+
+(deftest cloud-email-smtp-security
+  (mt/with-temporary-setting-values [cloud-email-smtp-security nil]
+    (testing "requires cloud-custom-smtp feature to be enabled"
+      (is (thrown-with-msg? Exception #"Setting cloud-email-smtp-security is not enabled because feature :cloud-custom-smtp is not available"
+                            (channel.settings/cloud-email-smtp-security! "ssl"))))
+    (mt/with-premium-features [:cloud-custom-smtp]
+      (testing "'none' is not allowed"
+        (is (thrown-with-msg? AssertionError #"Invalid cloud-email-smtp-security"
+                              (channel.settings/cloud-email-smtp-security! "none"))))
+      (testing "correctly sets the setting"
+        (channel.settings/cloud-email-smtp-security! "ssl")
+        (is (= :ssl (channel.settings/cloud-email-smtp-security)))))))
