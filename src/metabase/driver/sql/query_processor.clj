70ead09330c7890b1bc33f8120b2fbeef7cf15c4
lbrdnk <lbrdnk@users.noreply.github.com>
2025-06-16 18:51:56 +0200

    Fix searching for uuid column values in dashboard filter (#59272)
    * Fix searching for uuid column values in dashboard filter
    
    * Adjust so only the uuid fields are remapped for search
    
    * Address review remarks

--- src/metabase/driver/sql/query_processor.clj
+++ src/metabase/driver/sql/query_processor.clj
@@ -1548,6 +1548,11 @@
   [x]
   (and (mbql.u/mbql-clause? x)
        (isa? (or (:effective-type (get x 2))
+                 (let [field-id (second x)]
+                   (when (pos-int? field-id)
+                     (let [{:keys [base-type effective-type]}
+                           (driver-api/field (driver-api/metadata-provider) field-id)]
+                       (or effective-type base-type))))
                  (:base-type (get x 2)))
              :type/UUID)))
 
