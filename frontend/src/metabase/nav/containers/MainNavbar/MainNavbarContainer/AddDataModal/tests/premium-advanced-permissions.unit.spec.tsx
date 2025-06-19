8a91b5dc5d7e04255ae7d422c82598b0ccbfa30d
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2025-06-19 23:13:01 +0200

    Add CSV panel to the "Add data" modal (#59008)

---
+++ frontend/src/metabase/nav/containers/MainNavbar/MainNavbarContainer/AddDataModal/tests/premium-advanced-permissions.unit.spec.tsx
@@ -0,0 +1,71 @@
+import userEvent from "@testing-library/user-event";
+
+import { screen } from "__support__/ui";
+
+import { setupAdvancedPermissions } from "./setup";
+
+describe("Add data modal (EE with token)", () => {
+  describe("Database panel", () => {
+    it("should not offer a setting manager to manage databases", () => {
+      setupAdvancedPermissions({ isAdmin: false, canManageSettings: true });
+
+      expect(
+        screen.getByRole("tab", { name: /Database$/ }),
+      ).toBeInTheDocument();
+      expect(screen.queryByText("Manage databases")).not.toBeInTheDocument();
+    });
+  });
+
+  describe("CSV panel", () => {
+    it("should offer a setting manager to enable (csv) uploads", async () => {
+      setupAdvancedPermissions({
+        isAdmin: false,
+        canManageSettings: true,
+        uploadsEnabled: false,
+        canUpload: true,
+      });
+
+      await openTab("CSV");
+      expect(screen.getByText("Manage uploads")).toBeInTheDocument();
+      expect(screen.getByText("Enable uploads")).toBeInTheDocument();
+    });
+
+    it("should offer a setting manager to manage (csv) uploads", async () => {
+      setupAdvancedPermissions({
+        isAdmin: false,
+        canManageSettings: true,
+        uploadsEnabled: true,
+        canUpload: true,
+      });
+
+      await openTab("CSV");
+      expect(screen.getByText("Manage uploads")).toBeInTheDocument();
+    });
+
+    /**
+     * I think this is a bug, and it shouldn't work like this.
+     * But this behavior is already present in the other parts of the app so I didn't
+     * want to create an exception here.
+     * I argued that we shouldn't even show the uploads option to settings managers without
+     * sufficient data permissions, but was told by The Product that this is fine.
+     * Slack context: https://metaboat.slack.com/archives/C01LQQ2UW03/p1749213616959619
+     */
+    it("should offer a setting manager (who lacks upload permissions) to manage (csv) uploads", async () => {
+      setupAdvancedPermissions({
+        isAdmin: false,
+        canManageSettings: true,
+        uploadsEnabled: true,
+        canUpload: false,
+      });
+
+      await openTab("CSV");
+      expect(screen.getByText("Manage uploads")).toBeInTheDocument();
+    });
+  });
+});
+
+async function openTab(tabName: string) {
+  await userEvent.click(
+    screen.getByRole("tab", { name: new RegExp(`${tabName}$`) }),
+  );
+}
