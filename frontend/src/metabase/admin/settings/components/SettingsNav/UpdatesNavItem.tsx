407af455842667fc5d0d36e8d77324e6941b12dc
Fraser <fraser@metabase.com>
2025-06-19 12:28:35 +1200

    Implement nofication badge for updats

---
+++ frontend/src/metabase/admin/settings/components/SettingsNav/UpdatesNavItem.tsx
@@ -0,0 +1,44 @@
+import { t } from "ttag";
+
+import { useGetVersionInfoQuery } from "metabase/api";
+import { useSetting } from "metabase/common/hooks";
+import { useSelector } from "metabase/lib/redux";
+import { newVersionAvailable } from "metabase/lib/utils";
+import { Badge } from "metabase/ui";
+
+import { getCurrentVersion } from "../../selectors";
+
+import { SettingsNavItem } from "./SettingsNav";
+
+export function UpdatesNavItem() {
+  const { data: versionInfo } = useGetVersionInfoQuery();
+  const currentVersion = useSelector(getCurrentVersion);
+  const updateChannel = useSetting("update-channel") ?? "latest";
+  const latestVersion = versionInfo?.[updateChannel]?.version;
+
+  const isNewVersionAvailable =
+    latestVersion && newVersionAvailable({ currentVersion, latestVersion });
+
+  return (
+    <SettingsNavItem
+      path="updates"
+      label={<div>{t`Updates`}</div>}
+      icon="sparkles"
+      rightSection={
+        isNewVersionAvailable ? (
+          <Badge
+            data-testid="updates-nav-badge"
+            styles={{
+              root: {
+                color: "var(--mb-color-text-white)",
+                backgroundColor: "var(--mb-color-error)",
+              },
+            }}
+          >
+            {t`1`}
+          </Badge>
+        ) : null
+      }
+    />
+  );
+}
