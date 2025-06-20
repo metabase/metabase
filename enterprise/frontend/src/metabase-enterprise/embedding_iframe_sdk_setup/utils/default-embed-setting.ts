4332e1d73a023d2392007188b52acc878ab601b6
Phoomparin Mano <phoomparin@gmail.com>
2025-06-20 19:56:19 +0700

    cleanup default sdk iframe embed settings

--- enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/utils/default-embed-setting.ts
+++ enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/utils/default-embed-setting.ts
@@ -1,5 +1,11 @@
 import { match } from "ts-pattern";
 
+import type {
+  DashboardEmbedOptions,
+  ExplorationEmbedOptions,
+  QuestionEmbedOptions,
+} from "metabase-enterprise/embedding_iframe_sdk/types/embed";
+
 import type { SdkIframeEmbedSetupExperience } from "../types";
 
 export const getDefaultSdkIframeEmbedSettings = (
@@ -7,20 +13,32 @@ export const getDefaultSdkIframeEmbedSettings = (
   defaultEntityId: string | number,
 ) =>
   match(type)
-    .with("dashboard", () => ({
-      dashboardId: defaultEntityId,
-      isDrillThroughEnabled: true,
-      withDownloads: false,
-      withTitle: true,
-    }))
-    .with("chart", () => ({
-      questionId: defaultEntityId,
-      isDrillThroughEnabled: true,
-      withDownloads: false,
-      withTitle: true,
-    }))
-    .with("exploration", () => ({
-      template: "exploration",
-      isSaveEnabled: true,
-    }))
+    .with(
+      "dashboard",
+      (): DashboardEmbedOptions => ({
+        dashboardId: defaultEntityId,
+        isDrillThroughEnabled: true,
+        withDownloads: false,
+        withTitle: true,
+      }),
+    )
+    .with(
+      "chart",
+      (): QuestionEmbedOptions => ({
+        questionId: defaultEntityId,
+        isDrillThroughEnabled: true,
+        withDownloads: false,
+        withTitle: true,
+      }),
+    )
+    .with(
+      "exploration",
+      (): ExplorationEmbedOptions => ({
+        template: "exploration",
+        isSaveEnabled: true,
+      }),
+    )
     .exhaustive();
+
+export const DEFAULT_SDK_IFRAME_EMBED_SETTINGS =
+  getDefaultSdkIframeEmbedSettings("dashboard", 1);
