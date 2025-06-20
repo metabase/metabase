2ef8a0ebf7d6055be4e2d71b33173c0ae88a1d19
Cam Saul <github@camsaul.com>
2025-06-20 03:20:37 +0000

    13579 assertions, 0 failures, 0 errors.

--- test/metabase/query_processor_test/model_test.clj
+++ test/metabase/query_processor_test/model_test.clj
@@ -79,17 +79,22 @@
       (is (= [{:id (mt/id :reviews :created_at)
                :display_name "Reviews → Created At: Month"}
               {:display_name "Average of Rating"}
+              ;; TODO (Cam 6/17/25) -- the 'old' answer here was this, but it's returning something slightly different
+              ;; now that I added the [[metabase.lib.field/qp-model-metadata-for-stage]] stuff. Not really 100% sure
+              ;; what the ideal answer is here and if this is a bug or not. If we somehow revert to the old display name
+              ;; that's fine.
+              #_{:id (mt/id :reviews :created_at)
+                 :source_alias "Products+Reviews Summary - Reviews → Created At: Month"
+                 :display_name "Products+Reviews Summary - Reviews → Created At: Month → Created At"}
               {:id (mt/id :reviews :created_at)
-               :source_alias "Products+Reviews Summary - Reviews → Created At: Month"
-               ;; TODO (Cam 6/17/25) -- the 'old' answer here was this, but it's returning something slightly different
-               ;; now that I added the [[metabase.lib.field/qp-model-metadata-for-stage]] stuff. Not really 100% sure
-               ;; what the ideal answer is here and if this is a bug or not. If we somehow revert to the old display
-               ;; name that's fine.
-               ;;
-               ;; :display_name "Products+Reviews Summary - Reviews → Created At: Month → Created At"
-               :display_name "Reviews → Created At"}
-              {:source_alias "Products+Reviews Summary - Reviews → Created At: Month"
-               :display_name "Products+Reviews Summary - Reviews → Created At: Month → Sum"}]
+               ;; did I introduce a regression in display name? Nobody named Cam can figure out how it's supposed
+               ;; anyway.
+               :display_name "Reviews → Created At: Month"
+               :source_alias "Products+Reviews Summary - Reviews → Created At"}
+              #_{:source_alias "Products+Reviews Summary - Reviews → Created At: Month"
+               :display_name "Products+Reviews Summary - Reviews → Created At: Month → Sum"}
+              {:display_name "Products+Reviews Summary - Reviews → Created At → Sum of Price",
+               :source_alias "Products+Reviews Summary - Reviews → Created At"}]
              (->> (qp/process-query question)
                   mt/cols
                   (mapv #(select-keys % [:id :source_alias :display_name]))))))))
