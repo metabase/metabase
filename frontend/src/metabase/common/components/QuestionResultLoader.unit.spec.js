3bbda656c82d681a376bbbacec140fc341731a2c
Ryan Laurie <iethree@gmail.com>
2025-06-20 10:07:06 -0600

    move containers to common components

--- frontend/src/metabase/common/components/QuestionResultLoader.unit.spec.js
+++ frontend/src/metabase/common/components/QuestionResultLoader.unit.spec.js
@@ -1,8 +1,9 @@
 import { setupCardQueryEndpoints } from "__support__/server-mocks";
 import { render } from "__support__/ui";
-import { QuestionResultLoader } from "metabase/containers/QuestionResultLoader";
 import Question from "metabase-lib/v1/Question";
 import { createMockDataset } from "metabase-types/api/mocks";
+
+import { QuestionResultLoader } from "./QuestionResultLoader";
 
 describe("QuestionResultLoader", () => {
   it("should load a result given a question", () => {
