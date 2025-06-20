d327953bd4a53ea9aa6036b5bcf3ba7dfe23e13b
Phoomparin Mano <phoomparin@gmail.com>
2025-06-21 01:24:16 +0700

    add basic test scaffold

--- e2e/test/scenarios/embedding/sdk-iframe-embedding-setup/sdk-iframe-embedding-setup.cy.spec.ts
+++ e2e/test/scenarios/embedding/sdk-iframe-embedding-setup/sdk-iframe-embedding-setup.cy.spec.ts
@@ -1,13 +1,34 @@
 const { H } = cy;
 
-describe("scenarios > embedding", () => {
+describe("scenarios > embedding > sdk iframe embedding setup flow", () => {
   beforeEach(() => {
     H.restore();
     cy.signInAsAdmin();
     H.setTokenFeatures("all");
+
+    cy.intercept("GET", "/api/dashboard/**").as("dashboard");
   });
 
-  it("can setup an embed", () => {
+  it("shows a preview iframe when visiting the new embed page", () => {
     cy.visit("/embed/new");
+    cy.wait("@dashboard");
+
+    const iframe = getPreviewIframe();
+    iframe.within(() => {
+      cy.log("dashboard title is visible");
+      cy.findByText("Person overview").should("be.visible");
+
+      cy.log("dashboard card is visible");
+      cy.findByText("Person detail").should("be.visible");
+    });
   });
 });
+
+const getPreviewIframe = () =>
+  cy
+    .get("iframe")
+    .should("be.visible")
+    .its("0.contentDocument")
+    .should("exist")
+    .its("body")
+    .should("not.be.empty");
