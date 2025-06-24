9e47317c61275048b719b6c36ca975bf0bd464ef
Romeo Van Snick <romeo@romeovansnick.be>
2025-06-24 15:19:34 +0200

    Allow opening expression editor with custom clause snippet

--- frontend/src/metabase/query_builder/components/expressions/Editor/Editor.tsx
+++ frontend/src/metabase/query_builder/components/expressions/Editor/Editor.tsx
@@ -1,9 +1,6 @@
-import type { EditorState } from "@codemirror/state";
+import { EditorSelection, type EditorState } from "@codemirror/state";
 import { useDisclosure } from "@mantine/hooks";
-import CodeMirror, {
-  EditorSelection,
-  type ReactCodeMirrorRef,
-} from "@uiw/react-codemirror";
+import CodeMirror, { type ReactCodeMirrorRef } from "@uiw/react-codemirror";
 import cx from "classnames";
 import { useCallback, useMemo, useRef, useState } from "react";
 import { useMount } from "react-use";
@@ -15,6 +12,7 @@ import { getMetadata } from "metabase/selectors/metadata";
 import { Button, Tooltip as ButtonTooltip, Flex, Icon } from "metabase/ui";
 import type * as Lib from "metabase-lib";
 import {
+  type DefinedClauseName,
   type ExpressionError,
   diagnoseAndCompile,
   format,
@@ -36,6 +34,7 @@ import { Tooltip } from "./Tooltip";
 import { DEBOUNCE_VALIDATION_MS } from "./constants";
 import { useCustomTooltip } from "./custom-tooltip";
 import { useExtensions } from "./extensions";
+import { useInitialClause } from "./utils";
 
 type EditorProps = {
   id?: string;
@@ -49,6 +48,7 @@ type EditorProps = {
   error?: ExpressionError | Error | null;
   hasHeader?: boolean;
   onCloseEditor?: () => void;
+  initialExpressionClause?: DefinedClauseName | null;
 
   onChange: (
     clause: Lib.ExpressionClause | null,
@@ -74,6 +74,7 @@ export function Editor(props: EditorProps) {
     shortcuts,
     hasHeader,
     onCloseEditor,
+    initialExpressionClause,
   } = props;
 
   const ref = useRef<ReactCodeMirrorRef>(null);
@@ -145,6 +146,10 @@ export function Editor(props: EditorProps) {
     );
   }, []);
 
+  const applyInitialSnippet = useInitialClause({
+    initialExpressionClause,
+  });
+
   return (
     <>
       <LayoutMain className={cx(S.wrapper, { [S.formatting]: isFormatting })}>
@@ -163,6 +168,7 @@ export function Editor(props: EditorProps) {
           width="100%"
           indentWithTab={false}
           autoFocus
+          onCreateEditor={applyInitialSnippet}
         />
         <Errors error={error} />
 
---
+++ frontend/src/metabase/query_builder/components/expressions/Editor/utils.ts
@@ -0,0 +1,36 @@
+import { snippet } from "@codemirror/autocomplete";
+import type { EditorView } from "@codemirror/view";
+import { useCallback } from "react";
+
+import {
+  type DefinedClauseName,
+  expressionClauseSnippet,
+  getClauseDefinition,
+} from "metabase-lib/v1/expressions";
+
+export function useInitialClause({
+  initialExpressionClause,
+}: {
+  initialExpressionClause?: DefinedClauseName | null;
+}) {
+  return useCallback(
+    (view: EditorView) => {
+      if (!initialExpressionClause) {
+        return;
+      }
+
+      const clause = getClauseDefinition(initialExpressionClause);
+
+      snippet(expressionClauseSnippet(clause))(
+        {
+          state: view.state,
+          dispatch: view.dispatch,
+        },
+        null,
+        view.state.selection.main.from,
+        view.state.selection.main.to,
+      );
+    },
+    [initialExpressionClause],
+  );
+}
