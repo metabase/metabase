f8dbb43e346b9a69dc557b428f46d06f8235ab2a
Kamil Mielnik <kamil@kamilmielnik.com>
2025-06-24 22:11:24 +0700

    Update proportions

--- frontend/src/metabase/metadata/pages/DataModel/constants.ts
+++ frontend/src/metabase/metadata/pages/DataModel/constants.ts
@@ -4,7 +4,7 @@ export const COLUMN_CONFIG: Record<Column, ColumnSizeConfig> = {
   nav: { flex: "6 1 0", initial: 376, min: 280, max: 440 },
   table: { flex: "8 1 0", initial: 640, min: 280, max: 640 },
   field: { flex: "9 1 0", initial: 480, min: 280, max: 640 },
-  preview: { flex: "9 1 0", initial: 640, min: 440 + 2 * 32, max: 640 },
+  preview: { flex: "10 1 0", initial: 640, min: 440 + 2 * 32, max: 640 },
 };
 
 export const MIN_PREVIEW_CONTAINER_WIDTH = 440 + 2 * 32;
