f5aa70d869960315b029f9dfde24e97d0ed0220d
Oisin Coveney <oisin@metabase.com>
2025-06-24 22:01:32 +0100

    Convert DashboardTabs to context (#59973)

--- frontend/src/metabase/dashboard/containers/AutomaticDashboardApp/AutomaticDashboardApp.tsx
+++ frontend/src/metabase/dashboard/containers/AutomaticDashboardApp/AutomaticDashboardApp.tsx
@@ -159,7 +159,7 @@ const AutomaticDashboardAppInner = () => {
                 </div>
                 {dashboard && tabs.length > 1 && (
                   <div className={cx(CS.wrapper, CS.flex, CS.alignCenter)}>
-                    <DashboardTabs dashboardId={dashboard.id} />
+                    <DashboardTabs />
                   </div>
                 )}
               </FixedWidthContainer>
