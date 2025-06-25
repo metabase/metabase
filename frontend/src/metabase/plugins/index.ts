758e0aad33b28f2921b4d971756131b082706ff6
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-06-25 03:07:01 +0300

    [Metabot] Streaming error handling and retry prompt UI (#59937) (#60034)
    * save progress
    
    * refine error handling
    
    * add test coverage
    
    * self review
    
    * remove left over todo
    
    * small corrections
    
    * fix unit test
    
    Co-authored-by: Sloan Sparger <sloansparger@users.noreply.github.com>

--- frontend/src/metabase/plugins/index.ts
+++ frontend/src/metabase/plugins/index.ts
@@ -632,6 +632,9 @@ export const PLUGIN_RESOURCE_DOWNLOADS = {
 };
 
 const defaultMetabotContextValue: MetabotContext = {
+  prompt: "",
+  setPrompt: () => {},
+  promptInputRef: undefined,
   getChatContext: () => ({}) as any,
   registerChatContextProvider: () => () => {},
 };
