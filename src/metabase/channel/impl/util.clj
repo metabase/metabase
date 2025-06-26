291764425477c7b855b4019923b61b41e47fdae7
Noah Moss <noahbmoss@gmail.com>
2025-06-26 11:42:38 -0400

    question filters in email subs v1

--- src/metabase/channel/impl/util.clj
+++ src/metabase/channel/impl/util.clj
@@ -54,7 +54,13 @@
   top-level dashboard parameters."
   [parameters dashboard-parts]
   (let [inline-param-ids (->> dashboard-parts
-                              (mapcat :inline_parameters)
-                              (map :id)
+                              (mapcat (fn [dashboard-part]
+                                        (if-let [dashcard (:dashcard dashboard-part)]
+                                          ;; normal dashcards
+                                          (-> dashcard :visualization_settings :inline_parameters)
+                                          ;; header dashcards: viz settings are sent as the top-level dashboard part, so
+                                          ;; look for :inline_parameters directly
+                                          (-> dashboard-part :inline_parameters))))
+                              (keep :id)
                               set)]
     (filter #(not (inline-param-ids (:id %))) parameters)))
