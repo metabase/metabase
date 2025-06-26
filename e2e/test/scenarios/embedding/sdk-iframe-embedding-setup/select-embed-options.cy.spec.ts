8f9bab89a2ea2a23c39830674a07d79ffe03e821
Phoomparin Mano <phoomparin@gmail.com>
2025-06-26 23:58:59 +0700

    simplify tests

--- e2e/test/scenarios/embedding/sdk-iframe-embedding-setup/select-embed-options.cy.spec.ts
+++ e2e/test/scenarios/embedding/sdk-iframe-embedding-setup/select-embed-options.cy.spec.ts
@@ -78,51 +78,91 @@ describe("scenarios > embedding > sdk iframe embed setup > select embed options"
   it("should toggle drill-through option for chart", () => {
     navigateToEmbedOptionsStep({ experience: "chart" });
 
-    getEmbedSidebar().within(() => {
-      const drillThroughCheckbox = cy.findByLabelText(
-        "Allow users to drill through on data points",
-      );
-      drillThroughCheckbox.should("not.be.checked");
+    getEmbedSidebar()
+      .findByLabelText("Allow users to drill through on data points")
+      .should("be.checked");
 
-      drillThroughCheckbox.click();
-      drillThroughCheckbox.should("be.checked");
+    cy.log("drill-through should be disabled by default in chart preview");
+    H.getIframeBody().within(() => {
+      cy.findByText("18,760").click();
+      cy.findByText("See these Orders").should("exist");
+    });
+
+    cy.log("turn off drill-through");
+    getEmbedSidebar()
+      .findByLabelText("Allow users to drill through on data points")
+      .click()
+      .should("not.be.checked");
+
+    cy.log("drill-through should be disabled in chart preview");
+    H.getIframeBody().within(() => {
+      cy.findByText("18,760").click();
+      cy.findByText("See these Orders").should("not.exist");
     });
   });
 
   it("should toggle downloads option for chart", () => {
     navigateToEmbedOptionsStep({ experience: "chart" });
 
-    getEmbedSidebar().within(() => {
-      const downloadsCheckbox = cy.findByLabelText("Allow downloads");
-      downloadsCheckbox.should("not.be.checked");
+    getEmbedSidebar()
+      .findByLabelText("Allow downloads")
+      .should("not.be.checked");
 
-      downloadsCheckbox.click();
-      downloadsCheckbox.should("be.checked");
-    });
+    H.getIframeBody()
+      .findByTestId("question-download-widget-button")
+      .should("not.exist");
+
+    cy.log("turn on downloads");
+    getEmbedSidebar()
+      .findByLabelText("Allow downloads")
+      .click()
+      .should("be.checked");
+
+    H.getIframeBody()
+      .findByTestId("question-download-widget-button")
+      .should("be.visible");
   });
 
-  it("should show title option when drill-through is enabled for chart", () => {
+  it("should toggle title option for chart when drill-through is enabled", () => {
     navigateToEmbedOptionsStep({ experience: "chart" });
 
-    getEmbedSidebar().within(() => {
-      // Enable drill-through first to show title option
-      cy.findByLabelText("Allow users to drill through on data points").click();
-      cy.findByLabelText("Show chart title").should("be.visible");
-    });
+    cy.log("enable drill-through first to show title option");
+    getEmbedSidebar()
+      .findByLabelText("Allow users to drill through on data points")
+      .click()
+      .should("be.checked");
+
+    getEmbedSidebar().findByLabelText("Show chart title").should("be.checked");
+
+    H.getIframeBody().findByText("Orders, Count").should("be.visible");
+
+    cy.log("turn off title");
+    getEmbedSidebar()
+      .findByLabelText("Show chart title")
+      .click()
+      .should("not.be.checked");
+
+    H.getIframeBody().findByText("Orders, Count").should("not.exist");
   });
 
   it("should toggle save option for exploration", () => {
     navigateToEmbedOptionsStep({ experience: "exploration" });
 
-    getEmbedSidebar().within(() => {
-      const saveCheckbox = cy.findByLabelText(
-        "Allow users to save new questions",
-      );
-      saveCheckbox.should("not.be.checked");
+    getEmbedSidebar()
+      .findByLabelText("Allow users to save new questions")
+      .should("not.be.checked");
 
-      saveCheckbox.click();
-      saveCheckbox.should("be.checked");
-    });
+    cy.log("save button should not be visible by default");
+    H.getIframeBody().findByText("Save").should("not.exist");
+
+    cy.log("turn on save option");
+    getEmbedSidebar()
+      .findByLabelText("Allow users to save new questions")
+      .click()
+      .should("be.checked");
+
+    cy.log("save button should be visible in exploration preview");
+    H.getIframeBody().findByText("Save").should("be.visible");
   });
 
   it("should display theme color pickers", () => {
