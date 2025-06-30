bddacd1786196405b4198011bb7d03f68ce474ec
Tamás Benkő <tamas@metabase.com>
2025-07-01 01:42:48 +0300

    Add more tests

--- test/metabase/query_processor/middleware/expand_aggregations_test.clj
+++ test/metabase/query_processor/middleware/expand_aggregations_test.clj
@@ -3,6 +3,7 @@
    [clojure.test :refer [deftest is testing]]
    [medley.core :as m]
    [metabase.lib-be.metadata.jvm :as lib.metadata.jvm]
+   [metabase.lib.convert :as lib.convert]
    [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
    [metabase.lib.options :as lib.options]
@@ -10,7 +11,7 @@
    [metabase.test :as mt])
   (:import (clojure.lang ExceptionInfo)))
 
-(deftest expand-aggregations-test
+(deftest ^:parallel expand-aggregations-test
   (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))
         find-col (fn [query col-name]
                    (->> (lib/aggregable-columns query nil)
@@ -51,5 +52,35 @@
         (testing "dangling reference"
           (let [dangling-ref (str (random-uuid))
                 query (assoc-in query [:stages 0 :aggregation 1 3 2] dangling-ref)]
+            ;; this does not reach the expand-aggregations middleware, because the schema check fails earlier
             (is (thrown-with-msg? ExceptionInfo (re-pattern (str "no aggregation with uuid " dangling-ref))
-                                  (qp/process-query query)))))))))
+                                  (qp/process-query query)))))))
+    (testing "in card"
+      (mt/with-temp [:model/Card card {:dataset_query (lib.convert/->legacy-MBQL query)
+                                       :database_id (mt/id)
+                                       :name "Orders, Sum of Total and double Sum of Total"}]
+        (let [query (lib/query mp (lib.metadata/card mp (:id card)))]
+          (is (= {:columns ["sum" "2*sum"]
+                  :rows [[1510617.7 3021235.4]]}
+                 (mt/formatted-rows+column-names [2.0 2.0] (qp/process-query query)))))))))
+
+(deftest ^:parallel expand-aggregations-metric-test
+  (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))
+        find-col (fn [query col-name]
+                   (->> (lib/aggregable-columns query nil)
+                        (m/find-first (comp #{col-name} :display-name))))
+        metric-query (-> (lib/query mp (lib.metadata/table mp (mt/id :orders)))
+                         (lib/aggregate (lib/sum (lib.metadata/field mp (mt/id :orders :total)))))]
+    (testing "metrics can be referenced"
+      (mt/with-temp [:model/Card metric {:dataset_query (lib.convert/->legacy-MBQL metric-query)
+                                         :database_id (mt/id)
+                                         :name "Order Total"
+                                         :type :metric}]
+        (let [query (-> (lib/query mp (lib.metadata/table mp (mt/id :orders)))
+                        (lib/aggregate (lib.metadata/metric mp (:id metric))))
+              query (lib/aggregate query (lib/with-expression-name
+                                           (lib/* 2 (find-col query "Order Total"))
+                                           "2*total"))]
+          (is (= {:columns ["sum" "2*total"]
+                  :rows [[1510617.7 3021235.4]]}
+                 (mt/formatted-rows+column-names [2.0 2.0] (qp/process-query query)))))))))
