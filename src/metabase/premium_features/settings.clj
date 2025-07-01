b19523a08fd45aee2e8451234be0079f64d41640
Cam Saul <github@camsaul.com>
2025-07-01 21:57:36 +0000

    Better `defsetting` validation

--- src/metabase/premium_features/settings.clj
+++ src/metabase/premium_features/settings.clj
@@ -72,7 +72,7 @@
 
 (def ^:private premium-features
   "Set of defined premium feature keywords."
-  (atom #{}))
+  (atom (sorted-set)))
 
 (defn- default-premium-feature-getter [feature]
   (fn []
@@ -304,3 +304,8 @@
   :setter     :none
   :getter     -token-features
   :doc        false)
+
+(defn known-features
+  "Set of all known premium features."
+  []
+  @premium-features)
