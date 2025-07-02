5e47cdeb865242b39d904262f2333910226a0c3f
Nicolò Pretto <info@npretto.com>
2025-07-02 13:08:54 +0200

    EMB-532: hide the database sync and the app bars in setup/embedding

--- frontend/src/metabase/setup/components/EmbeddingSetup/steps/UserCreationStep.tsx
+++ frontend/src/metabase/setup/components/EmbeddingSetup/steps/UserCreationStep.tsx
@@ -2,6 +2,7 @@ import { t } from "ttag";
 
 import { useUpdateSettingsMutation } from "metabase/api";
 import { useDispatch, useSelector } from "metabase/lib/redux";
+import { loadCurrentUser } from "metabase/redux/user";
 import { Box, Text, Title } from "metabase/ui";
 import type { UserInfo } from "metabase-types/store";
 
@@ -20,6 +21,8 @@ export const UserCreationStep = () => {
 
   const handleSubmit = async (user: UserInfo) => {
     await dispatch(submitUser(user)).unwrap();
+    await dispatch(loadCurrentUser());
+
     // We want to set the embedding homepage visible if the user skips the rest of the flow.
     // This is the first place where we can do this, as we need the initial setup to be done.
     await updateSettings({ "embedding-homepage": "visible" });
