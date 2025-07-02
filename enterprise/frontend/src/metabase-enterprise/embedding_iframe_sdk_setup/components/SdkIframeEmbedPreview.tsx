836ce9bc681cb54a45cad7f142738417704053f5
Phoomparin Mano <poom@metabase.com>
2025-07-02 15:51:59 +0700

    Add live previews for new iframe embed flow (#59874)
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>

--- enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/components/SdkIframeEmbedPreview.tsx
+++ enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/components/SdkIframeEmbedPreview.tsx
@@ -1,3 +1,67 @@
+import { useEffect, useRef } from "react";
+
+import { useSetting } from "metabase/common/hooks";
+import { Box } from "metabase/ui";
+import type { MetabaseEmbed } from "metabase-enterprise/embedding_iframe_sdk/embed";
+
+import { useSdkIframeEmbedSetupContext } from "../context";
+import { DEFAULT_SDK_IFRAME_EMBED_SETTINGS } from "../utils/default-embed-setting";
+
+import S from "./SdkIframeEmbedSetup.module.css";
+
+declare global {
+  interface Window {
+    "metabase.embed": { MetabaseEmbed: typeof MetabaseEmbed };
+  }
+}
+
 export const SdkIframeEmbedPreview = () => {
-  return null;
+  const { settings } = useSdkIframeEmbedSetupContext();
+  const instanceUrl = useSetting("site-url");
+
+  const embedJsRef = useRef<MetabaseEmbed | null>(null);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `${instanceUrl}/app/embed.js`;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      const { MetabaseEmbed } = window["metabase.embed"];
+
+      embedJsRef.current = new MetabaseEmbed({
+        ...DEFAULT_SDK_IFRAME_EMBED_SETTINGS,
+
+        instanceUrl,
+        target: "#iframe-embed-container",
+        iframeClassName: S.EmbedPreviewIframe,
+        useExistingUserSession: true,
+      });
+    };
+
+    return () => {
+      embedJsRef.current?.destroy();
+      script.remove();
+    };
+  }, [instanceUrl]);
+
+  useEffect(() => {
+    if (embedJsRef.current) {
+      embedJsRef.current.updateSettings({
+        // Clear the existing experiences.
+        // This is necessary as `updateSettings` merges new settings with existing ones.
+        template: undefined,
+        questionId: undefined,
+        dashboardId: undefined,
+
+        ...settings,
+      });
+    }
+  }, [settings]);
+
+  return (
+    <div>
+      <Box id="iframe-embed-container" />
+    </div>
+  );
 };
