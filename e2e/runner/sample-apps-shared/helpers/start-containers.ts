a574e0072ea9ee3c9b25a0a6eb926b66a0d87d72
Timofey Kachalov <timofei@metabase.com>
2025-07-02 12:10:14 +0400

    Improve Sample apps healtcheck logic

--- e2e/runner/sample-apps-shared/helpers/start-containers.ts
+++ e2e/runner/sample-apps-shared/helpers/start-containers.ts
@@ -3,12 +3,12 @@ import { delay, shell } from "../../cypress-runner-utils";
 const HEALTH_CHECK_ATTEMPTS_COUNT = 60 * 5;
 const HEALTH_CHECK_WAIT_TIME_MS = 1000;
 
-async function waitForHealth(url: string) {
+async function waitForHealth(url: string, identifier: string) {
   for (let i = 0; i < HEALTH_CHECK_ATTEMPTS_COUNT; i++) {
     try {
       const res = await fetch(url);
       if (res.ok) {
-        console.log("Metabase is ready");
+        console.log(`${identifier} is ready`);
         return;
       }
     } catch {}
@@ -25,18 +25,28 @@ export async function startContainers({
   env,
   dockerUpCommand,
   dockerDownCommand,
+  healthcheckPorts,
 }: {
   cwd: string;
   env: Record<string, string | number>;
   dockerUpCommand: string;
   dockerDownCommand: string;
+  healthcheckPorts: number[];
 }) {
   console.log("Starting app in background...");
 
   try {
     shell(`${dockerUpCommand} -d`, { cwd, env });
 
-    await waitForHealth(`http://localhost:${env.MB_PORT}/api/health`);
+    const healthcheckPromises = healthcheckPorts.map((port) => {
+      if (port === env.MB_PORT) {
+        return waitForHealth(`http://localhost:${port}/api/health`, "Metabase");
+      }
+
+      return waitForHealth(`http://localhost:${port}`, `Sample App: ${port}`);
+    });
+
+    await Promise.all(healthcheckPromises);
   } catch {
     shell(dockerDownCommand, { cwd, env });
   }
