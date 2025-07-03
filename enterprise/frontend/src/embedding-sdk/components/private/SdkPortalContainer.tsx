d99054cd8a624e738943ce7d2ef5e7e0ca0d658c
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-07-03 15:32:36 +0700

    Fix popovers can't be rendered inside modals

--- enterprise/frontend/src/embedding-sdk/components/private/SdkPortalContainer.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/SdkPortalContainer.tsx
@@ -1,30 +1,21 @@
 import styled from "@emotion/styled";
 
 import ZIndex from "metabase/css/core/z-index.module.css";
-import {
-  EMBEDDING_SDK_FULL_PAGE_PORTAL_ROOT_ELEMENT_ID,
-  EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID,
-} from "metabase/embedding-sdk/config";
+import { EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID } from "metabase/embedding-sdk/config";
 
 import { PublicComponentStylesWrapper } from "./PublicComponentStylesWrapper";
 
 /**
  * This is the portal container used by popovers modals etc, it is wrapped with PublicComponentStylesWrapper
  * so that it has our styles applied.
- * Mantine components needs to have the defaultProps set to use `EMBEDDING_SDK_PORTAL_CONTAINER_ELEMENT_ID` as target for the portal
+ * Mantine components needs to have the defaultProps set to use `EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID` as target for the portal
  */
-export const FullPagePortalContainer = () => (
+export const PortalContainer = () => (
   <PublicComponentStylesWrapper>
     <FixedPosition
       className={ZIndex.Overlay}
-      id={EMBEDDING_SDK_FULL_PAGE_PORTAL_ROOT_ELEMENT_ID}
+      id={EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID}
     ></FixedPosition>
-  </PublicComponentStylesWrapper>
-);
-
-export const PortalContainer = () => (
-  <PublicComponentStylesWrapper>
-    <div id={EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID}></div>
   </PublicComponentStylesWrapper>
 );
 
