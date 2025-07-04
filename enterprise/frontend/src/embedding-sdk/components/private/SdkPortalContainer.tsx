a04bf71189158a69abf8909f025d37d08edb76ec
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-07-04 09:53:56 +0300

    fix(sdk): fix popovers can't be rendered inside modals (#60431) (#60481)
    * Fix popovers can't be rendered inside modals
    
    * Fix wrapped tooltips
    
    Co-authored-by: Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>

--- enterprise/frontend/src/embedding-sdk/components/private/SdkPortalContainer.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/SdkPortalContainer.tsx
@@ -1,36 +1,24 @@
-import styled from "@emotion/styled";
-
 import ZIndex from "metabase/css/core/z-index.module.css";
-import {
-  EMBEDDING_SDK_FULL_PAGE_PORTAL_ROOT_ELEMENT_ID,
-  EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID,
-} from "metabase/embedding-sdk/config";
+import { EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID } from "metabase/embedding-sdk/config";
+import { Box } from "metabase/ui";
 
 import { PublicComponentStylesWrapper } from "./PublicComponentStylesWrapper";
 
 /**
  * This is the portal container used by popovers modals etc, it is wrapped with PublicComponentStylesWrapper
  * so that it has our styles applied.
- * Mantine components needs to have the defaultProps set to use `EMBEDDING_SDK_PORTAL_CONTAINER_ELEMENT_ID` as target for the portal
+ * Mantine components needs to have the defaultProps set to use `EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID` as target for the portal
  */
-export const FullPagePortalContainer = () => (
-  <PublicComponentStylesWrapper>
-    <FixedPosition
-      className={ZIndex.Overlay}
-      id={EMBEDDING_SDK_FULL_PAGE_PORTAL_ROOT_ELEMENT_ID}
-    ></FixedPosition>
-  </PublicComponentStylesWrapper>
-);
-
 export const PortalContainer = () => (
   <PublicComponentStylesWrapper>
-    <div id={EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID}></div>
+    <Box
+      id={EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID}
+      className={ZIndex.Overlay}
+      // needed otherwise it will rendered "in place" and push the content below
+      pos="fixed"
+      left={0}
+      top={0}
+      w={"100%"}
+    ></Box>
   </PublicComponentStylesWrapper>
 );
-
-const FixedPosition = styled.div`
-  // needed otherwise it will rendered "in place" and push the content below
-  position: fixed;
-  left: 0;
-  top: 0;
-`;
