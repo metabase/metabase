ee2634baa842cb36cfda21ab12fdd8bf39ef33f0
Riley Thompson <rileythompson99@gmail.com>
2025-07-04 16:17:53 -0400

    add more narrow test

--- modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
+++ modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
@@ -20,6 +20,7 @@
    [metabase.driver.sql-jdbc.sync.interface :as sql-jdbc.sync.interface]
    [metabase.driver.sql.parameters.substitution :as sql.params.substitution]
    [metabase.driver.sql.query-processor :as sql.qp]
+   [metabase.events.core :as events]
    [metabase.lib-be.metadata.jvm :as lib.metadata.jvm]
    [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
@@ -1283,3 +1284,39 @@
             (finally
               ;; reset the snowflake rsa user to use the original public key
               (set-user-public-key :pk-public-key))))))))
+
+(deftest db-update-clears-secret-file-test
+  (testing "publishing a db update event with details changed notifies the db it was updated and clears the secret file memoization"
+    (mt/test-driver :snowflake
+      (let [normal-details (:details (mt/db))
+            priv-key->base64 (fn [priv-key-var]
+                               (-> (tx/db-test-env-var-or-throw :snowflake priv-key-var)
+                                   format-env-key
+                                   u/string-to-bytes
+                                   mt/bytes->base64-data-uri))
+            rsa-details (merge (dissoc normal-details :password)
+                               {:user (tx/db-test-env-var-or-throw :snowflake :pk-user)
+                                :private-key-options "uploaded"
+                                :private-key-value (priv-key->base64 :pk-private-key)
+                                :use-password false})
+            new-rsa-details (assoc rsa-details :private-key-value (priv-key->base64 :pk-private-key-2))]
+        (mt/with-temp [:model/Database rsa-db {:engine :snowflake :details rsa-details}]
+          (let [get-pk-file-str (fn [db]
+                                  (-> (:details db)
+                                      (#'driver.snowflake/resolve-private-key)
+                                      :private_key_file
+                                      slurp))
+                original-priv-key (get-pk-file-str rsa-db)
+                updating-rsa-db (merge rsa-db {:details new-rsa-details})
+                _ (t2/update! :model/Database (:id rsa-db) updating-rsa-db)
+                details-changed? (not= (:details rsa-db) (:details updating-rsa-db))
+                new-rsa-db (t2/select-one :model/Database (:id rsa-db))
+                _ (events/publish-event! :event/database-update {:object new-rsa-db
+                                                                 :user-id 1
+                                                                 :previous-object rsa-db
+                                                                 :details-changed? details-changed?})
+                new-priv-key (get-pk-file-str new-rsa-db)]
+            (is (not= rsa-db updating-rsa-db))
+            (is (not= new-rsa-db updating-rsa-db))
+            (is (= rsa-db new-rsa-db))
+            (is (not= original-priv-key new-priv-key))))))))
