4b6475dfd9e3b4bd8a4080dfebcef311af610e0b
Romeo Van Snick <romeo@romeovansnick.be>
2025-07-07 15:15:23 +0200

    Do not show error when snippet is active (#60445)
    * Do not show error when snippet is active
    
    * Add test for hiding errors while snippet is visible
    
    * Also suppress error underline when snippet is active

--- frontend/src/metabase/query_builder/components/expressions/Editor/Editor.tsx
+++ frontend/src/metabase/query_builder/components/expressions/Editor/Editor.tsx
@@ -38,7 +38,7 @@ import { Tooltip } from "./Tooltip";
 import { DEBOUNCE_VALIDATION_MS } from "./constants";
 import { useCustomTooltip } from "./custom-tooltip";
 import { useExtensions } from "./extensions";
-import { hasActiveSnippet, useInitialClause } from "./utils";
+import { hasActiveSnippet } from "./utils";
 
 type EditorProps = {
   id?: string;
@@ -150,10 +150,6 @@ export function Editor(props: EditorProps) {
     );
   }, []);
 
-  const applyInitialSnippet = useInitialClause({
-    initialExpressionClause,
-  });
-
   const [isSnippetActive, setIsSnippetActive] = useState(false);
   const handleUpdate = useCallback((update: ViewUpdate) => {
     setIsSnippetActive(hasActiveSnippet(update.state));
@@ -177,7 +173,6 @@ export function Editor(props: EditorProps) {
           width="100%"
           indentWithTab={false}
           autoFocus
-          onCreateEditor={applyInitialSnippet}
           onUpdate={handleUpdate}
           autoCorrect="off"
           tabIndex={0}
