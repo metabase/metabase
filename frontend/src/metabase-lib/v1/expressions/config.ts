f4584700f1d9ff8d5660c3a9c039cf378f7b687f
Eric Normand <ericwnormand@gmail.com>
2025-07-07 11:18:59 -0500

    Frontend implementation

--- frontend/src/metabase-lib/v1/expressions/config.ts
+++ frontend/src/metabase-lib/v1/expressions/config.ts
@@ -365,6 +365,13 @@ const CONVERSION = defineClauses(
         },
       ],
     },
+    today: {
+      displayName: "today",
+      type: "datetime",
+      requiresFeature: "expressions/today",
+      description: () => t`Returns the current date.`,
+      args: () => [],
+    },
     float: {
       displayName: "float",
       type: "number",
