a8505aaf42bb56792871e36c0a136c1ab786472e
Edward Paget <ed.paget@gmail.com>
2025-07-07 11:16:42 -0500

    fixup! use user attribute mapping
    Co-Authored-By: Dan Sutton <dan@metabase.com>

--- enterprise/backend/src/metabase_enterprise/tenants/core.clj
+++ enterprise/backend/src/metabase_enterprise/tenants/core.clj
@@ -3,6 +3,7 @@
    [metabase-enterprise.tenants.api :as api]
    [metabase.premium-features.core :refer [defenterprise]]
    [metabase.settings.core :as setting]
+   [metabase.tenants.core :as tenants]
    [toucan2.core :as t2]))
 
 (defenterprise login-attributes
@@ -42,3 +43,18 @@
   :feature :tenants
   [tenant]
   (api/create-tenant! tenant))
+
+(defenterprise attribute-structure
+  "EE version of `attribute-structure` serializes the combination of tenant and user attributes for the
+   given user with metadata about their provenance."
+  :feature :tenants
+  [user]
+  (let [tenant (when-let [tenant-id (:tenant_id user)]
+                 (t2/select-one :model/Tenant :id tenant-id))
+        combined-attributes (tenants/combine (:login_attributes user)
+                                             (:attributes tenant)
+                                             (when tenant
+                                               {"@tenant.slug" (:slug tenant)}))]
+    (assoc user
+           :structured_attributes combined-attributes
+           :login_attributes (update-vals combined-attributes :value))))
