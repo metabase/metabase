36d8bccc1d4d82ec333c58d46437cf6832158733
Nicola Mometto <nicola.mometto@metabase.com>
2025-07-07 13:24:03 +0100

    fix: only consider active fields!

--- src/metabase/sync/analyze/classify.clj
+++ src/metabase/sync/analyze/classify.clj
@@ -100,6 +100,7 @@
   [table :- i/TableInstance]
   (seq (apply t2/select :model/Field
               :table_id (u/the-id table)
+              :active true
               (reduce concat [] (sync.fingerprint/incomplete-analysis-kvs)))))
 
 (mu/defn classify-fields!
@@ -110,6 +111,7 @@
     (let [table-id (:id table)
           existing-name-field (t2/count :model/Field
                                         :table_id table-id
+                                        :active true
                                         :semantic_type :type/Name)
           found-name (atom (pos? existing-name-field))]
       {:fields-classified (count fields)
