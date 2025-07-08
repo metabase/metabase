b11dc5b8296343d43125b62cc4845a1ec3949a9b
Brad Anderson <brad@metabase.com>
2025-07-08 10:26:04 -0400

    M1 of Improve the experience of slow loading cards in dashboards (#60564)
    * Closes VIZ-1224 - Make dashcards use chart-specific skeletons (#60090)
    
    * Updates Dashboard cards' loading state to use already-made chart-specific loading skeletons
    * Adds SankeySkeleton
    * Tweaks MapSkeleton so its fill color can be overridden
    * Fixes double name issue in loading skeletons in pinned questions on collections page
    
    * Closes VIZ-1230 - Trigger spinner state when card takes longer to load (#60359)

--- frontend/src/metabase/visualizations/components/skeletons/SankeySkeleton/index.ts
+++ frontend/src/metabase/visualizations/components/skeletons/SankeySkeleton/index.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/no-default-export -- deprecated usage
-export { default, type LoadingViewProps } from "./LoadingView";
+export { default } from "./SankeySkeleton";
