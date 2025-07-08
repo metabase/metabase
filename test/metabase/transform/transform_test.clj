4a68f25a01ba56c4165d9ae88e6fd9a61a542330
Nicola Mometto <nicola.mometto@metabase.com>
2025-07-08 16:18:01 +0100

    replace test

--- test/metabase/transform/transform_test.clj
+++ test/metabase/transform/transform_test.clj
@@ -1,4 +1,4 @@
-(ns metabase.transform.transform-test
+(ns ^:mb/driver-tests metabase.transform.transform-test
   "Test for transforms"
   (:require
    [clojure.set :as set]
@@ -13,7 +13,7 @@
     (-> (mt/normal-drivers-with-feature :view)
         ;; WIP: for now we're focusing just on those two, full support later
         (set/intersection #{:postgres :h2}))
-    (testing "Can create a new view"
+    (testing "Can create and replace a new view"
       (mt/dataset (mt/dataset-definition "users-db"
                                          ["users"
                                           [{:field-name "name" :base-type :type/Text}
@@ -21,8 +21,15 @@
                                           [["Foo" 10]
                                            ["Bar" 20]
                                            ["Baz" 30]]])
+        (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "young_users")
         (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
                                  "young_users"
                                  "SELECT * FROM users where age < 25"))
         (is (= [["Foo"] ["Bar"]]
+               (mt/rows (qp/process-query (mt/native-query {:query "SELECT name FROM \"young_users\""})))))
+        (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                 "young_users"
+                                 "SELECT * FROM users where age < 35"
+                                 :replace? true))
+        (is (= [["Foo"] ["Bar"] ["Baz"]]
                (mt/rows (qp/process-query (mt/native-query {:query "SELECT name FROM \"young_users\""})))))))))
