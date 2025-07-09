385d6003305981bd0cebb572dbc4b86b9af5dc5a
Nicola Mometto <nicola.mometto@metabase.com>
2025-07-09 12:23:02 +0100

    add view api changelog and tests (#60636)
    * update changelog
    
    * allow create/drop view from h2
    
    * wip: test
    
    * another test
    
    * replace test
    
    * more tests
    
    * explicitely only enable for h2 and postgres
    
    * view with spaces
    
    * wip: disable views for redshift for now

--- src/metabase/driver/sql.clj
+++ src/metabase/driver/sql.clj
@@ -39,7 +39,6 @@
                  :expressions/datetime
                  :expressions/date
                  :expressions/text
-                 :view
                  :distinct-where
                  :database-routing]]
   (defmethod driver/database-supports? [:sql feature] [_driver _feature _db] true))
---
+++ test/metabase/transform/transform_test.clj
@@ -0,0 +1,130 @@
+(ns ^:mb/driver-tests metabase.transform.transform-test
+  "Test for transforms"
+  (:require
+   [clojure.test :refer :all]
+   [metabase.driver :as driver]
+   [metabase.query-processor :as qp]
+   [metabase.test :as mt]
+   [metabase.util :as u]))
+
+(deftest create-view-test
+  (mt/test-drivers
+    (mt/normal-drivers-with-feature :view)
+    (testing "Can create and replace a new view"
+      (mt/dataset (mt/dataset-definition "users-db"
+                                         ["users"
+                                          [{:field-name "name" :base-type :type/Text}
+                                           {:field-name "age" :base-type :type/Integer}]
+                                          [["Foo" 10]
+                                           ["Bar" 20]
+                                           ["Baz" 30]]])
+        (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "young_users")
+        (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                 "young_users"
+                                 "SELECT * FROM users where age < 25"))
+        (is (= [["Foo"] ["Bar"]]
+               (mt/rows (qp/process-query (mt/native-query {:query "SELECT name FROM \"young_users\""})))))
+        (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                 "young_users"
+                                 "SELECT * FROM users where age < 35"
+                                 :replace? true))
+        (is (= [["Foo"] ["Bar"] ["Baz"]]
+               (mt/rows (qp/process-query (mt/native-query {:query "SELECT name FROM \"young_users\""})))))))))
+
+(deftest drop-view-nonexistent-test
+  (mt/test-drivers
+    (mt/normal-drivers-with-feature :view)
+    (testing "Dropping a non-existent view should not throw an error"
+      (mt/dataset (mt/dataset-definition "simple-db"
+                                         ["table1"
+                                          [{:field-name "name" :base-type :type/Text}]
+                                          [["Alice"]
+                                           ["Bob"]]])
+        ;; Should not throw even if view doesn't exist
+        (is (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "nonexistent_view"))))))
+
+(deftest create-view-without-replace-fails-if-exists-test
+  (mt/test-drivers
+    (mt/normal-drivers-with-feature :view)
+    (testing "Creating a view that already exists should fail without :replace? true"
+      (mt/dataset (mt/dataset-definition "products-db"
+                                         ["products"
+                                          [{:field-name "name" :base-type :type/Text}
+                                           {:field-name "price" :base-type :type/Decimal}]
+                                          [["Widget" 10.99]
+                                           ["Gadget" 25.50]]])
+        (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "expensive_products")
+
+        (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                 "expensive_products"
+                                 "SELECT * FROM products WHERE price > 20"))
+
+        (is (thrown? Exception
+                     (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                          "expensive_products"
+                                          "SELECT * FROM products WHERE price > 15")))))))
+
+(deftest create-view-with-complex-query-test
+  (mt/test-drivers
+    (mt/normal-drivers-with-feature :view)
+    (testing "Creating a view with complex SQL"
+      (mt/dataset  (mt/dataset-definition "users-departments-db"
+                                          ["users"
+                                           [{:field-name "name" :base-type :type/Text}
+                                            {:field-name "department_id" :base-type :type/Integer}]
+                                           [["Alice" 10]
+                                            ["Bob" 20]
+                                            ["Charlie" 10]]]
+                                          ["departments"
+                                           [{:field-name "idx" :base-type :type/Integer}
+                                            {:field-name "name" :base-type :type/Text}]
+                                           [[10 "Engineering"]
+                                            [20 "Sales"]]])
+        (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "user_departments")
+
+        (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                 "user_departments"
+                                 "SELECT u.name as user_name, d.name as dept_name FROM users u JOIN departments d ON u.department_id = d.idx"))
+
+        (is (= #{["Alice" "Engineering"] ["Bob" "Sales"] ["Charlie" "Engineering"]}
+               (set (mt/rows (qp/process-query (mt/native-query {:query "SELECT user_name, dept_name FROM \"user_departments\""}))))))))))
+
+(deftest view-name-edge-cases-test
+  (mt/test-drivers
+    (mt/normal-drivers-with-feature :view)
+    (testing "View names with special characters and edge cases"
+      (mt/dataset (mt/dataset-definition "edge-case-db"
+                                         ["test_table"
+                                          [{:field-name "value" :base-type :type/Text}]
+                                          [["test"]]])
+
+        (doseq [view-name ["view_with_underscores"
+                           "ViewWithCamelCase"
+                           "view123"
+                           "view with spaces"]]
+          (testing (str "View name: " view-name)
+            (driver/drop-view! driver/*driver* (u/the-id (mt/db)) view-name)
+
+            (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                     view-name
+                                     "SELECT * FROM test_table"))
+
+            (is (= [[1 "test"]]
+                   (mt/rows (qp/process-query (mt/native-query {:query (format "SELECT * FROM \"%s\"" view-name)})))))
+
+            (driver/drop-view! driver/*driver* (u/the-id (mt/db)) view-name)))))))
+
+(deftest view-with-invalid-sql-test
+  (mt/test-drivers
+    (mt/normal-drivers-with-feature :view)
+    (testing "Creating a view with invalid SQL should fail"
+      (mt/dataset (mt/dataset-definition "error-db"
+                                         ["table1"
+                                          [{:field-name "idx" :base-type :type/Integer}]
+                                          [[1]]])
+        (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "invalid_view")
+
+        (is (thrown? Exception
+                     (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                          "invalid_view"
+                                          "SELECT * FROM nonexistent_table")))))))
