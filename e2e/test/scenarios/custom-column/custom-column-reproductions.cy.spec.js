9505cbbdef4486e3e750e86fe4089002b9279351
Romeo Van Snick <romeo@romeovansnick.be>
2025-07-09 16:22:22 +0200

    Add repro for "Custom Expression can fail query when mixing numeric and string functions" (#60691)
    * Add clickDone to enterCustomColumnDetails helper
    
    * Add repro for metabase#12938
    
    * Add test case for other repro in issue

--- e2e/test/scenarios/custom-column/custom-column-reproductions.cy.spec.js
+++ e2e/test/scenarios/custom-column/custom-column-reproductions.cy.spec.js
@@ -1343,3 +1343,39 @@ describe("issue 55128", () => {
     });
   });
 });
+
+describe("Issue 12938", () => {
+  beforeEach(() => {
+    H.restore();
+    cy.signInAsNormalUser();
+    H.openProductsTable({ mode: "notebook" });
+  });
+
+  it("should be possible to concat number with string (metabase#12938)", () => {
+    H.addCustomColumn();
+    H.enterCustomColumnDetails({
+      formula: "concat(floor([Rating]), [Title])",
+      name: "MyCustom",
+      clickDone: true,
+    });
+
+    H.visualize();
+    cy.get("main")
+      .findByText("There was a problem with your question")
+      .should("not.exist");
+  });
+
+  it("should be possible to concat number with string (metabase#12938)", () => {
+    H.addCustomColumn();
+    H.enterCustomColumnDetails({
+      formula: 'concat(hour([Created At]), ":", minute([Created At]))',
+      name: "MyCustom",
+      clickDone: true,
+    });
+
+    H.visualize();
+    cy.get("main")
+      .findByText("There was a problem with your question")
+      .should("not.exist");
+  });
+});
