9e0179a0efc7393417c413053d36f5886e9a1f0b
Filipe Silva <filipematossilva@gmail.com>
2025-07-09 13:24:25 +0100

    fixup: use db driver feature in DatabaseReplicationSection

---
+++ enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationPostgresInfo.tsx
@@ -0,0 +1,18 @@
+import { jt, t } from "ttag";
+
+import ExternalLink from "metabase/common/components/ExternalLink";
+import { Text } from "metabase/ui";
+
+export function DatabaseReplicationPostgresInfo() {
+  const link = (
+    <ExternalLink href="https://clickhouse.com/docs/integrations/clickpipes/postgres">
+      {t`adjust settings and permissions`}
+    </ExternalLink>
+  );
+
+  return (
+    <Text size="sm" c="text-medium">
+      {jt`Note: You may need to ${link} in the source database. The process might also require a database restart.`}
+    </Text>
+  );
+}
