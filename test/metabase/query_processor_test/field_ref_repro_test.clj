f70b481af3bce5506c2e1e4eb50a2ed9305bb2af
Tamás Benkő <tamas@metabase.com>
2025-07-09 19:42:10 +0300

    Add repro test for #27521

--- test/metabase/query_processor_test/field_ref_repro_test.clj
+++ test/metabase/query_processor_test/field_ref_repro_test.clj
@@ -4,9 +4,13 @@
   (:require
    [clojure.set :as set]
    [clojure.test :refer :all]
+   [medley.core :as m]
    [metabase.lib-be.metadata.jvm :as lib.metadata.jvm]
    [metabase.lib.core :as lib]
+   [metabase.lib.equality :as lib.equality]
    [metabase.lib.metadata :as lib.metadata]
+   [metabase.lib.metadata.calculation :as lib.metadata.calculation]
+   [metabase.lib.util :as lib.util]
    [metabase.query-processor :as qp]
    [metabase.test :as mt]))
 
@@ -60,3 +64,74 @@
               (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Column \".*very very long column title.*\" not found"
                                     (mt/rows+column-names
                                      (qp/process-query query)))))))))))
+
+;; other than producing the metadata for the card, there is no  query processing here
+(deftest ^:parallel duplicate-names-selection-test
+  (testing "Should be able to distinguish columns with the same name from a card with self join (#27521)"
+    (mt/with-driver :h2
+      (let [mp (lib.metadata.jvm/application-database-metadata-provider (mt/id))
+            card-data (-> {:dataset_query (mt/mbql-query orders
+                                            {:joins [{:source-table $$orders
+                                                      :alias "o"
+                                                      :fields [&o.orders.id]
+                                                      :condition [:= $id &o.orders.id]}]
+                                             :fields [$id]})
+                           :name "Q1"}
+                          mt/card-with-metadata)]
+        (mt/with-temp [:model/Card card card-data]
+          (let [card-meta (lib.metadata/card mp (:id card))
+                id-col    (m/find-first (comp #{"ID"} :name)
+                                        (lib/returned-columns (lib/query mp card-meta)))
+                query     (-> (lib/query mp (lib.metadata/table mp (mt/id :reviews)))
+                              (lib/join (lib/join-clause card-meta
+                                                         [(lib/= (lib.metadata/field mp (mt/id :reviews :id))
+                                                                 id-col)])))
+                stage     (lib.util/query-stage query -1)
+                visible   (lib.metadata.calculation/visible-columns query -1 stage)
+                returned  (lib.metadata.calculation/returned-columns query -1 stage)
+                marked    (lib.equality/mark-selected-columns query -1 visible returned)]
+            (is (=? [{:name "ID",         :display-name "ID"}
+                     {:name "PRODUCT_ID", :display-name "Product ID"}
+                     {:name "REVIEWER",   :display-name "Reviewer"}
+                     {:name "RATING",     :display-name "Rating"}
+                     {:name "BODY",       :display-name "Body"}
+                     {:name "CREATED_AT", :display-name "Created At"}
+                     {:name "ID",         :display-name "Q1 → ID"}
+                     {:name "ID_2",       :display-name "Q1 → ID"}
+                     {:name "ID",         :display-name "ID"}
+                     {:name "EAN",        :display-name "Ean"}
+                     {:name "TITLE",      :display-name "Title"}
+                     {:name "CATEGORY",   :display-name "Category"}
+                     {:name "VENDOR",     :display-name "Vendor"}
+                     {:name "PRICE",      :display-name "Price"}
+                     {:name "RATING",     :display-name "Rating"}
+                     {:name "CREATED_AT", :display-name "Created At"}]
+                    visible))
+            (is (=? [{:name "ID",         :display-name "ID"}
+                     {:name "PRODUCT_ID", :display-name "Product ID"}
+                     {:name "REVIEWER",   :display-name "Reviewer"}
+                     {:name "RATING",     :display-name "Rating"}
+                     {:name "BODY",       :display-name "Body"}
+                     {:name "CREATED_AT", :display-name "Created At"}
+                     {:name "ID_2",       :display-name "Q1 → ID"}
+                     {:name "ID_2_2",     :display-name "Q1 → ID"}]
+                    returned))
+            (is (=? [{:name "ID", :display-name "ID", :selected? true}
+                     {:name "PRODUCT_ID", :display-name "Product ID", :selected? true}
+                     {:name "REVIEWER", :display-name "Reviewer", :selected? true}
+                     {:name "RATING", :display-name "Rating", :selected? true}
+                     {:name "BODY", :display-name "Body", :selected? true}
+                     {:name "CREATED_AT", :display-name "Created At", :selected? true}
+                     ;; the following two Q1 → ID should have :selected? true
+                     {:name "ID", :display-name "Q1 → ID", :selected? false}
+                     {:name "ID_2", :display-name "Q1 → ID", :selected? false}
+                     ;; these are implicitly joinable fields, :selected? false is right
+                     {:name "ID", :display-name "ID", :selected? false}
+                     {:name "EAN", :display-name "Ean", :selected? false}
+                     {:name "TITLE", :display-name "Title", :selected? false}
+                     {:name "CATEGORY", :display-name "Category", :selected? false}
+                     {:name "VENDOR", :display-name "Vendor", :selected? false}
+                     {:name "PRICE", :display-name "Price", :selected? false}
+                     {:name "RATING", :display-name "Rating", :selected? false}
+                     {:name "CREATED_AT", :display-name "Created At", :selected? false}]
+                    marked))))))))
