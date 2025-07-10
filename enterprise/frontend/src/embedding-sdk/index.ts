479b5d332736bd846e99722e09297767047e6237
Nicolò Pretto <info@npretto.com>
2025-07-10 18:08:21 +0200

    remove code to trigger ci

--- enterprise/frontend/src/embedding-sdk/index.ts
+++ enterprise/frontend/src/embedding-sdk/index.ts
@@ -74,6 +74,3 @@ export type { EmbeddingEntityType } from "metabase-types/store/embedding-data-pi
 
 export type { ParameterValues } from "metabase/embedding-sdk/types/dashboard";
 export type { IconName } from "metabase/embedding-sdk/types/icon";
-
-// eslint-disable-next-line no-console
-console.log("new code!");
