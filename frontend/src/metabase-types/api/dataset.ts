941b9c1408e5723a1bf7a239a043fdbab7bba864
Sébastien <sebastien@metabase.com>
2025-07-10 12:15:45 +0200

    added type

--- frontend/src/metabase-types/api/dataset.ts
+++ frontend/src/metabase-types/api/dataset.ts
@@ -15,7 +15,7 @@ import type { DownloadPermission } from "./permissions";
 import type { DatasetQuery, DatetimeUnit, DimensionReference } from "./query";
 import type { TableId } from "./table";
 
-export type RowValue = string | number | null | boolean;
+export type RowValue = string | number | null | boolean | object;
 export type RowValues = RowValue[];
 
 export type BinningMetadata = {
