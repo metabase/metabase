1c1dc23fed6d87ff69abee39244f5ce998519f25
Nicola Mometto <nicola.mometto@metabase.com>
2025-07-10 12:13:06 +0100

    harden tests: use defdataset, native-query-rows, with-temp-copy-of-db

--- test/metabase/transform/transform_test.clj
+++ test/metabase/transform/transform_test.clj
@@ -9,39 +9,62 @@
    [metabase.util :as u]
    [toucan2.core :as t2]))
 
+(mt/defdataset users-db
+  [["users"
+    [{:field-name "name" :base-type :type/Text}
+     {:field-name "age" :base-type :type/Integer}]
+    [["Foo" 10]
+     ["Bar" 20]
+     ["Baz" 30]]]])
+
+(mt/defdataset products-db
+  [["products"
+    [{:field-name "name" :base-type :type/Text}
+     {:field-name "price" :base-type :type/Decimal}]
+    [["Widget" 10.99]
+     ["Gadget" 25.50]]]])
+
+(mt/defdataset users-departments-db
+  [["users"
+    [{:field-name "name" :base-type :type/Text}
+     {:field-name "department_id" :base-type :type/Integer}]
+    [["Alice" 10]
+     ["Bob" 20]
+     ["Charlie" 10]]]
+   ["departments"
+    [{:field-name "idx" :base-type :type/Integer}
+     {:field-name "name" :base-type :type/Text}]
+    [[10 "Engineering"]
+     [20 "Sales"]]]])
+
+(defn- native-query-rows [query]
+  (set (mt/rows (qp/process-query (mt/native-query {:query query})))))
+
 (deftest create-view-test
   (mt/test-drivers
     (mt/normal-drivers-with-feature :view)
     (testing "Can create and replace a new view"
-      (mt/dataset (mt/dataset-definition "users-db"
-                                         ["users"
-                                          [{:field-name "name" :base-type :type/Text}
-                                           {:field-name "age" :base-type :type/Integer}]
-                                          [["Foo" 10]
-                                           ["Bar" 20]
-                                           ["Baz" 30]]])
+      (mt/dataset users-db
+
         (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "young_users")
+
         (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
                                  "young_users"
                                  "SELECT * FROM users where age < 25"))
-        (is (= [["Foo"] ["Bar"]]
-               (mt/rows (qp/process-query (mt/native-query {:query "SELECT name FROM \"young_users\""})))))
+        (is (= #{["Foo"] ["Bar"]}
+               (native-query-rows "SELECT name FROM \"young_users\"")))
         (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
                                  "young_users"
                                  "SELECT * FROM users where age < 35"
                                  :replace? true))
-        (is (= [["Foo"] ["Bar"] ["Baz"]]
-               (mt/rows (qp/process-query (mt/native-query {:query "SELECT name FROM \"young_users\""})))))))))
+        (is (= #{["Foo"] ["Bar"] ["Baz"]}
+               (native-query-rows "SELECT name FROM \"young_users\"")))))))
 
 (deftest drop-view-nonexistent-test
   (mt/test-drivers
     (mt/normal-drivers-with-feature :view)
     (testing "Dropping a non-existent view should not throw an error"
-      (mt/dataset (mt/dataset-definition "simple-db"
-                                         ["table1"
-                                          [{:field-name "name" :base-type :type/Text}]
-                                          [["Alice"]
-                                           ["Bob"]]])
+      (mt/dataset users-db
         ;; Should not throw even if view doesn't exist
         (is (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "nonexistent_view"))))))
 
@@ -49,12 +72,8 @@
   (mt/test-drivers
     (mt/normal-drivers-with-feature :view)
     (testing "Creating a view that already exists should fail without :replace? true"
-      (mt/dataset (mt/dataset-definition "products-db"
-                                         ["products"
-                                          [{:field-name "name" :base-type :type/Text}
-                                           {:field-name "price" :base-type :type/Decimal}]
-                                          [["Widget" 10.99]
-                                           ["Gadget" 25.50]]])
+      (mt/dataset products-db
+
         (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "expensive_products")
 
         (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
@@ -70,18 +89,8 @@
   (mt/test-drivers
     (mt/normal-drivers-with-feature :view)
     (testing "Creating a view with complex SQL"
-      (mt/dataset  (mt/dataset-definition "users-departments-db"
-                                          ["users"
-                                           [{:field-name "name" :base-type :type/Text}
-                                            {:field-name "department_id" :base-type :type/Integer}]
-                                           [["Alice" 10]
-                                            ["Bob" 20]
-                                            ["Charlie" 10]]]
-                                          ["departments"
-                                           [{:field-name "idx" :base-type :type/Integer}
-                                            {:field-name "name" :base-type :type/Text}]
-                                           [[10 "Engineering"]
-                                            [20 "Sales"]]])
+      (mt/dataset users-departments-db
+
         (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "user_departments")
 
         (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
@@ -89,43 +98,30 @@
                                  "SELECT u.name as user_name, d.name as dept_name FROM users u JOIN departments d ON u.department_id = d.idx"))
 
         (is (= #{["Alice" "Engineering"] ["Bob" "Sales"] ["Charlie" "Engineering"]}
-               (set (mt/rows (qp/process-query (mt/native-query {:query "SELECT user_name, dept_name FROM \"user_departments\""}))))))))))
+               (native-query-rows "SELECT user_name, dept_name FROM \"user_departments\"")))))))
 
 (deftest view-name-edge-cases-test
   (mt/test-drivers
     (mt/normal-drivers-with-feature :view)
     (testing "View names with special characters and edge cases"
-      (mt/dataset (mt/dataset-definition "edge-case-db"
-                                         ["test_table"
-                                          [{:field-name "value" :base-type :type/Text}]
-                                          [["test"]]])
-
+      (mt/dataset users-db
         (doseq [view-name ["view_with_underscores"
                            "ViewWithCamelCase"
                            "view123"
                            "view with spaces"]]
+          (driver/drop-view! driver/*driver* (u/the-id (mt/db)) view-name)
+
           (testing (str "View name: " view-name)
-            (driver/drop-view! driver/*driver* (u/the-id (mt/db)) view-name)
+            (is (driver/create-view! driver/*driver* (u/the-id (mt/db)) view-name "SELECT * FROM users"))
 
-            (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
-                                     view-name
-                                     "SELECT * FROM test_table"))
-
-            (is (= [[1 "test"]]
-                   (mt/rows (qp/process-query (mt/native-query {:query (format "SELECT * FROM \"%s\"" view-name)})))))
-
-            (driver/drop-view! driver/*driver* (u/the-id (mt/db)) view-name)))))))
+            (is (= #{[1 "Foo" 10] [2 "Bar" 20] [3 "Baz" 30]}
+                   (native-query-rows (format "SELECT * FROM \"%s\"" view-name))))))))))
 
 (deftest view-with-invalid-sql-test
   (mt/test-drivers
     (mt/normal-drivers-with-feature :view)
     (testing "Creating a view with invalid SQL should fail"
-      (mt/dataset (mt/dataset-definition "error-db"
-                                         ["table1"
-                                          [{:field-name "idx" :base-type :type/Integer}]
-                                          [[1]]])
-        (driver/drop-view! driver/*driver* (u/the-id (mt/db)) "invalid_view")
-
+      (mt/dataset users-db
         (is (thrown? Exception
                      (driver/create-view! driver/*driver* (u/the-id (mt/db))
                                           "invalid_view"
@@ -135,51 +131,45 @@
   (mt/test-drivers
     (mt/normal-drivers-with-feature :view)
     (testing "Can create a new view and immediately sync it"
-      (mt/dataset (mt/dataset-definition "users-db"
-                                         ["users"
-                                          [{:field-name "name" :base-type :type/Text}
-                                           {:field-name "age" :base-type :type/Integer}]
-                                          [["Foo" 10]
-                                           ["Bar" 20]
-                                           ["Baz" 30]]])
+      (mt/dataset users-db
+        (mt/with-temp-copy-of-db
 
-        (let [{:keys [schema]} (t2/select-one :model/Table :name "users")
-              base-name "young_users"
-              view-name (if schema
-                          (str schema "." base-name)
-                          base-name)]
+          (let [{:keys [schema]} (t2/select-one :model/Table :name "users")
+                base-name "young_users"
+                view-name (if schema
+                            (str schema "." base-name)
+                            base-name)]
+            (driver/drop-view! driver/*driver* (u/the-id (mt/db)) view-name)
 
-          (driver/drop-view! driver/*driver* (u/the-id (mt/db)) view-name)
+            (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
+                                     view-name
+                                     "SELECT name, age FROM users where age < 25"))
 
-          (is (driver/create-view! driver/*driver* (u/the-id (mt/db))
-                                   view-name
-                                   "SELECT name, age FROM users where age < 25"))
+            ;; make sure we're indeed creating it anew
+            (t2/delete! :model/Table :name view-name)
 
-          ;; make sure we're indeed creating it anew
-          (t2/delete! :model/Table :name view-name)
+            (sync/sync-new-table-metadata! (mt/db) {:table-name base-name :schema schema})
 
-          (sync/sync-new-table-metadata! (mt/db) {:table-name base-name :schema schema})
-
-          (let [table (t2/select-one :model/Table :name base-name :schema schema)]
-            (is (=? {:schema schema,
-                     :name base-name,
-                     :active true,
-                     :display_name "Young Users"} table))
-            (is (=? [{:semantic_type nil,
-                      :effective_type :type/Text,
-                      :active true,
-                      :position 0,
-                      :visibility_type :normal,
-                      :display_name "Name",
-                      :database_position 0,
-                      :base_type :type/Text}
-                     {:semantic_type nil,
-                      :effective_type :type/Integer,
-                      :active true,
-                      :position 1,
-                      :visibility_type :normal,
-                      :display_name "Age",
-                      :database_position 1,
-                      :base_type :type/Integer}]
-                    (sort-by :position
-                             (t2/select :model/Field :table_id (u/the-id table)))))))))))
+            (let [table (t2/select-one :model/Table :name base-name :schema schema)]
+              (is (=? {:schema schema,
+                       :name base-name,
+                       :active true,
+                       :display_name "Young Users"} table))
+              (is (=? [{:semantic_type nil,
+                        :effective_type :type/Text,
+                        :active true,
+                        :position 0,
+                        :visibility_type :normal,
+                        :display_name "Name",
+                        :database_position 0,
+                        :base_type :type/Text}
+                       {:semantic_type nil,
+                        :effective_type :type/Integer,
+                        :active true,
+                        :position 1,
+                        :visibility_type :normal,
+                        :display_name "Age",
+                        :database_position 1,
+                        :base_type :type/Integer}]
+                      (sort-by :position
+                               (t2/select :model/Field :table_id (u/the-id table))))))))))))
