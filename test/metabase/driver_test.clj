674ccfd122b6407706c96c2c3e98d914daaee812
Dan Stone <wotbrew@gmail.com>
2025-07-11 11:07:13 +0100

    Assert table synced

--- test/metabase/driver_test.clj
+++ test/metabase/driver_test.clj
@@ -301,6 +301,7 @@
                       {:field-name "c" :base-type :type/Integer, :not-null? true}]
           []])
         (let [table   (t2/select-one :model/Table :db_id (mt/id) :name table-name)
+              _       (assert table)
               fields  (describe-fields-for-table (mt/db) table)
               [a b c] (->> ["a" "b" "c"]
                            (map #(ddl.i/format-name driver/*driver* %))
@@ -325,6 +326,7 @@
                       {:field-name "c" :base-type :type/Integer}]
           []])
         (let [table   (t2/select-one :model/Table :db_id (mt/id) :name table-name)
+              _       (assert table)
               fields  (describe-fields-for-table (mt/db) table)
               [a b c] (->> ["a" "b" "c"]
                            (map #(ddl.i/format-name driver/*driver* %))
@@ -350,6 +352,7 @@
                       {:field-name "c" :base-type :type/Integer}]
           []])
         (let [table   (t2/select-one :model/Table :db_id (mt/id) :name table-name)
+              _       (assert table)
               fields  (describe-fields-for-table (mt/db) table)
               [a b c] (->> ["a" "b" "c"]
                            (map #(ddl.i/format-name driver/*driver* %))
