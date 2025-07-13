cb19b43bcecbc604b3a45efcfe3d01e1ca34bacc
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-07-13 22:50:56 +0200

    DRY

--- e2e/snapshot-creators/qa-db.cy.snap.js
+++ e2e/snapshot-creators/qa-db.cy.snap.js
@@ -20,33 +20,15 @@ describe("qa databases snapshots", { tags: "@external" }, () => {
       addPostgresDatabase();
       snapshot("postgres-12");
 
-      setupWritableDB("postgres");
-      cy.get("@postgresID").then((id) => {
-        cy.log("**-- Enabling actions --**");
-        cy.request("PUT", `/api/database/${id}`, {
-          details: {
-            dbname: "writable_db",
-          },
-          settings: { "database-enable-actions": true },
-        });
-      });
+      convertToWritable("postgres");
       snapshot("postgres-writable");
+
       restoreAndAuthenticate();
 
       addMySQLDatabase({});
       snapshot("mysql-8");
 
-      setupWritableDB("mysql");
-      cy.get("@mysqlID").then((id) => {
-        cy.log("**-- Enabling actions --**");
-        cy.request("PUT", `/api/database/${id}`, {
-          details: {
-            dbname: "writable_db",
-            user: "root",
-          },
-          settings: { "database-enable-actions": true },
-        });
-      });
+      convertToWritable("mysql");
       snapshot("mysql-writable");
     }
     restore("blank");
@@ -56,4 +38,28 @@ describe("qa databases snapshots", { tags: "@external" }, () => {
 function restoreAndAuthenticate() {
   restore("default");
   cy.signInAsAdmin();
+}
+
+/**
+ * Takes the existing postgres or mysql database, creates a new database with the
+ * name `writable_db` if it doesn't exist already, and then alters the connection
+ * details to point to that new database.
+ *
+ * @param {"postgres" | "mysql"} engine
+ */
+function convertToWritable(engine) {
+  setupWritableDB(engine);
+
+  const idAlias = `@${engine}ID`;
+
+  cy.get(idAlias).then((id) => {
+    cy.log("**-- Enabling actions --**");
+    cy.request("PUT", `/api/database/${id}`, {
+      details: {
+        dbname: "writable_db",
+        ...(engine === "mysql" ? { user: "root" } : {}),
+      },
+      settings: { "database-enable-actions": true },
+    });
+  });
 }
