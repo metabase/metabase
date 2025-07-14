b4e7935eaf1380b2ace15be81c96153d8e1ddbee
Oisin Coveney <oisin@metabase.com>
2025-07-14 11:36:35 +0300

    Fix type errors, bad imports, bad values

--- frontend/src/metabase/dashboard/components/DashCard/DashCardActionsPanel/use-duplicate-dashcard.ts
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardActionsPanel/use-duplicate-dashcard.ts
@@ -6,11 +6,8 @@ import {
 } from "metabase/dashboard/actions";
 import { getExistingDashCards } from "metabase/dashboard/actions/utils";
 import { trackDashcardDuplicated } from "metabase/dashboard/analytics";
-import {
-  getDashboards,
-  getDashcards,
-  getSelectedTabId,
-} from "metabase/dashboard/selectors";
+import { useDashboardContext } from "metabase/dashboard/context";
+import { getDashboards, getDashcards } from "metabase/dashboard/selectors";
 import {
   generateTemporaryDashcardId,
   isQuestionDashCard,
@@ -19,21 +16,27 @@ import {
 import { getPositionForNewDashCard } from "metabase/lib/dashboard_grid";
 import { useDispatch, useSelector } from "metabase/lib/redux";
 import { isVisualizerDashboardCard } from "metabase/visualizer/utils";
-import type { Dashboard, DashboardCard } from "metabase-types/api";
+import type { DashboardCard } from "metabase-types/api";
 
 export function useDuplicateDashCard({
-  dashboard,
   dashcard,
 }: {
-  dashboard: Dashboard;
   dashcard?: DashboardCard;
 }) {
   const dispatch = useDispatch();
+  const { dashboard, selectedTabId } = useDashboardContext();
   const dashboards = useSelector(getDashboards);
   const dashcards = useSelector(getDashcards);
-  const selectedTabId = useSelector(getSelectedTabId);
 
   return useCallback(() => {
+    if (!dashboard) {
+      return () => {
+        throw new Error(
+          "duplicateDashcard was called with an undefined dashboard",
+        );
+      };
+    }
+
     if (!dashcard) {
       return () => {
         throw new Error(
@@ -90,5 +93,5 @@ export function useDuplicateDashCard({
     }
 
     trackDashcardDuplicated(dashboard.id);
-  }, [dispatch, dashboard.id, dashboards, dashcard, dashcards, selectedTabId]);
+  }, [dashboard, dashcard, dashboards, dashcards, selectedTabId, dispatch]);
 }
