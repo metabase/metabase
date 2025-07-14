3a4f58bf4a4501392a0ca113fbc0733a9b436c00
Romeo Van Snick <romeo@romeovansnick.be>
2025-07-14 16:56:29 +0200

    Use separate flag for cljs dev tools

--- frontend/src/metabase/dev.js
+++ frontend/src/metabase/dev.js
@@ -2,7 +2,7 @@ if (
   // eslint-disable-next-line no-undef
   process.env.ENABLE_CLJS_HOT_RELOAD === "true" ||
   // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "development"
+  process.env.ENABLE_CLJS_DEV_TOOLS === "true"
 ) {
   import("cljs/metabase.util.devtools");
 }
