f89975091edd28cf5b8927b83445afe7495b2574
Romeo Van Snick <romeo@romeovansnick.be>
2025-07-14 11:38:31 +0200

    Enable cljs dev tools when not hot reloading too

--- frontend/src/metabase/dev.js
+++ frontend/src/metabase/dev.js
@@ -1,4 +1,8 @@
-// eslint-disable-next-line no-undef
-if (process.env.ENABLE_CLJS_HOT_RELOAD === "true") {
+if (
+  // eslint-disable-next-line no-undef
+  process.env.ENABLE_CLJS_HOT_RELOAD === "true" ||
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV === "development"
+) {
   import("cljs/metabase.util.devtools");
 }
