5d76b69d9a3627b7b0b2b663b3790daa91f6e4e6
Jakub Chodorowicz <jakub.chodorowicz@metabase.com>
2025-07-15 17:29:09 +0200

    Add metabase window name (#60964)

--- enterprise/frontend/src/metabase-enterprise/license/use-upsell-flow.ts
+++ enterprise/frontend/src/metabase-enterprise/license/use-upsell-flow.ts
@@ -8,6 +8,7 @@ import { useSelector } from "metabase/lib/redux";
 import { useLicense } from "metabase-enterprise/settings/hooks/use-license";
 
 const NOTIFICATION_TIMEOUT = 30_000;
+const METABASE_INSTANCE_WINDOW_NAME = "metabase-instance";
 
 /**
  * This hook allows to create an upsell flow that listens to the events from the Store tab
@@ -57,6 +58,12 @@ export function useUpsellFlow({
   }
 
   useEffect(() => {
+    const { name } = window;
+    // Setting window name allows Store to move the focus back to the named window
+    if (name !== METABASE_INSTANCE_WINDOW_NAME) {
+      window.name = METABASE_INSTANCE_WINDOW_NAME;
+    }
+
     const listener = createListener({
       updateToken,
       storeOrigin,
@@ -65,6 +72,7 @@ export function useUpsellFlow({
     window.addEventListener("message", listener);
 
     return () => {
+      window.name = name;
       window.removeEventListener("message", listener);
     };
   }, [updateToken, storeOrigin]);
