8aacf5e20e53ed56857ef504fe23657585faf302
Romeo Van Snick <romeo@romeovansnick.be>
2025-07-15 19:09:08 +0200

    Remove virtualization from AccordionList altogether

--- frontend/src/metabase/common/components/AccordionList/AccordionList.tsx
+++ frontend/src/metabase/common/components/AccordionList/AccordionList.tsx
@@ -6,12 +6,6 @@ import {
   type RefObject,
   createRef,
 } from "react";
-import {
-  type Alignment,
-  CellMeasurer,
-  CellMeasurerCache,
-  List,
-} from "react-virtualized";
 import _ from "underscore";
 
 import { Box } from "metabase/ui";
@@ -62,22 +56,17 @@ type State = {
   openSection: number | null;
   searchText: string;
   cursor: Cursor | null;
-  scrollToAlignment: Alignment;
 };
 
 export class AccordionList<
   TItem extends Item,
   TSection extends Section<TItem> = Section<TItem>,
 > extends Component<Props<TItem, TSection>, State> {
-  _cache: CellMeasurerCache;
-
-  listRef: RefObject<List>;
   listRootRef: RefObject<HTMLDivElement>;
 
   _initialSelectedRowIndex?: number;
   _startIndex?: number;
   _stopIndex?: number;
-  _forceUpdateTimeout?: NodeJS.Timeout | null;
 
   constructor(props: Props<TItem, TSection>, context: unknown) {
     super(props, context);
@@ -105,96 +94,25 @@ export class AccordionList<
       openSection,
       searchText: "",
       cursor: null,
-      scrollToAlignment: "start",
     };
 
-    this._cache = new CellMeasurerCache({
-      fixedWidth: true,
-      minHeight: 10,
-    });
-
     this.listRootRef = createRef();
-    this.listRef = createRef();
   }
 
   componentDidMount() {
-    // NOTE: for some reason the row heights aren't computed correctly when
-    // first rendering, so force the list to update
-    this._forceUpdateList();
-    // `scrollToRow` upon mounting, after _forceUpdateList
-    // Use list.scrollToRow instead of the scrollToIndex prop since the
-    // causes the list's scrolling to be pinned to the selected row
     setTimeout(() => {
-      const container = this._getListContainerElement();
-
+      const container = this.listRootRef.current;
       const hasFocusedChildren = container?.contains(document.activeElement);
+      // focus the container on opening, unless a child element is already focused
       if (!hasFocusedChildren && this.props.hasInitialFocus) {
         container?.focus();
       }
 
-      const index = this._initialSelectedRowIndex;
-
-      if (
-        this.listRef.current &&
-        index != null &&
-        this._startIndex != null &&
-        this._stopIndex != null &&
-        !(index >= this._startIndex && index <= this._stopIndex)
-      ) {
-        this.listRef.current?.scrollToRow(index);
-      }
-    }, 0);
-  }
-
-  componentDidUpdate(_prevProps: Props<TItem, TSection>, prevState: State) {
-    // if anything changes that affects the selected rows we need to clear the row height cache
-    if (
-      this.state.openSection !== prevState.openSection ||
-      this.state.searchText !== prevState.searchText
-    ) {
-      this._clearRowHeightCache();
-    }
-  }
-
-  componentWillUnmount() {
-    // ensure _forceUpdateList is not called after unmounting
-    if (this._forceUpdateTimeout != null) {
-      clearTimeout(this._forceUpdateTimeout);
-      this._forceUpdateTimeout = null;
-    }
-  }
-
-  _getListContainerElement() {
-    const element = this.isVirtualized()
-      ? // @ts-expect-error: TODO remove reliance on internals here
-        this.listRef.current?.Grid?._scrollingContainer
-      : this.listRootRef.current;
-
-    return element ?? null;
-  }
-
-  // resets the row height cache when the displayed rows change
-  _clearRowHeightCache() {
-    this._cache.clearAll();
-    this._forceUpdateList();
-
-    // NOTE: unclear why this needs to be async
-    this._forceUpdateTimeout = setTimeout(() => {
-      this._forceUpdateTimeout = null;
-      this._forceUpdateList();
-    });
-  }
-
-  _forceUpdateList() {
-    if (this.listRef.current) {
-      // NOTE: unclear why this particular set of functions works, but it does
-      this.listRef.current.invalidateCellSizeAfterRender({
-        columnIndex: 0,
-        rowIndex: 0,
+      // scroll to the selected row on opening
+      container?.querySelector("[aria-selected=true]")?.scrollIntoView({
+        block: "center",
       });
-      this.listRef.current.forceUpdateGrid();
-      this.forceUpdate();
-    }
+    }, 0);
   }
 
   toggleSection = (sectionIndex: number) => {
@@ -302,10 +220,7 @@ export class AccordionList<
         this.canSelectSection,
       );
 
-      return this.setState({
-        cursor: prevCursor,
-        scrollToAlignment: "auto",
-      });
+      return this.setState({ cursor: prevCursor });
     }
 
     if (event.key === "ArrowDown") {
@@ -317,10 +232,7 @@ export class AccordionList<
         this.canSelectSection,
       );
 
-      return this.setState({
-        cursor: nextCursor,
-        scrollToAlignment: "auto",
-      });
+      return this.setState({ cursor: nextCursor });
     }
 
     if (event.key === "Enter") {
@@ -538,12 +450,6 @@ export class AccordionList<
     );
   };
 
-  // Because of virtualization, focused search input can be removed which does not trigger blur event.
-  // We need to restore focus on the component root container to make keyboard navigation working
-  handleSearchRemoval = () => {
-    this._getListContainerElement()?.focus();
-  };
-
   render() {
     const {
       id,
@@ -552,145 +458,52 @@ export class AccordionList<
       className,
       globalSearch = false,
       sections,
-      role = "grid",
       withBorders,
       "data-testid": testId,
-      maxHeight = Infinity,
-
       alwaysExpanded = false,
     } = this.props;
-    const { cursor, scrollToAlignment } = this.state;
 
     const rows = this.getRows();
-
-    const scrollToIndex =
-      cursor != null ? rows.findIndex(this.isRowSelected) : undefined;
-
     const searchRowIndex = rows.findIndex((row) => row.type === "search");
     const hasSearch = searchRowIndex >= 0;
 
-    if (!this.isVirtualized()) {
-      return (
-        <Box
-          className={cx(S.accordionListRoot, className, {
-            [S.hasSearch]: hasSearch,
-          })}
-          ref={this.listRootRef}
-          role="tree"
-          onKeyDown={this.handleKeyDown}
-          tabIndex={-1}
-          style={{
-            width,
-            ...style,
-          }}
-          data-testid={testId}
-        >
-          {rows.map((row, index) => (
-            <AccordionListCell<TItem, TSection>
-              key={index}
-              {...this.props}
-              row={row}
-              sections={sections}
-              onChange={this.handleChange}
-              searchText={this.state.searchText}
-              onChangeSearchText={this.handleChangeSearchText}
-              sectionIsExpanded={this.isSectionExpanded}
-              alwaysExpanded={
-                alwaysExpanded ||
-                (globalSearch && this.state.searchText.length > 0)
-              }
-              canToggleSections={this.canToggleSections()}
-              toggleSection={this.toggleSection}
-              hasCursor={this.isRowSelected(rows[index])}
-              withBorders={withBorders}
-            />
-          ))}
-        </Box>
-      );
-    }
-
-    const max =
-      maxHeight > 0 && maxHeight < Infinity ? maxHeight : window.innerHeight;
-
-    const height = Math.min(
-      max,
-      rows.reduce(
-        (height, _row, index) => height + this._cache.rowHeight({ index }),
-        0,
-      ),
-    );
-
     return (
-      <List
+      <Box
         id={id}
-        ref={this.listRef}
-        className={cx(className, { [S.hasSearch]: hasSearch })}
+        className={cx(S.accordionListRoot, className, {
+          [S.hasSearch]: hasSearch,
+        })}
+        ref={this.listRootRef}
+        role="tree"
+        onKeyDown={this.handleKeyDown}
+        tabIndex={-1}
         style={{
-          // HACK - Ensure the component can scroll
-          // This is a temporary fix to handle cases where the parent component doesn’t pass in the correct `maxHeight`
-          overflowY: "auto",
-          outline: "none",
-          maxWidth: width,
-          maxHeight,
+          width,
           ...style,
         }}
-        containerStyle={{
-          pointerEvents: "auto",
-          overflow: "auto",
-          maxHeight: "inherit",
-        }}
-        // @ts-expect-error: TODO
-        width={width}
-        height={height}
-        rowCount={rows.length}
-        deferredMeasurementCache={this._cache}
-        rowHeight={this._cache.rowHeight}
-        // HACK: needs to be large enough to render enough rows to fill the screen since we used
-        // the CellMeasurerCache to calculate the height
-        overscanRowCount={100}
-        scrollToIndex={scrollToIndex}
-        scrollToAlignment={scrollToAlignment}
-        containerRole={role}
-        containerProps={{
-          onKeyDown: this.handleKeyDown,
-          "data-testid": testId,
-        }}
-        rowRenderer={({ key, index, parent, style }) => (
-          <CellMeasurer
-            cache={this._cache}
-            columnIndex={0}
-            key={key}
-            rowIndex={index}
-            parent={parent}
-          >
-            {({ registerChild }) => (
-              <AccordionListCell<TItem, TSection>
-                ref={registerChild}
-                hasCursor={this.isRowSelected(rows[index])}
-                {...this.props}
-                style={style}
-                row={rows[index]}
-                sections={sections}
-                onChange={this.handleChange}
-                searchText={this.state.searchText}
-                onChangeSearchText={this.handleChangeSearchText}
-                sectionIsExpanded={this.isSectionExpanded}
-                canToggleSections={this.canToggleSections()}
-                toggleSection={this.toggleSection}
-                withBorders={withBorders}
-              />
-            )}
-          </CellMeasurer>
-        )}
-        onRowsRendered={({ startIndex, stopIndex }) => {
-          this._startIndex = startIndex;
-          this._stopIndex = stopIndex;
-
-          if (searchRowIndex < startIndex || searchRowIndex > stopIndex) {
-            this.handleSearchRemoval();
-          }
-        }}
-      />
+        data-testid={testId}
+      >
+        {rows.map((row, index) => (
+          <AccordionListCell<TItem, TSection>
+            key={index}
+            {...this.props}
+            row={row}
+            sections={sections}
+            onChange={this.handleChange}
+            searchText={this.state.searchText}
+            onChangeSearchText={this.handleChangeSearchText}
+            sectionIsExpanded={this.isSectionExpanded}
+            alwaysExpanded={
+              alwaysExpanded ||
+              (globalSearch && this.state.searchText.length > 0)
+            }
+            canToggleSections={this.canToggleSections()}
+            toggleSection={this.toggleSection}
+            hasCursor={this.isRowSelected(rows[index])}
+            withBorders={withBorders}
+          />
+        ))}
+      </Box>
     );
   }
 }
