85fa3a811580be2f60c76210c93e90e46d88f148
Dan Stone <wotbrew@gmail.com>
2025-07-16 17:25:30 +0100

    Sparks of joy

--- enterprise/backend/test/metabase_enterprise/semantic_search/query_test.clj
+++ enterprise/backend/test/metabase_enterprise/semantic_search/query_test.clj
@@ -3,13 +3,13 @@
    [clojure.test :refer :all]
    [metabase-enterprise.semantic-search.db :as semantic.db]))
 
-(defn- once-fixture [f]
+(defn- once-fixture! [f]
   (when semantic.db/db-url
     (when-not @semantic.db/data-source
       (semantic.db/init-db!))
     (f)))
 
-(use-fixtures :once #'once-fixture)
+(use-fixtures :once #'once-fixture!)
 
 (deftest database-initialised-test
   (is (some? @semantic.db/data-source))
