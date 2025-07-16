9e3bf93aaf2ec724a32f6737d19915f814467292
Noah Moss <noahbmoss@gmail.com>
2025-07-15 22:56:02 -0400

    remove ^:const to try to fix compilation errorin tests

--- test/metabase/test/util/namespace.clj
+++ test/metabase/test/util/namespace.clj
@@ -8,10 +8,6 @@
 (defonce ^{:doc "Vector of symbols of all Metabase namespaces, excluding test namespaces. This is intended
   for use by various test routines that need to load related namespaces.
 
-  DEPRECATED: Using this is an anti-pattern, it messes up our ability to analyze the code and find dependencies between
-  namespaces or to topographically sort them correctly during compilation. See
-  https://metaboat.slack.com/archives/CKZEMT1MJ/p1734635053499399 or ask Cam for more info.
-
   This was moved from metabase.util.jvm for testing purposes only."}
   ^:deprecated metabase-namespace-symbols
   (vec (sort (for [ns-symb (ns.find/find-namespaces (classpath/system-classpath))
