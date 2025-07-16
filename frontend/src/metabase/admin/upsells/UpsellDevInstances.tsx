92b8e677e95c38529fd207ad2b7d4cc6026d1f17
Jakub Chodorowicz <jakub.chodorowicz@metabase.com>
2025-07-16 10:36:15 +0200

    Disable new flow in dev instances upsell (#61048)

--- frontend/src/metabase/admin/upsells/UpsellDevInstances.tsx
+++ frontend/src/metabase/admin/upsells/UpsellDevInstances.tsx
@@ -1,7 +1,6 @@
 import { t } from "ttag";
 
 import { useSetting } from "metabase/common/hooks";
-import { PLUGIN_ADMIN_SETTINGS } from "metabase/plugins";
 import { getStoreUrl } from "metabase/selectors/settings";
 import { Text } from "metabase/ui";
 
@@ -12,10 +11,6 @@ type LOCATION = "embedding-page" | "settings-general";
 export function UpsellDevInstances({ location }: { location: LOCATION }) {
   const isDevMode = useSetting("development-mode?");
   const campaign = "dev_instances";
-  const { triggerUpsellFlow } = PLUGIN_ADMIN_SETTINGS.useUpsellFlow({
-    campaign,
-    location,
-  });
 
   if (isDevMode) {
     return null;
@@ -29,7 +24,6 @@ export function UpsellDevInstances({ location }: { location: LOCATION }) {
       buttonLink={getStoreUrl("account/new-dev-instance")}
       location={location}
       dismissible
-      onClick={triggerUpsellFlow}
     >
       <Text size="sm">
         {t`Test out code in staging in a separate Metabase instance before deploying to production.`}
