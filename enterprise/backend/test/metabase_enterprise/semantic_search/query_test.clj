aa8837e7c9c3842afa61b1e8525d739b9d1dfc06
Dan Stone <wotbrew@gmail.com>
2025-07-16 14:58:36 +0100

    Add a quick example test to see if things work

---
+++ enterprise/backend/test/metabase_enterprise/semantic_search/query_test.clj
@@ -0,0 +1,14 @@
+(ns metabase-enterprise.semantic-search.query-test
+  (:require [clojure.test :refer :all]
+            [metabase-enterprise.semantic-search.db :as semantic.db]))
+
+(defn once-fixture [f]
+  (when-not @semantic.db/data-source
+    (semantic.db/init-db!))
+  (f))
+
+(use-fixtures :once #'once-fixture)
+
+(deftest database-initialised-test
+  (is (some? @semantic.db/data-source))
+  (is (= {:test 1} (semantic.db/test-connection!))))
