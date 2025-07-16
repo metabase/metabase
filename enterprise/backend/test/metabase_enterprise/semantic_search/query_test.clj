7bacdc976123acd6a356f6feb9bae46701221895
Dan Stone <wotbrew@gmail.com>
2025-07-16 17:49:30 +0100

    Add a delay to appease the kondo gods
    Not sure this rule should apply to once-fixture, but for now lets fix
    it.

--- enterprise/backend/test/metabase_enterprise/semantic_search/query_test.clj
+++ enterprise/backend/test/metabase_enterprise/semantic_search/query_test.clj
@@ -3,13 +3,18 @@
    [clojure.test :refer :all]
    [metabase-enterprise.semantic-search.db :as semantic.db]))
 
-(defn- once-fixture! [f]
-  (when semantic.db/db-url
+;; delay is used to satisfy kondo validate-deftest rule (guarantees multiple threads cannot race to init the db)
+(def ^:private init-delay
+  (delay
     (when-not @semantic.db/data-source
-      (semantic.db/init-db!))
+      (semantic.db/init-db!))))
+
+(defn- once-fixture [f]
+  (when semantic.db/db-url
+    @init-delay
     (f)))
 
-(use-fixtures :once #'once-fixture!)
+(use-fixtures :once #'once-fixture)
 
 (deftest database-initialised-test
   (is (some? @semantic.db/data-source))
