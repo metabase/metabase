0e75b6bbacc0ea944d08e8a65e9dca7b3ee007ff
Kamil Mielnik <kamil@kamilmielnik.com>
2025-07-18 00:17:44 +0700

    SEM-463 Responsive buttons (#60928)
    * Add ResponsiveButton component
    
    * Prevent focus state being cut off
    
    * Make the buttons responsive in the table section
    
    * Make the buttons responsive in field section
    
    * Extract useResponsiveButtons
    
    * Add comments
    
    * Extract useResponsiveButtons in FieldSection
    
    * Add aria-label to the button
    
    * Add tests
    
    * Test tooltips
    
    * Fix indent

--- frontend/src/metabase/metadata/pages/DataModel/components/index.ts
+++ frontend/src/metabase/metadata/pages/DataModel/components/index.ts
@@ -2,6 +2,7 @@ export * from "./FieldSection";
 export * from "./FieldValuesModal";
 export * from "./NoDatabasesEmptyState";
 export * from "./PreviewSection";
+export * from "./ResponsiveButton";
 export * from "./SegmentsLink";
 export * from "./SyncOptionsModal";
 export * from "./TablePicker";
