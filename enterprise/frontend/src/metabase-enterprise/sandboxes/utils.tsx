9451905c54dc35d48568019b0c6017b0f857e581
Ryan Laurie <30528226+iethree@users.noreply.github.com>
2025-07-17 12:07:34 -0600

    allow tenant attributes CRUD (#60347)
    wip merge user and tenant attributes
    
    WIP use new api
    
    Split Mapping Editor
    
    types fixed
    
    so much unit testing
    
    handle null cases
    
    update tests
    
    update a couple more e2e tests
    
    less silly test
    
    make things nicer
    
    invalidate user cache on tenant update
    
    # Conflicts:
    #	enterprise/frontend/src/metabase-enterprise/api/tenants.ts

--- enterprise/frontend/src/metabase-enterprise/sandboxes/utils.tsx
+++ enterprise/frontend/src/metabase-enterprise/sandboxes/utils.tsx
@@ -1,11 +1,19 @@
 import type { ReactNode } from "react";
 import { t } from "ttag";
 
-import type { MappingEditorEntry } from "metabase/common/components/MappingEditor";
 import { Group, Icon, type SelectProps, Tooltip } from "metabase/ui";
-import type { GroupTableAccessPolicy, Table, Tenant } from "metabase-types/api";
+import type {
+  GroupTableAccessPolicy,
+  StructuredUserAttributes,
+  Table,
+  Tenant,
+} from "metabase-types/api";
 
-import type { GroupTableAccessPolicyParams } from "./types";
+import type {
+  GroupTableAccessPolicyParams,
+  MappingEditorEntry,
+  MappingType,
+} from "./types";
 
 const TENANT_SLUG_ATTRIBUTE = "@tenant.slug";
 
@@ -42,24 +50,78 @@ export const renderUserAttributesForSelect: SelectProps["renderOption"] = ({
   </Group>
 );
 
-export const getDisabledTenantUserAttribute = (
+// this needs to return a new array instead of mutating the original
+// in order to trigger a re-render
+export const addEntry = <T,>(entries: MappingEditorEntry<T>[]) => {
+  return [...entries, { key: "", value: "" }];
+};
+
+export const removeEntry = <T,>(
+  entries: MappingEditorEntry<T>[],
+  index: number,
+) => {
+  return entries.toSpliced(index, 1);
+};
+
+export const replaceEntryValue = <T,>(
+  entries: MappingEditorEntry<T>[],
+  index: number,
+  newValue: T,
+) => {
+  const newEntries = [...entries];
+  newEntries[index].value = newValue;
+  return newEntries;
+};
+
+export const replaceEntryKey = <T,>(
+  entries: MappingEditorEntry<T>[],
+  index: number,
+  newKey: string,
+) => {
+  const newEntries = [...entries];
+  newEntries[index].key = newKey;
+  return newEntries;
+};
+
+export const buildEntries = <T,>(
+  mapping?: MappingType<T>,
+): MappingEditorEntry<T>[] =>
+  Object.entries(mapping ?? {}).map(([key, value]) => ({ key, value }));
+
+export const buildMapping = <T,>(
+  entries: MappingEditorEntry<T>[],
+): MappingType<T> =>
+  entries.reduce((memo: MappingType<T>, { key, value }) => {
+    if (key) {
+      memo[key] = value;
+    }
+    return memo;
+  }, {});
+
+export const getExtraAttributes = (
+  structuredAttributes?: StructuredUserAttributes,
   tenant?: Tenant,
-): MappingEditorEntry[] => {
-  if (tenant) {
-    return [
-      {
-        key: TENANT_SLUG_ATTRIBUTE,
-        value: tenant.slug,
-        keyOpts: {
-          leftSection: (
-            <Tooltip label={t`This attribute is system defined`}>
-              <Icon name="info" c="text-light" />
-            </Tooltip>
-          ),
-        },
-      },
-    ];
+): StructuredUserAttributes | undefined => {
+  if (!tenant || structuredAttributes?.[TENANT_SLUG_ATTRIBUTE]) {
+    return structuredAttributes;
   }
 
-  return [];
+  // for newly created tenant users, we want to pre-populate their inherited attributes
+  return {
+    [TENANT_SLUG_ATTRIBUTE]: {
+      value: tenant.slug,
+      source: "tenant",
+      frozen: true,
+    },
+    ...Object.fromEntries(
+      Object.entries(tenant.attributes ?? {}).map(([key, value]) => [
+        key,
+        {
+          value,
+          source: "tenant",
+          frozen: false,
+        },
+      ]),
+    ),
+  };
 };
