8dd7b186509076d7d8c8f0b932d6528ffb05ab65
dan sutton <dan@dpsutton.com>
2025-07-17 15:57:13 -0500

    memory tests and new memory test runner
    - constrains memory at 400mb (ideally will be lower when we solve the
      issue around malli schema)
    - tests csv downloads to ensure we are streaming them and not realizing
      them in memory. It's only a single test but more will follow quickly
    - debating whether same test classpath root or a new one. Currently
      leaning towards new root so that these don't accidentally run in
      regular CI. Those jobs have 12GB of ram so it would just be slower and
      not instructive.

---
+++ performance/metabase/memory/csv_test.clj
@@ -0,0 +1,78 @@
+(ns ^:mb/memory metabase.memory.csv-test
+  (:require
+   [clojure.test :refer :all]
+   [metabase.driver :as driver]
+   [metabase.test :as mt]))
+
+(set! *warn-on-reflection* true)
+
+(defn- readable [bytes]
+  (cond (nil? bytes) "0 bytes"
+        (zero? bytes) "0 bytes"
+        :else
+        (let [units ["b" "kb" "mb" "gb" "tb"]
+              magnitude 1024.0]
+          (loop [[unit & remaining] units
+                 current (double bytes)]
+            (if (and (seq remaining) (> current magnitude))
+              (recur remaining (/ current magnitude))
+              (format "%.1f %s" current unit))))))
+
+(defn- consume
+  [^java.io.InputStream is]
+  (let [arr (byte-array 8024)]
+    (loop [c 0, gas 5000000]
+      (let [r (java.io.InputStream/.read is arr 0 (alength arr))]
+        (cond (zero? gas)
+              (throw (ex-info "Ran out of gas" {}))
+              (neg? r) c
+              :else (recur (+ c r) (dec gas)))))))
+
+(def driver->query
+  {:sqlite "WITH RECURSIVE generate_series(value) AS (
+  SELECT 1
+  UNION ALL
+  SELECT value + 1 FROM generate_series WHERE value + 1 <= 1000000
+)
+SELECT replace(hex(zeroblob(1024)), '00', 'xx') FROM generate_series;"
+   :postgres "SELECT repeat('x', 2048) FROM generate_series(1, 1000000)"
+   :mysql "SELECT REPEAT('x', 2048) FROM
+(SELECT @row := @row + 1 AS row_number FROM
+(SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 0) t1,
+(SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 0) t2,
+(SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 0) t3,
+(SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 0) t4,
+(SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 0) t5,
+(SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 UNION SELECT 0) t6,
+(SELECT @row := 0) r
+LIMIT 1000000) numbers"
+   :bigquery-cloud-sdk "SELECT REPEAT('x', 2048) FROM UNNEST(GENERATE_ARRAY(1, 1000000)) AS num"
+   :snowflake "SELECT REPEAT('x', 2048) FROM TABLE(GENERATOR(ROWCOUNT => 1000000))"
+   :clickhouse "SELECT repeat('x', 2048)
+FROM system.numbers
+LIMIT 1000000;"
+   })
+
+(deftest large-csv-test
+  (mt/test-drivers #{:postgres :mysql :sqlite :bigquery-cloud-sdk :snowflake}
+    (testing "Can download large CSVs without holding the entire results in memory #60733"
+      (let [large-query (or (driver->query driver/*driver*)
+                            (throw (ex-info "Driver doesn't implement big query"
+                                            {:driver driver/*driver*
+                                             :available (keys driver->query)})))
+            result (mt/user-real-request :crowberto :post 200 "dataset/csv"
+                                         {:request-options {:as :stream}}
+                                         {:query (mt/native-query
+                                                   {:query large-query})
+                                          :format_rows true})
+            size (consume result)]
+        (is (> size 2000000000)
+            (format "Only consumed %s but expected ~2gb" (readable size)))))))
+
+(comment
+  (mt/set-test-drivers! #{:postgres})
+  (mt/set-test-drivers! #{:sqlite})
+  (mt/set-test-drivers! #{:clickhouse})
+  (mt/set-test-drivers! #{:redshift})
+  (mt/test-drivers #{:redshift}
+    (mt/db)))
