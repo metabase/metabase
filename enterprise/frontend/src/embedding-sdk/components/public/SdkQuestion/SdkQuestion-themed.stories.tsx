269baf7e1a7102986f1b59f50fcca0ccc4d61f1b
Timofei <timofei@metabase.com>
2025-07-18 15:31:38 +0400

    Adjust type imports

--- enterprise/frontend/src/embedding-sdk/components/public/SdkQuestion/SdkQuestion-themed.stories.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/SdkQuestion/SdkQuestion-themed.stories.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 
-import type { MetabaseTheme } from "embedding-sdk";
 import { storybookSdkAuthDefaultConfig } from "embedding-sdk/test/CommonSdkStoryWrapper";
 import { questionIds } from "embedding-sdk/test/storybook-id-args";
 import { storybookThemes } from "embedding-sdk/test/storybook-themes";
-import { defineMetabaseTheme } from "metabase/embedding-sdk/theme";
+import {
+  type MetabaseTheme,
+  defineMetabaseTheme,
+} from "metabase/embedding-sdk/theme";
 import { Box } from "metabase/ui";
 
 import { MetabaseProvider } from "../MetabaseProvider";
