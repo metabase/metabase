35e1ab8f75ab07c8605503b31a150f54e67a244e
Dan Stone <wotbrew@gmail.com>
2025-07-21 17:13:36 +0100

    Aggressive WIP
    Might walk back some controversial changes here, just wanna see green first

--- enterprise/backend/src/metabase_enterprise/semantic_search/core.clj
+++ enterprise/backend/src/metabase_enterprise/semantic_search/core.clj
@@ -2,6 +2,7 @@
   "Enterprise implementations of semantic search core functions using defenterprise."
   (:require
    [metabase-enterprise.semantic-search.db :as semantic.db]
+   [metabase-enterprise.semantic-search.embedding :as semantic.embedding]
    [metabase-enterprise.semantic-search.index :as semantic.index]
    [metabase-enterprise.semantic-search.settings :as semantic.settings]
    [metabase.premium-features.core :refer [defenterprise]]))
@@ -19,7 +20,7 @@
   :feature :semantic-search
   [search-ctx]
   (when-not @semantic.db/data-source (semantic.db/init-db!))
-  (semantic.index/query-index search-ctx))
+  (semantic.index/query-index @semantic.db/data-source (semantic.embedding/get-active-model) search-ctx))
 
 (defenterprise update-index!
   "Enterprise implementation of semantic index updating."
@@ -28,31 +29,32 @@
   (when-not @semantic.db/data-source (semantic.db/init-db!))
   (let [documents (vec document-reducible)]
     (when (seq documents)
-      (semantic.index/upsert-index! documents))))
+      (semantic.index/upsert-index! @semantic.db/data-source (semantic.embedding/get-active-model) documents))))
 
 (defenterprise delete-from-index!
   "Enterprise implementation of semantic index deletion."
   :feature :semantic-search
   [model ids]
   (when-not @semantic.db/data-source (semantic.db/init-db!))
-  (semantic.index/delete-from-index! model ids))
+  (semantic.index/delete-from-index! @semantic.db/data-source (semantic.embedding/get-active-model) model ids))
 
 ;; TODO: add reindexing/table-swapping logic when index is detected as stale
 (defenterprise init!
   "Initialize the semantic search table and populate it with initial data."
   :feature :semantic-search
   [searchable-documents _opts]
-  (semantic.db/init-db!)
-  (semantic.index/create-index-table! {:force-reset? false})
-  (semantic.index/upsert-index! (into [] searchable-documents)))
+  (doto (semantic.db/init-db!)
+    (semantic.index/create-index-table! (semantic.embedding/get-active-model) {:force-reset? false})
+    (semantic.index/populate-index! (semantic.embedding/get-active-model) (into [] searchable-documents))))
 
 (defenterprise reindex!
   "Reindex the semantic search index."
   :feature :semantic-search
   [searchable-documents _opts]
   (when-not @semantic.db/data-source (semantic.db/init-db!))
-  (semantic.index/create-index-table! {:force-reset? false})
-  (semantic.index/upsert-index! (into [] searchable-documents)))
+  (doto @semantic.db/data-source
+    (semantic.index/create-index-table! (semantic.embedding/get-active-model) {:force-reset? false})
+    (semantic.index/populate-index! (semantic.embedding/get-active-model) (into [] searchable-documents))))
 
 ;; TODO: implement
 (defenterprise reset-tracking!
