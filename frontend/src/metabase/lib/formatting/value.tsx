ab9344384b6559f02c5233ef9e230890e0f9683d
Uladzimir Havenchyk <uladzimir@metabase.com>
2025-07-21 12:42:09 +0300

    more moment replacement

--- frontend/src/metabase/lib/formatting/value.tsx
+++ frontend/src/metabase/lib/formatting/value.tsx
@@ -128,7 +128,7 @@ function formatStringFallback(value: any, options: OptionsType = {}) {
   return value;
 }
 
-export function formatValueRaw(
+function formatValueRaw(
   value: unknown,
   options: OptionsType = {},
 ): React.ReactElement | string | number | null {
