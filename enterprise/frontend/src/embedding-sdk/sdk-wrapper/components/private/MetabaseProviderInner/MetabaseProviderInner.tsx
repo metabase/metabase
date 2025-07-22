f5d0a7da6791176f2eca48de2ed57e39ecf1fd38
Timofei <timofei@metabase.com>
2025-07-22 15:38:32 +0400

    Refactor MetabaseProviderInner

--- enterprise/frontend/src/embedding-sdk/sdk-wrapper/components/private/MetabaseProviderInner/MetabaseProviderInner.tsx
+++ enterprise/frontend/src/embedding-sdk/sdk-wrapper/components/private/MetabaseProviderInner/MetabaseProviderInner.tsx
@@ -1,7 +1,6 @@
 import type { Action, Store } from "@reduxjs/toolkit";
 import { type PropsWithChildren, useEffect, useRef } from "react";
 
-import { useMetabaseProviderStore } from "embedding-sdk/sdk-shared/hooks/use-metabase-provider-store";
 import {
   type MetabaseProviderPropsToStore,
   MetabaseProviderStore,
@@ -19,45 +18,44 @@ export const MetabaseProviderInner = ({
   store,
   props,
 }: PropsWithChildren<Props>) => {
-  const metabaseProviderStore = useMetabaseProviderStore();
-  const existingStore = metabaseProviderStore?.sdkStore;
+  const existingMetabaseProviderStoreRef = useRef(
+    MetabaseProviderStore.getInstance(),
+  );
 
-  // This makes the store stable across re-renders, but still not a singleton:
-  // we need a different store for each test or each storybook story
-  const initialized = useRef<boolean>(false);
-  const sdkStoreRef = useRef<Store<SdkStoreState, Action> | null>(null);
+  const initializedRef = useRef<boolean>(false);
+  const sdkStoreInitializedRef = useRef<boolean>(false);
 
-  const areMultipleMetabaseProvidersUsed =
-    !initialized.current && !!metabaseProviderStore;
-
-  if (areMultipleMetabaseProvidersUsed) {
-    console.warn(
-      // eslint-disable-next-line no-literal-metabase-strings -- error message
-      "Multiple instances of MetabaseProvider detected. Metabase Embedding SDK may work unexpectedly. Ensure only one instance of MetabaseProvider is rendered at a time.",
-    );
+  if (!initializedRef.current) {
+    if (existingMetabaseProviderStoreRef.current) {
+      console.warn(
+        // eslint-disable-next-line no-literal-metabase-strings -- error message
+        "Multiple instances of MetabaseProvider detected. Metabase Embedding SDK may work unexpectedly. Ensure only one instance of MetabaseProvider is rendered at a time.",
+      );
+    } else {
+      MetabaseProviderStore.initialize(props);
+      initializedRef.current = true;
+    }
   }
 
-  if (!sdkStoreRef.current) {
-    MetabaseProviderStore.initialize(props);
+  if (
+    !sdkStoreInitializedRef.current &&
+    !existingMetabaseProviderStoreRef.current &&
+    store
+  ) {
+    MetabaseProviderStore.getInstance()?.setSdkStore(store);
+    sdkStoreInitializedRef.current = true;
   }
 
   useEffect(() => {
     MetabaseProviderStore.getInstance()?.updateProps(props);
   }, [props]);
 
-  useEffect(() => {
-    initialized.current = true;
-
-    return () => {
+  useEffect(
+    () => () => {
       MetabaseProviderStore.cleanup();
-    };
-  }, []);
-
-  if (!sdkStoreRef.current && !existingStore && store) {
-    sdkStoreRef.current = store;
-
-    MetabaseProviderStore.getInstance()?.setSdkStore(sdkStoreRef.current);
-  }
+    },
+    [],
+  );
 
   return children;
 };
