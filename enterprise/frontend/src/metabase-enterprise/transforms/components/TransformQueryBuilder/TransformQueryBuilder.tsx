721394d2bc88e7121b3364eaea817b7119953486
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-07-23 15:54:43 -0400

    [skip ci]

--- enterprise/frontend/src/metabase-enterprise/transforms/components/TransformQueryBuilder/TransformQueryBuilder.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/TransformQueryBuilder/TransformQueryBuilder.tsx
@@ -5,10 +5,13 @@ import { t } from "ttag";
 import { useSetting } from "metabase/common/hooks";
 import Questions from "metabase/entities/questions";
 import { useDispatch, useSelector } from "metabase/lib/redux";
+import { checkNotNull } from "metabase/lib/types";
+import NativeQueryEditor from "metabase/query_builder/components/NativeQueryEditor/NativeQueryEditor";
 import { Notebook } from "metabase/querying/notebook/components/Notebook";
 import { getMetadata } from "metabase/selectors/metadata";
 import { Box, Button, Group } from "metabase/ui";
 import Question from "metabase-lib/v1/Question";
+import type NativeQuery from "metabase-lib/v1/queries/NativeQuery";
 import type { DatasetQuery } from "metabase-types/api";
 
 type TransformQueryBuilderProps = {
@@ -41,22 +44,42 @@ export function TransformQueryBuilder({
     setQuery(newQuestion.datasetQuery());
   };
 
+  const handleUpdateNativeQuery = async (newQuery: NativeQuery) => {
+    setQuery(newQuery.datasetQuery());
+  };
+
   return (
     <Box flex={1} h="100%" bg="white">
       <Group px="xl" pt="xl" justify="end">
         <Button onClick={() => onSave(query)}>{t`Save`}</Button>
         <Button onClick={onCancel}>{t`Cancel`}</Button>
       </Group>
-      <Notebook
-        question={question}
-        isDirty={false}
-        isRunnable={false}
-        isResultDirty={false}
-        reportTimezone={reportTimezone}
-        hasVisualizeButton={false}
-        updateQuestion={handleUpdateQuestion}
-        runQuestionQuery={() => Promise.resolve()}
-      />
+      {query.type === "query" ? (
+        <Notebook
+          question={question}
+          isDirty={false}
+          isRunnable={false}
+          isResultDirty={false}
+          reportTimezone={reportTimezone}
+          hasVisualizeButton={false}
+          updateQuestion={handleUpdateQuestion}
+          runQuestionQuery={() => Promise.resolve()}
+        />
+      ) : (
+        <NativeQueryEditor
+          question={question}
+          query={checkNotNull(question.legacyNativeQuery())}
+          viewHeight="full"
+          setDatasetQuery={handleUpdateNativeQuery}
+          enableRun={true}
+          hasEditingSidebar={false}
+          isNativeEditorOpen
+          hasParametersList={false}
+          resizable={false}
+          readOnly={false}
+          editorContext="action"
+        />
+      )}
     </Box>
   );
 }
