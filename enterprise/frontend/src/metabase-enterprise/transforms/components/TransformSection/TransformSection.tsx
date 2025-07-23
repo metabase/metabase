a254fc146279ceabb131b0c29d806945bbb47b00
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-07-23 00:01:31 -0400

    Basic page

--- enterprise/frontend/src/metabase-enterprise/transforms/components/TransformSection/TransformSection.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/TransformSection/TransformSection.tsx
@@ -2,7 +2,15 @@ import { t } from "ttag";
 
 import { NameDescriptionInput } from "metabase/metadata/components/NameDescriptionInput";
 import type { TransformSectionProps } from "metabase/plugins";
-import { Stack } from "metabase/ui";
+import {
+  Button,
+  Card,
+  Group,
+  Stack,
+  Text,
+  TextInputBlurChange,
+  Title,
+} from "metabase/ui";
 import { useGetTransformQuery } from "metabase-enterprise/api";
 import type { Transform } from "metabase-types/api";
 
@@ -22,19 +30,45 @@ type TransformSettingsProps = {
 
 function TransformSettings({ transform }: TransformSettingsProps) {
   return (
-    <Stack data-testid="transform-section" flex={1} gap={0} pb="xl">
-      <Stack bg="accent-gray-light" gap="lg" pb={12} pt="xl" px="xl">
-        <NameDescriptionInput
-          name={transform.name}
-          nameIcon="refresh_downstream"
-          nameMaxLength={254}
-          namePlaceholder={t`Give this transform a name`}
-          description=""
-          descriptionPlaceholder={t`Give this transform a description`}
-          onNameChange={() => undefined}
-          onDescriptionChange={() => undefined}
-        />
-      </Stack>
+    <Stack
+      flex={1}
+      p="xl"
+      gap="lg"
+      bg="accent-gray-light"
+      data-testid="transform-section"
+    >
+      <NameDescriptionInput
+        name={transform.name}
+        nameIcon="refresh_downstream"
+        nameMaxLength={254}
+        namePlaceholder={t`Give this transform a name`}
+        description=""
+        descriptionPlaceholder={t`Give this transform a description`}
+        onNameChange={() => undefined}
+        onDescriptionChange={() => undefined}
+      />
+      <Card p="xl" shadow="none" withBorder>
+        <Stack px="sm" gap="xl">
+          <Stack gap="sm">
+            <Title order={4}>{t`Generated table settings`}</Title>
+            <Text c="text-secondary">{t`Each transform creates a table in this database.`}</Text>
+          </Stack>
+          <TextInputBlurChange
+            label={t`What should the generated table be called in the database?`}
+            value={transform.target.table}
+            onBlurChange={() => undefined}
+          />
+          <TextInputBlurChange
+            label={t`The schema where this table should go`}
+            value={transform.target.schema}
+            onBlurChange={() => undefined}
+          />
+        </Stack>
+      </Card>
+      <Group>
+        <Button>{t`Run now`}</Button>
+        <Button>{t`Delete`}</Button>
+      </Group>
     </Stack>
   );
 }
