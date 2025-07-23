aff314ae1382c490010be88d00bff08033355bb2
Timofei <timofei@metabase.com>
2025-07-23 21:08:44 +0400

    Simplify metabase-provider-props-store.ts #2

--- enterprise/frontend/src/embedding-sdk/sdk-shared/hooks/use-metabase-provider-props-store.ts
+++ enterprise/frontend/src/embedding-sdk/sdk-shared/hooks/use-metabase-provider-props-store.ts
@@ -1,56 +1,21 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { useSyncExternalStore } from "use-sync-external-store/shim";
+import { useRef, useSyncExternalStore } from "react";
 
-import { MetabaseProviderPropsStore } from "embedding-sdk/sdk-shared/lib/metabase-provider-props-store";
-import { METABASE_PROVIDER_PROPS_STORE_INIT_EVENT_NAME } from "embedding-sdk/sdk-wrapper/config";
-import type { MetabaseProviderPropsStoreInitEvent } from "embedding-sdk/sdk-wrapper/types/sdk-store";
+import {
+  EMPTY_PROPS,
+  ensureMetabaseProviderPropsStore,
+} from "../lib/ensure-metabase-provider-props-store";
 
-export const useMetabaseProviderPropsStore = () => {
-  const [metabaseProviderPropsStore, setMetabaseProviderPropsStore] =
-    useState<MetabaseProviderPropsStore | null>(
-      MetabaseProviderPropsStore.getInstance(),
-    );
+export function useMetabaseProviderPropsStore() {
+  const storeRef =
+    useRef<ReturnType<typeof ensureMetabaseProviderPropsStore>>();
 
-  const handleEvent = useCallback((event: Event) => {
-    const customEvent =
-      event as CustomEvent<MetabaseProviderPropsStoreInitEvent>;
+  if (!storeRef.current) {
+    storeRef.current = ensureMetabaseProviderPropsStore();
+  }
 
-    if (customEvent.detail.status) {
-      setMetabaseProviderPropsStore(MetabaseProviderPropsStore.getInstance());
-    }
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener(
-      METABASE_PROVIDER_PROPS_STORE_INIT_EVENT_NAME,
-      handleEvent,
-    );
-
-    return () => {
-      document.removeEventListener(
-        METABASE_PROVIDER_PROPS_STORE_INIT_EVENT_NAME,
-        handleEvent,
-      );
-    };
-  }, [handleEvent]);
-
-  const subscribe = useMemo(() => {
-    if (!metabaseProviderPropsStore) {
-      return () => () => {};
-    }
-
-    return metabaseProviderPropsStore.subscribe;
-  }, [metabaseProviderPropsStore]);
-
-  const getSnapshot = useMemo(() => {
-    if (!metabaseProviderPropsStore) {
-      return () => null;
-    }
-
-    return metabaseProviderPropsStore.getSnapshot;
-  }, [metabaseProviderPropsStore]);
-
-  const props = useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
-
-  return metabaseProviderPropsStore && props ? props : null;
-};
+  return useSyncExternalStore(
+    storeRef.current.subscribe,
+    storeRef.current.getSnapshot,
+    () => EMPTY_PROPS,
+  );
+}
