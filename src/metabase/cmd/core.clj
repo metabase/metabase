0504b7413383b1755180e3a216767d051e109b15
Eric Normand <ericwnormand@gmail.com>
2025-07-23 11:19:59 -0500

    Merge branch 'poc-transform-table' of github.com:metabase/metabase into poc-transform-table

--- src/metabase/cmd/core.clj
+++ src/metabase/cmd/core.clj
@@ -270,6 +270,32 @@
       (log/error e "ERROR REMOVING ENCRYPTION.")
       (system-exit! 1))))
 
+(defn ^:command run-lego
+  "Run a lego from a file."
+  [filename]
+  (classloader/require 'metabase-enterprise.legos.actions)
+  (try
+    (let [data ((resolve 'metabase-enterprise.legos.actions/hippie-parse) (slurp filename))]
+      ((resolve 'metabase-enterprise.legos.actions/execute!) data)
+      (log/info "Lego executed.")
+      (system-exit! 0))
+    (catch Throwable e
+      (log/error e "ERROR EXECUTING LEGO.")
+      (system-exit! 1))))
+
+(defn ^:command run-plan
+  "Run a lego from a file."
+  [filename]
+  (classloader/require 'metabase-enterprise.legos.actions)
+  (try
+    (let [data ((resolve 'metabase-enterprise.legos.actions/hippie-parse) (slurp filename))]
+      ((resolve 'metabase-enterprise.legos.actions/execute-plan!) data)
+      (log/info "Lego plan executed.")
+      (system-exit! 0))
+    (catch Throwable e
+      (log/error e "ERROR EXECUTING LEGO PLAN.")
+      (system-exit! 1))))
+
 ;;; ------------------------------------------------ Validate Commands ----------------------------------------------
 
 (defn- arg-list-count-ok? [arg-list arg-count]
