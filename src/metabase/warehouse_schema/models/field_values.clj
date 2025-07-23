f1c7740a450e53fb0b41ac1943c5b37a3960ca03
Cam Saul <1455846+camsaul@users.noreply.github.com>
2025-07-23 09:59:33 -0700

    add-dimension-projections middleware => Lib  (#61349)
    * add-dimension-projections middleware => Lib (#39877)
    
    * Ban mt/with-column-remappings inside QP tests
    
    * Update pivot QP code to not convert to legacy
    
    * Rename middleware
    
    * Use the remap metadata provider in sandboxing tests

--- src/metabase/warehouse_schema/models/field_values.clj
+++ src/metabase/warehouse_schema/models/field_values.clj
@@ -21,7 +21,7 @@
     But they will also be automatically deleted when the Full FieldValues of the same Field got updated.
 
   There is also more written about how these are used for remapping in the docstrings
-  for [[metabase.parameters.chain-filter]] and [[metabase.query-processor.middleware.add-dimension-projections]]."
+  for [[metabase.parameters.chain-filter]] and [[metabase.query-processor.middleware.add-remaps]]."
   (:require
    [clojure.string :as str]
    [java-time.api :as t]
