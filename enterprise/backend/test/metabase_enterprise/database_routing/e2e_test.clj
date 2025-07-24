15e09f6e41e22bbd56af30c41b1cb36e0974f2c4
Riley Thompson <rileythompson99@gmail.com>
2025-07-24 01:34:42 -0400

    dataset name multimethod

--- enterprise/backend/test/metabase_enterprise/database_routing/e2e_test.clj
+++ enterprise/backend/test/metabase_enterprise/database_routing/e2e_test.clj
@@ -4,6 +4,7 @@
    [clojure.test :refer [deftest is testing]]
    [metabase-enterprise.test :as met]
    [metabase.app-db.core :as mdb]
+   [metabase.driver :as driver]
    [metabase.driver.settings :as driver.settings]
    [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
    [metabase.query-processor :as qp]
@@ -204,6 +205,34 @@
     (t2/update! :model/Database :id (:id child)
                 {:details (assoc (:details child) :destination-database true)})))
 
+(defmulti router-dataset-name
+  "Name for router dataset"
+  {:arglists '([driver])}
+  tx/dispatch-on-driver-with-test-extensions
+  :hierarchy #'driver/hierarchy)
+
+(defmethod router-dataset-name :default
+  [_driver]
+  "db-router-data")
+
+(defmethod router-dataset-name :redshift
+  [_driver]
+  "db-routing-data")
+
+(defmulti routed-dataset-name
+  "Name for routed dataset"
+  {:arglists '([driver])}
+  tx/dispatch-on-driver-with-test-extensions
+  :hierarchy #'driver/hierarchy)
+
+(defmethod routed-dataset-name :default
+  [_driver]
+  "db-routed-data")
+
+(defmethod routed-dataset-name :redshift
+  [_driver]
+  "db-routing-data")
+
 (deftest db-routing-e2e-test
   ;; todo: this is to quickly get tests against all drivers right now. We probably want to make a
   ;; few more nice helpers, and remove some of the above tests which are duplicative of the below.
@@ -211,14 +240,14 @@
     (mt/with-premium-features #{:database-routing}
       (binding [tx/*use-routing-dataset* true
                 tx/*use-routing-details* true]
-        (mt/dataset (mt/dataset-definition "db-routing-data"
+        (mt/dataset (mt/dataset-definition (routed-dataset-name driver/*driver*)
                                            ["t"
                                             [{:field-name "f", :base-type :type/Text}]
                                             [["routed-foo"]
                                              ["routed-bar"]]])
           (let [routed (mt/db)]
             (binding [tx/*use-routing-details* false]
-              (mt/dataset (mt/dataset-definition "db-routing-data"
+              (mt/dataset (mt/dataset-definition (router-dataset-name driver/*driver*)
                                                  ["t"
                                                   [{:field-name "f", :base-type :type/Text}]
                                                   [["original-foo"]
