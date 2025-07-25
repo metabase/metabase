b7f05e0712fd4d57b9ee5fea795d90d21eefe2e6
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-07-25 09:59:23 -0400

    Move notebook

--- enterprise/frontend/src/metabase-enterprise/transforms/components/TransformQueryBuilder/TransformNotebook/TransformNotebook.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/TransformQueryBuilder/TransformNotebook/TransformNotebook.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { ResizableBox } from "react-resizable";
+
 import { useSetting } from "metabase/common/hooks";
 import { Notebook } from "metabase/querying/notebook/components/Notebook";
+import { Flex } from "metabase/ui";
 import type Question from "metabase-lib/v1/Question";
+
+import { ResizableBoxHandle } from "./ResizableBoxHandle";
+import S from "./TransformNotebook.module.css";
+
+const NOTEBOOK_HEIGHT = 500;
 
 type TransformNotebookProps = {
   question: Question;
@@ -11,6 +20,7 @@ export function TransformNotebook({
   question,
   onChange,
 }: TransformNotebookProps) {
+  const [isResizing, setIsResizing] = useState(false);
   const reportTimezone = useSetting("report-timezone-long");
 
   const handleChange = (newQuestion: Question) => {
@@ -19,15 +29,27 @@ export function TransformNotebook({
   };
 
   return (
-    <Notebook
-      question={question}
-      isDirty={false}
-      isRunnable={false}
-      isResultDirty={false}
-      reportTimezone={reportTimezone}
-      hasVisualizeButton={false}
-      updateQuestion={handleChange}
-      runQuestionQuery={() => Promise.resolve()}
-    />
+    <ResizableBox
+      className={S.root}
+      axis="y"
+      height={NOTEBOOK_HEIGHT}
+      handle={<ResizableBoxHandle />}
+      resizeHandles={["s"]}
+      onResizeStart={() => setIsResizing(true)}
+      onResizeStop={() => setIsResizing(false)}
+    >
+      <Flex w="100%" style={{ overflowY: isResizing ? "hidden" : "auto" }}>
+        <Notebook
+          question={question}
+          isDirty={false}
+          isRunnable={false}
+          isResultDirty={false}
+          reportTimezone={reportTimezone}
+          hasVisualizeButton={false}
+          updateQuestion={handleChange}
+          runQuestionQuery={() => Promise.resolve()}
+        />
+      </Flex>
+    </ResizableBox>
   );
 }
