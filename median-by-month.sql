b8418c7ca065fd9fbdeb693f831fa0bd524a7a12
William Zimrin <william@metabase.com>
2025-07-25 12:15:27 -0500

    remove more stuff

--- median-by-month.sql
+++
@@ -1,5 +0,0 @@
-SELECT DATE_TRUNC('month', created_at) AS created_at,
-       count(*) as count,
-       extract(days from avg(time_to_close)) as days_to_close
-FROM time_to_close_bugs
-GROUP BY DATE_TRUNC('month', created_at)
