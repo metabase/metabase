78b23785a146f0fefc54f4da358668c45165c104
Phoomparin Mano <phoomparin@gmail.com>
2025-07-25 21:20:34 +0700

    cleanup card ui

--- frontend/src/metabase/admin/settings/components/EmbeddingSettings/EmbeddingSdkSettings/EmbeddingSdkSettings.tsx
+++ frontend/src/metabase/admin/settings/components/EmbeddingSettings/EmbeddingSdkSettings/EmbeddingSdkSettings.tsx
@@ -1,10 +1,7 @@
 import { match } from "ts-pattern";
-import { c, jt, t } from "ttag";
+import { jt, t } from "ttag";
 
-import {
-  SettingsPageWrapper,
-  SettingsSection,
-} from "metabase/admin/components/SettingsSection";
+import { SettingsPageWrapper } from "metabase/admin/components/SettingsSection";
 import { UpsellDevInstances } from "metabase/admin/upsells";
 import { UpsellSdkLink } from "metabase/admin/upsells/UpsellSdkLink";
 import ExternalLink from "metabase/common/components/ExternalLink";
@@ -20,9 +17,11 @@ import {
   Badge,
   Box,
   Button,
+  Divider,
   Flex,
   Group,
   Icon,
+  Popover,
   Text,
 } from "metabase/ui";
 
@@ -114,124 +113,176 @@ export function EmbeddingSdkSettings() {
     <SettingsPageWrapper title={t`Embedded Analytics SDK`}>
       <UpsellDevInstances location="embedding-page" />
 
-      <SettingsSection>
-        <Box>
-          <Group align="flex-start" gap="lg">
-            <Flex direction="column" gap="md">
-              <Group align="center" justify="space-between" w="100%" gap="xl">
-                <Group gap="sm">
-                  <EmbeddingToggle
-                    label={t`Embedding SDK for React`}
-                    settingKey="enable-embedding-sdk"
-                    labelPosition="right"
-                  />
-                </Group>
+      <Box
+        bd="1px solid var(--mb-color-border)"
+        bg="var(--mb-color-background)"
+        style={{ borderRadius: 8 }}
+      >
+        <Box p="lg">
+          <Group mb="md">
+            <Text size="lg" fw={600} c="text-dark">
+              {t`Embedded analytics SDK for React`}
+            </Text>
+          </Group>
 
-                <Text c="text-medium">
-                  {c(
-                    "{0} is the link to quickstart, {1} is the link to documentation",
-                  ).jt`
-                    Check out the ${(
-                      <ExternalLink
-                        key="quickstart-sdk"
-                        href={quickStartUrl}
-                      >{t`quickstart`}</ExternalLink>
-                    )} and
-                    ${(
-                      <ExternalLink
-                        key="documentation-sdk"
-                        href={documentationUrl}
-                      >{t`documentation`}</ExternalLink>
-                    )}.`}
-                </Text>
-              </Group>
+          <Group gap="sm" align="center" justify="space-between" w="100%">
+            <EmbeddingToggle
+              label={t`Enabled`}
+              settingKey="enable-embedding-sdk"
+              labelPosition="right"
+            />
 
-              {isSimpleEmbedFeatureEnabled && (
-                <Group align="center" justify="space-between" w="100%" gap="xl">
-                  <Group gap="sm">
-                    <EmbeddingToggle
-                      label={t`Simple Embedding`}
-                      labelPosition="right"
-                      settingKey="enable-embedding-simple"
-                    />
+            <Group gap="md">
+              <Button
+                size="compact-sm"
+                variant="outline"
+                component={ExternalLink}
+                href={quickStartUrl}
+                rightSection={<Icon size={12} name="external" />}
+                fz="sm"
+              >
+                {t`Quick start`}
+              </Button>
 
-                    <Badge size="sm">{t`Beta`}</Badge>
-                  </Group>
-
-                  {/** TODO: update the quickstart and documentation link to link to simple embedding docs */}
-                  <Text c="text-medium">
-                    {c(
-                      "{0} is the link to quickstart, {1} is the link to documentation",
-                    ).jt`
-                    Check out the ${(
-                      <ExternalLink
-                        key="quickstart-simple"
-                        href={quickStartUrl}
-                      >{t`quickstart`}</ExternalLink>
-                    )} and
-                    ${(
-                      <ExternalLink
-                        key="documentation-simple"
-                        href={documentationUrl}
-                      >{t`documentation`}</ExternalLink>
-                    )}.`}
-                  </Text>
-                </Group>
-              )}
-            </Flex>
+              <Button
+                size="compact-sm"
+                variant="outline"
+                component={ExternalLink}
+                href={documentationUrl}
+                rightSection={<Icon size={12} name="external" />}
+                fz="sm"
+              >
+                {t`Documentation`}
+              </Button>
+            </Group>
           </Group>
         </Box>
+
+        <Divider />
 
         <Alert
           data-testid="sdk-settings-alert-info"
           icon={
             <Icon color="var(--mb-color-text-secondary)" name="info_filled" />
           }
-          bg="var(--mb-color-background-info)"
-          style={{
-            borderColor: "var(--mb-color-border)",
-          }}
+          px="xl"
+          bg="none"
+          bd="none"
           variant="outline"
-          px="lg"
-          py="md"
-          maw={620}
         >
-          <Text size="sm">{apiKeyBannerText}</Text>
+          <Text size="sm" c="text-medium">
+            {apiKeyBannerText}
+          </Text>
         </Alert>
+      </Box>
 
-        <Box>
-          <AdminSettingInput
-            name="embedding-app-origins-sdk"
-            title={t`Authorized Origins`}
-            placeholder="https://*.example.com"
-            description={
-              isEmbeddingAvailable
-                ? t`Enter the origins for the websites or apps where you want to allow embedding, separated by a space. These origins apply to both SDK and simple embedding. Localhost is automatically included. Changes will take effect within one minute.`
-                : jt`Try out the SDK on localhost. To enable other sites, ${(<UpsellSdkLink />)} and enter the origins for the websites or apps where you want to allow SDK and simple embedding.`
-            }
-            inputType="text"
-            disabled={!canEditSdkOrigins}
-          />
+      {isSimpleEmbedFeatureEnabled && (
+        <Box
+          bg="var(--mb-color-background)"
+          p="lg"
+          bd="1px solid var(--mb-color-border)"
+          style={{ borderRadius: 8 }}
+        >
+          <Flex direction="column" gap="md">
+            <Group gap="sm">
+              <Text size="lg" fw={600} c="text-dark">
+                {t`Simple SDK Embedding`}
+              </Text>
+
+              <Badge size="sm">{t`Beta`}</Badge>
+            </Group>
+
+            <Group gap="sm" align="center" justify="space-between" w="100%">
+              <EmbeddingToggle
+                label={t`Enabled`}
+                labelPosition="right"
+                settingKey="enable-embedding-simple"
+              />
+
+              <Group gap="md">
+                <Button
+                  size="compact-sm"
+                  variant="outline"
+                  component={ExternalLink}
+                  href={quickStartUrl}
+                  rightSection={<Icon size={12} name="external" />}
+                  fz="sm"
+                >
+                  {t`Quick start`}
+                </Button>
+
+                <Button
+                  size="compact-sm"
+                  variant="outline"
+                  component={ExternalLink}
+                  href={documentationUrl}
+                  rightSection={<Icon size={12} name="external" />}
+                  fz="sm"
+                >
+                  {t`Documentation`}
+                </Button>
+              </Group>
+            </Group>
+          </Flex>
         </Box>
+      )}
 
-        {isEmbeddingAvailable && isHosted && (
-          <Box>
-            <SettingHeader
-              id="version-pinning"
-              title={t`Version pinning`}
-              description={t`Metabase Cloud instances are automatically upgraded to new releases. SDK packages are strictly compatible with specific version of Metabase. You can request to pin your Metabase to a major version and upgrade your Metabase and SDK dependency in a coordinated fashion.`}
-            />
-            <Button
-              size="compact-md"
-              variant="outline"
-              leftSection={<Icon size={12} name="mail" aria-hidden />}
-              component={ExternalLink}
-              fz="0.75rem"
-              href="mailto:help@metabase.com"
-            >{t`Request version pinning`}</Button>
-          </Box>
-        )}
-      </SettingsSection>
+      <Box
+        bg="var(--mb-color-background)"
+        p="lg"
+        bd="1px solid var(--mb-color-border)"
+        style={{ borderRadius: 8 }}
+      >
+        <Group gap="xs" mb="md">
+          <Text size="lg" fw={600} c="text-dark">
+            {t`Cross-Origin Resource Sharing (CORS)`}
+          </Text>
+          <Popover width={300} position="top" withArrow shadow="md">
+            <Popover.Target>
+              <Icon
+                size={16}
+                name="info"
+                c="text-medium"
+                style={{ cursor: "pointer" }}
+              />
+            </Popover.Target>
+            <Popover.Dropdown>
+              <Text size="sm">
+                {isEmbeddingAvailable
+                  ? t`Enter the origins for the websites or apps where you want to allow embedding, separated by a space. These origins apply to both SDK and simple embedding. Localhost is automatically included. Changes will take effect within one minute.`
+                  : jt`Try out the SDK on localhost. To enable other sites, ${(<UpsellSdkLink />)} and enter the origins for the websites or apps where you want to allow SDK and simple embedding.`}
+              </Text>
+            </Popover.Dropdown>
+          </Popover>
+        </Group>
+        <Text c="text-medium" mb="md">
+          {t`Enter the origins for the websites or apps where you want to allow SDK embedding.`}
+        </Text>
+        <AdminSettingInput
+          name="embedding-app-origins-sdk"
+          placeholder="https://*.example.com"
+          inputType="text"
+          disabled={!canEditSdkOrigins}
+        />
+      </Box>
+
+      {isEmbeddingAvailable && isHosted && (
+        <Box>
+          <SettingHeader
+            id="version-pinning"
+            title={t`Version pinning`}
+            description={t`Metabase Cloud instances are automatically upgraded to new releases. SDK packages are strictly compatible with specific version of Metabase. You can request to pin your Metabase to a major version and upgrade your Metabase and SDK dependency in a coordinated fashion.`}
+          />
+          <Button
+            size="compact-md"
+            variant="outline"
+            leftSection={<Icon size={12} name="mail" aria-hidden />}
+            component={ExternalLink}
+            fz="0.75rem"
+            href="mailto:help@metabase.com"
+          >{t`Request version pinning`}</Button>
+        </Box>
+      )}
     </SettingsPageWrapper>
   );
 }
