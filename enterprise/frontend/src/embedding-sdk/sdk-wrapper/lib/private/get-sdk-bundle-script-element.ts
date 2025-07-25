46c8411a0c25d5ec6925e0304d7eb4fa8a3245f7
Timofei <timofei@metabase.com>
2025-07-25 15:08:54 +0400

    Add e2e for Hosted Bundle

--- enterprise/frontend/src/embedding-sdk/sdk-wrapper/lib/private/get-sdk-bundle-script-element.ts
+++ enterprise/frontend/src/embedding-sdk/sdk-wrapper/lib/private/get-sdk-bundle-script-element.ts
@@ -1,6 +1,6 @@
 import { SDK_BUNDLE_SCRIPT_DATA_ATTRIBUTE_DASH_CASED } from "embedding-sdk/sdk-wrapper/config";
 
-export function getSdkBundleScriptElement() {
+export function getSdkBundleScriptElement(): HTMLScriptElement | null {
   return document.querySelector(
     `[data-${SDK_BUNDLE_SCRIPT_DATA_ATTRIBUTE_DASH_CASED}="true"]`,
   );
