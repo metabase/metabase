a0499f634e106a60d21db050b2800258b756bedf
Timofei <timofei@metabase.com>
2025-07-26 15:17:35 +0400

    Use Mantine styles with CSS layer for the SDK

--- enterprise/frontend/src/embedding-sdk/index.ts
+++ enterprise/frontend/src/embedding-sdk/index.ts
@@ -6,8 +6,8 @@ EMBEDDING_SDK_CONFIG.isEmbeddingSdk = true;
 
 // Mantine styles need to be imported before any of our components so that our styles win over
 // the default mantine styles
-import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
+import "@mantine/core/styles.layer.css";
+import "@mantine/dates/styles.layer.css";
 
 import "metabase/lib/dayjs";
 
