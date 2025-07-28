f34863625474107b5295e8e981546696283996ef
Timofei <timofei@metabase.com>
2025-07-28 20:21:48 +0400

    Handle store initialization issues

--- enterprise/frontend/src/embedding-sdk/sdk-wrapper/components/private/MetabaseProviderInner/MetabaseProviderInner.tsx
+++ enterprise/frontend/src/embedding-sdk/sdk-wrapper/components/private/MetabaseProviderInner/MetabaseProviderInner.tsx
@@ -1,11 +1,11 @@
-import { type ReactNode, useEffect, useState } from "react";
+import { type ReactNode, useEffect } from "react";
 
 import {
   type MetabaseProviderPropsStoreExternalProps,
   type MetabaseProviderPropsStoreInternalProps,
   ensureMetabaseProviderPropsStore,
 } from "embedding-sdk/sdk-shared/lib/ensure-metabase-provider-props-store";
-import { getWindow } from "embedding-sdk/sdk-shared/lib/get-window";
+import { useInitializeMetabaseProviderPropsStore } from "embedding-sdk/sdk-wrapper/hooks/private/use-initialize-metabase-provider-props-store";
 
 type Props = {
   className?: string;
@@ -14,58 +14,11 @@ type Props = {
   children: (state: { initialized: boolean }) => ReactNode;
 };
 
-const incrementProvidersCount = () => {
-  window.METABASE_PROVIDERS_COUNT = (window.METABASE_PROVIDERS_COUNT ?? 0) + 1;
-};
-
-const decrementProvidersCount = () => {
-  window.METABASE_PROVIDERS_COUNT = Math.max(
-    0,
-    (window.METABASE_PROVIDERS_COUNT ?? 1) - 1,
-  );
-};
-
-const shouldInitialize = () => {
-  return (getWindow()?.METABASE_PROVIDERS_COUNT ?? 0) <= 1;
-};
-
-const shouldCleanup = () => {
-  return (getWindow()?.METABASE_PROVIDERS_COUNT ?? 0) === 0;
-};
-
 export function MetabaseProviderInner({ reduxStore, props, children }: Props) {
-  const [initialized, setInitialized] = useState(false);
-
-  useEffect(() => {
-    incrementProvidersCount();
-
-    if (shouldInitialize()) {
-      ensureMetabaseProviderPropsStore(props);
-    } else {
-      console.warn(
-        // eslint-disable-next-line no-literal-metabase-strings -- Warning message
-        "Multiple instances of MetabaseProvider detected. Metabase Embedding SDK may work unexpectedly. Ensure only one instance of MetabaseProvider is rendered at a time.",
-      );
-    }
-
-    return () => {
-      decrementProvidersCount();
-
-      if (shouldCleanup()) {
-        ensureMetabaseProviderPropsStore().cleanup();
-      }
-    };
-    // eslint-disable-next-line -- Run on mount only
-  }, []);
-
-  useEffect(() => {
-    if (reduxStore && !initialized) {
-      ensureMetabaseProviderPropsStore().setProps({ reduxStore });
-
-      // Now the `MetabaseProviderPropsStore` store is initialized and the `reduxStore` prop is set in it
-      setInitialized(true);
-    }
-  }, [reduxStore, initialized]);
+  const { initialized } = useInitializeMetabaseProviderPropsStore(
+    props,
+    reduxStore,
+  );
 
   useEffect(() => {
     ensureMetabaseProviderPropsStore().setProps(props);
