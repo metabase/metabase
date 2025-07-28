29f0243b56ff95bf3195a7f2575f61ac7bcdd342
Timofei <timofei@metabase.com>
2025-07-28 15:51:15 +0400

    Refactor

--- enterprise/frontend/src/embedding-sdk/components/private/PublicComponentStylesWrapper.tsx
+++ enterprise/frontend/src/embedding-sdk/components/private/PublicComponentStylesWrapper.tsx
@@ -63,20 +63,22 @@ export const PublicComponentStylesWrapper = forwardRef<
  */
 // note: if we move this to  css.module, remember to add :global to .mb-wrapper
 export const SCOPED_CSS_RESET = css`
-  @layer embedding-sdk-reset {
-    :where(.mb-wrapper) *:where(button) {
-      border: 0;
-      background-color: transparent;
-    }
+  @layer embedding-sdk {
+    @layer reset {
+      :where(.mb-wrapper) *:where(button) {
+        border: 0;
+        background-color: transparent;
+      }
 
-    // fonts.styled.ts has a reset for list padding and margin in the main app, so we need to do it here
-    :where(.mb-wrapper) *:where(ul) {
-      padding: 0;
-      margin: 0;
-    }
+      // fonts.styled.ts has a reset for list padding and margin in the main app, so we need to do it here
+      :where(.mb-wrapper) *:where(ul) {
+        padding: 0;
+        margin: 0;
+      }
 
-    :where(.mb-wrapper) *:where(svg) {
-      display: inline;
+      :where(.mb-wrapper) *:where(svg) {
+        display: inline;
+      }
     }
   }
 `;
