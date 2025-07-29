c952d7b908d736e76725196f817acd32cf37a6b6
Luiz Arakaki <luiz.arakaki@metabase.com>
2025-07-29 13:53:02 -0300

    linter

--- enterprise/frontend/src/metabase-enterprise/sandboxes/components/EditSandboxingModal/EditSandboxingModal.tsx
+++ enterprise/frontend/src/metabase-enterprise/sandboxes/components/EditSandboxingModal/EditSandboxingModal.tsx
@@ -400,9 +400,8 @@ const TargetName = ({ policy, policyTable, target }: TargetNameProps) => {
     ) {
       return (
         <span>
-          {c(
-            "{0} is a name of a variable",
-          ).jt`${(<strong>{target[1][1]}</strong>)} variable`}
+          {c("{0} is a name of a variable")
+            .jt`${(<strong>{target[1][1]}</strong>)} variable`}
         </span>
       );
     } else if (target[0] === "dimension") {
@@ -440,9 +439,8 @@ const TargetName = ({ policy, policyTable, target }: TargetNameProps) => {
             const columnInfo = Lib.displayInfo(query, stageIndex, column);
             return (
               <span>
-                {c(
-                  "{0} is a name of a field",
-                ).jt`${(<strong>{columnInfo.displayName}</strong>)} field`}
+                {c("{0} is a name of a field")
+                  .jt`${(<strong>{columnInfo.displayName}</strong>)} field`}
               </span>
             );
           }}
