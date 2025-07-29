ad58a25fc6175f301cf1c0b0cfc09460e7f4f2c2
Edward Paget <ed.paget@gmail.com>
2025-07-29 16:30:49 -0500

    feat: associate cards with reports in create

--- enterprise/backend/src/metabase_enterprise/reports/api/report.clj
+++ enterprise/backend/src/metabase_enterprise/reports/api/report.clj
@@ -6,7 +6,8 @@
    [metabase.api.routes.common :refer [+auth]]
    [metabase.collections.models.collection :as collection]
    [metabase.models.interface :as mi]
-   [metabase.util.log :as log]
+   [metabase.util.i18n :refer [tru]]
+
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
 
@@ -23,6 +24,30 @@
    :join     [[(t2/table-name :model/ReportVersion) :ver] [:= :report.id :ver.report_id]]
    :order-by [[:report.id :asc]]
    :where    where-clause})
+
+(defn- validate-cards-for-report
+  "Validates that all provided card-ids exist and have type :in_report.
+   Returns the card records if all are valid, throws exception otherwise."
+  [card-ids]
+  (when (seq card-ids)
+    (let [cards (t2/select :model/Card :id [:in card-ids])]
+      ;; Check all cards were found
+      (when (not= (count cards) (count card-ids))
+        (let [found-ids (set (map :id cards))
+              missing-ids (remove found-ids card-ids)]
+          (throw (ex-info (tru "The following card IDs do not exist: {0}. Please verify the card IDs are correct and the cards have not been deleted." (vec missing-ids))
+                          {:status-code 404
+                           :error-type :cards-not-found
+                           :missing-card-ids missing-ids}))))
+      ;; Check all cards have type :in_report
+      (let [invalid-cards (remove #(= :in_report (keyword (:type %))) cards)]
+        (when (seq invalid-cards)
+          (throw (ex-info (tru "The following cards cannot be used in reports because they have the wrong type: {0}. Only cards with type ''in_report'' can be associated with reports. Please change the card type to ''in_report'' or use different cards."
+                               (mapv #(str "card " (:id %) " (type: " (:type %) ")") invalid-cards))
+                          {:status-code 400
+                           :error-type :invalid-card-type
+                           :invalid-cards (mapv #(select-keys % [:id :type]) invalid-cards)}))))
+      cards)))
 
 (defn get-report [id version]
   (api/check-404
@@ -44,14 +69,17 @@
   "Create a new `Report`."
   [_route-params
    _query-params
-   {:keys [name document version created_at updated_at collection_id]}
+   {:keys [name document version created_at updated_at collection_id card-ids]}
    :- [:map
        [:name :string]
        [:document :string]
-       [:collection_id {:optional true} [:maybe ms/PositiveInt]]]]
+       [:collection_id {:optional true} [:maybe ms/PositiveInt]]
+       [:card-ids {:optional true} [:vector ms/PositiveInt]]]]
   (api/check-superuser)
   (get-report (t2/with-transaction [conn]
-                (let [report-id (t2/insert-returning-pk! :conn conn :model/Report {:name name
+                ;; Validate cards first if provided
+                (let [validated-cards (validate-cards-for-report card-ids)
+                      report-id (t2/insert-returning-pk! :conn conn :model/Report {:name name
                                                                                    :collection_id collection_id})
                       report-version-id (t2/insert-returning-pk! :conn conn :model/ReportVersion
                                                                  {:report_id          report-id
@@ -62,6 +90,10 @@
                       _ (t2/select-one :conn conn :model/Report :id report-id)
                       _ (t2/select-one :conn conn :model/ReportVersion :report_id report-id)
                       _ (t2/update! :conn conn :model/Report report-id {:current_version_id report-version-id})]
+                  ;; Update cards with the new report-id if cards were validated
+                  (when validated-cards
+                    (doseq [card validated-cards]
+                      (t2/update! :conn conn :model/Card (:id card) {:report_document_id report-id})))
                   report-id)) nil))
 
 (api.macros/defendpoint :get "/:report-id"
@@ -78,12 +110,16 @@
    body :- [:map
             [:name {:optional true} :string]
             [:document {:optional true} :string]
-            [:collection_id {:optional true} [:maybe ms/PositiveInt]]]]
+            [:collection_id {:optional true} [:maybe ms/PositiveInt]]
+            [:card-ids {:optional true} [:vector ms/PositiveInt]]]]
   (api/check-superuser)
   (let [existing-report (get-report report-id nil)]
     (get-report
      (t2/with-transaction [conn]
-       (let [new-report-version-id (when (:document body)
+       ;; Validate cards first if provided
+       (let [validated-cards (when (:card-ids body)
+                               (validate-cards-for-report (:card-ids body)))
+             new-report-version-id (when (:document body)
                                      (t2/insert-returning-pk! :conn conn :model/ReportVersion
                                                               {:report_id          report-id
                                                                :document           (:document body)
@@ -99,6 +135,13 @@
                                                                :current_version_id (or new-report-version-id
                                                                                        :current_version_id)
                                                                :updated_at         (mi/now)})]
+         ;; Update cards with the report-id if cards were validated
+         (when validated-cards
+           ;; Clear existing card associations for this report
+           (t2/update! :conn conn :model/Card {:report_document_id report-id} {:report_document_id nil})
+           ;; Set new card associations
+           (doseq [card validated-cards]
+             (t2/update! :conn conn :model/Card (:id card) {:report_document_id report-id})))
          report-id)) nil)))
 
 (api.macros/defendpoint :get "/:report-id/versions"
--- enterprise/backend/test/metabase_enterprise/reports/api/report_test.clj
+++ enterprise/backend/test/metabase_enterprise/reports/api/report_test.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.set :as set]
    [clojure.test :refer :all]
+   [metabase-enterprise.reports.api.report :as report-api]
    [metabase.test :as mt]
    [toucan2.core :as t2]))
 
@@ -21,7 +22,102 @@
           (is (partial=
                {:document           "Doc 1"
                 :version_identifier 1}
-               report-doc-row)))))))
+               report-doc-row))))
+
+      (testing "should reject invalid card-ids"
+        (testing "non-integer values"
+          (mt/user-http-request :crowberto
+                                :post 400 "ee/report/"
+                                {:name "Report"
+                                 :document "Doc"
+                                 :card-ids ["invalid"]}))
+
+        (testing "negative integers"
+          (mt/user-http-request :crowberto
+                                :post 400 "ee/report/"
+                                {:name "Report"
+                                 :document "Doc"
+                                 :card-ids [-1 2]}))
+
+        (testing "zero values"
+          (mt/user-http-request :crowberto
+                                :post 400 "ee/report/"
+                                {:name "Report"
+                                 :document "Doc"
+                                 :card-ids [0 1]})))
+
+      (testing "card association functionality"
+        (mt/with-model-cleanup [:model/Card]
+          (testing "should associate valid :in_report cards with new report"
+            (mt/with-temp [:model/Card card1 {:name "Card 1"
+                                              :type :in_report
+                                              :dataset_query (mt/mbql-query venues)}
+                           :model/Card card2 {:name "Card 2"
+                                              :type :in_report
+                                              :dataset_query (mt/mbql-query venues)}]
+              (let [result (mt/user-http-request :crowberto
+                                                 :post 200 "ee/report/"
+                                                 {:name "Report with Valid Cards"
+                                                  :document "Doc with valid cards"
+                                                  :card-ids [(:id card1) (:id card2)]})
+                    updated-card1 (t2/select-one :model/Card :id (:id card1))
+                    updated-card2 (t2/select-one :model/Card :id (:id card2))]
+                (is (pos? (:id result)))
+                (is (= (:id result) (:report_document_id updated-card1)))
+                (is (= (:id result) (:report_document_id updated-card2))))))
+
+          (testing "should reject non-existent card-ids"
+            (let [non-existent-id 999999]
+              (mt/user-http-request :crowberto
+                                    :post 404 "ee/report/"
+                                    {:name "Report with Missing Cards"
+                                     :document "Doc"
+                                     :card-ids [non-existent-id]})))
+
+          (testing "should reject cards with wrong type"
+            (mt/with-temp [:model/Card wrong-type-card {:name "Wrong Type Card"
+                                                        :type :question
+                                                        :dataset_query (mt/mbql-query venues)}]
+              (is (=? {:message #"The following cards cannot be used in reports because they have the wrong type:.*"}
+                      (mt/user-http-request :crowberto
+                                            :post 400 "ee/report/"
+                                            {:name "Report with Wrong Type Cards"
+                                             :document "Doc"
+                                             :card-ids [(:id wrong-type-card)]})))))
+
+          (testing "should error on nil card-ids"
+            (mt/user-http-request :crowberto
+                                  :post 400 "ee/report/"
+                                  {:name "Report with Nil Cards"
+                                   :document "Doc with nil cards"
+                                   :card-ids nil}))
+
+          (testing "should handle empty card-ids gracefully"
+            (let [result (mt/user-http-request :crowberto
+                                               :post 200 "ee/report/"
+                                               {:name "Report with Empty Cards"
+                                                :document "Doc with empty cards"
+                                                :card-ids []})]
+              (is (pos? (:id result)))))
+
+          (testing "transaction rollback - when card update fails, report creation should be rolled back"
+            ;; This test ensures that if card updates fail, the entire transaction is rolled back
+            (mt/with-temp [:model/Card card {:name "Test Card"
+                                             :type :in_report
+                                             :dataset_query (mt/mbql-query venues)}]
+              ;; Create a scenario where card update might fail by using a mixed set of valid and invalid cards
+              (let [invalid-card-id 999999
+                    initial-reports-count (t2/count :model/Report)]
+                (mt/user-http-request :crowberto
+                                      :post 404 "ee/report/"
+                                      {:name "Report That Should Rollback"
+                                       :document "Doc"
+                                       :card-ids [(:id card) invalid-card-id]})
+                ;; Verify no new report was created due to rollback
+                (is (= initial-reports-count (t2/count :model/Report)))
+                ;; Verify the valid card wasn't updated
+                (let [unchanged-card (t2/select-one :model/Card :id (:id card))]
+                  (is (nil? (:report_document_id unchanged-card))))))))))))
 
 (deftest put-report-test
   (testing "PUT /api/ee/report/id"
@@ -37,11 +133,90 @@
                                            :put 200 (format "ee/report/%s" report-id) {:name "Report 2" :document "Doc 2"})]
           (is (partial= {:name     "Report 2"
                          :document "Doc 2"
-                         :version  2} result))
-          result))))
-  (testing "should return 404 for non-existent report"
-    (mt/user-http-request :crowberto
-                          :put 404 "ee/report/99999" {:name "Non-existent Report" :document "Doc"})))
+                         :version 2} result))))
+
+      (testing "card association functionality"
+        (mt/with-model-cleanup [:model/Card]
+          (testing "should associate valid :in_report cards with existing report"
+            (mt/with-temp [:model/Card card1 {:name "Card 1"
+                                              :type :in_report
+                                              :dataset_query (mt/mbql-query venues)}
+                           :model/Card card2 {:name "Card 2"
+                                              :type :in_report
+                                              :dataset_query (mt/mbql-query venues)}]
+              (let [result (mt/user-http-request :crowberto
+                                                 :put 200 (format "ee/report/%s" report-id)
+                                                 {:name "Updated Report with Cards"
+                                                  :document "Updated doc with cards"
+                                                  :card-ids [(:id card1) (:id card2)]})
+                    updated-card1 (t2/select-one :model/Card :id (:id card1))
+                    updated-card2 (t2/select-one :model/Card :id (:id card2))]
+                (is (= report-id (:id result)))
+                (is (= report-id (:report_document_id updated-card1)))
+                (is (= report-id (:report_document_id updated-card2))))))
+
+          (testing "should clear existing card associations when updating with new cards"
+            (mt/with-temp [:model/Card old-card {:name "Old Card"
+                                                 :type :in_report
+                                                 :report_document_id report-id
+                                                 :dataset_query (mt/mbql-query venues)}
+                           :model/Card new-card {:name "New Card"
+                                                 :type :in_report
+                                                 :dataset_query (mt/mbql-query venues)}]
+              (mt/user-http-request :crowberto
+                                    :put 200 (format "ee/report/%s" report-id)
+                                    {:card-ids [(:id new-card)]})
+              (let [updated-old-card (t2/select-one :model/Card :id (:id old-card))
+                    updated-new-card (t2/select-one :model/Card :id (:id new-card))]
+                (is (nil? (:report_document_id updated-old-card)))
+                (is (= report-id (:report_document_id updated-new-card))))))
+
+          (testing "should reject non-existent card-ids"
+            (let [non-existent-id 999999]
+              (is (=? {:message #"The following card IDs do not exist.*"}
+                      (mt/user-http-request :crowberto
+                                            :put 404 (format "ee/report/%s" report-id)
+                                            {:name "Report with Missing Cards"
+                                             :document "Doc"
+                                             :card-ids [non-existent-id]})))))
+
+          (testing "should reject cards with wrong type"
+            (mt/with-temp [:model/Card wrong-type-card {:name "Wrong Type Card"
+                                                        :type :question
+                                                        :dataset_query (mt/mbql-query venues)}]
+              (is (=? {:message #"The following cards cannot be used in reports.*"}
+                      (mt/user-http-request :crowberto
+                                            :put 400 (format "ee/report/%s" report-id)
+                                            {:name "Report with Wrong Type Cards"
+                                             :document "Doc"
+                                             :card-ids [(:id wrong-type-card)]})))))
+
+          (testing "should handle empty card-ids gracefully"
+            (let [result (mt/user-http-request :crowberto
+                                               :put 200 (format "ee/report/%s" report-id)
+                                               {:name "Report with Empty Cards"
+                                                :document "Doc with empty cards"
+                                                :card-ids []})]
+              (is (= report-id (:id result)))))
+
+          (testing "transaction rollback - when card update fails, report update should be rolled back"
+            (let [initial-report (t2/select-one :model/Report :id report-id)
+                  invalid-card-id 999999
+                  initial-versions-count (t2/count :model/ReportVersion :report_id report-id)]
+              (mt/user-http-request :crowberto
+                                    :put 404 (format "ee/report/%s" report-id)
+                                    {:name "Report That Should Rollback"
+                                     :document "Doc that should rollback"
+                                     :card-ids [invalid-card-id]})
+              ;; Verify no new report version was created due to rollback
+              (is (= initial-versions-count (t2/count :model/ReportVersion :report_id report-id)))
+              ;; Verify the report name wasn't updated
+              (let [unchanged-report (t2/select-one :model/Report :id report-id)]
+                (is (= (:name initial-report) (:name unchanged-report))))))))
+
+      (testing "should return 404 for non-existent report"
+        (mt/user-http-request :crowberto
+                              :put 404 "ee/report/99999" {:name "Non-existent Report" :document "Doc"})))))
 
 (deftest put-report-with-no-perms-test
   (mt/with-temp [:model/Collection {coll-id :id} {}
@@ -151,3 +326,92 @@
       (testing "should return 404 for non-existent report"
         (mt/user-http-request :crowberto
                               :get 404 "ee/report/99999/versions")))))
+
+(deftest validate-cards-for-report-test
+  (testing "validate-cards-for-report function"
+    (let [validate-cards #'report-api/validate-cards-for-report]
+
+      (testing "should pass with empty card list"
+        (is (nil? (validate-cards []))))
+
+      (testing "should pass with nil card list"
+        (is (nil? (validate-cards nil))))
+
+      (testing "should pass with valid :in_report cards"
+        (mt/with-temp [:model/Card card1 {:name "Valid Card 1"
+                                          :dataset_query (mt/mbql-query venues)
+                                          :display :table
+                                          :visualization_settings {}
+                                          :type :in_report}
+                       :model/Card card2 {:name "Valid Card 2"
+                                          :dataset_query (mt/mbql-query venues)
+                                          :display :table
+                                          :visualization_settings {}
+                                          :type :in_report}]
+          (let [result (validate-cards [(:id card1) (:id card2)])]
+            (is (= 2 (count result)))
+            (is (every? #(= :in_report (:type %)) result))
+            (is (= #{(:id card1) (:id card2)} (set (map :id result)))))))
+
+      (testing "should fail with non-existent card ids"
+        (is (thrown-with-msg?
+             clojure.lang.ExceptionInfo
+             #"The following card IDs do not exist: \[999999\].*"
+             (validate-cards [999999])))
+
+        (let [ex (try (validate-cards [999999 888888])
+                      (catch Exception e e))]
+          (is (= 404 (:status-code (ex-data ex))))
+          (is (= [999999 888888] (:missing-card-ids (ex-data ex))))))
+
+      (testing "should fail with cards that don't have :in_report type"
+        (mt/with-temp [:model/Card question-card {:name "Question Card"
+                                                  :dataset_query (mt/mbql-query venues)
+                                                  :display :table
+                                                  :visualization_settings {}
+                                                  :type :question}
+                       :model/Card model-card {:name "Model Card"
+                                               :dataset_query (mt/mbql-query venues)
+                                               :display :table
+                                               :visualization_settings {}
+                                               :type :model}]
+          (testing "single invalid card"
+            (let [ex (try (validate-cards [(:id question-card)])
+                          (catch Exception e e))]
+              (is (= 400 (:status-code (ex-data ex))))
+              (is (= [(:id question-card)] (mapv :id (:invalid-cards (ex-data ex)))))
+              (is (= [:question] (mapv :type (:invalid-cards (ex-data ex)))))))
+
+          (testing "multiple invalid cards"
+            (let [ex (try (validate-cards [(:id question-card) (:id model-card)])
+                          (catch Exception e e))]
+              (is (= 400 (:status-code (ex-data ex))))
+              (is (= #{(:id question-card) (:id model-card)}
+                     (set (mapv :id (:invalid-cards (ex-data ex))))))))))
+
+      (testing "should fail with mix of valid and invalid cards"
+        (mt/with-temp [:model/Card valid-card {:name "Valid Card"
+                                               :dataset_query (mt/mbql-query venues)
+                                               :display :table
+                                               :visualization_settings {}
+                                               :type :in_report}
+                       :model/Card invalid-card {:name "Invalid Card"
+                                                 :dataset_query (mt/mbql-query venues)
+                                                 :display :table
+                                                 :visualization_settings {}
+                                                 :type :question}]
+          (let [ex (try (validate-cards [(:id valid-card) (:id invalid-card)])
+                        (catch Exception e e))]
+            (is (= 400 (:status-code (ex-data ex))))
+            (is (= [(:id invalid-card)] (mapv :id (:invalid-cards (ex-data ex)))))))
+
+        (testing "should handle partial missing cards"
+          (mt/with-temp [:model/Card existing-card {:name "Existing Card"
+                                                    :dataset_query (mt/mbql-query venues)
+                                                    :display :table
+                                                    :visualization_settings {}
+                                                    :type :in_report}]
+            (let [ex (try (validate-cards [(:id existing-card) 999999])
+                          (catch Exception e e))]
+              (is (= 404 (:status-code (ex-data ex))))
+              (is (= [999999] (:missing-card-ids (ex-data ex)))))))))))
