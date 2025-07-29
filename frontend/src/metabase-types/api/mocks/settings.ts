9b0241aa11ba6ea7211b75107d1b1c567fdb1c4a
Phoomparin Mano <poom@metabase.com>
2025-07-30 01:38:15 +0700

    Rename feature token name from embedding_iframe_sdk to embedding_simple (#61548)

--- frontend/src/metabase-types/api/mocks/settings.ts
+++ frontend/src/metabase-types/api/mocks/settings.ts
@@ -107,7 +107,7 @@ export const createMockTokenFeatures = (
   content_verification: false,
   embedding: false,
   embedding_sdk: false,
-  embedding_iframe_sdk: false,
+  embedding_simple: false,
   etl_connections: false,
   etl_connections_pg: false,
   hosting: false,
