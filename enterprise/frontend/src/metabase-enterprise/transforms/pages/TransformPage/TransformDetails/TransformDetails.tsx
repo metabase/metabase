105b0c43f5cc170b81e55d91f3626e4d0248fc27
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-07-29 11:44:15 -0400

    [skip ci]

--- enterprise/frontend/src/metabase-enterprise/transforms/pages/TransformPage/TransformDetails/TransformDetails.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/pages/TransformPage/TransformDetails/TransformDetails.tsx
@@ -11,7 +11,7 @@ type TransformDetailsProps = {
 
 export function TransformDetails({ transform }: TransformDetailsProps) {
   return (
-    <Flex direction="column" flex={1} align="center" p="xl">
+    <Flex w="100%" direction="column" align="center" p="xl">
       <Stack w="100%" maw="60rem" gap="5rem">
         <NameSection transform={transform} />
         <ManageSection transform={transform} />
