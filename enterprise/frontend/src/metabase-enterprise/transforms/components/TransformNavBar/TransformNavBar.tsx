188f2b7aeaf3152ec56cfec5bd7777a06bd86afe
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-07-29 13:04:15 -0400

    navbar [skip ci]

--- enterprise/frontend/src/metabase-enterprise/transforms/components/TransformNavBar/TransformNavBar.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/TransformNavBar/TransformNavBar.tsx
@@ -1,3 +1,28 @@
-export function TransformNavBar() {
+import { useSelector } from "metabase/lib/redux";
+import type { TransformNavBarProps } from "metabase/plugins";
+import { getUserIsAdmin } from "metabase/selectors/user";
+
+export function TransformNavBar({ isActive }: TransformNavBarProps) {
+  const isAdmin = useSelector(getUserIsAdmin);
+  if (!isAdmin) {
+    return null;
+  }
+
+  return <TransformList isActive={isActive} />;
+}
+
+type TransformListProps = {
+  isActive: boolean;
+};
+
+function TransformList({ isActive }: TransformListProps) {
+  return <TransformToggle isActive={isActive} />;
+}
+
+type TransformToggleProps = {
+  isActive: boolean;
+};
+
+function TransformToggle(_props: TransformToggleProps) {
   return null;
 }
