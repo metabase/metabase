4681ef11e3e90cb91e31bc53213c99185f605e59
Timofei <timofei@metabase.com>
2025-07-29 17:32:30 +0400

    Use Mantine styles with CSS layer for the SDK

--- frontend/build/shared/rspack/css-config.js
+++ frontend/build/shared/rspack/css-config.js
@@ -3,7 +3,9 @@ const { IS_DEV_MODE } = require("../constants");
 module.exports.CSS_CONFIG = {
   modules: {
     auto: (filename) =>
-      !filename.includes("node_modules") && !filename.includes("vendor.css"),
+      ["node_modules", "vendor.css", "vendor.module.css"].every(
+        (excludedPattern) => !filename.includes(excludedPattern),
+      ),
     localIdentName: IS_DEV_MODE
       ? "[name]__[local]___[hash:base64:5]"
       : "[hash:base64:5]",
