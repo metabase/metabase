c3201478ea5fa6da11da15784582dd8c7c4644e3
Brad Anderson <brad@metabase.com>
2025-07-29 14:30:43 -0400

    UXW-28 Makes confirmation modal smarter; uses short version if all questions on a tab are dashboard questions, lists affected questions if it's a mix

--- frontend/src/metabase/dashboard/components/DashboardTabs/DashboardTabs.tsx
+++ frontend/src/metabase/dashboard/components/DashboardTabs/DashboardTabs.tsx
@@ -1,15 +1,18 @@
 import { t } from "ttag";
+import { uniq } from "underscore";
 
 import Button from "metabase/common/components/Button";
+import Link from "metabase/common/components/Link";
 import { Sortable } from "metabase/common/components/Sortable";
 import type { TabButtonMenuItem } from "metabase/common/components/TabButton";
 import { TabButton } from "metabase/common/components/TabButton";
 import { TabRow } from "metabase/common/components/TabRow";
 import { useConfirmation } from "metabase/common/hooks/use-confirmation";
+import CS from "metabase/css/core/index.css";
 import { useDashboardContext } from "metabase/dashboard/context";
 import { isVirtualDashCard } from "metabase/dashboard/utils";
 import { useRegisterShortcut } from "metabase/palette/hooks/useRegisterShortcut";
-import { Flex } from "metabase/ui";
+import { Flex, List } from "metabase/ui";
 import type { SelectedTabId } from "metabase-types/store";
 
 import S from "./DashboardTabs.module.css";
@@ -67,25 +70,49 @@ export function DashboardTabs() {
       label: t`Delete`,
       action: (_, value) => {
         const performDelete = () => deleteTab(value);
-        const hasDashboardQuestions = dashboard?.dashcards.some(
+        const tabQuestions = dashboard?.dashcards.filter(
           (dashcard) =>
-            dashcard.dashboard_tab_id === value &&
-            !isVirtualDashCard(dashcard) &&
-            dashcard.card.dashboard_id !== null,
+            dashcard.dashboard_tab_id === value && !isVirtualDashCard(dashcard),
         );
-        if (hasDashboardQuestions) {
-          show({
-            title: t`Delete your tab?`,
-            message: [
-              t`This will also delete the questions that are saved in it.`,
-              t`Are you sure you want to do this?`,
-            ].join(" "),
-            confirmButtonText: t`Delete tab`,
-            onConfirm: performDelete,
-          });
-        } else {
+        const tabDashboardQuestions = tabQuestions?.filter(
+          (dashcard) => dashcard.card.dashboard_id !== null,
+        );
+        const hasDashboardQuestions = !!tabDashboardQuestions?.length;
+        if (!hasDashboardQuestions) {
           performDelete();
+          return;
         }
+        const areAllDashboardQuestions =
+          tabQuestions?.length === tabDashboardQuestions.length;
+        show({
+          size: areAllDashboardQuestions ? "sm" : undefined,
+          title: areAllDashboardQuestions
+            ? t`Delete this tab and its charts?`
+            : t`Delete this tab?`,
+          message: areAllDashboardQuestions ? (
+            t`If you'd like to keep any of them, you can move them to a different tab, dashboard, or collection.`
+          ) : (
+            <>
+              {t`This will also delete any questions saved in it. If you'd like to keep any of these, move them to a different tab, dashboard, or collection.`}
+              <List ml="md" mt="sm">
+                {uniq(tabDashboardQuestions, (dc) => dc.card.id).map(
+                  (dashcard) => (
+                    <List.Item key={dashcard.card.id}>
+                      <Link
+                        to={`/question/${dashcard.card.id}`}
+                        className={CS.link}
+                      >
+                        {dashcard.card.name}
+                      </Link>
+                    </List.Item>
+                  ),
+                )}
+              </List>
+            </>
+          ),
+          confirmButtonText: t`Delete tab`,
+          onConfirm: performDelete,
+        });
       },
     });
   }
