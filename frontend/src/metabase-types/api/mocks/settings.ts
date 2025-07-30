a24b6daabd322ca38f15921626f7c96a854dd1d2
Sloan Sparger <sloansparger@gmail.com>
2025-07-30 13:33:38 -0500

    fix type issue

--- frontend/src/metabase-types/api/mocks/settings.ts
+++ frontend/src/metabase-types/api/mocks/settings.ts
@@ -134,6 +134,7 @@ export const createMockTokenFeatures = (
   ai_sql_generation: false,
   database_routing: false,
   development_mode: false,
+  semantic_search: false,
   ...opts,
 });
 
@@ -329,6 +330,7 @@ export const createMockSettings = (
   "scim-enabled": false,
   "scim-base-url": "http://localhost:3000/api/ee/scim/v2/",
   "snowplow-url": "",
+  "search-engine": "appdb",
   "search-typeahead-enabled": true,
   "setup-token": null,
   "session-cookies": null,
