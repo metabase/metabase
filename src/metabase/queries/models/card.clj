40fce6c3fa40bb714d0c09512dde4bb329aa45ce
Cam Saul <github@camsaul.com>
2025-07-30 17:26:58 +0000

    Fix formatting

--- src/metabase/queries/models/card.clj
+++ src/metabase/queries/models/card.clj
@@ -169,32 +169,32 @@
   (mi/instances-with-hydrated-data
    cards k
    (fn []
-     (let [card-ids          (map u/the-id cards)
-           all-dashboards    (t2/query {:union-all [ ;; First get dashboards from direct card connections
-                                                    {:nest
-                                                     {:select   [[:dc.card_id :card_id]
-                                                                 :d.name
-                                                                 :d.collection_id
-                                                                 :d.description
-                                                                 :d.id
-                                                                 :d.archived]
-                                                      :from     [[:report_dashboardcard :dc]]
-                                                      :join     [[:report_dashboard :d] [:= :dc.dashboard_id :d.id]]
-                                                      :where    [:in :dc.card_id [:inline card-ids]]
-                                                      :order-by [[:d.id :asc]]}}
-                                                    ;; Then get dashboards from series
-                                                    {:nest
-                                                     {:select   [[:dcs.card_id :card_id]
-                                                                 :d.name
-                                                                 :d.collection_id
-                                                                 :d.description
-                                                                 :d.id
-                                                                 :d.archived]
-                                                      :from     [[:dashboardcard_series :dcs]]
-                                                      :join     [[:report_dashboardcard :dc] [:= :dc.id :dcs.dashboardcard_id]
-                                                                 [:report_dashboard :d] [:= :d.id :dc.dashboard_id]]
-                                                      :where    [:in :dcs.card_id [:inline card-ids]]
-                                                      :order-by [[:d.id :asc]]}}]})]
+     (let [card-ids       (map u/the-id cards)
+           all-dashboards (t2/query {:union-all [ ;; First get dashboards from direct card connections
+                                                 {:nest
+                                                  {:select   [[:dc.card_id :card_id]
+                                                              :d.name
+                                                              :d.collection_id
+                                                              :d.description
+                                                              :d.id
+                                                              :d.archived]
+                                                   :from     [[:report_dashboardcard :dc]]
+                                                   :join     [[:report_dashboard :d] [:= :dc.dashboard_id :d.id]]
+                                                   :where    [:in :dc.card_id [:inline card-ids]]
+                                                   :order-by [[:d.id :asc]]}}
+                                                 ;; Then get dashboards from series
+                                                 {:nest
+                                                  {:select   [[:dcs.card_id :card_id]
+                                                              :d.name
+                                                              :d.collection_id
+                                                              :d.description
+                                                              :d.id
+                                                              :d.archived]
+                                                   :from     [[:dashboardcard_series :dcs]]
+                                                   :join     [[:report_dashboardcard :dc] [:= :dc.id :dcs.dashboardcard_id]
+                                                              [:report_dashboard :d] [:= :d.id :dc.dashboard_id]]
+                                                   :where    [:in :dcs.card_id [:inline card-ids]]
+                                                   :order-by [[:d.id :asc]]}}]})]
        (update-vals
         (group-by :card_id all-dashboards)
         (fn [dashes]
