ee5ac0a12152c4a14cddc684fa36aa8b83571664
Case Nelson <case@metabase.com>
2025-07-31 14:14:00 -0600

    fix: handle arrays in sparksql

--- modules/drivers/hive-like/test/metabase/driver/hive_like_test.clj
+++ modules/drivers/hive-like/test/metabase/driver/hive_like_test.clj
@@ -1,7 +1,9 @@
-(ns metabase.driver.hive-like-test
+(ns ^:mb/driver-tests metabase.driver.hive-like-test
   (:require
    [clojure.test :refer :all]
-   [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]))
+   [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]
+   [metabase.query-processor :as qp]
+   [metabase.test :as mt]))
 
 (deftest ^:parallel database-type->base-type-test
   (testing "make sure the various types we use for running tests are actually mapped to the correct DB type"
@@ -12,3 +14,9 @@
       :date      :type/Date
       :timestamp :type/DateTime
       :double    :type/Float)))
+
+(deftest ^:parallel array-test
+  (mt/test-drivers (mt/driver-select {:+parent :hive-like})
+    (let [query (mt/native-query {:query "select array(1,2,1)"})]
+      (is (= [["[1 2 3]"]]
+             (mt/rows (qp/process-query query)))))))
