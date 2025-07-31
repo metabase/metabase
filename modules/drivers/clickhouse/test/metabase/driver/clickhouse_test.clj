2257ea23d9ca4fc3860261a8003b21a6ea7b875e
Riley Thompson <rileythompson99@gmail.com>
2025-07-31 12:35:07 -0400

    re-enable broken tests

--- modules/drivers/clickhouse/test/metabase/driver/clickhouse_test.clj
+++ modules/drivers/clickhouse/test/metabase/driver/clickhouse_test.clj
@@ -277,40 +277,32 @@
                                 :target [:dimension [:template-tag "category_name"]]
                                 :value  ["African"]}]}))))))))
 
-#_(deftest ^:parallel ternary-with-variable-test
-    ;; TODO(rileythomp): enable these tests once the jdbc driver has been fixed
-    ;; broken in 0.8.4, waiting for fix
-    ;; https://github.com/metabase/metabase/issues/56690
-    ;; https://github.com/ClickHouse/clickhouse-java/issues/2348
-    (mt/test-driver :clickhouse
-      (testing "a query with a ternary and a variable should work correctly"
-        (is (= [[1 "African" 1]]
-               (mt/rows
-                (qp/process-query
-                 {:database (mt/id)
-                  :type :native
-                  :native {:query "SELECT *, true ? 1 : 0 AS foo
-                                   FROM test_data.categories
-                                   WHERE name = {{category_name}};"
-                           :template-tags {"category_name" {:type         :text
-                                                            :name         "category_name"
-                                                            :display-name "Category Name"}}}
-                  :parameters [{:type   :category
-                                :target [:variable [:template-tag "category_name"]]
-                                :value  "African"}]})))))))
+(deftest ^:parallel ternary-with-variable-test
+  (mt/test-driver :clickhouse
+    (testing "a query with a ternary and a variable should work correctly"
+      (is (= [[1 "African" 1]]
+             (mt/rows
+              (qp/process-query
+               {:database (mt/id)
+                :type :native
+                :native {:query "SELECT *, true ? 1 : 0 AS foo
+                                 FROM test_data.categories
+                                 WHERE name = {{category_name}};"
+                         :template-tags {"category_name" {:type         :text
+                                                          :name         "category_name"
+                                                          :display-name "Category Name"}}}
+                :parameters [{:type   :category
+                              :target [:variable [:template-tag "category_name"]]
+                              :value  "African"}]})))))))
 
-#_(deftest ^:parallel line-comment-block-comment-test
-    ;; TODO(rileythomp): enable these tests once the jdbc driver has been fix
-    ;; broken in  0.8.4, waiting for fix
-    ;; https://github.com/metabase/metabase/issues/57149
-    ;; https://github.com/ClickHouse/clickhouse-java/issues/2338
-    (mt/test-driver :clickhouse
-      (testing "a query with a line comment followed by a block comment should work correctly"
-        (is (= [[1]]
-               (mt/rows
-                (qp/process-query
-                 (mt/native-query
-                   {:query "-- foo
+(deftest ^:parallel line-comment-block-comment-test
+  (mt/test-driver :clickhouse
+    (testing "a query with a line comment followed by a block comment should work correctly"
+      (is (= [[1]]
+             (mt/rows
+              (qp/process-query
+               (mt/native-query
+                 {:query "-- foo
                             /* comment */
                             select 1;"}))))))))
 
