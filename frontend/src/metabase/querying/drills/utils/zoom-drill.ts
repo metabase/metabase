3a1d54e85f29ac88492c330219cce6415c64de85
Kamil Mielnik <kamil@kamilmielnik.com>
2025-07-31 17:21:15 +0700

    Change drill actions to navigate to new row detail page

--- frontend/src/metabase/querying/drills/utils/zoom-drill.ts
+++ frontend/src/metabase/querying/drills/utils/zoom-drill.ts
@@ -1,17 +1,21 @@
+import { push } from "react-router-redux";
 import { t } from "ttag";
 
 import { zoomInRow } from "metabase/query_builder/actions";
 import type { Drill } from "metabase/visualizations/types/click-actions";
-import type * as Lib from "metabase-lib";
+import * as Lib from "metabase-lib";
+import type { Dispatch } from "metabase-types/store";
 
 export const zoomDrill: Drill<Lib.ZoomDrillThruInfo> = ({
   drill,
   drillInfo,
   clicked,
   applyDrill,
+  query,
 }) => {
   const { objectId, isManyPks } = drillInfo;
   const isDashboard = clicked.extraData?.dashboard != null;
+  const tableId = Lib.sourceTableOrCardId(query);
 
   return [
     {
@@ -23,7 +27,15 @@ export const zoomDrill: Drill<Lib.ZoomDrillThruInfo> = ({
       default: true,
       ...(isDashboard
         ? { question: () => applyDrill(drill, objectId) }
-        : { action: () => zoomInRow({ objectId }) }),
+        : {
+            action: () => (dispatch: Dispatch) => {
+              if (tableId == null) {
+                zoomInRow({ objectId });
+              } else {
+                dispatch(push(`/table/${tableId}/detail/${objectId}`));
+              }
+            },
+          }),
       ...(!isManyPks ? { extra: () => ({ objectId }) } : {}),
     },
   ];
