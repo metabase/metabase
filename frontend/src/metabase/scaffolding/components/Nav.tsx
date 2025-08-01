8b8210f35c5ea1877c6911d65ead88f3af5eaf62
Kamil Mielnik <kamil@kamilmielnik.com>
2025-08-01 19:37:55 +0700

    QB-like header

--- frontend/src/metabase/scaffolding/components/Nav.tsx
+++ frontend/src/metabase/scaffolding/components/Nav.tsx
@@ -2,9 +2,11 @@ import type { ReactNode } from "react";
 import { Link } from "react-router";
 
 import { createMockMetadata } from "__support__/metadata";
-import { Group, Text, Title } from "metabase/ui";
+import { Box, Group, Text, Title } from "metabase/ui";
 import * as ML_Urls from "metabase-lib/v1/urls";
 import type { Table } from "metabase-types/api";
+
+import { TableBreadcrumbs } from "./TableBreadcrumbs";
 
 interface Props {
   children?: ReactNode;
@@ -15,34 +17,36 @@ interface Props {
 
 export const Nav = ({ children, rowId, rowName, table }: Props) => {
   return (
-    <Group align="flex-start" justify="space-between" pt="xl" px="xl">
-      <Group align="flex-end" gap="sm">
-        <Link to={getExploreTableUrl(table)}>
-          <Title>{table.display_name}</Title>
-        </Link>
+    <Group align="center" justify="space-between" pt="xl" px="xl">
+      <Group
+        align="flex-end"
+        gap={0}
+        wrap="nowrap"
+        flex={1}
+        style={{
+          fontSize: 20,
+        }}
+      >
+        <TableBreadcrumbs tableId={table.id} />
 
         {(rowName ?? rowId) != null && (
           <>
-            <Text c="text-medium" size="xl" fw="bold">
-              /
-            </Text>
+            <Separator />
 
             <Group
+              align="flex-end"
               component={Link}
               gap="sm"
               to={`/table/${table.id}/detail/${rowId}`}
+              style={{
+                fontSize: 20,
+              }}
+              c="text-primary"
+              fw="bold"
             >
-              {rowId != null && (
-                <Text c="text-medium" size="xl" fw="bold">
-                  {rowId}
-                </Text>
-              )}
+              {rowId != null && <Box>{rowId}</Box>}
 
-              {rowId != null && (
-                <Text c="text-medium" size="xl" fw="bold">
-                  {rowName}
-                </Text>
-              )}
+              {rowId != null && <Box>{rowName}</Box>}
             </Group>
           </>
         )}
@@ -54,6 +58,12 @@ export const Nav = ({ children, rowId, rowName, table }: Props) => {
     </Group>
   );
 };
+
+const Separator = () => (
+  <Text c="text-light" pl={"0.5em"} pr={"0.5em"}>
+    /
+  </Text>
+);
 
 export function getExploreTableUrl(table: Table): string {
   const metadata = createMockMetadata({
---
+++ frontend/src/metabase/scaffolding/components/TableBreadcrumbs/TableBreadcrumbs.tsx
@@ -0,0 +1,124 @@
+import { Link } from "react-router";
+
+import { createMockMetadata } from "__support__/metadata";
+import {
+  skipToken,
+  useGetTableQuery,
+  useListDatabaseSchemasQuery,
+} from "metabase/api";
+import { Flex, Group, Icon, Text } from "metabase/ui";
+import * as ML_Urls from "metabase-lib/v1/urls";
+import type { Table, TableId } from "metabase-types/api";
+
+import S from "./TableBreadcrumbs.module.css";
+
+interface Props {
+  className?: string;
+  hideTableName?: boolean;
+  tableId: TableId;
+}
+
+export const TableBreadcrumbs = ({
+  className,
+  hideTableName,
+  tableId,
+}: Props) => {
+  const { data: table } = useGetTableQuery({ id: tableId });
+
+  const { data: schemas, isLoading: isLoadingSchemas } =
+    useListDatabaseSchemasQuery(
+      table && table.db_id && table.schema ? { id: table.db_id } : skipToken,
+    );
+
+  if (!table || !table.db || isLoadingSchemas) {
+    return null;
+  }
+
+  return (
+    <>
+      <Group
+        component={Link}
+        to={`/browse/databases/${table.db_id}`}
+        c="text-light"
+        align="center"
+        gap={10}
+        className={S.breadcrumb}
+        wrap="nowrap"
+        fw={"bold"}
+        style={{
+          fontSize: 20,
+        }}
+      >
+        <Flex>
+          <Icon name="database" size={20} />
+        </Flex>
+
+        {table.db.name}
+      </Group>
+
+      {schemas && schemas.length > 1 && table.schema && (
+        <>
+          <Separator />
+
+          <Group
+            component={Link}
+            to={`/browse/databases/${table.db_id}/schema/${table.schema}`}
+            c="text-light"
+            align="center"
+            gap={10}
+            className={S.breadcrumb}
+            wrap="nowrap"
+            fw={"bold"}
+            style={{
+              fontSize: 20,
+            }}
+          >
+            {table.schema}
+          </Group>
+        </>
+      )}
+
+      {!hideTableName && (
+        <>
+          <Separator />
+
+          <Group
+            component={Link}
+            to={getExploreTableUrl(table)}
+            c="text-light"
+            align="center"
+            gap={10}
+            className={S.breadcrumb}
+            wrap="nowrap"
+            fw={"bold"}
+            style={{
+              fontSize: 20,
+            }}
+          >
+            {table.display_name}
+          </Group>
+        </>
+      )}
+    </>
+  );
+};
+
+const Separator = () => (
+  <Text c="text-light" pl={"0.5em"} pr={"0.5em"}>
+    /
+  </Text>
+);
+
+export function getExploreTableUrl(table: Table): string {
+  const metadata = createMockMetadata({
+    tables: table ? [table] : [],
+  });
+  const metadataTable = metadata?.table(table.id);
+  const question = metadataTable?.newQuestion();
+
+  if (!question) {
+    throw new Error("Unable to create question");
+  }
+
+  return ML_Urls.getUrl(question);
+}
