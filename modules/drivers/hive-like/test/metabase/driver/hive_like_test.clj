3399a07409e95911ca1d33a4fef466d149ab2687
Case Nelson <case@metabase.com>
2025-08-01 12:10:42 -0600

    Move test to child driver tests

--- modules/drivers/hive-like/test/metabase/driver/hive_like_test.clj
+++ modules/drivers/hive-like/test/metabase/driver/hive_like_test.clj
@@ -1,9 +1,8 @@
-(ns ^:mb/driver-tests metabase.driver.hive-like-test
+(ns metabase.driver.hive-like-test
+  "Do not mark this as ^:mb/driver-tests. CI will not see this file since this is an abstract driver."
   (:require
    [clojure.test :refer :all]
-   [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]
-   [metabase.query-processor :as qp]
-   [metabase.test :as mt]))
+   [metabase.driver.sql-jdbc.sync :as sql-jdbc.sync]))
 
 (deftest ^:parallel database-type->base-type-test
   (testing "make sure the various types we use for running tests are actually mapped to the correct DB type"
@@ -14,9 +13,3 @@
       :date      :type/Date
       :timestamp :type/DateTime
       :double    :type/Float)))
-
-(deftest ^:parallel array-test
-  (mt/test-drivers (mt/driver-select {:+parent :hive-like})
-    (let [query (mt/native-query {:query "select array(1,2,1)"})]
-      (is (= [["[1 2 3]"]]
-             (mt/rows (qp/process-query query)))))))
