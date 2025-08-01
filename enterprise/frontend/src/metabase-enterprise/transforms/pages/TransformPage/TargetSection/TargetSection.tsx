56e5d527911ac9b81bd7f1862f5fd9eaf9b1d315
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-07-31 23:42:26 -0400

    [skip ci]

--- enterprise/frontend/src/metabase-enterprise/transforms/pages/TransformPage/TargetSection/TargetSection.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/pages/TransformPage/TargetSection/TargetSection.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router";
+import { match } from "ts-pattern";
 import { t } from "ttag";
 
 import { Button, Group, Icon } from "metabase/ui";
 import { CardSection } from "metabase-enterprise/transforms/components/CardSection";
 import { getTableMetadataUrl } from "metabase-enterprise/transforms/urls";
-import type { Transform } from "metabase-types/api";
+import type { Transform, TransformTarget } from "metabase-types/api";
 
 type TargetSectionProps = {
   transform: Transform;
@@ -13,23 +14,49 @@ type TargetSectionProps = {
 export function TargetSection({ transform }: TargetSectionProps) {
   return (
     <CardSection
-      label={t`Generated view`}
+      label={getSectionLabel(transform.target)}
       description={t`Change what this transform generates and where.`}
     >
       <Group p="lg">
         <Button leftSection={<Icon name="pencil_lines" />}>
           {t`Change target`}
         </Button>
-        {transform.table && (
-          <Button
-            component={Link}
-            to={getTableMetadataUrl(transform.table)}
-            leftSection={<Icon name="label" />}
-          >
-            {t`Change target`}
-          </Button>
-        )}
+        <EditMetadataButton transform={transform} />
       </Group>
     </CardSection>
   );
+}
+
+function getSectionLabel({ type }: TransformTarget) {
+  return match(type)
+    .with("view", () => t`Generated view`)
+    .with("table", () => t`Generated table`)
+    .exhaustive();
+}
+
+type EditMetadataButtonProps = {
+  transform: Transform;
+};
+
+function EditMetadataButton({ transform }: EditMetadataButtonProps) {
+  if (transform.table == null) {
+    return null;
+  }
+
+  return (
+    <Button
+      component={Link}
+      to={getTableMetadataUrl(transform.table)}
+      leftSection={<Icon name="label" />}
+    >
+      {getEditButtonLabel(transform.target)}
+    </Button>
+  );
+}
+
+function getEditButtonLabel({ type }: TransformTarget) {
+  return match(type)
+    .with("view", () => t`Edit this view’s metadata`)
+    .with("table", () => t`Edit this table’s metadata`)
+    .exhaustive();
 }
