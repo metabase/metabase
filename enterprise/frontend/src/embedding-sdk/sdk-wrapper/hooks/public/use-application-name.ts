349ca8f9848fe125ffdb46166b0428062d4aafb4
Timofei <timofei@metabase.com>
2025-08-02 16:12:08 +0400

    Export useCreateDashboardApi hook

--- enterprise/frontend/src/embedding-sdk/sdk-wrapper/hooks/public/use-application-name.ts
+++ enterprise/frontend/src/embedding-sdk/sdk-wrapper/hooks/public/use-application-name.ts
@@ -1,5 +1,12 @@
 import { getWindow } from "embedding-sdk/sdk-shared/lib/get-window";
 import { useLazySelector } from "embedding-sdk/sdk-wrapper/hooks/private/use-lazy-selector";
 
+/**
+ * Returns application name.
+ * Returns `null` until the SDK is fully loaded and initialized.
+ *
+ * @function
+ * @category useApplicationName
+ */
 export const useApplicationName = () =>
   useLazySelector(getWindow()?.MetabaseEmbeddingSDK?.getApplicationName);
