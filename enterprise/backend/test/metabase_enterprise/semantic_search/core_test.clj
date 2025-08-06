efa9f4743aa2bf58f2d316549fec313213f5c36f
Noah Moss <noahbmoss@gmail.com>
2025-08-06 16:01:25 -0400

    add back one test

--- enterprise/backend/test/metabase_enterprise/semantic_search/core_test.clj
+++ enterprise/backend/test/metabase_enterprise/semantic_search/core_test.clj
@@ -15,13 +15,13 @@
                      #'semantic.tu/once-fixture
                      (fixtures/initialize :db)))
 
-; (deftest appdb-available-with-semantic
-;   (mt/with-premium-features #{:semantic-search}
-;     (mt/with-temporary-setting-values [search.settings/search-engine "semantic"]
-;       (with-open [_ (semantic.tu/open-temp-index!)]
-;         (semantic.tu/cleanup-index-metadata! semantic.tu/db semantic.tu/mock-index-metadata)
-;         (search/init-index! {:force-reset? false, :re-populate? false})
-;         (is (search.engine/supported-engine? :search.engine/appdb))))))
+(deftest appdb-available-with-semantic
+  (mt/with-premium-features #{:semantic-search}
+    (mt/with-temporary-setting-values [search.settings/search-engine "semantic"]
+      (with-open [_ (semantic.tu/open-temp-index!)]
+        (semantic.tu/cleanup-index-metadata! semantic.tu/db semantic.tu/mock-index-metadata)
+        (search/init-index! {:force-reset? false, :re-populate? false})
+        (is (search.engine/supported-engine? :search.engine/appdb))))))
 ;
 ; (deftest api-engine-switching-test
 ;   (mt/with-premium-features #{:semantic-search}
