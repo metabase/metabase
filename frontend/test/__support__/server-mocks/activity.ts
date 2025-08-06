fbc13136fd96e4c97cb8b56acec1d13313c53a52
Fraser <fraser@metabase.com>
2025-08-06 16:33:43 +1200

    Fix activity mock

--- frontend/test/__support__/server-mocks/activity.ts
+++ frontend/test/__support__/server-mocks/activity.ts
@@ -27,7 +27,6 @@ export function setupRecentViewsAndSelectionsEndpoints(
     response: {
       status: 200,
       body: { recents: recentItems },
-      ...mockOptions,
     },
     ...mockOptions,
   });
