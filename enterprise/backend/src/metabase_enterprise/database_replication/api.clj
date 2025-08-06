metabase/metabase

commit 805f4cafcfc9c0b23a894fbfb64cdf0c32a4b770

Turn the MB pg replication button into a modal (#61701)
Closes: https://linear.app/metabase/issue/CLO-4308/turn-the-mb-pg-replication-button-into-a-modal

enterprise/backend/src/metabase_enterprise/database_replication/api.clj
@@ -35,19 +35,35 @@
 
    This predicate checks that the quotas we got from the latest tokencheck have enough space for the database to be
   replicated."
-  [database]
+  [database schemas]
   (let [free-quota      (or
                          (some->> (m/find-first (comp #{"clickhouse-dwh"} :hosting-feature) (quotas))
                                   ((juxt :soft-limit :usage))
                                   (apply -))
                          -1)
+        include-patterns (->> schemas
+                              (filter (comp #{"include"} :type))
+                              (map (comp re-pattern :pattern)))
+        exclude-patterns (->> schemas
+                              (filter (comp #{"exclude"} :type))
+                              (map (comp re-pattern :pattern)))
         total-row-count (or
                          (some->>
                           (t2/hydrate database [:tables :fields])
                           :tables
                           (filter (comp (fn [x] (re-matches #"^[A-Za-z0-9_]+$" x)) :name))       ; sanitized name
                           (filter (fn [t] (some (comp #{:type/PK} :semantic_type) (:fields t)))) ; has-pkey
+                          (filter (fn [{:keys [schema]}]
+                                    (cond
+                                      (empty? schemas)
+                                      true
+                                      (not-empty include-patterns)
+                                      (some #(re-matches % schema) include-patterns)
+                                      (not-empty exclude-patterns)
+                                      (not (some #(re-matches % schema) exclude-patterns)))))
                           (map :estimated_row_count)
+                          ;; FIXME: `estimated_row_count` might be `nil`, in which case we should tell the user:
+                          ;;  "we don't know yet whether this will work, wait or try your luck"
                           (reduce +))
                          0)]
     (log/infof "Quota left: %s. Estimate db row count: %s" free-quota total-row-count)
@@ -55,7 +71,14 @@
 
 (api.macros/defendpoint :post "/connection/:database-id"
   "Create a new PG replication connection for the specified database."
-  [{:keys [database-id]} :- [:map [:database-id ms/PositiveInt]]]
+  ;; FIXME: First arg is route params, 2nd arg is query params, 3rd arg is body params:
+  [{:keys [database-id]} :- [:map [:database-id ms/PositiveInt]]
+   _query-params
+   {:keys [schemas]} :- [:map
+                         [:schemas {:optional true} [:sequential
+                                                     [:map
+                                                      [:type [:enum "include" "exclude"]]
+                                                      [:pattern :string]]]]]]
   (api/check-400 (database-replication.settings/database-replication-enabled) "PG replication integration is not enabled.")
   (let [database (t2/select-one :model/Database :id database-id)]
     (api/check-404 database)
@@ -66,13 +89,14 @@
                             (select-keys [:dbname :host :user :password :port])
                             (update :port #(or % 5432))) ;; port is required in the API, but optional in MB
             {:keys [schema-filters-type schema-filters-patterns]} (:details database)
-            schemas      (when-some [k ({"inclusion" :include, "exclusion" :exclude} schema-filters-type)]
-                           (when schema-filters-patterns
-                             {:schemas {k schema-filters-patterns}}))
-
-            secret       (merge {:credentials (merge {:dbtype "postgresql"} credentials)}
-                                schemas)]
-        (if (can-set-replication? database)
+            schemas     (-> (when-some [k ({"inclusion" :include, "exclusion" :exclude} schema-filters-type)]
+                              (for [pattern schema-filters-patterns]
+                                {:type k :pattern pattern}))
+                            (concat (map #(select-keys % [:type :pattern]) schemas))
+                            not-empty)
+            secret      {:credentials (merge {:dbtype "postgresql"} credentials)
+                         :schemas     schemas}]
+        (if (can-set-replication? database schemas)
           (let [{:keys [id]} (hm.client/call :create-connection, :type "pg_replication", :secret secret)
                 conn         {:connection-id id}]
             (database-replication.settings/database-replication-connections! (assoc conns (kw-id database-id) conn))

enterprise/frontend/src/metabase-enterprise/api/database-replication.ts
@@ -5,10 +5,17 @@ import { EnterpriseApi } from "./api";
 
 export const DatabaseReplicationApi = EnterpriseApi.injectEndpoints({
   endpoints: (builder) => ({
-    createDatabaseReplication: builder.mutation<void, DatabaseId>({
-      query: (databaseId) => ({
+    createDatabaseReplication: builder.mutation<
+      void,
+      {
+        databaseId: DatabaseId;
+        schemas?: { type: "include" | "exclude"; pattern: string }[];
+      }
+    >({
+      query: ({ databaseId, ...body }) => ({
         method: "POST",
         url: `/api/ee/database-replication/connection/${databaseId}`,
+        body,
       }),
       invalidatesTags: (_, error) =>
         invalidateTags(error, [tag("session-properties")]),

enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationButton.tsx
@@ -1,60 +1,0 @@
-import { t } from "ttag";
-
-import ActionButton from "metabase/common/components/ActionButton";
-import { useSetting } from "metabase/common/hooks";
-import {
-  useCreateDatabaseReplicationMutation,
-  useDeleteDatabaseReplicationMutation,
-} from "metabase-enterprise/api/database-replication";
-import type { DatabaseId } from "metabase-types/api";
-
-export function DatabaseReplicationButton({
-  databaseId,
-}: {
-  databaseId: DatabaseId;
-}) {
-  const showDatabaseReplication = useSetting("database-replication-enabled");
-  const connections = useSetting("database-replication-connections");
-
-  const [createDatabaseReplication] = useCreateDatabaseReplicationMutation();
-  const [deleteDatabaseReplication] = useDeleteDatabaseReplicationMutation();
-
-  const hasConnection = connections?.[databaseId] != null;
-
-  if (!showDatabaseReplication) {
-    return null;
-  }
-
-  const handleCreateReplication = () => {
-    return createDatabaseReplication(databaseId).unwrap();
-  };
-
-  const handleDeleteReplication = () => {
-    return deleteDatabaseReplication(databaseId).unwrap();
-  };
-
-  // Note: the api call will invalidate the session, which updates the setting, which swaps the buttons.
-  // When the button is swapped midway, it will use the other button's success text.
-  // So both buttons have the same innocuous success text.
-  if (hasConnection) {
-    return (
-      <ActionButton
-        actionFn={handleDeleteReplication}
-        normalText={t`Stop replicating to Data Warehouse`}
-        activeText={t`Stopping…`}
-        failedText={t`Failed to stop replication`}
-        successText={t`Done!`}
-      />
-    );
-  }
-
-  return (
-    <ActionButton
-      actionFn={handleCreateReplication}
-      normalText={t`Replicate to Data Warehouse`}
-      activeText={t`Starting…`}
-      failedText={t`Failed to replicate`}
-      successText={t`Done!`}
-    />
-  );
-}

enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationButton.unit.spec.tsx
@@ -1,140 +1,0 @@
-import { waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import fetchMock from "fetch-mock";
-
-import { findRequests } from "__support__/server-mocks";
-import { renderWithProviders, screen } from "__support__/ui";
-import {
-  createMockSettingsState,
-  createMockState,
-} from "metabase-types/store/mocks";
-
-import { DatabaseReplicationButton } from "./DatabaseReplicationButton";
-
-const setup = ({ databaseId = 1, settings = {} }) => {
-  const storeInitialState = createMockState({
-    settings: createMockSettingsState({
-      "database-replication-enabled": true,
-      "database-replication-connections": {},
-      ...settings,
-    }),
-  });
-
-  return renderWithProviders(
-    <DatabaseReplicationButton databaseId={databaseId} />,
-    {
-      storeInitialState,
-    },
-  );
-};
-
-describe("DatabaseReplicationButton", () => {
-  beforeEach(() => {
-    fetchMock.reset();
-    // Mock the API endpoints
-    fetchMock.post(
-      "express:/api/ee/database-replication/connection/:databaseId",
-      200,
-    );
-    fetchMock.delete(
-      "express:/api/ee/database-replication/connection/:databaseId",
-      200,
-    );
-  });
-
-  afterEach(() => {
-    fetchMock.restore();
-  });
-
-  describe("when database-replication-enabled is false", () => {
-    it("should not render button", () => {
-      setup({ settings: { "database-replication-enabled": false } });
-
-      expect(screen.queryByRole("button")).not.toBeInTheDocument();
-    });
-  });
-
-  describe("when database-replication-enabled is true", () => {
-    describe("and database has no replication connection", () => {
-      it("should render 'Replicate to Data Warehouse' button", () => {
-        setup({
-          databaseId: 1,
-          settings: { "database-replication-connections": {} },
-        });
-
-        expect(
-          screen.getByRole("button", { name: "Replicate to Data Warehouse" }),
-        ).toBeInTheDocument();
-      });
-
-      it("should call create API when clicked", async () => {
-        const view = userEvent.setup();
-        setup({
-          databaseId: 1,
-          settings: { "database-replication-connections": {} },
-        });
-
-        await view.click(
-          screen.getByRole("button", { name: "Replicate to Data Warehouse" }),
-        );
-
-        await waitFor(async () => {
-          const requests = await findRequests("POST");
-          expect(requests).toHaveLength(1);
-        });
-
-        const requests = await findRequests("POST");
-        expect(requests[0].url).toMatch(
-          /\/api\/ee\/database-replication\/connection\/1$/,
-        );
-      });
-    });
-
-    describe("and database has replication connection", () => {
-      it("should render 'Stop replicating to Data Warehouse' button", () => {
-        setup({
-          databaseId: 1,
-          settings: {
-            "database-replication-connections": {
-              1: { connection_id: "conn_123" },
-            },
-          },
-        });
-
-        expect(
-          screen.getByRole("button", {
-            name: "Stop replicating to Data Warehouse",
-          }),
-        ).toBeInTheDocument();
-      });
-
-      it("should call delete API when clicked", async () => {
-        const view = userEvent.setup();
-        setup({
-          databaseId: 1,
-          settings: {
-            "database-replication-connections": {
-              1: { connection_id: "conn_123" },
-            },
-          },
-        });
-
-        await view.click(
-          screen.getByRole("button", {
-            name: "Stop replicating to Data Warehouse",
-          }),
-        );
-
-        await waitFor(async () => {
-          const requests = await findRequests("DELETE");
-          expect(requests).toHaveLength(1);
-        });
-
-        const requests = await findRequests("DELETE");
-        expect(requests[0].url).toMatch(
-          /\/api\/ee\/database-replication\/connection\/1$/,
-        );
-      });
-    });
-  });
-});

enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationForm.tsx
@@ -1,0 +1,116 @@
+import { t } from "ttag";
+import * as Yup from "yup";
+
+import {
+  Form,
+  FormProvider,
+  FormSelect,
+  FormSubmitButton,
+  FormTextInput,
+} from "metabase/forms";
+import { Button, Flex, Group, Text } from "metabase/ui";
+import type { DatabaseId } from "metabase-types/api";
+
+const styles = {
+  wrapperProps: {
+    fw: 400,
+  },
+  labelProps: {
+    fz: "0.875rem",
+    mb: "0.75rem",
+  },
+};
+
+export interface DWHReplicationFormFields {
+  databaseId: DatabaseId;
+  schemaSelect: "all" | "include" | "exclude";
+  schemas: string;
+}
+
+const validationSchema = Yup.object({
+  schemaSelect: Yup.string().oneOf(["all", "include", "exclude"]),
+  schemas: Yup.string(),
+});
+
+type IFieldError =
+  | string
+  | {
+      message: string;
+    }
+  | {
+      errors: { [key: string]: any };
+    };
+
+const isFieldError = (error: unknown): error is IFieldError =>
+  typeof error === "string" ||
+  (error instanceof Object &&
+    (("message" in error && typeof error.message === "string") ||
+      ("errors" in error &&
+        error.errors instanceof Object &&
+        "schemas" in error.errors &&
+        typeof error.errors.schemas === "string")));
+
+export const handleFieldError = (error: unknown) => {
+  if (isFieldError(error)) {
+    if (typeof error === "string") {
+      throw { data: { errors: { schemas: error } } };
+    } else if ("message" in error) {
+      throw { data: { errors: { schemas: error.message } } };
+    } else if ("errors" in error) {
+      throw { data: error };
+    }
+  }
+};
+
+export const DatabaseReplicationForm = ({
+  onSubmit,
+  onCancel,
+  initialValues,
+}: {
+  onSubmit: (_: DWHReplicationFormFields) => void;
+  onCancel: () => void;
+  initialValues: DWHReplicationFormFields;
+}) => (
+  <FormProvider
+    initialValues={initialValues}
+    onSubmit={onSubmit}
+    validationSchema={validationSchema}
+  >
+    {({ dirty, values, setFieldValue }) => (
+      <Form>
+        <FormSelect
+          name="schemaSelect"
+          label={t`Select schemas to replicate`}
+          onChange={() => setFieldValue("excludeSchemas", null)}
+          data={[
+            { value: "all", label: t`All` },
+            { value: "include", label: t`Only these…` },
+            { value: "exclude", label: t`All except…` },
+          ]}
+          {...styles}
+        />
+        {values.schemaSelect !== "all" ? (
+          <>
+            <Text c="text-light">{t`Comma separated names of schemas that should ${values.schemaSelect === "exclude" ? "NOT " : ""}be replicated`}</Text>
+            <FormTextInput
+              name="schemas"
+              placeholder="e.g. public, auth"
+              {...styles}
+            />
+          </>
+        ) : undefined}
+        <Text c="text-light">{t`You will get an email once your data is ready to use.`}</Text>
+        <Flex justify="end">
+          <Group>
+            <Button onClick={onCancel}>{t`Cancel`}</Button>
+            <FormSubmitButton
+              disabled={!dirty}
+              label={t`Start replication`}
+              variant="filled"
+            />
+          </Group>
+        </Flex>
+      </Form>
+    )}
+  </FormProvider>
+);

enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationModal.tsx
@@ -1,0 +1,80 @@
+import { t } from "ttag";
+
+import { Modal, Progress, Text } from "metabase/ui";
+import { useCreateDatabaseReplicationMutation } from "metabase-enterprise/api/database-replication";
+import type { Database } from "metabase-types/api";
+
+import {
+  type DWHReplicationFormFields,
+  DatabaseReplicationForm,
+  handleFieldError as handleDWHReplicationFieldError,
+} from "./DatabaseReplicationForm";
+
+interface IRTKQueryError {
+  status: unknown;
+  data: unknown;
+}
+
+// https://redux-toolkit.js.org/rtk-query/usage/error-handling
+const isRTKQueryError = (error: unknown): error is IRTKQueryError =>
+  error instanceof Object && "status" in error && "data" in error;
+
+export const DatabaseReplicationModal = ({
+  isOpen,
+  onClose,
+  database,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  database: Database;
+}) => {
+  const [createDatabaseReplication] = useCreateDatabaseReplicationMutation();
+
+  const onSubmit = async ({
+    schemaSelect,
+    schemas,
+  }: DWHReplicationFormFields) =>
+    createDatabaseReplication({
+      databaseId: database.id,
+      schemas:
+        schemaSelect === "all"
+          ? undefined
+          : schemas
+              .split(",")
+              .map((pattern) => pattern.trim())
+              .map((pattern) => ({ type: schemaSelect, pattern })),
+    })
+      .unwrap()
+      .then(onClose)
+      .catch((error) => {
+        isRTKQueryError(error) && handleDWHReplicationFieldError(error.data);
+      });
+
+  return (
+    <Modal
+      opened={isOpen}
+      onClose={onClose}
+      size="36rem"
+      padding="2.5rem"
+      title={t`Set up database replication`}
+    >
+      <>
+        {/* FIXME: Get values from Store API and fix the layout to look like the UI design. */}
+        <Text c="text-light">{database.name}</Text>
+        <Text style={{ "font-weight": "bold" }}>{t`12,345,678 rows`}</Text>
+        <Text c="text-light">{t`Available Cloud Storage`}</Text>
+        <Text style={{ "font-weight": "bold" }}>{t`20M rows`}</Text>
+        <Progress value={(12_345_678 / 20_000_000) * 100} />
+      </>
+      <DatabaseReplicationForm
+        onSubmit={onSubmit}
+        onCancel={onClose}
+        initialValues={{
+          databaseId: database.id,
+          schemaSelect: "all",
+          schemas: "",
+        }}
+      />
+    </Modal>
+  );
+};

enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationSection.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { t } from "ttag";
 
 import {
@@ -5,10 +6,11 @@ import {
   DatabaseInfoSectionDivider,
 } from "metabase/admin/databases/components/DatabaseInfoSection";
 import { useSetting } from "metabase/common/hooks";
-import { Flex } from "metabase/ui";
+import { Button, Flex, Icon } from "metabase/ui";
+import { useDeleteDatabaseReplicationMutation } from "metabase-enterprise/api/database-replication";
 import type { Database } from "metabase-types/api";
 
-import { DatabaseReplicationButton } from "./DatabaseReplicationButton";
+import { DatabaseReplicationModal } from "./DatabaseReplicationModal";
 import { DatabaseReplicationPostgresInfo } from "./DatabaseReplicationPostgresInfo";
 import { DatabaseReplicationStatusInfo } from "./DatabaseReplicationStatusInfo";
 
@@ -26,7 +28,12 @@ export function DatabaseReplicationSection({
 }: {
   database: Database;
 }) {
+  const [showDWHReplicationModal, setShowDWHReplicationModal] =
+    useState<boolean>(false);
+  const connections = useSetting("database-replication-connections");
   const databaseReplicationEnabled = useSetting("database-replication-enabled");
+  const [deleteDatabaseReplication, { isLoading: isDeleting }] =
+    useDeleteDatabaseReplicationMutation();
   const databaseSupportsReplication = database.features?.includes(
     "database-replication",
   );
@@ -36,8 +43,11 @@ export function DatabaseReplicationSection({
   }
 
   const engineInfo = getEngineInfo(database.engine);
+  const hasConnection = connections?.[database.id] != null;
+  const onDelete = async () => {
+    await deleteDatabaseReplication(database.id).unwrap();
+  };
 
-  // button below will need to open the modal
   return (
     <DatabaseInfoSection
       condensed
@@ -47,7 +57,23 @@ export function DatabaseReplicationSection({
     >
       <Flex align="center" justify="space-between" gap="lg">
         <DatabaseReplicationStatusInfo databaseId={database.id} />
-        <DatabaseReplicationButton databaseId={database.id} />
+        {hasConnection ? (
+          <Button
+            onClick={onDelete}
+            loaderProps={{ children: t`Stopping…` }}
+            loading={isDeleting}
+            leftSection={<Icon name="trash" />}
+          >{t`Stop replicating to Data Warehouse`}</Button>
+        ) : (
+          <Button
+            onClick={() => setShowDWHReplicationModal(true)}
+          >{t`Replicate to Data Warehouse`}</Button>
+        )}
+        <DatabaseReplicationModal
+          database={database}
+          isOpen={showDWHReplicationModal}
+          onClose={() => setShowDWHReplicationModal(false)}
+        />
       </Flex>
 
       {engineInfo && (

commit c967380c72e6579cb6c5c7b331215f74ca368d13

Align replication schema-filters with HM
Fixes: 805f4cafcfc9c0b23a894fbfb64cdf0c32a4b770
Depends-on: https://github.com/metabase/harbormaster/pull/6445

enterprise/backend/src/metabase_enterprise/database_replication/api.clj
@@ -31,38 +31,55 @@
 (defn- database-id->connection-id [conns database-id]
   (->> database-id kw-id (get conns) :connection-id))
 
+(defn- prepare-filter
+  [filter-type schema-filters]
+  (some->> schema-filters
+           (filter (comp #{filter-type} :type))
+           (map (comp #(str/replace % #"(^|[^\\\\])\*" "$1.*") str/trim :pattern))
+           not-empty
+           (str/join "|")
+           re-pattern))
+
+(defn- schema-filters->fn [schema-filters]
+  (let [include-pattern (prepare-filter "include" schema-filters)
+        include-fn      #(re-matches include-pattern %)
+        exclude-pattern (prepare-filter "exclude" schema-filters)
+        exclude-fn      #(not (re-matches exclude-pattern %))]
+    (cond
+      (and (not include-pattern)
+           (not exclude-pattern))
+      (constantly true)
+
+      (and include-pattern
+           exclude-pattern)
+      (every-pred include-fn exclude-fn)
+
+      include-pattern
+      include-fn
+
+      exclude-pattern
+      exclude-fn)))
+
 (defn- token-check-quotas-info
   "Predicate that signals if replication looks right from the quota perspective.
 
    This predicate checks that the quotas we got from the latest tokencheck have enough space for the database to be
   replicated."
-  [database schemas]
+  [database schema-filters]
   (let [all-quotas                (quotas)
         free-quota                (or
                                    (some->> (m/find-first (comp #{"clickhouse-dwh"} :hosting-feature) all-quotas)
                                             ((juxt :soft-limit :usage))
                                             (apply -))
                                    -1)
-        include-patterns          (->> schemas
-                                       (filter (comp #{"include"} :type))
-                                       (map (comp re-pattern :pattern)))
-        exclude-patterns          (->> schemas
-                                       (filter (comp #{"exclude"} :type))
-                                       (map (comp re-pattern :pattern)))
+        filter-schema             (schema-filters->fn schema-filters)
         db-tables                 (some->> (t2/hydrate database [:tables :fields]) :tables) ; sanitized name
         sanitized-tables          (some->> db-tables
                                            (filter (comp (fn [x] (re-matches #"^[A-Za-z0-9_]+$" x)) :name))
-                                           (filter (fn [{:keys [schema]}]
-                                                     (cond
-                                                       (empty? schemas)
-                                                       true
-                                                       (not-empty include-patterns)
-                                                       (some #(re-matches % schema) include-patterns)
-                                                       (not-empty exclude-patterns)
-                                                       (not (some #(re-matches % schema) exclude-patterns))))))
+                                           (filter (comp filter-schema :schema)))
         {tables-without-pk false
          tables-with-pk    true}  (group-by (fn [t] (boolean (some (comp #{:type/PK} :semantic_type) (:fields t))))
-                                          sanitized-tables)
+                                            sanitized-tables)
         total-estimated-row-count (or
                                    (some->>
                                     tables-with-pk
@@ -96,29 +113,29 @@
   ;; FIXME: First arg is route params, 2nd arg is query params, 3rd arg is body params:
   [{:keys [database-id]} :- [:map [:database-id ms/PositiveInt]]
    _query-params
-   {:keys [schemas]} :- [:map
-                         [:schemas {:optional true} [:sequential
-                                                     [:map
-                                                      [:type [:enum "include" "exclude"]]
-                                                      [:pattern :string]]]]]]
+   {:keys [schema-filters]} :- [:map
+                                [:schema-filters {:optional true} [:sequential
+                                                                   [:map
+                                                                    [:type [:enum "include" "exclude"]]
+                                                                    [:pattern :string]]]]]]
   (api/check-400 (database-replication.settings/database-replication-enabled) "PG replication integration is not enabled.")
   (let [database (t2/select-one :model/Database :id database-id)]
     (api/check-404 database)
     (api/check-400 (= :postgres (:engine database)) "PG replication is only supported for PostgreSQL databases.")
     (let [conns (pruned-database-replication-connections)]
       (api/check-400 (not (database-id->connection-id conns database-id)) "Database already has an active replication connection.")
-      (let [credentials (-> (:details database)
-                            (select-keys [:dbname :host :user :password :port])
-                            (update :port #(or % 5432))) ;; port is required in the API, but optional in MB
+      (let [credentials    (-> (:details database)
+                               (select-keys [:dbname :host :user :password :port])
+                               (update :port #(or % 5432))) ;; port is required in the API, but optional in MB
             {:keys [schema-filters-type schema-filters-patterns]} (:details database)
-            schemas     (-> (when-some [k ({"inclusion" :include, "exclusion" :exclude} schema-filters-type)]
-                              (for [pattern (str/split schema-filters-patterns #",")]
-                                {:type k :pattern pattern}))
-                            (concat (map #(select-keys % [:type :pattern]) schemas))
-                            not-empty)
-            secret      {:credentials (merge {:dbtype "postgresql"} credentials)
-                         :schemas     schemas}]
-        (if (can-set-replication? database schemas)
+            schema-filters (-> (when-some [k ({"inclusion" :include, "exclusion" :exclude} schema-filters-type)]
+                                 (for [pattern (str/split schema-filters-patterns #",")]
+                                   {:type k :pattern pattern}))
+                               (concat (map #(select-keys % [:type :pattern]) schema-filters))
+                               not-empty)
+            secret         {:credentials    (merge {:dbtype "postgresql"} credentials)
+                            :schema-filters schema-filters}]
+        (if (can-set-replication? database schema-filters)
           (let [{:keys [id]} (hm.client/call :create-connection, :type "pg_replication", :secret secret)
                 conn         {:connection-id id}]
             (database-replication.settings/database-replication-connections! (assoc conns (kw-id database-id) conn))