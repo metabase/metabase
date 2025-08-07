e2ffde0e90c5af15c278e055739ca07fed15579c
Noah Moss <noahbmoss@gmail.com>
2025-08-07 18:51:14 -0400

    Revert "switch back to transducing version"
    This reverts commit 4a99c3c6869e28f56d794d8057f006700cfc8432.

--- enterprise/backend/src/metabase_enterprise/semantic_search/index.clj
+++ enterprise/backend/src/metabase_enterprise/semantic_search/index.clj
@@ -293,17 +293,11 @@
   model + model_id already exists, it will be replaced. Parallelizes batch insertion
   using a thread pool with a configurable thread count (default: 2)."
   [connectable index documents-reducible]
-  (cp/with-shutdown! [pool (cp/threadpool (semantic-settings/index-update-thread-count))]
-    (transduce
-     (comp
-      (partition-all *batch-size*)
-      (map #(cp/future pool (upsert-index-batch! connectable index %))))
-     (completing
-      (fn [acc f]
-        (let [result @f]
-          (merge-with + acc result))))
-     {}
-     documents-reducible)))
+  (cp/with-shutdown! [pool (cp/threadpool (semantic-settings/embedding-thread-count))]
+    (->> documents-reducible
+         (partition-all *batch-size*)
+         (cp/pmap pool #(upsert-index-batch! connectable index %))
+         (reduce (partial merge-with +) {}))))
 
 (defn- drop-index-table-sql
   [{:keys [table-name]}]
