13049dab5d52319a95465b77d3a57b8d88249373
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-08-07 09:47:46 -0400

    UI changes [skip ci]

--- enterprise/frontend/src/metabase-enterprise/transforms/pages/JobListPage/NewJobButton/NewJobButton.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/pages/JobListPage/NewJobButton/NewJobButton.tsx
@@ -1,11 +1,41 @@
+import { push } from "react-router-redux";
 import { t } from "ttag";
 
+import { useDispatch } from "metabase/lib/redux";
+import { useMetadataToasts } from "metabase/metadata/hooks";
 import { Button, Icon } from "metabase/ui";
+import { useCreateTransformJobMutation } from "metabase-enterprise/api";
+
+import { getJobUrl } from "../../../urls";
+
+const DEFAULT_SCHEDULE = "0 0 * * * ? *";
 
 export function NewJobButton() {
+  const [createJob, { isLoading }] = useCreateTransformJobMutation();
+  const { sendErrorToast } = useMetadataToasts();
+  const dispatch = useDispatch();
+
+  const handleCreate = async () => {
+    const { data: job, error } = await createJob({
+      name: t`New job`,
+      schedule: DEFAULT_SCHEDULE,
+    });
+
+    if (error) {
+      sendErrorToast(t`Failed to create a job`);
+    } else if (job != null) {
+      dispatch(push(getJobUrl(job.id)));
+    }
+  };
+
   return (
-    <Button variant="filled" leftSection={<Icon name="add" />}>
-      {t`Create a job`}
+    <Button
+      variant="filled"
+      leftSection={<Icon name="add" />}
+      disabled={isLoading}
+      onClick={handleCreate}
+    >
+      {isLoading ? t`Creating a job…` : t`Create a job`}
     </Button>
   );
 }
