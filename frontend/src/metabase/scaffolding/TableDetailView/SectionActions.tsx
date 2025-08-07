a4f09cfb34bff58c2d2aa4567b024f884037b1e2
Kamil Mielnik <kamil@kamilmielnik.com>
2025-08-07 22:05:06 +0700

    Button to toggle group highlight

--- frontend/src/metabase/scaffolding/TableDetailView/SectionActions.tsx
+++ frontend/src/metabase/scaffolding/TableDetailView/SectionActions.tsx
@@ -19,6 +19,8 @@ type ObjectViewSectionProps = {
   onUpdateSection?: (section: Partial<ObjectViewSectionSettings>) => void;
 };
 
+const HIGHLIGHTABLE_VARIANTS = ["highlight-2", "normal"];
+
 export function SectionActions({
   columns,
   section,
@@ -29,14 +31,39 @@ export function SectionActions({
 }: ObjectViewSectionProps) {
   return (
     <Group
+      align="center"
       bg="bg-white"
       gap="xs"
+      px="xs"
+      wrap="nowrap"
       style={{
         border: "1px solid var(--border-color)",
         borderRadius: "var(--mantine-radius-md)",
         boxShadow: "0 0 1px var(--mb-color-shadow)",
       }}
     >
+      {HIGHLIGHTABLE_VARIANTS.includes(section.variant) && onUpdateSection && (
+        <Tooltip
+          label={
+            section.variant === "normal"
+              ? t`Highlight group`
+              : t`Remove highlight`
+          }
+        >
+          <SectionAction
+            iconName={section.variant === "highlight-2" ? "table" : "list"}
+            onClick={() => {
+              onUpdateSection({
+                variant:
+                  section.variant === "normal"
+                    ? ("highlight-2" as const)
+                    : ("normal" as const),
+              });
+            }}
+          />
+        </Tooltip>
+      )}
+
       {onUpdateSection && (
         <ColumnPickerButton
           columns={columns}
