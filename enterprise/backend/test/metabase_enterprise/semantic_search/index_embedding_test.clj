2a4f64f0b33b4ae96f37d1aa4ad7f8e88fa7743f
Sloan Sparger <sloansparger@gmail.com>
2025-08-08 13:05:11 -0500

    death by lints

--- enterprise/backend/test/metabase_enterprise/semantic_search/index_embedding_test.clj
+++ enterprise/backend/test/metabase_enterprise/semantic_search/index_embedding_test.clj
@@ -2,7 +2,6 @@
   "Tests for things at the intersection of the index and embedding namespaces."
   (:require
    [clojure.test :refer :all]
-   [metabase-enterprise.semantic-search.embedding :as semantic.embedding]
    [metabase-enterprise.semantic-search.index :as semantic.index]
    [metabase.test :as mt]))
 
