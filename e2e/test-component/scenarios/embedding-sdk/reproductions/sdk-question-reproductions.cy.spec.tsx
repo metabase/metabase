c4f6bbbf0ab5c323d9585b1cba5e287242ac0788
Nicolò Pretto <info@npretto.com>
2025-08-08 16:58:51 +0200

    fix imports

--- e2e/test-component/scenarios/embedding-sdk/reproductions/sdk-question-reproductions.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/reproductions/sdk-question-reproductions.cy.spec.tsx
@@ -5,10 +5,12 @@ import {
   THIRD_COLLECTION_ID,
 } from "e2e/support/cypress_sample_instance_data";
 import { entityPickerModal, modal } from "e2e/support/helpers";
+import {
+  mockAuthProviderAndJwtSignIn,
+  mountSdkContent,
+  signInAsAdminAndEnableEmbeddingSdk,
+} from "e2e/support/helpers/component-testing-sdk";
 import { getSdkRoot } from "e2e/support/helpers/e2e-embedding-sdk-helpers";
-import { mountSdkContent } from "e2e/support/helpers/embedding-sdk-component-testing";
-import { signInAsAdminAndEnableEmbeddingSdk } from "e2e/support/helpers/embedding-sdk-testing";
-import { mockAuthProviderAndJwtSignIn } from "e2e/support/helpers/embedding-sdk-testing/embedding-sdk-helpers";
 
 describe("SDK reproductions - sdk-question", () => {
   beforeEach(() => {
