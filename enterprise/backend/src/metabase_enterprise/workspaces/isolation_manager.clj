8b9d8a0cc19faed259464ccd5e14e577f22eb543
dan sutton <dan@dpsutton.com>
2025-08-09 14:03:26 -0500

    evluator with steps in a tree

--- enterprise/backend/src/metabase_enterprise/workspaces/isolation_manager.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/isolation_manager.clj
@@ -39,7 +39,8 @@
    [metabase.driver.sql.util :as sql.u]
    [metabase.system.core :as system]
    [metabase.util.log :as log]
-   [toucan2.core :as t2]))
+   [metabase.util.malli :as mu]
+   [metabase.util.malli.registry :as mr]))
 
 (defn- instance-uuid-slug
   "Create a slug from the site UUID, taking the first character of each section."
@@ -60,9 +61,156 @@
         clean-workspace-id (str/replace (str workspace-id) #"[^a-zA-Z0-9]" "_")]
     (format "mb_iso_%s_%s_%s" instance-slug clean-workspace-id (name user-type))))
 
+(mr/def ::rule :string)
+(mr/def ::options :map)
+(mr/def ::steps
+  [:cat :keyword ::options [:* [:alt [:schema [:ref ::steps]] ::rule]]])
+
 (defmulti ^:private create-isolation*
   "Create database isolation for a workspace."
   (fn [driver workspace] driver))
+
+;; todo: use (jdbc/metadata-result (.getSchemas metadata))
+
+(mu/defn- postgres-steps :- ::steps
+  "Return postgres steps."
+  [{:keys [populator reader schema-name]}]
+  [:schema
+   {}
+   (format "CREATE SCHEMA %s" schema-name)
+   [:users
+    {}
+    ;; Create populator user with random password
+    [:populator
+     {}
+     (format "CREATE USER %s WITH PASSWORD '%s'" (:user populator) (:password populator))
+     [:populator-privileges
+      {}
+      ;; Grant privileges to populator user
+
+      ;; source data hardcorded to public: FIX
+      (format "GRANT USAGE ON SCHEMA %s TO %s" "public" (:user populator))
+      ;; todo: don't hardcode this to public
+      (format "GRANT USAGE ON SCHEMA public TO %s" (:user populator))
+
+      (format "GRANT USAGE ON SCHEMA %s TO %s" schema-name (:user populator))
+      (format "GRANT SELECT ON ALL TABLES IN SCHEMA %s TO %s" "public" (:user populator))
+      (format "GRANT CREATE ON SCHEMA %s TO %s" schema-name (:user populator))
+      (format "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %s TO %s"
+              schema-name (:user populator))]]
+    [:reader
+     {}
+     ;; Create reader user with random password
+     (format "CREATE USER %s WITH PASSWORD '%s'" (:user reader) (:password reader))
+     [:reader-privileges
+      {}
+      ;; Grant privileges to reader user
+      (format "GRANT USAGE ON SCHEMA %s TO %s" schema-name (:user reader))
+      (format "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %s TO %s"
+              schema-name (:user reader))]]]])
+
+
+(comment
+  (postgres-steps {:populator {:user "populator" :password "populator-pw"}
+                   :reader {:user "reader" :password "reader-pw"}
+                   :schema-name "scratchpad"})
+  (malli.core/validate ::steps
+                       (postgres-steps {:populator {:user "populator" :password "populator-pw"}
+                                        :reader {:user "reader" :password "reader-pw"}
+                                        :schema-name "scratchpad"}))
+  )
+
+(mu/defn evaluate-steps
+  [steps :- ::steps f]
+  ;; todo: put options onto each rule using options from the tree?  todo: how do i want to do that? making each leaf
+  ;; more complicated, or have a stack of frames? lets do a proper interpreter that can have stack frames. can get
+  ;; parent tree rule in this manner
+  (letfn [(subtree? [rule] (vector? rule))]
+    (loop [process (into [] (reverse (nthrest steps 2)))
+           acc     [[:tree (first steps)]]
+           state   :running]
+      (let [rule (peek process)]
+        ;; done
+        (cond (not (seq process))
+              [acc state]
+
+              ;; scoot through
+              (= state :error)
+              (let [info (if (vector? rule)
+                           [:skipping-tree (first rule)]
+                           [:skipping-step rule])]
+                (recur (pop process) (conj acc info) state))
+
+              ;; hit a tree node
+              (subtree? rule)
+              ;; todo: include options onto each rule from the rule?
+              (let [[tree-name _options & sub-rules] rule]
+                (recur (into (pop process) (reverse sub-rules))
+                       (conj acc [:tree tree-name])
+                       state))
+              :else
+              (let [[status _response :as result] (f rule)]
+                (when-not (#{:error :success} status)
+                  (throw (ex-info "Bad status from rule" {:rule rule :result result})))
+                (recur (pop process)
+                       (conj acc result)
+                       (case status :success state :error :error))))))))
+
+#_(mu/defn evaluate-steps
+  [steps :- ::steps f]
+  (reduce (fn [[acc running-state] rule]
+            ;; don't continue and prune space
+            (cond (= running-state :error)
+                  (let [info (if (vector? rule)
+                               [:skipping-tree (first rule)]
+                               [:skipping-step rule])]
+                    [(conj acc info) running-state])
+
+                  ;; recursive rule. if here we are running
+                  (vector? rule)
+                  (let [[result running-state] (evaluate-steps rule f)]
+                    [(into acc result) running-state])
+
+
+                  :else
+                  (let [[status _response :as result] (f rule)]
+                    [(conj acc result)
+                     (if (= status :error) :error running-state)])))
+          [[[:tree (first steps)]] :running]
+          (drop 2 steps)))
+
+(comment
+  (evaluate-steps (postgres-steps {:populator {:user "populator" :password "populator-pw"}
+                                   :reader {:user "reader" :password "reader-pw"}
+                                   :schema-name "scratchpad"})
+                  (fn [step] (if (= 0 (rand-int 10))
+                               [:error (format "error during: %s" step)]
+                               [:success (format "performed: %s" step)])))
+
+  (evaluate-steps [:schema
+                   {}
+                   "CREATE SCHEMA scratchpad"
+                   [:users
+                    {}
+                    [:populator
+                     {}
+                     "CREATE USER populator WITH PASSWORD 'populator-pw'"
+                     [:populator-privileges
+                      {}
+                      "GRANT USAGE ON SCHEMA public TO populator"
+                      "GRANT USAGE ON SCHEMA scratchpad TO populator"
+                      ]]
+                    [:reader
+                     {}
+                     "CREATE USER reader WITH PASSWORD 'reader-pw'"
+                     [:reader-privileges
+                      {}
+                      "GRANT USAGE ON SCHEMA scratchpad TO reader"]]]]
+                  (fn [step] (if (= 0 (rand-int 10))
+                               [:error (format "error during: %s" step)]
+                               [:success (format "performed: %s" step)]))))
+
+
 
 (defmethod create-isolation* :postgres
   [_driver {:keys [id connection-details]}]
@@ -71,92 +219,105 @@
                    :password (str (random-uuid))}
         reader {:user (isolation-user-name id :reader)
                 :password (str (random-uuid))}
-        jdbc-spec (sql-jdbc.conn/connection-details->spec :postgres connection-details)]
+        jdbc-spec (sql-jdbc.conn/connection-details->spec :postgres connection-details)
+        steps (postgres-steps {:populator populator
+                               :reader reader
+                               :schema-name schema-name})]
     (jdbc/with-db-transaction [tx jdbc-spec]
       ;; Create schema
-      (jdbc/execute! tx [(format "CREATE SCHEMA %s" schema-name)])
 
-      ;; Create populator user with random password
-      (jdbc/execute! tx [(format "CREATE USER %s WITH PASSWORD '%s'"
-                                 (:user populator) (:password populator))])
-
-      ;; Create reader user with random password
-      (jdbc/execute! tx [(format "CREATE USER %s WITH PASSWORD '%s'"
-                                 (:user reader) (:password reader))])
-
-      ;; Grant privileges to populator user
-      (jdbc/execute! tx [(format "GRANT USAGE ON SCHEMA %s TO %s" schema-name (:user populator))])
-      (jdbc/execute! tx [(format "GRANT USAGE ON SCHEMA %s TO %s" "public" (:user populator))])
-      (jdbc/execute! tx [(format "GRANT SELECT ON ALL TABLES IN SCHEMA %s TO %s" "public" (:user populator))])
-      (jdbc/execute! tx [(format "GRANT CREATE ON SCHEMA %s TO %s" schema-name (:user populator))])
-      (jdbc/execute! tx [(format "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %s TO %s"
-                                 schema-name (:user populator))])
-      ;; todo: don't hardcode this to public
-      (jdbc/execute! tx [(format "GRANT USAGE ON SCHEMA public TO %s" (:user populator))])
-
-      ;; Grant privileges to reader user
-      (jdbc/execute! tx [(format "GRANT USAGE ON SCHEMA %s TO %s" schema-name (:user reader))])
-      (jdbc/execute! tx [(format "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %s TO %s"
-                                 schema-name (:user reader))])
-
-      ;; Return isolation info
-      {:isolation-type :schema
-       :schema-name schema-name
-       :populator populator
-       :reader reader})))
+      (let [results (evaluate-steps steps (fn [sql]
+                                            (try [:success
+                                                  sql
+                                                  (jdbc/execute! tx [sql])]
+                                                 (catch Exception e
+                                                   [:error sql (ex-message e)]))))]
+        ;; Return isolation info
+        {:isolation-type :schema
+         :schema-name schema-name
+         :populator populator
+         :reader reader
+         :results results}))))
 
 ;; todo: create an evaluator of steps. steps will perhaps for a dag, preconditions, checks, failures. ideally a test
 ;; would have a list of all steps, randomly execute one, and then execute all steps and ensure that we can proceed to
 ;; the end. ie, any particular step tha tis already satisfied (schema already exists, user already exists) is ok and
 ;; we can carry on.
+
+(mu/defn- clickhouse-steps :- ::steps
+  [{:keys [populator reader database-name source-database-name]}]
+  (letfn [(q [d]
+            (try
+              (sql.u/quote-name :clickhouse :database (ddl.i/format-name :clickhouse d))
+              (catch Exception e
+                (log/errorf e "Error quoting %s" (pr-str d))
+                (throw e))))]
+    [:database
+     {}
+     (format "CREATE DATABASE %s" (q database-name))
+     [:users
+      {}
+      [:populator
+       {}
+       (format "CREATE USER %s IDENTIFIED WITH sha256_password BY '%s'"
+               (:user populator) (:password populator))
+       [:populator-privileges
+        {}
+        (format "GRANT SELECT ON %s.* TO %s" (q source-database-name)  (:user populator))
+        (format "GRANT CREATE ON %s.* TO %s" (q database-name) (:user populator))
+        (format "GRANT INSERT ON %s.* TO %s" (q database-name) (:user populator))
+        (format "GRANT SELECT ON %s.* TO %s" (q database-name) (:user populator))
+        (format "GRANT DROP ON %s.* TO %s" (q database-name) (:user populator))
+        (format "GRANT SHOW DATABASES ON *.* TO %s" (:user populator))]]
+      [:reader
+       {}
+       (format "CREATE USER %s IDENTIFIED WITH sha256_password BY '%s'"
+               (:user reader) (:password reader))
+       [:reader-privileges
+        {}
+        (format "GRANT CREATE ON %s.* TO %s" (q database-name) (:user reader))
+        (format "GRANT INSERT ON %s.* TO %s" (q database-name) (:user reader))
+        (format "GRANT DROP ON %s.* TO %s" (q database-name) (:user reader))
+        (format "GRANT SELECT ON %s.* TO %s" (q database-name) (:user reader))]]]]))
+
+(comment
+  (malli.core/validate ::steps
+                       (clickhouse-steps {:populator {:user "populator" :password "populator-pw"}
+                                          :reader {:user "reader" :password "reader-pw"}
+                                          :database-name "scratchpad"
+                                          :source-database-name "foo"}))
+  )
+
 (defmethod create-isolation* :clickhouse
   [_driver {:keys [id connection-details]}]
   (let [database-name (isolation-schema-name id)
         populator {:user (isolation-user-name id :populator) :password (str (random-uuid))}
         reader {:user (isolation-user-name id :reader) :password (str (random-uuid))}
         jdbc-spec (sql-jdbc.conn/connection-details->spec :clickhouse connection-details)
-        q (fn [d]
-            (try
-             (sql.u/quote-name :clickhouse :database (ddl.i/format-name :clickhouse d))
-             (catch Exception e
-               (log/errorf e "Error quoting %s" (pr-str d))
-               (throw e))))]
+        steps (clickhouse-steps {:populator populator :reader reader :database-name database-name
+                                 :source-database-name ((some-fn :db :dbname) connection-details)})]
 
     (jdbc/with-db-transaction [tx jdbc-spec]
-      ;; Create database
-      (jdbc/execute! tx [(format "CREATE DATABASE %s" (q database-name))])
-
-      ;; Create populator user with password
-      (jdbc/execute! tx [(format "CREATE USER %s IDENTIFIED WITH sha256_password BY '%s'"
-                                 (:user populator) (:password populator))])
-
-      ;; Create reader user with password
-      (jdbc/execute! tx [(format "CREATE USER %s IDENTIFIED WITH sha256_password BY '%s'"
-                                 (:user reader) (:password reader))])
-
-      ;; Grant privileges to populator user on origin
-      (jdbc/execute! tx [(format "GRANT SELECT ON %s.* TO %s" (q ((some-fn :db :dbname) connection-details))  (:user populator))])
-
-      ;; crud on new schema
-      (jdbc/execute! tx [(format "GRANT CREATE ON %s.* TO %s" (q database-name) (:user populator))])
-      (jdbc/execute! tx [(format "GRANT INSERT ON %s.* TO %s" (q database-name) (:user populator))])
-      (jdbc/execute! tx [(format "GRANT SELECT ON %s.* TO %s" (q database-name) (:user populator))])
-      (jdbc/execute! tx [(format "GRANT DROP ON %s.* TO %s" (q database-name) (:user populator))])
-      (jdbc/execute! tx [(format "GRANT SHOW DATABASES ON *.* TO %s" (:user populator))])
-
-
-      ;; Grant privileges to reader user
-      (jdbc/execute! tx [(format "GRANT CREATE ON %s.* TO %s" (q database-name) (:user reader))])
-      (jdbc/execute! tx [(format "GRANT INSERT ON %s.* TO %s" (q database-name) (:user reader))])
-      (jdbc/execute! tx [(format "GRANT DROP ON %s.* TO %s" (q database-name) (:user reader))])
-      (jdbc/execute! tx [(format "GRANT SELECT ON %s.* TO %s" (q database-name) (:user reader))])
-
-      {:isolation-type :database
-       :database-name database-name
-       :populator populator
-       :reader reader})))
+      (let [results (evaluate-steps steps (fn [sql]
+                                            (try [:success sql (jdbc/execute! tx [sql])]
+                                                 (catch Exception e
+                                                   [:error sql (ex-message e)]))))]
+        {:isolation-type :database
+         :database-name database-name
+         :populator populator
+         :reader reader
+         :results results}))))
 
 (comment
+
+  ;; future work to check for existing schemas
+  (let [metadata (.getMetaData (:connection tx))]
+    (let [schemas (into [] (map :table_schem) (jdbc/result-set-seq (.getSchemas metadata)))]
+      (tap> {:schemas schemas
+             :isolations (filter #(String/.startsWith % "mb__isolation_") schemas)
+             :our-isolations (filter #(String/.startsWith % "mb__isolation_7748c") schemas)
+             :already-exists? (some #{schema-name} schemas)
+             :driver _driver})))
   (def workspace {:connection-details (:details (toucan2.core/select-one :model/Database :id 19))
                   :id "workspace_manual_01"})
   ((some-fn :db :dbname) (:details (toucan2.core/select-one :model/Database :id 19)))
@@ -174,18 +335,6 @@
   {:deleted-database "mb__isolation_7748c_workspace_manual_01",
    :deleted-users ["mb_iso_7748c_workspace_manual_01_populator"
                    "mb_iso_7748c_workspace_manual_01_reader"]}
-  (pst 500)
-  (defn- quote-fn [driver]
-    (sql.u/quote-name driver ident (ddl.i/format-name driver entity))
-    )
-
-  (let [driver :clickhouse entity "test-db" ident :database]
-    (sql.u/quote-name driver ident (ddl.i/format-name driver entity)))
-
-  (defn- quote-name [s]
-    (let [parts (str/split (name s) #"\.")]
-      (str/join "." (map #(str "`" % "`") parts))))
-  (ddl.i/format-name :clickhouse "test-db")
   )
 
 (defn create-isolation
@@ -225,24 +374,29 @@
                            (isolation-user-name id :populator))
         reader-user (or (-> isolation-info :reader :user)
                         (isolation-user-name id :reader))
-        jdbc-spec (sql-jdbc.conn/connection-details->spec :postgres connection-details)]
+        jdbc-spec (sql-jdbc.conn/connection-details->spec :postgres connection-details)
+        teardown-steps [:remove-user
+                        {::continue-on-error? true}
+                        [:remove-privileges
+                         {}
+                         (format "REVOKE ALL PRIVILEGES ON ALL TABLES IN  SCHEMA %s FROM %s" "public" populator-user)
+                         (format "REVOKE ALL PRIVILEGES ON SCHEMA %s FROM %s" "public" populator-user)]
+                        [:remove-new-schema
+                         {}
+                         ;; Drop schema (CASCADE to drop all objects in it).
+                         (format "DROP SCHEMA IF EXISTS %s CASCADE" schema-name)]
+                        [:drop-user
+                         {}
+                         ;; Drop users
+                         (format "DROP USER IF EXISTS %s" populator-user)
+                         (format "DROP USER IF EXISTS %s" reader-user)]]]
     (jdbc/with-db-transaction [tx jdbc-spec]
-      ;; Revoke all privileges first todo: maintain the state of what schemas the populator was granted access
-      ;; to. defaulting to public bu tthis is not feasible
-      (try (jdbc/execute! tx [(format "REVOKE ALL PRIVILEGES ON ALL TABLES IN  SCHEMA %s FROM %s" "public" populator-user)])
-           (catch Exception _))
-      (try (jdbc/execute! tx [(format "REVOKE ALL PRIVILEGES ON SCHEMA %s FROM %s" "public" populator-user)])
-           (catch Exception _))
-
-      ;; Drop schema (CASCADE to drop all objects in it).
-      (jdbc/execute! tx [(format "DROP SCHEMA IF EXISTS %s CASCADE" schema-name)])
-
-      ;; Drop users
-      (jdbc/execute! tx [(format "DROP USER IF EXISTS %s" populator-user)])
-      (jdbc/execute! tx [(format "DROP USER IF EXISTS %s" reader-user)])
-
-      {:deleted-schema schema-name
-       :deleted-users [populator-user reader-user]})))
+      (let [results (evaluate-steps teardown-steps (fn [sql]
+                                                     (try [:success sql (jdbc/execute! tx [sql])]
+                                                          (catch Exception e
+                                                            [:error sql (ex-message e)]))))]
+        {:deleted-schema schema-name
+         :deleted-users [populator-user reader-user]}))))
 
 (defmethod delete-isolation* :clickhouse
   [_driver {:keys [id connection-details isolation-info]}]
