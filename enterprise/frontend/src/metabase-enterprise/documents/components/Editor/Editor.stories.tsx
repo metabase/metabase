5c489b9c9561f4ff8f35856ad2e598db72256fa9
Nick Fitzpatrick <nickfitz.582@gmail.com>
2025-08-11 17:43:36 -0400

    Markdown Loki Test

---
+++ enterprise/frontend/src/metabase-enterprise/documents/components/Editor/Editor.stories.tsx
@@ -0,0 +1,54 @@
+import type { Store } from "@reduxjs/toolkit";
+import type { StoryFn } from "@storybook/react/*";
+import _ from "underscore";
+
+import { mockSettings } from "__support__/settings";
+import { createMockEntitiesState } from "__support__/store";
+import { Api } from "metabase/api";
+import { MetabaseReduxProvider } from "metabase/lib/redux";
+import { publicReducers } from "metabase/reducers-public";
+import { getStore } from "metabase/store";
+import type { State } from "metabase-types/store";
+import { createMockState } from "metabase-types/store/mocks";
+
+import { Editor } from "./Editor";
+import Data from "./data/data.json";
+
+const storeInitialState = createMockState({
+  settings: mockSettings(),
+  entities: createMockEntitiesState({}),
+});
+const publicReducerNames = Object.keys(publicReducers);
+const initialState = _.pick(storeInitialState, ...publicReducerNames) as State;
+const reducers = publicReducers;
+
+const storeMiddleware = [Api.middleware];
+
+const store = getStore(
+  reducers,
+  initialState,
+  storeMiddleware,
+) as unknown as Store<State>;
+
+const ReduxDecorator = (Story: StoryFn) => {
+  return (
+    <MetabaseReduxProvider store={store}>
+      <Story />
+    </MetabaseReduxProvider>
+  );
+};
+
+const DefaultTemplate = () => (
+  <Editor content={JSON.stringify(Data.markdownTest)} />
+);
+
+export default {
+  title: "Components/Documents",
+  component: Editor,
+  decorators: [ReduxDecorator],
+  layout: "fullscreen",
+};
+
+export const Default = {
+  render: DefaultTemplate,
+};
