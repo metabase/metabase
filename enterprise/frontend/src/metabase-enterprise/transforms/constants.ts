ca89043e1c33337cbfc10273af1ad180380672cb
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-08-12 14:46:06 -0400

    E2E tests

--- enterprise/frontend/src/metabase-enterprise/transforms/constants.ts
+++ enterprise/frontend/src/metabase-enterprise/transforms/constants.ts
@@ -1,3 +1,5 @@
+import { isCypressActive } from "metabase/env";
+
 export const NAME_MAX_LENGTH = 254;
 
-export const POLLING_INTERVAL = 3000;
+export const POLLING_INTERVAL = isCypressActive ? 100 : 3000;
