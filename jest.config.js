58be34efee5ba7e211ad381a5dca7b12d167e67b
Timofei <timofei@metabase.com>
2025-08-12 14:26:33 +0400

    Handle `build-configs` path in jest tests

--- jest.config.js
+++ jest.config.js
@@ -30,6 +30,7 @@ const esmPackages = [
 
 const baseConfig = {
   moduleNameMapper: {
+    "^build-configs/(.*)$": "<rootDir>/frontend/build/$1",
     "\\.(css|less)$": "<rootDir>/frontend/test/__mocks__/styleMock.js",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/frontend/test/__mocks__/fileMock.js",
