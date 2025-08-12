bf467f0b3454ae6016ebf40a0a8e0cccc3c582e6
Kamil Mielnik <kamil@kamilmielnik.com>
2025-08-12 18:38:49 +0700

    Render images

--- frontend/src/metabase/detail-view/components/DetailsGroup/DetailsGroup.tsx
+++ frontend/src/metabase/detail-view/components/DetailsGroup/DetailsGroup.tsx
@@ -1,6 +1,4 @@
 import { useMemo } from "react";
-import { Link } from "react-router";
-import { t } from "ttag";
 
 import {
   getBodyColumns,
@@ -9,10 +7,10 @@ import {
 } from "metabase/detail-view/utils";
 import { useTranslateContent } from "metabase/i18n/hooks";
 import { Group, Stack, Text, rem } from "metabase/ui";
-import { isFK } from "metabase-lib/v1/types/utils/isa";
 import type { DatasetColumn, RowValues, Table } from "metabase-types/api";
 
 import S from "./DetailsGroup.module.css";
+import { Value } from "./Value";
 
 interface Props {
   columns: DatasetColumn[];
@@ -27,14 +25,11 @@ export const DetailsGroup = ({ columns, row, table }: Props) => {
   return (
     <Stack gap="lg">
       {bodyColumns.map((column, index) => {
-        const value = getRowValue(columns, column, row);
-        const isEmptyValue = value == null || value === "";
-        const isFk = isFK(column);
         const field = table.fields?.find((f) => f.id === column.id);
-        const newTableId = field?.target?.table_id;
+        const value = getRowValue(columns, column, row);
 
         return (
-          <Group align="flex-start" gap="xl" key={index}>
+          <Group align="flex-start" gap="xl" key={index} wrap="nowrap">
             <Text
               c="text-secondary"
               className={S.name}
@@ -44,27 +39,9 @@ export const DetailsGroup = ({ columns, row, table }: Props) => {
               {column.display_name}
             </Text>
 
-            {isEmptyValue && <Text c="text-light">{t`empty`}</Text>}
-
-            {!isEmptyValue && (!isFk || newTableId == null) && (
-              <Text c="text-primary" className={S.value} flex="1" fw="bold">
-                {renderValue(tc, value, column)}
-              </Text>
-            )}
-
-            {!isEmptyValue && isFk && newTableId != null && (
-              <Text
-                c="text-primary"
-                className={S.fk}
-                component={Link}
-                fw="bold"
-                my={rem(-1)}
-                px="sm"
-                to={`/table/${newTableId}/detail/${value}`}
-              >
-                {renderValue(tc, value, column)}
-              </Text>
-            )}
+            <Value column={column} field={field} value={value}>
+              {renderValue(tc, value, column)}
+            </Value>
           </Group>
         );
       })}
