240160f60552572957283fe0fb671d058b178448
Timofey Kachalov <timofei@metabase.com>
2025-08-14 02:06:15 +0400

    Add an eslint rule that warns for external references inside the Embedding SDK NPM package code (#61417)

--- enterprise/frontend/src/embedding-sdk/sdk-package/hooks/private/use-load-sdk-bundle.ts
+++ enterprise/frontend/src/embedding-sdk/sdk-package/hooks/private/use-load-sdk-bundle.ts
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 
+// eslint-disable-next-line no-external-references-for-sdk-package-code
 import { SDK_BUNDLE_FULL_PATH } from "build-configs/embedding-sdk/constants/sdk-bundle";
 import { SDK_BUNDLE_SCRIPT_DATA_ATTRIBUTE_PASCAL_CASED } from "embedding-sdk/sdk-package/config";
 import { getSdkBundleScriptElement } from "embedding-sdk/sdk-package/lib/private/get-sdk-bundle-script-element";
