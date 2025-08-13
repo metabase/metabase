8f301877e7fb052c7eb753aac909ac5f44e2e2b7
Konstantin Darutkin <spalt08@mail.ru>
2025-08-13 14:55:55 +0300

    fix: send correct data for type/Date columns (#62095)

--- enterprise/frontend/src/metabase-enterprise/table-editing/inputs/use-date-value-without-timezone.ts
+++ enterprise/frontend/src/metabase-enterprise/table-editing/inputs/use-date-value-without-timezone.ts
@@ -1,17 +1,33 @@
 import dayjs from "dayjs";
 import { useCallback, useMemo } from "react";
 
+export enum DateValueTimezoneFormat {
+  Always,
+  Never,
+  NonZero,
+}
+
+type UseDateValueWithoutTimezoneOptions = {
+  valueFormat?: string;
+  timezoneFormat?: DateValueTimezoneFormat;
+};
+
 export const useDateValueWithoutTimezone = (
   value: string | null,
-  valueFormat: string = "YYYY-MM-DD HH:mm:ss",
+  {
+    valueFormat = "YYYY-MM-DDTHH:mm:ss",
+    timezoneFormat = DateValueTimezoneFormat.NonZero,
+  }: UseDateValueWithoutTimezoneOptions = {},
 ) => {
-  const { timezoneOffsetString, localDate } = useMemo(() => {
-    // If no value, we use browser timezone
-    const timezoneOffsetString = dayjs
-      .parseZone(value ?? new Date())
-      .format("Z");
+  const { timezoneOffsetString, timezoneOffset, localDate } = useMemo(() => {
+    // If no value, we fall back to the browser timezone
+    const parsedDate = dayjs.parseZone(value ?? new Date());
+
+    const timezoneOffset = parsedDate.utcOffset();
+    const timezoneOffsetString = parsedDate.format("Z");
 
     return {
+      timezoneOffset,
       timezoneOffsetString,
       localDate: value ? dayjs.parseZone(value).format(valueFormat) : null,
     };
@@ -23,9 +39,17 @@ export const useDateValueWithoutTimezone = (
         return null;
       }
 
+      if (
+        timezoneFormat === DateValueTimezoneFormat.Never ||
+        (timezoneFormat === DateValueTimezoneFormat.NonZero &&
+          timezoneOffset === 0)
+      ) {
+        return dayjs(value).format(valueFormat);
+      }
+
       return dayjs(value).format(valueFormat) + timezoneOffsetString;
     },
-    [valueFormat, timezoneOffsetString],
+    [valueFormat, timezoneOffsetString, timezoneOffset, timezoneFormat],
   );
 
   return { localDate, timezoneOffsetString, restoreTimezone };
