fe396740227efeeec5b78111847f6f944433f044
Timofei <timofei@metabase.com>
2025-08-13 14:07:46 +0400

    Address PR comments: simplify useMetabaseProviderPropsStore

--- enterprise/frontend/src/embedding-sdk/sdk-shared/hooks/use-metabase-provider-props-store.ts
+++ enterprise/frontend/src/embedding-sdk/sdk-shared/hooks/use-metabase-provider-props-store.ts
@@ -1,25 +1,11 @@
-import { useEffect, useRef, useSyncExternalStore } from "react";
+import { useRef, useSyncExternalStore } from "react";
 
 import { ensureMetabaseProviderPropsStore } from "../lib/ensure-metabase-provider-props-store";
 
 export function useMetabaseProviderPropsStore() {
-  const storeRef =
-    useRef<ReturnType<typeof ensureMetabaseProviderPropsStore>>();
-
-  if (!storeRef.current) {
-    storeRef.current = ensureMetabaseProviderPropsStore();
-  }
-
-  useEffect(() => {
-    // To handle strict mode case when the component is remounted, and the `storeRef.current` is cleaned up
-    if (!storeRef.current) {
-      storeRef.current = ensureMetabaseProviderPropsStore();
-    }
-
-    return () => {
-      storeRef.current = undefined;
-    };
-  }, []);
+  const storeRef = useRef<ReturnType<typeof ensureMetabaseProviderPropsStore>>(
+    ensureMetabaseProviderPropsStore(),
+  );
 
   const props = useSyncExternalStore(
     storeRef.current.subscribe,
