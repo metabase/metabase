718d09030e64873a2d12ed7287c9ff689c837250
Timofei <timofei@metabase.com>
2025-08-14 15:08:46 +0400

    Address PR comments: add prefix for global object fields with react library

--- frontend/build/embedding-sdk/constants/external-dependencies.js
+++ frontend/build/embedding-sdk/constants/external-dependencies.js
@@ -2,17 +2,17 @@
  * The SDK should never bundle React itself
  *
  * @type {Readonly<{
- *   react: "React";
- *   "react/jsx-runtime": "ReactJSXRuntime";
- *   "react-dom": "ReactDOM";
- *   "react-dom/client": "ReactDOMClient";
- *   "react-dom/server": "ReactDOMServer";
+ *   react: "METABASE_REACT";
+ *   "react/jsx-runtime": "METABASE_REACT_JSX_RUNTIME";
+ *   "react-dom": "METABASE_REACT_DOM";
+ *   "react-dom/client": "METABASE_REACT_DOM_CLIENT";
+ *   "react-dom/server": "METABASE_REACT_DOM_SERVER";
  * }>}
  */
 module.exports.EXTERNAL_DEPENDENCIES = {
-  react: "React",
-  "react/jsx-runtime": "ReactJSXRuntime",
-  "react-dom": "ReactDOM",
-  "react-dom/client": "ReactDOMClient",
-  "react-dom/server": "ReactDOMServer",
+  react: "METABASE_REACT",
+  "react/jsx-runtime": "METABASE_REACT_JSX_RUNTIME",
+  "react-dom": "METABASE_REACT_DOM",
+  "react-dom/client": "METABASE_REACT_DOM_CLIENT",
+  "react-dom/server": "METABASE_REACT_DOM_SERVER",
 };
