4da2c0e9d287b0ce6c1e21d62de59ce7bed3ad05
lbrdnk <lbrdnk@users.noreply.github.com>
2025-08-14 11:22:03 +0200

    Cleanup

--- enterprise/backend/src/metabase_enterprise/semantic_search/db/migration/impl.clj
+++ enterprise/backend/src/metabase_enterprise/semantic_search/db/migration/impl.clj
@@ -3,7 +3,8 @@
    [metabase-enterprise.semantic-search.index-metadata :as semantic.index-metadata]))
 
 (def schema-version
-  "Version to compare the [[metabase-enterprise.semantic-search.db.migration/db-version]] with."
+  "Version to compare the [[metabase-enterprise.semantic-search.db.migration/db-version]] with. If this is higher,
+  schema migration will be performed."
   0)
 
 (defn migrate-schema!
@@ -12,8 +13,10 @@
   (semantic.index-metadata/create-tables-if-not-exists! tx index-metadata)
   (semantic.index-metadata/ensure-control-row-exists! tx index-metadata))
 
-;; TODO: Maybe this will turn out superfluous. Maybe only single code version and migrate fn should be in use.
-(def dynamic-schema-version 0)
+(def dynamic-schema-version
+  "Code version of dynamic schema (index_table_xyzs). If higher than what's found in db dynamic schema migration will
+  be attempted."
+  0)
 
 (defn migrate-dynamic-schema!
   "Migrate runtime-managed schema, ie. schema of `index_table_...` tables."
