264420090dd01ef4cf75743258aa2ccda4e01546
lbrdnk <lbrdnk@users.noreply.github.com>
2025-08-14 11:30:24 +0200

    Cleanup

--- enterprise/backend/src/metabase_enterprise/semantic_search/db/util.clj
+++ enterprise/backend/src/metabase_enterprise/semantic_search/db/util.clj
@@ -1,14 +1,18 @@
-(ns ^{:clj-kondo/ignore :discouraged-var} ; jdbc/active-tx?
+(ns ^{:clj-kondo/ignore [:discouraged-var]}
  metabase-enterprise.semantic-search.db.util
   (:require
    [next.jdbc :as jdbc]))
 
+(set! *warn-on-reflection* true)
+
 (defn tx-or-throw!
+  "Throw if not in transaction."
   [conn]
   (when-not (jdbc/active-tx? (.unwrap ^java.sql.Connection conn java.sql.Connection))
     (throw (Exception. "Not in transaction."))))
 
-(defn not-tx-or-throw!
-  [conn]
-  (when (jdbc/active-tx? (.unwrap ^java.sql.Connection conn java.sql.Connection))
-    (throw (Exception. "In transaction."))))
+;; Unused now, may be handy for with-write-tx, acquire-write-lock
+#_(defn not-tx-or-throw!
+    [conn]
+    (when (jdbc/active-tx? (.unwrap ^java.sql.Connection conn java.sql.Connection))
+      (throw (Exception. "In transaction."))))
