bd6ac2eb0dbdba90b6ce993a74a950aafde761a2
lbrdnk <lbrdnk@users.noreply.github.com>
2025-08-14 20:20:20 +0200

    migration 1

--- enterprise/backend/src/metabase_enterprise/semantic_search/db/migration/impl.clj
+++ enterprise/backend/src/metabase_enterprise/semantic_search/db/migration/impl.clj
@@ -1,16 +1,35 @@
 (ns metabase-enterprise.semantic-search.db.migration.impl
   (:require
-   [metabase-enterprise.semantic-search.index-metadata :as semantic.index-metadata]))
+   [honey.sql :as sql]
+   [metabase-enterprise.semantic-search.index-metadata :as semantic.index-metadata]
+   [next.jdbc :as jdbc]))
 
 (def schema-version
   "Version to compare the [[metabase-enterprise.semantic-search.db.migration/db-version]] with. If this is higher,
   schema migration will be performed."
-  0)
+  1)
+
+(defn- drop-all-but-migration-table
+  [tx]
+  (let [table-names (map (comp first vals)
+                         (jdbc/execute! tx
+                                        (sql/format
+                                         {:select [[:tablename :xi]]
+                                          :from [:pg_tables]
+                                          :where [:and
+                                                  [:<> :schemaname [:inline "information_schema"]]
+                                                  [:<> :schemaname [:inline "pg_catalog"]]
+                                                  [:<> :tablename  [:inline "migration"]]]})))]
+    (doseq [table table-names]
+      (sql/format
+       {:drop-table  [[[:raw table]]]}))))
 
 (defn migrate-schema!
   "Migrate schema (control, metadata, gate, ...). Migration author is responsible for removing leftovers if necessary
   and in general leaving schema in desired state."
   [tx {:keys [index-metadata] :as _opts}]
+  ;; ideally index_table tables are manipulated in dynamic schema part but for now it does not matter
+  (drop-all-but-migration-table tx)
   (semantic.index-metadata/create-tables-if-not-exists! tx index-metadata)
   (semantic.index-metadata/ensure-control-row-exists! tx index-metadata))
 
