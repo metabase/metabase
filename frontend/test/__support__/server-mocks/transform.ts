8cf2f088c2be41199f3b667cafbfa988cc264904
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-08-14 12:09:40 -0400

    Migration fix?

--- frontend/test/__support__/server-mocks/transform.ts
+++ frontend/test/__support__/server-mocks/transform.ts
@@ -10,6 +10,14 @@ export function setupCreateTransformJobEndpoint(job: TransformJob) {
   fetchMock.post("path:/api/ee/transform-job", job);
 }
 
+export function setupUpdateTransformJobEndpoint(job: TransformJob) {
+  fetchMock.put(`path:/api/ee/transform-job/${job.id}`, job);
+}
+
+export function setupRunTransformJobEndpoint(jobId: TransformJobId) {
+  fetchMock.post(`path:/api/ee/transform-job/${jobId}/run`, 200);
+}
+
 export function setupDeleteTransformJobEndpoint(jobId: TransformJobId) {
   fetchMock.delete(`path:/api/ee/transform-job/${jobId}`, 200);
 }
