897a2553098c14cb8f64390e3f87ba1352fecc9f
Kamil Mielnik <kamil@kamilmielnik.com>
2025-08-14 20:56:05 +0700

    Remove arrow navigation and footer

--- frontend/src/metabase/detail-view/pages/DetailView/DetailView.tsx
+++ frontend/src/metabase/detail-view/pages/DetailView/DetailView.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { push } from "react-router-redux";
+import { useEffect, useMemo } from "react";
 import { useUnmount } from "react-use";
 
 import { skipToken } from "metabase/api/api";
@@ -11,7 +10,6 @@ import {
 import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper/LoadingAndErrorWrapper";
 import {
   DetailsGroup,
-  Footer,
   Header,
   Relationships,
 } from "metabase/detail-view/components";
@@ -24,7 +22,7 @@ import type { StructuredDatasetQuery } from "metabase-types/api";
 
 import S from "./DetailView.module.css";
 import { DETAIL_VIEW_PADDING_LEFT } from "./constants";
-import { getObjectQuery, getTableQuery } from "./utils";
+import { getObjectQuery } from "./utils";
 
 interface Props {
   params: {
@@ -41,99 +39,22 @@ export function DetailView({ params }: Props) {
   const { data: table } = useGetTableQueryMetadataQuery({ id: tableId });
   const { data: tableForeignKeys } = useListTableForeignKeysQuery(tableId);
 
-  const query = useMemo<StructuredDatasetQuery | undefined>(() => {
-    return table ? getTableQuery(table) : undefined;
-  }, [table]);
-
   const objectQuery = useMemo<StructuredDatasetQuery | undefined>(() => {
     return table ? getObjectQuery(table, rowId) : undefined;
   }, [table, rowId]);
 
-  const { data: dataset } = useGetAdhocQueryQuery(query ? query : skipToken);
+  const { data: dataset } = useGetAdhocQueryQuery(
+    objectQuery ? objectQuery : skipToken,
+  );
+
   const data = useMemo(() => {
     return dataset ? extractRemappedColumns(dataset.data) : undefined;
   }, [dataset]);
 
-  const [currentRowIndex, setCurrentRowIndex] = useState<number>();
-
-  const rows = useMemo(() => data?.rows ?? [], [data]);
   const columns = useMemo(() => data?.results_metadata.columns ?? [], [data]);
   const headerColumns = useMemo(() => getHeaderColumns(columns), [columns]);
-
-  const rowFromList =
-    typeof currentRowIndex === "undefined" ? undefined : rows[currentRowIndex];
-
-  const { data: objectDataset } = useGetAdhocQueryQuery(
-    objectQuery ? objectQuery : skipToken,
-  );
-  const rowFromObject = useMemo(
-    () => (objectDataset?.data?.rows ?? [])[0],
-    [objectDataset],
-  );
-
-  const row = rowFromList ?? rowFromObject;
+  const row = useMemo(() => (data?.rows ?? [])[0], [data]);
   const rowName = getRowName(columns, row) || rowId;
-
-  const handleViewPreviousObjectDetail = useCallback(() => {
-    setCurrentRowIndex((currentIndex) => {
-      if (typeof currentIndex !== "number") {
-        return currentIndex;
-      }
-
-      const newIndex = currentIndex - 1;
-      const rowId = rows[newIndex]?.[0];
-
-      if (rowId !== undefined) {
-        dispatch(push(`/table/${tableId}/detail/${rowId}`));
-      }
-
-      return newIndex;
-    });
-  }, [rows, dispatch, tableId]);
-
-  const handleViewNextObjectDetail = useCallback(() => {
-    setCurrentRowIndex((currentIndex) => {
-      if (typeof currentIndex !== "number") {
-        return currentIndex;
-      }
-
-      const newIndex = currentIndex + 1;
-      const rowId = rows[newIndex]?.[0];
-
-      if (rowId !== undefined) {
-        dispatch(push(`/table/${tableId}/detail/${rowId}`));
-      }
-
-      return newIndex;
-    });
-  }, [rows, dispatch, tableId]);
-
-  const handlePreviousClick =
-    rows.length > 1 &&
-    typeof currentRowIndex === "number" &&
-    currentRowIndex > 0
-      ? handleViewPreviousObjectDetail
-      : undefined;
-
-  const handleNextClick =
-    rows.length > 1 &&
-    typeof currentRowIndex === "number" &&
-    currentRowIndex < rows.length - 1
-      ? handleViewNextObjectDetail
-      : undefined;
-
-  useEffect(() => {
-    if (!row) {
-      return;
-    }
-
-    if (rowId !== undefined) {
-      const idx = rows.findIndex((row) => String(row[0]) === String(rowId));
-      setCurrentRowIndex(idx >= 0 ? idx : undefined);
-    } else {
-      setCurrentRowIndex(undefined);
-    }
-  }, [rowId, rows, row]);
 
   useEffect(() => {
     dispatch(closeNavbar());
@@ -154,74 +75,52 @@ export function DetailView({ params }: Props) {
   }
 
   return (
-    <Stack bg="var(--mb-color-background-light)" gap={0} h="100%">
-      <Stack className={S.scrollable} gap={0} mih={0} h="100%">
-        {headerColumns.length > 0 && (
+    <Stack bg="var(--mb-color-background-light)" gap={0} mih="100%">
+      {headerColumns.length > 0 && (
+        <Box
+          bg="bg-white"
+          className={S.header}
+          pl={rem(DETAIL_VIEW_PADDING_LEFT)}
+          pr="xl"
+          py={rem(64)}
+        >
           <Box
-            bg="bg-white"
-            className={S.header}
-            pl={rem(DETAIL_VIEW_PADDING_LEFT)}
-            pr="xl"
-            py={rem(64)}
+            // intentionally misalign the header to create an "optical alignment effect" (due to rounded avatar)
+            ml={rem(-8)}
           >
-            <Box
-              // intentionally misalign the header to create an "optical alignment effect" (due to rounded avatar)
-              ml={rem(-8)}
-            >
-              <Header columns={columns} row={row} table={table} />
-            </Box>
+            <Header columns={columns} row={row} table={table} />
           </Box>
-        )}
+        </Box>
+      )}
 
+      <Group align="stretch" flex="1" gap={0} key={rowId} mih={0} wrap="nowrap">
         <Group
           align="flex-start"
+          bg="bg-white"
           flex="1"
-          gap={0}
-          key={rowId}
-          mih={0}
-          wrap="nowrap"
+          p="xl"
+          pl={rem(DETAIL_VIEW_PADDING_LEFT)}
         >
-          <Group
-            align="flex-start"
-            mih="100%"
-            flex="1"
-            bg="bg-white"
-            p="xl"
-            pl={rem(DETAIL_VIEW_PADDING_LEFT)}
-          >
-            <Stack gap={rem(64)} maw={rem(900)} w="100%">
-              {columns.length - headerColumns.length > 0 && (
-                <DetailsGroup columns={columns} row={row} table={table} />
-              )}
-            </Stack>
-          </Group>
-
-          {tableForeignKeys && tableForeignKeys.length > 0 && (
-            <Box flex="0 0 auto" px={rem(40)} py="xl" w={rem(440)}>
-              <Relationships
-                columns={columns}
-                row={row}
-                rowId={rowId}
-                rowName={rowName}
-                table={table}
-                tableForeignKeys={tableForeignKeys}
-              />
-            </Box>
-          )}
+          <Stack gap={rem(64)} h="100%" maw={rem(900)} w="100%">
+            {columns.length - headerColumns.length > 0 && (
+              <DetailsGroup columns={columns} row={row} table={table} />
+            )}
+          </Stack>
         </Group>
-      </Stack>
 
-      {typeof currentRowIndex === "number" &&
-        typeof dataset?.row_count === "number" && (
-          <Box bg="bg-white" className={S.footer} flex="0 0 auto">
-            <Footer
-              index={currentRowIndex}
-              rowsCount={dataset.row_count}
-              onNextClick={handleNextClick}
-              onPreviousClick={handlePreviousClick}
+        {tableForeignKeys && tableForeignKeys.length > 0 && (
+          <Box flex="0 0 auto" px={rem(40)} py="xl" w={rem(440)}>
+            <Relationships
+              columns={columns}
+              row={row}
+              rowId={rowId}
+              rowName={rowName}
+              table={table}
+              tableForeignKeys={tableForeignKeys}
             />
           </Box>
         )}
+      </Group>
     </Stack>
   );
 }
