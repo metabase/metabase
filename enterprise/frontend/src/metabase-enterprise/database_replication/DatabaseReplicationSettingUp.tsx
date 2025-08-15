f0fa135fc6d81eda5fb515ff9bb7d4036b88017a
losrebellos <ben@benoitvinay.com>
2025-08-15 13:27:19 +0200

    More on setting up and success layout

--- enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationSettingUp.tsx
+++ enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationSettingUp.tsx
@@ -1,19 +1,24 @@
 import { t } from "ttag";
 
-import { Box, Stack, Text, Title } from "metabase/ui";
+import { Box, Loader, Stack, Text, Title } from "metabase/ui";
 
 export const DatabaseReplicationSettingUp = () => (
-  <Stack align="center" my="6rem">
-    <Box h={96} w={96}>
+  <Stack align="center" gap="lg" my="6rem">
+    <Box h={96} pos="relative" w={96}>
       <img src="app/assets/img/metabot-cloud-96x96.svg" alt="Metabot Cloud" />
+
+      <Box bottom={0} pos="absolute" right={0}>
+        <Loader size={32} />
+      </Box>
     </Box>
 
-    <Stack align="center">
+    <Box ta="center">
       <Title c="text-primary" fz="lg">{t`Setting up, please wait`}</Title>
       <Text
         c="text-secondary"
         fz="md"
+        maw={360}
       >{t`This will take just a minute or so`}</Text>
-    </Stack>
+    </Box>
   </Stack>
 );
