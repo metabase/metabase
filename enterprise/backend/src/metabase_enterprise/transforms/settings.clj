2abf4105fa78cbca1b68d0abd2609ebc434a99f8
Eric Normand <ericwnormand@gmail.com>
2025-08-15 12:41:26 -0500

    Remove transforms-seeded setting

--- enterprise/backend/src/metabase_enterprise/transforms/settings.clj
+++ enterprise/backend/src/metabase_enterprise/transforms/settings.clj
@@ -16,12 +16,3 @@
   :encryption :no
   :audit      :getter)
 
-(setting/defsetting transforms-seeded
-  (deferred-tru "Indicates whether default transform tags and jobs have been seeded.")
-  :type       :boolean
-  :default    false
-  :visibility :internal
-  :doc        false
-  :export?    false
-  :encryption :no
-  :audit      :getter)
