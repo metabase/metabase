75ec00c60a4d0b8611615bcce3fa2c627a5ded3c
Timofei <timofei@metabase.com>
2025-08-16 02:43:55 +0400

    Update styles

--- enterprise/frontend/src/embedding-sdk/sdk-package/components/public/debug/SdkDebugInfo/SdkDebugInfo.stories.tsx
+++ enterprise/frontend/src/embedding-sdk/sdk-package/components/public/debug/SdkDebugInfo/SdkDebugInfo.stories.tsx
@@ -15,8 +15,23 @@ export default {
   decorators: [getHostedBundleStoryDecorator()],
 };
 
+const checkSizePx = 40;
 export const Default = () => (
-  <MetabaseProvider authConfig={config}>
-    <SdkDebugInfo />
-  </MetabaseProvider>
+  <div
+    style={{
+      width: "100vw",
+      height: "100vh",
+      backgroundImage:
+        "linear-gradient(45deg, #ccc 26%, transparent 26%)," +
+        "linear-gradient(135deg, #ccc 26%, transparent 26%)," +
+        "linear-gradient(45deg, transparent 75%, #ccc 75%)," +
+        "linear-gradient(135deg, transparent 75%, #ccc 75%)",
+      backgroundSize: `${checkSizePx * 2}px ${checkSizePx * 2}px`,
+      backgroundPosition: `0 0, ${checkSizePx}px 0, ${checkSizePx}px -${checkSizePx}px, 0px ${checkSizePx}px`,
+    }}
+  >
+    <MetabaseProvider authConfig={config}>
+      <SdkDebugInfo />
+    </MetabaseProvider>
+  </div>
 );
