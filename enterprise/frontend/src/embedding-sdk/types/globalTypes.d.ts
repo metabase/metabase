63b2bfba8e25bbdc4ce216aa4f4ebddf9381a84c
Timofei <timofei@metabase.com>
2025-08-15 19:52:50 +0400

    Remove some comments

--- enterprise/frontend/src/embedding-sdk/types/globalTypes.d.ts
+++ enterprise/frontend/src/embedding-sdk/types/globalTypes.d.ts
@@ -6,11 +6,7 @@ interface Window {
   ReactDOMClient?: unknown;
   ReactDOMServer?: unknown;
 
-  // Metabase Embedding SDK from Hosted Bundle
   MetabaseEmbeddingSDK?: import("embedding-sdk/types/sdk-bundle").MetabaseEmbeddingSdkBundleExports;
-
   METABASE_PROVIDER_PROPS_STORE?: import("embedding-sdk/sdk-shared/lib/ensure-metabase-provider-props-store").MetabaseProviderPropsStore;
-
-  // Build info
   METABASE_EMBEDDING_SDK_PACKAGE_BUILD_INFO?: import("metabase/embedding-sdk/types/build-info").BuildInfo;
 }
