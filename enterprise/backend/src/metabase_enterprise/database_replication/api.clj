bc812c6d5790a74e7d6d2299b7227b0395b8d1eb
Filipe Silva <filipematossilva@gmail.com>
2025-08-15 16:50:45 -0400

    feat: check replication table ownership
    Same as how HM does it.

--- enterprise/backend/src/metabase_enterprise/database_replication/api.clj
+++ enterprise/backend/src/metabase_enterprise/database_replication/api.clj
@@ -1,6 +1,7 @@
 (ns metabase-enterprise.database-replication.api
   (:require
-   [clojure.set :as set]
+   [clojure.core.memoize :as memoize]
+   [clojure.java.jdbc :as jdbc]
    [clojure.string :as str]
    [medley.core :as m]
    [metabase-enterprise.database-replication.settings :as database-replication.settings]
@@ -62,44 +63,109 @@
       exclude-pattern
       exclude-fn)))
 
-(def ^:private database->user
-  (comp :user :details))
+(defn find-tables
+  "Note: copied from harbormaster.connections.sql/find-tables. Keep in sync.
+  Returns table list filtered by schemas, including row count, ownership, and super user information.
+  Postgres specific."
+  [{:as conn, :keys [schema-filters]}]
+  (->>
+   "
+SELECT
+    c.relname AS table_name,
+    n.nspname AS table_schema,
+    c.relowner = r.oid AS is_owner,
+    (r.rolinherit AND EXISTS (
+        SELECT 1 FROM pg_auth_members am
+        WHERE am.member = r.oid AND am.roleid = c.relowner
+    )) AS inherits_owner,
+    r.rolsuper AS is_superuser,
+    EXISTS (
+        SELECT 1 FROM pg_auth_members am
+        JOIN pg_roles super_role ON super_role.oid = am.roleid
+        WHERE am.member = r.oid
+          AND (super_role.rolsuper = true OR super_role.rolname = 'rds_superuser')
+    ) AS inherits_superuser,
+    EXISTS (
+        SELECT 1 FROM pg_constraint con
+        WHERE con.conrelid = c.oid
+          AND con.contype = 'p'
+    ) AS has_pkey,
+    quote_ident(n.nspname) || '.' || quote_ident(c.relname) AS full_name,
+    pg_total_relation_size(quote_ident(n.nspname) || '.' || quote_ident(c.relname)) AS size,
+    COALESCE(s.n_live_tup, 0) AS estimated_row_count
+FROM
+    pg_catalog.pg_class c
+    JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+    JOIN pg_catalog.pg_roles r ON r.rolname = current_user
+    LEFT JOIN pg_stat_user_tables s ON s.relid = c.oid
+WHERE
+    c.relkind = 'r'  -- regular tables only
+    AND n.nspname NOT IN ('pg_catalog', 'information_schema')
+    AND has_schema_privilege(current_user, n.nspname, 'USAGE')
+ORDER BY
+    n.nspname,
+    c.relname;"
+   (jdbc/query (:credentials conn))
+   (filter (comp (schema-filters->fn schema-filters) :table_schema))
+   (map #(assoc % :has_ownership (boolean (some % [:is_superuser :inherits_superuser :is_owner :inherits_owner]))))
+   (sort-by :size)))
+
+(def ^:private find-tables-memo
+  (memoize/ttl find-tables :ttl/threshold (u/minutes->ms 5)))
 
 (defn- token-check-quotas-info
   "Predicate that signals if replication looks right from the quota perspective.
 
    This predicate checks that the quotas we got from the latest tokencheck have enough space for the database to be
   replicated."
-  [database schema-filters]
-  (let [connected-user             (database->user database)
-        all-quotas                 (quotas)
+  [{:as secret, :keys [schema-filters]}]
+  (let [all-quotas                 (quotas)
         free-quota                 (or
                                     (some->> (m/find-first (comp #{"clickhouse-dwh"} :hosting-feature) all-quotas)
                                              ((juxt :soft-limit :usage))
                                              (apply -))
                                     -1)
-        filter-schema              (schema-filters->fn schema-filters)
-        db-tables                  (some->> (t2/hydrate database [:tables :fields]) :tables) ; sanitized name
-        sanitized-tables           (some->> db-tables
-                                            (filter (comp (fn [x] (re-matches #"^[A-Za-z0-9_]+$" x)) :name))
-                                            (filter (comp filter-schema :schema)))
-        tables-without-pk          (set (remove (fn [t] (boolean (some (comp #{:type/PK} :semantic_type) (:fields t)))) sanitized-tables))
-        ;; https://www.postgresql.org/docs/17/ddl-priv.html
-        tables-without-owner-match (set (remove (comp #{connected-user} :owner) sanitized-tables))
+        all-tables                 (->> (dissoc secret :schema-filters)
+                                        find-tables-memo
+                                        ;; memo the slow query, then filter schemas over the memoized result
+                                        (filter (comp (schema-filters->fn schema-filters) :table_schema)))
+        replicated-tables          (->> all-tables
+                                        (filter :has_pkey)
+                                        (filter :has_ownership))
+        tables-without-pk          (filter (comp not :has_pkey) all-tables)
+        tables-without-owner-match (filter (comp not :has_ownership) all-tables)
         total-estimated-row-count  (or
                                     (some->>
-                                     (set/difference (set sanitized-tables) tables-without-pk tables-without-owner-match)
+                                     replicated-tables
                                      (map :estimated_row_count)
                                      (remove nil?)
                                      (reduce +))
-                                    0)]
+                                    0)
+        map-ks-fn                  #(-> (m/map-keys {:table_schema :schema, :table_name :name} %)
+                                        (select-keys [:schema :name]))]
     (log/infof "Quota left: %s. Estimate db row count: %s" free-quota total-estimated-row-count)
     {:free-quota                 free-quota
      :total-estimated-row-count  total-estimated-row-count
      :can-set-replication        (< total-estimated-row-count free-quota)
      :all-quotas                 all-quotas
-     :tables-without-pk          (map #(select-keys % [:schema :name]) tables-without-pk)
-     :tables-without-owner-match (map #(select-keys % [:schema :name]) tables-without-owner-match)}))
+     :all-tables                 all-tables
+     :replicated-tables          replicated-tables
+     :tables-without-pk          (map map-ks-fn tables-without-pk)
+     :tables-without-owner-match (map map-ks-fn tables-without-owner-match)}))
+
+(defn- ->secret [database replication-schema-filters]
+  (let [credentials    (-> (:details database)
+                           (select-keys [:dbname :host :user :password :port])
+                           (update :port #(or % 5432)) ;; port is required in the API, but optional in MB
+                           (merge {:dbtype "postgresql"}))
+        {:keys [schema-filters-type schema-filters-patterns]} (:details database)
+        schema-filters (-> (when-some [k ({"inclusion" :include, "exclusion" :exclude} schema-filters-type)]
+                             (for [pattern (str/split schema-filters-patterns #",")]
+                               {:type k :pattern pattern}))
+                           (concat (map #(select-keys % [:type :pattern]) replication-schema-filters))
+                           not-empty)]
+    {:credentials    credentials
+     :schema-filters schema-filters}))
 
 (api.macros/defendpoint :post "/connection/:database-id/preview"
   "Return info about pg-replication connection that is about to be created."
@@ -110,15 +176,9 @@
                                                                  [:map
                                                                   [:type [:enum "include" "exclude"]]
                                                                   [:pattern :string]]]]]]
-  (u/recursive-map-keys u/->camelCaseEn (token-check-quotas-info (t2/select-one :model/Database :id database-id) schemaFilters)))
-
-(defn can-set-replication?
-  "Predicate that signals if replication looks right from the quota perspective.
-
-   This predicate checks that the quotas we got from the latest tokencheck have enough space for the database to be
-  replicated."
-  [database schemas]
-  (:can-set-replication (token-check-quotas-info database schemas)))
+  (let [database (t2/select-one :model/Database :id database-id)
+        secret (->secret database schemaFilters)]
+    (u/recursive-map-keys u/->camelCaseEn (token-check-quotas-info secret))))
 
 (api.macros/defendpoint :post "/connection/:database-id"
   "Create a new PG replication connection for the specified database."
@@ -136,18 +196,8 @@
     (api/check-400 (= :postgres (:engine database)) "PG replication is only supported for PostgreSQL databases.")
     (let [conns (pruned-database-replication-connections)]
       (api/check-400 (not (database-id->connection-id conns database-id)) "Database already has an active replication connection.")
-      (let [credentials    (-> (:details database)
-                               (select-keys [:dbname :host :user :password :port])
-                               (update :port #(or % 5432))) ;; port is required in the API, but optional in MB
-            {:keys [schema-filters-type schema-filters-patterns]} (:details database)
-            schema-filters (-> (when-some [k ({"inclusion" :include, "exclusion" :exclude} schema-filters-type)]
-                                 (for [pattern (str/split schema-filters-patterns #",")]
-                                   {:type k :pattern pattern}))
-                               (concat (map #(select-keys % [:type :pattern]) schemaFilters))
-                               not-empty)
-            secret         {:credentials    (merge {:dbtype "postgresql"} credentials)
-                            :schema-filters schema-filters}]
-        (if (can-set-replication? database schema-filters)
+      (let [secret (->secret database schemaFilters)]
+        (if (:can-set-replication (token-check-quotas-info secret))
           (let [{:keys [id]} (hm.client/call :create-connection, :type "pg_replication", :secret secret)
                 conn         {:connection-id id}]
             (database-replication.settings/database-replication-connections! (assoc conns (kw-id database-id) conn))
