2763425403f2e83f69e4672fb5580406de78afce
losrebellos <ben@benoitvinay.com>
2025-08-18 15:46:56 +0200

    Error modal UI updates

--- enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationError.tsx
+++ enterprise/frontend/src/metabase-enterprise/database_replication/DatabaseReplicationError.tsx
@@ -3,7 +3,7 @@ import { t } from "ttag";
 import ExternalLink from "metabase/common/components/ExternalLink";
 import { useSelector } from "metabase/lib/redux";
 import { getShowMetabaseLinks } from "metabase/selectors/whitelabel";
-import { Box, Button, Flex, Group, Stack, Text, Title } from "metabase/ui";
+import { Button, Flex, Group, Icon, Stack, Text } from "metabase/ui";
 
 export const DatabaseReplicationError = ({
   error,
@@ -13,24 +13,29 @@ export const DatabaseReplicationError = ({
   onClose: () => void;
 }) => {
   const showMetabaseLinks = useSelector(getShowMetabaseLinks);
+
   return (
     <Stack align="center" gap="lg" my="4.5rem">
-      <Box ta="center">
-        <Title c="text-primary" fz="lg">{t`Couldn't replicate database`}</Title>
-        <Text c="text-secondary" fz="md" lh={1.43}>
-          {error ?? "Unknown error"}
-        </Text>
-      </Box>
+      <Text c="text-secondary" fz="md" lh={1.43}>
+        {error ?? "Unknown error"}
+      </Text>
 
       <Flex justify="end">
         <Group align="center">
           {showMetabaseLinks && (
-            <ExternalLink
+            <Button
+              component={ExternalLink}
+              variant="outline"
+              rightSection={<Icon name="external" />}
+              size="md"
               role="link"
               href="https://www.metabase.com/docs/latest/"
-            >{t`Read the docs`}</ExternalLink>
+            >
+              {t`Read the docs`}
+            </Button>
           )}
-          <Button onClick={onClose} size="md" variant="filled" miw="30%">
+
+          <Button variant="filled" size="md" onClick={onClose}>
             {t`Close`}
           </Button>
         </Group>
