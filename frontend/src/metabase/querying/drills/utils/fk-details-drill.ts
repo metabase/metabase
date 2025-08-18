2ffdd549d68f9a4e62a76e6da603033407e67bef
Kamil Mielnik <kamil@kamilmielnik.com>
2025-08-18 21:45:26 +0700

    Make FK drill take user to the new page

--- frontend/src/metabase/querying/drills/utils/fk-details-drill.ts
+++ frontend/src/metabase/querying/drills/utils/fk-details-drill.ts
@@ -1,14 +1,25 @@
+import { push } from "react-router-redux";
 import { t } from "ttag";
 
 import type { Drill } from "metabase/visualizations/types/click-actions";
 import type * as Lib from "metabase-lib";
+import type { Dispatch } from "metabase-types/store";
 
 export const fkDetailsDrill: Drill<Lib.FKDetailsDrillThruInfo> = ({
   drill,
   drillInfo,
   applyDrill,
+  clicked,
+  question,
 }) => {
+  const metadata = question.metadata();
   const { objectId, isManyPks } = drillInfo;
+
+  const targetField =
+    typeof clicked.column?.id === "number"
+      ? metadata.fields[clicked.column.id].target
+      : undefined;
+  const targetTableId = targetField?.table_id;
 
   return [
     {
@@ -18,7 +29,13 @@ export const fkDetailsDrill: Drill<Lib.FKDetailsDrillThruInfo> = ({
       buttonType: "horizontal",
       icon: "expand",
       default: true,
-      question: () => applyDrill(drill, objectId).setDefaultDisplay(),
+      ...(targetTableId == null
+        ? { question: () => applyDrill(drill, objectId).setDefaultDisplay() }
+        : {
+            action: () => (dispatch: Dispatch) => {
+              dispatch(push(`/table/${targetTableId}/detail/${objectId}`));
+            },
+          }),
       ...(!isManyPks ? { extra: () => ({ objectId }) } : {}),
     },
   ];
