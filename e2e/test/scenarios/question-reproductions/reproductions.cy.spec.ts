2a1106c2ca0258b14c091eb38b732482a17158c0
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-08-18 15:39:06 -0400

    e2e repro

--- e2e/test/scenarios/question-reproductions/reproductions.cy.spec.ts
+++ e2e/test/scenarios/question-reproductions/reproductions.cy.spec.ts
@@ -953,6 +953,12 @@ describe("issue 55631", () => {
   });
 });
 
+describe("issue 58371", () => {
+  it("should allow expression names with hyphens (metabase#58371)", () => {
+    cy.log("TBD");
+  });
+});
+
 describe.skip("issue 39033", () => {
   const question1Name = "Q1";
   const question1Details: NativeQuestionDetails = {
