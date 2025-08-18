6262230b70791d2580d74bd2cf6a57c6c3125c48
Konstantin Darutkin <spalt08@mail.ru>
2025-08-18 15:13:12 +0300

    fix: update expand row icon (#62268)
    * fix: update expand row icon
    
    * fix: review fixes

--- enterprise/frontend/src/metabase-enterprise/table-editing/table-edit/data-grid/use-table-column-row-select.tsx
+++ enterprise/frontend/src/metabase-enterprise/table-editing/table-edit/data-grid/use-table-column-row-select.tsx
@@ -1,31 +1,41 @@
 import type { Row, Table } from "@tanstack/react-table";
 import { useMemo } from "react";
 
-import type { ColumnOptions } from "metabase/data-grid";
-import { Checkbox, Flex, rem } from "metabase/ui";
+import { BaseCell, type ColumnOptions } from "metabase/data-grid";
+import { Checkbox, Flex, Group, Icon, Tooltip, rem } from "metabase/ui";
 import type { RowValue, RowValues } from "metabase-types/api";
+
+import S from "./EditTableDataGrid.module.css";
 
 export const ROW_SELECT_COLUMN_ID = "__MB_ROW_SELECT";
 
-export function useTableColumnRowSelect(hasRowSelection: boolean) {
+type UseTableColumnRowSelectProps = {
+  onRowEditClick: (rowIndex: number) => void;
+};
+
+export function useTableColumnRowSelect({
+  onRowEditClick,
+}: UseTableColumnRowSelectProps) {
   return useMemo<ColumnOptions<RowValues, RowValue> | undefined>(
-    () => (hasRowSelection ? getRowSelectColumn() : undefined),
-    [hasRowSelection],
+    () => getRowSelectColumn({ onRowEditClick }),
+    [onRowEditClick],
   );
 }
 
-export function getRowSelectColumn() {
+export function getRowSelectColumn({
+  onRowEditClick,
+}: UseTableColumnRowSelectProps) {
   return {
     id: ROW_SELECT_COLUMN_ID,
     name: "",
     accessorFn: () => null,
     header: ({ table }: { table: Table<RowValues> }) => (
       <Flex
-        p=".5rem"
+        p="0.75rem"
         h="100%"
         align="center"
-        justify="center"
         bg="var(--cell-bg-color)"
+        className={S.tableHeaderCell}
       >
         <Checkbox
           size={rem(16)}
@@ -42,21 +52,31 @@ export function getRowSelectColumn() {
       </Flex>
     ),
     cell: ({ row }: { row: Row<RowValues> }) => (
-      <Flex p=".5rem" h="100%" align="center" justify="center">
-        <Checkbox
-          size={rem(16)}
-          checked={row.getIsSelected()}
-          disabled={!row.getCanSelect()}
-          indeterminate={row.getIsSomeSelected()}
-          onChange={row.getToggleSelectedHandler()}
-          data-testid="row-select-checkbox"
-          styles={{
-            input: {
-              borderColor: "var(--mb-color-border)",
-            },
-          }}
-        />
-      </Flex>
+      <BaseCell>
+        <Group align="center" justify="flex-start" h="100%">
+          <Checkbox
+            size={rem(16)}
+            checked={row.getIsSelected()}
+            disabled={!row.getCanSelect()}
+            indeterminate={row.getIsSomeSelected()}
+            onChange={row.getToggleSelectedHandler()}
+            data-testid="row-select-checkbox"
+            styles={{
+              input: {
+                borderColor: "var(--mb-color-border)",
+              },
+            }}
+          />
+
+          <Tooltip label="Edit record">
+            <Icon
+              name="pencil"
+              className={S.pencilIcon}
+              onClick={() => onRowEditClick(row.index)}
+            />
+          </Tooltip>
+        </Group>
+      </BaseCell>
     ),
   };
 }
