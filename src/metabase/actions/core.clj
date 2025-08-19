4d5299a9a04de240d2b2ba26d9c466765b247859
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-08-19 12:46:58 +0300

    Better error messages for actions that violate custom constraints (#62271) (#62329)
    Co-authored-by: Chris Truter <crisptrutski@users.noreply.github.com>

--- src/metabase/actions/core.clj
+++ src/metabase/actions/core.clj
@@ -39,6 +39,7 @@
   validate-inputs!]
  [metabase.actions.error
   incorrect-value-type
+  violate-check-constraint
   violate-foreign-key-constraint
   violate-not-null-constraint
   violate-unique-constraint]
