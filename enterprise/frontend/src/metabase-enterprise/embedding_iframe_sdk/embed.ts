c95a6f708d8f3667768ee15ef5bb29629f13e075
Phoomparin Mano <phoomparin@gmail.com>
2025-08-19 17:26:19 +0700

    set a default iframe min-height of 600px

--- enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk/embed.ts
+++ enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk/embed.ts
@@ -288,6 +288,7 @@ export abstract class MetabaseEmbedElement extends HTMLElement {
     this._iframe.style.width = "100%";
     this._iframe.style.height = "100%";
     this._iframe.style.border = "none";
+    this._iframe.style.minHeight = "600px";
 
     this._iframe.setAttribute("data-metabase-embed", "true");
 
