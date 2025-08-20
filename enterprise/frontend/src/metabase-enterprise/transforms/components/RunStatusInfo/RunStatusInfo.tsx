d68a28c9ea93ebb6b132bcc3b3e993967d571dc8
Romeo Van Snick <romeo@romeovansnick.be>
2025-08-20 10:31:07 +1000

    Add run cancel button to run list

--- enterprise/frontend/src/metabase-enterprise/transforms/components/RunStatusInfo/RunStatusInfo.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/RunStatusInfo/RunStatusInfo.tsx
@@ -1,20 +1,24 @@
 import { Box, Group } from "metabase/ui";
-import type { TransformRunStatus } from "metabase-types/api";
+import type { Transform, TransformRunStatus } from "metabase-types/api";
 
 import { formatStatus } from "../../utils";
+import { RunCancelButton } from "../RunCancelButton/RunCancelButton";
 import { RunErrorInfo } from "../RunErrorInfo";
 
 type RunStatusInfoProps = {
+  transform?: Transform;
   status: TransformRunStatus;
   message: string | null;
   endTime: Date | null;
 };
 
 export function RunStatusInfo({
+  transform,
   status,
   message,
   endTime,
 }: RunStatusInfoProps) {
+  // const status = "started";
   const isError = status === "failed" || status === "timeout";
 
   return (
@@ -23,6 +27,7 @@ export function RunStatusInfo({
       {isError && message != null && (
         <RunErrorInfo message={message} endTime={endTime} />
       )}
+      <RunCancelButton transform={transform} status={status} />
     </Group>
   );
 }
