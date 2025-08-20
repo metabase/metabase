29411caa42d609dfc4027517dffffc29be832c34
Romeo Van Snick <romeo@romeovansnick.be>
2025-08-20 15:37:54 +1000

    Do not disable pointer-events for notebook preview

--- enterprise/frontend/src/metabase-enterprise/transforms/components/QueryView/QueryView.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/QueryView/QueryView.tsx
@@ -5,14 +5,12 @@ import { useSelector } from "metabase/lib/redux";
 import { CodeMirrorEditor as Editor } from "metabase/query_builder/components/NativeQueryEditor/CodeMirrorEditor";
 import { Notebook } from "metabase/querying/notebook/components/Notebook";
 import { getMetadata } from "metabase/selectors/metadata";
-import { Box, Center, Loader } from "metabase/ui";
+import { Center, Loader } from "metabase/ui";
 import * as Lib from "metabase-lib";
 import Question from "metabase-lib/v1/Question";
 import type { DatasetQuery } from "metabase-types/api";
 
 import { useQueryMetadata } from "../../hooks/use-query-metadata";
-
-import S from "./QueryView.module.css";
 
 type QueryViewProps = {
   query: DatasetQuery;
@@ -41,18 +39,16 @@ export function QueryView({ query }: QueryViewProps) {
   }
 
   return (
-    <Box className={S.nonInteractive}>
-      <Notebook
-        question={question}
-        reportTimezone={reportTimezone}
-        readOnly
-        isDirty={false}
-        isRunnable={false}
-        isResultDirty={false}
-        hasVisualizeButton={false}
-        updateQuestion={() => Promise.resolve()}
-        runQuestionQuery={() => Promise.resolve()}
-      />
-    </Box>
+    <Notebook
+      question={question}
+      reportTimezone={reportTimezone}
+      readOnly
+      isDirty={false}
+      isRunnable={false}
+      isResultDirty={false}
+      hasVisualizeButton={false}
+      updateQuestion={() => Promise.resolve()}
+      runQuestionQuery={() => Promise.resolve()}
+    />
   );
 }
