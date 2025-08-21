c431b6d54b4ccebd765020aae7c6fa107bf8c7ef
Phoomparin Mano <phoomparin@gmail.com>
2025-08-22 06:12:07 +0800

    add tests on admin status

--- frontend/src/metabase/nav/containers/MainNavbar/tests/premium.unit.spec.ts
+++ frontend/src/metabase/nav/containers/MainNavbar/tests/premium.unit.spec.ts
@@ -1,10 +1,15 @@
 import { screen } from "__support__/ui";
+import { createMockUser } from "metabase-types/api/mocks";
 
 import { setup } from "./setup";
 
 describe("nav > containers > MainNavbar (EE with token) > Getting Started section", () => {
   it("shows embedding hub if feature is present", async () => {
-    await setup({ hasEnterprisePlugins: true, hasEmbeddingHubFeature: true });
+    await setup({
+      hasEnterprisePlugins: true,
+      hasEmbeddingHubFeature: true,
+      user: createMockUser({ is_superuser: true }),
+    });
 
     expect(
       screen.getByRole("link", { name: "Embedding hub" }),
@@ -12,7 +17,23 @@ describe("nav > containers > MainNavbar (EE with token) > Getting Started sectio
   });
 
   it("hides embedding hub if feature is not present", async () => {
-    await setup({ hasEnterprisePlugins: true, hasEmbeddingHubFeature: false });
+    await setup({
+      hasEnterprisePlugins: true,
+      hasEmbeddingHubFeature: false,
+      user: createMockUser({ is_superuser: true }),
+    });
+
+    expect(
+      screen.queryByRole("link", { name: "Embedding hub" }),
+    ).not.toBeInTheDocument();
+  });
+
+  it("hides embedding hub if user is not admin", async () => {
+    await setup({
+      hasEnterprisePlugins: true,
+      hasEmbeddingHubFeature: true,
+      user: createMockUser({ is_superuser: false }),
+    });
 
     expect(
       screen.queryByRole("link", { name: "Embedding hub" }),
