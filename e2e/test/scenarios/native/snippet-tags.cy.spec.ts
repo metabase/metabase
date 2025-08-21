c818a081fcfbee30ceac8b846d740b74c5156fc5
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-08-20 21:43:38 -0400

    Add tests

--- e2e/test/scenarios/native/snippet-tags.cy.spec.ts
+++ e2e/test/scenarios/native/snippet-tags.cy.spec.ts
@@ -11,16 +11,13 @@ describe("scenarios > native > snippet tags", () => {
     H.NativeEditor.type("select id from products where ");
 
     cy.log("create a snippet");
-    cy.findByTestId("native-query-editor-action-buttons")
-      .icon("snippet")
-      .click();
+    getSnippetIcon().click();
     cy.findByTestId("sidebar-content").findByText("Create snippet").click();
     H.modal().within(() => {
-      cy.findByLabelText("Enter some SQL here so you can reuse it later").type(
-        "category = {{category}}",
-        { parseSpecialCharSequences: false },
-      );
-      cy.findByLabelText("Give your snippet a name").type("filter-snippet");
+      getContentInput().type("category = {{category}}", {
+        parseSpecialCharSequences: false,
+      });
+      getNameInput().type("filter-snippet");
       cy.button("Save").click();
     });
 
@@ -36,3 +33,15 @@ describe("scenarios > native > snippet tags", () => {
     H.assertQueryBuilderRowCount(54);
   });
 });
+
+function getSnippetIcon() {
+  return cy.findByTestId("native-query-editor-action-buttons").icon("snippet");
+}
+
+function getNameInput() {
+  return cy.findByLabelText("Give your snippet a name");
+}
+
+function getContentInput() {
+  return cy.findByLabelText("Enter some SQL here so you can reuse it later");
+}
