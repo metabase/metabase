57d2c109c7dd163869a74be861a243880c498a91
Kamil Mielnik <kamil@kamilmielnik.com>
2025-08-21 21:28:44 +0700

    Refactor

--- frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetailWrapper.tsx
+++ frontend/src/metabase/visualizations/components/ObjectDetail/ObjectDetailWrapper.tsx
@@ -4,16 +4,14 @@ import { skipToken, useListTableForeignKeysQuery } from "metabase/api";
 import { DetailViewSidesheet } from "metabase/detail-view/components";
 import { filterByPk } from "metabase/detail-view/utils";
 import Question from "metabase-lib/v1/Question";
-import type Table from "metabase-lib/v1/metadata/Table";
 import { getColumnKey } from "metabase-lib/v1/queries/utils/column-key";
-import { isPK } from "metabase-lib/v1/types/utils/isa";
-import type { Table as ApiTable, DatasetColumn } from "metabase-types/api";
 
 import { PaginationFooter } from "../PaginationFooter/PaginationFooter";
 
 import { ObjectDetailView } from "./ObjectDetailView";
 import S from "./ObjectDetailWrapper.module.css";
 import type { ObjectDetailProps } from "./types";
+import { getApiTable, getRowUrl } from "./utils";
 
 export function ObjectDetailWrapper({
   question,
@@ -115,40 +113,4 @@ export function ObjectDetailWrapper({
       )}
     </>
   );
-}
-
-function getApiTable(table: Table | undefined | null): ApiTable | undefined {
-  if (!table) {
-    return undefined;
-  }
-
-  const apiTable: ApiTable = {
-    ...table.getPlainObject(),
-    fields: table.original_fields,
-  } as ApiTable; // TODO get rid of this hack, we need a real ApiTable object
-
-  return apiTable;
-}
-
-function getRowUrl(
-  question: Question,
-  columns: DatasetColumn[],
-  table: ApiTable | undefined,
-  rowId: string | number,
-): string | undefined {
-  const pks = columns.filter(isPK);
-
-  if (pks.length !== 1) {
-    return undefined;
-  }
-
-  if (question.type() === "model") {
-    return `/model/${question.slug()}/detail/${rowId}`;
-  }
-
-  if (typeof table?.id === "number") {
-    return `/table/${table.id}/detail/${rowId}`;
-  }
-
-  return undefined;
 }
