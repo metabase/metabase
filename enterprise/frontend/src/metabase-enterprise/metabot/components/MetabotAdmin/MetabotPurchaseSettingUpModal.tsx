fae4e9a7935983eedf46ec553f1920c50bbffd9e
Dennis Schridde <dennis@metabase.com>
2025-08-22 14:35:33 +0200

    Translate image alt text

--- enterprise/frontend/src/metabase-enterprise/metabot/components/MetabotAdmin/MetabotPurchaseSettingUpModal.tsx
+++ enterprise/frontend/src/metabase-enterprise/metabot/components/MetabotAdmin/MetabotPurchaseSettingUpModal.tsx
@@ -36,7 +36,7 @@ export const MetabotPurchaseSettingUpModal = ({
         <Box h={96} pos="relative" w={96}>
           <img
             src="app/assets/img/metabot-cloud-96x96.svg"
-            alt="Metabot Cloud"
+            alt={t`Metabot Cloud`}
           />
 
           {isSettingUp && (
