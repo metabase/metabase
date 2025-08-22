957daac5af855766d9e29402d9f689a535ec671d
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-08-23 01:04:57 +0300

    fix: allow postgres downloads to use ddl sql (#62364) (#62594)
    Fixes a regression where making sure the autocommit was correctly set to
    enable reading streaming result sets disallowed ddl and other
    non-readonly changes in postgresql due to the interaction of the
    'readOnly', 'readOnlyMode', and 'autoCommit' jdbc driver settings.
    
    Co-authored-by: Edward Paget <edpaget@users.noreply.github.com>

--- src/metabase/driver/sql_jdbc/execute.clj
+++ src/metabase/driver/sql_jdbc/execute.clj
@@ -369,7 +369,11 @@
                     ;; otherwise it's a spec and we can't get the db
                     :else nil)]
       (set-role-if-supported! driver conn db))
-    (let [read-only? (not write?)]
+    (let [read-only? (not (or write?
+                              ;; we need to set autoCommit to false which causes postgresql
+                              ;; to enforce readOnly which will break queries that rely on
+                              ;; ddl statements, etc (#61892)
+                              (and (-> options :download?) (= driver :postgres))))]
       (try
         ;; Setting the connection to read-only does not prevent writes on some databases, and is meant
         ;; to be a hint to the driver to enable database optimizations
