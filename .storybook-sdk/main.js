489083fc0c771c1a09d562ad54dcaf45eca7999c
Timofei <timofei@metabase.com>
2025-08-22 15:50:16 +0400

    Rename embedding-sdk directory to embedding-sdk-bundle

--- .storybook-sdk/main.js
+++ .storybook-sdk/main.js
@@ -11,7 +11,7 @@ const { isEmbeddingSdkPackageInstalled, embeddingSdkPackageVersion } =
   resolveEmbeddingSdkPackage();
 
 module.exports = {
-  stories: ["../enterprise/frontend/src/embedding-sdk/**/*.stories.tsx"],
+  stories: ["../enterprise/frontend/src/embedding-sdk-bundle/**/*.stories.tsx"],
   staticDirs: ["../resources/frontend_client", "./msw-public"],
   addons: [
     "@storybook/addon-webpack5-compiler-babel",
@@ -89,7 +89,7 @@ function resolveEmbeddingSdkPackage() {
   } catch (err) {
     const sdkPackageTemplateJson = fs.readFileSync(
       path.resolve(
-        "./enterprise/frontend/src/embedding-sdk/package.template.json",
+        "./enterprise/frontend/src/embedding-sdk-bundle/package.template.json",
       ),
       "utf-8",
     );
