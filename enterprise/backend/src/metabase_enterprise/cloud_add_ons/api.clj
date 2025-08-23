ed37d80a6562bd38b5d97dd28d0d8916945da934
Dennis Schridde <dennis@metabase.com>
2025-08-23 18:48:24 +0200

    Move error message vars from local to file scope
    This works, because `deferred-tru` returns an object that is translated
    when `toString` is invoked on it, not when it is created.

--- enterprise/backend/src/metabase_enterprise/cloud_add_ons/api.clj
+++ enterprise/backend/src/metabase_enterprise/cloud_add_ons/api.clj
@@ -8,6 +8,13 @@
    [metabase.premium-features.core :as premium-features]
    [metabase.util.i18n :refer [deferred-tru]]))
 
+(def error-no-connection
+  (deferred-tru "Could not establish a connection to Metabase Cloud."))
+(def error-cannot-purchase
+  (deferred-tru "Could not purchase this add-on."))
+(def error-unexpected
+  (deferred-tru "Unexpected error"))
+
 (api.macros/defendpoint :post "/:product-type"
   "Purchase an add-on."
   [{:keys [product-type]} :- [:map
@@ -31,20 +38,17 @@
     {:status 403 :body "Only Metabase Store users can purchase add-ons."}
 
     :else
-    (let [error-no-connection   (deferred-tru "Could not establish a connection to Metabase Cloud.")
-          error-cannot-purchase (deferred-tru "Could not purchase this add-on.")
-          error-unexpected      (deferred-tru "Unexpected error")]
-      (try
-        (hm.client/call :change-add-ons, :upsert-add-ons [{:product-type product-type}])
-        (premium-features/clear-cache)
-        {:status 200 :body {}}
-        (catch Exception e
-          (case (-> e ex-data :status)
-            404 {:status 404 :body error-no-connection}
-            403 {:status 403 :body error-no-connection}
-            401 {:status 401 :body error-no-connection}
-            400 {:status 400 :body error-cannot-purchase}
-            {:status 500 :body error-unexpected}))))))
+    (try
+      (hm.client/call :change-add-ons, :upsert-add-ons [{:product-type product-type}])
+      (premium-features/clear-cache)
+      {:status 200 :body {}}
+      (catch Exception e
+        (case (-> e ex-data :status)
+          404 {:status 404 :body error-no-connection}
+          403 {:status 403 :body error-no-connection}
+          401 {:status 401 :body error-no-connection}
+          400 {:status 400 :body error-cannot-purchase}
+          {:status 500 :body error-unexpected})))))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/cloud-add-ons` routes."
