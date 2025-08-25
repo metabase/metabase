metabase/metabase

commit 31ddb144831b0fd9b037504388c8a8a68671de1d

Show modal earlier, before making the HTTP request to the backend

enterprise/frontend/src/metabase-enterprise/metabot/components/MetabotAdmin/MetabotPurchasePage.tsx
@@ -84,11 +84,11 @@ export const MetabotPurchasePage = () => {
   const [purchaseMetabotAddOn] = usePurchaseMetabotCloudAddOnMutation();
   const onSubmit = useCallback(
     async ({ terms_of_service }: MetabotPurchaseFormFields) => {
+      settingUpModalHandlers.open();
       await purchaseMetabotAddOn({
         terms_of_service,
       })
         .unwrap()
-        .then(() => settingUpModalHandlers.open())
         .catch((error: unknown) => {
           isFetchBaseQueryError(error) && handleFieldError(error.data);
         });

commit e0562a4a40e60b84e0904f72b47eb772f2823ddd

Close modal on error
Fixes: 31ddb144831b0fd9b037504388c8a8a68671de1d

enterprise/frontend/src/metabase-enterprise/metabot/components/MetabotAdmin/MetabotPurchasePage.tsx
@@ -91,6 +91,7 @@ export const MetabotPurchasePage = () => {
       })
         .unwrap()
         .catch((error: unknown) => {
+          settingUpModalHandlers.close();
           isFetchBaseQueryError(error) && handleFieldError(error.data);
         });
     },