4a0e8b63bda025b3bd6eb1de4268b045a8a84255
Mike Appleby <86076+appleby@users.noreply.github.com>
2025-08-26 09:35:17 -0700

    Fix whitespace / alignment

--- enterprise/backend/src/metabase_enterprise/semantic_search/scoring.clj
+++ enterprise/backend/src/metabase_enterprise/semantic_search/scoring.clj
@@ -179,7 +179,7 @@
   "The appdb-based scorers for search ranking results. Like `base-scorers`, but for scorers that need to query the appdb."
   [{:keys [limit-int] :as search-ctx}]
   (when-not (and limit-int (zero? limit-int))
-    (merge {:bookmarked   search.scoring/bookmark-score-expr}
+    (merge {:bookmarked search.scoring/bookmark-score-expr}
            (when-not (= :mysql (mdb/db-type))
              ;; The :user-recency scorer needs to be modified to work with mysql / mariadb (BOT-360)
              {:user-recency (search.scoring/inverse-duration
