2f349488c3e29403b6d20fd50792dc811f540962
Edward Paget <ed.paget@gmail.com>
2025-08-26 14:08:56 -0500

    api for managing branches

--- enterprise/backend/src/metabase_enterprise/branching/models/branch.clj
+++ enterprise/backend/src/metabase_enterprise/branching/models/branch.clj
@@ -1,6 +1,9 @@
 (ns metabase-enterprise.branching.models.branch
   (:require
+   [metabase.models.interface :as mi]
    [metabase.util :as u]
+   [metabase.util.malli :as mu]
+   [metabase.util.malli.schema :as ms]
    [methodical.core :as methodical]
    [toucan2.core :as t2]))
 
@@ -14,3 +17,47 @@
 (t2/define-before-insert :model/Branch
   [branch]
   (assoc branch :slug (u/slugify (:name branch))))
+
+ ;;; ------------------------------------------------- Helper Functions -------------------------------------------------
+
+(mu/defn parent :- [:maybe (ms/InstanceOf :model/Branch)]
+  "Get the parent branch of the given branch, if it exists."
+  [branch :- (ms/InstanceOf :model/Branch)]
+  (when-let [parent-id (:parent_branch_id branch)]
+    (t2/select-one :model/Branch :id parent-id)))
+
+(mu/defn children :- [:set (ms/InstanceOf :model/Branch)]
+  "Get all direct child branches of the given branch."
+  [branch :- (ms/InstanceOf :model/Branch)]
+  (set (t2/select :model/Branch :parent_branch_id (:id branch))))
+
+(mu/defn has-children? :- :boolean
+  "Check if the given branch has any child branches."
+  [branch :- (ms/InstanceOf :model/Branch)]
+  (t2/exists? :model/Branch :parent_branch_id (:id branch)))
+
+(mu/defn branches-by-parent-id :- [:set (ms/InstanceOf :model/Branch)]
+  "Get all branches with the specified parent_branch_id. If parent-id is nil, 
+   returns root-level branches (those with no parent)."
+  [parent-id :- [:maybe ms/PositiveInt]]
+  (set (t2/select :model/Branch :parent_branch_id parent-id)))
+
+;;; ------------------------------------------------- Hydration Methods -------------------------------------------------
+
+(mi/define-simple-hydration-method parent
+  :parent
+  "Hydrate a branch with its parent branch."
+  [branch]
+  (parent branch))
+
+(mi/define-simple-hydration-method children
+  :children
+  "Hydrate a branch with its direct child branches."
+  [branch]
+  (children branch))
+
+(mi/define-simple-hydration-method has-children?
+  :has_children
+  "Hydrate a branch with a boolean indicating whether it has child branches."
+  [branch]
+  (has-children? branch))
