4adbe6ac476df59862f9704447f6f8566bc4a517
Phoomparin Mano <phoomparin@gmail.com>
2025-08-26 19:58:16 +0700

    add a check for isSsoReady

--- frontend/src/metabase/embedding/embedding-hub/hooks/use-completed-embedding-hub-steps.ts
+++ frontend/src/metabase/embedding/embedding-hub/hooks/use-completed-embedding-hub-steps.ts
@@ -1,5 +1,7 @@
 import { useMemo } from "react";
 
+import { useSetting } from "metabase/common/hooks";
+
 import type { EmbeddingHubStepId } from "../types";
 
 /**
@@ -11,14 +13,22 @@ export const useCompletedEmbeddingHubSteps = (): Record<
   EmbeddingHubStepId,
   boolean
 > => {
+  const isJwtEnabled = useSetting("jwt-enabled");
+  const isSamlEnabled = useSetting("saml-enabled");
+  const isJwtConfigured = useSetting("jwt-configured");
+  const isSamlConfigured = useSetting("saml-configured");
+
+  const isSsoReady =
+    (isJwtEnabled && isJwtConfigured) || (isSamlEnabled && isSamlConfigured);
+
   return useMemo(() => {
     return {
       "create-test-embed": true,
       "add-data": false,
       "create-dashboard": false,
       "configure-row-column-security": false,
-      "secure-embeds": false,
+      "secure-embeds": isSsoReady,
       "embed-production": false,
     };
-  }, []);
+  }, [isSsoReady]);
 };
