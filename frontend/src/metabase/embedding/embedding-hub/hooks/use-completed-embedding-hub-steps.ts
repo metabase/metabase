e5ba62422bf1f5bf8998b289a76937de6c06b134
Phoomparin Mano <poom@metabase.com>
2025-08-26 23:23:12 +0700

    Setup sidebar and onboarding page for embedding hub (#62517)

---
+++ frontend/src/metabase/embedding/embedding-hub/hooks/use-completed-embedding-hub-steps.ts
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+
+import type { EmbeddingHubStepId } from "../types";
+
+/**
+ * Embedding Hub completion steps should be derived by the instance state at the time, or tracked manually in instance settings for some of them.
+ *
+ * TODO: make this live.
+ */
+export const useCompletedEmbeddingHubSteps = (): Record<
+  EmbeddingHubStepId,
+  boolean
+> => {
+  return useMemo(() => {
+    return {
+      "create-test-embed": true,
+      "add-data": false,
+      "create-dashboard": false,
+      "configure-row-column-security": false,
+      "secure-embeds": false,
+      "embed-production": false,
+    };
+  }, []);
+};
