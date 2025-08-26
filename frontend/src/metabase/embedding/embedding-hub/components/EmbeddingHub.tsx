1d63399d898bb53e101888bc54dcfae6afaf105f
Phoomparin Mano <phoomparin@gmail.com>
2025-08-26 11:44:36 +0700

    add a xray-dashboard wizard

--- frontend/src/metabase/embedding/embedding-hub/components/EmbeddingHub.tsx
+++ frontend/src/metabase/embedding/embedding-hub/components/EmbeddingHub.tsx
@@ -14,6 +14,7 @@ import type { EmbeddingHubModalToTrigger } from "../types/embedding-checklist";
 import { getEmbeddingHubSteps } from "../utils";
 
 import { EmbeddingHubChecklist } from "./EmbeddingHubChecklist";
+import { EmbeddingHubXrayPickerModal } from "./EmbeddingHubXrayPickerModal";
 
 export const EmbeddingHub = () => {
   const embeddingSteps = useMemo(() => getEmbeddingHubSteps(), []);
@@ -62,6 +63,11 @@ export const EmbeddingHub = () => {
 
         <CreateDashboardModal
           opened={openedModal?.type === "new-dashboard"}
+          onClose={closeModal}
+        />
+
+        <EmbeddingHubXrayPickerModal
+          opened={openModal?.type === "xray-dashboard"}
           onClose={closeModal}
         />
       </Box>
