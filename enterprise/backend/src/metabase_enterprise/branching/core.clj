61f4c68f4729133cffe18f3e82f43186069b2568
Edward Paget <ed.paget@gmail.com>
2025-08-26 13:39:33 -0500

    fallback on user setting

--- enterprise/backend/src/metabase_enterprise/branching/core.clj
+++ enterprise/backend/src/metabase_enterprise/branching/core.clj
@@ -2,11 +2,13 @@
   "Handles the logic for setting the current branch from an HTTP request and supports
   overriding t2/select to get branch-able models based on the current request."
   (:require
-   [metabase.premium-features.core :refer [defenterprise]]))
+   [metabase.premium-features.core :refer [defenterprise]]
+   [metabase.users.settings :as u.settings]))
 
 (defenterprise get-current-branch
   "Get the current branch from the X-Metabase-Branch header."
   :feature :none
   [request]
   (or (get-in request [:headers "X-Metabase-Branch"])
-      (get-in request [:headers "x-metabase-branch"])))
+      (get-in request [:headers "x-metabase-branch"])
+      (u.settings/git-branch)))
