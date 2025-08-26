744e91f41f88e323fe430898833772ca151902ba
Brad Anderson <brad@metabase.com>
2025-08-26 10:45:26 -0400

    Stubs out ellipsis menu in GitChangeList

--- enterprise/frontend/src/metabase-enterprise/git_sync/GitChangeList.tsx
+++ enterprise/frontend/src/metabase-enterprise/git_sync/GitChangeList.tsx
@@ -5,7 +5,7 @@ import {
   SettingsSection,
 } from "metabase/admin/components/SettingsSection";
 import { Table } from "metabase/common/components/Table";
-import { Button } from "metabase/ui";
+import { ActionIcon, Button, Flex, Icon, Menu } from "metabase/ui";
 
 export const GitChangeList = () => {
   return (
@@ -27,9 +27,72 @@ export const GitChangeList = () => {
               <td style={{ padding: "8px 16px" }}>{row.author}</td>
               <td style={{ padding: "8px 16px" }}>{row.change}</td>
               <td style={{ padding: "8px 16px" }}>
-                <Button variant="filled" color="danger" size="compact-sm">
-                  {t`Reject`}
-                </Button>
+                <Flex w="full" align="center" justify="end">
+                  <Button
+                    variant="filled"
+                    color="danger"
+                    size="compact-sm"
+                    mr="sm"
+                  >
+                    {t`Reject`}
+                  </Button>
+                  <Menu position="bottom-end">
+                    <Menu.Target>
+                      <ActionIcon
+                        variant="subtle"
+                        size="md"
+                        aria-label={t`More options`}
+                      >
+                        <Icon name="ellipsis" />
+                      </ActionIcon>
+                    </Menu.Target>
+                    <Menu.Dropdown>
+                      <Menu.Item leftSection={<Icon name="eye" />}>
+                        {t`View changes`}
+                      </Menu.Item>
+                      <Menu.Item
+                        leftSection={
+                          <svg
+                            width={16}
+                            viewBox="0 0 16 16"
+                            fill="none"
+                            stroke="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <line
+                              x1="4"
+                              x2="4"
+                              y1="2"
+                              y2="10"
+                              strokeWidth="1.5"
+                            />
+                            <circle cx="12" cy="4" r="2" strokeWidth="1.5" />
+                            <circle cx="4" cy="12" r="2" strokeWidth="1.5" />
+                            <path d="M12 6a6 6 0 0 1-6 6" strokeWidth="1.5" />
+                          </svg>
+                        }
+                      >
+                        {t`Switch to branch`}
+                      </Menu.Item>
+                      <Menu.Item
+                        leftSection={
+                          <svg
+                            height="1rem"
+                            viewBox="0 0 98 96"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                              fill="#24292f" // eslint-disable-line no-color-literals
+                            />
+                          </svg>
+                        }
+                      >
+                        {t`View on Github`}
+                      </Menu.Item>
+                    </Menu.Dropdown>
+                  </Menu>
+                </Flex>
               </td>
             </tr>
           )}
