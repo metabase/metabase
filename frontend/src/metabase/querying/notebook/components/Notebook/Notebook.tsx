5ef6064e05e3d4d307c49fc416a547c5a917e7e6
Uladzimir Havenchyk <uladzimir@metabase.com>
2025-08-27 13:02:34 +0300

    add basic dnd for customize colunns sidebar

--- frontend/src/metabase/querying/notebook/components/Notebook/Notebook.tsx
+++ frontend/src/metabase/querying/notebook/components/Notebook/Notebook.tsx
@@ -1,8 +1,15 @@
+import { arrayMove } from "@dnd-kit/sortable";
+import { useState } from "react";
+import { t } from "ttag";
+
 import type { DataPickerValue } from "metabase/common/components/Pickers/DataPicker";
 import { useDispatch } from "metabase/lib/redux";
-import { setUIControls } from "metabase/query_builder/actions";
-import { Box } from "metabase/ui";
+import { setUIControls, updateQuestion } from "metabase/query_builder/actions";
+import { ColumnPickerSidebar } from "metabase/query_builder/components/ColumnPickerSidebar/ColumnPickerSidebar";
+import { Box, Button, Flex } from "metabase/ui";
+import * as Lib from "metabase-lib";
 import type Question from "metabase-lib/v1/Question";
+import type { VisualizationSettings } from "metabase-types/api";
 
 import type { NotebookDataPickerOptions } from "../../types";
 import { NotebookStepList } from "../NotebookStepList";
@@ -26,7 +33,6 @@ export type NotebookProps = {
 };
 
 export const Notebook = ({
-  updateQuestion,
   reportTimezone,
   readOnly,
   question,
@@ -34,10 +40,11 @@ export const Notebook = ({
   isRunnable,
   isResultDirty,
   hasVisualizeButton = true,
-  runQuestionQuery,
-  setQueryBuilderMode,
   modelsFilterList,
   dataPickerOptions,
+  runQuestionQuery,
+  setQueryBuilderMode,
+  updateQuestion,
 }: NotebookProps) => {
   const dispatch = useDispatch();
 
@@ -57,17 +64,77 @@ export const Notebook = ({
           dataPickerOptions={dataPickerOptions}
         />
         {hasVisualizeButton && runQuestionQuery && (
-          <VisualizeButton
-            question={question}
-            isDirty={isDirty}
-            isRunnable={isRunnable}
-            isResultDirty={isResultDirty}
-            updateQuestion={updateQuestion}
-            runQuestionQuery={runQuestionQuery}
-            setQueryBuilderMode={setQueryBuilderMode}
-          />
+          <Flex gap="md">
+            <CustomizeColumnsButton question={question} />
+            <VisualizeButton
+              question={question}
+              isDirty={isDirty}
+              isRunnable={isRunnable}
+              isResultDirty={isResultDirty}
+              updateQuestion={updateQuestion}
+              runQuestionQuery={runQuestionQuery}
+              setQueryBuilderMode={setQueryBuilderMode}
+            />
+          </Flex>
         )}
       </Box>
     </NotebookProvider>
   );
 };
+
+interface CustomizeColumnsButtonProps {
+  question: Question;
+}
+
+function CustomizeColumnsButton({ question }: CustomizeColumnsButtonProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const query = question.query();
+  const dispatch = useDispatch();
+
+  const columns = Lib.returnedColumns(query, -1);
+
+  const handleClick = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const handleReorderColumns = (oldIndex: number, newIndex: number) => {
+    const settings = question.settings();
+    const columnsSettings = settings["table.columns"] ?? [];
+    let newSettings;
+
+    if (columnsSettings.length === 0) {
+      newSettings = {
+        ...settings,
+        "table.columns": columns.map((c) => {
+          const displayInfo = Lib.displayInfo(query, -1, c);
+          return { name: displayInfo.displayName, enabled: true };
+        }),
+      };
+
+      arrayMove(newSettings["table.columns"], oldIndex, newIndex);
+    }
+
+    const newQuestion = question.setSettings(
+      newSettings as VisualizationSettings,
+    );
+    dispatch(updateQuestion(newQuestion));
+  };
+
+  return (
+    <>
+      <Button onClick={handleClick}>{t`Customize Columns`}</Button>
+      {isSidebarOpen && (
+        <ColumnPickerSidebar
+          title={t`Reorder and rename columns`}
+          isOpen={isSidebarOpen}
+          query={question.query()}
+          stageIndex={-1}
+          onClose={() => setIsSidebarOpen(false)}
+          columns={columns}
+          isDraggable
+          onReorderColumns={handleReorderColumns}
+        />
+      )}
+    </>
+  );
+}
