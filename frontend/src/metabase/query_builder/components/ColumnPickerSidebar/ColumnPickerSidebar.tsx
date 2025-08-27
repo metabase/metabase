4c9bcda8944e085bdb403e8b1764d73b9e9510b0
Uladzimir Havenchyk <uladzimir@metabase.com>
2025-08-27 13:02:34 +0300

    add basic dnd for customize colunns sidebar

--- frontend/src/metabase/query_builder/components/ColumnPickerSidebar/ColumnPickerSidebar.tsx
+++ frontend/src/metabase/query_builder/components/ColumnPickerSidebar/ColumnPickerSidebar.tsx
@@ -1,4 +1,12 @@
-import { useEffect, useMemo, useState } from "react";
+import { DndContext, type DragEndEvent, closestCenter } from "@dnd-kit/core";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { forwardRef, useEffect, useMemo, useState } from "react";
 import { t } from "ttag";
 
 import {
@@ -28,10 +36,12 @@ interface ColumnPickerSidebarProps {
   isOpen: boolean;
   onClose: () => void;
   columns: Lib.ColumnMetadata[];
-  onToggle: (column: Lib.ColumnMetadata, isSelected: boolean) => void;
-  onSelectAll: () => void;
-  onSelectNone: () => void;
+  onToggle?: (column: Lib.ColumnMetadata, isSelected: boolean) => void;
+  onSelectAll?: () => void;
+  onSelectNone?: () => void;
   isColumnSelected?: (column: FieldPickerItem) => boolean;
+  isDraggable?: boolean;
+  onReorderColumns?: (oldIndex: number, newIndex: number) => void;
 }
 
 export function ColumnPickerSidebar({
@@ -45,6 +55,8 @@ export function ColumnPickerSidebar({
   onSelectAll,
   onSelectNone,
   isColumnSelected = _isColumnSelected,
+  isDraggable = false,
+  onReorderColumns,
 }: ColumnPickerSidebarProps) {
   const [localColumns, setLocalColumns] = useState<Lib.ColumnMetadata[]>([]);
   const [searchText, setSearchText] = useState("");
@@ -87,11 +99,35 @@ export function ColumnPickerSidebar({
 
   const handleAllToggle = () => {
     if (isAll) {
-      onSelectNone();
+      onSelectNone?.();
     } else {
-      onSelectAll();
+      onSelectAll?.();
     }
   };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (active.id !== over?.id && onReorderColumns) {
+      const oldIndex = localColumns.findIndex((column) => {
+        const info = Lib.displayInfo(query, stageIndex, column);
+        return info.longDisplayName === active.id;
+      });
+      const newIndex = localColumns.findIndex((column) => {
+        const info = Lib.displayInfo(query, stageIndex, column);
+        return info.longDisplayName === over?.id;
+      });
+
+      if (oldIndex !== -1 && newIndex !== -1) {
+        // reorder columns in viz settings
+        onReorderColumns(oldIndex, newIndex);
+      }
+    }
+  };
+
+  const ItemComponent = isDraggable
+    ? SortableColumnPickerItem
+    : ColumnPickerItem;
 
   return (
     <Sidesheet
@@ -123,31 +159,45 @@ export function ColumnPickerSidebar({
 
         <Box className={S.ColumnsList}>
           <ul className={S.ItemList}>
-            <li className={S.ToggleItem}>
-              <HoverParent as="label" className={S.Label}>
-                <Checkbox
-                  variant="stacked"
-                  checked={isAll}
-                  indeterminate={!isAll && !isNone}
-                  onChange={handleAllToggle}
-                />
-                <div className={S.ItemTitle}>
-                  {searchText ? t`Select these` : t`Select all`}
-                </div>
-              </HoverParent>
-            </li>
+            {!isDraggable && (
+              <li className={S.ToggleItem}>
+                <HoverParent as="label" className={S.Label}>
+                  <Checkbox
+                    variant="stacked"
+                    checked={isAll}
+                    indeterminate={!isAll && !isNone}
+                    onChange={handleAllToggle}
+                  />
+                  <div className={S.ItemTitle}>
+                    {searchText ? t`Select these` : t`Select all`}
+                  </div>
+                </HoverParent>
+              </li>
+            )}
 
-            {filteredItems.map((item) => {
-              return (
-                <ColumnPickerItem
-                  key={item.columnInfo.displayName}
-                  item={item}
-                  query={query}
-                  stageIndex={stageIndex}
-                  onToggle={onToggle}
-                />
-              );
-            })}
+            <DndContext
+              onDragEnd={handleDragEnd}
+              collisionDetection={closestCenter}
+              modifiers={[restrictToVerticalAxis]}
+            >
+              <SortableContext
+                items={filteredItems.map((item) => item.columnInfo.displayName)}
+                strategy={verticalListSortingStrategy}
+              >
+                {filteredItems.map((item) => {
+                  return (
+                    <ItemComponent
+                      key={item.columnInfo.displayName}
+                      item={item}
+                      query={query}
+                      stageIndex={stageIndex}
+                      onToggle={onToggle}
+                      isDraggable={isDraggable}
+                    />
+                  );
+                })}
+              </SortableContext>
+            </DndContext>
           </ul>
 
           {filteredItems.length === 0 && searchText && (
@@ -165,40 +215,111 @@ interface ColumnPickerItemProps {
   query: Lib.Query;
   stageIndex: number;
   item: FieldPickerItem & { isSelected: boolean; isDisabled: boolean };
-  onToggle: (column: Lib.ColumnMetadata, isSelected: boolean) => void;
+  onToggle?: (column: Lib.ColumnMetadata, isSelected: boolean) => void;
+  isDraggable: boolean;
+  handle?: React.ReactNode;
+  style?: React.CSSProperties;
 }
 
-function ColumnPickerItem({
-  query,
-  stageIndex,
-  item,
-  onToggle,
-}: ColumnPickerItemProps) {
+const ColumnPickerItem = forwardRef<HTMLLIElement, ColumnPickerItemProps>(
+  function ColumnPickerItem(
+    { query, stageIndex, item, onToggle, isDraggable, handle, ...rest },
+    ref,
+  ) {
+    const [editingName, setEditingName] = useState(
+      item.columnInfo.longDisplayName,
+    );
+
+    return (
+      <li key={item.columnInfo.displayName} ref={ref} {...rest}>
+        <HoverParent className={S.Label} as="label">
+          <Flex align="center" gap="xs" w="100%">
+            {isDraggable && handle}
+            {!isDraggable && (
+              <Checkbox
+                checked={item.isSelected}
+                disabled={item.isDisabled}
+                onChange={(event) =>
+                  onToggle?.(item.column, event.target.checked)
+                }
+              />
+            )}
+            {!isDraggable && (
+              <QueryColumnInfoIcon
+                className={S.ItemIcon}
+                query={query}
+                stageIndex={stageIndex}
+                column={item.column}
+                position="top-start"
+                size={16}
+              />
+            )}
+            <Box flex={1}>
+              {isDraggable ? (
+                <TextInput
+                  value={editingName}
+                  onChange={(event) =>
+                    setEditingName(event.currentTarget.value)
+                  }
+                  size="md"
+                  autoFocus
+                  onFocus={(e) => e.target.select()}
+                />
+              ) : (
+                <Text title={t`Click to edit display name`}>
+                  {item.columnInfo.longDisplayName}
+                </Text>
+              )}
+            </Box>
+          </Flex>
+        </HoverParent>
+      </li>
+    );
+  },
+);
+
+function SortableColumnPickerItem(
+  props: Omit<ColumnPickerItemProps, "handle" | "style">,
+) {
+  const { item } = props;
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: item.columnInfo.displayName,
+    disabled: !item.isSelected,
+  });
+
+  const Handle = () => {
+    return (
+      <ActionIcon
+        size="sm"
+        variant="subtle"
+        {...listeners}
+        style={{ cursor: isDragging ? "grabbing" : "grab" }}
+        aria-label={t`Drag to reorder`}
+      >
+        <Icon name="grabber" />
+      </ActionIcon>
+    );
+  };
+
   return (
-    <li key={item.columnInfo.displayName}>
-      <HoverParent className={S.Label} as="label">
-        <Flex align="center" gap="xs" w="100%">
-          <Checkbox
-            checked={item.isSelected}
-            disabled={item.isDisabled}
-            onChange={(event) => onToggle(item.column, event.target.checked)}
-          />
-          <QueryColumnInfoIcon
-            className={S.ItemIcon}
-            query={query}
-            stageIndex={stageIndex}
-            column={item.column}
-            position="top-start"
-            size={16}
-          />
-          {/* <Box flex={1}> */}
-          <Text title={t`Click to edit display name`}>
-            {item.columnInfo.longDisplayName}
-          </Text>
-          {/* </Box> */}
-        </Flex>
-      </HoverParent>
-    </li>
+    <ColumnPickerItem
+      {...props}
+      ref={setNodeRef}
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition,
+        opacity: isDragging ? 0.5 : 1,
+      }}
+      {...attributes}
+      handle={<Handle />}
+    />
   );
 }
 
