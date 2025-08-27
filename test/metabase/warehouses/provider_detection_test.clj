8bd57e182c75370078998568f510d6b3c52b1dd4
dan sutton <dan@dpsutton.com>
2025-08-27 08:42:25 -0500

    move postgres tests to postgres driver; add a few tests for yaml
    # Conflicts:
    #	test/metabase/driver/postgres_test.clj

--- test/metabase/warehouses/provider_detection_test.clj
+++ test/metabase/warehouses/provider_detection_test.clj
@@ -1,28 +1,24 @@
 (ns metabase.warehouses.provider-detection-test
   (:require
+   [clj-yaml.core :as yaml]
    [clojure.test :refer :all]
    [metabase.warehouses.provider-detection :as provider-detection]))
 
-(deftest detect-provider-from-database-test
-  (let [tests [["Aiven" "mydb-project.aivencloud.com"]
-               ["Amazon RDS" "czrs8kj4isg7.us-east-1.rds.amazonaws.com"]
-               ["Azure" "production-flexible-server.postgres.database.azure.com"]
-               ["Crunchy Data" "p.vbjrfujv5beutaoelw725gvi3i.db.postgresbridge.com"]
-               ["DigitalOcean" "cluster-do-user-1234567-0.db.ondigitalocean.com"]
-               ["Fly.io" "db.fly.dev"]
-               ["Neon" "ep-autumn-frost-alwlmval-pooler.ap-southeast-1.aws.neon.tech"]
-               ["PlanetScale" "my-db.horizon.psdb.cloud"]
-               ["Railway" "nodejs-copy-production-7aa4.up.railway.app"]
-               ["Render" "your_host_name.your_region-postgres.render.com"]
-               ["Scaleway" "my-db.region-1.scw.cloud"]
-               ["Supabase" "db.apbkobhfnmcqqzqeeqss.supabase.co"]
-               ["Supabase" "aws-0-us-west-1.pooler.supabase.com"]
-               ["Timescale" "service.project.tsdb.cloud.timescale.com"]]]
-    (testing "full database entity detection with postgres engine"
-      (doseq [[provider host] tests]
-        (let [database {:details {:host host} :engine :postgres}]
-          (is (= provider (provider-detection/detect-provider-from-database database)))))))
+(deftest detects-from-strings
+  (let [raw-yaml "
+providers:
+- {name: Foo™, pattern: foo\\.hosted\\.com$}
+- {name: Hosty®, pattern: (hosty\\.hosted.com|\\.hosted\\.by\\.hosted)$}"
+        providers (:providers (yaml/parse-string raw-yaml))]
+    (doseq [[expected host] [["Foo™" "random.23423.foo.hosted.com"]
+                             ;; ensure the escaped periods are not interpreted as wildcards
+                             [nil "random.23423.foo†hosted†com"]
+                             ["Hosty®" "random.2342342.hosty.hosted.com"]
+                             ["Hosty®" "random.hosted.by.hosted"]
+                             [nil "not.gonna.match.com"]]]
+      (is (= expected (#'provider-detection/detect-provider host providers))))))
 
+(deftest detect-provider-from-database-test
   (testing "database with unsupported engine returns nil"
     (let [database {:details {:host "czrs8kj4isg7.us-east-1.rds.amazonaws.com"} :engine :mysql}]
       (is (nil? (provider-detection/detect-provider-from-database database)))))
