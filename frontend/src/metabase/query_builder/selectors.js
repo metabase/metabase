1388fcc351ed96eb97675677016aae5147d77eca
Stas Gavrylov <stasgavrylov@gmail.com>
2025-08-27 10:36:55 +0300

    Fix incorrect condition to save list view models as tables

--- frontend/src/metabase/query_builder/selectors.js
+++ frontend/src/metabase/query_builder/selectors.js
@@ -1083,10 +1083,6 @@ export const getSubmittableQuestion = (state, question) => {
     .setQuery(cleanQuery)
     .setResultsMetadata(isResultDirty ? null : resultsMetadata);
 
-  if (submittableQuestion.display() === "list") {
-    submittableQuestion = submittableQuestion.setDisplay("table");
-  }
-
   return submittableQuestion;
 };
 
