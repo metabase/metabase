433635139a48edf1ced6239c47364259045595cd
Mike Appleby <86076+appleby@users.noreply.github.com>
2025-08-28 14:53:31 -0700

    Fix appdb-available-with-semantic test for mysql appdb
    When the appdb is :mysql, we should fallback to the in-place engine rather than appdb

--- enterprise/backend/test/metabase_enterprise/semantic_search/core_test.clj
+++ enterprise/backend/test/metabase_enterprise/semantic_search/core_test.clj
@@ -4,6 +4,7 @@
    [metabase-enterprise.semantic-search.core :as semantic.core]
    [metabase-enterprise.semantic-search.pgvector-api :as semantic.pgvector-api]
    [metabase-enterprise.semantic-search.test-util :as semantic.tu]
+   [metabase.app-db.core :as mdb]
    [metabase.search.appdb.core :as appdb]
    [metabase.search.engine :as search.engine]
    [metabase.search.settings :as search.settings]
@@ -14,13 +15,16 @@
                      (fixtures/initialize :db)
                      #'semantic.tu/once-fixture))
 
-(deftest appdb-available-with-semantic
+(deftest fallback-engine-available-with-semantic-test
   (mt/with-premium-features #{:semantic-search}
     (mt/with-temporary-setting-values [search.settings/search-engine "semantic"]
       (with-open [_ (semantic.tu/open-temp-index!)]
         (semantic.tu/cleanup-index-metadata! semantic.tu/db semantic.tu/mock-index-metadata)
         (semantic.tu/with-index!
-          (is (search.engine/supported-engine? :search.engine/appdb)))))))
+          (is (search.engine/supported-engine? (if (= :mysql (mdb/db-type))
+                                                 ;; appdb is not supported on :mysql, should fallback to in-place
+                                                 :search.engine/in-place
+                                                 :search.engine/appdb))))))))
 
 (deftest api-engine-switching-test
   (mt/with-premium-features #{:semantic-search}
