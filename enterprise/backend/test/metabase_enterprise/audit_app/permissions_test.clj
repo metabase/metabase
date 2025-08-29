319d8484f6aadeb6d43ae5012c796086695782a7
Bryan Maass <bryan.maass@gmail.com>
2025-08-29 09:38:56 -0600

    remove (unneeded?) poll in audit-db-basic-query-test

--- enterprise/backend/test/metabase_enterprise/audit_app/permissions_test.clj
+++ enterprise/backend/test/metabase_enterprise/audit_app/permissions_test.clj
@@ -34,9 +34,8 @@
   (mt/test-drivers #{:postgres :h2 :mysql}
     (audit-test/with-audit-db-restoration
       (mt/with-premium-features #{:audit-app}
-        (is (= "complete"
-               (tu/poll-until 5000 (#{"complete"}
-                                    (t2/select-one-fn :initial_sync_status :model/Database :id audit/audit-db-id)))))
+        (is (= "complete" (t2/select-one-fn :initial_sync_status :model/Database :id audit/audit-db-id))
+            "The sync status should be completed by this point. (In test it is synchronous!)")
         (mt/with-test-user :crowberto
           (testing "A query using a saved audit model as the source table runs succesfully"
             (let [audit-card (t2/select-one :model/Card :database_id audit/audit-db-id :type :model)]
