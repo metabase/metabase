f0aaa2f0a8d5935a0d37b1bbb7204039617f63a3
John Swanson <john.d.swanson@gmail.com>
2025-08-29 07:21:55 -0700

    use jgit instead

--- enterprise/backend/src/metabase_enterprise/git_source_of_truth/sources.clj
+++ enterprise/backend/src/metabase_enterprise/git_source_of_truth/sources.clj
@@ -1,14 +1,15 @@
 (ns metabase-enterprise.git-source-of-truth.sources
   (:require
    [clojure.java.io :as io]
-   [clojure.java.shell :as shell]
-   [clojure.string :as str]
    [metabase-enterprise.git-source-of-truth.settings :as settings]
    [metabase.util.log :as log])
   (:import
    (java.io File)
-   (java.nio.file Files)
-   (java.nio.file.attribute FileAttribute PosixFilePermissions)))
+   (org.eclipse.jgit.api Git)
+   (org.eclipse.jgit.transport UsernamePasswordCredentialsProvider
+                               RefSpec)))
+
+(set! *warn-on-reflection* true)
 
 (defprotocol ISource
   (load-source! [this dir]
@@ -21,27 +22,27 @@
   (push-branch! [this source-dir]
     "Pushes the contentents of `source-dir` to the branch specified."))
 
-(defn- execute-git-command
-  "Execute a git command in the specified directory. Returns {:exit-code :stdout :stderr}."
-  [directory & args]
-  (let [result (apply shell/sh "git" (concat args [:dir directory]))]
-    (log/debugf "Git command: git %s (exit: %d)" (str/join " " args) (:exit result))
-    (when-not (zero? (:exit result))
-      (log/errorf "Git command failed: %s" (:err result)))
-    result))
-
 (defn clone-repository!
-  "Clone git repository to a temporary directory. Returns the path to the cloned repository."
-  [url branch dir _token]
-  (let [result (execute-git-command dir "clone" "--single-branch" "--branch" branch url ".")]
-    (if (zero? (:exit result))
-      (log/infof "Successfully cloned repository to %s" dir)
-      (throw (ex-info (format "Failed to clone git repository: %s" (:err result))
+  "Clone git repository to a temporary directory using jgit. Returns the path to the cloned repository."
+  [url branch dir token]
+  (try
+    (log/info "Cloning repository" {:url url :branch branch :dir dir})
+    (-> (Git/cloneRepository)
+        (.setURI url)
+        (.setBranch branch)
+        (.setDirectory (io/file dir))
+        (.setCloneAllBranches false)
+        (.setCredentialsProvider (when token
+                                   (UsernamePasswordCredentialsProvider. "x-access-token" token)))
+        (.call)
+        (.close))
+    (log/info "Successfully cloned repository" {:dir dir})
+    (catch Exception e
+      (throw (ex-info (format "Failed to clone git repository: %s" (.getMessage e))
                       {:url url
                        :branch branch
-                       :exit-code (:exit result)
                        :dir dir
-                       :stderr (:err result)})))))
+                       :error (.getMessage e)})))))
 
 (defn get-serdes-path
   "Get the full path to the serdes files within the repository."
@@ -58,14 +59,6 @@
 
     full-path))
 
-(defn- write-0600! [content suffix]
-  (let [f (Files/createTempFile "git-demo-" (str "." suffix)
-                                (make-array java.nio.file.attribute.FileAttribute 0))]
-    (spit (.toFile f) content)
-    (try (Files/setPosixFilePermissions f (PosixFilePermissions/fromString "rw-------"))
-         (catch UnsupportedOperationException _))
-    (str f)))
-
 (defrecord GitSource [url source-branch token dest-branch]
   ISource
   (load-source! [_this dir]
@@ -74,22 +67,74 @@
 
   IDestination
   (push-branch! [_this source-dir]
-    (let [branch (str (random-uuid))
-          askpass (write-0600! "#!/usr/bin/env sh\nprintf \"%s\" \"$GIT_TOKEN\"\n" "askpass.sh")]
-      (.setExecutable (io/file askpass) true true)
-      (execute-git-command source-dir "init")
-      (execute-git-command source-dir "remote" "add" "origin" url)
-      (execute-git-command source-dir "fetch" "origin")
-      (execute-git-command source-dir "checkout" "-b" branch)
-      (try (execute-git-command source-dir "reset" "--mixed" (str "origin/" source-branch))
-           (catch Exception _ nil))
-      (execute-git-command source-dir "add" "-A")
-      (execute-git-command source-dir "commit" "-m" "Update content")
-      (execute-git-command source-dir "-c" "credential.username=x-access-token"
-                           "push" "--force" "-u" "origin" (str branch ":" dest-branch)
-                           :env {"GIT_ASKPASS" askpass
-                                 "GIT_TOKEN" token
-                                 "GIT_TERMINAL_PROMPT" "0"}))))
+    (let [branch-name (str (random-uuid))
+          repo-dir (io/file source-dir)]
+      (try
+        ;; Initialize or open existing repository
+        (log/info "Initializing git repository" {:dir source-dir})
+        (let [git-repo (if (.exists (io/file source-dir ".git"))
+                         (Git/open repo-dir)
+                         (-> (Git/init)
+                             (.setDirectory repo-dir)
+                             (.call)))
+              repo (.getRepository git-repo)]
+          (try
+            ;; Add origin remote if it doesn't exist
+            (log/info "Setting up remote origin" {:url url})
+            (-> (.remoteAdd git-repo)
+                (.setName "origin")
+                (.setUri (org.eclipse.jgit.transport.URIish. url))
+                (.call))
+
+            ;; Fetch from origin
+            (log/info "Fetching from origin")
+            (-> (.fetch git-repo)
+                (.setRemote "origin")
+                (.setCredentialsProvider (when token
+                                           (UsernamePasswordCredentialsProvider. "x-access-token" token)))
+                (.call))
+
+            ;; Create and checkout new branch
+            (log/info "Creating new branch" {:branch branch-name})
+            (-> (.checkout git-repo)
+                (.setOrphan true)
+                (.setName branch-name)
+                (.call))
+
+            (when-let [origin-ref (.findRef repo (str "origin/" source-branch))]
+              (-> (.reset git-repo)
+                  (.setMode org.eclipse.jgit.api.ResetCommand$ResetType/MIXED)
+                  (.setRef (str "origin/" source-branch))
+                  (.call)))
+
+            ;; Add all changes
+            (log/info "Adding all changes")
+            (-> (.add git-repo)
+                (.addFilepattern ".")
+                (.call))
+
+            ;; Commit changes
+            (log/info "Committing changes")
+            (-> (.commit git-repo)
+                (.setMessage "Update content")
+                (.call))
+
+            ;; Push to destination branch
+            (log/info "Pushing to destination branch" {:dest-branch dest-branch})
+            (-> (.push git-repo)
+                (.setRemote "origin")
+                (.setRefSpecs [(RefSpec. (str "refs/heads/" branch-name ":refs/heads/" dest-branch))])
+                (.setForce true)
+                (.setCredentialsProvider
+                 (when token
+                   (UsernamePasswordCredentialsProvider. "x-access-token" token)))
+                (.call))
+
+            (finally
+              (.close git-repo))))
+        (catch Exception e
+          (log/error e "Git push failed")
+          (throw e))))))
 
 (defn get-source
   "Returns the source that we should actually use. A source implements the `ISource` protocol."
