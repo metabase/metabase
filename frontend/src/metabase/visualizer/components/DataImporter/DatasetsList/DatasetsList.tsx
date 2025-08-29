8303387d00d52073ea84c52c23a5f7e6c3aea74b
Tyler Plude <tsplude@gmail.com>
2025-08-29 13:43:24 -0700

    init

--- frontend/src/metabase/visualizer/components/DataImporter/DatasetsList/DatasetsList.tsx
+++ frontend/src/metabase/visualizer/components/DataImporter/DatasetsList/DatasetsList.tsx
@@ -138,6 +138,7 @@ export function DatasetsList({
         models: ["card", "dataset", "metric"],
         include_dashboard_questions: true,
         include_metadata: true,
+        search_engine: "appdb",
         ...(visualizationType &&
           isCartesianChart(visualizationType) &&
           search.length === 0 && {
