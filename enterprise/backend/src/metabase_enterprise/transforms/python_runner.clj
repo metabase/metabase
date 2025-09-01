b8fb092432c5395717c96578d4111403c6e64384
Chris Truter <chris@metabase.com>
2025-09-01 14:38:30 +0200

    Linter stuff

--- enterprise/backend/src/metabase_enterprise/transforms/python_runner.clj
+++ enterprise/backend/src/metabase_enterprise/transforms/python_runner.clj
@@ -2,6 +2,7 @@
   (:require
    [clj-http.client :as http]
    [clojure.java.io :as io]
+   [clojure.string :as str]
    [metabase-enterprise.transforms.settings :as transforms.settings]
    [metabase.driver :as driver]
    [metabase.query-processor.compile :as qp.compile]
@@ -12,19 +13,13 @@
   (:import
    (com.amazonaws.auth BasicAWSCredentials)
    (com.amazonaws.client.builder AwsClientBuilder$EndpointConfiguration)
-   (com.amazonaws.services.s3 AmazonS3 AmazonS3ClientBuilder)
+   (com.amazonaws.services.s3 AmazonS3ClientBuilder)
    (com.amazonaws.services.s3.model GeneratePresignedUrlRequest ObjectMetadata PutObjectRequest)
    (java.io BufferedWriter ByteArrayInputStream ByteArrayOutputStream File OutputStream OutputStreamWriter)
-   (java.net URL)
    (java.nio.charset StandardCharsets)
    (java.util Date)))
 
 (set! *warn-on-reflection* true)
-
-(defn- safe-slurp
-  "Safely slurp a file, returning empty string on error."
-  [file]
-  (try (slurp file) (catch Exception _ "")))
 
 (defn- safe-delete
   "Safely delete a file."
@@ -51,28 +46,18 @@
   (driver/with-driver driver
     (let [native (qp.compile/compile {:type :query, :database db-id :query query})
 
-          query {:database db-id
-                 :type :native
-                 :native native}]
+          query  {:database db-id
+                  :type     :native
+                  :native   native}]
       (qp.store/with-metadata-provider db-id
         (driver/execute-reducible-query driver query {} respond)))))
 
-(defn- write-table-data! [id file]
-  (let [db-id (t2/select-one-fn :db_id (t2/table-name :model/Table) :id id)
-        driver (t2/select-one-fn :engine :model/Database db-id)
-        ;; TODO: limit
-        query {:source-table id}]
-    (execute-mbql-query driver db-id query
-                        (fn [{cols-meta :cols} reducible-rows]
-                          (with-open [os (io/output-stream file)]
-                            (write-to-stream! os (mapv :name cols-meta) reducible-rows))))))
-
 (defn- write-table-data-to-stream! [id]
-  (let [db-id (t2/select-one-fn :db_id (t2/table-name :model/Table) :id id)
+  (let [db-id  (t2/select-one-fn :db_id (t2/table-name :model/Table) :id id)
         driver (t2/select-one-fn :engine :model/Database db-id)
         ;; TODO: limit
-        query {:source-table id}
-        baos (ByteArrayOutputStream.)]
+        query  {:source-table id}
+        baos   (ByteArrayOutputStream.)]
     (execute-mbql-query driver db-id query
                         (fn [{cols-meta :cols} reducible-rows]
                           (write-to-stream! baos (mapv :name cols-meta) reducible-rows)))
@@ -106,7 +91,7 @@
       ;; Replace localhost with localstack so the URL works from within Docker containers
       (-> (.generatePresignedUrl s3-client url-request)
           (.toString)
-          (clojure.string/replace "http://localhost:4566" "http://localstack:4566")))))
+          (str/replace "http://localhost:4566" "http://localstack:4566")))))
 
 (defn- generate-presigned-put-url [^com.amazonaws.services.s3.AmazonS3 s3-client bucket-name key]
   (let [one-hour    (* 60 60 1000)
@@ -117,11 +102,11 @@
     ;; Replace localhost with localstack so the URL works from within Docker containers
     (-> (.generatePresignedUrl s3-client url-request)
         (.toString)
-        (clojure.string/replace "http://localhost:4566" "http://localstack:4566"))))
+        (str/replace "http://localhost:4566" "http://localstack:4566"))))
 
 (defn- read-from-s3 [^com.amazonaws.services.s3.AmazonS3 s3-client bucket-name key]
   (try
-    (let [object (.getObject s3-client bucket-name key)]
+    (let [object (.getObject s3-client ^String bucket-name ^String key)]
       (slurp (.getObjectContent object)))
     (catch com.amazonaws.services.s3.model.AmazonS3Exception e
       (if (= 404 (.getStatusCode e))
@@ -131,9 +116,9 @@
 (defn execute-python-code
   "Execute Python code using the Python execution server."
   [code table-name->id]
-  (let [mount-path (transforms.settings/python-execution-mount-path)
+  (let [mount-path    (transforms.settings/python-execution-mount-path)
         work-dir-name (str "run-" (System/currentTimeMillis) "-" (rand-int 10000))
-        work-dir (str mount-path "/" work-dir-name)
+        work-dir      (str mount-path "/" work-dir-name)
         work-dir-file (io/file work-dir)]
 
     ;; Ensure mount base path exists
@@ -142,40 +127,40 @@
     (.mkdirs work-dir-file)
 
     (try
-      (let [server-url (transforms.settings/python-execution-server-url)
-            s3-client (create-s3-client)
-            bucket-name "metabase-python-runner"
+      (let [server-url      (transforms.settings/python-execution-server-url)
+            s3-client       (create-s3-client)
+            bucket-name     "metabase-python-runner"
             ;; Generate S3 keys for output files
-            output-key (str work-dir-name "/output.csv")
-            stdout-key (str work-dir-name "/stdout.txt")
-            stderr-key (str work-dir-name "/stderr.txt")
+            output-key      (str work-dir-name "/output.csv")
+            stdout-key      (str work-dir-name "/stdout.txt")
+            stderr-key      (str work-dir-name "/stderr.txt")
             ;; Generate presigned URLs for writing
-            output-url (generate-presigned-put-url s3-client bucket-name output-key)
-            stdout-url (generate-presigned-put-url s3-client bucket-name stdout-key)
-            stderr-url (generate-presigned-put-url s3-client bucket-name stderr-key)
+            output-url      (generate-presigned-put-url s3-client bucket-name output-key)
+            stdout-url      (generate-presigned-put-url s3-client bucket-name stdout-key)
+            stderr-url      (generate-presigned-put-url s3-client bucket-name stderr-key)
             ;; Upload input table data
             table-name->url (into {} (map (fn [[table-name id]]
                                             (let [s3-key (str work-dir-name "/" (gensym) ".jsonl")
-                                                  data (write-table-data-to-stream! id)
-                                                  url (upload-to-s3-and-get-url s3-client bucket-name s3-key data)]
+                                                  data   (write-table-data-to-stream! id)
+                                                  url    (upload-to-s3-and-get-url s3-client bucket-name s3-key data)]
                                               [table-name url])))
                                   table-name->id)
-            response (http/post (str server-url "/execute")
-                                {:content-type     :json
-                                 :accept           :json
-                                 :body             (json/encode {:code          code
-                                                                 :working_dir   work-dir
-                                                                 :timeout       30
-                                                                 :table_mapping table-name->url
-                                                                 :output_url    output-url
-                                                                 :stdout_url    stdout-url
-                                                                 :stderr_url    stderr-url})
-                                 :throw-exceptions false
-                                 :as               :json})
+            response        (http/post (str server-url "/execute")
+                                       {:content-type     :json
+                                        :accept           :json
+                                        :body             (json/encode {:code          code
+                                                                        :working_dir   work-dir
+                                                                        :timeout       30
+                                                                        :table_mapping table-name->url
+                                                                        :output_url    output-url
+                                                                        :stdout_url    stdout-url
+                                                                        :stderr_url    stderr-url})
+                                        :throw-exceptions false
+                                        :as               :json})
 
-            result  (:body response)
+            result          (:body response)
             ;; TODO look into why some tests return json and others strings
-            result (if (string? result) (json/decode result keyword) result)]
+            result          (if (string? result) (json/decode result keyword) result)]
 
         (try
           (if (and (= 200 (:status response))
@@ -184,7 +169,7 @@
             (let [output-content (read-from-s3 s3-client bucket-name output-key)
                   stdout-content (read-from-s3 s3-client bucket-name stdout-key)
                   stderr-content (read-from-s3 s3-client bucket-name stderr-key)]
-              (if (not (clojure.string/blank? output-content))
+              (if (not (str/blank? output-content))
                 {:status 200
                  :body   {:output output-content
                           :stdout stdout-content
@@ -213,4 +198,4 @@
 
       (catch Exception e
         {:status 500
-         :body {:error (str "Failed to connect to Python execution server: " (.getMessage e))}}))))
+         :body   {:error (str "Failed to connect to Python execution server: " (.getMessage e))}}))))
