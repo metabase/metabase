19c7fa5bc34b41a36af414e166e05e28a44ca5de
Timofei <timofei@metabase.com>
2025-09-02 14:33:27 +0400

    Add disc/memory caching for hosted bundle on top of etag caching

--- src/metabase/server/middleware/etag_cache.clj
+++ src/metabase/server/middleware/etag_cache.clj
@@ -1,5 +1,4 @@
 (ns metabase.server.middleware.etag-cache
-  "ETag caching-related Ring middleware."
   (:require
    [clojure.string :as str]
    [metabase.config.core :as config]
@@ -7,52 +6,27 @@
 
 (set! *warn-on-reflection* true)
 
-(def etag-header
-  "Headers that tell browsers to cache a static resource and revalidate using the etag with the Metabase version hash."
-  {"Cache-Control" "public, max-age=0, must-revalidate"
-   ;; according to the spec, the ETag should be a quoted string
-   "ETag" (format "\"%s\"" config/mb-version-hash)})
+(defn- current-etag-header []
+  {"ETag" (format "\"%s\"" config/mb-version-hash)})
 
-(defn- matches-metabase-version-hash?
-  "Returns true if the If-None-Match header (possibly weak/quoted) equals the current version hash."
-  [if-none-match-value]
-  (when if-none-match-value
-    (let [normalized
-          (-> if-none-match-value
-              ;; strip any leading "W/" (weak ETag) prefix
-              (str/replace-first #"^W/" "")
-              ;; strip any leading or trailing quotes
-              (str/replace #"^\"|\"$" ""))]
-      (= normalized config/mb-version-hash))))
+(defn- parse-if-none-match [s]
+  (when s
+    (->> (str/split s #"\s*,\s*")
+         (map #(-> %
+                   (str/replace-first #"^W/" "")
+                   (str/replace #"^\"|\"$" "")))
+         set)))
 
-(def ^:private not-modified-response
-  "Returns a 304 Not Modified response with ETag and Cache-Control headers for the current Metabase version hash."
-  (-> (response/response "")
-      (response/status 304)
-      (update :headers merge etag-header)))
+(defn- etag-matches? [if-none-match]
+  (contains? (parse-if-none-match if-none-match) config/mb-version-hash))
 
-(defn- js-response-with-etag
-  "Given a Ring response map (typically from `resource-response`), set
-   the correct Content-Type and Metabase-version-based cache headers
-   for embedding-sdk.js."
-  [response]
-  (-> response
-      (response/content-type "application/javascript; charset=UTF-8")
-      (update :headers merge etag-header)))
-
-(defn js-etag-handler
-  "Returns a Ring handler that serves a JS file with ETag/Cache-Control headers.
-   If the If-None-Match header matches the current version hash and not in dev mode,
-   returns a 304 Not Modified response instead."
-  [resource-path]
-  (fn [request]
-    (let [if-none-match (get-in request [:headers "if-none-match"])]
-      (if (and (not config/is-dev?)
-               (matches-metabase-version-hash? if-none-match))
-
-        ;; send the pre-built 304 response
-        not-modified-response
-
-        ;; else, serve the file
-        (js-response-with-etag
-         (response/resource-response resource-path))))))
+(defn with-etag
+  "Return 304 + ETag if If-None-Match matches; else 200 base + ETag.
+   NOTE: does not set Cache-Control or Content-Type."
+  [response request]
+  (if (etag-matches? (get-in request [:headers "if-none-match"]))
+    (-> (response/response "")
+        (response/status 304)
+        (update :headers merge (current-etag-header)))
+    (-> response
+        (update :headers merge (current-etag-header)))))
--- test/metabase/server/middleware/etag_cache_test.clj
+++ test/metabase/server/middleware/etag_cache_test.clj
@@ -7,39 +7,57 @@
 
 (set! *warn-on-reflection* true)
 
-(deftest js-etag-handler-test
-  (testing "returns 304 when ETag matches and not in dev mode"
-    (with-redefs [config/is-dev? false
-                  response/resource-response
-                  (constantly {:status 200 :headers {} :body "dummy js"})]
-      (let [etag     (format "\"%s\"" config/mb-version-hash)
-            handler  (mw.etag-cache/js-etag-handler "frontend_client/app/embedding-sdk.js")
-            response (handler {:headers {"if-none-match" etag}})]
-        (is (= 304 (:status response)))
-        (is (= "" (:body response)))
-        (is (= mw.etag-cache/etag-header (:headers response))))))
+(defn- base-response
+  ([] (base-response {}))
+  ([headers]
+   {:status 200
+    :headers headers
+    :body "dummy js"}))
 
-  (testing "returns 200 with correct headers when ETag does not match"
-    (with-redefs [config/is-dev? false
-                  response/resource-response
-                  (constantly {:status 200 :headers {} :body "dummy js"})]
-      (let [etag     (format "\"different\"")
-            handler  (mw.etag-cache/js-etag-handler "frontend_client/app/embedding-sdk.js")
-            response (handler {:headers {"if-none-match" etag}})
-            headers  (:headers response)]
-        (is (= 200 (:status response)))
-        (is (= "application/javascript; charset=UTF-8"
-               (get headers "Content-Type")))
-        (is (= mw.etag-cache/etag-header
-               (select-keys headers (keys mw.etag-cache/etag-header))))))))
+(deftest with-etag-returns-304-when-etag-matches
+  (testing "Exact strong ETag match returns 304 with only ETag header added"
+    (let [etag (format "\"%s\"" config/mb-version-hash)
+          resp (mw.etag-cache/with-etag (base-response) {:headers {"if-none-match" etag}})]
+      (is (= 304 (:status resp)))
+      (is (= "" (:body resp)))
+      (is (= etag (get-in resp [:headers "ETag"])))
+      (is (nil? (get-in resp [:headers "Cache-Control"])))
+      (is (nil? (get-in resp [:headers "Content-Type"])))))
 
-(testing "returns 200 when in dev mode, ignoring ETag match"
-  (with-redefs [config/is-dev? true
-                response/resource-response
-                (constantly {:status 200 :headers {} :body "dummy js"})]
-    (let [etag     (format "\"%s\"" config/mb-version-hash)
-          handler  (mw.etag-cache/js-etag-handler "frontend_client/app/embedding-sdk.js")
-          response (handler {:headers {"if-none-match" etag}})]
-      (is (= 200 (:status response)))
-      (is (= mw.etag-cache/etag-header
-             (select-keys (:headers response) (keys mw.etag-cache/etag-header)))))))
+  (testing "Weak ETag (W/) is treated as match"
+    (let [etag-weak (format "W/\"%s\"" config/mb-version-hash)
+          resp      (mw.etag-cache/with-etag (base-response) {:headers {"if-none-match" etag-weak}})]
+      (is (= 304 (:status resp)))
+      (is (= "" (:body resp)))
+      (is (= (format "\"%s\"" config/mb-version-hash)
+             (get-in resp [:headers "ETag"])))))
+
+  (testing "Multiple ETags in If-None-Match; any match triggers 304"
+    (let [header (format "\"other\", W/\"%s\", \"another\"" config/mb-version-hash)
+          resp   (mw.etag-cache/with-etag (base-response) {:headers {"if-none-match" header}})]
+      (is (= 304 (:status resp)))
+      (is (= (format "\"%s\"" config/mb-version-hash)
+             (get-in resp [:headers "ETag"]))))))
+
+(deftest with-etag-returns-200-and-adds-etag-when-no-match
+  (testing "ETag does not match → 200; adds ETag, preserves existing headers; no Cache-Control/Content-Type"
+    (let [resp (mw.etag-cache/with-etag
+                 (base-response {"X-Foo" "bar"})
+                 {:headers {"if-none-match" "\"different\""}})]
+      (is (= 200 (:status resp)))
+      (is (= "dummy js" (:body resp)))
+      (is (= (format "\"%s\"" config/mb-version-hash)
+             (get-in resp [:headers "ETag"])))
+      (is (= "bar" (get-in resp [:headers "X-Foo"])))
+      (is (nil? (get-in resp [:headers "Cache-Control"])))
+      (is (nil? (get-in resp [:headers "Content-Type"])))))
+
+  (testing "Missing If-None-Match → 200; adds ETag only"
+    (let [resp (mw.etag-cache/with-etag (base-response) {:headers {}})]
+      (is (= 200 (:status resp)))
+      (is (= "dummy js" (:body resp)))
+      (is (= (format "\"%s\"" config/mb-version-hash)
+             (get-in resp [:headers "ETag"])))
+      (is (nil? (get-in resp [:headers "Cache-Control"])))
+      (is (nil? (get-in resp [:headers "Content-Type"]))))))
+
