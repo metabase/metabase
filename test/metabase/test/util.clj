27be472f92ce761e3967b066a0421fdeb092fed0
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-09-03 02:01:58 +0300

    Make Usage Analytics work on mysql (#62357) (#63048)
    Co-authored-by: bryan <bryan.maass@gmail.com>
    Co-authored-by: dan sutton <dan@dpsutton.com>

--- test/metabase/test/util.clj
+++ test/metabase/test/util.clj
@@ -1537,7 +1537,7 @@
   `(fn [{:keys ~(mapv (comp symbol name) bindings)}]
      ~@body))
 
-(defn do-poll-until [^Long timeout-ms thunk]
+(defn do-poll-until [^Long timeout-ms code thunk]
   (let [result-prom (promise)
         _timeouter (future (Thread/sleep timeout-ms) (deliver result-prom ::timeout))
         _runner (future (loop []
@@ -1546,7 +1546,8 @@
                             (recur))))
         result @result-prom]
     (cond (= result ::timeout) (throw (ex-info (str "Timeout after " timeout-ms "ms")
-                                               {:timeout-ms timeout-ms}))
+                                               {:timeout-ms timeout-ms
+                                                :code code}))
           (instance? Throwable result) (throw result)
           :else result)))
 
@@ -1560,6 +1561,7 @@
   [timeout-ms & body]
   `(do-poll-until
     ~timeout-ms
+    '~@body
     (fn ~'poll-body [] ~@body)))
 
 (methodical/defmethod =?/=?-diff [(Class/forName "[B") (Class/forName "[B")]
