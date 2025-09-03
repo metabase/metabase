da75a527772d5815154dabb9a19776326646cd97
dan sutton <dan@dpsutton.com>
2025-09-03 09:21:55 -0500

    Bit of cleanup
    - remove warn-on-reflection; there is no interop
    - clean up a bit of duplication in sql clause generation
    - remove unncessary try/catch around t2 access
    - remove a few boolean calls on boolean functions

--- enterprise/backend/src/metabase_enterprise/embedding_hub/api.clj
+++ enterprise/backend/src/metabase_enterprise/embedding_hub/api.clj
@@ -9,8 +9,6 @@
    [metabase.premium-features.core :as premium-features]
    [toucan2.core :as t2]))
 
-(set! *warn-on-reflection* true)
-
 (defn- has-user-added-database? []
   (t2/exists? :model/Database {:where [:and
                                        [:= :is_sample false]
@@ -19,32 +17,27 @@
 (defn- has-user-created-dashboard? []
   (let [example-dashboard-id (appearance/example-dashboard-id)
         audit-collection-ids (filter some? [(when-let [audit-coll (audit/default-audit-collection)] (:id audit-coll))
-                                            (when-let [custom-coll (audit/default-custom-reports-collection)] (:id custom-coll))])
-        where-clause [:and
-                      [:= :archived false]
-                      (when example-dashboard-id [:not= :id example-dashboard-id])
-                      (when (seq audit-collection-ids) [:not-in :collection_id audit-collection-ids])]
-        where-clause (filterv some? where-clause)]
-    (t2/exists? :model/Dashboard {:where where-clause})))
+                                            (when-let [custom-coll (audit/default-custom-reports-collection)] (:id custom-coll))])]
+    (t2/exists? :model/Dashboard {:where (cond-> [:and
+                                                  [:= :archived false]]
+                                           example-dashboard-id (conj [:not= :id example-dashboard-id])
+                                           (seq audit-collection-ids) (conj [:not-in :collection_id audit-collection-ids]))})))
 
 (defn- has-configured-sandboxes? []
-  (boolean
-   (and (premium-features/has-feature? :sandboxes)
-        (try
-          (t2/exists? :model/GroupTableAccessPolicy)
-          (catch Throwable _ false)))))
+  (and (premium-features/has-feature? :sandboxes)
+       (t2/exists? :model/GroupTableAccessPolicy)))
 
 (defn- has-configured-sso? []
-  (boolean (or (and (premium-features/has-feature? :sso-jwt) (sso/jwt-enabled) (sso/jwt-configured))
-               (and (premium-features/has-feature? :sso-saml) (sso/saml-enabled) (sso/saml-configured)))))
+  (or (and (premium-features/has-feature? :sso-jwt) (sso/jwt-enabled) (sso/jwt-configured))
+      (and (premium-features/has-feature? :sso-saml) (sso/saml-enabled) (sso/saml-configured))))
 
 (defn- embedding-hub-checklist []
-  {"add-data" (has-user-added-database?)
-   "create-dashboard" (has-user-created-dashboard?)
+  {"add-data"                      (has-user-added-database?)
+   "create-dashboard"              (has-user-created-dashboard?)
    "configure-row-column-security" (has-configured-sandboxes?)
-   "create-test-embed" (boolean (embedding.settings/embedding-hub-test-embed-snippet-created))
-   "embed-production" (boolean (embedding.settings/embedding-hub-production-embed-snippet-created))
-   "secure-embeds" (boolean (has-configured-sso?))})
+   "create-test-embed"             (embedding.settings/embedding-hub-test-embed-snippet-created)
+   "embed-production"              (embedding.settings/embedding-hub-production-embed-snippet-created)
+   "secure-embeds"                 (has-configured-sso?)})
 
 (api.macros/defendpoint :get "/checklist"
   "Get the embedding hub checklist status, indicating which setup steps have been completed."
