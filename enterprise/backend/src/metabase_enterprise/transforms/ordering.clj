d4344bbf7cf4144f7979a3ac43999a6da9528571
William Zimrin <william@metabase.com>
2025-09-03 16:51:31 -0500

    extend docstring

--- enterprise/backend/src/metabase_enterprise/transforms/ordering.clj
+++ enterprise/backend/src/metabase_enterprise/transforms/ordering.clj
@@ -126,7 +126,8 @@
 
 (defn available-transforms
   "Given an ordering (see transform-ordering), a set of running transform ids, and a set of completed transform ids,
-  computes which transforms are currently able to be run."
+  computes which transforms are currently able to be run.  Returns transform ids in the order that they appear in the
+  ordering map.  If you want them returned in a specific order, use a sorted-map-by."
   [ordering running complete]
   (for [[transform-id deps] ordering
         :when (and (not (or (running transform-id)
