55206891d22b1ac0f9c9cfd87e0017942c8cb834
Phoomparin Mano <phoomparin@gmail.com>
2025-09-03 16:14:15 +0700

    add tests for upsells

--- e2e/test/scenarios/embedding/embedding-smoketests.cy.spec.js
+++ e2e/test/scenarios/embedding/embedding-smoketests.cy.spec.js
@@ -107,12 +107,66 @@ describe("scenarios > embedding > smoke tests", { tags: "@OSS" }, () => {
       cy.findByRole("article", { name: /Interactive embedding/ }).within(() => {
         cy.findByText("Interactive embedding");
 
-        cy.findByRole("link", { name: "Try for free" })
-          .should("have.attr", "href")
-          .and(
-            "eq",
-            "https://www.metabase.com/product/embedded-analytics?utm_source=product&utm_medium=upsell&utm_campaign=embedding-interactive&utm_content=embedding-page&source_plan=oss",
-          );
+        cy.findByRole("button", { name: "Try for free" });
+      });
+    });
+
+    it("should show the sdk settings page with upsell button", () => {
+      cy.on("window:before:load", (win) => {
+        // prevent Cypress opening in a new window/tab and spy on this method
+        cy.stub(win, "open").as("open");
+      });
+
+      cy.visit("/admin/settings/embedding-in-other-applications/sdk");
+
+      mainPage().within(() => {
+        cy.contains("SDK for React").should("be.visible");
+        cy.contains("Embedded Analytics JS").should("be.visible");
+      });
+
+      cy.findByRole("button", { name: "Try for free" }).click();
+
+      cy.get("@open").should((spy) => {
+        const url = spy.getCall(0).args[0];
+
+        expect(url).to.include(
+          "https://test-store.metabase.com/checkout/upgrade/self-hosted",
+        );
+
+        expect(url).to.match(/utm_source=product/);
+        expect(url).to.match(/utm_medium=upsell/);
+        expect(url).to.match(/utm_campaign=embedded-analytics-js/);
+        expect(url).to.match(/utm_content=embedding-page/);
+        expect(url).to.match(/source_plan=oss/);
+      });
+    });
+
+    it("should show the interactive embed card with upsell button", () => {
+      cy.on("window:before:load", (win) => {
+        // prevent Cypress opening in a new window/tab and spy on this method
+        cy.stub(win, "open").as("open");
+      });
+
+      cy.visit("/admin/settings/embedding-in-other-applications");
+
+      mainPage().within(() => {
+        cy.contains("Interactive embedding").should("be.visible");
+      });
+
+      cy.findByRole("button", { name: "Try for free" }).click();
+
+      cy.get("@open").should((spy) => {
+        const url = spy.getCall(0).args[0];
+
+        expect(url).to.include(
+          "https://test-store.metabase.com/checkout/upgrade/self-hosted",
+        );
+
+        expect(url).to.match(/utm_source=product/);
+        expect(url).to.match(/utm_medium=upsell/);
+        expect(url).to.match(/utm_campaign=embedding-interactive/);
+        expect(url).to.match(/utm_content=embedding-page/);
+        expect(url).to.match(/source_plan=oss/);
       });
     });
 
