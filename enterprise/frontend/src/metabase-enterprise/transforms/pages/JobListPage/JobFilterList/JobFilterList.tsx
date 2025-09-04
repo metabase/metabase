df28bdb0e8ddfa1814401debbc4860a67582c944
Romeo Van Snick <romeo@romeovansnick.be>
2025-09-04 15:10:22 +1000

    Disable shortcuts that are in the past for Next run, which is always in the future

--- enterprise/frontend/src/metabase-enterprise/transforms/pages/JobListPage/JobFilterList/JobFilterList.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/pages/JobListPage/JobFilterList/JobFilterList.tsx
@@ -2,6 +2,7 @@ import { replace } from "react-router-redux";
 import { t } from "ttag";
 
 import { useDispatch } from "metabase/lib/redux";
+import type { DatePickerShortcut } from "metabase/querying/filters/types";
 import { Group } from "metabase/ui";
 import { getJobListUrl } from "metabase-enterprise/transforms/urls";
 import type { TransformTag, TransformTagId } from "metabase-types/api";
@@ -14,6 +15,8 @@ type FilterListProps = {
   tags: TransformTag[];
   params: JobListParams;
 };
+
+const NO_SHORTCUTS: DatePickerShortcut[] = [];
 
 export function JobFilterList({ tags, params }: FilterListProps) {
   const dispatch = useDispatch();
@@ -41,6 +44,7 @@ export function JobFilterList({ tags, params }: FilterListProps) {
         label={t`Next run`}
         value={params.nextRunStartTime}
         onChange={handleNextRunStartTimeChange}
+        availableShortcuts={NO_SHORTCUTS}
       />
       <TagFilterWidget
         tagIds={params.transformTagIds ?? []}
