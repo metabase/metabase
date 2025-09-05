metabase/metabase

commit 1dd56e5f148db1730726df2c720b417abd40219f

Postgres Replication to Cloud Storage (#59709)
* feat: harbormaster openapi client

* feat: pg replication backend

* feat: pg replication frontend

* ci: label for build docker job

* fix: update attached-dwh crufty tables on sync :update

Fix https://linear.app/metabase/issue/CLO-4223/mb-does-not-update-crufty-table-settings-on-config-change

* fixup: minor FE review items

* fixup: pg-replication -> database-replication

* fixup: use db driver feature in DatabaseReplicationSection

* fixup: jt link needs unique k

* fix: cache? not cache on setting

enterprise/backend/src/metabase_enterprise/harbormaster/client.clj
@@ -3,6 +3,8 @@
   (:require
    [clj-http.client :as http]
    [clojure.string :as str]
+   [martian.clj-http :as martian-http]
+   [martian.core :as martian]
    [medley.core :as m]
    [metabase.api.settings :as api.auth]
    [metabase.cloud-migration.core :as cloud-migration]
@@ -115,3 +117,102 @@
         response          (decode-response unparsed-response url request)
         success?          (calculate-success response url request)]
     [(if success? :ok :error) response]))
+
+;; OpenAPI-based API
+
+(defn- bearer-auth [secret]
+  {:name ::add-bearer-token
+   :enter (fn [ctx] (assoc-in ctx [:request :headers "Authorization"] (str "Bearer " secret)))})
+
+(defn- create-client
+  [store-api-url api-key]
+  (martian-http/bootstrap-openapi
+   (str store-api-url "/openapi.json")
+   ;; martian options for calling operations
+   {:server-url   store-api-url
+    :interceptors (into [(bearer-auth api-key)] martian-http/default-interceptors)}
+   ;; clj-http options for loading the openapi.json itself
+   {:headers {"Authorization" (str "Bearer " api-key)}}))
+
+(def ^:private create-client-memo
+  (memoize create-client))
+
+(defn- client []
+  (let [store-api-url (cloud-migration/store-api-url)
+        api-key       (api.auth/api-key)]
+    (when (str/blank? store-api-url)
+      (log/error "Missing store-api-url. Cannot create hm client config.")
+      (throw (ex-info (tru "Missing store-api-url.") {:store-api-url store-api-url})))
+    (when (str/blank? api-key)
+      (log/error "Missing api-key. Cannot create hm client config.")
+      (throw (ex-info (tru "Missing api-key.") {:api-key api-key})))
+    (create-client-memo store-api-url api-key)))
+
+(defn explore
+  "Explore the Harbormaster API, using Martian.
+  e.g.
+    (explore)                   ;; endpoint listing
+    (explore :list-connections) ;; spec for the endpoint with operation-id :list-connections, from the (explore) call."
+  [& args]
+  (apply martian/explore (client) args))
+
+(defn call
+  "Call the API, using Martian. Will throw on non 2xx.
+  e.g.
+    ;; call the :foo endpoint with {:some-id id}
+    ;; use (explore :list-connections) for params, if any, and pass them in a map
+    (call :list-connections)"
+  [operation-id & {:as args}]
+  (try
+    (:body (martian/response-for (client) operation-id args))
+    (catch Exception e
+      (log/errorf e "Error on Harbormaster operation call %s" operation-id)
+      (throw e))))
+
+(defn request
+  "Same as call, but return the request that will be performed.
+  Useful for debugging calls."
+  [operation-id & {:as args}]
+  (martian/request-for (client) operation-id args))
+
+(comment
+  ;; List all available operations on the API.
+  (explore)
+  ;; => [[:list-connections "Query connections for instance-id."]
+  ;;     [:create-connection "Create a connection."]
+  ;;     [:sync-connection "Sync a connection."]
+  ;;     [:get-connection "Pull a connection."]
+  ;;     [:update-connection "Update a connection."]
+  ;;     [:delete-connection "Delete a connection."]]
+
+  ;; Check params and returns for :list-connections
+  (explore :list-connections)
+  ;; => {:summary "Query connections for instance-id.",
+  ;;     :parameters {},
+  ;;     :returns
+  ;;     {200
+  ;;      [{{:k :hosted_instance_resource_id} Int,
+  ;;        {:k :created_at} (cond-pre Str Inst),
+  ;;        {:k :last_sync_at} Any,
+  ;;        {:k :error_detail} Any,
+  ;;        :type (enum "gdrive" "google_spreadsheet" "pg_replication" "sql_database"),
+  ;;        {:k :status_reason} Any,
+  ;;        {:k :last_sync_started_at} Any,
+  ;;        {:k :updated_at} (cond-pre Str Inst),
+  ;;        {:k :hosted_instance_id} (cond-pre Str Uuid),
+  ;;        :status (enum "syncing" "error" "paused" "initializing" "active"),
+  ;;        :id (cond-pre Str Uuid),
+  ;;        :error Any}]}}
+
+  ;; What's the request that (call :list-connections) will make?
+  (request :list-connections)
+  ;; => {:headers {"Authorization" "Bearer mb_api_key_128f293f4faca8ee89bdce1f36798e05c55715b4637801fac4a63101bb6f979d",
+  ;;               "Accept" "application/json"},
+  ;;     :method :get,
+  ;;     :url "http://harbormaster:5010/api/v2/mb/connections",
+  ;;     :as :text}
+
+  ;; Call the :list-connections operation.
+  (call :list-connections)
+  ;; => ()
+  )

.clj-kondo/config/modules/config.edn
@@ -1585,6 +1585,17 @@
            api
            util}}
 
+  enterprise/database-replication
+  {:team "Cloud"
+   :api  #{metabase-enterprise.database-replication.api
+           metabase-enterprise.database-replication.init}
+   :uses #{analytics
+           api
+           premium-features
+           settings
+           util
+           enterprise/harbormaster}}
+
   enterprise/database-routing
   {:team "Admin Webapp"
    :api  #{metabase-enterprise.database-routing.api}

.github/workflows/uberjar.yml
@@ -20,6 +20,8 @@ on:
       - "**test/**"
       - "**_test.clj"
       - "**/frontend/**.unit.*"
+  pull_request:
+    types: [labeled, opened, synchronize, reopened]
   workflow_dispatch:
     inputs:
       commit:
@@ -30,6 +32,8 @@ jobs:
     runs-on: ubuntu-22.04
     timeout-minutes: 5
     name: Get container info
+    # Skip if this is a PR without the build-docker-uberjar label
+    if: github.event_name != 'pull_request' || contains(github.event.pull_request.labels.*.name, 'build-docker-uberjar')
     outputs:
       editions: ${{ toJson(fromJson(steps.info.outputs.result).editions) }}
       ee: ${{ toJson(fromJson(steps.info.outputs.result).ee) }}

deps.edn
@@ -45,6 +45,7 @@
   com.github.blagerweij/liquibase-sessionlock {:mvn/version "1.6.9"}            ; session lock for liquibase migrations
   com.github.jknack/handlebars              ^:antq/exclude                      ; 4.4.0 requires java 17+
                                             {:mvn/version "4.3.1"}              ; templating engine
+  com.github.oliyh/martian-clj-http         {:mvn/version "0.1.31"}             ; openapi client
   com.github.pangloss/transducers           {:git/sha "c91d39046c8c64dc31b84ebb27154a8084fcda4c" ; like medley for transducers
                                              :git/url "https://github.com/pangloss/transducers"}
   com.github.seancorfield/honeysql          {:mvn/version "2.7.1310"}           ; Honey SQL 2. SQL generation from Clojure data maps

enterprise/backend/src/metabase_enterprise/api/routes.clj
@@ -15,6 +15,7 @@
    [metabase-enterprise.billing.api.routes]
    [metabase-enterprise.content-translation.routes]
    [metabase-enterprise.content-verification.api.routes]
+   [metabase-enterprise.database-replication.api :as database-replication.api]
    [metabase-enterprise.database-routing.api]
    [metabase-enterprise.email.api]
    [metabase-enterprise.gsheets.api :as gsheets.api]
@@ -44,6 +45,7 @@
    :collection-cleanup         (deferred-tru "Collection Cleanup")
    :content-translation        (deferred-tru "Content translation")
    :etl-connections            (deferred-tru "ETL Connections")
+   :etl-connections-pg         (deferred-tru "ETL Connections PG replication")
    :llm-autodescription        (deferred-tru "LLM Auto-description")
    :metabot-v3                 (deferred-tru "MetaBot")
    :scim                       (deferred-tru "SCIM configuration")
@@ -79,6 +81,10 @@
    "/autodescribe"                 (premium-handler 'metabase-enterprise.llm.api :llm-autodescription)
    "/billing"                      metabase-enterprise.billing.api.routes/routes
    "/content-translation"          (premium-handler metabase-enterprise.content-translation.routes/routes :content-translation)
+   "/database-replication"         (-> database-replication.api/routes ;; database-replication requires all these features.
+                                       (premium-handler :attached-dwh)
+                                       (premium-handler :etl-connections)
+                                       (premium-handler :etl-connections-pg))
    "/database-routing"             (premium-handler metabase-enterprise.database-routing.api/routes :database-routing)
    "/email"                        (premium-handler metabase-enterprise.email.api/routes :cloud-custom-smtp)
    "/gsheets"                      (-> gsheets.api/routes ;; gsheets requires both features.

enterprise/backend/src/metabase_enterprise/core/init.clj
@@ -9,6 +9,7 @@
    [metabase-enterprise.advanced-config.init]
    [metabase-enterprise.audit-app.init]
    [metabase-enterprise.cache.init]
+   [metabase-enterprise.database-replication.init]
    [metabase-enterprise.gsheets.init]
    [metabase-enterprise.llm.init]
    [metabase-enterprise.metabot-v3.init]

enterprise/backend/src/metabase_enterprise/database_replication/api.clj
@@ -1,0 +1,67 @@
+(ns metabase-enterprise.database-replication.api
+  (:require
+   [medley.core :as m]
+   [metabase-enterprise.database-replication.settings :as database-replication.settings]
+   [metabase-enterprise.harbormaster.client :as hm.client]
+   [metabase.api.common :as api]
+   [metabase.api.macros :as api.macros]
+   [metabase.api.routes.common :refer [+auth]]
+   [metabase.util.malli.schema :as ms]
+   [toucan2.core :as t2]))
+
+(set! *warn-on-reflection* true)
+
+(defn- pruned-database-replication-connections
+  "Delete any pg replication connections that don't exist anymore in Harbormaster.
+  Updates the setting if needed, and returns the pruned connections."
+  []
+  (let [hm-conn-ids  (->> (hm.client/call :list-connections) (filter #(-> % :type (= "pg_replication"))) (map :id) set)
+        conns        (database-replication.settings/database-replication-connections)
+        pruned-conns (m/filter-vals #(->> % :connection-id hm-conn-ids) conns)]
+    (when-not (= conns pruned-conns)
+      (database-replication.settings/database-replication-connections! pruned-conns))
+    pruned-conns))
+
+(defn- kw-id [id]
+  (->> id str keyword))
+
+(defn- database-id->connection-id [conns database-id]
+  (->> database-id kw-id (get conns) :connection-id))
+
+(api.macros/defendpoint :post "/connection/:database-id"
+  "Create a new PG replication connection for the specified database."
+  [{:keys [database-id]} :- [:map [:database-id ms/PositiveInt]]]
+  (api/check-400 (database-replication.settings/database-replication-enabled) "PG replication integration is not enabled.")
+  (let [database (t2/select-one :model/Database :id database-id)]
+    (api/check-404 database)
+    (api/check-400 (= :postgres (:engine database)) "PG replication is only supported for PostgreSQL databases.")
+    (let [conns (pruned-database-replication-connections)]
+      (api/check-400 (not (database-id->connection-id conns database-id)) "Database already has an active replication connection.")
+      (let [credentials (-> (:details database)
+                            (select-keys [:dbname :host :user :password :port])
+                            (update :port #(or % 5432))) ;; port is required in the API, but optional in MB
+            {:keys [schema-filters-type schema-filters-patterns]} (:details database)
+            schemas      (when-some [k ({"inclusion" :include, "exclusion" :exclude} schema-filters-type)]
+                           (when schema-filters-patterns
+                             {:schemas {k schema-filters-patterns}}))
+            secret       (merge {:credentials (merge {:dbtype "postgresql"} credentials)}
+                                schemas)
+            {:keys [id]} (hm.client/call :create-connection, :type "pg_replication", :secret secret)
+            conn         {:connection-id id}]
+        (database-replication.settings/database-replication-connections! (assoc conns (kw-id database-id) conn))
+        conn))))
+
+(api.macros/defendpoint :delete "/connection/:database-id"
+  "Delete PG replication connection for the specified database."
+  [{:keys [database-id]} :- [:map [:database-id ms/PositiveInt]]]
+  (api/check-400 (database-replication.settings/database-replication-enabled) "PG replication integration is not enabled.")
+  (let [conns (pruned-database-replication-connections)]
+    (when-let [connection-id (database-id->connection-id conns database-id)]
+      (hm.client/call :delete-connection, :connection-id connection-id)
+      (database-replication.settings/database-replication-connections! (dissoc conns (kw-id database-id)))
+      nil)))
+
+(def ^{:arglists '([request respond raise])} routes
+  "`/api/ee/database-replication` routes."
+  (api/+check-superuser
+   (api.macros/ns-handler *ns* api/+check-superuser +auth)))

enterprise/backend/src/metabase_enterprise/database_replication/init.clj
@@ -1,0 +1,3 @@
+(ns metabase-enterprise.database-replication.init
+  (:require
+   [metabase-enterprise.database-replication.settings]))

commit c8db4a667a721c1bdd988c8d51216509eb88fa9b

Cache Store OpenAPI description only for 5min
The Store API's OpenAPI description can change over time, but
Metabase's Store client would not notice.  For example, the client
would fail a HTTP request, not because the server rejected it, but
because the client "believes" it needs to send a field that the server
no longer requires.

Instead of caching the client and OpenAPI description for the whole
lifetime of the Metabase instance, cache it only for 5min.

Fixes: 1dd56e5f148db1730726df2c720b417abd40219f
References: https://metaboat.slack.com/archives/C010ZSXQY87/p1757002462314229

enterprise/backend/src/metabase_enterprise/harbormaster/client.clj
@@ -2,6 +2,7 @@
   "API client for interfacing with Harbormaster on store-api-url."
   (:require
    [clj-http.client :as http]
+   [clojure.core.memoize :as memoize]
    [clojure.string :as str]
    [martian.clj-http :as martian-http]
    [martian.core :as martian]
@@ -135,7 +136,8 @@
    {:headers {"Authorization" (str "Bearer " api-key)}}))
 
 (def ^:private create-client-memo
-  (memoize create-client))
+  (memoize/ttl create-client
+               :ttl/threshold (m.util/minutes->ms 5)))
 
 (defn- client []
   (let [store-api-url (store-api/store-api-url)