fb26211ac25365b0c6aae503747089694712812d
Aleksandr Lesnenko <alxnddr@gmail.com>
2025-09-05 12:20:02 -0400

    direct link to sync settings

--- enterprise/frontend/src/metabase-enterprise/git_sync/LibraryNav.tsx
+++ enterprise/frontend/src/metabase-enterprise/git_sync/LibraryNav.tsx
@@ -4,8 +4,8 @@ import { SettingsNavItem } from "metabase/admin/settings/components/SettingsNav"
 
 export const LibraryNav = () => {
   return (
-    <SettingsNavItem path="library" label={t`Library`} icon="folder">
-      <SettingsNavItem path="library/sync" label={t`Sync`} />
+    <SettingsNavItem path="library/sync" label={t`Library`} icon="folder">
+      {/* <SettingsNavItem path="library/sync" label={t`Sync`} /> */}
       {/* Not used for now so let's hide */}
       {/* <SettingsNavItem path="library/changes" label={t`Changes`} /> */}
     </SettingsNavItem>
