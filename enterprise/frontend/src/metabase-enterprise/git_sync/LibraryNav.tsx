ff37367b5aa56a78d5676c4ff736a62ec541c241
Aleksandr Lesnenko <alxnddr@gmail.com>
2025-09-05 12:14:43 -0400

    hide viewing changes

--- enterprise/frontend/src/metabase-enterprise/git_sync/LibraryNav.tsx
+++ enterprise/frontend/src/metabase-enterprise/git_sync/LibraryNav.tsx
@@ -6,7 +6,8 @@ export const LibraryNav = () => {
   return (
     <SettingsNavItem path="library" label={t`Library`} icon="folder">
       <SettingsNavItem path="library/sync" label={t`Sync`} />
-      <SettingsNavItem path="library/changes" label={t`Changes`} />
+      {/* Not used for now so let's hide */}
+      {/* <SettingsNavItem path="library/changes" label={t`Changes`} /> */}
     </SettingsNavItem>
   );
 };
