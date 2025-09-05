6d75564bdfc3ddceb87e0be596499432944bd437
Nathan Voxland <nathan@metabase.com>
2025-09-05 11:31:03 -0500

    Also avoid nil on native_query_vector

--- src/metabase/search/appdb/specialization/postgres.clj
+++ src/metabase/search/appdb/specialization/postgres.clj
@@ -60,7 +60,7 @@
 
    :with_native_query_vector
    [:||
-    (search.util/weighted-tsvector "A" (:name entity))
+    (search.util/weighted-tsvector "A" (or (:name entity) ""))
     (search.util/weighted-tsvector "B" (str/join " " (keep entity [:searchable_text :native_query])))]})
 
 ;; See https://www.postgresql.org/docs/current/textsearch-controls.html#TEXTSEARCH-RANKING
