e4123cec05ebd9eb5a9da774355c16ab826822da
Eric Normand <ericwnormand@gmail.com>
2025-09-08 09:10:19 -0500

    WIP

--- src/metabase/sync/sync_metadata/fks.clj
+++ src/metabase/sync/sync_metadata/fks.clj
@@ -29,22 +29,22 @@
                           ;; non-unique when lower-cased for some DBs. We have been doing case-insensitive matching
                           ;; since #5510 so this preserves behaviour to avoid possible regressions.
                           ;; It's possible this is to avoid
-                          :from   [[:metabase_field :f]]
-                          :join   [[:metabase_table :t] [:= :f.table_id :t.id]]
+                          :from [[:metabase_field :f]]
+                          :join [[:metabase_table :t] [:= :f.table_id :t.id]]
                           :left-join [[:metabase_field_user_settings :u] [:= :f.id :u.field_id]]
-                          :where  [:and
+                          :where [:and
                                    ;; ensure we are not overriding user-set fks
-                                   [:= :u.fk_target_field_id nil]
-                                   [:= :u.semantic_type nil]
+                                  [:= :u.fk_target_field_id nil]
+                                  [:= :u.semantic_type nil]
 
-                                   [:= :t.db_id db-id]
-                                   [:= [:lower :f.name] (u/lower-case-en column-name)]
-                                   [:= [:lower :t.name] (u/lower-case-en table-name)]
-                                   [:= [:lower :t.schema] (some-> table-schema u/lower-case-en)]
-                                   [:= :f.active true]
-                                   [:not= :f.visibility_type "retired"]
-                                   [:= :t.active true]
-                                   [:= :t.visibility_type nil]]})
+                                  [:= :t.db_id db-id]
+                                  [:= [:lower :f.name] (u/lower-case-en column-name)]
+                                  [:= [:lower :t.name] (u/lower-case-en table-name)]
+                                  [:= [:lower :t.schema] (some-> table-schema u/lower-case-en)]
+                                  [:= :f.active true]
+                                  [:not= :f.visibility_type "retired"]
+                                  [:= :t.active true]
+                                  [:= :t.visibility_type nil]]})
         fk-field-id-query (field-id-query db-id fk-table-schema fk-table-name fk-column-name)
         pk-field-id-query (field-id-query db-id pk-table-schema pk-table-name pk-column-name)
 
@@ -60,34 +60,34 @@
         q (case (mdb/db-type)
             :mysql
             {:update [:metabase_field :f]
-             :join   [[fk-field-id-query :fk] [:= :fk.id :f.id]
-                      [pk-field-id-query :pk]
-                      (valid-condition :pk.id)]
-             :set    {:fk_target_field_id :pk.id
+             :join [[fk-field-id-query :fk] [:= :fk.id :f.id]
+                    [pk-field-id-query :pk]
+                    (valid-condition :pk.id)]
+             :set {:fk_target_field_id :pk.id
                       ;; We need to reset has_field_values when it is auto-list as FKs should not be marked as such
-                      :has_field_values   [:case [:= :has_field_values "auto-list"] nil :else :has_field_values]
-                      :semantic_type      "type/FK"}}
+                   :has_field_values [:case [:= :has_field_values "auto-list"] nil :else :has_field_values]
+                   :semantic_type "type/FK"}}
             :postgres
             {:update [:metabase_field :f]
-             :from   [[fk-field-id-query :fk]]
-             :join   [[pk-field-id-query :pk] true]
-             :set    {:fk_target_field_id :pk.id
+             :from [[fk-field-id-query :fk]]
+             :join [[pk-field-id-query :pk] true]
+             :set {:fk_target_field_id :pk.id
                       ;; We need to reset has_field_values when it is auto-list as FKs should not be marked as such
-                      :has_field_values   [:case [:= :has_field_values "auto-list"] nil :else :has_field_values]
-                      :semantic_type      "type/FK"}
-             :where  [:and
-                      [:= :fk.id :f.id]
-                      (valid-condition :pk.id)]}
+                   :has_field_values [:case [:= :has_field_values "auto-list"] nil :else :has_field_values]
+                   :semantic_type "type/FK"}
+             :where [:and
+                     [:= :fk.id :f.id]
+                     (valid-condition :pk.id)]}
             :h2
             {:update [:metabase_field :f]
-             :set    {:fk_target_field_id pk-field-id-query
+             :set {:fk_target_field_id pk-field-id-query
                       ;; We need to reset has_field_values when it is auto-list as FKs should not be marked as such
-                      :has_field_values   [:case [:= :has_field_values "auto-list"] nil :else :has_field_values]
-                      :semantic_type      "type/FK"}
-             :where  [:and
-                      [:= :f.id fk-field-id-query]
-                      [:not= pk-field-id-query nil]
-                      (valid-condition pk-field-id-query)]})]
+                   :has_field_values [:case [:= :has_field_values "auto-list"] nil :else :has_field_values]
+                   :semantic_type "type/FK"}
+             :where [:and
+                     [:= :f.id fk-field-id-query]
+                     [:not= pk-field-id-query nil]
+                     (valid-condition pk-field-id-query)]})]
     (sql/format q :dialect (mdb/quoting-style (mdb/db-type)))))
 
 (mu/defn- mark-fk!
@@ -165,18 +165,53 @@
                                                                     :schema (:fk-table-schema fk-metadata))
                                   (sync-util/field-name-for-logging :name (:fk-column-name fk-metadata))))))))
 
+(mu/defn- clear-fk!
+  "Clears FK relationship for a field that no longer has an FK constraint in the database.
+  Updates fk_target_field_id to NULL and resets semantic_type to a sensible default."
+  [database :- i/DatabaseInstance
+   fk-metadata :- i/FKMetadataEntry]
+  (let [field-query {:select [:f.id]
+                     :from [[:metabase_field :f]]
+                     :join [[:metabase_table :t] [:= :f.table_id :t.id]]
+                     :left-join [[:metabase_field_user_settings :u] [:= :f.id :u.field_id]]
+                     :where [:and
+                             ;; ensure we are not overriding user-set fks
+                             [:= :u.fk_target_field_id nil]
+                             [:= :u.semantic_type nil]
+
+                             [:= :t.db_id (:id database)]
+                             [:= [:lower :f.name] (u/lower-case-en (:fk-column-name fk-metadata))]
+                             [:= [:lower :t.name] (u/lower-case-en (:fk-table-name fk-metadata))]
+                             [:= [:lower :t.schema] (some-> (:fk-table-schema fk-metadata) u/lower-case-en)]
+                             [:= :f.active true]
+                             [:not= :f.visibility_type "retired"]
+                             [:= :t.active true]
+                             [:= :t.visibility_type nil]
+                             ;; Only clear fields that currently have FK relationships
+                             [:not= :f.fk_target_field_id nil]]}
+        update-query {:update :metabase_field
+                      :set {:fk_target_field_id nil
+                            :semantic_type "type/Category"}
+                      :where [:in :id field-query]}]
+    (u/prog1 (t2/query update-query)
+      (when (pos? <>)
+        (log/info (u/format-color 'yellow "Clearing dropped foreign key from %s %s"
+                                  (sync-util/table-name-for-logging :name (:fk-table-name fk-metadata)
+                                                                    :schema (:fk-table-schema fk-metadata))
+                                  (sync-util/field-name-for-logging :name (:fk-column-name fk-metadata))))))))
+
 (mu/defn sync-fks-for-table!
   "Sync the foreign keys for a specific `table`."
   ([table :- i/TableInstance]
    (sync-fks-for-table! (table/database table) table))
 
   ([database :- i/DatabaseInstance
-    table    :- i/TableInstance]
+    table :- i/TableInstance]
    (sync-util/with-error-handling (format "Error syncing FKs for %s" (sync-util/name-for-logging table))
      (let [schema-names (when (driver.u/supports? (driver.u/database->driver database) :schemas database)
                           [(:schema table)])
-           fk-metadata  (into [] (fetch-metadata/fk-metadata database :schema-names schema-names :table-names [(:name table)]))]
-       {:total-fks   (count fk-metadata)
+           fk-metadata (into [] (fetch-metadata/fk-metadata database :schema-names schema-names :table-names [(:name table)]))]
+       {:total-fks (count fk-metadata)
         :updated-fks (sync-util/sum-numbers #(mark-fk! database %) fk-metadata)}))))
 
 (mu/defn sync-fks!
@@ -188,7 +223,7 @@
   This function also sets all the tables that should be synced to have `initial-sync-status=complete` once the sync is done."
   [database :- i/DatabaseInstance]
   (u/prog1 (sync-util/with-error-handling (format "Error syncing FKs for %s" (sync-util/name-for-logging database))
-             (let [driver       (driver.u/database->driver database)
+             (let [driver (driver.u/database->driver database)
                    schema-names (when (driver.u/supports? driver :schemas database)
                                   (sync-util/sync-schemas database))
                    db-fk-metadata (into #{} (fetch-metadata/fk-metadata database :schema-names schema-names))
