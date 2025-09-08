8e27d980be53dcdafc9b9809d1426e4527d681f5
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-09-08 15:57:24 -0400

    Remove deprecated metadata method

--- frontend/src/metabase/lib/redux/metadata.ts
+++ frontend/src/metabase/lib/redux/metadata.ts
@@ -1,5 +1,7 @@
 import { type Schema, normalize } from "normalizr";
 
+// this action is handled by entity reducers in `handleEntities` in
+// `frontend/src/metabase/lib/redux/utils.js`
 const UPDATE = "metabase/entities/UPDATE";
 
 export function updateMetadata(data: unknown, schema: Schema) {
