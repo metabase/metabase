ba34ffd9ca158c3cf9070787c7db45d7f1551a58
Mike Appleby <86076+appleby@users.noreply.github.com>
2025-09-08 17:11:20 -0700

    Add /ee/metabot-tools/get-transforms endpoint
    Returns a sequence of all python or native query transforms.
    
    BOT-391 [Backend] Create transform index tool API

--- enterprise/backend/test/metabase_enterprise/metabot_v3/tools/api_test.clj
+++ enterprise/backend/test/metabase_enterprise/metabot_v3/tools/api_test.clj
@@ -1110,3 +1110,35 @@
                     (request (assoc arguments
                                     :with_fields false
                                     :with_metrics false))))))))))
+
+(deftest get-transforms-test
+  (mt/with-premium-features #{:metabot-v3 :transforms}
+    (let [conversation-id (str (random-uuid))
+          rasta-ai-token (ai-session-token)
+          crowberto-ai-token (ai-session-token :crowberto (str (random-uuid)))]
+      (mt/with-temp [:model/Transform t1 {:name "People Transform"
+                                          :description "Simple select on People table"
+                                          :source {:type "query"
+                                                   :query (mt/native-query {:query "SELECT * FROM PEOPLE"})}
+                                          :target {:type "table" :name "t1_table" :schema nil}}
+                     ;; TODO make this a Python transform
+                     :model/Transform t2 {:name "Orders Transform"
+                                          :description "Simple select on Orders table"
+                                          :source {:type "query"
+                                                   :query (mt/native-query {:query "SELECT * FROM ORDERS"})}
+                                          :target {:type "table" :name "t2_table" :schema nil}}]
+        (testing "With insufficient permissions"
+          (is (= "You don't have permissions to do that."
+                 (mt/user-http-request :rasta :get 403 "ee/metabot-tools/get-transforms"
+                                       {:request-options {:headers {"x-metabase-session" rasta-ai-token}}}
+                                       {:conversation_id conversation-id}))))
+        (testing "With superuser permissions"
+          (is (=? {:structured_output [(select-keys t1 [:id :entity_id :name :description :source])
+                                       (select-keys t2 [:id :entity_id :name :description :source])]
+                   :conversation_id conversation-id}
+                  (-> (mt/user-http-request :rasta :get 200 "ee/metabot-tools/get-transforms"
+                                            {:request-options {:headers {"x-metabase-session" crowberto-ai-token}}}
+                                            {:conversation_id conversation-id})
+                      (update :structured_output (fn [output]
+                                                   (filter #(#{(:id t1) (:id t2)} (:id %))
+                                                           output)))))))))))
