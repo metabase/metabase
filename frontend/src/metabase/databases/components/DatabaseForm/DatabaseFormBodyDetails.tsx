96f687cbf60f60eb54a05b181329fb8933014bc6
Jakub Chodorowicz <jakub@chodorowicz.com>
2025-09-10 18:10:33 +0200

    Add field grouping by section

--- frontend/src/metabase/databases/components/DatabaseForm/DatabaseFormBodyDetails.tsx
+++ frontend/src/metabase/databases/components/DatabaseForm/DatabaseFormBodyDetails.tsx
@@ -14,19 +14,98 @@ interface DatabaseFormBodyDetailsProps {
   engine: Engine | undefined;
 }
 
+interface FieldMapper {
+  fieldRegexes: RegExp[];
+  className: string;
+  key: string;
+  skipEngine?: string[];
+  visibleIf?: Record<string, boolean>[];
+}
+
+const mappers: FieldMapper[] = [
+  {
+    fieldRegexes: [/^host$/, /^port$/],
+    // BigQuery Cloud SDK and Snowflake have just host field
+    skipEngine: ["bigquery-cloud-sdk", "snowflake"],
+    className: S.SubGroup,
+    key: "host-and-port",
+  },
+  {
+    // SSL and SSH-tunnel toggles that expand related fields
+    fieldRegexes: [/^ssl$/, /^tunnel-enabled$/],
+    // related fields to SSL and Tunnel
+    visibleIf: [
+      {
+        ssl: true,
+      },
+      {
+        "tunnel-enabled": true,
+      },
+    ],
+    className: S.FormField,
+    key: "ssl-and-tunnel",
+  },
+  {
+    fieldRegexes: [/^auth-enabled$/],
+    visibleIf: [
+      {
+        "auth-enabled": true,
+      },
+    ],
+    className: S.FormField,
+    key: "auth-and-ssl",
+  },
+  {
+    fieldRegexes: [/^kerberos$/],
+    className: S.FormField,
+    key: "kerberos",
+    visibleIf: [
+      {
+        kerberos: true,
+      },
+    ],
+  },
+  {
+    fieldRegexes: [/^let-user-control-scheduling$/],
+    visibleIf: [
+      {
+        "let-user-control-scheduling": true,
+      },
+    ],
+    className: S.FormField,
+    key: "let-user-control-scheduling",
+  },
+];
+
 export function DatabaseFormBodyDetails({
   fields,
   autofocusFieldName,
   engineKey,
   engine,
 }: DatabaseFormBodyDetailsProps) {
-  const groupedFields = groupFields({
+  const fieldMappers: FieldMapper[] = [
+    ...mappers,
+    ...fields
+      .filter((field) => field?.["visible-if"]?.["advanced-options"])
+      .map((field) => ({
+        fieldRegexes: [new RegExp(`^${field.name}$`)],
+        className: S.FormField,
+        key: field.name,
+      })),
+  ];
+
+  const mappedFields = fieldMappers.reduce<Array<EngineField | GroupField>>(
+    (acc, mapper) => {
+      if (engineKey && mapper?.skipEngine?.includes(engineKey)) {
+        return acc;
+      }
+
+      return groupFields(acc, mapper);
+    },
     fields,
-    fieldNames: ["host", "port"],
-    className: S.SubGroup,
-    key: "host-and-port",
-  });
-  const formFields = groupedFields.map((field) => {
+  );
+
+  const formFields = mappedFields.map((field) => {
     const isGroup = field instanceof GroupField;
     const formRow = (isGroup ? field.fields : [field]).map((f) => (
       <DatabaseDetailField
@@ -62,34 +141,59 @@ class GroupField {
   ) {}
 }
 
-function groupFields({
-  fields,
-  fieldNames,
-  className,
-  key,
-}: {
-  fields: EngineField[];
-  fieldNames: string[];
-  className: string;
-  key: string;
-}): Array<EngineField | GroupField> {
+function groupFields(
+  fields: Array<EngineField | GroupField>,
+  {
+    fieldRegexes,
+    visibleIf,
+    className,
+    key,
+  }: {
+    fieldRegexes: Array<RegExp>;
+    visibleIf?: Record<string, boolean>[];
+    className: string;
+    key: string;
+  },
+): Array<EngineField | GroupField> {
   if (fields.length === 0) {
     return fields;
   }
 
   // Indexes of the fields to combine into one group field
-  const indexes = fieldNames
-    .map((name) => fields.findIndex((field) => field.name === name))
+  const indexes = fields
+    .filter((field) => {
+      if (field instanceof GroupField) {
+        return false;
+      }
+
+      let result = false;
+
+      if (fieldRegexes) {
+        result = result || fieldRegexes.some((regex) => regex.test(field.name));
+      }
+
+      if (visibleIf) {
+        const res = visibleIf.some((rule) =>
+          Object.entries(rule).every(
+            ([key, value]) => field?.["visible-if"]?.[key] === value,
+          ),
+        );
+        result = result || res;
+      }
+
+      return result;
+    })
+    .map((field) => (field ? fields.indexOf(field) : -1))
     .filter((index) => index !== -1);
 
-  // If we haven't found all the fields, return the original fields
-  if (indexes.length !== fieldNames.length) {
+  // // If we haven't found all the fields, return the original fields
+  if (indexes.length === 0) {
     return fields;
   }
 
   // Combine the fields into a group field
   const combinedField = new GroupField(
-    indexes.map((index) => fields[index]),
+    indexes.map((index) => fields[index] as EngineField),
     className,
     key,
   );
