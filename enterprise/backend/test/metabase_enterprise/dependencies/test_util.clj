d6629d056bb50fe4722194a68ada1a75ec43c496
William Zimrin <william@metabase.com>
2025-09-10 10:23:51 -0500

    test data and tests for basic native deps calculation

---
+++ enterprise/backend/test/metabase_enterprise/dependencies/test_util.clj
@@ -0,0 +1,95 @@
+(ns metabase-enterprise.dependencies.test-util
+  (:require
+   [medley.core :as m]
+   [metabase.lib.convert :as lib.convert]
+   [metabase.lib.core :as lib]
+   [metabase.lib.metadata :as lib.metadata]
+   [metabase.lib.test-metadata :as meta]
+   [metabase.lib.test-util.metadata-providers.mock :as providers.mock]))
+
+(defn mock-card [metadata-provider {:keys [id query details]}]
+  (merge {:lib/type        :metadata/card
+          :id              id
+          :database-id     (:id (lib.metadata/database metadata-provider))
+          :dataset-query   (if (string? query)
+                             {:database (:id (lib.metadata/database metadata-provider))
+                              :type :native
+                              :native {:query query
+                                       :template-tags (lib/extract-template-tags metadata-provider query)}}
+                             (lib.convert/->legacy-MBQL query))
+          :name            (str (gensym))
+          :result-metadata (when (not (string? query))
+                             (->> (lib/returned-columns query)
+                                  (sort-by :id)))}
+         details))
+
+(defn mock-snippet [{:keys [id name content template-tags]}]
+  {:description   nil,
+   :archived      false,
+   :collection-id nil,
+   :id            id
+   :name          name
+   :content       content
+   :template-tags template-tags
+   :lib/type      :metadata/native-query-snippet})
+
+(defn mock-metadata-provider [{:keys [cards snippets]}]
+  (lib/composed-metadata-provider
+   meta/metadata-provider
+   (providers.mock/mock-metadata-provider
+    {:cards cards
+     :native-query-snippets snippets})))
+
+(defn default-metadata-provider []
+  (let [snippets (map mock-snippet
+                      [{:id 1
+                        :name "inner"
+                        :content "{{#card-ref}}"
+                        :template-tags {"#card-ref" {:card-id 1
+                                                     :type :card
+                                                     :name "#card-ref"
+                                                     :display-name "#Card Ref"
+                                                     :id (str (random-uuid))}}}
+                       {:id 2
+                        :name "outer"
+                        :content "{{snippet: inner}}"
+                        :template-tags {"snippet: inner" {:snippet-id 1
+                                                          :snippet-name "inner"
+                                                          :type :snippet
+                                                          :name "snippet: inner"
+                                                          :display-name "Snippet: Inner"
+                                                          :id (str (random-uuid))}}}])
+        initial-mock-mp (mock-metadata-provider {:snippets snippets})
+        initial-card-a (mock-card initial-mock-mp
+                                  {:id 1
+                                   :query (lib/query initial-mock-mp (meta/table-metadata :products))})
+        initial-card-b (mock-card initial-mock-mp
+                                  {:id 2
+                                   :query (lib/query initial-mock-mp (meta/table-metadata :orders))})]
+    (mock-metadata-provider
+     {:cards (into [initial-card-a
+                    initial-card-b]
+                   (map #(mock-card initial-mock-mp %))
+                   [{:id 3
+                     :query (lib/query initial-mock-mp initial-card-a)}
+                    {:id 4
+                     :query "select * from products"}
+                    {:id 5
+                     :query "select * from {{#1}}"}
+                    {:id 6
+                     :query "select * from {{snippet: inner}}"}
+                    {:id 7
+                     :query "select * from {{snippet: outer}}"}
+                    {:id 8
+                     :query (as-> (lib/query initial-mock-mp initial-card-b) q
+                              (lib/join q (-> (lib/join-clause initial-card-a)
+                                              (lib/with-join-conditions
+                                               [(lib/=
+                                                 (m/find-first #(= (:name %) "PRODUCT_ID")
+                                                               (lib/join-condition-lhs-columns
+                                                                q initial-card-b nil nil))
+                                                 (m/find-first #(= (:name %) "ID")
+                                                               (lib/join-condition-lhs-columns
+                                                                q initial-card-a nil nil)))])
+                                              (lib/with-join-fields :all))))}])
+      :snippets snippets})))
