35b5a65bdf4cd5f8837a985315b2d9f8b13318e3
Phoomparin Mano <phoomparin@gmail.com>
2025-09-12 00:59:18 +0700

    remove card icon from new upsell page

--- frontend/src/metabase/admin/settings/components/EmbeddingSettings/InteractiveEmbeddingUpsellPage/InteractiveEmbeddingUpsellPage.tsx
+++ frontend/src/metabase/admin/settings/components/EmbeddingSettings/InteractiveEmbeddingUpsellPage/InteractiveEmbeddingUpsellPage.tsx
@@ -1,16 +1,13 @@
 import { jt, t } from "ttag";
 
+import { SettingsSection } from "metabase/admin/components/SettingsSection";
 import { UpsellEmbeddingButton } from "metabase/admin/upsells/UpsellEmbeddingButton";
 import { UpsellGem } from "metabase/admin/upsells/components";
 import ExternalLink from "metabase/common/components/ExternalLink";
 import { getPlan } from "metabase/common/utils/plan";
 import { useSelector } from "metabase/lib/redux";
 import { getDocsUrl, getSetting } from "metabase/selectors/settings";
-import { Box, Group, Icon, Text } from "metabase/ui";
-
-import { EmbeddingOption } from "../EmbeddingOption/EmbeddingOption";
-
-import { InteractiveEmbeddingIcon } from "./InteractiveEmbeddingIcon";
+import { Box, Flex, Group, Icon, Text, Title } from "metabase/ui";
 
 export const InteractiveEmbeddingUpsellPage = () => {
   const plan = useSelector((state) =>
@@ -25,46 +22,53 @@ export const InteractiveEmbeddingUpsellPage = () => {
   );
 
   return (
-    <EmbeddingOption
-      icon={<InteractiveEmbeddingIcon />}
-      title={
-        <Group gap="sm">
-          <UpsellGem />
-          {t`Interactive embedding`}
-        </Group>
-      }
-      description={jt`Use interactive embedding when you want to ${(
-        <ExternalLink
-          href={`https://www.metabase.com/blog/why-full-app-embedding?utm_source=${plan}&utm_media=embed-settings`}
-          key="why-full-app-embedding"
-        >
-          {t`offer multi-tenant, self-service analytics`}
-        </ExternalLink>
-      )} and people want to create their own questions, dashboards, models, and more, all in their own data sandbox.`}
-    >
-      <Text
-        fw="bold"
-        c="brand"
-        component="a"
-        pos="relative"
-        href={quickStartUrl}
-        target="_blank"
-      >
-        {t`Check out our Quickstart`}{" "}
-        <Box ml="sm" top="2.5px" pos="absolute" component="span">
-          <Icon name="share" aria-hidden />
-        </Box>
-      </Text>
+    <SettingsSection>
+      <article>
+        <Flex gap="md" mb="sm" direction="row" align="center">
+          <Group gap="sm">
+            <UpsellGem />
 
-      <Group justify="space-between" align="center" w="100%">
-        <UpsellEmbeddingButton
-          url="https://www.metabase.com/product/embedded-analytics"
-          campaign="embedding-interactive"
-          location="embedding-page"
-          size="large"
-        />
-      </Group>
-    </EmbeddingOption>
+            <Title order={3}>{t`Interactive embedding`}</Title>
+          </Group>
+        </Flex>
+
+        <Text lh="1.25rem" mb="lg">
+          {jt`Use interactive embedding when you want to ${(
+            <ExternalLink
+              href={`https://www.metabase.com/blog/why-full-app-embedding?utm_source=${plan}&utm_media=embed-settings`}
+              key="why-full-app-embedding"
+            >
+              {t`offer multi-tenant, self-service analytics`}
+            </ExternalLink>
+          )} and people want to create their own questions, dashboards, models, and more, all in their own data sandbox.`}
+        </Text>
+
+        <Flex gap="lg" direction="column" align="flex-start">
+          <Text
+            fw="bold"
+            c="brand"
+            component="a"
+            pos="relative"
+            href={quickStartUrl}
+            target="_blank"
+          >
+            {t`Check out our Quickstart`}{" "}
+            <Box ml="sm" top="2.5px" pos="absolute" component="span">
+              <Icon name="share" aria-hidden />
+            </Box>
+          </Text>
+
+          <Group justify="space-between" align="center" w="100%">
+            <UpsellEmbeddingButton
+              url="https://www.metabase.com/product/embedded-analytics"
+              campaign="embedding-interactive"
+              location="embedding-page"
+              size="large"
+            />
+          </Group>
+        </Flex>
+      </article>
+    </SettingsSection>
   );
 };
 
