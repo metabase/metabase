283fbca9fcfec2a83656bc070d9715ee5cb0cec0
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-09-12 13:20:10 -0400

    Fix kondo

--- src/metabase/query_processor/util.clj
+++ src/metabase/query_processor/util.clj
@@ -181,7 +181,7 @@
   {:deprecated "0.57.0"}
   [field-ref cols]
   #_{:clj-kondo/ignore [:deprecated-var]}
-  (let [[_ttype id-or-name options :as field-ref] (field-normalizer field-ref)]
+  (let [[_ttype id-or-name _options :as field-ref] (field-normalizer field-ref)]
     (or
      ;; try match field_ref first
      (first (filter (fn [field-info]
