17d3c11d7f65b77c14f53e89042e4b3d1c95b4c0
Ngoc Khuat <qn.khuat@gmail.com>
2025-09-12 17:02:17 +0700

    mysql and postgres for now

--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -1289,8 +1289,6 @@
   dispatch-on-initialized-driver
   :hierarchy #'hierarchy)
 
-(declare rename-tables!*)
-
 (defmulti rename-tables!*
   "Driver-specific implementation of table renaming. Takes a pre-sorted map of {from-table to-table}.
    The input map has already been topologically sorted by the public rename-tables! function.
