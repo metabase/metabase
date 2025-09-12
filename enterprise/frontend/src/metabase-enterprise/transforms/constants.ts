cc0bd06ab148364821a8c131c29cecadf6cab782
Romeo Van Snick <romeo@romeovansnick.be>
2025-09-12 16:15:16 +1000

    Fix name for common lib

--- enterprise/frontend/src/metabase-enterprise/transforms/constants.ts
+++ enterprise/frontend/src/metabase-enterprise/transforms/constants.ts
@@ -7,4 +7,4 @@ export const POLLING_INTERVAL = isCypressActive ? 100 : 3000;
 export const FILTER_WIDGET_MIN_WIDTH = 300;
 export const FILTER_WIDGET_MAX_HEIGHT = 400;
 
-export const SHARED_LIB_IMPORT_NAME = "lib";
+export const SHARED_LIB_IMPORT_NAME = "common";
