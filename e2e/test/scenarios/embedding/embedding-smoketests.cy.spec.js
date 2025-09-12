737540c420bd3ecb40e8ab7b1f999b2a2917e012
Phoomparin Mano <phoomparin@gmail.com>
2025-09-12 18:54:48 +0700

    extract oss admin settings tests

--- e2e/test/scenarios/embedding/embedding-smoketests.cy.spec.js
+++ e2e/test/scenarios/embedding/embedding-smoketests.cy.spec.js
@@ -294,55 +294,6 @@ describe("scenarios > embedding > smoke tests", { tags: "@OSS" }, () => {
       });
     });
   });
-
-  it("should navigate to Modular Embedding when clicking Embedding in admin and show sidebar links", () => {
-    cy.log("Navigate to Embedding admin section");
-    cy.visit("/admin/embedding");
-
-    cy.log("Check that we're on the modular embedding page");
-    cy.url().should("include", "/admin/embedding/modular");
-    cy.get("main").findByText("Modular embedding").should("be.visible");
-
-    cy.log("Verify sidebar contains static smbedding link");
-    cy.findByTestId("admin-layout-sidebar")
-      .findByRole("link", { name: /Static/ })
-      .should("have.attr", "href", "/admin/embedding/static");
-
-    cy.log("Verify sidebar contains interactive embedding");
-    cy.findByTestId("admin-layout-sidebar")
-      .findByRole("link", { name: /Interactive/ })
-      .should("have.attr", "href", "/admin/embedding/interactive");
-
-    cy.log("Verify sidebar does not contain setup guide");
-    cy.findByTestId("admin-layout-sidebar")
-      .findByRole("link", { name: /Setup guide/ })
-      .should("not.exist");
-
-    cy.log("Verify 2 upsell icons are present in sidebar");
-    cy.findByTestId("admin-layout-sidebar")
-      .icon("gem")
-      .should("have.length", 2);
-  });
-
-  it("should show interactive embedding upsell", () => {
-    cy.visit("/admin/embedding/interactive");
-
-    mainPage().within(() => {
-      cy.findByRole("heading", { name: "Interactive embedding" }).should(
-        "be.visible",
-      );
-
-      cy.log("upsell gem icon should be visible");
-      cy.findByLabelText("gem icon").should("be.visible");
-
-      cy.findByRole("link", { name: /Check out our Quickstart/i })
-        .should("be.visible")
-        .and("have.attr", "href")
-        .and("contain", "interactive-embedding-quick-start-guide");
-
-      cy.findByRole("link", { name: "Try for free" }).should("be.visible");
-    });
-  });
 });
 
 function resetEmbedding() {
