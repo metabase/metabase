926e18de2ec2bf34b3287b51cdabcbde96a40c0e
Konstantin Darutkin <spalt08@mail.ru>
2025-09-12 14:38:32 +0300

    feat: add glossary UI

---
+++ frontend/src/metabase/reference/glossary/GlossarEditDefinitionModal.tsx
@@ -0,0 +1,84 @@
+import { type FormEvent, useCallback, useEffect, useState } from "react";
+import { t } from "ttag";
+
+import { Button, Group, Modal, Stack, TextInput, Textarea } from "metabase/ui";
+
+type GlossaryEditDefinitionModalProps = {
+  id?: string;
+  term?: string;
+  definition?: string;
+  opened: boolean;
+  onClose: () => void;
+  onSubmit: (id: string, term: string, definition: string) => void;
+  onDelete: (term: string) => void;
+};
+
+export function GlossaryEditDefinitionModal({
+  id,
+  term: initialTerm,
+  definition: initialDefinition,
+  opened,
+  onClose,
+  onSubmit,
+  onDelete,
+}: GlossaryEditDefinitionModalProps) {
+  const [term, setTerm] = useState("");
+  const [definition, setDefinition] = useState("");
+
+  useEffect(() => {
+    setTerm(initialTerm ?? "");
+    setDefinition(initialDefinition ?? "");
+  }, [initialTerm, initialDefinition]);
+
+  const handleSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      if (id) {
+        onSubmit(id, term, definition);
+        onClose();
+      }
+    },
+    [id, term, definition, onSubmit, onClose],
+  );
+
+  const handleDelete = useCallback(() => {
+    if (id) {
+      onDelete(id);
+      onClose();
+    }
+  }, [id, onDelete, onClose]);
+
+  return (
+    <Modal title={t`New definition`} onClose={onClose} opened={opened}>
+      <form onSubmit={handleSubmit}>
+        <Stack gap="xl" mt="md">
+          <TextInput
+            label={t`Term`}
+            placeholder={t`Data science`}
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <Textarea
+            label={t`Definition`}
+            placeholder={t`Data science is multifaceted and can be described as a science, a research paradigm, a research method, a discipline, a workflow, and a profession.`}
+            value={definition}
+            onChange={(e) => setDefinition(e.target.value)}
+            autosize
+            minRows={3}
+            maxRows={6}
+          />
+          <Group justify="flex-end">
+            <Button
+              type="button"
+              variant="filled"
+              color="danger"
+              onClick={handleDelete}
+            >{t`Delete`}</Button>
+            <Button type="submit" variant="filled">{t`Save`}</Button>
+          </Group>
+        </Stack>
+      </form>
+    </Modal>
+  );
+}
---
+++ frontend/src/metabase/reference/glossary/Glossary.tsx
@@ -0,0 +1,130 @@
+import { useDisclosure } from "@mantine/hooks";
+import cx from "classnames";
+import { useState } from "react";
+import { t } from "ttag";
+
+import {
+  ColumnHeader,
+  ItemCell,
+  TBody,
+  Table,
+  TableColumn,
+} from "metabase/common/components/ItemsTable/BaseItemsTable.styled";
+import CS from "metabase/css/core/index.css";
+import { Box, Button, Center, Group, Icon, Stack, Text } from "metabase/ui";
+
+import { GlossaryEditDefinitionModal } from "./GlossarEditDefinitionModal";
+import { GlossaryNewDefinitionModal } from "./GlossaryNewDefinitionModal";
+import {
+  type GlossaryDefinition,
+  useMockedGlossary,
+} from "./use-mocked-glossary";
+
+export function Glossary() {
+  const [newDefinitionOpened, newDefinitionHandler] = useDisclosure();
+  const [editingDefinition, setEditingDefinition] =
+    useState<GlossaryDefinition | null>(null);
+
+  const { mockedGlossary, addDefinition, updateDefinition, deleteDefinition } =
+    useMockedGlossary();
+
+  return (
+    <div>
+      <Group
+        align="center"
+        wrap="nowrap"
+        px="2.625rem"
+        h="3rem"
+        mt="2rem"
+        mb="0.5rem"
+        justify="space-between"
+      >
+        <Text fw="bold" fz="1.5rem">{t`Glossary`}</Text>
+
+        <Button
+          variant="default"
+          size="sm"
+          leftSection={<Icon name="add" />}
+          onClick={newDefinitionHandler.open}
+        >{t`New definition`}</Button>
+      </Group>
+      <div className={cx(CS.wrapper, CS.wrapperTrim)}>
+        <Stack mb="lg" gap="xs" align="flex-start">
+          <Text c="text-secondary">{t`Paragraph describing the motivation for this glossary and its purpose.`}</Text>
+        </Stack>
+        <Table>
+          <colgroup>
+            <TableColumn width="22.5%" />
+            <TableColumn width="70%" />
+            <TableColumn width="7.5%" />
+          </colgroup>
+          <thead>
+            <tr>
+              <ColumnHeader>{t`Term`}</ColumnHeader>
+              <ColumnHeader>{t`Definition`}</ColumnHeader>
+              <ColumnHeader />
+            </tr>
+          </thead>
+          <TBody>
+            {mockedGlossary.length === 0 && <EmptyGlossary />}
+            {mockedGlossary.map((term, index) => (
+              <tr key={index}>
+                <Box component="td" valign="top">
+                  <Text fw="bold">{term.term}</Text>
+                </Box>
+                <Box component="td" valign="top" pr="0">
+                  <Text lh="1.2">{term.definition}</Text>
+                </Box>
+                <Box component="td" valign="top" px="0" align="center">
+                  <Icon
+                    name="pencil"
+                    c="text-light"
+                    cursor="pointer"
+                    onClick={() =>
+                      setEditingDefinition({
+                        id: term.id,
+                        term: term.term,
+                        definition: term.definition,
+                      })
+                    }
+                  />
+                </Box>
+              </tr>
+            ))}
+          </TBody>
+        </Table>
+      </div>
+
+      <GlossaryNewDefinitionModal
+        opened={newDefinitionOpened}
+        onClose={newDefinitionHandler.close}
+        onSubmit={addDefinition}
+      />
+
+      <GlossaryEditDefinitionModal
+        id={editingDefinition?.id}
+        term={editingDefinition?.term}
+        definition={editingDefinition?.definition}
+        opened={editingDefinition !== null}
+        onClose={() => setEditingDefinition(null)}
+        onSubmit={updateDefinition}
+        onDelete={deleteDefinition}
+      />
+    </div>
+  );
+}
+
+function EmptyGlossary() {
+  return (
+    <tr>
+      <ItemCell colSpan={3}>
+        <Center p="sm">
+          <Text
+            c="text-secondary
+          "
+          >{t`No terms found.`}</Text>
+        </Center>
+      </ItemCell>
+    </tr>
+  );
+}
