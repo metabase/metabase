0440a4987e767d007821cedb3d88521ca96398bd
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-09-16 20:51:52 +0200

    Fine-tune some metabot events

--- enterprise/frontend/src/metabase-enterprise/metabot/components/MetabotChat/MetabotChat.tsx
+++ enterprise/frontend/src/metabase-enterprise/metabot/components/MetabotChat/MetabotChat.tsx
@@ -20,7 +20,6 @@ import {
 import { useGetSuggestedMetabotPromptsQuery } from "metabase-enterprise/api";
 import { MetabotResetLongChatButton } from "metabase-enterprise/metabot/components/MetabotChat/MetabotResetLongChatButton";
 
-import { trackMetabotRequestSent } from "../../analytics";
 import { useMetabotAgent, useMetabotChatHandlers } from "../../hooks";
 
 import Styles from "./MetabotChat.module.css";
@@ -203,7 +202,6 @@ export const MetabotChat = () => {
                   // prevent event from inserting new line + interacting with other content
                   e.preventDefault();
                   e.stopPropagation();
-                  trackMetabotRequestSent();
                   handleSubmitInput(metabot.prompt);
                 }
               }}
