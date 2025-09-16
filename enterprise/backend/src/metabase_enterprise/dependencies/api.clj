b6d0a85a418ca8f5e09a9256046c4ba8699e8286
Tamás Benkő <tamas@metabase.com>
2025-09-17 01:52:21 +0300

    Implement table to table mapping

--- enterprise/backend/src/metabase_enterprise/dependencies/api.clj
+++ enterprise/backend/src/metabase_enterprise/dependencies/api.clj
@@ -3,6 +3,7 @@
    [medley.core :as m]
    [metabase-enterprise.dependencies.core :as dependencies]
    [metabase.analyze.core :as analyze]
+   [metabase.api.common :as api]
    [metabase.api.macros :as api.macros]
    [metabase.api.routes.common :refer [+auth]]
    [metabase.api.util.handlers :as handlers]
@@ -13,6 +14,10 @@
    [metabase.lib.metadata :as lib.metadata]
    [metabase.lib.metadata.cached-provider :as lib.metadata.cached-provider]
    [metabase.lib.metadata.protocols :as lib.metadata.protocols]
+   [metabase.lib.options :as lib.options]
+   [metabase.lib.schema.id :as lib.schema.id]
+   [metabase.lib.util :as lib.util]
+   [metabase.lib.walk :as lib.walk]
    [metabase.models.interface :as mi]
    [metabase.native-query-snippets.core :as native-query-snippets]
    [metabase.queries.schema :as queries.schema]
@@ -99,7 +104,7 @@
 
 (defn- card-uses-snippet?
   [card {snippet-id :id snippet-name :name}]
-  (let [template-tags (-> card :dataset-query :native :template-tags vals)]
+  (let [template-tags (-> card :dataset_query :native :template-tags vals)]
     (some #(and (= (:type %) :snippet)
                 (or (= (:snippet-id %) snippet-id)
                     (= (:name %) snippet-name))) template-tags)))
@@ -117,7 +122,7 @@
   (let [card (lib.metadata/card mp card-id)]
     (cond-> card
       (card-uses-snippet? card snippet)
-      (assoc :result-metadata (calculate-native-result-metadata mp (:dataset-query card))))))
+      (assoc :result-metadata (calculate-native-result-metadata mp (:dataset_query card))))))
 
 (defn- broken-by-snippet
   [database-id snippet]
@@ -158,6 +163,101 @@
                        (map #(broken-by-snippet % snippet))
                        (apply merge-with merge))]
     (broken-cards-response breakages)))
+
+(defn- table-field-mapping
+  [from-fields to-fields]
+  (let [to-name->id (into {} (map (juxt :name :id)) to-fields)]
+    (for [{from-id :id, from-name :name} from-fields]
+      (if-let [to-id (to-name->id from-name)]
+        [from-id to-id]
+        (throw (ex-info (tru "Schemas do not match: Field ''{0}'' not found in destination table." from-name)
+                        {:status-code 400}))))))
+
+(defn- replace-table-ids-in-query
+  [query table-mapping]
+  (lib.walk/walk
+   query
+   (fn [_query _path-type _path stage-or-join]
+     (let [new-table-id (-> stage-or-join :source-table table-mapping)]
+       (cond-> stage-or-join
+         new-table-id (assoc :source-table new-table-id))))))
+
+(defn- replace-field-ids-in-query
+  [query field-mapping]
+  (lib.walk/walk-clauses
+   query
+   (fn [_query _path-type _path clause]
+     (when (lib.util/field-clause? clause)
+       (let [new-id  (-> clause (get 2) field-mapping)
+             clause' (cond-> clause
+                       new-id (assoc 2 new-id))]
+         (lib.options/update-options
+          clause'
+          (fn [opts]
+            (let [new-source-field-id (-> opts :source-field field-mapping)]
+              (cond-> opts
+                new-source-field-id (assoc :source-field new-source-field-id))))))))))
+
+(defn- update-table-and-field-refs [query table-mapping field-mapping]
+  (-> query
+      (replace-table-ids-in-query table-mapping)
+      (replace-field-ids-in-query field-mapping)))
+
+(defn- update-card-table-refs
+  [metadata-provider card table-mapping field-mapping]
+  (let [query (lib/query metadata-provider (:dataset_query card))
+        query' (update-table-and-field-refs query table-mapping field-mapping)]
+    (when (not= query' query)
+      ;; we are not ready to change the format of existing cards yet
+      #_{:clj-kondo/ignore [:discouraged-var]}
+      (t2/update! :model/Card (:id card) {:dataset_query (lib/->legacy-MBQL query')}))))
+
+(defn- update-transform-table-refs
+  [metadata-provider transform table-mapping field-mapping]
+  (let [source-query (lib/query metadata-provider (:source transform))
+        query'       (update-table-and-field-refs source-query table-mapping field-mapping)]
+    (when (not= query' source-query)
+      ;; other parts of the code are saving transforms in legacy format
+      #_{:clj-kondo/ignore [:discouraged-var]}
+      (t2/update! :model/Transform (:id transform) {:source (lib/->legacy-MBQL query')}))))
+
+(api.macros/defendpoint :post "/switch_tables"
+  "Replace references to a table and its fields to references to another table and the corresponding fields of that.
+  The replacement happens in cards and transforms."
+  [_route-params
+   _query-params
+   {:keys [table_mapping]} :- [:map [:table_mapping [:sequential [:tuple ::lib.schema.id/table ::lib.schema.id/table]]]]]
+  (api/check-superuser)
+  (if-let [table-id-map (not-empty (into {} (remove (fn [[from to]] (= from to))) table_mapping))]
+    (let [_ (when (not= (count table_mapping) (count table-id-map))
+              (api/throw-invalid-param-exception :table_mapping (tru "Duplicate ''from'' tables in table_mapping.")))
+          all-table-ids (not-empty (into #{} (mapcat identity) table-id-map))
+          _ (when (not= (t2/count :model/Table :id [:in all-table-ids])
+                        (count all-table-ids))
+              (api/throw-invalid-param-exception :table_mapping (tru "One or more table IDs do not exist.")))
+          [db-id & db-ids] (t2/select-fn-set :db_id
+                                             [:model/Table [[:distinct :db_id]]]
+                                             {:where [:in :id all-table-ids]
+                                              :limit 2})
+          _ (when db-ids
+              (api/throw-invalid-param-exception
+               :table_mapping (tru "table_mapping should map tables in the same database")))
+          table-id->fields (->> (t2/select [:model/Field :id :name :table_id] :table_id [:in all-table-ids])
+                                (group-by :table_id))
+          field-mapping (into {} (mapcat (fn [[from-id to-id]]
+                                           (table-field-mapping (table-id->fields from-id) (table-id->fields to-id))))
+                              table-id-map)
+          metadata-provider (lib-be.metadata.jvm/application-database-metadata-provider db-id)]
+      (run! #(update-card-table-refs metadata-provider % table-id-map field-mapping)
+            (t2/reducible-select :model/Card :database_id db-id))
+      (reduce (fn [_ transform]
+                (when (= (-> transform :source :database) db-id)
+                  (update-transform-table-refs metadata-provider transform table-id-map field-mapping)
+                  nil))
+              nil
+              (t2/reducible-select :model/Transform))
+      {:success true})
+    {:success true}))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/dependencies` routes."
