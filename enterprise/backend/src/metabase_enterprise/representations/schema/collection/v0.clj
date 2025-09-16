25c142a7aa0ea7d4a587582621d13d5779b54b1c
Timothy Dean <timothy@timothydean.dev>
2025-09-16 15:43:43 -0600

    v0 scaffolding collection.yml

---
+++ enterprise/backend/src/metabase_enterprise/representations/schema/collection/v0.clj
@@ -0,0 +1,42 @@
+(ns metabase-enterprise.representations.schema.collection.v0
+  "Schema for v0 of the human-writable collection representation format.
+   
+   Note: v0 is a theoretical work-in-progress and subject to change."
+  (:require
+   [metabase.lib.schema.common :as lib.schema.common]
+   [metabase.util.malli.registry :as mr]))
+
+;;; ------------------------------------ Schema Definitions ------------------------------------
+
+(mr/def ::type
+  [:enum {:decode/json keyword
+          :description "Entity type, must be 'collection' for this schema"}
+   :collection])
+
+(mr/def ::ref
+  [:and
+   {:description "Unique reference identifier for the collection, used for cross-references"}
+   ::lib.schema.common/non-blank-string
+   [:re #"^[a-z0-9][a-z0-9-_]*$"]])
+
+(mr/def ::name
+  [:and
+   {:description "Human-readable name for the collection"}
+   ::lib.schema.common/non-blank-string])
+
+(mr/def ::description
+  [:and
+   {:description "Optional documentation explaining the collection's purpose"}
+   :string])
+
+;;; ------------------------------------ Main Schema ------------------------------------
+
+(mr/def ::collection-v0
+  [:map
+   {:description "v0 schema for human-writable collection representation
+                  Collections organize cards, dashboards, and other resources.
+                  Every representations directory MUST have a collection.yml file."}
+   [:type ::type]
+   [:ref ::ref]
+   [:name ::name]
+   [:description {:optional true} ::description]])
