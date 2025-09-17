0d1b4e253a819a0f9918aad2e6a4922ac27c7c34
Cam Saul <1455846+camsaul@users.noreply.github.com>
2025-09-17 08:47:13 -0700

    cumulative-aggregations-middleware => Lib (#63592)
    * cumulative-aggregations-middleware => Lib
    
    * Ensure MBQL 5
    
    * Fix NOCOMMIT

--- src/metabase/query_processor/postprocess.clj
+++ src/metabase/query_processor/postprocess.clj
@@ -41,7 +41,7 @@
    [::mbql5  #'pivot-export/add-data-for-pivot-export]
    [::mbql5  #'large-int/convert-large-int-to-string]
    [::mbql5  #'viz-settings/update-viz-settings]
-   [::legacy #'qp.cumulative-aggregations/sum-cumulative-aggregation-columns]
+   [::mbql5  #'qp.cumulative-aggregations/sum-cumulative-aggregation-columns]
    [::mbql5  #'annotate/add-column-info]
    [::mbql5  #'fetch-source-query/add-dataset-info]])
 ;; ↑↑↑ POST-PROCESSING ↑↑↑ happens from BOTTOM TO TOP
