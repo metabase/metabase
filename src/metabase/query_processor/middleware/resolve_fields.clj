dec95aabe7334b3208eb23f77540c38bb8600e48
Cam Saul <1455846+camsaul@users.noreply.github.com>
2025-09-17 07:50:07 -0700

    resolve-joins middleware => Lib, remove add-source-metadata (#63608)

--- src/metabase/query_processor/middleware/resolve_fields.clj
+++ src/metabase/query_processor/middleware/resolve_fields.clj
@@ -1,14 +1,11 @@
 (ns metabase.query-processor.middleware.resolve-fields
   "Middleware that resolves the Fields referenced by a query."
   (:require
+   [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
-   [metabase.lib.schema :as lib.schema]
-   [metabase.lib.schema.id :as lib.schema.id]
-   [metabase.lib.util.match :as lib.util.match]
    [metabase.query-processor.error-type :as qp.error-type]
    [metabase.util :as u]
-   [metabase.util.i18n :refer [tru]]
-   [metabase.util.malli :as mu]))
+   [metabase.util.i18n :refer [tru]]))
 
 (defn- resolve-fields-with-ids!
   [metadata-providerable field-ids]
@@ -21,23 +18,12 @@
                                field-ids))]
     (recur metadata-providerable parent-ids)))
 
-(mu/defn- field-ids :- [:set ::lib.schema.id/field]
-  [query :- ::lib.schema/query]
-  (into #{}
-        (lib.util.match/match (:stages query)
-          [:field _opts (id :guard pos-int?)]
-          id
-
-          ;; stage metadata
-          {:lib/type :metadata/column, :id (id :guard pos-int?)}
-          id)))
-
 ;;; TODO (Cam 9/10/25) -- give this a more accurate name like `prefetch-all-fields` or
 ;;; `warm-metadata-provider-cache-with-all-fields` or something.
 (defn resolve-fields
   "Resolve all field referenced in the `query`, and store them in the Metadata Provider."
   [query]
-  (let [ids (field-ids query)]
+  (let [ids (lib/all-field-ids query)]
     (try
       (u/prog1 query
         (resolve-fields-with-ids! query ids))
