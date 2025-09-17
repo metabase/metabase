9a2250d2c17e7985c149a8eb6ef593e1c06b7ada
Oleksandr Solovyov <o@solovyov.net>
2025-09-17 15:51:03 +0300

    pr feedback

--- enterprise/backend/src/metabase_enterprise/comments/api.clj
+++ enterprise/backend/src/metabase_enterprise/comments/api.clj
@@ -10,6 +10,7 @@
    [metabase.events.core :as events]
    [metabase.request.core :as request]
    [metabase.users.api :as api.user]
+   [metabase.users.models.user :as user]
    [metabase.util :as u]
    [metabase.util.i18n :refer [deferred-tru]]
    [metabase.util.malli :as mu]
@@ -220,7 +221,8 @@
 (api.macros/defendpoint :get "/mentions"
   "Get a list of entities suitable for mentions. NOTE: only users for now."
   [_route-params _query-params]
-  (let [clauses (api.user/user-clauses nil nil nil nil)]
+  (let [clauses (user/filter-clauses nil nil nil nil {:limit  (request/limit)
+                                                      :offset (request/offset)})]
     ;; returns nothing while we're trying to figure out how do we deal with sandboxes and tenants etc
     ;; do not forget to uncomment tests (both api and e2e)
     {:data   (->> (t2/select [:model/User :id :first_name :last_name]
