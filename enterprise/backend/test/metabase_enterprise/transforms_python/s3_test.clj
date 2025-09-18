f03c2d1d6f62378494b667241c1fd7893971aea3
Chris Truter <crisptrutski@users.noreply.github.com>
2025-09-18 16:01:38 +0200

    S3 transport layer for python-runner (#63697)

---
+++ enterprise/backend/test/metabase_enterprise/transforms_python/s3_test.clj
@@ -0,0 +1,80 @@
+(ns metabase-enterprise.transforms-python.s3-test
+  "Tests for S3 operations in transforms-python module."
+  (:require
+   [clj-http.client :as http]
+   [clojure.string :as str]
+   [clojure.test :refer :all]
+   [metabase-enterprise.transforms-python.s3 :as s3]
+   [metabase-enterprise.transforms-python.settings :as transforms-python.settings]
+   [metabase.test :as mt])
+  (:import
+   (java.nio.file Files)
+   (java.nio.file.attribute FileAttribute)
+   (software.amazon.awssdk.services.s3 S3Client)
+   (software.amazon.awssdk.services.s3.model NoSuchKeyException)))
+
+(set! *warn-on-reflection* true)
+
+(comment
+  (remove-ns (ns-name *ns*)))
+
+(deftest s3-read-write-test
+  (testing "We can open an s3 connection, and read and write things"
+    (with-open [s3-client (s3/create-s3-client)]
+      (is (instance? S3Client s3-client))
+      (let [bucket (transforms-python.settings/python-storage-s-3-bucket)
+            key    (str "test-object-" (random-uuid) ".txt")
+            body   (str "Hello, S3! My secret is:" (random-uuid))]
+
+        (is (thrown? NoSuchKeyException (s3/read-to-string s3-client bucket key)))
+        (is (= :default (s3/read-to-string s3-client bucket key :default)))
+
+        (let [tmp-file (Files/createTempFile "s3-test" ".txt" (into-array FileAttribute []))]
+          (try
+            (spit (.toFile tmp-file) body)
+
+            (is (s3/upload-file s3-client bucket key (.toFile tmp-file)))
+
+            (is (= body (s3/read-to-string s3-client bucket key)))
+            (is (= body (s3/read-to-string s3-client bucket key :default)))
+
+            (s3/delete s3-client bucket key)
+
+            (is (thrown? NoSuchKeyException (s3/read-to-string s3-client bucket key)))
+            (is (= :default (s3/read-to-string s3-client bucket key :default)))
+
+            (finally
+              (Files/deleteIfExists tmp-file))))))))
+
+(deftest open-s3-shared-storage-test
+  (testing "open-s3-shared-storage! returns closeable derefable"
+    (mt/with-premium-features #{:transforms-python}
+      (let [table-name->id {"users" 1}
+            storage-ref    (s3/open-shared-storage! table-name->id)
+            {:keys [s3-client bucket-name objects]} @storage-ref]
+        (testing "can be dereferenced"
+          (doseq [[k {:keys [method path url]}] objects
+                  :let [url (str/replace url "localstack" "localhost")]]
+            (testing (format "%s (%s)" k method)
+              (is (= :not-created (s3/read-to-string s3-client bucket-name path :not-created)))
+              (let [content  (str (random-uuid))]
+                (case method
+                  ;; Files that we should write, and python-runner should fetch
+                  :get (let [tmp-file (Files/createTempFile "s3-test" ".txt" (into-array FileAttribute []))]
+                         (try
+                           (spit (.toFile tmp-file) content)
+                           (is (s3/upload-file s3-client bucket-name path (.toFile tmp-file)))
+                           (finally
+                             (Files/deleteIfExists tmp-file)))
+                         (is (= content (s3/read-to-string s3-client bucket-name path :not-created)))
+                         (is (= content (:body (http/get url)))))
+                  ;; Files that python-runner should post, and we should fetch
+                  :put (do
+                         (is (http/put url {:body content}))
+                         (is (= content (s3/read-to-string s3-client bucket-name path :not-created)))))))))
+
+        (testing "closing the ref deletes the files"
+          (.close storage-ref)
+          (doseq [[k {:keys [path]}] objects]
+            (testing (str k)
+              (is (= :not-created (s3/read-to-string s3-client bucket-name path :not-created))))))))))
