7bce3ff38a8a2af68f9afbdb304275584e545655
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2025-09-18 08:44:19 +0200

    Add analytics to Metabot events (#63415)
    * Add Metabot events
    
    * Fix tooltip label
    
    * Add new event for metabot chat controlled from the native editor

--- frontend/src/metabase-types/analytics/event.ts
+++ frontend/src/metabase-types/analytics/event.ts
@@ -384,6 +384,33 @@ export type TableEditingEvent =
   | TableEditButtonClickedEvent
   | TableEditingRecordModifiedEvent;
 
+export type MetabotChatOpenedEvent = ValidateEvent<{
+  event: "metabot_chat_opened";
+  triggered_from:
+    | "search"
+    | "command_palette"
+    | "keyboard_shortcut"
+    | "native_editor";
+}>;
+
+export type MetabotRequestSentEvent = ValidateEvent<{
+  event: "metabot_request_sent";
+}>;
+
+export type MetabotFixQueryClickedEvent = ValidateEvent<{
+  event: "metabot_fix_query_clicked";
+}>;
+
+export type MetabotExplainChartClickedEvent = ValidateEvent<{
+  event: "metabot_explain_chart_clicked";
+}>;
+
+export type MetabotEvent =
+  | MetabotChatOpenedEvent
+  | MetabotRequestSentEvent
+  | MetabotFixQueryClickedEvent
+  | MetabotExplainChartClickedEvent;
+
 export type SimpleEvent =
   | CustomSMTPSetupClickedEvent
   | CustomSMTPSetupSuccessEvent
@@ -429,4 +456,5 @@ export type SimpleEvent =
   | DocumentUpdatedEvent
   | DocumentPrintEvent
   | DatabaseHelpClickedEvent
-  | XRayEvent;
+  | XRayEvent
+  | MetabotEvent;
