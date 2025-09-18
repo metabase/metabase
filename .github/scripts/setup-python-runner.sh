4fea38de3508735f2cb41105c06b9a9258547611
Ngoc Khuat <qn.khuat@gmail.com>
2025-09-18 19:49:37 +0700

    fix drivers ci

--- .github/scripts/setup-python-runner.sh
+++ .github/scripts/setup-python-runner.sh
@@ -2,8 +2,9 @@
 set -e
 
 echo "Cloning python-runner repository..."
-git clone https://${METABASE_AUTOMATION_USER_TOKEN}@github.com/metabase/python-runner-container.git python-runner
+git clone --depth 1 https://${METABASE_AUTOMATION_USER_TOKEN}@github.com/metabase/python-runner-container.git python-runner
 
 echo "Starting python-runner..."
 cd python-runner
 make run
+cd ..
