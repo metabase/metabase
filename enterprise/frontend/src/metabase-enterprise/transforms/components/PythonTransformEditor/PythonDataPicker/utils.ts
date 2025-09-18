ee6fac18d936e8ab833ddbda48562b80551ac76b
Romeo Van Snick <romeo@romeovansnick.be>
2025-09-18 17:51:19 +1000

    Refactor PythonDataPicker
    # Conflicts:
    #	enterprise/frontend/src/metabase-enterprise/transforms/components/PythonTransformEditor/PythonDataPicker/PythonDataPicker.tsx

---
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/PythonTransformEditor/PythonDataPicker/utils.ts
@@ -0,0 +1,54 @@
+import { slugify as toSlug } from "metabase/lib/formatting";
+import type { PythonTransformTableAliases } from "metabase-types/api";
+
+import type { TableSelection } from "./types";
+
+export function getInitialTableSelections(
+  tables: PythonTransformTableAliases | undefined,
+) {
+  if (tables && Object.keys(tables).length > 0) {
+    return Object.entries(tables).map(([alias, tableId]) => ({
+      tableId,
+      alias,
+    }));
+  }
+  // Start with one empty row by default
+  return [
+    {
+      tableId: undefined,
+      alias: "",
+    },
+  ];
+}
+
+export function selectionsToTableAliases(
+  selections: TableSelection[],
+): PythonTransformTableAliases {
+  const tableAliases: PythonTransformTableAliases = {};
+
+  for (const selection of selections) {
+    const { alias, tableId } = selection;
+    if (tableId !== undefined && alias !== "") {
+      tableAliases[alias] = tableId;
+    }
+  }
+  return tableAliases;
+}
+
+export function slugify(name: string, usedNames: Set<string> = new Set()) {
+  if (name === "") {
+    return "";
+  }
+
+  const plain = toSlug(name).replace(/\./, "_");
+  if (!usedNames.has(plain)) {
+    return plain;
+  }
+  for (let index = 1; index <= 100; index++) {
+    const alias = `${plain}_${index}`;
+    if (!usedNames.has(alias)) {
+      return alias;
+    }
+  }
+  throw new Error("Could not find a unique alias");
+}
