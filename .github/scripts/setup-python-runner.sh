1585d8cf00def3e10b9326cb351a57c72a28e8ad
Ngoc Khuat <qn.khuat@gmail.com>
2025-09-18 16:59:58 +0700

    setup ci properly (#63693)

---
+++ .github/scripts/setup-python-runner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Cloning python-runner repository..."
+git clone --depth 1 https://${METABASE_AUTOMATION_USER_TOKEN}@github.com/metabase/python-runner-container.git python-runner
+
+echo "Starting python-runner..."
+cd python-runner
+make run
+cd ..
