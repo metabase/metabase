fa388420fad14cc15d0c239dfc8653cc1e994f15
Nicola Mometto <nicola.mometto@metabase.com>
2025-09-18 18:34:40 +0100

    fix: missing feature

--- enterprise/backend/test/metabase_enterprise/transforms_python/python_runner_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms_python/python_runner_test.clj
@@ -376,7 +376,7 @@
     (mt/with-temp-scheduler!
       (task/init! ::transforms.schedule/RunTransform)
       (mt/test-drivers #{:postgres}
-        (mt/with-premium-features #{:transforms-python}
+        (mt/with-premium-features #{:transforms-python :transforms}
           (mt/dataset transforms-dataset/transforms-test
             (transforms.tu/with-transform-cleanup! [target {:type   "table"
                                                             :schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))
