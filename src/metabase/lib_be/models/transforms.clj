ab9ba5d48f1c966c686d654daa9f9952bb01dcf2
Cam Saul <github@camsaul.com>
2025-09-19 16:36:59 -0700

    More amazing lib improvements

---
+++ src/metabase/lib_be/models/transforms.clj
@@ -0,0 +1,19 @@
+(ns metabase.lib-be.models.transforms
+  (:require
+   [metabase.lib-be.metadata.jvm :as lib.metadata.jvm]
+   [metabase.lib.core :as lib]
+   [metabase.models.interface :as mi]
+   [metabase.util.malli :as mu]))
+
+(mu/defn normalize-query
+  "Normalize an MBQL `query` to MBQL 5 and attach a metadata provider."
+  [query :- [:maybe :map]]
+  (cond
+    (empty? query)        {}
+    (:lib/metadata query) query
+    :else                 (lib/query lib.metadata.jvm/application-database-metadata-provider query)))
+
+(def transform-query
+  "Toucan 2 transform spec for Card `dataset_query` and other columns that store MBQL."
+  {:in  (comp mi/json-in lib/prepare-for-serialization normalize-query)
+   :out (comp (mi/catch-normalization-exceptions normalize-query) mi/json-out-without-keywordization)})
