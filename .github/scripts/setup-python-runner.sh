13b011b48b993658be345656f1c454e3db4df247
Dan Stone <wotbrew@gmail.com>
2025-09-19 14:59:41 +0100

    Add python_runner.clj (#63747)
    * Add python runner
    
    * Kondo
    
    * Traceback fix
    
    * Remove dead code
    
    * start the runner by default
    
    * Cancellation not used yet
    
    * Remove unused keys from fields-metadata
    
    * cherry pick driver fixes
    
    * start for postgres and mysql as well
    
    * clickhouse fixes
    
    * relax for snowflake
    
    * typo
    
    * exclude transform-python-test from appdb
    
    * exclude transforms-python-test as well
    
    * try exclude-tag here too
    
    ---------
    
    Co-authored-by: Ngoc Khuat <qn.khuat@gmail.com>
    Co-authored-by: Nicola Mometto <nicola.mometto@metabase.com>

--- .github/scripts/setup-python-runner.sh
+++ .github/scripts/setup-python-runner.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -e
 
+# Parse command line arguments
+SKIP_START=false
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --skip-start)
+      SKIP_START=true
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      echo "Usage: $0 [--skip-start]"
+      exit 1
+      ;;
+  esac
+done
+
 echo "Cloning python-runner repository..."
 git clone --depth 1 https://${METABASE_AUTOMATION_USER_TOKEN}@github.com/metabase/python-runner-container.git python-runner
 
