c1a7ecaddf0e49841e3faf7baaae6140417cd2d8
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-09-22 14:47:38 -0400

    E2E tests

--- e2e/test/scenarios/dependencies/dependencies.cy.spec.ts
+++ e2e/test/scenarios/dependencies/dependencies.cy.spec.ts
@@ -19,9 +19,8 @@ H.describeWithSnowplowEE("documents", () => {
 
   describe("questions", () => {
     it("should not show a confirmation if there are no breaking changes when updating a question", () => {
-      createQuestionContent().then(({ baseCard }) => {
-        H.visitQuestion(baseCard.id);
-      });
+      createQuestionContent();
+      H.visitQuestion("@questionId");
       H.openNotebook();
       H.getNotebookStep("expression").findByText("Expr").click();
       H.CustomExpressionEditor.clear().type("2 + 2");
@@ -32,10 +31,8 @@ H.describeWithSnowplowEE("documents", () => {
     });
 
     it("should be able to confirm or cancel breaking changes to a question", () => {
-      createQuestionContent().then(({ baseCard }) => {
-        H.visitQuestion(baseCard.id);
-      });
-
+      createQuestionContent();
+      H.visitQuestion("@questionId");
       H.openNotebook();
       H.getNotebookStep("expression").findByText("Expr").icon("close").click();
 
@@ -56,66 +53,120 @@ H.describeWithSnowplowEE("documents", () => {
     });
 
     it("should be able to navigate to affected questions or their collection", () => {
-      createQuestionContent().then(({ baseCard }) => {
-        cy.log("check that we can navigate to a broken question");
-        H.visitQuestion(baseCard.id);
-        H.openNotebook();
-        H.getNotebookStep("expression")
-          .findByText("Expr")
-          .icon("close")
-          .click();
-        H.queryBuilderHeader().button("Save").click();
-        H.modal().button("Save").click();
-        H.modal().findByText("Question with fields").click();
-        confirmDiscardChanges();
-        H.queryBuilderHeader()
-          .findByDisplayValue("Question with fields")
-          .should("be.visible");
+      createQuestionContent();
 
-        cy.log("check that we can navigate to the collection of that question");
-        cy.go("back");
-        H.getNotebookStep("expression")
-          .findByText("Expr")
-          .icon("close")
-          .click();
-        H.queryBuilderHeader().button("Save").click();
-        H.modal().button("Save").click();
-        H.modal().within(() => {
-          cy.findByText("Question with fields").should("be.visible");
-          cy.findByText("Our analytics").click();
-        });
-        confirmDiscardChanges();
-        H.collectionTable().should("be.visible");
+      cy.log("check that we can navigate to a broken question");
+      H.visitQuestion("@questionId");
+      H.openNotebook();
+      H.getNotebookStep("expression").findByText("Expr").icon("close").click();
+      H.queryBuilderHeader().button("Save").click();
+      H.modal().button("Save").click();
+      H.modal().findByText("Question with fields").click();
+      confirmDiscardChanges();
+      H.queryBuilderHeader()
+        .findByDisplayValue("Question with fields")
+        .should("be.visible");
+
+      cy.log("check that we can navigate to the collection of that question");
+      cy.go("back");
+      H.getNotebookStep("expression").findByText("Expr").icon("close").click();
+      H.queryBuilderHeader().button("Save").click();
+      H.modal().button("Save").click();
+      H.modal().within(() => {
+        cy.findByText("Question with fields").should("be.visible");
+        cy.findByText("Our analytics").click();
       });
+      confirmDiscardChanges();
+      H.collectionTable().should("be.visible");
+    });
+  });
+
+  describe("models", () => {
+    it("should not show a confirmation if there are no breaking changes when updating a model", () => {
+      createModelContent();
+      cy.get<number>("@modelId").then(H.visitModel);
+      H.openQuestionActions("Edit query definition");
+      H.NativeEditor.clear().type("SELECT ID, CATEGORY FROM PRODUCTS");
+      H.runNativeQuery();
+      H.datasetEditBar().button("Save changes").click();
+      cy.wait("@updateCard");
     });
   });
 });
 
 function createQuestionContent() {
-  return H.createQuestion({
-    name: "Base question",
-    query: {
-      "source-table": PRODUCTS_ID,
-      expressions: {
-        Expr: ["+", 1, 1],
+  H.createQuestion(
+    {
+      name: "Base question",
+      query: {
+        "source-table": PRODUCTS_ID,
+        expressions: {
+          Expr: ["+", 1, 1],
+        },
       },
     },
-  }).then(({ body: baseCard }) => {
-    return H.createQuestion({
+    { wrapId: true },
+  ).then(({ body: card }) => {
+    H.createQuestion({
       name: "Question with fields",
       query: {
-        "source-table": `card__${baseCard.id}`,
+        "source-table": `card__${card.id}`,
         filter: [">", ["field", "Expr", { "base-type": "type/Integer" }], 1],
       },
-    }).then(() => {
-      return H.createQuestion({
-        name: "Question without fields",
-        query: {
-          "source-table": `card__${baseCard.id}`,
+    });
+    H.createQuestion({
+      name: "Question without fields",
+      query: {
+        "source-table": `card__${card.id}`,
+      },
+    });
+  });
+}
+
+function createModelContent() {
+  H.createNativeQuestion(
+    {
+      name: "Base model",
+      type: "model",
+      native: {
+        query: "SELECT ID, TITLE, CATEGORY FROM PRODUCTS",
+        "template-tags": {},
+      },
+    },
+    { wrapId: true, idAlias: "modelId" },
+  ).then(({ body: card }) => {
+    cy.request("POST", `/api/card/${card.id}/query`);
+
+    const tagName = `#${card.id}`;
+    H.createNativeQuestion({
+      name: "Question with fields",
+      native: {
+        query: `SELECT ID, CATEGORY FROM {{#${card.id}}}`,
+        "template-tags": {
+          [tagName]: {
+            id: "10422a0f-292d-10a3-fd90-407cc9e3e20e",
+            name: tagName,
+            "display-name": tagName,
+            type: "card",
+            "card-id": card.id,
+          },
         },
-      }).then(() => {
-        return { baseCard };
-      });
+      },
+    });
+    H.createNativeQuestion({
+      name: "Question without fields",
+      native: {
+        query: `SELECT COUNT(*) FROM {{#${card.id}}}`,
+        "template-tags": {
+          [tagName]: {
+            id: "10422a0f-292d-10a3-fd90-407cc9e3e20f",
+            name: tagName,
+            "display-name": tagName,
+            type: "card",
+            "card-id": card.id,
+          },
+        },
+      },
     });
   });
 }
