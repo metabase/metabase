02a8bd6096545f7257c922e856935fced844444d
Phoomparin Mano <phoomparin@gmail.com>
2025-09-22 23:13:16 +0700

    remove embedding setup

--- frontend/src/metabase-types/analytics/event.ts
+++ frontend/src/metabase-types/analytics/event.ts
@@ -167,29 +167,6 @@ export type VisualizerModalEvent = ValidateEvent<
     }
 >;
 
-export type EmbeddingSetupStepKey =
-  | "welcome"
-  | "user-creation"
-  | "data-connection"
-  | "table-selection"
-  | "processing"
-  | "add-to-your-app"
-  | "done";
-export type EmbeddingSetupStepSeenEvent = ValidateEvent<{
-  event: "embedding_setup_step_seen";
-  event_detail: EmbeddingSetupStepKey;
-}>;
-
-export type EmbeddingSetupClickEvent = ValidateEvent<{
-  event: "embedding_setup_click";
-  event_detail:
-    | "setup-up-manually"
-    | "add-data-later-or-skip"
-    | "snippet-copied"
-    | "ill-do-this-later";
-  triggered_from: EmbeddingSetupStepKey;
-}>;
-
 export type EventsClickedEvent = ValidateEvent<{
   event: "events_clicked";
   triggered_from: "chart" | "collection";
@@ -493,8 +470,6 @@ export type SimpleEvent =
   | NewButtonItemClickedEvent
   | VisualizeAnotherWayClickedEvent
   | VisualizerModalEvent
-  | EmbeddingSetupStepSeenEvent
-  | EmbeddingSetupClickEvent
   | EventsClickedEvent
   | AddDataModalOpenedEvent
   | AddDataModalTabEvent
--- frontend/src/metabase/public/components/EmbedModal/EmbedModal.tsx
+++ frontend/src/metabase/public/components/EmbedModal/EmbedModal.tsx
@@ -5,7 +5,6 @@ import Modal from "metabase/common/components/Modal";
 import { useSelector } from "metabase/lib/redux";
 import type { EmbedModalStep } from "metabase/public/lib/types";
 import { getSetting } from "metabase/selectors/settings";
-import { getApplicationName } from "metabase/selectors/whitelabel";
 
 import { EmbedModalHeader } from "./EmbedModal.styled";
 
@@ -28,9 +27,6 @@ export const EmbedModal = ({ children, isOpen, onClose }: EmbedModalProps) => {
     getSetting(state, "show-static-embed-terms"),
   );
   const [embedType, setEmbedType] = useState<EmbedModalStep>(null);
-  const applicationName = useSelector(getApplicationName);
-
-  const isEmbeddingSetupStage = embedType === null;
 
   const goToNextStep = () => {
     if (embedType === null && shouldShowEmbedTerms) {
@@ -53,17 +49,15 @@ export const EmbedModal = ({ children, isOpen, onClose }: EmbedModalProps) => {
     <Modal
       isOpen={isOpen}
       onClose={onEmbedClose}
-      title={isEmbeddingSetupStage ? t`Embed ${applicationName}` : undefined}
       fit
       formModal={false}
       // needed to allow selecting with the mouse on the code samples
       enableMouseEvents
     >
-      {!isEmbeddingSetupStage && (
-        <EmbedModalHeader onClose={onEmbedClose} onBack={goBackToEmbedModal}>
-          {t`Static embedding`}
-        </EmbedModalHeader>
-      )}
+      <EmbedModalHeader onClose={onEmbedClose} onBack={goBackToEmbedModal}>
+        {t`Static embedding`}
+      </EmbedModalHeader>
+
       {children({ embedType, goToNextStep, goBackToEmbedModal })}
     </Modal>
   );
