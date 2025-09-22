dc9bbc95d561de0a61c5cf7033b9ca3213e78343
Eric Normand <ericwnormand@gmail.com>
2025-09-22 11:39:47 -0500

    More question tests

--- enterprise/backend/test/metabase_enterprise/representations/v0/question_test.clj
+++ enterprise/backend/test/metabase_enterprise/representations/v0/question_test.clj
@@ -1,7 +1,8 @@
 (ns metabase-enterprise.representations.v0.question-test
   (:require
-   [clojure.test :refer [deftest is testing use-fixtures]]
+   [clojure.test :refer :all]
    [metabase-enterprise.representations.core :as rep]
+   [metabase.test :as mt]
    [metabase.test.fixtures :as fixtures]
    [metabase.util.yaml :as yaml]
    [toucan2.core :as t2]))
@@ -23,9 +24,18 @@
           (is (thrown? clojure.lang.ExceptionInfo (rep/validate rep))))))))
 
 (deftest validate-exported-questions
-  (doseq [question (t2/select :model/Card :type :question)]
-    (let [edn (rep/export question)
-          ;; convert to yaml and read back in to convert keywords to strings, etc
-          yaml (yaml/generate-string edn)
-          rep  (yaml/parse-string yaml)]
-      (is (rep/validate rep)))))
+  (doseq [query [(mt/native-query {:query "select 1"})
+                 (mt/mbql-query users)]]
+    (mt/with-temp [:model/Card question {:type :question
+                                         :dataset_query query}]
+      (let [edn (rep/export question)
+            ;; convert to yaml and read back in to convert keywords to strings, etc
+            yaml (yaml/generate-string edn)
+            rep  (yaml/parse-string yaml)]
+        (is (rep/validate rep))))))
+
+(deftest can-import
+  (let [filename "test_resources/representations/v0/monthly-revenue.question.yml"
+        rep (yaml/from-file filename)]
+    (is (rep/persist! rep))))
+
