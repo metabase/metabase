d04ffc2925c69d6546c86fe1af7bd30ad4756ba1
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-09-22 09:20:27 -0400

    Change the default editor height for transforms (#63773)

--- enterprise/frontend/src/metabase-enterprise/transforms/components/QueryEditor/EditorBody/EditorBody.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/QueryEditor/EditorBody/EditorBody.tsx
@@ -23,7 +23,7 @@ import { ResizeHandle } from "../ResizeHandle";
 
 import S from "./EditorBody.module.css";
 
-const EDITOR_HEIGHT = 400;
+const EDITOR_HEIGHT = 550;
 
 const NATIVE_EDITOR_SIDEBAR_FEATURES = {
   dataReference: true,
