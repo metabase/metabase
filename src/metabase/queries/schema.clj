530f3514475c1cb29a051608ba7f348315deafde
Cam Saul <github@camsaul.com>
2025-09-22 12:30:55 -0700

    Test fixes :wrench:

--- src/metabase/queries/schema.clj
+++ src/metabase/queries/schema.clj
@@ -1,7 +1,12 @@
 (ns metabase.queries.schema
   (:require
    [metabase.lib.schema.metadata :as lib.schema.metadata]
-   [metabase.util.malli.registry :as mr]))
+   [metabase.util.malli.registry :as mr]
+   [potemkin :as p]))
+
+(p/import-vars
+ [lib.schema.metadata
+  card-types])
 
 (mr/def ::card-type
   [:ref ::lib.schema.metadata/card.type])
