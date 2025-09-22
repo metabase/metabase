e7e8a0d13134e27daaebc38ef345984ff1738195
Cam Saul <github@camsaul.com>
2025-09-22 15:23:10 -0700

    :annoyed:

--- src/metabase/queries/schema.clj
+++ src/metabase/queries/schema.clj
@@ -15,29 +15,30 @@
 (mr/def ::query
   :map
   ;; TODO (Cam 9/22/25) -- disabled because this breaks too many tests, enable this soon.
-  #_[:multi
-     {:dispatch (fn [x]
-                  (cond
-                    (not (map? x)) ::not-map
-                    (empty? x)     ::empty-map
-                    :else          ::non-empty-map))}
-     [::not-map       :map]
-     [::empty-map     [:= {:description "empty map"} {}]]
-     [::non-empty-map [:and
-                       [:map
-                        [:database ::lib.schema.id/database]]
-                       [:multi
-                        {:dispatch (fn [x]
-                                     (if (:lib/type x)
-                                       ::mbql5
-                                       ::legacy))}
-                        [::mbql5  ::lib.schema/query]
-                        [::legacy ::mbql.s/Query]]]]])
+  ;; [:multi
+  ;;  {:dispatch (fn [x]
+  ;;               (cond
+  ;;                 (not (map? x)) ::not-map
+  ;;                 (empty? x)     ::empty-map
+  ;;                 :else          ::non-empty-map))}
+  ;;  [::not-map       :map]
+  ;;  [::empty-map     [:= {:description "empty map"} {}]]
+  ;;  [::non-empty-map [:and
+  ;;                    [:map
+  ;;                     [:database ::lib.schema.id/database]]
+  ;;                    [:multi
+  ;;                     {:dispatch (fn [x]
+  ;;                                  (if (:lib/type x)
+  ;;                                    ::mbql5
+  ;;                                    ::legacy))}
+  ;;                     [::mbql5  ::lib.schema/query]
+  ;;                     [::legacy ::mbql.s/Query]]]]]
+  )
 
 (mr/def ::card
   [:map
    [:id            {:optional true} ::lib.schema.id/card]
    [:database_id   {:optional true} ::lib.schema.id/database]
    ;; TODO (Cam 9/22/25) -- disabled because this breaks too many tests, enable this soon.
-   #_[:type          {:optional true} [:ref ::lib.schema.metadata/card.type]]
+   ;; [:type          {:optional true} [:ref ::lib.schema.metadata/card.type]]
    [:dataset_query {:optional true} [:ref ::query]]])
