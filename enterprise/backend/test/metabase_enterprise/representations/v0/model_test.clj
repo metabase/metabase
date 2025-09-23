4434205bc07518d62cdd660d259d7e22c3c260e6
Eric Normand <ericwnormand@gmail.com>
2025-09-23 09:34:10 -0500

    Add model test

---
+++ enterprise/backend/test/metabase_enterprise/representations/v0/model_test.clj
@@ -0,0 +1,40 @@
+(ns metabase-enterprise.representations.v0.model-test
+  (:require
+   [clojure.test :refer :all]
+   [metabase-enterprise.representations.core :as rep]
+   [metabase.test :as mt]
+   [metabase.test.fixtures :as fixtures]
+   [metabase.util.yaml :as yaml]
+   [toucan2.core :as t2]))
+
+(use-fixtures :once (fixtures/initialize :db))
+
+(deftest validate-example-yamls
+  (testing "Testing valid examples"
+    (doseq [filename
+            ["test_resources/representations/v0/product-performance.model.yml"
+             "test_resources/representations/v0/sales-data-enriched.model.yml"
+             "test_resources/representations/v0/sale-data.model.yml"]]
+      (testing (str "Validating: " filename)
+        (let [rep (yaml/from-file filename)]
+          (is (rep/validate rep)))))))
+
+(deftest validate-exported-models
+  (doseq [query [(mt/native-query {:query "select 1"})
+                 (mt/mbql-query users)]]
+    (mt/with-temp [:model/Card model {:type :model
+                                      :dataset_query query}]
+      (let [edn (rep/export model)
+            ;; convert to yaml and read back in to convert keywords to strings, etc
+            yaml (yaml/generate-string edn)
+            rep  (yaml/parse-string yaml)]
+        (is (rep/validate rep))))))
+
+(deftest can-import
+  (doseq [filename ["test_resources/representations/v0/product-performance.model.yml"
+                    "test_resources/representations/v0/sales-data-enriched.model.yml"
+                    "test_resources/representations/v0/sale-data.model.yml"]]
+    (testing (str "Importing: " filename)
+      (let [rep (yaml/from-file filename)]
+        (is (rep/persist! rep))))))
+
