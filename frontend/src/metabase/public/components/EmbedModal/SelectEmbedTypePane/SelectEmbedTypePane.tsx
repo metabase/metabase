021198ff18fe00562166eb63c2ba4b31cf5d4c74
Phoomparin Mano <phoomparin@gmail.com>
2025-09-24 07:22:29 +0700

    remove unused no-literal-metabase-strings

--- frontend/src/metabase/public/components/EmbedModal/SelectEmbedTypePane/SelectEmbedTypePane.tsx
+++ frontend/src/metabase/public/components/EmbedModal/SelectEmbedTypePane/SelectEmbedTypePane.tsx
@@ -148,7 +148,6 @@ export function SelectEmbedTypePane({
             disabledLink="/admin/embedding/modular"
           >
             <List>
-              {/* eslint-disable-next-line no-literal-metabase-strings -- only admin sees this */}
               <List.Item>{t`A simple way to embed using plain JavaScript`}</List.Item>
               <List.Item>{t`Embed static or interactive dashboards and charts with drill-down, the query builder or let people browse and manage collections.`}</List.Item>
               <List.Item>
