2b7b10ba20ea7fa685303c3a8a4ea83b5f46b829
Riley Thompson <riley@metabase.com>
2025-09-24 01:10:13 -0400

    Fix starbust query cancellation with isClosed proxy implementations (#63857)
    * add isClosed impls to proxys
    
    * use mt/id in test
    
    * test all drivers
    
    * check if statements are supported, refactor statement-supported? multimethod to drive feature
    
    * add namespace to statements feature

--- modules/drivers/starburst/test/metabase/driver/starburst_test.clj
+++ modules/drivers/starburst/test/metabase/driver/starburst_test.clj
@@ -376,3 +376,15 @@
                    #"Access Denied: Cannot set role sysadmin"
                    (qp/process-query
                     (mt/native-query {:query "SELECT current_user"})))))))))))
+
+(deftest optimized-prepared-statement-is-closed-test
+  (mt/test-driver :starburst
+    (testing "can check isClosed on optimized prepared statement"
+      (sql-jdbc.execute/do-with-connection-with-options
+       driver/*driver* (mt/id) nil
+       (fn [^Connection conn]
+         (let [stmt (.prepareStatement conn "select 1" ResultSet/TYPE_FORWARD_ONLY ResultSet/CONCUR_READ_ONLY)
+               prepared-stmt (#'starburst/proxy-optimized-prepared-statement driver/*driver* conn stmt [])]
+           (is (false? (.isClosed prepared-stmt)))
+           (.close stmt)
+           (is (true? (.isClosed prepared-stmt)))))))))
--- src/metabase/driver.clj
+++ src/metabase/driver.clj
@@ -712,7 +712,16 @@
     :test/uuids-in-create-table-statements
 
     ;; Does this driver support Metabase's database routing feature?
-    :database-routing})
+    :database-routing
+
+    ;; Does this driver support replication?
+    :database-replication
+
+    ;; whether this driver supports checking table writeable permissions
+    :metadata/table-writable-check
+
+    ;; Does this driver support creating a java.sql.Statement via a Connection?
+    :jdbc/statements})
 
 (defmulti database-supports?
   "Does this driver and specific instance of a database support a certain `feature`?
