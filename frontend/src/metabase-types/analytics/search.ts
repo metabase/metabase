d3b414ce6b54e12d960ab2e52eea3784e1953302
Tyler Plude <tsplude@gmail.com>
2025-09-24 14:07:20 -0700

    Add result ID to search click event tracking

--- frontend/src/metabase-types/analytics/search.ts
+++ frontend/src/metabase-types/analytics/search.ts
@@ -75,6 +75,7 @@ export type SearchClickEvent = ValidateEvent<{
   search_engine: string | null;
   request_id: string | null;
   entity_model: string | null;
+  entity_id: number | null;
   search_term_hash: string | null;
   search_term: string | null;
 }>;
