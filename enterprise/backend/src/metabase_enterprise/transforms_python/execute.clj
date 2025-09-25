c925bdaa9a39d2f7644f4f1dbfb15222cb9bac95
Nicola Mometto <nicola.mometto@metabase.com>
2025-09-25 12:16:01 +0100

    replace_existing

--- enterprise/backend/src/metabase_enterprise/transforms_python/execute.clj
+++ enterprise/backend/src/metabase_enterprise/transforms_python/execute.clj
@@ -16,7 +16,7 @@
   (:import
    (java.io Closeable InputStream)
    (java.net SocketException)
-   (java.nio.file Files CopyOption)
+   (java.nio.file Files CopyOption StandardCopyOption)
    (java.nio.file.attribute FileAttribute)
    (java.time Duration)))
 
@@ -279,7 +279,7 @@
                                :events                 events
                                :transform-run-message  (message-log->transform-run-message message-log)})))
             (try
-              (Files/copy ^InputStream output-stream temp-path (u/varargs CopyOption))
+              (Files/copy ^InputStream output-stream temp-path (u/varargs CopyOption [StandardCopyOption/REPLACE_EXISTING]))
               (let [file-size (.length temp-file)]
                 (transforms.instrumentation/with-stage-timing [run-id :file-to-dwh]
                   (transfer-file-to-db driver db transform output-manifest temp-file))
