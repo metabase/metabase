a00a14d615909254e67c8502b63ed668f368f1c7
Ngoc Khuat <qn.khuat@gmail.com>
2025-09-26 15:37:45 +0700

    needs to load the impl

--- enterprise/backend/src/metabase_enterprise/transforms_python/init.clj
+++ enterprise/backend/src/metabase_enterprise/transforms_python/init.clj
@@ -1,5 +1,4 @@
 (ns metabase-enterprise.transforms-python.init
   (:require
+   [metabase-enterprise.transforms-python.impl]
    [metabase-enterprise.transforms-python.settings]))
-
-(comment metabase-enterprise.transforms-python.settings/keepme)
