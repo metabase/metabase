96c5218a0e711276095796d2badf09d49e719fc4
Noah Moss <noahbmoss@gmail.com>
2025-09-26 15:42:07 -0400

    Conditionally add transforms to model->db-model

--- src/metabase/search/config.clj
+++ src/metabase/search/config.clj
@@ -42,7 +42,8 @@
 (def model->db-model
   "Mapping of model name to :db_model and :alias"
   (cond-> api/model->db-model
-    config/ee-available? (assoc "document" {:db-model :model/Document :alias :document})))
+    config/ee-available? (assoc "document" {:db-model :model/Document :alias :document}
+                                "transform" {:db-model :model/Transform :alias :transform})))
 
 ;; We won't need this once fully migrated to specs, but kept for now in case legacy cod falls out of sync
 (def excluded-models
