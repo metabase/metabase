5e194641e02064bbb24b9f33b175abd59dcb42d0
Braden Shepherdson <braden@metabase.com>
2025-09-29 09:29:01 -0400

    Fix custom PK logic in dataset loading

--- modules/drivers/vertica/test/metabase/test/data/vertica.clj
+++ modules/drivers/vertica/test/metabase/test/data/vertica.clj
@@ -122,16 +122,13 @@
   "Dump a sequence of rows (as vectors) to a CSV file."
   [{:keys [field-definitions rows]} ^String filename]
   (try
-    (let [has-custom-pk? (when-let [pk (not-empty (sql.tx/fielddefs->pk-field-names field-definitions))]
-                           (= ["id"] pk))
+    (let [needs-pk?      (empty? (sql.tx/fielddefs->pk-field-names field-definitions))
           column-names   (cond->> (mapv :field-name field-definitions)
-                           (not has-custom-pk?)
-                           (cons "id"))
+                           needs-pk? (cons "id"))
           rows-with-id (for [[i row] (m/indexed rows)]
                          (cond->> (for [v row]
                                     (value->csv v))
-                           (not has-custom-pk?)
-                           (cons (inc i))))
+                           needs-pk? (cons (inc i))))
 
           csv-rows     (cons column-names rows-with-id)]
       (try
