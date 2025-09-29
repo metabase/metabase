d7d10c4c9563ad12caeae1f36beb8e48dc00fdfa
Timofei <timofei@metabase.com>
2025-09-29 12:08:55 +0400

    Add e2e

--- e2e/test-component/scenarios/embedding-sdk/sdk-question-native.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/sdk-question-native.cy.spec.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import {
+  type NativeQuestionDetails,
   createNativeQuestion,
   tableInteractiveBody,
 } from "e2e/support/helpers";
@@ -15,122 +16,218 @@ import { signInAsAdminAndEnableEmbeddingSdk } from "e2e/support/helpers/embeddin
 import { mockAuthProviderAndJwtSignIn } from "e2e/support/helpers/embedding-sdk-testing/embedding-sdk-helpers";
 import { Box, Button } from "metabase/ui";
 import type { DatasetColumn } from "metabase-types/api";
+import { createMockParameter } from "metabase-types/api/mocks";
 
 const { ORDERS, ORDERS_ID } = SAMPLE_DATABASE;
 
-describe("scenarios > embedding-sdk > interactive-question > native", () => {
-  beforeEach(() => {
-    signInAsAdminAndEnableEmbeddingSdk();
+const setup = ({ question }: { question: NativeQuestionDetails }) => {
+  signInAsAdminAndEnableEmbeddingSdk();
 
-    createNativeQuestion(
-      {
-        native: {
-          query: "SELECT * FROM orders WHERE {{ID}}",
-          "template-tags": {
-            ID: {
-              id: "6b8b10ef-0104-1047-1e1b-2492d5954322",
-              name: "ID",
-              "display-name": "ID",
-              type: "dimension",
-              dimension: ["field", ORDERS.ID, null],
-              "widget-type": "category",
-              default: null,
+  createNativeQuestion(question, { wrapId: true });
+
+  cy.signOut();
+  mockAuthProviderAndJwtSignIn();
+};
+
+const ensureColumnValueAfterParameterEditing = ({
+  columnName,
+  columnValue,
+}: {
+  columnName: string;
+  columnValue: string;
+}) => {
+  cy.wait("@cardQuery").then(() => {
+    tableInteractiveBody().within(() => {
+      cy.findAllByRole("row")
+        .its("length")
+        .then((rowsLength) => {
+          cy.get(
+            `[data-column-id="${columnName}"] [data-testid="cell-data"]`,
+          ).should(($cells) => {
+            expect($cells).to.have.length(rowsLength);
+
+            const mismatches = Cypress.$($cells).filter(
+              (_, el) => el.textContent.trim() !== columnValue,
+            );
+
+            expect(
+              mismatches,
+              `cells with non-${columnValue} content`,
+            ).to.have.length(0);
+          });
+        });
+    });
+  });
+};
+
+describe("scenarios > embedding-sdk > interactive-question > native", () => {
+  describe("common", () => {
+    beforeEach(() => {
+      setup({
+        question: {
+          native: {
+            query: "SELECT * FROM orders WHERE {{ID}}",
+            "template-tags": {
+              ID: {
+                id: "6b8b10ef-0104-1047-1e1b-2492d5954322",
+                name: "ID",
+                "display-name": "ID",
+                type: "dimension",
+                dimension: ["field", ORDERS.ID, null],
+                "widget-type": "category",
+                default: null,
+              },
             },
           },
         },
-      },
-      { wrapId: true },
-    );
-
-    cy.signOut();
-    mockAuthProviderAndJwtSignIn();
-  });
-
-  it("supports passing sql parameters to native questions", () => {
-    mountInteractiveQuestion({ initialSqlParameters: { ID: ORDERS_ID } });
-
-    cy.wait("@cardQuery").then(({ response }) => {
-      const { body } = response ?? {};
-
-      const rows = tableInteractiveBody().findAllByRole("row");
-
-      // There should be one row in the table
-      rows.should("have.length", 1);
-
-      const idColumnIndex = body.data.cols.findIndex(
-        (column: DatasetColumn) => column.name === "ID",
-      );
-
-      // The first row should have the same ID column value as the initial SQL parameters
-      // eslint-disable-next-line no-unsafe-element-filtering
-      rows
-        .findAllByTestId("cell-data")
-        .eq(idColumnIndex)
-        .should("have.text", String(ORDERS_ID));
-    });
-  });
-
-  it("should not crash when switching from new question to native question (metabase#60160)", () => {
-    const TestComponent = ({
-      nativeQuestionId,
-    }: {
-      nativeQuestionId: string | number;
-    }) => {
-      const [questionId, setQuestionId] = useState<string | number>("new");
-
-      return (
-        <Box>
-          <InteractiveQuestion questionId={questionId} />
-
-          <Button onClick={() => setQuestionId(nativeQuestionId)}>
-            use native question
-          </Button>
-        </Box>
-      );
-    };
-
-    cy.get<string>("@questionId").then((nativeQuestionId) => {
-      mountSdkContent(<TestComponent nativeQuestionId={nativeQuestionId} />);
-
-      getSdkRoot().within(() => {
-        cy.findByText("Pick your starting data").should("be.visible");
       });
+    });
 
-      cy.findByText("use native question").click();
+    it("supports passing sql parameters to native questions", () => {
+      mountInteractiveQuestion({ initialSqlParameters: { ID: ORDERS_ID } });
 
-      cy.log("should show native question table data");
+      cy.wait("@cardQuery").then(({ response }) => {
+        const { body } = response ?? {};
+
+        const rows = tableInteractiveBody().findAllByRole("row");
+
+        // There should be one row in the table
+        rows.should("have.length", 1);
+
+        const idColumnIndex = body.data.cols.findIndex(
+          (column: DatasetColumn) => column.name === "ID",
+        );
+
+        // The first row should have the same ID column value as the initial SQL parameters
+        // eslint-disable-next-line no-unsafe-element-filtering
+        rows
+          .findAllByTestId("cell-data")
+          .eq(idColumnIndex)
+          .should("have.text", String(ORDERS_ID));
+      });
+    });
+
+    it("should not crash when switching from new question to native question (metabase#60160)", () => {
+      const TestComponent = ({
+        nativeQuestionId,
+      }: {
+        nativeQuestionId: string | number;
+      }) => {
+        const [questionId, setQuestionId] = useState<string | number>("new");
+
+        return (
+          <Box>
+            <InteractiveQuestion questionId={questionId} />
+
+            <Button onClick={() => setQuestionId(nativeQuestionId)}>
+              use native question
+            </Button>
+          </Box>
+        );
+      };
+
+      cy.get<string>("@questionId").then((nativeQuestionId) => {
+        mountSdkContent(<TestComponent nativeQuestionId={nativeQuestionId} />);
+
+        getSdkRoot().within(() => {
+          cy.findByText("Pick your starting data").should("be.visible");
+        });
+
+        cy.findByText("use native question").click();
+
+        cy.log("should show native question table data");
+        getSdkRoot().within(() => {
+          cy.findByText("Pick your starting data").should("not.exist");
+          cy.findByText("test question").should("be.visible");
+          cy.findByText("110.93").should("be.visible");
+        });
+
+        cy.log("should not crash due to preview-query error");
+        cy.on("uncaught:exception", (error) => {
+          expect(
+            error.message.includes(
+              "preview-query cannot be called on native queries",
+            ),
+          ).to.be.false;
+        });
+      });
+    });
+
+    it("should not show filter/summarize/breakout buttons for native questions", () => {
+      mountInteractiveQuestion({});
+
       getSdkRoot().within(() => {
-        cy.findByText("Pick your starting data").should("not.exist");
+        cy.log("should show native question data");
         cy.findByText("test question").should("be.visible");
-        cy.findByText("110.93").should("be.visible");
-      });
 
-      cy.log("should not crash due to preview-query error");
-      cy.on("uncaught:exception", (error) => {
-        expect(
-          error.message.includes(
-            "preview-query cannot be called on native queries",
-          ),
-        ).to.be.false;
+        cy.log("should not show Filter button");
+        cy.findByText("Filter").should("not.exist");
+
+        cy.log("should not show Summarize button");
+        cy.findByText("Summarize").should("not.exist");
+
+        cy.log("should not show Group button");
+        cy.findByText("Group").should("not.exist");
       });
     });
   });
 
-  it("should not show filter/summarize/breakout buttons for native questions", () => {
-    mountInteractiveQuestion({});
+  describe("editable parameters", () => {
+    beforeEach(() => {
+      setup({
+        question: {
+          native: {
+            query:
+              "SELECT * FROM people WHERE state = {{State}} [[ and city = {{City}} ]] [[ and source = {{Source}} ]]",
+            "template-tags": {
+              State: {
+                type: "text",
+                name: "State",
+                id: "1",
+                "display-name": "State",
+              },
+              City: {
+                type: "text",
+                name: "City",
+                id: "2",
+                "display-name": "City",
+              },
+              Source: {
+                type: "text",
+                name: "Source",
+                id: "3",
+                "display-name": "Source",
+              },
+            },
+          },
+          parameters: [
+            createMockParameter({
+              id: "1",
+              slug: "State",
+            }),
+            createMockParameter({
+              id: "2",
+              slug: "City",
+            }),
+            createMockParameter({
+              id: "3",
+              slug: "Source",
+            }),
+          ],
+        },
+      });
+    });
 
-    getSdkRoot().within(() => {
-      cy.log("should show native question data");
-      cy.findByText("test question").should("be.visible");
+    it("shows editable parameters controls for sql parameters of native questions", () => {
+      mountInteractiveQuestion({
+        initialSqlParameters: { State: "AR" },
+        hiddenParameters: ["Source"],
+      });
 
-      cy.log("should not show Filter button");
-      cy.findByText("Filter").should("not.exist");
-
-      cy.log("should not show Summarize button");
-      cy.findByText("Summarize").should("not.exist");
-
-      cy.log("should not show Group button");
-      cy.findByText("Group").should("not.exist");
+      ensureColumnValueAfterParameterEditing({
+        columnName: "STATE",
+        columnValue: "AR",
+      });
     });
   });
 });
