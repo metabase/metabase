a08ef2c732fd69677f3f8e11737fcea35fc7c101
Ryan Laurie <iethree@gmail.com>
2025-09-29 14:03:13 -0600

    bench scaffold

---
+++ frontend/src/metabase/bench/routes.tsx
@@ -0,0 +1,68 @@
+import { IndexRedirect, Redirect } from "react-router";
+import { t } from "ttag";
+
+import RevisionHistoryApp from "metabase/admin/datamodel/containers/RevisionHistoryApp";
+import SegmentApp from "metabase/admin/datamodel/containers/SegmentApp";
+import SegmentListApp from "metabase/admin/datamodel/containers/SegmentListApp";
+import { createAdminRouteGuard } from "metabase/admin/utils";
+import { BrowseMetrics, BrowseModels } from "metabase/browse";
+import NotFoundFallbackPage from "metabase/common/components/NotFoundFallbackPage";
+import { Route } from "metabase/hoc/Title";
+import { DataModel } from "metabase/metadata/pages/DataModel";
+import {
+  PLUGIN_TRANSFORMS,
+} from "metabase/plugins";
+
+import { BenchApp } from "./components/BenchApp";
+
+export const getBenchRoutes = (store: Store, CanAccessSettings: boolean, IsAdmin: boolean) => (
+  <Route path="/bench" component={CanAccessSettings}>
+    <Route title={t`Bench`} component={BenchApp}>
+      <IndexRedirect to="overview" />
+      <Route path="overview" component={() => <div>Overview</div>} />
+      {PLUGIN_TRANSFORMS.getAdminRoutes()}
+      <Route path="segments" component={SegmentListApp} />
+      <Route path="models" component={BrowseModels} />
+      <Route path="metrics" component={BrowseMetrics} />
+
+
+      <Route path="metadata" component={createAdminRouteGuard("data-model")}>
+        <Route title={t`Table Metadata`}>
+          <IndexRedirect to="database" />
+          <Route path="database" component={DataModel} />
+          <Route path="database/:databaseId" component={DataModel} />
+          <Route
+            path="database/:databaseId/schema/:schemaId"
+            component={DataModel}
+          />
+          <Route
+            path="database/:databaseId/schema/:schemaId/table/:tableId"
+            component={DataModel}
+          />
+          <Route
+            path="database/:databaseId/schema/:schemaId/table/:tableId/field/:fieldId"
+            component={DataModel}
+          />
+          <Route component={DataModel}>
+            <Route path="segments" component={SegmentListApp} />
+            <Route path="segment/create" component={SegmentApp} />
+            <Route path="segment/:id" component={SegmentApp} />
+            <Route
+              path="segment/:id/revisions"
+              component={RevisionHistoryApp}
+            />
+          </Route>
+          <Redirect
+            from="database/:databaseId/schema/:schemaId/table/:tableId/settings"
+            to="database/:databaseId/schema/:schemaId/table/:tableId"
+          />
+          <Redirect
+            from="database/:databaseId/schema/:schemaId/table/:tableId/field/:fieldId/:section"
+            to="database/:databaseId/schema/:schemaId/table/:tableId/field/:fieldId"
+          />
+        </Route>
+      </Route>
+      <Route path="/*" component={NotFoundFallbackPage}/>
+    </Route>
+  </Route>
+);
