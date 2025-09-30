2cbce9084016e2c0cc3590371d6c42ff2a242581
Dan Stone <wotbrew@gmail.com>
2025-09-30 15:07:35 +0100

    Ensure premium feature enabled so setting can be changed

--- enterprise/backend/test/metabase_enterprise/transforms_python/python_runner_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms_python/python_runner_test.clj
@@ -498,14 +498,15 @@
 
 (deftest python-runner-timeout-test
   (testing "Python script execution respects timeout setting"
-    (tu/with-temporary-setting-values [python-runner-timeout-seconds 5]
-      (let [long-running-code (str "import time\n"
-                                   "import pandas as pd\n"
-                                   "\n"
-                                   "def transform():\n"
-                                   "    time.sleep(10)  # Sleep longer than timeout\n"
-                                   "    return pd.DataFrame({'result': ['should_not_reach_here']})")
-            result (execute! {:code long-running-code})]
-        (testing "Script should timeout after 5 seconds"
-          (is (contains? result :error))
-          (is (str/includes? (:error result) "timeout")))))))
+    (mt/with-premium-features #{:transforms-python}
+      (tu/with-temporary-setting-values [python-runner-timeout-seconds 5]
+        (let [long-running-code (str "import time\n"
+                                     "import pandas as pd\n"
+                                     "\n"
+                                     "def transform():\n"
+                                     "    time.sleep(10)  # Sleep longer than timeout\n"
+                                     "    return pd.DataFrame({'result': ['should_not_reach_here']})")
+              result            (execute! {:code long-running-code})]
+          (testing "Script should timeout after 5 seconds"
+            (is (contains? result :error))
+            (is (str/includes? (:error result) "timeout"))))))))
