10f91c933335c843574d8f97c92df9358366a4b8
lbrdnk <lbrdnk@users.noreply.github.com>
2025-09-30 14:02:14 +0200

    Enable copying of iframe cards

--- src/metabase/dashboards/api.clj
+++ src/metabase/dashboards/api.clj
@@ -442,6 +442,10 @@
               (:action_id dashboard-card)
               nil
 
+              ;; iframe cards need no manipulation
+              (some-> dashboard-card :visualization_settings :iframe)
+              dashboard-card
+
               ;; text cards need no manipulation
               (some-> dashboard-card :visualization_settings :virtual_card :display #{"text" "heading"})
               dashboard-card
