2b9f879c54895fa7889bd6aade0dfa7f10493eb5
Denis Berezin <denis.berezin@metabase.com>
2025-10-01 01:38:00 +0400

    WIP: models section for workbench (#64210)
    * WIP: models section for workbench
    
    * Add more reusable component for list section

---
+++ frontend/src/metabase/bench/components/ItemsListSection/ItemsListSection.tsx
@@ -0,0 +1,130 @@
+import type React from "react";
+import { useState } from "react";
+import { t } from "ttag";
+
+import { Box, Button, Flex, Group, Icon, Menu, Text } from "metabase/ui";
+
+import type { BenchItemsListSorting } from "./types";
+
+type ItemsListSectionProps = {
+  sectionTitle: React.ReactNode;
+  titleMenuItems: React.ReactNode;
+  onChangeSorting: (sorting: BenchItemsListSorting) => void;
+  onAddNewItem: () => void;
+
+  listItems: React.ReactNode;
+};
+
+export const ItemsListSection = ({
+  sectionTitle,
+  titleMenuItems,
+  onChangeSorting,
+  onAddNewItem,
+  listItems,
+}: ItemsListSectionProps) => {
+  const [isFiltersMenuOpen, setIsFiltersMenuOpen] = useState(false);
+  const [isSortingMenuOpen, setIsSortingMenuOpen] = useState(false);
+
+  return (
+    <Box miw={300}>
+      <Flex
+        direction="row"
+        justify="space-between"
+        align="center"
+        mb="md"
+        p="md"
+      >
+        <Menu
+          // filters
+          opened={isFiltersMenuOpen}
+          onClose={() => setIsFiltersMenuOpen(false)}
+          position="bottom-start"
+          shadow="md"
+          width={200}
+        >
+          <Menu.Target>
+            <Group
+              gap="sm"
+              style={{
+                cursor: "pointer",
+                userSelect: "none",
+              }}
+              onClick={() => setIsFiltersMenuOpen(!isFiltersMenuOpen)}
+            >
+              <Text size="lg" fw="bold" c="brand">
+                {sectionTitle}
+              </Text>
+              <Icon
+                name="chevrondown"
+                size={12}
+                c="brand"
+                style={{
+                  transform: isFiltersMenuOpen
+                    ? "rotate(180deg)"
+                    : "rotate(0deg)",
+                  transition: "transform 0.2s ease",
+                }}
+              />
+            </Group>
+          </Menu.Target>
+          <Menu.Dropdown>{titleMenuItems}</Menu.Dropdown>
+        </Menu>
+
+        <Group>
+          <Menu
+            // sorting
+            opened={isSortingMenuOpen}
+            onClose={() => setIsSortingMenuOpen(false)}
+            position="bottom-start"
+            shadow="md"
+            width={200}
+          >
+            <Menu.Target>
+              <Group
+                gap="sm"
+                style={{
+                  cursor: "pointer",
+                  userSelect: "none",
+                }}
+                onClick={() => setIsSortingMenuOpen(!isSortingMenuOpen)}
+              >
+                <Icon name="sort_arrows" size={12} c="brand" />
+                <Icon
+                  name="chevrondown"
+                  size={12}
+                  c="brand"
+                  style={{
+                    transform: isFiltersMenuOpen
+                      ? "rotate(180deg)"
+                      : "rotate(0deg)",
+                    transition: "transform 0.2s ease",
+                  }}
+                />
+              </Group>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item
+                onClick={() => onChangeSorting("alphabetical")}
+              >{t`Alphabetical`}</Menu.Item>
+              <Menu.Item
+                onClick={() => onChangeSorting("most-recent")}
+              >{t`Most recent`}</Menu.Item>
+              <Menu.Item
+                onClick={() => onChangeSorting("least-recent")}
+              >{t`Least recent`}</Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+
+          <Button
+            variant="filled"
+            color="brand"
+            compact
+            leftSection={<Icon name="add" size={12} />}
+            onClick={onAddNewItem}
+          />
+        </Group>
+      </Flex>
+      <ul>{listItems}</ul>
+    </Box>
+  );
+};
---
+++ frontend/src/metabase/bench/components/ItemsListSection/types.ts
@@ -0,0 +1,4 @@
+export type BenchItemsListSorting =
+  | "alphabetical"
+  | "most-recent"
+  | "least-recent";
