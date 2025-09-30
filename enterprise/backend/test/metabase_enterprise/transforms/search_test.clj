fc5ef7ce69c0a9a25a9a0763053f1ea52d9b2b1f
Noah Moss <noahbmoss@gmail.com>
2025-09-30 13:28:35 -0400

    expand ingestion tests

--- enterprise/backend/test/metabase_enterprise/transforms/search_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms/search_test.clj
@@ -2,13 +2,17 @@
   (:require
    [clojure.test :refer :all]
    [java-time.api :as t]
+   [metabase-enterprise.serialization.v2.ingest :as ingest]
+   [metabase.app-db.core :as mdb]
    [metabase.search.appdb.index :as search.index]
    [metabase.search.engine :as search.engine]
    [metabase.search.ingestion :as search.ingestion]
    [metabase.search.test-util :as search.tu]
    [metabase.test :as mt]
    [metabase.test.fixtures :as fixtures]
-   [toucan2.core :as t2]))
+   [toucan2.core :as t2])
+  (:import
+   (org.postgresql.util PGobject)))
 
 (set! *warn-on-reflection* true)
 
@@ -52,10 +56,9 @@
 
 (deftest transform-ingestion-test
   (search.tu/with-temp-index-table
-    (testing "A simple transform gets properly ingested & indexed for search"
-      (let [transform-name (mt/random-name)
-            now            (t/truncate-to (t/offset-date-time) :millis)]
-        (mt/with-temp [:model/Transform {transform-id :id} {:name        transform-name
+    (testing "A simple SQL transform gets properly ingested & indexed for search"
+      (let [now (t/truncate-to (t/offset-date-time) :millis)]
+        (mt/with-temp [:model/Transform {transform-id :id} {:name        "Test transform"
                                                             :description "A test transform"
                                                             :source      {:type "query"
                                                                           :query {:database (mt/id)
@@ -64,11 +67,85 @@
                                                                           :table "test_table"}
                                                             :created_at  now
                                                             :updated_at  now}]
-          (is (=? (index-entity
-                   {:model            "transform"
-                    :model_id         (str transform-id)
-                    :name             transform-name
-                    :database_id      (mt/id)
-                    :model_created_at now
-                    :model_updated_at now})
-                  (ingest-then-fetch! "transform" transform-name))))))))
+          (let [ingested-transform (ingest-then-fetch! "transform" "Test transform")]
+            (is (=? (index-entity
+                     {:model            "transform"
+                      :model_id         (str transform-id)
+                      :name             "Test transform"
+                      :database_id      (mt/id)
+                      :model_created_at now
+                      :model_updated_at now})
+                    ingested-transform))))))
+
+    (testing "A simple Python transform gets properly ingested & indexed for search"
+      (let [now (t/truncate-to (t/offset-date-time) :millis)]
+        (mt/with-temp [:model/Transform {transform-id :id} {:name        "Test Python transform"
+                                                            :description "A Python test transform"
+                                                            :source      {:type "python"
+                                                                          :source-database (mt/id)
+                                                                          :body "import pandas as pd\n"}
+                                                            :target      {:database (mt/id)}
+                                                            :created_at  now
+                                                            :updated_at  now}]
+          (let [ingested-transform (ingest-then-fetch! "transform" "Test Python transform")]
+            (is (=? (index-entity
+                     {:model            "transform"
+                      :model_id         (str transform-id)
+                      :name             "Test Python transform"
+                      :database_id      (mt/id)
+                      :model_created_at now
+                      :model_updated_at now})
+                    ingested-transform))))))
+
+    (testing "A simple MBQL transform gets properly ingested & indexed for search"
+      (let [now (t/truncate-to (t/offset-date-time) :millis)]
+        (mt/with-temp [:model/Transform {transform-id :id} {:name        "Test MBQL transform"
+                                                            :description "An MBQL test transform"
+                                                            :source      {:type "query"
+                                                                          :query (mt/mbql-query venues {:limit 10})}
+                                                            :target      {:database (mt/id)
+                                                                          :table "test_mbql_table"}
+                                                            :created_at  now
+                                                            :updated_at  now}]
+          (let [ingested-transform (ingest-then-fetch! "transform" "Test MBQL transform")]
+            (is (=? (index-entity
+                     {:model            "transform"
+                      :model_id         (str transform-id)
+                      :name             "Test MBQL transform"
+                      :database_id      (mt/id)
+                      :model_created_at now
+                      :model_updated_at now})
+                    ingested-transform))))))))
+
+(deftest transform-query-ingestion-test
+  (testing "Contents of SQL and Python transform sources are extracted and indexed for full-text search"
+    (when (= (mdb/db-type) :postgres)
+      (mt/with-temp [:model/Transform _ {:target {:database (mt/id)
+                                                  :table "test_table"}
+                                         :name "Test SQL transform"
+                                         :source {:type "query"
+                                                  :query {:database (mt/id)
+                                                          :native {:query "SELECT 1"}}}}]
+
+        (let [ingested-transform (ingest-then-fetch! "transform" "Test SQL transform")]
+          (is (= "'1':8B 'select':7B 'sql':2A,5B 'test':1A,4B 'transform':3A,6B"
+                 (.getValue ^PGobject (:with_native_query_vector ingested-transform))))))
+
+      (mt/with-temp [:model/Transform _ {:target {:database (mt/id)}
+                                         :source {:type "python"
+                                                  :source-database (mt/id)
+                                                  :body "import pandas as pd\n"}
+                                         :name "Test python transform"}]
+        (let [ingested-transform (ingest-then-fetch! "transform" "Test python transform")]
+          (is (= "'import':7B 'panda':8B 'pd':10B 'python':2A,5B 'test':1A,4B 'transform':3A,6B"
+                 (.getValue ^PGobject (:with_native_query_vector ingested-transform)))))))
+
+    (testing "MBQL queries are not indexed in with_native_query_vector"
+      (mt/with-temp [:model/Transform _ {:target {:database (mt/id)
+                                                  :table "test_mbql_table"}
+                                         :name "Test MBQL transform"
+                                         :source {:type "query"
+                                                  :query (mt/mbql-query venues {:limit 10})}}]
+        (let [ingested-transform (ingest-then-fetch! "transform" "Test MBQL transform")]
+          (is (= "'mbql':2A,5B 'test':1A,4B 'transform':3A,6B"
+                 (.getValue ^PGobject (:with_native_query_vector ingested-transform)))))))))
