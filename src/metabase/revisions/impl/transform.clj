a2d04a03d308917134195943a5da75039eaea1c0
Riley Thompson <rileythompson99@gmail.com>
2025-09-30 14:16:38 -0400

    remove unneeded arity

--- src/metabase/revisions/impl/transform.clj
+++ src/metabase/revisions/impl/transform.clj
@@ -5,8 +5,5 @@
 (def ^:private excluded-columns-for-transform-revision
   #{:id :entity_id :created_at :updated_at})
 
-(defmethod revision/serialize-instance :model/Transform
-  ([instance]
-   (revision/serialize-instance :model/Transform nil instance))
-  ([_model _id instance]
-   (apply dissoc instance excluded-columns-for-transform-revision)))
+(defmethod revision/serialize-instance :model/Transform [_model _id instance]
+  (apply dissoc instance excluded-columns-for-transform-revision))
