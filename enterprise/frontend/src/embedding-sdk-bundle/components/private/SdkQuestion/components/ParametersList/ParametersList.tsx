9e2b5def70de444ca8896c06c00d5ba21a158865
Timofei <timofei@metabase.com>
2025-09-30 22:15:28 +0400

    Make setParameterIndex as optional for ResponsiveParametersList

--- enterprise/frontend/src/embedding-sdk-bundle/components/private/SdkQuestion/components/ParametersList/ParametersList.tsx
+++ enterprise/frontend/src/embedding-sdk-bundle/components/private/SdkQuestion/components/ParametersList/ParametersList.tsx
@@ -4,6 +4,7 @@ import { useSdkQuestionContext } from "embedding-sdk-bundle/components/private/S
 import { ResponsiveParametersList } from "metabase/query_builder/components/ResponsiveParametersList";
 import * as Lib from "metabase-lib";
 import type { ParameterId } from "metabase-types/api";
+
 /**
  * Parameters list for SQL questions
  *
@@ -24,8 +25,6 @@ export const QuestionParametersList = () => {
     return isNative;
   }, [question]);
 
-  const query = useMemo(() => question?.legacyNativeQuery(), [question]);
-
   const parameters = useMemo(() => {
     if (!question || !originalQuestion) {
       return [];
@@ -42,7 +41,7 @@ export const QuestionParametersList = () => {
       );
   }, [originalQuestion, question]);
 
-  if (!question || !query || !isNativeQuestion) {
+  if (!question || !isNativeQuestion) {
     return null;
   }
 
@@ -55,22 +54,12 @@ export const QuestionParametersList = () => {
     updateQuestion(questionWithParams, { run: true });
   };
 
-  const setParameterIndex = (
-    parameterId: ParameterId,
-    parameterIndex: number,
-  ) => {
-    const newQuery = query.setParameterIndex(parameterId, parameterIndex);
-
-    updateQuestion(question.setDatasetQuery(newQuery.datasetQuery()));
-  };
-
   return (
     <ResponsiveParametersList
       cardId={question?.id()}
       dashboardId={question.dashboardId() ?? undefined}
       parameters={parameters}
       setParameterValue={setParameterValue}
-      setParameterIndex={setParameterIndex}
       enableParameterRequiredBehavior
       commitImmediately={false}
       isSortable={false}
