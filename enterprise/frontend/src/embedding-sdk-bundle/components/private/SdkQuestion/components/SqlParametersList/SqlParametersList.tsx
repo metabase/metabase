4d0d576c13f423d6e948b1519911488715f706f0
Timofey Kachalov <timofei@metabase.com>
2025-10-02 10:24:54 +0400

    feat(sdk-bundle): Add SqlParametersList component for InteractiveQuestion and StaticQuestion (#64187)

---
+++ enterprise/frontend/src/embedding-sdk-bundle/components/private/SdkQuestion/components/SqlParametersList/SqlParametersList.tsx
@@ -0,0 +1,77 @@
+import { useMemo } from "react";
+
+import { useSdkQuestionContext } from "embedding-sdk-bundle/components/private/SdkQuestion/context";
+import { ResponsiveParametersList } from "metabase/query_builder/components/ResponsiveParametersList";
+import * as Lib from "metabase-lib";
+import type { ParameterId } from "metabase-types/api";
+
+import SqlParametersListS from "./SqlParametersList.module.css";
+
+/**
+ * Parameters list for SQL questions
+ *
+ * @function
+ * @category InteractiveQuestion
+ */
+export const SqlParametersList = () => {
+  const {
+    question,
+    originalQuestion,
+    parameterValues,
+    updateParameterValues,
+    hiddenParameters,
+  } = useSdkQuestionContext();
+
+  const isNativeQuestion = useMemo(() => {
+    if (!question) {
+      return false;
+    }
+
+    const { isNative } = Lib.queryDisplayInfo(question.query());
+
+    return isNative;
+  }, [question]);
+
+  const parameters = useMemo(() => {
+    if (!question || !originalQuestion) {
+      return [];
+    }
+
+    const originalParameters = originalQuestion.card().parameters ?? [];
+
+    return question
+      .parameters()
+      .filter(
+        ({ id, slug }) =>
+          originalParameters.find(
+            (originalParameter) => originalParameter.id === id,
+          ) && !hiddenParameters?.includes(slug),
+      );
+  }, [question, originalQuestion, hiddenParameters]);
+
+  if (!question || !isNativeQuestion) {
+    return null;
+  }
+
+  const setParameterValue = (parameterId: ParameterId, value: string) => {
+    const nextParameterValues = {
+      ...parameterValues,
+      [parameterId]: value,
+    };
+
+    updateParameterValues(nextParameterValues);
+  };
+
+  return (
+    <ResponsiveParametersList
+      className={SqlParametersListS.SqlParametersList}
+      cardId={question.id()}
+      dashboardId={question.dashboardId() ?? undefined}
+      parameters={parameters}
+      setParameterValue={setParameterValue}
+      enableParameterRequiredBehavior
+      commitImmediately={false}
+      isSortable={false}
+    />
+  );
+};
