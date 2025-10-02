39692710bc16f63725e07b69b9ab014c0a1a33d5
Timofei <timofei@metabase.com>
2025-10-03 01:18:19 +0400

    WIP: fetch dashboard via fetchDashboard

--- enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/hooks/use-get-current-resource.ts
+++ enterprise/frontend/src/metabase-enterprise/embedding_iframe_sdk_setup/hooks/use-get-current-resource.ts
@@ -1,17 +1,25 @@
-import { skipToken, useGetCardQuery, useGetDashboardQuery } from "metabase/api";
+import { useAsync } from "react-use";
+
+import { skipToken, useGetCardQuery } from "metabase/api";
+import { fetchDashboard } from "metabase/dashboard/actions";
+import { getDashboardComplete } from "metabase/dashboard/selectors";
+import { useDispatch, useSelector } from "metabase/lib/redux";
 import type { SdkIframeEmbedSetupSettings } from "metabase-enterprise/embedding_iframe_sdk_setup/types";
 
 export const useGetCurrentResource = (
   settings: SdkIframeEmbedSetupSettings,
 ) => {
-  const {
-    data: dashboard,
-    isLoading: isDashboardLoading,
-    isFetching: isDashboardFetching,
-  } = useGetDashboardQuery(
-    settings.dashboardId ? { id: settings.dashboardId } : skipToken,
-    { refetchOnMountOrArgChange: true },
-  );
+  const dispatch = useDispatch();
+
+  const { loading: isDashboardLoading } = useAsync(async () => {
+    await dispatch(
+      fetchDashboard({
+        dashId: settings.dashboardId as number,
+        queryParams: {},
+      }),
+    );
+  }, [settings.dashboardId, dispatch]);
+  const dashboard = useSelector(getDashboardComplete);
 
   const {
     data: card,
@@ -25,6 +33,6 @@ export const useGetCurrentResource = (
   return {
     resource: dashboard ?? card ?? null,
     isLoading: isDashboardLoading || isCardLoading,
-    isFetching: isDashboardFetching || isCardFetching,
+    isFetching: isCardFetching,
   };
 };
