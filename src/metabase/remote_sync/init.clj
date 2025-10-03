089e8f59a39872da5625290145c82fab4b51ede4
Edward Paget <ed.paget@gmail.com>
2025-10-03 12:58:57 -0500

    Fix adding is_remote_sync hydration

---
+++ src/metabase/remote_sync/init.clj
@@ -0,0 +1,21 @@
+(ns metabase.remote-sync.init
+  (:require
+   [metabase.api.common :as api]
+   [metabase.models.interface :as mi]
+   [methodical.core :as methodical]
+   [toucan2.core :as t2]))
+
+(methodical/defmethod t2/batched-hydrate [:default :is_remote_synced]
+  "Batch hydration for whether an item is remote synced"
+  [model k items]
+  (mi/instances-with-hydrated-data items k
+                                   #(into {}
+                                          (map (juxt :id (comp api/bit->boolean :is_remote_synced))
+                                               #p (t2/select [model :id [[:and [:= :c.type [:inline "remote-synced"]]
+                                                                          [:not= :c.type nil]]
+                                                                         :is_remote_synced]]
+                                                             {:where [:in :id (map :id items)]
+                                                              :join [[:collection :c]
+                                                                     [:= :collection_id :c.id]]})))
+                                   :id
+                                   {:default false}))
