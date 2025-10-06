6e7430187faae6e3abd433d75e6608a7f412f1cb
Ryan Laurie <iethree@gmail.com>
2025-10-06 15:54:05 -0600

    make segments nice again

--- frontend/src/metabase/admin/routes.jsx
+++ frontend/src/metabase/admin/routes.jsx
@@ -6,9 +6,6 @@ import AdminApp from "metabase/admin/app/components/AdminApp";
 import { DatabaseEditApp } from "metabase/admin/databases/containers/DatabaseEditApp";
 import { DatabaseListApp } from "metabase/admin/databases/containers/DatabaseListApp";
 import { DatabasePage } from "metabase/admin/databases/containers/DatabasePage";
-import RevisionHistoryApp from "metabase/admin/datamodel/containers/RevisionHistoryApp";
-import SegmentApp from "metabase/admin/datamodel/containers/SegmentApp";
-import SegmentListApp from "metabase/admin/datamodel/containers/SegmentListApp";
 import { AdminEmbeddingApp } from "metabase/admin/embedding/containers/AdminEmbeddingApp";
 import { AdminPeopleApp } from "metabase/admin/people/containers/AdminPeopleApp";
 import { EditUserModal } from "metabase/admin/people/containers/EditUserModal";
@@ -40,7 +37,6 @@ import { ToolsApp } from "metabase/admin/tools/components/ToolsApp";
 import { EmbeddingHubAdminSettingsPage } from "metabase/embedding/embedding-hub";
 import { ModalRoute } from "metabase/hoc/ModalRoute";
 import { Route } from "metabase/hoc/Title";
-import { DataModel } from "metabase/metadata/pages/DataModel";
 import {
   PLUGIN_ADMIN_SETTINGS,
   PLUGIN_ADMIN_TOOLS,
@@ -74,42 +70,6 @@ const getRoutes = (store, CanAccessSettings, IsAdmin) => (
         <Route path=":databaseId/edit" component={DatabasePage} />
         <Route path=":databaseId" component={DatabaseEditApp}>
           {PLUGIN_DB_ROUTING.getDestinationDatabaseRoutes(IsAdmin)}
-        </Route>
-      </Route>
-      <Route path="datamodel" component={createAdminRouteGuard("data-model")}>
-        <Route title={t`Table Metadata`}>
-          <IndexRedirect to="database" />
-          <Route path="database" component={DataModel} />
-          <Route path="database/:databaseId" component={DataModel} />
-          <Route
-            path="database/:databaseId/schema/:schemaId"
-            component={DataModel}
-          />
-          <Route
-            path="database/:databaseId/schema/:schemaId/table/:tableId"
-            component={DataModel}
-          />
-          <Route
-            path="database/:databaseId/schema/:schemaId/table/:tableId/field/:fieldId"
-            component={DataModel}
-          />
-          <Route component={DataModel}>
-            <Route path="segments" component={SegmentListApp} />
-            <Route path="segment/create" component={SegmentApp} />
-            <Route path="segment/:id" component={SegmentApp} />
-            <Route
-              path="segment/:id/revisions"
-              component={RevisionHistoryApp}
-            />
-          </Route>
-          <Redirect
-            from="database/:databaseId/schema/:schemaId/table/:tableId/settings"
-            to="database/:databaseId/schema/:schemaId/table/:tableId"
-          />
-          <Redirect
-            from="database/:databaseId/schema/:schemaId/table/:tableId/field/:fieldId/:section"
-            to="database/:databaseId/schema/:schemaId/table/:tableId/field/:fieldId"
-          />
         </Route>
       </Route>
       {/* PEOPLE */}
