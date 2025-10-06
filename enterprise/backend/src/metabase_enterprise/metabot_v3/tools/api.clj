dc9cc5b94fecb9163c675f1cfe84cc163e660747
Noah Moss <noahbmoss@gmail.com>
2025-10-06 12:53:09 -0400

    fix malli recursive schema

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/api.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/api.clj
@@ -598,17 +598,20 @@
                                :with_metric_default_temporal_breakout :with-default-temporal-breakout?})}]])
 
 (mr/def ::table-result
-  [:map
-   [:id :int]
-   [:type [:enum :model :table]]
-   [:name :string]
-   [:display_name :string]
-   [:database_id :int]
-   [:database_schema {:optional true} [:maybe :string]] ; Schema name, if applicable
-   [:fields ::columns]
-   [:related_tables {:optional true} [:sequential ::table-result]]
-   [:description {:optional true} [:maybe :string]]
-   [:metrics {:optional true} [:sequential ::basic-metric]]])
+  [:schema
+   {:registry {::table-result
+               [:map
+                [:id :int]
+                [:type [:enum :model :table]]
+                [:name :string]
+                [:display_name :string]
+                [:database_id :int]
+                [:database_schema {:optional true} [:maybe :string]] ; Schema name, if applicable
+                [:fields ::columns]
+                [:related_tables {:optional true} [:sequential [:ref ::table-result]]]
+                [:description {:optional true} [:maybe :string]]
+                [:metrics {:optional true} [:sequential ::basic-metric]]]}}
+   ::table-result])
 
 (mr/def ::get-table-details-result
   [:or
