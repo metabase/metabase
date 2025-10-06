095d451fa51ec07618b3852394bce1ef794cfc4e
Romeo Van Snick <romeo@romeovansnick.be>
2025-10-06 16:00:25 +1100

    Add e2e test for empty data selector

--- e2e/test/scenarios/data-studio/transforms.cy.spec.ts
+++ e2e/test/scenarios/data-studio/transforms.cy.spec.ts
@@ -2317,8 +2317,45 @@ describe("scenarios > admin > transforms > runs", () => {
   });
 });
 
+H.describeWithSnowplowEE("scenarios > admin > transforms", () => {
+  beforeEach(() => {
+    H.restore();
+    H.resetSnowplow();
+    cy.signInAsAdmin();
+    H.activateToken("bleeding-edge");
+  });
+
+  afterEach(() => {
+    H.expectNoBadSnowplowEvents();
+  });
+
+  it("should not pick the only database when it is disabled in SQL editor", () => {
+    cy.log("create a new transform");
+    visitTransformListPage();
+    getTransformListPage().button("Create a transform").click();
+    H.popover().findByText("SQL query").click();
+
+    cy.findByTestId("gui-builder-data")
+      .findByText("Select a database")
+      .should("be.visible");
+  });
+
+  it("should not pick the only database when it is disabled in Python editor", () => {
+    cy.log("create a new transform");
+    visitTransformListPage();
+    getTransformListPage().button("Create a transform").click();
+    H.popover().findByText("Python script").click();
+
+    getPythonDataPicker().findByText("Select a database").should("be.visible");
+  });
+});
+
 function getTransformsNavLink() {
   return H.DataStudio.nav().findByRole("link", { name: "Transforms" });
+}
+
+function getTransformListPage() {
+  return cy.findByTestId("transform-list-page");
 }
 
 function getRunsNavLink() {
