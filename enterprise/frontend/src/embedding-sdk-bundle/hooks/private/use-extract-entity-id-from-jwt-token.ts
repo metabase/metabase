e5171c44e0c664d1e1afdcded1b21a99c9f473b2
Timofei <timofei@metabase.com>
2025-10-07 16:22:00 +0400

    Add the token prop/attribute #6

--- enterprise/frontend/src/embedding-sdk-bundle/hooks/private/use-extract-entity-id-from-jwt-token.ts
+++ enterprise/frontend/src/embedding-sdk-bundle/hooks/private/use-extract-entity-id-from-jwt-token.ts
@@ -1,28 +1,52 @@
 import { useMemo } from "react";
+import { t } from "ttag";
 
 import type { SdkEntityToken } from "embedding-sdk-bundle/types";
 import { extractEntityIdFromJwtToken, isJWT } from "metabase/lib/utils";
 
 export const useExtractEntityIdFromJwtToken = <TEntityId>({
+  isStaticEmbedding,
   entityId,
   token,
 }: {
+  isStaticEmbedding: boolean;
   entityId: TEntityId | undefined;
   token: SdkEntityToken | undefined;
-}) => {
+}): {
+  entityId: TEntityId | null;
+  token: SdkEntityToken | null;
+  tokenError?: string;
+} => {
   return useMemo<{
     entityId: TEntityId | null;
     token: SdkEntityToken | null;
   }>(() => {
-    if (token) {
-      return { entityId: extractEntityIdFromJwtToken(token), token };
-    }
-
     if (isJWT(entityId)) {
       return {
-        entityId: extractEntityIdFromJwtToken(entityId),
-        token: entityId,
+        entityId: null,
+        token: null,
+        tokenError: t`Passed id cannot be a JWT token.`,
       };
+    }
+
+    if (token) {
+      if (!isStaticEmbedding) {
+        return {
+          entityId: null,
+          token: null,
+          tokenError: t`Passing a token is only allowed for anonymous embedding.`,
+        };
+      }
+
+      if (!isJWT(token)) {
+        return {
+          entityId: null,
+          token: null,
+          tokenError: t`Passed token is not a valid JWT token.`,
+        };
+      }
+
+      return { entityId: extractEntityIdFromJwtToken(token), token };
     }
 
     if (entityId) {
@@ -36,5 +60,5 @@ export const useExtractEntityIdFromJwtToken = <TEntityId>({
       entityId: null,
       token: null,
     };
-  }, [entityId, token]);
+  }, [entityId, isStaticEmbedding, token]);
 };
