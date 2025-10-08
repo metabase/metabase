4eaa60ba241ee9591e63ae6aa3a05d3d6ee23a34
Ryan Laurie <iethree@gmail.com>
2025-10-08 17:30:36 -0600

    better debouncery

--- frontend/src/metabase/bench/components/utils.ts
+++ frontend/src/metabase/bench/components/utils.ts
@@ -1,22 +1,25 @@
-import { useCallback, useLayoutEffect, useState} from "react";
+import { useCallback, useLayoutEffect, useState } from "react";
 import _ from "underscore";
 
 /**
  * react-resizable-panels sizes are in percentages,
  * but we want to be able to specify some sizes in fixed pixel terms
  */
-export const useAbsoluteSize = ({ groupId }: {  groupId: string }) => {
+export const useAbsoluteSize = ({ groupId }: { groupId: string }) => {
   const [width, setWidth] = useState(1200);
 
   useLayoutEffect(() => {
-    const panelGroup = document.querySelector(`[data-panel-group-id="${groupId}"]`) as HTMLElement | null;
+    const panelGroup = document.querySelector(
+      `[data-panel-group-id="${groupId}"]`,
+    ) as HTMLElement | null;
     if (!panelGroup) {
       return;
     }
+
+    const debouncedSetWidth = _.debounce(setWidth, 250);
+
     const observer = new ResizeObserver(() => {
       const { width } = panelGroup.getBoundingClientRect();
-
-      const debouncedSetWidth = _.debounce(setWidth, 250);
       debouncedSetWidth(width);
     });
 
@@ -30,4 +33,4 @@ export const useAbsoluteSize = ({ groupId }: {  groupId: string }) => {
   const getSizeFn = useCallback((px: number) => (px / width) * 100, [width]);
 
   return getSizeFn;
-}
+};
