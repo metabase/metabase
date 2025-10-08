f3375d4c73774bb41c7fed3828298437a85fa0e1
Ryan Laurie <iethree@gmail.com>
2025-10-08 17:15:47 -0600

    pull in glossary

--- frontend/src/metabase/reference/glossary/GlossaryContainer.tsx
+++ frontend/src/metabase/reference/glossary/GlossaryContainer.tsx
@@ -1,4 +1,3 @@
-import cx from "classnames";
 import { t } from "ttag";
 
 import {
@@ -7,10 +6,7 @@ import {
   useListGlossaryQuery,
   useUpdateGlossaryMutation,
 } from "metabase/api";
-import SidebarLayout from "metabase/common/components/SidebarLayout";
-import CS from "metabase/css/core/index.css";
-import BaseSidebar from "metabase/reference/guide/BaseSidebar";
-import { Card, Group, Stack, Text } from "metabase/ui";
+import { Card, Stack, Text } from "metabase/ui";
 
 import S from "./Glossary.module.css";
 import { GlossaryTable } from "./GlossaryTable";
@@ -22,44 +18,29 @@ export function GlossaryContainer() {
   const [deleteGlossary] = useDeleteGlossaryMutation();
 
   return (
-    <SidebarLayout
-      className={cx(CS.flexFull, CS.relative)}
-      sidebar={<BaseSidebar />}
-    >
-      <Stack w="100%" h="100%">
-        <Group
-          align="center"
-          wrap="nowrap"
-          px="2.625rem"
-          h="3rem"
-          mt="2rem"
-          justify="space-between"
-        >
-          <Text fw="bold" fz="1.5rem">{t`Glossary`}</Text>
-        </Group>
-        <Stack
-          w="100%"
-          gap={0}
-          m={0}
-          className={cx(CS.wrapper, CS.wrapperTrim)}
-        >
-          <Card px="lg" pb="sm" withBorder shadow="none" className={S.card}>
-            <GlossaryTable
-              className={S.table}
-              glossary={glossary}
-              onCreate={async (term, definition) => {
-                await createGlossary({ term, definition });
-              }}
-              onEdit={async (id, term, definition) => {
-                await updateGlossary({ id, term, definition });
-              }}
-              onDelete={async (id) => {
-                await deleteGlossary({ id });
-              }}
-            />
-          </Card>
-        </Stack>
+    <Stack w="100%" h="100%" align="center" py="xl" px="lg" maw="60rem" mx="auto">
+      <Text fw="bold" fz="1.5rem" mb="lg" component="div" w="100%">{t`Glossary`}</Text>
+      <Stack
+        w="100%"
+        gap={0}
+        m={0}
+      >
+        <Card pb="sm" withBorder shadow="none">
+          <GlossaryTable
+            className={S.table}
+            glossary={glossary}
+            onCreate={async (term, definition) => {
+              await createGlossary({ term, definition });
+            }}
+            onEdit={async (id, term, definition) => {
+              await updateGlossary({ id, term, definition });
+            }}
+            onDelete={async (id) => {
+              await deleteGlossary({ id });
+            }}
+          />
+        </Card>
       </Stack>
-    </SidebarLayout>
+    </Stack>
   );
 }
