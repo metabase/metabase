fb34fc165464e4715421f0cfc7eea5feb71c8805
Timothy Dean <timothy@timothydean.dev>
2025-10-08 11:00:30 -0600

    babashka import: manually fix a few issues, but the code organization is pretty bad

--- enterprise/backend/src/metabase_enterprise/representations/v0/collection.clj
+++ enterprise/backend/src/metabase_enterprise/representations/v0/collection.clj
@@ -101,37 +101,16 @@
   [representation ref-index]
   (let [collection-data (import/yaml->toucan representation ref-index)
         entity-id (:entity_id collection-data)
-        location (:location collection-data)
-        is-root? (= location "/")]
-
-    (if is-root?
-      (let [collection-name (:name collection-data)
-            existing-collections (t2/select :model/Collection :name collection-name :location "/")]
-
-        (when (> (count existing-collections) 1)
-          (throw (ex-info (str "Multiple collections found with name: " collection-name)
-                          {:collection-name collection-name
-                           :count (count existing-collections)})))
-
-        (when (= (count existing-collections) 1)
-          (let [existing (first existing-collections)
-                archived-name (str collection-name " (archived)")]
-            (log/info "Renaming existing collection" collection-name "to" archived-name)
-            (t2/update! :model/Collection (:id existing) {:name archived-name})))
-
-        (log/info "Creating new root collection" (:name collection-data))
-        (t2/insert-returning-instance! :model/Collection collection-data))
-
-      (let [existing (when entity-id
-                       (t2/select-one :model/Collection :entity_id entity-id))]
-        (if existing
-          (do
-            (log/info "Updating existing nested collection" (:name collection-data) "with ref" (:ref representation))
-            (t2/update! :model/Collection (:id existing) (dissoc collection-data :entity_id))
-            (t2/select-one :model/Collection :id (:id existing)))
-          (do
-            (log/info "Creating new nested collection" (:name collection-data))
-            (t2/insert-returning-instance! :model/Collection collection-data)))))))
+        existing (when entity-id
+                   (t2/select-one :model/Collection :entity_id entity-id))]
+    (if existing
+      (do
+        (log/info "Updating existing collection" (:name collection-data) "with ref" (:ref representation))
+        (t2/update! :model/Collection (:id existing) (dissoc collection-data :entity_id))
+        (t2/select-one :model/Collection :id (:id existing)))
+      (do
+        (log/info "Creating new collection" (:name collection-data))
+        (t2/insert-returning-instance! :model/Collection collection-data)))))
 
 (defmethod export/export-entity :model/Collection [collection]
   (export collection))
