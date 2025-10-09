af0b621eeb33ec48ecd1a6bf93c3a5bbdd821bbe
Oleksandr Solovyov <alexander@solovyov.net>
2025-10-09 14:22:14 +0300

    [serdes] do not say "Loading ..." if entity is not being loaded (#64461)
    When investigating a separate issue I've noticed that we're logging "Loading ..." even for entities that are not actually being loaded. That's because we were printing this line when something is a dependency - even if it exists in the database and is not provided in the export.
    
    This changes the approach somewhat, we will still have some indication we need some entity, but only at DEBUG level, and instead the "Loading ..." will appear only if we're genuinely about to read something from an export and put it in the database.
    
    Not only it's easier to understand what's going on, it's going to be way less noisy.

--- enterprise/backend/test/metabase_enterprise/serialization/api_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/api_test.clj
@@ -239,7 +239,7 @@
                                                     {:request-options {:headers {"content-type" "multipart/form-data"}}}
                                                     {:file ba})]
                       (testing "We get our data items back"
-                        (is (= #{"Collection" "Dashboard" "Card" "Database" "TransformTag" "TransformJob"}
+                        (is (= #{"Collection" "Dashboard" "Card" "TransformTag" "TransformJob"}
                                (log-types (line-seq (io/reader (io/input-stream res)))))))
                       (testing "And they hit the db"
                         (is (= (:name dash) (t2/select-one-fn :name :model/Dashboard :entity_id (:entity_id dash))))
@@ -272,16 +272,7 @@
                                                         {:request-options {:headers {"content-type" "multipart/form-data"}}}
                                                         {:file ba}))
                             log (slurp (io/input-stream res))]
-                        (testing "3 header lines, then cards+database+collection, then the error"
-                          (is (re-find #"Failed to read file for Collection DoesNotExist" log))
-                          (is (re-find #"Cannot find file" log)) ;; underlying error
-                          (is (= {:deps-chain #{[{:id "**ID**", :model "Card"}]},
-                                  :error      :metabase-enterprise.serialization.v2.load/not-found,
-                                  :model      "Collection",
-                                  :path       [{:id "DoesNotExist", :model "Collection"}],
-                                  :local-id   nil
-                                  :table      :collection}
-                                 (extract-and-sanitize-exception-map log))))
+                        (is (re-find #"Failed to read file \{:path \"Collection DoesNotExist\"}" log))
                         (testing "Snowplow event about error was sent"
                           (is (=? {"success"       false
                                    "event"         "serialization"
@@ -290,7 +281,7 @@
                                    "duration_ms"   int?
                                    "count"         0
                                    "error_count"   0
-                                   "error_message" #"(?s)Failed to read file for Collection DoesNotExist.*"}
+                                   "error_message" "Failed to read file {:path \"Collection DoesNotExist\"}"}
                                   (-> (snowplow-test/pop-event-data-and-user-id!) last :data))))))
 
                     (testing "Skipping errors /api/ee/serialization/import"
@@ -300,9 +291,9 @@
                                                       :continue_on_error true)
                             log (slurp (io/input-stream res))]
                         (testing "3 header lines, then card+database+coll, error, then dashboard+coll"
-                          (is (= #{"Dashboard" "Card" "Database" "Collection" "TransformTag" "TransformJob"}
+                          (is (= #{"Dashboard" "Card" "Collection" "TransformTag" "TransformJob"}
                                  (log-types (str/split-lines log))))
-                          (is (re-find #"Failed to read file for Collection DoesNotExist" log)))
+                          (is (re-find #"Failed to read file \{:path \"Collection DoesNotExist\"}" log)))
                         (testing "Snowplow event about error was sent"
                           (is (=? {"success"     true
                                    "event"       "serialization"
