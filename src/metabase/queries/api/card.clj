65419bb673d3ebbb466fa4783a07464d8507307e
Edward Paget <ed.paget@gmail.com>
2025-10-09 14:18:34 -0500

    Fix issue with merge

--- src/metabase/queries/api/card.clj
+++ src/metabase/queries/api/card.clj
@@ -526,7 +526,7 @@
     (query-perms/check-run-permissions-for-query query)
     ;; check that we have permissions for the collection we're trying to save this card to, if applicable.
     ;; if a `dashboard-id` is specified, check permissions on the *dashboard's* collection ID.
-    (api/create-check :model/Card {:collection_id (actual-collection-id body)})
+    (api/create-check :model/Card {:collection_id (actual-collection-id card)})
     (try
       (lib/check-card-overwrite ::no-id query)
       (catch clojure.lang.ExceptionInfo e
