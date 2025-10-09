d29fe1a14ea3e7be15ef3775c35b6b4c566fc7de
Nicola Mometto <nicola.mometto@metabase.com>
2025-10-09 15:43:41 +0100

    wip

--- enterprise/backend/src/metabase_enterprise/transforms/schema.clj
+++ enterprise/backend/src/metabase_enterprise/transforms/schema.clj
@@ -1,8 +1,32 @@
 (ns metabase-enterprise.transforms.schema
   (:require
-   [metabase.lib.schema.common :as schema.common]
    [metabase.util.malli.registry :as mr]
    [metabase.util.malli.schema :as ms]))
+
+(mr/def ::keyset-strategy
+  [:map
+   [:type [:= "keyset"]]
+   [:keyset-column :string]
+   [:table-name {:optional true} :string]]) ; for python transforms with multiple tables
+
+(mr/def ::source-strategy
+  [:multi {:dispatch :type}
+   ["keyset" ::keyset-strategy]])
+
+(mr/def ::transform-source
+  [:multi {:dispatch (comp keyword :type)}
+   [:query
+    [:map
+     [:type [:= "query"]]
+     [:query [:map [:database :int]]]
+     [:source-strategy {:optional true} ::source-strategy]]]
+   [:python
+    [:map {:closed true}
+     [:source-database {:optional true} :int]
+     [:source-tables   [:map-of :string :int]]
+     [:type [:= "python"]]
+     [:body :string]
+     [:source-strategy {:optional true} ::source-strategy]]]])
 
 (mr/def ::append-config
   [:map
@@ -15,23 +39,10 @@
      [:primary-key [:sequential :string]]
      [:update-columns {:optional true} [:sequential :string]]]) ; columns to update on conflict
 
-(mr/def ::target-strategy
+(mr/def ::target-incremental-strategy
   [:multi {:dispatch :type}
    ["append" ::append-config]
    #_["merge" ::merge-config]])
-
-(mr/def ::transform-source
-  [:multi {:dispatch (comp keyword :type)}
-   [:query
-    [:map
-     [:type [:= "query"]]
-     [:query [:map [:database :int]]]]]
-   [:python
-    [:map {:closed true}
-     [:source-database {:optional true} :int]
-     [:source-tables   [:map-of :string :int]]
-     [:type [:= "python"]]
-     [:body :string]]]])
 
 (mr/def ::transform-target
   [:map
@@ -41,4 +52,4 @@
    [:name :string]
 
    ;; only for table-incremental, TODO: specify with a multi spec
-   [:target-strategy {:optional true} ::target-strategy]])
+   [:target-incremental-strategy {:optional true} ::target-incremental-strategy]])
