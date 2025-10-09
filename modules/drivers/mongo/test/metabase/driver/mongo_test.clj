9b1d0254c6a72685b4a58e1b6eaa19356a93de8d
Tamás Benkő <tamas@metabase.com>
2025-10-09 09:15:05 +0300

    Move test to ....driver.mongo.query-processor-test

--- modules/drivers/mongo/test/metabase/driver/mongo_test.clj
+++ modules/drivers/mongo/test/metabase/driver/mongo_test.clj
@@ -15,12 +15,9 @@
    [metabase.driver.settings :as driver.settings]
    [metabase.driver.util :as driver.u]
    [metabase.lib.core :as lib]
-   [metabase.lib.metadata :as lib.metadata]
-   [metabase.lib.test-util :as lib.tu]
    [metabase.lib.util.match :as lib.util.match]
    [metabase.query-processor :as qp]
    [metabase.query-processor.compile :as qp.compile]
-   [metabase.query-processor.pivot :as qp.pivot]
    [metabase.query-processor.test-util :as qp.test-util]
    [metabase.sync.core :as sync]
    [metabase.test :as mt]
@@ -1177,57 +1174,3 @@
       :type/MongoBSONID        "objectId"
       :type/MongoBinData       "binData"
       :type/IPAddress          "string")))
-
-(deftest ^:parallel pivot-query-based-on-native-card-test
-  (mt/test-driver :mongo
-    (testing "Pivot queries based on a native Mongo card return the right number of columns (#64124)"
-      (let [native-query (json/encode [{:$match {:_id 1}}
-                                       {:$project {:product_id :$product_id, :subtotal :$subtotal}}])
-            mp (lib.tu/mock-metadata-provider
-                (mt/metadata-provider)
-                {:cards [{:id              1
-                          :name            "Orders native mongo"
-                          :dataset-query   {:type     :native
-                                            :native   {:collection "orders"
-                                                       :query      native-query}
-                                            :database (mt/id)}
-                          :result_metadata [{:name         "product_id"
-                                             :base_type    :type/Integer
-                                             :display_name "product_id"}
-                                            {:name         "subtotal"
-                                             :base_type    :type/Float
-                                             :display_name "subtotal"}]}]})
-            breakout-by-column-name (fn [query col-name]
-                                      (lib/breakout query (m/find-first (comp #{col-name} :name)
-                                                                        (lib/breakoutable-columns query))))
-            query (-> (lib/query mp (lib.metadata/card mp 1))
-                      (lib/aggregate (lib/count))
-                      (breakout-by-column-name "product_id")
-                      (breakout-by-column-name "subtotal"))
-            pivot-query (assoc query
-                               :pivot_rows         [0]
-                               :pivot_cols         [1]
-                               :show_row_totals    true
-                               :show_column_totals true
-                               :info               {:context :ad-hoc})]
-        (is (=? {:data
-                 {:cols
-                  [{:lib/desired-column-alias "product_id"
-                    :field_ref                [:field "product_id" {:base-type :type/Integer}]
-                    :base_type                :type/Integer
-                    :effective_type           :type/Integer}
-                   {:lib/desired-column-alias "subtotal"
-                    :field_ref                [:field "subtotal" {:base-type :type/Float}]
-                    :base_type                :type/Float
-                    :effective_type           :type/Float}
-                   {:lib/desired-column-alias "pivot-grouping"
-                    :field_ref                [:expression "pivot-grouping"]
-                    :base_type                :type/Integer
-                    :effective_type           :type/Integer}
-                   {:lib/desired-column-alias "count"
-                    :field_ref                [:aggregation 0]
-                    :base_type                :type/Integer
-                    :semantic_type            :type/Quantity
-                    :effective_type           :type/Integer}]
-                  :rows [[14 37.65 0 1] [nil 37.65 1 1] [14 nil 2 1] [nil nil 3 1]]}}
-                (qp.pivot/run-pivot-query pivot-query)))))))
