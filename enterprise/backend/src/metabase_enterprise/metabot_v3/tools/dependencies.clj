1cf52df258b91ecef6120445a3932f4fac87db60
Noah Moss <noahbmoss@gmail.com>
2025-10-09 17:24:49 -0400

    update return format

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/dependencies.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/dependencies.clj
@@ -1,16 +1,44 @@
 (ns metabase-enterprise.metabot-v3.tools.dependencies
   (:require
-   [metabase-enterprise.dependencies.api :as dependencies.api]
-   [metabase-enterprise.metabot-v3.tools.util :as metabot-v3.tools.u]))
+   [metabase-enterprise.dependencies.core :as dependencies]
+   [metabase-enterprise.metabot-v3.tools.util :as metabot-v3.tools.u]
+   [metabase.lib-be.metadata.jvm :as lib-be.metadata.jvm]
+   [metabase.lib.metadata :as lib.metadata]
+   [metabase.util :as u]
+   [toucan2.core :as t2]))
+
+(defn- format-broken-transforms
+  "Format the breakages into a response suitable for Metabot."
+  [{:keys [transform]}]
+  (let [broken-transform-ids (keys transform)
+        broken-transforms    (when (seq broken-transform-ids)
+                               (t2/select [:model/Transform :id :name] :id [:in broken-transform-ids]))
+        transforms-by-id     (u/index-by :id broken-transforms)]
+    {:success        (empty? broken-transform-ids)
+     :bad_transforms (reduce
+                      (fn [acc [transform-id errors]]
+                        (let [transform (get transforms-by-id transform-id)]
+                          (conj acc {:transform transform :errors errors})))
+                      []
+                      transform)}))
 
 (defn check-transform-dependencies
-  "Check a proposed edit to a transform, and return the card, transform, etc. IDs for things that will break.
+  "Check a proposed edit to a SQL transform, and return transforms that will break.
   Takes a map with :id (required), :source (optional), and :target (optional) keys."
-  [args]
+  [{:keys [id source]}]
   (try
-    {:structured_output
-     (select-keys
-      (dependencies.api/check-transform-dependencies args)
-      [:success :bad_transforms])}
+    (let [result (if (= (keyword (:type source))
+                        :query)
+                   (let [database-id   (-> source :query :database)
+                         base-provider (lib-be.metadata.jvm/application-database-metadata-provider database-id)
+                         original      (lib.metadata/transform base-provider id)
+                         transform     (cond-> original
+                                         source (assoc :source source))
+                         edits         {:transform [transform]}
+                         breakages     (dependencies/errors-from-proposed-edits base-provider edits)]
+                     (format-broken-transforms breakages))
+                   ;; If this is a non-SQL query, we don't do any checks yet, so just return success
+                   {:success true :bad_transforms []})]
+      {:structured_output result})
     (catch Exception e
       (metabot-v3.tools.u/handle-agent-error e))))
