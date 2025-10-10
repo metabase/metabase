a597aaba45f5f01f2bd01ce8394ce7e17095d9cc
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-10-10 14:15:13 +0700

    Add a dropdown

--- frontend/src/metabase/embedding/components/ArchiveButton.tsx
+++ frontend/src/metabase/embedding/components/ArchiveButton.tsx
@@ -1,8 +1,9 @@
+import { useDisclosure } from "@mantine/hooks";
 import { t } from "ttag";
 
 import { useGetCollectionQuery } from "metabase/api";
 import { canArchiveItem } from "metabase/collections/utils";
-import { Button, Icon, Tooltip } from "metabase/ui";
+import { Button, Icon, Menu, Tooltip } from "metabase/ui";
 import type { CollectionItem } from "metabase-types/api";
 
 import S from "./ArchiveButton.module.css";
@@ -15,18 +16,38 @@ export function ArchiveButton({ item }: ArchiveButtonProps) {
     id: item.collection_id ?? "root",
   });
   const canArchive = collection ? canArchiveItem(item, collection) : false;
+  const [dropdownOpened, dropdownActions] = useDisclosure();
 
   if (!canArchive) {
     return null;
   }
+
   return (
-    <Tooltip label={t`Move to trash`}>
-      <Button
-        leftSection={<Icon name="archive" className={S.archiveIcon} />}
-        variant="inverse"
-        py="sm"
-        px="md"
-      />
-    </Tooltip>
+    <Menu opened={dropdownOpened} onClose={() => dropdownActions.close()}>
+      <Tooltip
+        label={t`Move to trash`}
+        opened={dropdownOpened ? false : undefined}
+        withArrow
+      >
+        <Menu.Target>
+          <Button
+            leftSection={<Icon name="archive" className={S.archiveIcon} />}
+            variant="inverse"
+            py="sm"
+            px="md"
+            onClick={dropdownActions.toggle}
+          />
+        </Menu.Target>
+      </Tooltip>
+      <Menu.Dropdown>
+        <Menu.Item>
+          <Button
+            variant="filled"
+            color="danger"
+            fullWidth
+          >{t`Move to trash`}</Button>
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
   );
 }
