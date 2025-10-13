0ffe27d96c7229c41fcffc81875a35695a8e320b
Romeo Van Snick <romeo@romeovansnick.be>
2025-10-13 16:09:26 +1100

    Fix useTestPythonTransform hook

--- enterprise/frontend/src/metabase-enterprise/transforms-python/components/PythonTransformEditor/hooks.ts
+++ enterprise/frontend/src/metabase-enterprise/transforms-python/components/PythonTransformEditor/hooks.ts
@@ -1,23 +1,16 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { t } from "ttag";
 
 import { getErrorMessage } from "metabase/api/utils";
 import { useExecutePythonMutation } from "metabase-enterprise/api/transform-python";
-import type { ExecutePythonTransformResponse } from "metabase-types/api";
+import type { TestPythonTransformResponse } from "metabase-types/api";
 
 import type { PythonTransformSourceDraft } from "./PythonTransformEditor";
-
-export type ExecutionResult = {
-  output?: string;
-  stdout?: string;
-  stderr?: string;
-  error?: string;
-};
 
 type TestPythonScriptState = {
   isRunning: boolean;
   isDirty: boolean;
-  executionResult: ExecutionResult | null;
+  executionResult: TestPythonTransformResponse;
   run: () => void;
   cancel: () => void;
 };
@@ -25,11 +18,11 @@ type TestPythonScriptState = {
 export function useTestPythonTransform(
   source: PythonTransformSourceDraft,
 ): TestPythonScriptState {
-  const [executePython, { isLoading: isRunning, originalArgs }] =
-    useExecutePythonMutation();
+  const [
+    executePython,
+    { data = null, error, isLoading: isRunning, originalArgs },
+  ] = useExecutePythonMutation();
   const abort = useRef<(() => void) | null>(null);
-  const [executionResult, setData] =
-    useState<ExecutePythonTransformResponse | null>(null);
 
   const isDirty = originalArgs?.code !== source.body;
 
@@ -37,30 +30,22 @@ export function useTestPythonTransform(
     if (source["source-database"] === undefined) {
       return null;
     }
+
     const request = executePython({
       code: source.body,
-      tables: source["source-tables"],
+      source_tables: source["source-tables"],
     });
+
     abort.current = () => request.abort();
-
-    try {
-      const data = await request.unwrap();
-      setData(data);
-    } catch (error) {
-      if (typeof error === "object" && error !== null) {
-        if ("name" in error && error.name === "AbortError") {
-          setData({ error: t`Python script execution was canceled` });
-          return;
-        }
-      }
-
-      const errorMessage = getErrorMessage(error, t`An unknown error occurred`);
-      setData({ error: errorMessage });
-    }
   };
 
   const cancel = () => {
     abort.current?.();
+  };
+
+  const executionResult = {
+    ...data,
+    error: getError(error) ?? data?.error,
   };
 
   return {
@@ -69,5 +54,20 @@ export function useTestPythonTransform(
     cancel,
     run,
     executionResult,
+  };
+}
+
+function getError(error: unknown) {
+  if (!error) {
+    return null;
+  }
+
+  if (typeof error === "object" && error !== null) {
+    if ("name" in error && error.name === "AbortError") {
+      return { message: t`Python script execution was canceled` };
+    }
+  }
+  return {
+    message: getErrorMessage(error, t`An unknown error occurred`),
   };
 }
