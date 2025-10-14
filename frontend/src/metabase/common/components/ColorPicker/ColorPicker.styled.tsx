a2d625b9c865cfe7b8d38de3d0d61c1ac98e2157
Uladzimir Havenchyk <uladzimir@metabase.com>
2025-10-14 14:43:39 +0300

    deps: replace react-color with react-colorful - lighter alternative

--- frontend/src/metabase/common/components/ColorPicker/ColorPicker.styled.tsx
+++ frontend/src/metabase/common/components/ColorPicker/ColorPicker.styled.tsx
@@ -9,63 +9,33 @@ export const ContentContainer = styled.div`
   padding: 1rem;
 `;
 
-// react-css inserts inline styles, but it's forbidden with our CSP headers
-// to fix this we copy relevant styles here
-// https://github.com/casesandberg/react-color/blob/v2.18.1/src/components/common/Hue.js#L78
-// https://github.com/casesandberg/react-color/blob/v2.18.1/src/components/common/Saturation.js#L94
-// eslint-disable-next-line no-color-literals
+// Styles for react-colorful components
 export const ControlsRoot = styled.div`
-  .hue-horizontal {
-    background: linear-gradient(
-      to right,
-      #f00 0%,
-      #ff0 17%,
-      #0f0 33%,
-      #0ff 50%,
-      #00f 67%,
-      #f0f 83%,
-      #f00 100%
-    );
+  .react-colorful {
+    width: 100%;
+    height: auto;
   }
 
-  .saturation-white {
-    background: linear-gradient(to right, #fff, rgba(255, 255, 255, 0));
+  .react-colorful__saturation {
+    height: 10rem;
+    margin-bottom: 1rem;
+    border-radius: 0.25rem;
   }
 
-  .saturation-black {
-    background: linear-gradient(to top, #000, rgba(0, 0, 0, 0));
+  .react-colorful__hue {
+    height: 0.5rem;
+    border-radius: 0.25rem;
   }
-`;
 
-export const SaturationContainer = styled.div`
-  position: relative;
-  height: 10rem;
-  margin-bottom: 1rem;
-  border-radius: 0.25rem;
-  overflow: hidden;
-`;
+  .react-colorful__pointer {
+    width: 0.875rem;
+    height: 0.875rem;
+    border: 2px solid var(--mb-color-bg-white);
+    border-radius: 50%;
+  }
 
-export const HueContainer = styled.div`
-  position: relative;
-  height: 0.5rem;
-  border-radius: 0.25rem;
-  overflow: hidden;
-`;
-
-const ControlsPointer = styled.div`
-  border: 2px solid var(--mb-color-bg-white);
-  border-radius: 50%;
-  pointer-events: none;
-`;
-
-export const SaturationPointer = styled(ControlsPointer)`
-  width: 0.875rem;
-  height: 0.875rem;
-  transform: translate(-50%, -50%);
-`;
-
-export const HuePointer = styled(ControlsPointer)`
-  width: 0.625rem;
-  height: 0.625rem;
-  transform: translate(-50%, -0.0625rem);
+  .react-colorful__hue-pointer {
+    width: 0.625rem;
+    height: 0.625rem;
+  }
 `;
