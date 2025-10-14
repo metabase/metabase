07b4e235c544bb274f441cf9152fbfd7066aa4c1
Noah Moss <noahbmoss@gmail.com>
2025-10-14 12:29:20 -0400

    fix more function calls in tests

--- modules/drivers/clickhouse/src/metabase/driver/clickhouse_qp.clj
+++ modules/drivers/clickhouse/src/metabase/driver/clickhouse_qp.clj
@@ -9,7 +9,7 @@
    [metabase.driver.clickhouse-version :as clickhouse-version]
    [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
    [metabase.driver.sql.parameters.substitution :as sql.params.substitution]
-   [metabase.driver.sql.query-processor :as sql.qp :refer [add-interval-honeysql-form]]
+   [metabase.driver.sql.query-processor :as sql.qp]
    [metabase.driver.sql.util :as sql.u]
    [metabase.util :as u]
    [metabase.util.date-2 :as u.date]
@@ -273,7 +273,7 @@
   (if (some interval? args)
     (if-let [[field intervals] (u/pick-first (complement interval?) args)]
       (reduce (fn [hsql-form [_ amount unit]]
-                (add-interval-honeysql-form driver hsql-form amount unit))
+                (h2x/add-interval-honeysql-form driver hsql-form amount unit))
               (sql.qp/->honeysql driver field)
               intervals)
       (throw (ex-info "Summing intervals is not supported" {:args args})))
