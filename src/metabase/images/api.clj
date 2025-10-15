68c83a75054f116ede517cbaa40479f5324cbed8
Cam Saul <github@camsaul.com>
2025-10-15 09:31:13 +0200

    API placeholders part 2

--- src/metabase/images/api.clj
+++ src/metabase/images/api.clj
@@ -1,9 +1,10 @@
 (ns metabase.images.api
   (:require
-   [metabase.api.macros :as api.macros]))
+   [metabase.api.macros :as api.macros]
+   [metabase.lib.schema.id :as lib.schema.id]))
 
 (api.macros/defendpoint :get "/:id"
-  "Fetch a specific image (contents of an image?)"
+  "This is our endpoint for serving the contents of an image that we have stored locally somewhere."
   []
   {})
 
@@ -11,3 +12,25 @@
   "Upload an image."
   []
   {:body #_FileOutputStream nil})
+
+;; GET /api/collection/:id/items needs to return collection_image
+
+;; GET /api/user + GET /api/user/:id needs to return profile pic URL
+
+;;; TODO -- not sure bout this =(
+(api.macros/defendpoint :post "/collection/:collection-id"
+  "Add an image to a collection (create a new :model/CollectionImage"
+  []
+  {})
+
+(api.macros/defendpoint :post "/card/:card-id/snapshot"
+  "Snapshot a Card. This will create a new CollectionImage."
+  [{:keys [card-id]} :- [:map
+                         [:card-id ::lib.schema.id/card]]]
+  {})
+
+(api.macros/defendpoint :get "/card/:card-id/snapshots"
+  "List all snapshots of a Card."
+  [{:keys [card-id]} :- [:map
+                         [:card-id ::lib.schema.id/card]]]
+  {})
