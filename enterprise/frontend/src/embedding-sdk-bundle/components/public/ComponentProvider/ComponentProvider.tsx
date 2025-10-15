446696b02313f82e142612ac910cc4d6bf76d66a
Timofei <timofei@metabase.com>
2025-10-15 08:13:17 +0200

    Call `setEmbedCommonEndpoints` from an entity

--- enterprise/frontend/src/embedding-sdk-bundle/components/public/ComponentProvider/ComponentProvider.tsx
+++ enterprise/frontend/src/embedding-sdk-bundle/components/public/ComponentProvider/ComponentProvider.tsx
@@ -21,7 +21,6 @@ import { MetabaseReduxProvider } from "metabase/lib/redux";
 import { PLUGIN_ANONYMOUS_EMBEDDING } from "metabase/plugins";
 import { LocaleProvider } from "metabase/public/LocaleProvider";
 import { setOptions } from "metabase/redux/embed";
-import { setEmbedCommonEndpoints } from "metabase/services";
 import { EmotionCacheProvider } from "metabase/styled-components/components/EmotionCacheProvider";
 import { MetabotProvider } from "metabase-enterprise/metabot/context";
 
