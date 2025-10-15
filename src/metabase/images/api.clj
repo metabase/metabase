3bbe219d2c4f52e6b8327399d9e1db49b08e8228
Dan Stone <wotbrew@gmail.com>
2025-10-15 11:07:19 +0100

    Null position

--- src/metabase/images/api.clj
+++ src/metabase/images/api.clj
@@ -71,7 +71,7 @@
              :model/CollectionImage
              {:image_id            (:id image)
               :collection_id       collection-id
-              :collection_position 0}))
+              :collection_position nil}))
           (assoc image :url (format "%s/api/images/%d/contents" (system/site-url) (:id image)))))
       (finally
         (io/delete-file tempfile true)))))
