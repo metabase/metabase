aedeee0c5787ebda9255435eeb6655f864164805
Dan Stone <wotbrew@gmail.com>
2025-10-15 08:48:06 +0100

    Copy to duck!

--- src/metabase/images/api.clj
+++ src/metabase/images/api.clj
@@ -1,7 +1,11 @@
 (ns metabase.images.api
   (:require
+   [clojure.java.io :as io]
    [metabase.api.macros :as api.macros]
-   [metabase.lib.schema.id :as lib.schema.id]))
+   [metabase.lib.schema.id :as lib.schema.id]
+   [metabase.util.log :as log]
+   [metabase.util.malli :as mu]
+   [metabase.util.malli.schema :as ms]))
 
 (api.macros/defendpoint :get "/:id"
   "This is our endpoint for serving the contents of an image that we have stored locally somewhere."
@@ -10,8 +14,23 @@
 
 (api.macros/defendpoint :post "/"
   "Upload an image."
-  []
-  {:body #_FileOutputStream nil})
+  {:multipart true}
+  [_
+   _
+   _
+   {{:strs [file]} :multipart-params}
+   :- [:map
+       [:multipart-params
+        [:map
+         ["file" (mu/with ms/File {:description "image data"})]]]]]
+  (let [{:keys [tempfile size]} file]
+    (try
+      (log/infof "Got a cool file with %d bytes" size)
+      (io/copy tempfile (io/file "/tmp/hack2025/duck.png"))
+      {:status 200
+       :body   {:message "cool"}}
+      (finally
+        (io/delete-file tempfile true)))))
 
 ;; GET /api/collection/:id/items needs to return collection_image
 
