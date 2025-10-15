d618869ac890da0aa6375560e8fb9be3d3c9e47d
Eric Normand <ericwnormand@gmail.com>
2025-10-15 08:55:14 +0200

    Revert "Merge remote-tracking branch 'origin/representations' into public-documents"
    This reverts commit 9834c047f3b78d79f4ff93b6ee403e318fe265a2, reversing
    changes made to aeb74fd78fd6b552c3a36e7a391301389db5d68b.

--- enterprise/backend/src/metabase_enterprise/representations/init.clj
+++
@@ -1,5 +0,0 @@
-(ns metabase-enterprise.representations.init
-  "Initialization namespace for the representations module."
-  (:require
-   [metabase-enterprise.representations.core]
-   [metabase-enterprise.representations.v0.init]))
