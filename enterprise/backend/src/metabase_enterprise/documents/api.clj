10ceb67315c6471c249e916a9d66d4aadb63bc02
Edward Paget <ed.paget@gmail.com>
2025-10-15 10:18:52 +0200

    Fix webhook errors

--- enterprise/backend/src/metabase_enterprise/documents/api.clj
+++ enterprise/backend/src/metabase_enterprise/documents/api.clj
@@ -2,9 +2,11 @@
   "`/api/ee/document/` routes"
   (:require
    [metabase-enterprise.documents.api.document]
+   [metabase-enterprise.documents.api.webhook]
    [metabase.api.util.handlers :as handlers]))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/document/` routes."
   (handlers/routes
+   metabase-enterprise.documents.api.webhook/routes
    metabase-enterprise.documents.api.document/routes))
