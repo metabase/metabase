1be72bf25e148bef4bcb07397fed3e15e673aec0
Ryan Laurie <iethree@gmail.com>
2025-10-15 11:43:34 +0200

    fix api

--- frontend/src/metabase/api/image.ts
+++ frontend/src/metabase/api/image.ts
@@ -12,24 +12,28 @@ export const imageApi = Api.injectEndpoints({
     uploadImage: builder.mutation<{ image_url: string }, UploadImageRequest>({
       query: ({ file, collectionId, userId }) => {
         const bodyFormData = new FormData();
+        console.log(file);
+        bodyFormData.append("name", file.name);
+        bodyFormData.append("type", file.type);
         bodyFormData.append("file", file);
         const queryParams = userId
-          ? `?user_id=${userId}`
-          : `?collection_id=${collectionId}`;
+          ? `?user-id=${userId}`
+          : `?collection-id=${collectionId}`;
         return {
           url: `/api/images${queryParams}`,
           method: "POST",
           headers: {
             "Content-Type": "multipart/form-data;",
           },
-          body: bodyFormData,
+          body: { formData: bodyFormData },
           formData: true,
+          fetch: true,
         };
       },
     }),
     getImageData: builder.query<{ name: string, image_url: string }, { id: number }>({
       query: ({ id }) => ({
-        url: `/api/image-data/${id}`,
+        url: `/api/images/${id}`,
       }),
     }),
   }),
