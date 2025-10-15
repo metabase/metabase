4c57634307bc0c331eca17fc005b5f472ce1561e
Chris Truter <chris@metabase.com>
2025-10-15 11:22:32 +0200

    Track change in shape using extension instead of transformer

--- src/metabase/documents/api/webhook.clj
+++ src/metabase/documents/api/webhook.clj
@@ -12,18 +12,15 @@
   ;; for now, we let the FE initialize the document with a manual save
   )
 
-(defn- change-event [user-id {id :documentName, document :document}]
+(defn- change-event [user-id {id :documentName, :keys [ydoc document]}]
   ;; work whether we have the new transformer to pass through the base64 ydoc too, or not
-  (let [id (parse-long id)
-        {:keys [ydoc document]} (if (:ydoc document)
-                                  document
-                                  {:ydoc     nil
-                                   :document document})]
+  (let [id (parse-long id)]
     (t2/update! :model/Document id {:document document
                                     :ydoc     ydoc})
-    (events/publish-event! :event/document-update
-                           {:object  document
-                            :user-id user-id})))
+    ;; not needed for demo
+    #_(events/publish-event! :event/document-update
+                             {:object  (t2/select :model/Document id)
+                              :user-id user-id})))
 
 (defn- disconnect-event [payload]
   ;; nothing to do here
