17c4c3339646aafb17b823d47c285c4bb5d31cf6
Brad Anderson <brad@metabase.com>
2025-10-15 15:18:28 -0400

    Fix incorrectly ported transform/job params

--- enterprise/frontend/src/metabase-enterprise/transforms/TransformLayouts.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/TransformLayouts.tsx
@@ -1,31 +1,40 @@
 import type { Location } from "history";
-import type { ReactNode } from "react";
+import type { PropsWithChildren } from "react";
 
 import { BenchLayout } from "metabase/bench/components/BenchLayout";
 
 import { JobList } from "./pages/JobListPage/JobList";
+import { getParsedParams as getJobListParams } from "./pages/JobListPage/utils";
 import { TransformList } from "./pages/TransformListPage/TransformList";
+import { getParsedParams as getTransformListParams } from "./pages/TransformListPage/utils";
 
 type TransformLayoutPropsParams = {
   transformId?: string;
   jobId?: string;
-  location?: string;
 };
 
-type TransformLayoutProps = {
+type TransformLayoutProps = PropsWithChildren<{
   params: TransformLayoutPropsParams;
   location: Location;
-  children?: ReactNode;
-};
+}>;
 
 export function TransformLayout({
   children,
   params,
   location,
 }: TransformLayoutProps) {
+  const parsedParams = getTransformListParams(location);
+  const selectedId = params.transformId ? +params.transformId : undefined;
+
   return (
     <BenchLayout
-      nav={<TransformList params={params} location={location} />}
+      nav={
+        <TransformList
+          params={parsedParams}
+          location={location}
+          selectedId={selectedId}
+        />
+      }
       name="transform"
     >
       {children}
@@ -38,9 +47,12 @@ export function JobsLayout({
   params,
   location,
 }: TransformLayoutProps) {
+  const parsedParams = getJobListParams(location);
+  const selectedId = params.jobId ? +params.jobId : undefined;
+
   return (
     <BenchLayout
-      nav={<JobList params={params} location={location} />}
+      nav={<JobList params={parsedParams} selectedId={selectedId} />}
       name="job"
     >
       {children}
