b7408de5f4d4ca1110902fd127fd451ebe665abe
Brad Anderson <brad@metabase.com>
2025-10-15 12:30:33 -0400

    Ports `initiallyExpanded` and `rightSection` Tree additions from `source-of-truth` branch and uses them in segments tree view

--- frontend/src/metabase/common/components/tree/types.ts
+++ frontend/src/metabase/common/components/tree/types.ts
@@ -2,11 +2,12 @@ import type * as React from "react";
 
 import type { IconName, IconProps } from "metabase/ui";
 
-export interface ITreeNodeItem {
+export interface ITreeNodeItem<TData = unknown> {
   id: string | number;
   name: string;
   icon: IconName | IconProps;
   children?: ITreeNodeItem[];
+  data?: TData;
 }
 
 export interface TreeNodeProps {
@@ -15,6 +16,7 @@ export interface TreeNodeProps {
   hasChildren: boolean;
   isExpanded: boolean;
   isSelected: boolean;
+  rightSection?: (item: ITreeNodeItem) => React.ReactNode;
   onSelect?: () => void;
   onToggleExpand: () => void;
   className?: string;
