9fcb29d5aaab01a57a7ab4e902437ae950a1d991
Cam Saul <github@camsaul.com>
2025-10-15 14:52:50 +0200

    Settings for s3

--- src/metabase/images/init.clj
+++ src/metabase/images/init.clj
@@ -1,4 +1,5 @@
 (ns metabase.images.init
   (:require
    ;; for the Toucan hydration methods
-   [metabase.images.models.image]))
+   [metabase.images.models.image]
+   [metabase.images.settings]))
---
+++ src/metabase/images/settings.clj
@@ -0,0 +1,22 @@
+(ns metabase.images.settings
+  (:require
+   [metabase.settings.core :as settings]
+   [metabase.util.i18n :as i18n]))
+
+(settings/defsetting image-upload-s3-bucket
+  (i18n/deferred-tru "S3 bucket to upload images to.")
+  :type :string
+  :visibility :admin
+  :encryption :no)
+
+(settings/defsetting image-upload-aws-access-key-id
+  (i18n/deferred-tru "AWS access key ID for image uploads.")
+  :type :string
+  :visibility :admin
+  :encryption :no)
+
+(settings/defsetting image-upload-aws-secret-access-key
+  (i18n/deferred-tru "AWS Secret Access Key for image uploads.")
+  :type :string
+  :visibility :admin
+  :encryption :when-encryption-key-set)
