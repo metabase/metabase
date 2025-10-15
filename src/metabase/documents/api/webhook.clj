b5be6d535c9f287ed9d847d93e65059671e9773c
Chris Truter <chris@metabase.com>
2025-10-15 13:01:35 +0200

    Event

--- src/metabase/documents/api/webhook.clj
+++ src/metabase/documents/api/webhook.clj
@@ -11,18 +11,16 @@
 (defn- load-event [{id :documentName}]
   ;; Return the ydoc and the extracted version
   (let [id (parse-long id)]
-    #p
-     (t2/select-one [:model/Document :document :ydoc] id)))
+    (t2/select-one [:model/Document :document :ydoc] id)))
 
 (defn- change-event [user-id {id :documentName, :keys [ydoc document]}]
   ;; work whether we have the new transformer to pass through the base64 ydoc too, or not
   (let [id (parse-long id)]
     (t2/update! :model/Document id {:document document
                                     :ydoc     ydoc})
-    ;; not needed for demo
-    #_(events/publish-event! :event/document-update
-                             {:object  (t2/select :model/Document id)
-                              :user-id user-id})))
+    (events/publish-even :event/document-update
+                         {:object  (t2/select-one :model/Document id)
+                          :user-id user-id})))
 
 (defn- disconnect-event [payload]
   ;; nothing to do here
@@ -34,9 +32,9 @@
    {{user-id :user_id} :context :keys [event payload]}]
   #p event
   (case event
-    "connect" (connect-event payload)
-    "load" (load-event payload)
-    "change" (change-event user-id payload)
+    "connect"    (connect-event payload)
+    "load"       (load-event payload)
+    "change"     (change-event user-id payload)
     "disconnect" (disconnect-event payload)
 
     (log/warn "Unexpected event" {:event event :payload payload})))
