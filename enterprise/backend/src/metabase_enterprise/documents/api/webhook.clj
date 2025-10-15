a2b7b90f949904b7216fbc781cab02997c73ed49
Edward Paget <ed.paget@gmail.com>
2025-10-15 10:21:07 +0200

    Only hook on onchange

--- enterprise/backend/src/metabase_enterprise/documents/api/webhook.clj
+++ enterprise/backend/src/metabase_enterprise/documents/api/webhook.clj
@@ -2,23 +2,14 @@
   (:require
    [metabase.api.macros :as api.macros]))
 
-(defn connect-event [payload]
-  {:user {:id 1 :name "Rasta Toucan"}})
-
-(defn create-event [payload])
 (defn change-event [payload])
-(defn disconnect-event [payload])
 
 (api.macros/defendpoint :post "/webhook"
   [_
    _
    {:keys [event payload]}]
-  #p p
   (case event
-    "connect" (connect-event payload)
-    "create" (create-event payload)
-    "change" (change-event payload)
-    "disconnect" (disconnect-event payload)))
+    "change" (change-event payload)))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/document/` routes."
