0f39867383f72eed4429a840ac2eeea0408d9088
Chris Truter <chris@metabase.com>
2025-10-15 11:09:59 +0200

    Save the raw ydoc, if we have it

--- enterprise/backend/src/metabase_enterprise/documents/api/webhook.clj
+++ enterprise/backend/src/metabase_enterprise/documents/api/webhook.clj
@@ -1,18 +1,29 @@
 (ns metabase-enterprise.documents.api.webhook
   (:require
-   [metabase.api.macros :as api.macros]))
+   [metabase.api.macros :as api.macros]
+   [metabase.events.core :as events]
+   [toucan2.core :as t2]))
 
 (defn- connect-event [payload]
   ;; nothing to do here, FE is initializing the user context on its own
   )
 
 (defn- create-event [payload]
-  ;; for now we let the FE initialize the document with a manual save
+  ;; for now, we let the FE initialize the document with a manual save
   )
 
-(defn- change-event [user-id {id :documentName, {:keys [document ydoc]} :document}]
-
-  )
+(defn- change-event [user-id {id :documentName, document :document}]
+  ;; work whether we have the new transformer to pass through the base64 ydoc too, or not
+  (let [id (parse-long id)
+        {:keys [ydoc document]} (if (:ydoc document)
+                                  document
+                                  {:ydoc     nil
+                                   :document document})]
+    (t2/update! :model/Document id {:document document
+                                    :ydoc     ydoc})
+    (events/publish-event! :event/document-update
+                           {:object  document
+                            :user-id user-id})))
 
 (defn- disconnect-event [payload]
   ;; nothing to do here
