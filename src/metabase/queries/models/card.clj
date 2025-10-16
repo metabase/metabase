5962c8ace335c531cdf2a919cfdf472d7544b169
John Swanson <john.d.swanson@gmail.com>
2025-10-16 07:46:17 -0700

    Search index includes analytics cards
    These were erroneously excluded from the index.

--- src/metabase/queries/models/card.clj
+++ src/metabase/queries/models/card.clj
@@ -1413,7 +1413,8 @@
                                        [:= :bookmark.user_id :current_user/id]]]
    :where [:and [:or [:= :collection.namespace nil]
                  [:= :collection.namespace "shared-tenant-collection"]
-                 [:= :collection.namespace "tenant-specific"]]
+                 [:= :collection.namespace "tenant-specific"]
+                 [:= :collection.namespace "analytics"]]
            [:= :this.document_id nil]]
    :joins        {:collection [:model/Collection [:= :collection.id :this.collection_id]]
                   :r          [:model/Revision [:and
