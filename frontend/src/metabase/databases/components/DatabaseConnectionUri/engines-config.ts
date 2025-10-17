cba23b577bdb8c9eb01a4768df72cf7f4031c801
Rodrigo López Dato <rodrigo@metabase.com>
2025-10-17 17:42:59 +0200

    fix tests

--- frontend/src/metabase/databases/components/DatabaseConnectionUri/engines-config.ts
+++ frontend/src/metabase/databases/components/DatabaseConnectionUri/engines-config.ts
@@ -14,10 +14,10 @@ export const enginesConfig: Record<EngineKey, Placeholder> = {
   databricks:
     "databricks://databricks.example.com:8123;httpPath=/sql/1.0/endpoints/abc;OAuthSecret=1234567890;OAuth2ClientId=xyz",
   mongo: undefined,
-  mysql: "mysql://user@pass@mysql.example.com:3306/dbname?ssl=true",
+  mysql: "mysql://user:pass@mysql.example.com:3306/dbname?ssl=true",
   oracle:
     "oracle:thin:@oracle.example.com:1521/mydbservice?ssl_server_cert_dn=ServerDN",
-  postgres: "postgresql://user@pass:pg.example.com:5432/mydb",
+  postgres: "postgresql://user:pass:pg.example.com:5432/mydb",
   "presto-jdbc":
     "presto://presto.example.com:1234/sample-catalog/sample-schema?SSL=true&SSLTrustStorePassword=1234",
   redshift:
