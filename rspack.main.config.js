8eb405c96b46821b1887532700e01bc23f12125f
Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>
2025-10-17 20:18:59 +0300

    speed up fe build by upgrading svgo (#64671)

--- rspack.main.config.js
+++ rspack.main.config.js
@@ -19,6 +19,7 @@ const { CSS_CONFIG } = require("./frontend/build/shared/rspack/css-config");
 const {
   getBannerOptions,
 } = require("./frontend/build/shared/rspack/get-banner-options");
+const { SVGO_CONFIG } = require("./frontend/build/shared/rspack/svgo-config");
 
 const ASSETS_PATH = __dirname + "/resources/frontend_client/app/assets";
 const FONTS_PATH = __dirname + "/resources/frontend_client/app/fonts";
@@ -197,6 +198,7 @@ const config = {
             loader: "@svgr/webpack",
             options: {
               ref: true,
+              svgoConfig: SVGO_CONFIG,
             },
           },
         ],
