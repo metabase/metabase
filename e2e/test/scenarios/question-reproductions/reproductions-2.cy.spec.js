4c732fe6227ee91e1b867673a507ea09abd94f3e
Kamil Mielnik <kamil@kamilmielnik.com>
2025-10-17 14:26:28 +0700

    Decrease flakiness

--- e2e/test/scenarios/question-reproductions/reproductions-2.cy.spec.js
+++ e2e/test/scenarios/question-reproductions/reproductions-2.cy.spec.js
@@ -631,38 +631,35 @@ describe("issue 43216", () => {
     });
   });
 
-  it(
-    "should update source question metadata when it changes (metabase#43216)",
-    { tags: "@flaky" },
-    () => {
-      cy.visit("/");
+  it("should update source question metadata when it changes (metabase#43216)", () => {
+    cy.visit("/");
 
-      cy.log("Create target question");
-      H.newButton("Question").click();
-      H.entityPickerModal().within(() => {
-        H.entityPickerModalTab("Collections").click();
-        cy.findByText("Source question").click();
-      });
-      H.saveQuestion("Target question");
+    cy.log("Create target question");
+    H.newButton("Question").click();
+    H.entityPickerModal().within(() => {
+      H.entityPickerModalTab("Collections").click();
+      cy.findByText("Source question").click();
+    });
+    H.saveQuestion("Target question");
 
-      cy.log("Update source question");
-      H.commandPaletteButton().click();
-      H.commandPalette().findByText("Source question").click();
-      cy.findByTestId("native-query-editor-container")
-        .findByText("Open Editor")
-        .click();
-      H.NativeEditor.focus().type(" , 4 as D");
-      H.saveSavedQuestion();
+    cy.log("Update source question");
+    H.commandPaletteButton().click();
+    H.commandPalette().findByText("Source question").click();
+    cy.findByTestId("native-query-editor-container")
+      .findByText("Open Editor")
+      .click();
+    H.NativeEditor.focus().type(" , 4 as D");
+    H.saveSavedQuestion();
 
-      cy.log("Assert updated metadata in target question");
-      H.commandPaletteButton().click();
-      H.commandPalette().findByText("Target question").click();
-      cy.findAllByTestId("header-cell").eq(3).should("have.text", "D");
-      H.openNotebook();
-      H.getNotebookStep("data").button("Pick columns").click();
-      H.popover().findByText("D").should("be.visible");
-    },
-  );
+    cy.log("Assert updated metadata in target question");
+    H.commandPaletteSearch("target", false); // intentionally use search instead of recents for deterministic order
+    H.waitForLoaderToBeRemoved();
+    H.commandPalette().findByText("Target question").click();
+    cy.findAllByTestId("header-cell").eq(3).should("have.text", "D");
+    H.openNotebook();
+    H.getNotebookStep("data").button("Pick columns").click();
+    H.popover().findByText("D").should("be.visible");
+  });
 });
 
 function updateQuestion() {
