5addcc4216d9f6cbe7f32203767de4003be9bff8
Cam Saul <github@camsaul.com>
2025-10-20 10:00:15 -0700

    Test fixes :wrench:

--- test/metabase/query_processor_test/nested_queries_test.clj
+++ test/metabase/query_processor_test/nested_queries_test.clj
@@ -16,7 +16,7 @@
    [metabase.lib.temporal-bucket :as lib.temporal-bucket]
    [metabase.lib.test-metadata :as meta]
    [metabase.lib.test-util :as lib.tu]
-   [metabase.lib.util :as lib.util]
+   [metabase.lib.util.unique-name-generator]
    [metabase.models.interface :as mi]
    [metabase.permissions.core :as perms]
    [metabase.query-permissions.core :as query-perms]
@@ -30,6 +30,7 @@
    [metabase.test.data.interface :as tx]
    [metabase.util :as u]
    [metabase.util.date-2 :as u.date]
+   [metabase.util.malli :as mu]
    [toucan2.core :as t2]))
 
 (deftest ^:parallel basic-test
@@ -364,17 +365,18 @@
                                        (count long-col-full-name)))
             ;; Disable truncate-alias when compiling the native query to ensure we don't further truncate the column.
             ;; We want to simulate a user-defined query where the column name is long, but valid for the driver.
-            native-sub-query   (with-redefs [lib.util/truncate-alias
+            native-sub-query   (with-redefs [metabase.lib.util.unique-name-generator/truncate-alias
                                              (fn mock-truncate-alias
                                                ([ss] ss)
                                                ([ss _] ss))]
-                                 (-> (mt/mbql-query people
-                                       {:source-table $$people
-                                        :aggregation  [[:aggregation-options [:count] {:name coun-col-name}]]
-                                        :breakout     [[:field %state {:name long-col-name}]]
-                                        :limit        5})
-                                     qp.compile/compile
-                                     :query))
+                                 (mu/disable-enforcement
+                                   (-> (mt/mbql-query people
+                                         {:source-table $$people
+                                          :aggregation  [[:aggregation-options [:count] {:name coun-col-name}]]
+                                          :breakout     [[:field %state {:name long-col-name}]]
+                                          :limit        5})
+                                       qp.compile/compile
+                                       :query)))
             query              (query-with-source-card 1 lib.schema.id/saved-questions-virtual-database-id)]
         (qp.store/with-metadata-provider (qp.test-util/metadata-provider-with-cards-with-metadata-for-queries
                                           [(mt/native-query {:query native-sub-query})])
