8282e48c3fda117e57fbab4e4bc79fdb62f99e18
Nicola Mometto <nicola.mometto@metabase.com>
2025-10-21 14:35:57 +0100

    more tests

--- enterprise/backend/test/metabase_enterprise/transforms/incremental_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms/incremental_test.clj
@@ -19,7 +19,7 @@
   [schema]
   {:database (mt/id)
    :type "native"
-   :native {:query (format "SELECT * FROM %stransforms_products WHERE id > {{watermark}}" (cond-> schema schema (str ".")))
+   :native {:query (format "SELECT * FROM %stransforms_products [[WHERE id > {{watermark}}]]" (cond-> schema schema (str ".")))
             :template-tags {"watermark" {:id "watermark"
                                          :name "watermark"
                                          :display-name "Watermark"
@@ -167,3 +167,160 @@
                       (testing "Watermark is updated to new MAX(id)"
                         (let [watermark (get-watermark-value (:id transform))]
                           (is (>= watermark 18) "Watermark should be updated"))))))))))))))
+
+(deftest switch-incremental-to-non-incremental-test
+  (testing "Switching an incremental transform to non-incremental overwrites data"
+    (mt/test-drivers (mt/normal-drivers-with-feature :transforms/table)
+      (mt/with-premium-features #{:transforms}
+        (mt/dataset transforms-dataset/transforms-test
+          (with-transform-cleanup! [target-table "switch_incr_to_non_incr"]
+            (let [schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))
+                  initial-payload (make-incremental-transform-payload "Switch Transform" target-table "id")]
+              (mt/with-temp [:model/Transform transform initial-payload]
+                (testing "Initial incremental run processes all data"
+                  (transforms.i/execute! transform {:run-method :manual})
+                  (transforms.tu/wait-for-table target-table 10000)
+                  (let [row-count (get-table-row-count target-table)
+                        watermark (get-watermark-value (:id transform))]
+                    (is (= 16 row-count) "Initial run should process all 16 products")
+                    (is (= 16 watermark) "Watermark should be created")))
+
+                (testing "Add new data and run incrementally"
+                  (with-insert-test-products!
+                    [{:name "Before Switch Product"
+                      :category "Widget"
+                      :price 59.99
+                      :created-at "2024-01-19T10:00:00"}]
+
+                    (transforms.i/execute! transform {:run-method :manual})
+                    (let [row-count (get-table-row-count target-table)]
+                      (is (= 17 row-count) "Should have 17 rows after incremental update"))))
+
+                (testing "Switch to non-incremental via PUT API"
+                  (let [non-incremental-payload {:source {:type "query"
+                                                          :query (make-incremental-source-query schema)}
+                                                 :target {:type "table"
+                                                          :schema schema
+                                                          :name target-table}}
+                        updated (mt/user-http-request :crowberto :put 200 (format "ee/transform/%d" (:id transform))
+                                                      non-incremental-payload)]
+                    (is (= "table" (-> updated :target :type)))
+                    (is (nil? (-> updated :source :source-incremental-strategy)))))
+
+                (testing "Non-incremental run overwrites all data"
+                  (let [transform (t2/select-one :model/Transform (:id transform))]
+                    (transforms.i/execute! transform {:run-method :manual})
+                    (let [row-count (get-table-row-count target-table)]
+                      (is (= 16 row-count) "Should overwrite to 16 rows (all original products)"))
+
+                    (testing "Running again still overwrites"
+                      (transforms.i/execute! transform {:run-method :manual})
+                      (let [row-count (get-table-row-count target-table)]
+                        (is (= 16 row-count) "Should still have 16 rows after another run")))))))))))))
+
+(deftest switch-non-incremental-to-incremental-test
+  (testing "Switching a non-incremental transform to incremental computes watermark from existing data"
+    (mt/test-drivers (mt/normal-drivers-with-feature :transforms/table)
+      (mt/with-premium-features #{:transforms}
+        (mt/dataset transforms-dataset/transforms-test
+          (with-transform-cleanup! [target-table "switch_non_incr_to_incr"]
+            (let [schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))
+                  initial-payload {:name "Non-Incremental Transform"
+                                   :source {:type "query"
+                                            :query (make-incremental-source-query schema)}
+                                   :target {:type "table"
+                                            :schema schema
+                                            :name target-table}}]
+              (mt/with-temp [:model/Transform transform initial-payload]
+                (testing "Initial non-incremental run creates table"
+                  (transforms.i/execute! transform {:run-method :manual})
+                  (transforms.tu/wait-for-table target-table 10000)
+                  (let [row-count (get-table-row-count target-table)]
+                    (is (= 16 row-count) "Initial run should process all 16 products")
+
+                    (testing "No watermark exists"
+                      (let [watermark (get-watermark-value (:id transform))]
+                        (is (nil? watermark) "No watermark for non-incremental transform")))))
+
+                (testing "Switch to incremental via PUT API"
+                  (let [incremental-payload {:source {:type "query"
+                                                      :query (make-incremental-source-query schema)
+                                                      :source-incremental-strategy {:type "keyset"
+                                                                                    :keyset-column "id"}}
+                                             :target {:type "table-incremental"
+                                                      :schema schema
+                                                      :name target-table
+                                                      :target-incremental-strategy {:type "append"}}}
+                        updated (mt/user-http-request :crowberto :put 200 (format "ee/transform/%d" (:id transform))
+                                                      incremental-payload)]
+                    (is (= "table-incremental" (-> updated :target :type)))
+                    (is (= "keyset" (-> updated :source :source-incremental-strategy :type)))))
+
+                (testing "First incremental run after switch processes no new data"
+                  (let [transform (t2/select-one :model/Transform (:id transform))]
+                    (transforms.i/execute! transform {:run-method :manual})
+                    (let [row-count (get-table-row-count target-table)
+                          watermark (get-watermark-value (:id transform))]
+                      (is (= 16 row-count) "Should still have 16 rows (no duplicates)")
+                      (is (= 16 watermark) "Watermark should be computed from existing data"))))
+
+                (testing "Add new data and run incrementally"
+                  (with-insert-test-products!
+                    [{:name "After Switch Product"
+                      :category "Gadget"
+                      :price 79.99
+                      :created-at "2024-01-20T10:00:00"}]
+
+                    (let [transform (t2/select-one :model/Transform (:id transform))]
+                      (transforms.i/execute! transform {:run-method :manual})
+                      (let [row-count (get-table-row-count target-table)
+                            watermark (get-watermark-value (:id transform))]
+                        (is (= 17 row-count) "Should append 1 new row (16 + 1 = 17)")
+                        (is (>= watermark 17) "Watermark should be updated to at least 17")))))))))))))
+
+(deftest flush-watermark-test
+  (testing "Flushing a watermark causes the next run to reprocess all data"
+    (mt/test-drivers (mt/normal-drivers-with-feature :transforms/table)
+      (mt/with-premium-features #{:transforms}
+        (mt/dataset transforms-dataset/transforms-test
+          (with-transform-cleanup! [target-table "flush_watermark_test"]
+            (let [transform-payload (make-incremental-transform-payload "Flush Watermark Transform" target-table "id")]
+              (mt/with-temp [:model/Transform transform transform-payload]
+                (testing "First run processes all data and creates watermark"
+                  (transforms.i/execute! transform {:run-method :manual})
+                  (transforms.tu/wait-for-table target-table 10000)
+                  (let [row-count (get-table-row-count target-table)
+                        watermark (get-watermark-value (:id transform))]
+                    (is (= 16 row-count) "First run should process all 16 products")
+                    (is (= 16 watermark) "Watermark should be MAX(id) = 16")))
+
+                (testing "Second run with no new data processes nothing (fixpoint)"
+                  (transforms.i/execute! transform {:run-method :manual})
+                  (let [row-count (get-table-row-count target-table)]
+                    (is (= 16 row-count) "Second run should not add any rows")))
+
+                (testing "Flush watermark via API"
+                  (mt/user-http-request :crowberto :post 204 (format "ee/transform/%d/flush-watermark" (:id transform)))
+                  (let [watermark (get-watermark-value (:id transform))]
+                    (is (nil? watermark) "Watermark should be deleted after flush")))
+
+                (testing "Run after flush reprocesses all data"
+                  (let [transform (t2/select-one :model/Transform (:id transform))]
+                    (transforms.i/execute! transform {:run-method :manual})
+                    (let [row-count (get-table-row-count target-table)
+                          watermark (get-watermark-value (:id transform))]
+                      (is (= 32 row-count) "Should append all 16 products again (16 + 16 = 32)")
+                      (is (= 16 watermark) "Watermark should be recomputed to MAX(id) = 16"))))
+
+                (testing "Cannot flush watermark for non-incremental transform"
+                  (let [schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))
+                        non-incremental-payload {:source {:type "query"
+                                                          :query (make-incremental-source-query schema)}
+                                                 :target {:type "table"
+                                                          :schema schema
+                                                          :name target-table}}]
+                    (mt/user-http-request :crowberto :put 200 (format "ee/transform/%d" (:id transform))
+                                          non-incremental-payload)
+                    (is (= "Only transforms with keyset incremental strategy can have their watermark flushed."
+                           (mt/user-http-request :crowberto :post 400 (format "ee/transform/%d/flush-watermark" (:id transform))))
+                        "Should return 400 for non-keyset incremental transform")))))))))))
