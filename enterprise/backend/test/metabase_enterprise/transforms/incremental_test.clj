86f615c264b64595671aaff0df6672b7f9927e76
Nicola Mometto <nicola.mometto@metabase.com>
2025-10-21 13:36:23 +0100

    refactor

--- enterprise/backend/test/metabase_enterprise/transforms/incremental_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms/incremental_test.clj
@@ -1,6 +1,7 @@
 (ns ^:mb/driver-tests metabase-enterprise.transforms.incremental-test
   "Tests for incremental transforms functionality."
   (:require
+   [clojure.string :as str]
    [clojure.test :refer :all]
    [metabase-enterprise.transforms.interface :as transforms.i]
    [metabase-enterprise.transforms.test-dataset :as transforms-dataset]
@@ -14,6 +15,66 @@
    [metabase.test :as mt]
    [toucan2.core :as t2]))
 
+(defn- make-incremental-source-query
+  "Create a native query with watermark template tag for incremental transforms."
+  []
+  {:database (mt/id)
+   :type "native"
+   :native {:query "SELECT * FROM transforms_products WHERE id > {{watermark}}"
+            :template-tags {"watermark" {:id "watermark"
+                                         :name "watermark"
+                                         :display-name "Watermark"
+                                         :type :number
+                                         :default 0
+                                         :required false}}}})
+
+(defn- make-incremental-transform-payload
+  "Create a transform payload for incremental transform testing."
+  [transform-name target-table-name keyset-column]
+  (let [schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))]
+    {:name transform-name
+     :source {:type "query"
+              :query (make-incremental-source-query)
+              :source-incremental-strategy {:type "keyset"
+                                            :keyset-column keyset-column}}
+     :target {:type "table-incremental"
+              :schema schema
+              :name target-table-name
+              :target-incremental-strategy {:type "append"}}}))
+
+(defn- get-table-row-count
+  "Get the row count of a table by name."
+  [table-name]
+  (let [table (t2/select-one :model/Table :name table-name)
+        mp (mt/metadata-provider)
+        table-metadata (lib.metadata/table mp (:id table))
+        count-query (lib/aggregate (lib/query mp table-metadata) (lib/count))
+        result (qp/process-query count-query)]
+    (-> result :data :rows first first)))
+
+(defn- get-watermark-value
+  "Get the watermark value for a transform."
+  [transform-id]
+  (t2/select-one-fn :watermark_value :model/TransformWatermark :transform_id transform-id))
+
+(defn- insert-test-products!
+  "Insert new products into the transforms_products table."
+  [products]
+  (let [schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))
+        source-table-name (t2/select-one-fn :name :model/Table (mt/id :transforms_products))
+        spec (sql-jdbc.conn/db->pooled-connection-spec (mt/id))
+        schema-prefix (if schema (str schema ".") "")
+        values-list (str/join ", "
+                              (map (fn [{:keys [name category price created-at]}]
+                                     (format "('%s', '%s', %s, '%s')" name category price created-at))
+                                   products))
+        insert-sql (format "INSERT INTO %s%s (name, category, price, created_at) VALUES %s"
+                           schema-prefix
+                           source-table-name
+                           values-list)]
+    (driver/execute-raw-queries! driver/*driver* spec [[insert-sql]])
+    (sync/sync-database! (mt/db) {:scan :schema})))
+
 (set! *warn-on-reflection* true)
 
 (deftest create-incremental-transform-test
@@ -22,25 +83,7 @@
       (mt/with-premium-features #{:transforms}
         (mt/dataset transforms-dataset/transforms-test
           (with-transform-cleanup! [target-table "incremental_test"]
-            (let [schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))
-                  source-query {:database (mt/id)
-                                :type "native"
-                                :native {:query "SELECT * FROM transforms_products WHERE id > {{watermark}}"
-                                         :template-tags {"watermark" {:id "watermark"
-                                                                      :name "watermark"
-                                                                      :display-name "Watermark"
-                                                                      :type :number
-                                                                      :default 0
-                                                                      :required false}}}}
-                  transform-payload {:name "Test Incremental Transform"
-                                     :source {:type "query"
-                                              :query source-query
-                                              :source-incremental-strategy {:type "keyset"
-                                                                            :keyset-column "id"}}
-                                     :target {:type "table-incremental"
-                                              :schema schema
-                                              :name target-table
-                                              :target-incremental-strategy {:type "append"}}}]
+            (let [transform-payload (make-incremental-transform-payload "Test Incremental Transform" target-table "id")]
               (testing "Transform is created successfully"
                 (mt/with-temp [:model/Transform transform transform-payload]
                   (is (some? (:id transform)))
@@ -69,76 +112,41 @@
       (mt/with-premium-features #{:transforms}
         (mt/dataset transforms-dataset/transforms-test
           (with-transform-cleanup! [target-table "incremental_twice"]
-            (let [schema (t2/select-one-fn :schema :model/Table (mt/id :transforms_products))
-                  source-table-name (t2/select-one-fn :name :model/Table (mt/id :transforms_products))
-                  source-query {:database (mt/id)
-                                :type "native"
-                                :native {:query "SELECT * FROM transforms_products WHERE id > {{watermark}}"
-                                         :template-tags {"watermark" {:id "watermark"
-                                                                      :name "watermark"
-                                                                      :display-name "Watermark"
-                                                                      :type :number
-                                                                      :default 0
-                                                                      :required false}}}}
-                  transform-payload {:name "Incremental Transform"
-                                     :source {:type "query"
-                                              :query source-query
-                                              :source-incremental-strategy {:type "keyset"
-                                                                            :keyset-column "id"}}
-                                     :target {:type "table-incremental"
-                                              :schema schema
-                                              :name target-table
-                                              :target-incremental-strategy {:type "append"}}}]
+            (let [transform-payload (make-incremental-transform-payload "Incremental Transform" target-table "id")]
               (mt/with-temp [:model/Transform transform transform-payload]
                 (testing "First run processes all data"
                   (transforms.i/execute! transform {:run-method :manual})
-                  (let [table (transforms.tu/wait-for-table target-table 10000)
-                        mp (mt/metadata-provider)
-                        table-metadata (lib.metadata/table mp (:id table))
-                        count-query (lib/aggregate (lib/query mp table-metadata) (lib/count))
-                        result (qp/process-query count-query)
-                        row-count (-> result :data :rows first first)]
+                  (transforms.tu/wait-for-table target-table 10000)
+                  (let [row-count (get-table-row-count target-table)]
                     (is (= 16 row-count) "First run should process all 16 products")
 
                     (testing "Watermark is created after first run"
-                      (let [watermark (t2/select-one :model/TransformWatermark :transform_id (:id transform))]
-                        (is (some? watermark))
-                        (is (= 16 (:watermark_value watermark)) "Watermark should be MAX(id) = 16")))))
+                      (let [watermark (get-watermark-value (:id transform))]
+                        (is (= 16 watermark) "Watermark should be MAX(id) = 16")))))
 
                 (testing "Second run with no new data processes nothing"
                   (transforms.i/execute! transform {:run-method :manual})
-                  (let [table (t2/select-one :model/Table :name target-table)
-                        mp (mt/metadata-provider)
-                        table-metadata (lib.metadata/table mp (:id table))
-                        count-query (lib/aggregate (lib/query mp table-metadata) (lib/count))
-                        result (qp/process-query count-query)
-                        row-count (-> result :data :rows first first)]
+                  (let [row-count (get-table-row-count target-table)]
                     (is (= 16 row-count) "Second run should not add any rows")
 
                     (testing "Watermark remains unchanged"
-                      (let [watermark (t2/select-one :model/TransformWatermark :transform_id (:id transform))]
-                        (is (= 16 (:watermark_value watermark)) "Watermark should still be 16")))))
+                      (let [watermark (get-watermark-value (:id transform))]
+                        (is (= 16 watermark) "Watermark should still be 16")))))
 
                 (testing "Third run after adding new data processes only new rows"
-                  (let [spec (sql-jdbc.conn/db->pooled-connection-spec (mt/id))
-                        schema-prefix (if schema (str schema ".") "")
-                        insert-sql (format "INSERT INTO %s%s (name, category, price, created_at) VALUES ('New Product 1', 'Widget', 99.99, '2024-01-17T10:00:00'), ('New Product 2', 'Gadget', 199.99, '2024-01-18T10:00:00')"
-                                           schema-prefix
-                                           source-table-name)]
-                    (driver/execute-raw-queries! driver/*driver* spec [[insert-sql]])
-                    (sync/sync-database! (mt/db) {:scan :schema})
+                  (insert-test-products! [{:name "New Product 1"
+                                           :category "Widget"
+                                           :price 99.99
+                                           :created-at "2024-01-17T10:00:00"}
+                                          {:name "New Product 2"
+                                           :category "Gadget"
+                                           :price 199.99
+                                           :created-at "2024-01-18T10:00:00"}])
 
-                    (transforms.i/execute! transform {:run-method :manual})
-                    (let [table (t2/select-one :model/Table :name target-table)
-                          mp (mt/metadata-provider)
-                          table-metadata (lib.metadata/table mp (:id table))
-                          count-query (lib/aggregate (lib/query mp table-metadata) (lib/count))
-                          result (qp/process-query count-query)
-                          row-count (-> result :data :rows first first)]
-                      (is (= 18 row-count) "Third run should add 2 new rows (16 + 2 = 18)")
+                  (transforms.i/execute! transform {:run-method :manual})
+                  (let [row-count (get-table-row-count target-table)]
+                    (is (= 18 row-count) "Third run should add 2 new rows (16 + 2 = 18)")
 
-                      (testing "Watermark is updated to new MAX(id)"
-                        (let [watermark (t2/select-one :model/TransformWatermark :transform_id (:id transform))]
-                          (is (= 18 (:watermark_value watermark)) "Watermark should be updated to 18"))))))))))))))
-
-
+                    (testing "Watermark is updated to new MAX(id)"
+                      (let [watermark (get-watermark-value (:id transform))]
+                        (is (= 18 watermark) "Watermark should be updated to 18")))))))))))))
