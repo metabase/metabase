84c4b214699b8efe23f4a176a700a839191b0bea
Timofei <timofei@metabase.com>
2025-10-21 11:23:31 +0400

    Fix `requestKey` calculation using stable stringify

--- frontend/src/metabase/lib/objects.ts
+++ frontend/src/metabase/lib/objects.ts
@@ -1,3 +1,5 @@
+import { sortObject } from "metabase-lib/v1/utils";
+
 export const getObjectEntries = <K extends string, V>(
   obj: Record<K, V>,
 ): [K, V][] => {
@@ -13,3 +15,7 @@ export const getObjectKeys = <K extends string>(
 export const getObjectValues = <V>(obj: Record<string, V>): V[] => {
   return Object.values(obj) as V[];
 };
+
+// Stringify with sorted keys to ensure stable orders.
+export const stableStringify = <T>(obj: T): string =>
+  JSON.stringify(sortObject(obj));
