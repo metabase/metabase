7f721edb6843b3a759b569ce1cf99bc31817f794
jeff bruemmer <jeff.bruemmer@gmail.com>
2025-10-22 10:33:49 -0400

    update test to accept 200

--- enterprise/backend/test/metabase_enterprise/documents/api/document_test.clj
+++ enterprise/backend/test/metabase_enterprise/documents/api/document_test.clj
@@ -2251,23 +2251,28 @@
                                                                            :attrs {:id card-id}}]}}]
           (t2/update! :model/Card card-id {:document_id doc-id})
 
-          (mt/with-group-for-user [group :rasta {:name "Rasta Group"}]
+          (let [all-users-group (perms/all-users-group)]
             ;; Grant collection read permissions so user can access the document
-            (perms/grant-collection-read-permissions! group coll-id)
+            (perms/grant-collection-read-permissions! all-users-group coll-id)
 
-            (testing "User without download permissions gets 403"
-              ;; Set download permissions to :no-self-service (no downloads allowed)
-              (data-perms/set-database-permission! group (mt/id) :perms/download-results :no)
+            (testing "User without download permissions yields permissions error in body (streaming uses 200)"
+              ;; Set download permissions to :no (no downloads allowed) for All Users group
+              (data-perms/set-database-permission! all-users-group (mt/id) :perms/download-results :no)
 
-              (mt/user-http-request :rasta
-                                    :post 403
-                                    (format "ee/document/%s/card/%s/query/csv" doc-id card-id)
-                                    {:parameters []
-                                     :format_rows false
-                                     :pivot_results false}))
+              (is (malli= [:map
+                           [:status [:= "failed"]]
+                           [:error_type [:= "missing-required-permissions"]]
+                           [:ex-data [:map
+                                      [:permissions-error? [:= true]]]]]
+                          (mt/user-http-request :rasta
+                                                :post 200
+                                                (format "ee/document/%s/card/%s/query/csv" doc-id card-id)
+                                                {:parameters []
+                                                 :format_rows false
+                                                 :pivot_results false}))))
 
             (testing "User with limited download permissions (10k rows) can download"
-              (data-perms/set-database-permission! group (mt/id) :perms/download-results :ten-thousand-rows)
+              (data-perms/set-database-permission! all-users-group (mt/id) :perms/download-results :ten-thousand-rows)
 
               (let [response (mt/user-http-request :rasta
                                                    :post 200
@@ -2279,7 +2284,7 @@
                 (is (string? response))))
 
             (testing "User with full download permissions can download"
-              (data-perms/set-database-permission! group (mt/id) :perms/download-results :one-million-rows)
+              (data-perms/set-database-permission! all-users-group (mt/id) :perms/download-results :one-million-rows)
 
               (let [response (mt/user-http-request :rasta
                                                    :post 200
