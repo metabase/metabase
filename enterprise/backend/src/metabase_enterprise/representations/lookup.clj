3e8cc7d24177bbb08f752fbb5a1d813c504fd85b
Eric Normand <ericwnormand@gmail.com>
2025-10-22 12:01:12 -0500

    Add docstrings in lookup

--- enterprise/backend/src/metabase_enterprise/representations/lookup.clj
+++ enterprise/backend/src/metabase_enterprise/representations/lookup.clj
@@ -9,13 +9,21 @@
    [metabase.util.malli :as mu]
    [toucan2.core :as t2]))
 
-(defn lookup-by-id [rep-type id]
+(defn lookup-by-id
+  "Find and return a toucan entity given the representation type and id."
+  [rep-type id]
   (t2/select-one (v0-common/type->model rep-type) :id id))
 
-(defn lookup-by-entity-id [rep-type entity-id]
+(defn lookup-by-entity-id
+  "Find and return a toucan entity given the representation type and entity id."
+  [rep-type entity-id]
   (t2/select-one (v0-common/type->model rep-type) :entity_id entity-id))
 
-(defn lookup-by-name [rep-type name]
+(defn lookup-by-name
+  "Find and return a toucan entity given the representation type and its name.
+
+   Will throw an exception if multiple entities are found with the same name."
+  [rep-type name]
   (let [entities (t2/select (v0-common/type->model rep-type) :name name)]
     (if (= 1 (count entities))
       (first entities)
