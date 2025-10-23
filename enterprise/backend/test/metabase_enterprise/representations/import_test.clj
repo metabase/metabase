b087de7b13c9cf7988bb1ad72efe53fa5aef0c88
Eric Normand <ericwnormand@gmail.com>
2025-10-23 12:23:37 -0500

    Guard against infinite loops in order-representations

--- enterprise/backend/test/metabase_enterprise/representations/import_test.clj
+++ enterprise/backend/test/metabase_enterprise/representations/import_test.clj
@@ -2,7 +2,6 @@
   (:require
    [clojure.test :refer :all]
    [metabase-enterprise.representations.import :as import]
-   [metabase.test :as mt]
    [metabase.test.fixtures :as fixtures]))
 
 (use-fixtures :once (fixtures/initialize :db))
@@ -10,31 +9,36 @@
 (deftest order-representations-self-reference-test
   (testing "order-representations throws exception when representation refers to itself"
     (let [reps [{:ref "a" :database "ref:a"}]
-          result (future (try
-                           (import/order-representations reps)
-                           (catch Exception e e)))]
-      (is (instance? Exception (deref result 100 ::timeout))
+          result-future (future (try
+                                  (import/order-representations reps)
+                                  (catch Exception e e)))
+          result (deref result-future 100 ::timeout)]
+      (future-cancel result-future)
+      (is (instance? Exception result)
           "Expected exception to be thrown")
-      (when (= ::timeout (deref result 0 ::timeout))
-        (future-cancel result)
-        (is false "order-representations took longer than 100ms, likely infinite loop")))))
+      (is (not= ::timeout result) "order-representations took longer than 100ms, likely infinite loop"))))
 
 (deftest order-representations-circular-reference-test
   (testing "order-representations throws exception when representations have circular dependency"
     (let [reps [{:ref "a" :database "ref:b"}
                 {:ref "b" :database "ref:a"}]
-          result (future (try
-                           (import/order-representations reps)
-                           (catch Exception e e)))]
-      (is (instance? Exception (deref result 100 ::timeout))
+          result-future (future (try
+                                  (import/order-representations reps)
+                                  (catch Exception e e)))
+          result (deref result-future 100 ::timeout)]
+      (future-cancel result-future)
+      (is (instance? Exception result)
           "Expected exception to be thrown")
-      (when (= ::timeout (deref result 0 ::timeout))
-        (future-cancel result)
-        (is false "order-representations took longer than 100ms, likely infinite loop")))))
+      (is (not= ::timeout result) "order-representations took longer than 100ms, likely infinite loop"))))
 
 (deftest order-representations-empty-test
   (testing "order-representations handles empty list"
-    (is (= [] (import/order-representations [])))))
+    (let [result-future (future (try
+                                  (import/order-representations [])
+                                  (catch Exception e e)))
+          result (deref result-future 100 ::timeout)]
+      (future-cancel result-future)
+      (is (= [] result)))))
 
 (deftest order-representations-chain-test
   (testing "order-representations orders chain correctly"
@@ -42,6 +46,10 @@
                 {:ref "c" :type :model :database "ref:d"}
                 {:ref "b" :type :database :name "ref:c"}
                 {:ref "d" :type :database :name "db"}]
-          ordered (import/order-representations reps)
+          result-future (future (try
+                                  (import/order-representations reps)
+                                  (catch Exception e e)))
+          ordered (deref result-future 100 ::timeout)
           refs (mapv :ref ordered)]
+      (future-cancel result-future)
       (is (= ["d" "c" "b" "a"] refs)))))
