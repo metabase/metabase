d1633a87c71ffd3778fb7f495f51137c422006c4
dan sutton <dan@dpsutton.com>
2025-10-23 13:22:26 -0500

    fixup test

--- test/metabase/test/util.clj
+++ test/metabase/test/util.clj
@@ -600,7 +600,7 @@
   The token-value binding will contain the random token that was set."
   [[token-value] & body]
   `(let [~token-value (premium-features.test-util/random-token)]
-     (with-redefs [metabase.premium-features.token-check/fetch-token-status
+     (with-redefs [metabase.premium-features.token-check/token-information
                    (constantly {:valid    true
                                 :status   "fake"
                                 :features ["test" "fixture"]
