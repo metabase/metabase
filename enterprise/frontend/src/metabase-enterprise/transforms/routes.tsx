14997c90d18e4c52dfbeee50168a5f044dc7130e
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-10-24 11:14:55 -0400

    Header

--- enterprise/frontend/src/metabase-enterprise/transforms/routes.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/routes.tsx
@@ -3,6 +3,7 @@ import { t } from "ttag";
 import { Route } from "metabase/hoc/Title";
 import { PLUGIN_TRANSFORMS_PYTHON } from "metabase/plugins";
 
+import { NewTransformPage } from "./pages/NewTransformPage";
 import { TransformLayout } from "./pages/TransformLayout";
 import { TransformQueryPage } from "./pages/TransformQueryPage";
 import { TransformSchedulePage } from "./pages/TransformSchedulePage";
@@ -12,6 +13,7 @@ export const getTransformRoutes = () => (
   <>
     <Route path="transforms">
       <Route title={t`Transforms`} component={TransformLayout}>
+        <Route path="new/:type" component={NewTransformPage} />
         <Route path=":transformId" component={TransformQueryPage} />
         <Route path=":transformId/schedule" component={TransformSchedulePage} />
         <Route path=":transformId/target" component={TransformTargetPage} />
