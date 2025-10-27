5b2704453dd1184bfafeb2890f023681054e0fc5
Noah Moss <noahbmoss@gmail.com>
2025-10-27 15:37:48 -0400

    update metabot tool apis

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/api.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/api.clj
@@ -628,6 +628,7 @@
    {:decode/tool-api-response #(update-keys % metabot-v3.u/safe->snake_case_en)}
    [:id :int]
    [:name :string]
+   [:type [:enum {:decode/tool-api-response name} "mbql" "native" "python"]]
    [:description {:optional true} [:maybe :string]]
    [:entity_id {:optional true} [:maybe :string]]
    ;; :source keys are not snake_cased to match what the FE expects / provides in user_is_viewing context
