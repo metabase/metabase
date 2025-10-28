68f0498bd512d411f5b006aae52fbb5dcf8e637a
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-10-28 11:38:17 +0100

    Simplify types

--- frontend/src/metabase-types/api/store.ts
+++ frontend/src/metabase-types/api/store.ts
@@ -42,7 +42,7 @@ export type BillingInfo = {
 
 export interface MetabasePlan {
   id: number;
-  name: MetabasePlanName;
+  name: string;
   description: string;
   alias: MetabasePlanAlias;
   product: string;
@@ -53,7 +53,7 @@ export interface MetabasePlan {
   per_user_price: string;
   price: string;
 
-  hosting_features: MetabasePlanHostingFeature[];
+  hosting_features: string[];
   token_features: TokenFeature[];
 
   addon_price?: string;
@@ -61,34 +61,6 @@ export interface MetabasePlan {
 }
 
 export type MetabasePlansResponse = MetabasePlan[];
-
-export type MetabasePlanHostingFeature =
-  | "can-have-dev-subscription"
-  | "clickhouse-dwh"
-  | "custom-domain"
-  | "dedicated-node"
-  | "invoiceable"
-  | "invoicing-eligibility"
-  | "is-dev-subscription"
-  | "metabase-api-key"
-  | "trial-up";
-
-export type MetabasePlanName =
-  | "Metabase Cloud Starter Plan (Yearly, with Storage)"
-  | "Metabase Dev Instance Cloud (Monthly)"
-  | "Metabase Cloud Starter Plan"
-  | "Metabase Pro Cloud (Yearly)"
-  | "Metabase Cloud Starter Plan (with Storage)"
-  | "Metabase Cloud Starter Plan (Yearly)"
-  | "Metabase Dev Instance Cloud (Yearly)"
-  | "Metabase Cloud - Business"
-  | "Metabase Pro Self-Hosted (Yearly)"
-  | "Metabase Dev Instance Self-Hosted (Monthly)"
-  | "Metabase Pro Self-Hosted"
-  | "Metabase Pro Cloud (with Storage)"
-  | "Metabase Pro Cloud"
-  | "Metabase Pro Cloud (Yearly, with Storage)"
-  | "Metabase Dev Instance Self-Hosted (Yearly)";
 
 export type MetabasePlanAlias =
   | "business"
@@ -114,14 +86,14 @@ export interface MetabaseAddon {
   name: string;
   short_name: string;
   description: string | null;
-  alias: MetabaseAddonAlias;
+  alias: string;
   product_type: MetabaseAddonProductType;
   deployment: string;
   billing_period_months: BillingPeriodMonths;
   active: boolean;
   self_service: boolean;
 
-  hosting_features: MetabaseAddonHostingFeature[];
+  hosting_features: string[];
   token_features: TokenFeature[];
 
   trialup_to_product_id: string | null;
@@ -138,29 +110,9 @@ export interface MetabaseAddon {
 
 export type MetabaseAddonsResponse = MetabaseAddon[];
 
-export type MetabaseAddonAlias =
-  | "metabase-ai"
-  | "metabase-ai-yearly"
-  | "etl-connections"
-  | "etl-connections-yearly"
-  | "dwh"
-  | "dwh-yearly"
-  | "python-execution"
-  | "python-execution-yearly"
-  | "metabase-ai-tiered"
-  | "metabase-ai-tiered-yearly";
-
 export type MetabaseAddonProductType =
   | "metabase-ai"
   | "etl-connections"
   | "dwh"
   | "python-execution"
   | "metabase-ai-tiered";
-
-export type MetabaseAddonHostingFeature =
-  | "metabase-ai"
-  | "semantic-search-db"
-  | "etl-connections"
-  | "metabase-api-key"
-  | "clickhouse-dwh"
-  | "python-runner";
