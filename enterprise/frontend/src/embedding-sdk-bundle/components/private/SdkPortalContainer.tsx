bb2e5cf0f6624d3bc744f6ae4b90ffc0f901f7bd
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-10-28 17:18:14 +0700

    Avoid portal root taking space under flex parents

--- enterprise/frontend/src/embedding-sdk-bundle/components/private/SdkPortalContainer.tsx
+++ enterprise/frontend/src/embedding-sdk-bundle/components/private/SdkPortalContainer.tsx
@@ -3,14 +3,16 @@ import { EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID } from "metabase/embedding-sdk/con
 import { Box } from "metabase/ui";
 
 import { PublicComponentStylesWrapper } from "./PublicComponentStylesWrapper";
+import S from "./SdkPortalContainer.style.css";
 
 /**
  * This is the portal container used by popovers modals etc, it is wrapped with PublicComponentStylesWrapper
  * so that it has our styles applied.
  * Mantine components needs to have the defaultProps set to use `EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID` as target for the portal
  */
+
 export const PortalContainer = () => (
-  <PublicComponentStylesWrapper>
+  <PublicComponentStylesWrapper className={S.portalWrapper}>
     <Box
       id={EMBEDDING_SDK_PORTAL_ROOT_ELEMENT_ID}
       className={ZIndex.Overlay}
