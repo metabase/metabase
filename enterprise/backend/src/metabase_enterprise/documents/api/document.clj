42375783459306bd091e74eb3ab980a3eb85abcb
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-10-30 00:09:37 +0700

    Don't check archived documents when archiving or unarchiving them

--- enterprise/backend/src/metabase_enterprise/documents/api/document.clj
+++ enterprise/backend/src/metabase_enterprise/documents/api/document.clj
@@ -197,8 +197,7 @@
    _query-params
    {:keys [name document collection_id collection_position cards] :as body} :- DocumentUpdateOptions]
   (let [existing-document (api/check-404 (get-document document-id))]
-    (api/write-check existing-document)
-    (when (not (false? (:archived body)))
+    (when-not (contains? body :archived)
       (api/check-not-archived existing-document))
     (when (api/column-will-change? :collection_id existing-document body)
       (m.document/validate-collection-move-permissions (:collection_id existing-document) collection_id))
