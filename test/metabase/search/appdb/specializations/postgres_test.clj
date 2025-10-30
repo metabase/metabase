90f5e7b976a8dd3e0803c93cb81a46df86828c4d
John Swanson <john.d.swanson@gmail.com>
2025-10-30 16:08:28 -0700

    add a test

--- test/metabase/search/appdb/specializations/postgres_test.clj
+++ test/metabase/search/appdb/specializations/postgres_test.clj
@@ -1,3 +1,13 @@
 (ns metabase.search.appdb.specializations.postgres-test
   (:require
-   [clojure.test :refer :all]))
+   [clojure.test :refer :all]
+   [metabase.search.appdb.specialization.api :as specialization]
+   [toucan2.core :as t2]))
+
+(deftest batch-upsert-test
+  (with-redefs [t2/query identity]
+    (let [query (specialization/batch-upsert! :some-table
+                                              [{:a :b}
+                                               {:b :c}])]
+      (is (= [{:a :b} {:b :c}] (:values query)))
+      (is (= {:a :excluded.a :b :excluded.b} (:do-update-set query))))))
