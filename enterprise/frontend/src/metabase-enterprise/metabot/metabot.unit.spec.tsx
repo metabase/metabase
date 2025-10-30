ed6d8b345633e6f0006b4198ee5175ba2197ce52
Sloan Sparger <sloansparger@gmail.com>
2025-10-30 13:54:28 -0500

    add unit tests

--- enterprise/frontend/src/metabase-enterprise/metabot/metabot.unit.spec.tsx
+++ enterprise/frontend/src/metabase-enterprise/metabot/metabot.unit.spec.tsx
@@ -72,16 +72,13 @@ function setup(
 
   setupEnterprisePlugins();
 
-  const {
-    ui = <Metabot />,
-    currentUser = createMockUser(),
-    metabotPluginInitialState = {
-      ...getMetabotInitialState(),
-      visible: true,
-      useStreaming: true,
-    },
-    promptSuggestions = [],
-  } = options || {};
+  const ui = options?.ui ?? <Metabot />;
+  const currentUser = options?.currentUser ?? createMockUser();
+  const metabotPluginInitialState = options?.metabotPluginInitialState ?? {
+    ...getMetabotInitialState(),
+    visible: true,
+  };
+  const promptSuggestions = options?.promptSuggestions ?? [];
 
   fetchMock.get(
     `path:/api/ee/metabot-v3/metabot/${FIXED_METABOT_IDS.DEFAULT}/prompt-suggestions`,
@@ -1020,6 +1017,58 @@ describe("metabot-streaming", () => {
         ).not.toBeInTheDocument();
       });
       expect(screen.queryByText(/xxxxxxx/)).not.toBeInTheDocument();
+    });
+  });
+
+  describe("experimental", () => {
+    describe("debug mode", () => {
+      const mockResponse = () => {
+        mockAgentEndpoint(
+          {
+            textChunks: [
+              `0:"Before"`,
+              `9:{"toolCallId":"debug_test","toolName":"debug_test","args":""}`,
+              `a:{"toolCallId":"debug_test","result":""}`,
+              `0:"After"`,
+              `d:{"finishReason":"stop","usage":{"promptTokens":4916,"completionTokens":8}}`,
+            ],
+          }, // small delay to cause loading state
+        );
+      };
+
+      it("should not show tool_call messages in chat if debug mode is disabled", async () => {
+        setup();
+        mockResponse();
+
+        await enterChatMessage("Don't show me tool call messages");
+        await assertConversation([
+          ["user", "Don't show me tool call messages"],
+          ["agent", "Before"],
+          ["agent", "After"],
+        ]);
+      });
+
+      it("should show tool_call messages in chat if debug mode is enabled", async () => {
+        setup({
+          metabotPluginInitialState: {
+            ...getMetabotInitialState(),
+            visible: true,
+            experimental: {
+              ...getMetabotInitialState().experimental,
+              debugMode: true,
+            },
+          },
+        });
+        mockResponse();
+
+        await enterChatMessage("Don't show me tool call messages");
+        await assertConversation([
+          ["user", "Don't show me tool call messages"],
+          ["agent", "Before"],
+          ["agent", "debug_test"],
+          ["agent", "After"],
+        ]);
+      });
     });
   });
 });
