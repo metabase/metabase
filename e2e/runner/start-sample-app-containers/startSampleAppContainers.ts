aee997136f7b1a35db65720986ee3082bf1b7d90
Timofei <timofei@metabase.com>
2025-10-30 15:14:01 +0400

    Debug

--- e2e/runner/start-sample-app-containers/startSampleAppContainers.ts
+++ e2e/runner/start-sample-app-containers/startSampleAppContainers.ts
@@ -43,7 +43,7 @@ export async function startSampleAppContainers(
     env,
     healthcheckPorts,
   } = setupConfig;
-  const branch = userOptions.SAMPLE_APP_BRANCH_NAME || defaultBranch;
+  const branch = defaultBranch;
 
   try {
     const { rootPath } = fetchApp({
