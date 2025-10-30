d9354378bfaf2cfe7cc69e95a8245e2ebac1b2b4
Timofei <timofei@metabase.com>
2025-10-30 19:28:06 +0400

    Handle `null` for `card`

--- src/metabase/queries/models/card.clj
+++ src/metabase/queries/models/card.clj
@@ -1099,9 +1099,9 @@
                 ;; `collection_id` and `description` can be `nil` (in order to unset them).
                 ;; Other values should only be modified if they're passed in as non-nil
                 (u/select-keys-when card-updates
-                                    :present #{:collection_id :collection_position :description :cache_ttl :archived_directly :dashboard_id :document_id}
+                                    :present #{:collection_id :collection_position :description :cache_ttl :archived_directly :dashboard_id :document_id :embedding_type}
                                     :non-nil #{:dataset_query :display :name :visualization_settings :archived
-                                               :enable_embedding :embedding_type :type :parameters :parameter_mappings :embedding_params
+                                               :enable_embedding :type :parameters :parameter_mappings :embedding_params
                                                :result_metadata :collection_preview :verified-result-metadata?}))
     ;; ok, now update dependent dashcard parameters
     (try
