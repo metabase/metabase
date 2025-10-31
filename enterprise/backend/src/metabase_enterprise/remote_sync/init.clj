458a4766289f28cdfff493784ff9bcf767c2ba32
Nathan Voxland <nathan@metabase.com>
2025-10-31 12:42:24 -0500

    Fixed module config

--- enterprise/backend/src/metabase_enterprise/remote_sync/init.clj
+++ enterprise/backend/src/metabase_enterprise/remote_sync/init.clj
@@ -1,6 +1,7 @@
 (ns metabase-enterprise.remote-sync.init
   (:require
    [metabase-enterprise.remote-sync.events]
+   [metabase-enterprise.remote-sync.settings]
    [metabase-enterprise.remote-sync.task.import]
    [metabase-enterprise.remote-sync.task.table-cleanup]))
 
