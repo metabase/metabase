657ab9590b63ef54b3a6f78889a69b547dd2a974
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-10-31 10:51:11 -0400

    Bench

--- frontend/src/metabase/bench/pages/BenchLayout/BenchNav/BenchNav.tsx
+++ frontend/src/metabase/bench/pages/BenchLayout/BenchNav/BenchNav.tsx
@@ -1,26 +1,20 @@
 import { Link } from "react-router";
 
 import * as Urls from "metabase/lib/urls";
-import { Box, Flex, Icon, type IconName } from "metabase/ui";
+import { Box, Icon, type IconName, Stack } from "metabase/ui";
 
 import S from "./BenchNav.module.css";
 
 export function BenchNav() {
   return (
-    <Flex
-      className={S.nav}
-      direction="column"
-      justify="space-between"
-      h="100%"
-      p="0.75rem"
-    >
-      <Box>
+    <Stack className={S.nav} justify="space-between" h="100%" p="0.75rem">
+      <Stack>
         <BenchNavItem icon="database" to="" />
-      </Box>
-      <Box>
+      </Stack>
+      <Stack>
         <BenchNavItem icon="schema" to={Urls.dependencyGraph()} />
-      </Box>
-    </Flex>
+      </Stack>
+    </Stack>
   );
 }
 
@@ -31,7 +25,14 @@ type BenchNavItemProps = {
 
 function BenchNavItem({ to, icon }: BenchNavItemProps) {
   return (
-    <Box className={S.item} component={Link} to={to} p="0.75rem">
+    <Box
+      className={S.item}
+      component={Link}
+      to={to}
+      display="block"
+      p="0.75rem"
+      bdrs="md"
+    >
       <Icon name={icon} />
     </Box>
   );
