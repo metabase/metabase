c5f7afae70a3013f9efe97986032fa188a615d13
lbrdnk <lbrdnk@users.noreply.github.com>
2025-11-03 15:33:50 +0100

    Fix suggested prompts schema and add test (#65299)

--- enterprise/backend/src/metabase_enterprise/metabot_v3/client.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/client.clj
@@ -290,7 +290,7 @@
   [:and
    [:map
     [:name :string]
-    [:type [:enum :number, :string, :date, :datetime, :time, :boolean, :null]]
+    [:type [:enum :number, :string, :date, :datetime, :time, :boolean, nil]]
     [:description {:optional true} [:maybe :string]]
     [:table-reference {:optional true} :string]]
    [:map {:encode/ai-service-request #(set/rename-keys % {:table-reference :table_reference})}]])
