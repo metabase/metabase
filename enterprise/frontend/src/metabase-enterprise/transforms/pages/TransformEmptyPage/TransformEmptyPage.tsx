8a3428008458f06077399b46ce61a13792539c07
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-11-03 09:14:53 -0500

    Empty states

--- enterprise/frontend/src/metabase-enterprise/transforms/pages/TransformEmptyPage/TransformEmptyPage.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/pages/TransformEmptyPage/TransformEmptyPage.tsx
@@ -1,12 +1,17 @@
 import { t } from "ttag";
 
-import { BenchEmptyState } from "metabase/bench/components/BenchEmptyState";
+import EmptyDashboardBot from "assets/img/dashboard-empty.svg";
+import EmptyState from "metabase/common/components/EmptyState";
+import { Center } from "metabase/ui";
 
 export function TransformEmptyPage() {
   return (
-    <BenchEmptyState
-      title={t`Pick a transform or create a new one`}
-      icon="transform"
-    />
+    <Center h="100%" bg="bg-light">
+      <EmptyState
+        illustrationElement={<img src={EmptyDashboardBot} />}
+        title={t`Pick a transform or create a new one`}
+        message={t`Create custom tables with transforms, and run them on a schedule.`}
+      />
+    </Center>
   );
 }
