f696a1be528de9214686eca5ca5efd4a1f88c922
metamben <103100869+metamben@users.noreply.github.com>
2025-11-05 01:46:34 +0300

    Add creator_id for transform (#65388)
    * Add creator_id for transform
    
    Closes QUE-2715

--- enterprise/backend/src/metabase_enterprise/transforms/api/transform_job.clj
+++ enterprise/backend/src/metabase_enterprise/transforms/api/transform_job.clj
@@ -148,7 +148,8 @@
   (log/info "Getting the transforms of transform job" job-id)
   (api/check-superuser)
   (api/check-404 (t2/select-one-pk :model/TransformJob :id job-id))
-  (transforms.jobs/job-transforms job-id))
+  (-> (transforms.jobs/job-transforms job-id)
+      (t2/hydrate :creator)))
 
 ;; TODO (Cam 10/28/25) -- fix this endpoint so it uses kebab-case for query parameters for consistency with the rest
 ;; of the REST API
