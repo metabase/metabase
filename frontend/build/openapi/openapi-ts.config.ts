07e7edbaf34c7f549aab86d1fb8c370602ea722f
Uladzimir Havenchyk <uladzimir@metabase.com>
2025-11-04 12:40:33 +0300

    do not clean cwd

--- frontend/build/openapi/openapi-ts.config.ts
+++ frontend/build/openapi/openapi-ts.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from "@hey-api/openapi-ts";
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   input: "./ts-types/openapi.json", // or .yaml
-  output: "frontend/src/metabase-types/openapi",
+  output: {
+    path: "frontend/src/metabase-types/openapi",
+    clean: false,
+  },
   types: {
     enums: "javascript", // This generates runtime enums!
   },
