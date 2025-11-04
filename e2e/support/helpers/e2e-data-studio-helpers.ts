21eaffb153dc51698803f1ae4d9eecadcfdc9bd5
Aleksandr Lesnenko <alxnddr@gmail.com>
2025-11-03 20:10:12 -0500

    specs

--- e2e/support/helpers/e2e-data-studio-helpers.ts
+++ e2e/support/helpers/e2e-data-studio-helpers.ts
@@ -1,4 +1,14 @@
 export const DataStudio = {
   header: () => cy.findByTestId("data-studio-header"),
   exitButton: () => DataStudio.header().findByText("Exit data studio"),
+  Transforms: {
+    content: () => cy.findByTestId("transforms-content"),
+    sidebar: () => cy.findByTestId("transforms-sidebar"),
+    saveChangesButton: () => DataStudio.Transforms.queryEditor().button("Save"),
+    queryEditor: () => cy.findByTestId("transform-query-editor"),
+  },
+  Jobs: {
+    content: () => cy.findByTestId("transforms-content"),
+    sidebar: () => cy.findByTestId("jobs-sidebar"),
+  },
 };
