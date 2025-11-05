64d30e6ffe8240195c512b7e22979ad31173a03c
Riley Thompson <rileythompson99@gmail.com>
2025-11-05 15:13:03 -0500

    Assert query works with invalid user agent

--- modules/drivers/databricks/test/metabase/driver/databricks_test.clj
+++ modules/drivers/databricks/test/metabase/driver/databricks_test.clj
@@ -269,7 +269,8 @@
       (with-redefs [config/mb-version-info {:tag "invalid agent"}]
         (sql-jdbc.conn/with-connection-spec-for-testing-connection
          [spec [:databricks (:details (mt/db))]]
-          (is (= "Metabase/invalid agent" (:UserAgentEntry spec))))))
+          (is (= "Metabase/invalid agent" (:UserAgentEntry spec)))
+          (is (= [{:a 1}] (jdbc/query spec ["select 1 as a"]))))))
     (testing "Additional options are added to :subname key of generated spec"
       (is (re-find #";IgnoreTransactions=0$"
                    (->> {:http-path "p/a/t/h",
