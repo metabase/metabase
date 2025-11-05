d04a9628b7ce66b035e7c25c82034d8f158914e3
Phoomparin Mano <phoomparin@gmail.com>
2025-11-05 23:41:21 +0700

    add EmbeddingTheme to model-set for serialization exports

--- enterprise/backend/src/metabase_enterprise/serialization/v2/extract.clj
+++ enterprise/backend/src/metabase_enterprise/serialization/v2/extract.clj
@@ -35,7 +35,10 @@
     (conj "Setting")
 
     (not (:no-transforms opts))
-    (conj "Transform" "TransformTag" "TransformJob")))
+    (conj "Transform" "TransformTag" "TransformJob")
+
+    (not (:no-embedding-themes opts))
+    (conj "EmbeddingTheme")))
 
 (defn make-targets-of-type
   "Returns a targets seq with model type and given ids"
