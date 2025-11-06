ccf3cdccfca4349a93003653950a63fc922a23fb
Dan Stone <wotbrew@gmail.com>
2025-11-06 10:58:47 +0000

    Remove transform_id from table-defaults

--- test/metabase/sync/sync_test.clj
+++ test/metabase/sync/sync_test.clj
@@ -126,8 +126,7 @@
     :archived_at false
     :deactivated_at false
     :updated_at  true
-    :owner_user_id false
-    :transform_id false}))
+    :owner_user_id false}))
 
 (defn- field-defaults []
   (merge
