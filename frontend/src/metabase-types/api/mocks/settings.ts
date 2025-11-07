d9a9f74c69e8102b0932db9a526b94999fd9c638
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-11-07 13:48:04 -0500

    test fixes

--- frontend/src/metabase-types/api/mocks/settings.ts
+++ frontend/src/metabase-types/api/mocks/settings.ts
@@ -142,7 +142,7 @@ export const createMockTokenFeatures = (
   transforms: false,
   remote_sync: false,
   "transforms-python": false,
-  "data-studio": false,
+  data_studio: false,
   ...opts,
 });
 
