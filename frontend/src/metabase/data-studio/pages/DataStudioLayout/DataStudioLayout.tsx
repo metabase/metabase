4d897184a2c5b41791a580ac575dd042ffd91b62
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-11-07 09:47:42 -0500

    types

--- frontend/src/metabase/data-studio/pages/DataStudioLayout/DataStudioLayout.tsx
+++ frontend/src/metabase/data-studio/pages/DataStudioLayout/DataStudioLayout.tsx
@@ -1,12 +1,17 @@
 import cx from "classnames";
 import type { ReactNode } from "react";
-import { Link } from "react-router";
+import { t } from "ttag";
 
+import { ForwardRefLink } from "metabase/common/components/Link";
 import { useSelector } from "metabase/lib/redux";
 import * as Urls from "metabase/lib/urls";
-import { PLUGIN_DEPENDENCIES } from "metabase/plugins";
+import {
+  PLUGIN_DEPENDENCIES,
+  PLUGIN_FEATURE_LEVEL_PERMISSIONS,
+  PLUGIN_TRANSFORMS,
+} from "metabase/plugins";
 import { getLocation } from "metabase/selectors/routing";
-import { Box, Flex, Icon, type IconName, Stack } from "metabase/ui";
+import { Box, Flex, Icon, type IconName, Stack, Tooltip } from "metabase/ui";
 
 import S from "./DataStudioLayout.module.css";
 
@@ -28,24 +33,36 @@ export function DataStudioLayout({ children }: DataStudioLayoutProps) {
 function DataStudioNav() {
   const location = useSelector(getLocation);
   const { pathname } = location;
+  const canAccessDataModel = useSelector(
+    PLUGIN_FEATURE_LEVEL_PERMISSIONS.canAccessDataModel,
+  );
+  const canAccessTransforms = useSelector(
+    PLUGIN_TRANSFORMS.canAccessTransforms,
+  );
+  const canAccessDataStructure = canAccessDataModel || canAccessTransforms;
 
   return (
     <Stack className={S.nav} h="100%" p="0.75rem" gap="0.75rem">
+      {canAccessDataStructure && (
+        <DataStudioNavItem
+          label={t`Data structure`}
+          icon="database"
+          to={Urls.dataModel()}
+          isSelected={
+            pathname.startsWith(Urls.dataModel()) ||
+            pathname.startsWith(Urls.transformList())
+          }
+        />
+      )}
       <DataStudioNavItem
-        to={Urls.dataModel()}
-        icon="database"
-        isSelected={
-          pathname.startsWith(Urls.dataModel()) ||
-          pathname.startsWith(Urls.transformList())
-        }
-      />
-      <DataStudioNavItem
-        to={Urls.dataStudioModeling()}
+        label={t`Modeling`}
         icon="model"
+        to={Urls.dataStudioModeling()}
         isSelected={pathname.startsWith(Urls.dataStudioModeling())}
       />
       {PLUGIN_DEPENDENCIES.isEnabled && (
         <DataStudioNavItem
+          label={t`Dependency graph`}
           icon="schema"
           to={Urls.dependencyGraph()}
           isSelected={pathname.startsWith(Urls.dependencyGraph())}
@@ -56,22 +73,32 @@ function DataStudioNav() {
 }
 
 type DataStudioNavItemProps = {
-  to: string;
+  label: string;
   icon: IconName;
+  to: string;
   isSelected: boolean;
 };
 
-function DataStudioNavItem({ to, icon, isSelected }: DataStudioNavItemProps) {
+const TOOLTIP_OPEN_DELAY = 1000;
+
+function DataStudioNavItem({
+  label,
+  icon,
+  to,
+  isSelected,
+}: DataStudioNavItemProps) {
   return (
-    <Box
-      className={cx(S.item, { [S.selected]: isSelected })}
-      component={Link}
-      to={to}
-      display="block"
-      p="0.75rem"
-      bdrs="md"
-    >
-      <Icon name={icon} />
-    </Box>
+    <Tooltip label={label} position="right" openDelay={TOOLTIP_OPEN_DELAY}>
+      <Box
+        className={cx(S.item, { [S.selected]: isSelected })}
+        component={ForwardRefLink}
+        to={to}
+        display="block"
+        p="0.75rem"
+        bdrs="md"
+      >
+        <Icon name={icon} />
+      </Box>
+    </Tooltip>
   );
 }
