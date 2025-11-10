d83e7d9d9c142faddfade707eb53b3fe3d65071f
Cam Saul <github@camsaul.com>
2025-11-10 13:53:35 -0800

    Let's try this

--- mage/src/mage/modules.clj
+++ mage/src/mage/modules.clj
@@ -69,35 +69,12 @@
       acc
       new-deps))))
 
-(declare unaffected-modules)
-(declare skip-driver-tests?)
-
 (defn- affected-modules
-  ([[git-ref]]
-   (let [updated    (updated-modules git-ref)
-         deps       (dependencies)
-         affected   (affected-modules deps updated)
-         unaffected (unaffected-modules deps updated)]
-     (println "These modules have changed:" (pr-str updated))
-     (println)
-     (println)
-     (println "These modules are unaffected by this change:" (pr-str unaffected))
-     (println)
-     (println)
-     (println "Driver tests" (if (skip-driver-tests? deps updated) "CAN" "CAN NOT") "be skipped.")
-     (println)
-     (println)
-     (println "You can run tests for these modules and all downstream modules as follows:")
-     (println)
-     (println)
-     (printf "clojure -X :dev:ee:ee-dev:test :only '%s'\n" (pr-str (mapv module->test-directory affected)))
-     (flush))
-   (System/exit 0))
-
-  ([deps modules]
-   (into (sorted-set)
-         (mapcat (partial indirect-dependents deps))
-         modules)))
+  "Set of modules that are direct or indirect dependents of `modules`, and thus are affected by changes to them."
+  [deps modules]
+  (into (sorted-set)
+        (mapcat (partial indirect-dependents deps))
+        modules))
 
 (defn- unaffected-modules
   "Return the set of modules that are unaffected "
@@ -109,8 +86,64 @@
 (comment
   (unaffected-modules (dependencies) '#{enterprise/billing}))
 
-(defn- skip-driver-tests?
-  [deps modules]
+(defn- skip-driver-tests? [deps modules]
   (let [unaffected (unaffected-modules deps modules)]
-    (and (contains? unaffected 'driver)
-         (contains? unaffected 'driver-api))))
+    (contains? unaffected 'driver)))
+
+(defn cli-print-affected-modules
+  [[git-ref, :as _command-line-args]]
+  (let [deps       (dependencies)
+        updated    (updated-modules git-ref)
+        affected   (affected-modules deps updated)
+        unaffected (unaffected-modules deps updated)]
+    (println "These modules have changed:" (pr-str updated))
+    (println)
+    (println)
+    (println "These modules are unaffected by this change:" (pr-str unaffected))
+    (println)
+    (println)
+    (println "Driver tests" (if (skip-driver-tests? deps updated) "CAN" "CAN NOT") "be skipped.")
+    (println)
+    (println)
+    (println "You can run tests for these modules and all downstream modules as follows:")
+    (println)
+    (println)
+    (printf "clojure -X :dev:ee:ee-dev:test :only '%s'\n" (pr-str (mapv module->test-directory affected)))
+    (flush))
+  (System/exit 0))
+
+(defn- always-run-driver-tests?
+  [git-ref]
+  (some (fn [filename]
+          (when (or (str/includes? filename "deps.edn")
+                    (str/includes? filename "modules/drivers/"))
+            (printf "Running driver tests because %s was changed\n" (pr-str filename))
+            (flush)
+            filename))
+        (u/updated-files (or git-ref "master"))))
+
+(defn cli-can-skip-driver-tests
+  "Exits with nonzero status code if we can skip "
+  [[git-ref, :as _command-line-args]]
+  (try
+    (let [deps        (dependencies)
+          git-ref     (or git-ref "master")
+          updated     (updated-modules git-ref)
+          unaffected  (unaffected-modules deps updated)
+          skip-tests? (skip-driver-tests? deps updated)]
+      ;; Not strictly necessary, but people looking at CI will appreciate having this extra info.
+      (println "These modules have changed:" (pr-str updated))
+      (println)
+      (println)
+      (println "These modules are unaffected by this change:" (pr-str unaffected))
+      (println)
+      (println)
+      (println "Driver tests" (if skip-tests? "CAN" "CAN NOT") "be skipped.")
+      (System/exit ^Long (cond
+                           (not skip-tests?)                  0
+                           (always-run-driver-tests? git-ref) 0
+                           :else                              1)))
+    ;; fail closed -- if code above barfs then still exit with status code zero (do not skip)
+    (catch Throwable e
+      (println "Error determining whether we can skip driver tests:\n" e)
+      (System/exit 0))))
