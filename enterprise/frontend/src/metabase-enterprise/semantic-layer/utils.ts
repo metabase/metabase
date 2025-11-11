5630234dac6d0b778d98daecb0162244a066fb03
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-11-11 15:01:50 -0500

    Simplify nav

--- enterprise/frontend/src/metabase-enterprise/semantic-layer/utils.ts
+++ enterprise/frontend/src/metabase-enterprise/semantic-layer/utils.ts
@@ -1,15 +1,5 @@
-import type { SemanticLayerCollectionType } from "metabase/plugins";
 import type { Collection } from "metabase-types/api";
 
 export function isSemanticLayerCollection({ type }: Partial<Collection>) {
   return type === "semantic-layer";
-}
-
-export function getSemanticLayerCollectionType({
-  type,
-}: Partial<Collection>): SemanticLayerCollectionType | undefined {
-  if (type !== "semantic-layer") {
-    return undefined;
-  }
-  return "root";
 }
