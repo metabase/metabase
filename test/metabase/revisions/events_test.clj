c2767e0fe586affe04743d37c13bb619ddcf6798
Tamás Benkő <tamas@metabase.com>
2025-11-12 00:56:38 +0300

    Fix revision events test

--- test/metabase/revisions/events_test.clj
+++ test/metabase/revisions/events_test.clj
@@ -275,21 +275,26 @@
       (events/publish-event! :event/segment-create {:object segment :user-id (mt/user->id :rasta)})
       (let [revision (-> (t2/select-one :model/Revision :model "Segment", :model_id (:id segment))
                          (select-keys [:model :user_id :object :is_reversion :is_creation :message]))]
-        (is (= {:model        "Segment"
-                :user_id      (mt/user->id :rasta)
-                :object       {:name                    "Toucans in the rainforest"
-                               :description             "Lookin' for a blueberry"
-                               :show_in_getting_started false
-                               :caveats                 nil
-                               :points_of_interest      nil
-                               :entity_id               (:entity_id segment)
-                               :archived                false
-                               :creator_id              (mt/user->id :rasta)
-                               :definition              {:filter [:= [:field 2 nil] "value"]}}
-                :is_reversion false
-                :is_creation  true
-                :message      nil}
-               (assoc revision :object (dissoc (:object revision) :id :table_id))))))))
+        (is (=? {:model        "Segment"
+                 :user_id      (mt/user->id :rasta)
+                 :object       {:name                    "Toucans in the rainforest"
+                                :description             "Lookin' for a blueberry"
+                                :show_in_getting_started false
+                                :caveats                 nil
+                                :points_of_interest      nil
+                                :entity_id               (:entity_id segment)
+                                :archived                false
+                                :creator_id              (mt/user->id :rasta)
+                                :definition              {:lib/type :mbql/query,
+                                                          :stages
+                                                          [{:lib/type :mbql.stage/mbql
+                                                            :source-table id
+                                                            :filters [[:= {} [:field {} 2] "value"]]}]
+                                                          :database database-id}}
+                 :is_reversion false
+                 :is_creation  true
+                 :message      nil}
+                (assoc revision :object (dissoc (:object revision) :id :table_id))))))))
 
 (deftest segment-update-test
   (testing :event/segment-update
@@ -301,24 +306,28 @@
                              (assoc {:object segment}
                                     :revision-message "updated"
                                     :user-id (mt/user->id :crowberto)))
-      (is (= {:model        "Segment"
-              :user_id      (mt/user->id :crowberto)
-              :object       {:name                    "Toucans in the rainforest"
-                             :description             "Lookin' for a blueberry"
-                             :show_in_getting_started false
-                             :caveats                 nil
-                             :points_of_interest      nil
-                             :entity_id               (:entity_id segment)
-                             :archived                false
-                             :creator_id              (mt/user->id :rasta)
-                             :definition              {:filter [:= [:field 2 nil] "value"]}}
-              :is_reversion false
-              :is_creation  false
-              :message      "updated"}
-             (update (t2/select-one [:model/Revision :model :user_id :object :is_reversion :is_creation :message]
-                                    :model "Segment"
-                                    :model_id (:id segment))
-                     :object dissoc :id :table_id))))))
+      (is (=? {:model        "Segment"
+               :user_id      (mt/user->id :crowberto)
+               :object       {:name                    "Toucans in the rainforest"
+                              :description             "Lookin' for a blueberry"
+                              :show_in_getting_started false
+                              :caveats                 nil
+                              :points_of_interest      nil
+                              :entity_id               (:entity_id segment)
+                              :archived                false
+                              :creator_id              (mt/user->id :rasta)
+                              :definition              {:lib/type :mbql/query,
+                                                        :stages [{:lib/type :mbql.stage/mbql
+                                                                  :source-table id
+                                                                  :filters [[:= {} [:field {} 2] "value"]]}]
+                                                        :database database-id}}
+               :is_reversion false
+               :is_creation  false
+               :message      "updated"}
+              (update (t2/select-one [:model/Revision :model :user_id :object :is_reversion :is_creation :message]
+                                     :model "Segment"
+                                     :model_id (:id segment))
+                      :object dissoc :id :table_id))))))
 
 (deftest segment-delete-test
   (testing :event/segment-delete
@@ -328,21 +337,26 @@
                                                       :definition {:filter [:= [:field 2 nil] "value"]}
                                                       :archived   true}]
       (events/publish-event! :event/segment-delete {:object segment :user-id (mt/user->id :rasta)})
-      (is (= {:model        "Segment"
-              :user_id      (mt/user->id :rasta)
-              :object       {:name                    "Toucans in the rainforest"
-                             :description             "Lookin' for a blueberry"
-                             :show_in_getting_started false
-                             :caveats                 nil
-                             :points_of_interest      nil
-                             :entity_id               (:entity_id segment)
-                             :archived                true
-                             :creator_id              (mt/user->id :rasta)
-                             :definition              {:filter [:= [:field 2 nil] "value"]}}
-              :is_reversion false
-              :is_creation  false
-              :message      nil}
-             (update (t2/select-one [:model/Revision :model :user_id :object :is_reversion :is_creation :message]
-                                    :model "Segment"
-                                    :model_id (:id segment))
-                     :object dissoc :id :table_id))))))
+      (is (=? {:model        "Segment"
+               :user_id      (mt/user->id :rasta)
+               :object       {:name                    "Toucans in the rainforest"
+                              :description             "Lookin' for a blueberry"
+                              :show_in_getting_started false
+                              :caveats                 nil
+                              :points_of_interest      nil
+                              :entity_id               (:entity_id segment)
+                              :archived                true
+                              :creator_id              (mt/user->id :rasta)
+                              :definition              {:lib/type :mbql/query,
+                                                        :stages
+                                                        [{:lib/type :mbql.stage/mbql
+                                                          :source-table id
+                                                          :filters [[:= {} [:field {} 2] "value"]]}]
+                                                        :database database-id}}
+               :is_reversion false
+               :is_creation  false
+               :message      nil}
+              (update (t2/select-one [:model/Revision :model :user_id :object :is_reversion :is_creation :message]
+                                     :model "Segment"
+                                     :model_id (:id segment))
+                      :object dissoc :id :table_id))))))
