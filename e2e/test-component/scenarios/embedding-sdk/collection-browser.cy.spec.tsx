be8f81a80be452f35a1b4ac57d791f431d74da0c
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-11-11 16:07:42 +0700

    Update the delete button copy for better UX

--- e2e/test-component/scenarios/embedding-sdk/collection-browser.cy.spec.tsx
+++ e2e/test-component/scenarios/embedding-sdk/collection-browser.cy.spec.tsx
@@ -80,11 +80,11 @@ describe("scenarios > embedding-sdk > collection browser", () => {
         cy.findByText("Our analytics").should("exist");
         cy.findByText("Orders in a dashboard").should("exist");
 
-        cy.log("delete the dashboard");
+        cy.log("move the dashboard to trash");
         cy.findByText(dashboardName).closest("tr").button("Actions").click();
       });
 
-      H.popover().findByRole("menuitem", { name: "Delete" }).click();
+      H.popover().findByRole("menuitem", { name: "Move to trash" }).click();
 
       cy.log("the deleted dashboard should be gone");
       getSdkRoot().findByText(dashboardName).should("not.exist");
