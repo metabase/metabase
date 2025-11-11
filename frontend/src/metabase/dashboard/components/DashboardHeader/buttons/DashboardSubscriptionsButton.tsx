e46bb473d0b999fc336668edf03669f6a32d2a8f
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-11-11 22:32:04 +0700

    Hide the subscription when email hasn't been set up

--- frontend/src/metabase/dashboard/components/DashboardHeader/buttons/DashboardSubscriptionsButton.tsx
+++ frontend/src/metabase/dashboard/components/DashboardHeader/buttons/DashboardSubscriptionsButton.tsx
@@ -1,5 +1,6 @@
 import { t } from "ttag";
 
+import { useHasEmailSetup } from "metabase/common/hooks";
 import { toggleSharing } from "metabase/dashboard/actions";
 import { useDispatch } from "metabase/lib/redux";
 import { ActionIcon, Icon, Tooltip } from "metabase/ui";
@@ -11,6 +12,13 @@ export const DashboardSubscriptionsButton = (
   const tooltipLabel = t`Subscriptions`;
   const dispatch = useDispatch();
   const handleClick = () => dispatch(toggleSharing());
+
+  // We decided not to show the subscriptions button if email is not set up
+  const hasEmailSetup = useHasEmailSetup();
+  if (!hasEmailSetup) {
+    return null;
+  }
+
   return (
     <Tooltip label={tooltipLabel}>
       <ActionIcon onClick={handleClick} aria-label={tooltipLabel} {...props}>
