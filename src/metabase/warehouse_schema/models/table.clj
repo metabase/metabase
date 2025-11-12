1b221e1758887722c54e9925e7bf6910a11fbb3e
Ngoc Khuat <qn.khuat@gmail.com>
2025-11-12 23:23:37 +0900

    reorder

--- src/metabase/warehouse_schema/models/table.clj
+++ src/metabase/warehouse_schema/models/table.clj
@@ -457,8 +457,7 @@
   {:copy      [:name :description :entity_type :active :display_name :visibility_type :schema
                :points_of_interest :caveats :show_in_getting_started :field_order :initial_sync_status :is_upload
                :database_require_filter :is_defective_duplicate :unique_table_helper :is_writable :data_authority
-               :data_source :owner_email :owner_user_id
-               :data_layer]
+               :data_source :data_layer :owner_email :owner_user_id]
    :skip      [:estimated_row_count :view_count]
    :transform {:created_at (serdes/date)
                :archived_at (serdes/date)
