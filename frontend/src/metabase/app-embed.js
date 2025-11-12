62fe686ffc5ea27839b4f0920810d02aebf3385a
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-11-12 22:29:45 +0700

    Fix static embedding or public embed outside iframe not working with #theme (#65740)
    * Fix static embedding or public embed outside iframe not working with #theme
    
    * Add tests
    
    * Make embedding prefix/suffix naming consistent

--- frontend/src/metabase/app-embed.js
+++ frontend/src/metabase/app-embed.js
@@ -3,8 +3,12 @@
  * file 'LICENSE-EMBEDDING.txt', which is part of this source code package.
  */
 
+import { setIsStaticEmbedding } from "metabase/embedding/config";
+
 import { init } from "./app";
 import { publicReducers } from "./reducers-public";
 import { getRoutes } from "./routes-embed";
+
+setIsStaticEmbedding();
 
 init(publicReducers, getRoutes, () => {});
