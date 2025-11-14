145fcb2683b5c45a51e424d0aa745fab3ba93eca
Fraser <fraser@metabase.com>
2025-11-14 16:08:22 +1300

    Refactor QuestionRowCount

--- frontend/src/metabase/query_builder/components/view/QuestionRowCount/QuestionRowCount.tsx
+++ frontend/src/metabase/query_builder/components/view/QuestionRowCount/QuestionRowCount.tsx
@@ -1,9 +1,8 @@
 import cx from "classnames";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { t } from "ttag";
 import _ from "underscore";
 
-import PopoverWithTrigger from "metabase/common/components/PopoverWithTrigger";
 import {
   type NumberFormatter,
   useNumberFormatter,
@@ -20,7 +19,7 @@ import {
   getIsResultDirty,
   getQuestion,
 } from "metabase/query_builder/selectors";
-import { Box } from "metabase/ui";
+import { Box, Popover, UnstyledButton } from "metabase/ui";
 import type { Limit } from "metabase-lib";
 import * as Lib from "metabase-lib";
 import type Question from "metabase-lib/v1/Question";
@@ -78,6 +77,7 @@ function QuestionRowCount({
   className,
   onChangeLimit,
 }: QuestionRowCountProps) {
+  const [opened, setOpened] = useState(false);
   const { isEditable, isNative } = Lib.queryDisplayInfo(question.query());
   const formatNumber = useNumberFormatter();
   const message = useMemo(() => {
@@ -101,31 +101,46 @@ function QuestionRowCount({
     return null;
   }
 
+  if (!canChangeLimit) {
+    return (
+      <RowCountLabel
+        className={className}
+        data-testid="question-row-count"
+        highlighted={limit != null}
+        disabled={!canChangeLimit}
+      >
+        {message}
+      </RowCountLabel>
+    );
+  }
+
   return (
-    <PopoverWithTrigger
-      triggerElement={
-        <RowCountLabel
-          className={className}
-          data-testid="question-row-count"
-          highlighted={limit != null}
-          disabled={!canChangeLimit}
+    <Popover opened={opened} onChange={setOpened} position="bottom-start">
+      <Popover.Target>
+        <UnstyledButton
+          onClick={() => setOpened(!opened)}
+          id={POPOVER_ID}
+          aria-haspopup="dialog"
         >
-          {message}
-        </RowCountLabel>
-      }
-      id={POPOVER_ID}
-      aria-role="dialog"
-      disabled={!canChangeLimit}
-    >
-      {({ onClose }: { onClose: () => void }) => (
+          <RowCountLabel
+            className={className}
+            data-testid="question-row-count"
+            highlighted={limit != null}
+            disabled={false}
+          >
+            {message}
+          </RowCountLabel>
+        </UnstyledButton>
+      </Popover.Target>
+      <Popover.Dropdown>
         <LimitPopover
           className={CS.p2}
           limit={limit}
           onChangeLimit={handleLimitChange}
-          onClose={onClose}
+          onClose={() => setOpened(false)}
         />
-      )}
-    </PopoverWithTrigger>
+      </Popover.Dropdown>
+    </Popover>
   );
 }
 
@@ -141,26 +156,21 @@ function RowCountLabel({
 }) {
   const label = t`Row count`;
   const { highlighted, ...propsForChild } = props;
-  return disabled ? (
+  return (
     <Box
       component="span"
-      className={cx(QuestionRowCountS.RowCountStaticLabel, className)}
-      {...propsForChild}
-      aria-label={label}
-    />
-  ) : (
-    <button
       className={cx(
-        QuestionRowCountS.RowCountButton,
+        disabled
+          ? QuestionRowCountS.RowCountStaticLabel
+          : QuestionRowCountS.RowCountButton,
         {
-          [QuestionRowCountS.isHighlighted]: highlighted,
+          [QuestionRowCountS.isHighlighted]: !disabled && highlighted,
         },
         className,
       )}
       {...propsForChild}
       aria-label={label}
-      aria-haspopup="dialog"
-      aria-controls={POPOVER_ID}
+      aria-controls={disabled ? undefined : POPOVER_ID}
     />
   );
 }
