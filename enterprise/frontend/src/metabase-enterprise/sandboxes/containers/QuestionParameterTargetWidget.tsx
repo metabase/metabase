23a5d4de64989a4c8b624728804c1c1c0416b196
Fraser <fraser@metabase.com>
2025-11-17 17:08:21 +1300

    Simplify Trigger

--- enterprise/frontend/src/metabase-enterprise/sandboxes/containers/QuestionParameterTargetWidget.tsx
+++ enterprise/frontend/src/metabase-enterprise/sandboxes/containers/QuestionParameterTargetWidget.tsx
@@ -1,45 +1,28 @@
-import cx from "classnames";
-import type { ReactNode } from "react";
 import { useState } from "react";
 
 import { QuestionLoaderHOC } from "metabase/common/components/QuestionLoader";
 import SelectButton from "metabase/common/components/SelectButton";
 import { ParameterTargetList } from "metabase/parameters/components/ParameterTargetList";
-import type { ParameterMappingOption } from "metabase/parameters/utils/mapping-options";
 import {
   getMappingOptionByTarget,
   getParameterMappingOptions,
 } from "metabase/parameters/utils/mapping-options";
-import { Popover, UnstyledButton } from "metabase/ui";
+import { Popover } from "metabase/ui";
 import type Question from "metabase-lib/v1/Question";
 import type { ParameterTarget } from "metabase-types/api";
-
-type ChildrenRenderProps = {
-  selected: ParameterMappingOption | undefined;
-  disabled: boolean;
-  placeholder?: string;
-};
 
 type QuestionParameterTargetWidgetProps = {
   question: Question;
   target?: ParameterTarget | null;
   onChange: (target: ParameterTarget) => void;
   placeholder?: string;
-  children?: ((props: ChildrenRenderProps) => ReactNode) | ReactNode;
 };
-
-const defaultChildren = ({ selected, placeholder }: ChildrenRenderProps) => (
-  <SelectButton hasValue={!!selected} className="border-med">
-    {selected ? selected.name : placeholder || "Select a target"}
-  </SelectButton>
-);
 
 function QuestionParameterTargetWidgetInner({
   question,
   target,
   onChange,
   placeholder,
-  children = defaultChildren,
 }: QuestionParameterTargetWidgetProps) {
   const [opened, setOpened] = useState(false);
 
@@ -50,26 +33,22 @@ function QuestionParameterTargetWidgetInner({
   const disabled = mappingOptions.length === 0;
   const selected = getMappingOptionByTarget(mappingOptions, target, question);
 
-  const triggerElement =
-    typeof children === "function"
-      ? children({ selected, disabled, placeholder })
-      : children;
-
   return (
     <Popover opened={opened} onChange={setOpened} disabled={disabled}>
       <Popover.Target>
-        <UnstyledButton
+        <SelectButton
+          hasValue={!!selected}
+          className="border-med"
           onClick={() => setOpened(!opened)}
           disabled={disabled}
-          className={cx({ disabled })}
         >
-          {triggerElement}
-        </UnstyledButton>
+          {selected ? selected.name : placeholder}
+        </SelectButton>
       </Popover.Target>
       <Popover.Dropdown>
         <ParameterTargetList
-          onChange={(target) => {
-            onChange(target);
+          onChange={(newTarget) => {
+            onChange(newTarget);
             setOpened(false);
           }}
           mappingOptions={mappingOptions}
