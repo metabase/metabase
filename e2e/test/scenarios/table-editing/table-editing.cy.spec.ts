0621964df015ef06e11412ae33288ff3d79122f9
Stas Gavrylov <stasgavrylov@gmail.com>
2025-11-18 13:50:15 +0100

    Update table editing e2e tests to include different column types

--- e2e/test/scenarios/table-editing/table-editing.cy.spec.ts
+++ e2e/test/scenarios/table-editing/table-editing.cy.spec.ts
@@ -3,6 +3,8 @@
  * Do not modify SAMPLE_DB data to test any table editing features.
  * It is used in multiple tests and any changes will break them.
  */
+import dayjs from "dayjs";
+
 import {
   SAMPLE_DB_ID,
   USER_GROUPS,
@@ -12,14 +14,14 @@ import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import { resetSnowplow } from "e2e/support/helpers/e2e-snowplow-helpers";
 import { DataPermissionValue } from "metabase/admin/permissions/types";
 
-const EDITABLE_TABLE_NAME = "many_data_types";
-const EDITABLE_TABLE_NAME_REGEX = new RegExp("Many Data Types", "i");
-const { ALL_USERS_GROUP } = USER_GROUPS;
-const DEFAULT_FIELD = "UUID";
-
 const { H } = cy;
-
+const { ALL_USERS_GROUP } = USER_GROUPS;
 const { ORDERS_ID, PRODUCTS_ID, ORDERS, PRODUCTS } = SAMPLE_DATABASE;
+
+const EDITABLE_SOURCE_TABLE_NAME = "many_data_types";
+const EDITABLE_SOURCE_TABLE_NAME_REGEX = new RegExp("Many Data Types", "i");
+const INLINE_EDIT_TEST_TABLE_NAME = "editing_test";
+const DEFAULT_FIELD = "UUID";
 
 describe("scenarios > table-editing", () => {
   beforeEach(() => {
@@ -28,7 +30,7 @@ describe("scenarios > table-editing", () => {
     H.restore("postgres-writable");
     H.resetTestTable({
       type: "postgres",
-      table: EDITABLE_TABLE_NAME,
+      table: EDITABLE_SOURCE_TABLE_NAME,
     });
 
     cy.signInAsAdmin();
@@ -43,7 +45,7 @@ describe("scenarios > table-editing", () => {
 
     H.resyncDatabase({
       dbId: WRITABLE_DB_ID,
-      tableName: EDITABLE_TABLE_NAME,
+      tableName: EDITABLE_SOURCE_TABLE_NAME,
     });
 
     H.activateToken("bleeding-edge");
@@ -56,22 +58,22 @@ describe("scenarios > table-editing", () => {
 
   it("should show edit icon on table browser", () => {
     openTableBrowser();
-    getTableEditIcon(EDITABLE_TABLE_NAME_REGEX).should("be.visible");
+    getTableEditIcon(EDITABLE_SOURCE_TABLE_NAME_REGEX).should("be.visible");
   });
 
   it("should not show edit icon on table browser if user is not admin", () => {
     cy.signInAsNormalUser();
 
     openTableBrowser();
-    getTableEditIcon(EDITABLE_TABLE_NAME_REGEX).should("not.exist");
+    getTableEditIcon(EDITABLE_SOURCE_TABLE_NAME_REGEX).should("not.exist");
   });
 
   it("should allow to open table data edit mode", () => {
     openTableBrowser();
-    openTableEdit(EDITABLE_TABLE_NAME_REGEX);
+    openTableEdit(EDITABLE_SOURCE_TABLE_NAME_REGEX);
 
     H.getTableId({
-      name: EDITABLE_TABLE_NAME,
+      name: EDITABLE_SOURCE_TABLE_NAME,
     }).then((tableId) => {
       H.expectUnstructuredSnowplowEvent({
         event: "edit_data_button_clicked",
@@ -85,7 +87,7 @@ describe("scenarios > table-editing", () => {
     cy.findByTestId("edit-table-data-root")
       .should("be.visible")
       .within(() => {
-        cy.findByText(EDITABLE_TABLE_NAME_REGEX).should("be.visible");
+        cy.findByText(EDITABLE_SOURCE_TABLE_NAME_REGEX).should("be.visible");
         cy.findByTestId("head-crumbs-container")
           .findByText("Edit")
           .should("be.visible");
@@ -94,7 +96,7 @@ describe("scenarios > table-editing", () => {
       });
 
     cy.findByTestId("head-crumbs-container")
-      .findByText(EDITABLE_TABLE_NAME_REGEX)
+      .findByText(EDITABLE_SOURCE_TABLE_NAME_REGEX)
       .click();
 
     cy.findByTestId("query-builder-root").should("be.visible");
@@ -104,7 +106,7 @@ describe("scenarios > table-editing", () => {
     it("should not allow to open table data view by default", () => {
       setTableEditingEnabledForDB(WRITABLE_DB_ID, false);
       openTableBrowser();
-      getTableEditIcon(EDITABLE_TABLE_NAME_REGEX).should("not.exist");
+      getTableEditIcon(EDITABLE_SOURCE_TABLE_NAME_REGEX).should("not.exist");
     });
   });
 
@@ -116,7 +118,7 @@ describe("scenarios > table-editing", () => {
     it("should not allow to open table data view", () => {
       H.getTableId({
         databaseId: WRITABLE_DB_ID,
-        name: EDITABLE_TABLE_NAME,
+        name: EDITABLE_SOURCE_TABLE_NAME,
       }).then((tableId) => {
         cy.visit(`/browse/databases/${WRITABLE_DB_ID}/tables/${tableId}/edit`);
         cy.findByTestId("edit-table-data-restricted").should("be.visible");
@@ -128,10 +130,31 @@ describe("scenarios > table-editing", () => {
     beforeEach(() => {
       resetSnowplow();
 
+      H.queryWritableDB(
+        `CREATE TABLE IF NOT EXISTS ${INLINE_EDIT_TEST_TABLE_NAME} AS SELECT id, uuid, integer, tinyint, string, date, datetime, boolean FROM ${EDITABLE_SOURCE_TABLE_NAME}`,
+        "postgres",
+      );
+      H.resyncDatabase({ dbId: WRITABLE_DB_ID });
+
       H.getTableId({
         databaseId: WRITABLE_DB_ID,
-        name: EDITABLE_TABLE_NAME,
+        name: INLINE_EDIT_TEST_TABLE_NAME,
       }).then((tableId) => {
+        /*
+          Adjusting "string" column type.
+          By default it is set to "type/Category" due to only 2 unique values in test dataset.
+          This causes to "string" column cell rendering a dropdown instead of an input field
+          when editing.
+        */
+        H.getFieldId({
+          tableId,
+          name: "string",
+        }).then((fieldId) => {
+          cy.request("PUT", `/api/field/${fieldId}`, {
+            semantic_type: null,
+          });
+        });
+
         cy.intercept("GET", `/api/table/${tableId}/query_metadata`).as(
           "getDataTable",
         );
@@ -243,7 +266,7 @@ describe("scenarios > table-editing", () => {
       H.undoToast().findByText("Successfully updated").should("be.visible");
       H.getTableId({
         databaseId: WRITABLE_DB_ID,
-        name: EDITABLE_TABLE_NAME,
+        name: INLINE_EDIT_TEST_TABLE_NAME,
       }).then((tableId) => {
         H.expectUnstructuredSnowplowEvent({
           event: "edit_data_record_modified",
@@ -256,23 +279,21 @@ describe("scenarios > table-editing", () => {
     });
 
     describe("inline cell editing", () => {
-      /**
-       * We test only a couple of columns which are visible on initial table rendering
-       * to avoid flakiness, since other columns are rendered lazily and are not
-       * present in the DOM right away. We'd have to scroll and look for them.
-       * Overall, testing different cell types editing should be done in scope
-       * of unit tests.
-       */
       const cases = [
         {
           dataType: "integer",
           column: "integer",
-          value: Math.floor(Math.random() * 1000),
+          value: Math.floor(Math.random() * 10000),
         },
         {
           dataType: "tinyint",
           column: "tinyint",
           value: Math.floor(Math.random() * 256),
+        },
+        {
+          dataType: "string",
+          column: "string",
+          value: "test",
         },
       ];
 
@@ -307,7 +328,7 @@ describe("scenarios > table-editing", () => {
 
           H.getTableId({
             databaseId: WRITABLE_DB_ID,
-            name: EDITABLE_TABLE_NAME,
+            name: INLINE_EDIT_TEST_TABLE_NAME,
           }).then((tableId) => {
             H.expectUnstructuredSnowplowEvent({
               event: "edit_data_record_modified",
@@ -320,6 +341,100 @@ describe("scenarios > table-editing", () => {
 
           H.undoToast().findByText("Successfully updated").should("be.visible");
         });
+      });
+
+      it("should allow to edit a cell with date type", () => {
+        cy.findByTestId("table-root")
+          .findAllByRole("row")
+          .eq(1)
+          .within(() => {
+            cy.get("[data-column-id='date']").as("targetCell").click({
+              scrollBehavior: false,
+            });
+          });
+
+        const day = Math.floor(Math.random() * 10) + 10; // 10-20
+
+        H.popover().within(() => {
+          cy.findByRole("button", { name: `${day} February 2020` }).click();
+        });
+
+        cy.wait("@updateTableData").then(({ response }) => {
+          const targetDate = dayjs(new Date(2020, 1, day)).format("YYYY-MM-DD");
+          const responseDate = dayjs(response?.body.outputs[0].row.date).format(
+            "YYYY-MM-DD",
+          );
+
+          expect(responseDate).to.equal(targetDate);
+        });
+
+        H.undoToast().findByText("Successfully updated").should("be.visible");
+      });
+
+      it("should allow to edit a cell with datetime type", () => {
+        cy.findByTestId("table-root")
+          .findAllByRole("row")
+          .eq(1)
+          .within(() => {
+            cy.get("[data-column-id='datetime']").as("targetCell").click({
+              scrollBehavior: false,
+            });
+          });
+
+        const day = Math.floor(Math.random() * 10) + 10; // 10-20
+        const hour = Math.floor(Math.random() * 12);
+        const minute = Math.floor(Math.random() * 60);
+
+        H.popover().within(() => {
+          cy.findByRole("button", { name: `${day} February 2020` }).click();
+          cy.findAllByRole("spinbutton").eq(0).type(hour.toString());
+          cy.findAllByRole("spinbutton").eq(1).type(minute.toString());
+          cy.get('select[data-am-pm="true"]').as("ampmSelect");
+          cy.get("@ampmSelect").select("AM");
+          // It's safe to click the last button because we're in the popover
+          // eslint-disable-next-line no-unsafe-element-filtering
+          cy.findAllByRole("button").last().click();
+        });
+
+        cy.wait("@updateTableData").then(({ response, request }) => {
+          const targetDate = dayjs(
+            new Date(2020, 1, day, hour, minute, 0),
+          ).format("YYYY-MM-DDTHH:mm:ss");
+
+          const requestDate = request.body.params.datetime;
+          const responseDate = response?.body.outputs[0].row.datetime;
+
+          // Check if request date matches the response date.
+          // In theory FE should preserve the initial input date timezone offset (might be based on the CI environment)
+          expect(requestDate).to.equal(responseDate);
+
+          // Check if date param matches the input date (without timezone offset at offset 19)
+          expect(requestDate.slice(0, 19)).to.equal(targetDate);
+        });
+
+        H.undoToast().findByText("Successfully updated").should("be.visible");
+      });
+
+      it("should allow to edit a cell with select type", () => {
+        cy.findByTestId("table-root")
+          .findAllByRole("row")
+          .eq(1)
+          .within(() => {
+            cy.get("[data-column-id='boolean']").as("targetCell").click({
+              scrollBehavior: false,
+            });
+          });
+
+        H.popover().within(() => {
+          // 3: true, false, null
+          cy.findAllByRole("option").should("have.length", 3).eq(0).click();
+
+          cy.wait("@updateTableData").then(({ response }) => {
+            expect(response?.body.outputs[0].op).to.equal("updated");
+          });
+        });
+
+        H.undoToast().findByText("Successfully updated").should("be.visible");
       });
 
       it("should not allow to edit PK cells", () => {
