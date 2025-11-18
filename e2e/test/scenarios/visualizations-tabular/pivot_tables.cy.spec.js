29e2e3e6e94cc04a6cf655b912e9fd7bfe1e7fec
Timofei <timofei@metabase.com>
2025-11-18 17:41:46 +0400

    Fix pivot_tables.cy.spec.js e2e

--- e2e/test/scenarios/visualizations-tabular/pivot_tables.cy.spec.js
+++ e2e/test/scenarios/visualizations-tabular/pivot_tables.cy.spec.js
@@ -652,6 +652,7 @@ describe("scenarios > visualizations > pivot tables", { tags: "@slow" }, () => {
         H.visitQuestion(card_id);
 
         cy.wrap(card_id).as("questionId");
+        cy.wrap(dashboard_id).as("dashboardId");
       });
     });
 
@@ -691,12 +692,24 @@ describe("scenarios > visualizations > pivot tables", { tags: "@slow" }, () => {
             });
           }
 
-          cy.get("@questionId").then((questionId) => {
-            H.openLegacyStaticEmbeddingModal({
+          const { alias, resource } = {
+            question: {
+              alias: "@questionId",
               resource: "question",
-              resourceId: questionId,
+            },
+            dashboard: {
+              alias: "@dashboardId",
+              resource: "dashboard",
+            },
+          }[test.case];
+
+          cy.get(alias).then((id) => {
+            H.openLegacyStaticEmbeddingModal({
+              resource: resource,
+              resourceId: id,
               activeTab: "parameters",
               confirmSave: test.confirmSave,
+              unpublishBeforeOpen: false,
             });
           });
 
