197f19baa0191d7bd609c68f1b03218f7000fa97
Anderson <ands.alves.nunes@gmail.com>
2025-11-18 10:41:45 -0300

    Make sure 'Pull changes' button is not truncated on remote sync settings (#65932)

--- enterprise/frontend/src/metabase-enterprise/remote_sync/components/RemoteSyncAdminSettings/RemoteSyncAdminSettings.tsx
+++ enterprise/frontend/src/metabase-enterprise/remote_sync/components/RemoteSyncAdminSettings/RemoteSyncAdminSettings.tsx
@@ -274,6 +274,7 @@ export const RemoteSyncAdminSettings = () => {
                           <FormTextInput
                             name={BRANCH_KEY}
                             label={t`Sync branch`}
+                            mr={0}
                             placeholder="main"
                             {...getEnvSettingProps(
                               settingDetails?.[BRANCH_KEY],
