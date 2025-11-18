d981e64f0a76afe32b2260d14f0cf03a608768c2
Nicolò Pretto <info@npretto.com>
2025-11-18 15:56:43 +0100

    move sdk bundle code to the oss folder (#65753)
    * move the actual sdk folders
    
    * update rspack config and eslint config
    
    * fix relative import in CommonSdkStoryWrapper
    
    * allow default exports in preview.tsx
    
    * update eslint config to allow imports from frontend/src/embedding-sdk-shared
    
    * remove legacy eslint disable directive
    
    * temporarily 'disable' eslint-disable-next-line no-external-references-for-sdk-package-code that has not been ported yet
    
    * update rspack config to use new paths
    
    * update jest config to use new paths
    
    * add `embedding-sdk-package` to restricted imports
    
    * copy eslint files to apply to the new sdk folders
    
    * add eslint-disable-next-line no-restricted-imports to some places where we need emotion
    
    * fix jest setup
    
    * add and use setupEmbeddingSdkEnterprisePlugins
    
    * use `useSetting` instead of `getApplicationColors` to remove enterprise import
    
    * move defineMetabaseAuthConfig to embedding-sdk-shared
    
    * move MetabotProvider to embedding-sdk-bundle
    
    * fix docs generation issue about types
    
    * fix prettier ignore file
    
    * move back tests to ee folder
    
    * remove ../../../ from imports
    
    * fix eslint issues
    
    * fix, again, jest config :sad:

--- rspack.iframe-sdk-embed.config.js
+++ rspack.iframe-sdk-embed.config.js
@@ -18,7 +18,7 @@ const SCRIPT_TAG_PATH = path.resolve(
 const BUILD_PATH = __dirname + "/resources/frontend_client";
 const EMBEDDING_SRC_PATH = __dirname + "/enterprise/frontend/src/embedding";
 const SDK_BUNDLE_SRC_PATH =
-  __dirname + "/enterprise/frontend/src/embedding-sdk-bundle";
+  __dirname + "/frontend/src/embedding-sdk-bundle";
 
 const OUT_FILE_NAME = "embed.js";
 const OUT_TEMP_PATH = path.resolve(BUILD_PATH, "tmp-embed-js");
