8df3bf8590db6598585ebec741356ac9654831ef
Nathan Voxland <nathan@metabase.com>
2025-11-19 14:54:27 -0600

    Added library/tree API

--- enterprise/backend/src/metabase_enterprise/library/api.clj
+++ enterprise/backend/src/metabase_enterprise/library/api.clj
@@ -5,6 +5,7 @@
    [metabase.api.routes.common :refer [+auth]]
    [metabase.collections.core :as collections]
    [metabase.collections.models.collection :as collection]
+   [metabase.permissions.core :as perms]
    [toucan2.core :as t2]))
 
 (set! *warn-on-reflection* true)
@@ -22,7 +23,7 @@
 
 (defn- add-here-and-below [collection]
   (let [descendent-ids (map :id (collection/descendants-flat collection))
-        below (t2/select-fn-set :type [:model/Card :type] :collection_id [:in descendent-ids])]
+        below          (t2/select-fn-set :type [:model/Card :type] :collection_id [:in descendent-ids])]
     ;;This function is only used on the root Library which cannot have items directly in it
     ;;So can assume :here is empty, and all descendants are :below
     (assoc collection :here []
@@ -44,6 +45,40 @@
          :effective_children)
         (add-here-and-below))
     {:message "Library does not exist"}))
+
+(defn- select-collections
+  []
+  (t2/select :model/Collection
+             {:where    [:and
+                         [:in :type [collection/library-collection-type
+                                     collection/library-models-collection-type
+                                     collection/library-metrics-collection-type]]
+                         (collection/visible-collection-filter-clause
+                          :id
+                          {:include-archived-items    :exclude
+                           :include-trash-collection? false
+                           :permission-level          :read
+                           :archive-operation-id      nil})]
+              :order-by [[:%lower.name :asc]]}))
+
+(api.macros/defendpoint :get "/tree"
+  "This matches /api/collection/tree but only returns the library collection."
+  [_route-params
+   _query]
+  (let [collections              (-> (select-collections)
+                                     (t2/hydrate :can_write))
+        collection-type-ids      (reduce (fn [acc {collection-id :collection_id, card-type :type, :as _card}]
+                                           (update acc (case (keyword card-type)
+                                                         :model :dataset
+                                                         :metric :metric
+                                                         :card) conj collection-id))
+                                         {:dataset #{}
+                                          :metric  #{}
+                                          :card    #{}}
+                                         (t2/reducible-query {:select-distinct [:collection_id :type]
+                                                              :from            [:report_card]
+                                                              :where           [:= :archived false]}))]
+    (collection/collections->tree collection-type-ids collections)))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/library` routes."
