2ad9b43cb2579f1d8f253e62093044036134dd83
Nicolò Pretto <info@npretto.com>
2025-11-20 15:39:21 +0100

    remove hash logic in /auth/sso to speed up auth flow

--- enterprise/backend/src/metabase_enterprise/sso/integrations/jwt.clj
+++ enterprise/backend/src/metabase_enterprise/sso/integrations/jwt.clj
@@ -87,7 +87,7 @@
       (and is-embedded-analytics-js? (not (embed.settings/enable-embedding-simple)))
       (throw-simple-embedding-disabled)
 
-      (and is-modular-embedding? (token-utils/has-token request))
+      (and is-modular-embedding? jwt (token-utils/has-token request))
       (generate-response-token (:session result) (:jwt-data result))
 
       ;; JWT provided - use auth-identity/login!
@@ -99,8 +99,8 @@
 
       ;; No JWT - return IdP URL for modular embedding or redirect
       is-modular-embedding?
-      (response/response (token-utils/with-token {:url (sso-settings/jwt-identity-provider-uri)
-                                                  :method "jwt"}))
+      (response/response {:url (sso-settings/jwt-identity-provider-uri)
+                          :method "jwt"})
 
       :else
       (redirect-to-idp (sso-settings/jwt-identity-provider-uri) redirect))))
