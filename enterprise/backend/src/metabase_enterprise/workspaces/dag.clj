5eae5b04f4ebc45a91dfedff4e36a3fc6331e960
Chris Truter <chris@metabase.com>
2025-11-20 13:23:45 +0200

    Move abstract dag implementation out as a test helper. Write tests

--- enterprise/backend/src/metabase_enterprise/workspaces/dag.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/dag.clj
@@ -2,162 +2,9 @@
   (:require
    [clojure.set :as set]
    [flatland.ordered.map :as ordered-map]
-   [metabase.util :as u]
    [toucan2.core :as t2]))
 
-;; TODO handle shadowing / execution of transforms that depend on checkout out models
-
-(defn- related [t id]
-  (keyword (str (name t) (subs (name id) 1))))
-
-(def ^:private x->t (partial related :t))
-
-(defn- is? [t id] (= (name t) (str (first (name id)))))
-
-(defn- lexico [id]
-  (let [n (name id)]
-    [(parse-long (subs n 1))
-     ({\x 1 \t 2 \m 3} (first n))]))
-
-(defn- expand-shorthand
-  "As shorthand we can use transforms in the place of their induced tables.
-   This function expands the graph to insert the induced tables, and replace the corresponding references."
-  [{:keys [dependencies] :as graph}]
-  (assoc graph :dependencies
-         (let [xs (set (filter #(is? :x %) (concat (keys dependencies) (mapcat val dependencies))))]
-           (into (sorted-map-by #(compare (lexico %1) (lexico %2)))
-                 (merge (zipmap (map x->t xs) (map vector xs))
-                        (into {} (filter (comp #(is? :t %) key)) dependencies)
-                        (update-vals
-                         (into {} (remove (comp #(is? :t %) key)) dependencies)
-                         (fn [deps] (->> deps
-                                         (map (fn [id]
-                                                (if (is? :x id)
-                                                  (x->t id)
-                                                  id)))
-                                         (sort-by lexico)
-                                         (vec)))))))))
-
-(def ^:private example-shorthand
-  {:check-outs   #{:x3 :m6 :m10 :m13}
-   :dependencies {:x3  [:x1 :t2]
-                  :x4  [:x3]
-                  :m6  [:x4 :t5]
-                  :m10 [:t9]
-                  :x11 [:m10]
-                  :m12 [:x11]
-                  :m13 [:x11 :m12]}})
-
-(def ^:private example
-  {:check-outs   #{:x3 :m6 :m10 :m13}
-   :dependencies {:t1  [:x1]
-                  :x3  [:t1 :t2]
-                  :t3  [:x3]
-                  :x4  [:t3]
-                  :t4  [:x4]
-                  :m6  [:t4 :t5]
-                  :m10 [:t9]
-                  :x11 [:m10]
-                  :t11 [:x11]
-                  :m12 [:t11]
-                  :m13 [:t11 :m12]}})
-
-(comment (= example (expand-shorthand example-shorthand)))
-
-(defn- children [dependencies]
-  (reduce
-   (fn [acc [child parents]]
-     (reduce (fn [acc p] (update acc p u/conjv child)) acc parents))
-   (sorted-map-by #(compare (lexico %1) (lexico %2)))
-   dependencies))
-
-(comment
-  (children (:dependencies example)))
-
-(defn- ->ts [ids]
-  (into #{}
-        (keep (fn [id]
-                (cond (is? :t id) id
-                      (is? :x id) (x->t id))))
-        ids))
-
-;; naive dfs toposort
-
-(defn- toposort-visit [node child->parents visited result]
-  (cond
-    (visited node) [visited result]
-    :else (let [parents (child->parents node [])
-                [visited' result'] (reduce (fn [[v r] p]
-                                             (toposort-visit p child->parents v r))
-                                           [(conj visited node) result]
-                                           parents)]
-            [visited' (conj result' node)])))
-
-(defn- toposort-dfs [child->parents]
-  ;; we assume the graph contains the degenerate nodes as keys with empty parent lists
-  (let [all-nodes (set (keys child->parents))]
-    (loop [visited   #{}
-           result    []
-           remaining all-nodes]
-      (if (empty? remaining)
-        result
-        (let [node (first remaining)
-              [visited' result'] (toposort-visit node child->parents visited result)]
-          (recur visited' result' (disj remaining node)))))))
-
-(defn- toposort-map [child->parents]
-  (let [child->parents (if (map? child->parents) child->parents (into {} child->parents))]
-    (into (ordered-map/ordered-map)
-          (keep (fn [n] (when-let [deps (get child->parents n)] [n deps])))
-          (toposort-dfs child->parents))))
-
-;; naive version - calculate above and below, and take intersection
-
-;; safety
-(def ^:private max-iterations 100)
-
-(defn- path-induced-subgraph [{:keys [check-outs dependencies] :as _graph}]
-  (let [->parents  dependencies
-        ->children (children dependencies)]
-    (loop [iteration  1
-           below      check-outs
-           above      check-outs
-           seen-down? check-outs
-           seen-up?   check-outs
-           to-descend check-outs
-           to-ascend  check-outs]
-      (if (and (empty? to-descend) (empty? to-ascend))
-        ;; Inputs are all the external tables that we depend on.
-        ;; NOTE: if there is a chain of models, we assume that ->parent here maps all the way to the source table
-        (let [nodes    (set/intersection above below)
-              entities (remove #(is? :t %) nodes)
-              tables   (sort-by lexico (filter #(is? :t %) nodes))]
-          {:check-outs   (sort-by lexico check-outs)
-           :inputs       (sort-by lexico (remove nodes (->ts (mapcat ->parents check-outs))))
-           :tables       tables
-           :transforms   (sort-by lexico (filter #(is? :x %) nodes))
-           :entities     (sort-by lexico entities)
-           :dependencies (apply dissoc
-                                (toposort-map
-                                 (for [n nodes]
-                                   [n (vec (filter nodes (get dependencies n)))]))
-                                tables)})
-        (if (> iteration max-iterations)
-          {:error "max iterations exceeded"}
-          (let [next-down (into #{} (comp (mapcat ->children) (remove seen-down?)) to-descend)
-                next-up   (into #{} (comp (mapcat ->parents) (remove seen-up?)) to-ascend)]
-            (recur (inc iteration)
-                   (into below next-down)
-                   (into above next-up)
-                   (into seen-down? next-down)
-                   (into seen-up? next-up)
-                   next-down
-                   next-up)))))))
-
-(comment
-  (path-induced-subgraph example))
-
-;;;; Database-backed path-induced subgraph calculation
+;;;; Internal helpers
 
 (def ^:private type->db-type
   "Mapping from our entity type keywords to the dependency table's type strings."
@@ -198,6 +45,31 @@
                                 [[:inline id] :entity_id]]})
                     tuples)})
 
+;;;; Toposort
+
+(defn- toposort-visit [node child->parents visited result]
+  (cond
+    (visited node) [visited result]
+    :else (let [parents (child->parents node [])
+                [visited' result'] (reduce (fn [[v r] p]
+                                             (toposort-visit p child->parents v r))
+                                           [(conj visited node) result]
+                                           parents)]
+            [visited' (conj result' node)])))
+
+(defn- toposort-dfs [child->parents]
+  (let [all-nodes (set (keys child->parents))]
+    (loop [visited   #{}
+           result    []
+           remaining all-nodes]
+      (if (empty? remaining)
+        result
+        (let [node (first remaining)
+              [visited' result'] (toposort-visit node child->parents visited result)]
+          (recur visited' result' (disj remaining node)))))))
+
+;;;; Transitive closure queries
+
 (defn- transitive-closure
   "Compute transitive closure in the given direction.
    `direction` is :upstream (follow dependencies) or :downstream (follow dependents)."
@@ -224,17 +96,19 @@
                   :select [:entity_type :entity_id]
                   :from   [cte-name]})))))
 
-(defn upstream-entities
+(defn- upstream-entities
   "Given a map of entity types to IDs, find all upstream entities (dependencies).
    Returns a set of {:id :type} maps."
   [entities-by-type]
   (transitive-closure entities-by-type :upstream))
 
-(defn downstream-entities
+(defn- downstream-entities
   "Given a map of entity types to IDs, find all downstream entities (dependents).
    Returns a set of {:id :type} maps."
   [entities-by-type]
   (transitive-closure entities-by-type :downstream))
+
+;;;; Dependency fetching
 
 (defn- entity-dependencies
   "Fetch all dependencies between the given entities within the dependency table.
@@ -251,7 +125,6 @@
                                          :to_entity_type :to_entity_id]
                                 :from   [:dependency]
                                 :where  (into [:or] conditions)})]
-      ;; Group by source entity, filtering to only include targets in our set
       (reduce (fn [acc {:keys [from_entity_type from_entity_id to_entity_type to_entity_id]}]
                 (let [from-entity {:id from_entity_id, :type (keyword from_entity_type)}
                       to-entity   {:id to_entity_id,   :type (keyword to_entity_type)}]
@@ -293,67 +166,42 @@
            (mapv (fn [row]
                    {:id (get row result-id) :type :table}))))))
 
-(defn path-induced-subgraph-entities
+;;;; Public API
+
+(defn path-induced-subgraph
   "Given a map of entity types to IDs, compute the path-induced subgraph.
    `entities-by-type` is a map like {:transform [1 2 3]}.
 
    Returns a map with:
    - :check-outs   - the input entities
-   - :inputs       - external tables that check-outs depend on (not in subgraph)
-   - :tables       - output tables generated by transforms in the subgraph
-   - :transforms   - Transform entities in the subgraph
-   - :entities     - all entities in the subgraph (transforms, tables, etc.)
+   - :inputs       - external tables that entities in the subgraph depend on, ordered by id
+   - :outputs      - output tables generated by entities in the subgraph, in topological order
+   - :transforms   - transforms in the subgraph, in topological order
+   - :entities     - all non-table entities in the subgraph (just transforms for now)
    - :dependencies - toposorted map from entity to its dependencies within subgraph"
   [entities-by-type]
-  (let [starting-set   (tuples->entity-set (entities-map->tuples entities-by-type))
-        upstream       (or (upstream-entities entities-by-type) #{})
-        downstream     (or (downstream-entities entities-by-type) #{})
-        subgraph-set   (set/intersection upstream downstream)
-        transforms     (filterv #(= :transform (:type %)) subgraph-set)
-        output-tables  (or (fetch-related-tables transforms :downstream) [])
-        full-set       (set/union subgraph-set (set output-tables))
-        inputs         (vec (remove full-set (fetch-related-tables full-set :upstream)))
-        deps-map       (entity-dependencies full-set)
-        sorted         (toposort-entities full-set deps-map)
-        sorted-deps    (into (ordered-map/ordered-map)
-                             (keep (fn [entity]
-                                     (when-not (= :table (:type entity))
-                                       [entity (vec (get deps-map entity []))])))
-                             sorted)]
+  (let [starting-set  (tuples->entity-set (entities-map->tuples entities-by-type))
+        upstream      (or (upstream-entities entities-by-type) #{})
+        downstream    (or (downstream-entities entities-by-type) #{})
+        subgraph-set  (set/intersection upstream downstream)
+        transforms    (filterv #(= :transform (:type %)) subgraph-set)
+        output-tables (or (fetch-related-tables transforms :downstream) [])
+        full-set      (set/union subgraph-set (set output-tables))
+        inputs        (->> upstream
+                           (filter #(= :table (:type %)))
+                           (remove full-set)
+                           (sort-by :id)
+                           vec)
+        deps-map      (entity-dependencies full-set)
+        sorted        (toposort-entities full-set deps-map)
+        sorted-deps   (into (ordered-map/ordered-map)
+                            (keep (fn [entity]
+                                    (when-not (= :table (:type entity))
+                                      [entity (vec (get deps-map entity []))])))
+                            sorted)]
     {:check-outs   (vec starting-set)
      :inputs       inputs
-     :tables       output-tables
+     :outputs      output-tables
      :transforms   transforms
      :entities     (vec full-set)
      :dependencies sorted-deps}))
-
-(comment
-  (upstream-entities {:transform [8]})
-  (downstream-entities {:transform [8]})
-
-  (path-induced-subgraph-entities {:transform [8]}))
-
-(comment
-  (path-induced-subgraph example)
-
-  {:check-outs   '(:x3 :m6 :m10 :m12),
-   ;; these are all the external tables that we will need ro access to
-   :inputs       '(:t1 :t2 :t5 :t9),
-   ;; these are all the tables we need to "shadow" in the workspace schema (previously called outputs)
-   :tables       '(:t3 :t4 :t11),
-   ;; these are all the transforms we need to execute when running the workspace (replaces "downstream")
-   :transforms   '(:x3 :x4 :x11)
-   ;; these are the appdb entities that need to be shadowed with "_workspace" versions.
-   ;; this excludes the tables, which I'm assuming will be handled separately via sync.
-   ;; we might change our mind here and just manually create the metadata along with
-   ;; the other entities, if that's easier.
-   :entities     '(:x3 :x4 :m6 :m10 :x11 :t11 :m12)
-   ;; a mapping from entities that we copy to their dependencies within the subgraph
-   ;; they may have other dependencies outside the subgraph, but we ignore these
-   ;; as they don't need to be remapped
-   :dependencies {:x3  []
-                  :x4  [:t3]
-                  :m6  [:t4]
-                  :m10 []
-                  :x11 [:m10]
-                  :m12 [:t11]}})
---
+++ enterprise/backend/test/metabase_enterprise/workspaces/dag_test.clj
@@ -0,0 +1,233 @@
+(ns metabase-enterprise.workspaces.dag-test
+  (:require
+   [clojure.test :refer :all]
+   [metabase-enterprise.dependencies.models.dependency :as deps]
+   [metabase-enterprise.workspaces.dag :as dag]
+   [metabase-enterprise.workspaces.dag-abstract :as dag-abstract]
+   [metabase.test :as mt]
+   [toucan2.core :as t2]))
+
+;;;; Example graphs for testing
+
+(def ^:private example
+  {:check-outs   #{:x3 :m6 :m10 :m13}
+   :dependencies {:x3  [:x1 :t2]
+                  :x4  [:x3]
+                  :m6  [:x4 :t5]
+                  :m10 [:t9]
+                  :x11 [:m10]
+                  :m12 [:x11]
+                  :m13 [:x11 :m12]}})
+
+;;;; Test data helpers
+
+(defn- create-test-graph!
+  "Create test transforms and tables from shorthand notation, returning id mappings.
+
+   Shorthand uses keywords like :x1, :t2, :m3 where:
+   - :x<n> is a transform
+   - :t<n> is a table (source/generated)
+   - :m<n> is a model (card)
+
+   Returns a map from shorthand id to real database id, e.g. {:x1 123, :t2 456}
+
+   NOTE: Caller should wrap in mt/with-model-cleanup for proper cleanup."
+  [{:keys [check-outs dependencies]}]
+  (let [all-ids    (set (concat (keys dependencies)
+                                (mapcat val dependencies)
+                                check-outs))
+        transforms (filter #(= \x (first (name %))) all-ids)
+        tables     (filter #(= \t (first (name %))) all-ids)
+        ;; Create mock tables
+        table-ids  (into {}
+                         (for [t tables]
+                           (let [id (t2/insert-returning-pk! :model/Table
+                                                             {:db_id  (mt/id)
+                                                              :name   (str "test_table_" (name t))
+                                                              :active true})]
+                             [t id])))
+        ;; Create transforms that reference their input tables
+        tx-ids     (into {}
+                         (for [tx transforms]
+                           (let [;; Find parent tables for this transform
+                                 parent-tables (->> (get dependencies tx [])
+                                                    (filter #(= \t (first (name %))))
+                                                    (map table-ids))
+                                 source-table  (first parent-tables)
+                                 id            (t2/insert-returning-pk! :model/Transform
+                                                                        {:name   (str "Test " (name tx))
+                                                                         :source {:type  :query
+                                                                                  :query (if source-table
+                                                                                           {:database   (mt/id)
+                                                                                            :type       :query
+                                                                                            :query      {:source-table source-table}}
+                                                                                           {:database (mt/id)
+                                                                                            :type     :native
+                                                                                            :native   {:query "SELECT 1"}})}
+                                                                         :target {:type   "table"
+                                                                                  :schema "public"
+                                                                                  :name   (str "test_output_" (name tx))}})]
+                             [tx id])))
+        id-map     (merge tx-ids table-ids)]
+    ;; Create dependencies
+    (doseq [[child parents] dependencies
+            :let [child-type (case (first (name child))
+                               \x "transform"
+                               \t "table"
+                               \m "card")
+                  child-id   (id-map child)]
+            parent parents
+            :let [parent-type (case (first (name parent))
+                                \x "transform"
+                                \t "table"
+                                \m "card")
+                  parent-id   (id-map parent)]]
+      (deps/replace-dependencies!
+       child-type child-id
+       {parent-type #{parent-id}}))
+    id-map))
+
+(defn- translate-result
+  "Translate result from real IDs back to shorthand notation for easier comparison."
+  [result id-map]
+  (let [reverse-map (into {} (map (fn [[k v]] [v k]) id-map))
+        translate   (fn [{:keys [id]}]
+                      (get reverse-map id))]
+    (-> result
+        (update :check-outs #(set (map translate %)))
+        (update :inputs #(set (map translate %)))
+        (update :outputs #(set (map translate %)))
+        (update :transforms #(set (map translate %)))
+        (update :entities #(set (map translate %)))
+        (update :dependencies
+                (fn [deps]
+                  (into {}
+                        (map (fn [[k v]]
+                               [(translate k) (set (map translate v))]))
+                        deps))))))
+
+;;;; Tests
+
+(deftest path-induced-subgraph-basic-test
+  (mt/with-premium-features #{:workspaces :dependencies}
+    (mt/with-temp [:model/Transform tx {:name "Test Transform"}]
+      (let [table-id (mt/id :orders)]
+        ;; Set up: transform depends on orders table
+        (deps/replace-dependencies! "transform" (:id tx) {"table" #{table-id}})
+
+        (testing "single transform returns correct subgraph"
+          (let [result (dag/path-induced-subgraph {:transform [(:id tx)]})]
+            (is (= [{:id (:id tx) :type :transform}] (:check-outs result)))
+            (is (= [{:id (:id tx) :type :transform}] (:transforms result)))
+            ;; Input should be the orders table
+            (is (some #(= table-id (:id %)) (:inputs result)))))))))
+
+(deftest path-induced-subgraph-chain-test
+  (mt/with-premium-features #{:workspaces :dependencies}
+    (mt/with-model-cleanup [:model/Transform :model/Dependency]
+      (mt/with-temp [:model/Transform tx1 {:name "Transform 1"}
+                     :model/Transform tx2 {:name "Transform 2"}
+                     :model/Transform tx3 {:name "Transform 3"}]
+        (let [table-id (mt/id :orders)]
+          ;; Chain: orders -> tx1 -> tx2 -> tx3
+          (deps/replace-dependencies! "transform" (:id tx1) {"table" #{table-id}})
+          (deps/replace-dependencies! "transform" (:id tx2) {"transform" #{(:id tx1)}})
+          (deps/replace-dependencies! "transform" (:id tx3) {"transform" #{(:id tx2)}})
+
+          (testing "middle transform includes upstream and downstream"
+            (let [result (dag/path-induced-subgraph {:transform [(:id tx2)]})]
+              ;; tx2 is checked out
+              (is (= [{:id (:id tx2) :type :transform}] (:check-outs result)))
+              ;; Should include tx1 (upstream) and tx3 (downstream) since tx2 is on path between them
+              ;; Actually no - tx2 is the only thing checked out, so we only get what's on paths through tx2
+              ;; tx1 -> tx2 -> tx3 means tx2 has tx1 upstream and tx3 downstream
+              ;; The intersection is just tx2 itself
+              (is (= #{{:id (:id tx2) :type :transform}} (set (:transforms result)))))))))))
+
+(deftest path-induced-subgraph-diamond-test
+  (mt/with-premium-features #{:workspaces :dependencies}
+    (mt/with-model-cleanup [:model/Transform :model/Dependency]
+      (testing "diamond dependency pattern"
+        ;; Create: t1 -> x1 -> t2
+        ;;              \-> x2 -> t3
+        ;;         both x1 and x2 depend on t1
+        ;;         if we check out x1 and x2, we should get both
+        (mt/with-temp [:model/Transform tx1 {:name "Transform 1"}
+                       :model/Transform tx2 {:name "Transform 2"}]
+          (let [source-table (mt/id :orders)]
+            (deps/replace-dependencies! "transform" (:id tx1) {"table" #{source-table}})
+            (deps/replace-dependencies! "transform" (:id tx2) {"table" #{source-table}})
+
+            (let [result (dag/path-induced-subgraph {:transform [(:id tx1) (:id tx2)]})]
+              (is (= 2 (count (:check-outs result))))
+              (is (= 2 (count (:transforms result)))))))))))
+
+(deftest upstream-entities-test
+  (mt/with-premium-features #{:workspaces :dependencies}
+    (mt/with-model-cleanup [:model/Transform :model/Dependency]
+      (mt/with-temp [:model/Transform tx1 {:name "Transform 1"}
+                     :model/Transform tx2 {:name "Transform 2"}]
+        (let [table-id (mt/id :orders)]
+          (deps/replace-dependencies! "transform" (:id tx1) {"table" #{table-id}})
+          (deps/replace-dependencies! "transform" (:id tx2) {"transform" #{(:id tx1)}})
+
+          (testing "upstream includes transitive dependencies"
+            (let [upstream (#'dag/upstream-entities {:transform [(:id tx2)]})]
+              (is (contains? upstream {:id (:id tx2) :type :transform}))
+              (is (contains? upstream {:id (:id tx1) :type :transform}))
+              (is (contains? upstream {:id table-id :type :table})))))))))
+
+(deftest downstream-entities-test
+  (mt/with-premium-features #{:workspaces :dependencies}
+    (mt/with-model-cleanup [:model/Transform :model/Dependency]
+      (mt/with-temp [:model/Transform tx1 {:name "Transform 1"}
+                     :model/Transform tx2 {:name "Transform 2"}]
+        (deps/replace-dependencies! "transform" (:id tx2) {"transform" #{(:id tx1)}})
+
+        (testing "downstream includes transitive dependents"
+          (let [downstream (#'dag/downstream-entities {:transform [(:id tx1)]})]
+            (is (contains? downstream {:id (:id tx1) :type :transform}))
+            (is (contains? downstream {:id (:id tx2) :type :transform}))))))))
+
+(deftest path-induced-subgraph-shorthand-test
+  (mt/with-premium-features #{:workspaces :dependencies}
+    (mt/with-model-cleanup [:model/Table :model/Transform :model/Dependency]
+      (testing "graph built from shorthand matches abstract solver"
+        (let [shorthand  {:check-outs   #{:x1}
+                          :dependencies {:x1 [:t1]}}
+              id-map     (create-test-graph! shorthand)
+              result     (dag/path-induced-subgraph {:transform [(id-map :x1)]})
+              translated (translate-result result id-map)]
+          ;; Check-outs should be :x1
+          (is (= #{:x1} (:check-outs translated)))
+          ;; Transforms should be :x1
+          (is (= #{:x1} (:transforms translated)))
+          ;; Inputs should be :t1
+          (is (contains? (:inputs translated) :t1)))))))
+
+(deftest expand-solver-test
+  (testing "expand-shorthand inserts interstitial nodes for transform output tables"
+    (is (= {:check-outs   #{:x3 :m6 :m10 :m13}
+            :dependencies {:t1  [:x1]
+                           :x3  [:t1 :t2]
+                           :t3  [:x3]
+                           :x4  [:t3]
+                           :t4  [:x4]
+                           :m6  [:t4 :t5]
+                           :m10 [:t9]
+                           :x11 [:m10]
+                           :t11 [:x11]
+                           :m12 [:t11]
+                           :m13 [:t11 :m12]}}
+           (dag-abstract/expand-shorthand example)))))
+
+(testing "path-induced-subgraph computes correct result for example graph"
+  (let [result (dag-abstract/path-induced-subgraph (dag-abstract/expand-shorthand example))]
+    ;; Check that we get the expected structure
+    (is (= 4 (count (:check-outs result))))
+    (is (every? #(contains? (set (:check-outs result)) %)
+                [:x3 :m6 :m10 :m13]))
+    ;; Inputs should be external tables
+    (is (every? #(dag-abstract/is? :t %) (:inputs result)))
+    ;; Transforms should only be :x entities
+    (is (every? #(dag-abstract/is? :x %) (:transforms result)))))
