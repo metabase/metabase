63f1c5cdcd1aa7a154c9cded13ecdf9c9c52dd13
Brad Anderson <brad@metabase.com>
2025-11-20 16:59:02 -0500

    Fix #65501 - Resets page when filters change. Also works with browser back/forward buttons.

--- frontend/src/metabase/search/containers/SearchApp.jsx
+++ frontend/src/metabase/search/containers/SearchApp.jsx
@@ -9,7 +9,6 @@ import EmptyState from "metabase/common/components/EmptyState";
 import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
 import { PaginationControls } from "metabase/common/components/PaginationControls";
 import { NoObjectError } from "metabase/common/components/errors/NoObjectError";
-import { usePagination } from "metabase/common/hooks/use-pagination";
 import Search from "metabase/entities/search";
 import { usePageTitle } from "metabase/hooks/use-page-title";
 import { useDispatch } from "metabase/lib/redux";
@@ -32,12 +31,19 @@ import {
 } from "metabase/search/utils";
 import { Box, Group, Paper, Text } from "metabase/ui";
 
+const getPageFromLocation = (location) => {
+  const maybePage = location.query?.page
+    ? parseInt(location.query.page, 10)
+    : 0;
+  return maybePage || 0;
+};
+
 function SearchApp({ location }) {
   const dispatch = useDispatch();
 
   usePageTitle(t`Search`);
 
-  const { handleNextPage, handlePreviousPage, page } = usePagination();
+  const page = getPageFromLocation(location);
 
   const searchText = useMemo(
     () => getSearchTextFromLocation(location),
@@ -74,6 +80,15 @@ function SearchApp({ location }) {
     },
     [onChangeLocation, searchText],
   );
+
+  const advancePage = (howMany = 1) => {
+    dispatch(
+      push({
+        pathname: "search",
+        query: { q: searchText.trim(), ...searchFilters, page: page + howMany },
+      }),
+    );
+  };
 
   const { data, error, isFetching, requestId } = useSearchQuery(query);
   const list = useMemo(() => {
@@ -122,8 +137,8 @@ function SearchApp({ location }) {
                   page={page}
                   itemsLength={list.length}
                   total={data.total}
-                  onNextPage={handleNextPage}
-                  onPreviousPage={handlePreviousPage}
+                  onNextPage={() => advancePage(1)}
+                  onPreviousPage={() => advancePage(-1)}
                 />
               </Group>
             </Box>
