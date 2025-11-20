13a018db374a9b52cadb1a6749d5cd23b7e09b09
Jeff Bruemmer <jeff.bruemmer@gmail.com>
2025-11-20 14:36:29 -0500

    Update API documentation function description

--- src/metabase/cmd/core.clj
+++ src/metabase/cmd/core.clj
@@ -139,7 +139,7 @@
   (println "File encoding:"   (System/getProperty "file.encoding")))
 
 (defn ^:command api-documentation
-  "Generate an HTML and JSON files for all API endpoints."
+  "Generate an HTML file and a JSON file for API docs."
   []
   (classloader/require 'metabase.cmd.endpoint-dox)
   ((resolve 'metabase.cmd.endpoint-dox/generate-dox!)))
