24d6fc4e23097e8fa1c7b65e6cf03fec05eb5075
John Swanson <john.swanson@metabase.com>
2025-11-21 11:29:00 -0800

    Fix flaky Remote Sync test (#65862)

--- e2e/test/scenarios/admin/remote-sync.cy.spec.ts
+++ e2e/test/scenarios/admin/remote-sync.cy.spec.ts
@@ -595,7 +595,7 @@ describe("Remote Sync", () => {
 
       cy.visit("/admin/settings/remote-sync");
       cy.findByLabelText("Sync branch").clear().type("test");
-      cy.button("Save changes").click();
+      cy.findByTestId("remote-sync-submit-button").click();
 
       cy.findByTestId("admin-layout-content")
         .findByText("Success")
@@ -607,7 +607,7 @@ describe("Remote Sync", () => {
 
       H.waitForTask({ taskName: "import" });
 
-      cy.findByRole("button", { name: "Save changes" }).should("be.disabled");
+      cy.findByTestId("remote-sync-submit-button").should("be.disabled");
 
       cy.visit("/");
 
