ef1584b845c9e79cce6b29cde7034508e7154d07
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-11-24 20:12:20 -0500

    FE

--- frontend/src/metabase/entities/table-symlinks.ts
+++ frontend/src/metabase/entities/table-symlinks.ts
@@ -1,7 +1,7 @@
 import { createEntity } from "metabase/lib/entities";
 import * as Urls from "metabase/lib/urls";
 import { TableSymlinkSchema } from "metabase/schema";
-import type { TableSymlink } from "metabase-types/api";
+import type { CollectionItem } from "metabase-types/api";
 
 /**
  * @deprecated use "metabase/api" instead
@@ -11,8 +11,10 @@ export const TableSymlinks = createEntity({
   nameOne: "tableSymlink",
   schema: TableSymlinkSchema,
   objectSelectors: {
-    getUrl: ({ table_id, database_id }: TableSymlink) =>
-      Urls.queryBuilderTable(table_id, database_id),
+    getUrl: ({ table_id, database_id }: CollectionItem) =>
+      table_id != null && database_id != null
+        ? Urls.queryBuilderTable(table_id, database_id)
+        : "",
     getIcon: () => ({ name: "table" }),
   },
 });
