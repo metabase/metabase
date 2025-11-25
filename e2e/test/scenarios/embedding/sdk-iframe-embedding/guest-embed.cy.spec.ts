ff3815d2799f3b036c31fef01962537cd8ee3502
Timofei <timofei@metabase.com>
2025-11-25 17:20:42 +0400

    Add oss embedjs guest embed test

--- e2e/test/scenarios/embedding/sdk-iframe-embedding/guest-embed.cy.spec.ts
+++ e2e/test/scenarios/embedding/sdk-iframe-embedding/guest-embed.cy.spec.ts
@@ -5,51 +5,62 @@ const { H } = cy;
 const { ORDERS, ORDERS_ID } = SAMPLE_DATABASE;
 
 describe("scenarios > embedding > sdk iframe embedding > guest-embed", () => {
-  beforeEach(() => {
-    H.prepareGuestEmbedSdkIframeEmbedTest({
-      onPrepare: () => {
-        createQuestion({
-          name: "47563",
-          enable_embedding: true,
-          embedding_type: "guest-embed",
-          query: {
-            "source-table": ORDERS_ID,
-            aggregation: [["max", ["field", ORDERS.QUANTITY, null]]],
-            breakout: [["field", ORDERS.PRODUCT_ID, null]],
-            limit: 2,
-          },
-        }).then(({ body: question }) => {
-          cy.wrap(question.id).as("questionId");
-        });
-      },
-    });
-  });
+  ([{ instance: "oss" }, { instance: "ee" }] as const).forEach(
+    ({ instance }) => {
+      describe(
+        instance,
+        { ...(instance === "oss" && { tags: "@OSS" }) },
+        () => {
+          beforeEach(() => {
+            H.prepareGuestEmbedSdkIframeEmbedTest({
+              instance,
+              onPrepare: () => {
+                createQuestion({
+                  name: "47563",
+                  enable_embedding: true,
+                  embedding_type: "guest-embed",
+                  query: {
+                    "source-table": ORDERS_ID,
+                    aggregation: [["max", ["field", ORDERS.QUANTITY, null]]],
+                    breakout: [["field", ORDERS.PRODUCT_ID, null]],
+                    limit: 2,
+                  },
+                }).then(({ body: question }) => {
+                  cy.wrap(question.id).as("questionId");
+                });
+              },
+            });
+          });
 
-  it("shows a static question", () => {
-    cy.get("@questionId").then(async (questionId) => {
-      const token = await getSignedJwtForResource({
-        resourceId: questionId as unknown as number,
-        resourceType: "question",
-      });
+          it("shows a static question", () => {
+            cy.get("@questionId").then(async (questionId) => {
+              const token = await getSignedJwtForResource({
+                resourceId: questionId as unknown as number,
+                resourceType: "question",
+              });
 
-      const frame = H.loadSdkIframeEmbedTestPage({
-        metabaseConfig: { isGuestEmbed: true },
-        elements: [
-          {
-            component: "metabase-question",
-            attributes: {
-              token,
-            },
-          },
-        ],
-      });
+              const frame = H.loadSdkIframeEmbedTestPage({
+                metabaseConfig: { isGuestEmbed: true },
+                elements: [
+                  {
+                    component: "metabase-question",
+                    attributes: {
+                      token,
+                    },
+                  },
+                ],
+              });
 
-      cy.wait("@getCardQuery");
+              cy.wait("@getCardQuery");
 
-      frame.within(() => {
-        cy.findByText("Product ID").should("be.visible");
-        cy.findByText("Max of Quantity").should("be.visible");
-      });
-    });
-  });
+              frame.within(() => {
+                cy.findByText("Product ID").should("be.visible");
+                cy.findByText("Max of Quantity").should("be.visible");
+              });
+            });
+          });
+        },
+      );
+    },
+  );
 });
