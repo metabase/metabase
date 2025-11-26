59e53cbb386fcfc35c54486be961c985dbb4fcf9
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-11-26 16:52:03 +0700

    Close subscription sidebar when unchecking `withSubscriptions` checkbox on EAJS

--- frontend/src/embedding-sdk-bundle/components/public/dashboard/InteractiveDashboard/InteractiveDashboard.tsx
+++ frontend/src/embedding-sdk-bundle/components/public/dashboard/InteractiveDashboard/InteractiveDashboard.tsx
@@ -1,8 +1,9 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
-import { useSdkSelector } from "embedding-sdk-bundle/store";
+import { useSdkDispatch, useSdkSelector } from "embedding-sdk-bundle/store";
 import { getPlugins } from "embedding-sdk-bundle/store/selectors";
 import type { MetabasePluginsConfig } from "embedding-sdk-bundle/types/plugins";
+import { setSharing as setDashboardSubscriptionSidebarOpen } from "metabase/dashboard/actions";
 import { PublicOrEmbeddedDashCardMenu } from "metabase/dashboard/components/DashCard/PublicOrEmbeddedDashCardMenu";
 import { DASHBOARD_ACTION } from "metabase/dashboard/components/DashboardHeader/DashboardHeaderButtonRow/dashboard-action-keys";
 import { isQuestionCard } from "metabase/dashboard/utils";
@@ -38,6 +39,13 @@ const InteractiveDashboardInner = (props: InteractiveDashboardProps) => {
       }),
     [plugins],
   );
+
+  const dispatch = useSdkDispatch();
+  useEffect(() => {
+    if (!props.withSubscriptions) {
+      dispatch(setDashboardSubscriptionSidebarOpen(false));
+    }
+  }, [dispatch, props.withSubscriptions]);
 
   return (
     <SdkDashboard
