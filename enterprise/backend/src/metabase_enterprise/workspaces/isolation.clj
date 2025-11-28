d3680a1dc8aa1366ae8691bb9de919e26d698832
Ngoc Khuat <qn.khuat@gmail.com>
2025-11-28 18:34:32 +0700

    Redshift support

--- enterprise/backend/src/metabase_enterprise/workspaces/isolation.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/isolation.clj
@@ -10,6 +10,17 @@
    [metabase.system.core :as system]
    [toucan2.core :as t2]))
 
+(set! *warn-on-reflection* true)
+
+;;;; Dispatch for database/driver/... multimethods
+
+;; I'm making this to have a var so var can be passed as dispatch to `duplicate-transform-table!` during
+;; development as this is subject to change.
+(defn dispatch-on-engine
+  "Take engine from database `db` and dispatch on that."
+  [database & _args]
+  (driver.u/database->driver database))
+
 ;;;; Naming
 
 ;; re-using https://github.com/metabase/metabase/pull/61887/commits/c92e4a9cc451c61a13fef19ed9d6107873b17f07
@@ -39,14 +50,23 @@
   (let [instance-slug (instance-uuid-slug (str (system/site-uuid)))]
     (format "mb_isolation_%s_%s" instance-slug workspace-id)))
 
-;;;; Dispatch for database/driver/... multimethods
+(def ^:private password-char-sets
+  "Character sets for password generation. Cycles through these to ensure representation from each."
+  ["ABCDEFGHJKLMNPQRSTUVWXYZ"
+   "abcdefghjkmnpqrstuvwxyz"
+   "23456789"
+   "!#$%&*+-="])
 
-;; I'm making this to have a var so var can be passed as dispatch to `duplicate-transform-table!` during
-;; development as this is subject to change.
-(defn dispatch-on-engine
-  "Take engine from database `db` and dispatch on that."
-  [database & _args]
-  (driver.u/database->driver database))
+(defn- random-isolated-password
+  "Generate a random password suitable for most database engines.
+   Ensures the password contains characters from all sets (uppercase, lowercase, digits, special)
+   by cycling through the character sets. Result is shuffled for randomness."
+  []
+  (->> (cycle password-char-sets)
+       (take 32)
+       (map rand-nth)
+       shuffle
+       (apply str)))
 
 ;;;; Isolation init
 
@@ -58,18 +78,18 @@
 
 (defmethod grant-read-access-to-tables! :postgres
   [database workspace tables]
-  (let [driver         (driver.u/database->driver database)
-        jdbc-spec      (sql-jdbc.conn/connection-details->spec driver (:details database))
-        read-user-name (-> workspace :database_details :user)
+  (let [read-user-name (-> workspace :database_details :user)
         sqls           (->> (for [table tables]
                               [(format "GRANT USAGE ON SCHEMA %s TO %s" (:schema table) read-user-name)
                                (format "GRANT SELECT ON TABLE %s.%s TO %s" (:schema table) (:name table) read-user-name)])
                             flatten
                             ;; drop mutliple grant usage on the same schema
                             distinct)]
-
-    (doseq [sql sqls]
-      (jdbc/execute! jdbc-spec [sql]))))
+    (jdbc/with-db-transaction [t-conn (sql-jdbc.conn/db->pooled-connection-spec (:id database))]
+      (with-open [stmt (.createStatement ^java.sql.Connection (:connection t-conn))]
+        (doseq [sql sqls]
+          (.addBatch ^java.sql.Statement stmt ^String sql))
+        (.executeBatch ^java.sql.Statement stmt)))))
 
 (defmulti init-workspace-database-isolation!
   "Create database isolation for a workspace. Return the database details."
@@ -79,18 +99,19 @@
 
 (defmethod init-workspace-database-isolation! :postgres
   [database workspace]
-  (let [driver        (driver.u/database->driver database)
-        schema-name   (isolation-schema-name (:id workspace))
+  (let [schema-name   (isolation-schema-name (:id workspace))
         read-user     {:user     (isolation-user-name (:id workspace))
-                       :password (str (random-uuid))}
-        jdbc-spec     (sql-jdbc.conn/connection-details->spec driver (:details database))]
-    (doseq [sql [(format "CREATE SCHEMA %s" schema-name)
-                 (format "CREATE USER %s WITH PASSWORD '%s'" (:user read-user) (:password read-user))
-                 ;; grant all access on the destination schema
-                 (format "GRANT USAGE ON SCHEMA %s TO %s" schema-name (:user read-user))
-                 ;; need to be able insert and dropping, rename tables from this chema
-                 (format "GRANT ALL PRIVILEGES ON SCHEMA %s TO %s" schema-name (:user read-user))]]
-      (jdbc/execute! jdbc-spec [sql]))
+                       :password (random-isolated-password)}]
+    (jdbc/with-db-transaction [t-conn (sql-jdbc.conn/db->pooled-connection-spec (:id database))]
+      (with-open [stmt (.createStatement ^java.sql.Connection (:connection t-conn))]
+        (doseq [sql [(format "CREATE SCHEMA %s" schema-name)
+                     (format "CREATE USER %s WITH PASSWORD '%s'" (:user read-user) (:password read-user))
+                     ;; grant all access on the destination schema
+                     (format "GRANT USAGE ON SCHEMA %s TO %s" schema-name (:user read-user))
+                     ;; need to be able insert and dropping, rename tables from this chema
+                     (format "GRANT ALL PRIVILEGES ON SCHEMA %s TO %s" schema-name (:user read-user))]]
+          (.addBatch ^java.sql.Statement stmt ^String sql))
+        (.executeBatch ^java.sql.Statement stmt)))
     {:schema           schema-name
      :database_details read-user}))
 
@@ -103,24 +124,44 @@
   :hierarchy #'driver/hierarchy)
 
 (defmethod duplicate-output-table! :postgres [database workspace output]
-  (let [details            (:details database)
-        driver*            (driver.u/database->driver database)
-        jdbc-spec          (sql-jdbc.conn/connection-details->spec driver* details)
-        source-schema      (:schema output)
+  (let [source-schema      (:schema output)
         source-table       (:name output)
         isolated-schema    (:schema workspace)
         isolated-table     (isolated-table-name output)]
     (assert (every? some? [source-schema source-table isolated-schema isolated-table]) "Figured out table")
     ;; TODO: execute the following only if the transform was previously executed and its table exists.
-    (doseq [sql [(format (str "CREATE TABLE \"%s\".\"%s\""
-                              "  AS SELECT * FROM \"%s\".\"%s\""
-                              "WITH NO DATA")
-                         isolated-schema
-                         isolated-table
-                         source-schema
-                         source-table)
-                 (format "ALTER TABLE \"%s\".\"%s\" OWNER TO %s" isolated-schema isolated-table (-> workspace :database_details :user))]]
-      (jdbc/execute! jdbc-spec [sql]))
+    (jdbc/with-db-transaction [t-conn (sql-jdbc.conn/db->pooled-connection-spec (:id database))]
+      (with-open [stmt (.createStatement ^java.sql.Connection (:connection t-conn))]
+        (doseq [sql [(format (str "CREATE TABLE \"%s\".\"%s\""
+                                  "  AS SELECT * FROM \"%s\".\"%s\""
+                                  "WITH NO DATA")
+                             isolated-schema
+                             isolated-table
+                             source-schema
+                             source-table)
+                     (format "ALTER TABLE \"%s\".\"%s\" OWNER TO %s" isolated-schema isolated-table (-> workspace :database_details :user))]]
+          (.addBatch ^java.sql.Statement stmt ^String sql))
+        (.executeBatch ^java.sql.Statement stmt)))
+    (let [table-metadata (ws.sync/sync-transform-mirror! database isolated-schema isolated-table)]
+      (select-keys table-metadata [:id :schema :name]))))
+
+(defmethod duplicate-output-table! :redshift [database workspace output]
+  (let [source-schema      (:schema output)
+        source-table       (:name output)
+        isolated-schema    (:schema workspace)
+        isolated-table     (isolated-table-name output)]
+    (assert (every? some? [source-schema source-table isolated-schema isolated-table]) "Figured out table")
+    ;; TODO: execute the following only if the transform was previously executed and its table exists.
+    (jdbc/with-db-transaction [t-conn (sql-jdbc.conn/db->pooled-connection-spec (:id database))]
+      (with-open [stmt (.createStatement ^java.sql.Connection (:connection t-conn))]
+        (doseq [sql [(format (str "CREATE TABLE \"%s\".\"%s\"" " (LIKE \"%s\".\"%s\")")
+                             isolated-schema
+                             isolated-table
+                             source-schema
+                             source-table)
+                     (format "ALTER TABLE \"%s\".\"%s\" OWNER TO %s" isolated-schema isolated-table (-> workspace :database_details :user))]]
+          (.addBatch ^java.sql.Statement stmt ^String sql))
+        (.executeBatch ^java.sql.Statement stmt)))
     (let [table-metadata (ws.sync/sync-transform-mirror! database isolated-schema isolated-table)]
       (select-keys table-metadata [:id :schema :name]))))
 
@@ -147,9 +188,13 @@
                                             :workspace_id  (:id workspace)})
                                (assoc hydrated-output :mapping isolated-table)))))))
 
+#_(ngoc/with-tc (init-workspace-database-isolation! database workspace))
+
 (defn ensure-database-isolation!
   "Wrapper around the driver method, to make migrations easier in future."
   [workspace database]
+  (def workspace workspace)
+  (def database database)
   ;; TODO: Make this check the ws existence aka fail closed ~atm
   (let [{:keys [schema database_details]} (init-workspace-database-isolation! database workspace)]
     (t2/update! :model/Workspace (:id workspace) {:schema           schema
