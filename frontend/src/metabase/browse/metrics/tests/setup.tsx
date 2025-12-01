31ef86685426eaf8d45a8439c7bfe0ce6fad3144
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-12-01 12:30:12 -0500

    Remove outdated selectors

--- frontend/src/metabase/browse/metrics/tests/setup.tsx
+++ frontend/src/metabase/browse/metrics/tests/setup.tsx
@@ -4,22 +4,21 @@ import { setupEnterprisePlugins } from "__support__/enterprise";
 import {
   setupCardEndpoints,
   setupCardQueryEndpoints,
-  setupDatabasesEndpoints,
   setupRecentViewsEndpoints,
   setupSearchEndpoints,
   setupSettingsEndpoints,
 } from "__support__/server-mocks";
 import { mockSettings } from "__support__/settings";
 import { renderWithProviders } from "__support__/ui";
-import type { Database, TokenFeatures } from "metabase-types/api";
+import type { TokenFeatures } from "metabase-types/api";
 import {
   createMockCard,
   createMockCollection,
-  createMockDatabase,
   createMockDataset,
   createMockSearchResult,
   createMockTokenFeatures,
   createMockUser,
+  createMockUserPermissions,
 } from "metabase-types/api/mocks";
 import {
   createMockSetupState,
@@ -211,10 +210,8 @@ export type SetupOpts = {
   showMetabaseLinks?: boolean;
   hasEnterprisePlugins?: boolean;
   tokenFeatures?: Partial<TokenFeatures>;
-  databases?: Database[];
+  canCreateQueries?: boolean;
 };
-
-const MOCK_DATABASE = createMockDatabase({ id: 1, name: "Database Name" });
 
 export function setup({
   metricCount = Infinity,
@@ -222,7 +219,7 @@ export function setup({
   showMetabaseLinks = true,
   hasEnterprisePlugins,
   tokenFeatures = {},
-  databases = [MOCK_DATABASE],
+  canCreateQueries = true,
 }: SetupOpts = {}) {
   const state = createMockState({
     setup: createMockSetupState({
@@ -232,7 +229,12 @@ export function setup({
       "show-metabase-links": showMetabaseLinks,
       "token-features": createMockTokenFeatures(tokenFeatures),
     }),
-    currentUser: createMockUser({ id: 1 }),
+    currentUser: createMockUser({
+      id: 1,
+      permissions: createMockUserPermissions({
+        can_create_queries: canCreateQueries,
+      }),
+    }),
   });
 
   if (hasEnterprisePlugins) {
@@ -247,7 +249,6 @@ export function setup({
   const metrics = mockMetricResults.slice(0, metricCount);
   const recentMetrics = mockRecentMetrics.slice(0, recentMetricCount);
 
-  setupDatabasesEndpoints(databases);
   setupSettingsEndpoints([]);
   setupSearchEndpoints(metrics.map(createMockSearchResult));
   setupRecentViewsEndpoints(recentMetrics);
