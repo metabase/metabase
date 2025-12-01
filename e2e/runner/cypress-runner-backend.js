2c6b4152325c43a631d4e8cf84ebb97c4e05ee7e
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2025-12-01 19:00:16 +0100

    Test deps.edn in CI

--- e2e/runner/cypress-runner-backend.js
+++ e2e/runner/cypress-runner-backend.js
@@ -6,7 +6,7 @@ const http = require("http");
 const os = require("os");
 const path = require("path");
 
-const { BACKEND_HOST, BACKEND_PORT } = require("./constants/backend-port");
+const { BACKEND_PORT } = require("./constants/backend-port");
 const { delay } = require("./cypress-runner-utils");
 
 const CypressBackend = {
@@ -27,38 +27,12 @@ const CypressBackend = {
       this.createServer();
     }
     if (!this.server.process) {
-      const javaFlags = [
-        "-XX:+IgnoreUnrecognizedVMOptions", // ignore options not recognized by this Java version (e.g. Java 8 should ignore Java 9 options)
-        "-Dh2.bindAddress=localhost", // fix H2 randomly not working (?)
-        "-Djava.awt.headless=true", // when running on macOS prevent little Java icon from popping up in Dock
-        "-Dmail.smtps.ssl.trust=*", // trust self signed certs for testing
-        "-Duser.timezone=US/Pacific",
-        // FIXME @nemanjaglumac 2025-11-28
-        // This is a bit convoluted because this particular log4j config outputs all logs to a file (logs/test.log)
-        // See: https://github.com/metabase/metabase/pull/21360
-        // If we want them in the console, we'd have to unset the log4j config, to let stdio inherit the logs.
-        // Override in your .env if you need to see the logs in the console locally.
-        process.env.SHOW_BACKEND_LOGS === "true"
-          ? null
-          : `-Dlog4j.configurationFile=file:${__dirname}/../../frontend/test/__runner__/log4j2.xml`,
-      ].filter(Boolean);
-
       const metabaseConfig = {
-        MB_DB_TYPE: "h2",
         MB_DB_FILE: this.server.dbFile,
-        MB_JETTY_HOST: BACKEND_HOST,
         MB_JETTY_PORT: BACKEND_PORT,
-        MB_ENABLE_TEST_ENDPOINTS: "true",
-        MB_DANGEROUS_UNSAFE_ENABLE_TESTING_H2_CONNECTIONS_DO_NOT_ENABLE: "true",
-        MB_LAST_ANALYTICS_CHECKSUM: "-1",
-        MB_DB_CONNECTION_URI: "", // ignore connection URI in favor of the db file
-        MB_CONFIG_FILE_PATH: "__cypress__", // ignore config.yml
-        MB_HTTP_CHANNEL_HOST_STRATEGY: "allow-all", // we use a local webhook service for testing
-        MB_SNOWPLOW_AVAILABLE: true,
-        MB_SNOWPLOW_URL: "http://localhost:9090",
       };
 
-      this.server.process = spawn("java", [...javaFlags, "-jar", jarPath], {
+      this.server.process = spawn("java", ["-jar", jarPath], {
         env: {
           ...process.env,
           ...metabaseConfig,
