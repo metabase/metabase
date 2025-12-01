e440239b79830c4aee1fff6bad4712533a7e484a
Eric Normand <ericwnormand@gmail.com>
2025-12-01 13:40:24 -0600

    Move question test

--- enterprise/backend/test/metabase_enterprise/representations/v0/question_test.clj
+++ enterprise/backend/test/metabase_enterprise/representations/v0/question_test.clj
@@ -105,6 +105,47 @@
                 (finally
                   (t2/delete! :model/Card (:id question)))))))))))
 
+(deftest export-import-test
+  (testing "Export database and question with refs, then import in order"
+    (mt/with-temp [:model/Database database {:name "Test Database for Refs"
+                                             :engine :h2
+                                             :details {}}]
+      (let [db-id     (:id database)
+            db-export (rep/export database)
+            db-ref    (:name db-export)]
+        (mt/with-temp [:model/Card question {:name          "Test Question with Ref"
+                                             :type          :question
+                                             :database_id   db-id
+                                             :dataset_query {:database db-id
+                                                             :type :native
+                                                             :native {:query "SELECT 1"}}}]
+          (let [question-export (rep/export question)]
+            (is (= (str "ref:database-" db-id) (:database question-export))
+                "Question export should reference database with ref")
+            (is (= db-ref (str "database-" db-id))
+                "Database export should have correct ref")
+            (let [db-yaml (rep-yaml/generate-string db-export)
+                  question-yaml (rep-yaml/generate-string question-export)
+                  db-rep (rep-yaml/parse-string db-yaml)
+                  question-rep (rep-yaml/parse-string question-yaml)]
+              (is (rep-read/parse db-rep)
+                  "Database representation should validate")
+              (is (rep-read/parse question-rep)
+                  "Question representation should validate")
+              (let [ref-index (v0-common/map-entity-index {db-ref database})
+                    persisted-question (rep/insert! question-rep ref-index)]
+                (try
+                  (is persisted-question "Question should persist successfully")
+                  (is (= (:id database) (:database_id persisted-question))
+                      "Imported question should reference imported database")
+                  (let [re-exported (rep/export persisted-question)
+                        re-exported-yaml (rep-yaml/generate-string re-exported)
+                        re-exported-rep (rep-yaml/parse-string re-exported-yaml)]
+                    (is (=? (dissoc question-rep :name :entity-id) re-exported-rep)
+                        "Re-exported representation should match original"))
+                  (finally
+                    (t2/delete! :model/Card (:id persisted-question))))))))))))
+
 (deftest representation-type-test
   (doseq [entity (t2/select :model/Card :type :question)]
     (is (= :question (v0-common/representation-type entity)))))
