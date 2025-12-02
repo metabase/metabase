b8c8387c77a72bfcaa43a7504321e074f6a36af1
dan sutton <dan@dpsutton.com>
2025-12-02 15:06:59 -0600

    Add tests, add functionality
    tests:
    ensure it (a) sets the cookie, (b) honors the cookie, and when it
    restores the cache, to set the cookie so it doesn't keep thrashing. But
    presumably it won't since it's up to date. My "in the future" version is
    a bit synthetic.
    
    Functionality wise, updating the cache now returns and this will saet
    the header with the most recent cache setting

--- src/metabase/server/middleware/settings_cache.clj
+++ src/metabase/server/middleware/settings_cache.clj
@@ -14,16 +14,20 @@
     (let [last-known-update   (setting/cache-last-updated-at)]
       ;; If the cookie timestamp is newer than our cached timestamp, restore the cache
       (when (and last-known-update
-                 (try (> (compare cookie-timestamp last-known-update) 0)
+                 (try (pos? (compare cookie-timestamp last-known-update))
                       (catch Exception _e
                         (log/infof "Strange last known update cookie: %s" cookie-timestamp)
                         false)))
         (log/info "Settings cookie indicates cache is out of date. Refreshing...")
-        (setting/restore-cache!)))))
+        (setting/restore-cache!)
+        ::restored))))
 
 (defn- maybe-set-settings-last-updated-cookie
-  [response]
-  (if (-> response :mb/cookies :cookie/settings-cache-timestamp)
+  "Set the most recent settings cache update timestamp if this request changed a setting (it will have
+  the :cookie/settings-cache-timestamp) or if this request caused a cache to be updated."
+  [restored? response]
+  (if (or restored?
+          (-> response :mb/cookies :cookie/settings-cache-timestamp))
     (if-let [timestamp (setting/cache-last-updated-at)]
       (response/set-cookie (update response :mb/cookies dissoc :cookie/settings-cache-timestamp)
                            settings-last-updated-cookie-name
@@ -42,5 +46,5 @@
   ensuring consistency in multi-instance deployments without waiting for the 60-second polling interval."
   [handler]
   (fn [request respond raise]
-    (check-and-update-settings-cache request)
-    (handler request (comp respond maybe-set-settings-last-updated-cookie) raise)))
+    (let [restored? (check-and-update-settings-cache request)]
+      (handler request (comp respond #(maybe-set-settings-last-updated-cookie restored? %)) raise))))
