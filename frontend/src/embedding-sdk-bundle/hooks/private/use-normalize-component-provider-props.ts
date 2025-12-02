2299fcf43baeb4612442999ff0418d020272ee41
Timofei <timofei@metabase.com>
2025-12-02 23:00:54 +0400

    Fix normalize #2

--- frontend/src/embedding-sdk-bundle/hooks/private/use-normalize-component-provider-props.ts
+++ frontend/src/embedding-sdk-bundle/hooks/private/use-normalize-component-provider-props.ts
@@ -1,11 +1,11 @@
 import type { ComponentProviderProps } from "embedding-sdk-bundle/components/public/ComponentProvider";
+import { useSdkSelector } from "embedding-sdk-bundle/store";
 import { getHasTokenFeature } from "embedding-sdk-bundle/store/selectors";
-import { useLazySelector } from "embedding-sdk-shared/hooks/use-lazy-selector";
 
 export const useNormalizeComponentProviderProps = (
   props: Omit<ComponentProviderProps, "children">,
 ): Omit<ComponentProviderProps, "children"> => {
-  const hasTokenFeature = useLazySelector(getHasTokenFeature);
+  const hasTokenFeature = useSdkSelector(getHasTokenFeature);
   const normalizedProps = { ...props };
 
   // For OSS usage we prevent defining a locale or theme
