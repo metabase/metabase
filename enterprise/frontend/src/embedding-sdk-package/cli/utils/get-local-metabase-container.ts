1233ef769aac0f29864a518c42377095c0c54937
Fraser <fraser@metabase.com>
2025-12-04 12:23:56 +1300

    Tidy and test without fallback

--- enterprise/frontend/src/embedding-sdk-package/cli/utils/get-local-metabase-container.ts
+++ enterprise/frontend/src/embedding-sdk-package/cli/utils/get-local-metabase-container.ts
@@ -1,4 +1,4 @@
-import { exec as execCallback } from "node:child_process";
+import { exec as execCallback } from "child_process";
 import { promisify } from "util";
 
 import { safeJsonParse } from "metabase/lib/json-parse";
