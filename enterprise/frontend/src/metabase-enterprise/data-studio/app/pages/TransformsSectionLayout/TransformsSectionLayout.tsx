a7153e316d724e937895ada20bbfcbd7c6751eab
Brad Anderson <brad@metabase.com>
2025-12-03 15:44:25 -0500

    Minor cleanup: makes TransformsSectionLayout use SectionLayout again for simplicity and because we can

--- enterprise/frontend/src/metabase-enterprise/data-studio/app/pages/TransformsSectionLayout/TransformsSectionLayout.tsx
+++ enterprise/frontend/src/metabase-enterprise/data-studio/app/pages/TransformsSectionLayout/TransformsSectionLayout.tsx
@@ -3,8 +3,8 @@ import type { ReactNode } from "react";
 import { t } from "ttag";
 
 import { usePageTitle } from "metabase/hooks/use-page-title";
-import { PLUGIN_METABOT } from "metabase/plugins";
-import { Box, Flex, Stack } from "metabase/ui";
+
+import { SectionLayout } from "../../components/SectionLayout";
 
 type TransformsSectionLayoutProps = {
   location: Location;
@@ -16,14 +16,5 @@ export function TransformsSectionLayout({
 }: TransformsSectionLayoutProps) {
   usePageTitle(t`Transforms`);
 
-  return (
-    <Stack h="100%" gap={0}>
-      <Flex flex={1} mih={0} miw={0}>
-        <Box flex={1} miw={0}>
-          {children}
-        </Box>
-        <PLUGIN_METABOT.MetabotDataStudioSidebar />
-      </Flex>
-    </Stack>
-  );
+  return <SectionLayout>{children}</SectionLayout>;
 }
