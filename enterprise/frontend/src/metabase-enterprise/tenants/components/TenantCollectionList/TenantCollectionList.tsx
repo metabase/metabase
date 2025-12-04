4f60e04304d022071db6b2b5aed983cf0daeb3a7
Phoomparin Mano <phoomparin@gmail.com>
2025-12-04 14:57:36 +0700

    uxw-2456: remove drop shadows from dedicated tenant collection cards

--- enterprise/frontend/src/metabase-enterprise/tenants/components/TenantCollectionList/TenantCollectionList.tsx
+++ enterprise/frontend/src/metabase-enterprise/tenants/components/TenantCollectionList/TenantCollectionList.tsx
@@ -1,20 +1,15 @@
 import { t } from "ttag";
 
 import { BrowserCrumbs } from "metabase/common/components/BrowserCrumbs";
-import Card from "metabase/common/components/Card";
 import { Grid } from "metabase/common/components/Grid";
 import Link from "metabase/common/components/Link";
 import CS from "metabase/css/core/index.css";
 import { ROOT_COLLECTION } from "metabase/entities/collections/constants";
 import * as Urls from "metabase/lib/urls";
-import { Box, Flex, Icon, Loader } from "metabase/ui";
+import { Box, Card, Flex, Group, Icon, Loader } from "metabase/ui";
 import { useListTenantsQuery } from "metabase-enterprise/api";
 
-import {
-  CardContent,
-  ListGridItem,
-  ListHeader,
-} from "./TenantCollectionList.styled";
+import { ListGridItem, ListHeader } from "./TenantCollectionList.styled";
 
 export const TenantCollectionList = () => {
   const { data, isLoading } = useListTenantsQuery({ status: "active" });
@@ -46,16 +41,17 @@ export const TenantCollectionList = () => {
                 tenant.tenant_collection_id && (
                   <ListGridItem key={tenant.id} role="list-item">
                     <Link to={`/collection/${tenant.tenant_collection_id}`}>
-                      <Card className={CS.p2} hoverable>
-                        <CardContent>
+                      <Card shadow="none" withBorder>
+                        <Group gap="xs">
                           <Icon
                             name="folder"
                             className={CS.mr1}
                             c="text-medium"
                             size={18}
                           />
+
                           <h3>{tenant.name}</h3>
-                        </CardContent>
+                        </Group>
                       </Card>
                     </Link>
                   </ListGridItem>
