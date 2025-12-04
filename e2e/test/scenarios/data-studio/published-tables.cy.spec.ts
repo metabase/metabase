d6db287197bd133b218cf4b363270533f96998d8
Alexander Polyankin <alexander.polyankin@metabase.com>
2025-12-03 21:35:25 -0500

    Tests

--- e2e/test/scenarios/data-studio/published-tables.cy.spec.ts
+++ e2e/test/scenarios/data-studio/published-tables.cy.spec.ts
@@ -4,7 +4,7 @@ import { SAMPLE_DB_ID } from "e2e/support/cypress_data";
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import type { QuestionDetails } from "e2e/support/helpers";
 
-const { ORDERS_ID, PRODUCTS_ID } = SAMPLE_DATABASE;
+const { ORDERS_ID, PRODUCTS_ID, PEOPLE_ID } = SAMPLE_DATABASE;
 
 const productsQuestionDetails: QuestionDetails = {
   dataset_query: {
@@ -12,6 +12,16 @@ const productsQuestionDetails: QuestionDetails = {
     database: SAMPLE_DB_ID,
     query: {
       "source-table": PRODUCTS_ID,
+    },
+  },
+};
+
+const peopleQuestionDetails: QuestionDetails = {
+  dataset_query: {
+    type: "query",
+    database: SAMPLE_DB_ID,
+    query: {
+      "source-table": PEOPLE_ID,
     },
   },
 };
@@ -60,13 +70,9 @@ describe("scenarios > data studio > published tables", () => {
       cy.signIn("nodata");
 
       H.visitQuestionAdhoc(productsQuestionDetails);
-      H.tableHeaderClick("Category");
-      H.popover().within(() => {
-        cy.findByText("Filter by this column").click();
-        cy.findByText("Gadget").click();
-        cy.button("Add filter").click();
-      });
-      H.assertQueryBuilderRowCount(53);
+      H.tableInteractive().findByText("82.75").click();
+      H.popover().findByText("=").click();
+      H.assertQueryBuilderRowCount(1);
     });
 
     it("should be able to use explicit joins when not all FK tables are published", () => {
@@ -106,5 +112,42 @@ describe("scenarios > data studio > published tables", () => {
       H.visualize();
       H.assertQueryBuilderRowCount(1);
     });
+
+    it("should be able to use list field values", () => {
+      H.publishTables({ table_ids: [PRODUCTS_ID] });
+      cy.signIn("nodata");
+
+      H.visitQuestionAdhoc(productsQuestionDetails);
+      H.tableHeaderClick("Category");
+      H.popover().within(() => {
+        cy.findByText("Filter by this column").click();
+        cy.findByText("Gadget").click();
+        cy.button("Add filter").click();
+      });
+      H.assertQueryBuilderRowCount(53);
+    });
+
+    it("should be able to use search field values", () => {
+      H.publishTables({ table_ids: [PEOPLE_ID] });
+      cy.signIn("nodata");
+
+      H.visitQuestionAdhoc(peopleQuestionDetails);
+      H.tableHeaderClick("Name");
+      H.popover().findByText("Filter by this column").click();
+      popoverByIndex(1).findByText("Aaron Hand").click();
+      popoverByIndex(0).findByPlaceholderText("Search by Name").type("Myrtle");
+      popoverByIndex(1).findByText("Myrtle Johns").click();
+      popoverByIndex(0).findByPlaceholderText("Search by Name").type("{esc}");
+      H.popover().within(() => {
+        cy.findByText("Aaron Hand").should("be.visible");
+        cy.findByText("Myrtle Johns").should("be.visible");
+        cy.button("Add filter").click();
+      });
+      H.assertQueryBuilderRowCount(2);
+    });
   });
 });
+
+function popoverByIndex(index: number) {
+  return H.popover().should("have.length", 2).eq(index);
+}
