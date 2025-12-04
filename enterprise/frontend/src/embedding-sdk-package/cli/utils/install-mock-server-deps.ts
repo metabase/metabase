a538d34099a380dced07f4bed40ca27440ce7b87
Fraser <fraser@metabase.com>
2025-12-04 15:45:23 +1300

    Remove node prefix

--- enterprise/frontend/src/embedding-sdk-package/cli/utils/install-mock-server-deps.ts
+++ enterprise/frontend/src/embedding-sdk-package/cli/utils/install-mock-server-deps.ts
@@ -1,5 +1,5 @@
 import { exec as execCallback } from "child_process";
-import path from "node:path";
+import path from "path";
 import { promisify } from "util";
 
 import { detect } from "detect-package-manager";
