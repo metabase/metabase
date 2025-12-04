4afcb8e94b0b7e5e43613f86be7f446793586d4e
Ngoc Khuat <qn.khuat@gmail.com>
2025-12-04 20:24:36 +0700

    remove deps on workspaces and make sure we clean up the tables

--- enterprise/backend/test/metabase_enterprise/workspaces/e2e_test.clj
+++ enterprise/backend/test/metabase_enterprise/workspaces/e2e_test.clj
@@ -25,31 +25,32 @@
 
 (deftest isolation-e2e-test
   (mt/test-drivers #_(mt/normal-drivers-with-feature :workspace) #{:postgres #_:h2}
-    (mt/with-model-cleanup [:model/Transform :model/Workspace]
-      (mt/with-temp [:model/Transform {transform-id :id} {:name   "Transform 1"
-                                                          :source {:type  "query"
-                                                                   :query (mt/native-query (mbql->native (mt/mbql-query orders {:limit 1})))}
-                                                          :target {:type "table"
-                                                                   :name (str "test_table_1_" (mt/random-name))}}]
-        (let [workspace             (mt/with-current-user (mt/user->id :crowberto)
-                                      (ws.common/create-workspace! (mt/user->id :crowberto)
-                                                                   {:name        (mt/random-name)
-                                                                    :database_id (mt/id)
-                                                                    :upstream    {:transforms [transform-id]}}))
-              isolated-transform-id (t2/select-one-fn :downstream_id :model/WorkspaceMappingTransform :workspace_id (:id workspace))
-              isolated-transform    (t2/select-one :model/Transform isolated-transform-id)
-              executed-transform    (execute-workspace-transform! workspace isolated-transform {:run-method :manual})
-              output-table          (-> executed-transform :object :output-table)]
-          (testing "execute the transform with the original query is fine and the output is in an isolated schema"
-            (transforms.tu/wait-for-table (name output-table) 1000)
-            (is (str/starts-with? (namespace output-table) "mb__isolation"))
-            (is (= 1 (count (transforms.tu/table-rows (name output-table))))))
+    (transforms.tu/with-transform-cleanup! [output-table-name (str "test_table_1_" (mt/random-name))]
+      (mt/with-model-cleanup [:model/Transform :model/Workspace]
+        (mt/with-temp [:model/Transform {transform-id :id} {:name   "Transform 1"
+                                                            :source {:type  "query"
+                                                                     :query (mt/native-query (mbql->native (mt/mbql-query orders {:limit 1})))}
+                                                            :target {:type "table"
+                                                                     :name output-table-name}}]
+          (let [workspace             (mt/with-current-user (mt/user->id :crowberto)
+                                        (ws.common/create-workspace! (mt/user->id :crowberto)
+                                                                     {:name        (mt/random-name)
+                                                                      :database_id (mt/id)
+                                                                      :upstream    {:transforms [transform-id]}}))
+                isolated-transform-id (t2/select-one-fn :downstream_id :model/WorkspaceMappingTransform :workspace_id (:id workspace))
+                isolated-transform    (t2/select-one :model/Transform isolated-transform-id)
+                executed-transform    (execute-workspace-transform! workspace isolated-transform {:run-method :manual})
+                output-table          (-> executed-transform :object :output-table)]
+            (testing "execute the transform with the original query is fine and the output is in an isolated schema"
+              (transforms.tu/wait-for-table (name output-table) 1000)
+              (is (str/starts-with? (namespace output-table) "mb__isolation"))
+              (is (= 1 (count (transforms.tu/table-rows (name output-table))))))
 
-          (testing "changing the query without granting access will fail"
-            (t2/update! :model/Transform isolated-transform-id {:source {:type  "query"
-                                                                         ;; TODO we should use native query here
-                                                                         :query (mt/mbql-query venues {:limit 1})}})
-            (is (thrown-with-msg?
-                 Exception
-                 #"ERROR: permission denied for table.*"
-                 #p (execute-workspace-transform! workspace (t2/select-one :model/Transform isolated-transform-id) {:run-method :manual})))))))))
+            (testing "changing the query without granting access will fail"
+              (t2/update! :model/Transform isolated-transform-id {:source {:type  "query"
+                                                                          ;; TODO we should use native query here
+                                                                           :query (mt/mbql-query venues {:limit 1})}})
+              (is (thrown-with-msg?
+                   Exception
+                   #"ERROR: permission denied for table.*"
+                   #p (execute-workspace-transform! workspace (t2/select-one :model/Transform isolated-transform-id) {:run-method :manual}))))))))))
