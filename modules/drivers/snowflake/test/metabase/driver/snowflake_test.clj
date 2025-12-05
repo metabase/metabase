6396f7b70adfbe9ecb9a84720511b0bc6db1abf6
Riley Thompson <riley@metabase.com>
2025-12-05 12:59:53 -0500

    backport merge

--- modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
+++ modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
@@ -1399,5 +1399,27 @@
                                                  []]]]
         (testing msg
           (let [result (qp/process-query (filter-query filter))]
-            (is (= exp-rows (mt/rows result)))
-            (is (str/includes? (-> result :data :native_form :query) exp-filter))))))))
+            (is (str/includes? (-> result :data :native_form :query) exp-filter))
+            (is (= exp-rows (mt/rows result)))))))))
+
+(deftest snowflake-with-dbname-in-details-gets-synced-test
+  (testing "db with a valid db and an invalid dbname in details should be synced with db correctly"
+    (mt/test-driver :snowflake
+      (let [priv-key-val (mt/priv-key->base64-uri (tx/db-test-env-var-or-throw :snowflake :private-key))]
+        (mt/with-temp [:model/Database db {:engine :snowflake
+                                           :details (-> (:details (mt/db))
+                                                        (dissoc :private-key-id)
+                                                        (assoc :private-key-options "uploaded")
+                                                        (assoc :private-key-value priv-key-val)
+                                                        (assoc :use-password false)
+                                                        (assoc :dbname nil))}]
+          (is (= {:tables
+                  #{{:name "users",      :schema "PUBLIC", :description nil}
+                    {:name "venues",     :schema "PUBLIC", :description nil}
+                    {:name "checkins",   :schema "PUBLIC", :description nil}
+                    {:name "categories", :schema "PUBLIC", :description nil}
+                    {:name "orders",     :schema "PUBLIC", :description nil}
+                    {:name "people",     :schema "PUBLIC", :description nil}
+                    {:name "products",   :schema "PUBLIC", :description nil}
+                    {:name "reviews",    :schema "PUBLIC", :description nil}}}
+                 (driver/describe-database :snowflake db))))))))
