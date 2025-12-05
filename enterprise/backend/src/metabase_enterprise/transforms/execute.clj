4f98f13971d9ee8bfc4f71dc20a7f8614886d088
lbrdnk <lbrdnk@users.noreply.github.com>
2025-12-05 12:40:36 +0100

    Hack to make transforms exec work until there is ws specific api

--- enterprise/backend/src/metabase_enterprise/transforms/execute.clj
+++ enterprise/backend/src/metabase_enterprise/transforms/execute.clj
@@ -9,10 +9,14 @@
 
   For workspace transforms, execution must go through the workspace-specific endpoint.
   Calling this function directly with a workspace transform will throw a 404 error."
-  [transform opts]
-  (when (:workspace_id transform)
-    (throw (ex-info "Workspace transforms must be executed through the workspace API"
-                    {:status-code 404
-                     :transform-id (:id transform)
-                     :workspace-id (:workspace_id transform)})))
-  (transforms.i/execute! transform opts))
+  [transform {:keys [start-promise] :as opts}]
+  ;; This is used on FE for workspace transform execution until proper API is provided in workspaces module!
+  ;; Hence temp enabling that.
+  ;; TODO: Cleanup when moving to woskapce transform execution api.
+  (if false #_(:workspace_id transform)
+      (deliver start-promise
+               (ex-info "Workspace transforms must be executed through the workspace API"
+                        {:status-code 404
+                         :transform-id (:id transform)
+                         :workspace-id (:workspace_id transform)}))
+      (transforms.i/execute! transform opts)))
