b6e6d996049740f038126339ae115e233da23f3c
Noah Moss <noahbmoss@gmail.com>
2025-12-05 11:58:00 -0500

    more endpoint migrations

--- enterprise/backend/src/metabase_enterprise/metabot_v3/tools/snippets.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/snippets.clj
@@ -15,7 +15,7 @@
 
 (defn get-snippet-details
   "Retrieve a specific SQL snippet by ID, including its content."
-  [snippet-id]
+  [{:keys [snippet-id]}]
   (try
     {:structured_output
      (when snippet-id
