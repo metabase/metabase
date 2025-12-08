dc4b0b51d52e44a5d7da23b57c572f0ddfd3a10c
github-automation-metabase <166700802+github-automation-metabase@users.noreply.github.com>
2025-12-09 00:54:46 +0200

    Fix issue where having a metabot block inside a document prevents it from loading when publicly shared (#66564) (#66628)
    Co-authored-by: Brad Anderson <brad@metabase.com>

--- enterprise/frontend/src/metabase-enterprise/rich_text_editing/tiptap/extensions/MetabotEmbed/__support__/editor-mocks.ts
+++ enterprise/frontend/src/metabase-enterprise/rich_text_editing/tiptap/extensions/MetabotEmbed/__support__/editor-mocks.ts
@@ -1,6 +1,6 @@
 import type { Editor } from "@tiptap/react";
 
-export const createMockEditor = (overrides = {}): Editor =>
+export const createMockEditor = (overrides: Partial<Editor> = {}): Editor =>
   ({
     on: jest.fn(),
     off: jest.fn(),
@@ -14,6 +14,10 @@ export const createMockEditor = (overrides = {}): Editor =>
     state: {
       selection: { $from: {}, empty: true },
       doc: { nodeAt: jest.fn(), resolve: jest.fn() },
+    },
+    options: {
+      editable: true,
+      ...overrides.options,
     },
     ...overrides,
   }) as unknown as Editor;
