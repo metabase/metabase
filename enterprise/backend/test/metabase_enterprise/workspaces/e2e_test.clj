5258f030adc3c86aebe4d5696a7a501d22020f5f
Ngoc Khuat <qn.khuat@gmail.com>
2025-12-09 19:24:25 +0700

    Test fixes

--- enterprise/backend/test/metabase_enterprise/workspaces/e2e_test.clj
+++ enterprise/backend/test/metabase_enterprise/workspaces/e2e_test.clj
@@ -11,6 +11,7 @@
    [metabase.query-processor.preprocess :as qp.preprocess]
    ^{:clj-kondo/ignore [:deprecated-namespace]} [metabase.query-processor.store :as qp.store]
    [metabase.test :as mt]
+   [metabase.util :as u]
    [toucan2.core :as t2]))
 
 (defn- execute-workspace-transform!
@@ -31,26 +32,32 @@
                                                             :source {:type  "query"
                                                                      :query (mt/native-query (mbql->native (mt/mbql-query orders {:limit 1})))}
                                                             :target {:type "table"
-                                                                     :name output-table-name}}]
-          (let [workspace             (mt/with-current-user (mt/user->id :crowberto)
-                                        (ws.common/create-workspace! (mt/user->id :crowberto)
-                                                                     {:name        (mt/random-name)
-                                                                      :database_id (mt/id)
-                                                                      :upstream    {:transforms [transform-id]}}))
-                isolated-transform-id (t2/select-one-fn :downstream_id :model/WorkspaceMappingTransform :workspace_id (:id workspace))
-                isolated-transform    (t2/select-one :model/Transform isolated-transform-id)
-                executed-transform    (execute-workspace-transform! workspace isolated-transform {:run-method :manual})
-                output-table          (-> executed-transform :object :output-table)]
+                                                                     :name output-table-name
+                                                                     :database (mt/id)
+                                                                     :schema (t2/select-one-fn :schema :model/Table (mt/id :orders))}}]
+          (let [workspace          (mt/with-current-user (mt/user->id :crowberto)
+                                     (ws.common/create-workspace! (mt/user->id :crowberto)
+                                                                  {:name        (mt/random-name)
+                                                                   :database_id (mt/id)
+                                                                   :upstream    {:transforms [transform-id]}}))
+                workspace          (u/poll {:thunk      #(t2/select-one :model/Workspace (:id workspace))
+                                            :done?      #(= :ready (:status %))
+                                            :timeout-ms 5000})
+                isolated-transform (t2/select-one :model/Transform :workspace_id (:id workspace))
+                executed-transform (execute-workspace-transform! workspace isolated-transform {:run-method :manual})
+                output-table       (-> executed-transform :object :output-table)]
             (testing "execute the transform with the original query is fine and the output is in an isolated schema"
+              (u/poll {:thunk     #(t2/select-one :model/Table :name (name output-table))
+                       :done?      some?
+                       :timeout-ms 1000})
               (transforms.tu/wait-for-table (name output-table) 1000)
               (is (str/starts-with? (namespace output-table) "mb__isolation"))
               (is (= 1 (count (transforms.tu/table-rows (name output-table))))))
 
             (testing "changing the query without granting access will fail"
-              (t2/update! :model/Transform isolated-transform-id {:source {:type  "query"
-                                                                          ;; TODO we should use native query here
-                                                                           :query (mt/mbql-query venues {:limit 1})}})
+              (t2/update! :model/Transform (:id isolated-transform) {:source {:type  "query"
+                                                                              :query (mt/native-query (mbql->native (mt/mbql-query venues {:limit 1})))}})
               (is (thrown-with-msg?
                    Exception
                    #"ERROR: permission denied for table.*"
-                   #p (execute-workspace-transform! workspace (t2/select-one :model/Transform isolated-transform-id) {:run-method :manual}))))))))))
+                   (execute-workspace-transform! workspace (t2/select-one :model/Transform (:id isolated-transform)) {:run-method :manual}))))))))))
