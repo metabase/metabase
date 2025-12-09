70b9ebc1f00fef3823df243f126e120f11f58ab2
Brad Anderson <brad@metabase.com>
2025-12-09 09:17:17 -0500

    Imports isPublicEmbedding to prevent app crash

--- frontend/src/metabase/styled-components/containers/GlobalStyles/GlobalStyles.tsx
+++ frontend/src/metabase/styled-components/containers/GlobalStyles/GlobalStyles.tsx
@@ -4,7 +4,10 @@ import { useMemo } from "react";
 
 import { baseStyle, rootStyle } from "metabase/css/core/base.styled";
 import { defaultFontFiles } from "metabase/css/core/fonts.styled";
-import { isStaticEmbedding } from "metabase/embedding/config";
+import {
+  isPublicEmbedding,
+  isStaticEmbedding,
+} from "metabase/embedding/config";
 import { getSitePath } from "metabase/lib/dom";
 import { useSelector } from "metabase/lib/redux";
 import { getMetabaseCssVariables } from "metabase/styled-components/theme/css-variables";
