6c70f48edb3374a6cd8c5df8885ccfba83dfdbe0
Oleksandr Yakushev <alex@bytopia.org>
2025-12-09 17:29:21 +0200

    [retry] Rewrite metabase.util.retry to use diehard instead of resilience4j (#66177)

--- docs/configuring-metabase/environment-variables.md
+++ docs/configuring-metabase/environment-variables.md
@@ -1403,13 +1403,13 @@ Number of hours a password reset is considered valid.
 
 The initial retry delay in milliseconds.
 
-### `MB_RETRY_MAX_ATTEMPTS`
+### `MB_RETRY_MAX_RETRIES`
 
 - Type: integer
-- Default: `7`
-- [Configuration file name](./config-file.md): `retry-max-attempts`
+- Default: `6`
+- [Configuration file name](./config-file.md): `retry-max-retries`
 
-The maximum number of attempts for an event.
+The maximum number of retries for a failed event.
 
 ### `MB_RETRY_MAX_INTERVAL_MILLIS`
 
@@ -1431,7 +1431,7 @@ The delay multiplier between attempts.
 
 - Type: double
 - Default: `0.1`
-- [Configuration file name](./config-file.md): `retry-randomization-factor`
+- [Configuration file name](./config-file.md): `retry-jitter-factor`
 
 The randomization factor of the retry delay.
 
--- test/metabase/util/retry_test.clj
+++ test/metabase/util/retry_test.clj
@@ -4,62 +4,67 @@
    [metabase.test.util :as tu]
    [metabase.util.retry :as retry])
   (:import
-   (clojure.lang ExceptionInfo)
-   [io.github.resilience4j.retry Retry]))
-
-(set! *warn-on-reflection* true)
-
-(defn test-retry-decorate-fn
-  "Decorates a function with a retrying mechanism."
-  [retry]
-  (fn [f]
-    (fn [& args]
-      (let [callable (reify Callable (call [_] (apply f args)))]
-        (.call (Retry/decorateCallable retry callable))))))
+   (clojure.lang ExceptionInfo)))
 
 (deftest retrying-on-exception-test
   (testing "recovery possible"
     (let [f +
-          retries-needed 3
-          flaky-f (tu/works-after (* 2 retries-needed) f)
-          retry-opts (assoc (#'retry/retry-configuration)
-                            :max-attempts retries-needed
-                            :max-interval-millis 1)
-          retry (retry/random-exponential-backoff-retry "test-retry" retry-opts)
-          retrying-f (retry/decorate flaky-f retry)
-          params (range 6)]
-      (is (thrown? ExceptionInfo (apply flaky-f params)))
-      (is (thrown? ExceptionInfo (apply retrying-f params)))
-      (is (= (apply f params) (apply retrying-f params)))))
+          params (range 6)
+          works-after 3
+          flaky-f (tu/works-after works-after f)]
+      (is (= (apply f params)
+             (retry/with-retry (assoc (retry/retry-configuration)
+                                      :max-retries works-after
+                                      :initial-interval-millis 1)
+               (apply flaky-f params))))))
   (testing "recovery impossible"
     (let [f +
-          retries-needed 1
-          flaky-f (tu/works-after retries-needed f)
-          retry-opts (assoc (#'retry/retry-configuration)
-                            :max-attempts retries-needed
-                            :max-interval-millis 1
-                            :retry-on-exception-pred #(-> % ex-data :remaining nil?))
-          retry (retry/random-exponential-backoff-retry "test-retry" retry-opts)
-          retrying-f (retry/decorate flaky-f retry)
-          params (range 6)]
-      (is (thrown? ExceptionInfo (apply retrying-f params))))))
+          params (range 6)
+          works-after 3
+          flaky-f (tu/works-after works-after f)]
+      (is (thrown? ExceptionInfo
+                   (retry/with-retry (assoc (retry/retry-configuration)
+                                            :max-retries (dec works-after)
+                                            :initial-interval-millis 1)
+                     (apply flaky-f params)))))))
 
 (deftest retrying-on-result-test
   (testing "recovery possible"
     (let [a (atom 0)
-          f #(swap! a inc)
-          retry-opts (assoc (#'retry/retry-configuration)
-                            :retry-on-result-pred odd?
-                            :max-interval-millis 1)
-          retry (retry/random-exponential-backoff-retry "test-retry" retry-opts)
-          retrying-f (retry/decorate f retry)]
-      (is (= 2 (retrying-f)))))
+          f #(swap! a inc)]
+      (is (= 2 (retry/with-retry (assoc (retry/retry-configuration)
+                                        :max-retries 1
+                                        :retry-if (fn [val _] (odd? val))
+                                        :initial-interval-millis 1)
+                 (f))))))
 
   (testing "recovery impossible"
-    (let [f (constantly 1)
-          retry-opts (assoc (#'retry/retry-configuration)
-                            :retry-on-result-pred odd?
-                            :max-interval-millis 1)
-          retry (retry/random-exponential-backoff-retry "test-retry" retry-opts)
-          retrying-f (retry/decorate f retry)]
-      (is (= 1 (retrying-f))))))
+    (let [f (constantly 1)]
+      (is (= 1 (retry/with-retry (assoc (retry/retry-configuration)
+                                        :max-retries 1
+                                        :retry-if (fn [val _] (odd? val))
+                                        :initial-interval-millis 1)
+                 (f)))))))
+
+;; For use by other tests.
+
+(defn retry-analytics-config-hook [& [extra-config]]
+  (let [state (atom {:retries 0})]
+    [(fn [config]
+       (-> config
+           (assoc :initial-interval-millis 1
+                  :max-retries 1)
+           (merge extra-config)
+           (update :on-success (fn [f]
+                                 (fn [res]
+                                   (swap! state assoc :success true)
+                                   (when f (f res)))))
+           (update :on-failure (fn [f]
+                                 (fn [res ex]
+                                   (swap! state assoc :success false)
+                                   (when f (f res ex)))))
+           (update :on-retry (fn [f]
+                               (fn [res ex]
+                                 (swap! state update :retries inc)
+                                 (when f (f res ex)))))))
+     state]))
