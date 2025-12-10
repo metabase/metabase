22b5766c406f914ab7f6e73d0746561f5ddb6100
Edward Paget <edpaget@users.noreply.github.com>
2025-12-10 14:36:44 -0600

    Update EntityPickerModal.tsx

--- frontend/src/metabase/common/components/EntityPicker/components/EntityPickerModal/EntityPickerModal.tsx
+++ frontend/src/metabase/common/components/EntityPicker/components/EntityPickerModal/EntityPickerModal.tsx
@@ -259,8 +259,6 @@ export function EntityPickerModal<
 
     computedTabs.push(...passedTabs);
 
-    // console.log({ selectedFolder, initialValue, selectedTabId });
-
     if (hasSearchTab) {
       computedTabs.push({
         id: SEARCH_TAB_ID,
