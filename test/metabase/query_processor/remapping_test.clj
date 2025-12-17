0a0aef1053402c14d6fc8000ce54c955e5a959ae
Eric Normand <ericwnormand@gmail.com>
2025-12-16 19:52:15 -0600

    Move tests and add one for retired

--- test/metabase/query_processor/remapping_test.clj
+++ test/metabase/query_processor/remapping_test.clj
@@ -560,3 +560,47 @@
                (mt/formatted-rows [int str str str str str 2.0 str str str str 2.0 str
                                    int int 2.0 2.0 2.0 2.0 str int str str]
                                   (qp/process-query query))))))))
+
+(deftest ^:parallel fk-do-not-include-should-not-break-test
+  (testing "Check that we don't error when there's a do-not-include (sensitive) foreign key (#64050)"
+    (let [mp (-> (mt/metadata-provider)
+                 (lib.tu/remap-metadata-provider (mt/id :orders :product_id)
+                                                 (mt/id :products :title))
+                 (lib.tu/merged-mock-metadata-provider
+                  {:fields [{:id (mt/id :products :id)
+                             :visibility-type :sensitive}]}))
+          query (-> (lib/query mp (lib.metadata/table mp (mt/id :orders)))
+                    (lib/limit 1))]
+      (is (seq (mt/rows (qp/process-query query)))))))
+
+(deftest ^:parallel fk-retired-should-not-break-test
+  (testing "Check that we don't error when there's a retired foreign key (#64050)"
+    (let [mp (-> (mt/metadata-provider)
+                 (lib.tu/remap-metadata-provider (mt/id :orders :product_id)
+                                                 (mt/id :products :title))
+                 (lib.tu/merged-mock-metadata-provider
+                  {:fields [{:id (mt/id :products :id)
+                             :visibility-type :retired}]}))
+          query (-> (lib/query mp (lib.metadata/table mp (mt/id :orders)))
+                    (lib/limit 1))]
+      (is (seq (mt/rows (qp/process-query query)))))))
+
+(deftest ^:parallel fk-do-not-include-should-not-break-nested-test
+  (testing "Check that we don't error when there's a do-not-include (sensitive) foreign key (#64050)"
+    (let [mp (-> (mt/metadata-provider)
+                 (lib.tu/remap-metadata-provider (mt/id :orders :product_id)
+                                                 (mt/id :products :title))
+                 (lib.tu/merged-mock-metadata-provider
+                  {:fields [{:id (mt/id :products :id)
+                             :visibility-type :sensitive}]}))
+          query (-> (lib/query mp (lib.metadata/table mp (mt/id :orders)))
+                    (lib/limit 1))
+          mp (lib.tu/mock-metadata-provider mp {:cards
+                                                [{:id              1
+                                                  :name            "NATIVE"
+                                                  :database-id     (mt/id)
+                                                  :dataset-query   query
+                                                  :type :model}]})]
+      (let [q2 (-> (lib/query mp (lib.metadata/card mp 1))
+                   (lib/limit 1))]
+        (is (seq (mt/rows (qp/process-query q2))))))))
