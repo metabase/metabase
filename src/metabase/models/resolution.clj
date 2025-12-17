9ac4a2840b26c39d8766f2bf6788cdb1d157b087
lbrdnk <lbrdnk@users.noreply.github.com>
2025-12-17 17:30:15 +0100

    Fix drop-entity-ids-test

--- src/metabase/models/resolution.clj
+++ src/metabase/models/resolution.clj
@@ -111,7 +111,8 @@
     :model/UserKeyValue                      metabase.user-key-value.models.user-key-value
     :model/UserParameterValue                metabase.users.models.user-parameter-value
     :model/ViewLog                           metabase.view-log.models.view-log
-    :model/Workspace                         metabase-enterprise.workspaces.models.workspace})
+    :model/Workspace                         metabase-enterprise.workspaces.models.workspace
+    :model/WorkspaceTransform                metabase-enterprise.workspaces.models.workspace})
 
 ;;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ;;; !!                                                                                                !!
