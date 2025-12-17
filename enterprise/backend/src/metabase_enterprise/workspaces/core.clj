89e92ee71f24ffc4982b0c82bb851299da9895d9
Chris Truter <chris@metabase.com>
2025-12-17 14:25:43 +0200

    Remove exports from workspaces.core

--- enterprise/backend/src/metabase_enterprise/workspaces/core.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/core.clj
@@ -1,13 +1,2 @@
-(ns metabase-enterprise.workspaces.core
-  (:require
-   [metabase-enterprise.workspaces.dependencies]
-   [metabase-enterprise.workspaces.isolation]
-   [potemkin :as p]))
-
-(p/import-vars
- [metabase-enterprise.workspaces.dependencies
-  analyze-entity
-  write-dependencies!]
-
- [metabase-enterprise.workspaces.isolation
-  with-workspace-isolation])
+;; For now, we do not anticipate any non-API usage of this module.
+(ns metabase-enterprise.workspaces.core)
