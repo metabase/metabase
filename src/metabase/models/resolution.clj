694c574514b0a43837bb01dc5af288c5dacd2c45
William Zimrin <william@metabase.com>
2025-12-18 17:51:02 -0600

    fix another test

--- src/metabase/models/resolution.clj
+++ src/metabase/models/resolution.clj
@@ -13,6 +13,7 @@
 
   Tests will check to make sure new models get included in this map."
   '{:model/Action                            metabase.actions.models
+    :model/AnalysisFinding                   metabase-enterprise.dependencies.models.analysis-finding
     :model/ApiKey                            metabase.api-keys.models.api-key
     :model/ApplicationPermissionsRevision    metabase.permissions.models.application-permissions-revision
     :model/AuditLog                          metabase.audit-app.models.audit-log
