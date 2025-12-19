c2b0bab2d9ce48a0ac8b8e75f1858bb58e5c16e5
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-12-19 18:41:02 +0700

    Fix more tests

--- test/metabase/pulse/models/pulse_test.clj
+++ test/metabase/pulse/models/pulse_test.clj
@@ -52,7 +52,8 @@
    :dashboard_id        nil
    :skip_if_empty       false
    :archived            false
-   :parameters          []})
+   :parameters          []
+   :creation_context    nil})
 
 (deftest retrieve-pulse-test
   (testing "this should cover all the basic Pulse attributes"
