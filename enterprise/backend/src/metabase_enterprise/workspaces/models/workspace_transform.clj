347fd7039d7bf3b98217cd46806f1567aaa799b4
lbrdnk <lbrdnk@users.noreply.github.com>
2025-12-19 14:21:32 +0100

    Throw on missing ref_id

--- enterprise/backend/src/metabase_enterprise/workspaces/models/workspace_transform.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/models/workspace_transform.clj
@@ -101,5 +101,7 @@
 
 (t2/define-before-insert :model/WorkspaceTransform
   [instance]
-  (cond-> instance
-    (not (:ref_id instance)) (assoc :ref_id (generate-ref-id))))
+  (when-not (string? (not-empty (:ref_id instance)))
+    (throw (ex-info "ref_id required for WorkspaceTransform insertion."
+                    {:instance instance})))
+  instance)
