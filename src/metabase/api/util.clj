aa82747818a63e3461feb0b09a34c93029e38f06
Stas Gavrylov <stasgavrylov@gmail.com>
2025-12-19 05:47:06 -0800

    Add schema for /random_token

--- src/metabase/api/util.clj
+++ src/metabase/api/util.clj
@@ -8,13 +8,8 @@
 (set! *warn-on-reflection* true)
 
 ;; TODO (Cam 10/28/25) -- fix this endpoint route to use kebab-case for consistency with the rest of our REST API
-;;
-;; TODO (Cam 2025-11-25) please add a response schema to this API endpoint, it makes it easier for our customers to
-;; use our API + we will need it when we make auto-TypeScript-signature generation happen
-;;
-#_{:clj-kondo/ignore [:metabase/validate-defendpoint-route-uses-kebab-case
-                      :metabase/validate-defendpoint-has-response-schema]}
-(api.macros/defendpoint :get "/random_token"
+#_{:clj-kondo/ignore [:metabase/validate-defendpoint-route-uses-kebab-case]}
+(api.macros/defendpoint :get "/random_token" :- [:map [:token :string]]
   "Return a cryptographically secure random 32-byte token, encoded as a hexadecimal string.
    Intended for use when creating a value for `embedding-secret-key`."
   []
