316296a5cdbd6dbe781fddbb767998fc1aa3c06f
Phoomparin Mano <phoomparin@gmail.com>
2025-12-19 19:03:10 +0700

    fix failing test

--- e2e/test/scenarios/admin-2/tenants.cy.spec.ts
+++ e2e/test/scenarios/admin-2/tenants.cy.spec.ts
@@ -786,11 +786,7 @@ describe("tenant users", () => {
       cy.visit(`/auth/sso?return_to=/question/notebook&jwt=${key}`),
     );
 
-    H.miniPickerBrowseAll().click();
-    H.entityPickerModal().within(() => {
-      H.entityPickerModalItem(0, "Databases").click();
-      H.entityPickerModalItem(1, "Products").click();
-    });
+    H.popover().findByText("Products").click();
     cy.button("Visualize").click();
 
     cy.get("[data-column-id=CATEGORY]")
@@ -806,11 +802,7 @@ describe("tenant users", () => {
       cy.visit(`/auth/sso?return_to=/question/notebook&jwt=${key}`),
     );
 
-    H.miniPickerBrowseAll().click();
-    H.entityPickerModal().within(() => {
-      H.entityPickerModalItem(0, "Databases").click();
-      H.entityPickerModalItem(1, "Products").click();
-    });
+    H.popover().findByText("Products").click();
     cy.button("Visualize").click();
 
     cy.get("[data-column-id=CATEGORY]")
