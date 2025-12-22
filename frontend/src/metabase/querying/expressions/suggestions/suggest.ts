ce3c1439b1e21f3efde4370a36a5e55a85401748
Romeo Van Snick <romeo@romeovansnick.be>
2025-12-22 13:04:38 +1100

    Add suggestions for measures

--- frontend/src/metabase/querying/expressions/suggestions/suggest.ts
+++ frontend/src/metabase/querying/expressions/suggestions/suggest.ts
@@ -16,6 +16,7 @@ import { suggestAggregations } from "./aggregations";
 import { suggestFields } from "./fields";
 import { suggestFunctions } from "./functions";
 import { suggestLiterals } from "./literals";
+import { suggestMeasures } from "./measures";
 import { suggestMetrics } from "./metrics";
 import { suggestSegments } from "./segments";
 
@@ -30,6 +31,7 @@ export function suggestions(options: SuggestOptions) {
       suggestAggregations(options),
       suggestFields(options),
       suggestMetrics(options),
+      suggestMeasures(options),
       suggestSegments(options),
     ].filter(isNotNull),
   });
