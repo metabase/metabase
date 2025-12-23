8a9b4fbd05db2c9f5dbbf6a759e39603475b6968
Oleksandr Solovyov <o@solovyov.net>
2025-12-23 14:57:47 +0200

    do not choose sample database

--- enterprise/backend/src/metabase_enterprise/workspaces/api.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/api.clj
@@ -269,7 +269,7 @@
   []
   (:id
    (u/seek #(driver.u/supports? (:engine %) :workspace %)
-           (t2/select :model/Database {:order-by [:id]}))))
+           (t2/select :model/Database :is_sample false {:order-by [:id]}))))
 
 (api.macros/defendpoint :post "/" :- Workspace
   "Create a new workspace
