09bcab9e5dff117f5f166a72139909e87ee90468
Timofei <timofei@metabase.com>
2025-12-25 01:03:03 +0400

    Add reproduction #2

--- e2e/test/scenarios/embedding/sdk-iframe-embedding/metabase-browser.cy.spec.ts
+++ e2e/test/scenarios/embedding/sdk-iframe-embedding/metabase-browser.cy.spec.ts
@@ -1,7 +1,5 @@
-import { USERS } from "e2e/support/cypress_data";
 import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
-import { ALL_USERS_GROUP_ID } from "e2e/support/cypress_sample_instance_data";
-import { mockAuthProviderAndJwtSignIn } from "e2e/support/helpers/embedding-sdk-testing/embedding-sdk-helpers";
+import { DATA_GROUP_ID } from "e2e/support/cypress_sample_instance_data";
 import type { CollectionPermission } from "metabase-types/api";
 
 const { H } = cy;
@@ -11,8 +9,6 @@ const READ: CollectionPermission = "read";
 const NONE: CollectionPermission = "none";
 
 const setupEmbed = (elementHtml: string) => {
-  cy.intercept("GET", "/api/collection/*").as("getCollection");
-
   H.visitCustomHtmlPage(`
     ${H.getNewEmbedScriptTag()}
     ${H.getNewEmbedConfigurationScript({})}
@@ -23,20 +19,22 @@ const setupEmbed = (elementHtml: string) => {
 describe("scenarios > embedding > sdk iframe embedding > metabase-browser", () => {
   describe("collection permissions", () => {
     it("should show an error when initial-collection points to a collection the user has no access to", () => {
-      cy.signInAsAdmin();
-      H.prepareSdkIframeEmbedTest({ withToken: "bleeding-edge" });
+      H.prepareSdkIframeEmbedTest({
+        withToken: "bleeding-edge",
+        signOut: false,
+      });
 
       H.createCollection({
         name: "Restricted Collection",
       }).then(({ body: collection }) => {
         cy.updateCollectionGraph({
-          [ALL_USERS_GROUP_ID]: {
+          [DATA_GROUP_ID]: {
             root: READ,
             [collection.id]: NONE,
           },
         });
 
-        mockAuthProviderAndJwtSignIn(USERS.nocollection);
+        cy.signIn("nocollection");
 
         setupEmbed(`
           <metabase-browser
@@ -45,23 +43,26 @@ describe("scenarios > embedding > sdk iframe embedding > metabase-browser", () =
           />
         `);
 
-        cy.wait("@getCollection");
-
         H.getSimpleEmbedIframeContent().within(() => {
+          cy.findByText("You don't have access to this collection").should(
+            "be.visible",
+          );
           cy.findByText("New exploration").should("not.exist");
         });
       });
     });
 
-    it("should not allow saving when user has no curate permissions on initial-collection (reproduces the bug)", () => {
-      cy.signInAsAdmin();
-      H.prepareSdkIframeEmbedTest({ withToken: "bleeding-edge" });
+    it("should not show New exploration button when user has no curate permissions on initial-collection", () => {
+      H.prepareSdkIframeEmbedTest({
+        withToken: "bleeding-edge",
+        signOut: false,
+      });
 
       H.createCollection({
         name: "Read Only Collection",
       }).then(({ body: collection }) => {
         cy.updateCollectionGraph({
-          [ALL_USERS_GROUP_ID]: {
+          [DATA_GROUP_ID]: {
             root: READ,
             [collection.id]: READ,
           },
@@ -73,7 +74,7 @@ describe("scenarios > embedding > sdk iframe embedding > metabase-browser", () =
           collection_id: collection.id,
         });
 
-        mockAuthProviderAndJwtSignIn(USERS.nocollection);
+        cy.signIn("nocollection");
 
         setupEmbed(`
           <metabase-browser
@@ -83,18 +84,11 @@ describe("scenarios > embedding > sdk iframe embedding > metabase-browser", () =
         `);
 
         H.getSimpleEmbedIframeContent().within(() => {
-          cy.findByText("New exploration").should("be.visible").click();
+          // User can see the collection contents (they have read access)
+          cy.findByText("Test Question").should("be.visible");
 
-          cy.findByText("Pick your starting data").should("be.visible");
-          cy.findByText("Orders").click();
-
-          cy.findByRole("button", { name: "Visualize" }).should("be.visible");
-
-          cy.findByRole("button", { name: "Visualize" }).click();
-
-          cy.findByTestId("visualization-root").should("be.visible");
-
-          cy.findByText("Save").should("not.exist");
+          // But New exploration button should be hidden since they can't save
+          cy.findByText("New exploration").should("not.exist");
         });
       });
     });
