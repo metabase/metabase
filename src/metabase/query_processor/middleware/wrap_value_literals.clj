8e1c4d6c19eec9c2048d44d1c6d436f1c82ef07a
Eric Normand <ericwnormand@gmail.com>
2025-12-29 08:42:35 -0600

    Use y-type for effective type

--- src/metabase/query_processor/middleware/wrap_value_literals.clj
+++ src/metabase/query_processor/middleware/wrap_value_literals.clj
@@ -235,7 +235,7 @@
           y-type (lib.schema.expression/type-of y)]
       [tag opts
        (add-type-info x {:base-type x-type :effective-type x-type})
-       (add-type-info y {:base-type y-type :effective-type x-type})])
+       (add-type-info y {:base-type y-type :effective-type y-type})])
 
     ;; field and literal
     [(tag :guard #{:= :!= :< :> :<= :>=}) opts field (x :guard raw-value?)]
