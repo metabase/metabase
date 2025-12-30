0bdd453e4b7cde7fbcefb05c0ce0cb366919f1ba
Romeo Van Snick <romeo@romeovansnick.be>
2025-12-30 17:17:11 +1100

    Add test for measure dependencies

--- e2e/test/scenarios/data-studio/measures/measures-queries.cy.spec.ts
+++ e2e/test/scenarios/data-studio/measures/measures-queries.cy.spec.ts
@@ -779,6 +779,62 @@ describe("scenarios > data studio > measures > queries", () => {
           .should("not.exist");
       });
     });
+
+    it("should show all of a measure's dependencies", () => {
+      H.createMeasure({
+        name: "Other Measure",
+        table_id: ORDERS_ID,
+        definition: {
+          "source-table": ORDERS_ID,
+          aggregation: [["count"]],
+        },
+      }).then(({ body: otherMeasure }) => {
+        H.createSegment({
+          name: "Table Segment",
+          table_id: ORDERS_ID,
+          definition: {
+            "source-table": ORDERS_ID,
+            filter: [">", ["field", ORDERS.TOTAL, null], 100],
+          },
+        }).then(({ body: segment }) => {
+          H.createMeasure({
+            name: MEASURE_NAME,
+            table_id: ORDERS_ID,
+            definition: {
+              "source-table": ORDERS_ID,
+              aggregation: [
+                // TODO: this does not work for some reason
+                [
+                  "+",
+                  1,
+                  ["measure", otherMeasure.id],
+                  ["count-where", ["segment", segment.id]],
+                ],
+              ],
+            },
+          }).then(({ body: measure }) => {
+            cy.visit(`/data-studio/dependencies?id=${measure.id}&type=measure`);
+
+            cy.log("verify table dependency");
+            H.DependencyGraph.graph()
+              .findByLabelText("Orders")
+              .should("be.visible")
+              .within(() => {
+                cy.findByText("1 segment").should("be.visible");
+                cy.findByText("2 measures").should("be.visible");
+              });
+
+            cy.log("verify measure dependency");
+            H.DependencyGraph.graph()
+              .findByLabelText("Other Measure")
+              .should("be.visible")
+              .within(() => {
+                cy.findByText("1 measure").should("be.visible");
+              });
+          });
+        });
+      });
+    });
   });
 });
 
