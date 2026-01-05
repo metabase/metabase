89b302cdf05bc3a5288c39278d370520c3895401
Nicola Mometto <nicola.mometto@metabase.com>
2026-01-05 15:08:40 +0000

    configurable interval, clamped between 1 min and 12 hours

--- src/metabase/premium_features/task/send_metering.clj
+++ src/metabase/premium_features/task/send_metering.clj
@@ -4,12 +4,25 @@
    [clojurewerkz.quartzite.jobs :as jobs]
    [clojurewerkz.quartzite.schedule.simple :as simple]
    [clojurewerkz.quartzite.triggers :as triggers]
+   [metabase.premium-features.settings :as settings]
    [metabase.premium-features.token-check :as token-check]
    [metabase.task.core :as task]
    [metabase.util :as u]
    [metabase.util.log :as log]))
 
 (set! *warn-on-reflection* true)
+
+(def ^:private min-interval-ms (u/minutes->ms 1))
+(def ^:private max-interval-ms (u/hours->ms 12))
+(def ^:private default-interval-ms (u/minutes->ms 15))
+
+(defn- metering-interval-ms
+  "Returns the metering interval in milliseconds, clamped between 1 minute and 12 hours.
+   Defaults to 15 minutes if not set."
+  []
+  (if-let [v (settings/send-metering-interval-ms)]
+    (max min-interval-ms (min max-interval-ms v))
+    default-interval-ms))
 
 (task/defjob ^{:doc "Send metering events for billing purposes"}
   SendMeteringEvents
@@ -30,6 +43,6 @@
                  (triggers/start-now)
                  (triggers/with-schedule
                   (simple/schedule
-                   (simple/with-interval-in-milliseconds (u/minutes->ms 15))
+                   (simple/with-interval-in-milliseconds (metering-interval-ms))
                    (simple/repeat-forever))))]
     (task/schedule-task! job trigger)))
