1811af294451ee9c61844248d7c4f55832ea9b30
dan sutton <dan@dpsutton.com>
2026-01-05 08:45:12 -0600

    use an events namespace
    not doing an init namespace as that's similar but not the same. Those
    are for settings and models. It's fine to just require the event
    namespace in the core namespace. it's a bit of ceremony but the pattern
    is useful

--- src/metabase/api_routes/core.clj
+++ src/metabase/api_routes/core.clj
@@ -1,9 +1,7 @@
 (ns metabase.api-routes.core
   (:require
+   [metabase.api-routes.events]
    [metabase.api-routes.routes]
-   [metabase.events.core :as events]
-   [metabase.util.log :as log]
-   [methodical.core :as methodical]
    [potemkin :as p]))
 
 (comment metabase.api-routes.routes/keep-me)
@@ -11,22 +9,3 @@
 (p/import-vars
  [metabase.api-routes.routes
   routes])
-
-(derive ::api-events :metabase/event)
-(derive :event/api-handler-update ::api-events)
-
-;; Should be triggered in dev mode only to update the openapi.json doc when api schemas change this then will update
-;; the metabase-types for FE via hot-reloading. This used to live in metabase.api.events. But because it needs a
-;; reference to all routes, that brought a module usage of api-routes. But this is a can of worms because
-;; api-routes (must) use all modules to expose the routes. So now this is a bit split brain: the
-;; api.docs/request-spec-regenerartion function is sent over in the event payload from the api module and the event
-;; handler is registered in this module which has access to all of the routes.
-(methodical/defmethod events/publish-event! ::api-events
-  [_topic {rebuild-fn :api.docs/request-rebuild :as _event}]
-  (try
-    (log/debug "Requesting OpenAPI regeneration")
-    (if rebuild-fn
-      (rebuild-fn routes)
-      (log/debug "No build function provided to rebuild OpenAPI docs"))
-    (catch Throwable e
-      (log/debug e "Failed to trigger OpenAPI regeneration"))))
