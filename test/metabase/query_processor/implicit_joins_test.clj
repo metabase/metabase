6d897adc401be2be484f634f4bb442ee423ed5a5
Riley Thompson <rileythompson99@gmail.com>
2026-01-07 11:10:38 -0500

    adjust length for certain drivers

--- test/metabase/query_processor/implicit_joins_test.clj
+++ test/metabase/query_processor/implicit_joins_test.clj
@@ -233,10 +233,10 @@
 (mt/defdataset long-col-name-dataset
   [["long_col_name" [{:field-name "fk"
                       :base-type :type/Integer}
-                     {:field-name "big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_"
+                     {:field-name "big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_"
                       :base-type :type/Text}]
     [[1 "a"]]]
-   ["long_col_name_2" [{:field-name "big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_"
+   ["long_col_name_2" [{:field-name "big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_big_column_"
                         :base-type :type/Text}]
     [["b"]]]])
 
