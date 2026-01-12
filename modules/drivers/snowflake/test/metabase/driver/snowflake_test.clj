4d6e579096c4bd561912bee194ae6b6c03d90b48
Timothy Dean <timothy@timothydean.dev>
2026-01-12 14:36:05 -0700

    Attempting a reproducing test

--- modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
+++ modules/drivers/snowflake/test/metabase/driver/snowflake_test.clj
@@ -1443,3 +1443,34 @@
                     {:name "products",   :schema "PUBLIC", :description nil}
                     {:name "reviews",    :schema "PUBLIC", :description nil}}}
                  (driver/describe-database :snowflake db))))))))
+
+;; Dataset definition for issue #68065
+(mt/defdataset time-column-test
+  [["event_times" [{:field-name "description"
+                    :base-type :type/Text}
+                   {:field-name "event_time"
+                    :base-type :type/Time}]
+    [["Meeting" "10:30:00"]
+     ["Deadline" "15:00:00"]
+     ["Lunch" "12:00:00"]]]])
+
+(deftest time-column-in-nested-query-test
+  (testing "TIME column with temporal bucketing in nested query should not cast to timestampntz (#68065)"
+    (mt/test-driver :snowflake
+      (mt/dataset time-column-test
+        (let [mp (mt/metadata-provider)
+              table (lib.metadata/table mp (mt/id :event_times))
+              event-time (lib.metadata/field mp (mt/id :event_times :event_time))
+              ;; Create a query with an expression to force nesting
+              query (-> (lib/query mp table)
+                        (lib/expression "test_expr" (lib/+ 1 1))
+                        (lib/breakout (lib/with-temporal-bucket event-time :hour))
+                        (lib/aggregate (lib/count)))]
+          ;; Get the native SQL without executing
+          (let [native-sql (-> (qp/compile query) :query)]
+            (testing "Generated SQL should not contain CAST to timestampntz for TIME column"
+              (is (not (str/includes? (str/lower-case native-sql) "as timestampntz"))
+                  (str "SQL should not cast TIME column to timestampntz, but got: " native-sql)))
+            (testing "Generated SQL should use DATE_TRUNC on the TIME column"
+              (is (str/includes? (str/upper-case native-sql) "DATE_TRUNC")
+                  (str "SQL should use DATE_TRUNC, but got: " native-sql)))))))))
