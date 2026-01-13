47aeb4a93f6dafd1f88848288751e8f067ab02b4
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2026-01-13 05:36:12 -0800

    Enable codespell for dev/ folder

--- mage/src/mage/codespell.clj
+++ mage/src/mage/codespell.clj
@@ -7,7 +7,7 @@
 
 (def ^:private codespell-dirs
   "Keep these in sync with the list of directories in `.github/workflows/codespell.yml`."
-  [#_"dev"
+  ["dev"
    "docs"
    #_"e2e"
    "enterprise"
