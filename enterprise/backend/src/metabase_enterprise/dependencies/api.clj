571f9f0dd6b5cb255f68ca7023605a47ce020884
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-01-13 10:03:29 -0500

    Update enterprise/backend/src/metabase_enterprise/dependencies/api.clj
    Co-authored-by: Riley Thompson <riley@metabase.com>

--- enterprise/backend/src/metabase_enterprise/dependencies/api.clj
+++ enterprise/backend/src/metabase_enterprise/dependencies/api.clj
@@ -756,7 +756,7 @@
    - `offset`: Default 0
    - `limit`: Default 50
 
-   Returns a a map with:
+   Returns a map with:
    - `data`: List of broken items, each with `:id`, `:type`, `:data`, and `:error`s fields
    - `total`: Total count of matched items
    - `offset`: Applied offset
