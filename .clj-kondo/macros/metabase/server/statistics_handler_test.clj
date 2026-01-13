5e74d8fb91f42bb6b8cda4ed53cc28b41dd3ae8c
dan sutton <dan@dpsutton.com>
2026-01-13 15:18:42 -0600

    bit of linter appeasement

--- .clj-kondo/macros/metabase/server/statistics_handler_test.clj
+++ .clj-kondo/macros/metabase/server/statistics_handler_test.clj
@@ -1,7 +1,6 @@
 (ns macros.metabase.server.statistics-handler-test)
 
 (defmacro with-server
-  [[hold-chan hit-chan] & body]
-  `(let [~hold-chan nil
-         ~hit-chan  nil]
+  [[_hold-chan _hit-chan] & body]
+  `(let []
      ~@body))
