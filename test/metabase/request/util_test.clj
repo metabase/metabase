d6cb5d11542f55b3e7a87e0fe0ce34e5c18ee0f0
Timofei <timofei@metabase.com>
2026-01-13 17:25:14 +0400

    Fix test

--- test/metabase/request/util_test.clj
+++ test/metabase/request/util_test.clj
@@ -9,21 +9,21 @@
 
 (deftest ^:parallel cacheable?-test
   (testing "JS/CSS with cache-busting hash are cacheable"
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/dist/main.abc123def.js"})))
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/dist/styles.abc123def.css"}))))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/dist/main.abc123def.js"})))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/dist/styles.abc123def.css"}))))
   (testing "Resources in /app/dist/ with hex hash prefix are cacheable"
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/dist/abc123def456.png"}))))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/dist/abc123def456.png"}))))
   (testing "Font files are cacheable"
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/fonts/Lato/lato-v16-latin-regular.woff2"})))
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/fonts/Lato/lato-v16-latin-regular.woff"})))
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/fonts/CustomFont/custom.ttf"})))
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/fonts/CustomFont/custom.otf"})))
-    (is (true? (req.util/cacheable? {:request-method :get :uri "/app/fonts/CustomFont/custom.eot"}))))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/fonts/Lato/lato-v16-latin-regular.woff2"})))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/fonts/Lato/lato-v16-latin-regular.woff"})))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/fonts/CustomFont/custom.ttf"})))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/fonts/CustomFont/custom.otf"})))
+    (is (some? (req.util/cacheable? {:request-method :get :uri "/app/fonts/CustomFont/custom.eot"}))))
   (testing "Non-GET requests are not cacheable"
-    (is (false? (req.util/cacheable? {:request-method :post :uri "/app/fonts/Lato/lato.woff2"}))))
+    (is (not (req.util/cacheable? {:request-method :post :uri "/app/fonts/Lato/lato.woff2"}))))
   (testing "Other paths are not cacheable"
-    (is (nil? (req.util/cacheable? {:request-method :get :uri "/api/dashboard/1"})))
-    (is (nil? (req.util/cacheable? {:request-method :get :uri "/app/dist/main.js"})))))
+    (is (not (req.util/cacheable? {:request-method :get :uri "/api/dashboard/1"})))
+    (is (not (req.util/cacheable? {:request-method :get :uri "/app/dist/main.js"})))))
 
 (deftest ^:parallel https?-test
   (doseq [[headers expected] {{"x-forwarded-proto" "https"}    true
