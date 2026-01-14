5098b7c8858cc7de8921b2f974b64930dde57f91
Nicola Mometto <nicola.mometto@metabase.com>
2026-01-14 11:54:27 +0000

    feat: export more utils

--- src/metabase/task_history/core.clj
+++ src/metabase/task_history/core.clj
@@ -11,4 +11,8 @@
  [metabase.task-history.models.task-history
   with-task-history]
  [metabase.task-history.models.task-run
+  *run-id*
+  complete-task-run!
+  with-restored-run-id
+  with-run-id-meta
   with-task-run])
