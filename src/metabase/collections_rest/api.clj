4f68251d6c4faa915ba9371b054d8f7f93ae6db9
Nathan Voxland <nathan@metabase.com>
2026-01-14 09:31:20 -0600

    Merged from master

--- src/metabase/collections_rest/api.clj
+++ src/metabase/collections_rest/api.clj
@@ -491,7 +491,7 @@
    :where  [:and
             (poison-when-pinned-clause pinned-state)
             [:= :collection_id (:id collection)]
-            (when-not api/*is-superuser?*
+            (when-not (perms/has-any-transforms-permission? api/*current-user-id*)
               [:=
                [:inline 0]
                [:inline 1]])]})
