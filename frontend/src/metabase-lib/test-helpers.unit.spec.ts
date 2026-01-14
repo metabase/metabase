21d67ec2f1ed5b4ee60781d6d5b7aac7218c14ea
Romeo Van Snick <romeo@romeovansnick.be>
2026-01-14 15:36:20 +1100

    Expand example in tests

--- frontend/src/metabase-lib/test-helpers.unit.spec.ts
+++ frontend/src/metabase-lib/test-helpers.unit.spec.ts
@@ -1,10 +1,19 @@
-import { SAMPLE_DATABASE, createTestJsQuery } from "metabase-lib/test-helpers";
+import * as Lib from "metabase-lib";
+import {
+  SAMPLE_DATABASE,
+  SAMPLE_METADATA,
+  createTestJsQuery,
+} from "metabase-lib/test-helpers";
 import { ORDERS_ID, PEOPLE_ID } from "metabase-types/api/mocks/presets";
 
 describe("createTestQuery", () => {
   it("should create a query with stages", () => {
     expect(() => {
-      createTestJsQuery({
+      const provider = Lib.metadataProvider(
+        SAMPLE_DATABASE.id,
+        SAMPLE_METADATA,
+      );
+      const query = createTestJsQuery(provider, {
         databaseId: SAMPLE_DATABASE.id,
         stages: [
           {
@@ -12,6 +21,14 @@ describe("createTestQuery", () => {
               type: "table",
               id: ORDERS_ID,
             },
+            limit: 101,
+            fields: [
+              {
+                type: "column",
+                table: "ORDERS",
+                name: "ID",
+              },
+            ],
             joins: [
               {
                 source: {
@@ -44,9 +61,72 @@ describe("createTestQuery", () => {
                 ],
               },
             ],
+            filters: [
+              {
+                type: "operator",
+                operator: ">=",
+                args: [
+                  {
+                    type: "column",
+                    table: "ORDERS", // TODO: get rid of this
+                    name: "TOTAL",
+                  },
+                  {
+                    type: "literal",
+                    value: 10,
+                  },
+                ],
+              },
+            ],
+            aggregations: [
+              {
+                name: "Foo",
+                value: {
+                  type: "operator",
+                  operator: "avg",
+                  args: [
+                    {
+                      type: "column",
+                      table: "ORDERS", // TODO: get rid of this?
+                      name: "TOTAL",
+                    },
+                  ],
+                },
+              },
+            ],
+            breakouts: [
+              {
+                table: "ORDERS",
+                name: "TOTAL",
+                binningCount: 10,
+              },
+            ],
+            expressions: [
+              {
+                name: "Derp",
+                value: {
+                  type: "operator",
+                  operator: "+",
+                  args: [
+                    { type: "literal", value: 1 },
+                    { type: "literal", value: 50 },
+                  ],
+                },
+              },
+            ],
+            orderBy: [
+              {
+                name: "TOTAL",
+                table: "ORDERS",
+                direction: "desc",
+              },
+            ],
           },
         ],
       });
+
+      // eslint-disable-next-line no-console
+      console.log(JSON.stringify(query, null, 2));
     }).not.toThrow();
   });
 });
