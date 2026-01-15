f2b1ec17c63414c06772aba299d96794b0823420
da <paoliniluis@gmail.com>
2026-01-15 07:57:16 -0300

    Improve error logging in notification send process
    Fix silly mistakes that I do when I'm sleepy

--- src/metabase/notification/send.clj
+++ src/metabase/notification/send.clj
@@ -167,7 +167,7 @@
                           (doseq [message messages]
                             (channel-send-retrying! id payload_type handler message)))
                         (catch Exception e
-                          (log/error e "Error sending to channel %s" (handler->channel-name handler))))))
+                          (log/errorf e "Error sending to channel %s" (handler->channel-name handler))))))
                   (log/info "Done processing notification")))
               (do-after-notification-sent hydrated-notification notification-payload (some? skip-reason))
               (prometheus/inc! :metabase-notification/send-ok {:payload-type payload_type}))))
@@ -367,7 +367,7 @@
                                                    (log/warn "Notification worker interrupted, shutting down")
                                                    (throw (InterruptedException.)))
                                                  (catch Throwable e
-                                                   (log/errorf e "Error in notification worker")))))))
+                                                   (log/error e "Error in notification worker")))))))
         ensure-enough-workers! (fn []
                                  (dotimes [i (- pool-size (.getActiveCount ^ThreadPoolExecutor executor))]
                                    (log/debugf "Not enough workers, starting a new one %d/%d"
