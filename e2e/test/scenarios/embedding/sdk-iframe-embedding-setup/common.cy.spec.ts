ddecd16711bca22c63dca8582f2831fce6d2ff61
Timofei <timofei@metabase.com>
2026-01-16 16:07:47 +0400

    Move tests

--- e2e/test/scenarios/embedding/sdk-iframe-embedding-setup/common.cy.spec.ts
+++ e2e/test/scenarios/embedding/sdk-iframe-embedding-setup/common.cy.spec.ts
@@ -1,7 +1,11 @@
+import { ORDERS_QUESTION_ID } from "e2e/support/cypress_sample_instance_data";
+import { embedModalEnableEmbedding } from "e2e/support/helpers";
+
 import {
   getEmbedSidebar,
   navigateToEntitySelectionStep,
   navigateToGetCodeStep,
+  visitNewEmbedPage,
 } from "./helpers";
 
 const { H } = cy;
@@ -15,6 +19,7 @@ describe("scenarios > embedding > sdk iframe embed setup > common", () => {
     H.activateToken("bleeding-edge");
     H.enableTracking();
     H.updateSetting("enable-embedding-simple", true);
+    H.updateSetting("enable-embedding-static", true);
 
     cy.intercept("GET", "/api/dashboard/**").as("dashboard");
 
@@ -84,5 +89,65 @@ describe("scenarios > embedding > sdk iframe embed setup > common", () => {
     cy.findAllByTestId("settings-sidebar-link")
       .contains("General")
       .should("be.visible");
+  });
+
+  describe("auth type switch", () => {
+    it("should reset experience to a default only when switching from SSO to Guest auth type and the current experience does not support guest embeds", () => {
+      visitNewEmbedPage();
+
+      getEmbedSidebar().within(() => {
+        cy.findByLabelText("Metabase account (SSO)").click();
+
+        cy.findByLabelText("Browser").click();
+        cy.findByLabelText("Browser").should("be.checked");
+
+        cy.findByLabelText("Guest").click();
+
+        cy.findByLabelText("Dashboard").should("be.checked");
+
+        cy.findByLabelText("Chart").click();
+        cy.findByLabelText("Chart").should("be.checked");
+
+        cy.findByLabelText("Metabase account (SSO)").click();
+
+        cy.findByLabelText("Chart").should("be.checked");
+
+        cy.findByLabelText("Guest").click();
+
+        cy.findByLabelText("Chart").should("be.checked");
+      });
+    });
+
+    it("should not reset experience when changing auth type on `options` step", () => {
+      H.visitQuestion(ORDERS_QUESTION_ID);
+
+      H.openSharingMenu("Embed");
+
+      getEmbedSidebar().within(() => {
+        embedModalEnableEmbedding();
+
+        H.waitForSimpleEmbedIframesToLoad();
+
+        H.getSimpleEmbedIframeContent().within(() => {
+          cy.findByText("Orders").should("be.visible");
+        });
+
+        cy.findByLabelText("Metabase account (SSO)").click();
+
+        H.waitForSimpleEmbedIframesToLoad();
+
+        H.getSimpleEmbedIframeContent().within(() => {
+          cy.findByText("Orders").should("be.visible");
+        });
+
+        cy.findByLabelText("Guest").click();
+
+        H.waitForSimpleEmbedIframesToLoad();
+
+        H.getSimpleEmbedIframeContent().within(() => {
+          cy.findByText("Orders").should("be.visible");
+        });
+      });
+    });
   });
 });
