9fd3736fad1473637824ecdc1a9fb753b082eefa
dan sutton <dan@dpsutton.com>
2026-01-19 16:00:13 -0600

    fix kondo lint (removes a legacy usage as well :) 💰

--- src/metabase/sync/analyze/fingerprint.clj
+++ src/metabase/sync/analyze/fingerprint.clj
@@ -9,8 +9,6 @@
    [metabase.driver :as driver]
    [metabase.driver.util :as driver.u]
    [metabase.lib.schema.metadata.fingerprint :as lib.schema.metadata.fingerprint]
-   ;; legacy usage -- don't do things like this going forward
-   ^{:clj-kondo/ignore [:deprecated-namespace :discouraged-namespace]} [metabase.query-processor.store :as qp.store]
    [metabase.sync.interface :as i]
    [metabase.sync.util :as sync-util]
    [metabase.util :as u]
