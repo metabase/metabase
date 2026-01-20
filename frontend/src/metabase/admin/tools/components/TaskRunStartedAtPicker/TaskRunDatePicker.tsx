08a8c518f012cd5328229a63c85123aaf57432b1
Egor Grushin <egor@metabase.com>
2026-01-20 00:46:13 +0000

    GDGT-1520 add a started at filter

---
+++ frontend/src/metabase/admin/tools/components/TaskRunStartedAtPicker/TaskRunDatePicker.tsx
@@ -0,0 +1,50 @@
+import { t } from "ttag";
+
+import { Select, type SelectProps } from "metabase/ui";
+import type { SelectData } from "metabase/ui/components/inputs/Select/Select";
+import type { TaskRunDateFilterOption } from "metabase-types/api";
+
+type TaskRunDatePickerProps = Omit<
+  SelectProps,
+  "data" | "value" | "onChange"
+> & {
+  value: TaskRunDateFilterOption | null;
+  onChange: (value: TaskRunDateFilterOption | null) => void;
+};
+
+export const TaskRunDatePicker = ({
+  value,
+  onChange,
+  ...props
+}: TaskRunDatePickerProps) => {
+  const data: SelectData<TaskRunDateFilterOption> = [
+    { label: t`Today`, value: "thisday" },
+    { label: t`Yesterday`, value: "past1days" },
+    { label: t`Previous week`, value: "past1weeks" },
+    { label: t`Previous 7 days`, value: "past7days" },
+    { label: t`Previous 30 days`, value: "past30days" },
+    { label: t`Previous month`, value: "past1months" },
+    { label: t`Previous 3 months`, value: "past3months" },
+    { label: t`Previous 12 months`, value: "past12months" },
+  ];
+
+  return (
+    <Select
+      comboboxProps={{
+        middlewares: {
+          flip: true,
+          size: {
+            padding: 6,
+          },
+        },
+        position: "bottom-start",
+        width: 300,
+      }}
+      clearable
+      data={data}
+      value={value}
+      onChange={onChange}
+      {...props}
+    />
+  );
+};
---
+++ frontend/src/metabase/admin/tools/components/TaskRunStartedAtPicker/index.ts
@@ -0,0 +1,4 @@
+export {
+  TaskRunDatePicker,
+  type TaskRunDatePickerRange,
+} from "./TaskRunDatePicker";
