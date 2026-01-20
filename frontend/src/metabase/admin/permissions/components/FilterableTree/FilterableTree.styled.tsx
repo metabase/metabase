39b606362e560043a031a89a39fa8a2cd6958649
Nick Fitzpatrick <nickfitz.582@gmail.com>
2026-01-20 15:04:01 -0400

    stupid spelling

--- frontend/src/metabase/admin/permissions/components/FilterableTree/FilterableTree.styled.tsx
+++ frontend/src/metabase/admin/permissions/components/FilterableTree/FilterableTree.styled.tsx
@@ -31,7 +31,7 @@ export const EmptyStateContainer = styled.div`
 export const AdminTreeNode = styled(Tree.Node)`
   color: ${(props) =>
     // TODO: We should really come up with a better solution for this, but since admin-navbar is currently not determined by
-    // user whitelabelling, this is somewhat safe (and consistent whith what is in the admin header nav)
+    // user whitelabelling, this is somewhat safe (and consistent with what is in the admin header nav)
     props.isSelected ? "var(--mantine-color-white)" : color("text-secondary")};
   background-color: ${(props) =>
     props.isSelected ? color("admin-navbar") : "unset"};
