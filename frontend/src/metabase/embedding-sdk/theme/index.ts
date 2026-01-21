53f555de310f7fae231dc0ed6c10ecae03e710ce
Phoomparin Mano <phoomparin@gmail.com>
2026-01-21 23:51:15 +0700

    remove no-literal-metabase-strings from file name

--- frontend/src/metabase/embedding-sdk/theme/index.ts
+++ frontend/src/metabase/embedding-sdk/theme/index.ts
@@ -1,6 +1,5 @@
 export * from "./default-component-theme";
 export * from "./MetabaseTheme";
-// eslint-disable-next-line no-literal-metabase-strings -- file name
 export * from "./MetabaseThemeV2";
 export * from "./theme-version";
 
