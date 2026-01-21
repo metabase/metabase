cc4690b768e26dc5ccf87ae80f5b2d8666d0e8b2
Sébastien <sebastien@metabase.com>
2026-01-21 10:42:55 +0100

    Fixed no-literal-metabase-strings eslint rule for `export * from './MetabaseThing';` (#68411)
    fixed eslint custom rule

--- frontend/src/metabase/embedding-sdk/theme/index.ts
+++ frontend/src/metabase/embedding-sdk/theme/index.ts
@@ -1,5 +1,4 @@
 export * from "./default-component-theme";
-// eslint-disable-next-line no-literal-metabase-strings -- file name
 export * from "./MetabaseTheme";
 
 export * from "./define-metabase-theme";
