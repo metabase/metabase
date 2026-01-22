965b0154b0c107a70e67a3f39a8e616f39ad555a
Romeo Van Snick <romeo@romeovansnick.be>
2026-01-22 15:24:10 +1100

    Add createTestJsQuery wrapper for cypress

--- e2e/support/helpers/mbql/index.ts
+++ e2e/support/helpers/mbql/index.ts
@@ -1,10 +1,29 @@
 import type * as Lib from "metabase-lib";
+import type { CreateTestQueryOpts } from "metabase-lib/test-helpers";
+import type { DatabaseId } from "metabase-types/api";
 
 import type { GetMetadataOpts } from "./types";
 
 export async function getMetadataProvider(
   opts?: GetMetadataOpts,
-): Cypress.Chainable<Lib.MetadataProvider> {
+): Promise<Lib.MetadataProvider> {
   const { getMetadataProvider } = await import("./metadata-provider");
   return getMetadataProvider(opts);
+}
+
+export async function createTestJsQuery(
+  metadataProvider: Lib.MetadataProvider,
+  opts: CreateTestQueryOpts,
+) {
+  const { createTestJsQuery } = await import("metabase-lib/test-helpers");
+  return createTestJsQuery(metadataProvider, opts);
+}
+
+export async function createTestNativeJsQuery(
+  metadataProvider: Lib.MetadataProvider,
+  databaseId: DatabaseId,
+  query: string,
+) {
+  const { createTestNativeJsQuery } = await import("metabase-lib/test-helpers");
+  return createTestNativeJsQuery(metadataProvider, databaseId, query);
 }
