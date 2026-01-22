ac4aea7a35ac6440e28b4ea251ead8fff2b5dfcc
lbrdnk <lbrdnk@users.noreply.github.com>
2026-01-22 14:57:38 +0100

    Remove final-non-reserved

--- src/metabase/driver/sql.clj
+++ src/metabase/driver/sql.clj
@@ -51,8 +51,7 @@
                  :expressions/today
                  :distinct-where
                  :database-routing
-                 :dependencies/native
-                 :final-non-reserved]]
+                 :dependencies/native]]
   (defmethod driver/database-supports? [:sql feature] [_driver _feature _db] true))
 
 (defmethod driver/database-supports? [:sql :persist-models-enabled]
