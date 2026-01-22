3044814242b01c1266e2d81432fdfe220ce9281b
Romeo Van Snick <romeo@romeovansnick.be>
2026-01-22 15:14:42 +1100

    Simplify wrapper

--- e2e/support/helpers/mbql/index.ts
+++ e2e/support/helpers/mbql/index.ts
@@ -2,13 +2,9 @@ import type * as Lib from "metabase-lib";
 
 import type { GetMetadataOpts } from "./types";
 
-export function getMetadataProvider(
+export async function getMetadataProvider(
   opts?: GetMetadataOpts,
 ): Cypress.Chainable<Lib.MetadataProvider> {
-  const promise = import("./metadata-provider").then(
-    ({ getMetadataProvider }) => getMetadataProvider(opts),
-  );
-
-  // @ts-expect-error: Cypress.Chainable collapses the Promise, but the types do not reflect that
-  return cy.wrap(promise) as Cypress.Chainable<Lib.MetadataProvider>;
+  const { getMetadataProvider } = await import("./metadata-provider");
+  return getMetadataProvider(opts);
 }
