adce0d108cac7aa99cb16fb37287c4a4e609248b
Romeo Van Snick <romeo@romeovansnick.be>
2026-01-23 14:04:45 +1100

    Add repro for metabase#68378

--- e2e/test/scenarios/data-studio/transforms.cy.spec.ts
+++ e2e/test/scenarios/data-studio/transforms.cy.spec.ts
@@ -3531,6 +3531,34 @@ describe("scenarios > admin > transforms", () => {
   });
 });
 
+describe("issue #68378", () => {
+  beforeEach(() => {
+    H.restore("postgres-writable");
+    H.resetTestTable({ type: "postgres", table: "empty_schema" });
+    cy.signInAsAdmin();
+    H.activateToken("bleeding-edge");
+    cy.request("POST", `/api/database/${WRITABLE_DB_ID}/sync_schema`);
+  });
+
+  it("should show empty schema's when picking a target schema (metabase#68378)", () => {
+    visitTransformListPage();
+    cy.button("Create a transform").click();
+    H.popover().findByText("SQL query").click();
+    H.NativeEditor.type("SELECT 42", { allowFastSet: true });
+
+    cy.log("Save with empty_schema as target schema");
+    getQueryEditor().button("Save").click();
+
+    H.modal().within(() => {
+      cy.findByLabelText("Name").clear().type("SQL transform");
+      cy.findByLabelText("Schema").click();
+    });
+    H.popover().findByText("empty_schema").should("be.visible").click();
+
+    H.modal().button("Save").click();
+  });
+});
+
 function getTransformsNavLink() {
   return H.DataStudio.nav().findByRole("link", { name: "Transforms" });
 }
