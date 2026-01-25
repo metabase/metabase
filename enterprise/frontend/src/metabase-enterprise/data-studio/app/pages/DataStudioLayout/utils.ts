3c7b043c5a6f0299c4ddb526a7f1f3aec8c3e7f7
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-01-24 20:09:58 -0500

    fixes

--- enterprise/frontend/src/metabase-enterprise/data-studio/app/pages/DataStudioLayout/utils.ts
+++ enterprise/frontend/src/metabase-enterprise/data-studio/app/pages/DataStudioLayout/utils.ts
@@ -6,8 +6,8 @@ type TabName =
   | "transforms"
   | "jobs"
   | "runs"
-  | "dependency-diagnostics"
   | "dependencies"
+  | "dependency-diagnostics"
   | "glossary";
 
 export const getCurrentTab = (pathname: string): TabName => {
