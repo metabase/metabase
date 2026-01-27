ef77c85c3c933a401fdb0a779a57e7dfe79a4d14
William Zimrin <william@metabase.com>
2026-01-27 09:02:57 -0600

    maybe fix pg?

--- test/metabase/query_processor/implicit_joins_test.clj
+++ test/metabase/query_processor/implicit_joins_test.clj
@@ -239,7 +239,7 @@
     [[1 "a1"]
      [2 "a2"]
      [2 "a3"]]]
-   ["long_col_name_2" [{:field-name "foo_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcd"
+   ["long_col_name_2" [{:field-name "foo_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abc"
                         :base-type :type/Integer}
                        {:field-name "abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_"
                         :base-type :type/Text}]
@@ -254,7 +254,7 @@
             table (lib.metadata/table mp (mt/id :long_col_name))
             query (lib/query mp table)
             fk-field (mt/id :long_col_name :fk)
-            id-2-field (mt/id :long_col_name_2 :foo_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcd)]
+            id-2-field (mt/id :long_col_name_2 :foo_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abcdefg_abc)]
         (t2/update! :model/Field fk-field {:semantic_type :type/FK
                                            :fk_target_field_id id-2-field})
         (testing "Implicit join with long column name should use actual DB column name as source alias (#67002)"
