e564b1e6f09dd05c8142c13ded5247763c5bd204
John Swanson <john.swanson@metabase.com>
2026-01-29 13:27:17 -0800

    add missing placeholder file

---
+++ enterprise/backend/src/metabase_enterprise/transforms/core.clj
@@ -0,0 +1,9 @@
+(ns metabase-enterprise.transforms.core
+  "Enterprise module placeholder for transforms.
+
+  Transforms are weird: they have different behavior on OSS vs EE. We need to test this, so we need tests
+  in `enterprise/backend/test/metabase_enterprise/transforms/api_test.clj`.
+
+  But we need a 'real' module, because all tests must belong to a module.
+
+  Hence, this placeholder.")
