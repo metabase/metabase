25e13335a31a8408a1271f0678097b34333c7b86
Fraser <fraser@metabase.com>
2026-01-29 15:34:48 +1300

    Fix lint tests

--- frontend/lint/tests/no-literal-metabase-strings.unit.spec.js
+++ frontend/lint/tests/no-literal-metabase-strings.unit.spec.js
@@ -33,14 +33,6 @@ export * from "./MetabaseLinksToggleWidget";`,
     code: `
   const label = "some string"`,
   },
-  {
-    // "Detect disabled rule next line",
-    code: `
-  function MyComponent() {
-    // eslint-disable-next-line no-literal-metabase-strings -- In admin settings
-    return <div>Metabase store {"interpolation"} something else</div>;
-  }`,
-  },
 ];
 
 const INVALID_CASES = [
@@ -126,37 +118,12 @@ const INVALID_CASES = [
   }`,
     error: /Metabase string must not be used directly./,
   },
-  {
-    name: "Detect disabled rule next line",
-    code: `
-  function MyComponent() {
-    // eslint-disable-next-line no-literal-metabase-strings
-    return <div>Metabase store {"interpolation"} something else</div>;
-  }`,
-    error:
-      /Please add comment to indicate the reason why this rule needs to be disabled./,
-  },
-  {
-    name: "Detect disabled rule block",
-    code: `
-  /* eslint-disable no-literal-metabase-strings */
-  function MyComponent() {
-    return <div>Metabase store {"interpolation"} something else</div>;
-  }`,
-    error: "Please use inline disable with comments instead.",
-  },
 ];
 
 ruleTester.run("no-literal-metabase-strings", noLiteralMetabaseString, {
   valid: VALID_CASES,
-  invalid: INVALID_CASES.map((invalidCase) => {
-    return {
-      code: invalidCase.code,
-      errors: [
-        {
-          message: invalidCase.error,
-        },
-      ],
-    };
-  }),
+  invalid: INVALID_CASES.map((invalidCase) => ({
+    code: invalidCase.code,
+    errors: [{ message: invalidCase.error }],
+  })),
 });
