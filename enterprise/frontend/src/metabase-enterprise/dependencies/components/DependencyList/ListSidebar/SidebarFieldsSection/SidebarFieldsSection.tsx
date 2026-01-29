60f73c680b300c3819f3bcc1a0a85bc74585c1bf
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-01-28 21:00:05 -0500

    wip

--- enterprise/frontend/src/metabase-enterprise/dependencies/components/DependencyList/ListSidebar/SidebarFieldsSection/SidebarFieldsSection.tsx
+++ enterprise/frontend/src/metabase-enterprise/dependencies/components/DependencyList/ListSidebar/SidebarFieldsSection/SidebarFieldsSection.tsx
@@ -1,8 +1,18 @@
 import cx from "classnames";
 import { t } from "ttag";
 
+import { getColumnIcon } from "metabase/common/utils/columns";
 import CS from "metabase/css/core/index.css";
-import { Badge, Box, Card, Group, Stack, Title } from "metabase/ui";
+import {
+  Badge,
+  Box,
+  Card,
+  FixedSizeIcon,
+  Group,
+  Stack,
+  Title,
+} from "metabase/ui";
+import * as Lib from "metabase-lib";
 import type { DependencyNode, Field } from "metabase-types/api";
 
 import { getNodeFields, getNodeFieldsLabel } from "../../../../utils";
@@ -40,12 +50,24 @@ type FieldListItemProps = {
 };
 
 function FieldListItem({ field }: FieldListItemProps) {
+  const fieldInfo = Lib.legacyColumnTypeInfo(field);
+  const fieldIcon = getColumnIcon(fieldInfo);
+
   return (
     <Stack className={S.item} p="md" gap="xs">
-      <Box className={CS.textWrap} lh="1rem">
-        {field.display_name}
-      </Box>
-      <Box className={cx(CS.textWrap, CS.textMonospace)} fz="sm" lh="1rem">
+      <Group gap="xs" wrap="nowrap">
+        <FixedSizeIcon name={fieldIcon} c="brand" />
+        <Box className={CS.textWrap} lh="1rem">
+          {field.display_name}
+        </Box>
+      </Group>
+      <Box
+        className={cx(CS.textWrap, CS.textMonospace)}
+        ml="md"
+        pl="xs"
+        fz="sm"
+        lh="1rem"
+      >
         {field.name}
       </Box>
     </Stack>
