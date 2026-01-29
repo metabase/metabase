c7a86fc1b198f2886353529f4a6cc7d10eeaeadc
lbrdnk <lbrdnk@users.noreply.github.com>
2026-01-29 18:09:26 +0100

    Add internal MB_SQL_TOOLS_PARSER_BACKEND setting

--- src/metabase/sql_tools/init.clj
+++ src/metabase/sql_tools/init.clj
@@ -1,5 +1,6 @@
 (ns metabase.sql-tools.init
   (:require
-   ;; Following is needed to force method's registration
+   ;; metabase.sql-tools.<parser> is required to ensure method's registration
    [metabase.sql-tools.macaw.core]
+   [metabase.sql-tools.settings]
    [metabase.sql-tools.sqlglot.core]))
---
+++ src/metabase/sql_tools/settings.clj
@@ -0,0 +1,22 @@
+(ns metabase.sql-tools.settings
+  (:require
+   [metabase.config.core :as config]
+   [metabase.settings.core :as setting :refer [defsetting]]
+   [metabase.util.i18n :refer [deferred-tru tru]]))
+
+(def ^:private available-parser-backends
+  #{:macaw :sqlglot})
+
+(defsetting sql-tools-parser-backend
+  (deferred-tru "Parser backend of `sql-tools` module.")
+  :visibility :internal
+  :type :keyword
+  :default (if config/is-dev?
+             :sqlglot
+             :macaw)
+  :setter (fn [new-value]
+            (or (available-parser-backends (keyword new-value))
+                (throw (ex-info (tru "Invalid sql-tools parser backend.")
+                                {:value (keyword new-value)
+                                 :avialable available-parser-backends})))
+            (setting/set-value-of-type! :keyword :sql-tools-parser-backend new-value)))
