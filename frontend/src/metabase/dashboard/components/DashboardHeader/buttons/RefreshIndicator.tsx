853bc9dcafb56858159da230ce4b592e29ae8df4
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2026-01-29 06:05:00 -0800

    Add the actual icon

--- frontend/src/metabase/dashboard/components/DashboardHeader/buttons/RefreshIndicator.tsx
+++ frontend/src/metabase/dashboard/components/DashboardHeader/buttons/RefreshIndicator.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
+
+import CS from "metabase/css/core/index.css";
 import { useDashboardContext } from "metabase/dashboard/context";
 
+import { RefreshWidgetTarget } from "../../RefreshWidget/RefreshWidgetTarget";
+
+/**
+ * This is only used in modular embeddings
+ */
 export function RefreshIndicator() {
-  const { refreshPeriod } = useDashboardContext();
+  const { refreshPeriod, setRefreshElapsedHook } = useDashboardContext();
+  const [elapsed, setElapsed] = useState<number | null>(null);
+
+  useEffect(() => {
+    setRefreshElapsedHook(setElapsed);
+  }, [setRefreshElapsedHook]);
+
   if (refreshPeriod == null || refreshPeriod <= 0) {
     return null;
   }
-  return "refresh indicator";
+  return (
+    <RefreshWidgetTarget
+      elapsed={elapsed}
+      period={refreshPeriod}
+      className={CS.cursorDefault}
+    />
+  );
 }
