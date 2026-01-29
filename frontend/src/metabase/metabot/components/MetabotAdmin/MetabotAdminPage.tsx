4b3dac0358c1727bf3faa0124afa6b4aece497bf
Sloan Sparger <sloansparger@gmail.com>
2026-01-28 19:44:20 -0600

    clean up oss view

--- frontend/src/metabase/metabot/components/MetabotAdmin/MetabotAdminPage.tsx
+++ frontend/src/metabase/metabot/components/MetabotAdmin/MetabotAdminPage.tsx
@@ -1,4 +1,4 @@
-import { IndexRedirect, Route } from "react-router";
+import { IndexRoute, Redirect } from "react-router";
 import { t } from "ttag";
 
 import ErrorBoundary from "metabase/ErrorBoundary";
@@ -6,74 +6,23 @@ import {
   AdminNavItem,
   AdminNavWrapper,
 } from "metabase/admin/components/AdminNav";
-import {
-  SettingsPageWrapper,
-  SettingsSection,
-} from "metabase/admin/components/SettingsSection";
-import { UpsellGem } from "metabase/admin/upsells/components";
 import { AdminSettingsLayout } from "metabase/common/components/AdminLayout/AdminSettingsLayout";
-import { Flex, Text } from "metabase/ui";
+import { Flex } from "metabase/ui";
 
 import { MetabotSQLGenerationSettingsSection } from "./MetabotSQLGenerationSettingsSection";
 
-// import { MetabotPurchasePageForNonStoreUser } from "metabase-enterprise/metabot/components/MetabotAdmin/MetabotPurchasePage/MetabotPurchasePageForNonStoreUser";
-// import { useSelector } from "metabase/lib/redux";
-// import { getStoreUsers } from "metabase/selectors/store-users";
-
 export function getAdminRoutes() {
   return [
-    <IndexRedirect key="index" to="sql-generation" />,
-    <Route
-      key="sql-generation"
-      path="sql-generation"
-      component={MetabotAdminSQLGenerationPage}
-    />,
-    <Route
-      key="metabot"
-      path=":metabotId"
-      component={MetabotAdminUpsellPage}
-    />,
+    <IndexRoute key="index" component={MetabotAdminPage} />,
+    <Redirect key="redirect" from="*" to="/admin/metabot" />,
   ];
 }
 
-function MetabotAdminSQLGenerationPage() {
+function MetabotAdminPage() {
   return (
     <AdminSettingsLayout sidebar={<MetabotNavPane />}>
       <ErrorBoundary>
         <MetabotSQLGenerationSettingsSection />
-      </ErrorBoundary>
-    </AdminSettingsLayout>
-  );
-}
-
-function MetabotAdminUpsellPage() {
-  // const { isStoreUser, anyStoreUserEmailAddress } = useSelector(getStoreUsers);
-
-  return (
-    <AdminSettingsLayout sidebar={<MetabotNavPane />}>
-      <ErrorBoundary>
-        <SettingsPageWrapper>
-          <SettingsSection
-            title={
-              <Flex gap="xs" align="center">
-                <UpsellGem size="1.25rem" mb="1px" />
-                {t`Purchase Metabot`}
-              </Flex>
-            }
-            description={t`AI exploration built on your permissions and semantic definitions`}
-          >
-            <Text>
-              {t`Metabot helps you move faster and understand your data better.`}
-              <br />
-              {t`You can ask it to generate SQL, and build or explain queries.`}
-            </Text>
-            {/* isStoreUser ? null : ( // <MetabotPurchasePageForStoreUser />
-              <MetabotPurchasePageForNonStoreUser
-                anyStoreUserEmailAddress={anyStoreUserEmailAddress}
-              />
-            ) */}
-          </SettingsSection>
-        </SettingsPageWrapper>
       </ErrorBoundary>
     </AdminSettingsLayout>
   );
@@ -87,29 +36,7 @@ function MetabotNavPane() {
           key="sql"
           icon="sql"
           label={t`SQL Generation`}
-          path="/admin/metabot/sql-generation"
-        />
-        <AdminNavItem
-          key="metabot"
-          icon="metabot"
-          label={
-            <Flex gap="sm">
-              {t`Metabot`}
-              <UpsellGem />
-            </Flex>
-          }
-          path="/admin/metabot/1"
-        />
-        <AdminNavItem
-          key="embedded-metabot"
-          icon="metabot"
-          label={
-            <Flex gap="sm">
-              {t`Embedded Metabot`}
-              <UpsellGem />
-            </Flex>
-          }
-          path="/admin/metabot/2"
+          path="/admin/metabot"
         />
       </AdminNavWrapper>
     </Flex>
