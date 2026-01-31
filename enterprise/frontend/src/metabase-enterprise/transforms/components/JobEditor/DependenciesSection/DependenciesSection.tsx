af5272b14ed3dcb1199b7dcb4d04f77ecc791c0d
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-01-31 03:42:25 -0500

    fixes

--- enterprise/frontend/src/metabase-enterprise/transforms/components/JobEditor/DependenciesSection/DependenciesSection.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/components/JobEditor/DependenciesSection/DependenciesSection.tsx
@@ -1,18 +1,20 @@
+import type { Row } from "@tanstack/react-table";
+import { useCallback, useMemo } from "react";
 import { push } from "react-router-redux";
 import { t } from "ttag";
 
-import { AdminContentTable } from "metabase/common/components/AdminContentTable";
 import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
+import CS from "metabase/css/core/index.css";
 import { useDispatch } from "metabase/lib/redux";
 import * as Urls from "metabase/lib/urls";
-import { Card } from "metabase/ui";
+import { Card, TreeTable, useTreeTableInstance } from "metabase/ui";
 import { useListTransformJobTransformsQuery } from "metabase-enterprise/api";
 import type { Transform, TransformJobId } from "metabase-types/api";
 
 import { ListEmptyState } from "../../ListEmptyState";
 import { TitleSection } from "../../TitleSection";
 
-import S from "./DependenciesSection.module.css";
+import { getColumns } from "./utils";
 
 type DependenciesSectionProps = {
   jobId: TransformJobId;
@@ -48,24 +50,40 @@ type TransformTableProps = {
 };
 
 export function TransformTable({ transforms }: TransformTableProps) {
+  const columns = useMemo(() => getColumns(), []);
   const dispatch = useDispatch();
 
-  const handleRowClick = (transform: Transform) => {
-    dispatch(push(Urls.transform(transform.id)));
-  };
+  const handleRowActivate = useCallback(
+    (row: Row<Transform>) => {
+      dispatch(push(Urls.transform(row.original.id)));
+    },
+    [dispatch],
+  );
+
+  const treeTableInstance = useTreeTableInstance<Transform>({
+    data: transforms,
+    columns,
+    getNodeId: (transform) => String(transform.id),
+    onRowActivate: handleRowActivate,
+  });
 
   return (
-    <AdminContentTable columnTitles={[t`Transform`, t`Target`]}>
-      {transforms.map((transform) => (
-        <tr
-          key={transform.id}
-          className={S.row}
-          onClick={() => handleRowClick(transform)}
-        >
-          <td className={S.wrap}>{transform.name}</td>
-          <td className={S.wrap}>{transform.target.name}</td>
-        </tr>
-      ))}
-    </AdminContentTable>
+    <Card
+      className={CS.overflowHidden}
+      p={0}
+      flex="0 1 auto"
+      mih={0}
+      shadow="none"
+      withBorder
+    >
+      <TreeTable
+        instance={treeTableInstance}
+        emptyState={
+          <ListEmptyState label={t`There are no transforms for this job.`} />
+        }
+        ariaLabel={t`Job transforms`}
+        onRowClick={handleRowActivate}
+      />
+    </Card>
   );
 }
