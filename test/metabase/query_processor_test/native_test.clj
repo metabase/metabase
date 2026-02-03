37e7aa63b316f1c038b7af8548536d06f6e53592
Eric Normand <ericwnormand@gmail.com>
2026-02-03 14:00:55 -0600

    Whitespace

--- test/metabase/query_processor_test/native_test.clj
+++ test/metabase/query_processor_test/native_test.clj
@@ -158,4 +158,3 @@
                     [_idx with-default-val without-default-val] (first (mt/rows results))]
                 (testing "MBQL: case with and without default should produce the same result for matching rows"
                   (is (= without-default-val with-default-val original-without-default-val)))))))))))
-
