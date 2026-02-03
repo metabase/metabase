0955ad8e27a90ed0b1ddd8d7e3be46e2cc7b3051
Sébastien <sebastien@metabase.com>
2026-02-03 13:23:36 +0100

    .

--- enterprise/backend/test/metabase_enterprise/database_routing/common_test.clj
+++ enterprise/backend/test/metabase_enterprise/database_routing/common_test.clj
@@ -1,57 +1,51 @@
 (ns metabase-enterprise.database-routing.common-test
   (:require
    [clojure.test :refer [deftest is testing]]
-   [metabase-enterprise.database-routing.common :refer [router-db-or-id->destination-db-id*]]
+   [metabase-enterprise.database-routing.common :refer [router-db-or-id->destination-db-id]]
+   [metabase.api.common :as api]
    [metabase.test :as mt]))
 
+;; Tests for database routing functionality, specifically verifying that guest embedding 
+;; works correctly with database routing by allowing anonymous users to use the router database.
+;; This enables guest embeds to work with databases that have routing enabled.
+
 (deftest router-db-or-id->destination-db-id-test
-  (testing "router-db-or-id->destination-db-id* function"
-    (mt/with-temp [:model/Database router-db {:name "Router DB"}
-                   :model/DatabaseRouter _ {:database_id (:id router-db)
-                                            :user_attribute "department"}
-                   :model/Database dest-db {:name "Engineering DB"
-                                            :router_database_id (:id router-db)}]
+  (mt/with-temp [:model/Database router-db {:name "Router DB"}
+                 :model/DatabaseRouter _ {:database_id (:id router-db)
+                                          :user_attribute "department"}
+                 :model/Database dest-db {:name "Engineering DB"
+                                          :router_database_id (:id router-db)}
+                 :model/User user {:email "test@example.com"}]
 
-      (testing "anonymous users (guest embeds) should return nil (use router database)"
-        (is (nil? (router-db-or-id->destination-db-id*
-                   true  ; is-anonymous-user?
-                   {}    ; user-attributes (empty for anonymous)
-                   false ; is-superuser?
-                   (:id router-db)))))
+    (testing "Anonymous users (guest embeds) should return nil to use router database"
+      ;; Test the key guest embedding scenario: anonymous user should return nil
+      (binding [api/*current-user-id* nil
+                api/*current-user* (atom nil)
+                api/*is-superuser?* false]
+        (is (nil? (router-db-or-id->destination-db-id (:id router-db)))
+            "Anonymous users should return nil to use the router database for guest embedding")))
 
-      (testing "authenticated users with user attribute should route to destination database"
+    (testing "Authenticated users with user attribute should route to destination database"
+      (binding [api/*current-user-id* (:id user)
+                api/*current-user* (atom (assoc user :attributes {"department" "Engineering DB"}))
+                api/*is-superuser?* false]
         (is (= (:id dest-db)
-               (router-db-or-id->destination-db-id*
-                false ; is-anonymous-user?
-                {"department" "Engineering DB"} ; user-attributes
-                false ; is-superuser?
-                (:id router-db)))))
+               (router-db-or-id->destination-db-id (:id router-db)))
+            "Users with matching attributes should be routed to destination database")))
 
-      (testing "authenticated users without user attribute should get an error"
+    (testing "Superusers without user attribute should return nil to use router database"
+      (binding [api/*current-user-id* (:id user)
+                api/*current-user* (atom (assoc user :attributes {}))
+                api/*is-superuser?* true]
+        (is (nil? (router-db-or-id->destination-db-id (:id router-db)))
+            "Superusers should use router database when no user attribute is set")))
+
+    (testing "Authenticated non-superusers without user attribute should get an error"
+      (binding [api/*current-user-id* (:id user)
+                api/*current-user* (atom (assoc user :attributes {}))
+                api/*is-superuser?* false]
         (is (thrown-with-msg?
              clojure.lang.ExceptionInfo
              #"Required user attribute is missing"
-             (router-db-or-id->destination-db-id*
-              false ; is-anonymous-user?
-              {}    ; user-attributes (empty)
-              false ; is-superuser?
-              (:id router-db)))))
-
-      (testing "superusers without user attribute should return nil (use router database)"
-        (is (nil? (router-db-or-id->destination-db-id*
-                   false ; is-anonymous-user?
-                   {}    ; user-attributes (empty)
-                   true  ; is-superuser?
-                   (:id router-db)))))
-
-      (testing "users with __METABASE_ROUTER__ value should return nil (use router database)"
-        (is (nil? (router-db-or-id->destination-db-id*
-                   false ; is-anonymous-user?
-                   {"department" "__METABASE_ROUTER__"} ; user-attributes
-                   false ; is-superuser?
-                   (:id router-db)))))
-
-      ;; Note: Guest embedding now works with database routing. Anonymous users (api/*current-user-id* = nil)
-      ;; are properly handled by binding api/*current-user-id* to nil in embedding-rest.api.common functions.
-      ;; This allows both cards and dashboards in guest embeds to use the router database by default.
-      )))
+             (router-db-or-id->destination-db-id (:id router-db)))
+            "Non-superusers without attributes should get an error")))))
