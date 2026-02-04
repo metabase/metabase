608a31273dbf66ca688e746bf329ee7aec9c1f71
Fraser <fraser@metabase.com>
2026-02-04 21:10:01 +1300

    [ci skip] Shorten deprecation statement
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- frontend/src/metabase/common/components/PopoverWithTrigger/PopoverWithTrigger.tsx
+++ frontend/src/metabase/common/components/PopoverWithTrigger/PopoverWithTrigger.tsx
@@ -2,6 +2,6 @@ import { Popover } from "metabase/common/components/Popover";
 import { Triggerable } from "metabase/common/components/Triggerable";
 
 /**
- * @deprecated prefer Popover from "metabase/ui" + useState instead
+ * @deprecated prefer Popover from "metabase/ui"
  */
 export const PopoverWithTrigger = Triggerable(Popover);
