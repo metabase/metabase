0dd0a9feb2c9c008e19ce2d133df305fe6438485
Edward Paget <ed.paget@gmail.com>
2026-02-04 14:02:30 -0600

    feat: add /api/metric/:id enpoint
    fix the api route from metrics -> metric

--- src/metabase/metrics/api.clj
+++ src/metabase/metrics/api.clj
@@ -1,14 +1,19 @@
 (ns metabase.metrics.api
-  "/api/metrics endpoints."
+  "/api/metric endpoints."
   (:require
+   [metabase.api.common :as api]
    [metabase.api.macros :as api.macros]
    [metabase.collections.models.collection :as collection]
+   [metabase.lib-be.core :as lib-be]
+   [metabase.lib-metric.core :as lib-metric]
    [metabase.request.core :as request]
+   [metabase.util.malli :as mu]
    [metabase.util.malli.registry :as mr]
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
 
 (mr/def ::Metric
+  "Schema for a Metric in list responses (without hydrated dimensions)."
   [:map
    [:id            ms/PositiveInt]
    [:name          :string]
@@ -17,6 +22,16 @@
    [:collection    {:optional true} [:maybe [:map
                                              [:id [:maybe ms/PositiveInt]]
                                              [:name :string]]]]])
+
+(mr/def ::MetricWithDimensions
+  "Schema for a Metric with hydrated dimensions (returned from GET /:id)."
+  [:merge
+   ::Metric
+   [:map
+    [:dimensions         {:optional true} [:maybe [:sequential :map]]]
+    [:dimension_mappings {:optional true} [:maybe [:sequential :map]]]
+    [:dataset_query      {:optional true} :map]
+    [:database_id        {:optional true} [:maybe ms/PositiveInt]]]])
 
 (mr/def ::MetricsResponse
   [:map
@@ -68,3 +83,23 @@
      :limit  limit
      :offset offset
      :data   data}))
+
+(defn- hydrate-dimensions
+  "Hydrate dimensions onto a metric by computing from visible-columns and reconciling with persisted."
+  [metric]
+  (let [database-id       (:database_id metric)
+        mp                (lib-be/application-database-metadata-provider database-id)
+        metric-with-type  (assoc metric :lib/type :metadata/metric)]
+    (lib-metric/hydrate-dimensions mp metric-with-type)))
+
+(mu/defn- hydrated-metric [id :- ms/PositiveInt]
+  (-> (api/read-check (t2/select-one :model/Card :id id :type "metric"))
+      (t2/hydrate :collection)
+      hydrate-dimensions))
+
+(api.macros/defendpoint :get "/:id" :- ::MetricWithDimensions
+  "Fetch a `Metric` with ID.
+
+  Returns the metric with hydrated dimensions and dimension mappings."
+  [{:keys [id]} :- [:map [:id ms/PositiveInt]]]
+  (hydrated-metric id))
