ae68b2411d37bbb870bcc6ffed42588f783cfd6e
Noah Moss <noahbmoss@gmail.com>
2026-02-04 16:55:24 -0500

    fix test nesting

--- enterprise/backend/test/metabase_enterprise/metabot_v3/tools/transforms_test.clj
+++ enterprise/backend/test/metabase_enterprise/metabot_v3/tools/transforms_test.clj
@@ -27,34 +27,34 @@
                   (mt/with-test-user :rasta
                     (let [result   (:structured_output (metabot-v3.tools.transforms/get-transforms {}))
                           returned (first (filter #(= (:id transform) (:id %)) result))]
-                      (is (nil? returned))))))))))))
+                      (is (nil? returned)))))))))))))
 
-  (deftest get-transform-details-test
-    (testing "get-transform-details returns transform when user can query the source database"
-      (mt/with-premium-features #{:metabot-v3 :transforms}
-        (mt/with-temp [:model/Transform transform
-                       {:name   "Test Transform"
-                        :source {:type  "query"
-                                 :query (lib/native-query (mt/metadata-provider) "SELECT 1")}}]
-          (mt/with-test-user :crowberto
-            (let [result (:structured_output (metabot-v3.tools.transforms/get-transform-details
-                                              {:transform-id (:id transform)}))]
-              (is (= (:id transform) (:id result)))))))))
+(deftest get-transform-details-test
+  (testing "get-transform-details returns transform when user can query the source database"
+    (mt/with-premium-features #{:metabot-v3 :transforms}
+      (mt/with-temp [:model/Transform transform
+                     {:name   "Test Transform"
+                      :source {:type  "query"
+                               :query (lib/native-query (mt/metadata-provider) "SELECT 1")}}]
+        (mt/with-test-user :crowberto
+          (let [result (:structured_output (metabot-v3.tools.transforms/get-transform-details
+                                            {:transform-id (:id transform)}))]
+            (is (= (:id transform) (:id result)))))))))
 
-  (deftest get-transform-details-blocked-test
-    (testing "get-transform-details throws when user cannot query the source database"
-      (mt/with-premium-features #{:metabot-v3 :transforms}
-        (mt/with-temp [:model/Database {db-id :id} {}]
-          (let [mp (lib-be/application-database-metadata-provider db-id)]
-            (mt/with-temp [:model/Transform transform
-                           {:name   "Blocked Transform"
-                            :source {:type  "query"
-                                     :query (lib/native-query mp "SELECT 1")}}]
-              (mt/with-data-analyst-role! (mt/user->id :rasta)
-                (mt/with-db-perm-for-group! (perms-group/all-users) db-id :perms/create-queries :no
-                  (mt/with-test-user :rasta
-                    (is (thrown-with-msg?
-                         clojure.lang.ExceptionInfo
-                         #"You don't have permissions to do that"
-                         (metabot-v3.tools.transforms/get-transform-details
-                          {:transform-id (:id transform)})))))))))))))
+(deftest get-transform-details-blocked-test
+  (testing "get-transform-details throws when user cannot query the source database"
+    (mt/with-premium-features #{:metabot-v3 :transforms}
+      (mt/with-temp [:model/Database {db-id :id} {}]
+        (let [mp (lib-be/application-database-metadata-provider db-id)]
+          (mt/with-temp [:model/Transform transform
+                         {:name   "Blocked Transform"
+                          :source {:type  "query"
+                                   :query (lib/native-query mp "SELECT 1")}}]
+            (mt/with-data-analyst-role! (mt/user->id :rasta)
+              (mt/with-db-perm-for-group! (perms-group/all-users) db-id :perms/create-queries :no
+                (mt/with-test-user :rasta
+                  (is (thrown-with-msg?
+                       clojure.lang.ExceptionInfo
+                       #"You don't have permissions to do that"
+                       (metabot-v3.tools.transforms/get-transform-details
+                        {:transform-id (:id transform)}))))))))))))
