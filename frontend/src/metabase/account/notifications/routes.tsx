4530a585ad21150254e6f3526742d2a7ec6f5055
Stas Gavrylov <stasgavrylov@gmail.com>
2026-02-04 05:02:48 -0800

    Migrate to TS: ModalRoute refinement

--- frontend/src/metabase/account/notifications/routes.tsx
+++ frontend/src/metabase/account/notifications/routes.tsx
@@ -1,4 +1,3 @@
-import type { ComponentType } from "react";
 import { Route } from "react-router";
 
 import { ModalRoute } from "metabase/hoc/ModalRoute";
@@ -10,32 +9,19 @@ import { NotificationsApp } from "./containers/NotificationsApp";
 import { UnsubscribeAlertModal } from "./containers/UnsubscribeAlertModal";
 import UnsubscribePulseModal from "./containers/UnsubscribePulseModal";
 
-// ModalRoute passes additional props (params, location) to modals via spread,
-// but its type only declares onClose. Cast to ComponentType to work around this.
-// TODO: Fix ModalRoute types to properly declare the props it passes to modals.
-const modal = <T,>(component: T) =>
-  component as ComponentType<{ onClose: () => void }>;
-
 const getRoutes = (): React.JSX.Element => (
   <Route path="notifications" component={NotificationsApp}>
-    <ModalRoute path="help" modal={modal(HelpModal)} />
-    <ModalRoute
-      path="alert/:alertId/archive"
-      modal={modal(DeleteAlertModal)}
-      noWrap
-    />
-    <ModalRoute
-      path="pulse/:pulseId/archive"
-      modal={modal(ArchivePulseModal)}
-    />
+    <ModalRoute path="help" modal={HelpModal} />
+    <ModalRoute path="alert/:alertId/archive" modal={DeleteAlertModal} noWrap />
+    <ModalRoute path="pulse/:pulseId/archive" modal={ArchivePulseModal} />
     <ModalRoute
       path="alert/:alertId/unsubscribe"
-      modal={modal(UnsubscribeAlertModal)}
+      modal={UnsubscribeAlertModal}
       noWrap
     />
     <ModalRoute
       path="pulse/:pulseId/unsubscribe"
-      modal={modal(UnsubscribePulseModal)}
+      modal={UnsubscribePulseModal}
     />
   </Route>
 );
