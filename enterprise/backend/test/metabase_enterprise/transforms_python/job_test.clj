9eb1b0cd5ec5d44e7f1b73b1db6cc908e2852cdd
John Swanson <john.swanson@metabase.com>
2026-02-04 12:03:33 -0800

    kondo

--- enterprise/backend/test/metabase_enterprise/transforms_python/job_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms_python/job_test.clj
@@ -1,6 +1,7 @@
 (ns ^:mb/driver-tests ^:mb/transforms-python-test metabase-enterprise.transforms-python.job-test
   (:require
    [clojure.test :refer :all]
+   ^{:clj-kondo/ignore [:discouraged-namespace]}
    [clojure.tools.logging :as log]
    [metabase.driver :as driver]
    [metabase.models.transforms.transform-run :as transform-run]
