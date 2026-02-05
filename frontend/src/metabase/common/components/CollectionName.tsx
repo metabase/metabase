b3f643fad67a15a020e0faba79dc543e2a872eac
Fraser <fraser@metabase.com>
2026-02-05 15:27:04 +1300

    Convert CollectionName to TypeScript (#69082)

--- frontend/src/metabase/common/components/CollectionName.tsx
+++ frontend/src/metabase/common/components/CollectionName.tsx
@@ -1,13 +1,17 @@
-/* eslint-disable react/prop-types */
 import { Collections, ROOT_COLLECTION } from "metabase/entities/collections";
 import { useTranslateContent } from "metabase/i18n/hooks";
+import type { CollectionId } from "metabase-types/api";
 
-export const CollectionName = ({ id }) => {
+interface CollectionNameProps {
+  id: CollectionId | null | undefined;
+}
+
+export const CollectionName = ({ id }: CollectionNameProps) => {
   const tc = useTranslateContent();
 
   if (id === "root" || id === null) {
     return <span>{tc(ROOT_COLLECTION.name)}</span>;
-  } else if (id === undefined || isNaN(id)) {
+  } else if (id === undefined || (typeof id === "number" && isNaN(id))) {
     return null;
   } else {
     return <Collections.Name id={id} />;
