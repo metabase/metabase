7181868ada5bc5ac6d843ada51f24b31fb1e2570
lbrdnk <lbrdnk@users.noreply.github.com>
2026-02-06 14:43:26 +0100

    Revert "Fix replace names quoting, mysql"
    This reverts commit e34135fad6a51cb8d89eedf5d805fc7004dd0b0d.
    
    https://github.com/metabase/metabase/actions/runs/21750654625/job/62747500670?pr=68911
    There's set of different failures. Interestingly, also in macaw
    replace-names tests. That is due to direct use of sql-tools/replace-names
    in those tests, not the macaw specfic implementation.

--- enterprise/backend/src/metabase_enterprise/workspaces/impl.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/impl.clj
@@ -9,7 +9,6 @@
    [metabase-enterprise.workspaces.util :as ws.u]
    [metabase.driver.sql :as driver.sql]
    [metabase.driver.sql.util :as sql.util]
-   [metabase.sql-tools.settings :as sql-tools.settings]
    [metabase.util :as u]
    [metabase.util.log :as log]
    [toucan2.core :as t2]))
@@ -176,14 +175,7 @@
                                        (or (driver.sql/default-schema engine)
                                            ((some-fn :dbname :db) details)))
                                      databases)
-        #_#_db-id->quoted    (u/index-by :id quote-default-schema databases)
-        db-id->quoted    (u/index-by :id (fn [{:keys [details engine] :as db}]
-                                           (if (= :macaw (sql-tools.settings/sql-tools-parser-backend))
-                                             (quote-default-schema db)
-                                             (or (driver.sql/default-schema engine)
-                                                 ;; For MySQL and similar, use database name from connection details
-                                                 ((some-fn :dbname :db) details))))
-                                     databases)
+        db-id->quoted    (u/index-by :id quote-default-schema databases)
         fallback-map     (merge
                           (table-ids-fallbacks :global_schema :global_table :global_table_id all-outputs)
                           (table-ids-fallbacks :isolated_schema :isolated_table :isolated_table_id all-outputs))
