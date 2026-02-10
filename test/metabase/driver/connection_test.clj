a9614778576e84d46e6e2ef9023291d46e5462d2
Timothy Dean <timothy@timothydean.dev>
2026-02-10 16:59:28 -0700

    Move mongo tests to mongo module

--- test/metabase/driver/connection_test.clj
+++ test/metabase/driver/connection_test.clj
@@ -1,8 +1,7 @@
 (ns metabase.driver.connection-test
   (:require
    [clojure.test :refer :all]
-   [metabase.driver.connection :as driver.conn]
-   [metabase.driver.mongo.database :as mongo.db]))
+   [metabase.driver.connection :as driver.conn]))
 
 (deftest effective-details-default-test
   (testing "effective-details returns :details when *connection-type* is :default"
@@ -64,39 +63,4 @@
       (is (= :write-data driver.conn/*connection-type*)))
     (is (= :default driver.conn/*connection-type*))))
 
-;;; ------------------------------------------------ Driver Integration Tests ------------------------------------------------
 
-(deftest mongo-details-normalized-respects-connection-type-test
-  (testing "Mongo's details-normalized uses effective-details for full database objects"
-    (let [read-details {:dbname "test-db" :host "read-host"}
-          write-details {:dbname "test-db" :host "write-host"}
-          database {:details read-details
-                    :write_data_details write-details}]
-      (testing "default connection type uses :details"
-        (let [result (mongo.db/details-normalized database)]
-          (is (= "read-host" (:host result)))))
-      (testing "write connection type uses :write_data_details"
-        (driver.conn/with-write-connection
-          (let [result (mongo.db/details-normalized database)]
-            (is (= "write-host" (:host result))))))))
-
-  (testing "Mongo's details-normalized falls back to :details when no write details"
-    (let [read-details {:dbname "test-db" :host "read-host"}
-          database {:details read-details}]
-      (driver.conn/with-write-connection
-        (let [result (mongo.db/details-normalized database)]
-          (is (= "read-host" (:host result))))))))
-
-(deftest mongo-details-normalized-conn-uri-respects-connection-type-test
-  (testing "Mongo's details-normalized uses effective-details for conn-uri databases"
-    (let [read-details {:conn-uri "mongodb://read-host:27017/test-db"}
-          write-details {:conn-uri "mongodb://write-host:27017/test-db"}
-          database {:details read-details
-                    :write_data_details write-details}]
-      (testing "default connection type uses :details"
-        (let [result (mongo.db/details-normalized database)]
-          (is (= "mongodb://read-host:27017/test-db" (:conn-uri result)))))
-      (testing "write connection type uses :write_data_details"
-        (driver.conn/with-write-connection
-          (let [result (mongo.db/details-normalized database)]
-            (is (= "mongodb://write-host:27017/test-db" (:conn-uri result)))))))))
