3515e8073c883982195315c7fb1ea84a645f207a
Nicola Mometto <nicola.mometto@metabase.com>
2026-02-10 14:18:41 +0000

    feat(transforms-util): disable-max-results

--- src/metabase/transforms/util.clj
+++ src/metabase/transforms/util.clj
@@ -19,6 +19,7 @@
    [metabase.models.transforms.transform-run :as transform-run]
    [metabase.query-processor :as qp]
    [metabase.query-processor.compile :as qp.compile]
+   [metabase.query-processor.middleware.limit :as limit]
    [metabase.query-processor.parameters.dates :as params.dates]
    [metabase.query-processor.pipeline :as qp.pipeline]
    [metabase.sync.core :as sync]
@@ -271,7 +272,9 @@
 (defn massage-sql-query
   "Adjusts mbql query for use in a transform."
   [query]
-  (assoc-in query [:middleware :disable-remaps?] true))
+  (-> query
+      (assoc-in [:middleware :disable-remaps?] true)
+      limit/disable-max-results))
 
 (defn- checkpoint-incremental?
   "Returns true if `source` uses checkpoint-based incremental strategy."
