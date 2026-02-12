544672a63e8f6207a6030963a6cfc604cdc85c02
Nick Fitzpatrick <nickfitz.582@gmail.com>
2026-02-12 09:46:41 -0400

    Wochentag

--- e2e/test/scenarios/visualizations-tabular/drillthroughs/column_extract_drill.cy.spec.js
+++ e2e/test/scenarios/visualizations-tabular/drillthroughs/column_extract_drill.cy.spec.js
@@ -242,7 +242,7 @@ describe("extract action", () => {
       H.openOrdersTable({ limit: 1 });
       extractColumnAndCheck({
         column: "Created At",
-        option: "Tag der Woche",
+        option: "Wochentag",
         value: "Dienstag",
         extraction: "Auszug Tag, Monat…",
       });
