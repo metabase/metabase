75b6db177f9db77483d5b6cf58d1503dd321bbbf
Romeo Van Snick <romeo@romeovansnick.be>
2026-02-12 17:32:32 +1100

    Make serializeCardForUrl implementation match the Question.serializeCardForUrl implementation

--- frontend/src/metabase/lib/card.ts
+++ frontend/src/metabase/lib/card.ts
@@ -4,21 +4,49 @@ import { b64hash_to_utf8, utf8_to_b64url } from "metabase/lib/encoding";
 import { equals } from "metabase/lib/utils";
 import type { Card, UnsavedCard } from "metabase-types/api";
 
-function getCleanCard(card: Card | UnsavedCard) {
-  return _.pick(card, [
+type SerializeCardOptions = {
+  includeDatasetQuery?: boolean;
+  includeOriginalCardId?: boolean;
+  includeDisplayIsLocked?: boolean;
+  creationType?: string;
+};
+
+function getCleanCard(
+  card: Card | UnsavedCard,
+  {
+    includeDatasetQuery = true,
+    includeOriginalCardId = true,
+    includeDisplayIsLocked = false,
+    creationType,
+  }: SerializeCardOptions = {},
+) {
+  const keysToInclude = [
     "collection_id",
+    "dashboard_id",
     "dashboardId",
     "dashcardId",
-    "dataset_query",
     "description",
     "display",
-    "displayIsLocked",
     "name",
-    "original_card_id",
     "parameters",
     "type",
     "visualization_settings",
-  ]);
+  ];
+
+  if (includeDatasetQuery) {
+    keysToInclude.push("dataset_query");
+  }
+  if (includeOriginalCardId) {
+    keysToInclude.push("original_card_id");
+  }
+  if (includeDisplayIsLocked) {
+    keysToInclude.push("displayIsLocked");
+  }
+  if (creationType) {
+    keysToInclude.push("creationType");
+  }
+
+  return _.pick({ ...card, creationType }, ...keysToInclude);
 }
 
 export function isEqualCard(card1: Card, card2: Card) {
@@ -30,8 +58,11 @@ export function isEqualCard(card1: Card, card2: Card) {
 }
 
 // TODO Atte Keinänen 5/31/17 Deprecated, we should move tests to Questions.spec.js
-export function serializeCardForUrl(card: Card | UnsavedCard): string {
-  return utf8_to_b64url(JSON.stringify(getCleanCard(card)));
+export function serializeCardForUrl(
+  card: Card | UnsavedCard,
+  options: SerializeCardOptions = {},
+) {
+  return utf8_to_b64url(JSON.stringify(getCleanCard(card, options)));
 }
 
 export function deserializeCardFromUrl(serialized: string): Card {
