92b3602024b1a369f26069b13d4864efcb593b19
Kamil Mielnik <kamil@kamilmielnik.com>
2026-02-12 17:53:13 +0700

    Add a repro for GDGT-1776

--- e2e/test/scenarios/data-studio/transforms/reproductions.cy.spec.ts
+++ e2e/test/scenarios/data-studio/transforms/reproductions.cy.spec.ts
@@ -1,3 +1,5 @@
+import { createMockSearchResult } from "metabase-types/api/mocks";
+
 const { H } = cy;
 
 describe("issue #68378", () => {
@@ -25,6 +27,54 @@ describe("issue #68378", () => {
     H.popover().findByText("empty_schema").should("be.visible").click();
 
     H.modal().button("Save").click();
+  });
+});
+
+describe("issue GDGT-1776", () => {
+  beforeEach(() => {
+    H.restore("postgres-writable");
+    H.resetTestTable({ type: "postgres", table: "empty_schema" });
+    cy.signInAsAdmin();
+    H.activateToken("bleeding-edge");
+
+    const ITEMS_COUNT = 1000;
+
+    cy.intercept("GET", "/api/collection/root/items*", {
+      statusCode: 200,
+      body: {
+        data: Array.from({ length: ITEMS_COUNT }).map((_value, index) => {
+          return createMockSearchResult({
+            id: index + 1,
+            model: "table",
+          });
+        }),
+        limit: null,
+        models: [
+          "card",
+          "collection",
+          "dashboard",
+          "dataset",
+          "document",
+          "metric",
+          "pulse",
+          "table",
+          "timeline",
+        ],
+        offset: null,
+        total: ITEMS_COUNT,
+      },
+    }).as("getUsers");
+  });
+
+  it("should not crash the app when processing lots of hidden items in the MiniPicker (GDGT-1776)", () => {
+    visitTransformListPage();
+    cy.button("Create a transform").click();
+    H.popover().findByText("Query builder").click();
+    H.popover().findByText("Our analytics").click();
+
+    cy.findByTestId("loading-indicator").should("not.exist");
+    H.main().findByText("Something’s gone wrong").should("not.exist");
+    cy.button("Cancel").should("be.visible");
   });
 });
 
