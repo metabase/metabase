5b6a814fc9986483d898cab497b2b38a81bc0215
Romeo Van Snick <romeo@romeovansnick.be>
2026-02-12 17:28:28 +1100

    Simplify serializeCardForUrl

--- frontend/src/metabase/lib/card.ts
+++ frontend/src/metabase/lib/card.ts
@@ -1,23 +1,24 @@
+import _ from "underscore";
+
 import { b64hash_to_utf8, utf8_to_b64url } from "metabase/lib/encoding";
 import { equals } from "metabase/lib/utils";
 import type { Card, UnsavedCard } from "metabase-types/api";
 
 function getCleanCard(card: Card | UnsavedCard) {
-  return {
-    name: "name" in card ? card.name : undefined,
-    collection_id: "collection_id" in card ? card.collection_id : undefined,
-    description: "description" in card ? card.description : undefined,
-    dataset_query: card.dataset_query,
-    display: card.display,
-    displayIsLocked:
-      "displayIsLocked" in card ? card.displayIsLocked : undefined,
-    parameters: card.parameters,
-    dashboardId: card.dashboardId,
-    dashcardId: card.dashcardId,
-    visualization_settings: card.visualization_settings,
-    original_card_id: card.original_card_id,
-    type: "type" in card ? card.type : undefined,
-  };
+  return _.pick(card, [
+    "collection_id",
+    "dashboardId",
+    "dashcardId",
+    "dataset_query",
+    "description",
+    "display",
+    "displayIsLocked",
+    "name",
+    "original_card_id",
+    "parameters",
+    "type",
+    "visualization_settings",
+  ]);
 }
 
 export function isEqualCard(card1: Card, card2: Card) {
