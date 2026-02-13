1e48df80c3b467f4bb543fc20d8039d411ed18af
Romeo Van Snick <romeo@romeovansnick.be>
2026-02-13 14:33:10 +1100

    Correctly ignore possibly infinite type instantiations

--- frontend/src/metabase/documents/documents.slice.ts
+++ frontend/src/metabase/documents/documents.slice.ts
@@ -84,7 +84,8 @@ const documentsSlice = createSlice({
       state.cardEmbeds = action.payload;
     },
     setCurrentDocument: (state, action: PayloadAction<Document | null>) => {
-      // @ts-expect-error - possibly infinite type error
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore - possibly infinite type error
       state.currentDocument = action.payload;
     },
     resetDocuments: () => {
