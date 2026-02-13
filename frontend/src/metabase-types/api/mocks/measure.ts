371590b1bcd75b1de540f2d624ecfb0e794f8030
Romeo Van Snick <romeo@romeovansnick.be>
2026-02-13 14:21:50 +1100

    Break cyclic imports

--- frontend/src/metabase-types/api/mocks/measure.ts
+++ frontend/src/metabase-types/api/mocks/measure.ts
@@ -1,6 +1,4 @@
-import * as Lib from "metabase-lib";
-import { SAMPLE_PROVIDER } from "metabase-lib/test-helpers";
-import type { Measure } from "metabase-types/api";
+import type { Measure, OpaqueDatasetQuery } from "metabase-types/api";
 
 export const createMockMeasure = (opts?: Partial<Measure>): Measure => ({
   id: 1,
@@ -8,18 +6,12 @@ export const createMockMeasure = (opts?: Partial<Measure>): Measure => ({
   description: "A measure",
   table_id: 1,
   archived: false,
-  definition: Lib.toJsQuery(
-    Lib.createTestQuery(SAMPLE_PROVIDER, {
-      stages: [
-        {
-          source: {
-            type: "table",
-            id: 1,
-          },
-        },
-      ],
-    }),
-  ),
+  // HACK: we cannot create a valid OpaqueDatasetQuery here since that would
+  // lead to cyclic imports in metabase-types/api/mocks.
+  //
+  // Most unit tests don't rely on the definition of the measure and the ones that do
+  // can override the definition with a valid OpaqueDatasetQuery.
+  definition: {} as unknown as OpaqueDatasetQuery,
   definition_description: "",
   created_at: "2021-01-01T00:00:00Z",
   updated_at: "2021-01-01T00:00:00Z",
