1797e77fe3b5ecbc43cda416bd4ba8c0e60dfd9e
Anderson <ands.alves.nunes@gmail.com>
2026-02-13 20:38:45 -0300

    Add tracking events for transform run tags update and dependency diagnostics row selection (#69531)
    * Add events for transform run tags and dependency diagnostics row selection
    
    * Fix e2e test
    
    * Adjust event as per review / Fix e2e tests

--- e2e/test/scenarios/dependencies/dependency-broken-list.cy.spec.ts
+++ e2e/test/scenarios/dependencies/dependency-broken-list.cy.spec.ts
@@ -82,10 +82,12 @@ describe("scenarios > dependencies > broken list", () => {
     cy.signInAsAdmin();
     H.activateToken("bleeding-edge");
     createContent();
+    H.resetSnowplow();
   });
 
   afterEach(() => {
     dropTransformTable();
+    H.expectNoBadSnowplowEvents();
   });
 
   describe("analysis", () => {
@@ -98,8 +100,8 @@ describe("scenarios > dependencies > broken list", () => {
     });
   });
 
-  describe("sidebar", () => {
-    it("should show broken dependents", () => {
+  describe("selecting entities", () => {
+    it("should show sidebar for broken dependents and trigger snowplow event", () => {
       H.DependencyDiagnostics.visitBrokenDependencies();
 
       cy.log("table dependents");
@@ -109,6 +111,11 @@ describe("scenarios > dependencies > broken list", () => {
         transform: TABLE_TRANSFORM,
         missingColumns: ["score", "status"],
         brokenDependents: BROKEN_TABLE_DEPENDENTS,
+      });
+      H.expectUnstructuredSnowplowEvent({
+        event: "dependency_diagnostics_entity_selected",
+        triggered_from: "broken",
+        event_detail: "table",
       });
 
       cy.log("question dependents");
