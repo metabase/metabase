45cb2a5c9c8d914a7b87c9a8d6bbba8e8db85f22
Fraser <frasermcintosh1@gmail.com>
2026-02-16 13:22:33 +1300

    Tidy types

--- frontend/src/metabase/admin/datamodel/selectors.ts
+++ frontend/src/metabase/admin/datamodel/selectors.ts
@@ -3,5 +3,4 @@ import type { State } from "metabase-types/store";
 export const getPreviewSummary = (state: State) =>
   state.admin.datamodel.previewSummary;
 export const getRevisions = (state: State) => state.admin.datamodel.revisions;
-// Non-null assertion: getCurrentUser is only used in authenticated contexts
-export const getCurrentUser = (state: State) => state.currentUser!;
+export const getCurrentUser = (state: State) => state.currentUser;
