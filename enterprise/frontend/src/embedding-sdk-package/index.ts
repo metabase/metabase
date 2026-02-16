2f623d72bf88192739bec3df52f917d0d90c2579
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2026-02-16 07:01:15 -0800

    Fix doc error

--- enterprise/frontend/src/embedding-sdk-package/index.ts
+++ enterprise/frontend/src/embedding-sdk-package/index.ts
@@ -134,7 +134,10 @@ export type {
   SqlParameterValues,
 } from "embedding-sdk-bundle/types";
 
-export type { ModularEmbeddingEntityType } from "metabase-types/store/embedding-data-picker";
+export type {
+  ModularEmbeddingEntityType,
+  EmbeddingDataPicker,
+} from "metabase-types/store/embedding-data-picker";
 
 export type { ParameterValues } from "metabase/embedding-sdk/types/dashboard";
 export type { IconName } from "metabase/embedding-sdk/types/icon";
