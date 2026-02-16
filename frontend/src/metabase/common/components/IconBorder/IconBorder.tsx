2a76e71093373e0e9a76ab3192ac1f6f9d2fec91
Uladzimir Havenchyk <uladzimir@metabase.com>
2026-02-16 15:41:53 +0300

    chore: convert IconBorder component to typescript

---
+++ frontend/src/metabase/common/components/IconBorder/IconBorder.tsx
@@ -0,0 +1,38 @@
+import { Flex, type FlexProps } from "metabase/ui";
+
+interface IconBorderProps extends FlexProps {
+  borderWidth?: number;
+  borderStyle?: React.CSSProperties["borderStyle"];
+  borderColor?: React.CSSProperties["borderColor"];
+  className?: string;
+  style?: React.CSSProperties;
+  children: React.ReactElement<{
+    size?: number | string;
+    width?: number | string;
+  }>;
+}
+
+export function IconBorder({
+  borderWidth = 1,
+  borderStyle = "solid",
+  borderColor = "currentcolor",
+  className,
+  children,
+}: IconBorderProps) {
+  const size =
+    parseInt(String(children.props.size || children.props.width), 10) * 2;
+
+  return (
+    <Flex
+      align="center"
+      justify="center"
+      bdrs="99px"
+      bd={`${borderWidth}px ${borderStyle} ${borderColor}`}
+      w={size}
+      h={size}
+      className={className}
+    >
+      {children}
+    </Flex>
+  );
+}
