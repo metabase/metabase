e0d2c4ab06a73e8198bd0b4d547f7fe18b2898c4
Kevin Kelleher <kevin.kelleher@metabase.com>
2026-02-17 15:54:47 -0500

    Visualizer trend lines and bug fixes: colors, settings, legends, axes (#69296)

--- frontend/src/metabase/visualizer/utils/merge-data.ts
+++ frontend/src/metabase/visualizer/utils/merge-data.ts
@@ -8,6 +8,7 @@ import type {
   VisualizerDataSource,
   VisualizerDataSourceId,
 } from "metabase-types/api";
+import type { Insight } from "metabase-types/api/insight";
 
 import { extractReferencedColumns } from "./column";
 import { getDataSourceIdFromNameRef, isDataSourceNameRef } from "./data-source";
@@ -46,6 +47,7 @@ export function mergeVisualizerData({
   const referencedColumns = extractReferencedColumns(columnValuesMapping);
 
   const referencedColumnValuesMap: Record<string, RowValues> = {};
+  const insights: Insight[] = [];
   referencedColumns.forEach((ref) => {
     const dataset = datasets[ref.sourceId];
     if (!dataset) {
@@ -57,6 +59,15 @@ export function mergeVisualizerData({
     if (columnIndex >= 0) {
       const values = dataset.data.rows.map((row) => row[columnIndex]);
       referencedColumnValuesMap[ref.name] = values;
+    }
+    const insight = dataset.data.insights?.find(
+      (insight) => insight.col === ref.originalName,
+    );
+    if (insight) {
+      insights.push({
+        ...insight,
+        col: ref.name,
+      });
     }
   });
 
@@ -80,6 +91,7 @@ export function mergeVisualizerData({
   return {
     cols: columns,
     rows: _.zip(...unzippedRows),
+    insights,
     results_metadata: { columns },
   };
 }
