4f2d4a8e1ebe200c00a530b4e0234ff2211e73cd
Fraser <frasermcintosh1@gmail.com>
2026-02-18 10:07:56 +1300

    Revert to class component

--- frontend/src/metabase/common/components/ExpandingContent/ExpandingContent.tsx
+++ frontend/src/metabase/common/components/ExpandingContent/ExpandingContent.tsx
@@ -1,82 +1,84 @@
 import cx from "classnames";
-import {
-  type ReactNode,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { Component, type ReactNode } from "react";
 
 import CS from "metabase/css/core/index.css";
 import { isReducedMotionPreferred } from "metabase/lib/dom";
 
 interface ExpandingContentProps {
-  isOpen?: boolean;
-  isInitiallyOpen?: boolean;
-  duration?: number;
-  animateHeight?: boolean;
-  animateOpacity?: boolean;
-  children?: ReactNode;
+  isOpen: boolean;
+  isInitiallyOpen: boolean;
+  children: ReactNode;
 }
 
-export function ExpandingContent({
-  isOpen: isOpenProp,
-  isInitiallyOpen,
-  duration = 300,
-  animateHeight = true,
-  animateOpacity = true,
-  children,
-}: ExpandingContentProps) {
-  const [isOpen, setIsOpen] = useState(
-    isInitiallyOpen == null ? true : !!isInitiallyOpen,
-  );
-  const [isTransitioning, setIsTransitioning] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
-  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+interface ExpandingContentState {
+  isOpen: boolean;
+  isTransitioning: boolean;
+}
 
-  const clearTimer = useCallback(() => {
-    if (timerRef.current != null) {
-      clearTimeout(timerRef.current);
-      timerRef.current = null;
+export class ExpandingContent extends Component<
+  ExpandingContentProps,
+  ExpandingContentState
+> {
+  _ref: HTMLDivElement | null = null;
+  _timer: ReturnType<typeof setTimeout> | null = null;
+
+  constructor(props: ExpandingContentProps) {
+    super(props);
+    this.state = {
+      isOpen: props.isInitiallyOpen == null ? true : !!props.isInitiallyOpen,
+      // keep track of whether we're currently transitioning so we can set maxHeight to "none" when not
+      isTransitioning: false,
+    };
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps: ExpandingContentProps) {
+    this.setOpen(nextProps.isOpen);
+  }
+  componentDidMount() {
+    this.setOpen(this.props.isOpen);
+  }
+  componentWillUnmount() {
+    this.clearTimer();
+  }
+
+  setOpen(isOpen: boolean | undefined) {
+    const open = !!isOpen;
+    if (this.state.isOpen !== open) {
+      this.clearTimer();
+      this.setState({ isOpen: open, isTransitioning: true }, () => {
+        this._timer = setTimeout(() => {
+          this.setState({ isTransitioning: false });
+        }, 300);
+      });
     }
-  }, []);
-
-  useEffect(() => {
-    const open = !!isOpenProp;
-    if (isOpen !== open) {
-      clearTimer();
-      setIsOpen(open);
-      setIsTransitioning(true);
-      timerRef.current = setTimeout(() => {
-        setIsTransitioning(false);
-      }, duration);
+  }
+  clearTimer() {
+    if (this._timer != null) {
+      clearTimeout(this._timer);
+      this._timer = null;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isOpenProp, duration, clearTimer]);
+  }
 
-  useEffect(() => {
-    return clearTimer;
-  }, [clearTimer]);
-
-  const transition = isReducedMotionPreferred()
-    ? "none"
-    : `all ${duration}ms ease`;
-
-  const maxHeight: number | string = isTransitioning
-    ? (ref.current && ref.current.scrollHeight) || 0
-    : "none";
-
-  return (
-    <div
-      ref={ref}
-      style={{
-        transition,
-        maxHeight: !animateHeight || isOpen ? maxHeight : 0,
-        opacity: !animateOpacity || isOpen ? 1 : 0,
-      }}
-      className={cx({ [CS.overflowHidden]: !isOpen })}
-    >
-      {children}
-    </div>
-  );
+  render() {
+    const { children } = this.props;
+    const transition = isReducedMotionPreferred() ? "none" : "all 300ms ease";
+    const { isOpen, isTransitioning } = this.state;
+    // get the actual content height (after the first render)
+    const maxHeight = isTransitioning
+      ? (this._ref && this._ref.scrollHeight) || 0
+      : "none";
+    return (
+      <div
+        ref={(ref) => (this._ref = ref)}
+        style={{
+          transition,
+          maxHeight: isOpen ? maxHeight : 0,
+          opacity: isOpen ? 1 : 0,
+        }}
+        className={cx({ [CS.overflowHidden]: !isOpen })}
+      >
+        {children}
+      </div>
+    );
+  }
 }
