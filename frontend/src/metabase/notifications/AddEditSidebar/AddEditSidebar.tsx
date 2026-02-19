f76b3caa4e33fc911f771f6f66ca234fe73696d7
Fraser <frasermcintosh1@gmail.com>
2026-02-19 14:42:54 +1300

    Tidy types

--- frontend/src/metabase/notifications/AddEditSidebar/AddEditSidebar.tsx
+++ frontend/src/metabase/notifications/AddEditSidebar/AddEditSidebar.tsx
@@ -8,10 +8,10 @@ import type { State } from "metabase-types/store";
 import { AddEditEmailSidebar as AddEditEmailSidebarComponent } from "./AddEditEmailSidebar";
 import { AddEditSlackSidebar as AddEditSlackSidebarComponent } from "./AddEditSlackSidebar";
 
-const mapStateToProps = (state: State, props: Record<string, unknown>) => {
+const mapStateToProps = (state: State) => {
   return {
-    parameters: getParameters(state, props),
-    hiddenParameters: getTabHiddenParameterSlugs(state, props),
+    parameters: getParameters(state),
+    hiddenParameters: getTabHiddenParameterSlugs(state),
   };
 };
 
