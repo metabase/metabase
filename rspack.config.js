dfc25a51f1ee8e04e271b5e36f75c5f586bf2b69
Timofei <timofei@metabase.com>
2026-02-19 23:31:38 +0400

    Build only SDK

--- rspack.config.js
+++ rspack.config.js
@@ -2,7 +2,11 @@
 /* eslint-env node */
 /* eslint-disable import/no-commonjs */
 
-const configs = [require("./rspack.main.config")];
+const configs = [];
+
+if (process.env.SKIP_MAIN_APP !== "true") {
+  configs.push(require("./rspack.main.config"));
+}
 
 if (process.env.SKIP_EMBEDDING_SDK !== "true") {
   // Build the embed.js script for the sdk iframe embedding
