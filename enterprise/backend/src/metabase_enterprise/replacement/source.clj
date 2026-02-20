adc39fd7baadeafabe7155916e5a2cb436b3b128
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-02-20 01:14:59 -0500

    wip fe

--- enterprise/backend/src/metabase_enterprise/replacement/source.clj
+++ enterprise/backend/src/metabase_enterprise/replacement/source.clj
@@ -4,6 +4,7 @@
    [metabase.lib-be.core :as lib-be]
    [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
+   [metabase.util :as u]
    [metabase.util.malli.registry :as mr]
    [toucan2.core :as t2]))
 
@@ -57,9 +58,9 @@
   {:id             (:id col)
    :name           (column-match-key col)
    :display_name   (or (:display-name col) "")
-   :base_type      (some-> (:base-type col) name)
-   :effective_type (some-> (:effective-type col) name)
-   :semantic_type  (some-> (:semantic-type col) name)})
+   :base_type      (some-> (:base-type col) u/qualified-name)
+   :effective_type (some-> (:effective-type col) u/qualified-name)
+   :semantic_type  (some-> (:semantic-type col) u/qualified-name)})
 
 (defn- fetch-source
   "Fetch source metadata and its database ID."
