5f20304033b05c32229f710333219c61c24d415f
Fraser <fraser@metabase.com>
2026-02-23 11:06:38 +1300

    Try fix e2e tests

--- e2e/test/scenarios/native-filters/sql-field-filter-types.cy.spec.js
+++ e2e/test/scenarios/native-filters/sql-field-filter-types.cy.spec.js
@@ -95,7 +95,7 @@ describe("scenarios > filters > sql filters > field filter > Date", () => {
 
       cy.findByTestId("query-visualization-root").within(() => {
         // Scroll to ensure target element is rendered due to table virtualization
-        H.tableInteractiveScrollContainer().scrollTo(0, 300);
+        H.tableInteractiveScrollContainer().scrollTo(0, 800);
         cy.findByText(representativeResult);
       });
     });
@@ -120,7 +120,7 @@ describe("scenarios > filters > sql filters > field filter > Date", () => {
 
       cy.findByTestId("query-visualization-root").within(() => {
         // Scroll to ensure target element is rendered due to table virtualization
-        H.tableInteractiveScrollContainer().scrollTo(0, 300);
+        H.tableInteractiveScrollContainer().scrollTo(0, 800);
         cy.findByText(representativeResult);
       });
     });
