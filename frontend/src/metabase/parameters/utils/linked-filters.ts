b7ea645c347b93cf986ea29285bca95a96c11108
Fraser <frasermcintosh1@gmail.com>
2026-02-24 12:10:07 +1300

    Convert linked-filters to ts

--- frontend/src/metabase/parameters/utils/linked-filters.ts
+++ frontend/src/metabase/parameters/utils/linked-filters.ts
@@ -3,13 +3,14 @@ import {
   TYPE_SUPPORTS_LINKED_FILTERS,
 } from "metabase-lib/v1/parameters/constants";
 import { getParameterType } from "metabase-lib/v1/parameters/utils/parameter-type";
+import type { Parameter } from "metabase-types/api";
 
-export function canUseLinkedFilters(parameter) {
+export function canUseLinkedFilters(parameter: Parameter): boolean {
   const type = getParameterType(parameter);
-  return TYPE_SUPPORTS_LINKED_FILTERS.includes(type);
+  return (TYPE_SUPPORTS_LINKED_FILTERS as readonly string[]).includes(type);
 }
 
-export function usableAsLinkedFilter(parameter) {
+export function usableAsLinkedFilter(parameter: Parameter): boolean {
   const type = getParameterType(parameter);
   return FIELD_FILTER_PARAMETER_TYPES.includes(type);
 }
