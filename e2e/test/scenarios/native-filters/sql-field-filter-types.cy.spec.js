083853217ad2b12e191800f75504879701d2b6e3
Fraser <fraser@metabase.com>
2026-02-24 15:44:51 +1300

    Test sql field filter fix

--- e2e/test/scenarios/native-filters/sql-field-filter-types.cy.spec.js
+++ e2e/test/scenarios/native-filters/sql-field-filter-types.cy.spec.js
@@ -81,7 +81,7 @@ describe("scenarios > filters > sql filters > field filter > Date", () => {
     });
   });
 
-  it("when set through the filter widget", () => {
+  it.only("when set through the filter widget", () => {
     dateFilters.forEach(([subType, { value, representativeResult }]) => {
       cy.log(`Make sure it works for ${subType.toUpperCase()}`);
 
@@ -94,8 +94,6 @@ describe("scenarios > filters > sql filters > field filter > Date", () => {
       SQLFilter.runQuery();
 
       cy.findByTestId("query-visualization-root").within(() => {
-        // Scroll to ensure target element is rendered due to table virtualization
-        H.tableInteractiveScrollContainer().scrollTo(0, 300);
         cy.findByText(representativeResult);
       });
     });
