c531cdaa96426278a53fb9ab1f46cf915a29d077
Timothy Dean <timothy@timothydean.dev>
2026-02-24 14:40:42 -0500

    Remove new endpoints, use type everywhere

--- enterprise/backend/src/metabase_enterprise/transforms_python/api.clj
+++ enterprise/backend/src/metabase_enterprise/transforms_python/api.clj
@@ -1,6 +1,7 @@
 (ns metabase-enterprise.transforms-python.api
   (:require
    [clojure.string :as str]
+   [metabase-enterprise.transforms-javascript.models.javascript-library :as javascript-library]
    [metabase-enterprise.transforms-python.models.python-library :as python-library]
    [metabase-enterprise.transforms-python.python-runner :as python-runner]
    [metabase-enterprise.transforms-python.settings :as transforms-python.settings]
@@ -13,36 +14,48 @@
    [metabase.util.malli.schema :as ms]
    [toucan2.core :as t2]))
 
-(defn get-python-library-by-path
-  "Get Python library details by path for use by other APIs."
-  [path]
-  (-> (python-library/get-python-library-by-path path)
-      api/read-check
-      (select-keys [:source :path :created_at :updated_at])))
+(defn- javascript?
+  [type]
+  (= type "javascript"))
+
+(defn get-library-by-path
+  "Get library details by path. Type is \"python\" (default) or \"javascript\"."
+  [path type]
+  (let [get-fn (if (javascript? type)
+                 javascript-library/get-javascript-library-by-path
+                 python-library/get-python-library-by-path)]
+    (-> (get-fn path)
+        api/read-check
+        (select-keys [:source :path :created_at :updated_at]))))
 
 ;; TODO (Cam 2025-11-25) please add a response schema to this API endpoint, it makes it easier for our customers to
 ;; use our API + we will need it when we make auto-TypeScript-signature generation happen
 ;;
 #_{:clj-kondo/ignore [:metabase/validate-defendpoint-has-response-schema]}
 (api.macros/defendpoint :get "/library/:path"
-  "Get the Python library for user modules."
+  "Get the library for user modules. Pass `?type=javascript` for JavaScript, defaults to Python."
   [{:keys [path]} :- [:map [:path ms/NonBlankString]]
-   _query-params]
-  (get-python-library-by-path path))
+   {:keys [type]} :- [:map [:type {:optional true} [:maybe :string]]]]
+  (get-library-by-path path type))
 
 ;; TODO (Cam 2025-11-25) please add a response schema to this API endpoint, it makes it easier for our customers to
 ;; use our API + we will need it when we make auto-TypeScript-signature generation happen
 ;;
 #_{:clj-kondo/ignore [:metabase/validate-defendpoint-has-response-schema]}
 (api.macros/defendpoint :put "/library/:path"
-  "Update the Python library source code for user modules."
+  "Update the library source code for user modules. Pass `type` in the body for JavaScript."
   [{:keys [path]} :- [:map [:path ms/NonBlankString]]
    _query-params
-   body :- [:map {:closed true}
-            [:source :string]]]
+   body :- [:map
+            [:source :string]
+            [:type {:optional true} [:maybe :string]]]]
   ;; Check permission directly since this is an upsert endpoint - the library may not exist yet.
   (api/check-403 (perms/has-any-transforms-permission? api/*current-user-id*))
-  (python-library/update-python-library-source! path (:source body)))
+  (let [{:keys [source type]} body
+        update-fn (if (javascript? type)
+                    javascript-library/update-javascript-library-source!
+                    python-library/update-python-library-source!)]
+    (update-fn path source)))
 
 (api.macros/defendpoint :post "/test-run"
   :- [:map
@@ -51,11 +64,13 @@
       [:output {:optional true} [:map
                                  [:cols [:sequential [:map [:name :string]]]]
                                  [:rows [:sequential :any]]]]]
-  "Evaluate an ad-hoc python transform on a sample of input data.
+  "Evaluate an ad-hoc transform on a sample of input data.
+  Pass `type` as \"javascript\" to run JavaScript; defaults to \"python\".
   Intended for short runs for early feedback. Input/output/timeout limits apply."
   [_
    _
    {:keys [code
+           type
            source_tables
            output_row_limit
            per_input_row_limit]
@@ -63,6 +78,7 @@
            per_input_row_limit 100}}
    :- [:map
        [:code                                 :string]
+       [:type {:optional true} [:maybe :string]]
        [:source_tables                        [:map-of {:min 1} :string :int]]
        [:output_row_limit    {:optional true} [:and :int [:> 1] [:<= 100]]]
        [:per_input_row_limit {:optional true} [:and :int [:> 1] [:<= 100]]]]]
@@ -75,7 +91,8 @@
                  :source-tables   source_tables
                  :per-input-limit per_input_row_limit
                  :row-limit       output_row_limit
-                 :timeout-secs    (transforms-python.settings/python-runner-test-run-timeout-seconds)})
+                 :timeout-secs (transforms-python.settings/python-runner-test-run-timeout-seconds)
+                 :language (if (javascript? type) "javascript" "python")})
         logs   (str/join "\n" (map :message (:logs result)))]
     (if (= :succeeded (:status result))
       {:logs   logs
