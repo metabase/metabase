634dceaacaf2607aeb505fa808284b1163bc1984
Fraser <fraser@metabase.com>
2026-02-24 16:50:43 +1300

    Tidies

--- e2e/test/scenarios/native-filters/sql-field-filter-types.cy.spec.js
+++ e2e/test/scenarios/native-filters/sql-field-filter-types.cy.spec.js
@@ -94,6 +94,8 @@ describe("scenarios > filters > sql filters > field filter > Date", () => {
       SQLFilter.runQuery();
 
       cy.findByTestId("query-visualization-root").within(() => {
+        // Scroll to ensure target element is rendered due to table virtualization
+        H.tableInteractiveScrollContainer().scrollTo(0, 300);
         cy.findByText(representativeResult);
       });
     });
@@ -118,9 +120,7 @@ describe("scenarios > filters > sql filters > field filter > Date", () => {
 
       cy.findByTestId("query-visualization-root").within(() => {
         // Scroll to ensure target element is rendered due to table virtualization
-        cy.wait(500);
-        H.tableInteractiveScrollContainer().scrollTo(0, 800);
-        cy.wait(500);
+        H.tableInteractiveScrollContainer().scrollTo(0, 300);
         cy.findByText(representativeResult);
       });
     });
