9b29bcfe276ae1a6cd3c92b4188e5edde752f8b8
Eric Normand <ericwnormand@gmail.com>
2026-02-25 09:22:03 -0500

    Better order for joke-of-the-day

--- src/metabase/api_routes/routes.clj
+++ src/metabase/api_routes/routes.clj
@@ -184,9 +184,9 @@
    "/field"                (+auth metabase.warehouse-schema-rest.api/field-routes)
    "/frontend-errors"      'metabase.frontend-errors.api
    "/geojson"              'metabase.geojson.api
-   "/joke-of-the-day"      'metabase.joke-of-the-day.api
    "/glossary"             (+auth 'metabase.glossary.api)
    "/google"               (+auth metabase.sso.api/google-auth-routes)
+   "/joke-of-the-day"      'metabase.joke-of-the-day.api
    "/ldap"                 (+auth metabase.sso.api/ldap-routes)
    "/llm"                  (+auth metabase.llm.api/routes)
    "/logger"               (+auth 'metabase.logger.api)
