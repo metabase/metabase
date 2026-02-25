6f76fdc5530d7715df70dc0ea4dec88c841aba45
Jakub Chodorowicz <jakub.chodorowicz@metabase.com>
2026-02-25 11:02:17 +0100

    add old format compatibility test

--- test/metabase/models/transforms/transform_test.clj
+++ test/metabase/models/transforms/transform_test.clj
@@ -1,7 +1,8 @@
 (ns metabase.models.transforms.transform-test
   (:require
    [clojure.test :refer :all]
-   [metabase.test :as mt]))
+   [metabase.test :as mt]
+   [toucan2.core :as t2]))
 
 (deftest source-database-id-set-test
   (testing "inserting a transform correctly sets the source-database-id column"
@@ -22,3 +23,29 @@
                                      :type     "native"
                                      :native   {:query "SELECT 1"}}}}]
       (is (= (mt/id) (:source_database_id transform))))))
+
+(deftest source-tables-backward-compat-test
+  (testing "source-tables stored in old map format is converted to array format on read"
+    (mt/with-temp [:model/Transform {transform-id :id}
+                   {:name   "Python Transform"
+                    :source {:type             "python"
+                             :body             "print('hello')"
+                             :source-database  (mt/id)
+                             :source-tables    [{:alias "orders" :table (mt/id :orders)}
+                                                {:alias "products" :table (mt/id :products)}]}}]
+      ;; Simulate old map format by writing directly to DB
+      (t2/update! :model/Transform transform-id
+                  {:source {:type             "python"
+                            :body             "print('hello')"
+                            :source-database  (mt/id)
+                            :source-tables    {"orders" (mt/id :orders)
+                                               "products" (mt/id :products)}}})
+      (let [transform (t2/select-one :model/Transform transform-id)
+            source-tables (get-in transform [:source :source-tables])]
+        (testing "result is a vector, not a map"
+          (is (vector? source-tables)))
+        (testing "each entry has :alias and :table keys"
+          (is (every? #(and (contains? % :alias) (contains? % :table)) source-tables)))
+        (testing "all aliases and table IDs are present"
+          (is (= #{"orders" "products"} (into #{} (map :alias) source-tables)))
+          (is (= #{(mt/id :orders) (mt/id :products)} (into #{} (map :table) source-tables))))))))
