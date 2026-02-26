fca667ce4081d479c6189e9e58d980db41268d87
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-02-25 22:10:37 -0500

    wip be

--- test/metabase/lib_be/source_swap_test.clj
+++ test/metabase/lib_be/source_swap_test.clj
@@ -5,18 +5,26 @@
    [metabase.lib.core :as lib]
    [metabase.lib.test-metadata :as meta]))
 
+(defn- orders-source []
+  {:type :table, :id (meta/id :orders)})
+
+(defn- products-source []
+  {:type :table, :id (meta/id :products)})
+
 (defn- products->orders-field-id-mapping []
   {(meta/id :products :id) (meta/id :orders :id)})
 
 (deftest ^:parallel swap-source-in-query-test
-  (let [query (lib/query meta/metadata-provider (meta/table-metadata :products))]
-    (is (=? {:stages [{:source-table (meta/id :orders)}]}
-            (lib-be/swap-source-in-query query
-                                         {:type :table, :id (meta/id :products)}
-                                         {:type :table, :id (meta/id :orders)}
-                                         (products->orders-field-id-mapping))))))
+  (testing "should swap :source-table"
+    (let [query (lib/query meta/metadata-provider (meta/table-metadata :products))]
+      (is (=? {:stages [{:source-table (meta/id :orders)}]}
+              (lib-be/swap-source-in-query query
+                                           (products-source)
+                                           (orders-source)
+                                           (products->orders-field-id-mapping)))))))
 
 (deftest ^:parallel swap-source-in-parameter-target-test
-  (is (= [:dimension [:field (meta/id :orders :id) nil]]
-         (lib-be/swap-source-in-parameter-target [:dimension [:field (meta/id :products :id) nil]]
-                                                 (products->orders-field-id-mapping)))))
+  (testing "should swap the field id"
+    (is (= [:dimension [:field (meta/id :orders :id) nil]]
+           (lib-be/swap-source-in-parameter-target [:dimension [:field (meta/id :products :id) nil]]
+                                                   (products->orders-field-id-mapping))))))
