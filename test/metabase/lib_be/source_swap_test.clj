11f5ecbcd041f9913d52cc9a12327838ccd2eb02
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-02-25 22:49:06 -0500

    wip be

--- test/metabase/lib_be/source_swap_test.clj
+++ test/metabase/lib_be/source_swap_test.clj
@@ -7,78 +7,90 @@
    [metabase.lib.test-metadata :as meta]
    [metabase.lib.test-util :as lib.tu]))
 
+(deftest ^:parallel upgrade-field-refs-in-query-noop-test
+  (testing "should return the identical query when no refs are upgraded"
+    (let [query (-> (lib/query meta/metadata-provider (meta/table-metadata :orders))
+                    (lib/with-fields [(meta/field-metadata :orders :id)
+                                      (meta/field-metadata :orders :total)
+                                      (meta/field-metadata :orders :created-at)]))]
+      (is (= query
+             (lib-be/upgrade-field-refs-in-query query))))))
+
 (deftest ^:parallel upgrade-field-refs-in-query-source-table-test
-  (let [query (-> (lib/query meta/metadata-provider (meta/table-metadata :orders))
-                  (lib/with-fields [(meta/field-metadata :orders :id)
-                                    (meta/field-metadata :orders :total)
-                                    (meta/field-metadata :orders :created-at)])
-                  (lib/join (lib/join-clause (meta/table-metadata :products)))
-                  (lib/expression "double-tax" (lib/* (meta/field-metadata :orders :tax) 2))
-                  (lib/filter (lib/> (meta/field-metadata :orders :total) 10))
-                  (lib/aggregate (lib/sum (meta/field-metadata :orders :total)))
-                  (lib/breakout (meta/field-metadata :orders :product-id))
-                  (lib/order-by (meta/field-metadata :orders :product-id))
-                  (lib/append-stage)
-                  (lib/filter (lib/> (meta/field-metadata :orders :product-id) 5)))]
-    (is (=? {:stages [{:source-table (meta/id :orders)
-                       :fields       [[:field {} (meta/id :orders :id)]
-                                      [:field {} (meta/id :orders :total)]
-                                      [:field {} (meta/id :orders :created-at)]
-                                      [:expression {} "double-tax"]]
-                       :joins        [{:stages     [{:source-table (meta/id :products)}]
-                                       :conditions [[:= {}
-                                                     [:field {} (meta/id :orders :product-id)]
-                                                     [:field {} (meta/id :products :id)]]]}]
-                       :expressions  [[:* {:lib/expression-name "double-tax"} [:field {} (meta/id :orders :tax)] 2]]
-                       :filters      [[:> {} [:field {} (meta/id :orders :total)] 10]]
-                       :aggregation  [[:sum {} [:field {} (meta/id :orders :total)]]]
-                       :breakout     [[:field {} (meta/id :orders :product-id)]]
-                       :order-by     [[:asc {} [:field {} (meta/id :orders :product-id)]]]}
-                      {:filters [[:> {} [:field {} "PRODUCT_ID"] 5]]}]}
-            (lib-be/upgrade-field-refs-in-query query)))))
+  (testing "should preserve field id refs in the same stage as the table, but upgrade next stages"
+    (let [query (-> (lib/query meta/metadata-provider (meta/table-metadata :orders))
+                    (lib/with-fields [(meta/field-metadata :orders :id)
+                                      (meta/field-metadata :orders :total)
+                                      (meta/field-metadata :orders :created-at)])
+                    (lib/join (lib/join-clause (meta/table-metadata :products)))
+                    (lib/expression "double-tax" (lib/* (meta/field-metadata :orders :tax) 2))
+                    (lib/filter (lib/> (meta/field-metadata :orders :total) 10))
+                    (lib/aggregate (lib/sum (meta/field-metadata :orders :total)))
+                    (lib/breakout (meta/field-metadata :orders :product-id))
+                    (lib/order-by (meta/field-metadata :orders :product-id))
+                    (lib/append-stage)
+                    (lib/filter (lib/> (meta/field-metadata :orders :product-id) 5)))]
+      (is (=? {:stages [{:source-table (meta/id :orders)
+                         :fields       [[:field {} (meta/id :orders :id)]
+                                        [:field {} (meta/id :orders :total)]
+                                        [:field {} (meta/id :orders :created-at)]
+                                        [:expression {} "double-tax"]]
+                         :joins        [{:stages     [{:source-table (meta/id :products)}]
+                                         :conditions [[:= {}
+                                                       [:field {} (meta/id :orders :product-id)]
+                                                       [:field {} (meta/id :products :id)]]]}]
+                         :expressions  [[:* {:lib/expression-name "double-tax"} [:field {} (meta/id :orders :tax)] 2]]
+                         :filters      [[:> {} [:field {} (meta/id :orders :total)] 10]]
+                         :aggregation  [[:sum {} [:field {} (meta/id :orders :total)]]]
+                         :breakout     [[:field {} (meta/id :orders :product-id)]]
+                         :order-by     [[:asc {} [:field {} (meta/id :orders :product-id)]]]}
+                        {:filters [[:> {} [:field {} "PRODUCT_ID"] 5]]}]}
+              (lib-be/upgrade-field-refs-in-query query))))))
 
 (deftest ^:parallel upgrade-field-refs-in-query-source-card-test
-  (let [base-query (lib/query meta/metadata-provider (meta/table-metadata :orders))
-        mp         (lib.tu/metadata-provider-with-card-from-query 1 base-query)
-        query      (-> (lib/query mp (lib.metadata/card mp 1))
-                       (lib/with-fields [(meta/field-metadata :orders :id)
-                                         (meta/field-metadata :orders :total)
-                                         (meta/field-metadata :orders :created-at)])
-                       (lib/join (lib/join-clause (meta/table-metadata :products)))
-                       (lib/expression "double-tax" (lib/* (meta/field-metadata :orders :tax) 2))
-                       (lib/filter (lib/> (meta/field-metadata :orders :total) 10))
-                       (lib/aggregate (lib/sum (meta/field-metadata :orders :total)))
-                       (lib/breakout (meta/field-metadata :orders :product-id))
-                       (lib/order-by (meta/field-metadata :orders :product-id))
-                       (lib/append-stage)
-                       (lib/filter (lib/> (meta/field-metadata :orders :product-id) 5)))]
-    (is (=? {:stages [{:source-card  1
-                       :fields       [[:field {} "ID"]
-                                      [:field {} "TOTAL"]
-                                      [:field {} "CREATED_AT"]
-                                      [:expression {} "double-tax"]]
-                       :joins        [{:stages     [{:source-table (meta/id :products)}]
-                                       :conditions [[:= {}
-                                                     [:field {} "PRODUCT_ID"]
-                                                     [:field {} (meta/id :products :id)]]]}]
-                       :expressions  [[:* {:lib/expression-name "double-tax"} [:field {} "TAX"] 2]]
-                       :filters      [[:> {} [:field {} "TOTAL"] 10]]
-                       :aggregation  [[:sum {} [:field {} "TOTAL"]]]
-                       :breakout     [[:field {} "PRODUCT_ID"]]
-                       :order-by     [[:asc {} [:field {} "PRODUCT_ID"]]]}
-                      {:filters [[:> {} [:field {} "PRODUCT_ID"] 5]]}]}
-            (lib-be/upgrade-field-refs-in-query query)))))
+  (testing "should upgrade field id refs for a card"
+    (let [base-query (lib/query meta/metadata-provider (meta/table-metadata :orders))
+          mp         (lib.tu/metadata-provider-with-card-from-query 1 base-query)
+          query      (-> (lib/query mp (lib.metadata/card mp 1))
+                         (lib/with-fields [(meta/field-metadata :orders :id)
+                                           (meta/field-metadata :orders :total)
+                                           (meta/field-metadata :orders :created-at)])
+                         (lib/join (lib/join-clause (meta/table-metadata :products)))
+                         (lib/expression "double-tax" (lib/* (meta/field-metadata :orders :tax) 2))
+                         (lib/filter (lib/> (meta/field-metadata :orders :total) 10))
+                         (lib/aggregate (lib/sum (meta/field-metadata :orders :total)))
+                         (lib/breakout (meta/field-metadata :orders :product-id))
+                         (lib/order-by (meta/field-metadata :orders :product-id))
+                         (lib/append-stage)
+                         (lib/filter (lib/> (meta/field-metadata :orders :product-id) 5)))]
+      (is (=? {:stages [{:source-card  1
+                         :fields       [[:field {} "ID"]
+                                        [:field {} "TOTAL"]
+                                        [:field {} "CREATED_AT"]
+                                        [:expression {} "double-tax"]]
+                         :joins        [{:stages     [{:source-table (meta/id :products)}]
+                                         :conditions [[:= {}
+                                                       [:field {} "PRODUCT_ID"]
+                                                       [:field {} (meta/id :products :id)]]]}]
+                         :expressions  [[:* {:lib/expression-name "double-tax"} [:field {} "TAX"] 2]]
+                         :filters      [[:> {} [:field {} "TOTAL"] 10]]
+                         :aggregation  [[:sum {} [:field {} "TOTAL"]]]
+                         :breakout     [[:field {} "PRODUCT_ID"]]
+                         :order-by     [[:asc {} [:field {} "PRODUCT_ID"]]]}
+                        {:filters [[:> {} [:field {} "PRODUCT_ID"] 5]]}]}
+              (lib-be/upgrade-field-refs-in-query query))))))
 
 (deftest ^:parallel upgrade-field-refs-in-query-join-card-test
-  (let [products-query (lib/query meta/metadata-provider (meta/table-metadata :products))
-        mp             (lib.tu/metadata-provider-with-card-from-query 1 products-query)
-        query          (-> (lib/query mp (meta/table-metadata :orders))
-                           (lib/join (lib/join-clause (lib.metadata/card mp 1)
-                                                      [(lib/= (meta/field-metadata :orders :product-id)
-                                                              (meta/field-metadata :products :id))])))]
-    (is (=? {:stages [{:source-table (meta/id :orders)
-                       :joins        [{:stages     [{:source-card 1}]
-                                       :conditions [[:= {}
-                                                     [:field {} (meta/id :orders :product-id)]
-                                                     [:field {} "ID"]]]}]}]}
-            (lib-be/upgrade-field-refs-in-query query)))))
+  (testing "should upgrade field id refs for a joined card"
+    (let [products-query (lib/query meta/metadata-provider (meta/table-metadata :products))
+          mp             (lib.tu/metadata-provider-with-card-from-query 1 products-query)
+          query          (-> (lib/query mp (meta/table-metadata :orders))
+                             (lib/join (lib/join-clause (lib.metadata/card mp 1)
+                                                        [(lib/= (meta/field-metadata :orders :product-id)
+                                                                (meta/field-metadata :products :id))])))]
+      (is (=? {:stages [{:source-table (meta/id :orders)
+                         :joins        [{:stages     [{:source-card 1}]
+                                         :conditions [[:= {}
+                                                       [:field {} (meta/id :orders :product-id)]
+                                                       [:field {} "ID"]]]}]}]}
+              (lib-be/upgrade-field-refs-in-query query))))))
