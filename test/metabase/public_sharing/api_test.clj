525f7ff65785cf60b9e3375d44c24ec81ba0b95c
Noah Moss <noahbmoss@gmail.com>
2026-02-26 13:43:51 -0500

    Backport #70153 to 56

--- test/metabase/public_sharing/api_test.clj
+++ test/metabase/public_sharing/api_test.clj
@@ -582,7 +582,44 @@
               (client/client :get 200 (str "public/card/" uuid "/query/csv"), :format :csv)
               (is (= :public-csv-download (:context (qe)))))))))))
 
+(deftest card-endpoints-require-public-sharing-enabled-test
+  (testing "Card endpoints return 400 when public sharing is disabled"
+    (mt/with-temporary-setting-values [enable-public-sharing true]
+      (with-temp-public-card [{uuid :public_uuid}]
+        (let [endpoints [[:get (format "public/card/%s" uuid)]
+                         [:get (format "public/card/%s/query" uuid)]
+                         [:get (format "public/card/%s/query/json" uuid)]
+                         [:get (format "public/card/%s/params/_STATIC_CATEGORY_/values" uuid)]
+                         [:get (format "public/card/%s/params/_STATIC_CATEGORY_/search/A" uuid)]
+                         [:get (format "public/card/%s/params/_STATIC_CATEGORY_/remapping" uuid)
+                          :value "foo"]]]
+          (mt/with-temporary-setting-values [enable-public-sharing false]
+            (doseq [[method url & kvs] endpoints]
+              (testing (str (name method) " " url)
+                (is (= "An error occurred."
+                       (apply client/client method 400 url kvs)))))))))))
+
 ;;; ---------------------------------------- GET /api/public/dashboard/:uuid -----------------------------------------
+
+(deftest dashboard-endpoints-require-public-sharing-enabled-test
+  (testing "Dashboard endpoints return 400 when public sharing is disabled"
+    (mt/with-temporary-setting-values [enable-public-sharing true]
+      (with-temp-public-dashboard-and-card [dash card dashcard]
+        (let [dash-uuid   (:public_uuid dash)
+              dashcard-id (u/the-id dashcard)
+              card-id     (u/the-id card)
+              endpoints   [[:get  (format "public/dashboard/%s" dash-uuid)]
+                           [:get  (format "public/dashboard/%s/dashcard/%d/card/%d" dash-uuid dashcard-id card-id)]
+                           [:post (format "public/dashboard/%s/dashcard/%d/card/%d/json" dash-uuid dashcard-id card-id)]
+                           [:get  (format "public/dashboard/%s/params/_VENUE_ID_/values" dash-uuid)]
+                           [:get  (format "public/dashboard/%s/params/_VENUE_ID_/search/foo" dash-uuid)]
+                           [:get  (format "public/dashboard/%s/params/_VENUE_ID_/remapping" dash-uuid)
+                            :value "1"]]]
+          (mt/with-temporary-setting-values [enable-public-sharing false]
+            (doseq [[method url & kvs] endpoints]
+              (testing (str (name method) " " url)
+                (is (= "An error occurred."
+                       (apply client/client method 400 url kvs)))))))))))
 
 (deftest get-public-dashboard-errors-test
   (testing "GET /api/public/dashboard/:uuid"
@@ -1720,6 +1757,7 @@
 
 (deftest oembed-test
   (testing "GET /oembed"
-    (let [response (client/client :get 200 "public/oembed?url=path/to/url&format=json")]
-      (is (= "1.0" (:version response)))
-      (is (= "rich" (:type response))))))
+    (mt/with-temporary-setting-values [enable-public-sharing true]
+      (let [response (client/client :get 200 "public/oembed?url=path/to/url&format=json")]
+        (is (= "1.0" (:version response)))
+        (is (= "rich" (:type response)))))))
