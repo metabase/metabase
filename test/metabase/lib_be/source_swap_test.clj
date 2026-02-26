2c5b2cd57018ea7576e87f4c7ebfc760724a139e
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-02-25 21:42:13 -0500

    wip be

--- test/metabase/lib_be/source_swap_test.clj
+++ test/metabase/lib_be/source_swap_test.clj
@@ -1,64 +1,46 @@
 (ns metabase.lib-be.source-swap-test
   (:require
    [clojure.test :refer :all]
-   [metabase.lib-be.source-swap :as source-swap]
-   [metabase.lib.core :as lib]
-   [metabase.lib.test-metadata :as meta]
+   [metabase.lib-be.core :as lib-be]
    [metabase.test]))
 
-(defn- orders-source []
-  {:type :table :id (meta/id :orders)})
-
-(defn- products-source []
-  {:type :table :id (meta/id :products)})
-
-(defn- orders->products-field-id-mapping []
-  {(meta/id :orders :id)         (meta/id :products :id)
-   (meta/id :orders :created-at) (meta/id :products :created-at)})
-
-(deftest ^:parallel build-swap-field-id-mapping-table-to-table-test
-  (testing "should build a map of shared fields between orders and products"
-    (let [query (lib/query meta/metadata-provider (meta/table-metadata :orders))]
-      (is (= (orders->products-field-id-mapping)
-             (source-swap/build-swap-field-id-mapping query (orders-source) (products-source)))))))
-
-(deftest ^:parallel swap-source-in-query-table-to-table-test
-  (testing "should replace source table and remap field IDs in all clauses"
-    (let [query (-> (lib/query meta/metadata-provider (meta/table-metadata :orders))
-                    (lib/with-fields [(meta/field-metadata :orders :id)
-                                      (meta/field-metadata :orders :created-at)])
-                    (lib/join (-> (lib/join-clause (meta/table-metadata :people)
-                                                   [(lib/= (meta/field-metadata :orders :id)
-                                                           (meta/field-metadata :people :id))])
-                                  (lib/with-join-alias "People")))
-                    (lib/expression "id-plus-one" (lib/+ (meta/field-metadata :orders :id) 1))
-                    (lib/filter (lib/> (meta/field-metadata :orders :id) 0))
-                    (lib/aggregate (lib/min (meta/field-metadata :orders :created-at)))
-                    (lib/breakout (meta/field-metadata :orders :created-at))
-                    (lib/order-by (meta/field-metadata :orders :id)))]
-      (is (=? {:stages [{:source-table (meta/id :products)
-                         :fields       [[:field {} (meta/id :products :id)]
-                                        [:field {} (meta/id :products :created-at)]
-                                        [:expression {} "id-plus-one"]]
-                         :joins        [{:alias      "People"
-                                         :stages     [{:source-table (meta/id :people)}]
-                                         :conditions [[:= {}
-                                                       [:field {} (meta/id :products :id)]
-                                                       [:field {:join-alias "People"} (meta/id :people :id)]]]}]
-                         :expressions  [[:+ {:lib/expression-name "id-plus-one"}
-                                         [:field {} (meta/id :products :id)] 1]]
-                         :filters      [[:> {} [:field {} (meta/id :products :id)] 0]]
-                         :aggregation  [[:min {} [:field {} (meta/id :products :created-at)]]]
-                         :breakout     [[:field {} (meta/id :products :created-at)]]
-                         :order-by     [[:asc {} [:field {} (meta/id :products :id)]]]}]}
-              (source-swap/swap-source-in-query query
-                                                (orders-source)
-                                                (products-source)
-                                                (orders->products-field-id-mapping)))))))
-
 (deftest ^:parallel swap-source-in-parameter-target-test
-  (testing "should swap field ID in a dimension target"
-    (is (= [:dimension [:field (meta/id :products :id) nil]]
-           (source-swap/swap-source-in-parameter-target
-            [:dimension [:field (meta/id :orders :id) nil]]
-            (orders->products-field-id-mapping))))))
+  (let [field-id-mapping {1 2}]
+    (are [expected-target original-target]
+         (= expected-target
+            (lib-be/swap-source-in-parameter-target original-target field-id-mapping))
+      ;; regular field id ref
+      [:dimension [:field 2 nil]]
+      [:dimension [:field 1 nil]]
+
+      ;; regular field id ref with options
+      [:dimension [:field 2 {:base-type :type/BigInteger}]]
+      [:dimension [:field 1 {:base-type :type/BigInteger}]]
+
+      ;; field ref with dimension options
+      [:dimension [:field 2 nil] {:stage-number 0}]
+      [:dimension [:field 1 nil] {:stage-number 0}]
+
+      ;; regular field name ref
+      [:dimension [:field "ID" {:base-type :type/BigInteger}]]
+      [:dimension [:field "ID" {:base-type :type/BigInteger}]]
+
+      ;; explicit join
+      [:dimension [:field 2 {:join-alias "Orders"}]]
+      [:dimension [:field 1 {:join-alias "Orders"}]]
+
+      ;; implicit join - change the field id
+      [:dimension [:field 2 {:source-field 9}]]
+      [:dimension [:field 1 {:source-field 9}]]
+
+      ;; implicit join - change the source field id
+      [:dimension [:field 9 {:source-field 2}]]
+      [:dimension [:field 9 {:source-field 1}]]
+
+      ;; field filter template tag should be left unchanged
+      [:dimension [:template-tag "category"]]
+      [:dimension [:template-tag "category"]]
+
+      ;; variable template tag should be left unchanged
+      [:variable [:template-tag "category"]]
+      [:variable [:template-tag "category"]])))
