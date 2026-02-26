f263256782f4909728cb9ad8ffc39033703db16b
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-02-25 22:16:00 -0500

    wip be

--- test/metabase/lib_be/source_swap_test.clj
+++ test/metabase/lib_be/source_swap_test.clj
@@ -12,19 +12,30 @@
   {:type :table, :id (meta/id :products)})
 
 (defn- products->orders-field-id-mapping []
-  {(meta/id :products :id) (meta/id :orders :id)})
+  {(meta/id :products :id) (meta/id :orders :id)
+   (meta/id :products :created-at) (meta/id :orders :created-at)})
 
-(deftest ^:parallel swap-source-in-query-test
-  (testing "should swap :source-table"
-    (let [query (lib/query meta/metadata-provider (meta/table-metadata :products))]
-      (is (=? {:stages [{:source-table (meta/id :orders)}]}
-              (lib-be/swap-source-in-query query
-                                           (products-source)
-                                           (orders-source)
-                                           (products->orders-field-id-mapping)))))))
+(deftest ^:parallel swap-source-in-query-table-test
+  (let [query (lib/query meta/metadata-provider (meta/table-metadata :products))]
+    (is (=? {:stages [{:source-table (meta/id :orders)}]}
+            (lib-be/swap-source-in-query query
+                                         (products-source)
+                                         (orders-source)
+                                         (products->orders-field-id-mapping))))))
 
-(deftest ^:parallel swap-source-in-parameter-target-test
-  (testing "should swap the field id"
-    (is (= [:dimension [:field (meta/id :orders :id) nil]]
-           (lib-be/swap-source-in-parameter-target [:dimension [:field (meta/id :products :id) nil]]
-                                                   (products->orders-field-id-mapping))))))
+(deftest ^:parallel swap-source-in-query-fields-test
+  (let [query (-> (lib/query meta/metadata-provider (meta/table-metadata :products))
+                  (lib/with-fields [(meta/field-metadata :products :id)
+                                    (meta/field-metadata :products :created-at)]))]
+    (is (=? {:stages [{:source-table (meta/id :orders)
+                       :fields [[:field {} (meta/id :orders :id)]
+                                [:field {} (meta/id :orders :created-at)]]}]}
+            (lib-be/swap-source-in-query query
+                                         (products-source)
+                                         (orders-source)
+                                         (products->orders-field-id-mapping))))))
+
+(deftest ^:parallel swap-source-in-parameter-target-field-id-test
+  (is (= [:dimension [:field (meta/id :orders :id) nil]]
+         (lib-be/swap-source-in-parameter-target [:dimension [:field (meta/id :products :id) nil]]
+                                                 (products->orders-field-id-mapping)))))
