f1bee47073e134380bd19a4b60a46064d1020998
Timothy Dean <timothy@timothydean.dev>
2026-02-26 12:02:35 -0500

    Moving tests around

--- test/metabase/transforms/feature_gating_test.clj
+++ test/metabase/transforms/feature_gating_test.clj
@@ -1,19 +1,13 @@
 (ns metabase.transforms.feature-gating-test
-  "Tests for feature gating of polyglot transforms."
+  "Tests for feature gating predicates (OSS behavior).
+  Tests that depend on runner language registration live in
+  metabase-enterprise.transforms-runner.feature-gating-test."
   (:require
    [clojure.test :refer :all]
    [metabase.test :as mt]
-   [metabase.transforms.feature-gating :as transforms.gating]
-   [metabase.transforms.util :as transforms.util]))
+   [metabase.transforms.feature-gating :as transforms.gating]))
 
 (set! *warn-on-reflection* true)
-
-(def ^:private runner-languages
-  [:python :javascript :clojure])
-
-(defn- make-transform [source-type]
-  {:source {:type (name source-type)}
-   :target {:database 1 :type "table" :name "out" :schema "s"}})
 
 ;;; ---------------------------------------- Feature flag predicates ------------------------------------------------
 
@@ -54,19 +48,6 @@
   (mt/with-premium-features #{:transforms}
     (is (= #{"native" "mbql"} (transforms.gating/enabled-source-types)))))
 
-(deftest enabled-source-types-full-features-test
-  (mt/with-premium-features #{:transforms :transforms-python}
-    (let [types (transforms.gating/enabled-source-types)]
-      (testing "includes query types"
-        (is (contains? types "native"))
-        (is (contains? types "mbql")))
-      (testing "includes python"
-        (is (contains? types "python")))
-      (testing "includes all runner languages"
-        (doseq [lang (transforms.gating/runner-language-types)]
-          (testing (str "includes " lang)
-            (is (contains? types lang))))))))
-
 ;;; ---------------------------------------- any-transforms-enabled? -----------------------------------------------
 
 (deftest any-transforms-enabled-test
@@ -76,24 +57,3 @@
   (mt/with-premium-features #{:transforms :transforms-python}
     (is (true? (transforms.gating/any-transforms-enabled?)))))
 
-;;; ---------------------------------------- check-feature-enabled -------------------------------------------------
-
-(deftest check-feature-enabled-full-features-test
-  (mt/with-premium-features #{:transforms :transforms-python}
-    (testing "query transform enabled"
-      (is (true? (transforms.util/check-feature-enabled (make-transform :query)))))
-    (testing "all runner languages enabled"
-      (doseq [lang runner-languages]
-        (testing (str "language: " lang)
-          (is (true? (transforms.util/check-feature-enabled (make-transform lang)))))))))
-
-(deftest check-feature-enabled-no-python-feature-test
-  (mt/with-premium-features #{:transforms}
-    (testing "query transform still enabled"
-      (is (true? (transforms.util/check-feature-enabled (make-transform :query)))))
-    (testing "python disabled without :transforms-python"
-      (is (false? (transforms.util/check-feature-enabled (make-transform :python)))))
-    (testing "other runner languages disabled without :transforms-python"
-      (doseq [lang [:javascript :clojure]]
-        (testing (str "language: " lang)
-          (is (false? (transforms.util/check-feature-enabled (make-transform lang)))))))))
