3ca4195a512b1e0697fb7f80172c49c86d2ee2fb
Romeo Van Snick <romeo@romeovansnick.be>
2026-02-27 12:43:26 +1100

    Add helper to visit card

--- e2e/support/helpers/e2e-card-helpers.ts
+++ e2e/support/helpers/e2e-card-helpers.ts
@@ -6,6 +6,7 @@ import type {
 
 import { createTestNativeQuery, createTestQuery } from "./api";
 import { type CardDetails as CardDetails_, createCard } from "./api/createCard";
+import { visitMetric, visitModel, visitQuestion } from "./e2e-misc-helpers";
 
 type CardDetails<T> = Omit<CardDetails_, "dataset_query"> & {
   dataset_query: T;
@@ -25,4 +26,15 @@ export function createCardWithTestNativeQuery(
   return createTestNativeQuery(details.dataset_query).then((dataset_query) =>
     createCard({ ...details, dataset_query }),
   );
+}
+
+export function visitCard(card: Pick<Card, "id" | "type">) {
+  switch (card.type) {
+    case "question":
+      return visitQuestion(card.id);
+    case "model":
+      return visitModel(card.id);
+    case "metric":
+      return visitMetric(card.id);
+  }
 }
