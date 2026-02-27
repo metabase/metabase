f33d9721e8430e5106160e8e9c53f50e11802855
Romeo Van Snick <romeo@romeovansnick.be>
2026-02-27 15:52:03 +1100

    Add helpers to visit ad hoc cards

--- e2e/support/helpers/e2e-card-helpers.ts
+++ e2e/support/helpers/e2e-card-helpers.ts
@@ -6,6 +6,7 @@ import type {
 
 import { createTestNativeQuery, createTestQuery } from "./api";
 import { type CardDetails as CardDetails_, createCard } from "./api/createCard";
+import { visitQuestionAdhoc } from "./e2e-ad-hoc-question-helpers";
 import { visitMetric, visitModel, visitQuestion } from "./e2e-misc-helpers";
 
 type CardDetails<T> = Omit<CardDetails_, "dataset_query"> & {
@@ -37,4 +38,41 @@ export function visitCard(card: Pick<Card, "id" | "type">) {
     case "metric":
       return visitMetric(card.id);
   }
+}
+
+export function visitAdHocQuestionWithTestQuery(
+  details: CardDetails<TestQuerySpecWithDatabase>,
+  options: {
+    mode?: "notebook";
+    skipWaiting?: boolean;
+  } = {},
+) {
+  return H.createTestQuery(details.dataset_query).then((dataset_query) => {
+    const question: Card = {
+      display: "table" as const,
+      ...details,
+      dataset_query,
+    };
+    return visitQuestionAdhoc(question, options);
+  });
+}
+
+export function visitAdHocQuestionWithTestNativeQuery(
+  details: CardDetails<TestNativeQuerySpecWithDatabase>,
+  options: {
+    mode?: "notebook";
+    autorun?: boolean;
+    skipWaiting?: boolean;
+  } = {},
+) {
+  return H.createTestNativeQuery(details.dataset_query).then(
+    (dataset_query) => {
+      const question: Card = {
+        display: "table" as const,
+        ...details,
+        dataset_query,
+      };
+      return visitQuestionAdhoc(question, options);
+    },
+  );
 }
