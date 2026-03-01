38e95080bbc4a5cb12c04c7a1644e339f8460563
Nicolò Pretto <info@npretto.com>
2026-03-01 17:05:39 +0100

    clean up consts

--- frontend/build/embedding-sdk/constants/sdk-bundle.js
+++ frontend/build/embedding-sdk/constants/sdk-bundle.js
@@ -4,17 +4,11 @@ module.exports.SDK_BUNDLE_PATH = SDK_BUNDLE_PATH;
 const SDK_BUNDLE_LEGACY_PATH = `${SDK_BUNDLE_PATH}/legacy`;
 module.exports.SDK_BUNDLE_LEGACY_PATH = SDK_BUNDLE_LEGACY_PATH;
 
-const SDK_BUNDLE_CHUNKS_PATH = `${SDK_BUNDLE_PATH}/chunks`;
-module.exports.SDK_BUNDLE_CHUNKS_PATH = SDK_BUNDLE_CHUNKS_PATH;
-
 const SDK_BUNDLE_FILENAME = "embedding-sdk.js";
-module.exports.SDK_BUNDLE_FILENAME = SDK_BUNDLE_FILENAME;
+module.exports.SDK_BUNDLE_FILENAME = "embedding-sdk.js";
 
 // Single URL used by the NPM package for all scenarios.
 // The backend decides what to serve based on the packageVersion query param.
 module.exports.SDK_BUNDLE_FULL_PATH = `app/${SDK_BUNDLE_FILENAME}`;
 
-const SDK_BUNDLE_BOOTSTRAP_FILENAME = "embedding-sdk.js";
-module.exports.SDK_BUNDLE_BOOTSTRAP_FILENAME = SDK_BUNDLE_BOOTSTRAP_FILENAME;
-
-module.exports.SDK_BUNDLE_BOOTSTRAP_FULL_PATH = `${SDK_BUNDLE_CHUNKS_PATH}/${SDK_BUNDLE_BOOTSTRAP_FILENAME}`;
+module.exports.SDK_BUNDLE_BOOTSTRAP_FILENAME = "embedding-sdk.js";
