444f3c6fa321598ba9e2d56d10fa89c8b9e1bbfd
lbrdnk <lbrdnk@users.noreply.github.com>
2026-03-03 16:37:16 +0100

    Add metabot-v3.tools.sql.common

---
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/tools/sql/common.clj
@@ -0,0 +1,35 @@
+(ns metabase-enterprise.metabot-v3.tools.sql.common
+  "Namespace that aggregates functionality common to tools sql namespaces.
+
+  Those are:
+  - `metabot-v3.tools.create-sql-query`,
+  - `metabot-v3.tools.edit-sql-query`,
+  - `metabot-v3.tools.replace-sql-query`.
+
+  Each of those namespaces define an _operation_:
+  - `create-sql-query`,
+  - `edit-sql-query`,
+  - `replace-sql-query`."
+  (:require
+   [metabase-enterprise.metabot-v3.tools.sql-validation :as metabot-v3.tools.sql-validation]
+   [metabase.util.malli.registry :as mr]))
+
+(mr/def ::action-result
+  "Each of the _opeartions_ performs an _action_ manipulating a query.
+  Key of the action result represent
+  - query-id :: id of a query stored in the context or memory,
+  - query-content :: sql that is a result of an action,
+  - query :: query map that wrapps the `query-content`,
+  - database :: id of the database that query belongs to."
+  [:map
+   [:query-id :any]
+   [:query-content :any]
+   [:query :any]
+   [:database :any]])
+
+(mr/def ::operation-result
+  "Result of an operation as described this ns' docstring. Stores validation result and action result iff validation
+  was successful."
+  [:map
+   [:validation-result ::metabot-v3.tools.sql-validation/validation-result]
+   [:action-result {:optional true} ::action-result]])
