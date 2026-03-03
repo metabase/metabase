8737fc5e1adcd4240582a0256261428dbe6f20b2
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-03-02 22:06:27 -0500

    fix

--- enterprise/backend/src/metabase_enterprise/writable_connection/core.clj
+++ enterprise/backend/src/metabase_enterprise/writable_connection/core.clj
@@ -4,7 +4,7 @@
 
 (defenterprise database-write-data-details
   "Returns the `:write-data-details` for a database when the `:writable-connection` feature is available.
-  
+
   `database` is a [[lib/metadata]] database instance. To pass a Toucan2 database instance, use [[driver.u/ensure-lib-database]]."
   :feature :writable-connection
   [database]
