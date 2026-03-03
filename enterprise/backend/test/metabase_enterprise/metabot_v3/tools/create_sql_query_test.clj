eb04cb0d5878094960c3e8c7ae528cb4d6b5f13e
lbrdnk <lbrdnk@users.noreply.github.com>
2026-03-03 14:58:23 +0100

    Add create-sql-query-validation-failure-test

--- enterprise/backend/test/metabase_enterprise/metabot_v3/tools/create_sql_query_test.clj
+++ enterprise/backend/test/metabase_enterprise/metabot_v3/tools/create_sql_query_test.clj
@@ -1,7 +1,9 @@
 (ns metabase-enterprise.metabot-v3.tools.create-sql-query-test
   (:require
+   [clojure.string :as str]
    [clojure.test :refer :all]
    [metabase-enterprise.metabot-v3.tools.create-sql-query :as create-sql-query]
+   [metabase.driver :as driver]
    [metabase.test :as mt]))
 
 (deftest create-sql-query-test
@@ -21,3 +23,19 @@
                     :type :native
                     :native {:query sql}}
                    (:query result)))))))))
+
+(deftest create-sql-query-validation-failure-test
+  (mt/test-drivers #{:postgres}
+    (mt/with-temp [:model/Database {db-id :id} {:engine driver/*driver*}]
+      (testing "attempts to create query with wrong syntax"
+        (mt/with-current-user (mt/user->id :crowberto)
+          (let [sql "select 123abc ="
+
+                {:keys [valid? dialect error-message]}
+                (create-sql-query/create-sql-query
+                 {:database-id db-id
+                  :sql sql
+                  :name "Test Query"})]
+            (is (= "postgres" dialect))
+            (is (false? valid?))
+            (is (str/starts-with? error-message "Invalid expression / Unexpected token."))))))))
