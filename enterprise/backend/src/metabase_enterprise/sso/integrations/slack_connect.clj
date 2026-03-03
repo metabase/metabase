b83b144e0a2af1ed3ae34b78f4aa7d4ffbd3cf0a
Noah Moss <noahbmoss@gmail.com>
2026-03-03 15:38:03 -0500

    Slack SSO info logs in initiate & callback endpoints

--- enterprise/backend/src/metabase_enterprise/sso/integrations/slack_connect.clj
+++ enterprise/backend/src/metabase_enterprise/sso/integrations/slack_connect.clj
@@ -43,6 +43,11 @@
   "Initiate Slack Connect SSO flow. Redirects to Slack authorization endpoint."
   [request]
   (check-slack-connect-prereqs!)
+  (log/infof "Slack Connect SSO initiate: auth-mode=%s, has-session-cookie=%s, current-user-id=%s, redirect-uri=%s"
+             (sso-settings/slack-connect-authentication-mode)
+             (boolean (get-in request [:cookies request/metabase-session-cookie :value]))
+             api/*current-user-id*
+             (slack-redirect-uri))
   (let [{:keys [redirect]} (:params request)
         _ (when (and (= "link-only" (sso-settings/slack-connect-authentication-mode))
                      (not api/*current-user-id*))
@@ -56,6 +61,10 @@
                                                        :authenticated-user api/*current-user*
                                                        :redirect-uri (slack-redirect-uri)
                                                        :final-redirect redirect-url))]
+    (log/infof "Slack Connect SSO initiate auth result: success=%s, has-redirect-url=%s, browser-id=%s"
+               (:success? auth-result)
+               (boolean (:redirect-url auth-result))
+               (:browser-id request))
     (cond
       (= :redirect (:success? auth-result))
       (sso/wrap-oidc-redirect auth-result
@@ -71,29 +80,42 @@
   "Handle Slack Connect OIDC callback with authorization code."
   [request]
   (check-slack-connect-prereqs!)
-  (let [{:keys [code state]} (:params request)
-        login-result (auth-identity/login! :provider/slack-connect
-                                           (assoc request
-                                                  :authenticated-user api/*current-user*
-                                                  :code code
-                                                  :state state
-                                                  :oidc-provider :slack-connect
-                                                  :redirect-uri (slack-redirect-uri)
-                                                  :device-info (request/device-info request)))]
-    (cond
-      (:success? login-result)
-      (let [final-redirect (or (:redirect-url login-result) "/")
-            base-response (-> (response/redirect final-redirect)
-                              (sso/clear-oidc-state-cookie))]
-        (log/infof "Slack authentication successful for user %s" (get-in login-result [:user :email]))
-        (if-let [session (:session login-result)]
-          (request/set-session-cookies request
-                                       base-response
-                                       session
-                                       (t/zoned-date-time (t/zone-id "GMT")))
-          base-response))
+  (let [{:keys [code state error error_description]} (:params request)
+        oidc-state-cookie (get-in request [:cookies "metabase.OIDC_STATE" :value])]
+    (log/infof "Slack Connect SSO callback: has-code=%s, has-state=%s, has-oidc-cookie=%s, error=%s, error_description=%s"
+               (boolean code) (boolean state) (boolean oidc-state-cookie) error error_description)
+    (when-not oidc-state-cookie
+      (log/warnf "Slack Connect callback missing OIDC state cookie. Request cookies: %s"
+                 (keys (:cookies request))))
+    (let [login-result (auth-identity/login! :provider/slack-connect
+                                             (assoc request
+                                                    :authenticated-user api/*current-user*
+                                                    :code code
+                                                    :state state
+                                                    :oidc-provider :slack-connect
+                                                    :redirect-uri (slack-redirect-uri)
+                                                    :device-info (request/device-info request)))]
+      (log/infof "Slack Connect SSO callback login result: success=%s, has-session=%s, has-user=%s, redirect-url=%s"
+                 (:success? login-result)
+                 (boolean (:session login-result))
+                 (boolean (:user login-result))
+                 (:redirect-url login-result))
+      (cond
+        (:success? login-result)
+        (let [final-redirect (or (:redirect-url login-result) "/")
+              base-response (-> (response/redirect final-redirect)
+                                (sso/clear-oidc-state-cookie))]
+          (log/infof "Slack authentication successful for user %s, setting session cookie=%s"
+                     (get-in login-result [:user :email])
+                     (boolean (:session login-result)))
+          (if-let [session (:session login-result)]
+            (request/set-session-cookies request
+                                         base-response
+                                         session
+                                         (t/zoned-date-time (t/zone-id "GMT")))
+            base-response))
 
-      :else
-      (let [error-msg (or (:message login-result) (tru "Slack authentication failed"))]
-        (log/errorf "Slack authentication failed: %s" error-msg)
-        (throw (ex-info error-msg {:status-code 401}))))))
+        :else
+        (let [error-msg (or (:message login-result) (tru "Slack authentication failed"))]
+          (log/errorf "Slack authentication failed: %s (full result: %s)" error-msg (pr-str login-result))
+          (throw (ex-info error-msg {:status-code 401})))))))
