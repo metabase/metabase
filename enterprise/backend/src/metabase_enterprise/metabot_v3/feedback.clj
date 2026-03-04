ae104e248bd4467a2c9d27dbbe631eee35c4d439
Noah Moss <noahbmoss@gmail.com>
2026-03-03 20:01:33 -0500

    fix tests

--- enterprise/backend/src/metabase_enterprise/metabot_v3/feedback.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/feedback.clj
@@ -9,12 +9,11 @@
 
 (defn submit-to-harbormaster!
   "Submit metabot feedback to Harbormaster via the Store API.
-   Throws if the token or Store API URL is missing."
+   Returns the HTTP response on success, or nil if the token or Store API URL is missing."
   [feedback]
   (let [token    (premium-features/premium-embedding-token)
         base-url (store-api/store-api-url)]
-    (when (or (str/blank? token) (str/blank? base-url))
-      (throw (ex-info "Missing token or URL for submitting feedback to Harbormaster" {:token token :base-url base-url})))
-    (http/post (str base-url "/api/v2/metabot/feedback/" token)
-               {:content-type :json
-                :body         (json/encode feedback)})))
+    (when-not (or (str/blank? token) (str/blank? base-url))
+      (http/post (str base-url "/api/v2/metabot/feedback/" token)
+                 {:content-type :json
+                  :body         (json/encode feedback)}))))
