777af0caf09f6e299e3f0c504589985f835ef0d9
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-03-05 14:13:43 -0500

    disable plugin

--- enterprise/frontend/src/metabase-enterprise/replacement/index.ts
+++ enterprise/frontend/src/metabase-enterprise/replacement/index.ts
@@ -5,7 +5,9 @@ import { hasPremiumFeature } from "metabase-enterprise/settings";
 import { ReplaceDataSourceModal } from "./components/ReplaceDataSourceModal";
 
 export function initializePlugin() {
-  if (hasPremiumFeature("dependencies")) {
+  // TODO: enable the plugin when it's ready
+  // eslint-disable-next-line no-constant-condition
+  if (hasPremiumFeature("dependencies") && false) {
     PLUGIN_REPLACEMENT.isEnabled = true;
     PLUGIN_REPLACEMENT.canUserReplaceSource = getUserIsAdmin;
     PLUGIN_REPLACEMENT.ReplaceDataSourceModal = ReplaceDataSourceModal;
