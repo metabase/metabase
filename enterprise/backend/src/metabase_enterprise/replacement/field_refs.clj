cedff00cdc737eb636d2defbe63f6001ced9a295
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-03-05 00:52:49 -0500

    wip be

--- enterprise/backend/src/metabase_enterprise/replacement/field_refs.clj
+++ enterprise/backend/src/metabase_enterprise/replacement/field_refs.clj
@@ -3,53 +3,12 @@
    [medley.core :as m]
    [metabase-enterprise.replacement.schema :as replacement.schema]
    [metabase-enterprise.replacement.util :as replacement.util]
+   [metabase-enterprise.replacement.viz :as replacement.viz]
    [metabase.lib-be.core :as lib-be]
    [metabase.lib.core :as lib]
-   [metabase.lib.field.resolution :as lib.field.resolution]
-   [metabase.lib.metadata :as lib.metadata]
-   [metabase.lib.options :as lib.options]
-   [metabase.lib.ref :as lib.ref]
-   [metabase.lib.schema :as lib.schema]
-   [metabase.lib.util :as lib.util]
    [metabase.models.visualization-settings :as vs]
    [metabase.util.malli :as mu]
    [toucan2.core :as t2]))
-
-(mu/defn- card-upgrade-viz-settings :- :map
-  [query         :- ::lib.schema/query
-   viz-settings  :- :map]
-  (letfn [(ref->maybe-name [ref]
-            (when (lib.util/field-clause? ref)
-              (when-let [column (lib.field.resolution/resolve-field-ref query -1 ref)]
-                (when-not (:lib.field.resolution/fallback-metadata? column)
-                  ((some-fn :lib/deduplicated-name :name) column)))))
-          (legacy-ref-or-name->maybe-name [ref-or-name]
-            (or (when (and (vector? ref-or-name) (= :field (keyword (first ref-or-name))))
-                  (-> ref-or-name lib/->pMBQL ref->maybe-name))
-                ref-or-name))
-          (legacy-refs-or-names->names [refs-or-names]
-            (into [] (keep legacy-ref-or-name->maybe-name) refs-or-names))
-          (column-setting->maybe-ref [#::vs{:keys [field-id field-metadata]}]
-            (some-> (lib.metadata/field query field-id)
-                    lib.ref/ref
-                    (lib.options/update-options merge field-metadata)))
-          (update-column-settings [column-settings]
-            (m/map-kv (fn [k v]
-                        (or (when (::vs/field-id k)
-                              (when-some [ref (column-setting->maybe-ref k)]
-                                (when-some [name (ref->maybe-name ref)]
-                                  [{::vs/column-name name} v])))
-                            [k v]))
-                      column-settings))
-          (update-column-formatting [column-formatting]
-            (into [] (map #(m/update-existing % :columns legacy-refs-or-names->names)) column-formatting))]
-    (-> viz-settings
-        (m/update-existing ::vs/column-settings update-column-settings)
-        (m/update-existing :table.column_formatting update-column-formatting)
-        (m/update-existing-in [:pivot_table.column_split :rows] legacy-refs-or-names->names)
-        (m/update-existing-in [:pivot_table.column_split :columns] legacy-refs-or-names->names)
-        (m/update-existing-in [:pivot_table.column_split :values] legacy-refs-or-names->names)
-        (m/update-existing-in [:pivot_table.collapsed_rows :rows] legacy-refs-or-names->names))))
 
 (defn- card-upgrade-field-refs!
   [card]
@@ -57,7 +16,7 @@
     (let [query         (:dataset_query card)
           query'        (lib-be/upgrade-field-refs-in-query query)
           viz-settings  (:visualization_settings card)
-          viz-settings' (some->> viz-settings vs/db->norm (card-upgrade-viz-settings query') vs/norm->db)
+          viz-settings' (some->> viz-settings vs/db->norm (replacement.viz/update-card-viz-settings query') vs/norm->db)
           changes       (cond-> {}
                           (not= query query') (assoc :dataset_query query')
                           (not= viz-settings viz-settings') (assoc :visualization_settings viz-settings'))
@@ -96,62 +55,21 @@
         (t2/update! :model/Measure (:id measure) {:definition query'})))))
 
 (defn- dashcard-upgrade-parameter-mappings
-  [parameter-mappings cards-by-id]
+  [parameter-mappings card-id->query]
   (mapv (fn [mapping]
-          (or (when-let [card (get cards-by-id (:card_id mapping))]
-                (let [query (:dataset_query card)]
-                  (when (replacement.util/valid-query? query)
-                    (m/update-existing mapping :target #(lib-be/upgrade-field-ref-in-parameter-target query %)))))
+          (or (when-let [query (get card-id->query (:card_id mapping))]
+                (m/update-existing mapping :target #(lib-be/upgrade-field-ref-in-parameter-target query %)))
               mapping))
         parameter-mappings))
 
-(defn- click-behavior->card-id
-  [click-behavior]
-  (when (= ::vs/question (::vs/link-type click-behavior))
-    (::vs/link-target-id click-behavior)))
-
-(defn- click-behaviors->card-ids
-  [viz-settings]
-  (concat
-   ;; global click behavior (non-table cards)
-   (when-let [id (click-behavior->card-id (::vs/click-behavior viz-settings))]
-     [id])
-   ;; per-column click behaviors
-   (keep (fn [[_col-key col-viz]]
-           (click-behavior->card-id (::vs/click-behavior col-viz)))
-         (::vs/column-settings viz-settings))))
-
-(defn- dashcard-upgrade-viz-settings
-  [viz-settings cards-by-id]
-  (letfn [(upgrade-mapping [query mapping]
-            (let [target (::vs/param-mapping-target mapping)]
-              (if-let [dimension (::vs/param-dimension target)]
-                (let [dimension' (lib-be/upgrade-field-ref-in-parameter-target query dimension)]
-                  (assoc-in mapping [::vs/param-mapping-target ::vs/param-dimension] dimension'))
-                mapping)))
-          (upgrade-click-behavior [click-behavior]
-            (or (when-let [card-id (click-behavior->card-id click-behavior)]
-                  (when-let [query (some-> (get cards-by-id card-id) :dataset_query)]
-                    (when (replacement.util/valid-query? query)
-                      (m/update-existing click-behavior ::vs/parameter-mapping
-                                         (fn [mappings]
-                                           (m/map-vals #(upgrade-mapping query %) mappings))))))
-                click-behavior))
-          (upgrade-column-settings [col-settings]
-            (m/map-vals #(m/update-existing % ::vs/click-behavior upgrade-click-behavior)
-                        col-settings))]
-    (-> viz-settings
-        (m/update-existing ::vs/click-behavior upgrade-click-behavior)
-        (m/update-existing ::vs/column-settings upgrade-column-settings))))
-
 (defn- dashcard-upgrade-field-refs!
-  [dashcard cards-by-id]
+  [dashcard card-id->query]
   (let [parameter-mappings  (:parameter_mappings dashcard)
-        parameter-mappings' (dashcard-upgrade-parameter-mappings parameter-mappings cards-by-id)
+        parameter-mappings' (dashcard-upgrade-parameter-mappings parameter-mappings card-id->query)
         viz-settings        (:visualization_settings dashcard)
         viz-settings'       (some-> viz-settings
                                     vs/db->norm
-                                    (dashcard-upgrade-viz-settings cards-by-id)
+                                    (replacement.viz/update-dashcard-viz-settings card-id->query lib-be/upgrade-field-ref-in-parameter-target)
                                     vs/norm->db)
         changes (cond-> {}
                   (not= parameter-mappings parameter-mappings')
@@ -171,14 +89,13 @@
                                            (cons (:card_id dashcard)
                                                  (concat
                                                   (keep :card_id (:parameter_mappings dashcard))
-                                                  (click-behaviors->card-ids (-> dashcard :visualization_settings vs/db->norm))))))
+                                                  (replacement.viz/dashboard-viz-settings->card-ids (-> dashcard :visualization_settings vs/db->norm))))))
                                  (remove nil?))
                            dashcards)
-        cards-by-id  (when (seq all-card-ids)
-                       (->> (t2/select :model/Card :id [:in all-card-ids])
-                            (m/index-by :id)))]
+        card-id->query (when (seq all-card-ids)
+                         (t2/select-pk->fn :dataset_query :model/Card :id [:in all-card-ids]))]
     (doseq [dashcard dashcards]
-      (dashcard-upgrade-field-refs! dashcard cards-by-id))))
+      (dashcard-upgrade-field-refs! dashcard card-id->query))))
 
 (mu/defn upgrade!
   "Upgrade field refs in an entity.
---
+++ enterprise/backend/src/metabase_enterprise/replacement/viz.clj
@@ -0,0 +1,85 @@
+(ns metabase-enterprise.replacement.viz
+  (:require
+   [medley.core :as m]
+   [metabase.lib.core :as lib]
+   [metabase.lib.field.resolution :as lib.field.resolution]
+   [metabase.lib.metadata :as lib.metadata]
+   [metabase.lib.options :as lib.options]
+   [metabase.lib.ref :as lib.ref]
+   [metabase.lib.util :as lib.util]
+   [metabase.models.visualization-settings :as vs]
+   [metabase.util.malli :as mu]))
+
+(defn update-card-viz-settings
+  [query
+   viz-settings]
+  (letfn [(ref->maybe-name [ref]
+            (when (lib.util/field-clause? ref)
+              (when-let [column (lib.field.resolution/resolve-field-ref query -1 ref)]
+                (when-not (:lib.field.resolution/fallback-metadata? column)
+                  ((some-fn :lib/deduplicated-name :name) column)))))
+          (legacy-ref-or-name->name [ref-or-name]
+            (or (when (and (vector? ref-or-name) (= :field (keyword (first ref-or-name))))
+                  (-> ref-or-name lib/->pMBQL ref->maybe-name))
+                ref-or-name))
+          (legacy-refs-or-names->names [refs-or-names]
+            (into [] (keep legacy-ref-or-name->name) refs-or-names))
+          (column-setting->maybe-ref [#::vs{:keys [field-id field-metadata]}]
+            (some-> (lib.metadata/field query field-id)
+                    lib.ref/ref
+                    (lib.options/update-options merge field-metadata)))
+          (update-column-settings [column-settings]
+            (m/map-kv (fn [k v]
+                        (or (when (::vs/field-id k)
+                              (when-some [ref (column-setting->maybe-ref k)]
+                                (when-some [name (ref->maybe-name ref)]
+                                  [{::vs/column-name name} v])))
+                            [k v]))
+                      column-settings))
+          (update-column-formatting [column-formatting]
+            (into [] (map #(m/update-existing % :columns legacy-refs-or-names->names)) column-formatting))]
+    (-> viz-settings
+        (m/update-existing ::vs/column-settings update-column-settings)
+        (m/update-existing :table.column_formatting update-column-formatting)
+        (m/update-existing-in [:pivot_table.column_split :rows] legacy-refs-or-names->names)
+        (m/update-existing-in [:pivot_table.column_split :columns] legacy-refs-or-names->names)
+        (m/update-existing-in [:pivot_table.column_split :values] legacy-refs-or-names->names)
+        (m/update-existing-in [:pivot_table.collapsed_rows :rows] legacy-refs-or-names->names))))
+
+(defn click-behavior->card-id
+  [click-behavior]
+  (when (= ::vs/question (::vs/link-type click-behavior))
+    (::vs/link-target-id click-behavior)))
+
+(defn dashboard-viz-settings->card-ids
+  [viz-settings]
+  (concat
+   ;; global click behavior (non-table cards)
+   (when-let [id (click-behavior->card-id (::vs/click-behavior viz-settings))]
+     [id])
+   ;; per-column click behaviors
+   (keep (fn [[_col-key col-viz]]
+           (click-behavior->card-id (::vs/click-behavior col-viz)))
+         (::vs/column-settings viz-settings))))
+
+(defn update-dashcard-viz-settings
+  [viz-settings card-id->query update-fn]
+  (letfn [(upgrade-mapping [query mapping]
+            (let [target (::vs/param-mapping-target mapping)]
+              (if-let [dimension (::vs/param-dimension target)]
+                (let [dimension' (update-fn query dimension)]
+                  (assoc-in mapping [::vs/param-mapping-target ::vs/param-dimension] dimension'))
+                mapping)))
+          (upgrade-click-behavior [click-behavior]
+            (or (when-let [card-id (click-behavior->card-id click-behavior)]
+                  (when-let [query (get card-id->query card-id)]
+                    (m/update-existing click-behavior ::vs/parameter-mapping
+                                       (fn [mappings]
+                                         (m/map-vals #(upgrade-mapping query %) mappings)))))
+                click-behavior))
+          (upgrade-column-settings [col-settings]
+            (m/map-vals #(m/update-existing % ::vs/click-behavior upgrade-click-behavior)
+                        col-settings))]
+    (-> viz-settings
+        (m/update-existing ::vs/click-behavior upgrade-click-behavior)
+        (m/update-existing ::vs/column-settings upgrade-column-settings))))
