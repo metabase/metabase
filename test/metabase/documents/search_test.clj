a2e43e1996dd010c426f426a66b20ecb697bd826
Nathan Voxland <nathan@metabase.com>
2026-03-05 21:23:23 -0600

    Fixing tests

--- test/metabase/documents/search_test.clj
+++ test/metabase/documents/search_test.clj
@@ -3,6 +3,7 @@
    [clojure.test :refer :all]
    [java-time.api :as t]
    [metabase.events.core :as events]
+   [metabase.mq.test-util :as mq.tu]
    [metabase.permissions.core :as perms]
    [metabase.permissions.models.permissions-group :as perms-group]
    [metabase.search.test-util :as search.tu]
@@ -33,23 +34,24 @@
     :visualization_settings {:global {:title nil}}}))
 
 (deftest card-search-exclusion-test
-  (testing "Cards with document_id are excluded from search indexing"
-    (let [card-name (mt/random-name)
-          regular-card-name (str card-name "-regular")]
-      (search.tu/with-temp-index-table
-        (mt/with-temp [:model/Document document {:name "Test Document"
-                                                 :document (text->prose-mirror-ast "")}
-                       :model/Card document-card (-> (card-with-name-and-query card-name)
-                                                     (assoc :document_id (u/the-id document)))
-                       :model/Card regular-card (card-with-name-and-query regular-card-name)]
+  (mq.tu/with-sync-mq
+    (testing "Cards with document_id are excluded from search indexing"
+      (let [card-name (mt/random-name)
+            regular-card-name (str card-name "-regular")]
+        (search.tu/with-temp-index-table
+          (mt/with-temp [:model/Document document {:name "Test Document"
+                                                   :document (text->prose-mirror-ast "")}
+                         :model/Card document-card (-> (card-with-name-and-query card-name)
+                                                       (assoc :document_id (u/the-id document)))
+                         :model/Card regular-card (card-with-name-and-query regular-card-name)]
 
-          (testing "Search API excludes document cards"
-            (let [results (mt/user-http-request :crowberto :get 200 "search" :q card-name :models "card")]
-              (is (not (some #(= (:id %) (u/the-id document-card)) (:data results)))
-                  "Document card should not appear in search results"))
-            (let [regular-results (mt/user-http-request :crowberto :get 200 "search" :q regular-card-name :models "card")]
-              (is (some #(= (:id %) (u/the-id regular-card)) (:data regular-results))
-                  "Regular card should appear in search results"))))))))
+            (testing "Search API excludes document cards"
+              (let [results (mt/user-http-request :crowberto :get 200 "search" :q card-name :models "card")]
+                (is (not (some #(= (:id %) (u/the-id document-card)) (:data results)))
+                    "Document card should not appear in search results"))
+              (let [regular-results (mt/user-http-request :crowberto :get 200 "search" :q regular-card-name :models "card")]
+                (is (some #(= (:id %) (u/the-id regular-card)) (:data regular-results))
+                    "Regular card should appear in search results")))))))))
 
 (deftest document-permission-filtering-test
   (testing "Document search respects collection-based permissions"
