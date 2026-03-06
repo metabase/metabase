5207c1c832c1ed84b769532da33889c47f9548c5
Kamil Mielnik <kamil@kamilmielnik.com>
2026-03-06 14:25:31 +0700

    Improve types

--- frontend/src/metabase/visualizations/lib/mapping.ts
+++ frontend/src/metabase/visualizations/lib/mapping.ts
@@ -51,15 +51,12 @@ const stateNamesMap = new Map(
  *
  * Currently transforms US state names to state codes for the "us_states" region map, and just lowercases all others.
  */
-export function getCanonicalRowKey(
-  key: RowValue,
-  region?: string,
-): string | undefined {
+export function getCanonicalRowKey(key: RowValue, region?: string): string {
   const normalizedKey = String(key).toLowerCase();
   // Special case for supporting both US state names and state codes
   // This should be ok because we know there's no overlap between state names and codes, and we know the "us_states" region map expects codes
   if (region === "us_states" && stateNamesMap.has(normalizedKey)) {
-    return stateNamesMap.get(normalizedKey);
+    return stateNamesMap.get(normalizedKey) as string; // ok to cast because 1 line above we check presence of the key in the map
   } else if (
     region === "world_countries" &&
     normalizedKey in COUNTRY_NAME_TO_CODE
