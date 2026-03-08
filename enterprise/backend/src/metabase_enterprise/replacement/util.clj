4c14989db8bae455bcecd8c110605b91a36c08d3
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-03-07 23:47:30 -0500

    fix

--- enterprise/backend/src/metabase_enterprise/replacement/util.clj
+++ enterprise/backend/src/metabase_enterprise/replacement/util.clj
@@ -6,7 +6,7 @@
 
 (mu/defn valid-query? :- :boolean
   "Returns true if `maybe-query` has at least one stage.
-  
+
   This is used to skip fully broken queries in the app db."
   [maybe-query :- [:maybe :map]]
   (some? (seq (:stages maybe-query))))
