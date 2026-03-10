a11c0f458be533a3ee5ce275c9af29ff7c37f883
Kamil Mielnik <kamil@kamilmielnik.com>
2026-03-10 16:56:37 +0700

    Improve types

--- frontend/src/metabase-types/guards/visualization.ts
+++ frontend/src/metabase-types/guards/visualization.ts
@@ -1,5 +1,7 @@
 import { isObject } from "./common";
 
-export function isObjectWithRaw<T>(object: T): object is T & { _raw?: T } {
+export function isObjectWithRaw<T>(
+  object: T,
+): object is T & { _raw: T | undefined } {
   return isObject(object) && "_raw" in object;
 }
