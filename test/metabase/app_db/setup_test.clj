b8897a81401c1b472859ec2143e085292bd87ddf
Ben Grabow <ben.grabow@gmail.com>
2026-03-11 10:47:32 -0500

    DRY the tests

--- test/metabase/app_db/setup_test.clj
+++ test/metabase/app_db/setup_test.clj
@@ -29,40 +29,16 @@
 
 (deftest supported-app-db-version?-test
   (testing "Should be able to check if an app DB is a supported version"
-    (testing "for H2"
-      (is (true? (#'mdb.setup/supported-app-db-version? :h2 {:major 2 :minor 1 :patch 214})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :h2 {:major 3 :minor 1 :patch 214})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :h2 {:major 2 :minor 2 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :h2 {:major 3 :minor 0 :patch 0})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :h2 {:major 2 :minor 1 :patch 213})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :h2 {:major 2 :minor 0 :patch 214})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :h2 {:major 1 :minor 1 :patch 214}))))
-    (testing "for postgres"
-      (is (true? (#'mdb.setup/supported-app-db-version? :postgres {:major 14 :minor 0 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :postgres {:major 15 :minor 0 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :postgres {:major 14 :minor 1 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :postgres {:major 14 :minor 1 :patch 1})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :postgres {:major 13 :minor 0 :patch 0})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :postgres {:major 14 :minor -1 :patch 0})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :postgres {:major 14 :minor 0 :patch -1}))))
+    (letfn [(test-supported-versions [db expected-min-version]
+              (doseq [diff [nil {:major 1} {:minor 1} {:patch 1}]]
+                (is (true? (#'mdb.setup/supported-app-db-version? db (merge-with + expected-min-version diff)))))
+              (doseq [diff [{:major -1} {:minor -1} {:patch -1}]]
+                (is (false? (#'mdb.setup/supported-app-db-version? db (merge-with + expected-min-version diff))))))]
 
-    (testing "for mysql"
-      (is (true? (#'mdb.setup/supported-app-db-version? :mysql {:major 8 :minor 4 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :mysql {:major 9 :minor 4 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :mysql {:major 8 :minor 5 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :mysql {:major 8 :minor 4 :patch 1})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :mysql {:major 7 :minor 4 :patch 0})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :mysql {:major 8 :minor 3 :patch 0})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :mysql {:major 8 :minor 4 :patch -1}))))
-
-    (testing "for mariadb"
-      (is (true? (#'mdb.setup/supported-app-db-version? :mariadb {:major 10 :minor 6 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :mariadb {:major 11 :minor 6 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :mariadb {:major 10 :minor 7 :patch 0})))
-      (is (true? (#'mdb.setup/supported-app-db-version? :mariadb {:major 10 :minor 6 :patch 1})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :mariadb {:major 9 :minor 6 :patch 0})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :mariadb {:major 10 :minor 5 :patch 0})))
-      (is (false? (#'mdb.setup/supported-app-db-version? :mariadb {:major 10 :minor 6 :patch -1}))))))
+      (test-supported-versions :h2 {:major 2 :minor 1 :patch 214})
+      (test-supported-versions :postgres {:major 14 :minor 0 :patch 0})
+      (test-supported-versions :mysql {:major 8 :minor 4 :patch 0})
+      (test-supported-versions :mariadb {:major 10 :minor 6 :patch 0}))))
 
 (deftest parse-db-version-test
   (testing "Can parse H2 version strings"
