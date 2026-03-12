4575f08ce5c86c8931d0416dce50520d9c05ec66
Egor Grushin <egor@metabase.com>
2026-03-12 15:54:10 +0000

    gdgt-1864 added a column limiter

--- frontend/src/metabase/data-grid/hooks/use-row-pinning-by-count.ts
+++ frontend/src/metabase/data-grid/hooks/use-row-pinning-by-count.ts
@@ -3,23 +3,17 @@ import { useMemo } from "react";
 
 type UseRowPinningByCountProps<TData> = {
   top?: number;
-  bottom?: number;
   data: TData[];
   getRowId: (originalRow: TData, index: number, parent?: Row<TData>) => string;
 };
 
 export const useRowPinningByCount = <TData>({
   top = 0,
-  bottom = 0,
   data,
   getRowId,
-}: UseRowPinningByCountProps<TData>): RowPinningState =>
-  useMemo(() => {
+}: UseRowPinningByCountProps<TData>): RowPinningState => {
+  return useMemo(() => {
     const topIds = data.slice(0, top).map((row, index) => getRowId(row, index));
-
-    const bottomShift = data.length - bottom;
-    const bottomIds = data
-      .slice(bottomShift)
-      .map((row, index) => getRowId(row, bottomShift + index));
-    return { top: topIds, bottom: bottomIds };
-  }, [top, bottom, data, getRowId]);
+    return { top: topIds };
+  }, [top, data, getRowId]);
+};
