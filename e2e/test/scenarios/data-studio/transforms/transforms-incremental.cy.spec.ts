5e3fc91739d5755cd02b291ea871c877756a6668
Jakub Chodorowicz <jakub.chodorowicz@metabase.com>
2026-03-13 20:06:10 +0100

    add python incremental transform e2e test

--- e2e/test/scenarios/data-studio/transforms/transforms-incremental.cy.spec.ts
+++ e2e/test/scenarios/data-studio/transforms/transforms-incremental.cy.spec.ts
@@ -153,6 +153,129 @@ describe("scenarios > admin > transforms incremental", () => {
       });
     });
 
+    it(
+      "should be able to create and run a Python incremental transform",
+      { tags: ["@python"] },
+      () => {
+        H.setPythonRunnerSettings();
+        cy.log("create a new transform");
+        visitTransformListPage();
+        cy.button("Create a transform").click();
+        H.popover().findByText("Python script").click();
+
+        H.expectUnstructuredSnowplowEvent({
+          event: "transform_create",
+          event_detail: "python",
+        });
+
+        cy.findByTestId("python-transform-top-bar")
+          .findByText("Writable Postgres12")
+          .click();
+        H.popover().findByText(DB_NAME).click();
+
+        getPythonDataPicker().findByText("Select a table…").click();
+        H.entityPickerModal().findByText(SOURCE_TABLE).click();
+
+        H.PythonEditor.clear().paste(
+          `import pandas as pd
+
+def transform(animals):
+    return pd.DataFrame([{"name": "test", "score": 0}])`,
+        );
+
+        getQueryEditor().button("Save").click();
+
+        H.modal().within(() => {
+          cy.findByLabelText("Name").clear().type("Python transform");
+          cy.findByLabelText("Table name").clear().type(TARGET_TABLE);
+          cy.findByRole("switch", {
+            name: /Only process new and changed data/i,
+          }).click({ force: true });
+          cy.button("Save").click();
+          cy.wait("@createTransform").then(({ response }) => {
+            const transformId = response?.body?.id;
+            if (transformId != null) {
+              cy.wrap(transformId).as("transformId");
+            }
+          });
+        });
+
+        cy.log("run the transform and make sure its table can be queried");
+        H.DataStudio.Transforms.runTab().click();
+        runTransformAndWaitForSuccess();
+        H.expectUnstructuredSnowplowEvent({
+          event: "transform_trigger_manual_run",
+        });
+
+        cy.findByRole("link", { name: "See all runs" }).click();
+
+        cy.findByRole("treegrid", { name: "Transform runs" }).within(() => {
+          cy.findByRole("row", { name: /Python transform/i }).click();
+        });
+
+        cy.findByRole("region", { name: "Info" }).within(() => {
+          cy.findByRole("group", { name: "Checkpoint to" }).within(() => {
+            cy.findByText(/30/).should("be.visible");
+          });
+        });
+
+        cy.log(
+          "add one element to the source table and run incremental transform again",
+        );
+        H.queryWritableDB(
+          'INSERT INTO "Schema A"."Animals" (name, score) VALUES (\'NewRow\', 31)',
+        );
+
+        cy.go("back");
+        H.DataStudio.Transforms.runTab().click();
+        runTransformAndWaitForSuccess();
+
+        cy.log(
+          "verify the new element was picked up in the incremental transfer",
+        );
+        cy.findByRole("link", { name: "See all runs" }).click();
+        cy.findByRole("treegrid", { name: "Transform runs" }).within(() => {
+          cy.findAllByRole("row", { name: /Python transform/i })
+            .first()
+            .click();
+        });
+        cy.findByRole("region", { name: "Info" }).within(() => {
+          cy.findByRole("group", { name: "Checkpoint to" }).within(() => {
+            cy.findByText(/31/).should("be.visible");
+          });
+        });
+
+        cy.log("go to Transform Settings and reset checkpoint");
+        cy.go("back");
+        H.DataStudio.Transforms.settingsTab().click();
+        cy.findByRole("group", { name: /Current checkpoint/i }).within(() => {
+          cy.findByText(/31/).should("exist");
+        });
+        cy.button("Reset checkpoint").click();
+        H.modal().within(() => {
+          cy.button("Reset").click();
+        });
+        cy.wait("@resetCheckpoint");
+
+        cy.log(
+          "go to Runs tab, run transform again and check new run has checkpoint to 31",
+        );
+        H.DataStudio.Transforms.runTab().click();
+        runTransformAndWaitForSuccess();
+        cy.findByRole("link", { name: "See all runs" }).click();
+        cy.findByRole("treegrid", { name: "Transform runs" }).within(() => {
+          cy.findAllByRole("row", { name: /Python transform/i })
+            .first()
+            .click();
+        });
+        cy.findByRole("region", { name: "Info" }).within(() => {
+          cy.findByRole("group", { name: "Checkpoint to" }).within(() => {
+            cy.findByText(/31/).should("be.visible");
+          });
+        });
+      },
+    );
+
     it("should be able to create and run a native SQL incremental transform", () => {
       cy.log("create a new transform");
       visitTransformListPage();
@@ -165,7 +288,7 @@ describe("scenarios > admin > transforms incremental", () => {
       });
 
       H.popover().findByText(DB_NAME).click();
-      H.NativeEditor.type("SELECT * FROM {{ source_table }}", {
+      H.NativeEditor.type("SELECT * FROM {{t}}", {
         allowFastSet: true,
       }).blur();
 
@@ -175,7 +298,7 @@ describe("scenarios > admin > transforms incremental", () => {
         .click();
       editorSidebar().findByLabelText("Variable type").click();
       H.popover().findByText("Table").click();
-      H.popover().findByText("Schema a").click();
+      H.popover().findByText("Schema B").click();
       H.popover().findByText(SOURCE_TABLE).click();
 
       getQueryEditor().button("Save").click();
@@ -279,16 +402,14 @@ function getRunButton(options: { timeout?: number } = {}) {
   return cy.findAllByTestId("run-button").eq(0, options);
 }
 
-function _getTableLink({ isActive = true }: { isActive?: boolean } = {}) {
-  return cy
-    .findByTestId("table-link")
-    .should("have.attr", "aria-disabled", String(!isActive));
-}
-
 function getQueryEditor() {
   return cy.findByTestId("transform-query-editor");
 }
 
 function editorSidebar() {
   return cy.findByTestId("editor-sidebar");
+}
+
+function getPythonDataPicker() {
+  return cy.findByTestId("python-data-picker");
 }
