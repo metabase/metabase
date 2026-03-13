440798a9ebf671a3d73524cf96e2fc0d2363a60c
Mike Appleby <86076+appleby@users.noreply.github.com>
2026-03-12 19:52:58 -0700

    Use TMD funcs to compute percentiles, quartiles, skew and kurtosis in histogram.clj

--- enterprise/backend/src/metabase_enterprise/metabot_v3/stats/histogram.clj
+++ enterprise/backend/src/metabase_enterprise/metabot_v3/stats/histogram.clj
@@ -1,65 +1,32 @@
 (ns metabase-enterprise.metabot-v3.stats.histogram
   "Histogram chart statistics computation."
   (:require
-   [metabase-enterprise.metabot-v3.stats.util :as stats.u]))
+   [metabase-enterprise.metabot-v3.stats.util :as stats.u]
+   [tech.v3.datatype.functional :as dfn]))
 
 (set! *warn-on-reflection* true)
 
 (def ^:private min-shape-metrics-points 8)
-(def ^:private skewness-threshold 0.5)
-
-(defn- compute-percentile
-  "Compute a percentile of sorted values using linear interpolation. pct: 0-100."
-  [sorted-vals pct]
-  (let [n (count sorted-vals)]
-    (if (= n 1)
-      (first sorted-vals)
-      (let [idx   (* (/ pct 100.0) (dec n))
-            lower (int (Math/floor idx))
-            upper (int (Math/ceil idx))
-            frac  (- idx lower)]
-        (+ (* (- 1.0 frac) (nth sorted-vals lower))
-           (* frac (nth sorted-vals upper)))))))
 
 (defn- compute-percentiles
-  "Compute percentiles map for a sorted sequence of values."
-  [sorted-vals]
-  (into {}
-        (for [pct [25 50 75 90 95 99]]
-          [pct (compute-percentile sorted-vals pct)])))
+  "Compute percentiles map for a sequence of values."
+  [values]
+  (let [pcts [25 50 75 90 95 99]]
+    (zipmap pcts (dfn/percentiles values pcts))))
 
 (defn- compute-quartiles
-  "Compute quartile stats from sorted values."
-  [sorted-vals]
-  (let [q1  (compute-percentile sorted-vals 25)
-        med (compute-percentile sorted-vals 50)
-        q3  (compute-percentile sorted-vals 75)]
+  "Compute quartile stats from values."
+  [values]
+  (let [[_min q1 med q3 _max] (dfn/quartiles values)]
     {:q1     q1
      :median med
      :q3     q3
      :iqr    (- q3 q1)}))
 
-(defn- compute-skewness
-  "Compute adjusted Fisher-Pearson skewness coefficient."
-  [values mean-val std-dev n]
-  (when (and (>= n 3) (pos? std-dev))
-    (let [cubed-zscores (map (fn [x] (Math/pow (/ (- (double x) mean-val) std-dev) 3)) values)
-          sum-cubed     (reduce + 0.0 cubed-zscores)]
-      (* (/ (* n (double sum-cubed))
-            (* (dec n) (- n 2)))
-         1.0))))
-
-(defn- compute-excess-kurtosis
-  "Compute Fisher's excess kurtosis (normal distribution = 0)."
-  [values mean-val std-dev n]
-  (when (and (>= n 4) (pos? std-dev))
-    (let [fourth-zscores (map (fn [x] (Math/pow (/ (- (double x) mean-val) std-dev) 4)) values)
-          sum-fourth     (reduce + 0.0 fourth-zscores)
-          term1          (/ (* n (inc n) sum-fourth)
-                            (* (dec n) (- n 2) (- n 3)))
-          term2          (/ (* 3.0 (Math/pow (dec n) 2))
-                            (* (- n 2) (- n 3)))]
-      (- term1 term2))))
+(defn- nan->nil
+  "Convert NaN to nil so cond-> predicates work correctly."
+  [x]
+  (when-not (Double/isNaN (double x)) x))
 
 (defn compute-series-stats
   "Compute histogram stats for a single series using its y_values (counts/frequencies)
@@ -79,15 +46,12 @@
         :bin_data     []
         :distribution {:percentiles {} :quartiles {:q1 0 :median 0 :q3 0 :iqr 0}}}
        (let [summary     (stats.u/compute-summary valid)
-             sorted-vals (sort valid)
-             percentiles (compute-percentiles sorted-vals)
-             quartiles   (compute-quartiles sorted-vals)
-             mean-val    (:mean summary)
-             std-dev     (:std_dev summary)
+             percentiles (compute-percentiles valid)
+             quartiles   (compute-quartiles valid)
              skewness    (when (>= n min-shape-metrics-points)
-                           (compute-skewness valid mean-val std-dev n))
+                           (nan->nil (dfn/skew valid)))
              kurtosis    (when (>= n min-shape-metrics-points)
-                           (compute-excess-kurtosis valid mean-val std-dev n))]
+                           (nan->nil (dfn/kurtosis valid)))]
          {:summary      summary
           :data_points  n
           :bin_data     (mapv vector valid-xs valid)
