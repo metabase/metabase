5b6087a0b9af7f0ffeaa42a58aecae23037e2831
Kamil Mielnik <kamil@kamilmielnik.com>
2026-03-13 21:28:59 +0700

    Refactor props to getProps

--- frontend/src/metabase/visualizations/visualizations/Table/Table.tsx
+++ frontend/src/metabase/visualizations/visualizations/Table/Table.tsx
@@ -329,9 +329,9 @@ export class Table extends Component<TableProps, TableState> {
         title: t`Display as`,
         widget: options.length === 2 ? "radio" : "select",
         default: defaultValue,
-        props: {
+        getProps: () => ({
           options,
-        },
+        }),
       };
     }
 
