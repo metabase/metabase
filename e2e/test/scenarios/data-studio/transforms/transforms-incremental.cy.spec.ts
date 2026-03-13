8f0031968b84c7a625bbf860768e370074125c58
Jakub Chodorowicz <jakub.chodorowicz@metabase.com>
2026-03-13 19:44:49 +0100

    add native sql incremental transform test

--- e2e/test/scenarios/data-studio/transforms/transforms-incremental.cy.spec.ts
+++ e2e/test/scenarios/data-studio/transforms/transforms-incremental.cy.spec.ts
@@ -152,6 +152,117 @@ describe("scenarios > admin > transforms incremental", () => {
         });
       });
     });
+
+    it("should be able to create and run a native SQL incremental transform", () => {
+      cy.log("create a new transform");
+      visitTransformListPage();
+      cy.button("Create a transform").click();
+      H.popover().findByText("SQL query").click();
+
+      H.expectUnstructuredSnowplowEvent({
+        event: "transform_create",
+        event_detail: "native",
+      });
+
+      H.popover().findByText(DB_NAME).click();
+      H.NativeEditor.type("SELECT * FROM {{ source_table }}", {
+        allowFastSet: true,
+      }).blur();
+
+      cy.log("configure table variable for incremental support");
+      cy.findByTestId("native-query-top-bar")
+        .findByLabelText("Variables")
+        .click();
+      editorSidebar().findByLabelText("Variable type").click();
+      H.popover().findByText("Table").click();
+      H.popover().findByText("Schema a").click();
+      H.popover().findByText(SOURCE_TABLE).click();
+
+      getQueryEditor().button("Save").click();
+
+      H.modal().within(() => {
+        cy.findByLabelText("Name").clear().type("SQL transform");
+        cy.findByLabelText("Table name").clear().type(TARGET_TABLE);
+        cy.findByRole("switch", {
+          name: /Only process new and changed data/i,
+        }).click({ force: true });
+        cy.button("Save").click();
+        cy.wait("@createTransform");
+      });
+
+      cy.log("run the transform and make sure its table can be queried");
+      H.DataStudio.Transforms.runTab().click();
+      runTransformAndWaitForSuccess();
+      H.expectUnstructuredSnowplowEvent({
+        event: "transform_trigger_manual_run",
+      });
+
+      cy.findByRole("link", { name: "See all runs" }).click();
+      cy.findByRole("treegrid", { name: "Transform runs" }).within(() => {
+        cy.findByRole("row", { name: /SQL transform/i }).click();
+      });
+
+      cy.findByRole("region", { name: "Info" }).within(() => {
+        cy.findByRole("group", { name: "Checkpoint to" }).within(() => {
+          cy.findByText(/30/).should("be.visible");
+        });
+      });
+
+      cy.log(
+        "add one element to the source table and run incremental transform again",
+      );
+      H.queryWritableDB(
+        'INSERT INTO "Schema A"."Animals" (name, score) VALUES (\'NewRow\', 31)',
+      );
+
+      cy.go("back");
+      H.DataStudio.Transforms.runTab().click();
+      runTransformAndWaitForSuccess();
+
+      cy.log(
+        "verify the new element was picked up in the incremental transfer",
+      );
+      cy.findByRole("link", { name: "See all runs" }).click();
+      cy.findByRole("treegrid", { name: "Transform runs" }).within(() => {
+        cy.findAllByRole("row", { name: /SQL transform/i })
+          .first()
+          .click();
+      });
+      cy.findByRole("region", { name: "Info" }).within(() => {
+        cy.findByRole("group", { name: "Checkpoint to" }).within(() => {
+          cy.findByText(/31/).should("be.visible");
+        });
+      });
+
+      cy.log("go to Transform Settings and reset checkpoint");
+      cy.go("back");
+      H.DataStudio.Transforms.settingsTab().click();
+      cy.findByRole("group", { name: /Current checkpoint/i }).within(() => {
+        cy.findByText(/31/).should("exist");
+      });
+      cy.button("Reset checkpoint").click();
+      H.modal().within(() => {
+        cy.button("Reset").click();
+      });
+      cy.wait("@resetCheckpoint");
+
+      cy.log(
+        "go to Runs tab, run transform again and check new run has checkpoint to 31",
+      );
+      H.DataStudio.Transforms.runTab().click();
+      runTransformAndWaitForSuccess();
+      cy.findByRole("link", { name: "See all runs" }).click();
+      cy.findByRole("treegrid", { name: "Transform runs" }).within(() => {
+        cy.findAllByRole("row", { name: /SQL transform/i })
+          .first()
+          .click();
+      });
+      cy.findByRole("region", { name: "Info" }).within(() => {
+        cy.findByRole("group", { name: "Checkpoint to" }).within(() => {
+          cy.findByText(/31/).should("be.visible");
+        });
+      });
+    });
   });
 });
 
@@ -176,4 +287,8 @@ function _getTableLink({ isActive = true }: { isActive?: boolean } = {}) {
 
 function getQueryEditor() {
   return cy.findByTestId("transform-query-editor");
+}
+
+function editorSidebar() {
+  return cy.findByTestId("editor-sidebar");
 }
