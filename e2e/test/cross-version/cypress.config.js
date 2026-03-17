ca3cb195b012d745cf14439769626f9ae05bce7d
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2026-03-17 19:13:51 +0000

    Use Ts specs

--- e2e/test/cross-version/cypress.config.js
+++ e2e/test/cross-version/cypress.config.js
@@ -5,7 +5,7 @@ const { defaultConfig } = require("../../support/config");
 module.exports = defineConfig({
   e2e: {
     ...defaultConfig,
-    specPattern: "e2e/test/cross-version/scenarios/**/*.cy.spec.js",
+    specPattern: "e2e/test/cross-version/scenarios/**/*.cy.spec.ts",
     viewportWidth: 1600,
     viewportHeight: 1200,
   },
