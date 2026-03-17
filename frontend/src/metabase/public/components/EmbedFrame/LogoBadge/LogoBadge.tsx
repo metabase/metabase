8e0092d92f35b7f7876d25a78f9f5c644f723d2d
LeinorainPuyatsteel <leinorainautida.puyatsteel@gmail.com>
2026-03-17 08:52:42 +0800

    Removed Logo

--- frontend/src/metabase/public/components/EmbedFrame/LogoBadge/LogoBadge.tsx
+++ frontend/src/metabase/public/components/EmbedFrame/LogoBadge/LogoBadge.tsx
@@ -8,26 +8,28 @@ import EmbedFrameS from "../EmbedFrame.module.css";
 import LogoBadgeStyle from "./LogoBadge.module.css";
 import MetabaseLogoWithText from "./metabase_logo_with_text.svg?component";
 
-export const LogoBadge = ({ dark }: { dark: boolean }) => {
-  const utmContentValue = `embedded_banner_${encodeURIComponent(
-    getHostAppUrlDomain(),
-  )}`;
+// export const LogoBadge = ({ dark }: { dark: boolean }) => {
+//   const utmContentValue = `embedded_banner_${encodeURIComponent(
+//     getHostAppUrlDomain(),
+//   )}`;
 
-  return (
-    <ExternalLink
-      className={cx(EmbedFrameS.LogoBadge, LogoBadgeStyle.metabaseLink, {
-        [LogoBadgeStyle.dark]: dark,
-        [LogoBadgeStyle.light]: !dark,
-      })}
-      href={`https://www.metabase.com?utm_medium=referral&utm_source=product&utm_campaign=powered_by_metabase&utm_content=${utmContentValue}`}
-      target="_blank"
-    >
-      <span>{t`Powered by`}</span>
-      {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This embedding badge which we don't want to show the whitelabeled name */}
-      <MetabaseLogoWithText height={32} aria-label="Metabase" />
-    </ExternalLink>
-  );
-};
+//   return (
+//     <ExternalLink
+//       className={cx(EmbedFrameS.LogoBadge, LogoBadgeStyle.metabaseLink, {
+//         [LogoBadgeStyle.dark]: dark,
+//         [LogoBadgeStyle.light]: !dark,
+//       })}
+//       href={`https://www.metabase.com?utm_medium=referral&utm_source=product&utm_campaign=powered_by_metabase&utm_content=${utmContentValue}`}
+//       target="_blank"
+//     >
+//       <span>{t`Powered by`}</span>
+//       {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This embedding badge which we don't want to show the whitelabeled name */}
+//       <MetabaseLogoWithText height={32} aria-label="Metabase" />
+//     </ExternalLink>
+//   );
+// };
+
+export const LogoBadge = () => null;
 
 function getHostAppUrlDomain() {
   // based on https://stackoverflow.com/questions/3420004/access-parent-url-from-iframe
