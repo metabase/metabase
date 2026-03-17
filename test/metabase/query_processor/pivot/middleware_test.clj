eecb254c922f5c12c1d7814dcfd72531b0abb58a
Cam Saul <github@camsaul.com>
2026-03-17 15:30:08 -0700

    Test fix :wrench:

--- test/metabase/query_processor/pivot/middleware_test.clj
+++ test/metabase/query_processor/pivot/middleware_test.clj
@@ -3,28 +3,31 @@
    [clojure.test :refer :all]
    [metabase.query-processor.pivot.middleware :as qp.pivot.middleware]))
 
-(deftest ^:parallel splice-in-remap-test
-  (let [splice #'qp.pivot.middleware/splice-in-remap]
+(deftest ^:parallel full-breakout-combination-test
+  (letfn [(combo [breakout-combination remaps]
+            (#'qp.pivot.middleware/full-breakout-combination
+             {:qp.pivot/remapped-breakout-combination breakout-combination
+              :qp.pivot/remapped-indexes              remaps}))]
     (is (= []
-           (splice [] {1 0, 4 3})))
+           (combo [] {1 0, 4 3})))
     (is (= [0 1]
-           (splice [0] {1 0, 4 3})))
+           (combo [0] {1 0, 4 3})))
     (is (= [2]
-           (splice [1] {1 0, 4 3})))
+           (combo [1] {1 0, 4 3})))
     (is (= [0 1 2]
-           (splice [0 1] {1 0, 4 3})))
+           (combo [0 1] {1 0, 4 3})))
     (is (= [0 1 3 4]
-           (splice [0 2] {1 0, 4 3})))
+           (combo [0 2] {1 0, 4 3})))
     (testing "chained remapping"
       (is (= [1 2 3 5]
-             (splice [1 2] {1 2, 2 5})))
+             (combo [1 2] {1 2, 2 5})))
       (is (= [1 2 3 4 5]
-             (splice [1 2 3] {1 2, 2 5})))
+             (combo [1 2 3] {1 2, 2 5})))
       (is (= [1 2 3 5 6]
-             (splice [1 2 4] {1 2, 2 5})))
+             (combo [1 2 4] {1 2, 2 5})))
       (is (= [1 2 3 5 7]
-             (splice [1 2 5] {1 2, 2 5})))
+             (combo [1 2 5] {1 2, 2 5})))
       (is (= [1 2 3 5 8]
-             (splice [1 2 6] {1 2, 2 5})))
+             (combo [1 2 6] {1 2, 2 5})))
       (is (= [1 2 3 5 8]
-             (splice [1 2 6] {1 2, 2 5, 3 2}))))))
+             (combo [1 2 6] {1 2, 2 5, 3 2}))))))
