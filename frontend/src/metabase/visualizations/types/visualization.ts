3c229eb38d188439206125484e5c33fa2a437d4d
Kamil Mielnik <kamil@kamilmielnik.com>
2026-03-18 15:59:11 +0700

    GDGT-1998 Get rid of VisualizationSettingDefinition["marginBottom"] (#71004)

--- frontend/src/metabase/visualizations/types/visualization.ts
+++ frontend/src/metabase/visualizations/types/visualization.ts
@@ -354,7 +354,6 @@ export type VisualizationSettingDefinition<
   ) => string;
   autoOpenWhenUnset?: boolean;
   default?: TValue;
-  marginBottom?: string;
   noPadding?: boolean;
   value?: TValue;
   set?: boolean;
@@ -391,8 +390,12 @@ export type CompleteVisualizationSettingDefinition<
   T = unknown,
   TValue = unknown,
   TProps extends Record<string, unknown> = Record<string, unknown>,
-> = Omit<VisualizationSettingDefinition<T, TValue, TProps>, "getProps"> & {
+> = Omit<
+  VisualizationSettingDefinition<T, TValue, TProps>,
+  "getMarginBottom" | "getProps"
+> & {
   id: string;
+  marginBottom?: string;
   props: Partial<TProps>;
 };
 
@@ -450,6 +453,8 @@ export type VisualizationSettingsDefinitions<
   >;
   "graph.colors"?: SeriesSettingDefinition<Value, Props>;
   "graph.dimensions"?: SeriesSettingDefinition<Value, Props>;
+  "graph.goal_label"?: SeriesSettingDefinition<Value, Props>;
+  "graph.goal_value"?: SeriesSettingDefinition<Value, Props>;
   "graph.metrics"?: SeriesSettingDefinition<Value, Props>;
   "graph.label_value_frequency"?: SeriesSettingDefinition<
     Value,
@@ -469,6 +474,7 @@ export type VisualizationSettingsDefinitions<
     ChartSettingSeriesOrderProps
   >;
   "graph.series_order_dimension"?: SeriesSettingDefinition<Value, Props>;
+  "graph.show_goal"?: SeriesSettingDefinition<Value, Props>;
   "graph.show_mean"?: SeriesSettingDefinition<Value, Props>;
   "graph.show_stack_values"?: SeriesSettingDefinition<Value, Props>;
   "graph.show_trendline"?: SeriesSettingDefinition<Value, Props>;
