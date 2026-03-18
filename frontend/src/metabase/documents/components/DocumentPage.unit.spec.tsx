5bcfb29ff2f94c507f906a6e10d7c753a1f4d54b
Uladzimir Havenchyk <uladzimir@metabase.com>
2026-03-18 12:33:19 +0300

    fix test

--- frontend/src/metabase/documents/components/DocumentPage.unit.spec.tsx
+++ frontend/src/metabase/documents/components/DocumentPage.unit.spec.tsx
@@ -11,6 +11,22 @@ import { createMockDocument } from "metabase-types/api/mocks";
 
 import { DocumentPage } from "./DocumentPage";
 
+beforeAll(() => {
+  // IntersectionObserver is not available in jsdom
+  globalThis.IntersectionObserver = class IntersectionObserver {
+    readonly root = null;
+    readonly rootMargin = "0px";
+    readonly thresholds = [0];
+    constructor(private cb: IntersectionObserverCallback) {}
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+    takeRecords(): IntersectionObserverEntry[] {
+      return [];
+    }
+  } as unknown as typeof globalThis.IntersectionObserver;
+});
+
 const setup = () => {
   setupBookmarksEndpoints([]);
   setupDocumentEndpoints(
