3eb2342d8888a93c839842f42cccff67f689c0cb
Riley Thompson <rileythompson99@gmail.com>
2026-03-18 18:08:46 -0400

    fix sandboxing tests

--- enterprise/backend/test/metabase_enterprise/sandbox/query_processor/middleware/sandboxing_test.clj
+++ enterprise/backend/test/metabase_enterprise/sandbox/query_processor/middleware/sandboxing_test.clj
@@ -55,9 +55,13 @@
      (qp.store/with-metadata-provider (mt/id)
        (sql.qp/->honeysql
         (or driver/*driver* :h2)
-        [:field field-id {::add/source-table (mt/id table-key)
-                          ::add/source-alias field-name
-                          ::add/desired-alias field-name}])))))
+        (if (isa? driver/hierarchy driver/*driver* :sql-mbql5)
+          [:field {::add/source-table (mt/id table-key)
+                   ::add/source-alias field-name
+                   ::add/desired-alias field-name} field-id]
+          [:field field-id {::add/source-table (mt/id table-key)
+                            ::add/source-alias field-name
+                            ::add/desired-alias field-name}]))))))
 
 (defn- venues-category-mbql-gtap-def []
   {:query (mt/mbql-query venues)
