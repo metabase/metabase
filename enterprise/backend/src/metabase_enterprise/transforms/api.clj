2c459fcade17fc8b5cee1d71478aca02e9c6ae4a
Ngoc Khuat <qn.khuat@gmail.com>
2026-03-19 17:14:31 +0900

    Move inspector to top-level transforms_inspector module, mirroring OSS pattern

--- enterprise/backend/src/metabase_enterprise/transforms/api.clj
+++ enterprise/backend/src/metabase_enterprise/transforms/api.clj
@@ -1,7 +1,7 @@
 (ns metabase-enterprise.transforms.api
   "EE API routes for transforms, served under `/api/ee/transforms`."
   (:require
-   [metabase-enterprise.transforms.inspector.api :as inspector.api]))
+   [metabase-enterprise.transforms-inspector.api :as inspector.api]))
 
 (def ^{:arglists '([request respond raise])} routes
   "`/api/ee/transforms` routes."
