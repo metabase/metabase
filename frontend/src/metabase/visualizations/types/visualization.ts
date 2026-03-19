32cf9b372cbed51ecf6eb5330ac1a3d35c70ee32
Kamil Mielnik <kamil@kamilmielnik.com>
2026-03-19 19:55:41 +0700

    Fix

--- frontend/src/metabase/visualizations/types/visualization.ts
+++ frontend/src/metabase/visualizations/types/visualization.ts
@@ -277,7 +277,6 @@ export type ColumnSettingDefinition<TValue, TProps = unknown> = {
   title?: string;
   hint?: string;
   widget?: string | ComponentType<any>;
-  default?: TValue;
   props?: TProps;
   inline?: boolean;
   readDependencies?: string[];
