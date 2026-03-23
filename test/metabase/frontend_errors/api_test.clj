5eee81e56a5a8c310be7de7fefc46fbc70720988
Chris Truter <chris@metabase.com>
2026-03-23 15:05:11 +0200

    Fix FE metric tests

--- test/metabase/frontend_errors/api_test.clj
+++ test/metabase/frontend_errors/api_test.clj
@@ -7,8 +7,7 @@
   (testing "POST /api/frontend-errors increments the counter for component-crash and returns 204"
     (mt/with-prometheus-system! [_ system]
       (let [initial (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
-        (is (= {:status 204, :body nil}
-               (mt/user-http-request :rasta :post 204 "frontend-errors" {:type "component-crash"})))
+        (is (nil? (mt/user-http-request :rasta :post 204 "frontend-errors" {:type "component-crash"})))
         (is (< initial (mt/metric-value system :metabase-frontend/errors {:type "component-crash"}))))))
 
   (testing "POST /api/frontend-errors with type=chart-render-error tracks separately"
@@ -18,5 +17,6 @@
         (is (< initial (mt/metric-value system :metabase-frontend/errors {:type "chart-render-error"}))))))
 
   (testing "POST /api/frontend-errors rejects unknown type values"
-    (is (= "value may be: chart-render-error, component-crash"
-           (mt/user-http-request :rasta :post 400 "frontend-errors" {:type "bogus"})))))
+    (is (= {:errors {:type "enum of component-crash, chart-render-error"}}
+           (select-keys (mt/user-http-request :rasta :post 400 "frontend-errors" {:type "bogus"})
+                        [:errors])))))
