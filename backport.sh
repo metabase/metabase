187c4ba0bd0e00b141e84b89d630ba0f592275ce
Ryan Laurie <iethree@gmail.com>
2026-03-23 14:53:19 -0600

    remove backport.sh

--- backport.sh
+++
@@ -1,4 +0,0 @@
-git reset HEAD~1
-rm ./backport.sh
-git cherry-pick 88a87db2b8d2ca246c25a036517b09a81b0810c8
-echo 'Resolve conflicts and force push this branch.\n\nTo backport translations run: bin/i18n/merge-translations <release-branch>'
