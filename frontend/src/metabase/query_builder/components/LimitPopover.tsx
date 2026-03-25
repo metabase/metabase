12de0dcc5e15f54df33ff86c4fc1ed3bf6ea6b6e
Nick Fitzpatrick <nickfitz.582@gmail.com>
2026-03-25 20:40:20 +0530

    update limit popover components to mantine

--- frontend/src/metabase/query_builder/components/LimitPopover.tsx
+++ frontend/src/metabase/query_builder/components/LimitPopover.tsx
@@ -2,10 +2,9 @@ import cx from "classnames";
 import { type KeyboardEvent, useState } from "react";
 import { t } from "ttag";
 
-import { Radio } from "metabase/common/components/Radio";
 import CS from "metabase/css/core/index.css";
 import { formatNumber } from "metabase/lib/formatting";
-import { LimitInput } from "metabase/querying/components/LimitInput";
+import { NumberInput, Radio, Stack } from "metabase/ui";
 import { HARD_ROW_LIMIT } from "metabase-lib/v1/queries/utils";
 
 interface CustomRowLimitProps {
@@ -28,18 +27,16 @@ const CustomRowLimit = ({
   const exceedsMax = !isNaN(numericValue) && numericValue > maxRowLimit;
 
   return (
-    <LimitInput
-      small
+    <NumberInput
+      size="sm"
       value={inputValue}
       className={cx({
         [cx(CS.textBrand, CS.borderBrand)]: limit != null && !exceedsMax,
       })}
-      style={exceedsMax ? { borderColor: "var(--mb-color-error)" } : undefined}
+      error={exceedsMax && t`Value exceeds maximum of ${maxRowLimit}`}
       placeholder={t`Pick a limit`}
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-        setInputValue(e.target.value)
-      }
-      onKeyPress={(e: KeyboardEvent<HTMLInputElement>) => {
+      onChange={(value: number | string) => setInputValue(value.toString())}
+      onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
         if (e.nativeEvent.isComposing) {
           return;
         }
@@ -80,31 +77,37 @@ export const LimitPopover = ({
 
   return (
     <div className={cx(className, CS.textBold, CS.textMedium)}>
-      <Radio
-        vertical
+      <Radio.Group
         value={limit == null ? "maximum" : "custom"}
-        options={[
-          {
-            name: t`Show maximum (first ${formatNumber(HARD_ROW_LIMIT)})`,
-            value: "maximum",
-          },
-          {
-            name: (
+        onChange={(value: string) =>
+          value === "maximum"
+            ? onChangeLimit(null)
+            : onChangeLimit(effectiveMaxLimit)
+        }
+      >
+        <Stack>
+          <Radio
+            value="maximum"
+            label={t`Show maximum (first ${formatNumber(effectiveMaxLimit)})`}
+          />
+          <Radio
+            value="custom"
+            label={
               <CustomRowLimit
-                key={limit == null ? "a" : "b"}
                 limit={limit}
                 onChangeLimit={onChangeLimit}
                 onClose={onClose}
                 maxRowLimit={effectiveMaxLimit}
               />
-            ),
-            value: "custom",
-          },
-        ]}
-        onChange={(value: string) =>
-          value === "maximum" ? onChangeLimit(null) : onChangeLimit(2000)
-        }
-      />
+            }
+            styles={{
+              inner: {
+                marginTop: "0.5rem",
+              },
+            }}
+          />
+        </Stack>
+      </Radio.Group>
     </div>
   );
 };
