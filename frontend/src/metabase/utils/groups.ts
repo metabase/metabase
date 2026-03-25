79953dc48453b84e62da251f38c83df04beabc76
Romeo Van Snick <romeo@romeovansnick.be>
2026-03-25 18:01:47 +1100

    Remove need for eslint comments

--- frontend/src/metabase/utils/groups.ts
+++ frontend/src/metabase/utils/groups.ts
@@ -4,16 +4,12 @@ import type { SpecialGroupType } from "metabase/admin/permissions/types";
 import { PLUGIN_TENANTS } from "metabase/plugins";
 import type { GroupInfo } from "metabase-types/api";
 
-const SPECIAL_GROUP_NAMES = new Map([
-  // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
-  ["All Users", t`All Users`],
-  // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
-  ["Administrators", t`Administrators`],
-  // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
-  ["All tenant users", t`All tenant users`],
-  // eslint-disable-next-line ttag/no-module-declaration -- see metabase#55045
-  ["Data Analysts", t`Data Analysts`],
-]);
+const SPECIAL_GROUP_NAMES: Record<string, () => string> = {
+  "All Users": () => t`All Users`,
+  Administrators: () => t`Administrators`,
+  "All tenant users": () => t`All tenant users`,
+  "Data Analysts": () => t`Data Analysts`,
+};
 
 export function isDefaultGroup(group: Pick<GroupInfo, "magic_group_type">) {
   return group.magic_group_type === "all-internal-users";
@@ -52,7 +48,8 @@ export function canEditMembership(group: Pick<GroupInfo, "magic_group_type">) {
 }
 
 export function getGroupNameLocalized(group: Pick<GroupInfo, "name">) {
-  return SPECIAL_GROUP_NAMES.get(group.name) ?? group.name;
+  const specialName = SPECIAL_GROUP_NAMES[group.name];
+  return specialName ? specialName() : group.name;
 }
 
 export function getGroupSortOrder(group: Pick<GroupInfo, "magic_group_type">) {
