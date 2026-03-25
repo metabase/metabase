632a4f44a18666af30c829eab3d5576f1428e3bb
Egor Grushin <egor@metabase.com>
2026-03-25 12:17:28 +0000

    add bail feature to the e2e stress test flake workflow

--- e2e/support/cypress-stress-test.config.js
+++ e2e/support/cypress-stress-test.config.js
@@ -5,7 +5,9 @@ const {
   component: embeddingSdkComponentTestConfig,
 } = require("./cypress-embedding-sdk-component-test.config");
 
+const bail = process.env.BAIL === "true" ? 1 : 0;
+
 module.exports = defineConfig({
-  e2e: { ...defaultConfig, retries: 0 },
-  component: { ...embeddingSdkComponentTestConfig, retries: 0 },
+  e2e: { ...defaultConfig, retries: 0, bail },
+  component: { ...embeddingSdkComponentTestConfig, retries: 0, bail },
 });
