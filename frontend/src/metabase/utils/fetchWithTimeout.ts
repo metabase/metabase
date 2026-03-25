cc0502f81ab1d1d8ecda4813972d82f54edbff79
Romeo Van Snick <romeo@romeovansnick.be>
2026-03-25 16:21:33 +1100

    Clean up fetchWithTimeout

--- frontend/src/metabase/utils/fetchWithTimeout.ts
+++ frontend/src/metabase/utils/fetchWithTimeout.ts
@@ -1,28 +1,38 @@
 const DEFAULT_TIMEOUT = 7000;
 
-/* wrapper around fetch that allows passing a timeout prop */
-export function fetchWithTimeout(
+type RequestInitWithTimeout = Omit<RequestInit, "signal"> & {
+  timeout?: number;
+};
+
+/**
+ * Fetches a URL with a timeout.
+ *
+ * TODO(romeovs): should use AbortSignal.timeout instead?
+ */
+export async function fetchWithTimeout(
   url: string,
-  options: RequestInit & { timeout?: number } = {},
+  options: RequestInitWithTimeout,
 ) {
   const controller = new AbortController();
   const timeout = setTimeout(() => {
     controller.abort();
   }, options?.timeout || DEFAULT_TIMEOUT);
 
-  return fetch(url, {
-    ...options,
-    signal: controller.signal,
-  })
-    .then((res) => {
-      clearTimeout(timeout);
-      return res;
-    })
-    .catch((err: Error) => {
-      clearTimeout(timeout);
+  try {
+    const response = await fetch(url, {
+      ...options,
+      signal: controller.signal,
+    });
+    return response;
+  } catch (err) {
+    if (err instanceof Error) {
       if (err.name === "AbortError") {
         throw new Error(`Request tp ${url} timed out`);
       }
-      throw new Error(err.message || "error fetching");
-    });
+      throw new Error(err.message);
+    }
+    throw new Error("error fetching");
+  } finally {
+    clearTimeout(timeout);
+  }
 }
