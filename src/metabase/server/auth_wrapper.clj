c4f1202914936fdec1c1622f710ac501c43c35f9
Noah Moss <noahbmoss@gmail.com>
2026-03-26 16:25:18 -0400

    simplify

--- src/metabase/server/auth_wrapper.clj
+++ src/metabase/server/auth_wrapper.clj
@@ -2,35 +2,13 @@
   (:require
    [metabase.api.util.handlers :as handlers]
    [metabase.config.core :as config]
-   [metabase.sso.api.slack-connect :as slack-connect.api]
-   [ring.util.response :as response]))
-
-(let [bad-req (response/bad-request {:message "The auth/sso endpoint only exists in enterprise builds"
-                                     :status "ee-build-required"})]
-  (defn- not-enabled
-    [_req respond _raise]
-    (respond bad-req)))
-
-(def ^{:arglists '([request respond raise])} ee-missing-routes
-  "Ring routes for auth (SAML) API endpoints.
-   Slack Connect routes are always available (OSS). Other SSO routes require EE."
-  ;; follows the same form as [[metabase-enterprise.sso.api.routes]]. Compojure is a bit opaque so need to manually keep
-  ;; them in sync.
-  (handlers/route-map-handler
-   {"/auth" {"/sso"  (handlers/route-map-handler
-                      {"/slack-connect" slack-connect.api/routes
-                       "/"              not-enabled})}
-    "/api"  {"/saml" not-enabled
-             "/ee"   {"/sso" {"/oidc" not-enabled}}}}))
+   [metabase.sso.api.slack-connect :as slack-connect.api]))
 
 ;; This needs to be injected into [[metabase.server.routes/routes]] -- not [[metabase.api-routes.core/routes]] !!!
-;;
-;; TODO -- should we make a `metabase-enterprise.routes` namespace where this can live instead of injecting it
-;; directly?
-;;
-;; TODO -- we need to feature-flag this based on the `:sso-` feature flags
 (def routes
-  "Ring routes for auth (SAML) api endpoints. If enterprise is not present, will return a nicer message"
-  (if (and config/ee-available? (not *compile-files*))
-    (requiring-resolve 'metabase-enterprise.sso.api.routes/routes)
-    ee-missing-routes))
+  "Ring routes for auth API endpoints.
+   Slack Connect (OSS) is always available. Other SSO routes (SAML, JWT, OIDC) require EE."
+  (handlers/routes
+   (handlers/route-map-handler {"/auth" {"/sso" {"/slack-connect" slack-connect.api/routes}}})
+   (when (and config/ee-available? (not *compile-files*))
+     (requiring-resolve 'metabase-enterprise.sso.api.routes/routes))))
