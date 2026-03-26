ac1222735d200ba90980380d1eb58d736ed5d214
Romeo Van Snick <romeo@romeovansnick.be>
2026-03-26 12:31:09 +1100

    Move sortObject into metabase/utils/object

--- frontend/src/metabase/utils/objects.ts
+++ frontend/src/metabase/utils/objects.ts
@@ -1,5 +1,3 @@
-import { sortObject } from "metabase-lib/v1/utils";
-
 export const getObjectEntries = <K extends string, V>(
   obj: Record<K, V>,
 ): [K, V][] => {
@@ -17,6 +15,32 @@ export const objectFromEntries = <K extends string, V>(
 ): Record<K, V> => {
   return Object.fromEntries(entries) as Record<K, V>;
 };
+
+// `sortObject` copies objects for deterministic serialization.
+// Objects that have equal keys and values don't necessarily serialize to the
+// same string. JSON.stringify prints properties in inserted order. This function
+// sorts keys before adding them to the duplicated object to ensure consistent
+// serialization.
+export function sortObject(obj: any | any[]): any | any[] {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(sortObject);
+  }
+
+  const sortedKeyValues = Object.entries(obj).sort(([keyA], [keyB]) =>
+    keyA.localeCompare(keyB),
+  );
+  const o: Record<string, any> = {};
+
+  for (const [k, v] of sortedKeyValues) {
+    o[k] = sortObject(v);
+  }
+
+  return o;
+}
 
 // Stringify with sorted keys to ensure stable orders.
 export const stableStringify = <T>(obj: T): string =>
--- frontend/src/metabase/utils/objects.unit.spec.ts
+++ frontend/src/metabase/utils/objects.unit.spec.ts
@@ -1,4 +1,4 @@
-import { isSerializable } from "./objects";
+import { isSerializable, sortObject } from "./objects";
 
 describe("isSerializable", () => {
   describe("primitive values", () => {
@@ -245,5 +245,37 @@ describe("isSerializable", () => {
         }),
       ).toBe(false);
     });
+  });
+});
+
+describe("sortObject", () => {
+  it("should serialize identically regardless of property creation order", () => {
+    const o1: Record<string, number> = {};
+    o1.a = 1;
+    o1.b = 2;
+    const o2: Record<string, number> = {};
+    o2.b = 2;
+    o2.a = 1;
+
+    expect(JSON.stringify(sortObject(o1))).toEqual(
+      JSON.stringify(sortObject(o2)),
+    );
+  });
+
+  it("should not sort arrays", () => {
+    expect(sortObject(["a", "c", "b"])).toEqual(["a", "c", "b"]);
+  });
+
+  it("should sort keys recursively", () => {
+    const o1 = { o: {} as Record<string, number> };
+    o1.o.a = 1;
+    o1.o.b = 2;
+    const o2 = { o: {} as Record<string, number> };
+    o2.o.b = 2;
+    o2.o.a = 1;
+
+    expect(JSON.stringify(sortObject(o1))).toEqual(
+      JSON.stringify(sortObject(o2)),
+    );
   });
 });
