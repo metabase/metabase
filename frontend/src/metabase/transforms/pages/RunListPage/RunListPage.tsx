fef8e838ee60c22b2490a027c458f6c36d09ebea
Romeo Van Snick <romeo@romeovansnick.be>
2026-03-27 16:11:24 +1100

    Fix imprts after merge

--- frontend/src/metabase/transforms/pages/RunListPage/RunListPage.tsx
+++ frontend/src/metabase/transforms/pages/RunListPage/RunListPage.tsx
@@ -13,6 +13,7 @@ import {
 import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
 import { DataStudioBreadcrumbs } from "metabase/data-studio/common/components/DataStudioBreadcrumbs";
 import { PaneHeader } from "metabase/data-studio/common/components/PaneHeader";
+import { usePageTitle } from "metabase/hooks/use-page-title";
 import { POLLING_INTERVAL } from "metabase/transforms/constants";
 import { Center, Flex, Stack } from "metabase/ui";
 import { useDispatch } from "metabase/utils/redux";
