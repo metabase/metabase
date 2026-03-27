0cb0c7b964872ee31cd84f917117f97412a5be8f
Brad Anderson <brad@metabase.com>
2026-03-27 15:59:24 -0400

    GRO-281 adds configurable "EnableTransformsCard" to add-basic-cloud-transforms screen

--- enterprise/frontend/src/metabase-enterprise/transforms/upsells/pages/TransformsUpsellPage/TransformsUpsellPage.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/upsells/pages/TransformsUpsellPage/TransformsUpsellPage.tsx
@@ -10,6 +10,7 @@ import { LineDecorator } from "metabase/data-studio/upsells/components/LineDecor
 import type { BillingPeriod } from "metabase/data-studio/upsells/types";
 import { useSelector } from "metabase/lib/redux";
 import { getStoreUsers } from "metabase/selectors/store-users";
+import { EnableTransformsCard } from "metabase/transforms/pages/EnableTransformsPage/EnableTransformsCard";
 import {
   Card,
   Center,
@@ -37,6 +38,11 @@ import { TierSelection, type TransformTier } from "./components/TierSelection";
  * transforms enabled by default.
  */
 export function TransformsUpsellPage() {
+  const [shouldShowAgreement, setShouldShowAgreement] = useState(false);
+  const enableTransforms = () => {
+    setShouldShowAgreement(true);
+  };
+
   const bulletPoints = [
     t`Schedule and run transforms as groups with jobs`,
     t`Fast runs with incremental transforms that respond to data changes`,
@@ -121,60 +127,66 @@ export function TransformsUpsellPage() {
         py="xl"
       >
         <LineDecorator>
-          <Card
-            className={cx(S.container, { [S.singleColumn]: !canUserPurchase })}
-            withBorder
-          >
-            <Stack gap="lg" className={S.leftColumn} p="xl">
-              <Title order={2}>
-                {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins. */}
-                {t`Start transforming your data in Metabase`}
-              </Title>
-              <Text c="text-secondary">
-                {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins. */}
-                {t`Clean up, reshape, and reuse data directly in Metabase. Add transforms to build models your whole team can use.`}
-              </Text>
-              <Stack gap="lg" py="sm">
-                {bulletPoints.map((point) => (
-                  <Flex direction="row" gap="sm" key={point}>
-                    <Center w={24} h={24}>
-                      <Icon name="check_filled" size={16} c="text-brand" />
-                    </Center>
-                    <Text c="text-secondary">{point}</Text>
-                  </Flex>
-                ))}
-              </Stack>
-              {!canUserPurchase && (
-                <Text fw="bold">
-                  {anyStoreUserEmailAddress
-                    ? // eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins.
-                      t`Please ask a Metabase Store Admin (${anyStoreUserEmailAddress}) to enable this for you.`
-                    : // eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins.
-                      t`Please ask a Metabase Store Admin to enable this for you.`}
+          {!shouldShowAgreement ? (
+            <EnableTransformsCard enableTransforms={enableTransforms} />
+          ) : (
+            <Card
+              className={cx(S.container, {
+                [S.singleColumn]: !canUserPurchase,
+              })}
+              withBorder
+            >
+              <Stack gap="lg" className={S.leftColumn} p="xl">
+                <Title order={2}>
+                  {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins. */}
+                  {t`Start transforming your data in Metabase`}
+                </Title>
+                <Text c="text-secondary">
+                  {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins. */}
+                  {t`Clean up, reshape, and reuse data directly in Metabase. Add transforms to build models your whole team can use.`}
                 </Text>
-              )}
-            </Stack>
-            {canUserPurchase && (
-              <>
-                <Divider orientation="vertical" />
-                <Stack gap="lg" className={S.rightColumn} p="xl">
-                  <Title order={3}>{rightColumnTitle}</Title>
-                  <TierSelection
-                    advancedTransformsPrice={advancedTransformsPrice}
-                    basicTransformsPrice={basicTransformsPrice}
-                    billingPeriod={billingPeriod}
-                    selectedTier={selectedTier}
-                    setSelectedTier={setSelectedTier}
-                  />
-                  <PricingSummary
-                    dueTodayAmount={dueTodayAmount}
-                    isOnTrial={isOnTrial}
-                    selectedTier={selectedTier}
-                  />
+                <Stack gap="lg" py="sm">
+                  {bulletPoints.map((point) => (
+                    <Flex direction="row" gap="sm" key={point}>
+                      <Center w={24} h={24}>
+                        <Icon name="check_filled" size={16} c="text-brand" />
+                      </Center>
+                      <Text c="text-secondary">{point}</Text>
+                    </Flex>
+                  ))}
                 </Stack>
-              </>
-            )}
-          </Card>
+                {!canUserPurchase && (
+                  <Text fw="bold">
+                    {anyStoreUserEmailAddress
+                      ? // eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins.
+                        t`Please ask a Metabase Store Admin (${anyStoreUserEmailAddress}) to enable this for you.`
+                      : // eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins.
+                        t`Please ask a Metabase Store Admin to enable this for you.`}
+                  </Text>
+                )}
+              </Stack>
+              {canUserPurchase && (
+                <>
+                  <Divider orientation="vertical" />
+                  <Stack gap="lg" className={S.rightColumn} p="xl">
+                    <Title order={3}>{rightColumnTitle}</Title>
+                    <TierSelection
+                      advancedTransformsPrice={advancedTransformsPrice}
+                      basicTransformsPrice={basicTransformsPrice}
+                      billingPeriod={billingPeriod}
+                      selectedTier={selectedTier}
+                      setSelectedTier={setSelectedTier}
+                    />
+                    <PricingSummary
+                      dueTodayAmount={dueTodayAmount}
+                      isOnTrial={isOnTrial}
+                      selectedTier={selectedTier}
+                    />
+                  </Stack>
+                </>
+              )}
+            </Card>
+          )}
         </LineDecorator>
       </Flex>
     </DottedBackground>
