09a0fe1ad862e42979aae12bc662c1402b20852b
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-03-27 13:11:24 -0400

    fix tests

--- src/metabase/segments/models/segment.clj
+++ src/metabase/segments/models/segment.clj
@@ -230,12 +230,13 @@
         (concat ["segments" (serdes/storage-leaf-file-name id label)]))))
 
 (defmethod serdes/make-spec "Segment" [_model-name _opts]
-  {:copy [:name :points_of_interest :archived :caveats :description :entity_id :show_in_getting_started]
-   :skip [:dependency_analysis_version]
+  {:copy      [:name :points_of_interest :archived :caveats :description :entity_id :show_in_getting_started]
+   :skip      [:dependency_analysis_version]
    :transform {:created_at (serdes/date)
-               :table_id (serdes/fk :model/Table)
+               :table_id   (serdes/fk :model/Table)
                :creator_id (serdes/fk :model/User)
-               :definition {:export serdes/export-mbql :import serdes/import-mbql}}})
+               :definition {:export serdes/export-mbql :import serdes/import-mbql}}
+   :default-values {:archived false :show_in_getting_started false}})
 
 ;;;; ------------------------------------------------- Search ----------------------------------------------------------
 
