1574a28b4f51f3e84b85ca269a3eff1f1c3f4979
Nathan Voxland <nathan@metabase.com>
2026-03-28 01:26:44 -0500

    Fixed tests

--- src/metabase/mq/init.clj
+++ src/metabase/mq/init.clj
@@ -1,6 +1,7 @@
 (ns metabase.mq.init
   "Initializes the mq subsystem at startup."
   (:require
+   [metabase.config.core :as config]
    [metabase.mq.impl :as mq.impl]
    [metabase.mq.listener :as listener]
    [metabase.mq.publish-buffer :as publish-buffer]
@@ -17,21 +18,29 @@
   #{:topic.backend/appdb :topic.backend/memory})
 
 (defmethod startup/def-startup-logic! ::MqInit [_]
-  (let [queue-be (keyword "queue.backend" (mq.settings/queue-backend))
-        topic-be (keyword "topic.backend" (mq.settings/topic-backend))]
-    (when-not (contains? valid-queue-backends queue-be)
-      (throw (ex-info (str "Invalid queue backend: " queue-be
-                           ". Valid backends: " valid-queue-backends)
-                      {:backend queue-be :valid valid-queue-backends})))
-    (when-not (contains? valid-topic-backends topic-be)
-      (throw (ex-info (str "Invalid topic backend: " topic-be
-                           ". Valid backends: " valid-topic-backends)
-                      {:backend topic-be :valid valid-topic-backends})))
-    (alter-var-root #'q.backend/*backend* (constantly queue-be))
-    (alter-var-root #'topic.backend/*backend* (constantly topic-be))
-    (log/infof "Queue backend set to %s" queue-be)
-    (log/infof "Topic backend set to %s" topic-be)
-    (listener/register-listeners!)
-    (publish-buffer/start-publish-buffer-flush!)
-    (mq.impl/start-worker-pool!)
-    (mq.impl/start-transports)))
+  (if config/is-test?
+    ;; In tests, use the sync backend (inline processing, no background threads).
+    ;; Tests that need MQ use `with-sync-mq`. Appdb-specific tests call backend methods directly.
+    (do
+      (alter-var-root #'q.backend/*backend* (constantly :queue.backend/sync))
+      (alter-var-root #'topic.backend/*backend* (constantly :topic.backend/sync))
+      (listener/register-listeners!))
+    ;; In production, use configured backends with background polling.
+    (let [queue-be (keyword "queue.backend" (mq.settings/queue-backend))
+          topic-be (keyword "topic.backend" (mq.settings/topic-backend))]
+      (when-not (contains? valid-queue-backends queue-be)
+        (throw (ex-info (str "Invalid queue backend: " queue-be
+                             ". Valid backends: " valid-queue-backends)
+                        {:backend queue-be :valid valid-queue-backends})))
+      (when-not (contains? valid-topic-backends topic-be)
+        (throw (ex-info (str "Invalid topic backend: " topic-be
+                             ". Valid backends: " valid-topic-backends)
+                        {:backend topic-be :valid valid-topic-backends})))
+      (alter-var-root #'q.backend/*backend* (constantly queue-be))
+      (alter-var-root #'topic.backend/*backend* (constantly topic-be))
+      (log/infof "Queue backend set to %s" queue-be)
+      (log/infof "Topic backend set to %s" topic-be)
+      (listener/register-listeners!)
+      (publish-buffer/start-publish-buffer-flush!)
+      (mq.impl/start-worker-pool!)
+      (mq.impl/start-transports))))
