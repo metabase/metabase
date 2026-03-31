6b153e7fb0a7e1f2566c3fb89f65a014ef02fd38
Eric Normand <ericwnormand@gmail.com>
2026-03-30 19:31:21 -0500

    Bind *param-values-query* in card param values endpoints
    The cherry-picked commit was missing the backend permission changes:
    - Bind *param-values-query* in card param values endpoints (matching dashboards)
    - Skip api/read-check on Field in search-values-from-field-id when
      *param-values-query* is true

--- src/metabase/parameters/field.clj
+++ src/metabase/parameters/field.clj
@@ -4,6 +4,7 @@
    [metabase.api.common :as api]
    [metabase.lib.schema.id :as lib.schema.id]
    [metabase.parameters.chain-filter :as chain-filter]
+   [metabase.query-processor.middleware.permissions :as qp.perms]
    [metabase.parameters.field-values :as params.field-values]
    [metabase.parameters.field.search-values-query :as search-values-query]
    [metabase.util.log :as log]
@@ -78,7 +79,9 @@
   "Search for values of a field given by `field-id` that contain `query`."
   [field-id     :- ::lib.schema.id/field
    query-string :- [:maybe :string]]
-  (let [field        (api/read-check (t2/select-one :model/Field :id field-id))
+  (let [field        (if qp.perms/*param-values-query*
+                       (t2/select-one :model/Field :id field-id)
+                       (api/read-check (t2/select-one :model/Field :id field-id)))
         search-field (or (some->> (chain-filter/remapped-field-id field-id)
                                   (t2/select-one :model/Field :id))
                          field)]
