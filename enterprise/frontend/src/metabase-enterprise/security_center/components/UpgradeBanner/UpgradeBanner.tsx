a05b7e979ffa32ddb21cad3606e840d4fe846fe3
Sébastien <sebastien@metabase.com>
2026-04-01 13:59:08 +0200

    upgrade banner (#71800)

---
+++ enterprise/frontend/src/metabase-enterprise/security_center/components/UpgradeBanner/UpgradeBanner.tsx
@@ -0,0 +1,40 @@
+import { c, t } from "ttag";
+
+import { ExternalLink } from "metabase/common/components/ExternalLink";
+import { Anchor, Group, Icon, Text } from "metabase/ui";
+
+import S from "./UpgradeBanner.module.css";
+
+const UPGRADE_DOCS_URL =
+  // eslint-disable-next-line metabase/no-unconditional-metabase-links-render -- This component only renders inside the admin-only Security Center page.
+  "https://www.metabase.com/docs/latest/installation-and-operation/upgrading-metabase";
+
+interface UpgradeBannerProps {
+  targetVersion: string;
+}
+
+export function UpgradeBanner({ targetVersion }: UpgradeBannerProps) {
+  return (
+    <Group
+      className={S.root}
+      gap="sm"
+      wrap="nowrap"
+      data-testid="upgrade-banner"
+    >
+      <Icon name="warning" className={S.icon} />
+      <Text fw="bold" size="md" className={S.text}>
+        {c("{0} is a version number like v0.59.4")
+          .t`A security update is available. Update to ${targetVersion} or later to resolve known issues.`}
+      </Text>
+      <Anchor
+        component={ExternalLink}
+        href={UPGRADE_DOCS_URL}
+        className={S.link}
+        fw="bold"
+        size="md"
+      >
+        {t`View upgrade instructions`}
+      </Anchor>
+    </Group>
+  );
+}
