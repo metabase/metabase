9680f19e8ff1d1cf5869868516071c65359753ac
Eric Normand <ericwnormand@gmail.com>
2026-04-01 15:46:19 -0500

    Fix race condition in data-studio sync tests
    Move latch await inside with-redefs so sync/sync-table! and
    sync/update-field-values-for-table! are still redef'd when the
    quick-task executor runs the queued tasks.

--- enterprise/backend/test/metabase_enterprise/data_studio/api/table_test.clj
+++ enterprise/backend/test/metabase_enterprise/data_studio/api/table_test.clj
@@ -124,10 +124,10 @@
                                            nil)]
             (mt/user-http-request :crowberto :post 204 "ee/data-studio/table/sync-schema" {:database_ids [d1],
                                                                                            :schema_ids   [(format "%d:FOO" d2)]
-                                                                                           :table_ids    [t4]}))
-          (testing "sync called?"
-            (is (true? (.await latch 4 TimeUnit/SECONDS)))
-            (is (= [t1 t2 t4 t5] (map :id @tables)))))))))
+                                                                                           :table_ids    [t4]})
+            (testing "sync called?"
+              (is (true? (.await latch 4 TimeUnit/SECONDS)))
+              (is (= [t1 t2 t4 t5] (map :id @tables))))))))))
 
 (deftest ^:parallel non-admins-cant-trigger-bulk-rescan-values-test
   (mt/with-premium-features #{:data-studio}
@@ -154,10 +154,10 @@
                                                               nil)]
             (mt/user-http-request :crowberto :post 204 "ee/data-studio/table/rescan-values" {:database_ids [d1],
                                                                                              :schema_ids   [(format "%d:FOO" d2)]
-                                                                                             :table_ids    [t4]}))
-          (testing "rescanned?"
-            (is (true? (.await latch 4 TimeUnit/SECONDS)))
-            (is (= [t1 t2 t4 t5] (map :id @tables)))))))))
+                                                                                             :table_ids    [t4]})
+            (testing "rescanned?"
+              (is (true? (.await latch 4 TimeUnit/SECONDS)))
+              (is (= [t1 t2 t4 t5] (map :id @tables))))))))))
 
 (deftest ^:parallel non-admins-cant-trigger-bulk-discard-values-test
   (mt/with-premium-features #{:data-studio}
