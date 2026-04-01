01cba07964b6badea3ef66cbaf4844cffb31e108
Riley Thompson <rileythompson99@gmail.com>
2026-04-01 14:11:56 -0400

    use regex

--- e2e/test/scenarios/actions/model-actions.cy.spec.js
+++ e2e/test/scenarios/actions/model-actions.cy.spec.js
@@ -773,7 +773,7 @@ describe(
           cy.button(SAMPLE_QUERY_ACTION.name).click();
 
           cy.findByText(
-            "Error executing Action: Invalid impersonated native query. Must be a single select statement.",
+            /Error executing Action:.*Invalid impersonated native query\. Must be a single select statement\./,
           );
         });
 
