faeba14acc3dc35ed535e1d36b78f50ab861fc49
Sébastien <sebastien@metabase.com>
2026-04-02 08:53:23 +0200

    no longer calling pulse/form_input in guest embeds

--- frontend/src/embedding-sdk-bundle/components/public/notifications/QuestionAlertsButton.tsx
+++ frontend/src/embedding-sdk-bundle/components/public/notifications/QuestionAlertsButton.tsx
@@ -1,5 +1,13 @@
 import { PLUGIN_NOTIFICATIONS_SDK } from "embedding-sdk-bundle/components/public/notifications";
+import { useSdkSelector } from "embedding-sdk-bundle/store";
+import { getIsGuestEmbed } from "embedding-sdk-bundle/store/selectors";
 
 export function QuestionAlertsButton() {
+  const isGuestEmbed = useSdkSelector(getIsGuestEmbed);
+
+  if (isGuestEmbed) {
+    return null;
+  }
+
   return <PLUGIN_NOTIFICATIONS_SDK.QuestionAlertsButton />;
 }
