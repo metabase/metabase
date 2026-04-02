06b56c53480256aadc1a6191dbb28243410cc095
Sébastien <sebastien@metabase.com>
2026-04-02 09:08:43 +0200

    better

--- frontend/src/embedding-sdk-bundle/components/public/notifications/QuestionAlertsButton.tsx
+++ frontend/src/embedding-sdk-bundle/components/public/notifications/QuestionAlertsButton.tsx
@@ -1,13 +1,5 @@
 import { PLUGIN_NOTIFICATIONS_SDK } from "embedding-sdk-bundle/components/public/notifications";
-import { useSdkSelector } from "embedding-sdk-bundle/store";
-import { getIsGuestEmbed } from "embedding-sdk-bundle/store/selectors";
 
 export function QuestionAlertsButton() {
-  const isGuestEmbed = useSdkSelector(getIsGuestEmbed);
-
-  if (isGuestEmbed) {
-    return null;
-  }
-
   return <PLUGIN_NOTIFICATIONS_SDK.QuestionAlertsButton />;
 }
