b8ee6c4db61eab748e6ea47f09219790d2a83f97
Nicola Mometto <nicola.mometto@metabase.com>
2026-04-06 10:40:16 +0100

    tests

--- enterprise/backend/test/metabase_enterprise/api/session_test.clj
+++ enterprise/backend/test/metabase_enterprise/api/session_test.clj
@@ -56,7 +56,7 @@
                               :cloud-custom-smtp
                               :workspaces
                               :writable-connection}
-    (is (= {:admin_security_center          true
+    (is (= {:admin_security_center          false ;; requires self-hosted (non-cloud)
             :advanced_permissions           true
             :ai_controls                    true
             :attached_dwh                   true
@@ -106,4 +106,10 @@
             :dependencies                   false
             :workspaces                     true
             :writable_connection            true}
-           (:token-features (mt/user-http-request :crowberto :get 200 "session/properties"))))))
+           (:token-features (mt/user-http-request :crowberto :get 200 "session/properties")))))
+
+(deftest security-center-token-feature-test
+  (testing "admin_security_center is true for self-hosted with the feature flag"
+    (mt/with-premium-features #{:admin-security-center}
+      (is (true? (:admin_security_center
+                  (:token-features (mt/user-http-request :crowberto :get 200 "session/properties")))))))))
