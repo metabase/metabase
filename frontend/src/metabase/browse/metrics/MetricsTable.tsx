4ecc4e2513a4576b9e7476c77fadfc62facb18c5
Nick Fitzpatrick <nickfitz.582@gmail.com>
2026-04-08 21:30:25 +0530

    re-adding comment since it's relevant again

--- frontend/src/metabase/browse/metrics/MetricsTable.tsx
+++ frontend/src/metabase/browse/metrics/MetricsTable.tsx
@@ -188,6 +188,9 @@ function MetricRow({ metric }: { metric?: MetricResult }) {
       const url = Urls.metric({ id, name, type: "metric" });
       const subpathSafeUrl = Urls.getSubpathSafeUrl(url);
 
+      // TODO: metabase/metabse#47713
+      // trackMetricClick(metric.id);
+
       event.preventDefault();
       event.stopPropagation();
 
