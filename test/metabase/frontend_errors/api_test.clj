7bc13eaa46f00cc7cb49d53f454b8d5c1acb07ed
Chris Truter <chris@metabase.com>
2026-04-09 17:58:06 +0200

    Add no-cookie coverage for frontend error throttling

--- test/metabase/frontend_errors/api_test.clj
+++ test/metabase/frontend_errors/api_test.clj
@@ -69,6 +69,24 @@
               (is (= (inc initial-count) count-after-throttling))
               (is (str/starts-with? (get-in resp [:body :error]) "Too many attempts!")))))))
 
+    (testing "POST /api/frontend-errors throttles repeated requests from the same IP even without a browser cookie"
+      (mt/with-prometheus-system! [_ system]
+        (with-redefs [frontend-errors.api/frontend-errors-throttler
+                      (throttle/make-throttler :frontend-errors :attempts-threshold 1)
+                      request.settings/source-address-header
+                      (constantly "x-forwarded-for")]
+          (let [request-options {:request-options {:headers {"x-forwarded-for" "10.1.2.3"}}}
+                initial-count   (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
+            (is (nil? (mt/user-http-request :rasta :post 204 "frontend-errors" request-options
+                                            {:type "component-crash"})))
+            (let [resp                  (mt/user-http-request-full-response :crowberto :post 429 "frontend-errors"
+                                                                            request-options
+                                                                            {:type "component-crash"})
+                  count-after-throttling (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
+              (is (= (inc initial-count) count-after-throttling))
+              (is (str/starts-with? (get-in resp [:body :error]) "Too many attempts!"))
+              (is (string? (get-in resp [:headers "Retry-After"]))))))))
+
     (testing "POST /api/frontend-errors throttles repeated invalid payloads before validation"
       (with-redefs [frontend-errors.api/frontend-errors-throttler
                     (throttle/make-throttler :frontend-errors :attempts-threshold 1)
