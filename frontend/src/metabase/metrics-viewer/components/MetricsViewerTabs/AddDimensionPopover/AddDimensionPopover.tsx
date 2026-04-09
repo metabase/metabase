948a1a5ec3478bd20851efcf1a9bf7c23acb173a
Kevin Kelleher <kevin.kelleher@metabase.com>
2026-04-09 10:35:40 -0700

    design improvements

--- frontend/src/metabase/metrics-viewer/components/MetricsViewerTabs/AddDimensionPopover/AddDimensionPopover.tsx
+++ frontend/src/metabase/metrics-viewer/components/MetricsViewerTabs/AddDimensionPopover/AddDimensionPopover.tsx
@@ -91,7 +91,7 @@ export function AddDimensionPopover({
           aria-label={t`Add dimension tab`}
           onClick={() => setIsOpen(true)}
         >
-          <Icon name="add" />
+          <Icon name="add" c="icon-primary" />
         </ActionIcon>
       </Popover.Target>
       <Popover.Dropdown p={0} className={S.dropdown}>
