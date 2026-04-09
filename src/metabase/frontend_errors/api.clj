fa0e9cf72114b6c768491aeee2c64ca23c64862d
Chris Truter <chris@metabase.com>
2026-04-09 17:25:13 +0200

    Throttle frontend errors API

--- src/metabase/frontend_errors/api.clj
+++ src/metabase/frontend_errors/api.clj
@@ -3,10 +3,25 @@
    [metabase.analytics.prometheus :as prometheus]
    [metabase.api.common :as api]
    [metabase.api.macros :as api.macros]
-   [metabase.util.malli.registry :as mr]))
+   [metabase.request.core :as request]
+   [metabase.util.malli.registry :as mr]
+   [metabase.util.throttle :as u.throttle]
+   [throttle.core :as throttle])
+  (:import
+   (clojure.lang ExceptionInfo)))
 
 (mr/def ::frontend-error-type
   [:enum "component-crash" "chart-render-error"])
+
+;; This endpoint is unauthenticated and writes to a Prometheus counter, so it is a
+;; natural target for abuse (flooding to distort metrics or exhaust resources).
+;; Throttle by client IP. The threshold is generous enough to accommodate a page
+;; with many components reporting errors in a burst, but low enough to blunt a
+;; sustained flood from a single source.
+(def ^:private frontend-errors-throttler
+  (throttle/make-throttler :frontend-errors
+                           :attempts-threshold 100
+                           :attempt-ttl-ms     (* 60 1000)))
 
 #_{:clj-kondo/ignore [:metabase/validate-defendpoint-has-response-schema]}
 (api.macros/defendpoint :post "/"
@@ -14,9 +29,16 @@
    with the given `type` label."
   [_route-params
    _query-params
-   {:keys [type]} :- [:map [:type ::frontend-error-type]]]
-  (prometheus/inc! :metabase-frontend/errors {:type type})
-  api/generic-204-no-content)
+   {:keys [type]} :- [:map [:type ::frontend-error-type]]
+   req]
+  (try
+    (throttle/with-throttling [frontend-errors-throttler (request/ip-address req)]
+      (prometheus/inc! :metabase-frontend/errors {:type type})
+      api/generic-204-no-content)
+    (catch ExceptionInfo e
+      (if (u.throttle/throttle-exception? e)
+        (u.throttle/throttle-response e {:error (ex-message e)})
+        (throw e)))))
 
 ;;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ;;; !! Endpoints in this namespace do not currently require auth! Keep this in mind when adding new ones. !!
--- test/metabase/frontend_errors/api_test.clj
+++ test/metabase/frontend_errors/api_test.clj
@@ -1,7 +1,12 @@
 (ns metabase.frontend-errors.api-test
   (:require
+   [clojure.string :as str]
    [clojure.test :refer :all]
-   [metabase.test :as mt]))
+   [metabase.frontend-errors.api :as frontend-errors.api]
+   [metabase.test :as mt]
+   [metabase.test.data.users :as test.users]
+   [metabase.test.http-client :as client]
+   [throttle.core :as throttle]))
 
 (deftest post-frontend-errors-test
   (testing "POST /api/frontend-errors increments the counter for component-crash and returns 204"
@@ -19,4 +24,14 @@
   (testing "POST /api/frontend-errors rejects unknown type values"
     (is (= {:errors {:type "enum of component-crash, chart-render-error"}}
            (select-keys (mt/user-http-request :rasta :post 400 "frontend-errors" {:type "bogus"})
-                        [:errors])))))
+                        [:errors]))))
+
+  (testing "POST /api/frontend-errors is throttled by IP once the threshold is exceeded"
+    (with-redefs [frontend-errors.api/frontend-errors-throttler
+                  (throttle/make-throttler :frontend-errors :attempts-threshold 1)]
+      (mt/user-http-request :rasta :post 204 "frontend-errors" {:type "component-crash"})
+      (let [resp (client/client-full-response (test.users/username->token :rasta)
+                                              :post 429 "frontend-errors"
+                                              {:type "component-crash"})]
+        (is (str/starts-with? (get-in resp [:body :error]) "Too many attempts!"))
+        (is (string? (get-in resp [:headers "Retry-After"])))))))
