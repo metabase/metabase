709871b9e93b9a6f34e6da3d8a0d06b83ca295e6
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-09 15:57:50 +0200

    Rewrite compression module to es module

--- frontend/build/shared/rspack/compression.js
+++ frontend/build/shared/rspack/compression.js
@@ -1,32 +1,31 @@
-const zlib = require("zlib");
+import zlib from "zlib";
 
 // TODO(romeovs): Use compression-rspack-plugin once the configs are using ES modules
-const CompressionPlugin = require("compression-webpack-plugin");
+// const CompressionPlugin = require("compression-webpack-plugin");
+import CompressionPlugin from "compression-webpack-plugin";
 
-const { IS_DEV_MODE } = require("../constants");
+import { IS_DEV_MODE } from "../constants.js";
 
-if (IS_DEV_MODE) {
-  module.exports.COMPRESSION_CONFIG = [];
-} else {
-  module.exports.COMPRESSION_CONFIG = [
-    new CompressionPlugin({
-      algorithm: "gzip",
-      test: /\.(js|css)$/,
-      filename: "[path][base].gz",
-      compressionOptions: {
-        level: 9,
-      },
-    }),
-    new CompressionPlugin({
-      algorithm: "brotliCompress",
-      test: /\.(js|css)$/,
-      filename: "[path][base].br",
-      compressionOptions: {
-        // @ts-expect-error
-        params: {
-          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+export const COMPRESSION_CONFIG = IS_DEV_MODE
+  ? []
+  : [
+      new CompressionPlugin({
+        algorithm: "gzip",
+        test: /\.(js|css)$/,
+        filename: "[path][base].gz",
+        compressionOptions: {
+          level: 9,
         },
-      },
-    }),
-  ];
-}
+      }),
+      new CompressionPlugin({
+        algorithm: "brotliCompress",
+        test: /\.(js|css)$/,
+        filename: "[path][base].br",
+        compressionOptions: {
+          // @ts-expect-error
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+      }),
+    ];
