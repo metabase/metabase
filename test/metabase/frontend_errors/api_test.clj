e35858650726258c8be07ab404fce89ba7e51a81
Chris Truter <chris@metabase.com>
2026-04-09 18:16:23 +0200

    Reduce boilerplate in tests

--- test/metabase/frontend_errors/api_test.clj
+++ test/metabase/frontend_errors/api_test.clj
@@ -8,6 +8,26 @@
    [metabase.test :as mt]
    [throttle.core :as throttle]))
 
+(def ^:private throttled-response
+  {:status  429
+   :headers {"Retry-After" string?}
+   :body    {:error #(str/starts-with? % "Too many attempts!")}})
+
+(defn- request-options
+  ([]
+   {:request-options {:headers {"x-forwarded-for" "10.1.2.3"}}})
+  ([device-id]
+   (let [browser-id-cookie-name @#'mw.browser-cookie/browser-id-cookie-name]
+     {:request-options {:headers {"x-forwarded-for" "10.1.2.3"
+                                  "cookie"          (str browser-id-cookie-name "=" device-id)}}})))
+
+(defmacro ^:private with-throttled-frontend-errors [& body]
+  `(with-redefs [frontend-errors.api/frontend-errors-throttler
+                 (throttle/make-throttler :frontend-errors :attempts-threshold 1)
+                 request.settings/source-address-header
+                 (constantly "x-forwarded-for")]
+     ~@body))
+
 (deftest post-frontend-errors-test
   (testing "POST /api/frontend-errors increments the counter for component-crash and returns 204"
     (mt/with-prometheus-system! [_ system]
@@ -28,13 +48,8 @@
 
   (testing "POST /api/frontend-errors is throttled for the same IP once the threshold is exceeded"
     (mt/with-prometheus-system! [_ system]
-      (with-redefs [frontend-errors.api/frontend-errors-throttler
-                    (throttle/make-throttler :frontend-errors :attempts-threshold 1)
-                    request.settings/source-address-header
-                    (constantly "x-forwarded-for")]
-        (let [browser-id-cookie-name @#'mw.browser-cookie/browser-id-cookie-name
-              request-options {:request-options {:headers {"x-forwarded-for" "10.1.2.3"
-                                                           "cookie"          (str browser-id-cookie-name "=device-a")}}}
+      (with-throttled-frontend-errors
+        (let [request-options (request-options "device-a")
               initial-count   (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
           (is (nil? (mt/user-http-request :rasta :post 204 "frontend-errors" request-options
                                           {:type "component-crash"})))
@@ -45,22 +60,13 @@
                 count-after-throttling    (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
             (is (< initial-count count-after-first-request))
             (is (= count-after-first-request count-after-throttling))
-            (is (=? {:status  429
-                     :headers {"Retry-After" string?}
-                     :body    {:error #(str/starts-with? % "Too many attempts!")}}
-                    resp))))))
+            (is (=? throttled-response resp)))))))
 
     (testing "POST /api/frontend-errors throttles requests from the same IP even if the browser ID changes"
       (mt/with-prometheus-system! [_ system]
-        (with-redefs [frontend-errors.api/frontend-errors-throttler
-                      (throttle/make-throttler :frontend-errors :attempts-threshold 1)
-                      request.settings/source-address-header
-                      (constantly "x-forwarded-for")]
-          (let [browser-id-cookie-name @#'mw.browser-cookie/browser-id-cookie-name
-                device-a-opts          {:request-options {:headers {"x-forwarded-for" "10.1.2.3"
-                                                                    "cookie"          (str browser-id-cookie-name "=device-a")}}}
-                device-b-opts          {:request-options {:headers {"x-forwarded-for" "10.1.2.3"
-                                                                    "cookie"          (str browser-id-cookie-name "=device-b")}}}
+        (with-throttled-frontend-errors
+          (let [device-a-opts          (request-options "device-a")
+                device-b-opts          (request-options "device-b")
                 initial-count          (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
             (is (nil? (mt/user-http-request :rasta :post 204 "frontend-errors" device-a-opts
                                             {:type "component-crash"})))
@@ -69,17 +75,12 @@
                                                                              {:type "component-crash"})
                   count-after-throttling   (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
               (is (= (inc initial-count) count-after-throttling))
-              (is (=? {:status 429
-                       :body   {:error #(str/starts-with? % "Too many attempts!")}}
-                      resp)))))))
+              (is (=? throttled-response resp)))))))
 
     (testing "POST /api/frontend-errors throttles repeated requests from the same IP even without a browser cookie"
       (mt/with-prometheus-system! [_ system]
-        (with-redefs [frontend-errors.api/frontend-errors-throttler
-                      (throttle/make-throttler :frontend-errors :attempts-threshold 1)
-                      request.settings/source-address-header
-                      (constantly "x-forwarded-for")]
-          (let [request-options {:request-options {:headers {"x-forwarded-for" "10.1.2.3"}}}
+        (with-throttled-frontend-errors
+          (let [request-options (request-options)
                 initial-count   (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
             (is (nil? (mt/user-http-request :rasta :post 204 "frontend-errors" request-options
                                             {:type "component-crash"})))
@@ -88,19 +89,11 @@
                                                                             {:type "component-crash"})
                   count-after-throttling (mt/metric-value system :metabase-frontend/errors {:type "component-crash"})]
               (is (= (inc initial-count) count-after-throttling))
-              (is (=? {:status  429
-                       :headers {"Retry-After" string?}
-                       :body    {:error #(str/starts-with? % "Too many attempts!")}}
-                      resp)))))))
+              (is (=? throttled-response resp)))))))
 
     (testing "POST /api/frontend-errors throttles repeated invalid payloads before validation"
-      (with-redefs [frontend-errors.api/frontend-errors-throttler
-                    (throttle/make-throttler :frontend-errors :attempts-threshold 1)
-                    request.settings/source-address-header
-                    (constantly "x-forwarded-for")]
-        (let [browser-id-cookie-name @#'mw.browser-cookie/browser-id-cookie-name
-              request-options {:request-options {:headers {"x-forwarded-for" "10.1.2.3"
-                                                           "cookie"          (str browser-id-cookie-name "=device-a")}}}]
+      (with-throttled-frontend-errors
+        (let [request-options (request-options "device-a")]
           (is (= {:errors {:type "enum of component-crash, chart-render-error"}}
                  (select-keys (mt/user-http-request :rasta :post 400 "frontend-errors" request-options
                                                     {:type "bogus"})
@@ -108,7 +101,4 @@
           (let [resp (mt/user-http-request-full-response :crowberto :post 429 "frontend-errors"
                                                          request-options
                                                          {:type "still-bogus"})]
-            (is (=? {:status  429
-                     :headers {"Retry-After" string?}
-                     :body    {:error #(str/starts-with? % "Too many attempts!")}}
-                    resp))))))))
+            (is (=? throttled-response resp))))))))
