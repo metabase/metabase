3a1a7f1e24011e95ea5ae720f618dbb879875fa6
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-11 14:40:41 +0200

    Use static/static-resource handler

--- src/metabase/server/middleware/embedding_sdk_bundle.clj
+++ src/metabase/server/middleware/embedding_sdk_bundle.clj
@@ -3,44 +3,40 @@
   (:require
    [metabase.config.core :as config]
    [metabase.server.lib.etag-cache :as lib.etag-cache]
+   [metabase.server.routes.static :as static]
    [ring.util.response :as response]))
 
 (set! *warn-on-reflection* true)
 
-(def ^:private bundle-resource    "frontend_client/app/embedding-sdk/legacy/embedding-sdk.js")
-(def ^:private bootstrap-resource "frontend_client/app/embedding-sdk/chunks/embedding-sdk.js")
-(def ^:private content-type    "application/javascript; charset=UTF-8")
-(def ^:private default-cache-header      {"Cache-Control" "public, max-age=60"})
-(def ^:private far-future-cache-header   {"Cache-Control" "public, max-age=31536000, immutable"})
-(def ^:private no-store-cache-header     {"Cache-Control" "no-store"})
-(def ^:private vary-accept-encoding-header {"Vary" "Accept-Encoding"})
+(def ^:private bundle-resource          "frontend_client/app/embedding-sdk/legacy/embedding-sdk.js")
+(def ^:private bootstrap-resource       "frontend_client/app/embedding-sdk/chunks/embedding-sdk.js")
+(def ^:private content-type             "application/javascript; charset=UTF-8")
+(def ^:private default-cache-header     "public, max-age=60")
+(def ^:private far-future-cache-header  "public, max-age=31536000, immutable")
+(def ^:private no-store-cache-header    "no-store")
 
 (defn- not-found []
   (-> (response/not-found "Not found")
       (update :headers merge no-store-cache-header)))
 
-(defn- serve-for-dev [base]
-  (-> base
-      (response/content-type content-type)
-      (update :headers merge no-store-cache-header)))
+(defn- serve-for-dev [response _request]
+  (cond-> response
+    (assoc-in [:headers "Cache-Control"] no-store-cache-header)))
 
-(defn- serve-for-prod [base request]
-  (let [resp (lib.etag-cache/with-etag base request)]
-    (cond-> resp
-      true                    (update :headers merge
-                                      default-cache-header
-                                      vary-accept-encoding-header)
-      (= 200 (:status resp)) (response/content-type content-type))))
+(defn- serve-for-prod [response request]
+  (some-> response
+          (lib.etag-cache/with-etag request)
+          (assoc-in [:headers "Cache-Control"] default-cache-header)))
 
 (defn- serve-resource-handler
   "Serve a JS resource with ETag + 60s caching in prod, no-store in dev."
   [resource]
   (fn [request]
-    (let [base (response/resource-response resource)]
-      (cond
-        (nil? base)       (not-found)
-        config/is-prod?   (serve-for-prod base request)
-        :else             (serve-for-dev base)))))
+    (let [response (static/static-resource request resource)
+          serve    (if config/is-prod? serve-for-prod serve-for-dev)]
+      (or (some-> (static/static-resource request resource)
+                  (serve request))
+          (not-found)))))
 
 (defn serve-bundle-handler
   "Serve /app/embedding-sdk.js.
@@ -68,7 +64,7 @@
   [filename]
   (let [resource (str "frontend_client/app/embedding-sdk/chunks/" filename)]
     (fn [_request]
-      (let [base (response/resource-response resource)]
+      (let [base (static/static-resource request resource)]
         (if (nil? base)
           (not-found)
           (-> base
