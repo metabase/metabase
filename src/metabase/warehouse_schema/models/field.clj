b58eb0d3de0467f8b1722e408e1560548a9e9a6a
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-04-12 22:05:29 -0400

    Revert "fix"
    This reverts commit ef51fed191c8b6dee695db4995c42d893202f4a4.

--- src/metabase/warehouse_schema/models/field.clj
+++ src/metabase/warehouse_schema/models/field.clj
@@ -465,7 +465,7 @@
                :table_id           (serdes/fk :model/Table)
                :fk_target_field_id (serdes/fk :model/Field)
                :parent_id          (serdes/fk :model/Field)
-               :dimensions         (serdes/nested :model/Dimension :field_id (assoc opts :sparse? true))}
+               :dimensions         (serdes/nested :model/Dimension :field_id opts)}
    :defaults {:active                     true
               :database_is_auto_increment false
               :database_required          false
