44bff5a859752729eb6e3621a6aae340b8f09dee
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-13 21:56:52 +0200

    Move metbabase/utils/click-behavior into metabase/dashboard

--- frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/ClickBehaviorSidebar.tsx
+++ frontend/src/metabase/dashboard/components/ClickBehaviorSidebar/ClickBehaviorSidebar.tsx
@@ -8,7 +8,7 @@ import {
   type DashboardContextReturned,
   useDashboardContext,
 } from "metabase/dashboard/context";
-import { isTableDisplay } from "metabase/utils/click-behavior";
+import { isTableDisplay } from "metabase/dashboard/utils";
 import {
   canSaveClickBehavior,
   clickBehaviorIsValid,
