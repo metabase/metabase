50e490de81fe4d01b1e88e8e196ce7e2c01f1796
Nick Fitzpatrick <nick@metabase.com>
2026-04-14 16:11:27 +0530

    Typescript common components (#72288)
    * Entity Menu stuff, Expanding Content
    
    * LastEditInfoLabel, ListItem
    
    * buncha test files
    
    * DebounceFrame, more spec files
    
    * type adjustments

--- frontend/src/metabase/common/components/ListSearchField/ListSearchField.unit.spec.tsx
+++ frontend/src/metabase/common/components/ListSearchField/ListSearchField.unit.spec.tsx
@@ -4,17 +4,17 @@ import { ListSearchField } from "./ListSearchField";
 
 describe("ListSearchField", () => {
   it("should render", async () => {
-    render(<ListSearchField autoFocus type="number" foo />);
+    render(<ListSearchField autoFocus type="number" />);
     expect(screen.getByTestId("list-search-field")).toBeInTheDocument();
   });
 
   it("should have focused the input field", () => {
-    render(<ListSearchField autoFocus type="number" foo />);
+    render(<ListSearchField autoFocus type="number" />);
     expect(screen.getByTestId("list-search-field")).toHaveFocus();
   });
 
   it("should pass through any additional input properties", () => {
-    render(<ListSearchField autoFocus type="number" foo />);
+    render(<ListSearchField autoFocus type="number" />);
     expect(screen.getByTestId("list-search-field")).toHaveAttribute(
       "type",
       "number",
