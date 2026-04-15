7d683c5075cc65a4cff152799fbcad698a0f34ce
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-15 09:29:30 +0200

    Do not silenly hide errors

--- frontend/build/shared/rspack/compression.js
+++ frontend/build/shared/rspack/compression.js
@@ -12,40 +12,36 @@ function getCompressionConfig() {
     return [];
   }
 
-  try {
-    // load these modules conditionally so storybook doesn't choke on them
-    const zlib = require("zlib");
-    const {
-      CompressionRspackPlugin,
-    } = require("./plugins/CompressionRspackPlugin");
+  // load these modules conditionally so storybook doesn't choke on them
+  const zlib = require("zlib");
+  const {
+    CompressionRspackPlugin,
+  } = require("./plugins/CompressionRspackPlugin");
 
-    // eslint-disable-next-line no-console
-    console.log(`[Compression] Compressing assets`);
+  // eslint-disable-next-line no-console
+  console.log(`[Compression] Compressing assets`);
 
-    return [
-      new CompressionRspackPlugin({
-        algorithm: "gzip",
-        test: COMPRESSION_ASSET_TEST,
-        filename: "[path][base].gz",
-        compressionOptions: {
-          level: zlib.constants.Z_MAX_LEVEL,
+  return [
+    new CompressionRspackPlugin({
+      algorithm: "gzip",
+      test: COMPRESSION_ASSET_TEST,
+      filename: "[path][base].gz",
+      compressionOptions: {
+        level: zlib.constants.Z_MAX_LEVEL,
+      },
+    }),
+    new CompressionRspackPlugin({
+      algorithm: "brotliCompress",
+      test: COMPRESSION_ASSET_TEST,
+      filename: "[path][base].br",
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]:
+            zlib.constants.BROTLI_MAX_QUALITY,
         },
-      }),
-      new CompressionRspackPlugin({
-        algorithm: "brotliCompress",
-        test: COMPRESSION_ASSET_TEST,
-        filename: "[path][base].br",
-        compressionOptions: {
-          params: {
-            [zlib.constants.BROTLI_PARAM_QUALITY]:
-              zlib.constants.BROTLI_MAX_QUALITY,
-          },
-        },
-      }),
-    ];
-  } catch (e) {
-    return [];
-  }
+      },
+    }),
+  ];
 }
 
 module.exports.COMPRESSION_CONFIG = getCompressionConfig();
