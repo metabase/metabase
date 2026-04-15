2096f9fd3e81de1379642d65561d31c38d3dbe15
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-15 08:38:36 +0200

    Clarify why the plugin lives in a separate file

--- frontend/build/shared/rspack/plugins/CompressionRspackPlugin.js
+++ frontend/build/shared/rspack/plugins/CompressionRspackPlugin.js
@@ -1,1 +1,3 @@
+// This needs to live in an esm file because the compression-rspack-plugin package
+// does not declare commonjs exports.
 export { CompressionRspackPlugin } from "compression-rspack-plugin";
