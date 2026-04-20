5d9195e760690c1ccf23def0cba0738d37ed24bc
Nicola Mometto <nicola.mometto@metabase.com>
2026-04-20 11:44:09 +0100

    fix: test on ee enabled

--- test/metabase/transforms_base/util_test.clj
+++ test/metabase/transforms_base/util_test.clj
@@ -1,6 +1,7 @@
 (ns metabase.transforms-base.util-test
   (:require
    [clojure.test :refer :all]
+   [metabase.config.core :as config]
    [metabase.test :as mt]
    [metabase.transforms-base.util :as transforms-base.u]))
 
@@ -14,13 +15,14 @@
         (is (nil? (transforms-base.u/throw-if-db-routing-enabled!
                    {:name "OSS transform"}
                    (mt/db)))))))
-  (testing "with :database-routing premium feature enabled, the check throws on a routing-enabled database"
-    (mt/with-premium-features #{:database-routing}
-      (mt/with-temp [:model/DatabaseRouter _ {:database_id    (mt/id)
-                                              :user_attribute "db_name"}]
-        (is (thrown-with-msg?
-             clojure.lang.ExceptionInfo
-             #".*database routing turned on"
-             (transforms-base.u/throw-if-db-routing-enabled!
-              {:name "Routing transform"}
-              (mt/db))))))))
+  (when config/ee-available?
+    (testing "with :database-routing premium feature enabled, the check throws on a routing-enabled database"
+      (mt/with-premium-features #{:database-routing}
+        (mt/with-temp [:model/DatabaseRouter _ {:database_id    (mt/id)
+                                                :user_attribute "db_name"}]
+          (is (thrown-with-msg?
+               clojure.lang.ExceptionInfo
+               #".*database routing turned on"
+               (transforms-base.u/throw-if-db-routing-enabled!
+                {:name "Routing transform"}
+                (mt/db)))))))))
