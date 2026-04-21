d79e6865d24d868a4a84f99cdb277ef83629b714
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-21 17:34:01 +0200

    Simplify trackPageView

--- frontend/src/metabase/utils/analytics-untyped.ts
+++ frontend/src/metabase/utils/analytics-untyped.ts
@@ -3,23 +3,17 @@ import * as Snowplow from "@snowplow/browser-tracker";
 import Settings from "metabase/utils/settings";
 
 export const trackPageView = (url: string): void => {
-  if (!url || !Settings.trackingEnabled()) {
+  if (!url || !Settings.trackingEnabled() || !Settings.snowplowEnabled()) {
     return;
   }
 
-  if (Settings.snowplowEnabled()) {
-    trackSnowplowPageView(getSanitizedUrl(url));
-  }
-};
-
-const trackSnowplowPageView = (url: string): void => {
   Snowplow.setReferrerUrl("#");
-  Snowplow.setCustomUrl(url);
+  Snowplow.setCustomUrl(getSanitizedUrl(url));
   Snowplow.trackPageView();
 };
 
 const getSanitizedUrl = (url: string): string => {
-  const urlWithoutSlug = url.replace(/(\/\d+)-[^/]+$/, (match, path) => path);
+  const urlWithoutSlug = url.replace(/(\/\d+)-[^/]+$/, (_match, path) => path);
   const urlWithoutHost = new URL(urlWithoutSlug, Settings.snowplowUrl() ?? "");
 
   return urlWithoutHost.href;
