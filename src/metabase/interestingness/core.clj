d1c08997260cfe0c96ba4cb8221811b20d28f35d
Nathan Voxland <nathan@metabase.com>
2026-04-21 16:28:35 -0500

    Removed measure interestingness for now

--- src/metabase/interestingness/core.clj
+++ src/metabase/interestingness/core.clj
@@ -15,7 +15,6 @@
    [metabase.interestingness.chart.repr]
    [metabase.interestingness.chart.stats]
    [metabase.interestingness.dimension]
-   [metabase.interestingness.measure]
    [potemkin :as p]))
 
 (p/import-vars
@@ -24,9 +23,6 @@
 
  [metabase.interestingness.dimension
   dimension-interestingness]
-
- [metabase.interestingness.measure
-  measure-interestingness]
 
  [metabase.interestingness.chart.repr
   generate-representation]
