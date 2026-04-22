6cfa19b596c0c5e59aa7a44d41dea1860893edf9
Jakub Chodorowicz <jakub.chodorowicz@metabase.com>
2026-04-22 16:10:33 +0200

    reset UpsellCustomViz

--- frontend/src/metabase/admin/upsells/UpsellCustomViz.tsx
+++ frontend/src/metabase/admin/upsells/UpsellCustomViz.tsx
@@ -1,48 +1,21 @@
 import { t } from "ttag";
 
-import { useAdminSetting } from "metabase/api/utils";
 import { UpsellBanner } from "metabase/common/components/upsells/components";
 import { UPGRADE_URL } from "metabase/common/components/upsells/constants";
 import { useHasTokenFeature } from "metabase/common/hooks";
 import { PLUGIN_ADMIN_SETTINGS } from "metabase/plugins";
-import { Switch, Text } from "metabase/ui";
-
-const settingName = "custom-viz-enabled";
 
 export const UpsellCustomViz = ({ location }: { location: string }) => {
-  const hasCustomVizAvailable = useHasTokenFeature("custom-viz-available");
-  const {
-    value: settingValue,
-    updateSetting,
-    settingDetails,
-  } = useAdminSetting(settingName);
+  const hasCustomViz = useHasTokenFeature("custom-viz");
   const campaign = "custom-viz";
   const { triggerUpsellFlow } = PLUGIN_ADMIN_SETTINGS.useUpsellFlow({
     campaign,
     location,
   });
 
-  if (hasCustomVizAvailable) {
+  if (hasCustomViz) {
     return null;
   }
-
-  if (settingDetails?.is_env_setting) {
-    return (
-      <UpsellBanner
-        title={t`Build your own visualizations`}
-        campaign={campaign}
-        buttonText={t`Try for free`}
-        buttonLink={UPGRADE_URL}
-        location={location}
-        onClick={triggerUpsellFlow}
-      >
-        <Text>{t`Create custom chart types tailored to your data using the Custom visualizations SDK.`}</Text>
-        <Text c="text-secondary">{t`Custom visualizations are set via environment variable.`}</Text>
-      </UpsellBanner>
-    );
-  }
-
-  const isEnabled = Boolean(settingValue);
 
   return (
     <UpsellBanner
@@ -53,21 +26,7 @@ export const UpsellCustomViz = ({ location }: { location: string }) => {
       location={location}
       onClick={triggerUpsellFlow}
     >
-      <Text>{t`Create custom chart types tailored to your data using the Custom visualizations SDK.`}</Text>
-      <Switch
-        mt="sm"
-        size="sm"
-        label={t`Enable custom visualizations`}
-        description={t`Allow custom visualizations to be used in this instance.`}
-        labelPosition="left"
-        checked={isEnabled}
-        onChange={(event) =>
-          updateSetting({
-            key: settingName,
-            value: event.currentTarget.checked,
-          })
-        }
-      />
+      {t`Create custom chart types tailored to your data using the Custom visualizations SDK.`}
     </UpsellBanner>
   );
 };
