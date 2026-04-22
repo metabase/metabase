0ddd60613eca1558f345d9d3d906750e0443b6f1
Ben Grabow <ben.grabow@gmail.com>
2026-04-22 10:49:29 -0400

    Example config value

--- enterprise/backend/src/metabase_enterprise/workspaces/core.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/core.clj
@@ -2,6 +2,12 @@
 
 (def ^:private workspaces-config (atom nil))
 
+(def ^:private example-config
+  {:name "github",
+   :databases
+   {(keyword "Analytics Data Warehouse") {:input_schemas ["raw_github"]
+                                          :output_schema "mb__isolation_754bd_github"}}})
+
 (defn active?
   []
   (boolean @workspaces-config))
