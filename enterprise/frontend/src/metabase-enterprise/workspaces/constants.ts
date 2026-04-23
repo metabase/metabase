38d92acbeac03f0242600fd78c3c5eb9c873cd58
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-04-23 09:23:19 -0400

    table metadata

--- enterprise/frontend/src/metabase-enterprise/workspaces/constants.ts
+++ enterprise/frontend/src/metabase-enterprise/workspaces/constants.ts
@@ -2,6 +2,10 @@ export const POLLING_INTERVAL = 1000;
 
 export const DOCKER_RUN_COMMAND = `docker run -d -p 3000:3000 \\
   -v $(pwd)/config.yml:/config.yml \\
+  -v $(pwd)/metadata.json:/metadata.json \\
+  -v $(pwd)/field_values.json:/field_values.json \\
   -e MB_CONFIG_FILE_PATH=/config.yml \\
+  -e MB_TABLE_METADATA_FILE_PATH=/metadata.json \\
+  -e MB_FIELD_VALUES_FILE_PATH=/field_values.json \\
   --name metabase \\
   metabase/metabase-enterprise:latest`;
