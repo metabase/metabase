82be7f28f075d600c51ee22563962fc582c089a7
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-23 09:36:27 -0700

    Rearrange branches for clarit

--- frontend/src/metabase-lib/v1/urls.ts
+++ frontend/src/metabase-lib/v1/urls.ts
@@ -53,41 +53,42 @@ export function getQuestionUrlWithParameters(
   }
 
   const { isNative, isEditable } = Lib.queryDisplayInfo(question.query());
-  if (!isNative) {
-    let questionWithParameters = question.setParameters(parameters);
 
-    if (isEditable) {
-      // treat the dataset/model question like it is already composed so that we can apply
-      // dataset/model-specific metadata to the underlying dimension options
-      const needsComposing = question.type() !== "question";
-      questionWithParameters = needsComposing
-        ? question.composeQuestionAdhoc().setParameters(parameters)
-        : questionWithParameters;
-      questionWithParameters = questionWithParameters
-        .setParameterValues(parameterValues)
-        ._convertParametersToMbql({ isComposed: needsComposing });
-
-      return getQuestionUrl(questionWithParameters, {
-        originalQuestion,
-        includeDisplayIsLocked,
-        query: objectId === undefined ? {} : { objectId },
-      });
-    }
-
-    const query = getParameterValuesBySlug(parameters, parameterValues);
-    return getQuestionUrl(questionWithParameters.markDirty(), {
-      query,
+  if (isNative) {
+    const query = question.legacyNativeQuery() as NativeQuery;
+    return getQuestionUrl(question, {
+      query: remapParameterValuesToTemplateTags(
+        query.templateTags(),
+        parameters,
+        parameterValues,
+      ),
       includeDisplayIsLocked,
     });
   }
 
-  const query = question.legacyNativeQuery() as NativeQuery;
-  return getQuestionUrl(question, {
-    query: remapParameterValuesToTemplateTags(
-      query.templateTags(),
-      parameters,
-      parameterValues,
-    ),
+  let questionWithParameters = question.setParameters(parameters);
+
+  if (isEditable) {
+    // treat the dataset/model question like it is already composed so that we can apply
+    // dataset/model-specific metadata to the underlying dimension options
+    const needsComposing = question.type() !== "question";
+    questionWithParameters = needsComposing
+      ? question.composeQuestionAdhoc().setParameters(parameters)
+      : questionWithParameters;
+    questionWithParameters = questionWithParameters
+      .setParameterValues(parameterValues)
+      ._convertParametersToMbql({ isComposed: needsComposing });
+
+    return getQuestionUrl(questionWithParameters, {
+      originalQuestion,
+      includeDisplayIsLocked,
+      query: objectId === undefined ? {} : { objectId },
+    });
+  }
+
+  const query = getParameterValuesBySlug(parameters, parameterValues);
+  return getQuestionUrl(questionWithParameters.markDirty(), {
+    query,
     includeDisplayIsLocked,
   });
 }
