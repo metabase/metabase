2a14028fc924739ef3d21b00593f9459eab6f91d
Sébastien <sebastien@metabase.com>
2026-04-23 14:53:00 +0200

    removed remaining merge conflict marker

--- frontend/src/metabase/admin/embedding/hooks/index.ts
+++ frontend/src/metabase/admin/embedding/hooks/index.ts
@@ -1,8 +1,5 @@
 export { useDefaultEmbeddingThemeSettings } from "./use-default-embedding-theme-settings";
-<<<<<<< HEAD
 export { useDeleteThemeFlow } from "./use-delete-theme-flow";
-=======
 export { useDefaultEmbeddingThemes } from "./use-default-embedding-themes";
->>>>>>> 72ac69387ca (transactional route to keep the SSOT client side)
 export { useEmbeddingThemeEditor } from "./use-embedding-theme-editor";
 export { useEnsureDefaultEmbeddingThemes } from "./use-ensure-default-embedding-themes";
