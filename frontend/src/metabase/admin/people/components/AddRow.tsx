812af78508ab44a3c1ad329936aceae7fe28a030
StephenODea54 <odeastephen1@gmail.com>
2026-04-23 15:40:12 -0400

    Fix add members chip overflow in group settings

--- frontend/src/metabase/admin/people/components/AddRow.tsx
+++ frontend/src/metabase/admin/people/components/AddRow.tsx
@@ -30,33 +30,37 @@ export const AddRow = ({
     my="1rem"
     p="0.5rem"
     display="relative"
-    align="center"
+    direction="column"
     bd="1px solid var(--mb-color-brand)"
     style={{ borderRadius: "0.5rem" }}
   >
-    {children}
-    <Input
-      type="text"
-      variant="unstyled"
-      flex="1 0 auto"
-      fz="lg"
-      styles={{ input: { background: "transparent" } }}
-      value={value}
-      placeholder={placeholder}
-      aria-label={ariaLabel}
-      autoFocus
-      onKeyDown={onKeyDown}
-      onChange={onChange}
-    />
-    <Button variant="subtle" bg="transparent" onClick={onCancel} mr="sm">
-      {t`Cancel`}
-    </Button>
-    <Button
-      variant={isValid ? "filled" : "outline"}
-      disabled={!isValid}
-      onClick={onDone}
-    >
-      {t`Add`}
-    </Button>
+    <Flex align="center" wrap="wrap">
+      {children}
+      <Input
+        type="text"
+        variant="unstyled"
+        flex="1 0 auto"
+        fz="lg"
+        styles={{ input: { background: "transparent" } }}
+        value={value}
+        placeholder={placeholder}
+        aria-label={ariaLabel}
+        autoFocus
+        onKeyDown={onKeyDown}
+        onChange={onChange}
+      />
+    </Flex>
+    <Flex justify="flex-end" mt="0.5rem">
+      <Button variant="subtle" bg="transparent" onClick={onCancel} mr="sm">
+        {t`Cancel`}
+      </Button>
+      <Button
+        variant={isValid ? "filled" : "outline"}
+        disabled={!isValid}
+        onClick={onDone}
+      >
+        {t`Add`}
+      </Button>
+    </Flex>
   </Flex>
 );
