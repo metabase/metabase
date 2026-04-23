a5b3716d20068d36c91966c2ece959c9d5e3d057
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-04-23 09:23:54 -0400

    fix

--- enterprise/frontend/src/metabase-enterprise/workspaces/constants.ts
+++ enterprise/frontend/src/metabase-enterprise/workspaces/constants.ts
@@ -5,7 +5,7 @@ export const DOCKER_RUN_COMMAND = `docker run -d -p 3000:3000 \\
   -v $(pwd)/metadata.json:/metadata.json \\
   -v $(pwd)/field_values.json:/field_values.json \\
   -e MB_CONFIG_FILE_PATH=/config.yml \\
-  -e MB_TABLE_METADATA_FILE_PATH=/metadata.json \\
-  -e MB_FIELD_VALUES_FILE_PATH=/field_values.json \\
+  -e MB_TABLE_METADATA_PATH=/metadata.json \\
+  -e MB_FIELD_VALUES_PATH=/field_values.json \\
   --name metabase \\
   metabase/metabase-enterprise:latest`;
