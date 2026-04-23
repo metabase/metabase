b35670690ede718dee5ac3991307c23cd633a712
Nick Fitzpatrick <nickfitz.582@gmail.com>
2026-04-23 18:39:13 +0530

    fix import

--- frontend/src/metabase/transforms/pages/TransformListPage/TransformListPage.unit.spec.tsx
+++ frontend/src/metabase/transforms/pages/TransformListPage/TransformListPage.unit.spec.tsx
@@ -12,9 +12,9 @@ import {
   screen,
   waitForLoaderToBeRemoved,
 } from "__support__/ui";
-import { createMockState } from "metabase/redux/store/mocks";
 import type { TokenFeatures } from "metabase-types/api";
 import { createMockTokenFeatures } from "metabase-types/api/mocks";
+import { createMockState } from "metabase-types/store/mocks";
 
 import { TransformListPage } from "./TransformListPage";
 
