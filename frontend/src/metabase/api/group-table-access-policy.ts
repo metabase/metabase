5b9130f5bf62f134a2d8f834dc1b588f8993f582
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-24 03:02:24 -0700

    Coalesce empty string to null

--- frontend/src/metabase/api/group-table-access-policy.ts
+++ frontend/src/metabase/api/group-table-access-policy.ts
@@ -21,7 +21,7 @@ export const groupTableAccessPolicyApi = Api.injectEndpoints({
     // When both table_id and group_id are provided /api/mt/gtap returns
     // a single policy, so we need to use a different type for that case.
     getGroupTableAccessPolicy: builder.query<
-      GroupTableAccessPolicy,
+      GroupTableAccessPolicy | null,
       { group_id: number; table_id: number }
     >({
       query: (params) => ({
@@ -30,6 +30,15 @@ export const groupTableAccessPolicyApi = Api.injectEndpoints({
         params,
       }),
       providesTags: () => [listTag("group-table-access-policy")],
+      transformResponse(response: GroupTableAccessPolicy | "" | null) {
+        // The api returns 204 No Content when there is no policy.
+        // This gets interpreted as an empty string by RTK query, which is not what we want.
+        // Here we coalesce the empty string into null.
+        if (response === "") {
+          return null;
+        }
+        return response;
+      },
     }),
   }),
 });
