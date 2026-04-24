5a09905b583fe907db00cc4e7bdce7307aa3e43a
Tyler Plude <tsplude@gmail.com>
2026-04-24 13:36:41 -0700

    drop deprecated v_metabot_usage

--- enterprise/frontend/src/metabase-enterprise/audit_app/metabot-analytics/constants.ts
+++ enterprise/frontend/src/metabase-enterprise/audit_app/metabot-analytics/constants.ts
@@ -4,5 +4,4 @@ export const AUDIT_DB_ID = 13371337;
 // View names in the audit database for metabot analytics.
 export const VIEW_CONVERSATIONS = "v_metabot_conversations";
 export const VIEW_MESSAGES = "v_metabot_messages";
-export const VIEW_USAGE = "v_metabot_usage";
 export const VIEW_USAGE_LOG = "v_ai_usage_log";
