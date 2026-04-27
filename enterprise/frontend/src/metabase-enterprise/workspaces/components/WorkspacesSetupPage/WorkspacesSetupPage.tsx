34e7a942919a8c5677525867a77efd1785a696d4
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-04-27 13:19:24 -0400

    fix

--- enterprise/frontend/src/metabase-enterprise/workspaces/components/WorkspacesSetupPage/WorkspacesSetupPage.tsx
+++
@@ -1,7 +0,0 @@
-import { t } from "ttag";
-
-import { SettingsPageWrapper } from "metabase/admin/components/SettingsSection";
-
-export const WorkspacesSetupPage = () => {
-  return <SettingsPageWrapper title={t`Setup`} />;
-};
--- enterprise/frontend/src/metabase-enterprise/workspaces/components/WorkspacesSetupPage/index.ts
+++
@@ -1,1 +0,0 @@
-export { WorkspacesSetupPage } from "./WorkspacesSetupPage";
