6b6736d67077ebef2bf6d8b812eb7f9e6038b8fc
Aleksandr Lesnenko <alxnddr@gmail.com>
2026-04-29 13:13:10 -0400

    setting for workspace instances

--- enterprise/backend/src/metabase_enterprise/workspaces/api.clj
+++ enterprise/backend/src/metabase_enterprise/workspaces/api.clj
@@ -2,10 +2,12 @@
   (:require
    [metabase-enterprise.workspaces.api.workspace-instance]
    [metabase-enterprise.workspaces.api.workspace-manager]
+   [metabase-enterprise.workspaces.settings]
    [metabase.api.macros :as api.macros]))
 
 (comment metabase-enterprise.workspaces.api.workspace-instance/keep-me
-         metabase-enterprise.workspaces.api.workspace-manager/keep-me)
+         metabase-enterprise.workspaces.api.workspace-manager/keep-me
+         metabase-enterprise.workspaces.settings/keep-me)
 
 (def ^{:arglists '([request respond raise])} manager-routes
   "`/api/ee/workspace-manager` routes."
