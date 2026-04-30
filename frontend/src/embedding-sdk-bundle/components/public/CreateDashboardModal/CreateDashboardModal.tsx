77868bb422073e7c4e39bb1619fc78c431f983e1
Romeo Van Snick <romeo@romeovansnick.be>
2026-04-29 23:53:04 -0700

    Switch CreateDashboardModal to use useGetCollectionQuery

--- frontend/src/embedding-sdk-bundle/components/public/CreateDashboardModal/CreateDashboardModal.tsx
+++ frontend/src/embedding-sdk-bundle/components/public/CreateDashboardModal/CreateDashboardModal.tsx
@@ -7,8 +7,9 @@ import type {
   MetabaseDashboard,
   SdkCollectionId,
 } from "embedding-sdk-bundle/types";
+import { useGetCollectionQuery } from "metabase/api";
 import { CreateDashboardModal as CreateDashboardModalCore } from "metabase/common/CreateDashboard/CreateDashboardModal";
-import { useCollectionQuery, useLocale } from "metabase/common/hooks";
+import { useLocale } from "metabase/common/hooks";
 import { useSelector } from "metabase/redux";
 
 import { createDashboardModalSchema } from "./CreateDashboardModal.schema";
@@ -67,7 +68,7 @@ const CreateDashboardModalInner = ({
       : undefined,
   );
 
-  const { isLoading: isCollectionQueryLoading } = useCollectionQuery({
+  const { isLoading: isCollectionQueryLoading } = useGetCollectionQuery({
     id: collectionIdSlug,
   });
 
