d585e3864a33602f71fed6e7e27f45e32ffcf942
Alexander Polyankin <alexander.polyankin@metabase.com>
2026-05-01 13:08:52 -0400

    simplify

--- enterprise/frontend/src/metabase-enterprise/workspaces/manager/pages/WorkspaceSetupPage/SetupSection/SetupSection.tsx
+++ enterprise/frontend/src/metabase-enterprise/workspaces/manager/pages/WorkspaceSetupPage/SetupSection/SetupSection.tsx
@@ -1,10 +1,9 @@
 import { jt, t } from "ttag";
 
-import { Anchor, Button, Code, Divider, Stack, Text } from "metabase/ui";
+import { Button, Code, Stack, Text } from "metabase/ui";
 import { TitleSection } from "metabase-enterprise/workspaces/common/components/TitleSection";
 import type { Workspace } from "metabase-types/api";
 
-const LOCAL_INSTANCE_URL = "http://localhost:3000";
 const CONFIG_FILE_NAME = "config.yml";
 
 type SetupSectionProps = {
@@ -18,8 +17,6 @@ export function SetupSection({ workspace }: SetupSectionProps) {
       description={t`Run a local instance backed by this workspace's data, so you can iterate on changes safely.`}
     >
       <DownloadConfigSection workspace={workspace} />
-      <Divider />
-      <RunInstanceSection />
     </TitleSection>
   );
 }
@@ -34,37 +31,11 @@ function DownloadConfigSection({ workspace }: DownloadConfigSectionProps) {
   return (
     <Stack p="md" gap="sm" align="flex-start">
       <Text>
-        {jt`Download the workspace's ${<Code key="cfg">{CONFIG_FILE_NAME}</Code>} file. It contains the isolated database credentials:`}
+        {jt`Pass this ${<Code key="cfg">{CONFIG_FILE_NAME}</Code>}, containing the database credentials, when starting the instance.`}
       </Text>
       <Button component="a" href={configUrl} download="config.yml">
         {t`Download config.yml`}
       </Button>
-    </Stack>
-  );
-}
-
-function RunInstanceSection() {
-  const dockerCommand = `docker run -d -p 3000:3000 \\
-  -v $(pwd)/config.yml:/config.yml \\
-  -e MB_CONFIG_FILE_PATH=/config.yml \\
-  -e MB_PREMIUM_EMBEDDING_TOKEN \\
-  metabase/metabase-enterprise:latest`;
-
-  return (
-    <Stack p="md" gap="sm">
-      <Text>
-        {jt`Start the developer instance at ${(
-          <Anchor
-            key="url"
-            href={LOCAL_INSTANCE_URL}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {LOCAL_INSTANCE_URL}
-          </Anchor>
-        )}:`}
-      </Text>
-      <Code block>{dockerCommand}</Code>
     </Stack>
   );
 }
