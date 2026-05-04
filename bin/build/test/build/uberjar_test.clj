86e41655e3c2e45c9bae4ac712447b083a5974ec
Chris Truter <chris@metabase.com>
2026-05-04 17:53:38 +0200

    Inline excluded? helper into are body

--- bin/build/test/build/uberjar_test.clj
+++ bin/build/test/build/uberjar_test.clj
@@ -5,18 +5,15 @@
 
 (set! *warn-on-reflection* true)
 
-(defn- excluded? [path]
-  (let [patterns @#'build.uberjar/dependency-ignore-patterns]
-    (boolean (some #(re-matches % path) patterns))))
-
 (deftest dependency-ignore-patterns-test
   (testing "carve-out for drivers excluded from AOT keeps their .clj source (#73560)"
-    (are [path excluded] (= excluded (excluded? path))
-      "metabase/driver/oracle.clj"      false
-      "metabase/driver/vertica.clj"     false
-      "metabase/driver/oracle.cljc"     false
-      "metabase/driver/oracle/util.clj" false ; future helper namespaces under the carved-out drivers
-      "metabase/driver/h2.clj"          true
-      "metabase/util.clj"               true
-      "metabase/driver/oracleish.clj"   true
-      "metabase/util/oracle.clj"        true)))
+    (let [patterns @#'build.uberjar/dependency-ignore-patterns]
+      (are [path excluded] (= excluded (boolean (some #(re-matches % path) patterns)))
+        "metabase/driver/oracle.clj"      false
+        "metabase/driver/vertica.clj"     false
+        "metabase/driver/oracle.cljc"     false
+        "metabase/driver/oracle/util.clj" false ; future helper namespaces under the carved-out drivers
+        "metabase/driver/h2.clj"          true
+        "metabase/util.clj"               true
+        "metabase/driver/oracleish.clj"   true
+        "metabase/util/oracle.clj"        true))))
