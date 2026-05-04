54f5f35c204c2dddd07a0982043e69a937538d3a
dan sutton <dan@dpsutton.com>
2026-05-04 16:05:22 -0500

    oracle doesn't have group in 58 and below UI

--- modules/drivers/oracle/test/metabase/driver/oracle_test.clj
+++ modules/drivers/oracle/test/metabase/driver/oracle_test.clj
@@ -110,9 +110,8 @@
 (deftest connection-properties-test
   (testing "Connection properties should be returned properly (including transformation of secret types)"
     (with-redefs [premium-features/is-hosted? (constantly false)]
-      (let [expected [{:type :group
-                       :fields [{:name "host"}
-                                {:name "port"}]}
+      (let [expected [{:name "host"}
+                      {:name "port"}
                       {:name "sid"}
                       {:name "service-name"}
                       {:name "user"}
