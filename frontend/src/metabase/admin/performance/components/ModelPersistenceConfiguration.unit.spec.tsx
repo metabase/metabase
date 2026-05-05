f263fc611f764f56c823cbbb9789fe5442e200ac
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2026-05-05 19:40:31 +0000

    Fix path

--- frontend/src/metabase/admin/performance/components/ModelPersistenceConfiguration.unit.spec.tsx
+++ frontend/src/metabase/admin/performance/components/ModelPersistenceConfiguration.unit.spec.tsx
@@ -7,8 +7,8 @@ import {
 } from "__support__/server-mocks";
 import { mockSettings } from "__support__/settings";
 import { renderWithProviders, screen, waitFor } from "__support__/ui";
-import { createMockState } from "metabase/redux/store/mocks";
 import { createMockSettings } from "metabase-types/api/mocks";
+import { createMockState } from "metabase-types/store/mocks";
 
 import { ModelPersistenceConfiguration } from "./ModelPersistenceConfiguration";
 
