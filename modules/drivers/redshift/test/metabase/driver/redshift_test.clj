bc82693dc9760608669041dd101606c4b932b320
Bryan Maass <bryan.maass@gmail.com>
2026-05-05 16:20:12 -0600

    Remove unused require after mview auto-retry cherry-pick
    The cherry-pick of master commit 06c24e6b945 switched
    sync-materialized-views-test from sql-jdbc.describe-database/describe-database
    to driver/describe-database, leaving the alias unused on this branch.
    Kondo runs strict (warnings -> exit 2) so this failed CI.

--- modules/drivers/redshift/test/metabase/driver/redshift_test.clj
+++ modules/drivers/redshift/test/metabase/driver/redshift_test.clj
@@ -6,7 +6,6 @@
    [metabase.driver :as driver]
    [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
    [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
-   [metabase.driver.sql-jdbc.sync.describe-database :as sql-jdbc.describe-database]
    [metabase.driver.sql.query-processor :as sql.qp]
    [metabase.plugins.jdbc-proxy :as jdbc-proxy]
    [metabase.public-settings :as public-settings]
