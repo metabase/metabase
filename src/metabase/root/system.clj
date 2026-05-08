628b2d3446c0209ca664277e39dadd8d63461b03
Ben Grabow <ben.grabow@gmail.com>
2026-05-08 09:57:26 -0500

    reset! and swap! against the root binding not the dynamic binding

--- src/metabase/root/system.clj
+++ src/metabase/root/system.clj
@@ -12,14 +12,14 @@
   "Make a component handle with state stored under a key in the global *system* map."
   [k]
   (reify mc/MutableComponentHandle
-    (current [_] (get @*system* k *system*))
+    (current [_] (get @*system* k))
     (root [_] (get @(.getRawRoot ^Var #'*system*) k))
     (do-with-value [_ new-value thunk]
       (binding [*system* (atom (assoc @*system* k new-value))]
         (thunk)))
     (reset-value! [_ new-value]
-      (swap! *system* assoc k new-value))
+      (swap! (.getRawRoot ^Var #'*system*) assoc k new-value))
     (swap-value! [_ f]
-      (swap! *system* (fn [s] (update s k f))))
+      (swap! (.getRawRoot ^Var #'*system*) update k f))
     (swap-value! [_ f args]
-      (swap! *system* (fn [s] (apply update s k f args))))))
+      (swap! (.getRawRoot ^Var #'*system*) (fn [s] (apply update s k f args))))))
