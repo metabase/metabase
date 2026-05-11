ebe45c819e5b5a3ad8f976aa895cd311e76f8469
Riley Thompson <riley@metabase.com>
2026-05-11 15:28:04 -0400

    Allow impersonated writes for custom actions (#73580)
    * allow impersonated writes for actions
    
    * add test requires
    
    * fix tests
    
    * exclude postgres
    
    * fix tests

--- test/metabase/driver/sql_test.clj
+++ test/metabase/driver/sql_test.clj
@@ -7,7 +7,7 @@
    [metabase.query-processor.compile :as qp.compile]
    [metabase.test :as mt]))
 
-(deftest ^:parallel is-single-select-stmt?-test
+(deftest ^:parallel is-single-stmt-of-type?-test
   (mt/test-drivers (mt/normal-drivers-with-feature :connection-impersonation)
     (let [mp (mt/metadata-provider)
           products (lib.metadata/table mp (mt/id :products))
@@ -16,16 +16,16 @@
                     (lib/filter (lib/= product-category "Widget")))
           native-query (:query (qp.compile/compile-with-inline-parameters query))]
       (testing "A single SELECT statement returns true and the reconstructed SQL"
-        (are [sql] (=? {:is-single-select? true, :sql string?}
-                       (#'driver.sql/is-single-select-stmt? sql))
+        (are [sql] (=? {:is-single-stmt? true, :sql string?}
+                       (#'driver.sql/is-single-stmt-of-type? sql "read"))
           native-query
           "SELECT 1"
           "SELECT * FROM table"
           "WITH x AS (SELECT * FROM foo) SELECT * from x"
           "WITH x AS (SELECT a FROM foo), y AS (SELECT b FROM bar), z AS (SELECT c FROM baz) SELECT x.a, y.b, z.c FROM x, y, z")))
     (testing "All other queries are rejected"
-      (are [sql] (=? {:is-single-select? false}
-                     (#'driver.sql/is-single-select-stmt? sql))
+      (are [sql] (=? {:is-single-stmt? false}
+                     (#'driver.sql/is-single-stmt-of-type? sql "read"))
         "SELECT ("
         "SELECT 1; SELECT 2"
         "SET ROLE NONE"
