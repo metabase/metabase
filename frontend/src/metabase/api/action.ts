69f17e1e9e21264fac5d1cd83e61863bd806fc8b
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-11 22:49:11 -0700

    Remove actions from schema.js entirely since they're not needed in metadata

--- frontend/src/metabase/api/action.ts
+++ frontend/src/metabase/api/action.ts
@@ -1,6 +1,5 @@
 import _ from "underscore";
 
-import { ActionSchema } from "metabase/schema";
 import type {
   CreateActionRequest,
   GetActionRequest,
@@ -19,7 +18,6 @@ import {
   provideActionListTags,
   provideActionTags,
 } from "./tags";
-import { hydrateLegacyEntities } from "./utils/hydrate-legacy-entities";
 
 export const actionApi = Api.injectEndpoints({
   endpoints: (builder) => ({
@@ -30,7 +28,6 @@ export const actionApi = Api.injectEndpoints({
         params,
       }),
       providesTags: (collections = []) => provideActionListTags(collections),
-      onQueryStarted: hydrateLegacyEntities([ActionSchema]),
     }),
     getAction: builder.query<WritebackAction, GetActionRequest>({
       query: ({ id }) => ({
@@ -38,7 +35,6 @@ export const actionApi = Api.injectEndpoints({
         url: `/api/action/${id}`,
       }),
       providesTags: (action) => (action ? provideActionTags(action) : []),
-      onQueryStarted: hydrateLegacyEntities(ActionSchema),
     }),
     createAction: builder.mutation<WritebackAction, CreateActionRequest>({
       query: (body) => ({
