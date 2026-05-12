e32f40f99383c3eca259050b48a47eb12d4c7c13
Nicola Mometto <nicola.mometto@metabase.com>
2026-05-12 14:26:40 +0100

    a test

---
+++ enterprise/backend/test/metabase_enterprise/transform_optimizer/proposal_cache_test.clj
@@ -0,0 +1,71 @@
+(ns metabase-enterprise.transform-optimizer.proposal-cache-test
+  (:require
+   [clojure.test :refer [deftest is testing use-fixtures]]
+   [metabase-enterprise.transform-optimizer.proposal-cache :as cache]))
+
+(set! *warn-on-reflection* true)
+
+(use-fixtures :each (fn [t] (#'cache/clear-all!) (t) (#'cache/clear-all!)))
+
+(deftest put-and-get-test
+  (testing "stored proposals are retrievable by user+transform+id"
+    (cache/put-all! 1 99 [{:id "p1" :body "SELECT 1"}
+                          {:id "p2" :body "SELECT 2"}])
+    (is (= {:id "p1" :body "SELECT 1"}
+           (cache/get-one 1 99 "p1")))
+    (is (= {:id "p2" :body "SELECT 2"}
+           (cache/get-one 1 99 "p2"))))
+
+  (testing "missing id returns nil rather than throwing"
+    (cache/put-all! 1 99 [{:id "p1"}])
+    (is (nil? (cache/get-one 1 99 "p2"))))
+
+  (testing "skips proposals with blank/nil ids"
+    (cache/put-all! 1 99 [{:id nil :body "x"} {:id "" :body "y"} {:id "p3"}])
+    (is (nil? (cache/get-one 1 99 nil)))
+    (is (nil? (cache/get-one 1 99 "")))
+    (is (= {:id "p3"} (cache/get-one 1 99 "p3")))))
+
+(deftest user-isolation-test
+  (testing "user A cannot read proposals stored for user B"
+    (cache/put-all! 1 99 [{:id "p1" :body "SECRET"}])
+    (is (nil? (cache/get-one 2 99 "p1"))
+        "different user-id must not see the proposal")
+    (is (some? (cache/get-one 1 99 "p1"))
+        "owner still sees it")))
+
+(deftest transform-isolation-test
+  (testing "proposal stored for transform A is not visible under transform B"
+    (cache/put-all! 1 99 [{:id "p1"}])
+    (is (nil? (cache/get-one 1 100 "p1")))
+    (is (some? (cache/get-one 1 99 "p1")))))
+
+(deftest get-many-test
+  (testing "returns proposals in input order + reports missing"
+    (cache/put-all! 1 99 [{:id "p1" :body "a"} {:id "p3" :body "c"}])
+    (let [[found missing] (cache/get-many 1 99 ["p1" "p2" "p3" "p4"])]
+      (is (= [{:id "p1" :body "a"} {:id "p3" :body "c"}] found))
+      (is (= ["p2" "p4"] missing))))
+
+  (testing "all present ⇒ no missing"
+    (cache/put-all! 1 99 [{:id "p1"} {:id "p2"}])
+    (let [[found missing] (cache/get-many 1 99 ["p1" "p2"])]
+      (is (= 2 (count found)))
+      (is (empty? missing))))
+
+  (testing "all missing ⇒ all reported as missing"
+    (let [[found missing] (cache/get-many 1 99 ["p1" "p2"])]
+      (is (empty? found))
+      (is (= ["p1" "p2"] missing)))))
+
+(deftest re-put-overwrites-test
+  (testing "calling put-all! again with the same id replaces the previous entry"
+    (cache/put-all! 1 99 [{:id "p1" :body "v1"}])
+    (cache/put-all! 1 99 [{:id "p1" :body "v2"}])
+    (is (= "v2" (:body (cache/get-one 1 99 "p1"))))))
+
+(deftest empty-put-noop-test
+  (testing "put-all! with empty proposals does nothing (no errors)"
+    (cache/put-all! 1 99 [])
+    (cache/put-all! 1 99 nil)
+    (is (nil? (cache/get-one 1 99 "anything")))))
