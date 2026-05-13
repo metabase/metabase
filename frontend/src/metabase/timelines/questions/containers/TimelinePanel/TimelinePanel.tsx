f8fa07f7e3d903407d9ac4d47d65a60b4f3d1fe3
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-13 10:02:53 -0700

    Wrap component in error and loading wrapper

--- frontend/src/metabase/timelines/questions/containers/TimelinePanel/TimelinePanel.tsx
+++ frontend/src/metabase/timelines/questions/containers/TimelinePanel/TimelinePanel.tsx
@@ -1,6 +1,7 @@
 import type { ComponentProps } from "react";
 
 import { useGetCollectionQuery } from "metabase/api";
+import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
 import { useSetArchive } from "metabase/common/hooks";
 import { ROOT_COLLECTION } from "metabase/entities/collections";
 import type { CollectionId, TimelineEvent } from "metabase-types/api";
@@ -18,20 +19,25 @@ function TimelinePanelContainer({
   ...props
 }: TimelinePanelContainerProps) {
   const archive = useSetArchive();
-  const { data: collection } = useGetCollectionQuery({
+  const {
+    data: collection,
+    isLoading,
+    error,
+  } = useGetCollectionQuery({
     id: collectionId == null ? ROOT_COLLECTION.id : collectionId,
   });
   const onArchiveEvent = (event: TimelineEvent) =>
     archive({ id: event.id, model: "timeline-event" }, true);
-  if (!collection) {
-    return null;
-  }
   return (
-    <TimelinePanel
-      {...props}
-      collection={collection}
-      onArchiveEvent={onArchiveEvent}
-    />
+    <LoadingAndErrorWrapper loading={isLoading} error={error} noWrapper>
+      {collection ? (
+        <TimelinePanel
+          {...props}
+          collection={collection}
+          onArchiveEvent={onArchiveEvent}
+        />
+      ) : null}
+    </LoadingAndErrorWrapper>
   );
 }
 
