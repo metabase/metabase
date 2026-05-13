01c5d72ccf64fa9b1006ea25cadab9c45511d64b
ignacio <132273646+ignacio-mb@users.noreply.github.com>
2026-05-13 15:55:05 -0300

    Increase limit from 300 to 1000 in SlackChannelField

--- frontend/src/metabase/notifications/channels/SlackChannelField.tsx
+++ frontend/src/metabase/notifications/channels/SlackChannelField.tsx
@@ -84,7 +84,7 @@ export const SlackChannelField = ({
         data={displayNames}
         value={value}
         placeholder={t`Pick a user or channel...`}
-        limit={300}
+        limit={1000}
         onBlur={handleBlur}
         onChange={handleChange}
       />
