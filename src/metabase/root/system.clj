4ee4909db2d77b3bfd99f383245cbe45782d9d68
Ben Grabow <ben.grabow@gmail.com>
2026-05-14 09:16:10 -0500

    Use defrecord for component handle so key is accessible

--- src/metabase/root/system.clj
+++ src/metabase/root/system.clj
@@ -8,20 +8,27 @@
 (defonce ^{:doc "The system" :dynamic true :private true} *system*
   (atom {}))
 
+(defrecord ComponentHandle [k]
+  mc/MutableComponentHandle
+  (current [_] (get @*system* k))
+  (root [_] (get @(.getRawRoot ^Var #'*system*) k))
+  (binding [_ new-value thunk]
+    (clojure.core/binding [*system* (atom (assoc @*system* k new-value))]
+      (thunk)))
+  (reset! [_ new-value]
+    (clojure.core/swap! *system* assoc k new-value))
+  (swap! [_ f]
+    (clojure.core/swap! *system* update k f))
+  (swap! [_ f args]
+    (clojure.core/swap! *system*
+                        (fn swap!* [old-system]
+                          (apply update old-system k f args))))
+  (alter-root [_ new-value]
+    (alter-var-root #'*system*
+                    (fn alter-root* [old-system-atom]
+                      (atom (assoc @old-system-atom k new-value))))))
+
 (defn mutable-component-handle
   "Make a component handle with state stored under a key in the global *system* map."
   [k]
-  (reify mc/MutableComponentHandle
-    (current [_] (get @*system* k))
-    (root [_] (get @(.getRawRoot ^Var #'*system*) k))
-    (binding [_ new-value thunk]
-      (clojure.core/binding [*system* (atom (assoc @*system* k new-value))]
-        (thunk)))
-    (reset! [_ new-value]
-      (clojure.core/swap! *system* assoc k new-value))
-    (swap! [_ f]
-      (clojure.core/swap! *system* update k f))
-    (swap! [_ f args]
-      (clojure.core/swap! *system* (fn sys-swap! [old-system] (apply update old-system k f args))))
-    (alter-root [_ new-value]
-      (alter-var-root #'*system* (fn sys-alter-root [old-system-atom] (atom (assoc @old-system-atom k new-value)))))))
+  (->ComponentHandle k))
