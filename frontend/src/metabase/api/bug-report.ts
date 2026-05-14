c13db192f28b822eb2bfcb396a4a866c7b2b2d9e
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-14 18:33:21 +0300

    Add bug-reporting rtk hook

--- frontend/src/metabase/api/bug-report.ts
+++ frontend/src/metabase/api/bug-report.ts
@@ -1,6 +1,7 @@
-import type { ErrorPayload } from "metabase-types/api";
+import type { BugReportDetails, ErrorPayload } from "metabase-types/api";
 
 import { Api } from "./api";
+import legacyApi from "./legacy-client";
 
 interface BugReportResponse {
   success: boolean;
@@ -18,7 +19,23 @@ export const bugReportApi = Api.injectEndpoints({
         body,
       }),
     }),
+    getBugReportDetails: builder.query<BugReportDetails, void>({
+      query: () => "/api/bug-reporting/details",
+    }),
   }),
 });
 
-export const { useSendBugReportMutation } = bugReportApi;
+export const {
+  useSendBugReportMutation,
+  useGetBugReportDetailsQuery,
+  useLazyGetBugReportDetailsQuery,
+} = bugReportApi;
+
+// This is not an API call: it returns a URL to be opened as an external link,
+// which is why it can't use RTK Query. It needs `api.basename` so it works
+// when Metabase is deployed at a subpath.
+export const getConnectionPoolDetailsUrl = () => {
+  const path = "/api/bug-reporting/connection-pool-details";
+  const { href } = new URL(legacyApi.basename + path, location.origin);
+  return href;
+};
