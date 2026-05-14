d271d6ff1d915e7cb8d8fd59e92a65ed1256061b
Riley Thompson <rileythompson99@gmail.com>
2026-05-14 18:45:15 -0400

    Clean up tests

--- modules/drivers/teradata/test/metabase/test/data/teradata.clj
+++ modules/drivers/teradata/test/metabase/test/data/teradata.clj
@@ -1,12 +1,11 @@
 (ns metabase.test.data.teradata
-  (:require [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
-            [metabase.test.data
-             [interface :as tx]
-             [sql :as sql.tx]
-             [sql-jdbc :as sql-jdbc.tx]]
-            [metabase.test.data.sql-jdbc
-             [execute :as execute]
-             [load-data :as load-data]]))
+  (:require
+   [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
+   [metabase.test.data.interface :as tx]
+   [metabase.test.data.sql :as sql.tx]
+   [metabase.test.data.sql-jdbc :as sql-jdbc.tx]
+   [metabase.test.data.sql-jdbc.execute :as execute]
+   [metabase.test.data.sql-jdbc.load-data :as load-data]))
 
 (sql-jdbc.tx/add-test-extensions! :teradata)
 
