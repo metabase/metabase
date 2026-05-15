9520789a4bf0677e9a7cdb2a9d097a76a84466d4
Anderson <anderson@metabase.com>
2026-05-14 23:31:09 -0300

    Hydrate command palette search from URL

--- frontend/src/metabase/palette/components/Palette.tsx
+++ frontend/src/metabase/palette/components/Palette.tsx
@@ -1,8 +1,7 @@
 import type { Query } from "history";
-import { KBarPortal, KBarSearch, VisualState, useKBar } from "kbar";
+import { KBarPortal, VisualState, useKBar } from "kbar";
 import { useEffect, useRef } from "react";
 import { type PlainRoute, withRouter } from "react-router";
-import { t } from "ttag";
 
 import { useOnClickOutside } from "metabase/common/hooks/use-on-click-outside";
 import { useSelector } from "metabase/redux";
@@ -13,6 +12,7 @@ import { isWithinIframe } from "metabase/utils/iframe";
 import { useCommandPalette } from "../hooks/useCommandPalette";
 import { useCommandPaletteBasicActions } from "../hooks/useCommandPaletteBasicActions";
 
+import { HydratedKBarSearch } from "./HydratedKBarSearch";
 import S from "./Palette.module.css";
 import { PaletteResults } from "./PaletteResults";
 
@@ -58,6 +58,8 @@ export const PaletteContainer = withRouter(
   }) => {
     const { query } = useKBar();
     const ref = useRef(null);
+    const searchText =
+      typeof locationQuery.q === "string" ? locationQuery.q : "";
 
     const {
       searchRequestId,
@@ -83,10 +85,7 @@ export const PaletteContainer = withRouter(
       >
         <Stack gap={rem(4)} pb="lg">
           <Box pos="relative">
-            <KBarSearch
-              className={S.input}
-              defaultPlaceholder={t`Search for anything…`}
-            />
+            <HydratedKBarSearch searchText={searchText} />
 
             <Stack
               className={S.iconContainer}
