0810bac1a0ce4280384f5073e1323544a63474c6
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-18 19:39:13 +0300

    Accept {"field_order": [1,2,3]} in table api as wel as [1,2,3]

--- src/metabase/warehouse_schema_rest/api/table.clj
+++ src/metabase/warehouse_schema_rest/api/table.clj
@@ -428,8 +428,12 @@
   [{:keys [id]} :- [:map
                     [:id ms/PositiveInt]]
    _query-params
-   field-order :- [:sequential ms/PositiveInt]]
-  (-> (t2/select-one :model/Table :id id) api/write-check (table/custom-order-fields! field-order))
+   ;; Accept either a bare sequential (legacy) or a wrapped {:field_order [...]} body.
+   body :- [:or
+            [:sequential ms/PositiveInt]
+            [:map [:field_order [:sequential ms/PositiveInt]]]]]
+  (let [field-order (if (map? body) (:field_order body) body)]
+    (-> (t2/select-one :model/Table :id id) api/write-check (table/custom-order-fields! field-order)))
   {:success true})
 
 (mu/defn- update-csv!
