1c3d1bb01f47e0bd079c1e2f86c9e5eed45547d3
Phil Hagelberg <phil@hagelb.org>
2026-05-27 10:54:25 -0700

    Make with-temp-test-data use a randomized dataset name.
    This isn't a problem on most DBs, but on cloud DBs where everything is
    shared, we have to use a random name unless the dataset is immutable
    and never deleted.

--- test/metabase/actions/test_util.clj
+++ test/metabase/actions/test_util.clj
@@ -125,7 +125,9 @@
       ...)"
   {:style/indent :defn}
   [table-definitions & body]
-  `(do-with-dataset-definition (tx/dataset-definition "temp-test-data" ~table-definitions) (fn [] ~@body)))
+  `(do-with-dataset-definition (tx/dataset-definition (format "temp-test-data-%s" (random-uuid))
+                                                      ~table-definitions)
+                               (fn [] ~@body)))
 
 (defmacro with-empty-db
   "Sets the current dataset to a freshly created db that gets destroyed at the conclusion of `body`.
