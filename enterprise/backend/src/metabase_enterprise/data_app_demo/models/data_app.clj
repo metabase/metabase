903fbbb90bd70f81c7db21a696ee0081b36fe791
Timofei <timofei@metabase.com>
2026-05-28 01:35:34 +0700

    Setup app publishing and serving

---
+++ enterprise/backend/src/metabase_enterprise/data_app_demo/models/data_app.clj
@@ -0,0 +1,61 @@
+(ns metabase-enterprise.data-app-demo.models.data-app
+  (:require
+   [metabase.api.common :as api]
+   [metabase.models.interface :as mi]
+   [methodical.core :as methodical]
+   [toucan2.core :as t2])
+  (:import
+   (java.sql Blob)))
+
+(set! *warn-on-reflection* true)
+
+(methodical/defmethod t2/table-name :model/DataApp [_model] :data_app)
+
+(defn- blob->bytes ^bytes [v]
+  (cond
+    (nil? v)           nil
+    (instance? Blob v) (let [^Blob b v] (.getBytes b 1 (int (.length b))))
+    :else              v))
+
+(def ^:private transform-bundle
+  "Coerce JDBC `Blob` values into plain byte arrays on read."
+  {:in  identity
+   :out blob->bytes})
+
+(t2/deftransforms :model/DataApp
+  {:bundle transform-bundle})
+
+(doto :model/DataApp
+  (derive :metabase/model)
+  (derive :hook/timestamped?))
+
+(def non-blob-columns
+  "Columns to select for normal data-app metadata reads, excluding the raw bundle blob."
+  [:id :name :display_name :bundle_hash :creator_id :created_at :updated_at])
+
+(defn select-one-non-blob
+  "Like `t2/select-one` on `:model/DataApp`, but excludes the bundle blob."
+  [& conditions]
+  (apply t2/select-one (into [:model/DataApp] non-blob-columns) conditions))
+
+(defn select-non-blob
+  "Like `t2/select` on `:model/DataApp`, but excludes the bundle blob."
+  [& conditions]
+  (apply t2/select (into [:model/DataApp] non-blob-columns) conditions))
+
+(defmethod mi/can-read? :model/DataApp
+  ([_instance]   (some? api/*current-user-id*))
+  ([_model _pk]  (some? api/*current-user-id*)))
+
+(defmethod mi/can-write? :model/DataApp
+  ([_instance]   api/*is-superuser?*)
+  ([_model _pk]  api/*is-superuser?*))
+
+(defmethod mi/can-create? :model/DataApp
+  [_model _instance]
+  api/*is-superuser?*)
+
+(methodical/defmethod mi/to-json :model/DataApp
+  "Never include the raw bundle bytes in JSON."
+  [data-app json-generator]
+  (next-method (dissoc data-app :bundle) json-generator))
