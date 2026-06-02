b3e5ae05d12ef8ac5009101e1f7ea84f21ea079f
Fraser <fraser@metabase.com>
2026-06-03 10:10:49 +1200

    Fix nav violations (#74738)

--- frontend/src/metabase/app/nav/Navbar.tsx
+++ frontend/src/metabase/app/nav/Navbar.tsx
@@ -1,31 +1,42 @@
 import { useMemo } from "react";
 import { type WithRouterProps, withRouter } from "react-router";
 
-import { getAdminPaths } from "metabase/admin/app/selectors";
 import { useListDatabasesQuery } from "metabase/api";
+import { getDashboard } from "metabase/dashboard/selectors";
+import { AdminNavbar } from "metabase/nav/components/AdminNavbar";
+import { MainNavbar } from "metabase/nav/containers/MainNavbar";
 import { connect } from "metabase/redux";
-import type { AdminPath, State } from "metabase/redux/store";
+import type { AdminPath, State, StoreDashboard } from "metabase/redux/store";
+import { getAdminPaths } from "metabase/selectors/admin";
 import { getIsNavbarOpen } from "metabase/selectors/app";
 import { getUser } from "metabase/selectors/user";
 import type { User } from "metabase-types/api";
-
-import { AdminNavbar } from "../components/AdminNavbar";
-
-import MainNavbar from "./MainNavbar";
 
 type NavbarProps = WithRouterProps & {
   isOpen: boolean;
   user: User | null;
   adminPaths: AdminPath[];
+  dashboard?: StoreDashboard;
 };
 
 const mapStateToProps = (state: State) => ({
   isOpen: getIsNavbarOpen(state),
   user: getUser(state),
   adminPaths: getAdminPaths(state),
+  // Can't use the dashboard entity loader instead.
+  // The dashboard page uses DashboardsApi.get directly,
+  // so we can't re-use data between these components.
+  dashboard: getDashboard(state),
 });
 
-function Navbar({ isOpen, user, location, params, adminPaths }: NavbarProps) {
+function NavbarInner({
+  isOpen,
+  user,
+  location,
+  params,
+  adminPaths,
+  dashboard,
+}: NavbarProps) {
   useListDatabasesQuery();
   const isAdminApp = useMemo(
     () => location.pathname.startsWith("/admin/"),
@@ -39,9 +50,13 @@ function Navbar({ isOpen, user, location, params, adminPaths }: NavbarProps) {
   return isAdminApp ? (
     <AdminNavbar path={location.pathname} adminPaths={adminPaths} />
   ) : (
-    <MainNavbar isOpen={isOpen} location={location} params={params} />
+    <MainNavbar
+      isOpen={isOpen}
+      location={location}
+      params={params}
+      dashboard={dashboard}
+    />
   );
 }
 
-// eslint-disable-next-line import/no-default-export -- deprecated usage
-export default withRouter(connect(mapStateToProps)(Navbar));
+export const Navbar = withRouter(connect(mapStateToProps)(NavbarInner));
