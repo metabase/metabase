71d2d30e2188aeb088debbd3dd8595015d512279
Timofey Kachalov <timofei@metabase.com>
2026-06-11 15:24:21 +0400

    Add custom actions for data apps (#75627)

--- frontend/src/embedding-sdk-bundle/index.ts
+++ frontend/src/embedding-sdk-bundle/index.ts
@@ -92,6 +92,7 @@ const sdkBundleExports: MetabaseEmbeddingSdkBundleExports = {
   queryMetric,
   queryQuestion,
   dataAppRouting,
+  executeAction,
 };
 
 // Define a global export METABASE_EMBEDDING_SDK_BUNDLE for SDK package
