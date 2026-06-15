1f2138dfa02f2189208d540c13e83dc053a18668
Timofey Kachalov <timofei@metabase.com>
2026-06-15 23:03:34 +0400

    Add OSS/no token upsell banner placeholder for data apps (#75893)

--- enterprise/frontend/src/metabase-enterprise/data_apps/DataAppsNav.tsx
+++
@@ -1,9 +0,0 @@
-import { t } from "ttag";
-
-import { SettingsNavItem } from "metabase/admin/settings/components/SettingsNav";
-
-export const DataAppsNav = () => {
-  return (
-    <SettingsNavItem path="data-apps" label={t`Data apps`} icon="dashboard" />
-  );
-};
