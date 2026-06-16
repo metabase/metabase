9de278f2a86419cf47777c49d674939d449692b8
Phoomparin Mano <phoomparin@gmail.com>
2026-06-17 03:44:56 +0700

    remove useQuestionQuery and useMetricQuery hook

--- enterprise/frontend/src/embedding-sdk-package/index.ts
+++ enterprise/frontend/src/embedding-sdk-package/index.ts
@@ -68,7 +68,6 @@ export type {
   MetabaseQueryOptions,
   UseMetabaseQueryResult,
 } from "./hooks/public/use-metabase-query";
-export { useQuestionQuery } from "./hooks/public/use-question-query";
 export { defineMetabaseAuthConfig } from "./lib/public/define-metabase-auth-config";
 export { defineMetabaseTheme } from "./lib/public/define-metabase-theme";
 
@@ -202,27 +201,7 @@ export type {
   SchemaParameter,
   SchemaRow,
 } from "./hooks/public/data-schema";
-export type {
-  UseQuestionQueryOptions,
-  UseQuestionQueryResult,
-} from "./hooks/public/use-question-query";
-export type {
-  MetricBreakout,
-  MetricFilter,
-  UseMetricQueryOptions,
-  UseMetricQueryResult,
-} from "./hooks/public/use-metric-query";
-export type {
-  MetricSchema,
-  QuestionSchema,
-  QueryData,
-  SchemaColumn,
-  SchemaJavaScriptType,
-  SchemaParameter,
-  SchemaRow,
-} from "./hooks/public/data-schema";
 export type { QueryQuestionResult } from "embedding-sdk-bundle/lib/query-question";
-export type { QueryMetricResult } from "embedding-sdk-bundle/lib/query-metric";
 
 export type {
   EmbeddingEntityType,
