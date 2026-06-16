cc56b12f16357865e7ee3af559730828f180a07e
Phoomparin Mano <phoomparin@gmail.com>
2026-06-17 03:44:56 +0700

    remove useQuestionQuery and useMetricQuery hook

--- enterprise/frontend/src/embedding-sdk-package/hooks/public/use-metric-query/index.ts
+++
@@ -1,7 +0,0 @@
-export { useMetricQuery } from "./use-metric-query";
-export type {
-  MetricBreakout,
-  MetricFilter,
-  UseMetricQueryOptions,
-  UseMetricQueryResult,
-} from "./use-metric-query";
--- enterprise/frontend/src/embedding-sdk-package/hooks/public/use-question-query/index.ts
+++
@@ -1,5 +0,0 @@
-export { useQuestionQuery } from "./use-question-query";
-export type {
-  UseQuestionQueryOptions,
-  UseQuestionQueryResult,
-} from "./use-question-query";
