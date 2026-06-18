8e2c0cc1788203aec532a5464486e8995643cf92
Fraser <fraser@metabase.com>
2026-06-18 21:12:32 +1200

    Remove legacy SettingsApi service (#76046)

--- frontend/src/metabase/services.js
+++
@@ -1,7 +0,0 @@
-import { GET, PUT } from "metabase/api/legacy-client";
-
-export const SettingsApi = {
-  list: GET("/api/setting"),
-  put: PUT("/api/setting/:key"),
-  putAll: PUT("/api/setting"),
-};
