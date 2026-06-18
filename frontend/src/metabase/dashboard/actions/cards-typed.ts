metabase/metabase

commit 5325f240e9f70f2e6b42f6c8e6fe4b94d3e44a56

Remove Question entity (#74652)
* Migrate question entity-framework hooks off the Questions entity

* Migrate read-only Question consumers off the Questions entity

* Migrate dashboard card actions off the Questions entity

* Migrate query-builder card actions off the Questions entity

* Remove the questions entity

* Do not drop dashcard sizes in api call

* Handle errors in card query

* Add error/loading state to action creator modal

* Avoid refetching when forbidden

* Avoid stale cards in card mutations

frontend/src/metabase/dashboard/actions/cards-typed.ts
@@ -3,7 +3,7 @@ import { t } from "ttag";
 import _ from "underscore";
 
 import { cardApi } from "metabase/api";
-import { Questions } from "metabase/entities/questions";
+import { entityCompatibleQuery } from "metabase/entities/utils";
 import { loadMetadataForCard } from "metabase/questions/actions";
 import { createThunkAction } from "metabase/redux";
 import type { Dispatch, GetState } from "metabase/redux/store";
@@ -172,11 +172,12 @@ export type AddCardToDashboardOpts = NewDashCardOpts & {
 
 export const addCardToDashboard =
   ({ dashId, tabId, cardId }: AddCardToDashboardOpts) =>
-  async (dispatch: Dispatch, getState: GetState) => {
-    await dispatch(Questions.actions.fetch({ id: cardId }));
-    const card = Questions.selectors
-      .getObject(getState(), { entityId: cardId })
-      .card();
+  async (dispatch: Dispatch) => {
+    const card = await entityCompatibleQuery(
+      { id: cardId },
+      dispatch,
+      cardApi.endpoints.getCard,
+    );
 
     const dashcardId = generateTemporaryDashcardId();
     const dashcard = dispatch(
@@ -252,10 +253,11 @@ export const replaceCard =
   async (dispatch: Dispatch, getState: GetState) => {
     const dashboardId = getDashboardId(getState());
 
-    await dispatch(Questions.actions.fetch({ id: nextCardId }));
-    const card = Questions.selectors
-      .getObject(getState(), { entityId: nextCardId })
-      .card();
+    const card = await entityCompatibleQuery(
+      { id: nextCardId },
+      dispatch,
+      cardApi.endpoints.getCard,
+    );
 
     await dispatch(
       setDashCardAttributes({
@@ -296,10 +298,11 @@ export const addCardWithVisualization =
     const cards: Card[] = [];
 
     for (const cardId of cardIds) {
-      await dispatch(Questions.actions.fetch({ id: cardId }));
-      const card: Card = Questions.selectors
-        .getObject(getState(), { entityId: cardId })
-        .card();
+      const card: Card = await entityCompatibleQuery(
+        { id: cardId },
+        dispatch,
+        cardApi.endpoints.getCard,
+      );
       cards.push(card);
     }
 
@@ -345,10 +348,11 @@ export const replaceCardWithVisualization =
     const cards: Card[] = [];
 
     for (const cardId of cardIds) {
-      await dispatch(Questions.actions.fetch({ id: cardId }));
-      const card: Card = Questions.selectors
-        .getObject(getState(), { entityId: cardId })
-        .card();
+      const card: Card = await entityCompatibleQuery(
+        { id: cardId },
+        dispatch,
+        cardApi.endpoints.getCard,
+      );
       cards.push(card);
     }
 

frontend/src/metabase/dashboard/actions/cards.unit.spec.ts
@@ -360,7 +360,7 @@ async function runAddCardToDashboard({
     dashId,
     tabId,
     cardId,
-  })(store.dispatch, store.getState);
+  })(store.dispatch);
   const nextState = store.getState();
 
   const tempDashCardId =

enterprise/frontend/src/embedding/data-picker/DataSelector/DataSelector.jsx
@@ -5,6 +5,7 @@ import { t } from "ttag";
 import _ from "underscore";
 
 import {
+  cardApi,
   databaseApi,
   useLazyListDatabaseSchemaTablesQuery,
   useLazyListDatabaseSchemasQuery,
@@ -14,7 +15,6 @@ import {
 import { EmptyState } from "metabase/common/components/EmptyState";
 import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
 import CS from "metabase/css/core/index.css";
-import { Questions } from "metabase/entities/questions";
 import { entityCompatibleQuery } from "metabase/entities/utils";
 import { connect } from "metabase/redux";
 import { fetchTableMetadata } from "metabase/redux/tables";
@@ -957,9 +957,9 @@ const DataSelector = _.compose(
         }),
         hasDataAccess: canUserCreateQueries(state),
         hasNestedQueriesEnabled: getSetting(state, "enable-nested-queries"),
-        selectedQuestion: Questions.selectors.getObject(state, {
-          entityId: getQuestionIdFromVirtualTableId(ownProps.selectedTableId),
-        }),
+        selectedQuestion: getMetadata(state).question(
+          getQuestionIdFromVirtualTableId(ownProps.selectedTableId),
+        ),
       };
     },
     (dispatch) => ({
@@ -972,10 +972,10 @@ const DataSelector = _.compose(
         ),
       fetchFields: (tableId) => dispatch(fetchTableMetadata({ id: tableId })),
       fetchQuestion: (id) =>
-        dispatch(
-          Questions.actions.fetch({
-            id: getQuestionIdFromVirtualTableId(id),
-          }),
+        entityCompatibleQuery(
+          { id: getQuestionIdFromVirtualTableId(id) },
+          dispatch,
+          cardApi.endpoints.getCard,
         ),
     }),
   ),

enterprise/frontend/src/metabase-enterprise/sandboxes/components/EditSandboxingModal/EditSandboxingModal.tsx
@@ -16,10 +16,10 @@ import {
   getQuestionPickerValue,
 } from "metabase/common/components/Pickers/QuestionPicker";
 import { QuestionLoader } from "metabase/common/components/QuestionLoader";
+import { QuestionName } from "metabase/common/components/QuestionName";
 import { Radio } from "metabase/common/components/Radio";
 import { useToggle } from "metabase/common/hooks/use-toggle";
 import CS from "metabase/css/core/index.css";
-import { EntityName } from "metabase/entities/containers/EntityName";
 import { useTranslateContent } from "metabase/i18n/hooks";
 import { GTAPApi } from "metabase/services";
 import { Button, Center, Icon, Loader } from "metabase/ui";
@@ -353,10 +353,7 @@ const PolicySummary = ({ policy, policyTable }: PolicySummaryProps) => {
           policy.card_id
             ? jt`rows in the ${(
                 <strong key="question-name">
-                  <EntityName
-                    entityType="questions"
-                    entityId={policy.card_id}
-                  />
+                  <QuestionName id={policy.card_id} />
                 </strong>
               )} question`
             : jt`rows in the ${(

frontend/src/metabase/actions/containers/ActionCreator/ActionCreator.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import type { Route } from "react-router";
 import { t } from "ttag";
-import _ from "underscore";
 
 import {
   skipToken,
   useCreateActionMutation,
   useGetActionQuery,
+  useGetCardQuery,
   useListDatabasesQuery,
   useUpdateActionMutation,
 } from "metabase/api";
@@ -15,8 +15,7 @@ import { Modal } from "metabase/common/components/Modal";
 import { useBeforeUnload } from "metabase/common/hooks/use-before-unload";
 import { useCallbackEffect } from "metabase/common/hooks/use-callback-effect";
 import { useToast } from "metabase/common/hooks/use-toast";
-import { Questions } from "metabase/entities/questions";
-import { connect } from "metabase/redux";
+import { connect, useSelector } from "metabase/redux";
 import type { State } from "metabase/redux/store";
 import { getMetadata } from "metabase/selectors/metadata";
 import type Question from "metabase-lib/v1/Question";
@@ -42,7 +41,7 @@ interface OwnProps {
   databaseId?: DatabaseId;
 
   action?: WritebackAction;
-  route: Route;
+  route?: Route;
 
   onSubmit?: (action: WritebackAction) => void;
   onClose?: () => void;
@@ -201,8 +200,14 @@ function ActionCreatorWithContext({
   databaseId,
   action,
   ...props
-}: Props) {
+}: OwnProps & StateProps) {
   useListDatabasesQuery();
+  useGetCardQuery(props.modelId != null ? { id: props.modelId } : skipToken);
+  const model = useSelector((state) =>
+    props.modelId != null
+      ? (getMetadata(state).question(props.modelId) ?? undefined)
+      : undefined,
+  );
   const { data: initialAction } = useGetActionQuery(
     props.actionId != null ? { id: props.actionId } : skipToken,
   );
@@ -215,16 +220,15 @@ function ActionCreatorWithContext({
       databaseId={databaseId}
       metadata={metadata}
     >
-      <ActionCreator {...props} databaseId={databaseId} metadata={metadata} />
+      <ActionCreator
+        {...props}
+        model={model}
+        databaseId={databaseId}
+        metadata={metadata}
+      />
     </ActionContext>
   );
 }
 
 // eslint-disable-next-line import/no-default-export -- deprecated usage
-export default _.compose(
-  Questions.load({
-    id: (state: State, props: OwnProps) => props?.modelId,
-    entityAlias: "model",
-  }),
-  connect(mapStateToProps),
-)(ActionCreatorWithContext);
+export default connect(mapStateToProps)(ActionCreatorWithContext);

frontend/src/metabase/actions/containers/ActionCreatorModal/ActionCreatorModal.tsx
@@ -2,13 +2,14 @@ import type { LocationDescriptor } from "history";
 import { useEffect } from "react";
 import type { Route } from "react-router";
 import { replace } from "react-router-redux";
-import _ from "underscore";
 
-import { skipToken, useGetActionQuery } from "metabase/api";
-import { Questions } from "metabase/entities/questions";
-import { connect } from "metabase/redux";
+import { skipToken, useGetActionQuery, useGetCardQuery } from "metabase/api";
+import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
+import type { ModalComponentProps } from "metabase/hoc/ModalRoute";
+import { connect, useSelector } from "metabase/redux";
 import { setErrorPage } from "metabase/redux/app";
-import type { AppErrorDescriptor, State } from "metabase/redux/store";
+import type { AppErrorDescriptor } from "metabase/redux/store";
+import { getMetadata } from "metabase/selectors/metadata";
 import * as Urls from "metabase/urls";
 import type Question from "metabase-lib/v1/Question";
 import type { WritebackAction } from "metabase-types/api";
@@ -17,11 +18,10 @@ import ActionCreator from "../ActionCreator";
 
 interface OwnProps {
   params: {
-    slug: string;
+    slug?: string;
     actionId?: string;
   };
   onClose: () => void;
-  onChangeLocation: (location: LocationDescriptor) => void;
 }
 
 interface EntityLoaderProps {
@@ -31,7 +31,7 @@ interface EntityLoaderProps {
 }
 
 interface RouteProps {
-  route: Route;
+  route?: Route;
 }
 
 interface DispatchProps {
@@ -60,7 +60,7 @@ function ActionCreatorModal({
 }: ActionCreatorModalProps) {
   const actionId = Urls.extractEntityId(params.actionId);
   const modelId = Urls.extractEntityId(params.slug);
-  const databaseId = model.databaseId();
+  const databaseId = model.databaseId() ?? undefined;
 
   const { isLoading: isActionLoading, data: action } = useGetActionQuery(
     actionId === undefined ? skipToken : { id: actionId },
@@ -99,15 +99,33 @@ function ActionCreatorModal({
   );
 }
 
-function getModelId(state: State, props: OwnProps) {
-  return Urls.extractEntityId(props.params.slug);
+function ActionCreatorModalLoader({
+  params,
+  onClose,
+  ...dispatchProps
+}: ModalComponentProps & DispatchProps) {
+  const modelId = Urls.extractEntityId(params.slug);
+  const { isLoading, error } = useGetCardQuery(
+    modelId != null ? { id: modelId } : skipToken,
+  );
+  const model = useSelector((state) =>
+    modelId != null ? getMetadata(state).question(modelId) : undefined,
+  );
+
+  if (isLoading || error != null || !model) {
+    return <LoadingAndErrorWrapper loading={isLoading} error={error} />;
+  }
+
+  return (
+    <ActionCreatorModal
+      params={params}
+      onClose={onClose}
+      model={model}
+      loading={isLoading}
+      {...dispatchProps}
+    />
+  );
 }
 
 // eslint-disable-next-line import/no-default-export -- deprecated usage
-export default _.compose(
-  Questions.load({
-    id: getModelId,
-    entityAlias: "model",
-  }),
-  connect(null, mapDispatchToProps),
-)(ActionCreatorModal);
+export default connect(null, mapDispatchToProps)(ActionCreatorModalLoader);

frontend/src/metabase/common/components/QuestionName.tsx
@@ -1,0 +1,24 @@
+import { useGetCardQuery } from "metabase/api";
+import { useTranslateContent } from "metabase/i18n/hooks";
+import { getName } from "metabase/utils/name";
+import type { CardId } from "metabase-types/api";
+
+interface QuestionNameProps {
+  id: CardId | null | undefined;
+}
+
+export const QuestionName = ({ id }: QuestionNameProps) => {
+  if (id == null || (typeof id === "number" && isNaN(id))) {
+    return null;
+  }
+  return <FetchedQuestionName id={id} />;
+};
+
+const FetchedQuestionName = ({ id }: { id: CardId }) => {
+  const tc = useTranslateContent();
+  const { currentData: card } = useGetCardQuery({ id });
+  if (!card) {
+    return null;
+  }
+  return <span>{tc(getName(card))}</span>;
+};

frontend/src/metabase/common/components/QuestionName.unit.spec.tsx
@@ -1,0 +1,65 @@
+import fetchMock from "fetch-mock";
+
+import { setupCardEndpoints } from "__support__/server-mocks";
+import { renderWithProviders, screen, waitFor } from "__support__/ui";
+import { createMockCard } from "metabase-types/api/mocks";
+
+import { QuestionName } from "./QuestionName";
+
+const TEST_CARD = createMockCard({ name: "Sales overview" });
+
+const renderName = (id: Parameters<typeof QuestionName>[0]["id"]) =>
+  renderWithProviders(
+    <div data-testid="wrapper">
+      <QuestionName id={id} />
+    </div>,
+  );
+
+describe("QuestionName", () => {
+  it("renders the question name returned from the API", async () => {
+    setupCardEndpoints(TEST_CARD);
+
+    renderName(TEST_CARD.id);
+
+    expect(await screen.findByText("Sales overview")).toBeInTheDocument();
+  });
+
+  it("renders nothing while the question is loading", () => {
+    setupCardEndpoints(TEST_CARD);
+
+    renderName(TEST_CARD.id);
+
+    expect(screen.getByTestId("wrapper")).toBeEmptyDOMElement();
+  });
+
+  it("renders nothing when the question cannot be found", async () => {
+    fetchMock.get(`path:/api/card/${TEST_CARD.id}`, 404);
+
+    renderName(TEST_CARD.id);
+
+    await waitFor(() => {
+      expect(
+        fetchMock.callHistory.calls(`path:/api/card/${TEST_CARD.id}`),
+      ).toHaveLength(1);
+    });
+    expect(screen.getByTestId("wrapper")).toBeEmptyDOMElement();
+  });
+
+  it("renders nothing when the id is null", () => {
+    renderName(null);
+
+    expect(screen.getByTestId("wrapper")).toBeEmptyDOMElement();
+  });
+
+  it("renders nothing when the id is undefined", () => {
+    renderName(undefined);
+
+    expect(screen.getByTestId("wrapper")).toBeEmptyDOMElement();
+  });
+
+  it("renders nothing when the id is NaN", () => {
+    renderName(NaN);
+
+    expect(screen.getByTestId("wrapper")).toBeEmptyDOMElement();
+  });
+});

commit 79538a409b366e9bd70f0d1ee72ced34ce3c90b5

fix: restore cache-first fetch in addCardToDashboard
Regression from 5325f240e9f (Remove Question entity, PR #74652) which
replaced `Questions.actions.fetch` with `runRtkEndpoint`. That refactor
changed the fetch behavior: `Questions.actions.fetch` was cache-first
(read entity store synchronously on cache hit), while `runRtkEndpoint`
defaults to `forceRefetch: true`, always hitting the network even when
the card is already in the RTK Query cache.

Fix: check `cardApi.endpoints.getCard.select({ id: cardId })(getState())`
first; fall through to `runRtkEndpoint({ forceRefetch: false })` only
on a cache miss.

On a cache hit, the fetch now resolves synchronously before any await,
which means `addDashCardToDashboard` (setting dashcard.isAdded = true)
is dispatched before the user can navigate away — closing the race
that caused DashboardLeaveConfirmationModal to not appear.

Affects both the main app dashboard (DashboardApp, AddCardSidebar) and
the Embedding SDK EditableDashboard.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01RqZciiDtVTuLso7nQ3LFG3

frontend/src/metabase/dashboard/actions/cards-typed.ts
@@ -172,12 +172,20 @@ export type AddCardToDashboardOpts = NewDashCardOpts & {
 
 export const addCardToDashboard =
   ({ dashId, tabId, cardId }: AddCardToDashboardOpts) =>
-  async (dispatch: Dispatch) => {
-    const card = await runRtkEndpoint(
-      { id: cardId },
-      dispatch,
-      cardApi.endpoints.getCard,
-    );
+  async (dispatch: Dispatch, getState: GetState) => {
+    const cached = cardApi.endpoints.getCard.select({ id: cardId })(
+      getState(),
+    ).data;
+    const card =
+      cached ??
+      (await runRtkEndpoint(
+        { id: cardId },
+        dispatch,
+        cardApi.endpoints.getCard,
+        {
+          forceRefetch: false,
+        },
+      ));
 
     const dashcardId = generateTemporaryDashcardId();
     const dashcard = dispatch(

frontend/src/metabase/dashboard/actions/cards.unit.spec.ts
@@ -356,7 +356,7 @@ async function runAddCardToDashboard({
     dashId,
     tabId,
     cardId,
-  })(store.dispatch);
+  })(store.dispatch, store.getState);
   const nextState = store.getState();
 
   const tempDashCardId =