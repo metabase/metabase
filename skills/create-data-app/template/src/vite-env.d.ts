c39b90f5054cda11e4bf9d55d2525b97700ed080
Timofey Kachalov <timofei@metabase.com>
2026-06-26 16:47:53 +0400

    Rename data app env variables and read them only in dev mode (#76604)

--- skills/create-data-app/template/src/vite-env.d.ts
+++ skills/create-data-app/template/src/vite-env.d.ts
@@ -1,6 +1,6 @@
 interface ImportMetaEnv {
-  readonly VITE_MB_URL: string;
-  readonly VITE_MB_API_KEY: string;
+  readonly DATA_APP_MB_URL: string;
+  readonly DATA_APP_MB_API_KEY: string;
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv;
