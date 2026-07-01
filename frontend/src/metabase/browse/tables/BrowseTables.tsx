7882d7831a568cd259a0ccb0dcc73e00d724061a
Uladzimir Havenchyk <uladzimir@metabase.com>
2026-07-01 16:12:52 +0300

    handle schema named permalinks

--- frontend/src/metabase/browse/tables/BrowseTables.tsx
+++ frontend/src/metabase/browse/tables/BrowseTables.tsx
@@ -1,9 +1,46 @@
+import { useEffect } from "react";
+import { replace } from "react-router-redux";
+
+import { useListDatabasesQuery } from "metabase/api";
+import { NotFound } from "metabase/common/components/ErrorPages";
+import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
+import { findDatabaseByName } from "metabase/common/utils/database";
+import { useDispatch } from "metabase/redux";
 import { Flex } from "metabase/ui";
+import * as Urls from "metabase/urls";
 
 import S from "../components/BrowseContainer.module.css";
 import { BrowseDataHeader } from "../components/BrowseDataHeader";
 
 import { TableBrowser } from "./TableBrowser";
+
+const SchemaNameRedirect = ({
+  name,
+  schemaName,
+}: {
+  name: string;
+  schemaName: string;
+}) => {
+  const dispatch = useDispatch();
+  const { data, isLoading } = useListDatabasesQuery();
+  const database = findDatabaseByName(data?.data ?? [], name);
+
+  useEffect(() => {
+    if (database) {
+      dispatch(
+        replace(
+          Urls.browseSchema({ db_id: database.id, schema_name: schemaName }),
+        ),
+      );
+    }
+  }, [database, schemaName, dispatch]);
+
+  if (isLoading || database) {
+    return <LoadingAndErrorWrapper loading />;
+  }
+
+  return <NotFound />;
+};
 
 export const BrowseTables = ({
   params: { dbId, schemaName },
@@ -13,6 +50,10 @@ export const BrowseTables = ({
     schemaName: string;
   };
 }) => {
+  if (Urls.extractEntityId(dbId) == null) {
+    return <SchemaNameRedirect name={dbId} schemaName={schemaName} />;
+  }
+
   return (
     <Flex
       className={S.browseContainer}
