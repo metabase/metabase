b32659efdd3a630c5f762865177e904f41efedb0
Romeo Van Snick <romeo@romeovansnick.be>
2026-07-02 02:41:51 -0700

    Migrate DataSelector unit tests to TypeScript

---
+++ frontend/src/metabase/querying/common/components/DataSelector/DataSelectorSchemaPicker/DataSelectorSchemaPicker.unit.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "__support__/ui";
+import { checkNotNull } from "metabase/utils/types";
+import type Schema from "metabase-lib/v1/metadata/Schema";
+import { getSchemaDisplayName } from "metabase-lib/v1/metadata/utils/schema";
+
+import { DataSelectorSchemaPicker } from "./DataSelectorSchemaPicker";
+
+describe("DataSelectorSchemaPicker", () => {
+  it("displays schema name", () => {
+    const schemaName = "Schema name";
+    const schemas = [{ name: schemaName }] as Schema[];
+    render(
+      <DataSelectorSchemaPicker
+        schemas={schemas}
+        hasFiltering
+        hasInitialFocus
+        hasNextStep={false}
+        isLoading={false}
+        onChangeSchema={jest.fn()}
+      />,
+    );
+
+    expect(
+      screen.getByText(checkNotNull(getSchemaDisplayName(schemaName))),
+    ).toBeInTheDocument();
+  });
+});
