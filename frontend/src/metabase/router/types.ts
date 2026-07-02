78a9d1cb7d90a3b5b74e02bd8a0ef8947927e834
Romeo Van Snick <romeo@romeovansnick.be>
2026-07-02 15:58:25 -0700

    Link react-router v7 API reference from facade types

--- frontend/src/metabase/router/types.ts
+++ frontend/src/metabase/router/types.ts
@@ -1,13 +1,26 @@
+/**
+ * A partial path, mirroring react-router v7's `Path`.
+ *
+ * @see https://api.reactrouter.com/v7/interfaces/react-router.Path.html
+ */
 export interface Path {
   pathname: string;
   search: string;
   hash: string;
 }
 
-/** A destination, mirroring react-router v7's `To`. */
+/**
+ * A destination, mirroring react-router v7's `To`.
+ *
+ * @see https://api.reactrouter.com/v7/types/react-router.To.html
+ */
 export type To = string | Partial<Path>;
 
-/** A location, mirroring react-router v7's `Location`. */
+/**
+ * A location, mirroring react-router v7's `Location`.
+ *
+ * @see https://api.reactrouter.com/v7/interfaces/react-router.Location.html
+ */
 export interface Location<State = unknown> {
   pathname: string;
   search: string;
@@ -16,26 +29,54 @@ export interface Location<State = unknown> {
   key: string;
 }
 
+/**
+ * Navigation options, mirroring react-router v7's `NavigateOptions`.
+ *
+ * @see https://api.reactrouter.com/v7/interfaces/react-router.NavigateOptions.html
+ */
 export interface NavigateOptions {
   replace?: boolean;
   state?: unknown;
 }
 
+/**
+ * The function returned by `useNavigate`, mirroring react-router v7's
+ * `NavigateFunction`.
+ *
+ * @see https://api.reactrouter.com/v7/interfaces/react-router.NavigateFunction.html
+ */
 export interface NavigateFunction {
   (to: To, options?: NavigateOptions): void;
   (delta: number): void;
 }
 
+/**
+ * Parsed route params, mirroring react-router v7's `Params`.
+ *
+ * @see https://api.reactrouter.com/v7/types/react-router.Params.html
+ */
 export type Params<Key extends string = string> = {
   readonly [key in Key]: string | undefined;
 };
 
+/**
+ * Accepted inputs for building search params, mirroring react-router v7's
+ * `URLSearchParamsInit`.
+ *
+ * @see https://api.reactrouter.com/v7/types/react-router.URLSearchParamsInit.html
+ */
 export type URLSearchParamsInit =
   | string
   | URLSearchParams
   | string[][]
   | Record<string, string | string[]>;
 
+/**
+ * The setter returned by `useSearchParams`, mirroring react-router v7's
+ * `SetURLSearchParams`.
+ *
+ * @see https://api.reactrouter.com/v7/types/react-router.SetURLSearchParams.html
+ */
 export type SetURLSearchParams = (
   nextInit?:
     | URLSearchParamsInit
