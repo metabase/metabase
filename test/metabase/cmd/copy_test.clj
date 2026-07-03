3e3bdda0c809bfeabde2e69d03c1b505fd828ff1
Nicola Mometto <nicola.mometto@metabase.com>
2026-07-03 13:33:18 +0100

    exclude model

--- test/metabase/cmd/copy_test.clj
+++ test/metabase/cmd/copy_test.clj
@@ -73,6 +73,7 @@
     :model/Transform
     :model/TransformRun
     :model/TransformRunCancelation
+    :model/TransformDagRun
     :model/TransformJob
     :model/TransformJobRun
     :model/TransformJobTransformTag
