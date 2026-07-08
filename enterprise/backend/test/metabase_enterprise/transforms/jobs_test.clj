6b34a1f1baf564ac0a1247b000ec6c7f283a57f0
Ngoc Khuat <qn.khuat@gmail.com>
2026-07-08 20:59:08 +0700

    Fix redshift transform job dependency-detection flake (#77095)

--- enterprise/backend/test/metabase_enterprise/transforms/jobs_test.clj
+++ enterprise/backend/test/metabase_enterprise/transforms/jobs_test.clj
@@ -10,6 +10,7 @@
    [metabase-enterprise.transforms.test-dataset :as transforms-dataset]
    [metabase-enterprise.transforms.test-util :refer [with-transform-cleanup!]]
    [metabase.driver :as driver]
+   [metabase.driver.sql.util :as sql.u]
    [metabase.lib.core :as lib]
    [metabase.lib.metadata :as lib.metadata]
    [metabase.notification.seed :as notification.seed]
@@ -303,7 +304,8 @@
                                  ;; depends on faulty transform
                                  :model/Transform t2 {:name "transform2"
                                                       :source {:type :query
-                                                               :query (lib/native-query mp (str/replace sql "transforms_products" (:name target1)))}
+                                                               :query (lib/native-query mp (format "SELECT * FROM %s"
+                                                                                                   (sql.u/quote-name driver/*driver* :table (:schema target1) (:name target1))))}
                                                       :creator_id (mt/user->id :crowberto)
                                                       :target target2}
                                  :model/TransformTransformTag _tag2 {:transform_id (:id t2)
