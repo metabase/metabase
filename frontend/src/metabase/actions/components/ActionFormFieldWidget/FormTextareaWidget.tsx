35f9c21d596d0f429b217a13dd9093b0ea82cda5
Egor Grushin <egor@metabase.com>
2026-07-09 02:09:36 +0100

    GDGT-2584 DRYed FormTextareaWidget

--- frontend/src/metabase/actions/components/ActionFormFieldWidget/FormTextareaWidget.tsx
+++ frontend/src/metabase/actions/components/ActionFormFieldWidget/FormTextareaWidget.tsx
@@ -1,11 +1,8 @@
-import { useField } from "formik";
-import type { ChangeEvent, ReactNode, Ref } from "react";
-import { forwardRef, useCallback } from "react";
+import type { ReactNode, Ref } from "react";
+import { forwardRef } from "react";
 
 import type { ActionFormFieldProps } from "metabase/actions/types";
-import { FormField } from "metabase/common/components/FormField";
-import { useUniqueId } from "metabase/common/hooks/use-unique-id";
-import { Textarea } from "metabase/ui";
+import { FormTextarea } from "metabase/forms";
 
 type FormTextareaWidgetProps = ActionFormFieldProps & {
   actions?: ReactNode;
@@ -28,46 +25,14 @@ export const FormTextareaWidget = forwardRef(function FormTextareaWidget(
   }: FormTextareaWidgetProps,
   ref: Ref<HTMLTextAreaElement>,
 ) {
-  const id = useUniqueId();
-  const [{ value }, { error, touched }, { setValue, setTouched }] =
-    useField(name);
-
-  const handleChange = useCallback(
-    (event: ChangeEvent<HTMLTextAreaElement>) => {
-      const newValue = event.target.value;
-      if (newValue === "") {
-        setValue(nullable ? null : undefined);
-      } else {
-        setValue(newValue);
-      }
-    },
-    [nullable, setValue],
-  );
-
-  const handleBlur = useCallback(() => {
-    setTouched(true);
-  }, [setTouched]);
-
   return (
-    <FormField
-      title={title}
-      actions={actions}
-      description={description}
-      htmlFor={id}
-      error={touched ? error : undefined}
-      optional={optional}
-    >
-      <Textarea
-        {...props}
-        ref={ref}
-        id={id}
-        name={name}
-        value={value ?? ""}
-        error={touched && Boolean(error)}
-        minRows={5}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-    </FormField>
+    <FormTextarea
+      {...props}
+      ref={ref}
+      name={name}
+      label={title}
+      nullable={nullable}
+      minRows={5}
+    />
   );
 });
