987923ed97b2b1d628ae23d09e9cc08ce2862d33
Kamil Mielnik <kamil@kamilmielnik.com>
2026-07-10 15:54:56 +0700

    Fix cast, move justification

--- e2e/test/scenarios/dashboard-filters-2/shared/dashboard-filters-query-stages.ts
+++ e2e/test/scenarios/dashboard-filters-2/shared/dashboard-filters-query-stages.ts
@@ -3,6 +3,7 @@ import { SAMPLE_DATABASE } from "e2e/support/cypress_sample_database";
 import type {
   Card,
   ConcreteFieldReference,
+  DashboardId,
   StructuredQuery,
 } from "metabase-types/api";
 
@@ -724,13 +725,8 @@ export function goBackToDashboard() {
   cy.findByTestId("dashboard-grid").should("be.visible");
 }
 
-export function getDashboardId(): Cypress.Chainable<number> {
-  return (
-    cy
-      .get("@dashboardId")
-      // Unjustified type cast. FIXME
-      .then((dashboardId) => dashboardId as unknown as number)
-  );
+export function getDashboardId(): Cypress.Chainable<DashboardId> {
+  return cy.get<DashboardId>("@dashboardId").then((dashboardId) => dashboardId);
 }
 
 export function waitForPublicDashboardData(requestCount: number) {
