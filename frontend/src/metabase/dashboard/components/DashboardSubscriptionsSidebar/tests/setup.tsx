ae3c380545add791b541a1ec78f9f23f09d2c823
Nick Fitzpatrick <nickfitz.582@gmail.com>
2026-07-14 00:57:24 +0530

    Show all connected parameters when setting up a subscription

--- frontend/src/metabase/dashboard/components/DashboardSubscriptionsSidebar/tests/setup.tsx
+++ frontend/src/metabase/dashboard/components/DashboardSubscriptionsSidebar/tests/setup.tsx
@@ -10,6 +10,7 @@ import { renderWithProviders } from "__support__/ui";
 import { getNextId } from "__support__/utils";
 import { MockDashboardContext } from "metabase/dashboard/context/mock-context";
 import { isEmbeddingSdk as mockIsEmbeddingSdk } from "metabase/embedding-sdk/config";
+import type { SelectedTabId } from "metabase/redux/store";
 import {
   createMockDashboardState,
   createMockState,
@@ -19,6 +20,7 @@ import type {
   Dashboard,
   DashboardCard,
   DashboardSubscription,
+  DashboardTab,
   TokenFeatures,
 } from "metabase-types/api";
 import {
@@ -62,9 +64,11 @@ const defaultParameters = [
 function createDashboardState(
   dashboard: Dashboard,
   dashcards: DashboardCard[],
+  selectedTabId: SelectedTabId = null,
 ) {
   return createMockDashboardState({
     dashboardId: dashboard.id,
+    selectedTabId,
     dashcards: dashcards.reduce(
       (acc, card) => {
         acc[card.id] = card;
@@ -89,6 +93,8 @@ type SetupOpts = {
   isAdmin?: boolean;
   dashcards?: DashboardCard[];
   parameters?: UiParameter[];
+  tabs?: DashboardTab[];
+  selectedTabId?: SelectedTabId;
   isEmbeddingSdk?: boolean;
   setSharing?: (sharing: boolean) => void;
   pulses?: (Partial<DashboardSubscription> & { id: number })[];
@@ -107,6 +113,8 @@ export function setup({
   isAdmin = false,
   dashcards = defaultDashcards,
   parameters = defaultParameters,
+  tabs,
+  selectedTabId = null,
   isEmbeddingSdk = false,
   setSharing,
   pulses = [],
@@ -116,6 +124,7 @@ export function setup({
   const dashboard = createMockDashboard({
     dashcards,
     parameters,
+    tabs,
   });
 
   const channelData: {
@@ -206,7 +215,7 @@ export function setup({
           last_name: currentUser?.lastName,
           is_superuser: isAdmin,
         }),
-        dashboard: createDashboardState(dashboard, dashcards),
+        dashboard: createDashboardState(dashboard, dashcards, selectedTabId),
       }),
     },
   );
