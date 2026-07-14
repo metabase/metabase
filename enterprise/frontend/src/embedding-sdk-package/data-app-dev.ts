7ef141cf6657427f48cbd157f6ca587602ce587d
Timofei <timofei@metabase.com>
2026-07-14 08:26:26 +0400

    Fix import

--- enterprise/frontend/src/embedding-sdk-package/data-app-dev.ts
+++ enterprise/frontend/src/embedding-sdk-package/data-app-dev.ts
@@ -1,8 +1,4 @@
 // Browser part of the `@metabase/embedding-sdk-react/data-app-dev` with dev app client exports
-export {
-  DataAppDevProvider,
-  type DataAppDevProviderProps,
-} from "./components/public/DataAppDevProvider/DataAppDevProvider";
 export { DevToolbar } from "./components/public/debug/DevToolbar/DevToolbar";
 export { installDevDiagnostics } from "./components/public/debug/DevToolbar/diagnostics";
 export { createDataAppSandbox } from "metabase-enterprise/data_apps/sandbox";
