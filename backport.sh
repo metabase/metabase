46975470457621c56a7c47f9fe4773d0d4f0f632
losrebellos <ben@benoitvinay.com>
2026-07-16 09:56:41 +0200

    Remove backport script

--- backport.sh
+++
@@ -1,4 +0,0 @@
-git reset HEAD~1
-rm ./backport.sh
-git cherry-pick b2d652c6ab417a40c354cb5fd9353a97c3e40f06
-echo 'Resolve conflicts and force push this branch.\n\nTo backport translations run: bin/i18n/merge-translations <release-branch>'
