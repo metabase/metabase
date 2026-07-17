b0b11dca9187d9adbb3bfd3162bfd6d550944a6d
Nicola Mometto <nicola.mometto@metabase.com>
2026-07-17 09:29:22 +0100

    test seccomp

--- .github/scripts/setup-python-runner.sh
+++ .github/scripts/setup-python-runner.sh
@@ -19,7 +19,8 @@ while [[ $# -gt 0 ]]; do
 done
 
 echo "Cloning python-runner repository..."
-git clone --depth 1 https://${METABASE_AUTOMATION_USER_TOKEN}@github.com/metabase/python-runner-container.git python-runner
+git clone --depth 1 https://${METABASE_AUTOMATION_USER_TOKEN}@github.com/metabase/python-runner-container.git --branch seccomp python-runner
+
 
 if [ "$SKIP_START" = false ]; then
   echo "Starting python-runner..."
