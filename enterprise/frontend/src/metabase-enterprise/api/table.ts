349faabf7f15d15461085d9b06976e98eb5a28bb
Ngoc Khuat <qn.khuat@gmail.com>
2026-07-21 23:46:21 +0700

    Frontend seed API client

--- enterprise/frontend/src/metabase-enterprise/api/table.ts
+++ enterprise/frontend/src/metabase-enterprise/api/table.ts
@@ -1,7 +1,6 @@
 import type {
   BulkTableRequest,
   PublishTablesResponse,
-  Table,
 } from "metabase-types/api";
 
 import { EnterpriseApi } from "./api";
@@ -9,20 +8,6 @@ import { invalidateTags, tag } from "./tags";
 
 export const tableApi = EnterpriseApi.injectEndpoints({
   endpoints: (builder) => ({
-    createSeed: builder.mutation<Table, { name: string; file: File }>({
-      query: ({ name, file }) => {
-        const formData = new FormData();
-        formData.append("name", name);
-        formData.append("file", file);
-        return {
-          method: "POST",
-          url: "/api/ee/data-studio/table/seed",
-          body: formData,
-        };
-      },
-      invalidatesTags: (_, error) =>
-        invalidateTags(error, [tag("table"), tag("collection")]),
-    }),
     publishTables: builder.mutation<PublishTablesResponse, BulkTableRequest>({
       query: (body) => ({
         method: "POST",
@@ -44,8 +29,5 @@ export const tableApi = EnterpriseApi.injectEndpoints({
   }),
 });
 
-export const {
-  useCreateSeedMutation,
-  usePublishTablesMutation,
-  useUnpublishTablesMutation,
-} = tableApi;
+export const { usePublishTablesMutation, useUnpublishTablesMutation } =
+  tableApi;
