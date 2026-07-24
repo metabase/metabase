8724e1794cd62fce0ce4bfeec5cf0060bacea9fb
Chris Truter <chris@metabase.com>
2026-07-24 13:48:35 +0200

    OSI foundation: entity-retrieval spec layer + osi_ai_context generation metadata and CRUD

--- src/metabase/entity_retrieval/db.clj
+++ src/metabase/entity_retrieval/db.clj
@@ -5,13 +5,15 @@
    [toucan2.core :as t2]))
 
 (defn ai-contexts-for-entities
-  "The OsiAiContext rows for the given `[entity-type entity-local-id]` pairs."
+  "The human-approved OsiAiContext rows for the given `[entity-type entity-local-id]` pairs."
   [entity-type+ids]
   ;; row-value IN isn't portable across our app DBs, so match the wanted (type, id) pairs with OR-of-ANDs.
   (t2/select :model/OsiAiContext
-             {:where (into [:or]
-                           (map (fn [[entity-type entity-local-id]]
-                                  [:and
-                                   [:= :entity_type entity-type]
-                                   [:= :entity_local_id entity-local-id]]))
-                           entity-type+ids)}))
+             {:where [:and
+                      [:= :data_source "human"]
+                      (into [:or]
+                            (map (fn [[entity-type entity-local-id]]
+                                   [:and
+                                    [:= :entity_type entity-type]
+                                    [:= :entity_local_id entity-local-id]]))
+                            entity-type+ids)]}))
