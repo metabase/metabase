0374e1244c839757708fd0b7d9ba17bd801ab477
Egor Grushin <egor@metabase.com>
2026-07-28 21:24:19 +0100

    GDGT-2768 GDGT-2769 remove deprecated TableColumnFingerprintInfo and TableColumnInfo (#78519)

--- frontend/src/metabase/common/components/MetadataInfo/QueryColumnInfoIcon/QueryColumnInfoIcon.unit.spec.tsx
+++ frontend/src/metabase/common/components/MetadataInfo/QueryColumnInfoIcon/QueryColumnInfoIcon.unit.spec.tsx
@@ -6,22 +6,31 @@ import {
   columnFinder,
 } from "metabase-lib/test-helpers";
 
-import { QueryColumnInfoIcon } from "./ColumnInfoIcon";
+import { QueryColumnInfoIcon } from "./QueryColumnInfoIcon";
 
-function setup(table: string, column: string) {
-  const query = Lib.createTestQuery(SAMPLE_PROVIDER, DEFAULT_TEST_QUERY);
-  const columns = Lib.visibleColumns(query, 0);
-  const findColumn = columnFinder(query, columns);
-  const col = findColumn(table, column);
+const STAGE_INDEX = -1;
 
-  return render(
-    <QueryColumnInfoIcon query={query} stageIndex={-1} column={col} />,
-  );
+interface SetupOpts {
+  tableName: string;
+  columnName: string;
 }
+
+const setup = ({ tableName, columnName }: SetupOpts) => {
+  const query = Lib.createTestQuery(SAMPLE_PROVIDER, DEFAULT_TEST_QUERY);
+  const findColumn = columnFinder(query, Lib.visibleColumns(query, 0));
+
+  render(
+    <QueryColumnInfoIcon
+      query={query}
+      stageIndex={STAGE_INDEX}
+      column={findColumn(tableName, columnName)}
+    />,
+  );
+};
 
 describe("QueryColumnInfoIcon", () => {
   it("should show the hovercard only on hover", async () => {
-    setup("PRODUCTS", "CATEGORY");
+    setup({ tableName: "PRODUCTS", columnName: "CATEGORY" });
 
     const icon = screen.getByLabelText("More info");
 
