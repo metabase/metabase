245af1e945152490cea29af59f39642e0aaf0914
Kamil Mielnik <kamil@kamilmielnik.com>
2026-08-04 00:31:54 +0700

    Custom viz SDK 2.0 (#78354)

--- enterprise/frontend/src/metabase-enterprise/custom_viz/custom-viz-common.ts
+++ enterprise/frontend/src/metabase-enterprise/custom_viz/custom-viz-common.ts
@@ -1,13 +1,14 @@
 import type { CustomVisualization } from "custom-viz";
 import type { ComponentType } from "react";
 
+import { columnSettings } from "metabase/visualizations/lib/settings/column";
 import type {
   Visualization,
   VisualizationPassThroughProps,
   VisualizationProps,
 } from "metabase/visualizations/types/visualization";
 import type {
-  CustomVizPluginId,
+  CustomVizPluginRuntime,
   VisualizationDisplay,
 } from "metabase-types/api";
 
@@ -30,19 +31,18 @@ export function applyDefaultVisualizationProps(
   vizDef: CustomVisualization<Record<string, unknown>>,
   settings: {
     identifier: VisualizationDisplay;
-    pluginId: CustomVizPluginId;
+    plugin: CustomVizPluginRuntime;
     getUiName: () => string;
     iconUrl?: string | undefined;
     isDev?: boolean;
   },
 ): Visualization {
+  const { plugin, ...componentSettings } = settings;
   return Object.assign(Component, {
-    settings:
-      sanitizePluginSettings(
-        vizDef.settings,
-        vizDef.mount,
-        settings.pluginId,
-      ) ?? {},
+    settings: {
+      ...columnSettings({ getHidden: () => true }),
+      ...sanitizePluginSettings(vizDef.settings, vizDef.mount, plugin),
+    },
     checkRenderable: vizDef.checkRenderable,
     noHeader: vizDef.noHeader ?? false,
     canSavePng: vizDef.canSavePng ?? false,
@@ -50,6 +50,7 @@ export function applyDefaultVisualizationProps(
     minSize: vizDef.minSize,
     defaultSize: vizDef.defaultSize,
     isDev: settings.isDev,
-    ...settings,
+    pluginId: plugin.id,
+    ...componentSettings,
   });
 }
