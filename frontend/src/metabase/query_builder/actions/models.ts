e061057b0b9ac79f8185ee1d55d86974f02a7502
Romeo Van Snick <romeo@romeovansnick.be>
2026-08-03 18:43:51 -0700

    Move the query builder's action types off the actions barrel

--- frontend/src/metabase/query_builder/actions/models.ts
+++ frontend/src/metabase/query_builder/actions/models.ts
@@ -1,6 +1,7 @@
 import { createAction } from "redux-actions";
 import { t } from "ttag";
 
+import { SET_METADATA_DIFF } from "metabase/redux/query-builder";
 import type {
   DatasetEditorTab,
   Dispatch,
@@ -76,7 +77,6 @@ export const turnModelIntoQuestion =
     );
   };
 
-export const SET_METADATA_DIFF = "metabase/qb/SET_METADATA_DIFF";
 export const setMetadataDiff = createAction(SET_METADATA_DIFF);
 
 export const onModelPersistenceChange =
