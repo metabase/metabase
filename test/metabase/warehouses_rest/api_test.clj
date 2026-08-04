a6f29b02223df9f563125acc803df2f0b008e6e3
technomancy <technomancy@gmail.com>
2026-08-04 11:07:44 -0700

    Fix broken backend test assertions (#78902) (#79389)
    * Fix broken backend test assertions (#78902)
    
    * Adjust some error messages in tests.
    
    ---------
    
    Co-authored-by: Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>

--- test/metabase/warehouses_rest/api_test.clj
+++ test/metabase/warehouses_rest/api_test.clj
@@ -2551,10 +2551,10 @@
         (testing "does not includes undefined keys by default"
           (is (not (contains? (:settings (mt/user-http-request :crowberto :get 200 (str "database/" db-id)))
                               :undefined-setting))))
-        (is (= "Error checking the readability of :undefined-setting setting. The setting will be hidden in API response."
-               (-> (messages)
-                   first
-                   :message)))))))
+        (is (re-find #"Error checking the readability of :undefined-setting setting."
+                     (-> (messages)
+                         first
+                         :message)))))))
 
 (deftest autocomplete-suggestions-do-not-include-dashboard-cards
   (testing "GET /api/database/:id/card_autocomplete_suggestions"
