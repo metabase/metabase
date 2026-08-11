be3046cb85c382a2e5365c06b251a5648fffff99
Romeo Van Snick <romeo@romeovansnick.be>
2026-08-11 02:42:15 -0700

    Fetch the Data Studio Library metric pages when their route is matched

--- enterprise/frontend/src/metabase-enterprise/data-studio/library/metrics/routes.tsx
+++ enterprise/frontend/src/metabase-enterprise/data-studio/library/metrics/routes.tsx
@@ -1,37 +1,77 @@
 import { PLUGIN_DEPENDENCIES } from "metabase/plugins";
 import { Route } from "metabase/router";
 
-import { DataStudioMetricAboutPage } from "./pages/DataStudioMetricAboutPage";
-import { DataStudioMetricDependenciesPage } from "./pages/DataStudioMetricDependenciesPage";
-import { DataStudioMetricDimensionsPage } from "./pages/DataStudioMetricDimensionsPage";
-import { DataStudioMetricHistoryPage } from "./pages/DataStudioMetricHistoryPage";
-import { DataStudioMetricOverviewPage } from "./pages/DataStudioMetricOverviewPage";
-import { DataStudioMetricQueryPage } from "./pages/DataStudioMetricQueryPage";
-import { DataStudioNewMetricPage } from "./pages/NewMetricPage";
+/**
+ * The Data Studio Library metric pages, in one chunk.
+ *
+ * Each of these wraps the core page of the same name. While they were imported
+ * eagerly here, `metabase/metrics/pages/*` could not leave the initial bundle
+ * however lazy its own routes were, because this module reached them from the
+ * other side.
+ */
+const newMetricPage = () =>
+  import(
+    /* webpackChunkName: "data-studio-metrics" */ "./pages/NewMetricPage"
+  ).then(({ DataStudioNewMetricPage }) => ({
+    Component: DataStudioNewMetricPage,
+  }));
+
+const metricAboutPage = () =>
+  import(
+    /* webpackChunkName: "data-studio-metrics" */ "./pages/DataStudioMetricAboutPage"
+  ).then(({ DataStudioMetricAboutPage }) => ({
+    Component: DataStudioMetricAboutPage,
+  }));
+
+const metricOverviewPage = () =>
+  import(
+    /* webpackChunkName: "data-studio-metrics" */ "./pages/DataStudioMetricOverviewPage"
+  ).then(({ DataStudioMetricOverviewPage }) => ({
+    Component: DataStudioMetricOverviewPage,
+  }));
+
+const metricDimensionsPage = () =>
+  import(
+    /* webpackChunkName: "data-studio-metrics" */ "./pages/DataStudioMetricDimensionsPage"
+  ).then(({ DataStudioMetricDimensionsPage }) => ({
+    Component: DataStudioMetricDimensionsPage,
+  }));
+
+const metricQueryPage = () =>
+  import(
+    /* webpackChunkName: "data-studio-metrics" */ "./pages/DataStudioMetricQueryPage"
+  ).then(({ DataStudioMetricQueryPage }) => ({
+    Component: DataStudioMetricQueryPage,
+  }));
+
+const metricDependenciesPage = () =>
+  import(
+    /* webpackChunkName: "data-studio-metrics" */ "./pages/DataStudioMetricDependenciesPage"
+  ).then(({ DataStudioMetricDependenciesPage }) => ({
+    Component: DataStudioMetricDependenciesPage,
+  }));
+
+const metricHistoryPage = () =>
+  import(
+    /* webpackChunkName: "data-studio-metrics" */ "./pages/DataStudioMetricHistoryPage"
+  ).then(({ DataStudioMetricHistoryPage }) => ({
+    Component: DataStudioMetricHistoryPage,
+  }));
 
 export function getDataStudioMetricRoutes() {
   return (
     <Route path="metrics">
-      <Route path="new" element={<DataStudioNewMetricPage />} />
-      <Route path=":cardId" element={<DataStudioMetricAboutPage />} />
-      <Route
-        path=":cardId/overview"
-        element={<DataStudioMetricOverviewPage />}
-      />
-      <Route
-        path=":cardId/dimensions"
-        element={<DataStudioMetricDimensionsPage />}
-      />
-      <Route path=":cardId/query" element={<DataStudioMetricQueryPage />} />
+      <Route path="new" lazy={newMetricPage} />
+      <Route path=":cardId" lazy={metricAboutPage} />
+      <Route path=":cardId/overview" lazy={metricOverviewPage} />
+      <Route path=":cardId/dimensions" lazy={metricDimensionsPage} />
+      <Route path=":cardId/query" lazy={metricQueryPage} />
       {PLUGIN_DEPENDENCIES.isEnabled && (
-        <Route
-          path=":cardId/dependencies"
-          element={<DataStudioMetricDependenciesPage />}
-        >
+        <Route path=":cardId/dependencies" lazy={metricDependenciesPage}>
           <Route index element={<PLUGIN_DEPENDENCIES.DependencyGraphPage />} />
         </Route>
       )}
-      <Route path=":cardId/history" element={<DataStudioMetricHistoryPage />} />
+      <Route path=":cardId/history" lazy={metricHistoryPage} />
     </Route>
   );
 }
