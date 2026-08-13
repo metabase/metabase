5efdec98b5847c678e9512621eea2f7a26fe0b0f
Denis Berezin <denis.berezin@metabase.com>
2026-08-13 19:19:37 +0300

    UXW-4961 Convert data-grid to TS (#79788)
    * UXW-4961 Convert data-grid to TS
    
    * review fixes

--- frontend/src/metabase/visualizations/lib/data_grid.ts
+++ frontend/src/metabase/visualizations/lib/data_grid.ts
@@ -1,11 +1,29 @@
 import _ from "underscore";
 
 import * as Pivot from "cljs/metabase.pivot.js";
+import { checkNotNull } from "metabase/utils/types";
 import { formatValue } from "metabase/value-formatting";
 import { makeCellBackgroundGetter } from "metabase/visualizations/lib/table_format";
+import type {
+  ComputedVisualizationSettings,
+  PivotedDatasetColumn,
+  PivotedRowValues,
+} from "metabase/visualizations/types";
+import type {
+  BodyItem,
+  HeaderItem,
+} from "metabase/visualizations/visualizations/PivotTable/types";
 import { migratePivotColumnSplitSetting } from "metabase-lib/v1/queries/utils/pivot";
+import type {
+  DatasetColumn,
+  DatasetData,
+  RowValue,
+  RowValues,
+} from "metabase-types/api";
 
-export function isPivotGroupColumn(col) {
+export function isPivotGroupColumn(
+  col: Pick<DatasetColumn, "name"> | undefined,
+) {
   return col?.name === "pivot-grouping";
 }
 
@@ -17,16 +35,41 @@ export const COLUMN_SORT_ORDER = "pivot_table.column_sort_order";
 export const COLUMN_SORT_ORDER_ASC = "ascending";
 export const COLUMN_SORT_ORDER_DESC = "descending";
 
-export function multiLevelPivot(data, settings) {
-  if (!settings[COLUMN_SPLIT_SETTING]) {
+export type MultiLevelPivotData = {
+  leftHeaderItems: HeaderItem[];
+  topHeaderItems: HeaderItem[];
+  rowCount: number;
+  columnCount: number;
+  rowIndex: RowValue[][];
+  getRowSection: (columnIndex: number, rowIndex: number) => BodyItem[];
+  rowIndexes: number[];
+  columnIndexes: number[];
+  valueIndexes: number[];
+  columnsWithoutPivotGroup: DatasetColumn[];
+};
+
+type ProcessedPivotTable = Pick<
+  MultiLevelPivotData,
+  "leftHeaderItems" | "topHeaderItems" | "getRowSection"
+> & {
+  columnIndex: RowValue[][] | null;
+  rowIndex: RowValue[][] | null;
+};
+
+export function multiLevelPivot(
+  data: Pick<DatasetData, "rows" | "cols">,
+  settings: ComputedVisualizationSettings,
+): MultiLevelPivotData | null {
+  const columnSplitSetting = settings[COLUMN_SPLIT_SETTING];
+  if (!columnSplitSetting) {
     return null;
   }
   const columnSplit = migratePivotColumnSplitSetting(
-    settings[COLUMN_SPLIT_SETTING] ?? { rows: [], columns: [], values: [] },
+    columnSplitSetting,
     data.cols,
   );
 
-  const columns = Pivot.columns_without_pivot_group(data.cols);
+  const columns: DatasetColumn[] = Pivot.columns_without_pivot_group(data.cols);
 
   const {
     columns: columnIndexes,
@@ -38,7 +81,8 @@ export function multiLevelPivot(data, settings) {
       .filter((index) => index !== -1),
   );
 
-  const columnSettings = columns.map((column) => settings.column(column));
+  const getColumnSettings = checkNotNull(settings.column);
+  const columnSettings = columns.map((column) => getColumnSettings(column));
 
   // pivot tables have a lot of repeated values, so we use memoized formatters for each column
   const [valueFormatters, topIndexFormatters, leftIndexFormatters] = [
@@ -48,8 +92,8 @@ export function multiLevelPivot(data, settings) {
   ].map((indexes) =>
     indexes.map((index) =>
       _.memoize(
-        (value) => formatValue(value, columnSettings[index]),
-        (value) => JSON.stringify(value) + String(index),
+        (value: RowValue) => formatValue(value, columnSettings[index]),
+        (value: RowValue) => JSON.stringify(value) + String(index),
       ),
     ),
   );
@@ -58,7 +102,7 @@ export function multiLevelPivot(data, settings) {
   // computed in CLJS by metabase.pivot.core/get-rows-from-pivot-data, and we
   // want to avoid an extra round trip to CLJS (this can probably be improved,
   // maybe by computing background color right before rendering)
-  const makeColorGetter = (rows) => {
+  const makeColorGetter = (rows: RowValues[]) => {
     return makeCellBackgroundGetter(
       rows,
       columns,
@@ -74,7 +118,7 @@ export function multiLevelPivot(data, settings) {
       leftHeaderItems,
       topHeaderItems,
       getRowSection,
-    } = Pivot.process_pivot_table(
+    }: ProcessedPivotTable = Pivot.process_pivot_table(
       data,
       rowIndexes,
       columnIndexes,
@@ -119,8 +163,22 @@ export function multiLevelPivot(data, settings) {
   }
 }
 
+type PivotedTableData = {
+  cols: PivotedDatasetColumn[];
+  columns: RowValue[];
+  rows: PivotedRowValues[];
+  sourceRows: (number | null)[][];
+  rows_truncated: DatasetData["rows_truncated"];
+};
+
 // This is the pivot function used in the normal table visualization.
-export function pivot(data, normalCol, pivotCol, cellCol, settings) {
+export function pivot(
+  data: Pick<DatasetData, "rows" | "cols" | "rows_truncated">,
+  normalCol: number,
+  pivotCol: number,
+  cellCol: number,
+  settings?: ComputedVisualizationSettings,
+): PivotedTableData {
   const { pivotValues, normalValues } = distinctValuesSorted(
     data.rows,
     pivotCol,
@@ -131,18 +189,20 @@ export function pivot(data, normalCol, pivotCol, cellCol, settings) {
   pivotValues.unshift(data.cols[normalCol].display_name);
 
   // start with an empty grid that we'll fill with the appropriate values
-  const pivotedRows = normalValues.map((normalValues, index) => {
-    const row = pivotValues.map(() => null);
+  const pivotedRows = normalValues.map((normalValue) => {
+    const row: PivotedRowValues = pivotValues.map(() => null);
     // for onVisualizationClick:
     row._dimension = {
-      value: normalValues,
+      value: normalValue,
       column: data.cols[normalCol],
     };
     return row;
   });
 
   // keep a record of which row the data came from for onVisualizationClick
-  const sourceRows = normalValues.map(() => pivotValues.map(() => null));
+  const sourceRows: (number | null)[][] = normalValues.map(() =>
+    pivotValues.map(() => null),
+  );
 
   // fill it up with the data
   for (let j = 0; j < data.rows.length; j++) {
@@ -162,7 +222,7 @@ export function pivot(data, normalCol, pivotCol, cellCol, settings) {
   };
 
   // provide some column metadata to maintain consistency
-  const cols = pivotValues.map(function (value, idx) {
+  const cols = pivotValues.map<PivotedDatasetColumn>(function (value, idx) {
     if (idx === 0) {
       // first column is always the coldef of the normal column
       return data.cols[normalCol];
@@ -171,7 +231,7 @@ export function pivot(data, normalCol, pivotCol, cellCol, settings) {
         ...data.cols[cellCol],
         // `name` must be the same for conditional formatting, but put the
         // formatted pivotted value in the `display_name`
-        display_name: formatValue(value, pivotColumnSettings) || "",
+        display_name: String(formatValue(value, pivotColumnSettings) || ""),
         // for onVisualizationClick:
         _dimension: {
           value: value,
@@ -190,9 +250,13 @@ export function pivot(data, normalCol, pivotCol, cellCol, settings) {
   };
 }
 
-function distinctValuesSorted(rows, pivotColIdx, normalColIdx) {
-  const normalSet = new Set();
-  const pivotSet = new Set();
+function distinctValuesSorted(
+  rows: RowValues[],
+  pivotColIdx: number,
+  normalColIdx: number,
+) {
+  const normalSet = new Set<RowValue>();
+  const pivotSet = new Set<RowValue>();
 
   const normalSortState = new SortState();
   const pivotSortState = new SortState();
@@ -217,23 +281,31 @@ function distinctValuesSorted(rows, pivotColIdx, normalColIdx) {
   return { normalValues, pivotValues };
 }
 
-// This should work for both strings and numbers
-const DEFAULT_COMPARE = (a, b) => (a < b ? -1 : a > b ? 1 : 0);
+const DEFAULT_COMPARE = (a: RowValue, b: RowValue) => {
+  if (typeof a === "string" && typeof b === "string") {
+    return a < b ? -1 : a > b ? 1 : 0;
+  }
+  const left = Number(a);
+  const right = Number(b);
+  return left < right ? -1 : left > right ? 1 : 0;
+};
 
 class SortState {
+  compare: (a: RowValue, b: RowValue) => number;
+
+  asc = true;
+  desc = true;
+  lastValue: RowValue | undefined = undefined;
+
+  groupAsc = true;
+  groupDesc = true;
+  lastGroupKey: RowValue | undefined = undefined;
+  isGrouped = false;
+
   constructor(compare = DEFAULT_COMPARE) {
     this.compare = compare;
-
-    this.asc = true;
-    this.desc = true;
-    this.lastValue = undefined;
-
-    this.groupAsc = true;
-    this.groupDesc = true;
-    this.lastGroupKey = undefined;
-    this.isGrouped = false;
   }
-  update(value, groupKey) {
+  update(value: RowValue, groupKey: RowValue) {
     // skip the first value since there's nothing to compare it to
     if (this.lastValue !== undefined) {
       // compare the current value with the previous value
@@ -258,7 +330,7 @@ class SortState {
     this.lastValue = value;
     this.lastGroupKey = groupKey;
   }
-  sort(array) {
+  sort(array: RowValue[]) {
     if (this.isGrouped) {
       if (this.groupAsc && this.groupDesc) {
         console.warn("This shouldn't happen");
