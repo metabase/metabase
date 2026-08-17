41554600872df2b2ee709462dba45e6c2184fa1a
Romeo Van Snick <romeo@romeovansnick.be>
2026-08-16 20:45:28 -0700

    Delete the leaflet and pivot-table lazy boundaries the chart split made redundant

--- frontend/src/metabase/visualizations/visualizations/Map/Map.tsx
+++ frontend/src/metabase/visualizations/visualizations/Map/Map.tsx
@@ -1,33 +1,10 @@
-import { Suspense, lazy, memo } from "react";
+import { memo } from "react";
 
-import { Flex } from "metabase/ui";
-import MapSkeleton from "metabase/visualizations/components/skeletons/MapSkeleton/MapSkeleton";
 import { isSameSeries } from "metabase/visualizations/lib/utils";
 import type { VisualizationProps } from "metabase/visualizations/types";
 
+import { MapRenderer } from "./MapRenderer";
 import { MAP_VIZ_DEFINITION } from "./definition";
-
-// Leaflet (and the map renderer that uses it) is loaded lazily so it stays out
-// of the initial bundle for the majority of users who never open a map.
-const MapRenderer = lazy(() =>
-  import(/* webpackChunkName: "map-renderer" */ "./MapRenderer").then(
-    (module) => ({ default: module.MapRenderer }),
-  ),
-);
-
-function MapComponent(props: VisualizationProps) {
-  return (
-    <Suspense
-      fallback={
-        <Flex h="100%" w="100%" direction="column">
-          <MapSkeleton />
-        </Flex>
-      }
-    >
-      <MapRenderer {...props} />
-    </Suspense>
-  );
-}
 
 function arePropsEqual(prev: VisualizationProps, next: VisualizationProps) {
   const sameSize = prev.width === next.width && prev.height === next.height;
@@ -39,6 +16,6 @@ function arePropsEqual(prev: VisualizationProps, next: VisualizationProps) {
 }
 
 export const Map = Object.assign(
-  memo(MapComponent, arePropsEqual),
+  memo(MapRenderer, arePropsEqual),
   MAP_VIZ_DEFINITION,
 );
--- frontend/src/metabase/visualizations/visualizations/PivotTable/PivotTable.tsx
+++ frontend/src/metabase/visualizations/visualizations/PivotTable/PivotTable.tsx
@@ -1,37 +1,15 @@
-import { Suspense, lazy } from "react";
-
 import { connect } from "metabase/redux";
 import type { State } from "metabase/redux/store";
 import { getSetting } from "metabase/settings";
-import TableSkeleton from "metabase/visualizations/components/skeletons/TableSkeleton";
-import type { VisualizationProps } from "metabase/visualizations/types";
 
+import { PivotTableView } from "./PivotTableInner";
 import { PIVOT_TABLE_DEFINITION } from "./definition";
 
 const mapStateToProps = (state: State) => ({
   fontFamily: getSetting(state, "application-font"),
 });
 
-// react-virtualized (and the grid renderer that uses it) is loaded lazily so it
-// stays out of the initial bundle for the majority of users who never open a
-// pivot table.
-const PivotTableView = lazy(() =>
-  import(/* webpackChunkName: "pivot-table-view" */ "./PivotTableInner").then(
-    (module) => ({ default: module.PivotTableView }),
-  ),
-);
-
-function PivotTableComponent(
-  props: VisualizationProps & { className?: string },
-) {
-  return (
-    <Suspense fallback={<TableSkeleton />}>
-      <PivotTableView {...props} />
-    </Suspense>
-  );
-}
-
 export const PivotTable = Object.assign(
-  connect(mapStateToProps)(PivotTableComponent),
+  connect(mapStateToProps)(PivotTableView),
   PIVOT_TABLE_DEFINITION,
 );
