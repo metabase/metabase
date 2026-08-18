ed4792d172ae3dc96fdab51fc9cc18a69510edce
Uladzimir Havenchyk <uladzimir@metabase.com>
2026-08-18 23:34:39 +0300

    move the timeline panel and event modals to a shared module

--- frontend/src/metabase/query_builder/actions/timelines.ts
+++ frontend/src/metabase/query_builder/actions/timelines.ts
@@ -7,7 +7,8 @@ import {
   SHOW_TIMELINE_EVENTS,
 } from "metabase/redux/query-builder";
 import type { Dispatch, GetState } from "metabase/redux/store";
-import type { CollectionId, Timeline } from "metabase-types/api";
+import { getCollectionTimelines } from "metabase/visualizations/lib/timeline-events-visibility";
+import type { CollectionId } from "metabase-types/api";
 
 import { getFetchedTimelines } from "../selectors";
 
@@ -19,10 +20,9 @@ export const showTimelineEvents = createAction(SHOW_TIMELINE_EVENTS);
 export const showTimelinesForCollection =
   (collectionId?: CollectionId | null) =>
   (dispatch: Dispatch, getState: GetState) => {
-    const fetchedTimelines: Timeline[] = getFetchedTimelines(getState());
-    const collectionTimelines = collectionId
-      ? fetchedTimelines.filter((t) => t.collection_id === collectionId)
-      : fetchedTimelines.filter((t) => t.collection_id == null);
-
+    const collectionTimelines = getCollectionTimelines(
+      getFetchedTimelines(getState()),
+      collectionId,
+    );
     dispatch(showTimelineEvents(collectionTimelines.flatMap((t) => t.events)));
   };
