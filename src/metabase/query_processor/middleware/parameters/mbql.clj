537d013d7d33f75053bcc1c8f1e9e457c48ced47
Riley Thompson <rileythompson99@gmail.com>
2026-08-18 18:21:25 -0400

    remove test stuff

--- src/metabase/query_processor/middleware/parameters/mbql.clj
+++ src/metabase/query_processor/middleware/parameters/mbql.clj
@@ -183,7 +183,6 @@
 
         (= (:type param) :temporal-unit)
         (let [stage' (update-breakout-unit query stage (assoc param :value param-value))]
-          (tap> stage')
           (recur stage' more-params))
 
         :else
@@ -191,48 +190,3 @@
                                 (log/warnf "build-filter-clause did not return a valid clause for param %s" (pr-str (:id param))))
               stage'        (lib.filter/add-filter-to-stage stage filter-clause)]
           (recur stage' more-params))))))
-
-(comment
-
-  {:aggregation [[:count #:lib{:uuid "6d3c5948-9bc8-4d36-bdac-6d250bafba31"}]],
-   :lib/type :mbql.stage/mbql,
-   :source-table 2510,
-   :joins
-   [{:lib/type :mbql/join,
-     :alias "Products",
-     :strategy :left-join,
-     :conditions
-     [[:=
-       #:lib{:uuid "c480e372-0d04-4153-92ab-534fd6307562"}
-       [:field
-        {:base-type :type/DateTime,
-         :temporal-unit :week,
-         :lib/uuid "a9891bfc-9b5f-497a-84e5-63f83d931fee",
-         :effective-type :type/DateTime,
-         :lib/original-effective-type :type/DateTime}
-        18330]
-       [:field
-        {:base-type :type/DateTime,
-         :temporal-unit :day,
-         :join-alias "Products",
-         :lib/uuid "21435582-80a7-4b60-8c65-4dce24084764",
-         :effective-type :type/DateTime}
-        18352]]],
-     :stages [{:lib/type :mbql.stage/mbql, :source-table 2512}],
-     :lib/options {"lib/uuid" "621650ff-b4e2-488b-9b1a-87ecbcb0c607"}}],
-   :breakout
-   [[:field
-     {:base-type :type/DateTime,
-      :temporal-unit :week,
-      :lib/uuid "917557d9-a2e9-4063-8d73-ae0ed705b29e",
-      :effective-type :type/DateTime,
-      :lib/original-effective-type :type/DateTime}
-     18330]],
-   :parameters
-   [{:type :temporal-unit,
-     :value "week",
-     :id "80098aaa",
-     :target
-     [:dimension
-      [:field 18330 {:base-type :type/DateTime, :temporal-unit :day}]
-      {:stage-number 0}]}]})
