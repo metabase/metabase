b6da18e922884444fe4f094efdf39ec8b85cae23
Romeo Van Snick <romeo@romeovansnick.be>
2026-08-18 23:02:25 -0700

    Serve database engines from an endpoint instead of a public setting

--- test/metabase/warehouses_rest/api_test.clj
+++ test/metabase/warehouses_rest/api_test.clj
@@ -12,6 +12,7 @@
    [metabase.audit-app.core :as audit]
    [metabase.config.core :as config]
    [metabase.driver :as driver]
+   [metabase.driver.h2 :as h2]
    [metabase.driver.settings :as driver.settings]
    [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
    [metabase.driver.util :as driver.u]
@@ -54,6 +55,8 @@
    (java.sql Connection)
    (java.util.concurrent CountDownLatch Executors)
    (org.quartz JobDetail TriggerKey)))
+
+(comment h2/keep-me)
 
 (set! *warn-on-reflection* true)
 
@@ -156,6 +159,14 @@
          :result_metadata [{:name         "num_toucans"
                             :display_name "Num Toucans"
                             :base_type    :type/Integer}]))
+
+(deftest ^:parallel engines-test
+  (testing "GET /api/database/engines"
+    (let [engines (mt/user-http-request :rasta :get 200 "database/engines")]
+      (testing "returns connection properties per engine"
+        (is (=? {:driver-name "H2"} (:h2 engines))))
+      (testing "H2 is superseded so it stays out of the UI DB edit forms"
+        (is (= "deprecated" (get-in engines [:h2 :superseded-by])))))))
 
 (deftest ^:parallel get-database-test
   (testing "GET /api/database/:id"
