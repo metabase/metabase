237c590129a1865a87e0b68a149df37786c3287e
Egor Grushin <egor@metabase.com>
2026-08-20 22:05:32 +0100

    Redesign Radio

--- frontend/src/metabase/ui/components/inputs/Radio/index.tsx
+++ frontend/src/metabase/ui/components/inputs/Radio/index.tsx
@@ -1,3 +1,8 @@
-export { Radio } from "@mantine/core";
+import { Radio as MantineRadio } from "@mantine/core";
+
+import { RadioCard } from "./RadioCard";
+
+export const Radio = Object.assign(MantineRadio, { Card: RadioCard });
 export { radioOverrides } from "./Radio.config";
 export type { RadioProps, RadioGroupProps } from "@mantine/core";
+export type { RadioCardProps } from "./RadioCard";
