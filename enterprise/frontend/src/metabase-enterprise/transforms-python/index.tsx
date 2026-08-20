9d6a2e75020177372456a0a64521fbffca59f8d6
Romeo Van Snick <romeo@romeovansnick.be>
2026-08-19 21:56:12 -0700

    Load the enterprise admin pages through plugin route slots

--- enterprise/frontend/src/metabase-enterprise/transforms-python/index.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms-python/index.tsx
@@ -3,9 +3,13 @@ import { hasPremiumFeature } from "metabase-enterprise/settings";
 
 import { PythonTransformEditor } from "./components/PythonTransformEditor/lazy";
 import { SHARED_LIB_IMPORT_PATH } from "./constants";
-import { PythonRunnerSettingsPage } from "./pages/PythonRunnerSettingsPage";
 import { getPythonTransformsRoutes, getPythonUpsellRoutes } from "./routes";
 import { getPythonSourceValidationResult } from "./utils";
+
+const pythonRunnerSettingsPage = () =>
+  import("./pages/PythonRunnerSettingsPage").then(
+    ({ PythonRunnerSettingsPage }) => ({ Component: PythonRunnerSettingsPage }),
+  );
 
 /**
  * Initialize transforms-python plugin features that depend on hasPremiumFeature.
@@ -18,8 +22,8 @@ export function initializePlugin() {
     PLUGIN_TRANSFORMS_PYTHON.getPythonSourceValidationResult =
       getPythonSourceValidationResult;
     PLUGIN_TRANSFORMS_PYTHON.TransformEditor = PythonTransformEditor;
-    PLUGIN_TRANSFORMS_PYTHON.PythonRunnerSettingsPage =
-      PythonRunnerSettingsPage;
+    PLUGIN_TRANSFORMS_PYTHON.pythonRunnerSettingsPage =
+      pythonRunnerSettingsPage;
   } else {
     PLUGIN_TRANSFORMS_PYTHON.getPythonTransformsRoutes = getPythonUpsellRoutes;
   }
