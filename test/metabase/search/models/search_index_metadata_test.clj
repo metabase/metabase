de73dc1c860f26428ecf12a133522f9963de784d
Chris Truter <chris@metabase.com>
2026-08-20 17:38:50 +0200

    Clarify pending index deletion contract

--- test/metabase/search/models/search_index_metadata_test.clj
+++ test/metabase/search/models/search_index_metadata_test.clj
@@ -27,14 +27,14 @@
         (is (= index-1 (search-index-metadata/active-pending! engine version)))
         (is (= {:active index-1} (indexes)))
         (testing "Deleting an active index is a no-op"
-          (is (zero? (search-index-metadata/delete-index! engine version index-1)))
+          (is (zero? (search-index-metadata/delete-pending-index! engine version index-1)))
           (is (= {:active index-1} (indexes)))))
       (testing "If there is no pending index, it will return the current index"
         (is (= index-1 (search-index-metadata/active-pending! engine version))))
       (testing "You can retire an index"
         (is (search-index-metadata/create-pending! engine version index-2))
         (testing "Deleting a pending index removes it"
-          (is (= 1 (search-index-metadata/delete-index! engine version index-2)))
+          (is (= 1 (search-index-metadata/delete-pending-index! engine version index-2)))
           (is (= {:active index-1} (indexes)))
           (is (search-index-metadata/create-pending! engine version index-2)))
         (is (= {:active index-1 :pending index-2} (indexes)))
