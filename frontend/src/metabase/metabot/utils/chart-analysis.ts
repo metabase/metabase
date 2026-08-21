92ab58c480359c48d92c74654f2a7596b4a590f5
Sloan Sparger <sloansparger@users.noreply.github.com>
2026-08-21 17:03:48 -0500

    [Metabot] Remove explain chart button (#80355)
    * remove explain chart
    
    * update docs
    
    * doc change

--- frontend/src/metabase/metabot/utils/chart-analysis.ts
+++ frontend/src/metabase/metabot/utils/chart-analysis.ts
@@ -1,49 +1,4 @@
-import type {
-  CustomVizDisplayType,
-  VisualizationDisplay,
-} from "metabase-types/api";
-import { isCustomVizDisplay } from "metabase-types/guards";
-
-const CHART_ANALYSIS_ENABLED = {
-  // enabled
-  area: true,
-  bar: true,
-  boxplot: true,
-  combo: true,
-  funnel: true,
-  gauge: true,
-  line: true,
-  map: true,
-  pie: true,
-  progress: true,
-  row: true,
-  sankey: true,
-  scatter: true,
-  treemap: true,
-  waterfall: true,
-  // disabled
-  action: false,
-  "audit-table": false,
-  heading: false,
-  iframe: false,
-  link: false,
-  object: false,
-  pivot: false,
-  placeholder: false,
-  scalar: false,
-  smartscalar: false,
-  table: false,
-  text: false,
-  list: false,
-} as const satisfies {
-  [display in Exclude<VisualizationDisplay, CustomVizDisplayType>]: boolean;
-};
-
-type EnabledChartTypes = {
-  [K in keyof typeof CHART_ANALYSIS_ENABLED]: (typeof CHART_ANALYSIS_ENABLED)[K] extends true
-    ? K
-    : never;
-}[keyof typeof CHART_ANALYSIS_ENABLED];
+import type { VisualizationDisplay } from "metabase-types/api";
 
 // SVG renderer is faster + preferred, since it clones the existing SVG DOM node. However, in the
 // following cases we need to fallback to the slower PNG approach:
@@ -66,10 +21,6 @@ export const CHART_ANALYSIS_RENDER_FORMATS = {
   scatter: "svg",
   treemap: "svg",
   waterfall: "svg",
-} as const satisfies { [display in EnabledChartTypes]: "png" | "svg" | "none" };
-
-export const canAnalyzeQuestion = (display: VisualizationDisplay) => {
-  return isCustomVizDisplay(display)
-    ? false
-    : (CHART_ANALYSIS_ENABLED[display] ?? false);
-};
+} as const satisfies Partial<
+  Record<VisualizationDisplay, "png" | "svg" | "none">
+>;
