4d3f45941131db76bd24d2599725ec4d851f37f9
Phoomparin Mano <poom@metabase.com>
2026-08-25 00:39:25 +0700

    Check embedding saml popup source (#80494)
    * check saml popup origins
    
    * throw when site-url does not match instance-url
    
    * Fix SAML embedding popup validation

--- e2e/support/helpers/embedding-sdk-testing/embedding-sdk-helpers.ts
+++ e2e/support/helpers/embedding-sdk-testing/embedding-sdk-helpers.ts
@@ -129,12 +129,8 @@ export function stubWindowOpenForSamlPopup({
   isUserValid?: boolean;
 } = {}) {
   cy.window().then((win) => {
-    const popup = {
-      closed: false,
-      close: () => {
-        popup.closed = true;
-      },
-    };
+    const popup = win;
+    cy.stub(popup, "close");
 
     // stub `window.open(IDP_URI, ...)` for the SAML popup
     cy.stub(win, "open")
@@ -156,10 +152,10 @@ export function stubWindowOpenForSamlPopup({
                   exp: Math.floor(Date.now() / 1000) + 600, // 10 minutes
                 },
               },
-              origin: "*",
+              origin: win.location.origin,
+              source: popup,
             }),
           );
-          popup.close();
         }, 100); // simulate async delay
 
         return popup;
