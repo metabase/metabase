62885b74475104cefbef794c7fcd494a3a1b7c79
Egor Grushin <egor@metabase.com>
2026-08-25 14:40:57 +0100

    forwardRef radio card

--- frontend/src/metabase/ui/components/inputs/Radio/RadioCard.tsx
+++ frontend/src/metabase/ui/components/inputs/Radio/RadioCard.tsx
@@ -6,7 +6,7 @@ import {
   Text,
   rem,
 } from "@mantine/core";
-import type { ReactNode } from "react";
+import { type ReactNode, forwardRef } from "react";
 
 export type RadioCardProps = Omit<MantineRadioCardProps, "children"> & {
   label?: ReactNode;
@@ -16,37 +16,44 @@ export type RadioCardProps = Omit<MantineRadioCardProps, "children"> & {
   withIndicator?: boolean;
 };
 
-export const RadioCard = ({
-  label,
-  description,
-  leftSection,
-  disabled,
-  withIndicator = true,
-  ...props
-}: RadioCardProps) => (
-  <MantineRadioCard disabled={disabled} {...props}>
-    {withIndicator && <MantineRadioIndicator disabled={disabled} />}
-    {leftSection}
-    <Stack component="span" gap={rem(4)} miw={0}>
-      {label && (
-        <Text
-          component="span"
-          c={disabled ? "text-disabled" : "text-primary"}
-          fw={700}
-          lh={rem(18)}
-        >
-          {label}
-        </Text>
-      )}
-      {description && (
-        <Text
-          component="span"
-          size="sm"
-          c={disabled ? "text-disabled" : "text-secondary"}
-        >
-          {description}
-        </Text>
-      )}
-    </Stack>
-  </MantineRadioCard>
+export const RadioCard = forwardRef<HTMLButtonElement, RadioCardProps>(
+  function RadioCard(
+    {
+      label,
+      description,
+      leftSection,
+      disabled,
+      withIndicator = true,
+      ...props
+    },
+    ref,
+  ) {
+    return (
+      <MantineRadioCard disabled={disabled} {...props} ref={ref}>
+        {withIndicator && <MantineRadioIndicator disabled={disabled} />}
+        {leftSection}
+        <Stack component="span" gap={rem(4)} miw={0}>
+          {label && (
+            <Text
+              component="span"
+              c={disabled ? "text-disabled" : "text-primary"}
+              fw={700}
+              lh={rem(18)}
+            >
+              {label}
+            </Text>
+          )}
+          {description && (
+            <Text
+              component="span"
+              size="sm"
+              c={disabled ? "text-disabled" : "text-secondary"}
+            >
+              {description}
+            </Text>
+          )}
+        </Stack>
+      </MantineRadioCard>
+    );
+  },
 );
