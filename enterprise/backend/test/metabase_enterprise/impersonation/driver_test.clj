3f87c426e181cc999af834e580abfcbbecf17d18
Nicola Mometto <nicola.mometto@metabase.com>
2026-08-25 09:49:28 +0100

    Add missing metabase.driver.sql require to impersonation driver-test

--- enterprise/backend/test/metabase_enterprise/impersonation/driver_test.clj
+++ enterprise/backend/test/metabase_enterprise/impersonation/driver_test.clj
@@ -12,6 +12,7 @@
    [metabase.driver :as driver]
    [metabase.driver.connection :as driver.conn]
    [metabase.driver.settings :as driver.settings]
+   [metabase.driver.sql :as driver.sql]
    [metabase.driver.sql-jdbc.connection :as sql-jdbc.conn]
    [metabase.driver.sql-jdbc.execute :as sql-jdbc.execute]
    [metabase.lib.core :as lib]
