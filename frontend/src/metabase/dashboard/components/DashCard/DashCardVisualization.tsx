359d028136b49baba085cac660e1b7e2e6ecb041
Uladzimir Havenchyk <uladzimir@metabase.com>
2026-08-27 00:44:32 +0300

    address review

--- frontend/src/metabase/dashboard/components/DashCard/DashCardVisualization.tsx
+++ frontend/src/metabase/dashboard/components/DashCard/DashCardVisualization.tsx
@@ -198,8 +198,14 @@ export function DashCardVisualization({
 
   const datasets = useSelector((state) => getDashcardData(state, dashcard.id));
 
-  const { isEnabled: isTimelineEventsEnabled, props: timelineEventsProps } =
-    useDashCardTimelineEvents(dashcard);
+  const {
+    isEnabled: isTimelineEventsEnabled,
+    timelineEvents,
+    selectedTimelineEventIds,
+    onOpenTimelines,
+    onSelectTimelineEvents,
+    onDeselectTimelineEvents,
+  } = useDashCardTimelineEvents(dashcard);
 
   const inlineParameters = useSelector((state) =>
     getDashCardInlineValuePopulatedParameters(state, dashcard.id),
@@ -606,7 +612,11 @@ export function DashCardVisualization({
           renderLoadingView={renderLoadingView}
           titleMenuItems={titleMenuItems}
           errorMessageOverride={visualizerErrMsg}
-          {...timelineEventsProps}
+          timelineEvents={timelineEvents}
+          selectedTimelineEventIds={selectedTimelineEventIds}
+          onOpenTimelines={onOpenTimelines}
+          onSelectTimelineEvents={onSelectTimelineEvents}
+          onDeselectTimelineEvents={onDeselectTimelineEvents}
           enableEntityNavigation={enableEntityNavigation}
           onSameOriginNavigation={onSameOriginNavigation}
           autoAdjustSettings
