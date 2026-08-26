b5300454d75380e437975ca95bc3a7c710396ce8
Nicola Mometto <nicola.mometto@metabase.com>
2026-08-26 10:30:59 +0100

    Add perf edge-case tests for largest-common-subseq-length

--- test/metabase/search/in_place/util_test.clj
+++ test/metabase/search/in_place/util_test.clj
@@ -47,6 +47,27 @@
                                                          ["foo" "garden" "path"])))))))
 
 (deftest ^:parallel largest-common-subseq-length-large-grid-test
-  (testing "a 30x30 fully-matching grid computes promptly with the correct length"
-    (is (= 30
-           (search.util/largest-common-subseq-length = (repeat 30 :x) (repeat 30 :x))))))
+  (let [subseq-length (partial search.util/largest-common-subseq-length =)]
+    (testing "a 30x30 fully-matching grid computes promptly with the correct length"
+      (is (= 30 (subseq-length (repeat 30 :x) (repeat 30 :x)))))
+    (testing "no matches at all (every cell is a dead end)"
+      (is (= 0 (subseq-length (range 200) (range 200 400)))))
+    (testing "alternating tokens: many short runs that never chain"
+      (is (= 1 (subseq-length (take 200 (cycle [:a :b])) (repeat 200 :a)))))
+    (testing "a single mismatch in the middle halves the run"
+      (is (= 100 (subseq-length (repeat 201 :x) (concat (repeat 100 :x) [:y] (repeat 100 :x))))))
+    (testing "asymmetric inputs"
+      (is (= 1 (subseq-length [:x] (repeat 5000 :x))))
+      (is (= 1 (subseq-length (repeat 5000 :x) [:x]))))
+    (testing "well above the 30-token cap the scorer applies"
+      (is (= 500 (subseq-length (range 500) (range 500)))))))
+
+(deftest ^:parallel largest-common-subseq-length-eq-call-count-test
+  (testing "eq is invoked exactly once per cell of the n*m grid, regardless of match density"
+    (doseq [[xs ys] [[(range 40) (range 25)]
+                     [(repeat 40 :x) (repeat 25 :x)]
+                     [(range 40) (range 100 125)]]]
+      (let [calls (atom 0)
+            eq    (fn [a b] (swap! calls inc) (= a b))]
+        (search.util/largest-common-subseq-length eq xs ys)
+        (is (= (* 40 25) @calls))))))
