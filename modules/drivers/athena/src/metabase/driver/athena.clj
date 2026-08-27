2b7b42513e96761fdf2fe3f5c031dc062b9c36c7
Ben Grabow <ben.grabow@gmail.com>
2026-08-27 13:23:44 -0500

    Fix some linter errors. Remove their linter ignores

--- modules/drivers/athena/src/metabase/driver/athena.clj
+++ modules/drivers/athena/src/metabase/driver/athena.clj
@@ -519,11 +519,14 @@
                                                  "VIEW"]))]
     (vec (jdbc/metadata-result rs))))
 
-;; one-off REPL script kept for reference; leans on test namespaces drivers can't require
-#_{:clj-kondo/ignore [:aliased-namespace-symbol :discouraged-var :unresolved-namespace]}
+;; one-off REPL script kept for reference
+#_{:clj-kondo/ignore [:discouraged-var]}
 (comment
+  (require
+   '[metabase.test.data.dataset-definitions]
+   '[metabase.test.data.interface])
   ;; Script on following lines was used to get available table types, used in the `get-tables` implementation.
-  (with-open [conn (clojure.java.jdbc/get-connection
+  (with-open [conn (jdbc/get-connection
                     (sql-jdbc.conn/connection-details->spec
                      :athena
                      (metabase.test.data.interface/dbdef->connection-details
