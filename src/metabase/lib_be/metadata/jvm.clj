b538bf886a7879b3e16105662c395ffc3d79df6d
Cam Saul <github@camsaul.com>
2026-08-27 17:41:30 -0700

    Metadata for plain-`:field` expressions should propagate Field ID and Table ID

--- src/metabase/lib_be/metadata/jvm.clj
+++ src/metabase/lib_be/metadata/jvm.clj
@@ -49,6 +49,12 @@
   {:metadata/card   ::lib.schema.metadata/card
    :metadata/column ::lib.schema.metadata/column})
 
+;; TODO (Cam 2026-08-27) Consider whether we should just have this be the normal behavior for normalizing
+;; application-database-style metadata to Lib-style metadata, e.g. why can't we just use
+;;
+;;    (metabase.lib.core/normalize :metabase.lib.schema.metadata/table table-metadata)
+;;
+;; to do this? Seems like these rules can be rolled into the schemas themselves
 (mu/defn instance->metadata
   "Convert a (presumably) Toucan 2 instance of an application database model with `snake_case` keys to a Lib style
   metadata instance with `:lib/type` and `kebab-case` keys."
