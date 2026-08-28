78cd801bbd8d53abd0c44d71153a21757c8e76cc
Romeo Van Snick <romeo@romeovansnick.be>
2026-08-27 20:29:03 -0700

    Split the SDK environment spec per artifact

--- enterprise/frontend/src/embedding-sdk-package/test/environment.unit.spec.ts
+++ enterprise/frontend/src/embedding-sdk-package/test/environment.unit.spec.ts
@@ -5,12 +5,12 @@ describe("SDK environment config", () => {
     EMBEDDING_SDK_CONFIG.isEmbeddingSdk = false;
   });
 
-  it("sets isEmbeddingSdk() to true when SDK bundle is imported", async () => {
+  it("sets isEmbeddingSdk() to true when SDK is imported", async () => {
     expect(
       (await import("metabase/embedding-sdk/config")).isEmbeddingSdk(),
     ).toBe(false);
 
-    await import("embedding-sdk-bundle");
+    await import("embedding-sdk-package");
 
     expect(
       (await import("metabase/embedding-sdk/config")).isEmbeddingSdk(),
