bab47b6a10e5477b002e87e53892a5ef6bb0af0e
Nick Fitzpatrick <nickfitz.582@gmail.com>
2026-08-31 16:57:44 -0300

    useRenderedDataVisibility

--- frontend/src/metabase/visualizations/visualizations/CartesianChart/CartesianChart.tsx
+++ frontend/src/metabase/visualizations/visualizations/CartesianChart/CartesianChart.tsx
@@ -268,8 +268,7 @@ function CartesianChartInner(props: VisualizationProps) {
           <DataPointsVisiblePopover
             isDashboard={isDashboard}
             isVisualizer={isVisualizer}
-            chartModel={chartModel}
-            settings={settings}
+            chartInstance={chartInstance}
           />
           <TimelineEventsBand
             chartInstance={chartInstance}
