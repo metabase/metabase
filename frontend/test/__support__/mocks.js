61c11afa51ef888f5bc177485224d50912e7ba6b
Egor Grushin <egor@metabase.com>
2026-09-01 17:29:26 +0100

    codemirror mock: sync value prop in render, not effect (fixes LogLevelsModal lag)

--- frontend/test/__support__/mocks.js
+++ frontend/test/__support__/mocks.js
@@ -29,14 +29,16 @@ global.window.ResizeObserver = class ResizeObserver {
 jest.mock("metabase/analytics");
 
 jest.mock("@uiw/react-codemirror", () => {
-  const { forwardRef, useEffect, useState } = jest.requireActual("react");
+  const { forwardRef, useState } = jest.requireActual("react");
 
   const MockEditor = forwardRef((props, ref) => {
     const { indentWithTab, extensions, basicSetup, editable, ...rest } = props;
     const [value, setValue] = useState(props.value ?? "");
-    useEffect(() => {
+    const [syncedValue, setSyncedValue] = useState(props.value);
+    if (props.value !== syncedValue) {
+      setSyncedValue(props.value);
       setValue(props.value ?? "");
-    }, [props.value]);
+    }
     return (
       // @ts-expect-error: some props types are different on CodeMirror
       <textarea
