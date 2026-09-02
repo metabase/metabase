d67f990f88b9b61838d4d177dc3b8c9cff4f3c55
Timofei <timofei@metabase.com>
2026-09-02 17:25:47 +0400

    Fix tests #2

--- e2e/test/scenarios/admin-2/people.cy.spec.js
+++ e2e/test/scenarios/admin-2/people.cy.spec.js
@@ -30,7 +30,7 @@ const totalUsers = Object.keys(USERS).length;
 
 describe("scenarios > admin > people", () => {
   beforeEach(() => {
-    cy.intercept("GET", "/api/permissions/group").as("getGroups");
+    cy.intercept("GET", "/api/permissions/group*").as("getGroups");
     cy.intercept("GET", "/api/api-key").as("listApiKeys");
     H.restore();
     cy.signInAsAdmin();
