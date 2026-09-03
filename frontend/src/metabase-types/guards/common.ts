8b75137abb6fd6ee320f1801e0d8fcc214f3d346
Kamil Mielnik <kamil@kamilmielnik.com>
2026-09-03 17:59:21 +0700

    Cleanup

--- frontend/src/metabase-types/guards/common.ts
+++ frontend/src/metabase-types/guards/common.ts
@@ -10,15 +10,14 @@ export const isFunction = (
   return typeof value === "function";
 };
 
-// Tag checks see through proxies and across realms, instanceof doesn't.
-const getTag = (value: unknown): string =>
-  Object.prototype.toString.call(value);
+export const isMap = (value: unknown): value is Map<unknown, unknown> => {
+  return Object.prototype.toString.call(value) === "[object Map]";
+};
 
-export const isMap = (value: unknown): value is Map<unknown, unknown> =>
-  getTag(value) === "[object Map]";
+export const isSet = (value: unknown): value is Set<unknown> => {
+  return Object.prototype.toString.call(value) === "[object Set]";
+};
 
-export const isSet = (value: unknown): value is Set<unknown> =>
-  getTag(value) === "[object Set]";
-
-export const isDate = (value: unknown): value is Date =>
-  getTag(value) === "[object Date]";
+export const isDate = (value: unknown): value is Date => {
+  return Object.prototype.toString.call(value) === "[object Date]";
+};
