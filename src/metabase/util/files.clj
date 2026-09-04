e43de7c7e0500f282e639115f043acb2ab151de7
Tyler Plude <tsplude@gmail.com>
2026-09-04 09:44:49 -0700

    extract code-location->path and add jimfs-backed regression test

--- src/metabase/util/files.clj
+++ src/metabase/util/files.clj
@@ -14,7 +14,7 @@
    [metabase.util.log :as log])
   (:import
    (java.io FileNotFoundException)
-   (java.net URL)
+   (java.net URI URL)
    (java.nio.file CopyOption Files FileSystem FileSystemAlreadyExistsException
                   FileSystems LinkOption OpenOption Path Paths StandardCopyOption)
    (java.nio.file.attribute FileAttribute)
@@ -208,6 +208,13 @@
                (getResource ".keep-me")
                getProtocol)))
 
+(defn code-location->path
+  "Convert a code-source location URI into a native filesystem path string."
+  ^String [^URI uri]
+  ;; Paths/get decodes %-escapes AND yields the platform-native form. URI.getPath only decodes: on Windows it returns
+  ;; `/C:/...`, which java.nio.file.Path rejects (#81733).
+  (str (Paths/get uri)))
+
 (defn get-jar-path
   "Returns the path to the currently running jar file.
 
@@ -219,8 +226,7 @@
       (.getCodeSource)
       (.getLocation)
       (.toURI)
-      (Paths/get)
-      (str)))
+      (code-location->path)))
 
 (defn find-in-current-jar
   "Find matching files in the current jar. See
