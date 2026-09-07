7bf7a0915fac46d0208f07844cbc8570da704299
Uladzimir Havenchyk <125459446+uladzimirdev@users.noreply.github.com>
2026-09-07 11:12:09 +0300

    add tracking events (#81775)
    * add tracking events
    
    * Update frontend/src/metabase/query_builder/containers/QueryBuilder.timeline-events.unit.spec.tsx
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>
    
    * Update frontend/src/metabase/query_builder/containers/QueryBuilder.timeline-events.unit.spec.tsx
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>
    
    * fixup
    
    * fix naming
    
    * do not show timeline events on public dashboards and embedding (#81818)
    
    * handle public dashboard and embedding cases
    
    * fixup
    
    ---------
    
    Co-authored-by: Kamil Mielnik <kamil@kamilmielnik.com>

--- frontend/src/metabase/query_builder/components/view/QuestionTimelineWidget/analytics.ts
+++
@@ -1,7 +0,0 @@
-import { trackSimpleEvent } from "metabase/analytics";
-
-export const trackEventsClicked = () =>
-  trackSimpleEvent({
-    event: "events_clicked",
-    triggered_from: "chart",
-  });
