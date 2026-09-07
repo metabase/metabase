4ef8f257ed544474dea082afba8ae1ef68f2e8bf
Egor Grushin <egor@metabase.com>
2026-09-07 10:43:17 +0100

    Button: banner label inherits on-dark color (GDGT-2453)

--- frontend/src/metabase/archive/components/ArchivedEntityBanner/BannerButton.tsx
+++ frontend/src/metabase/archive/components/ArchivedEntityBanner/BannerButton.tsx
@@ -15,10 +15,7 @@ export const BannerButton = ({
   <Button size="md" variant="on-dark-secondary" onClick={onClick}>
     <Flex align="center" gap="sm">
       <FixedSizeIcon size={12} name={iconName} />
-      <Text
-        color="text-primary-inverse"
-        display={{ base: "none", sm: "inline" }}
-      >
+      <Text c="inherit" display={{ base: "none", sm: "inline" }}>
         {children}
       </Text>
     </Flex>
