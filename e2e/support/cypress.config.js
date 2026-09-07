23a440f73d1f19fabb4ae3b2e3c982f77b6f579d
Fraser <frasermcintosh1@gmail.com>
2026-09-08 11:37:40 +1200

    Use affected-test plans to narrow E2E tests

--- e2e/support/cypress.config.js
+++ e2e/support/cypress.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require("cypress");
 
+const { readSpecPlan } = require("../runner/read-spec-plan");
+
 const { mainConfig } = require("./config");
 
-module.exports = defineConfig({ e2e: mainConfig });
+const specFiles = readSpecPlan(process.env.E2E_SPEC_PATHS_FILE);
+
+module.exports = defineConfig({
+  e2e: {
+    ...mainConfig,
+    ...(specFiles !== null && { specPattern: specFiles }),
+  },
+});
