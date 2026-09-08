aaf0da35d3b3bc5895ff302ad20bf6ea8d8b21b3
Kamil Mielnik <kamil@kamilmielnik.com>
2026-09-08 01:54:16 -0700

    Read the gauge segments widget props from the raw series like the goal value widget

--- frontend/src/metabase/visualizations/visualizations/Gauge/definition.ts
+++ frontend/src/metabase/visualizations/visualizations/Gauge/definition.ts
@@ -80,6 +80,8 @@ export const GAUGE_CHART_DEFINITION: VisualizationDefinition = {
       },
       widget: "segmentsEditor",
       persistDefault: true,
+      // the transformed series drop `data.referenced_entities`
+      useRawSeries: true,
       getProps: ([{ card, data }]) => ({
         data,
         datasetQuery: card.dataset_query,
