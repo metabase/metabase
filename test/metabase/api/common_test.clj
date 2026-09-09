8b58b48375ce067f5ca385e6da426636f645b633
Ben Grabow <ben.grabow@metabase.com>
2026-09-09 17:28:31 -0500

    Test registration fixes (#82130)
    * Add missing requires
    
    * Add missing requires
    
    * Apply batched suggestions from code review
    
    Co-authored-by: Ben Grabow <ben.grabow@metabase.com>
    
    * Sort nses
    
    * */keep-me

--- test/metabase/api/common_test.clj
+++ test/metabase/api/common_test.clj
@@ -4,7 +4,9 @@
    [mb.hawk.assert-exprs.approximately-equal :as hawk.approx]
    [metabase.api.common :as api]
    [metabase.events.core :as events]
+   [metabase.measures.models.measure]
    [metabase.models.interface :as mi]
+   [metabase.segments.models.segment]
    [metabase.server.middleware.exceptions :as mw.exceptions]
    [metabase.server.middleware.misc :as mw.misc]
    [metabase.server.middleware.security :as mw.security]
@@ -12,6 +14,9 @@
    [metabase.util.i18n :refer [tru]]
    [methodical.core :as methodical])
   (:import (clojure.lang ExceptionInfo)))
+
+(comment metabase.measures.models.measure/keep-me
+         metabase.segments.models.segment/keep-me)
 
 ;;; TESTS FOR CHECK (ETC)
 
