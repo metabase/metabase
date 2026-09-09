05cf9113f24c3746a058350ff2699b87b9b71c03
Tyler Plude <tsplude@gmail.com>
2026-09-09 07:39:32 -0700

    [GIT-81733] Handle jar native filesystem paths (#81927)
    * return a native filesystem path from get-jar-path
    
    * extract code-location->path and add jimfs-backed regression test

--- test/metabase/util/files_test.clj
+++ test/metabase/util/files_test.clj
@@ -2,7 +2,13 @@
   (:require
    [clojure.test :refer :all]
    [metabase.test :as mt]
-   [metabase.util.files :as u.files]))
+   [metabase.util :as u]
+   [metabase.util.files :as u.files])
+  (:import
+   (com.google.common.jimfs Configuration Jimfs)
+   (java.net URI)))
+
+(set! *warn-on-reflection* true)
 
 (deftest is-regular-file-test
   (mt/with-temp-file [file "temp-file"]
@@ -16,3 +22,13 @@
         (spit file-in-dir "abc") ; create a file in the dir to make sure it exists
         (is (u.files/regular-file? (u.files/get-path file-in-dir)))))
     (is (not (u.files/regular-file? (u.files/get-path dir))))))
+
+(deftest ^:parallel code-location->path-test
+  (testing "%-escapes decode to native paths"
+    (is (= "/tmp/jar location/x.jar"
+           (u.files/code-location->path (URI. "file:/tmp/jar%20location/x.jar")))))
+  (testing "windows-semantics filesystems yield drive-letter native form (#81733)"
+    (with-open [fs (Jimfs/newFileSystem (Configuration/windows))]
+      (let [uri (.toUri (.getPath fs "C:\\jar location\\x.jar" (u/varargs String)))]
+        (is (= "C:\\jar location\\x.jar"
+               (u.files/code-location->path uri)))))))
