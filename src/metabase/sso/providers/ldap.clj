3743eacd279dcc1b0ba0e36daf2dcc57fad5c1bc
Braden Shepherdson <braden@metabase.com>
2026-09-09 16:25:25 -0400

    fix up ::provider/supports-mfa usages to use the custom auth-identity hierarchy

--- src/metabase/sso/providers/ldap.clj
+++ src/metabase/sso/providers/ldap.clj
@@ -18,7 +18,7 @@
 (auth-identity/derive! :provider/ldap :metabase.auth-identity.provider/provider)
 
 ;; LDAP Authenticated users are eligible for our MFA flow
-(derive :provider/ldap :metabase.auth-identity.provider/supports-mfa)
+(auth-identity/derive! :provider/ldap :metabase.auth-identity.provider/supports-mfa)
 
 ;; LDAP is an SSO provider that auto-creates users
 (auth-identity/derive! :provider/ldap :metabase.auth-identity.provider/create-user-if-not-exists)
