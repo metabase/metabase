bbefefcc670767fe115e1d30abff13f138dc7a94
Romeo Van Snick <romeo@romeovansnick.be>
2026-09-08 22:49:56 -0700

    Select the action-creator question instead of building it

--- frontend/src/metabase/querying/action-creator/QueryActionContextProvider/QueryActionContextProvider.tsx
+++ frontend/src/metabase/querying/action-creator/QueryActionContextProvider/QueryActionContextProvider.tsx
@@ -9,28 +9,20 @@ import type {
 import { ActionContext } from "metabase/actions/containers/ActionCreator/ActionContext";
 import type { CreateQueryActionParams } from "metabase/actions/types";
 import { getDefaultFormSettings } from "metabase/actions/utils";
-import type {
-  CardQuestionBuilder,
-  DraftQuestionBuilder,
-} from "metabase/metadata-store";
-import {
-  useQuestionFromCard,
-  useQuestionFromOpts,
-} from "metabase/metadata-store";
+import { useSelector } from "metabase/redux";
 import type Question from "metabase-lib/v1/Question";
 import { getTemplateTagParametersFromCard } from "metabase-lib/v1/parameters/utils/template-tags";
 import type NativeQuery from "metabase-lib/v1/queries/NativeQuery";
 import type {
   ActionFormSettings,
-  Card,
   DatabaseId,
   NativeDatasetQuery,
-  VisualizationSettings,
   WritebackParameter,
   WritebackQueryAction,
 } from "metabase-types/api";
 
 import { QueryActionEditor } from "./QueryActionEditor";
+import { getActionQuestion } from "./selectors";
 import {
   setParameterTypesFromFieldSettings,
   setTemplateTagTypesFromFieldSettings,
@@ -42,61 +34,6 @@ export interface QueryActionContextProviderProps extends ActionContextProviderPr
 
 // ActionCreator uses the NativeQueryEditor, which expects a Question object
 // This utilities help us to work with the WritebackQueryAction as with a Question
-
-function newQuestion(
-  buildDraftQuestion: DraftQuestionBuilder,
-  databaseId?: DatabaseId,
-) {
-  return buildDraftQuestion({
-    DEPRECATED_RAW_MBQL_type: "native",
-    DEPRECATED_RAW_MBQL_databaseId: databaseId,
-  });
-}
-
-function convertActionToQuestionCard(
-  action: WritebackQueryAction,
-): Card<NativeDatasetQuery> {
-  return {
-    id: action.id,
-    entity_id: action.entity_id,
-    created_at: action.created_at,
-    updated_at: action.updated_at,
-    name: action.name,
-    description: action.description,
-    dataset_query: action.dataset_query,
-    display: "action",
-    visualization_settings:
-      // Unjustified type cast. FIXME
-      action.visualization_settings as VisualizationSettings,
-    type: "question",
-    can_write: true,
-    can_restore: false,
-    can_delete: false,
-    public_uuid: null,
-    collection_id: null,
-    collection_position: null,
-    dashboard: null,
-    result_metadata: [],
-    cache_ttl: null,
-    last_query_start: null,
-    average_query_time: null,
-    archived: false,
-    enable_embedding: false,
-    embedding_params: null,
-    initially_published_at: null,
-    can_manage_db: true,
-    dashboard_count: null,
-    dashboard_id: null,
-  };
-}
-
-function convertActionToQuestion(
-  action: WritebackQueryAction,
-  buildQuestionFromCard: CardQuestionBuilder,
-) {
-  const question = buildQuestionFromCard(convertActionToQuestionCard(action));
-  return question.setParameters(action.parameters);
-}
 
 function convertQuestionToAction(
   question: Question,
@@ -126,40 +63,16 @@ function convertQuestionToAction(
   };
 }
 
-interface ResolveQuestionOpts {
-  buildQuestionFromCard: CardQuestionBuilder;
-  buildDraftQuestion: DraftQuestionBuilder;
-  databaseId?: DatabaseId;
-}
-
-function resolveQuestion(
-  action: WritebackQueryAction | undefined,
-  {
-    buildQuestionFromCard,
-    buildDraftQuestion,
-    databaseId,
-  }: ResolveQuestionOpts,
-) {
-  return action
-    ? convertActionToQuestion(action, buildQuestionFromCard)
-    : newQuestion(buildDraftQuestion, databaseId);
-}
-
 export function QueryActionContextProvider({
   initialAction,
   databaseId,
   children,
 }: QueryActionContextProviderProps) {
-  const buildQuestionFromCard = useQuestionFromCard();
-  const buildDraftQuestion = useQuestionFromOpts();
-
-  const [initialQuestion, setInitialQuestion] = useState(
-    resolveQuestion(initialAction, {
-      buildQuestionFromCard,
-      buildDraftQuestion,
-      databaseId,
-    }),
+  const resolvedQuestion = useSelector((state) =>
+    getActionQuestion(state, initialAction, databaseId),
   );
+
+  const [initialQuestion, setInitialQuestion] = useState(resolvedQuestion);
   const initialFormSettings = useMemo(
     () => getDefaultFormSettings(initialAction?.visualization_settings),
     [initialAction?.visualization_settings],
@@ -188,13 +101,8 @@ export function QueryActionContextProvider({
   const canSave = !query.isEmpty();
 
   useEffect(() => {
-    const newQuestion = resolveQuestion(initialAction, {
-      buildQuestionFromCard,
-      buildDraftQuestion,
-      databaseId,
-    });
-    setInitialQuestion(newQuestion);
-    setQuestion(newQuestion);
+    setInitialQuestion(resolvedQuestion);
+    setQuestion(resolvedQuestion);
     // we do not want to update this any time
     // the props or metadata change, only if action id changes
     // eslint-disable-next-line react-hooks/exhaustive-deps
