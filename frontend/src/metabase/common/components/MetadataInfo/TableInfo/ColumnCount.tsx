a7aa531f92a66b35b071820dbf0332d5a769babb
Romeo Van Snick <romeo@romeovansnick.be>
2026-09-10 23:20:42 -0700

    Read TableInfo from the store

--- frontend/src/metabase/common/components/MetadataInfo/TableInfo/ColumnCount.tsx
+++ frontend/src/metabase/common/components/MetadataInfo/TableInfo/ColumnCount.tsx
@@ -1,11 +1,8 @@
 import { msgid, ngettext } from "ttag";
 
-import type Table from "metabase-lib/v1/metadata/Table";
-
 import { Label, LabelContainer } from "../MetadataInfo.styled";
 
-export function ColumnCount({ table }: { table: Table }) {
-  const fieldCount = table.numFields();
+export function ColumnCount({ fieldCount }: { fieldCount: number }) {
   return (
     <LabelContainer color="text-primary">
       <Label>
