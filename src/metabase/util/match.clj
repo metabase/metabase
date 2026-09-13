932d68ed40cc3c64aecf7dd14a06c02cda3de27b
Oleksandr Yakushev <alex@bytopia.org>
2026-09-14 01:43:05 +0300

    [match] Implement replace-all to avoid infinite &recurs

--- src/metabase/util/match.clj
+++ src/metabase/util/match.clj
@@ -427,10 +427,23 @@
       ~form
       ~(when contains-&parents? []))))
 
+(defmacro replace-all
+  "Like [[replace]], but walks the `form` from the inside outwards, thus applying the transform multiple times (in a
+  \"postwalk\" fashion). Doesn't support `&parents` anaphora yet. NB: unlike [[replace]], this macro walks map keys
+  individually. Make sure that the match body doesn't trigger on map keys. "
+  [form & clauses]
+  (let [replace-fn-symb (gensym "replace-")
+        contains-&parents? (contains-symbol? clauses '&parents)]
+    (when contains-&parents?
+      (throw (ex-info "&parents is not supported by replace-all." {})))
+    `(perf/postwalk (fn ~replace-fn-symb [~'&match]
+                      (match-one ~'&match
+                        ~@clauses
+                        ~'_ ~'&match))
+                    ~form)))
+
 (defmacro replace-in
   "Like `replace`, but only replaces things in the part of `x` in the keypath `ks` (i.e. the way to `update-in` works.)"
   {:style/indent :defn}
   [x ks & patterns-and-results]
   `(metabase.util.match.impl/update-in-unless-empty ~x ~ks (fn [x#] (replace x# ~@patterns-and-results))))
-
-;; TODO - it would be useful to have something like a `replace-all` function as well
