ligaturehp/practice-planner-metabase

Polish Slackbot upload handling

src/metabase/slackbot/uploads.clj
@@ -1,5 +1,5 @@
 (ns metabase.slackbot.uploads
-  "CSV upload handling for slackbot."
+  "CSV and TSV upload handling for Slack."
   (:require
    [clojure.java.io :as io]
    [clojure.string :as str]
@@ -12,46 +12,50 @@
 
 (set! *warn-on-reflection* true)
 
-(def ^:private max-file-size-bytes
-  "Maximum file size for CSV uploads (200MB)"
+(def ^:private max-slack-upload-size-bytes
+  "Maximum Slack upload size, independent of the 50 MB HTTP multipart upload limit."
   (* 200 1024 1024))
 
-(def ^:private allowed-csv-filetypes
-  "File types that are allowed for CSV uploads"
+(def ^:private supported-filetypes
+  "Slack file types that can be uploaded."
   #{"csv" "tsv"})
 
-(defn- csv-file?
-  "Whether `file` is a CSV or TSV, going by the filetype Slack reports."
+(defn- supported-file?
+  "Whether Slack reports `file` as a CSV or TSV."
   [{:keys [filetype]}]
-  (contains? allowed-csv-filetypes filetype))
+  (contains? supported-filetypes filetype))
 
 (defn- file-size-error
   "Why `file` is too large to upload, or nil if it is not."
   [{:keys [name size]}]
-  (when (> size max-file-size-bytes)
-    (format "File '%s' exceeds %dMB size limit" name (quot max-file-size-bytes (* 1024 1024)))))
+  (when (> size max-slack-upload-size-bytes)
+    (format "I couldn't upload %s because it is larger than the %d MB limit."
+            name
+            (quot max-slack-upload-size-bytes (* 1024 1024)))))
 
 (defn- upload-target
-  "The database uploads go to, with the schema and table prefix to create tables under, or nil if no
-  database has uploads enabled."
+  "The upload database, schema, and table prefix, or nil when uploads are not configured."
   []
   (when-let [db (upload/current-database)]
     {:db           db
      :schema-name  (:uploads_schema_name db)
      :table-prefix (:uploads_table_prefix db)}))
 
-(defn- process-csv-file
-  "Process a single CSV file upload. Returns a result map with either
-   :model-id/:model-name (success) or :error (failure)."
-  [client {:keys [db schema-name table-prefix]} {:keys [name url_private] :as file}]
+(defn- create-temp-file!
+  "Create a temporary file using the validated Slack file type as its suffix."
+  [filetype]
+  (File/createTempFile "slack-upload-" (str "." filetype)))
+
+(defn- upload-file!
+  "Upload one CSV or TSV file and return its model details or a safe error message."
+  [client {:keys [db schema-name table-prefix]} {:keys [name filetype url_private] :as file}]
   (if-let [size-error (file-size-error file)]
     (do
       (log/warnf "[slackbot] File exceeds size limit: error=%s" size-error)
-      {:error size-error :filename name})
+      {:error size-error, :filename name})
     (try
-      ;; [[upload/create-csv-upload!]] reads the file more than once, so the download has to land on
-      ;; disk first.
-      (let [temp-file (File/createTempFile "slack-upload-" (str "-" name))]
+      ;; [[upload/create-csv-upload!]] reads the file more than once, so download it to disk first.
+      (let [temp-file (create-temp-file! filetype)]
         (try
           (with-open [^InputStream stream (slackbot.client/download-file-stream client url_private)]
             (io/copy stream temp-file))
@@ -62,7 +66,7 @@
                          :schema-name   schema-name
                          :table-prefix  table-prefix
                          :collection-id nil})]
-            (log/infof "[slackbot] File uploaded: model_id=%s" (:id result))
+            (log/infof "[slackbot] File uploaded: model_id=%d" (:id result))
             (analytics/inc! :metabase-slackbot/file-uploads {:result "success"})
             {:filename   name
              :model-id   (:id result)
@@ -70,72 +74,79 @@
           (finally
             (io/delete-file temp-file true))))
       (catch Exception e
-        (log/warnf "[slackbot] File upload failed: error=%s" (ex-message e))
+        (log/warn e "[slackbot] File upload failed" {:filename name})
         (analytics/inc! :metabase-slackbot/file-uploads {:result "error"})
-        {:error (ex-message e) :filename name}))))
+        {:error    (format "I couldn't upload %s because something went wrong. Please try again." name)
+         :filename name}))))
 
-(defn- process-file-uploads
-  "Process all files from a Slack event. Returns a map with:
-   :results - seq of individual file results
-   :skipped - seq of non-CSV filenames that were skipped"
+(defn- upload-files!
+  "Upload supported files and return their results with the names of unsupported files."
   [client target files]
-  (let [{csv-files true other-files false} (group-by csv-file? files)
-        skipped                            (mapv :name other-files)]
+  (let [{supported-files true unsupported-files false} (group-by supported-file? files)
+        skipped                                        (mapv :name unsupported-files)]
     (when (seq skipped)
-      (log/debugf "[slackbot] Skipping %d non-CSV files" (count skipped)))
-    {:results (mapv #(process-csv-file client target %) csv-files)
+      (log/debugf "[slackbot] Skipping %d unsupported files" (count skipped)))
+    {:results (mapv #(upload-file! client target %) supported-files)
      :skipped skipped}))
 
-(defn- assistant-message
-  "One message to inject into the AI request."
+(defn- assistant-history-message
+  "A Metabot message that can be sent directly or added to the AI history."
   [content]
   {:role    :assistant
    :content content})
 
-(def ^:private uploads-not-enabled-message
-  "CSV uploads are not enabled. An administrator needs to configure a database for uploads in Admin > Settings > Uploads.")
+(def ^:private uploads-not-configured-message
+  "Uploads aren't configured yet. Ask your Metabase admin to choose an upload database in Admin > Settings > Uploads.")
 
-(def ^:private no-permission-message
-  "You don't have permission to upload files. Contact your Metabase administrator.")
+(def ^:private upload-unavailable-message
+  "I can't upload files to the configured database. Ask your Metabase admin to check the upload settings and your permissions.")
 
-(defn- build-upload-system-messages
-  "Messages describing what became of each attached file."
+(defn- uploaded-files-message
+  "A Metabot message describing successful uploads."
+  [successes]
+  (if (= 1 (count successes))
+    (let [{:keys [filename model-id model-name]} (first successes)]
+      (format "I uploaded %s as the Metabase model %s (ID %d). I can help you query it." filename model-name model-id))
+    (format "I uploaded these files as Metabase models: %s. I can help you query them."
+            (str/join ", " (map #(format "%s as %s (ID %d)"
+                                         (:filename %)
+                                         (:model-name %)
+                                         (:model-id %))
+                                successes)))))
+
+(defn- failed-files-message
+  "A Metabot message describing failed uploads."
+  [failures]
+  (str/join " " (map :error failures)))
+
+(defn- skipped-files-message
+  "A Metabot message describing unsupported attachments."
+  [skipped]
+  (format "I can only upload CSV and TSV files, so I skipped the following: %s." (str/join ", " skipped)))
+
+(defn- build-upload-history
+  "Metabot messages describing what became of each attached file."
   [{:keys [results skipped]}]
   (let [successes (filter :model-id results)
         failures  (filter :error results)]
     (cond-> []
       (seq successes)
-      (conj (assistant-message
-             (format "CSV files attached to this message are now available as models in Metabase: %s. You can help the user query this data."
-                     (str/join ", " (map #(format "%s (model ID: %s)"
-                                                  (:filename %)
-                                                  (:model-id %))
-                                         successes)))))
+      (conj (assistant-history-message (uploaded-files-message successes)))
 
       (seq failures)
-      (conj (assistant-message
-             (format "These files attached to this message could not be uploaded: %s. Explain the errors to the user."
-                     (str/join ", " (map #(format "%s: %s"
-                                                  (:filename %)
-                                                  (:error %))
-                                         failures)))))
+      (conj (assistant-history-message (failed-files-message failures)))
 
       (seq skipped)
-      (conj (assistant-message
-             (format "These files attached to this message are neither CSV nor TSV, so they were not uploaded: %s. Let the user know that only CSV and TSV files can be uploaded."
-                     (str/join ", " skipped)))))))
+      (conj (assistant-history-message (skipped-files-message skipped))))))
 
-(defn handle-file-uploads
-  "Upload the CSV files attached to a Slack message, downloading each with `client`.
-  Returns nil if there are no files, otherwise a map of:
-   :upload-result   - per-file results and the names of skipped files, absent if nothing was attempted
-   :system-messages - messages to inject into the AI request describing the outcome"
+(defn handle-file-uploads!
+  "Upload attached CSV and TSV files with `client` and return Metabot `:extra-history` with any `:upload-result`, or nil when no files are attached."
   [client files]
   (when (seq files)
     (if-let [{:keys [db schema-name] :as target} (upload-target)]
       (if-not (upload/can-create-upload? db schema-name)
-        {:system-messages [(assistant-message no-permission-message)]}
-        (let [result (process-file-uploads client target files)]
-          {:upload-result   result
-           :system-messages (build-upload-system-messages result)}))
-      {:system-messages [(assistant-message uploads-not-enabled-message)]})))
+        {:extra-history [(assistant-history-message upload-unavailable-message)]}
+        (let [result (upload-files! client target files)]
+          {:upload-result result
+           :extra-history (build-upload-history result)}))
+      {:extra-history [(assistant-history-message uploads-not-configured-message)]})))

metabase/metabase

src/metabase/slackbot/uploads.clj
@@ -1,5 +1,5 @@
 (ns metabase.slackbot.uploads
-  "CSV upload handling for slackbot."
+  "CSV and TSV upload handling for Slack."
   (:require
    [clojure.java.io :as io]
    [clojure.string :as str]
@@ -12,39 +12,41 @@
 
 (set! *warn-on-reflection* true)
 
-(def ^:private max-file-size-bytes
-  "Maximum file size for CSV uploads (200MB)"
+(def ^:private max-slack-upload-size-bytes
+  "Maximum Slack upload size, independent of the 50 MB HTTP multipart upload limit."
   (* 200 1024 1024))
 
-(def ^:private allowed-csv-filetypes
-  "File types that are allowed for CSV uploads"
+(def ^:private supported-filetypes
+  "Slack file types that can be uploaded."
   #{"csv" "tsv"})
 
-(defn- csv-file?
-  "Whether `file` is a CSV or TSV, going by the filetype Slack reports."
+(defn- supported-file?
+  "Whether Slack reports `file` as a CSV or TSV."
   [{:keys [filetype]}]
-  (contains? allowed-csv-filetypes filetype))
+  (contains? supported-filetypes filetype))
 
 (defn- remote-file?
-  "Whether a Slack file is stored outside Slack, so its URL and size come from the app that registered it rather
-  than from Slack. Those are never downloaded, whatever filetype they claim."
+  "Whether Slack reports `file` as stored outside Slack, so its URL and size come from the app that registered it
+  rather than from Slack. Those are never downloaded, whatever filetype they claim."
   [{:keys [mode]}]
   (= "external" mode))
 
 (defn- size-limit-message
-  "The user-facing message for a file over [[max-file-size-bytes]]."
+  "The message for a file over [[max-slack-upload-size-bytes]]."
   [filename]
-  (format "File '%s' exceeds %dMB size limit" filename (quot max-file-size-bytes (* 1024 1024))))
+  (format "I couldn't upload %s because it is larger than the %d MB limit."
+          filename
+          (quot max-slack-upload-size-bytes (* 1024 1024))))
 
 (defn- file-size-error
   "Why `file` is too large to upload, or nil if it is not."
   [{:keys [name size]}]
-  (when (> size max-file-size-bytes)
+  (when (> size max-slack-upload-size-bytes)
     (size-limit-message name)))
 
 (defn- copy-to-file!
-  "Copy `in` into `file`, refusing more than [[max-file-size-bytes]]. The size on the event is only what the sender
-  declared, so the limit has to hold as the bytes arrive."
+  "Copy `in` into `file`, refusing more than [[max-slack-upload-size-bytes]]. The size on the event is only what
+  the sender declared, so the limit has to hold as the bytes arrive."
   [^InputStream in ^File file filename]
   (let [buf (byte-array 8192)]
     (with-open [^OutputStream out (io/output-stream file)]
@@ -52,50 +54,34 @@
         (let [n (.read in buf)]
           (when-not (neg? n)
             (let [total (+ written n)]
-              (when (> total max-file-size-bytes)
-                (throw (ex-info (size-limit-message filename) {:status-code 400})))
+              (when (> total max-slack-upload-size-bytes)
+                (throw (ex-info (size-limit-message filename) {:status-code 400, ::over-size-limit true})))
               (.write out buf 0 n)
               (recur total))))))))
 
-(def ^:private generic-upload-error
-  ;; Handed a bare internal-error note, the model told the user no file had been attached.
-  "Metabase hit an internal error while saving the file, so the upload didn't finish. Ask a Metabase admin to check the server logs.")
-
-(defn- upload-error-message
-  "Returns failure text that is safe to hand to the model.
-   The upload layer's own 4xx errors carry a message written for the user and have no cause. Anything else,
-   including the 4xx that relays a raw driver error along with its cause, gets [[generic-upload-error]], since
-   driver and JDBC messages can name hosts or accounts."
-  [e]
-  (let [{:keys [status-code]} (ex-data e)]
-    (if (and (integer? status-code)
-             (<= 400 status-code 499)
-             (nil? (ex-cause e))
-             (not (str/blank? (ex-message e))))
-      (ex-message e)
-      generic-upload-error)))
-
 (defn- upload-target
-  "The database uploads go to, with the schema and table prefix to create tables under, or nil if no
-  database has uploads enabled."
+  "The upload database, schema, and table prefix, or nil when uploads are not configured."
   []
   (when-let [db (upload/current-database)]
     {:db           db
      :schema-name  (:uploads_schema_name db)
      :table-prefix (:uploads_table_prefix db)}))
 
-(defn- process-csv-file
-  "Process a single CSV file upload. Returns a result map with either
-   :model-id/:model-name (success) or :error (failure)."
-  [client {:keys [db schema-name table-prefix]} {:keys [name url_private] :as file}]
+(defn- create-temp-file!
+  "Create a temporary file using the validated Slack file type as its suffix."
+  [filetype]
+  (File/createTempFile "slack-upload-" (str "." filetype)))
+
+(defn- upload-file!
+  "Upload one CSV or TSV file and return its model details or a safe error message."
+  [client {:keys [db schema-name table-prefix]} {:keys [name filetype url_private] :as file}]
   (if-let [size-error (file-size-error file)]
     (do
       (log/warnf "[slackbot] File exceeds size limit: error=%s" size-error)
-      {:error size-error :filename name})
+      {:error size-error, :filename name})
     (try
-      ;; [[upload/create-csv-upload!]] reads the file more than once, so the download has to land on
-      ;; disk first.
-      (let [temp-file (File/createTempFile "slack-upload-" (str "-" name))]
+      ;; [[upload/create-csv-upload!]] reads the file more than once, so download it to disk first.
+      (let [temp-file (create-temp-file! filetype)]
         (try
           (with-open [^InputStream stream (slackbot.client/download-file-stream client url_private)]
             (copy-to-file! stream temp-file name))
@@ -106,7 +92,7 @@
                          :schema-name   schema-name
                          :table-prefix  table-prefix
                          :collection-id nil})]
-            (log/infof "[slackbot] File uploaded: model_id=%s" (:id result))
+            (log/infof "[slackbot] File uploaded: model_id=%d" (:id result))
             (analytics/inc! :metabase-slackbot/file-uploads {:result "success"})
             {:filename   name
              :model-id   (:id result)
@@ -114,83 +100,106 @@
           (finally
             (io/delete-file temp-file true))))
       (catch Exception e
-        (log/warnf e "[slackbot] File upload failed: error=%s" (ex-message e))
+        (log/warn e "[slackbot] File upload failed" {:filename name})
         (analytics/inc! :metabase-slackbot/file-uploads {:result "error"})
-        {:error (upload-error-message e) :filename name}))))
+        ;; Repeat back only the size limit and the upload module's own 4xx errors, which describe a problem in the file.
+        ;; The upload module rethrows driver and JDBC errors as a 4xx carrying a cause, and those can name hosts.
+        {:error    (cond
+                     (::over-size-limit (ex-data e))
+                     (ex-message e)
 
-(defn- process-file-uploads
-  "Process all files from a Slack event. Returns a map with:
-   :results - seq of individual file results
-   :skipped - seq of non-CSV filenames that were skipped
-   :remote  - seq of filenames refused for being stored outside Slack"
+                     (and (let [status (:status-code (ex-data e))]
+                            (and (integer? status) (<= 400 status 499)))
+                          (nil? (ex-cause e))
+                          (not (str/blank? (ex-message e))))
+                     (format "I couldn't upload %s: %s" name (ex-message e))
+
+                     :else
+                     (format "I couldn't upload %s because something went wrong. Please try again." name))
+         :filename name}))))
+
+(defn- upload-files!
+  "Upload supported files and return their results with the names of unsupported and remote files."
   [client target files]
-  (let [{remote-files true local-files false}  (group-by remote-file? files)
-        {csv-files true other-files false}     (group-by csv-file? local-files)
-        skipped                                (mapv :name other-files)
-        remote                                 (mapv :name remote-files)]
+  (let [{remote-files true local-files false}          (group-by remote-file? files)
+        {supported-files true unsupported-files false} (group-by supported-file? local-files)
+        skipped                                        (mapv :name unsupported-files)
+        remote                                         (mapv :name remote-files)]
     (when (seq skipped)
-      (log/debugf "[slackbot] Skipping %d non-CSV files" (count skipped)))
+      (log/debugf "[slackbot] Skipping %d unsupported files" (count skipped)))
     (when (seq remote)
       (log/debugf "[slackbot] Refusing %d files stored outside Slack" (count remote)))
-    {:results (mapv #(process-csv-file client target %) csv-files)
+    {:results (mapv #(upload-file! client target %) supported-files)
      :skipped skipped
      :remote  remote}))
 
-(defn- assistant-message
-  "One message to inject into the AI request."
+(defn- assistant-history-message
+  "A Metabot message that can be sent directly or added to the AI history."
   [content]
   {:role    :assistant
    :content content})
 
-(def ^:private uploads-not-enabled-message
-  "CSV uploads are not enabled. An administrator needs to configure a database for uploads in Admin > Settings > Uploads.")
+(def ^:private uploads-not-configured-message
+  "Uploads aren't configured yet. Ask your Metabase admin to choose an upload database in Admin > Settings > Uploads.")
 
-(def ^:private no-permission-message
-  "You don't have permission to upload files. Contact your Metabase administrator.")
+(def ^:private upload-unavailable-message
+  "I can't upload files to the configured database. Ask your Metabase admin to check the upload settings and your permissions.")
 
-(defn- build-upload-system-messages
-  "Messages describing what became of each attached file."
+(defn- uploaded-files-message
+  "A Metabot message describing successful uploads."
+  [successes]
+  (if (= 1 (count successes))
+    (let [{:keys [filename model-id model-name]} (first successes)]
+      (format "I uploaded %s as the Metabase model %s (ID %d). I can help you query it." filename model-name model-id))
+    (format "I uploaded these files as Metabase models: %s. I can help you query them."
+            (str/join ", " (map #(format "%s as %s (ID %d)"
+                                         (:filename %)
+                                         (:model-name %)
+                                         (:model-id %))
+                                successes)))))
+
+(defn- failed-files-message
+  "A Metabot message describing failed uploads."
+  [failures]
+  (str/join " " (map :error failures)))
+
+(defn- skipped-files-message
+  "A Metabot message describing unsupported attachments."
+  [skipped]
+  (format "I can only upload CSV and TSV files, so I skipped the following: %s." (str/join ", " skipped)))
+
+(defn- remote-files-message
+  "A Metabot message describing attachments stored outside Slack."
+  [remote]
+  (format "I can only upload files stored in Slack itself, so I skipped the following: %s."
+          (str/join ", " remote)))
+
+(defn- build-upload-history
+  "Metabot messages describing what became of each attached file."
   [{:keys [results skipped remote]}]
   (let [successes (filter :model-id results)
         failures  (filter :error results)]
     (cond-> []
       (seq successes)
-      (conj (assistant-message
-             (format "CSV files attached to this message are now available as models in Metabase: %s. You can help the user query this data."
-                     (str/join ", " (map #(format "%s (model ID: %s)"
-                                                  (:filename %)
-                                                  (:model-id %))
-                                         successes)))))
+      (conj (assistant-history-message (uploaded-files-message successes)))
 
       (seq failures)
-      (conj (assistant-message
-             (format "These files attached to this message could not be uploaded: %s. Explain the errors to the user."
-                     (str/join ", " (map #(format "%s: %s"
-                                                  (:filename %)
-                                                  (:error %))
-                                         failures)))))
+      (conj (assistant-history-message (failed-files-message failures)))
 
       (seq skipped)
-      (conj (assistant-message
-             (format "These files attached to this message are neither CSV nor TSV, so they were not uploaded: %s. Let the user know that only CSV and TSV files can be uploaded."
-                     (str/join ", " skipped))))
+      (conj (assistant-history-message (skipped-files-message skipped)))
 
       (seq remote)
-      (conj (assistant-message
-             (format "These files attached to this message are stored outside Slack, so Metabase cannot upload them: %s. Let the user know the file has to be uploaded to Slack itself."
-                     (str/join ", " remote)))))))
+      (conj (assistant-history-message (remote-files-message remote))))))
 
-(defn handle-file-uploads
-  "Upload the CSV files attached to a Slack message, downloading each with `client`.
-  Returns nil if there are no files, otherwise a map of:
-   :upload-result   - per-file results and the names of skipped files, absent if nothing was attempted
-   :system-messages - messages to inject into the AI request describing the outcome"
+(defn handle-file-uploads!
+  "Upload attached CSV and TSV files with `client` and return Metabot `:extra-history` with any `:upload-result`, or nil when no files are attached."
   [client files]
   (when (seq files)
     (if-let [{:keys [db schema-name] :as target} (upload-target)]
       (if-not (upload/can-create-upload? db schema-name)
-        {:system-messages [(assistant-message no-permission-message)]}
-        (let [result (process-file-uploads client target files)]
-          {:upload-result   result
-           :system-messages (build-upload-system-messages result)}))
-      {:system-messages [(assistant-message uploads-not-enabled-message)]})))
+        {:extra-history [(assistant-history-message upload-unavailable-message)]}
+        (let [result (upload-files! client target files)]
+          {:upload-result result
+           :extra-history (build-upload-history result)}))
+      {:extra-history [(assistant-history-message uploads-not-configured-message)]})))