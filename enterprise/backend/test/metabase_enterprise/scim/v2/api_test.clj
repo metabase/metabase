a9e4f33f4dc0fdb8c785fab6ce0fe7ad2d299ab7
Timofei <timofei@metabase.com>
2026-09-14 13:58:31 +0400

    Exclude data app permission groups from SCIM

--- enterprise/backend/test/metabase_enterprise/scim/v2/api_test.clj
+++ enterprise/backend/test/metabase_enterprise/scim/v2/api_test.clj
@@ -103,6 +103,15 @@
                             :display "Test Group"}]
                 :meta     {:resourceType "User"}}
                response)))))
+    (testing "Data-app group memberships are not exposed"
+      (mt/with-temp [:model/User                       user      {:email "scim-app-test@metabase.com"}
+                     :model/PermissionsGroup           group     {:name "Test Group"}
+                     :model/PermissionsGroup           app-group {:name "App Group" :is_data_app_group true}
+                     :model/PermissionsGroupMembership _         {:user_id (:id user) :group_id (:id group)}
+                     :model/PermissionsGroupMembership _         {:user_id (:id user) :group_id (:id app-group)}]
+        (let [entity-id (t2/select-one-fn :entity_id :model/User :id (:id user))
+              response  (scim-client :get 200 (format "ee/scim/v2/Users/%s" entity-id))]
+          (is (= ["Test Group"] (map :display (:groups response)))))))
     (testing "404 is returned when fetching a non-existent user"
       (scim-client :get 404 (format "ee/scim/v2/Users/%s" (random-uuid))))))
 
@@ -361,7 +370,17 @@
                   (is (= 0 (count (get response :Resources))))))
               (testing "Error if unsupported filter operation is provided"
                 (scim-client :get 400 (format "ee/scim/v2/Users?filter=%s"
-                                              (codec/url-encode "displayName ne \"Group 1\"")))))))))
+                                              (codec/url-encode "displayName ne \"Group 1\""))))
+              (testing "Data-app groups are not listed"
+                (mt/with-temp [:model/PermissionsGroup app-group {:name "App Group" :is_data_app_group true}]
+                  (let [app-entity-id (t2/select-one-fn :entity_id :model/PermissionsGroup :id (:id app-group))
+                        response      (scim-client :get 200 "ee/scim/v2/Groups")]
+                    (is (not (contains? (into #{} (map :id) (:Resources response)) app-entity-id)))
+                    (is (= (count (:Resources response)) (:totalResults response))))
+                  (let [response (scim-client :get 200 (format "ee/scim/v2/Groups?filter=%s"
+                                                               (codec/url-encode "displayName eq \"App Group\"")))]
+                    (is (= 0 (get response :totalResults)))
+                    (is (= 0 (count (get response :Resources))))))))))))
     (deftest fetch-group-test
       (with-scim-setup!
         (testing "A single group can be fetched in the SCIM format by entity ID with its members"
@@ -384,6 +403,10 @@
           (let [entity-ids (t2/select-fn-set :entity_id :model/PermissionsGroup
                                              {:where [:in :id #{(:id (perms-group/admin)) (:id (perms-group/all-users))}]})]
             (doseq [entity-id entity-ids]
+              (scim-client :get 404 (format "ee/scim/v2/Groups/%s" entity-id)))))
+        (testing "404 is returned when fetching a data-app group"
+          (mt/with-temp [:model/PermissionsGroup app-group {:name "App Group" :is_data_app_group true}]
+            (let [entity-id (t2/select-one-fn :entity_id :model/PermissionsGroup :id (:id app-group))]
               (scim-client :get 404 (format "ee/scim/v2/Groups/%s" entity-id)))))))))
 
 (deftest create-group-test
@@ -418,7 +441,19 @@
                           (t2/hydrate :members))]
             (is (= new-group-name (:name group)))
             (is (= 1 (count (:members group))))
-            (is (= (mt/user->id :crowberto) (-> group :members first :user_id)))))))))
+            (is (= (mt/user->id :crowberto) (-> group :members first :user_id)))))))
+    (testing "404 is returned when trying to update a data-app group, and its membership is left alone"
+      (mt/with-temp [:model/PermissionsGroup app-group {:name "App Group" :is_data_app_group true}
+                     :model/PermissionsGroupMembership _ {:user_id (mt/user->id :rasta) :group_id (:id app-group)}]
+        (let [entity-id    (t2/select-one-fn :entity_id :model/PermissionsGroup :id (:id app-group))
+              group-update {:schemas     ["urn:ietf:params:scim:schemas:core:2.0:Group"]
+                            :id          entity-id
+                            :displayName "Renamed App Group"
+                            :members     [{:value (t2/select-one-fn :entity_id :model/User :id (mt/user->id :crowberto))}]}]
+          (scim-client :put 404 (format "ee/scim/v2/Groups/%s" entity-id) group-update)
+          (is (= "App Group" (t2/select-one-fn :name :model/PermissionsGroup :id (:id app-group))))
+          (is (= #{(mt/user->id :rasta)}
+                 (t2/select-fn-set :user_id :model/PermissionsGroupMembership :group_id (:id app-group)))))))))
 
 (deftest delete-group-test
   (with-scim-setup!
@@ -435,4 +470,9 @@
       (let [entity-ids (t2/select-fn-set :entity_id :model/PermissionsGroup
                                          {:where [:in :id #{(:id (perms-group/admin)) (:id (perms-group/all-users))}]})]
         (doseq [entity-id entity-ids]
-          (scim-client :delete 404 (format "ee/scim/v2/Groups/%s" entity-id)))))))
+          (scim-client :delete 404 (format "ee/scim/v2/Groups/%s" entity-id)))))
+    (testing "404 is returned when trying to delete a data-app group, and it survives"
+      (mt/with-temp [:model/PermissionsGroup app-group {:name "App Group" :is_data_app_group true}]
+        (let [entity-id (t2/select-one-fn :entity_id :model/PermissionsGroup :id (:id app-group))]
+          (scim-client :delete 404 (format "ee/scim/v2/Groups/%s" entity-id))
+          (is (t2/exists? :model/PermissionsGroup :id (:id app-group))))))))
