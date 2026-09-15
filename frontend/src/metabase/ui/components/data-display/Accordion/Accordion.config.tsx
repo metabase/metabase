97e018338aa9639517f65749f6060f22e4af0b29
Anna Tomka <anna@metabase.com>
2026-09-15 18:42:29 +0200

    feat(ui): restyle Accordion from Figma

--- frontend/src/metabase/ui/components/data-display/Accordion/Accordion.config.tsx
+++ frontend/src/metabase/ui/components/data-display/Accordion/Accordion.config.tsx
@@ -1,12 +1,24 @@
 import { Accordion } from "@mantine/core";
 
+import { Icon } from "../../icons";
+
 import AccordionStyles from "./Accordion.module.css";
+
+const CHEVRON_SIZE = 12;
 
 export const accordionOverrides = {
   Accordion: Accordion.extend({
+    defaultProps: {
+      variant: "separated",
+      radius: "sm",
+      chevron: <Icon name="chevrondown" size={CHEVRON_SIZE} />,
+      chevronSize: CHEVRON_SIZE,
+    },
     classNames: {
+      root: AccordionStyles.root,
       control: AccordionStyles.control,
       label: AccordionStyles.label,
+      icon: AccordionStyles.icon,
       item: AccordionStyles.item,
       content: AccordionStyles.content,
       chevron: AccordionStyles.chevron,
