1c483b6eb8d34f3217cfa16369eabc1846efcb3b
Kevin Kelleher <kevin.kelleher@metabase.com>
2026-09-17 13:56:49 -0700

    Fix static viz Row Chart failures on remapped columns (#82684)
    * make remapping idempotent
    
    * copy the existing remapping so it doesn't get mutated

--- frontend/src/metabase/visualizations/types/columns.ts
+++ frontend/src/metabase/visualizations/types/columns.ts
@@ -3,7 +3,6 @@ import type { DatasetColumn, DatasetData } from "metabase-types/api";
 export type RemappingHydratedDatasetColumn = DatasetColumn & {
   remapped_from_index?: number;
   remapped_to_column?: DatasetColumn;
-  remapping?: Map<any, any>;
 };
 
 export type RemappingHydratedChartData = DatasetData & {
