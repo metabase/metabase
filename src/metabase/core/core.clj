b245d4776114fd557ee5b1394b629b7216c45661
Braden Shepherdson <braden@metabase.com>
2026-09-23 10:52:12 -0400

    Fix a busted `:require` from the previous commit

--- src/metabase/core/core.clj
+++ src/metabase/core/core.clj
@@ -18,8 +18,8 @@
    [metabase.embedding.settings :as embed.settings]
    [metabase.embeddings.startup :as embeddings.startup]
    [metabase.events.core :as events]
-   [metabase.file-watcher.core :as file-watcher]
    [metabase.initialization-status.core :as init-status]
+   [metabase.jekyll-mode.core :as jekyll-mode]
    [metabase.llm.startup :as llm.startup]
    [metabase.logger.core :as logger]
    [metabase.metrics.core :as metrics]
@@ -254,7 +254,7 @@
   (task/start-scheduler!)
   (queue/start-listeners!)
   (init-status/set-complete!)
-  (file-watcher/start!)
+  (jekyll-mode/start!)
   (log/info "Metabase Initialization COMPLETE"))
 
 (defn init!
