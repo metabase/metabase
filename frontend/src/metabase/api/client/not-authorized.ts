c2919c3697072d1f843ef1ccbeb2ca05b2b9cb58
Kamil Mielnik <kamil@kamilmielnik.com>
2026-09-24 21:34:22 +0700

    Add a TODO

--- frontend/src/metabase/api/client/not-authorized.ts
+++ frontend/src/metabase/api/client/not-authorized.ts
@@ -6,6 +6,7 @@ const NOT_AUTHORIZED_PAGE_TRIGGERS = [
 ];
 
 // If any of these receives a 403, we should display the "not authorized" page.
+// TODO: get rid of this awful hack.
 export function shouldShowNotAuthorizedPage(url: string): boolean {
   return NOT_AUTHORIZED_PAGE_TRIGGERS.some((regex) => regex.test(url));
 }
