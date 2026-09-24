a2c192778002d5e1104cf9829961b3544934f204
Andrei Chernykh <andrei.chernykh@metabase.com>
2026-09-24 16:23:05 -0400

    Migrate the pin drop zone off Emotion

--- frontend/src/metabase/common/collections/components/PinDropZone/PinDropZone.tsx
+++ frontend/src/metabase/common/collections/components/PinDropZone/PinDropZone.tsx
@@ -1,26 +1,45 @@
-import type { PinDropTargetRenderArgs } from "./PinDropZone.styled";
-import {
-  PinDropTargetIndicator,
-  StyledPinDropTarget,
-} from "./PinDropZone.styled";
+import cx from "classnames";
+
+import { PinDropTarget } from "metabase/common/components/dnd/PinDropTarget";
+import { Box, rem } from "metabase/ui";
+
+import S from "./PinDropZone.module.css";
 
 type PinDropZoneProps = {
   variant: "pin" | "unpin";
   empty?: boolean;
 };
 
+type PinDropTargetRenderArgs = {
+  hovered: boolean;
+  highlighted: boolean;
+};
+
 function PinDropZone({ variant, empty, ...props }: PinDropZoneProps) {
   return (
-    <StyledPinDropTarget
+    <PinDropTarget
+      className={S.dropTarget}
       variant={variant}
       pinIndex={variant === "pin" ? 1 : null}
       hideUntilDrag
       {...props}
     >
-      {(args: PinDropTargetRenderArgs) => (
-        <PinDropTargetIndicator empty={empty} {...args} />
+      {({ hovered, highlighted }: PinDropTargetRenderArgs) => (
+        <Box
+          className={cx(S.indicator, {
+            [S.hovered]: hovered,
+            [S.empty]: empty,
+          })}
+          display={hovered || highlighted ? undefined : "none"}
+          pos="absolute"
+          top={0}
+          bottom={0}
+          left={0}
+          right={0}
+          mih={rem(32)}
+        />
       )}
-    </StyledPinDropTarget>
+    </PinDropTarget>
   );
 }
 
