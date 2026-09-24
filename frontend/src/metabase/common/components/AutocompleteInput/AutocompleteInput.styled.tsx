8317274709ca46915e51034e20b0e9400f688472
Andrei Chernykh <andrei.chernykh@metabase.com>
2026-09-24 09:46:29 -0700

    Migrate AutocompleteInput dropdown off Emotion (#82948)

--- frontend/src/metabase/common/components/AutocompleteInput/AutocompleteInput.styled.tsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-restricted-imports
-import styled from "@emotion/styled";
-
-import { SelectList } from "metabase/common/components/SelectList";
-
-export const OptionsList = styled(SelectList)`
-  padding: 0.5rem;
-  min-width: 300px;
-`;
